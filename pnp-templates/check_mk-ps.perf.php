c002218e3ce49e89273f2c74910520f1d0778877
Mathias Kettner <mk@Eiger.mathias-kettner.de>
2009-12-02 19:08:54 +0100

    New check ps.perf: ps with performance data

---
+++ pnp-templates/check_mk-ps.perf.php
@@ -0,0 +1,42 @@
+<?php
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2009             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+# 
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+# 
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+$vertical = "count";
+$format = "%3.0lf";
+$upto = max(20, $CRIT[1]);
+$color = "8040f0";
+$line = "202060";
+
+$opt[1] = " --vertical-label \"count\" -X0 -L5 -l 0 -u $upto --title \"Number of Processes\" ";
+
+$def[1] = "DEF:count=$rrdfile:$DS[1]:MAX ";
+$def[1] .= "AREA:count#$color:\"Processes\"     ";
+$def[1] .= "LINE1:count#$line:\"\" ";
+$def[1] .= "GPRINT:count:LAST:\"Current\: $format\" ";
+$def[1] .= "GPRINT:count:MAX:\"Maximum\: $format \" ";
+$def[1] .= "HRULE:$WARN[1]#FFFF00:\"Warning at $WARN[1]\" ";
+$def[1] .= "HRULE:$CRIT[1]#FF0000:\"Critical at $CRIT[1]\" ";
+
+?>

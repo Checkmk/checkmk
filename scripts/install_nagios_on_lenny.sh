0ca7d82e9a4ea06c9dfd0b3a08b75cb5ebf48d9f
Mathias Kettner <mk@mathias-kettner.de>
2009-12-12 16:55:37 +0100

    Switched version to 1.1.1rc

--- scripts/install_nagios_on_lenny.sh
+++ scripts/install_nagios_on_lenny.sh
@@ -27,7 +27,7 @@
 
 VERSION=${1:-3.2.0}
 PNPVERSION=${2:-0.4.14}
-MKVERSION=${3:-1.1.0}
+MKVERSION=${3:-1.1.1rc}
 NAGIOS_MIRROR=dfn
 NAGVIS_URL='http://nagvis.git.sourceforge.net/git/gitweb.cgi?p=nagvis/nagvis;a=snapshot;h=8f1c2e25a7a73deaf2b721d482086241be94ee66;sf=tgz'
 

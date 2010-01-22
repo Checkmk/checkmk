69f4ab495f6fa99f9f63322795c62afa5c25d91b
root <root@nagios01.biochem.mpg.de>
2010-01-22 11:43:26 +0100

    install_nagios.sh: SuSE: add missing package php5-mbstring

--- scripts/install_nagios.sh
+++ scripts/install_nagios.sh
@@ -170,7 +170,7 @@ then
    zypper update
    zypper -n install apache2 mailx apache2-mod_python apache2-mod_php5 php5-gd gd-devel \
 	xinetd wget xorg-x11-libXpm-devel psmisc less graphviz-devel graphviz-gd \
-	php5-sqlite php5-gettext python-rrdtool php5-zlib php5-sockets
+	php5-sqlite php5-gettext python-rrdtool php5-zlib php5-sockets php5-mbstring
 else
    yum update
    yum -y install httpd gcc mailx php php-gd gd-devl xinetd wget psmisc less mod_python \

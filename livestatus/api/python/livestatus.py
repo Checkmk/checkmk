d34a9d7f2521209f51d77032f9a96bd200862914
Mathias Kettner <mk@mathias-kettner.de>
2010-02-04 14:56:32 +0100

    Livestatus: new multisite API

--- livestatus/api/python/livestatus.py
+++ livestatus/api/python/livestatus.py
@@ -24,6 +24,7 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
+import socket
 
 """MK Livestatus Python API
 
@@ -54,64 +55,16 @@ class MKLivestatusSocketError(MKLivestatusException):
    def __init__(self, reason):
       MKLivestatusException.__init__(self, reason)
 
-class MKLivestatusQueryError(MKLivestatusException):
+class MKLivestatusConfigError(MKLivestatusException):
    def __init__(self, reason):
       MKLivestatusException.__init__(self, reason)
 
-class connection:
-   """A connection to MK Livestatus. The connection to the socket 
-      is either created by calling connect() or by issuing a query().
-      The connection is closed if the object is destroyed or if a
-      socket error occurs. 
-   """
+class MKLivestatusQueryError(MKLivestatusException):
+   def __init__(self, code, reason):
+      MKLivestatusException.__init__(self, "%s: %s" % (code, reason))
+      self.code = code
 
-   def __init__(self, socketpath):
-      """Create a new connection to a MK Livestatus socket"""
-      self.socketpath = socketpath
-      self.socket_state = DOWN
-
-   def connect(self):
-      """Does the actual connect to the UNIX socket"""
-      import socket
-      try:
-	 self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-	 self.socket.connect(self.socketpath)
-	 self.socket_state = UP
-      except Exception, e:
-         self.socket_state = DOWN
-	 raise MKLivestatusSocketError(str(e))
-
-   def query(self, query, add_headers = ""):
-      """Private function for querying Livestatus. Better use the
-	 other variants of query_...()"""
-      if self.socket_state != UP:
-	 self.connect()
-
-      if not query.endswith("\n"):
-	 query += "\n"
-      query += add_headers
-      if not query.endswith("\n"):
-	 query += "\n"
-      query += "OutputFormat: json\nKeepAlive: on\nResponseHeader: fixed16\n\n"
-
-      try:
-          self.socket.send(query)
-      except e:
-	  self.socket_state = DOWN
-	  raise MKLivestatusSocketError(str(e))
-
-      resp = self.receive_data(16)
-      code = resp[0:3]
-      length = int(resp[4:15].lstrip())
-      data = self.receive_data(length)
-      if code == "200":
-         try:
-	    return eval(data)
-         except:
-	    raise MKLivestatusQueryError("Invalid response from Livestatus")
-      else:
-	 raise MKLivestatusQueryError(data.strip())
-
+class Helpers:
    def query_value(self, query):
       """Issues a query that returns exactly one line and one columns and returns 
 	 the response as a single value"""
@@ -149,15 +102,131 @@ class connection:
 	 result.append(dict(zip(headers, line)))
       return result
 
-   def receive_data(self, size):
-      try:
-	 result = ""
-	 while size > 0:
+class BaseConnection:
+    def __init__(self, socketurl):
+	"""Create a new connection to a MK Livestatus socket"""
+	self.socketurl = socketurl
+	self.socket_state = DOWN
+	self.socket_target = None
+	self.create_socket(socketurl)
+
+    def set_timeout(self, timeout):
+	self.socket.settimeout(timeout)
+ 
+    def create_socket(self, url):
+	parts = url.split(":")
+	if parts[0] == "unix":
+	    if len(parts) != 2:
+		raise MKLivestatusConfigError("Invalid livestatus unix url: %s. Correct example is 'unix:/var/run/nagios/rw/live'" % url)
+	    self.socket_target = parts[1]
+	    self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+	elif parts[0] == "tcp":
+	    try:
+		host = parts[1]
+		port = int(parts[2])
+	    except:
+		raise MKLivestatusConfigError("Invalid livestatus tcp url '%s'. Correct example is 'tcp:somehost:6557'" % url)
+	    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    self.socket_target = ( host, port )
+
+    def connect(self):
+	if self.socket_state == DOWN:
+	    try:
+		self.socket.connect(self.socket_target)
+	        self.socket_state = UP
+	    except Exception, e:
+		raise MKLivestatusSocketError("Cannot connect to '%s': %s" % (self.socketurl, e))
+
+    def receive_data(self, size):
+	result = ""
+	while size > 0:
 	    packet = self.socket.recv(size)
 	    size -= len(packet)
 	    result += packet
-	 return result
-      except Exception, e:
-	 self.socket_state = DOWN
-	 raise MKLivestatusSocketError(str(e))
+	return result
+
+    def query(self, query, add_headers = ""):
+        if self.socket_state != UP:
+	    self.connect()
+        if not query.endswith("\n"):
+	    query += "\n"
+        query += add_headers
+        if not query.endswith("\n"):
+	    query += "\n"
+        query += "OutputFormat: json\nKeepAlive: on\nResponseHeader: fixed16\n\n"
+
+        try:
+	    self.socket.send(query)
+	    resp = self.receive_data(16)
+	    code = resp[0:3]
+	    length = int(resp[4:15].lstrip())
+	    data = self.receive_data(length)
+	    if code == "200":
+		try:
+		    return eval(data)
+		except:
+		    raise MKLivestatusQueryError("Invalid response from Livestatus")
+	    else:
+	       raise MKLivestatusQueryError(code, data.strip())
+        except 1: # Exception, e:
+	    self.socket_state = DOWN
+	    raise MKLivestatusSocketError(str(e))
+
+class SingleSiteConnection(BaseConnection, Helpers):
+    def __init__(self, unixsocketpath):
+	BaseConnection.__init__(self, "unix:" + unixsocketpath)
+
+# sites is a dictionary from site name to a dict.
+# Keys in the dictionary:
+# socket:   socketurl (obligatory)
+# timeout:  timeout for tcp/unix in seconds
+
+class MultiSiteConnection(Helpers):
+    def __init__(self, sites):
+	self.sites = sites
+	self.connections = []
+	self.deadsites = {}
+	self.prepend_site = False
+	for sitename, site in sites.items():
+	    try:
+		url = site["socket"]
+	        connection = BaseConnection(url)
+		if "timeout" in site:
+		   connection.set_timeout(int(site["timeout"]))
+		connection.connect()
+	        self.connections.append((sitename, site, connection))
+
+	    except Exception, e:
+		self.deadsites[sitename] = {
+		    "exception" : e,
+		    "site"      : site,
+		}
+
+    def set_prepend_site(self, p):
+	self.prepend_site = p
+
+    def dead_sites(self):
+	return self.deadsites
+
+    def alive_sites(self):
+	return self.connections.keys()
+
+    def query(self, query, add_headers = ""):
+	result = []
+	stillalive = []
+	for sitename, site, connection in self.connections:
+	    try:
+		r = connection.query(query, add_headers)
+		if self.prepend_site:
+		    r = [ [sitename] + l for l in r ]
+		result += r
+		stillalive.append( (sitename, site, connection) )
+	    except Exception, e:
+		self.deadsites[sitename] = {
+		    "exception" : e,
+		    "site" : site,
+		}
+	self.alive_sites = stillalive
+	return result
 

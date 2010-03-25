0077cf4d96a72bbf7908ea2594e5fda902550f2d
Lars Michelsen <lm@mathias-kettner.de>
2010-03-25 17:37:31 +0100

    Fixed sidebar quicksearch and javascript errors in IE

--- web/plugins/sidebar/search.py
+++ web/plugins/sidebar/search.py
@@ -33,17 +33,16 @@
 import views, defaults
 
 def render_searchform():
-    html.write('<script type="text/javascript" src="%s/search.js"></script>' % defaults.checkmk_web_uri)
     html.write('<div id="mk_side_search">')
     html.write('<input id="mk_side_search_field" type="text" name="search" />')
     html.write('</div>')
+    html.write('<script type="text/javascript" src="%s/search.js"></script>' % defaults.checkmk_web_uri)
     html.write('<script type="text/javascript">')
 
     # Store (user) hosts in JS array
     data = html.live.query("GET hosts\nColumns: name alias\n")
-    html.write('var aSearchHosts = %s;' % data)
-
-    html.write('mkSearchAddField("mk_side_search_field", "main", "%s");</script>' % defaults.checkmk_web_uri)
+    html.write("aSearchHosts = %s;\n" % data)
+    html.write('</script>')
 
 sidebar_snapins["search"] = {
     "title" : "Quicksearch",

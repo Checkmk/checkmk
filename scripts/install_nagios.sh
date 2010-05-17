383d6b5e8be401637d3badfcfb54a8f18f2e504d
Mathias Kettner <mk@mathias-kettner.de>
2010-05-17 11:38:16 +0200

    install_nagios.sh: add pnp5-iconv for SLES11

--- scripts/install_nagios.sh
+++ scripts/install_nagios.sh
@@ -243,7 +243,7 @@ then
    zypper -n install apache2 mailx apache2-mod_python apache2-mod_php5 php5-gd gd-devel \
 	xinetd wget xorg-x11-libXpm-devel psmisc less graphviz-devel graphviz-gd \
 	php5-sqlite php5-gettext python-rrdtool php5-zlib php5-sockets php5-mbstring gcc \
-	cairo-devel libxml-devel libxml2-devel pango-devel gcc-c++ net-snmp
+	cairo-devel libxml-devel libxml2-devel pango-devel gcc-c++ net-snmp php5-iconv
 else
    yum update
    yum -y install httpd gcc mailx php php-gd gd-devl xinetd wget psmisc less mod_python \

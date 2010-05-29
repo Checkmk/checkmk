c57d854f975ff9e2d12e125c156dd5e2aade10f1
Mathias Kettner <mk@mathias-kettner.de>
2010-05-29 14:07:34 +0200

    New Perf-O-Meter for CPU utilization

--- web/plugins/perfometer/check_mk.py
+++ web/plugins/perfometer/check_mk.py
@@ -8,6 +8,8 @@
 # 3. The parsed performance data as a list of 7-tuples of
 #    (varname, value, unit, warn, crit, min, max)
 
+def perfometer_td(perc, color):
+    return '<td style="background-color: %s; width: %d%%;"></td>' % (color, int(float(perc)))
 
 def perfometer_check_mk_df(row, check_command, perf_data):
     h = '<table><tr>'
@@ -15,12 +17,25 @@ def perfometer_check_mk_df(row, check_command, perf_data):
     perc_used = 100 * (float(value) / float(maxx))
     perc_free = 100 - float(perc_used)
     color = { 0: "#0f8", 1: "#ff2", 2: "#f22", 3: "#fa2" }[row["service_state"]]
-    h += '<td style="background-color: %s; width: %d%%;"></td>' % (color, perc_used)
-    h += '<td style="background-color: white; width: %d%%;"></td>' % perc_free
+    h += perfometer_td(perc_used, color)
+    h += perfometer_td(perc_free, "white")
     h += "</tr></table>"
-# return "%d%% of %.1fGB used" % (perc_used, float(value)/1024.0), h
+    # return "%d%% of %.1fGB used" % (perc_used, float(value)/1024.0), h
     return "%d%%" % perc_used, h
     return "%d%% used" % perc_used, h
 
 perfometers["check_mk-df"] = perfometer_check_mk_df
 perfometers["check_mk-vms_df"] = perfometer_check_mk_df
+
+
+def perfometer_check_mk_kernel_util(row, check_command, perf_data):
+    h = '<table><tr>'
+    h += perfometer_td(perf_data[0][1], "#f60")
+    h += perfometer_td(perf_data[1][1], "#6f2")
+    h += perfometer_td(perf_data[2][1], "#0bc")
+    total = sum([float(p[1]) for p in perf_data])
+    h += perfometer_td(100.0 - total, "white")
+    h += "</tr></table>"
+    return "%d%%" % total, h
+
+perfometers["check_mk-kernel.util"] = perfometer_check_mk_kernel_util

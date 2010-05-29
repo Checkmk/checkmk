7663f95b4ed315a147a90aa32dc2075183490b62
Mathias Kettner <mk@mathias-kettner.de>
2010-05-29 18:45:34 +0200

    Multisite: new snapin "Custom Links"

--- multisite.mk
+++ multisite.mk
@@ -59,3 +59,44 @@ admin_users = [ "nagiosadmin" ]
 # sound_url = "http://otherhost/path/to/sound/"
 # or
 # sound_url = "/nagios/alarms/"
+
+
+# Custom links for "Custom Links" Snapin. Feel free to add your
+# own links here...
+
+# Links for everyone
+custom_links['guest'] = [
+  ( "Classical Nagios GUI", "/nagios/", "link_home.gif" ),
+  ( "Addons", True, [
+        ( "PNP4Nagios", "/pnp4nagios/",       "link_reporting.gif" ),
+        ( "NagVis", False, [
+            ( "Automap",    "/nagvis/index.php?map=__automap", "link_map.gif"),
+            ( "Demo map",   "/nagvis/index.php?map=demo-map",  "link_map.gif"),
+            ( "Demo Map 2", "/nagvis/index.php?map=demo2",     "link_map.gif"),
+        ]),
+  ]),
+]
+
+# The members of the role 'user' get the same links as the guests
+# but some in addition
+custom_links['user'] = custom_links['guest'] + [
+  ( "Open Source Components", False, [
+        ( "Multisite",     "http://mathias-kettner.de/checkmk_multisite.html" ),
+        ( "MK Livestatus", "http://mathias-kettner.de/checkmk_livestatus.html" ),
+        ( "Check_MK",      "http://mathias-kettner.de/check_mk.html" ),
+        ( "Nagios",        "http://www.nagios.org/" ), 
+        ( "PNP4Nagios",    "http://pnp4nagios.org/" ),
+        ( "NagVis",        "http://nagvis.org/" ),
+        ( "RRDTool",       "http://oss.oetiker.ch/rrdtool/" ),
+   ])
+]
+
+# The admins yet get further links
+custom_links['admin'] = custom_links['user'] + [
+  ( "Support", False, [
+      ( "Mathias Kettner",              "http://mathias-kettner.de/" ),
+      ( "Check_MK Mailinglists",        "http://mathias-kettner.de/check_mk_lists.html" ),
+      ( "Check_MK Portal (inofficial)", "http://check-mk-portal.org/"),
+      ( "Nagios Portal (German)",       "http://nagios-portal.org"),
+  ])
+]

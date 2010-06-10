a5e7e95bcf7a4cbc6be00fbf1ab0153798947f14
Mathias Kettner <mk@mathias-kettner.de>
2010-06-10 16:01:49 +0200

    Logfile: new filter for plugin output

--- web/plugins/views/filters.py
+++ web/plugins/views/filters.py
@@ -487,4 +487,4 @@ class FilterLogClass(Filter):
 	    return "".join(headers) + ("Or: %d\n" % len(headers))
 
 declare_filter(255, FilterLogClass())
-declare_filter(202, FilterText("output",  "Log: plugin output", "log", "log_plugin_output", "log_plugin_output", "~~"))
+declare_filter(202, FilterText("log_plugin_output",  "Log: plugin output", "log", "log_plugin_output", "log_plugin_output", "~~"))

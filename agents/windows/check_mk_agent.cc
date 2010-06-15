d55328c28f06a259d94ae54c0cfef78334c0b979
Mathias Kettner <mk@mathias-kettner.de>
2010-06-15 16:38:54 +0200

    Switched version to 1.1.6

--- agents/windows/check_mk_agent.cc
+++ agents/windows/check_mk_agent.cc
@@ -54,7 +54,7 @@
 #include <string.h>
 #include <locale.h>
 
-#define CHECK_MK_VERSION "1.1.6rc3"
+#define CHECK_MK_VERSION "1.1.6"
 #define CHECK_MK_AGENT_PORT 6556
 #define SERVICE_NAME "Check_MK_Agent"
 #define KiloByte 1024
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,7 +24,7 @@
 # Boston, MA 02110-1301 USA.
 
 
-VERSION=1.1.6rc3
+VERSION=1.1.6
 NAME=check_mk
 LANG=
 LC_ALL=

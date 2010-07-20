fadea1e359ee1ec70f6b1901abaafa59e6cc0c39
Lars Michelsen <lm@mathias-kettner.de>
2010-07-20 13:02:03 +0200

    win_dhcp_pools: Filtering more error messages from netsh output on german servers where no dhcp is configured

--- agents/windows/plugins/win_dhcp_pools.bat
+++ agents/windows/plugins/win_dhcp_pools.bat
@@ -1,3 +1,3 @@
 @echo off
 echo ^<^<^<win_dhcp_pools^>^>^>
-netsh dhcp server show mibinfo | find /V ": dhcp."
+netsh dhcp server show mibinfo | find /V ": dhcp." | find /V "DHCP-Serverversion wurde" | find /V "nicht richtig funktionieren."

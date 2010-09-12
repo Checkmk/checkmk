8a0d6e8f5cf2251f9ea3e4ccea869fdce93b6e40
Mathias Kettner <mk@mathias-kettner.de>
2010-09-12 13:25:22 +0200

    Livestatus: fix type of num_service_* from list to int

--- livestatus/src/ServicelistStateColumn.h
+++ livestatus/src/ServicelistStateColumn.h
@@ -54,7 +54,6 @@ class ServicelistStateColumn : public IntColumn
 public:
    ServicelistStateColumn(string name, string description, int logictype, int offset, int indirect_offset)
     : IntColumn(name, description, indirect_offset), _offset(offset), _logictype(logictype) {};
-   int type() { return COLTYPE_LIST; };
    int32_t getValue(void *data, Query *);
    servicesmember *getMembers(void *data);
    static int32_t getValue(int logictype, servicesmember *services, Query *);

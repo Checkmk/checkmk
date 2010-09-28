d750b61bd4486fe4503976dfcb96355c1207484c
Mathias Kettner <mk@mathias-kettner.de>
2010-09-28 21:21:48 +0200

    fix broken links in "Edit view -> Try out" situation

--- web/htdocs/views.py
+++ web/htdocs/views.py
@@ -1676,7 +1676,7 @@ def link_to_view(content, row, linkview):
         for filt in filters:
             filtervars += filt.variable_settings(row)
 
-        uri = html.makeuri_contextless([("view_name", linkview)] + filtervars)
+        uri = "view.py?" + htmllib.urlencode_vars([("view_name", linkview)] + filtervars)
         content = "<a href=\"%s\">%s</a>" % (uri, content)
     return content
 

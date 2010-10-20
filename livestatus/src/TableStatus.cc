2dc56353ce0058112978a671e2f51ed5aea26503
Mathias Kettner <mk@mathias-kettner.de>
2010-10-20 16:20:10 +0200

    Livestatus: table status has new columns for command buffer

--- livestatus/src/TableStatus.cc
+++ livestatus/src/TableStatus.cc
@@ -55,6 +55,9 @@ extern int check_external_commands;
 extern int num_cached_log_messages;
 extern int interval_length;
 
+extern circular_buffer external_command_buffer;
+extern int external_command_buffer_slots;
+
 TableStatus::TableStatus()
 {
    addColumn(new GlobalCountersColumn("neb_callbacks", 
@@ -132,6 +135,17 @@ TableStatus::TableStatus()
 
    addColumn(new StringPointerColumn("program_version", 
 	    "The version of the monitoring daemon", get_program_version()));
+
+   // External command buffer
+   addColumn(new IntPointerColumn("external_command_buffer_slots", 
+               "The size of the buffer for the external commands",
+               &external_command_buffer_slots));
+   addColumn(new IntPointerColumn("external_command_buffer_usage",
+               "The number of slots in use of the external command buffer",
+               &(external_command_buffer.items)));
+   addColumn(new IntPointerColumn("external_command_buffer_max",
+           "The maximum number of slots used in the external command buffer",
+           &(external_command_buffer.high)));
   
    // Livestatus' own status
    addColumn(new IntPointerColumn("cached_log_messages", 

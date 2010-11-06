73c3b1766c50da8c13d8f15981e467b3f596e0f5
Mathias Kettner <mk@mathias-kettner.de>
2010-11-06 10:08:04 +0100

    perfdata_format now defaults to "pnp"

--- main.mk
+++ main.mk
@@ -1,8 +1,3 @@
 # Put your host names here
 # all_hosts = [ 'localhost' ]
 all_hosts = [ ]
-
-# Are you using PNP4Nagios and MRPE checks? This will make PNP
-# choose the correct template for standard Nagios checks:
-# perfdata_format = "pnp"
-

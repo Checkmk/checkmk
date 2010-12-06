04a3dd971218faaa0c8a6a03c1a34ddfffc79a8a
Mathias Kettner <mk@mathias-kettner.de>
2010-12-06 11:52:03 +0100

    new check check_mk.only_from: check IP access configuration

--- multisite.mk
+++ multisite.mk
@@ -18,11 +18,12 @@ admin_users = [ "nagiosadmin" ]
 
 # Sites to connect to. If this variable is unset, a single
 # connection to the local host is done.
-#sites = {
-#   # connect to local Nagios
-#   "local" : {
-#        "alias" : "Munich"
-#   },
+sites = {
+   # connect to local Nagios
+   "wato" : {
+        "alias" : "Munich"
+   },
+}
 #
 #   # connect to remote site (e.g. local OMD site 'paris')
 #   "paris": {

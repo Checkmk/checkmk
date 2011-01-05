a561461879963f55f72d33cead82295589dae2a0
Mathias Kettner <mk@mathias-kettner.de>
2011-01-05 20:50:01 +0100

    BI: first successful tree compile

--- web/htdocs/bi.py
+++ web/htdocs/bi.py
@@ -12,51 +12,98 @@ except NameError:
     from sets import Set as set
 
 # ZWEITER VERSUCH, komplett von vorne
+SINGLE = 'single'
+MULTIPLE = 'multi'
 
 aggregation_rules = {}
 
 aggregation_rules["HostRessources"] = (
-    "Host Ressources", "worst", [ "HOST" ], [ 
-        ( "Kernel",      [ "$HOST$" ]       ), 
-        ( "Filesystems", [ "$HOST$" ]       ),
-        ( "NIC",         [ "$HOST$", ".*" ] ),
+    "Host Ressources", [ "aHOST" ], "worst", [ 
+        ( "Kernel",      [ "$HOST$" ] ), 
+        ( "Filesystems", [ "$HOST$" ] ),
+        ( "Networking",  [ "$HOST$" ] ),
+    ]
+)
+
+aggregation_rules["Database"] = (
+    "Database $DB", [ "aDB", "HOSTs" ], "worst", [
+        ( "proc_.*$DB$.*", "$HOST$" ),
+        ( "LOG /var/ora/$DB$.log", "$HOST$" ),
     ]
 )
 
 aggregation_rules["Kernel"] = (
-    "Kernel", [ "HOST" ], "worst", [ 
+    "Kernel of $HOST$", [ "aHOST" ], "worst", [ 
         ( "$HOST$", "Kernel" ), 
     ]
 )
 
 aggregation_rules["Filesystems"] = (
-    "Filesystems", [ "HOST" ], "worst", [
+    "Filesystems", [ "aHOST" ], "worst", [
         ( "$HOST$", "fs" ),
     ]
 )
 
+aggregation_rules["Networking"] = (
+    "Networking", [ "aHOST" ], "worst", [
+        ( "NIC", [ "$HOST$", ".*" ] )
+    ]
+)
+
 aggregation_rules["NIC"] = (
-    "Network Interface $NIC$", ["HOST", "NIC"], "worst", [ 
+    "Network Interface $NIC$", ["aHOST", "aNIC"], "worst", [ 
         ( "$HOST$", "NIC ($NIC$) " )
     ]
 )
 
 aggregation_rules["OS"] = (
-    "Operating System", [ "HOST" ], "worst", [
+    "Operating System", [ "aHOST" ], "worst", [
         ( "HostRessources", [ "$HOST$" ] ),
         ( "$HOST$", "Check_MK" ),
     ]
 )
 
+aggregation_rules["Check_MK"] = (
+    "Check_MK", [ "HOSTs" ], "worst", [
+        ( "$HOST$", "Check_MK$" ),
+        ( "$HOST$", "Check_MK Inventory$" ),
+    ]
+)
+
+aggregation_rules["GlobalService"] = (
+    "$SERVICE$", [ "HOSTs", "aSERVICE" ], "worst", [
+        ( "$HOST$", "$SERVICE$" )
+    ]
+)
+
 aggregations = [
- ( "OSses",   "OS",  [ '.*' ] ),
- ( "Network", "NIC", [ 'localhost', 'eth0' ] ),
+# ( "OSses",      "OS",            [ 'localhost' ] ),
+ ( "OSses",      "HostRessources",            [ '.*' ] ),
+# ( "Kernel",     "Kernel",        [ '.*' ] ),
+# ( "Network",    "NIC",           [ 'localhost', 'eth0' ] ),
+# ( "Monitoring", "Check_MK",      [ '.*' ] ), 
+# ( "Monitoring", "GlobalService", [ '.*', 'Check.*' ] ),
 ]
 
+# Ueberlegung: Wenn man als Hostname '.*' angibt (oder ALL_HOSTS und irgendwelche Tags),
+# dann ist die Frage, *wo* genau das Ausmultipliziren stattfindet. So könnte die Regel
+# ( "OSses",   "OS",  [ '.*' ] ) eigentlich das .* schon auf der höchsten Ebene auflösen
+# und mehrere Aggegationen erzeugen. Wenn das .* ganz nach unten geht, wird nur eine
+# einzige Aggregation erzeugt, welche die Daten von allen Hosts vereint.
+
+# Alternativ könnte eine untere Regel irgendwie erzwingen, dass sie für jedes unterschiedliche
+# Vorkommen des Musters separat erzeugt wird. Es ist ja so, dass von einer Blatt-Regel, die man
+# mit Mustern aufruft, für jede Inkarnation die Instanziierung der Argumente hochgemeldet wird.
+
+
+# Idee zur Syntax der Host-Tags:
+# '.*|linux' ==> ['linux'], ALL_HOSTS
 
 aggregation_forest = {}
 
 
+# Load the static configuration of all services and hosts (including tags)
+# without state and store it in the global variable g_services.
 def load_services():
     global g_services
     g_services = {}
@@ -67,36 +114,97 @@ def load_services():
         g_services[host] = (tags, services)
         
 
+# Precompile the forest of BI rules. Forest? A collection of trees.
+# The compiled forest does not contain any regular expressions.
 def compile_forest():
     load_services()
-
     for group, rulename, args in aggregations:
-        # Schwierigkeit hier: die args können reguläre Ausdrücke enthalten.
         rule = aggregation_rules[rulename]
+        # Compute new trees and add them to group
         entries = aggregation_forest.get(group, [])
         entries += compile_aggregation(rule, args)
         aggregation_forest[group] = entries
 
+    for group, trees in aggregation_forest.items():
+        debug("GROUP %s" % group)
+        for tree in trees:
+            instargs, node = tree
+            ascii = render_tree(node)
+            html.write("<pre>\n" + ascii + "<pre>\n")
 
+def render_tree(node, indent = ""):
+    h = ""
+    if len(node) == 3: # leaf node
+        h += indent + "H/S: %s/%s\n" % (node[1], node[2])
+    else:
+        h += indent + "Aggregation:\n"
+        indent += "    "
+        h += indent + "Description:  %s\n" % node[1]
+        h += indent + "Needed Hosts: %s\n" % " ".join(node[0])
+        h += indent + "Aggregation:  %s\n" % node[2]
+        h += indent + "Nodes:\n"
+        for node in node[3]:
+            h += render_tree(node, indent + "  ")
+        h += "\n"
+    return h
+
+
+# Compute dictionary of arguments from arglist and
+# actual values
+def make_arginfo(arglist, args):
+    arginfo = {}
+    for name, value in zip(arglist, args):
+        if name[0] == 'a':
+            expansion = SINGLE
+            name = name[1:]
+        elif name[-1] == 's':
+            expansion = MULTIPLE
+            name = name[:-1]
+        else:
+            raise MKConfigError("Invalid argument name %s. Must begin with 'a' or end with 's'." % name)
+        arginfo[name] = (expansion, value)
+    return arginfo
+
+
+# Precompile one aggregation rule. This outputs a list of trees.
+# This function is called recursively.
 def compile_aggregation(rule, args):
-    description, arglist, func, nodes = rule
-    arg = dict(zip(arglist, args))
+    description, arglist, funcname, nodes = rule
+    arginfo = make_arginfo(arglist, args)
     elements = []
     for node in nodes:
         # Each node can return more than one incarnation (due to regexes in 
         # the arguments)
-        elements += aggregate_node(arg, node)
-    return elements
+        if type(node[1]) == str: # leaf node
+            elements += aggregate_leaf_node(arginfo, node[0], node[1])
+        else:
+            rule = aggregation_rules[node[0]]
+            instargs = [ instantiate(arg, arginfo)[0] for arg in node[1] ]
+            elements += compile_aggregation(rule, instargs)
 
+    # Now compile one or more rules from elements. We group
+    # all elements into one rule together, that have the same
+    # value for all SINGLE arguments
+    groups = {}
+    single_names = [ varname for (varname, (expansion, value)) in arginfo.items() if expansion == SINGLE ]
+    for instargs, node in elements:
+        key = tuple([ instargs[varname] for varname in single_names ])
+        nodes = groups.get(key, [])
+        nodes.append(node)
+        groups[key] = nodes
 
-def aggregate_node(arg, node):
-    if type(node[1]) == str: # leaf node
-        elements = aggregate_leaf_node(arg, node[0], node[1])
-    else:
-        elements = aggregate_inter_node(arg, node[0], node[1])
-    return elements
+    result = []
+    for key, nodes in groups.items():
+        needed_hosts = set([])
+        for node in nodes:
+            needed_hosts.update(node[0])
+        inst = dict(zip(single_names, key))
+        description = subst_vars(description, inst)
+        result.append((inst, (list(needed_hosts), description, funcname, nodes)))
+    return result
 
-
+# Helper function that finds all occurrances of a variable
+# enclosed with $ and $. Returns a list of positions.
 def find_variables(pattern, varname):
     found = []
     start = 0
@@ -108,24 +216,61 @@ def find_variables(pattern, varname):
         else:
             return found
 
-def instantiate(pattern, arg):
+def subst_vars(pattern, arg):
+    for name, value in arg.items():
+        pattern = pattern.replace('$'+name+'$', value)
+    return pattern
+
+def instantiate(pattern, arginfo):
     # Wir bekommen hier z.B. rein:
     # pattern = "Some $DB$ and some $PROC$"
-    # arg = { "PROC": "[a-z].*", "DB": "X..",  }
+    # arginfo = { "PROC": (SINGLE, "[a-z].*"), "DB": (MULTIPLE, "X.."),  }
     # Die Antwort ist:
     # return "Some (X..) and some ([a-z].*)", [ 'DB', 'PROC' ]
-    # Bedenke: Eine Variable kann mehrfachersetzt werden.
+    # Bedenke: Eine Variable kann mehrfach ersetzt werden.
 
+    # Replace variables with values. Values are assumed to 
+    # contain regular expressions and are put into brackets.
+    # This allows us later to get the actual value when the regex
+    # is matched against an actual host or service name.
+    # Difficulty: when a variable appears twice, the second
+    # occurrance must be a back reference to the first one. We
+    # need to make sure, that both occurrances match the *same*
+    # string. 
+
+    # Example:
+    # "some text $A$ other $B$ and $A$"
+    # A = "a.*z", B => "hirn"
+    # result: "some text (a.*z) other (hirn) and (\1)"
     substed = []
     newpattern = pattern
-    for k, v in arg.items():
-        places = find_variables(pattern, v)
-        for p in places:
-            substed.append((p, v))
-        newpattern = newpattern.replace('$'+k+'$', '('+v+')')
-    substed.sort()
-    return newpattern, [ v for (p,v) in substed ]
 
+    first_places = []
+    # Determine order of first occurrance of each variable
+    for varname, (expansion, value) in arginfo.items():
+        places = find_variables(pattern, varname)
+        if len(places) > 0:
+            first_places.append((places[0], varname))
+    first_places.sort()
+    varorder = [ varname for (place, varname) in first_places ]
+    backrefs = {}
+    for num, var in enumerate(varorder):
+        backrefs[var] = num + 1
+    
+    # Replace variables
+    for varname, (expansion, value) in arginfo.items():
+        places = find_variables(pattern, varname)
+        value = value.replace('$', '\001') # make $ invisible
+        if len(places) > 0:
+            for p in places:
+                substed.append((p, varname))
+            newpattern = newpattern.replace('$' + varname + '$', '(' + value + ')', 1)
+            newpattern = newpattern.replace('$' + varname + '$', '(\\%d)' % backrefs[varname])
+    substed.sort()
+    newpattern = newpattern.replace('\001', '$')
+    return newpattern, [ v for (p, v) in substed ]
+
+            
 def do_match(reg, text):
     mo = regex(reg).match(text)
     if not mo:
@@ -134,51 +279,41 @@ def do_match(reg, text):
         return list(mo.groups())
 
 
-def aggregate_leaf_node(arg, host, service):
-    debug("LEAF NODE: %s" % ((arg, host, service),))
-
+def aggregate_leaf_node(arginfo, host, service):
     # replace placeholders in host and service with arg
     # service = 'NIC $NIC$ .*'
-    host_re, host_vars = instantiate(host, arg)
+    host_re, host_vars = instantiate(host, arginfo)
     # service = 'NIC $NIC$ .*'
-    service_re, service_vars = instantiate(service, arg)
+    service_re, service_vars = instantiate(service, arginfo)
     # service_re = (['NIC'], 'NIC (.*) .*')  # Liste von argumenten
 
     found = []
 
     for host, (tags, services) in g_services.items():
         # Tags vom Host prüfen (hier noch nicht in Konfiguration enthalten)
-        instargs = do_match(host_re, host)
-        if instargs:
-            newarg = arg.copy()
-            newarg.update(dict(zip(host_vars, instargs)))
+        host_instargs = do_match(host_re, host)
+        if host_instargs:
             for service in services:
-                instargs = do_match(service_re, service)
-                if instargs != None:
-                    newarg = arg.copy()
-                    newarg.update(dict(zip(service_vars, instargs)))
-                    found.append((host, service))
+                svc_instargs = do_match(service_re, service)
+                if svc_instargs != None:
+                    newarg = {} # dict([ (k,v) for (k,(e,v)) in arginfo.items()])
+                    newarg.update(dict(zip(host_vars, host_instargs)))
+                    newarg.update(dict(zip(service_vars, svc_instargs)))
+                    found.append((newarg, ([host], host, service)))
 
     # Jetzt in eine Liste umwandeln
     # return in_liste(found)
-    debug("==> %s" % pprint.pformat(found))
     return found
-
-def aggregate_inter_node(arg, rulename, args):
-    rule = aggregation_rules[rulename]
-    description, arglist, funcname, nodelist = rule
-    elements = compile_aggregation(rule, args)
-    debug("ARG: %s" % arg)
-    debug("RULENAME: %s" % rulename)
-    debug("ELEMENTS: %s" % elements)
-    return []
 
 regex_cache = {}
 def regex(r):
     rx = regex_cache.get(r)
     if rx:
         return rx
-    rx = re.compile(r)
+    try:
+        rx = re.compile(r)
+    except Exception, e:
+        raise MKConfigError("Invalid regular expression '%s': %s" % (r, e))
     regex_cache[r] = rx
     return rx
 

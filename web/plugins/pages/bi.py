376aa9838739ea4c2a3a0ab9cf6ee27ca9cc9d50
Mathias Kettner <mk@mathias-kettner.de>
2011-01-07 15:08:16 +0100

    BI: editable state assumptions

--- web/plugins/pages/bi.py
+++ web/plugins/pages/bi.py
@@ -1,6 +1,7 @@
 import bi
 
 pagehandlers.update({
-    "bi_debug" : bi.page_debug,
-    "bi" :       bi.page_all,
+    "bi_debug" :          bi.page_debug,
+    "bi" :                bi.page_all,
+    "bi_set_assumption" : bi.ajax_set_assumption,
 })

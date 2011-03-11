01d57d7293e476949d49114f655b1f444dc3b7ba
Mathias Kettner <mk@mathias-kettner.de>
2011-03-11 16:02:05 +0100

    Agent simulator use uptime(f,r) instead of sinus

--- modules/agent_simulator.py
+++ modules/agent_simulator.py
@@ -23,16 +23,15 @@ def agent_simulator_process(output):
 
     return output
 
-def agentsim_uptime(factor = 1.0):
-    return our_uptime() * factor
+def agentsim_uptime(rate = 1.0, period = None): # period = sinus wave
+    if period == None:
+        return our_uptime() * rate
+    else:
+        a = (rate * period) / (2.0 * math.pi)
+        u = our_uptime()
+        return u * rate + int(a * math.sin(u / (2.0 * math.pi * period)))
 
 def agentsim_enum(values, period = 1): # period is in seconds
     hit = our_uptime() / period % len(values)
     return values[hit]
 
-# rate: average rate counter is raised per second
-# period: period of sinus wave added to this (in seconds)
-def agentsim_sinus(rate = 1, period = 360):
-    a = (rate * period) / (2.0 * math.pi)
-    u = our_uptime()
-    return u * rate + int(a * math.sin(u / (2.0 * math.pi * period)))

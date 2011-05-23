6e6ace98839dbda3e1d95cc0399557cf3856c5ec
Mathias Kettner <mk@mathias-kettner.de>
2011-05-23 16:57:57 +0200

    Added Perf-O-Meter for Brocade FC Ports

--- web/plugins/perfometer/check_mk.py
+++ web/plugins/perfometer/check_mk.py
@@ -231,6 +231,7 @@ perfometers["check_mk-if64"] = perfometer_check_mk_if
 perfometers["check_mk-lnx_if"] = perfometer_check_mk_if
 perfometers["check_mk-hpux_if"] = perfometer_check_mk_if
 perfometers["check_mk-mcdata_fcport"] = perfometer_check_mk_if
+perfometers["check_mk-brocade_fcport"] = perfometer_check_mk_if
 
 def perfometer_oracle_tablespaces(row, check_command, perf_data):
     current = float(perf_data[0][1])

83e14bb19c32db1fed9ac857b358a069eee0eb3b
Mathias Kettner <mk@mathias-kettner.de>
2011-10-18 08:46:22 +0200

    Dashboard: use more space on the right side

--- web/htdocs/js/dashboard.js
+++ web/htdocs/js/dashboard.js
@@ -98,7 +98,7 @@ function resize_dashlets(id, code)
 
 function set_dashboard_size()
 {
-  var body_padding = 10;
+  var body_padding = 0;
   var width  = pageWidth();
   var height = pageHeight();
   width  = width - 2*screen_margin - 2*body_padding;

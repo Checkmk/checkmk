6e5650e2e19ce59eb2ea7d74a180d54110d634a0
Andreas Boesl <ab@mathias-kettner.de>
2011-11-03 20:02:15 +0100

    WATO: TextAttribute, new parameter size(default size=20)

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -3175,10 +3175,11 @@ class Attribute:
 # A simple text attribute. It is stored in 
 # a Python unicode string
 class TextAttribute(Attribute):
-    def __init__(self, name, title, help = None, default_value="", mandatory=False, allow_empty=True):
+    def __init__(self, name, title, help = None, default_value="", mandatory=False, allow_empty=True, size=20):
         Attribute.__init__(self, name, title, help, default_value)
         self._mandatory = mandatory
         self._allow_empty = allow_empty
+        self._size = size
 
     def paint(self, value, hostname):
         if not value:
@@ -3192,7 +3193,7 @@ class TextAttribute(Attribute):
     def render_input(self, value):
         if value == None: 
             value = ""
-        html.text_input("attr_" + self.name(), value)
+        html.text_input("attr_" + self.name(), value, size = self._size)
 
     def from_html_vars(self):
         value = html.var_utf8("attr_" + self.name())

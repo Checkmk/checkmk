80efbaacd84d9799067aa2e35d82929d204439e7
Mathias Kettner <mk@mathias-kettner.de>
2011-11-30 14:12:15 +0100

    Use check_mk.css also in mobile.py

--- web/htdocs/mobile.py
+++ web/htdocs/mobile.py
@@ -25,7 +25,7 @@ def mobile_html_head(title, read_code=""):
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>%s</title>
   <link rel="stylesheet" type="text/css" href="jquery/jquery.mobile-1.0.css">
-  <link rel="stylesheet" type="text/css" href="Xcheck_mk.css">
+  <link rel="stylesheet" type="text/css" href="check_mk.css">
   <link rel="stylesheet" type="text/css" href="status.css">
   <link rel="stylesheet" type="text/css" href="mobile.css">
   <script type='text/javascript' src='jquery/jquery-1.6.4.min.js'></script>

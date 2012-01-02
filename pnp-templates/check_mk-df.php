dd198d266f83dd2f98b6e168dcf733c22177948e
Mathias Kettner <mk@mathias-kettner.de>
2012-01-02 10:31:35 +0100

    Filesystem checks: normalize trend perfdata to MB/24h

--- pnp-templates/check_mk-df.php
+++ pnp-templates/check_mk-df.php
@@ -58,15 +58,18 @@ $def[1] .= "GPRINT:var1:LAST:\"current\: %6.2lf GB\" ";
 $def[1] .= "GPRINT:var1:MAX:\"max\: %6.2lf GB \" ";
 $def[1] .= "GPRINT:var1:AVERAGE:\"avg\: %6.2lf GB\" ";
 
-# Second graph is optional and shows trend
+# Second graph is optional and shows trend. The MAX field
+# of the third variable contains (size of the filesystem in MB
+# / range in hours). From that we can compute the configured range.
 if (isset($DS[2])) {
     $size_mb_per_hours = floatval($MAX[3]); // this is size_mb / range(hours)
     $size_mb = floatval($MAX[1]);
     $hours = 1.0 / ($size_mb_per_hours / $size_mb);
     $range = sprintf("%.0fh", $hours);
 
-    // Current growth / shrinking
-    $opt[2] = "--vertical-label '+/- MB / $range' -l -1 -u 1 -X0 --title '$hostname: Growth of $fstitle' ";
+    // Current growth / shrinking. This value is give as MB / 24 hours. 
+    // Note: This has changed in 1.1.13i3. Prior it was MB / trend_range!
+    $opt[2] = "--vertical-label '+/- MB / 24h' -l -1 -u 1 -X0 --title '$hostname: Growth of $fstitle' ";
     $def[2] = "DEF:growth_max=$RRDFILE[2]:$DS[2]:MAX ";
     $def[2] .= "DEF:growth_min=$RRDFILE[2]:$DS[2]:MIN ";
     $def[2] .= "DEF:trend=$RRDFILE[3]:$DS[3]:AVERAGE ";
@@ -77,25 +80,24 @@ if (isset($DS[2])) {
     $def[2] .= "HRULE:0#c0c0c0 ";
     $def[2] .= "AREA:growth_pos#3060f0:\"Grow\" "; 
     $def[2] .= "AREA:growth_neg#30f060:\"Shrink \" "; 
-    $def[2] .= "GPRINT:growth:LAST:\"Current\: %+9.2lfMB / $range\" ";
-    $def[2] .= "GPRINT:growth:MAX:\"Max\: %+9.2lfMB / $range\\n\" ";
+    $def[2] .= "GPRINT:growth:LAST:\"Current\: %+9.2lfMB / 24h\" ";
+    $def[2] .= "GPRINT:growth:MAX:\"Max\: %+9.2lfMB / 24h\\n\" ";
 
     // Trend
-    $opt[3] = "--vertical-label '+/- MB / $range' -l -1 -u 1 -X0 --title '$hostname: Trend for $fstitle' ";
+    $opt[3] = "--vertical-label '+/- MB / 24h' -l -1 -u 1 -X0 --title '$hostname: Trend for $fstitle' ";
     $def[3] = "DEF:trend=$RRDFILE[3]:$DS[3]:AVERAGE ";
     $def[3] .= "HRULE:0#c0c0c0 ";
     $def[3] .= "LINE1:trend#000000:\"Trend\:\" ";
-    $def[3] .= "GPRINT:trend:LAST:\"%+7.2lf MB/$range\" "; 
+    $def[3] .= "GPRINT:trend:LAST:\"%+7.2lf MB/24h\" "; 
     if ($WARN[3]) {
-        $warn_mb = sprintf("%.2fMB", $WARN[3]);
+        $warn_mb = sprintf("%.2fMB", $WARN[3] * $hours / 24.0);
         $def[3] .= "LINE1:$WARN[3]#ffff00:\"Warn\: $warn_mb / $range\" ";
     }
     if ($CRIT[3]) {
-        $crit_mb = sprintf("%.2fMB", $CRIT[3]);
+        $crit_mb = sprintf("%.2fMB", $CRIT[3] * $hours / 24.0);
         $def[3] .= "LINE1:$CRIT[3]#ff0000:\"Crit\: $crit_mb / $range\" ";
     }
     $def[3] .= "COMMENT:\"\\n\" ";
 }
-
 
 ?>

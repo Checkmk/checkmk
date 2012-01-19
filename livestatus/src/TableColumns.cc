ecb82568d2ecacad6ca5255ac5f2351ff2ed7287
Mathias Kettner <mk@mathias-kettner.de>
2012-01-19 09:29:58 +0100

    FIX: fix type column in 'GET columns' for dict-type columns

--- livestatus/src/TableColumns.cc
+++ livestatus/src/TableColumns.cc
@@ -62,7 +62,7 @@ void TableColumns::answerQuery(Query *query)
 
 const char *TableColumns::getValue(Column *column, int colcol)
 {
-    static const char *typenames[5] = { "int", "float", "string", "list", "time" };
+    static const char *typenames[6] = { "int", "float", "string", "list", "time", "dict" };
 
     switch (colcol)
     {

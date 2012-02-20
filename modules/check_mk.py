dd7a4cc90a5916331cc44ee182a8b36ac5fc0a66
Mathias Kettner <mk@mathias-kettner.de>
2012-02-20 14:38:39 +0100

    ps: output node a process is running on
    This is only done if the check is run on a cluster

--- modules/check_mk.py
+++ modules/check_mk.py
@@ -2032,6 +2032,13 @@ def make_inventory(checkname, hostnamelist, check_only=False, include_state=Fals
             checkname_base = checkname.split('.')[0]    # make e.g. 'lsi' from 'lsi.arrays'
             try:
                 info = get_realhost_info(hostname, ipaddress, checkname_base, inventory_max_cachefile_age)
+                # Add information about nodes if check wants this
+                if check_info[checkname]["node_info"]:
+                    if cluster_of(hostname):
+                        add_host = hostname
+                    else:
+                        add_host = None
+                    info = [ [add_host] + line for line in info ]
             except MKAgentError, e:
                 # This special handling is needed for the inventory check. It needs special
                 # handling for WATO.

4558ab2e953f9b8480855c74d457d3d6b44f04cf
Mathias Kettner <mk@mathias-kettner.de>
2012-05-14 17:28:15 +0200

    Another styling fix for the sidebar

--- web/htdocs/js/sidebar.js
+++ web/htdocs/js/sidebar.js
@@ -391,7 +391,7 @@ function setSidebarHeight() {
   if (height == 0)
     return;
 
-  oContent.style.height = (height - oHeader.clientHeight - oFooter.clientHeight + 0) + 'px';
+  oContent.style.height = (height - oHeader.clientHeight - oFooter.clientHeight + 4) + 'px';
 
   oFooter = null;
   oContent = null;

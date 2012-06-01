6a9da332ff26d6b4d8367c41e2674d48512e12b0
Lars Michelsen <lm@mathias-kettner.de>
2012-06-01 09:16:55 +0200

    WATO view editor: Made new strip_bad_chars function work with unicode strings

--- web/htdocs/forms.py
+++ web/htdocs/forms.py
@@ -98,7 +98,19 @@ def edit_dictionary(entries, value, focus=None, hover_help=True, validate=None, 
 # New functions for painting forms
 
 def strip_bad_chars(x):
-    return "".join([c for c in x if c > ' ' and c < 'z']).translate(None, "'&;<>\"")
+    s = "".join([c for c in x if c > ' ' and c < 'z'])
+
+    if type(s) == unicode:
+        return s.translate({
+            ord(u"'"): None,
+            ord(u"&"): None,
+            ord(u";"): None,
+            ord(u"<"): None,
+            ord(u">"): None,
+            ord(u"\""): None,
+        })
+    else:
+        return s.translate(None, u"'&;<>\"")
 
 def header(title, isopen = True, table_id = ""):
     global g_header_open

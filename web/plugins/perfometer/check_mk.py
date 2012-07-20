638efe913a14e63d4f3f19fa00b4df474b14d288
Mathias Kettner <mk@mathias-kettner.de>
2012-07-20 17:26:14 +0200

    vms_system.procs: new check for number of processes on OpenVMS

--- web/plugins/perfometer/check_mk.py
+++ web/plugins/perfometer/check_mk.py
@@ -537,3 +537,8 @@ def perfometer_vms_system_ios(row, check_command, perf_data):
 perfometers["check_mk-vms_system.ios"] = perfometer_vms_system_ios
 
 
+def perfometer_check_mk_vms_system_procs(row, check_command, perf_data):
+    color = { 0: "#a4f", 1: "#ff2", 2: "#f22", 3: "#fa2" }[row["service_state"]]
+    return "%d" % int(perf_data[0][1]), perfometer_logarithmic(perf_data[0][1], 100, 2, color)
+
+perfometers["check_mk-vms_system.procs"] = perfometer_check_mk_vms_system_procs

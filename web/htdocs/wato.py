00be680a1f55d7c4081e294b41adbf85dea672e4
Mathias Kettner <mk@mathias-kettner.de>
2012-10-11 16:50:48 +0200

    WATO: Always allow to configure timeperiod 7X24

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -6175,9 +6175,8 @@ class TimeperiodSelection(ElementSelection):
 
     def get_elements(self):
         timeperiods = load_timeperiods()
-        if "7X24" not in timeperiods:
-            timeperiods["7X24"] = _("Always")
-        elements = dict([ (name, "%s - %s" % (name, tp["alias"])) for (name, tp) in timeperiods.items() ])
+        elements = dict([ ("7X24", _("Always")) ] + \
+           [ (name, "%s - %s" % (name, tp["alias"])) for (name, tp) in timeperiods.items() ])
         return elements
 
 # Check if a timeperiod is currently in use and cannot be deleted

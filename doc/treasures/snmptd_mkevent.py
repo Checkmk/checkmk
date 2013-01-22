a2789266d5d51f7f6312fa3b7c0b4fb755afcead
Bastian Kuhn <bk@mathias-kettner.de>
2013-01-22 09:43:27 +0100

    Small bugfix in mail_mkevent and snmptd_mkevent

--- doc/treasures/snmptd_mkevent.py
+++ doc/treasures/snmptd_mkevent.py
@@ -5,6 +5,11 @@
 #all traps to the mkeventd
 #
 # Bastian Kuhn, bk@mathias-kettner.de
+# Use this script only for testing.
+# It can lead to a poor performance: for 
+# every received trap the python interpreter is 
+# started and the script is called
+
 import time
 import sys
 
@@ -18,5 +23,5 @@ msg = " ".join(data[2:])
 host, ip = data[:2] 
 out = open(deamon_path, "w")
 timestamp = time.strftime("%b %d %H:%M:%S", time.localtime(time.time()))
-out.write("<5>%s %s trap: %s" % (timestamp, host, msg))
+out.write("<5>%s %s trap: %s\n" % (timestamp, host, msg))
 out.close()

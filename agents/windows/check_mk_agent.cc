0fad3efc87f4ab57bf6e4ecb2e5c808ce55bdf9b
Andreas Boesl <ab@mathias-kettner.de>
2013-04-03 18:14:50 +0200

    new release 1.2.2b7

--- agents/windows/check_mk_agent.cc
+++ agents/windows/check_mk_agent.cc
@@ -74,7 +74,7 @@
 //  | Declarations of macrosk, structs and function prototypes             |
 //  '----------------------------------------------------------------------'
 
-#define CHECK_MK_VERSION "1.2.2b6"
+#define CHECK_MK_VERSION "1.2.2b7"
 #define CHECK_MK_AGENT_PORT 6556
 #define SERVICE_NAME "Check_MK_Agent"
 #define KiloByte 1024
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,7 +24,7 @@
 # Boston, MA 02110-1301 USA.
 
 
-VERSION=1.2.2b6
+VERSION=1.2.2b7
 NAME=check_mk
 LANG=
 LC_ALL=

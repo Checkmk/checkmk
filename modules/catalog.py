cfa110dcef85c3ab803c8a03b379105e752a96d3
Mathias Kettner <mk@mathias-kettner.de>
2013-06-25 16:06:04 +0200

    Added manpage catalog

---
+++ modules/catalog.py
@@ -0,0 +1,7 @@
+manpage_catalog_titles = {
+    "hw"       : "Appliances, other dedicated Hardware",
+    "os"       : "Operating Systems",
+    "app"      : "Applications",
+    "generic"  : "Generic check plugins",
+    "unsorted" : "Uncategorized",
+}

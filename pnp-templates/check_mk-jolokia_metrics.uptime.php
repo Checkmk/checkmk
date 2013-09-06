d1b768fcee679d26c557b9cb5460a209ec0469d8
Lars Michelsen <lm@mathias-kettner.de>
2013-09-06 15:41:07 +0200

    jolokia_metrics.uptime: Added pnp template

---
+++ pnp-templates/check_mk-jolokia_metrics.uptime.php
@@ -0,0 +1,36 @@
+<?php
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2013             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+$title = str_replace("_", " ", $servicedesc);
+$opt[1] = "--vertical-label 'Uptime (days)' -l0 --title \"$hostname / $title: Time since last reboot\" ";
+
+$def[1] = "DEF:sec=$RRDFILE[1]:$DS[1]:MAX ";
+$def[1] .= "CDEF:uptime=sec,86400,/ ";
+$def[1] .= "AREA:uptime#80f000:\"Uptime (days)\"\\n ";
+$def[1] .= "LINE:uptime#408000 ";
+$def[1] .= "GPRINT:uptime:LAST:\"%7.2lf %s LAST\"\\t ";
+$def[1] .= "GPRINT:uptime:MAX:\"%7.2lf %s MAX\" ";
+$def[1] .= "GPRINT:uptime:AVERAGE:\"%7.2lf %s AVG\" ";
+?>

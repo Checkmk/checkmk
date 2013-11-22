60f0e0a2cb0c6bae0579d43377f7ac1f135e44ad
Lars Michelsen <lm@mathias-kettner.de>
2013-11-22 14:01:49 +0100

    Updated help text of use_inline_snmp option in WATO

--- web/plugins/wato/check_mk_configuration.py
+++ web/plugins/wato/check_mk_configuration.py
@@ -1001,15 +1001,15 @@ register_configvar(group,
 register_configvar(group,
     "use_inline_snmp",
     Checkbox(
-        title = _("Use Inline SNMP (EXPERIMENTAL)"),
+        title = _("Use Inline SNMP"),
         label = _("Enable inline SNMP (directly use net-snmp libraries)"),
-        help = _("In older versions Check_MK used command line calls of net-snmp "
-                 "tools like snmpget or snmpwalk. For each request a new command line "
-                 "program had to be executed. The new default is the inline SNMP implementation "
+        help = _("By default Check_MK uses command line calls of Net-SNMP tools like snmpget or "
+                 "snmpwalk to gather SNMP information. For each request a new command line "
+                 "program is being executed. It is now possible to use the inline SNMP implementation "
                  "which calls the net-snmp libraries directly via its python bindings. This "
-                 "should increase the performance of SNMP checks in a significant way. "
-                 "For compatibility the classic SNMP integration is still available and can be "
-                 "enabled here."),
+                 "should increase the performance of SNMP checks in a significant way. The inline "
+                 "SNMP mode is a feature which improves the performance for large installations and "
+                 "only available via our subscription."),
         default_value = False
     ),
     need_restart = True

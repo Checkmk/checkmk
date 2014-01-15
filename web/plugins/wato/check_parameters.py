5267542f902ded52f8b5dd48be1629442007537e
Götz Golla <gg@mathias-kettner.de>
2014-01-15 09:53:06 +0100

    FIX apc_symmetra: transformation from old tuple to new dict format fixed and improved

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -3585,88 +3585,94 @@ register_check_parameters(
     "first"
 )
 
+def apc_convert_from_tuple(params):
+    if type(params) in (list, tuple):
+        params = { "levels": params}
+    return params
+
 register_check_parameters(
    subgroup_environment,
     "apc_symentra",
     _("APC Symmetra Checks"),
-    Dictionary(
-        elements = [
-            ("levels",
-            Tuple(
-                title = _("Levels of battery parameters during normal operation"),
-                elements = [
-                    Integer(
-                        title = _("Critical Battery Capacity"),
-                        help = _("The battery capacity in percent at and below which a critical state is triggered"),
-                        unit = "%",
-                        default_value = 95,
+    Transform(
+        Dictionary(
+            elements = [
+                ("levels",
+                Tuple(
+                    title = _("Levels of battery parameters during normal operation"),
+                    elements = [
+                        Integer(
+                            title = _("Critical Battery Capacity"),
+                            help = _("The battery capacity in percent at and below which a critical state is triggered"),
+                            unit = "%", default_value = 95,
+                        ),
+                        Integer(
+                            title = _("Critical Battery Temperature"),
+                            help = _("The critical temperature of the battery"),
+                            unit = _("C"),
+                            default_value = 40,
+                        ),
+                        Integer(
+                            title = _("Critical Battery Current"),
+                            help = _("The critical battery current in Ampere"),
+                            unit = _("A"),
+                            default_value = 1,
+                        ),
+                        Integer(
+                            title = _("Critical Battery Voltage"),
+                            help = _("The output voltage at and below which a critical state "
+                                     "is triggered."),
+                            unit = _("V"),
+                            default_value = 220,
+                        ),
+                    ]
+                )),
+                ("output_load",
+                Tuple(
+                  title = _("Current Output Load"),
+                  help = _("Here you can set levels on the current percentual output load of the UPS. "
+                           "This load affects the running time of all components being supplied "
+                           "with battery power."),
+                  elements = [
+                     Percentage(
+                         title = _("Warning level"),
+                     ),
+                     Percentage(
+                         title = _("Critical level"),
+                     ),
+                  ]
+                )),
+                ("post_calibration_levels",
+                Dictionary(
+                    title = _("Levels of battery parameters after calibration"),
+                    help = _("After a battery calibration the battery capacity is reduced until the "
+                             "battery is fully charged again. Here you can specify an alternative "
+                             "lower level in this post-calibration phase. "
+                             "Since apc devices remember the time of the last calibration only "
+                             "as a date, the alternative lower level will be applied on the whole "
+                             "day of the calibration until midnight. You can extend this time period "
+                             "with an additional time span to make sure calibrations occuring just "
+                             "before midnight do not trigger false alarms."
                     ),
-                    Integer(
-                        title = _("Critical Battery Temperature"),
-                        help = _("The critical temperature of the battery"),
-                        unit = _("C"),
-                        default_value = 40,
-                    ),
-                    Integer(
-                        title = _("Critical Battery Current"),
-                        help = _("The critical battery current in Ampere"),
-                        unit = _("A"),
-                        default_value = 1,
-                    ),
-                    Integer(
-                        title = _("Critical Battery Voltage"),
-                        help = _("The output voltage at and below which a critical state is triggered."),
-                        unit = _("V"),
-                        default_value = 220,
-                    ),
-                ]
-            )),
-            ("output_load",
-            Tuple(
-              title = _("Current Output Load"),
-              help = _("Here you can set levels on the current percentual output load of the UPS. "
-                       "This load affects the running time of all components being supplied "
-                       "with battery power."),
-              elements = [
-                 Percentage(
-                     title = _("Warning level"),
-                 ),
-                 Percentage(
-                     title = _("Critical level"),
-                 ),
-              ]
-
-            )
-            ),
-            ("post_calibration_levels",
-            Dictionary(
-                title = _("Levels of battery parameters after calibration"),
-                help = _("After a battery calibration the battery capacity is reduced until the "
-                         "battery is fully charged again. Here you can specify an alternative lower "
-                         "level in this post-calibration phase. "
-                         "Since apc devices remember the time of the last calibration only "
-                         "as a date, the alternative lower level will be applied on the whole "
-                         "day of the calibration until midnight. You can extend this time period "
-                         "with an additional time span to make sure calibrations occuring just "
-                         "before midnight do not trigger false alarms."
-                ),
-                elements = [
-                    ("altcapacity",
-                    Percentage(
-                        title = _("Alternative critical battery capacity after calibration"),
-                        default_value = 50,
-                    )),
-                    ("additional_time_span",
-                    Integer(
-                        title = ("Extend post-calibration phase by additional time span"),
-                        unit = _("minutes"),
-                        default_value = 0,
-                    )),
-                ],
-                optional_keys = False,
-            )),
-        ],
-        optional_keys = ['post_calibration_levels', 'output_load'],
+                    elements = [
+                        ("altcapacity",
+                        Percentage(
+                            title = _("Alternative critical battery capacity after calibration"),
+                            default_value = 50,
+                        )),
+                        ("additional_time_span",
+                        Integer(
+                            title = ("Extend post-calibration phase by additional time span"),
+                            unit = _("minutes"),
+                            default_value = 0,
+                        )),
+                    ],
+                    optional_keys = False,
+                )),
+            ],
+            optional_keys = ['post_calibration_levels', 'output_load'],
+        ),
+        forth = apc_convert_from_tuple
     ),
     None,
     "first"

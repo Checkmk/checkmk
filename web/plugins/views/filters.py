eee91d2ba5718aee869bef24cfaee51563b11a0e
Andreas Boesl <ab@mathias-kettner.de>
2014-01-27 19:26:20 +0100

    FIX Multisite host tag filter: Now uses exact match

--- web/plugins/views/filters.py
+++ web/plugins/views/filters.py
@@ -817,7 +817,7 @@ class FilterHostTags(Filter):
 
             if op and val:
                 operator = op != 'is' and '!~' or '~'
-                headers.append('Filter: host_custom_variables %s TAGS %s' % (operator, val))
+                headers.append('Filter: host_custom_variables %s TAGS (^|[ ])%s($|[ ])' % (operator, val))
 
         if headers:
             return '\n'.join(headers) + '\n'

d6267b709e644f8c6c5218a22e4d995acccf7f41
Bernd Stroessenreuther <bs@mathias-kettner.de>
2014-07-22 14:38:12 +0200

    bintec_sensors.voltage, bintec_sensors.temp: New Checks for Voltage and Temperature Sensors of Bintec Routers

--- pnp-templates/check_mk-bintec_sensors.voltage.php
+++ pnp-templates/check_mk-bintec_sensors.voltage.php
@@ -23,14 +23,14 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-$opt[1] = "--vertical-label \"Celsius\"  -l 0 -u 70 --title \"$hostname / $servicedesc\" ";
+$opt[1] = "--vertical-label \"Volt\"  -l 0 --title \"$hostname / $servicedesc\" ";
 
 $def[1] = "DEF:var1=$RRDFILE[1]:$DS[1]:MAX ";
-$def[1] .= "AREA:var1#2080ff:\"Temperature\:\" ";
-$def[1] .= "GPRINT:var1:LAST:\"%2.0lf °C\" ";
+$def[1] .= "AREA:var1#808000:\"Voltage\:\" ";
+$def[1] .= "GPRINT:var1:LAST:\"%2.3lf V\" ";
 $def[1] .= "LINE1:var1#000080:\"\" ";
-$def[1] .= "GPRINT:var1:MAX:\"(Max\: %2.0lf °C,\" ";
-$def[1] .= "GPRINT:var1:AVERAGE:\"Avg\: %2.0lf °C)\\n\" ";
+$def[1] .= "GPRINT:var1:MAX:\"(Max\: %2.3lf V,\" ";
+$def[1] .= "GPRINT:var1:AVERAGE:\"Avg\: %2.3lf V)\\n\" ";
 if ($WARN[1] != "") {
     $def[1] .= "HRULE:$WARN[1]#FFFF00:\"Warning\: $WARN[1] °C\" ";
     $def[1] .= "HRULE:$CRIT[1]#FF0000:\"Critical\: $CRIT[1] °C\" ";

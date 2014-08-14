eb1afcc4d851f47cbdee269f8cc274ef5bc705f1
Mathias Kettner <mk@mathias-kettner.de>
2014-08-14 16:49:52 +0200

    Added missing PNP templates

---
+++ pnp-templates/check_mk-printer_pages.php
@@ -0,0 +1,32 @@
+<?php
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2013             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# ails.  You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+$opt[1] = "--vertical-label 'Pages printed' -X0 -l0 --title \"$servicedesc\" "; 
+$def[1] = "DEF:pages=$RRDFILE[1]:$DS[1]:MAX ";
+$def[1] .= "AREA:pages#808080:\"Pages printed\" ";
+$def[1] .= "LINE:pages#404040 ";
+$def[1] .= "GPRINT:pages:MIN:\"%7.3lf %s first\" ";
+$def[1] .= "GPRINT:pages:LAST:\"%7.3lf %s last\" ";
+?>

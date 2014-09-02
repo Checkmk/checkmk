22388245b5621e139d9297f44ecb57e363b41676
Mathias Kettner <mk@mathias-kettner.de>
2014-09-02 17:27:47 +0200

    Fixed title of humidity item in WATO rule

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -2969,9 +2969,9 @@ register_check_parameters(
               Integer(title = _("Critical if above"), unit="%" ),
               ]),
     TextAscii(
-        title = _("Service descriptions"),
+        title = _("Sensor names"),
         allow_empty = False),
-     None
+    None
 )
 
 register_check_parameters(
@@ -2989,6 +2989,7 @@ register_check_parameters(
      None,
      None
 )
+
 register_check_parameters(
     subgroup_applications,
     "oracle_tablespaces",

c9118d2553ccf19dec7a1fc65fc70d330b817215
Lars Michelsen <lm@mathias-kettner.de>
2015-01-07 08:44:31 +0100

    #1802 FIX Links in messages like "successfully sent X commands" are now working again
    The werk #1800 introduced a problem where different HTML tags which are used in
    message texts were not interpreted as intended. This change makes the links
    working again and also allows <sup> and <p> tags in messages again.

--- web/htdocs/htmllib.py
+++ web/htdocs/htmllib.py
@@ -987,7 +987,9 @@ class html:
         # <b>, <tt>, <i> to be part of the exception message. The tags
         # are escaped first and then fixed again after attrencode.
         msg = self.attrencode(obj)
-        msg = re.sub(r'&lt;(/?)(b|tt|i|br|pre)&gt;', r'<\1\2>', msg)
+        msg = re.sub(r'&lt;(/?)(b|tt|i|br|pre|a|sup|p)&gt;', r'<\1\2>', msg)
+        # Also repair link definitions
+        msg = re.sub(r'&lt;a href=&quot;(.*)&quot;&gt;', r'<a href="\1">', msg)
 
         if self.output_format == "html":
             if self.mobile:

939152f4368ce775f04b8fec955d56b06befe322
Goetz Golla <gg@mathias-kettner.de>
2015-01-10 00:09:49 +0100

    #1848 FIX df: title of pnp graphs for filesystem checks fixed
    The title of pnp graphs for filesystem checks (df...) where broken since the new service description
    of these checks ("Filesystem ...") was introduced. This has been fixed now.

--- pnp-templates/check_mk-df.php
+++ pnp-templates/check_mk-df.php
@@ -41,7 +41,7 @@ foreach ($NAME as $i => $n) {
 # RRDtool Options
 #$servicedes=$NAGIOS_SERVICEDESC
 
-$fsname = str_replace("_", "/", substr($servicedesc, 3));
+$fsname = str_replace("_", "/", substr($servicedesc,11));
 $fstitle = $fsname;
 
 # Hack for windows: replace C// with C:\

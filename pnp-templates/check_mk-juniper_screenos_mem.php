5fd0ed5a72b2dc0b03ca3b9b31fec290083077d4
Goetz Golla <gg@mathias-kettner.de>
2015-01-09 14:30:35 +0100

    #1463 FIX juniper_screenos_mem, juniper_trpz_mem: pnp template fixed

--- pnp-templates/check_mk-juniper_screenos_mem.php
+++ pnp-templates/check_mk-juniper_screenos_mem.php
@@ -23,13 +23,29 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-$opt[1] = "--vertical-label 'MB' -l 0 --title \"Main Memory (RAM)\" ";
+if ($MAX[1]/1024/1024 > 10000) {
+    $scale = 1024 * 1024 * 1024;
+    $unit = 'GB';
+}
+else {
+    $scale = 1024 * 1024;
+    $unit = 'MB';
+}
+
+$total = sprintf("%.2f", $MAX[1] / $scale);
+$warn = sprintf("%.2f", $WARN[1] / $scale);
+$crit = sprintf("%.2f", $CRIT[1] / $scale);
+
+$opt[1] = "--vertical-label $unit -l 0 --title \"Main Memory (RAM)\" -X0 --upper-limit " . $total;
 #
-$def[1]  =  "DEF:used=$RRDFILE[1]:$DS[1]:AVERAGE " ;
+
+$def[1]  = "DEF:used_bytes=$RRDFILE[1]:$DS[1]:AVERAGE " ;
+$def[1] .= "CDEF:used=used_bytes,$scale,/ ";
 $def[1] .= "AREA:used#60f020:\"Used\" " ;
-$def[1] .= "GPRINT:used:MIN:\"Min\: %2.0lf MB\" " ;
-$def[1] .= "GPRINT:used:MAX:\"Max\: %2.0lf MB\" " ;
-$def[1] .= "GPRINT:used:LAST:\"Last\: %2.0lf MB\\n\" " ;
-$def[1] .= "HRULE:$WARN[1]#FFFF00:\"Warn\" ";
-$def[1] .= "HRULE:$CRIT[1]#FF0000:\"Crit\\n\" ";
+$def[1] .= "GPRINT:used:MIN:\"Min\: %2.1lf $unit\" " ;
+$def[1] .= "GPRINT:used:MAX:\"Max\: %2.1lf $unit\" " ;
+$def[1] .= "GPRINT:used:LAST:\"Last\: %2.1lf $unit\\n\" " ;
+$def[1] .= "HRULE:$warn#FFFF00:\"Warn\" ";
+$def[1] .= "HRULE:$crit#FF0000:\"Crit\" ";
+$def[1] .= "HRULE:$total#000000:\"RAM installed $total $unit\\n\" ";
 ?>

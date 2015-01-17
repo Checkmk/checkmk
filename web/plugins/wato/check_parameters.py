36654c5552b1e2d69b7ed7183f91b88e892a33d8
Goetz Golla <gg@mathias-kettner.de>
2015-01-17 09:57:17 +0100

    #1849 netscaler_dnsrates: new check for DNS statistics of Citrix Netscaler Loadbalancers

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -7503,3 +7503,32 @@ register_check_parameters(
     ),
     None, None
 )
+
+register_check_parameters(
+    subgroup_applications,
+    "netscaler_dnsrates",
+    _("Citrix Netscaler DNS counter rates"),
+    Dictionary(
+        help = _("Counter rates of DNS parameters for Citrix Netscaler Loadbalancer "
+                 "Appliances"),
+        elements =  [
+            ("query",
+            Tuple(
+                title = _("Upper Levels for Total Number of DNS queries"),
+                elements = [
+                    Float(title = _("Warning at"), default_value=1500.0, unit="/sec"),
+                    Float(title = _("Critical at"), default_value=2000.0, unit="/sec")],
+                ),
+            ),
+            ("answer",
+            Tuple(
+                title = _("Upper Levels for Total Number of DNS replies"),
+                elements = [
+                    Float(title = _("Warning at"), default_value=1500.0, unit="/sec"),
+                    Float(title = _("Critical at"), default_value=2000.0, unit="/sec")],
+                ),
+            ),
+        ]
+    ),
+    None, None
+)

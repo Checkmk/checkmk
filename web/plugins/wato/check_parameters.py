b7e7a65b97e70498eca5caa1ef01c92bf5be6824
Goetz Golla <gg@mathias-kettner.de>
2015-01-21 12:47:46 +0100

    #1854 netscaler_tcp_conns: new check to monitor tcp connections on Citrix Netscaler Loadbalancer Appliances

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -7537,5 +7537,47 @@ register_check_parameters(
             ),
         ]
     ),
-    None, None
+    None,
+    "dict"
+)
+
+register_check_parameters(
+    subgroup_applications,
+    "netscaler_tcp_conns",
+    _("Citrix Netscaler Loadbalancer TCP Connections"),
+    Dictionary(
+        elements = [
+            ( "client_conns",
+                Tuple(
+                    title = _("Max. number of client connections"),
+                    elements = [
+                        Integer(
+                            title = _("Warning at"),
+                            default_value = 25000,
+                        ),
+                        Integer(
+                            title = _("Critical at"),
+                            default_value = 30000,
+                        ),
+                    ]
+                ),
+            ),
+            ( "server_conns",
+                Tuple(
+                    title = _("Max. number of server connections"),
+                    elements = [
+                        Integer(
+                            title = _("Warning at"),
+                            default_value = 25000,
+                        ),
+                        Integer(
+                            title = _("Critical at"),
+                            default_value = 30000,
+                        ),
+                    ]
+                ),
+            ),
+        ]),
+    None,
+    "dict"
 )

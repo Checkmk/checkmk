b7329dbd6c6010b7cf3085d878e2d9e2629922da
Goetz Golla <gg@mathias-kettner.de>
2015-02-23 09:20:38 +0100

    Publish the plugin for the Exchange Mailbox Server checks

---
+++ agents/windows/plugins/msexch_dag.ps1
@@ -0,0 +1,11 @@
+# Load Exchange Management Powershell Plugin
+try{ (Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010 -ErrorAction:Stop) }
+
+# exit without any output if this fails
+catch{exit}
+
+write-host "<<<msexch_dag:sep(58)>>>"
+Get-MailboxDatabaseCopyStatus | Format-List
+
+write-host "<<<msexch_replhealth:sep(58)>>>"
+Test-ReplicationHealth | Format-List

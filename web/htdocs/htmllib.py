f2ab3d4b5600c1bf87fd3dc81a6a0ac57fb0a280
Lars Michelsen <lm@mathias-kettner.de>
2015-03-02 08:37:36 +0100

    Also allowing xhtml <br> tags in exception texts

--- web/htdocs/htmllib.py
+++ web/htdocs/htmllib.py
@@ -1002,7 +1002,7 @@ class html:
         # <b>, <tt>, <i> to be part of the exception message. The tags
         # are escaped first and then fixed again after attrencode.
         msg = self.attrencode(obj)
-        msg = re.sub(r'&lt;(/?)(b|tt|i|br|pre|a|sup|p|li|ul|ol)&gt;', r'<\1\2>', msg)
+        msg = re.sub(r'&lt;(/?)(b|tt|i|br(?: /)?|pre|a|sup|p|li|ul|ol)&gt;', r'<\1\2>', msg)
         # Also repair link definitions
         msg = re.sub(r'&lt;a href=&quot;(.*)&quot;&gt;', r'<a href="\1">', msg)
 

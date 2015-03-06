ddddc3478f32bf89fd10dde400c8070b8e08bc04
Lars Michelsen <lm@mathias-kettner.de>
2015-03-06 12:49:51 +0100

    #2092 FIX Dashboards: Possible to configure an empty custom title

--- web/htdocs/dashboard.py
+++ web/htdocs/dashboard.py
@@ -568,7 +568,7 @@ def render_dashlet(name, board, nr, dashlet, wato_folder, add_url_vars):
     html.write('<div class="%s" id="dashlet_%d">' % (' '.join(classes), nr))
 
     title = dashlet.get('title', dashlet_type.get('title'))
-    if title and dashlet.get('show_title'):
+    if title != None and dashlet.get('show_title'):
         url = dashlet.get("title_url", None)
         if url:
             title = '<a href="%s">%s</a>' % (url, _u(title))

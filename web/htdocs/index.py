5f5f534868c7c12061ac206b8decce88f3110292
Lars Michelsen <lm@mathias-kettner.de>
2015-03-20 14:01:11 +0100

    Fixed wrong named function call

--- web/htdocs/index.py
+++ web/htdocs/index.py
@@ -275,7 +275,7 @@ def handler(req, fields = None, profiling = True):
             response_code = apache.HTTP_BAD_GATEWAY
 
         if ty in [MKConfigError, MKGeneralException]:
-            log(LOG_ERR, _("%s: %s") % (plain_title, e))
+            logger(LOG_ERR, _("%s: %s") % (plain_title, e))
 
     except (apache.SERVER_RETURN,
             (apache.SERVER_RETURN, apache.HTTP_UNAUTHORIZED),

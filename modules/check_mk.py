54d172cbcbe526ae5ae2f4f9c74ecec4506b6915
Lars Michelsen <lm@mathias-kettner.de>
2015-04-15 16:57:21 +0200

    Fixed exception in shortly added icons and actions code when ps checks are not configured with dict params

--- modules/check_mk.py
+++ modules/check_mk.py
@@ -1530,7 +1530,7 @@ def icons_and_actions_of(what, hostname, svcdesc = None, checkname = None, param
 
         # Some WATO rules might register icons on their own
         checkgroup = checkgroup_of.get(checkname, checkname)
-        if checkgroup in [ 'ps', 'services' ]:
+        if checkgroup in [ 'ps', 'services' ] and type(params) == dict:
             icon = params.get('icon')
             if icon:
                 actions.add(icon)

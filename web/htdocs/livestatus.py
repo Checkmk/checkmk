165a09fb5f9f769a0b5343c7cf51907b49300e88
Lars Michelsen <lm@mathias-kettner.de>
2015-04-21 10:49:26 +0200

    Improved handling of graph hover when distributed site is not reachable

--- web/htdocs/livestatus.py
+++ web/htdocs/livestatus.py
@@ -109,7 +109,11 @@ class Helpers:
     def query_row(self, query):
         """Issues a query that returns one line of data and returns the elements
            of that line as list"""
-        return self.query(query, "ColumnHeaders: off\n")[0]
+        result = self.query(query, "ColumnHeaders: off\n")
+        try:
+            return result[0]
+        except IndexError:
+            raise MKLivestatusNotFoundError(query)
 
     def query_row_assoc(self, query):
         """Issues a query that returns one line of data and returns the elements

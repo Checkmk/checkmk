d0c8671f18efbad5fde7035be9589d452cd18a25
Lars Michelsen <lm@mathias-kettner.de>
2015-05-11 09:12:23 +0200

    #2253 FIX Availability context button is now visible again for host- and servicegroups

--- web/htdocs/views.py
+++ web/htdocs/views.py
@@ -1164,10 +1164,8 @@ def render_view(view, rows, datasource, group_painters, painters,
                        row_count > 0 and command_form,
                        # Take into account: layout capabilities
                        can_display_checkboxes and not view.get("force_checkboxes"), show_checkboxes,
-                       # Show link to availability. This exists only for plain hosts
-                       # and services table. The grouping tables have columns that statehist
-                       # is missing. That way some of the filters might fail.
-                       datasource["table"] in [ "hosts", "services", ] or "aggr" in datasource["infos"])
+                       # Show link to availability
+                       "host" in datasource["infos"] or "service" in datasource["infos"] or "aggr" in datasource["infos"])
 
     # User errors in filters
     html.show_user_errors()

b5aad1502eb0acb5487429b5377f4483c85f42f9
Andreas Boesl <ab@mathias-kettner.de>
2015-05-19 13:40:23 +0200

    #2274 'check_mk_agent.exe test' now also outputs stderr of plugins

--- agents/windows/check_mk_agent.cc
+++ agents/windows/check_mk_agent.cc
@@ -285,6 +285,7 @@ script_async_execution g_default_script_async_execution = SEQUENTIAL;
 bool verbose_mode               = false;
 bool g_crash_debug              = false;
 bool do_tcp                     = false;
+bool with_stderr                = false;
 bool force_tcp_output           = false; // if true, send socket data immediately
 bool do_file                    = false;
 static FILE* fileout;
@@ -2729,7 +2730,9 @@ int launch_program(script_container* cont)
     SECURITY_ATTRIBUTES sa;
     SECURITY_DESCRIPTOR sd;   // security information for pipes
     PROCESS_INFORMATION pi;
-    HANDLE newstdout,read_stdout;  // pipe handles
+    HANDLE script_stdout,read_stdout;  // pipe handles
+    HANDLE script_stderr,read_stderr;
+
 
     // initialize security descriptor (Windows NT)
     if (IsWinNT())
@@ -2743,32 +2746,35 @@ int launch_program(script_container* cont)
     sa.nLength = sizeof(SECURITY_ATTRIBUTES);
     sa.bInheritHandle = true;                       // allow inheritable handles
 
-    if (!CreatePipe(&read_stdout,&newstdout,&sa,0)) // create stdout pipe
-    {
+    if (!CreatePipe(&read_stdout,&script_stdout,&sa,0)) // create stdout pipe
         return 1;
-    }
+
+    if (!CreatePipe(&read_stderr,&script_stderr,&sa,0)) // create stderr pipe
+         return 1;
+
 
     //set startupinfo for the spawned process
     GetStartupInfo(&si);
     si.dwFlags = STARTF_USESTDHANDLES|STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
-    si.hStdOutput = newstdout;
+    si.hStdOutput = script_stdout;
 
+    if (with_stderr)
+        si.hStdError = script_stdout;
+    else
+        si.hStdError = script_stderr;
 
-    // Redirect sterr to NUL device
-    SECURITY_ATTRIBUTES secattr;
-    secattr.nLength = sizeof secattr;
-    secattr.lpSecurityDescriptor = NULL;
-    secattr.bInheritHandle = TRUE;
-    HANDLE hnul = CreateFile("NUL", GENERIC_WRITE, 0, &secattr, OPEN_EXISTING, 0, NULL);
-    si.hStdError = hnul;
 
     // spawn the child process
     if (!CreateProcess(NULL,cont->path,NULL,NULL,TRUE,CREATE_NEW_CONSOLE,
                 NULL,NULL,&si,&pi))
     {
-        CloseHandle(newstdout);
+        CloseHandle(script_stdout);
         CloseHandle(read_stdout);
+
+        CloseHandle(script_stderr);
+        CloseHandle(read_stderr);
+
         return 1;
     }
 
@@ -2797,6 +2803,12 @@ int launch_program(script_container* cont)
         }
         GetExitCodeProcess(pi.hProcess, &exit);      // while the process is running
         while (!buffer_full) {
+            if (!with_stderr) {
+                PeekNamedPipe(read_stderr, buf, sizeof(buf), &bread, &avail, NULL);
+                if (avail > 0)
+                    // Just read from the pipe, we do not use this data
+                    ReadFile(read_stderr, buf, sizeof(buf) - 1, &bread, NULL);
+            }
             PeekNamedPipe(read_stdout, buf, sizeof(buf), &bread, &avail, NULL);
             if (avail == 0)
                 break;
@@ -2840,9 +2852,11 @@ int launch_program(script_container* cont)
     CloseHandle(cont->job_object);
     CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
-    CloseHandle(newstdout);
+    CloseHandle(script_stdout);
     CloseHandle(read_stdout);
-    CloseHandle(hnul);
+    CloseHandle(script_stderr);
+    CloseHandle(read_stderr);
+
     return exit_code;
 }
 
@@ -4442,9 +4456,10 @@ void do_debug()
 }
 
 
-void do_test()
+void do_test(bool output_stderr)
 {
-    do_tcp = false;
+    do_tcp  = false;
+    with_stderr = output_stderr;
     SOCKET dummy;
     open_crash_log();
     crash_log("Started in test mode.");
@@ -4922,7 +4937,7 @@ int main(int argc, char **argv)
     else if (argc <= 1)
         RunService();
     else if (!strcmp(argv[1], "test"))
-        do_test();
+        do_test(true);
     else if (!strcmp(argv[1], "file")) {
         if (argc < 3) {
             fprintf(stderr, "Please specify the name of an output file.\n");
@@ -4934,7 +4949,7 @@ int main(int argc, char **argv)
             exit(1);
         }
         do_file = true;
-        do_test();
+        do_test(false);
         fclose(fileout);
     }
     else if (!strcmp(argv[1], "adhoc"))

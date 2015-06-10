1a0f19b00a55dcdeab3dfeeb840755db4c1d2a2b
Konstantin Büttner <kb@mathias-kettner.de>
2015-06-10 15:33:15 +0200

    pfsense_counter: New check to monitor various global counters of pfsense firewalls

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -324,60 +324,69 @@ register_check_parameters(
     match_type = "dict",
 )
 
-# register_check_parameters(
-#     subgroup_applications,
-#     "pfsense_counter",
-#     _("Various global packet rates for pfSense Firewalls"),
-#     Dictionary(
-#         elements = [
-#             ("timerange", Integer(
-#                 title = _("Timeframe for Average"),
-#                 unit = _("min"),
-#                 default_value = 5)),
-#             ("match", Tuple(
-#                 title = _("Levels for rate of packets that matched a rule"),
-#                 elements = [
-#                     Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 20),
-#                     Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 30),
-#                 ])),
-#             ("badoffset", Tuple(
-#                 title = _("Levels for rate of packets with bad offset"),
-#                 elements = [
-#                     Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 20),
-#                     Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 30),
-#                 ])),
-#             ("fragment", Tuple(
-#                 title = _("Levels for rate of fragmented packets"),
-#                 elements = [
-#                     Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 20),
-#                     Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 30),
-#                 ])),
-#             ("short", Tuple(
-#                 title = _("Levels for rate of short packets"),
-#                 elements = [
-#                     Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 20),
-#                     Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 30),
-#                 ])),
-#             ("normalize", Tuple(
-#                 title = _("Levels for rate of normalized packets"),
-#                 elements = [
-#                     Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 20),
-#                     Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 30),
-#                 ])),
-#             ("memdrop", Tuple(
-#                 title = _("Levels for rate of packets dropped due to memory limitations"),
-#                 elements = [
-#                     Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 20),
-#                     Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 30),
-#                 ])),
-#         ],
-#     ),
-#     TextAscii(
-#         title = _("Interface"),
-#         help = _("The description of the interface as provided by the device"),
-#     ),
-#     match_type = "dict",
-# )
+register_check_parameters(
+     subgroup_applications,
+     "pfsense_counter",
+     _("pfSense Firewall Packet Rates"),
+     Dictionary(
+         help = _("This rule set is for configuring levels for global packet counters."),
+         elements = [
+             ("average", Integer(
+                  title = _("Averaging"),
+                  help = _("When this option is activated then the packet rates are being "
+                           "averaged <b>before</b> the levels are being applied. Setting this to zero will "
+                           "deactivate averaging."),
+                  unit = _("min"),
+                  default_value = 15,
+                  label = _("Compute average over last "),
+                  )),
+             ("matched", Tuple(
+                             title = _("Levels for rate of packets that matched a rule"),
+                             elements = [
+                                Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 85.0),
+                                Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 90.0),
+                             ],
+             )),
+             ("badoffset", Tuple(
+                             title = _("Levels for rate of packets with bad offset"),
+                             elements = [
+                                Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 85.0),
+                                Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 90.0),
+                             ],
+                 )),
+             ("fragment", Tuple(
+                             title = _("Levels for rate of fragmented packets"),
+                             elements = [
+                                Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 85.0),
+                                Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 90.0),
+                             ],
+                 )),
+             ("short", Tuple(
+                             title = _("Levels for rate of short packets"),
+                             elements = [
+                                Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 85.0),
+                                Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 90.0),
+                             ],
+                 )),
+             ("normalized", Tuple(
+                             title = _("Levels for rate of normalized packets"),
+                             elements = [
+                                Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 85.0),
+                                Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 90.0),
+                             ],
+                 )),
+             ("memdrop", Tuple(
+                             title = _("Levels for rate of packets dropped due to memory limitations"),
+                             elements = [
+                                Float(title = _("Warning at"), unit = _("pkts/s"), default_value = 85.0),
+                                Float(title = _("Critical at"), unit = _("pkts/s"), default_value = 90.0),
+                             ],
+                 )),
+         ],
+     ),
+     None,
+     match_type = "dict",
+)
 
 #.
 #   .--Environment---------------------------------------------------------.

c995a1118c8f9d57e4ca3e77f1cffdac5fdf0cc8
Simon Betz <si@mathias-kettner.de>
2015-08-14 13:01:55 +0200

    inserted spaces, better readability

--- web/plugins/metrics/check_mk.py
+++ web/plugins/metrics/check_mk.py
@@ -2145,7 +2145,7 @@ metric_info["net_data_sent"] = {
 }
 
 for ty, unit in [ ("requests", "1/s"), ("bytes", "bytes/s"), ("secs", "1/s") ]:
-    metric_info[ty+"_cmk_views"] = {
+    metric_info[ty + "_cmk_views"] = {
         "title" : _("Check_MK: Views"),
         "unit"  : unit,
         "color" : "#ff8080",

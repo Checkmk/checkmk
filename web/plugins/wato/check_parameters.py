53bfa0156289e783f3b9ebf0283c1c31bc3a3d3a
Simon Betz <si@mathias-kettner.de>
2015-09-11 16:18:36 +0200

    adapted hp_msa checks to the guidelines

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -9101,6 +9101,69 @@ register_check_parameters(
 )
 
 register_check_parameters(
+    subgroup_environment,
+    "hp_msa_psu_voltage",
+    _("HP MSA Power Supply Voltage Levels"),
+    Dictionary(
+        help = _("Here you can configure the 3.3V and 12V voltage levels for each power supply."),
+        elements = [
+            ( "levels_33v_lower",
+            Tuple(
+                title = _("3.3 Volt Output Lower Levels"),
+                elements = [
+                    Float(title = _("warning if below or equal"),  unit = "V", default_value = 3.25),
+                    Float(title = _("critical if below or equal"), unit = "V", default_value = 3.20),
+                ]
+            )),
+            ( "levels_33v_upper",
+            Tuple(
+                title = _("3.3 Volt Output Upper Levels"),
+                elements = [
+                    Float(title = _("warning if above or equal"),  unit = "V", default_value = 3.4),
+                    Float(title = _("critical if above or equal"), unit = "V", default_value = 3.45),
+                ]
+            )),
+            ( "levels_5v_lower",
+            Tuple(
+                title = _("3.3 Volt Output Lower Levels"),
+                elements = [
+                    Float(title = _("warning if below or equal"),  unit = "V", default_value = 3.25),
+                    Float(title = _("critical if below or equal"), unit = "V", default_value = 3.20),
+                ]
+            )),
+            ( "levels_5v_upper",
+            Tuple(
+                title = _("3.3 Volt Output Upper Levels"),
+                elements = [
+                    Float(title = _("warning if above or equal"),  unit = "V", default_value = 3.4),
+                    Float(title = _("critical if above or equal"), unit = "V", default_value = 3.45),
+                ]
+            )),
+            ( "levels_12v_lower",
+            Tuple(
+                title = _("12 Volt Output Lower Levels"),
+                elements = [
+                    Float(title = _("warning if below or equal"),  unit = "V", default_value = 11.9),
+                    Float(title = _("critical if below or equal"), unit = "V", default_value = 11.8),
+                ]
+            )),
+            ( "levels_12v_upper",
+            Tuple(
+                title = _("12 Volt Output Upper Levels"),
+                elements = [
+                    Float(title = _("warning if above or equal"),  unit = "V", default_value = 12.1),
+                    Float(title = _("critical if above or equal"), unit = "V", default_value = 12.2),
+                ]
+            ))
+        ]
+    ),
+    TextAscii(
+        title = _("Power Supply name"),
+        help = _("The identifier of the power supply.")),
+    "dict"
+)
+
+register_check_parameters(
      subgroup_applications,
     "jvm_gc",
     _("JVM garbage collection levels"),

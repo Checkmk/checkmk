25db139bdfc9ca7a203cd894b77662d0f623a235
Lars Michelsen <lm@mathias-kettner.de>
2015-09-11 10:22:45 +0200

    #2604 FIX Dashboards: Improved error handling in case of broken dashlet

--- web/htdocs/js/checkmk.js
+++ web/htdocs/js/checkmk.js
@@ -2502,7 +2502,7 @@ function add_to_visual(visual_type, visual_name)
 
 function pagetype_add_to_container(page_type, page_name)
 {
-    var element_type = popup_data[0]; // e.g. 'graph'
+    var element_type = popup_data[0]; // e.g. 'pnpgraph'
     var create_info  = popup_data[1]; // complex JSON struct describing the thing
     var create_info_json = JSON.stringify(create_info);
 

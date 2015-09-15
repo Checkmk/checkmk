7b66585d8f4ab81f6ea13af23d3442d6a3022e3c
Mathias Kettner <mk@mathias-kettner.de>
2015-09-15 17:42:16 +0200

    Updated all file headers

--- agents/windows/nowin.cc
+++ agents/windows/nowin.cc
@@ -9,40 +9,18 @@
 // +------------------------------------------------------------------+
 //
 // This file is part of Check_MK.
-// The official homepage is at http://mathias-kettner.de/check_mk.
+// Copyright by Mathias Kettner and Mathias Kettner GmbH.  All rights reserved.
 //
-// check_mk is free software;  you can redistribute it and/or modify it
+// Check_MK is free software;  you can redistribute it and/or modify it
 // under the  terms of the  GNU General Public License  as published by
-// the Free Software Foundation in version 2.  check_mk is  distributed
-// in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
-// out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
-// PARTICULAR PURPOSE. See the  GNU General Public License for more de-
-// ails.  You should have  received  a copy of the  GNU  General Public
-// License along with GNU Make; see the file  COPYING.  If  not,  write
-// to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
-// Boston, MA 02110-1301 USA.
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int main(int argc, char **argv)
-{
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s MINUTES\n", argv[0]);
-        exit(1);
-    }
-    int minutes = atoi(argv[1]);
-    time_t now = time(0);
-    time_t then = now + minutes * 60;
-
-    struct tm *t;
-    char out[64];
-    t = localtime(&now);
-    strftime(out, sizeof(out), "%Y-%m-%d %H:%M", t);
-    printf("%s ", out);
-    t = localtime(&then);
-    strftime(out, sizeof(out), "%Y-%m-%d %H:%M", t);
-    printf("%s\n", out);
-}
+// the Free Software Foundation in version 2.
+//
+// Check_MK is  distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY;  without even the implied warranty of
+// MERCHANTABILITY  or  FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have  received  a copy of the  GNU  General Public
+// License along with Check_MK.  If  not, email to mk@mathias-kettner.de
+// or write to the postal address provided at www.mathias-kettner.de
 
--- doc/treasures/mod_contacts_mk.py
+++ doc/treasures/mod_contacts_mk.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# Copyright by Mathias Kettner and Mathias Kettner GmbH.  All rights reserved.
+#
+# Check_MK is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.
+#
+# Check_MK is  distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY;  without even the implied warranty of
+# MERCHANTABILITY  or  FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have  received  a copy of the  GNU  General Public
+# License along with Check_MK.  If  not, email to mk@mathias-kettner.de
+# or write to the postal address provided at www.mathias-kettner.de
+
+#!/usr/bin/python
 # This script can be used to modify the contacts.mk file
 # The core mus be reloaded after changes
 import sys, os

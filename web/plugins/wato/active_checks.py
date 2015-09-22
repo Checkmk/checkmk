a6910ea6c8368953c416d076d7fbeefb7adbea51
Sebastian Herbord <sh@mathias-kettner.de>
2015-09-22 10:24:37 +0200

    #2628 check_bi_aggr service now also goes into downtime if the monitored bi is in downtime
    the service is assigned a "real" downtime even if the downtime of the bi is derived from the
    aggregated services.
    The downtime is also removed automatically when the bi leaves its own downtime.
    Assignment and removal of the downtime happens only when the check is run so there will be a delay
    between bi going into downtime and the service following.

--- web/plugins/wato/active_checks.py
+++ web/plugins/wato/active_checks.py
@@ -331,7 +331,7 @@ register_rule(group,
            Dictionary(
                title = _("Optional parameters"),
                elements = [
-                   ("name", 
+                   ("name",
                         TextUnicode(
                             title = _("Alternative Service description"),
                             help = _("The service description will be this name instead <i>DNS Servername</i>"),
@@ -1515,6 +1515,15 @@ register_rule(group,
                             ( "ok", _("Force to be OK") ),
                             ( "warn", _("Force to be WARN, if aggregate is not OK") ),
                           ]
+                    )),
+                    ("track_downtimes",
+                     Checkbox(
+                        title = _("Track downtimes"),
+                        label = _("Automatically track downtimes of aggregation"),
+                        help = _("If this is active, the check will automatically go into downtime "
+                                "whenever the aggregation does. This downtime is also cleaned up "
+                                "automatically when the aggregation leaves downtime. "
+                                "Downtimes you set manually for this check are unaffected."),
                     )),
                 ]
             ),

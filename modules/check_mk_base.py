28eb7e4ae1e0c5b76f11d8a4d5f75dc35dc45079
Mathias Kettner <mk@mathias-kettner.de>
2015-10-25 13:55:02 +0100

    #2668 FIX jar_signature: Handle case correctly where certificate is already expired

--- modules/check_mk_base.py
+++ modules/check_mk_base.py
@@ -51,6 +51,7 @@ import signal
 import math
 import tempfile
 import traceback
+import subprocess
 
 # PLANNED CLEANUP:
 # - central functions for outputting verbose information and bailing
@@ -769,9 +770,7 @@ def get_agent_info(hostname, ipaddress, max_cache_age):
 def get_agent_info_program(commandline):
     exepath = commandline.split()[0] # for error message, hide options!
 
-    import subprocess
-    if opt_verbose:
-        sys.stderr.write("Calling external program %s\n" % commandline)
+    vverbose("Calling external program %s\n" % commandline)
     try:
         p = subprocess.Popen(commandline, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         stdout, stderr = p.communicate()

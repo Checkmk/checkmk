Checkmk/checkmk

commit 2f0d9872ab8232ab6e0c1a92a6a2c7f2cfe961ec

some cleanup and fixes in the modernized perfcounter code and added a small test application

agents/windows/build_version
@@ -1,1 +1,1 @@
-673
+675

agents/windows/check_mk_agent.cc
@@ -118,15 +118,6 @@ const char *check_mk_version = CHECK_MK_VERSION;
 #endif
 
 
-#ifdef _LP64
-#define PRIdword  "d"
-#define PRIudword "u"
-#else
-#define PRIdword  "ld"
-#define PRIudword "lu"
-#endif
-
-
 using namespace std;
 
 

agents/windows/Configuration.cc
@@ -25,6 +25,7 @@
 
 #include "Configuration.h"
 #include "stringutil.h"
+#include "PerfCounter.h"
 #include <cstdio>
 #include <cstdlib>
 #include <cassert>
@@ -180,71 +181,6 @@ bool Configuration::handleGlobalConfigVariable(char *var, char *value)
 }
 
 
-// retrieve the next line from a multi-sz registry key
-const TCHAR *get_next_multi_sz(const std::vector<TCHAR> &data, size_t &offset)
-{
-    const TCHAR *next = &data[offset];
-    size_t len = strlen(next);
-    if ((len == 0) || (offset + len > data.size())) {
-        // the second condition would only happen with an invalid registry value but that's not
-        // unheard of
-        return NULL;
-    } else {
-        offset += len + 1;
-        return next;
-    }
-}
-
-
-int Configuration::getCounterIdFromLang(const char *language, const char *counter_name)
-{
-    char regkey[512];
-    snprintf(regkey, sizeof(regkey), "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib\\%s", language);
-    HKEY hKey;
-    LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, regkey, REG_MULTI_SZ, KEY_READ, &hKey);
-
-    // preflight
-    std::vector<TCHAR> szValueName;
-    DWORD dwcbData = 0;
-    RegQueryValueEx(hKey, "Counter", NULL, NULL, (LPBYTE)&szValueName[0], &dwcbData);
-    szValueName.resize(dwcbData);
-    // actual read op
-    RegQueryValueEx(hKey, "Counter", NULL, NULL, (LPBYTE) &szValueName[0], &dwcbData);
-    RegCloseKey (hKey);
-
-    if (result != ERROR_SUCCESS) {
-        return -1;
-    }
-
-    size_t offset = 0;
-    for(;;) {
-        const TCHAR *id = get_next_multi_sz(szValueName, offset);
-        const TCHAR *name = get_next_multi_sz(szValueName, offset);
-        if ((id == NULL) || (name == NULL)) {
-            break;
-        }
-        if (strcmp(name, counter_name) == 0) {
-            return strtol(id, NULL, 10);
-        }
-    }
-
-    return -1;
-}
-
-int Configuration::getPerfCounterId(const char *counter_name)
-{
-    int counter_id;
-    // Try to find it in current language
-    if ((counter_id = getCounterIdFromLang("CurrentLanguage", counter_name)) != -1)
-        return counter_id;
-
-    // Try to find it in english
-    if ((counter_id = getCounterIdFromLang("009", counter_name)) != -1)
-        return counter_id;
-
-    return -1;
-}
-
 bool Configuration::handleWinperfConfigVariable(char *var, char *value)
 {
     if (!strcmp(var, "counters")) {
@@ -262,7 +198,7 @@ bool Configuration::handleWinperfConfigVariable(char *var, char *value)
         for (unsigned int i = 0; i < strlen(value); i++)
             if (!isdigit(value[i])) {
                 is_digit = false;
-                int id = getPerfCounterId(value);
+                int id = PerfCounterObject::resolve_counter_name(value);
                 if (id == -1) {
                     fprintf(stderr, "No matching performance counter id found for %s.\n", value);
                     return false;

agents/windows/PerfCounter.cc
@@ -1,6 +1,7 @@
 #include "PerfCounter.h"
 #include "stringutil.h"
 #include <windows.h>
+#include <tchar.h>
 #include <vector>
 #include <cstdio>
 #include <stdexcept>
@@ -169,7 +170,6 @@ PerfCounterObject::PerfCounterObject(unsigned int counter_base_number)
             // the size of performance counter blocks is varible and may change concurrently,
             // so there is no way to ensure the buffer is large enough before the call, we
             // can only increase the buffer size until the call succeeds
-            verbose("Buffer for RegQueryValueEx too small. Resizing...");
             buffer_size = _buffer.size() * 2;
             _buffer.resize(buffer_size);
         } else {
@@ -186,8 +186,11 @@ PerfCounterObject::PerfCounterObject(unsigned int counter_base_number)
     _buffer.resize(buffer_size);
 
     _object = findObject(counter_base_number);
+    if (_object == NULL) {
+        throw std::runtime_error("counter id not found");
+    }
 
-    if (_object->NumInstances == 0) {
+    if (_object->NumInstances <= 0) {
         // set the datablock pointer, but only on an instanceless
         // counter, otherwise it is meaningless
         PERF_COUNTER_DEFINITION *counter = FirstCounter(_object);
@@ -261,4 +264,103 @@ std::vector<PerfCounter> PerfCounterObject::counters() const
     }
     return result;
 }
+
+
+// retrieve the next line from a multi-sz registry key
+const TCHAR *get_next_multi_sz(const std::vector<TCHAR> &data, size_t &offset)
+{
+    const TCHAR *next = &data[offset];
+    size_t len = strlen(next);
+    if ((len == 0) || (offset + len > data.size())) {
+        // the second condition would only happen with an invalid registry value but that's not
+        // unheard of
+        return NULL;
+    } else {
+        offset += len + 1;
+        return next;
+    }
+}
+
+
+PerfCounterObject::CounterList PerfCounterObject::counter_list(const char *language)
+{
+    CounterList output;
+
+    HKEY hKey;
+    LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+            (std::string("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib\\") + language).c_str(),
+            REG_MULTI_SZ, KEY_READ, &hKey);
+
+    // preflight
+    std::vector<char> szValueName;
+    DWORD dwcbData = 0;
+    RegQueryValueExA(hKey, "Counter", NULL, NULL, (LPBYTE)&szValueName[0], &dwcbData);
+    szValueName.resize(dwcbData);
+    // actual read op
+    RegQueryValueExA(hKey, "Counter", NULL, NULL, (LPBYTE) &szValueName[0], &dwcbData);
+    RegCloseKey(hKey);
+
+    if (result == ERROR_SUCCESS) {
+        size_t offset = 0;
+        for(;;) {
+            const char *id = get_next_multi_sz(szValueName, offset);
+            const char *name = get_next_multi_sz(szValueName, offset);
+            if ((id == NULL) || (name == NULL)) {
+                break;
+            }
+
+            output.push_back(std::make_pair<int, std::string>(strtol(id, NULL, 10), name));
+        }
+    }
+
+    return output;
+
+}
+
+
+int PerfCounterObject::resolve_counter_name(const char *counter_name, const char *language)
+{
+    if (language == NULL) {
+        // "autodetect", which means we try local language and english
+        int result = resolve_counter_name(counter_name, "CurrentLanguage");
+        if (result == -1) {
+            result = resolve_counter_name(counter_name, "009");
+        }
+        return result;
+    }
+    else {
+        HKEY hKey;
+        LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+                (std::string("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Perflib\\") + language).c_str(),
+                REG_MULTI_SZ, KEY_READ, &hKey);
+
+        // preflight
+        std::vector<TCHAR> szValueName;
+        DWORD dwcbData = 0;
+        RegQueryValueEx(hKey, "Counter", NULL, NULL, (LPBYTE)&szValueName[0], &dwcbData);
+        szValueName.resize(dwcbData);
+        // actual read op
+        RegQueryValueEx(hKey, "Counter", NULL, NULL, (LPBYTE) &szValueName[0], &dwcbData);
+        RegCloseKey (hKey);
+
+        if (result != ERROR_SUCCESS) {
+            return -1;
+        }
+
+        size_t offset = 0;
+        for(;;) {
+            const TCHAR *id = get_next_multi_sz(szValueName, offset);
+            const TCHAR *name = get_next_multi_sz(szValueName, offset);
+            if ((id == NULL) || (name == NULL)) {
+                break;
+            }
+            if (_tcscmp(name, counter_name) == 0) {
+                return _tcstol(id, NULL, 10);
+            }
+        }
+
+        return -1;
+    }
+}
+
 

agents/windows/PerfCounter.h
@@ -71,6 +71,10 @@ class PerfCounterObject {
 
 public:
 
+    typedef std::vector<std::pair<int, std::string>> CounterList;
+
+public:
+
     PerfCounterObject(unsigned counter_base_number);
 
     bool isEmpty() const;
@@ -78,6 +82,9 @@ public:
     std::vector<PERF_INSTANCE_DEFINITION*> instances() const;
     std::vector<std::wstring> instanceNames() const;
     std::vector<PerfCounter> counters() const;
+
+    static CounterList counter_list(const char *language);
+    static int resolve_counter_name(const char *name, const char *language = NULL);
 
 private:
 

agents/windows/perftest.cc
@@ -1,0 +1,81 @@
+#include "PerfCounter.h"
+#include "stringutil.h"
+#include "types.h"
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
+
+void print_usage(const char *exe_name)
+{
+    printf("Usage: %s pattern"
+            "\n\t                    - print all performance counters that match the pattern"
+            , exe_name);
+}
+
+
+void print_perf_counter(int counter_id, const char *counter_name)
+{
+    PerfCounterObject counterObject(counter_id);
+
+    if (!counterObject.isEmpty()) {
+        printf("<<<%s:%d>>>\n", counter_name, counter_id);
+        printf("index,type,\"%ls\"\n", join(counterObject.instanceNames(), L"\",\"").c_str());
+
+        std::vector<PERF_INSTANCE_DEFINITION*> instances = counterObject.instances();
+
+        // output counters
+        for (const PerfCounter &counter : counterObject.counters()) {
+            printf("%" PRIudword, counter.titleIndex());
+            printf(",%s", counter.typeName().c_str());
+            for (ULONGLONG value : counter.values(instances)) {
+                printf(",%" PRIu64, value);
+            }
+            printf("\n");
+        }
+    }
+}
+
+
+void print_perf_counter(const char *counter_pattern)
+{
+    for (auto counter : PerfCounterObject::counter_list("CurrentLanguage")) {
+        if (globmatch(counter_pattern, counter.second.c_str())) {
+            try {
+                print_perf_counter(counter.first, counter.second.c_str());
+            } catch (const std::exception &e) {
+                printf("Failed to read %s:%d: %s\n",
+                        counter.second.c_str(), counter.first, e.what());
+            }
+        }
+    }
+    for (auto counter : PerfCounterObject::counter_list("009")) {
+        if (globmatch(counter_pattern, counter.second.c_str())) {
+            try {
+                print_perf_counter(counter.first, counter.second.c_str());
+            } catch (const std::exception &e) {
+                printf("Failed to read %s:%d: %s\n",
+                        counter.second.c_str(), counter.first, e.what());
+            }
+        }
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    try {
+        print_perf_counter(argv[1]);
+
+        return 0;
+    } catch (const std::exception &e) {
+        printf("Failed: %s\n", e.what());
+    }
+
+
+    return 0;
+}

commit 99652b77cd35ad18f6773c6812f39774653dba5f

fixed a bug introduced in 2f0d9872ab8232ab6e0c1a92a6a2c7f2cfe961ec, causing missing memory info in ps section

agents/windows/Makefile
@@ -124,6 +124,7 @@ check_mk_agent-64.exe.versioned:
 
 
 %.exe: %.unversioned.exe %.exe.versioned
+	true
 
 # -----------------------------------------------------------------------------
 # NSIS installer will be removed and replaced by multiarch MSI installer
@@ -156,7 +157,7 @@ nowin.exe: nowin.cc
 	$(CXX) -o $@ $<
 
 clean:
-	rm -f *.o *~ svn-*.tmp *.rc *.res obj/*.o obj64/*.o
+	rm -f *.o *.exe *~ svn-*.tmp *.rc *.res obj/*.o obj64/*.o
 	make -C msibuild clean
 
 mrproper: clean

agents/windows/build_version
@@ -1,1 +1,1 @@
-675
+677

agents/windows/check_mk_agent.cc
@@ -1120,7 +1120,7 @@ bool ExtractProcessOwner(HANDLE hProcess_i, string& csOwner_o)
 
 process_entry_t get_process_perfdata()
 {
-    map<ULONGLONG, process_entry> process_info;
+    process_entry_t process_info;
 
     PerfCounterObject counterObject(230);  // process base number
 
@@ -1132,7 +1132,7 @@ process_entry_t get_process_perfdata()
 
         std::vector<process_entry> entries(instances.size()); // one instance = one process
 
-        // output counters
+        // gather counters
         for (const PerfCounter &counter : counterObject.counters()) {
             std::vector<ULONGLONG> values = counter.values(instances);
             for (std::size_t i = 0; i < values.size(); ++i) {
@@ -1147,6 +1147,10 @@ process_entry_t get_process_perfdata()
                               break;
                 }
             }
+        }
+
+        for (const process_entry &entry : entries) {
+            process_info[entry.process_id] = entry;
         }
     }
     return process_info;
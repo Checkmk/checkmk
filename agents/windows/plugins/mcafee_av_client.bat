f43680f8c25c0984ebb4d2101515445ef136c7c9
Sebastian Herbord <sh@mathias-kettner.de>
2016-02-01 15:15:40 +0100

    #3063 FIX mcaffee_av_client: plugin didn't work with 32-bit av software installed on 64-bit system

--- agents/windows/plugins/mcafee_av_client.bat
+++ agents/windows/plugins/mcafee_av_client.bat
@@ -8,7 +8,12 @@ rem #  -------------------------------------------------------------------------
 setlocal enableDelayedExpansion
 set dateval=
 
-for /f "tokens=3" %%a in ('reg query "HKLM\SOFTWARE\McAfee\AvEngine" ^|find "AVDatDate" ^|find "REG_SZ"') do @set dateval=%%a
+for /f "tokens=3" %%a in ('reg query "HKLM\SOFTWARE\McAfee\AvEngine" ^|find /I "AVDatDate" ^|find "REG_SZ"') do @set dateval=%%a
+if "%dateval%"=="" (
+    rem # on 64 bit systems the registry key is in the wow64/32 branch
+    for /f "tokens=3" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\McAfee\AvEngine" ^|find /I "AVDatDate" ^|find "REG_SZ"') do @set dateval=%%a
+)
+
 if not "%dateval%"=="" (
   echo ^<^<^<mcafee_av_client^>^>^>
   echo %dateval%

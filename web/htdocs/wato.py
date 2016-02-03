5ff2068f7e33c97174f738bb2002f330139a1073
Lars Michelsen <lm@mathias-kettner.de>
2016-02-03 15:38:02 +0100

    Fixed wrong code formating

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -14022,8 +14022,7 @@ def create_sample_config():
     save_notification_rules(notification_rules)
 
     if "create_cee_sample_config" in globals():
-            create_cee_sample_config()
-
+        create_cee_sample_config()
 
     # Make sure the host tag attributes are immediately declared!
     config.wato_host_tags = wato_host_tags

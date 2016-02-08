b1c1cb8a37f31a8644ff1a30401208c42d8ac8a6
Simon Betz <si@mathias-kettner.de>
2016-02-08 10:44:44 +0100

    fixed typo in all headers

--- pnp-templates/check_mk-hivemanager_devices.php
+++ pnp-templates/check_mk-hivemanager_devices.php
@@ -18,7 +18,7 @@
 # in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
 # out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
 # PARTICULAR PURPOSE. See the  GNU General Public License for more de-
-# ails.  You should have  received  a copy of the  GNU  General Public
+# tails. You should have  received  a copy of the  GNU  General Public
 # License along with GNU Make; see the file  COPYING.  If  not,  write
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.

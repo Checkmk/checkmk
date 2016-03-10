880dda03365cb1fbd5f5c09be706debbb8fca8cf
Lars Michelsen <lm@mathias-kettner.de>
2016-03-10 18:08:37 +0100

    Invalid exception handling when trying to edit a non existing custom user attribute

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -14259,7 +14259,7 @@ def mode_edit_custom_attr(phase, what):
     if not new:
         attr = [ a for a in attrs if a['name'] == name ]
         if not attr:
-            raise MKUserError(_('The attribute does not exist.'))
+            raise MKUserError(None, _('The attribute does not exist.'))
         else:
             attr = attr[0]
     else:

575d06aa84f8003282da6e5681964f999383fe5d
Lars Michelsen <lm@mathias-kettner.de>
2016-04-01 08:23:03 +0200

    FilterInvtableAge: Fixed broken function call

--- web/plugins/visuals/inventory.py
+++ web/plugins/visuals/inventory.py
@@ -77,11 +77,14 @@ class FilterInvtableAge(Filter):
         Age(display=["days"]).render_input(self.name + "_to", 0)
         html.write("</tr></table>")
 
+
     def double_height(self):
         return True
 
+
     def filter_table(self, rows):
-        return filter_table_with_conversion(self, rows, lambda age: age)
+        return self.filter_table_with_conversion(rows, lambda age: age)
+
 
     def filter_table_with_conversion(self, rows, conv):
         from_value = Age().from_html_vars(self.name + "_from")
@@ -102,6 +105,7 @@ class FilterInvtableAge(Filter):
                     continue
                 newrows.append(row)
         return newrows
+
 
 
 class FilterInvtableTimestampAsAge(FilterInvtableAge):

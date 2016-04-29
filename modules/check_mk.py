27fd503c17b3500690b2a53a26e3b5e8ce8a3917
Lars Michelsen <lm@mathias-kettner.de>
2016-04-29 17:00:42 +0200

    Removed test stuff

--- modules/check_mk.py
+++ modules/check_mk.py
@@ -48,8 +48,6 @@ import inspect
 # These variable will be substituted at 'make dist' time
 check_mk_version  = '(inofficial)'
 
-LKAHFIA@H%@%A:O OHOH
-
 #   .--Prelude-------------------------------------------------------------.
 #   |                  ____           _           _                        |
 #   |                 |  _ \ _ __ ___| |_   _  __| | ___                   |

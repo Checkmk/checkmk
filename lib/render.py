6ca251e0d43e95e815463e90f07ef50ebc9dbcfc
Mathias Kettner <mk@mathias-kettner.de>
2016-06-07 15:09:39 +0200

    Start experimental lib module cmk.render

--- lib/render.py
+++ lib/render.py
@@ -23,3 +23,24 @@
 # License along with GNU Make; see the file  COPYING.  If  not,  write
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
+
+"""This module contains functions that transform Python values into
+text representations optimized for human beings - with optional localization.
+The resulting strings are not ment to be parsed into values again later. They
+are just for optical output purposes."""
+
+# THIS IS STILL EXPERIMENTAL
+
+import time
+
+# TODO: Clean this up one day by using the way recommended by gettext.
+# (See https://docs.python.org/2/library/gettext.html). For this we
+# need the path to the locale files here.
+try:
+    _
+except NameError:
+    _ = lambda x: x # Fake i18n when not available
+
+
+def date(timestamp):
+    return time.strftime("%Y-%m-%d", time.localtime(timestamp))

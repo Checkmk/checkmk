25d22b337b8f5ece85953e8eaf37e0079b5d79ed
Andreas Boesl <ab@mathias-kettner.de>
2016-06-13 14:24:11 +0200

    3543 mk_inotify: new plugin to monitor file system operations (linux)
    This plugin allows you to check the age of the last file operation
    on configurable files and folders. You can check the age of the files or folders
    last <tt>create</tt>, <tt>open</tt>, <tt>delete</tt>, <tt>modify</tt> and <tt>access</tt>
    operation.

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -2232,6 +2232,44 @@ register_check_parameters(
     match_type = "dict"
 )
 
+register_check_parameters(
+    subgroup_storage,
+    "inotify",
+    _("INotify Levels"),
+    Dictionary(
+        help = _("This rule allows you to set levels for specific Inotify changes. "
+                 "Keep in mind that you can only monitor operations which are actually "
+                 "enabled in the Inotify plugin. So it might be a good idea to cross check "
+                 "these levels here with the configuration rule in the agent bakery. "),
+        elements = [
+            ('age_last_operation',
+                ListOf(
+                    Tuple(
+                        elements = [
+                            DropdownChoice( title = _("INotify Operation"),
+                                choices = [ ("create", _("Create")),
+                                            ("delete", _("Delete")),
+                                            ("access", _("Access")),
+                                            ("modify", _("Modify")),
+                                            ("open", _("Open")) ]
+                            ),
+                            Age(title=_("Warning at")),
+                            Age(title=_("Critical at")),
+                        ],
+                    ),
+                    title = _("Age of last operation"),
+                    movable = False
+             )),
+        ],
+        optional_keys = None,
+    ),
+    TextAscii(
+        title = _("The filesystem path, prefixed with <i>File </i> or <i>Folder </i>"),
+    ),
+    match_type = "dict"
+)
+
+
 
 #.
 #   .--Unsorted--(Don't create new stuff here!)----------------------------.

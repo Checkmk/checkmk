a962b14c265d5e236958a2634619c6f154637a79
Lars Michelsen <lm@mathias-kettner.de>
2016-07-01 11:54:53 +0200

    3661 FIX Service discovery is now possible with only "modify services" permission
    Users which are permitted on a host, are not permitted to modify existing hosts
    but have the permission "modify services" are now able to perform service discoveries
    again using WATO.

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -2686,7 +2686,6 @@ def mode_inventory(phase, firsttime):
             html.context_button(_("Full Scan"), html.makeuri([("_scan", "yes")]))
 
     elif phase == "action":
-        host.need_permission("write")
         if html.check_transaction():
             # Settings for showing parameters
             if html.var("_show_parameters"):

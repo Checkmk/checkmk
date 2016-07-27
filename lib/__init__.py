4543625bdbe1ffc56eec19978c06862762806b9c
Lars Michelsen <lm@mathias-kettner.de>
2016-07-27 16:09:37 +0200

    Added own version info to lib

--- lib/__init__.py
+++ lib/__init__.py
@@ -29,4 +29,4 @@
 This library is currently handled as internal module of Check_MK and
 does not offer stable APIs. The code may change at any time."""
 
-__version__ = "(inofficial)"
+__version__ = "1.4.0i1"

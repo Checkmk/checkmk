d7fe58298bec139a0b91c8d9c569201247a866a1
Simon Betz <si@mathias-kettner.de>
2016-09-02 11:04:05 +0200

    3868 f5_bigip_cluster_status: node states are configurable for F5 BigIP devices since version 11.2

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -480,6 +480,19 @@ register_check_parameters(
                 default_value = "active_standby"
              )
             ),
+            ("v11_2_states",
+                Dictionary(
+                    title = _("Interpretation of failover cluster state"),
+                    help  = _("Here you can set the failover state for BIG-IP system of version 11.2.0"),
+                    elements = [
+                        ("0", MonitoringState(title = "Unknown", default_value = 3)),
+                        ("1", MonitoringState(title = "Offline", default_value = 2)),
+                        ("2", MonitoringState(title = "Forced offline", default_value = 2)),
+                        ("3", MonitoringState(title = "Standby", default_value = 0)),
+                        ("4", MonitoringState(title = "Active", default_value = 0)),
+                    ]
+                )
+            )
         ],
         required_keys = ["type"]
     ),

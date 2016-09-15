89e2408a1d54fe28bd5c2f66bd0a7d0068e181e6
Lars Michelsen <lm@mathias-kettner.de>
2016-09-15 13:22:51 +0200

    Fixed crash reporting modifying data strcutures it reported a crash for

---
+++ tests/lib/test_crash_reporting.py
@@ -0,0 +1,62 @@
+import copy
+import cmk.crash_reporting as crash_reporting
+
+def test_format_var_for_export_strip_nested_dict():
+    orig_var = {
+        "a": {
+            "b": {
+                "c": {
+                    "d": {
+
+                    },
+                },
+            },
+        },
+    }
+
+    var = copy.deepcopy(orig_var)
+    formated = crash_reporting.format_var_for_export(var)
+
+    # Stripped?
+    assert formated["a"]["b"]["c"]["d"] == "Max recursion depth reached"
+
+    # Not modified original data
+    assert orig_var == var
+
+
+def test_format_var_for_export_strip_large_data():
+    orig_var = {
+        "a": {
+            "y": ("a" * 1024 * 1024) + "a"
+        },
+    }
+
+    var = copy.deepcopy(orig_var)
+    formated = crash_reporting.format_var_for_export(var)
+
+    # Stripped?
+    assert formated["a"]["y"].endswith("(1 bytes stripped)")
+
+    # Not modified original data
+    assert orig_var == var
+
+
+def test_format_var_for_export_strip_nested_dict_with_list():
+    orig_var = {
+        "a": {
+            "b": {
+                "c": [
+                    {}
+                ],
+            },
+        },
+    }
+
+    var = copy.deepcopy(orig_var)
+    formated = crash_reporting.format_var_for_export(var)
+
+    # Stripped?
+    assert formated["a"]["b"]["c"][0] == "Max recursion depth reached"
+
+    # Not modified original data
+    assert orig_var == var

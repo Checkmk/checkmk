c0bb36c9e04de31232b30a1fba57407462d1babc
Marcel Arentz <ma@mathias-kettner.de>
2016-09-23 11:58:21 +0200

    3840 FIX cisco_qos: Fixed wrong units in graphs
    The used units in graphs for QoS-Services changed from bytes/s to bits/s
    and is now equivalent to other network service graphs.

--- web/plugins/metrics/check_mk.py
+++ web/plugins/metrics/check_mk.py
@@ -5780,8 +5780,8 @@ graph_info.append({
 graph_info.append({
     "title"   : _("QoS class traffic"),
     "metrics" : [
-        ( "qos_outbound_bytes_rate", "area" ),
-        ( "qos_dropped_bytes_rate", "-area" ),
+        ( "qos_outbound_bytes_rate,8,*@bits/s", "area" ),
+        ( "qos_dropped_bytes_rate,8,*@bits/s", "-area" ),
     ],
 })
 

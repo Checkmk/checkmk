1a96062e43009eedbc70093759f4492971e4f9e1
Konstantin Büttner <kb@mathias-kettner.de>
2016-10-25 17:29:42 +0200

    3749 storcli_pdisks, storcli_vdrives: New checks to monitor RAID status via StorCLI utility

---
+++ agents/windows/plugins/storcli.bat
@@ -0,0 +1,13 @@
+@ECHO off & setlocal EnableDelayedExpansion
+
+REM customize StorCli path to your needs
+SET "StorCli=C:\Program Files\StorCLI\storcli64.exe"
+
+IF NOT EXIST !StorCli! GOTO END
+
+ECHO ^<^<^<storcli_pdisks^>^>^>
+"!StorCli!" /call/eall/sall show
+ECHO ^<^<^<storcli_vdrives^>^>^>
+"!StorCli!" /call/vall show
+
+:END

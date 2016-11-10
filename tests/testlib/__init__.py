dc4714ec19d58b008584e8133fafe6f62ce4a861
Lars Michelsen <lm@mathias-kettner.de>
2016-11-10 08:31:30 +0100

    Fixed permissions of .f12 call

--- tests/testlib/__init__.py
+++ tests/testlib/__init__.py
@@ -256,7 +256,8 @@ class Site(object):
         for path in paths:
             if os.path.exists("%s/.f12" % path):
                 print("Executing .f12 in \"%s\"" % path)
-                assert os.system("cd \"%s\" ; ONLY_COPY=1 bash .f12" % path) >> 8 == 0
+                assert os.system("cd \"%s\" ; "
+                                 "sudo ONLY_COPY=1 SITE=%s bash .f12" % (path, self.id)) >> 8 == 0
 
 
     def rm_if_not_reusing(self):

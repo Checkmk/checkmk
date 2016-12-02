c5b5f99929a18867aa57e3a69f717989eddcb588
Lars Michelsen <lm@mathias-kettner.de>
2016-12-02 10:33:16 +0100

    Added test for new get_graph API

--- tests/web/test_webapi.py
+++ tests/web/test_webapi.py
@@ -64,3 +64,23 @@ def test_activate_changes(web, site):
     finally:
         web.delete_host("test-host-activate")
         web.activate_changes()
+
+
+def test_get_graph(web):
+    try:
+        web.add_host("test-host-get-graph", attributes={
+            "ipaddress": "127.0.0.1",
+        })
+        web.discover_services("test-host-get-graph")
+        web.activate_changes()
+
+        data = web.get_regular_graph("test-host-get-graph", "Check_MK", 0)
+        assert len(data["curves"]) == 4
+        assert data["curves"][0]["title"] == "CPU time in user space"
+        assert data["curves"][1]["title"] == "CPU time in operating system"
+        assert data["curves"][2]["title"] == "Time spent waiting for Check_MK agent"
+        assert data["curves"][3]["title"] == "Total execution time"
+
+    finally:
+        web.delete_host("test-host-get-graph")
+        web.activate_changes()

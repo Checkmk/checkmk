e100a55266d2657d46eabbd5461045aefa618f9c
Lars Michelsen <lm@mathias-kettner.de>
2016-12-05 14:29:15 +0100

    4130 FIX Improved error handling when trying to edit not existant rules

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -12220,7 +12220,13 @@ def mode_edit_ruleset(phase):
         rulesets = load_rulesets(rule_folder)
         rules = rulesets.get(varname, [])
 
-        rulenr = int(html.var("_rulenr")) # rule number relativ to folder
+        try:
+            rulenr = int(html.var("_rulenr")) # rule number relativ to folder
+            rule = rules[rulenr]
+        except (IndexError, TypeError, ValueError):
+            raise MKUserError("rulenr", _("You are trying to edit a rule which does not exist "
+                                              "anymore."))
+
         action = html.var("_action")
 
         if action == "delete":
@@ -12252,7 +12258,7 @@ def mode_edit_ruleset(phase):
         else:
             if not html.check_transaction():
                 return None # browser reload
-            rule = rules[rulenr]
+
             del rules[rulenr]
             if action == "up":
                 rules[rulenr-1:rulenr-1] = [ rule ]
@@ -12827,10 +12833,10 @@ def mode_edit_rule(phase, new = False):
             return
         rulenr = len(rules)
     else:
-        rulenr = int(html.var("rulenr"))
         try:
+            rulenr = int(html.var("rulenr"))
             rule = rules[rulenr]
-        except IndexError:
+        except (TypeError, ValueError, IndexError):
             if phase == "action":
                 raise MKUserError("rulenr", _("You are trying to edit a rule which does not exist "
                                               "anymore."))

9b2e58be11ef790710baa2ed0ba57a3bcb33367c
Roland Halbig <rh@mathias-kettner.de>
2016-12-07 12:43:21 +0100

    Fixed generator test.

--- tests/web/test_generator.py
+++ tests/web/test_generator.py
@@ -16,17 +16,26 @@ from htmllib import HTML, HTMLGenerator
 
 
 
-#def test_HTMLGenerator():
-#
-#    html = HTMLGenerator()
-#    html.plug()
-#
-#    html.open_div()
-#    assert html.drain() == "<div>"
-#
-#    html.open_div().write("test").close_div()
-#    assert html.drain() == "<div>test</div>"
-#
-#    html.open_table().open_tr().td("1").td("2").close_tr().close_table()
-#    assert html.drain() == "<table><tr><td>1</td><td>2</td></tr></table>"
-#
+def test_HTMLGenerator():
+
+    html = HTMLGenerator()
+    html.plug()
+
+    html.open_div()
+    assert html.drain() == "<div>"
+
+    #html.open_div().write("test").close_div()
+    html.open_div()
+    html.write("test")
+    html.close_div()
+    assert html.drain() == "<div>test</div>"
+
+    #html.open_table().open_tr().td("1").td("2").close_tr().close_table()
+    html.open_table()
+    html.open_tr()
+    html.td("1")
+    html.td("2")
+    html.close_tr()
+    html.close_table()
+    assert html.drain() == "<table><tr><td>1</td><td>2</td></tr></table>"
+

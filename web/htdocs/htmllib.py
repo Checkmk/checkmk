f0f5545cb6c845b29899eef10ef5724add5d0370
Lars Michelsen <lm@mathias-kettner.de>
2016-12-14 08:53:21 +0100

    Worked around recently introduced encoding issues in the GUI
    Change-Id: I33dcaa7235aaf196cc326f0aa4f6e6ddbeaf8ba5

--- web/htdocs/htmllib.py
+++ web/htdocs/htmllib.py
@@ -189,7 +189,23 @@ class HTML(object):
 
 
     def __str__(self):
-        return self.__bytebatzen__()
+        # Against the sense of the __str__() method, we need to return the value
+        # as unicode here. Why? There are many cases where something like
+        # "%s" % HTML(...) is done in the GUI code. This calls the __str__ function
+        # because the origin is a str() object. The call will then return a UTF-8
+        # encoded str() object. This brings a lot of compatbility issues to the code
+        # which can not be solved easily.
+        # To deal with this situation we need the implicit conversion from str to
+        # unicode that happens when we return a unicode value here. In all relevant
+        # cases this does exactly what we need. It would only fail if the origin
+        # string contained characters that can not be decoded with the ascii codec
+        # which is not relevant for us here.
+        #
+        # This is problematic:
+        #   html.write("%s" % HTML("ä"))
+        #
+        # Bottom line: We should relly cleanup internal unicode/str handling.
+        return self.value
 
 
     def __repr__(self):

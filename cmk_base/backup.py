6fb66a68e9b9da246ffe2843a2c614f0cf3048aa
Lars Michelsen <lm@mathias-kettner.de>
2016-12-28 14:02:54 +0100

    Moved backup,restore,donation modes to separate modules
    Change-Id: I446702867f32690b09946afea9d9a62f3e387fc5

---
+++ cmk_base/backup.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+""" Check_MK comes with a simple backup and restore of the current
+configuration and cache files (cmk --backup and cmk --restore). This
+is implemented here. """
+
+import os
+import shutil
+import tarfile
+import time
+import cStringIO as StringIO
+
+import cmk.paths
+import cmk.render as render
+from cmk.exceptions import MKGeneralException
+
+import cmk_base.console as console
+
+def backup_paths():
+    return [
+        # tarname               path                 canonical name   description                is_dir owned_by_nagios www_group
+        ('check_mk_configfile', cmk.paths.main_config_file,    "main.mk",       "Main configuration file",           False, False, False ),
+        ('final_mk',            cmk.paths.final_config_file,   "final.mk",      "Final configuration file final.mk", False, False, False ),
+        ('check_mk_configdir',  cmk.paths.check_mk_config_dir, "",              "Configuration sub files",           True,  False, False ),
+        ('autochecksdir',       cmk.paths.autochecks_dir,      "",              "Automatically inventorized checks", True,  False, False ),
+        ('counters_directory',  cmk.paths.counters_dir,        "",              "Performance counters",              True,  True,  False ),
+        ('tcp_cache_dir',       cmk.paths.tcp_cache_dir,       "",              "Agent cache",                       True,  True,  False ),
+        ('logwatch_dir',        cmk.paths.logwatch_dir,        "",              "Logwatch",                          True,  True,  True  ),
+    ]
+
+
+def do_backup(tarname):
+    console.verbose("Creating backup file '%s'...\n", tarname)
+    tar = tarfile.open(tarname, "w:gz")
+
+    for name, path, canonical_name, descr, is_dir, \
+        _unused_owned_by_nagios, _unused_group_www in backup_paths():
+
+        absdir = os.path.abspath(path)
+        if os.path.exists(path):
+            if is_dir:
+                basedir = absdir
+                filename = "."
+                subtarname = name + ".tar"
+                subdata = os.popen("tar cf - --dereference --force-local -C '%s' '%s'" % \
+                                   (basedir, filename)).read()
+            else:
+                basedir = os.path.dirname(absdir)
+                filename = os.path.basename(absdir)
+                subtarname = canonical_name
+                subdata = file(absdir).read()
+
+            info = tarfile.TarInfo(subtarname)
+            info.mtime = time.time()
+            info.uid = 0
+            info.gid = 0
+            info.size = len(subdata)
+            info.mode = 0644
+            info.type = tarfile.REGTYPE
+            info.name = subtarname
+            console.verbose("  Added %s (%s) with a size of %s\n", descr, absdir, render.bytes(info.size))
+            tar.addfile(info, StringIO.StringIO(subdata))
+
+    tar.close()
+    console.verbose("Successfully created backup.\n")
+
+
+def do_restore(tarname):
+    console.verbose("Restoring from '%s'...\n", tarname)
+
+    if not os.path.exists(tarname):
+        raise MKGeneralException("Unable to restore: File does not exist")
+
+    # TODO: Cleanup owned_by_nagios and group_www handling - not needed in pure OMD env anymore
+    for name, path, canonical_name, descr, is_dir, owned_by_nagios, group_www in backup_paths():
+        absdir = os.path.abspath(path)
+        if is_dir:
+            basedir = absdir
+            filename = "."
+            if os.path.exists(absdir):
+                console.verbose("  Deleting old contents of '%s'\n", absdir)
+                # The path might point to a symbalic link. So it is no option
+                # to call shutil.rmtree(). We must delete just the contents
+                for f in os.listdir(absdir):
+                    if f not in [ '.', '..' ]:
+                        try:
+                            p = absdir + "/" + f
+                            if os.path.isdir(p):
+                                shutil.rmtree(p)
+                            else:
+                                os.remove(p)
+                        except Exception, e:
+                            console.warning("  Cannot delete %s: %s", p, e)
+        else:
+            basedir = os.path.dirname(absdir)
+            filename = os.path.basename(absdir)
+            canonical_path = basedir + "/" + canonical_name
+            if os.path.exists(canonical_path):
+                console.verbose("  Deleting old version of '%s'\n", canonical_path)
+                os.remove(canonical_path)
+
+        if not os.path.exists(basedir):
+            console.verbose("  Creating directory %s\n", basedir)
+            os.makedirs(basedir)
+
+        console.verbose("  Extracting %s (%s)\n", descr, absdir)
+        if is_dir:
+            os.system("tar xzf '%s' --force-local --to-stdout '%s' 2>/dev/null "
+                      "| tar xf - -C '%s' '%s' 2>/dev/null" % \
+                      (tarname, name + ".tar", basedir, filename))
+        else:
+            os.system("tar xzf '%s' --force-local --to-stdout '%s' 2>/dev/null > '%s' 2>/dev/null" %
+                      (tarname, filename, canonical_path))
+
+    console.verbose("Successfully restored backup.\n")

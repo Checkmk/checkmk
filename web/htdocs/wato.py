61081c74754cc07bb485c5e370bfd09c22b45ae5
Lars Michelsen <lm@mathias-kettner.de>
2017-01-10 14:21:17 +0100

    4227 New manual WATO configuration lock mechanism
    It is now possible to manually lock the WATO configuration for modifications and set
    the configuration to read only mode.
    
    You can find the option to configure it below <i>Global Settings > Read only mode</i>.
    
    On that page the WATO configuration can be set to read only mode for all users that are not
    permitted to ignore the read only mode. All users that are permitted to set the read only mode
    can disable it again when another permitted user enabled it before.
    
    The read only mode can be set permanently or in a specific time window. You can set a message
    which is then shown to all users while the read only mode is active.
    
    Change-Id: I94fac7c7c23d49444016dc1ea506cbaa2a61433f

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -202,6 +202,9 @@ def page_handler():
                 if modeperms:
                     ensure_mode_permissions(modeperms)
 
+            if is_read_only_mode_enabled() and not may_override_read_only_mode():
+                raise MKUserError(None, read_only_message())
+
             result = modefunc("action")
             if type(result) == tuple:
                 newmode, action_message = result
@@ -265,6 +268,9 @@ def page_handler():
                     else:
                         html.context_button(buttontext, folder_preserving_link([("mode", target)]))
             html.end_context_buttons()
+
+        if not html.is_transaction() or may_override_read_only_mode():
+            show_read_only_warning()
 
         # Show outcome of action
         if html.has_user_errors():
@@ -6072,6 +6078,9 @@ def mode_globalvars(phase):
 
     elif phase == "buttons":
         global_buttons()
+
+        if config.user.may("wato.set_read_only"):
+            html.context_button(_("Read only mode"), folder_preserving_link([("mode", "read_only")]), "read_only")
         return
 
     # Get default settings of all configuration variables of interest in the domain
@@ -15927,6 +15936,117 @@ def scan_ip_addresses(folder, ip_addresses):
 
 
 #.
+#   .--Read-Only-----------------------------------------------------------.
+#   |           ____                _        ___        _                  |
+#   |          |  _ \ ___  __ _  __| |      / _ \ _ __ | |_   _            |
+#   |          | |_) / _ \/ _` |/ _` |_____| | | | '_ \| | | | |           |
+#   |          |  _ <  __/ (_| | (_| |_____| |_| | | | | | |_| |           |
+#   |          |_| \_\___|\__,_|\__,_|      \___/|_| |_|_|\__, |           |
+#   |                                                     |___/            |
+#   +----------------------------------------------------------------------+
+#   | WATO can be set into read only mode manually.                        |
+#   '----------------------------------------------------------------------'
+
+class ModeManageReadOnly(WatoMode):
+    def __init__(self):
+        super(ModeManageReadOnly, self).__init__()
+        self._settings = config.wato_read_only
+
+
+    def title(self):
+        return _("Manage configuration read only mode")
+
+
+    def buttons(self):
+        global_buttons()
+        html.context_button(_("Back"), folder_preserving_link([("mode", "globalvars")]), "back")
+
+
+    def action(self):
+        settings = self._vs().from_html_vars("_read_only")
+        self._vs().validate_value(settings, "_read_only")
+        self._settings = settings
+
+        self._save()
+
+
+    def _save(self):
+        store.save_to_mk_file(multisite_dir + "read_only.mk",
+                          "wato_read_only", self._settings)
+
+
+    def page(self):
+        html.p(
+            _("The WATO configuration can be set to read only mode for all users that are not "
+              "permitted to ignore the read only mode. All users that are permitted to set the "
+              "read only can disable it again when another permitted user enabled it before."))
+        html.begin_form("read_only", method="POST")
+        self._vs().render_input("_read_only", self._settings)
+        html.button('_save', _('Save'), 'submit')
+        html.hidden_fields()
+        html.end_form()
+
+
+    def _vs(self):
+        return Dictionary(
+            title = _("Read only mode"),
+            optional_keys = False,
+            render = "form",
+            elements = [
+                ("enabled",
+                    Alternative(
+                        title = _("Enabled"),
+                        style = "dropdown",
+                        elements = [
+                            FixedValue(
+                                False,
+                                title = _("Disabled "),
+                                totext = "Not enabled",
+                            ),
+                            FixedValue(
+                                True,
+                                title = _("Enabled permanently"),
+                                totext = _("Enabled until disabling"),
+                            ),
+                            Tuple(
+                                title = _("Enabled in time range"),
+                                elements = [
+                                    AbsoluteDate(
+                                        title = _("Start"),
+                                        include_time = True,
+                                    ),
+                                    AbsoluteDate(
+                                        title = _("Until"),
+                                        include_time = True,
+                                        default_value = time.time() + 3600,
+                                    ),
+                                ],
+                            ),
+                        ],
+                    ),
+                ),
+                ("rw_users", ListOf(
+                    UserSelection(),
+                    title = _("Can still edit"),
+                    help = _("Users listed here are still allowed to modify things."),
+                    movable = False,
+                    add_label = _("Add user"),
+                    default_value = [config.user.id],
+                )),
+                ("message", TextAreaUnicode(
+                    title = _("Message"),
+                    rows = 3,
+                )),
+            ]
+        )
+
+
+def show_read_only_warning():
+    if is_read_only_mode_enabled():
+        html.show_warning(read_only_message())
+
+
+#.
 #   .--Helpers-------------------------------------------------------------.
 #   |                  _   _      _                                        |
 #   |                 | | | | ___| |_ __   ___ _ __ ___                    |
@@ -16571,6 +16691,7 @@ modes = {
    "backup_restore"     : (["backups"], ModeBackupRestore),
    "passwords"          : (["passwords"], ModePasswords),
    "edit_password"      : (["passwords"], ModeEditPassword),
+   "read_only"          : (["set_read_only"], ModeManageReadOnly),
 }
 
 builtin_host_attribute_names = []
@@ -16841,6 +16962,10 @@ def load_plugins(force):
         _("Allows to download the current agent output or SNMP walks of the monitored hosts."),
         [ "admin" ])
 
+    config.declare_permission("wato.set_read_only",
+        _("Set WATO to read only mode for other users"),
+        _("Prevent other users from making modifications to WATO."),
+        [ "admin" ])
 
     load_web_plugins("wato", globals())
 

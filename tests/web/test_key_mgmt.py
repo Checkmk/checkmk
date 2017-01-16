81e0614144c62c75ba8f45000dd46aafaed06917
Lars Michelsen <lm@mathias-kettner.de>
2017-01-16 08:15:22 +0100

    Added new test for creating keys
    Change-Id: Ie76b31734c90715b2bebf77a9458e8a76fb6d22e

---
+++ tests/web/test_key_mgmt.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+import os
+import time
+
+def test_key_mgmt_create_key(monkeypatch):
+    import config
+    monkeypatch.setattr(config.user, "id", u"dingdöng")
+    monkeypatch.setitem(os.environ, "OMD_SITE", "heute")
+    monkeypatch.setattr(time, "time", lambda: 123)
+
+    import requests
+    requests.get("http://127.0.0.1/")
+
+    import requests
+    print requests.__file__
+    import key_mgmt
+    print key_mgmt.__file__
+    key_dict = key_mgmt.PageEditKey()._generate_key(u"älias", "passphra$e")
+    assert type(key_dict) == dict
+    assert sorted(key_dict.keys()) == [ "alias", "certificate", "date", "owner", "private_key" ]
+    assert type(key_dict["alias"]) == unicode
+    assert key_dict["alias"] == u"älias"
+
+    assert key_dict["date"] == 123
+
+    assert type(key_dict["owner"]) == unicode
+    assert key_dict["owner"] == u"dingdöng"
+
+    assert key_dict["certificate"].startswith("-----BEGIN CERTIFICATE---")
+    assert key_dict["private_key"].startswith("-----BEGIN ENCRYPTED PRIVATE KEY---")

eb212279590074cb3b9c168b191806ed0c8f7abe
Lars Michelsen <lm@mathias-kettner.de>
2017-02-10 11:21:15 +0100

    Tests: Added tests for setuid binaries and werk saving/loading
    Change-Id: I29796f2ea2a0ab0d89c2864e68a1a62d9240f023

---
+++ tests/omd/test_setuid_binaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import stat
+
+def test_setuid_binaries(site):
+    setuid_binaries = [
+        "bin/mkeventd_open514",
+        "lib/nagios/plugins/check_dhcp",
+        "lib/nagios/plugins/check_icmp",
+    ]
+
+    if site.version.edition() == "enterprise":
+        setuid_binaries += [
+            "lib/cmc/icmpreceiver",
+            "lib/cmc/icmpsender",
+        ]
+
+    for rel_path in setuid_binaries:
+        path = os.path.join(site.root, rel_path)
+        assert os.path.exists(path)
+        assert os.stat(path).st_mode & stat.S_ISUID == stat.S_ISUID, \
+            "Missing setuid bit on %s" % path

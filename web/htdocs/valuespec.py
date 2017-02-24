4c8a843209a617dcc222890c73dad9da7e02b4d3
Lars Michelsen <lm@mathias-kettner.de>
2017-02-24 09:53:07 +0100

    ListOf valuespec: Fixed possible exception in complain phase
    Change-Id: I7028bdcc096c74d897a7b1c196f9ec501a4c194b

--- web/htdocs/valuespec.py
+++ web/htdocs/valuespec.py
@@ -978,7 +978,7 @@ class ListOf(ValueSpec):
 
 
     def get_indexes(self, varprefix):
-        count = int(html.var(varprefix + "_count"))
+        count = int(html.var(varprefix + "_count", 0))
         n = 1
         indexes = {}
         while n <= count:

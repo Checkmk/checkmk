c8f617264965cdcc0a543a9bb0c9efac8ca86f3c
Andreas Boesl <ab@mathias-kettner.de>
2017-02-24 10:21:20 +0100

    Windows agent fileinfo monitoring: Now able to configure globbing patterns for subdirectories
    Change-Id: I6b90a3d3d7adb423fc6623600f898a57fdbe03a4

--- agents/windows/sections/SectionFileinfo.cc
+++ agents/windows/sections/SectionFileinfo.cc
@@ -24,6 +24,7 @@
 
 #include "SectionFileinfo.h"
 #include <iomanip>
+#include <sstream>
 
 extern double current_time();
 extern double file_time(const FILETIME *filetime);
@@ -47,45 +48,114 @@ bool SectionFileinfo::produceOutputInner(std::ostream &out,
     return true;
 }
 
-void SectionFileinfo::outputFileinfos(std::ostream &out, const char *path) {
-    WIN32_FIND_DATA data;
-    HANDLE h = FindFirstFileEx(path, FindExInfoStandard, &data,
-                               FindExSearchNameMatch, NULL, 0);
-    bool found_file = false;
 
-    if (h != INVALID_HANDLE_VALUE) {
-        // compute basename of path: search backwards for '\'
-        const char *basename = "";
-        char *end = strrchr(path, '\\');
-        if (end) {
-            *end = 0;
-            basename = path;
-        }
-        found_file = outputFileinfo(out, basename, &data);
-        while (FindNextFile(h, &data))
-            found_file = outputFileinfo(out, basename, &data) || found_file;
-        if (end) *end = '\\';  // repair string
-        FindClose(h);
+void SectionFileinfo::get_directories(std::string base_path)
+{
+    WIN32_FIND_DATA findData;
+    HANDLE findHandle;
 
-        if (!found_file) {
-            out << path << "|missing|" << current_time() << "\n";
-        }
-    } else {
-        DWORD e = GetLastError();
-        out << path << "|missing|" << e << "\n";
+    std::stringstream ss;
+    ss << base_path << "\\"  << "*.*";
+    if ( (findHandle=FindFirstFile(ss.str().c_str(), &findData)) != INVALID_HANDLE_VALUE )
+    {
+        do
+        {
+            if ( strcmp(findData.cFileName,".") &&
+                    strcmp(findData.cFileName,"..") ) // Skip Root (..) and current (.) dir
+            {
+                ss.str("");
+                ss.clear();
+                ss << base_path << "\\" << findData.cFileName;
+                if ( findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) // Directory found
+                    get_directories(ss.str());
+                else
+                    _temp_files.push_back(ss.str());
+            }
+        } while ( FindNextFile(findHandle, &findData) );
+        FindClose(findHandle);
     }
 }
 
-bool SectionFileinfo::outputFileinfo(std::ostream &out, const char *basename,
-                                     WIN32_FIND_DATA *data) {
-    uint64_t size = to_u64(data->nFileSizeLow, data->nFileSizeHigh);
-
-    if (0 == (data->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-        out << basename << "\\" << data->cFileName << "|" << size << "|"
-            << std::fixed << std::setprecision(0)
-            << file_time(&data->ftLastWriteTime) << "\n";
-        return true;
+void SectionFileinfo::determine_filepaths_full_search(std::string base_path, std::string search_pattern) {
+    get_directories(base_path);
+    for (auto entry : _temp_files) {
+        if (globmatch(search_pattern.c_str(), entry.c_str())) {
+            _found_files.push_back(entry);
+        }
     }
-    return false;
+    _temp_files.clear();
+}
+
+void SectionFileinfo::determine_filepaths_simple_search(std::string base_path, std::string search_pattern) {
+    WIN32_FIND_DATA data;
+    auto findHandle = FindFirstFileEx(search_pattern.c_str(), FindExInfoStandard, &data,
+                               FindExSearchNameMatch, NULL, 0);
+
+    std::stringstream ss;
+    if (findHandle != INVALID_HANDLE_VALUE) {
+        do {
+            if (true || (0 == (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))) {
+                ss.str("");
+                ss.clear();
+                ss << base_path << "\\" << data.cFileName;
+                _found_files.push_back(ss.str());
+            }
+        }  while (FindNextFile(findHandle, &data));
+        FindClose(findHandle);
+    }
+}
+
+
+
+void SectionFileinfo::determine_filepaths(std::string search_pattern) {
+    auto pos_asterisk      = search_pattern.find("*");
+    auto pos_question_mark = search_pattern.find("?");
+    auto pos_backslash     = search_pattern.find_last_of("\\");
+    auto pos_min_glob      = std::min(pos_asterisk, pos_question_mark);
+
+    auto subpattern = search_pattern.substr(0, pos_min_glob);
+    auto base_path  = subpattern.substr(0, subpattern.find_last_of("\\"));
+
+    if (pos_backslash > pos_min_glob) {
+        determine_filepaths_full_search(base_path, search_pattern);
+    }
+    else {
+        determine_filepaths_simple_search(base_path, search_pattern);
+    }
+}
+
+void SectionFileinfo::outputFileinfos(std::ostream &out, const char *path) {
+    std::string search_pattern(path);
+    _found_files.clear();
+
+    determine_filepaths(search_pattern);
+
+    bool found_file = false;
+    for (auto entry : _found_files) {
+        found_file = outputFileinfo(out, entry) || found_file;
+    }
+
+    if (!found_file) {
+        out << path << "|missing|" << current_time() << "\n";
+    }
+}
+
+bool SectionFileinfo::outputFileinfo(std::ostream &out, const std::string filename) {
+    auto hFile = std::make_unique<ManagedHandle>(CreateFile(filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL,
+            OPEN_EXISTING, 0, NULL));
+
+    FILETIME ftCreate, ftAccess, ftWrite;
+    if (!GetFileTime(hFile->get_handle(), &ftCreate, &ftAccess, &ftWrite))
+        return false;
+
+    LARGE_INTEGER size;
+    if(!GetFileSizeEx(hFile->get_handle(), &size))
+        return false;
+
+    out << filename.c_str() << "|" << size.QuadPart << "|"
+        << std::fixed << std::setprecision(0)
+        << file_time(&ftWrite) << "\n";
+
+    return true;
 }
 

c85f9bd92eb30967829391e82ac504fdf3c0b345
Simon Betz <si@mathias-kettner.de>
2017-03-13 08:00:45 +0100

    4493 FIX mk_oracle.ps1: Now takes version into account which fixed wrong calculation of tablespaces
    Change-Id: I31478990fe997618ce7b61a88c7ea961c4172006

--- agents/windows/plugins/mk_oracle.ps1
+++ agents/windows/plugins/mk_oracle.ps1
@@ -613,10 +613,12 @@ Function sql_tablespaces {
                   ||'|'|| ONLINE_STATUS ||'|'|| BLOCK_SIZE
                   ||'|'|| decode(tstatus,'READ ONLY', 'READONLY', tstatus) || '|' || free_blocks
                   ||'|'|| contents
+                  ||'|'|| iversion
            from v$database d , (
                     select f.file_name, f.tablespace_name, f.status fstatus, f.AUTOEXTENSIBLE,
                     f.blocks, f.maxblocks, f.USER_BLOCKS, f.INCREMENT_BY,
-                    f.ONLINE_STATUS, t.BLOCK_SIZE, t.status tstatus, nvl(sum(fs.blocks),0) free_blocks, t.contents
+                    f.ONLINE_STATUS, t.BLOCK_SIZE, t.status tstatus, nvl(sum(fs.blocks),0) free_blocks, t.contents,
+                    (select version from v$instance) iversion
                     from dba_data_files f, dba_tablespaces t, dba_free_space fs
                     where f.tablespace_name = t.tablespace_name
                     and f.file_id = fs.file_id(+)
@@ -626,7 +628,8 @@ Function sql_tablespaces {
                     UNION
                     select f.file_name, f.tablespace_name, f.status, f.AUTOEXTENSIBLE,
                     f.blocks, f.maxblocks, f.USER_BLOCKS, f.INCREMENT_BY, 'TEMP',
-                    t.BLOCK_SIZE, t.status, sum(sh.blocks_free) free_blocks, 'TEMPORARY'
+                    t.BLOCK_SIZE, t.status, sum(sh.blocks_free) free_blocks, 'TEMPORARY',
+                    (select version from v$instance) version
                     from v$thread th, dba_temp_files f, dba_tablespaces t, v$temp_space_header sh
                     WHERE f.tablespace_name = t.tablespace_name and f.file_id = sh.file_id
                     GROUP BY th.instance, f.file_name, f.tablespace_name, f.status,

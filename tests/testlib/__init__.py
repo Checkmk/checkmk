f003c17bde280dadae7a13c5f490ba4d6063b162
Lars Michelsen <lm@mathias-kettner.de>
2017-04-11 11:06:38 +0200

    Python path replace was done too late
    Change-Id: I67786a8ed3eadd0b599ac7d61a4605faadc04baa

--- tests/testlib/__init__.py
+++ tests/testlib/__init__.py
@@ -343,8 +343,8 @@ class Site(object):
             (self.root, self.version.version_path())) >> 8 != 0:
             raise Exception("Failed to set new version to bin/python")
 
-        if os.system("echo 'sys.path.insert(0, \"%s/lib/python\")' "
-                     "| sudo tee -a %s/share/check_mk/web/htdocs/index.py >/dev/null" %
+        if os.system("sudo sed -i \"s|^initialize()$|sys.path.insert(0, '%s/lib/python')\ninitialize()|g\" "
+                     "%s/share/check_mk/web/htdocs/index.py" %
             (self.version.version_path(), self.version.version_path())) >> 8 != 0:
             raise Exception("Failed to add python module path of fake version")
 

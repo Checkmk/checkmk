822522b53f6fd1d5ad9cc3440b273d50db26e203
Lars Michelsen <lm@mathias-kettner.de>
2017-04-12 20:51:53 +0200

    Finally fixed GUI crawl test after starting with CME
    Change-Id: Id431395b5bf1ad6ef298ddc078f1e6c9cf27d515

--- tests/testlib/__init__.py
+++ tests/testlib/__init__.py
@@ -345,7 +345,11 @@ class Site(object):
         execute("sudo chrpath -r %s/lib %s/bin/python" %
             (self.root, self.version.version_path()))
 
-        execute("sudo sed -i 's|^initialize()$|sys.path.insert(0, \"%s/lib/python\")\\ninitialize()|g' " \
+        self._add_version_path_to_index_py()
+
+
+    def _add_version_path_to_index_py(self):
+        os.system("sudo sed -i '0,/^$/s|^$|import sys ; sys.path.insert(0, \"%s/lib/python\")\\n|' " \
               "%s/share/check_mk/web/htdocs/index.py" % (self.version.version_path(), self.version.version_path()))
 
 
@@ -398,6 +402,8 @@ class Site(object):
                                       (path, self.id)) >> 8 == 0
                 print("Executing .f12 in \"%s\" DONE" % path)
                 sys.stdout.flush()
+
+        self._add_version_path_to_index_py()
 
 
     def _enable_mod_python_debug(self):
@@ -741,7 +747,7 @@ class WebSession(requests.Session):
         if allow_errors and matches:
             print "Found error message, but it's allowed: %s" % matches.groups()
         else:
-            assert not matches, "Found error message(s): %s" % "\n".join(matches.groups())
+            assert not matches, "Found error message: %s" % matches.groups()
 
 
     def _check_html_page_resources(self, response):

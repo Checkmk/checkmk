4ac28dfaae043a4f15ae422417f15dadf38ba26a
Sven Panne <sp@mathias-kettner.de>
2017-05-08 14:34:13 +0200

    Nuked yet another global variable.
    Change-Id: Iaad26b8b4c94313afe2da6cbe4c022715945b0e4

--- livestatus/src/MonitoringCore.h
+++ livestatus/src/MonitoringCore.h
@@ -90,6 +90,7 @@ public:
     virtual Encoding dataEncoding() = 0;
     virtual size_t maxResponseSize() = 0;
     virtual size_t maxCachedMessages() = 0;
+    virtual bool stateHistoryFilteringEnabled() = 0;
 
     virtual Logger *loggerLivestatus() = 0;
 

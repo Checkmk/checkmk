d904734a47dec760abbe91fb7062ddb47bb9305a
Simon Betz <si@mathias-kettner.de>
2017-05-10 09:24:17 +0200

    4648 FIX mk_oracle: Fixed logswitches SQL statement for ORACLE RAC case
    Change-Id: I76a8ddbcf10d5ab2c06d6b19e4c55bb9ef38ac88

--- agents/windows/plugins/mk_oracle.ps1
+++ agents/windows/plugins/mk_oracle.ps1
@@ -1098,9 +1098,9 @@ select upper(i.instance_name)
                   || '|' || logswitches
            from v$instance i ,
                 (select count(1) logswitches
-                 from v$loghist
-                 where first_time > sysdate - 1/24
-                )
+                 from v\$loghist h , v\$instance i
+                 where h.first_time > sysdate - 1/24
+                 and h.thread# = i.instance_number)
                 ;
 
 '@

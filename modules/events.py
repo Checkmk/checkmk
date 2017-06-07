e946ee5a9fca6009e47506baa3fef3740390eef0
Andreas Boesl <ab@mathias-kettner.de>
2017-06-07 17:21:52 +0200

    4709 FIX Notifications: Missing SERVICECONTACTGROUPNAMES/HOSTCONTACTGROUPNAMES parameters no longer causes a notification not to be sent
    This werk reverts part of WK3886. The nagios core did not set the environment parameters mentioned in the topic.
    As a result the notifcation condition <i>Match Contact Groups</i> did never match and a notification was not sent.
    This has been changed so that this condition check is skipped, whenever this information is missing.
    
    An error scenario should always let the message through.
    
    The issue, that the nagios core does not set this environment info will be fixed in a future werk.
    
    Change-Id: If330de216270299a54f406a913e654526d540381

--- modules/events.py
+++ modules/events.py
@@ -517,8 +517,9 @@ def event_match_contactgroups(rule, context):
 
     if required_groups != None:
         if cgn == None:
-            return "No information about contact groups is in the context, but " \
-                   "must be in group %s" % (" or ".join(required_groups))
+            event_log("Warning: No information about contact groups in the context. " \
+                       "Seems that you don't use the Check_MK Microcore. ")
+            return
 
         if cgn:
             contactgroups = cgn.split(",")

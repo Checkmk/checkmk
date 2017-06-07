81658b523020711e39a21852a3057303e8272f21
Sven Rueß <sr@mathias-kettner.de>
2017-06-07 19:42:09 +0200

    Revert "4796 FIX Fixed standard setting for rule based notification in new sites"
    This reverts commit eea6a5e7df847c4ea298c07dd66bf754ebd10676.
    
    Change-Id: I0580fd8f6c285c5ec8def41e23eee2cfacd37012

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -14386,6 +14386,7 @@ def create_sample_config():
                 "barracuda_mailqueues",
                 "qmail_stats",
             ],
+            "enable_rulebased_notifications": True,
         }
     )
 

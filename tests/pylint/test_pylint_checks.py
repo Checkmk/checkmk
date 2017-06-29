5b18f79abe0b30f5fa8bb30799637d3af202a9de
Lars Michelsen <lm@mathias-kettner.de>
2017-06-29 10:43:32 +0200

    Always cleanup pylint test directories
    Change-Id: Idac21f872e598ccf22a85488c68e3c621a5f7647

--- tests/pylint/test_pylint_checks.py
+++ tests/pylint/test_pylint_checks.py
@@ -11,10 +11,8 @@ import testlib.pylint_cmk as pylint_cmk
 # Mark all tests in this file to be pylint checks
 pytestmark = pytest.mark.pylint
 
-def test_pylint_checks():
-    base_path = pylint_cmk.get_test_dir()
-
-    f = file(base_path + "/cmk-checks.py", "w")
+def test_pylint_checks(pylint_test_dir):
+    f = file(pylint_test_dir + "/cmk-checks.py", "w")
 
     # add the modules
     for path in pylint_cmk.ordered_module_files():
@@ -34,6 +32,6 @@ def test_pylint_checks():
 
     f.close()
 
-    exit_code = pylint_cmk.run_pylint(base_path, cleanup_test_dir=True)
+    exit_code = pylint_cmk.run_pylint(pylint_test_dir)
     assert exit_code == 0, "PyLint found an error in checks, inventory " \
                            "or agent bakery plugins"

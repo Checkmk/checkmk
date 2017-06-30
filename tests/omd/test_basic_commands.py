8ade5cc78f94e46425354f3d9177bff5442ae57d
Lars Michelsen <lm@mathias-kettner.de>
2017-06-30 14:03:57 +0200

    Cleaned up packaging of some commands
    Change-Id: Ia8a8148692b2dc433d60e60eba14c720d77f7081

---
+++ tests/omd/test_basic_commands.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import stat
+
+def test_basic_commands(site):
+    commands = [
+        "bin/mkp",
+        "bin/check_mk",
+        "bin/cmk",
+        "bin/omd",
+    ]
+
+    for rel_path in commands:
+        path = os.path.join(site.root, rel_path)
+        assert os.path.exists(path)

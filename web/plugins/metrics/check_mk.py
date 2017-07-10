8b5115e7194b16a59195445d40634b1714e3a8a5
Marcel Arentz <ma@mathias-kettner.de>
2017-07-10 15:26:55 +0200

    4915 solaris_mem: Unified graphs and Per-O-Meter with common memory checks
    The graphs have been different between common memory checks and services
    provided by solaris_mem. This has been changed now. A side effect is the
    Per-O-Meter which is now also available.
    
    Change-Id: I79dc6a76cc4afc527e70e134a3bce47a50c50507

--- web/plugins/metrics/check_mk.py
+++ web/plugins/metrics/check_mk.py
@@ -3640,6 +3640,7 @@ ram_used_swap_translation = {
 
 check_metrics["check_mk-statgrab_mem"] = ram_used_swap_translation
 check_metrics["check_mk-hr_mem"] = ram_used_swap_translation
+check_metrics["check_mk-solaris_mem"] = ram_used_swap_translation
 
 check_metrics["check_mk-mem.used"] = {
     "ramused"       : { "name" : "mem_used",  "scale" : MB },

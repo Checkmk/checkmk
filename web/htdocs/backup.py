2f9b90c8ba11dda98a5f4d1e291151d2578c8639
Lars Michelsen <lm@mathias-kettner.de>
2017-07-14 12:43:22 +0200

    Fixed issue in recent change
    Change-Id: Ie827b674a45360ea7e558896a99c0a7a61bba00b

--- web/htdocs/backup.py
+++ web/htdocs/backup.py
@@ -1351,7 +1351,8 @@ class BackupTargetLocal(BackupTargetType):
 
         # Check write access for the site user
         try:
-            file(os.path.join(value, "write_test_%d" % time.time()), "w")
+            test_file_path = os.path.join(value, "write_test_%d" % time.time())
+            file(test_file_path, "w")
             os.unlink(test_file_path)
         except IOError, e:
             if is_cma():

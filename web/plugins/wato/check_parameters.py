11d055411553a8b84e8e26a3dad5d310dcd33be7
Tom Baerwinkel <tb@mathias-kettner.de>
2017-07-17 14:02:22 +0200

    4811 mssql_backup: Set limits for different type of backups
    For the check mssql_backup it is now possible to set limits for
    different types of backups (e.g. database backup, log backup etc.)
    instead of just one limit that is valid for all backups. The
    limits can be set in the rule "MSSQL Time since last Backup".
    If no backup types are supported by the agent the parameter
    "Database Backup" can be used to set a general limit.
    
    Change-Id: Iea7ff7a86d03e6fd5a043c9bdb6f120053febed8

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -9168,23 +9168,99 @@ register_check_parameters(
     "dict"
 )
 
+#register_check_parameters(
+#    subgroup_applications,
+#    "mssql_backup",
+#    _("MSSQL Time since last Backup"),
+#    Optional(
+#        Tuple(
+#            elements = [
+#              Age(title = _("Warning if older than")),
+#              Age(title = _("Critical if older than"))
+#            ]
+#        ),
+#        title = _("Specify time since last successful backup"),
+#    ),
+#    TextAscii(
+#        title = _("Service descriptions"),
+#        allow_empty = False),
+#    match_type = "first",
+#)
+#Old rule:
+#checkgroup_parameters['mssql_backup'] = [
+#  ( (8640000, 33696000), [], ALL_HOSTS, ALL_SERVICES, {} ),
+#] = checkgroup_parameters['mssql_backup']
+
 register_check_parameters(
     subgroup_applications,
     "mssql_backup",
     _("MSSQL Time since last Backup"),
-    Optional(
-        Tuple(
+    Transform(
+        Dictionary(
+            help = _("This rule allows you to set limits on the age of backups for "
+                     "different backup types. If your agent does not support "
+                     "backup types (e.g. <i>Log Backup</i>, <i>Database Diff "
+                     "Backup</i>, etc.) you can use the option <i>Database Backup"
+                     "</i> to set a general limit"),
             elements = [
-              Age(title = _("Warning if older than")),
-              Age(title = _("Critical if older than"))
+                ("database", Tuple(
+                    title = _("Database Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("database_diff", Tuple(
+                    title = _("Database Diff Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("log", Tuple(
+                    title = _("Log Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("file_or_filegroup", Tuple(
+                    title = _("File or Filegroup Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("file_diff", Tuple(
+                    title = _("File Diff Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("partial", Tuple(
+                    title = _("Partial Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("partial_diff", Tuple(
+                    title = _("Partial Diff Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+                ("unspecific", Tuple(
+                    title = _("Unspecific Backup"),
+                    elements = [
+                        Age(title = _("Warning if older than")),
+                        Age(title = _("Critical if older than")),
+                ])),
+
             ]
         ),
-        title = _("Specify time since last successful backup"),
+        forth = lambda params: (params if type(params) == dict
+                                else {'database': (params[0], params[1])})
     ),
     TextAscii(
         title = _("Service descriptions"),
         allow_empty = False),
-    match_type = "first",
+    "dict",
 )
 
 register_check_parameters(

6b9945fb7372e018dd769d35d029045a3be5b1de
Sven Panne <sp@mathias-kettner.de>
2017-07-20 13:21:53 +0200

    5021 FIX Make sure that the output of the event console active check is valid
    It was possible that part of the check output looked like (incorrectly
    formatted) performance data, confusing other parts of Check_MK, e.g.  the
    graphing subsystem. This has been fixed.
    
    Change-Id: I5d1798891919c6cc1be440255d865317978f8663

--- active_checks/check_mkevents.cc
+++ active_checks/check_mkevents.cc
@@ -30,24 +30,71 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <cerrno>
-#include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
 #include <memory>
-#include <sstream>
+#include <sstream>  // IWYU pragma: keep
 #include <string>
 #include <vector>
 
+using std::cout;
+using std::endl;
+using std::ostream;
 using std::string;
 using std::stringstream;
+using std::to_string;
 using std::vector;
 
+enum class State { ok = 0, warn = 1, crit = 2, unknown = 3 };
+
+ostream &operator<<(ostream &os, const State &state) {
+    switch (state) {
+        case State::ok:
+            return os << "OK";
+        case State::warn:
+            return os << "WARN";
+        case State::crit:
+            return os << "CRIT";
+        case State::unknown:
+            return os << "UNKNOWN";
+    }
+    return os;  // make compilers happy
+}
+
+[[noreturn]] void reply(State state, const string &output) {
+    cout << state << " - ";
+    // Make sure that plugin output does not contain a vertical bar. If that is
+    // the case then replace it with a Uniocode "Light vertical bar". Same as in
+    // Check_MK.
+    for (char i : output) {
+        if (i == '|') {
+            // \u2758 (utf-8 encoded light vertical bar)
+            cout << "\xe2\x94\x82";  // NOLINT
+        } else {
+            cout << i;
+        }
+    }
+    cout << endl;
+    exit(static_cast<int>(state));
+}
+
+[[noreturn]] void ioError(const string &message) {
+    reply(State::unknown, message + " (" + strerror(errno) + ")");
+}
+
+[[noreturn]] void tcpError(const string &message, const string &addr,
+                           int port) {
+    ioError(message + " to event daemon via TCP " + addr + ":" +
+            to_string(port));
+}
+
 void usage() {
-    printf(
-        "Usage: check_mkevents [-s SOCKETPATH] [-H REMOTE:PORT] [-a] HOST [APPLICATION]");
-    printf("\n -a    do not take into account acknowledged events.\n");
-    printf(
-        " HOST  may be a hostname, and IP address or hostname/IP-address.\n");
+    reply(
+        State::unknown,
+        "Usage: check_mkevents [-s SOCKETPATH] [-H REMOTE:PORT] [-a] HOST [APPLICATION]\n"
+        " -a    do not take into account acknowledged events.\n"
+        " HOST  may be a hostname, and IP address or hostname/IP-address.");
 }
 
 string prepare_hostname_regex(const char *s) {
@@ -75,8 +122,7 @@ int main(int argc, char **argv) {
     int remote_port = 6558;
     char *application = nullptr;
     bool ignore_acknowledged = false;
-    char unixsocket_path[1024];
-    unixsocket_path[0] = 0;
+    string unixsocket_path;
 
     int argc_count = argc;
     for (int i = 1; i < argc; i++) {
@@ -85,7 +131,7 @@ int main(int argc, char **argv) {
             i++;
             argc_count -= 2;
         } else if (strcmp("-s", argv[i]) == 0 && i < argc + 1) {
-            strncpy(unixsocket_path, argv[i + 1], sizeof(unixsocket_path));
+            unixsocket_path = argv[i + 1];
             i++;
             argc_count -= 2;
         } else if (strcmp("-a", argv[i]) == 0) {
@@ -103,20 +149,16 @@ int main(int argc, char **argv) {
 
     if (host == nullptr) {
         usage();
-        exit(3);
     }
 
     // Get omd environment
-    if (unixsocket_path[0] == 0 && remote_host == nullptr) {
+    if (unixsocket_path.empty() && remote_host == nullptr) {
         char *omd_path = getenv("OMD_ROOT");
-        if (omd_path != nullptr) {
-            snprintf(unixsocket_path, sizeof(unixsocket_path),
-                     "%s/tmp/run/mkeventd/status", omd_path);
-        } else {
-            printf(
-                "UNKNOWN - OMD_ROOT is not set, no socket path is defined.\n");
-            exit(3);
+        if (omd_path == nullptr) {
+            reply(State::unknown,
+                  "OMD_ROOT is not set, no socket path is defined.");
         }
+        unixsocket_path = string(omd_path) + "/tmp/run/mkeventd/status";
     }
 
     if (remote_host != nullptr) {
@@ -124,9 +166,9 @@ int main(int argc, char **argv) {
         struct in_addr **addr_list;
         char *remote_hostaddress = strtok(remote_host, ":");
         if ((he = gethostbyname(remote_hostaddress)) == nullptr) {
-            printf("UNKNOWN - Unable to resolve remote host address: %s\n",
-                   remote_hostaddress);
-            return 3;
+            reply(State::unknown,
+                  "Unable to resolve remote host address: " +
+                      string(remote_hostaddress));
         }
         addr_list = reinterpret_cast<struct in_addr **>(he->h_addr_list);
         for (int i = 0; addr_list[i] != nullptr; i++) {
@@ -159,17 +201,12 @@ int main(int argc, char **argv) {
 
         if (0 > connect(sock, reinterpret_cast<struct sockaddr *>(&addr),
                         sizeof(struct sockaddr_in))) {
-            printf(
-                "UNKNOWN - Cannot connect to event daemon via TCP %s:%d (%s)\n",
-                remote_hostipaddress, remote_port, strerror(errno));
-            exit(3);
+            tcpError("Cannot connect", remote_hostipaddress, remote_port);
         }
     } else {
         sock = socket(PF_UNIX, SOCK_STREAM, 0);
         if (sock < 0) {
-            printf("UNKNOWN - Cannot create client socket: %s\n",
-                   strerror(errno));
-            exit(3);
+            ioError("Cannot create client socket");
         }
 
         tv.tv_sec = 3;
@@ -178,14 +215,12 @@ int main(int argc, char **argv) {
         struct sockaddr_un addr;
         memset(&addr, 0, sizeof(struct sockaddr_un));
         addr.sun_family = AF_UNIX;
-        strncpy(addr.sun_path, unixsocket_path, sizeof(addr.sun_path));
+        strncpy(addr.sun_path, unixsocket_path.c_str(), sizeof(addr.sun_path));
 
         if (0 > connect(sock, reinterpret_cast<struct sockaddr *>(&addr),
                         sizeof(struct sockaddr_un))) {
-            printf(
-                "UNKNOWN - Cannot connect to event daemon via UNIX socket %s (%s)\n",
-                unixsocket_path, strerror(errno));
-            exit(3);
+            ioError("Cannot connect to event daemon via UNIX socket " +
+                    unixsocket_path);
         }
     }
 
@@ -216,19 +251,15 @@ int main(int argc, char **argv) {
         while (bytes_to_write > 0) {
             ssize_t bytes_written = write(sock, buffer, bytes_to_write);
             if (bytes_written == -1) {
-                printf(
-                    "UNKNOWN - Cannot send query to event daemon via TCP %s:%d (%s)\n",
-                    remote_hostipaddress, remote_port, strerror(errno));
-                exit(3);
+                tcpError("Cannot send query", remote_hostipaddress,
+                         remote_port);
             }
             buffer += bytes_written;
             bytes_to_write -= bytes_written;
         }
         if (shutdown(sock, SHUT_WR) == -1) {
-            printf(
-                "UNKNOWN - Cannot shutdown socket to event daemon via TCP %s:%d (%s)\n",
-                remote_hostipaddress, remote_port, strerror(errno));
-            exit(3);
+            tcpError("Cannot shutdown socket", remote_hostipaddress,
+                     remote_port);
         }
     }
 
@@ -279,10 +310,9 @@ int main(int argc, char **argv) {
     // Basic header validation
     if (idx_event_phase == -1 || idx_event_state == -1 ||
         idx_event_text == -1) {
-        printf(
-            "UNKNOWN - Invalid answer from event daemon\n%s\nQuery was:\n%s\n",
-            response_stream.str().c_str(), query_message.c_str());
-        exit(3);
+        reply(State::unknown,
+              "Invalid answer from event daemon\n" + response_stream.str() +
+                  "\nQuery was:\n" + query_message);
     }
 
     // Get data
@@ -307,7 +337,7 @@ int main(int argc, char **argv) {
 
     // Generate output
     string worst_row_event_text;
-    int worst_state = 0;
+    State worst_state = State::ok;
     int count = 0;
     int unhandled = 0;
 
@@ -315,10 +345,10 @@ int main(int argc, char **argv) {
         count++;
         const char *p = it.at(idx_event_phase).c_str();
         if (strcmp(p, "open") == 0 || !ignore_acknowledged) {
-            int s = atoi(it.at(idx_event_state).c_str());
-            if (s == 3) {
-                if (worst_state < 2) {
-                    worst_state = 3;
+            auto s = static_cast<State>(atoi(it.at(idx_event_state).c_str()));
+            if (s == State::unknown) {
+                if (worst_state < State::crit) {
+                    worst_state = State::unknown;
                     worst_row_event_text = it.at(idx_event_text);
                 }
             } else if (s >= worst_state) {
@@ -331,37 +361,18 @@ int main(int argc, char **argv) {
         }
     }
 
-    // make sure that plugin output does not contain a vertical bar. If that is
-    // the case then replace it with a Uniocode "Light vertical bar". Same as in
-    // Check_MK.
-    string text;
-    text.reserve(worst_row_event_text.size());
-    for (char i : worst_row_event_text) {
-        if (i == '|') {
-            // \u2758 (utf-8 encoded light vertical bar)
-            text += "\xe2\x94\x82";  // NOLINT
-        } else {
-            text += i;
-        }
+    if (count == 0) {
+        string app =
+            application == nullptr ? "" : (string(application) + " on ");
+        reply(State::ok, "no events for " + app + host);
     }
 
-    if (count == 0 && application != nullptr) {
-        printf("OK - no events for %s on host %s\n", application, host);
-    } else if (count == 0) {
-        printf("OK - no events for %s\n", host);
-    } else {
-        const char *state_text =
-            worst_state == 0
-                ? "OK"
-                : worst_state == 1 ? "WARN"
-                                   : worst_state == 2 ? "CRIT" : "UNKNOWN";
-        printf("%s - %d events (%d unacknowledged)", state_text, count,
-               unhandled);
-        if (text.length() > 0) {
-            printf(", worst state is %s (Last line: %s)", state_text,
-                   text.c_str());
-        }
-        printf("\n");
+    stringstream output;
+    output << count << " events (" << unhandled << " unacknowledged)";
+    if (!worst_row_event_text.empty()) {
+        output << ", worst state is " << worst_state
+               << " (Last line: " << worst_row_event_text << ")";
     }
-    return worst_state;
+    reply(worst_state, output.str());
+    return 0;  // never reached
 }

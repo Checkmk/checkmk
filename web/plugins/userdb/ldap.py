0156a78a8b364f0671119012a836b3619691d4a9
Lars Michelsen <lm@mathias-kettner.de>
2017-08-29 16:34:59 +0200

    Improved recent implicit connect/disconnect fix
    Change-Id: I3b2c0b963d261f310761f0a65c55f00d873482f9

--- web/plugins/userdb/ldap.py
+++ web/plugins/userdb/ldap.py
@@ -567,7 +567,7 @@ class LDAPUserConnector(UserConnector):
         # In some environments, the connection to the LDAP server does not seem to
         # be as stable as it is needed. So we try to repeat the query for three times.
         # -> Don't retry when implicit connect is disabled
-        tries_left = 2 if implicit_connect else 0
+        tries_left = 2
         success = False
         last_exc = None
         while not success:
@@ -601,7 +601,7 @@ class LDAPUserConnector(UserConnector):
                                             'within the ldap or adapt the limit settings of the LDAP server.'))
             except (ldap.SERVER_DOWN, ldap.TIMEOUT, MKLDAPException), e:
                 last_exc = e
-                if tries_left:
+                if implicit_connect and tries_left:
                     self.log('  Received %r. Retrying with clean connection...' % e)
                     self.disconnect()
                     time.sleep(0.5)

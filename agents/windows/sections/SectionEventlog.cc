b8c468c5f51064d23546e800484cd59dc4eb1173
Jukka Aro <ja@mathias-kettner.de>
2017-09-21 13:01:24 +0200

    5034 FIX Do not skip first entry in Windows eventlog
    The eventlog reference for the first record to be read was wrongly
    advanced one past the previously read entry already when seeking
    for new entries. This caused the reference to point past the actual
    end of eventlog and skip the first message that arrived after that.
    The correct behaviour is to only advance the reference if there really
    are new entries in the eventlog and not only when trying to seek one
    past the previous end.
    
    Change-Id: Ie6ce104b6484cdc1bd1bce56a3f158a7c39eb4f3

--- agents/windows/sections/SectionEventlog.cc
+++ agents/windows/sections/SectionEventlog.cc
@@ -168,7 +168,7 @@ void SectionEventlog::outputEventlog(std::ostream &out, LPCWSTR logname,
             // record_number is the last event we read, so we want to seek past
             // it
             bool record_maxxed = std::numeric_limits<uint64_t>::max() == first_record;
-            first_record = log->seek(first_record + (record_maxxed ? 0 : 1));
+            log->seek(first_record + (record_maxxed ? 0 : 1));
 
             uint64_t last_record = first_record;
 

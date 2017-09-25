1e609497ed72ad91b95055356f18d5bc962ede7d
Simon Betz <si@mathias-kettner.de>
2017-09-25 14:26:07 +0200

    5290 salesforce_instances: Monitors the instances of Salesforce
    Change-Id: I671562ecc9b0ad4b74d206f06dbad342adfac862

--- agents/special/lib/cmk_special_agent_api.py
+++ agents/special/lib/cmk_special_agent_api.py
@@ -24,4 +24,73 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
+
+import sys, requests, getopt
+
+
 """Place for common code shared among different Check_MK special agents"""
+
+
+class AgentJSON(object):
+    def __init__(self, key, title):
+        self._key = key
+        self._title = title
+
+
+    def usage(self):
+        sys.stderr.write("""
+Check_MK %s Agent
+
+USAGE: agent_%s --section_url [{section_name},{url}]
+
+    Parameters:
+        --section_url   Pair of section_name and url
+                        Can be defined multiple times
+        --debug         Output json data with pprint
+
+""" % (self._title, self._key))
+
+
+    def get_content(self):
+        short_options = "h"
+        long_options  = ["section_url=", "help", "newline_replacement=", "debug"]
+
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], short_options, long_options)
+        except getopt.GetoptError, err:
+            sys.stderr.write("%s\n" % err)
+            sys.exit(1)
+
+        sections = []
+        newline_replacement = "\\n"
+        opt_debug = False
+
+        for o,a in opts:
+            if o in [ "--section_url" ]:
+                sections.append(a.split(",", 1))
+            elif o in [ "--newline_replacement" ]:
+                newline_replacement = a
+            elif o in [ "--debug" ]:
+                opt_debug = True
+            elif o in [ '-h', '--help' ]:
+                self.usage()
+                sys.exit(0)
+
+        if not sections:
+            self.usage()
+            sys.exit(0)
+
+        content = {}
+        for section_name, url in sections:
+            content.setdefault(section_name, [])
+            content[section_name].append(requests.get(url).text.replace("\n", newline_replacement))
+
+        if opt_debug:
+            import pprint, json
+            for line in content:
+                try:
+                    pprint.pprint(json.loads(line))
+                except:
+                    print line
+        else:
+            return content

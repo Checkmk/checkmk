Checkmk/checkmk

commit 83f7e69b20b6edb0c66efbd106fab3634cc15411

5034 FIX Do not skip first entry in Windows eventlog
Change-Id: I27ad8b00fc69460c2d66e84720e0f343c0153c5e

agents/windows/build_version
@@ -1,1 +1,1 @@
-2930
+2932

agents/windows/sections/SectionEventlog.cc
@@ -178,7 +178,7 @@ void SectionEventlog::outputEventlog(std::ostream &out, const char *logname,
             // it
             bool record_maxxed =
                 std::numeric_limits<uint64_t>::max() == first_record;
-            first_record = log->seek(first_record + (record_maxxed ? 0 : 1));
+            log->seek(first_record + (record_maxxed ? 0 : 1));
 
             uint64_t last_record = first_record;
 

.werks/5034
@@ -1,0 +1,11 @@
+Title: Do not skip first entry in Windows eventlog
+Level: 1
+Component: checks
+Compatible: compat
+Edition: cre
+Version: 1.5.0i1
+Date: 1506332777
+Class: fix
+Under certain circumstances, the first entry in Windows eventlog was skipped after starting the Windows agent. Now the first entry is always processed as expected.
+
+

commit c1c98cd00f3bbd7b9bdfd464bf84e62a1ed4d11d

5409 FIX Windows eventlog: wrong last state saved
The initial run of Windows agent after fresh installation caused an
incorrect last state to be written in the state file eventstate.txt.
As a result, no new entries were read from the eventlog. Broken since
commit 83f7e69b20b6edb0c66efbd106fab3634cc15411.

Change-Id: I47378f667b7ecf8c481f7ed9c7105623b4051a2b

.werks/5409
@@ -1,0 +1,16 @@
+Title: Windows eventlog: wrong last state saved
+Level: 1
+Component: checks
+Class: fix
+Compatible: compat
+Edition: cre
+State: unknown
+Version: 1.5.0i1
+Date: 1509376514
+
+The initial run of Windows agent after fresh installation caused an
+incorrect last state to be written in the state file eventstate.txt.
+As a result, no new entries were read from the eventlog. Broken since
+commit 91797ac9f7d69fc94119c08ea0df4baccaea2b6.
+
+

agents/windows/EventLog.cc
@@ -297,23 +297,23 @@ void EventLog::reset() {
 
 wstring EventLog::getName() const { return _name; }
 
-uint64_t EventLog::seek(uint64_t record_number) {
+void EventLog::seek(uint64_t record_number) {
     DWORD oldest_record, record_count;
 
     if (_log.GetOldestEventLogRecord(&oldest_record) &&
         (record_number < oldest_record)) {
-        // can't seek to older record
+        // Beyond the oldest record:
         _record_offset = oldest_record;
     } else if (_log.GetNumberOfEventLogRecords(&record_count) &&
-               (record_number > oldest_record + record_count)) {
+               (record_number >= oldest_record + record_count)) {
+        // Beyond the newest record. Note: set offset intentionally to the next
+        // record after the currently last one!
         _record_offset = oldest_record + record_count;
     } else {
-        // not actually seeking but storing for the next actual read
+        // Within bounds, the offset for the next actual read:
         _record_offset = record_number;
     }
     _buffer_offset = _buffer_used;  // enforce that a new chunk is fetched
-
-    return _record_offset;
 }
 
 std::unique_ptr<IEventLogRecord> EventLog::read() {
@@ -348,6 +348,18 @@ std::unique_ptr<IEventLogRecord> EventLog::read() {
         return std::make_unique<EventLogRecord>(result, _resolver);
     } else {
         return std::unique_ptr<IEventLogRecord>();
+    }
+}
+
+uint64_t EventLog::getLastRecordId() {
+    DWORD oldestRecord = 0;
+    DWORD recordCount = 0;
+    if (_log.GetOldestEventLogRecord(&oldestRecord) &&
+        _log.GetNumberOfEventLogRecords(&recordCount) &&
+        oldestRecord + recordCount > 0) {
+        return oldestRecord + recordCount - 1;
+    } else {
+        return 0;
     }
 }
 

agents/windows/EventLog.h
@@ -149,7 +149,7 @@ public:
      * In this case this function will still work as expected but the next read
      * will be slow.
      */
-    virtual uint64_t seek(uint64_t record_id) override;
+    virtual void seek(uint64_t record_id) override;
 
     /**
      * read the next eventlog record
@@ -158,6 +158,11 @@ public:
      * longer
      */
     virtual std::unique_ptr<IEventLogRecord> read() override;
+
+    /**
+     * return the ID of the last record in eventlog
+     */
+    virtual uint64_t getLastRecordId() override;
 
     /**
      * get a list of dlls that contain eventid->message mappings for this

agents/windows/EventLogVista.cc
@@ -338,7 +338,7 @@ std::wstring EventLogVista::renderBookmark(EVT_HANDLE bookmark) const {
     return buffer;
 }
 
-uint64_t EventLogVista::seek(uint64_t record_id) {
+void EventLogVista::seek(uint64_t record_id) {
     {
         // The api to retrieve the oldest event log id is bugged. bloody hell...
         // to get the right offset if record_id is beyond the valid range, we
@@ -410,8 +410,6 @@ uint64_t EventLogVista::seek(uint64_t record_id) {
         throw win_exception(
             _winapi, std::string("failed to subscribe to ") + to_utf8(_path));
     }
-
-    return record_id;
 }
 
 std::unique_ptr<IEventLogRecord> EventLogVista::read() {
@@ -423,6 +421,25 @@ std::unique_ptr<IEventLogRecord> EventLogVista::read() {
     return std::make_unique<EventLogRecordVista>(
         _events[_next_event++], _evt.get(), _render_context->get_handle(),
         _winapi);
+}
+
+uint64_t EventLogVista::getLastRecordId() {
+    auto log = std::make_unique<EventLogWrapper>(
+        *_evt, EvtQueryReverseDirection, _path, _winapi);
+
+    EVT_HANDLE event_handle = nullptr;
+    DWORD num_events = 0;
+    if (evt().next &&
+        evt().next(log->get_handle(), 1, &event_handle, INFINITE, 0,
+                   &num_events)) {
+        auto event = std::make_unique<ManagedEventHandle>(*_evt, event_handle);
+
+        return EventLogRecordVista(event->get_handle(), _evt.get(),
+                                   _render_context->get_handle(), _winapi)
+            .recordId();
+    } else {
+        return 0;
+    }
 }
 
 bool EventLogVista::fillBuffer() {

agents/windows/EventLogVista.h
@@ -150,9 +150,11 @@ public:
 
     virtual void reset() override;
 
-    virtual uint64_t seek(uint64_t record_id) override;
+    virtual void seek(uint64_t record_id) override;
 
     virtual std::unique_ptr<IEventLogRecord> read() override;
+
+    virtual uint64_t getLastRecordId() override;
 
 private:
     static const int EVENT_BLOCK_SIZE = 16;

agents/windows/IEventLog.h
@@ -70,13 +70,17 @@ public:
      * seek to the specified record on the next read or, if the record_number is
      * older than the oldest existing record, seek to the beginning. If the
      * record_number is the highest representable uint32_t, seek to the end of
-     * the log such that only future events are retrieveda
+     * the log such that only future events are retrieved
      *
-     * returns the actual record_id we seeked to, which may differ from the
-     * input
-     * if it was outside the available range
+     * WARNING:
+     * The implementations for pre-Vista and post-Vista are completely
+     * different.
+     * We *must not* return any value as it is different between pre/post Vista.
+     * For obtaining the ID of the last record in eventlog, please use
+     * getLastRecordId instead. It has own implementations for pre/post Vista
+     * but return a uniformly correct value.
      */
-    virtual uint64_t seek(uint64_t record_id) = 0;
+    virtual void seek(uint64_t record_id) = 0;
 
     /**
      * read the next eventlog record
@@ -85,6 +89,11 @@ public:
      * longer
      */
     virtual std::unique_ptr<IEventLogRecord> read() = 0;
+
+    /**
+     * return the ID of the last record in eventlog
+     */
+    virtual uint64_t getLastRecordId() = 0;
 
     /**
      * get a list of dlls that contain eventid->message mappings for this

agents/windows/build_version
@@ -1,1 +1,1 @@
-2992
+2994

agents/windows/sections/SectionEventlog.cc
@@ -166,6 +166,9 @@ std::pair<uint64_t, int> processEventLog(
         previouslyReadId + (uint64limits::max() == previouslyReadId ? 0 : 1);
     int worstState = 0;
     uint64_t lastRecordId = previouslyReadId;
+    // WARNING:
+    // seek implementations for pre-Vista and post-Vista are completely different.
+    // seek *must not* return any value as it is different between pre/post Vista.
     log.seek(seekPosition);
     while (auto record = std::move(log.read())) {
         lastRecordId = record->recordId();
@@ -210,8 +213,11 @@ uint64_t SectionEventlog::outputEventlog(std::ostream &out, const char *logname,
                 };
                 processEventLog(*log, previouslyReadId, level, outputRecord);
             }
-
-            return lastReadId;
+            // Return the last entry number. We need to fetch the last record ID
+            // separately if INT_MAX was used as seek offset and no new entries
+            // were read.
+            return (std::numeric_limits<uint64_t>::max() == lastReadId) ?
+              log->getLastRecordId() : lastReadId;
         }
     } catch (const std::exception &e) {
         Error(_logger) << "failed to read event log: " << e.what() << std::endl;
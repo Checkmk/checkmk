6b7a655446b5baf9ace8e92559b3a762bad80e91
Lars Michelsen <lm@mathias-kettner.de>
2017-11-03 11:31:46 +0100

    Updated test
    Change-Id: I18a47e53b7618294160d7d85fe062f44af9175ad

--- tests/omd/test_python.py
+++ tests/omd/test_python.py
@@ -20,7 +20,7 @@ def test_03_python_interpreter_version(site):
     p = site.execute(["python", "-V"],
                      stderr=subprocess.PIPE)
     version = p.stderr.read()
-    assert version.startswith("Python 2.7.13")
+    assert version.startswith("Python 2.7.14")
 
 
 def test_03_python_path(site):

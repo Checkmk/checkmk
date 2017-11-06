2e14b698f1ec8620535adc03ffba47838885fc04
Sven Panne <sp@mathias-kettner.de>
2017-11-06 10:55:27 +0100

    Unify ContactGroupsColumn implementations.
    Change-Id: I114d2e347d02d2531b7b1ee11d818460fcec2e17

--- livestatus/src/ContactGroupsColumn.cc
+++ livestatus/src/ContactGroupsColumn.cc
@@ -24,16 +24,31 @@
 
 #include "ContactGroupsColumn.h"
 #include "Row.h"
+
+#ifdef CMC
+#include "ContactList.h"
+#include "Object.h"
+#include "cmc.h"
+#else
 #include "nagios.h"
+#endif
 
 std::vector<std::string> ContactGroupsColumn::getValue(
     Row row, const contact * /*auth_user*/,
     std::chrono::seconds /*timezone_offset*/) const {
     std::vector<std::string> names;
+#ifdef CMC
+    if (auto object = columnData<Object>(row)) {
+        for (const auto &name : object->_contact_list->groupNames()) {
+            names.push_back(name);
+        }
+    }
+#else
     if (auto p = columnData<contactgroupsmember *>(row)) {
         for (auto cgm = *p; cgm != nullptr; cgm = cgm->next) {
             names.emplace_back(cgm->group_ptr->group_name);
         }
     }
+#endif
     return names;
 }

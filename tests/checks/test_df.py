d014c8bcf3dc5451bc159d1efa88941e2aedd28a
Konstantin Büttner <kb@mathias-kettner.de>
2017-11-27 10:03:20 +0100

    df: Test cases for Windows inventory
    Change-Id: I0bac6dd97803fb672d54378a3bb5d27769702cf0

--- tests/checks/test_df.py
+++ tests/checks/test_df.py
@@ -4,21 +4,33 @@ import sys
 
 pytestmark = pytest.mark.checks
 
-info_df_1 = [[u'/dev/sda4',
-       u'ext4',
-       u'143786696',
-       u'101645524',
-       u'34814148',
-       u'75%',
-       u'/'],
-      [u'[df_inodes_start]'],
-      [u'/dev/sda4', u'ext4', u'9142272', u'1654272', u'7488000', u'19%', u'/'],
-      [u'[df_inodes_end]']]
+info_df_lnx = [[u'/dev/sda4',
+      u'ext4',
+      u'143786696',
+      u'101645524',
+      u'34814148',
+      u'75%',
+      u'/'],
+     [u'[df_inodes_start]'],
+     [u'/dev/sda4', u'ext4', u'9142272', u'1654272', u'7488000', u'19%', u'/'],
+     [u'[df_inodes_end]']]
+
+info_df_win = [[u'C:\\', u'NTFS', u'8192620', u'7724268', u'468352', u'95%', u'C:\\'],
+     [u'New_Volume', u'NTFS', u'10240796', u'186256', u'10054540', u'2%', u'E:\\'],
+     [u'New_Volume',
+     u'NTFS',
+     u'124929596',
+     u'50840432',
+     u'74089164',
+     u'41%',
+     u'F:\\']]
 
 @pytest.mark.parametrize("info,result,include_volume_name", [
     ([], [], False),
-    (info_df_1, [(u'/', {})], False),
-    (info_df_1, [(u'/dev/sda4 /', {})], True),
+    (info_df_lnx, [(u'/', {})], False),
+    (info_df_lnx, [(u'/dev/sda4 /', {})], True),
+    (info_df_win, [(u'E:/', {}), (u'F:/', {}), (u'C:/', {})], False),
+    (info_df_win, [(u'New_Volume E:/', {}), (u'New_Volume F:/', {}), (u'C:\\ C:/', {})], True),
 ])
 def test_df_discovery_with_parse(check_manager, monkeypatch, info, result, include_volume_name):
     import cmk_base.checks

fe3876192d285aeb969176172c79f57bee3bc01f
Konstantin Büttner <kb@mathias-kettner.de>
2017-11-28 16:25:45 +0100

    test_df_check: Add non-operational test case for check function, missing default params
    Change-Id: I01ef435279df7252ffbf5e6fbe80403d0d93f532

--- tests/checks/test_df_check.py
+++ tests/checks/test_df_check.py
@@ -240,3 +240,25 @@ def test_df_discovery_with_parse(check_manager, monkeypatch, info, expected_resu
     monkeypatch.setitem(cmk_base.checks._check_contexts["df"], "host_extra_conf_merged", lambda _, __: {"include_volume_name": include_volume_name})
     assert check.run_discovery(check.run_parse(info)) == expected_result
     cmk_base.config_cache.clear_all()
+
+
+# TODO: Make this work by finding a way to get a check's default levels in this context.
+@pytest.mark.parametrize("item,params,info,expected_result", [
+#    (u"/", "default", info_df_lnx, {})
+])
+def test_df_check_with_parse(check_manager, monkeypatch, item, params, info, expected_result):
+    import cmk_base
+    check = check_manager.get_check("df")
+
+    if params == "default":
+        params = check.default_levels
+
+    result = check.run_check(item, params, check.run_parse(info))
+    if "status" in expected_result:
+        assert result[0] == expected_result["status"]
+    if "infotext" in expected_result:
+        assert result[1] == expected_result["infotext"]
+    if "perfdata" in expected_result:
+        assert result[2] == expected_result["perfdata"]
+
+    cmk_base.config_cache.clear_all()

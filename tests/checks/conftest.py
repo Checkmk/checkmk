2ed6f3b7280ec3ad8ad54695f9984bd37eb22083
Lars Michelsen <lm@mathias-kettner.de>
2017-11-29 10:31:21 +0100

    Check tests: Automatically use fresh config/runtime caches
    Change-Id: I7f029f1b64919e6fe2208ac3c260eeac77be87f6

--- tests/checks/conftest.py
+++ tests/checks/conftest.py
@@ -11,3 +11,12 @@ def check_manager():
     manager = testlib.CheckManager()
     manager.load()
     return manager
+
+
+# Automatically refresh caches for each test
+@pytest.fixture(autouse=True, scope="function")
+def clear_config_caches(monkeypatch):
+    import cmk_base
+    import cmk_base.caching
+    monkeypatch.setattr(cmk_base, "config_cache", cmk_base.caching.CacheManager())
+    monkeypatch.setattr(cmk_base, "runtime_cache", cmk_base.caching.CacheManager())

fed891a46d24dba31532102fa5eb1e2157eeb46f
Konstantin Büttner <kb@mathias-kettner.de>
2017-12-07 11:19:10 +0100

    Check tests: Initial version of a class to be used to model basic check results.
    Even the basic assertions it provides already uncover strange behavour of the df check.
    
    Change-Id: Ica048a79723718fee7e54db649509754b901d12a

---
+++ tests/checks/checktestlib.py
@@ -0,0 +1,32 @@
+
+class BasicCheckResult(object):
+    """A basic check result
+
+    This class models a basic check result (status, infotext, perfdata) and provides
+    facilities to match it against conditions, such as 'Status is...' or 'Infotext contains...'
+    """
+
+    def __init__(self, status, infotext, perfdata=None):
+        """We perform some basic consistency checks during initialization"""
+        assert status in [0,1,2,3]
+        assert type(infotext) == str
+        assert "\n" not in infotext
+        self.status = status
+        self.infotext = infotext
+        if perfdata is not None:
+            assert type(perfdata) == list
+            assert all([len(entry) >= 2 for entry in perfdata])
+
+            for entry in perfdata:
+                assert type(entry) == tuple
+                assert len(entry) >= 2
+
+                # TODO: This is very basic. There is more way more magic involved
+                #       in what kind of values are allowed as metric names.
+                #       I'm not too sure unicode should be allowed, either.
+                assert type(entry[0]) in [str, unicode]
+
+                assert type(entry[1]) in [int, float]
+                assert all(type(value) in [int, float, type(None)] for value in entry[2:])
+        self.perfdata = perfdata
+

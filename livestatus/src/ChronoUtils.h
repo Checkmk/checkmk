7baaf29c2c6035acf5f4e634e20698e1e7191547
Sven Panne <sp@mathias-kettner.de>
2017-12-08 13:48:07 +0100

    Automatic clang-tidy fix for google-explicit-constructor.
    Change-Id: I3228cec384c35082da04a09005aac98ad845c4d0

--- livestatus/src/ChronoUtils.h
+++ livestatus/src/ChronoUtils.h
@@ -113,10 +113,11 @@ typename Dur::rep time_point_part(std::chrono::system_clock::time_point &tp) {
 
 class FormattedTimePoint {
 public:
-    FormattedTimePoint(std::chrono::system_clock::time_point tp,
-                       const std::string &format = default_format)
+    explicit FormattedTimePoint(std::chrono::system_clock::time_point tp,
+                                const std::string &format = default_format)
         : _tp(tp), _format(format) {}
-    FormattedTimePoint(time_t t, const std::string &format = default_format)
+    explicit FormattedTimePoint(time_t t,
+                                const std::string &format = default_format)
         : _tp(std::chrono::system_clock::from_time_t(t)), _format(format) {}
 
     friend std::ostream &operator<<(std::ostream &os,

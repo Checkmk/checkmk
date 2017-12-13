309b56449ba36bab886d11c07f19242cf87dd772
Konstantin Büttner <kb@mathias-kettner.de>
2017-12-13 18:34:55 +0100

    test_statgrab_cpu_check: Remove unnecessary argument timepoints
    This level of generality is excessive at this point. We really only need to initialize all counters
    and get one 'real' result.
    
    Change-Id: Ica69b993552490a0e57e10bdf0b22637db091030

--- tests/checks/test_statgrab_cpu_check.py
+++ tests/checks/test_statgrab_cpu_check.py
@@ -5,8 +5,8 @@ from cmk_base.check_api import MKCounterWrapped
 pytestmark = pytest.mark.checks
 
 
-@pytest.mark.parametrize("timepoints,time_to_info,params,expected_results", [
-([60],lambda t: [[u'ctxsw', u'%d' % int(t*50)],
+@pytest.mark.parametrize("time_to_info,params,expected_result", [
+(lambda t: [[u'ctxsw', u'%d' % int(t*50)],
                    [u'idle', u'%d' % int(t*50)],
                    [u'intrs', u'%d' % int(t*50)],
                    [u'iowait', u'%d' % int(t*50)],
@@ -22,13 +22,12 @@ pytestmark = pytest.mark.checks
                    [u'vctxsw', u'%d' % int(t*50)]],
             {}, None),
 ])
-def test_statgrab_cpu_check(check_manager, monkeypatch, timepoints, time_to_info, params, expected_results):
+def test_statgrab_cpu_check(check_manager, monkeypatch, time_to_info, params, expected_result):
     import time
     check = check_manager.get_check("statgrab_cpu")
     try:
         result = list(check.run_check(None, params, time_to_info(0)))
     except MKCounterWrapped:
         pass
-    for t in timepoints:
-        monkeypatch.setattr("time.time", lambda: t)
-        result = list(check.run_check(None, params, time_to_info(t)))
+    monkeypatch.setattr("time.time", lambda: 60)
+    result = list(check.run_check(None, params, time_to_info(60)))

3cd1ef2dbd54ce720fb2db62df4f8bfbf3e813a6
Tom Baerwinkel <tb@mathias-kettner.de>
2018-01-18 18:26:05 +0100

    5490 ra32e_power, ra32e_sensors, ra32e_sensors.heatindex, ra32e_sensors.humidity, ra32e_sensors.power, ra32e_sensors.voltage, ra32e_switch: new checks to monitor AVTECH Room Alert 32E devices
    Change-Id: Ief51995f1d2acd17e7831e38b44705f9e769bb90

---
+++ tests/checks/test_ra32e_power_check.py
@@ -0,0 +1,40 @@
+import pytest
+from checktestlib import BasicCheckResult
+
+pytestmark = pytest.mark.checks
+
+RA32E_POWER = "ra32e_power"
+
+@pytest.mark.parametrize("info,result", [
+    ([[u'']], None),
+    ([[u'0']], [(None, {})])
+])
+def test_ra32e_power_discovery(check_manager, info, result):
+    check = check_manager.get_check(RA32E_POWER)
+    assert check.run_discovery(info) == result
+
+
+def test_ra32e_power_check_battery(check_manager):
+    check = check_manager.get_check(RA32E_POWER)
+    result = check.run_check(None, {}, [['0']])
+
+    assert len(result) == 2
+    status, infotext = result
+    assert status == 1
+    assert "battery" in infotext
+
+
+def test_ra32e_power_check_acpower(check_manager):
+    check = check_manager.get_check(RA32E_POWER)
+    result = BasicCheckResult(*check.run_check(None, {}, [['1']]))
+
+    assert result.status == 0
+    assert 'AC/Utility' in result.infotext
+
+
+def test_ra32e_power_check_nodata(check_manager):
+    check = check_manager.get_check(RA32E_POWER)
+    result = BasicCheckResult(*check.run_check(None, {}, [['']]))
+
+    assert result.status == 3
+    assert 'unknown' in result.infotext

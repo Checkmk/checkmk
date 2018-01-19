7a83d96ed7d973ee7c6e7285e05bbcd37abaceed
Konstantin Büttner <kb@mathias-kettner.de>
2018-01-19 14:50:24 +0100

    Basic test for kernel.util
    Change-Id: I3e4118dd892cb20bd3c6f89ecea8c2b53b0d6ddf

---
+++ tests/checks/test_kernel_check.py
@@ -0,0 +1,19 @@
+import pytest
+import pprint
+from cmk_base.check_api import MKCounterWrapped
+import checktestlib
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize("time_to_info,params,predicate", [
+(lambda t: [["cpu", 15*t, 10*t, 5*t, 5*t, 5*t, 5*t, 5*t]], {}, lambda cr: True),
+])
+def test_kernel_util_check(check_manager, monkeypatch, time_to_info, params, predicate):
+    check = check_manager.get_check("kernel.util")
+    try:
+        list(check.run_check(None, params, time_to_info(0)))
+    except MKCounterWrapped:
+        pass
+    result = checktestlib.CheckResult(check.run_check(None, params, time_to_info(60)))
+    assert predicate(result)

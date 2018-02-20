abe009ab6d8294857cce278e36e4572a174290e4
Lars Michelsen <lm@mathias-kettner.de>
2018-02-20 11:30:52 +0100

    Add more management board tests
    Change-Id: Ided35be1bc222e5078210af4f0f6ddcd7a4b3de5

--- tests/cmk_base/test_mgmt_board.py
+++ tests/cmk_base/test_mgmt_board.py
@@ -5,36 +5,12 @@ from testlib import web
 
 import cmk_base.config as config
 
-@pytest.fixture(scope="module")
-def test_cfg(web, site):
-    print "Applying default config"
-    web.add_host("mgmt-host1", attributes={
-        "ipaddress": "127.0.0.1",
-        "management_protocol": "snmp",
-        "management_snmp_community": "HOST",
-    })
-    web.add_host("mgmt-host2", attributes={
-        "ipaddress": "127.0.0.1",
-        "management_protocol": "snmp",
-        "management_address": "127.0.0.2",
-    })
-    web.add_host("mgmt-host3", attributes={
-        "ipaddress": "127.0.0.1",
-        "management_protocol": None,
-        "management_address": "127.0.0.1",
-        "management_snmp_community": "HOST",
-    })
-
-    web.activate_changes()
-
+@pytest.fixture(autouse=True)
+def test_cfg(web):
     import cmk.debug
     cmk.debug.enable()
 
-    import cmk_base.checks as checks
-    checks.load()
-    config.load()
-
-    yield None
+    yield
 
     #
     # Cleanup code
@@ -43,24 +19,128 @@ def test_cfg(web, site):
 
     cmk.debug.disable()
 
-    web.delete_host("mgmt-host1")
-    web.delete_host("mgmt-host2")
-    web.delete_host("mgmt-host3")
+    if web.host_exists("mgmt-host"):
+        web.delete_host("mgmt-host")
+
+    if web.folder_exists("folder1"):
+        web.delete_folder("folder1")
+
     web.activate_changes()
 
+    config.load()
 
-def test_management_board_configc(test_cfg):
-    assert config.has_management_board("mgmt-host1")
-    assert config.management_address_of("mgmt-host1") == "127.0.0.1"
-    assert config.management_protocol_of("mgmt-host1") == "snmp"
-    assert config.management_credentials_of("mgmt-host1") == "HOST"
 
-    assert config.has_management_board("mgmt-host2")
-    assert config.management_address_of("mgmt-host2") == "127.0.0.2"
-    assert config.management_protocol_of("mgmt-host2") == "snmp"
-    assert config.management_credentials_of("mgmt-host2") == "public"
+def test_mgmt_explicit_settings(web):
+    web.add_host("mgmt-host", attributes={
+        "ipaddress": "127.0.0.1",
+        "management_protocol": "snmp",
+        "management_snmp_community": "HOST",
+    })
 
-    assert config.has_management_board("mgmt-host3") == False
-    assert config.management_address_of("mgmt-host3") == "127.0.0.1"
-    assert config.management_protocol_of("mgmt-host3") == None
-    assert config.management_credentials_of("mgmt-host3") == "HOST"
+    config.load()
+    assert config.has_management_board("mgmt-host")
+    assert config.management_address_of("mgmt-host") == "127.0.0.1"
+    assert config.management_protocol_of("mgmt-host") == "snmp"
+    assert config.management_credentials_of("mgmt-host") == "HOST"
+
+
+def test_mgmt_explicit_address(web):
+    web.add_host("mgmt-host", attributes={
+        "ipaddress": "127.0.0.1",
+        "management_protocol": "snmp",
+        "management_address": "127.0.0.2",
+    })
+
+    config.load()
+    assert config.has_management_board("mgmt-host")
+    assert config.management_address_of("mgmt-host") == "127.0.0.2"
+    assert config.management_protocol_of("mgmt-host") == "snmp"
+    assert config.management_credentials_of("mgmt-host") == "public"
+
+
+def test_mgmt_disabled(web):
+    web.add_host("mgmt-host", attributes={
+        "ipaddress": "127.0.0.1",
+        "management_protocol": None,
+        "management_address": "127.0.0.1",
+        "management_snmp_community": "HOST",
+    })
+
+    config.load()
+    assert config.has_management_board("mgmt-host") == False
+    assert config.management_address_of("mgmt-host") == "127.0.0.1"
+    assert config.management_protocol_of("mgmt-host") == None
+    assert config.management_credentials_of("mgmt-host") == "HOST"
+
+
+def test_mgmt_inherit_credentials_explicit_host(web):
+    web.add_folder("folder1", attributes={
+        "management_snmp_community": "FOLDER",
+    })
+
+    web.add_host("mgmt-host", folder="folder1", attributes={
+        "ipaddress": "127.0.0.1",
+        "management_protocol": "snmp",
+        "management_snmp_community": "HOST",
+    })
+
+    config.load()
+    assert config.has_management_board("mgmt-host")
+    assert config.management_address_of("mgmt-host") == "127.0.0.1"
+    assert config.management_protocol_of("mgmt-host") == "snmp"
+    assert config.management_credentials_of("mgmt-host") == "HOST"
+
+
+
+def test_mgmt_inherit_credentials(web):
+    web.add_folder("folder1", attributes={
+        "management_snmp_community": "FOLDER",
+    })
+
+    web.add_host("mgmt-host", folder="folder1", attributes={
+        "ipaddress": "127.0.0.1",
+        "management_protocol": "snmp",
+    })
+
+    config.load()
+    assert config.has_management_board("mgmt-host")
+    assert config.management_address_of("mgmt-host") == "127.0.0.1"
+    assert config.management_protocol_of("mgmt-host") == "snmp"
+    assert config.management_credentials_of("mgmt-host") == "FOLDER"
+
+
+def test_mgmt_inherit_protocol_explicit_host(web):
+    web.add_folder("folder1", attributes={
+        "management_protocol": None,
+        "management_snmp_community": "FOLDER",
+    })
+
+    web.add_host("mgmt-host", folder="folder1", attributes={
+        "ipaddress": "127.0.0.1",
+        "management_protocol": "snmp",
+        "management_snmp_community": "HOST",
+    })
+
+    config.load()
+    assert config.has_management_board("mgmt-host")
+    assert config.management_address_of("mgmt-host") == "127.0.0.1"
+    assert config.management_protocol_of("mgmt-host") == "snmp"
+    assert config.management_credentials_of("mgmt-host") == "HOST"
+
+
+
+def test_mgmt_inherit_protocol(web):
+    web.add_folder("folder1", attributes={
+        "management_protocol": "snmp",
+        "management_snmp_community": "FOLDER",
+    })
+
+    web.add_host("mgmt-host", folder="folder1", attributes={
+        "ipaddress": "127.0.0.1",
+    })
+
+    config.load()
+    assert config.has_management_board("mgmt-host")
+    assert config.management_address_of("mgmt-host") == "127.0.0.1"
+    assert config.management_protocol_of("mgmt-host") == "snmp"
+    assert config.management_credentials_of("mgmt-host") == "FOLDER"

937f16e461107aeee15fc80e1986c96813ccf7bb
Sven Panne <sp@mathias-kettner.de>
2018-02-22 13:29:50 +0100

    clang-formatted
    Change-Id: Iba6bfce8368333a69d0cd6f46279b27d981237be

--- bin/mkeventd_open514.cc
+++ bin/mkeventd_open514.cc
@@ -97,8 +97,9 @@ int main(int argc, char **argv) {
         addr.sin_addr.s_addr = 0;
         if (0 != bind(syslog_sock, reinterpret_cast<struct sockaddr *>(&addr),
                       sizeof(addr))) {
-            perror("Cannot bind UDP socket for syslog to port "
-                   "(Is SUID bit set on mkeventd_open514? Is \"nosuid\" not set on the filesystem?)");
+            perror(
+                "Cannot bind UDP socket for syslog to port "
+                "(Is SUID bit set on mkeventd_open514? Is \"nosuid\" not set on the filesystem?)");
             exit(1);
         }
 
@@ -134,8 +135,9 @@ int main(int argc, char **argv) {
         if (0 != bind(syslog_tcp_sock,
                       reinterpret_cast<struct sockaddr *>(&addr),
                       sizeof(addr))) {
-            perror("Cannot bind TCP socket for syslog-tcp to port "
-                   "(Is SUID bit set on mkeventd_open514? Is \"nosuid\" not set on the filesystem?)");
+            perror(
+                "Cannot bind TCP socket for syslog-tcp to port "
+                "(Is SUID bit set on mkeventd_open514? Is \"nosuid\" not set on the filesystem?)");
             exit(1);
         }
 
@@ -170,8 +172,9 @@ int main(int argc, char **argv) {
         addr.sin_addr.s_addr = 0;
         if (0 != bind(snmptrap_sock, reinterpret_cast<struct sockaddr *>(&addr),
                       sizeof(addr))) {
-            perror("Cannot bind UDP socket for snmptrap to port "
-                   "(Is SUID bit set on mkeventd_open514? Is \"nosuid\" not set on the filesystem?)");
+            perror(
+                "Cannot bind UDP socket for snmptrap to port "
+                "(Is SUID bit set on mkeventd_open514? Is \"nosuid\" not set on the filesystem?)");
             exit(1);
         }
 

ee2b7a281f0852d3a61b0fb150e4b9fc8aefe73c
Konstantin Büttner <kb@mathias-kettner.de>
2018-03-06 10:56:51 +0100

    5711 FIX cisco_mem_asa64: Fix incorrect labelling of graph
    Previously, the performance value would be incorrectly displayed
    as absolute usage in the GUI, when it is in fact relative.
    This has been fixed.
    
    Change-Id: I40de7621818f577bcc3caac3245d489932344d1d

--- web/plugins/metrics/check_mk.py
+++ web/plugins/metrics/check_mk.py
@@ -5240,6 +5240,10 @@ check_metrics["check_mk-cisco_mem_asa64"] = {
     "trend"    : { "name" : "mem_trend" },
 }
 
+check_metrics["check_mk-cisco_mem_asa64"] = {
+    "mem_used" : { "name" : "mem_used_percent" }
+}
+
 check_metrics["check_mk-fortigate_sessions_base"] = {
     "session"   : { "name" : "active_sessions" }
 }

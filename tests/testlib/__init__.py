807a4c2a7e557815faed47b1e8d89dfecfad1287
Lars Michelsen <lm@mathias-kettner.de>
2018-03-06 14:01:02 +0100

    Add quick lock hint for locking
    Change-Id: I7b9aee665b0065eac7660953869ad5f536d4163c

--- tests/testlib/__init__.py
+++ tests/testlib/__init__.py
@@ -354,7 +354,9 @@ class Site(object):
             raise Exception("The site %s already exists." % self.id)
 
         if not self.exists():
-            with lockfile.FileLock("/tmp/cmk-test-create-site.lock"):
+            print("Getting site create lock (/tmp/cmk-test-create-site.lock)...")
+            with lockfile.FileLock("/tmp/cmk-test-create-site"):
+                print("Have site create lock")
                 p = subprocess.Popen(["/usr/bin/sudo", "/usr/bin/omd",
                                       "-V", self.version.version_directory(),
                                       "create",

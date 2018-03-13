9dcff9b8b44cedfed7140899ef1ebd2770f6b783
Jukka Aro <ja@mathias-kettner.de>
2018-03-13 15:02:09 +0100

    Win-agent: const correctness fixes 2
    Declare two members of SectionCheckMK as const. Move createInfoFields to
    a standalone function in anonymous namespace.

--- agents/windows/sections/SectionCheckMK.h
+++ agents/windows/sections/SectionCheckMK.h
@@ -41,13 +41,12 @@ public:
                    const WinApiAdaptor &winapi);
 
 protected:
-    virtual bool produceOutputInner(std::ostream &out, const std::optional<std::string> &) override;
+    virtual bool produceOutputInner(
+        std::ostream &out, const std::optional<std::string> &) override;
 
 private:
-    std::vector<KVPair> createInfoFields() const;
-
-    Configurable<bool> _crash_debug;
-    OnlyFromConfigurable &_only_from;
+    const Configurable<bool> _crash_debug;
+    const OnlyFromConfigurable &_only_from;
 
     // static fields
     const std::vector<KVPair> _info_fields;

a046323453ef82cf559b3d44946cafb22429284b
Simon Betz <si@mathias-kettner.de>
2018-04-09 09:25:56 +0200

    Adapted test
    Change-Id: I5aeb4af9448ce75cc5932f9e1831190769fd9a8b

--- tests/unit/checks/test_nfsmounts_and_cifsmounts_check.py
+++ tests/unit/checks/test_nfsmounts_and_cifsmounts_check.py
@@ -16,29 +16,29 @@ pytestmark = pytest.mark.checks
     ),
     (  # single mountpoint with data
         [[u'/ABCshare', u'ok', u'491520', u'460182', u'460182', u'65536']],
-        [('/ABCshare', None)],
+        [('/ABCshare', {})],
         [('/ABCshare', {}, BasicCheckResult(0, "6.4% used (1.91 GB of 30.00 GB)", None)),
          ('/ZZZshare', {}, BasicCheckResult(3, "/ZZZshare not mounted", None))]
     ),
     (  # two mountpoints with empty data
         [[u'/AB', u'ok', u'-', u'-', u'-', u'-'],
          [u'/ABC', u'ok', u'-', u'-', u'-', u'-']],
-        [('/AB', None),
-         ('/ABC', None)],
+        [('/AB', {}),
+         ('/ABC', {})],
         [('/AB', {}, BasicCheckResult(0, "Mount seems OK", None)),
          ('/ABC', {}, BasicCheckResult(0, "Mount seems OK", None))]
     ),
     (  # Mountpoint with spaces and permission denied
         [[u'/var/dba', u'export', u'Permission', u'denied'],
          [u'/var/dbaexport', u'ok', u'201326592', u'170803720', u'170803720', u'32768']],
-        [('/var/dbaexport', None),
-         ('/var/dba export', None)],
+        [('/var/dbaexport', {}),
+         ('/var/dba export', {})],
         [('/var/dba export', {}, BasicCheckResult(2, 'Permission denied', None)),
          ('/var/dbaexport', {}, BasicCheckResult(0, '15.2% used (931.48 GB of 6.00 TB)', None))]
     ),
     (  # with perfdata
         [[u'/PERFshare', u'ok', u'491520', u'460182', u'460182', u'65536']],
-        [('/PERFshare', None)],
+        [('/PERFshare', {})],
         [('/PERFshare', {'has_perfdata': True},
           BasicCheckResult(0, "6.4% used (1.91 GB of 30.00 GB)", [
               PerfValue('fs_size', 491520 * 65536), PerfValue('fs_used', 491520 * 65536 - 460182 * 65536)
@@ -46,18 +46,18 @@ pytestmark = pytest.mark.checks
     ),
     (  # state == 'hanging'
         [[u'/test', u'hanging', u'hanging', u'0', u'0', u'0', u'0']],
-        [('/test hanging', None)],
+        [('/test hanging', {})],
         [('/test hanging', {'has_perfdata': True},
           BasicCheckResult(2, "Server not responding", None))]
     ),
     (  # unknown state
         [[u'/test', u'unknown', u'unknown', u'1', u'1', u'1', u'1']],
-        [('/test unknown', None)],
+        [('/test unknown', {})],
         [('/test unknown', {}, BasicCheckResult(2, "Unknown state", None))]
     ),
     (  # zero block size
         [[u'/test', u'perfdata', u'ok', u'0', u'460182', u'460182', u'0']],
-        [('/test perfdata', None)],
+        [('/test perfdata', {})],
         [('/test perfdata', {'has_perfdata': True},
           # TODO: display a better error message
           #BasicCheckResult(0, "server is responding", [PerfValue('fs_size', 0), PerfValue('fs_used', 0)]))]

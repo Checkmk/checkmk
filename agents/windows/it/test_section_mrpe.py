Checkmk/checkmk

commit 6503810d95eddff68cd9c7faabe8d64c15c5cc02

Windows agent: remove unused code in stringutil.*

agents/windows/build_version
@@ -1,1 +1,1 @@
-3098
+3100

agents/windows/sections/SectionMRPE.cc
@@ -115,10 +115,10 @@ bool SectionMRPE::produceOutputInner(std::ostream &out) {
             }
             command.readStdout(pos, buffer.size() - (pos - buf_start), false);
 
-            char *output_end = rstrip(&buffer[0]);
-            char *plugin_output = lstrip(&buffer[0]);
+            rtrim(buffer);
+            ltrim(buffer);
             // replace newlines
-            std::transform(plugin_output, output_end, plugin_output,
+            std::transform(buffer.cbegin(), buffer.cend(), buffer.begin(),
                            [](char ch) {
                                if (ch == '\n') return '\1';
                                if (ch == '\r')
@@ -127,7 +127,7 @@ bool SectionMRPE::produceOutputInner(std::ostream &out) {
                                    return ch;
                            });
             int nagios_code = command.exitCode();
-            out << nagios_code << " " << plugin_output << "\n";
+            out << nagios_code << " " << buffer << "\n";
             Debug(_logger) << "Script finished";
         } catch (const std::exception &e) {
             Error(_logger) << "mrpe failed: " << e.what();

agents/windows/stringutil.cc
@@ -12,73 +12,6 @@
 using std::string;
 using std::wstring;
 
-char *lstrip(char *s) {
-    while (isspace(*s)) s++;
-    return s;
-}
-
-const char *lstrip(const char *s) {
-    while (isspace(*s)) s++;
-    return s;
-}
-
-char *rstrip(char *s) {
-    char *end = s + strlen(s);  // point one beyond last character
-    while (end > s && isspace(*(end - 1))) {
-        end--;
-    }
-    *end = 0;
-    return end;
-}
-
-char *strip(char *s) {
-    rstrip(s);
-    return lstrip(s);
-}
-
-std::vector<const char *> split_line(char *pos, int (*split_pred)(int)) {
-    std::vector<const char *> result;
-
-    char *current_word = pos;
-    while (*pos != '\0') {
-        if (split_pred(*pos)) {
-            *pos = '\0';
-            const char *trimmed = strip(current_word);
-            if (*trimmed != '\0') {
-                result.push_back(trimmed);
-            }
-            current_word = pos + 1;
-        }
-        ++pos;
-    }
-    const char *trimmed = strip(current_word);
-    if (*trimmed != '\0') {
-        result.push_back(trimmed);
-    }
-    return result;
-}
-
-char *next_word(char **line) {
-    if (*line == 0)  // allow subsequent calls without checking
-        return 0;
-
-    char *end = *line + strlen(*line);
-    char *value = *line;
-    while (value < end) {
-        value = lstrip(value);
-        char *s = value;
-        while (*s && !isspace(*s)) s++;
-        *s = 0;
-        *line = s + 1;
-        rstrip(value);
-        if (strlen(value) > 0)
-            return value;
-        else
-            return 0;
-    }
-    return 0;
-}
-
 template <>
 std::regex possiblyQuotedRegex<char>() {
     return std::regex{"(\"([^\"]+)\"|'([^']+)'|[^\" \\t]+)"};
@@ -87,25 +20,6 @@ std::regex possiblyQuotedRegex<char>() {
 template <>
 std::wregex possiblyQuotedRegex<wchar_t>() {
     return std::wregex{L"(\"([^\"]+)\"|'([^']+)'|[^\" \\t]+)"};
-}
-
-unsigned long long string_to_llu(const char *s) {
-    unsigned long long value = 0;
-    unsigned long long mult = 1;
-    const char *e = s + strlen(s);
-    while (e > s) {
-        --e;
-        value += mult * (*e - '0');
-        mult *= 10;
-    }
-    return value;
-}
-
-void lowercase(char *s) {
-    while (*s) {
-        *s = tolower(*s);
-        s++;
-    }
 }
 
 int parse_boolean(const char *value) {

agents/windows/stringutil.h
@@ -41,11 +41,6 @@ using std::sregex_token_iterator;
 
 class WinApiAdaptor;
 
-char *lstrip(char *s);
-const char *lstrip(const char *s);
-char *rstrip(char *s);
-char *strip(char *s);
-
 inline void ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.cbegin(), s.cend(),
                                     [](int ch) { return !std::isspace(ch); }));
@@ -57,9 +52,6 @@ inline void rtrim(std::string &s) {
                 .base(),
             s.end());
 }
-
-std::vector<const char *> split_line(char *pos, int (*split_pred)(int));
-char *next_word(char **line);
 
 template <typename CharT>
 inline std::vector<std::basic_string<CharT>> tokenizeBase(
@@ -125,8 +117,6 @@ inline std::vector<std::basic_string<CharT>> tokenizePossiblyQuoted(
 }
 
 unsigned long long string_to_llu(const char *s);
-
-void lowercase(char *s);
 
 int parse_boolean(const char *value);
 

commit 6265266f866f04aeca275150d0a5a0befd957d2c

5923 FIX Windows: section MRPE was incorrectly formatted
The output of section MRPE was incorrectly formatted. A null character
at the end of the output string caused the output to be cut abruptly
and missing a newline character before the following section header.
Broken by commit 6503810d95eddff68cd9c7faabe8d64c15c5cc02 that was
included in releases 1.5.0i3 and 1.5.0b1.

.werks/5923
@@ -1,0 +1,16 @@
+Title: Windows: section MRPE was incorrectly formatted
+Level: 1
+Component: checks
+Compatible: compat
+Edition: cre
+Version: 1.6.0i1
+Date: 1524656219
+Class: fix
+
+The output of section MRPE was incorrectly formatted. A null character
+at the end of the output string caused the output to be cut abruptly
+and missing a newline character before the following section header.
+Broken by commit d0ae246003d630f80ad5ff3fbe469552738141a9 that was
+included in releases 1.5.0i3 and 1.5.0b1.
+
+

agents/windows/build_version
@@ -1,1 +1,1 @@
-3250
+3252

agents/windows/sections/SectionMRPE.cc
@@ -104,8 +104,7 @@ bool SectionMRPE::produceOutputInner(std::ostream &out,
         try {
             ExternalCmd command(modified_command, _env, _logger, _winapi);
             Debug(_logger) << "Script started -> collecting data";
-            string buffer;
-            buffer.resize(8192);
+            string buffer(8192, '\0');
             char *buf_start = &buffer[0];
             char *pos = &buffer[0];
             while (command.exitCode() == STILL_ACTIVE) {
@@ -114,10 +113,13 @@ bool SectionMRPE::produceOutputInner(std::ostream &out,
                 pos += read;
                 _winapi.Sleep(10);
             }
-            command.readStdout(pos, buffer.size() - (pos - buf_start), false);
-
+            DWORD read = command.readStdout(
+                pos, buffer.size() - (pos - buf_start), false);
+            pos += read;
+            buffer.resize(pos - buf_start);
             rtrim(buffer);
             ltrim(buffer);
+
             // replace newlines
             std::transform(buffer.cbegin(), buffer.cend(), buffer.begin(),
                            [](char ch) {

agents/windows/it/test_section_mrpe.py
@@ -11,6 +11,7 @@ from remote import (actual_output, assert_subprocess, config, remote_ip,
 
 class Globals(object):
     section = 'mrpe'
+    alone = True
     pluginname = 'check_crit.bat'
     param = 'foobar'
     checkname = 'Dummy'
@@ -25,9 +26,13 @@ def testfile():
     return os.path.basename(__file__)
 
 
-@pytest.fixture
-def testconfig(config):
-    config.set('global', 'sections', Globals.section)
+@pytest.fixture(params=['alone', 'with_systemtime'])
+def testconfig(request, config):
+    Globals.alone = request.param == 'alone'
+    if Globals.alone:
+        config.set('global', 'sections', Globals.section)
+    else:
+        config.set('global', 'sections', '%s systemtime' % Globals.section)
     config.set('global', 'crash_debug', 'yes')
     config.add_section(Globals.section)
     if Globals.newline < 0:
@@ -43,11 +48,17 @@ def testconfig(config):
 @pytest.fixture
 def expected_output():
     drive = r'[A-Z]:%s' % re.escape(os.sep)
-    return [
-        re.escape(r'<<<%s>>>' % Globals.section),
-        r'\(%s\) %s 2 CRIT - This check is always critical' %
-        (Globals.pluginname, Globals.checkname)
-    ]
+    expected = [
+            re.escape(r'<<<%s>>>' % Globals.section),
+            r'\(%s\) %s 2 CRIT - This check is always critical' %
+            (Globals.pluginname, Globals.checkname)
+        ]
+    if not Globals.alone:
+        expected += [
+            re.escape(r'<<<systemtime>>>'),
+            r'\d+'
+        ]
+    return expected
 
 
 @pytest.fixture(
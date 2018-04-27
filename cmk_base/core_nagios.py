cfc92a98748ce92e8ca799f9f507cb81c84f7b32
Lars Michelsen <lm@mathias-kettner.de>
2018-04-27 08:16:13 +0200

    6036 FIX Fixed broken IP lookup in Check_MK service when using Nagios core
    The "Check_MK" services of hosts were not doing IP resolution correctly,
    leading to messages like "(No output returned from plugin)" and non working
    checking for these hosts.
    
    Change-Id: I0c21093b1c9b96a76a8692882728c6c12c513f8c

--- cmk_base/core_nagios.py
+++ cmk_base/core_nagios.py
@@ -1094,7 +1094,6 @@ if '-d' in sys.argv:
         nodes = [ (hostname, ipaddress) ]
 
     output.write("config.ipaddresses = %r\n\n" % needed_ipaddresses)
-    output.write("ip_lookup.lookup_ip_address = lambda hostname: config.ipaddresses.get(hostname)\n\n");
 
     # datasource programs. Is this host relevant?
 

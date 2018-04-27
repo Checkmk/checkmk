8413b81af4766eaee2907d4802e6e27259a517cc
Lars Michelsen <lm@mathias-kettner.de>
2018-04-27 10:30:55 +0200

    6037 FIX Fixed broken "Primary IP address family" rule set
    When using the rule set "Primary IP address family" to change the
    IP address family of dual stack hosts to IPv6, this rule was not
    effective.
    
    Change-Id: I364757474544262f8686b84a14b6c5eb827f923b

--- tests/unit/cmk_base/test_config.py
+++ tests/unit/cmk_base/test_config.py
@@ -1,6 +1,7 @@
 import pytest
 
 import cmk_base.config as config
+import cmk_base.rulesets as rulesets
 
 @pytest.mark.parametrize("hostname,tags,result", [
     ("testhost", [], True),
@@ -24,3 +25,24 @@ def test_is_ipv4_host(monkeypatch, hostname, tags, result):
 def test_is_no_ip_host(monkeypatch, hostname, tags, result):
     monkeypatch.setattr(config, "tags_of_host", lambda h: {hostname: tags}[h])
     assert config.is_no_ip_host(hostname) == result
+
+
+@pytest.mark.parametrize("hostname,tags,result,ruleset", [
+    ("testhost", [], False, []),
+    ("testhost", ["ip-v4"], False,
+     [ ( 'ipv6', [], rulesets.ALL_HOSTS, {} ), ]),
+    ("testhost", ["ip-v4", "ip-v6"], False, []),
+    ("testhost", ["ip-v4", "ip-v6"], True,
+     [ ( 'ipv6', [], rulesets.ALL_HOSTS, {} ), ]),
+    ("testhost", ["ip-v6"], True, []),
+    ("testhost", ["ip-v6"], True,
+     [ ( 'ipv4', [], rulesets.ALL_HOSTS, {} ), ]),
+    ("testhost", ["ip-v6"], True,
+     [ ( 'ipv6', [], rulesets.ALL_HOSTS, {} ), ]),
+    ("testhost", ["no-ip"], False, []),
+])
+def test_is_ipv6_primary_host(monkeypatch, hostname, tags, result, ruleset):
+    monkeypatch.setattr(config, "all_hosts", ["%s|%s" % (hostname, "|".join(tags))])
+    monkeypatch.setattr(config, "tags_of_host", lambda h: {hostname: tags}[h])
+    monkeypatch.setattr(config, "primary_address_family", ruleset)
+    assert config.is_ipv6_primary(hostname) == result

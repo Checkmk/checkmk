b5f11c03e7b461c2bc17ca49bb65d6981d7dcbef
Lars Michelsen <lm@mathias-kettner.de>
2018-05-14 10:55:54 +0200

    6068 FIX Fixed misleading "Last user synchronization ran into an exception: User synchronization" message
    When opening the Users page of the configuration an error message "Last user synchronization ran into an exception: User synchronization"
    could be shown since 1.5.0b2. Even when no LDAP connection is configured. This error message has been fixed.
    
    Change-Id: I3d6d5ba18239078404b3bf1bef88f13778ae024a

--- web/htdocs/userdb.py
+++ web/htdocs/userdb.py
@@ -1376,3 +1376,4 @@ class UserSyncBackgroundJob(gui_background_job.GUIBackgroundJob):
 
         job_interface.send_progress_update(_("Finalizing synchronization"))
         general_userdb_job()
+        return True

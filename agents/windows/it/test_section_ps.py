0287aa615770bcbd370b1218a8ba0df4e3c27b49
Jukka Aro <ja@mathias-kettner.de>
2018-05-16 15:36:09 +0200

    Win-agent: test section ps followed by systemtime
    Check that the ps section is correctly formatted until the end by
    adding section systemtime after it. This is necessary for ensuring that
    e. g. the newline is not left out at the end of the ps section.
    
    Change-Id: Ica9d418ca67e7a4dc3e2cbeea88e43f20931f44c

--- agents/windows/it/test_section_ps.py
+++ agents/windows/it/test_section_ps.py
@@ -7,8 +7,9 @@ import re
 from remote import (actual_output, config, remotetest, remotedir, wait_agent,
                     write_config)
 
-section = 'ps'
-
+class Globals(object):
+    section = 'ps'
+    alone = True
 
 @pytest.fixture
 def testfile():
@@ -17,24 +18,36 @@ def testfile():
 
 @pytest.fixture(params=['yes', 'no'], ids=['use_wmi=yes', 'use_wmi=no'])
 def testconfig(request, config):
-    config.set('global', 'sections', section)
+    config.set('global', 'sections', Globals.section)
     config.set('global', 'crash_debug', 'yes')
-    config.add_section(section)
-    config.set(section, 'use_wmi', request.param)
+    config.add_section(Globals.section)
+    config.set(Globals.section, 'use_wmi', request.param)
     return config
 
 
-@pytest.fixture(params=['yes', 'no'], ids=['full_path=yes', 'full_path=no'])
-def full_path_config(request, testconfig):
-    testconfig.set(section, 'full_path', request.param)
+@pytest.fixture(params=['alone', 'with_systemtime'])
+def testconfig_sections(request, testconfig):
+    Globals.alone = request.param == 'alone'
+    if Globals.alone:
+        testconfig.set('global', 'sections', Globals.section)
+    else:
+        testconfig.set('global', 'sections', '%s systemtime' % Globals.section)
     return testconfig
+
+
+@pytest.fixture(params=['yes', 'no'], ids=['full_path=yes', 'full_path=no'])
+def full_path_config(request, testconfig_sections):
+    testconfig_sections.set(Globals.section, 'full_path', request.param)
+    return testconfig_sections
 
 
 @pytest.fixture
 def expected_output():
-    return chain([re.escape(r'<<<ps:sep(9)>>>')],
-                 repeat(r'\([^,]+,\d+,\d+,\d+,\d+,\d+,\d+,\d+,\d+,\d+,\d+\)\s+'
-                        r'(.+\.[Ee][Xx][Ee]|System( Idle Process)?)'))
+    re_str = (r'\([^,]+,\d+,\d+,\d+,\d+,\d+,\d+,\d+,\d+,\d+,\d+\)\s+'
+              r'(.+\.[Ee][Xx][Ee]|System( Idle Process)?)')
+    if not Globals.alone:
+        re_str += r'|' + re.escape(r'<<<systemtime>>>') + r'|\d+'
+    return chain([re.escape(r'<<<ps:sep(9)>>>')], repeat(re_str))
 
 
 def test_section_ps(request, full_path_config, expected_output, actual_output,

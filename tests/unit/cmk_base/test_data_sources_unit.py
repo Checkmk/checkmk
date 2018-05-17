e0b60d9e1740429beec683e1525b75d0484c571e
Lars Michelsen <lm@mathias-kettner.de>
2018-05-17 10:31:39 +0200

    Revert "6063 FIX Special agents: Remove unreplaced macros from special agent command lines"
    This reverts commit 12548f016ffe4528067ffb1de11fa0ff8e4b7bb0.

--- tests/unit/cmk_base/test_data_sources_unit.py
+++ tests/unit/cmk_base/test_data_sources_unit.py
@@ -34,23 +34,3 @@ def test_disable_data_source_cache_no_write(mocker):
     disabled_checker = mocker.patch.object(source, "is_agent_cache_disabled")
     assert source._write_cache_file("X") is None
     disabled_checker.assert_called_once()
-
-
-def test_ds_command_line_expansion_legacy_macros():
-    source = cmk_base.data_sources.programs.DSProgramDataSource("hostname", "ipaddress", "echo '<IP> <HOST>'")
-    assert source._get_command_line() == "echo 'ipaddress hostname'"
-
-
-def test_ds_command_line_expansion_host_macros():
-    source = cmk_base.data_sources.programs.DSProgramDataSource("hostname", "ipaddress", "echo '$HOSTNAME$'")
-    assert source._get_command_line() == "echo 'hostname'"
-
-
-def test_ds_command_line_expansion_left_over_macros():
-    source = cmk_base.data_sources.programs.DSProgramDataSource("hostname", "ipaddress", "echo '$BLABLUB$'")
-    assert source._get_command_line() == "echo ''"
-
-
-def test_ds_command_line_expansion_skip_shell_variables():
-    source = cmk_base.data_sources.programs.DSProgramDataSource("hostname", "ipaddress", "echo '$BLABLUB$ $XXX'")
-    assert source._get_command_line() == "echo ' $XXX'"

ec1b31ab4f2877fd05eb65b4a3da4b460b402157
Lars Michelsen <lm@mathias-kettner.de>
2018-05-25 17:46:42 +0200

    6174 FIX Fixed discovery for host with management board without IP addresses configured
    The discovery was not working correctly for hosts that have the management board
    protocol enabled but no explicit IP address set and no management board IP configured.
    
    Change-Id: I993adc25839541715532c7b0c6034fbcfaa10f35

--- tests/unit/cmk_base/test_data_sources_unit.py
+++ tests/unit/cmk_base/test_data_sources_unit.py
@@ -1,6 +1,10 @@
 import pytest
 
+import cmk_base.data_sources.abstract
 import cmk_base.data_sources.snmp
+import cmk_base.ip_lookup as ip_lookup
+import cmk_base.config as config
+import cmk_base.exceptions
 
 def test_data_source_cache_default():
     source = cmk_base.data_sources.snmp.SNMPDataSource("hostname", "ipaddress")
@@ -34,3 +38,33 @@ def test_disable_data_source_cache_no_write(mocker):
     disabled_checker = mocker.patch.object(source, "is_agent_cache_disabled")
     assert source._write_cache_file("X") is None
     disabled_checker.assert_called_once()
+
+
+def test_mgmt_board_data_source_is_ip_address():
+    _is_ipaddress = cmk_base.data_sources.abstract.ManagementBoardDataSource._is_ipaddress
+    assert _is_ipaddress(None) == False
+    assert _is_ipaddress("localhost") == False
+    assert _is_ipaddress("abc 123") == False
+    assert _is_ipaddress("127.0.0.1") == True
+    assert _is_ipaddress("::1") == True
+    assert _is_ipaddress("fe80::807c:f8ff:fea9:9f12") == True
+
+
+@pytest.mark.parametrize("result,address,resolvable", [
+    (None, None, True),
+    ("127.0.0.1", "127.0.0.1", True),
+    ("127.0.1.1", "lolo", True),
+    (None, "lolo", False),
+])
+def test_mgmt_board_data_source_management_board_ipaddress(monkeypatch, result, address, resolvable):
+    source = cmk_base.data_sources.snmp.SNMPManagementBoardDataSource("hostname", "ipaddress")
+
+    if resolvable:
+        monkeypatch.setattr(ip_lookup, "lookup_ip_address", lambda h: "127.0.1.1")
+    else:
+        def raise_exc(h):
+            raise cmk_base.exceptions.MKIPAddressLookupError("Failed to...")
+        monkeypatch.setattr(ip_lookup, "lookup_ip_address", raise_exc)
+
+    monkeypatch.setattr(config, "management_address_of", lambda h: address)
+    assert source._management_board_ipaddress("hostname") == result

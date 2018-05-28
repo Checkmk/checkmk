af6ea314ec883c0a6cb0be3287f135707b0322fd
Lars Michelsen <lm@mathias-kettner.de>
2018-05-28 09:37:00 +0200

    Cleaned up imports
    Change-Id: Iec960bb3aa395c3e6c03fb3bcb1cb427144ba99d

--- web/plugins/userdb/ldap.py
+++ web/plugins/userdb/ldap.py
@@ -46,16 +46,17 @@ import os
 import time
 import copy
 
-import config
-import watolib
-
-import cmk.paths
-import log
-
 # docs: http://www.python-ldap.org/doc/html/index.html
 import ldap
 import ldap.filter
 from ldap.controls import SimplePagedResultsControl
+
+import cmk.paths
+
+import config
+import watolib
+import log
+from lib import *
 
 # LDAP attributes are case insensitive, we only use lower case!
 # Please note: This are only default values. The user might override this

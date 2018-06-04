6305af4fb661866987fb806dde802207b49e9160
Moritz Kiemer <mo@mathias-kettner.de>
2018-06-04 14:59:10 +0200

    6199 apc_symmetra: Consider calibration status
    Users can now configure the rule "APC Symmetra Checks" to set a custom state
    for an invalid calibration status of the battery.
    The default is 'OK', which corresponds to the old behaviour.
    
    Change-Id: I782dab5544a56aa6ca37ab5a2bae27a89dbf4591

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -4885,6 +4885,105 @@ register_check_parameters(
     match_type = "first",
 )
 
+
+def transform_apc_symmetra(params):
+    if type(params) in (list, tuple):
+        params = { "levels": params }
+
+    if "levels" in params and len(params["levels"]) > 2:
+        cap = float(params["levels"][0])
+        params["capacity"] = (cap, cap)
+        del params["levels"]
+
+    if "output_load" in params:
+        del params["output_load"]
+
+    return params
+
+
+register_check_parameters(
+   subgroup_environment,
+    "apc_symentra",
+    _("APC Symmetra Checks"),
+    Transform(
+        Dictionary(
+            elements = [
+                ("capacity",
+                    Tuple(
+                        title = _("Levels of battery capacity"),
+                        elements = [
+                            Percentage(
+                                title = _("Warning below"),
+                                default_value = 95.0,
+                            ),
+                            Percentage(
+                                title = _("Critical below"),
+                                default_value = 90.0,
+                            ),
+                        ]
+                    ),
+                ),
+                ("calibration_state",
+                     MonitoringState(
+                         title = _("State if calibration is invalid"),
+                         default_value = 0,
+                     )
+                ),
+                ("post_calibration_levels",
+                Dictionary(
+                    title = _("Levels of battery parameters after calibration"),
+                    help = _("After a battery calibration the battery capacity is reduced until the "
+                             "battery is fully charged again. Here you can specify an alternative "
+                             "lower level in this post-calibration phase. "
+                             "Since apc devices remember the time of the last calibration only "
+                             "as a date, the alternative lower level will be applied on the whole "
+                             "day of the calibration until midnight. You can extend this time period "
+                             "with an additional time span to make sure calibrations occuring just "
+                             "before midnight do not trigger false alarms."
+                    ),
+                    elements = [
+                        ("altcapacity",
+                        Percentage(
+                            title = _("Alternative critical battery capacity after calibration"),
+                            default_value = 50,
+                        )),
+                        ("additional_time_span",
+                        Integer(
+                            title = ("Extend post-calibration phase by additional time span"),
+                            unit = _("minutes"),
+                            default_value = 0,
+                        )),
+                    ],
+                    optional_keys = False,
+                )),
+            ("battime",
+            Tuple(
+                title = _("Time left on battery"),
+                elements = [
+                    Age(
+                        title = _("Warning at"),
+                        help = _("Time left on Battery at and below which a warning state is triggered"),
+                        default_value = 0,
+                        display = [ "hours", "minutes" ]
+                    ),
+                    Age(
+                        title = _("Critical at"),
+                        help = _("Time Left on Battery at and below which a critical state is triggered"),
+                        default_value = 0,
+                        display = [ "hours", "minutes" ]
+                    ),
+                ],
+            ),
+        )],
+            optional_keys = ['post_calibration_levels', 'output_load', 'battime'],
+        ),
+        forth = transform_apc_symmetra,
+    ),
+    None,
+    "first"
+)
+
+
 #.
 #   .--Storage-------------------------------------------------------------.
 #   |                 ____  _                                              |
@@ -13728,95 +13827,6 @@ register_check_parameters(
     "first"
 )
 
-def transform_apc_symmetra(params):
-    if type(params) in (list, tuple):
-        params = { "levels": params }
-
-    if "levels" in params and len(params["levels"]) > 2:
-        cap = float(params["levels"][0])
-        params["capacity"] = (cap, cap)
-        del params["levels"]
-
-    if "output_load" in params:
-        del params["output_load"]
-
-    return params
-
-register_check_parameters(
-   subgroup_environment,
-    "apc_symentra",
-    _("APC Symmetra Checks"),
-    Transform(
-        Dictionary(
-            elements = [
-                ("capacity",
-                    Tuple(
-                        title = _("Levels of battery capacity"),
-                        elements = [
-                            Percentage(
-                                title = _("Warning below"),
-                                default_value = 95.0,
-                            ),
-                            Percentage(
-                                title = _("Critical below"),
-                                default_value = 90.0,
-                            ),
-                        ]
-                    ),
-                ),
-                ("post_calibration_levels",
-                Dictionary(
-                    title = _("Levels of battery parameters after calibration"),
-                    help = _("After a battery calibration the battery capacity is reduced until the "
-                             "battery is fully charged again. Here you can specify an alternative "
-                             "lower level in this post-calibration phase. "
-                             "Since apc devices remember the time of the last calibration only "
-                             "as a date, the alternative lower level will be applied on the whole "
-                             "day of the calibration until midnight. You can extend this time period "
-                             "with an additional time span to make sure calibrations occuring just "
-                             "before midnight do not trigger false alarms."
-                    ),
-                    elements = [
-                        ("altcapacity",
-                        Percentage(
-                            title = _("Alternative critical battery capacity after calibration"),
-                            default_value = 50,
-                        )),
-                        ("additional_time_span",
-                        Integer(
-                            title = ("Extend post-calibration phase by additional time span"),
-                            unit = _("minutes"),
-                            default_value = 0,
-                        )),
-                    ],
-                    optional_keys = False,
-                )),
-            ("battime",
-            Tuple(
-                title = _("Time left on battery"),
-                elements = [
-                    Age(
-                        title = _("Warning at"),
-                        help = _("Time left on Battery at and below which a warning state is triggered"),
-                        default_value = 0,
-                        display = [ "hours", "minutes" ]
-                    ),
-                    Age(
-                        title = _("Critical at"),
-                        help = _("Time Left on Battery at and below which a critical state is triggered"),
-                        default_value = 0,
-                        display = [ "hours", "minutes" ]
-                    ),
-                ],
-            ),
-        )],
-            optional_keys = ['post_calibration_levels', 'output_load', 'battime'],
-        ),
-        forth = transform_apc_symmetra,
-    ),
-    None,
-    "first"
-)
 
 register_check_parameters(
     subgroup_environment,

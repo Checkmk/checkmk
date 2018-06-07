b442d0901b567277076b11b946544a9e5822e817
Sven Panne <sp@mathias-kettner.de>
2018-06-07 10:17:48 +0200

    Fixed typo.
    Change-Id: Ifcb2e27a82f1516d56ba9c419dc264d7fcf2b10f

--- cmk_base/modes/check_mk.py
+++ cmk_base/modes/check_mk.py
@@ -989,8 +989,8 @@ modes.register(Mode(
 #   '----------------------------------------------------------------------'
 
 def mode_dump_nagios_config(args):
-    fromt cmk_base.core_nagios import create_config
-    core_nagios.create_config(sys.stdout, args if len(args) else None)
+    from cmk_base.core_nagios import create_config
+    create_config(sys.stdout, args if len(args) else None)
 
 modes.register(Mode(
     long_option="nagios-config",

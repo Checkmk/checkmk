84a59f597ffcff0f0238bceeb1d838f398e5c142
Sven Panne <sp@mathias-kettner.de>
2018-06-12 14:43:27 +0200

    Removed an import cycle related to an SNMP scan.
    This brings the number of cycles down to 6 (from 7).
    
    Change-Id: I0dde9f6272e9222ee4d242fa307640e7b750138d

--- cmk_base/discovery.py
+++ cmk_base/discovery.py
@@ -46,15 +46,14 @@ import cmk_base.ip_lookup as ip_lookup
 import cmk_base.checks as checks
 import cmk_base.check_api as check_api
 import cmk_base.item_state as item_state
-import cmk_base.snmp as snmp
 import cmk_base.checking as checking
 import cmk_base.data_sources as data_sources
 import cmk_base.check_table as check_table
 import cmk_base.core
-from cmk_base.exceptions import MKAgentError, MKParseFunctionError, \
-    MKSNMPError, MKTimeout
+from cmk_base.exceptions import MKAgentError, MKParseFunctionError, MKTimeout
 import cmk_base.cleanup
 import cmk_base.check_utils
+import cmk_base.snmpscan as snmp_scan
 
 try:
     import cmk_base.cee.keepalive as keepalive
@@ -711,7 +710,7 @@ def _get_sources_for_discovery(hostname, ipaddress, check_plugin_names, do_snmp_
             source.set_do_snmp_scan(do_snmp_scan)
             source.set_use_snmpwalk_cache(False)
             source.set_ignore_check_interval(True)
-            source.set_check_plugin_name_filter(gather_snmp_check_plugin_names)
+            source.set_check_plugin_name_filter(snmp_scan.gather_snmp_check_plugin_names)
 
     # When check types are specified via command line, enforce them and disable auto detection
     if check_plugin_names:
@@ -723,139 +722,6 @@ def _get_sources_for_discovery(hostname, ipaddress, check_plugin_names, do_snmp_
 def _get_host_sections_for_discovery(sources, use_caches):
     max_cachefile_age = config.inventory_max_cachefile_age if use_caches else 0
     return sources.get_host_sections(max_cachefile_age)
-
-
-# gather auto_discovered check_plugin_names for this host
-def gather_snmp_check_plugin_names(access_data, on_error, do_snmp_scan, for_inventory=False, for_mgmt_board=False):
-    check_plugin_names = set()
-
-    try:
-        check_plugin_names.update(snmp_scan(access_data, on_error=on_error,
-                                     do_snmp_scan=do_snmp_scan, for_inv=for_inventory,
-                                     for_mgmt_board=for_mgmt_board))
-    except Exception, e:
-        if on_error == "raise":
-            raise
-        elif on_error == "warn":
-            console.error("SNMP scan failed: %s\n" % e)
-
-    return list(check_plugin_names)
-
-
-def snmp_scan(access_data, on_error="ignore", for_inv=False, do_snmp_scan=True, for_mgmt_board=False):
-    import cmk_base.inventory_plugins as inventory_plugins
-
-    hostname = access_data["hostname"]
-
-    # Make hostname globally available for scan functions.
-    # This is rarely used, but e.g. the scan for if/if64 needs
-    # this to evaluate if_disabled_if64_checks.
-    checks.set_hostname(hostname)
-
-    snmp.initialize_single_oid_cache(access_data)
-    console.vverbose("  SNMP scan:\n")
-    if not rulesets.in_binary_hostlist(hostname, config.snmp_without_sys_descr):
-        for oid, name in [ (".1.3.6.1.2.1.1.1.0", "system description"),
-                           (".1.3.6.1.2.1.1.2.0", "system object") ]:
-            value = snmp.get_single_oid(access_data, oid, do_snmp_scan=do_snmp_scan)
-            if value == None:
-                raise MKSNMPError(
-                    "Cannot fetch %s OID %s. This might be OK for some bogus devices. "
-                    "In that case please configure the ruleset \"Hosts without system "
-                    "description OID\" to tell Check_MK not to fetch the system "
-                    "description and system object OIDs." % (name, oid))
-    else:
-        # Fake OID values to prevent issues with a lot of scan functions
-        console.vverbose("       Skipping system description OID "
-                 "(Set .1.3.6.1.2.1.1.1.0 and .1.3.6.1.2.1.1.2.0 to \"\")\n")
-        snmp.set_single_oid_cache(hostname, ".1.3.6.1.2.1.1.1.0", "")
-        snmp.set_single_oid_cache(hostname, ".1.3.6.1.2.1.1.2.0", "")
-
-    found_check_plugin_names = []
-    if for_inv:
-        items = inventory_plugins.inv_info.items()
-    else:
-        items = checks.check_info.items()
-
-    positive_found = []
-    default_found = []
-
-    for check_plugin_name, _unused_check in items:
-        if check_plugin_name in config.ignored_checktypes:
-            continue
-        else:
-            if for_inv and not inventory_plugins.is_snmp_plugin(check_plugin_name):
-                continue
-            elif not for_inv and not cmk_base.check_utils.is_snmp_check(check_plugin_name):
-                continue
-
-        section_name = cmk_base.check_utils.section_name_of(check_plugin_name)
-        # The scan function should be assigned to the section_name, because
-        # subchecks sharing the same SNMP info of course should have
-        # an identical scan function. But some checks do not do this
-        # correctly
-        if check_plugin_name in checks.snmp_scan_functions:
-            scan_function = checks.snmp_scan_functions[check_plugin_name]
-        elif section_name in checks.snmp_scan_functions:
-            scan_function = checks.snmp_scan_functions[section_name]
-        elif section_name in inventory_plugins.inv_info:
-            scan_function = inventory_plugins.inv_info[section_name].get("snmp_scan_function")
-        else:
-            scan_function = None
-
-        if scan_function:
-            try:
-                def oid_function(oid, default_value=None):
-                    value = snmp.get_single_oid(access_data, oid, check_plugin_name, do_snmp_scan=do_snmp_scan)
-                    if value == None:
-                        return default_value
-                    else:
-                        return value
-
-                result = scan_function(oid_function)
-                if result is not None and type(result) not in [ str, bool ]:
-                    if on_error == "warn":
-                        console.warning("   SNMP scan function of %s returns invalid type %s." %
-                                (check_plugin_name, type(result)))
-                    elif on_error == "raise":
-                        raise MKGeneralException("SNMP Scan aborted.")
-                elif result:
-                    found_check_plugin_names.append(check_plugin_name)
-                    positive_found.append(check_plugin_name)
-            except MKGeneralException:
-                # some error messages which we explicitly want to show to the user
-                # should be raised through this
-                raise
-            except:
-                if on_error == "warn":
-                    console.warning("   Exception in SNMP scan function of %s" % check_plugin_name)
-                elif on_error == "raise":
-                    raise
-        else:
-            found_check_plugin_names.append(check_plugin_name)
-            default_found.append(check_plugin_name)
-
-    _output_snmp_check_plugins("SNMP scan found", positive_found)
-    if default_found:
-        _output_snmp_check_plugins("SNMP without scan function", default_found)
-
-    if for_inv:
-        filtered = found_check_plugin_names
-    else:
-        filtered = checks.filter_by_management_board(hostname, found_check_plugin_names,
-                                                     for_mgmt_board, for_discovery=True)
-    _output_snmp_check_plugins("SNMP filtered check plugin names", filtered)
-    snmp.write_single_oid_cache(access_data)
-    return sorted(filtered)
-
-
-def _output_snmp_check_plugins(title, collection):
-    if collection:
-        collection_out = " ".join(sorted(collection))
-    else:
-        collection_out = "-"
-    console.vverbose("   %-35s%s%s%s%s\n" % \
-                    (title, tty.bold, tty.yellow, collection_out, tty.normal))
 
 
 def _execute_discovery(multi_host_sections, hostname, ipaddress, check_plugin_name, on_error):

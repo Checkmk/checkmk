8b676e25ebdb5c81761865bcb36405db5df6d959
Fabian Binder <fabian.binder@comnetgmbh.com>
2018-07-05 13:57:00 +0200

    Added metrics definition for aix_if checks
    Change-Id: I36e9471ad7378e478140582f95d488149af3d360

--- cmk/gui/plugins/metrics/check_mk.py
+++ cmk/gui/plugins/metrics/check_mk.py
@@ -4859,6 +4859,7 @@ check_metrics["check_mk-winperf_if"]                = if_translation
 check_metrics["check_mk-emc_vplex_if"]              = if_translation
 check_metrics["check_mk-huawei_osn_if"]             = if_translation
 check_metrics["check_mk-if_fortigate"]              = if_translation
+check_metrics["check_mk-aix_if"]                    = if_translation
 
 check_metrics["check_mk-brocade_fcport"] = {
     "in"             : { "name": "fc_rx_bytes", },

3f03b96cd2520170de80c5a9466c88d8696728f9
Lars Michelsen <lm@mathias-kettner.de>
2018-07-12 09:10:23 +0200

    Refactored cron plugins to new module hierarchy
    * Moved web/plugins/cron to cmk/gui/plugins/cron
    * Plugins are real modules now
    * Did not change plugin API yet
    
    Change-Id: Ie2f85883a39acb0f334f9f695949d673ab8b2833

--- cmk/gui/plugins/cron/user_sync.py
+++ cmk/gui/plugins/cron/user_sync.py
@@ -24,14 +24,7 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-import cmk.gui.background_job
+import cmk.gui.userdb as userdb
+from . import register_job
 
-import cmk.gui.gui_background_job as gui_background_job
-from cmk.gui.log import logger
-
-def housekeeping():
-    housekeep_classes = gui_background_job.GUIBackgroundJob.get_concrete_subclasses()
-    cmk.gui.background_job.BackgroundJobManager(logger).do_housekeeping(housekeep_classes)
-
-register_job(housekeeping)
-
+register_job(userdb.execute_userdb_job)

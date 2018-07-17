dcca09c6661ae0d047363d73ecd7ff69d3001fd0
Simon Betz <si@mathias-kettner.de>
2018-07-17 15:12:48 +0200

    6312 FIX filesystem: Fixed blemish in discovery dialog: Invalid check parameter: Undefined key 'patterns' in the dictionary
    Change-Id: I105842d94e96118a065df6b550ada4279f8e30ae

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -6536,6 +6536,7 @@ register_check_parameters(
     Dictionary(
         elements = filesystem_elements,
         hidden_keys = ["flex_levels"],
+        ignored_keys = ["patterns"],
     ),
     TextAscii(
         title = _("Mount point"),

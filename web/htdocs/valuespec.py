af7a1fb2a18d00c374831bcffd7b3f2624650b17
Lars Michelsen <lm@mathias-kettner.de>
2018-07-18 21:24:23 +0200

    6354 FIX Timespecific parameters: List of timeperiods is now sorted
    Change-Id: I874004bb32d9b0d913349784c16e0269388e3173

--- web/htdocs/valuespec.py
+++ web/htdocs/valuespec.py
@@ -4632,7 +4632,7 @@ class TimeperiodSelection(DropdownChoice):
         if always not in elements:
             elements.insert(0, always)
 
-        return elements
+        return sorted(elements, key=lambda x: x[1].lower())
 
 
 class TimeperiodValuespec(ValueSpec):

d49e5d4b264f32fa8a6251c53214754f199f15d2
Lars Michelsen <lm@mathias-kettner.de>
2018-07-23 15:05:31 +0200

    6363 FIX Fixed "host_inventory" exception when using "Has inventory data" filter
    When using the filter "Has inventory data" in a host view an exception like
    this could occur: "Internal error: 'host_inventory'"
    
    Change-Id: I4ed0023993db4588b074d8f27bceb9b6c6e0b3cb

--- web/plugins/visuals/inventory.py
+++ web/plugins/visuals/inventory.py
@@ -444,6 +444,9 @@ class FilterHasInventory(FilterTristate):
     def __init__(self):
         FilterTristate.__init__(self, "has_inv", _("Has Inventory Data"), "host", "host_inventory")
 
+    def need_inventory(self):
+        return True
+
     def filter(self, infoname):
         return "" # No Livestatus filtering right now
 

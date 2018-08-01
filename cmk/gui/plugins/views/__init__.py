b5530be32b18c9f95cb3414a2e08fd799b9ed0b3
Lars Michelsen <lm@mathias-kettner.de>
2018-08-01 17:28:08 +0200

    Cleaned up star import for view plugins
    This helps pylint
    
    Change-Id: I18ed1c36ddd0db59b0d2b991e66d08e10992fbaa

--- cmk/gui/plugins/views/__init__.py
+++ cmk/gui/plugins/views/__init__.py
@@ -40,10 +40,53 @@ __all__ = [ os.path.basename(f)[:-3] for f in modules if f not in [ "__init__.py
 #   |                         |___/                                        |
 #   '----------------------------------------------------------------------'
 
-# TODO: Would be better to replace this star import with an explicit list of
-# names needed for the plugins. Then we would have something like an official
-# plugin API. At least a list of names that are intended to be used by plugins.
-from cmk.gui.plugins.views.utils import *
+from cmk.gui.plugins.views.utils import (
+    cmp_custom_variable,
+    cmp_ip_address,
+    cmp_num_split,
+    cmp_service_name_equiv,
+    cmp_simple_number,
+    cmp_simple_string,
+    cmp_string_list,
+    declare_1to1_sorter,
+    declare_simple_sorter,
+    display_options,
+    do_query_data,
+    EmptyCell,
+    format_plugin_output,
+    get_graph_timerange_from_painter_options,
+    get_host_tags,
+    get_perfdata_nth_value,
+    get_tag_group,
+    group_value,
+    inventory_displayhints,
+    is_stale,
+    join_row,
+    link_to_view,
+    multisite_builtin_views,
+    multisite_commands,
+    multisite_datasources,
+    multisite_layouts,
+    multisite_painter_options,
+    multisite_painters,
+    multisite_sorters,
+    output_csv_headers,
+    paint_age,
+    painter_options,
+    paint_host_list,
+    paint_nagiosflag,
+    paint_stalified,
+    pnp_url,
+    query_data,
+    register_command_group,
+    render_cache_info,
+    replace_action_url_macros,
+    row_id,
+    transform_action_url,
+    url_to_view,
+    view_is_enabled,
+    view_title,
+)
 
 #.
 #   .--Plugins-------------------------------------------------------------.

01c3978de28515a97c9b7787a82042344ae629a7
Moritz Kiemer <mo@mathias-kettner.de>
2018-08-10 11:17:21 +0200

    windows_mulipath: Neither crash nor discover anything upon invalid agent output
    FEED-857
    
    Change-Id: I9e859900396c96ee808a39440e34f512784c726d

---
+++ tests/unit/checks/generictests/datasets/windows_multipath_1.py
@@ -0,0 +1,15 @@
+
+
+checkname = 'windows_multipath'
+
+
+info = [[u'C:\\Program',
+         u'Files',
+         u'(x86)\\check_mk\\plugins\\windows_multipath.vbs(19,',
+         u'1)',
+         u'(null):',
+         u'0x80041010']]
+
+
+discovery = {'': []}
+

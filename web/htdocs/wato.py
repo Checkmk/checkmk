14da6c7bdd35d85386c648f307139cda586edcd0
Lars Michelsen <lm@mathias-kettner.de>
2018-08-16 13:13:30 +0200

    6289 FIX User profile: Theme changes are applied to result page automatically
    The user profile page did not apply changed themes to
    the result page right after changing the theme. The user
    always had to reload the page to make the content frame
    use the new theme.
    
    Change-Id: I3378a8d3b395007f18a71ddaeec3399ac32d5592

--- web/htdocs/wato.py
+++ web/htdocs/wato.py
@@ -15188,6 +15188,8 @@ def page_user_profile(change_pw=False):
             html.http_redirect(html.var('_origtarget', 'index.py'))
         else:
             html.message(_("Successfully updated user profile."))
+            # Ensure theme changes are applied without additional user interaction
+            html.immediate_browser_redirect(0.5, html.makeuri([]))
 
     if html.has_user_errors():
         html.show_user_errors()

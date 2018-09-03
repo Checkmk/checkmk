8584b711d859f354ff74495b0f045f0fd2bae1fd
Moritz Kiemer <mo@mathias-kettner.de>
2018-09-03 15:20:59 +0200

    6795 msexch_database: plugin now runs on german client systems
    Previously the msexch_database plugin could not be used on
    german windows systems.
    
    Change-Id: Ie3ea89efee49d4f42ac1b63974d1229aa5e068a3

--- agents/windows/plugins/msexch_database.ps1
+++ agents/windows/plugins/msexch_database.ps1
@@ -1,5 +1,12 @@
 # MS Exchange Database counters
 
-Write-Host "<<<msexch_database:sep(59)>>>"
+# localize counter name
+$locale = ([System.Globalization.Cultureinfo]::CurrentCulture.name)
+switch -wildcard($locale){
+    "en-*" {$counter_name = "\MSExchange Database ==> Instances(*)\*"}
+    "de-*" {$counter_name = "\MSExchange-Datenbank  ==> Instanzen(*)\*"}
+}
 
-Get-Counter -Counter "\MSExchange Database ==> Instances(*)\*" | % {$_.CounterSamples} | Select path,cookedvalue | ConvertTo-CSV -NoTypeInformation -Delimiter ";"
+Write-Host "<<<msexch_database:sep(59)>>>"
+Write-Host "locale;"$locale
+Get-Counter -Counter $counter_name | % {$_.CounterSamples} | Select path,cookedvalue | ConvertTo-CSV -NoTypeInformation -Delimiter ";"

5a7d4a72b6c0fd65c009c7362b24fa13ed052cd3
Tom Baerwinkel <tb@mathias-kettner.de>
2018-09-07 12:28:58 +0200

    Add tests for WMI timeouts
    Change-Id: I1edb4f9bdc23dabfbd4ffe25e5795b01be061a81

---
+++ tests/unit/checks/test_wmi_check.py
@@ -0,0 +1,41 @@
+import pytest
+
+from checktestlib import (
+    DiscoveryResult,
+    CheckResult,
+    assertDiscoveryResultsEqual,
+)
+
+from cmk_base.check_api import MKCounterWrapped
+
+pytestmark = pytest.mark.checks
+
+info_wmi_timeout = [[u'WMItimeout']]
+
+info_subsection_wmi_timeout = [
+    [u'[system_perf]'],
+    [u'WMItimeout'],
+    [u'[computer_system]'],
+    [u'name', u'unimportant', u'data'],
+]
+
+
+@pytest.mark.parametrize("check_name,info,expected", [
+    ('wmi_webservices', info_wmi_timeout, []),
+    ('wmi_cpuload', info_subsection_wmi_timeout, []),
+])
+def test_wmi_cpu_load_discovery(check_manager, check_name, info, expected):
+    check = check_manager.get_check(check_name)
+    discovery_result = DiscoveryResult(check.run_discovery(check.run_parse(info)))
+    discovery_expected = DiscoveryResult(expected)
+    assertDiscoveryResultsEqual(discovery_result, discovery_expected)
+
+
+@pytest.mark.parametrize("check_name,info,expected", [
+    ('wmi_webservices', info_wmi_timeout, None),
+    ('wmi_cpuload', info_subsection_wmi_timeout, None),
+])
+def test_wmi_cpuload_timeout_exceptions(check_manager, check_name, info, expected):
+    check = check_manager.get_check(check_name)
+    with pytest.raises(MKCounterWrapped):
+        CheckResult(check.run_check(None, {}, check.run_parse(info)))

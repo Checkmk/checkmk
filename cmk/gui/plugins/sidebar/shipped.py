a0ce03ab00b9d67d3ff3137bcdc1060b4ee91f10
Lars Michelsen <lm@mathias-kettner.de>
2018-09-13 20:32:35 +0200

    6610 SEC Fixed possible XSS using the dokuwiki snapin
    The content of the DokuWiki page named "sidebar" was inserted into the DokuWiki
    view of Check_MK, but was is not correctly sanitized. This can only be done by
    an administrator of the page, but every user who can access the DokuWiki view
    was affected by the vulnerability.
    
    Change-Id: I6c36e9d0459465257f840a2b6220f775c9d23541

--- cmk/gui/plugins/sidebar/shipped.py
+++ cmk/gui/plugins/sidebar/shipped.py
@@ -675,7 +675,7 @@ def render_wiki():
                     bulletlink(name, "/%s/wiki/doku.php?id=%s" % (config.omd_site(), link))
 
             else:
-                html.write(line)
+                html.write_text(line)
 
         if ul_started == True:
             html.close_ul()

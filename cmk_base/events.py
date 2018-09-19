5e9402c389bc089cd6ee9e05729db10f5db74d1e
Óscar Nájera <on@mathias-kettner.de>
2018-09-19 16:02:38 +0200

    6640 FIX Fixed context for HOSTURL and SERVICEURL
    Links to services and hosts URL missed the site key.
    
    Change-Id: I18dadcc47e07c61a24f4bcf384a9193ebdb7cb3f

--- cmk_base/events.py
+++ cmk_base/events.py
@@ -298,11 +298,11 @@ def complete_raw_context(raw_context, with_dump, event_log):
             raw_context["MICROTIME"] = "%d" % (time.time()*1000000)
 
         raw_context['HOSTURL'] = '/check_mk/index.py?start_url=%s' % \
-                            urllib.quote('view.py?view_name=hoststatus&host=%s' % raw_context['HOSTNAME'])
+                            urllib.quote('view.py?view_name=hoststatus&host=%s&site=%s' % (raw_context['HOSTNAME'], raw_context['OMD_SITE']))
         if raw_context['WHAT'] == 'SERVICE':
             raw_context['SERVICEURL'] = '/check_mk/index.py?start_url=%s' % \
-                                        urllib.quote('view.py?view_name=service&host=%s&service=%s' %
-                                                     (raw_context['HOSTNAME'], raw_context['SERVICEDESC']))
+                                        urllib.quote('view.py?view_name=service&host=%s&service=%s&site=%s' %
+                                                     (raw_context['HOSTNAME'], raw_context['SERVICEDESC'], raw_context['OMD_SITE']))
 
         # Relative Timestamps for several macros
         for macro in [ 'LASTHOSTSTATECHANGE', 'LASTSERVICESTATECHANGE', 'LASTHOSTUP', 'LASTSERVICEOK' ]:

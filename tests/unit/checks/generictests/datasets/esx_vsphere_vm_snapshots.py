e1c5cff5d9698d90f13de3f8fba0277ec28788dd
Moritz Kiemer <mo@mathias-kettner.de>
2018-09-26 12:11:24 +0200

    esx_vsphere_vms: clean up snapshot check
    Change-Id: I316669110c7ce1036ba5fdb70bd9a6507eb5c24e

---
+++ tests/unit/checks/generictests/datasets/esx_vsphere_vm_snapshots.py
@@ -0,0 +1,36 @@
+
+
+checkname = 'esx_vsphere_vm'
+
+
+info = [['snapshot.rootSnapshotList',
+         '1',
+         '1363596734',
+         'poweredOff',
+         '20130318_105600_snapshot_LinuxI|2',
+         '1413977827',
+         'poweredOn',
+         'LinuxI',
+         'Testsnapshot']]
+
+
+discovery = {'cpu': [],
+             'datastores': [],
+             'guest_tools': [],
+             'heartbeat': [],
+             'mem_usage': [],
+             'mounted_devices': [],
+             'name': [],
+             'running_on': [],
+             'snapshots': [(None, {})]}
+
+
+checks = {
+    'snapshots': [
+        (None, 'default', [(0, 'Number of Snapshots 2', []),
+                           (0, 'Powered On: LinuxI Testsnapshot', []),
+                           (0, 'Latest Snapshot: LinuxI Testsnapshot 2014-10-22 13:37:07', []),
+                           (0, 'Oldest Snapshot: 20130318_105600_snapshot_LinuxI 2013-03-18 09:52:14', []),
+                          ]),
+    ]
+}

1c2dd5b2d26fbe7a24cd385911d1498ca9648d5d
Lars Michelsen <lm@mathias-kettner.de>
2018-09-26 10:38:39 +0200

    Add missing log_level setting during tests
    Change-Id: If9df26967f3c2291c044a79db076e1d5b106bf26

--- cmk/ec/main.py
+++ cmk/ec/main.py
@@ -3917,9 +3917,10 @@ def load_configuration(settings, logger, slave_status):
         levels = config["log_level"]
         logger.setLevel(levels["cmk.mkeventd"])
         logger.getChild("EventServer").setLevel(levels["cmk.mkeventd.EventServer"])
+        if "cmk.mkeventd.EventServer.snmp" in levels:
+            logger.getChild("EventServer.snmp").setLevel(levels["cmk.mkeventd.EventServer.snmp"])
         logger.getChild("EventStatus").setLevel(levels["cmk.mkeventd.EventStatus"])
         logger.getChild("StatusServer").setLevel(levels["cmk.mkeventd.StatusServer"])
-        logger.getChild("EventServer.snmp").setLevel(levels["cmk.mkeventd.EventServer.snmp"])
         logger.getChild("lock").setLevel(levels["cmk.mkeventd.lock"])
 
     # Are we a replication slave? Parts of the configuration

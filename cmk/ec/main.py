48e1125fe3e3a68095eab71876158e6ea50ce41e
Lars Michelsen <lm@mathias-kettner.de>
2018-09-26 09:37:02 +0200

    Renamed new SNMP logger to cmk.mkeventd.EventServer.snmp
    Change-Id: I7e45b36894d24a76cf27b3949c5ec2f841ce7a67

--- cmk/ec/main.py
+++ cmk/ec/main.py
@@ -1297,7 +1297,7 @@ class EventServer(ECServerThread):
         self._syslog_tcp = None
         self._snmptrap = None
         self._mib_resolver = None
-        self._snmp_logger = logger.getChild("snmp")
+        self._snmp_logger = self._logger.getChild("snmp")
 
         self._rules = []
         self._hash_stats = []
@@ -5049,9 +5049,9 @@ def load_configuration(settings, slave_status):
         levels = config["log_level"]
         logger.setLevel(levels["cmk.mkeventd"])
         logger.getChild("EventServer").setLevel(levels["cmk.mkeventd.EventServer"])
+        logger.getChild("EventServer.snmp").setLevel(levels["cmk.mkeventd.EventServer.snmp"])
         logger.getChild("EventStatus").setLevel(levels["cmk.mkeventd.EventStatus"])
         logger.getChild("StatusServer").setLevel(levels["cmk.mkeventd.StatusServer"])
-        logger.getChild("snmp").setLevel(levels["cmk.mkeventd.snmp"])
         logger.getChild("lock").setLevel(levels["cmk.mkeventd.lock"])
 
     # Configure the auto deleting indexes in the DB when mongodb is enabled

4d1e5f097fafaa6790c0e83b09670726925d029a
Moritz Kiemer <mo@mathias-kettner.de>
2018-09-26 07:46:06 +0200

    fileinfo: Fix bug in agents and make parse_function more robust
    Change-Id: I558e0da2db7694f24421118f1713965807640788

---
+++ tests/unit/checks/generictests/datasets/fileinfo_new_empty.py
@@ -0,0 +1,15 @@
+
+
+checkname = 'fileinfo'
+
+
+info = [
+    ['1536557964'],
+    ['[[[header]]]'],
+    ['name', 'status', 'size', 'time'],
+    ['[[[content]]]'],
+]
+
+
+discovery = {'': [], 'groups': []}
+

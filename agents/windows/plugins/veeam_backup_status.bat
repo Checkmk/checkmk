7a2d7fe3696d8aae394660cb0d3767f8c8c1556c
Simon Betz <si@mathias-kettner.de>
2018-09-28 14:51:35 +0200

    6733 FIX veeam_backup_status: Plugin is bakeable
    Change-Id: I2ab4de22a0d7d18225124e07b836536da2dbe98a

--- agents/windows/plugins/veeam_backup_status.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM version 0.9
-REM Put this file in cmk Plugins-Folder *only* if you need to run
-REM the veeam_backup_status.ps1 powershell script and you
-REM have no 64 bit check_mk agent available
-REM In this case the powershell script needs to be put somewhere else
-REM (see example here) and is called from this .bat script with the 64 bit powershell
-%systemroot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Unrestricted " & ""C:\skripte\veeam_backup_status.ps1"""

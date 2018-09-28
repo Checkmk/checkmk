250af3c3f07b0c1c3ef784db6a9f376784ac0f97
Moritz Kiemer <mo@mathias-kettner.de>
2018-09-28 11:25:45 +0200

    6789 esx_vsphere_sensors: List sensors in long output
    Also major cleanup and some tests.
    
    Change-Id: I97417ea1068e4c6157184849407013c3b8159c9c

---
+++ tests/unit/checks/generictests/datasets/esx_vsphere_sensors_2.py
@@ -0,0 +1,16 @@
+
+
+checkname = 'esx_vsphere_sensors'
+
+
+info = [
+    ['Dummy sensor', '', '', '', '', '', 'green', 'all is good', 'the sun is shining']
+]
+
+
+discovery = {'': [(None, [])]}
+
+
+checks = {'': [(None, 'default', [(0, ('All sensors are in normal state\n'
+                                       'Sensors operating normal are:\n'
+                                       'Dummy sensor: all is good (the sun is shining)'), [])])]}

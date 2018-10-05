426b1178f2047ed73ac1b727854834c52a9a29fc
Konstantin Büttner <kb@mathias-kettner.de>
2018-10-05 10:55:04 +0200

    kernel.util: New test case for unit test
    Change-Id: Ie92fce141114f6c778fd14e046cec8104686c10b

--- tests/unit/checks/test_kernel_check.py
+++ tests/unit/checks/test_kernel_check.py
@@ -7,6 +7,7 @@ pytestmark = pytest.mark.checks
 
 @pytest.mark.parametrize("time_to_info,params,predicate", [
 (lambda t: [["cpu", 15*t, 10*t, 5*t, 5*t, 5*t, 5*t, 5*t]], {}, lambda cr: True),
+(lambda t: [["cpu", 15*t, 10*t, 5*t, 5*t, 5*t, 5*t, 5*t]], {'core_util_graph': True, 'iowait': (30.0, 50.0)}, lambda cr: True),
 ])
 def test_kernel_util_check(check_manager, time_to_info, params, predicate):
     check = check_manager.get_check("kernel.util")

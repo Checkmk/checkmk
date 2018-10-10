9776206aaed49358d7c9005b4818da9795611759
Lars Michelsen <lm@mathias-kettner.de>
2018-10-10 08:44:06 +0200

    Use correct versions for docker tests in 1.5.0 branch
    Change-Id: I67e2e58682ade51539c086ce7d65bdbb581fde7f

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -282,7 +282,7 @@ def test_update(request, client, version):
     # Pick a random old version that we can use to the setup the initial site with
     # Later this site is being updated to the current daily build
     old_version = testlib.CMKVersion(
-        version="1.5.0p3",
+        version="1.5.0p5",
         edition=testlib.CMKVersion.CEE,
         branch="1.5.0",
     )

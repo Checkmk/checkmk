558b86c83900bc0f110527b3f3d7421f2eafb2fb
mpana <mp@spearhead.systems>
2018-10-11 12:16:38 +0300

    new nfsiostat plugin
    Change-Id: I9327b1209986d7f4c6118c864c36b4578033f5b9

--- cmk/gui/plugins/metrics/check_mk.py
+++ cmk/gui/plugins/metrics/check_mk.py
@@ -294,6 +294,13 @@ unit_info["rpm"] = {
     "render": lambda v: cmk.render.physical_precision(v, 4, _("rpm")),
 }
 
+unit_info['bytes/op'] = {
+    'title': _('Read size per operation'),
+    'unit': 'bytes/op',
+    'color': '#4080c0',
+    "render": bytes_human_readable,
+}
+
 #.
 #   .--Metrics-------------------------------------------------------------.
 #   |                   __  __      _        _                             |
@@ -4464,6 +4471,86 @@ metric_info["connections_perc_used"] = {
     "title": _("Parallel connections load"),
     "unit": "%",
     "color": "21/a",
+}
+
+metric_info['op_s'] = {
+    'title': _('Operations per second'),
+    'unit': 'count',
+    'color': '#90ee90',
+}
+
+metric_info['rpc_backlog'] = {
+    'title': _('RPC Backlog'),
+    'unit': 'count',
+    'color': '#90ee90',
+}
+
+metric_info['read_ops'] = {
+    'title': _('Read operations'),
+    'unit': '1/s',
+    'color': '34/a',
+}
+
+metric_info['read_b_s'] = {
+    'title': _('Read size per second'),
+    'unit': 'bytes/s',
+    'color': '#80ff20',
+}
+
+metric_info['read_b_op'] = {
+    'title': _('Read size per operation'),
+    'unit': 'bytes/op',
+    'color': '#4080c0',
+    "render"   : bytes_human_readable,
+}
+
+metric_info['read_retrans'] = {
+    'title': _('Read retransmission'),
+    'unit': '%',
+    'color': '#90ee90',
+}
+
+metric_info['read_avg_rtt_ms'] = {
+    'title': _('Read average rtt'),
+    'unit': 's',
+    'color': '#90ee90',
+}
+
+metric_info['read_avg_exe_ms'] = {
+    'title': _('Read average exe'),
+    'unit': 's',
+    'color': '#90ee90',
+}
+
+metric_info['write_ops_s'] = {
+    'title': _('Write operations'),
+    'unit': '1/s',
+    'color': '34/a',
+}
+
+metric_info['write_b_s'] = {
+    'title': _('Writes size per second'),
+    'unit': 'bytes/s',
+    'color': '#80ff20',
+}
+
+metric_info['write_b_op'] = {
+    'title': _('Writes size per operation'),
+    'unit': 'bytes/op',
+    'color': '#4080c0',
+    "render"   : bytes_human_readable,
+}
+
+metric_info['write_avg_rtt_ms'] = {
+    'title': _('Write average rtt'),
+    'unit': 's',
+    'color': '#90ee90',
+}
+
+metric_info['write_avg_exe_ms'] = {
+    'title': _('Write average exe'),
+    'unit': 's',
+    'color': '#90ee90',
 }
 
 #.

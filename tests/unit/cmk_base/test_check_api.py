34a1a70830148ffda9e3ef5c44aeacfcc21c7168
Kenneth Okoh <ko@mathias-kettner.de>
2018-10-12 14:42:00 +0200

    Check function decorator: Adjusted for item in parsed but parsed[item] empty
    Change-Id: I18d97315821b349fb8bd398ef8e49eb0015b7db9

--- tests/unit/cmk_base/test_check_api.py
+++ tests/unit/cmk_base/test_check_api.py
@@ -12,10 +12,13 @@ def check_foo(item, params, parsed_item_data):
 
 def test_get_parsed_item_data():
     params = {}
-    parsed = {1: "one"}
+    parsed = {1: "one", 3: {}, 4: [], 5: ""}
     info = [[1, "one"], [2, "two"]]
     assert check_foo(1, params, parsed) == (2, "bar")
     assert check_foo(2, params, parsed) == None
+    assert check_foo(3, params, parsed) == None
+    assert check_foo(4, params, parsed) == None
+    assert check_foo(5, params, parsed) == None
     assert check_foo(1, params, info) == (3, "Wrong usage of decorator function 'get_parsed_item_data': parsed is not a dict")
     assert check_foo.__name__ == "check_foo"
 

747d4d5aabfc1c009adfe67879bb678d1b9bb403
Lars Michelsen <lm@mathias-kettner.de>
2018-10-17 09:29:54 +0200

    Moved multiple modes to dedicated modules
    Change-Id: I9a6e1e8edaf8a6e7c4f682641df40ca7bcb98367

---
+++ cmk/gui/wato/pages/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+"""Main entry page for configuration of global variables, rules, groups,
+timeperiods, users, etc."""
+
+from cmk.gui.i18n import _
+
+from cmk.gui.plugins.wato.utils.main_menu import (
+    MainMenu,
+    get_modules,
+)
+
+from cmk.gui.plugins.wato import (
+    WatoMode,
+    mode_registry,
+    changelog_button,
+)
+
+@mode_registry.register
+class ModeMain(WatoMode):
+    @classmethod
+    def name(cls):
+        return "main"
+
+
+    @classmethod
+    def permissions(cls):
+        return []
+
+
+    def title(self):
+        return _("WATO - Check_MK's Web Administration Tool")
+
+
+    def buttons(self):
+        changelog_button()
+
+
+    def page(self):
+        MainMenu(get_modules()).show()
---
+++ cmk/gui/wato/pages/object_parameters.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+"""Mode for displaying and modifying the rule based host and service
+parameters. This is a host/service overview page over all things that can be
+modified via rules."""
+
+import cmk.gui.config as config
+import cmk.gui.watolib as watolib
+import cmk.gui.forms as forms
+from cmk.gui.i18n import _
+from cmk.gui.globals import html
+from cmk.gui.exceptions import MKUserError
+
+from cmk.gui.plugins.wato import (
+    WatoMode,
+    mode_registry,
+)
+
+from cmk.gui.plugins.wato.utils.context_buttons import (
+    host_status_button,
+    service_status_button,
+)
+
+@mode_registry.register
+class ModeObjectParameters(WatoMode):
+    _PARAMETERS_UNKNOWN = []
+    _PARAMETERS_OMIT = []
+
+    @classmethod
+    def name(cls):
+        return "object_parameters"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["hosts", "rulesets"]
+
+
+    def _from_vars(self):
+        self._hostname = html.get_ascii_input("host") # may be empty in new/clone mode
+        self._host = watolib.Folder.current().host(self._hostname)
+        if self._host is None:
+            raise MKUserError("host", _('The given host does not exist.'))
+        self._host.need_permission("read")
+
+        # TODO: Validate?
+        self._service = html.get_unicode_input("service")
+
+
+    def title(self):
+        title = _("Parameters of") + " " + self._hostname
+        if self._service:
+            title += " / " + self._service
+        return title
+
+
+    def buttons(self):
+        if self._service:
+            prefix = _("Host-")
+        else:
+            prefix = u""
+        html.context_button(_("Folder"), watolib.folder_preserving_link([("mode", "folder")]), "back")
+        if self._service:
+            service_status_button(self._hostname, self._service)
+        else:
+            host_status_button(self._hostname, "hoststatus")
+        html.context_button(prefix + _("Properties"), watolib.folder_preserving_link([("mode", "edit_host"), ("host", self._hostname)]), "edit")
+        html.context_button(_("Services"), watolib.folder_preserving_link([("mode", "inventory"), ("host", self._hostname)]), "services")
+        if not self._host.is_cluster():
+            html.context_button(prefix + _("Diagnostic"),
+              watolib.folder_preserving_link([("mode", "diag_host"), ("host", self._hostname)]), "diagnose")
+
+
+    def page(self):
+        all_rulesets = watolib.AllRulesets()
+        all_rulesets.load()
+
+        # For services we make a special handling the for origin and parameters
+        # of that service!
+        if self._service:
+            self._show_service_rules(all_rulesets)
+
+
+        last_maingroup = None
+        for groupname in sorted(watolib.g_rulespecs.get_host_groups()):
+            maingroup = groupname.split("/")[0]
+            for rulespec in sorted(watolib.g_rulespecs.get_by_group(groupname), key = lambda x: x.title):
+                if (rulespec.item_type == 'service') == (not self._service):
+                    continue # This rule is not for hosts/services
+
+                # Open form for that group here, if we know that we have at least one rule
+                if last_maingroup != maingroup:
+                    last_maingroup = maingroup
+                    rulegroup = watolib.get_rulegroup(maingroup)
+                    forms.header(rulegroup.title, isopen = maingroup == "monconf", narrow=True, css="rulesettings")
+                    html.help(rulegroup.help)
+
+                self._output_analysed_ruleset(all_rulesets, rulespec, self._service)
+
+        forms.end()
+
+
+    def _show_service_rules(self, all_rulesets):
+        serviceinfo = watolib.check_mk_automation(self._host.site_id(), "analyse-service", [self._hostname, self._service])
+        if not serviceinfo:
+            return
+
+        forms.header(_("Check origin and parameters"), isopen = True, narrow=True, css="rulesettings")
+        origin = serviceinfo["origin"]
+        origin_txt = {
+            "active"  : _("Active check"),
+            "static"  : _("Manual check"),
+            "auto"    : _("Inventorized check"),
+            "classic" : _("Classical check"),
+        }[origin]
+        self._render_rule_reason(_("Type of check"), None, "", "", False, origin_txt)
+
+        # First case: discovered checks. They come from var/check_mk/autochecks/HOST.
+        if origin ==  "auto":
+            checkgroup = serviceinfo["checkgroup"]
+            checktype = serviceinfo["checktype"]
+            if not checkgroup:
+                self._render_rule_reason(_("Parameters"), None, "", "", True, _("This check is not configurable via WATO"))
+
+            # Logwatch needs a special handling, since it is not configured
+            # via checkgroup_parameters but via "logwatch_rules" in a special
+            # WATO module.
+            elif checkgroup == "logwatch":
+                rulespec = watolib.g_rulespecs.get("logwatch_rules")
+                self._output_analysed_ruleset(all_rulesets, rulespec,
+                                        serviceinfo["item"], serviceinfo["parameters"])
+
+            else:
+                # Note: some discovered checks have a check group but
+                # *no* ruleset for discovered checks. One example is "ps".
+                # That can be configured as a manual check or created by
+                # inventory. But in the later case all parameters are set
+                # by the inventory. This will be changed in a later version,
+                # but we need to address it anyway.
+                grouprule = "checkgroup_parameters:" + checkgroup
+                if not watolib.g_rulespecs.exists(grouprule):
+                    try:
+                        rulespec = watolib.g_rulespecs.get("static_checks:" + checkgroup)
+                    except KeyError:
+                        rulespec = None
+
+                    if rulespec:
+                        url = watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', "static_checks:" + checkgroup), ('host', self._hostname)])
+                        self._render_rule_reason(_("Parameters"), url, _("Determined by discovery"), None, False,
+                                   rulespec.valuespec._elements[2].value_to_text(serviceinfo["parameters"]))
+                    else:
+                        self._render_rule_reason(_("Parameters"), None, "", "", True, _("This check is not configurable via WATO"))
+
+                else:
+                    rulespec = watolib.g_rulespecs.get(grouprule)
+                    self._output_analysed_ruleset(all_rulesets, rulespec,
+                                            serviceinfo["item"], serviceinfo["parameters"])
+
+        elif origin == "static":
+            checkgroup = serviceinfo["checkgroup"]
+            checktype = serviceinfo["checktype"]
+            if not checkgroup:
+                html.write_text(_("This check is not configurable via WATO"))
+            else:
+                rulespec = watolib.g_rulespecs.get("static_checks:" + checkgroup)
+                itemspec = rulespec.item_spec
+                if itemspec:
+                    item_text = itemspec.value_to_text(serviceinfo["item"])
+                    title = rulespec.item_spec.title()
+                else:
+                    item_text = serviceinfo["item"]
+                    title = _("Item")
+                self._render_rule_reason(title, None, "", "", False, item_text)
+                self._output_analysed_ruleset(all_rulesets, rulespec,
+                                        serviceinfo["item"], self._PARAMETERS_OMIT)
+                html.write(rulespec.valuespec._elements[2].value_to_text(serviceinfo["parameters"]))
+                html.close_td()
+                html.close_tr()
+                html.close_table()
+
+
+        elif origin == "active":
+            checktype = serviceinfo["checktype"]
+            rulespec = watolib.g_rulespecs.get("active_checks:" + checktype)
+            self._output_analysed_ruleset(all_rulesets, rulespec, None, serviceinfo["parameters"])
+
+        elif origin == "classic":
+            rule_nr  = serviceinfo["rule_nr"]
+            rules    = all_rulesets.get("custom_checks").get_rules()
+            rule_folder, rule_index, _rule = rules[rule_nr]
+
+            url = watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', "custom_checks"), ('host', self._hostname)])
+            forms.section(html.render_a(_("Command Line"), href=url))
+            url = watolib.folder_preserving_link([
+                ('mode', 'edit_rule'),
+                ('varname', "custom_checks"),
+                ('rule_folder', rule_folder.path()),
+                ('rulenr', rule_index),
+                ('host', self._hostname)])
+
+            html.open_table(class_="setting")
+            html.open_tr()
+
+            html.open_td(class_="reason")
+            html.a("%s %d %s %s" % (_("Rule"), rule_index + 1, _("in"), rule_folder.title()), href=url)
+            html.close_td()
+            html.open_td(class_=["settingvalue", "used"])
+            if "command_line" in serviceinfo:
+                html.tt(serviceinfo["command_line"])
+            else:
+                html.write_text(_("(no command line, passive check)"))
+            html.close_td()
+
+            html.close_tr()
+            html.close_table()
+
+
+    def _render_rule_reason(self, title, title_url, reason, reason_url, is_default, setting):
+        if title_url:
+            title = html.render_a(title, href=title_url)
+        forms.section(title)
+
+        if reason:
+            reason = html.render_a(reason, href=reason_url)
+
+        html.open_table(class_="setting")
+        html.open_tr()
+        if is_default:
+            html.td(html.render_i(reason), class_="reason")
+            html.td(setting, class_=["settingvalue", "unused"])
+        else:
+            html.td(reason, class_="reason")
+            html.td(setting, class_=["settingvalue", "used"])
+        html.close_tr()
+        html.close_table()
+
+
+    def _output_analysed_ruleset(self, all_rulesets, rulespec, service, known_settings=None):
+        if known_settings is None:
+            known_settings = self._PARAMETERS_UNKNOWN
+
+        def rule_url(rule):
+            return watolib.folder_preserving_link([
+                ('mode',        'edit_rule'),
+                ('varname',     varname),
+                ('rule_folder', rule.folder.path()),
+                ('rulenr',      rule.index()),
+                ('host',        self._hostname),
+                ('item',        watolib.mk_repr(service) if service else ''),
+            ])
+
+        varname = rulespec.name
+        valuespec = rulespec.valuespec
+
+        url = watolib.folder_preserving_link([
+            ('mode', 'edit_ruleset'),
+            ('varname', varname),
+            ('host', self._hostname),
+            ('item', watolib.mk_repr(service)),
+        ])
+
+        forms.section(html.render_a(rulespec.title, url))
+
+        ruleset = all_rulesets.get(varname)
+        setting, rules = ruleset.analyse_ruleset(self._hostname, service)
+
+        html.open_table(class_="setting")
+        html.open_tr()
+        html.open_td(class_="reason")
+
+        # Show reason for the determined value
+        if len(rules) == 1:
+            rule_folder, rule_index, rule = rules[0]
+            url = rule_url(rule)
+            html.a(_("Rule %d in %s") % (rule_index + 1, rule_folder.title()), href=rule_url(rule))
+
+        elif len(rules) > 1:
+            html.a("%d %s" % (len(rules), _("Rules")), href=url)
+
+        else:
+            html.i(_("Default Value"))
+        html.close_td()
+
+        # Show the resulting value or factory setting
+        html.open_td(class_=["settingvalue", "used" if len(rules) > 0 else "unused"])
+
+        # In some cases we now the settings from a check_mk automation
+        if known_settings is self._PARAMETERS_OMIT:
+            return
+
+        # Special handling for logwatch: The check parameter is always None. The actual
+        # patterns are configured in logwatch_rules. We do not have access to the actual
+        # patterns here but just to the useless "None". In order not to complicate things
+        # we simply display nothing here.
+        elif varname == "logwatch_rules":
+            pass
+
+        elif known_settings is not self._PARAMETERS_UNKNOWN:
+            try:
+                html.write(valuespec.value_to_text(known_settings))
+            except Exception, e:
+                if config.debug:
+                    raise
+                html.write_text(_("Invalid parameter %r: %s") % (known_settings, e))
+
+        else:
+            # For match type "dict" it can be the case the rule define some of the keys
+            # while other keys are taken from the factory defaults. We need to show the
+            # complete outcoming value here.
+            if rules and ruleset.match_type() == "dict":
+                if rulespec.factory_default is not watolib.Rulespec.NO_FACTORY_DEFAULT \
+                    and rulespec.factory_default is not watolib.Rulespec.FACTORY_DEFAULT_UNUSED:
+                    fd = rulespec.factory_default.copy()
+                    fd.update(setting)
+                    setting = fd
+
+            if valuespec and not rules: # show the default value
+                if rulespec.factory_default is watolib.Rulespec.FACTORY_DEFAULT_UNUSED:
+                    # Some rulesets are ineffective if they are empty
+                    html.write_text(_("(unused)"))
+
+                elif rulespec.factory_default is not watolib.Rulespec.NO_FACTORY_DEFAULT:
+                    # If there is a factory default then show that one
+                    setting = rulespec.factory_default
+                    html.write(valuespec.value_to_text(setting))
+
+                elif ruleset.match_type() in ("all", "list"):
+                    # Rulesets that build lists are empty if no rule matches
+                    html.write_text(_("(no entry)"))
+
+                else:
+                    # Else we use the default value of the valuespec
+                    html.write(valuespec.value_to_text(valuespec.default_value()))
+
+            # We have a setting
+            elif valuespec:
+                if ruleset.match_type() == "all":
+                    html.write(", ".join([valuespec.value_to_text(e) for e in setting]))
+                else:
+                    html.write(valuespec.value_to_text(setting))
+
+            # Binary rule, no valuespec, outcome is True or False
+            else:
+                html.img("images/rule_%s%s.png" % ("yes" if setting else "no", "_off" if not rules else ''),
+                         class_="icon", align="absmiddle", title=_("yes") if setting else _("no"))
+        html.close_td()
+        html.close_tr()
+        html.close_table()

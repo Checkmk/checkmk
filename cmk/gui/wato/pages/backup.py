e5efe352af1a724dc4c56b22942a147551293dd8
Lars Michelsen <lm@mathias-kettner.de>
2018-10-17 09:29:54 +0200

    Moved multiple modes to dedicated modules
    Change-Id: I9a6e1e8edaf8a6e7c4f682641df40ca7bcb98367

---
+++ cmk/gui/wato/pages/backup.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+"""Pages for managing backup and restore of WATO"""
+
+import cmk.paths
+
+import cmk.gui.config as config
+import cmk.gui.watolib as watolib
+import cmk.gui.backup as backup
+from cmk.gui.i18n import _
+from cmk.gui.globals import html
+from cmk.gui.plugins.wato.utils.context_buttons import home_button
+from cmk.gui.plugins.wato.utils.base_modes import WatoWebApiMode
+
+from cmk.gui.plugins.wato import (
+    WatoMode,
+    mode_registry,
+)
+
+
+class SiteBackupTargets(backup.Targets):
+    def __init__(self):
+        super(SiteBackupTargets, self).__init__(backup.site_config_path())
+
+
+
+@mode_registry.register
+class ModeBackup(backup.PageBackup, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def title(self):
+        return _("Site backup")
+
+
+    def jobs(self):
+        return watolib.SiteBackupJobs()
+
+
+    def keys(self):
+        return SiteBackupKeypairStore()
+
+
+    def home_button(self):
+        home_button()
+
+
+
+@mode_registry.register
+class ModeBackupTargets(backup.PageBackupTargets, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_targets"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def title(self):
+        return _("Site backup targets")
+
+
+    def targets(self):
+        return SiteBackupTargets()
+
+
+    def jobs(self):
+        return watolib.SiteBackupJobs()
+
+
+    def page(self):
+        self.targets().show_list()
+        backup.SystemBackupTargetsReadOnly().show_list(editable=False, title=_("System global targets"))
+
+
+
+@mode_registry.register
+class ModeEditBackupTarget(backup.PageEditBackupTarget, WatoMode):
+    @classmethod
+    def name(cls):
+        return "edit_backup_target"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def targets(self):
+        return SiteBackupTargets()
+
+
+
+@mode_registry.register
+class ModeEditBackupJob(backup.PageEditBackupJob, WatoMode):
+    @classmethod
+    def name(cls):
+        return "edit_backup_job"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def jobs(self):
+        return watolib.SiteBackupJobs()
+
+
+    def targets(self):
+        return SiteBackupTargets()
+
+
+    def backup_target_choices(self):
+        choices = self.targets().choices()
+
+        # Only add system wide defined targets that don't conflict with
+        # the site specific backup targets
+        choice_dict = dict(choices)
+        for key, title in backup.SystemBackupTargetsReadOnly().choices():
+            if key not in choice_dict:
+                choices.append((key, _("%s (system wide)") % title))
+
+        return sorted(choices, key=lambda (x, y): y.title())
+
+
+    def _validate_target(self, value, varprefix):
+        targets = self.targets()
+        try:
+            targets.get(value)
+        except KeyError:
+            backup.SystemBackupTargetsReadOnly().validate_target(value, varprefix)
+            return
+
+        targets.validate_target(value, varprefix)
+
+
+    def keys(self):
+        return SiteBackupKeypairStore()
+
+
+
+@mode_registry.register
+class ModeBackupJobState(backup.PageBackupJobState, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_job_state"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def jobs(self):
+        return watolib.SiteBackupJobs()
+
+
+
+class ModeAjaxBackupJobState(WatoWebApiMode):
+    def page(self):
+        config.user.need_permission("wato.backups")
+        if html.var("job") == "restore":
+            page = backup.PageBackupRestoreState()
+        else:
+            page = ModeBackupJobState()
+        page.show_job_details()
+
+
+cmk.gui.pages.register_page_handler("ajax_backup_job_state", lambda: ModeAjaxBackupJobState().page())
+
+
+class SiteBackupKeypairStore(backup.BackupKeypairStore):
+    def __init__(self):
+        super(SiteBackupKeypairStore, self).__init__(
+            cmk.paths.default_config_dir + "/backup_keys.mk",
+            "keys")
+
+
+
+@mode_registry.register
+class ModeBackupKeyManagement(SiteBackupKeypairStore, backup.PageBackupKeyManagement, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_keys"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def jobs(self):
+        return watolib.SiteBackupJobs()
+
+
+
+@mode_registry.register
+class ModeBackupEditKey(SiteBackupKeypairStore, backup.PageBackupEditKey, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_edit_key"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+
+@mode_registry.register
+class ModeBackupUploadKey(SiteBackupKeypairStore, backup.PageBackupUploadKey, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_upload_key"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def _upload_key(self, key_file, value):
+        watolib.log_audit(None, "upload-backup-key",
+                  _("Uploaded backup key '%s'") % value["alias"])
+        super(ModeBackupUploadKey, self)._upload_key(key_file, value)
+
+
+
+@mode_registry.register
+class ModeBackupDownloadKey(SiteBackupKeypairStore, backup.PageBackupDownloadKey, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_download_key"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def _file_name(self, key_id, key):
+        return "Check_MK-%s-%s-backup_key-%s.pem" % (backup.hostname(), config.omd_site(), key_id)
+
+
+
+@mode_registry.register
+class ModeBackupRestore(backup.PageBackupRestore, WatoMode):
+    @classmethod
+    def name(cls):
+        return "backup_restore"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["backups"]
+
+
+    def title(self):
+        if not self._target:
+            return _("Site restore")
+        return _("Restore from target: %s") % self._target.title()
+
+
+    def targets(self):
+        return SiteBackupTargets()
+
+
+    def keys(self):
+        return SiteBackupKeypairStore()
+
+
+    def _get_target(self, target_ident):
+        try:
+            return self.targets().get(target_ident)
+        except KeyError:
+            return backup.SystemBackupTargetsReadOnly().get(target_ident)
+
+
+    def _show_target_list(self):
+        super(ModeBackupRestore, self)._show_target_list()
+        backup.SystemBackupTargetsReadOnly().show_list(
+                                editable=False, title=_("System global targets"))
+
+
+    def _show_backup_list(self):
+        self._target.show_backup_list("Check_MK")
---
+++ cmk/gui/wato/pages/read_only.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+"""WATO can be set into read only mode manually using this mode"""
+
+import time
+
+import cmk.store as store
+
+import cmk.gui.config as config
+import cmk.gui.watolib as watolib
+from cmk.gui.i18n import _
+from cmk.gui.globals import html
+
+from cmk.gui.valuespec import (
+    Tuple,
+    FixedValue,
+    Alternative,
+    ListOf,
+    TextAreaUnicode,
+    Dictionary,
+    AbsoluteDate,
+)
+
+from cmk.gui.plugins.wato import (
+    WatoMode,
+    mode_registry,
+    global_buttons,
+)
+
+
+@mode_registry.register
+class ModeManageReadOnly(WatoMode):
+    @classmethod
+    def name(cls):
+        return "read_only"
+
+
+    @classmethod
+    def permissions(cls):
+        return ["set_read_only"]
+
+
+    def __init__(self):
+        super(ModeManageReadOnly, self).__init__()
+        self._settings = config.wato_read_only
+
+
+    def title(self):
+        return _("Manage configuration read only mode")
+
+
+    def buttons(self):
+        global_buttons()
+        html.context_button(_("Back"), watolib.folder_preserving_link([("mode", "globalvars")]), "back")
+
+
+    def action(self):
+        settings = self._vs().from_html_vars("_read_only")
+        self._vs().validate_value(settings, "_read_only")
+        self._settings = settings
+
+        self._save()
+
+
+    def _save(self):
+        store.save_to_mk_file(watolib.multisite_dir + "read_only.mk",
+                          "wato_read_only", self._settings, pprint_value = config.wato_pprint_config)
+
+
+    def page(self):
+        html.p(
+            _("The WATO configuration can be set to read only mode for all users that are not "
+              "permitted to ignore the read only mode. All users that are permitted to set the "
+              "read only can disable it again when another permitted user enabled it before."))
+        html.begin_form("read_only", method="POST")
+        self._vs().render_input("_read_only", self._settings)
+        html.button('_save', _('Save'), 'submit')
+        html.hidden_fields()
+        html.end_form()
+
+
+    def _vs(self):
+        return Dictionary(
+            title = _("Read only mode"),
+            optional_keys = False,
+            render = "form",
+            elements = [
+                ("enabled",
+                    Alternative(
+                        title = _("Enabled"),
+                        style = "dropdown",
+                        elements = [
+                            FixedValue(
+                                False,
+                                title = _("Disabled "),
+                                totext = "Not enabled",
+                            ),
+                            FixedValue(
+                                True,
+                                title = _("Enabled permanently"),
+                                totext = _("Enabled until disabling"),
+                            ),
+                            Tuple(
+                                title = _("Enabled in time range"),
+                                elements = [
+                                    AbsoluteDate(
+                                        title = _("Start"),
+                                        include_time = True,
+                                    ),
+                                    AbsoluteDate(
+                                        title = _("Until"),
+                                        include_time = True,
+                                        default_value = time.time() + 3600,
+                                    ),
+                                ],
+                            ),
+                        ],
+                    ),
+                ),
+                ("rw_users", ListOf(
+                    watolib.UserSelection(),
+                    title = _("Can still edit"),
+                    help = _("Users listed here are still allowed to modify things."),
+                    movable = False,
+                    add_label = _("Add user"),
+                    default_value = [config.user.id],
+                )),
+                ("message", TextAreaUnicode(
+                    title = _("Message"),
+                    rows = 3,
+                )),
+            ]
+        )

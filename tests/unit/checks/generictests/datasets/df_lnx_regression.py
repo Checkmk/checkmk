36b0fecced2017b72aa90316cdff91409cabaf55
Moritz Kiemer <mo@mathias-kettner.de>
2018-10-19 09:13:42 +0200

    df_lnx_regression.py: Add some tests
    Change-Id: I3e32251e406741d5d42f2e4b1be843c0e44b6efc

--- tests/unit/checks/generictests/datasets/df_lnx_regression.py
+++ tests/unit/checks/generictests/datasets/df_lnx_regression.py
@@ -42,6 +42,32 @@ checks = {
               ),
              ]
          ),
+         (u'/', {"show_reserved": True},
+             [(0, '75.8% used (103.92 of 137.13 GB), therein reserved for root: 5.1% (7155.30 MB),' \
+                  ' trend: 0.00 B / 24 hours',
+                 [(u'/', 106418.50390625, 112333.35625, 126375.02578125, 0, 140416.6953125),
+                  ('fs_size', 140416.6953125, None, None, None, None),
+                  ('reserved', 7155.296875, None, None, None, None),
+                  ('growth', 0.0, None, None, None, None),
+                  ('trend', 0, None, None, 0, 5850.695638020833),
+                  ('inodes_used', 1654272, 8228044.8, 8685158.4, 0, 9142272),
+                 ]
+              ),
+             ]
+         ),
+         (u'/dev/sda4 /', {"show_reserved": True},
+             [(0, '75.8% used (103.92 of 137.13 GB), therein reserved for root: 5.1% (7155.30 MB),' \
+                  ' trend: 0.00 B / 24 hours',
+                 [(u'/', 106418.50390625, 112333.35625, 126375.02578125, 0, 140416.6953125),
+                  ('fs_size', 140416.6953125, None, None, None, None),
+                  ('reserved', 7155.296875, None, None, None, None),
+                  ('growth', 0.0, None, None, None, None),
+                  ('trend', 0, None, None, 0, 5850.695638020833),
+                  ('inodes_used', 1654272, 8228044.8, 8685158.4, 0, 9142272),
+                 ]
+              ),
+             ]
+         ),
          (u'/home', 'default', [(3, 'filesystem not found', [])]
          ),
         ]

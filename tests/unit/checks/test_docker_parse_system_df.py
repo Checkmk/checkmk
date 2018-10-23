ca8cf284ba0d8c641e3012bbddcf497eb15f5de2
Moritz Kiemer <mo@mathias-kettner.de>
2018-10-23 14:31:33 +0200

    6835 FIX Docker: Fix crash in Docker inventory for buggy Docker versions
    Since version 1.5.0p7 the docker inventory crashed when confronted
    with empty docker image descriptions.
    
    Change-Id: I98a3c1cf127d419216cd4a8685fc896c819c6aa7

---
+++ tests/unit/checks/test_docker_parse_system_df.py
@@ -0,0 +1,71 @@
+import pytest
+import os
+import re
+
+pytestmark = pytest.mark.checks
+
+execfile(os.path.join(os.path.dirname(__file__), '../../../checks/docker.include'))
+
+regex = re.compile
+
+@pytest.mark.parametrize('indata,outdata', [
+    ([], {}),
+    ([
+      ['TYPE           TOTAL  ACTIVE   SIZE       RECLAIMABLE'],
+      ['Images         7      6        2.076 GB   936.9 MB (45%)'],
+      ['Containers     22     0        2.298 GB   2.298 GB (100%)'],
+      ['Local Volumes  5      5        304 B      0 B (0%)'],
+      ], {"images": {
+              "Type": "Images",
+              "TotalCount": 7,
+              "Active": 6,
+              "Size": 2076000000,
+              "Reclaimable": 936900000},
+          "containers": {
+              "Type": "Containers",
+              "TotalCount": 22,
+              "Active": 0,
+              "Size": 2298000000,
+              "Reclaimable": 2298000000},
+          "local volumes": {
+              "Type": "Local Volumes",
+              "TotalCount": 5,
+              "Active": 5,
+              "Size": 304,
+              "Reclaimable": 0}
+          }),
+    ([
+      ['TYPE                TOTAL               ACTIVE              SIZE                RECLAIMABLE'],
+      ['Images              15                  2                   9.57GB              8.674GB (90%)'],
+      ['Containers          2                   1                   1.226GB             1.224GB (99%)'],
+      ['Local Volumes       1                   1                   9.323MB             0B (0%)'],
+      ['Build Cache         0                   0                   0B                  0B'],
+      ], {"images": {
+              "Type": "Images",
+              "TotalCount": 15,
+              "Active": 2,
+              "Size": 9570000000,
+              "Reclaimable": 8674000000},
+          "containers": {
+              "Type": "Containers",
+              "TotalCount": 2,
+              "Active": 1,
+              "Size": 1226000000,
+              "Reclaimable": 1224000000},
+          "local volumes": {
+              "Type": "Local Volumes",
+              "TotalCount": 1,
+              "Active": 1,
+              "Size": 9323000,
+              "Reclaimable": 0},
+          "build cache": {
+              "Type": "Build Cache",
+              "TotalCount": 0,
+              "Active": 0,
+              "Size": 0,
+              "Reclaimable": 0},
+          }),
+])
+def test_parse_docker_system_df(indata, outdata):
+    parsed = parse_docker_system_df(indata)
+    assert parsed == outdata, "expected: %r, got %r" % (outdata, parsed)

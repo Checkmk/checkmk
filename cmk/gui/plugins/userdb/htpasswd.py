8225f6d469a79592547df82640685a1506ae792d
Lars Michelsen <lm@mathias-kettner.de>
2018-10-24 10:10:33 +0200

    6846 SEC More secure password hashing
    Passwords of local users of the Check_MK GUI are now hashed using SHA256
    (salted, 535000 rounds) to increase the security of the stored user logon
    passwords.
    
    All existing users will still be able to login using their already hashed
    passwords. Once a user changes his password or a new user is created, these
    will be hashed using the new algorithm.
    
    Why SHA256? Check_MK supports different authentication frontends for verifying
    the local credentials: a) basic authentication (done by apache) and b) the GUI
    form + cookie based authentication.
    
    The default is b). This option is toggled with the "omd config" option
    MULTISITE_COOKIE_AUTH. In case the basic authentication is chosen it is only
    possible to use hashing algorithms that are supported by apache which
    performs the authentication in this situation.
    
    For best compatibility in all mentioned situations we use the SHA256 scheme.
    
    CMK-1151
    
    Change-Id: I3e8ea4bdfe813e5d290ef94406340b89c73bfc9a

--- cmk/gui/plugins/userdb/htpasswd.py
+++ cmk/gui/plugins/userdb/htpasswd.py
@@ -24,19 +24,26 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-import time
-import crypt
 import os
 import pathlib2 as pathlib
 from typing import Dict, Text # pylint: disable=unused-import
 
-import cmk.store as store
-import cmk.paths
+from passlib.context import CryptContext
+from passlib.hash import sha256_crypt
 
-import cmk.gui.md5crypt
-from cmk.gui.i18n import _
+import cmk.paths
+import cmk.store as store
 from cmk.gui.exceptions import MKUserError
+from cmk.gui.i18n import _
+
 from . import UserConnector, user_connector_registry
+
+crypt_context = CryptContext(schemes=[
+    "sha256_crypt",
+    # Kept for compatibility with Check_MK < 1.6
+    "apr_md5_crypt",
+    "des_crypt",
+])
 
 class Htpasswd(object):
     """Thin wrapper for loading and saving the htpasswd file"""
@@ -76,11 +83,26 @@ class Htpasswd(object):
         store.save_file("%s" % self._path, output.encode("utf-8"))
 
 
-
-def encrypt_password(password, salt=None, prefix="1"):
-    if not salt:
-        salt = "%06d" % (1000000 * (time.time() % 1.0))
-    return cmk.gui.md5crypt.md5crypt(password, salt, '$%s$' % prefix)
+# Check_MK supports different authentication frontends for verifying the
+# local credentials:
+#
+# a) basic authentication
+# b) GUI form + cookie based authentication
+#
+# The default is b). This option is toggled with the "omd config" option
+# MULTISITE_COOKIE_AUTH. In case the basic authentication is chosen it
+# is only possible use hashing algorithms that are supported by the
+# web server which performs the authentication.
+#
+# See:
+# - https://httpd.apache.org/docs/2.2/misc/password_encryptions.html
+# - https://httpd.apache.org/docs/2.4/misc/password_encryptions.html
+# - https://passlib.readthedocs.io/en/stable/lib/passlib.apache.html
+#
+# For best compatibility in all mentioned situations we use the sha256_crypt
+# scheme.
+def encrypt_password(password):
+    return sha256_crypt.hash(password)
 
 
 @user_connector_registry.register
@@ -104,15 +126,16 @@ class HtpasswdUserConnector(UserConnector):
     # USERDB API METHODS
     #
 
+
     def check_credentials(self, user_id, password):
-        users = Htpasswd(pathlib.Path(cmk.paths.htpasswd_file)).load()
+        users = self._get_htpasswd().load()
         if user_id not in users:
             return None # not existing user, skip over
 
         if self._is_automation_user(user_id):
             raise MKUserError(None, _("Automation user rejected"))
 
-        if self.password_valid(users[user_id], password):
+        if self._password_valid(users[user_id], password):
             return user_id
         return False
 
@@ -121,14 +144,18 @@ class HtpasswdUserConnector(UserConnector):
         return os.path.isfile(cmk.paths.var_dir + "/web/" + user_id.encode("utf-8") + "/automation.secret")
 
 
-    # Validate hashes taken from the htpasswd file. This method handles
-    # crypt() and md5 hashes. This should be the common cases in the
-    # used htpasswd files.
-    def password_valid(self, pwhash, password):
-        if pwhash.startswith('$1$') or pwhash.startswith('$apr1$'):
-            prefix, salt = pwhash.split('$', 3)[1:3]
-            return pwhash == encrypt_password(password, salt, prefix)
-        return pwhash == crypt.crypt(password, pwhash[:2])
+    # Validate hashes taken from the htpasswd file. For the moment this function
+    # needs to be able to deal with des_crypt and apr-md5 hashes which were used
+    # by installations till Check_MK 1.6. The current algorithm also needs to be
+    # handled: sha256_crypt.
+    def _password_valid(self, pwhash, password):
+        try:
+            return crypt_context.verify(password, pwhash)
+        except ValueError:
+            # ValueError("hash could not be identified")
+            # Is raised in case of locked users because we prefix the hashes with
+            # a "!" sign in this situation.
+            return False
 
 
     def save_users(self, users):
@@ -137,7 +164,7 @@ class HtpasswdUserConnector(UserConnector):
         # users from htpasswd are lost. If you start managing users with
         # WATO, you should continue to do so or stop doing to for ever...
         # Locked accounts get a '!' before their password. This disable it.
-        creds = {}
+        entries = {}
 
         for uid, user in users.items():
             # only process users which are handled by htpasswd connector
@@ -145,11 +172,11 @@ class HtpasswdUserConnector(UserConnector):
                 continue
 
             if user.get("password"):
-                if user.get("locked", False):
-                    locksym = '!'
-                else:
-                    locksym = ""
+                entries[uid] = "%s%s" % \
+                    ("!" if user.get("locked", False) else "", user["password"])
 
-                creds[uid] = "%s%s" % (locksym, user["password"])
+        self._get_htpasswd().save(entries)
 
-        Htpasswd(cmk.paths.htpasswd_file).save(creds)
+
+    def _get_htpasswd(self):
+        return Htpasswd(pathlib.Path(cmk.paths.htpasswd_file))

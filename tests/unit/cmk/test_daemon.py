8c23f61409fb276c949899dbe451b0969d44e423
Lars Michelsen <lm@mathias-kettner.de>
2018-11-05 08:42:10 +0100

    Add context manager for PID file locking
    * Added tests to verify PID locking mechanism
    
    Change-Id: Id8c69cf894e6c1b34b76798af76882c97a1e4c6d

---
+++ tests/unit/cmk/test_daemon.py
@@ -0,0 +1,58 @@
+import pytest
+from pathlib2 import Path
+import os
+
+import cmk.store as store
+import cmk.daemon as daemon
+from cmk.exceptions import MKGeneralException
+
+@pytest.fixture(autouse=True)
+def cleanup_locks():
+    yield
+    store.release_all_locks()
+
+
+def test_lock_with_pid_file(tmpdir):
+    pid_file = Path(tmpdir) / "test.pid"
+
+    daemon.lock_with_pid_file("%s" % pid_file)
+
+    assert store.have_lock("%s" % pid_file)
+
+    with pid_file.open() as f:
+        assert int(f.read()) == os.getpid()
+
+
+def test_cleanup_locked_pid_file(tmpdir):
+    pid_file = Path(tmpdir) / "test.pid"
+
+    assert not store.have_lock("%s" % pid_file)
+    daemon.lock_with_pid_file("%s" % pid_file)
+    assert store.have_lock("%s" % pid_file)
+
+    daemon._cleanup_locked_pid_file("%s" % pid_file)
+
+    assert not store.have_lock("%s" % pid_file)
+
+
+def test_pid_file_lock_context_manager(tmpdir):
+    pid_file = Path(tmpdir) / "test.pid"
+
+    assert not store.have_lock("%s" % pid_file)
+
+    with daemon.pid_file_lock(pid_file):
+        assert store.have_lock("%s" % pid_file)
+
+
+def test_pid_file_lock_context_manager_exception(tmpdir):
+    pid_file = Path(tmpdir) / "test.pid"
+
+    assert not store.have_lock("%s" % pid_file)
+    try:
+        with daemon.pid_file_lock(pid_file):
+            assert store.have_lock("%s" % pid_file)
+            raise MKGeneralException("bla")
+    except MKGeneralException:
+        pass
+
+    assert not store.have_lock("%s" % pid_file)

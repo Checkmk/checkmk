8ad1dba81e1dcbd3924dfac9ab9ced392a97ecac
Lars Michelsen <lm@mathias-kettner.de>
2018-11-05 22:48:04 +0100

    Add plugin hierarchy for watolib (preparing for dcd config domain)
    CMK-1141
    
    Change-Id: I3021bf38e684bd4e4c2387316f4808bcfe55495a

--- cmk/gui/watolib.py
+++ cmk/gui/watolib.py
@@ -129,6 +129,18 @@ from cmk.gui.valuespec import (
 if cmk.is_managed_edition():
     import cmk.gui.cme.managed as managed
 
+from cmk.gui.plugins.watolib.utils import (
+    ConfigDomain,
+    config_domain_registry,
+    wato_fileheader,
+    configvars,
+)
+
+import cmk.gui.plugins.watolib
+
+if not cmk.is_raw_edition():
+    import cmk.gui.cee.plugins.watolib
+
 #.
 #   .--Constants-----------------------------------------------------------.
 #   |              ____                _              _                    |
@@ -554,104 +566,6 @@ def get_number_of_pending_changes():
     changes = ActivateChanges()
     changes.load()
     return len(changes.grouped_changes())
-
-
-class ConfigDomain(object):
-    needs_sync = True
-    needs_activation = True
-    always_activate = False
-    ident = None
-    in_global_settings = True
-
-    @classmethod
-    def enabled_domains(cls):
-        return [d for d in config_domain_registry.values() if d.enabled()]
-
-    @classmethod
-    def get_always_activate_domain_idents(cls):
-        return [d.ident for d in config_domain_registry.values() if d.always_activate]
-
-    @classmethod
-    def get_class(cls, ident):
-        return config_domain_registry[ident]
-
-    @classmethod
-    def enabled(cls):
-        return True
-
-    @classmethod
-    def get_all_default_globals(cls):
-        settings = {}
-        for domain in ConfigDomain.enabled_domains():
-            settings.update(domain().default_globals())
-        return settings
-
-    def config_dir(self):
-        raise NotImplementedError()
-
-    def config_file(self, site_specific):
-        if site_specific:
-            return os.path.join(self.config_dir(), "sitespecific.mk")
-        return os.path.join(self.config_dir(), "global.mk")
-
-    def activate(self):
-        raise MKGeneralException(_("The domain \"%s\" does not support activation.") % self.ident)
-
-    def load(self, site_specific=False):
-        filename = self.config_file(site_specific)
-        settings = {}
-
-        if not os.path.exists(filename):
-            return {}
-
-        try:
-            execfile(filename, settings, settings)
-
-            # FIXME: Do not modify the dict while iterating over it.
-            for varname in list(settings.keys()):
-                if varname not in g_configvars:
-                    del settings[varname]
-
-            return settings
-        except Exception, e:
-            raise MKGeneralException(_("Cannot read configuration file %s: %s") % (filename, e))
-
-    def load_site_globals(self):
-        return self.load(site_specific=True)
-
-    def save(self, settings, site_specific=False):
-        filename = self.config_file(site_specific)
-
-        output = wato_fileheader()
-        for varname, value in settings.items():
-            output += "%s = %s\n" % (varname, pprint.pformat(value))
-
-        store.makedirs(os.path.dirname(filename))
-        store.save_file(filename, output)
-
-    def save_site_globals(self, settings):
-        self.save(settings, site_specific=True)
-
-    def default_globals(self):
-        """Returns a dictionary that contains the default settings
-        of all configuration variables of this config domain."""
-        raise NotImplementedError()
-
-    def _get_global_config_var_names(self):
-        """Returns a list of all global config variable names
-        associated with this config domain."""
-        return [varname for (varname, var) in configvars().items() if var[0] == self.__class__]
-
-
-class ConfigDomainRegistry(cmk.plugin_registry.ClassRegistry):
-    def plugin_base_class(self):
-        return ConfigDomain
-
-    def _register(self, plugin_class):
-        self._entries[plugin_class.ident] = plugin_class
-
-
-config_domain_registry = ConfigDomainRegistry()
 
 
 @config_domain_registry.register
@@ -3908,70 +3822,6 @@ def collect_attributes(for_what, do_validate=True, varprefix=""):
 
 
 #.
-#   .--Global configuration------------------------------------------------.
-#   |       ____ _       _           _                    __ _             |
-#   |      / ___| | ___ | |__   __ _| |   ___ ___  _ __  / _(_) __ _       |
-#   |     | |  _| |/ _ \| '_ \ / _` | |  / __/ _ \| '_ \| |_| |/ _` |      |
-#   |     | |_| | | (_) | |_) | (_| | | | (_| (_) | | | |  _| | (_| |      |
-#   |      \____|_|\___/|_.__/ \__,_|_|  \___\___/|_| |_|_| |_|\__, |      |
-#   |                                                          |___/       |
-#   +----------------------------------------------------------------------+
-#   |  Code for loading and saving global configuration variables. This is |
-#   |  not only needed by the WATO for mode for editing these, but e.g.    |
-#   |  also in the code for distributed WATO (handling of site specific    |
-#   |  globals).
-#   '----------------------------------------------------------------------'
-
-g_configvars = {}
-g_configvar_groups = {}
-g_configvar_order = {}
-
-
-def configvars():
-    return g_configvars
-
-
-def configvar_groups():
-    return g_configvar_groups
-
-
-def configvar_order():
-    return g_configvar_order
-
-
-def configvar_show_in_global_settings(varname):
-    try:
-        return configvars()[varname][-1]
-    except KeyError:
-        return False
-
-
-# domain is one of the ConfigDomain classes
-def register_configvar(group,
-                       varname,
-                       valuespec,
-                       domain=None,
-                       need_restart=None,
-                       allow_reset=True,
-                       in_global_settings=True):
-
-    if domain is None:
-        domain = ConfigDomainCore
-
-    # New API is to hand over the class via domain argument. But not all calls have been
-    # migrated. Perform the translation here.
-    if isinstance(domain, basestring):
-        domain = ConfigDomain.get_class(domain)
-
-    g_configvar_groups.setdefault(group, []).append((domain, varname, valuespec))
-    g_configvars[varname] = domain, valuespec, need_restart, allow_reset, in_global_settings
-
-
-def register_configvar_group(title, order=None):
-    if order is not None:
-        configvar_order()[title] = 18
-
-
 # Persistenz: Speicherung der Werte
 # - WATO speichert seine Variablen für main.mk in conf.d/wato/global.mk
 # - Daten, die der User in main.mk einträgt, müssen WATO auch bekannt sein.
@@ -4002,7 +3852,7 @@ def load_configuration_settings(site_specific=False):
 def save_global_settings(vars_, site_specific=False):
     per_domain = {}
     for varname, (domain, _valuespec, _need_restart, _allow_reset,
-                  _in_global_settings) in g_configvars.items():
+                  _in_global_settings) in configvars().items():
         if varname not in vars_:
             continue
         per_domain.setdefault(domain.ident, {})[varname] = vars_[varname]
@@ -10352,10 +10202,6 @@ def validate_all_hosts(hostnames, force_all=False):
         return hosts_errors
     else:
         return {}
-
-
-def wato_fileheader():
-    return "# Created by WATO\n# encoding: utf-8\n\n"
 
 
 g_need_sidebar_reload = None

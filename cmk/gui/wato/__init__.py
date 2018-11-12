Checkmk/checkmk

commit 26a3a187ccdf6f4247af9675ec4f4ec9b5497424

Fix typecheck idiom in cmk/gui/{plugins,wato}
CMK-837

Change-Id: Idf20235b42db88f2f3f4e3e65c78c3e8f7288e17

cmk/gui/wato/__init__.py
@@ -447,7 +447,7 @@ def page_handler():
                 raise MKUserError(None, watolib.read_only_message())
 
             result = mode.action()
-            if type(result) == tuple:
+            if isinstance(result, tuple):
                 newmode, action_message = result
             else:
                 newmode = result
@@ -535,7 +535,7 @@ def get_mode_permission_and_class(mode_name):
     if mode_class is None:
         raise MKGeneralException(_("No such WATO module '<tt>%s</tt>'") % mode_name)
 
-    if type(mode_class) == type(lambda: None):
+    if callable(mode_class):
         raise MKGeneralException(_("Deprecated WATO module: Implemented as function. "
                                    "This needs to be refactored as WatoMode child class."))
 
@@ -779,7 +779,7 @@ def execute_network_scan_job():
             found = watolib.do_remote_automation(config.site(folder.site_id()), "network-scan",
                                           [("folder", folder.path())])
 
-        if type(found) != list:
+        if not isinstance(found, list):
             raise MKGeneralException(_("Received an invalid network scan result: %r") % found)
 
         add_scanned_hosts_to_folder(folder, found)

cmk/gui/plugins/metrics/utils.py
@@ -423,10 +423,10 @@ def _operator_minmax(a, b, func):
 
 
 def _evaluate_literal(expression, translated_metrics):
-    if type(expression) == int:
+    if isinstance(expression, int):
         return float(expression), unit_info["count"], None
 
-    elif type(expression) == float:
+    elif isinstance(expression, float):
         return expression, unit_info[""], None
 
     elif expression[0].isdigit() or expression[0] == '-':

cmk/gui/wato/mkeventd.py
@@ -1462,7 +1462,7 @@ class ModeEventConsoleRulePacks(EventConsoleMode):
 
                 for rule in rule_pack["rules"]:
                     result = cmk.gui.mkeventd.event_rule_matches(rule_pack, rule, event)
-                    if type(result) == tuple:
+                    if isinstance(result, tuple):
                         cancelling, groups = result
 
                         if not cancelling and rule.get("drop") == "skip_pack":
@@ -1680,7 +1680,7 @@ class ModeEventConsoleRules(EventConsoleMode):
                 html.icon(_("This rule is currently disabled and will not be applied"), "disabled")
             elif event:
                 result = cmk.gui.mkeventd.event_rule_matches(self._rule_pack, rule, event)
-                if type(result) != tuple:
+                if not isinstance(result, tuple):
                     html.icon(_("Rule does not match: %s") % result, "rulenmatch")
                 else:
                     cancelling, groups = result
@@ -1724,7 +1724,7 @@ class ModeEventConsoleRules(EventConsoleMode):
                 else:
                     html.write_text(_("DROP"))
             else:
-                if type(rule['state']) == tuple:
+                if isinstance(rule['state'], tuple):
                     stateval = rule["state"][0]
                 else:
                     stateval = rule["state"]
@@ -3328,7 +3328,7 @@ group = "eventconsole"
 
 
 def convert_mkevents_hostspec(value):
-    if type(value) == list:
+    if isinstance(value, list):
         return value
     elif value == "$HOSTADDRESS$":
         return [ "$HOSTADDRESS$" ]

cmk/gui/wato/pages/activate_changes.py
@@ -156,10 +156,10 @@ class ModeActivateChanges(WatoMode, watolib.ActivateChanges):
 
     # TODO: Remove once new changes mechanism has been implemented
     def _extract_from_file(self, filename, elements):
-        if type(elements) == list:
+        if isinstance(elements, list):
             multitar.extract(tarfile.open(filename, "r"), elements)
 
-        elif type(elements) == dict:
+        elif isinstance(elements, dict):
             multitar.extract_domains(tarfile.open(filename, "r"), elements)
 
     # TODO: Remove once new changes mechanism has been implemented

cmk/gui/wato/pages/check_catalog.py
@@ -159,13 +159,13 @@ class ModeCheckPlugins(WatoMode):
 
         # searches in {"name" : "asd", "title" : "das", ...}
         def get_matched_entry(entry):
-            if type(entry) == dict:
+            if isinstance(entry, dict):
                 name = entry.get("name", "")
-                if type(name) == str:
+                if isinstance(name, str):
                     name = name.decode("utf8")
 
                 title = entry.get("title", "")
-                if type(title) == str:
+                if isinstance(title, str):
                     title = title.decode("utf8")
                 if self._search in name.lower() or self._search in title.lower():
                     return entry
@@ -173,7 +173,7 @@ class ModeCheckPlugins(WatoMode):
             return None
 
         def check_entries(key, entries):
-            if type(entries) == list:
+            if isinstance(entries, list):
                 these_matches = []
                 for entry in entries:
                     match = get_matched_entry(entry)
@@ -193,7 +193,7 @@ class ModeCheckPlugins(WatoMode):
                             if name:
                                 handled_check_names.add(name)
 
-            elif type(entries) == dict:
+            elif isinstance(entries, dict):
                 for k, subentries in entries.items():
                     check_entries(k, subentries)
 
@@ -236,7 +236,7 @@ class ModeCheckPlugins(WatoMode):
         return tree
 
     def _render_manpage_topic(self):
-        if type(self._manpages) == list:
+        if isinstance(self._manpages, list):
             self._render_manpage_list(self._manpages, self._path[-1], self._topic_title)
             return
 
@@ -271,7 +271,7 @@ class ModeCheckPlugins(WatoMode):
                 self._render_manpage_list(subnode, path_comp, title)
 
     def _get_check_plugin_stats(self, subnode):
-        if type(subnode) == list:
+        if isinstance(subnode, list):
             num_cats = 1
             num_plugins = len(subnode)
         else:
@@ -293,7 +293,7 @@ class ModeCheckPlugins(WatoMode):
         html.h2(heading)
         table.begin(searchable=False, sortable=False, css="check_catalog")
         for entry in sorted(manpage_list, cmp=lambda a, b: cmp(a["title"], b["title"])):
-            if type(entry) != dict:
+            if not isinstance(entry, dict):
                 continue
             table.row()
             url = html.makeuri([("mode", "check_manpage"), ("check_type", entry["name"]),

cmk/gui/wato/pages/host_diagnose.py
@@ -163,7 +163,7 @@ class ModeDiagHost(WatoMode):
         # snmp_community or the snmp_v3_credentials
         vs_dict = {}
         for key, value in self._host.attributes().items():
-            if key == "snmp_community" and type(value) == tuple:
+            if key == "snmp_community" and isinstance(value, tuple):
                 vs_dict["snmp_v3_credentials"] = value
                 continue
             vs_dict[key] = value

cmk/gui/wato/pages/host_rename.py
@@ -555,7 +555,7 @@ def rename_host_in_event_rules(oldname, newname):
     # Notification channels of flexible notifications also can have host conditions
     for user in users.itervalues():
         method = user.get("notification_method")
-        if method and type(method) == tuple and method[0] == "flexible":
+        if method and isinstance(method, tuple) and method[0] == "flexible":
             channels_changed = 0
             for channel in method[1]:
                 if channel.get("only_hosts"):

cmk/gui/wato/pages/host_tags.py
@@ -611,9 +611,8 @@ def rename_host_tags_after_confirmation(tag_id, operations):
         raise MKUserError("id_0", _("Aborting change."))
 
     elif mode:
-        if tag_id and type(
-                operations
-        ) == list:  # make attribute unknown to system, important for save() operations
+        # make attribute unknown to system, important for save() operations
+        if tag_id and isinstance(operations, list):
             watolib.undeclare_host_tag_attribute(tag_id)
         affected_folders, affected_hosts, affected_rulesets = \
         _change_host_tags_in_folders(tag_id, operations, mode, watolib.Folder.root_folder())
@@ -651,7 +650,7 @@ def rename_host_tags_after_confirmation(tag_id, operations):
                 [("mode", "edit_ruleset"), ("varname", ruleset.name)]), ruleset.title())
         message += "</ul>"
 
-    if not message and type(operations) == tuple:  # deletion of unused tag group
+    if not message and isinstance(operations, tuple):  # deletion of unused tag group
         html.open_div(class_="really")
         html.begin_form("confirm")
         html.write_text(_("Please confirm the deletion of the tag group."))
@@ -662,7 +661,7 @@ def rename_host_tags_after_confirmation(tag_id, operations):
         html.close_div()
 
     elif message:
-        if type(operations) == list:
+        if isinstance(operations, list):
             wato_html_head(_("Confirm tag deletion"))
         else:
             wato_html_head(_("Confirm tag modifications"))
@@ -677,7 +676,7 @@ def rename_host_tags_after_confirmation(tag_id, operations):
         html.begin_form("confirm")
 
         # Check if operations contains removal
-        if type(operations) == list:
+        if isinstance(operations, list):
             have_removal = True
         else:
             have_removal = False
@@ -722,7 +721,7 @@ def _change_host_tags_in_folders(tag_id, operations, mode, folder):
         attrname = "tag_" + tag_id
         attributes = folder.attributes()
         if attrname in attributes:  # this folder has set the tag group in question
-            if type(operations) == list:  # deletion of tag group
+            if isinstance(operations, list):  # deletion of tag group
                 if attrname in attributes:
                     affected_folders.append(folder)
                     if mode != "check":
@@ -767,7 +766,7 @@ def _change_host_tags_in_hosts(folder, tag_id, operations, mode, hostlist):
         attributes = host.attributes()
         attrname = "tag_" + tag_id
         if attrname in attributes:
-            if type(operations) == list:  # delete complete tag group
+            if isinstance(operations, list):  # delete complete tag group
                 affected_hosts.append(host)
                 if mode != "check":
                     del attributes[attrname]
@@ -808,7 +807,7 @@ def _change_host_tags_in_rules(folder, operations, mode):
     for ruleset in rulesets.get_rulesets().itervalues():
         for _folder, _rulenr, rule in ruleset.get_rules():
             # Handle deletion of complete tag group
-            if type(operations) == list:  # this list of tags to remove
+            if isinstance(operations, list):  # this list of tags to remove
                 for tag in operations:
                     if tag != None and (tag in rule.tag_specs or "!" + tag in rule.tag_specs):
                         affected_rulesets.add(ruleset)

commit 3cd978f74fba62272aa912cb70496b9bb63debb7

Fixed broken WATO after 26a3a187ccd
Change-Id: I9e5930328d5e412f401e5930276708d0244cab6b

cmk/gui/wato/__init__.py
@@ -94,6 +94,7 @@ import sys
 import time
 import traceback
 import copy
+import inspect
 from hashlib import sha256
 
 import cmk
@@ -535,7 +536,7 @@ def get_mode_permission_and_class(mode_name):
     if mode_class is None:
         raise MKGeneralException(_("No such WATO module '<tt>%s</tt>'") % mode_name)
 
-    if callable(mode_class):
+    if inspect.isfunction(mode_class):
         raise MKGeneralException(_("Deprecated WATO module: Implemented as function. "
                                    "This needs to be refactored as WatoMode child class."))
 
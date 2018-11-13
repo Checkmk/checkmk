59f4996adaca6ea2731a9c3102a16f4893c315fa
Lars Michelsen <lm@mathias-kettner.de>
2018-11-13 13:54:40 +0100

    YAPFed special agents
    Change-Id: If013440ad21b7057dbf6c9b4f7c6e062e36c9677

--- agents/special/lib/cmk_special_agent_api.py
+++ agents/special/lib/cmk_special_agent_api.py
@@ -24,10 +24,7 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-
 import sys, requests, getopt
-
-
 """Place for common code shared among different Check_MK special agents"""
 
 
@@ -35,7 +32,6 @@ class AgentJSON(object):
     def __init__(self, key, title):
         self._key = key
         self._title = title
-
 
     def usage(self):
         sys.stderr.write("""
@@ -50,10 +46,9 @@ USAGE: agent_%s --section_url [{section_name},{url}]
 
 """ % (self._title, self._key))
 
-
     def get_content(self):
         short_options = "h"
-        long_options  = ["section_url=", "help", "newline_replacement=", "debug"]
+        long_options = ["section_url=", "help", "newline_replacement=", "debug"]
 
         try:
             opts, args = getopt.getopt(sys.argv[1:], short_options, long_options)
@@ -65,14 +60,14 @@ USAGE: agent_%s --section_url [{section_name},{url}]
         newline_replacement = "\\n"
         opt_debug = False
 
-        for o,a in opts:
-            if o in [ "--section_url" ]:
+        for o, a in opts:
+            if o in ["--section_url"]:
                 sections.append(a.split(",", 1))
-            elif o in [ "--newline_replacement" ]:
+            elif o in ["--newline_replacement"]:
                 newline_replacement = a
-            elif o in [ "--debug" ]:
+            elif o in ["--debug"]:
                 opt_debug = True
-            elif o in [ '-h', '--help' ]:
+            elif o in ['-h', '--help']:
                 self.usage()
                 sys.exit(0)
 

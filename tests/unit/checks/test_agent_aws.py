7f5d8f6686c7d691e15d4cb349502485aa3c5ef5
Simon Betz <si@mathias-kettner.de>
2018-11-14 13:23:15 +0100

    Introduced special agent for monitoring Amazon Web Services
    Change-Id: I4736dca691ae76988fcef52a4ab10c273a471350

---
+++ tests/unit/checks/test_agent_aws.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.mark.parametrize("params,result", [
+    ({
+        "--aws-access-key-id": "user",
+        "--aws-secret-access-key": "d1ng",
+        "--region": 'region-eu',
+    }, ['--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng', '--region', 'region-eu']),
+])
+def test_aws(check_manager, params, result):
+    agent = check_manager.get_special_agent("agent_aws")
+    arguments = agent.argument_func(params, "host", "address")
+    assert arguments == result

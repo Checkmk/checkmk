dc511b00d559a34d3eff6514e977e9bfe31eeec2
Moritz Kiemer <mo@mathias-kettner.de>
2018-11-15 15:06:15 +0100

    jolokia_generic/jolokia_metric: refactor and add testcase
    Change-Id: Icb962af677c2c5d309d462a29261bff876ceaa13

--- tests/unit/checks/generictests/datasets/jolokia_generic.py
+++ tests/unit/checks/generictests/datasets/jolokia_generic.py
@@ -3,12 +3,22 @@ checkname = 'jolokia_generic'
 info = [
     ['Instance1', 'Mitglieder_Anzahl', -23, 'number'],
     ['Instance2', 'Mitglieder Anzahl', -23, 'number'],
+    [
+        'JIRA,com.atlassian.jira:type=web.requests,invocation.count', 'jira.name',
+        'invocation.count', 'number'
+    ],
+    [
+        'JIRA,com.atlassian.jira:type=web.requests,invocation.count', 'jira.value', '2624460',
+        'number'
+    ],
 ]
 
 discovery = {
     '': [
         ('Instance1 MBean Mitglieder_Anzahl', {}),
         ('Instance2 MBean Mitglieder Anzahl', {}),
+        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count MBean jira.name', {}),
+        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count MBean jira.value', {}),
     ],
     'rate': [],
     'string': []
@@ -20,5 +30,9 @@ checks = {
          [(0, '-23.0', [('generic_number', -23.0, None, None, None, None)])]),
         ('Instance2 MBean Mitglieder Anzahl', 'default',
          [(0, '-23.0', [('generic_number', -23.0, None, None, None, None)])]),
+        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count MBean jira.name', 'default',
+         [(3, "Non-numeric MBean value", [])]),
+        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count MBean jira.value', 'default',
+         [(0, "2624460.0", [('generic_number', 2624460.0, None, None, None, None)])]),
     ]
 }
---
+++ tests/unit/checks/test_jolokia_include.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+
+pytestmark = pytest.mark.checks
+
+execfile(os.path.join(os.path.dirname(__file__), '../../../checks/jolokia.include'))
+
+
+@pytest.mark.parametrize('line,length,result', [
+    (list('ABCDEF'), 3, ["A", "B C D E", "F"]),
+    (list('ABCDEF'), 4, ["A", "B C D", "E", "F"]),
+    (list('AB'), 2, list("AB")),
+])
+def test_jolokia_basic_split(line, length, result):
+    split_up = jolokia_basic_split(line, length)
+    assert result == split_up
+
+
+@pytest.mark.parametrize('line,length', [
+    (['too', 'short'], 3),
+    (['too', 'short', 'aswell'], 4),
+])
+def test_jolokia_basic_split_fail_value(line, length):
+    with pytest.raises(ValueError):
+        jolokia_basic_split(line, length)
+
+
+@pytest.mark.parametrize('line,length', [
+    (['too', 'short'], 1),
+])
+def test_jolokia_basic_split_fail_notimplemented(line, length):
+    with pytest.raises(NotImplementedError):
+        jolokia_basic_split(line, length)

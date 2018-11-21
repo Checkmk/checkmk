db76784fdd09106be93f9c958a454c71bdff5b71
Tom Baerwinkel <tb@mathias-kettner.de>
2018-11-21 16:54:13 +0100

    k8s_resources: use capacity for inventory
    The checks k8s_resources.pods, k8s_resources.memory and
    k8s_resources.cpu now discover services if a capacity is given.
    
    Change-Id: Id6a1f36416137103b66c1eca4c7c235faaf5f31c

---
+++ tests/unit/checks/generictests/datasets/k8s_resources_regression.py
@@ -0,0 +1,27 @@
+checkname = 'k8s_resources'
+
+info = [[
+    u'{"capacity": {"pods": 110, "cpu": 1.0, "memory": 609845248.0}, "requests": {"cpu": 0.2, "memory": 0.0}, "limits": {"cpu": 0.5, "memory": 0.0}, "allocations": {}}'
+]]
+
+discovery = {
+    'cpu': [(None, {})],
+    'memory': [(None, {})],
+    'pods': [(None, {})],
+}
+
+checks = {
+    'cpu': [(None, 'default', [
+        (0, 'Request: 0.200', []),
+        (0, 'Limit: 0.500', []),
+        (0, 'Capacity: 1.000', []),
+    ])],
+    'memory': [(None, 'default', [
+        (0, 'Request: 0.00 B', []),
+        (0, 'Capacity: 581.59 MB', []),
+    ])],
+    'pods': [(None, 'default', [
+        (0, 'Pods: 0', []),
+        (0, 'Capacity: 110', []),
+    ])],
+}

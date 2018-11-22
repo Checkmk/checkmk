50dff3ddde6af1c83f8ff5d833bd4f4cb163445f
Kenneth Okoh <ko@mathias-kettner.de>
2018-11-22 16:17:12 +0100

    Refactored custom link snapin to new snapin API and updated links
    Change-Id: Iaaaa4ca9bed919a0d98a8886755cfe1712931c74

--- cmk/gui/plugins/sidebar/custom_links.py
+++ cmk/gui/plugins/sidebar/custom_links.py
@@ -29,62 +29,76 @@ import cmk.gui.config as config
 from cmk.gui.htmllib import HTML
 from cmk.gui.i18n import _
 from cmk.gui.globals import html
-from cmk.gui.plugins.sidebar import sidebar_snapins, simplelink
+from cmk.gui.plugins.sidebar import (
+    SidebarSnapin,
+    snapin_registry,
+    simplelink,
+)
 
 
-def render_custom_links():
-    links = config.custom_links.get(config.user.baserole_id)
-    if not links:
-        html.write_text((_(
-            "Please edit <tt>%s</tt> in order to configure which links are shown in this snapin.") %
-                         (cmk.paths.default_config_dir + "/multisite.mk")) + "\n")
-        return
+@snapin_registry.register
+class CustomLinks(SidebarSnapin):
+    @staticmethod
+    def type_name():
+        return "custom_links"
 
-    def render_list(ids, links):
-        n = 0
-        for entry in links:
-            n += 1
-            try:
-                if isinstance(entry[1], type(True)):
-                    idss = ids + [str(n)]
-                    id_ = '/'.join(idss)
-                    html.begin_foldable_container(
-                        "customlinks", id_, isopen=entry[1], title=entry[0])
-                    render_list(idss, entry[2])
-                    html.end_foldable_container()
-                elif isinstance(entry[1], str):
-                    frame = entry[3] if len(entry) > 3 else "main"
+    @classmethod
+    def title(cls):
+        return _("Custom Links")
 
-                    if len(entry) > 2 and entry[2]:
-                        icon_file = entry[2]
+    @classmethod
+    def description(cls):
+        return _("This snapin contains custom links which can be "
+                 "configured via the configuration variable "
+                 "<tt>custom_links</tt> in <tt>multisite.mk</tt>")
 
-                        # Old configs used files named "link_<name>.gif". Those .gif files have
-                        # been removed from Check_MK. Replacing such images with the default icon
-                        if icon_file.endswith(".gif"):
+    def show(self):
+        links = config.custom_links.get(config.user.baserole_id)
+        if not links:
+            html.write_text((_(
+                "Please edit <tt>%s</tt> in order to configure which links are shown in this snapin."
+            ) % (cmk.paths.default_config_dir + "/multisite.mk")) + "\n")
+            return
+
+        def render_list(ids, links):
+            n = 0
+            for entry in links:
+                n += 1
+                try:
+                    if isinstance(entry[1], type(True)):
+                        idss = ids + [str(n)]
+                        id_ = '/'.join(idss)
+                        html.begin_foldable_container(
+                            "customlinks", id_, isopen=entry[1], title=entry[0])
+                        render_list(idss, entry[2])
+                        html.end_foldable_container()
+                    elif isinstance(entry[1], str):
+                        frame = entry[3] if len(entry) > 3 else "main"
+
+                        if len(entry) > 2 and entry[2]:
+                            icon_file = entry[2]
+
+                            # Old configs used files named "link_<name>.gif". Those .gif files have
+                            # been removed from Check_MK. Replacing such images with the default icon
+                            if icon_file.endswith(".gif"):
+                                icon_file = "icon_link.png"
+                        else:
                             icon_file = "icon_link.png"
+
+                        linktext = HTML(html.render_icon("images/%s" % icon_file) + " " + entry[0])
+
+                        simplelink(linktext, entry[1], frame)
                     else:
-                        icon_file = "icon_link.png"
+                        html.write_text(
+                            _("Second part of tuple must be list or string, not %s\n") % str(
+                                entry[1]))
+                except Exception as e:
+                    html.write_text(_("invalid entry %s: %s<br>\n") % (entry, e))
 
-                    linktext = HTML(html.render_icon("images/%s" % icon_file) + " " + entry[0])
+        render_list([], links)
 
-                    simplelink(linktext, entry[1], frame)
-                else:
-                    html.write_text(
-                        _("Second part of tuple must be list or string, not %s\n") % str(entry[1]))
-            except Exception as e:
-                html.write_text(_("invalid entry %s: %s<br>\n") % (entry, e))
-
-    render_list([], links)
-
-
-sidebar_snapins["custom_links"] = {
-    "title": _("Custom Links"),
-    "description": _("This snapin contains custom links which can be "
-                     "configured via the configuration variable "
-                     "<tt>custom_links</tt> in <tt>multisite.mk</tt>"),
-    "render": render_custom_links,
-    "allowed": ["user", "admin", "guest"],
-    "styles": """
+    def styles(self):
+        return """
 #snapin_custom_links div.sublist {
     padding-left: 10px;
 }
@@ -96,4 +110,3 @@ sidebar_snapins["custom_links"] = {
     height: 16px;
 }
 """
-}

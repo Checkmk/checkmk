94c28d98f4256df93183f970cbefc3687d3d2742
Tom Baerwinkel <tb@mathias-kettner.de>
2018-11-24 14:12:46 +0100

    k8s_resources: additionally return allocatable Pods, CPU and memory
    CMK-511
    
    Change-Id: Ia8549b57e187b1b9ce626afbf074078550253708

--- tests/unit/checks/generictests/datasets/k8s_resources_regression.py
+++ tests/unit/checks/generictests/datasets/k8s_resources_regression.py
@@ -1,7 +1,7 @@
 checkname = 'k8s_resources'
 
 info = [[
-    u'{"capacity": {"pods": 110, "cpu": 1.0, "memory": 609845248.0}, "requests": {"cpu": 0.2, "memory": 0.0}, "limits": {"cpu": 0.5, "memory": Infinity}, "allocations": {}}'
+    u'{"allocatable": {"pods": 110, "cpu": 0.9400000000000001, "memory": 609845248.0}, "capacity": {"pods": 110, "cpu": 1.0, "memory": 609845248.0}, "requests": {"cpu": 0.2, "memory": 0.0}, "limits": {"cpu": 0.5, "memory": Infinity}, "allocations": {}}'
 ]]
 
 discovery = {
@@ -14,15 +14,18 @@ checks = {
     'cpu': [(None, 'default', [
         (0, 'Request: 0.200', []),
         (0, 'Limit: 0.500', []),
+        (0, 'Allocatable: 0.940', []),
         (0, 'Capacity: 1.000', []),
     ])],
     'memory': [(None, 'default', [
         (0, 'Request: 0.00 B', []),
         (0, 'Limit: n.a.', []),
+        (0, 'Allocatable: 581.59 MB', []),
         (0, 'Capacity: 581.59 MB', []),
     ])],
     'pods': [(None, 'default', [
         (0, 'Pods: 0', []),
+        (0, 'Allocatable: 110', []),
         (0, 'Capacity: 110', []),
     ])],
 }

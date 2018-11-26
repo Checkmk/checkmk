5f5941040fc553dfe58ea366f7d071c152413ca2
Óscar Nájera <on@mathias-kettner.de>
2018-11-26 12:05:02 +0100

    [FEED-488] Fix configuration input for f5_bigip_conns check
    In the F5 Loadbalance Connections there was an inconsistent use of
    configuration parameters being predictive levels but requiring for
    connection rates to use simple fixed levels tuples.
    
    This fix avoids the misconfiguration and warns the user at runtime.
    
    Drop test in this version, because there is no checklib
    
    FEED-488
    
    Change-Id: Iac99b51ab0e0bbc0d4f9ef27446e9ee9af2f823a

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -9501,9 +9501,12 @@ register_check_parameters(
                 )
             ),
             ( "connections_rate_lower",
-                Levels(
-                     title = _("Minimum connections per second"),
-                     default_value = None,
+                Tuple(
+                    title=_("Minimum connections per second"),
+                    elements=[
+                        Integer(title=_("Warning at")),
+                        Integer(title=_("Critical at")),
+                    ],
                 )
             ),
         ]),

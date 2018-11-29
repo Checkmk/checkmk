bd3ab832e7648bf7dbf6fcf9a6efa6e56b212736
Lars Michelsen <lm@mathias-kettner.de>
2018-11-29 13:24:05 +0100

    Add integration test based regression test for CMK-1381
    Change-Id: Ia7b3b9e2148f94a06db3b76856389b63a1d4b9fe

--- tests/integration/livestatus/test_livestatus.py
+++ tests/integration/livestatus/test_livestatus.py
@@ -2,14 +2,22 @@
 # encoding: utf-8
 
 import pytest
+import itertools
+
 from testlib import web
+
 
 @pytest.fixture(scope="module")
 def default_cfg(web):
     print "Applying default config"
-    web.add_host("livestatus-test-host", attributes={
-        "ipaddress": "127.0.0.1",
-    })
+    web.add_host(
+        "livestatus-test-host", attributes={
+            "ipaddress": "127.0.0.1",
+        })
+    web.add_host(
+        "livestatus-test-host.domain", attributes={
+            "ipaddress": "127.0.0.1",
+        })
 
     web.discover_services("livestatus-test-host")
 
@@ -22,11 +30,12 @@ def default_cfg(web):
     print "Cleaning up default config"
 
     web.delete_host("livestatus-test-host")
+    web.delete_host("livestatus-test-host.domain")
 
 
 # Simply detects all tables by querying the columns table and then
 # queries each of those tables without any columns and filters
-@pytest.mark.parametrize(("core"), [ "nagios", "cmc" ])
+@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
 def test_tables(default_cfg, site, core):
     site.set_config("CORE", core, with_restart=True)
 
@@ -39,31 +48,60 @@ def test_tables(default_cfg, site, core):
 
     for table in existing_tables:
         if core == "nagios" and table == "statehist":
-            continue # the statehist table in nagios can not be fetched without time filter
+            continue  # the statehist table in nagios can not be fetched without time filter
 
         result = site.live.query("GET %s\n" % table)
-        assert type(result) == list
+        assert isinstance(result, list)
 
 
-@pytest.mark.parametrize(("core"), [ "nagios", "cmc" ])
+@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
 def test_host_table(default_cfg, site, core):
     site.set_config("CORE", core, with_restart=True)
 
     rows = site.live.query("GET hosts")
-    assert type(rows) == list
-    assert len(rows) >= 2 # header + min 1 host
+    assert isinstance(rows, list)
+    assert len(rows) >= 2  # header + min 1 host
 
 
-@pytest.mark.parametrize(("core"), [ "nagios", "cmc" ])
+@pytest.mark.parametrize("core,query_and_result",
+                         itertools.product([
+                             "nagios",
+                             "cmc",
+                         ], [
+                             {
+                                 "query": ("GET hosts\n"
+                                           "Columns: host_name\n"
+                                           "Filter: host_name = livestatus-test-host.domain\n"),
+                                 "result": [{
+                                     u'name': u'livestatus-test-host.domain',
+                                 },],
+                             },
+                             {
+                                 "query": ("GET hosts\n"
+                                           "Columns: host_name\n"
+                                           "Filter: host_name = livestatus-test-host\n"),
+                                 "result": [{
+                                     u'name': u'livestatus-test-host',
+                                 },],
+                             },
+                         ]))
+def test_host_table_host_equal_filter(default_cfg, site, core, query_and_result):
+    site.set_config("CORE", core, with_restart=True)
+
+    rows = site.live.query_table_assoc(query_and_result["query"])
+    assert rows == query_and_result["result"]
+
+
+@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
 def test_service_table(default_cfg, site, core):
     site.set_config("CORE", core, with_restart=True)
 
     rows = site.live.query("GET services\nFilter: host_name = livestatus-test-host\n"
                            "Columns: description\n")
-    assert type(rows) == list
-    assert len(rows) >= 20 # header + min 1 service
+    assert isinstance(rows, list)
+    assert len(rows) >= 20  # header + min 1 service
 
-    descriptions = [ r[0] for r in rows ]
+    descriptions = [r[0] for r in rows]
 
     assert "Check_MK" in descriptions
     assert "Check_MK Discovery" in descriptions

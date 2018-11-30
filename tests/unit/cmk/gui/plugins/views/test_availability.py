3b630b9646e56b3615128d344c7bc257e161e661
Lars Michelsen <lm@mathias-kettner.de>
2018-11-30 12:40:00 +0100

    6918 FIX Availability: Time format is not only affected by time range
    In case a availability is displaying a single day time range, the times
    of the single entries are formatted without date information.
    
    In case there are annotations or downtimes that affect multiple days the
    from and until columns also only showed the start or end time instead of
    the date. This was some kind of misleading when trying to understand why
    such a downtime matched a given period.
    
    CMK-1353
    
    Change-Id: Iec290d656540add153c5fa28957fcb95c46f6a01

--- tests/unit/cmk/gui/plugins/views/test_availability.py
+++ tests/unit/cmk/gui/plugins/views/test_availability.py
@@ -1,5 +1,6 @@
 import pytest
 
+import cmk.render
 import cmk.gui.plugins.views.availability as availability
 
 
@@ -16,3 +17,21 @@ import cmk.gui.plugins.views.availability as availability
 def test_relevant_annotation_times(annotation_from, annotation_until, result):
     assert availability._annotation_affects_time_range(annotation_from, annotation_until, 30,
                                                        60) == result
+
+
+@pytest.mark.parametrize("annotation_times,result", [
+    ([
+        (1543446000 + 7200, 1543446000 + 14400),
+        (1543446000 + 28800, 1543446000 + 32400),
+    ], cmk.render.time_of_day),
+    ([
+        (1543446000, 1543446000),
+    ], cmk.render.time_of_day),
+    ([(1543446000 - 3600, 1543446000 + 3600)], cmk.render.date_and_time),
+    ([(1543446000, 1543446000 + 86400)], cmk.render.date_and_time),
+    ([(1543446000 + 82800, 1543446000 + 172800)], cmk.render.date_and_time),
+])
+def test_get_annotation_date_render_function(annotation_times, result):
+    annotations = [((None, None, None), {"from": s, "until": e}) for s, e in annotation_times]
+    assert availability.get_annotation_date_render_function(
+        annotations, {"range": ((1543446000, 1543446000 + 86399), "bla")}) == result

0c192fd94e9a11d791fbf29df7966a54edac273e
Tom Baerwinkel <tb@mathias-kettner.de>
2018-12-04 16:45:53 +0100

    Add flycheck-python-mypy-executable to .dir-locals.el
    Change-Id: I7b7b6eba72adcf20135e0332f9836b04600cc0fb

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,4 @@
-((c++-mode . ((flycheck-cppcheck-suppressions . ("passedByValue")))))
+((c++-mode . ((flycheck-cppcheck-suppressions . ("passedByValue"))))
+ (python-mode . ((eval . (setq flycheck-python-mypy-executable
+                               (concat (locate-dominating-file default-directory dir-locals-file)
+                                       "tests/static/.venv/bin/mypy"))))))

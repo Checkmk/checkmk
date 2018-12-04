ac4d90bb03a772ef849762ac36e26a4632d0c9c9
Moritz Kiemer <mo@mathias-kettner.de>
2018-12-04 14:23:36 +0100

    6943 FIX juniper_bgp_state: Fix item generation for IPv6 addresses
    The check now extracts the proper remote IP address in case of
    IPv6 addresses as item, instead of a part of the OID end.
    This change requires a rediscovery.
    
    CMK-1334
    
    Change-Id: Ib3275f88dcca9ed1bb23bff92c6551c91796b3b7

--- tests/unit/checks/generictests/datasets/juniper_bgp_state_1_regression.py
+++ tests/unit/checks/generictests/datasets/juniper_bgp_state_1_regression.py
@@ -1,22 +1,22 @@
 checkname = 'juniper_bgp_state'
 
 info = [
-    [u'6.1.80.81.193.134.1.80.81.192.3', u'6', u'2'],
-    [u'6.1.80.81.193.134.1.80.81.192.24', u'3', u'2'],
+    [u'6', u'2', '64 60 01 22'],
+    [u'3', u'2', '64 60 01 26'],
 ]
 
 discovery = {
-    '': [(u'80.81.192.24', {}), (u'80.81.192.3', {}),
+    '': [(u'100.96.1.34', {}), (u'100.96.1.38', {}),
         ]
 }
 
 checks = {
     '': [
-        (u'80.81.192.24', 'default',
-         [(2, u'Status with peer 80.81.192.24 is active', []),
+        (u'100.96.1.34', 'default',
+         [(0, u'Status with peer 100.96.1.34 is established', []),
           (0, 'operational status: running', [])]),
-        (u'80.81.192.3', 'default',
-         [(0, u'Status with peer 80.81.192.3 is established', []),
+        (u'100.96.1.38', 'default',
+         [(2, u'Status with peer 100.96.1.38 is active', []),
           (0, 'operational status: running', [])]),
     ]
 }

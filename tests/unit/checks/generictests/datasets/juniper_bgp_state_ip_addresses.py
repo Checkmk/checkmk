c80c35362c6dbbe767b547e2190c43ed03991d91
Moritz Kiemer <mo@mathias-kettner.de>
2018-12-06 14:12:39 +0100

    fix unexpected snmp parsing
    Change-Id: I3e88d7277f448d4a071aaf051d95a7e2fc24cc0a

--- tests/unit/checks/generictests/datasets/juniper_bgp_state_ip_addresses.py
+++ tests/unit/checks/generictests/datasets/juniper_bgp_state_ip_addresses.py
@@ -1,37 +1,25 @@
+
+
 checkname = 'juniper_bgp_state'
 
-info = [
-    [u'4', u'1', 'DE AD BE EF 00 40 01 11 00 00 00 00 00 00 00 01'],
-    [u'4', u'2', '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'],
-    [u'4', u'2', 'DE AD BE EF 00 40 01 11 00 00 00 00 00 00 00 00'],
-    [u'4', u'1', '00 00 00 00 00 40 01 11 00 00 10 0A 00 00 00 00'],
-    [u'5', u'2', 'this is not a valid IP'],
+
+info = [[u'4',
+         u'1',
+         [222, 173, 190, 239, 0, 64, 1, 17, 0, 0, 0, 0, 0, 0, 0, 1]],
+        [u'4', u'2', [0] * 16],
 ]
 
-discovery = {
-    '': [
-        (u'[dead:beef:40:111::1]', {}), (u'[::]', {}),
-        (u'[dead:beef:40:111::]', {}), (u'[::40:111:0:100a:0:0]', {}),
-        (u"'this is not a valid IP'", {}),
-        ]
-}
 
-checks = {
-    '': [
-        (u'[dead:beef:40:111::1]', 'default',
-         [(0, u'Status with peer [dead:beef:40:111::1] is opensent', []),
-          (1, 'operational status: halted', [])]),
-        (u'[::]', 'default',
-         [(2, u'Status with peer [::] is opensent', []),
-          (0, 'operational status: running', [])]),
-        (u'[dead:beef:40:111::]', 'default',
-         [(2, u'Status with peer [dead:beef:40:111::] is opensent', []),
-          (0, 'operational status: running', [])]),
-        (u'[::40:111:0:100a:0:0]', 'default',
-         [(0, u'Status with peer [::40:111:0:100a:0:0] is opensent', []),
-          (1, 'operational status: halted', [])]),
-        (u"'this is not a valid IP'", 'default',
-         [(2, u"Status with peer 'this is not a valid IP' is openconfirm", []),
-          (0, 'operational status: running', [])]),
-    ]
-}
+discovery = {'': [('[dead:beef:40:111::1]', {}), ("[::]", {})]}
+
+
+checks = {'': [('[dead:beef:40:111::1]',
+                'default',
+                [(0, 'Status with peer [dead:beef:40:111::1] is opensent', []),
+                 (1, 'operational status: halted', [])]),
+                (u"[::]",
+                'default',
+                [(2,
+                    u"Status with peer [::] is opensent",
+                  []),
+                 (0, 'operational status: running', [])])]}

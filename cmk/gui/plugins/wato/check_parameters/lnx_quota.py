8b60f3f8bfab7939651e6444c6bd02d9dab717a1
Kenneth Okoh <ko@mathias-kettner.de>
2018-12-06 14:47:46 +0100

    Refactoring: Moved more application check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: Iacf6509fd5722951b6d7f5c9c7eea849e49e7342

---
+++ cmk/gui/plugins/wato/check_parameters/lnx_quota.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Checkbox,
+    Dictionary,
+    TextAscii,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications,
+    "lnx_quota",
+    _("Linux quota check"),
+    Dictionary(
+        optional_keys=None,
+        elements=[
+            ("user", Checkbox(
+                title=_("Monitor user quotas"),
+                label=_("Enable"),
+                default_value=True,
+            )),
+            ("group", Checkbox(
+                title=_("Monitor group quotas"),
+                label=_("Enable"),
+            )),
+        ]),
+    TextAscii(
+        title=_("filesystem"),
+        help=_("Name of filesystem with quotas enabled"),
+    ),
+    match_type='dict',
+)
---
+++ cmk/gui/plugins/wato/check_parameters/ruckus_ap.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Integer,
+    Optional,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications, "ruckus_ap", _("Ruckus Spot Access Points"),
+    Tuple(
+        elements=[
+            Optional(
+                Tuple(
+                    elements=[
+                        Integer(title=_("Warning at"), default_value=1, unit=_("devices")),
+                        Integer(title=_("Critical at"), default_value=1, unit=_("devices")),
+                    ],),
+                sameline=True,
+                label=_("Levels for <i>device time drifted</i>"),
+                none_label=_("No levels set"),
+                none_value=(None, None)),
+            Optional(
+                Tuple(
+                    elements=[
+                        Integer(title=_("Warning at"), default_value=1, unit=_("devices")),
+                        Integer(title=_("Critical at"), default_value=1, unit=_("devices")),
+                    ],),
+                sameline=True,
+                label=_("Levels for <i>device not responding</i>"),
+                none_label=_("No levels set"),
+                none_value=(None, None)),
+        ],), TextAscii(
+            title=_("Band"),
+            help=_("Name of the band, e.g. 5 GHz"),
+        ), "first")

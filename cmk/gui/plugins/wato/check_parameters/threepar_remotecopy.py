b2e8d2a5d7ce8d8932bfe67c52e976a8fbea46d6
Kenneth Okoh <ko@mathias-kettner.de>
2018-12-06 14:02:47 +0100

    Refactoring: Moved some application check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I6f39ccebbcd8b11eb250910dd9f42994a184e457

---
+++ cmk/gui/plugins/wato/check_parameters/threepar_remotecopy.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    MonitoringState,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications, "threepar_remotecopy", _("3PAR Remote Copy"),
+    Dictionary(elements=[
+        ("1", MonitoringState(
+            title=_("Status: NORMAL"),
+            default_value=0,
+        )),
+        ("2", MonitoringState(
+            title=_("Status: STARTUP"),
+            default_value=1,
+        )),
+        ("3", MonitoringState(
+            title=_("Status: SHUTDOWN"),
+            default_value=1,
+        )),
+        ("4", MonitoringState(
+            title=_("Status: ENABLE"),
+            default_value=0,
+        )),
+        ("5", MonitoringState(
+            title=_("Status: DISBALE"),
+            default_value=2,
+        )),
+        ("6", MonitoringState(
+            title=_("Status: INVALID"),
+            default_value=2,
+        )),
+        ("7", MonitoringState(
+            title=_("Status: NODEUP"),
+            default_value=1,
+        )),
+        ("8", MonitoringState(
+            title=_("Status: UPGRADE"),
+            default_value=0,
+        )),
+    ]), None, "dict")

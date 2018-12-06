55fc9d8a3c496db23ea0cdef5f8a8f61c2fea162
Sven Panne <sp@mathias-kettner.de>
2018-12-06 09:39:01 +0100

    Use projectile to avoid unexpanded ~.
    Change-Id: I891327bd1c52321de44a8483712bf3f8b05a3d50

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((c++-mode . ((flycheck-cppcheck-suppressions . ("passedByValue"))))
- (python-mode . ((eval . (setq flycheck-python-mypy-executable
-                               (concat (locate-dominating-file default-directory dir-locals-file)
-                                       "tests/static/.venv/bin/mypy"))))))
+ (python-mode . ((eval setq flycheck-python-mypy-executable
+                       (concat (projectile-locate-dominating-file default-directory dir-locals-file)
+                               "tests/static/.venv/bin/mypy")))))

b5afb0d4b1f00b17d0bc51e38da79169099272bd
Sven Panne <sp@mathias-kettner.de>
2018-12-06 11:54:00 +0100

    Expected package sizes changed again...
    Change-Id: Ie4974926b74270787576c13a24c9dcb26b2fecb2

--- tests/packaging/test_files.py
+++ tests/packaging/test_files.py
@@ -23,10 +23,10 @@ def _get_package_paths(version_path, what):
 
 # In case packages grow/shrink this check has to be changed.
 @pytest.mark.parametrize("what,min_size,max_size", [
-    ("rpm", 152 * 1024 * 1024, 235 * 1024 * 1024),
-    ("deb", 126 * 1024 * 1024, 196 * 1024 * 1024),
-    ("cma", 214 * 1024 * 1024, 241 * 1024 * 1024),
-    ("tar.gz", 370 * 1024 * 1024, 439 * 1024 * 1024),
+    ("rpm", 176 * 1024 * 1024, 253 * 1024 * 1024),
+    ("deb", 142 * 1024 * 1024, 155 * 1024 * 1024),
+    ("cma", 248 * 1024 * 1024, 259 * 1024 * 1024),
+    ("tar.gz", 349 * 1024 * 1024, 461 * 1024 * 1024),
 ])
 def test_package_sizes(version_path, what, min_size, max_size):
     for pkg in _get_package_paths(version_path, what):

acddb0f23a7154a48df21a69686a3cb49870d61a
Florian Kromer <fk@mathias-kettner.de>
2018-12-07 16:56:59 +0100

    6898 alcatel_cpu_aos7: Add support for new switches
    This werk adds cpu usage monitoring (alcatel_cpu) support for the new
    switch devices running at least AOS7. The check alcatel_cpu_aos7 reuses
    the default values of check alcatel_cpu for consistent check behaviour.
    
    CMK-1188
    
    Change-Id: Ib9e4972a5fc77f5991ae6269c3ab1f5ac4f42af8

---
+++ tests/unit/checks/generictests/datasets/alcatel_aos7.py
@@ -0,0 +1,7 @@
+checkname = 'alcatel_cpu_aos7'
+
+info = [['17']]
+
+discovery = {'': [(None, 'alcatel_cpu_default_levels')]}
+
+checks = {'': [(None, (90.0, 95.0), [(0, 'total: 17.0%', [('util', 17, 90.0, 95.0, 0, 100)])])]}

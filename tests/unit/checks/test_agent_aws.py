b0853eaa949f2bb9e57225b72153c6cb60972054
Simon Betz <si@mathias-kettner.de>
2018-12-11 13:50:12 +0100

    agent_aws: Default resolution of 5 min requires an aysnchronous execution
    Change-Id: Id24213ae2bf13b1be06450b6b707ef010d46ab02

--- tests/unit/checks/test_agent_aws.py
+++ tests/unit/checks/test_agent_aws.py
@@ -6,7 +6,7 @@ import pytest
         "--aws-access-key-id": "user",
         "--aws-secret-access-key": "d1ng",
         "--region": 'region-eu',
-    }, ['--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng', '--region', 'region-eu']),
+    }, ['--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng', '--region', 'region-eu', 'host']),
 ])
 def test_aws(check_manager, params, result):
     agent = check_manager.get_special_agent("agent_aws")

071213442344a92e376684891dbb5431a222e452
Lars Michelsen <lm@mathias-kettner.de>
2018-12-12 10:02:45 +0100

    YAPFed test file
    Change-Id: If1b7bc3bf3ce122076e4113a89181d361d5d901a

--- tests/integration/cmk/gui/test_crawl.py
+++ tests/integration/cmk/gui/test_crawl.py
@@ -17,26 +17,23 @@ from testlib import web, var_dir
 
 from testlib import CMKWebSession
 
+
 class InvalidUrl(Exception):
     pass
 
 
-
 class Url(object):
     def __init__(self, url, orig_url=None, referer_url=None):
-        self.url         = url
-        self.orig_url    = orig_url
+        self.url = url
+        self.orig_url = orig_url
         self.referer_url = referer_url
-
 
     def __hash__(self):
         return hash(self.url)
 
-
     # Strip host and site prefix
     def neutral_url(self):
         return "check_mk/" + self.url.split("/check_mk/", 1)[1]
-
 
     # Strip proto and host
     def url_without_host(self):
@@ -46,20 +43,18 @@ class Url(object):
         return urlunsplit(parsed)
 
 
-
 class Worker(threading.Thread):
     def __init__(self, num, crawler):
         super(Worker, self).__init__()
-        self.name      = "worker-%d" % num
-        self.crawler   = crawler
-        self.daemon    = True
+        self.name = "worker-%d" % num
+        self.crawler = crawler
+        self.daemon = True
         self.terminate = False
-        self.idle      = True
+        self.idle = True
 
         self.client = CMKWebSession(self.crawler.site)
         self.client.login()
         self.client.set_language("en")
-
 
     def run(self):
         while not self.terminate:
@@ -70,17 +65,14 @@ class Worker(threading.Thread):
                     try:
                         self.visit_url(url)
                     except Exception as e:
-                        self.error(url, "Failed to visit: %s\n%s" %
-                                     (e, traceback.format_exc()))
+                        self.error(url, "Failed to visit: %s\n%s" % (e, traceback.format_exc()))
                     self.crawler.todo.task_done()
             except Queue.Empty:
                 self.idle = True
                 time.sleep(0.5)
 
-
     def stop(self):
         self.terminate = True
-
 
     def visit_url(self, url):
         if url.url in self.crawler.visited:
@@ -111,48 +103,54 @@ class Worker(threading.Thread):
         if content_type.startswith("text/html"):
             self.check_response(url, response)
         elif content_type.startswith("text/plain"):
-            pass # no specific test
+            pass  # no specific test
         elif content_type.startswith("text/csv"):
-            pass # no specific test
-        elif content_type in [ "image/png", "image/gif" ]:
-            pass # no specific test
-        elif content_type in [ "application/pdf" ]:
-            pass # no specific test
-        elif content_type in [ "application/x-rpm", "application/x-deb", "application/x-debian-package",
-                               "application/x-gzip", "application/x-msdos-program", "application/x-msi",
-                               "application/x-tgz", "application/x-redhat-package-manager",
-                               "application/x-pkg",
-                               "text/x-chdr", "text/x-c++src", "text/x-sh", ]:
-            pass # no specific test
+            pass  # no specific test
+        elif content_type in ["image/png", "image/gif"]:
+            pass  # no specific test
+        elif content_type in ["application/pdf"]:
+            pass  # no specific test
+        elif content_type in [
+                "application/x-rpm",
+                "application/x-deb",
+                "application/x-debian-package",
+                "application/x-gzip",
+                "application/x-msdos-program",
+                "application/x-msi",
+                "application/x-tgz",
+                "application/x-redhat-package-manager",
+                "application/x-pkg",
+                "text/x-chdr",
+                "text/x-c++src",
+                "text/x-sh",
+        ]:
+            pass  # no specific test
         else:
             self.error(url, "Unknown content type: %s" % (content_type))
             return
 
-
     def update_stats(self, url, duration, content_size):
         stats = self.crawler.stats.setdefault(url.neutral_url(), {
-            "first_duration"     : duration,
-            "first_content_size" : content_size,
+            "first_duration": duration,
+            "first_content_size": content_size,
         })
 
-        avg_duration     = (duration + stats.get("avg_duration", duration)) / 2.0
+        avg_duration = (duration + stats.get("avg_duration", duration)) / 2.0
         avg_content_size = (content_size + stats.get("avg_content_size", content_size)) / 2.0
 
         stats.update({
-            "orig_url"          : url.orig_url,
-            "referer_url"       : url.referer_url,
-            "num_visited"       : stats.get("num_visited", 0) + 1,
-            "last_duration"     : duration,
-            "last_content_size" : content_size,
-            "avg_duration"      : avg_duration,
-            "avg_content_size"  : avg_content_size,
+            "orig_url": url.orig_url,
+            "referer_url": url.referer_url,
+            "num_visited": stats.get("num_visited", 0) + 1,
+            "last_duration": duration,
+            "last_content_size": content_size,
+            "avg_duration": avg_duration,
+            "avg_content_size": avg_content_size,
         })
-
 
     def error(self, url, s):
         s = "[%s - found on %s] %s" % (url.url, url.referer_url, s)
         self.crawler.error(s)
-
 
     def check_response(self, url, response):
         soup = BeautifulSoup(response.text, "lxml")
@@ -163,7 +161,6 @@ class Worker(threading.Thread):
         self.check_links(url, soup)
         self.check_frames(url, soup)
         self.check_iframes(url, soup)
-
 
     def check_content(self, url, response, soup):
         ignore_texts = [
@@ -182,18 +179,14 @@ class Worker(threading.Thread):
             if not skip:
                 self.error(url, "Found error: %s" % (element))
 
-
     def check_frames(self, url, soup):
         self.check_referenced(url, soup, "frame", "src")
-
 
     def check_iframes(self, url, soup):
         self.check_referenced(url, soup, "iframe", "src")
 
-
     def check_links(self, url, soup):
         self.check_referenced(url, soup, "a", "href")
-
 
     def check_referenced(self, referer_url, soup, tag, attr):
         elements = soup.find_all(tag)
@@ -203,7 +196,7 @@ class Worker(threading.Thread):
             url = self.normalize_url(self.crawler.site.internal_url, orig_url)
 
             if url is None:
-               continue
+                continue
 
             try:
                 self.verify_is_valid_url(url)
@@ -222,7 +215,6 @@ class Worker(threading.Thread):
             if crawl_it:
                 #file("/tmp/todo", "a").write("%s (%s)\n" % (url, referer_url.url))
                 self.crawler.todo.put(Url(url, orig_url=orig_url, referer_url=referer_url.url))
-
 
     def verify_is_valid_url(self, url):
         parsed = urlsplit(url)
@@ -266,7 +258,6 @@ class Worker(threading.Thread):
         if parsed.path.startswith("/%s/check_mk/agents/" % self.crawler.site.id):
             raise InvalidUrl("Skipping agent download file: %s" % url)
 
-
     def normalize_url(self, base_url, url):
         url = urljoin(base_url, url.rstrip("#"))
         parsed = list(urlsplit(url))
@@ -277,8 +268,10 @@ class Worker(threading.Thread):
 class SetQueue(Queue.Queue):
     def _init(self, maxsize):
         self.queue = set()
+
     def _put(self, item):
         self.queue.add(item)
+
     def _get(self):
         return self.queue.pop()
 
@@ -286,8 +279,8 @@ class SetQueue(Queue.Queue):
 class TestCrawler(object):
     @pytest.mark.type("gui_crawl")
     def test_crawl(self, site):
-        self.stats   = {}
-        self.todo    = SetQueue()
+        self.stats = {}
+        self.todo = SetQueue()
         self.started = time.time()
         self.visited = []
         self.skipped = set()
@@ -297,8 +290,8 @@ class TestCrawler(object):
         self.handled = set()
         self.handled_lock = threading.Lock()
 
-        self.errors  = []
-        self.site    = site
+        self.errors = []
+        self.site = site
         self.num_workers = 10
 
         self.load_stats()
@@ -310,64 +303,56 @@ class TestCrawler(object):
 
         self.report()
 
-
     def stats_file(self):
         return var_dir() + "/crawl.stats"
-
 
     def report_file(self):
         return var_dir() + "/crawl.report"
 
-
     def web_log_file(self):
         return var_dir() + "/crawl-web.log"
 
-
     def apache_error_log_file(self):
         return var_dir() + "/crawl-apache_error_log.log"
-
 
     def load_stats(self):
         try:
             self.stats = eval(file(self.stats_file()).read())
         except IOError as e:
             if e.errno == 2:
-                pass # Not existing files are OK
+                pass  # Not existing files are OK
             else:
                 raise
-
 
     def save_stats(self):
         if not os.path.exists(var_dir()):
             os.makedirs(var_dir())
-        file(self.stats_file()+".tmp", "w").write(pprint.pformat(self.stats) + "\n")
-        os.rename(self.stats_file()+".tmp", self.stats_file())
-
+        file(self.stats_file() + ".tmp", "w").write(pprint.pformat(self.stats) + "\n")
+        os.rename(self.stats_file() + ".tmp", self.stats_file())
 
     def update_total_stats(self, finished):
         stats = self.stats.setdefault("_TOTAL_", {})
 
         stats["last_num_visited"] = len(self.visited)
-        stats["last_duration"]    = time.time() - self.started
-        stats["last_errors"]      = self.errors
-        stats["last_finished"]    = finished
+        stats["last_duration"] = time.time() - self.started
+        stats["last_errors"] = self.errors
+        stats["last_finished"] = finished
 
         if finished:
             if stats.get("last_finished_num_visited", 0) > 0:
                 perc = float(stats["last_num_visited"]) * 100 / stats["last_finished_num_visited"]
                 if perc < 80.0:
-                    self.error("Finished and walked %d URLs, previous run walked %d URLs. That "
-                               "is %0.2f %% of the previous run. Something seems to be wrong."
-                                 % (stats["last_num_visited"], stats["last_finished_num_visited"],
-                                    perc))
+                    self.error(
+                        "Finished and walked %d URLs, previous run walked %d URLs. That "
+                        "is %0.2f %% of the previous run. Something seems to be wrong." %
+                        (stats["last_num_visited"], stats["last_finished_num_visited"], perc))
 
             stats["last_finished_num_visited"] = stats["last_num_visited"]
-            stats["last_finished_duration"]    = stats["last_duration"]
-            stats["last_finished_errors"]      = stats["last_errors"]
-
+            stats["last_finished_duration"] = stats["last_duration"]
+            stats["last_finished_errors"] = stats["last_errors"]
 
     def report(self):
-        with file(self.report_file()+".tmp", "w") as f:
+        with file(self.report_file() + ".tmp", "w") as f:
             f.write("Skipped URLs:\n")
             for skipped_url in sorted(self.skipped):
                 f.write("  %s\n" % skipped_url)
@@ -379,32 +364,30 @@ class TestCrawler(object):
             f.write("\n")
 
             if self.errors:
-                f.write("Crawled %d URLs in %d seconds. Failures:\n%s\n" %
-                        (len(self.visited), time.time() - self.started, "\n".join(self.errors)))
+                f.write("Crawled %d URLs in %d seconds. Failures:\n%s\n" % (len(
+                    self.visited), time.time() - self.started, "\n".join(self.errors)))
 
         # Copy the previous file for analysis
         #if os.path.exists(self.report_file()):
         #    open(self.report_file()+".old", "w").write(open(self.report_file()).read())
 
-        os.rename(self.report_file()+".tmp", self.report_file())
+        os.rename(self.report_file() + ".tmp", self.report_file())
 
         if self.errors:
             for site_path, test_path in [
-                    ("var/log/web.log", self.web_log_file()),
-                    ("var/log/apache/error_log", self.apache_error_log_file()),
-                ]:
+                ("var/log/web.log", self.web_log_file()),
+                ("var/log/apache/error_log", self.apache_error_log_file()),
+            ]:
                 if self.site.file_exists(site_path):
-                    open(test_path+".tmp", "w").write(self.site.read_file(site_path))
-                    os.rename(test_path+".tmp", test_path)
+                    open(test_path + ".tmp", "w").write(self.site.read_file(site_path))
+                    os.rename(test_path + ".tmp", test_path)
 
-            pytest.fail("Crawled %d URLs in %d seconds. Failures:\n%s" %
-                        (len(self.visited), time.time() - self.started, "\n".join(self.errors)))
-
+            pytest.fail("Crawled %d URLs in %d seconds. Failures:\n%s" % (len(
+                self.visited), time.time() - self.started, "\n".join(self.errors)))
 
     def error(self, msg):
         print(msg)
         self.errors.append(msg)
-
 
     def crawl(self):
         finished = False
@@ -418,10 +401,10 @@ class TestCrawler(object):
             start = time.time()
             last_tick, last_num_visited = time.time(), 0
             while True:
-                now          = time.time()
-                duration     = max(now - start, 1)
-                num_visited  = len(self.visited)
-                num_idle     = len([ w for w in workers if w.idle ])
+                now = time.time()
+                duration = max(now - start, 1)
+                num_visited = len(self.visited)
+                num_idle = len([w for w in workers if w.idle])
                 rate_runtime = num_visited / duration
 
                 if now > last_tick and num_visited > last_num_visited:
@@ -429,15 +412,15 @@ class TestCrawler(object):
                 else:
                     rate_tick = 0
 
-                last_tick        = now
+                last_tick = now
                 last_num_visited = num_visited
 
-                print("Workers: %d (Idle: %d), Rate: %0.2f/s (1sec: %0.2f/s), Duration: %d sec, "
-                      "Visited: %s, Todo: %d" %
-                    (self.num_workers, num_idle, rate_runtime, rate_tick,
-                     duration, num_visited, self.todo.qsize()))
+                print(
+                    "Workers: %d (Idle: %d), Rate: %0.2f/s (1sec: %0.2f/s), Duration: %d sec, "
+                    "Visited: %s, Todo: %d" % (self.num_workers, num_idle, rate_runtime, rate_tick,
+                                               duration, num_visited, self.todo.qsize()))
 
-                if self.todo.qsize() == 0 and all([ w.idle for w in workers ]):
+                if self.todo.qsize() == 0 and all([w.idle for w in workers]):
                     break
                 else:
                     time.sleep(1)

002987b5f3acec96c55b908518cbddb93338867f
Tom Baerwinkel <tb@mathias-kettner.de>
2018-12-17 09:51:14 +0100

    Change azure generic test to new structure
    Change-Id: I65ad6b70c3fc1fff22fe695063ae923a66035752

--- tests/unit/checks/generictests/datasets/azure_storageaccounts.py
+++ tests/unit/checks/generictests/datasets/azure_storageaccounts.py
@@ -39,52 +39,56 @@ info = [
     ['Availability', 'total', '190700.0', 'percent', '1544602380', 'PT1H', 'None'],
 ]
 
-
 _common_discovery = [(u'glastonburydiag381', {}), (u'st0ragetestaccount', {})]
 
 MB = 1024**2
 
 discovery = {sub: _common_discovery for sub in ('', 'flow', 'performance')}
 
-
 checks = {
     '': [
-        (u'glastonburydiag381', 'default', [
+        (u'glastonburydiag381', {}, [
             (0, u'Kind: Storage', []),
             (0, 'Used capacity: 10.03 GB', [('used_space', 10773519964, None, None, 0, None)]),
             (0, u'Location: westeurope', []),
         ]),
-        (u'st0ragetestaccount', {'used_capacity_levels': (2 * MB, 4 * MB)}, [
+        (u'st0ragetestaccount', {
+            'used_capacity_levels': (2 * MB, 4 * MB)
+        }, [
             (0, u'Kind: BlobStorage', []),
-            (1, 'Used capacity: 3.65 MB (warn/crit at 2.00 MB/4.00 MB)', [
-                ('used_space', 3822551, 2 * MB, 4 * MB, 0, None)]),
+            (1, 'Used capacity: 3.65 MB (warn/crit at 2.00 MB/4.00 MB)',
+             [('used_space', 3822551, 2 * MB, 4 * MB, 0, None)]),
             (0, u'Location: westeurope', []),
             (0, u'Monitoring: some value', []),
         ]),
     ],
-    'flow': [
-        (u'glastonburydiag381', 'default', [
-            (0, 'Ingress: 41.20 MB', [('ingress', 43202937, None, None, 0, None)]),
-            (0, 'Egress: 5.57 MB', [('egress', 5835881, None, None, 0, None)]),
-            (0, 'Transactions: 1907', [('transactions', 1907.0, None, None, 0, None)]),
-        ]),
-        (u'st0ragetestaccount', {'egress_levels': (100 * MB, 200 * MB)}, [
-            (0, 'Ingress: 30.88 kB', [('ingress', 31620, None, None, 0, None)]),
-            (2, 'Egress: 226.03 MB (warn/crit at 100.00 MB/200.00 MB)', [
-                ('egress', 237007090, 100 * MB, 200 * MB, 0, None),
-            ]),
-            (0, 'Transactions: 62', [('transactions', 62.0, None, None, 0, None)]),
-        ])
-    ],
+    'flow': [(u'glastonburydiag381', {}, [
+        (0, 'Ingress: 41.20 MB', [('ingress', 43202937, None, None, 0, None)]),
+        (0, 'Egress: 5.57 MB', [('egress', 5835881, None, None, 0, None)]),
+        (0, 'Transactions: 1907', [('transactions', 1907.0, None, None, 0, None)]),
+    ]),
+             (u'st0ragetestaccount', {
+                 'egress_levels': (100 * MB, 200 * MB)
+             }, [
+                 (0, 'Ingress: 30.88 kB', [('ingress', 31620, None, None, 0, None)]),
+                 (2, 'Egress: 226.03 MB (warn/crit at 100.00 MB/200.00 MB)', [
+                     ('egress', 237007090, 100 * MB, 200 * MB, 0, None),
+                 ]),
+                 (0, 'Transactions: 62', [('transactions', 62.0, None, None, 0, None)]),
+             ])],
     'performance': [
-        (u'glastonburydiag381', 'default', [
-            (0, 'Success server latency: 20105 ms', [('server_latency', 20.105, None, None, 0, None)]),
-            (0, 'End-to-end server latency: 37606 ms', [('e2e_latency', 37.606, None, None, 0, None)]),
+        (u'glastonburydiag381', {}, [
+            (0, 'Success server latency: 20105 ms', [('server_latency', 20.105, None, None, 0,
+                                                      None)]),
+            (0, 'End-to-end server latency: 37606 ms', [('e2e_latency', 37.606, None, None, 0,
+                                                         None)]),
             (0, 'Availability: 190700.0%', [('availability', 190700.0, None, None, 0, None)]),
         ]),
-        (u'st0ragetestaccount', 'default', [
-            (0, 'Success server latency: 5624 ms', [('server_latency', 5.624, None, None, 0, None)]),
-            (0, 'End-to-end server latency: 9584 ms', [('e2e_latency', 9.584, None, None, 0, None)]),
+        (u'st0ragetestaccount', {}, [
+            (0, 'Success server latency: 5624 ms', [('server_latency', 5.624, None, None, 0,
+                                                     None)]),
+            (0, 'End-to-end server latency: 9584 ms', [('e2e_latency', 9.584, None, None, 0,
+                                                        None)]),
             (0, 'Availability: 6200.0%', [('availability', 6200.0, None, None, 0, None)]),
         ]),
     ],

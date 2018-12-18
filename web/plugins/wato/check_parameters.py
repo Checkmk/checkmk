51745f3fc241503e095cbfd0a9933b04beb5d229
Marcel Schulte <ms@mathias-kettner.de>
2018-12-18 15:37:08 +0100

    6873 FIX df: made rule being found when searching for "inodes"
    When searching for "inodes" in "host & service parameters" the rule was not found.
    This has been fixed now, as well as searching the plugin catalogue for the same phrase.
    
    Change-Id: I9af97c8b2a000c0164f48996b69d8b89a7c42add

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -6531,6 +6531,7 @@ register_check_parameters(
     "filesystem",
     _("Filesystems (used space and growth)"),
     Dictionary(
+        help=_("This ruleset allows to set parameters for space and inodes usage"),
         elements = filesystem_elements,
         hidden_keys = ["flex_levels"],
         ignored_keys = ["patterns"],

5d56a66da836f4a5755f1e05d897cb04a430df71
Lars Michelsen <lm@mathias-kettner.de>
2018-12-18 15:49:40 +0100

    try inventory automation call: Updated test to changed caching behaviours
    Change-Id: Ie31ef249271da1c434bd751940b8b4f743281cbe

--- tests/integration/cmk_base/test_data_sources.py
+++ tests/integration/cmk_base/test_data_sources.py
@@ -15,37 +15,48 @@ import cmk_base.automations
 # INTEGRATION TESTS
 #
 
+
 @pytest.fixture(scope="module")
 def test_cfg(web, site):
     print "Applying default config"
-    web.add_host("ds-test-host1", attributes={
-        "ipaddress": "127.0.0.1",
-    })
-    web.add_host("ds-test-host2", attributes={
-        "ipaddress": "127.0.0.1",
-    }
-    )
-    web.add_host("ds-test-node1", attributes={
-        "ipaddress": "127.0.0.1",
-    })
-    web.add_host("ds-test-node2", attributes={
-        "ipaddress": "127.0.0.1",
-    })
+    web.add_host(
+        "ds-test-host1", attributes={
+            "ipaddress": "127.0.0.1",
+        })
+    web.add_host(
+        "ds-test-host2", attributes={
+            "ipaddress": "127.0.0.1",
+        })
+    web.add_host(
+        "ds-test-node1", attributes={
+            "ipaddress": "127.0.0.1",
+        })
+    web.add_host(
+        "ds-test-node2", attributes={
+            "ipaddress": "127.0.0.1",
+        })
 
-    web.add_host("ds-test-cluster1", attributes={
-        "ipaddress": "127.0.0.1",
+    web.add_host(
+        "ds-test-cluster1",
+        attributes={
+            "ipaddress": "127.0.0.1",
         },
-        cluster_nodes=[ "ds-test-node1", "ds-test-node2", ],
+        cluster_nodes=[
+            "ds-test-node1",
+            "ds-test-node2",
+        ],
     )
 
-    site.write_file("etc/check_mk/conf.d/ds-test-host.mk",
+    site.write_file(
+        "etc/check_mk/conf.d/ds-test-host.mk",
         "datasource_programs.append(('cat ~/var/check_mk/agent_output/<HOST>', [], ALL_HOSTS))\n")
 
     site.makedirs("var/check_mk/agent_output/")
-    for h in [ "ds-test-host1", "ds-test-host2",
-               "ds-test-node1", "ds-test-node2" ]:
-        site.write_file("var/check_mk/agent_output/%s" % h,
-            file("%s/tests/integration/cmk_base/test-files/linux-agent-output" % repo_path()).read())
+    for h in ["ds-test-host1", "ds-test-host2", "ds-test-node1", "ds-test-node2"]:
+        site.write_file(
+            "var/check_mk/agent_output/%s" % h,
+            file(
+                "%s/tests/integration/cmk_base/test-files/linux-agent-output" % repo_path()).read())
 
     web.activate_changes()
 
@@ -98,15 +109,15 @@ def _patch_data_source_run(monkeypatch, **kwargs):
     _counter_run = 0
 
     defaults = {
-        "_may_use_cache_file"              : False,
-        "_use_outdated_cache_file"         : False,
-        "_max_cachefile_age"               : 0, # check_max_cachefile_age
-        "_no_cache"                        : False,
-        "_use_outdated_persisted_sections" : False,
-        "_do_snmp_scan"                    : False,
-        "_on_error"                        : "raise",
-        "_use_snmpwalk_cache"              : True,
-        "_ignore_check_interval"           : True,
+        "_may_use_cache_file": False,
+        "_use_outdated_cache_file": False,
+        "_max_cachefile_age": 0,  # check_max_cachefile_age
+        "_no_cache": False,
+        "_use_outdated_persisted_sections": False,
+        "_do_snmp_scan": False,
+        "_on_error": "raise",
+        "_use_snmpwalk_cache": True,
+        "_ignore_check_interval": True,
     }
     defaults.update(kwargs)
 
@@ -117,7 +128,8 @@ def _patch_data_source_run(monkeypatch, **kwargs):
         assert self._use_outdated_cache_file == defaults["_use_outdated_cache_file"]
 
         if isinstance(self, cmk_base.data_sources.abstract.CheckMKAgentDataSource):
-            assert self._use_outdated_persisted_sections == defaults["_use_outdated_persisted_sections"]
+            assert self._use_outdated_persisted_sections == defaults[
+                "_use_outdated_persisted_sections"]
 
         elif isinstance(self, cmk_base.data_sources.SNMPDataSource):
             assert self._do_snmp_scan == defaults["_do_snmp_scan"]
@@ -132,7 +144,11 @@ def _patch_data_source_run(monkeypatch, **kwargs):
 
         return result
 
-    monkeypatch.setattr(cmk_base.data_sources.abstract.DataSource, "_run", cmk_base.data_sources.abstract.DataSource.run, raising=False)
+    monkeypatch.setattr(
+        cmk_base.data_sources.abstract.DataSource,
+        "_run",
+        cmk_base.data_sources.abstract.DataSource.run,
+        raising=False)
     monkeypatch.setattr(cmk_base.data_sources.abstract.DataSource, "run", run)
 
 
@@ -140,17 +156,29 @@ def _patch_data_source_run(monkeypatch, **kwargs):
 # When called with an explicit list of hosts the cache is not used by default, the option
 # --cache enables it and --no-cache enforce never to use it
 @pytest.mark.parametrize(("hosts"), [
-    (["ds-test-host1"], {"_max_cachefile_age": 0}),
-    (["ds-test-cluster1"], {"_max_cachefile_age": 90}),
+    (["ds-test-host1"], {
+        "_max_cachefile_age": 0
+    }),
+    (["ds-test-cluster1"], {
+        "_max_cachefile_age": 90
+    }),
     ([], {}),
 ])
 @pytest.mark.parametrize(("cache"), [
     (None, {}),
-    (True, {"_may_use_cache_file": True, "_use_outdated_cache_file": True}),
-    (False, {"_may_use_cache_file": False, "_no_cache": True}),
+    (True, {
+        "_may_use_cache_file": True,
+        "_use_outdated_cache_file": True
+    }),
+    (False, {
+        "_may_use_cache_file": False,
+        "_no_cache": True
+    }),
 ])
 @pytest.mark.parametrize(("force"), [
-    (True, {"_use_outdated_persisted_sections": True}),
+    (True, {
+        "_use_outdated_persisted_sections": True
+    }),
     (False, {}),
 ])
 def test_mode_inventory_caching(test_cfg, hosts, cache, force, monkeypatch):
@@ -173,7 +201,7 @@ def test_mode_inventory_caching(test_cfg, hosts, cache, force, monkeypatch):
         if cache[0] == True:
             cmk_base.modes.check_mk.option_cache()
         elif cache[0] == False:
-            cmk_base.modes.check_mk.option_no_cache() # --no-cache
+            cmk_base.modes.check_mk.option_no_cache()  # --no-cache
 
         options = {}
         if force[0]:
@@ -189,8 +217,7 @@ def test_mode_inventory_caching(test_cfg, hosts, cache, force, monkeypatch):
         else:
             valid_hosts = hosts[0]
 
-        num_runs = len([ h for h in valid_hosts
-                         if not config.is_cluster(h) ])*2
+        num_runs = len([h for h in valid_hosts if not config.is_cluster(h)]) * 2
 
         assert _counter_run == num_runs
     finally:
@@ -205,7 +232,7 @@ def test_mode_inventory_as_check(test_cfg, monkeypatch):
 
 
 def test_mode_discover_marked_hosts(test_cfg, monkeypatch):
-    _patch_data_source_run(monkeypatch, _max_cachefile_age=120) # inventory_max_cachefile_age
+    _patch_data_source_run(monkeypatch, _max_cachefile_age=120)  # inventory_max_cachefile_age
     # TODO: First configure auto discovery to make this test really work
     cmk_base.modes.check_mk.mode_discover_marked_hosts()
     #assert _counter_run == 2
@@ -218,7 +245,11 @@ def test_mode_check_discovery_default(test_cfg, monkeypatch):
 
 
 def test_mode_check_discovery_cached(test_cfg, monkeypatch):
-    _patch_data_source_run(monkeypatch, _max_cachefile_age=120, _use_outdated_cache_file=True, _may_use_cache_file=True)
+    _patch_data_source_run(
+        monkeypatch,
+        _max_cachefile_age=120,
+        _use_outdated_cache_file=True,
+        _may_use_cache_file=True)
 
     try:
         cmk_base.modes.check_mk.option_cache()
@@ -232,7 +263,7 @@ def test_mode_check_discovery_cached(test_cfg, monkeypatch):
 def test_mode_discover_all_hosts(test_cfg, monkeypatch):
     _patch_data_source_run(monkeypatch, _may_use_cache_file=True, _max_cachefile_age=120)
     cmk_base.modes.check_mk.mode_discover({"discover": 1}, [])
-    assert _counter_run == len(config.all_active_realhosts())*2
+    assert _counter_run == len(config.all_active_realhosts()) * 2
 
 
 def test_mode_discover_explicit_hosts(test_cfg, monkeypatch):
@@ -244,7 +275,11 @@ def test_mode_discover_explicit_hosts(test_cfg, monkeypatch):
 
 def test_mode_discover_explicit_hosts_cache(test_cfg, monkeypatch):
     try:
-        _patch_data_source_run(monkeypatch, _max_cachefile_age=120, _may_use_cache_file=True, _use_outdated_cache_file=True)
+        _patch_data_source_run(
+            monkeypatch,
+            _max_cachefile_age=120,
+            _may_use_cache_file=True,
+            _use_outdated_cache_file=True)
         cmk_base.modes.check_mk.option_cache()
         cmk_base.modes.check_mk.mode_discover({"discover": 1}, ["ds-test-host1"])
         assert _counter_run == 2
@@ -255,7 +290,7 @@ def test_mode_discover_explicit_hosts_cache(test_cfg, monkeypatch):
 
 def test_mode_discover_explicit_hosts_no_cache(test_cfg, monkeypatch):
     _patch_data_source_run(monkeypatch, _no_cache=True, _max_cachefile_age=0)
-    cmk_base.modes.check_mk.option_no_cache() # --no-cache
+    cmk_base.modes.check_mk.option_no_cache()  # --no-cache
     cmk_base.modes.check_mk.mode_discover({"discover": 1}, ["ds-test-host1"])
     assert _counter_run == 2
 
@@ -279,7 +314,7 @@ def test_mode_check_explicit_host_cache(test_cfg, monkeypatch):
 
 def test_mode_check_explicit_host_no_cache(test_cfg, monkeypatch):
     _patch_data_source_run(monkeypatch, _no_cache=True, _max_cachefile_age=0)
-    cmk_base.modes.check_mk.option_no_cache() # --no-cache
+    cmk_base.modes.check_mk.option_no_cache()  # --no-cache
     cmk_base.modes.check_mk.mode_check({}, ["ds-test-host1"])
     assert _counter_run == 2
 
@@ -303,18 +338,31 @@ def test_mode_dump_agent_explicit_host_cache(test_cfg, monkeypatch):
 
 def test_mode_dump_agent_explicit_host_no_cache(test_cfg, monkeypatch):
     _patch_data_source_run(monkeypatch, _no_cache=True, _max_cachefile_age=0)
-    cmk_base.modes.check_mk.option_no_cache() # --no-cache
+    cmk_base.modes.check_mk.option_no_cache()  # --no-cache
     cmk_base.modes.check_mk.mode_dump_agent("ds-test-host1")
     assert _counter_run == 2
 
 
 @pytest.mark.parametrize(("scan"), [
-    ("@noscan", {"_do_snmp_scan": False, "_may_use_cache_file": True, "_max_cachefile_age": 120}),
-    ("@scan", {"_do_snmp_scan": True, "_may_use_cache_file": False, "_max_cachefile_age": 0}),
+    ("@noscan", {
+        "_do_snmp_scan": False,
+        "_may_use_cache_file": True,
+        "_max_cachefile_age": 120,
+        "_use_outdated_cache_file": True,
+    }),
+    ("@scan", {
+        "_do_snmp_scan": True,
+        "_may_use_cache_file": False,
+        "_max_cachefile_age": 0
+    }),
 ])
 @pytest.mark.parametrize(("raise_errors"), [
-    ("@raiseerrors", {"_on_error": "raise"}),
-    (None, {"_on_error": "ignore"}),
+    ("@raiseerrors", {
+        "_on_error": "raise"
+    }),
+    (None, {
+        "_on_error": "ignore"
+    }),
 ])
 def test_automation_try_discovery_caching(test_cfg, scan, raise_errors, monkeypatch):
     kwargs = {}
@@ -333,17 +381,31 @@ def test_automation_try_discovery_caching(test_cfg, scan, raise_errors, monkeypa
 
 
 @pytest.mark.parametrize(("raise_errors"), [
-    ("@raiseerrors", {"_on_error": "raise"}),
-    (None, {"_on_error": "ignore"}),
+    ("@raiseerrors", {
+        "_on_error": "raise"
+    }),
+    (None, {
+        "_on_error": "ignore"
+    }),
 ])
 @pytest.mark.parametrize(("scan"), [
-    (None, {"_do_snmp_scan": False}),
-    ("@scan", {"_do_snmp_scan": True}),
+    (None, {
+        "_do_snmp_scan": False
+    }),
+    ("@scan", {
+        "_do_snmp_scan": True
+    }),
 ])
-@pytest.mark.parametrize(("cache"), [
-    ("@cache", {"_max_cachefile_age": 120}), # TODO: Why not _may_use_cache_file=True? like try-discovery
-    (None, {"_max_cachefile_age": 0}),
-])
+@pytest.mark.parametrize(
+    ("cache"),
+    [
+        ("@cache", {
+            "_max_cachefile_age": 120
+        }),  # TODO: Why not _may_use_cache_file=True? like try-discovery
+        (None, {
+            "_max_cachefile_age": 0
+        }),
+    ])
 def test_automation_discovery_caching(test_cfg, scan, cache, raise_errors, monkeypatch):
     kwargs = {}
     kwargs.update(raise_errors[1])
@@ -358,7 +420,7 @@ def test_automation_discovery_caching(test_cfg, scan, cache, raise_errors, monke
     if cache[0] is not None:
         args.append(cache[0])
 
-    args += [ "fixall", "ds-test-host1" ]
+    args += ["fixall", "ds-test-host1"]
 
     _patch_data_source_run(monkeypatch, **kwargs)
 
@@ -369,7 +431,7 @@ def test_automation_discovery_caching(test_cfg, scan, cache, raise_errors, monke
 def test_automation_diag_host_caching(test_cfg, monkeypatch):
     _patch_data_source_run(monkeypatch)
 
-    args = ["ds-test-host1", "agent", "127.0.0.1", None, 6557, 10, 5, 5, None ]
+    args = ["ds-test-host1", "agent", "127.0.0.1", None, 6557, 10, 5, 5, None]
     cmk_base.automations.check_mk.AutomationDiagHost().execute(args)
     assert _counter_run == 2
 
@@ -388,6 +450,7 @@ def test_automation_diag_host_caching(test_cfg, monkeypatch):
 #
 # UNIT TESTS
 #
+
 
 # Automatically refresh caches for each test
 @pytest.fixture(scope="function")
@@ -462,21 +525,25 @@ def test_data_sources_of_hosts(clear_config_caches, monkeypatch):
 
     import cmk_base.data_sources
 
-    all_hosts = [ ("%s|%s" % (name, h["tags"])) for name, h in hosts ]
+    all_hosts = [("%s|%s" % (name, h["tags"])) for name, h in hosts]
     monkeypatch.setattr(config, "all_hosts", all_hosts)
 
     monkeypatch.setattr(config, "datasource_programs", [
-        ( 'echo 1', [], ['ds-host-14', 'all-agents-host', 'all-special-host' ], {} ),
+        ('echo 1', [], ['ds-host-14', 'all-agents-host', 'all-special-host'], {}),
     ])
 
     monkeypatch.setitem(config.special_agents, "jolokia", [
-        ( {}, [], ['special-host-14', 'all-agents-host', 'all-special-host', ], {} ),
+        ({}, [], [
+            'special-host-14',
+            'all-agents-host',
+            'all-special-host',
+        ], {}),
     ])
 
     config.collect_hosttags()
 
     for hostname, host_attrs in hosts:
         sources = cmk_base.data_sources.DataSources(hostname, "127.0.0.1")
-        source_names = [ s.__class__.__name__ for s in sources.get_data_sources() ]
+        source_names = [s.__class__.__name__ for s in sources.get_data_sources()]
         assert host_attrs["sources"] == source_names, \
             "Wrong sources for %s" % hostname

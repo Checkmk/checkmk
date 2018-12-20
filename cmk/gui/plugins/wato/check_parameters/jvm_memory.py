f682263c94e770c941b68be443847f4b4b6f1318
Kenneth Okoh <ko@mathias-kettner.de>
2018-12-20 14:10:02 +0100

    Refactoring: Moved more application check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I1aff984513e7a0da0ee65789082a8d1d37626edf

---
+++ cmk/gui/plugins/wato/check_parameters/jvm_memory.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Alternative,
+    Dictionary,
+    Integer,
+    Percentage,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications, "jvm_memory", _("JVM memory levels"),
+    Dictionary(
+        help=_("This rule allows to set the warn and crit levels of the heap / "
+               "non-heap and total memory area usage on web application servers. "
+               "Other keywords for this rule: Tomcat, Jolokia, JMX. "),
+        elements=[
+            ("totalheap",
+             Alternative(
+                 title=_("Total Memory Levels"),
+                 elements=[
+                     Tuple(
+                         title=_("Percentage levels of used space"),
+                         elements=[
+                             Percentage(title=_("Warning at"), label=_("% usage")),
+                             Percentage(title=_("Critical at"), label=_("% usage")),
+                         ]),
+                     Tuple(
+                         title=_("Absolute free space in MB"),
+                         elements=[
+                             Integer(title=_("Warning if below"), unit=_("MB")),
+                             Integer(title=_("Critical if below"), unit=_("MB")),
+                         ])
+                 ])),
+            ("heap",
+             Alternative(
+                 title=_("Heap Memory Levels"),
+                 elements=[
+                     Tuple(
+                         title=_("Percentage levels of used space"),
+                         elements=[
+                             Percentage(title=_("Warning at"), label=_("% usage")),
+                             Percentage(title=_("Critical at"), label=_("% usage")),
+                         ]),
+                     Tuple(
+                         title=_("Absolute free space in MB"),
+                         elements=[
+                             Integer(title=_("Warning if below"), unit=_("MB")),
+                             Integer(title=_("Critical if below"), unit=_("MB")),
+                         ])
+                 ])),
+            ("nonheap",
+             Alternative(
+                 title=_("Nonheap Memory Levels"),
+                 elements=[
+                     Tuple(
+                         title=_("Percentage levels of used space"),
+                         elements=[
+                             Percentage(title=_("Warning at"), label=_("% usage")),
+                             Percentage(title=_("Critical at"), label=_("% usage")),
+                         ]),
+                     Tuple(
+                         title=_("Absolute free space in MB"),
+                         elements=[
+                             Integer(title=_("Warning if below"), unit=_("MB")),
+                             Integer(title=_("Critical if below"), unit=_("MB")),
+                         ])
+                 ])),
+            ("perm",
+             Tuple(
+                 title=_("Perm Memory usage"),
+                 elements=[
+                     Percentage(title=_("Warning at"), label=_("% usage")),
+                     Percentage(title=_("Critical at"), label=_("% usage")),
+                 ])),
+        ]),
+    TextAscii(
+        title=_("Name of the virtual machine"),
+        help=_("The name of the application server"),
+        allow_empty=False,
+    ), "dict")

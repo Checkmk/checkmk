05c0ba383bab44a7753495d5b04ff2b398a6c4b9
Kenneth Okoh <ko@mathias-kettner.de>
2018-12-20 13:51:51 +0100

    Refactoring: Moved fireeye and generic check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I99cacd27c12625382aca2617da165ff085e1a29d

---
+++ cmk/gui/plugins/wato/check_parameters/fireeye_lic.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Integer,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications,
+    "fireeye_lic",
+    _("Fireeye Licenses"),
+    Dictionary(elements=[(
+        "days",
+        Tuple(
+            title=_("Levels for Fireeye License Expiration"),
+            elements=[
+                Integer(title="Warning at", default_value=90, unit="days"),
+                Integer(title="Critical at", default_value=120, unit="days"),
+            ]),
+    )]),
+    TextAscii(title=_("License Feature")),
+    match_type="dict",
+)
---
+++ cmk/gui/plugins/wato/check_parameters/fireeye_mailq.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Integer,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications,
+    "fireeye_mailq",
+    _("Fireeye Mail Queues"),
+    Dictionary(elements=[
+        ("deferred",
+         Tuple(
+             title=_("Levels for Deferred Queue length"),
+             elements=[
+                 Integer(title="Warning at", default_value=10, unit="Mails"),
+                 Integer(title="Critical at", default_value=15, unit="Mails"),
+             ])),
+        ("hold",
+         Tuple(
+             title=_("Levels for Hold Queue length"),
+             elements=[
+                 Integer(title="Warning at", default_value=500, unit="Mails"),
+                 Integer(title="Critical at", default_value=700, unit="Mails"),
+             ])),
+        ("drop",
+         Tuple(
+             title=_("Levels for Drop Queue length"),
+             elements=[
+                 Integer(title="Warning at", default_value=10, unit="Mails"),
+                 Integer(title="Critical at", default_value=15, unit="Mails"),
+             ])),
+    ]),
+    None,
+    match_type="dict",
+)

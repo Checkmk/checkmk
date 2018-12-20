57ad24f41756e018260aba26878c5f1e33e6fd3f
Moritz Kiemer <mo@mathias-kettner.de>
2018-12-20 08:35:40 +0100

    6949 ACME SBC Health (via SNMP)
    This SNMP check reports the health status in percent and the redundancy state of
    an ACME SBC device.
    
    Change-Id: Ifbc2573820dfce036b93a3e2cc40644c6a2f7d8d

---
+++ tests/unit/checks/generictests/datasets/acme_sbc_snmp.py
@@ -0,0 +1,25 @@
+
+
+checkname = 'acme_sbc_snmp'
+
+
+info = [
+    ['20', '2'],
+]
+
+
+discovery = {
+    '': [
+        (None, {}),
+    ],
+}
+
+
+checks = {
+    '': [
+        (None, {'levels_lower': (99, 75)}, [
+            (0, 'Health state: active', []),
+            (2, 'Score: 20% (warn/crit at or below 99%/75%)', []),
+        ]),
+    ],
+}

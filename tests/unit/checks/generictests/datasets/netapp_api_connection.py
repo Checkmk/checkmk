dbdbfdc7c6f4c340098337de6c440af5d871b85b
Florian Kromer <fk@mathias-kettner.de>
2019-01-04 14:14:38 +0100

    netapp_api_connection: add generic data reg test
    Change-Id: I6c1c5e813794ed5e1969fd572c3c05841bc93ffe

---
+++ tests/unit/checks/generictests/datasets/netapp_api_connection.py
@@ -0,0 +1,13 @@
+checkname = 'netapp_api_connection'
+
+info = [['line_0_element_0', 'line_0_element_1'], ['line_1_element_0', 'line_1_element_1'],
+        ['line_2_element_0', 'line_2_element_1', 'line_2_element_2']]
+
+discovery = {'': [(None, [])]}
+
+checks = {
+    '': [(None, {}, [(
+        1,
+        'line_0_element_0 line_0_element_1, line_1_element_0 line_1_element_1, line_2_element_0 line_2_element_1 line_2_element_2',
+        [])])]
+}

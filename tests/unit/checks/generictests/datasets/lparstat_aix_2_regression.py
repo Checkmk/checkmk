132ffac8b5ff342419260c62ad4b151a01213cb6
Moritz Kiemer <mo@mathias-kettner.de>
2019-01-07 13:52:07 +0100

    lparstat_aix: Refactor and remove minor inconsistencies
    CMK-1297
    
    Change-Id: I7357f245f981b54b16154ad11dc56c9677385ef5

---
+++ tests/unit/checks/generictests/datasets/lparstat_aix_2_regression.py
@@ -0,0 +1,75 @@
+
+
+checkname = 'lparstat_aix'
+
+
+info = [[u'System',
+         u'configuration:',
+         u'type=Shared',
+         u'mode=Uncapped',
+         u'smt=4',
+         u'lcpu=8',
+         u'mem=16384MB',
+         u'psize=4',
+         u'ent=1.00'],
+        [u'%user',
+         u'%sys',
+         u'%wait',
+         u'%idle',
+         u'physc',
+         u'%entc',
+         u'lbusy',
+         u'vcsw',
+         u'phint',
+         u'%nsp',
+         u'%utcyc'],
+        [u'-----',
+         u'-----',
+         u'------',
+         u'------',
+         u'-----',
+         u'-----',
+         u'------',
+         u'-----',
+         u'-----',
+         u'-----',
+         u'------'],
+        [u'0.2',
+         u'0.4',
+         u'0.0',
+         u'99.3',
+         u'0.02',
+         u'1.7',
+         u'0.0',
+         u'215',
+         u'3',
+         u'101',
+         u'0.64']]
+
+
+discovery = {'': [(None, 'lparstat_default_levels')],
+             'cpu_util': [(None, 'kernel_util_default_levels')]}
+
+
+checks = {
+    '': [
+        (None, (5, 10), [
+            (0, u'Physc: 0.02', [(u'physc', 0.02, None, None, None, None)]),
+            (0, u'Entc: 1.7%', [(u'entc', 1.7, None, None, None, None)]),
+            (0, u'Lbusy: 0.0', [(u'lbusy', 0.0, None, None, None, None)]),
+            (0, u'Vcsw: 215.0', [(u'vcsw', 215.0, None, None, None, None)]),
+            (0, u'Phint: 3.0', [(u'phint', 3.0, None, None, None, None)]),
+            (0, u'Nsp: 101.0%', [(u'nsp', 101.0, None, None, None, None)]),
+            (0, u'Utcyc: 0.64%', [(u'utcyc', 0.64, None, None, None, None)]),
+        ]),
+    ],
+    'cpu_util': [
+        (None, None, [
+            (0, 'user: 0.2%, system: 0.4%, wait: 0.0%', [
+                ('user', 0.2, None, None, None, None),
+                ('system', 0.4, None, None, None, None),
+                ('wait', 0.0, None, None, None, None),
+            ]),
+        ]),
+    ],
+}
--- tests/unit/checks/generictests/datasets/lparstat_aix_regression.py
+++ tests/unit/checks/generictests/datasets/lparstat_aix_regression.py
@@ -1,6 +1,13 @@
 checkname = 'lparstat_aix'
 
-info = [['0.2', '1.2', '0.2', '98.6', '0.02', '9.3', '0.1', '519', '0', '101', '0.00']]
+info = [
+    [u'System', u'configuration:', u'type=Shared', u'mode=Uncapped', u'smt=4', u'lcpu=8',
+     u'mem=16384MB', u'psize=4', u'ent=1.00'],
+    [u'%user', u'%sys', u'%wait', u'%idle', u'physc', u'%entc', u'lbusy', u'app', u'vcsw',
+     u'phint', u'%nsp'],
+    [u'this line is ignored'],
+    ['0.2', '1.2', '0.2', '98.6', '0.02', '9.3', '0.1', '519', '0', '101', '0.00'],
+]
 
 discovery = {
     '': [(None, 'lparstat_default_levels')],
@@ -8,12 +15,17 @@ discovery = {
 }
 
 checks = {
-    '': [(None, None,
-          [(0, 'Physc: 0.02, Entc: 9.3%, Lbusy: 0.1, App: 519, Vcsw: 0, Phint: 101, Nsp: 0.00%',
-            [('physc', 0.02, None, None, None, None), ('entc', 9.3, None, None, None, None),
-             ('lbusy', 0.1, None, None, None, None), ('app', 519.0, None, None, None, None),
-             ('vcsw', 0.0, None, None, None, None), ('phint', 101.0, None, None, None, None),
-             ('nsp', 0.0, None, None, None, None)])])],
+    '': [
+        (None, None, [
+            (0, 'Physc: 0.02',[('physc', 0.02, None, None, None, None)]),
+            (0, 'Entc: 9.3%', [('entc', 9.3, None, None, None, None)]),
+            (0, 'Lbusy: 0.1', [('lbusy', 0.1, None, None, None, None)]),
+            (0, 'App: 519.0', [('app', 519.0, None, None, None, None)]),
+            (0, 'Vcsw: 0.0', [('vcsw', 0.0, None, None, None, None)]),
+            (0, 'Phint: 101.0', [('phint', 101.0, None, None, None, None)]),
+            (0, 'Nsp: 0.0%', [('nsp', 0.0, None, None, None, None)]),
+        ]),
+    ],
     'cpu_util': [
         (None, None, [(0, 'user: 0.2%, system: 1.2%, wait: 0.2%',
                             [('user', 0.2, None, None, None, None),

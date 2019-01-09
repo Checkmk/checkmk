ac5f2eac86053ad75e00679c94b98891fd068e26
Florian Kromer <fk@mathias-kettner.de>
2019-01-09 18:44:47 +0100

    Icons in mkeventd.py refactored to new plugin registry structure
    CMK-1514
    
    Change-Id: I497aa998166242923a581f5fa4a0bf14c2aa948b

--- cmk/gui/plugins/views/icons/mkeventd.py
+++ cmk/gui/plugins/views/icons/mkeventd.py
@@ -25,84 +25,92 @@
 # Boston, MA 02110-1301 USA.
 
 import cmk.gui.config as config
-from cmk.gui.i18n import _
 from cmk.gui.globals import html
-
-from cmk.gui.plugins.views.icons import multisite_icons_and_actions
-
-
-def paint_mkeventd(what, row, tags, custom_vars):
-    if not config.mkeventd_enabled:
-        return
-
-    # show for services based on the mkevents active check
-    command = row[what + '_check_command']
-
-    if what != 'service' or not command.startswith('check_mk_active-mkevents'):
-        return
-
-    if '!' not in command:
-        return
-
-    host = None
-    app = None
-
-    # Extract parameters from check_command:
-    args = command.split('!')[1].split()
-    if not args:
-        return
-
-    # Handle -a and -H options. Sorry for the hack. We currently
-    # have no better idea
-    if len(args) >= 2 and args[0] == '-H':
-        args = args[2:]  # skip two arguments
-    if len(args) >= 1 and args[0] == '-a':
-        args = args[1:]
-
-    if len(args) >= 1:
-        if args[0] == '$HOSTNAME$':
-            host = row['host_name']
-        elif args[0] == '$HOSTADDRESS$':
-            host = row['host_address']
-        else:
-            host = args[0]
-
-    # If we have no host then the command line from the check_command seems
-    # to be garbled. Better show nothing in this case.
-    if not host:
-        return
-
-    # It is possible to have a central event console, this is the default case.
-    # Another possible architecture is to have an event console in each site in
-    # a distributed environment. For the later case the base url need to be
-    # constructed here
-    url_prefix = ''
-    if getattr(config, 'mkeventd_distributed', False):
-        site = config.site(row["site"])
-        url_prefix = site['url_prefix'] + 'check_mk/'
-
-    url_vars = [
-        ("view_name", "ec_events_of_monhost"),
-        ("site", row["site"]),
-        ("host", row["host_name"]),
-    ]
-
-    title = _('Events of Host %s') % (row["host_name"])
-
-    if len(args) >= 2:
-        app = args[1].strip('\'').replace("\\\\", "\\")
-        title = _('Events of Application "%s" on Host %s') % (app, host)
-        url_vars.append(("event_application", app))
-
-    url = 'view.py?' + html.urlencode_vars(url_vars)
-
-    return 'mkeventd', title, url_prefix + url
+from cmk.gui.i18n import _
+from cmk.gui.plugins.views.icons import Icon, icon_and_action_registry
 
 
-multisite_icons_and_actions["mkeventd"] = {
-    "columns": ["check_command"],
-    "host_columns": ["address", "name"],
-    "paint": paint_mkeventd,
-    "toplevel": False,
-    "sort_index": 30,
-}
+@icon_and_action_registry.register
+class MkeventdIcon(Icon):
+    @classmethod
+    def ident(cls):
+        return "mkeventd"
+
+    def default_toplevel(self):
+        return False
+
+    def default_sort_index(self):
+        return 30
+
+    def columns(self):
+        return ["check_command"]
+
+    def host_columns(self):
+        return ["address", "name"]
+
+    def render(self, what, row, tags, custom_vars):
+        if not config.mkeventd_enabled:
+            return
+
+        # show for services based on the mkevents active check
+        command = row[what + '_check_command']
+
+        if what != 'service' or not command.startswith('check_mk_active-mkevents'):
+            return
+
+        if '!' not in command:
+            return
+
+        host = None
+        app = None
+
+        # Extract parameters from check_command:
+        args = command.split('!')[1].split()
+        if not args:
+            return
+
+        # Handle -a and -H options. Sorry for the hack. We currently
+        # have no better idea
+        if len(args) >= 2 and args[0] == '-H':
+            args = args[2:]  # skip two arguments
+        if len(args) >= 1 and args[0] == '-a':
+            args = args[1:]
+
+        if len(args) >= 1:
+            if args[0] == '$HOSTNAME$':
+                host = row['host_name']
+            elif args[0] == '$HOSTADDRESS$':
+                host = row['host_address']
+            else:
+                host = args[0]
+
+        # If we have no host then the command line from the check_command seems
+        # to be garbled. Better show nothing in this case.
+        if not host:
+            return
+
+        # It is possible to have a central event console, this is the default case.
+        # Another possible architecture is to have an event console in each site in
+        # a distributed environment. For the later case the base url need to be
+        # constructed here
+        url_prefix = ''
+        if getattr(config, 'mkeventd_distributed', False):
+            site = config.site(row["site"])
+            url_prefix = site['url_prefix'] + 'check_mk/'
+
+        url_vars = [
+            ("view_name", "ec_events_of_monhost"),
+            ("site", row["site"]),
+            ("host", row["host_name"]),
+        ]
+
+        title = _('Events of Host %s') % (row["host_name"])
+
+        if len(args) >= 2:
+            app = args[1].strip('\'').replace("\\\\", "\\")
+            title = _('Events of Application "%s" on Host %s') % (app, host)
+            url_vars.append(("event_application", app))
+
+        url = 'view.py?' + html.urlencode_vars(url_vars)
+
+        return 'mkeventd', title, url_prefix + url

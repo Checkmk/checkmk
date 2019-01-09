4383e26081db65c2d226a56993a8b2548a484be0
Moritz Kiemer <mo@mathias-kettner.de>
2019-01-09 11:57:22 +0100

    6951 vms_cpu: Levels for utilization
    Levels and averaging can now be configured for the utilization.
    
    Change-Id: I94443f5b6b63a83dabafaa99e42b6c178ae80396

--- tests/unit/checks/generictests/datasets/vms_cpu_regression.py
+++ tests/unit/checks/generictests/datasets/vms_cpu_regression.py
@@ -2,17 +2,23 @@ checkname = 'vms_cpu'
 
 info = [['1', '99.17', '0.54', '0.18', '0.00']]
 
-discovery = {'': [(None, 'vms_cpu_default_levels')]}
+discovery = {'': [(None, {})]}
 
 checks = {
     '': [
-        (None, None, [(0, 'user: 0.5%, system: 0.1%, wait: 0.2%',
-                       [('user', 0.54, None, None, None, None),
-                        ('system', 0.10999999999999827, None, None, None, None),
-                        ('wait', 0.18, 0, 0, None, None)])]),
-        (None, (0.1, 0.5), [(1, 'user: 0.5%, system: 0.1%, wait: 0.2%',
-                             [('user', 0.54, None, None, None, None),
-                              ('system', 0.10999999999999827, None, None, None, None),
-                              ('wait', 0.18, 0.1, 0.5, None, None)])]),
-    ]
+        (None, None, [
+            (0, 'user: 0.54%', [('user', 0.54)]),
+            (0, 'system: 0.11%', [('system', 0.10999999999999827)]),
+            (0, 'wait: 0.18%', [('wait', 0.18)]),
+            (0, 'total cpu: 0.83%', [('util', 0.8299999999999983, None, None, 0, 100)]),
+            (0, "100% corresponding to: 1.00 CPU", [('cpu_entitlement', 1)]),
+        ]),
+        (None, (0.1, 0.5), [
+            (0, 'user: 0.54%', [('user', 0.54)]),
+            (0, 'system: 0.11%', [('system', 0.10999999999999827)]),
+            (1, 'wait: 0.18% (warn/crit at 0.10%/0.50%)', [('wait', 0.18, 0.1, 0.5)]),
+            (0, 'total cpu: 0.83%', [('util', 0.8299999999999983, None, None, 0, 100)]),
+            (0, "100% corresponding to: 1.00 CPU", [('cpu_entitlement', 1)]),
+        ]),
+    ],
 }

250226948dedc9d02aba439c66236f226ac03e3f
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-10 13:25:20 +0100

    Refactoring: Moved emcvnx and mongodb check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I7324647ea46e2396932206fcaafa039e33e5fb59

---
+++ cmk/gui/plugins/wato/check_parameters/emcvnx_storage_pools.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Percentage,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersStorage,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersStorage,
+    "emcvnx_storage_pools",
+    _("EMC VNX storage pools"),
+    Dictionary(elements=[
+        ("percent_full",
+         Tuple(
+             title=_("Upper levels for physical capacity in percent"),
+             elements=[
+                 Percentage(title=_("Warning at"), default_value=70.0),
+                 Percentage(title=_("Critical at"), default_value=90.0),
+             ])),
+    ]),
+    TextAscii(title=_("Pool name")),
+    match_type="dict",
+)
---
+++ cmk/gui/plugins/wato/check_parameters/mongodb_mem.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Filesize,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersStorage,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersStorage,
+    "mongodb_mem",
+    _("MongoDB Memory"),
+    Dictionary(
+        title=_("MongoDB Memory"),
+        elements=[
+            ("resident_levels",
+             Tuple(
+                 title=_("Resident memory usage"),
+                 help=
+                 _("The value of resident is roughly equivalent to the amount of RAM, "
+                   "currently used by the database process. In normal use this value tends to grow. "
+                   "In dedicated database servers this number tends to approach the total amount of system memory."
+                  ),
+                 elements=[
+                     Filesize(title=_("Warning at"), default_value=1 * 1024**3),
+                     Filesize(title=_("Critical at"), default_value=2 * 1024**3),
+                 ],
+             )),
+            ("mapped_levels",
+             Tuple(
+                 title=_("Mapped memory usage"),
+                 help=_(
+                     "The value of mapped shows the amount of mapped memory by the database. "
+                     "Because MongoDB uses memory-mapped files, this value is likely to be to be "
+                     "roughly equivalent to the total size of your database or databases."),
+                 elements=[
+                     Filesize(title=_("Warning at"), default_value=1 * 1024**3),
+                     Filesize(title=_("Critical at"), default_value=2 * 1024**3),
+                 ],
+             )),
+            ("virtual_levels",
+             Tuple(
+                 title=_("Virtual memory usage"),
+                 help=_(
+                     "Virtual displays the quantity of virtual memory used by the mongod process. "
+                 ),
+                 elements=[
+                     Filesize(title=_("Warning at"), default_value=2 * 1024**3),
+                     Filesize(title=_("Critical at"), default_value=4 * 1024**3),
+                 ],
+             )),
+        ]),
+    None,
+    match_type="dict")

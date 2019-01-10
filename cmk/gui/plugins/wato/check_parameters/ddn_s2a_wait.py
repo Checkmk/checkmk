43460ff5302afeff436e1104b73896a73c2017f1
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-10 17:01:31 +0100

    Refactoring: Moved more storage check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I3197a257ca0bc65ac3610eb19382a5316763ee97

---
+++ cmk/gui/plugins/wato/check_parameters/ddn_s2a_wait.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    DropdownChoice,
+    Float,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersStorage,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersStorage, "ddn_s2a_wait", _(u"Read/write wait for DDN S2A devices"),
+    Dictionary(elements=[
+        ("read_avg",
+         Tuple(
+             title=_(u"Read wait average"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="s"),
+                 Float(title=_(u"Critical at"), unit="s"),
+             ])),
+        ("read_min",
+         Tuple(
+             title=_(u"Read wait minimum"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="s"),
+                 Float(title=_(u"Critical at"), unit="s"),
+             ])),
+        ("read_max",
+         Tuple(
+             title=_(u"Read wait maximum"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="s"),
+                 Float(title=_(u"Critical at"), unit="s"),
+             ])),
+        ("write_avg",
+         Tuple(
+             title=_(u"Write wait average"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="s"),
+                 Float(title=_(u"Critical at"), unit="s"),
+             ])),
+        ("write_min",
+         Tuple(
+             title=_(u"Write wait minimum"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="s"),
+                 Float(title=_(u"Critical at"), unit="s"),
+             ])),
+        ("write_max",
+         Tuple(
+             title=_(u"Write wait maximum"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="s"),
+                 Float(title=_(u"Critical at"), unit="s"),
+             ])),
+    ]),
+    DropdownChoice(title=_(u"Host or Disk"), choices=[
+        ("Disk", _(u"Disk")),
+        ("Host", _(u"Host")),
+    ]), "dict")
---
+++ cmk/gui/plugins/wato/check_parameters/storage_iops.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Float,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersStorage,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersStorage, "storage_iops", _(u"I/O operations for DDN S2A devices"),
+    Dictionary(elements=[
+        ("read",
+         Tuple(
+             title=_(u"Read IO operations per second"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="1/s"),
+                 Float(title=_(u"Critical at"), unit="1/s"),
+             ])),
+        ("write",
+         Tuple(
+             title=_(u"Write IO operations per second"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="1/s"),
+                 Float(title=_(u"Critical at"), unit="1/s"),
+             ])),
+        ("total",
+         Tuple(
+             title=_(u"Total IO operations per second"),
+             elements=[
+                 Float(title=_(u"Warning at"), unit="1/s"),
+                 Float(title=_(u"Critical at"), unit="1/s"),
+             ])),
+    ]), TextAscii(title=_(u"Port index or 'Total'")), "dict")

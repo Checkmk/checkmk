9ff2ab337d144f6cab215e566d8274c57e7be290
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-11 17:21:03 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I86b9a48da8237f6d28e435689a16a45fa1f46051

---
+++ cmk/gui/plugins/wato/check_parameters/juniper_cpu_util.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Percentage,
+    TextAscii,
+    Transform,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersOperatingSystem,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersOperatingSystem,
+    "juniper_cpu_util",
+    _("Juniper Processor Utilization of Routing Engine"),
+    Transform(
+        Dictionary(
+            help=_("CPU utilization of routing engine."),
+            optional_keys=[],
+            elements=[
+                (
+                    "levels",
+                    Tuple(
+                        title=_("Specify levels in percentage of processor routing engine usage"),
+                        elements=[
+                            Percentage(title=_("Warning at"), default_value=80.0),
+                            Percentage(title=_("Critical at"), default_value=90.0),
+                        ],
+                    ),
+                ),
+            ]),
+        forth=lambda old: not old and {'levels': (80.0, 90.0)} or old,
+    ),
+    TextAscii(title=_("Routing Engine"),),
+    "dict",
+)

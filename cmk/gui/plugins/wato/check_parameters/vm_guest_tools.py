c16e3471c1abd6d9fb6e564403de9a59c91d9654
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-11 17:55:38 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I71e503a89f7682286a375da6476dd34232104711

---
+++ cmk/gui/plugins/wato/check_parameters/vm_guest_tools.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    MonitoringState,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersOperatingSystem,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersOperatingSystem,
+    "vm_guest_tools",
+    _("Virtual machine (for example ESX) guest tools status"),
+    Dictionary(
+        optional_keys=False,
+        elements=[
+            ("guestToolsCurrent",
+             MonitoringState(
+                 title=_("VMware Tools is installed, and the version is current"),
+                 default_value=0,
+             )),
+            ("guestToolsNeedUpgrade",
+             MonitoringState(
+                 title=_("VMware Tools is installed, but the version is not current"),
+                 default_value=1,
+             )),
+            ("guestToolsNotInstalled",
+             MonitoringState(
+                 title=_("VMware Tools have never been installed"),
+                 default_value=2,
+             )),
+            ("guestToolsUnmanaged",
+             MonitoringState(
+                 title=_("VMware Tools is installed, but it is not managed by VMWare"),
+                 default_value=1,
+             )),
+        ]),
+    None,
+    "dict",
+)

8abce32d0b1e986b8a2abb7f2424ff22b70ca616
Alex Zurhake <az@mathias-kettner.de>
2019-01-14 14:36:59 +0100

    CMK-1468: Add to Jenkinsfile:     Build Linux Agents 64bit and 32bit     Make Dist     Build Check MK Package for Centos-6 and Ubuntu 18.04
    Add Dcokerfiles for:
        Agent builder 32bit and 64 bit
    
    Install Binutils and GCC-8 on:
        Centos-6
        Ubuntu 18.04
    
    Change-Id: I6088bed44be630e848ffa6ff0c2aeaa5e50087f2

---
+++ buildscripts/infrastructure/build-nodes/agent-builder/build-image-64.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -ex
+### Build a docker image for ubuntu i386.
+
+### settings
+ARCHITECTURE="amd64"
+DISTRO="feisty"
+CHROOT_PATH="/var/chroot/$DISTRO"
+MIRROR='http://old-releases.ubuntu.com/ubuntu'
+docker_image="ubuntu:$DISTRO"
+
+### make sure that the required tools are installed
+packages="debootstrap schroot apparmor"
+# which docker || packages="$packages docker.io"
+# apt-get install -y $packages
+
+### install a minbase system with debootstrap
+export DEBIAN_FRONTEND=noninteractive
+package_names=(
+    "make"
+    "openssl"
+    "libssl-dev"
+    "libreadline5-dev"
+    "libsqlite3-dev"
+    "libbz2-dev"
+    "ca-certificates"
+    "gcc"
+    "locales"
+    "pkg-config"
+    "wget"
+)
+packages=$(IFS=,; echo "${package_names[*]}")
+# TODO: Would use --force-check-gpg, but not supported on wheezy (root server)
+debootstrap \
+    --components=main,universe \
+    --include="$packages" \
+    --arch $ARCHITECTURE $DISTRO $CHROOT_PATH $MIRROR
+
+## Nun ein kleiner Hack um zu verhindern, dass beim Installieren der Pakete
+# die Daemonen der Pakete gestartet werden. Ist nicht dramatisch, erzeugt
+# aber doofe Fehlermeldungen.
+echo exit 101 > $CHROOT_PATH/usr/sbin/policy-rc.d
+chmod +x $CHROOT_PATH/usr/sbin/policy-rc.d
+
+cp /etc/resolv.conf $CHROOT_PATH/etc/resolv.conf
+
+## kill any processes that are running on chroot
+chroot_pids=$(for p in /proc/*/root; do ls -l $p; done | grep $CHROOT_PATH | cut -d'/' -f3)
+test -z "$chroot_pids" || (kill -9 $chroot_pids; sleep 2)
+
+### unmount /proc
+umount $CHROOT_PATH/proc || echo "Already unmounted"
+
+### create a tar archive from the chroot directory
+tar cfz ubuntu.tgz -C $CHROOT_PATH .
+
+### import this tar archive into a docker image:
+cat ubuntu.tgz | docker import - $docker_image --message "Build with https://github.com/docker-32bit/ubuntu"
+
+# ### push image to Docker Hub
+# docker push $docker_image
+
+### cleanup
+rm ubuntu.tgz
+rm -rf $CHROOT_PATH

aedb1f3cd346720f0800559e281b3d076a4cfb37
Simon Betz <si@mathias-kettner.de>
2019-01-14 13:40:14 +0100

    agent_aws: Regions and services can be configured in rule set 'Amazon Web Services (AWS)'
    Change-Id: I772082d3af8571acfa4c98d207edce81c90e2ed1

--- tests/unit/checks/test_agent_aws.py
+++ tests/unit/checks/test_agent_aws.py
@@ -1,13 +1,15 @@
 import pytest
 
 
-@pytest.mark.parametrize("params,result", [
-    ({
-        "--aws-access-key-id": "user",
-        "--aws-secret-access-key": "d1ng",
-        "--region": 'region-eu',
-    }, ['--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng', '--region', 'region-eu', 'host']),
-])
+@pytest.mark.parametrize("params,result", [({
+    "--aws-access-key-id": "user",
+    "--aws-secret-access-key": "d1ng",
+    "--regions": ['region-eu'],
+    "--services": ['ec2', 'ebs'],
+}, [
+    '--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng', '--regions', 'region-eu',
+    '--services', 'ec2', 'ebs', '--hostname', 'host'
+])])
 def test_aws(check_manager, params, result):
     agent = check_manager.get_special_agent("agent_aws")
     arguments = agent.argument_func(params, "host", "address")

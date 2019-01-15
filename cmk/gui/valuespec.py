ceaca4a38c66d07175bad5781fc0beb18780a7f1
Lars Michelsen <lm@mathias-kettner.de>
2019-01-15 17:28:59 +0100

    7071 FIX Fixed missing form error highlighting for cascaded dropdown fields
    Some dropdown fields were not highlighted in case there are input validation
    errors detected during form submit. This made it hard for the user to find
    the field that needs to be corrected.
    
    Change-Id: I6174434b506e6e3371410e5677ce25d741115b4c

--- cmk/gui/valuespec.py
+++ cmk/gui/valuespec.py
@@ -2053,11 +2053,11 @@ class CascadingDropdown(ValueSpec):
                             self._encoding_type.__name__)
                     vs.validate_datatype(value[1], varprefix + "_%d" % nr)
                 return
-        raise MKUserError(varprefix, _("Value %r is not allowed here.") % value)
+        raise MKUserError(varprefix + "_sel", _("Value %r is not allowed here.") % value)
 
     def validate_value(self, value, varprefix):
         if self._no_preselect and value == self._no_preselect_value:
-            raise MKUserError(varprefix, self._no_preselect_error)
+            raise MKUserError(varprefix + "_sel", self._no_preselect_error)
 
         choices = self.choices()
         for nr, (val, _title, vs) in enumerate(choices):
@@ -2066,7 +2066,7 @@ class CascadingDropdown(ValueSpec):
                     vs.validate_value(value[1], varprefix + "_%d" % nr)
                 ValueSpec.custom_validate(self, value, varprefix)
                 return
-        raise MKUserError(varprefix, _("Value %r is not allowed here.") % (value,))
+        raise MKUserError(varprefix + "_sel", _("Value %r is not allowed here.") % (value,))
 
 
 # The same logic as the dropdown choice, but rendered

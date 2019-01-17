fda56cc19971017c2f6b150f55c3537d491d93b0
Simon Betz <si@mathias-kettner.de>
2019-01-17 15:45:34 +0100

    7010 quantum_libsmall_status: Supports Quantum Tape Library which provides SNMP table .1.3.6.1.4.1.3764
    Change-Id: I902bac18cf73e4ea20f58ca4e5a9fca63ecf696c

---
+++ tests/unit/checks/generictests/datasets/quantum_libsmall_status_12_regression.py
@@ -0,0 +1,29 @@
+
+
+checkname = 'quantum_libsmall_status'
+
+
+info = [[],
+        [[u'1.0', u'1'],
+         [u'2.0', u'1'],
+         [u'3.0', u'1'],
+         [u'4.0', u'1'],
+         [u'5.0', u'1'],
+         [u'6.0', u'1'],
+         [u'7.0', u'1'],
+         [u'8.0', u'2']]]
+
+
+discovery = {'': [(None, None)]}
+
+
+checks = {'': [(None,
+                {},
+                [(0, 'Power: good', []),
+                 (0, 'Cooling: good', []),
+                 (0, 'Control: good', []),
+                 (0, 'Connectivity: good', []),
+                 (0, 'Robotics: good', []),
+                 (0, 'Media: good', []),
+                 (0, 'Drive: good', []),
+                 (0, 'Operator action request: no', [])])]}

a29a8aad3443b3429760af81cc8c9f51a657be9a
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-17 14:27:43 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I15e7df24609d1c4421c472fb1484ecc71cb55128

---
+++ cmk/gui/plugins/wato/check_parameters/vm_snapshots.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Age,
+    Dictionary,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications,
+    "vm_snapshots",
+    _("Virtual Machine Snapshots"),
+    Dictionary(elements=[
+        ("age",
+         Tuple(
+             title=_("Age of the last snapshot"),
+             elements=[
+                 Age(title=_("Warning if older than")),
+                 Age(title=_("Critical if older than"))
+             ])),
+        ("age_oldest",
+         Tuple(
+             title=_("Age of the oldest snapshot"),
+             elements=[
+                 Age(title=_("Warning if older than")),
+                 Age(title=_("Critical if older than"))
+             ])),
+    ]),
+    None,
+    match_type="dict",
+)

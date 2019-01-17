b6ce956e5dfc1e30f8432ccbe7db6271dafcd085
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-17 15:24:28 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: Ieb3c585dff93da1d9a5b446ac45ada4acc377288

---
+++ cmk/gui/plugins/wato/check_parameters/plugs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    DropdownChoice,
+    TextAscii,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersEnvironment,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersEnvironment,
+    "plugs",
+    _("State of PDU Plugs"),
+    DropdownChoice(
+        help=_("This rule sets the required state of a PDU plug. It is meant to "
+               "be independent of the hardware manufacturer."),
+        title=_("Required plug state"),
+        choices=[
+            ("on", _("Plug is ON")),
+            ("off", _("Plug is OFF")),
+        ],
+        default_value="on"),
+    TextAscii(
+        title=_("Plug item number or name"),
+        help=
+        _("Whether you need the number or the name depends on the check. Just take a look to the service description."
+         ),
+        allow_empty=True),
+    match_type="first",
+)
---
+++ cmk/gui/plugins/wato/check_parameters/raid_disk.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    DropdownChoice,
+    TextAscii,
+    Transform,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersStorage,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersStorage, "raid_disk", _("RAID: state of a single disk"),
+    Transform(
+        Dictionary(elements=[
+            (
+                "expected_state",
+                TextAscii(
+                    title=_("Expected state"),
+                    help=_("State the disk is expected to be in. Typical good states "
+                           "are online, host spare, OK and the like. The exact way of how "
+                           "to specify a state depends on the check and hard type being used. "
+                           "Please take examples from discovered checks for reference.")),
+            ),
+            ("use_device_states",
+             DropdownChoice(
+                 title=_("Use device states and overwrite expected status"),
+                 choices=[
+                     (False, _("Ignore")),
+                     (True, _("Use device states")),
+                 ],
+                 default_value=True,
+             )),
+        ]),
+        forth=lambda x: isinstance(x, str) and {"expected_state": x} or x,
+    ),
+    TextAscii(
+        title=_("Number or ID of the disk"),
+        help=_("How the disks are named depends on the type of hardware being "
+               "used. Please look at already discovered checks for examples.")), "first")

a7c925810ca13e7256b597f9fd7fd77d4b4c1eba
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-18 12:17:09 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: Ic790d053426a618b57d41eab2e937918e67fc995

---
+++ cmk/gui/plugins/wato/check_parameters/jvm_tp.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Alternative,
+    Dictionary,
+    Integer,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications, "jvm_tp", _("JVM tomcat threadpool levels"),
+    Dictionary(
+        help=_("This ruleset also covers Tomcat, Jolokia and JMX. "),
+        elements=[
+            ("currentThreadCount",
+             Alternative(
+                 title=_("Current thread count levels"),
+                 elements=[
+                     Tuple(
+                         title=_("Percentage levels of current thread count in threadpool"),
+                         elements=[
+                             Integer(title=_("Warning at"), unit=_(u"%"), allow_empty=False),
+                             Integer(title=_("Critical at"), unit=_(u"%"), allow_empty=False),
+                         ])
+                 ])),
+            ("currentThreadsBusy",
+             Alternative(
+                 title=_("Current threads busy levels"),
+                 elements=[
+                     Tuple(
+                         title=_("Percentage of current threads busy in threadpool"),
+                         elements=[
+                             Integer(title=_("Warning at"), unit=_(u"%"), allow_empty=False),
+                             Integer(title=_("Critical at"), unit=_(u"%"), allow_empty=False),
+                         ])
+                 ])),
+        ]),
+    TextAscii(
+        title=_("Name of the virtual machine and/or<br>threadpool"),
+        help=_("The name of the application server"),
+        allow_empty=False,
+    ), "dict")

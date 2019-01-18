0fe5ebe6a75e972fd095654c712d12aa10343cbf
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-18 11:19:15 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: Ib52ba816e3f291e900bf979c36a1987806250fe3

---
+++ cmk/gui/plugins/wato/check_parameters/sym_brightmail_queues.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Integer,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications,
+    "sym_brightmail_queues",
+    "Symantec Brightmail Queues",
+    Dictionary(
+        help=_("This check is used to monitor successful email delivery through "
+               "Symantec Brightmail Scanner appliances."),
+        elements=[
+            ("connections",
+             Tuple(
+                 title=_("Number of connections"),
+                 elements=[
+                     Integer(title=_("Warning at")),
+                     Integer(title=_("Critical at")),
+                 ])),
+            ("messageRate",
+             Tuple(
+                 title=_("Number of messages delivered"),
+                 elements=[
+                     Integer(title=_("Warning at")),
+                     Integer(title=_("Critical at")),
+                 ])),
+            ("dataRate",
+             Tuple(
+                 title=_("Amount of data processed"),
+                 elements=[
+                     Integer(title=_("Warning at")),
+                     Integer(title=_("Cricital at")),
+                 ])),
+            ("queuedMessages",
+             Tuple(
+                 title=_("Number of messages currently queued"),
+                 elements=[
+                     Integer(title=_("Warning at")),
+                     Integer(title=_("Critical at")),
+                 ])),
+            ("queueSize",
+             Tuple(
+                 title=_("Size of the queue"),
+                 elements=[
+                     Integer(title=_("Warning at")),
+                     Integer(title=_("Critical at")),
+                 ])),
+            ("deferredMessages",
+             Tuple(
+                 title=_("Number of messages in deferred state"),
+                 elements=[
+                     Integer(title=_("Warning at")),
+                     Integer(title=_("Critical at")),
+                 ])),
+        ],
+    ),
+    TextAscii(title=_("Instance name"), allow_empty=True),
+    "dict",
+)

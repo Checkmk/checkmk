8d94784ed8bdb3756a1164ea28a7428dce66a60e
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-18 12:13:13 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: Icb16750e9ea04be57da2f99786d55d00a0b07f53

---
+++ cmk/gui/plugins/wato/check_parameters/citrix_load.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Percentage,
+    Transform,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications,
+    "citrix_load",
+    _("Load of Citrix Server"),
+    Transform(
+        Tuple(
+            title=_("Citrix Server load"),
+            elements=[
+                Percentage(title=_("Warning at"), default_value=85.0, unit="percent"),
+                Percentage(title=_("Critical at"), default_value=95.0, unit="percent"),
+            ]),
+        forth=lambda x: (x[0] / 100.0, x[1] / 100.0),
+        back=lambda x: (int(x[0] * 100), int(x[1] * 100))),
+    None,
+    match_type="first",
+)

e49d6e4fffc0e394bf5b2646826c10e720735e88
Kenneth Okoh <ko@mathias-kettner.de>
2019-01-18 11:23:21 +0100

    Refactoring: Moved check parameters from unsorted.py to dedicated modules (CMK-1393)
    Change-Id: I34b669d38403851245922a74dfaac1716daef9bd

---
+++ cmk/gui/plugins/wato/check_parameters/wmic_process.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Integer,
+    Percentage,
+    TextAscii,
+    Tuple,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    register_check_parameters,
+)
+
+register_check_parameters(
+    RulespecGroupCheckParametersApplications, "wmic_process",
+    _("Memory and CPU of processes on Windows"),
+    Tuple(
+        elements=[
+            TextAscii(
+                title=_("Name of the process"),
+                allow_empty=False,
+            ),
+            Integer(title=_("Memory warning at"), unit="MB"),
+            Integer(title=_("Memory critical at"), unit="MB"),
+            Integer(title=_("Pagefile warning at"), unit="MB"),
+            Integer(title=_("Pagefile critical at"), unit="MB"),
+            Percentage(title=_("CPU usage warning at")),
+            Percentage(title=_("CPU usage critical at")),
+        ],),
+    TextAscii(
+        title=_("Process name for usage in the Nagios service description"), allow_empty=False),
+    "first", False)

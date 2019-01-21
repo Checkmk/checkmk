57b82eb1e03fb3d464575d29a680cac42e4f5022
Tom Baerwinkel <tb@mathias-kettner.de>
2019-01-21 12:55:40 +0100

    lnx_thermal: use get_parsed_item_data decorator
    Add a regression test and reformat the existing regression tests, as
    well.
    
    Change-Id: I4e889660bfb3ce13e832716731de5ce45fb72994

--- tests/unit/checks/generictests/datasets/lnx_thermal.py
+++ tests/unit/checks/generictests/datasets/lnx_thermal.py
@@ -19,28 +19,63 @@ discovery = {
 }
 
 checks = {
-    '': [('Zone 0', {
-             'device_levels_handling': 'devdefault',
-             'levels': (70.0, 80.0)
-         }, [(0, u'57.0 \xb0C', [('temp', 57.0, 127.0, 127.0, None, None)])]),
-         ('Zone 1', {
-             'device_levels_handling': 'devdefault',
-             'levels': (70.0, 80.0)
-         }, [(0, u'65.0 \xb0C', [('temp', 65.0, 95.5, 100.0, None, None)])]),
-         ('Zone 2', {
-             'device_levels_handling': 'devdefault',
-             'levels': (70.0, 80.0)
-         }, [(0, u'44.0 \xb0C', [('temp', 44.0, 70.0, 80.0, None, None)])]),
-         ('Zone 3', {
-             'device_levels_handling': 'devdefault',
-             'levels': (70.0, 80.0)
-         }, [(0, u'48.0 \xb0C', [('temp', 48.0, 77.0, 87.0, None, None)])]),
-         ('Zone 4', {
-             'device_levels_handling': 'devdefault',
-             'levels': (70.0, 80.0)
-         }, [(0, u'20.0 \xb0C', [('temp', 20.0, 70.0, 80.0, None, None)])]),
-         ('Zone 5', {
-             'device_levels_handling': 'devdefault',
-             'levels': (70.0, 80.0)
-         }, [(0, u'27.0 \xb0C', [('temp', 27.0, 70.0, 80.0, None, None)])])]
+    '': [
+        (
+            'Zone 0',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            [(0, u'57.0 \xb0C', [('temp', 57.0, 127.0, 127.0, None, None)])],
+        ),
+        (
+            'Zone 1',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            [(0, u'65.0 \xb0C', [('temp', 65.0, 95.5, 100.0, None, None)])],
+        ),
+        (
+            'Zone 2',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            [(0, u'44.0 \xb0C', [('temp', 44.0, 70.0, 80.0, None, None)])],
+        ),
+        (
+            'Zone 3',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            [(0, u'48.0 \xb0C', [('temp', 48.0, 77.0, 87.0, None, None)])],
+        ),
+        (
+            'Zone 4',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            [(0, u'20.0 \xb0C', [('temp', 20.0, 70.0, 80.0, None, None)])],
+        ),
+        (
+            'Zone 5',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            [(0, u'27.0 \xb0C', [('temp', 27.0, 70.0, 80.0, None, None)])],
+        ),
+        # test for an unknown item
+        (
+            'Zone 6',
+            {
+                'device_levels_handling': 'devdefault',
+                'levels': (70.0, 80.0)
+            },
+            None
+        ),
+    ]
 }

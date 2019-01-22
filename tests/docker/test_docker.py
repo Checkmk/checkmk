2d8da1d47cfa01c20b6b07dd05d0de53f3f6f550
Lars Michelsen <lm@mathias-kettner.de>
2019-01-22 17:57:16 +0100

    use newer version for docker update tests (one that includes bd69c580339091e54abbe44b7fbbc02529841844)
    Change-Id: Ic98b6636a80b3f19fce23327d6b171fd2fbe10ac

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -295,7 +295,7 @@ def test_update(request, client, version):
     # Pick a random old version that we can use to the setup the initial site with
     # Later this site is being updated to the current daily build
     old_version = testlib.CMKVersion(
-        version="1.5.0p5",
+        version="1.5.0p8",
         edition=testlib.CMKVersion.CEE,
         branch="1.5.0",
     )

b19bf93f9bcb7836c5c9fa7202693f62864fccc9
Sven Panne <sp@mathias-kettner.de>
2019-01-23 15:32:08 +0100

    Use CRE header for CRE stuff. :-}
    Change-Id: I4ecfd5c8c9db6d883a2603442ab835eae165ef00

--- cmk/gui/plugins/webapi/inventory.py
+++ cmk/gui/plugins/webapi/inventory.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
-# .------------------------------------------------------------------------.
-# |                ____ _               _        __  __ _  __              |
-# |               / ___| |__   ___  ___| | __   |  \/  | |/ /              |
-# |              | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /               |
-# |              | |___| | | |  __/ (__|   <    | |  | | . \               |
-# |               \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\              |
-# |                                        |_____|                         |
-# |             _____       _                       _                      |
-# |            | ____|_ __ | |_ ___ _ __ _ __  _ __(_)___  ___             |
-# |            |  _| | '_ \| __/ _ \ '__| '_ \| '__| / __|/ _ \            |
-# |            | |___| | | | ||  __/ |  | |_) | |  | \__ \  __/            |
-# |            |_____|_| |_|\__\___|_|  | .__/|_|  |_|___/\___|            |
-# |                                     |_|                                |
-# |                     _____    _ _ _   _                                 |
-# |                    | ____|__| (_) |_(_) ___  _ __                      |
-# |                    |  _| / _` | | __| |/ _ \| '_ \                     |
-# |                    | |__| (_| | | |_| | (_) | | | |                    |
-# |                    |_____\__,_|_|\__|_|\___/|_| |_|                    |
-# |                                                                        |
-# | mathias-kettner.com                                 mathias-kettner.de |
-# '------------------------------------------------------------------------'
-#  This file is part of the Check_MK Enterprise Edition (CEE).
-#  Copyright by Mathias Kettner and Mathias Kettner GmbH.  All rights reserved.
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
 #
-#  Distributed under the Check_MK Enterprise License.
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
 #
-#  You should have  received  a copy of the Check_MK Enterprise License
-#  along with Check_MK. If not, email to mk@mathias-kettner.de
-#  or write to the postal address provided at www.mathias-kettner.de
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
 
 from cmk.gui.inventory import inventory_of_host
 

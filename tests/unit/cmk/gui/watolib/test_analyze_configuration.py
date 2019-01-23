f979f3d63d06e073bb3c915154e09e099324ba10
Lars Michelsen <lm@mathias-kettner.de>
2019-01-23 09:33:16 +0100

    Split up watolib tests
    CMK-1572
    
    Change-Id: Idd4ec85046d97228c771e94a45248026f1f8508d

---
+++ tests/unit/cmk/gui/watolib/test_analyze_configuration.py
@@ -0,0 +1,29 @@
+# Triggers plugin loading of plugins.wato which registers all the plugins
+import cmk.gui.wato  # pylint: disable=unused-import
+import cmk.gui.watolib as watolib
+
+
+def test_registered_ac_tests():
+    registered_plugins = sorted(watolib.ac_test_registry.keys())
+    assert registered_plugins == sorted([
+        'ACTestAlertHandlerEventTypes',
+        'ACTestApacheNumberOfProcesses',
+        'ACTestApacheProcessUsage',
+        'ACTestBackupConfigured',
+        'ACTestBackupNotEncryptedConfigured',
+        'ACTestCheckMKHelperUsage',
+        'ACTestESXDatasources',
+        'ACTestGenericCheckHelperUsage',
+        'ACTestHTTPSecured',
+        'ACTestLDAPSecured',
+        'ACTestLiveproxyd',
+        'ACTestLivestatusUsage',
+        'ACTestNumberOfUsers',
+        'ACTestOldDefaultCredentials',
+        'ACTestPersistentConnections',
+        'ACTestRulebasedNotifications',
+        'ACTestSecureAgentUpdaterTransport',
+        'ACTestSecureNotificationSpoolerMessages',
+        'ACTestSizeOfExtensions',
+        'ACTestTmpfs',
+    ])

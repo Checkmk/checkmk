09fe577051ecf0ca1e5d6b2e91b357d68bf70904
Tom Baerwinkel <tb@mathias-kettner.de>
2019-01-24 18:22:28 +0100

    7055 FIX jolokia_generic: remove duplicate MBean in item
    Werk 6901 introduced an additional word "MBean" in the item of
    jolokia_generic checks. This changed the service description
    to "MBean text1 MBean text2". This werk reintroduces the old
    service description "MBean text1 text2".
    
    If you have services that contain two times the word "MBean"
    a service rediscovery is necessary to obtain the old format.
    
    Change-Id: I11710413f52e2a6ce2e720377c25814a83ccff4b

---
+++ tests/unit/checks/test_jolokia_generic.py
@@ -0,0 +1,20 @@
+import pytest
+from checktestlib import assertDiscoveryResultsEqual, DiscoveryResult
+
+pytestmark = pytest.mark.checks
+
+info = [[u'PingFederate-CUK-CDI', u'TotalRequests', u'64790', u'number'],
+        [u'PingFederate-CUK-CDI', u'MaxRequestTime', u'2649', u'rate']]
+
+
+@pytest.mark.parametrize("check,info,expected_result", [
+    ('jolokia_generic', info, [(u'PingFederate-CUK-CDI TotalRequests', {})]),
+    ('jolokia_generic.rate', info, [(u'PingFederate-CUK-CDI MaxRequestTime', {})]),
+])
+def test_jolokia_generic_discovery(check_manager, check, info, expected_result):
+    parsed = check_manager.get_check('jolokia_generic').run_parse(info)
+    discovered = check_manager.get_check(check).run_discovery(parsed)
+    assertDiscoveryResultsEqual(
+        DiscoveryResult(discovered),
+        DiscoveryResult(expected_result),
+    )

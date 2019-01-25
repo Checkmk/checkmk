c9beb18d93ff3c417413a8a8cc8fc6cb3d6537bc
Tom Baerwinkel <tb@mathias-kettner.de>
2019-01-25 17:30:23 +0100

    6657 Kubernetes checks for node Network and Filesystem usage
    Kubernetes special agent obtains the node statistics of network and
    filesystem usage for each node. Corresponding checks are available at node
    or cluster level.
    
    CMK-1541
    
    Change-Id: I6b6b2182c31c53f9d89e97981a49a7a427645931

---
+++ checkman/k8s_stats.fs
@@ -0,0 +1,12 @@
+title: Kubernetes node and cluster level filesystem usage
+agents: linux
+catalog: containerization/kubernetes
+license: GPL
+distribution: check_mk
+description:
+ This check monitors at cluster and node level the filesystem's usage.
+item:
+ The logical disk partition filesystem.
+
+inventory:
+ One service per filesystem

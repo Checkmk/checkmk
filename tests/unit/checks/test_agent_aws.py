e828341d1b706b88a0d5774976be44f58ecef680
Simon Betz <si@mathias-kettner.de>
2019-01-28 08:26:19 +0100

    agent_aws: Allow choosing specific buckets and fetch tagging information for S3 services
    Change-Id: Ie4be4185bf4216310c31751d185602e7d9f560ae

--- tests/unit/checks/test_agent_aws.py
+++ tests/unit/checks/test_agent_aws.py
@@ -12,10 +12,19 @@ import pytest
     "--aws-access-key-id": "user",
     "--aws-secret-access-key": "d1ng",
     "--regions": ['region-eu'],
-    "--services": {'ec2': None, 's3': None},
+    "--services": {'ec2': None, 's3': {'buckets': 'all'}},
 }, [
     '--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng',
     '--regions', 'region-eu', '--services', 'ec2', 's3', '--hostname', 'host'
+]),({
+    "--aws-access-key-id": "user",
+    "--aws-secret-access-key": "d1ng",
+    "--regions": ['region-eu'],
+    "--services": {'ec2': None, 's3': {'buckets': ('buckets', ['A', 'B'])}},
+}, [
+    '--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng',
+    '--regions', 'region-eu', '--services', 'ec2', 's3', '--buckets', 'A', 'B',
+    '--hostname', 'host'
 ])])
 def test_aws(check_manager, params, result):
     agent = check_manager.get_special_agent("agent_aws")

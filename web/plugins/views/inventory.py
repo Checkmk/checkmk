d434c5e136f6e5b1ce50f7c3d3067466bb8fe75c
Tom Baerwinkel <tb@mathias-kettner.de>
2019-01-31 11:16:42 +0100

    k8s_roles: don't show age in HW/SW inventory
    Change-Id: I9b74742ae4dd7b6fca1742fccf65233de23c9fda

--- web/plugins/views/inventory.py
+++ web/plugins/views/inventory.py
@@ -860,12 +860,11 @@ inventory_displayhints.update({
     },
     ".software.applications.kubernetes.roles:": {
         "title": _("Roles"),
-        "keyorder": ["role", "namespace", "age"],
+        "keyorder": ["role", "namespace"],
     },
 
     ".software.applications.k8s.roles:*.role"                     : {"title"  : _("Name")},
     ".software.applications.k8s.roles:*.namespace"                : {"title"  : _("Namespace")},
-    ".software.applications.k8s.roles:*.age"                      : {"title"  : _("Age")},
     ".software.applications.citrix."                              : { "title" : _("Citrix") },
     ".software.applications.citrix.controller."                   : { "title" : _("Controller") },
     ".software.applications.citrix.controller.controller_version" : { "title" : _("Controller Version"), },

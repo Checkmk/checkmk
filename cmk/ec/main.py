2d901c3b4022fa1e0d54d711df0685ffc4206faf
Sven Panne <sp@mathias-kettner.de>
2019-02-07 13:42:55 +0100

    Tiny simplification: has_canceling_match is always set (just like has_match)
    Backport of b1d123212b.
    CMK-1644
    
    Change-Id: I9cb9781eb05ea108ce19011fe954483cb57922c0

--- cmk/ec/main.py
+++ cmk/ec/main.py
@@ -2545,9 +2545,11 @@ class EventServer(ECServerThread):
         if "cancel_priority" in rule:
             up, lo = rule["cancel_priority"]
             match_priority["has_canceling_match"] = p < prio_from or p > prio_to
+        else:
+            match_priority["has_canceling_match"] = False
 
         if match_priority["has_match"] is False and\
-           match_priority.get("has_canceling_match", False) is False:
+           match_priority["has_canceling_match"] is False:
             return False
 
         return True
@@ -2616,7 +2618,7 @@ class EventServer(ECServerThread):
                 if "cancel_application" in rule and \
                    match_groups.get("match_groups_syslog_application_ok", False) is False:
                     self._logger.info("  did not cancel event, because of wrong syslog application")
-                if "cancel_priority" in rule and match_priority.get("has_canceling_match", False) is False:
+                if "cancel_priority" in rule and match_priority["has_canceling_match"] is False:
                     self._logger.info("  did not cancel event, because of wrong cancel priority")
 
         return False

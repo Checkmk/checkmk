6046d89389f3a18337a0fc28147e6011c9636aa7
Simon Betz <si@mathias-kettner.de>
2019-02-09 13:22:33 +0100

    agent_aws: Separated global services from region dependent services, improved logging, removed dead code
    Change-Id: Idf11ff2ed1b30d80b63b76a644acd8547634704e

--- tests/unit/checks/test_agent_aws.py
+++ tests/unit/checks/test_agent_aws.py
@@ -3,12 +3,12 @@ import pytest
 
 #TODO more tests
 @pytest.mark.parametrize("params,result", [({
-    "aws_access_key_id": "user",
-    "aws_secret_access_key": "d1ng",
+    "access_key_id": "user",
+    "secret_access_key": "d1ng",
     "regions": ['region-eu'],
 }, [
-    '--aws-access-key-id', 'user', '--aws-secret-access-key', 'd1ng',
-    '--regions', 'region-eu', '--hostname', 'host'
+    '--access-key-id', 'user', '--secret-access-key', 'd1ng', '--regions', 'region-eu',
+    '--hostname', 'host'
 ])])
 def test_aws(check_manager, params, result):
     agent = check_manager.get_special_agent("agent_aws")

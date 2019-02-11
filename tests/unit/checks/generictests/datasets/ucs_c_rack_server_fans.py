a0590ded7552542a681029c50aa46c1fed5dfc7a
Ronny Bruska <rb@mathias-kettner.de>
2019-02-11 11:45:32 +0100

    7152 ucs_c_rack_server_fans: New check for monitoring ucs c series
    This werk adds a check reporting the state of ucs c series fans.
    
    CMK-1067
    
    Change-Id: I6457893d531511bc5cce152526b2b5493962a4da

---
+++ tests/unit/checks/generictests/datasets/ucs_c_rack_server_fans.py
@@ -0,0 +1,49 @@
+# yapf: disable
+
+checkname = 'ucs_c_rack_server_fans'
+
+
+info = [['equipmentFan',
+         'dn sys/rack-unit-1/fan-module-1-1/fan-1',
+         'id 1',
+         'operability operable'],
+        ['equipmentFan',
+         'dn sys/rack-unit-1/fan-module-1-1/fan-2',
+         'id 2',
+         'operability operable'],
+        ['equipmentFan',
+         'dn sys/rack-unit-2/fan-module-1-1/fan-1',
+         'id 1',
+         'operability operable'],
+        ['equipmentFan',
+         'dn sys/rack-unit-2/fan-module-1-1/fan-2',
+         'id 2',
+         'operability bla'],
+        ['equipmentFan',
+         'dn sys/rack-unit-2/fan-module-1-1/fan-3',
+         'id 3',
+         'operability blub']]
+
+
+discovery = {'': [('Rack Unit 1 Module 1-1 1', {}),
+                  ('Rack Unit 1 Module 1-1 2', {}),
+                  ('Rack Unit 2 Module 1-1 1', {}),
+                  ('Rack Unit 2 Module 1-1 2', {}),
+                  ('Rack Unit 2 Module 1-1 3', {})]}
+
+
+checks = {'': [('Rack Unit 1 Module 1-1 1',
+                {},
+                [(0, 'Operability Status is operable', [])]),
+               ('Rack Unit 1 Module 1-1 2',
+                {},
+                [(0, 'Operability Status is operable', [])]),
+               ('Rack Unit 2 Module 1-1 1',
+                {},
+                [(0, 'Operability Status is operable', [])]),
+               ('Rack Unit 2 Module 1-1 2',
+                {},
+                [(3, 'Unknown Operability Status: bla', [])]),
+               ('Rack Unit 2 Module 1-1 3',
+                {},
+                [(3, 'Unknown Operability Status: blub', [])])]}

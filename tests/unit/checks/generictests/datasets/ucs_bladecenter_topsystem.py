0360ab3b2c0b9ff82e9d1aa99261aa246b795ec9
Florian Kromer <fk@mathias-kettner.de>
2019-02-11 17:33:58 +0100

    ucs_bladecenter_topsystem: add regression test
    Change-Id: I5cde32e83447d8a2aa10ba17f61a94915843557d

---
+++ tests/unit/checks/generictests/datasets/ucs_bladecenter_topsystem.py
@@ -0,0 +1,15 @@
+checkname = 'ucs_bladecenter_topsystem'
+
+info = [[
+    'topSystem', 'Address 192.168.1.1', 'CurrentTime 2015-07-15T16:40:27.600', 'Ipv6Addr ::',
+    'Mode cluster', 'Name svie23ucsfi01', 'SystemUpTime 125:16:10:53'
+]]
+
+discovery = {'': [(None, None)]}
+
+checks = {
+    '': [(None, {}, [(0, 'Address: 192.168.1.1', []), (0, 'CurrentTime: 2015-07-15T16:40:27.600',
+                                                       []), (0, 'Ipv6Addr: ::', []),
+                     (0, 'Mode: cluster', []), (0, 'Name: svie23ucsfi01', []),
+                     (0, 'SystemUpTime: 125:16:10:53', [])])]
+}

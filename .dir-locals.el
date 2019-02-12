1fc852b12279718ebf9884856b022a2645e2c0aa
Sven Panne <sp@mathias-kettner.de>
2019-02-12 10:01:27 +0100

    Use sass-lint from our local node_modules directory.
    Change-Id: I90331b7f5cc6ed09300cca288a6920b66dc3d99a

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ((c++-mode . ((flycheck-cppcheck-suppressions . ("passedByValue"))))
  (python-mode . ((eval setq flycheck-python-mypy-executable
                        (concat (projectile-locate-dominating-file default-directory dir-locals-file)
-                               "tests/static/run_mypy")))))
+                               "tests/static/run_mypy"))))
+ (scss-mode . ((eval setq flycheck-sass/scss-sass-lint-executable
+                     (concat (projectile-locate-dominating-file default-directory dir-locals-file)
+                             "node_modules/.bin/sass-lint"))))
+ )

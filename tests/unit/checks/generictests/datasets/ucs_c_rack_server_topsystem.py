eb1b5ba6770163e0a878b167c576f5a749dbc58c
Florian Kromer <fk@mathias-kettner.de>
2019-02-22 08:59:11 +0100

    7040 ucs_c_rack_server_topsystem: Topsystem Information
    This werk adds a check which displays information about Cisco UCS C-Series Rack Servers.
    
    CMK-1067
    
    Change-Id: I46d2913fe22745f80501843708080cb4c061f573

---
+++ tests/unit/checks/generictests/datasets/ucs_c_rack_server_topsystem.py
@@ -0,0 +1,23 @@
+# yapf: disable
+
+checkname = 'ucs_c_rack_server_topsystem'
+
+
+info = [['topSystem',
+         'dn sys',
+         'address 192.168.1.1',
+         'currentTime Wed Feb  6 09:12:12 2019',
+         'mode stand-alone',
+         'name CIMC-istreamer2a-etn']]
+
+
+discovery = {'': [(None, None)]}
+
+
+checks = {'': [(None,
+                {},
+                [(0, 'DN: sys', []),
+                 (0, 'IP: 192.168.1.1', []),
+                 (0, 'Date and time: 2019-02-06 09:12:12', []),
+                 (0, 'Mode: stand-alone', []),
+                 (0, 'Name: CIMC-istreamer2a-etn', [])])]}

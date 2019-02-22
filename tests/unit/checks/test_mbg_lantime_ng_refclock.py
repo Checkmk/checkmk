0cd2e88fe6c6240b56f4291b82182e061afb6266
Tom Baerwinkel <tb@mathias-kettner.de>
2019-02-22 09:23:16 +0100

    7061 FIX mbg_lantime_ng_refclock.gps: only apply satellite levels if required
    The check now only applies levels for the number of connected satellites if the device
    substate indicates that a satellite connection is necessary. Substates that require
    a satellite connection are "GPS sync", "GPS tracking", "GPS antenna disconnected",
    "GPS warm boot", "GPS cold boot", "GPS antenna short circuit" and "MRS GPS sync".
    
    Change-Id: Ife770bc1aa278d41962c0eba6f4b132fcd0c41b3

--- tests/unit/checks/test_mbg_lantime_ng_refclock.py
+++ tests/unit/checks/test_mbg_lantime_ng_refclock.py
@@ -9,6 +9,9 @@ meinberg_lantime_2 = [[u'1', u'28', u'3', u'1', u'52', u'62', u'100', u'101', u'
 meinberg_lantime_5 = [[
     u'1', u'14', u'3', u'1', u'150', u'6', u'8', u'0', u'0', u'1', u'not announced'
 ]]
+meinberg_lantime_6 = [[
+    u'1', u'30', u'3', u'1', u'155', u'0', u'8', u'0', u'0', u'0', u'2016-12-31'
+]]
 
 
 @pytest.mark.parametrize(
@@ -43,6 +46,7 @@ def test_check_mbg_lantime_ng_refclock(check_manager, info, item, params, expect
         (meinberg_lantime_1, DiscoveryResult([('1', 'mbg_lantime_refclock_default_levels')])),
         (meinberg_lantime_2, DiscoveryResult([])),  # don't discover GPS clocks
         (meinberg_lantime_5, DiscoveryResult([('1', 'mbg_lantime_refclock_default_levels')])),
+        (meinberg_lantime_6, DiscoveryResult([('1', 'mbg_lantime_refclock_default_levels')])),
     ])
 def test_discovery_mbg_lantime_ng_refclock_gps(check_manager, info, expected):
     check = check_manager.get_check("mbg_lantime_ng_refclock.gps")
@@ -50,23 +54,45 @@ def test_discovery_mbg_lantime_ng_refclock_gps(check_manager, info, expected):
     assertDiscoveryResultsEqual(check, discovery, expected)
 
 
-@pytest.mark.parametrize("info,item,params,expected", [
-    (meinberg_lantime_1, '1', (3, 3),
-     CheckResult([
-         BasicCheckResult(
-             1, 'Type: gps170, Usage: primary, State: not synchronized (GPS antenna disconnected)',
-             None),
-         BasicCheckResult(0, 'Next leap second: 2012-06-30', None),
-         BasicCheckResult(2, 'Satellites: 0/12 (warn/crit below 3/3)', None)
-     ])),
-    (meinberg_lantime_5, '1', (3, 3),
-     CheckResult([
-         BasicCheckResult(0, 'Type: gps170, Usage: primary, State: synchronized (MRS GPS sync)',
-                          None),
-         BasicCheckResult(0, 'Next leap second: not announced', None),
-         BasicCheckResult(0, 'Satellites: 6/8', None)
-     ])),
-])
+@pytest.mark.parametrize(
+    "info,item,params,expected",
+    [
+        (
+            meinberg_lantime_1,
+            '1',
+            (3, 3),
+            CheckResult([
+                BasicCheckResult(
+                    1,
+                    'Type: gps170, Usage: primary, State: not synchronized (GPS antenna disconnected)',
+                    None),
+                BasicCheckResult(0, 'Next leap second: 2012-06-30', None),
+                BasicCheckResult(2, 'Satellites: 0/12 (warn/crit below 3/3)', None)
+            ]),
+        ),
+        (
+            meinberg_lantime_5,
+            '1',
+            (3, 3),
+            CheckResult([
+                BasicCheckResult(
+                    0, 'Type: gps170, Usage: primary, State: synchronized (MRS GPS sync)', None),
+                BasicCheckResult(0, 'Next leap second: not announced', None),
+                BasicCheckResult(0, 'Satellites: 6/8', None)
+            ]),
+        ),
+        (
+            meinberg_lantime_6,
+            '1',
+            (3, 3),
+            CheckResult([
+                BasicCheckResult(
+                    0, 'Type: gps180, Usage: primary, State: synchronized (MRS NTP sync)', None),
+                BasicCheckResult(0, 'Next leap second: 2016-12-31', None),
+                # satellites are not checked here
+            ]),
+        ),
+    ])
 def test_check_mbg_lantime_ng_refclock_gps(check_manager, info, item, params, expected):
     check = check_manager.get_check("mbg_lantime_ng_refclock.gps")
     result = CheckResult(check.run_check(item, params, info))

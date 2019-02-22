d5146e0d320199563b3119304174313db5865ec9
Moritz Kiemer <mo@mathias-kettner.de>
2019-02-22 11:12:52 +0100

    mssql_tablespaces: add test
    Change-Id: I3cd4bb34e26b265dcc0a6b551269923f7f386eea

---
+++ tests/unit/checks/generictests/datasets/mssql_tablespaces_regression.py
@@ -0,0 +1,30 @@
+# yapf: disable
+
+checkname = 'mssql_tablespaces'
+
+
+info = [
+    ['MSSQL_SQLEXPRESS', 'master', '5.25', 'MB', '1.59', 'MB', '2464',
+     'KB', '1096', 'KB', '1024', 'KB', '344', 'KB'],
+]
+
+
+discovery = {
+    '': [
+        ('MSSQL_SQLEXPRESS master', {}),
+    ],
+}
+
+
+checks = {
+    '': [
+        ('MSSQL_SQLEXPRESS master', {}, [
+            (0, 'Size: 5.25 MB', [('size', 5505024.0, None, None, None, None)]),
+            (0, 'Unallocated space: 1.59 MB, 30.3%', []),
+            (0, 'Reserved space: 2.41 MB, 45.8%', []),
+            (0, 'Data: 1.07 MB, 20.4%', []),
+            (0, 'Indexes: 1.00 MB, 19.0%', []),
+            (0, 'Unused: 344.00 kB, 6.4%', []),
+        ]),
+    ],
+}

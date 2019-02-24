eb9490497853055d70804fd32c3cec77e816df04
Simon Betz <si@mathias-kettner.de>
2019-02-24 16:58:22 +0100

    7100 mk_sap_hana: Refactored plugin; Deprecated old checks; Add new checks
    Deprecated checks are:
    sap_hana_filesystem,
    sap_hana_full_backup,
    sap_hana_mem,
    sap_hana_process_list,
    sap_hana_version,
    
    New checks are:
    sap_hana_backup,
    sap_hana_data_volume,
    sap_hana_diskusage,
    sap_hana_ess,
    sap_hana_events,
    sap_hana_license,
    sap_hana_memrate,
    sap_hana_proc,
    sap_hana_replication_status,
    sap_hana_status,
    
    In order to make the SAP HANA checks work you have to install
    the agent plugin {{mk_sap_hana}} on your clients.
    
    If you have installed the old agent plugin then you have to
    perform a rediscovery on these hosts.
    
    Thanks to Robert S. and Werner F.
    
    Change-Id: Ifec16db32f8b83d8712d95f0c37438415e390760

---
+++ cmk/gui/plugins/wato/check_parameters/sap_hana.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Alternative,
+    Tuple,
+    Age,
+    FixedValue,
+    TextAscii,
+    Filesize,
+    Percentage,
+    CascadingDropdown,
+)
+from cmk.gui.plugins.wato import (
+    RulespecGroupCheckParametersApplications,
+    CheckParameterRulespecWithItem,
+    rulespec_registry,
+)
+
+
+@rulespec_registry.register
+class RulespecCheckgroupParametersSapHanaBackup(CheckParameterRulespecWithItem):
+    @property
+    def group(self):
+        return RulespecGroupCheckParametersApplications
+
+    @property
+    def check_group_name(self):
+        return "sap_hana_backup"
+
+    @property
+    def title(self):
+        return _("SAP HANA Backup")
+
+    @property
+    def match_type(self):
+        return "dict"
+
+    @property
+    def parameter_valuespec(self):
+        return Dictionary(elements=[
+            ('backup_age_levels',
+             Alternative(
+                 title=_("Upper levels for the backup age"),
+                 style="dropdown",
+                 elements=[
+                     Tuple(
+                         title=_("Set levels"),
+                         elements=[
+                             Age(title=_("Warning at")),
+                             Age(title=_("Critical at")),
+                         ]),
+                     Tuple(
+                         title=_("No levels"),
+                         elements=[
+                             FixedValue(None, totext=""),
+                             FixedValue(None, totext=""),
+                         ]),
+                 ])),
+        ])
+
+    @property
+    def item_spec(self):
+        return TextAscii(title=_("The instance name and backup type"))
+
+
+@rulespec_registry.register
+class RulespecCheckgroupParametersSapHanaLicense(CheckParameterRulespecWithItem):
+    @property
+    def group(self):
+        return RulespecGroupCheckParametersApplications
+
+    @property
+    def check_group_name(self):
+        return "sap_hana_license"
+
+    @property
+    def title(self):
+        return _("SAP HANA License")
+
+    @property
+    def match_type(self):
+        return "dict"
+
+    @property
+    def parameter_valuespec(self):
+        return Dictionary(elements=[
+            ('license_size',
+             Alternative(
+                 title=_("Upper levels for the license size"),
+                 style="dropdown",
+                 elements=[
+                     Tuple(
+                         title=_("Set levels"),
+                         elements=[
+                             Filesize(title=_("Warning at")),
+                             Filesize(title=_("Critical at")),
+                         ]),
+                     Tuple(
+                         title=_("No levels"),
+                         elements=[
+                             FixedValue(None, totext=""),
+                             FixedValue(None, totext=""),
+                         ]),
+                 ])),
+            ('license_usage_perc',
+             Alternative(
+                 title=_("Upper levels for the license usage"),
+                 style="dropdown",
+                 elements=[
+                     Tuple(
+                         title=_("Set levels"),
+                         elements=[
+                             Percentage(title=_("Warning at")),
+                             Percentage(title=_("Critical at")),
+                         ]),
+                     Tuple(
+                         title=_("No levels"),
+                         elements=[
+                             FixedValue(None, totext=""),
+                             FixedValue(None, totext=""),
+                         ]),
+                 ])),
+        ])
+
+    @property
+    def item_spec(self):
+        return TextAscii(title=_("The instance name"))
+
+
+@rulespec_registry.register
+class RulespecCheckgroupParametersSapHanaMemory(CheckParameterRulespecWithItem):
+    @property
+    def group(self):
+        return RulespecGroupCheckParametersApplications
+
+    @property
+    def check_group_name(self):
+        return "sap_hana_memory"
+
+    @property
+    def title(self):
+        return _("SAP HANA Memory")
+
+    @property
+    def match_type(self):
+        return "dict"
+
+    @property
+    def parameter_valuespec(self):
+        return Dictionary(
+            elements=[
+                ("levels",
+                 CascadingDropdown(
+                     title=_("Levels for memory usage"),
+                     choices=[
+                         ("perc_used", _("Percentual levels for used memory"),
+                          Tuple(
+                              elements=[
+                                  Percentage(
+                                      title=_("Warning at a memory usage of"),
+                                      default_value=80.0,
+                                      maxvalue=None),
+                                  Percentage(
+                                      title=_("Critical at a memory usage of"),
+                                      default_value=90.0,
+                                      maxvalue=None)
+                              ],)),
+                         ("abs_free", _("Absolute levels for free memory"),
+                          Tuple(
+                              elements=[
+                                  Filesize(title=_("Warning below")),
+                                  Filesize(title=_("Critical below"))
+                              ],)),
+                         ("ignore", _("Do not impose levels")),
+                     ],
+                 )),
+            ],
+            optional_keys=[],
+        )
+
+    @property
+    def item_spec(self):
+        return TextAscii(title=_("The instance name"))

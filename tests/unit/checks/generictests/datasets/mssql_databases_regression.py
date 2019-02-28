a335c8badf396ea7f5251ac62c396e0c4dea63f4
Moritz Kiemer <mo@mathias-kettner.de>
2019-02-28 12:18:03 +0100

    mssql_databases: prepare error handling.
    CMK-1570
    
    Change-Id: Ic59dc85e753aae54154294e00550761eb8c59715

--- tests/unit/checks/generictests/datasets/mssql_databases_regression.py
+++ tests/unit/checks/generictests/datasets/mssql_databases_regression.py
@@ -8,14 +8,18 @@ info = [['MSSQL_MSSQL46', 'CorreLog_Report_T', 'ONLINE', 'FULL', '0', '0'],
         ['MSSQL_MSSQL46', 'msdb', 'ONLINE', 'SIMPLE', '0', '0'],
         ['MSSQL_MSSQL46', 'NOC_ALARM_T', 'ONLINE', 'FULL', '0', '1'],
         ['MSSQL_MSSQL46', 'test_autoclose', 'ONLINE', 'FULL', '1', '0'],
-        ['MSSQL_MSSQL46', 'test_autoclose', 'RECOVERY', 'PENDING', 'FULL', '1', '0']]
+        ['MSSQL_MSSQL46', 'test_autoclose', 'RECOVERY', 'PENDING', 'FULL', '1', '0'],
+        ['MSSQL_Mouse', '-', 'ERROR: We are out of cheese!', '-', '-', '-']
+        ]
 
 
 discovery = {'': [('MSSQL_MSSQL46 CorreLog_Report_T', {}),
                   ('MSSQL_MSSQL46 NOC_ALARM_T', {}),
                   ('MSSQL_MSSQL46 master', {}),
                   ('MSSQL_MSSQL46 msdb', {}),
-                  ('MSSQL_MSSQL46 test_autoclose', {})]}
+                  ('MSSQL_MSSQL46 test_autoclose', {}),
+                  ('MSSQL_Mouse -', {}),
+                  ]}
 
 
 checks = {'': [('MSSQL_MSSQL46 CorreLog_Report_T',
@@ -47,4 +51,8 @@ checks = {'': [('MSSQL_MSSQL46 CorreLog_Report_T',
                 [(0, 'Status: ONLINE', []),
                  (0, 'Recovery: FULL', []),
                  (1, 'Auto close: on', []),
-                 (0, 'Auto shrink: off', [])])]}
+                 (0, 'Auto shrink: off', [])]),
+                ('MSSQL_Mouse -',
+                 {},
+                 [(2, 'We are out of cheese!')])
+                ]}

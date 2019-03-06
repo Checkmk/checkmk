e751d26769c5b9089d3a8905b8fcec66d9b331b2
Simon Betz <si@mathias-kettner.de>
2019-03-06 11:04:43 +0100

    ibm_svc_*: Special agent provides a header, otherwise use a default header
    CMK-1618
    
    Change-Id: I6bf4c04db38af1aca8e717508a684ef27cdf2f01

---
+++ tests/unit/checks/generictests/datasets/ibm_svc_enclosure_regression_9.py
@@ -0,0 +1,25 @@
+# yapf: disable
+
+
+checkname = 'ibm_svc_enclosure'
+
+
+info = [[u'0',
+         u'online',
+         u'control',
+         u'9843-AE2',
+         u'6860407',
+         u'2',
+         u'2',
+         u'2',
+         u'12']]
+
+
+discovery = {'': [(u'0', {})]}
+
+
+checks = {'': [(u'0',
+                {},
+                [(0, u'Status: online', []),
+                 (0, u'Online canisters: 2', []),
+                 (0, u'Online PSUs: 2', [])])]}

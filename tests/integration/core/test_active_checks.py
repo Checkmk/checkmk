482530daa4fca5871fdbb0c16118114c2929e099
Lars Michelsen <lm@mathias-kettner.de>
2019-03-06 11:40:33 +0100

    test_active_check: Reduced number of core changes
    Change-Id: Ie8692e72c85402a816e8f670e743b88df8015365

--- tests/integration/core/test_active_checks.py
+++ tests/integration/core/test_active_checks.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
+# pylint: disable=redefined-outer-name
 
+import collections
 import pytest
 
-from testlib import web
+from testlib import web  # pylint: disable=unused-import
 
 import cmk_base.config as config
 
+DefaultConfig = collections.namedtuple("DefaultConfig", ["core"])
 
-@pytest.fixture(scope="module")
-def test_cfg(web, site):
+
+@pytest.fixture(scope="module", params=["nagios", "cmc"])
+def test_cfg(request, web, site):
+    config = DefaultConfig(core=request.param)
+    site.set_config("CORE", config.core, with_restart=True)
+
     print "Applying default config"
     web.add_host(
         "test-host", attributes={
@@ -18,7 +25,7 @@ def test_cfg(web, site):
         })
 
     web.activate_changes()
-    yield None
+    yield config
 
     #
     # Cleanup code
@@ -28,10 +35,7 @@ def test_cfg(web, site):
     web.delete_host("test-host")
 
 
-@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
-def test_active_check_execution(test_cfg, site, web, core):
-    site.set_config("CORE", core, with_restart=True)
-
+def test_active_check_execution(test_cfg, site, web):
     try:
         web.set_ruleset(
             "custom_checks",
@@ -75,10 +79,7 @@ def test_active_check_execution(test_cfg, site, web, core):
         web.activate_changes()
 
 
-@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
-def test_active_check_macros(test_cfg, site, web, core):
-    site.set_config("CORE", core, with_restart=True)
-
+def test_active_check_macros(test_cfg, site, web):
     macros = {
         "$HOSTADDRESS$": "127.0.0.1",
         "$HOSTNAME$": "test-host",
@@ -141,7 +142,7 @@ def test_active_check_macros(test_cfg, site, web, core):
 
             expected_output = "Output: %s" % value
             # TODO: Cleanup difference between nagios/cmc
-            if core == "nagios":
+            if test_cfg.core == "nagios":
                 expected_output = expected_output.strip()
                 if var == "$_HOSTTAGS$":
                     splitted_output = plugin_output.split(" ")

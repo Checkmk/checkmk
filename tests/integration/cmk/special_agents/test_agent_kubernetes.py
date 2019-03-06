d69091a1cff456e571a07f83ab3c731c5af46143
Tom Baerwinkel <tb@mathias-kettner.de>
2019-03-06 08:41:05 +0100

    Allow "null" rules in ClusterRoles in the k8s client
    In some OpenShift installations several ClusterRoles with a value
    of "null" for the rules are created. This is not allowed by the
    official Kubernetes API, but we have to handle it here to be
    able to monitor such installations.
    
    CMK-1738
    
    Change-Id: I275769560c0fa968cfd8f3fa9fd2235ecfdda4bc

---
+++ tests/integration/cmk/special_agents/test_agent_kubernetes.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kubernetes.client.models import V1ClusterRole
+
+
+def test_v1_clusterrole_without_rules():
+    try:
+        role = V1ClusterRole(rules=None)
+    except ValueError:
+        pytest.fail(
+            "It must be possible to instantiate a V1ClusterRole without explicit roles. "
+            "This is assured by patch 0020-kubernetes-allow-empty-rules-in-clusterrole.dif.")
+
+    assert role.rules == []

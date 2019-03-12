1260fd6f33847616d98318e5b9db9be060148e62
Alex Zurhake <az@mathias-kettner.de>
2019-03-12 09:19:01 +0100

    add libpq.so.5 as dependency needed by psycopg2
    Change-Id: Ife7130dc797c1e7d4426ef7d1b82ee74592fdcd1

--- omd/distros/SLES_12.mk
+++ omd/distros/SLES_12.mk
@@ -39,6 +39,7 @@ OS_PACKAGES    += libpcap1 # needed for ICMP of CMC
 OS_PACKAGES     += libffi4 # needed for pyOpenSSL and dependant
 OS_PACKAGES    += libjpeg62 # needed by PIL
 OS_PACKAGES    += libgthread-2_0-0 # Needed by cmc (rrd library)
+OS_PACKAGES    += libpq5
 USERADD_OPTIONS   = -M
 ADD_USER_TO_GROUP = gpasswd -a %(user)s %(group)s
 PACKAGE_INSTALL   = zypper -n refresh ; zypper -n install

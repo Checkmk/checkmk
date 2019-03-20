3825416656f0a06681704b1c5bc90cb0cb881b3c
Simon Betz <si@mathias-kettner.de>
2019-03-20 10:42:18 +0100

    Fixed integration test: test_automation_get_agent_output
    Change-Id: Ie24d37ac08576ccc57f2e0cfe5bfb4baa70ea299

--- cmk_base/modes/check_mk.py
+++ cmk_base/modes/check_mk.py
@@ -391,7 +391,7 @@ def mode_dump_agent(hostname):
         # Show errors of problematic data sources
         has_errors = False
         for source in sources.get_data_sources():
-            source_state, source_output, _source_perfdata = source.get_summary_result()
+            source_state, source_output, _source_perfdata = source.get_summary_result_for_checking()
             if source_state != 0:
                 console.error("ERROR [%s]: %s\n" % (source.id(), source_output))
                 has_errors = True

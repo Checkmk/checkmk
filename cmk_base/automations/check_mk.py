596e7d827984f5f66d158359109c1ff8291e6009
Simon Betz <si@mathias-kettner.de>
2019-03-20 10:42:18 +0100

    Fixed integration test: test_automation_get_agent_output
    Change-Id: Ie24d37ac08576ccc57f2e0cfe5bfb4baa70ea299

--- cmk_base/automations/check_mk.py
+++ cmk_base/automations/check_mk.py
@@ -1393,7 +1393,7 @@ class AutomationGetAgentOutput(Automation):
 
                 # Optionally show errors of problematic data sources
                 for source in sources.get_data_sources():
-                    source_state, source_output, _source_perfdata = source.get_summary_result()
+                    source_state, source_output, _source_perfdata = source.get_summary_result_for_checking()
                     if source_state != 0:
                         success = False
                         output += "[%s] %s\n" % (source.id(), source_output)

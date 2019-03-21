6e55d07349d31e51612b471372d82753a78d631c
Konstantin Büttner <kb@mathias-kettner.de>
2019-03-21 15:46:44 +0100

    6988 FIX netstat: Fix crashing check on AIX 7
    The netstat command separates the port from the IP address with a dot (.) instead
    of a colon (:) on some AIX versions, which the check previously could not parse
    correctly. This has been fixed.
    
    Change-Id: I600625b8a5f9d4e7725c646ea082330a93643225

---
+++ tests/unit/checks/test_netstat_parse.py
@@ -0,0 +1,18 @@
+import pytest
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize(
+    "info,expected_parsed",
+    [
+        ([], []),
+        ([["tcp", "0", "0", "0.0.0.0:6556", "0.0.0.0:*", "LISTENING"]],
+         [("TCP", ["0.0.0.0", "6556"], ["0.0.0.0", "*"], "LISTENING")]),
+        # Some AIX systems separate the port with a dot (.) instead of a colon (:)
+        ([["tcp4", "0", "0", "127.0.0.1.1234", "127.0.0.1.5678", "ESTABLISHED"]],
+         [("TCP", ["127.0.0.1", "1234"], ["127.0.0.1", "5678"], "ESTABLISHED")]),
+    ])
+def test_parse_netstat(check_manager, info, expected_parsed):
+    parsed = check_manager.get_check('netstat').run_parse(info)
+    assert parsed == expected_parsed

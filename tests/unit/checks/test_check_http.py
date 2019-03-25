d6e350bf830cc358e15f11d625ba616c9c2e0658
Moritz Kiemer <mo@mathias-kettner.de>
2019-03-25 14:23:43 +0100

    7222 FIX check_http: port config for certificate check via proxy
    If users had the active check "Check HTTP" configured to check a certificate
    via a proxy, the option "TCP Port" had wrongly been used as the proxys port
    instead of the certificate servers port.
    
    The port defined in the option "TCP Port" is now applied to the certificate
    server, regardless of whether a proxy is used.
    
    If you relied on the wrong behaviour, remove the option "TCP Port" and include
    the port for the proxy in the proxy settings.
    
    NOTE:
    To make this work, we have to pass proxy address and server name not
    using the "-I proxy.com" and "-H server.com" option, but as the first
    and second argument, respectively. This weird hack bypasses a bug in
    the check_http commandline parsing:
    When we pass the arguments this way, we can include the *servers* port
    in the second argument (e.g. "server.com:8443"). When we try the same
    with "-H server.vom:8443" the port will be stripped, and ignored if
    the "-p" option is set, and used as the proxys port otherwise.
    
    Change-Id: I2f7babed02b652186f996a91e53048a3689c9ebb

--- tests/unit/checks/test_check_http.py
+++ tests/unit/checks/test_check_http.py
@@ -3,40 +3,101 @@ import pytest
 pytestmark = pytest.mark.checks
 
 
-@pytest.mark.parametrize('params,expected_args', [(
-    (None, {
-        'onredirect': 'follow',
-        'port': 80,
-        'uri': '/images',
-        'urlize': True,
-        'virthost': ('www.test123.de', True)
-    }),
-    ['-H', 'www.test123.de', '-p', 80, '-u', '/images', '--onredirect=follow', '-L'],
-), (
-    (None, {
-        'extended_perfdata': True,
-        'method': 'CONNECT',
-        'port': 3128,
-        'proxy': '163.172.86.64',
-        'ssl': 'auto',
-        'uri': '/images',
-        'virthost': ('www.test123.de', True)
-    }),
-    [
-        '-I', '163.172.86.64', '-H', 'www.test123.de', '-p', 3128, '-u', '/images', '--ssl',
-        '--extended-perfdata', '-j', 'CONNECT'
-    ],
-), (
-    (None, {
-        'cert_days': (10, 20),
-        'cert_host': 'www.test123.com'
-    }),
-    ['-I', 'www.test123.com', '-H', '$_HOSTADDRESS_4$', '-C', '10,20'],
-)])
+@pytest.mark.parametrize('params,expected_args', [
+    (
+        (None, {
+            'onredirect': 'follow',
+            'port': 80,
+            'uri': '/images',
+            'urlize': True,
+            'virthost': ('www.test123.de', True)
+        }),
+        ['-H', 'www.test123.de', '-p', 80, '-u', '/images', '--onredirect=follow', '-L'],
+    ),
+    (
+        (None, {
+            'extended_perfdata': True,
+            'method': 'CONNECT',
+            'port': 3128,
+            'proxy': '163.172.86.64',
+            'ssl': 'auto',
+            'uri': '/images',
+            'virthost': ('www.test123.de', True)
+        }),
+        [
+            '-I', '163.172.86.64', '-H', 'www.test123.de', '-p', 3128, '-u', '/images', '--ssl',
+            '--extended-perfdata', '-j', 'CONNECT'
+        ],
+    ),
+    (
+        (None, {
+            'cert_days': (10, 20),
+            'cert_host': 'www.test123.com',
+            'port': '42',
+        }),
+        ['-C', '10,20', '-p', '42', 'www.test123.com'],
+    ),
+    (
+        (None, {
+            'cert_days': (10, 20),
+            'cert_host': 'www.test123.com',
+            'port': '42',
+            'proxy': 'p.roxy',
+        }),
+        ['-C', '10,20', '--ssl', '-j', 'CONNECT', 'p.roxy', 'www.test123.com:42'],
+    ),
+    (
+        (None, {
+            'cert_days': (10, 20),
+            'cert_host': 'www.test123.com',
+            'port': '42',
+            'proxy': 'p.roxy:23',
+        }),
+        ['-C', '10,20', '-p', '23', '--ssl', '-j', 'CONNECT', 'p.roxy', 'www.test123.com:42'],
+    ),
+    (
+        (None, {
+            'cert_days': (10, 20),
+            'cert_host': 'www.test123.com',
+            'port': '42',
+            'proxy': '[dead:beef::face]:23',
+        }),
+        [
+            '-C', '10,20', '-p', '23', '--ssl', '-j', 'CONNECT', '[dead:beef::face]',
+            'www.test123.com:42'
+        ],
+    ),
+    (
+        (None, {
+            'virthost': (None, True),
+            'proxy': "foo.bar",
+        }),
+        ['-I', 'foo.bar'],
+    ),
+    (
+        (None, {
+            'virthost': (None, False),
+            'proxy': "foo.bar",
+        }),
+        ['-I', 'foo.bar'],
+    ),
+    (
+        (None, {
+            'virthost': (None, True),
+        }),
+        [],
+    ),
+    (
+        (None, {
+            'virthost': (None, False),
+        }),
+        ['-I', '$_HOSTADDRESS_4$'],
+    ),
+])
 def test_check_http_argument_parsing(check_manager, params, expected_args):
-    """Tests if all required arguments are present. The tests do not check the order of arguments."""
+    """Tests if all required arguments are present."""
     active_check = check_manager.get_active_check('check_http')
-    assert sorted(active_check.run_argument_function(params)) == sorted(expected_args)
+    assert active_check.run_argument_function(params) == expected_args
 
 
 @pytest.mark.parametrize('params,expected_description', [

2041cdbdda627649e1b711f8131f37bf3789d6dc
Alex Zurhake <az@mathias-kettner.de>
2019-03-27 13:09:40 +0100

    CMK-1899: build CMK docker container
    Change-Id: Ib5084e6d5352cd075317cabbff1c80e517f9ce78

---
+++ buildscripts/scripts/build-cmk-container.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -e
+unset LANG
+
+log() { echo "[$(date '+%F %T')] ==============================" "$@"; }
+die() { log "$@"; exit 1; }
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ]; then
+    echo "Aufrufen: bw-docker-bauen [BRANCH] [EDITION] [VERSION]"
+    echo "          bw-docker-bauen 1.5.0 enterprise 1.5.0p4"
+    echo
+    exit 0
+fi
+
+BRANCH=$1
+EDITION=$2
+VERSION=$3
+
+if [ $EDITION = raw ]; then
+    SUFFIX=.cre
+elif [ $EDITION = enterprise ]; then
+    SUFFIX=.cee
+elif [ $EDITION = managed ]; then
+    SUFFIX=.cme
+else
+    die "FEHLER: Unbekannte Edition '$EDITION'"
+fi
+
+mkdir -p /bauwelt/tmp
+TMP_PATH=$(mktemp --directory -p /bauwelt/tmp --suffix=.cmk-docker)
+DOCKER_PATH="$TMP_PATH/check-mk-${EDITION}-${VERSION}${SUFFIX}/docker"
+DOCKER_IMAGE_ARCHIVE="check-mk-${EDITION}-docker-${VERSION}.tar.gz"
+PKG_NAME="check-mk-${EDITION}-${VERSION}"
+PKG_FILE="${PKG_NAME}_0.stretch_$(dpkg --print-architecture).deb"
+
+trap "rm -rf \"$TMP_PATH\"" SIGTERM SIGHUP SIGINT
+
+log "Unpack source tar to $TMP_PATH"
+tar -xz -C "$TMP_PATH" -f "/bauwelt/download/${VERSION}/check-mk-${EDITION}-${VERSION}${SUFFIX}.tar.gz"
+
+log "Copy debian package..."
+cp "/bauwelt/download/${VERSION}/${PKG_FILE}" "$DOCKER_PATH/"
+
+log "Building container image"
+make -C "$DOCKER_PATH" "$DOCKER_IMAGE_ARCHIVE"
+
+log "Verschiebe Image-Tarball..."
+mv -v "$DOCKER_PATH/$DOCKER_IMAGE_ARCHIVE" "/bauwelt/download/${VERSION}/"
+
+if [ $EDITION = raw ]; then
+    log "Erstelle \"{$BRANCH}-latest\" tag..."
+    docker tag "checkmk/check-mk-${EDITION}:${VERSION}" "checkmk/check-mk-${EDITION}:${BRANCH}-latest"
+
+    log "Erstelle \"latest\" tag..."
+    docker tag "checkmk/check-mk-${EDITION}:${VERSION}" "checkmk/check-mk-${EDITION}:latest"
+
+    log "Lade zu Docker-Hub hoch..."
+    DOCKERCLOUD_NAMESPACE=checkmk docker push "checkmk/check-mk-${EDITION}:${VERSION}"
+    DOCKERCLOUD_NAMESPACE=checkmk docker push "checkmk/check-mk-${EDITION}:${BRANCH}-latest"
+    DOCKERCLOUD_NAMESPACE=checkmk docker push "checkmk/check-mk-${EDITION}:latest"
+fi
+
+log "Räume temporäres Verzeichnis $TMP_PATH weg"
+rm -rf "$TMP_PATH"

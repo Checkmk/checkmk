6123e226bbdce7619da8016017989108d58ba781
Lars Michelsen <lm@mathias-kettner.de>
2019-03-27 08:39:12 +0100

    Refactor hand crafted rule matching out of several checks
    These checks discover services based on service discovery rulesets, which
    are regular host rulesets and don't need specific matching code in the
    checks. They can simply use the generic host_extra_conf() function
    instead of hand individual matching.
    
    Change-Id: Ia5373ecfc877ed67b9e39e3b0a581b45683e1892

--- tests/unit/checks/conftest.py
+++ tests/unit/checks/conftest.py
@@ -14,5 +14,10 @@ def check_manager():
 def clear_config_caches(monkeypatch):
     import cmk_base
     import cmk_base.caching
+    import cmk_base.config
     monkeypatch.setattr(cmk_base, "config_cache", cmk_base.caching.CacheManager())
     monkeypatch.setattr(cmk_base, "runtime_cache", cmk_base.caching.CacheManager())
+
+    monkeypatch.setattr(cmk_base.config, "all_hosts", ["non-existent-testhost"])
+    monkeypatch.setattr(cmk_base.config, "host_paths", {"non-existent-testhost": "/"})
+    cmk_base.config.get_config_cache().initialize()

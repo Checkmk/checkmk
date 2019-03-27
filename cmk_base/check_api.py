a81fbbd879795331fdca94d93de3853f6ce4b02d
Lars Michelsen <lm@mathias-kettner.de>
2019-03-27 12:49:33 +0100

    Use generic host_extra_conf() for filtering host related rules
    This is the only check left that uses all_matching_hosts(). We can avoid
    this by bringing the logwatch_rules ruleset in the format of a host
    ruleset and use the more common host_extra_conf() function.
    
    Change-Id: I745d1d1aac76ebaca5f0b7a5da72f7d7f2e7778e

--- cmk_base/check_api.py
+++ cmk_base/check_api.py
@@ -218,13 +218,13 @@ host_extra_conf_merged = _config.host_extra_conf_merged
 
 # TODO: Only used by logwatch check. Can we clean this up?
 get_rule_options = _config.get_rule_options
-all_matching_hosts = _config.all_matching_hosts
 
 # These functions were used in some specific checks until 1.6. Don't add it to
 # the future check API. It's kept here for compatibility reasons for now.
 in_extraconf_hostlist = _config.in_extraconf_hostlist
 hosttags_match_taglist = _config.hosttags_match_taglist
 tags_of_host = _config.tags_of_host
+all_matching_hosts = _config.all_matching_hosts
 
 nagios_illegal_chars = _config.nagios_illegal_chars
 is_ipv6_primary = _config.is_ipv6_primary

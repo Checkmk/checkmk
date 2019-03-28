bd25414571342a5f9a204230851d6a1142d017df
Moritz Kiemer <mo@mathias-kettner.de>
2019-03-28 11:12:35 +0100

    mk_docker.py: report exceptions to server
    Change-Id: I56fba1a5a68a02107fde945eee9ba11a5047722b

---
+++ tests/unit/checks/generictests/datasets/docker_node_info_unknown.py
@@ -0,0 +1,28 @@
+# yapf: disable
+
+
+checkname = 'docker_node_info'
+
+
+info = [
+    ['@docker_version_info', '{"PluginVersion": "0.1", "DockerPyVersion": "3.7.0", "ApiVersion": "1.39"}'],
+    ['{"Name": "klappson"}'],
+    ['@docker_version_info', '{"PluginVersion": "0.1", "DockerPyVersion": "3.7.0", "ApiVersion": "1.39"}'],
+    ['{"Unknown": "Plugin exception in section_node_disk_usage: Kokosnuss geklaut"}'],
+]
+
+
+discovery = {
+    '': [(None, {})],
+    'containers': [(None, {})],
+}
+
+
+checks = {
+    '': [
+        (None, {}, [
+            (0, u'Daemon running on host klappson', []),
+            (3, u'Plugin exception in section_node_disk_usage: Kokosnuss geklaut', []),
+        ]),
+    ],
+}

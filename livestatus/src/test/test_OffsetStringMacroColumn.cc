457d9985a30be7224cc207fc0e1e23f70ab470a0
Sven Panne <sp@mathias-kettner.de>
2019-04-01 15:32:11 +0200

    First dead simple NEB unit test.
    Change-Id: Ia88b6895f8dfbec5e9847e87d1503ef1ce6d8c9d

--- livestatus/src/test/test_OffsetStringMacroColumn.cc
+++ livestatus/src/test/test_OffsetStringMacroColumn.cc
@@ -1,8 +1,35 @@
 #include <memory>
-#include "StringUtils.h"
+#include "Column.h"
+#include "OffsetStringHostMacroColumn.h"
+#include "Row.h"
 #include "gtest/gtest.h"
+#include "nagios.h"
 
-TEST(OffsetStringMacroColumn, dummy) {
-    EXPECT_EQ("foobar", mk::unsafe_tolower("fOObAr"));
-    EXPECT_EQ("blah  ", mk::lstrip("   blah  "));
+TEST(OffsetStringHostMacroColumn, simple) {
+    // Backwards you must build the list, my young padawan...
+    customvariablesmember cvm1{const_cast<char *>("HARRY"),
+                               const_cast<char *>("HIRSCH"), 0, nullptr};
+    customvariablesmember cvm2{const_cast<char *>("ERNIE"),
+                               const_cast<char *>("BERT"), 0, &cvm1};
+    customvariablesmember *custom_variables = &cvm2;
+    Row row{&custom_variables};
+
+    OffsetStringHostMacroColumn oshmc("funny_column_name", "Cool description!",
+                                      -1, -1, -1, nullptr, 0);
+
+    EXPECT_EQ("funny_column_name", oshmc.name());
+    EXPECT_EQ("Cool description!", oshmc.description());
+    EXPECT_EQ(&custom_variables, oshmc.columnData<void>(row));
+    EXPECT_EQ(ColumnType::string, oshmc.type());
+
+    auto cvm = *oshmc.columnData<const customvariablesmember *const>(row);
+    EXPECT_STREQ("ERNIE", cvm->variable_name);
+    EXPECT_STREQ("BERT", cvm->variable_value);
+
+    cvm = cvm->next;
+    EXPECT_STREQ("HARRY", cvm->variable_name);
+    EXPECT_STREQ("HIRSCH", cvm->variable_value);
+
+    cvm = cvm->next;
+    EXPECT_EQ(nullptr, cvm);
 }

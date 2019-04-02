6835a5999c317933841058e6aa28596be246d29c
Lars Michelsen <lm@mathias-kettner.de>
2019-04-02 17:00:23 +0200

    Let me format this for you...
    https://www.pianojoe.de/code/simpsongenerator/creator/create.php?w=I+will+not+push+my+changes+to+git+without+YAPFing+my+code
    
    Change-Id: I7c145da9b563edbd0c4dfed38f1ef7d5553bb01b

--- cmk/gui/plugins/metrics/check_mk.py
+++ cmk/gui/plugins/metrics/check_mk.py
@@ -4337,19 +4337,19 @@ def register_oracle_metrics():
             "color": color,
         }
 
-    for what, descr, unit, color in [
-        ("oracle_sga_size","SGA","bytes", "11/a"),
-        ("oracle_sga_buffer_cache","Buffer Cache","bytes", "15/a"),
-        ("oracle_sga_shared_pool","Shared Pool","bytes", "21/a"),
-        ("oracle_sga_redo_buffer","Redo Buffer","bytes", "43/b"),
-        ("oracle_sga_java_pool","Java Pool","bytes", "26/a"),
-        ("oracle_sga_large_pool","Large Pool","bytes", "14/a"),
-        ("oracle_sga_streams_pool","Streams Pool","bytes", "16/a")]:
+    for what, descr, unit, color in [("oracle_sga_size", "SGA", "bytes", "11/a"),
+                                     ("oracle_sga_buffer_cache", "Buffer Cache", "bytes", "15/a"),
+                                     ("oracle_sga_shared_pool", "Shared Pool", "bytes", "21/a"),
+                                     ("oracle_sga_redo_buffer", "Redo Buffer", "bytes", "43/b"),
+                                     ("oracle_sga_java_pool", "Java Pool", "bytes", "26/a"),
+                                     ("oracle_sga_large_pool", "Large Pool", "bytes", "14/a"),
+                                     ("oracle_sga_streams_pool", "Streams Pool", "bytes", "16/a")]:
         metric_info["%s" % what] = {
             "title": _("ORACLE %s") % descr,
             "unit": unit,
             "color": color,
         }
+
 
 register_oracle_metrics()
 
@@ -10473,13 +10473,13 @@ graph_info["oracle_library_cache_statistics"] = {
 graph_info["oracle_sga_info"] = {
     "title": _("ORACLE SGA Information"),
     "metrics": [
-        ("oracle_sga_size","line"),
+        ("oracle_sga_size", "line"),
         ("oracle_sga_buffer_cache", "stack"),
-        ("oracle_sga_shared_pool","stack"),
-        ("oracle_sga_redo_buffer","stack"),
-        ("oracle_sga_java_pool","stack"),
-        ("oracle_sga_large_pool","stack"),
-        ("oracle_sga_streams_pool","stack"),
+        ("oracle_sga_shared_pool", "stack"),
+        ("oracle_sga_redo_buffer", "stack"),
+        ("oracle_sga_java_pool", "stack"),
+        ("oracle_sga_large_pool", "stack"),
+        ("oracle_sga_streams_pool", "stack"),
     ],
     "optional_metrics": [
         "oracle_sga_java_pool",

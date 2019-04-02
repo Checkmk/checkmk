3498975396621d311baf9fa3b525c1728cc12331
Óscar Nájera <on@mathias-kettner.de>
2019-04-02 16:19:33 +0200

    New TimeSeries class to describe RRDdata
    This class allows to track performance data and binds to each value the
    measurement time stamp.
    
    It allows for up-sampling data by backward filling values
    
    CMK-1761
    
    Change-Id: I81646be8f009cd1bbd4efec0ca9c298abe002c4d

--- tests/unit/cmk/utils/test_prediction_commons.py
+++ tests/unit/cmk/utils/test_prediction_commons.py
@@ -3,6 +3,26 @@ import pytest
 import cmk.utils.prediction as prediction
 
 
+@pytest.mark.parametrize("twindow, result", [((0, 0, 0), []),
+                                             ((100, 200, 25), [125, 150, 175, 200])])
+def test_rrdtimestamps(twindow, result):
+    assert prediction.rrd_timestamps(twindow) == result
+
+
+@pytest.mark.parametrize("rrddata, twindow, shift, upsampled", [
+    ([10, 20, 10, 20], (10, 20, 10), 0, [20]),
+    ([10, 20, 10, 20], (10, 20, 5), 0, [20, 20]),
+    ([10, 20, 10, 20], (20, 30, 5), 10, [20, 20]),
+    ([0, 120, 40, 25, 65, 105], (300, 400, 10), 300, [25, 25, 25, 25, 65, 65, 65, 65, 105, 105]),
+    ([0, 120, 40, 25, None, 105],
+     (300, 400, 10), 300, [25, 25, 25, 25, None, None, None, None, 105, 105]),
+    ([0, 120, 40, 25, 65, 105], (330, 410, 10), 300, [25, 65, 65, 65, 65, 105, 105, 105]),
+])
+def test_time_series_upsampling(rrddata, twindow, shift, upsampled):
+    ts = prediction.TimeSeries(rrddata)
+    assert ts.bfill_upsample(twindow, shift) == upsampled
+
+
 @pytest.mark.parametrize("ref_value, stdev, sig, params, levels_factor, result", [
     (2, 0.5, 1, ("absolute", (3, 5)), 0.5, (3.5, 4.5)),
     (2, 0.5, -1, ("relative", (20, 50)), 0.5, (1.6, 1)),

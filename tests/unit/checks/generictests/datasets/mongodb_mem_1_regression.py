e2342924a057a50dc1bf4a71f5d304963909f44f
Moritz Kiemer <mo@mathias-kettner.de>
2019-04-04 10:51:33 +0200

    mongodb_mem: add test
    Change-Id: I6b5cceeaf48433e837deb725a15c1b0689a9dd49

---
+++ tests/unit/checks/generictests/datasets/mongodb_mem_1_regression.py
@@ -0,0 +1,31 @@
+# yapf: disable
+
+
+checkname = 'mongodb_mem'
+
+
+info = [['resident', '856'],
+        ['supported', 'True'],
+        ['virtual', '6100'],
+        ['mappedWithJournal', '5374'],
+        ['mapped', '2687'],
+        ['bits', '64'],
+        ['note', 'fields', 'vary', 'by', 'platform'],
+        ['page_faults', '86'],
+        ['heap_usage_bytes', '65501032']]
+
+
+discovery = {'': [(None, {})]}
+
+
+checks = {'': [(None,
+                {},
+                [(0,
+                  'Resident usage: 856.00 MB',
+                  [('process_resident_size', 897581056, None, None, None, None)]),
+                 (0,
+                  'Virtual usage: 5.96 GB',
+                  [('process_virtual_size', 6396313600, None, None, None, None)]),
+                 (0,
+                  'Mapped usage: 2.62 GB',
+                  [('process_mapped_size', 2817523712, None, None, None, None)])])]}

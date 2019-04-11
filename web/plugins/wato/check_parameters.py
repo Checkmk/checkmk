c8e2b052dac738b725f5d0ee50fc49bc857ba75f
Moritz Kiemer <mo@mathias-kettner.de>
2019-04-11 08:05:06 +0200

    7398 FIX heartbeat_crm: Inconsistent parameter format
    Due to an inconsistent parameter format, the default parameters could not be displayed during discovery.
    
    Change-Id: I2f029f23d750e4ccea49daa03862e94465f5b492

--- web/plugins/wato/check_parameters.py
+++ web/plugins/wato/check_parameters.py
@@ -15571,42 +15571,54 @@ register_check_parameters(
 )
 
 
+def _transform_heartbeat_crm(params):
+    if isinstance(params, dict):
+        return params
+    par_dict = {'max_age': params[0]}
+    if params[1]:
+        par_dict['dc'] = params[1]
+    if params[2] > -1:
+        par_dict['num_nodes'] = params[2]
+    if params[3] > -1:
+        par_dict['num_resources'] = params[3]
+    return par_dict
+
+
 register_check_parameters(
     subgroup_storage,
     "heartbeat_crm",
     _("Heartbeat CRM general status"),
-    Tuple(
+    Transform(Dictionary(
         elements = [
-            Integer(
-                title = _("Maximum age"),
-                help = _("Maximum accepted age of the reported data in seconds"),
-                unit = _("seconds"),
-                default_value = 60,
-            ),
-            Optional(
-                TextAscii(
-                    allow_empty = False
-                ),
-                title = _("Expected DC"),
-                help = _("The hostname of the expected distinguished controller of the cluster"),
-            ),
-            Optional(
-                Integer(
-                    min_value = 2,
-                    default_value = 2
-                ),
-                title = _("Number of Nodes"),
-                help = _("The expected number of nodes in the cluster"),
-            ),
-            Optional(
-                Integer(
-                    min_value = 0,
-                ),
-                title = _("Number of Resources"),
-                help = _("The expected number of resources in the cluster"),
-            ),
-        ]
-    ),
+            ("max_age",
+             Integer(
+                 title = _("Maximum age"),
+                 help = _("Maximum accepted age of the reported data in seconds"),
+                 unit = _("seconds"),
+                 default_value = 60,
+            )),
+            ("dc",
+             TextAscii(
+                 allow_empty = False,
+                 title = _("Expected DC"),
+                 help = _("The hostname of the expected distinguished controller of the cluster"),
+            )),
+            ("num_nodes",
+             Integer(
+                 min_value = 0,
+                 default_value = 2,
+                 title = _("Number of Nodes"),
+                 help = _("The expected number of nodes in the cluster"),
+            )),
+            ("num_resources",
+             Integer(
+                 min_value = 0,
+                 title = _("Number of Resources"),
+                 help = _("The expected number of resources in the cluster"),
+            )),
+        ],
+        optional_keys=["dc", "num_nodes", "num_resources"],
+    ), forth=_transform_heartbeat_crm),
     None,
     match_type = "first",
 )

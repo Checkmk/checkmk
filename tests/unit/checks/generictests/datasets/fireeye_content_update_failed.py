f1cb3d5301e022c8cf643462b3416e5c8a41ed90
Florian Kromer <fk@mathias-kettner.de>
2019-04-12 11:23:04 +0200

    7436 fireeye_content: Optional configuration of levels for the age of the last update
    Previously the age of the last content update was shown in an informative manner.
    This werk adds the option to configure the upper levels for the age of the last
    content update in WATO rule {Fireeye Security Content}.
    
    CMK-1938
    
    Change-Id: I0a7cf8f0bc2411a855d76b7e46a087a1e68a5316

---
+++ tests/unit/checks/generictests/datasets/fireeye_content_update_failed.py
@@ -0,0 +1,18 @@
+# yapf: disable
+
+
+checkname = 'fireeye_content'
+
+
+info = [['456.180', '0', '2016/02/26 15:42:06']]
+
+
+discovery = {'': [(None, {})]}
+
+
+checks = {'': [(None,
+                {},
+                [(1, 'Update: failed', []),
+                 (0, 'Last update: 2016/02/26 15:42:06', []),
+                 (0, 'Age: 3.1 y', []),
+                 (0, 'Security version: 456.180', [])])]}

b854142f550c144ba26eb956291db19061703e92
Ronny Bruska <rb@mathias-kettner.de>
2019-04-13 22:03:43 +0200

    7407 check_elasticsearch_query: New active check to query elasticsearch
    You can now query the occurrence of log messages in elasticsearch for a
    specified interval. It is possible to set warn/crit level on the message
    count.
    
    It is possible to use free-text search or set a defined index and/or fieldname
    to search.
    
    Change-Id: Ia098a3ff432c36572275513cb789911067aabe37

--- cmk/gui/plugins/wato/active_checks.py
+++ cmk/gui/plugins/wato/active_checks.py
@@ -64,6 +64,8 @@ from cmk.gui.plugins.wato import (
     PluginCommandLine,
     IndividualOrStoredPassword,
 )
+from cmk.gui.plugins.wato.utils import (
+    PasswordFromStore,)
 
 from cmk.gui.exceptions import MKUserError
 
@@ -2571,5 +2573,132 @@ class RulespecActiveChecksBySsh(HostRulespec):
                          TextAscii(
                              title=_("Keyfile"), help=_("Identity of an authorized key"), size=30)),
                     ])
+            ],
+        )
+
+
+@rulespec_registry.register
+class RulespecActiveChecksElasticsearch(HostRulespec):
+    @property
+    def group(self):
+        return RulespecGroupActiveChecks
+
+    @property
+    def name(self):
+        return "active_checks:elasticsearch_query"
+
+    @property
+    def match_type(self):
+        return "all"
+
+    @property
+    def valuespec(self):
+        return Dictionary(
+            required_keys=["svc_item", "pattern", "timerange"],
+            title=_("Query elasticsearch logs"),
+            help=_("You can search indices for defined patterns in defined fieldnames."),
+            elements=[
+                ("svc_item",
+                 TextUnicode(
+                     title=_("Item suffix"),
+                     help=_("Here you can define what service description (item) is "
+                            "used for the created service. The resulting item "
+                            "is always prefixed with 'Elasticsearch Query'."),
+                     allow_empty=False,
+                     size=16,
+                 )),
+                ("hostname",
+                 TextAscii(
+                     title=_("DNS hostname or IP address"),
+                     help=_(
+                         'You can specify a hostname or IP address different from the IP address '
+                         'of the host this check will be assigned to.'),
+                     allow_empty=False,
+                 )),
+                ("user", TextAscii(title=_("Username"), size=32, allow_empty=True)),
+                ("password", PasswordFromStore(
+                    title=_("Password of the user"),
+                    allow_empty=False,
+                )),
+                ("protocol",
+                 DropdownChoice(
+                     title=_("Protocol"),
+                     help=_("Here you cand define which protocol to use, default is https."),
+                     choices=[
+                         ("http", "HTTP"),
+                         ("https", "HTTPS"),
+                     ],
+                     default_value="https",
+                 )),
+                ("port",
+                 Integer(
+                     title=_("Port"),
+                     help=
+                     _("Use this option to query a port which is different from standard port 9200."
+                      ),
+                     default_value=9200,
+                     allow_empty=False,
+                 )),
+                ("pattern",
+                 TextUnicode(
+                     title=_("Search pattern"),
+                     help=
+                     _("Here you can define what search pattern should be used. "
+                       "You can use Kibana query language as described "
+                       "<a href=\"https://www.elastic.co/guide/en/kibana/current/kuery-query.html\""
+                       "target=\"_blank\">here</a>. To optimize search speed, use defined indices and fields "
+                       "otherwise all indices and fields will be searched."),
+                     allow_empty=False,
+                     size=32,
+                 )),
+                ("index",
+                 ListOfStrings(
+                     title=_("Indices to query"),
+                     help=_("Here you can define what index should be queried "
+                            "for the defined search. You can query one or "
+                            "multiple indices. Without this option all indices "
+                            "are queried. If you want to speed up your search, "
+                            "use definded indices."),
+                     orientation="horizontal",
+                     allow_empty=False,
+                     size=48,
+                 )),
+                ("fieldname",
+                 ListOfStrings(
+                     title=_("Fieldnames to query"),
+                     help=_(
+                         "Here you can define fieldnames that should be used "
+                         "in the search. Regexp query is allowed as described "
+                         "<a href=\"https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-regexp-query.html\""
+                         "target=\"_blank\">here</a>. If you want to speed up your search, "
+                         "use defined indices."),
+                     allow_empty=False,
+                     orientation="horizontal",
+                     size=32,
+                 )),
+                ("timerange",
+                 Age(
+                     title=_("Timerange"),
+                     help=
+                     _("Here you can define the timerange to query, eg. the last x minutes from now. "
+                       "The query will then check for the count of log messages in the defined range. "
+                       "Default is 1 minute."),
+                     display=["days", "hours", "minutes"],
+                     allow_empty=False,
+                     default_value=60,
+                 )),
+                ('count',
+                 Tuple(
+                     title=_("Thresholds on message count"),
+                     elements=[
+                         Integer(
+                             title=_("Warning at or above"),
+                             unit=_("log messages"),
+                             allow_empty=False),
+                         Integer(
+                             title=_("Critical at or above"),
+                             unit=_("log messages"),
+                             allow_empty=False)
+                     ])),
             ],
         )

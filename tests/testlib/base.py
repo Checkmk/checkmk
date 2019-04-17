82b057e46d90203f91fbd1e91838b097a96f9848
Lars Michelsen <lm@mathias-kettner.de>
2019-04-17 17:47:45 +0200

    Introduce testlib.base.Scenario() to simplify unit test setups
    Instead of the many individual monkeypatching calls we now have a
    standard wrapper class Scenario() that collects all test config
    settings.
    
    Once all needed configurations are made the apply() method is used to
    apply the config to cmk_base.config and update the config_cache object.
    
    Change-Id: I6550d23a52e8f58051e7e6f98066544aa0107262

---
+++ tests/testlib/base.py
@@ -0,0 +1,49 @@
+import cmk_base.config as config
+
+
+class Scenario(object):
+    """Helper class to modify the Check_MK base configuration for unit tests"""
+
+    def __init__(self, site_id="unit"):
+        super(Scenario, self).__init__()
+        self.config = {
+            "distributed_wato_site": site_id,
+            "all_hosts": [],
+            "host_paths": {},
+            "host_tags": {},
+            "clusters": {},
+        }
+        self.config_cache = config.get_config_cache()
+
+    def add_host(self, hostname, tags=None, folder="/"):
+        if tags is None:
+            tags = []
+
+        self.config["all_hosts"].append("%s|%s" % (hostname, "|".join(tags)))
+        self.config["host_paths"][hostname] = folder
+        #self.config["host_tags"][hostname] = tags
+        return self
+
+    def add_cluster(self, hostname, tags, folder="/", nodes=None):
+        if nodes is None:
+            nodes = []
+        name_with_tags = "%s|%s" % (hostname, "|".join(tags))
+        self.config["clusters"][name_with_tags] = nodes
+        self.config["host_paths"][hostname] = folder
+        #self.config["host_tags"][hostname] = tags
+        return self
+
+    def set_option(self, varname, option):
+        self.config[varname] = option
+        return self
+
+    def set_ruleset(self, varname, ruleset):
+        self.config[varname] = ruleset
+        return self
+
+    def apply(self, monkeypatch):
+        for key, value in self.config.items():
+            monkeypatch.setattr(config, key, value)
+
+        self.config_cache.initialize()
+        return self.config_cache

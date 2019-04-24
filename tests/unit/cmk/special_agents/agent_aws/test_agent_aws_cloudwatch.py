61259d1a81600c06e7b9b9795787f73c6a0adc81
Simon Betz <si@mathias-kettner.de>
2019-04-24 10:49:52 +0200

    Cleaned up AWS cloudwatch test
    Change-Id: Ic0679db27e06e1a94a6bd86e16516aced80cdebd

--- tests/unit/cmk/special_agents/agent_aws/test_agent_aws_cloudwatch.py
+++ tests/unit/cmk/special_agents/agent_aws/test_agent_aws_cloudwatch.py
@@ -1,6 +1,8 @@
-# encoding: utf-8
+# pylint: disable=redefined-outer-name
 
 import pytest
+from agent_aws_fake_clients import (
+    CloudwatchDescribeAlarmsInstanceCreator,)
 
 from cmk.special_agents.agent_aws import (
     AWSConfig,
@@ -9,186 +11,55 @@ from cmk.special_agents.agent_aws import (
     CloudwatchAlarms,
 )
 
-#TODO what about enums?
-
-#   .--fake client---------------------------------------------------------.
-#   |             __       _               _ _            _                |
-#   |            / _| __ _| | _____    ___| (_) ___ _ __ | |_              |
-#   |           | |_ / _` | |/ / _ \  / __| | |/ _ \ '_ \| __|             |
-#   |           |  _| (_| |   <  __/ | (__| | |  __/ | | | |_              |
-#   |           |_|  \__,_|_|\_\___|  \___|_|_|\___|_| |_|\__|             |
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
-
 
 class FakeCloudwatchClient(object):
     def describe_alarms(self, AlarmNames=None):
-        alarms = [
-            {
-                'AlarmName': 'string1',
-                'AlarmArn': 'string1',
-                'AlarmDescription': 'string1',
-                'AlarmConfigurationUpdatedTimestamp': "datetime(2015, 1, 1)",
-                'ActionsEnabled': "True|False",
-                'OKActions': ['string1',],
-                'AlarmActions': ['string1',],
-                'InsufficientDataActions': ['string1',],
-                'StateValue': "'OK' | 'ALARM' | 'INSUFFICIENT_DATA'",
-                'StateReason': 'string1',
-                'StateReasonData': 'string1',
-                'StateUpdatedTimestamp': "datetime(2015, 1, 1)",
-                'MetricName': 'string1',
-                'Namespace': 'string1',
-                'Statistic': "'SampleCount' | 'Average' | 'Sum' | 'Minimum' | 'Maximum'",
-                'ExtendedStatistic': 'string1',
-                'Dimensions': [{
-                    'Name': 'string1',
-                    'Value': 'string1'
-                },],
-                'Period': 123,
-                'Unit': "'Seconds' | 'Microseconds' | 'Milliseconds' | 'Bytes' | 'Kilobytes' |"
-                        "'Megabytes' | 'Gigabytes' | 'Terabytes' | 'Bits' | 'Kilobits' |"
-                        "'Megabits' | 'Gigabits' | 'Terabits' | 'Percent' | 'Count' |"
-                        "'Bytes/Second' | 'Kilobytes/Second' | 'Megabytes/Second' |"
-                        "'Gigabytes/Second' | 'Terabytes/Second' | 'Bits/Second' |"
-                        "'Kilobits/Second' | 'Megabits/Second' | 'Gigabits/Second' |"
-                        "'Terabits/Second' | 'Count/Second' | 'None'",
-                'EvaluationPeriods': 123,
-                'DatapointsToAlarm': 123,
-                'Threshold': 123.0,
-                'ComparisonOperator': "'GreaterThanOrEqualToThreshold' | 'GreaterThanThreshold' |"
-                                      "'LessThanThreshold' | 'LessThanOrEqualToThreshold'",
-                'TreatMissingData': 'string1',
-                'EvaluateLowSampleCountPercentile': 'string1',
-                'Metrics': [{
-                    'Id': 'string1',
-                    'MetricStat': {
-                        'Metric': {
-                            'Namespace': 'string1',
-                            'MetricName': 'string1',
-                            'Dimensions': [{
-                                'Name': 'string1',
-                                'Value': 'string1'
-                            },]
-                        },
-                        'Period': 123,
-                        'Stat': 'string1',
-                        'Unit': "'Seconds' | 'Microseconds' | 'Milliseconds' | 'Bytes' |"
-                                "'Kilobytes' | 'Megabytes' | 'Gigabytes' | 'Terabytes' | 'Bits' |"
-                                "'Kilobits' | 'Megabits' | 'Gigabits' | 'Terabits' | 'Percent' |"
-                                "'Count' | 'Bytes/Second' | 'Kilobytes/Second' |"
-                                "'Megabytes/Second' | 'Gigabytes/Second' | 'Terabytes/Second' |"
-                                "'Bits/Second' | 'Kilobits/Second' | 'Megabits/Second' |"
-                                "'Gigabits/Second' | 'Terabits/Second' | 'Count/Second' | 'None'"
-                    },
-                    'Expression': 'string1',
-                    'Label': 'string1',
-                    'ReturnData': "True|False"
-                },]
-            },
-            {
-                'AlarmName': 'string2',
-                'AlarmArn': 'string2',
-                'AlarmDescription': 'string2',
-                'AlarmConfigurationUpdatedTimestamp': "datetime(2015, 1, 1)",
-                'ActionsEnabled': "True|False",
-                'OKActions': ['string2',],
-                'AlarmActions': ['string2',],
-                'InsufficientDataActions': ['string2',],
-                'StateValue': "'OK' | 'ALARM' | 'INSUFFICIENT_DATA'",
-                'StateReason': 'string2',
-                'StateReasonData': 'string2',
-                'StateUpdatedTimestamp': "datetime(2015, 1, 1)",
-                'MetricName': 'string2',
-                'Namespace': 'string2',
-                'Statistic': "'SampleCount' | 'Average' | 'Sum' | 'Minimum' | 'Maximum'",
-                'ExtendedStatistic': 'string2',
-                'Dimensions': [{
-                    'Name': 'string2',
-                    'Value': 'string2'
-                },],
-                'Period': 123,
-                'Unit': "'Seconds' | 'Microseconds' | 'Milliseconds' | 'Bytes' | 'Kilobytes' |"
-                        "'Megabytes' | 'Gigabytes' | 'Terabytes' | 'Bits' | 'Kilobits' |"
-                        "'Megabits' | 'Gigabits' | 'Terabits' | 'Percent' | 'Count' |"
-                        "'Bytes/Second' | 'Kilobytes/Second' | 'Megabytes/Second' |"
-                        "'Gigabytes/Second' | 'Terabytes/Second' | 'Bits/Second' |"
-                        "'Kilobits/Second' | 'Megabits/Second' | 'Gigabits/Second' |"
-                        "'Terabits/Second' | 'Count/Second' | 'None'",
-                'EvaluationPeriods': 123,
-                'DatapointsToAlarm': 123,
-                'Threshold': 123.0,
-                'ComparisonOperator': "'GreaterThanOrEqualToThreshold' | 'GreaterThanThreshold' |"
-                                      "'LessThanThreshold' | 'LessThanOrEqualToThreshold'",
-                'TreatMissingData': 'string2',
-                'EvaluateLowSampleCountPercentile': 'string2',
-                'Metrics': [{
-                    'Id': 'string2',
-                    'MetricStat': {
-                        'Metric': {
-                            'Namespace': 'string2',
-                            'MetricName': 'string2',
-                            'Dimensions': [{
-                                'Name': 'string2',
-                                'Value': 'string2'
-                            },]
-                        },
-                        'Period': 123,
-                        'Stat': 'string2',
-                        'Unit': "'Seconds' | 'Microseconds' | 'Milliseconds' | 'Bytes' |"
-                                "'Kilobytes' | 'Megabytes' | 'Gigabytes' | 'Terabytes' | 'Bits' |"
-                                "'Kilobits' | 'Megabits' | 'Gigabits' | 'Terabits' | 'Percent' |"
-                                "'Count' | 'Bytes/Second' | 'Kilobytes/Second' |"
-                                "'Megabytes/Second' | 'Gigabytes/Second' | 'Terabytes/Second' |"
-                                "'Bits/Second' | 'Kilobits/Second' | 'Megabits/Second' |"
-                                "'Gigabits/Second' | 'Terabits/Second' | 'Count/Second' | 'None'"
-                    },
-                    'Expression': 'string2',
-                    'Label': 'string2',
-                    'ReturnData': "True|False"
-                },]
-            },
-        ]
+        alarms = CloudwatchDescribeAlarmsInstanceCreator.create_instances(amount=2)
         if AlarmNames:
             alarms = [alarm for alarm in alarms if alarm['AlarmName'] in AlarmNames]
         return {'MetricAlarms': alarms, 'NextToken': 'string'}
 
 
-#.
+@pytest.fixture()
+def get_cloudwatch_alarms_sections():
+    def _create_cloudwatch_alarms_sections(alarm_names):
+        region = 'region'
+        config = AWSConfig('hostname', (None, None))
+        config.add_single_service_config('cloudwatch_alarms', alarm_names)
+
+        fake_cloudwatch_client = FakeCloudwatchClient()
+
+        cloudwatch_alarms_limits_distributor = ResultDistributor()
+
+        cloudwatch_alarms_limits = CloudwatchAlarmsLimits(fake_cloudwatch_client, region, config,
+                                                          cloudwatch_alarms_limits_distributor)
+        cloudwatch_alarms = CloudwatchAlarms(fake_cloudwatch_client, region, config)
+
+        cloudwatch_alarms_limits_distributor.add(cloudwatch_alarms)
+        return cloudwatch_alarms_limits, cloudwatch_alarms
+
+    return _create_cloudwatch_alarms_sections
 
 
-@pytest.mark.parametrize("alarm_names,amount_alarms", [
+cloudwatch_params = [
     (None, 2),
     ([], 2),
-    (['string1'], 1),
+    (['AlarmName-0'], 1),
     (['not found'], 1),
-    (['string1', 'too many'], 1),
-    (['string1', 'string2'], 2),
-    (['string1', 'string2', 'too many'], 2),
-])
-def test_agent_aws_cloudwatch_result_distribution(alarm_names, amount_alarms):
-    region = 'region'
-    config = AWSConfig('hostname', (None, None))
-    config.add_single_service_config('cloudwatch_alarms', alarm_names)
+    (['AlarmName-0', 'too many'], 1),
+    (['AlarmName-0', 'AlarmName-1'], 2),
+    (['AlarmName-0', 'AlarmName-1', 'too many'], 2),
+]
 
-    fake_cloudwatch_client = FakeCloudwatchClient()
 
-    cloudwatch_alarms_limits_distributor = ResultDistributor()
-
-    cloudwatch_alarms_limits = CloudwatchAlarmsLimits(fake_cloudwatch_client, region, config,
-                                                      cloudwatch_alarms_limits_distributor)
-    cloudwatch_alarms = CloudwatchAlarms(fake_cloudwatch_client, region, config)
-
-    cloudwatch_alarms_limits_distributor.add(cloudwatch_alarms)
-
+@pytest.mark.parametrize("alarm_names,amount_alarms", cloudwatch_params)
+def test_agent_aws_cloudwatch_alarms_limits(get_cloudwatch_alarms_sections, alarm_names,
+                                            amount_alarms):
+    cloudwatch_alarms_limits, _cloudwatch_alarms = get_cloudwatch_alarms_sections(alarm_names)
     cloudwatch_alarms_limits_results = cloudwatch_alarms_limits.run().results
-    cloudwatch_alarms_results = cloudwatch_alarms.run().results
 
-    #--CloudwatchAlarmsLimits-----------------------------------------------
     assert cloudwatch_alarms_limits.interval == 300
     assert cloudwatch_alarms_limits.name == "cloudwatch_alarms_limits"
-    assert cloudwatch_alarms.interval == 300
-    assert cloudwatch_alarms.name == "cloudwatch_alarms"
 
     assert len(cloudwatch_alarms_limits_results) == 1
     cloudwatch_alarms_limits_result = cloudwatch_alarms_limits_results[0]
@@ -201,7 +72,16 @@ def test_agent_aws_cloudwatch_result_distribution(alarm_names, amount_alarms):
     assert cloudwatch_alarms_limits_content.limit == 5000
     assert cloudwatch_alarms_limits_content.amount == 2
 
-    #--CloudwatchAlarms-----------------------------------------------------
+
+@pytest.mark.parametrize("alarm_names,amount_alarms", cloudwatch_params)
+def test_agent_aws_cloudwatch_alarms(get_cloudwatch_alarms_sections, alarm_names, amount_alarms):
+    cloudwatch_alarms_limits, cloudwatch_alarms = get_cloudwatch_alarms_sections(alarm_names)
+    _cloudwatch_alarms_limits_results = cloudwatch_alarms_limits.run().results
+    cloudwatch_alarms_results = cloudwatch_alarms.run().results
+
+    assert cloudwatch_alarms.interval == 300
+    assert cloudwatch_alarms.name == "cloudwatch_alarms"
+
     assert len(cloudwatch_alarms_results) == 1
     cloudwatch_alarms_result = cloudwatch_alarms_results[0]
     assert cloudwatch_alarms_result.piggyback_hostname == ''

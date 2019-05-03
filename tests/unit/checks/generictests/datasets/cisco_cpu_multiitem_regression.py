edde9d8a4b39ae5b2aad67dbe407f47f25e1090d
Moritz Kiemer <mo@mathias-kettner.de>
2019-05-03 09:14:18 +0200

    cisco_cpu_multiitem: cleanup
    Change-Id: I6306a3f941b31381e7b0c237b231bd3aa1a9f4ac

--- tests/unit/checks/generictests/datasets/cisco_cpu_multiitem_regression.py
+++ tests/unit/checks/generictests/datasets/cisco_cpu_multiitem_regression.py
@@ -14,5 +14,5 @@ discovery = {'': [(u'0', {})]}
 checks = {'': [(u'0',
                 {'levels': (80.0, 90.0)},
                 [(0,
-                  '24.0% utilization in the last 5 minutes',
+                    'Utilization in the last 5 minutes: 24.0%',
                   [('util', 24.0, 80.0, 90.0, 0, 100)])])]}

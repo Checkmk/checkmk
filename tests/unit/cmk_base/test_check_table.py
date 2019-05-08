848b0fb30d62439c2dbced889546aff5bac8d667
Lars Michelsen <lm@mathias-kettner.de>
2019-05-08 16:29:11 +0200

    Check table: Add tests for cluster/node handling
    CMK-2030
    
    Change-Id: I46ce5ce6b24e53e469d4a83e7e6ff890b41a2ab5

--- tests/unit/cmk_base/test_check_table.py
+++ tests/unit/cmk_base/test_check_table.py
@@ -7,27 +7,19 @@ import cmk_base.check_table as check_table
 
 # TODO: This misses a lot of cases
 # - different get_check_table arguments
-# - handling of clusters / nodes
 @pytest.mark.parametrize(
-    "hostname,autochecks,expected_result",
+    "hostname,expected_result",
     [
-        ("empty-host", [], {}),
+        ("empty-host", {}),
         # Skip the autochecks automatically for ping hosts
-        ("ping-host", [("smart.temp", "bla", {})], {}),
-        ("no-autochecks", [], {
+        ("ping-host", {}),
+        ("no-autochecks", {
             ('smart.temp', '/dev/sda'): ({
                 'levels': (35, 40)
             }, u'Temperature SMART /dev/sda', []),
         }),
         # Static checks overwrite the autocheck definitions
-        ("autocheck-overwrite", [
-            ('smart.temp', '/dev/sda', {
-                "is_autocheck": True
-            }),
-            ('smart.temp', '/dev/sdb', {
-                "is_autocheck": True
-            }),
-        ], {
+        ("autocheck-overwrite", {
             ('smart.temp', '/dev/sda'): ({
                 'levels': (35, 40)
             }, u'Temperature SMART /dev/sda', []),
@@ -35,24 +27,52 @@ import cmk_base.check_table as check_table
                 'is_autocheck': True
             }, u'Temperature SMART /dev/sdb', []),
         }),
-        ("ignore-not-existing-checks", [
-            ("bla.blub", "ITEM", {}),
-        ], {}),
-        ("ignore-disabled-rules", [], {
+        ("ignore-not-existing-checks", {}),
+        ("ignore-disabled-rules", {
             ('smart.temp', 'ITEM2'): ({
                 'levels': (35, 40)
             }, u'Temperature SMART ITEM2', []),
         }),
-        ("static-check-overwrite", [], {
+        ("static-check-overwrite", {
             ('smart.temp', '/dev/sda'): ({
                 'levels': (35, 40),
                 'rule': 1
             }, u'Temperature SMART /dev/sda', [])
         }),
+        ("node1", {
+            ('smart.temp', 'auto-not-clustered'): ({}, u'Temperature SMART auto-not-clustered', []),
+            ('smart.temp', 'static-node1'): ({
+                'levels': (35, 40)
+            }, u'Temperature SMART static-node1', []),
+        }),
+        ("cluster1", {
+            ('smart.temp', 'static-cluster'): ({
+                'levels': (35, 40)
+            }, u'Temperature SMART static-cluster', []),
+            ('smart.temp', 'auto-clustered'): ({
+                'levels': (35, 40)
+            }, u'Temperature SMART auto-clustered', []),
+        }),
     ])
-def test_get_check_table(monkeypatch, hostname, autochecks, expected_result):
+def test_get_check_table(monkeypatch, hostname, expected_result):
+    autochecks = {
+        "ping-host": [("smart.temp", "bla", {})],
+        "autocheck-overwrite": [
+            ('smart.temp', '/dev/sda', {
+                "is_autocheck": True
+            }),
+            ('smart.temp', '/dev/sdb', {
+                "is_autocheck": True
+            }),
+        ],
+        "ignore-not-existing-checks": [("bla.blub", "ITEM", {}),],
+        "node1": [("smart.temp", "auto-clustered", {}), ("smart.temp", "auto-not-clustered", {})],
+    }
+
     ts = Scenario().add_host(hostname, tags=["test"])
     ts.add_host("ping-host", tags=["no-agent"])
+    ts.add_host("node1")
+    ts.add_cluster("cluster1", nodes=["node1"])
     ts.set_option(
         "static_checks",
         {
@@ -69,11 +89,16 @@ def test_get_check_table(monkeypatch, hostname, autochecks, expected_result):
                 (('smart.temp', '/dev/sda', {
                     "rule": 2
                 }), [], ["static-check-overwrite"]),
+                (('smart.temp', 'static-node1', {}), [], ["node1"]),
+                (('smart.temp', 'static-cluster', {}), [], ["cluster1"]),
             ]
         },
     )
+    ts.set_ruleset("clustered_services", [
+        ([], ['node1'], [u'Temperature SMART auto-clustered$']),
+    ])
     config_cache = ts.apply(monkeypatch)
-    monkeypatch.setattr(config_cache, "get_autochecks_of", lambda h: autochecks)
+    monkeypatch.setattr(config_cache, "get_autochecks_of", lambda h: autochecks.get(h, []))
 
     config.load_checks(check_api.get_check_api_context, ["checks/smart"])
     config.add_wato_static_checks_to_checks()

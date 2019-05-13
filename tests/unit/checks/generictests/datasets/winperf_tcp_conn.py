695a25f863e04917ee95f3f2a5e24889a15816ae
Florian Kromer <fk@mathias-kettner.de>
2019-05-13 16:24:39 +0200

    winperf_tcp_conn: Add gen data reg test
    Change-Id: I4aa7a3d3a50162fffc0080032bf4d9d43ca253cc

---
+++ tests/unit/checks/generictests/datasets/winperf_tcp_conn.py
@@ -0,0 +1,25 @@
+# yapf: disable
+
+
+checkname = 'winperf_tcp_conn'
+
+
+info = [['1557816444.39', '638', '2156250'],
+        ['2', '53267', 'counter'],
+        ['4', '3', 'rawcount'],
+        ['6', '23', 'rawcount'],
+        ['8', '1', 'rawcount'],
+        ['10', '1', 'rawcount'],
+        ['12', '12', 'rawcount'],
+        ['14', '34830', 'counter'],
+        ['16', '18437', 'counter']]
+
+
+discovery = {'': [(None, 'tcp_conn_stats_default_levels')]}
+
+
+checks = {'': [(None,
+                {},
+                [(0,
+                  'ESTABLISHED: 53267',
+                  [('ESTABLISHED', 53267, None, None, None, None)])])]}

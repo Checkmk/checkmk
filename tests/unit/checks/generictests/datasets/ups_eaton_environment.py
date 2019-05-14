e3dbeef64c9673f05a7ce906d5ad347d2dfb23b8
Moritz Kiemer <mo@mathias-kettner.de>
2019-05-14 21:08:13 +0200

    Test ups_eaton_environment: fix format
    Change-Id: I20171d06e3296cdc4796f7395bd009b36a3d3d7f

--- tests/unit/checks/generictests/datasets/ups_eaton_environment.py
+++ tests/unit/checks/generictests/datasets/ups_eaton_environment.py
@@ -1,19 +1,32 @@
+# -*- encoding: utf-8
 # yapf: disable
 
 
 checkname = 'ups_eaton_enviroment'
 
 
-info = [['1', '40', '3']]
+info = [
+    ['1', '40', '3'],
+]
 
 
-discovery = {'': [(None, {})]}
+discovery = {
+    '': [
+        (None, {}),
+    ],
+}
 
 
-checks = {'': [(None,
-                {'humidity': (65, 80), 'remote_temp': (40, 50), 'temp': (40, 50)},
-                [(1,
-                  u'Temperature: 1\xb0C (warn/crit at 40\xb0C/50\xb0C), Remote-Temperature: 40\xb0C (warn/crit at 40\xb0C/50\xb0C)(!), Humidity: 3% (warn/crit at 65%/80%)',
-                  [('temp', 1, 40, 50, None, None),
-                   ('remote_temp', 40, 40, 50, None, None),
-                   ('humidity', 3, 65, 80, None, None)])])]}
+checks = {
+    '': [
+        (None, {'humidity': (65, 80), 'remote_temp': (40, 50), 'temp': (40, 50)}, [
+            (1, u'Temperature: 1°C (warn/crit at 40°C/50°C),'
+                u' Remote-Temperature: 40°C (warn/crit at 40°C/50°C)(!),'
+                u' Humidity: 3% (warn/crit at 65%/80%)', [
+                    ('temp', 1, 40, 50, None, None),
+                    ('remote_temp', 40, 40, 50, None, None),
+                    ('humidity', 3, 65, 80, None, None),
+                ]),
+        ]),
+    ],
+}

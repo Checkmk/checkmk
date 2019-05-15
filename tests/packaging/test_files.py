103e52e79c1a08d0714ca736809be97a2deabe15
Sven Panne <sp@mathias-kettner.de>
2019-05-15 09:09:15 +0200

    Synched expected package sizes once again.
    Change-Id: I79cae6d685634082249402b5e241489520e010d9

--- tests/packaging/test_files.py
+++ tests/packaging/test_files.py
@@ -26,10 +26,10 @@ def _get_package_paths(version_path, what):
 # In case packages grow/shrink this check has to be changed.
 # TODO: Handle Docker tar.gz differently?
 @pytest.mark.parametrize("what,min_size,max_size", [
-    ("rpm", 167 * 1024 * 1024, 202 * 1024 * 1024),
-    ("deb", 132 * 1024 * 1024, 144 * 1024 * 1024),
-    ("cma", 242 * 1024 * 1024, 252 * 1024 * 1024),
-    ("tar.gz", 400 * 1024 * 1024, 443 * 1024 * 1024),
+    ("rpm", 168 * 1024 * 1024, 203 * 1024 * 1024),
+    ("deb", 133 * 1024 * 1024, 144 * 1024 * 1024),
+    ("cma", 243 * 1024 * 1024, 253 * 1024 * 1024),
+    ("tar.gz", 384 * 1024 * 1024, 394 * 1024 * 1024),
 ])
 def test_package_sizes(version_path, what, min_size, max_size):
     for pkg in _get_package_paths(version_path, what):

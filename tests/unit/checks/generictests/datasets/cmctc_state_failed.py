a98b3aea8343c8cf91795bdfe796a29242d1ce9a
Florian Kromer <fk@mathias-kettner.de>
2019-05-15 16:44:40 +0200

    cmctc_state: Add reg tests
    Change-Id: I7bf9e10a5de67eafa25d96ebe9e3e6adf5b549b7

---
+++ tests/unit/checks/generictests/datasets/cmctc_state_failed.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'cmctc_state'
+
+
+info = [['1', '3']]
+
+
+discovery = {'': [(None, {})]}
+
+
+checks = {'': [(None, {}, [(2, 'Status: failed, Units connected: 3', [])])]}
---
+++ tests/unit/checks/generictests/datasets/cmctc_state_ok.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'cmctc_state'
+
+
+info = [['2', '3']]
+
+
+discovery = {'': [(None, {})]}
+
+
+checks = {'': [(None, {}, [(0, 'Status: ok, Units connected: 3', [])])]}

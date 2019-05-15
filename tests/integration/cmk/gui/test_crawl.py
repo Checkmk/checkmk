710beaff10f1f6384bfa641bbadc08cc8da2fb98
Sven Panne <sp@mathias-kettner.de>
2019-05-15 16:16:56 +0200

    Added missing imports.
    Funnily enough, we don't run pylint on our tests... :-/
    
    Change-Id: I8f6da26d1af9d9e33db69585b67b15e0d44c0d3f

--- tests/integration/cmk/gui/test_crawl.py
+++ tests/integration/cmk/gui/test_crawl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import errno
 import os
 import sys
 import time
--- tests/unit/livestatus/test_livestatus_unit.py
+++ tests/unit/livestatus/test_livestatus_unit.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import errno
 import socket
 import ssl
 from contextlib import closing

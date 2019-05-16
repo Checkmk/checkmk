b8f476e57f1a0589cdc1470a786edd54ab89cbf9
Florian Kromer <fk@mathias-kettner.de>
2019-05-16 17:55:04 +0200

    7720 Fix oracle_logswitches: Handle db error maybe provided via the agent output
    An agent may potentially provide an output containing an error message resulting
    from the underlying SQL query. The check does not crash during discovery anymore.
    
    Change-Id: Ia2fcaf48b101fd36e6c6bda4692f9518752db50a

--- tests/unit/checks/generictests/datasets/oracle_logswitches.py
+++ tests/unit/checks/generictests/datasets/oracle_logswitches.py
@@ -1,22 +1,55 @@
+# -*- encoding: utf-8
 # yapf: disable
 
 
 checkname = 'oracle_logswitches'
 
 
-info = [['pengt', '15'], ['hirni', '22']]
+info = [['eln',
+         'ORACLE_BASE',
+         'environment',
+         'variable',
+         'is',
+         'not',
+         'being',
+         'set',
+         'since',
+         'this'],
+        ['eln',
+         'information',
+         'is',
+         'not',
+         'available',
+         'for',
+         'the',
+         'current',
+         'user',
+         'ID',
+         'nagios.'],
+        ['eln',
+         'You',
+         'can',
+         'set',
+         'ORACLE_BASE',
+         'manually',
+         'if',
+         'it',
+         'is',
+         'required.'],
+        ['eln', '15'],
+        ['hirni', '22']]
 
 
-discovery = {'': [('hirni', {}), ('pengt', {})]}
+discovery = {'': [('eln', {}), ('hirni', {})]}
 
 
-checks = {'': [('hirni',
-                {'levels': (50, 100), 'levels_lower': (20, 15)},
-                [(0,
-                  '22 log switches in the last 60 minutes (warn/crit below 15/20) (warn/crit at 50/100)',
-                  [('logswitches', 22, 50, 100, 0, None)])]),
-               ('pengt',
+checks = {'': [('eln',
                 {'levels': (50, 100), 'levels_lower': (-1, -1)},
                 [(0,
                   '15 log switches in the last 60 minutes (warn/crit below -1/-1) (warn/crit at 50/100)',
-                  [('logswitches', 15, 50, 100, 0, None)])])]}
+                  [('logswitches', 15, 50, 100, 0, None)])]),
+               ('hirni',
+                {'levels': (50, 100), 'levels_lower': (-1, -1)},
+                [(0,
+                  '22 log switches in the last 60 minutes (warn/crit below -1/-1) (warn/crit at 50/100)',
+                  [('logswitches', 22, 50, 100, 0, None)])])]}

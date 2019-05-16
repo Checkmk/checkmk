ce8aced49488316845530d7831b1b9cde5d32900
Florian Kromer <fk@mathias-kettner.de>
2019-05-16 16:27:47 +0200

    7498 FIX tsm_scratch: Prevent crash due to unusual agent output
    It is know that the agent output may contain warnings resulting from queries of
    the tape monitored storage device. This lead to crashes of the check during
    discovery. This werk fixes this issue.
    
    Change-Id: If72387de50c7366fab99d0f46ce38a3fbff40d13

--- tests/unit/checks/generictests/datasets/tsm_scratch.py
+++ tests/unit/checks/generictests/datasets/tsm_scratch.py
@@ -6,7 +6,15 @@ checkname = 'tsm_scratch'
 
 
 info = [[u'defaultinstance', u'296', u'DD4200.CB'],
-        [u'defaultinstance', u'128', u'DD4200.GOLD']]
+        [u'defaultinstance', u'128', u'DD4200.GOLD'],
+        [u'ANR2034E',
+         u'SELECT:',
+         u'No',
+         u'match',
+         u'found',
+         u'using',
+         u'this',
+         u'criteria.']]
 
 
 discovery = {'': [(u'defaultinstance / DD4200.CB', 'tsm_scratch_default_levels'),

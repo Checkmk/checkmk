28dd107d584fc0545dc3500fa4c1c4992ea54a48
Florian Kromer <fk@mathias-kettner.de>
2019-05-16 17:08:07 +0200

    juniper_trpz_aps_sessions: Add reg test for edge case
    Access points w.o. raadios do not lead to a crash of the parse function.
    
    Change-Id: I8277f71e5606f21e69f480214271bb0903c828b3

---
+++ tests/unit/checks/generictests/datasets/juniper_trpz_aps_sessions_no_radios.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'juniper_trpz_aps_sessions'
+
+
+info = [[[None,
+          'h19-mx',
+          u'10.48.55.55.51.54.48.55.49.52.55',
+          u'7',
+          u'C.O2.1_IMC-81']]]
+
+
+discovery = {'': []}

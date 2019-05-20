0fd3eb86eeccfdd981264dc0ba6f9235ded92d67
Simon Betz <si@mathias-kettner.de>
2019-05-20 16:42:02 +0200

    updated crashtest
    Change-Id: I6812db159cfec0fa25694ffc0f1f9f4609836c39

--- tests/unit/checks/generictests/crashtest.py
+++ tests/unit/checks/generictests/crashtest.py
@@ -104,7 +104,7 @@ class CrashDataset(WritableDataset):
         init_dict['info'] = local_vars.get('info')
         self.vars = local_vars
         self.crash_id = crash_report_fn[-20:-7]
-        filepath = '/tmp/testfile_%s_%s.py' % (checkname, self.crash_id)
+        filepath = '/tmp/%s_%s.py' % (checkname, self.crash_id)
         super(CrashDataset, self).__init__(filepath, init_dict)
 
     def _find_checkname_from_traceback(self, traceback):

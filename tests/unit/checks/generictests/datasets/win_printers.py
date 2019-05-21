e14efc2e553afb701dc0459a26c75f7485d682fc
Florian Kromer <fk@mathias-kettner.de>
2019-05-21 10:12:36 +0200

    win_printers: Add gen data reg test
    Change-Id: Iafafaf1a035bf5791e81993154138a5141e893aa

---
+++ tests/unit/checks/generictests/datasets/win_printers.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'win_printers'
+
+
+info = [['PrinterStockholm', '3', '4', '0'], ['WH1_BC_O3_UPS', '0', '3', '8']]
+
+
+discovery = {'': [('PrinterStockholm', {}), ('WH1_BC_O3_UPS', {})]}
+
+
+checks = {'': [('PrinterStockholm',
+                {'crit_states': [9, 10], 'warn_states': [8, 11]},
+                [(0, '3 jobs current, State: Printing, ', [])]),
+               ('WH1_BC_O3_UPS',
+                {'crit_states': [9, 10], 'warn_states': [8, 11]},
+                [(1, '0 jobs current, State: Idle, Error State: Jammed(!)', [])])]}

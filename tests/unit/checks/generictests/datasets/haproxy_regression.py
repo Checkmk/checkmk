9863459d697b52eeb0960e734b13783948afbd7e
Simon Betz <si@mathias-kettner.de>
2019-05-23 10:05:30 +0200

    7695 FIX haproxy: Skip invalid lines which led to a crash while discovery
    Change-Id: Ib39aec6d65b9bcca2d15169315e998ac50b3717f

---
+++ tests/unit/checks/generictests/datasets/haproxy_regression.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'haproxy'
+
+
+# too short line (len<33)
+info = [[0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0,
+         0]]
+
+
+discovery = {'frontend': [], 'server': []}

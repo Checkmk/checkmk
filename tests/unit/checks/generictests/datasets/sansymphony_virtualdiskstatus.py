1faf605f20dce8528e2863bb9163e458f1d9a60e
Moritz Kiemer <mo@mathias-kettner.de>
2019-05-28 11:56:11 +0200

    sansymphony_virtualdiskstatus: allow more that two values in output
    Change-Id: Ia55f04606e83e1e6877eb43eb7049b9d66b137fb

--- tests/unit/checks/generictests/datasets/sansymphony_virtualdiskstatus.py
+++ tests/unit/checks/generictests/datasets/sansymphony_virtualdiskstatus.py
@@ -5,11 +5,27 @@
 checkname = 'sansymphony_virtualdiskstatus'
 
 
-info = [['testvmfs01', 'Online'], ['vmfs01', 'anything else']]
+info = [
+    ['testvmfs01', 'Online'],
+    ['vmfs01', 'anything', 'else'],
+]
 
 
-discovery = {'': [('testvmfs01', None), ('vmfs01', None)]}
+discovery = {
+    '': [
+        ('testvmfs01', {}),
+        ('vmfs01', {}),
+    ],
+}
 
 
-checks = {'': [('testvmfs01', {}, [(0, 'Volume state is: Online', [])]),
-               ('vmfs01', {}, [(2, 'Volume state is: anything else', [])])]}
+checks = {
+    '': [
+        ('testvmfs01', {}, [
+            (0, 'Volume state is: Online', []),
+        ]),
+        ('vmfs01', {}, [
+            (2, 'Volume state is: anything else', []),
+        ]),
+    ],
+}

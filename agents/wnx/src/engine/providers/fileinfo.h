3547af6dac9167ffb595ba5c6b7b9f8e717b3fee
Sergey Kipnis <sergey.kipnis@tribe29.com>
2019-06-04 17:25:22 +0200

    [CMK-2162] - workaround for error in std::filesystem::last_file_write in MSVC 2017
    - wrapper and global define set in stdafx.h
    - unit testing later
    
    Change-Id: I1ce8e073611eccd1fbc7ecb9381391a2f8ad8983

--- agents/wnx/src/engine/providers/fileinfo.h
+++ agents/wnx/src/engine/providers/fileinfo.h
@@ -5,6 +5,10 @@
 #ifndef fileinfo_h__
 #define fileinfo_h__
 
+#if defined(USE_EXPERIMENTAL_FILESYSTEM)
+#include <experimental/filesystem>
+#endif
+
 #include <filesystem>
 #include <string>
 
@@ -29,8 +33,26 @@ private:
 #if defined(GTEST_INCLUDE_GTEST_GTEST_H_)
     friend class FileInfoTest;
     FRIEND_TEST(FileInfoTest, Base);
+    FRIEND_TEST(FileInfoTest, CheckOutput);
 #endif
 };
+
+// function is used to avoid error in MS VC 2017 with non-experimental
+// filesystem last_write_time generates absurdly big numbers
+// #TODO CHECK in 2019
+// returns chrono::something
+inline auto GetFileTimeSinceEpoch(const std::filesystem::path& file) noexcept {
+    std::error_code ec;
+#if defined(USE_EXPERIMENTAL_FILESYSTEM)
+    std::experimental::filesystem::v1::path fp = file.c_str();
+    auto file_last_touch_full =
+        std::experimental::filesystem::v1::last_write_time(fp, ec);
+    return file_last_touch_full.time_since_epoch();
+#else
+    auto file_last_touch_full = std::filesystem::last_write_time(file, ec);
+    return file_last_touch_full.time_since_epoch();
+#endif
+}
 
 }  // namespace provider
 

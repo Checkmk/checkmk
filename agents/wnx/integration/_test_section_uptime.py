99c5b8d00082a3d08f804d8fde79c94a01bcc115
Sergey Kipnis <sergey.kipnis@tribe29.com>
2019-06-05 13:56:38 +0200

    [CMK-2192] - precommit
    Change-Id: Ib06529706c7b6eb98f0ca4ddc53cfa5719ecf508

---
+++ agents/wnx/integration/_test_section_uptime.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset: 4 -*-
+import os
+import pytest
+import re
+from local import actual_output, make_ini_config, localtest, wait_agent, write_config
+
+
+class Globals(object):
+    section = 'uptime'
+    alone = True
+
+
+@pytest.fixture
+def testfile():
+    return os.path.basename(__file__)
+
+
+@pytest.fixture(params=['alone', 'with_systemtime'])
+def testconfig(request, make_ini_config):
+    Globals.alone = request.param == 'alone'
+    if Globals.alone:
+        make_ini_config.set('global', 'sections', Globals.section)
+    else:
+        make_ini_config.set('global', 'sections', '%s systemtime' % Globals.section)
+    make_ini_config.set('global', 'crash_debug', 'yes')
+    return make_ini_config
+
+
+@pytest.fixture
+def expected_output():
+    expected = [r'<<<%s>>>' % Globals.section, r'\d+']
+    if not Globals.alone:
+        expected += [re.escape(r'<<<systemtime>>>'), r'\d+']
+    return expected
+
+
+def test_section_uptime(request, testconfig, expected_output, actual_output, testfile):
+    # request.node.name gives test name
+    localtest(expected_output, actual_output, testfile, request.node.name)
---
+++ agents/wnx/integration/test_section_df.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset: 4 -*-
+import os
+import pytest
+import re
+from local import (actual_output, make_yaml_config, localtest, exec_dir, wait_agent, write_config)
+
+
+class Globals(object):
+    section = 'df'
+    alone = True
+
+
+@pytest.fixture
+def testfile():
+    return os.path.basename(__file__)
+
+
+@pytest.fixture(params=['alone', 'with_systemtime'])
+def testconfig(request, make_yaml_config):
+    Globals.alone = request.param == 'alone'
+    if Globals.alone:
+        make_yaml_config['global']['sections'] = Globals.section
+    else:
+        make_yaml_config['global']['sections'] = Globals.section
+    return make_yaml_config
+
+
+@pytest.fixture
+def expected_output():
+    drive = r'[A-Z]:%s' % re.escape(os.sep)
+    expected = [
+        re.escape(r'<<<%s:sep(9)>>>' % Globals.section),
+        r'%s\s+\w+\s+\d+\s+\d+\s+\d+\s+\d{1,3}%s\s+%s' % (drive, re.escape('%'), drive)
+    ]
+    if not Globals.alone:
+        expected += [re.escape(r'<<<systemtime>>>'), r'\d+']
+    return expected
+
+
+def test_section_df(request, testconfig, expected_output, actual_output, testfile):
+    # request.node.name gives test name
+    localtest(expected_output, actual_output, testfile, request.node.name)

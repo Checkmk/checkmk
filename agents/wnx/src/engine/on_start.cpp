23f6ebfea7f9826cf0d5bcaeffe412174633b05d
Sergey Kipnis <sergey.kipnis@tribe29.com>
2019-06-06 09:57:17 +0200

    [CMK-2192] - possibility to use another root/user folder
    - minor: CheckMk -> CheckMK
    - simple unit test
    
    Change-Id: I6c86fabc691612efa67a40d445817ca3936789c3

--- agents/wnx/src/engine/on_start.cpp
+++ agents/wnx/src/engine/on_start.cpp
@@ -5,6 +5,7 @@
 
 #include <atomic>
 #include <string>
+#include <string_view>
 
 #include "cfg.h"
 #include "common/cfg_info.h"
@@ -19,13 +20,48 @@ static std::atomic<bool> S_OnStartCalled = false;
 bool ConfigLoaded() { return S_ConfigLoaded; }
 namespace cfg {
 
+bool UseAlternateDirs(std::wstring_view base) {
+    namespace fs = std::filesystem;
+    fs::path root_dir = base;
+    fs::path data_dir = base;
+    data_dir /= L"ProgramData";
+
+    std::error_code ec;
+    if (std::filesystem::exists(data_dir, ec)) {
+        XLOG::d("Already {} exists, no action required", data_dir.u8string());
+    } else {
+        if (!fs::create_directories(data_dir, ec)) {
+            XLOG::l("Cannot create test folder {} error:{}",
+                    data_dir.u8string(), ec.value());
+            return false;
+        }
+    }
+    details::G_ConfigInfo.initAll(L"",       // no service
+                                  root_dir,  // default
+                                  data_dir   // this is data folder
+    );
+
+    return true;
+}
+
 bool DetermineWorkingFolders(AppType Type) {
     using namespace cma::cfg;
     namespace fs = std::filesystem;
     switch (Type) {
-        case AppType::exe:
-            details::G_ConfigInfo.initAll(L"", L"", L"");
-            break;
+        case AppType::exe:  // main exe
+        {
+            // we are using special env variable for testing
+            // THIS VARIABLE MUST BE TEMPORARY
+            auto cma_test_dir = cma::tools::win::GetEnv(kTemporaryRoot);
+            if (cma_test_dir.empty())
+                details::G_ConfigInfo.initAll(L"", L"", L"");
+            else {
+                if (!UseAlternateDirs(cma_test_dir)) return false;
+            }
+            XLOG::l.t("Using root = '{}' and user = '{}' folders ",
+                      details::G_ConfigInfo.getRootDir().u8string(),
+                      details::G_ConfigInfo.getUserDir().u8string());
+        } break;
 
         case AppType::srv:
             details::G_ConfigInfo.initAll(cma::srv::kServiceName, L"", L"");
@@ -36,27 +72,12 @@ bool DetermineWorkingFolders(AppType Type) {
             auto remote_machine_string =
                 cma::tools::win::GetEnv(L"REMOTE_MACHINE");
 
-            fs::path root_dir = remote_machine_string;
-            fs::path data_dir = remote_machine_string;
-            data_dir /= L"ProgramData";
+            if (!UseAlternateDirs(remote_machine_string)) return false;
+            XLOG::l.t("Using root = '{}' and user = '{}' folders ",
+                      details::G_ConfigInfo.getRootDir().u8string(),
+                      details::G_ConfigInfo.getUserDir().u8string());
 
-            std::error_code ec;
-            if (std::filesystem::exists(data_dir, ec)) {
-                XLOG::d("Already {} exists, no action required",
-                        data_dir.u8string());
-            } else {
-                if (!fs::create_directories(data_dir, ec)) {
-                    XLOG::l("Cannot create test folder {} error:{}",
-                            data_dir.u8string(), ec.value());
-                    return false;
-                }
-            }
-            details::G_ConfigInfo.initAll(L"",       // no service
-                                          root_dir,  // default
-                                          data_dir   // this is data folder
-            );
-            break;
-        }
+        } break;
         case AppType::automatic:
             XLOG::l.crit("Invalid value of the AppType");
             break;

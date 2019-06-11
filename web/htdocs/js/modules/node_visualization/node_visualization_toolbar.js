8bc47e97ee3375cdeedb42fced4f4503e259c35f
Andreas Boesl <andreas.boesl@tribe29.com>
2019-06-11 11:25:41 +0200

    BIVis Part3: Main instance with top level components, exluding viewport
    Change-Id: Ie916031378579c1e7f9d62d7113f941c5083fa3b

---
+++ web/htdocs/js/modules/node_visualization/node_visualization_toolbar.js
@@ -0,0 +1,71 @@
+//   .-Toolbar------------------------------------------------------------.
+//   |                 _____           _ _                                |
+//   |                |_   _|__   ___ | | |__   __ _ _ __                 |
+//   |                  | |/ _ \ / _ \| | '_ \ / _` | '__|                |
+//   |                  | | (_) | (_) | | |_) | (_| | |                   |
+//   |                  |_|\___/ \___/|_|_.__/ \__,_|_|                   |
+//   |                                                                    |
+//   +--------------------------------------------------------------------+
+
+import * as node_visualization_search from "node_visualization_search"
+import * as node_visualization_datasources from "node_visualization_datasources"
+
+export class Toolbar {
+    constructor(main_instance) {
+        this.main_instance = main_instance
+        this.selection = this.main_instance.get_div_selection().append("div")
+                                          .attr("id", "toolbar")
+                                          .classed("toolbar", true)
+
+        this._toolbar_plugins = {}
+        this.plugins_content_selection = this.selection.append("div")
+                                                    .attr("id", "content")
+        this.plugins_togglebutton_selection = this.selection.append("div")
+                                                    .attr("id", "togglebuttons")
+
+        this._setup_toolbar_plugins()
+        this._update_toolbar_plugins()
+    }
+
+    _setup_toolbar_plugins() {
+        this._register_toolbar_plugin(node_visualization_search.SearchAggregationsPlugin)
+    }
+
+    _register_toolbar_plugin(toolbar_plugin_class) {
+        if (toolbar_plugin_class.prototype.id() in this._toolbar_plugins)
+            return
+        this.add_toolbar_plugin_instance(new toolbar_plugin_class(this.main_instance))
+    }
+
+    add_toolbar_plugin_instance(toolbar_plugin_instance) {
+        this._toolbar_plugins[toolbar_plugin_instance.id()] = toolbar_plugin_instance
+        this._update_toolbar_plugins()
+    }
+
+    _update_toolbar_plugins() {
+        let plugin_ids = Object.keys(this._toolbar_plugins)
+        plugin_ids.sort((a, b) => {
+            return this._toolbar_plugins[a].sort_index < this._toolbar_plugins[b].sort_index
+        })
+
+        plugin_ids.forEach(plugin_id=>{
+            let plugin = this._toolbar_plugins[plugin_id]
+            let content_selection = this.plugins_content_selection.append("div").attr("id", plugin_id)
+            let togglebutton_selection = null
+            if (plugin.has_toggle_button()) {
+                togglebutton_selection = this.plugins_togglebutton_selection.append("div")
+                                                    .attr("id", plugin_id).classed("togglebutton on up", true)
+                                                        .classed("noselect", true)
+                                                        .classed("box", true)
+                                                        .classed("off", true)
+                                                        .classed("down", true)
+                                                    .on("click", ()=>plugin.toggle_active())
+            }
+
+            plugin.setup_selections(togglebutton_selection, content_selection)
+            plugin.render_togglebutton()
+            if (plugin.active)
+                plugin.update_active_state()
+        })
+    }
+}

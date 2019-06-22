01b35cf7cab187303c7e105a2b20f441f9328b20
Lars Michelsen <lm@tribe29.com>
2019-06-22 14:44:02 +0200

    Move label processing to dedicated module
    The labels will be needed in cmk_base and cmk.gui code once it they
    will be used for ruleset matching. Moved the common code (mainly the
    effective label calculation) to cmk.utils.labels to prepare for that.
    
    CMK-1942
    
    Change-Id: I5bdb813218100a880648387c0b0ae53d38bbe428

---
+++ tests/unit/cmk/utils/test_labels.py
@@ -0,0 +1,44 @@
+# encoding: utf-8
+# pylint: disable=redefined-outer-name
+
+import pytest  # type: ignore
+import cmk.utils.paths
+from cmk.utils.labels import (
+    DiscoveredHostLabelsStore,
+    DiscoveredServiceLabelsStore,
+)
+
+
+@pytest.fixture()
+def discovered_host_labels_dir(tmp_path, monkeypatch):
+    path = tmp_path / "var" / "check_mk" / "discovered_host_labels"
+    monkeypatch.setattr(cmk.utils.paths, "discovered_host_labels_dir", path)
+    return path
+
+
+def test_discovered_host_labels_store_file_path(discovered_host_labels_dir):
+    assert DiscoveredHostLabelsStore("host").file_path == discovered_host_labels_dir / "host.mk"
+
+
+def test_discovered_host_labels_store_load_default(discovered_host_labels_dir):
+    store = DiscoveredHostLabelsStore("host")
+    assert not store.file_path.exists()  # pylint: disable=no-member
+    assert store.load() == {}
+
+
+@pytest.fixture()
+def discovered_service_labels_dir(tmp_path, monkeypatch):
+    path = tmp_path / "var" / "check_mk" / "discovered_service_labels"
+    monkeypatch.setattr(cmk.utils.paths, "discovered_service_labels_dir", path)
+    return path
+
+
+def test_discovered_service_labels_store_file_path(discovered_service_labels_dir):
+    assert DiscoveredServiceLabelsStore(
+        "host", "SÄRVICE").file_path == discovered_service_labels_dir / "host" / "SÄRVICE.mk"
+
+
+def test_discovered_service_labels_store_load_default(discovered_service_labels_dir):
+    store = DiscoveredServiceLabelsStore("host", "SÄRVICE")
+    assert not store.file_path.exists()  # pylint: disable=no-member
+    assert store.load() == {}

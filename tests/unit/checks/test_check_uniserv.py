bc6c30f1951033e6989823bca9ad1611a9f54a3c
Simon Jess <simon.jess@tribe29.com>
2019-06-25 13:37:05 +0200

    Fixed test
    Change-Id: If131e100e68df0c96f57db577b37723a081821c3

--- tests/unit/checks/test_check_uniserv.py
+++ tests/unit/checks/test_check_uniserv.py
@@ -4,9 +4,23 @@ pytestmark = pytest.mark.checks
 
 
 @pytest.mark.parametrize("params,expected_args", [
-    ({}, "$HOSTADDRESS$"),
+    ({
+        "port": 123,
+        "service": "foobar",
+        "job": "version"
+    }, ["$HOSTADDRESS$", 123, "foobar", "VERSION"]),
+    ({
+        "port": 123,
+        "service": "foobar",
+        "job": ("address", {
+            "street": "street",
+            "street_no": 0,
+            "city": "city",
+            "search_regex": "regex"
+        })
+    }, ["$HOSTADDRESS$", 123, "foobar", "ADDRESS", "street", 0, "city", "regex"]),
 ])
-def test_check_mkevents_argument_parsing(check_manager, params, expected_args):
+def test_check_uniserv_argument_parsing(check_manager, params, expected_args):
     """Tests if all required arguments are present."""
-    active_check = check_manager.get_active_check("check_mkevents")
+    active_check = check_manager.get_active_check("check_uniserv")
     assert active_check.run_argument_function(params) == expected_args

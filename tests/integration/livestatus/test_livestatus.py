2429e4dd83a1d639243f1fc54300c2315584e363
Lars Michelsen <lm@tribe29.com>
2019-07-01 19:22:10 +0200

    Cleaned up test host creation
    Change-Id: Iaabc40dbf255d89ca4e9c887657292f2dd160c99

--- tests/integration/livestatus/test_livestatus.py
+++ tests/integration/livestatus/test_livestatus.py
@@ -2,31 +2,20 @@
 # encoding: utf-8
 
 import pytest
-from testlib import web
+from testlib import web, create_linux_test_host
+
 
 @pytest.fixture(scope="module")
-def default_cfg(web):
+def default_cfg(request, site, web):
     print "Applying default config"
-    web.add_host("livestatus-test-host", attributes={
-        "ipaddress": "127.0.0.1",
-    })
-
+    create_linux_test_host(request, web, site, "livestatus-test-host")
     web.discover_services("livestatus-test-host")
-
     web.activate_changes()
-    yield None
-
-    #
-    # Cleanup code
-    #
-    print "Cleaning up default config"
-
-    web.delete_host("livestatus-test-host")
 
 
 # Simply detects all tables by querying the columns table and then
 # queries each of those tables without any columns and filters
-@pytest.mark.parametrize(("core"), [ "nagios", "cmc" ])
+@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
 def test_tables(default_cfg, site, core):
     site.set_config("CORE", core, with_restart=True)
 
@@ -39,31 +28,31 @@ def test_tables(default_cfg, site, core):
 
     for table in existing_tables:
         if core == "nagios" and table == "statehist":
-            continue # the statehist table in nagios can not be fetched without time filter
+            continue  # the statehist table in nagios can not be fetched without time filter
 
         result = site.live.query("GET %s\n" % table)
         assert type(result) == list
 
 
-@pytest.mark.parametrize(("core"), [ "nagios", "cmc" ])
+@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
 def test_host_table(default_cfg, site, core):
     site.set_config("CORE", core, with_restart=True)
 
     rows = site.live.query("GET hosts")
     assert type(rows) == list
-    assert len(rows) >= 2 # header + min 1 host
+    assert len(rows) >= 2  # header + min 1 host
 
 
-@pytest.mark.parametrize(("core"), [ "nagios", "cmc" ])
+@pytest.mark.parametrize(("core"), ["nagios", "cmc"])
 def test_service_table(default_cfg, site, core):
     site.set_config("CORE", core, with_restart=True)
 
     rows = site.live.query("GET services\nFilter: host_name = livestatus-test-host\n"
                            "Columns: description\n")
     assert type(rows) == list
-    assert len(rows) >= 20 # header + min 1 service
+    assert len(rows) >= 20  # header + min 1 service
 
-    descriptions = [ r[0] for r in rows ]
+    descriptions = [r[0] for r in rows]
 
     assert "Check_MK" in descriptions
     assert "Check_MK Discovery" in descriptions

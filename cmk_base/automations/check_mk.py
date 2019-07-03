629b6e0971465fa8df9469dce80ef23ac905b8c3
Lars Michelsen <lm@tribe29.com>
2019-07-03 09:50:48 +0200

    7904 FIX Host diagnose: Only contact agent source in agent test
    In previous versions the agent test also tried to contact SNMP (when configured).
    
    Change-Id: I6ff0b19a9317feaa001529b56530ac3abdfad615

--- cmk_base/automations/check_mk.py
+++ cmk_base/automations/check_mk.py
@@ -1168,6 +1168,8 @@ class AutomationDiagHost(Automation):
                         source.set_port(agent_port)
                         if tcp_connect_timeout is not None:
                             source.set_timeout(tcp_connect_timeout)
+                    else:
+                        continue
 
                     output += source.run_raw()
                     if source.exception():

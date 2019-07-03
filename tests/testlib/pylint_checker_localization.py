88a2614a16b2d67aa5f706f0a8539d11e2578969
Simon Jess <simon.jess@tribe29.com>
2019-07-03 12:59:38 +0200

    test-futurize: fixed libfuturize.fixes.fix_filter
    Change-Id: Ia0244594ed0739c65e3416a337c991ba57942d68

--- tests/testlib/pylint_checker_localization.py
+++ tests/testlib/pylint_checker_localization.py
@@ -188,7 +188,7 @@ class EscapingProtectionChecker(TranslationBaseChecker):
                 return False
             # Case 2
             elif not all_unescapable and parent_is_HTML(node):
-                if filter(lambda x: x != "img", tags):
+                if [x for x in tags if x != "img"]:
                     message = "OK! Is protected by HTML(...)!\n"
                     message += "'''%s'''\n----> %s" % (first.value, ", ".join(tags))
                     self.add_message(self.MESSAGE_ID, args=message, node=node)

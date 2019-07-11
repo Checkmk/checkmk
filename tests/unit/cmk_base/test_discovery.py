4488d3af6e798b25af2982fe2d98588a67436454
Lars Michelsen <lm@tribe29.com>
2019-07-11 20:19:23 +0200

    Make DiscoveredService comparable to others
    This prepares the way to cleanup all the different comparison
    data structures during discovered service processing, e.g. used
    for preventing duplicate services.
    
    CMK-1816
    
    Change-Id: I9d2b589f6a674e54defc6acd6379d88dc9e79d59

---
+++ tests/unit/cmk_base/test_discovery.py
@@ -0,0 +1,42 @@
+# pylint: disable=redefined-outer-name
+
+import pytest  # type: ignore
+
+import cmk_base.discovery as discovery
+
+
+def test_discovered_service_init():
+    s = discovery.DiscoveredService("abc", u"Item", u"ABC Item", "None")
+    assert s.check_plugin_name == "abc"
+    assert s.item == u"Item"
+    assert s.description == u"ABC Item"
+    assert s.paramstr == "None"
+
+    with pytest.raises(AttributeError):
+        s.xyz = "abc"  # pylint: disable=assigning-non-slot
+
+
+def test_discovered_service_eq():
+    s1 = discovery.DiscoveredService("abc", u"Item", u"ABC Item", "None")
+    s2 = discovery.DiscoveredService("abc", u"Item", u"ABC Item", "None")
+    s3 = discovery.DiscoveredService("xyz", u"Item", u"ABC Item", "None")
+    s4 = discovery.DiscoveredService("abc", u"Xtem", u"ABC Item", "None")
+    s5 = discovery.DiscoveredService("abc", u"Item", u"ABC Item", "[]")
+
+    assert s1 == s1
+    assert s1 == s2
+    assert s1 != s3
+    assert s1 != s4
+    assert s1 == s5
+
+    assert s1 in [s1]
+    assert s1 in [s2]
+    assert s1 not in [s3]
+    assert s1 not in [s4]
+    assert s1 in [s5]
+
+    assert s1 in set([s1])
+    assert s1 in set([s2])
+    assert s1 not in set([s3])
+    assert s1 not in set([s4])
+    assert s1 in set([s5])

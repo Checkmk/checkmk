5816bc3b6f0f946e442c45fae3529adaa712faa8
Wontek Hong <wontek.hong@tribe29.com>
2019-07-18 16:44:06 +0200

    8813 netapp_api_psu: check functionality extension which allows to summarize individual services into one
    The check functionality is extended by allowing the user to group individual power supply monitoring services into one larger service. This service will display the total number of power supply units and additionally highlight the non OK units.
    
    Change-Id: I377f0445863c61768dc7ccdc7db08308b39acd17

---
+++ cmk/gui/plugins/wato/check_parameters/netapp_api_psu.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +------------------------------------------------------------------+
+# |             ____ _               _        __  __ _  __           |
+# |            / ___| |__   ___  ___| | __   |  \/  | |/ /           |
+# |           | |   | '_ \ / _ \/ __| |/ /   | |\/| | ' /            |
+# |           | |___| | | |  __/ (__|   <    | |  | | . \            |
+# |            \____|_| |_|\___|\___|_|\_\___|_|  |_|_|\_\           |
+# |                                                                  |
+# | Copyright Mathias Kettner 2014             mk@mathias-kettner.de |
+# +------------------------------------------------------------------+
+#
+# This file is part of Check_MK.
+# The official homepage is at http://mathias-kettner.de/check_mk.
+#
+# check_mk is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    CascadingDropdown,
+    Dictionary,
+)
+
+from cmk.gui.plugins.wato import (
+    rulespec_registry,
+    RulespecGroupCheckParametersDiscovery,
+    HostRulespec,
+)
+
+
+@rulespec_registry.register
+class RulespecInventoryIpmiRules(HostRulespec):
+    @property
+    def group(self):
+        return RulespecGroupCheckParametersDiscovery
+
+    @property
+    def name(self):
+        return "discovery_netapp_api_psu_rules"
+
+    @property
+    def match_type(self):
+        return "dict"
+
+    @property
+    def valuespec(self):
+        return Dictionary(
+            title=_("Discovery of Netapp power supply units"),
+            elements=[
+                ("mode",
+                 CascadingDropdown(
+                     title=_("Specify discovery mode"),
+                     help=
+                     _("Option which allows to specify whether all power supply units will be grouped into one service (summary) or each unit gets allocated one individual service (single)."
+                      ),
+                     orientation="vertical",
+                     choices=[
+                         ("summarize", _("Summary")),
+                         ("single", _("Single")),
+                     ])),
+            ],
+        )

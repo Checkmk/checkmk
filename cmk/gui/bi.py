38dfa4f1b922c12de2d0f3884edba202c5868072
Óscar Nájera <oscar.najera@tribe29.com>
2019-07-18 18:36:15 +0200

    7447 FIX Duplicated livestatus query limit exceeded
    Change-Id: Id6d569347bee833dfd50aa7d9be9cf130e2481b9

--- cmk/gui/bi.py
+++ cmk/gui/bi.py
@@ -3325,7 +3325,8 @@ def table(view, columns, query, only_sites, limit, all_active_filters):
 
             row["aggr_group"] = group
             rows.append(row)
-            if cmk.gui.view_utils.query_limit_exceeded_with_warn(rows, limit, config.user):
+            if cmk.gui.view_utils.row_limit_exceeded(len(rows), limit):
+                cmk.gui.view_utils.query_limit_exceeded_warn(limit, config.user)
                 del rows[limit:]
                 return rows
     return rows
@@ -3496,7 +3497,8 @@ def singlehost_table(view, columns, query, only_sites, limit, all_active_filters
             row.update(new_row)
             row["aggr_group"] = group
             rows.append(row)
-            if cmk.gui.view_utils.query_limit_exceeded_with_warn(rows, limit, config.user):
+            if cmk.gui.view_utils.row_limit_exceeded(len(rows), limit):
+                cmk.gui.view_utils.query_limit_exceeded_warn(limit, config.user)
                 del rows[limit:]
                 return rows
 

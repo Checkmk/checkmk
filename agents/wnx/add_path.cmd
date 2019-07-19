713510403f914893bdb4d44302ae1f1b8711683b
Sergey Kipnis <sergey.kipnis@tribe29.com>
2019-07-19 11:56:48 +0200

    [CMK-2379] - version is moved to separate file
    Change-Id: Id18c33ffda2dd216911bcb073269d75537ce2dcd

---
+++ agents/wnx/add_path.cmd
@@ -0,0 +1,9 @@
+@echo off
+if "%1%"=="" powershell Write-Host "you need a path" -Foreground Red && exit 1
+set np=%1
+echo %path%|find /i "%np%">nul  || @echo 'new path %path%;%np%'  && goto set_path
+powershell Write-Host "path exists" -Foreground Green
+exit 0
+:set_path
+CHOICE /C YN /M "Press Y for Yes, N for No." 
+if %errorlevel%==1 powershell Write-Host "path exists" -Foreground Green && setx path %path%;%np% && exit 0

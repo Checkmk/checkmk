c3477b34e98e2f2cd026240e7e9e2a6bb1266672
Simon Jess <simon.jess@tribe29.com>
2019-07-22 12:14:17 +0200

    7923 FIX websphere_mq_queues: Do not crash if timestamp of a queue could not be parsed
    Change-Id: I40238361756fd69adf06631387273f2eaeab43ce

--- tests/unit/checks/generictests/datasets/websphere_mq_queues.py
+++ tests/unit/checks/generictests/datasets/websphere_mq_queues.py
@@ -20,11 +20,17 @@ info = [['0', 'BRK.REPLY.CONVERTQ', '2016_04_08-16_31_43'],
          'DEAD.QUEUE.SECURITY',
          '100000',
          'CURDEPTH(0)LGETDATE(2016-04-08)LGETTIME(15.31.43)',
-         '2016_04_08-16_31_43']]
+         '2016_04_08-16_31_43'],
+        ['2000',
+         'DEAD.QUEUE.SECURITY2',
+         '200000',
+         'CURDEPTH(0)LGETDATE(2016-04-08)LGETTIME(15.31.43)',
+         'foobar']]
 
 
 discovery = {'': [('BRK.REPLY.CONVERTQ', 'websphere_mq_queues_default_levels'),
-                  ('DEAD.QUEUE.SECURITY', 'websphere_mq_queues_default_levels')]}
+                  ('DEAD.QUEUE.SECURITY', 'websphere_mq_queues_default_levels'),
+                  ('DEAD.QUEUE.SECURITY2', 'websphere_mq_queues_default_levels')]}
 
 
 checks = {'': [('BRK.REPLY.CONVERTQ',
@@ -40,4 +46,10 @@ checks = {'': [('BRK.REPLY.CONVERTQ',
                   '1000/100000 messages in queue (warn/crit at 1000/1200)',
                   [('queue', 1000, 1000, 1200, None, 100000)]),
                  (0, '1.0%', []),
-                 (0, 'Messages not processed since 60 m', [])])]}
+                 (0, 'Messages not processed since 60 m', [])]),
+               ('DEAD.QUEUE.SECURITY2',
+                {'message_count': (1000, 1200), 'message_count_perc': (80.0, 90.0)},
+                [(2,
+                  '2000/200000 messages in queue (warn/crit at 1000/1200)',
+                  [('queue', 2000, 1000, 1200, None, 200000)]),
+                 (0, '1.0%', [])])]}

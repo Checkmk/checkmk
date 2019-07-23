53f483da2604f0fd20eecb88710b69157add571e
Lars Michelsen <lm@tribe29.com>
2019-07-23 20:49:27 +0200

    Remove odd logwatch pattern test: Logfile matching, but no host name given
    Change-Id: I9aeaa558ea2f95aefcaa6971780fefff632bb898

--- cmk/gui/watolib/rulesets.py
+++ cmk/gui/watolib/rulesets.py
@@ -884,11 +884,6 @@ class Rule(object):
         for reason in self._get_mismatch_reasons_of_match_object(match_object):
             yield reason
 
-    def matches_item(self, svc_desc_or_item, svc_desc):
-        match_object = cmk_base.export.ruleset_match_object_for_checkgroup_parameters(
-            None, svc_desc_or_item, svc_desc)
-        return any(self._get_mismatch_reasons_of_match_object(match_object))
-
     def _get_mismatch_reasons_of_match_object(self, match_object):
         matcher = cmk_base.export.get_ruleset_matcher()
 

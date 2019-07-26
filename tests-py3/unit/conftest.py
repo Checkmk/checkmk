02f9c84e23cd09ebc3ab9980206910f18f4157db
Lars Michelsen <lm@tribe29.com>
2019-07-26 21:27:16 +0200

    Prepare for Python3 unit tests
    * Add Makefile target
    * Add hierarchy for test files and first basic unit tests
    * Prepare gerrit job for python3 unit tests and add script to gather
      the list of all python3 files
    * Add jenkins script for the dedicated python3 unit test job
    
    Change-Id: I1eba250520c7bf3a5fad1631f13fd6755bbbe976

---
+++ tests-py3/unit/conftest.py
@@ -0,0 +1,26 @@
+# pylint: disable=redefined-outer-name
+
+import socket
+import pytest  # type: ignore
+import cmk
+
+
+@pytest.fixture(autouse=True)
+def patch_omd_site(monkeypatch):
+    monkeypatch.setattr(cmk, "omd_site", lambda: "NO_SITE")
+
+
+# TODO: This fixes our unit tests when executing the tests while the local
+# resolver uses a search domain which uses wildcard resolution. e.g. in a
+# network where mathias-kettner.de is in the domain search list and
+# [anything].mathias-kettner.de resolves to an IP address.
+# Clean this up once we don't have this situation anymore e.g. via VPN.
+@pytest.fixture()
+def fixup_ip_lookup(monkeypatch):
+    # Fix IP lookup when
+    def _getaddrinfo(host, port, family=None, socktype=None, proto=None, flags=None):
+        if family == socket.AF_INET:
+            return "0.0.0.0"
+        raise NotImplementedError()
+
+    monkeypatch.setattr(socket, "getaddrinfo", _getaddrinfo)

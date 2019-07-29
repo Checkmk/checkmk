c543f97263cd6ae947695b6f4f06a8035eea1ca7
Lars Michelsen <lm@tribe29.com>
2019-07-29 16:50:49 +0200

    Fix python/python3 detection of pylint tests
    Change-Id: I6c0eefac7e1034197bb80d836246f0fd980db705

--- tests/testlib/pylint_cmk.py
+++ tests/testlib/pylint_cmk.py
@@ -112,7 +112,7 @@ def is_python_file(path):
 
     # Only add python files
     shebang = file(path, "r").readline()
-    if shebang.startswith("#!") and "python" in shebang:
+    if shebang.startswith("#!") and shebang.endswith("python"):
         return True
 
     return False

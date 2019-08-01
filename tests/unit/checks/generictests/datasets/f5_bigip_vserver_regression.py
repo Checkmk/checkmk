6f28e891a8e9f135bda8a4b4df282abe87bf0cb5
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-08-01 10:00:30 +0200

    f5_bigip_vserver: fix display of ip address
    Change-Id: Ib4fbfea06dbf37b4ea72843c38d06d543c935f55

--- tests/unit/checks/generictests/datasets/f5_bigip_vserver_regression.py
+++ tests/unit/checks/generictests/datasets/f5_bigip_vserver_regression.py
@@ -14,6 +14,8 @@ info = [
     [u'/Common/sight-seeing.wurmhole.univ_HTTP2HTTPS', u'4', u'1', (u"The children pool member(s) either"
      u" don't have service checking enabled, or service check results are not available yet"),
      u'\xd4;xK', u'', u'', u'', u'', u'', u'', u'', u'', u'0', u''],
+    [u'/Common/starfleet.space', u'4', u'', u"To infinity and beyond!", u'\xde\xca\xff\xed', u'', u'',
+     u'', u'', u'', u'', u'', u'', u'0', u''],
 ]
 
 
@@ -22,6 +24,7 @@ discovery = {
         (u'/Common/sight-seeing.wurmhole.univ', {}),
         (u'/Common/sight-seeing.wurmhole.univ_HTTP2HTTPS', {}),
         (u'/Common/www.wurmhole.univ_HTTP2HTTPS', {}),
+        (u'/Common/starfleet.space', {}),
     ],
 }
 
@@ -29,13 +32,21 @@ discovery = {
 checks = {
     '': [
         (u'/Common/sight-seeing.wurmhole.univ_HTTP2HTTPS', {}, [
-            (1, u'Virtual Server with IP 2 is enabled, State availability is unknown, Detail: T', []),
+            (0, u'Virtual Server with IP 212.59.120.75 is enabled', []),
+            (1, u'State availability is unknown, Detail: T', []),
             (0, 'Client connections: 0', [('connections', 0, None, None, None, None)]),
             (0, 'Rate: 0.00/sec', []),
         ]),
         (u'/Common/www.wurmhole.univ', {}, []),
         (u'/Common/www.wurmhole.univ_HTTP2HTTPS', {}, [
-            (1, u'Virtual Server with IP 2 is enabled, State availability is unknown, Detail: T', []),
+            (0, u'Virtual Server with IP 212.59.120.73 is enabled', []),
+            (1, u'State availability is unknown, Detail: T', []),
+            (0, 'Client connections: 0', [('connections', 0, None, None, None, None)]),
+            (0, 'Rate: 0.00/sec', []),
+        ]),
+        (u'/Common/starfleet.space', {}, [
+            (1, u'Virtual Server with IP 222.202.255.237 is in unknown state', []),
+            (1, u'State availability is unknown, Detail: T', []),
             (0, 'Client connections: 0', [('connections', 0, None, None, None, None)]),
             (0, 'Rate: 0.00/sec', []),
         ]),

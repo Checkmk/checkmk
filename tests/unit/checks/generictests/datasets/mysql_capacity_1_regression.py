58cc43e61df2c2a45ce3ad4525eeb0f1889fbe00
Mathias Laurin <mathias.laurin@tribe29.com>
2019-08-05 14:17:05 +0200

    8900 FIX mysql_capacity: Fix problem with warn/crit size
    The crit and warn levels to mysql capacity check values were wrongly
    multiplied by 1024.  For example, kB was interpreted to MB.  This
    has been fixed.
    
    FEED-3895
    
    Change-Id: I1e5e6a21b43a1fbd3593bceb7f43d1b1b876e103

--- tests/unit/checks/generictests/datasets/mysql_capacity_1_regression.py
+++ tests/unit/checks/generictests/datasets/mysql_capacity_1_regression.py
@@ -20,6 +20,32 @@ discovery = {
 
 checks = {
     '': [
-        ('mysql:test', {}, [(0, 'Size: 390.30 MB', [('database_size', 409255936, None, None, None, None)])]),
+        (
+            'mysql:test',
+            {},
+            [(0, 'Size: 390.30 MB', [('database_size', 409255936, None, None, None, None)])]
+        ),
+        (
+            'mysql:test',
+            (40960, 51200),
+            [
+                (
+                    2,
+                    'Size: 390.30 MB (warn/crit at 40.00 kB/50.00 kB)',
+                    [('database_size', 409255936, 40960, 51200, None, None)]
+                )
+            ]
+        ),
+        (
+            'mysql:test',
+            (40960000000, 51200000000),
+            [
+                (
+                    0,
+                    'Size: 390.30 MB',
+                    [('database_size', 409255936, 40960000000, 51200000000, None, None)]
+                )
+            ]
+        ),
     ],
 }

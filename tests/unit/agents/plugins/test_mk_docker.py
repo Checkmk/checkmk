33323dff21f08ec73c8e018147d8ecff2260a998
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-08-08 08:12:20 +0200

    7964 mk_docker.py: No longer pipe checkmk agent into containers
    The plugin mk_docker.py tries to execute the linux agent inside the container.
    If the contianer had no agent installed, plugin used to pipe the linux agent
    into the docker container. The latter is no longer done.
    
    The agent is still executed if users have installed in on the container.
    
    CMK-2506
    
    Change-Id: I06a006e901b340bf4504c8b1154b11a4adf830d4

--- tests/unit/agents/plugins/test_mk_docker.py
+++ tests/unit/agents/plugins/test_mk_docker.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.join(cmk_path(), 'agents', 'plugins'))
 import mk_docker  # pylint: disable=import-error,wrong-import-position
 
 PLUGIN_CHECKSUMS = {
-    '0.1': 'c162e333908cc6f13de56b837aa95c28',
+    '0.1': '537dc5f2b41a454ebbf0ef8accd9f6b2',
 }
 
 

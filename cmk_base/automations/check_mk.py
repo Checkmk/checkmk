467b0479c2924e86efe849847d9d1d759e7603d5
Lars Michelsen <lm@tribe29.com>
2019-08-13 18:15:53 +0200

    8851 FIX Fix broken "Parameters of X" for clustered services
    The "Parameters of X" page always showed an exception like
    "'list' object has no attribute 'check_plugin_name'".
    
    Add type hints that would have prevented this issue.
    
    FEED-4097
    
    Change-Id: Ic21c97c4f563c0c1c84c2b5370b0b55e01b28daf

--- cmk_base/automations/check_mk.py
+++ cmk_base/automations/check_mk.py
@@ -573,6 +573,7 @@ class AutomationAnalyseServices(Automation):
     # TODO: Was ist mit Clustern???
     # TODO: Klappt das mit automatischen verschatten von SNMP-Checks (bei dual Monitoring)
     def _get_service_info(self, config_cache, host_config, servicedesc):
+        # type: (config.ConfigCache, config.HostConfig, Text) -> Dict
         hostname = host_config.hostname
         check_api_utils.set_hostname(hostname)
 
@@ -601,12 +602,12 @@ class AutomationAnalyseServices(Automation):
         # TODO: There is a lot of duplicated logic with discovery.py/check_table.py. Clean this
         # whole function up.
         if host_config.is_cluster:
-            services = []
-            for node in host_config.nodes:
+            services = []  # type: List[cmk_base.check_utils.Service]
+            for node in host_config.nodes or []:
                 for service in config_cache.get_autochecks_of(node):
                     if hostname == config_cache.host_of_clustered_service(
                             node, service.description):
-                        services.append(services)
+                        services += services
         else:
             services = config_cache.get_autochecks_of(hostname)
 

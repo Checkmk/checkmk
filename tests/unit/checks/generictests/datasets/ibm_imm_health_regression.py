51d08a6f756cede99a8512b04be961085a4951e4
Lukas Lengler <lukas.lengler@tribe29.com>
2019-08-20 17:25:40 +0200

    Added tests for checks 6
    Change-Id: I669639a6f08ba9fefed3e499bccf7d629fa96d20

---
+++ tests/unit/checks/generictests/datasets/ibm_imm_health_regression.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'ibm_imm_health'
+
+
+info = [[u'223523'],
+        [u'2342'],
+        [u'234'],
+        [u'23352']]
+
+
+discovery = {'': [(None, None)]}
+
+
+checks = {'': [(None, {}, [(3, u'23352(234)', [])])]}
---
+++ tests/unit/checks/generictests/datasets/ibm_rsa_health_regression.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'ibm_rsa_health'
+
+
+info = [[u'0'], [u'1'], [u'Critical'], [u'SSL Server Certificate Error']]
+
+
+discovery = {'': [(None, None)]}
+
+
+checks = {'': [(None, {}, [(2, u'SSL Server Certificate Error(Critical)', [])])]}

d0e80b4f362656aacd8777b35e2d6aa80960cdb8
Lars Michelsen <lm@tribe29.com>
2019-08-22 13:28:07 +0200

    Use same module loading logic for all agent plugin unit tests
    * No ad-hoc sys.path modification needed anymore
    * python compiled files are handled in the same way
    * Standardization :-)
    
    Change-Id: Ia2e95309d3c7a297d3483c8de6114b8a8cfb2b00

--- tests/unit/agents/plugins/test_mk_filestats.py
+++ tests/unit/agents/plugins/test_mk_filestats.py
@@ -1,18 +1,17 @@
 # -*- encoding: utf-8
+# pylint: disable=protected-access,redefined-outer-name
 import os
 import ast
-import sys
-import pytest
-from testlib import cmk_path  # pylint: disable=import-error
-
-sys.path.insert(0, os.path.join(cmk_path(), 'agents', 'plugins'))
-
-import mk_filestats  # pylint: disable=import-error,wrong-import-position
-
-# pylint: disable=protected-access
+import pytest  # type: ignore
+from testlib import import_module
 
 
-def test_lazy_file():
+@pytest.fixture(scope="module")
+def mk_filestats():
+    return import_module("agents/plugins/mk_filestats.py")
+
+
+def test_lazy_file(mk_filestats):
     lfile = mk_filestats.LazyFileStats("no such file")
     assert lfile.path == os.path.abspath("no such file")
     assert lfile._size is None
@@ -39,7 +38,7 @@ def test_lazy_file():
     "input_one": None,
     "input_two": None
 })])
-def test_get_file_iterator_invalid(config):
+def test_get_file_iterator_invalid(mk_filestats, config):
     with pytest.raises(ValueError):
         mk_filestats.get_file_iterator(config)
 
@@ -52,7 +51,7 @@ def test_get_file_iterator_invalid(config):
         "input_patterns": '"foo bar" gee*'
     }, ["foo bar", "gee*"]),
 ])
-def test_get_file_iterator_pattern(config, pat_list):
+def test_get_file_iterator_pattern(mk_filestats, config, pat_list):
     iter_obj = mk_filestats.get_file_iterator(config)
     assert isinstance(iter_obj, mk_filestats.PatternIterator)
     assert iter_obj._patterns == pat_list
@@ -65,14 +64,14 @@ def test_get_file_iterator_pattern(config, pat_list):
     ('<=', (2000., 1024, "1000"), (False, True, True)),
     ('==', (2000., 1024, "1000"), (False, True, False)),
 ])
-def test_numeric_filter(operator, values, results):
+def test_numeric_filter(mk_filestats, operator, values, results):
     num_filter = mk_filestats.AbstractNumericFilter('%s1024' % operator)
     for value, result in zip(values, results):
         assert result == num_filter._matches_value(value)
 
 
 @pytest.mark.parametrize("invalid_arg", ['<>1024', '<NaN'])
-def test_numeric_filter_raises(invalid_arg):
+def test_numeric_filter_raises(mk_filestats, invalid_arg):
     with pytest.raises(ValueError):
         mk_filestats.AbstractNumericFilter(invalid_arg)
 
@@ -84,7 +83,7 @@ def test_numeric_filter_raises(invalid_arg):
                              (True, False, False),
                          ), (r'/.*', ("relative/path/should/be/resolved",), (True,)),
                           (u'[^ð]*ð{2}[^ð]*', (u'foðbar', u'fððbar'), (False, True))])
-def test_path_filter(reg_pat, paths, results):
+def test_path_filter(mk_filestats, reg_pat, paths, results):
     path_filter = mk_filestats.RegexFilter(reg_pat)
     for path, result in zip(paths, results):
         lazy_file = mk_filestats.LazyFileStats(path)
@@ -99,12 +98,12 @@ def test_path_filter(reg_pat, paths, results):
         "filter_size": "!=käse"
     },
 ])
-def test_get_file_filters_invalid(config):
+def test_get_file_filters_invalid(mk_filestats, config):
     with pytest.raises(ValueError):
         mk_filestats.get_file_filters(config)
 
 
-def test_get_file_filters():
+def test_get_file_filters(mk_filestats):
     config = {"filter_size": ">1", "filter_age": "==0", "filter_regex": "foo"}
     filters = mk_filestats.get_file_filters(config)
     assert len(filters) == 3
@@ -114,12 +113,12 @@ def test_get_file_filters():
 
 
 @pytest.mark.parametrize("config", [{}, {"output": "/dev/null"}])
-def test_get_ouput_aggregator_invalid(config):
+def test_get_ouput_aggregator_invalid(mk_filestats, config):
     with pytest.raises(ValueError):
         mk_filestats.get_output_aggregator(config)
 
 
 @pytest.mark.parametrize("output_value", ["count_only", "file_stats"])
-def test_get_ouput_aggregator(output_value):
+def test_get_ouput_aggregator(mk_filestats, output_value):
     aggr = mk_filestats.get_output_aggregator({"output": output_value})
     assert aggr is getattr(mk_filestats, "output_aggregator_%s" % output_value)

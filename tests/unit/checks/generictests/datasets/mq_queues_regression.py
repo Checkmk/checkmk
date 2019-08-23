a6450fa7f55c3fbd60e520d6f2645aa47d97b273
Simon Jess <simon.jess@tribe29.com>
2019-08-23 09:44:36 +0200

    8947 FIX mq_queues: Fixed crash because default parameters were not discovered correctly
    Change-Id: I9de863fd865b80f255e32e0de1902135602061bd

--- tests/unit/checks/generictests/datasets/mq_queues_regression.py
+++ tests/unit/checks/generictests/datasets/mq_queues_regression.py
@@ -8,8 +8,7 @@ checkname = 'mq_queues'
 info = [[u'[[Queue_App1_App2]]'], [u'1', u'2', u'3', u'4']]
 
 
-discovery = {'': [(u'Queue_App1_App2',
-                   {'consumerCount': (None, None), 'size': (None, None)})]}
+discovery = {'': [(u'Queue_App1_App2', {})]}
 
 
 checks = {'': [(u'Queue_App1_App2',

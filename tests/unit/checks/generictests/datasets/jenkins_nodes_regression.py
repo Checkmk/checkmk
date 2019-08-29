95bd56a480a70488fa3d758fd0680461f8eac4ea
Ronny Bruska <ronny.bruska@tribe29.com>
2019-08-29 13:51:08 +0200

    7757 jenkins_nodes: New check to monitor Jenkins nodes
    This check monitors the state of jenkins nodes. The service output gives
    information about the node description, the number of executers, if the node is
    an JNLPAgent, the idle and offline state and the average node response time. It
    also outputs the free temp space.
    
    CMK-2281
    
    Change-Id: I7fb4ca5dff687d062f6dc644bd7b99c04fe733fa

---
+++ tests/unit/checks/generictests/datasets/jenkins_nodes_regression.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'jenkins_nodes'
+
+
+info = [[u'[{"displayName": "master", "description": "the master Jenkins node", "temporarilyOffline": false, "monitorData": {}, "numExecutors": 0, "idle": true, "offlineCause": null, "offline": false, "_class": "hudson.model.Hudson$MasterComputer", "jnlpAgent": false}, {"displayName": "BuildNode1", "description": "", "temporarilyOffline": false, "monitorData": {}, "numExecutors": 20, "idle": false, "offlineCause": null, "offline": false, "_class": "hudson.slaves.SlaveComputer", "jnlpAgent": false}, {"displayName": "BuildNode2", "description": "", "temporarilyOffline": false, "monitorData": {}, "numExecutors": 10, "idle": true, "offlineCause": {"_class": "hudson.slaves.OfflineCause$LaunchFailed"}, "offline": true, "_class": "hudson.slaves.SlaveComputer", "jnlpAgent": false}, {"displayName": "Windows", "description": "Name: VM01", "temporarilyOffline": false, "monitorData": {}, "numExecutors": 1, "idle": true, "offlineCause": null, "offline": true, "_class": "hudson.slaves.SlaveComputer", "jnlpAgent": true}, {"displayName": "Windows", "description": "vM", "temporarilyOffline": false, "monitorData": {}, "numExecutors": 1, "idle": true, "offlineCause": null, "offline": false, "_class": "hudson.slaves.SlaveComputer", "jnlpAgent": true}]']]
+
+
+discovery = {'': [(u'BuildNode1', {}),
+                  (u'BuildNode2', {}),
+                  (u'Windows', {}),
+                  (u'master', {})]}
+
+
+checks = {'': [(u'BuildNode1',
+                {'jenkins_offline': 2},
+                [(0, u'Description: ', []),
+                 (0, 'Number of executers: 20', []),
+                 (0, 'JNLPAgent: False', []),
+                 (0, 'Idle: False', []),
+                 (0, 'Offline: False', [])]),
+               (u'BuildNode2',
+                {'jenkins_offline': 2},
+                [(0, u'Description: ', []),
+                 (0, 'Number of executers: 10', []),
+                 (0, 'JNLPAgent: False', []),
+                 (0, 'Idle: True', []),
+                 (2, 'Offline: True', [])]),
+               (u'Windows',
+                {'jenkins_offline': 2},
+                [(0, u'Description: Name: VM01', []),
+                 (0, 'Number of executers: 1', []),
+                 (0, 'JNLPAgent: True', []),
+                 (0, 'Idle: True', []),
+                 (2, 'Offline: True', []),
+                 (0, u'Description: vM', []),
+                 (0, 'Number of executers: 1', []),
+                 (0, 'JNLPAgent: True', []),
+                 (0, 'Idle: True', []),
+                 (0, 'Offline: False', [])]),
+               (u'master',
+                {'jenkins_offline': 2},
+                [(0, u'Description: the master Jenkins node', []),
+                 (0, 'Number of executers: 0', []),
+                 (0, 'JNLPAgent: False', []),
+                 (0, 'Idle: True', []),
+                 (0, 'Offline: False', [])])]}

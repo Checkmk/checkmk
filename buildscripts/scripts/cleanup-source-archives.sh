c7d22fa90cf22e1dceef02ea8cff3b112b80c152
Lars Michelsen <lm@tribe29.com>
2019-09-02 17:09:23 +0200

    Different shell cleanups
    Change-Id: If2857ea94d8ae36b145435d9825e6fb1b40e0307

--- buildscripts/scripts/cleanup-source-archives.sh
+++ buildscripts/scripts/cleanup-source-archives.sh
@@ -5,8 +5,8 @@
 set -e -o pipefail
 
 SRC_PATHS=$@
-if [ -z "$SRC_PATHS" ]; then 
-    echo "$0 PACKAGE..." 
+if [ -z "$SRC_PATHS" ]; then
+    echo "$0 PACKAGE..."
     echo "Example: $0 /path/to/package.tar.gz"
     exit 1
 fi
@@ -16,26 +16,26 @@ for SRC_PATH in $SRC_PATHS; do
     DIRNAME="${FILENAME%.tar.gz}"
     REMOVE_DIRS=""
     echo "=> $SRC_PATH"
-     
-    if tar tvzf $SRC_PATH | grep "$DIRNAME/managed" >/dev/null; then 
+
+    if tar tvzf $SRC_PATH | grep "$DIRNAME/managed" >/dev/null; then
         echo "Found CME specific components..."
         REMOVE_DIRS+=" $DIRNAME/managed"
     fi
-     
-    if tar tvzf $SRC_PATH | grep "$DIRNAME/enterprise" >/dev/null; then 
+
+    if tar tvzf $SRC_PATH | grep "$DIRNAME/enterprise" >/dev/null; then
         echo "Found CEE specific components..."
         REMOVE_DIRS+=" $DIRNAME/enterprise"
     fi
-    
+
     if [ -z "$REMOVE_DIRS" ]; then
         echo "Found no files to be removed. Done."
         continue
     fi
-    
+
     echo "Removing$REMOVE_DIRS..."
-    gunzip -c "$SRC_PATH" | tar -v --delete$REMOVE_DIRS | gzip > "$SRC_PATH.new"
+    gunzip -c "$SRC_PATH" | tar -v --delete$REMOVE_DIRS | gzip >"$SRC_PATH.new"
     mv "$SRC_PATH.new" "$SRC_PATH"
-    
+
     echo "Checking for remaining CEE/CME files..."
     if tar tvzf $SRC_PATH | grep -E "$DIRNAME/(managed|enterprise)"; then
         echo "ERROR: Still found some CEE/CME specific components."

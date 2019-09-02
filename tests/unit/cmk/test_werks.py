177fd966e202ab6e71065b94d66570cc4ee8ce82
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-09-02 20:31:28 +0200

    unit tests: CMA werks are now limited from 9000 to 9999
    Change-Id: I997aae27d393571ec725589bd4d0b5e2a6bd9aa2

--- tests/unit/cmk/test_werks.py
+++ tests/unit/cmk/test_werks.py
@@ -27,7 +27,7 @@ def test_write_precompiled_werks(edition, tmpdir, monkeypatch):
     cme_werks = dict([ (w["id"], w) for w in all_werks.values() if w["edition"] == "cme" ])
 
     assert len(cre_werks) > 1000
-    assert [ w for w in cre_werks.keys() if w >= 9000 ] == []
+    assert [ w for w in cre_werks.keys() if 9000 <= w < 10000 ] == []
     cmk.werks.write_precompiled_werks(os.path.join(tmp_dir, "werks"), cre_werks)
 
     assert len(cee_werks) > 700

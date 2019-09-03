d7bc02ad62a8d9e03ad73857dda8fd7a639c0893
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-09-03 16:10:04 +0200

    10003 docker_container_cpu: Plugin no longer requires agent in container
    The cpu service corresponding to the plugin docker_container_cpu no longer
    requires the Checkmk agent to be installed on the monitored container.
    
    Change-Id: I89b4f46a90a5bc9d412295c2cb20ce13489883b7

--- tests/unit/agents/plugins/test_mk_docker.py
+++ tests/unit/agents/plugins/test_mk_docker.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.join(cmk_path(), 'agents', 'plugins'))
 import mk_docker  # pylint: disable=import-error,wrong-import-position
 
 PLUGIN_CHECKSUMS = {
-    '0.1': '4f8dd3ae304e0420e8fc8f863cb8d174',
+    '0.1': '8b57c5a0ed5512e2f02e1d5ad4996215',
 }
 
 

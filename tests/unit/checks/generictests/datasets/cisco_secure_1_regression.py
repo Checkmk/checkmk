bfec480409b6642506a838d0f04530ae2e6420a2
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2019-09-03 16:29:06 +0200

    8791 FIX cisco_secure: don't crash if the interface name cannot be resolved
    Change-Id: I0dfc62aa63396a21fbb24a2f6b3fd9546702d4e8

--- tests/unit/checks/generictests/datasets/cisco_secure_1_regression.py
+++ tests/unit/checks/generictests/datasets/cisco_secure_1_regression.py
@@ -17,6 +17,7 @@ info = [
         [u'3', u'2', u'3', u'0', u'\x22\x33\x44\x55\x66\x77'],
         [u'4', u'2', u'', u'0', u'\x33\x44\x55\x66\x77\x88'],
         [u'5', u'3', u'2', u'0', u'\x44\x55\x66\x77\x88\x99'],
+        [u'6', u'3', u'2', u'0', u'\x55\x66\x77\x88\x99\xAA'],  # '6' is not in info[0]
     ],
 ]
 
@@ -45,6 +46,11 @@ checks = {
             (
                 3,
                 u'Port TenGigabitEthernet1/1/4: could not be enabled due to certain reasons (violation count: 0, last MAC: 44:55:66:77:88:99) unknown enabled state',
+                [],
+            ),
+            (
+                3,
+                u'Port 6: could not be enabled due to certain reasons (violation count: 0, last MAC: 55:66:77:88:99:aa) unknown enabled state',
                 [],
             ),
         ],

994dee93fe43aec0d9262787cdb1f6ccffa3300a
Sven Panne <sven.panne@tribe29.com>
2019-09-05 15:07:42 +0200

    Added missing cmk.utils.debug imports.
    Change-Id: I48b39ae82c5e2912e4f4286e474f78ec88b01c35

--- cmk_base/core_config.py
+++ cmk_base/core_config.py
@@ -31,6 +31,7 @@ import sys
 from typing import Text, Optional, Any, List, Dict  # pylint: disable=unused-import
 import six
 
+import cmk.utils.debug
 import cmk.utils.paths
 import cmk.utils.tty as tty
 import cmk.utils.password_store

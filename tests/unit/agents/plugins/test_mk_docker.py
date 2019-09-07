f6c1dfb68c9b256192f4617f286650e978a182f1
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-09-07 12:03:26 +0200

    mk_docker.py: prepare for parallelization
     * make _get_device_map a property
     * pull the container iteration into a function
    
    Change-Id: I76d4b5db2ae2152d3a7154b155ee03cc9dd092ec

--- tests/unit/agents/plugins/test_mk_docker.py
+++ tests/unit/agents/plugins/test_mk_docker.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.join(cmk_path(), 'agents', 'plugins'))
 import mk_docker  # pylint: disable=import-error,wrong-import-position
 
 PLUGIN_CHECKSUMS = {
-    '0.1': '1004391f5d510cab3c0a3f90e477b783',
+    '0.1': 'e49d015902e40b32f792c5665533c643',
 }
 
 

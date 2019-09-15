d597d097f802c56cc906bc53434450cba457afa3
Checkmk release system <feedback@check-mk.org>
2019-09-15 19:57:34 +0200

    Set version to 1.6.0b11
    Change-Id: I8258bbb5e9ffcc70babe172790e2c9d1bc29525f

--- cmk/__init__.py
+++ cmk/__init__.py
@@ -28,7 +28,7 @@
 This library is currently handled as internal module of Check_MK and
 does not offer stable APIs. The code may change at any time."""
 
-__version__ = "1.6.0b10"
+__version__ = "1.6.0b11"
 
 import os
 

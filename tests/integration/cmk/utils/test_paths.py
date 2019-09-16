0a93702cec92c4f9b2cc937659a5fd6fe1d0b457
Lars Michelsen <lm@tribe29.com>
2019-09-16 12:11:45 +0200

    Fix path tests
    Change-Id: I76759bd7f3b2e2428e9813658798eb9c23b5cf22

--- tests/integration/cmk/utils/test_paths.py
+++ tests/integration/cmk/utils/test_paths.py
@@ -8,6 +8,20 @@ pathlib_paths = [
     "discovered_host_labels_dir",
     "piggyback_dir",
     "piggyback_source_dir",
+    "mib_dir",
+    "local_share_dir",
+    "local_checks_dir",
+    "local_notifications_dir",
+    "local_inventory_dir",
+    "local_check_manpages_dir",
+    "local_agents_dir",
+    "local_web_dir",
+    "local_pnp_templates_dir",
+    "local_doc_dir",
+    "local_locale_dir",
+    "local_bin_dir",
+    "local_lib_dir",
+    "local_mib_dir",
 ]
 
 

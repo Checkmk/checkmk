be72f96e14161d490bff96c8860d8bd97801bc40
Jonas Kluger <jonas.kluger@tribe29.com>
2019-09-17 14:34:13 +0200

    10076 FIX check_traceroute: Fixed IP address error while doing the check
    When running the check the result always returned an error message, that the IP address cannot handle the commandline.
    
    Change-Id: I1a4ed07909fed5b7f48ef8aa36b3118779fb2376

--- tests/unit/checks/test_check_traceroute.py
+++ tests/unit/checks/test_check_traceroute.py
@@ -8,12 +8,12 @@ pytestmark = pytest.mark.checks
                              "dns": False,
                              "routers": [],
                              "method": ""
-                         }, ["-n", '"$HOSTADDRESS$"']),
+                         }, ["-n", "$HOSTADDRESS$"]),
                           ({
                               "dns": True,
                               "routers": [("127.0.0.1", "WARN")],
                               "method": "icmp"
-                          }, ["-I", "-WARN", "127.0.0.1", '"$HOSTADDRESS$"'])])
+                          }, ["-I", "-WARN", "127.0.0.1", "$HOSTADDRESS$"])])
 def test_check_traceroute_argument_parsing(check_manager, params, expected_args):
     """Tests if all required arguments are present."""
     active_check = check_manager.get_active_check("check_traceroute")

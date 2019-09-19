c40f17651064158518c6ec6554b4ef3446e15c1f
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-09-19 09:15:10 +0200

    10136 docker_container_status.uptime: Add uptime service for containers w/o checkmk agent
    Change-Id: I6484efee4ab221c4a9e849c00bcab0fdd9b067bd

--- tests/unit/checks/generictests/datasets/docker_container_status_2.py
+++ tests/unit/checks/generictests/datasets/docker_container_status_2.py
@@ -10,7 +10,29 @@ info = [[u'@docker_version_info',
          [u'{"Status": "exited", "Pid": 0, "OOMKilled": false, "Dead": false, "RestartPolicy": {"MaximumRetryCount": 0, "Name": "always"}, "Paused": false, "Running": false, "FinishedAt": "2019-07-11T15:18:44.293247926Z", "Restarting": false, "Error": "", "StartedAt": "2019-07-11T13:45:30.378476501Z", "ExitCode": 0, "NodeName": "Quarktasche"}']]
 
 
-discovery = {'': [(None, {})], 'health': []}
+extra_sections = {
+    '': [[]],
+    'health': [[]],
+    'uptime': [[]],
+}
+
+discovery = {
+    '': [
+        (None, {}),
+    ],
+    'health': [],
+}
 
 
-checks = {'': [(None, {}, [(2, u'Container exited on node Quarktasche', [])])]}
+checks = {
+    '': [
+        (None, {}, [
+            (2, u'Container exited on node Quarktasche', []),
+        ]),
+    ],
+    'uptime': [
+        (None, {}, [
+            (0, "[exited]", [("uptime", 0.0)]),
+        ]),
+    ],
+}

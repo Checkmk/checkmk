b45a141d23a52b63a443a16035f203b559c82640
Lars Michelsen <lm@tribe29.com>
2019-09-25 18:21:25 +0200

    mk_logwatch: Add non-ascii log line processing unit test
    Change-Id: I06796e0d925c8d015d722ef981a801d7d909bf56

--- tests/unit/agents/plugins/test_mk_logwatch.py
+++ tests/unit/agents/plugins/test_mk_logwatch.py
@@ -9,6 +9,8 @@ import pytest  # type: ignore
 from testlib import import_module
 import six
 
+from testlib import cmk_path  # pylint: disable=import-error
+
 
 @pytest.fixture(scope="module")
 def mk_logwatch():
@@ -309,6 +311,62 @@ def test_log_lines_iter(mk_logwatch):
     assert log_iter.get_position() == os.stat(mk_logwatch.__file__).st_size
 
 
+@pytest.mark.parametrize(
+    "use_specific_encoding,lines,expected_result",
+    [
+        # UTF-8 encoding works by default
+        (None, [
+            b"abc1",
+            u"äbc2".encode("utf-8"),
+            b"abc3",
+        ], [
+            u"abc1\n",
+            u"äbc2\n",
+            u"abc3\n",
+        ]),
+        # Replace characters that can not be decoded
+        (None, [
+            b"abc1",
+            u"äbc2".encode("latin-1"),
+            b"abc3",
+        ], [
+            u"abc1\n",
+            u"\ufffdbc2\n",
+            u"abc3\n",
+        ]),
+        # Set custom encoding
+        ("latin-1", [
+            b"abc1",
+            u"äbc2".encode("latin-1"),
+            b"abc3",
+        ], [
+            u"abc1\n",
+            u"äbc2\n",
+            u"abc3\n",
+        ]),
+    ])
+def test_non_ascii_line_processing(mk_logwatch, tmp_path, monkeypatch, use_specific_encoding, lines,
+                                   expected_result):
+    # Write test logfile first
+    log_path = tmp_path.joinpath("testlog")
+    with log_path.open("wb") as f:
+        f.write(b"\n".join(lines) + "\n")
+
+    # Now test processing
+    log_iter = mk_logwatch.LogLinesIter(str(log_path))
+    if use_specific_encoding:
+        log_iter._enc = use_specific_encoding
+
+    result = []
+    while True:
+        l = log_iter.next_line()
+        if l is None:
+            break
+        result.append(l)
+
+    assert result == expected_result
+
+
 class MockStdout(object):
     def isatty(self):
         return False
@@ -445,20 +503,3 @@ def fake_filesystem(tmp_path):
     assert os.stat(str(hard_linked_file_a)) == os.stat(str(hard_linked_file_b))
 
     return fake_fs
-
-
-def _print_tree(directory, resolve=True):
-    """
-    Print fake filesystem with optional expansion of symlinks.
-    Don't remove this helper function. Used for debugging.
-    """
-    print('%s' % directory)
-    for path in sorted(directory.rglob('*')):
-        if resolve:
-            try:  # try resolve symlinks
-                path = path.resolve()
-            except Exception:
-                pass
-        depth = len(path.relative_to(directory).parts)
-        spacer = '    ' * depth
-        print('%s%s' % (spacer, path.name))

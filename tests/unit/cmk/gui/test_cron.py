5142de3ba525a2734d61f4a4bb4df6ad5aaf3cd0
Lars Michelsen <lm@tribe29.com>
2019-09-27 15:42:28 +0200

    10241 FIX Report scheduler: The scheduler and stored report cleanup jobs were not triggered
    The scheduled reports were not triggered in previous 1.6 releases.
    
    Change-Id: If16041ef3997682c30b029f3a9329b4535123243

---
+++ tests/unit/cmk/gui/test_cron.py
@@ -0,0 +1,13 @@
+import cmk.gui.cron as cron
+
+
+def test_registered_jobs():
+    found_jobs = sorted(["%s.%s" % (f.__module__, f.__name__) for f in cron.multisite_cronjobs])
+    assert found_jobs == sorted([
+        'cmk.gui.cee.reporting.cleanup_stored_reports',
+        'cmk.gui.cee.reporting.do_scheduled_reports',
+        'cmk.gui.inventory.run',
+        'cmk.gui.plugins.cron.gui_background_job.housekeeping',
+        'cmk.gui.userdb.execute_userdb_job',
+        'cmk.gui.wato.execute_network_scan_job',
+    ])

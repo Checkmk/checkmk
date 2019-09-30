19adceb6307b41b907f36c2e32d03939720d8ed8
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-09-30 13:30:53 +0200

    mk_errpt.aix: do renaming for user
    Change-Id: Ib8ecc8ea230119cfb622d8e0b4ca3699c6938781

--- tests/unit/agents/plugins/test_mk_errpt_aix.py
+++ tests/unit/agents/plugins/test_mk_errpt_aix.py
@@ -6,13 +6,17 @@ from testlib import cmk_path
 PLUGIN = os.path.join(cmk_path(), 'agents', 'plugins', 'mk_errpt.aix')
 
 ERRPT_OUTPUT = [
-    'IDENTIFIER TIMESTAMP  T C RESOURCE_NAME  DESCRIPTION',
-    '8650BE3F   0820122810 I H ent2           ETHERCHANNEL RECOVERY',
-    'F3846E13   0820122510 P H ent2           ETHERCHANNEL FAILOVER',
-    '8650BE3F   0820104410 I H ent2           ETHERCHANNEL RECOVERY',
-    'F3846E13   0820093810 P H ent2           ETHERCHANNEL FAILOVER',
-    '8650BE3F   0820090910 I H ent2           ETHERCHANNEL RECOVERY',
+    u'IDENTIFIER TIMESTAMP  T C RESOURCE_NAME  DESCRIPTION',
+    u'8650BE3F   0820122810 I H ent2           ETHERCHANNEL RECOVERY',
+    u'F3846E13   0820122510 P H ent2           ETHERCHANNEL FAILOVER',
+    u'8650BE3F   0820104410 I H ent2           ETHERCHANNEL RECOVERY',
+    u'F3846E13   0820093810 P H ent2           ETHERCHANNEL FAILOVER',
+    u'8650BE3F   0820090910 I H ent2           ETHERCHANNEL RECOVERY',
 ]
+
+STATE_FILE_NAME = "mk_errpt_aix.last_reported"
+
+LEGACY_STATE_FILE_NAME = "mk_logwatch_aix.last_reported"
 
 
 def _prepare_mock_errpt(tmp_path, errpt_output):
@@ -29,29 +33,29 @@ def _get_env(tmp_path):
     return env
 
 
-class StateFile(object):
-    def __init__(self, tmp_path):
-        super(StateFile, self).__init__()
-        self.name = str(tmp_path.joinpath('mk_errpt_aix.last_reported'))
-
-    def set(self, state):
-        if state is None:
-            try:
-                os.remove(self.name)
-            except OSError:
-                pass
-            return
-        with open(self.name, 'w') as statefile:
-            statefile.write("%s\n" % state)
-
-    def content(self):
+def prepare_state(filepath, write_name, state):
+    # make sure we have no left-over files
+    for base_file in (STATE_FILE_NAME, LEGACY_STATE_FILE_NAME):
         try:
-            content = open(self.name).read()
-            # ends with newline
-            assert content[-1] == '\n'
-            return content[:-1]
-        except IOError:
-            return None
+            filepath.joinpath(base_file).unlink()
+        except OSError:
+            pass
+
+    if state is None:
+        return
+
+    with filepath.joinpath(write_name).open('w') as statefile:
+        statefile.write(u"%s\n" % state)
+
+
+def read_state(filepath):
+    try:
+        with filepath.joinpath(STATE_FILE_NAME).open() as statefile:
+            new_state = statefile.read()
+            assert new_state[-1] == u"\n"
+            return new_state[:-1]
+    except IOError:
+        return None
 
 
 def _format_expected(lines):
@@ -61,33 +65,43 @@ def _format_expected(lines):
 
 
 @pytest.mark.parametrize(
-    "errpt_output,last_reported,expectations",
+    "state_file_name,errpt_output,last_reported,expectations",
     [
         (
+            STATE_FILE_NAME,
             ERRPT_OUTPUT,
-            [None, "", ERRPT_OUTPUT[3], ERRPT_OUTPUT[1], "something else entirely"],
+            [None, u"", ERRPT_OUTPUT[3], ERRPT_OUTPUT[1], u"something else entirely"],
             [ERRPT_OUTPUT[1:], ERRPT_OUTPUT[1:], ERRPT_OUTPUT[1:3], [], ERRPT_OUTPUT[1:]],
         ),
         (
+            STATE_FILE_NAME,
             ERRPT_OUTPUT[:1],  # no output, just header
-            [None, "", "what ever"],
+            [None, u"", u"what ever"],
             [[], [], []],
         ),
+        (  # legacy statefile name:
+            'mk_logwatch_aix.last_reported',
+            ERRPT_OUTPUT,
+            [None, u"", ERRPT_OUTPUT[3], ERRPT_OUTPUT[1], u"something else entirely"],
+            [ERRPT_OUTPUT[1:], ERRPT_OUTPUT[1:], ERRPT_OUTPUT[1:3], [], ERRPT_OUTPUT[1:]],
+        ),
     ])
-def test_mk_errpt_aix(tmp_path, errpt_output, last_reported, expectations):
+def test_mk_errpt_aix(tmp_path, state_file_name, errpt_output, last_reported, expectations):
 
     _prepare_mock_errpt(tmp_path, errpt_output)
     env = _get_env(tmp_path)
-    statefile = StateFile(tmp_path)
 
     for state, expected in zip(last_reported, expectations):
 
-        statefile.set(state)
+        prepare_state(tmp_path, state_file_name, state)
 
         output = subprocess.check_output(PLUGIN, env=env)
+        expected = _format_expected(expected)
+        assert output == expected, "expected\n  %r, but got\n  %r" % (expected, output)
 
-        assert output == _format_expected(expected)
+        new_state = read_state(tmp_path)
+
         if len(errpt_output) > 1:  # we should have updated state file
-            assert statefile.content() == errpt_output[1]
+            assert new_state == errpt_output[1]
         else:  # it should not have changed
-            assert statefile.content() == state
+            assert new_state == state

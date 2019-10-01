46ed83ec3c489a0d012f5053a676ef4fdb4c41ad
Mathias Laurin <mathias.laurin@tribe29.com>
2019-10-01 11:10:34 +0200

    10268 Monitor ingresses from Kubernetes
    This werk adds check for Kubernetes ingresses.  It outputs path, port,
    and service name.
    
    The check adds one entry per path.
    
    CMK-2865
    
    Change-Id: I3a46af654c4633fd501ea296c300bfc1e99a9dfb

---
+++ tests/unit/checks/generictests/datasets/k8s_ingress_infos.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8
+# yapf: disable
+from cmk_base.discovered_labels import HostLabel
+
+checkname = 'k8s_ingress_infos'
+
+parsed = {
+    u'cafe-ingress': {
+        u'load_balancers': [{
+            u'ip': u'10.0.2.15',
+            u'hostname': u''
+        }],
+        u'backends': [
+            [u'cafe.example.com/tea', u'tea-svc', 80],
+            [u'cafe.example.com/coffee', u'coffee-svc', 80]
+        ],
+        u'hosts': {
+            u'cafe-secret': [u'cafe.example.com']
+        }
+    }
+}
+
+discovery = {
+    '': [
+        HostLabel(u'cmk/kubernetes_object', u'ingress', plugin_name=None),
+        (u'cafe.example.com/coffee', None), (u'cafe.example.com/tea', None)
+    ]
+}
+
+checks = {
+    '': [
+        (
+            u'cafe.example.com/coffee', {}, [
+                (0, u'Ports: 80, 443', []), (0, u'Service: coffee-svc:80', [])
+            ]
+        ),
+        (
+            u'cafe.example.com/tea', {}, [
+                (0, u'Ports: 80, 443', []), (0, u'Service: tea-svc:80', [])
+            ]
+        )
+    ]
+}

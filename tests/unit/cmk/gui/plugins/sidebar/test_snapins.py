4a4ec97a2bc30e1296733022e5513302ca968e4b
Lars Michelsen <lm@tribe29.com>
2019-10-02 15:42:45 +0200

    Add unit tests
    Change-Id: I6e41f010afd869c375bbdce2d4b7400616da7fab

---
+++ tests/unit/cmk/gui/plugins/sidebar/test_snapins.py
@@ -0,0 +1,56 @@
+# Needed to trigger plugin loading
+import cmk.gui.sidebar  # pylint: disable=unused-import
+
+from cmk.gui.plugins.sidebar.utils import snapin_registry
+
+
+def test_registered_snapins():
+    assert sorted(snapin_registry.keys()) == sorted([
+        'about',
+        'admin',
+        'admin_mini',
+        'biaggr_groups',
+        'biaggr_groups_tree',
+        'bookmarks',
+        'cmc_stats',
+        'custom_links',
+        'dashboards',
+        'hostgroups',
+        'hostmatrix',
+        'hosts',
+        'master_control',
+        'mkeventd_performance',
+        'nagios_legacy',
+        'nagvis_maps',
+        'performance',
+        'problem_hosts',
+        'reports',
+        'search',
+        'servicegroups',
+        'sitestatus',
+        'speedometer',
+        'tactical_overview',
+        'tag_tree',
+        'time',
+        'views',
+        'wato_folders',
+        'wato_foldertree',
+        'wiki',
+    ])
+
+
+def test_refresh_snapins():
+    refresh_snapins = [s.type_name() for s in snapin_registry.values() if s.refresh_regularly()]
+    assert sorted(refresh_snapins) == sorted([
+        'admin',
+        'admin_mini',
+        'cmc_stats',
+        'performance',
+        'hostmatrix',
+        'mkeventd_performance',
+        'nagvis_maps',
+        'sitestatus',
+        'tactical_overview',
+        'tag_tree',
+        'time',
+    ])

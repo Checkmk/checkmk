dbf610bb7aa2d1e441c34badfb1c81fc244c32db
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-10-04 14:35:05 +0200

    emc_isilon_cpu: use check_cpu_util
    Change-Id: I09e12d5994b5e167190d99a87b88d1747e02e47f

--- tests/unit/checks/generictests/datasets/emc_isilon_cpu_regression.py
+++ tests/unit/checks/generictests/datasets/emc_isilon_cpu_regression.py
@@ -5,11 +5,19 @@ checkname = 'emc_isilon_cpu'
 
 info = [['123', '234', '231', '567']]
 
-discovery = {'': [(None, None)]}
+discovery = {'': [(None, {})]}
 
 checks = {
-    '': [(None, None, [(0, 'user: 35.7%, system: 23.1%, total: 115.5%',
-                      [('user', 35.7, None, None, None, None),
-                       ('system', 23.1, None, None, None, None),
-                       ('interrupt', 56.7, None, None, None, None)])])]
+    '': [(None, {}, [
+        (0, 'User: 35.7%', [('user', 35.7, None, None, None, None)]),
+        (0, 'System: 23.1%', [('system', 23.1, None, None, None, None)]),
+        (0, 'Interrupt: 56.7%', [('interrupt', 56.7, None, None, None, None)]),
+        (0, 'Total: 115%', [])
+    ]),
+       (None, None, [
+        (0, 'User: 35.7%', [('user', 35.7, None, None, None, None)]),
+        (0, 'System: 23.1%', [('system', 23.1, None, None, None, None)]),
+        (0, 'Interrupt: 56.7%', [('interrupt', 56.7, None, None, None, None)]),
+        (0, 'Total: 115%', [])
+    ])]
 }

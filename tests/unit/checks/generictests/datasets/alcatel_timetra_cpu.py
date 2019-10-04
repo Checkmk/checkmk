0bd9051cb311bfd676616e10bf813ed5735e700d
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-10-04 12:23:56 +0200

    alcatel_timetra_cpu: use check_cpu_util
    CMK-2782
    
    Change-Id: I6c2b5cf6b8d13e1910833090c295c0f6a9e371bf

--- tests/unit/checks/generictests/datasets/alcatel_timetra_cpu.py
+++ tests/unit/checks/generictests/datasets/alcatel_timetra_cpu.py
@@ -8,6 +8,6 @@ info = [['92']]
 discovery = {'': [(None, 'alcatel_timetra_cpu_default_levels')]}
 
 checks = {
-    '': [(None, (90.0, 95.0), [(1, 'total: 92.0% (warn/crit at 90.0%/95.0%)',
+    '': [(None, (90.0, 95.0), [(1, 'Total CPU: 92.0% (warn/crit at 90.0%/95.0%)',
                                 [('util', 92, 90.0, 95.0, 0, 100)])])]
 }

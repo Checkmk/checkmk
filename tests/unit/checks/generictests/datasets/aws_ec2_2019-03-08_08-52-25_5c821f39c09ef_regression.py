924a93c57b1ea5ab56dbbcb8ae4924e8f37501da
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-10-07 11:02:01 +0200

    aws_ec2: make factory_settings gui compliant
    Change-Id: I5367f4efa962284139c7ead4c52db4604c125eea

--- tests/unit/checks/generictests/datasets/aws_ec2_2019-03-08_08-52-25_5c821f39c09ef_regression.py
+++ tests/unit/checks/generictests/datasets/aws_ec2_2019-03-08_08-52-25_5c821f39c09ef_regression.py
@@ -1,28 +1,38 @@
 # -*- encoding: utf-8
 # yapf: disable
 
-
 checkname = u'aws_ec2'
 
+parsed = {
+    'Summary': {
+        'CPUUtilization': 0.1,
+        'NetworkIn': 3540.4,
+        'StatusCheckFailed_Instance': 0.0,
+        'NetworkOut': 27942.1,
+        'StatusCheckFailed_System': 0.0
+    }
+}
 
-parsed = {'Summary': {'CPUUtilization': 0.1,
-                      'NetworkIn': 3540.4,
-                      'NetworkOut': 27942.1,
-                      'StatusCheckFailed_Instance': 0.0,
-                      'StatusCheckFailed_System': 0.0}}
+discovery = {
+    '': [(None, {})],
+    'cpu_util': [(None, {})],
+    'network_io': [('Summary', {})],
+    'disk_io': [],
+    'cpu_credits': []
+}
 
-
-discovery = {'': [(None, {})],
-             'cpu_credits': [],
-             'cpu_util': [(None, {})],
-             'disk_io': [],
-             'network_io': [('Summary', {})]}
-
-
-checks = {'': [(None, {}, [(0, 'System: passed', []), (0, 'Instance: passed', [])])],
-          'cpu_util': [(None,
-                        {'levels': (90.0, 95.0)},
-                        [(0, 'Total CPU: 0.1%', [('util', 0.1, 90.0, 95.0, 0, 100)])])],
-          'network_io': [('Summary',
-                          {'errors': (0.01, 0.1)},
-                          [(0, '[0] (up) speed unknown', [])])]}
+checks = {
+    '': [(None, {}, [(0, 'System: passed', []), (0, 'Instance: passed', [])])],
+    'cpu_util':
+    [(None, {
+        'util': (0.01, 95.0)
+    }, [(1, 'Total CPU: 0.1% (warn/crit at 0.01%/95.0%)', [('util', 0.1, 0.01,
+                                                            95.0, 0, 100)])]),
+     (None, {
+         'util': (90.0, 95.0)
+     }, [(0, 'Total CPU: 0.1%', [('util', 0.1, 90.0, 95.0, 0, 100)])])],
+    'network_io': [('Summary', {
+        'errors_in': (0.01, 0.1),
+        'errors_out': (0.01, 0.1)
+    }, [(0, '[0] (up) speed unknown', [])])]
+}

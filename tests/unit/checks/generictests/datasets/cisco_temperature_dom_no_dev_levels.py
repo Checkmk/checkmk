ac901463a45810bbe88d30f162d42aeeac646420
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-10-07 21:43:37 +0200

    tests: complete datasets
    The cleaned up generictests will be a little bit more strict,
    in the sense that the full discovery results must be present.
    
    Since we can create them using the generictests/regression.py
    script, this is an acceptable requirement.
    
    Change-Id: Ib6a69cdb80646502c96ff7e8c818b8b88b0c297f

--- tests/unit/checks/generictests/datasets/cisco_temperature_dom_no_dev_levels.py
+++ tests/unit/checks/generictests/datasets/cisco_temperature_dom_no_dev_levels.py
@@ -1,29 +1,34 @@
 # -*- encoding: utf-8
 # yapf: disable
 
-
 checkname = u'cisco_temperature'
-
 
 parsed = {
     '14': {
         u'NoLevels': {
-            'raw_dev_state': u'1',
-            'dev_levels': None,
-            'reading': 3.14,
-            'dev_state': (0, 'awesome'),
             'descr': '',
-        },
-    },
+            'reading': 3.14,
+            'raw_dev_state': u'1',
+            'dev_state': (0, 'awesome'),
+            'dev_levels': None
+        }
+    }
 }
 
+discovery = {'': [], 'dom': [(u'NoLevels', {})]}
 
 checks = {
     '': [],
     'dom': [
-        ('NoLevels', {}, [
-            (0, 'Status: awesome', []),
-            (0, 'Signal power: 3.14 dBm', [('signal_power_dbm', 3.14)]),
-        ]),
-    ],
+        (
+            u'NoLevels', {}, [
+                (0, 'Status: awesome', []),
+                (
+                    0, 'Signal power: 3.14 dBm', [
+                        ('signal_power_dbm', 3.14, None, None, None, None)
+                    ]
+                )
+            ]
+        )
+    ]
 }

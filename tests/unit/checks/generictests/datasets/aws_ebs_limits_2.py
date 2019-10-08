12c0195f86445c1307b2d4e851868781c5666482
Simon Jess <simon.jess@tribe29.com>
2019-10-08 10:02:45 +0200

    10212 FIX aws_limits: Fixed parsing old agent data which does not contain information about region
    Change-Id: I8605110190c9058331152ac3dee9c3e96c40e4b0

---
+++ tests/unit/checks/generictests/datasets/aws_ebs_limits_2.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+checkname = 'aws_ebs_limits'
+
+info = [['[["block_store_snapshots",', '"TITLE",', '10,', '1,', '"REGION"]]']]
+
+discovery = {'': [(None, {})]}
+
+checks = {
+    '': [
+        (
+            None, {
+                'block_store_space_gp2': (None, 80.0, 90.0),
+                'block_store_space_sc1': (None, 80.0, 90.0),
+                'block_store_space_st1': (None, 80.0, 90.0),
+                'block_store_snapshots': (None, 80.0, 90.0),
+                'block_store_iops_io1': (None, 80.0, 90.0),
+                'block_store_space_standard': (None, 80.0, 90.0),
+                'block_store_space_io1': (None, 80.0, 90.0)
+            }, [
+                (
+                    0, 'No levels reached', [
+                        (
+                            u'aws_ebs_block_store_snapshots', 1, None, None,
+                            None, None
+                        )
+                    ]
+                ), (0, u'\nTITLE: 1 (of max. 10) (Region REGION)', [])
+            ]
+        )
+    ]
+}

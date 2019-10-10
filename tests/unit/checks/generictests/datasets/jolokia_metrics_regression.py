32f1b07cd296310b814c17ca378baef1323a4d43
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-10-10 13:42:37 +0200

    jolokia_metrics: expand test
    Change-Id: Ica0b80badd7ed29cc7c92e600ea02f6792b94089

--- tests/unit/checks/generictests/datasets/jolokia_metrics_regression.py
+++ tests/unit/checks/generictests/datasets/jolokia_metrics_regression.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8
 # yapf: disable
 
-
 checkname = 'jolokia_metrics'
 
+freeze_time = '2019-10-10 20:38:55'
 
 info = [
     ['MyInstance', 'NonHeapMemoryUsage', '101078952'],
@@ -15,39 +15,104 @@ info = [
     ['MyInstance', 'PeakThreadCount', '191'],
     ['MyInstance', 'TotalStartedThreadCount', '941'],
     ['MyInstance', 'Uptime', '572011375'],
-    ['MyInstance,java.lang:name=PS_MarkSweep,type=GarbageCollector', 'CollectionCount', '0'],
+    [
+        'MyInstance,java.lang:name=PS_MarkSweep,type=GarbageCollector',
+        'CollectionCount', '66'
+    ],
+    [
+        'MyInstance,java.lang:name=PS_Scavenge,type=GarbageCollector',
+        'CollectionCount', '51203'
+    ],
+    [
+        'MyInstance,java.lang:name=PS_MarkSweep,type=GarbageCollector',
+        'CollectionTime', '115326'
+    ],
+    [
+        'MyInstance,java.lang:name=PS_Scavenge,type=GarbageCollector',
+        'CollectionTime', '5710533'
+    ]
 ]
 
-
 discovery = {
-    'app_sess': [],
     'app_state': [],
-    'bea_queue': [],
-    'bea_requests': [],
-    'bea_sess': [],
-    'bea_threads': [],
-    'cache_hits': [],
-    'gc': [('MyInstance GC PS_MarkSweep', {})],
-    'in_memory': [],
-    'mem': [('MyInstance', {})],
     'off_heap': [],
+    'mem': [('MyInstance', {})],
+    'bea_threads': [],
+    'app_sess': [],
+    'writer': [],
     'on_disk': [],
-    'perm_gen': [],
-    'requests': [],
-    'serv_req': [],
-    'threads': [],
-    'tp': [],
     'uptime': [('MyInstance', None)],
-    'writer': []}
+    'tp': [],
+    'bea_requests': [],
+    'bea_queue': [],
+    'threads': [],
+    'serv_req': [],
+    'cache_hits': [],
+    'in_memory': [],
+    'gc':
+    [('MyInstance GC PS_MarkSweep', {}), ('MyInstance GC PS_Scavenge', {})],
+    'requests': [],
+    'bea_sess': [],
+    'perm_gen': []
+}
 
-
-checks = {  # this is just a test for mem.
+checks = {
     'mem': [
-        ('MyInstance', {}, [
-            (0, 'Heap: 2253MB/24.8% used, Nonheap: 96MB/54.8% used, Total: 2350MB/25.3% used', [
-                ('heap', 2253.324188232422, None, None, None, 9102.5),
-                ('nonheap', 96.39640045166016, None, None, None, 176.0),
-            ]),
-        ]),
+        (
+            'MyInstance', {}, [
+                (
+                    0,
+                    'Heap: 2253MB/24.8% used, Nonheap: 96MB/54.8% used, Total: 2350MB/25.3% used',
+                    [
+                        ('heap', 2253.324188232422, None, None, None, 9102.5),
+                        (
+                            'nonheap', 96.39640045166016, None, None, None,
+                            176.0
+                        )
+                    ]
+                )
+            ]
+        )
     ],
+    'uptime': [
+        (
+            'MyInstance', {}, [
+                (
+                    0, 'Up since Fri Oct  4 07:45:24 2019 (6d 14:53:31)', [
+                        ('uptime', 572011, None, None, None, None)
+                    ]
+                )
+            ]
+        )
+    ],
+    'gc': [
+        (
+            'MyInstance GC PS_MarkSweep', {}, [
+                (
+                    0, '0.00 GC Count/minute', [
+                        ('CollectionCount', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, '0.00 GC ms/minute', [
+                        ('CollectionTime', 0.0, None, None, None, None)
+                    ]
+                )
+            ]
+        ),
+        (
+            'MyInstance GC PS_Scavenge', {}, [
+                (
+                    0, '0.00 GC Count/minute', [
+                        ('CollectionCount', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, '0.00 GC ms/minute', [
+                        ('CollectionTime', 0.0, None, None, None, None)
+                    ]
+                )
+            ]
+        )
+    ]
 }

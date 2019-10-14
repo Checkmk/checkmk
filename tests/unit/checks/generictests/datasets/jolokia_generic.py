bb0b03315c539c8f8b62500ede8ef8dceb760314
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-10-14 21:31:44 +0200

    jolokia_generic: extend tests
    Change-Id: I6c7fd245522424e98fff09d20279284300e36381

--- tests/unit/checks/generictests/datasets/jolokia_generic.py
+++ tests/unit/checks/generictests/datasets/jolokia_generic.py
@@ -1,38 +1,89 @@
+# -*- encoding: utf-8
 # yapf: disable
 checkname = 'jolokia_generic'
 
+freeze_time = '2019-10-14 20:55:30'
+
 info = [
-    ['Instance1', 'Mitglieder_Anzahl', -23, 'number'],
-    ['Instance2', 'Mitglieder Anzahl', -23, 'number'],
+    ['Instance1', 'Mitglieder_Anzahl', '-23', 'number'],
+    ['Instance2', 'Mitglieder Anzahl', '-23', 'number'],
+    ['Instance2', 'Beitritte', '42', 'rate'],
+    ['Instance2', 'Current Motto', 'Never gonna give you up', 'string'],
     [
-        'JIRA,com.atlassian.jira:type=web.requests,invocation.count', 'jira.name',
-        'invocation.count', 'number'
+        'JIRA,com.atlassian.jira:type=web.requests,invocation.count',
+        'jira.name', 'invocation.count', 'number'
     ],
     [
-        'JIRA,com.atlassian.jira:type=web.requests,invocation.count', 'jira.value', '2624460',
-        'number'
-    ],
+        'JIRA,com.atlassian.jira:type=web.requests,invocation.count',
+        'jira.value', '2624460', 'number'
+    ]
 ]
 
 discovery = {
     '': [
         ('Instance1 Mitglieder_Anzahl', {}),
         ('Instance2 Mitglieder Anzahl', {}),
-        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count jira.value', {}),
+        (
+            'JIRA,com.atlassian.jira:type=web.requests,invocation.count jira.value',
+            {}
+        )
     ],
-    'rate': [],
-    'string': []
+    'rate': [('Instance2 Beitritte', {})],
+    'string': [('Instance2 Current Motto', {})]
 }
 
 checks = {
     '': [
-        ('Instance1 Mitglieder_Anzahl', {}, [(0, '-23.0', [('generic_number', -23.0, None, None,
-                                                            None, None)])]),
-        ('Instance2 Mitglieder Anzahl', {}, [(0, '-23.0', [('generic_number', -23.0, None, None,
-                                                            None, None)])]),
-        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count jira.name', {},
-         [(3, "Non-numeric MBean value", [])]),
-        ('JIRA,com.atlassian.jira:type=web.requests,invocation.count jira.value', {},
-         [(0, "2624460.0", [('generic_number', 2624460.0, None, None, None, None)])]),
-    ]
+        (
+            'Instance1 Mitglieder_Anzahl', {}, [
+                (
+                    0, '-23.0', [
+                        ('generic_number', -23.0, None, None, None, None)
+                    ]
+                )
+            ]
+        ),
+        (
+            'Instance2 Mitglieder Anzahl', {}, [
+                (
+                    0, '-23.0', [
+                        ('generic_number', -23.0, None, None, None, None)
+                    ]
+                )
+            ]
+        ),
+        (
+            'JIRA,com.atlassian.jira:type=web.requests,invocation.count jira.name',
+            {}, [(3, 'Non-numeric MBean value', [])]
+        ),
+        (
+            'JIRA,com.atlassian.jira:type=web.requests,invocation.count jira.value',
+            {}, [
+                (
+                    0, '2624460.0', [
+                        ('generic_number', 2624460.0, None, None, None, None)
+                    ]
+                )
+            ]
+        )
+    ],
+    'rate': [
+        (
+            'Instance2 Beitritte', {}, [
+                (
+                    0, '0.3', [
+                        (
+                            'generic_rate', 0.328125, None, None,
+                            None, None
+                        )
+                    ]
+                )
+            ]
+        )
+    ],
+    'string':
+    [('Instance2 Current Motto', {}, [(0, 'Never gonna give you up', [])])]
 }
+
+
+mock_item_state = {'rate': (1571086402, 0)}

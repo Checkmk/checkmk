b4777c4fbbd4b31a0a4d2a6fe02ce5317125a0b4
Sven Rueß <github@sritd.de>
2019-10-29 15:58:09 +0100

    Fixed detection of McAfee plugin on 64 and 32 bit systems
    Signed-off-by: Sven Rueß <github@sritd.de>

--- agents/windows/plugins/mcafee_av_client.bat
+++ agents/windows/plugins/mcafee_av_client.bat
@@ -8,10 +8,14 @@ rem #  -------------------------------------------------------------------------
 setlocal enableDelayedExpansion
 set dateval=
 
-for /f "tokens=3" %%a in ('reg query "HKLM\SOFTWARE\McAfee\AvEngine" ^|find /I "AVDatDate" ^|find "REG_SZ"') do @set dateval=%%a
-if "%dateval%"=="" (
-    rem # on 64 bit systems the registry key is in the wow64/32 branch
-    for /f "tokens=3" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\McAfee\AvEngine" ^|find /I "AVDatDate" ^|find "REG_SZ"') do @set dateval=%%a
+rem # on 64 bit systems
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  for /f "eol=S skip=2 tokens=3" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\McAfee\AvEngine" /v AVDatDate /t REG_SZ 2^>nul') do @set dateval=%%a
+)
+
+rem # on 32 bit systems
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+  for /f "eol=S skip=2 tokens=3" %%a in ('reg query "HKLM\SOFTWARE\McAfee\AvEngine" /v AVDatDate /t REG_SZ 2^>nul') do @set dateval=%%a
 )
 
 if not "%dateval%"=="" (

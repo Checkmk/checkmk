a69839ea335a40fb4967294f95871e2acc5d6cac
Lars Michelsen <lm@tribe29.com>
2019-10-29 11:10:58 +0000

    Add new integration test to verify the locales in our test environments
    Change-Id: I48bf58f9b9ccea3512b2c32499b7edb572c6ff6e

---
+++ tests/integration/omd/test_environment.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import subprocess
+
+
+def test_locales(site):
+    p = site.execute(["locale"], stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+
+    assert "LANG=C.UTF-8" in output
+    assert "LC_ALL=C.UTF-8" in output

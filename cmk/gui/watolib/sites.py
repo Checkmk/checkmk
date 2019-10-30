1739dca7cd535e99f66ab2e9f5bfe99e55d19829
Andreas Boesl <andreas.boesl@tribe29.com>
2019-10-30 15:49:18 +0100

    7282 FIX liveproxyd: Reduced CPU usage
    Introduced first fixes to reduce the CPU usage of the liveproxyd. More to come.
    
    Change-Id: I93d87ea37a1dec2b7f01ea97f72a80c668081a1a

--- cmk/gui/watolib/sites.py
+++ cmk/gui/watolib/sites.py
@@ -561,6 +561,8 @@ class CEESiteManagement(SiteManagement):
                 "socket": siteconf["socket"],
             }
 
+            conf[siteid]["connect_timeout"] = siteconf.get("timeout", 2)
+
             if "tcp" in proxy_params:
                 conf[siteid]["tcp"] = proxy_params["tcp"]
 

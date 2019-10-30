4202b32e08cda2869fa673dff105458ae8c88735
Lars Michelsen <lm@tribe29.com>
2019-10-30 09:39:29 +0100

    Try to fix random signing issues by directly invoking gpg instead of dpkg-sig
    Change-Id: I44527dc60db38474900736ee61ed6cf097c222c3

--- buildscripts/scripts/sign-packages.sh
+++ buildscripts/scripts/sign-packages.sh
@@ -48,10 +48,8 @@ if ls $TARGET/*.deb >/dev/null 2>&1; then
     echo "+ Sign DEB packages..."
     for DEB in $TARGET/*.deb; do
         echo "$GPG_PASSPHRASE" |
-            dpkg-sig -p \
-                -g '--batch --no-tty --passphrase-fd=0 --passphrase-repeat=0 --pinentry-mode loopback' \
-                --sign builder -k $KEY_ID \
-                "$DEB"
+            gpg --clearsign --default-key 434DAC48C4503261 --batch --no-tty --passphrase-fd=0 \
+                --passphrase-repeat=0 --pinentry-mode loopback "$DEB"
     done
 
     echo "Verify singed DEB packages..."

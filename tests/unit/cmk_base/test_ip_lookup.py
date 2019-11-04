b4928b21b7508bd2139e9052228bd6ac496adbd7
Lars Michelsen <lm@tribe29.com>
2019-11-04 08:01:12 +0100

    Improve "Update DNS cache" performance
    * The IPLookupCache is now managed by a special form of the
      DictCache which also cares about writing the persisted cache.
      This is not ideal, but seemed to be the least invasive way.
    * The cache updates the DNS cache for each individual lookup
      by default as it did it before.
    * But this object can now be set to "do not persist" mode to just
      capture the updates in memory during subsequent lookups like
      it's done in ip_lookup.update_dns_cache().
    * After the lookups are done, the save() method can be called
      once to perform a single bulk operation
    
    The operation is now a 100 times faster (tested with ~2000 hosts and a
    fast responding DNS server.
    
    Made some cleanups on the way and added several tests for
    the affected functions.
    
    Change-Id: Ib927cff7556ae07348e457973a1e0c6fa355a2ef

--- tests/unit/cmk_base/test_ip_lookup.py
+++ tests/unit/cmk_base/test_ip_lookup.py
@@ -1,6 +1,8 @@
+import socket
 import pytest  # type: ignore
-from testlib.base import Scenario
 from pathlib2 import Path
+
+from testlib.base import Scenario
 import cmk_base.ip_lookup as ip_lookup
 
 
@@ -45,7 +47,8 @@ def test_get_ip_lookup_cache_existing(_cache_file):
 
 def test_update_ip_lookup_cache_empty_file(_cache_file):
     cache_id = "host1", 4
-    ip_lookup._update_ip_lookup_cache(cache_id, "127.0.0.1")
+    ip_lookup_cache = ip_lookup._get_ip_lookup_cache()
+    ip_lookup_cache.update_cache(cache_id, "127.0.0.1")
 
     cache = ip_lookup._load_ip_lookup_cache(lock=False)
     assert cache[cache_id] == "127.0.0.1"
@@ -58,8 +61,9 @@ def test_update_ip_lookup_cache_extend_existing_file(_cache_file):
     cache_id1 = "host1", 4
     cache_id2 = "host2", 4
 
-    ip_lookup._update_ip_lookup_cache(cache_id1, "127.0.0.1")
-    ip_lookup._update_ip_lookup_cache(cache_id2, "127.0.0.2")
+    ip_lookup_cache = ip_lookup._get_ip_lookup_cache()
+    ip_lookup_cache.update_cache(cache_id1, "127.0.0.1")
+    ip_lookup_cache.update_cache(cache_id2, "127.0.0.2")
 
     cache = ip_lookup._load_ip_lookup_cache(lock=False)
     assert cache[cache_id1] == "127.0.0.1"
@@ -73,11 +77,23 @@ def test_update_ip_lookup_cache_update_existing_entry(_cache_file):
     with _cache_file.open(mode="w", encoding="utf-8") as f:
         f.write(u"%r" % {cache_id1: "1", cache_id2: "2"})
 
-    ip_lookup._update_ip_lookup_cache(cache_id1, "127.0.0.1")
+    ip_lookup_cache = ip_lookup._get_ip_lookup_cache()
+    ip_lookup_cache.update_cache(cache_id1, "127.0.0.1")
 
     cache = ip_lookup._load_ip_lookup_cache(lock=False)
     assert cache[cache_id1] == "127.0.0.1"
     assert cache[cache_id2] == "2"
+
+
+def test_ip_lookup_cache_update_without_persistence(_cache_file):
+    cache_id1 = "host1", 4
+
+    ip_lookup_cache = ip_lookup._get_ip_lookup_cache()
+    ip_lookup_cache.persist_on_update = False
+    ip_lookup_cache.update_cache(cache_id1, "127.0.0.1")
+
+    assert ip_lookup_cache[cache_id1] == "127.0.0.1"
+    assert not _cache_file.exists()
 
 
 def test_load_legacy_lookup_cache(_cache_file):
@@ -92,6 +108,32 @@ def test_load_legacy_lookup_cache(_cache_file):
     assert cache[cache_id2] == "127.0.0.2"
 
 
+def test_update_dns_cache(monkeypatch, _cache_file):
+    def _getaddrinfo(host, port, family=None, socktype=None, proto=None, flags=None):
+        # Needs to return [(family, type, proto, canonname, sockaddr)] but only
+        # caring about the address
+        return {
+            ("blub", socket.AF_INET): [(family, None, None, None, ("127.0.0.13", 1337))],
+            ("bla", socket.AF_INET): [(family, None, None, None, ("127.0.0.37", 1337))],
+            ("dual", socket.AF_INET): [(family, None, None, None, ("127.0.0.42", 1337))],
+        }[(host, family)]
+
+    monkeypatch.setattr(socket, "getaddrinfo", _getaddrinfo)
+
+    ts = Scenario()
+    ts.add_host("blub", tags={"criticality": "offline"})
+    ts.add_host("bla")
+    ts.add_host("dual", tags={"address_family": "ip-v4v6"})
+    ts.apply(monkeypatch)
+
+    assert ip_lookup.update_dns_cache() == (3, ["dual"])
+
+    # Check persisted data
+    cache = ip_lookup._load_ip_lookup_cache(lock=False)
+    assert cache[("blub", 4)] == "127.0.0.13"
+    assert ("dual", 6) not in cache
+
+
 def test_clear_ip_lookup_cache(_cache_file):
     with _cache_file.open(mode="w", encoding="utf-8") as f:
         f.write(u"%r" % {("host1", 4): "127.0.0.1"})
@@ -99,7 +141,7 @@ def test_clear_ip_lookup_cache(_cache_file):
     ip_lookup_cache = ip_lookup._get_ip_lookup_cache()
     assert ip_lookup_cache[("host1", 4)] == "127.0.0.1"
 
-    ip_lookup._clear_ip_lookup_cache()
+    ip_lookup._clear_ip_lookup_cache(ip_lookup_cache)
 
     assert len(ip_lookup_cache) == 0
     assert not _cache_file.exists()

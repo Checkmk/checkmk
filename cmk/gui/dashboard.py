09c0b44777776e56f52b6d022cdf8fe021e937b3
Lars Michelsen <lm@tribe29.com>
2019-11-04 11:02:52 +0100

    10381 FIX Fix editing dashlet views
    The general view options, painter, sorters and group painters could not be
    edited once a view was added to a dashboard.
    
    Change-Id: I6942a3d90771e431d4eb8d03db956c924cdc17d9

--- cmk/gui/dashboard.py
+++ cmk/gui/dashboard.py
@@ -1397,9 +1397,9 @@ def page_edit_dashlet():
     elif isinstance(params, (Dictionary, Transform)):
         vs_type = params
 
-    elif hasattr(params, '__call__'):
+    elif isinstance(params, tuple):
         # It's a tuple of functions which should be used to render and parse the params
-        render_input_func, handle_input_func = params()
+        render_input_func, handle_input_func = params
 
     if html.request.var('save') and html.transaction_valid():
         try:

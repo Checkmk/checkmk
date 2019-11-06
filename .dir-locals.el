48c1403bf8409f42d558fca89612f1aa424e2201
Sven Panne <sven.panne@tribe29.com>
2019-11-06 18:14:20 +0100

    The mypy runner has been moved.
    Change-Id: I77d2b96568fa7c82c992d35d524e496e1598a759

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ((c++-mode . ((flycheck-cppcheck-suppressions . ("passedByValue"))))
  (python-mode . ((eval setq flycheck-python-mypy-executable
                        (concat (projectile-locate-dominating-file default-directory dir-locals-file)
-                               "tests/static/run_mypy"))))
+                               "scripts/run-mypy"))))
  (scss-mode . ((eval setq flycheck-sass/scss-sass-lint-executable
                      (concat (projectile-locate-dominating-file default-directory dir-locals-file)
                              "node_modules/.bin/sass-lint"))))

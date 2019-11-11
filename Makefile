Checkmk/checkmk

commit b3501ea935768a37c3ce3254a53ba3210721ac2c

Revert "Cleanup pipenv dependency to external librrd"
This reverts commit 9cad81dd7f3712c4f518fb9b2ab2cfbbf24aa017.

Makefile
@@ -378,6 +378,7 @@ mrclean:
 	    --exclude="virtual-envs/*/.venv/"
 
 setup:
+# librrd-dev is still needed by the python rrd package we build in our virtual environment
 	sudo apt-get install \
 	    aptitude \
 	    autoconf \
@@ -391,6 +392,7 @@ setup:
 	    g++ \
 	    libclang-7-dev \
 	    libpcap-dev \
+		librrd-dev \
 	    llvm-7-dev \
 	    libsasl2-dev \
 	    libldap2-dev \

omd/.gitignore
@@ -24,7 +24,5 @@ openhardwaremonitor-[0-9].[0-9].[0-9]/
 patch-stamp
 python*-modules-1.[67].0i1.cme/
 re2-20[12][0-9]-[01][0-9]-[0-3][0-9]/
-rrdtool-[0-9].[0-9].[0-9]/
-.modulebuildrc
 rpm.buildroot
 rpm.topdir

scripts/run-pipenv
@@ -5,8 +5,6 @@
 set -e -o pipefail
 
 REPO_PATH="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" >/dev/null 2>&1 && pwd)"
-RRDTOOL_VERSION=1.7.1
-
 PYTHON_VERSION=$1
 shift
 
@@ -32,18 +30,10 @@ if [ -z "$SKIP_MAKEFILE_CALL" ]; then
     make --silent -C "$REPO_PATH/virtual-envs/$PYTHON_VERSION" .venv 1>&2
 fi
 
-if [ ! -e "$REPO_PATH/omd/rrdtool-$RRDTOOL_VERSION/src/.libs/librrd.so" ]; then
-    echo "librrd missing, building..." >&2
-    LANG=C make --silent -j6 -C "$REPO_PATH/omd" rrdtool-build-library >&2
-fi
-
 PIPENV_VENV_IN_PROJECT=true \
     PIPENV_NOSPIN=true \
     PIPENV_HIDE_EMOJIS=true \
     PIPENV_NO_INHERIT=true \
     PIPENV_PIPFILE="$REPO_PATH/virtual-envs/$PYTHON_VERSION/Pipfile" \
     PIPENV_IGNORE_VIRTUALENVS=1 \
-    CFLAGS="-I$REPO_PATH/omd/rrdtool-$RRDTOOL_VERSION/src/" \
-    LDFLAGS="-L$REPO_PATH/omd/rrdtool-$RRDTOOL_VERSION/src/.libs/" \
-    LD_LIBRARY_PATH="$REPO_PATH/omd/rrdtool-$RRDTOOL_VERSION/src/.libs:$LD_LIBRARY_PATH" \
     pipenv "$@"

commit 1e3fa532c366e7602af6f5218d1c9633c5199992

Add missing dependencies to `make setup`
This fixes the case where f12 would not work in `enterprise/core/src`
anymore.  This was likely broken by b3501ea9357.

Change-Id: I109e038e9ba8d67932d2a566e0b293fabd4b8a19

Makefile
@@ -392,7 +392,9 @@ setup:
 	    g++ \
 	    libclang-7-dev \
 	    libpcap-dev \
-		librrd-dev \
+	    librrd-dev \
+	    libxml2-dev \
+	    libpango1.0-dev \
 	    llvm-7-dev \
 	    libsasl2-dev \
 	    libldap2-dev \
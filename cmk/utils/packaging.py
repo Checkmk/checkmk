63fe038fe384ea51a41cd80ceb08b895d4aa15a8
Lars Michelsen <lm@tribe29.com>
2019-11-28 13:00:04 +0100

    10558 FIX Extension packages: Fixed performance issue
    The performance of the calculation of the packaged files has been
    improved to make the extension package management faster. Especially
    when managing a larger number of files, this change can improve the user
    experience of the package management.
    
    In addition to the performance fix made in 1.6, the overall performance
    of the WATO Extension packages module has been improved by changing some
    things in the underlying library layer.
    
    Change-Id: I38a646fe10b00a3fb96d8613b9d2a0cbcb9b9fb1

--- cmk/utils/packaging.py
+++ cmk/utils/packaging.py
@@ -455,7 +455,8 @@ def _files_in_dir(part, directory, prefix=""):
 
 
 def unpackaged_files_in_dir(part, directory):
-    return [f for f in _files_in_dir(part, directory) if f not in packaged_files_in_dir(part)]
+    packaged = set(packaged_files_in_dir(part))
+    return [f for f in _files_in_dir(part, directory) if f not in packaged]
 
 
 def packaged_files_in_dir(part):

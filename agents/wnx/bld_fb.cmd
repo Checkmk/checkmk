9d064bb0d14e238e04bb30e353473f556c0ab5f8
Sergey Kipnis <sergey.kipnis@tribe29.com>
2019-11-28 12:49:46 +0100

    Windows Frozen Binaries: small but important improvement - PYINSTALLER_CONFIG_DIR is moved to the 32-bit zone.
    Change-Id: I331ec0987f1a6dad4e9607373ffffb95e2e85eda

--- agents/wnx/bld_fb.cmd
+++ agents/wnx/bld_fb.cmd
@@ -8,6 +8,10 @@
 set p32path=C:\Python27.32
 
 if not exist %p32path% powershell Write-Host "You have to install 32 bit python in directory %p32path%" -Foreground Red && exit /B 1
+
+set PYINSTALLER_CONFIG_DIR=%p32path%\cache
+if not exist %PYINSTALLER_CONFIG_DIR% powershell Write-Host "Creating cache %PYINSTALLER_CONFIG_DIR%" -Foreground Green && mkdir %PYINSTALLER_CONFIG_DIR%
+
 if "%1" == "" powershell Write-Host "Usage: bld_fb python_file" -Foreground Yellow && exit /B 1
 if not exist "%1" powershell Write-Host "Script %1 not found" -Foreground Red && exit /B 1
 powershell Write-Host "Building Script %1 ..." -Foreground Green
@@ -16,7 +20,7 @@ set pupx=C:\ProgramData\chocolatey\bin
 
 @rem The build command itself
 @rem echo %ppath%\pyinstaller --clean --onefile --upx-dir="C:\ProgramData\chocolatey\bin" --log-level=INFO %2 %1
-%ppath%\pyinstaller --clean --onefile --upx-dir=%pupx% --distpath="build\fb\dist" --workpath="build\fb\work" --specpath="build\fb\spec" --log-level=ERROR %2 %1
+%ppath%\pyinstaller --clean --onefile --upx-dir=%pupx% --distpath="build\fb\dist" --workpath="build\fb\work" --specpath="build\fb\spec" --log-level=INFO %2 %1
 
 if not "%errorlevel%"  == "0" powershell Write-Host "Script %1 build is failed" -Foreground Red && exit /B 1
 powershell Write-Host "Script %1 build SUCCESS" -Foreground Green

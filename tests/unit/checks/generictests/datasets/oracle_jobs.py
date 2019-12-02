7909cd1125906a98e82786d2ee12c5ceda5d6e63
Grzegorz Gorski <Grzegorz.Gorski@opitz-consulting.com>
2019-12-02 15:05:30 +0000

    oracle_jobs: Bugfix for IndexError: list index out of range during Discovery
    Discovery check generated UNKNOWN state due to wrong handling of lines with
    less than three items.

---
+++ tests/unit/checks/generictests/datasets/oracle_jobs.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8
+# yapf: disable
+checkname = 'oracle_jobs'
+
+info = [
+    [
+        'ORCLCDB',
+        'Debug (121): ORA-01219: database or pluggable database not open: queries allowed on fixed tables or views only'
+    ]
+]
+
+discovery = {'': []}

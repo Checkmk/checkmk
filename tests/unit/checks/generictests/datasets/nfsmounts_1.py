2f45dff1f6eeb1229f4b3a4a812f04a42de4872d
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-12-06 15:28:23 +0100

    nfsmounts: extend tests
    Change-Id: I7e05348543626d0c41bd329ad6707bc7e132e931

--- tests/unit/checks/generictests/datasets/nfsmounts_1.py
+++ tests/unit/checks/generictests/datasets/nfsmounts_1.py
@@ -1,10 +1,38 @@
+# -*- encoding: utf-8
 # yapf: disable
 checkname = 'nfsmounts'
 
-info = [['/path/to/share', 'ok', '1611668', '794767', '712899', '32768']]
+info = [
+    ['/path/to/share1', 'hanging', '1611668', '794767', '712899', '32768'],
+    ['/path/to/share2', 'ok', '-', '-', '-', '-'],
+    ['/path/to/share3', 'drunk', '1611668', '794767', '712899', '32768'],
+    ['/path/to/share4', 'ok', '1611668', '794767', '712899', '32768']
+]
 
-parsed = {'/path/to/share': {'data': ['1611668', '794767', '712899', '32768'], 'state': 'ok'}}
+discovery = {
+    '': [
+        ('/path/to/share1', {}), ('/path/to/share2', {}),
+        ('/path/to/share3', {}), ('/path/to/share4', {})
+    ]
+}
 
-discovery = {'': [('/path/to/share', {})]}
-
-checks = {'': [('/path/to/share', {}, [(0, '55.8% used (27.43 GB of 49.18 GB)', [])])]}
+checks = {
+    '': [
+        ('/path/to/share1', {}, [(2, 'Server not responding', [])]),
+        ('/path/to/share2', {}, [(0, 'Mount seems OK', [])]),
+        ('/path/to/share3', {}, [(2, 'Unknown state', [])]),
+        (
+            '/path/to/share4', {}, [
+                (0, '55.8% used (27.43 GB of 49.18 GB)', []),
+            ],
+        ),
+        (
+            '/path/to/share4', {'has_perfdata': True}, [
+                (0, '55.8% used (27.43 GB of 49.18 GB)', [
+                    ('fs_size', 52811137024),
+                    ('fs_used', 29450862592),
+                ]),
+            ],
+        ),
+    ]
+}

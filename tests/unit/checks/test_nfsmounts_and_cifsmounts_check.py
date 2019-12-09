d150bc6e6f062ea33837aad35253879c1c95421d
Moritz Kiemer <moritz.kiemer@tribe29.com>
2019-12-09 13:06:50 +0100

    nfsmounts,cifsmounts: refactor unittest
    Change-Id: Idf8768a6384fccc9c2f0f1a43f911ee98d784800

--- tests/unit/checks/test_nfsmounts_and_cifsmounts_check.py
+++ tests/unit/checks/test_nfsmounts_and_cifsmounts_check.py
@@ -1,4 +1,5 @@
 import pytest  # type: ignore
+from collections import namedtuple
 from checktestlib import (
     BasicCheckResult,
     CheckResult,
@@ -14,6 +15,22 @@ from checktestlib import (
 
 pytestmark = pytest.mark.checks
 
+Size = namedtuple('Size', 'info,total,used,text')
+
+size1 = Size(
+    [u'491520', u'460182', u'460182', u'65536'],
+    491520 * 65536,
+    491520 * 65536 - 460182 * 65536,
+    "6.4% used (1.91 GB of 30.00 GB)",
+)
+
+size2 = Size(
+    [u'201326592', u'170803720', u'170803720', u'32768'],
+    None,  # not in use
+    None,  # not in use
+    "15.2% used (931.48 GB of 6.00 TB)",
+)
+
 
 @pytest.mark.parametrize(
     "info,discovery_expected,check_expected",
@@ -21,9 +38,8 @@ pytestmark = pytest.mark.checks
         (  # no info
             [], [], ()),
         (  # single mountpoint with data
-            [[u'/ABCshare', u'ok', u'491520', u'460182', u'460182', u'65536']
-            ], [('/ABCshare', {})], [
-                ('/ABCshare', {}, BasicCheckResult(0, "6.4% used (1.91 GB of 30.00 GB)", None)),
+            [[u'/ABCshare', u'ok'] + size1.info], [('/ABCshare', {})], [
+                ('/ABCshare', {}, BasicCheckResult(0, size1.text, None)),
             ]),
         (  # two mountpoints with empty data
             [[u'/AB', u'ok', u'-', u'-', u'-', u'-'], [u'/ABC', u'ok', u'-', u'-', u'-', u'-']], [
@@ -31,21 +47,21 @@ pytestmark = pytest.mark.checks
             ], [('/AB', {}, BasicCheckResult(0, "Mount seems OK", None)),
                 ('/ABC', {}, BasicCheckResult(0, "Mount seems OK", None))]),
         (  # Mountpoint with spaces and permission denied
-            [[u'/var/dba', u'export', u'Permission', u'denied'],
-             [u'/var/dbaexport', u'ok', u'201326592', u'170803720', u'170803720', u'32768']], [
-                 ('/var/dbaexport', {}), ('/var/dba export', {})
-             ], [('/var/dba export', {}, BasicCheckResult(2, 'Permission denied', None)),
-                 ('/var/dbaexport', {},
-                  BasicCheckResult(0, '15.2% used (931.48 GB of 6.00 TB)', None))]),
+            [[u'/var/dba', u'export', u'Permission',
+              u'denied'], [u'/var/dbaexport', u'ok'] + size2.info], [
+                  ('/var/dbaexport', {}), ('/var/dba export', {})
+              ], [('/var/dba export', {}, BasicCheckResult(2, 'Permission denied', None)),
+                  ('/var/dbaexport', {}, BasicCheckResult(0, size2.text, None))]),
         (  # with perfdata
-            [[u'/PERFshare', u'ok', u'491520', u'460182', u'460182', u'65536']
-            ], [('/PERFshare', {})], [('/PERFshare', {
-                'has_perfdata': True
-            },
-                                       BasicCheckResult(0, "6.4% used (1.91 GB of 30.00 GB)", [
-                                           PerfValue('fs_size', 491520 * 65536),
-                                           PerfValue('fs_used', 491520 * 65536 - 460182 * 65536)
-                                       ]))]),
+            [[u'/PERFshare', u'ok'] + size1.info], [('/PERFshare', {})], [
+                ('/PERFshare', {
+                    'has_perfdata': True
+                },
+                 BasicCheckResult(
+                     0, size1.text,
+                     [PerfValue('fs_size', size1.total),
+                      PerfValue('fs_used', size1.used)]))
+            ]),
         (  # state == 'hanging'
             [[u'/test', u'hanging', u'hanging', u'0', u'0', u'0', u'0']
             ], [('/test hanging', {})], [('/test hanging', {

ed75347dc01131e61851ae4c4f639e27ab830711
Mathias Götze <Mathias.Goetze@iteratio.com>
2019-12-09 13:19:01 +0100

    Added new QNAP checks

---
+++ tests/unit/checks/generictests/datasets/qnap_info.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+checkname = 'QNAP Info'
+
+info = [[u'Linux TS-659 4.2.6', u'NBUCA0700170C', u'440376848']]
+
+discovery = {'': [(None, {})]}
+
+checks = {
+    '': [
+        (
+            None, {}, [
+                (
+                    0, 'Linux TS-659 4.2.6 -- Serial Nr.: NBUCA0700170C -- Uptime: 51 d'
+                ),
+                ]
+        )
+        ]
+}

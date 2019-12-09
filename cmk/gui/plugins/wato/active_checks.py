eea0198176753c5fb65b5272a22138d05b4dff86
Marcel Caspar <marcel.caspar@tribe29.com>
2019-12-09 15:33:23 +0100

    Bugfix: fix unnecessary html escaping in wato active check tooltip
    The help text of the active check 'Classical active and passive Monitoring checks'
    contained html escaped quotation marks, which where shown that way to the user.
    
    Change-Id: I390be076423cfa26aa81fdf33709c00b82f84d98

--- cmk/gui/plugins/wato/active_checks.py
+++ cmk/gui/plugins/wato/active_checks.py
@@ -1569,7 +1569,7 @@ rulespec_registry.register(
 def _valuespec_custom_checks():
     return Dictionary(
         title=_("Classical active and passive Monitoring checks"),
-        help=_("With this ruleset you can configure &quot;classical Monitoring checks&quot; "
+        help=_("With this ruleset you can configure \"classical Monitoring checks\" "
                "to be executed directly on your monitoring server. These checks "
                "will not use Check_MK. It is also possible to configure passive "
                "checks that are fed with data from external sources via the "

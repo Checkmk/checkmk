dd10b41cac57824db179a7b4a3304faa72c6060e
Lars Michelsen <lm@tribe29.com>
2019-12-12 14:25:16 +0100

    Another pylint command fix
    Change-Id: Ibd35df1f969c96161d8309bfdec0bd58280a0d38

--- tests/pylint/test_pylint.py
+++ tests/pylint/test_pylint.py
@@ -43,7 +43,8 @@ def test_pylint(pylint_test_dir):
 
 def _get_files_to_check(pylint_test_dir):
     p = subprocess.Popen(
-        ["%s/scripts/find-python-files" % repo_path(), sys.version_info[0]],
+        ["%s/scripts/find-python-files" % repo_path(),
+         str(sys.version_info[0])],
         stdout=subprocess.PIPE,
         encoding="utf-8",
         shell=False,

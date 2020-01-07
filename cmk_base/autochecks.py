e1b9d4a13801dae3c39b5f9e5cb550209bd0e6b2
Andreas Boesl <andreas.boesl@tribe29.com>
2020-01-07 16:03:23 +0100

    10519 FIX Autochecks files of cluster nodes will no longer contain duplicates from other nodes
    Due to an error, previous versions could save duplicate service entries for nodes in a cluster.
    The autochecks file for these cluster nodes could grow on every service discovery.
    
    As a side effect of this bug all services of NodeA were also incorrectly discovered on NodeB.
    
    Change-Id: I70a3a775fe7ab2c78ac797b83664cf38dd420bb1

--- cmk_base/autochecks.py
+++ cmk_base/autochecks.py
@@ -25,7 +25,7 @@
 # Boston, MA 02110-1301 USA.
 """Caring about persistance of the discovered services (aka autochecks)"""
 
-from typing import Iterator, Any, Dict, Union, Tuple, Text, Optional, List  # pylint: disable=unused-import
+from typing import Iterator, Any, Dict, Union, Set, Tuple, Text, Optional, List  # pylint: disable=unused-import
 import os
 import sys
 import ast
@@ -397,8 +397,8 @@ def _set_autochecks_of_cluster(host_config, new_items):
 
     config_cache = config.get_config_cache()
 
-    new_autochecks = []  # type: List[DiscoveredService]
     for node in host_config.nodes:
+        new_autochecks = []  # type: List[DiscoveredService]
         for existing_service in parse_autochecks_file(node):
             if host_config.hostname != config_cache.host_of_clustered_service(
                     node, existing_service.description):
@@ -407,6 +407,8 @@ def _set_autochecks_of_cluster(host_config, new_items):
         for discovered_service in new_items:
             new_autochecks.append(discovered_service)
 
+        new_autochecks = _remove_duplicate_autochecks(new_autochecks)
+
         # write new autochecks file for that host
         save_autochecks_file(node, new_autochecks)
 
@@ -414,6 +416,19 @@ def _set_autochecks_of_cluster(host_config, new_items):
     # Remove them. The autochecks are only stored in the nodes autochecks files
     # these days.
     remove_autochecks_file(host_config.hostname)
+
+
+def _remove_duplicate_autochecks(autochecks):
+    # type: (List[DiscoveredService]) -> List[DiscoveredService]
+    """ Cleanup routine. Earlier versions (<1.6.0p8) may have introduced duplicates in the autochecks file"""
+    duplicates = set()  # type: Set[Tuple[CheckPluginName, Item]]
+    cleaned_autochecks = []
+    for service in autochecks:
+        service_id = (service.check_plugin_name, service.item)
+        if service_id not in duplicates:
+            duplicates.add(service_id)
+            cleaned_autochecks.append(service)
+    return cleaned_autochecks
 
 
 def has_autochecks(hostname):

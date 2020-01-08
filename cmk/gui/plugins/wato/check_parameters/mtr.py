994402ae88e10a654f0893725c4e4e39baf4c556
Simon Jess <simon.jess@tribe29.com>
2020-01-08 08:30:30 +0100

    10669 FIX mtr: Fixed applying levels configured in WATO which had no effect
    Change-Id: I1503827d0c24dc8ff517ac766a4797574f1551b1

--- cmk/gui/plugins/wato/check_parameters/mtr.py
+++ cmk/gui/plugins/wato/check_parameters/mtr.py
@@ -30,6 +30,7 @@ from cmk.gui.valuespec import (
     Tuple,
     Integer,
     TextAscii,
+    Transform,
 )
 
 from cmk.gui.plugins.wato import (
@@ -45,48 +46,69 @@ def _item_spec_mtr():
                      allow_empty=False)
 
 
+def _transform_mtr_params(p):
+    if "avg" in p:
+        p["rta"] = p.pop("avg")
+    if "stddev" in p:
+        p["rtstddev"] = p.pop("stddev")
+    if "loss" in p:
+        p["pl"] = p.pop("loss")
+    return p
+
+
 def _parameter_valuespec_mtr():
-    return Dictionary(
-        help=_(
-            "This ruleset can be used to change MTR's (Matt's traceroute) warning and crit levels for packet loss, average "
-            "roundtrip and standard deviation."),
-        elements=[
-            ("avg",
-             Tuple(
-                 title=_("Average roundtrip time in ms"),
-                 elements=[
-                     Integer(title=_("Warning at"), default_value=150, unit=_("ms"), minvalue=0),
-                     Integer(title=_("Critical at"), default_value=250, unit=_("ms"), minvalue=0),
-                 ],
-                 help=_(
-                     "The maximum average roundtrip time in ms before this service goes into warning/critical. "
-                     "This alarm only applies to the target host, not the hops in between."),
-             )),
-            ("stddev",
-             Tuple(
-                 title=_("Standard deviation of roundtrip times in ms"),
-                 elements=[
-                     Integer(title=_("Warning at"), default_value=150, unit=_("ms"), minvalue=0),
-                     Integer(title=_("Critical at"), default_value=250, unit=_("ms"), minvalue=0),
-                 ],
-                 help=
-                 _("The maximum standard deviation on the roundtrip time in ms before this service goes into"
-                   "warning/critical. This alarm only applies to the target host, not the hops in between."
-                  ),
-             )),
-            ("loss",
-             Tuple(
-                 title=_("Packet loss in percentage"),
-                 elements=[
-                     Integer(title=_("Warning at"), default_value=10, unit=_("%"), minvalue=0),
-                     Integer(title=_("Critical at"), default_value=25, unit=_("%"), minvalue=0),
-                 ],
-                 help=_(
-                     "The maximum allowed percentage of packet loss to the destination before this service "
-                     "goes into warning/critical."),
-             )),
-        ],
-        optional_keys=False,
+    return Transform(
+        Dictionary(
+            help=_(
+                "This ruleset can be used to change MTR's (Matt's traceroute) warning and crit levels for packet loss, average "
+                "roundtrip and standard deviation."),
+            elements=[
+                ("rta",
+                 Tuple(
+                     title=_("Average roundtrip time in ms"),
+                     elements=[
+                         Integer(title=_("Warning at"), default_value=150, unit=_("ms"),
+                                 minvalue=0),
+                         Integer(title=_("Critical at"),
+                                 default_value=250,
+                                 unit=_("ms"),
+                                 minvalue=0),
+                     ],
+                     help=_(
+                         "The maximum average roundtrip time in ms before this service goes into warning/critical. "
+                         "This alarm only applies to the target host, not the hops in between."),
+                 )),
+                ("rtstddev",
+                 Tuple(
+                     title=_("Standard deviation of roundtrip times in ms"),
+                     elements=[
+                         Integer(title=_("Warning at"), default_value=150, unit=_("ms"),
+                                 minvalue=0),
+                         Integer(title=_("Critical at"),
+                                 default_value=250,
+                                 unit=_("ms"),
+                                 minvalue=0),
+                     ],
+                     help=
+                     _("The maximum standard deviation on the roundtrip time in ms before this service goes into"
+                       "warning/critical. This alarm only applies to the target host, not the hops in between."
+                      ),
+                 )),
+                ("pl",
+                 Tuple(
+                     title=_("Packet loss in percentage"),
+                     elements=[
+                         Integer(title=_("Warning at"), default_value=10, unit=_("%"), minvalue=0),
+                         Integer(title=_("Critical at"), default_value=25, unit=_("%"), minvalue=0),
+                     ],
+                     help=_(
+                         "The maximum allowed percentage of packet loss to the destination before this service "
+                         "goes into warning/critical."),
+                 )),
+            ],
+            optional_keys=False,
+        ),
+        forth=_transform_mtr_params,
     )
 
 

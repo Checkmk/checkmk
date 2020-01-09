2a4c7122e0c55207e9ce3f336c014caca5e7e87a
Alex Zurhake <alex.zurhake@tribe29.com>
2020-01-09 13:03:51 +0100

    Select docker image do build in
    - use groovy library to select docker tag
    - default dockertag is branch-latest
    - default tag can be overwritten by folder variable
    - default tag and folder variable can be overwritten by build property
    
    Change-Id: I3b0da6b5731956d61e1a4f74cdb237bbc073ad85

--- buildscripts/scripts/lib/versioning.groovy
+++ buildscripts/scripts/lib/versioning.groovy
@@ -1,4 +1,4 @@
-// library for calculation fo v3ersion numbers
+// library for calculation of version numbers
 import java.text.SimpleDateFormat
 
 def get_branch(scm) {
@@ -22,6 +22,22 @@ def get_docker_tag(scm) {
     def DATE = get_date()
     def HASH = get_git_hash()
     return BRANCH + '-' + DATE + '-' + HASH
+}
+
+def select_docker_tag(BRANCH, BUILD_TAG, FOLDER_TAG) {
+    // Empty folder prperties are null pointers
+    // Other emput string variables have the value ''
+    if (FOLDERTAG != null) {
+        return FOLDER_TAG
+    }
+    if (BUILD_TAG != '') {
+        return BUILD_TAG
+    }
+    return BRANCH + '-latest'
+}
+
+def print_image_tag() {
+    sh "cat /version.txt"
 }
 
 return this

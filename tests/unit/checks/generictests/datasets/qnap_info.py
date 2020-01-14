8f32c7191fef62d6d386e5156b2c169899ec450f
Mathias Götze <Mathias.Goetze@iteratio.com>
2020-01-14 13:28:01 +0100

    Added QNAP devices

---
+++ tests/unit/checks/generictests/datasets/qnap_info.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+checkname = 'qnap_info'
+
+info = [[u'Linux TS-659 4.2.6', u'NBUCA0700170C', u'440376848']]
+
+discovery = {'': [(None, {})]}
+
+checks = {
+    '': [
+        (
+            None, {}, [
+                (0, 'Linux TS-659 4.2.6 -- Serial Nr.: NBUCA0700170C -- Uptime: 51 d'),
+                ]
+        )
+        ]
+}

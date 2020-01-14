e8f35d79039bd7ff8313b8d1aeb26710cf9cb911
Frans Fürst <frans.fuerst@tribe29.com>
2020-01-14 15:43:49 +0100

    solaris_mem: add initial test
    Change-Id: I3552d9c479fef5d4b980710a3116763b81c178be

---
+++ tests/unit/checks/generictests/datasets/solaris_mem.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8
+# yapf: disable
+checkname = 'solaris_mem'
+
+info = [
+    [
+        u'Memory:', u'128G', u'phys', u'mem,', u'42G', u'free', u'mem,',
+        u'19G', u'total', u'swap,', u'19G', u'free', u'swap'
+    ]
+]
+
+discovery = {'': [(None, {})]}
+
+checks = {
+    '': [
+        (
+            None, {
+                'levels': (150.0, 200.0)
+            }, [
+                (
+                    0,
+                    '86.00 GB used (this is 67.2% of 128.00 GB RAM + 19.00 GB SWAP)',
+                    [
+                        ('swapused', 0.0, None, None, 0, 19456.0),
+                        ('ramused', 88064.0, None, None, 0, 131072.0),
+                        ('memused', 88064.0, 196608.0, 262144.0, 0, 150528.0)
+                    ]
+                )
+            ]
+        )
+    ]
+}

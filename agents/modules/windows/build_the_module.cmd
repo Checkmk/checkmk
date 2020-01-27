4bccd27fe0de7a1f3567311db75d126c55cb55a2
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-01-27 13:41:46 +0100

    [CMK-3810] - jenkins integration #1
    Change-Id: I1125438649c5b67273222b235c12307f296b7478

---
+++ agents/modules/windows/build_the_module.cmd
@@ -0,0 +1,29 @@
+@rem Main buidling script for Python
+
+@echo off
+@if "%1"=="python" goto python_build 
+@if "%1"=="success" goto success_build 
+@if "%1"=="fail" goto fail_build 
+goto usage
+
+:success_build
+powershell Write-Host 'Simulating success build...' -foreground Cyan
+@powershell Write-Host 'Warning: This is success build to test jenkins pipeline' -foreground Yellow
+@powershell Write-Host 'Build Success' -foreground Green
+exit /b 0
+
+:fail_build
+powershell Write-Host 'Simulating fail build...' -foreground Cyan
+@powershell Write-Host 'Warning: This is fail build to test jenkins pipeline' -foreground Yellow
+@powershell Write-Host 'Build Fail' -foreground Red
+exit /b 11
+
+:python_build
+powershell Write-Host 'Building pyhon...' -foreground Cyan
+make 
+goto exit
+
+:usage:
+@if "%1"==""  powershell Write-Host "Possible parameters:`n `tpython `t- builds python module`n`tsuccess`t- always return success`n`tfail `t- always returns fail" -foreground Cyan && exit /b 9
+
+:exit

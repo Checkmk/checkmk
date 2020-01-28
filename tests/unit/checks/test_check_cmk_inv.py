5b7acdc708730af6b6ba3fd76921df1c988896a4
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-01-28 12:42:52 +0100

    check_cmk_inv: move arguments into function
    Change-Id: I3a13cbc8db2e6ae99efca17ee05661488e2bff9c

--- tests/unit/checks/test_check_cmk_inv.py
+++ tests/unit/checks/test_check_cmk_inv.py
@@ -2,12 +2,15 @@ import pytest
 
 pytestmark = pytest.mark.checks
 
+STATIC_ARGS = ["--cache", "--inventory-as-check", "$HOSTNAME$"]
+
 
 @pytest.mark.parametrize("params,expected_args", [
-    ({}, ["--inv-fail-status=1", "--hw-changes=0", "--sw-changes=0", "--sw-missing=0"]),
+    ({},
+     ["--inv-fail-status=1", "--hw-changes=0", "--sw-changes=0", "--sw-missing=0"] + STATIC_ARGS),
     ({
         "timeout": 0
-    }, ["--inv-fail-status=1", "--hw-changes=0", "--sw-changes=0", "--sw-missing=0"]),
+    }, ["--inv-fail-status=1", "--hw-changes=0", "--sw-changes=0", "--sw-missing=0"] + STATIC_ARGS),
 ])
 def test_check_cmk_inv_argument_parsing(check_manager, params, expected_args):
     """Tests if all required arguments are present."""

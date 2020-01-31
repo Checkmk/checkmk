e3192296b6a9cc69a7252b3c1d332fc716e250f8
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2020-01-31 13:29:05 +0100

    10744 FIX mssql_blocked_sessions: don't crash in clustered setup
    In a clustered setup the mssql_blocked_sessions check plugin crashed
    if an instance is moved from one node to another and one node reported
    blocked sessions while the other node did not. This is fixed.
    
    Change-Id: I1f89f3415183447172b14f8fa60c399752765535

---
+++ tests/unit/checks/generictests/datasets/mssql_blocked_sessions_clustered.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'mssql_blocked_sessions'
+
+
+info = [
+    ['ID 1', 'No blocking sessions'],
+    ['ID 1', '1', '232292187', 'Foo', '2'],
+]
+
+
+discovery = {'': [('ID 1', {})]}
+
+
+checks = {'': [(
+    'ID 1',
+    {'state': 2},
+    [
+        (2, 'Summary: 1 blocked by 1 ID(s)', []),
+        (0, '\nSession 1 blocked by 2 (Type: Foo, Wait: 2.7 d)', [])
+    ]
+)]}

750adebc80b5d7615025f3be6803b6c014ea9867
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-02-11 08:51:40 +0100

    [CMK-3724] - multi-line support for windows tasks plugin
    Change-Id: If98df7b64313fdc0e2660a38bdb348ec81eab8da

--- agents/windows/plugins/windows_tasks.ps1
+++ agents/windows/plugins/windows_tasks.ps1
@@ -4,8 +4,26 @@
 
 Write-Host "<<<windows_tasks:sep(58):encoding(cp437)>>>"
 $lang = Get-UICulture | select -expand LCID
+
+# "..",".\n..","aaa"\n
+# We assume that correct newline can be placed only after "
+# Processing
+# \r\n   -> 'Z_Z'
+# "      -> 'o_o'
+# o_oZ_Z -> '\"\r\n' # the only valid new line symbols
+# o_o    -> '"'
+# Z_Z    -> ''
+
+# encoding "\n and "
+$raw = (schtasks /query /fo csv -v | out-string) -replace '\r\n', 'Z_Z'
+$l = $raw -replace '\"', 'o_o'
+# decoding
+$d = $l -replace 'o_oZ_Z', "`"`r`n"
+$d = $d -replace 'o_o', '"'
+$d = $d -replace 'Z_Z', ''
+$tasks = $d | ConvertFrom-Csv
+
 if ($lang -eq 1031){
-	$tasks = schtasks /query /fo csv -v | ConvertFrom-Csv
 	foreach ($task in $tasks){
 		if (($task.HostName -match "^$($Env:Computername)$") -and ($task.AufgabenName -notlike '\Microsoft*') -and ($task.AufgabenName -notlike '*zachteHRM*')){
 			Write-Host "TaskName `t: "$task.AufgabenName
@@ -21,5 +39,5 @@ if ($lang -eq 1031){
 		}
 	}
 } elseif ($lang -eq 1033){
-	schtasks /query /fo csv -v | ConvertFrom-Csv | ? {$_.HostName -match "^$($Env:Computername)$" -and $_.TaskName -notlike '\Microsoft*' -and $_.TaskName -notlike '*zachteHRM*'} | fl taskname,"last run time","next run time","last result","scheduled task state"
+	$tasks | ? {$_.HostName -match "^$($Env:Computername)$" -and $_.TaskName -notlike '\Microsoft*' -and $_.TaskName -notlike '*zachteHRM*'} | fl taskname,"last run time","next run time","last result","scheduled task state"
 }

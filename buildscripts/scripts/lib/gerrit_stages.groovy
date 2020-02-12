98b0e87219a818de5eacfc4ef713a215d4168b8b
Alex Zurhake <alex.zurhake@tribe29.com>
2020-02-12 14:47:28 +0100

    Add stage status table to build descrtion
    Change-Id: I843c56138d4a27dd7a6ba9d9835bb05986d5a575

--- buildscripts/scripts/lib/gerrit_stages.groovy
+++ buildscripts/scripts/lib/gerrit_stages.groovy
@@ -25,13 +25,54 @@ def run(Map args) {
         if (args.CONDITION) {
             dir(args.DIR) {
                 withEnv(args.ENV_VARS) {
-                    sh(". /bauwelt/bin/bw-setup-jenkins-env ; " + args.COMMAND)
+                    STATUS = sh(script: ". /bauwelt/bin/bw-setup-jenkins-env ; " + args.COMMAND, returnStatus: true)
+                    desc_add_status_row(args.NAME, STATUS)
+                    sh('exit ' + STATUS)
                 }
             }
         } else {
             println(args.TEXT_ON_SKIP)
+            desc_add_status_row(args.NAME, 'skipped')
         }
     }
+}
+
+
+// Functions to add status of stages in from of tables
+def desc_init() {
+    currentBuild.description = ""
+}
+def desc_add_line(TEXT) {
+    currentBuild.description += '<p>' + TEXT + '</p>'
+}
+def desc_add_table_head() {
+    currentBuild.description += '<table>'
+}
+def desc_add_table_bottom() {
+    currentBuild.description += '</table>'
+}
+def desc_add_table() {
+    desc_add_table_head()
+    desc_add_table_bottom()
+}
+def desc_rm_table_bottom() {
+    currentBuild.description -= '</table>'
+}
+def desc_add_row(ITEM_1, ITEM_2) {
+    desc_rm_table_bottom()
+    currentBuild.description += '<tr><td>' + ITEM_1 + '</td><td>' + ITEM_2 + '</td></tr>'
+    desc_add_table_bottom()
+}
+def desc_add_status_row(STAGE, STATUS) {
+    desc_rm_table_bottom()
+    if (STATUS == 0) {
+        currentBuild.description += '<tr><td>' + STAGE + '</td><td style="color: green;">success</td></tr>'
+    } else if (STATUS == 'skipped') {
+        currentBuild.description += '<tr><td>' + STAGE + '</td><td style="color: grey;">skipped</td></tr>'
+    } else {
+        currentBuild.description += '<tr><td>' + STAGE + '</td><td style="color: red;">failed</td></tr>'
+    }
+    desc_add_table_bottom()
 }
 
 return this

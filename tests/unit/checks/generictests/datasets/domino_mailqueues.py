078bdae5741f5d98eeb47be921aceab501a3c7f4
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-02-14 10:47:55 +0100

    domino_mailqueues: remove use of OID_STRING
    Change-Id: Iabffdad60ae76cf73c3ad151624cacc8e19c8e18

--- tests/unit/checks/generictests/datasets/domino_mailqueues.py
+++ tests/unit/checks/generictests/datasets/domino_mailqueues.py
@@ -5,11 +5,7 @@ checkname = "domino_mailqueues"
 
 
 info = [
-    [[u'.1.3.6.1.4.1.334.72.1.1.4.1.0', u'1']],
-    [[u'.1.3.6.1.4.1.334.72.1.1.4.6.0', u'4711']],
-    [[u'.1.3.6.1.4.1.334.72.1.1.4.21.0', u'815']],
-    [[u'.1.3.6.1.4.1.334.72.1.1.4.31.0', u'1']],
-    [[u'.1.3.6.1.4.1.334.72.1.1.4.34.0', u'12']],
+    [u'1', u'4711', u'815', u'1', u'12'],
 ]
 
 
@@ -27,27 +23,27 @@ discovery = {
 checks = {
     '': [
         ("lnDeadMail", {'queue_length': (300, 350)}, [
-            (0, "1 Dead Mails", [
+            (0, "Dead mails: 1", [
                 ('mails', 1, 300, 350, None, None),
             ]),
         ]),
         ("lnWaitingMail", {'queue_length': (300, 350)}, [
-            (2, "4711 Waiting Mails (Warn/Crit at 300/350)", [
+            (2, "Waiting mails: 4711 (warn/crit at 300/350)", [
                 ('mails', 4711, 300, 350, None, None),
             ]),
         ]),
         ("lnMailHold", {'queue_length': (300, 350)}, [
-            (2, "815 Mails on Hold (Warn/Crit at 300/350)", [
+            (2, "Mails on hold: 815 (warn/crit at 300/350)", [
                 ('mails', 815, 300, 350, None, None),
             ]),
         ]),
         ("lnMailTotalPending", {'queue_length': (300, 350)}, [
-            (0, "1 Total Pending Mails", [
+            (0, "Total pending mails: 1", [
                 ('mails', 1, 300, 350, None, None),
             ]),
         ]),
         ("InMailWaitingforDNS", {'queue_length': (300, 350)}, [
-            (0, "12 Mails waiting for DNS", [
+            (0, "Mails waiting for DNS: 12", [
                 ('mails', 12, 300, 350, None, None),
             ])
         ]),

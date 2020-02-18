1ec38de5680e17e8e35bbab519b50de32b1c0207
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-02-18 11:37:29 +0100

    [CMK-3301] - current time is always correct now
    - extended unit tests
    
    Change-Id: I5d036f88c4be61626516e9bc420376fbe0c42a5f

--- agents/wnx/src/engine/providers/ps.cpp
+++ agents/wnx/src/engine/providers/ps.cpp
@@ -180,13 +180,13 @@ time_t ConvertWmiTimeToHumanTime(const std::string &creation_date) noexcept {
     auto creation_tm = *std::localtime(&current_time);
 
     // fill variable fields data
-    creation_tm.tm_year = std::strtoul(year.c_str(), nullptr, 0) - 1900;
-    creation_tm.tm_mon = std::strtoul(month.c_str(), nullptr, 0) - 1;
-    creation_tm.tm_mday = std::strtoul(day.c_str(), nullptr, 0);
+    creation_tm.tm_year = std::strtoul(year.c_str(), nullptr, 10) - 1900;
+    creation_tm.tm_mon = std::strtoul(month.c_str(), nullptr, 10) - 1;
+    creation_tm.tm_mday = std::strtoul(day.c_str(), nullptr, 10);
 
-    creation_tm.tm_hour = std::strtoul(hour.c_str(), nullptr, 0);
-    creation_tm.tm_min = std::strtoul(minutes.c_str(), nullptr, 0);
-    creation_tm.tm_sec = std::strtoul(seconds.c_str(), nullptr, 0);
+    creation_tm.tm_hour = std::strtoul(hour.c_str(), nullptr, 10);
+    creation_tm.tm_min = std::strtoul(minutes.c_str(), nullptr, 10);
+    creation_tm.tm_sec = std::strtoul(seconds.c_str(), nullptr, 10);
     creation_tm.tm_isdst = -1;  // we do not know DST, so we will ask system
 
     // calculate with possible correction of not-so-important fields

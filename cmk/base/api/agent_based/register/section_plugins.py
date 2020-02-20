430d9f6acda03ee76bb13e67544582489f57c717
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-02-20 11:49:45 +0100

    API: introduce validation and factory for agent section plugins
    Change-Id: I54c6899e52c1d6fcc25c224f387632244a6faa5d

---
+++ cmk/base/api/agent_based/register/section_plugins.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""Background tools required to register a section plugin
+"""
+from typing import (  # pylint: disable=unused-import
+    Any, Generator, List, Optional, Union)
+import inspect
+
+from cmk.base.discovered_labels import HostLabel
+from cmk.base.api import PluginName
+from cmk.base.api.agent_based.section_types import (
+    AgentParseFunction,
+    AgentSectionPlugin,
+    HostLabelFunction,
+)
+
+
+def _validate_parse_function(parse_function):
+    # type: (AgentParseFunction) -> None
+    if not inspect.isfunction(parse_function):
+        raise TypeError("parse function must be a function: %r" % (parse_function,))
+
+    if inspect.isgeneratorfunction(parse_function):
+        raise TypeError("parse function must not be a generator function: %r" % (parse_function,))
+
+    parameters = inspect.signature(parse_function).parameters
+    if list(parameters) != ['string_table']:
+        raise ValueError("parse function must accept exactly one argument 'string_table'")
+
+
+def _validate_host_label_function(host_label_function):
+    # type: (HostLabelFunction) -> None
+    if not inspect.isgeneratorfunction(host_label_function):
+        raise TypeError("host label function must be a generator function: %r" %
+                        (host_label_function,))
+
+    parameters = inspect.signature(host_label_function).parameters
+    if list(parameters) not in (['section'], ['_section']):
+        raise ValueError("host label function must accept exactly one argument 'section'")
+
+
+def _validate_supercedings(supercedes):
+    # type: (List[PluginName]) -> None
+    if not len(supercedes) == len(set(supercedes)):
+        raise ValueError("duplicate supercedes entry")
+
+
+def _noop_host_label_function(section):  # pylint: disable=unused-argument
+    # type: (Any) -> Generator[HostLabel, None, None]
+    return
+    yield  # pylint: disable=unreachable
+
+
+def _create_host_label_function(host_label_function):
+    # type: (Optional[HostLabelFunction]) -> HostLabelFunction
+    if host_label_function is None:
+        return _noop_host_label_function
+
+    _validate_host_label_function(host_label_function)
+    return host_label_function
+
+
+def _create_supercedes(supercedes):
+    # type: (Optional[List[str]]) -> List[PluginName]
+    if supercedes is None:
+        return []
+
+    supercedes_plugins = [PluginName(n) for n in supercedes]
+    _validate_supercedings(supercedes_plugins)
+
+    return sorted(supercedes_plugins)
+
+
+def create_agent_section_plugin(
+    *,
+    name,
+    parsed_section_name=None,
+    parse_function,
+    host_label_function=None,
+    supercedes=None,
+    forbidden_names,
+):
+    # type: (str, Optional[str], AgentParseFunction, Optional[HostLabelFunction], Optional[List[str]], List[PluginName]) -> AgentSectionPlugin
+    """Return an AgentSectionPlugin object after validating and converting the arguments one by one"""
+
+    plugin_name = PluginName(name, forbidden_names=forbidden_names)
+
+    _validate_parse_function(parse_function)
+
+    return AgentSectionPlugin(
+        plugin_name,
+        PluginName(parsed_section_name) if parsed_section_name else plugin_name,
+        parse_function,
+        _create_host_label_function(host_label_function),
+        _create_supercedes(supercedes),
+    )

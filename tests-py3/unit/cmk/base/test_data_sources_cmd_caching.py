1a0c06f182eaf8c2dd6e36420fc8137d612565f1
Kenneth Okoh <kenneth.okoh@tribe29.com>
2020-03-05 12:03:31 +0100

    New license headers: tests-py3/ (CMK-1073)
    Change-Id: I8aa0e6bcbd6be9ec7f9c5323c80d9a61f0e3f0c7

--- tests-py3/unit/cmk/base/test_data_sources_cmd_caching.py
+++ tests-py3/unit/cmk/base/test_data_sources_cmd_caching.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 # pylint: disable=redefined-outer-name
 # These tests verify the behaviour of the Check_MK base methods
 # that do the actual checking/discovery/inventory work. Especially

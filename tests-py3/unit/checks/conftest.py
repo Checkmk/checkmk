1481fa5af05e1aee2d9a3434d151cd1e8da82c5e
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-03-06 09:12:33 +0100

    testing: remove tests/unit/checks
    Change-Id: Iebd75887408046afdb25b9e138d70a50a2b5a329

--- tests-py3/unit/checks/conftest.py
+++ tests-py3/unit/checks/conftest.py
@@ -2,10 +2,6 @@ import pytest  # type: ignore[import]
 import testlib  # type: ignore[import]
 from testlib.base import Scenario  # type: ignore[import]
 
-# TODO Python 3: Copied conftest from orig conftest file (Python 2)
-# If new fixtures are implemented, don't forget to port them to
-#   tests/unit/checks/conftest.py
-
 
 @pytest.fixture(scope="module")
 def check_manager():

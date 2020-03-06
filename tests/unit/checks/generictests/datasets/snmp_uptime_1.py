dcab248e1f0c522227f2738ef84ca54877f9abaf
Joerg Herbel <joerg.herbel@tribe29.com>
2020-03-06 11:35:52 +0100

    10908 FIX snmp_uptime: Fix crash for some Fortigate devices
    Users experienced crashes of uptime checks with some Fortigate devices. This is because
    the data delivered by these devices does not contain any timestamps. As a solution, the
    uptime check will not be discovered any more if the delivered data cannot be interpreted
    as a timestamp. To apply this fix, users have to re-discover the services of the
    corresponding hosts.
    
    Change-Id: Iba3e52df1335c1cdb2fd724e7a58c56d3ac8364f

---
+++ tests/unit/checks/generictests/datasets/snmp_uptime_1.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'snmp_uptime'
+
+
+freeze_time = '1970-02-12 22:59:33'
+
+
+info = [['2297331594', '']]
+
+
+discovery = {'': [(None, {})]}
+
+
+checks = {'': [(None,
+                {},
+                [(0,
+                  'Up since Fri May 23 02:30:58 1969 (265d 21:28:35)',
+                  [('uptime', 22973315, None, None, None, None)])])]}

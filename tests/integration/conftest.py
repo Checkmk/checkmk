a1da2935a4b6d3c0f4de1c2b7cea7b5620dadf11
Lars Michelsen <lm@tribe29.com>
2020-03-10 15:35:17 +0100

    Site state is managed by run-integration-test.py, not the site fixture
    Change-Id: I52eb51968e342d2765d31dd27ba5aff7560bcba2

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -13,8 +13,4 @@ from testlib.utils import current_base_branch_name
 @pytest.fixture(scope="session", autouse=True)
 def site(request):
     sf = get_site_factory(prefix="int_", update_from_git=True, install_test_python_modules=True)
-    site_obj = sf.get_existing_site(current_base_branch_name())
-    yield site_obj
-    print("")
-    print("Cleanup site processes after test execution...")
-    site_obj.stop()
+    return sf.get_existing_site(current_base_branch_name())

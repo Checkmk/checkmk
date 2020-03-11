515532647e2ed9711109eda47bba60ab9ef44d77
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-03-11 10:54:15 +0100

    10600 FIX Interface Bonding
    If no master aggregation ID for interface bonding was known, the check plugins
    lnx_bonding, windows_os_bonding, windows_intel_bonding and ovs_bonding could crash.
    
    In this case we now use the first slaves aggregation ID as expected value for
    all aggregation IDs.
    
    Change-Id: I8246bdce7ee8b5b790fa2d5116858a69c840f985

---
+++ tests-py3/unit/checks/test_bonding_include.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize("params, status, result", [
+    (
+        {
+            'ieee_302_3ad_agg_id_missmatch_state': 1
+        },
+        {
+            'aggregator_id': u'1',
+            'interfaces': {
+                u'ens1f0': {
+                    'aggregator_id': u'1',
+                },
+                u'ens1f1': {
+                    'aggregator_id': u'1',
+                }
+            },
+        },
+        [],
+    ),
+    (
+        {
+            'ieee_302_3ad_agg_id_missmatch_state': 1
+        },
+        {
+            'aggregator_id': u'1',
+            'interfaces': {
+                u'ens1f0': {
+                    'aggregator_id': u'1',
+                },
+                u'ens1f1': {
+                    'aggregator_id': u'2',
+                }
+            },
+        },
+        [
+            (1, "Missmatching aggregator ID of ens1f1: 2"),
+        ],
+    ),
+    (
+        {
+            'ieee_302_3ad_agg_id_missmatch_state': 1
+        },
+        {
+            'interfaces': {
+                u'ens1f0': {
+                    'aggregator_id': u'1',
+                },
+                u'ens1f1': {
+                    'aggregator_id': u'1',
+                }
+            },
+        },
+        [],
+    ),
+    (
+        {
+            'ieee_302_3ad_agg_id_missmatch_state': 2
+        },
+        {
+            'interfaces': {
+                u'ens1f0': {
+                    'aggregator_id': u'1',
+                },
+                u'ens1f1': {
+                    'aggregator_id': u'2',
+                }
+            },
+        },
+        [
+            (2, "Missmatching aggregator ID of ens1f1: 2"),
+        ],
+    ),
+])
+def test_check_ieee_302_3ad_specific(check_manager, params, status, result):
+    bonding_check = check_manager.get_check("lnx_bonding")
+    check_ieee_302_3ad_specific = bonding_check.context["_check_ieee_302_3ad_specific"]
+    assert list(check_ieee_302_3ad_specific(params, status)) == result

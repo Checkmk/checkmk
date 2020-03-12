407ef81593e88cb4278ac8830c4086583c0771bb
Lukas Lengler <lukas.lengler@tribe29.com>
2020-03-12 11:11:23 +0100

    Capitalizing titles in manpages 10/2
    Change-Id: I2131f10151367a634d7b82f3de77c258f7372d15

--- checkman/k8s_stats.fs
+++ checkman/k8s_stats.fs
@@ -1,4 +1,4 @@
-title: Kubernetes: Node and cluster level filesystem usage
+title: Kubernetes: Node and Cluster Level Filesystem Usage
 agents: kubernetes
 catalog: containerization/kubernetes
 license: GPL

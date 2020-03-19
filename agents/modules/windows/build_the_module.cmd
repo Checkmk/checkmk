422ef472dba7aea44db31569f4807bef653f4f52
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-03-19 14:02:12 +0100

    [CMK-3994] - improved build and extended commandline
    - Dumb Strategy introduced and default(this is safe and quick), build_the_module.cmd python or build_the_module.cmd reuse
    - Full Rebuild Strategy can be used with build_the_module.cmd build
    - virtualenv is now limited to version 20.0.10 because 20.0.11 has a annoying regression bug inside
    
    Change-Id: I12fe7b6173ae5fa4091000684c0a94bc1b9fdc48

--- agents/modules/windows/build_the_module.cmd
+++ agents/modules/windows/build_the_module.cmd
@@ -1,10 +1,12 @@
 @rem Main buidling script for Python
 
 @echo off
-@if "%1"=="python" goto python_build 
+@if "%1"=="python" goto python_reuse
+@if "%1"=="reuse" goto python_reuse
+@if "%1"=="build" goto python_build 
 @if "%1"=="success" goto success_build 
 @if "%1"=="fail" goto fail_build 
-goto usage
+powershell Write-Host 'Invalid parameter...' -foreground Red && goto usage
 
 :success_build
 powershell Write-Host 'Simulating success build...' -foreground Cyan
@@ -20,14 +22,19 @@ exit /b 11
 
 :python_build
 powershell Write-Host 'Building python...' -foreground Cyan
-rem make 
+make 
+goto exit
+
+:python_reuse
+powershell Write-Host 'Delivering prebuild python...' -foreground Cyan
 copy backup\python-3.8.zip ..\..\..\artefacts
 goto exit
 
-:usage:
+:usage
 @rem we are using powershell to call the executable with admin rights and wait
 @rem this trick is required to install python in non very interactive environments
 @rem like jenkins build
-@if "%1"==""  powershell Write-Host "Possible parameters:`n `tpython `t- builds python module`n`tsuccess`t- always return success`n`tfail `t- always returns fail" -foreground Cyan && exit /b 9
-
+@powershell Write-Host "Possible parameters:" -foreground Cyan
+@powershell Write-Host "`tpython `t- delivers prebuild python module: DEPRECATED" -foreground Yellow
+@powershell Write-Host "`treuse `t- delivers prebuild python module`n`tbuild`t- builds python module`n`tsuccess`t- always return success`n`tfail `t- always returns fail" -foreground Cyan && exit /b 9
 :exit

73f63ff046e101a92f8c37acf4760a814b548f59
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-03-22 17:21:27 +0100

    API: add types required for checking
    CMK-4007
    
    Change-Id: Iea03f4e3de2fe4ad5f33da313e9b1ef6ed4935db

--- cmk/base/api/agent_based/checking_types.py
+++ cmk/base/api/agent_based/checking_types.py
@@ -5,10 +5,13 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 """Types and classes used by the API for check plugins
 """
-from typing import List, Optional
+from typing import Callable, Dict, Iterable, List, NamedTuple, Optional, Tuple  # pylint: disable=unused-import
 import sys
 import collections
+import enum
 
+from cmk.utils import pnp_cleanup as quote_pnp_string
+from cmk.base.api import PluginName
 from cmk.base.discovered_labels import ServiceLabel
 
 
@@ -89,6 +92,14 @@ class Service:
                                                           self.parameters, self._labels)
 
 
+@enum.unique
+class state(enum.IntEnum):
+    OK = 0
+    WARN = 1
+    CRIT = 2
+    UNKNOWN = 3
+
+
 class MetricFloat(float):
     """Extends the float representation for Infinities in such way that
     they can be parsed by eval"""
@@ -99,3 +110,133 @@ class MetricFloat(float):
         if self < -1 * sys.float_info.max:
             return '-1e%d' % (sys.float_info.max_10_exp + 1)
         return super(MetricFloat, self).__repr__()
+
+
+class Metric:
+    @staticmethod
+    def validate_name(metric_name):
+        # this is not very elegant, but it ensures consistency to cmk.utils.misc.pnp_cleanup
+        pnp_name = quote_pnp_string(metric_name)
+        if metric_name != pnp_name:
+            offenders = ''.join(set(metric_name) - set(pnp_name))
+            raise TypeError("invalid character(s) in metric name: %r" % offenders)
+
+    @staticmethod
+    def _sanitize_single_value(field, value):
+        # (str, Optional[float]) -> Optional[MetricFloat]
+        if value is None:
+            return None
+        if isinstance(value, (int, float)):
+            return MetricFloat(value)
+        raise TypeError("%s values for metric must be float, int or None" % field)
+
+    def _sanitize_optionals(self, field, values):
+        # (str, Tuple[Optional[float], Optional[float]]) -> Tuple[Optional[MetricFloat], Optional[MetricFloat]]
+        if not isinstance(values, tuple) or len(values) != 2:
+            raise TypeError("%r for metric must be a 2-tuple" % field)
+
+        return tuple(self._sanitize_single_value(field, v) for v in values)
+
+    def __init__(
+        self,
+        name,  # type: str
+        value,  # type: float
+        *args,  # type: str  # *, # type shoud be "nothing"
+        levels=(None, None),  # type: Tuple[Optional[float], Optional[float]]
+        boundaries=(None, None),  # type: Tuple[Optional[float], Optional[float]]
+    ):
+        # type: (...) -> None
+        if args:
+            # TODO (mo): unhack this CMK-3983
+            raise TypeError()
+
+        self.validate_name(name)
+
+        if not isinstance(value, (int, float)):
+            raise TypeError("value for metric must be float or int, got %r" % (value,))
+
+        self._name = name
+        self._value = MetricFloat(value)
+        self._levels = self._sanitize_optionals('levels', levels)
+        self._boundaries = self._sanitize_optionals('boundaries', boundaries)
+
+    @property
+    def name(self):
+        # type: () -> str
+        return self._name
+
+    @property
+    def value(self):
+        # type: () -> MetricFloat
+        return self._value
+
+    @property
+    def levels(self):
+        # () -> Tuple[Optional[MetricFloat], Optional[MetricFloat]]
+        return self._levels
+
+    @property
+    def boundaries(self):
+        # () -> Tuple[Optional[MetricFloat], Optional[MetricFloat]]
+        return self._boundaries
+
+
+class Result(NamedTuple("ResultTuple", [
+    ("state", state),
+    ("details", str),
+])):
+    def __new__(cls, state_arg, details):
+        # type: (state, str) -> Result
+        if not isinstance(details, str):
+            raise TypeError("details must be str, got %r" % (details,))
+        if '\n' in details:
+            raise ValueError("newline not allowed in details")
+        return super(Result, cls).__new__(
+            cls,
+            state=state(state_arg),
+            details=details.strip(','),
+        )
+
+
+class AdditionalDetails:
+    """Additional details for a check result"""
+    @staticmethod
+    def _validate(value):
+        # type: (Iterable[str]) -> List[str]
+        if isinstance(value, str):
+            raise TypeError("AdditionalDetails expected iterable of str, got %r" % (value,))
+
+        try:
+            consumed = list(value)
+        except TypeError:
+            raise TypeError("AdditionalDetails expected iterable of str, got %r" % (value,))
+
+        if any(not isinstance(s, str) for s in consumed):
+            raise TypeError("AdditionalDetails expected iterable of str, got %r" % (value,))
+
+        return consumed
+
+    def __init__(self, iter_lines):
+        # type: (Iterable[str]) -> None
+        self._lines = [s.strip('\n') for s in self._validate(iter_lines)]
+
+    def __str__(self):
+        return '\n'.join(self._lines)
+
+
+class IgnoreResults:
+    pass
+
+
+CheckPlugin = NamedTuple("CheckPlugin", [
+    ("name", PluginName),
+    ("sections", List[PluginName]),
+    ("servce_name", str),
+    ("discover_function", Callable),
+    ("discover_default_parameters", Optional[Dict]),
+    ("discover_ruleset_name", Optional[PluginName]),
+    ("check_function", Callable),
+    ("check_default_parameters", Optional[Dict]),
+    ("check_ruleset_name", Optional[PluginName]),
+    ("cluster_check_function", Optional[Callable]),
+])

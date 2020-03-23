2cafec0b7d23a42c7eade380e48d24c039eef561
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-03-23 12:30:41 +0100

    tests: make inventory tests py3 only
    Change-Id: Ic84fa23a44d9833073483b1d9d31bf0437fe9bb6

--- tests-py3/unit/inventory/conftest.py
+++ tests-py3/unit/inventory/conftest.py
@@ -7,10 +7,6 @@
 import pytest  # type: ignore[import]
 import testlib  # type: ignore[import]
 
-# TODO Python 3: Copied conftest from orig conftest file (Python 2)
-# If new fixtures are implemented, don't forget to port them to
-#   tests/unit/inventory/conftest.py
-
 
 @pytest.fixture(scope="module")
 def inventory_plugin_manager():

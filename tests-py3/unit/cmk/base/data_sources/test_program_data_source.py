36c40664f3462117d0b0bf2718a3dceb5933ec97
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2020-03-24 07:47:11 +0100

    Remove unused import to make pylint happy
    Change-Id: I1429ee32cf39c51e19b86ddf977e3533448d204c

--- tests-py3/unit/cmk/base/data_sources/test_program_data_source.py
+++ tests-py3/unit/cmk/base/data_sources/test_program_data_source.py
@@ -4,8 +4,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import os
-
 import pytest  # type: ignore[import]
 
 import cmk.base.config as config

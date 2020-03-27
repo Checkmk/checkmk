dcb891c17d6692f814f57d66bbfd6be12fdd9eb3
Lars Michelsen <lm@tribe29.com>
2020-03-27 10:51:42 +0100

    Simplify download agents page: Show names for all files instead of titles for some files
    Change-Id: I5d738c0f947750c7401a2a7e9b9c339c8d205da3

--- cmk/gui/wato/pages/download_agents.py
+++ cmk/gui/wato/pages/download_agents.py
@@ -7,18 +7,10 @@
 
 import os
 import glob
-import sys
 
 from typing import (  # pylint: disable=unused-import
-    List, Dict, Text, Optional,
+    List, Text,
 )
-import six
-
-# Explicitly check for Python 3 (which is understood by mypy)
-if sys.version_info[0] >= 3:
-    from pathlib import Path  # pylint: disable=import-error
-else:
-    from pathlib2 import Path
 
 import cmk.utils.paths
 import cmk.utils.render
@@ -67,7 +59,7 @@ class ModeDownloadAgents(WatoMode):
                 + glob.glob(cmk.utils.paths.agents_dir + "/*.rpm") \
                 + glob.glob(cmk.utils.paths.agents_dir + "/windows/c*.msi")
 
-        self._download_table(_("Packaged Agents"), {}, packed)
+        self._download_table(_("Packaged Agents"), packed)
 
         titles = {
             '': _('Linux/Unix Agents'),
@@ -91,7 +83,6 @@ class ModeDownloadAgents(WatoMode):
             '/windows/sections',
         ]
 
-        file_titles = {}
         other_sections = []
         for root, _dirs, files in os.walk(cmk.utils.paths.agents_dir):
             file_paths = []
@@ -99,9 +90,6 @@ class ModeDownloadAgents(WatoMode):
             if relpath not in banned_paths:
                 title = titles.get(relpath, relpath)
                 for filename in files:
-                    if filename == "CONTENTS":
-                        file_titles.update(self._read_agent_contents_file(root))
-
                     path = root + '/' + filename
                     if path not in packed and 'deprecated' not in path:
                         file_paths.append(path)
@@ -109,71 +97,29 @@ class ModeDownloadAgents(WatoMode):
                 other_sections.append((title, file_paths))
 
         for title, file_paths in sorted(other_sections):
-            useful_file_paths = [
-                p for p in file_paths
-                if file_titles.get(p, "") is not None and not p.endswith("/CONTENTS")
-            ]
-            file_titles.update(self._read_plugin_inline_comments(useful_file_paths))
+            useful_file_paths = [p for p in file_paths if not p.endswith("/CONTENTS")]
             if useful_file_paths:
-                self._download_table(title, file_titles, sorted(useful_file_paths))
+                self._download_table(title, sorted(useful_file_paths))
         html.close_div()
 
-    def _download_table(self, title, file_titles, paths):
-        # type: (Text, Dict[str, Optional[Text]], List[str]) -> None
+    def _download_table(self, title, paths):
+        # type: (Text, List[str]) -> None
         forms.header(title)
         forms.container()
         for path in paths:
             os_path = path
             relpath = path.replace(cmk.utils.paths.agents_dir + '/', '')
             filename = path.split('/')[-1]
-            title = file_titles.get(os_path, filename) or u""
 
             file_size = os.stat(os_path).st_size
 
             # FIXME: Rename classes etc. to something generic
             html.open_div(class_="ruleset")
             html.open_div(style="width:300px;", class_="text")
-            html.a(title, href="agents/%s" % relpath)
+            html.a(filename, href="agents/%s" % relpath)
             html.span("." * 200, class_="dots")
             html.close_div()
             html.div(cmk.utils.render.fmt_bytes(file_size), style="width:60px;", class_="rulecount")
             html.close_div()
             html.close_div()
         forms.end()
-
-    def _read_plugin_inline_comments(self, file_paths):
-        # type: (List[str]) -> Dict[str, Optional[Text]]
-        comment_prefixes = ["# ", "REM ", "$!# "]
-        windows_bom = b"\xef\xbb\xbf"
-        file_titles = {}  # type: Dict[str, Optional[Text]]
-        for path in file_paths:
-            with open(path, "rb") as f:
-                first_bytes = f.read(500)
-
-            if first_bytes.startswith(windows_bom):
-                first_bytes = first_bytes[len(windows_bom):]
-
-            first_lines = six.ensure_text(first_bytes).splitlines()
-            for line in first_lines:
-                for prefix in comment_prefixes:
-                    if line.startswith(prefix) and len(line) > len(prefix) and line[len(
-                            prefix)].isalpha():
-                        file_titles[path] = line[len(prefix):].strip()
-                        break
-                if path in file_titles:
-                    break
-        return file_titles
-
-    def _read_agent_contents_file(self, root):
-        # type: (str) -> Dict[str, Optional[Text]]
-        file_titles = {}  # type: Dict[str, Optional[Text]]
-        with Path(root, "CONTENTS").open(encoding="utf-8") as f:
-            for line in f:
-                line = line.strip()
-                if line and not line.startswith("#"):
-                    file_name, title = line.split(None, 1)
-                    if title == "(hide)":
-                        file_titles[root + "/" + file_name] = None
-                    else:
-                        file_titles[root + "/" + file_name] = title
-        return file_titles

021b20e60f8d74733e1ed40d6578f6b6c33bab6b
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-03-29 15:03:38 +0200

    CheckAPI: tools for auto migration of check plugins
    CMK-4008
    
    Change-Id: I9caca1e23a4b5e98757777d9532af6b656db7acf

---
+++ cmk/base/api/agent_based/register/check_plugins_legacy.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""Helper to register a new-style section based on config.check_info
+"""
+from typing import Any, Callable, Dict, Generator, List, Optional, Tuple, Union
+import functools
+import itertools
+
+from cmk.base.api import PluginName
+from cmk.base.api.agent_based.checking_types import (
+    management_board,
+    CheckPlugin,
+    Metric,
+    state,
+    Result,
+    Service,
+)
+from cmk.base.api.agent_based.register.check_plugins import create_check_plugin
+import cmk.base.config as config
+from cmk.base.check_api_utils import (
+    Service as LegacyService,
+    HOST_ONLY as LEGACY_HOST_ONLY,
+    MGMT_ONLY as LEGACY_MGMT_ONLY,
+    HOST_PRECEDENCE as LEGACY_HOST_PREC,
+)
+from cmk.base.discovered_labels import HostLabel, DiscoveredHostLabels
+
+DUMMY_RULESET_NAME = "non_existent_auto_migration_dummy_rule"
+
+# There are so many check_info keys, make sure we didn't miss one.
+CONSIDERED_KEYS = {
+    "check_function",
+    "default_levels_variable",
+    "extra_sections",
+    "group",
+    "handle_empty_info",  # obsolete
+    "handle_real_time_checks",  # obsolete
+    "has_perfdata",  # obsolete
+    "includes",
+    "inventory_function",
+    "management_board",
+    "node_info",  # handled in section
+    "parse_function",
+    "service_description",
+    "snmp_info",  # handled in section
+    "snmp_scan_function",  # handled in section
+}
+
+
+def _create_management_board_option(check_info_dict):
+    # type: (Dict[str, Any]) -> Optional[management_board]
+    legacy_value = check_info_dict.get("management_board")
+    if legacy_value in (None, LEGACY_HOST_PREC):
+        return None
+    if legacy_value == LEGACY_HOST_ONLY:
+        return management_board.DISABLED
+    if legacy_value == LEGACY_MGMT_ONLY:
+        return management_board.EXCLUSIVE
+    raise NotImplementedError("unknown management_board value: %r" % (legacy_value,))
+
+
+def _create_discovery_function(check_info_dict):
+    # type: (Dict[str, Any]) -> Callable
+    """Create an API compliant discovery function"""
+
+    # 1) ensure we have the correct signature
+    # 2) ensure it is a generator of Service instances
+    def discovery_migration_wrapper(section):
+        original_discovery_result = check_info_dict["inventory_function"](section)
+        if not original_discovery_result:
+            return
+
+        for element in original_discovery_result:
+            if isinstance(element, (HostLabel, DiscoveredHostLabels)):
+                # these are dealt with in the host_label_function!
+                continue
+            if isinstance(element, LegacyService):
+                yield Service(
+                    item=element.item,
+                    parameters=element.parameters,
+                    labels=list(element.service_labels),
+                )
+            elif isinstance(element, tuple) and len(element) in (2, 3):
+                item, params = element[0], element[-1]
+                if not isinstance(params, dict):
+                    # our new service object will refuse to take any parameters but a dict
+                    params = wrap_parameters(params)
+                yield Service(item=item, parameters=params)
+            else:
+                # just let it through. Base must deal with bogus return types anyway.
+                yield element
+
+    return discovery_migration_wrapper
+
+
+def _create_check_function(name, check_info_dict, ruleset_name):
+    # type: (str, Dict[str, Any], Optional[str]) -> Callable
+    """Create an API compliant check function"""
+    service_descr = check_info_dict["service_description"]
+    if not isinstance(service_descr, str):
+        raise ValueError("[%s]: invalid service description: %r" % (name, service_descr))
+
+    # 1) ensure we have the correct signature
+    sig_function = _create_signature_check_function(
+        requires_item="%s" in service_descr,
+        requires_params=ruleset_name is not None,
+        original_function=check_info_dict["check_function"],
+    )
+
+    # 2) unwrap parameters and ensure it is a generator of valid instances
+    @functools.wraps(sig_function)
+    def check_result_generator(*args, **kwargs):
+        assert not args, "pass arguments as keywords to check function"
+        if "params" in kwargs:
+            kwargs["params"] = unwrap_parameters(kwargs["params"])
+
+        subresults = sig_function(**kwargs)
+        if isinstance(subresults, tuple):  # just one result
+            subresults = [subresults]
+
+        # Once we have seen a newline in *any* subresult,
+        # all remaining output is sent to the details page!
+        # I'm not saying that is good, but we stay compatible.
+        opt_newline = ""
+        for subresult in subresults:
+            opt_newline = yield from _create_new_result(opt_newline, *subresult)
+
+    return check_result_generator
+
+
+def _create_new_result(
+    implicit_newline,  # type: str
+    legacy_state,  # type: int
+    legacy_text,  # type: str
+    legacy_metrics=(),  # type: Union[Tuple, List]
+):
+    # type: (...) -> Generator[Union[Metric, Result], None, str]
+    kwargs = {"state": state(legacy_state)}  # type: Dict[str, Any]
+    components = (implicit_newline + legacy_text).split("\n", 1)
+    kwargs["summary"] = components[0]
+    if len(components) > 1:
+        # make sure its longer than summary
+        kwargs["details"] = ("%%-%ds" % (len(components[0]) + 1)) % components[1]
+
+    yield Result(**kwargs)
+
+    for metric in legacy_metrics:
+        # fill up with None:
+        name, value, warn, crit, min_, max_ = (
+            v for v, _ in itertools.zip_longest(metric, range(6)))
+        yield Metric(name, value, levels=(warn, crit), boundaries=(min_, max_))
+
+    return "\n" if len(components) > 1 else ""
+
+
+def _create_signature_check_function(
+    requires_item,  # type: bool
+    requires_params,  # type: bool
+    original_function,  # type: Callable
+):
+    # type: (...) -> Callable
+    """Create the function for a check function with the required signature"""
+    if requires_item:
+        if requires_params:
+
+            def check_migration_wrapper(item, params, section):
+                return original_function(item, params, section)
+        else:
+            # suppress "All conditional function variants must have identical signatures"
+            def check_migration_wrapper(item, section):  # type: ignore[misc]
+                return original_function(item, {}, section)
+    else:
+        if requires_params:
+
+            def check_migration_wrapper(params, section):  # type: ignore[misc]
+                return original_function(None, params, section)
+        else:
+
+            def check_migration_wrapper(section):  # type: ignore[misc]
+                return original_function(None, {}, section)
+
+    return check_migration_wrapper
+
+
+def _create_wrapped_parameters(check_plugin_name, check_info_dict):
+    # type: (str, Dict[str, Any]) -> Optional[Dict[str, Any]]
+    """compute default parameters and wrap them in a dictionary"""
+    var_name = check_info_dict.get("default_levels_variable")
+    if var_name is None:
+        return {} if check_info_dict.get("group") else None
+
+    # look in factory settings
+    parameters = config.factory_settings.get(var_name)
+    if isinstance(parameters, dict):
+        return parameters
+    if parameters is not None:
+        return wrap_parameters(parameters)
+
+    # look in check context
+    parameters = config.get_check_context(check_plugin_name).get(var_name)
+    if isinstance(parameters, dict):
+        return parameters
+    if parameters is not None:
+        return wrap_parameters(parameters)
+
+    raise ValueError("[%s]: default levels variable %r is undefined" %
+                     (check_plugin_name, var_name))
+
+
+def _create_cluster_legacy_mode_from_hell(check_function):
+    # type: (Callable) -> Callable
+    @functools.wraps(check_function)
+    def cluster_legacy_mode_from_hell(*args, **kwargs):
+        raise NotImplementedError("This just a dummy to pass validation.")
+        yield  # pylint: disable=unreachable
+
+    cluster_legacy_mode_from_hell.__name__ = "cluster_legacy_mode_from_hell"
+    return cluster_legacy_mode_from_hell
+
+
+def create_check_plugin_from_legacy(check_plugin_name, check_info_dict, forbidden_names):
+    # type: (str, Dict[str, Any], List[PluginName]) -> CheckPlugin
+
+    if check_info_dict.get("handle_empty_info"):
+        raise NotImplementedError("[%s]: 'handle_empty_info' is obsolete" % check_plugin_name)
+    if check_info_dict.get('extra_sections'):
+        raise NotImplementedError("[%s]: cannot auto-migrate plugins with extra sections" %
+                                  check_plugin_name)
+
+    # make sure we haven't missed something important:
+    unconsidered_keys = set(check_info_dict) - CONSIDERED_KEYS
+    assert not unconsidered_keys, ("Unconsidered key(s) in check_info[%r]: %r" %
+                                   (check_plugin_name, unconsidered_keys))
+
+    new_check_name = maincheckify(check_plugin_name)
+
+    discovery_function = _create_discovery_function(check_info_dict)
+
+    check_default_parameters = _create_wrapped_parameters(check_plugin_name, check_info_dict)
+    check_ruleset_name = check_info_dict.get("group")
+    if check_ruleset_name is None and check_default_parameters is not None:
+        check_ruleset_name = DUMMY_RULESET_NAME
+    check_function = _create_check_function(
+        check_plugin_name,
+        check_info_dict,
+        check_ruleset_name,
+    )
+
+    return create_check_plugin(
+        name=new_check_name,
+        sections=[check_plugin_name.split('.', 1)[0]],
+        service_name=check_info_dict['service_description'],
+        management_board_option=_create_management_board_option(check_info_dict),
+        discovery_function=discovery_function,
+        discovery_default_parameters=None,  # legacy madness!
+        discovery_ruleset_name=None,
+        check_function=check_function,
+        check_default_parameters=check_default_parameters,
+        check_ruleset_name=check_ruleset_name,
+        cluster_check_function=_create_cluster_legacy_mode_from_hell(check_function),
+        forbidden_names=forbidden_names,
+    )
+
+
+def maincheckify(subcheck_name):
+    # type: (str) -> str
+    """Get new plugin name
+
+    The new API does not know about "subchecks", so drop the dot notation.
+    The validation step will prevent us from having colliding plugins.
+    """
+    return subcheck_name.replace('.', '_')
+
+
+PARAMS_WRAPPER_KEY = "auto-migration-wrapper-key"
+
+
+def wrap_parameters(parameters):
+    # type: (Any) -> Dict[str, Any]
+    return {PARAMS_WRAPPER_KEY: parameters}
+
+
+def unwrap_parameters(parameters):
+    # type: (Dict[str, Any]) -> Any
+    if PARAMS_WRAPPER_KEY not in parameters:
+        return parameters
+    assert len(parameters) == 1, "unexpected keys in parameters: %r" % (parameters,)
+    return parameters[PARAMS_WRAPPER_KEY]
---
+++ tests-py3/unit/cmk/base/api/agent_based/register/test_check_plugins_legacy.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# pylint: disable=protected-access
+
+import inspect
+
+import pytest  # type: ignore[import]
+
+from cmk.base.api import PluginName
+import cmk.base.api.agent_based.checking_types as checking_types
+import cmk.base.api.agent_based.register.check_plugins_legacy as check_plugins_legacy
+from cmk.base.check_api_utils import Service as OldService
+from cmk.base.discovered_labels import HostLabel
+
+
+def dummy_generator(section):  # pylint: disable=unused-argument
+    return
+    yield  # pylint: disable=unreachable
+
+
+MINIMAL_CHECK_INFO = {
+    "service_description": "Norris Device",
+    "inventory_function": dummy_generator,
+    "check_function": dummy_generator,
+}
+
+
+def test_create_discovery_function():
+    def insane_discovery(info):
+        """Completely crazy discovery function:
+
+            * wrong arg name
+            * is not a generator
+            * returns all kinds of stuff
+        """
+        assert info == ["info"]
+        return [
+            ("foo", {}),
+            "some string",
+            HostLabel("whoop", "deedoo"),
+            OldService("bar", {"P": "O"}),
+        ]
+
+    new_function = check_plugins_legacy._create_discovery_function(
+        {"inventory_function": insane_discovery},)
+
+    fixed_params = inspect.signature(new_function).parameters
+    assert list(fixed_params) == ["section"]
+    assert inspect.isgeneratorfunction(new_function)
+
+    result = list(new_function(["info"]))
+    assert result == [
+        checking_types.Service(item="foo"),
+        "some string",  # bogus value let through intentionally
+        checking_types.Service(item="bar", parameters={"P": "O"}),
+    ]
+
+
+def test_create_check_function():
+    def insane_check(item, _no_params, info):
+        assert item == "Test Item"
+        assert _no_params == {}
+        assert info == ["info"]
+        yield 0, "Main info", [("mymetric", 23, 2, 3)]
+        yield 1, "still main, but very long\nadditional1"
+        yield 2, "additional2\nadditional3"
+
+    new_function = check_plugins_legacy._create_check_function(
+        "test_plugin",
+        {
+            "check_function": insane_check,
+            "service_description": "Foo %s",
+        },
+        None,
+    )
+
+    fixed_params = inspect.signature(new_function).parameters
+    assert list(fixed_params) == ["item", "section"]
+    assert inspect.isgeneratorfunction(new_function)
+
+    result = new_function(item="Test Item", section=["info"])
+    assert list(result) == [
+        checking_types.Result(
+            state=checking_types.state.OK,
+            summary="Main info",
+        ),
+        checking_types.Metric("mymetric", 23.0, levels=(2.0, 3.0)),
+        checking_types.Result(
+            state=checking_types.state.WARN,
+            summary="still main, but very long",
+            details="additional1               ",
+        ),
+        checking_types.Result(state=checking_types.state.CRIT, details="additional2\nadditional3"),
+    ]
+
+
+def test_create_check_plugin_from_legacy():
+    plugin = check_plugins_legacy.create_check_plugin_from_legacy(
+        "norris",
+        MINIMAL_CHECK_INFO,
+        [],
+    )
+
+    assert plugin.name == PluginName("norris")
+    assert plugin.sections == [PluginName("norris")]
+    assert plugin.service_name == MINIMAL_CHECK_INFO["service_description"]
+    assert plugin.discovery_function.__name__ == 'discovery_migration_wrapper'
+    assert plugin.discovery_default_parameters is None
+    assert plugin.discovery_ruleset_name is None
+    assert plugin.check_function.__name__ == 'check_migration_wrapper'
+    assert plugin.check_default_parameters is None
+    assert plugin.check_ruleset_name is None
+    assert plugin.cluster_check_function.__name__ == "cluster_legacy_mode_from_hell"
+
+
+@pytest.mark.parametrize("params", [
+    "foo_levels",
+    (1, 2),
+])
+def test_un_wrap_parameters(params):
+    wrapped = check_plugins_legacy.wrap_parameters(params)
+    assert isinstance(wrapped, dict)
+    assert check_plugins_legacy.unwrap_parameters(wrapped) is params

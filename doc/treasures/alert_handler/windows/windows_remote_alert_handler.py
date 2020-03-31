5dc1b967ca88c0a1fc0fccf7c60f3817300192c2
Lars Michelsen <lm@tribe29.com>
2020-03-31 23:50:52 +0200

    windows_remote_alert_handler.py: Add some more missing imports
    fixes #151
    
    Change-Id: I786b5d438bbcd1daf6f8ee1bef423945379133c7

--- doc/treasures/alert_handler/windows/windows_remote_alert_handler.py
+++ doc/treasures/alert_handler/windows/windows_remote_alert_handler.py
@@ -30,10 +30,16 @@
 #  along with Check_MK. If not, email to mk@mathias-kettner.de
 #  or write to the postal address provided at www.mathias-kettner.de
 
-from cmk.gui.cee.plugins.wato.alert_handling import (
-    register_alert_handler_parameters,)
-from cmk.gui.plugins.wato.utils import (
-    PasswordFromStore,)
+import re
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    TextAscii,
+)
+
+from cmk.gui.cee.plugins.wato.alert_handling import register_alert_handler_parameters
+from cmk.gui.plugins.wato.utils import PasswordFromStore
 
 register_alert_handler_parameters(
     "windows_remote",

a84cbf5580af2597e137f452f0218097ce433e30
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-04-06 20:09:25 +0200

    API: expose all present section related API elements
    Change-Id: I6b47e9fb95ac2805249a993ac06f0b59b45e68bc

--- cmk/base/plugins/agent_based/v0/__init__.py
+++ cmk/base/plugins/agent_based/v0/__init__.py
@@ -3,20 +3,45 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+"""API for agent based plugins
+"""
+# For an explanation of what is what see comments in __all__definition at the end
+
 from cmk.base.api.agent_based.utils import (  # pylint: disable=cmk-module-layer-violation
-    all_of, any_of, startswith, endswith, matches,
+    parse_string_table, all_of, any_of, contains, startswith, endswith, matches, equals, exists,
+    not_contains, not_startswith, not_endswith, not_matches, not_equals, not_exists,
 )
 
-from cmk.base.api.agent_based.section_types import SNMPTree  # pylint: disable=cmk-module-layer-violation
+from cmk.base.api.agent_based.section_types import SNMPTree, OIDEnd  # pylint: disable=cmk-module-layer-violation
+from cmk.base.discovered_labels import HostLabel  # pylint: disable=cmk-module-layer-violation
+from cmk.base.snmp_utils import OIDCached, OIDBytes  # pylint: disable=cmk-module-layer-violation
 
 from . import register
 
 __all__ = [
+    # register functions
     "register",
+    ## SECTION related
+    "SNMPTree",
+    "OIDEnd",
+    "OIDCached",
+    "OIDBytes",
+    "HostLabel",
+    # detect spec helper
     "all_of",
     "any_of",
+    "contains",
     "startswith",
     "endswith",
     "matches",
-    "SNMPTree",
+    "equals",
+    "exists",
+    "not_contains",
+    "not_startswith",
+    "not_endswith",
+    "not_matches",
+    "not_equals",
+    "not_exists",
+    # utils
+    "parse_string_table",
 ]

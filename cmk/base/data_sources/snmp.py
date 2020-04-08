8f1e6ff8b2ab03bc671970e6719a1df394d20f10
Mathias Laurin <mathias.laurin@tribe29.com>
2020-04-08 15:12:41 +0200

    SNMP data source: do not build strings before logging
    Also: remove unnecessary parens
    
    CMK-4143
    
    Change-Id: I942d8d766d10e61ab1b29070856b6921c0c856d7

--- cmk/base/data_sources/snmp.py
+++ cmk/base/data_sources/snmp.py
@@ -260,17 +260,17 @@ class SNMPDataSource(ABCSNMPDataSource):
             # This checks data is configured to be persisted (snmp_check_interval) and recent enough.
             # Skip gathering new data here. The persisted data will be added later
             if self._persisted_sections and section_name in self._persisted_sections:
-                self._logger.debug("%s: Skip fetching data (persisted info exists)" %
-                                   (check_plugin_name))
+                self._logger.debug("%s: Skip fetching data (persisted info exists)",
+                                   check_plugin_name)
                 continue
 
             # Prevent duplicate data fetching of identical section in case of SNMP sub checks
             if section_name in info:
-                self._logger.debug("%s: Skip fetching data (section already fetched)" %
-                                   (check_plugin_name))
+                self._logger.debug("%s: Skip fetching data (section already fetched)",
+                                   check_plugin_name)
                 continue
 
-            self._logger.debug("%s: Fetching data" % (check_plugin_name))
+            self._logger.debug("%s: Fetching data", check_plugin_name)
 
             # oid_info can now be a list: Each element  of that list is interpreted as one real oid_info
             # and fetches a separate snmp table.

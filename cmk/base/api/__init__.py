e4c23b4375e41c299695d1917df398dba160aef2
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-04-08 09:45:14 +0200

    CheckAPI: reluctantly add exceptions to PluginNames
    Change-Id: I0a372a5658a06926e4b554ce6b2c105ec1a22bc3

--- cmk/base/api/__init__.py
+++ cmk/base/api/__init__.py
@@ -19,9 +19,21 @@ class PluginName:
     """
     VALID_CHARACTERS = string.ascii_letters + '_' + string.digits
 
+    # unfortunately, we have some WATO rules that contain dots or dashs.
+    # In order not to break things, but be consistent in the future
+    # we maintain a list of exceptions.
+    _LEGACY_NAMING_EXCEPTIONS = {
+        'drbd.net', 'drbd.disk', 'drbd.stats', 'fileinfo-groups', 'hpux_snmp_cs.cpu',
+        'j4p_performance.mem', 'j4p_performance.threads', 'j4p_performance.uptime',
+        'j4p_performance.app_state', 'j4p_performance.app_sess', 'j4p_performance.serv_req',
+        'sap_value-groups'
+    }
+
     def __init__(self, plugin_name, forbidden_names=None):
         # type: (str, Optional[Iterable[PluginName]]) -> None
         self._value = plugin_name
+        if plugin_name in self._LEGACY_NAMING_EXCEPTIONS:
+            return
 
         if not isinstance(plugin_name, str):
             raise TypeError("PluginName must initialized from str")
@@ -30,7 +42,7 @@ class PluginName:
 
         for char in plugin_name:
             if char not in self.VALID_CHARACTERS:
-                raise ValueError("invalid character for PluginName: %r" % (char,))
+                raise ValueError("invalid character for PluginName %r: %r" % (plugin_name, char))
 
         if forbidden_names and any(plugin_name == str(fn) for fn in forbidden_names):
             raise ValueError("duplicate plugin name: %r" % (plugin_name,))

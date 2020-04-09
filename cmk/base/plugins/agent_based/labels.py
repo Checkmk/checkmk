87a2b5a6db7b6ab9dff48d509e09719f5cce77f2
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-04-09 10:03:19 +0200

    CheckAPI: migrate pure section: labels
    Change-Id: I8ac0600b17109a28bf68ff11e9b352f6e159603b

---
+++ cmk/base/plugins/agent_based/labels.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+import json
+from cmk.base.plugins.agent_based.v0 import register, HostLabel  # type: ignore[import]
+
+
+def parse_labels(string_table):
+    """Load json dicts
+
+    Example:
+
+        <<<labels:sep(0)>>>
+        {"tier": "control-plane", "component": "kube-scheduler"}
+
+    """
+    labels = {}
+    for line in string_table:
+        labels.update(json.loads(line[0]))
+    return labels
+
+
+def host_label_function_labels(section):
+    for pair in section.items():
+        yield HostLabel(*pair)
+
+
+register.agent_section(
+    name="labels",
+    parse_function=parse_labels,
+    host_label_function=host_label_function_labels,
+)

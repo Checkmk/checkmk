b216947dba6fbe6202f21ff7fdc949149dd96091
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-04-22 10:57:41 +0200

    add test for mutli_host_sections
    Change-Id: I7f5b78bf38e981d678e2e38e0eaa554d8a393c48

--- tests-py3/unit/cmk/base/data_sources/test_host_sections.py
+++ tests-py3/unit/cmk/base/data_sources/test_host_sections.py
@@ -3,21 +3,55 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+import collections
 
 import pytest  # type: ignore[import]
 from testlib.base import Scenario
+
+import cmk.base.config as config
+from cmk.base.api import PluginName
 import cmk.base.ip_lookup as ip_lookup
 from cmk.base.data_sources.host_sections import MultiHostSections
 from cmk.base.data_sources.abstract import AgentHostSections
 
+_TestSection = collections.namedtuple(
+    "TestSection",
+    "name, parsed_section_name, parse_function, supercedes",
+)
+
+SECTION_ONE = _TestSection(
+    PluginName("one"),
+    PluginName("parsed"),
+    lambda x: {
+        "parsed_by": "one",
+        "node": x[0][0]
+    },
+    [],
+)
+
+SECTION_TWO = _TestSection(
+    PluginName("two"),
+    PluginName("parsed"),
+    lambda x: {
+        "parsed_by": "two",
+        "node": x[0][0]
+    },
+    [PluginName("one")],
+)
+
+MOCK_SECTIONS = {
+    SECTION_ONE.name: SECTION_ONE,
+    SECTION_TWO.name: SECTION_TWO,
+}
+
 NODE_1 = [
-    ["node1 data 1"],
-    ["node1 data 2"],
+    ["node1", "data 1"],
+    ["node1", "data 2"],
 ]
 
 NODE_2 = [
-    ["node2 data 1"],
-    ["node2 data 2"],
+    ["node2", "data 1"],
+    ["node2", "data 2"],
 ]
 
 
@@ -39,6 +73,39 @@ def _set_up(monkeypatch, hostname, nodes, cluster_mapping):
 
     monkeypatch.setattr(ip_lookup, "lookup_ip_address", lambda h: "127.0.0.1")
     monkeypatch.setattr(config_cache, "host_of_clustered_service", host_of_clustered_service)
+    monkeypatch.setattr(config, "get_registered_section_plugin", MOCK_SECTIONS.get)
+
+
+@pytest.mark.parametrize(
+    "node_section_content,expected_result",
+    [
+        ({}, None),
+        ({
+            "one": NODE_1
+        }, {
+            "parsed_by": "one",
+            "node": "node1"
+        }),
+        ({
+            "two": NODE_1
+        }, {
+            "parsed_by": "two",
+            "node": "node1"
+        }),
+        # TODO (mo): CMK-4232 # ({"one": NODE_1, "two": NODE_1}, {"parsed_by": "two", "node": "node1"}),
+    ])
+def test_get_parsed_section(monkeypatch, node_section_content, expected_result):
+
+    _set_up(monkeypatch, "node1", None, {})
+
+    multi_host_sections = MultiHostSections()
+    multi_host_sections.add_or_get_host_sections("node1", "127.0.0.1",
+                                                 AgentHostSections(sections=node_section_content))
+
+    content = multi_host_sections.get_parsed_section(PluginName("parsed"), ("node1", "127.0.0.1"))
+
+    assert expected_result == content,\
+           "Section content: Expected '%s' but got '%s'" % (expected_result, content)
 
 
 @pytest.mark.parametrize(

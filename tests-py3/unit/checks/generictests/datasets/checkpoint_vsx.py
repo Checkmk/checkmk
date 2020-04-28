92a436422500067fe90a9d0b4ee669460f282ce8
Ronny Bruska <ronny.bruska@tribe29.com>
2020-04-28 13:45:14 +0200

    checkpoint_vsx: Changed item to "VSID-NAME VSID_NR"
    Change-Id: Id742e7a9984fefb54dcf4493d756067c119d8fdb

--- tests-py3/unit/checks/generictests/datasets/checkpoint_vsx.py
+++ tests-py3/unit/checks/generictests/datasets/checkpoint_vsx.py
@@ -11,38 +11,62 @@ checkname = 'checkpoint_vsx'
 info = [
     [
         [
-            '0', 'imo1fwdc004', 'VSX Gateway', '195.23.49.134', 'Standard',
-            'nix', 'Trust established', 'Standby'
+            '0', 'my_vsid1', 'VSX Gateway', '192.168.1.11', 'Standard',
+            'ACTIVE', 'Trust established', 'Standby'
+        ],
+        [
+            '1', 'my_vsid2', 'VSX Gateway', '192.168.1.111', 'Standard',
+            'STANDBY', 'not known', 'Standby'
         ]
     ],
     [
         [
-            '0', '104470', '499900', '150512', '369', '150143', '0',
-            '46451524', '44344', '0', '2386'
+            '104470', '499900', '150512', '369', '150143', '0', '46451524',
+            '44344', '0', '2386'
+        ],
+        [
+            '104470', '499900', '150512', '369', '150143', '0', '46451524',
+            '44344', '0', '2386'
         ]
     ]
 ]
 
 discovery = {
-    '': [('1', {})],
-    'connections': [('1', {})],
-    'packets': [('1', {})],
-    'traffic': [('1', {})],
-    'status': [('1', {})]
+    '': [('my_vsid1 0', {}), ('my_vsid2 1', {})],
+    'connections': [('my_vsid1 0', {}), ('my_vsid2 1', {})],
+    'packets': [('my_vsid1 0', {}), ('my_vsid2 1', {})],
+    'traffic': [('my_vsid1 0', {}), ('my_vsid2 1', {})],
+    'status': [('my_vsid1 0', {}), ('my_vsid2 1', {})]
 }
 
 checks = {
     '': [
         (
-            '1', {}, [
-                (0, 'Name: imo1fwdc004', []), (0, 'Type: VSX Gateway', []),
-                (0, 'Main IP: 195.23.49.134', [])
+            'my_vsid1 0', {}, [
+                (0, 'Type: VSX Gateway', []), (0, 'Main IP: 192.168.1.11', [])
+            ]
+        ),
+        (
+            'my_vsid2 1', {}, [
+                (0, 'Type: VSX Gateway', []),
+                (0, 'Main IP: 192.168.1.111', [])
             ]
         )
     ],
     'connections': [
         (
-            '1', {
+            'my_vsid1 0', {
+                'levels_perc': (90.0, 95.0)
+            }, [
+                (
+                    0, 'Used connections: 104470', [
+                        ('connections', 104470, None, None, None, None)
+                    ]
+                ), (0, 'Used percentage: 20.9%', [])
+            ]
+        ),
+        (
+            'my_vsid2 1', {
                 'levels_perc': (90.0, 95.0)
             }, [
                 (
@@ -55,7 +79,36 @@ checks = {
     ],
     'packets': [
         (
-            '1', {}, [
+            'my_vsid1 0', {}, [
+                (
+                    0, 'Total number of packets processed: 0 1/s', [
+                        ('packets', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, 'Total number of accepted packets: 0 1/s', [
+                        ('packets_accepted', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, 'Total number of dropped packets: 0 1/s', [
+                        ('packets_dropped', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, 'Total number of rejected packets: 0 1/s', [
+                        ('packets_rejected', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, 'Total number of logs sent: 0 1/s', [
+                        ('logged', 0.0, None, None, None, None)
+                    ]
+                )
+            ]
+        ),
+        (
+            'my_vsid2 1', {}, [
                 (
                     0, 'Total number of packets processed: 0 1/s', [
                         ('packets', 0.0, None, None, None, None)
@@ -86,7 +139,26 @@ checks = {
     ],
     'traffic': [
         (
-            '1', {}, [
+            'my_vsid1 0', {}, [
+                (
+                    0, 'Total number of bytes accepted: 0.00 B 1/s', [
+                        ('bytes_accepted', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, 'Total number of bytes dropped: 0.00 B 1/s', [
+                        ('bytes_dropped', 0.0, None, None, None, None)
+                    ]
+                ),
+                (
+                    0, 'Total number of bytes rejected: 0.00 B 1/s', [
+                        ('bytes_rejected', 0.0, None, None, None, None)
+                    ]
+                )
+            ]
+        ),
+        (
+            'my_vsid2 1', {}, [
                 (
                     0, 'Total number of bytes accepted: 0.00 B 1/s', [
                         ('bytes_accepted', 0.0, None, None, None, None)
@@ -107,11 +179,19 @@ checks = {
     ],
     'status': [
         (
-            '1', {}, [
+            'my_vsid1 0', {}, [
                 (0, 'HA Status: Standby', []),
                 (0, 'SIC Status: Trust established', []),
                 (0, 'Policy name: Standard', []),
-                (2, 'Policy type: nix (no policy installed)', [])
+                (0, 'Policy type: ACTIVE', [])
+            ]
+        ),
+        (
+            'my_vsid2 1', {}, [
+                (0, 'HA Status: Standby', []),
+                (2, 'SIC Status: not known', []),
+                (0, 'Policy name: Standard', []),
+                (2, 'Policy type: STANDBY (no policy installed)', [])
             ]
         )
     ]

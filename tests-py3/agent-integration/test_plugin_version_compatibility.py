1309add04a3ece4afa22e6007114eaaf0dd58758
Lars Michelsen <lm@tribe29.com>
2020-04-30 20:04:51 +0200

    Update test to match current file naming approach
    Change-Id: I1f23b85ce99c7c0132e307cfe170a95448c9d163

--- tests-py3/agent-integration/test_plugin_version_compatibility.py
+++ tests-py3/agent-integration/test_plugin_version_compatibility.py
@@ -47,8 +47,16 @@ def _get_python_plugins():
 
 
 @pytest.mark.parametrize("plugin_path", _get_python_plugins())
-@pytest.mark.parametrize("python_version", ["2.5", "2.6", "2.7"])
+@pytest.mark.parametrize("python_version", ["2.5", "2.6", "2.7", "3.7"])
 def test_agent_plugin_syntax_compatibility(python_container, plugin_path, python_version):
+    if plugin_path.endswith(".py2") and not python_version.startswith("2"):
+        pytest.skip("Do not test .py2 with Python 3 "
+                    "(Plugins are needed for compatibilit with 2.5 and newer)")
+
+    if not plugin_path.endswith(".py2") and python_version in ["2.5", "2.6"]:
+        pytest.skip("Do not test .py with Python 2 "
+                    "(Plugins are needed for compatibilit with 2.5 and newer)")
+
     _exit_code, output = python_container.exec_run(
         ["python%s" % python_version, ("/cmk/%s" % plugin_path)],
         workdir="/cmk",

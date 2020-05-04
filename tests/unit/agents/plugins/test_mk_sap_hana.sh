0381a3e0537d14830db5023020f3f09bb0871f5f
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2020-05-04 17:23:04 +0200

    Revert "Revert "10937 Monitor SAP HANA Connect State""
    This reverts commit 9fa4f774df759e5818c7ba7c8ccc8193397eb6a1.
    
    Change-Id: I376df0fa26bb14f0b84fa1cbc47dfc6ad87686e1

--- tests/unit/agents/plugins/test_mk_sap_hana.sh
+++ tests/unit/agents/plugins/test_mk_sap_hana.sh
@@ -19,13 +19,15 @@ oneTimeSetUp () {
 
     export MK_CONFDIR=${SHUNIT_TMPDIR}
     export USERSTOREKEY="storekey"
+    export PASSWORD_CONNECT="123"
+    export USER_CONNECT="hana"
 
     # shellcheck disable=SC1090
     . "$MK_SAP_HANA_PLUGIN_PATH" >/dev/null 2>&1
 
     # Mock sys calls
     nslookup () {
-        if [ "$1" = "lavdb10y04001" ];then
+        if [ "$1" = "myServer" ];then
             cat <<"output"
 Server:		127.0.0.53
 Address:	127.0.0.53#53
@@ -41,7 +43,83 @@ wlo1: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
     inet 192.168.1.1
 output
     }
+#
+}
 
+sim_landscape_1_0_worker() {
+    cat <<output
+| Host     | Host   | Host   | Failover | Remove | Storage   | Storage   | Failover     | Failover     | NameServer  | NameServer  | IndexServer | IndexServer | Host         | Host         |
+|          | Active | Status | Status   | Status | Config    | Actual    | Config Group | Actual Group | Config Role | Actual Role | Config Role | Actual Role | Config Roles | Actual Roles |
+|          |        |        |          |        | Partition | Partition |              |              |             |             |             |             |              |              |
+| -------- | ------ | ------ | -------- | ------ | --------- | --------- | ------------ | ------------ | ----------- | ----------- | ----------- | ----------- | ------------ | ------------ |
+| do_not_care_1 | yes    | ok     |          |        |         3 |         3 | default      | default      | slave       | slave       | foobar      | slave       | foobar       | foobar |
+| do_not_care_2 | yes    | info   |          |        |         0 |         8 | default      | default      | master 2    | slave       | standby     | slave       | standby      | foobar |
+| myServer | yes    | ok     |          |        |         4 |         4 | default      | default      | slave       | slave       | foobar      | slave       | foobar       | worker |
+output
+}
+
+sim_landscape_1_0_not_worker() {
+    cat <<output
+| Host     | Host   | Host   | Failover | Remove | Storage   | Storage   | Failover     | Failover     | NameServer  | NameServer  | IndexServer | IndexServer | Host         | Host         |
+|          | Active | Status | Status   | Status | Config    | Actual    | Config Group | Actual Group | Config Role | Actual Role | Config Role | Actual Role | Config Roles | Actual Roles |
+|          |        |        |          |        | Partition | Partition |              |              |             |             |             |             |              |              |
+| -------- | ------ | ------ | -------- | ------ | --------- | --------- | ------------ | ------------ | ----------- | ----------- | ----------- | ----------- | ------------ | ------------ |
+| do_not_care_1 | yes    | ok     |          |        |         3 |         3 | default      | default      | slave       | slave       | foobar      | slave       | foobar       | foobar |
+| do_not_care_2 | yes    | info   |          |        |         0 |         8 | default      | default      | master 2    | slave       | standby     | slave       | standby      | foobar |
+| myServer | yes    | ok     |          |        |         4 |         4 | default      | default      | slave       | slave       | foobar      | slave       | foobar       | not-worker |
+output
+}
+
+sim_landscape_2_0_worker() {
+    cat <<output
+| Host          | Host   | Host   | Failover | Remove | Storage   | Storage   | Failover | Failover | NameServer | NameServer | IndexServer | IndexServer | Host    | Host    | Worker  | Worker  |
+|               | Active | Status | Status   | Status | Config    | Actual    | Config   | Actual   | Config     | Actual     | Config      | Actual      | Config  | Actual  | Config  | Actual  |
+|               |        |        |          |        | Partition | Partition | Group    | Group    | Role       | Role       | Role        | Role        | Roles   | Roles   | Groups  | Groups  |
+| ------------- | ------ | ------ | -------- | ------ | --------- | --------- | -------- | -------- | ---------- | ---------- | ----------- | ----------- | ------- | ------- | ------- | ------- |
+| myServer | yes    | ok     |          |        |         1 |         1 | default  | default  | master 1   | master     | foobar      | master      | foobar | worker | default | default |
+| do_not_care_1 | yes    | ok     |          |        |         2 |         2 | default  | default  | master 2   | slave      | foobar      | slave       | foobar | foobar | default | default |
+| do_not_care_2 | no     | ignore |          |        |         0 |         0 | default  | default  | master 3   | slave      | standby     | standby     | standby | standby | default | -       |
+output
+}
+
+sim_landscape_2_0_not_worker() {
+    cat <<output
+| Host          | Host   | Host   | Failover | Remove | Storage   | Storage   | Failover | Failover | NameServer | NameServer | IndexServer | IndexServer | Host    | Host    | Worker  | Worker  |
+|               | Active | Status | Status   | Status | Config    | Actual    | Config   | Actual   | Config     | Actual     | Config      | Actual      | Config  | Actual  | Config  | Actual  |
+|               |        |        |          |        | Partition | Partition | Group    | Group    | Role       | Role       | Role        | Role        | Roles   | Roles   | Groups  | Groups  |
+| ------------- | ------ | ------ | -------- | ------ | --------- | --------- | -------- | -------- | ---------- | ---------- | ----------- | ----------- | ------- | ------- | ------- | ------- |
+| myServer | yes    | ok     |          |        |         1 |         1 | default  | default  | master 1   | master     | foobar      | master      | foobar | not-worker | default | default |
+| do_not_care_1 | yes    | ok     |          |        |         2 |         2 | default  | default  | master 2   | slave      | foobar      | slave       | foobar | foobar | default | default |
+| do_not_care_2 | no     | ignore |          |        |         0 |         0 | default  | default  | master 3   | slave      | standby     | standby     | standby | standby | default | -       |
+output
+
+}
+
+
+sim_odbcreg_1_0_OK() {
+    cat <<out
+
+ODBC Driver test.
+
+Connect string: 'SERVERNODE=Hana-host:3inst15;SERVERDB=BAS;UID=MyName;PWD=MyPass;'.
+retcode:         0
+outString(68):  SERVERNODE={Hana-host:3inst15};SERVERDB=BAS;UID=MyName;PWD=MyPass;
+Driver version SAP HDB 1.00 (2013-10-15).
+Select now(): 2013-11-12 15:44:55.272000000 (29)
+out
+}
+
+sim_odbcreg_2_0_OK() {
+    cat <<out
+
+ODBC Driver test.
+
+Connect string: 'SERVERNODE=Hana-host:3inst13;SERVERDB=BAS;UID=MyName;PWD=MyPass;'.
+retcode:         0
+outString(68):  SERVERNODE={Hana-host:3inst13};SERVERDB=BAS;UID=MyName;PWD=MyPass;
+Driver version SAP HDB 1.00 (2013-10-15).
+Select now(): 2013-11-12 15:44:55.272000000 (29)
+out
 }
 
 #   .--tests---------------------------------------------------------------.
@@ -58,56 +136,44 @@ output
 
 test_mk_sap_hana_single_hostname () {
 
-    # Mock su / python call
-    su () {
-
-        if { [[ "$4"  == *"sid"* ]] && [[ "$2" == *"inst_user"* ]] && [[ "$4" == *"landscapeHostConfiguration.py"* ]]; }; then
-        cat <<output
+    # Mock landscape
+    landscape=$(cat <<output
 | Host |
 |      |
 |      |
 | ------------- |
-| lavdb10y04001 |
+| myServer |
 output
-        fi
-    }
+)
 
-    actual=$(sap_hana_internal_hostname "sid" "inst" "inst_user")
-    expected="lavdb10y04001"
+    actual=$(sap_hana_host_from_landscape  "$landscape")
+    expected="myServer"
     assertEquals "$expected" "$actual"
 
 }
 
 test_mk_sap_hana_multiple_hostnames () {
 
-    # Mock su / python call
-    su () {
-
-        if { [[ "$4"  == *"sid"* ]] && [[ "$2" == *"inst_user"* ]] && [[ "$4" == *"landscapeHostConfiguration.py"* ]]; }; then
-        cat <<output
+    # Mock landscape
+    landscape=$(cat <<output
 | Host |
 |      |
 |      |
 | ------------- |
-| lavdb10y04002 |
-| lavdb10y04001 |
+| do_not_care_1 |
+| myServer |
 output
-        fi
-    }
-
-    actual=$(sap_hana_internal_hostname "sid" "inst" "inst_user")
-    expected="lavdb10y04001"
+)
+    actual=$(sap_hana_host_from_landscape   "$landscape")
+    expected="myServer"
     assertEquals "$expected" "$actual"
 
 }
 
 test_mk_sap_hana_multiple_hostnames_non_active () {
 
-    # Mock su / python call
-    su () {
-
-        if { [[ "$4"  == *"sid"* ]] && [[ "$2" == *"inst_user"* ]] && [[ "$4" == *"landscapeHostConfiguration.py"* ]]; }; then
-        cat <<output
+    # Mock landscape
+    landscape=$(cat <<output
 | Host |
 |      |
 |      |
@@ -115,11 +181,115 @@ test_mk_sap_hana_multiple_hostnames_non_active () {
 | non-active-host1 |
 | non-active-host2 |
 output
+)
+
+    actual=$(sap_hana_host_from_landscape "$landscape")
+    expected=""
+    assertEquals "$expected" "$actual"
+
+}
+
+test_mk_sap_hana_1-0_get_role () {
+
+    # Mock landscape
+    landscape=$(sim_landscape_1_0_worker)
+
+    actual=$(sap_hana_role_from_landscape "$landscape" "myServer")
+    expected="worker"
+    assertEquals "$expected" "$actual"
+
+}
+
+test_mk_sap_hana_2-0_get_role () {
+
+    # Mock landscape
+    landscape=$(sim_landscape_2_0_worker)
+    actual=$(sap_hana_role_from_landscape "$landscape" "myServer")
+    expected="worker"
+    assertEquals "$expected" "$actual"
+
+}
+
+test_mk_sap_hana_1_connect_OK () {
+
+    # Mocks
+    landscape=$(sim_landscape_1_0_worker)
+    status="Version;;1.00.122.22.1543461992 (fa/hana1sp12)"
+    su () {
+        called_bin=$(echo "$4" | awk '{print $1}')
+        if [[ "$called_bin" != *"odbcreg"* ]]; then
+            # Ensure that we're called for odbcred
+            return
+        fi
+
+        port=$(echo "$4" | awk '{print $2}' | awk -F":" '{print $2}')
+        if [ "$port" = "3inst15" ];then
+            # Ensure that we're called with the correct port
+            sim_odbcreg_1_0_OK
         fi
     }
 
-    actual=$(sap_hana_internal_hostname "sid" "inst" "inst_user")
-    expected=""
+    actual=$(sap_hana_connect "sid" "inst" "inst_user" "myServer" "$landscape" "$status")
+    expected=$(sim_odbcreg_1_0_OK | tr ';' ','  |  tr '\n' ';' | sed -e "s/^;//g" -e "s/;$//g")
+    assertEquals "$expected" "$actual"
+}
+
+test_mk_sap_hana_1_connect_standby_not_worker () {
+
+    # Mocks
+    landscape=$(sim_landscape_1_0_not_worker)
+    status="Version;;1.00.122.22.1543461992 (fa/hana1sp12)"
+
+    actual=$(sap_hana_connect "sid" "inst" "inst_user" "myServer" "$landscape" "$status")
+    expected="retcode: 1"
+    assertEquals "$expected" "$actual"
+}
+
+test_mk_sap_hana_2_connect_OK () {
+
+    # Mocks
+    landscape=$(sim_landscape_2_0_worker)
+    status="Version;;2.00.122.22.1543461992 (fa/hana1sp12)"
+    su () {
+        called_bin=$(echo "$4" | awk '{print $1}')
+        if [[ "$called_bin" != *"odbcreg"* ]]; then
+            # Ensure that we're called for odbcred
+            return
+        fi
+
+        # Ensure that we're called with the correct port
+        port=$(echo "$4" | awk '{print $2}' | awk -F":" '{print $2}')
+        if [ "$port" = "3inst13" ];then
+            sim_odbcreg_2_0_OK
+        fi
+    }
+
+    actual=$(sap_hana_connect "sid" "inst" "inst_user" "myServer" "$landscape" "$status")
+    expected=$(sim_odbcreg_2_0_OK | tr ';' ','  |  tr '\n' ';' | sed -e "s/^;//g" -e "s/;$//g")
+
+    assertEquals "$expected" "$actual"
+}
+
+test_mk_sap_hana_2_connect_standby_not_worker () {
+
+    # Mocks
+    landscape=$(sim_landscape_2_0_not_worker)
+    status="Version;;2.00.122.22.1543461992 (fa/hana1sp12)"
+
+    actual=$(sap_hana_connect "sid" "inst" "inst_user" "myServer" "$landscape" "$status")
+    expected="retcode: 1"
+
+    assertEquals "$expected" "$actual"
+}
+
+test_mk_sap_hana_unknown_version() {
+
+    # Mocks
+    status="Version;;9.22.122.22.123445 (fa/hanaUNKNOWN)"
+
+    actual=$(sap_hana_connect "sid" "inst" "inst_user" "do not care" "do not care" "$status")
+    expected="Cannot determine port due to unknown HANA version."
+
     assertEquals "$expected" "$actual"
 
 }

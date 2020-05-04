a2fd74ab69e1673b2f8566517cd1b01d9a1e9507
Lars Michelsen <lm@tribe29.com>
2020-05-04 19:53:47 +0200

    Revert "Trying to fix unstable test_generate_snapshot tests"
    This reverts commit 0a53b0557e69c5052c0caae4d55aedf1f11e4486.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -37,7 +37,6 @@ def patch_omd_site(monkeypatch):
     store.makedirs(cmk.utils.paths.default_config_dir + '/conf.d/wato')
     store.makedirs(cmk.utils.paths.default_config_dir + '/multisite.d/wato')
     store.makedirs(cmk.utils.paths.default_config_dir + '/mkeventd.d/wato')
-    store.makedirs(cmk.utils.paths.default_config_dir + '/mkeventd.d/mkp/rule_packs')
     _touch(cmk.utils.paths.default_config_dir + '/mkeventd.mk')
 
 

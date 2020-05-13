7ceede416b890f3e6bc98a3fa69b1d8f6ea82e65
Joerg Herbel <joerg.herbel@tribe29.com>
2020-05-13 10:40:08 +0200

    11096 hyperv_vms: use default levels instead of discovered state
    The check hyperv_vms now uses a set of default levels to map the
    state of VMs to monitoring states. Before, by default, the check
    compared the current VM state against the state at the point when
    the service was discovered and went CRIT if the two states did not
    match. This behavior is still available, but the check now has to
    be specifically configured to do this, see the rule "State of
    Microsoft Hyper-V Server VMs".
    
    Furthermore, the check now always works when executed as a manual
    check. Before, it might have crashed in this case, depending on
    the used configuration.
    
    This change is incompatible in the sense that users have to adjust
    the rules for this check to make it behave the way it did before.
    
    Change-Id: Id4864f2f5dae5d472d87ba0596498c0f82391644

--- tests-py3/unit/checks/generictests/datasets/hyperv_vms.py
+++ tests-py3/unit/checks/generictests/datasets/hyperv_vms.py
@@ -7,38 +7,75 @@
 # yapf: disable
 # type: ignore
 
+_defaults = {'FastSaved': 0,
+             'FastSavedCritical': 2,
+             'FastSaving': 0,
+             'FastSavingCritical': 2,
+             'Off': 1,
+             'OffCritical': 2,
+             'Other': 3,
+             'Paused': 0,
+             'PausedCritical': 2,
+             'Pausing': 0,
+             'PausingCritical': 2,
+             'Reset': 1,
+             'ResetCritical': 2,
+             'Resuming': 0,
+             'ResumingCritical': 2,
+             'Running': 0,
+             'RunningCritical': 2,
+             'Saved': 0,
+             'SavedCritical': 2,
+             'Saving': 0,
+             'SavingCritical': 2,
+             'Starting': 0,
+             'StartingCritical': 2,
+             'Stopping': 1,
+             'StoppingCritical': 2}
+
 
 checkname = 'hyperv_vms'
 
 
-info = [[u'"Name"', u'"State"', u'"Uptime"', u'"Status"'],
-        [u'"AUN-CAA"', u'"Off"', u'"00:00:00"', u'"Operating normally"'],
+info = [['"Name"', '"State"', '"Uptime"', '"Status"'],
         ['Q-WSUS', 'Running', '4.21:44:29', 'Operating normally'],
-        ['weg-ca-webserver', 'Off', '00:00:00', 'Operating normally'],
-        ['z4058044_snap (23.05.2014 - 09:29:29)',
-         'Running',
-         '18:20:34',
-         'Operating normally'],
+        ['"AUN-CAA"', '"Off"', '"00:00:00"', '"Operating normally"'],
+        ['weg-ca-webserver', 'Wrong', '00:00:00', 'Operating normally'],
+        ['z4058044_snap (23.05.2014 - 09:29:29)', 'Running', '18:20:34', 'Operating normally'],
+        ['z230897', 'Stopping', '18:20:34', 'VM crashed'],
+        ['hlv2', 'UnknownState', '00:00:00', 'Totally normal'],
+        ['hlv3', 'Running', '00:00:00', 'Operating normally'],
         ['& : File C:\\Program Files (x86)\\check_mk\\plugins\\windows_os_bonding.ps1 cannot'],
-        [''],
-]
+        ['']]
 
 
 discovery = {'': [('Q-WSUS', {'state': 'Running'}),
-                  ('weg-ca-webserver', {'state': 'Off'}),
+                  ('AUN-CAA', {'state': 'Off'}),
+                  ('weg-ca-webserver', {'state': 'Wrong'}),
                   ('z4058044_snap (23.05.2014 - 09:29:29)', {'state': 'Running'}),
-                  (u'AUN-CAA', {'state': u'Off'})]}
+                  ('z230897', {'state': 'Stopping'}),
+                  ('hlv2', {'state': 'UnknownState'}),
+                  ('hlv3', {'state': 'Running'})]}
 
 
 checks = {'': [('Q-WSUS',
-                {'state': 'Running'},
-                [(0, 'State Running (Operating normally) matches discovery', [])]),
+                {'state': 'Running', **_defaults},
+                [(0, 'State is Running (Operating normally)')]),
+               ('AUN-CAA',
+                {'state': 'Off', **_defaults},
+                [(1, 'State is Off (Operating normally)')]),
                ('weg-ca-webserver',
-                {'state': 'Off'},
-                [(0, 'State Off (Operating normally) matches discovery', [])]),
+                {'state': 'Wrong', **_defaults},
+                [(3, 'Unknown state Wrong (Operating normally)')]),
                ('z4058044_snap (23.05.2014 - 09:29:29)',
-                {'state': 'Paused', 'Off': 1},
-                [(2, 'State Running (Operating normally) does not match discovery (Paused)', [])]),
-               (u'AUN-CAA',
-                {'state': u'Off', 'Off': 2},
-                [(2, u'State is Off (Operating normally)', [])])]}
+                {'state': 'Running', **_defaults, 'compare_discovery': True},
+                [(0, 'State Running (Operating normally) matches discovery')]),
+               ('z230897',
+                {'state': 'Running', **_defaults, 'compare_discovery': True},
+                [(2, 'State Stopping (VM crashed) does not match discovery (Running)')]),
+               ('hlv2',
+                {'state': 'UnknownState', **_defaults, 'compare_discovery': True},
+                [(0, 'State UnknownState (Totally normal) matches discovery')]),
+               ('hlv3',
+                {**_defaults, 'compare_discovery': True},
+                [(3, 'State is Running (Operating normally), discovery state is not available')])]}

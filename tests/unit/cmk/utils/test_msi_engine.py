511dc9c2bf3a954834c3b2b964c17f289a1f0443
Sven Panne <sven.panne@tribe29.com>
2020-05-13 18:03:01 +0200

    Even more symlinking.
    Change-Id: I37cd14fadf33ded1e8b1313341b9f680707cb292

---
+++ tests/unit/cmk/utils/test_msi_engine.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# pylint: disable=redefined-outer-name
+
+import pytest  # type: ignore[import] # pylint: disable=unused-import
+
+import cmk.utils.msi_engine as msi_engine
+
+
+def test_parse_command_line():
+    msi_file, source_dir, revision, version_name, aghash = msi_engine.parse_command_line(
+        ["stub", "msi", "dir", "rev", "vers", "aghash"])
+    assert msi_file == "msi"
+    assert source_dir == "dir"
+    assert revision == "rev"
+    assert version_name == "vers"
+    assert aghash == "aghash"
+    assert not msi_engine.opt_verbose
+    msi_file, source_dir, revision, version_name, aghash = msi_engine.parse_command_line(
+        ["stub", "-v", "msi", "dir", "rev", "vers", "aghash"])
+    assert msi_file == "msi"
+    assert source_dir == "dir"
+    assert revision == "rev"
+    assert version_name == "vers"
+    assert aghash == "aghash"
+    assert msi_engine.opt_verbose
+
+
+EXPECTED_FILE_TABLE = ["check_mk_install_yml", "checkmk.dat", "plugins_cap", "python_3.8.zip"]
+
+EXPECTED_COMPONENT_TABLE = [
+    "check_mk_install_yml_", "checkmk.dat", "plugins_cap_", "python_3.8.zip"
+]
+
+
+# check of constants: we do noy want to break build after soem refactoring, renaming or typo-fix
+def test_msi_tables():
+    assert msi_engine.msi_file_table() == EXPECTED_FILE_TABLE
+    assert msi_engine.msi_component_table() == EXPECTED_COMPONENT_TABLE
+
+
+def test_msi_file_table():
+    a = msi_engine.msi_file_table()
+    assert len(a) == len(EXPECTED_FILE_TABLE)  # size for now(yml, dat & cap, zip)
+    a_sorted = sorted(a)
+    assert a == a_sorted  # array should be sorted
+
+
+def test_msi_component_table():
+    a = msi_engine.msi_component_table()
+    assert len(a) == len(EXPECTED_COMPONENT_TABLE)  # size now(yml, dat & cap, zip)
+    a_sorted = sorted(a)
+    assert a == a_sorted  # array should be sorted

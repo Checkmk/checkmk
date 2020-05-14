9c185727fa3ce2a08f3552671ea9beef7ca8885f
Lars Michelsen <lm@tribe29.com>
2020-05-14 16:02:00 +0200

    Add Dynamic Configuration connector API to plugin API doc
    Change-Id: I9c8ff750d7825c16b1fea84da884b482ce2a154f

--- doc/plugin-api/source/conf.py
+++ doc/plugin-api/source/conf.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../../enterprise/cmk/cee/dcd/plugins/connectors/'))
 sys.path.insert(0, os.path.abspath('../../../cmk/base/plugins/agent_based/'))
 sys.path.insert(0, os.path.abspath('../../../'))
 

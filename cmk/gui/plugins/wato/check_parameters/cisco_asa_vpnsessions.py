010c72f22aae290909ca06261d3415e6dca40351
thl-cmk <thl-cmk@outlook.com>
2020-05-20 20:00:40 +0000

    fixed metrics clientless, moved wato to cmk/gui/plugins/wato/check_parameters, removed cmk/gui/plugins/wato/check_parameters/asa_svc_sessions.py

---
+++ cmk/gui/plugins/wato/check_parameters/cisco_asa_vpnsessions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: GNU General Public License v2
+#
+# Author : thl-cmk[at]outlook[dot]com
+# Date   : 2020-04-27
+# Content: wato plugin for snmp check 'cisco_asa_vpnsessions'
+#          to configure waring/critical levels
+#
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Integer,
+    TextAscii,
+    Tuple,
+)
+
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersNetworking,
+)
+
+
+def _parameter_valuespec_cisco_asa_vpnsessions():
+    return Dictionary(elements=[
+        ('WarnCrit',
+         Tuple(
+             title=_('Number of active sessions'),
+             help=_('This check monitors the number of active/peak sessions'),
+             elements=[
+                 Integer(title=_('Warning at'), unit=_('sessions'), default_value=10),
+                 Integer(title=_('Critical at'), unit=_('sessions'), default_value=100),
+             ],
+         )),
+    ],)
+
+
+def _item_spec_cisco_asa_vpnsessions():
+    return TextAscii(
+        title=_('VPN session type'),
+        help=_('One of AnyConnect, Clientless, IPSec L2L or IPSec RA. '),
+        allow_empty=False,
+    )
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name='cisco_asa_vpnsessions',
+        group=RulespecGroupCheckParametersNetworking,
+        item_spec=_item_spec_cisco_asa_vpnsessions,
+        match_type='dict',
+        parameter_valuespec=_parameter_valuespec_cisco_asa_vpnsessions,
+        title=lambda: _('Cisco ASA VPN sessions'),
+    ))

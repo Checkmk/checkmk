97384d6a70b3b0687b75af9d573d88ff134f0eac
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-05-26 09:12:53 +0200

    inventory: migrate all sections IX
    Change-Id: I63248dce3fa7d3799e55d5cb1f1ca377ed5626fb

--- cmk/base/plugins/agent_based/checkpoint_vpn_tunnels.py
+++ cmk/base/plugins/agent_based/checkpoint_vpn_tunnels.py
@@ -3,27 +3,28 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from cmk.base.plugins.agent_based.agent_based_api.v0 import (
-    contains,
-    OIDEnd,
+from .agent_based_api.v0 import (
     parse_string_table,
     register,
     SNMPTree,
 )
 
+from .utils import checkpoint
+
 register.snmp_section(
-    name="cisco_vlans",
+    name="checkpoint_inv_tunnels",
     parse_function=parse_string_table,
     trees=[
         SNMPTree(
-            base=".1.3.6.1.4.1.9.9.68.1.2.2.1",
+            base=".1.3.6.1.4.1.2620.500.9002.1",
             oids=[
-                OIDEnd(),
-                "1",  # vmVlanType
-                "2",  # vmVlan
-                "4",  # vmVlans
+                "1",  # tunnelPeerIpAddr
+                "7",  # tunnelSourceIpAddr
+                "2",  # tunnelPeerObjName
+                "6",  # tunnelInterface
+                "8"  # tunnelLinkPriority
             ],
         ),
     ],
-    detect=contains(".1.3.6.1.2.1.1.1.0", "cisco"),
+    detect=checkpoint.DETECT,
 )
---
+++ cmk/base/plugins/agent_based/inv_cisco_vlans.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+from cmk.base.plugins.agent_based.agent_based_api.v0 import (
+    contains,
+    OIDEnd,
+    parse_string_table,
+    register,
+    SNMPTree,
+)
+
+register.snmp_section(
+    name="inv_cisco_vlans",
+    parse_function=parse_string_table,
+    trees=[
+        SNMPTree(
+            base=".1.3.6.1.4.1.9.9.68.1.2.2.1",
+            oids=[
+                OIDEnd(),
+                "1",  # vmVlanType
+                "2",  # vmVlan
+                "4",  # vmVlans
+            ],
+        ),
+    ],
+    detect=contains(".1.3.6.1.2.1.1.1.0", "cisco"),
+)

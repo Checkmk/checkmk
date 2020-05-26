f71f23cdc7b7efedb0c1eaad6b7b1e03b33499b1
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-05-26 09:37:30 +0200

    Fixed cherry-pick error in Windows Agent.
    Change-Id: I55bd753a159924bc8dc185a29fb20aaec702b26b

--- agents/wnx/src/engine/cap.cpp
+++ agents/wnx/src/engine/cap.cpp
@@ -344,7 +344,7 @@ bool Process(const std::string &cap_name, ProcMode Mode,
             return false;
         }
         if (data.empty()) {
-            XLOG::l.w("CAP file {} has emty file file {}", CapFileName, name);
+            XLOG::l.w("CAP file {} has emty file file {}", cap_name, name);
         }
         const auto full_path = ProcessPluginPath(name);
 

42af2535324e5ef3ef560bc72c4430e7e2518569
Lars Michelsen <lm@tribe29.com>
2020-05-27 16:32:27 +0200

    Update Python environment integration tests
    Change-Id: I4114118522340f482d9481901655326b871d1c30

--- tests-py3/integration/omd/test_python.py
+++ tests-py3/integration/omd/test_python.py
@@ -12,28 +12,28 @@ import pytest  # type: ignore[import]
 
 
 def test_01_python_interpreter_exists(site):
-    assert os.path.exists(site.root + "/bin/python")
+    assert os.path.exists(site.root + "/bin/python3")
 
 
 def test_02_python_interpreter_path(site):
     p = site.execute(["which", "python"], stdout=subprocess.PIPE)
     path = p.stdout.read().strip()
-    assert path == "/omd/sites/%s/bin/python" % site.id
+    assert path == "/omd/sites/%s/bin/python3" % site.id
 
 
 def test_03_python_interpreter_version(site):
     p = site.execute(["python", "-V"], stderr=subprocess.PIPE)
     version = p.stderr.read()
-    assert version.startswith("Python 2.7.17")
+    assert version.startswith("Python 3.7.4")
 
 
 def test_03_python_path(site):
     p = site.execute(["python", "-c", "import sys ; print(sys.path)"], stdout=subprocess.PIPE)
     sys_path = eval(p.stdout.read())
     assert sys_path[0] == ""
-    assert site.root + "/local/lib/python" in sys_path
-    assert site.root + "/lib/python" in sys_path
-    assert site.root + "/lib/python2.7" in sys_path
+    assert site.root + "/local/lib/python3" in sys_path
+    assert site.root + "/lib/python3" in sys_path
+    assert site.root + "/lib/python3.7" in sys_path
 
     for p in sys_path:
         if p != "" and not p.startswith(site.root):
@@ -41,11 +41,11 @@ def test_03_python_path(site):
 
 
 def test_01_pip_exists(site):
-    assert os.path.exists(site.root + "/bin/pip")
+    assert os.path.exists(site.root + "/bin/pip3")
 
 
 def test_02_pip_path(site):
-    p = site.execute(["which", "pip"], stdout=subprocess.PIPE)
+    p = site.execute(["which", "pip3"], stdout=subprocess.PIPE)
     path = p.stdout.read().strip()
     assert path == "/omd/sites/%s/bin/pip" % site.id
 
@@ -53,7 +53,7 @@ def test_02_pip_path(site):
 def test_03_pip_interpreter_version(site):
     p = site.execute(["pip", "-V"], stdout=subprocess.PIPE)
     version = p.stdout.read()
-    assert version.startswith("pip 19.2.3")
+    assert version.startswith("pip 19.0.3")
 
 
 # TODO: Improve this test to automatically adapt the expected modules from our Pipfile

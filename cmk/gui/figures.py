8b777de0fc3f5818607319ed392902b5dc1ad5f1
Andreas Boesl <andreas.boesl@tribe29.com>
2020-05-29 09:55:02 +0200

    NTOP: figures.py: added TableFigureDataCreator, creates table configs for javascript tables
    Change-Id: Iccc2ca17901a28efe49a4ebdef231714763fda3f

--- cmk/gui/figures.py
+++ cmk/gui/figures.py
@@ -6,7 +6,7 @@
 
 # Basic functions for cmk_figures
 
-from typing import Type
+from typing import Type  # pylint: disable=unused-import
 import abc
 import json
 
@@ -102,3 +102,78 @@ class ABCFigureDashlet(Dashlet, metaclass=abc.ABCMeta):
 
     def on_resize(self):
         return "%s.resize()" % self.instance_name
+
+
+class TableFigureDataCreator(object):
+    """ Helps to create the data shown in a table_figure.js figure
+        The javascript TableFigure supports partial data updates and the integration of dc.js pie charts within cells
+        {
+          "classes": ["classes", "for", "table"],              // classes for this table
+          "rows": [
+             [ {                                               // Dict, representing a row
+                 "classes": ["classes", "for", "row"],         // classes for this row
+                 "cells": [ {                                  // Dict, representing a cell. All keys are optional
+                   "text": "Text to display",
+                   "html": "HTML to to display",               // overrules "text" key
+                   "cell_type": "td"                           // supports td and th
+                   "classes": ["classes", "for", "cell"],      // classes for this cell
+                   "rowspan": 1,
+                   "colspan": 3,
+                   "figure_config": {figure_definition}        // cell with figure config, including data
+                 } ]
+             ], ...
+             }
+          ]
+        }
+    """
+    @classmethod
+    def get_text_cell(cls, text, attrs=None, classes=None):
+        cell = {"text": text}
+        cell.update(cls.compute_attr_and_classes(attrs, classes))
+        return cell
+
+    @classmethod
+    def get_html_cell(cls, html_content, attrs=None, classes=None):
+        cell = {"html": html_content}
+        cell.update(cls.compute_attr_and_classes(attrs, classes))
+        return cell
+
+    # Cell with text or html content, only used for classes and attr
+    # This cell is generally used by graphs
+    @classmethod
+    def get_empty_cell(cls, attrs=None, classes=None):
+        return cls.compute_attr_and_classes(attrs, classes)
+
+    @classmethod
+    def compute_attr_and_classes(cls, attrs, classes):
+        extra = {}
+        attrs = {} if attrs is None else attrs
+        for key, value in attrs.items():
+            extra[key] = value
+        if classes:
+            extra["classes"] = classes
+        return extra
+
+    # Cell which contains a dc graphing element
+    @classmethod
+    def get_figure_cell(cls, figure_config, attrs=None, classes=None):
+        classes = classes or []
+        classes.append("figure_cell")
+        attrs = attrs or {}
+        attrs["id"] = figure_config["id"]
+        cell = cls.get_empty_cell(attrs, classes)
+        figure_config["selector"] = "#%s" % figure_config["id"]
+        cell["figure_config"] = figure_config
+        return cell
+
+    @classmethod
+    def get_pie_chart_config(cls, attr_id, data, title=None):
+        """ Generates the configuration for a pie chart diagram
+            The actual percentage values are calculated in js
+            [
+              {"label1": "test", "value": 23},
+              {"label2": "test2, "value": 42}
+                ....
+            ]
+        """
+        return {"id": attr_id, "type": "pie_chart", "title": title, "data": data}

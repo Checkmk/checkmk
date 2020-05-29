35447539fe5a5e75a54c8e423db24f4a5f60f0e1
Sven Panne <sven.panne@tribe29.com>
2020-05-29 10:14:54 +0200

    Revert "Make write(2) non-blocking"
    This reverts commit 1a2928486c805961b1302464038dd140946d2fea.  It is an
    attempt to fix the problem where ICMPSender::readErrorMessages() goes into
    an infinite loop when omd is started via systemd.  If this loop really goes
    away, we have to rework the reverted fix and re-land it.
    
    Change-Id: Iaa7ce60dc58b2b23ab37635e83a96e70f4f2c552

--- livestatus/src/OutputBuffer.cc
+++ livestatus/src/OutputBuffer.cc
@@ -30,7 +30,6 @@
 #include <iomanip>
 #include "Logger.h"
 #include "Poller.h"
-#include "SocketUtils.h"
 
 OutputBuffer::OutputBuffer(int fd, const bool &termination_flag, Logger *logger)
     : _fd(fd)
@@ -69,7 +68,6 @@ void OutputBuffer::writeData(std::ostringstream &os) {
     };
     const char *buffer = static_cast<Hack *>(os.rdbuf())->base();
     size_t bytes_to_write = os.tellp();
-    auto nb = NonBlocking{_fd};
     while (!shouldTerminate() && bytes_to_write > 0) {
         if (!Poller{}.wait(std::chrono::milliseconds(100), _fd, PollEvents::out,
                            _logger)) {
@@ -78,19 +76,16 @@ void OutputBuffer::writeData(std::ostringstream &os) {
             }
             break;
         }
-        const ssize_t written = ::write(_fd, buffer, bytes_to_write);
-        if (written >= 0) {
-            buffer += written;
-            bytes_to_write -= written;
-        } else if (written == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
-            continue;
-        } else {
+        ssize_t bytes_written = write(_fd, buffer, bytes_to_write);
+        if (bytes_written == -1) {
             generic_error ge("could not write " +
                              std::to_string(bytes_to_write) +
                              " bytes to client socket");
             Informational(_logger) << ge;
             break;
         }
+        buffer += bytes_written;
+        bytes_to_write -= bytes_written;
     }
 }
 

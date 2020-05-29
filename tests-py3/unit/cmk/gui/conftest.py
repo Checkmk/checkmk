8b0e8f81d1dd5eb1c6db0bf57cd6d2d30c91db54
Lars Michelsen <lm@tribe29.com>
2020-05-29 08:19:15 +0200

    Fix wrong shebang
    Change-Id: I9c7e7d473bf88623452315c717311edea885241e

--- tests-py3/unit/cmk/gui/conftest.py
+++ tests-py3/unit/cmk/gui/conftest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
@@ -95,7 +95,7 @@ def _mk_user_obj(username, password, automation=False):
             },
             'is_new_user': True,
         }
-    }
+    }  # type: dict
     if automation:
         user[username]['attributes'].update(automation_secret=password,)
     return user

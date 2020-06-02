a644cca34f3a50bd0c69b2612fa36ba747d196c8
Lars Michelsen <lm@tribe29.com>
2020-06-02 21:11:51 +0200

    Remove add-apt-repository dependency
    Change-Id: Ie1cca170f89c11dd44be816ec0560c97778baebc

--- buildscripts/infrastructure/build-nodes/scripts/install-docker.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-docker.sh
@@ -11,7 +11,7 @@ ubuntu-*)
 
     # Install docker software
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >/etc/apt/sources.list.d/docker.list
     apt-get update
     apt-get install -y docker-ce
     rm -rf /var/lib/apt/lists/*

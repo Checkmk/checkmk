e5ec4c6b786b0d21fffc0c59238059a6ebb52ffa
Lars Michelsen <lm@tribe29.com>
2020-06-02 21:02:15 +0200

    Add docker to the 19.04 image to be able to fully replace the ubunut-common image
    Change-Id: I03543eed4a9f1ff757d9556f6ce7997e6e8ca8a4

---
+++ buildscripts/infrastructure/build-nodes/scripts/install-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+set -e -o pipefail
+
+case "$DISTRO" in
+ubuntu-*)
+    echo "Installing for Ubuntu"
+
+    # Install docker software
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get update
+    apt-get install -y docker-ce
+    rm -rf /var/lib/apt/lists/*
+
+    # This is needed to have access to the docker daemon socket that is exposed to the container
+    # when jobs are executed that need to control docker containers. Yes, this grants a lot of
+    # permissions to the job...
+    groupadd -g 999 docker
+
+    exit 0
+    ;;
+*)
+    echo "ERROR: Unhandled DISTRO: $DISTRO"
+    exit 1
+    ;;
+esac

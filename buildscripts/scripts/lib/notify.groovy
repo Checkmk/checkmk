f18e3e5e6746b1d8d0316ded73147a4152e89c1c
Alex Zurhake <alex.zurhake@tribe29.com>
2020-06-02 20:39:15 +0200

    Notify via Mail
    Change-Id: I940608626443bd6401e7d3c049e4c1c730b09318

--- buildscripts/scripts/lib/notify.groovy
+++ buildscripts/scripts/lib/notify.groovy
@@ -1,5 +1,8 @@
 // library for simple string modifications
 package lib
+// Workaround since CHANGE_AUTHOR_EMAIL is not available
+// Bug: https://issues.jenkins-ci.org/browse/JENKINS-39838
+AUTHOR_MAIL = sh(script: "git log -1 --pretty=format:%ae", returnStdout: true).toString().trim()
 
 
 // Send a build failed massage to jenkins
@@ -17,7 +20,27 @@ Error Message:
     )
 }
 
+// Send a build failed message via mail
+def mail_build_failed(error) {
+    mail(
+        to: AUTHOR_MAIL,
+        cc: '',
+        bcc: '',
+        from: JENKINS_MAIL,
+        replyTo: '',
+        subject: "Error in ${env.JOB_NAME}",
+        body: """
+Build Failed:
+    ${env.JOB_NAME} ${env.BUILD_NUMBER}
+    ${env.BUILD_URL}
+Error Message:
+    ${error}
+                """
+   )
+}
+
 def notify_error(error) {
+    mail_build_failed(error)
     slack_build_failed(error)
     // after notifying everybody, the error needs to be thrown again
     // This ensures that the build status is set correctly

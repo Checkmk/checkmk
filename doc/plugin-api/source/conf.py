15770a5c01359d335ce5a0e5364d60ae53eb4ad0
Lars Michelsen <lm@tribe29.com>
2020-06-03 21:12:39 +0200

    Improve Plugin API theming
    Change-Id: I31d1f56e6ddb91202e30763884512628fc997d9d

--- doc/plugin-api/source/conf.py
+++ doc/plugin-api/source/conf.py
@@ -30,6 +30,7 @@ author = 'tribe29'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx_autodoc_typehints',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,7 +50,12 @@ add_module_names = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Do not render the "View page source" links on all doc pages
+html_show_sourcelink = False
+
+# Theme specific options (see https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html)
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

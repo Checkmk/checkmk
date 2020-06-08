6af592b9272803327b137e694ef888ed006c3705
Frans Fürst <frans.fuerst@tribe29.com>
2020-06-08 14:24:00 +0200

    11162 Agent and check plugins for zorp connections
    New agent and check plugin to monitor the total and active connections of Zorp firewalls.
    Levels for the total number of connections can be defined in WATO.
    
    Change-Id: Ifbb2e91fab2e48f236353498b4468e86c727c40f

---
+++ cmk/gui/plugins/wato/check_parameters/zorp_connections_params.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Tuple,
+    Integer,
+)
+
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithoutItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersOperatingSystem,
+)
+
+
+def _parameter_valuespec_zorp_connections():
+    return Dictionary(
+        elements=[
+            ("levels",
+             Tuple(
+                 title=_("Levels"),
+                 elements=[
+                     Integer(
+                         title=_("Warning at"),
+                         default_value=15,
+                     ),
+                     Integer(
+                         title=_("Critical at"),
+                         default_value=20,
+                     ),
+                 ],
+             )),
+        ],
+        required_keys=['levels'],  # There is only one value, so its required
+    )
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithoutItem(
+        check_group_name="zorp_connections",
+        group=RulespecGroupCheckParametersOperatingSystem,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_zorp_connections,
+        title=lambda: _("Zorp connections"),
+    ))

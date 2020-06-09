908619cc7ddf3720b227d25a101048ad8dd81cae
Joerg Herbel <joerg.herbel@tribe29.com>
2020-06-09 10:00:16 +0200

    11159 Grouping of NetApp Volumes
    NetApp volumes collected by the NetApp special agent, which are monitored
    by the check <tt>netapp_api_volumes<\tt>, can now be grouped using the rule
    "Filesystem grouping patterns". This allows for combining multiple volumes into
    a single service. Services corresponding to grouped volumes deliver the same
    performance data as individual volumes and are also configurable via the rule
    "NetApp Volumes".
    
    This werk is incompatible in the sense that it can result in unwanted grouping
    of NetApp volumes. This will happen if user-defined rules for the grouping of
    filesystems match services monitoring NetApp volumes. In this case, users have
    to refine the corresponding rules.
    
    Change-Id: I3a0d3b122d4f8a025df0278b32a6a1b724dbd079

--- tests-py3/unit/checks/generictests/datasets/netapp_api_volumes.py
+++ tests-py3/unit/checks/generictests/datasets/netapp_api_volumes.py
@@ -72,7 +72,109 @@ info = [
      'read_data 808349225', 'cifs_read_ops 1642', 'write_latency 1096990',
      'cifs_read_data 736920818', 'nfs_read_data 0', 'write_ops 3864', 'nfs_write_data 0',
      'cifs_write_latency 0'],
+    ['volume vol0', 'size-available 209570025472', 'state online', 'files-total 7782396',
+     'files-used 10737', 'size-total 228170137600', 'fcp_write_data 0', 'fcp_read_data 0',
+     'cifs_write_data 0', 'iscsi_read_latency 0', 'iscsi_write_data 0', 'read_data 788111488271',
+     'nfs_write_latency 108420', 'san_write_latency 0', 'san_write_data 0',
+     'read_latency 20135569973', 'cifs_read_latency 0', 'fcp_write_latency 0', 'fcp_read_latency 0',
+     'iscsi_write_latency 0', 'nfs_read_latency 19007164', 'iscsi_read_data 0',
+     'instance_name vol0', 'cifs_read_data 0', 'nfs_read_data 180684471',
+     'write_latency 127083421865', 'san_read_data 0', 'san_read_latency 0',
+     'write_data 470709801331', 'nfs_write_data 6121275', 'cifs_write_latency 0'],
+    ['volume vol_bronze1_fundorado_cifs', 'size-available 5681445965824', 'state online',
+     'files-total 31876689', 'files-used 3058888', 'size-total 9400824418304', 'fcp_write_data 0',
+     'fcp_read_data 0', 'cifs_write_data 5993355785148', 'iscsi_read_latency 0',
+     'iscsi_write_data 0', 'read_data 10083880638990', 'nfs_write_latency 0', 'san_write_latency 0',
+     'san_write_data 0', 'read_latency 62090615998', 'cifs_read_latency 59464480059',
+     'fcp_write_latency 0', 'fcp_read_latency 0', 'iscsi_write_latency 0', 'nfs_read_latency 0',
+     'iscsi_read_data 0', 'instance_name vol_bronze1_fundorado_cifs',
+     'cifs_read_data 9748984278714', 'nfs_read_data 0', 'write_latency 132180118822',
+     'san_read_data 0', 'san_read_latency 0', 'write_data 6300214716764', 'nfs_write_data 0',
+     'cifs_write_latency 128809541597'],
+    ['volume vol_bronze1_ftpfreenet_nfs', 'size-available 2348812120064', 'state online',
+     'files-total 31876689', 'files-used 2694615', 'size-total 4947802324992', 'fcp_write_data 0',
+     'fcp_read_data 0', 'cifs_write_data 0', 'iscsi_read_latency 0', 'iscsi_write_data 0',
+     'read_data 14495321097272', 'nfs_write_latency 436623773693', 'san_write_latency 0',
+     'san_write_data 0', 'read_latency 1321841542993', 'cifs_read_latency 0', 'fcp_write_latency 0',
+     'fcp_read_latency 0', 'iscsi_write_latency 0', 'nfs_read_latency 1318276162059',
+     'iscsi_read_data 0', 'instance_name vol_bronze1_ftpfreenet_nfs', 'cifs_read_data 0',
+     'nfs_read_data 14038673026364', 'write_latency 441267513699', 'san_read_data 0',
+     'san_read_latency 0', 'write_data 5516992855944', 'nfs_write_data 5106734367496',
+     'cifs_write_latency 0'],
+    ['volume vol_bronze1_nfs', 'size-available 18063153672192', 'state online',
+     'files-total 392536979', 'files-used 32824906', 'size-total 33425153486848',
+     'fcp_write_data 0', 'fcp_read_data 0', 'cifs_write_data 629525512283', 'iscsi_read_latency 0',
+     'iscsi_write_data 0', 'read_data 258198800160064', 'nfs_write_latency 3284753782325',
+     'san_write_latency 0', 'san_write_data 0', 'read_latency 18906485485409',
+     'cifs_read_latency 59911208593', 'fcp_write_latency 0', 'fcp_read_latency 0',
+     'iscsi_write_latency 0', 'nfs_read_latency 18738739813315', 'iscsi_read_data 0',
+     'instance_name vol_bronze1_nfs', 'cifs_read_data 1373882337514',
+     'nfs_read_data 245196278213658', 'write_latency 3412600142574', 'san_read_data 0',
+     'san_read_latency 0', 'write_data 241225507897086', 'nfs_write_data 229575392006609',
+     'cifs_write_latency 6374158509'],
+    ['volume vol_bronze1_enterprise_vault', 'size-available 6317505187840', 'state online',
+     'files-total 386818032', 'files-used 233640753', 'size-total 17757112791040',
+     'fcp_write_data 0', 'fcp_read_data 0', 'cifs_write_data 2797504453226', 'iscsi_read_latency 0',
+     'iscsi_write_data 0', 'read_data 13198309405534', 'nfs_write_latency 0', 'san_write_latency 0',
+     'san_write_data 0', 'read_latency 1332614234054', 'cifs_read_latency 1332014859907',
+     'fcp_write_latency 0', 'fcp_read_latency 0', 'iscsi_write_latency 0', 'nfs_read_latency 0',
+     'iscsi_read_data 0', 'instance_name vol_bronze1_enterprise_vault',
+     'cifs_read_data 13060307696514', 'nfs_read_data 0', 'write_latency 75431342425',
+     'san_read_data 0', 'san_read_latency 0', 'write_data 2935420540154', 'nfs_write_data 0',
+     'cifs_write_latency 73712224394'],
+    ['volume vol_bronze1_cifs', 'size-available 12137213788160', 'state online',
+     'files-total 31876689', 'files-used 10483961', 'size-total 16712576745472', 'fcp_write_data 0',
+     'fcp_read_data 0', 'cifs_write_data 6531283943261', 'iscsi_read_latency 0',
+     'iscsi_write_data 0', 'read_data 4002760800079', 'nfs_write_latency 13176712585',
+     'san_write_latency 0', 'san_write_data 0', 'read_latency 958566213067',
+     'cifs_read_latency 878519925020', 'fcp_write_latency 0', 'fcp_read_latency 0',
+     'iscsi_write_latency 0', 'nfs_read_latency 76274494693', 'iscsi_read_data 0',
+     'instance_name vol_bronze1_cifs', 'cifs_read_data 3017117067272', 'nfs_read_data 513505960083',
+     'write_latency 439931061563', 'san_read_data 0', 'san_read_latency 0',
+     'write_data 8408643107314', 'nfs_write_data 1445857053805', 'cifs_write_latency 422105859254'],
+    ['volume vol_bronze1_firebirdbackup_nfs', 'size-available 457455501312', 'state online',
+     'files-total 31876689', 'files-used 145', 'size-total 831230795776', 'fcp_write_data 0',
+     'fcp_read_data 0', 'cifs_write_data 0', 'iscsi_read_latency 0', 'iscsi_write_data 0',
+     'read_data 14518295021500', 'nfs_write_latency 284302930608', 'san_write_latency 0',
+     'san_write_data 0', 'read_latency 287461642429', 'cifs_read_latency 0', 'fcp_write_latency 0',
+     'fcp_read_latency 0', 'iscsi_write_latency 0', 'nfs_read_latency 283857474274',
+     'iscsi_read_data 0', 'instance_name vol_bronze1_firebirdbackup_nfs', 'cifs_read_data 0',
+     'nfs_read_data 14143837216272', 'write_latency 289091386833', 'san_read_data 0',
+     'san_read_latency 0', 'write_data 31787066083361', 'nfs_write_data 31122622092264',
+     'cifs_write_latency 0'],
+    ['volume vol_bronze1_singlescm_nfs', 'size-available 68105011200', 'state offline',
+     'files-total 3112959', 'files-used 154127', 'size-total 107374182400', 'fcp_write_data 0',
+     'fcp_read_data 0', 'cifs_write_data 0', 'iscsi_read_latency 0', 'iscsi_write_data 0',
+     'read_data 1675013596', 'nfs_write_latency 32687796', 'san_write_latency 0',
+     'san_write_data 0', 'read_latency 334395407', 'cifs_read_latency 0', 'fcp_write_latency 0',
+     'fcp_read_latency 0', 'iscsi_write_latency 0', 'nfs_read_latency 330848378',
+     'iscsi_read_data 0', 'instance_name vol_bronze1_singlescm_nfs', 'cifs_read_data 0',
+     'nfs_read_data 284389248', 'write_latency 248329456', 'san_read_data 0', 'san_read_latency 0',
+     'write_data 50241577605', 'nfs_write_data 5550238259', 'cifs_write_latency 0']
 ]
+
+mock_host_conf = {
+    '':
+        [
+            [
+                {'group_name': 'euedcnas1710.v_1710_data',
+                 'patterns_include': ['euedcnas1710.v_1710_data'],
+                 'patterns_exclude': []},
+                {'group_name': 'group1',
+                 'patterns_include': ['vol0'],
+                 'patterns_exclude': []},
+                {'group_name': 'group2',
+                 'patterns_include': ['vol_bronze1*'],
+                 'patterns_exclude': []},
+                {'group_name': 'group2',
+                 'patterns_include': [],
+                 'patterns_exclude': ['vol_bronze1_cifs']},
+                {'group_name': 'group3',
+                 'patterns_include': [],
+                 'patterns_exclude': []},
+            ],
+        ],
+}
 
 discovery = {
     "": [
@@ -80,7 +182,10 @@ discovery = {
         ("euedcnas1012.vol_1012_root", {}),
         ("euedcnas1710.v_1710_a2mac1", {}),
         ("euedcnas1710.v_1710_aspera", {}),
-        ("euedcnas1710.v_1710_data", {}),
+        ("euedcnas1710.v_1710_data", {'patterns': (['euedcnas1710.v_1710_data'], [])}),
+        ('group1', {'patterns': (['vol0'], [])}),
+        ('group2', {'patterns': (['vol_bronze1*'], ['vol_bronze1_cifs'])}),
+        ('vol_bronze1_cifs', {}),
     ]
 }
 
@@ -92,7 +197,7 @@ def mock_util(*args):
 
     for item in ["euedcnas1011.vol_1011_root", "euedcnas1012.vol_1012_root",
                  "euedcnas1710.v_1710_a2mac1", "euedcnas1710.v_1710_aspera",
-                 "euedcnas1710.v_1710_data"]:
+                 "euedcnas1710.v_1710_data", "group1", "group2"]:
 
         item_state['df.%s.delta' % item, None] = (1591789747, 0)
         item_state['df.%s.trend' % item, (1591797021.0, None)] = (1591789747, 0)
@@ -194,8 +299,8 @@ checks = {
                     '60.04% used (180.13 of 300.00 GB), (warn/crit at 50.0%/60.0%), trend: +121.29 '
                     'GB / 24 hours - growing too fast (warn/crit at 100.00 MB/200.00 MB per 24.0 h)'
                     '(!!), growing too fast (warn/crit at 5.000%/10.000% per 24.0 h)(!!), time '
-                    'left until disk full: 23 hours, Inodes Used: 0.04%, inodes available: 9.34 '
-                    'M/99.96%, ',
+                    'left until disk full: 23 hours, Inodes Used: 0.04%, inodes available: 9.34 M/'
+                    '99.96%, ',
                     [('fs_used', 184455.7265625, 153600.0, 184320.0, 0, 307200.0),
                      ('fs_size', 307200.0), ('fs_used_percent', 60.04418182373047),
                      ('growth', 2190950.615204839),
@@ -232,7 +337,8 @@ checks = {
                 'magic': 0.8, 'trend_mb': (100, 200),
                 'trend_perc': (5.0, 10.0),
                 'trend_timeleft': (12, 6),
-                'trend_showtimeleft': True
+                'trend_showtimeleft': True,
+                'patterns': (['euedcnas1710.v_1710_data'], [])
             },
             [
                 (
@@ -259,6 +365,56 @@ checks = {
                      ('iscsi_read_ops', 0.0), ('iscsi_read_latency', 0.0),
                      ('iscsi_write_data', 0.0), ('iscsi_write_ops', 0.0),
                      ('iscsi_write_latency', 0.0)],
+                ),
+                (
+                    0,
+                    '1 volume(s) in group',
+                ),
+            ],
+        ),
+        (
+            "group1",
+            {
+                'patterns': (['vol0'], [])
+            },
+            [
+                (
+                    0,
+                    '8.15% used (17.32 of 212.50 GB), trend: +11.66 GB / 24 hours, ',
+                    [('fs_used', 17738.44921875, 174080.0, 195840.0, 0, 217600.0),
+                     ('fs_size', 217600.0),
+                     ('fs_used_percent', 8.151860854204964), ('growth', 210695.9049353863),
+                     ('trend', 11943.480410396396, None, None, 0, 9066.666666666666),
+                     ('inodes_used', 10737, None, None, 0.0, 7782396.0)],
+                ),
+                (
+                    0,
+                    '1 volume(s) in group',
+                ),
+            ],
+        ),
+        (
+            "group2",
+            {
+                'patterns': (['vol_bronze1*'], ['vol_bronze1_cifs'])
+            },
+            [
+                (
+                    0,
+                    '50.47% used (30.46 of 60.36 TB), trend: +20.51 TB / 24 hours, ',
+                    [('fs_used', 31942130.44140625, 50630282.453125, 56959067.759765625, 0,
+                      63287853.06640625), ('fs_size', 63287853.06640625),
+                     ('fs_used_percent', 50.471186639701976), ('growth', 379406113.5740308),
+                     ('trend', 21506965.151722867, None, None, 0, 2636993.8777669272),
+                     ('inodes_used', 272219307, None, None, 0.0, 874985078.0)],
+                ),
+                (
+                    0,
+                    '6 volume(s) in group',
+                ),
+                (
+                    1,
+                    'Volume vol_bronze1_singlescm_nfs is offline',
                 ),
             ],
         ),

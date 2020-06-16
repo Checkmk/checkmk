f49b6a627a0e699682053168bca88137e392b510
thl-cmk <thl-cmk@outlook.com>
2020-06-16 19:54:21 +0200

    [Sophos XG-FIREWALL] adds sophos_swap, streamlines sophos_cpu/memory/disk/swap, adds absolute values to sophos_disk/memory

---
+++ cmk/gui/plugins/wato/check_parameters/sophos_swap.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    Dictionary,
+    Percentage,
+    Tuple,
+)
+
+from cmk.gui.plugins.wato import (
+    CheckParameterRulespecWithoutItem,
+    rulespec_registry,
+    RulespecGroupCheckParametersOperatingSystem,
+)
+
+
+def _parameter_valuespec_sophos_swap():
+    return Dictionary(elements=[
+        ("swap_levels",
+         Tuple(
+             title=_("Swap memory percentage usage"),
+             elements=[
+                 Percentage(title=_("Warning at"), default_value=80),
+                 Percentage(title=_("Critical at"), default_value=90)
+             ],
+         )),
+    ])
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithoutItem(
+        check_group_name="sophos_swap",
+        group=RulespecGroupCheckParametersOperatingSystem,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_sophos_swap,
+        title=lambda: _("Sophos Swap memory utilization"),
+    ))

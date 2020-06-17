abf6ead5d00612baddd8a9258adc2a525ba507eb
Mathias Laurin <mathias.laurin@tribe29.com>
2020-06-17 11:08:09 +0200

    linting: More unused imports
    Change-Id: Ie66fac95753e2d0d45409cd0789b9270c23aef9d

--- cmk/base/data_sources/ipmi.py
+++ cmk/base/data_sources/ipmi.py
@@ -4,10 +4,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Dict, List, Optional, Set, cast
+from typing import Dict, List, Optional, cast
 
 from cmk.utils.type_defs import (
-    CheckPluginName,
     HostAddress,
     HostName,
     RawAgentData,

0c2345f281c5ae85070d83aa766e65ec62b2d82d
Simon Jess <simon.jess@tribe29.com>
2020-06-25 12:08:31 +0200

    11226 FIX hitachi_hus_dku: Fixed informational text about component: "Processor" -> "Power Supply"
    Change-Id: I8a02103c31b97d9084375b6b1000ea3ffd77b66b

---
+++ tests/unit/checks/generictests/datasets/hitachi_hus_dku_regression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# yapf: disable
+# type: ignore
+
+checkname = 'hitachi_hus_dku'
+
+info = [['210221', '1', '1', '1', '1']]
+
+discovery = {'': [('210221', None)]}
+
+checks = {'': [('210221', {}, [(0, 'OK: Power Supply: no error, Fan: no error, Environment: no error, Drive: no error', [])])]}

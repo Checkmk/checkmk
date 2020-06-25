873b6fdeabcc0ba8e2a7160f42f2f360b02f4d8e
Simon Jess <simon.jess@tribe29.com>
2020-06-25 12:08:31 +0200

    11226 FIX hitachi_hus_dku: Fixed informational text about component: "Processor" -> "Power Supply"
    Change-Id: I8a02103c31b97d9084375b6b1000ea3ffd77b66b

---
+++ tests-py3/unit/checks/generictests/datasets/hitachi_hus_dku_regression.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# yapf: disable
+# type: ignore
+
+checkname = 'hitachi_hus_dku'
+
+info = [['210221', '1', '1', '1', '1']]
+
+discovery = {'': [('210221', None)]}
+
+checks = {'': [('210221', {}, [(0, 'OK: Power Supply: no error, Fan: no error, Environment: no error, Drive: no error', [])])]}

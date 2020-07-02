45e3cf5e2122a3602a20a00eb640f9efac2f6c70
Joerg Herbel <joerg.herbel@tribe29.com>
2020-07-02 16:39:30 +0200

    Added generic test datasets for apc_rackpdu_power
    Change-Id: I3c16e0a0349b113e24acc11d68d6edfea2cc95c3

---
+++ tests/unit/checks/generictests/datasets/apc_rackpdu_power_1.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'apc_rackpdu_power'
+
+
+info = [[[u'luz0010x', u'0']],
+        [[u'0']],
+        [[u'1', u'0', u'1'], [u'2', u'0', u'1'], [u'3', u'0', u'1']]]
+
+
+discovery = {'': [(u'Device luz0010x', {}),
+                  (u'Phase 1', {}),
+                  (u'Phase 2', {}),
+                  (u'Phase 3', {})]}
+
+
+checks = {'': [(u'Device luz0010x',
+                {},
+                [(0, 'Power: 0.0 W', [('power', 0.0, None, None, None, None)])]),
+               (u'Phase 1',
+                {},
+                [(0, 'Current: 0.0 A', [('current', 0.0, None, None, None, None)]),
+                 (0, 'load normal', [])]),
+               (u'Phase 2',
+                {},
+                [(0, 'Current: 0.0 A', [('current', 0.0, None, None, None, None)]),
+                 (0, 'load normal', [])]),
+               (u'Phase 3',
+                {},
+                [(0, 'Current: 0.0 A', [('current', 0.0, None, None, None, None)]),
+                 (0, 'load normal', [])])]}

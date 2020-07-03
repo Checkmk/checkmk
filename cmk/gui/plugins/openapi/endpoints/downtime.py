0feb2b7a7d1127ebb73e36cda4347a90e5400b38
Wontek Hong <wontek.hong@tribe29.com>
2020-07-03 09:28:32 +0200

    API downtime: GET list downtimes
    Change-Id: I88be4dd2c4abfefd84c552d446114b1448dfbde1

--- cmk/gui/plugins/openapi/endpoints/downtime.py
+++ cmk/gui/plugins/openapi/endpoints/downtime.py
@@ -3,12 +3,18 @@
 # Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+import json
 from datetime import datetime
 
 from cmk.gui.http import Response
+from cmk.gui import sites
+from cmk.gui.plugins.openapi.livestatus_helpers.queries import Query
+from cmk.gui.plugins.openapi.livestatus_helpers.tables.downtimes import Downtimes
+from cmk.gui.plugins.openapi.restful_objects.utils import ParamDict
 from cmk.gui.watolib.downtime import (execute_livestatus_command, determine_downtime_mode,
                                       DowntimeSchedule)
-from cmk.gui.plugins.openapi.restful_objects import (endpoint_schema, request_schemas, constructors)
+from cmk.gui.plugins.openapi.restful_objects import (endpoint_schema, request_schemas, constructors,
+                                                     response_schemas)
 
 RECURRING_OPTIONS = {
     "hour": 1,
@@ -54,9 +60,96 @@ def create_downtime(params):
     return Response(status=204)
 
 
+@endpoint_schema(constructors.collection_href('downtime'),
+                 '.../collection',
+                 method='get',
+                 parameters=[
+                     ParamDict.create('host_name', 'query', required=False,
+                                      schema_type='string').to_dict(),
+                     ParamDict.create('service_description',
+                                      'query',
+                                      required=False,
+                                      schema_type='string').to_dict()
+                 ],
+                 response_schema=response_schemas.DomainObjectCollection)
+def list_service_downtimes(param):
+    live = sites.live()
+
+    q = Query([
+        Downtimes.id,
+        Downtimes.host_name,
+        Downtimes.service_description,
+        Downtimes.is_service,
+        Downtimes.author,
+        Downtimes.start_time,
+        Downtimes.end_time,
+        Downtimes.recurring,
+        Downtimes.comment,
+    ])
+
+    host_name = param.get('host_name')
+    if host_name is not None:
+        q = q.filter(Downtimes.host_name.contains(host_name))
+
+    service_description = param.get('service_description')
+    if service_description is not None:
+        q = q.filter(Downtimes.service_description.contains(service_description))
+
+    gen_downtimes = q.iterate(live)
+    return _serve_downtimes(gen_downtimes)
+
+
+def _serve_downtimes(downtimes):
+    response = Response()
+    response.set_data(json.dumps(serialize_downtimes(downtimes)))
+    response.set_content_type('application/json')
+    return response
+
+
+def serialize_downtimes(downtimes):
+    entries = []
+    for downtime_info in downtimes:
+        service_description = downtime_info.get("service_description")
+        if service_description:
+            downtime_detail = "service: %s" % service_description
+        else:
+            downtime_detail = "host: %s" % downtime_info["host_name"]
+
+        entries.append(
+            constructors.domain_object(
+                domain_type='downtime',
+                identifier=downtime_info['id'],
+                title='Downtime for %s' % downtime_detail,
+                extensions=_downtime_properties(downtime_info),
+            ))
+
+    return constructors.object_collection(
+        name='all',
+        domain_type='downtime',
+        entries=entries,
+        base='',
+    )
+
+
+def _downtime_properties(info):
+    return {
+        "host_name": info['host_name'],
+        "author": info['author'],
+        "is_service": 'yes' if info["is_service"] else 'no',
+        "start_time": _time_utc(datetime.fromtimestamp(info['start_time'])),
+        "end_time": _time_utc(datetime.fromtimestamp(info['end_time'])),
+        "recurring": 'yes' if info['recurring'] else 'no',
+        "comment": info['comment']
+    }
+
+
 def _service_spec(service_description, host_name):
     return "%s;%s" % (service_description, host_name)
 
 
 def _time_dt(time_str):
     return datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%S%z")
+
+
+def _time_utc(time_dt):
+    return time_dt.strftime("%Y-%m-%dT%H:%M:%S%z")
---
+++ tests/unit/cmk/gui/plugins/openapi/test_openapi_downtime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.gui.plugins.openapi.livestatus_helpers.testing import MockLiveStatusConnection
+
+
+def test_openapi_livestatus_hosts(wsgi_app, with_automation_user, suppress_automation_calls,
+                                  mock_livestatus):
+    live: MockLiveStatusConnection = mock_livestatus
+    username, secret = with_automation_user
+    wsgi_app.set_authorization(('Bearer', username + " " + secret))
+    base = '/NO_SITE/check_mk/api/v0'
+
+    downtime_result = [[
+        54, 'heute', 'CPU load', 1, 'cmkadmin', 1593770319, 1596448719, 0, 'Downtime for service'
+    ]]
+
+    live.expect_query([
+        'GET downtimes',
+        'Columns: id host_name service_description is_service author start_time end_time recurring comment'
+    ],
+                      result=downtime_result)
+    with live:
+        resp = wsgi_app.call_method('get',
+                                    base + "/domain-types/downtime/collections/all",
+                                    status=200)
+        assert len(resp.json['value']) == 1

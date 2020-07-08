57d607b0de0ff2a1096eb36bd07ed994028d78b8
Christoph Rauch <christoph.rauch@tribe29.com>
2020-07-08 10:56:33 +0200

    REST-API: host endpoint refactor
     * add documentation
    
    Change-Id: Iaebb89b1c110dd7a7a90fcdd1b1206834f218fab

--- cmk/gui/plugins/openapi/endpoints/host.py
+++ cmk/gui/plugins/openapi/endpoints/host.py
@@ -18,36 +18,47 @@ from cmk.gui.plugins.openapi.restful_objects.utils import ParamDict
                  '.../collection',
                  method='get',
                  parameters=[
-                     HOST_NAME(location='query', required=False),
+                     HOST_NAME(
+                         location='query',
+                         required=False,
+                         description=Hosts.name.__doc__,
+                     ),
                      ParamDict.create(
                          'host_alias',
                          'query',
                          required=False,
+                         description=Hosts.alias.__doc__,
                          schema_type='string',
-                     ).to_dict(),
+                     ),
                      ParamDict.create(
                          'acknowledged',
                          'query',
                          required=False,
+                         example='1',
+                         description=Hosts.acknowledged.__doc__,
                          schema_type='boolean',
-                     ).to_dict(),
+                     ),
                      ParamDict.create(
                          'in_downtime',
                          'query',
                          required=False,
+                         example="0",
+                         description="Whether the host is currently in a downtime (0/1)",
                          schema_type='boolean',
-                     ).to_dict(),
+                     ),
                      ParamDict.create(
                          'status',
                          'query',
                          required=False,
+                         description=Hosts.state.__doc__,
                          schema_type='integer',
                          schema_num_minimum=0,
                          schema_num_maximum=3,
-                     ).to_dict(),
+                     ),
                  ],
                  response_schema=response_schemas.DomainObjectCollection)
 def list_hosts(param):
+    """List currently monitored hosts."""
     live = sites.live()
 
     q = Query([

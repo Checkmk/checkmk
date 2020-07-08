f9b7c9af0e263a1368077024f414062f876ef5b6
Christoph Rauch <christoph.rauch@tribe29.com>
2020-07-08 10:55:57 +0200

    REST-API: endpoint docstrings
    Change-Id: If2f02ef37c2d9ef6dc660ff7cf25860f8501f2a6

--- cmk/gui/plugins/openapi/endpoints/host.py
+++ cmk/gui/plugins/openapi/endpoints/host.py
@@ -3,6 +3,9 @@
 # Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+"""Host status endpoints
+"""
+
 from cmk.gui import sites
 from cmk.gui.plugins.openapi.livestatus_helpers.queries import Query
 from cmk.gui.plugins.openapi.livestatus_helpers.tables import Hosts
--- cmk/gui/plugins/openapi/endpoints/service_discovery.py
+++ cmk/gui/plugins/openapi/endpoints/service_discovery.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
+"""Service discovery endpoints."""
 import json
 
 from cmk.gui import watolib

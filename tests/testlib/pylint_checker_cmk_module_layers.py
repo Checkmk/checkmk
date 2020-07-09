a028b1bef1f40c785a83097324ddaab9192504e0
Sven Panne <sven.panne@tribe29.com>
2020-07-09 17:00:34 +0200

    Introduce ModulePath NewType plus related renamings.
    Change-Id: I9f0a635de742c4a4f620aa9d7f538de0b15d51ac

--- tests/testlib/pylint_checker_cmk_module_layers.py
+++ tests/testlib/pylint_checker_cmk_module_layers.py
@@ -18,6 +18,7 @@ from pylint.interfaces import IAstroidChecker  # type: ignore[import]
 from testlib import cmk_path
 
 ModuleName = NewType('ModuleName', str)
+ModulePath = NewType('ModulePath', str)  # TODO: use pathlib.Path
 Component = NewType('Component', str)
 
 
@@ -47,17 +48,17 @@ _COMPONENTS = (
 )
 
 _EXPLICIT_FILE_TO_COMPONENT = {
-    "web/app/index.wsgi": Component("cmk.gui"),
-    "bin/update_rrd_fs_names.py": Component("cmk.base"),
-    "bin/check_mk": Component("cmk.base"),
-    "bin/cmk-update-config": Component("cmk.update_config"),
-    "bin/mkeventd": Component("cmk.ec"),
-    "enterprise/bin/liveproxyd": Component("cmk.cee.liveproxy"),
-    "enterprise/bin/mknotifyd": Component("cmk.cee.mknotifyd"),
-    "enterprise/bin/dcd": Component("cmk.cee.dcd"),
+    ModulePath("web/app/index.wsgi"): Component("cmk.gui"),
+    ModulePath("bin/update_rrd_fs_names.py"): Component("cmk.base"),
+    ModulePath("bin/check_mk"): Component("cmk.base"),
+    ModulePath("bin/cmk-update-config"): Component("cmk.update_config"),
+    ModulePath("bin/mkeventd"): Component("cmk.ec"),
+    ModulePath("enterprise/bin/liveproxyd"): Component("cmk.cee.liveproxy"),
+    ModulePath("enterprise/bin/mknotifyd"): Component("cmk.cee.mknotifyd"),
+    ModulePath("enterprise/bin/dcd"): Component("cmk.cee.dcd"),
     # CEE specific notification plugins
-    "notifications/servicenow": Component("cmk.cee.notification_plugins"),
-    "notifications/jira_issues": Component("cmk.cee.notification_plugins"),
+    ModulePath("notifications/servicenow"): Component("cmk.cee.notification_plugins"),
+    ModulePath("notifications/jira_issues"): Component("cmk.cee.notification_plugins"),
 }
 
 
@@ -85,23 +86,25 @@ class CMKModuleLayerChecker(BaseChecker):
         if not imported.startswith("cmk"):
             return  # We only care about our own modules, ignore this
 
-        file_path = node.root().file[self.path_prefix_len_to_strip:]  # Make relative
+        # We use paths relative to our project root only.
+        absolute_path: str = node.root().file
+        importing_path = ModulePath(absolute_path[self.path_prefix_len_to_strip:])
 
-        if file_path.startswith("tests/"):
+        if importing_path.startswith("tests/"):
             return  # No validation in tests
 
         # Pylint fails to detect the correct module path here. Instead of realizing that the file
         # cmk/base/automations/cee.py is cmk.base.automations.cee it thinks the module is "cee".
         # We can silently ignore these files because the linked files at enterprise/... are checked.
-        if os.path.islink(file_path):
+        if os.path.islink(importing_path):
             return  # Ignore symlinked files instead of checking them twice, ignore this
 
-        importing = self._get_module_name_of_file(node, file_path)
+        importing = self._get_module_name_of_file(node, importing_path)
 
-        if not self._is_import_allowed(file_path, importing, imported):
+        if not self._is_import_allowed(importing_path, importing, imported):
             self.add_message("cmk-module-layer-violation", node=node, args=(imported, importing))
 
-    def _get_module_name_of_file(self, node: Statement, file_path: str) -> ModuleName:
+    def _get_module_name_of_file(self, node: Statement, importing_path: ModulePath) -> ModuleName:
         """Fixup module names"""
         # Emacs' flycheck stores files to be checked in a temporary file with a prefix.
         module_name = removeprefix(node.root().name, "flycheck_")
@@ -110,7 +113,7 @@ class CMKModuleLayerChecker(BaseChecker):
             ("cmk", "base", "plugins", "agent_based", "utils", ""),
             ("cmk", "base", "plugins", "agent_based", ""),
         ]:
-            if file_path.startswith('/'.join(segments)):
+            if importing_path.startswith('/'.join(segments)):
                 return ModuleName('.'.join(segments) + module_name)
 
         # Fixup managed and enterprise module names
@@ -124,17 +127,17 @@ class CMKModuleLayerChecker(BaseChecker):
                     "enterprise/cmk/%s/cee/" % component,
                     "managed/cmk/%s/cme/" % component,
             ]:
-                if file_path.startswith(prefix):
+                if importing_path.startswith(prefix):
                     return ModuleName("cmk.%s.%s" % (component, module_name))
 
         if module_name.startswith("cee.") or module_name.startswith("cme."):
             return ModuleName("cmk.%s" % module_name)
         return ModuleName(module_name)
 
-    def _is_import_allowed(self, file_path: str, importing: ModuleName,
+    def _is_import_allowed(self, importing_path: ModulePath, importing: ModuleName,
                            imported: ModuleName) -> bool:
         for component in _COMPONENTS:
-            if not self._is_part_of_component(importing, file_path, component):
+            if not self._is_part_of_component(importing, importing_path, component):
                 continue
 
             if self._is_disallowed_snmplib_import(importing, component):
@@ -151,12 +154,12 @@ class CMKModuleLayerChecker(BaseChecker):
 
         return self._is_utility_import(imported)
 
-    def _is_part_of_component(self, importing: ModuleName, file_path: str,
+    def _is_part_of_component(self, importing: ModuleName, importing_path: ModulePath,
                               component: Component) -> bool:
         if self._is_import_in_component(importing, component):
             return True
 
-        explicit_component = _EXPLICIT_FILE_TO_COMPONENT.get(file_path)
+        explicit_component = _EXPLICIT_FILE_TO_COMPONENT.get(importing_path)
         if explicit_component is not None:
             return explicit_component == component
 
@@ -165,10 +168,10 @@ class CMKModuleLayerChecker(BaseChecker):
         if component == "cmk.base" and importing.startswith("cmk_pylint"):
             return True
 
-        if component == "cmk.notification_plugins" and file_path.startswith("notifications/"):
+        if component == "cmk.notification_plugins" and importing_path.startswith("notifications/"):
             return True
 
-        if component == "cmk.special_agents" and file_path.startswith("agents/special/"):
+        if component == "cmk.special_agents" and importing_path.startswith("agents/special/"):
             return True
 
         return False

ef2c4befb5e2b6e7e68ea57cc76adabe7fcc4a01
Frans Fürst <frans.fuerst@tribe29.com>
2020-07-09 15:30:52 +0200

    check_mail_loop: refactoring: code cleanup
    Change-Id: I1b3d18638e110b99abeae831882a95858b729798

--- tests/unit/active_checks/test_ac_check_mail_loop.py
+++ tests/unit/active_checks/test_ac_check_mail_loop.py
@@ -30,56 +30,127 @@ def test_ac_check_mail_main_loop_failed_to_send_mail(check_mail_loop):
         'to',
     ])
     assert state == 3
-    assert info.startswith('Failed to send mail')
+    assert info.startswith('Failed to')
     assert perf is None
 
 
 @pytest.mark.parametrize(
     "warning, critical, expected_mails, fetched_mails, expected_result",
     [
-        (None, 3600, {}, {}, (0, 'Did not receive any new mail', [])),
+        (
+            None,
+            3600,
+            {},
+            {},
+            (0, 'Did not receive any new mail', []),
+        ),
         # No received mails
-        (None, 3600, {
-            '0-123': (0, 123)
-        }, {},
-         (2, 'Did not receive any new mail, Lost: 1 (Did not arrive within 3600 seconds)', [])),
-        (None, 1000, {
-            '0-123': (0, 123)
-        }, {},
-         (2, 'Did not receive any new mail, Lost: 1 (Did not arrive within 1000 seconds)', [])),
-        (None, 1000, {
-            '0-123': (0, 123),
-            '0-Bar': (0, 'Bar'),
-        }, {},
-         (2, 'Did not receive any new mail, Lost: 2 (Did not arrive within 1000 seconds)', [])),
-        (None, 2**64, {
-            '0-123': (0, 123),
-        }, {}, (0, 'Did not receive any new mail, Currently waiting for 1 mails', [])),
-        (None, 2**64, {
-            '0-123': (0, 123),
-            '0-Bar': (0, 'Bar'),
-        }, {}, (0, 'Did not receive any new mail, Currently waiting for 2 mails', [])),
+        (
+            None,
+            3600,
+            {
+                '0-123': (0, 123)
+            },
+            {},
+            (
+                2,
+                'Did not receive any new mail, Lost: 1 (Did not arrive within 3600 seconds)',
+                [],
+            ),
+        ),
+        (
+            None,
+            1000,
+            {
+                '0-123': (0, 123)
+            },
+            {},
+            (
+                2,
+                'Did not receive any new mail, Lost: 1 (Did not arrive within 1000 seconds)',
+                [],
+            ),
+        ),
+        (
+            None,
+            1000,
+            {
+                '0-123': (0, 123),
+                '0-Bar': (0, 'Bar'),
+            },
+            {},
+            (
+                2,
+                'Did not receive any new mail, Lost: 2 (Did not arrive within 1000 seconds)',
+                [],
+            ),
+        ),
+        (
+            None,
+            2**64,
+            {
+                '0-123': (0, 123),
+            },
+            {},
+            (
+                0,
+                'Did not receive any new mail, Currently waiting for 1 mails',
+                [],
+            ),
+        ),
+        (
+            None,
+            2**64,
+            {
+                '0-123': (0, 123),
+                '0-Bar': (0, 'Bar'),
+            },
+            {},
+            (
+                0,
+                'Did not receive any new mail, Currently waiting for 2 mails',
+                [],
+            ),
+        ),
         # No expected mails
         (None, 3600, {}, {
             '0-123': (0, 123),
         }, (0, 'Did not receive any new mail', [])),
         # Both fetched and expected mails
-        (None, 3600, {
-            '0-123': (0, 123),
-            '0-456': (0, 456),
-        }, {
-            '0-123': (0, 123),
-            '0-456': (0, 456),
-        }, (0, 'Received 2 mails within average of 289 seconds', [('duration', 289.5, '', 3600)])),
-        (None, 3600, {
-            '0-123': (0, 123),
-            '0-789': (0, 789),
-        }, {
-            '0-123': (0, 123),
-            '0-456': (0, 456),
-        }, (2, 'Mail received within 123 seconds, Lost: 1 (Did not arrive within 3600 seconds)', [
-            ('duration', 123, '', 3600)
-        ])),
+        (
+            None,
+            3600,
+            {
+                '0-123': (0, 123),
+                '0-456': (0, 456),
+            },
+            {
+                '0-123': (0, 123),
+                '0-456': (0, 456),
+            },
+            (
+                0,
+                'Received 2 mails within average of 289 seconds',
+                [('duration', 289.5, '', 3600)],
+            ),
+        ),
+        (
+            None,
+            3600,
+            {
+                '0-123': (0, 123),
+                '0-789': (0, 789),
+            },
+            {
+                '0-123': (0, 123),
+                '0-456': (0, 456),
+            },
+            (
+                2,
+                'Mail received within 123 seconds, Lost: 1 (Did not arrive within 3600 seconds)',
+                [('duration', 123, '', 3600)],
+            ),
+        ),
     ])
 def test_ac_check_mail_loop(check_mail_loop, warning, critical, expected_mails, fetched_mails,
                             expected_result):

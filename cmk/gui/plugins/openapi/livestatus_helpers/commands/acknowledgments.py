8fdb984d82de308ed8f74ef3ee59ce42c370dd7b
Christoph Rauch <christoph.rauch@tribe29.com>
2020-07-15 18:03:51 +0200

    Livestatus helpers: commands for acknowledgements
    Change-Id: I70a1cdfd0eb3f47d03f62e6892648c569021790e

---
+++ cmk/gui/plugins/openapi/livestatus_helpers/commands/acknowledgments.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""This module contains helpers to trigger acknowledgments.
+"""
+
+from cmk.gui.plugins.openapi.livestatus_helpers.commands.lowlevel import send_command
+
+
+def acknowledge_service_problem(
+    connection,
+    host_name: str,
+    service_description: str,
+    sticky: bool = False,
+    notify: bool = False,
+    persistent: bool = False,
+    user: str = '',
+    comment: str = '',
+):
+    """Acknowledge the current problem for the given service.
+
+    When acknowledging a problem, furhter notifications for the service are disabled, as
+    long as the service doesn't change state. At state change, notifications are re-enabled.
+
+    Args:
+        connection:
+            A livestatus connection object.
+
+        host_name:
+            The host-name for which this acknowledgement is for.
+
+        service_description:
+            The service description of the service, whose problems shall be acknowledged.
+
+        sticky:
+            If set, only a state-change of the service to an OK state will discard the
+            acknowledgement. Otherwise it will be discarded on any state-change. Defaults to False.
+
+        notify:
+            If set, notifications will be sent out to the configured contacts. Defaults to False.
+
+        persistent:
+            If set, the comment will persist a restart. Defaults to False.
+
+        user:
+        comment:
+
+    Examples:
+
+        >>> from cmk.gui.plugins.openapi.livestatus_helpers.testing import simple_expect
+        >>> cmd = "COMMAND [...] ACKNOWLEDGE_SVC_PROBLEM;example.com;drain;1;0;0;;"
+        >>> with simple_expect(cmd) as live:
+        ...     acknowledge_service_problem(live, 'example.com', 'drain')
+
+    """
+    acknowledgement = 2 if sticky else 1  # 1: normal, 2: sticky
+
+    return send_command(
+        connection,
+        "ACKNOWLEDGE_SVC_PROBLEM",
+        [
+            host_name,
+            service_description,
+            acknowledgement,
+            int(notify),
+            int(persistent),
+            user,
+            comment,
+        ],
+    )
+
+
+def acknowledge_host_problem(
+    connection,
+    host_name,
+    sticky: bool = False,
+    notify: bool = False,
+    persistent: bool = False,
+    user: str = "",
+    comment: str = "",
+):
+    """Acknowledge the current problem for the given host.
+
+    When acknowledging a problem, notifications for the host are disabled, as long as the
+    host doesn't change state. At state change, notifications are re-enabled.
+
+    Args:
+        connection:
+            A livestatus connection object.
+
+        host_name:
+            The host-name for which this acknowledgement is for.
+
+        sticky:
+            If set, only a state-change of the host to an UP state will discard the acknowledgement.
+            Otherwise it will be discarded on any state-change. Defaults to False.
+
+        notify:
+            If set, notifications will be sent out to the configured contacts. Defaults to False.
+
+        persistent:
+            If set, the comment will persist a restart. Defaults to False.
+
+        user:
+        comment:
+
+    Examples:
+
+        >>> from cmk.gui.plugins.openapi.livestatus_helpers.testing import simple_expect
+        >>> cmd = "COMMAND [...] ACKNOWLEDGE_HOST_PROBLEM;example.com;1;0;0;;"
+        >>> with simple_expect(cmd) as live:
+        ...     acknowledge_host_problem(live, 'example.com')
+
+    """
+    acknowledgement = 2 if sticky else 1  # 1: normal, 2: sticky
+
+    return send_command(
+        connection,
+        "ACKNOWLEDGE_HOST_PROBLEM",
+        [
+            host_name,
+            acknowledgement,
+            int(notify),
+            int(persistent),
+            user,
+            comment,
+        ],
+    )

13e22145cf422a94154a0197be7724a95ee4eab1
Sven Panne <sven.panne@tribe29.com>
2020-07-20 15:06:29 +0200

    Comments only.
    Change-Id: I4de66e6e7d07423fa75df97aac6ca36ef1e30d14

--- livestatus/src/IntColumn.h
+++ livestatus/src/IntColumn.h
@@ -37,6 +37,11 @@ public:
     [[nodiscard]] std::unique_ptr<Aggregator> createAggregator(
         AggregationFactory factory) const override;
 
+    // TODO(sp): The only 2 places where auth_user is actually used are
+    // HostListStateColumn::getValue() and ServiceListStateColumn::getValue().
+    // These methods aggregate values for hosts/services, but they should do
+    // this only for "allowed" hosts/services. Find a better design than this
+    // parameter passing hell..
     virtual int32_t getValue(Row row, const contact *auth_user) const = 0;
 };
 

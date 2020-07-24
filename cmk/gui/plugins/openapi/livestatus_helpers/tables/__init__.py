bc71a1eae7f1ae5969ac7c9c0d1399296b24bbd8
Christoph Rauch <christoph.rauch@tribe29.com>
2020-07-24 12:23:29 +0200

    Livestatus helpers: add servicegroups/hostgroups table definitions
    Change-Id: Ia831dd54afd09d40ddebc83757247450fab55c71

--- cmk/gui/plugins/openapi/livestatus_helpers/tables/__init__.py
+++ cmk/gui/plugins/openapi/livestatus_helpers/tables/__init__.py
@@ -4,4 +4,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 from cmk.gui.plugins.openapi.livestatus_helpers.tables.hosts import Hosts
+from cmk.gui.plugins.openapi.livestatus_helpers.tables.hostgroups import Hostgroups
 from cmk.gui.plugins.openapi.livestatus_helpers.tables.services import Services
+from cmk.gui.plugins.openapi.livestatus_helpers.tables.servicegroups import Servicegroups

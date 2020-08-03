7d7abee2d1e220f818f4ed46f7fd2649f16d4733
Lars Michelsen <lm@tribe29.com>
2020-08-03 09:07:18 +0200

    11254 FIX Improve validation of host diagnostic page
    The AJAX page creating the result for the single diagnostic tests of the host
    diagnostic page was not properly validating the inputs received with the HTTP
    request variables.
    
    Change-Id: I8cc212c7d14a6cc6be4ba404530e72f9dffe52d7

--- cmk/gui/wato/pages/host_diagnose.py
+++ cmk/gui/wato/pages/host_diagnose.py
@@ -39,7 +39,6 @@ from cmk.gui.pages import page_registry, AjaxPage
 
 from cmk.gui.valuespec import (
     TextAscii,
-    DropdownChoice,
     Integer,
     Float,
     Dictionary,
@@ -113,7 +112,7 @@ class ModeDiagHost(WatoMode):
 
         if html.request.var('_save'):
             # Save the ipaddress and/or community
-            vs_host = self._vs_host()
+            vs_host = _vs_host(self._hostname)
             new = vs_host.from_html_vars('vs_host')
             vs_host.validate_value(new, 'vs_host')
 
@@ -138,11 +137,11 @@ class ModeDiagHost(WatoMode):
             return "edit_host", return_message
 
     def _validate_diag_html_vars(self):
-        vs_host = self._vs_host()
+        vs_host = _vs_host(self._hostname)
         host_vars = vs_host.from_html_vars("vs_host")
         vs_host.validate_value(host_vars, "vs_host")
 
-        vs_rules = self._vs_rules()
+        vs_rules = _vs_rules()
         rule_vars = vs_rules.from_html_vars("vs_rules")
         vs_rules.validate_value(rule_vars, "vs_rules")
 
@@ -169,7 +168,7 @@ class ModeDiagHost(WatoMode):
                 continue
             vs_dict[key] = value
 
-        vs_host = self._vs_host()
+        vs_host = _vs_host(self._hostname)
         vs_host.render_input("vs_host", vs_dict)
         html.help(vs_host.help())
 
@@ -183,7 +182,7 @@ class ModeDiagHost(WatoMode):
 
         value = {}
         forms.section(legend=False)
-        vs_rules = self._vs_rules()
+        vs_rules = _vs_rules()
         vs_rules.render_input("vs_rules", value)
         html.help(vs_rules.help())
         forms.end()
@@ -212,9 +211,6 @@ class ModeDiagHost(WatoMode):
             html.show_user_errors()
             return
 
-        # TODO: Insert any vs_host valuespec validation
-        #       These tests can be called with invalid valuespec settings...
-        # TODO: Replace hard coded icon paths with dynamic ones to old or new theme
         for ident, title in ModeDiagHost.diag_host_tests():
             html.h3(title)
             html.open_table(class_=["data", "test"])
@@ -238,92 +234,107 @@ class ModeDiagHost(WatoMode):
 
             html.close_tr()
             html.close_table()
-            html.javascript('cmk.host_diagnose.start_test(%s, %s, %s)' %
+            html.javascript('cmk.host_diagnose.start_test(%s, %s, %s, %s)' %
                             (json.dumps(ident), json.dumps(self._hostname),
+                             json.dumps(list(html.request.itervars(prefix="vs_"))),
                              json.dumps(html.transaction_manager.fresh_transid())))
 
-    def _vs_host(self):
-        return Dictionary(required_keys=['hostname'],
-                          elements=[
-                              ('hostname',
-                               FixedValue(self._hostname, title=_('Hostname'), allow_empty=False)),
-                              ('ipaddress',
-                               HostAddress(
-                                   title=_("IPv4 Address"),
-                                   allow_empty=False,
-                                   allow_ipv6_address=False,
-                               )),
-                              ('snmp_community',
-                               Password(title=_("SNMPv1/2 community"), allow_empty=False)),
-                              ('snmp_v3_credentials',
-                               cmk.gui.plugins.wato.SNMPCredentials(default_value=None,
-                                                                    only_v3=True)),
-                          ])
 
-    def _vs_rules(self):
-        if config.user.may('wato.add_or_modify_executables'):
-            ds_option = [
-                ('datasource_program', TextAscii(
-                    title = _("Datasource Program (<a href=\"%s\">Rules</a>)") % \
-                        watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'datasource_programs')]),
-                    help = _("For agent based checks Check_MK allows you to specify an alternative "
-                             "program that should be called by Check_MK instead of connecting the agent "
-                             "via TCP. That program must output the agent's data on standard output in "
-                             "the same format the agent would do. This is for example useful for monitoring "
-                             "via SSH.") + monitoring_macro_help() + " "
-                         + _("This option can only be used with the permission \"Can add or modify executables\"."),
-                ))
-            ]
-        else:
-            ds_option = []
+def _vs_host(host_name):
+    return Dictionary(
+        required_keys=['hostname'],
+        elements=[
+            ('hostname', FixedValue(
+                host_name,
+                title=_('Hostname'),
+                allow_empty=False,
+            )),
+            ('ipaddress',
+             HostAddress(
+                 title=_("IPv4 Address"),
+                 allow_empty=False,
+                 allow_ipv6_address=False,
+             )),
+            ('snmp_community', Password(
+                title=_("SNMPv1/2 community"),
+                allow_empty=False,
+            )),
+            ('snmp_v3_credentials',
+             cmk.gui.plugins.wato.SNMPCredentials(
+                 default_value=None,
+                 only_v3=True,
+             )),
+        ],
+    )
 
-        return Dictionary(
-            optional_keys = False,
-            elements = [
-                ('agent_port', Integer(
-                    allow_empty = False,
-                    minvalue = 1,
-                    maxvalue = 65535,
-                    default_value = 6556,
-                    title = _("Check_MK Agent Port (<a href=\"%s\">Rules</a>)") % \
-                        watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'agent_ports')]),
-                    help = _("This variable allows to specify the TCP port to "
-                             "be used to connect to the agent on a per-host-basis.")
-                )),
-                ('tcp_connect_timeout', Float(
-                    allow_empty = False,
-                    minvalue = 1.0,
-                    default_value = 5.0,
-                    unit = _("sec"),
-                    display_format = "%.0f",  # show values consistent to
-                    size = 2,                 # SNMP-Timeout
-                    title = _("TCP Connection Timeout (<a href=\"%s\">Rules</a>)") % \
-                        watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'tcp_connect_timeouts')]),
-                    help = _("This variable allows to specify a timeout for the "
-                            "TCP connection to the Check_MK agent on a per-host-basis."
-                            "If the agent does not respond within this time, it is considered to be unreachable.")
-                )),
-                ('snmp_timeout', Integer(
-                    allow_empty = False,
-                    title = _("SNMP-Timeout (<a href=\"%s\">Rules</a>)") % \
-                        watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'snmp_timing')]),
-                    help = _("After a request is sent to the remote SNMP agent we will wait up to this "
-                             "number of seconds until assuming the answer get lost and retrying."),
-                    default_value = 1,
-                    minvalue = 1,
-                    maxvalue = 60,
-                    unit = _("sec"),
-                )),
-                ('snmp_retries', Integer(
-                    allow_empty = False,
-                    title = _("SNMP-Retries (<a href=\"%s\">Rules</a>)") % \
-                        watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'snmp_timing')]),
-                    default_value = 5,
-                    minvalue = 0,
-                    maxvalue = 50,
-                )),
-            ] + ds_option,
-        )
+
+def _vs_rules():
+    if config.user.may('wato.add_or_modify_executables'):
+        ds_option = [(
+            'datasource_program',
+            TextAscii(
+                title=_("Datasource Program (<a href=\"%s\">Rules</a>)") %
+                watolib.folder_preserving_link([('mode', 'edit_ruleset'),
+                                                ('varname', 'datasource_programs')]),
+                help=_(
+                    "For agent based checks Check_MK allows you to specify an alternative "
+                    "program that should be called by Check_MK instead of connecting the agent "
+                    "via TCP. That program must output the agent's data on standard output in "
+                    "the same format the agent would do. This is for example useful for monitoring "
+                    "via SSH.") + monitoring_macro_help() + " " +
+                _("This option can only be used with the permission \"Can add or modify executables\"."
+                 ),
+            ))]
+    else:
+        ds_option = []
+
+    return Dictionary(
+        optional_keys = False,
+        elements = [
+            ('agent_port', Integer(
+                allow_empty = False,
+                minvalue = 1,
+                maxvalue = 65535,
+                default_value = 6556,
+                title = _("Check_MK Agent Port (<a href=\"%s\">Rules</a>)") % \
+                    watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'agent_ports')]),
+                help = _("This variable allows to specify the TCP port to "
+                         "be used to connect to the agent on a per-host-basis.")
+            )),
+            ('tcp_connect_timeout', Float(
+                allow_empty = False,
+                minvalue = 1.0,
+                default_value = 5.0,
+                unit = _("sec"),
+                display_format = "%.0f",  # show values consistent to
+                size = 2,                 # SNMP-Timeout
+                title = _("TCP Connection Timeout (<a href=\"%s\">Rules</a>)") % \
+                    watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'tcp_connect_timeouts')]),
+                help = _("This variable allows to specify a timeout for the "
+                        "TCP connection to the Check_MK agent on a per-host-basis."
+                        "If the agent does not respond within this time, it is considered to be unreachable.")
+            )),
+            ('snmp_timeout', Integer(
+                allow_empty = False,
+                title = _("SNMP-Timeout (<a href=\"%s\">Rules</a>)") % \
+                    watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'snmp_timing')]),
+                help = _("After a request is sent to the remote SNMP agent we will wait up to this "
+                         "number of seconds until assuming the answer get lost and retrying."),
+                default_value = 1,
+                minvalue = 1,
+                maxvalue = 60,
+                unit = _("sec"),
+            )),
+            ('snmp_retries', Integer(
+                allow_empty = False,
+                title = _("SNMP-Retries (<a href=\"%s\">Rules</a>)") % \
+                    watolib.folder_preserving_link([('mode', 'edit_ruleset'), ('varname', 'snmp_timing')]),
+                default_value = 5,
+                minvalue = 0,
+                maxvalue = 50,
+            )),
+        ] + ds_option,
+    )
 
 
 @page_registry.register_page("wato_ajax_diag_host")
@@ -334,8 +345,8 @@ class ModeAjaxDiagHost(AjaxPage):
         if not config.user.may('wato.diag_host'):
             raise MKAuthException(_('You are not permitted to perform this action.'))
 
-        if not html.check_transaction():
-            raise MKAuthException(_("Invalid transaction"))
+        #if not html.check_transaction():
+        #    raise MKAuthException(_("Invalid transaction"))
 
         request = self.webapi_request()
 
@@ -360,45 +371,44 @@ class ModeAjaxDiagHost(AjaxPage):
         if _test not in dict(ModeDiagHost.diag_host_tests()).keys():
             raise MKGeneralException(_('Invalid test.'))
 
-        # TODO: Use ModeDiagHost._vs_rules() for processing/validation?
+        vs_host = _vs_host(hostname)
+        host_vars = vs_host.from_html_vars("vs_host")
+        vs_host.validate_value(host_vars, "vs_host")
+
+        vs_rules = _vs_rules()
+        rule_vars = vs_rules.from_html_vars("vs_rules")
+        vs_rules.validate_value(rule_vars, "vs_rules")
+
         args = [""] * 13
-        for idx, what in enumerate([
-                'ipaddress',
-                'snmp_community',
-                'agent_port',
-                'snmp_timeout',
-                'snmp_retries',
-                'tcp_connect_timeout',
+
+        for idx, (collection, what) in enumerate([
+            (host_vars, 'ipaddress'),
+            (host_vars, 'snmp_community'),
+            (rule_vars, 'agent_port'),
+            (rule_vars, 'snmp_timeout'),
+            (rule_vars, 'snmp_retries'),
+            (rule_vars, 'tcp_connect_timeout'),
         ]):
-            args[idx] = request.get(what, "")
+            if what in collection:
+                args[idx] = str(collection[what])
 
         if config.user.may('wato.add_or_modify_executables'):
-            args[6] = request.get("datasource_program", "")
+            args[6] = rule_vars["datasource_program"]
 
-        if request.get("snmpv3_use"):
-            snmpv3_use = {
-                "0": "noAuthNoPriv",
-                "1": "authNoPriv",
-                "2": "authPriv",
-            }.get(request.get("snmpv3_use"))
-            args[7] = snmpv3_use
-            if snmpv3_use != "noAuthNoPriv":
-                snmpv3_auth_proto = {
-                    DropdownChoice.option_id("md5"): "md5",
-                    DropdownChoice.option_id("sha"): "sha"
-                }.get(request.get("snmpv3_auth_proto"))
-                args[8] = snmpv3_auth_proto
-                args[9] = request.get("snmpv3_security_name")
-                args[10] = request.get("snmpv3_security_password")
-                if snmpv3_use == "authPriv":
-                    snmpv3_privacy_proto = {
-                        DropdownChoice.option_id("DES"): "DES",
-                        DropdownChoice.option_id("AES"): "AES"
-                    }.get(request.get("snmpv3_privacy_proto"))
-                    args[11] = snmpv3_privacy_proto
-                    args[12] = request.get("snmpv3_privacy_password")
+        if "snmp_v3_credentials" in host_vars:
+            v3opts = host_vars["snmp_v3_credentials"]
+            args[7] = v3opts[0]  # Mode
+
+            if v3opts[0] != "noAuthNoPriv":
+                args[8] = v3opts[1]  # auth protocol
+                args[9] = v3opts[2]  # auth name
+                args[10] = v3opts[3]  # auth pass phrase
+
+                if v3opts[0] == "authPriv":
+                    args[11] = v3opts[4]  # privacy protocol
+                    args[12] = v3opts[5]  # privacy pass phrase
             else:
-                args[9] = request.get("snmpv3_security_name")
+                args[9] = v3opts[1]  # sec name
 
         result = watolib.check_mk_automation(host.site_id(), "diag-host", [hostname, _test] + args)
         return {
--- web/htdocs/js/modules/host_diagnose.js
+++ web/htdocs/js/modules/host_diagnose.js
@@ -25,57 +25,29 @@
 import * as utils from "utils";
 import * as ajax from "ajax";
 
-export function start_test(ident, hostname, transid) {
+export function start_test(ident, hostname, request_vars, transid) {
     var log   = document.getElementById(ident + "_log");
     var img   = document.getElementById(ident + "_img");
     var retry = document.getElementById(ident + "_retry");
 
     retry.style.display = "none";
 
-    var vars = "";
-    vars = "&_transid=" + encodeURIComponent(transid);
-    vars += "&ipaddress=" + encodeURIComponent(document.getElementsByName("vs_host_p_ipaddress")[0].value);
+    let params = [
+        "host=" + encodeURIComponent(hostname),
+        "_transid=" + encodeURIComponent(transid),
+        "_test=" + encodeURIComponent(ident)
+    ];
 
-
-    if (document.getElementsByName("vs_host_p_snmp_community_USE")[0].checked)
-        vars += "&snmp_community=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_community")[0].value);
-
-    var v3_use;
-    if (document.getElementsByName("vs_host_p_snmp_v3_credentials_USE")[0].checked) {
-        v3_use = encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_use")[0].value);
-        vars += "&snmpv3_use=" + v3_use;
-        if (v3_use == "0") {
-            vars += "&snmpv3_security_name=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_0_1")[0].value);
-        }
-        else if (v3_use == "1") {
-            vars += "&snmpv3_auth_proto=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_1_1")[0].value);
-            vars += "&snmpv3_security_name=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_1_2")[0].value);
-            vars += "&snmpv3_security_password=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_1_3")[0].value);
-        }
-        else if (v3_use == "2") {
-            vars += "&snmpv3_auth_proto=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_2_1")[0].value);
-            vars += "&snmpv3_security_name=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_2_2")[0].value);
-            vars += "&snmpv3_security_password=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_2_3")[0].value);
-            vars += "&snmpv3_privacy_proto=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_2_4")[0].value);
-            vars += "&snmpv3_privacy_password=" + encodeURIComponent(document.getElementsByName("vs_host_p_snmp_v3_credentials_2_5")[0].value);
-        }
-    }
-
-    vars += "&agent_port=" + encodeURIComponent(document.getElementsByName("vs_rules_p_agent_port")[0].value);
-    vars += "&tcp_connect_timeout=" + encodeURIComponent(document.getElementsByName("vs_rules_p_tcp_connect_timeout")[0].value);
-    vars += "&snmp_timeout=" + encodeURIComponent(document.getElementsByName("vs_rules_p_snmp_timeout")[0].value);
-    vars += "&snmp_retries=" + encodeURIComponent(document.getElementsByName("vs_rules_p_snmp_retries")[0].value);
-    if (document.getElementsByName("vs_rules_p_datasource_program").length > 0) {
-        vars += "&datasource_program=" + encodeURIComponent(document.getElementsByName("vs_rules_p_datasource_program")[0].value);
+    for (let i = 0; i < request_vars.length; i++) {
+        params.push(encodeURIComponent(request_vars[i][0]) + "=" + encodeURIComponent(request_vars[i][1]));
     }
 
     img.src = img.src.replace(/(.*\/icon_).*(\.png$)/i, "$1reload$2");
     utils.add_class(img, "reloading");
 
     log.innerHTML = "...";
-    ajax.get_url("wato_ajax_diag_host.py?host=" + encodeURIComponent(hostname)
-        + "&_test=" + encodeURIComponent(ident) + vars,
-        handle_host_diag_result, { "hostname": hostname, "ident": ident }); // eslint-disable-line indent
+    ajax.get_url("wato_ajax_diag_host.py?" + params.join("&"),
+        handle_host_diag_result, { "hostname": hostname, "ident": ident, "request_vars": request_vars }); // eslint-disable-line indent
 }
 
 function handle_host_diag_result(data, response_json) {
@@ -109,5 +81,5 @@ function handle_host_diag_result(data, response_json) {
 
     retry.src = retry.src.replace(/(.*\/icon_).*(\.png$)/i, "$1reload$2");
     retry.style.display = "inline";
-    retry.parentNode.href = "javascript:cmk.host_diagnose.start_test('"+data.ident+"', '"+data.hostname+"', '"+response.result.next_transid+"');";
+    retry.parentNode.href = "javascript:cmk.host_diagnose.start_test('"+data.ident+"', '"+data.hostname+"', "+JSON.stringify(data.request_vars)+", '"+response.result.next_transid+"');";
 }

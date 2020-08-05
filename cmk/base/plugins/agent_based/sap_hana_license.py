936ae701ba691f75e634e86d1f95541520f6547f
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2020-08-05 15:24:37 +0200

    Migrate sap_hana_license: Do the migration
    * This fixes also the bug, where the Graphs for "Used licenses" and "Size of license" where not created.
    
    CMK-5088
    
    Change-Id: I7242726d8dfecb1d7981364732f7d96e7a2bc961

---
+++ cmk/base/plugins/agent_based/sap_hana_license.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections import namedtuple
+
+from typing import Mapping
+
+from .utils import sap_hana
+from .agent_based_api.v0 import (
+    render,
+    check_levels,
+    register,
+    Service,
+    Result,
+    state,
+)
+
+from .agent_based_api.v0.type_defs import (
+    AgentStringTable,
+    DiscoveryGenerator,
+    CheckGenerator,
+    Parameters,
+)
+
+SAP_HANA_MAYBE = namedtuple("SAP_HANA_MAYBE", ["bool", "value"])
+
+
+def parse_sap_hana_license(string_table: AgentStringTable) -> sap_hana.ParsedSection:
+    section: sap_hana.ParsedSection = {}
+    for sid_instance, lines in sap_hana.parse_sap_hana(string_table).items():
+        for line in lines:
+            if len(line) < 7:
+                continue
+
+            inst = section.setdefault(sid_instance, {})
+            for index, key, in [
+                (0, "enforced"),
+                (1, "permanent"),
+                (2, "locked"),
+                (5, "valid"),
+            ]:
+                value = line[index]
+                inst[key] = SAP_HANA_MAYBE(_parse_maybe_bool(value), value)
+
+            for index, key, in [
+                (3, "size"),
+                (4, "limit"),
+            ]:
+                try:
+                    inst[key] = int(line[index])
+                except ValueError:
+                    pass
+            inst["expiration_date"] = line[6]
+    return section
+
+
+register.agent_section(
+    name="sap_hana_license",
+    parse_function=parse_sap_hana_license,
+)
+
+
+def _parse_maybe_bool(value):
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+    return
+
+
+def discovery_sap_hana_license(section: sap_hana.ParsedSection) -> DiscoveryGenerator:
+    for elem in section:
+        yield Service(item=elem)
+
+
+def check_sap_hana_license(item: str, params: Parameters,
+                           section: sap_hana.ParsedSection) -> CheckGenerator:
+
+    data = section.get(item)
+    if data is None:
+        return
+
+    enforced = data['enforced']
+    if enforced.bool:
+        yield from _check_product_usage(data['size'], data['limit'], params)
+    elif enforced.bool is None:
+        yield Result(state=state.UNKNOWN, summary="Status: unknown[%s]" % enforced.value)
+    else:
+        yield Result(state=state.OK, summary="Status: unlimited")
+
+    permanent = data["permanent"]
+    if permanent.bool:
+        yield Result(state=state.OK, summary='License: %s' % permanent.value)
+    else:
+        yield Result(state=state.WARN, summary='License: not %s' % permanent.value)
+
+    valid = data["valid"]
+    if not valid.bool:
+        yield Result(state=state.WARN, summary='not %s' % valid.value)
+
+    expiration_date = data["expiration_date"]
+    if expiration_date != "?":
+        yield Result(state=state.WARN, summary='Expiration date: %s' % expiration_date)
+
+    # It ONE physical device and at least two nodes.
+    # Thus we only need to check the first one.
+    return
+
+
+def _check_product_usage(size, limit, params):
+    yield from check_levels(size,
+                            metric_name="license_size",
+                            levels_upper=params.get('license_size'),
+                            render_func=render.bytes,
+                            label="Size")
+
+    try:
+        usage_perc = 100.0 * size / limit
+    except ZeroDivisionError:
+        yield Result(state=state.WARN, summary='Usage: cannot calculate')
+    else:
+        yield from check_levels(usage_perc,
+                                metric_name="license_usage_perc",
+                                levels_upper=params.get('license_usage_perc'),
+                                render_func=render.percent,
+                                label="Usage")
+
+
+def cluster_check_sap_hana_license(item: str, params: Parameters,
+                                   section: Mapping[str, sap_hana.ParsedSection]) -> CheckGenerator:
+    yield Result(state=state.OK, summary='Nodes: %s' % ', '.join(section.keys()))
+    for node_section in section.values():
+        if item in node_section:
+            yield from check_sap_hana_license(item, params, node_section)
+            return
+
+
+register.check_plugin(
+    name="sap_hana_license",
+    service_name="SAP HANA License %s",
+    discovery_function=discovery_sap_hana_license,
+    check_default_parameters={
+        'license_usage_perc': (80.0, 90.0),
+    },
+    check_ruleset_name="sap_hana_license",
+    check_function=check_sap_hana_license,
+    cluster_check_function=cluster_check_sap_hana_license,
+)
---
+++ tests/unit/cmk/base/plugins/agent_based/test_sap_hana_license.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+from cmk.base.api.agent_based.type_defs import Parameters
+from cmk.base.plugins.agent_based.agent_based_api.v0 import (
+    Service,
+    state,
+    Result,
+    Metric,
+)
+import cmk.base.plugins.agent_based.sap_hana_license as sap_hana_license
+
+SECTION = {
+    'Y04 10': {
+        'enforced': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'locked': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'expiration_date': u'2020-08-02 23:59:59.999999000',
+        'permanent': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'limit': 2147483647,
+        'valid': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'size': 33
+    },
+    'H62 10': {
+        'enforced': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'locked': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'expiration_date': u'?',
+        'permanent': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'limit': 12300,
+        'valid': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'size': 19
+    },
+    'X04 55': {
+        'enforced': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'locked': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'expiration_date': u'2020-08-02 23:59:59.999999000',
+        'permanent': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'limit': 10,
+        'valid': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'size': 5
+    },
+    'X00 00': {
+        'enforced': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'locked': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'expiration_date': u'2020-08-02 23:59:59.999999000',
+        'permanent': sap_hana_license.SAP_HANA_MAYBE(bool=False, value=u'FALSE'),
+        'limit': 0,
+        'valid': sap_hana_license.SAP_HANA_MAYBE(bool=True, value=u'TRUE'),
+        'size': 5
+    }
+}
+
+
+@pytest.mark.parametrize("string_table_row, expected_parsed_data", [
+    (
+        [
+            ['[[H62 10]]'],
+            ['FALSE', 'TRUE', 'FALSE', '19', '12300', 'TRUE', '?'],
+        ],
+        {
+            "H62 10": SECTION["H62 10"]
+        },
+    ),
+    (
+        [['[[Y04 10]]'],
+         ['FALSE', 'FALSE', 'FALSE', '33', '2147483647', 'TRUE', '2020-08-02 23:59:59.999999000']],
+        {
+            "Y04 10": SECTION["Y04 10"]
+        },
+    ),
+    (
+        [['[[X04 55]]'],
+         ['TRUE', 'FALSE', 'FALSE', '5', '10', 'TRUE', '2020-08-02 23:59:59.999999000']],
+        {
+            "X04 55": SECTION["X04 55"]
+        },
+    ),
+])
+def test_sap_hana_license_parse(string_table_row, expected_parsed_data):
+    assert sap_hana_license.parse_sap_hana_license(string_table_row) == expected_parsed_data
+
+
+def test_sap_hana_license_discovery():
+    assert list(sap_hana_license.discovery_sap_hana_license(SECTION)) == [
+        Service(item='Y04 10', parameters={}, labels=[]),
+        Service(item='H62 10', parameters={}, labels=[]),
+        Service(item='X04 55', parameters={}, labels=[]),
+        Service(item='X00 00', parameters={}, labels=[]),
+    ]
+
+
+@pytest.mark.parametrize(
+    "cur_item, result",
+    [("Y04 10", [
+        Result(state=state.OK, summary='Status: unlimited', details='Status: unlimited'),
+        Result(state=state.WARN, summary='License: not FALSE', details='License: not FALSE'),
+        Result(state=state.WARN,
+               summary='Expiration date: 2020-08-02 23:59:59.999999000',
+               details='Expiration date: 2020-08-02 23:59:59.999999000'),
+    ]),
+     ("H62 10", [
+         Result(state=state.OK, summary='Status: unlimited', details='Status: unlimited'),
+         Result(state=state.OK, summary='License: TRUE', details='License: TRUE'),
+     ]),
+     ("X04 55", [
+         Result(state=state.OK, summary='Size: 5 B', details='Size: 5 B'),
+         Metric('license_size', 5.0, levels=(None, None), boundaries=(None, None)),
+         Result(state=state.OK, summary='Usage: 50.0%', details='Usage: 50.0%'),
+         Metric('license_usage_perc', 50.0, levels=(None, None), boundaries=(None, None)),
+         Result(state=state.WARN, summary='License: not FALSE', details='License: not FALSE'),
+         Result(state=state.WARN,
+                summary='Expiration date: 2020-08-02 23:59:59.999999000',
+                details='Expiration date: 2020-08-02 23:59:59.999999000'),
+     ]),
+     (
+         "X00 00",
+         [
+             Result(state=state.OK, summary='Size: 5 B', details='Size: 5 B'),
+             Metric('license_size', 5.0, levels=(None, None), boundaries=(None, None)),
+             Result(state=state.WARN,
+                    summary='Usage: cannot calculate',
+                    details='Usage: cannot calculate'),
+             Result(state=state.WARN, summary='License: not FALSE', details='License: not FALSE'),
+             Result(state=state.WARN,
+                    summary='Expiration date: 2020-08-02 23:59:59.999999000',
+                    details='Expiration date: 2020-08-02 23:59:59.999999000'),
+         ],
+     )])
+def test_sap_hana_license_check(cur_item, result):
+    yielded_results = list(
+        sap_hana_license.check_sap_hana_license(cur_item, Parameters({}), SECTION))
+    assert yielded_results == result

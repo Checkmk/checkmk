655e5137619737d0f9db84540885e23523e589c3
Joerg Herbel <joerg.herbel@tribe29.com>
2020-08-11 07:55:08 +0200

    Check API: migration of section and check plugin netscaler_sslcertificates
    CMK-5085
    
    Change-Id: Ied8fe41fd77c28cdd3b44a8ced16cc360a5d9f71

---
+++ cmk/base/plugins/agent_based/netscaler_sslcertificates.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import (
+    Mapping,
+    Optional,
+)
+from .agent_based_api.v0 import (
+    check_levels,
+    register,
+    Service,
+    SNMPTree,
+    startswith,
+)
+from .agent_based_api.v0.type_defs import (
+    CheckGenerator,
+    DiscoveryGenerator,
+    SNMPStringTable,
+    Parameters,
+)
+
+# example SNMP output:
+#
+# .1.3.6.1.4.1.5951.4.1.1.56.1.1.1.20.67.79.77.79.68.79.95.82.83.65.95.67.101.114.116.95.65.117.116.104 COMODO_RSA_Cert_Auth
+# .1.3.6.1.4.1.5951.4.1.1.56.1.1.1.21.110.115.45.115.101.114.118.101.114.45.99.101.114.116.105.102.105.99.97.116.101 ns-server-certificate
+# .1.3.6.1.4.1.5951.4.1.1.56.1.1.1.25.67.79.77.79.68.79.95.82.83.65.95.67.101.114.116.95.65.117.116.104.95.82.111.111.116 COMODO_RSA_Cert_Auth_Root
+# .1.3.6.1.4.1.5951.4.1.1.56.1.1.5.20.67.79.77.79.68.79.95.82.83.65.95.67.101.114.116.95.65.117.116.104 4286
+# .1.3.6.1.4.1.5951.4.1.1.56.1.1.5.21.110.115.45.115.101.114.118.101.114.45.99.101.114.116.105.102.105.99.97.116.101 3655
+# .1.3.6.1.4.1.5951.4.1.1.56.1.1.5.25.67.79.77.79.68.79.95.82.83.65.95.67.101.114.116.95.65.117.116.104.95.82.111.111.116 1106
+
+Section = Mapping[str, int]
+
+
+def parse_netscaler_sslcertificates(string_table: SNMPStringTable) -> Section:
+    """
+    >>> parse_netscaler_sslcertificates([[['cert1', '3'], ['cert2', '100']]])
+    {'cert1': 3, 'cert2': 100}
+    """
+    return {certname: int(daysleft) for certname, daysleft in string_table[0]}
+
+
+register.snmp_section(
+    name="netscaler_sslcertificates",
+    parse_function=parse_netscaler_sslcertificates,
+    trees=[
+        SNMPTree(
+            base=".1.3.6.1.4.1.5951.4.1.1.56.1.1",
+            oids=[
+                "1",  # sslCertKeyName
+                "5",  # sslDaysToExpire
+            ],
+        ),
+    ],
+    detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.5951.1"),
+)
+
+
+def discover_netscaler_sslcertificates(section: Section) -> DiscoveryGenerator:
+    """
+    >>> list(discover_netscaler_sslcertificates({'cert1': 3, 'cert2': 100, '': 4}))
+    [Service(item='cert1', parameters={}, labels=[]), Service(item='cert2', parameters={}, labels=[])]
+    """
+    for certname in section:
+        if certname:
+            yield Service(item=certname)
+
+
+def _check_netscaler_sslcertificates(
+    item: str,
+    params: Parameters,
+    section: Section,
+    node_name: Optional[str] = None,
+) -> CheckGenerator:
+    if item not in section:
+        return
+    label = 'certificate valid for'
+    yield from check_levels(
+        section[item],
+        levels_lower=params['age_levels'],
+        metric_name='daysleft',
+        render_func=lambda d: str(d) + " days",
+        label=label if node_name is None else '[%s]: %s' % (node_name, label),
+    )
+
+
+def check_netscaler_sslcertificates(
+    item: str,
+    params: Parameters,
+    section: Section,
+) -> CheckGenerator:
+    yield from _check_netscaler_sslcertificates(
+        item,
+        params,
+        section,
+    )
+
+
+def cluster_check_netscaler_sslcertificates(
+    item: str,
+    params: Parameters,
+    section: Mapping[str, Section],
+) -> CheckGenerator:
+    for node_name, node_section in section.items():
+        yield from _check_netscaler_sslcertificates(
+            item,
+            params,
+            node_section,
+            node_name=node_name,
+        )
+
+
+register.check_plugin(
+    name="netscaler_sslcertificates",
+    service_name="SSL Certificate %s",
+    discovery_function=discover_netscaler_sslcertificates,
+    check_ruleset_name="netscaler_sslcerts",
+    check_default_parameters={
+        "age_levels": (30, 10),
+    },
+    check_function=check_netscaler_sslcertificates,
+    cluster_check_function=cluster_check_netscaler_sslcertificates,
+)

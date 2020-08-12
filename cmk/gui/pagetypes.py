7fcea1d8743faef6b68d85b3fa44fe0b971c2ac3
Lars Michelsen <lm@tribe29.com>
2020-08-12 16:18:44 +0200

    Revert "Add missing permission check"
    This reverts commit 11383b1e43b656f86df8004a820bca328f61fed1.

--- cmk/gui/pagetypes.py
+++ cmk/gui/pagetypes.py
@@ -656,9 +656,6 @@ class Overridable(Base):
         )
 
     def page_menu_entry_edit(self) -> PageMenuEntry:
-        if not self.may_edit():
-            return
-
         return PageMenuEntry(
             title=_("Edit properties"),
             icon_name="edit",

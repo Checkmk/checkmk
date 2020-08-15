33ac527974187801f4e418e5b9ad71890591cef8
Lars Michelsen <lm@tribe29.com>
2020-08-15 20:56:13 +0200

    Spread type hints
    Change-Id: I13d7251a2b5259c485095a5c35dcc199570ffcda

--- cmk/gui/plugins/wato/builtin_modules.py
+++ cmk/gui/plugins/wato/builtin_modules.py
@@ -1189,8 +1189,8 @@ class MainModuleAgentsLinux(MainModule):
 # Register the builtin agent download page on the top level of WATO only when the agent bakery
 # does not exist (e.g. when using CRE)
 if cmk_version.is_raw_edition():
-    main_module_registry.register(MainModuleAgentsWindows)
     main_module_registry.register(MainModuleAgentsLinux)
+    main_module_registry.register(MainModuleAgentsWindows)
 
 
 @main_module_registry.register

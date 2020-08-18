80bac99f713ce954da863eef5a1cea07b38dc56d
Mathias Laurin <mathias.laurin@tribe29.com>
2020-08-18 11:49:01 +0200

    Partly revert aac251ab60
    The "fix" from aac251ab60 did not correct the bug but hid it.
    Now that we have a real fix, we can remove the workaround.
    
    Change-Id: I0feba7eb23a763f7155d7d0511d045f568a3baf9

--- cmk/fetchers/tcp.py
+++ cmk/fetchers/tcp.py
@@ -30,7 +30,7 @@ class TCPFetcher(AgentFetcher):
         # type (...) -> None
         super(TCPFetcher, self).__init__()
         self._family = socket.AddressFamily(family)
-        self._address = (address[0], address[1])
+        self._address = address
         self._timeout = timeout
         self._encryption_settings = encryption_settings
         self._logger = logging.getLogger("cmk.fetchers.tcp")

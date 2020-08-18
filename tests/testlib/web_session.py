377abbf4ea3f1981e14a34fb5a6e1afae888c6d4
Lars Michelsen <lm@tribe29.com>
2020-08-18 17:44:40 +0200

    Try to fix GUI crawl after user profile page change
    Change-Id: Ie412bf512269a0ee7e9807aefeb7fe0b204e9ec4

--- tests/testlib/web_session.py
+++ tests/testlib/web_session.py
@@ -217,6 +217,8 @@ class CMKWebSession:
                 "ua_start_url_use": "0",
                 "ua_ui_theme_use": "0",
                 # Encoded None using DropdownChoice.option_id
+                "ua_ui_sidebar_position": "dc937b59892604f5a86ac96936cd7ff09e25f18ae6b758e8014a24c7fa039e91",
+                "ua_icons_per_item": "dc937b59892604f5a86ac96936cd7ff09e25f18ae6b758e8014a24c7fa039e91",
                 "ua_ui_basic_advanced_mode": "dc937b59892604f5a86ac96936cd7ff09e25f18ae6b758e8014a24c7fa039e91",
                 "language": lang,
                 "_save": "Save",

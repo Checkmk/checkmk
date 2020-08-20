bdb761995ce032e46042f2ea09a4c29d579ecf65
Andreas Boesl <andreas.boesl@tribe29.com>
2020-08-20 11:10:37 +0200

    snmp_scan function could not handle multiline strings -> re.DOTALL
    Change-Id: Ie5a6f36341b8cdc96d8137dfb3172984b3b54630

--- cmk/snmplib/snmp_scan.py
+++ cmk/snmplib/snmp_scan.py
@@ -59,7 +59,7 @@ def _evaluate_snmp_detection_atom(
         # check for "not_exists"
         return pattern == ".*" and not flag
     # ignore case!
-    return bool(regex(pattern, re.IGNORECASE).fullmatch(value)) is flag
+    return bool(regex(pattern, re.IGNORECASE | re.DOTALL).fullmatch(value)) is flag
 
 
 # gather auto_discovered check_plugin_names for this host

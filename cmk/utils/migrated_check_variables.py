8facc9cb84298f0aaf0b4cc1a11b2f45b3e7e77b
Joerg Herbel <joerg.herbel@tribe29.com>
2020-09-01 15:08:54 +0200

    Remove some unnecessary variables from migrated_check_variables
    Change-Id: Ie389e23127bb725e9aff913d3beed9618b648cee

--- cmk/utils/migrated_check_variables.py
+++ cmk/utils/migrated_check_variables.py
@@ -26,56 +26,5 @@ ntp_default_levels = (10, 200.0, 500.0)  # stratum, ms sys_time_offset_offset
 # hr_mem (amongst others)
 memused_default_levels = (150.0, 200.0)
 
-# netscaler_vserver
-netscaler_vserver_states = {
-    "0": (1, "unknown"),
-    "1": (2, "down"),
-    "2": (1, "unknown"),
-    "3": (1, "busy"),
-    "4": (1, "out of service"),
-    "5": (1, "transition to out of service"),
-    "7": (0, "up"),
-}
-netscaler_vserver_types = {
-    "0": "http",
-    "1": "ftp",
-    "2": "tcp",
-    "3": "udp",
-    "4": "ssl bridge",
-    "5": "monitor",
-    "6": "monitor udp",
-    "7": "nntp",
-    "8": "http server",
-    "9": "http client",
-    "10": "rpc server",
-    "11": "rpc client",
-    "12": "nat",
-    "13": "any",
-    "14": "ssl",
-    "15": "dns",
-    "16": "adns",
-    "17": "snmp",
-    "18": "ha",
-    "19": "monitor ping",
-    "20": "sslOther tcp",
-    "21": "aaa",
-    "23": "secure monitor",
-    "24": "ssl vpn udp",
-    "25": "rip",
-    "26": "dns client",
-    "27": "rpc server",
-    "28": "rpc client",
-    "62": "service unknown",
-    "69": "tftp",
-}
-netscaler_vserver_entitytypes = {
-    "0": "unknown",
-    "1": "loadbalancing",
-    "2": "loadbalancing group",
-    "3": "ssl vpn",
-    "4": "content switching",
-    "5": "cache redirection",
-}
-
 # ps
 ps_default_levels = {"levels": (1, 1, 99999, 99999)}

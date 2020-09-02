4897e014be21f200fe547e65b47987301b5e40fb
Joerg Herbel <joerg.herbel@tribe29.com>
2020-09-02 09:57:56 +0200

    Migration of statgrab_net
    CMK-5321
    
    Change-Id: Iea6d7a5d9ab709040e65ed07029f965108677243

--- tests/unit/checks/test_generic_legacy_conversion.py
+++ tests/unit/checks/test_generic_legacy_conversion.py
@@ -2736,7 +2736,6 @@ def test_no_new_or_vanished_legacy_checks(config_check_info):
         'statgrab_cpu',
         'statgrab_disk',
         'statgrab_load',
-        'statgrab_net',
         'steelhead_connections',
         'steelhead_peers',
         'steelhead_status',

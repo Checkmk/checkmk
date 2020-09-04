f4eca3d8c69bfa73821654707093f46519c5e9b9
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-09-04 09:54:56 +0200

    logwatch: migrate reclassification to utils
    Change-Id: Ic4352251652d1eea131f8b2d3bf787fa906e5a80

--- cmk/base/plugins/agent_based/utils/logwatch.py
+++ cmk/base/plugins/agent_based/utils/logwatch.py
@@ -14,7 +14,11 @@
 #                                                                                       #
 #########################################################################################
 
-from typing import Dict, List, TypedDict
+from typing import Any, Counter, Dict, List, TypedDict
+
+import re
+
+from ..agent_based_api.v0 import regex
 
 ItemData = TypedDict(
     "ItemData",
@@ -33,3 +37,32 @@ SectionLogwatch = TypedDict(
     },
     total=True,
 )
+
+
+def reclassify(
+    counts: Counter[int],
+    patterns: Dict[str, Any],  # all I know right now :-(
+    text: str,
+    old_level: str,
+) -> str:
+
+    # Reclassify state if a given regex pattern matches
+    # A match overrules the previous state->state reclassification
+    for level, pattern, _ in patterns.get("reclassify_patterns", []):
+        reg = regex(pattern, re.UNICODE)
+        if reg.search(text):
+            # If the level is not fixed like 'C' or 'W' but a pair like (10, 20),
+            # then we count how many times this pattern has already matched and
+            # assign the levels according to the number of matches of this pattern.
+            if isinstance(level, tuple):
+                warn, crit = level
+                newcount = counts[id(pattern)] + 1
+                counts[id(pattern)] = newcount
+                if newcount >= crit:
+                    return 'C'
+                return 'W' if newcount >= warn else 'I'
+            return level
+
+    # Reclassify state to another state
+    change_state_paramkey = ("%s_to" % old_level).lower()
+    return patterns.get("reclassify_states", {}).get(change_state_paramkey, old_level)

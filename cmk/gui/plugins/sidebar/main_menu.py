eb05a67b863b0eda4b9c8765d500e20f5d5e15d9
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2020-09-07 17:18:22 +0200

    Use flex-grow on a sentinel
    The flex-grow value of 1000 is now used on a separate
    sentinel div. This is necessary since "display: none"
    may be set on the last-child so that its flex-grow
    value is not respected.
    
    Change-Id: I3510cfa5e66bd103b434f0565bb86cdbf35c0e4b

--- cmk/gui/plugins/sidebar/main_menu.py
+++ cmk/gui/plugins/sidebar/main_menu.py
@@ -119,6 +119,7 @@ class MegaMenuRenderer:
         html.open_div(class_="content inner")
         for topic in topics:
             self._show_topic(topic, menu.name)
+        html.div(None, class_="sentinel")
         html.close_div()
         html.close_div()
         html.javascript(hide_entries_js)

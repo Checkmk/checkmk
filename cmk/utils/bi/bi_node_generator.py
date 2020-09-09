993578b269b419ec8d8c3472f779a9225eb49c15
Andreas Boesl <andreas.boesl@tribe29.com>
2020-09-09 16:18:38 +0200

    BI rework: bi_node_generator: creates branches/leafs from nodes
    Change-Id: I526612b6feb97eafbea15d9446d3321f6deda791

---
+++ cmk/utils/bi/bi_node_generator.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+#   .--NodeGen.------------------------------------------------------------.
+#   |              _   _           _       ____                            |
+#   |             | \ | | ___   __| | ___ / ___| ___ _ __                  |
+#   |             |  \| |/ _ \ / _` |/ _ \ |  _ / _ \ '_ \                 |
+#   |             | |\  | (_) | (_| |  __/ |_| |  __/ | | |_               |
+#   |             |_| \_|\___/ \__,_|\___|\____|\___|_| |_(_)              |
+#   |                                                                      |
+#   +----------------------------------------------------------------------+
+
+from marshmallow import Schema  # type: ignore[import]
+from typing import List, Type
+
+from cmk.utils.bi.bi_lib import (
+    AbstractBICompiledNode,
+    ReqNested,
+    MacroMappings,
+)
+
+from cmk.utils.bi.bi_actions import (
+    BIActionSchema,
+    BICallARuleAction,
+    BIStateOfHostActionSchema,
+)
+
+from cmk.utils.bi.bi_node_generator_interface import AbstractBINodeGenerator
+from cmk.utils.bi.bi_search import BISearchSchema
+
+
+class BINodeGenerator(AbstractBINodeGenerator):
+    @classmethod
+    def schema(cls) -> Type["BINodeGeneratorSchema"]:
+        return BINodeGeneratorSchema
+
+    def compile(self, macros: MacroMappings) -> List[AbstractBICompiledNode]:
+        action_results = []
+        search_results = self.search.execute(macros)
+        for search_result in search_results:
+            action_arguments = macros.copy()
+            action_arguments.update(search_result)
+            if self.restrict_rule_title is not None and isinstance(self.action, BICallARuleAction):
+                rule_title = self.action.preview_rule_title(action_arguments)
+                if rule_title != self.restrict_rule_title:
+                    continue
+
+            action_results.extend(self.action.execute(action_arguments))
+        return action_results
+
+
+class BINodeGeneratorSchema(Schema):
+    search = ReqNested(BISearchSchema, default={"type": "empty"}, example={"type": "empty"})
+    action = ReqNested(BIActionSchema,
+                       default=BIStateOfHostActionSchema().dump({}).data,
+                       example=BIStateOfHostActionSchema().dump({
+                           "host_regex": "testhost"
+                       }).data)

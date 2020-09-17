ae69f83ad22fbfb4b0ac576ca4da9f3c7042dfb4
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-09-17 14:54:02 +0200

    migrate eval_regex.include
    Change-Id: I658c4a665ef5cb3da9b79d5136862429d4def8ea

--- cmk/base/plugins/agent_based/utils/eval_regex.py
+++ cmk/base/plugins/agent_based/utils/eval_regex.py
@@ -4,9 +4,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import re
+
 
 def instantiate_regex_pattern_once(pattern, match):
-    import cmk.utils.regex
     # this correctly handles \( and \) but not [^)] - sorry
-    return re.compile(r"(?<!\\)\(.*?(?<!\\)\)").sub(cmk.utils.regex.escape_regex_chars(match),
-                                                    pattern, 1)
+    return re.compile(r"(?<!\\)\(.*?(?<!\\)\)").sub(re.escape(match), pattern, 1)

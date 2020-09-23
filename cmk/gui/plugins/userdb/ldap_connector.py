8118706fbc8140b8a15d4133715c57133676d3a3
Ronny Bruska <ronny.bruska@tribe29.com>
2020-09-23 09:59:59 +0200

    11481 FIX LDAP: Fix AttributeError if attribute "Disable Notifications" is used
    SUP-4169
    
    Change-Id: I0117fabac7b177d66d89e7f75f05bfc526cc1012

--- cmk/gui/plugins/userdb/ldap_connector.py
+++ cmk/gui/plugins/userdb/ldap_connector.py
@@ -2048,7 +2048,12 @@ def ldap_filter_of_connection(connection_id, *args, **kwargs):
 
 def ldap_sync_simple(user_id, ldap_user, user, user_attr, attr):
     if attr in ldap_user:
-        return {user_attr: ldap_user[attr][0]}
+        attr_value = ldap_user[attr][0]
+        if user_attr != 'disable_notifications':
+            return {user_attr: attr_value}
+        # LDAP attribute in boolean format sends str "TRUE" or "FALSE"
+        if attr_value == 'TRUE':
+            return {user_attr: {'disable': True}}
     return {}
 
 

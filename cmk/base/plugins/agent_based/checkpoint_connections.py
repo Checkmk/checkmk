da20bb122ee19041559a34e73c0b155aa3bb7a06
Gerd Stolz <stolz.gerd@gmx.de>
2020-09-24 18:16:50 +0200

    Port checkpoint connections to new api + include peak + maximum values
    started typing + finished check + parse function
    
    Finished testing. TODO: what is params['auto-migrated...']`
    
    Include style tipps provided by Moritz Kiemer
    
    Use fetch instead of trees in snmp_scan block

--- cmk/base/plugins/agent_based/checkpoint_connections.py
+++ cmk/base/plugins/agent_based/checkpoint_connections.py
@@ -7,18 +7,80 @@
 # .1.3.6.1.2.1.1.1.0 Linux gateway1 2.6.18-92cp #1 SMP Tue Dec 4 21:44:22 IST 2012 i686
 # .1.3.6.1.4.1.2620.1.1.25.3.0 19190
 
-from .agent_based_api.v1 import register, SNMPTree
+from collections import namedtuple
+
+from .agent_based_api.v1 import check_levels, register, Result, Service, SNMPTree, state
+from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, Parameters, SNMPStringTable
 from .utils import checkpoint
 
+checkpoint_connections_default_levels = {"pct": (80, 90)}
 
-def parse_checkpoint_connections(string_table):
-    raw_value = string_table[0][0][0]
-    return {"count": int(raw_value)}
+Section = namedtuple('Section', 'current peak maximum')
+
+
+def parse_checkpoint_connections(string_table: SNMPStringTable) -> Section:
+    current_raw_value = string_table[0][0][0]
+    peak_raw_value = string_table[0][0][1]
+    maximum_raw_value = string_table[0][0][2]
+    return Section(
+        current=int(current_raw_value),
+        peak=int(peak_raw_value),
+        maximum=int(maximum_raw_value),
+    )
 
 
 register.snmp_section(
     name="checkpoint_connections",
     parse_function=parse_checkpoint_connections,
     detect=checkpoint.DETECT,
-    fetch=[SNMPTree(base=".1.3.6.1.4.1.2620.1.1.25", oids=['3'])],
+    fetch=[
+        SNMPTree(
+            base=".1.3.6.1.4.1.2620.1.1.25",
+            oids=[
+                '3',  # CHECKPOINT-MIB - fwNumConn - current connections
+                '4',  # CHECKPOINT-MIB - fwPeakNumConn - peak number of connections
+                '10',  # CHECKPOINT-MIB - fwConnTableLimit - connection table limit
+            ])
+    ],
+)
+
+
+def discover_checkpoint_connections(section: Section) -> DiscoveryResult:
+    yield Service()
+
+
+def check_checkpoint_connections(
+    params: Parameters,
+    section: Section,
+) -> CheckResult:
+    maximum = section.maximum
+    #infotext = "%d current, %d peak, %d maximum" % (current, peak, maximum)
+
+    if "pct" in params and maximum > 0:
+        warn_pct, crit_pct = params["pct"]
+        warn = maximum * warn_pct / 100
+        crit = maximum * crit_pct / 100
+    else:
+        # up until cmk 1.6 this check only supported user set absolute values
+        warn, crit = params["auto-migration-wrapper-key"]
+
+    upper_boundary = maximum if maximum > 0 else None
+    yield from check_levels(
+        value=section.current,
+        levels_upper=(warn, crit),
+        metric_name="connections",
+        boundaries=(0, upper_boundary),
+        label="Current connections",
+    )
+    yield Result(state=state.OK,
+                 summary="Peak: {0.peak}, Connection table limit: {0.maximum}".format(section))
+
+
+register.check_plugin(
+    name="checkpoint_connections",
+    service_name="Connections",
+    discovery_function=discover_checkpoint_connections,
+    check_function=check_checkpoint_connections,
+    check_default_parameters=checkpoint_connections_default_levels,
+    check_ruleset_name="checkpoint_connections",
 )

245b506c59dc7db139b93e3349b8cbaa80209ae0
Lisa Pichler <lisa.pichler@tribe29.com>
2020-09-30 15:10:28 +0200

    11327 filestats: added additional rules functionality
    The check parameter 'additional_rules' allow the user to configure
    additional levels for max/min size/age for files within a file group.
    These files are matched by regular expression and can be a file group
    themselves or single files.
    
    CMK-5537
    
    Change-Id: Ib63f0c39001a93112c959b889015829520d5a160

--- tests/unit/checks/generictests/datasets/filestats_additional_rules_regression.py
+++ tests/unit/checks/generictests/datasets/filestats_additional_rules_regression.py
@@ -11,7 +11,7 @@ checkname = 'filestats'
 info = [
     ['[[[file_stats foo]]]'],
     [
-        "{'age': 21374, 'mtime': 1600757875, 'path': '/var/log/boot.log', 'size': 0, 'stat_status': 'ok', 'type': 'file'}"
+        "{'age': 21374, 'mtime': 1600757875, 'path': '/var/log/boot.log', 'size': 141894, 'stat_status': 'ok', 'type': 'file'}"
     ],
     [
         "{'stat_status': 'ok', 'age': 0, 'mtime': 160779533, 'path': '/var/log/syslog', 'type': 'file', 'size': 13874994}"
@@ -32,19 +32,32 @@ discovery = {'': [('foo', {})], 'single': []}
 checks = {
     '': [
         (
-            'foo', {'maxsize_largest': (1, 2), 'show_all_files': True}, [
+            'foo', {
+                'maxsize_largest': (4, 5),
+                'additional_rules':
+                [('/var/log/sys*', {
+                    'maxsize_largest': (1, 2)
+                })],
+                'show_all_files': True,
+            }, [
                 (
                     0, 'Files in total: 5', [
                         ('file_count', 5, None, None, None, None)
                     ]
                 ),
-                (0, 'Smallest: 0.00 B', []),
+                (0, 'Smallest: 479.00 B', []),
+                (2, 'Largest: 138.57 kB (warn/crit at 4.00 B/5.00 B)', []),
+                (0, 'Newest: 356 m', []),
+                (0, 'Oldest: 53 d', []),
+                (0, "Files matching '/var/log/sys*': 3", []),
+                (0, 'Smallest: 5.07 MB', []),
                 (2, 'Largest: 21.10 MB (warn/crit at 1.00 B/2.00 B)', []),
-                (0, 'Newest: 0.00 s', []), (0, 'Oldest: 53 d', []),
-                (0, '\n(Remaining) files in file group:\n[/var/log/apport.log.2.gz] Age: 53 d, Size: 479.00 B(!!)\n[/var/log/syslog] Age: 0.00 s, Size: 13.23 MB(!!)\n[/var/log/syslog.1] Age: 19 d, Size: 21.10 MB(!!)\n[/var/log/syslog.3.gz] Age: 47 d, Size: 5.07 MB(!!)',
-                    []
-                )
+                (0, 'Newest: 0.00 s', []),
+                (0, 'Oldest: 47 d', []),
+                (0, '\nFiles matching \'/var/log/sys*\':\n', []),
+                (0, '[/var/log/syslog] Age: 0.00 s, Size: 13.23 MB(!!)\n[/var/log/syslog.1] Age: 19 d, Size: 21.10 MB(!!)\n[/var/log/syslog.3.gz] Age: 47 d, Size: 5.07 MB(!!)', []),
+                (0, '\n(Remaining) files in file group:\n[/var/log/apport.log.2.gz] Age: 53 d, Size: 479.00 B(!!)\n[/var/log/boot.log] Age: 356 m, Size: 138.57 kB(!!)', []),
             ]
-        )
+        ),
     ]
 }

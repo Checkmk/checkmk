2ea4a99a45445ab6aedffd6ac336b44713190b52
Lukas Lengler <lukas.lengler@tribe29.com>
2020-10-07 11:17:15 +0200

    Added Version to all agent plugins #3
    All shell script files in agent/plugins
    
    Change-Id: I0727181311c12a753c36d2ca8821b0e317679a8b

--- agents/plugins/asmcmd.sh
+++ agents/plugins/asmcmd.sh
@@ -3,4 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+VERSION="2.0.0i1"
+
 su - griduser -c "asmcmd $@"

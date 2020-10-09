20e041a8e06b37dd4a116f257595b71bf9fba04f
Lukas Lengler <lukas.lengler@tribe29.com>
2020-10-09 13:01:43 +0200

    Fixed NoSuchInstance and noSuchName in PySNMP Backend
    If the PySNMP returns those values the Backend should return None.
    This was already implemented in walks but was missing in get.
    noSuchName: SNMPv1
    NoSuchInstance: SNMPv2/3
    
    Change-Id: Ia5eadbf1c9e395847e13c8144a5388977c48c7d9

--- tests/integration/cmk/lib/snmplib/backend_snmp.py
+++ tests/integration/cmk/lib/snmplib/backend_snmp.py
@@ -44,13 +44,14 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.parametrize("oid", [
     (".1.3.6.1.2.1.4.12.0"),
+    (".1.3.6.1"),
     (".1.3.6.1.2.1.2.2.1.6.1"),
     (".1.3.6.1.2.1.2.2.1.6.*"),
 ])
 def test_get_ipv4(oid: OID):
     configs = _create_configs_ipv4()
     result_pysnmp, result_inline, result_classic = _create_results_snmpbackend_get(oid, configs)
-
+    print(result_pysnmp, result_inline, result_classic)
     assert result_pysnmp == result_inline == result_classic
 
 

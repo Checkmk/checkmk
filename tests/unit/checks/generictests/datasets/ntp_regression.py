349e7efce532b8ee551abca5d44ba8475780c873
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-10-12 15:39:53 +0200

    ntp.summary: move peer info to details
    Change-Id: I820928f4b66ead50467bcc8e4cf09e4770e76cd4

--- tests/unit/checks/generictests/datasets/ntp_regression.py
+++ tests/unit/checks/generictests/datasets/ntp_regression.py
@@ -27,7 +27,7 @@ discovery = {
 checks = {
     'time': [
         (None, {'alert_delay': (300, 3600), 'ntp_levels': (10, 200.0, 500.0)}, [
-            (0, 'found 1 peers, but none is suitable', []),
+            (0, 'Found 1 peers, but none is suitable', []),
             (0, 'Just started monitoring', []),
         ]),
     ],

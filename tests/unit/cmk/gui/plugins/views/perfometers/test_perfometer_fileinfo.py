d12eca49787222fd111b002d6720b1ae1febd38d
Lisa Pichler <lisa.pichler@tribe29.com>
2020-10-26 17:04:57 +0100

    fileinfo.groups: added test for GUI perfometer transformation
    CMK-6051
    
    Change-Id: I97b0846a6a70e2a8b3b9a375c5f90a117ea94874

---
+++ tests/unit/cmk/gui/plugins/views/perfometers/test_perfometer_fileinfo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+
+from cmk.gui.htmllib import HTML
+from cmk.gui.plugins.views.perfometers.check_mk import (
+    perfometer_fileinfo_groups,)
+
+
+@pytest.mark.parametrize("perf_data, expected_result", [
+    (
+        [
+            ('count', 1, '', None, None, None, None),
+            ('size', 0, '', None, None, None, None),
+            ('size_largest', 0, '', None, None, None, None),
+            ('size_smallest', 0, '', None, None, None, None),
+            ('age_oldest', 5, '', None, None, None, None),
+            ('age_newest', 5, '', None, None, None, None),
+        ],
+        (
+            '0 Tot / 0.00 s',
+            HTML(
+                "<div class=\"stacked\"><table><tr><td style=\"width: 0%; background-color: #aabb50\" class=\"inner\"></td>"
+                "<td style=\"width: 100%; background-color: #ffffff\" class=\"inner\"></td></tr></table>"
+                "<table><tr><td style=\"width: 0%; background-color: #ccff50\" class=\"inner\"></td>"
+                "<td style=\"width: 100%; background-color: #ffffff\" class=\"inner\"></td></tr></table></div>"
+            ),
+        ),
+    ),
+])
+def test_perfometer_fileinfo_groups(perf_data, expected_result, module_wide_request_context):
+    assert perfometer_fileinfo_groups('', '', perf_data) == expected_result

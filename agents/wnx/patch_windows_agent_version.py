92fbf9af3f732997bb02a22d5258372fcc7f60c8
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-10-30 17:37:20 +0100

    Provide possibility to change version of the Windows Client in Windows directly.
    add simple python script to patch Windows agent version in Windows
    
    Usage:
    make -C agents\wnx NEW_VERSION="2.0.0i1" setversion
    
    Background. In Windows sed works extremely strange and call it from the makefile may be impossible(environment variables have not been inlined in sed command line). Powershell uses not very standard/strange regex extension.
    
    Change-Id: I7953fb8dcd4eb5feda34e6b3ca0bd530308783c5

---
+++ agents/wnx/patch_windows_agent_version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# Simple python script to replace version in the file
+# first param is a file usually wnx\src\common\wnx_version.h
+# second param is version without double quotes, for example, 2.0.0i1
+
+import sys
+import re
+
+content = ""
+with open(sys.argv[1], "r") as f:
+    content = f.read()
+
+with open(sys.argv[1], "w") as f:
+    pattern = r'(^#define CMK_WIN_AGENT_VERSION )("[^"]*")'
+    ret = re.sub(pattern, f'\\1"{sys.argv[2]}"', content)
+    f.write(ret)

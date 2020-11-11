61af5d8a4ca28157a0d6de3c06a8821fabaf2e87
Frans Fürst <frans.fuerst@tribe29.com>
2020-11-11 11:51:02 +0100

    11543 FIX Wrong handling of 'None' values in bonding checks
    Discovery and check functions for *_bonding would are provided with "None" instead of
    None values but don't handle them correctly. This fix doesn't turn "None" into None
    but handles those values correctly
    
    Change-Id: I7e816afb76f3f7bc95a1b29f332d6db25bbdfdba

--- cmk/base/check_legacy_includes/bonding.py
+++ cmk/base/check_legacy_includes/bonding.py
@@ -5,6 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 # type: ignore[var-annotated,list-item,import,assignment,misc,operator]  # TODO: see which are needed in this file
+
 BONDING_CHECK_DEFAULT_PARAMETERS = {
     'ieee_302_3ad_agg_id_missmatch_state': 1,
     'expect_active': 'ignore',
@@ -12,17 +13,27 @@ BONDING_CHECK_DEFAULT_PARAMETERS = {
 
 
 def inventory_bonding(parsed):
-    inventory = []
-    for bond, status in parsed.items():
-        if status["status"] in ("up", "degraded"):
-            # If no information about primary interface is available
-            # then assume currently active one as primary
-            if "primary" not in status and "active" in status:
-                params = {"primary": status["active"]}
-            else:
-                params = {}
-            inventory.append((bond, params))
-    return inventory
+    """Return bonds together with params containing the active interface as primary one in case
+    there is no primary interface configured.
+    >>> inventory_bonding({'bond0': {
+    ...     'status': 'up',
+    ...     'mode': 'fault-tolerance',
+    ...     'interfaces': {
+    ...         'eth2': {'status': 'up', 'hwaddr': 'aa:bb:cc:dd:ee:ff', 'failures': 1},
+    ...         'eth3': {'status': 'down', 'hwaddr': '11:22:33:44:55:66', 'failures': 0}},
+    ...     'active': 'eth2',
+    ...     'primary': 'None'}})
+    [('bond0', {'primary': 'eth2'})]
+    """
+    return [
+        (bond, params)  #
+        for bond, props in parsed.items()  #
+        if props["status"] in {"up", "degraded"}  #
+        for primary, active in ((props.get("primary", "None"), props.get("active", "None")),)  #
+        for params in ({} if primary != "None" or active == "None" else {
+            "primary": active
+        },)
+    ]
 
 
 def _check_ieee_302_3ad_specific(params, status):
@@ -38,52 +49,71 @@ def _check_ieee_302_3ad_specific(params, status):
 
 
 def check_bonding(item, params, parsed):
-    status = parsed.get(item)
-    if not status:
+    """
+    >>> for result in check_bonding(
+    ...     "bond0", {
+    ...        'ieee_302_3ad_agg_id_missmatch_state': 1,
+    ...        'expect_active': 'ignore',
+    ...        'primary': 'eth2'},
+    ...     {'bond0': {
+    ...         'status': 'up',
+    ...         'mode': 'fault-tolerance',
+    ...         'interfaces': {
+    ...             'eth2': {'status': 'up', 'hwaddr': 'f8:4f:57:72:11:34', 'failures': 1},
+    ...             'eth3': {'status': 'down', 'hwaddr': 'f8:4f:57:72:11:36', 'failures': 0}},
+    ...         'active': 'eth2',
+    ...         'primary': 'None'}}):
+    ...   print(result)
+    (0, 'Mode: fault-tolerance')
+    (0, 'Primary: eth2')
+    (0, 'eth2/f8:4f:57:72:11:34 up')
+    (1, 'eth3/f8:4f:57:72:11:36 down')
+    (0, 'Bond status: up')
+    """
+    properties = parsed.get(item)
+    if not properties:
+        return  # => status: UNKN
+
+    if properties["status"] not in {"up", "degraded"}:
+        yield 2, "Interface is " + properties["status"]
         return
 
-    if status["status"] not in ("up", "degraded"):
-        yield 2, "Interface is " + status["status"]
-        return
+    active_if = properties.get("active", "None")
+    if active_if == "None":
+        yield 2, "No active interface"
 
-    mode = status["mode"]
+    mode = properties["mode"]
     yield 0, "Mode: %s" % mode
     if "IEEE 802.3ad" in mode:
-        for result in _check_ieee_302_3ad_specific(params, status):
-            yield result
+        yield from _check_ieee_302_3ad_specific(params, properties)
 
-    speed = status.get('speed')
+    speed = properties.get('speed')
     if speed:
         yield 0, "Speed: %s" % speed
 
-    for eth, slave in status["interfaces"].items():
-        state = int(slave["status"] != 'up')
-        if "hwaddr" in slave:
-            yield state, "%s/%s %s" % (eth, slave["hwaddr"], slave["status"])
-        else:
-            yield state, "%s %s" % (eth, slave["status"])
-
-    primary = status.get("primary", params.get("primary"))
-    if primary:
+    current_primary = properties.get("primary", "None")
+    primary = current_primary if current_primary != "None" else params.get("primary", "None")
+    if primary != "None":
         yield 0, "Primary: %s" % primary
 
-    active = status.get("active")
-    if active:
-        state = 0
-        info = "Active: %s" % active
+    expected_active = {
+        "primary": primary,
+        "lowest": min(properties["interfaces"]),
+        "ignore": None,
+    }.get(params["expect_active"])
 
-        expected_active = None
-        expect = params["expect_active"]
+    if expected_active is None:
+        # we don't expect an interface to be up and others to be down so check whether all
+        # interfaces are up
+        for eth, slave in properties["interfaces"].items():
+            state = 0 if slave["status"] == 'up' else 1
+            if "hwaddr" in slave:
+                yield state, "%s/%s %s" % (eth, slave["hwaddr"], slave["status"])
+            else:
+                yield state, "%s %s" % (eth, slave["status"])
+    elif expected_active == active_if:
+        yield 0, "Active: %s" % active_if
+    else:
+        yield 1, "Active: %s (expected is %s)" % (active_if, expected_active)
 
-        if expect == "primary":
-            expected_active = primary
-        elif expect == "lowest":
-            expected_active = min(status["interfaces"])
-
-        if expected_active is not None and expected_active != active:
-            state = 1
-            info += " (expected is %s)" % expected_active
-
-        yield state, info
-
-    yield 0 if status["status"] == "up" else 1, "Bond status: %s" % status["status"]
+    yield 0 if properties["status"] == "up" else 1, "Bond status: %s" % properties["status"]

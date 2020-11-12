90a3547bede4724e8b7faa4a7174ca90d2ceb0e1
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2020-11-12 12:49:00 +0100

    First steps torwards SW Docu
    * Add first uml diagram
    * Provide dockerfile for plotting umls with plantuml
    * Add script for generating pngs from .uml
    
    CMK-6177
    
    Change-Id: I0ce7936bc55c185707194aed5defc829afc31269

---
+++ doc/sw-architecture/uml-to-png.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OUTPUT_DIR="./output/"
+
+docker build --tag plantuml .
+
+uml_files=$(ls *.uml)
+
+for uml_file in ${uml_files}
+do
+   :
+   echo "Creating png from $uml_file"
+   cat "$uml_file" | docker run --rm -i plantuml:latest -tpng > $OUTPUT_DIR${uml_file/.uml/.png}
+done
+

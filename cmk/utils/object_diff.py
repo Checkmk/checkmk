af5b1216fe6f5d5bb8615f03e36d3f8f7607cdca
Lars Michelsen <lm@tribe29.com>
2020-11-12 09:57:59 +0100

    Add helper module to create a description of object changes
    make_object_diff from cmk.utils.object_diff uses the deepdiff module to
    get an idea about the object changes and returns a text describing the
    changes for users.
    
    CMK-6187
    
    Change-Id: I704278dbd36ec0a953646e98d20206dbb820a42e

---
+++ cmk/utils/object_diff.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import Any
+
+from deepdiff import DeepDiff  # type: ignore[import]
+from deepdiff.helper import get_type  # type: ignore[import]
+
+ObjectDiff = str
+
+__all__ = [
+    "make_object_diff",
+    "ObjectDiff",
+]
+
+
+def make_object_diff(old: Any, new: Any) -> str:
+    """Creates a text representing the object differences for humans"""
+    diff = DeepDiff(old, new, view="tree")
+    return pretty(diff)
+
+
+def pretty(diff: DeepDiff) -> str:
+    """Copy of DeepDiff.pretty() to execute our own pretty_print_diff()"""
+    result = []
+    keys = sorted(
+        diff.tree.keys())  # sorting keys to guarantee constant order across python versions.
+    for key in keys:
+        for item_key in diff.tree[key]:
+            result += [pretty_print_diff(item_key)]
+
+    return '\n'.join(result)
+
+
+PRETTY_FORM_TEXTS = {
+    "type_changes": "Value of {diff_path} changed from {val_t1} to {val_t2}.",
+    "values_changed": "Value of {diff_path} changed from {val_t1} to {val_t2}.",
+    "dictionary_item_added": "Attribute {diff_path} with value {val_t2} added.",
+    "dictionary_item_removed": "Attribute {diff_path} with value {val_t1} removed.",
+    "iterable_item_added": "Item {diff_path} with value {val_t2} added.",
+    "iterable_item_removed": "Item {diff_path} with value {val_t1} removed.",
+    "attribute_added": "Attribute {diff_path} with value {val_t2} added.",
+    "attribute_removed": "Attribute {diff_path} with value {val_t1} removed.",
+    "set_item_added": "Item {val_t2} with value {val_t2} added.",
+    "set_item_removed": "Item {val_t1} with value {val_t1} removed.",
+    "repetition_change": "Repetition change for item {diff_path}.",
+}
+
+
+def pretty_print_diff(diff: DeepDiff) -> str:
+    """Copy of deepdiff.serialization.pretty_print_diff to slighlty adapt the output format"""
+    type_t1 = get_type(diff.t1).__name__
+    type_t2 = get_type(diff.t2).__name__
+
+    val_t1 = '"{}"'.format(str(diff.t1)) if type_t1 == "str" else str(diff.t1)
+    val_t2 = '"{}"'.format(str(diff.t2)) if type_t2 == "str" else str(diff.t2)
+
+    return PRETTY_FORM_TEXTS.get(diff.report_type, "").format(diff_path=diff_path(diff),
+                                                              type_t1=type_t1,
+                                                              type_t2=type_t2,
+                                                              val_t1=val_t1,
+                                                              val_t2=val_t2)
+
+
+def diff_path(diff: DeepDiff) -> str:
+    """Reformat diff path to be a little more intuitive
+
+    We could also build our own path renderer (See DiffLevel.path of deepdiff), but this seems to be
+    good enough for the moment.
+    """
+    path = diff.path(root='')
+    if path == "":
+        return "object"  # The whole object changed it's value (e.g. "a" -> "b")
+
+    return path.strip("[]").replace("['", "").replace("']", "/").replace("'", "\"")
---
+++ tests/unit/cmk/utils/test_object_diff.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.utils.object_diff import make_object_diff
+
+
+def test_make_object_diff_nothing_changed():
+    assert make_object_diff(None, None) == ''
+    assert make_object_diff({}, {}) == ''
+    assert make_object_diff([], []) == ''
+
+
+def test_make_object_diff_str():
+    assert make_object_diff("a", "b") == 'Value of object changed from "a" to "b".'
+
+
+def test_make_object_diff_new_dict_key():
+    assert make_object_diff({}, {"a": "1"}) == 'Attribute "a" with value "1" added.'
+
+
+def test_make_object_diff_removed_dict_key():
+    assert make_object_diff({"a": "1"}, {}) == 'Attribute "a" with value "1" removed.'
+
+
+def test_make_object_diff_changed_dict_value():
+    assert make_object_diff({"a": "0"}, {"a": "1"}) == 'Value of "a" changed from "0" to "1".'
+
+
+def test_make_object_diff_multiple_changes():
+    assert make_object_diff({
+        "a": "0",
+        "b": "1"
+    }, {"a": "1"}) == 'Attribute "b" with value "1" removed.\nValue of "a" changed from "0" to "1".'
+
+
+def test_make_object_diff_list_remove_item():
+    assert make_object_diff([1, 2], [1]) == 'Item 1 with value 2 removed.'
+
+
+def test_make_object_diff_list_remove_dict():
+    assert make_object_diff([1, {"1": "a"}], [1]) == 'Item 1 with value {\'1\': \'a\'} removed.'

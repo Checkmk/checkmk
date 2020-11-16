3cd70cacd8926d0480274b586fe9c242f132ac15
Lars Michelsen <lm@tribe29.com>
2020-11-16 20:01:19 +0100

    Update integration tests to new response format
    Change-Id: Icf28e59bb694c48ccebb12ee0a7d19161e1d4fc1

--- tests/integration/cmk/base/test_automations.py
+++ tests/integration/cmk/base/test_automations.py
@@ -132,14 +132,11 @@ def test_automation_discovery_no_host(test_cfg, site):
 def test_automation_discovery_single_host(test_cfg, site):
     data = _execute_automation(site, "inventory", args=["@raiseerrors", "new", "modes-test-host"])
 
-    assert isinstance(data, tuple)
-    assert len(data) == 2
+    assert isinstance(data, dict)
+    assert len(data) == 1
 
-    assert len(data[0]) == 1
-    assert "modes-test-host" in data[0]
-    assert len(data[0]["modes-test-host"]) == 6
-
-    assert data[1] == {}
+    assert data["modes-test-host"]["diff_text"] == "Nothing was changed."
+    assert data["modes-test-host"]["error_text"] is None
 
 
 def test_automation_discovery_multiple_hosts(test_cfg, site):
@@ -147,39 +144,33 @@ def test_automation_discovery_multiple_hosts(test_cfg, site):
                                "inventory",
                                args=["@raiseerrors", "new", "modes-test-host", "modes-test-host2"])
 
-    assert isinstance(data, tuple)
-    assert len(data) == 2
+    assert isinstance(data, dict)
+    assert len(data) == 1
 
-    assert len(data[0]) == 2
-    assert "modes-test-host" in data[0]
-    assert "modes-test-host2" in data[0]
-    assert len(data[0]["modes-test-host"]) == 6
-    assert len(data[0]["modes-test-host2"]) == 6
-
-    assert data[1] == {}
+    assert data["modes-test-host"]["diff_text"] == "Nothing was changed."
+    assert data["modes-test-host"]["error_text"] is None
+    assert data["modes-test-host2"]["diff_text"] == "Nothing was changed."
+    assert data["modes-test-host2"]["error_text"] is None
 
 
 def test_automation_discovery_not_existing_host(test_cfg, site):
     data = _execute_automation(site, "inventory", args=["@raiseerrors", "new", "xxxhost"])
 
-    assert isinstance(data, tuple)
-    assert len(data) == 2
+    assert isinstance(data, dict)
+    assert len(data) == 1
 
-    assert data[0]["xxxhost"] == [0, 0, 0, 0, 0, 0]
-    assert data[1] == {"xxxhost": ''}
+    assert data["xxxhost"]["diff_text"] == "Nothing was changed."
+    assert data["xxxhost"]["error_text"] is None
 
 
 def test_automation_discovery_with_cache_option(test_cfg, site):
     data = _execute_automation(site, "inventory", args=["new", "modes-test-host"])
 
-    assert isinstance(data, tuple)
-    assert len(data) == 2
+    assert isinstance(data, dict)
+    assert len(data) == 1
 
-    assert len(data[0]) == 1
-    assert "modes-test-host" in data[0]
-    assert len(data[0]["modes-test-host"]) == 6
-
-    assert data[1] == {}
+    assert data["modes-test-host"]["diff_text"] == "Nothing was changed."
+    assert data["modes-test-host"]["error_text"] is None
 
 
 def test_automation_analyse_service_autocheck(test_cfg, site):

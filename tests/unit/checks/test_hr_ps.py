e17aea6305c155b929c6c1120ceda8bae3d409bc
Lisa Pichler <lisa.pichler@tribe29.com>
2020-11-16 10:55:16 +0100

    hr_ps: added test file
    FEED-5293
    
    Change-Id: I909e3cd35e1f54079b7678691d1d835d609c864a

---
+++ tests/unit/checks/test_hr_ps.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+
+from checktestlib import (
+    assertDiscoveryResultsEqual,
+    DiscoveryResult,
+    MockHostExtraConf,
+)
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize('info, discovery_params, expected_discovery_result', [
+    (
+        [
+            ['systemd', '/usr/lib/systemd/systemd', '2'],
+            ['lvmetad', '/usr/sbin/lvmetad', '2'],
+            ['tuned', '/usr/bin/python2', '2'],
+            ['sshd', '/usr/sbin/sshd', '2'],
+            ['java', '/usr/lib/jvm/jre-11-openjdk/bin/java', '2'],
+            ['sssd', '/usr/sbin/sssd', '2'],
+            ['snmpd', '/usr/sbin/snmpd', '1'],
+        ],
+        {
+            'descr': 'sshd',
+            'match_name_or_path': ('match_name', 'sshd'),
+        },
+        [
+            (
+                'sshd',
+                {
+                    'match_name_or_path': ('match_name', 'sshd'),
+                    'match_status': None,
+                    'match_groups': [],
+                },
+            ),
+        ],
+    ),
+    (
+        [
+            ['systemd', '/usr/lib/systemd/systemd', '2'],
+            ['lvmetad', '/usr/sbin/lvmetad', '2'],
+            ['tuned', '/usr/bin/python2', '2'],
+            ['sshd', '/usr/sbin/sshd', '2'],
+            ['java', '/usr/lib/jvm/jre-11-openjdk/bin/java', '2'],
+            ['sssd', '/usr/sbin/sssd', '2'],
+            ['snmpd', '/usr/sbin/snmpd', '1'],
+        ],
+        {
+            'descr': 'sshd',
+            'match_name_or_path': ('match_path', '/usr/sbin/sshd'),
+        },
+        [],
+    ),
+])
+def test_hr_ps_discovery(check_manager, info, discovery_params, expected_discovery_result):
+    '''Test that the hr_ps check returns the correct discovery results given different
+    discovery parameters.
+    '''
+    check = check_manager.get_check('hr_ps')
+
+    with MockHostExtraConf(check, discovery_params, 'host_extra_conf'):
+        actual_discovery_result = check.run_discovery(check.run_parse(info))
+
+    assertDiscoveryResultsEqual(
+        check,
+        DiscoveryResult(actual_discovery_result),
+        DiscoveryResult(expected_discovery_result),
+    )

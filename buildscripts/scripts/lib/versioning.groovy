c71e7ab3269378610ee3be2fdad00c232add4e4f
Sven Panne <sven.panne@tribe29.com>
2020-11-17 08:28:20 +0100

    Tiny simplification.
    Change-Id: Ia2a0f79c71e7a018bef9c1a74cc15c52cd42592b

--- buildscripts/scripts/lib/versioning.groovy
+++ buildscripts/scripts/lib/versioning.groovy
@@ -124,8 +124,7 @@ def patch_demo(DEMO) {
     if (DEMO == 'yes') {
         sh '''sed -ri 's/^(DEMO_SUFFIX[[:space:]]*:?= *).*/\\1'" .demo/" defines.make'''
         sh 'mv omd/packages/nagios/{9999-demo-version.dif,patches/9999-demo-version.dif}'
-        sh '''sed -i 's/#ifdef DEMOVERSION/#if 1/g' enterprise/core/src/TrialManager.h'''
-        sh '''sed -i 's/#ifdef DEMOVERSION/#if 1/g' enterprise/core/src/test/test_TrialManager.cc'''
+        sh '''sed -i 's/#ifdef DEMOVERSION/#if 1/g' enterprise/core/src/{TrialManager.h,test/test_TrialManager.cc}'''
     }
 }
 

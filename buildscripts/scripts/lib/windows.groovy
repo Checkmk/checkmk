fe9f597808e42fee683c1dd52474623a97d28f11
Alex Zurhake <alex.zurhake@tribe29.com>
2020-11-19 12:43:01 +0100

    Stop windows build jobs after a max of 15 minutes
    Change-Id: I85ee8aa77e6b3ca0264e147fcb60711754d18a54

--- buildscripts/scripts/lib/windows.groovy
+++ buildscripts/scripts/lib/windows.groovy
@@ -9,45 +9,49 @@ def build(Map args) {
     }
 
     stage("Windows ${args.TARGET} build") {
-        try {
-            if (args.TARGET == "module") {
-                bat 'cd agents\\modules\\windows && call build_the_module.cmd python'
-                ARTIFACTS = 'python-3.8.zip'
-            } else if (args.TARGET == "cached") {
-                bat 'cd agents\\modules\\windows && call build_the_module.cmd cached ' + args.CREDS + ' ' + args.CACHE_URL
-                ARTIFACTS = 'python-3.8.zip'
-            } else if (args.TARGET == "agent") {
-                bat 'cd agents\\wnx && call build_release.cmd'
-                ARTIFACTS = "check_mk_agent-64.exe,check_mk_agent.exe,check_mk_agent.msi,check_mk.user.yml"
-            } else if (args.TARGET == "test_integration") {
-                bat 'cd agents\\wnx && call call_integration_tests.cmd'
-            } else {
-                throw new Exception(args.TARGET + " is not known!")
+        // Windows integration test is the longest running test with kust under 3 min
+        // No job should exceed 3*5 = 15 minutes
+        timeout(time: 15, unit: 'MINUTES') {
+            try {
+                if (args.TARGET == "module") {
+                    bat 'cd agents\\modules\\windows && call build_the_module.cmd python'
+                    ARTIFACTS = 'python-3.8.zip'
+                } else if (args.TARGET == "cached") {
+                    bat 'cd agents\\modules\\windows && call build_the_module.cmd cached ' + args.CREDS + ' ' + args.CACHE_URL
+                    ARTIFACTS = 'python-3.8.zip'
+                } else if (args.TARGET == "agent") {
+                    bat 'cd agents\\wnx && call build_release.cmd'
+                    ARTIFACTS = "check_mk_agent-64.exe,check_mk_agent.exe,check_mk_agent.msi,check_mk.user.yml"
+                } else if (args.TARGET == "test_integration") {
+                    bat 'cd agents\\wnx && call call_integration_tests.cmd'
+                } else {
+                    throw new Exception(args.TARGET + " is not known!")
+                }
+    
+                if (ARTIFACTS != '') {
+                   if (args.STASH_NAME == null ) {
+                      archive_artifacts(ARTIFACTS, ARTIFACTS_DIR)
+                   } else {
+                      stash_artifacts(ARTIFACTS, args.STASH_NAME, ARTIFACTS_DIR)
+                   }
+                }
+    
+            } catch(ERROR) {
+                mail(
+                    to: WIN_DEV_MAIL,
+                    cc: '', 
+                    bcc: '', 
+                    from: JENKINS_MAIL, 
+                    replyTo: '', 
+                    subject: "Win Error in $BUILD_URL",
+                    body: """
+                        The following Error appered in 
+                        Build URL: $BUILD_URL \n
+                    """
+                        + ERROR.getMessage()
+                )
+                throw ERROR
             }
-
-            if (ARTIFACTS != '') {
-               if (args.STASH_NAME == null ) {
-                  archive_artifacts(ARTIFACTS, ARTIFACTS_DIR)
-               } else {
-                  stash_artifacts(ARTIFACTS, args.STASH_NAME, ARTIFACTS_DIR)
-               }
-            }
-
-        } catch(ERROR) {
-            mail(
-                to: WIN_DEV_MAIL,
-                cc: '', 
-                bcc: '', 
-                from: JENKINS_MAIL, 
-                replyTo: '', 
-                subject: "Win Error in $BUILD_URL",
-                body: """
-                    The following Error appered in 
-                    Build URL: $BUILD_URL \n
-                """
-                    + ERROR.getMessage()
-            )
-            throw ERROR
         }
     }
 }

bcf32f2bf14be6dd966ca9270a31026631c58055
Lisa Pichler <lisa.pichler@tribe29.com>
2020-11-24 12:04:31 +0100

    Revert "fileinfo.groups: do not return metrics for additional rules"
    CMK-6454
    
    This reverts commit 6ef113f17e4f8598b2aab1898ba94bba5df09094.
    
    Change-Id: Ifc24bd38ceac55aa26d69eef48877853d4714f04

--- tests/unit/checks/generictests/datasets/fileinfo_additional_rules_regression.py
+++ tests/unit/checks/generictests/datasets/fileinfo_additional_rules_regression.py
@@ -77,17 +77,23 @@ checks = {
                 (
                     0, 'Files matching /gw/ro-gr/do/wpgate/gwia/dsnhold/5753717f.*'
                 ),
-                (0, 'Count: 1', []),
-                (0, 'Size: 384 B', []),
-                (2, 'Largest size: 384 B (warn/crit at 1 B/2 B)', []),
+                (0, 'Count: 1', [('count', 1, None, None, None, None)]),
+                (0, 'Size: 384 B', [('size', 384, None, None, None, None)]),
+                (2, 'Largest size: 384 B (warn/crit at 1 B/2 B)', [('size_largest', 384, 1, 2, None, None)]),
                 (
-                    0, 'Smallest size: 384 B', []
+                    0, 'Smallest size: 384 B', [
+                        ('size_smallest', 384, None, None, None, None)
+                    ]
                 ),
                 (
-                    0, 'Oldest age: 3.1 y', []
+                    0, 'Oldest age: 3.1 y', [
+                        ('age_oldest', 98209582, None, None, None, None)
+                    ]
                 ),
                 (
-                    0, 'Newest age: 3.1 y', []
+                    0, 'Newest age: 3.1 y', [
+                        ('age_newest', 98209582, None, None, None, None)
+                    ]
                 ),
                 (   0, '\nInclude patterns: /gw/ro-gr/do/wpgate/gwia/dsnhold*\nFiles matching /gw/ro-gr/do/wpgate/gwia/dsnhold/5753717f.*:\n[/gw/ro-gr/do/wpgate/gwia/dsnhold/5753717f.000] Age: 3.1 y, Size: 384 B(!!)\n(Remaining) files in file group:\n[/gw/ro-gr/do/wpgate/gwia/dsnhold/5753f6e9.000] Age: 3.1 y, Size: 384 B(!)\n[/gw/ro-gr/do/wpgate/gwia/dsnhold/5754104f.000] Age: 3.1 y, Size: 384 B(!)\n[/gw/ro-gr/do/wpgate/gwia/dsnhold/57543199.000] Age: 3.1 y, Size: 384 B(!)'
                 )

02cf2a391ea054bfe97380801d75c69cfedb5694
Lisa Pichler <lisa.pichler@tribe29.com>
2020-11-26 13:42:54 +0100

    agent_aws: added test file
    CMK-5535
    
    Change-Id: I7db1874bbb5672b220a6383ebdeb799b152c23fc

---
+++ tests/unit/checks/test_agent_aws.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+from testlib import SpecialAgent  # type: ignore[import]
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize(
+    'params, expected_args',
+    [
+        (
+            {
+                'access_key_id': 'strawberry',
+                'secret_access_key': ('password', 'strawberry098'),
+                'proxy_details': {
+                    'proxy_host': '1.1.1',
+                    'proxy_port': 22,
+                    'proxy_user': 'banana',
+                    'proxy_password': 'banana123',
+                },
+                'assume_role': {},
+                'global_services': {
+                    'ce': None,
+                },
+                'regions': [],
+                'services': {
+                    'ec2': {
+                        'selection': 'all',
+                        'limits': True,
+                    },
+                    'ebs': {
+                        'selection': 'all',
+                        'limits': True,
+                    },
+                    'cloudfront': None,
+                },
+            },
+            [
+                "--access-key-id",
+                "strawberry",
+                "--secret-access-key",
+                "strawberry098",
+                "--proxy-host",
+                "1.1.1",
+                "--proxy-port",
+                "22",
+                "--proxy-user",
+                "banana",
+                "--proxy-password",
+                "banana123",
+                "--global-services",
+                "ce",
+                "--services",
+                "cloudfront",
+                "ebs",
+                "ec2",
+                "--ec2-limits",
+                "--ebs-limits",
+                "--hostname",
+                "testhost",
+            ],
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_aws_argument_parsing(params, expected_args):
+    """Tests if all required arguments are present."""
+    agent = SpecialAgent('agent_aws')
+    arguments = agent.argument_func(params, "testhost", "address")
+    assert arguments == expected_args

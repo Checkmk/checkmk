a2a13b468bc7e8095550060b7db2d9c75669a256
Lisa Pichler <lisa.pichler@tribe29.com>
2020-11-26 16:38:32 +0100

    agent_azure: added test file
    CMK-6501
    
    Change-Id: I65955b3cdd3a802449fe2a717db871ce31092771

---
+++ tests/unit/checks/test_agent_azure.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+from testlib import SpecialAgent  # type: ignore[import]
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize(
+    'params, expected_args',
+    [
+        (
+            {
+                'subscription': 'banana',
+                'tenant': 'strawberry',
+                'client': 'blueberry',
+                'secret': 'vurystrong',
+                'config': {},
+            },
+            [
+                '--subscription',
+                'banana',
+                '--tenant',
+                'strawberry',
+                '--client',
+                'blueberry',
+                '--secret',
+                'vurystrong',
+            ],
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_azure_argument_parsing(params, expected_args):
+    """Tests if all required arguments are present."""
+    agent = SpecialAgent('agent_azure')
+    arguments = agent.argument_func(params, "testhost", "address")
+    assert arguments == expected_args

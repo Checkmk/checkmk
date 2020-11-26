62ede88d44f25dc44d0dc4cd4f6fd6c2c4116fde
Lisa Pichler <lisa.pichler@tribe29.com>
2020-11-26 11:30:47 +0100

    agent_aws: added test file for aws sections
    CMK-6495
    
    Change-Id: I349137842f6f7d25add730347795d6cef7ef3932

---
+++ tests/unit/cmk/special_agents/agent_aws/test_agent_aws_sections.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+
+from cmk.special_agents.agent_aws import (
+    AWSSectionsGeneric,
+    AWSSectionResult,
+)
+
+
+class TestAWSSections:
+    @pytest.fixture
+    def services(self):
+        return "testservice"
+
+    @pytest.fixture
+    def region(self):
+        return "testregion"
+
+    @pytest.fixture
+    def config(self):
+        return "testconfig"
+
+    @pytest.fixture
+    def generic_section(self, services, region, config):
+        return AWSSectionsGeneric(services, region, config)
+
+    def test_section_header(self, generic_section, capsys):
+        cached_data = {
+            ('costs_and_usage', 1606382471.693873, 38582.763184): [
+                AWSSectionResult(
+                    piggyback_hostname='',
+                    content=[
+                        {
+                            'TimePeriod': {
+                                'Start': '2020-11-25',
+                                'End': '2020-11-26',
+                            },
+                            'Total': {
+                                'UnblendedCost': {
+                                    'Amount': '0',
+                                    'Unit': 'USD',
+                                }
+                            },
+                            'Groups': [],
+                            'Estimated': True,
+                        },
+                    ],
+                )
+            ],
+        }
+        generic_section._write_section_results(cached_data)
+        section_stdout = capsys.readouterr().out
+        assert section_stdout.split(
+            '\n')[0] == '<<<aws_costs_and_usage:cached(1606382471,38642.763184)>>>'

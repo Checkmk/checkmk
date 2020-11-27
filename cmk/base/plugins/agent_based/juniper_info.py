f4bd056dd819f52bcfb3e67bd2bd148d501d9f22
Moritz Kiemer <moritz.kiemer@tribe29.com>
2020-11-27 13:45:43 +0100

    juniper_info: migrate
    Change-Id: I03399ff9f772ef28d435f5c38a06ab79096907a7

--- cmk/base/plugins/agent_based/juniper_info.py
+++ cmk/base/plugins/agent_based/juniper_info.py
@@ -5,6 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from .agent_based_api.v1 import (
+    Attributes,
     register,
     SNMPTree,
     startswith,
@@ -22,4 +23,22 @@ register.snmp_section(
         ),
     ],
     detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.2636.1.1.1.2"),
+)
+
+
+def inventory_juniper_info(section):
+    model, serial = section[0][0]
+
+    yield Attributes(
+        path=["hardware", "system"],
+        inventory_attributes={
+            "serial_number": serial,
+            "model_name": model,
+        },
+    )
+
+
+register.inventory_plugin(
+    name='juniper_info',
+    inventory_function=inventory_juniper_info,
 )

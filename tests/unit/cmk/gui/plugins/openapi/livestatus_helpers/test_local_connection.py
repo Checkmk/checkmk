a1f653c5911899fe71f487e784701f1049a3db3c
Christoph Rauch <christoph.rauch@tribe29.com>
2020-12-02 10:08:06 +0100

    Livestatus: intercept queries
     * collect executed queries on each site individually
       in a thread-local collection
     * trigger intercepting with a context manager
     * test for this feature
    
    Change-Id: Ifbb6ff852556ecfac8a45dba2b9958b2aa1ec7e8

--- tests/unit/cmk/gui/plugins/openapi/livestatus_helpers/test_local_connection.py
+++ tests/unit/cmk/gui/plugins/openapi/livestatus_helpers/test_local_connection.py
@@ -3,9 +3,11 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+from cmk.gui import sites
 from cmk.gui.plugins.views import RowTableLivestatus
 from cmk.gui.plugins.wato.ac_tests import ACTestGenericCheckHelperUsage
 from cmk.gui.views import View
+from livestatus import intercept_queries
 
 
 def test_local_connection_mocked(mock_livestatus):
@@ -52,3 +54,15 @@ def test_row_table_object(mock_livestatus, register_builtin_html):
             limit=None,
             all_active_filters=[],
         )
+
+
+def test_intercept_queries(mock_livestatus, register_builtin_html):
+    with mock_livestatus(expect_status_query=True):
+        live = sites.live()
+
+    mock_livestatus.expect_query("GET hosts\nColumns: name")
+    with mock_livestatus(expect_status_query=False), intercept_queries() as queries:
+        live.query("GET hosts\nColumns: name")
+
+    # livestatus.py appends a lot of extra columns, so we only check for startswith
+    assert queries[0].startswith("GET hosts\nColumns: name\n")

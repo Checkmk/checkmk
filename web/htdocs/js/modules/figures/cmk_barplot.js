c3a5632fcfac25555a5e2001d0c5074215182f71
Óscar Nájera <oscar.najera@tribe29.com>
2020-12-04 13:22:22 +0100

    Redo bar plots as stated from UX project
    - Bars are now horizontal thin stripes
    - Bar labels place on top of bar, no need for out of plot margin
    - Axis are conventionally invisible
    - Vertical background lines to orient user of values in plot
    - Discard tiles and gradients
    - Bars are colored by level. Light blue when no levels are known, then the
      standard ok,warn,crit from the cmk_figures make levels.
    
    CMK-5647
    
    Change-Id: Ic8407acf42c34b51a8e995518206cf08cd080fb4
    (cherry picked from commit 3c02b937c36bd784953e4848fbfaf0b9bf126b5f)

--- web/htdocs/js/modules/figures/cmk_barplot.js
+++ web/htdocs/js/modules/figures/cmk_barplot.js
@@ -14,7 +14,7 @@ class BarplotFigure extends cmk_figures.FigureBase {
 
     constructor(div_selector, fixed_size = null) {
         super(div_selector, fixed_size);
-        this.margin = {top: 20, right: 10, bottom: 150, left: 70};
+        this.margin = {top: 20, right: 10, bottom: 10, left: 10};
 
         this._time_dimension = this._crossfilter.dimension(d => d.timestamp);
         this._tag_dimension = this._crossfilter.dimension(d => d.tag);
@@ -23,16 +23,16 @@ class BarplotFigure extends cmk_figures.FigureBase {
     }
 
     initialize() {
-        this.svg = this._div_selection.append("svg");
+        this.svg = this._div_selection.append("svg").classed("renderer", true);
         this.plot = this.svg.append("g");
 
         // X axis
-        this.scale_x = d3.scaleBand().padding(0.2);
-        this.plot.append("g").classed("x_axis", true).call(d3.axisBottom(this.scale_x));
+        this.scale_x = d3.scaleLinear();
+        this.plot.append("g").classed("x_axis", true).call(d3.axisTop(this.scale_x));
 
         // Y axis
-        this.scale_y = d3.scaleLinear();
-        this.plot.append("g").classed("y_axis", true).call(d3.axisLeft(this.scale_y));
+        this.scale_y = d3.scaleBand().padding(0.2);
+        this.plot.append("g").classed("y_axis", true).call(d3.axisRight(this.scale_y));
     }
 
     render() {
@@ -50,9 +50,6 @@ class BarplotFigure extends cmk_figures.FigureBase {
         this.scale_x.range([0, this.plot_size.width]);
         this.scale_y.range([this.plot_size.height, 0]);
         this.plot.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-        this.plot
-            .select("g.x_axis")
-            .attr("transform", "translate(0," + this.plot_size.height + ")");
     }
 
     _update_plot_definitions(plot_definitions) {
@@ -63,6 +60,17 @@ class BarplotFigure extends cmk_figures.FigureBase {
             if (plot_definition.plot_type != "single_value") return;
             this._plot_definitions.push(plot_definition);
         });
+    }
+
+    render_grid() {
+        // Grid
+        let height = this.plot_size.height;
+        this.plot
+            .selectAll("g.grid.vertical")
+            .data([null])
+            .join("g")
+            .classed("grid vertical", true)
+            .call(d3.axisTop(this.scale_x).ticks(5).tickSize(-height).tickFormat(""));
     }
 
     update_gui() {
@@ -79,30 +87,32 @@ class BarplotFigure extends cmk_figures.FigureBase {
         this._time_dimension.filter(d => d == this._time_dimension.top(1)[0].timestamp);
 
         this.resize();
-        this.scale_x.domain(this._plot_definitions.map(d => d.label));
+        this.scale_y.domain(this._plot_definitions.map(d => d.label));
         this.plot
-            .selectAll("g.x_axis")
-            .call(d3.axisBottom(this.scale_x))
-            .selectAll("text")
-            .attr("transform", "translate(-8, 0) rotate(-45)")
-            .style("text-anchor", "end");
+            .selectAll("g.y_axis")
+            .classed("axis", true)
+            .call(d3.axisRight(this.scale_y))
+            .selectAll("text");
 
         let used_tags = this._plot_definitions.map(d => d.use_tags[0]);
         let points = this._tag_dimension.filter(d => used_tags.includes(d)).top(Infinity);
-        this.scale_y.domain([0, d3.max(points, d => d.value) * 1.2]);
+        const domain = [0, d3.max(points, d => d.value) * 1.2];
+        this.scale_x.domain(domain);
         this._tag_dimension.filterAll();
-        this.plot.selectAll("g.y_axis").call(
-            d3.axisLeft(this.scale_y).tickFormat(d => {
-                if (d > 999) return d3.format(".2s")(d);
-                return d3.format(",")(d);
-            })
-        );
+        this.plot
+            .selectAll("g.x_axis")
+            .classed("axis", true)
+            .call(
+                d3.axisTop(this.scale_x).tickFormat(d => {
+                    if (d > 999) return d3.format(".2s")(d);
+                    return d3.format(",")(d);
+                })
+            );
+
+        this.render_grid();
 
         this._render_bar_containers();
-        this._render_values();
-        this._render_levels();
-        this._render_bar_tilers();
-        this._render_bar_gradients();
+        this._render_values(domain);
         let plot = this._data.plot_definitions.filter(d => d.plot_type == "single_value")[0];
         if (!plot) return;
         cmk_figures.state_component(this, plot.svc_state);
@@ -113,123 +123,35 @@ class BarplotFigure extends cmk_figures.FigureBase {
             .selectAll("g.bar")
             .data(this._plot_definitions, d => d.id)
             .join(enter => enter.append("g").classed("bar", true))
-            .attr("transform", d => {
-                return "translate(" + this.scale_x(d.label) + ",0)";
-            });
+            .attr("transform", d => "translate(0, " + (this.scale_y(d.label) + 24) + ")");
     }
 
-    _render_values() {
+    _render_values(domain) {
         this.plot
             .selectAll("g.bar")
             .selectAll("rect.value")
             .data(d => {
                 let point = this._tag_dimension.filter(tag => tag == d.use_tags[0]).top(1)[0];
-                return [point != undefined ? point : {value: 0}];
+                if (point === undefined) point = {value: 0};
+
+                const levels = cmk_figures.make_levels(domain, d.metrics);
+                point.level_color = levels.length
+                    ? levels.find(element => point.value < element.to).color
+                    : "#3CC2FF";
+                return [point];
             })
             .join(enter =>
                 enter
                     .append("rect")
                     .classed("value", true)
-                    .attr("y", d => this.scale_y(d.value))
-                    .attr("width", this.scale_x.bandwidth())
-                    .attr("height", d => this.plot_size.height - this.scale_y(d.value))
+                    .attr("height", 4)
+                    .attr("width", d => this.scale_x(d.value))
             )
             .transition()
-            .attr("y", d => this.scale_y(d.value))
-            .attr("width", this.scale_x.bandwidth())
-            .attr("height", d => this.plot_size.height - this.scale_y(d.value))
-            .attr("fill", (d, idx) => "url(#color-gradient-bar-" + idx + ")");
+            .attr("width", d => this.scale_x(d.value))
+            .attr("fill", d => d.level_color);
 
         this._tag_dimension.filterAll();
-    }
-
-    _render_levels() {
-        let levels = this.plot
-            .selectAll("g.bar")
-            .selectAll("rect.level")
-            .data(
-                d => {
-                    let levels = [];
-                    let metrics = d.metrics;
-                    // TODO: remove dummy metrics
-                    // metrics = {};
-                    // metrics.crit = 0.9 * d.value;
-                    // metrics.warn = 0.8 * d.value;
-                    if (metrics == undefined) return levels;
-                    [
-                        {what: "warn", color: "#fffe44"},
-                        {what: "crit", color: "#ff3232"},
-                    ].forEach(level => {
-                        if (metrics[level.what] == undefined) return;
-                        levels.push({level: level, value: metrics[level.what]});
-                    });
-                    return levels;
-                },
-                d => d.what
-            );
-
-        levels
-            .join(enter => enter.append("rect").classed("level", true))
-            .attr("y", d => this.scale_y(d["value"]))
-            .attr("opacity", 0.5)
-            .attr("fill", d => d.level.color)
-            .attr("width", this.scale_x.bandwidth())
-            .attr("height", 2);
-    }
-
-    _render_bar_tilers() {
-        let tiles = [];
-        let max_tiles = 20;
-        let tile_height = this.plot_size.height / max_tiles;
-
-        for (let i = 1; i <= max_tiles; i++) tiles.push(i);
-
-        this.plot
-            .selectAll("g.bar")
-            .selectAll("rect.tiler")
-            .data(tiles)
-            .join(enter =>
-                enter
-                    .append("rect")
-                    .classed("tiler", true)
-                    .attr("stroke", "#dddddd1f")
-                    .attr("opacity", 0.5)
-                    .attr("stroke-width", 1.5)
-                    .attr("fill", "none")
-            )
-            .attr("y", d => this.plot_size.height - d * tile_height)
-            .attr("height", () => tile_height - 2)
-            .attr("width", () => this.scale_x.bandwidth());
-    }
-
-    _render_bar_gradients() {
-        // Add the color gradient for each bar
-        this.plot
-            .selectAll("g.bar")
-            .selectAll("linearGradient")
-            .data((d, idx) => [[d, idx]])
-            .join("linearGradient")
-            .attr("id", d => "color-gradient-bar-" + d[1])
-            .attr("gradientUnits", "userSpaceOnUse")
-            .attr("x1", 0)
-            .attr("y1", 0)
-            .attr("x2", this.scale_x.bandwidth())
-            .attr("y2", 0)
-            .selectAll("stop")
-            .data(() => {
-                return [
-                    {offset: "0%", color: "#343434", opacity: 0.0},
-                    {offset: "30%", color: "#13d389", opacity: 0.35},
-                    {offset: "50%", color: "#13d389", opacity: 1},
-                    {offset: "70%", color: "#13d389", opacity: 0.35},
-                    {offset: "100%", color: "#343434", opacity: 0.0},
-                ];
-            })
-            .enter()
-            .append("stop")
-            .attr("offset", d => d.offset)
-            .attr("stop-color", d => d.color)
-            .attr("stop-opacity", d => d.opacity);
     }
 }
 

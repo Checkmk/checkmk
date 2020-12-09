318ca8d802aac94286ced181915441e3ba06c1bc
Lars Michelsen <lm@tribe29.com>
2020-12-09 10:27:11 +0100

    Revert "cpu_utilization_multiitem valuespec: add transform from old tuple based values"
    This reverts commit bf763d8c41bdf14672f6398780675ff00c7ca693.

--- cmk/gui/plugins/wato/check_parameters/cpu_utilization_multiitem.py
+++ cmk/gui/plugins/wato/check_parameters/cpu_utilization_multiitem.py
@@ -6,7 +6,6 @@
 
 from cmk.gui.i18n import _
 from cmk.gui.valuespec import (
-    Transform,
     Dictionary,
     Integer,
     Percentage,
@@ -22,38 +21,34 @@ from cmk.gui.plugins.wato import (
 
 
 def _parameter_valuespec_cpu_utilization_multiitem():
-    return Transform(
-        Dictionary(
-            help=_("The CPU utilization sums up the percentages of CPU time that is used "
-                   "for user processes and kernel routines over all available cores within "
-                   "the last check interval. The possible range is from 0% to 100%"),
-            elements=[
-                ("levels",
-                 Tuple(
-                     title=_("Alert on too high CPU utilization"),
-                     elements=[
-                         Percentage(title=_("Warning at a utilization of"), default_value=90.0),
-                         Percentage(title=_("Critical at a utilization of"), default_value=95.0),
-                     ],
-                 )),
-                ("average",
-                 Integer(
-                     title=_("Averaging"),
-                     help=_("Average the CPU utilization over the specified time period "
-                            "before levels are applied."),
-                     unit=_("minutes"),
-                     minvalue=1,
-                     default_value=15,
-                     label=_("Compute average over last "),
-                 )),
-            ],
-        ),
-        forth=transform_valuespec_cpu_utilization_multiitem,
+    return Dictionary(
+        help=_("The CPU utilization sums up the percentages of CPU time that is used "
+               "for user processes and kernel routines over all available cores within "
+               "the last check interval. The possible range is from 0% to 100%"),
+        elements=[
+            (
+                "levels",
+                Tuple(
+                    title=_("Alert on too high CPU utilization"),
+                    elements=[
+                        Percentage(title=_("Warning at a utilization of"), default_value=90.0),
+                        Percentage(title=_("Critical at a utilization of"), default_value=95.0)
+                    ],
+                ),
+            ),
+            ("average",
+             Integer(
+                 title=_("Averaging"),
+                 help=
+                 _("Average the CPU utilization over the specified time period before levels are applied."
+                  ),
+                 unit=_("minutes"),
+                 minvalue=1,
+                 default_value=15,
+                 label=_("Compute average over last "),
+             )),
+        ],
     )
-
-
-def transform_valuespec_cpu_utilization_multiitem(read_params):
-    return read_params if isinstance(read_params, dict) else {"levels": read_params}
 
 
 rulespec_registry.register(

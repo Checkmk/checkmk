cd76f66b7ee03f9b9059a6e62e5ffc4dd00b126a
Sergey Kipnis <sergey.kipnis@tribe29.com>
2020-12-10 10:17:37 +0100

    [CMK-6120]- remove obsolete globals related to testing/validation
    Also removed old build script, corrected readme, fixed code(no more globals)
    
    Change-Id: Ieb3ef52658184ba18f96cfe5bebb326a42167c36

--- agents/wnx/src/engine/stdafx_defines.h
+++ agents/wnx/src/engine/stdafx_defines.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
-// This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-// conditions defined in the file COPYING, which is part of this source code package.
+// This file is part of Checkmk (https://checkmk.com). It is subject to the
+// terms and conditions defined in the file COPYING, which is part of this
+// source code package.
 
 //
 // THIS is DEFINES for pre-compiled header for Engine Project
@@ -21,7 +22,5 @@
                                                            // smart enough
 
 #define FMT_HEADER_ONLY
-
-#define _SILENCE_CXX17_STRSTREAM_DEPRECATION_WARNING
 
 #define NOMINMAX  // for Windows

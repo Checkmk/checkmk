512ccf9db190d32c1d1ba9e240e3ce8affb10839
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2020-12-14 11:15:54 +0100

    Disable ntop's TopTalkers Dashboard for b2
    * The feature is not ready, so it is excluded for b2
    
    Change-Id: Ifeda17389fd7fa22776806c89e8b4e7e163ffc0f

--- tests/unit/cmk/gui/test_gui_config.py
+++ tests/unit/cmk/gui/test_gui_config.py
@@ -527,7 +527,6 @@ def test_registered_permissions():
         expected_permissions += [
             'dashboard.ntop_alerts',
             'dashboard.ntop_flows',
-            'dashboard.ntop_top_talkers',
             'general.edit_reports',
             'icons_and_actions.agent_deployment',
             'icons_and_actions.status_shadow',

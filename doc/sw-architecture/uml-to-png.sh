b1affa14bc2150f5bf768fccfe6dc34e0c1cd609
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2020-12-16 13:49:41 +0100

    SW-Arch proceedings
    * add proc liveproxyd
    * add whitebox for top level
    * export multiple definitions to globals.iuml
    * apdat docker build: we now build with a single docker cmd
    
    CMK-6567
    
    Change-Id: I4928769475f13bfa2ed40160b29c2607f0f98c48

--- doc/sw-architecture/uml-to-png.sh
+++ doc/sw-architecture/uml-to-png.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-OUTPUT_DIR="./output/"
 
 docker build --tag plantuml .
-
-uml_files=$(ls *.puml)
-
-for uml_file in ${uml_files}
-do
-   :
-   echo "Creating png from $uml_file"
-   cat "$uml_file" | docker run --rm -i plantuml:latest -tpng > $OUTPUT_DIR${uml_file/.puml/.png}
-done
-
+docker run -t -v "$PWD":"$PWD" -w "$PWD" plantuml:latest java -Djava.awt.headless=true -jar /plantuml.jar *.puml -o output/

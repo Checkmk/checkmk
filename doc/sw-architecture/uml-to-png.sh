fe7a8cd7b446de8b5ef4d51c1f4fcbf40fd4462b
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2020-12-16 09:50:54 +0100

    Rename file extenstions
    * This facilitates rendering in IntelliJ
    
    Change-Id: Ia83322d3dcb76edad9b9a88f7c8668c92afe3e2e

--- doc/sw-architecture/uml-to-png.sh
+++ doc/sw-architecture/uml-to-png.sh
@@ -3,12 +3,12 @@ OUTPUT_DIR="./output/"
 
 docker build --tag plantuml .
 
-uml_files=$(ls *.uml)
+uml_files=$(ls *.puml)
 
 for uml_file in ${uml_files}
 do
    :
    echo "Creating png from $uml_file"
-   cat "$uml_file" | docker run --rm -i plantuml:latest -tpng > $OUTPUT_DIR${uml_file/.uml/.png}
+   cat "$uml_file" | docker run --rm -i plantuml:latest -tpng > $OUTPUT_DIR${uml_file/.puml/.png}
 done
 

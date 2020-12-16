dde3ccf4edb52747e768b17790f135c4e775317e
Joerg Herbel <joerg.herbel@tribe29.com>
2020-12-16 11:56:56 +0100

    Setup search: add unit tests for match item generators
    CMK-6465
    
    Change-Id: I5cc6fa5b3c7d092170ebe8a3ed13134d73afa2d8

---
+++ tests/unit/cmk/gui/wato/pages/test_global_settings.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import (
+    Iterable,
+    Tuple,
+)
+from cmk.gui.plugins.watolib.utils import (
+    ConfigVariable,
+    ConfigVariableGroup,
+)
+from cmk.gui.wato.pages.global_settings import (
+    ABCConfigDomain,
+    MatchItemGeneratorSettings,
+    ModeEditGlobals,
+)
+from cmk.gui.watolib.search import MatchItem
+from cmk.gui.valuespec import (
+    TextAscii,
+    ValueSpec,
+)
+
+
+def test_match_item_generator_settings(monkeypatch, module_wide_request_context):
+    class SomeConfigVariable(ConfigVariable):
+        def ident(self) -> str:
+            return "ident"
+
+        def valuespec(self) -> ValueSpec:
+            return TextAscii(title="title")
+
+    class SomeSettingsMode(ModeEditGlobals):
+        def iter_all_configuration_variables(
+                self) -> Iterable[Tuple[ConfigVariableGroup, Iterable[ConfigVariable]]]:
+            return [(
+                ConfigVariableGroup(),
+                [SomeConfigVariable()],
+            )]
+
+    monkeypatch.setattr(ABCConfigDomain, "get_all_default_globals", lambda: {})
+
+    assert list(
+        MatchItemGeneratorSettings(
+            "settings",
+            "Settings",
+            SomeSettingsMode,
+        ).generate_match_items()) == [
+            MatchItem(
+                title='title',
+                topic='Settings',
+                url='wato.py?mode=edit_configvar&varname=ident',
+                match_texts=['title', 'ident'],
+            ),
+        ]

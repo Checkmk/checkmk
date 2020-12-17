cbbbc959fcb8d4994a7e82e5169be219424c7771
Óscar Nájera <oscar.najera@tribe29.com>
2020-12-17 14:08:30 +0100

    Migrate percent and scientific unit rendering to JS
    - Make percent render testable in Python
    
    CMK-5872
    
    Change-Id: Ie8fca8798847c51d8a7158b5b0a000adb36a4c19

--- web/htdocs/js/modules/number_format.js
+++ web/htdocs/js/modules/number_format.js
@@ -39,3 +39,46 @@ export function fmt_nic_speed(speed) {
 
     return fmt_number_with_precision(speedi, 1000, 2, false, "bit/s");
 }
+
+export function percent(perc, scientific_notation = false) {
+    if (perc == 0) return "0%";
+    let result = "";
+    if (scientific_notation && Math.abs(perc) >= 100000) {
+        result = scientific(perc, 1);
+    } else if (Math.abs(perc) >= 100) {
+        result = perc.toFixed(0);
+    } else if (0 < Math.abs(perc) < 0.01) {
+        result = perc.toFixed(7);
+        if (parseFloat(result) == 0) return "0%";
+        if (scientific_notation && Math.abs(perc) < 0.0001) {
+            result = scientific(perc, 1);
+        } else {
+            result = result.replace(/0+$/, "");
+        }
+    } else {
+        result = drop_dotzero(perc, 2);
+    }
+
+    if (Number.isInteger(parseFloat(result)) && perc < 100) result = result + ".0";
+
+    return result + "%";
+}
+
+export function scientific(v, precision) {
+    if (v == 0) return "0";
+    if (v < 0) return "-" + scientific(-1 * v, precision);
+
+    let [mantissa, exponent] = frexpb(v, 10);
+    if (-3 <= exponent <= 4) return v.toFixed(Math.max(0, precision - exponent));
+    return v.toExponetial(precision);
+}
+
+export function frexpb(x, base) {
+    let exp = Math.floor(Math.log(x) / Math.log(base));
+    let mantissa = x / base ** exp;
+    if (mantissa < 1) {
+        mantissa *= base;
+        exp -= 1;
+    }
+    return [mantissa, exp];
+}

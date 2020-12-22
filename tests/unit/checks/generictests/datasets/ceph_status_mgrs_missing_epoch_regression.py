303936e70b0afc1a6f505b279511dde671caba4d
Lisa Pichler <lisa.pichler@tribe29.com>
2020-12-22 18:04:49 +0100

    11740 FIX ceph_status_mgrs: fix crash on KeyError(epoch)
    CMK-6848
    
    Change-Id: I9068cb4cef7d625f6f20d93c747592b45441f346

---
+++ tests/unit/checks/generictests/datasets/ceph_status_mgrs_missing_epoch_regression.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# yapf: disable
+# type: ignore
+checkname = 'ceph_status'
+
+info = [
+    ['{'], ['"health":', '{'], ['"status":', '"HEALTH_OK",'],
+    ['"checks":', '{},'], ['"mutes":', '[]'], ['},'],
+    ['"election_epoch":', '175986,'], ['"mgrmap":', '{'],
+    ['"available":', 'true,'], ['"num_standbys":', '3,'], ['"modules":', '['],
+    ['"dashboard",'], ['"diskprediction_local",'], ['"restful",'],
+    ['"status"'], ['],'], ['"services":', '{'],
+    ['"dashboard":', '"http://gcd-virthost4.ad.gcd.de:8080/"'], ['}'], ['},'],
+    ['"progress_events":', '{}'], ['}']
+]
+
+discovery = {'': [(None, {})], 'osds': [], 'pgs': [], 'mgrs': [(None, {})]}
+
+checks = {
+    '': [
+        (
+            None, {
+                'epoch': (1, 3, 30)
+            }, [
+                (0, 'Health: OK', []),
+                (0, 'Epoch rate (30 m average): 0.00', [])
+            ]
+        )
+    ],
+    'mgrs': [(None, {
+        'epoch': (1, 2, 5)
+    }, [])]
+}

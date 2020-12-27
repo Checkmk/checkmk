cc7b678fad1a348c982820f2ee12e850a32869f1
Lars Michelsen <lm@tribe29.com>
2020-12-27 20:27:29 +0100

    Register Checkmk GPG key to build / test containers
    With this trust we can drop "--no-gpg-checks" from the zypper calls.
    
    Change-Id: I44cc13559f6c5a64b84e448178e43a1dfc3bbf45

---
+++ buildscripts/infrastructure/build-nodes/scripts/install-cmk-dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# Prepares the current system (must be a supported distro) for installing a
+# Checkmk package.
+
+set -e -o pipefail
+
+echo "Add Check_MK-pubkey.gpg to gpg"
+gpg --import /opt/Check_MK-pubkey.gpg
+
+case "$DISTRO" in
+centos-* | sles-*)
+    echo "Add Check_MK-pubkey.gpg to RPM"
+    rpm --import /opt/Check_MK-pubkey.gpg
+    ;;
+esac
+
+# TODO: Install distro specific dependencies of Checkmk to make building of
+# daily test container faster (since dependency installation is not needed
+# anymore)

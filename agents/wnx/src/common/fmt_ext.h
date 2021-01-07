69b820f312fff4da8d7b64db267ba6098d356d13
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-01-07 18:19:58 +0100

    Add custom formatter for microseconds, milliseconds, nanoseconds und seconds. This spares us printing .count() and prevent errors when adding a suffix.
    Change-Id: I7e291732a4425c278bd0433c94ed3bae26368052
    fmt::format("{}", std::chrono::milliseconds{15}) gives now always "23ms".

--- agents/wnx/src/common/fmt_ext.h
+++ agents/wnx/src/common/fmt_ext.h
@@ -1,14 +1,17 @@
 // Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
-// This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-// conditions defined in the file COPYING, which is part of this source code package.
+// This file is part of Checkmk (https://checkmk.com). It is subject to the
+// terms and conditions defined in the file COPYING, which is part of this
+// source code package.
 
 //
 // support ftomatting of user definde data types
 // No C++ file
 #pragma once
 
-#include <filesystem>
 #include <fmt/format.h>
+
+#include <chrono>
+#include <filesystem>
 
 template <>
 struct fmt::formatter<std::filesystem::path> {
@@ -20,5 +23,57 @@ struct fmt::formatter<std::filesystem::path> {
     template <typename FormatContext>
     auto format(const std::filesystem::path& p, FormatContext& ctx) {
         return format_to(ctx.out(), "{}", p.u8string());
+    }
+};
+
+template <>
+struct fmt::formatter<std::chrono::milliseconds> {
+    constexpr auto parse(format_parse_context& ctx) {
+        // Return an iterator past the end of the parsed range:
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const std::chrono::milliseconds& p, FormatContext& ctx) {
+        return format_to(ctx.out(), "{}ms", p.count());
+    }
+};
+
+template <>
+struct fmt::formatter<std::chrono::seconds> {
+    constexpr auto parse(format_parse_context& ctx) {
+        // Return an iterator past the end of the parsed range:
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const std::chrono::seconds& p, FormatContext& ctx) {
+        return format_to(ctx.out(), "{}s", p.count());
+    }
+};
+
+template <>
+struct fmt::formatter<std::chrono::microseconds> {
+    constexpr auto parse(format_parse_context& ctx) {
+        // Return an iterator past the end of the parsed range:
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const std::chrono::microseconds& p, FormatContext& ctx) {
+        return format_to(ctx.out(), "{}us", p.count());
+    }
+};
+
+template <>
+struct fmt::formatter<std::chrono::nanoseconds> {
+    constexpr auto parse(format_parse_context& ctx) {
+        // Return an iterator past the end of the parsed range:
+        return ctx.end();
+    }
+
+    template <typename FormatContext>
+    auto format(const std::chrono::nanoseconds& p, FormatContext& ctx) {
+        return format_to(ctx.out(), "{}ns", p.count());
     }
 };

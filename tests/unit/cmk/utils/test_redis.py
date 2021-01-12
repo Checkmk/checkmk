c1244a32c03a0b105de9f0050f31ed7ac5bef179
Joerg Herbel <joerg.herbel@tribe29.com>
2021-01-12 13:54:03 +0100

    Implement utilities for managing Redis DBs
    For now, the only functionality is a function to get a correctly typed
    client for the default DB (index 0).
    
    CMK-6792
    
    Change-Id: I91f4fab2198b4d0b2af4a5ae9e3c634ab80c0a94

---
+++ tests/unit/cmk/utils/test_redis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.utils.redis import get_redis_client
+
+
+class TestCheckmkRedisClient:
+    def test_initialization_decode_activated(self):
+        assert get_redis_client().connection_pool.connection_kwargs.get(
+            'decode_responses',
+            False,
+        )

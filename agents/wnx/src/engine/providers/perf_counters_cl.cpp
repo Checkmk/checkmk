3750700d96b2326de09033b5da9ff12501a71d42
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-01-12 17:56:48 +0100

    [CMK-6907] - fix perf_counters_cl.cpp
    Change-Id: I9f94b9600ce19918534609e2783c9dc05b753e46

--- agents/wnx/src/engine/providers/perf_counters_cl.cpp
+++ agents/wnx/src/engine/providers/perf_counters_cl.cpp
@@ -1,45 +1,52 @@
-
-// provides basic api to start and stop service
-// Command Line entry point
+// Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+// This file is part of Checkmk (https://checkmk.com). It is subject to the
+// terms and conditions defined in the file COPYING, which is part of this
+// source code package.
 
 #include "stdafx.h"
 
 #include "providers/perf_counters_cl.h"
 
 #include <string>
+#include <string_view>
 
 #include "cfg.h"
 #include "common/cmdline_info.h"
 #include "common/wtools.h"
 #include "logger.h"
 #include "providers/p_perf_counters.h"
-#include "tools/_raii.h"
 
-namespace cma {
+namespace cma::provider {
 
-namespace provider {
+namespace {
+void RemoveTrailingCR(std::string& accu) {
+    if (!accu.empty() && accu.back() == '\n') {
+        accu.pop_back();
+    }
+}
+}  // namespace
 
 std::string AccumulateCounters(
-    const std::wstring& prefix_name,
+    std::wstring_view prefix_name,
     const std::vector<std::wstring_view>& counter_array) {
-    using namespace cma::tools;
-
     std::string accu;
     for (const auto& cur_counter : counter_array) {
-        auto [key, name] = ParseKeyValue(cur_counter, exe::cmdline::kSplitter);
+        auto [key, name] =
+            tools::ParseKeyValue(cur_counter, exe::cmdline::kSplitter);
+
+        // ip is not a real counter
         if (key == L"ip") {
             XLOG::d.i("From ip {}", wtools::ConvertToUTF8(name));
             continue;
         }
 
-        std::replace(key.begin(), key.end(), L'*', L' ');
+        std::ranges::replace(key, L'*', L' ');
 
         if (!name.empty() && !key.empty())
-            accu += cma::provider::BuildWinPerfSection(prefix_name, name, key);
-
-        // sends results to carrier
+            accu += provider::BuildWinPerfSection(prefix_name, name, key);
     }
-    if (!accu.empty() && accu.back() == '\n') accu.pop_back();
+
+    RemoveTrailingCR(accu);
 
     return accu;
 }
@@ -48,19 +55,20 @@ std::string AccumulateCounters(
 // accumulates all data in counters
 // sends accumulated data to internal port
 // return 0 on success
-int RunPerf(const std::wstring& PeerName,  // name assigned by starting program
-            const std::wstring& Port,      // format as in carrier.h mail:*
-            const std::wstring& Id,        // answer id, should be set
-            int Timeout,                   // how long wait for execution
-            std::vector<std::wstring_view> CounterArray  // name of counters
+int RunPerf(
+    const std::wstring& peer_name,  // name assigned by starting program
+    const std::wstring& port,       // format as in carrier.h mail:*
+    const std::wstring& answer_id,  // answer id, should be set
+    int /*timeout*/,                // how long wait for execution
+    const std::vector<std::wstring_view> counter_array  // name of counters
 ) {
-    auto accu = AccumulateCounters(PeerName, CounterArray);
+    auto accu = AccumulateCounters(peer_name, counter_array);
 
-    auto result = cma::carrier::CoreCarrier::FireSend(
-        PeerName, Id, Port, accu.c_str(), accu.size());
-    XLOG::d.i("Send to {} {}", wtools::ConvertToUTF8(Port), accu.size());
+    auto result = carrier::CoreCarrier::FireSend(peer_name, answer_id, port,
+                                                 accu.c_str(), accu.size());
+    XLOG::d.i("Send to '{}' [{}]", wtools::ConvertToUTF8(port), accu.size());
+
     return result ? 0 : -1;
 }
 
-}  // namespace provider
-};  // namespace cma
+};  // namespace cma::provider

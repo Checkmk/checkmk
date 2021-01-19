d98d7b88dda5f55ca2b8844b4543573b8b558b55
Joerg Herbel <joerg.herbel@tribe29.com>
2021-01-19 16:05:51 +0100

    11834 cmk_site_statistics: new check plugin to monitor site statistics
    This werk introduces the new check plugin <tt>cmk_site_statistics</tt>.
    This plugin monitors the number of hosts and services of Checkmk sites.
    It reports the total number of hosts, the number of hosts which are not
    UP as well as metrics recording the number of hosts which are UP, DOWN,
    in downtime and unreachable. Similarly, for services, the plugin reports
    the total number of services, the number of services which are not OK
    and metrics recording the number of services which are OK, in downtime,
    on down hosts, WARNING, UNKNOWN and CRITICAL.
    
    This check plugin creates one service per Checkmk site. These services
    will be discovered on hosts with one ore more sites which have the
    Checkmk Linux agent installed. The monitoring state of these services is
    always OK and they are not configurable.
    
    CMK-6975
    
    Change-Id: I2707adcbb7c05442765179fc313dab433b13f9ae

---
+++ cmk/base/plugins/agent_based/cmk_site_statistics.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from dataclasses import (
+    dataclass,
+    fields,
+)
+from typing import (
+    Dict,
+    Mapping,
+    Tuple,
+    Union,
+)
+from .agent_based_api.v1 import (
+    Metric,
+    Result,
+    Service,
+    State,
+    register,
+)
+from .agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+    StringTable,
+)
+
+
+@dataclass
+class HostStatistics:
+    up: int
+    down: int
+    unreachable: int
+    in_downtime: int
+
+
+@dataclass
+class ServiceStatistics:
+    ok: int
+    in_downtime: int
+    on_down_hosts: int
+    warning: int
+    unknown: int
+    critical: int
+
+
+Section = Mapping[str, Tuple[HostStatistics, ServiceStatistics]]
+
+
+def parse_cmk_site_statistics(string_table: StringTable) -> Section:
+    """
+    >>> from pprint import pprint
+    >>> pprint(parse_cmk_site_statistics([
+    ... ['[heute]'],
+    ... ['1', '0', '0', '0'],
+    ... ['32', '0', '0', '2', '0', '1']]))
+    {'heute': (HostStatistics(up=1, down=0, unreachable=0, in_downtime=0),
+               ServiceStatistics(ok=32, in_downtime=0, on_down_hosts=0, warning=2, unknown=0, critical=1))}
+    """
+    section: Dict[str, Tuple[HostStatistics, ServiceStatistics]] = {}
+    iter_lines = iter(string_table)
+
+    while True:
+        try:
+            line_site_name = next(iter_lines)
+        except StopIteration:
+            return section
+
+        site_name = line_site_name[0][1:-1]
+        host_stats = HostStatistics(*(int(n) for n in next(iter_lines)))
+        service_stats = ServiceStatistics(*(int(n) for n in next(iter_lines)))
+        section[site_name] = host_stats, service_stats
+
+
+register.agent_section(
+    name="cmk_site_statistics",
+    parse_function=parse_cmk_site_statistics,
+)
+
+
+def discover_cmk_site_statistics(section: Section) -> DiscoveryResult:
+    yield from (Service(item=site_name) for site_name in section)
+
+
+def _host_results(host_stats: HostStatistics) -> CheckResult:
+    n_hosts_not_up = host_stats.down + host_stats.unreachable + host_stats.in_downtime
+    n_hosts_total = n_hosts_not_up + host_stats.up
+    yield Result(
+        state=State.OK,
+        summary=f"Total hosts: {n_hosts_total}",
+    )
+    yield Result(
+        state=State.OK,
+        summary=f"Problem hosts: {n_hosts_not_up}",
+        details="\n".join((
+            f"UP hosts: {host_stats.up}",
+            f"DOWN hosts: {host_stats.down}",
+            f"Unreachable hosts: {host_stats.unreachable}",
+            f"Hosts in downtime: {host_stats.in_downtime}",
+        )),
+    )
+
+
+def _service_results(service_stats: ServiceStatistics) -> CheckResult:
+    n_services_not_ok = (service_stats.in_downtime + service_stats.on_down_hosts +
+                         service_stats.warning + service_stats.unknown + service_stats.critical)
+    n_services_total = n_services_not_ok + service_stats.ok
+    yield Result(
+        state=State.OK,
+        summary=f"Total services: {n_services_total}",
+    )
+    yield Result(
+        state=State.OK,
+        summary=f"Problem services: {n_services_not_ok}",
+        details="\n".join((
+            f"Services in downtime: {service_stats.in_downtime}",
+            f"Services of down hosts: {service_stats.on_down_hosts}",
+            f"WARNING services: {service_stats.warning}",
+            f"UNKNOWN services: {service_stats.unknown}",
+            f"CRITICAL services: {service_stats.critical}",
+        )),
+    )
+
+
+def _metrics_from_stats(
+    stats: Union[HostStatistics, ServiceStatistics],
+    metrics_prefix: str,
+) -> CheckResult:
+    for field in fields(stats):
+        yield Metric(
+            f"{metrics_prefix}_{field.name}",
+            getattr(stats, field.name),
+        )
+
+
+def check_cmk_site_statistics(item: str, section: Section) -> CheckResult:
+    if item not in section:
+        return
+    host_stats, service_stats = section[item]
+    yield from _host_results(host_stats)
+    yield from _service_results(service_stats)
+    yield from _metrics_from_stats(host_stats, "cmk_hosts")
+    yield from _metrics_from_stats(service_stats, "cmk_services")
+
+
+register.check_plugin(
+    name="cmk_site_statistics",
+    service_name="Site %s statistics",
+    discovery_function=discover_cmk_site_statistics,
+    check_function=check_cmk_site_statistics,
+)

0ab6a37af8213608837d28461065781854f675ad
Óscar Nájera <oscar.najera@tribe29.com>
2021-01-20 17:09:42 +0100

    Refactor Dashlet graph to raise error only on dashlet render
    Information about graph identitification and title, which needs to be used
    in dashlet require initialization. If it fails unfortunately access to
    dashlet editor is not longer possible.
    
    Initialization is performed in two pass. Once to work with the instance and
    second to enforce exception when it is rendered.
    
    Change-Id: I5c59d1468bf57777418ef794818c0dc57db0d80a

--- tests/unit/cmk/gui/test_dashboard.py
+++ tests/unit/cmk/gui/test_dashboard.py
@@ -104,7 +104,7 @@ def test_dashlet_refresh_intervals(register_builtin_html, type_name, expected_re
     if dashlet_type.has_context():
         dashlet_spec["context"] = {}
     if type_name in ["pnpgraph", "custom_graph"]:
-        monkeypatch.setattr(dashlet_type, "graph_identification", lambda s: ("template", {}))
+        monkeypatch.setattr(dashlet_type, "graph_identification", lambda s, c: ("template", {}))
         monkeypatch.setattr("cmk.gui.plugins.metrics.html_render.resolve_graph_recipe",
                             lambda g, d: [{
                                 "title": '1'

7ab15b9bfc6dff812f2d9a2f4a17706b820d97d0
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-01-20 16:16:04 +0100

    omd_info: deobfuscate & fix typing
    Change-Id: I6aa77e56525ba203c9862d7fef99adf89fbda4c5

--- cmk/base/plugins/agent_based/omd_info.py
+++ cmk/base/plugins/agent_based/omd_info.py
@@ -4,51 +4,56 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Dict
+from typing import Dict, List
 from .agent_based_api.v1 import register
 from .agent_based_api.v1.type_defs import StringTable
 
-Section = Dict[str, Dict[str, str]]
+Section = Dict[str, Dict[str, Dict[str, str]]]
 
 
 def parse_omd_info(string_table: StringTable) -> Section:
     """
-    >>> for k, v in parse_omd_info([
-    ...         ['[versions]'],
-    ...         ['version', 'number', 'edition', 'demo'],
-    ...         ['v1.full', 'v1', 'full', '0'],
-    ...         ['v2.full', 'v2', 'full', '0'],
-    ...         ['[sites]'],
-    ...         ['site', 'used_version', 'autostart'],
-    ...         ['heute', 'v1', '0'],
-    ...         ['stable', 'v2', '0'],
-    ... ]).items():
-    ...     print(k)
-    ...     for k2, v2 in v.items():
-    ...         print("  ", k2, v2)
-    versions
-       v1.full {'version': 'v1.full', 'number': 'v1', 'edition': 'full', 'demo': '0'}
-       v2.full {'version': 'v2.full', 'number': 'v2', 'edition': 'full', 'demo': '0'}
-    sites
-       heute {'site': 'heute', 'used_version': 'v1', 'autostart': '0'}
-       stable {'site': 'stable', 'used_version': 'v2', 'autostart': '0'}
+    >>> from pprint import pprint
+    >>> pprint(parse_omd_info([
+    ...     ['[versions]'],
+    ...     ['version', 'number', 'edition', 'demo'],
+    ...     ['v1.full', 'v1', 'full', '0'],
+    ...     ['v2.full', 'v2', 'full', '0'],
+    ...     ['[sites]'],
+    ...     ['site', 'used_version', 'autostart'],
+    ...     ['heute', 'v1', '0'],
+    ...     ['beta', 'v2', '0'],
+    ... ]))
+    {'sites': {'beta': {'autostart': '0', 'site': 'beta', 'used_version': 'v2'},
+               'heute': {'autostart': '0', 'site': 'heute', 'used_version': 'v1'}},
+     'versions': {'v1.full': {'demo': '0',
+                              'edition': 'full',
+                              'number': 'v1',
+                              'version': 'v1.full'},
+                  'v2.full': {'demo': '0',
+                              'edition': 'full',
+                              'number': 'v2',
+                              'version': 'v2.full'}}}
     """
-    result = {}
-    current_section: Dict[str, str] = {}
-    headers = None
-    for line in string_table:
-        if not line:
-            continue
+    section: Section = {}
+    current_subsection: Dict[str, Dict[str, str]] = {}
+    headers: List[str] = []
+    for line in (l for l in string_table if l):
         if line[0][0] == "[" and line[0][-1] == "]":
-            current_section = {}
-            result[line[0].strip("[]")] = current_section
-            headers = None
-        elif current_section is not None:
-            if headers is None:
-                headers = list(map(str, line))
-            else:
-                current_section[line[0]] = dict(zip(headers, line))
-    return result
+            current_subsection = section.setdefault(line[0].strip("[]"), {})
+            headers = []  # 'reset'
+            continue
+
+        if not headers:
+            headers = line
+            continue
+
+        current_subsection[line[0]] = dict(zip(headers, line))
+
+    return section
 
 
-register.agent_section(name="omd_info", parse_function=parse_omd_info)
+register.agent_section(
+    name="omd_info",
+    parse_function=parse_omd_info,
+)

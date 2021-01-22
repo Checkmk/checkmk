3d371b3f0d6b8109f91a112bf87e5c7879ed93e7
Andreas Boesl <andreas.boesl@tribe29.com>
2021-01-22 10:04:51 +0100

    updated cmk_site_overview.js
    Change-Id: Idf1a5795ab1f2ca0ae9bcf1c3baabfcb74807384

--- web/htdocs/js/modules/figures/cmk_site_overview.js
+++ web/htdocs/js/modules/figures/cmk_site_overview.js
@@ -2,12 +2,6 @@ import * as d3 from "d3";
 import * as utils from "utils";
 import * as cmk_figures from "cmk_figures";
 
-// Used for rapid protoyping, bypassing webpack
-// var cmk_figures = cmk.figures; /* eslint-disable-line no-undef */
-// var dc = dc; /* eslint-disable-line no-undef */
-// var d3 = d3; /* eslint-disable-line no-undef */
-// var crossfilter = crossfilter; /* eslint-disable-line no-undef */
-
 class SiteOverview extends cmk_figures.FigureBase {
     static ident() {
         return "site_overview";
@@ -21,7 +15,6 @@ class SiteOverview extends cmk_figures.FigureBase {
     initialize(debug) {
         cmk_figures.FigureBase.prototype.initialize.call(this, debug);
         this.svg = this._div_selection.append("svg");
-        this.plot = this.svg.append("g");
 
         this._tooltip = this._div_selection.append("div").classed("tooltip", true);
         this.tooltip_generator = new cmk_figures.FigureTooltip(this._tooltip);
@@ -41,7 +34,6 @@ class SiteOverview extends cmk_figures.FigureBase {
         //   ],
         //  "plot_definitions" : [
         //    {
-        //       "display_optionA": blabla
         //    }
         //   ]
         // }
@@ -72,6 +64,173 @@ class SiteOverview extends cmk_figures.FigureBase {
     render_hosts() {}
 
     render_sites() {
+        let element_boxes = this.svg
+            .selectAll("g.element_box")
+            .data(this._crossfilter.all())
+            .join(enter => enter.append("g").classed("element_box", true));
+
+        let geometry = this._compute_geometry();
+        this._render_hexagons(element_boxes, geometry);
+
+        // Place element_boxes
+        element_boxes.transition().attr("transform", (d, idx) => {
+            let x = (idx % geometry.num_columns) * geometry.box_width;
+            let y = Math.trunc(idx / geometry.num_columns) * geometry.box_height;
+            return (
+                "translate(" +
+                (x + geometry.box_area_left) +
+                "," +
+                (y + geometry.box_area_top) +
+                ")"
+            );
+        });
+    }
+
+    _render_hexagons(element_boxes, geometry) {
+        let tooltip_generator = this.tooltip_generator;
+
+        let handle_click = function (element) {
+            if (element.type != "icon_element") {
+                location.href = utils.makeuri(element.url_add_vars);
+            }
+        };
+
+        let hexagon_boxes = element_boxes
+            .selectAll("g")
+            .data(d => [d])
+            .join("g")
+            .attr(
+                "transform",
+                "translate(" +
+                    geometry.hexagon_center_left +
+                    "," +
+                    geometry.hexagon_center_top +
+                    ")"
+            )
+            .style("cursor", "pointer")
+            .on("click", handle_click);
+
+        let largest_element_count = 0;
+        for (const element of this._crossfilter.all()) {
+            if (element.type != "icon_element" && element.total.count > largest_element_count)
+                largest_element_count = element.total.count;
+        }
+
+        // Now render all hexagons
+        hexagon_boxes.each(function (element) {
+            let hexagon_box = d3.select(this);
+
+            if (element.type == "icon_element") {
+                // Special handling for IconElement (displaying down / disabled sites)
+                hexagon_box
+                    .selectAll("path.hexagon_0")
+                    .data([element])
+                    .join(enter => enter.append("path").classed("hexagon_0", true))
+                    .attr("d", d3.hexbin().hexagon(geometry.hexagon_radius * 0.5))
+                    .attr("title", element.title)
+                    .classed(element.css_class, true)
+                    .attr("fill", "#ffffff30");
+
+                // TODO: Enable once we have our icons
+                //hexagon_box
+                //    .selectAll("path.hexagon_icon")
+                //    .data([element])
+                //    .join(enter => enter.append("image").classed("hexagon_icon", true))
+                //    .attr(
+                //        "xlink:href",
+                //        "themes/modern-dark/images/icon_" + element.css_class + ".svg"
+                //    )
+                //    .attr("width", 24)
+                //    .attr("height", 24)
+                //    .attr("x", -12)
+                //    .attr("y", -12);
+            } else {
+                // The scale is controlled by the total count of an element compared to the largest
+                // element
+                let scale = Math.max(
+                    0.5,
+                    Math.pow(element.total.count / largest_element_count, 0.3)
+                );
+
+                // Now render the parts of an element (cubical sizing)
+                let sum = element.total.count;
+                for (let i = 0; i < element.parts.length; i++) {
+                    let part = element.parts[element.parts.length - 1 - i];
+
+                    let radius =
+                        (Math.pow(sum, 0.33) / Math.pow(element.total.count, 0.33)) *
+                        geometry.hexagon_radius;
+                    sum -= part.count;
+
+                    let hexagon = hexagon_box
+                        .selectAll("path.hexagon_" + i)
+                        .data([element])
+                        .join(enter => enter.append("path").classed("hexagon_" + i, true))
+                        .attr("d", d3.hexbin().hexagon(radius * scale))
+                        .attr("title", part.title)
+                        .attr("fill", part.color);
+
+                    if (i == 0) hexagon.attr("stroke", "#13d389");
+                }
+            }
+            tooltip_generator.add_support(hexagon_box.node());
+        });
+
+        // Text centered below the hexagon
+        hexagon_boxes
+            .selectAll("text")
+            .data(element => (geometry.show_label ? [element.title] : []))
+            .join("text")
+            .attr("text-anchor", "middle")
+            .text(element => element)
+            .attr("y", geometry.hexagon_radius + 15);
+    }
+
+    _render_inner_hexagons(selection, outer_radius) {
+        // Nur als Beispiel, kann wieder raus
+
+        let data = this._crossfilter.all();
+        // Daten vorbereiten
+        data.forEach(entry => {
+            // Hier werden jeweils 4 innen Hexagons angelegt
+            let inner_config = [];
+            inner_config.push({
+                name: "crit",
+                radius: outer_radius * 0.7,
+                color: "red",
+            });
+            inner_config.push({
+                name: "warn",
+                radius: outer_radius * 0.4,
+                color: "yellow",
+            });
+            inner_config.push({
+                name: "downtime",
+                radius: outer_radius * 0.2,
+                color: "blue",
+            });
+            inner_config.push({
+                name: "ok",
+                radius: outer_radius * 0.1,
+                color: "green",
+            });
+            entry.inner_config = inner_config;
+        });
+
+        // Daten rendern
+        selection
+            .selectAll("path.inner")
+            .data(
+                d => d.inner_config,
+                d => d.name
+            )
+            .join("path")
+            .classed("inner", true)
+            .attr("d", d => d3.hexbin().hexagon(d.radius))
+            .attr("fill", d => d.color);
+    }
+
+    _compute_geometry() {
         let width = this.plot_size.width;
         let height = this.plot_size.height;
 
@@ -140,26 +299,22 @@ class SiteOverview extends cmk_figures.FigureBase {
             let hexagon_center_top =
                 hexagon_radius * (1 + box_v_rel_padding) + (box_height - necessary_box_height) / 2;
 
-            let label_baseline_top =
-                hexagon_center_top + hexagon_radius + label_height + label_v_padding;
-
             // Reduce number of columns, trying to balance the rows
             num_columns = Math.ceil(num_elements / num_rows);
             box_width = box_area_width / num_columns;
 
             let hexagon_center_left = box_width / 2;
-            let label_center_left = box_width / 2;
 
             return {
                 num_columns: num_columns,
                 hexagon_center_top: hexagon_center_top,
                 hexagon_center_left: hexagon_center_left,
                 hexagon_radius: hexagon_radius,
+                box_area_left: box_area_left,
+                box_area_top: box_area_top,
                 box_width: box_width,
                 box_height: box_height,
                 show_label: show_label,
-                label_baseline_top: label_baseline_top,
-                label_center_left: label_center_left,
             };
         };
 
@@ -167,122 +322,7 @@ class SiteOverview extends cmk_figures.FigureBase {
         while (geometry === null) {
             geometry = compute_geometry(num_columns++);
         }
-
-        let largest_element_count = 0;
-        for (const element of this._data.data) {
-            if (element.type != "icon_element" && element.total.count > largest_element_count)
-                largest_element_count = element.total.count;
-        }
-
-        let element_boxes = this.svg
-            .selectAll("g.element_box")
-            .data(this._data.data)
-            .join(enter => enter.append("g").classed("element_box", true));
-
-        let hexagon_center_pos =
-            "translate(" + geometry.hexagon_center_left + "," + geometry.hexagon_center_top + ")";
-
-        let handle_click = function (element) {
-            if (element.type != "icon_element") {
-                location.href = utils.makeuri(element.url_add_vars);
-            }
-        };
-
-        let hexagon_boxes = element_boxes
-            .selectAll("g")
-            .data(d => [d])
-            .join("g")
-            .attr("transform", hexagon_center_pos)
-            .style("cursor", "pointer")
-            .on("click", handle_click);
-
-        let tooltip_generator = this.tooltip_generator;
-
-        // Now render all hexagons
-        hexagon_boxes.each(function (element) {
-            let hexagon_box = d3.select(this);
-
-            if (element.type == "icon_element") {
-                // Special handling for IconElement (displaying down / disabled sites)
-                hexagon_box
-                    .selectAll("path.hexagon_0")
-                    .data([element])
-                    .join(enter => enter.append("path").classed("hexagon_0", true))
-                    .attr("d", d3.hexbin().hexagon(geometry.hexagon_radius * 0.5))
-                    .attr("title", element.title)
-                    .classed(element.css_class, true)
-                    .attr("fill", "#ffffff30");
-
-                // TODO: Enable once we have our icons
-                //hexagon_box
-                //    .selectAll("path.hexagon_icon")
-                //    .data([element])
-                //    .join(enter => enter.append("image").classed("hexagon_icon", true))
-                //    .attr(
-                //        "xlink:href",
-                //        "themes/modern-dark/images/icon_" + element.css_class + ".svg"
-                //    )
-                //    .attr("width", 24)
-                //    .attr("height", 24)
-                //    .attr("x", -12)
-                //    .attr("y", -12);
-            } else {
-                // The scale is controlled by the total count of an element compared to the largest
-                // element
-                let scale = Math.max(
-                    0.5,
-                    Math.pow(element.total.count / largest_element_count, 0.3)
-                );
-
-                // Now render the parts of an element (cubical sizing)
-                let sum = element.total.count;
-                for (let i = 0; i < element.parts.length; i++) {
-                    let part = element.parts[element.parts.length - 1 - i];
-
-                    let radius =
-                        (Math.pow(sum, 0.33) / Math.pow(element.total.count, 0.33)) *
-                        geometry.hexagon_radius;
-                    sum -= part.count;
-
-                    let hexagon = hexagon_box
-                        .selectAll("path.hexagon_" + i)
-                        .data([element])
-                        .join(enter => enter.append("path").classed("hexagon_" + i, true))
-                        .attr("d", d3.hexbin().hexagon(radius * scale))
-                        .attr("title", part.title)
-                        .attr("fill", part.color);
-
-                    if (i == 0) hexagon.attr("stroke", "#13d389");
-                }
-            }
-
-            if (geometry.show_label) {
-                element_boxes
-                    .selectAll("text")
-                    .data(element => [element.title])
-                    .join("text")
-                    .text(element => element)
-                    .attr("x", function () {
-                        return geometry.label_center_left - this.getBBox().width / 2;
-                    })
-                    .attr("y", function () {
-                        return geometry.label_baseline_top;
-                    })
-                    .style("cursor", "pointer")
-                    .on("click", handle_click);
-            } else {
-                element_boxes.selectAll("text").remove();
-            }
-
-            tooltip_generator.add_support(hexagon_box.node());
-        });
-
-        // Place element_boxes
-        element_boxes.transition().attr("transform", (d, idx) => {
-            let x = (idx % geometry.num_columns) * geometry.box_width;
-            let y = Math.trunc(idx / geometry.num_columns) * geometry.box_height;
-            return "translate(" + (x + box_area_left) + "," + (y + box_area_top) + ")";
-        });
+        return geometry;
     }
 }
 

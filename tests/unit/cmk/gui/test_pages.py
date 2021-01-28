ee99179561b0a19be28256e7f19ce9c0c3210778
Óscar Nájera <oscar.najera@tribe29.com>
2021-01-28 20:57:34 +0100

    Moved bar charts to work without its data generator
    - Simplified title. It belongs to dashlet not data generator
    - The legend had repeated to title. Beforehand agreed to drop that.
    - For the moment workaround make all classmethods instance methods to track
      log_class & type. Will be simplified next
    
    Change-Id: I2d47833f0684809b3d7b692c532c862403d04e84

--- tests/unit/cmk/gui/test_pages.py
+++ tests/unit/cmk/gui/test_pages.py
@@ -15,8 +15,6 @@ import cmk.gui.pages
 def test_registered_pages():
     expected_pages = [
         'add_bookmark',
-        'ajax_alerts_bar_chart_dashlet_data',
-        'ajax_notifications_bar_chart_dashlet_data',
         'ajax_figure_dashlet_data',
         'ajax_bi_rule_preview',
         'ajax_bi_aggregation_preview',

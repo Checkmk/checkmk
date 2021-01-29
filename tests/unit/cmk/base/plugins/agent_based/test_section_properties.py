dc8986e6a8769e0240336f33b4f0ed2d582c90d2
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-01-29 12:17:14 +0100

    inv_if,if32: share a detection OID
    The section "inv_if" used to be detected, if a device had at least two (!)
    network interfaces. It will now be detected, if an ifTable is present.
    
    Note that this section is currently only used in a HaSI plugin, so this
    change has no practical effect (apart from fetching one OID less during discovery).
    
    Change-Id: I086b94f0e625a9e0c3ed7c6c3fd75037d5584096

--- tests/unit/cmk/base/plugins/agent_based/test_section_properties.py
+++ tests/unit/cmk/base/plugins/agent_based/test_section_properties.py
@@ -58,9 +58,7 @@ def test_section_detection_uses_sysdescr_or_sysobjid():
     }
 
     known_exceptions = {
-        '.1.3.6.1.2.1.105.1.3.1.1.*': {'pse_poe'},
-        '.1.3.6.1.2.1.2.1.0': {'inv_if'},
-        '.1.3.6.1.2.1.2.2.1.*': {'if'},
+        '.1.3.6.1.2.1.2.2.1.*': {'if', 'inv_if'},
         '.1.3.6.1.2.1.25.1.1.0': {'hr_cpu', 'hr_fs', 'hr_ps'},
         '.1.3.6.1.2.1.31.1.1.1.6.*': {'if64', 'if64adm'},
         '.1.3.6.1.2.1.43.*': {
@@ -71,6 +69,7 @@ def test_section_detection_uses_sysdescr_or_sysobjid():
             'printer_supply',
         },
         '.1.3.6.1.2.1.47.1.1.1.1.*': {'snmp_extended_info'},
+        '.1.3.6.1.2.1.105.1.3.1.1.*': {'pse_poe'},
         '.1.3.6.1.4.1.14848.2.1.1.1.0': {'etherbox'},
         '.1.3.6.1.4.1.2036.2.1.1.4.0': {'snmp_quantum_storage_info'},
         '.1.3.6.1.4.1.232.2.2.4.2.0': {

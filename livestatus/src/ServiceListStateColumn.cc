00aeba007476f7238957ed5f1bdf29f482bf593b
Sven Panne <sven.panne@tribe29.com>
2021-02-02 10:57:12 +0100

    Route access to acknowledgement state through Java-style accessors.
    Although not aesthetically pleasing, this is an intermediate step to get rid
    of the redundant State::_acknowledged flag.  It was probably only introduced
    due to technical limitations of the old Livestatus column machinery.
    
    Change-Id: I56ce5fb0f231a61f68ac8797cb4a771ab8afa2a3

--- livestatus/src/ServiceListStateColumn.cc
+++ livestatus/src/ServiceListStateColumn.cc
@@ -48,7 +48,7 @@ int32_t ServiceListStateColumn::getValueFromServices(MonitoringCore *mc,
                        static_cast<ServiceState>(state->_current_state),
                        static_cast<ServiceState>(state->_last_hard_state),
                        state->_has_been_checked,
-                       state->_acknowledged ||
+                       state->getAcknowledged() ||
                            state->_scheduled_downtime_depth > 0,
                        result);
             }

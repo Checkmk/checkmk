d35c3772896c436dd25bd47104c3cbf7ca37fda0
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2021-02-04 21:03:24 +0100

    Dont minify JS in development mode
    Change-Id: I4dd76ec7b250ca7f1c75e8c02a72a71cf05b225c

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
     mode: "production",
     devtool: "source-map",
     optimization: {
-        minimize: true,
         minimizer: [
             new TerserPlugin({
                 terserOptions: {

81936e25798ce0fb9d0030f40cd2313888507698
Benedikt Seidl <benedikt.seidl@tribe29.com>
2021-02-08 07:37:21 +0100

    checks/cpu: add test for predictive values
    CMK-7072
    
    Change-Id: I1f8e84d2c4034053e23b0634fce395a5a7e38dd2

---
+++ tests/unit/checks/test_cpu.py
@@ -0,0 +1,20 @@
+import pytest  # type: ignore[import]
+from testlib import Check  # type: ignore[import]
+from cmk.base.plugins.agent_based.utils.cpu import Section
+
+pytestmark = pytest.mark.checks
+
+
+def test_cpu_loads_predictive(mocker):
+    # make sure cpu_load check can handle predictive values
+    # absolute values are tested via generic tests
+    mocker.patch("cmk.base.check_api._prediction.get_levels", return_value=(None, (2.2, 4.2)))
+    check_cpu = Check("cpu.loads")
+    params = {'period': 'minute', 'horizon': 1, 'levels_upper': ('absolute', (2.0, 4.0))}
+    section = Section(load=(0.5, 1.0, 1.5), num_cpus=4, num_threads=123)
+    result = check_cpu.run_check(None, params, section)
+
+    assert result == (0, '15 min load: 1.50 (no reference for prediction yet) '
+                      'at 4 cores (0.38 per core)', [('load1', 0.5, None, None, 0, 4),
+                                                     ('load5', 1.0, None, None, 0, 4),
+                                                     ('load15', 1.5, None, None, 0, 4)])

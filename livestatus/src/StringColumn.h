a5f6668ed379f588389f00c252b3b6cb6c4fe7d3
Mathias Laurin <mathias.laurin@tribe29.com>
2021-02-10 13:27:22 +0100

    Fix StringLambdaColumn::Constant and Reference handling
    CMK-7076
    
    Change-Id: Ia1bdbeec49cbf142a58ed73e0c281d4916c17912

--- livestatus/src/StringColumn.h
+++ livestatus/src/StringColumn.h
@@ -24,19 +24,11 @@ class Aggregator;
 class Row;
 class RowRenderer;
 
-// TODO(sp): Is there a way to have a default value in the template parameters?
-// Currently it is hardwired to the empty string.
-template <class T>
-class StringColumn : public Column {
-public:
-    struct Constant;
-    struct Reference;
-    struct PerfData;
-    StringColumn(std::string name, std::string description,
-                 ColumnOffsets offsets, std::function<std::string(const T&)> gv)
-        : Column(std::move(name), std::move(description), std::move(offsets))
-        , get_value_(std::move(gv)) {}
-
+namespace detail {
+struct StringColumn : ::Column {
+    class Constant;
+    class Reference;
+    using ::Column::Column;
     ~StringColumn() override = default;
 
     [[nodiscard]] ColumnType type() const override {
@@ -62,7 +54,26 @@ public:
                                  "' not supported");
     }
 
-    [[nodiscard]] std::string getValue(Row row) const {
+    [[nodiscard]] virtual std::string getValue(Row row) const = 0;
+};
+}  // namespace detail
+
+// TODO(sp): Is there a way to have a default value in the template parameters?
+// Currently it is hardwired to the empty string.
+template <class T>
+class StringColumn : public ::detail::StringColumn {
+public:
+    using ::detail::StringColumn::Constant;
+    using ::detail::StringColumn::Reference;
+    struct PerfData;
+
+    StringColumn(std::string name, std::string description,
+                 ColumnOffsets offsets, std::function<std::string(const T&)> gv)
+        : detail::StringColumn(std::move(name), std::move(description),
+                               std::move(offsets))
+        , get_value_(std::move(gv)) {}
+
+    [[nodiscard]] std::string getValue(Row row) const override {
         using namespace std::string_literals;
         const T* data = columnData<T>(row);
         return data == nullptr ? ""s : get_value_(*data);
@@ -72,18 +83,27 @@ private:
     std::function<std::string(const T&)> get_value_;
 };
 
-template <class T>
-struct StringColumn<T>::Constant : StringColumn {
-    Constant(std::string name, std::string description, const std::string& x)
-        : StringColumn(std::move(name), std::move(description), {},
-                       [x](const T& /*t*/) { return x; }){};
+class detail::StringColumn::Constant : public detail::StringColumn {
+public:
+    Constant(std::string name, std::string description, std::string x)
+        : detail::StringColumn(std::move(name), std::move(description), {})
+        , x{std::move(x)} {}
+
+    [[nodiscard]] std::string getValue(Row /*row*/) const override { return x; }
+
+private:
+    const std::string x;
 };
 
-template <class T>
-struct StringColumn<T>::Reference : StringColumn {
+class detail::StringColumn::Reference : public detail::StringColumn {
+public:
     Reference(std::string name, std::string description, const std::string& x)
-        : StringColumn(std::move(name), std::move(description), {},
-                       [&x](const T& /*t*/) { return x; }){};
+        : detail::StringColumn(std::move(name), std::move(description), {})
+        , x{x} {}
+    [[nodiscard]] std::string getValue(Row /*row*/) const override { return x; }
+
+private:
+    const std::string& x;
 };
 
 template <class T>

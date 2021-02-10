f8c40b0bea658e71892ecc7e3af18991c64dc8e5
Joerg Herbel <joerg.herbel@tribe29.com>
2021-02-10 10:13:32 +0100

    Fix a file header (was missed while merging a PR)
    Change-Id: I19dae5938dee7ac1b95f2a383c940728e7a0ce13

--- tests/unit/cmk/base/plugins/agent_based/test_inventory_fortigate_ha.py
+++ tests/unit/cmk/base/plugins/agent_based/test_inventory_fortigate_ha.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-# author: Oguzhan Cicek, OpenSource Security GmbH - oguzhan(at)os-s.de
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.base.plugins.agent_based.inventory_fortigate_ha import (
     parse_fortigate_ha,

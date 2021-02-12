ce781904514e61fd461afdf113d4ec5d42b3c698
Mathias Laurin <mathias.laurin@tribe29.com>
2021-02-12 10:05:14 +0100

    Re "Lambda-ify BlobLambdaColumn::File & BlobColumn"
    Revert "Revert "Lambda-ify FileColumn -> BlobLambdaColumn::File""
    This reverts commit b97c183dcad10b24bccddca4e83c02f07385e948.
    
    Revert "Revert "Merge BlobLambdaColumn into BlobColumn""
    This reverts commit 93c106fee50187dd97ff3f8895d6473501156502.
    
    Fix: missing ColumnOffsets for the BlobColumn::File.
    
    CMK-7076
    
    Change-Id: I30f4ded4dd82ebc2a81f5f0c8b00f30caa491821

--- livestatus/src/BlobColumn.h
+++ livestatus/src/BlobColumn.h
@@ -9,14 +9,19 @@
 #include "config.h"  // IWYU pragma: keep
 
 #include <chrono>
+#include <filesystem>
 #include <functional>
+#include <iterator>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <utility>
 #include <vector>
 
 #include "Column.h"
 #include "Filter.h"
+#include "Logger.h"
+#include "POSIXUtils.h"
 #include "Row.h"
 #include "contact_fwd.h"
 #include "opids.h"
@@ -47,22 +52,18 @@ public:
 };
 }  // namespace detail
 
-struct BlobColumn : ::detail::BlobColumn {
-    // This is for the legacy code and will go away once
-    // the callers are ported to BlobLambdaColumn.
-    using ::detail::BlobColumn::BlobColumn;
-};
-
 template <class T>
-class BlobLambdaColumn : public ::detail::BlobColumn {
+class BlobColumn : public ::detail::BlobColumn {
 public:
     using ::detail::BlobColumn::Constant;
     using ::detail::BlobColumn::Reference;
-    BlobLambdaColumn(const std::string &name, const std::string &description,
-                     const ColumnOffsets &offsets,
-                     std::function<std::vector<char>(const T &)> f)
-        : BlobColumn(name, description, offsets), get_value_{std::move(f)} {}
-    ~BlobLambdaColumn() override = default;
+    struct File;
+    BlobColumn(std::string name, std::string description, ColumnOffsets offsets,
+               std::function<std::vector<char>(const T &)> f)
+        : detail::BlobColumn{std::move(name), std::move(description),
+                             std::move(offsets)}
+        , get_value_{std::move(f)} {}
+    ~BlobColumn() override = default;
     [[nodiscard]] std::unique_ptr<std::vector<char>> getValue(
         Row row) const override {
         const T *data = columnData<T>(row);
@@ -79,6 +80,7 @@ public:
     Constant(std::string name, std::string description, std::vector<char> v)
         : detail::BlobColumn(std::move(name), std::move(description), {})
         , v{std::move(v)} {};
+    ~Constant() override = default;
     [[nodiscard]] std::unique_ptr<std::vector<char>> getValue(
         Row /*row*/) const override {
         return std::make_unique<std::vector<char>>(v);
@@ -93,6 +95,7 @@ public:
     Reference(std::string name, std::string description,
               const std::vector<char> &v)
         : BlobColumn(std::move(name), std::move(description), {}), v{v} {};
+    ~Reference() override = default;
     [[nodiscard]] std::unique_ptr<std::vector<char>> getValue(
         Row /*row*/) const override {
         return std::make_unique<std::vector<char>>(v);
@@ -101,5 +104,76 @@ public:
 private:
     const std::vector<char> &v;
 };
+
+template <class T>
+struct BlobColumn<T>::File : BlobColumn {
+    File(std::string name, std::string description, ColumnOffsets,
+         std::function<std::filesystem::path()> basepath,
+         std::function<std::filesystem::path(const T &)> filepath);
+    ~File() override = default;
+};
+
+namespace detail {
+template <class T>
+class FileImpl {
+public:
+    FileImpl(std::function<std::filesystem::path()> basepath,
+             std::function<std::filesystem::path(const T &)> filepath)
+        : _basepath{std::move(basepath)}
+        , _filepath{std::move(filepath)}
+        , _logger{
+              Logger::getLogger("cmk.livestatus"),
+              // Logger-lambda from Column::Column.
+              [](std::ostream &os) { os << "[" << getThreadName() << "] "; }} {}
+    std::vector<char> operator()(const T & /*data*/) const;
+    Logger *logger() const { return &_logger; }
+
+private:
+    const std::function<std::filesystem::path()> _basepath;
+    const std::function<std::filesystem::path(const T &)> _filepath;
+    mutable ContextLogger _logger;
+};
+}  // namespace detail
+
+template <class T>
+std::vector<char> detail::FileImpl<T>::operator()(const T &data) const {
+    auto path = _basepath();
+    if (!std::filesystem::exists(path)) {
+        // The basepath is not configured.
+        return {};
+    }
+    auto filepath = _filepath(data);
+    if (!filepath.empty()) {
+        path /= filepath;
+    }
+    if (!std::filesystem::is_regular_file(path)) {
+        Debug(logger()) << path << " is not a regular file";
+        return {};
+    }
+    auto file_size = std::filesystem::file_size(path);
+    std::ifstream ifs;
+    ifs.open(path, std::ifstream::in | std::ifstream::binary);
+    if (!ifs.is_open()) {
+        generic_error ge("cannot open " + path.string());
+        Warning(logger()) << ge;
+        return {};
+    }
+    using iterator = std::istreambuf_iterator<char>;
+    auto buffer = std::vector<char>(file_size);
+    buffer.assign(iterator{ifs}, iterator{});
+    if (buffer.size() != file_size) {
+        Warning(logger()) << "premature EOF reading " << path;
+        return {};
+    }
+    return buffer;
+}
+
+template <class T>
+BlobColumn<T>::File::File(
+    std::string name, std::string description, ColumnOffsets offsets,
+    std::function<std::filesystem::path()> basepath,
+    std::function<std::filesystem::path(const T &)> filepath)
+    : BlobColumn{std::move(name), std::move(description), std::move(offsets),
+                 detail::FileImpl{std::move(basepath), std::move(filepath)}} {}
 
 #endif  // BlobColumn_h

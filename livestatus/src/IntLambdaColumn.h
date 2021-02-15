b449c1ccaa7fd7ded196b47df8f0f2983262f159
Mathias Laurin <mathias.laurin@tribe29.com>
2021-02-15 09:34:36 +0100

    Let Int(\l)Column take a lambda with `const contact*`.
    The second lambda with an extra argument is required at a
    couple of places but we do not want to add the extra (unused)
    argument everywhere an IntColumn is instantiated.
    
    The current patch has the advantage of being transparent
    to the callers.
    
    A better solution would be to not require the `auth_user` but
    there is currently no easy way to do this.
    
    CMK-7076
    
    Change-Id: Ie4ccc1f56eca31adbf3f94448956c431f2cc27ac

--- livestatus/src/IntLambdaColumn.h
+++ livestatus/src/IntLambdaColumn.h
@@ -9,6 +9,7 @@
 #include <functional>
 #include <string>
 #include <utility>
+#include <variant>
 
 #include "IntColumn.h"
 #include "contact_fwd.h"
@@ -26,23 +27,32 @@ struct IntColumn : ::IntColumn {
 
 template <class T, int32_t Default = 0>
 class IntLambdaColumn : public detail::IntColumn {
+    using f0_t = std::function<int(const T&)>;
+    using f1_t = std::function<int(const T&, const contact*)>;
+    using function_type = std::variant<f0_t, f1_t>;
+
 public:
     using ::detail::IntColumn::Constant;
     using ::detail::IntColumn::Reference;
     IntLambdaColumn(const std::string& name, const std::string& description,
-                    const ColumnOffsets& offsets,
-                    const std::function<int(const T&)>& f)
+                    const ColumnOffsets& offsets, const function_type& f)
         : detail::IntColumn{name, description, offsets}, f_{f} {}
     ~IntLambdaColumn() override = default;
 
-    std::int32_t getValue(Row row,
-                          const contact* /*auth_user*/) const override {
+    std::int32_t getValue(Row row, const contact* auth_user) const override {
         const T* data = columnData<T>(row);
-        return data == nullptr ? Default : f_(*data);
+        if (std::holds_alternative<f0_t>(f_)) {
+            return data == nullptr ? Default : std::get<f0_t>(f_)(*data);
+        } else if (std::holds_alternative<f1_t>(f_)) {
+            return data == nullptr ? Default
+                                   : std::get<f1_t>(f_)(*data, auth_user);
+        } else {
+            throw std::runtime_error("unreachable");
+        }
     }
 
 private:
-    const std::function<int(const T&)> f_;
+    const function_type f_;
 };
 
 class detail::IntColumn::Constant : public detail::IntColumn {

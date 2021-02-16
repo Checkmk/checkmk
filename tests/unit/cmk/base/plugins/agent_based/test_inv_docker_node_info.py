d947c69686fa263305345b682daa926d0c5f4218
Benedikt Seidl <benedikt.seidl@tribe29.com>
2021-02-16 09:25:22 +0100

    12118 FIX Fix docker inventory: docker_node_info could not handle docker swarm
    The automatic api translation could not handle the data if docker swarm was
    activated. The error message was:
    
    WARNING: docker_node_info: failed: Unable to convert legacy results.
    Please migrate plugin to new API
    
    Change-Id: I6ff8c57867c5085966a950c523a04a82a810f338

--- tests/unit/cmk/base/plugins/agent_based/test_inv_docker_node_info.py
+++ tests/unit/cmk/base/plugins/agent_based/test_inv_docker_node_info.py
@@ -8,7 +8,7 @@ import pytest  # type: ignore[import]
 
 from cmk.utils.type_defs import InventoryPluginName
 import cmk.base.api.agent_based.register as agent_based_register
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Attributes
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Attributes, TableRow
 
 
 @pytest.mark.usefixtures("config_load_all_inventory_plugins")
@@ -51,6 +51,54 @@ from cmk.base.plugins.agent_based.agent_based_api.v1 import Attributes
                     },
                 ),
             ]),
+        ({
+            "ServerVersion": "20.10.3",
+            "IndexServerAddress": "https://index.docker.io/v1/",
+            "Swarm": {
+                "LocalNodeState": "active",
+                "NodeID": "x2my5tv8bqg0yh5jq98gzodr2",
+                "RemoteManagers": [{
+                    "NodeID": "x2my5tv8bqg0yh5jq98gzodr2",
+                    "Addr": "101.102.103.104:2377"
+                }],
+            },
+            "Containers": 7,
+            "ContainersRunning": 4,
+            "ContainersPaused": 0,
+            "ContainersStopped": 3,
+            "Images": 7,
+            "Labels": ["this_is_a_label_in=etc_docker_daemon_json", "another=label"],
+        }, [
+            Attributes(path=['software', 'applications', 'docker'],
+                       inventory_attributes={
+                           'version': '20.10.3',
+                           'registry': 'https://index.docker.io/v1/',
+                           'swarm_state': 'active',
+                           'swarm_node_id': 'x2my5tv8bqg0yh5jq98gzodr2',
+                       },
+                       status_attributes={
+                           'num_containers_total': 7,
+                           'num_containers_running': 4,
+                           'num_containers_paused': 0,
+                           'num_containers_stopped': 3,
+                           'num_images': 7
+                       }),
+            TableRow(path=['software', 'applications', 'docker', 'node_labels'],
+                     key_columns={'label': 'this_is_a_label_in=etc_docker_daemon_json'},
+                     inventory_columns={},
+                     status_columns={}),
+            TableRow(path=['software', 'applications', 'docker', 'node_labels'],
+                     key_columns={'label': 'another=label'},
+                     inventory_columns={},
+                     status_columns={}),
+            TableRow(path=['software', 'applications', 'docker', 'swarm_manager'],
+                     key_columns={
+                         'NodeID': 'x2my5tv8bqg0yh5jq98gzodr2',
+                         'Addr': '101.102.103.104:2377',
+                     },
+                     inventory_columns={},
+                     status_columns={}),
+        ]),
     ])
 def test_inv_docker_node_info(parsed, expected):
     plugin = agent_based_register.get_inventory_plugin(InventoryPluginName('docker_node_info'))

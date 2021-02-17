8a7a85435fadb7c75a2fd74a684a839d09e3bc7e
Ulrich Bschorer <ulrich.bschorer@tribe29.com>
2021-02-17 17:52:36 +0100

    Validate regexes in BI rules during config update
    Change-Id: I4394b6693d8fd117dd1384fd97d1a2ab9584d367

--- tests/unit/cmk/utils/bi/test_bi_legacy_config_converter.py
+++ tests/unit/cmk/utils/bi/test_bi_legacy_config_converter.py
@@ -6,6 +6,7 @@
 
 import cmk.utils.bi.bi_legacy_config_converter
 import bi_test_data.sample_config as sample_config
+import logging
 
 
 def test_bi_legacy_config_conversion(monkeypatch):
@@ -13,5 +14,5 @@ def test_bi_legacy_config_conversion(monkeypatch):
                         lambda x: sample_config.LEGACY_BI_PACKS_CONFIG_STRING)
 
     schema_from_legacy_packs = cmk.utils.bi.bi_legacy_config_converter.BILegacyConfigConverter(
-    ).get_schema_for_packs()
+        logging.Logger("unit test")).get_schema_for_packs()
     assert sample_config.bi_packs_config["packs"] == schema_from_legacy_packs

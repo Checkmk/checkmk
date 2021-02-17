37c70cdaf7265289b191e77d2aa221e3a58824bf
Lars Michelsen <lm@tribe29.com>
2021-02-17 14:18:19 +0100

    12134 FIX Fix dynamic configuration executing only phase 1 in some cases
    When a dcd connection was configured to operate in the central site of
    a distributed setup, the connection could fail with only executing
    the phase 1 of the synchronization. This was caused by a file
    <tt>etc/check_mk/dcd.d/wato/distributed.mk</tt> in the central site
    telling the DCD that it was a remote site.
    
    Change-Id: I9f293f8016f63ad101bff6462231c7a867a9d994

--- cmk/gui/watolib/activate_changes.py
+++ cmk/gui/watolib/activate_changes.py
@@ -1884,10 +1884,10 @@ def _create_distributed_wato_file_for_dcd(base_dir: Path, is_remote: bool) -> No
     if cmk_version.is_raw_edition():
         return
 
-    with base_dir.joinpath("etc/check_mk/dcd.d/wato/distributed.mk").open(mode="w",
-                                                                          encoding="utf-8") as f:
-        f.write(ensure_str(wato_fileheader()))
-        f.write("dcd_is_wato_remote_site = %r\n" % is_remote)
+    output = wato_fileheader()
+    output += "dcd_is_wato_remote_site = %r\n" % is_remote
+
+    store.save_file(base_dir.joinpath("etc/check_mk/dcd.d/wato/distributed.mk"), output)
 
 
 def create_site_globals_file(site_id: SiteId, tmp_dir: str,

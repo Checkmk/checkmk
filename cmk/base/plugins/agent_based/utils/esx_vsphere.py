808e934a4ada8d8e6b9eb2e63a2c9000df41259f
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2021-02-17 09:08:28 +0100

    Make pylint happy: esx_vsphere
    * Do not import from other plugins
    
    Change-Id: Idbf708f2b0ae95c33d569b7effb3820f6771d5c8

--- cmk/base/plugins/agent_based/utils/esx_vsphere.py
+++ cmk/base/plugins/agent_based/utils/esx_vsphere.py
@@ -5,4 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from typing import Dict, List
+from collections import OrderedDict
+
+Section = OrderedDict
 SectionVM = Dict[str, List[str]]

42fa82d3a2b65c65bb9efc8333f69a598b8ae8c8
Ronny Bruska <ronny.bruska@tribe29.com>
2021-02-18 07:49:59 +0100

    12072 FIX Fix show mode always displaying show more
    CMK-7207
    
    Change-Id: I4c9b2b8a0b58fc36f8de2e3d639aa3d36ab55706

--- cmk/gui/valuespec.py
+++ cmk/gui/valuespec.py
@@ -4820,7 +4820,7 @@ class Dictionary(ValueSpec):
                     isopen=self._form_isopen,
                     narrow=self._form_narrow,
                     show_more_toggle=self._section_has_show_more(section_elements),
-                    show_more_mode=bool(config.user.show_mode),
+                    show_more_mode=config.user.show_mode != "default_show_less",
                     help_text=self.help(),
                 )
             self.render_input_form_header(varprefix, value, header, section_elements, css=css)

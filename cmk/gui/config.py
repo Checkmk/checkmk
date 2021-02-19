d9d33cd357b4b664f68c9cd04f35110fa9c4dac7
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2021-02-19 10:45:35 +0100

    Fix is_ntop_available & is_ntop_configured
    * We need to test if ntop integration is activated for all username cases
    * Add tests accordingly
    
    Change-Id: Ia3a071470e2dd3f921cef124fd94d63fb283b912

--- cmk/gui/config.py
+++ cmk/gui/config.py
@@ -317,24 +317,32 @@ def get_ntop_connection() -> Optional[Dict]:
         return None
 
 
+def get_ntop_connection_mandatory() -> Dict:
+    connection = get_ntop_connection()
+    return connection if connection else {}
+
+
 def is_ntop_available() -> bool:
     # Use this function if you want to know if the ntop intergration is available in general
-    return bool(get_ntop_connection())
+    return isinstance(get_ntop_connection(), dict)
 
 
 def is_ntop_configured() -> bool:
     # Use this function if you want to know if the connection to ntop is fully set-up
     # e.g. to decide if ntop links should be hidden
-    ntop = get_ntop_connection()
 
-    if is_ntop_available() and isinstance(ntop, dict):
-        is_ntop_activated = ntop.get("is_activated", False)
-        custom_attribute_name = ntop.get("use_custom_attribute_as_ntop_username", "")
+    if is_ntop_available():
+        ntop = get_ntop_connection_mandatory()
+        if not ntop.get("is_activated", False):
+            return False
+        custom_attribute_name = ntop.get("use_custom_attribute_as_ntop_username", False)
+
         # We currently have two options to get an ntop username
         # 1) User needs to define his own -> if this string is empty, declare ntop as not configured
         # 2) Take the checkmk username as ntop username -> always declare ntop as configured
-        return bool(is_ntop_activated and isinstance(custom_attribute_name, str) and
-                    user.get_attribute(custom_attribute_name, "")) or not custom_attribute_name
+        return (bool(user.get_attribute(custom_attribute_name, '')) if isinstance(
+            custom_attribute_name, str) else not custom_attribute_name)
+
     return False
 
 

2e2837a71d55df12505d5b6679818cfc82255ced
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-02-24 16:57:54 +0100

    [CMK-7277] - make test more clear and simple
    Change-Id: I71682e2be6c4d203def5cd474ce6ef62056829e2

--- tests/unit/cmk/utils/test_msi_patch.py
+++ tests/unit/cmk/utils/test_msi_patch.py
@@ -149,29 +149,26 @@ def test_patch_package_code_with_state(conf_dir):
     assert _id == ""
 
 
-def test_patch_version(conf_dir):
-    # prepare file to tests
+def test_patch_version(tmpdir):
+    """Tests version patching using the test file from test directory"""
+
+    # base data
     fname = u"test_msi_patch_version.tst"
-    matrix = b"( VersionNT >= 600 )"
-    fixed = b"( VersionNT >= 602 )"
+    result = b"( VersionNT >= 602 )"
+
+    # copy
     src = _get_test_file(fname=fname)
-
-    # prepare test file
-    dst = conf_dir / fname
+    dst = Path(tmpdir) / fname
     shutil.copy(src, dst)
-    base_content = dst.read_bytes()
-    assert base_content.find(matrix) != -1
 
+    # testing
     assert not msi_patch.patch_windows_version(dst / "xx", new_version="602")  # no file
     assert not msi_patch.patch_windows_version(dst, new_version="6")  # bad version
     assert not msi_patch.patch_windows_version(dst, new_version="6020")  # bad version
     assert msi_patch.patch_windows_version(dst, new_version="602")  # valid call -> success
     assert not msi_patch.patch_windows_version(dst, new_version="602")  # no matrix -> fail
-
     assert dst.stat().st_size == src.stat().st_size
-
-    new_content = dst.read_bytes()
-    assert new_content.find(fixed) != -1
+    assert dst.read_bytes().find(result) != -1
 
 
 def check_content(new_content: bytes, base_content: bytes, pos: int, uuid: str,

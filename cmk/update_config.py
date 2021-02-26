Checkmk/checkmk

commit 25eb803c3c8218a3462988175549da2e4a85bead

12145 FIX Config update: Site specific global settings are now also updated
During update from previous versions to Checkmk 2.0 some old global
configuration settings are rewritten. This is now also done in the site
specific global settings automatically during "omd update".

Change-Id: I2ea88b4b229a7b51b792328bbf8806349f50a708

cmk/update_config.py
@@ -54,6 +54,7 @@ from cmk.gui.plugins.dashboard.utils import builtin_dashboards, get_all_dashboar
 from cmk.gui.plugins.userdb.utils import save_connection_config, load_connection_config, USER_SCHEME_SERIAL  # pylint: disable=cmk-module-layer-violation
 from cmk.gui.plugins.watolib.utils import filter_unknown_settings  # pylint: disable=cmk-module-layer-violation
 from cmk.gui.watolib.changes import AuditLogStore, ObjectRef, ObjectRefType  # pylint: disable=cmk-module-layer-violation
+from cmk.gui.watolib.sites import site_globals_editable, SiteManagementFactory  # pylint: disable=cmk-module-layer-violation
 import cmk.gui.watolib.tags  # pylint: disable=cmk-module-layer-violation
 import cmk.gui.watolib.hosts_and_folders  # pylint: disable=cmk-module-layer-violation
 import cmk.gui.watolib.rulesets  # pylint: disable=cmk-module-layer-violation
@@ -194,6 +195,20 @@ class UpdateConfig:
         # Load full config (with undefined settings)
         global_config = cmk.gui.watolib.global_settings.load_configuration_settings(
             full_config=True)
+        self._update_global_config(global_config)
+        cmk.gui.watolib.global_settings.save_global_settings(global_config)
+
+        self._update_site_specific_global_settings()
+
+    def _update_site_specific_global_settings(self):
+        site_mgmt = SiteManagementFactory().factory()
+        configured_sites = site_mgmt.load_sites()
+        for site_id, site_spec in configured_sites.items():
+            if site_globals_editable(site_id, site_spec):
+                self._update_global_config(site_spec.setdefault("globals", {}))
+        site_mgmt.save_sites(configured_sites)
+
+    def _update_global_config(self, global_config):
         # Replace old settings with new ones
         for old_config_name, new_config_name, replacement in REMOVED_GLOBALS_MAP:
             if old_config_name in global_config:
@@ -209,9 +224,7 @@ class UpdateConfig:
 
         # Delete unused settings
         global_config = filter_unknown_settings(global_config)
-
-        # Write updated settings
-        cmk.gui.watolib.global_settings.save_global_settings(global_config)
+        return global_config
 
     def _rewrite_autochecks(self):
         check_variables = cmk.base.config.get_check_variables()

.werks/12145
@@ -1,0 +1,13 @@
+Title: Config update: Site specific global settings are now also updated
+Class: fix
+Compatible: compat
+Component: wato
+Date: 1614246996
+Edition: cre
+Knowledge: doc
+Level: 1
+Version: 2.0.0b8
+
+During update from previous versions to Checkmk 2.0 some old global
+configuration settings are rewritten. This is now also done in the site
+specific global settings automatically during "omd update".

cmk/gui/wato/pages/sites.py
@@ -81,25 +81,12 @@ from cmk.gui.page_menu import (
     make_simple_form_page_menu,
 )
 
-from cmk.gui.watolib.sites import is_livestatus_encrypted
+from cmk.gui.watolib.sites import is_livestatus_encrypted, site_globals_editable
 from cmk.gui.watolib.activate_changes import (clear_site_replication_status,
                                               get_trial_expired_message)
 from cmk.gui.wato.pages.global_settings import ABCGlobalSettingsMode, ABCEditGlobalSettingMode
 
 from cmk.gui.utils.urls import makeuri_contextless, make_confirm_link
-
-
-def _site_globals_editable(site_id, site):
-    # Site is a remote site of another site. Allow to edit probably pushed site
-    # specific globals when remote WATO is enabled
-    if watolib.is_wato_slave_site():
-        return True
-
-    # Local site: Don't enable site specific locals when no remote sites configured
-    if not config.has_wato_slave_sites():
-        return False
-
-    return site["replication"] or config.site_is_local(site_id)
 
 
 @mode_registry.register
@@ -716,7 +703,7 @@ class ModeDistributedMonitoring(WatoMode):
                 html.render_tt(site_id))
             html.icon_button(delete_url, _("Delete"), "delete")
 
-        if _site_globals_editable(site_id, site):
+        if site_globals_editable(site_id, site):
             globals_url = watolib.folder_preserving_link([("mode", "edit_site_globals"),
                                                           ("site", site_id)])
 
@@ -1405,7 +1392,7 @@ def _page_menu_dropdown_site_details(site_id: str, site: Dict,
 
 def _page_menu_entries_site_details(site_id: str, site: Dict,
                                     current_mode: str) -> Iterator[PageMenuEntry]:
-    if current_mode != "edit_site_globals" and _site_globals_editable(site_id, site):
+    if current_mode != "edit_site_globals" and site_globals_editable(site_id, site):
         yield PageMenuEntry(
             title=_("Global settings"),
             icon_name="configuration",

cmk/gui/watolib/sites.py
@@ -664,9 +664,22 @@ def _update_distributed_wato_file(sites):
         _delete_distributed_wato_file()
 
 
-def is_livestatus_encrypted(site):
+def is_livestatus_encrypted(site) -> bool:
     family_spec, address_spec = site["socket"]
     return family_spec in ["tcp", "tcp6"] and address_spec["tls"][0] != "plain_text"
+
+
+def site_globals_editable(site_id, site) -> bool:
+    # Site is a remote site of another site. Allow to edit probably pushed site
+    # specific globals when remote WATO is enabled
+    if config.is_wato_slave_site():
+        return True
+
+    # Local site: Don't enable site specific locals when no remote sites configured
+    if not config.has_wato_slave_sites():
+        return False
+
+    return site["replication"] or config.site_is_local(site_id)
 
 
 def _delete_distributed_wato_file():

commit a81affc8a1547bed90239dd0941542492f804e6a

Fix activation of wrong hosts on remote sites
This regression was introduced with 25eb803c3c8218a3462988175549da2e4a85bead

Change-Id: If5648351136fe83b9e24b8a726c2d08875dc85e0

cmk/update_config.py
@@ -208,7 +208,7 @@ class UpdateConfig:
         for site_id, site_spec in configured_sites.items():
             if site_globals_editable(site_id, site_spec):
                 self._update_global_config(site_spec.setdefault("globals", {}))
-        site_mgmt.save_sites(configured_sites)
+        site_mgmt.save_sites(configured_sites, activate=False)
 
     def _update_global_config(self, global_config):
         # Replace old settings with new ones
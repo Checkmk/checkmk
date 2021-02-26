f844be34b1eab3986e4a6849a9756939310d3d98
Mathias Laurin <mathias.laurin@tribe29.com>
2021-02-26 09:26:12 +0100

    docker.py: Fix unused import
    Change-Id: Id018d93583769be29e21361189bad283316a0d9f

--- cmk/base/plugins/agent_based/utils/docker.py
+++ cmk/base/plugins/agent_based/utils/docker.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Any, Dict, Optional, NamedTuple, Iterable, Tuple
+from typing import Any, Dict, Optional, NamedTuple, Iterable
 import json
 
 from ..agent_based_api.v1.type_defs import StringTable

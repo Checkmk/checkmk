d9066512f277230b80c45ae46f45eaa41227fc4c
Ronny Bruska <ronny.bruska@tribe29.com>
2021-02-26 09:46:44 +0100

    12217 FIX Set topic to "Other" if view topic is empty
    CMK-7292
    
    Change-Id: Ia3e8722ddd55e7df322f00c661d2b620230def3c

--- cmk/gui/views.py
+++ cmk/gui/views.py
@@ -1640,6 +1640,9 @@ def transform_view_to_valuespec_value(view):
         if key in view:
             view["view"][key] = view[key]
 
+    if view["topic"] == "":
+        view["topic"] = "other"
+
     view["view"]['options'] = []
     for key, _title in view_editor_options():
         if view.get(key):

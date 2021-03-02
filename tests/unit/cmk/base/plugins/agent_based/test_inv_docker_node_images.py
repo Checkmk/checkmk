699eeb899a4fdb9a4175a350b3ff846ae485c898
Benedikt Seidl <benedikt.seidl@tribe29.com>
2021-03-02 08:18:29 +0100

    inventory: docker: fix problem with missing labels
    Change-Id: Ib74b80d63661f3d4645ad599f7568a6daf015abb

--- tests/unit/cmk/base/plugins/agent_based/test_inv_docker_node_images.py
+++ tests/unit/cmk/base/plugins/agent_based/test_inv_docker_node_images.py
@@ -25,6 +25,32 @@ AGENT_OUTPUT = (
     '"container": "label", "image_label_command_line": "1", "image_label_dockerfile": "2"}}, '
     '"Image": "sha256:b2bf42ca5d8f3245e58832a1d67d5ce9cfbfe754e9b552b5e5f0e6d26dea4aa5"}')
 
+AGENT_OUTPUT_NULL_LABELS_ST = [
+    [
+        '@docker_version_info',
+        '{"PluginVersion": "0.1", "DockerPyVersion": "4.1.0", "ApiVersion": "1.41"}'
+    ],
+    ['[[[images]]]'],
+    [
+        '{"Id": "sha256:666620a54926240737e6619b0246917892f8ea99f8f80eb3cf8a5f0d77a91c55", "RepoTags": ["plan'
+        'tuml:latest"], "RepoDigests": [], "Parent": "sha256:3d69b34c9f0fc8debed7d47e1bb87b20192c642a515ddc93'
+        'da4baf3a9d3d8a1f", "Comment": "", "Created": "2021-02-25T08:42:22.47977742Z", "Container": "2c78e65e'
+        '14fd8f87df002ac2631b92a25ddfb0ef06386f04cb46495145bcb58a", "ContainerConfig": {"Hostname": "", "Doma'
+        'inname": "", "User": "", "AttachStdin": false, "AttachStdout": false, "AttachStderr": false, "Tty": '
+        'false, "OpenStdin": false, "StdinOnce": false, "Env": ["re", "mo", "ved"], "Cmd": ["dot", "-version"'
+        '], "Image": "sha256:3d69b34c9f0fc8debed7d47e1bb87b20192c642a515ddc93da4baf3a9d3d8a1f", "Volumes": nu'
+        'll, "WorkingDir": "", "Entrypoint": null, "OnBuild": null, "Labels": null}, "DockerVersion": "20.10.'
+        '3", "Author": "", "Config": {"Hostname": "", "Domainname": "", "User": "", "AttachStdin": false, "At'
+        'tachStdout": false, "AttachStderr": false, "Tty": false, "OpenStdin": false, "StdinOnce": false, "En'
+        'v": ["re", "mo", "ved"], "Cmd": ["jshell"], "ArgsEscaped": true, "Image": "sha256:3d69b34c9f0fc8debe'
+        'd7d47e1bb87b20192c642a515ddc93da4baf3a9d3d8a1f", "Volumes": null, "WorkingDir": "", "Entrypoint": nu'
+        'll, "OnBuild": null, "Labels": null}, "Architecture": "amd64", "Os": "linux", "Size": 389770065, "Vi'
+        'rtualSize": 389770065, "GraphDriver": {"Data": {"LowerDir": "re", "MergedDir": "mo", "UpperDir": "ve'
+        'd", "WorkDir": "2"}, "Name": "overlay2"}, "RootFS": {"Type": "layers", "Layers": ["re", "mo", "ved"]'
+        '}, "Metadata": {"LastTagTime": "2021-02-27T20:49:31.186386234+01:00"}}'
+    ],
+]
+
 
 @pytest.mark.usefixtures("load_all_agent_based_plugins")
 def test_inv_docker_node_images():
@@ -58,4 +84,26 @@ def test_inv_docker_node_images():
                  key_columns={'id': 'b2bf42ca5d8f'},
                  inventory_columns={},
                  status_columns={'amount_containers': 1}),
+    ]
+
+
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
+def test_inv_docker_node_images_labels_null():
+    plugin = agent_based_register.get_inventory_plugin(InventoryPluginName('docker_node_images'))
+    assert plugin
+    assert list(plugin.inventory_function(AGENT_OUTPUT_NULL_LABELS_ST)) == [
+        TableRow(path=['software', 'applications', 'docker', 'images'],
+                 key_columns={'id': '666620a54926'},
+                 inventory_columns={
+                     'repotags': 'plantuml:latest',
+                     'repodigests': '',
+                     'creation': '2021-02-25T08:42:22.47977742Z',
+                     'size': 389770065,
+                     'labels': ''
+                 },
+                 status_columns={}),
+        TableRow(path=['software', 'applications', 'docker', 'images'],
+                 key_columns={'id': '666620a54926'},
+                 inventory_columns={},
+                 status_columns={'amount_containers': 0}),
     ]

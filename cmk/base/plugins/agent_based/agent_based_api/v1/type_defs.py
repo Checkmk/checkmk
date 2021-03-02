c94323396d370e9ed41784007aaff6437a3bc58b
Lars Michelsen <lm@tribe29.com>
2021-03-02 17:19:49 +0100

    Fix missing submodules in API docs
    * The submodules render, clusterize and so on are available again.
    * Hack to prevent the submodule from being added to the toc: Declare
      submodule titles as ".. rubric::". This is far from ideal, but the
      only working solution I found with sphinx-apidoc. Let's think about
      removing sphinx-apidoc from our build procedure in the next step.
    
    Change-Id: I2113b6928072c411feb84a97878831cd351f5a6d

--- cmk/base/plugins/agent_based/agent_based_api/v1/type_defs.py
+++ cmk/base/plugins/agent_based/agent_based_api/v1/type_defs.py
@@ -4,8 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 """
-type_defs
-~~~~~~~~~
+.. rubric:: type_defs
 
 This module contains type definitions that users can use if they choose
 to leverage the power of type annotations in their check plugins.

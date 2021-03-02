ab85c6d5f3ce216bcd08f55288e1ff4061748f31
Ronny Bruska <ronny.bruska@tribe29.com>
2021-03-02 21:25:30 +0100

    Remove unused import
    Change-Id: Idc9618adab905cb8405b7bfe18092e662b504e99

--- cmk/gui/plugins/views/commands.py
+++ cmk/gui/plugins/views/commands.py
@@ -5,7 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import time
-from typing import Any, Dict, Optional, Literal, List, Tuple
+from typing import Any, Optional, Literal, List, Tuple
 
 import livestatus
 

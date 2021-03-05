97a8c2ec560115fa28fd3682fee15b9fb2547156
Lisa Pichler <lisa.pichler@tribe29.com>
2021-03-05 12:49:49 +0100

    12361 FIX watchdog_sensors: not possible to apply different rules to dew point and temperature
    SUP-5520
    
    Change-Id: Ib198139fd8dd6dc7959d5711a6e8b15e568bab2a

--- tests/unit/checks/generictests/datasets/watchdog_sensors_regression.py
+++ tests/unit/checks/generictests/datasets/watchdog_sensors_regression.py
@@ -6,24 +6,37 @@ checkname = 'watchdog_sensors'
 
 
 info = [[[u'3.2.0', u'1']],
-        [[u'1', u'First Floor Ambient', u'1', u'213', u'37', u'60', u'']]]
+        [[u'1', u'First Floor Ambient', u'1', u'213', u'37', u'60', u''],
+         [u'2', u'Second Floor Ambient', u'1', u'200', u'30', u'40', u'']]]
 
 
-discovery = {'': [(u'1', {})],
-             'dew': [(u'1', {})],
-             'humidity': [(u'1', {})],
-             'temp': [(u'1', {})]}
+discovery = {'': [(u'Watchdog 1', {}), (u'Watchdog 2', {})],
+             'dew': [(u'Dew point 1', {}), (u'Dew point 2', {})],
+             'humidity': [(u'Humidity 1', {}), (u'Humidity 2', {})],
+             'temp': [(u'Temperature 1', {}), (u'Temperature 2', {})]}
 
 
-checks = {'': [(u'1',
+checks = {'': [(u'Watchdog 1',
                 {},
-                [(0, 'available', []), (0, u'Location: First Floor Ambient', [])])],
-          'dew': [(u'1',
+                [(0, 'available', []), (0, u'Location: First Floor Ambient', [])]),
+               (u'Watchdog 2',
+                {},
+                [(0, 'available', []), (0, u'Location: Second Floor Ambient', [])])],
+          'dew': [(u'Dew point 1',
                    {},
-                   [(0, u'6.0 \xb0C', [('temp', 6.0, None, None, None, None)])])],
-          'humidity': [(u'1',
+                   [(0, u'6.0 \xb0C', [('temp', 6.0, None, None, None, None)])]),
+                  (u'Dew point 2',
+                   {},
+                   [(0, u'4.0 \xb0C', [('temp', 4.0, None, None, None, None)])])],
+          'humidity': [(u'Humidity 1',
                         {'levels': (50, 55), 'levels_lower': (10, 15)},
-                        [(0, '37.0%', [('humidity', 37, 50, 55, None, None)])])],
-          'temp': [(u'1',
+                        [(0, '37.0%', [('humidity', 37, 50, 55, None, None)])]),
+                       (u'Humidity 2',
+                        {'levels': (50, 55), 'levels_lower': (10, 15)},
+                        [(0, '30.0%', [('humidity', 30, 50, 55, None, None)])])],
+          'temp': [(u'Temperature 1',
                     {},
-                    [(0, u'21.3 \xb0C', [('temp', 21.3, None, None, None, None)])])]}
+                    [(0, u'21.3 \xb0C', [('temp', 21.3, None, None, None, None)])]),
+                   (u'Temperature 2',
+                    {},
+                    [(0, u'20.0 \xb0C', [('temp', 20.0, None, None, None, None)])])]}

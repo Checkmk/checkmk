d1ce9198d6da7ffc76e848eee6e0679f04b43c99
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-03-10 23:06:58 +0100

    ps: refactor
    This change will allow to filter the used headers in one central place.
    
    Change-Id: Icb274e3eb4dedb059ddaa8c5d406f6a4e7b4e700

--- cmk/base/plugins/agent_based/utils/ps.py
+++ cmk/base/plugins/agent_based/utils/ps.py
@@ -186,28 +186,19 @@ def format_process_list(processes, html_output):
             return "%.1f%s" % (value, unit)
         return "%s%s" % (value, unit)
 
+    # keys to output and default values:
+    headers = dict.fromkeys((key for process in processes for key, _value in process), '')
+
     if html_output:
         table_bracket = "<table>%s</table>"
         line_bracket = "<tr>%s</tr>"
         cell_bracket = "<td>%.0s%s</td>"
         cell_seperator = ""
-
-        headers = []
-        headers_found = set()
-
-        for process in processes:
-            for key, _value in process:
-                if key not in headers_found:
-                    headers.append(key)
-                    headers_found.add(key)
+        header_line = "<tr><th>" + "</th><th>".join(headers) + "</th></tr>"
 
         # make sure each process has all fields from the table
-        processes_filled = []
-        for process in processes:
-            dictified = dict(process)
-            processes_filled.append([(key, dictified.get(key, "")) for key in headers])
-        processes = processes_filled
-        header_line = "<tr><th>" + "</th><th>".join(headers) + "</th></tr>"
+        processes = [{**headers, **dict(process)}.items() for process in processes]
+
     else:
         table_bracket = "%s"
         line_bracket = "%s\r\n"
@@ -216,10 +207,9 @@ def format_process_list(processes, html_output):
         header_line = ""
 
     return table_bracket % (header_line + "".join([
-        line_bracket %
-        cell_seperator.join([cell_bracket % (key, format_value(value))
-                             for key, value in process])
-        for process in processes
+        line_bracket % cell_seperator.join([
+            cell_bracket % (key, format_value(value)) for key, value in process if key in headers
+        ]) for process in processes
     ]))
 
 

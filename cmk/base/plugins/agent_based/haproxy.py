a7449522bee7467d424cc64b2b4b28a92d7b9473
Sofia Colakovic <sofia.colakovic@tribe29.com>
2021-03-11 10:14:26 +0100

    haproxy checks migration
    SUP-980
    
    Change-Id: I6548c870888fd52a3111aa730140746b8b2cbb75

---
+++ cmk/base/plugins/agent_based/haproxy.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from time import time
+from enum import Enum
+from typing import Dict, Any
+
+from .agent_based_api.v1.type_defs import StringTable, CheckResult, DiscoveryResult
+from .agent_based_api.v1 import (render, register, Result, Service, State, get_value_store,
+                                 get_rate, check_levels)
+
+
+class HAProxyFEStatus(Enum):
+    OPEN = "OPEN"
+    STOP = "STOP"
+
+
+class HAProxyServerStatus(Enum):
+    UP = "UP"
+    DOWN = "DOWN"
+    NOLB = "NOLB"
+    MAINT = "MAINT"
+    DRAIN = "DRAIN"
+    NO_CHECK = "no check"
+
+
+Section = Dict
+
+
+def parse_haproxy(string_table: StringTable) -> Section:
+    parsed = {}
+    for line in string_table:
+        if len(line) <= 32 or line[32] not in ("0", "2"):
+            continue
+
+        data: Dict[str, Any] = {"status": line[17]}
+
+        if line[32] == "0":
+            data["type"] = "frontend"
+            item = line[0]
+            try:
+                data["stot"] = int(line[7])
+            except ValueError:
+                continue
+
+        elif line[32] == "2":
+            data["type"] = "server"
+            item = "%s/%s" % (line[0], line[1])
+            data["layer_check"] = line[36]
+            for key, idx in (("uptime", 23), ("active", 19), ("backup", 20)):
+                try:
+                    data[key] = int(line[idx])
+                except ValueError:
+                    continue
+
+        parsed[item] = data
+
+    return parsed
+
+
+register.agent_section(
+    name="haproxy",
+    parse_function=parse_haproxy,
+)
+
+
+def discover_haproxy_frontend(section: Section) -> DiscoveryResult:
+    for key in section.keys():
+        if section[key]["type"] == "frontend":
+            yield Service(item=key)
+
+
+def check_haproxy_frontend(item: str, section: Section) -> CheckResult:
+    data = section.get(item)
+    if data is None:
+        return
+
+    status = data.get("status")
+    if status == HAProxyFEStatus.OPEN.name:
+        yield Result(state=State.OK, summary=f"Status: {status}")
+    else:
+        yield Result(state=State.CRIT, summary=f"Status: {status}")
+
+    stot = data.get("stot")
+    if stot is not None:
+        value_store = get_value_store()
+        session_rate = get_rate(value_store, f"sessions.{item}", time(), stot)
+        yield from check_levels(value=session_rate,
+                                metric_name="session_rate",
+                                label="Session Rate")
+
+
+register.check_plugin(name="haproxy_frontend",
+                      sections=["haproxy"],
+                      service_name="HAProxy Frontend %s",
+                      discovery_function=discover_haproxy_frontend,
+                      check_function=check_haproxy_frontend)
+
+
+def discover_haproxy_server(section: Section) -> DiscoveryResult:
+    for key in section.keys():
+        if section[key]["type"] == "server":
+            yield Service(item=key)
+
+
+def check_haproxy_server(item: str, section: Section) -> CheckResult:
+    data = section.get(item)
+    if data is None:
+        return
+
+    status = data.get("status")
+    if status == HAProxyServerStatus.UP.name:
+        yield Result(state=State.OK, summary=f"Status: {status}")
+    else:
+        yield Result(state=State.CRIT, summary=f"Status: {status}")
+
+    if data.get("active"):
+        yield Result(state=State.OK, summary="Active")
+    elif data.get("backup"):
+        yield Result(state=State.OK, summary="Backup")
+    else:
+        yield Result(state=State.CRIT, summary="Neither active nor backup")
+
+    yield Result(state=State.OK, summary=f"Layer Check: {data['layer_check']}")
+
+    uptime = data.get("uptime")
+    if uptime is not None:
+        yield Result(state=State.OK, summary=f"Up since {render.timespan(uptime)}")
+
+
+register.check_plugin(name="haproxy_server",
+                      sections=["haproxy"],
+                      service_name="HAProxy Server %s",
+                      discovery_function=discover_haproxy_server,
+                      check_function=check_haproxy_server)

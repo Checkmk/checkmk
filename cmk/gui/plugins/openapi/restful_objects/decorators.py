24299c1a58a838f7fc046403a7d95fbe8d608267
Christoph Rauch <christoph.rauch@tribe29.com>
2021-03-18 11:44:35 +0100

    REST API: decorators, typing
    Change-Id: I9bde1fefdac46447f78940c7c4156862a9859c14

--- cmk/gui/plugins/openapi/restful_objects/decorators.py
+++ cmk/gui/plugins/openapi/restful_objects/decorators.py
@@ -849,7 +849,7 @@ def _tag_from_schema(schema: Type[Schema]) -> OpenAPITag:
     return tag
 
 
-def _docstring_name(docstring: Union[Any, str, None]) -> str:
+def _docstring_name(docstring: Optional[str]) -> str:
     """Split the docstring by title and rest.
 
     This is part of the rest.
@@ -875,7 +875,7 @@ def _docstring_name(docstring: Union[Any, str, None]) -> str:
     return [part.strip() for part in apispec.utils.dedent(docstring).split("\n\n", 1)][0]
 
 
-def _docstring_description(docstring: Union[Any, str, None]) -> Optional[str]:
+def _docstring_description(docstring: Optional[str]) -> Optional[str]:
     """Split the docstring by title and rest.
 
     This is part of the rest.

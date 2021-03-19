d50493ea5c16a54f448ea71cba4d948d06018b51
Lisa Pichler <lisa.pichler@tribe29.com>
2021-03-19 23:43:52 +0100

    12363 FIX fileinfo: files with special characters not discovered
    SUP-5694
    
    Change-Id: I0f5dc3a924fb3b7722530a109bcc92f9e5b80d8c

---
+++ tests/unit/checks/generictests/datasets/fileinfo_special_chars_regression.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8
+# yapf: disable
+
+
+checkname = 'fileinfo'
+
+
+info = [[u'1615877902'],
+        [u'D:\\A\\Ge\\Daten\\D\xc3\xbc-\xc3\u2013bec\xc3\xbclljs\\1Export.csv',
+         u'5357555',
+         u'1615877586'],
+        [u'D:\\A\\Ge\\Daten\\D\xc3\xbc-\xc3\u2013bec\xc3\xbclljs\\2Export.csv',
+         u'5498391',
+         u'1615877590']]
+
+
+discovery = {'': [(u'D:\\A\\Ge\\Daten\\D\xc3\xbc-\xc3\u2013bec\xc3\xbclljs\\1Export.csv',
+                   {}),
+                  (u'D:\\A\\Ge\\Daten\\D\xc3\xbc-\xc3\u2013bec\xc3\xbclljs\\2Export.csv',
+                   {})],
+             'groups': []}
+
+
+checks = {'': [(u'D:\\A\\Ge\\Daten\\D\xc3\xbc-\xc3\u2013bec\xc3\xbclljs\\1Export.csv',
+                {},
+                [(0, 'Size: 5.11 MB', [('size', 5357555, None, None, None, None)]),
+                 (0, 'Age: 5 m', [('age', 316, None, None, None, None)])]),
+               (u'D:\\A\\Ge\\Daten\\D\xc3\xbc-\xc3\u2013bec\xc3\xbclljs\\2Export.csv',
+                {},
+                [(0, 'Size: 5.24 MB', [('size', 5498391, None, None, None, None)]),
+                 (0, 'Age: 5 m', [('age', 312, None, None, None, None)])])]}

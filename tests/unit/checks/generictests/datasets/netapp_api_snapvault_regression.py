fa9f39f59d47ef504e5fed6a2da9700ba62eab65
Lisa Pichler <lisa.pichler@tribe29.com>
2021-03-20 02:34:15 +0100

    netapp_api_snapvault: useful refactorings for migration
    Change-Id: Iaf52422a02e29c8aa4a6cd24e642a680aa9d5c0b

--- tests/unit/checks/generictests/datasets/netapp_api_snapvault_regression.py
+++ tests/unit/checks/generictests/datasets/netapp_api_snapvault_regression.py
@@ -34,14 +34,14 @@ discovery = {
 checks = {
     '': [('/vol/ipb_datap/', {}, [(0, 'Source-System: 172.31.12.15', []),
                                   (0, 'Status: idle state snapvaulted', []),
-                                  (0, 'Lag-Time: 14 h', [])]),
+                                  (0, 'Lag Time: 14 h', [])]),
          ('/vol/ipb_datas/', {}, [(0, 'Source-System: 172.31.12.15', []),
                                   (0, 'Status: idle state snapvaulted', []),
-                                  (0, 'Lag-Time: 14 h', [])]),
+                                  (0, 'Lag Time: 14 h', [])]),
          ('/vol/ipb_user/', {}, [(0, 'Source-System: 172.31.12.15', []),
-                                 (0, 'Status: idle state snapvaulted', []), (0, 'Lag-Time: 26 h',
+                                 (0, 'Status: idle state snapvaulted', []), (0, 'Lag Time: 26 h',
                                                                              [])]),
          ('/vol/ipb_vol0/', {}, [(0, 'Source-System: 172.31.12.15', []),
-                                 (0, 'Status: idle state snapvaulted', []), (0, 'Lag-Time: 26 h',
+                                 (0, 'Status: idle state snapvaulted', []), (0, 'Lag Time: 26 h',
                                                                              [])])]
 }

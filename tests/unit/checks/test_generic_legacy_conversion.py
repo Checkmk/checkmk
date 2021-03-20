9837215c41cdb8aeab706f5989627a9f9119a2ed
Lisa Pichler <lisa.pichler@tribe29.com>
2021-03-20 02:34:51 +0100

    netapp_api_snapvault: migrated check plugin
    * no functional changes in the check plugin - changes exclusively to
      make it compatible
    * no data changes to test cases
    * included test case from generic regression test file in parse,
      discover and check tests
    
    Change-Id: I53413d4d1d7aff57f6c5eedd999f34d014e27b4d

--- tests/unit/checks/test_generic_legacy_conversion.py
+++ tests/unit/checks/test_generic_legacy_conversion.py
@@ -2254,7 +2254,6 @@ def test_no_new_or_vanished_legacy_checks(fix_plugin_legacy):
         'netapp_api_psu.summary',
         'netapp_api_qtree_quota',
         'netapp_api_snapshots',
-        'netapp_api_snapvault',
         'netapp_api_status',
         'netapp_api_systemtime',
         'netapp_api_temp',

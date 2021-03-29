3ac684e0fdc60085086395513d58323fc374f207
Marcel Arentz <gdspd_you@open-one.de>
2021-03-29 15:20:58 +0000

    12424 FIX <tt>check_mail</tt>: Fix <tt>error: argument --connect-timeout: invalid int value</tt>
    There was bug in the active check <tt>check_mail</tt>, which checks if the login into
    a given mailbox is working. In case a connection timeout was configured, the check
    crashed with
    <tt>error: argument --connect-timeout: invalid int value</tt>
    This has been fixed.
    
    Closes #355
    
    Change-Id: I9e376f8551d739cf33ecf7f2f0c852ed8da10acc

--- tests/unit/checks/test_check_mail.py
+++ tests/unit/checks/test_check_mail.py
@@ -10,19 +10,32 @@ from testlib import ActiveCheck  # type: ignore[import]
 pytestmark = pytest.mark.checks
 
 
-@pytest.mark.parametrize("params,expected_args", [({
-    "fetch": ("IMAP", ({
-        "ssl": (True, 143),
-        "auth": ("foo", "bar"),
-    }))
-}, [
-    "--fetch-protocol=IMAP",
-    "--fetch-server=$HOSTADDRESS$",
-    "--fetch-tls",
-    "--fetch-port=143",
-    "--fetch-username=foo",
-    "--fetch-password=bar",
-])])
+@pytest.mark.parametrize(
+    "params,expected_args",
+    [
+        (
+            {
+                "fetch": (
+                    "IMAP",
+                    {
+                        "ssl": (True, 143),
+                        "auth": ("foo", "bar"),
+                    },
+                ),
+                "connect_timeout": 15,
+            },
+            [
+                "--fetch-protocol=IMAP",
+                "--fetch-server=$HOSTADDRESS$",
+                "--fetch-tls",
+                "--fetch-port=143",
+                "--fetch-username=foo",
+                "--fetch-password=bar",
+                "--connect-timeout=15",
+            ],
+        ),
+    ],
+)
 def test_check_mail_argument_parsing(params, expected_args):
     """Tests if all required arguments are present."""
     active_check = ActiveCheck("check_mail")

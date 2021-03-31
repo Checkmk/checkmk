ae0093f8b29326d10af8c49bd4d017fc210d6a8b
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-03-31 12:40:04 +0200

    SUP-5782: introduce midlayer abstract class AbstractMemoryObserver in cmk.utils.observer
    - reimplement MemoryObserver as a subclass from AbstractMemoryObserver
    - add unit tests
    - refactoring: replace OOP Kung-Fu with simple call to superclass member function _register_check
    
    This CL is intended to provide basis for future memory watchdog class in bin/fetcher.
    
    Change-Id: I4731e05b4743d0abbee7d38347869eb9021e18f8

--- cmk/utils/observer.py
+++ cmk/utils/observer.py
@@ -6,13 +6,18 @@
 
 import abc
 import logging
-from typing import Optional
+from typing import Optional, Final, Dict
 
 from cmk.utils.log import VERBOSE
 from cmk.utils.type_defs import HostName
+from cmk.utils.caching import config_cache
+from cmk.utils.caching import runtime_cache
+import cmk.utils.render as render
+import cmk.utils.misc
 
 __all__ = [
     "ABCResourceObserver",
+    "AbstractMemoryObserver",
 ]
 
 
@@ -25,11 +30,14 @@ class ABCResourceObserver(abc.ABC):
         self._num_check_cycles = 0
         self._hostname = "<unknown>"
 
-    @abc.abstractmethod
-    def check_resources(self, hostname: Optional[HostName]) -> None:
+    def _register_check(self, hostname: Optional[HostName]) -> None:
         self._num_check_cycles += 1
         if hostname is not None:
             self._hostname = hostname
+
+    @abc.abstractmethod
+    def check_resources(self, hostname: Optional[HostName]) -> None:
+        raise NotImplementedError()
 
     def config_has_changed(self) -> None:
         pass
@@ -43,3 +51,63 @@ class ABCResourceObserver(abc.ABC):
 
     def _verbose_output_enabled(self) -> bool:
         return self._logger.isEnabledFor(logging.DEBUG)
+
+
+class AbstractMemoryObserver(ABCResourceObserver):
+    """Observes usage of the memory by the current process. Excessive memory usage by
+    process is defined as (initial VM size)*self._allowed_growth/100.
+    Initial VM size is stored at 5-th call of check_resources().
+    """
+    __slots__ = ['_memory_usage', '_allowed_growth', '_steady_cycle_num']
+
+    def __init__(self, allowed_growth: int) -> None:
+        """allowed_growth is the permitted increase of the VM size measured in percents."""
+        super(AbstractMemoryObserver, self).__init__()
+        self._memory_usage = 0
+        self._allowed_growth = allowed_growth
+        self._steady_cycle_num: Final = 5
+
+    def memory_usage(self) -> int:
+        return self._memory_usage
+
+    def _validate_size(self) -> bool:
+        """ Determines whether RAM limit was exceeded.
+        Registers (once) memory status when steady state is achieved.
+        """
+
+        # We should have reached a steady state after 5 check cycles.
+        if self._num_check_cycles < self._steady_cycle_num:
+            return True
+
+        # We observe every cycle after reaching the steady state.
+        # This is OK performance-wise: ~7 microseconds per observation.
+        new_memory_usage = self._vm_size()
+        if self._num_check_cycles == self._steady_cycle_num:
+            self._print_global_memory_usage()
+            self._memory_usage = new_memory_usage
+            return True
+
+        return new_memory_usage <= int(self._allowed_growth / 100 * self._memory_usage)
+
+    @staticmethod
+    def _vm_size() -> int:
+        with open('/proc/self/statm') as f:  # see: man proc(5).
+            return int(f.read().split()[0]) * 4096
+
+    def _print_global_memory_usage(self) -> None:
+        if not self._verbose_output_enabled():
+            return
+        globals_sizes = {
+            varname: cmk.utils.misc.total_size(value) for (varname, value) in globals().items()
+        }
+        self._dump("APPROXIMATE SIZES: GLOBALS TOP 50", globals_sizes, 50)
+        for title, module in [
+            ("CONFIG CACHE", config_cache),
+            ("RUNTIME CACHE", runtime_cache),
+        ]:
+            self._dump("APPROXIMATE SIZES: %s" % title, module.dump_sizes(), None)
+
+    def _dump(self, header: str, sizes: Dict[str, int], limit: Optional[int]) -> None:
+        self._warning("=== %s ====" % header)
+        for varname, size_bytes in sorted(sizes.items(), key=lambda x: x[1], reverse=True)[:limit]:
+            self._warning("%10s %s" % (render.fmt_bytes(size_bytes), varname))

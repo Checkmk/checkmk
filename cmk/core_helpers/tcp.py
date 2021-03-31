8d2648e110d24c136b01f8ee98175636b4c75328
Andreas Umbreit <andreas.umbreit@tribe29.com>
2021-03-31 22:28:42 +0200

    CMK-7588: Support encrypted agent data with 99 header
    This was supported accidentally until recently, and
    is not supported explicitly in order to overcome
    the corresponding bug in Windows agents < 2.0.0p1
    
    Change-Id: I38a4420acd9f3d1ccf58df6a1b94985bb38dbaa2

--- cmk/core_helpers/tcp.py
+++ cmk/core_helpers/tcp.py
@@ -193,5 +193,16 @@ class TCPFetcher(AgentFetcher):
                     password=password,
                     digest=md5,
                 ))
+        # Support encrypted agent data with "99" header.
+        # This was not intended, but the Windows agent accidentally sent this header
+        # instead of "00" up to 2.0.0p1, so we keep this for a while.
+        # Caution: "99" for real-time check data means "unencrypted"!
+        if protocol == 99:
+            return AgentRawData(
+                decrypt_aes_256_cbc_legacy(
+                    ciphertext=encrypted_pkg,
+                    password=password,
+                    digest=md5,
+                ))
 
         raise MKFetcherError(f"Unsupported protocol version: {protocol}")

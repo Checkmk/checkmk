a7953ea606980b0db490820247b1a64ae5f5b5ab
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-03-31 07:53:13 +0200

    docker_container_status: siplify doctest
    Change-Id: I4a3a29560d3a22b0996c3ecdc2670f103b741a72

--- cmk/base/plugins/agent_based/docker_container_status.py
+++ cmk/base/plugins/agent_based/docker_container_status.py
@@ -57,25 +57,29 @@ def parse_docker_container_status(string_table: StringTable) -> Dict[str, Any]:
 
 def host_labels_docker_container_status(section) -> HostLabelGenerator:
     """
-    >>> from pprint import pprint as pp
-    >>> list(host_labels_docker_container_status({}))
-    [HostLabel('cmk/docker_object', 'container')]
-    >>> list(host_labels_docker_container_status({"ImageTags": []}))
-    [HostLabel('cmk/docker_object', 'container')]
-    >>> pp(list(host_labels_docker_container_status({"ImageTags": ["doctor:strange"]})))
-    [HostLabel('cmk/docker_object', 'container'),
-     HostLabel('cmk/docker_image', 'doctor:strange'),
-     HostLabel('cmk/docker_image_name', 'doctor'),
-     HostLabel('cmk/docker_image_version', 'strange')]
-    >>> pp(list(host_labels_docker_container_status({"ImageTags": ["fiction/doctor:strange"]})))
-    [HostLabel('cmk/docker_object', 'container'),
-     HostLabel('cmk/docker_image', 'fiction/doctor:strange'),
-     HostLabel('cmk/docker_image_name', 'doctor'),
-     HostLabel('cmk/docker_image_version', 'strange')]
-    >>> pp(list(host_labels_docker_container_status({"ImageTags": ["library:8080/fiction/doctor"]})))
-    [HostLabel('cmk/docker_object', 'container'),
-     HostLabel('cmk/docker_image', 'library:8080/fiction/doctor'),
-     HostLabel('cmk/docker_image_name', 'doctor')]
+        >>> from pprint import pprint
+        >>> def show(gen):
+        ...     for l in gen: print(':'.join(l))
+        ...
+        >>> show(host_labels_docker_container_status({}))
+        cmk/docker_object:container
+        >>> show(host_labels_docker_container_status({"ImageTags": []}))
+        cmk/docker_object:container
+        >>> show(host_labels_docker_container_status({"ImageTags": ["doctor:strange"]}))
+        cmk/docker_object:container
+        cmk/docker_image:doctor:strange
+        cmk/docker_image_name:doctor
+        cmk/docker_image_version:strange
+        >>> show(host_labels_docker_container_status({"ImageTags": ["fiction/doctor:strange"]}))
+        cmk/docker_object:container
+        cmk/docker_image:fiction/doctor:strange
+        cmk/docker_image_name:doctor
+        cmk/docker_image_version:strange
+        >>> show(host_labels_docker_container_status({"ImageTags": ["library:8080/fiction/doctor"]}))
+        cmk/docker_object:container
+        cmk/docker_image:library:8080/fiction/doctor
+        cmk/docker_image_name:doctor
+
     """
     yield HostLabel("cmk/docker_object", "container")
 

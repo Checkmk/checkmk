bc6f1bef572f8f740f10986b8dbf0979dd121b2f
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-04-12 16:26:33 +0200

    12503 FIX mssql_counters_file_sizes: Stale services on clusters
    SUP-5853
    
    Change-Id: I5dfacfd4924c39c62024d5559760a06786beccd9

--- cmk/base/plugins/agent_based/mssql_counters_sqlstats.py
+++ cmk/base/plugins/agent_based/mssql_counters_sqlstats.py
@@ -16,6 +16,7 @@ from .agent_based_api.v1 import (
 )
 
 from .utils.mssql_counters import (
+    accumulate_node_results,
     Section,
     get_rate_or_none,
     get_item,
@@ -116,8 +117,11 @@ def _cluster_check_base(
     Result(state=<State.OK: 0>, summary='[node1] 1.0/s')
     Metric('sql_compilations_per_second', 1.0, boundaries=(0.0, None))
     """
-    for node_name, node_section in section.items():
-        yield from _check_common(value_store, time_point, node_name, item, params, node_section)
+    yield from accumulate_node_results(
+        node_check_function=lambda node_name, node_section: _check_common(
+            value_store, time_point, node_name, item, params, node_section),
+        section=section,
+    )
 
 
 def cluster_check_mssql_counters_sqlstats(

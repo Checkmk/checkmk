ff4abcd0fff0d93cf39f48b2ff6f67111a95e0ca
Lisa Pichler <lisa.pichler@tribe29.com>
2021-04-15 09:54:47 +0200

    winperf_processor.util: remove legacy plugin
    Change-Id: If360063d170058937c6f89a82826273ffc1a0ede

--- tests/unit/checks/test_generic_legacy_conversion.py
+++ tests/unit/checks/test_generic_legacy_conversion.py
@@ -2726,7 +2726,6 @@ def test_no_new_or_vanished_legacy_checks(fix_plugin_legacy):
         'winperf.diskstat',
         'winperf_mem',
         'winperf_msx_queues',
-        'winperf_processor.util',
         'winperf_ts_sessions',
         'wmi_cpuload',
         'wmi_webservices',

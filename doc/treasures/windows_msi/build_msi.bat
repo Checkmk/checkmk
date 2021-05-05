f7ca5d6d23aa8d8ae5d4dab6760ebae335d86db9
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-05-05 13:24:35 +0200

    CMK-7859: remove old windows agent files
    Change-Id: I47b7688d6bf3df53007e66418c4efaeb669cfb23

--- doc/treasures/windows_msi/build_msi.bat
+++
@@ -1,7 +0,0 @@
-"C:\Program Files (x86)\wix\candle.exe" -ext WixUtilExtension C:\mkmsi\cmk_WixUI_InstallDir.wxs
-"C:\Program Files (x86)\wix\candle.exe" -ext WixUtilExtension C:\mkmsi\cmk_InstallDirDlg.wxs
-"C:\Program Files (x86)\wix\candle.exe" -ext WixUtilExtension C:\mkmsi\check_mk_agent_baked.wxs
-"C:\Program Files (x86)\wix\candle.exe" -ext WixUtilExtension C:\mkmsi\check_mk_agent_vanilla.wxs
-"C:\Program Files (x86)\wix\light.exe" -ext WixUIExtension -ext WixUtilExtension -sval -o check_mk_agent_baked.msi C:\mkmsi\check_mk_agent_baked.wixobj C:\mkmsi\cmk_WixUI_InstallDir.wixobj C:\mkmsi\cmk_InstallDirDlg.wixobj
-"C:\Program Files (x86)\wix\light.exe" -ext WixUIExtension -ext WixUtilExtension -sval -o check_mk_agent_vanilla.msi C:\mkmsi\check_mk_agent_vanilla.wixobj C:\mkmsi\cmk_WixUI_InstallDir.wixobj C:\mkmsi\cmk_InstallDirDlg.wixobj
-@pause

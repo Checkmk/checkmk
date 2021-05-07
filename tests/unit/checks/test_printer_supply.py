250ce82f1d0bf7d9794b7d50f989facc56a1a728
Joerg Herbel <joerg.herbel@tribe29.com>
2021-05-07 08:52:54 +0200

    12877 FIX <tt>printer_supply</tt>: handle trailing null bytes in prtMarkerColorantValue
    The check plugin <tt>printer_supply</tt>, which monitors the fill levels of
    printer supplies via SNMP, is now able to handle trailing null bytes in the
    field prtMarkerColorantValue (observed for example for HP Jetdirect 153).
    Previously, such null bytes broke the displaying of the corresponding service
    output in the GUI.
    
    Closes #369
    
    Change-Id: I74df27d4c5270e6053e326e705090b9fd91b8a72

---
+++ tests/unit/checks/test_printer_supply.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code packageii
+
+import pytest
+
+from testlib import Check
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.parametrize(
+    "string_table, expected_result",
+    [
+        pytest.param(
+            [
+                [['1.1', 'black\x00']],
+                [['Patrone Schwarz 508A HP CF360A\x00', '19', '100', '9', '3', '1']],
+            ],
+            [
+                [
+                    'Patrone Schwarz 508A HP CF360A\x00',
+                    '19',
+                    '100',
+                    '9',
+                    '3',
+                    '1',
+                    'black',
+                ],
+            ],
+            id="with null bytes",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_parse_printer_supply(string_table, expected_result):
+    assert Check("printer_supply").run_parse(string_table) == expected_result

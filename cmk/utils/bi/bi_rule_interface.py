863498d1533292057e0438ac052639d4f778988d
Joerg Herbel <joerg.herbel@tribe29.com>
2021-05-10 10:51:22 +0200

    Remove unused import to make pylint happy
    Change-Id: Iea598e2ca056ae4de98363cbc5ab1da36b1d15cc

--- cmk/utils/bi/bi_rule_interface.py
+++ cmk/utils/bi/bi_rule_interface.py
@@ -5,7 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import abc
-from typing import List, Dict, Any, Type, Sequence, Tuple
+from typing import List, Dict, Any, Type, Sequence
 import cmk.utils.plugin_registry
 from cmk.utils.bi.bi_lib import (
     ABCBICompiledNode,

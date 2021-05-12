4853d07047fb5820ab291b492cbe8f747b1e54b0
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2021-05-12 15:10:12 +0200

    Fix 2020-36242 for win agent env
    Change-Id: I598d173fd944253e51e08cef56bd8ee84a977319

--- agents/wnx/python_packages.cmd
+++ agents/wnx/python_packages.cmd
@@ -60,3 +60,4 @@ call python_package_install !ppath! pyOpenSSL
 call python_package_install !ppath! requests[socks]
 call python_package_install !ppath! pathlib
 call python_package_install !ppath! pathlib2
+call python_package_install !ppath! "cryptography==3.3.2"

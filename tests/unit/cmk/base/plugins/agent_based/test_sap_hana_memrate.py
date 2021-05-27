78098752bf4d61003f639536f0add873460e0f90
Sofia Colakovic <sofia.colakovic@tribe29.com>
2021-05-27 11:05:49 +0200

    migrate sap_hana_memrate
    Change-Id: I087099ebdaada0c211f862981fb70a813e6fe0ab

--- tests/unit/cmk/base/plugins/agent_based/test_sap_hana_memrate.py
+++ tests/unit/cmk/base/plugins/agent_based/test_sap_hana_memrate.py
@@ -6,9 +6,12 @@
 
 import pytest
 
-from testlib import Check
+from cmk.base.api.agent_based import register
+from cmk.utils.type_defs import SectionName, CheckPluginName
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, State, Service, Metric
 
 
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
 @pytest.mark.parametrize("info, expected_result", [
     (
         [
@@ -39,22 +42,30 @@ from testlib import Check
     }),
 ])
 def test_parse_sap_hana_memrate(info, expected_result):
-    result = Check("sap_hana_memrate").run_parse(info)
+    section_name = SectionName("sap_hana_memrate")
+    section_plugin = register.get_section_plugin(section_name)
+    result = section_plugin.parse_function(info)
     assert result == expected_result
 
 
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
 @pytest.mark.parametrize("info, expected_result", [
     ([
         ["[[HXE 90 SYSTEMDB]]"],
         ["mem_rate", "5115896693", "7297159168"],
-    ], [("HXE 90 SYSTEMDB", {})]),
+    ], [Service(item="HXE 90 SYSTEMDB")]),
 ])
 def test_inventory_sap_hana_memrate(info, expected_result):
-    section = Check("sap_hana_memrate").run_parse(info)
-    result = Check("sap_hana_memrate").run_discovery(section)
-    assert list(result) == expected_result
+    section_name = SectionName("sap_hana_memrate")
+    section = register.get_section_plugin(section_name).parse_function(info)
+
+    plugin_name = CheckPluginName("sap_hana_memrate")
+    plugin = register.get_check_plugin(plugin_name)
+    if plugin:
+        assert list(plugin.discovery_function(section)) == expected_result
 
 
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
 @pytest.mark.parametrize("item, info, expected_result", [
     (
         "HXE 90 SYSTEMDB",
@@ -62,12 +73,17 @@ def test_inventory_sap_hana_memrate(info, expected_result):
             ["[[HXE 90 SYSTEMDB]]"],
             ["mem_rate", "5115896693", "7297159168"],
         ],
-        [(0, "Usage: 70.11% - 4.76 GB of 6.80 GB", [
-            ("memory_used", 5115896693, None, None, 0, 7297159168)
-        ])],
+        [
+            Result(state=State.OK, summary="Usage: 70.11% - 4.76 GiB of 6.80 GiB"),
+            Metric("memory_used", 5115896693.0, boundaries=(0.0, 7297159168.0))
+        ],
     ),
 ])
 def test_check_sap_hana_memrate(item, info, expected_result):
-    section = Check("sap_hana_memrate").run_parse(info)
-    result = Check("sap_hana_memrate").run_check(item, {}, section)
-    assert list(result) == expected_result
+    section_name = SectionName("sap_hana_memrate")
+    section = register.get_section_plugin(section_name).parse_function(info)
+
+    plugin_name = CheckPluginName("sap_hana_memrate")
+    plugin = register.get_check_plugin(plugin_name)
+    if plugin:
+        assert list(plugin.check_function(item, {}, section)) == expected_result

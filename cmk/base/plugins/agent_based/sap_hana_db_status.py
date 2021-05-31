70712eb5b99b88436111afd06bb21c8f52a17d3b
Sofia Colakovic <sofia.colakovic@tribe29.com>
2021-05-31 16:03:21 +0200

    sap_hana_db_status check
    CMK-7935
    
    Change-Id: I7e1000baa157d18a0bb72b388d23b48fe72f6d45

---
+++ cmk/base/plugins/agent_based/sap_hana_db_status.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import Dict
+
+from .utils import sap_hana
+from .agent_based_api.v1 import register, Service, Result, State
+from .agent_based_api.v1.type_defs import (
+    DiscoveryResult,
+    StringTable,
+    CheckResult,
+)
+
+
+def parse_sap_hana_db_status(string_table: StringTable) -> Dict[str, str]:
+    return {
+        sid_instance: lines[0][0]
+        for sid_instance, lines in sap_hana.parse_sap_hana(string_table).items()
+    }
+
+
+register.agent_section(
+    name="sap_hana_db_status",
+    parse_function=parse_sap_hana_db_status,
+)
+
+
+def discovery_sap_hana_db_status(section: sap_hana.ParsedSection) -> DiscoveryResult:
+    for item in section:
+        yield Service(item=item)
+
+
+def check_sap_hana_db_status(item: str, section: Dict[str, str]) -> CheckResult:
+    db_status = section.get(item)
+    if db_status is None:
+        return
+
+    state = State.OK if db_status == "OK" else State.CRIT
+    yield Result(state=state, summary=db_status)
+
+
+register.check_plugin(
+    name="sap_hana_db_status",
+    service_name="SAP HANA Database Status %s",
+    discovery_function=discovery_sap_hana_db_status,
+    check_function=check_sap_hana_db_status,
+    cluster_check_function=sap_hana.get_cluster_check(check_sap_hana_db_status),
+)
---
+++ tests/unit/cmk/base/plugins/agent_based/test_sap_hana_db_status.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.base.api.agent_based import register
+from cmk.utils.type_defs import SectionName, CheckPluginName
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, State, Service
+
+
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
+@pytest.mark.parametrize("info, expected_result", [(
+    [
+        ["[[HXE 98]]"],
+        ["OK"],
+    ],
+    {
+        "HXE 98": "OK"
+    },
+)])
+def test_parse_sap_hana_db_status(info, expected_result):
+    section_name = SectionName("sap_hana_db_status")
+    section_plugin = register.get_section_plugin(section_name)
+    result = section_plugin.parse_function(info)
+    assert result == expected_result
+
+
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
+@pytest.mark.parametrize("info, expected_result", [
+    (
+        [
+            ["[[HXE 98]]"],
+            ["OK"],
+        ],
+        [Service(item="HXE 98")],
+    ),
+])
+def test_inventory_sap_hana_db_status(info, expected_result):
+    section_name = SectionName("sap_hana_db_status")
+    section = register.get_section_plugin(section_name).parse_function(info)
+    plugin_name = CheckPluginName("sap_hana_db_status")
+    plugin = register.get_check_plugin(plugin_name)
+    if plugin:
+        assert list(plugin.discovery_function(section)) == expected_result
+
+
+@pytest.mark.usefixtures("load_all_agent_based_plugins")
+@pytest.mark.parametrize("item, info, expected_result", [
+    (
+        "HXE 98",
+        [
+            ["[[HXE 98]]"],
+            ["OK"],
+        ],
+        [Result(state=State.OK, summary="OK")],
+    ),
+    (
+        "HXE 98",
+        [
+            ["[[HXE 98]]"],
+            ["DB status failed: * -10104: Invalid value for KEY"],
+        ],
+        [Result(state=State.CRIT, summary="DB status failed: * -10104: Invalid value for KEY")],
+    ),
+    (
+        "HXE 99",
+        [
+            ["[[HXE 98]]"],
+            ["OK"],
+        ],
+        [],
+    ),
+])
+def test_check_sap_hana_db_status(item, info, expected_result):
+    section_name = SectionName("sap_hana_db_status")
+    section = register.get_section_plugin(section_name).parse_function(info)
+    plugin_name = CheckPluginName("sap_hana_db_status")
+    plugin = register.get_check_plugin(plugin_name)
+    if plugin:
+        assert list(plugin.check_function(item, section)) == expected_result

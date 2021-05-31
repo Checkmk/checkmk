eac9baad7dfaab67b0253faee682ac18f24ec72c
Joerg Herbel <joerg.herbel@tribe29.com>
2021-05-31 20:36:26 +0200

    Remove unused import
    Change-Id: If4881b41e2bc93ed0e3bf361f8bdbb4e562b09db

--- cmk/base/plugins/agent_based/sap_hana_replication_status.py
+++ cmk/base/plugins/agent_based/sap_hana_replication_status.py
@@ -4,7 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Any, Mapping, Final, Dict
+from typing import Any, Mapping, Final
 
 from .utils import sap_hana
 from .agent_based_api.v1 import register, Service, Result, State

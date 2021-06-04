08f5e972f8cf5dd99ca7883df39cea4c3d58ce1d
Sven Panne <sven.panne@tribe29.com>
2021-06-04 10:57:13 +0200

    Added groovy and hirsute platforms. Added Clang 12 support.
    Change-Id: I8eeaf2a6b48eb4c15f5713212e5dee70dad162fd

--- buildscripts/infrastructure/build-nodes/scripts/install-clang.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-clang.sh
@@ -29,6 +29,7 @@ declare -A LLVM_VERSION_PATTERNS
 LLVM_VERSION_PATTERNS[9]="-9"
 LLVM_VERSION_PATTERNS[10]="-10"
 LLVM_VERSION_PATTERNS[11]=""
+LLVM_VERSION_PATTERNS[12]=""
 
 if [ ! ${LLVM_VERSION_PATTERNS[$LLVM_VERSION]+_} ]; then
     echo "This script does not support LLVM version $LLVM_VERSION"
@@ -47,8 +48,10 @@ case "$DIST_VERSION" in
     Ubuntu_18.04 )    REPO_NAME="deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic$LLVM_VERSION_STRING  main" ;;
     Ubuntu_18.10 )    REPO_NAME="deb http://apt.llvm.org/cosmic/   llvm-toolchain-cosmic$LLVM_VERSION_STRING  main" ;;
     Ubuntu_19.04 )    REPO_NAME="deb http://apt.llvm.org/disco/    llvm-toolchain-disco$LLVM_VERSION_STRING   main" ;;
-    Ubuntu_19.10 )   REPO_NAME="deb http://apt.llvm.org/eoan/      llvm-toolchain-eoan$LLVM_VERSION_STRING    main" ;;
-    Ubuntu_20.04 )   REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING    main" ;;
+    Ubuntu_19.10 )    REPO_NAME="deb http://apt.llvm.org/eoan/     llvm-toolchain-eoan$LLVM_VERSION_STRING    main" ;;
+    Ubuntu_20.04 )    REPO_NAME="deb http://apt.llvm.org/focal/    llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
+    Ubuntu_20.10 )    REPO_NAME="deb http://apt.llvm.org/groovy/   llvm-toolchain-groovy$LLVM_VERSION_STRING  main" ;;
+    Ubuntu_21.04 )    REPO_NAME="deb http://apt.llvm.org/hirsute/  llvm-toolchain-hirsute$LLVM_VERSION_STRING main" ;;
     * )
         echo "Distribution '$DISTRO' in version '$VERSION' is not supported by this script (${DIST_VERSION})."
         exit 2

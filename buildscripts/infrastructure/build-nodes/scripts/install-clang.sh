24524ad0a770b0bcf7ea35b1c91bd98ef2d07b49
Sven Panne <sven.panne@tribe29.com>
2021-06-04 11:01:27 +0200

    Assimilated clang-tidy installation.
    It should better be in sync with the rest of the clang toolchain, anyway.
    Therefore, let's install everything in one sweep.
    
    Change-Id: I767996bb5d71b963583d11401cc36b767d567711

--- buildscripts/infrastructure/build-nodes/scripts/install-clang.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-clang.sh
@@ -63,4 +63,10 @@ esac
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 add-apt-repository "${REPO_NAME}"
 apt-get update
-apt-get install -y clang-$LLVM_VERSION lldb-$LLVM_VERSION lld-$LLVM_VERSION clangd-$LLVM_VERSION libclang-$LLVM_VERSION-dev
+apt-get install -y \
+        clang-$LLVM_VERSION \
+        clangd-$LLVM_VERSION \
+        clang-tidy-$LLVM_VERSION \
+        lld-$LLVM_VERSION \
+        lldb-$LLVM_VERSION \
+        libclang-$LLVM_VERSION-dev

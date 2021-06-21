05567f433a55ae7526c18673f5ecf6b587e37ef1
Joerg Herbel <joerg.herbel@tribe29.com>
2021-06-21 10:04:01 +0200

    12981 FIX <tt>oracle_jobs</tt>: Fix ValueError for setups with CDB and nonCDB on same host
    On hosts with CDB and nonCDB databases, the check <tt>oracle_jobs</tt> crashed.
    This has been fixed.
    
    Closes #377
    
    Change-Id: I2e0316e3d96dc4c5db04d59d2ed9912cf90a0618

--- tests/unit/checks/test_oracle_jobs.py
+++ tests/unit/checks/test_oracle_jobs.py
@@ -1,4 +1,5 @@
-import pytest  # type: ignore
+import pytest
+
 from cmk_base.check_api import MKCounterWrapped
 
 pytestmark = pytest.mark.checks
@@ -24,3 +25,88 @@ def test_oracle_jobs_check_error(check_manager, info):
     check = check_manager.get_check('oracle_jobs')
     with pytest.raises(MKCounterWrapped):
         check.run_check("DB19.SYS.JOB1", {}, info)
+
+
+_STRING_TABLE_CDB_NONCDB = [
+    [
+        'CDB',
+        'CDB$ROOT',
+        'SYS',
+        'AUTO_SPACE_ADVISOR_JOB',
+        'SCHEDULED',
+        '0',
+        '46',
+        'TRUE',
+        '15-JUN-21 01.01.01.143871 AM +00:00',
+        '-',
+        'SUCCEEDED',
+    ],
+    [
+        'NONCDB',
+        'SYS',
+        'AUTO_SPACE_ADVISOR_JOB',
+        'SCHEDULED',
+        '995',
+        '1129',
+        'TRUE',
+        '16-JUN-21 01.01.01.143871 AM +00:00',
+        'MAINTENANCE_WINDOW_GROUP',
+        '',
+    ],
+]
+
+
+def test_discovery_cdb_noncdb(check_manager):
+    assert list(check_manager.get_check('oracle_jobs').run_discovery(_STRING_TABLE_CDB_NONCDB)) == [
+        (
+            'CDB.CDB$ROOT.SYS.AUTO_SPACE_ADVISOR_JOB',
+            {},
+        ),
+        (
+            'NONCDB.SYS.AUTO_SPACE_ADVISOR_JOB',
+            {},
+        ),
+    ]
+
+
+@pytest.mark.parametrize(
+    "item, result",
+    [
+        pytest.param(
+            'CDB.CDB$ROOT.SYS.AUTO_SPACE_ADVISOR_JOB',
+            (
+                0,
+                'Job-State: SCHEDULED, Enabled: Yes, Last Duration: 0.00 s, Next Run: 15-JUN-21 01.01.01.143871 AM +00:00, Last Run Status: SUCCEEDED (ignored disabled Job)',
+                [
+                    ('duration', 0),
+                ],
+            ),
+            id="cdb",
+        ),
+        pytest.param(
+            'NONCDB.SYS.AUTO_SPACE_ADVISOR_JOB',
+            (
+                1,
+                'Job-State: SCHEDULED, Enabled: Yes, Last Duration: 16 m, Next Run: 16-JUN-21 01.01.01.143871 AM +00:00,  no log information found(!)',
+                [
+                    ('duration', 995),
+                ],
+            ),
+            id="noncdb",
+        ),
+    ],
+)
+def test_check_cdb_noncdb(
+        check_manager,
+        item,
+        result,
+):
+    assert check_manager.get_check('oracle_jobs').run_check(
+        item,
+        {
+            'disabled': False,
+            'status_missing_jobs': 2,
+            'missinglog': 1,
+        },
+        _STRING_TABLE_CDB_NONCDB,
+    ) == result

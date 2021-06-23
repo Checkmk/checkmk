879b4375fbda0a7e69bff20e87dc15226dc144d3
Mathias Laurin <mathias.laurin@tribe29.com>
2021-06-23 11:42:36 +0200

    Add 2nd template arg to base ListColumn::Callback
    Now, we can use this as the base class to columns
    such as `comment` and `downtime`.
    
    CMK-7076
    
    Change-Id: Ic3e1b9ce28e40c96c1469ed4e370b80f7174ee4b

--- livestatus/src/ListLambdaColumn.h
+++ livestatus/src/ListLambdaColumn.h
@@ -6,8 +6,11 @@
 #ifndef ListLambdaColumn_h
 #define ListLambdaColumn_h
 
+// We use `std::transform` but IWYU does not want the header.
+#include <algorithm>  // IWYU pragma: keep
 #include <chrono>
 #include <functional>
+#include <iterator>
 #include <memory>
 #include <stdexcept>
 #include <string>
@@ -27,13 +30,18 @@ namespace detail::column {
 // Specialize this function in the classes deriving ListColumn.
 template <class U>
 std::string serialize(const U&);
+
+template <>
+inline std::string serialize(const std::string& s) {
+    return s;
+}
 }  // namespace detail::column
 
 struct ListColumn : Column {
     using value_type = std::vector<std::string>;
     class Constant;
     class Reference;
-    template <class T>
+    template <class T, class U = std::string>
     class Callback;
     using Column::Column;
     ~ListColumn() override = default;
@@ -58,10 +66,12 @@ struct ListColumn : Column {
 
 // TODO(sp): Is there a way to have a default value in the template parameters?
 // Currently it is hardwired to the empty vector.
-template <class T>
+template <class T, class U>
 class ListColumn::Callback : public ListColumn {
-    using f0_t = std::function<value_type(const T&)>;
-    using f1_t = std::function<value_type(const T&, const contact*)>;
+    using f0_t = std::function<std::vector<U>(const T&)>;
+    using f1_t = std::function<std::vector<U>(const T&, const contact*)>;
+
+protected:
     using function_type = std::variant<f0_t, f1_t>;
 
 public:
@@ -70,22 +80,64 @@ public:
         : ListColumn{name, description, offsets}, f_{std::move(f)} {}
     ~Callback() override = default;
 
-    value_type getValue(
+    [[nodiscard]] value_type getValue(
         Row row, const contact* auth_user,
-        std::chrono::seconds /*timezone_offset*/) const override {
+        std::chrono::seconds timezone_offset) const override {
+        auto entries = getEntries(row, auth_user, timezone_offset);
+        std::vector<std::string> values;
+        std::transform(entries.begin(), entries.end(),
+                       std::back_inserter(values),
+                       detail::column::serialize<U>);
+        return values;
+    }
+
+    [[nodiscard]] std::vector<U> getEntries(
+        Row row, const contact* auth_user,
+        std::chrono::seconds /*timezone_offset*/) const {
         const T* data = columnData<T>(row);
+        if (data == nullptr) {
+            return Default;
+        }
         if (std::holds_alternative<f0_t>(f_)) {
-            return data == nullptr ? Default : std::get<f0_t>(f_)(*data);
+            return std::get<f0_t>(f_)(*data);
         }
         if (std::holds_alternative<f1_t>(f_)) {
-            return data == nullptr ? Default
-                                   : std::get<f1_t>(f_)(*data, auth_user);
+            return std::get<f1_t>(f_)(*data, auth_user);
+        }
+        throw std::runtime_error("unreachable");
+    }
+
+    [[nodiscard]] std::vector<U> getEntries(Row row,
+                                            const contact* auth_user) const {
+        const T* data = columnData<T>(row);
+        if (data == nullptr) {
+            return Default;
+        }
+        if (std::holds_alternative<f0_t>(f_)) {
+            return std::get<f0_t>(f_)(*data);
+        }
+        if (std::holds_alternative<f1_t>(f_)) {
+            return std::get<f1_t>(f_)(*data, auth_user);
+        }
+        throw std::runtime_error("unreachable");
+    }
+
+    [[nodiscard]] std::vector<U> getEntries(Row row) const {
+        const T* data = columnData<T>(row);
+        if (data == nullptr) {
+            return Default;
+        }
+        if (std::holds_alternative<f0_t>(f_)) {
+            return std::get<f0_t>(f_)(*data);
+        }
+        if (std::holds_alternative<f1_t>(f_)) {
+            throw std::runtime_error("wrong lambda/overload");
         }
         throw std::runtime_error("unreachable");
     }
 
 private:
-    const value_type Default{};
+    const std::vector<U> Default{};
     function_type f_;
 };
 
--- livestatus/src/test/test_ListColumn.cc
+++ livestatus/src/test/test_ListColumn.cc
@@ -3,6 +3,7 @@
 // terms and conditions defined in the file COPYING, which is part of this
 // source code package.
 
+#include <algorithm>
 #include <chrono>
 #include <string>
 #include <vector>

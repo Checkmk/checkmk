cff1162441520d7d872b0d0adafe4b77d65a13e2
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-06-27 21:42:07 +0200

    inotify: improve section type
    Change-Id: I7d6139c69305059b73ef706d4bacf688e96ea38c

--- tests/unit/checks/test_inotify.py
+++ tests/unit/checks/test_inotify.py
@@ -5,6 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from typing import Dict
+from collections import Counter
 
 import pytest  # type: ignore[import]
 
@@ -25,87 +26,53 @@ INFO = [
     ["1465470058", "delete", "/tmp/noti/test"],
 ]
 
-PARSED = {
-    'configured': {
-        'file': {'/tmp/noti/nodata', '/tmp/noti/test'},
-        'folder': {'/tmp/noti'}
+PARSED = (
+    # warnings
+    Counter({'I assume a warning looks like this!': 1}),
+    # configured:
+    {
+        '/tmp/noti/nodata': 'file',
+        '/tmp/noti/test': 'file',
+        '/tmp/noti': 'folder',
     },
-    'meta': {
-        'warnings': {
-            'I assume a warning looks like this!': 1
-        }
+    # stats
+    {
+        '/tmp/noti': {
+            'modify': 1465470056,
+            'open': 1465470056,
+            'delete': 1465470058,
+        },
+        '/tmp/noti/test': {
+            'modify': 1465470056,
+            'open': 1465470056,
+            'delete': 1465470058
+        },
     },
-    'stats': {
-        '/tmp/noti': [{
-            'mode': 'modify',
-            'mtime': '1465470055',
-            'size': '5',
-            'timestamp': 1465470055
-        }, {
-            'mode': 'open',
-            'mtime': '1465470055',
-            'size': '5',
-            'timestamp': 1465470055
-        }, {
-            'mode': 'modify',
-            'mtime': '1465470055',
-            'size': '5',
-            'timestamp': 1465470055
-        }, {
-            'mode': 'modify',
-            'mtime': '1465470056',
-            'size': '5',
-            'timestamp': 1465470056
-        }, {
-            'mode': 'open',
-            'mtime': '1465470056',
-            'size': '5',
-            'timestamp': 1465470056
-        }, {
-            'mode': 'delete',
-            'timestamp': 1465470058
-        }],
-        '/tmp/noti/test': [{
-            'mode': 'modify',
-            'mtime': '1465470055',
-            'size': '5',
-            'timestamp': 1465470055
-        }, {
-            'mode': 'open',
-            'mtime': '1465470055',
-            'size': '5',
-            'timestamp': 1465470055
-        }, {
-            'mode': 'modify',
-            'mtime': '1465470055',
-            'size': '5',
-            'timestamp': 1465470055
-        }, {
-            'mode': 'modify',
-            'mtime': '1465470056',
-            'size': '5',
-            'timestamp': 1465470056
-        }, {
-            'mode': 'open',
-            'mtime': '1465470056',
-            'size': '5',
-            'timestamp': 1465470056
-        }, {
-            'mode': 'delete',
-            'timestamp': 1465470058
-        }]
-    },
-}
+)
+
+
+def Section(*a, **kw):
+    return Check("inotify").context["Section"](*a, **kw)
+
+
+def parse_inotify(*a, **kw):
+    return Check("inotify").context["parse_inotify"](*a, **kw)
+
+
+def discover_inotify(*a, **kw):
+    return Check("inotify").context["discover_inotify"](*a, **kw)
+
+
+def check_inotify(*a, **kw):
+    return Check("inotify").context["_check_inotify"](*a, **kw)
 
 
 def test_inotify_parse():
-    parse_inotify = Check("inotify").context["parse_inotify"]
-    assert PARSED == parse_inotify(INFO)
+    assert Section(*PARSED) == parse_inotify(INFO)
 
 
 def test_discovery():
-    discover_inotify = Check("inotify").context["inventory_inotify"]
-    assert sorted(discover_inotify(PARSED)) == [
+    assert sorted(discover_inotify(Section(*PARSED))) == [
         ('File /tmp/noti/nodata', {}),
         ('File /tmp/noti/test', {}),
         ('Folder /tmp/noti', {}),
@@ -113,7 +80,6 @@ def test_discovery():
 
 
 def test_updated_data():
-    check_inotify = Check("inotify").context["_check_inotify"]
     item = "Folder /tmp/noti"
     params = {
         'age_last_operation': [
@@ -122,63 +88,47 @@ def test_updated_data():
             ('just_for_test_coverage', 1, 2),
         ]
     }
+    section = Section(*PARSED)
     last_status: Dict = {}
     now = 1465470156
 
-    assert list(check_inotify(item, params, PARSED, last_status, now)) == [
+    assert list(check_inotify(item, params, section, last_status, now)) == [
         (0, 'Last Delete: 98 s ago'),
         (1, 'Last Modify: 100 s ago (> 90 s)'),
         (2, 'Last Open: 100 s ago (> 90 s)'),
         (3, 'Last Just_For_Test_Coverage unknown'),
         (1, 'Incomplete data!'),
-        (1, '1 Warnings: I assume a warning looks like this!'),
+        (1, '1 warning(s): I assume a warning looks like this!'),
     ]
     assert last_status == {
-        'delete': {
-            'mode': 'delete',
-            'timestamp': 1465470058
-        },
-        'modify': {
-            'mode': 'modify',
-            'mtime': '1465470056',
-            'size': '5',
-            'timestamp': 1465470056
-        },
-        'open': {
-            'mode': 'open',
-            'mtime': '1465470056',
-            'size': '5',
-            'timestamp': 1465470056
-        },
+        'delete': 1465470058,
+        'modify': 1465470056,
+        'open': 1465470056,
     }
 
 
 def test_nodata():
-    check_inotify = Check("inotify").context["_check_inotify"]
     item = "File /tmp/noti/nodata"
     params = {'age_last_operation': [('modify', 90, 110)]}
+    section = Section(Counter(), *PARSED[1:])
     last_status: Dict = {}
     now = 1465470156
 
-    assert list(check_inotify(item, params, PARSED, last_status, now)) == [
+    assert list(check_inotify(item, params, section, last_status, now)) == [
         (3, 'Last Modify unknown'),
-        (1, 'Incomplete data!'),
-        (1, '1 Warnings: I assume a warning looks like this!'),
         (0, 'No data available yet'),
     ]
     assert not last_status
 
 
 def test_old_status():
-    check_inotify = Check("inotify").context["_check_inotify"]
     item = "File /tmp/noti/nodata"
     params = {'age_last_operation': [('modify', 90, 110)]}
-    last_status = {'modify': {"timestamp": 1465470000}}
+    section = Section(Counter(), *PARSED[1:])
+    last_status = {'modify': 1465470000}
     now = 1465470156
 
-    assert list(check_inotify(item, params, PARSED, last_status, now)) == [
+    assert list(check_inotify(item, params, section, last_status, now)) == [
         (2, 'Last Modify: 156 s ago (> 110 s)'),
-        (1, 'Incomplete data!'),
-        (1, '1 Warnings: I assume a warning looks like this!'),
     ]
-    assert last_status == {'modify': {"timestamp": 1465470000}}
+    assert last_status == {'modify': 1465470000}

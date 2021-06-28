f18992a5e7593aea06b6b1976f7542ad1a9584da
Ronny Bruska <ronny.bruska@tribe29.com>
2021-06-28 15:46:54 +0200

    Revert "Revert "12963 FIX Fix BI aggregation work tree""
    This reverts commit 1f069a919f23be955c035a4694efbcf924b3378f.
    
    Change-Id: I550a7b10cd0f70ac6e16b9abea6b3484f3dc013b

--- cmk/gui/plugins/sidebar/bi.py
+++ cmk/gui/plugins/sidebar/bi.py
@@ -88,14 +88,15 @@ class SidebarSnapinAggregationGroupTree(SidebarSnapin):
 
     def _render_tree(self, tree):
         for group, attrs in tree.iteritems():
+            aggr_group_tree = "/".join(attrs["__path__"])
             fetch_url = html.makeuri_contextless([
                 ("view_name", "aggr_all"),
-                ("aggr_group_tree", "/".join(attrs["__path__"])),
+                ("aggr_group_tree", aggr_group_tree),
             ], "view.py")
 
             if attrs.get('__children__'):
                 html.begin_foldable_container(
-                    "bi_aggregation_group_trees", group, False,
+                    "bi_aggregation_group_trees", aggr_group_tree, False,
                     HTML(html.render_a(
                         group,
                         href=fetch_url,

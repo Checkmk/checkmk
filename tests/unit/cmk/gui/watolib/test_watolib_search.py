010508c230f0021754013bb65a0d387324ce80f0
Joerg Herbel <joerg.herbel@tribe29.com>
2021-06-30 16:57:29 +0200

    Small structural improvements in tests for setup search index
    Typing, sorting of imports etc.
    
    Change-Id: Ic554c06c6b58aada837d128ae4f32351ad737226

--- tests/unit/cmk/gui/watolib/test_watolib_search.py
+++ tests/unit/cmk/gui/watolib/test_watolib_search.py
@@ -5,27 +5,27 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
 from cmk.gui.type_defs import SearchResult
-from cmk.gui.watolib import (
-    hosts_and_folders,
-    search,
-)
+from cmk.gui.watolib import hosts_and_folders, search
 from cmk.gui.watolib.hosts_and_folders import Folder
 from cmk.gui.watolib.search import (
     ABCMatchItemGenerator,
     IndexBuilder,
     IndexNotFoundException,
     IndexSearcher,
+)
+from cmk.gui.watolib.search import (
     MatchItem,
-    MatchItems,
     MatchItemGeneratorRegistry,
+    MatchItems,
     PermissionsHandler,
     URLChecker,
 )
 
 
-def test_match_item():
+def test_match_item() -> None:
     assert MatchItem(
         "1",
         "2",
@@ -83,7 +83,7 @@ class MatchItemGeneratorChangeDep(ABCMatchItemGenerator):
 
 
 @pytest.fixture(name="get_languages", scope="function", autouse=True)
-def fixture_get_languages(monkeypatch):
+def fixture_get_languages(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setattr(
         search,
         "get_languages",
@@ -92,7 +92,7 @@ def fixture_get_languages(monkeypatch):
 
 
 @pytest.fixture(name="match_item_generator_registry")
-def fixture_match_item_generator_registry():
+def fixture_match_item_generator_registry() -> MatchItemGeneratorRegistry:
     match_item_generator_registry = MatchItemGeneratorRegistry()
     match_item_generator_registry.register(MatchItemGeneratorLocDep("localization_dependent"))
     match_item_generator_registry.register(MatchItemGeneratorChangeDep("change_dependent"))
@@ -100,60 +100,71 @@ def fixture_match_item_generator_registry():
 
 
 @pytest.fixture(name="index_builder")
-def fixture_index_builder(match_item_generator_registry):
+def fixture_index_builder(
+    match_item_generator_registry: MatchItemGeneratorRegistry,) -> IndexBuilder:
     return IndexBuilder(match_item_generator_registry)
 
 
 @pytest.fixture(name="index_searcher")
-def fixture_index_searcher(index_builder):
+def fixture_index_searcher(index_builder: IndexBuilder) -> IndexSearcher:
     index_searcher = IndexSearcher()
     index_searcher._redis_client = index_builder._redis_client
     return index_searcher
 
 
 class TestIndexBuilder:
-    def test_update_only_not_built(self, with_admin_login, index_builder):
+    @pytest.mark.usefixtures("with_admin_login")
+    def test_update_only_not_built(
+        self,
+        index_builder: IndexBuilder,
+    ) -> None:
         index_builder.build_changed_sub_indices("something")
         assert not index_builder.index_is_built(index_builder._redis_client)
 
 
 class TestIndexBuilderAndSearcher:
-    def test_full_build_and_search(self, with_admin_login, index_builder, index_searcher):
+    @pytest.mark.usefixtures("with_admin_login")
+    def test_full_build_and_search(
+        self,
+        index_builder: IndexBuilder,
+        index_searcher: IndexSearcher,
+    ) -> None:
         index_builder.build_full_index()
         assert list(index_searcher.search("**")) == [
             ("Change-dependent", [SearchResult(title="change_dependent", url="")]),
             ("Localization-dependent", [SearchResult(title="localization_dependent", url="")]),
         ]
 
+    @pytest.mark.usefixtures("with_admin_login")
     def test_update_and_search_no_update(
         self,
-        with_admin_login,
-        index_builder,
-        index_searcher,
-    ):
+        index_builder: IndexBuilder,
+        index_searcher: IndexSearcher,
+    ) -> None:
         index_builder._mark_index_as_built()
         index_builder.build_changed_sub_indices("something")
         assert not list(index_searcher.search("**"))
 
+    @pytest.mark.usefixtures("with_admin_login")
     def test_update_and_search_with_update(
         self,
-        with_admin_login,
-        index_builder,
-        index_searcher,
-    ):
+        index_builder: IndexBuilder,
+        index_searcher: IndexSearcher,
+    ) -> None:
         index_builder._mark_index_as_built()
         index_builder.build_changed_sub_indices("some_change_dependent_whatever")
         assert list(index_searcher.search("**")) == [
             ("Change-dependent", [SearchResult(title="change_dependent", url="")]),
         ]
 
+    @pytest.mark.usefixtures("with_admin_login")
     def test_update_with_empty_and_search(
         self,
-        with_admin_login,
-        match_item_generator_registry,
-        index_builder,
-        index_searcher,
-    ):
+        monkeypatch: MonkeyPatch,
+        match_item_generator_registry: MatchItemGeneratorRegistry,
+        index_builder: IndexBuilder,
+        index_searcher: IndexSearcher,
+    ) -> None:
         """
         Test if things can also be deleted from the index during an update
         """
@@ -161,8 +172,13 @@ class TestIndexBuilderAndSearcher:
             yield from ()
 
         index_builder.build_full_index()
-        match_item_generator_registry[
-            "change_dependent"].generate_match_items = empty_match_item_gen
+
+        monkeypatch.setattr(
+            match_item_generator_registry["change_dependent"],
+            "generate_match_items",
+            empty_match_item_gen,
+        )
+
         index_builder.build_changed_sub_indices("some_change_dependent_whatever")
         assert list(index_searcher.search("**")) == [
             ("Localization-dependent", [SearchResult(title="localization_dependent", url="")]),
@@ -170,28 +186,33 @@ class TestIndexBuilderAndSearcher:
 
 
 @pytest.fixture(name="created_host_url")
-def fixture_created_host_url(with_admin_login):
+def fixture_created_host_url(with_admin_login) -> str:
     folder = Folder.root_folder()
     folder.create_hosts([("host", {}, [])])
     return "wato.py?folder=&host=host&mode=edit_host"
 
 
 class TestURLChecker:
-    def test_is_permitted_false(self, module_wide_request_context):
+    @pytest.mark.usefixtures("module_wide_request_context")
+    def test_is_permitted_false(self) -> None:
         assert not URLChecker().is_permitted("wato.py?folder=&mode=service_groups")
 
-    def test_is_permitted_true(self, with_admin_login):
+    @pytest.mark.usefixtures("with_admin_login")
+    def test_is_permitted_true(self) -> None:
         assert URLChecker().is_permitted("wato.py?folder=&mode=service_groups")
 
-    def test_is_permitted_host_true(self, created_host_url):
+    def test_is_permitted_host_true(
+        self,
+        created_host_url: str,
+    ) -> None:
         assert URLChecker().is_permitted(created_host_url)
 
     def test_is_permitted_host_false(
         self,
-        monkeypatch,
+        monkeypatch: MonkeyPatch,
         module_wide_request_context,
-        created_host_url,
-    ):
+        created_host_url: str,
+    ) -> None:
         monkeypatch.setattr(
             hosts_and_folders.config.user,
             "may",
@@ -201,18 +222,20 @@ class TestURLChecker:
 
 
 class TestPermissionHandler:
-    def test_may_see_category(self, with_admin_login):
+    @pytest.mark.usefixtures("with_admin_login")
+    def test_may_see_category(self) -> None:
         permissions_handler = PermissionsHandler()
         for category in permissions_handler._category_permissions:
             assert permissions_handler.may_see_category(category)
 
 
 class TestIndexSearcher:
-    def test_search_no_index(self, with_admin_login):
+    @pytest.mark.usefixtures("with_admin_login")
+    def test_search_no_index(self) -> None:
         with pytest.raises(IndexNotFoundException):
             list(IndexSearcher().search("change_dep"))
 
-    def test_sort_search_results(self):
+    def test_sort_search_results(self) -> None:
         assert list(
             IndexSearcher._sort_search_results({
                 "Hosts": [SearchResult(title="host", url="")],

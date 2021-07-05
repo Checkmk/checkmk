07f4fcd51fb5673d3a89bbd8eac8dccdd4f5b9b2
Mathias Laurin <mathias.laurin@tribe29.com>
2021-07-05 11:19:45 +0200

    Shell linting: replace "grep|awk" with "awk"
    Simpler and more readable as it is less redundant.
    
    Change-Id: I6043db204e1fdb8908a04d6ecd474b0810cc2fb8

--- buildscripts/docker_image_aliases/resolve.sh
+++ buildscripts/docker_image_aliases/resolve.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd $SCRIPT_DIR
 
-docker build $1 | grep "Successfully built" | awk '{print $3}'
+docker build $1 | awk '/Successfully built/{print $3}'
 
 # echo "$(grep $1 $SCRIPT_DIR/docker_image_aliases.txt | awk '{print $2}')"

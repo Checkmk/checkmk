a96477977d5323e34aa5709bbd57357c2304698d
Christoph Rauch <christoph.rauch@tribe29.com>
2021-07-05 09:34:31 +0200

    12949 add folder to REST API host endpoints
    The path of the folder in which a host resides in is now returned
    in the "extensions" section of the host endpoints.
    
    Change-Id: I77d2417da28c57b862601382791b8d44711268e1

--- cmk/gui/plugins/openapi/restful_objects/response_schemas.py
+++ cmk/gui/plugins/openapi/restful_objects/response_schemas.py
@@ -290,6 +290,7 @@ class DomainObject(Linkable):
 
 
 class HostExtensions(BaseSchema):
+    folder = fields.FolderField(description="The folder, in which this host resides.",)
     attributes = fields.attributes_field(
         "host",
         "update",

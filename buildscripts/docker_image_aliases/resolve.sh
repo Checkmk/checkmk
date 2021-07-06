76ae9199a10e7fca11b547b22e052c267e88d482
Frans Fürst <frans.fuerst@tribe29.com>
2021-07-06 16:20:49 +0200

    docker image aliases: make resolve more verbose about errors
    Change-Id: I03a62d74adf9f287c0d2292596120387541e131d

--- buildscripts/docker_image_aliases/resolve.sh
+++ buildscripts/docker_image_aliases/resolve.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+
+# Return SHA of image referenced by $1
+# echo "$(grep $1 $SCRIPT_DIR/docker_image_aliases.txt | awk '{print $2}')"
+#
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-cd $SCRIPT_DIR
+DOCKER_RESULT=$(docker build "${SCRIPT_DIR}/$1" 2>docker-image-alias-resolve-error.txt)
+RESULT_IMAGE_ID=$(awk '/Successfully built/{print $3}' <<< "${DOCKER_RESULT}")
 
-docker build $1 | awk '/Successfully built/{print $3}'
+if [ -n "$RESULT_IMAGE_ID" ] ; then
+    echo $RESULT_IMAGE_ID
+else
+    echo "Could not resolve $1, error was:" 1>&2
+    cat docker-image-alias-resolve-error.txt 1>&2
+    echo "Make sure you're correctly logged into the registry and the image exists" 1>&2
+    echo "INVALID_IMAGE_ID"
+fi
 
-# echo "$(grep $1 $SCRIPT_DIR/docker_image_aliases.txt | awk '{print $2}')"

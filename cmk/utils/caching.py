4d3e3e4e22ea90329438fefb35623ace80181844
Andreas Boesl <andreas.boesl@tribe29.com>
2021-07-07 12:40:50 +0200

    Moved cmk.gui.utils.caching -> cmk.utils.caching
    Change-Id: I29679dab74a73307443e03984b3ad1414166e684

--- cmk/utils/caching.py
+++ cmk/utils/caching.py
@@ -7,8 +7,25 @@
 
 import collections
 from typing import Dict
+from functools import wraps, lru_cache
 
 import cmk.utils.misc
+
+
+# Used as decorator wrapper for functools.lru_cache in order to bind the cache to an instance method
+# rather than the class method.
+def instance_method_lru_cache(*cache_args, **cache_kwargs):
+    def cache_decorator(func):
+        @wraps(func)
+        def cache_factory(self, *args, **kwargs):
+            instance_cache = lru_cache(*cache_args, **cache_kwargs)(func)
+            instance_cache = instance_cache.__get__(self, self.__class__)
+            setattr(self, func.__name__, instance_cache)
+            return instance_cache(*args, **kwargs)
+
+        return cache_factory
+
+    return cache_decorator
 
 
 class CacheManager:

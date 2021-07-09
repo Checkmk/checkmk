f1004e7aa5b8a0ddfc6338aea8e79e9c0ada38d6
Alex Zurhake <alex.zurhake@tribe29.com>
2021-07-09 10:59:04 +0200

    upload to tstbuilds
    All packages are uploaded to interim teststorage.
    If the build should be published (parameter: DEPLOY_TO_WEBSITE == true):
        - download packages
        - upload and deploy to website
    
    WEB_DEPLOY and INTERNAL_DEPLOY variables are global Jenkins variables.
    
    Change-Id: I0bb587b8d8548cb701c17d196e9c6f04e2926205

--- buildscripts/scripts/lib/upload_artifacts.groovy
+++ buildscripts/scripts/lib/upload_artifacts.groovy
@@ -7,17 +7,24 @@ def upload(Map args) {
     // FILE_PATH: Path where the File is stored
     // FILE_NAME: Name of the File to be uploaded
     // CMK_VERS: Version that should be uploaded
-    // UPLOAD_DEST: Where shoult the packages be uploaded to
-    // SHALL_PUBLISH: bool whether or not the package should be uploaded to the website
+    // UPLOAD_DEST: Where should the packages be uploaded to
+    // PORT: Port fo upload dest
     stage(args.NAME + ' upload package') {
         def FILE_BASE = get_file_base(args.FILE_PATH)
         def ARCHIVE_BASE = get_archive_base(FILE_BASE) 
         
-        if (args.SHALL_PUBLISH) {
-            via_rsync(ARCHIVE_BASE, args.CMK_VERS, args.FILE_NAME, args.UPLOAD_DEST)
-        } else {
-            via_archive(FILE_BASE)
-        }
+        via_rsync(ARCHIVE_BASE, args.CMK_VERS, args.FILE_NAME, args.UPLOAD_DEST, args.PORT)
+    }
+}
+
+def download(DOWNLOAD_DEST, PORT, CMK_VERSION) {
+    withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
+        sh """
+            rsync -av --relative \
+                -e "ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p ${PORT}" \
+                ${DOWNLOAD_DEST}/./${CMK_VERSION}/* \
+                .
+        """
     }
 }
 
@@ -29,27 +36,23 @@ def get_archive_base(FILE_BASE) {
     return sh(script: "dirname ${FILE_BASE}", returnStdout: true).toString().trim()
 }
 
-def via_rsync(ARCHIVE_BASE, CMK_VERS, FILE_NAME, UPLOAD_DEST) {
+def get_file_name(FILE_PATH) { 
+    return sh(script: "basename ${FILE_PATH}", returnStdout: true).toString().trim()
+}
+
+def via_rsync(ARCHIVE_BASE, CMK_VERS, FILE_NAME, UPLOAD_DEST, PORT) {
     withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
         sh """
             rsync -av --relative \
                 --exclude '*dbgsym*.deb' \
-                -e "ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p 52022" \
+                -e "ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p ${PORT}" \
                 ${ARCHIVE_BASE}/./${CMK_VERS}/${FILE_NAME} \
                 ${UPLOAD_DEST}
         """
     }
 }
 
-def via_archive(FILE_BASE) {
-    dir(FILE_BASE) {
-        // Multiple subsequent calls overwrite the previous artifacts. For this reason
-        // we always archive all available files
-        archiveArtifacts("*")
-    }
-}
-
-def create_and_upload_hashes(ARCHIVE_DIR, scm, SHALL_PUBLISH, UPLOAD_DEST, CMK_VERS) {
+def create_and_upload_hashes(ARCHIVE_DIR, scm, UPLOAD_DEST, PORT, CMK_VERS) {
     stage("Create and upload file hashes") {
         def HASHES_PATH = ARCHIVE_DIR + "/HASHES"
         sh("cd ${ARCHIVE_DIR} ; sha256sum -- *.{tar.gz,rpm,deb,cma,cmk} | sort -k 2 > ${HASHES_PATH}")
@@ -59,16 +62,16 @@ def create_and_upload_hashes(ARCHIVE_DIR, scm, SHALL_PUBLISH, UPLOAD_DEST, CMK_V
             FILE_NAME: "HASHES",
             CMK_VERS: CMK_VERS,
             UPLOAD_DEST: UPLOAD_DEST,
-            SHALL_PUBLISH: SHALL_PUBLISH,
+            PORT: PORT,
         )
     }
 }
 
-def deploy_to_website(UPLOAD_URL, CMK_VERS) {
+def deploy_to_website(UPLOAD_URL, PORT, CMK_VERS) {
     stage("Deploy to Website") {
         withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
             sh """
-                ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p 52022 ${UPLOAD_URL} \
+                ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p ${PORT} ${UPLOAD_URL} \
                     ln -sf /var/downloads/checkmk/${CMK_VERS} /smb-share-customer/checkmk
             """
         }

fc8e7563db8a0810f4fb5c5ce31d76392902ffe2
Sofia Colakovic <sofia.colakovic@tribe29.com>
2021-07-12 18:40:23 +0200

    tests for fileinfo and sap_hana_fileinfo
    CMK-8239
    
    Change-Id: Idc75231939dd97808409198916cc4a357a061ba5

--- tests/unit/checks/test_sap_hana_fileinfo.py
+++ tests/unit/checks/test_sap_hana_fileinfo.py
@@ -5,9 +5,49 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import pytest
+import collections
 
+from testlib import Check
+from checktestlib import MockHostExtraConf
 from cmk.utils.type_defs import CheckPluginName
 from cmk.base.item_state import MKCounterWrapped
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, State, Metric
+
+FileinfoItem = collections.namedtuple("FileinfoItem", "name missing failed size time")
+
+
+@pytest.mark.parametrize("item, parsed, expected_result", [
+    (
+        "file1234.txt",
+        {},
+        [Result(state=State.UNKNOWN, summary='Missing reference timestamp')],
+    ),
+    (
+        "C:\\Datentransfer\\ORU\\KC\\KC_41135.hl7",
+        {
+            "reftime": 1563288717,
+            "files": {
+                "C:\\Datentransfer\\ORU\\KC\\KC_41135.hl7": FileinfoItem(
+                    name="C:\\Datentransfer\\ORU\\KC\\KC_41135.hl7",
+                    missing=False,
+                    failed=False,
+                    size=2414,
+                    time=1415625918),
+            }
+        },
+        [
+            Result(state=State.OK, summary='Size: 2414 B'),
+            Metric('size', 2414.0),
+            Result(state=State.OK, summary='Age: 4.7 y'),
+            Metric('age', 147662799.0)
+        ],
+    ),
+])
+def test_sap_hana_fileinfo(fix_register, item, parsed, expected_result):
+    plugin = fix_register.check_plugins[CheckPluginName("sap_hana_fileinfo")]
+    result = list(plugin.check_function(item=item, params={}, section=parsed))
+
+    assert result == expected_result
 
 
 @pytest.mark.parametrize("item, parsed", [
@@ -19,7 +59,7 @@ from cmk.base.item_state import MKCounterWrapped
         },
     ),
 ])
-def test_sap_hana_fileinfo(fix_register, item, parsed):
+def test_sap_hana_fileinfo_stale(fix_register, item, parsed):
     plugin = fix_register.check_plugins[CheckPluginName("sap_hana_fileinfo")]
     with pytest.raises(MKCounterWrapped) as e:
         list(plugin.check_function(item=item, params={}, section=parsed))
@@ -27,6 +67,38 @@ def test_sap_hana_fileinfo(fix_register, item, parsed):
     assert e.value.args[0] == "Login into database failed."
 
 
+@pytest.mark.parametrize("item, parsed, params, expected_result", [
+    (
+        "file1234.txt",
+        {},
+        {},
+        [Result(state=State.UNKNOWN, summary='Missing reference timestamp')],
+    ),
+    ("C:\\Datentransfer\\ORU\\KC\\KC_41135.hl7", {
+        "reftime": 1563288717,
+        "files": {
+            "C:\\Datentransfer\\ORU\\KC\\KC_41135.hl7": FileinfoItem(
+                name="C:\\Datentransfer\\ORU\\KC\\KC_41135.hl7",
+                missing=False,
+                failed=False,
+                size=2414,
+                time=1415625918),
+        }
+    }, {}, [Result(state=State.UNKNOWN, summary='No group pattern found.')]),
+])
+def test_sap_hana_fileinfo_groups(fix_register, item, parsed, params, expected_result):
+    fileinfo_groups_check = Check('fileinfo.groups')
+    plugin = fix_register.check_plugins[CheckPluginName("sap_hana_fileinfo_groups")]
+
+    def mock_host_extra_conf(_hostname, _rulesets):
+        return []
+
+    with MockHostExtraConf(fileinfo_groups_check, mock_host_extra_conf, 'host_extra_conf'):
+        result = list(plugin.check_function(item=item, params=params, section=parsed))
+
+    assert result == expected_result
+
+
 @pytest.mark.parametrize("item, parsed", [
     (
         "file1234.txt",
@@ -36,7 +108,7 @@ def test_sap_hana_fileinfo(fix_register, item, parsed):
         },
     ),
 ])
-def test_sap_hana_fileinfo_groups(fix_register, item, parsed):
+def test_sap_hana_fileinfo_groups_stale(fix_register, item, parsed):
     plugin = fix_register.check_plugins[CheckPluginName("sap_hana_fileinfo_groups")]
     with pytest.raises(MKCounterWrapped) as e:
         list(plugin.check_function(item=item, params={}, section=parsed))

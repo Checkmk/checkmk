b23e1322930e36d532c7a4d339221681debad59b
Stefan Hölzl <stefan.hoelzl@tribe29.com>
2021-07-13 13:59:52 +0200

    13052 iis_app_pool_state: New check to monitor IIS Application Pool States
    New check to monitor each Application Pool State for an IIS.
    Discovers a service for each Application Pool.
    
    SUP-6262
    
    Change-Id: I34392e91bb14dc6351477eea1aac9eb75334b948

---
+++ tests/unit/cmk/base/plugins/agent_based/test_iis_app_pool_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.base.plugins.agent_based.iis_app_pool_state import check_iis_app_pool_state, IisAppPoolState, DefaultCheckParameters
+from cmk.base.plugins.agent_based.agent_based_api.v1 import State, Result
+
+
+@pytest.mark.parametrize("item,section,params,results", [
+    ("app", dict(app=IisAppPoolState.Initialized), DefaultCheckParameters, [
+        Result(state=State.WARN, summary='State: Initialized'),
+    ]),
+    ("app", dict(app=IisAppPoolState.ShutdownPending), DefaultCheckParameters, [
+        Result(state=State.CRIT, summary='State: ShutdownPending'),
+    ]),
+    ("app", dict(), DefaultCheckParameters, [
+        Result(state=State.UNKNOWN, summary='app is unknown'),
+    ]),
+    ("app", dict(app=IisAppPoolState.Running), dict(state_mapping={"Running": State.CRIT.value}), [
+        Result(state=State.CRIT, summary='State: Running'),
+    ]),
+    ("app", dict(app=IisAppPoolState.Running), dict(state_mapping=dict()), [
+        Result(state=State.CRIT, summary='State: Running'),
+    ]),
+    ("app", dict(app=IisAppPoolState.Running), dict(), [
+        Result(state=State.CRIT, summary='State: Running'),
+    ]),
+])
+def test_check_iis_app_pool_state(item, section, params, results):
+    assert list(check_iis_app_pool_state(item, params, section)) == results

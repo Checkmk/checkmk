520dc81c6dd23ddbc17bb78556fd0c11e638c3f4
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-07-22 17:10:56 +0200

    SUO-6777 - improve code signing for Windows Agent
    - support external parameter with file name and password
    - sign of resulting msi file
    - add support for timestamp
    - use SHA256 instead of SHA1
    
    Change-Id: I969bff525273475868af7876db6a5cea4caac549

--- agents/wnx/build_release.cmd
+++ agents/wnx/build_release.cmd
@@ -1,25 +1,35 @@
 @echo off
-rem File to Build MSI usingMS BUild system
-rem Problem based on the fact that we have one x86 Playfrom for MSI
-rem but two, x86 and x64, for exe and MSI must have both targets
-rem build before owm build start. 
-rem this is for command line only
-rem In GUI we should do Batch Rebuild of everything
-rem variables to set OPTOIONALLY, when you are using the same git checkout multiple times
-rem arte - final artefacts, expected bz build script
-rem WNX_BUILD - in the future this is name of subfloder to build out
-rem creates # artefacts in the output folder
+:: File to Build MSI usingMS BUild system
+:: Problem based on the fact that we have one x86 Playfrom for MSI
+:: but two, x86 and x64, for exe and MSI must have both targets
+:: build before owm build start. 
+:: this is for command line only
+:: In GUI we should do Batch Rebuild of everything
+:: variables to set OPTOIONALLY, when you are using the same git checkout multiple times
+:: arte - final artefacts, expected bz build script
+:: WNX_BUILD - in the future this is name of subfloder to build out
+:: creates # artefacts in the output folder
+
+:: 
+:: Sign mode:
+:: build_release file password
+:: file is always in c:\common\store should be well protected from access
+::
+:: Standard Mode:
+:: build_release
+::
+
 SETLOCAL EnableDelayedExpansion
 
-rem CHECK FOR CHOCO
-rem if choco is absent then build is not possible(we can't dynamically control environment)
+:: CHECK FOR CHOCO
+:: if choco is absent then build is not possible(we can't dynamically control environment)
 powershell Write-Host "Looking for choco..." -Foreground White
 @choco -v > nul
 @if "%errorlevel%" NEQ "0" powershell Write-Host "choco must be installed!" -Foreground Red && exit /b 55
 powershell Write-Host "[+] choco" -Foreground Green
 
-rem CHECK FOR make
-rem if make is absent then we try to install it using choco. Failure meand build fail, make is mandatory
+:: CHECK FOR make
+:: if make is absent then we try to install it using choco. Failure meand build fail, make is mandatory
 powershell Write-Host "Looking for make..." -Foreground White
 for /f %%i in ('where make') do set make_exe=%%i
 if "!make_exe!" == "" (
@@ -30,17 +40,17 @@ if "!make_exe!" == "" powershell Write-Host "make not found, something is really
 )
 powershell Write-Host "[+] make" -Foreground Green
  
-rem read version from the C++ agent
+:: read version from the C++ agent
 set /p wnx_version_raw=<src\common\wnx_version.h
-rem parse version
+:: parse version
 set wnx_version=%wnx_version_raw:~30,40%
 
-rem check that version is minimally ok
+:: check that version is minimally ok
 set wnx_version_mark=%wnx_version_raw:~0,29%
 if not "%wnx_version_mark%" == "#define CMK_WIN_AGENT_VERSION" powershell Write-Host "wnx_version.h is invalid" -Foreground Red && exit /b 67
 powershell Write-Host "wnx_version.h is ok" -Foreground Green
 
-rem #define CMK_WIN_AGENT_VERSION "
+:: #define CMK_WIN_AGENT_VERSION "
 
 set cur_dir=%cd%
 set arte=%cur_dir%\..\..\artefacts
@@ -52,7 +62,7 @@ set ExternalCompilerOptions=/DDECREASE_COMPILE_TIME
 if "%1" == "SIMULATE_OK" powershell Write-Host "Successful Build" -Foreground Green && echo aaa > %arte%\check_mk_service.msi  && exit /b 0
 if "%1" == "SIMULATE_FAIL" powershell Write-Host "Failed Install build" -Foreground Red && del %arte%\check_mk_service.msi  && exit /b 8
 
-@rem CHECK for line ending
+:: CHECK for line ending
 @py -3 check_crlf.py 
 @if errorlevel 1 powershell Write-Host "Line Encoding Error`r`n`tPlease check how good repo was checked out" -Foreground Red && exit /b 113
 
@@ -69,24 +79,30 @@ powershell Write-Host "Building MSI..." -Foreground White
 powershell -ExecutionPolicy ByPass -File msb.ps1
 if not %errorlevel% == 0 powershell Write-Host "Failed Build" -Foreground Red && exit /b 7
 
+if not "%2" == "" (
+powershell Write-Host "Signing Executables" -Foreground White
+@call sign_windows_exe c:\common\store\%1 %2 %build_dir%\check_mk_service\x64\Release\check_mk_service64.exe
+@call sign_windows_exe c:\common\store\%1 %2 %build_dir%\check_mk_service\Win32\Release\check_mk_service32.exe
+)
+
 %msbuild% wamain.sln /t:install /p:Configuration=Release,Platform=x86
 if not %errorlevel% == 0 powershell Write-Host "Failed Install build" -Foreground Red && exit /b 8
 
-@rem Patch Version Phase: Patch version value direct in the msi file
-rem set version:
-rem remove quotes
+:: Patch Version Phase: Patch version value direct in the msi file
+:: set version:
+:: remove quotes
 echo %wnx_version:~1,-1%
-rem info
+:: info
 powershell Write-Host "Setting Version in MSI: %wnx_version%" -Foreground Green
-@rem command
+:: command
 @echo cscript.exe //nologo WiRunSQL.vbs %arte%\check_mk_agent.msi "UPDATE `Property` SET `Property`.`Value`='%wnx_version:~1,-1%' WHERE `Property`.`Property`='ProductVersion'"
 cscript.exe //nologo WiRunSQL.vbs %build_dir%\install\Release\check_mk_service.msi "UPDATE `Property` SET `Property`.`Value`='%wnx_version:~1,-1%' WHERE `Property`.`Property`='ProductVersion'"
-@rem check result
+:: check result
 if not %errorlevel% == 0 powershell Write-Host "Failed version set" -Foreground Red && exit /b 34
 
 goto end
-@rem Unit Tests Phase: post processing/build special modules using make
-@rem this phase is skipped, there is no need to inculde unit tests in the build script
+:: Unit Tests Phase: post processing/build special modules using make
+:: this phase is skipped, there is no need to inculde unit tests in the build script
 powershell Write-Host "starting unit tests" -Foreground Cyan 
 
 pushd %arte%
@@ -104,7 +120,7 @@ exit 100
 
 :end
 
-@rem Deploy Phase: post processing/build special modules using make
+:: Deploy Phase: post processing/build special modules using make
 copy %build_dir%\watest\Win32\Release\watest32.exe %arte% /y	
 copy %build_dir%\watest\x64\Release\watest64.exe %arte% /Y	
 copy %build_dir%\install\Release\check_mk_service.msi %arte%\check_mk_agent.msi /y || powershell Write-Host "Failed to copy msi" -Foreground Red && exit /b 33
@@ -114,6 +130,12 @@ copy install\resources\check_mk.user.yml %arte%
 powershell Write-Host "File Deployment succeeded" -Foreground Green
 
 
-@rem Additional Phase: post processing/build special modules using make
+:: Additional Phase: post processing/build special modules using make
 !make_exe! msi_patch || powershell Write-Host "Failed to patch MSI exec" -Foreground Red && echo set && exit /b 36
+if not "%2" == "" (
+powershell Write-Host "Signing MSI" -Foreground White
+@call sign_windows_exe c:\common\store\%1 %2 %arte%\check_mk_agent.msi
+)
+
+
 

e30f30e39a88319eb9f6aac29c52f60854bb7801
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-07-22 18:02:11 +0200

    SUP-6777 - add password and file for code signing
    Change-Id: I1e937e4ce07f48232c4abd6fe4fef9e49155fbed

--- buildscripts/scripts/lib/windows.groovy
+++ buildscripts/scripts/lib/windows.groovy
@@ -19,7 +19,7 @@ def build(Map args) {
                     bat 'cd agents\\modules\\windows && call build_the_module.cmd cached ' + args.CREDS + ' ' + args.CACHE_URL
                     ARTIFACTS = 'python-3.8.cab,python-3.4.cab'
                 } else if (args.TARGET == "agent") {
-                    bat 'cd agents\\wnx && call build_release.cmd'
+                    bat 'cd agents\\wnx && call build_release.cmd tribe29.pfx password'
                     ARTIFACTS = "check_mk_agent-64.exe,check_mk_agent.exe,check_mk_agent.msi,check_mk.user.yml,check_mk.yml,watest32.exe,watest64.exe"
                 } else if (args.TARGET == "test_unit") {
                     bat 'cd agents\\wnx && call call_unit_tests.cmd -*_Long:*Integration:*Flaky'

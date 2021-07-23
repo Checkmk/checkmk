eb065877abc4db108493e2d7d38ddf370d8e9f76
Lars Michelsen <lm@tribe29.com>
2021-07-23 10:07:16 +0200

    Integrate protobuf (protoc) into build containers
    First step to integrate protobuf into our tooling. Next is to integrate
    protobuf it into our python and C++ worlds.
    
    The script install-protobuf-cpp.sh is mainly used for installing
    protobuf for our build container images. But the script may also be used
    for local installations e.g. on ubuntu using:
    
    ```
    sudo buildscripts/infrastructure/build-nodes/scripts/install-protobuf-cpp.sh --user
    ```
    
    This mode will install protobuf to `~/.local/`.
    
    Alternatively You may find it useful to use "apt-get install
    protobuf-compiler" instead.
    
    Change-Id: I062215d5c6477e8e295a3e8cd1bf1bc3cd60b2e6

---
+++ buildscripts/infrastructure/build-nodes/scripts/install-protobuf-cpp.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+set -e -o pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "${SCRIPT_DIR}/build_lib.sh"
+
+PROTOBUF_VERSION=3.17.3
+PACKAGE_NAME=protobuf-cpp
+DIR_NAME=protobuf-${PROTOBUF_VERSION}
+ARCHIVE_NAME=${PACKAGE_NAME}-${PROTOBUF_VERSION}.tar.gz
+
+# Increase this to enforce a recreation of the build cache
+BUILD_ID=1
+
+INSTALL_PREFIX=
+TARGET_DIR=/opt
+USE_BUILD_CACHE=1
+VERIFY_INSTALL=1
+
+# option parsing ###############################################################
+
+OPTIONS=$(getopt -o 'u' --long 'user' -n "$(basename $0)" -- "$@")
+if [[ $? -ne 0 ]]; then
+    echo "error parsing options"
+    exit 1
+fi
+eval set -- "$OPTIONS"
+unset OPTIONS
+
+while true; do
+    case "$1" in
+    '-u' | '--user')
+        INSTALL_PREFIX="${HOME}/.local"
+        TARGET_DIR=${INSTALL_PREFIX}${TARGET_DIR}
+        USE_BUILD_CACHE=
+        VERIFY_INSTALL=
+        shift
+        continue
+        ;;
+    '--')
+        shift
+        break
+        ;;
+    *)
+        echo "internal error"
+        exit 1
+        ;;
+    esac
+done
+
+
+# build/install ################################################################
+
+build_package() {
+    mkdir -p "$TARGET_DIR/src"
+    cd "$TARGET_DIR/src"
+
+    if [ -n "$USE_BUILD_CACHE" ]; then
+        mirrored_download "${ARCHIVE_NAME}" "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/${ARCHIVE_NAME}"
+    else
+        wget -O "${ARCHIVE_NAME}" "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/${ARCHIVE_NAME}"
+    fi
+
+    # Now build the package
+    tar xf "${ARCHIVE_NAME}"
+    cd "${DIR_NAME}"
+    ./configure --prefix="${TARGET_DIR}/${DIR_NAME}"
+    make -j6
+    make install
+
+    cd "$TARGET_DIR"
+    rm -rf "$TARGET_DIR/src"
+}
+
+install() {
+    mkdir -p "${INSTALL_PREFIX}/usr/bin"
+    ln -sf "${TARGET_DIR}/${DIR_NAME}/bin/"* "${INSTALL_PREFIX}/usr/bin/"
+
+    log "Register library"
+    mkdir -p "${INSTALL_PREFIX}/etc/ld.so.conf.d"
+    echo "${TARGET_DIR}/${DIR_NAME}/lib" >"${INSTALL_PREFIX}/etc/ld.so.conf.d/${PACKAGE_NAME}.conf"
+
+    if [ -z "$INSTALL_PREFIX" ]; then
+        ldconfig
+    fi
+
+    mkdir -p "${INSTALL_PREFIX}/usr/include"
+    cp -prl ${TARGET_DIR}/${DIR_NAME}/include/* "${INSTALL_PREFIX}/usr/include"
+
+    if [ -d "${INSTALL_PREFIX}/usr/lib64/pkgconfig" ]; then
+        PKGCONFIG_DIR=${INSTALL_PREFIX}/usr/lib64/pkgconfig
+    else
+        PKGCONFIG_DIR=${INSTALL_PREFIX}/usr/lib/pkgconfig
+    fi
+
+    mkdir -p "${PKGCONFIG_DIR}"
+    cp -prl ${TARGET_DIR}/${DIR_NAME}/lib/pkgconfig/*.pc "${PKGCONFIG_DIR}"
+}
+
+verify_install() {
+    if [ -z "$VERIFY_INSTALL" ]; then
+        return
+    fi
+
+    if ! type protoc >/dev/null 2>&1; then
+        echo "ERROR: protoc not in $PATH"
+        exit 1
+    fi
+
+    protoc --version
+    if [ "$(protoc --version)" != "libprotoc ${PROTOBUF_VERSION}" ]; then
+        echo "ERROR: protoc version invalid (expected ${PROTOBUF_VERSION})"
+        exit 1
+    fi
+
+    pkg-config --cflags protobuf
+    pkg-config --libs protobuf
+}
+
+if [ -n "$USE_BUILD_CACHE" ]; then
+    # Get the sources from nexus or upstream
+    cached_build "${TARGET_DIR}" "${DIR_NAME}" "${BUILD_ID}" "${DISTRO}" "${BRANCH_VERSION}"
+else
+    build_package
+fi
+install
+verify_install

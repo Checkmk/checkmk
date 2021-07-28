f322151073b4bc029d86778c975578b790b634df
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-07-28 14:28:28 +0200

    check_mk_agent.aix: move functions
    Change-Id: I5d32a64ca20a3173dd6c0912abc6ee40781951d0

--- tests/agent-unit/test_common_agent_parts.sh
+++ tests/agent-unit/test_common_agent_parts.sh
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-AGENT_LINUX="${UNIT_SH_AGENTS_DIR}/check_mk_agent.linux"
+AGENT_STEM="${UNIT_SH_AGENTS_DIR}/check_mk_agent"
 
 MARK_B="# BEGIN COMMON AGENT CODE"
 MARK_E="# END COMMON AGENT CODE"
@@ -15,16 +15,25 @@ _get_marked_lines() {
 
 test_common_code_blocks () {
 
-    LINUX_REFERENCE=$(_get_marked_lines "$AGENT_LINUX")
+    LINUX_REFERENCE=$(_get_marked_lines "${AGENT_STEM}.linux")
 
     while read -r agent_file; do
         COMMON_BLOCK="$(_get_marked_lines "$agent_file")"
         DIFF="$(diff <(echo "$LINUX_REFERENCE") <(echo "$COMMON_BLOCK"))"
         assertEquals "AGENT: $agent_file" "" "$DIFF"
-    done < <(grep -l "$MARK_B" "${UNIT_SH_AGENTS_DIR}/check_mk_agent."*)
+    done < <(grep -l "$MARK_B" "${AGENT_STEM}."*)
 
 }
 
+test_functions_are_not_all_over_the_place() {
+    for flavor in aix freebsd hpux linux macosx netbsd openbsd openwrt solaris; do
+
+        assertEquals " agent ${flavor}: functions are not sorted:" "section_* run_* main_* " \
+            "$(grep -E '^((section)|(run)|(main)).*\(\)' "${AGENT_STEM}.${flavor}" \
+               | sed 's/[_(].*//' | uniq | sed 's/$/_*/' | tr '\n' ' ')"
+
+    done
+}
 
 # shellcheck disable=SC1090
 . "$UNIT_SH_SHUNIT2"

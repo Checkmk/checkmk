966a84e7fae45f25cc63150a256dc4df3907c9b0
Tom Baerwinkel <tom.baerwinkel@tribe29.com>
2021-08-09 11:24:59 +0200

    Add stripped-down versions of quote_plus and quote
    They fit exactly our needs and are 40-50% faster.
    
    CMK-8035
    
    Change-Id: I0dc9f40f79deb7b1029e937a66eb5fa7e9c884f2

--- cmk/gui/utils/url_encoder.py
+++ cmk/gui/utils/url_encoder.py
@@ -5,11 +5,35 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from typing import Optional, Union
-from urllib.parse import quote_plus
 
 from six import ensure_str
 
 from cmk.gui.type_defs import HTTPVariables
+
+_ALWAYS_SAFE = frozenset(b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                         b'abcdefghijklmnopqrstuvwxyz'
+                         b'0123456789'
+                         b'_.-~'
+                         b' ')
+_ALWAYS_SAFE_BYTES = bytes(_ALWAYS_SAFE)
+_QUOTED = {b: chr(b) if b in _ALWAYS_SAFE else '%{:02X}'.format(b) for b in range(256)}
+
+
+def quote(string: str) -> str:
+    """More performant version of urllib.parse equivalent to the call quote(string, safe=' ')."""
+    if not string:
+        return string
+    bs = string.encode('utf-8', 'strict')
+    if not bs.rstrip(_ALWAYS_SAFE_BYTES):
+        return bs.decode()
+    return ''.join([_QUOTED[char] for char in bs])
+
+
+def quote_plus(string: str) -> str:
+    """More performant version of urllib.parse equivalent to the call quote_plus(string)."""
+    if ' ' not in string:
+        return quote(string)
+    return quote(string).replace(' ', '+')
 
 
 def _quote_pair(varname: str, value: Union[None, int, str]):

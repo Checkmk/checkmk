e315b308ed54a950524c9250049dad2f68527448
Maximilian Wirtz <maximilian.wirtz@tribe29.com>
2021-08-24 09:04:05 +0200

    Improved typing in test_host_config.py
    Change-Id: I28bd0992e0156107d472ec75d78b942d78e9bbd8

--- tests/unit/cmk/ec/test_host_config.py
+++ tests/unit/cmk/ec/test_host_config.py
@@ -5,18 +5,22 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import logging
+from typing import Any, Dict, List, Optional
 
 import pytest
+
+from cmk.utils.livestatus_helpers.testing import MockLiveStatusConnection
+from cmk.utils.type_defs import HostName
 
 from cmk.ec.host_config import HostConfig, HostInfo
 
 
 @pytest.fixture(name="host_config")
-def fixture_host_config():
+def fixture_host_config() -> HostConfig:
     return HostConfig(logging.getLogger("cmk.mkeventd.EventServer"))
 
 
-def _heute_config():
+def _heute_config() -> Dict[str, Any]:
     return {
         'name': 'heute',
         'alias': 'heute alias',
@@ -33,7 +37,7 @@ def _heute_config():
     }
 
 
-def _example_com_config():
+def _example_com_config() -> Dict[str, Any]:
     return {
         'name': 'example.com',
         'alias': 'example.com alias',
@@ -50,7 +54,7 @@ def _example_com_config():
     }
 
 
-def _test_table():
+def _test_table() -> List[Dict[str, Any]]:
     return [
         _heute_config(),
         _example_com_config(),
@@ -58,16 +62,16 @@ def _test_table():
 
 
 @pytest.fixture(name="live")
-def fixture_livestatus(mock_livestatus):
+def fixture_livestatus(mock_livestatus: MockLiveStatusConnection) -> MockLiveStatusConnection:
     mock_livestatus.set_sites(["local"])
     mock_livestatus.add_table('hosts', _test_table())
     return mock_livestatus
 
 
-@pytest.mark.parametrize("search_term, result", [
+@pytest.mark.parametrize("hostname_str, result", [
     ("heute",
      HostInfo(
-         name='heute',
+         name=HostName('heute'),
          alias='heute alias',
          address='127.0.0.1',
          custom_variables={
@@ -83,7 +87,9 @@ def fixture_livestatus(mock_livestatus):
     ("HEUTE", None),
     ("127.0.0.1", None),
 ])
-def test_host_config(host_config, live, search_term, result):
+def test_host_config(host_config: HostConfig, live: MockLiveStatusConnection, hostname_str: str,
+                     result: Optional[HostInfo]) -> None:
+    hostname = HostName(hostname_str)
     with live(expect_status_query=False):
         live.expect_query(["GET status", "Columns: program_start", "ColumnHeaders: off"])
         live.expect_query([
@@ -91,22 +97,23 @@ def test_host_config(host_config, live, search_term, result):
             "Columns: name alias address custom_variables contacts contact_groups",
             "ColumnHeaders: on",
         ])
-        assert host_config.get_config_for_host(search_term) == result
+        assert host_config.get_config_for_host(hostname) == result
         # Data is cached and not queried twice.
         live.expect_query(["GET status", "Columns: program_start", "ColumnHeaders: off"])
-        assert host_config.get_config_for_host(search_term) == result
+        assert host_config.get_config_for_host(hostname) == result
 
 
 @pytest.mark.parametrize("search_term, result", [
-    ('heute', 'heute'),
-    ('HEUTE', 'heute'),
-    ('127.0.0.1', 'heute'),
-    ('server.example.com', 'example.com'),
-    ('SERVER.example.com', 'example.com'),
+    ('heute', HostName('heute')),
+    ('HEUTE', HostName('heute')),
+    ('127.0.0.1', HostName('heute')),
+    ('server.example.com', HostName('example.com')),
+    ('SERVER.example.com', HostName('example.com')),
     ('not-matching', None),
-    ('heute alias', 'heute'),
+    ('heute alias', HostName('heute')),
 ])
-def test_host_config_get_canonical_name(host_config, live, search_term, result):
+def test_host_config_get_canonical_name(host_config: HostConfig, live: MockLiveStatusConnection,
+                                        search_term: str, result: Optional[HostName]) -> None:
     with live(expect_status_query=False):
         live.expect_query(["GET status", "Columns: program_start", "ColumnHeaders: off"])
         live.expect_query([
@@ -120,7 +127,8 @@ def test_host_config_get_canonical_name(host_config, live, search_term, result):
         assert host_config.get_canonical_name(search_term) == result
 
 
-def test_host_config_get_canonical_name_is_cached_updated(host_config, live):
+def test_host_config_get_canonical_name_is_cached_updated(host_config: HostConfig,
+                                                          live: MockLiveStatusConnection) -> None:
     with live(expect_status_query=False):
         live.expect_query(["GET status", "Columns: program_start", "ColumnHeaders: off"])
         live.expect_query([
@@ -128,7 +136,7 @@ def test_host_config_get_canonical_name_is_cached_updated(host_config, live):
             "Columns: name alias address custom_variables contacts contact_groups",
             "ColumnHeaders: on",
         ])
-        assert host_config.get_canonical_name("heute alias") == "heute"
+        assert host_config.get_canonical_name("heute alias") == HostName("heute")
 
         # Update the config to simulate a config change
         live.tables["hosts"]["local"][0]["alias"] = "new alias"
@@ -145,4 +153,4 @@ def test_host_config_get_canonical_name_is_cached_updated(host_config, live):
         assert host_config.get_canonical_name("heute alias") is None
 
         live.expect_query(["GET status", "Columns: program_start", "ColumnHeaders: off"])
-        assert host_config.get_canonical_name("new alias") == "heute"
+        assert host_config.get_canonical_name("new alias") == HostName("heute")

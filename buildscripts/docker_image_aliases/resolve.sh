f520a5c6b2b521e584ae7e74ae95d7603070e247
Frans Fürst <frans.fuerst@tribe29.com>
2021-08-24 11:31:29 +0200

    docker image aliases: make resolve.sh output more descriptive error messages (and fix -q)
    Change-Id: I058f822351292c423d30c0aa8a8248f39fe762ec

--- buildscripts/docker_image_aliases/resolve.sh
+++ buildscripts/docker_image_aliases/resolve.sh
@@ -6,23 +6,32 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-DOCKER_RESULT=$(docker build --pull "${SCRIPT_DIR}/$1" 2>docker-image-alias-resolve-error.txt)
+RESOLVE_ERROR_FILE="docker-image-alias-resolve-error.txt"
+
+DOCKER_RESULT=$(docker build --pull "${SCRIPT_DIR}/$1" 2> ${RESOLVE_ERROR_FILE})
 RESULT_IMAGE_ID=$(awk '/Successfully built/{print $3}' <<< "${DOCKER_RESULT}")
+
+if [ -n "$RESULT_IMAGE_ID" ] ; then
+    echo $RESULT_IMAGE_ID
+else
+    echo "Could not resolve $1, error was:" 1>&2
+    cat ${RESOLVE_ERROR_FILE} 1>&2
+    echo "Make sure the image alias exists, you're correctly logged into the registry and the image exists on the registry" 1>&2
+    echo "INVALID_IMAGE_ID"
+    exit -1
+fi
+
+rm ${RESOLVE_ERROR_FILE}
+
+# >>> This is a workaround for images being deleted on Nexus.
+#     It should be replaced with a solution which allows to keep images forever without having
+#     to pull them regularly.
 
 # Get the nexus repo tag via the meta.yml file. You're asking why not via docker image inspect?
 # It seems that we don't always get the nexus repo tag via the field "RepoTags", so we go this way...
 REPO_TAG=$(grep "tag:" "${SCRIPT_DIR}/$1/meta.yml" | awk '{ print $2}')
 
 # We need to pull also the tag, otherwise Nexus may delete those images
-docker pull -q "${REPO_TAG}" > /dev/null
+docker pull --quiet "${REPO_TAG}" | true
 
-if [ -n "$RESULT_IMAGE_ID" ] ; then
-    rm docker-image-alias-resolve-error.txt
-    echo $RESULT_IMAGE_ID
-else
-    echo "Could not resolve $1, error was:" 1>&2
-    cat docker-image-alias-resolve-error.txt 1>&2
-    echo "Make sure you're correctly logged into the registry and the image exists" 1>&2
-    echo "INVALID_IMAGE_ID"
-fi
-
+# <<<

2f35f427ae262ac15dd8cc1b3bb4cb24c526c505
Maximilian Wirtz <maximilian.wirtz@tribe29.com>
2021-08-30 12:58:19 +0200

    Fix XSS in graph title
    See also Werk #13068
    
    CMK-7140
    
    Change-Id: Ic324125a65b544de2f6e0d7093fa02d97ef09283

--- cmk/gui/utils/rendering.py
+++ cmk/gui/utils/rendering.py
@@ -13,6 +13,7 @@ from typing import (
 from cmk.gui.globals import html
 from cmk.gui.i18n import _u
 from cmk.gui.utils.html import HTML
+from cmk.gui.escaping import escape_text
 
 
 def text_with_links_to_user_translated_html(
@@ -20,7 +21,7 @@ def text_with_links_to_user_translated_html(
     separator: str = "",
 ) -> HTML:
     return HTML(separator).join(
-        html.render_a(user_translation, href=url) if url else user_translation
+        html.render_a(user_translation, href=url) if url else escape_text(user_translation)
         for txt, url in elements
         for user_translation in [_u(txt)]
         if txt)

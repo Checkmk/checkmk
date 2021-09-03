ec3c6d2b7068922a3e30d22b25626848086303d7
Lisa Pichler <lisa.pichler@tribe29.com>
2021-09-03 17:37:34 +0200

    systemd unit tests: consolidate II
    * Discovery:
    ** Single systemd check (with discovery parameter)
    ** Summary systemd check
    
    Change-Id: Ia1f2a6908298616d5caee650fb588d3ef62fb824

--- tests/unit/checks/test_systemd_units.py
+++ tests/unit/checks/test_systemd_units.py
@@ -10,6 +10,8 @@ import pytest
 
 from tests.testlib import Check
 
+from .checktestlib import MockHostExtraConf
+
 pytestmark = pytest.mark.checks
 
 
@@ -448,3 +450,166 @@ def test_services_split(services, blacklist, expected):
 def test_parse_systemd_units(string_table, section):
     check = Check('systemd_units')
     assert check.run_parse(string_table) == section
+
+
+SECTION = {
+    'service': {
+        'virtualbox': UnitEntry(name='virtualbox',
+                                type='service',
+                                load='loaded',
+                                active='active',
+                                sub='exited',
+                                description='LSB: VirtualBox Linux kernel module',
+                                state='unknown'),
+        'bar': UnitEntry(name='bar',
+                         type='service',
+                         load='loaded',
+                         active='failed',
+                         sub='failed',
+                         description='a bar service',
+                         state='unknown'),
+        'foo': UnitEntry(
+            name='foo',
+            type='service',
+            load='loaded',
+            active='failed',
+            sub='failed',
+            description='Arbitrary Executable File Formats File System Automount Point',
+            state='unknown')
+    },
+    'device': {
+        'dev-disk-by\\x2did-ata\\x2dAPPLE_SSD_SM0256G_S29CNYDG865465': UnitEntry(
+            name='dev-disk-by\\x2did-ata\\x2dAPPLE_SSD_SM0256G_S29CNYDG865465',
+            type='device',
+            load='loaded',
+            active='active',
+            sub='plugged',
+            description='APPLE_SSD_SM0256G',
+            state='unknown')
+    },
+    'path': {
+        'cups': UnitEntry(name='cups',
+                          type='path',
+                          load='loaded',
+                          active='active',
+                          sub='running',
+                          description='CUPS Scheduler',
+                          state='unknown')
+    },
+    'scope': {
+        'init': UnitEntry(name='init',
+                          type='scope',
+                          load='loaded',
+                          active='active',
+                          sub='running',
+                          description='System and Service Manager',
+                          state='unknown')
+    },
+    'slice': {
+        'system-getty': UnitEntry(name='system-getty',
+                                  type='slice',
+                                  load='loaded',
+                                  active='active',
+                                  sub='active',
+                                  description='system-getty.slice',
+                                  state='unknown')
+    },
+    'socket': {
+        'systemd-journald': UnitEntry(name='systemd-journald',
+                                      type='socket',
+                                      load='loaded',
+                                      active='active',
+                                      sub='running',
+                                      description='Journal Socket',
+                                      state='unknown')
+    },
+    'swap': {
+        'swapfile': UnitEntry(name='swapfile',
+                              type='swap',
+                              load='loaded',
+                              active='failed',
+                              sub='failed',
+                              description='/swapfile',
+                              state='unknown')
+    },
+    'timer': {
+        'apt-daily-upgrade': UnitEntry(name='apt-daily-upgrade',
+                                       type='timer',
+                                       load='loaded',
+                                       active='active',
+                                       sub='waiting',
+                                       description='Daily apt upgrade and clean activities',
+                                       state='unknown')
+    },
+    'automount': {
+        'proc-sys-fs-.service.binfmt_misc': UnitEntry(
+            name='proc-sys-fs-.service.binfmt_misc',
+            type='automount',
+            load='loaded',
+            active='active',
+            sub='running',
+            description='Arbitrary Executable File Formats File System Automount Point',
+            state='unknown')
+    },
+}
+
+
+@pytest.mark.parametrize('section, discovery_params, discovered_services', [
+    (
+        SECTION,
+        [
+            {
+                "names": ["~virtualbox.*"]
+            },
+        ],
+        [('virtualbox', {})],
+    ),
+    (
+        SECTION,
+        [],
+        [],
+    ),
+    (
+        {},
+        [
+            {
+                "names": ["~virtualbox.*"]
+            },
+        ],
+        [],
+    ),
+    (
+        SECTION,
+        [
+            {
+                "names": ["~aardvark.*"]
+            },
+        ],
+        [],
+    ),
+])
+def test_discover_systemd_units_services(section, discovery_params, discovered_services):
+    check = Check('systemd_units.services')
+
+    def mocked_host_conf(_hostname, ruleset):
+        if ruleset is check.context.get('discovery_systemd_units_services_rules'):
+            return discovery_params
+        raise AssertionError('Unknown ruleset in mock host_extra_conf')
+
+    with MockHostExtraConf(check, mocked_host_conf, 'host_extra_conf'):
+        assert list(check.run_discovery(section)) == discovered_services
+
+
+@pytest.mark.parametrize('section, discovered_services', [
+    (
+        SECTION,
+        [('Summary', {})],
+    ),
+    (
+        {},
+        [],
+    ),
+])
+def test_discover_systemd_units_services_summary(section, discovered_services):
+    check = Check('systemd_units.services_summary')
+    assert list(check.run_discovery(section)) == discovered_services

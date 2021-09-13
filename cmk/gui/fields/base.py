6e15c2afe1610f3fea9cb986c08cbf28fd47eac9
Christoph Rauch <christoph.rauch@tribe29.com>
2021-09-13 13:58:40 +0200

    REST API fields: add MultiNested combination field
     * these can be used to represent "anyOf" polymorphisms
       as defined in the OpenAPI spec.
    
    Change-Id: Id091e25932b5dbe60cfcdf9f6f03aa44a6e20c40

--- cmk/gui/fields/base.py
+++ cmk/gui/fields/base.py
@@ -4,9 +4,11 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 import collections
+import typing
 
-from marshmallow import Schema
-from marshmallow.decorators import post_dump, post_load
+from apispec.ext.marshmallow import common  # type: ignore[import]
+from marshmallow import fields, post_dump, post_load, Schema, types, utils, ValidationError
+from marshmallow.error_store import ErrorStore
 
 
 class BaseSchema(Schema):
@@ -27,3 +29,122 @@ class BaseSchema(Schema):
         if self.cast_to_dict and isinstance(data, collections.OrderedDict):
             return dict(data)
         return data
+
+
+class MultiNested(fields.Field):
+    """
+
+    >>> class User(BaseSchema):
+    ...     cast_to_dict = True
+    ...     name = fields.String()
+
+    >>> class Luser(BaseSchema):
+    ...     cast_to_dict = True
+    ...     epithet = fields.String()
+
+    >>> class People(BaseSchema):
+    ...     cast_to_dict = True
+    ...     lusers = MultiNested([User(), Luser()], many=True)
+
+    >>> nested = People()
+    >>> nested.load({'lusers': [{'name': 'Hans Wurst'}, {'epithet': 'DAU'}]})
+    {'lusers': [{'name': 'Hans Wurst'}, {'epithet': 'DAU'}]}
+
+    >>> nested.declared_fields['lusers'].metadata
+    {'anyOf': [<User(many=False)>, <Luser(many=False)>]}
+
+    """
+
+    def __init__(
+        self,
+        nested: typing.List[typing.Type[fields.SchemaABC]],
+        mode: typing.Literal["anyOf", "allOf"] = "anyOf",
+        *,
+        default: typing.Any = fields.missing_,
+        only: typing.Optional[types.StrSequenceOrSet] = None,
+        exclude: types.StrSequenceOrSet = (),
+        many: bool = False,
+        unknown: typing.Optional[str] = None,
+        **kwargs,
+    ):
+        if mode != "anyOf":
+            raise NotImplementedError("allOf is not yet implemented.")
+
+        context = getattr(self.parent, "context", {})
+        context.update(kwargs.get("context", {}))
+
+        self.nested = []
+        schema_inst: Schema
+        for schema in nested:
+            schema_inst = common.resolve_schema_instance(schema)
+            schema_inst.context.update(context)
+            self.nested.append(schema_inst)
+
+        self.mode = mode
+        self.only = only
+        self.exclude = exclude
+        self.many = many
+        self.unknown = unknown
+        super().__init__(default=default, metadata={"anyOf": nested}, **kwargs)
+
+    def _serialize(
+        self,
+        value: typing.Any,
+        attr: str,
+        obj: typing.Any,
+        **kwargs,
+    ):
+        if value is None:
+            return None
+
+        error_store = ErrorStore()
+        for schema in self.nested:
+            try:
+                return common.resolve_schema_instance(schema).dump(value, many=self.many)
+            except ValidationError as exc:
+                error_store.store_error(exc.messages, field_name=exc.field_name)
+
+        raise ValidationError(error_store.errors, data=value)
+
+    def _test_collection(self, value):
+        if self.many and not utils.is_collection(value):
+            raise self.make_error("type", input=value, type=value.__class__.__name__)
+
+    def _check_schemas(self, scalar, partial=None):
+        error_store = ErrorStore()
+        for schema in self.nested:
+            try:
+                return common.resolve_schema_instance(schema).load(
+                    scalar,
+                    unknown=self.unknown,
+                    partial=partial,
+                )
+            except ValidationError as exc:
+                error_store.store_error(exc.messages, field_name=exc.field_name)
+
+        raise ValidationError(error_store.errors, data=scalar)
+
+    def _deserialize(
+        self,
+        value: typing.Any,
+        attr: typing.Optional[str],
+        data: typing.Optional[typing.Mapping[str, typing.Any]],
+        **kwargs,
+    ):
+        error_store = ErrorStore()
+        if self.many:
+            result = []
+            if utils.is_collection(value):
+                for collection_entry in value:
+                    result.append(self._check_schemas(collection_entry))
+                return result
+
+            raise self.make_error("type", input=value, type=value.__class__.__name__)
+
+        for schema in self.nested:
+            try:
+                return common.resolve_schema_instance(schema).load(value, unknown=self.unknown)
+            except ValidationError as exc:
+                error_store.store_error(exc.messages, field_name=exc.field_name)
+
+        raise ValidationError(error_store.errors, data=value)

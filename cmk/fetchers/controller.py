c18fa96942446934021d4b997896008f0247075a
Mathias Laurin <mathias.laurin@tribe29.com>
2021-09-13 13:02:02 +0200

    Add a timeout module
    Move the signal-based Timeout context manager to a module to
    increase its visibility and let it be reused elsewhere.
    
    The code is general and useful enough for that.
    
    CMK-7738
    
    Change-Id: Ieddf3e2f9a65759c5bf5dcaaa2ae8aa06affd243

--- cmk/fetchers/controller.py
+++ cmk/fetchers/controller.py
@@ -8,19 +8,17 @@ import contextlib
 import json
 import logging
 import os
-import signal
 import sys
 import traceback
 from pathlib import Path
-from types import FrameType
-from typing import Any, Final, Dict, Iterable, Iterator, List, NamedTuple, Optional
+from typing import Any, Dict, Iterable, Iterator, List, NamedTuple
 
 import cmk.utils.cleanup
 import cmk.utils.paths as paths
 from cmk.utils.cpu_tracking import CPUTracker, Snapshot
-from cmk.utils.exceptions import MKTimeout
 from cmk.utils.fetcher_crash_reporting import create_fetcher_crash_dump
 from cmk.utils.observer import ABCResourceObserver
+from cmk.utils.timeout import MKTimeout, Timeout
 from cmk.utils.type_defs import ConfigSerial, HostName, result
 
 from . import ABCFetcher, FetcherType, MaxAge, protocol
@@ -74,31 +72,6 @@ class GlobalConfig(NamedTuple):
                 "snmp_plugin_store": self.snmp_plugin_store.serialize(),
             },
         }
-
-
-class Timeout:
-    def __init__(self, timeout: int, *, message: str) -> None:
-        self.timeout: Final = timeout
-        self.message: Final = message
-        self._signaled = False
-
-    @property
-    def signaled(self) -> bool:
-        return self._signaled
-
-    def _handler(self, signum: int, frame: Optional[FrameType]) -> None:
-        self._signaled = True
-        raise MKTimeout(self.message)
-
-    def __enter__(self) -> 'Timeout':
-        self._signaled = False
-        signal.signal(signal.SIGALRM, self._handler)
-        signal.alarm(self.timeout)
-        return self
-
-    def __exit__(self, *_args) -> None:
-        signal.signal(signal.SIGALRM, signal.SIG_IGN)
-        signal.alarm(0)
 
 
 class Command(NamedTuple):

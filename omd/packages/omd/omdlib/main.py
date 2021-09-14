fcb084ad798cc7d3d3bb1007a8be6575f319c4a1
Andreas Boesl <andreas.boesl@tribe29.com>
2021-09-14 15:58:40 +0200

    12665 FIX OMD: Using global ldap authorization caused excessive LDAP communication when switching to site user
    Checkmk servers which not only use the local system user and group database (/etc/passwd and /etc/group),
    but also use an integration with a 3rd party system could run into performance issues.
    A common example for such a setup is an LDAP integration.
    This happened, for example, with <tt>omd su sitename</tt> or with any check_mk_agent (calls <tt>omd status --bare</tt>)
    
    Change-Id: I128ace656c974e0c4faa9c930b8d1ab107c9093f

--- omd/packages/omd/omdlib/main.py
+++ omd/packages/omd/omdlib/main.py
@@ -605,9 +605,9 @@ def switch_to_site_user(site):
 
 
 def groups_of(username):
-    group_ids = set([g.gr_gid for g in grp.getgrall() if username in g.gr_mem])
-    group_ids.add(pwd.getpwnam(username).pw_gid)
-    return list(group_ids)
+    # Note: Do NOT use grp.getgrall to fetch all availabile groups
+    # Certain setups might have ldap group authorization and may start excessive queries
+    return map(int, os.popen("id -G '%s'" % username).read().split())
 
 
 #.

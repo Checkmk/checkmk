f5e621aa69f0e1563da3febb04f1a2f2bced8fc4
Stefan Hölzl <stefan.hoelzl@tribe29.com>
2021-09-21 15:01:33 +0200

    pins pipenv/virtualenv version on build containers
    CMK-8602
    
    Change-Id: I929c6eeb1be60cd57eae488d90511de04e4181e2

--- buildscripts/infrastructure/build-nodes/scripts/install-pipenv.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-pipenv.sh
@@ -5,37 +5,4 @@
 
 set -e -o pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-. "${SCRIPT_DIR}/build_lib.sh"
-
-# read optional command line argument
-if [ "$#" -eq 1 ]; then
-    PYTHON_VERSION=$1
-else
-    cd "${SCRIPT_DIR}"
-    while true; do
-        if [ -e defines.make ]; then
-            PYTHON_VERSION=$(make --no-print-directory --file=defines.make print-PYTHON_VERSION)
-            break
-        elif [ $PWD == / ]; then
-            failure "could not determine Python version"
-        else
-            cd ..
-        fi
-    done
-fi
-
-PYTHON_DIR_NAME=Python-${PYTHON_VERSION}
-
-PIPENV_VERSION=2020.11.15
-ARCHIVE_NAME=v${PIPENV_VERSION}.tar.gz
-DIR_NAME=pipenv-${PIPENV_VERSION}
-TARGET_DIR=/opt
-
-cd "${TARGET_DIR}"
-mirrored_download "${ARCHIVE_NAME}" "https://github.com/pypa/pipenv/archive/${ARCHIVE_NAME}"
-tar xf "${ARCHIVE_NAME}"
-cd "${DIR_NAME}"
-pip3 install .
-
-ln -sf "${TARGET_DIR}/${PYTHON_DIR_NAME}/bin/pipenv"* /usr/bin
+pip3 install pipenv==2021.5.29 virtualenv==20.7.2

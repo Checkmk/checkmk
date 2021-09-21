ffc4a37bd1a35f27cf1ffa1e8255b62a20b496a8
Lars Michelsen <lm@tribe29.com>
2021-09-21 16:14:34 +0200

    Add unit test for creating the sample config
    Change-Id: Icf3b8c3cd77c965f6c25dc8e72a08c6b1d8c2dcc

---
+++ tests/unit/cmk/gui/watolib/test_sample_config.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from pathlib import Path
+
+from cmk.utils.paths import omd_root
+
+from cmk.gui.watolib.sample_config import init_wato_datastructures
+
+
+def test_init_wato_data_structures():
+    init_wato_datastructures()
+    assert Path(omd_root, "etc/check_mk/conf.d/wato/rules.mk").exists()
+    assert Path(omd_root, "etc/check_mk/multisite.d/wato/tags.mk").exists()
+    assert Path(omd_root, "etc/check_mk/conf.d/wato/global.mk").exists()
+    assert Path(omd_root, "var/check_mk/web/automation").exists()
+    assert Path(omd_root, "var/check_mk/web/automation/automation.secret").exists()

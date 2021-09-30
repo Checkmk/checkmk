fd8af9aa35d0cc96afb9e543cec987bff655e08e
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-09-30 07:55:01 +0200

    13224 DEV: make legacy includes packageable
    This only affects plugin developers.
    
    With this werk, changes to files in <tt>cmk/base/check_legacy_includes</tt> can now be included in MKPs.
    The files in the sites <tt>local</tt> folder will be considered, and win over their shipped counterparts.
    
    This has been made necessary to backport various comissioned features or bug fixes.
    
    <b>Note:</b> This is, as the name suggests, all legacy code.
    The <tt>check_legacy_includes</tt> and its contents are <b>deprecated</b> and we strongly advise against using it.
    
    Change-Id: I4c2f9c4c4e9c060155876dae3cdec6db42d66017

--- cmk/base/check_legacy_includes/__init__.py
+++ cmk/base/check_legacy_includes/__init__.py
@@ -1,1 +1,15 @@
-# type: ignore[list-item,import,assignment,misc,operator]  # TODO: see which are needed in this file
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+#
+#    This file (cmk.base.check_legacy_includes.__init__.py) is only kept
+#    to make our dev tools (pylint & co) happy.
+#    It will not be deployed, as cmk.base.check_legacy_includes is supposed
+#    to be a namespace that can be shadowed/extended using the local/
+#    hierarchy.
+#
+#    This file must not contain any code.
+#

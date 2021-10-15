1ac5fa0d2d3dd56bc805a1668906a42e18622956
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2021-10-15 14:45:22 +0200

    Query ntop's REST API Version
    * REST API v1: we do not have an endpoint yet, so this is our fallback
    * From REST API v2 on, we can query the current version and all
    compatible ones and then choose a suitable one
    * Cache the version in Redis - there is no need to query it at every
    request
    
    CMK-8589
    
    Change-Id: Ic2d1ca87e1eb0e87ca26449d8231626ab3321c4c

--- cmk/utils/redis.py
+++ cmk/utils/redis.py
@@ -73,8 +73,8 @@ def query_redis(client: 'RedisDecoded',
         return query_callback()
     except MKTimeout:
         raise
-    except Exception:
-        raise DataUnavailableException()
+    except Exception as e:
+        raise DataUnavailableException(e)
     finally:
         if query_lock.owned():
             query_lock.release()

2b41753a219473ad8099c1f09c6ce772c0f8aa9c
Moritz Kiemer <moritz.kiemer@tribe29.com>
2021-10-19 08:09:47 +0200

    add commas for easier picking
    Change-Id: Ic1748045f3a4161f805bb8088be2b0704123e963

--- cmk/gui/plugins/wato/check_parameters/systemd_services_summary.py
+++ cmk/gui/plugins/wato/check_parameters/systemd_services_summary.py
@@ -23,71 +23,90 @@ from cmk.gui.plugins.wato import (
 
 
 def _parameter_valuespec_systemd_services():
-    return Dictionary(elements=[
-        ("states",
-         Dictionary(
-             title=_("Map systemd states to monitoring states"),
-             elements=[
-                 ("active",
-                  MonitoringState(
-                      title=_("Monitoring state if service is active"),
-                      default_value=0,
-                  )),
-                 ("inactive",
-                  MonitoringState(
-                      title=_("Monitoring state if service is inactive"),
-                      default_value=0,
-                  )),
-                 ("failed",
-                  MonitoringState(
-                      title=_("Monitoring state if service is failed"),
-                      default_value=2,
-                  )),
-             ],
-         )),
-        ("states_default",
-         MonitoringState(
-             title=_("Monitoring state for any other service state"),
-             default_value=2,
-         )),
-        ("ignored",
-         ListOf(
-             RegExpUnicode(
-                 title=_("Pattern (Regex)"),
-                 size=40,
-                 mode=RegExp.infix,
-             ),
-             title=_("Exclude services matching provided regex patterns"),
-             help=_(
-                 '<p>You can optionally define one or multiple regular expressions '
-                 'where a matching case will result in the exclusion of the concerning service(s). '
-                 'This allows to ignore services which are known to fail beforehand. </p>'),
-             add_label=_("Add pattern"),
-         )),
-        ("activating_levels",
-         Tuple(
-             title=_("Define a tolerating time period for activating services"),
-             help=
-             _("Choose time levels (in seconds) for which a service is allowed to be in an 'activating' state"
-              ),
-             elements=[
-                 Integer(title=_("Warning at"), unit=_("seconds"), default_value=30),
-                 Integer(title=_("Critical at"), unit=_("seconds"), default_value=60),
-             ])),
-        ("reloading_levels",
-         Tuple(
-             title=_("Define a tolerating time period for reloading services"),
-             help=
-             _("Choose time levels (in seconds) for which a service is allowed to be in a 'reloading' state"
-              ),
-             elements=[
-                 Integer(title=_("Warning at"), unit=_("seconds"), default_value=30),
-                 Integer(title=_("Critical at"), unit=_("seconds"), default_value=60),
-             ])),
-    ],
-                      help=_(
-                          "This ruleset only applies to the Summary Systemd service and not the individual "
-                          "Systemd services."))
+    return Dictionary(
+        elements=[
+            (
+                "states",
+                Dictionary(
+                    title=_("Map systemd states to monitoring states"),
+                    elements=[
+                        (
+                            "active",
+                            MonitoringState(
+                                title=_("Monitoring state if service is active"),
+                                default_value=0,
+                            ),
+                        ),
+                        (
+                            "inactive",
+                            MonitoringState(
+                                title=_("Monitoring state if service is inactive"),
+                                default_value=0,
+                            ),
+                        ),
+                        (
+                            "failed",
+                            MonitoringState(
+                                title=_("Monitoring state if service is failed"),
+                                default_value=2,
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+            (
+                "states_default",
+                MonitoringState(
+                    title=_("Monitoring state for any other service state"),
+                    default_value=2,
+                ),
+            ),
+            (
+                "ignored",
+                ListOf(
+                    RegExpUnicode(
+                        title=_("Pattern (Regex)"),
+                        size=40,
+                        mode=RegExp.infix,
+                    ),
+                    title=_("Exclude services matching provided regex patterns"),
+                    help=_(
+                        '<p>You can optionally define one or multiple regular expressions '
+                        'where a matching case will result in the exclusion of the concerning service(s). '
+                        'This allows to ignore services which are known to fail beforehand. </p>'),
+                    add_label=_("Add pattern"),
+                ),
+            ),
+            (
+                "activating_levels",
+                Tuple(
+                    title=_("Define a tolerating time period for activating services"),
+                    help=
+                    _("Choose time levels (in seconds) for which a service is allowed to be in an 'activating' state"
+                     ),
+                    elements=[
+                        Integer(title=_("Warning at"), unit=_("seconds"), default_value=30),
+                        Integer(title=_("Critical at"), unit=_("seconds"), default_value=60),
+                    ],
+                ),
+            ),
+            (
+                "reloading_levels",
+                Tuple(
+                    title=_("Define a tolerating time period for reloading services"),
+                    help=
+                    _("Choose time levels (in seconds) for which a service is allowed to be in a 'reloading' state"
+                     ),
+                    elements=[
+                        Integer(title=_("Warning at"), unit=_("seconds"), default_value=30),
+                        Integer(title=_("Critical at"), unit=_("seconds"), default_value=60),
+                    ],
+                ),
+            ),
+        ],
+        help=_("This ruleset only applies to the Summary Systemd service and not the individual "
+               "Systemd services."),
+    )
 
 
 rulespec_registry.register(

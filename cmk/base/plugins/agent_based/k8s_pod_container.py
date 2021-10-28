4cdf718836c9d7add8640ec94b6dabebc4c17b21
Simon Jess <simon.jess@tribe29.com>
2021-10-28 08:23:50 +0200

    Migrate inventory plugin k8s_pod_container
    Change-Id: I0c10c15452ffa5a55bdcf315f915c987308f10a7

--- cmk/base/plugins/agent_based/k8s_pod_container.py
+++ cmk/base/plugins/agent_based/k8s_pod_container.py
@@ -3,14 +3,17 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Dict
 
-from .agent_based_api.v1 import HostLabel, register
-from .agent_based_api.v1.type_defs import HostLabelGenerator
-from .utils import k8s
+from typing import Any, Mapping
+
+from .agent_based_api.v1 import HostLabel, register, TableRow
+from .agent_based_api.v1.type_defs import HostLabelGenerator, InventoryResult
+from .utils import docker, k8s
+
+Section = Mapping[str, Mapping[str, Any]]
 
 
-def host_labels(section: Dict) -> HostLabelGenerator:
+def host_labels(section: Section) -> HostLabelGenerator:
     if section:
         yield HostLabel("cmk/kubernetes_object", "pod")
         yield HostLabel("cmk/kubernetes", "yes")
@@ -20,4 +23,38 @@ register.agent_section(
     name="k8s_pod_container",
     parse_function=k8s.parse_json,
     host_label_function=host_labels,
+)
+
+
+def inventory_k8s_pod_container(section: Section) -> InventoryResult:
+    for container_name, container_data in sorted(section.items()):
+        yield TableRow(
+            path=["software", "applications", "kubernetes", "pod_container"],
+            key_columns={
+                "name": container_name,
+            },
+            inventory_columns={
+                "image": container_data["image"],
+                "image_pull_policy": container_data["image_pull_policy"],
+                "image_id": (
+                    docker.get_short_id(container_data["image_id"])
+                    if container_data["image_id"]
+                    else "No ID"
+                ),
+            },
+            status_columns={
+                "ready": "yes" if container_data["ready"] else "no",
+                "restart_count": container_data["restart_count"],
+                "container_id": (
+                    docker.get_short_id(container_data["container_id"])
+                    if container_data["container_id"]
+                    else "No ID"
+                ),
+            },
+        )
+
+
+register.inventory_plugin(
+    name="k8s_pod_container",
+    inventory_function=inventory_k8s_pod_container,
 )

7190277faf38d7d5311a34ceabbb113b0702d7a3
Lars Michelsen <lm@tribe29.com>
2021-11-02 16:10:44 +0100

    Revert "Remove xinetd from distro deps"
    This reverts commit 0813376afe88b1187d804394783621e2bf80447e.
    
    Also needs to be reverted temporarily to make nightly jobs work again.

--- omd/distros/UBUNTU_17.04.mk
+++ omd/distros/UBUNTU_17.04.mk
@@ -31,6 +31,7 @@ OS_PACKAGES    += rsync
 OS_PACKAGES    += smbclient
 OS_PACKAGES    += rpcbind # otherwise missing path in util.pm
 OS_PACKAGES    += unzip
+OS_PACKAGES    += xinetd
 OS_PACKAGES    += freeradius-utils
 #
 # Checkmk build specific packages below

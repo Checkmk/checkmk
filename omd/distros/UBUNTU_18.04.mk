e8c18e5cfaf77aebf3f4aef1a339dfccc2331c86
Mathias Laurin <mathias.laurin@tribe29.com>
2021-11-10 14:04:36 +0100

    Revert "Re systemd"
    This reverts commit 9a3ce4d7a9dadc43084db8f53d5419958243fecf.
    
    --
    
    Revert "Add ed as dependency to the distros"
    
    This reverts commit fd5e8776b15d401d5a2db50c671dab8880b301ad.

--- omd/distros/UBUNTU_18.04.mk
+++ omd/distros/UBUNTU_18.04.mk
@@ -10,7 +10,6 @@ OS_PACKAGES    += traceroute # needed for Checkmk parent scan
 OS_PACKAGES    += curl
 OS_PACKAGES    += dialog
 OS_PACKAGES    += dnsutils
-OS_PACKAGES    += ed
 OS_PACKAGES    += graphviz
 OS_PACKAGES    += apache2
 OS_PACKAGES    += apache2-utils # contains htpasswd2
@@ -30,9 +29,9 @@ OS_PACKAGES    += php-json
 OS_PACKAGES    += php-pear
 OS_PACKAGES    += rsync
 OS_PACKAGES    += smbclient
-OS_PACKAGES    += systemd
 OS_PACKAGES    += rpcbind # otherwise missing path in util.pm
 OS_PACKAGES    += unzip
+OS_PACKAGES    += xinetd
 OS_PACKAGES    += freeradius-utils
 #
 # Checkmk build specific packages below

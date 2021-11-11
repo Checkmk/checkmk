80f6a9d03888d8a9d50cef226f55b3e8baf426d5
Frans Fürst <frans.fuerst@tribe29.com>
2021-11-11 10:07:52 +0100

    remove noisy doctests
    Change-Id: I29c5a77fe835e242709f4937ec69d2687c17bea0

--- cmk/base/plugins/agent_based/inventory_statgrab_net.py
+++ cmk/base/plugins/agent_based/inventory_statgrab_net.py
@@ -14,33 +14,6 @@ from .utils.inventory_interfaces import inventorize_interfaces
 
 
 def parse_statgrab_net(string_table: type_defs.StringTable) -> interfaces.Section:
-    """
-    >>> from pprint import pprint
-    >>> pprint(parse_statgrab_net([
-    ... ['lo0.duplex', 'unknown'], ['lo0.interface_name', 'lo0'], ['lo0.speed', '0'],
-    ... ['lo0.up', 'true'], ['mac.collisions', '0'], ['mac.collisions', '0'],
-    ... ['mac.collisions', '0'], ['mac.collisions', '0'], ['mac.ierrors', '0'],
-    ... ['mac.ierrors', '0'], ['mac.ierrors', '0'], ['mac.ierrors', '0'],
-    ... ['mac.interface_name', 'mac'], ['mac.interface_name', 'mac'],
-    ... ['mac.interface_name', 'mac'], ['mac.interface_name', 'mac'],
-    ... ['mac.ipackets', '1268296097'], ['mac.ipackets', '38927952'],
-    ... ['mac.ipackets', '565577805'], ['mac.ipackets', '50729410'], ['mac.oerrors', '0'],
-    ... ['mac.oerrors', '0'], ['mac.oerrors', '0'], ['mac.oerrors', '0'],
-    ... ['mac.opackets', '565866338'], ['mac.opackets', '8035845'],
-    ... ['mac.opackets', '13022050069'], ['mac.opackets', '102'], ['mac.rx', '8539777403'],
-    ... ['mac.rx', '9040025900'], ['mac.rx', '144543115933'], ['mac.rx', '125659024941'],
-    ... ['mac.systime', '1413287036'], ['mac.systime', '1413287036'], ['mac.systime', '1413287036'],
-    ... ['mac.systime', '1413287036'], ['mac.tx', '15206'], ['mac.tx', '19679032546569'],
-    ... ['mac.tx', '124614022405'], ['mac.tx', '482272878'], ['vnet0.collisions', '0'],
-    ... ['vnet0.duplex', 'unknown'], ['vnet0.ierrors', '0'], ['vnet0.interface_name', 'vnet0'],
-    ... ['vnet0.ipackets', '1268296097'], ['vnet0.oerrors', '0'], ['vnet0.opackets', '13022050069'],
-    ... ['vnet0.rx', '125659024941'], ['vnet0.speed', '0'], ['vnet0.systime', '1413287036'],
-    ... ['vnet0.tx', '19679032546569'], ['vnet0.up', 'true'],
-    ... ]))
-    [Interface(index='1', descr='lo0', alias='lo0', type='24', speed=0, oper_status='1', in_octets=0, in_ucast=0, in_mcast=0, in_bcast=0, in_discards=0, in_errors=0, out_octets=0, out_ucast=0, out_mcast=0, out_bcast=0, out_discards=0, out_errors=0, out_qlen=0, phys_address='', oper_status_name='up', speed_as_text='', group=None, node=None, admin_status=None, total_octets=0),
-     Interface(index='2', descr='mac', alias='mac', type='6', speed=0, oper_status='2', in_octets=125659024941, in_ucast=50729410, in_mcast=0, in_bcast=0, in_discards=0, in_errors=0, out_octets=482272878, out_ucast=102, out_mcast=0, out_bcast=0, out_discards=0, out_errors=0, out_qlen=0, phys_address='', oper_status_name='down', speed_as_text='', group=None, node=None, admin_status=None, total_octets=126141297819),
-     Interface(index='3', descr='vnet0', alias='vnet0', type='6', speed=0, oper_status='1', in_octets=125659024941, in_ucast=1268296097, in_mcast=0, in_bcast=0, in_discards=0, in_errors=0, out_octets=19679032546569, out_ucast=13022050069, out_mcast=0, out_bcast=0, out_discards=0, out_errors=0, out_qlen=0, phys_address='', oper_status_name='up', speed_as_text='', group=None, node=None, admin_status=None, total_octets=19804691571510)]
-    """
     nics: Dict[str, Dict[str, str]] = {}
     for nic_varname, value in string_table:
         nic_id, varname = nic_varname.split(".")

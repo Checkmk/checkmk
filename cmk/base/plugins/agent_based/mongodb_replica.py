fb4dadc7d975e5bb9dd7e72fe2407274de499af7
Joerg Herbel <joerg.herbel@tribe29.com>
2021-11-15 16:29:51 +0100

    mongodb_replica: Migrate section
    The new section is designed s.t. we can easily
    1) change the corresponding agent plugin to produce json
    2) fix a bug in the mongodb_replica check plugin
    in the following commits.
    
    SUP-7891
    
    Change-Id: I0eb99d717ebe69fb1fd9f1e7932f20b76f2f26c8

---
+++ cmk/base/plugins/agent_based/mongodb_replica.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from dataclasses import dataclass
+from typing import Optional
+
+from .agent_based_api.v1 import register
+from .agent_based_api.v1.type_defs import StringTable
+
+
+@dataclass(frozen=True)
+class Secondaries:
+    active: Optional[str]
+
+
+@dataclass(frozen=True)
+class ReplicaSet:
+    primary: Optional[str]
+    secondaries: Secondaries
+    arbiters: Optional[str]
+
+
+def parse_mongodb_replica(string_table: StringTable) -> ReplicaSet:
+    section_dict = {line[0]: line[1] for line in string_table}
+    primary = section_dict.get("primary")
+    return ReplicaSet(
+        primary=primary if primary != "n/a" else None,
+        secondaries=Secondaries(active=section_dict.get("hosts"),),
+        arbiters=section_dict.get("arbiters"),
+    )
+
+
+register.agent_section(
+    name="mongodb_replica",
+    parse_function=parse_mongodb_replica,
+)
---
+++ tests/unit/cmk/base/plugins/agent_based/test_mongodb_replica.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
+from cmk.base.plugins.agent_based.mongodb_replica import (
+    parse_mongodb_replica,
+    ReplicaSet,
+    Secondaries,
+)
+
+
+@pytest.mark.parametrize(
+    "string_table, parsed_section",
+    [
+        pytest.param(
+            [
+                [
+                    "primary",
+                    "idbv0068.xyz.de:27017",
+                ],
+                [
+                    "hosts",
+                    "idbv0067.xyz:27017 idbv0068.xyz.de:27017",
+                ],
+                [
+                    "arbiters",
+                    "idbv0069.xyz.de:27017",
+                ],
+            ],
+            ReplicaSet(
+                primary="idbv0068.xyz.de:27017",
+                secondaries=Secondaries(active="idbv0067.xyz:27017 idbv0068.xyz.de:27017",),
+                arbiters="idbv0069.xyz.de:27017",
+            ),
+            id="primary present",
+        ),
+        pytest.param(
+            [
+                [
+                    "primary",
+                    "n/a",
+                ],
+                [
+                    "hosts",
+                    "idbv0067.xyz:27017 idbv0068.xyz.de:27017",
+                ],
+                [
+                    "arbiters",
+                    "idbv0069.xyz.de:27017",
+                ],
+            ],
+            ReplicaSet(
+                primary=None,
+                secondaries=Secondaries(active="idbv0067.xyz:27017 idbv0068.xyz.de:27017",),
+                arbiters="idbv0069.xyz.de:27017",
+            ),
+            id="primary missing",
+        ),
+    ],
+)
+def test_parse_mongodb_replica(
+    string_table: StringTable,
+    parsed_section: ReplicaSet,
+) -> None:
+    assert parse_mongodb_replica(string_table) == parsed_section

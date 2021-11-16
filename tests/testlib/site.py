b9a83886453a31106ca84cf02ea4a9ca2e6c39fc
Lars Michelsen <lm@tribe29.com>
2021-11-16 16:21:24 +0100

    Fix integration test flakes caused by log rotation
    The Nagios log rotation happens every day at 00:00 UTC with the
    default configuration and the CMC log rotation is executed every
    monday at 00:00 UTC.
    
    Since our integration tests WatchLog log watch is not able to
    handle the log rotation we now disable the rotation during the
    integration tests.
    
    Change-Id: I107339adc5da1ed7c102ed4fc5ce91b256fd52da

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -404,11 +404,13 @@ class Site:
             assert os.path.exists("/omd/sites/%s" % self.id)
 
             self._ensure_sample_config_is_present()
-            self._set_number_of_helpers()
-            self._enabled_liveproxyd_debug_logging()
+            if not self.version.is_raw_edition():
+                self._set_number_of_helpers()
+                self._enable_cmc_core_dumps()
+                self._enable_cmc_debug_logging()
+                self._disable_cmc_log_rotation()
+                self._enabled_liveproxyd_debug_logging()
             self._enable_mkeventd_debug_logging()
-            self._enable_cmc_core_dumps()
-            self._enable_cmc_debug_logging()
             self._enable_gui_debug_logging()
             self._tune_nagios()
 
@@ -525,7 +527,7 @@ class Site:
     def _enable_cmc_debug_logging(self):
         self.makedirs("etc/check_mk/conf.d")
         self.write_file(
-            "etc/check_mk/conf.d/cmc-logging.mk", "cmc_log_levels = %r\n" % {
+            "etc/check_mk/conf.d/cmc-debug-logging.mk", "cmc_log_levels = %r\n" % {
                 'cmk.alert': 7,
                 'cmk.carbon': 7,
                 'cmk.core': 7,
@@ -536,6 +538,13 @@ class Site:
                 'cmk.rrd': 7,
                 'cmk.smartping': 7,
             })
+
+    def _disable_cmc_log_rotation(self):
+        self.makedirs("etc/check_mk/conf.d")
+        self.write_file(
+            "etc/check_mk/conf.d/cmc-log-rotation.mk",
+            "cmc_log_rotation_method = 4\ncmc_log_limit = 1073741824\n",
+        )
 
     def _enable_gui_debug_logging(self):
         self.makedirs("etc/check_mk/multisite.d")
@@ -550,14 +559,19 @@ class Site:
             })
 
     def _tune_nagios(self):
-        # We want nagios to process queued external commands as fast as possible.  Even if we
-        # set command_check_interval to -1, nagios is doing some sleeping in between the
-        # command processing. We reduce the sleep time here to make it a little faster.
         self.write_file(
-            "etc/nagios/nagios.d/zzz-test-tuning.cfg", "log_passive_checks=1\n"
+            "etc/nagios/nagios.d/zzz-test-tuning.cfg",
+            # We need to observe these entries with WatchLog for our tests
+            "log_passive_checks=1\n"
+            # We want nagios to process queued external commands as fast as possible.  Even if we
+            # set command_check_interval to -1, nagios is doing some sleeping in between the
+            # command processing. We reduce the sleep time here to make it a little faster.
             "service_inter_check_delay_method=n\n"
             "host_inter_check_delay_method=n\n"
-            "sleep_time=0.05\n")
+            "sleep_time=0.05\n"
+            # WatchLog is not able to handle log rotations. Disable the rotation during tests.
+            "log_rotation_method=n\n",
+        )
 
     def _install_test_python_modules(self):
         venv = virtualenv_path()

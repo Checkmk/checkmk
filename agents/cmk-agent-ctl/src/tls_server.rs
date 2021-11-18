00e00ea371ea6ad768d1b4b8dc6ae5d5624b391b
Andreas Umbreit <andreas.umbreit@tribe29.com>
2021-11-18 16:33:47 +0100

    CMK-8937: cmk-agent-ctl: Verify client certificate in pull mode
    Change-Id: I17714ba8559005db9682831f2a877cfbe9e2df22

--- agents/cmk-agent-ctl/src/tls_server.rs
+++ agents/cmk-agent-ctl/src/tls_server.rs
@@ -1,12 +1,14 @@
 use super::config;
+use rustls::RootCertStore;
 use rustls::{
-    server::ResolvesServerCertUsingSni, sign::CertifiedKey, sign::RsaSigningKey, Certificate,
-    PrivateKey, ServerConfig, ServerConnection, Stream as RustlsStream,
+    server::AllowAnyAuthenticatedClient, server::ResolvesServerCertUsingSni, sign::CertifiedKey,
+    sign::RsaSigningKey, Certificate, PrivateKey, ServerConfig, ServerConnection,
+    Stream as RustlsStream,
 };
 use rustls_pemfile::Item;
 use std::error::Error;
 use std::fs::File;
-use std::io::Result as IoResult;
+use std::io::{self, Result as IoResult};
 use std::io::{Read, Write};
 use std::os::unix::prelude::FromRawFd;
 use std::sync::Arc;
@@ -29,36 +31,35 @@ fn tls_config(server_specs: Vec<config::ServerSpec>) -> Result<Arc<ServerConfig>
     Ok(Arc::new(
         ServerConfig::builder()
             .with_safe_defaults()
-            .with_no_client_auth()
-            .with_cert_resolver(sni_resolver(server_specs)?),
+            .with_client_cert_verifier(AllowAnyAuthenticatedClient::new(root_cert_store(
+                &server_specs,
+            )?))
+            .with_cert_resolver(sni_resolver(&server_specs)?),
     ))
 }
 
+fn root_cert_store(
+    server_specs: &Vec<config::ServerSpec>,
+) -> Result<RootCertStore, Box<dyn Error>> {
+    let mut cert_store = RootCertStore::empty();
+
+    for spec in server_specs {
+        cert_store
+            .add(&certificate(&mut spec.root_cert.as_bytes())?)
+            .unwrap();
+    }
+
+    Ok(cert_store)
+}
+
 fn sni_resolver(
-    server_specs: Vec<config::ServerSpec>,
+    server_specs: &Vec<config::ServerSpec>,
 ) -> Result<Arc<ResolvesServerCertUsingSni>, Box<dyn Error>> {
     let mut resolver = rustls::server::ResolvesServerCertUsingSni::new();
 
     for spec in server_specs {
-        let key = if let Item::PKCS8Key(it) =
-            rustls_pemfile::read_one(&mut spec.private_key.as_bytes())
-                .unwrap()
-                .unwrap()
-        {
-            Ok(PrivateKey(it))
-        } else {
-            Err("Could not load private key")
-        }?;
-
-        let cert = if let Item::X509Certificate(it) =
-            rustls_pemfile::read_one(&mut spec.certificate.as_bytes())
-                .unwrap()
-                .unwrap()
-        {
-            Ok(Certificate(it))
-        } else {
-            Err("Could not load certificate")
-        }?;
+        let key = private_key(&mut spec.private_key.as_bytes())?;
+        let cert = certificate(&mut spec.certificate.as_bytes())?;
 
         let certified_key =
             CertifiedKey::new(vec![cert], Arc::new(RsaSigningKey::new(&key).unwrap()));
@@ -67,6 +68,26 @@ fn sni_resolver(
     }
 
     Ok(Arc::new(resolver))
+}
+
+fn private_key(bytes: &mut dyn io::BufRead) -> Result<PrivateKey, Box<dyn Error>> {
+    Ok(
+        if let Item::PKCS8Key(it) = rustls_pemfile::read_one(bytes).unwrap().unwrap() {
+            Ok(PrivateKey(it))
+        } else {
+            Err("Could not load private key")
+        }?,
+    )
+}
+
+fn certificate(bytes: &mut dyn io::BufRead) -> Result<Certificate, Box<dyn Error>> {
+    Ok(
+        if let Item::X509Certificate(it) = rustls_pemfile::read_one(bytes).unwrap().unwrap() {
+            Ok(Certificate(it))
+        } else {
+            Err("Could not load certificate")
+        }?,
+    )
 }
 
 pub struct IoStream {

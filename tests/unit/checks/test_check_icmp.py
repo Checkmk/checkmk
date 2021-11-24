5f7c9e1c9b29f3acbf1ca99f98de67f5b5db2a39
Jan-Philipp Litza <jpl@plutex.de>
2021-11-24 16:25:06 +0100

    13519 FIX <tt>check_icmp</tt>: Fix "Failed to resolve $: Name or service not known"
    Depending on the configuration, the active check <tt>check_icmp</tt> (ICMP
    Echo Requests) crashed with
    "Failed to resolve $: Name or service not known".
    
    This happened with the following configuration options below "Alternative
    address to ping":
    <ul>
    <li>Ping all IPv4 addresses</li>
    <li>Ping all IPv6 addresses</li>
    <li>Ping additional IPv4 addresses</li>
    <li>Ping additional IPv6 addresses</li>
    <li>Ping IPv4 address identified by its index</li>
    <li>Ping IPv6 address identified by its index</li>
    </ul>
    
    Closes #416
    
    Change-Id: I62e67a4bc89f6cd91b4b58eeba0721370cc50acc

--- tests/unit/checks/test_check_icmp.py
+++ tests/unit/checks/test_check_icmp.py
@@ -4,6 +4,8 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Any, Mapping
+
 import pytest
 
 from tests.testlib import ActiveCheck
@@ -14,10 +16,37 @@ pytestmark = pytest.mark.checks
 @pytest.mark.parametrize(
     "params,expected_args",
     [
-        ({"timeout": 30}, "-t 30 -w 200.00,80% -c 500.00,100% $HOSTADDRESS$"),
+        pytest.param(
+            {"timeout": 30},
+            "-t 30 -w 200.00,80% -c 500.00,100% $HOSTADDRESS$",
+            id="timeout",
+        ),
+        pytest.param(
+            {"address": "all_ipv4addresses"},
+            "-w 200.00,80% -c 500.00,100% $_HOSTADDRESSES_4$ $_HOSTADDRESS_4$",
+            id="all ipv4 addresses",
+        ),
+        pytest.param(
+            {"address": "additional_ipv6addresses"},
+            "-w 200.00,80% -c 500.00,100% -6 $_HOSTADDRESSES_6$",
+            id="additional ipv6 addresses",
+        ),
+        pytest.param(
+            {"address": ("indexed_ipv4address", 1)},
+            "-w 200.00,80% -c 500.00,100% $_HOSTADDRESS_4_1$",
+            id="indexed ipv4 address",
+        ),
+        pytest.param(
+            {"address": ("explicit", "my.custom.address")},
+            "-w 200.00,80% -c 500.00,100% my.custom.address",
+            id="explicit address",
+        ),
     ],
 )
-def test_check_icmp_argument_parsing(params, expected_args):
+def test_check_icmp_argument_parsing(
+    params: Mapping[str, Any],
+    expected_args: str,
+) -> None:
     """Tests if all required arguments are present."""
     active_check = ActiveCheck("check_icmp")
     assert active_check.run_argument_function(params) == expected_args

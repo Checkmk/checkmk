d598ec712662d6025bf636d49cd388404de78b56
Benedikt Seidl <benedikt.seidl@tribe29.com>
2021-11-29 12:41:04 +0100

    remove unused import
    Change-Id: I26f0663a64001f167c48c0cdcab57103c3bd18d9

--- cmk/utils/bi/bi_searcher.py
+++ cmk/utils/bi/bi_searcher.py
@@ -4,7 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Dict, List, Tuple, Optional
+from typing import Dict, List, Tuple
 
 from cmk.utils.bi.bi_lib import ABCBISearcher, BIHostData, BIHostSearchMatch, BIServiceSearchMatch
 from cmk.utils.regex import regex

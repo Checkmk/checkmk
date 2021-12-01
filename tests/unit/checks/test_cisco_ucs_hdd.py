b1ee6d89adb5dceb68c5a30626fba5d5c1bbe9ef
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2021-12-01 15:09:25 +0100

    11825 FIX cisco_ucs_hdd discovery crashed
    Due to a regression, the discovery of cisco_ucs_hdd crashed with:
    <tt>ValueError (too many values to unpack (expected 6))</tt>
    This issue should only be visible in 2.0.0p16 and is fixed now.
    
    SUP-8625
    
    Change-Id: I0e71769e149457acdcf54d6551fc86eca09406f1

---
+++ tests/unit/checks/test_cisco_ucs_hdd.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest  # type: ignore[import]
+from testlib import Check  # type: ignore[import]
+
+from checktestlib import DiscoveryResult, assertDiscoveryResultsEqual
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_cisco_ucs_hdd_parse_and_discovery():
+    info = [
+        ['1', 'HDD', '1', '10L0A089FJPF', '572325', 'TOSHIBA', '1'],
+        ['5', '', '6', '', '0', '', '0'],
+        ['5', '', '9', '', '0', '', '0'],
+    ]
+
+    expected = DiscoveryResult([
+        ('1', None),
+        ('5', None),
+    ])
+
+    check = Check('cisco_ucs_hdd')
+    actual = DiscoveryResult(check.run_discovery(check.run_parse(info)))
+    assertDiscoveryResultsEqual(check, actual=actual, expected=expected)

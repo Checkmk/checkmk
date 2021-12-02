9c413df165c806ba31c9018b8b7837361b0aad02
Lars Michelsen <lm@tribe29.com>
2021-12-02 21:11:44 +0100

    load_plugins_with_exceptions: Enable recursion into packages
    Previously load_plugins_with_exceptions was only able to deal with
    modules in the given module path. Now it's also diving into packages and
    loads the nested modules.
    
    Change-Id: Ice75006f127a6df43a9a63a8d3ed4e8285de73a3

--- cmk/utils/plugin_loader.py
+++ cmk/utils/plugin_loader.py
@@ -35,9 +35,13 @@ def load_plugins_with_exceptions(package_name: str) -> Generator[Tuple[str, Exce
     __import__(package_name)
     package = sys.modules[package_name]
     module_path: List[str] = getattr(package, "__path__", [])
-    for _loader, plugin_name, _is_pkg in pkgutil.walk_packages(module_path):
+    for _loader, plugin_name, is_pkg in pkgutil.walk_packages(module_path):
         try:
-            importlib.import_module("%s.%s" % (package_name, plugin_name))
+            full_name = "%s.%s" % (package_name, plugin_name)
+            importlib.import_module(full_name)
+
+            if is_pkg:
+                yield from load_plugins_with_exceptions(full_name)
         except Exception as exc:
             yield plugin_name, exc
 

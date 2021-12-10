f720e91924f84fb9b0d04e653fd78215e045f715
Konstantin Baikov <konstantin.baikov@tribe29.com>
2021-12-10 16:47:30 +0100

    Fix typos in common special agents
    Change-Id: I81f21696017ca6c18eabed6399cef6d73aff8df7

--- cmk/special_agents/utils/prometheus.py
+++ cmk/special_agents/utils/prometheus.py
@@ -4,7 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 """
-Common functions used in Promtheus related Special agents
+Common functions used in Prometheus related Special agents
 """
 
 from cmk.special_agents.utils.request_helper import (

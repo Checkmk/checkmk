35c34e8da4386067ca314a4942f50a387a41bb27
Ronny Bruska <ronny.bruska@tribe29.com>
2021-12-15 09:47:57 +0100

    13509 FIX Fix impossible host renaming if uninvolved sites have changes
    SUP-8164
    
    Change-Id: Iab5ce347595b48088bf3d1264fe012a15c5849e3

--- cmk/gui/wato/pages/host_rename.py
+++ cmk/gui/wato/pages/host_rename.py
@@ -9,6 +9,7 @@ import socket
 from typing import Optional, Type
 
 from cmk.utils.regex import regex
+from cmk.utils.site import omd_site
 
 import cmk.gui.background_job as background_job
 import cmk.gui.forms as forms
@@ -45,6 +46,7 @@ from cmk.gui.valuespec import (
 )
 from cmk.gui.wato.pages.folders import ModeFolder
 from cmk.gui.wato.pages.hosts import ModeEditHost, page_menu_host_entries
+from cmk.gui.watolib.changes import SiteChanges
 from cmk.gui.watolib.host_rename import perform_rename_hosts
 from cmk.gui.watolib.hosts_and_folders import validate_host_uniqueness
 
@@ -465,9 +467,20 @@ class ModeRenameHost(WatoMode):
         return menu
 
     def action(self) -> ActionResult:
-        if watolib.get_pending_changes_info():
+        local_site = omd_site()
+        renamed_host_site = self._host.site_id()
+        if (
+            SiteChanges(SiteChanges.make_path(local_site)).read()
+            or SiteChanges(SiteChanges.make_path(renamed_host_site)).read()
+        ):
             raise MKUserError(
-                "newname", _("You cannot rename a host while you have pending changes.")
+                "newname",
+                _(
+                    "You cannot rename a host while you have "
+                    "pending changes on the central site (%s) or the "
+                    "site the host is monitored on (%s)."
+                )
+                % (local_site, renamed_host_site),
             )
 
         newname = request.var("newname")

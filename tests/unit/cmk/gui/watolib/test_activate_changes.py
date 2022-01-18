aaa792d9ad9866b80ddc7aea59084d59046f0876
Christoph Rauch <christoph.rauch@tribe29.com>
2022-01-18 09:34:41 +0100

    rules fixture: fix tests 3nd time
    Change-Id: I076202622a64f31a11ba0e2bd38395580bca518e

--- tests/unit/cmk/gui/watolib/test_activate_changes.py
+++ tests/unit/cmk/gui/watolib/test_activate_changes.py
@@ -237,11 +237,23 @@ def test_automation_get_config_sync_state():
                 None,
                 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
             ),
+            "etc/check_mk/mkeventd.d/wato/rules.mk": (
+                33200,
+                147,
+                None,
+                "b129ce86d7aa063c31b0de5062196082744813a1ab506308aae36e5919badc50",
+            ),
             "etc/htpasswd": (
                 33200,
                 0,
                 None,
                 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            ),
+            "etc/omd/site.conf": (
+                33200,
+                683,
+                None,
+                "f549a7f82c6841c886b56d4290c325faa939aa74f37a4b577e46d5789d10d0f5",
             ),
         },
         0,

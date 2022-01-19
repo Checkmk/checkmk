afee3d4fa19abeb9fc65ee83e40a9369d6aef7e0
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-01-19 15:45:20 +0100

    discover receiver port
    Add an unsecured endpoint for port discovery
    
    Change-Id: I35339abe12878663c878208e34ee930bb246870e

--- cmk/gui/wsgi/applications/helper_apps.py
+++ cmk/gui/wsgi/applications/helper_apps.py
@@ -5,11 +5,29 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 from __future__ import annotations
 
+from functools import lru_cache
 from pprint import pformat
 from typing import List, TYPE_CHECKING
 
+from cmk.utils.site import get_omd_config
+
 if TYPE_CHECKING:
     from cmk.gui.wsgi.type_defs import StartResponse, WSGIApplication, WSGIEnvironment, WSGIResponse
+
+
+def discover_receiver(environ: WSGIEnvironment, start_response: StartResponse) -> WSGIResponse:
+    """Report the port of the agent receiver
+
+    We report the agent receivers port on this unprotected URL.
+    We don't give away an information here that an attacker could not find out with a port scan.
+    """
+    return serve_string(f"{_get_receiver_port()}")(environ, start_response)
+
+
+@lru_cache
+def _get_receiver_port() -> int:
+    # make sure we really only ever report a number and nothing more
+    return int(get_omd_config()["CONFIG_AGENT_RECEIVER_PORT"])
 
 
 def dump_environ_app(environ: WSGIEnvironment, start_response: StartResponse) -> WSGIResponse:

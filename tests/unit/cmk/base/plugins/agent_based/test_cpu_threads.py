8572fbb6c4c9380339c9e14da5fa771c6cbcaa34
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-01-26 08:27:21 +0100

    13405 FIX Allow to disable thread levels
    The rule "Number of threads" can set both relative and absolute levels
    for the number of threads. Up until this Werk it was not possible to
    remove said levels completely once they where set.
    
    Now this is possible. For example you can define a base rule matching
    all hosts to check for absolute levels and add another rule matching a
    single host removing the absolute levels from the base rule and only
    check for relative levels. To accomplish that choose "No levels" for the
    absolute levels on the single host.
    
    SUP-9106
    
    Change-Id: I4ef4c893b6a85498fbf881c0003907b6c5df3e5f

--- tests/unit/cmk/base/plugins/agent_based/test_cpu_threads.py
+++ tests/unit/cmk/base/plugins/agent_based/test_cpu_threads.py
@@ -8,8 +8,8 @@ from typing import Any, Dict
 
 import pytest
 
-from cmk.base.api.agent_based.checking_classes import Metric, Result, State
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Service
+from cmk.base.api.agent_based.checking_classes import Metric, Result, Service, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
 from cmk.base.plugins.agent_based.cpu import parse_cpu
 from cmk.base.plugins.agent_based.cpu_threads import check_cpu_threads, discover_cpu_threads
 from cmk.base.plugins.agent_based.utils.cpu import Load, Section, Threads
@@ -45,6 +45,12 @@ def test_cpu_threads_max_threads():
     }
 
 
+STRING_TABLE_RELATIVE: StringTable = [
+    ["0.88", "0.83", "0.87", "2/1748", "21050", "8"],
+    ["124069"],
+]
+
+
 @pytest.mark.parametrize(
     "info, check_result",
     [
@@ -56,7 +62,7 @@ def test_cpu_threads_max_threads():
             },
         ),
         (
-            [["0.88", "0.83", "0.87", "2/1748", "21050", "8"], ["124069"]],
+            STRING_TABLE_RELATIVE,
             {
                 Metric("threads", 1748.0, levels=(2000.0, 4000.0)),
                 Result(state=State.OK, summary="1748"),
@@ -69,6 +75,39 @@ def test_cpu_threads_max_threads():
 def test_cpu_threads_regression(info, check_result):
     section = parse_cpu(info)
     assert section is not None
-    params = {"levels": (2000, 4000)}
+    params = {"levels": ("levels", (2000, 4000))}
     assert list(discover_cpu_threads(section)) == [Service()]
     assert set(check_cpu_threads(params, section)) == check_result
+
+
+@pytest.mark.parametrize(
+    "params, levels",
+    [
+        pytest.param(
+            {},
+            {"thread_usage": (None, None), "threads": (None, None)},
+            id="implicitly no levels set",
+        ),
+        pytest.param(
+            {"levels_percent": "no_levels"},
+            {"thread_usage": (None, None), "threads": (None, None)},
+            id="explicitly unset levels_percent",
+        ),
+        pytest.param(
+            {"levels_percent": ("levels", (10, 20))},
+            {"thread_usage": (10.0, 20.0), "threads": (None, None)},
+            id="levels set",
+        ),
+    ],
+)
+def test_relative_but_no_absolute_levels(
+    params,
+    levels,
+):
+    section = parse_cpu(STRING_TABLE_RELATIVE)  # type: ignore[arg-type]
+    assert section
+    found_levels = {}
+    for element in check_cpu_threads(params=params, section=section):
+        if isinstance(element, Metric):
+            found_levels[element.name] = element.levels
+    assert found_levels == levels

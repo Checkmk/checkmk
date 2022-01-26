356cb3cc48d6cce724e8ebc84046ad0ce69bb184
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-01-26 08:27:21 +0100

    13405 FIX Allow to disable thread levels
    The rule "Number of threads" can set both relative and absolute levels
    for the number of threads. Up until this Werk it was not possible to
    remove said levels completely once they where set.
    
    Now this is possible. For example you can define a base rule matching
    all hosts to check for absolute levels and add another rule matching a
    single host removing the absolute levels from the base rule and only
    check for relative levels. To accomplish that choose "No levels" for the
    absolute levels on the single host.
    
    SUP-9106
    
    Change-Id: I4ef4c893b6a85498fbf881c0003907b6c5df3e5f

---
+++ tests/unit/cmk/base/plugins/agent_based/test_cpu_loads.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.utils.type_defs import CheckPluginName, SectionName
+
+import cmk.base.api.agent_based.register as agent_based_register
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
+
+STRING_TABLE: StringTable = [["0.88", "0.83", "0.87", "2/2148", "21050", "8"]]
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_basic_cpu_loads():
+    agent_section = agent_based_register.get_section_plugin(SectionName("cpu"))
+    plugin = agent_based_register.get_check_plugin(CheckPluginName("cpu_loads"))
+    assert plugin
+
+    section = agent_section.parse_function(STRING_TABLE)  # type: ignore[arg-type]
+    result = list(
+        plugin.check_function(
+            params={"auto-migration-wrapper-key": (5.0, 10.0)},
+            section=section,
+        ))
+    assert result == [
+        Result(state=State.OK, summary="15 min load: 0.87 at 8 cores (0.11 per core)"),
+        Metric("load1", 0.88, levels=(40.0, 80.0), boundaries=(0.0, 8.0)),
+        Metric("load5", 0.83, levels=(40.0, 80.0), boundaries=(0.0, 8.0)),
+        Metric("load15", 0.87, levels=(40.0, 80.0), boundaries=(0.0, 8.0)),
+    ]
---
+++ tests/unit/cmk/base/plugins/agent_based/test_cpu_threads.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.utils.type_defs import CheckPluginName, SectionName
+
+import cmk.base.api.agent_based.register as agent_based_register
+from cmk.base.api.agent_based.checking_classes import CheckPlugin
+from cmk.base.api.agent_based.type_defs import SectionPlugin
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
+
+STRING_TABLE_RELATIVE: StringTable = [
+    # agent output contains the maximum number of threads
+    # so we can also check for thread usage
+    ["0.88", "0.83", "0.87", "2/1748", "21050", "8"],
+    ["124069"],
+]
+
+STRING_TABLE: StringTable = [["0.88", "0.83", "0.87", "2/2148", "21050", "8"]]
+
+
+@pytest.fixture(name="plugin")
+def fixture_plugin(config_load_all_checks):
+    plugin = agent_based_register.get_check_plugin(CheckPluginName("cpu_threads"))
+    assert plugin
+    return plugin
+
+
+@pytest.fixture(name="agent_section")
+def fixture_agent_section(config_load_all_checks):
+    agent_section = agent_based_register.get_section_plugin(SectionName("cpu"))
+    return agent_section
+
+
+def test_basic_cpu_threads_with_absolute_count(
+    plugin: CheckPlugin,
+    agent_section: SectionPlugin,
+) -> None:
+    section = agent_section.parse_function(STRING_TABLE_RELATIVE)  # type: ignore[arg-type]
+    assert list(plugin.check_function(params={}, section=section)) == [
+        Result(state=State.OK, summary="Count: 1748 threads"),
+        Metric("threads", 1748.0),
+        Result(state=State.OK, summary="Usage: 1.41%"),
+        Metric("thread_usage", 1.408893438328672),
+    ]
+
+
+def test_basic_cpu_threads_without_absolute_count(
+    plugin: CheckPlugin,
+    agent_section: SectionPlugin,
+) -> None:
+    section = agent_section.parse_function(STRING_TABLE)  # type: ignore[arg-type]
+    assert list(plugin.check_function(params={}, section=section)) == [
+        Result(state=State.OK, summary="Count: 2148 threads"),
+        Metric("threads", 2148.0),
+    ]
+
+
+@pytest.mark.parametrize(
+    "params, levels",
+    [
+        pytest.param(
+            {},
+            {
+                "thread_usage": (None, None),
+                "threads": (None, None)
+            },
+            id="implicitly no levels set",
+        ),
+        pytest.param(
+            {"levels_percent": "no_levels"},
+            {
+                "thread_usage": (None, None),
+                "threads": (None, None)
+            },
+            id="explicitly unset levels_percent",
+        ),
+        pytest.param(
+            {"levels_percent": ("levels", (10, 20))},
+            {
+                "thread_usage": (10, 20),
+                "threads": (None, None)
+            },
+            id="levels set",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_relative_but_no_absolute_levels(
+    params,
+    levels,
+    plugin: CheckPlugin,
+    agent_section: SectionPlugin,
+):
+    section = agent_section.parse_function(STRING_TABLE_RELATIVE)  # type: ignore[arg-type]
+    found_levels = {}
+    for element in plugin.check_function(params=params, section=section):
+        if isinstance(element, Metric):
+            found_levels[element.name] = element.levels
+    assert found_levels == levels

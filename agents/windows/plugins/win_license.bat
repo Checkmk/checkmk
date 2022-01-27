8af5ca787ddd4d3017547304338584dcf764a96a
LuNie <luk3@outlook.de>
2022-01-27 13:21:38 +0100

    Fix for non english windows installations to force english output.
    - Add fallback if copy fails
    
    Thanks @LuNie
    
    Signed-off-by: Max Linke <max.linke@tribe29.com>
    Change-Id: Ic5f91770d35d43aae87b5efb1b14ab3ca138d607

--- agents/windows/plugins/win_license.bat
+++ agents/windows/plugins/win_license.bat
@@ -5,4 +5,14 @@ REM * plugin to gather and output Windows activation status
 REM ***
 
 echo ^<^<^<win_license^>^>^>
-cscript //NoLogo %windir%/System32/slmgr.vbs /dli
+
+REM create a copy of slmgr.vbs outside the system32 folder to force english text output, because this way the language files can't be found anymore
+copy /Y "%windir%\system32\slmgr.vbs" "%temp%\checkmk_slmgr.vbs" > NUL
+
+if exist "%temp%\checkmk_slmgr.vbs" (
+    cscript //NoLogo "%temp%\checkmk_slmgr.vbs" /dli
+    del "%temp%\checkmk_slmgr.vbs" > NUL
+) else (
+    REM fallback in case temp directory cannot be written to
+    cscript //NoLogo "%windir%\System32\slmgr.vbs" /dli
+)

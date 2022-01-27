ef0d9e94af1796c86de8b8f9395790dc426f7784
LuNie <luk3@outlook.de>
2022-01-27 13:21:38 +0100

    Fix for non english windows installations to force english output.

--- agents/windows/plugins/win_license.bat
+++ agents/windows/plugins/win_license.bat
@@ -5,4 +5,11 @@ REM * plugin to gather and output Windows activation status
 REM ***
 
 echo ^<^<^<win_license^>^>^>
-cscript //NoLogo %windir%/System32/slmgr.vbs /dli
+if not exist "%temp%\slmgr.vbs" (
+    copy "%windir%\system32\slmgr.vbs" "%temp%\slmgr.vbs" > NUL
+)
+
+if exist "%temp%\slmgr.vbs" (
+    cscript //NoLogo "%temp%\slmgr.vbs" /dli
+    del "%temp%\slmgr.vbs" > NUL
+)

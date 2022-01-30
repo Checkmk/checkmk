be42f5204bf29c7e61d62c45efc935f8abbecf28
Lars Michelsen <lm@tribe29.com>
2022-01-30 20:55:54 +0100

    Extend module doc string
    Change-Id: Ib0ff174aedee29f53daa8194b0c4f6a230af4650

--- cmk/utils/password_store.py
+++ cmk/utils/password_store.py
@@ -6,14 +6,34 @@
 """This module is meant to be used by components (e.g. active checks, notifications, bakelets)
 that support getting credentials from the Check_MK password store.
 
-The module needs to be included and then the script needs to run the
-replace_passwords() function. This should be done early in the script
-to make the pwstore option handling transparent for the script.
+The password store mechanic provides a mechanism for keeping passwords out of the cmdline of a
+process, e.g. an active check plugin. It has been built to extend existing plugins with as small
+modificiations as possible. It is built out of two parts:
 
-Do it like this:
+a) Adding arguments for the command line. This job is done for active checks plugins by
+   `cmk.base.core_config._prepare_check_command` and `cmk.base.check_api.passwordstore_get_cmdline`.
+
+b) Extracting arguments from the command line. This is done by `password_store.replace_passwords`
+   for python plugins and for C monitoring plugins by the patches which can be found at
+   `omd/packages/monitoring-plugins/patches/0003-cmk-password-store.dif`.
+
+   The most interesting part is, that the password store arguments are replaced before the existing
+   argument handling of the active check plugins is executed. This way we don't have to deal with
+   the individual mechanics of the active check plugins. We can hook into the entry point of the
+   plugin, do our work and leave the rest to the plugin.
+
+Python active check plugins need to do something like this before the argv are processed.
 
   import cmk.utils.password_store
   cmk.utils.password_store.replace_passwords()
+
+  (... use regular argv processing ...)
+
+For cases where the password ID is not received from the command line, for example a configuration
+file, there is the `extract` function which can be used like this:
+
+  import cmk.utils.password_store
+  password = cmk.utils.password_store.extract("pw_id")
 
 """
 

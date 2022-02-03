0374ea7fe32d23df29e05904c9660bdc603eac81
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-02-03 14:52:49 +0100

    13407 FIX RESTAPI: do not update config generation on get requests
    Before this werk all rest api calls would generate config updates and trigger
    git commits (if git config changes is configured).
    After this werk this is only the case for http methods other than get.
    
    SUP-8793
    
    Change-Id: I8f08d8ff01f0131acb57fd2046622b5aa69e9d7c

--- tests/unit/cmk/gui/plugins/openapi/test_restful_objects.py
+++ tests/unit/cmk/gui/plugins/openapi/test_restful_objects.py
@@ -4,6 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import cmk.gui.plugins.openapi.restful_objects.decorators
 from cmk.gui.plugins.openapi.restful_objects import response_schemas
 from cmk.gui.plugins.openapi.restful_objects.type_defs import StatusCode, StatusCodeInt
 
@@ -84,3 +85,45 @@ def test_domain_object():
 
 def test_status_codes_match():
     assert StatusCodeInt.__args__ == tuple(int(sc) for sc in StatusCode.__args__)  # type: ignore
+
+
+def test_no_config_generation_on_get(
+    wsgi_app,
+    with_automation_user,
+    with_host,
+    monkeypatch,
+    mocker,
+):
+    """
+    update_config_generation should only be called on posts, not on gets: SUP-8793
+    """
+    username, secret = with_automation_user
+    wsgi_app.set_authorization(("Bearer", username + " " + secret))
+    base = "/NO_SITE/check_mk/api/1.0"
+
+    mock = mocker.Mock()
+    monkeypatch.setattr(
+        cmk.gui.plugins.openapi.restful_objects.decorators,
+        "update_config_generation",
+        mock,
+    )
+
+    wsgi_app.call_method(
+        "get",
+        base + "/objects/host_config/heute",
+        status=200,
+        headers={"Accept": "application/json"},
+    )
+    # we have a get request, so we expect update_config not to be called
+    mock.assert_not_called()
+
+    wsgi_app.call_method(
+        "post",
+        base + "/domain-types/host_config/collections/all",
+        params='{"host_name": "foobar", "folder": "/"}',
+        status=200,
+        content_type="application/json",
+        headers={"Accept": "application/json"},
+    )
+    # we have a post request, so we expect update_config to be called
+    mock.assert_called_once()

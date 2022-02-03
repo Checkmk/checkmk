d487e9251ffe60d6f80789022dee8ee776982e64
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-02-03 13:41:14 +0100

    Makeover help texts
    Change-Id: Ib797e628547e3426b004adc26cf58335746cf85a

--- agents/cmk-agent-ctl/src/cli.rs
+++ agents/cmk-agent-ctl/src/cli.rs
@@ -6,41 +6,44 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 pub struct RegistrationArgs {
+    /// Checkmk site to register with
     #[structopt(long, short = "s", parse(from_str))]
     pub server: Option<String>,
 
+    /// API user to use for registration
     #[structopt(long, short = "u", requires = "password", parse(from_str))]
     pub user: Option<String>,
 
+    /// Password for API user
     #[structopt(long, short = "p", requires = "user", parse(from_str))]
     pub password: Option<String>,
 
+    /// Name of this host in the monitoring site
     #[structopt(long, short = "H", parse(from_str))]
     pub host_name: Option<String>,
 
-    /// Blindly trust the server certificate used by the Checkmk instance contacted for registration
+    /// Blindly trust the server certificate of the Checkmk site
     #[structopt(long)]
     pub trust_server_cert: bool,
 }
 
 #[derive(StructOpt)]
 pub struct StatusArgs {
+    /// Write output in JSON format
     #[structopt(long)]
     pub json: bool,
 }
 
 #[derive(StructOpt)]
 pub struct DeleteArgs {
-    /// Connection name or UUID of the connection to be deleted. Connection name can be either
-    /// 'servername:port' or 'imported-...', see the output of the 'status' command.
+    /// The connection to delete
     #[structopt(name = "CONNECTION")]
     pub connection: String,
 }
 
 #[derive(StructOpt)]
 pub struct ImportArgs {
-    /// Path to a file containing the JSON-encoded connection to be imported (output of the
-    /// 'register-surrogate-pull' command). If not provided, the JSON data is expected via stdin.
+    /// The file to import. If not provided, data is read from standard input.
     #[structopt(name = "CONNECTION_FILE")]
     pub conn_file: Option<std::path::PathBuf>,
 
@@ -52,39 +55,65 @@ pub struct ImportArgs {
 #[derive(StructOpt)]
 #[structopt(name = "cmk-agent-ctl", about = "Checkmk agent controller.")]
 pub enum Args {
-    #[structopt(about = "Register with a Checkmk instance for monitoring")]
+    /// Register with a Checkmk site
+    ///
+    /// Register with a Checkmk instance for monitoring. The required information
+    /// can be read from a config file or must be passed via command line.
+    #[structopt()]
     Register(RegistrationArgs),
 
-    #[structopt(about = "Surrogate registration for hosts which cannot register themselves")]
+    /// Register with a Checkmk site on behalf of another host
+    ///
+    /// This allows a surrogate registration for hosts which cannot register themselves.
+    /// The gathered connection information is written to standard output.
+    #[structopt()]
     RegisterSurrogatePull(RegistrationArgs),
 
-    #[structopt(
-        about = "Push monitoring data to all Checkmk instances where this host is registered"
-    )]
+    /// Push monitoring data to all Checkmk sites configured for 'push'
+    ///
+    /// This command will collect monitoring data, send them to all
+    /// Checkmk site configured for 'push' and exit.
+    #[structopt()]
     Push {},
 
-    #[structopt(
-        about = "Handle an incoming connection from a Checkmk instance for collecting monitoring data"
-    )]
+    /// Handle incoming connections from Checkmk sites collecting monitoring data
+    ///
+    /// This command will listen for incoming connections
+    #[structopt()]
     Pull {},
 
-    #[structopt(
-        about = "Run cmk-agent-ctl as a daemon that handles all pull and push connections"
-    )]
+    /// Run as daemon and handle all pull and push connections
+    ///
+    /// Listen for incoming connections (as the 'pull' command does),
+    /// and send data to all Checkmk sites configured for 'push'
+    /// (as the 'push' command does) once a minute.
+    #[structopt()]
     Daemon {},
 
-    #[structopt(about = "Collects monitoring data and prints it to stdout")]
+    /// Collect monitoring data and write it to standard output
+    #[structopt()]
     Dump {},
 
-    #[structopt(about = "Query the registration status of this host")]
+    /// Query the registration status of this host
+    #[structopt()]
     Status(StatusArgs),
 
-    #[structopt(about = "Delete a connection to a Checkmk instance")]
+    /// Delete a connection to a Checkmk instance
+    ///
+    /// Connections can be specified either by their name or their UUID.
+    /// The connections name is '<servername>:<port>' or 'imported-<number>',
+    /// see the output of the 'status' command.
+    #[structopt()]
     Delete(DeleteArgs),
 
-    #[structopt(about = "Delete all connections to Checkmk instances")]
+    /// Delete all connections to Checkmk sites
+    #[structopt()]
     DeleteAll {},
 
-    #[structopt(about = "Import a pull connection, provided either via stdin or from a file")]
+    /// Import a pull connection from file or standard input
+    ///
+    /// A connection is imported from the JSON-encoded connection information.
+    /// A compatible dataset can be created using the 'register-surrogate-pull' command.
+    #[structopt()]
     Import(ImportArgs),
 }

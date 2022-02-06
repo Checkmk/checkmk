a3eac7eb04c5ffaaf9644b9034bc3580d4e6e8d7
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-02-06 20:48:52 +0100

    Add internal windows TCP port to agent controller
    Change-Id: I7b1de8a4cc49a365688d2d2b3a8549936cec12e7

--- agents/cmk-agent-ctl/src/constants.rs
+++ agents/cmk-agent-ctl/src/constants.rs
@@ -8,8 +8,12 @@ pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg(unix)]
 pub const CMK_AGENT_USER: &str = "cmk-agent";
-//TODO: Pass agent port via cmdline or config.
+//TODO: Pass agent port via cmdline(Windows/Linux) or config(Linux).
 pub const AGENT_PORT: &str = "6556";
+
+//TODO(sk): Pass internal  port via cmdline
+#[cfg(windows)]
+pub const WINDOWS_INTERNAL_PORT: &str = "50001";
 
 #[cfg(windows)]
 pub const ENV_PROGRAM_DATA: &str = "ProgramData";

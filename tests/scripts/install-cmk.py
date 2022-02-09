ca10371c716b518e95204d80a56752966c1ff774
Anastasiya Liatsetskaya <anastasiya.liatsetskaya@tribe29.com>
2022-02-09 18:45:50 +0100

    Fixes for pylint:consider-using-with, p12
    CMK-8545
    
    Change-Id: I16c3e54822ce1a1a22525932bc196ee609c5ac0b

--- tests/scripts/install-cmk.py
+++ tests/scripts/install-cmk.py
@@ -190,10 +190,10 @@ class ABCPackageManager(abc.ABC):
         if os.geteuid() != 0:
             cmd.insert(0, "sudo")
 
-        p = subprocess.Popen(  # pylint:disable=consider-using-with
-            cmd, shell=False, close_fds=True, encoding="utf-8"
+        completed_process = subprocess.run(
+            cmd, shell=False, close_fds=True, encoding="utf-8", check=False
         )
-        if p.wait() >> 8 != 0:
+        if completed_process.returncode >> 8 != 0:
             raise Exception("Failed to install package")
 
 

b6d6f20546ccbeb40525c5fd71d6e08bc34178cc
Joerg Herbel <joerg.herbel@tribe29.com>
2022-02-09 09:12:25 +0100

    13521 FIX McAfee AV client agent plugin: Fix <tt>ValueError (time data u'search:' ...)</tt>
    SUP-8313
    
    Change-Id: I6a9e30e0cba3ab2ff41fadf4ccce138c7a84a168

--- agents/windows/plugins/mcafee_av_client.bat
+++ agents/windows/plugins/mcafee_av_client.bat
@@ -11,12 +11,12 @@ set dateval=
 
 rem # on 64 bit systems
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-  for /f "eol=S skip=2 tokens=3" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\McAfee\AvEngine" /v AVDatDate /t REG_SZ 2^>nul') do @set dateval=%%a
+  for /f "skip=2 tokens=3" %%a in ('reg query "HKLM\SOFTWARE\Wow6432Node\McAfee\AvEngine" /v AVDatDate 2^>nul') do @set dateval=%%a
 )
 
 rem # on 32 bit systems
 if "%PROCESSOR_ARCHITECTURE%" == "x86" (
-  for /f "eol=S skip=2 tokens=3" %%a in ('reg query "HKLM\SOFTWARE\McAfee\AvEngine" /v AVDatDate /t REG_SZ 2^>nul') do @set dateval=%%a
+  for /f "skip=2 tokens=3" %%a in ('reg query "HKLM\SOFTWARE\McAfee\AvEngine" /v AVDatDate 2^>nul') do @set dateval=%%a
 )
 
 if not "%dateval%"=="" (

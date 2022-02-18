075801fdb690399605fde76ec2508ad427b6371e
Mathias Laurin <mathias.laurin@tribe29.com>
2022-02-18 08:39:38 +0100

    Revert "Decrypt password store for plugins"
    Breaks on older distros.
    
    This reverts commit a29175681086f1ea276a50e115dc2c5719d78513.

--- omd/packages/monitoring-plugins/cmk_password_store.h
+++ omd/packages/monitoring-plugins/cmk_password_store.h
@@ -1,67 +1,16 @@
 /* Replace passwords in command line from Check_MK password store */
 
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <openssl/ossl_typ.h>
-#include <openssl/evp.h>
-#include <openssl/aes.h>
-#include <openssl/bio.h>
-
-/* Keep these constants in sync with `cmk/utils/encryption.py`. */
-#define cmk_scrypt_N (1 << 14)
-#define cmk_scrypt_r 8
-#define cmk_scrypt_p 1
-#define cmk_scrypt_keylen 32
 
 void cmk_bail_out(const char *reason)
 {
     fprintf(stderr, "Invalid --pwstore= option: %s\n", reason);
     exit(3);
-}
-
-
-typedef struct cmk_header_ctx
-{
-    uint16_t version;
-    unsigned char salt[AES_BLOCK_SIZE];
-    unsigned char iv[AES_BLOCK_SIZE];
-    unsigned char tag[AES_BLOCK_SIZE];
-} cmk_header_ctx;
-
-#define cmk_header_length 50
-
-
-cmk_header_ctx *cmk_header_new(void)
-{
-    return (cmk_header_ctx *)calloc(1, sizeof(cmk_header_ctx));
-}
-
-
-void cmk_header_free(cmk_header_ctx *ctx)
-{
-    free(ctx);
-}
-
-
-ssize_t cmk_header_parse(
-        cmk_header_ctx *ctx,
-        const unsigned char *buffer, size_t buflen)
-{
-    if (buflen < cmk_header_length) {
-        return -1;
-    }
-    // Code uses big endian on the Python side so we cannot simply memcpy.
-    ctx->version += buffer[0] << 8 * 1;
-    ctx->version += buffer[1] << 8 * 0;
-    memcpy(ctx->salt, &buffer[2], AES_BLOCK_SIZE);
-    memcpy(ctx->iv, &buffer[18], AES_BLOCK_SIZE);
-    memcpy(ctx->tag, &buffer[34], AES_BLOCK_SIZE);
-    return 0;
 }
 
 
@@ -100,113 +49,6 @@ err_buf:
     free(buf);
 
 err:
-    errno = 0;
-    return -1;
-}
-
-
-ssize_t cmk_aes_gcm_decrypt(
-        const unsigned char *key,
-        const unsigned char *iv, size_t ivlen,
-        const unsigned char *tag, size_t taglen,
-        const unsigned char *inbuf, size_t inlen,
-        unsigned char *outbuf)
-{
-    /* See also `openssl/demos/cipher/aesgcm.c`. */
-    int outlen = -1;
-    EVP_CIPHER_CTX *ctx = NULL;
-    if ((ctx = EVP_CIPHER_CTX_new()) == NULL) {
-        puts("failed to create cipher context");
-        goto err;
-    }
-    /* Select cipher */
-    if (!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL)) {
-        puts("failed to select the cipher");
-        goto err_ciph;
-    }
-    /* Set key and IV */
-    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, ivlen, NULL)) {
-        puts("failed to set the IV length");
-        goto err_ciph;
-    }
-    /* Set tag */
-    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, taglen, (void *)tag)) {
-        puts("failed to set the tag");
-        goto err_ciph;
-    }
-    if (!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) {
-        puts("failed to set the key and IV");
-        goto err_ciph;
-    }
-    /* Decrypt input */
-    if (!EVP_DecryptUpdate(ctx, outbuf, &outlen, inbuf, inlen)) {
-        puts("failed to decrypt the ciphertext");
-        goto err_ciph;
-    }
-    /* Verify tag */
-    int finlen = 0;
-    int rv = -1;
-    if ((rv = EVP_DecryptFinal_ex(ctx, NULL, &finlen)) <= 0) {
-        puts("failed tag validation");
-        goto err_ciph;
-    }
-
-    EVP_CIPHER_CTX_free(ctx);
-    return outlen + finlen;
-
-err_ciph:
-    EVP_CIPHER_CTX_free(ctx);
-
-err:
-    return -1;
-}
-
-
-/* outbuf is allocated on success */
-ssize_t cmk_decrypt(
-    /* etc/password_store.secret */
-    const char *pass, size_t passlen,
-    /* var/check_mk/stored_passwords */
-    const unsigned char *pwfile, size_t pwfilelen,
-    unsigned char **outbuf)
-{
-    ssize_t outlen = -1;
-    cmk_header_ctx *header = cmk_header_new();
-    if (header == NULL) {
-        puts("failed to allocate header context");
-        goto err;
-    }
-    cmk_header_parse(header, pwfile, pwfilelen);
-
-    unsigned char key[cmk_scrypt_keylen] = {0};
-    EVP_PBE_scrypt(
-            pass, passlen,
-            header->salt, sizeof header->salt,
-            cmk_scrypt_N, cmk_scrypt_r, cmk_scrypt_p, 0,
-            key, sizeof key);
-
-    unsigned char *buf = malloc(pwfilelen);
-    if (buf == NULL) {
-        goto err_header;
-    }
-    outlen = cmk_aes_gcm_decrypt(
-            key, header->iv, sizeof header->iv, header->tag, sizeof header->tag,
-            &pwfile[cmk_header_length], pwfilelen - cmk_header_length, buf);
-    if (outlen <= 0) {
-        goto err_buf;
-    }
-
-    cmk_header_free(header);
-    *outbuf = buf;
-    return outlen;
-
-err_buf:
-    free(buf);
-
-err_header:
-    cmk_header_free(header);
-
-err:
     return -1;
 }
 
@@ -231,44 +73,19 @@ char *cmk_lookup_password(const char *pw_id)
         cmk_bail_out("Cannot open stored_passwords file");
     }
 
-    char pwsecretpath[4096];  /* PATH_MAX from <linux/limits.h> */
-    ok = snprintf(
-        pwsecretpath, sizeof pwsecretpath,
-        "%s/etc/password_store.secret", omd_root);
-    if (ok < 0 || ok >= sizeof pwsecretpath) {
-        cmk_bail_out("passwort_store.secret path too long");
-    }
-    unsigned char *pwsecret = NULL;
-    ssize_t pwsecretlen = cmk_read_file(pwsecretpath, &pwsecret);
-    if (pwsecretlen == -1) {
-        cmk_bail_out("Cannot open password_store.secret file.");
-    }
-
-    unsigned char *text = NULL;
-    ssize_t textlen = -1;
-    if ((textlen = cmk_decrypt(
-            pwsecret, pwsecretlen, pwfile, pwfilelen, &text)) == -1) {
-       cmk_bail_out("Could not decrypt password store");
-    }
-    free(pwfile);
-    free(pwsecret);
-    if (text == NULL) {
-        cmk_bail_out("Could not decrypt password store");
-    }
-
     // The strict aliasing rule has an exception for signed/unsigned types
     // so the cast on the next line is OK.
-    char *line = strtok(strndup((const char *)text, textlen), "\n");
+    char *line = strtok(strndup((const char *)pwfile, pwfilelen), "\n");
     while (line) {
         if (strlen(line) == 0)
             cmk_bail_out("Invalid zero sized line in stored_passwords file.");
         if (strncmp(line, pw_id, strlen(pw_id)) == 0 && line[strlen(pw_id)] == ':') {
-            free(text);
+            free(pwfile);
             return line + strlen(pw_id) + 1;
         }
         line = strtok(NULL, "\n");
     }
-    free(text);
+    free(pwfile);
     return NULL;
 }
 

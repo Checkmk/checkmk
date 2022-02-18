e91b0a320beab87f6dee6e780761493c33745f1a
Joerg Herbel <joerg.herbel@tribe29.com>
2022-02-18 11:27:48 +0100

    agent deployment: unix: fix systemd/xinetd selection
    Change-Id: Ia574dd935d2bf7c31a0d7297e7b07692149f48a0

---
+++ tests/agent-unit/scripts/super-server/test_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+TESTEE="${UNIT_SH_AGENTS_DIR}/scripts/super-server/setup"
+
+
+setUp() {
+    # shellcheck source=../../agents/scripts/super-server/setup
+    MK_SOURCE_ONLY="true" source "$TESTEE"
+    :
+}
+
+test_systemd2_version() {
+    _module_subfolders() {
+        echo "/a/b/c/1_xinetd"
+        echo "/a/b/c/0_systemd"
+    }
+    assertEquals $'systemd\nxinetd' "$(_available_modules)"
+}
+
+# shellcheck disable=SC1090
+. "$UNIT_SH_SHUNIT2"

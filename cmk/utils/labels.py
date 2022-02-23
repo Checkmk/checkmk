6ec29975ded9d6aca00b4efb0753738daa255ba9
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-02-23 23:09:42 +0100

    Use object store
    Change-Id: Id7bd0d707c6759ba0faaec5c91ef827c2c7515f7

--- cmk/utils/labels.py
+++ cmk/utils/labels.py
@@ -6,6 +6,7 @@
 """Helper functions for dealing with Checkmk labels of all kind"""
 
 import os
+from ast import literal_eval
 from typing import Callable, Dict, Final, List, Mapping, Tuple
 
 import cmk.utils.paths
@@ -126,14 +127,12 @@ class LabelManager:
         )
 
 
-class DiscoveredHostLabelsStore:
-    """Managing persistence of discovered labels"""
+class LabelsSerializer:
+    def serialize(self, data: Mapping[str, HostLabelValueDict]) -> bytes:
+        return repr(data).encode("utf-8")
 
-    def __init__(self, hostname: HostName) -> None:
-        self.hostname: Final = hostname
-        self.file_path: Final = cmk.utils.paths.discovered_host_labels_dir / f"{hostname}.mk"
-
-    def load(self) -> Mapping[str, HostLabelValueDict]:
+    @staticmethod
+    def deserialize(raw: bytes) -> Mapping[str, HostLabelValueDict]:
         # Skip labels discovered by the previous HW/SW inventory approach
         # (which was addded+removed in 1.6 beta)
         return {
@@ -141,18 +140,28 @@ class DiscoveredHostLabelsStore:
                 "value": str(val["value"]),
                 "plugin_name": str(val["plugin_name"]) if "plugin_name" in val else None,
             }
-            for key, val in store.load_object_from_file(str(self.file_path), default={}).items()
+            for key, val in literal_eval(raw.decode("utf-8")).items()
             if isinstance(val, dict)
         }
 
-    def save(self, labels: Mapping[str, HostLabelValueDict]) -> None:
-        if not labels:
-            if self.file_path.exists():
-                self.file_path.unlink()
-            return
 
+class DiscoveredHostLabelsStore:
+    """Managing persistence of discovered labels"""
+
+    def __init__(self, hostname: HostName) -> None:
+        super().__init__()
+        self._store = store.ObjectStore(
+            path=cmk.utils.paths.discovered_host_labels_dir / f"{hostname}.mk",
+            serializer=LabelsSerializer(),
+        )
+        self.file_path: Final = self._store.path
+
+    def load(self) -> Mapping[str, HostLabelValueDict]:
+        return self._store.read_obj(default={})
+
+    def save(self, labels: Mapping[str, HostLabelValueDict]) -> None:
         self.file_path.parent.mkdir(parents=True, exist_ok=True)
-        store.save_object_to_file(str(self.file_path), labels)
+        self._store.write_obj(labels)
 
 
 class BuiltinHostLabelsStore:

6aa264992662574e21a18638d517101a95bdd82c
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-02-23 21:18:43 +0100

    shellcheck/format buildscripts I
    Change-Id: I5c46a9a2197f7fa1cdb3a8450493a1367f151ad9

--- buildscripts/docker_image_aliases/update.sh
+++ buildscripts/docker_image_aliases/update.sh
@@ -5,31 +5,32 @@
 # If you plan to migrate this script to another interpreter keep in mind it has to run on bare
 # Jenkins nodes
 
-cd "$(dirname ${BASH_SOURCE[0]})"
+# shellcheck disable=SC2164
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [ -z $@ ]; then
+if [ -z "$*" ]; then
     echo "Provide a list of image alias names or 'all' for all registered images"
     exit 0
 fi
 
-IMAGE_ALIASES=$([ $1 == "all" ] && echo IMAGE_* || echo "$@")
+IMAGE_ALIASES=$([ "$1" = "all" ] && echo IMAGE_* || echo "$@")
 
 for i in $IMAGE_ALIASES; do
     if [ ! -f "$i/meta.yml" ]; then
         echo "$i/meta.yml does not exist please provide existing image aliases only"
+        # shellcheck disable=SC2242 # Can only exit with status 0-255
         exit -1
     fi
 done
-
 
 echo "Update images for: $IMAGE_ALIASES"
 
 for i in $IMAGE_ALIASES; do
     echo "Update image for $i"
-    CMD="./register.py $i $(awk '/source: /{print $2}' < $i/meta.yml)"
-    echo "remove old image alias $(dirname ${BASH_SOURCE[0]})/$i"
-    rm -rf "$(pwd)/$i"
+    CMD="./register.py $i $(awk '/source: /{print $2}' <"$i/meta.yml")"
+    echo "remove old image alias $(dirname "${BASH_SOURCE[0]}")/$i"
+    PWD="$(pwd)"
+    rm -rf "${PWD:?}/$i"
     echo "re-register $i by running $CMD"
     $CMD
 done
-

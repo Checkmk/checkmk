db2ddc042387d608ea142b4eea6296f5d413bff7
Solomon Jacobs <solomon.jacobs@tribe29.com>
2022-02-26 16:28:21 +0100

    kube_pod_status: Add missing container status
    Change-Id: I7d0c92fa581fc02a44a737038708524fc34ef176

--- cmk/gui/plugins/wato/check_parameters/kube_pod_status.py
+++ cmk/gui/plugins/wato/check_parameters/kube_pod_status.py
@@ -22,6 +22,7 @@ CONTAINER_STATUSES = [
     "CrashLoopBackOff",
     "ImagePullBackOff",
     "OOMKilled",
+    "InvalidImageName",
 ]
 
 INIT_STATUSES = [f"Init:{status}" for status in CONTAINER_STATUSES]

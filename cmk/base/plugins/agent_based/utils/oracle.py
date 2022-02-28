395c6128b5f9f59751acc50385550c7039b1e7a4
Lisa Pichler <lisa.pichler@tribe29.com>
2022-02-28 20:50:52 +0100

    oracle utils: add license header
    Change-Id: I8385fd6014e39bcdede628917d588eedba40507e

--- cmk/base/plugins/agent_based/utils/oracle.py
+++ cmk/base/plugins/agent_based/utils/oracle.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 from typing import Any, Dict, List, Mapping, Optional, Tuple, TypedDict
 
 from ..agent_based_api.v1 import State

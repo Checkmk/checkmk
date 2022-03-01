979dd906d396809ad463dab4cab575b12117e1aa
Lukas Lengler <lukas.lengler@tribe29.com>
2022-03-01 08:36:28 +0100

    13740 FIX Veeam: Make veeam_cdp_jobs section optional
    The CDP job option is now optional since not every Veeam instance supports it.
    
    Change-Id: I4038a66a743148497036b870ad9ee7545b0cacdc

--- agents/windows/plugins/veeam_backup_status.ps1
+++ agents/windows/plugins/veeam_backup_status.ps1
@@ -49,9 +49,16 @@ foreach ($tapeJob in $tapeJobs)
     }
 
 
-$myCdpJobsText = "<<<veeam_cdp_jobs:sep(124)>>>`n"
-
+try {
 $cdpjobs = Get-VBRCDPPolicy | select-Object Name,NextRun,PolicyState
+}
+catch {
+write-host "CDP jobs not supported"
+$cdpjobs = $false
+}
+
+if ( $cdpjobs ) {
+$myCdpJobsText = "<<<veeam_cdp_jobs:sep(124)>>>`n"
 
 foreach ($mycdpjobs in $cdpjobs)
 	{
@@ -66,6 +73,7 @@ foreach ($mycdpjobs in $cdpjobs)
 	}
 
 write-host $myCdpJobsText
+}
 
 $myJobsText = "<<<veeam_jobs:sep(9)>>>`n"
 $myTaskText = ""

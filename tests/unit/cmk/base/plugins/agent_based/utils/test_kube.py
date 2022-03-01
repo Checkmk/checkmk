b26fdc8e2cbb855a216a81664bd1ceff53a58688
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-03-01 14:08:10 +0100

    13410 FIX KUBE: adapt kubernetes labels to cmk
    Kubernetes labels can have empty strings as values for labels (for
    example node-role.kubernetes.io/control-plane:).
    In checkmk labels can not have an empty value.
    
    In order to have valid labels in checkmk, empty labels values are replaced
    with true.
    
    Change-Id: I2f79612ba478d7c4c5896f6afd84d3b678fdc5cd

---
+++ tests/unit/cmk/base/plugins/agent_based/utils/test_kube.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import HostLabel
+from cmk.base.plugins.agent_based.utils.k8s import Label, LabelName, Labels
+from cmk.base.plugins.agent_based.utils.kube import kube_labels_to_cmk_labels
+
+
+def test_kube_labels_to_cmk_labels():
+    labels: Labels = {
+        LabelName("asd"): Label(name="asd", value="bsd"),
+        LabelName("empty"): Label(name="empty", value=""),
+    }
+    result = list(kube_labels_to_cmk_labels(labels))
+    assert result == [
+        HostLabel("asd", "bsd"),
+        HostLabel("empty", "true"),
+    ]

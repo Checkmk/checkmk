1fd1f546dce9c56fa94b520a65b3037bcbe9d92c
LuNie <luk3@outlook.de>
2022-03-02 15:59:01 +0100

    Proposed changes implemented

--- agents/windows/plugins/win_license.bat
+++ agents/windows/plugins/win_license.bat
@@ -5,13 +5,14 @@ REM * plugin to gather and output Windows activation status
 REM ***
 
 echo ^<^<^<win_license^>^>^>
-if not exist "%temp%\slmgr.vbs" (
-    copy "%windir%\system32\slmgr.vbs" "%temp%\slmgr.vbs" > NUL
-)
 
-if exist "%temp%\slmgr.vbs" (
-    cscript //NoLogo "%temp%\slmgr.vbs" /dli
-    del "%temp%\slmgr.vbs" > NUL
+REM create a copy of slmgr.vbs outside the system32 folder to force english text output, because this way the language files can't be found anymore
+copy /Y "%windir%\system32\slmgr.vbs" "%temp%\checkmk_slmgr.vbs" > NUL
+
+if exist "%temp%\checkmk_slmgr.vbs" (
+    cscript //NoLogo "%temp%\checkmk_slmgr.vbs" /dli
+    del "%temp%\checkmk_slmgr.vbs" > NUL
 ) else (
+    REM fallback in case temp directory cannot be written to
     cscript //NoLogo "%windir%\System32\slmgr.vbs" /dli
 )

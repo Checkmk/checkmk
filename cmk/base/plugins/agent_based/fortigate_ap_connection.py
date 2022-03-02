ea2e23fdeee2426d69422ef6b77b805cb3802b09
Matthias Hähnel <matthias.haehnel@sixt.com>
2022-03-02 13:29:36 +0100

    sort order - now really
    sort order - now really

--- cmk/base/plugins/agent_based/fortigate_ap_connection.py
+++ cmk/base/plugins/agent_based/fortigate_ap_connection.py
@@ -6,7 +6,7 @@
 
 from typing import List, Mapping, NamedTuple, Tuple, TypedDict
 
-from .agent_based_api.v1 import check_levels, register, startswith, Result, Service, SNMPTree, State
+from .agent_based_api.v1 import check_levels, register, Result, Service, SNMPTree, startswith, State
 from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, StringTable
 
 

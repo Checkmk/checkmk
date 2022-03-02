d891cfffd2b6c15b0418d331e3da0d2fbe3f9518
Patrick Ecke <patrick.ecke@comnetgmbh.com>
2022-03-02 14:36:25 +0100

    Add mysql files

--- cmk/gui/plugins/wato/check_parameters/mysql_db_size.py
+++ cmk/gui/plugins/wato/check_parameters/mysql_db_size.py
@@ -10,7 +10,7 @@ from cmk.gui.plugins.wato.utils import (
     rulespec_registry,
     RulespecGroupCheckParametersApplications,
 )
-from cmk.gui.valuespec import Filesize, Optional, TextInput, Tuple
+from cmk.gui.valuespec import Filesize, Optional, TextInput, Tuple, Integer, Percentage, Checkbox, Dictionary, Transform
 
 
 def _item_spec_mysql_db_size():
@@ -20,19 +20,61 @@ def _item_spec_mysql_db_size():
     )
 
 
+def convert_old_params(params):
+    if not isinstance(params, dict):
+        params = {"mysql_size": params}
+    return params
+
+
 def _parameter_valuespec_mysql_db_size():
-    return Optional(
-        Tuple(
-            elements=[
-                Filesize(title=_("warning at")),
-                Filesize(title=_("critical at")),
-            ],
-        ),
-        help=_(
-            "The check will trigger a warning or critical state if the size of the "
-            "database exceeds these levels."
-        ),
-        title=_("Impose limits on the size of the database"),
+    return Transform(
+        Dictionary(elements=[
+            ("mysql_size",
+             Optional(
+                 Tuple(
+                     elements=[
+                         Filesize(title=_("warning at")),
+                         Filesize(title=_("critical at")),
+                     ],
+                 ),
+                 help=_(
+                     "The check will trigger a warning or critical state if the size of the "
+                     "database exceeds these levels."
+                 ),
+                 title=_("Impose limits on the size of the database"),
+             )),
+            ("trend_range",
+             Optional(Integer(title=_("Time Range for trend computation"),
+                              default_value=24,
+                              minvalue=1,
+                              unit=_("hours")),
+                      title=_("Trend computation"),
+                      label=_("Enable trend computation"))),
+            ("trend_mb",
+             Tuple(title=_("Levels on trends in MB per time range"),
+                   elements=[
+                       Integer(title=_("Warning at"), unit=_("MB / range"), default_value=100),
+                       Integer(title=_("Critical at"), unit=_("MB / range"), default_value=200)
+                   ])),
+            ("trend_perc",
+             Tuple(title=_("Levels for the percentual growth per time range"),
+                   elements=[
+                       Percentage(
+                           title=_("Warning at"),
+                           unit=_("% / range"),
+                           default_value=5,
+                       ),
+                       Percentage(
+                           title=_("Critical at"),
+                           unit=_("% / range"),
+                           default_value=10,
+                       ),
+                   ])),
+            ("trend_perfdata",
+             Checkbox(title=_("Trend performance data"),
+                      label=_("Enable generation of performance data from trends"))),
+        ]),
+        forth=convert_old_params,
     )
 
 

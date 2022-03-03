cd46de636812689b1f0ec310b086d8afce875ecc
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2022-03-03 10:43:13 +0100

    Create and archive a screenhot on failure
    * we also should be able to continue after the first failure
    
    Change-Id: I4c8ade2c2c5717e7c1ec7a97e7798d4a47c6806e

--- tests/testlib/playwright/plugin.py
+++ tests/testlib/playwright/plugin.py
@@ -9,7 +9,8 @@ the pytest-playwright addon's fixtures are too "aggressive" and are loaded in
 all tests. So some functionality is inspierd from this module
 See: https://github.com/microsoft/playwright-pytest
 """
-
+import logging
+import os
 import typing as t
 
 import pytest
@@ -17,10 +18,13 @@ from playwright.sync_api import (
     Browser,
     BrowserContext,
     BrowserType,
+    Error,
     Page,
     Playwright,
     sync_playwright,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="session", name="browser_type_launch_args")
@@ -33,6 +37,13 @@ def _browser_type_launch_args(pytestconfig: t.Any) -> dict:
     if slowmo_option:
         launch_options["slow_mo"] = slowmo_option
     return launch_options
+
+
+def _build_artifact_test_folder(
+    pytestconfig: t.Any, request: pytest.FixtureRequest, folder_or_file_name: str
+) -> str:
+    output_dir = pytestconfig.getoption("--output")
+    return os.path.join(output_dir, request.node.nodeid, folder_or_file_name)
 
 
 @pytest.fixture(scope="session", name="playwright")
@@ -62,9 +73,40 @@ def _context(
     pytestconfig: t.Any,
     request: pytest.FixtureRequest,
 ) -> t.Generator[BrowserContext, None, None]:
+    pages: t.List[Page] = []
     context = browser.new_context()
+    context.on("page", lambda page: pages.append(page))  # pylint: disable=unnecessary-lambda
     yield context
-    context.close()
+    try:
+        _may_create_screenshot(request, pytestconfig, pages)
+    finally:
+        context.close()
+
+
+def _may_create_screenshot(
+    request: pytest.FixtureRequest,
+    pytestconfig: t.Any,
+    pages: t.List[Page],
+):
+    failed = request.node.rep_call.failed if hasattr(request.node, "rep_call") else True
+    screenshot_option = pytestconfig.getoption("--screenshot")
+    capture_screenshot = screenshot_option == "on" or (
+        failed and screenshot_option == "only-on-failure"
+    )
+    if capture_screenshot:
+        # At the moment we're only using one page.
+        # Extend this here as soon we have a use case for multiple pages
+        assert len(pages) == 1
+        page = pages[0]
+
+        human_readable_status = "failed" if failed else "finished"
+        screenshot_path = _build_artifact_test_folder(
+            pytestconfig, request, f"test-{human_readable_status}.png"
+        )
+        try:
+            page.screenshot(timeout=5000, path=screenshot_path)
+        except Error as e:
+            logger.info("Failed to create screenshot of page %s due to: %s", page, e)
 
 
 @pytest.fixture(name="page")
@@ -98,6 +140,20 @@ def _browser_name(pytestconfig: t.Any) -> t.Optional[str]:
     raise NotImplementedError("When using unittest specifying multiple browsers is not supported")
 
 
+# Making test result information available in fixtures
+# https://docs.pytest.org/en/latest/example/simple.html#making-test-result-information-available-in-fixtures
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item: t.Any) -> t.Generator[None, t.Any, None]:
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
 def pytest_addoption(parser: t.Any) -> None:
     group = parser.getgroup("playwright", "Playwright")
     group.addoption(
@@ -118,4 +174,16 @@ def pytest_addoption(parser: t.Any) -> None:
         default=0,
         type=int,
         help="Run tests with slow mo",
+    )
+    group.addoption(
+        "--output",
+        default="test-results",
+        help="Directory for artifacts produced by tests, defaults to test-results.",
+    )
+    group.addoption(
+        "--screenshot",
+        default="off",
+        choices=["on", "off", "only-on-failure"],
+        help="Whether to automatically capture a screenshot after each test. "
+        "If you choose only-on-failure, a screenshot of the failing page only will be created.",
     )

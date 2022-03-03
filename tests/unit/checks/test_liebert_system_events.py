f663a3a152196c0077cc73c7f5a4d4b8d516bb00
Lisa Pichler <lisa.pichler@tribe29.com>
2022-03-03 16:00:08 +0100

    liebert_system_events: additional test case for incactive events
    Change-Id: I494739565543f187d958f6c43c8b012feb9c0f84

--- tests/unit/checks/test_liebert_system_events.py
+++ tests/unit/checks/test_liebert_system_events.py
@@ -96,6 +96,16 @@ def test_discover_liebert_system_events(string_table, discovered_item):
             [(0, "Normal")],
             id="State is OK when there are no events",
         ),
+        pytest.param(
+            {
+                "events": {
+                    "Ambient Air Temperature Sensor Issue": "Inactive Event",
+                    "Supply Fluid Over Temp": "Inactive Event",
+                },
+            },
+            [],
+            id="State is UNKNOWN (implicitly) when there are only incative events",
+        ),
     ],
 )
 def test_check_liebert_system_events(string_table, check_results):

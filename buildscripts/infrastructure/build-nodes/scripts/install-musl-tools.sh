5bec31b49fa449c91c5583d2fb110892dcdf1e34
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-03-04 13:46:51 +0100

    shellcheck/format buildscripts for real
    Change-Id: I15c5d78f9727ea30c72e4e48865cc9d8a3234115

--- buildscripts/infrastructure/build-nodes/scripts/install-musl-tools.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-musl-tools.sh
@@ -6,19 +6,19 @@
 set -e -o pipefail
 
 case "$DISTRO" in
-ubuntu-20.04)
-    echo "Installing for Ubuntu 20.04"
+    ubuntu-20.04)
+        echo "Installing for Ubuntu 20.04"
 
-    apt-get update
-    apt-get install -y musl-tools
-    rm -rf /var/lib/apt/lists/*
+        apt-get update
+        apt-get install -y musl-tools
+        rm -rf /var/lib/apt/lists/*
 
-    exit 0
-    ;;
-*)
-    # We need musl to build a static binary of the agent controller. The agent controller is only
-    # built in the Ubuntu 20.04 image, hence, we only need musl there.
-    echo "ERROR: Unhandled DISTRO: $DISTRO"
-    exit 1
-    ;;
+        exit 0
+        ;;
+    *)
+        # We need musl to build a static binary of the agent controller. The agent controller is only
+        # built in the Ubuntu 20.04 image, hence, we only need musl there.
+        echo "ERROR: Unhandled DISTRO: $DISTRO"
+        exit 1
+        ;;
 esac

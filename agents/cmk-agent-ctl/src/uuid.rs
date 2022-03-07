5fce3290c2ab8f96f1b610681731ad917a3408ca
Joerg Herbel <joerg.herbel@tribe29.com>
2022-03-07 16:07:50 +0100

    agent controller: remove now unused module
    Change-Id: I29aaa8bc9c56e174c9b2d53cf25aa172da7aa7e3

--- agents/cmk-agent-ctl/src/uuid.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
-// This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-// conditions defined in the file COPYING, which is part of this source code package.
-
-use uuid::Uuid;
-
-pub fn make() -> String {
-    return Uuid::new_v4().to_string();
-}

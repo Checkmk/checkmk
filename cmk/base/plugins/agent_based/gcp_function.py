806a4792d7549086527cc5e068a566130e5030dd
Max Linke <max.linke@tribe29.com>
2022-03-07 15:47:04 +0100

    update cloud function check plugin to use assets
    Change-Id: I3b0bb14236509f6be6c69b03bbaa9248f3e6eae4

--- cmk/base/plugins/agent_based/gcp_function.py
+++ cmk/base/plugins/agent_based/gcp_function.py
@@ -3,14 +3,12 @@
 # Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Any, Mapping
+from typing import Any, Mapping, Optional
 
-from cmk.base.plugins.agent_based.agent_based_api.v1 import render
+from cmk.base.plugins.agent_based.agent_based_api.v1 import register, render, Service, ServiceLabel
 from cmk.base.plugins.agent_based.utils import gcp
-from cmk.base.plugins.agent_based.utils.gcp import discover
 
-from .agent_based_api.v1 import register
-from .agent_based_api.v1.type_defs import CheckResult, StringTable
+from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, StringTable
 
 
 def parse_gcp_function(string_table: StringTable) -> gcp.Section:
@@ -20,9 +18,34 @@ def parse_gcp_function(string_table: StringTable) -> gcp.Section:
 register.agent_section(name="gcp_service_cloud_functions", parse_function=parse_gcp_function)
 
 
+def discover(
+    section_gcp_service_cloud_functions: Optional[gcp.Section],
+    section_gcp_assets: Optional[gcp.AssetSection],
+) -> DiscoveryResult:
+    if section_gcp_assets is None:
+        return
+    asset_type = "cloudfunctions.googleapis.com/CloudFunction"
+    functions = [a for a in section_gcp_assets if a.asset.asset_type == asset_type]
+    for function in functions:
+        data = function.asset.resource.data
+        item = data["name"].split("/")[-1]
+        labels = [
+            ServiceLabel("gcp/location", function.asset.resource.location),
+            ServiceLabel("gcp/function/name", item),
+            ServiceLabel("gcp/projectId", section_gcp_assets.project),
+        ]
+        yield Service(item=item, labels=labels)
+
+
 def check_gcp_function_instances(
-    item: str, params: Mapping[str, Any], section: gcp.Section
+    item: str,
+    params: Mapping[str, Any],
+    section_gcp_service_cloud_functions: Optional[gcp.Section],
+    section_gcp_assets: Optional[gcp.AssetSection],
 ) -> CheckResult:
+    if section_gcp_service_cloud_functions is None:
+        return
+    section = section_gcp_service_cloud_functions
     metrics = {
         "faas_total_instance_count": gcp.MetricSpec(
             "cloudfunctions.googleapis.com/function/instance_count", str
@@ -37,7 +60,7 @@ def check_gcp_function_instances(
 
 register.check_plugin(
     name="gcp_function_instances",
-    sections=["gcp_service_cloud_functions"],
+    sections=["gcp_service_cloud_functions", "gcp_assets"],
     service_name="GCP Cloud Function instances %s",
     check_ruleset_name="gcp_function_instances",
     discovery_function=discover,
@@ -47,8 +70,14 @@ register.check_plugin(
 
 
 def check_gcp_function_execution(
-    item: str, params: Mapping[str, Any], section: gcp.Section
+    item: str,
+    params: Mapping[str, Any],
+    section_gcp_service_cloud_functions: Optional[gcp.Section],
+    section_gcp_assets: Optional[gcp.AssetSection],
 ) -> CheckResult:
+    if section_gcp_service_cloud_functions is None:
+        return
+    section = section_gcp_service_cloud_functions
     metrics = {
         # TODO: this is the total. Separate by state
         "faas_execution_count": gcp.MetricSpec(
@@ -70,7 +99,7 @@ def check_gcp_function_execution(
 
 register.check_plugin(
     name="gcp_function_execution",
-    sections=["gcp_service_cloud_functions"],
+    sections=["gcp_service_cloud_functions", "gcp_assets"],
     service_name="GCP Cloud Function execution %s",
     check_ruleset_name="gcp_function_execution",
     discovery_function=discover,
@@ -80,8 +109,14 @@ register.check_plugin(
 
 
 def check_gcp_function_egress(
-    item: str, params: Mapping[str, Any], section: gcp.Section
+    item: str,
+    params: Mapping[str, Any],
+    section_gcp_service_cloud_functions: Optional[gcp.Section],
+    section_gcp_assets: Optional[gcp.AssetSection],
 ) -> CheckResult:
+    if section_gcp_service_cloud_functions is None:
+        return
+    section = section_gcp_service_cloud_functions
     metrics = {
         "net_data_sent": gcp.MetricSpec(
             "cloudfunctions.googleapis.com/function/network_egress", render.bytes
@@ -93,7 +128,7 @@ def check_gcp_function_egress(
 
 register.check_plugin(
     name="gcp_function_egress",
-    sections=["gcp_service_cloud_functions"],
+    sections=["gcp_service_cloud_functions", "gcp_assets"],
     service_name="GCP Cloud Function egress %s",
     check_ruleset_name="gcp_function_egress",
     discovery_function=discover,

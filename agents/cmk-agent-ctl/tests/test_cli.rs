f1c1687e1444124aa3acb25b5df33584900d3a34
Andreas Umbreit <andreas.umbreit@tribe29.com>
2022-03-08 14:45:02 +0100

    cmk-agent-ctl: Introduce common module for integration tests
    A module containing common code used by test cases has to
    go to a seperate folder to not get recognized as a testing module.
    
    Also switching to tokio for tests involving threads/async.
    
    Change-Id: Ieb0676d6750d1b3c5f0bec35942c357c5b3db844

--- agents/cmk-agent-ctl/tests/test_cli.rs
+++ agents/cmk-agent-ctl/tests/test_cli.rs
@@ -1,10 +1,13 @@
 // Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
+
+mod common;
+
+#[cfg(unix)]
 use anyhow::Result as AnyhowResult;
 use assert_cmd::{prelude::OutputAssertExt, Command};
-use predicates::prelude::*;
-use std::io::{BufRead, BufReader, Write};
+use predicates::prelude::predicate;
 
 const BINARY: &str = "cmk-agent-ctl";
 
@@ -18,26 +21,16 @@ fn test_help() {
 }
 
 #[cfg(unix)]
-#[test]
-fn test_dump() -> AnyhowResult<()> {
+#[tokio::test(flavor = "multi_thread")]
+async fn test_dump() -> AnyhowResult<()> {
     let test_dir = tempfile::tempdir().unwrap().into_path();
     std::fs::create_dir(test_dir.join("run"))?;
-    let unix_socket =
-        std::os::unix::net::UnixListener::bind(test_dir.join("run/check-mk-agent.socket"))?;
 
-    let agent_stream_thread = std::thread::spawn(move || -> AnyhowResult<()> {
-        let (unix_stream, _) = unix_socket.accept()?;
-        let mut buf_reader = BufReader::new(unix_stream);
-        let mut buf = String::new();
-        buf_reader.read_line(&mut buf)?;
-
-        assert_eq!(buf, "\n");
-
-        let mut unix_stream = buf_reader.into_inner();
-        unix_stream.write_all(b"some test agent output")?;
-        unix_stream.flush()?;
-        Ok(())
-    });
+    let socket_addr = test_dir.join("run/check-mk-agent.socket");
+    let agent_stream_thread = tokio::spawn(tokio::time::timeout(
+        tokio::time::Duration::from_secs(1),
+        common::unix::agent_socket(socket_addr),
+    ));
 
     let mut cmd = Command::cargo_bin(BINARY)?;
 
@@ -48,7 +41,7 @@ fn test_dump() -> AnyhowResult<()> {
         .assert()
         .stdout(predicate::str::contains("some test agent output"));
 
-    agent_stream_thread.join().unwrap()?;
+    agent_stream_thread.await???;
 
     Ok(())
 }

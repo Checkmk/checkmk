058d855c2188607eb2efe9d5b21ad905ac889aa0
Max Linke <max.linke@tribe29.com>
2022-03-11 11:40:43 +0100

    migrate synology info plugin
    Change-Id: Ib45bafcbd2165b02e1b55ba376a56f0b3b19f792

---
+++ cmk/base/plugins/agent_based/synology_info.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+from dataclasses import dataclass
+from typing import Optional, Sequence
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import (
+    register,
+    Result,
+    Service,
+    SNMPTree,
+    State,
+)
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+    StringTable,
+)
+from cmk.base.plugins.agent_based.utils import synology
+
+
+@dataclass(frozen=True)
+class Section:
+    model: str
+    serialnumber: str
+    os: str
+
+    @classmethod
+    def from_row(cls, row: Sequence[str]) -> "Section":
+        return cls(model=row[0], serialnumber=row[1], os=row[2])
+
+
+def parse(string_table: StringTable) -> Optional[Section]:
+    """
+    >>> assert parse([]) is None
+    >>> assert parse([["model", "SN7", "DSM"]]) == Section(model="model", serialnumber="SN7", os="DSM")
+    """
+    if not string_table:
+        return None
+    return Section.from_row(string_table[0])
+
+
+register.snmp_section(
+    name="synology_info",
+    detect=synology.detect(),
+    parse_function=parse,
+    fetch=SNMPTree(
+        base=".1.3.6.1.4.1.6574.1.5",
+        oids=[
+            "1",  # Model
+            "2",  # SerialNumber
+            "3",  # OS Version
+        ],
+    ),
+)
+
+
+def discovery(section: Section) -> DiscoveryResult:
+    yield Service()
+
+
+def check(section: Section) -> CheckResult:
+    summary = f"Model: {section.model}, S/N: {section.serialnumber}, OS Version: {section.os}"
+    yield Result(state=State.OK, summary=summary)
+
+
+register.check_plugin(
+    name="synology_info",
+    sections=["synology_info"],
+    service_name="Info",
+    discovery_function=discovery,
+    check_function=check,
+)
---
+++ tests/unit/cmk/base/plugins/agent_based/test_synology_info.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+from cmk.base.plugins.agent_based import synology_info
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, State
+
+SECTION_TABLE = [["DS218", "19A0QEN742405", "DSM 7.0-42218"]]
+
+
+def test_parsing():
+    section = synology_info.parse(SECTION_TABLE)
+    assert section == synology_info.Section(
+        model="DS218", serialnumber="19A0QEN742405", os="DSM 7.0-42218"
+    )
+
+
+def test_discovery():
+    section = synology_info.parse(SECTION_TABLE)
+    assert section is not None
+    services = list(synology_info.discovery(section))
+    assert len(services) == 1
+
+
+def test_result_state():
+    section = synology_info.parse(SECTION_TABLE)
+    assert section is not None
+    result = list(synology_info.check(section=section))[0]
+    assert isinstance(result, Result)
+    assert result.state == State.OK
+    assert result.summary == "Model: DS218, S/N: 19A0QEN742405, OS Version: DSM 7.0-42218"

c30f55f8cd7aa216452c19d64e7cb10878dda307
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-03-11 08:52:25 +0100

    make shell unit tests more consistent
     * the make target is now called test-unit-shell
     * all shell unit tests are in tests/unit-shell
     * the runner and the test lib are moved to unit-shell
     * the runner can be executed from anywhere inside the git repo
    
    Change-Id: I8fcf62e4fad9bd32261bfc1475acdf27d77479a3

--- tests/unit-shell/runner.sh
+++ tests/unit-shell/runner.sh
@@ -3,32 +3,29 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-export UNIT_SH_SHUNIT2="./shunit2"
-export UNIT_SH_AGENTS_DIR="../agents"
+_REPO="$(git rev-parse --show-toplevel)"
+
+export UNIT_SH_SHUNIT2="${_REPO}/tests/unit-shell/shunit2"
+export UNIT_SH_AGENTS_DIR="${_REPO}/agents"
 export UNIT_SH_PLUGINS_DIR="$UNIT_SH_AGENTS_DIR/plugins"
 
 _failed_tests=""
 
 run_file() {
-    echo -n "Running $1"
-    if ! OUTPUT=$("$1"); then
-         _failed_tests="$_failed_tests $1"
-         echo -e "\n$OUTPUT"
+    bname="${1##.*tests/unit-shell/}"
+    printf "%s" "${bname}"
+    if ! OUTPUT=$("${1}"); then
+         _failed_tests="$_failed_tests ${bname}"
+         printf "\n%s" "${OUTPUT}"
     else
-        echo "${OUTPUT##*Ran}" | tr '\n.' ' '
-        echo
+        printf "%s" "${OUTPUT##*Ran}" | tr '\n.' ' '
+        printf "\n"
     fi
 }
 
-while IFS= read -r -d '' test_file
-do
+find "${_REPO}/tests/unit-shell" -name "test*.sh"  | while read -r test_file; do
     run_file "${test_file}"
-done <  <(find ./agent-unit -name "test*.sh" -print0)
-
-while IFS= read -r -d '' test_file
-do
-    run_file "${test_file}"
-done <  <(find ./agent-plugin-unit -name "test*.sh" -print0)
+done
 
 if [ -n "$_failed_tests" ]; then
     echo "Failed shell unit tests: $_failed_tests" >&2

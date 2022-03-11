28f6e4bcf71ca96b0f9cf0a2a04ea6155cd49efe
Solomon Jacobs <solomon.jacobs@tribe29.com>
2022-03-11 14:06:44 +0100

    kube_statefulset_info: add plugin
    CMK-9807
    
    Change-Id: I88378d236528cd77bbb1af5948ce8abfaacfd1b9

---
+++ cmk/base/plugins/agent_based/kube_statefulset_info.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import json
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import register, Service
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+    StringTable,
+)
+from cmk.base.plugins.agent_based.utils.k8s import StatefulSetInfo
+from cmk.base.plugins.agent_based.utils.kube_info import check_info
+
+
+def parse(string_table: StringTable) -> StatefulSetInfo:
+    """Parses `string_table` into a StatefulSetInfo instance
+
+    >>> parse([[
+    ... '{"name": "oh-lord",'
+    ... '"namespace": "have-mercy",'
+    ... '"labels": {},'
+    ... '"selector": {"match_labels": {}, "match_expressions": [{"key": "app", "operator": "In", "values": ["sleep"]}]},'
+    ... '"creation_timestamp": 1638798546.0,'
+    ... '"cluster": "sweet-jesus"}'
+    ... ]])
+    StatefulSetInfo(name='oh-lord', namespace='have-mercy', labels={}, selector=Selector(match_labels={}, match_expressions=[{'key': 'app', 'operator': 'In', 'values': ['sleep']}]), creation_timestamp=1638798546.0, cluster='sweet-jesus')
+    """
+    return StatefulSetInfo(**json.loads(string_table[0][0]))
+
+
+register.agent_section(
+    name="kube_statefulset_info_v1",
+    parsed_section_name="kube_statefulset_info",
+    parse_function=parse,
+)
+
+
+def discovery(section: StatefulSetInfo) -> DiscoveryResult:
+    yield Service()
+
+
+def check_kube_statefulset_info(section: StatefulSetInfo) -> CheckResult:
+    yield from check_info(
+        {
+            "name": section.name,
+            "namespace": section.namespace,
+            "creation_timestamp": section.creation_timestamp,
+        }
+    )
+
+
+register.check_plugin(
+    name="kube_statefulset_info",
+    service_name="Info",
+    discovery_function=discovery,
+    check_function=check_kube_statefulset_info,
+)

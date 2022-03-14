6cb77d89ba1f769e4eeed8b3c99a8b09fea24f16
Joerg Herbel <joerg.herbel@tribe29.com>
2022-03-14 18:27:25 +0100

    13762 FIX Ruleset "Cisco quality of service": Add missing title to suboption
    SUP-9674
    
    Change-Id: Ifb8619e4bd8a54b6d61a98cf4bf7f1a2bc3f9453

--- cmk/gui/plugins/wato/check_parameters/cisco_qos.py
+++ cmk/gui/plugins/wato/check_parameters/cisco_qos.py
@@ -4,6 +4,8 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Sequence
+
 from cmk.gui.i18n import _
 from cmk.gui.valuespec import (
     Alternative,
@@ -20,6 +22,44 @@ from cmk.gui.plugins.wato import (
     rulespec_registry,
     RulespecGroupCheckParametersNetworking,
 )
+
+
+def _bandwidth_alternatives() -> Sequence[Tuple]:
+    return [
+        Tuple(
+            title=_("Percentual levels (in relation to policy speed)"),
+            elements=[
+                Percentage(
+                    title=_("Warning at"),
+                    maxvalue=1000,
+                    label=_("% of port speed"),
+                ),
+                Percentage(
+                    title=_("Critical at"),
+                    maxvalue=1000,
+                    label=_("% of port speed"),
+                ),
+            ],
+        ),
+        Tuple(
+            title=_("Absolute levels in bits or bytes per second"),
+            help=_(
+                "Depending on the measurement unit (defaults to bit), the absolute levels are set "
+                "in bits or bytes per second."),
+            elements=[
+                Integer(
+                    title=_("Warning at"),
+                    size=10,
+                    label=_("bits / bytes per second"),
+                ),
+                Integer(
+                    title=_("Critical at"),
+                    size=10,
+                    label=_("bits / bytes per second"),
+                ),
+            ],
+        ),
+    ]
 
 
 def _parameter_valuespec_cisco_qos():
@@ -39,32 +79,7 @@ def _parameter_valuespec_cisco_qos():
              title=_("Used bandwidth (traffic)"),
              help=_("Settings levels on the used bandwidth is optional. If you do set "
                     "levels you might also consider using averaging."),
-             elements=[
-                 Tuple(
-                     title=_("Percentual levels (in relation to policy speed)"),
-                     elements=[
-                         Percentage(title=_("Warning at"),
-                                    maxvalue=1000,
-                                    label=_("% of port speed")),
-                         Percentage(title=_("Critical at"),
-                                    maxvalue=1000,
-                                    label=_("% of port speed")),
-                     ],
-                 ),
-                 Tuple(
-                     title=_("Absolute levels in bits or bytes per second"),
-                     help=
-                     _("Depending on the measurement unit (defaults to bit) the absolute levels are set in bit or byte"
-                      ),
-                     elements=[
-                         Integer(title=_("Warning at"), size=10,
-                                 label=_("bits / bytes per second")),
-                         Integer(title=_("Critical at"),
-                                 size=10,
-                                 label=_("bits / bytes per second")),
-                     ],
-                 )
-             ],
+             elements=_bandwidth_alternatives(),
          )),
         ("average",
          Integer(
@@ -79,26 +94,7 @@ def _parameter_valuespec_cisco_qos():
         ("drop",
          Alternative(
              title=_("Number of dropped bits or bytes per second"),
-             help=_(
-                 "Depending on the measurement unit (defaults to bit) you can set the warn and crit "
-                 "levels for the number of dropped bits or bytes"),
-             elements=[
-                 Tuple(
-                     title=_("Percentual levels (in relation to policy speed)"),
-                     elements=[
-                         Percentage(title=_("Warning at"),
-                                    maxvalue=1000,
-                                    label=_("% of port speed")),
-                         Percentage(title=_("Critical at"),
-                                    maxvalue=1000,
-                                    label=_("% of port speed")),
-                     ],
-                 ),
-                 Tuple(elements=[
-                     Integer(title=_("Warning at"), size=8, label=_("bits / bytes per second")),
-                     Integer(title=_("Critical at"), size=8, label=_("bits / bytes per second")),
-                 ],)
-             ],
+             elements=_bandwidth_alternatives(),
          )),
     ],)
 

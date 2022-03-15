a0aa8d83725af868438eda9d079b9120ef903b12
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-03-15 17:49:04 +0100

    Add to artifacts non-signed version of MSI
    SUP-9627
    
    Change-Id: I87f24b63d79542cf8fc8e59f629dab1ed91e0d35

--- agents/wnx/build_release.cmd
+++ agents/wnx/build_release.cmd
@@ -136,6 +136,7 @@ powershell Write-Host "File Deployment succeeded" -Foreground Green
 !make_exe! msi_patch || powershell Write-Host "Failed to patch MSI exec" -Foreground Red && echo set && exit /b 36
 if not "%2" == "" (
 powershell Write-Host "Signing MSI" -Foreground White
+copy /Y %arte%\check_mk_agent.msi %arte%\check_mk_agent_no_sign.msi
 @call sign_windows_exe c:\common\store\%1 %2 %arte%\check_mk_agent.msi
 )
 

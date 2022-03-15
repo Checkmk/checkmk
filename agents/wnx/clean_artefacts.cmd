9eb8588162036c3defa8a4188df94bab0178b9f6
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-03-15 18:30:28 +0100

    Add non-signed version of MSI to the distro
    SUP-9627
    
    Change-Id: I6f8661d634d5c321659c335c2a7f05c1da942171

--- agents/wnx/clean_artefacts.cmd
+++ agents/wnx/clean_artefacts.cmd
@@ -1,6 +1,7 @@
 @echo off
 if "%arte%" == "" powershell Write-Host "erte not defined" -Foreground Red  && exit /b 1
 del %arte%\check_mk_service.msi 2> nul
+del %arte%\check_mk_service_no_sign.msi 2> nul
 del %arte%\check_mk_agent.msi 2> nul
 del %arte%\check_mk_agent.exe 2> nul
 del %arte%\check_mk_agent-64.exe 2> nul

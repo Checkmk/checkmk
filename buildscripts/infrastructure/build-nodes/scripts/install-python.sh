2d3c9d4a2eed62b5730eb2645cd6d78b8877aa60
Maximilian Wirtz <maximilian.wirtz@tribe29.com>
2022-03-17 08:31:59 +0100

    13725 SEC Update openssl to 1.1.1n
    Change-Id: I2dea766278535f4fa54e5bb9c1e341de62f37ca4

--- buildscripts/infrastructure/build-nodes/scripts/install-python.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-python.sh
@@ -31,14 +31,14 @@ else
     done
 fi
 
-OPENSSL_VERSION=1.1.1l
+OPENSSL_VERSION=1.1.1n
 OPENSSL_PATH="/opt/openssl-${OPENSSL_VERSION}"
 DIR_NAME=Python-${PYTHON_VERSION}
 ARCHIVE_NAME=${DIR_NAME}.tgz
 TARGET_DIR=/opt
 
 # Increase this to enforce a recreation of the build cache
-BUILD_ID=3
+BUILD_ID=4
 
 build_package() {
     mkdir -p /opt/src

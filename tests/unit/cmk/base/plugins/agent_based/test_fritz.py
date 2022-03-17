0f9f18c8f56571cd597b532adffdc8835793b5d1
Joerg Herbel <joerg.herbel@tribe29.com>
2022-03-17 09:03:48 +0100

    fritz checks: add unit tests
    Change-Id: I6b0038352b6823852a41e4936f8ccd9505c05c82

--- tests/unit/cmk/base/plugins/agent_based/test_fritz.py
+++ tests/unit/cmk/base/plugins/agent_based/test_fritz.py
@@ -4,7 +4,32 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.base.plugins.agent_based.fritz import parse_fritz
+import pytest
+
+from testlib import on_time, get_value_store_fixture
+from cmk.utils.type_defs import CheckPluginName
+from cmk.base.api.agent_based import register
+from cmk.base.plugins.agent_based.agent_based_api.v1 import (
+    Metric,
+    Result,
+    Service,
+    State,
+)
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import CheckResult, DiscoveryResult
+from cmk.base.plugins.agent_based.fritz import (
+    check_fritz_wan_if,
+    discover_fritz_wan_if,
+    parse_fritz,
+    Section,
+)
+from cmk.base.plugins.agent_based.utils.interfaces import (
+    CHECK_DEFAULT_PARAMETERS,
+    DISCOVERY_DEFAULT_PARAMETERS,
+)
+
+from cmk.base.plugins.agent_based.utils import interfaces
+
+value_store_fixture = get_value_store_fixture(interfaces)
 
 _STRING_TABLE = [
     ["VersionOS", "137.06.83"],
@@ -67,3 +92,306 @@ _SECTION = {
 
 def test_parse_fritz() -> None:
     assert parse_fritz(_STRING_TABLE) == _SECTION
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [
+                Service(
+                    item="0",
+                    parameters={
+                        "discovered_oper_status": ["1"],
+                        "discovered_speed": 25088000,
+                    },
+                ),
+            ],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [],
+            id="empty data",
+        ),
+    ],
+)
+def test_discover_fritz_wan_if(
+    section: Section,
+    expected_result: DiscoveryResult,
+) -> None:
+    assert (list(discover_fritz_wan_if(
+        [DISCOVERY_DEFAULT_PARAMETERS],
+        section,
+    )) == expected_result)
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [
+                Result(
+                    state=State.OK,
+                    summary="[WAN]",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="(up)",
+                    details="Operational state: up",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="Speed: 25.1 MBit/s",
+                ),
+            ],
+            id="standard case",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("value_store")
+def test_check_fritz_wan_if(
+    section: Section,
+    expected_result: CheckResult,
+) -> None:
+    assert (list(check_fritz_wan_if(
+        "0",
+        CHECK_DEFAULT_PARAMETERS,
+        section,
+    )) == expected_result)
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [Service()],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [],
+            id="empty data",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_discover_fritz_conn(
+    section: Section,
+    expected_result: DiscoveryResult,
+) -> None:
+    plugin = register.get_check_plugin(CheckPluginName("fritz_conn"))
+    assert plugin
+    assert list(plugin.discovery_function(section)) == expected_result
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [
+                Result(
+                    state=State.OK,
+                    summary="Status: Connected",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="WAN IP Address: 217.235.84.223",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="Up since Thu Mar 17 11:07:38 2022, uptime: 0:00:01",
+                ),
+                Metric(
+                    "uptime",
+                    1.0,
+                ),
+            ],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [
+                Result(
+                    state=State.OK,
+                    summary="Status: None",
+                ),
+                Result(
+                    state=State.UNKNOWN,
+                    summary="unhandled connection status",
+                ),
+                Result(
+                    state=State.WARN,
+                    notice="Not connected",
+                ),
+            ],
+            id="empty data",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_fritz_conn(
+    section: Section,
+    expected_result: CheckResult,
+) -> None:
+    plugin = register.get_check_plugin(CheckPluginName("fritz_conn"))
+    assert plugin
+    with on_time(1647515259, "UTC"):
+        assert list(plugin.check_function(
+            params={},
+            section=section,
+        )) == expected_result
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [Service()],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [],
+            id="empty data",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_discover_fritz_config(
+    section: Section,
+    expected_result: DiscoveryResult,
+) -> None:
+    plugin = register.get_check_plugin(CheckPluginName("fritz_config"))
+    assert plugin
+    assert list(plugin.discovery_function(section)) == expected_result
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [
+                Result(
+                    state=State.OK,
+                    summary=
+                    "Auto Disconnect Time: 0, DNS-Server1: 217.237.148.102, DNS-Server2: 217.237.151.115, VoIP-DNS-Server1: 217.237.148.102, VoIP-DNS-Server2: 217.237.151.115, uPnP Config Enabled: 1",
+                ),
+            ],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [
+                Result(
+                    state=State.UNKNOWN,
+                    summary="Configuration info is missing",
+                ),
+            ],
+            id="empty data",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_fritz_config(
+    section: Section,
+    expected_result: CheckResult,
+) -> None:
+    plugin = register.get_check_plugin(CheckPluginName("fritz_config"))
+    assert plugin
+    assert list(plugin.check_function(
+        params={},
+        section=section,
+    )) == expected_result
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [Service()],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [],
+            id="empty data",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_discover_fritz_link(
+    section: Section,
+    expected_result: DiscoveryResult,
+) -> None:
+    plugin = register.get_check_plugin(CheckPluginName("fritz_link"))
+    assert plugin
+    assert list(plugin.discovery_function(section)) == expected_result
+
+
+@pytest.mark.parametrize(
+    [
+        "section",
+        "expected_result",
+    ],
+    [
+        pytest.param(
+            _SECTION,
+            [
+                Result(
+                    state=State.OK,
+                    summary=
+                    "Link Status: Up, Physical Link Status: Up, Link Type: PPPoE, WAN Access Type: DSL",
+                ),
+            ],
+            id="standard case",
+        ),
+        pytest.param(
+            {},
+            [Result(
+                state=State.UNKNOWN,
+                summary="Link info is missing",
+            )],
+            id="empty data",
+        ),
+    ],
+)
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_fritz_link(
+    section: Section,
+    expected_result: CheckResult,
+) -> None:
+    plugin = register.get_check_plugin(CheckPluginName("fritz_link"))
+    assert plugin
+    assert list(plugin.check_function(
+        params={},
+        section=section,
+    )) == expected_result

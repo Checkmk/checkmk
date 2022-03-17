bcd5154ebe551838b8f746bf2032ab5f870a3b53
Joerg Herbel <joerg.herbel@tribe29.com>
2022-03-17 13:17:41 +0100

    fritz checks: Some cosmetic improvements
    Change-Id: Ica172f7cbdafe3c66e2771d106fc9ea1eaecc944

--- cmk/base/plugins/agent_based/fritz.py
+++ cmk/base/plugins/agent_based/fritz.py
@@ -23,17 +23,13 @@ register.agent_section(
 )
 
 
-#
-# WAN Interface Check
-#
+_LINK_STATUS_MAP = {
+    None: "4",
+    "Up": "1",
+}
+
+
 def _section_to_interface(section: Section) -> interfaces.Section:
-    link_stat = section.get("NewLinkStatus")
-    if not link_stat:
-        oper_status = "4"
-    elif link_stat == "Up":
-        oper_status = "1"
-    else:
-        oper_status = "2"
     return [
         interfaces.Interface(
             index="0",
@@ -41,7 +37,10 @@ def _section_to_interface(section: Section) -> interfaces.Section:
             alias="WAN",
             type="6",
             speed=int(section.get("NewLayer1DownstreamMaxBitRate", 0)),
-            oper_status=oper_status,
+            oper_status=_LINK_STATUS_MAP.get(
+                section.get("NewLinkStatus"),
+                "2",
+            ),
             in_octets=int(section.get("NewTotalBytesReceived", 0)),
             out_octets=int(section.get("NewTotalBytesSent", 0)),
         )
@@ -93,44 +92,49 @@ register.check_plugin(
 
 
 def discover_fritz_conn(section: Section) -> DiscoveryResult:
-    conn_stat = section.get("NewConnectionStatus")
-    if conn_stat and conn_stat != "Unconfigured" and "NewExternalIPAddress" in section:
+    if (
+        (conn_stat := section.get("NewConnectionStatus"))
+        and conn_stat != "Unconfigured"
+        and "NewExternalIPAddress" in section
+    ):
         yield Service()
 
 
 def check_fritz_conn(section: Section) -> CheckResult:
-    conn_stat = section.get("NewConnectionStatus")
-    yield Result(
-        state=State.OK,
-        summary="Status: %s" % conn_stat,
-    )
-
-    if conn_stat not in ("Connected", "Connecting", "Disconnected", "Unconfigured"):
-        yield Result(
-            state=State.UNKNOWN,
-            summary="unhandled connection status",
-        )
-
-    if conn_stat == "Connected":
+    if (conn_stat := section.get("NewConnectionStatus")) == "Connected":
         yield Result(
             state=State.OK,
-            summary="WAN IP Address: %s" % section.get("NewExternalIPAddress"),
+            summary=f"Connection status: {conn_stat}",
+        )
+        yield Result(
+            state=State.OK,
+            summary=f"WAN IP Address: {section.get('NewExternalIPAddress', 'unknown')}",
+        )
+    elif conn_stat in {
+        "Connected",
+        "Connecting",
+        "Disconnected",
+        "Unconfigured",
+    }:
+        yield Result(
+            state=State.WARN,
+            summary=f"Connection status: {conn_stat}",
         )
     else:
         yield Result(
-            state=State.WARN,
-            summary="Not connected",
+            state=State.UNKNOWN,
+            summary=f"Connection status: {conn_stat}"
+            if conn_stat
+            else "Got no connection status from device",
         )
 
-    last_err = section.get("NewLastConnectionError")
-    if last_err and last_err != "ERROR_NONE":
+    if (last_err := section.get("NewLastConnectionError")) and last_err != "ERROR_NONE":
         yield Result(
             state=State.OK,
             summary="Last Error: %s" % last_err,
         )
 
-    uptime_str = section.get("NewUptime")
-    if uptime_str:
+    if uptime_str := section.get("NewUptime"):
         yield from uptime.check(
             {},
             uptime.Section(
@@ -154,16 +158,30 @@ def discover_fritz_config(section: Section) -> DiscoveryResult:
         yield Service()
 
 
+_CONFIG_FIELDS = [
+    ("NewAutoDisconnectTime", "Auto-disconnect time"),
+    ("NewDNSServer1", "DNS server 1"),
+    ("NewDNSServer2", "DNS server 2"),
+    ("NewVoipDNSServer1", "VoIP DNS server 1"),
+    ("NewVoipDNSServer2", "VoIP DNS server 2"),
+    ("NewUpnpControlEnabled", "uPnP config enabled"),
+]
+
+_UNCONFIGURED_VALUE = "0.0.0.0"
+
+
 def check_fritz_config(section: Section) -> CheckResult:
-    label_val = [
-        ("Auto Disconnect Time", section.get("NewAutoDisconnectTime", "0.0.0.0")),
-        ("DNS-Server1", section.get("NewDNSServer1", "0.0.0.0")),
-        ("DNS-Server2", section.get("NewDNSServer2", "0.0.0.0")),
-        ("VoIP-DNS-Server1", section.get("NewVoipDNSServer1", "0.0.0.0")),
-        ("VoIP-DNS-Server2", section.get("NewVoipDNSServer2", "0.0.0.0")),
-        ("uPnP Config Enabled", section.get("NewUpnpControlEnabled", "0.0.0.0")),
+    output = [
+        f"{label}: {value}"
+        for key, label in _CONFIG_FIELDS
+        if (
+            value := section.get(
+                key,
+                _UNCONFIGURED_VALUE,
+            )
+        )
+        != _UNCONFIGURED_VALUE
     ]
-    output = ["%s: %s" % (l, v) for l, v in label_val if v != "0.0.0.0"]
     yield (
         Result(
             state=State.OK,
@@ -191,14 +209,16 @@ def discover_fritz_link(section: Section) -> DiscoveryResult:
         yield Service()
 
 
+_LINK_FIELDS = [
+    ("NewLinkStatus", "Link status"),
+    ("NewPhysicalLinkStatus", "Physical link status"),
+    ("NewLinkType", "Link type"),
+    ("NewWANAccessType", "WAN access type"),
+]
+
+
 def check_fritz_link(section: Section) -> CheckResult:
-    label_val = [
-        ("Link Status", section.get("NewLinkStatus")),
-        ("Physical Link Status", section.get("NewPhysicalLinkStatus")),
-        ("Link Type", section.get("NewLinkType")),
-        ("WAN Access Type", section.get("NewWANAccessType")),
-    ]
-    output = ["%s: %s" % (l, v) for l, v in label_val if v]
+    output = [f"{label}: {value}" for key, label in _LINK_FIELDS if (value := section.get(key))]
     yield (
         Result(
             state=State.OK,

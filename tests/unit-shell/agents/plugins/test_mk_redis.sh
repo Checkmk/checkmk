30055084ba174584535f801d28e3430a741bdf54
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-03-18 08:53:56 +0100

    cleanup mk_redis test
    Main change in plugin: Don't do anything if sourced.
    
    Also:
     * We don't need to override the redis-cli and waitmax commands anymore
       (they can be overridden directly anyway).
     * Set the environment in the test only where needed
     * The two test functions where not independent
    
    Change-Id: Ic072f8ea4cb21dd267af1108d4d026a663b58421

--- tests/unit-shell/agents/plugins/test_mk_redis.sh
+++ tests/unit-shell/agents/plugins/test_mk_redis.sh
@@ -3,13 +3,10 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-MK_REDIS_PLUGIN_PATH="$UNIT_SH_PLUGINS_DIR/mk_redis"
+MK_SOURCE_ONLY=true source "${UNIT_SH_PLUGINS_DIR}/mk_redis"
 
 oneTimeSetUp() {
-    MK_CONFDIR=${SHUNIT_TMPDIR}
-    REDIS_CLI_CMD=echo
-    WAITMAX_CMD=echo
-    cat <<EOF >"${MK_CONFDIR}/mk_redis.cfg"
+    cat <<EOF >"${SHUNIT_TMPDIR}/mk_redis.cfg"
 
 REDIS_INSTANCES=(LOCAL IPHOST)
 REDIS_HOST_LOCAL="/var/redis/redis.sock"
@@ -20,23 +17,21 @@ REDIS_PORT_IPHOST="6380"
 REDIS_PASSWORD_IPHOST='MYPASSWORD'
 
 EOF
-    . "$MK_REDIS_PLUGIN_PATH" >/dev/null
 }
 
-test_load_config() {
-    load_config
+test_mk_redis_config() {
+    MK_CONFDIR="${SHUNIT_TMPDIR}" load_config
     assertEquals "/var/redis/redis.sock" "$REDIS_HOST_LOCAL"
     assertEquals "unix-socket" "$REDIS_PORT_LOCAL"
-}
 
-test_redis_args() {
-    redis_args LOCAL
-    assertEquals "-s $REDIS_HOST_LOCAL info" "${REDIS_ARGS[*]}"
-    redis_args IPHOST
-    assertEquals "-h $REDIS_HOST_IPHOST -p $REDIS_PORT_IPHOST -a $REDIS_PASSWORD_IPHOST info" "${REDIS_ARGS[*]}"
+    redis_args "LOCAL"
+    assertEquals "-s /var/redis/redis.sock info" "${REDIS_ARGS[*]}"
+
+    redis_args "IPHOST"
+    assertEquals "-h 127.0.0.1 -p 6380 -a MYPASSWORD info" "${REDIS_ARGS[*]}"
 
     assertEquals "-h" "${REDIS_ARGS[0]}"
-    assertEquals "$REDIS_PASSWORD_IPHOST" "${REDIS_ARGS[5]}"
+    assertEquals "MYPASSWORD" "${REDIS_ARGS[5]}"
 
 }
 

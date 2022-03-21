00570d9601b49049b36138caef74ab358eb7d63e
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-03-21 08:19:29 +0100

    improve typing and modernize
    Change-Id: I42cc2c02e99ac05d045c20ffec70cc4e6b45542b

--- cmk/base/check_legacy_includes/bonding.py
+++ cmk/base/check_legacy_includes/bonding.py
@@ -4,47 +4,58 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-BONDING_CHECK_DEFAULT_PARAMETERS = {
-    "ieee_302_3ad_agg_id_missmatch_state": 1,
-    "expect_active": "ignore",
-}
+from typing import Any, Iterable, Mapping
+
+from cmk.base.plugins.agent_based.utils import bonding
+
+DiscoveryResult = Iterable[tuple[str, dict[str, Any]]]
+CheckResult = Iterable[tuple[int, str]]
 
 
-def inventory_bonding(parsed):
+def _discovery_params(bond: bonding.Bond) -> dict[str, str]:
+    if bond.get("primary", "None") == "None" and (active := bond.get("active", "None")) != "None":
+        return {"primary": active}
+    return {}
+
+
+def discover_bonding(section: bonding.Section) -> DiscoveryResult:
     """Return bonds together with params containing the active interface as primary one in case
     there is no primary interface configured.
-    >>> inventory_bonding({'bond0': {
-    ...     'status': 'up',
-    ...     'mode': 'fault-tolerance',
-    ...     'interfaces': {
-    ...         'eth2': {'status': 'up', 'hwaddr': 'aa:bb:cc:dd:ee:ff', 'failures': 1},
-    ...         'eth3': {'status': 'down', 'hwaddr': '11:22:33:44:55:66', 'failures': 0}},
-    ...     'active': 'eth2',
-    ...     'primary': 'None'}})
+    >>> list(discover_bonding({
+    ...     'bond0': {
+    ...         'status': 'up',
+    ...         'mode': 'fault-tolerance',
+    ...         'interfaces': {
+    ...             'eth2': {'status': 'up', 'hwaddr': 'aa:bb:cc:dd:ee:ff', 'failures': 1},
+    ...             'eth3': {'status': 'down', 'hwaddr': '11:22:33:44:55:66', 'failures': 0}},
+    ...         'active': 'eth2',
+    ...         'primary': 'None',
+    ...     },
+    ... }))
     [('bond0', {'primary': 'eth2'})]
     """
-    return [
-        (bond, params)  #
-        for bond, props in parsed.items()  #
-        if props["status"] in {"up", "degraded"}  #
-        for primary, active in ((props.get("primary", "None"), props.get("active", "None")),)  #
-        for params in ({} if primary != "None" or active == "None" else {"primary": active},)
-    ]
+    yield from (
+        (bond_name, _discovery_params(bond))
+        for bond_name, bond in section.items()
+        if bond["status"] in {"up", "degraded"}
+    )
 
 
-def _check_ieee_302_3ad_specific(params, status):
+def _check_ieee_302_3ad_specific(params: Mapping[str, Any], status: bonding.Bond) -> CheckResult:
     master_id = status.get("aggregator_id")
-    mismatch_state = params["ieee_302_3ad_agg_id_missmatch_state"]
 
     for eth, slave in status["interfaces"].items():
         slave_id = slave["aggregator_id"]
         if master_id is None:
             master_id = slave_id
         if slave_id != master_id:
-            yield mismatch_state, "Mismatching aggregator ID of %s: %s" % (eth, slave_id)
+            yield (
+                params["ieee_302_3ad_agg_id_missmatch_state"],
+                f"Mismatching aggregator ID of {eth}: {slave_id}",
+            )
 
 
-def check_bonding(item, params, parsed):
+def check_bonding(item: str, params: Mapping[str, Any], section: bonding.Section) -> CheckResult:
     """
     >>> for result in check_bonding(
     ...     "bond0", {
@@ -60,18 +71,21 @@ def check_bonding(item, params, parsed):
     ...         'active': 'eth2',
     ...         'primary': 'None'}}):
     ...   print(result)
+    (0, 'Status: up')
     (0, 'Mode: fault-tolerance')
     (0, 'Primary: eth2')
     (0, 'eth2/f8:4f:57:72:11:34 up')
     (1, 'eth3/f8:4f:57:72:11:36 down')
-    (0, 'Bond status: up')
     """
-    properties = parsed.get(item)
-    if not properties:
-        return  # => status: UNKN
+    if (properties := section.get(item)) is None:
+        return
 
-    if properties["status"] not in {"up", "degraded"}:
-        yield 2, "Interface is " + properties["status"]
+    status = properties["status"]
+    yield (
+        {"up": 0, "degraded": 1}.get(status, 2),
+        f"Status: {status}",
+    )
+    if status not in {"up", "degraded"}:
         return
 
     mode = properties["mode"]
@@ -79,14 +93,13 @@ def check_bonding(item, params, parsed):
     if "IEEE 802.3ad" in mode:
         yield from _check_ieee_302_3ad_specific(params, properties)
 
-    speed = properties.get("speed")
-    if speed:
-        yield 0, "Speed: %s" % speed
+    if (speed := properties.get("speed")) is not None:
+        yield 0, f"Speed: {speed}"
 
     current_primary = properties.get("primary", "None")
     primary = current_primary if current_primary != "None" else params.get("primary", "None")
     if primary != "None":
-        yield 0, "Primary: %s" % primary
+        yield 0, f"Primary: {primary}"
 
     expected_active = {
         "primary": primary,
@@ -101,12 +114,16 @@ def check_bonding(item, params, parsed):
         for eth, slave in properties["interfaces"].items():
             state = 0 if slave["status"] == "up" else 1
             if "hwaddr" in slave:
-                yield state, "%s/%s %s" % (eth, slave["hwaddr"], slave["status"])
+                yield state, f"{eth}/{slave['hwaddr']} {slave['status']}"
             else:
-                yield state, "%s %s" % (eth, slave["status"])
+                yield state, f"{eth} {slave['status']}"
     elif expected_active == active_if:
-        yield 0, "Active: %s" % active_if
+        yield 0, f"Active: {active_if}"
     else:
-        yield 1, "Active: %s (expected is %s)" % (active_if, expected_active)
+        yield 1, f"Active: {active_if} (expected is {expected_active})"
 
-    yield 0 if properties["status"] == "up" else 1, "Bond status: %s" % properties["status"]
+
+BONDING_CHECK_DEFAULT_PARAMETERS = {
+    "ieee_302_3ad_agg_id_missmatch_state": 1,
+    "expect_active": "ignore",
+}

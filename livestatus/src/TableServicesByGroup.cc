501042e7ba00a85291ad8e421531d94a47d344da
Sven Panne <sven.panne@tribe29.com>
2022-03-24 10:38:58 +0100

    Tiny tweaks only, making NEB/CMC more similar.
    Change-Id: I9db31a8c000186d052f9a9abc2fede0ba6d2671a

--- livestatus/src/TableServicesByGroup.cc
+++ livestatus/src/TableServicesByGroup.cc
@@ -5,7 +5,10 @@
 
 #include "TableServicesByGroup.h"
 
+#include <optional>
+
 #include "Column.h"
+#include "Logger.h"
 #include "MonitoringCore.h"
 #include "Query.h"
 #include "Row.h"
@@ -46,6 +49,27 @@ void TableServicesByGroup::answerQuery(Query *query) {
                query->processDataset(Row(&sag));
     };
 
+    // If we know the service group, we simply iterate over it.
+    if (auto value = query->stringValueRestrictionFor("groups")) {
+        Debug(logger()) << "using service group index with '" << *value << "'";
+        if (const auto *group =
+                find_servicegroup(const_cast<char *>(value->c_str()))) {
+            if (is_authorized_for_service_group(core()->groupAuthorization(),
+                                                core()->serviceAuthorization(),
+                                                group, query->authUser())) {
+                for (const auto *m = group->members; m != nullptr;
+                     m = m->next) {
+                    if (!process(m->service_ptr, group)) {
+                        return;
+                    }
+                }
+            }
+        }
+        return;
+    }
+
+    // In the general case, we have to process all service groups.
+    Debug(logger()) << "using full table scan";
     for (const auto *group = servicegroup_list; group != nullptr;
          group = group->next) {
         if (is_authorized_for_service_group(core()->groupAuthorization(),

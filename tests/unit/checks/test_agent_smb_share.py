a1b67cebc58184fca2b7ea64e970966f8a265100
Saloni Mordekar <saloni.mordekar@tribe29.com>
2022-03-29 08:51:35 +0200

    New tests
    Change-Id: Ia5ef3799d2722df93542b71e1dabf959cc69d0b4

---
+++ tests/unit/checks/test_agent_smb_share.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from tests.testlib import SpecialAgent
+
+
+def test_agent_smb_share_arguments_password_store() -> None:
+    params = {
+        "authentication": ("user", ("password", "passwd")),
+        "patterns": [],
+    }
+    agent = SpecialAgent("agent_smb_share")
+    assert agent.argument_func(params, "testhost", "1.2.3.4") == [
+        "testhost",
+        "1.2.3.4",
+        "--username",
+        "user",
+        "--password",
+        "passwd",
+    ]

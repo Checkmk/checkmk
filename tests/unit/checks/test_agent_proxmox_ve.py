cdf227510be7fd14364c53876b830bfded9669d5
Konstantin Baikov <konstantin.baikov@tribe29.com>
2022-03-30 11:22:51 +0200

    Use password_store in proxmox ve agent
    Change-Id: I26e7598403b4bc5ed90655ae1798893e3006b7fe

--- tests/unit/checks/test_agent_proxmox_ve.py
+++ tests/unit/checks/test_agent_proxmox_ve.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
@@ -12,8 +11,10 @@ def test_agent_proxmox_ve_arguments_password_store() -> None:
     params = {
         "username": "user",
         "password": ("password", "passwd"),
-        "instance": "test",
-        "protocol": "https",
+        "port": "443",
+        "no-cert-check": True,
+        "timeout": "30",
+        "log-cutoff-weeks": "4",
     }
     agent = SpecialAgent("agent_proxmox_ve")
     assert agent.argument_func(params, "testhost", "1.2.3.4") == [
@@ -21,5 +22,12 @@ def test_agent_proxmox_ve_arguments_password_store() -> None:
         "user",
         "-p",
         "passwd",
+        "--port",
+        "443",
+        "--no-cert-check",
+        "--timeout",
+        "30",
+        "--log-cutoff-weeks",
+        "4",
         "testhost",
     ]

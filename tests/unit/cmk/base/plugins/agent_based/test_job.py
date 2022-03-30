5bf4985abd0ab915cb31a9abe2cbfe84a5470f12
Lukas Lengler <lukas.lengler@tribe29.com>
2022-03-30 10:58:21 +0200

    13742 FIX Crash check job: Cannot render negative timespan
    When the system time is incorrect the start time of a job can be in the future.
    This caused the job check to crash when rendering the age of the job.
    
    Now the absolute value is displayed and a note to check the system time is added.
    
    CMK-10084
    
    Change-Id: Iba2353069f89534cc725621026fdffb57455094f

--- tests/unit/cmk/base/plugins/agent_based/test_job.py
+++ tests/unit/cmk/base/plugins/agent_based/test_job.py
@@ -4,6 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import time
 from copy import copy
 from typing import List, Union
 
@@ -363,6 +364,7 @@ def test_process_job_stats(
                 job_data,
                 age_levels,
                 exit_code_to_state_map,
+                time.time(),
             )
         ) == list(expected_results)
 

21767ed6af341ee2eea96c4dbd8c4590e7c68fbb
Lars Michelsen <lm@tribe29.com>
2022-03-31 08:27:04 +0200

    13643 Bulk host create/update calls now report succeeded and failed hosts
    The REST API calls cmk/bulk_create/invoke and bulk-update/invoke may perform
    only parts of the requested actions in some cases. The endpoints now report
    additional structured information regarding the failed hosts and the cause of
    the issue and the collection of succeeded hosts.
    
    Change-Id: I3e7ae079efc883417fe01bc40fe1eb47e96f08a2

--- cmk/gui/plugins/openapi/endpoints/folder_config.py
+++ cmk/gui/plugins/openapi/endpoints/folder_config.py
@@ -41,7 +41,7 @@ from cmk.gui import fields as gui_fields
 from cmk.gui import watolib
 from cmk.gui.exceptions import MKUserError
 from cmk.gui.http import Response
-from cmk.gui.plugins.openapi.endpoints.host_config import host_collection
+from cmk.gui.plugins.openapi.endpoints.host_config import serve_host_collection
 from cmk.gui.plugins.openapi.endpoints.utils import folder_slug
 from cmk.gui.plugins.openapi.restful_objects import (
     constructors,
@@ -105,7 +105,7 @@ def create(params):
 def hosts_of_folder(params):
     """Show all hosts in a folder"""
     folder: watolib.CREFolder = params["folder"]
-    return host_collection(folder.hosts().values())
+    return serve_host_collection(folder.hosts().values())
 
 
 @Endpoint(

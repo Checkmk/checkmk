6fbc4859c86742e8146debba82a7e4ab075539c4
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-04-01 16:25:37 +0200

    13843 FIX Active discovery check: use cached data
    Change-Id: Iab7728ceb4953e0663130d8307893b1eb63ef66b

--- cmk/base/sources/fetcher_configuration.py
+++ cmk/base/sources/fetcher_configuration.py
@@ -9,9 +9,12 @@ from typing import Any, Dict, Optional
 
 from cmk.utils.type_defs import HostAddress
 
-import cmk.base.core_config as core_config
-from cmk.base.config import HostConfig
+from cmk.core_helpers import FetcherType
 
+import cmk.base.core_config as core_config
+from cmk.base.config import HostConfig, max_cachefile_age
+
+from ._abstract import Source
 from ._checkers import make_sources
 
 __all__ = ["fetchers", "clusters"]
@@ -24,13 +27,27 @@ def get_ip_address(host_config: HostConfig) -> Optional[HostAddress]:
     return core_config.ip_address_of(host_config, socket.AF_INET)
 
 
+def _fixup_caching_info(source: Source) -> Source:
+    if source.fetcher_type is not FetcherType.SNMP:
+        # By default this is MaxAge.none(). For checking this is the same as max_cachefile_age(),
+        # but:
+        # During discovery, the allowed cache age is (by default) 120 seconds, such that the
+        # discovery service won't steal data in the TCP case.
+        # But we do want to see new services, so for SNMP we leave the cache age at zero.
+        # For TCP, we ensure updated caches by triggering the "Check_MK" service whenever the
+        # user manually triggers "Check_MK Discovery", but then use cached data during the actual
+        # discovery
+        source.file_cache_max_age = max_cachefile_age()
+    return source
+
+
 def fetchers(host_config: HostConfig) -> Dict[str, Any]:
     ipaddress = get_ip_address(host_config)
     return {
         "fetchers": [
             {
                 "fetcher_type": c.fetcher_type.name,
-                "fetcher_params": c.fetcher_configuration,
+                "fetcher_params": _fixup_caching_info(c).fetcher_configuration,
             }
             for c in make_sources(host_config, ipaddress)
         ]

624546a73e7d20ce717ddf9aae48f24fe61eeb1e
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-04-04 14:34:05 +0200

    Add support for windows paths for `checkmk_agent_plugins`
    This CL adds possibility to parse correctly data from windows
    agent.
    Windows and Linux stuff is now fully separated in main code.
    
    CMK-10205
    
    Change-Id: I1227e40c6f0f0411630ebc17eecd201243441c8a

--- tests/unit/cmk/base/plugins/agent_based/test_checkmk_agent_plugins.py
+++ tests/unit/cmk/base/plugins/agent_based/test_checkmk_agent_plugins.py
@@ -4,36 +4,82 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from enum import auto, Enum
+from typing import Callable, List
+
+import pytest
+
+from cmk.base.api.agent_based.type_defs import StringTable
 from cmk.base.plugins.agent_based import checkmk_agent_plugins as cap
 from cmk.base.plugins.agent_based.agent_based_api.v1 import TableRow
 from cmk.base.plugins.agent_based.utils import checkmk
 
-_OUTPUT = [
-    ["pluginsdir /usr/lib/check_mk_agent/plugins"],
-    ["localdir /usr/lib/check_mk_agent/local"],
-    ['/usr/lib/check_mk_agent/plugins/mk_filestats.py:__version__ = "2.1.0i1"'],
-    ['/usr/lib/check_mk_agent/local/sync_local_check.sh:CMK_VERSION="3.14.15"'],
-    ['/usr/lib/check_mk_agent/plugins/123/zorp:CMK_VERSION="2.1.0i1"'],
-]
+# TODO(sk): Test must be rewritten fully to be based only on the next info
+# Input consts: main_dir(os) and sep(os)
+# Input fixture: [(filename, plugin|local, subdir|None, VERSION_STRING, VERSION_VALUE),...]
+# Calculations: caching(subdir, os), etc
 
 
-_SECTION = checkmk.PluginSection(
-    plugins=[
-        checkmk.Plugin("mk_filestats.py", "2.1.0i1", 2010010100, None),
-        checkmk.Plugin("zorp", "2.1.0i1", 2010010100, 123),
-    ],
-    local_checks=[
-        checkmk.Plugin("sync_local_check.sh", "3.14.15", 3141550000, None),
+class _OsType(Enum):
+    win = auto()
+    lnx = auto()
+
+
+def _main_dir(os_type: _OsType) -> str:
+    return (
+        "C:\\ProgramData\\checkmk\\agent\\"
+        if os_type == _OsType.win
+        else "/usr/lib/check_mk_agent/"
+    )
+
+
+def _sep(os_type: _OsType) -> str:
+    return "\\" if os_type == _OsType.win else "/"
+
+
+def _agent_output(os_type: _OsType) -> List[List[str]]:
+    main_dir = _main_dir(os_type)
+    sep = _sep(os_type)
+    return [
+        [f"pluginsdir {main_dir}plugins"],
+        [f"localdir {main_dir}local"],
+        [f'{main_dir}plugins{sep}mk_filestats.py:__version__ = "2.1.0i1"'],  # win skips! this entry
+        [f'{main_dir}local{sep}sync_local_check.sh:CMK_VERSION="3.14.15"'],
+        [f'{main_dir}plugins{sep}123{sep}zorp:CMK_VERSION = "2.1.0i1"'],  # 123 caching in lnx
+        [f'{main_dir}plugins{sep}bad_file:XYZ_VERSION = "2.1.0i1"'],  # invalid entry, to be ignored
+    ]
+
+
+def _section(os_type: _OsType) -> checkmk.PluginSection:
+    return checkmk.PluginSection(
+        plugins=[
+            checkmk.Plugin(
+                name,
+                "2.1.0i1",
+                2010010100,
+                123 if os_type == _OsType.lnx and name == "zorp" else None,
+            )
+            for name in (["mk_filestats.py", "zorp"] if os_type == _OsType.lnx else ["zorp"])
+        ],
+        local_checks=[
+            checkmk.Plugin("sync_local_check.sh", "3.14.15", 3141550000, None),
+        ],
+    )
+
+
+@pytest.mark.parametrize(
+    "parser,os_type",
+    [
+        (cap.parse_checkmk_agent_plugins_lnx, _OsType.lnx),
+        (cap.parse_checkmk_agent_plugins_win, _OsType.win),
     ],
 )
+def test_parse_ok_lnx(parser: Callable[[StringTable], checkmk.PluginSection], os_type: _OsType):
+    assert parser(_agent_output(os_type)) == _section(os_type)
 
 
-def test_parse_ok():
-    assert cap.parse_checkmk_agent_plugins_lnx(_OUTPUT) == _SECTION
-
-
-def test_inventory() -> None:
-    assert list(cap.inventory_checkmk_agent_plugins(_SECTION)) == [
+def test_inventory_lnx() -> None:
+    assert list(cap.inventory_checkmk_agent_plugins(_section(_OsType.lnx))) == [
         TableRow(
             path=["software", "applications", "checkmk-agent", "plugins"],
             key_columns={"name": "mk_filestats.py"},
@@ -43,6 +89,21 @@ def test_inventory() -> None:
             path=["software", "applications", "checkmk-agent", "plugins"],
             key_columns={"name": "zorp"},
             inventory_columns={"version": "2.1.0i1", "cache_interval": 123},
+        ),
+        TableRow(
+            path=["software", "applications", "checkmk-agent", "local_checks"],
+            key_columns={"name": "sync_local_check.sh"},
+            inventory_columns={"version": "3.14.15", "cache_interval": None},
+        ),
+    ]
+
+
+def test_inventory_win() -> None:
+    assert list(cap.inventory_checkmk_agent_plugins(_section(_OsType.win))) == [
+        TableRow(
+            path=["software", "applications", "checkmk-agent", "plugins"],
+            key_columns={"name": "zorp"},
+            inventory_columns={"version": "2.1.0i1", "cache_interval": None},
         ),
         TableRow(
             path=["software", "applications", "checkmk-agent", "local_checks"],

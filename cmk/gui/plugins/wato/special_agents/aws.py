d9a6b30906109fa9e654e76511962f9cc694f763
Giordano Tomassorri <giordano.tomassorri@tribe29.com>
2022-04-04 19:44:36 +0200

    14370 Add AWS/CloudFront check
    With this check, you can monitor and add alarms on the following
    metrics for each CloudFront distribution: Requests, Bytes downloaded,
    Bytes uploaded, Total error rate, 4xx error rate, 5xx error rate.
    
    A new service will be created for each CloudFront distribution and you
    can filter the ones to monitor by distribution ID or by AWS tags.
    
    Change-Id: Ie1e22a1d70179768072609aed05ad95db6b26a89

--- cmk/gui/plugins/wato/special_agents/aws.py
+++ cmk/gui/plugins/wato/special_agents/aws.py
@@ -192,6 +192,43 @@ def _valuespec_special_agents_aws():
                                     value=None, totext=_("Monitor Route53"), title=_("Route53")
                                 ),
                             ),
+                            (
+                                "cloudfront",
+                                Dictionary(
+                                    title=_("CloudFront"),
+                                    elements=[
+                                        _vs_element_aws_service_selection(),
+                                        (
+                                            "host_assignment",
+                                            CascadingDropdown(
+                                                title=_("Define Host Assignment"),
+                                                help=_(
+                                                    "Define the host to assign the discovered CloudFront services."
+                                                    " You can assign them to the AWS host as any other AWS service or"
+                                                    " to a piggyback host that will be named as the 'Origin Domain'"
+                                                    " specified in the CloudFront distribution - in case you select the"
+                                                    " second option, you will have to create the piggyback host(s)"
+                                                ),
+                                                choices=[
+                                                    (
+                                                        "aws_host",
+                                                        _("Assign services to AWS host"),
+                                                    ),
+                                                    (
+                                                        "domain_host",
+                                                        _(
+                                                            "Assign services to piggyback host(s) named as the"
+                                                            " 'Origin Domain' configured in AWS"
+                                                        ),
+                                                    ),
+                                                ],
+                                            ),
+                                        ),
+                                    ],
+                                    default_keys=[],
+                                    optional_keys=[],
+                                ),
+                            ),
                         ],
                     ),
                 ),

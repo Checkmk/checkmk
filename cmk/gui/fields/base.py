b229d566fc35fcb4976bb1299bc0a5b9a8b15233
Christoph Rauch <christoph.rauch@tribe29.com>
2022-04-05 17:54:10 +0200

    REST API: doc updates
    Change-Id: Ief0a23fa82e3a7b35241050699c831395f179357

--- cmk/gui/fields/base.py
+++ cmk/gui/fields/base.py
@@ -76,7 +76,16 @@ class BaseSchema(Schema):
 
 
 class FieldWrapper:
-    def __init__(self, field: fields.Field):
+    """Wrapper for marshmallow fields.
+
+    We need this because marshmallow automatically registers fields as attributes on schemas to its
+    `declared_fields` dictionary and removes it from the class's __dict__.
+
+    This is not what we want in some cases. Therefore, we wrap the fields to make sure they are not
+    registered as attributes.
+    """
+
+    def __init__(self, field: fields.Field) -> None:
         self.field = field
 
 

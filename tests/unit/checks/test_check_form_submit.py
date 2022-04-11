ac16b9b621a90ea02332ecad66ae20b015aaabf0
Joerg Herbel <joerg.herbel@tribe29.com>
2022-04-11 12:20:51 +0200

    Cleanups in active check form_submit
    * Switch to argparse
    * Add typehints
    * Remove unused configuration field in GUI
    
    Change-Id: I0c3ab034d737b95247ec0a58559ae42a35cc030a

--- tests/unit/checks/test_check_form_submit.py
+++ tests/unit/checks/test_check_form_submit.py
@@ -4,21 +4,78 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Mapping, Sequence
+
 import pytest
 
 from tests.testlib import ActiveCheck
 
-pytestmark = pytest.mark.checks
-
 
 @pytest.mark.parametrize(
-    "params,expected_args",
+    ["params", "expected_args"],
     [
-        (["foo", {}], ["-I", "$HOSTADDRESS$"]),
-        (["foo", {"port": 80}], ["-p", 80, "-I", "$HOSTADDRESS$"]),
+        pytest.param(
+            (
+                "foo",
+                {},
+            ),
+            [
+                "$HOSTADDRESS$",
+            ],
+            id="minimal configuration",
+        ),
+        pytest.param(
+            (
+                "foo",
+                {"port": 80},
+            ),
+            [
+                "$HOSTADDRESS$",
+                "--port",
+                80,
+            ],
+            id="with port",
+        ),
+        pytest.param(
+            (
+                "foo",
+                {
+                    "hosts": ["12.3.4.51", "some-other-host"],
+                    "uri": "/some/where",
+                    "ssl": True,
+                    "timeout": 5,
+                    "expect_regex": ".*abc",
+                    "form_name": "cool form",
+                    "query": "key1=val1&key2=val2",
+                    "num_succeeded": (1, 0),
+                },
+            ),
+            [
+                "12.3.4.51",
+                "some-other-host",
+                "--uri",
+                "/some/where",
+                "--tls",
+                "--timeout",
+                5,
+                "--expected_regex",
+                ".*abc",
+                "--form_name",
+                "cool form",
+                "--query_params",
+                "key1=val1&key2=val2",
+                "--levels",
+                1,
+                0,
+            ],
+            id="extensive configuration",
+        ),
     ],
 )
-def test_check_form_submit_argument_parsing(params, expected_args):
+def test_check_form_submit_argument_parsing(
+    params: tuple[str, Mapping[str, object]],
+    expected_args: Sequence[object],
+) -> None:
     """Tests if all required arguments are present."""
     active_check = ActiveCheck("check_form_submit")
     assert active_check.run_argument_function(params) == expected_args

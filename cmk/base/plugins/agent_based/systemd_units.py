d2fb6cc976962eb91ac093c0bde1f1a59916eb2f
Max Linke <max.linke@tribe29.com>
2022-04-11 15:00:19 +0200

    Migrate Systemd units check plugin
    Change-Id: I2e6a0c6a44a82f584233a5c1a6b97133275a6bea

--- cmk/base/plugins/agent_based/systemd_units.py
+++ cmk/base/plugins/agent_based/systemd_units.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+import time
+from collections import defaultdict
+from typing import Any, Iterable, Mapping, NamedTuple, Sequence
+
+from .agent_based_api.v1 import (
+    check_levels,
+    get_value_store,
+    regex,
+    register,
+    render,
+    Result,
+    Service,
+    State,
+)
+from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, StringTable
 
 # <<<systemd_units>>>
 #   UNIT                                   LOAD   ACTIVE SUB    DESCRIPTION
@@ -81,11 +96,6 @@
 #             received from its bus interfaces. These are used to manage sets of system
 #             processes that are created externally.
 
-from typing import Iterable, List, NamedTuple, Tuple
-
-CheckResult = Iterable[Tuple[int, str, List[Tuple]]]
-
-
 _SYSTEMD_UNITS = [
     ".service ",
     ".socket ",
@@ -127,40 +137,43 @@ class UnitEntry(NamedTuple):
     enabled_status: str
 
 
-def parse_systemd_units(info):
-    if not info:
+Section = Mapping[str, Mapping[str, UnitEntry]]
+
+
+def parse(string_table: StringTable) -> Section:
+    if not string_table:
         return {}
 
-    iter_info = iter(info)
+    iter_string_table = iter(string_table)
     enabled_status_collection = {}
 
-    line = next(iter_info)
+    line = next(iter_string_table)
 
     if line[0] == "[list-unit-files]":
-        for line in iter_info:
+        for line in iter_string_table:
             if line[0].startswith("["):
                 break
             if len(line) >= 2 and line[1] in _SYSTEMD_UNIT_FILE_STATES:
                 enabled_status_collection[line[0]] = line[1]
 
-    parsed = {}
+    parsed: dict[str, dict[str, UnitEntry]] = defaultdict(dict)
     if line[0] == "[all]":
         try:
-            line = next(iter_info)
+            line = next(iter_string_table)
 
         # no services listed
         except StopIteration:
             return parsed
 
-        for row in iter_info:
+        for row in iter_string_table:
             if row[0] in {"●", "*"}:
                 row.pop(0)
-            line = " ".join(row)
+            joinedline = " ".join(row)
             for unit_marker in _SYSTEMD_UNITS:
                 utype = unit_marker.strip(" ")
                 if row[0].endswith(utype):
                     unit_type = unit_marker.strip(". ")
-                    name, remains = line.split(unit_marker, 1)
+                    name, remains = joinedline.split(unit_marker, 1)
                     if "@" in name:
                         pos = name.find("@")
                         temp = name[: pos + 1]
@@ -180,16 +193,12 @@ def parse_systemd_units(info):
                         descr,
                         enabled_status,
                     )
-                    parsed.setdefault(unit.unit_type, {})[unit.name] = unit
+                    parsed[unit.unit_type][unit.name] = unit
                     break
     return parsed
 
 
-# .
-
-check_info["systemd_units"] = {
-    "parse_function": parse_systemd_units,
-}
+register.agent_section(name="systemd_units", parse_function=parse)
 
 #   .--services------------------------------------------------------------.
 #   |                                     _                                |
@@ -200,38 +209,15 @@ check_info["systemd_units"] = {
 #   |                                                                      |
 #   '----------------------------------------------------------------------'
 
-factory_settings["systemd_services_default_levels"] = {
-    "states": {
-        "active": 0,
-        "inactive": 0,
-        "failed": 2,
-    },
-    "states_default": 2,
-    "else": 2,  # missleading name, used if service vanishes
-}
 
-factory_settings["systemd_services_summary_default_levels"] = {
-    "states": {
-        "active": 0,
-        "inactive": 0,
-        "failed": 2,
-    },
-    "states_default": 2,
-    "activating_levels": (30, 60),
-    "deactivating_levels": (30, 60),
-    "reloading_levels": (30, 60),
-}
-
-discovery_systemd_units_services_rules = []
-
-
-def discovery_systemd_units_services(parsed):
-
+def discovery_systemd_units_services(
+    params: Sequence[Mapping[str, Any]], section: Section
+) -> DiscoveryResult:
     # Filter out volatile systemd service units created by the Checkmk agent which appear and
     # disappear frequently. No matter what the user configures, we do not want to discover them.
     filtered_services = [
         service
-        for service in parsed.get("service", {}).values()
+        for service in section.get("service", {}).values()
         if not regex("^check-mk-agent@.+").match(service.name)
     ]
 
@@ -243,7 +229,7 @@ def discovery_systemd_units_services(parsed):
                 if regex(entry[1:]).match(name):
                     return True
                 continue
-            elif entry == name:
+            if entry == name:
                 return True
         return False
 
@@ -252,7 +238,7 @@ def discovery_systemd_units_services(parsed):
             return True
         return any(s in (None, state) for s in rule_states)
 
-    for settings in host_extra_conf(host_name(), discovery_systemd_units_services_rules):
+    for settings in params:
         descriptions = settings.get("descriptions", [])
         names = settings.get("names", [])
         states = settings.get("states", [])
@@ -262,37 +248,51 @@ def discovery_systemd_units_services(parsed):
                 and regex_match(names, service.name)
                 and state_match(states, service.active_status)
             ):
-                yield service.name, {}
+                yield Service(item=service.name)
 
 
-def check_systemd_units_services(item, params, parsed):
-    services = parsed.get("service", {})
+def check_systemd_units_services(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    services = section.get("service", {})
     service = services.get(item, None)
     if service is None:
-        yield params["else"], "Service not found"
+        yield Result(state=State(params["else"]), summary="Service not found")
         return
 
     state = params["states"].get(service.active_status, params["states_default"])
-    yield state, "Status: %s" % service.active_status
-    yield 0, service.description
+    yield Result(state=State(state), summary=f"Status: {service.active_status}")
+    yield Result(state=State.OK, summary=service.description)
 
 
-check_info["systemd_units.services"] = {
-    "inventory_function": discovery_systemd_units_services,
-    "check_function": check_systemd_units_services,
-    "service_description": "Systemd Service %s",
-    "group": "systemd_services",
-    "default_levels_variable": "systemd_services_default_levels",
-}
+register.check_plugin(
+    name="systemd_units_services",
+    sections=["systemd_units"],
+    service_name="Systemd Service %s",
+    check_ruleset_name="systemd_services",
+    discovery_function=discovery_systemd_units_services,
+    discovery_default_parameters={},
+    discovery_ruleset_name="discovery_systemd_units_services_rules",
+    discovery_ruleset_type=register.RuleSetType.ALL,
+    check_function=check_systemd_units_services,
+    check_default_parameters={
+        "states": {
+            "active": 0,
+            "inactive": 0,
+            "failed": 2,
+        },
+        "states_default": 2,
+        "else": 2,  # missleading name, used if service vanishes
+    },
+)
 
 
-def discovery_systemd_units_services_summary(parsed):
-    if parsed:
-        yield "Summary", {}
+def discovery_systemd_units_services_summary(section: Section) -> DiscoveryResult:
+    yield Service()
 
 
 def _services_split(services, blacklist):
-    services_organised = {
+    services_organised: dict[str, list] = {
         "included": [],
         "excluded": [],
         "disabled": [],
@@ -319,7 +319,8 @@ def _services_split(services, blacklist):
 
 
 def _check_temporary_state(services, params, service_state) -> CheckResult:
-    previous_state = get_item_state(service_state, {})
+    value_store = get_value_store()
+    previous_state = value_store.get(service_state, {})
     now = int(time.time())
     current_state = {}
     levels = params.get("%s_levels" % service_state)
@@ -327,84 +328,76 @@ def _check_temporary_state(services, params, service_state) -> CheckResult:
         state_since = previous_state.get(service.name, now)
         current_state[service.name] = state_since
         elapsed_time = now - state_since
-        yield check_levels(
+        yield from check_levels(
             elapsed_time,
-            None,
-            levels,
-            human_readable_func=get_age_human_readable,
-            infoname="Service '%s' %s for" % (service.name, service_state),
+            levels_upper=levels,
+            render_func=render.timespan,
+            label="Service '%s' %s for" % (service.name, service_state),
+            notice_only=True,
         )
 
-    set_item_state(service_state, current_state)
+    value_store[service_state] = current_state
 
 
-def _check_non_ok_services(systemd_services, params, output_string) -> CheckResult:
-    servicenames_by_status = {}
+def _check_non_ok_services(systemd_services, params, output_string) -> Iterable[Result]:
+    servicenames_by_status: dict[Any, Any] = {}
     for service in systemd_services:
         servicenames_by_status.setdefault(service.active_status, []).append(service.name)
 
     for status, service_names in sorted(servicenames_by_status.items()):
-        state = params["states"].get(status, params["states_default"])
-        if state == 0:
+        state = State(params["states"].get(status, params["states_default"]))
+        if state == State.OK:
             continue
 
         count = len(service_names)
         services_text = ", ".join(sorted(service_names))
         info = output_string % (count, "" if count == 1 else "s", status, services_text)
 
-        yield state, info, []
+        yield Result(state=State(state), summary=info)
 
 
-def check_systemd_units_services_summary(_no_item, params, parsed):
-    services = parsed.get("service", {}).values()
+def check_systemd_units_services_summary(
+    params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    services = section.get("service", {}).values()
     blacklist = params.get("ignored", [])
-    yield 0, "Total: %d" % len(services)
-
+    yield Result(state=State.OK, summary="Total: %d" % len(services))
     services_organised = _services_split(services, blacklist)
-
-    yield 0, "Disabled: %d" % len(services_organised["disabled"])
+    yield Result(state=State.OK, summary="Disabled: %d" % len(services_organised["disabled"]))
     # some of the failed ones might be ignored, so this is OK:
-    yield 0, "Failed: %d" % sum(s.active_status == "failed" for s in services)
-
-    long_output: List[Tuple[int, str, List[Tuple]]] = []
-
+    yield Result(
+        state=State.OK, summary="Failed: %d" % sum(s.active_status == "failed" for s in services)
+    )
     included_template = "%d service%s %s (%s)"
-    for subresult in _check_non_ok_services(
-        services_organised["included"], params, included_template
-    ):
-        if subresult[0]:
-            yield subresult
-        else:
-            long_output.append(subresult)
+    yield from _check_non_ok_services(services_organised["included"], params, included_template)
 
     static_template = "%d static service%s %s (%s)"
-    for subresult in _check_non_ok_services(services_organised["static"], params, static_template):
-        if subresult[0]:
-            yield subresult
-        else:
-            long_output.append(subresult)
+    yield from _check_non_ok_services(services_organised["static"], params, static_template)
 
     for temporary_type in ("activating", "reloading", "deactivating"):
-        for subresult in _check_temporary_state(
+        yield from _check_temporary_state(
             services_organised[temporary_type], params, temporary_type
-        ):
-            if subresult[0]:
-                yield subresult
-            else:
-                long_output.append(subresult)
-
+        )
     if services_organised["excluded"]:
-        yield 0, "\nIgnored: %d" % len(services_organised["excluded"])
-
-    yield from ((s, f"\n{t}", m) for s, t, m in long_output)
+        yield Result(state=State.OK, notice="Ignored: %d" % len(services_organised["excluded"]))
 
 
-check_info["systemd_units.services_summary"] = {
-    "inventory_function": discovery_systemd_units_services_summary,
-    "check_function": check_systemd_units_services_summary,
-    "service_description": "Systemd Service %s",
-    "group": "systemd_services_summary",
-    "default_levels_variable": "systemd_services_summary_default_levels",
-}
-
-# .
+register.check_plugin(
+    name="systemd_units_services_summary",
+    sections=["systemd_units"],
+    discovery_function=discovery_systemd_units_services_summary,
+    check_function=check_systemd_units_services_summary,
+    check_ruleset_name="systemd_services_summary",
+    service_name="Systemd Service Services",
+    check_default_parameters={
+        "states": {
+            "active": 0,
+            "inactive": 0,
+            "failed": 2,
+        },
+        "states_default": 2,
+        "activating_levels": (30, 60),
+        "deactivating_levels": (30, 60),
+        "reloading_levels": (30, 60),
+    },
+)

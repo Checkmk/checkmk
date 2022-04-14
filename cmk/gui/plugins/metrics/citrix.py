ffac28ed5edb1ac250e4201ee3233ca0f9e7f702
Max Linke <max.linke@tribe29.com>
2022-04-14 13:18:21 +0200

    14026 FIX rename license graph
    Change license metric display name
    
    Change-Id: I95a6d7abe423f1792d422815a469cee791343df2

--- cmk/gui/plugins/metrics/citrix.py
+++ cmk/gui/plugins/metrics/citrix.py
@@ -5,11 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.gui.i18n import _
-
-from cmk.gui.plugins.metrics import (
-    metric_info,
-    graph_info,
-)
+from cmk.gui.plugins.metrics import graph_info, metric_info
 
 #.
 #   .--Metrics-------------------------------------------------------------.
@@ -43,17 +39,6 @@ metric_info["citrix_load"] = {
 #   +----------------------------------------------------------------------+
 #   |  Definitions of time series graphs                                   |
 #   '----------------------------------------------------------------------'
-
-graph_info["citrix_licenses"] = {
-    "title": _("Citrix licenses"),
-    "metrics": [("licenses", "area"),],
-    "scalars": [
-        "licenses:warn",
-        "licenses:crit",
-        ("licenses:max#000000", "Installed licenses"),
-    ],
-    "range": (0, "licenses:max"),
-}
 
 graph_info["citrix_serverload"] = {
     "title": _("Citrix Serverload"),
---
+++ cmk/gui/plugins/metrics/licenses.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.gui.i18n import _l
+from cmk.gui.plugins.metrics.utils import graph_info
+
+# .
+#   .--Graphs--------------------------------------------------------------.
+#   |                    ____                 _                            |
+#   |                   / ___|_ __ __ _ _ __ | |__  ___                    |
+#   |                  | |  _| '__/ _` | '_ \| '_ \/ __|                   |
+#   |                  | |_| | | | (_| | |_) | | | \__ \                   |
+#   |                   \____|_|  \__,_| .__/|_| |_|___/                   |
+#   |                                  |_|                                 |
+#   +----------------------------------------------------------------------+
+#   |  Definitions of time series graphs                                   |
+#   '----------------------------------------------------------------------'
+
+graph_info["licenses"] = {
+    "title": _l("Licenses"),
+    "metrics": [("licenses", "area"),],
+    "scalars": [
+        "licenses:warn",
+        "licenses:crit",
+        ("licenses:max#000000", "Installed licenses"),
+    ],
+    "range": (0, "licenses:max"),
+}

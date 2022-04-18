e145a312fc6369e9b4aa441a65dddaaeac7d56e5
Solomon Jacobs <solomon.jacobs@tribe29.com>
2022-04-18 19:11:15 +0200

    transform: improve typing labels
    Change-Id: I47e1968adbba2406fe053f3a1455c6c50206c42e

--- cmk/special_agents/utils_kubernetes/transform.py
+++ cmk/special_agents/utils_kubernetes/transform.py
@@ -90,7 +90,7 @@ def convert_to_timestamp(k8s_date_time: Union[str, datetime.datetime]) -> float:
     return date_time.timestamp()
 
 
-def parse_labels(labels: Mapping[str, str]) -> Optional[Mapping[LabelName, Label]]:
+def parse_labels(labels: Optional[Mapping[str, str]]) -> Optional[Mapping[LabelName, Label]]:
     if labels is None:
         return None
     return {LabelName(k): Label(name=LabelName(k), value=LabelValue(v)) for k, v in labels.items()}

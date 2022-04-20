ded28ecb5e460cff56d8731ebeba4b03fe60735f
Mazen Alkatlabee <mazen.alkatlabee@tribe29.com>
2022-04-20 22:22:50 +0200

    fix ts errors in sites.ts
    Change-Id: I98cf6d5b9a37636bd0eb624f74d90befe084702f

--- web/htdocs/js/modules/sites.ts
+++ web/htdocs/js/modules/sites.ts
@@ -5,12 +5,16 @@
 import * as ajax from "ajax";
 import $ from "jquery";
 
+interface SiteState {
+    livestatus: string;
+    replication: string;
+}
 export function fetch_site_status() {
     ajax.call_ajax("wato_ajax_fetch_site_status.py", {
         response_handler: function (handler_data, response_json) {
             let response = JSON.parse(response_json);
             let success = response.result_code === 0;
-            let site_states = response.result;
+            let site_states: Record<string, SiteState> = response.result;
 
             if (!success) {
                 show_error("Site status update failed: " + site_states);
@@ -18,12 +22,12 @@ export function fetch_site_status() {
             }
 
             for (let [site_id, site_status] of Object.entries(site_states)) {
-                var livestatus_container = document.getElementById("livestatus_status_" + site_id);
+                var livestatus_container = document.getElementById("livestatus_status_" + site_id)!;
                 livestatus_container.innerHTML = site_status.livestatus;
 
                 var replication_container = document.getElementById(
                     "replication_status_" + site_id
-                );
+                )!;
                 replication_container.innerHTML = site_status.replication;
             }
         },
@@ -39,7 +43,7 @@ export function fetch_site_status() {
 
 function show_error(msg) {
     var o = document.getElementById("message_container");
-    o.innerHTML = "<div class=error>" + msg + "</div>";
+    o!.innerHTML = "<div class=error>" + msg + "</div>";
 
     // Remove all loading icons
     $(".reloading").remove();

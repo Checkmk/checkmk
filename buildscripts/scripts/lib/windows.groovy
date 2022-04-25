adcece54692e3b8943f8981b53cf75aa2d0872d3
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-04-25 13:40:56 +0200

    Add win-agent test results to artifacts
    CMK-10634
    
    Change-Id: I38a0e9a2342459e0d0b7b8ed668f7286e68cf7c4

--- buildscripts/scripts/lib/windows.groovy
+++ buildscripts/scripts/lib/windows.groovy
@@ -20,7 +20,7 @@ def build(Map args) {
                     ARTIFACTS = 'python-3.cab,python-3.4.cab'
                 } else if (args.TARGET == "agent_with_sign") {
                     bat 'cd agents\\wnx && call build_release.cmd tribe29.pfx ' + args.PASSWORD
-                    ARTIFACTS = "check_mk_agent-64.exe,check_mk_agent.exe,check_mk_agent.msi,check_mk_agent_unsigned.msi,check_mk.user.yml,check_mk.yml,watest32.exe,watest64.exe"
+                    ARTIFACTS = "check_mk_agent-64.exe,check_mk_agent.exe,check_mk_agent.msi,check_mk_agent_unsigned.msi,check_mk.user.yml,check_mk.yml,watest32.exe,watest64.exe,unit_tests_results.zip"
                 } else if (args.TARGET == "agent_no_sign") {
                     bat 'cd agents\\wnx && call build_release.cmd'
                     ARTIFACTS = "check_mk_agent-64.exe,check_mk_agent.exe,check_mk_agent.msi,check_mk.user.yml,check_mk.yml,watest32.exe,watest64.exe"
@@ -28,8 +28,10 @@ def build(Map args) {
                     bat 'cd agents\\cmk-agent-ctl && call cargo_build.cmd'
                 } else if (args.TARGET == "test_unit") {
                     bat 'cd agents\\wnx && call call_unit_tests.cmd -*_Long:*Integration:*Flaky'
+                    ARTIFACTS = "unit_tests_results.zip"
                 } else if (args.TARGET == "test_integration") {
                     bat 'cd agents\\wnx && call call_integration_tests.cmd'
+                    ARTIFACTS = "integration_tests_results.zip"
                 } else {
                     throw new Exception(args.TARGET + " is not known!")
                 }

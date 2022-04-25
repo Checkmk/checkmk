77947c426bbcb701b4f7dbcaf43560933cf41008
Lukas Lengler <lukas.lengler@tribe29.com>
2022-04-25 09:14:47 +0200

    Migrate nfsmounts and cifsmounts to new Check API
    Part 3: Add perfdata in unittests by introducing value_store fixture
    
    Change-Id: I3f0db8ba4aa19e3e1a1c3befe0b0a7290ea47ddd

--- tests/unit/cmk/base/plugins/agent_based/test_network_fs_mounts.py
+++ tests/unit/cmk/base/plugins/agent_based/test_network_fs_mounts.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Any, Mapping, NamedTuple, Sequence
+from typing import NamedTuple, Sequence
 
 import pytest
 
@@ -11,16 +11,29 @@ from tests.testlib import on_time
 
 from cmk.utils.type_defs import CheckPluginName, SectionName
 
+import cmk.base.item_state
 from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
 from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
     CheckResult,
     DiscoveryResult,
     StringTable,
 )
+from cmk.base.plugins.agent_based.utils.df import FILESYSTEM_DEFAULT_LEVELS
 
 check_name = "nfsmounts"
 
 NOW_SIMULATED = 581792400, "UTC"
+
+
+@pytest.fixture(name="value_store_patch")
+def value_store_fixture(monkeypatch):
+    value_store_patched = {
+        "df.%s.delta" % "/ABCshare": [2000000, 30000000],
+        "df.%s.delta" % "/PERFshare": [2000000, 30000000],
+        "df.%s.delta" % "/var/dbaexport": [2000000, 30000000],
+    }
+    monkeypatch.setattr(cmk.base.item_state, "get_value_store", lambda: value_store_patched)
+    yield value_store_patched
 
 
 # TODO: drop this after migration
@@ -63,12 +76,12 @@ size1 = SizeWithUsage(
     ["491520", "460182", "460182", "65536"],
     491520 * 65536,
     491520 * 65536 - 460182 * 65536,
-    "6.38% used (1.91 of 30.00 GB)",
+    "6.38% used (1.91 of 30.00 GB), trend: 0.00 B / 24 hours",
 )
 
 size2 = SizeBasic(
     ["201326592", "170803720", "170803720", "32768"],
-    "15.16% used (931.48 GB of 6.00 TB)",
+    "15.16% used (931.48 GB of 6.00 TB), trend: 0.00 B / 24 hours",
 )
 
 
@@ -119,13 +132,12 @@ def test_network_fs_mounts_discovery(
 
 
 @pytest.mark.parametrize(
-    "string_table, item, params, check_result",
+    "string_table, item, check_result",
     [
-        ([], "", {}, []),  # no info
+        ([], "", []),  # no info
         (  # single mountpoint with data
             [["/ABCshare", "ok", *size1.info]],
             "/ABCshare",
-            {"has_perfdata": True},
             [
                 Result(state=State.OK, summary=size1.text),
                 Metric(
@@ -135,30 +147,28 @@ def test_network_fs_mounts_discovery(
                     boundaries=(0.0, 32212254720.0),
                 ),
                 Metric("fs_size", 32212254720.0),
+                Metric("fs_growth", -54252.567354853214),
+                Metric("fs_trend", 0.0, boundaries=(0.0, 15534.459259259258)),
             ],
         ),
         (  # two mountpoints with empty data
             [["/AB", "ok", "-", "-", "-", "-"], ["/ABC", "ok", "-", "-", "-", "-"]],
             "/AB",
-            {"has_perfdata": True},
             [Result(state=State.OK, summary="Mount seems OK")],
         ),
         (
             [["/AB", "ok", "-", "-", "-", "-"], ["/ABC", "ok", "-", "-", "-", "-"]],
             "/ABC",
-            {},
             [Result(state=State.OK, summary="Mount seems OK")],
         ),
         (  # Mountpoint with spaces and permission denied
             [["/var/dba", "export", "Permission", "denied"], ["/var/dbaexport", "ok", *size2.info]],
             "/var/dba export",
-            {"has_perfdata": True},
             [Result(state=State.CRIT, summary="Permission denied")],
         ),
         (
             [["/var/dba", "export", "Permission", "denied"], ["/var/dbaexport", "ok", *size2.info]],
             "/var/dbaexport",
-            {"has_perfdata": True},
             [
                 Result(state=State.OK, summary=size2.text),
                 Metric(
@@ -168,12 +178,13 @@ def test_network_fs_mounts_discovery(
                     boundaries=(0.0, 6597069766656.0),
                 ),
                 Metric("fs_size", 6597069766656.0),
+                Metric("fs_growth", -52531.05513336153),
+                Metric("fs_trend", 0.0, boundaries=(0.0, 3181457.256296296)),
             ],
         ),
         (  # with perfdata
             [["/PERFshare", "ok", *size1.info]],
             "/PERFshare",
-            {"has_perfdata": True},
             [
                 Result(state=State.OK, summary=size1.text),
                 Metric(
@@ -183,24 +194,23 @@ def test_network_fs_mounts_discovery(
                     boundaries=(0.0, 32212254720.0),
                 ),
                 Metric("fs_size", 32212254720.0),
+                Metric("fs_growth", -54252.567354853214),
+                Metric("fs_trend", 0.0, boundaries=(0.0, 15534.459259259258)),
             ],
         ),
         (  # state == 'hanging'
             [["/test", "hanging", "hanging", "0", "0", "0", "0"]],
             "/test hanging",
-            {"has_perfdata": True},
             [Result(state=State.CRIT, summary="Server not responding")],
         ),
         (  # unknown state
             [["/test", "unknown", "unknown", "1", "1", "1", "1"]],
             "/test unknown",
-            {"has_perfdata": True},
             [Result(state=State.CRIT, summary="Unknown state: unknown")],
         ),
         (  # zero block size
             [["/test", "perfdata", "ok", "0", "460182", "460182", "0"]],
             "/test perfdata",
-            {"has_perfdata": True},
             [Result(state=State.CRIT, summary="Stale fs handle")],
         ),
     ],
@@ -208,11 +218,18 @@ def test_network_fs_mounts_discovery(
 def test_network_fs_mounts_check(
     string_table: StringTable,
     item: str,
-    params: Mapping[str, Any],
     check_result: CheckResult,
     parse_network_fs_mounts,
     check_network_fs_mount,
+    value_store_patch,
 ):
     section = parse_network_fs_mounts(string_table)
     with on_time(*NOW_SIMULATED):
-        assert list(check_network_fs_mount(item, params, section)) == check_result
+        assert (
+            list(
+                check_network_fs_mount(
+                    item, {**FILESYSTEM_DEFAULT_LEVELS, **{"has_perfdata": True}}, section
+                )
+            )
+            == check_result
+        )

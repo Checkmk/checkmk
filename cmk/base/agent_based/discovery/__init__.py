0bb923481b5813cdd45c919f9a3065631aad653c
Giordano Tomassorri <giordano.tomassorri@tribe29.com>
2022-04-27 12:47:36 +0200

    Fix vanished cluster services marked as monitored
    Vanished services were not marked as vanished in clusters even if they
    appeared as vanished in the single hosts.
    
    Fixes CMK-10358
    
    Change-Id: I84d46a2ad968c71602bf93080f1ebc94e353dfe2

--- cmk/base/agent_based/discovery/__init__.py
+++ cmk/base/agent_based/discovery/__init__.py
@@ -1169,7 +1169,7 @@ def _get_cluster_services(
             parsed_sections_broker=parsed_sections_broker,
             run_plugin_names=EVERYTHING,
             forget_existing=False,
-            keep_vanished=True,
+            keep_vanished=False,
             on_error=on_error,
         )
 

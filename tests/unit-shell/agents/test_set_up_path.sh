d214863896f8d1588299c90815b37cb1bb45122c
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-05-02 09:15:00 +0200

    14083 FIX Fix linux agent using 100% CPU (again)
    Werk <i>#14027: Fix linux agent using 100% CPU</i> actually increased the problem of async agent services using 100% CPU on some systems.
    
    Change-Id: I675b1b973350b2d5269f037512ade9e800498ac7

---
+++ tests/unit-shell/agents/test_set_up_path.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+AGENT_LINUX="${UNIT_SH_AGENTS_DIR}/check_mk_agent.linux"
+
+# shellcheck source=agents/check_mk_agent.linux
+MK_SOURCE_AGENT="true" source "$AGENT_LINUX"
+
+test_set_up_path_already_in_path() {
+    assertEquals "/foo:/usr/local/bin:/bar" "$(set_up_path "/foo:/usr/local/bin:/bar")"
+}
+
+test_set_up_path_extended() {
+    assertEquals "/foo:/bar:/usr/local/bin" "$(set_up_path "/foo:/bar")"
+}
+
+# shellcheck disable=SC1090
+. "$UNIT_SH_SHUNIT2"

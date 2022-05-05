93b7992a737318c0a529f6d47c5f9e039ac27fb3
Sven Panne <sven.panne@tribe29.com>
2022-05-05 12:11:06 +0200

    Suppression removal + cleanups + simplifications.
    Change-Id: Ib8802aca495c5c0f67c3ae3ea686b0d9e2d8d653

--- cmk/utils/cpu_tracking.py
+++ cmk/utils/cpu_tracking.py
@@ -9,16 +9,8 @@ from __future__ import annotations
 import os
 import posix
 from dataclasses import dataclass
-from typing import Any, Iterable, Mapping
 
 from cmk.utils.log import console
-
-__all__ = ["CPUTracker", "Snapshot", "times_result"]
-
-
-def times_result(seq: Iterable[float]) -> posix.times_result:
-    # mypy warnings are false positives.
-    return posix.times_result(tuple(seq))  # type: ignore[arg-type, call-arg]
 
 
 @dataclass(frozen=True)
@@ -30,32 +22,33 @@ class Snapshot:
         return max(self.process.elapsed - sum(self.process[:4]), 0.0)
 
     @classmethod
-    def null(cls):
-        return cls(times_result((0.0, 0.0, 0.0, 0.0, 0.0)))
+    def null(cls) -> Snapshot:
+        return cls(posix.times_result((0.0, 0.0, 0.0, 0.0, 0.0)))
 
     @classmethod
     def take(cls) -> Snapshot:
         return cls(os.times())
 
     @classmethod
-    def deserialize(cls, serialized: Mapping[str, Any]) -> Snapshot:
+    def deserialize(cls, serialized: object) -> Snapshot:
         try:
-            return cls(times_result(serialized["process"]))
+            assert isinstance(serialized, dict)
+            return cls(posix.times_result(serialized["process"]))
         except LookupError as exc:
             raise ValueError(serialized) from exc
 
-    def serialize(self) -> Mapping[str, Any]:
+    def serialize(self) -> object:
         return {"process": tuple(self.process)}
 
     def __add__(self, other: Snapshot) -> Snapshot:
         if not isinstance(other, Snapshot):
             return NotImplemented
-        return Snapshot(times_result(t0 + t1 for t0, t1 in zip(self.process, other.process)))
+        return Snapshot(posix.times_result(t0 + t1 for t0, t1 in zip(self.process, other.process)))
 
     def __sub__(self, other: Snapshot) -> Snapshot:
         if not isinstance(other, Snapshot):
             return NotImplemented
-        return Snapshot(times_result(t0 - t1 for t0, t1 in zip(self.process, other.process)))
+        return Snapshot(posix.times_result(t0 - t1 for t0, t1 in zip(self.process, other.process)))
 
     def __bool__(self) -> bool:
         return self != Snapshot.null()
@@ -70,12 +63,12 @@ class CPUTracker:
     def __repr__(self) -> str:
         return "%s()" % type(self).__name__
 
-    def __enter__(self):
+    def __enter__(self) -> CPUTracker:
         self._start = Snapshot.take()
         console.vverbose("[cpu_tracking] Start [%x]\n", id(self))
         return self
 
-    def __exit__(self, *exc_info):
+    def __exit__(self, *exc_info) -> None:
         self._end = Snapshot.take()
         console.vverbose("[cpu_tracking] Stop [%x - %s]\n", id(self), self.duration)
 
--- tests/unit/cmk/utils/test_cpu_tracking.py
+++ tests/unit/cmk/utils/test_cpu_tracking.py
@@ -4,8 +4,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# pylint: disable=redefined-outer-name
-
 import json
 
 import pytest
@@ -13,53 +11,43 @@ import pytest
 from cmk.utils.cpu_tracking import Snapshot
 
 
-def json_identity(serializable):
+def json_identity(serializable: object) -> object:
     return json.loads(json.dumps(serializable))
-
-
-@pytest.fixture
-def set_time(monkeypatch):
-    def setter(value):
-        monkeypatch.setattr("time.monotonic", lambda value=value: value)
-
-    return setter
 
 
 class TestCpuTracking:
     @pytest.fixture
-    def null(self):
+    def null(self) -> Snapshot:
         return Snapshot.null()
 
     @pytest.fixture
-    def now(self):
+    def now(self) -> Snapshot:
         return Snapshot.take()
 
-    def test_eq_neq(self, null, now):
+    def test_eq_neq(self, null: Snapshot, now):
         assert null == Snapshot.null()
         assert null != now
         assert now != null
         assert bool(null) is False
-        assert bool(null) is not True
         assert bool(now) is True
-        assert bool(now) is not False
 
-    def test_add_null_null(self, null):
+    def test_add_null_null(self, null: Snapshot) -> None:
         assert null + null == null
 
-    def test_add_null_now(self, null, now):
+    def test_add_null_now(self, null: Snapshot, now) -> None:
         assert null + now == now
 
-    def test_sub_null_null(self, null):
+    def test_sub_null_null(self, null: Snapshot) -> None:
         assert null - null == null
 
-    def test_sub_now_null(self, now, null):
+    def test_sub_now_null(self, now: Snapshot, null: Snapshot) -> None:
         assert now - null == now
 
-    def test_sub_now_now(self, now, null):
+    def test_sub_now_now(self, now: Snapshot, null: Snapshot) -> None:
         assert now - now == null
 
-    def test_json_serialization_null(self, null):
+    def test_json_serialization_null(self, null: Snapshot) -> None:
         assert Snapshot.deserialize(json_identity(null.serialize())) == null
 
-    def test_json_serialization_now(self, now):
+    def test_json_serialization_now(self, now) -> None:
         assert Snapshot.deserialize(json_identity(now.serialize())) == now

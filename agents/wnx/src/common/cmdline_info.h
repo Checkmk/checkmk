b8cae0f3cd98088c3b3691b76871a8ce29d407cb
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-05-05 09:45:29 +0200

    Improve typing in win-agent
    Change-Id: Ib37ed5af4cd46115cd2fdd0e1b37ae7e609163c2

--- agents/wnx/src/common/cmdline_info.h
+++ agents/wnx/src/common/cmdline_info.h
@@ -11,7 +11,6 @@
 
 #pragma once
 #include <string>
-#include <tuple>
 
 #include "tools/_misc.h"
 
@@ -19,25 +18,35 @@ namespace cma {
 namespace exe {
 namespace cmdline {
 // 1st Param
-constexpr const wchar_t* kTestParam = L"-test";
-constexpr const wchar_t* kLegacyTestParam = L"test";
-constexpr const wchar_t* kHelpParam = L"-help";
-constexpr const wchar_t* kRunParam = L"-run";          // runs as app
-constexpr const wchar_t* kRunOnceParam = L"-runonce";  // runs as app
+constexpr const wchar_t *kTestParam = L"-test";
+constexpr const wchar_t *kLegacyTestParam = L"test";
+constexpr const wchar_t *kHelpParam = L"-help";
+constexpr const wchar_t *kRunParam = L"-run";          // runs as app
+constexpr const wchar_t *kRunOnceParam = L"-runonce";  // runs as app
 
-constexpr const wchar_t* kId = L"id";
-constexpr const wchar_t* kTimeout = L"timeout";
+constexpr const wchar_t *kId = L"id";
+constexpr const wchar_t *kTimeout = L"timeout";
 
 constexpr wchar_t kSplitter = L':';
 
-inline auto ParseExeCommandLine(const std::vector<std::wstring>& args) {
-    auto make_error_answer = [](int ErrorCode) -> auto {
-        return make_tuple(ErrorCode, std::wstring(), std::wstring(),
-                          std::wstring());
+struct ExeCommandLine {
+    int error_code;
+    std::wstring name;
+    std::wstring id_val;
+    std::wstring timeout_val;
+};
+
+inline ExeCommandLine ParseExeCommandLine(
+    const std::vector<std::wstring> &args) {
+    auto make_error_answer = [](int error_code) {
+        return ExeCommandLine{.error_code = error_code,
+                              .name = {},
+                              .id_val = {},
+                              .timeout_val = {}};
+        ;
     };
 
     if (args.size() < 3) {
-        xlog::l("Invalid command line").print();
         return make_error_answer(2);
     }
     // NAME
@@ -47,11 +56,9 @@ inline auto ParseExeCommandLine(const std::vector<std::wstring>& args) {
     auto [port_type, port_addr] =
         tools::ParseKeyValue(args[1], exe::cmdline::kSplitter);
     if (port_type.empty()) {
-        xlog::l("Port type is empty").print();
         return make_error_answer(3);
     }
     if (port_addr.empty()) {
-        xlog::l("Port addr is empty").print();
         return make_error_answer(4);
     }
 
@@ -59,12 +66,10 @@ inline auto ParseExeCommandLine(const std::vector<std::wstring>& args) {
     auto [id_key, id_val] =
         tools::ParseKeyValue(args[2], exe::cmdline::kSplitter);
     if (id_key != exe::cmdline::kId) {
-        xlog::l("IDkey is bad or absent").print();
         return make_error_answer(5);
     }
 
     if (id_val.empty()) {
-        xlog::l("Value of ID is empty").print();
         return make_error_answer(6);
     }
 
@@ -72,14 +77,16 @@ inline auto ParseExeCommandLine(const std::vector<std::wstring>& args) {
     auto [timeout_key, timeout_val] =
         tools::ParseKeyValue(args[3], exe::cmdline::kSplitter);
     if (timeout_key != exe::cmdline::kTimeout) {
-        xlog::l("Timeout Key is bad or absent").print();
         return make_error_answer(7);
     }
     if (timeout_val.empty()) {
-        xlog::l("Value of Timeout is empty").print();
         return make_error_answer(8);
     }
-    return std::make_tuple(0, name, id_val, timeout_val);
+
+    return ExeCommandLine{.error_code = 0,
+                          .name = name,
+                          .id_val = id_val,
+                          .timeout_val = timeout_val};
 }
 
 // 2-nd param

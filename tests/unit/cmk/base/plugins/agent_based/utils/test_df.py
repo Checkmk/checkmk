1664b95f920852ea9b0a93c17ba9e2fceca95442
Joerg Herbel <joerg.herbel@tribe29.com>
2022-05-06 11:56:34 +0200

    Add some df unit tests
    These tests reveal a bug related to zero free inodes in the legacy df
    checks. We also add some tests for the non-legacy df checks to ensure
    that this bug does not affect these checks.
    
    FEED-6948
    
    Change-Id: Ibf113a2d753f5c7795ca7dfb184ecac8b5f03287

--- tests/unit/cmk/base/plugins/agent_based/utils/test_df.py
+++ tests/unit/cmk/base/plugins/agent_based/utils/test_df.py
@@ -4,9 +4,13 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import pytest  # type: ignore[import]
+import pytest
 
-from cmk.base.plugins.agent_based.utils.df import get_filesystem_levels
+from typing import Optional, Tuple
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import CheckResult
+from cmk.base.plugins.agent_based.utils import df
 
 pytestmark = pytest.mark.checks
 
@@ -94,5 +98,210 @@ pytestmark = pytest.mark.checks
     ),
 ])
 def test_get_filesystem_levels(size_gb, params, expected_levels):
-    levels = get_filesystem_levels(size_gb, params)
+    levels = df.get_filesystem_levels(size_gb, params)
     assert levels == expected_levels
+
+
+@pytest.mark.parametrize(
+    ["data", "expected_result"],
+    [
+        pytest.param(
+            (
+                102655,
+                58814,
+                122,
+                None,
+                None,
+            ),
+            [
+                Metric(
+                    "fs_used",
+                    43841.0,
+                    levels=(82124.0, 92389.5),
+                    boundaries=(0.0, 102655.0),
+                ),
+                Metric(
+                    "fs_size",
+                    102655.0,
+                    boundaries=(0.0, None),
+                ),
+                Metric(
+                    "fs_used_percent",
+                    42.707125809751105,
+                    levels=(80.0, 90.0),
+                    boundaries=(0.0, 100.0),
+                ),
+                Result(
+                    state=State.OK,
+                    summary="42.71% used (42.8 of 100 GiB)",
+                ),
+                Metric(
+                    "growth",
+                    161105947.82608697,
+                ),
+                Result(
+                    state=State.OK,
+                    summary="trend per 1 day 0 hours: +154 TiB",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="trend per 1 day 0 hours: +156939.21%",
+                ),
+                Metric(
+                    "trend",
+                    161105947.82608697,
+                    boundaries=(0.0, 4277.291666666667),
+                ),
+                Result(
+                    state=State.OK,
+                    summary="Time left until disk full: 32 seconds",
+                ),
+            ],
+            id="no inode information",
+        ),
+        pytest.param(
+            (
+                102655,
+                58814,
+                122,
+                65486,
+                111,
+            ),
+            [
+                Metric(
+                    "fs_used",
+                    43841.0,
+                    levels=(82124.0, 92389.5),
+                    boundaries=(0.0, 102655.0),
+                ),
+                Metric(
+                    "fs_size",
+                    102655.0,
+                    boundaries=(0.0, None),
+                ),
+                Metric(
+                    "fs_used_percent",
+                    42.707125809751105,
+                    levels=(80.0, 90.0),
+                    boundaries=(0.0, 100.0),
+                ),
+                Result(
+                    state=State.OK,
+                    summary="42.71% used (42.8 of 100 GiB)",
+                ),
+                Metric(
+                    "growth",
+                    161105947.82608697,
+                ),
+                Result(
+                    state=State.OK,
+                    summary="trend per 1 day 0 hours: +154 TiB",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="trend per 1 day 0 hours: +156939.21%",
+                ),
+                Metric(
+                    "trend",
+                    161105947.82608697,
+                    boundaries=(0.0, 4277.291666666667),
+                ),
+                Result(
+                    state=State.OK,
+                    summary="Time left until disk full: 32 seconds",
+                ),
+                Metric(
+                    "inodes_used",
+                    65375.0,
+                    levels=(58937.4, 62211.7),
+                    boundaries=(0.0, 65486.0),
+                ),
+                Result(
+                    state=State.CRIT,
+                    summary=
+                    "Inodes used: 99.83% (warn/crit at 90.00%/95.00%), Inodes available: 111 (0.17%)",
+                ),
+            ],
+            id="with inode information",
+        ),
+        pytest.param(
+            (
+                102655,
+                58814,
+                122,
+                65486,
+                0,
+            ),
+            [
+                Metric(
+                    "fs_used",
+                    43841.0,
+                    levels=(82124.0, 92389.5),
+                    boundaries=(0.0, 102655.0),
+                ),
+                Metric(
+                    "fs_size",
+                    102655.0,
+                    boundaries=(0.0, None),
+                ),
+                Metric(
+                    "fs_used_percent",
+                    42.707125809751105,
+                    levels=(80.0, 90.0),
+                    boundaries=(0.0, 100.0),
+                ),
+                Result(
+                    state=State.OK,
+                    summary="42.71% used (42.8 of 100 GiB)",
+                ),
+                Metric(
+                    "growth",
+                    161105947.82608697,
+                ),
+                Result(
+                    state=State.OK,
+                    summary="trend per 1 day 0 hours: +154 TiB",
+                ),
+                Result(
+                    state=State.OK,
+                    summary="trend per 1 day 0 hours: +156939.21%",
+                ),
+                Metric(
+                    "trend",
+                    161105947.82608697,
+                    boundaries=(0.0, 4277.291666666667),
+                ),
+                Result(
+                    state=State.OK,
+                    summary="Time left until disk full: 32 seconds",
+                ),
+                Metric(
+                    "inodes_used",
+                    65486.0,
+                    levels=(58937.4, 62211.7),
+                    boundaries=(0.0, 65486.0),
+                ),
+                Result(
+                    state=State.CRIT,
+                    summary=
+                    "Inodes used: 100.00% (warn/crit at 90.00%/95.00%), Inodes available: 0 (0%)",
+                ),
+            ],
+            id="zero inodes left",
+        ),
+    ],
+)
+def test_df_check_filesystem_single(
+    data: Tuple[float, float, float, Optional[float], Optional[float]],
+    expected_result: CheckResult,
+) -> None:
+    assert (list(
+        df.df_check_filesystem_single(
+            {
+                "/fake.delta": (100, 954),
+            },
+            "/fake",
+            *data,
+            df.FILESYSTEM_DEFAULT_LEVELS,
+            this_time=123,
+        )) == expected_result)

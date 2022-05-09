9e70354a633537b6724e95af7af259ff9c7ce1a9
Max Linke <max.linke@tribe29.com>
2022-05-09 14:32:02 +0200

    migrate mysql capacity check
    Fix: Feed-6883
    
    Change-Id: Ie912762ce5b4a6a40691cebf9fe2e9fd923fcfcd

--- cmk/gui/plugins/wato/check_parameters/mysql_db_size.py
+++ cmk/gui/plugins/wato/check_parameters/mysql_db_size.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+from typing import Union
 
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
@@ -10,7 +11,8 @@ from cmk.gui.plugins.wato.utils import (
     rulespec_registry,
     RulespecGroupCheckParametersApplications,
 )
-from cmk.gui.valuespec import Filesize, Optional, TextInput, Tuple
+from cmk.gui.plugins.wato.utils.simple_levels import SimpleLevels
+from cmk.gui.valuespec import Dictionary, Filesize, TextInput, Transform
 
 
 def _item_spec_mysql_db_size():
@@ -20,25 +22,40 @@ def _item_spec_mysql_db_size():
     )
 
 
+def _transform(params: Union[dict, tuple[float, float]]) -> dict[str, tuple[float, float]]:
+    if isinstance(params, dict):
+        if "levels" not in params:
+            params["levels"] = None
+        return params
+    return {"levels": params}
+
+
 def _parameter_valuespec_mysql_db_size():
-    return Optional(
-        valuespec=Tuple(
+    return Transform(
+        valuespec=Dictionary(
             elements=[
-                Filesize(title=_("warning at")),
-                Filesize(title=_("critical at")),
+                (
+                    "levels",
+                    SimpleLevels(
+                        Filesize,
+                        help=_(
+                            "The service will trigger a warning or critical state if the size of the "
+                            "database exceeds these levels."
+                        ),
+                        title=_("Impose limits on the size of the database"),
+                    ),
+                )
             ],
+            optional_keys=False,
         ),
-        help=_(
-            "The check will trigger a warning or critical state if the size of the "
-            "database exceeds these levels."
-        ),
-        title=_("Impose limits on the size of the database"),
+        forth=_transform,
     )
 
 
 rulespec_registry.register(
     CheckParameterRulespecWithItem(
         check_group_name="mysql_db_size",
+        match_type="dict",
         group=RulespecGroupCheckParametersApplications,
         item_spec=_item_spec_mysql_db_size,
         parameter_valuespec=_parameter_valuespec_mysql_db_size,
---
+++ tests/unit/cmk/base/plugins/agent_based/test_mysql_capacity.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+from typing import Optional
+
+import pytest
+
+from cmk.base.api.agent_based.checking_classes import Metric, Result, Service, State
+from cmk.base.api.agent_based.type_defs import StringTable
+from cmk.base.plugins.agent_based import mysql_capacity
+
+
+@pytest.mark.parametrize(
+    "sub_section, instance_name",
+    [
+        pytest.param(None, "mysql", id="default instance"),
+        pytest.param("[[]]", "mysql", id="empty instance"),
+        pytest.param("[[cmk]]", "cmk", id="named instance"),
+    ],
+)
+def test_parse(sub_section: Optional[str], instance_name: str):
+    args: StringTable = [
+        ["greendb", "163840", "1428160512"],
+    ]
+    if sub_section is not None:
+        args = [
+            [
+                sub_section,
+            ],
+            args[0],
+        ]
+    expected = {instance_name: {"greendb": 163840}}
+    assert mysql_capacity.parse_size(args) == expected
+
+
+def test_parse_empty_instance_default_to_previous():
+    args = [
+        ["[[some]]"],
+        ["greendb", "163840", "1428160512"],
+        ["[[]]"],
+        ["reddb", "163840", "1428160512"],
+    ]
+    expected = {
+        "some": {
+            "greendb": 163840,
+            "reddb": 163840,
+        }
+    }
+    assert mysql_capacity.parse_size(args) == expected
+
+
+def test_parse_exclude_non_int_size_info():
+    args = [
+        ["greendb", "1dd63840", "1428160512"],
+    ]
+    expected: dict[str, dict[str, int]] = {}
+    assert mysql_capacity.parse_size(args) == expected
+
+
+def test_discovery():
+    section = {
+        "mysql": {
+            "red": 12,
+            "information_schema": 12,
+            "performance_schema": 12,
+            "mysql": 12,
+        }
+    }
+    assert list(mysql_capacity.discover_capacity(section)) == [Service(item="mysql:red")]
+
+
+def test_check():
+    item = "mysql:reddb"
+    params = {"levels": (None, None)}
+    section = {"mysql": {"reddb": 42}}
+
+    assert list(mysql_capacity.check_capacity(item, params, section)) == [
+        Result(state=State.OK, summary="Size: 42 B"),
+        Metric("database_size", 42.0),
+    ]

6b5673589080cf1755f0d52d3bccb707e49cdf94
Alex Zurhake <alex.zurhake@tribe29.com>
2022-05-10 10:39:48 +0200

    Upload the FREE edition to docker hub as well
    Change-Id: I61ced6b5aa32eae922531c4b60155c9954c22e8a

--- buildscripts/scripts/build-cmk-container.sh
+++ buildscripts/scripts/build-cmk-container.sh
@@ -68,7 +68,7 @@ build_image() {
 }
 
 push_image() {
-   if [ $EDITION = raw ]; then
+   if [ "$EDITION" = raw ] || [ "$EDITION" = free ]; then
        docker_push "" "checkmk"
    else
        docker_push "registry.checkmk.com" "/${EDITION}"

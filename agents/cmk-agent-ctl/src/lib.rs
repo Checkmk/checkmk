7e9826c3734e743ac630db2a42b719428ddff9a6
Joerg Herbel <joerg.herbel@tribe29.com>
2022-05-10 09:00:22 +0200

    cmk-agent-ctl: Check if agent socket is operational upon startup
    This reverts commit b3f14a4fbf97ae44f01df5e48ad2e391e3a9ecc2.
    
    CMK-10449
    
    Change-Id: Iae6eeba86c806b8893d19d84c7719b8b5500caa0

--- agents/cmk-agent-ctl/src/lib.rs
+++ agents/cmk-agent-ctl/src/lib.rs
@@ -13,7 +13,7 @@ mod setup;
 pub mod site_spec;
 mod tls_server;
 pub mod types;
-use anyhow::{Context, Result as AnyhowResult};
+use anyhow::{anyhow, Context, Result as AnyhowResult};
 use config::{JSONLoader, TOMLLoader};
 
 use modes::daemon::daemon;
@@ -28,6 +28,8 @@ use modes::status::status;
 pub use setup::init;
 
 pub fn run_requested_mode(args: cli::Args, paths: setup::PathResolver) -> AnyhowResult<()> {
+    agent_socket_operational(&args)?;
+
     let registration_preset = config::RegistrationPreset::load(&paths.registration_preset_path)?;
     let runtime_config = config::RuntimeConfig::load(&paths.config_path)?;
     let mut registry = config::Registry::from_file(&paths.registry_path)
@@ -96,5 +98,23 @@ pub fn run_requested_mode(args: cli::Args, paths: setup::PathResolver) -> Anyhow
             delete_all_args.enable_insecure_connections,
             &legacy_pull_marker,
         ),
+    }
+}
+
+// This check is currently only useful on Unix. On Windows, the internal agent address can be passed
+// on the command line, so we cannot easily check this for any mode.
+fn agent_socket_operational(args: &cli::Args) -> AnyhowResult<()> {
+    let agent_channel = setup::agent_channel();
+    match *args {
+        cli::Args::Register { .. } | cli::Args::Import { .. } => {
+            match agent_channel.operational() {
+                true => Ok(()),
+                false => Err(anyhow!(format!(
+                    "Something seems wrong with the agent socket ({}), aborting",
+                    agent_channel
+                ))),
+            }
+        }
+        _ => Ok(()),
     }
 }

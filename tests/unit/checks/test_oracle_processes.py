5d08e82a76caf0d106f79e68c543b0a72f1b4bde
LukaRacic <luka.racic@tribe29.com>
2022-05-13 15:31:16 +0200

    oracle_processes: Added/refactored unit tests
    Change-Id: I3c2d5f90e453542ee1733315cf39475566dc2fde

--- tests/unit/checks/test_oracle_processes.py
+++ tests/unit/checks/test_oracle_processes.py
@@ -8,21 +8,28 @@ from typing import Sequence, Union
 
 import pytest
 
-from tests.testlib import Check
+from tests.unit.conftest import FixRegister
 
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result
+from cmk.utils.type_defs import CheckPluginName
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
 from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
 
 pytestmark = pytest.mark.checks
 
 
 @pytest.mark.parametrize(
-    "string_table, discovered_item",
+    "section, discovered_item",
     [
         pytest.param(
             [["DB1DEV2", "1152", "1500"]],
-            [("DB1DEV2", {})],
-            id="One Oracle process is discovered",
+            [Service(item="DB1DEV2")],
+            id="One valid Oracle process is discovered",
+        ),
+        pytest.param(
+            [["Error", "Message:"]],
+            [Service(item="Error")],
+            id="One error Oracle process is discovered",
         ),
         pytest.param(
             [],
@@ -32,52 +39,74 @@ pytestmark = pytest.mark.checks
     ],
 )
 def test_discover_oracle_processes(
-    string_table: StringTable,
-    discovered_item: Sequence[str],
+    section: StringTable,
+    discovered_item: Sequence[Service],
+    fix_register: FixRegister,
 ) -> None:
-    assert list(Check("oracle_processes").run_discovery(string_table)) == discovered_item
+    check = fix_register.check_plugins[CheckPluginName("oracle_processes")]
+    assert list(check.discovery_function(section)) == discovered_item
 
 
 @pytest.mark.parametrize(
-    "string_table, item, check_results",
+    "section, item, check_result",
     [
         pytest.param(
             [["FDMTST", "50", "300"]],
             "FDMTST",
             [
-                0,
-                "50 of 300 processes are used (16%, warn/crit at 70%/90%)",
-                [("processes", 50, 210.0, 270.0)],
+                Result(
+                    state=State.OK,
+                    summary="50 of 300 processes are used (16%, warn/crit at 70%/90%)",
+                ),
+                Metric(name="processes", value=50, levels=(210.0, 270.0)),
             ],
-            id="OK oracle process",
+            id="Oracle process OK state",
         ),
         pytest.param(
             [["DB1DEV2", "1152", "1500"]],
             "DB1DEV2",
             [
-                1,
-                "1152 of 1500 processes are used (76%, warn/crit at 70%/90%)",
-                [("processes", 1152, 1050.0, 1350.0)],
+                Result(
+                    state=State.WARN,
+                    summary="1152 of 1500 processes are used (76%, warn/crit at 70%/90%)",
+                ),
+                Metric(name="processes", value=1152, levels=(1050.0, 1350.0)),
             ],
-            id="WARN oracle process",
+            id="Oracle process state WARN",
+        ),
+        pytest.param(
+            [["DB1DEV2", "1450", "1500"]],
+            "DB1DEV2",
+            [
+                Result(
+                    state=State.CRIT,
+                    summary="1450 of 1500 processes are used (96%, warn/crit at 70%/90%)",
+                ),
+                Metric(name="processes", value=1450, levels=(1050.0, 1350.0)),
+            ],
+            id="Oracle process state CRIT",
         ),
         pytest.param(
             [["Error", "Message:"], ["Error", "Message:"]],
             "Error",
             [
-                3,
-                'Found error in agent output "Message:"',
+                Result(
+                    state=State.UNKNOWN,
+                    summary='Found error in agent output "Message:"',
+                )
             ],
             id="UNKNOWN on error from 1.6 solaris agent plugin output",
         ),
     ],
 )
 def test_check_oracle_processes(
-    string_table: StringTable,
+    section: StringTable,
     item: str,
-    check_results: Sequence[Union[Result, Metric]],
+    check_result: Sequence[Union[Result, Metric]],
+    fix_register: FixRegister,
 ) -> None:
+    check = fix_register.check_plugins[CheckPluginName("oracle_processes")]
     assert (
-        list(Check("oracle_processes").run_check(item, {"levels": (70.0, 90.0)}, string_table))
-        == check_results
+        list(check.check_function(item=item, params={"levels": (70.0, 90.0)}, section=section))
+        == check_result
     )

b5e7fa5cdd0c7f2c35f6f0641326e78787b3fdfd
Anastasiya Liatsetskaya <anastasiya.liatsetskaya@tribe29.com>
2022-05-14 00:46:14 +0200

    Put by_ssh check into separate file
    CMK-10470
    
    Change-Id: I387fc2c411f5277940142be81f2b5565e88404a0

--- cmk/gui/plugins/wato/active_checks_module.py
+++ cmk/gui/plugins/wato/active_checks_module.py
@@ -15,7 +15,6 @@ from cmk.gui.plugins.wato.utils import (
 )
 from cmk.gui.valuespec import (
     Age,
-    Alternative,
     Dictionary,
     DropdownChoice,
     FixedValue,
@@ -137,112 +136,6 @@ rulespec_registry.register(
         match_type="all",
         name="custom_checks",
         valuespec=_valuespec_custom_checks,
-    )
-)
-
-
-def _valuespec_active_checks_by_ssh():
-    return Tuple(
-        title=_("Check via SSH service"),
-        help=_("Checks via SSH. "),
-        elements=[
-            TextInput(
-                title=_("Command"),
-                help=_("Command to execute on remote host."),
-                allow_empty=False,
-                size=50,
-            ),
-            Dictionary(
-                title=_("Optional parameters"),
-                elements=[
-                    (
-                        "description",
-                        TextInput(
-                            title=_("Service Description"),
-                            help=_(
-                                "Must be unique for every host. Defaults to command that is executed."
-                            ),
-                            size=50,
-                        ),
-                    ),
-                    (
-                        "hostname",
-                        TextInput(
-                            title=_("DNS Hostname or IP address"),
-                            default_value="$HOSTADDRESS$",
-                            allow_empty=False,
-                            help=_(
-                                "You can specify a hostname or IP address different from IP address "
-                                "of the host as configured in your host properties."
-                            ),
-                        ),
-                    ),
-                    (
-                        "port",
-                        Integer(
-                            title=_("SSH Port"),
-                            help=_("Default is 22."),
-                            minvalue=1,
-                            maxvalue=65535,
-                            default_value=22,
-                        ),
-                    ),
-                    (
-                        "ip_version",
-                        Alternative(
-                            title=_("IP-Version"),
-                            elements=[
-                                FixedValue(value="ipv4", totext="", title=_("IPv4")),
-                                FixedValue(value="ipv6", totext="", title=_("IPv6")),
-                            ],
-                        ),
-                    ),
-                    (
-                        "timeout",
-                        Integer(
-                            title=_("Seconds before connection times out"),
-                            unit=_("sec"),
-                            default_value=10,
-                        ),
-                    ),
-                    (
-                        "logname",
-                        TextInput(
-                            title=_("Username"), help=_("SSH user name on remote host"), size=30
-                        ),
-                    ),
-                    (
-                        "identity",
-                        TextInput(
-                            title=_("Keyfile"), help=_("Identity of an authorized key"), size=50
-                        ),
-                    ),
-                    (
-                        "accept_new_host_keys",
-                        FixedValue(
-                            value=True,
-                            title=_("Enable automatic host key acceptance"),
-                            help=_(
-                                "This will automatically accept hitherto-unseen keys"
-                                "but will refuse connections for changed or invalid hostkeys"
-                            ),
-                            totext=_(
-                                "Automatically stores the host key with no manual input requirement"
-                            ),
-                        ),
-                    ),
-                ],
-            ),
-        ],
-    )
-
-
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupIntegrateOtherServices,
-        match_type="all",
-        name="active_checks:by_ssh",
-        valuespec=_valuespec_active_checks_by_ssh,
     )
 )
 

36e830e3bb1fd0a5b032e411782114ec4f7dd471
Lars Michelsen <lm@tribe29.com>
2022-05-13 22:34:14 +0200

    Remove left over systemd init script
    Change-Id: I73ebfa273e68b089de33989a1c5b2a687bbb96ed

--- tests/integration/omd/test_init_scripts.py
+++ tests/integration/omd/test_init_scripts.py
@@ -21,7 +21,6 @@ def test_init_scripts(site: Site):
         "pnp_gearman_worker",
         "rrdcached",
         "xinetd",
-        "systemd",
         "stunnel",
         "redis",
         "agent-receiver",

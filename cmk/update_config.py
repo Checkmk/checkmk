490aa516632f1ca05728570775bc1f0351383054
Lars Michelsen <lm@tribe29.com>
2022-05-13 22:40:29 +0200

    13648 FIX Fix failed "Migrate LDAP connections" of connections without bind credentials
    Change-Id: Icef73bfe524b1b4e3ab8a979337188b09246886f

--- cmk/update_config.py
+++ cmk/update_config.py
@@ -1163,10 +1163,11 @@ class UpdateConfig:
         for connection in connections:
             connection.setdefault("type", "ldap")
 
-            dn, password = connection["bind"]
-            if isinstance(password, tuple):
-                continue
-            connection["bind"] = (dn, ("password", password))
+            if "bind" in connection:
+                dn, password = connection["bind"]
+                if isinstance(password, tuple):
+                    continue
+                connection["bind"] = (dn, ("password", password))
 
         save_connection_config(connections)
 

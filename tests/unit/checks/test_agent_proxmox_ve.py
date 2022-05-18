b94abb50cb06e3c063e05d66fdb8a6140db1a9ee
Joerg Herbel <joerg.herbel@tribe29.com>
2022-05-18 18:24:24 +0200

    Special agents proxmox_ve & mobileiron: Fix password store usage
    There apparently was some misunderstanding here.
    
    Change-Id: I2d0e4124e8b12f7131902994b194a1648a47cc3f

--- tests/unit/checks/test_agent_proxmox_ve.py
+++ tests/unit/checks/test_agent_proxmox_ve.py
@@ -3,31 +3,69 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Any, Mapping, Sequence
+
+import pytest
+
 from tests.testlib import SpecialAgent
 
 
-def test_agent_proxmox_ve_arguments_password_store() -> None:
-
-    params = {
-        "username": "user",
-        "password": ("password", "passwd"),
-        "port": "443",
-        "no-cert-check": True,
-        "timeout": "30",
-        "log-cutoff-weeks": "4",
-    }
-    agent = SpecialAgent("agent_proxmox_ve")
-    assert agent.argument_func(params, "testhost", "1.2.3.4") == [
-        "-u",
-        "user",
-        "-p",
-        "passwd",
-        "--port",
-        "443",
-        "--no-cert-check",
-        "--timeout",
-        "30",
-        "--log-cutoff-weeks",
-        "4",
-        "testhost",
-    ]
+@pytest.mark.parametrize(
+    ["params", "expected_result"],
+    [
+        pytest.param(
+            {
+                "username": "user",
+                "password": ("password", "passwd"),
+                "port": "443",
+                "no-cert-check": True,
+                "timeout": "30",
+                "log-cutoff-weeks": "4",
+            },
+            [
+                "-u",
+                "user",
+                "-p",
+                "passwd",
+                "--port",
+                "443",
+                "--no-cert-check",
+                "--timeout",
+                "30",
+                "--log-cutoff-weeks",
+                "4",
+                "testhost",
+            ],
+            id="explicit_password",
+        ),
+        pytest.param(
+            {
+                "username": "user",
+                "password": ("store", "passwd"),
+                "timeout": "40",
+            },
+            [
+                "-u",
+                "user",
+                "-p",
+                ("store", "passwd", "%s"),
+                "--timeout",
+                "40",
+                "testhost",
+            ],
+            id="password_from_store",
+        ),
+    ],
+)
+def test_agent_proxmox_ve_arguments(
+    params: Mapping[str, Any],
+    expected_result: Sequence[Any],
+) -> None:
+    assert (
+        SpecialAgent("agent_proxmox_ve").argument_func(
+            params,
+            "testhost",
+            "1.2.3.4",
+        )
+        == expected_result
+    )

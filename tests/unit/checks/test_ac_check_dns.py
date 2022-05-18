498a5ee883f8370fbc8e674645dc0427b15ae569
Solomon Jacobs <solomon.jacobs@tribe29.com>
2022-05-18 16:27:07 +0200

    14130 FIX Clarify "Check DNS service"
    Previously, the parameter <tt>DNS Server</tt> of <tt>Check DNS service</tt> rule was optional.
    Moreover, the options <tt>this host</tt> and <tt>default DNS Server</tt> would both do the same:
    They would use the default DNS server(s) specified inside of /etc/resolve.conf.  With this werk, the
    option <tt>this host</tt> now (again) will use the address of the host on which the service is
    generated. Secondly, the rule has been reworked to be more concise.
    
    Intervention is necessary if
    
    - you have explicitely set the DNS Server to <tt>this host</tt> and
    
    - want your site to operate in the same fashion as before.
    
    In this case, you can set the option <tt>DNS Server</tt> to <tt>Use the default DNS server(s)
    specified in /etc/resolv.conf</tt>.
    
    This change was originally implemented via the changes
    If81741768dca952470b04a7884c2cc106494b8c4
    I1e2ec8cc0a89a786a8b5a1d82e01c482faeea82c
    I1aa1120c6be4c3343ffcff2b365258efd3066f1c
    I35e09ba5d0cb3186b1e84e926fdfcce29352dade
    I0954822f2b4887d1876ebde67afc982af732acf4
    I0711a27fc896437fff091687ae20b26743c72624
    I48d27ce625b371561a955e0c625385a1deba1720
    which now have been squashed.
    
    SUP-8172
    
    Co-authored-by: Lisa Pichler <lisa.pichler@tribe29.com>
    Change-Id: I48d27ce625b371561a955e0c625385a1deba1720

--- tests/unit/checks/test_ac_check_dns.py
+++ tests/unit/checks/test_ac_check_dns.py
@@ -8,42 +8,147 @@ import pytest
 
 from tests.testlib import ActiveCheck
 
+from cmk.gui.plugins.wato.active_checks.dns import _transform_check_dns_settings
+
 
 @pytest.mark.parametrize(
     "params, result",
     [
-        (("DESCR", {}), ["-H", "DESCR", "-s", "$HOSTADDRESS$", "-L"]),
         (
-            (
-                "DESCR",
-                {
-                    "expected_addresses_list": ["1.2.3.4", "5.6.7.8"],
-                },
-            ),
+            {
+                "hostname": "DESCR",
+                "server": None,
+            },
+            ["-H", "DESCR", "-s", "$HOSTADDRESS$", "-L"],
+        ),
+        (
+            {
+                "hostname": "DESCR",
+                "server": None,
+                "expected_addresses_list": ["1.2.3.4", "5.6.7.8"],
+            },
             ["-H", "DESCR", "-s", "$HOSTADDRESS$", "-L", "-a", "1.2.3.4", "-a", "5.6.7.8"],
         ),
         (
-            (
-                "DESCR",
-                {
-                    "expect_all_addresses": True,
-                    "expected_addresses_list": ["5.6.7.8", "1.2.3.4"],
-                },
-            ),
+            {
+                "hostname": "DESCR",
+                "server": None,
+                "expect_all_addresses": True,
+                "expected_addresses_list": ["5.6.7.8", "1.2.3.4"],
+            },
             ["-H", "DESCR", "-s", "$HOSTADDRESS$", "-L", "-a", "5.6.7.8", "-a", "1.2.3.4"],
         ),
         (
-            (
-                "DESCR",
-                {
-                    "expect_all_addresses": False,
-                    "expected_addresses_list": ["1.2.3.4", "5.6.7.8"],
-                },
-            ),
+            {
+                "hostname": "DESCR",
+                "server": None,
+                "expect_all_addresses": False,
+                "expected_addresses_list": ["1.2.3.4", "5.6.7.8"],
+            },
             ["-H", "DESCR", "-s", "$HOSTADDRESS$", "-a", "1.2.3.4", "-a", "5.6.7.8"],
+        ),
+        (
+            {
+                "hostname": "google.de",
+                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
+                "server": "127.0.0.53",
+                "timeout": 10,
+                "response_time": (1.0, 2.0),
+                "expected_authority": True,
+            },
+            [
+                "-H",
+                "google.de",
+                "-s",
+                "127.0.0.53",
+                "-L",
+                "-a",
+                "1.2.3.4",
+                "-a",
+                "C0FE::FE11",
+                "-A",
+                "-w",
+                "1.000000",
+                "-c",
+                "2.000000",
+                "-t",
+                10,
+            ],
         ),
     ],
 )
 def test_ac_check_dns_expected_addresses(params, result):
     active_check = ActiveCheck("check_dns")
     assert active_check.run_argument_function(params) == result
+
+
+@pytest.mark.parametrize(
+    "params, result",
+    [
+        [
+            (
+                "google.de",
+                {},
+            ),
+            {
+                "hostname": "google.de",
+                "server": None,
+            },
+        ],
+        [
+            (
+                "google.de",
+                {"expected_address": "1.2.3.4,C0FE::FE11"},
+            ),
+            {
+                "hostname": "google.de",
+                "server": None,
+                "expect_all_addresses": True,
+                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
+            },
+        ],
+        [
+            (
+                "google.de",
+                {
+                    "expected_address": "1.2.3.4,C0FE::FE11",
+                    "server": "127.0.0.53",
+                    "timeout": 10,
+                    "response_time": (1.0, 2.0),
+                    "expected_authority": True,
+                },
+            ),
+            {
+                "hostname": "google.de",
+                "expect_all_addresses": True,
+                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
+                "server": "127.0.0.53",
+                "timeout": 10,
+                "response_time": (1.0, 2.0),
+                "expected_authority": True,
+            },
+        ],
+        [
+            (
+                "google.de",
+                {
+                    "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
+                    "server": "127.0.0.53",
+                    "timeout": 10,
+                    "response_time": (1.0, 2.0),
+                    "expected_authority": True,
+                },
+            ),
+            {
+                "hostname": "google.de",
+                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
+                "server": "127.0.0.53",
+                "timeout": 10,
+                "response_time": (1.0, 2.0),
+                "expected_authority": True,
+            },
+        ],
+    ],
+)
+def test_legacy_params(params, result):
+    assert _transform_check_dns_settings(params) == result

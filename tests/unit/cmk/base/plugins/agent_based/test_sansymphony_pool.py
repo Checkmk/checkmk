8eabe026bbb94c6d7efba80a8e608fcc76b44fb4
Lisa Pichler <lisa.pichler@tribe29.com>
2022-05-19 12:53:05 +0200

    sansymphony_pool: remove fix register from unit tests
    Change-Id: I68e145745808bfec478fcb4624a1b3af48c9c811

--- tests/unit/cmk/base/plugins/agent_based/test_sansymphony_pool.py
+++ tests/unit/cmk/base/plugins/agent_based/test_sansymphony_pool.py
@@ -4,44 +4,22 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import pytest
 
-from tests.unit.conftest import FixRegister
-
-from cmk.utils.type_defs import CheckPluginName, SectionName
-
-from cmk.base.api.agent_based.checking_classes import CheckPlugin
-from cmk.base.api.agent_based.type_defs import AgentSectionPlugin
 from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
-from cmk.base.plugins.agent_based.sansymphony_pool import SansymphonyPool
+from cmk.base.plugins.agent_based.sansymphony_pool import (
+    check_sansymphony_pool,
+    discover_sansymphony_pool,
+    parse_sansymphony_pool,
+    SansymphonyPool,
+)
 
 
-@pytest.fixture(scope="session", name="check_plugin")
-def check_plugin_from_fix_register(fix_register: FixRegister) -> CheckPlugin:
-    return fix_register.check_plugins[CheckPluginName("sansymphony_pool")]
+def test_parse_sansymphony_pool_no_agent_output() -> None:
+    assert parse_sansymphony_pool([]) == {}
 
 
-@pytest.fixture(scope="session", name="section_plugin")
-def section_plugin_from_fix_register(fix_register: FixRegister) -> AgentSectionPlugin:
-    return fix_register.agent_sections[SectionName("sansymphony_pool")]
-
-
-def test_parse_sansymphony_pool_no_agent_output(section_plugin: AgentSectionPlugin) -> None:
-    assert section_plugin.parse_function([]) == {}
-
-
-def test_parse_sansymphony_pool(section_plugin: AgentSectionPlugin) -> None:
-    assert section_plugin.parse_function(
-        [
-            [
-                "Disk_pool_1",
-                "57",
-                "Running",
-                "ReadWrite",
-                "Dynamic",
-            ]
-        ]
-    ) == {
+def test_parse_sansymphony_pool() -> None:
+    assert parse_sansymphony_pool([["Disk_pool_1", "57", "Running", "ReadWrite", "Dynamic",]]) == {
         "Disk_pool_1": SansymphonyPool(
             name="Disk_pool_1",
             percent_allocated=57.0,
@@ -52,13 +30,13 @@ def test_parse_sansymphony_pool(section_plugin: AgentSectionPlugin) -> None:
     }
 
 
-def test_discover_sansymphony_pool_no_agent_output(check_plugin: CheckPlugin) -> None:
-    assert list(check_plugin.discovery_function({})) == []
+def test_discover_sansymphony_pool_no_agent_output() -> None:
+    assert list(discover_sansymphony_pool({})) == []
 
 
-def test_discover_sansymphony_pool(check_plugin: CheckPlugin) -> None:
+def test_discover_sansymphony_pool() -> None:
     assert list(
-        check_plugin.discovery_function(
+        discover_sansymphony_pool(
             {
                 "Disk_pool_1": SansymphonyPool(
                     name="Disk_pool_1",
@@ -72,7 +50,7 @@ def test_discover_sansymphony_pool(check_plugin: CheckPlugin) -> None:
     ) == [Service(item="Disk_pool_1")]
 
 
-def test_check_sansymphony_pool_item_not_found(check_plugin: CheckPlugin) -> None:
+def test_check_sansymphony_pool_item_not_found() -> None:
     section = {
         "Disk_pool_1": SansymphonyPool(
             name="Disk_pool_1",
@@ -84,7 +62,7 @@ def test_check_sansymphony_pool_item_not_found(check_plugin: CheckPlugin) -> Non
     }
     assert (
         list(
-            check_plugin.check_function(
+            check_sansymphony_pool(
                 item="Disk_pool_2",
                 params={"allocated_pools_percentage_upper": (80.0, 90.0)},
                 section=section,
@@ -94,9 +72,7 @@ def test_check_sansymphony_pool_item_not_found(check_plugin: CheckPlugin) -> Non
     )
 
 
-def test_check_sansymphony_pool_status_running_cache_mode_readwrite(
-    check_plugin: CheckPlugin,
-) -> None:
+def test_check_sansymphony_pool_status_running_cache_mode_readwrite() -> None:
     section = {
         "Disk_pool_1": SansymphonyPool(
             name="Disk_pool_1",
@@ -107,7 +83,7 @@ def test_check_sansymphony_pool_status_running_cache_mode_readwrite(
         ),
     }
     assert list(
-        check_plugin.check_function(
+        check_sansymphony_pool(
             item="Disk_pool_1",
             params={"allocated_pools_percentage_upper": (80.0, 90.0)},
             section=section,
@@ -122,9 +98,7 @@ def test_check_sansymphony_pool_status_running_cache_mode_readwrite(
     ]
 
 
-def test_check_sansymphony_pool_status_running_cache_mode_read(
-    check_plugin: CheckPlugin,
-) -> None:
+def test_check_sansymphony_pool_status_running_cache_mode_read() -> None:
     section = {
         "Disk_pool_1": SansymphonyPool(
             name="Disk_pool_1",
@@ -135,7 +109,7 @@ def test_check_sansymphony_pool_status_running_cache_mode_read(
         ),
     }
     assert list(
-        check_plugin.check_function(
+        check_sansymphony_pool(
             item="Disk_pool_1",
             params={"allocated_pools_percentage_upper": (80.0, 90.0)},
             section=section,
@@ -150,9 +124,7 @@ def test_check_sansymphony_pool_status_running_cache_mode_read(
     ]
 
 
-def test_check_sansymphony_pool_status_stale(
-    check_plugin: CheckPlugin,
-) -> None:
+def test_check_sansymphony_pool_status_stale() -> None:
     section = {
         "Disk_pool_1": SansymphonyPool(
             name="Disk_pool_1",
@@ -163,7 +135,7 @@ def test_check_sansymphony_pool_status_stale(
         ),
     }
     assert list(
-        check_plugin.check_function(
+        check_sansymphony_pool(
             item="Disk_pool_1",
             params={"allocated_pools_percentage_upper": (80.0, 90.0)},
             section=section,
@@ -178,9 +150,7 @@ def test_check_sansymphony_pool_status_stale(
     ]
 
 
-def test_check_sansymphony_pool_percent_allocated(
-    check_plugin: CheckPlugin,
-) -> None:
+def test_check_sansymphony_pool_percent_allocated() -> None:
     section = {
         "Disk_pool_1": SansymphonyPool(
             name="Disk_pool_1",
@@ -191,7 +161,7 @@ def test_check_sansymphony_pool_percent_allocated(
         ),
     }
     assert list(
-        check_plugin.check_function(
+        check_sansymphony_pool(
             item="Disk_pool_1",
             params={"allocated_pools_percentage_upper": (80.0, 90.0)},
             section=section,

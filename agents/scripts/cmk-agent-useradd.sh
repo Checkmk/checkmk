915c6f92b37d25d1b50fe59662d7a4a2df17a427
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-05-20 12:31:46 +0200

    14094 FIX Permission error during agent controller installation
    A regression in Checkmk 2.1.0b9 could lead to a permission error during the installation of the Checkmk agent.
    
    Change-Id: I16ddb8b59cf8b0cb99fb08b57705b73a7649d555

--- agents/scripts/cmk-agent-useradd.sh
+++ agents/scripts/cmk-agent-useradd.sh
@@ -58,11 +58,11 @@ main() {
     # Create home directory manually instead of doing this on user creation,
     # because it might already exist with wrong ownership
     mkdir -p ${HOMEDIR}
+    chown -R cmk-agent:cmk-agent ${HOMEDIR}
     if [ "${user_is_new}" ]; then
         _allow_legacy_pull
         _issue_legacy_pull_warning
     fi
-    chown -R cmk-agent:cmk-agent ${HOMEDIR}
     unset homedir comment usershell
 
 }

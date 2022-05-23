bda93dafe422029ddf874ef2eff0aa1e86bcf287
Konstantin Baikov <konstantin.baikov@tribe29.com>
2022-05-23 15:00:18 +0200

    Extend mobileiron tests
    This includes several partitions tests
    and long results with more than 200 devices
    which require pagination
    
    Change-Id: I99b83f31aee3b0d7e0acea169420e79e46b5e9d0

--- tests/unit/cmk/special_agents/test_agent_mobileiron.py
+++ tests/unit/cmk/special_agents/test_agent_mobileiron.py
@@ -2,8 +2,6 @@
 # Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
-
 import argparse
 import json
 from pathlib import Path
@@ -12,21 +10,49 @@ import responses
 
 from cmk.special_agents.agent_mobileiron import agent_mobileiron_main
 
-URL = "https://example.com:443/api/v1/device?rows=200&start=0&dmPartitionId=103881"
+URL1 = "https://example.com:443/api/v1/device?rows=200&start=0&dmPartitionId=103881"
+URL2 = "https://example.com:443/api/v1/device?rows=200&start=0&dmPartitionId=103882"
+URL3 = "https://example.com:443/api/v1/device?rows=200&start=200&dmPartitionId=103881"
 
-
-test_device = Path(
+test_device1 = Path(
     Path(__file__).parent / "agent_mobileiron_files" / "example_output.json"
+).read_text()
+
+test_device2 = Path(
+    Path(__file__).parent / "agent_mobileiron_files" / "example_output_partition_2.json"
+).read_text()
+
+test_device3 = Path(
+    Path(__file__).parent / "agent_mobileiron_files" / "example_output_overflow.json"
 ).read_text()
 
 
 @responses.activate
-def test_agent_output(capsys):
-    """Agent output contains piggyback data and sections"""
+def test_agent_output_2_partitions(capsys):
+    """Agent output contains piggyback data and sections from both partitions in args
+    and from the second page of the first URL (in case total number of devices is more than 200)"""
+    # standard request to the first partition but with more than 300 devices in reply
+    # this triggers the URL3 request
     responses.add(
         responses.GET,
-        URL,
-        json=json.loads(test_device),
+        URL1,
+        json=json.loads(test_device1),
+        status=200,
+    )
+
+    # standard request to the second partition
+    responses.add(
+        responses.GET,
+        URL2,
+        json=json.loads(test_device2),
+        status=200,
+    )
+
+    # additional request to the first partition with the &start=200 param
+    responses.add(
+        responses.GET,
+        URL3,
+        json=json.loads(test_device3),
         status=200,
     )
     args = argparse.Namespace(
@@ -34,7 +60,7 @@ def test_agent_output(capsys):
         port="443",
         username="",
         password="",
-        partition=[103881],
+        partition=[103881, 103882],
         no_cert_check=False,
         proxy_host=None,
         proxy_port=None,
@@ -48,6 +74,16 @@ def test_agent_output(capsys):
     assert captured.err == "", "stderr is not empty"
     assert (
         """<<<<device1>>>>\n<<<mobileiron_section:sep(0)>>>\n{""" in captured.out
+    ), "piggyback header is not correct"
+
+    # make sure the device from the second partition is in
+    assert (
+        """<<<<partition2device1>>>>\n<<<mobileiron_section:sep(0)>>>\n{""" in captured.out
+    ), "piggyback header is not correct"
+
+    # make sure the device from the overflow is in
+    assert (
+        """<<<<overflowdevice1>>>>\n<<<mobileiron_section:sep(0)>>>\n{""" in captured.out
     ), "piggyback header is not correct"
 
     # make sure the latest registration is chosen

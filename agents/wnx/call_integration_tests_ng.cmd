d05026c35f8138e59f7e9c10b872f2552d99adb1
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-05-28 18:18:59 +0200

    Polish a bit integration script
    CMK-3963
    
    Change-Id: I372908eb6ba26ca8776a0510882b8c834ef68b3e

--- agents/wnx/call_integration_tests_ng.cmd
+++ agents/wnx/call_integration_tests_ng.cmd
@@ -12,21 +12,24 @@ set cur_dir=%cd%
 set WNX_INTEGRATION_BASE_DIR=%temp%\test_wnx_%random%
 set DEBUG_HOME_DIR=%WNX_INTEGRATION_BASE_DIR%\test\data
 set WNX_TEST_ROOT=%WNX_INTEGRATION_BASE_DIR%\test\root
-::net stop checkmkservice
 set arte=%cur_dir%\..\..\artefacts
-set results=integration_ng_tests_results.zip
+set results=integration_tests_ng_results.zip
 
 powershell Write-Host "NG Integration Tests are starting in %WNX_INTEGRATION_BASE_DIR%" -Foreground Cyan
-:: 
+
+:: Firewall processing
 echo fw rule - AllowIntegrationNg1
 powershell Remove-NetFirewallRule -DisplayName "AllowIntegrationNg1" 2>nul
 powershell New-NetFirewallRule -DisplayName "AllowIntegrationMg1" -Direction Inbound -Program %WNX_TEST_ROOT%\check_mk_agent.exe -RemoteAddress LocalSubnet -Action Allow >nul
 echo fw rule - AllowIntegrationNg2
 powershell Remove-NetFirewallRule -DisplayName "AllowIntegrationNg2" 2>nul
 powershell New-NetFirewallRule -DisplayName "AllowIntegrationNg2" -Direction Inbound -Program %DEBUG_HOME_DIR%\bin\cmk-agent-ctl.exe -RemoteAddress LocalSubnet -Action Allow >nul
-:: tests wait for this env variable
+
+:: TEST!
 py -3 -m pytest tests\integration\%* || set failed=1
 
+
+:: Firewall processing again
 echo fw rules deletion...
 powershell Remove-NetFirewallRule -DisplayName "AllowIntegrationNg1" 2>nul
 powershell Remove-NetFirewallRule -DisplayName "AllowIntegrationNg2" 2>nul
@@ -40,7 +43,7 @@ exit /b 0
 powershell Write-Host "Integration Test Success" -Foreground Green
 exit /b 0
 
-
+:: NOT REACHABLE
 :zip_results
 ren %arte%\%results% %arte%\%results%.sav 2>nul
 pushd %WNX_INTEGRATION_BASE_DIR% && ( call :zip_and_remove & popd )

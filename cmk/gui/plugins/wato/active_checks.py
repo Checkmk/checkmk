516811eef93eaf64b271a824d36503197fc53679
Lars Michelsen <lm@tribe29.com>
2022-05-31 17:57:22 +0200

    Revert "14130 FIX Clarify "Check DNS service""
    This reverts commit b048285f8e834f5bde31930935b07840c73f9689.
    
    The change broke existing check_dns configurations. Reverting for now to
    fix it separately.
    
    Change-Id: Ie96481c2ca62614a555e2afb06a3730d8c470d49

--- cmk/gui/plugins/wato/active_checks.py
+++ cmk/gui/plugins/wato/active_checks.py
@@ -5,9 +5,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import copy
-from typing import Any, Mapping
-from typing import Tuple as TypeTuple
-from typing import Union
+from typing import Any, Mapping, Union
 
 from cmk.gui.exceptions import MKUserError
 from cmk.gui.i18n import _
@@ -489,34 +487,18 @@ rulespec_registry.register(
 )
 
 
-def _transform_check_dns_settings(
-    params: Union[Mapping[str, Any], TypeTuple[str, Mapping[str, Any]]]
-) -> Mapping[str, Any]:
-    if isinstance(params, Mapping):
-        return params
-
-    hostname, optional_params = params
-    if "server" not in optional_params:
-        optional_params = {"server": None, **optional_params}
-    return {
-        "hostname": hostname,
-        **(
-            __transform_legacy_optional_params(optional_params)
-            if "expected_address" in optional_params
-            else optional_params
-        ),
-    }
-
-
-def __transform_legacy_optional_params(optional_params: Mapping[str, Any]) -> Mapping[str, Any]:
+def _transform_check_dns_settings(params: Mapping[str, Any]) -> Mapping[str, Any]:
     """
-    >>> __transform_legacy_optional_params({'expected_address': '1.2.3.4,C0FE::FE11'})
+    >>> _transform_check_dns_settings({'expected_address': '1.2.3.4,C0FE::FE11'})
     {'expect_all_addresses': True, 'expected_addresses_list': ['1.2.3.4', 'C0FE::FE11']}
-    >>> __transform_legacy_optional_params({'expected_address': ['A,B', 'C']})
+    >>> _transform_check_dns_settings({'expected_address': ['A,B', 'C']})
     {'expect_all_addresses': True, 'expected_addresses_list': ['A', 'B', 'C']}
 
     """
-    legacy_addresses = optional_params["expected_address"]
+    legacy_addresses = params.get("expected_address")
+    if legacy_addresses is None:
+        return params
+
     return {
         "expect_all_addresses": True,
         "expected_addresses_list": (
@@ -524,117 +506,117 @@ def __transform_legacy_optional_params(optional_params: Mapping[str, Any]) -> Ma
             if isinstance(legacy_addresses, str)
             else sum((entry.split(",") for entry in legacy_addresses), [])
         ),
-        **{k: v for k, v in optional_params.items() if k != "expected_address"},
+        **{k: v for k, v in params.items() if k != "expected_address"},
     }
 
 
 def _valuespec_active_checks_dns():
-    return Transform(
-        valuespec=Dictionary(
-            title=_("Check DNS service"),
-            help=_(
-                "Check the resolution of a hostname into an IP address by a DNS "
-                "server. This check uses <tt>check_dns</tt> from the standard "
-                "Nagios plugins. Note, that check_dns will always be executed in "
-                "the monitoring site. By default, the configured host(s) that "
-                "this rule applies to is used as DNS server. This behaviour can "
-                "be configured by using the option <tt>DNS Server</tt>. "
-            ),
-            elements=[
-                (
-                    "hostname",
-                    TextInput(
-                        title=_("Queried Hostname or IP address"),
-                        allow_empty=False,
-                        help=_("The name or IPv4 address you want to query"),
-                    ),
-                ),
-                (
-                    "name",
-                    TextInput(
-                        title=_("Alternative Service description"),
-                        help=_(
-                            "The service description will be this name instead <i>DNS Servername</i>"
-                        ),
-                    ),
-                ),
-                (
-                    "server",
-                    Alternative(
-                        title=_("DNS Server"),
-                        help=_("The DNS server you want to use for the lookup"),
-                        elements=[
-                            FixedValue(
-                                value=None,
-                                title=_(
-                                    "Use the address of the host for which the service is generated"
-                                ),
-                                totext=_("This option is set by default."),
-                            ),
-                            TextInput(
-                                title=_("Specify DNS Server"),
-                                allow_empty=False,
-                            ),
-                            FixedValue(
-                                value="default DNS server",
-                                title=_(
-                                    "Use the default DNS server(s) specified in /etc/resolv.conf"
-                                ),
-                            ),
-                        ],
-                    ),
-                ),
-                (
-                    "expect_all_addresses",
-                    DropdownChoice(
-                        title=_("Address matching"),
-                        choices=[
-                            (True, _("Expect all of the addresses")),
-                            (False, _("Expect at least one of the addresses")),
-                        ],
-                    ),
-                ),
-                (
-                    "expected_addresses_list",
-                    ListOfStrings(
-                        title=_("Expected DNS answers"),
-                        help=_(
-                            "List all allowed expected answers here. If query for an "
-                            "IP address then the answer will be host names, that end "
-                            "with a dot."
-                        ),
-                    ),
-                ),
-                (
-                    "expected_authority",
-                    FixedValue(
-                        value=True,
-                        title=_("Expect Authoritative DNS Server"),
-                        totext=_("Expect Authoritative"),
-                    ),
-                ),
-                (
-                    "response_time",
-                    Tuple(
-                        title=_("Expected response time"),
-                        elements=[
-                            Float(title=_("Warning if above"), unit=_("sec"), default_value=1),
-                            Float(title=_("Critical if above"), unit=_("sec"), default_value=2),
-                        ],
-                    ),
-                ),
-                (
-                    "timeout",
-                    Integer(
-                        title=_("Seconds before connection times out"),
-                        unit=_("sec"),
-                        default_value=10,
-                    ),
-                ),
-            ],
-            required_keys=["hostname", "server"],
+    return Tuple(
+        title=_("Check DNS service"),
+        help=_(
+            "Check the resolution of a hostname into an IP address by a DNS server. "
+            "This check uses <tt>check_dns</tt> from the standard Nagios plugins."
         ),
-        forth=_transform_check_dns_settings,
+        elements=[
+            TextInput(
+                title=_("Queried Hostname or IP address"),
+                allow_empty=False,
+                help=_("The name or IPv4 address you want to query"),
+            ),
+            Transform(
+                Dictionary(
+                    title=_("Optional parameters"),
+                    elements=[
+                        (
+                            "name",
+                            TextInput(
+                                title=_("Alternative Service description"),
+                                help=_(
+                                    "The service description will be this name instead <i>DNS Servername</i>"
+                                ),
+                            ),
+                        ),
+                        (
+                            "server",
+                            Alternative(
+                                title=_("DNS Server"),
+                                elements=[
+                                    FixedValue(
+                                        value=None,
+                                        totext=_("this host"),
+                                        title=_("Use this host as a DNS server for the lookup"),
+                                    ),
+                                    TextInput(
+                                        title=_("Specify DNS Server"),
+                                        allow_empty=False,
+                                        help=_(
+                                            "Optional DNS server you want to use for the lookup"
+                                        ),
+                                    ),
+                                    FixedValue(
+                                        value="default DNS server",
+                                        totext=_("default DNS server"),
+                                        title=_("Use default DNS server"),
+                                    ),
+                                ],
+                            ),
+                        ),
+                        (
+                            "expect_all_addresses",
+                            DropdownChoice(
+                                title=_("Address matching"),
+                                choices=[
+                                    (True, _("Expect all of the addresses")),
+                                    (False, _("Expect at least one of the addresses")),
+                                ],
+                            ),
+                        ),
+                        (
+                            "expected_addresses_list",
+                            ListOfStrings(
+                                title=_("Expected DNS answers"),
+                                help=_(
+                                    "List all allowed expected answers here. If query for an "
+                                    "IP address then the answer will be host names, that end "
+                                    "with a dot."
+                                ),
+                            ),
+                        ),
+                        (
+                            "expected_authority",
+                            FixedValue(
+                                value=True,
+                                title=_("Expect Authoritative DNS Server"),
+                                totext=_("Expect Authoritative"),
+                            ),
+                        ),
+                        (
+                            "response_time",
+                            Tuple(
+                                title=_("Expected response time"),
+                                elements=[
+                                    Float(
+                                        title=_("Warning if above"), unit=_("sec"), default_value=1
+                                    ),
+                                    Float(
+                                        title=_("Critical if above"), unit=_("sec"), default_value=2
+                                    ),
+                                ],
+                            ),
+                        ),
+                        (
+                            "timeout",
+                            Integer(
+                                title=_("Seconds before connection times out"),
+                                unit=_("sec"),
+                                default_value=10,
+                            ),
+                        ),
+                    ],
+                ),
+                forth=_transform_check_dns_settings,
+            ),
+        ],
     )
 
 

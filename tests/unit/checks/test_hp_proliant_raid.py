2bab3a64e701dc6a62a3f70e8a51a627d1e60356
Lisa Pichler <lisa.pichler@tribe29.com>
2022-05-31 09:02:59 +0200

    hp_proliant_raid: unit tests
    * "migrate" generic test
    * new test case for rebuilds that do not have a progress percentage
    
    SUP-10361
    
    Change-Id: Ib1c3c11a8fab036e64e5eccb362551bf7525e35b

---
+++ tests/unit/checks/test_hp_proliant_raid.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from tests.unit.conftest import FixRegister
+
+from cmk.utils.type_defs import CheckPluginName, SectionName
+
+from cmk.base.api.agent_based.checking_classes import CheckPlugin
+from cmk.base.api.agent_based.type_defs import SNMPSectionPlugin
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, Service, State
+
+
+@pytest.fixture(name="check_plugin")
+def check_plugin_from_fix_register(fix_register: FixRegister) -> CheckPlugin:
+    return fix_register.check_plugins[CheckPluginName("hp_proliant_raid")]
+
+
+@pytest.fixture(name="section_plugin")
+def section_plugin_from_fix_register(fix_register: FixRegister) -> SNMPSectionPlugin:
+    return fix_register.snmp_sections[SectionName("hp_proliant_raid")]
+
+
+@pytest.fixture(name="string_table")
+def snmp_section():
+    return [
+        [
+            ["1", "", "2", "286070", "4294967295"],
+            ["2", "", "2", "25753986", "4294967295"],
+            ["3", "", "2", "30523320", "4294967295"],
+            ["4", "", "2", "15", "4294967295"],
+            ["5", "", "2", "15", "4294967295"],
+            ["6", "", "2", "17169273", "4294967295"],
+        ],
+    ]
+
+
+def test_discover_hp_proliant_raid_no_snmp_data(
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+) -> None:
+    assert list(check_plugin.discovery_function({})) == []
+
+
+def test_discover_hp_proliant_raid_aa(
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+    string_table,
+) -> None:
+    discovery_results = list(
+        check_plugin.discovery_function(section_plugin.parse_function(string_table))
+    )
+    assert discovery_results[1:] == [
+        Service(item="2"),
+        Service(item="3"),
+        Service(item="4"),
+        Service(item="5"),
+        Service(item="6"),
+    ]
+
+
+def test_check_hp_proliant_raid_item_not_found(
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+    string_table,
+) -> None:
+    assert (
+        list(
+            check_plugin.check_function(
+                item="!111elf",
+                params={},
+                section=section_plugin.parse_function(string_table),
+            )
+        )
+        == []
+    )
+
+
+def test_check_hp_proliant_raid(
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+    string_table,
+) -> None:
+    assert list(
+        check_plugin.check_function(
+            item="",
+            params={},
+            section=section_plugin.parse_function(string_table),
+        )
+    ) == [
+        Result(state=State.OK, summary="Status: OK, Logical volume size: 279.37 GB"),
+    ]
+
+
+def test_check_hp_proliant_raid_progress_cannot_be_determined(
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+) -> None:
+    assert list(
+        check_plugin.check_function(
+            item="banana",
+            params={},
+            section=section_plugin.parse_function(
+                [
+                    [
+                        ["1", "banana", "7", "286070", "4294967295"],
+                    ],
+                ]
+            ),
+        )
+    ) == [
+        Result(
+            state=State.WARN,
+            summary="Status: rebuilding, Logical volume size: 279.37 GBRebuild: 4294967295%",
+        )
+    ]

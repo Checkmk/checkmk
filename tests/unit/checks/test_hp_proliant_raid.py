daee694680497cb722111b5a5cb3444364d864f5
Lisa Pichler <lisa.pichler@tribe29.com>
2022-05-31 09:02:59 +0200

    hp_proliant_raid: unit tests
    * "migrate" generic test
    * new test case for rebuilds that do not have a progress percentage
    
    SUP-10361
    
    Change-Id: Ib1c3c11a8fab036e64e5eccb362551bf7525e35b

---
+++ tests/unit/checks/test_hp_proliant_raid.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from testlib import Check  # type: ignore[import]
+
+
+@pytest.fixture(name="string_table")
+def snmp_section():
+    return [
+        ["1", "", "2", "286070", "4294967295"],
+        ["2", "", "2", "25753986", "4294967295"],
+        ["3", "", "2", "30523320", "4294967295"],
+        ["4", "", "2", "15", "4294967295"],
+        ["5", "", "2", "15", "4294967295"],
+        ["6", "", "2", "17169273", "4294967295"],
+    ]
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_discover_hp_proliant_raid_no_snmp_data() -> None:
+    check_plugin = Check("hp_proliant_raid")
+    assert list(check_plugin.run_discovery({})) == []
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_discover_hp_proliant_raid(string_table,) -> None:
+    check_plugin = Check("hp_proliant_raid")
+    discovery_results = list(check_plugin.run_discovery(check_plugin.run_parse(string_table)))
+    assert discovery_results[1:] == [
+        ("2", None),
+        ("3", None),
+        ("4", None),
+        ("5", None),
+        ("6", None),
+    ]
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_hp_proliant_raid_item_not_found(string_table,) -> None:
+    check_plugin = Check("hp_proliant_raid")
+    assert (check_plugin.run_check(
+        "!111elf",
+        {},
+        check_plugin.run_parse(string_table),
+    ) is None)
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_hp_proliant_raid(string_table,) -> None:
+    check_plugin = Check("hp_proliant_raid")
+    assert list(check_plugin.run_check(
+        "",
+        {},
+        check_plugin.run_parse(string_table),
+    )) == [
+        0,
+        "Status: OK, Logical volume size: 279.37 GB",
+    ]
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_hp_proliant_raid_progress_cannot_be_determined() -> None:
+    check_plugin = Check("hp_proliant_raid")
+    assert list(
+        check_plugin.run_check(
+            "banana",
+            {},
+            check_plugin.run_parse([
+                ["1", "banana", "7", "286070", "4294967295"],
+            ]),
+        )) == [1, "Status: rebuilding, Logical volume size: 279.37 GBRebuild: 4294967295%"]

508f30cd37b066d2e70441b9ee732b1225d84833
Sofia Colakovic <sofia.colakovic@tribe29.com>
2022-06-02 10:29:26 +0200

    tests for sentry_pdu_outlets
    Change-Id: I3fcdd6fa75deeb8a9b4e9c9d5b9236063c2edb10

--- tests/unit/cmk/base/plugins/agent_based/test_sentry_pdu_outlets.py
+++ tests/unit/cmk/base/plugins/agent_based/test_sentry_pdu_outlets.py
@@ -4,8 +4,16 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Mapping, Sequence
+
 import pytest
 
+from tests.unit.conftest import FixRegister
+
+from cmk.utils.type_defs import CheckPluginName
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, Service, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import CheckResult, DiscoveryResult
 from cmk.base.plugins.agent_based.sentry_pdu_outlets import parse_sentry_pdu_outlets
 
 
@@ -16,3 +24,45 @@ from cmk.base.plugins.agent_based.sentry_pdu_outlets import parse_sentry_pdu_out
 def test_parse_sentry_pdu_outlets(string_table, expected_section):
     section = parse_sentry_pdu_outlets(string_table)
     assert section == expected_section
+
+
+@pytest.mark.parametrize(
+    "parsed, expected_discovery",
+    [({"A1 A_1": 0, "A2 A_2": 3}, [Service(item="A1 A_1"), Service(item="A2 A_2")])],
+)
+def test_inventory_sentry_pdu_outlets(
+    fix_register: FixRegister,
+    parsed: Mapping[str, int],
+    expected_discovery: Sequence[DiscoveryResult],
+) -> None:
+    check_plugin = fix_register.check_plugins[CheckPluginName("sentry_pdu_outlets")]
+    result = list(check_plugin.discovery_function(parsed))
+    assert result == expected_discovery
+
+
+@pytest.mark.parametrize(
+    "parsed, item, expected_result",
+    [
+        pytest.param(
+            {"A1 A_1": 0, "A2 A_2": 3},
+            "A1 A_1",
+            [Result(state=State.OK, summary="Status: off")],
+            id="known_state",
+        ),
+        pytest.param(
+            {"A1 A_1": 0, "A2 A_2": 33},
+            "A2 A_2",
+            [Result(state=State.UNKNOWN, summary="Unhandled state: 33")],
+            id="unknown_state",
+        ),
+    ],
+)
+def test_check_sentry_pdu_outlets(
+    fix_register: FixRegister,
+    parsed: Mapping[str, int],
+    item: str,
+    expected_result: Sequence[CheckResult],
+) -> None:
+    check_plugin = fix_register.check_plugins[CheckPluginName("sentry_pdu_outlets")]
+    result = list(check_plugin.check_function(item=item, params={}, section=parsed))
+    assert result == expected_result

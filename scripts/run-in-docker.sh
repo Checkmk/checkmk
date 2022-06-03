2ead62094c709e401c161db60ca62281475bc10c
Frans Fürst <frans.fuerst@tribe29.com>
2022-06-03 06:21:53 +0200

    run-in-docker: enable containerized and native work in the same worktree
    In short this change
    * .. allows both containerized and native scenario in the same worktree
    * .. only addresses local development
    * .. introduces a $HOME directory for the container
    * .. should not affect the way scripts are being run
    * .. does not affect the native scenario
    * .. should not affect CI builds
    
    Currently a given check_mk clone/worktree has to be used in
    acontainerized or a native context. Both at a time are not possible
    because of conflicting artifacts directories (currently only
    `omd/build/` and `.venv/`). In an ideal world those directories could be
    located in separate dedicated build directories but that's currently not
    the case.
    This change makes `run-in-docker.sh` provide separate artifact
    directories for the containerized case by mapping them on top of those
    for the native build. Additionally a dedicated $HOME directory is being
    created and passed as environment variable to address caching and user
    based state.
    
    There is room for some improvement regarding initial build dependencies
    forcing `make .venv` to be triggered every time a command is being
    exectuted in containerized context (doesn't take too long, though).
    
    Change-Id: Ib8ea77b97518c9a1a6808f44ff0e0ca0cfde8a23

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -14,10 +14,42 @@ REPO_DIR="$(git rev-parse --show-toplevel)"
 # in case of worktrees $REPO_DIR might not contain the actual repository clone
 GIT_COMMON_DIR="$(realpath "$(git rev-parse --git-common-dir)")"
 
+CMD="$*"
+
+# This block makes sure a local containerized session does not interfere with
+# native builds. Maybe in the future this script should not be executed in
+# a CI environment (since those come with their own containerization solutions)
+# rendering this distinction unnessesary.
+if [ "$USER" != "jenkins" ]; then
+    # Create directories which otherwise would get created by root
+    # rendering the native build broken
+    mkdir -p "${REPO_DIR}/.venv"
+    mkdir -p "${REPO_DIR}/omd/build"
+    mkdir -p "${REPO_DIR}/build_user_home/"
+
+    if [ ! -d "${REPO_DIR}/.docker_workspace/venv" ]; then
+        CMD="touch ${REPO_DIR}/Pipfile.lock; $*"
+    fi
+
+    # Create directories for build artifacts which we want to have separated
+    # in native and containerized builds
+    # Here might be coming more, you keep an open eye, too, please
+
+    mkdir -p "${REPO_DIR}/.docker_workspace/venv"
+    mkdir -p "${REPO_DIR}/.docker_workspace/omd_build"
+    mkdir -p "${REPO_DIR}/.docker_workspace/home"
+    DOCKER_LOCAL_ARGS="
+        -v "${REPO_DIR}/.docker_workspace/venv:${REPO_DIR}/.venv" \
+        -v "${REPO_DIR}/.docker_workspace/omd_build:${REPO_DIR}/omd/build" \
+        -v "${REPO_DIR}/.docker_workspace/home:${REPO_DIR}/build_user_home/" \
+        -e HOME="${REPO_DIR}/build_user_home/" \
+        "
+fi
+
 : "${IMAGE_ALIAS:=IMAGE_TESTING}"
 : "${IMAGE_ID:="$("${REPO_DIR}"/buildscripts/docker_image_aliases/resolve.sh "${IMAGE_ALIAS}")"}"
 
-echo "Running in Docker container from image ${IMAGE_ID} (workdir=${PWD})"
+echo "Running in Docker container from image ${IMAGE_ID} (cmd=$*) (workdir=${PWD})"
 
 # shellcheck disable=SC2086
 docker run -t -a stdout -a stderr \
@@ -26,6 +58,7 @@ docker run -t -a stdout -a stderr \
     -u "${UID}:$(id -g)" \
     -v "${REPO_DIR}:${REPO_DIR}" \
     -v "${GIT_COMMON_DIR}:${GIT_COMMON_DIR}" \
+    ${DOCKER_LOCAL_ARGS} \
     -v "/var/run/docker.sock:/var/run/docker.sock" \
     --group-add="$(getent group docker | cut -d: -f3)" \
     -w "${PWD}" \
@@ -40,4 +73,4 @@ docker run -t -a stdout -a stderr \
     -e CI \
     ${DOCKER_RUN_ADDOPTS} \
     "${IMAGE_ID}" \
-    "$@"
+    sh -c "${CMD}"

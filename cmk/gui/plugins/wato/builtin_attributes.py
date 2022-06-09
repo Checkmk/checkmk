33d988bbcb6e9757498558faef15343aeaa8ef1b
Ronny Bruska <ronny.bruska@tribe29.com>
2022-06-09 14:01:12 +0200

    14328 FIX Fix host search with option "Locked by"
    CMK-10732
    
    Change-Id: If7f152cc33be99252129722c2c3a92c9f81ee1e3

--- cmk/gui/plugins/wato/builtin_attributes.py
+++ cmk/gui/plugins/wato/builtin_attributes.py
@@ -6,7 +6,7 @@
 import time
 
 import cmk.utils.tags
-from cmk.utils.type_defs import HostName, List
+from cmk.utils.type_defs import HostName, List, Union
 
 import cmk.gui.hooks as hooks
 import cmk.gui.userdb as userdb
@@ -1048,6 +1048,11 @@ class HostAttributeLockedBy(ABCHostAttributeValueSpec):
                 "The identity is built out of the Site ID, the program name and the connection ID."
             ),
         )
+
+    def filter_matches(
+        self, crit: list[str], value: Union[list[str], tuple[str, str, str]], hostname
+    ) -> bool:
+        return crit == list(value)
 
 
 class LockedByValuespec(Tuple):

2498cb76628907d5f0aad48cdf33466ccc350606
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-06-12 17:24:26 +0200

    Enable integration tests win-agent
    Integration test are `real` integration tests
    
    CMK-10642
    
    Change-Id: Ia0d9aadae1740b82512c8aa9413f5809ec74d22f

--- agents/wnx/run_integration_tests.cmd
+++ agents/wnx/run_integration_tests.cmd
@@ -17,4 +17,4 @@ if "%p%"=="integration" call scripts\call_integration_tests.cmd && powershell wr
 if not "%p%"=="all" powershell write-Host "Bad parameter %p%" -Foreground Red && exit /b 1
 call call_unit_tests.cmd *Integration && powershell write-Host "Success 1" -Foreground Green  ||  powershell write-Host "FAIL 1" -Foreground Red
 call scripts\call_regression_tests.cmd && powershell write-Host "Success 2" -Foreground Green  ||  powershell write-Host "FAIL 2" -Foreground Red && exit /b 17
-call scripts\call_integration_tests.cmd && powershell write-Host "Success 3" -Foreground Green && exit /b 0  ||  powershell write-Host "FAIL 3" -Foreground Red
+call scripts\call_integration_tests.cmd && powershell write-Host "Success 3" -Foreground Green && exit /b 0  ||  powershell write-Host "FAIL 3" -Foreground Red  && exit /b 18

f14216efdef3bfb4d3c5e1e4c1d719e409b7412c
Anastasiya Liatsetskaya <anastasiya.liatsetskaya@tribe29.com>
2022-06-15 19:31:32 +0200

    vnx_quotas special agent: Add option to use password store
    We will later on add a werk which describes this change for all affected
    special agents.
    
    CMK-6775
    
    Change-Id: I9b8f6acd59b53193f2beeb0506953060c8963f81

--- cmk/gui/plugins/wato/special_agents/vnx_quotas.py
+++ cmk/gui/plugins/wato/special_agents/vnx_quotas.py
@@ -6,8 +6,8 @@
 
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.special_agents.common import RulespecGroupDatasourceProgramsHardware
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import Dictionary, Password, TextInput
+from cmk.gui.plugins.wato.utils import HostRulespec, IndividualOrStoredPassword, rulespec_registry
+from cmk.gui.valuespec import Dictionary, TextInput
 from cmk.gui.watolib.rulespecs import Rulespec
 
 
@@ -21,7 +21,7 @@ def _valuespec_special_agents_vnx_quotas():
         title=_("VNX quotas and filesystems"),
         elements=[
             ("user", TextInput(title=_("NAS DB user name"))),
-            ("password", Password(title=_("Password"))),
+            ("password", IndividualOrStoredPassword(title=_("Password"))),
             ("nas_db", TextInput(title=_("NAS DB path"))),
         ],
         optional_keys=[],
--- tests/unit/checks/test_agent_vnx_quotas.py
+++ tests/unit/checks/test_agent_vnx_quotas.py
@@ -4,23 +4,55 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Any, Mapping, Sequence
+
+import pytest
+
 from tests.testlib import SpecialAgent
 
 
-def test_agent_vnx_quotas_arguments_password_store() -> None:
+@pytest.mark.parametrize(
+    ["params", "expected_args"],
+    [
+        pytest.param(
+            {
+                "user": "username",
+                "password": ("password", "passwd"),
+                "nas_db": "",
+            },
+            [
+                "-u",
+                "username",
+                "-p",
+                "passwd",
+                "--nas-db",
+                "",
+                "1.2.3.4",
+            ],
+            id="with explicit password",
+        ),
+        pytest.param(
+            {
+                "user": "username",
+                "password": ("store", "vnx_quotas"),
+                "nas_db": "",
+            },
+            [
+                "-u",
+                "username",
+                "-p",
+                ("store", "vnx_quotas", "%s"),
+                "--nas-db",
+                "",
+                "1.2.3.4",
+            ],
+            id="with password from store",
+        ),
+    ],
+)
+def test_agent_vnx_quotas_arguments_password_store(
+    params: Mapping[str, Any], expected_args: Sequence[Any]
+) -> None:
 
-    params = {
-        "user": "username",
-        "password": "passwd",
-        "nas_db": "",
-    }
     agent = SpecialAgent("agent_vnx_quotas")
-    assert agent.argument_func(params, "testhost", "1.2.3.4") == [
-        "-u",
-        "username",
-        "-p",
-        "passwd",
-        "--nas-db",
-        "",
-        "1.2.3.4",
-    ]
+    assert agent.argument_func(params, "testhost", "1.2.3.4") == expected_args

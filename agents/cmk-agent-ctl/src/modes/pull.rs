45084efc8a02d7898789e3455d3e40e4f528391b
Joerg Herbel <joerg.herbel@tribe29.com>
2022-06-15 12:34:09 +0200

    14416 FIX Agent controller: Fix agent data transport for IPv4-only hosts
    CMK-10777
    
    Change-Id: I8b6a0690f90ca9c77218e1fe36ecc1ca929932be

--- agents/cmk-agent-ctl/src/modes/pull.rs
+++ agents/cmk-agent-ctl/src/modes/pull.rs
@@ -7,12 +7,12 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::sync::Arc;
 
-use crate::{config, monitoring_data, tls_server, types};
-use anyhow::{anyhow, Context, Error as AnyhowError, Result as AnyhowResult};
+use crate::{config, misc::anyhow_error_to_human_redable, monitoring_data, tls_server, types};
+use anyhow::{anyhow, bail, Context, Error as AnyhowError, Result as AnyhowResult};
 use async_trait::async_trait;
 use log::{debug, info, warn};
 use socket2::{Domain, SockAddr, Socket, Type};
-use std::net::{IpAddr, Ipv6Addr, SocketAddr, TcpListener as TcpListenerStd};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpListener as TcpListenerStd};
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::Semaphore;
@@ -24,13 +24,19 @@ const HEADER_VERSION: &[u8] = b"\x00\x00";
 const ONE_MINUTE: u64 = 60;
 const FIVE_MINUTES: u64 = 300;
 
+struct ListeningConfig {
+    pub addr_v4: Ipv4Addr,
+    pub addr_v6: Ipv6Addr,
+    pub port: u16,
+}
+
 trait PullState {
     fn refresh(&mut self) -> AnyhowResult<()>;
     fn tls_acceptor(&self) -> TlsAcceptor;
     fn allow_legacy_pull(&self) -> bool;
     fn is_active(&self) -> bool;
     fn ip_allowlist(&self) -> &[String];
-    fn listening_address(&self) -> SocketAddr;
+    fn listening_config(&self) -> ListeningConfig;
     fn connection_timeout(&self) -> u64;
 }
 struct PullStateImpl {
@@ -78,8 +84,12 @@ impl PullState for PullStateImpl {
         &self.config.allowed_ip
     }
 
-    fn listening_address(&self) -> SocketAddr {
-        SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), self.config.port)
+    fn listening_config(&self) -> ListeningConfig {
+        ListeningConfig {
+            addr_v4: Ipv4Addr::UNSPECIFIED,
+            addr_v6: Ipv6Addr::UNSPECIFIED,
+            port: self.config.port,
+        }
     }
 
     fn connection_timeout(&self) -> u64 {
@@ -203,29 +213,64 @@ async fn _pull(
             pull_state.refresh()?;
             continue;
         }
-        info!(
-            "Start listening for incoming pull requests on {}.",
-            pull_state.listening_address()
-        );
+        info!("Start listening for incoming pull requests");
         _pull_cycle(&mut pull_state, &mut guard, agent_output_collector.clone()).await?;
     }
 }
 
-fn tcp_listener(listening_address: SocketAddr) -> AnyhowResult<TcpListenerStd> {
-    let socket = Socket::new(Domain::IPV6, Type::STREAM, None)?;
-    socket.set_only_v6(false)?;
+fn configure_socket(socket: Socket) -> AnyhowResult<Socket> {
     socket.set_nonblocking(true)?;
-
     // https://stackoverflow.com/questions/3229860/what-is-the-meaning-of-so-reuseaddr-setsockopt-option-linux
     // Allow re-using the address, even if there are still connections. After sending the agent
     // data, we end up with a waiting connection which will be dropped at some point, but not
     // immediately (this is OK, standard TCP protocol). However, this will block the re-creation
     // of the socket if the agent controller is restarted (agent update or manual restart).
     socket.set_reuse_address(true)?;
+    Ok(socket)
+}
 
-    socket.bind(&SockAddr::from(listening_address))?;
+fn tcp_listener_v4(address: Ipv4Addr, port: u16) -> AnyhowResult<TcpListenerStd> {
+    let socket = configure_socket(Socket::new(Domain::IPV4, Type::STREAM, None)?)?;
+    socket.bind(&SockAddr::from(SocketAddr::new(IpAddr::V4(address), port)))?;
     socket.listen(4096)?;
     Ok(socket.into())
+}
+
+fn tcp_listener_v6(address: Ipv6Addr, port: u16) -> AnyhowResult<TcpListenerStd> {
+    let socket = configure_socket(Socket::new(Domain::IPV6, Type::STREAM, None)?)?;
+    socket.set_only_v6(false)?;
+    socket.bind(&SockAddr::from(SocketAddr::new(IpAddr::V6(address), port)))?;
+    socket.listen(4096)?;
+    Ok(socket.into())
+}
+
+fn tcp_listener(listening_config: ListeningConfig) -> AnyhowResult<TcpListenerStd> {
+    let err_v6 = match tcp_listener_v6(listening_config.addr_v6, listening_config.port) {
+        Ok(listener) => {
+            info!(
+                "Listening on {} for incoming pull connections (IPv6 & IPv4 if activated)",
+                listener.local_addr()?
+            );
+            return Ok(listener);
+        }
+        Err(err_v6) => err_v6,
+    };
+    info!("Failed to open IPv6 socket for pull connections, attempting with IPv4");
+    let err_v4 = match tcp_listener_v4(listening_config.addr_v4, listening_config.port) {
+        Ok(listener) => {
+            info!(
+                "Listening on {} for incoming pull connections (IPv4)",
+                listener.local_addr()?
+            );
+            return Ok(listener);
+        }
+        Err(err_v4) => err_v4,
+    };
+    bail!(
+        "Failed to listen on TCP socket for incoming pull connections.\n\nError with IPV6:\n{}\n\nError with IPV4:\n{}",
+        anyhow_error_to_human_redable(&err_v6),
+        anyhow_error_to_human_redable(&err_v4),
+    );
 }
 
 async fn _pull_cycle(
@@ -233,7 +278,7 @@ async fn _pull_cycle(
     guard: &mut MaxConnectionsGuard,
     agent_output_collector: impl AgentOutputCollector,
 ) -> AnyhowResult<()> {
-    let listener = TcpListener::from_std(tcp_listener(pull_state.listening_address())?)?;
+    let listener = TcpListener::from_std(tcp_listener(pull_state.listening_config())?)?;
 
     loop {
         let (stream, remote) = match match timeout(
@@ -428,23 +473,52 @@ mod tests {
         assert_eq!(agout.encode(b"abc").unwrap(), expected_result);
     }
 
-    const LISTENER_PORT: u16 = 45147;
-    fn expected_addr() -> String {
-        format!("[::1]:{}", LISTENER_PORT)
+    fn listening_config(port: u16) -> ListeningConfig {
+        ListeningConfig {
+            addr_v4: Ipv4Addr::UNSPECIFIED,
+            addr_v6: Ipv6Addr::UNSPECIFIED,
+            port,
+        }
+    }
+
+    // we rely on our CI system using IPv6
+    #[test]
+    fn test_tcp_listener() {
+        let port = 45147;
+        assert_eq!(
+            tcp_listener(listening_config(port))
+                .unwrap()
+                .local_addr()
+                .unwrap()
+                .to_string(),
+            format!("[::]:{}", port)
+        );
+    }
+
+    // we rely on our CI system using IPv6
+    #[test]
+    fn test_tcp_listener_v6() {
+        let lc = listening_config(45148);
+        assert_eq!(
+            tcp_listener_v6(lc.addr_v6, lc.port)
+                .unwrap()
+                .local_addr()
+                .unwrap()
+                .to_string(),
+            format!("[::]:{}", lc.port)
+        );
     }
 
     #[test]
-    fn test_tcp_listener() {
+    fn test_tcp_listener_ipv4() {
+        let lc = listening_config(45149);
         assert_eq!(
-            tcp_listener(SocketAddr::new(
-                IpAddr::from_str("::1").unwrap(),
-                LISTENER_PORT
-            ))
-            .unwrap()
-            .local_addr()
-            .unwrap()
-            .to_string(),
-            expected_addr()
+            tcp_listener_v4(lc.addr_v4, lc.port)
+                .unwrap()
+                .local_addr()
+                .unwrap()
+                .to_string(),
+            format!("0.0.0.0:{}", lc.port)
         );
     }
 

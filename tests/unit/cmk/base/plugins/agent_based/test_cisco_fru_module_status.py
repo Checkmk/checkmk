eff85ceab03c6053a72cd00c55e9fa52110bd849
Joerg Herbel <joerg.herbel@tribe29.com>
2022-06-17 13:39:45 +0200

    cisco_fru_module_status: migrate section
    SUP-10602
    
    Change-Id: I75b0b663ea1864bb6b7a185f22901c3c64c368be

--- tests/unit/cmk/base/plugins/agent_based/test_cisco_fru_module_status.py
+++ tests/unit/cmk/base/plugins/agent_based/test_cisco_fru_module_status.py
@@ -7,9 +7,10 @@ import pytest
 
 from tests.unit.conftest import FixRegister
 
-from cmk.utils.type_defs import CheckPluginName, SectionName
+from cmk.utils.type_defs import CheckPluginName
 
 from cmk.base.api.agent_based.checking_classes import CheckPlugin
+from cmk.base.plugins.agent_based import cisco_fru_module_status
 from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, Service, State
 
 
@@ -18,30 +19,25 @@ def fixture_check_plugin(fix_register: FixRegister) -> CheckPlugin:
     return fix_register.check_plugins[CheckPluginName("cisco_fru_module_status")]
 
 
-def test_parse(fix_register) -> None:
-    assert fix_register.snmp_sections[SectionName("cisco_fru_module_status")].parse_function(
+def test_parse() -> None:
+    assert cisco_fru_module_status.parse(
         [
             [
-                ["32", "Fabric card module", "9", "Fabric card module"],
-                [
-                    "149",
-                    "Nexus7700 C7706 (6 Slot) Chassis",
-                    "3",
-                    "Nexus7700 C7706 (6 Slot) Chassis",
-                ],
-                ["214", "LinecardSlot-1", "5", "LinecardSlot-1"],
-                ["406", "Backplane", "4", "Backplane"],
-                ["470", "N77-AC-3KW PS-1", "6", "PowerSupply-1"],
-                ["534", "Fan Module-1", "7", "Fan Module-1"],
-                ["598", "module-1 processor-1", "1", "module-1 processor-1"],
-                ["4950", "Linecard-1 Port-1", "10", "Linecard-1 Port-1"],
+                ["32", "9", "Fabric card module"],
+                ["149", "3", "Nexus7700 C7706 (6 Slot) Chassis"],
+                ["214", "5", "LinecardSlot-1"],
+                ["406", "4", "Backplane"],
+                ["470", "6", "PowerSupply-1"],
+                ["534", "7", "Fan Module-1"],
+                ["598", "1", "module-1 processor-1"],
+                ["4950", "10", "Linecard-1 Port-1"],
             ],
             [
                 ["32", "2"],
             ],
         ]
     ) == {
-        "32": {"name": "Fabric card module", "state": (0, "OK")},
+        "32": cisco_fru_module_status.Module(state="2", name="Fabric card module"),
     }
 
 
@@ -49,7 +45,7 @@ def test_discover(check_plugin: CheckPlugin) -> None:
     assert list(
         check_plugin.discovery_function(
             {
-                "32": {"name": "Fabric card module", "state": (0, "OK")},
+                "32": cisco_fru_module_status.Module(state="2", name="Fabric card module"),
             }
         )
     ) == [
@@ -63,7 +59,7 @@ def test_check(check_plugin: CheckPlugin) -> None:
             item="32",
             params={},
             section={
-                "32": {"name": "Fabric card module", "state": (0, "OK")},
+                "32": cisco_fru_module_status.Module(state="2", name="Fabric card module"),
             },
         )
     ) == [

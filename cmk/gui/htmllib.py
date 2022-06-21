ea5bda96a6b9b72972f4a962dd881b63005e50c1
Ronny Bruska <ronny.bruska@tribe29.com>
2022-06-21 09:21:08 +0200

    14335 FIX Fix crash if no icon and an emblem was configured
    FEED-7129
    
    Change-Id: Ie68199dbefcdba84235a671a44538a0a835f8a0b

--- cmk/gui/htmllib.py
+++ cmk/gui/htmllib.py
@@ -2547,6 +2547,8 @@ class html(ABCHTMLGenerator):
             classes.append(class_)
 
         icon_name = icon["icon"] if isinstance(icon, dict) else icon
+        if icon_name is None:
+            icon_name = "empty"
         src = icon_name if "/" in icon_name else self.detect_icon_path(icon_name, prefix="icon_")
         if src.endswith(".png"):
             classes.append("png")

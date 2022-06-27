0862b8c7ef5e2263517a2682776c908be32d4139
Sofia Colakovic <sofia.colakovic@tribe29.com>
2022-06-27 15:56:23 +0200

    separate test code quality to multiple stages
    Change-Id: If783529ffc7f842480a433555af42d1cd1a6339f

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -68,9 +68,7 @@ docker run -t -a stdout -a stderr \
     -e DOCKER_ADDOPTS \
     -e MYPY_ADDOPTS \
     -e PYTHON_FILES \
-    -e IS_PIPFILE_CHANGED \
     -e CHANGED_FILES \
-    -e IS_WERK_CHANGED \
     -e RESULTS \
     -e WORKDIR \
     -e CI \

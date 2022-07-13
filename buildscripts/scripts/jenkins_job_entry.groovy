9bb49b4c9d4b7cb43c05db6d6e7904b7afc45b3e
Frans Fürst <frans.fuerst@tribe29.com>
2022-07-13 15:06:10 +0200

    Adds jenkins_job_entry.groovy which acts as Checkmk specific entry point for Jenkins jobs
     #jjb-migration
    
    Change-Id: I3c7d3b5382dce51f98403ef777ead97319f1bcf2

---
+++ buildscripts/scripts/jenkins_job_entry.groovy
@@ -0,0 +1,51 @@
+/// This is the branch specific Checkmk main entry point. It exists to
+/// avoid redundant code in the actual job definition files and to be able
+/// to provide a standard environment for all Checkmk jobs
+import java.text.SimpleDateFormat
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
+
+def main(job_definition_file) {
+
+    // https://github.com/comquent/imperative-when/commit/9133dc840adad30c75f497000716981e53032d55
+    // TODO make this public
+    conditional_stage = {String name, boolean condition, body -> 
+        body.resolveStrategy = Closure.OWNER_FIRST;
+        body.delegate = [:];
+        stage(name) {
+            if (condition) {
+                print("Execute conditional stage ${STAGE_NAME}");
+                body();
+            } else {
+                print("Skip conditional stage ${STAGE_NAME}");
+                Utils.markStageSkippedForConditional(STAGE_NAME);
+            }
+        }
+    }
+
+    /// brings raise, load_json, cmd_output
+    load("${checkout_dir}/buildscripts/scripts/utils/common.groovy");
+    load("${checkout_dir}/buildscripts/scripts/utils/docker_util.groovy");
+
+    docker_registry_no_http = DOCKER_REGISTRY.split('://')[1];
+
+    /// in order to spoiler spooky effects encountered just
+    /// before midnight we keep a single date for the whole
+    /// job
+    // TODO: this should be passed through by trigger-jobs
+    build_date = (new SimpleDateFormat("yyyy.MM.dd")).format(new Date());
+
+    // FIXME: should be defined elsewhere
+    DOCKER_TAG_FOLDER = "master-latest";
+
+    try {
+        load("${checkout_dir}/${job_definition_file}").main();
+    } catch(Exception e) {
+        // sh("figlet ERROR");
+        dir("${checkout_dir}") {
+            load("${checkout_dir}/buildscripts/scripts/utils/notify.groovy").notify_error(e);
+        }
+    }
+}
+return this;
+

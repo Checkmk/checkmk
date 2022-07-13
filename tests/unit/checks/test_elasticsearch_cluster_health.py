df6d297b94931e789ff84a97cea5a16452e2c52d
Andreas Umbreit <andreas.umbreit@tribe29.com>
2022-07-13 16:34:18 +0200

    13685 FIX elasticsearch_cluster_health: Handle status correctly
    The "Elasticsearch Cluster Health" ignored the elasticsearch status
    (green, yellow, red) and always showed up with state "OK".
    This also held true for a custom mapping defined by service parameters.
    
    Change-Id: Iae4f834bc4e0b6c09e978442f99b665ab8d0640b

---
+++ tests/unit/checks/test_elasticsearch_cluster_health.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from tests.testlib import Check
+
+from .checktestlib import CheckResult
+
+pytestmark = pytest.mark.checks
+
+CHECK_NAME = "elasticsearch_cluster_health"
+
+
+@pytest.mark.parametrize(
+    "parameters, item, info, expected_result",
+    [
+        (
+            {},
+            None,
+            [
+                ["cluster_name", "elasticsearch"],
+                ["status", "yellow"],
+                ["timed_out", "False"],
+                ["number_of_nodes", "5"],
+                ["number_of_data_nodes", "5"],
+                ["active_primary_shards", "747"],
+                ["active_shards", "1613"],
+                ["relocating_shards", "0"],
+                ["initializing_shards", "0"],
+                ["unassigned_shards", "0"],
+                ["delayed_unassigned_shards", "0"],
+                ["number_of_pending_tasks", "0"],
+                ["number_of_in_flight_fetch", "0"],
+                ["task_max_waiting_in_queue_millis", "0"],
+                ["active_shards_percent_as_number", "100.0"],
+            ],
+            CheckResult(
+                [
+                    (0, "Name: elasticsearch", None),
+                    (0, "Data nodes: 5", [("number_of_data_nodes", 5)]),
+                    (0, "Nodes: 5", [("number_of_nodes", 5)]),
+                    (1, "Status: yellow", None),
+                ]
+            ),
+        ),
+        (
+            {"green": 0, "red": 2, "yellow": 3},
+            None,
+            [["status", "yellow"]],
+            CheckResult(
+                (3, "Status: yellow (State changed by rule)", None),
+            ),
+        ),
+    ],
+)
+def test_check_function(parameters, item, info, expected_result):
+    check = Check(CHECK_NAME)
+    parsed = check.run_parse(info)
+    assert CheckResult(check.run_check(item, parameters, parsed)) == expected_result

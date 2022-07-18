afcc539a824250e6469a1a85923670d6f18f9996
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-07-18 11:40:11 +0200

    rename global constant
    Change-Id: Id6f742789531944a4120845302030a940ba4c86b

--- tests/unit/checks/test_generic_legacy_conversion.py
+++ tests/unit/checks/test_generic_legacy_conversion.py
@@ -816,7 +816,6 @@ def test_all_check_variables_present(fix_plugin_legacy) -> None:
         "websphere_mq_queues_default_levels",
         "win_dhcp_pools_default_levels",
         "win_dhcp_pools_inventorize_empty",
-        "win_dhcp_pools_stats_translate",
         "win_netstat_states",
         "win_printer_default_levels",
         "windows_license_default_levels",

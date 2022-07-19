36099434cb6d506538b27406a34850867c0cac79
Konstantin Baikov <konstantin.baikov@tribe29.com>
2022-07-19 15:43:58 +0200

    Remove encodinc pragma from misc places
    Change-Id: I7233b7a9c90428d0b83065d4745a0845c7310e14

--- cmk/utils/livestatus_helpers/tables/_create_table.py
+++ cmk/utils/livestatus_helpers/tables/_create_table.py
@@ -34,7 +34,6 @@ from typing import Final
 import jinja2
 
 TABLE_FILE_TEMPLATE: Final = '''#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.

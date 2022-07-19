dd6c8689481087e285acf7b33e1a2597dcfbfb44
Giordano Tomassorri <giordano.tomassorri@tribe29.com>
2022-07-19 22:11:25 +0200

    14528 FIX SAP HANA backup check in cluster mode: go stale if cannot connect to DB
    Before this commit, if the connection to a database failed, the SAP HANA
    Backup service in a cluster host was marked as OK.
    
    This is now changed and the service goes stale.
    
    SUP-10479
    
    Change-Id: I08cb2c18846b4c56a69050047ef065527d6278ad

--- cmk/base/plugins/agent_based/sap_hana_backup.py
+++ cmk/base/plugins/agent_based/sap_hana_backup.py
@@ -160,6 +160,9 @@ def cluster_check_sap_hana_backup(
         if item in node_section:
             yield from check_sap_hana_backup(item, params, node_section)
             return
+    # If the item is not found in the section, then the service should be reported as stale
+    # SUP-10479
+    raise IgnoreResultsError("Login into database failed.")
 
 
 register.check_plugin(

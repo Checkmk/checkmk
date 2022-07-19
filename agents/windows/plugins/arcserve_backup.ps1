a7b45642d07b4bb9183bd30b3bd03f1a15968d1d
Checkmk release system <feedback@checkmk.com>
2022-07-19 14:14:05 +0200

    Set version to 2.0.0p28

--- agents/windows/plugins/arcserve_backup.ps1
+++ agents/windows/plugins/arcserve_backup.ps1
@@ -1,4 +1,4 @@
-$CMK_VERSION = "2.0.0p27"
+$CMK_VERSION = "2.0.0p28"
 ####
 ## ArcServe.ps1
 ####
--- agents/windows/plugins/mk_msoffice.ps1
+++ agents/windows/plugins/mk_msoffice.ps1
@@ -1,4 +1,4 @@
-$CMK_VERSION = "2.0.0p27"
+$CMK_VERSION = "2.0.0p28"
 ## filename for timestamp
 $MK_CONFDIR = $env:MK_CONFDIR
 

2a2a6000ecd35a2bf97e464616df106da84aa693
Lars Michelsen <lm@tribe29.com>
2022-07-19 10:46:43 +0200

    Cleanup livestatus usage from RBN processing
    In case a user configured the contact selection option "Members of
    contact groups" (contact_groups), a Livestatus query was made to get the
    members of the configured contact groups. This was done every time such
    a rule matched a notification.
    
    In specific situations such a lookup could cause dead locks between the
    Microcore and the notify helper. Since the notification helpers have the
    whole base configuration in memory, we can simply lookup the group
    memberships without the Livestatus lookup.
    
    Change-Id: Ia78b26153ea53ea39e3e7e4be5922e93eab268d8

--- tests/unit/cmk/base/test_notify.py
+++ tests/unit/cmk/base/test_notify.py
@@ -5,13 +5,19 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import os
+from typing import Mapping
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+from tests.testlib.base import Scenario
+
+from cmk.utils.type_defs import ContactgroupName, ContactName
 
 from cmk.base import notify
 
 
-def test_os_environment_does_not_override_notification_script_env(monkeypatch):
+def test_os_environment_does_not_override_notification_script_env(monkeypatch: MonkeyPatch) -> None:
     """Regression test for Werk #7339"""
     monkeypatch.setattr(os, "environ", {"NOTIFY_CONTACTEMAIL": ""})
     script_env = notify.notification_script_env({"CONTACTEMAIL": "ab@test.de"})
@@ -68,3 +74,27 @@ def test_raw_context_from_env_pipe_decoding(environ, expected):
 )
 def test_create_plugin_context(raw_context, params, expected):
     assert notify.create_plugin_context(raw_context, params) == expected
+
+
+@pytest.fixture(name="user_groups")
+def fixture_user_groups() -> Mapping[ContactName, list[ContactgroupName]]:
+    return {
+        "ding": ["foo"],
+        "dong": ["bar", "all"],
+        "harry": ["foo"],
+    }
+
+
+def test_rbn_groups_contacts(
+    monkeypatch: MonkeyPatch, user_groups: Mapping[ContactName, list[ContactgroupName]]
+) -> None:
+    ts = Scenario()
+    ts.set_option(
+        "contacts", {name: {"contactgroups": groups} for name, groups in user_groups.items()}
+    )
+    ts.apply(monkeypatch)
+    assert notify.rbn_groups_contacts([]) == set()
+    assert notify.rbn_groups_contacts(["nono"]) == set()
+    assert notify.rbn_groups_contacts(["all"]) == set(["dong"])
+    assert notify.rbn_groups_contacts(["foo"]) == set(["ding", "harry"])
+    assert notify.rbn_groups_contacts(["foo", "all"]) == set(["ding", "dong", "harry"])

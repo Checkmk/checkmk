cc270bb864a4889222ffe10aaccc0f8563233fb3
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2022-07-21 13:39:56 +0200

    No need for Pipfile anymore to build build image
    ... the python version is centralized in defines.make
    
    Change-Id: Ib5e32c0c585a15d807633deae071d1973c644a3e

--- buildscripts/scripts/build-build-images.groovy
+++ buildscripts/scripts/build-build-images.groovy
@@ -10,7 +10,7 @@
 
 def main() {
     global_dry_run_level = 0;
-    
+
     check_job_parameters([
         "SYNC_WITH_IMAGES_WITH_UPSTREAM",
         "PUBLISH_IMAGES",
@@ -67,7 +67,7 @@ def main() {
                     " | awk '{print \$2}'").replaceAll("[\r\n]+", "");
                 image_ids[distro] = resolve_docker_image_alias(alias_names[distro]);
             }
-            sh("cp defines.make Pipfile omd/strip_binaries buildscripts/infrastructure/build-nodes/scripts");
+            sh("cp defines.make omd/strip_binaries buildscripts/infrastructure/build-nodes/scripts");
         }
 
         dir("${checkout_dir}/buildscripts/infrastructure/build-nodes") {
@@ -109,4 +109,3 @@ def main() {
     }
 }
 return this;
-

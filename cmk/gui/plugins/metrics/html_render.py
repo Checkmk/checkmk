1307f94525e4a2bbf3c53f0d72e89c3a924eb9cb
Joerg Herbel <joerg.herbel@tribe29.com>
2022-07-25 15:46:57 +0200

    Better handling of MKMissingDataError in graph rendering
    This improves the main dashboard in case of missing data in the
    "Percentage of total service problems" graph. Became prominent
    with werk 14423.
    
    Change-Id: Ie53298ea8ba9da34423aa5f7f5a1a580e953b6d8

--- cmk/gui/plugins/metrics/html_render.py
+++ cmk/gui/plugins/metrics/html_render.py
@@ -26,7 +26,7 @@ import cmk.utils.render
 from cmk.utils.type_defs import TimeRange
 
 from cmk.gui.config import active_config
-from cmk.gui.exceptions import MKGeneralException
+from cmk.gui.exceptions import MKGeneralException, MKMissingDataError
 from cmk.gui.htmllib.generator import HTMLWriter
 from cmk.gui.htmllib.html import html
 from cmk.gui.http import request, response
@@ -824,6 +824,8 @@ def render_graph_content_html(
         output += render_graph_error_html(
             _("Cannot fetch data via Livestatus"), _("Cannot create graph")
         )
+    except MKMissingDataError as e:
+        return html.render_message(str(e))
 
     except Exception as e:
         output += render_graph_error_html(e, _("Cannot create graph"))

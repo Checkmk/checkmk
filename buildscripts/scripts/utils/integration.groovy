3909dbb5aa69b0035157024e020f4a535a9955df
Frans Fürst <frans.fuerst@tribe29.com>
2022-07-27 10:54:15 +0200

    buildscripts-utils: modernize testing-helper to work with new groovy jobs
     #jjb-migration
    
    Change-Id: I89606133408d2f080cde8fbe7002aa99afa9caae

--- buildscripts/scripts/utils/integration.groovy
+++ buildscripts/scripts/utils/integration.groovy
@@ -2,75 +2,97 @@
 
 // library for managing shared integration like test logic
 
-def build(Map args) {
+def run_make_targets(Map args) {
     println("""
-        ||== RUN INTEGRATION TEST ==================================================================
+        ||== RUN INTEGRATION TEST ==============================================
         ${args.collect({key, value ->
           "||  ${key}=${value}"
         }).join("\n")}
-        ||==========================================================================================
+        ||======================================================================
         """.stripMargin());
 
     def DOCKER_BUILDS = [:]
 
+    // TODO: this should be done by the top level scripts
     docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
-        def BUILD_IMAGE = docker.build(
-            "build-image:${env.BUILD_ID}", "--pull ${checkout_dir}/buildscripts/docker_image_aliases/IMAGE_TESTING")
-        // The commands are executed with the 1001:1000 UID:GID (non-root).
-        // The download credentials are needed by the image build part
-        BUILD_IMAGE.inside("--group-add=${args.DOCKER_GROUP_ID} --ulimit nofile=1024:1024 --env HOME=/home/jenkins -v /var/run/docker.sock:/var/run/docker.sock") {
-            def versioning = load "${checkout_dir}/buildscripts/scripts/utils/versioning.groovy"
-            def artifacts_helper = load "${checkout_dir}/buildscripts/scripts/utils/upload_artifacts.groovy"
-            def CMK_VERSION = versioning.get_cmk_version(scm, args.VERSION)
-            def IMAGE_VERSION = args.VERSION == "git" ? versioning.get_date() : CMK_VERSION
+        docker_image_from_alias("IMAGE_TESTING").inside(
+            "--group-add=${args.DOCKER_GROUP_ID} --ulimit nofile=1024:1024 --env HOME=/home/jenkins \
+            -v /var/run/docker.sock:/var/run/docker.sock") {
+            
+            // TODO dir + set WORKSPACE is needed due to nested dependency
+            dir("${checkout_dir}") {
+                withEnv(["WORKSPACE=${checkout_dir}"]) {
+                
+                    // TODO preprocess values in top level scripts
+                    def versioning = load "${checkout_dir}/buildscripts/scripts/utils/versioning.groovy"
+                    def artifacts_helper = load "${checkout_dir}/buildscripts/scripts/utils/upload_artifacts.groovy"
+                    def image_version = args.VERSION == "git" ? versioning.get_date() : args.cmk_version;
 
-            sh("rm -rf \"${WORKSPACE}/packages\"")
-            if(args.DISTRO_LIST == ["ubuntu-20.04"]) {
-                artifacts_helper.download_deb(
-                    INTERNAL_DEPLOY_DEST, INTERNAL_DEPLOY_PORT, IMAGE_VERSION,
-                    "${WORKSPACE}/packages/${IMAGE_VERSION}", args.EDITION, "focal")
-            }
-            else if(args.DISTRO_LIST.size() == 1) {
-                throw new Exception("Please add a case to download only the needed package for ${args.DISTRO_LIST}")
-            }
-            else {
-                artifacts_helper.download_version_dir(INTERNAL_DEPLOY_DEST, INTERNAL_DEPLOY_PORT, IMAGE_VERSION, "${WORKSPACE}/packages/${IMAGE_VERSION}")
-            }
+                    // TODO make independent from WORKSPACE
+                    sh("rm -rf \"${WORKSPACE}/packages\"")
+                    if (args.DISTRO_LIST == ["ubuntu-20.04"]) {
+                        artifacts_helper.download_deb(
+                            INTERNAL_DEPLOY_DEST,
+                            INTERNAL_DEPLOY_PORT,
+                            image_version,
+                            "${WORKSPACE}/packages/${image_version}",
+                            args.EDITION,
+                            "focal",
+                        );
+                    }
+                    else if(args.DISTRO_LIST.size() == 1) {
+                        throw new Exception("Please add a case to download only the needed package for ${args.DISTRO_LIST}")
+                    }
+                    else {
+                        artifacts_helper.download_version_dir(
+                            INTERNAL_DEPLOY_DEST,
+                            INTERNAL_DEPLOY_PORT,
+                            IMAGE_VERSION,
+                            "${WORKSPACE}/packages/${image_version}",
+                        );
+                    }
 
-            // Cleanup test results directory before starting the test to prevent previous
-            // runs somehow affecting the current run.
-            sh("[ -d test-results ] && rm -rf test-results || true")
+                    // Cleanup test results directory before starting the test to prevent previous
+                    // runs somehow affecting the current run.
+                    sh("[ -d test-results ] && rm -rf test-results || true")
 
-            // Initialize our virtual environment before parallelization
-            sh("make .venv")
+                    // Initialize our virtual environment before parallelization
+                    sh("make .venv")
 
-            // Then execute the tests
-            try {
-                args.DISTRO_LIST.each { DISTRO ->
-                    DOCKER_BUILDS[DISTRO] = {
-                        stage(DISTRO + ' test') {
-                            dir ('tests') {
-                                sh "RESULT_PATH='${WORKSPACE}/test-results/${DISTRO}' EDITION='"+args.EDITION+"' DOCKER_TAG='"+args.DOCKER_TAG+"' VERSION='$CMK_VERSION' DISTRO='$DISTRO' BRANCH='${args.BRANCH}' make "+args.MAKE_TARGET
+                    // Then execute the tests
+                    try {
+                        args.DISTRO_LIST.each { DISTRO ->
+                            DOCKER_BUILDS[DISTRO] = {
+                                stage(DISTRO + ' test') {
+                                    dir ('tests') {
+                                        sh("""RESULT_PATH='${WORKSPACE}/test-results/${DISTRO}' \
+                                        EDITION='${args.EDITION}' \
+                                        DOCKER_TAG='${args.DOCKER_TAG}' \
+                                        VERSION='${args.cmk_version}' \
+                                        DISTRO='$DISTRO' \
+                                        BRANCH='${args.BRANCH}' \
+                                        make ${args.MAKE_TARGET}""");
+                                    }
+                                }
                             }
                         }
+                        parallel DOCKER_BUILDS
+                    } finally {
+                        stage('archive artifacts') {
+                            archiveArtifacts("test-results/**")
+                            xunit([Custom(
+                                customXSL: "$JENKINS_HOME/userContent/xunit/JUnit/0.1/pytest-xunit.xsl",
+                                deleteOutputFiles: true,
+                                failIfNotNew: true,
+                                pattern: "**/junit.xml",
+                                skipNoTestFiles: false,
+                                stopProcessingIfError: true
+                            )])
+                        }
                     }
-                }
-                parallel DOCKER_BUILDS
-            } finally {
-                stage('archive artifacts') {
-                    archiveArtifacts("test-results/**")
-                    xunit([Custom(
-                        customXSL: "$JENKINS_HOME/userContent/xunit/JUnit/0.1/pytest-xunit.xsl",
-                        deleteOutputFiles: true,
-                        failIfNotNew: true,
-                        pattern: "**/junit.xml",
-                        skipNoTestFiles: false,
-                        stopProcessingIfError: true
-                    )])
                 }
             }
         }
     }
 }
-
-return this
+return this;

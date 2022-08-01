d9b3670bfed26ad57eac56a380b1d300fbff3b01
Max Linke <max.linke@tribe29.com>
2022-08-01 12:48:17 +0200

    try to convice gerrit of the move later
    Change-Id: I0d36358a45431610544cfe09da0d9040337dbd59

--- tests/unit/cmk/base/plugins/agent_based/test_etherbox.py
+++ tests/unit/cmk/base/plugins/agent_based/test_etherbox.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
@@ -103,7 +102,7 @@ def test_check_switch(check_switch: CheckFunction) -> None:
         [["", "3"]],  # type
         [["", "42"]],  # value
     ]
-    results = list(check_switch(item="9.3", section=section, params={}))
+    results = list(check_switch(item="9.3", section=section, params={"state": "closed"}))
     assert set(results) == {
         Result(state=State.OK, summary="[n] Switch contact closed"),
         Metric("switch_contact", 42.0),

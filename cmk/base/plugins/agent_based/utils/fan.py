9598fa0c036bbfd3bd7dafa79fe3ba30ca2cf55f
Tim Peisker <tim.peisker@tribe29.com>
2022-08-01 11:30:51 +0200

    added primekey_fan check
    and migrated legacy fan check to new API
    
    SUP-9917
    
    Change-Id: I09f5565c00a9a523332fce9ca91de5d7069e75ee

---
+++ cmk/base/plugins/agent_based/utils/fan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import Any, Mapping
+
+from ..agent_based_api.v1 import check_levels
+from ..agent_based_api.v1.type_defs import CheckResult
+
+
+def check_fan(rpm: float, params: Mapping[str, Any]) -> CheckResult:
+    return check_levels(
+        rpm,
+        levels_lower=params.get("lower"),
+        levels_upper=params.get("upper"),
+        metric_name="fan" if params.get("output_metrics") else None,
+        render_func=lambda r: f"{int(r)} RPM",
+        label="Speed",
+    )

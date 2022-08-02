33cc09b2dceebb57f5419a12a99bb731151d4e6d
Lisa Pichler <lisa.pichler@tribe29.com>
2022-08-02 09:35:59 +0200

    14503 FIX ups_test: crash with KeyError (0)
    CMK-11115
    
    Change-Id: I723e3000b5369bdf39426f87d6ac57cf8b460012

---
+++ tests/unit/checks/test_ups_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from testlib import Check  # type: ignore[import]
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_ups_test_unknown_test_result():
+    check = Check("ups_test")
+    assert list(check.run_check(
+        None,
+        (0, 0),
+        [[['2400776998']], [['0', '0', 'aardvark']]],
+    ))[0] == (3, "Last test: unknown (aardvark)")

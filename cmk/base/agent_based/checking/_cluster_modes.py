73eba95de0d09fa31b6dc399ae2c4e10d1bb523f
Lukas Lengler <lukas.lengler@tribe29.com>
2022-08-05 12:33:46 +0200

    14149 FIX Crash: Clustered services using predefined cluster modes
    Using Clustered services with predefined cluster mode certain services would crash
    if no summary (therefore only notice) was available for a node result.
    
    This issue occured for many check plugins but the known ones were:
    <ul>
    <li>apache_status
    <li>fileinfo_groups
    <li>if64
    <li>mrpe
    <li>ntp_time
    </ul>
    
    This issue is solved and the correct information is now displayed.
    
    Change-Id: I9df29059fb420bb686580bb13cdc72dee9991c34

--- cmk/base/agent_based/checking/_cluster_modes.py
+++ cmk/base/agent_based/checking/_cluster_modes.py
@@ -226,7 +226,7 @@ class Summarizer:
         yield from (
             Result(
                 state=State.OK,
-                notice=r.summary,
+                notice=r.summary if r.summary else r.details,
                 details=f"{r.details}{state_markers[int(r.state)]}",
             )
             for node in secondary_nodes

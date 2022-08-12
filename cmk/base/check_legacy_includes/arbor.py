c7c04621e89e9ae58361a37eaa1d5e597c851af8
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-08-12 21:30:19 +0200

    use check_levels
    Change-Id: I56b47b614577cb46c9694694154b41f0775b207e

--- cmk/base/check_legacy_includes/arbor.py
+++ cmk/base/check_legacy_includes/arbor.py
@@ -3,24 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-#   .--Common--------------------------------------------------------------.
-#   |              ____                                                    |
-#   |             / ___|___  _ __ ___  _ __ ___   ___  _ __                |
-#   |            | |   / _ \| '_ ` _ \| '_ ` _ \ / _ \| '_ \               |
-#   |            | |__| (_) | | | | | | | | | | | (_) | | | |              |
-#   |             \____\___/|_| |_| |_|_| |_| |_|\___/|_| |_|              |
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
-
-
-def arbor_test_threshold(perc, name, warn, crit):
-    status = perc > crit and 2 or perc > warn and 1 or 0
-    infotext = "%s used: %d%%" % (name, perc)
-
-    if status > 0:
-        infotext += " (warn/crit at %.1f%%/%.1f%%) (%s)" % (warn, crit, "!" * status)
-    return status, infotext
-
+from cmk.base.check_api import check_levels
 
 # .
 #   .--Memory--------------------------------------------------------------.
@@ -45,29 +28,12 @@ def inventory_arbor_memory(parsed):
 
 
 def check_arbor_memory(no_item, params, parsed):
-    def worst_status(*args):
-        order = [0, 1, 3, 2]
-        return sorted(args, key=lambda x: order[x], reverse=True)[0]
-
-    def combine_infotext(*blocks):
-        return ", ".join([b for b in blocks if b])
-
     ram_perc, swap_perc = map(int, parsed["memory"])
 
-    ram_warn, ram_crit = params["levels_ram"][1]
-    swap_warn, swap_crit = params["levels_swap"][1]
-
-    ram_status, ram_info = arbor_test_threshold(ram_perc, "RAM", ram_warn, ram_crit)
-    swap_status, swap_info = arbor_test_threshold(swap_perc, "Swap", swap_warn, swap_crit)
-
-    infotext = combine_infotext(ram_info, swap_info)
-
-    perfdata = [
-        ("mem_used_percent", ram_perc, ram_warn, ram_crit),
-        ("swap_used_percent", swap_perc, swap_warn, swap_crit),
-    ]
-
-    return worst_status(ram_status, swap_status), infotext, perfdata
+    yield check_levels(ram_perc, "mem_used_percent", params["levels_ram"][1], infoname="Used RAM")
+    yield check_levels(
+        swap_perc, "swap_used_percent", params["levels_swap"][1], infoname="Used Swap"
+    )
 
 
 # .
@@ -89,8 +55,8 @@ def inventory_arbor_disk_usage(parsed):
 
 def check_arbor_disk_usage(no_item, params, parsed):
     usage = int(parsed["disk"])
-    status, infotext = arbor_test_threshold(usage, "Disk", *params["levels"])
-    return status, infotext, [("disk_utilization", float(usage) / 100.0)]
+    yield check_levels(usage, None, params["levels"], infoname="Disk usage")
+    yield 0, "", [("disk_utilization", float(usage) / 100.0)]
 
 
 # .
@@ -136,40 +102,14 @@ def inventory_arbor_drop_rate(parsed):
 
 def check_arbor_drop_rate(no_item, params, parsed):
     drop_rate = int(parsed["drop_rate"])
-    infotext = "%s pps" % drop_rate
-
-    lower_status = 0
-    lower_levels = params.get("levels_lower")
-    if lower_levels:
-        warn, crit = lower_levels
-
-        if drop_rate <= crit:
-            lower_status, label = 2, "(!!)"
-        elif drop_rate <= warn:
-            lower_status, label = 1, "(!)"
-
-        if lower_status:
-            infotext += " (warn/crit below %.1f/%.1f)%s" % (warn, crit, label)
-
-    upper_status = 0
-    upper_levels = params.get("levels")
-    if upper_levels:
-        warn, crit = upper_levels
-
-        if drop_rate >= crit:
-            upper_status, label = 2, "(!!)"
-        elif drop_rate >= warn:
-            upper_status, label = 1, "(!)"
-
-        if upper_status:
-            infotext += " (warn/crit above %.1f/%.1f)%s" % (warn, crit, label)
-
-        perfdata: list[tuple] = [("if_in_pkts", drop_rate, warn, crit)]
-    else:
-        perfdata = [("if_in_pkts", drop_rate)]
-
-    status = max(lower_status, upper_status)
-    yield status, infotext, perfdata
+    lower_levels = params.get("levels_lower") or (None, None)
+    upper_levels = params.get("levels") or (None, None)
+    yield check_levels(
+        drop_rate,
+        "if_in_pkts",
+        upper_levels + lower_levels,
+        human_readable_func=lambda x: "%.1f pps",
+    )
 
 
 # .

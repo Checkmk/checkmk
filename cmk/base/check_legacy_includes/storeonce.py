69b2cf357556900e8e05335d223e72163c394aac
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-08-16 08:20:56 +0200

    prepare for migration
    Change-Id: I435fe16c4b995afbee16fe188832469b6ff7d7d1

--- cmk/base/check_legacy_includes/storeonce.py
+++ cmk/base/check_legacy_includes/storeonce.py
@@ -2,16 +2,28 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+from typing import Any, Final, Iterable, Mapping
 
-
-from cmk.base.check_api import get_bytes_human_readable
+from cmk.base.plugins.agent_based.agent_based_api.v1 import render
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
 
 from .df import df_check_filesystem_list
 
+CheckResult = Iterable[tuple[int, str, list] | tuple[int, str]]
+SectionServiceSets = Mapping[str, Mapping[str, str]]
 
-def parse_storeonce_servicesets(info):
-    parsed: dict = {}
-    for line in info:
+STATE_MAP: Final = {
+    "0": 3,  # Unknown
+    "1": 0,  # OK
+    "2": 0,  # Information
+    "3": 1,  # Warning
+    "4": 2,  # Critical
+}
+
+
+def parse_storeonce_servicesets(string_table: StringTable) -> SectionServiceSets:
+    parsed: dict[str, dict[str, str]] = {}
+    for line in string_table:
         if line[0].startswith("["):
             item = line[0]
             parsed[item] = {}
@@ -20,21 +32,12 @@ def parse_storeonce_servicesets(info):
     return parsed
 
 
-def translate_storeonce_status(status):
-    translate_state = {
-        "0": 3,  # Unknown
-        "1": 0,  # OK
-        "2": 0,  # Information
-        "3": 1,  # Warning
-        "4": 2,  # Critical
-    }
-    return translate_state[status]
-
-
-def _get_storeonce_space_values(values, type_):
+def _get_storeonce_space_values(
+    values: Mapping[str, str | int | float], type_: str
+) -> tuple[float, float, float]:
     key = "%s in bytes" % type_
     if key in values:
-        return float(values[key]), 0, 0
+        return float(values[key]), 0.0, 0.0
 
     # combined(total) = local + cloud
     type_ = type_.replace(" Space", "")
@@ -48,7 +51,9 @@ def _get_storeonce_space_values(values, type_):
     )
 
 
-def check_storeonce_space(item, params, values):
+def check_storeonce_space(
+    item: str, params: Mapping[str, Any], values: Mapping[str, str | int | float]
+) -> CheckResult:
     total_bytes, cloud_bytes, local_bytes = _get_storeonce_space_values(values, "Capacity")
     free_bytes, free_cloud_bytes, free_local_bytes = _get_storeonce_space_values(
         values, "Free Space"
@@ -57,16 +62,16 @@ def check_storeonce_space(item, params, values):
     factor = 1024 * 1024
     yield df_check_filesystem_list(
         item, params, [(item, total_bytes / factor, free_bytes / factor, 0)]
-    )  # fixed: true-division
+    )
 
     if cloud_bytes:
-        yield 0, "Total cloud: %s" % get_bytes_human_readable(cloud_bytes)
+        yield 0, "Total cloud: %s" % render.bytes(cloud_bytes)
     if local_bytes:
-        yield 0, "Total local: %s" % get_bytes_human_readable(local_bytes)
+        yield 0, "Total local: %s" % render.bytes(local_bytes)
     if free_cloud_bytes:
-        yield 0, "Free cloud: %s" % get_bytes_human_readable(free_cloud_bytes)
+        yield 0, "Free cloud: %s" % render.bytes(free_cloud_bytes)
     if free_local_bytes:
-        yield 0, "Free local: %s" % get_bytes_human_readable(free_local_bytes)
+        yield 0, "Free local: %s" % render.bytes(free_local_bytes)
 
     dedupl_ratio_str = values.get("Deduplication Ratio") or values.get("dedupeRatio")
     if dedupl_ratio_str is not None:

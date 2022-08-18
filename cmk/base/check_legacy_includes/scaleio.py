742f61bf6357ddc4e00e18e7524b47666fd7f4f0
LukaRacic <luka.racic@tribe29.com>
2022-08-18 15:39:58 +0200

    scaleio_storage_pool_totalrw: Refactored scaleio_storage_pool_totalrw and relevant tests
    Change-Id: If65ee216cc0ad0a383794556145b4dc022d3730e

--- cmk/base/check_legacy_includes/scaleio.py
+++ cmk/base/check_legacy_includes/scaleio.py
@@ -5,28 +5,31 @@
 
 
 # pylint: disable=no-else-return
+from typing import Mapping, MutableMapping, Sequence
+
 from cmk.base.plugins.agent_based.utils.scaleio import (  # pylint: disable=unused-import
     convert_scaleio_space,
     parse_scaleio,
+    ScaleioSection,
 )
 
 
 # Values can be in every unit. We need Bytes for
 # diskstat.include
-def convert_to_bytes(tp, unit):
+def convert_to_bytes(tp: float, unit: str) -> float | None:
     """Convert the throughput values from the storage pool to Bytes
 
-    >>> convert_to_bytes(1, "Bytes")
-    1
-    >>> convert_to_bytes(1, "KB")
-    1024
-    >>> convert_to_bytes(1, "MB")
-    1048576
-    >>> convert_to_bytes(1, "GB")
-    1073741824
-    >>> convert_to_bytes(1, "TB")
-    1099511627776
-    >>> convert_to_bytes(1, "Not_known")
+    >>> convert_to_bytes(1.0, "Bytes")
+    1.0
+    >>> convert_to_bytes(1.0, "KB")
+    1024.0
+    >>> convert_to_bytes(1.0, "MB")
+    1048576.0
+    >>> convert_to_bytes(1.0, "GB")
+    1073741824.0
+    >>> convert_to_bytes(1.0, "TB")
+    1099511627776.0
+    >>> convert_to_bytes(1.0, "Not_known")
 
     """
 
@@ -43,24 +46,26 @@ def convert_to_bytes(tp, unit):
     return None
 
 
-def get_disks(item, read_data, write_data):
+def get_disks(
+    item: str, read_data: Sequence[str], write_data: Sequence[str]
+) -> Mapping[str, MutableMapping[str, None | float]]:
     """Creates a dictionary with the pool as the key and read/write tp and ios data as values
 
     >>> read_data = ['7', 'IOPS', '33.2', 'KB', '(33996', 'Bytes)', 'per-second']
     >>> write_data = ['63', 'IOPS', '219.6', 'KB', '(224870', 'Bytes)', 'per-second']
     >>> get_disks("item_name", read_data, write_data)
-    {'item_name': {'node': None, 'read_ios': 7, 'read_throughput': 33996, 'write_ios': 63, 'write_throughput': 224870}}
+    {'item_name': {'node': None, 'read_ios': 7.0, 'read_throughput': 33996.8, 'write_ios': 63.0, 'write_throughput': 224870.4}}
     """
 
-    read_tp = convert_to_bytes(int(read_data[-3].strip("(")), read_data[-2].strip(")"))
-    write_tp = convert_to_bytes(int(write_data[-3].strip("(")), write_data[-2].strip(")"))
+    read_tp = convert_to_bytes(float(read_data[2]), read_data[3])
+    write_tp = convert_to_bytes(float(write_data[2]), write_data[3])
 
     disks = {
         item: {
             "node": None,
-            "read_ios": int(read_data[0]),
+            "read_ios": float(read_data[0]),
             "read_throughput": read_tp,
-            "write_ios": int(write_data[0]),
+            "write_ios": float(write_data[0]),
             "write_throughput": write_tp,
         }
     }

fb01fb9f711f3520857b878e1706ac3ede1f2284
LukaRacic <luka.racic@tribe29.com>
2022-08-18 14:48:43 +0200

    scaleio_storage_pool_totalrw: Added doc tests
    Change-Id: I0e112b9fcc4825e38f31b28b629db86cb8d72444

--- cmk/base/check_legacy_includes/scaleio.py
+++ cmk/base/check_legacy_includes/scaleio.py
@@ -14,6 +14,22 @@ from cmk.base.plugins.agent_based.utils.scaleio import (  # pylint: disable=unus
 # Values can be in every unit. We need Bytes for
 # diskstat.include
 def convert_to_bytes(tp, unit):
+    """Convert the throughput values from the storage pool to Bytes
+
+    >>> convert_to_bytes(1, "Bytes")
+    1
+    >>> convert_to_bytes(1, "KB")
+    1024
+    >>> convert_to_bytes(1, "MB")
+    1048576
+    >>> convert_to_bytes(1, "GB")
+    1073741824
+    >>> convert_to_bytes(1, "TB")
+    1099511627776
+    >>> convert_to_bytes(1, "Not_known")
+
+    """
+
     if unit == "Bytes":
         return tp
     elif unit == "KB":
@@ -28,6 +44,14 @@ def convert_to_bytes(tp, unit):
 
 
 def get_disks(item, read_data, write_data):
+    """Creates a dictionary with the pool as the key and read/write tp and ios data as values
+
+    >>> read_data = ['7', 'IOPS', '33.2', 'KB', '(33996', 'Bytes)', 'per-second']
+    >>> write_data = ['63', 'IOPS', '219.6', 'KB', '(224870', 'Bytes)', 'per-second']
+    >>> get_disks("item_name", read_data, write_data)
+    {'item_name': {'node': None, 'read_ios': 7, 'read_throughput': 33996, 'write_ios': 63, 'write_throughput': 224870}}
+    """
+
     read_tp = convert_to_bytes(int(read_data[-3].strip("(")), read_data[-2].strip(")"))
     write_tp = convert_to_bytes(int(write_data[-3].strip("(")), write_data[-2].strip(")"))
 

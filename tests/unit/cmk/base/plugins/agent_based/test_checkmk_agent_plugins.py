902b6a7c121cb45db7188020a605a1665000e1ac
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-08-19 16:55:08 +0200

    rework test_checkmk_agent_plugins
    Change-Id: I669fd8607fd39bbd3c122d8bc4b243469b4f967b

--- tests/unit/cmk/base/plugins/agent_based/test_checkmk_agent_plugins.py
+++ tests/unit/cmk/base/plugins/agent_based/test_checkmk_agent_plugins.py
@@ -3,25 +3,82 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from dataclasses import dataclass
 from enum import auto, Enum
-from typing import Callable, List
+from typing import Callable
 
 import pytest
+
+from cmk.utils.version import parse_check_mk_version
 
 from cmk.base.api.agent_based.type_defs import StringTable
 from cmk.base.plugins.agent_based import checkmk_agent_plugins as cap
 from cmk.base.plugins.agent_based.agent_based_api.v1 import TableRow
 from cmk.base.plugins.agent_based.utils import checkmk
 
-# TODO(sk): Test must be rewritten fully to be based only on the next info
-# Input consts: main_dir(os) and sep(os)
-# Input fixture: [(filename, plugin|local, subdir|None, VERSION_STRING, VERSION_VALUE),...]
-# Calculations: caching(subdir, os), etc
-
 
 class _OsType(Enum):
     win = auto()
     lnx = auto()
+
+
+class _FileKind(Enum):
+    plugin = "plugins"
+    local = "local_checks"
+
+    def __str__(self):
+        return str(self.value)
+
+
+@dataclass(frozen=True)
+class File:
+    name: str
+    version: str
+    kind: _FileKind
+    cache_interval: int | None
+
+    def as_table_row(self) -> TableRow:
+        return TableRow(
+            path=["software", "applications", "checkmk-agent", str(self.kind)],
+            key_columns={"name": self.name},
+            inventory_columns={"version": self.version, "cache_interval": self.cache_interval},
+        )
+
+
+def _win_file_list() -> list[File]:
+    return [
+        File(name="zorp", version="2.1.0i1", cache_interval=None, kind=_FileKind.plugin),
+        File(
+            name="sync_local_check.sh",
+            version="3.14.15",
+            cache_interval=None,
+            kind=_FileKind.local,
+        ),
+    ]
+
+
+def _lin_file_list() -> list[File]:
+    return [
+        File(name="mk_filestats.py", version="2.1.0i1", cache_interval=None, kind=_FileKind.plugin),
+        File(name="zorp", version="2.1.0i1", cache_interval=123, kind=_FileKind.plugin),
+        File(
+            name="sync_local_check.sh",
+            version="3.14.15",
+            cache_interval=None,
+            kind=_FileKind.local,
+        ),
+    ]
+
+
+def _file_list(os_type: _OsType) -> list[File]:
+    match os_type:
+        case _OsType.win:
+            return _win_file_list()
+        case _OsType.lnx:
+            return _lin_file_list()
+
+    # the only way to satisfy pylint, which is not smart enough to support match correctly
+    return []
 
 
 def _main_dir(os_type: _OsType) -> str:
@@ -32,83 +89,83 @@ def _main_dir(os_type: _OsType) -> str:
     )
 
 
-def _sep(os_type: _OsType) -> str:
-    return "\\" if os_type == _OsType.win else "/"
+@dataclass(frozen=True)
+class _AgentRow:
+    dir: str
+    name: str
+    ver: str
+    cache: str
 
 
-def _agent_output(os_type: _OsType) -> List[List[str]]:
+def _produce_agent_output(os_type: _OsType) -> list[list[str]]:
     main_dir = _main_dir(os_type)
-    sep = _sep(os_type)
+    sep = "\\" if os_type == _OsType.win else "/"
+    rows = [
+        _AgentRow(
+            dir="plugins" if f.kind == _FileKind.plugin else "local",
+            name=f.name,
+            ver=f.version,
+            cache="" if f.cache_interval is None else f"{f.cache_interval}{sep}",
+        )
+        for f in _lin_file_list()
+    ]
+    plugin_rows = [
+        [f"{main_dir}{rows[i].dir}{sep}{rows[i].cache}{rows[i].name}{marker}{rows[i].ver}"]
+        for i, marker in zip([0, 2, 1], [":__version__ = ", ":CMK_VERSION=", ":CMK_VERSION = "])
+    ]
     return [
         [f"pluginsdir {main_dir}plugins"],
         [f"localdir {main_dir}local"],
-        [f'{main_dir}plugins{sep}mk_filestats.py:__version__ = "2.1.0i1"'],  # win skips! this entry
-        [f'{main_dir}local{sep}sync_local_check.sh:CMK_VERSION="3.14.15"'],
-        [f'{main_dir}plugins{sep}123{sep}zorp:CMK_VERSION = "2.1.0i1"'],  # 123 caching in lnx
-        [f'{main_dir}plugins{sep}bad_file:XYZ_VERSION = "2.1.0i1"'],  # invalid entry, to be ignored
+        *plugin_rows,
+        [f'{main_dir}plugins{sep}bad_file:XYZ_VERSION = "2.1.0i1"'],
     ]
 
 
-def _section(os_type: _OsType) -> checkmk.PluginSection:
+def _get_parser(os_type: _OsType) -> Callable[[StringTable], checkmk.PluginSection]:
+    return (
+        cap.parse_checkmk_agent_plugins_lnx
+        if os_type == _OsType.lnx
+        else cap.parse_checkmk_agent_plugins_win
+    )
+
+
+def _produce_plugin_section(os_type: _OsType) -> checkmk.PluginSection:
+    parser = _get_parser(os_type)
+    return parser(_produce_agent_output(os_type))
+
+
+def _expected_section(files: list[File]) -> checkmk.PluginSection:
     return checkmk.PluginSection(
         plugins=[
             checkmk.Plugin(
-                name,
-                "2.1.0i1",
-                2010010100,
-                123 if os_type == _OsType.lnx and name == "zorp" else None,
+                f.name,
+                f.version,
+                parse_check_mk_version(f.version),
+                f.cache_interval,
             )
-            for name in (["mk_filestats.py", "zorp"] if os_type == _OsType.lnx else ["zorp"])
+            for f in files
+            if f.kind == _FileKind.plugin
         ],
         local_checks=[
-            checkmk.Plugin("sync_local_check.sh", "3.14.15", 3141550000, None),
+            checkmk.Plugin(
+                f.name,
+                f.version,
+                parse_check_mk_version(f.version),
+                f.cache_interval,
+            )
+            for f in files
+            if f.kind == _FileKind.local
         ],
     )
 
 
-@pytest.mark.parametrize(
-    "parser,os_type",
-    [
-        (cap.parse_checkmk_agent_plugins_lnx, _OsType.lnx),
-        (cap.parse_checkmk_agent_plugins_win, _OsType.win),
-    ],
-)
-def test_parse_ok_lnx(
-    parser: Callable[[StringTable], checkmk.PluginSection], os_type: _OsType
-) -> None:
-    assert parser(_agent_output(os_type)) == _section(os_type)
+@pytest.mark.parametrize("os_type", [_OsType.lnx, _OsType.win])
+def test_parse_ok(os_type: _OsType) -> None:
+    assert _produce_plugin_section(os_type) == _expected_section(_file_list(os_type))
 
 
-def test_inventory_lnx() -> None:
-    assert list(cap.inventory_checkmk_agent_plugins(_section(_OsType.lnx))) == [
-        TableRow(
-            path=["software", "applications", "checkmk-agent", "plugins"],
-            key_columns={"name": "mk_filestats.py"},
-            inventory_columns={"version": "2.1.0i1", "cache_interval": None},
-        ),
-        TableRow(
-            path=["software", "applications", "checkmk-agent", "plugins"],
-            key_columns={"name": "zorp"},
-            inventory_columns={"version": "2.1.0i1", "cache_interval": 123},
-        ),
-        TableRow(
-            path=["software", "applications", "checkmk-agent", "local_checks"],
-            key_columns={"name": "sync_local_check.sh"},
-            inventory_columns={"version": "3.14.15", "cache_interval": None},
-        ),
-    ]
-
-
-def test_inventory_win() -> None:
-    assert list(cap.inventory_checkmk_agent_plugins(_section(_OsType.win))) == [
-        TableRow(
-            path=["software", "applications", "checkmk-agent", "plugins"],
-            key_columns={"name": "zorp"},
-            inventory_columns={"version": "2.1.0i1", "cache_interval": None},
-        ),
-        TableRow(
-            path=["software", "applications", "checkmk-agent", "local_checks"],
-            key_columns={"name": "sync_local_check.sh"},
-            inventory_columns={"version": "3.14.15", "cache_interval": None},
-        ),
-    ]
+@pytest.mark.parametrize("os_type", [_OsType.lnx, _OsType.win])
+def test_inventory(os_type: _OsType) -> None:
+    expected_inventory = [f.as_table_row() for f in _file_list(os_type)]
+    obtained_inventory = list(cap.inventory_checkmk_agent_plugins(_produce_plugin_section(os_type)))
+    assert obtained_inventory == expected_inventory

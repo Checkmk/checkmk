c901a7c1799862df3584e0f1c07ecc14eaea5011
Sven Panne <sven.panne@tribe29.com>
2022-08-26 11:45:44 +0200

    Disable gprofng for ancient distros.
    Change-Id: Iea196dfd5f71f14cbfa12c2b2bd7216775d5d5d5

--- buildscripts/infrastructure/build-nodes/scripts/install-gnu-toolchain.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-gnu-toolchain.sh
@@ -35,7 +35,7 @@ BUILD_DIR=/opt/src
 # NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE
 # Only the GCC_VERSION is part of the cache key, so be sure to bump this, too,
 # e.g. when changing the binutils or gdb version!
-BUILD_ID=3
+BUILD_ID=4
 
 download_sources() {
     # Get the sources from nexus or upstream
@@ -65,7 +65,22 @@ build_binutils() {
     tar xzf binutils-${BINUTILS_VERSION}.tar.gz
     mkdir binutils-${BINUTILS_VERSION}-build
     cd binutils-${BINUTILS_VERSION}-build
+    # HACK: Dispatching on the distro is not nice, we should really check the versions.
+    case "$DISTRO" in
+        centos-7)
+            echo "makeinfo too old, gprofng's docs won't build"
+            BINUTILS_CONFIGURE_ADD_OPTS="--disable-gprofng"
+            ;;
+        sles-12*)
+            echo "bison 2.7 is too old, gprofng requires bison 3.0.4 or later"
+            BINUTILS_CONFIGURE_ADD_OPTS="--disable-gprofng"
+            ;;
+        *)
+            BINUTILS_CONFIGURE_ADD_OPTS=""
+            ;;
+    esac
     ../binutils-${BINUTILS_VERSION}/configure \
+        "${BINUTILS_CONFIGURE_ADD_OPTS}" \
         --prefix="${PREFIX}"
     make -j4
     make install

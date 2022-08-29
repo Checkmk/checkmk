bc5e915d3069569c716d1f1b45ce8f7fce9c8b3c
Joerg Herbel <joerg.herbel@tribe29.com>
2022-08-29 12:19:57 +0200

    Add CEE-only folder attribute for baking agents even if folder is empty
    CMK-10988
    
    Change-Id: I9df728a523d795051b5b8553165e21e479d2bf59

--- tests/unit/cmk/gui/watolib/test_host_attributes.py
+++ tests/unit/cmk/gui/watolib/test_host_attributes.py
@@ -6,7 +6,7 @@
 # pylint: disable=redefined-outer-name
 import pytest
 
-from cmk.utils.version import is_plus_edition
+from cmk.utils.version import is_plus_edition, is_raw_edition
 
 import cmk.gui.watolib.host_attributes as attrs
 
@@ -287,6 +287,24 @@ expected_attributes = {
         if is_plus_edition()
         else {}
     ),
+    **(
+        {
+            "bake_agent_package": {
+                "depends_on_roles": [],
+                "depends_on_tags": [],
+                "editable": True,
+                "from_config": False,
+                "show_in_folder": True,
+                "show_in_form": False,
+                "show_in_host_search": False,
+                "show_in_table": False,
+                "show_inherited_value": False,
+                "topic": "Monitoring agents",
+            },
+        }
+        if not is_raw_edition()
+        else {}
+    ),
     "tag_snmp_ds": {
         "depends_on_roles": [],
         "depends_on_tags": [],
@@ -530,6 +548,7 @@ def test_host_attributes(for_what, new) -> None:  # type:ignore[no-untyped-def]
         "monitoring_agents": [
             "tag_agent",
             *(("cmk_agent_connection",) if is_plus_edition() else ()),
+            *(("bake_agent_package",) if not is_raw_edition() else ()),
             "tag_snmp_ds",
             "snmp_community",
             "tag_piggyback",

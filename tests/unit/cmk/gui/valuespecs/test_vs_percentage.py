83b5be43115a5b7e385f611c7b5f88a1bb76637f
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-08-29 15:04:27 +0200

    Add test for valuespec.Percentage
    Change-Id: I89907b32e67eaf905bab0d9cc03ddea528af3961

---
+++ tests/unit/cmk/gui/valuespecs/test_vs_percentage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import cmk.gui.valuespec as vs
+
+from .utils import expect_validate_failure, expect_validate_success
+
+
+class TestValueSpecPercentage:
+    def test_validate(self) -> None:
+        expect_validate_failure(vs.Percentage(), 105.0)
+        expect_validate_success(vs.Percentage(), 101.0)
+        expect_validate_failure(vs.Percentage(), -10.0)
+        expect_validate_failure(vs.Percentage(), 10)  # type: ignore
+        expect_validate_success(vs.Percentage(allow_int=True), 10)  # type: ignore
+        expect_validate_failure(vs.Percentage(allow_int=True), "10")  # type: ignore
+
+    def test_value_to_html(self) -> None:
+        assert vs.Percentage().value_to_html(100) == "100.0%"
+        assert vs.Percentage().value_to_html(10) == "10.0%"
+        assert vs.Percentage().value_to_html(1) == "1.0%"
+        assert vs.Percentage().value_to_html(0.1) == "0.1%"
+        assert vs.Percentage().value_to_html(0.01) == "0.0%"
+        assert vs.Percentage().value_to_html(-0.01) == "-0.0%"

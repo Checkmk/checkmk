62e8f8562273e170c153396f29e76426052d88b9
Checkmk release system <feedback@checkmk.com>
2022-08-30 11:44:38 +0200

    Set version to 2.0.0p29

--- agents/windows/plugins/megaraid.bat
+++ agents/windows/plugins/megaraid.bat
@@ -1,5 +1,5 @@
 @ECHO off & setlocal EnableDelayedExpansion
-set CMK_VERSION="2.0.0p28"
+set CMK_VERSION="2.0.0p29"
 REM **********************************************************************
 REM * Script: megaraid.bat
 REM * Author: Josef Hack
--- agents/windows/plugins/mk_dhcp_enabled.bat
+++ agents/windows/plugins/mk_dhcp_enabled.bat
@@ -1,5 +1,5 @@
 @echo off
-set CMK_VERSION="2.0.0p28"
+set CMK_VERSION="2.0.0p29"
 echo ^<^<^<winperf_if^>^>^>
 echo [dhcp_start]
 wmic path Win32_NetworkAdapterConfiguration get Description, dhcpenabled

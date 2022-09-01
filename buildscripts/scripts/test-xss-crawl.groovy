4d17e9bb61de78833601e432cf9a496508586f20
Frans Fürst <frans.fuerst@tribe29.com>
2022-09-01 14:27:30 +0200

    fixes xss-crawl job to use the right artifact name component
    Change-Id: Ia1f9b2c2187c3adc7bba9dd3f08221356bf88393

--- buildscripts/scripts/test-xss-crawl.groovy
+++ buildscripts/scripts/test-xss-crawl.groovy
@@ -1,9 +1,12 @@
 #!groovy
 
+/// test-xss-crawl.groovy
+
 def main() {
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
     def testing_helper = load("${checkout_dir}/buildscripts/scripts/utils/integration.groovy");
     def branch_name = versioning.safe_branch_name(scm);
+    def version = "daily";
 
     check_environment_variables([
         "DOCKER_TAG",
@@ -14,14 +17,14 @@ def main() {
             DOCKER_GROUP_ID: get_docker_group_id(),
             DISTRO_LIST: ["ubuntu-20.04"],
             EDITION: "enterprise",
-            VERSION: "daily",
+            VERSION: version,
             DOCKER_TAG: versioning.select_docker_tag(
                 branch_name,
                 "", 
                 ""),   // FIXME was DOCKER_TAG_DEFAULT before
             MAKE_TARGET: "test-xss-crawl-docker",
             BRANCH: branch_name,
-            cmk_version: versioning.get_cmk_version(branch_name, "git"),
+            cmk_version: versioning.get_cmk_version(branch_name, version),
         );
     } finally {
         stage('archive crawler report') {

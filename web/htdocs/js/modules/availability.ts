24dd766cd21ff2531333858e08b397e23be3f5d1
Mazen Alkatlabee <mazen.alkatlabee@tribe29.com>
2022-09-06 17:26:24 +0200

    fix eslint errors in availability.ts
    Change-Id: Ic8d76e83095c12184af2c3b367d59a3b8615140a

--- web/htdocs/js/modules/availability.ts
+++ web/htdocs/js/modules/availability.ts
@@ -5,7 +5,7 @@
 import * as utils from "utils";
 
 export function timeline_hover(timeline_nr, row_nr, onoff) {
-    var row = document.getElementById(
+    const row = document.getElementById(
         "timetable_" + timeline_nr + "_entry_" + row_nr
     );
     if (!row) return;
@@ -18,7 +18,7 @@ export function timeline_hover(timeline_nr, row_nr, onoff) {
 }
 
 export function timetable_hover(timeline_nr, row_nr, onoff) {
-    var slice = document.getElementById(
+    const slice = document.getElementById(
         "timeline_" + timeline_nr + "_entry_" + row_nr
     );
     if (!slice) return;

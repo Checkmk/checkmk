97c442d48a41b4da9faf756db7878d77320bdb9b
Marcel Arentz <marcel.arentz@tribe29.com>
2022-09-06 13:45:59 +0200

    Some changes for readability
    Change-Id: I2851d625c2392d61ed890ea11bc417cad6cfefab

--- cmk/base/plugins/agent_based/mobileiron_statistics.py
+++ cmk/base/plugins/agent_based/mobileiron_statistics.py
@@ -14,7 +14,9 @@ from cmk.base.plugins.agent_based.agent_based_api.v1 import (
     Metric,
     register,
     render,
+    Result,
     Service,
+    State,
 )
 
 from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult
@@ -25,18 +27,32 @@ def check_mobileiron_sourcehost(
     params: Mapping[str, Any], section: SourceHostSection
 ) -> CheckResult:
 
-    yield Metric(name="mobileiron_devices_total", value=section.total_count)
+    yield Metric(
+        name="mobileiron_devices_total",
+        value=section.total_count,
+    )
 
     yield Metric(name="mobileiron_non_compliant", value=section.non_compliant)
 
     non_compliant_percent = section.non_compliant / section.total_count * 100
     yield from check_levels(
-        label=f"Number of non-compliant devices: {section.non_compliant} out of {section.total_count}",
+        label="Non-compliant devices",
         value=non_compliant_percent,
+        metric_name="mobileiron_non_compliant_summary",
         levels_upper=params.get("non_compliant_summary_levels"),
         render_func=render.percent,
         notice_only=True,
         boundaries=(0, 100),
+    )
+
+    yield Result(
+        state=State.OK,
+        summary=f"Non-compliant: {section.non_compliant}",
+    )
+
+    yield Result(
+        state=State.OK,
+        summary=f"Total: {section.total_count}",
     )
 
 

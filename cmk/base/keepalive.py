d257de96e87b2fb75adcafa33b9fab43068571f7
Mathias Laurin <mathias.laurin@tribe29.com>
2022-09-07 14:26:23 +0200

    Move (and rename) NoKeepalive
    Let us not assume that we *never* know in which
    edition we are running.  ;)
    
    CMK-11212
    
    Change-Id: I825cdb8ec3f7479817bb981de133e7fe74225b84

--- cmk/base/keepalive.py
+++ cmk/base/keepalive.py
@@ -3,31 +3,16 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Literal, NoReturn, Optional
-
-from cmk.utils.type_defs import HostName, KeepaliveAPI, ServiceDetails, ServiceName, ServiceState
+from cmk.utils.type_defs import KeepaliveAPI
 from cmk.utils.version import Edition
+
+from cmk.base.no_keepalive import NO_KEEPALIVE
 
 
 def get_keepalive(edition: Edition) -> KeepaliveAPI:
     if edition is Edition.CRE:
-        return NoOpKeepaliveDummy()
+        return NO_KEEPALIVE
 
     import cmk.base.cee.keepalive as keepalive  # type: ignore[import] # pylint: disable=no-name-in-module
 
     return keepalive
-
-
-class NoOpKeepaliveDummy:
-    def enabled(self) -> Literal[False]:
-        return False
-
-    def add_check_result(
-        self,
-        host: HostName,
-        service: ServiceName,
-        state: ServiceState,
-        output: ServiceDetails,
-        cache_info: Optional[tuple[int, int]],
-    ) -> NoReturn:
-        raise NotImplementedError("Keepalive not available")

c88ce68e94270411cabdc7220401792fcd30d3e2
Sven Panne <sven.panne@tribe29.com>
2022-09-07 10:35:26 +0200

    Re-arranged code a bit. Explicitly use python3. Disable capture.
    Change-Id: I28cf348643540eb091fed867dbaf5a326327f2b6

--- tests/pylint/test_pylint.py
+++ tests/pylint/test_pylint.py
@@ -8,7 +8,7 @@ import multiprocessing
 import os
 import subprocess
 import tempfile
-from collections.abc import Sequence
+from collections.abc import Iterable, Sequence
 from io import TextIOWrapper
 from pathlib import Path
 from typing import Iterator
@@ -19,31 +19,57 @@ from tests.testlib import cmk_path, repo_path
 
 
 def test_pylint(capsys: pytest.CaptureFixture[str]) -> None:
-    with capsys.disabled():
+    with tempfile.TemporaryDirectory(prefix="cmk_pylint_") as tmpdir, capsys.disabled():
         print("\n")
-        retcode = subprocess.call("python -m pylint --version".split(), shell=False)
+        assert run_pylint(["--version"]) == 0
         print()
-        assert not retcode
-
-    with tempfile.TemporaryDirectory(prefix="cmk_pylint_") as pylint_test_dir:
-        exit_code = run_pylint(Path(repo_path()), _get_files_to_check(Path(pylint_test_dir)))
-    assert exit_code == 0, "PyLint found an error"
+        assert run_pylint(construct_args(Path(tmpdir))) == 0, "Pylint found an error"
 
 
-def _get_files_to_check(pylint_test_dir: Path) -> Sequence[Path]:
+def run_pylint(args: Iterable[str]) -> int:
+    cmd = ["python3", "-m", "pylint"] + list(args)
+    return subprocess.run(cmd, cwd=repo_path(), check=False).returncode
+
+
+def construct_args(tmpdir: Path) -> list[str]:
+    args = [
+        f"--rcfile={Path(repo_path()) / '.pylintrc'}",
+        f"--jobs={num_jobs_to_use()}",
+    ]
+    if pylint_args := os.environ.get("PYLINT_ARGS"):
+        args += pylint_args.split(" ")
+    args.extend(str(f) for f in _get_files_to_check(tmpdir))
+    return args
+
+
+def num_jobs_to_use() -> int:
+    # Naive heuristic, but looks OK for our use cases:\ Normal quad core CPUs
+    # with HT report 8 CPUs (=> 6 jobs), our server 24-core CPU reports 48 CPUs
+    # (=> 11 jobs). Just using 0 (meaning: use all reported CPUs) might just
+    # work, too, but it's probably a bit too much.
+    #
+    # On our CI server there are currently up to 5 parallel Gerrit jobs allowed
+    # which trigger pylint + 1 explicit pylint job per Checkmk branch. This
+    # means that there may be up to 8 pylint running in parallel. Currently
+    # these processes consume about 400 MB of rss memory.  To prevent swapping
+    # we need to reduce the parallelization of pylint for the moment.
+    if os.environ.get("USER") == "jenkins":
+        return int(multiprocessing.cpu_count() / 8.0) + 3
+    return int(multiprocessing.cpu_count() / 8.0) + 5
+
+
+def _get_files_to_check(tmpdir: Path) -> Sequence[Path]:
     # Add the compiled files for things that are no modules yet
-    (pylint_test_dir / "__init__.py").touch()
-    _compile_check_plugins(pylint_test_dir)
+    (tmpdir / "__init__.py").touch()
+    _compile_check_plugins(tmpdir)
 
     # Not checking compiled check, inventory, bakery plugins with Python 3
-    files = [pylint_test_dir]
+    files = [tmpdir]
 
     completed_process = subprocess.run(
         [f"{repo_path()}/scripts/find-python-files"],
         stdout=subprocess.PIPE,
         encoding="utf-8",
-        shell=False,
-        close_fds=True,
         check=False,
     )
 
@@ -76,6 +102,16 @@ def _get_files_to_check(pylint_test_dir: Path) -> Sequence[Path]:
         files.append(Path(fname))
 
     return files
+
+
+def _compile_check_plugins(pylint_test_dir: Path) -> None:
+    for idx, f_name in enumerate(check_files(Path(repo_path()) / "checks")):
+        with stand_alone_template(pylint_test_dir / f"cmk_checks_{idx}.py") as file_handle:
+            add_file(file_handle, f_name)
+
+
+def check_files(base_dir: Path) -> Sequence[Path]:
+    return sorted(Path(base_dir) / f for f in os.listdir(base_dir) if not f.startswith("."))
 
 
 @contextlib.contextmanager
@@ -117,16 +153,6 @@ special_agent_info                 = {}
         yield file_handle
 
 
-def _compile_check_plugins(pylint_test_dir: Path) -> None:
-    for idx, f_name in enumerate(check_files(Path(repo_path()) / "checks")):
-        with stand_alone_template(pylint_test_dir / f"cmk_checks_{idx}.py") as file_handle:
-            add_file(file_handle, f_name)
-
-
-def check_files(base_dir: Path) -> Sequence[Path]:
-    return sorted(Path(base_dir) / f for f in os.listdir(base_dir) if not f.startswith("."))
-
-
 def add_file(f: TextIOWrapper, path: Path) -> None:
     relpath = os.path.relpath(os.path.realpath(path), cmk_path())
     f.write("# -*- encoding: utf-8 -*-")
@@ -135,38 +161,3 @@ def add_file(f: TextIOWrapper, path: Path) -> None:
     f.write("#\n")
     f.write("\n")
     f.write(path.read_text())
-
-
-def run_pylint(base_path: Path, files_to_check: Sequence[Path]) -> int:
-    cmd = [
-        "python",
-        "-m",
-        "pylint",
-        f"--rcfile={Path(repo_path()) / '.pylintrc'}",
-        f"--jobs={num_jobs_to_use()}",
-    ]
-    pylint_args = args.split(" ") if (args := os.environ.get("PYLINT_ARGS")) else []
-    files = pylint_args + [str(f) for f in files_to_check]
-    print(
-        f"Running pylint in '{base_path}' with: {subprocess.list2cmdline(cmd)}"
-        f" [{len(files)} files omitted]"
-    )
-    exit_code = subprocess.run(cmd + files, shell=False, cwd=base_path, check=False).returncode
-    print(f"Finished with exit code: {exit_code}")
-    return exit_code
-
-
-def num_jobs_to_use() -> int:
-    # Naive heuristic, but looks OK for our use cases:\ Normal quad core CPUs
-    # with HT report 8 CPUs (=> 6 jobs), our server 24-core CPU reports 48 CPUs
-    # (=> 11 jobs). Just using 0 (meaning: use all reported CPUs) might just
-    # work, too, but it's probably a bit too much.
-    #
-    # On our CI server there are currently up to 5 parallel Gerrit jobs allowed
-    # which trigger pylint + 1 explicit pylint job per Checkmk branch. This
-    # means that there may be up to 8 pylint running in parallel. Currently
-    # these processes consume about 400 MB of rss memory.  To prevent swapping
-    # we need to reduce the parallelization of pylint for the moment.
-    if os.environ.get("USER") == "jenkins":
-        return int(multiprocessing.cpu_count() / 8.0) + 3
-    return int(multiprocessing.cpu_count() / 8.0) + 5

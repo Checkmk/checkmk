9c10425e3dda44986f5942c7577d6304721a2b7c
Lars Michelsen <lm@tribe29.com>
2022-09-07 20:06:04 +0200

    Additional fix for container update
    Initially create the old hook during update. This file
    will be replaced later during update procedure with the
    correct file for the Checkmk version.
    
    Change-Id: I3bb248bfe6bed4e68b5062e27bdf7fc840a24ec9

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -98,6 +98,9 @@ fi
 if ! getent passwd "$CMK_SITE_ID" >/dev/null; then
     useradd -u 1000 -d "/omd/sites/$CMK_SITE_ID" -c "OMD site $CMK_SITE_ID" -g "$CMK_SITE_ID" -G omd -s /bin/bash "$CMK_SITE_ID"
 fi
+if [ ! -f "/omd/apache/$CMK_SITE_ID.conf" ]; then
+    echo "Include /omd/sites/$CMK_SITE_ID/etc/apache/mode.conf" >"/omd/apache/$CMK_SITE_ID.conf"
+fi
 
 # In case the version symlink is dangling we are in an update situation: The
 # volume was previously attached to a container with another Checkmk version.

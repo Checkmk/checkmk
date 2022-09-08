ba5fe6f858ca4fe6fc317759e7ec0e2c8296d556
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-09-08 15:32:47 +0200

    Add test for valuespec.RegExp
    CMK-11015
    
    Change-Id: I21b8f309d57d5d4f01e1d9cf5e326d3d8c6d0cf9

---
+++ tests/unit/cmk/gui/valuespecs/test_vs_regexp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+
+import cmk.gui.valuespec as vs
+
+from .utils import expect_validate_failure, expect_validate_success
+
+
+class TestRegexp:
+    def test_validate(self) -> None:
+        expect_validate_success(vs.RegExp("infix"), "^$")
+        expect_validate_failure(vs.RegExp("infix"), "(", match="Invalid regular expression:")
+        expect_validate_failure(
+            vs.RegExp("infix", mingroups=1), "^$", match="You need at least <b>1</b> groups."
+        )
+        expect_validate_success(vs.RegExp("infix", mingroups=1), "^(.+)$")
+        expect_validate_failure(
+            vs.RegExp("infix", maxgroups=2),
+            "^(.)(.)(.)$",
+            match="It must have at most <b>2</b> groups.",
+        )
+        expect_validate_success(vs.RegExp("infix", maxgroups=2), "^(.+)$")

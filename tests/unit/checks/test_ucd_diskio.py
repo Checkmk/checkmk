c1f37f34f62c163a0dd733d01c16d9209ec9fac6
LukaRacic <luka.racic@tribe29.com>
2022-09-08 11:33:26 +0200

    ucd_diskio: Added test for the check function
    Change-Id: Ic5c924fa71b79949c5bf9f2330f0731925ad1207

--- tests/unit/checks/test_ucd_diskio.py
+++ tests/unit/checks/test_ucd_diskio.py
@@ -9,9 +9,11 @@ from tests.unit.conftest import FixRegister
 
 from cmk.utils.type_defs import CheckPluginName, SectionName
 
+from cmk.base import item_state
 from cmk.base.api.agent_based.checking_classes import CheckPlugin
 from cmk.base.api.agent_based.type_defs import SNMPSectionPlugin
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Service
+from cmk.base.api.agent_based.utils import GetRateError
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, Service, State
 
 
 @pytest.fixture(name="check_plugin")
@@ -50,3 +52,56 @@ def test_discover_ucd_diskio(  # type:ignore[no-untyped-def]
 
 def test_discover_ucd_diskio_with_empty_section(check_plugin: CheckPlugin) -> None:
     assert list(check_plugin.discovery_function({})) == []
+
+
+def test_check_ucd_diskio_item_not_found(  # type:ignore[no-untyped-def]
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+    string_table,
+) -> None:
+    assert (
+        list(
+            check_plugin.check_function(
+                item="not_found",
+                params={},
+                section=section_plugin.parse_function(string_table),
+            )
+        )
+        == []
+    )
+
+
+def test_check_ucd_diskio_raise_get_rate_error(  # type:ignore[no-untyped-def]
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+    string_table,
+) -> None:
+    with pytest.raises(GetRateError):
+        list(
+            check_plugin.check_function(
+                item="ram0",
+                params={},
+                section=section_plugin.parse_function(string_table),
+            )
+        )
+
+
+def test_check_function_first_result_is_ok(  # type:ignore[no-untyped-def]
+    check_plugin: CheckPlugin,
+    section_plugin: SNMPSectionPlugin,
+    string_table,
+) -> None:
+    for field in ["read_ios", "write_ios", "read_throughput", "write_throughput"]:
+        # Setting the previous states, so that the get_rate function doesn't return a GetRateError
+        item_state.set_item_state(f"ucd_disk_io_{field}.ram0", (0, 0))
+
+    check_result = list(
+        check_plugin.check_function(
+            item="ram0",
+            params={},
+            section=section_plugin.parse_function(string_table),
+        )
+    )
+
+    assert check_result[0] == Result(state=State.OK, summary="[1]")
+    assert len(check_result) == 9  # The first result plus a Result and Metric for every field

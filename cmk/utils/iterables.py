690a710defa967db5fea8aacc93b01a9810e21b4
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-09-09 22:11:42 +0200

    cleanup dead code
    Change-Id: I3f07a56e073faf1aee866648bbadf9491ae39182

--- cmk/utils/iterables.py
+++ cmk/utils/iterables.py
@@ -3,36 +3,15 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Callable, Iterable, List, Optional, Tuple, TypeVar
+from collections.abc import Iterable
+from typing import Callable, TypeVar
 
 T = TypeVar("T")
 
 
-def first(iterable: Iterable[T]) -> Optional[T]:
-    """Return the first element of an iterable or None if it is empty.
-
-    Examples:
-
-        >>> first([1, 2, 3])
-        1
-
-        >>> first([])
-
-    Args:
-        iterable (Iterable): Something that supports Python's iterator protocol.
-
-    Returns:
-        The first entry of the iterable or None
-    """
-    try:
-        return next(iter(iterable))
-    except StopIteration:
-        return None
-
-
-def partition(pred: Callable[[T], bool], iterable: Iterable[T]) -> Tuple[List[T], List[T]]:
-    yay: List[T] = []
-    nay: List[T] = []
+def partition(pred: Callable[[T], bool], iterable: Iterable[T]) -> tuple[list[T], list[T]]:
+    yay: list[T] = []
+    nay: list[T] = []
     for x in iterable:
         (yay if pred(x) else nay).append(x)
     return yay, nay

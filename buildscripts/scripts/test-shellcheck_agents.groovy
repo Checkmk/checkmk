fbc32f54eff0be60c8112555311cb1bdd3f3d6df
Frans Fürst <frans.fuerst@tribe29.com>
2022-09-09 14:10:04 +0200

    add forgotten groovy script
     #jjb-migration
    
    Change-Id: Id4f78dbb5c5b46f4e3636b70162b1a15a43e1789

--- buildscripts/scripts/test-shellcheck_agents.groovy
+++ buildscripts/scripts/test-shellcheck_agents.groovy
@@ -1,34 +1,31 @@
-def main() {
-    /*
-    properties([
-        pipelineTriggers([pollSCM('H/3 * * * *')]),
-    ])
+#!groovy
 
-    def TEST_IMAGE = docker.build("test-image:${env.BUILD_ID}", "--pull buildscripts/docker_image_aliases/IMAGE_TESTING")
-    TEST_IMAGE.inside("--ulimit nofile=1024:1024 --init") {
+/// test-shellcheck_agents.groovy
+
+def main() {
+    dir("${checkout_dir}") {
         stage("Execute Test") {
-            catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
-                dir("tests") {
-                    sh("""make SHELLCHECK_OUTPUT_ARGS="-f gcc" test-shellcheck""");
+            docker_image_from_alias("IMAGE_TESTING").inside("--ulimit nofile=1024:1024 --init") {
+                catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
+                    dir("tests") {
+                        sh("""make SHELLCHECK_OUTPUT_ARGS="-f gcc" test-shellcheck""");
+                    }
                 }
             }
         }
         stage("Analyse Issues") {
-            def GCC = scanForIssues(tool: gcc())
             publishIssues(
-                issues:[GCC],
+                issues:[scanForIssues(tool: gcc())],
                 trendChartType: 'TOOLS_ONLY',
                 qualityGates: [
                     [
                         threshold: 165,
                         type: 'TOTAL',
-                        unstable: false
+                        unstable: false,
                     ]
                 ]
             )
         }
     }
-    */
 }
 return this;
-

e7690f03c7ccdc839c361c3f2e5e8329d9ad6b54
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-09-13 16:06:53 +0200

    add configuring powershell script
    This script overwrites Windows defaults to defaults
    required by windows agent.
    In this CL only Utf8-encoding is enforced.
    
    Change-Id: Id4bf8568f9db27cb61c28a58c8f6d325db138d28

---
+++ agents/wnx/install/resources/configure_and_exec.ps1
@@ -0,0 +1,16 @@
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# Script to correctly configure defaults for other powershel scripts 
+
+$executable = $args[0]
+$OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+switch ( $args.count ) {
+    0 {}
+    1 { & "$executable" }
+    2 { & "$executable" $args[1] }
+    3 { & "$executable" $args[1] $args[2] }
+    4 { & "$executable" $args[1] $args[2] $args[3] }
+    default {}
+}

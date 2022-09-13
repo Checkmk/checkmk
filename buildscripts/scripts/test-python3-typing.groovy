d2d4847e1a73bea7454e6701e87ebbe9d76d2fa5
Frans Fürst <frans.fuerst@tribe29.com>
2022-09-13 15:09:05 +0200

    Fix test-python3-typing folder access issue
     #jjb-migration
    
    Change-Id: I7699b945d03b176467cffcede365e34f0a5f89e1

--- buildscripts/scripts/test-python3-typing.groovy
+++ buildscripts/scripts/test-python3-typing.groovy
@@ -3,41 +3,41 @@
 /// test-python3-typing.groovy
 
 def main() {
-    stage("Execute Test") {
-        sh("""
-            MYPY_ADDOPTS='--cobertura-xml-report=$WORKSPACE/mypy_reports --html-report=$WORKSPACE/mypy_reports/html' \
-            make -C $checkout_dir/tests test-mypy-docker
-           """);
-    }
+    dir("${checkout_dir}") {
+        stage("Execute Test") {
+            sh("""
+                MYPY_ADDOPTS='--cobertura-xml-report=$checkout_dir/mypy_reports --html-report=$checkout_dir/mypy_reports/html' \
+                make -C tests test-mypy-docker
+               """);
+        }
 
-    stage("Archive reports") {
-        archiveArtifacts(artifacts: "mypy_reports/**");
-    }
+        stage("Archive reports") {
+            archiveArtifacts(artifacts: "mypy_reports/**");
+        }
 
-    stage("Analyse Issues") {
-        publishIssues(
-            issues:[scanForIssues(tool: clang())],
-            trendChartType: 'TOOLS_ONLY',
-            qualityGates: [
-                [
+        stage("Analyse Issues") {
+            publishIssues(
+                issues:[scanForIssues(tool: clang())],
+                trendChartType: 'TOOLS_ONLY',
+                qualityGates: [[
                     threshold: 1,
                     type: 'TOTAL',
                     unstable: false,
-                ]
-            ]
-        )
-    }
+                ]]
+            )
+        }
 
-    stage("Publish coverage") {
-        publishHTML([
-            allowMissing: false,
-            alwaysLinkToLastBuild: false,
-            keepAll: true,
-            reportDir: 'mypy_reports/html',
-            reportFiles: 'index.html',
-            reportName: 'Typing coverage',
-            reportTitles: '',
-        ])
+        stage("Publish coverage") {
+            publishHTML([
+                allowMissing: false,
+                alwaysLinkToLastBuild: false,
+                keepAll: true,
+                reportDir: 'mypy_reports/html',
+                reportFiles: 'index.html',
+                reportName: 'Typing coverage',
+                reportTitles: '',
+            ])
+        }
     }
 }
 return this;

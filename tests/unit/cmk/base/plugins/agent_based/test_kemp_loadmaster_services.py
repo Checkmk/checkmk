a478144e6696ae76d2cfd633c78cac329cfa9199
Solomon Jacobs <solomon.jacobs@tribe29.com>
2022-09-13 13:12:04 +0200

    14558 FIX kempload_master_services: Missing Conns metric
    The active connections are now shown in the service graph, again.
    
    A regression caused by Change: Ib8e31cd682f0da2b43108f2fadc446b0af9aadec
    SUP-11554
    
    Change-Id: I450a215735ca0de7ff14782ea66f87fc1f88b932

--- tests/unit/cmk/base/plugins/agent_based/test_kemp_loadmaster_services.py
+++ tests/unit/cmk/base/plugins/agent_based/test_kemp_loadmaster_services.py
@@ -8,7 +8,7 @@ from typing import Sequence
 
 import pytest
 
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, Service, State
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
 from cmk.base.plugins.agent_based.kemp_loadmaster_services import (
     check_kemp_loadmaster_services,
     discover_kemp_loadmaster_services,
@@ -52,6 +52,7 @@ def test_discovery_kemp_loadmaster_services(section: Section,
             [
                 Result(state=State.OK, summary="Status: in service"),
                 Result(state=State.OK, summary="Active connections: 200"),
+                Metric("conns", 200),
             ],
         ),
         (
@@ -59,6 +60,7 @@ def test_discovery_kemp_loadmaster_services(section: Section,
             [
                 Result(state=State.OK, summary="Status: in service"),
                 Result(state=State.OK, summary="Active connections: 100"),
+                Metric("conns", 100),
             ],
         ),
         (
@@ -66,6 +68,7 @@ def test_discovery_kemp_loadmaster_services(section: Section,
             [
                 Result(state=State.UNKNOWN, summary="Status: unknown[8]"),
                 Result(state=State.OK, summary="Active connections: 0"),
+                Metric("conns", 0),
             ],
         ),
     ],

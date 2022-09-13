6f4342932117a2194ba40d9b0a73245d5627a1bd
Frans Fürst <frans.fuerst@tribe29.com>
2022-09-13 14:57:51 +0200

    Fix test-shell_format to not spawn a container
     #jjb-migration
    
    Change-Id: Ic8ffa92538e9398e36f011d2a97eafec4b1c3778

--- buildscripts/scripts/test-shell_format.groovy
+++ buildscripts/scripts/test-shell_format.groovy
@@ -5,16 +5,18 @@
 def main() {
     stage('Check shell format') {
         dir("${checkout_dir}") {
-            docker_image_from_alias("IMAGE_TESTING").inside("--ulimit nofile=1024:1024 --init") {
-                sh("make -C tests test-format-shell")
-            }
+            sh("make -C tests test-format-shell")
         }
     }
     stage("Analyse Issues") {
         publishIssues(
             issues:[scanForIssues(tool: clang())],
             trendChartType: 'TOOLS_ONLY',
-            qualityGates: [[threshold: 1, type: 'TOTAL', unstable: false]],
+            qualityGates: [[
+                threshold: 1,
+                type: 'TOTAL',
+                unstable: false,
+            ]],
         );
     }
 }

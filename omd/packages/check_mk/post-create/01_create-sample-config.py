e663a98ee4586a190e26ea006903a31b0f561dbd
Moritz Kiemer <moritz.kiemer@tribe29.com>
2022-09-20 14:43:32 +0200

    Fix creation of sample config
    Writing the hosts.mk file requires permissions...
    
    Change-Id: Id3378afa95c2c01ee4dc42e4a3e645c87027991c

--- omd/packages/check_mk/post-create/01_create-sample-config.py
+++ omd/packages/check_mk/post-create/01_create-sample-config.py
@@ -7,10 +7,11 @@
 
 
 from cmk.gui import main_modules
+from cmk.gui.utils.logged_in import SuperUserContext
 from cmk.gui.utils.script_helpers import gui_context
 from cmk.gui.watolib.sample_config import init_wato_datastructures
 
 if __name__ == "__main__":
     main_modules.load_plugins()
-    with gui_context():
+    with gui_context(), SuperUserContext():
         init_wato_datastructures()

23880db25f22a0324fe59179c985484bab4fc098
Christoph Rauch <christoph.rauch@tribe29.com>
2022-09-22 16:01:33 +0200

    14508 FIX profiling using "enable_by_var" in the REST API
    When using the query string variable "_profile=1" to dynamically enable profiling,
    the REST API complained about "_profile" being an unknown paramter. This is no longer
    the case, as the argument is now removed before passing the request to the REST API.
    
    There are no futher actions to be taken.
    
    Change-Id: I70f8cda5d5c995832cf8eecb10a31f35be8922c7

--- cmk/gui/plugins/openapi/restful_objects/decorators.py
+++ cmk/gui/plugins/openapi/restful_objects/decorators.py
@@ -214,7 +214,30 @@ def _render_path_item(
     return response
 
 
-def _from_multi_dict(multi_dict: MultiDict) -> Dict[str, Union[List[str], str]]:
+ArgDict = dict[str, str | list[str]]
+
+
+def _filter_profile_headers(arg_dict: ArgDict) -> ArgDict:
+    """Filter the _profile variable from the query string
+
+    Args:
+        arg_dict:
+            A dict of query string arguments
+
+    Returns:
+        A new dict without the '_profile' parameter.
+
+
+    Examples:
+
+        >>> _filter_profile_headers({'foo': 'bar', '_profile': '1'})
+        {'foo': 'bar'}
+
+    """
+    return {key: value for key, value in arg_dict.items() if not key.startswith("_profile")}
+
+
+def _from_multi_dict(multi_dict: MultiDict) -> ArgDict:
     """Transform a MultiDict to a non-heterogenous dict
 
     Meaning: lists are lists and lists of lenght 1 are scalars.
@@ -674,7 +697,9 @@ class Endpoint:
 
             try:
                 if query_schema:
-                    _params.update(query_schema().load(_from_multi_dict(request.args)))
+                    _params.update(
+                        query_schema().load(_filter_profile_headers(_from_multi_dict(request.args)))
+                    )
 
                 if header_schema:
                     _params.update(header_schema().load(request.headers))

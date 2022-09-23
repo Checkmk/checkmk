5d86c333c6a17a87386d717faba86efb303ee2f1
Christoph Rauch <christoph.rauch@tribe29.com>
2022-09-23 14:23:25 +0200

    REST API group tests: isolate tests better
     * some tests failed due to stale groups of other tests
     * every test now deletes its created groups
    
    Change-Id: I5571bed7be8261a51779ee736ea4ef636560a6db

--- tests/unit/cmk/gui/plugins/openapi/test_openapi_group_config.py
+++ tests/unit/cmk/gui/plugins/openapi/test_openapi_group_config.py
@@ -95,7 +95,7 @@ def test_openapi_bulk_groups(  # type:ignore[no-untyped-def]
     base = "/NO_SITE/check_mk/api/1.0"
     resp = aut_user_auth_wsgi_app.call_method(
         "post",
-        base + "/domain-types/%s_group_config/actions/bulk-create/invoke" % (group_type,),
+        base + f"/domain-types/{group_type}_group_config/actions/bulk-create/invoke",
         params=json.dumps({"entries": groups}),
         headers={"Accept": "application/json"},
         status=200,
@@ -113,7 +113,7 @@ def test_openapi_bulk_groups(  # type:ignore[no-untyped-def]
 
     _resp = aut_user_auth_wsgi_app.call_method(
         "post",
-        base + "/domain-types/%s_group_config/actions/bulk-create/invoke" % (group_type,),
+        base + f"/domain-types/{group_type}_group_config/actions/bulk-create/invoke",
         params=json.dumps({"entries": groups}),
         headers={"Accept": "application/json"},
         status=400,
@@ -130,7 +130,7 @@ def test_openapi_bulk_groups(  # type:ignore[no-untyped-def]
 
     _resp = aut_user_auth_wsgi_app.call_method(
         "put",
-        base + "/domain-types/%s_group_config/actions/bulk-update/invoke" % (group_type,),
+        base + f"/domain-types/{group_type}_group_config/actions/bulk-update/invoke",
         params=json.dumps({"entries": update_groups}),
         headers={"Accept": "application/json"},
         status=200,
@@ -157,7 +157,7 @@ def test_openapi_bulk_groups(  # type:ignore[no-untyped-def]
 
     _resp = aut_user_auth_wsgi_app.call_method(
         "put",
-        base + "/domain-types/%s_group_config/actions/bulk-update/invoke" % (group_type,),
+        base + f"/domain-types/{group_type}_group_config/actions/bulk-update/invoke",
         params=json.dumps({"entries": partial_update_groups}),
         headers={"Accept": "application/json"},
         status=200,
@@ -174,8 +174,8 @@ def test_openapi_bulk_groups(  # type:ignore[no-untyped-def]
 
     _resp = aut_user_auth_wsgi_app.call_method(
         "post",
-        base + "/domain-types/%s_group_config/actions/bulk-delete/invoke" % (group_type,),
-        params=json.dumps({"entries": [f"{group['name']}" for group in groups]}),
+        base + f"/domain-types/{group_type}_group_config/actions/bulk-delete/invoke",
+        params=json.dumps({"entries": [group["name"] for group in groups]}),
         headers={"Accept": "application/json"},
         status=204,
         content_type="application/json",
@@ -236,20 +236,36 @@ def test_openapi_groups_with_customer(  # type:ignore[no-untyped-def]
     )
     assert resp.json_body["extensions"]["customer"] == "provider"
 
+    aut_user_auth_wsgi_app.delete(
+        base + f"/objects/{group_type}_group_config/{name}",
+        headers={"If-Match": resp.headers["ETag"], "Accept": "application/json"},
+        status=204,
+        content_type="application/json",
+    )
+
 
 @managedtest
 @pytest.mark.parametrize("group_type", ["host", "contact", "service"])
 def test_openapi_group_values_are_links(  # type:ignore[no-untyped-def]
-    group_type, wsgi_app, with_automation_user
+    group_type, wsgi_app, with_automation_user, base
 ) -> None:
     username, secret = with_automation_user
     wsgi_app.set_authorization(("Bearer", username + " " + secret))
 
-    base = "/NO_SITE/check_mk/api/1.0"
-    postfix = f"/domain-types/{group_type}_group_config/collections/all"
-    url = f"{base}{postfix}"
+    collection_url = f"{base}/domain-types/{group_type}_group_config/collections/all"
+    response = wsgi_app.call_method(
+        url=collection_url,
+        method="GET",
+        headers={"Accept": "application/json"},
+    )
+    json_data = response.json
+    assert len(json_data["value"]) == 0
 
-    group = {"name": "foo_bar", "alias": "foo bar", "customer": "global"}
+    group = {
+        "name": f"{group_type}_foo_bar",
+        "alias": f"{group_type} foo bar",
+        "customer": "global",
+    }
     _response = wsgi_app.call_method(
         "post",
         base + f"/domain-types/{group_type}_group_config/collections/all",
@@ -259,7 +275,11 @@ def test_openapi_group_values_are_links(  # type:ignore[no-untyped-def]
         content_type="application/json",
     )
 
-    response = wsgi_app.call_method(url=url, method="GET", headers={"Accept": "application/json"})
+    response = wsgi_app.call_method(
+        url=collection_url,
+        method="GET",
+        headers={"Accept": "application/json"},
+    )
     assert response.status_code == 200
 
     json_data = response.json

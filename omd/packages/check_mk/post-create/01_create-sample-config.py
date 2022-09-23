c66d49a627903c918f83245e5068068a6244402b
Lars Michelsen <lm@tribe29.com>
2022-09-23 16:55:51 +0200

    Create sample config: Improve logging
    * Initialize logger module (info log level by default)
    * Add flag to enable verbose or debug logging
    
    Change-Id: Ida67468f9e22a9e680ca15339e0d21e148f5972e

--- omd/packages/check_mk/post-create/01_create-sample-config.py
+++ omd/packages/check_mk/post-create/01_create-sample-config.py
@@ -7,12 +7,40 @@
 
 # pylint: disable=cmk-module-layer-violation
 
+import argparse
+import sys
+
+import cmk.utils.log as log
+
 from cmk.gui import main_modules
 from cmk.gui.logged_in import SuperUserContext
 from cmk.gui.utils.script_helpers import gui_context
 from cmk.gui.watolib.sample_config import init_wato_datastructures
 
-if __name__ == "__main__":
+
+def parse_arguments(args: list[str]) -> argparse.Namespace:
+    p = argparse.ArgumentParser(description=__doc__)
+    p.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbose mode (use multiple times for more output)",
+    )
+
+    return p.parse_args(args)
+
+
+def main(args: list[str]) -> int:
+    arguments = parse_arguments(args)
+    log.setup_console_logging()
+    log.logger.setLevel(log.verbosity_to_log_level(arguments.verbose))
+
     main_modules.load_plugins()
     with gui_context(), SuperUserContext():
         init_wato_datastructures()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))

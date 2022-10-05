99afcad7c8e57e9194a5bbd5459345c6bc7ebeb7
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-10-05 14:54:00 +0200

    Add test for valuespec MetricName
    CMK-11015
    
    Change-Id: Ibb64ac3f37aa51c530ebbb76745a70ee660ec8da

--- cmk/gui/plugins/metrics/utils.py
+++ cmk/gui/plugins/metrics/utils.py
@@ -30,8 +30,6 @@ from typing import (
     Union,
 )
 
-from six import ensure_str
-
 from livestatus import SiteId
 
 import cmk.utils.regex
@@ -47,7 +45,7 @@ from cmk.utils.version import parse_check_mk_version
 import cmk.gui.sites as sites
 from cmk.gui.config import active_config
 from cmk.gui.ctx_stack import g
-from cmk.gui.exceptions import MKGeneralException, MKUserError
+from cmk.gui.exceptions import MKGeneralException
 from cmk.gui.htmllib.generator import HTMLWriter
 from cmk.gui.i18n import _
 from cmk.gui.log import logger
@@ -1471,14 +1469,6 @@ class MetricName(DropdownChoiceWithHostAndServiceHints):
             **kwargs,
         }
         super().__init__(**kwargs_with_defaults)
-
-    def _validate_value(self, value: str | None, varprefix: str) -> None:
-        if (
-            value is not None
-            and self._regex
-            and not self._regex.match(ensure_str(value))  # pylint: disable= six-ensure-str-bin-call
-        ):
-            raise MKUserError(varprefix, self._regex_error)
 
     def _choices_from_value(self, value: str | None) -> Choices:
         if value is None:
---
+++ tests/unit/cmk/gui/valuespecs/test_vs_metricname.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+
+from cmk.gui.plugins.metrics.utils import MetricName
+
+from .utils import expect_validate_failure, expect_validate_success
+
+
+class TestValueSpecMetricName:
+    def test_validate(self) -> None:
+        expect_validate_failure(
+            MetricName(),
+            "99",
+            match=(
+                "Metric names must only consist of letters, digits "
+                "and underscores and they must start with a letter."
+            ),
+        )
+        expect_validate_success(MetricName(), "asd")

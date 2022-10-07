c157a0204b02cf3167b64d6d966ea219dec48d18
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2022-10-07 09:42:56 +0200

    Fix cleanup and refactor ssh calls to archive
     #rc-tagging
    
    Change-Id: Ic60be0eae3510a59f372c9e45dde74c5e22c1031

--- buildscripts/scripts/lib/upload_artifacts.groovy
+++ buildscripts/scripts/lib/upload_artifacts.groovy
@@ -99,24 +99,33 @@ def create_hashes(ARCHIVE_DIR) {
     }
 }
 
-def deploy_to_website(UPLOAD_URL, PORT, CMK_VERS) {
-    stage("Deploy to Website") {
-        withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
-            // CMK_VERS can contain a rc information like v2.1.0p6-rc1. On the website, we only want to have official
-            // releases.
-            def TARGET_VERSION = versioning.strip_rc_number_from_version(CMK_VERS)
-
-            // We also do not keep rc versions on the archive, so we're cleaning up
-            sh """
-                ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p ${PORT} ${UPLOAD_URL} \
-                    " \
-                    mv ${downloads_path}${CMK_VERS} ${downloads_path}${TARGET_VERSION}; \
-                    ln -sf ${downloads_path}${TARGET_VERSION} /smb-share-customer/checkmk/${TARGET_VERSION}; \
-                    rm -r ${downloads_path}${TARGET_VERSION}-rc*; \
-                    "
-            """
-        }
+def execute_cmd_on_archive_server(cmd) {
+    withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
+        sh """
+           ssh -o StrictHostKeyChecking=no -i ${RELEASE_KEY} -p ${WEB_DEPLOY_PORT} ${WEB_DEPLOY_URL} "${cmd}"
+        """
     }
+}
+
+def deploy_to_website(CMK_VERS) {
+    stage("Deploy to Website") {
+        // CMK_VERS can contain a rc information like v2.1.0p6-rc1.
+        // On the website, we only want to have official releases.
+        def TARGET_VERSION = versioning.strip_rc_number_from_version(CMK_VERS)
+
+        // We also do not want to keep rc versions on the archive.
+        // So rename the folder in case we have a rc
+        if (TARGET_VERSION != CMK_VERS) {
+            execute_cmd_on_archive_server("mv ${downloads_path}${CMK_VERS} ${downloads_path}${TARGET_VERSION};")
+        }
+        execute_cmd_on_archive_server("ln -sf " +
+            "${downloads_path}${TARGET_VERSION} /smb-share-customer/checkmk/${TARGET_VERSION};");
+    }
+}
+
+def cleanup_rc_candidates_of_version(CMK_VERS) {
+    def TARGET_VERSION = versioning.strip_rc_number_from_version(CMK_VERS)
+    execute_cmd_on_archive_server("rm -rf ${downloads_path}${TARGET_VERSION}-rc*;")
 }
 
 return this

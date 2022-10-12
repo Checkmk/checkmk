053283e8d9dae2312daf05e78d373cf0ecbc98e7
Frans Fürst <frans.fuerst@tribe29.com>
2022-10-12 08:00:58 +0200

    Remove deprecated .jenkins files, keeping history: 1
    Remove new .groovy files temporarily to make names available
    
    Change-Id: I4dc4db226c2890f4f5b9798a51753a908741d551

--- buildscripts/scripts/utils/docker_util.groovy
+++
@@ -1,9 +0,0 @@
-#!groovy
-
-load("${checkout_dir}/buildscripts/scripts/utils/docker_image_aliases_helper.groovy");
-
-// Make sure the docker group from outside the container is added inside of the contaienr
-get_docker_group_id = { it ->
-    cmd_output("getent group docker | cut -d: -f3");
-}
-

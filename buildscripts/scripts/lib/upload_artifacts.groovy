c24051c1c02d3cd2e453a06a27ceefd1eedec4d1
Alex Zurhake <alex.zurhake@tribe29.com>
2022-10-12 14:19:57 +0200

    14844 One hash file per package
    The HASHES file is split into one PACKAGE_NAME.hash file per package.
    
    Change-Id: I9193cbd17c43fa63a34d2d2a11aa69b430c50023

--- buildscripts/scripts/lib/upload_artifacts.groovy
+++ buildscripts/scripts/lib/upload_artifacts.groovy
@@ -87,8 +87,13 @@ def via_rsync(ARCHIVE_BASE, CMK_VERS, FILE_NAME, UPLOAD_DEST, PORT) {
 
 def create_hashes(ARCHIVE_DIR) {
     stage("Create file hashes") {
-        def HASHES_PATH = ARCHIVE_DIR + "/HASHES"
-        sh("cd ${ARCHIVE_DIR} ; sha256sum -- *.{tar.gz,rpm,deb,cma,cmk} | sort -k 2 > ${HASHES_PATH}")
+        sh("""
+            for FILE in *.{tar.gz,rpm,deb,cma,cmk}; do
+                if [ -f \${FILE} ] ; then
+                    sha256sum -- \${FILE} > \${FILE}.hash
+                fi
+            done
+        """);
     }
 }
 

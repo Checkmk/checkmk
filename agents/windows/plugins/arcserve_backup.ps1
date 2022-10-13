508755cc7caec8829b6acb83dc8857c10f3c1d9f
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2022-10-13 14:25:22 +0200

    Revert "Set version to 2.1.0p15"
    This reverts commit 876d450bae2de0b61c450b830f08ea2091dbacfd.
    
    Change-Id: I2c6845596fc7d9531cb144c0137a858671ba28c5

--- agents/windows/plugins/arcserve_backup.ps1
+++ agents/windows/plugins/arcserve_backup.ps1
@@ -1,4 +1,4 @@
-$CMK_VERSION = "2.1.0p15"
+$CMK_VERSION = "2.1.0p14"
 ####
 ## ArcServe.ps1
 ####
--- agents/windows/plugins/windows_intel_bonding.bat
+++ agents/windows/plugins/windows_intel_bonding.bat
@@ -1,5 +1,5 @@
 @echo off
-set CMK_VERSION="2.1.0p15"
+set CMK_VERSION="2.1.0p14"
 echo ^<^<^<windows_intel_bonding^>^>^>
 
 

59ee0703c395b6ba4b4ae690a39a700c6634e70b
Sven Panne <sven.panne@tribe29.com>
2022-10-13 19:34:27 +0200

    Unbreak GitHub actions, once again...
    Change-Id: Ifcca416a81e1da725f076b52509781ae2c3c73eb

--- cmk/base/core_config.py
+++ cmk/base/core_config.py
@@ -846,7 +846,7 @@ def get_host_attributes(hostname: HostName, config_cache: ConfigCache) -> Object
         attrs["_ACTIONS"] = ",".join(actions)
 
     if cmk_version.is_managed_edition():
-        attrs["_CUSTOMER"] = config.current_customer
+        attrs["_CUSTOMER"] = config.current_customer  # type: ignore[attr-defined] # needed for CRE/CEE
 
     return attrs
 

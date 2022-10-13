01fdbf246fa56f75926b724cdc69e7b1aa99a839
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-10-13 22:06:35 +0200

    temporary disable integration test for windows python module
    https://tribe29.slack.com/archives/C01EA6ZBG58/p1665682730543079
    
    Change-Id: Ie58b2023b93af080ce3c59da28888610a0c1e7fb

--- agents/modules/windows/build_the_module.cmd
+++ agents/modules/windows/build_the_module.cmd
@@ -30,7 +30,7 @@ del %tmpFile%
   call build_the_cached.cmd "%artefact_dir%" %2 %3 3.4 4 || powershell Write-Host "Failed python 1" -foreground red && exit /b 11
   call build_the_cached.cmd "%artefact_dir%" %2 %3 %python_version% || powershell Write-Host "Failed python 2" -foreground red && exit /b 12
   powershell Write-Host "Builds are successfull" -foreground green
-  make integration || powershell Write-Host "Failed integration" -foreground red && exit /b 13
+  powershell Write-Host "Integration is temporary disabled. REVERT ME ASAP" -foreground red
   goto exit
 )
 powershell Write-Host 'Invalid parameters' -Foreground Red 

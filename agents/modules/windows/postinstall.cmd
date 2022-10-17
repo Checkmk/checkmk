a454ae49416fde0e83b6f944d9eadeb1e2079dbb
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-10-17 19:00:50 +0200

    fix python module build
    latest Python venvs, starting from 3.10.4, have a different dir structure
    
    Change-Id: I9939930a6719d2f0e33f043d904d44b8d168ee6c

--- agents/modules/windows/postinstall.cmd
+++ agents/modules/windows/postinstall.cmd
@@ -9,4 +9,4 @@
 if not exist .\.venv\Scripts powershell Write-Host "`absent required folder .venv" -foreground red && exit /b 1
 if not exist .\Lib powershell Write-Host "`absent required folder lib" -foreground red && exit /b 2
 
-mklink /J DLLs .\.venv\Scripts
+:: at the moment nothing to do

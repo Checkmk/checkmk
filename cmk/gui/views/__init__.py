1faaafcadcc26ef5cf6f6fd77e40bc64d6284d0a
Lars Michelsen <lm@tribe29.com>
2022-10-25 08:36:48 +0200

    Extract more things from cmk.gui.views.__init__
    Change-Id: I9ef750334129357b89fed0c3471a642a77f2fcfe

--- cmk/gui/views/__init__.py
+++ cmk/gui/views/__init__.py
@@ -5,7 +5,7 @@
 
 import functools
 from itertools import chain
-from typing import Any, Callable, Dict, Iterable, Iterator, List, Optional, Sequence, Set
+from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Set
 from typing import Tuple as _Tuple
 from typing import Union
 
@@ -16,8 +16,8 @@ import cmk.utils.paths
 import cmk.utils.version as cmk_version
 from cmk.utils.cpu_tracking import CPUTracker, Snapshot
 from cmk.utils.site import omd_site
-from cmk.utils.structured_data import SDPath, StructuredDataNode
-from cmk.utils.type_defs import HostName, UserId
+from cmk.utils.structured_data import StructuredDataNode
+from cmk.utils.type_defs import UserId
 
 import cmk.gui.log as log
 import cmk.gui.pages
@@ -41,9 +41,7 @@ from cmk.gui.http import request
 from cmk.gui.i18n import _, _u
 from cmk.gui.inventory import (
     get_short_inventory_filepath,
-    get_status_data_via_livestatus,
     load_filtered_and_merged_tree,
-    load_latest_delta_tree,
     LoadStructuredDataError,
 )
 from cmk.gui.logged_in import user
@@ -63,11 +61,8 @@ from cmk.gui.plugins.views.icons.utils import (
 )
 from cmk.gui.plugins.views.utils import (
     Cell,
-    get_tag_groups,
     JoinCell,
     load_used_options,
-    Painter,
-    painter_registry,
     register_legacy_command,
     register_painter,
 )
@@ -75,12 +70,10 @@ from cmk.gui.plugins.visuals.utils import (
     Filter,
     get_livestatus_filter_headers,
     visual_type_registry,
-    VisualType,
 )
 from cmk.gui.sorter import register_sorter, SorterEntry
 from cmk.gui.type_defs import (
     ColumnName,
-    HTTPVariables,
     Perfdata,
     PerfometerSpec,
     Row,
@@ -91,12 +84,12 @@ from cmk.gui.type_defs import (
 )
 from cmk.gui.utils.urls import makeuri_contextless
 from cmk.gui.utils.user_errors import user_errors
-from cmk.gui.valuespec import Hostname
 from cmk.gui.view import View
 from cmk.gui.view_renderer import ABCViewRenderer, GUIViewRenderer
 from cmk.gui.view_store import get_all_views, get_permitted_views, multisite_builtin_views
 from cmk.gui.view_utils import get_labels, render_labels, render_tag_groups
 from cmk.gui.views.builtin_views import builtin_views
+from cmk.gui.views.host_tag_plugins import register_tag_plugins
 from cmk.gui.views.inventory import register_table_views_and_columns, update_paint_functions
 from cmk.gui.views.page_ajax_filters import AjaxInitialViewFilters
 from cmk.gui.views.page_ajax_popup_action_menu import ajax_popup_action_menu
@@ -108,6 +101,7 @@ from cmk.gui.views.page_edit_view import (
     PageAjaxCascadingRenderPainterParameters,
 )
 from cmk.gui.views.page_edit_views import page_edit_views
+from cmk.gui.views.visual_type import VisualTypeViews
 
 from . import availability
 
@@ -131,136 +125,8 @@ page_registry.register_page("ajax_cascading_render_painer_parameters")(
 )
 page_registry.register_page("ajax_reschedule")(PageRescheduleCheck)
 page_registry.register_page("ajax_initial_view_filters")(AjaxInitialViewFilters)
-
-
-@visual_type_registry.register
-class VisualTypeViews(VisualType):
-    """Register the views as a visual type"""
-
-    @property
-    def ident(self) -> str:
-        return "views"
-
-    @property
-    def title(self) -> str:
-        return _("view")
-
-    @property
-    def plural_title(self):
-        return _("views")
-
-    @property
-    def ident_attr(self):
-        return "view_name"
-
-    @property
-    def multicontext_links(self):
-        return False
-
-    @property
-    def show_url(self):
-        return "view.py"
-
-    def page_menu_add_to_entries(self, add_type: str) -> Iterator[PageMenuEntry]:
-        return iter(())
-
-    def add_visual_handler(self, target_visual_name, add_type, context, parameters):
-        return None
-
-    def load_handler(self):
-        pass
-
-    @property
-    def permitted_visuals(self):
-        return get_permitted_views()
-
-    def link_from(  # type:ignore[no-untyped-def]
-        self, linking_view, linking_view_rows, visual, context_vars: HTTPVariables
-    ) -> bool:
-        """This has been implemented for HW/SW inventory views which are often useless when a host
-        has no such information available. For example the "Oracle Tablespaces" inventory view is
-        useless on hosts that don't host Oracle databases."""
-        result = super().link_from(linking_view, linking_view_rows, visual, context_vars)
-        if result is False:
-            return False
-
-        link_from = visual["link_from"]
-        if not link_from:
-            return True  # No link from filtering: Always display this.
-
-        context = dict(context_vars)
-        if (hostname := context.get("host")) is None:
-            # No host data? Keep old behaviour
-            return True
-
-        if hostname == "":
-            return False
-
-        # TODO: host is not correctly validated by visuals. Do it here for the moment.
-        try:
-            Hostname().validate_value(hostname, None)
-        except MKUserError:
-            return False
-
-        if not (site_id := context.get("site")):
-            return False
-
-        return _has_inventory_tree(
-            HostName(hostname),
-            SiteId(str(site_id)),
-            link_from.get("has_inventory_tree", []),
-            is_history=False,
-        ) or _has_inventory_tree(
-            HostName(hostname),
-            SiteId(str(site_id)),
-            link_from.get("has_inventory_tree_history", []),
-            is_history=True,
-        )
-
-
-def _has_inventory_tree(
-    hostname: HostName,
-    site_id: SiteId,
-    paths: Sequence[SDPath],
-    is_history: bool,
-) -> bool:
-    if not paths:
-        return False
-
-    # FIXME In order to decide whether this view is enabled
-    # do we really need to load the whole tree?
-    try:
-        struct_tree = _get_struct_tree(is_history, hostname, site_id)
-    except LoadStructuredDataError:
-        return False
-
-    if not struct_tree:
-        return False
-
-    if struct_tree.is_empty():
-        return False
-
-    return any(
-        (node := struct_tree.get_node(path)) is not None and not node.is_empty() for path in paths
-    )
-
-
-def _get_struct_tree(
-    is_history: bool, hostname: HostName, site_id: SiteId
-) -> Optional[StructuredDataNode]:
-    struct_tree_cache = g.setdefault("struct_tree_cache", {})
-    cache_id = (is_history, hostname, site_id)
-    if cache_id in struct_tree_cache:
-        return struct_tree_cache[cache_id]
-
-    if is_history:
-        struct_tree = load_latest_delta_tree(hostname)
-    else:
-        row = get_status_data_via_livestatus(site_id, hostname)
-        struct_tree = load_filtered_and_merged_tree(row)
-
-    struct_tree_cache[cache_id] = struct_tree
-    return struct_tree
+visual_type_registry.register(VisualTypeViews)
+register_post_config_load_hook(register_tag_plugins)
 
 
 @permission_section_registry.register
@@ -494,94 +360,6 @@ def transform_old_dict_based_icons():
         )
 
         icon_and_action_registry.register(icon_class)
-
-
-def _register_tag_plugins():
-    if getattr(_register_tag_plugins, "_config_hash", None) == _calc_config_hash():
-        return  # No re-register needed :-)
-    _register_host_tag_painters()
-    _register_host_tag_sorters()
-    setattr(_register_tag_plugins, "_config_hash", _calc_config_hash())
-
-
-def _calc_config_hash() -> int:
-    return hash(repr(active_config.tags.get_dict_format()))
-
-
-register_post_config_load_hook(_register_tag_plugins)
-
-
-def _register_host_tag_painters():
-    # first remove all old painters to reflect delted painters during runtime
-    for key in list(painter_registry.keys()):
-        if key.startswith("host_tag_"):
-            painter_registry.unregister(key)
-
-    for tag_group in active_config.tags.tag_groups:
-        if tag_group.topic:
-            long_title = tag_group.topic + " / " + tag_group.title
-        else:
-            long_title = tag_group.title
-
-        ident = "host_tag_" + tag_group.id
-        spec = {
-            "title": _("Host tag:") + " " + long_title,
-            "short": tag_group.title,
-            "columns": ["host_tags"],
-        }
-        cls = type(
-            "HostTagPainter%s" % str(tag_group.id).title(),
-            (Painter,),
-            {
-                "_ident": ident,
-                "_spec": spec,
-                "_tag_group_id": tag_group.id,
-                "ident": property(lambda self: self._ident),
-                "title": lambda self, cell: self._spec["title"],
-                "short_title": lambda self, cell: self._spec["short"],
-                "columns": property(lambda self: self._spec["columns"]),
-                "render": lambda self, row, cell: _paint_host_tag(row, self._tag_group_id),
-                # Use title of the tag value for grouping, not the complete
-                # dictionary of custom variables!
-                "group_by": lambda self, row, _cell: _paint_host_tag(row, self._tag_group_id)[1],
-            },
-        )
-        painter_registry.register(cls)
-
-
-def _paint_host_tag(row, tgid):
-    return "", _get_tag_group_value(row, "host", tgid)
-
-
-def _register_host_tag_sorters():
-    for tag_group in active_config.tags.tag_groups:
-        register_sorter(
-            "host_tag_" + str(tag_group.id),
-            {
-                "_tag_group_id": tag_group.id,
-                "title": _("Host tag:") + " " + tag_group.title,
-                "columns": ["host_tags"],
-                "cmp": lambda self, r1, r2: _cmp_host_tag(r1, r2, self._spec["_tag_group_id"]),
-            },
-        )
-
-
-def _cmp_host_tag(r1, r2, tgid):
-    host_tag_1 = _get_tag_group_value(r1, "host", tgid)
-    host_tag_2 = _get_tag_group_value(r2, "host", tgid)
-    return (host_tag_1 > host_tag_2) - (host_tag_1 < host_tag_2)
-
-
-def _get_tag_group_value(row, what, tag_group_id) -> str:  # type:ignore[no-untyped-def]
-    tag_id = get_tag_groups(row, "host").get(tag_group_id)
-
-    tag_group = active_config.tags.get_tag_group(tag_group_id)
-    if tag_group:
-        label = dict(tag_group.get_tag_choices()).get(tag_id, _("N/A"))
-    else:
-        label = tag_id or _("N/A")
-
-    return label or _("N/A")
 
 
 # .

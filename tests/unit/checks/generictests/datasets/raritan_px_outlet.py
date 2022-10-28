bc539a1b0a02f681edc61d7a62cbeea6cab22c93
Jan-Philipp Litza <jpl@plutex.de>
2022-10-28 12:37:27 +0200

    15012 Add outlet label for raritan px outlet check
    We now add the outlet label in the "raritan_px_outlets" check.
    
    Change-Id: I2f3bcf06121bb4c3c2298c8fbaba725626b063be
    closes: #531

--- tests/unit/checks/generictests/datasets/raritan_px_outlet.py
+++ tests/unit/checks/generictests/datasets/raritan_px_outlet.py
@@ -9,17 +9,30 @@
 
 checkname = 'raritan_px_outlets'
 
-info = [[
-    "3",
-    "1",
-    "3",
-    "3",
-    "3",
-    "3",
-    "3",
-]]
+info = [
+        [
+            "3",
+            "label",
+            "1",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+        ],
+        [
+            "2",
+            "",
+            "1",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+        ],
+]
 
-discovery = {'': [("3", {})]}
+discovery = {'': [("3", {}), ("2", {})]}
 
 checks = {
     '': [
@@ -27,13 +40,26 @@ checks = {
             '3',
             {},
             [
-              (0, "Operational status: on", []),
+              (0, "[label]", []),
+              (0, "Device status: on(0)", []),
               (0, "Voltage: 0.0 V", [("voltage", 0.003)]),
               (0, "Current: 0.0 A", [("current", 0.003)]),
               (0, "Power: 3.0 W", [("power", 3.0)]),
               (0, "Apparent Power: 3.0 VA", [("appower", 3.0)]),
               (0, "Energy: 3.0 Wh", [("energy", 3.0)]),
             ]
-        )
+        ),
+        (
+            '2',
+            {},
+            [
+              (0, "Device status: on(0)", []),
+              (0, "Voltage: 0.0 V", [("voltage", 0.003)]),
+              (0, "Current: 0.0 A", [("current", 0.003)]),
+              (0, "Power: 3.0 W", [("power", 3.0)]),
+              (0, "Apparent Power: 3.0 VA", [("appower", 3.0)]),
+              (0, "Energy: 3.0 Wh", [("energy", 3.0)]),
+            ]
+        ),
     ],
 }

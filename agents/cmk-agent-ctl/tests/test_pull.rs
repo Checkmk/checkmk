1606b555e1200eee62c021f175b2c7f5b84321b1
Joerg Herbel <joerg.herbel@tribe29.com>
2022-10-28 12:11:24 +0200

    Agent controller: Rework pull integration tests
    Call the controller command line instead of manually starting pull
    threads.
    
    Change-Id: Ib07f1fd5d280b7f8dbf4ab3c15f2ee6dbab4101b

--- agents/cmk-agent-ctl/tests/test_pull.rs
+++ agents/cmk-agent-ctl/tests/test_pull.rs
@@ -7,13 +7,14 @@
 #![allow(dead_code)]
 mod common;
 use self::certs::X509Certs;
-use anyhow::Result as AnyhowResult;
-use cmk_agent_ctl::{certs as lib_certs, configuration::config, site_spec, types};
+use anyhow::{Context, Result as AnyhowResult};
+use cmk_agent_ctl::{certs as lib_certs, configuration::config, site_spec};
 use common::certs;
-use std::io::{Read, Write};
+use std::io::{Read, Result as IoResult, Write};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
+use tokio::process::{Child, Command};
 
 fn registry(
     path: &Path,
@@ -37,24 +38,15 @@ fn registry(
     registry
 }
 
-fn pull_config(
-    port: u16,
-    agent_channel: types::AgentChannel,
-    registry: config::Registry,
-) -> config::PullConfig {
-    config::PullConfig {
-        allowed_ip: vec![],
-        port,
-        max_connections: 3,
-        connection_timeout: 1,
-        agent_channel,
-        registry,
-    }
-}
-
 #[test]
 fn test_pull_inconsistent_cert() -> AnyhowResult<()> {
-    let test_dir = common::setup_test_dir("cmk_agent_ctl_test_pull");
+    #[cfg(windows)]
+    if !is_elevated::is_elevated() {
+        println!("Test is skipped, must be in elevated mode");
+        return Ok(());
+    }
+
+    let test_dir = common::setup_test_dir("cmk_agent_ctl_test_pull_inconsistent_cert");
     let test_path = test_dir.path();
 
     let controller_uuid = uuid::Uuid::new_v4();
@@ -65,13 +57,20 @@ fn test_pull_inconsistent_cert() -> AnyhowResult<()> {
         &x509_certs,
         controller_uuid,
     );
+    registry.save()?;
 
-    let error =
-        cmk_agent_ctl::modes::pull::pull(pull_config(1234, "dummy".into(), registry)).unwrap_err();
+    let output_err = common::controller_command()
+        .env("DEBUG_HOME_DIR", test_path)
+        .arg("pull")
+        .timeout(std::time::Duration::from_secs(5))
+        .unwrap_err();
+    let stderr = std::str::from_utf8(&output_err.as_output().unwrap().stderr)?;
+    assert!(stderr.contains("Could not initialize TLS"));
+    assert!(stderr.contains("The server certificate is not valid for the given name"));
 
-    assert!(error.to_string().contains("Could not initialize TLS"));
-
-    Ok(())
+    test_dir
+        .close()
+        .context("Failed to remove temporary test directory")
 }
 
 fn tls_client_connection(certs: X509Certs, address: &str) -> rustls::ClientConnection {
@@ -105,64 +104,57 @@ struct PullFixture {
     uuid: String,
     certs: common::certs::X509Certs,
     agent_stream_thread: tokio::task::JoinHandle<std::result::Result<(), anyhow::Error>>,
-    pull_thread: tokio::task::JoinHandle<std::result::Result<(), anyhow::Error>>,
+    pull_child_proc: Child,
     test_dir: tempfile::TempDir,
 }
 
 impl PullFixture {
-    fn setup(port: u16, prefix: &str, save_legacy: bool) -> AnyhowResult<PullFixture> {
+    fn setup(prefix: &str, port: &u16) -> AnyhowResult<Self> {
         // Uncomment for debugging
         // common::init_logging(&test_path.join("log"))?;
         let test_dir = common::setup_test_dir(prefix);
         let test_path = test_dir.path();
-        std::env::set_var("DEBUG_HOME_DIR", test_path.to_str().unwrap());
-        let test_port = port;
         let test_agent_output = "some test agent output";
         #[cfg(unix)]
         let agent_socket_address = common::setup_agent_socket_path(test_path);
         #[cfg(windows)]
         let agent_socket_address = "localhost:7999".to_string();
-        let (uuid, pull_config, certs) =
-            Self::pull_setup(test_path, test_port, agent_socket_address.as_str().into());
-        if save_legacy {
-            pull_config.registry.save()?;
-        }
+        let (uuid, certs) = Self::setup_registry(test_path)?;
         let agent_stream_thread = tokio::spawn(common::agent::agent_response_loop(
             agent_socket_address,
             test_agent_output.to_string(),
         ));
-        // Setup the pull thread that we intend to test
-        let pull_thread = tokio::task::spawn(cmk_agent_ctl::modes::pull::async_pull(pull_config));
 
-        Ok(PullFixture {
+        Ok(Self {
             test_agent_output: test_agent_output.to_string(),
             uuid,
             certs,
             agent_stream_thread,
-            pull_thread,
+            pull_child_proc: Self::start_pull_process(test_path, port)?,
             test_dir,
         })
     }
 
-    fn pull_setup(
-        path: &Path,
-        port: u16,
-        agent_channel: types::AgentChannel,
-    ) -> (String, config::PullConfig, certs::X509Certs) {
+    fn setup_registry(path: &Path) -> AnyhowResult<(String, certs::X509Certs)> {
         let controller_uuid = uuid::Uuid::new_v4();
         let x509_certs =
             certs::X509Certs::new("Test CA", "Test receiver", &controller_uuid.to_string());
-        let registry = registry(
+        registry(
             &path.join("registered_connections.json"),
             &x509_certs,
             controller_uuid,
-        );
-
-        (
-            controller_uuid.to_string(),
-            pull_config(port, agent_channel, registry),
-            x509_certs,
         )
+        .save()?;
+
+        Ok((controller_uuid.to_string(), x509_certs))
+    }
+
+    fn start_pull_process(path_home_dir: &Path, port: &u16) -> IoResult<Child> {
+        Command::new(assert_cmd::cargo::cargo_bin("cmk-agent-ctl"))
+            .env("DEBUG_HOME_DIR", path_home_dir)
+            .env("DEBUG_CONNECTION_TIMEOUT", "1")
+            .args(["pull", "--port", &port.to_string()])
+            .spawn()
     }
 
     fn compressed_agent_output(&self) -> AnyhowResult<Vec<u8>> {
@@ -174,9 +166,9 @@ impl PullFixture {
         Ok(compressed_agent_output)
     }
 
-    fn teardown(self) {
+    async fn teardown(mut self) {
         self.agent_stream_thread.abort();
-        self.pull_thread.abort();
+        self.pull_child_proc.kill().await.unwrap();
         self.test_dir.close().unwrap();
     }
 
@@ -192,13 +184,13 @@ impl PullFixture {
         std::fs::remove_file(self.path_legacy_pull_marker()).unwrap()
     }
 
-    fn path_legacy_pull_marker(&self) -> std::path::PathBuf {
+    fn path_legacy_pull_marker(&self) -> PathBuf {
         self.test_dir.path().join("allow-legacy-pull")
     }
 }
 
-async fn _test_pull_tls_main(socket_addr: SocketAddr) -> AnyhowResult<()> {
-    let fixture: PullFixture = PullFixture::setup(socket_addr.port(), "test_pull_tls_main", false)?;
+async fn _test_pull_tls_main(prefix: &str, socket_addr: SocketAddr) -> AnyhowResult<()> {
+    let fixture: PullFixture = PullFixture::setup(prefix, &socket_addr.port())?;
     // Give it some time to provide the TCP socket
     tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
 
@@ -223,7 +215,7 @@ async fn _test_pull_tls_main(socket_addr: SocketAddr) -> AnyhowResult<()> {
     let mut tls_stream = rustls::Stream::new(&mut client_connection, &mut tcp_stream);
     assert!(tls_stream.read_to_end(&mut message_buf).is_err());
 
-    fixture.teardown();
+    fixture.teardown().await;
 
     Ok(())
 }
@@ -232,19 +224,26 @@ async fn _test_pull_tls_main(socket_addr: SocketAddr) -> AnyhowResult<()> {
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(target_os = "windows", ignore)]
 async fn test_pull_tls_main_ipv4() -> AnyhowResult<()> {
-    _test_pull_tls_main(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9970)).await
+    _test_pull_tls_main(
+        "test_pull_tls_main_ipv4",
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9970),
+    )
+    .await
 }
 
 // TODO(sk): Fix this test
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(target_os = "windows", ignore)]
 async fn test_pull_tls_main_ipv6() -> AnyhowResult<()> {
-    _test_pull_tls_main(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 9971)).await
+    _test_pull_tls_main(
+        "test_pull_tls_main_ipv6",
+        SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 9971),
+    )
+    .await
 }
 
-async fn _test_pull_tls_check_guards(socket_addr: SocketAddr) -> AnyhowResult<()> {
-    let fixture: PullFixture =
-        PullFixture::setup(socket_addr.port(), "test_pull_tls_check_guards", false)?;
+async fn _test_pull_tls_check_guards(prefix: &str, socket_addr: SocketAddr) -> AnyhowResult<()> {
+    let fixture: PullFixture = PullFixture::setup(prefix, &socket_addr.port())?;
 
     // Give it some time to provide the TCP socket
     tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
@@ -263,23 +262,31 @@ async fn _test_pull_tls_check_guards(socket_addr: SocketAddr) -> AnyhowResult<()
     assert!(tcp_stream_3.read_exact(&mut id_buf).is_ok());
     assert!(tcp_stream_4.read_exact(&mut id_buf).is_err());
 
-    fixture.teardown();
+    fixture.teardown().await;
     Ok(())
 }
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_pull_tls_check_guards_ipv4() -> AnyhowResult<()> {
-    _test_pull_tls_check_guards(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9980)).await
+    _test_pull_tls_check_guards(
+        "test_pull_tls_check_guards_ipv4",
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9980),
+    )
+    .await
 }
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_pull_tls_check_guards_ipv6() -> AnyhowResult<()> {
-    _test_pull_tls_check_guards(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 9981)).await
+    _test_pull_tls_check_guards(
+        "test_pull_tls_check_guards_ipv6",
+        SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 9981),
+    )
+    .await
 }
 
 #[cfg(unix)]
-async fn _test_pull_legacy(socket_addr: SocketAddr) -> AnyhowResult<()> {
-    let fixture: PullFixture = PullFixture::setup(socket_addr.port(), "test_pull_legacy", true)?;
+async fn _test_pull_legacy(prefix: &str, socket_addr: SocketAddr) -> AnyhowResult<()> {
+    let fixture: PullFixture = PullFixture::setup(prefix, &socket_addr.port())?;
     // Give it some time to provide the TCP socket.
     tokio::time::sleep(tokio::time::Duration::from_millis(1500)).await;
 
@@ -314,18 +321,26 @@ async fn _test_pull_legacy(socket_addr: SocketAddr) -> AnyhowResult<()> {
     assert_eq!(message_buf, b"");
     assert!(std::net::TcpStream::connect(socket_addr).is_err());
 
-    fixture.teardown();
+    fixture.teardown().await;
     Ok(())
 }
 
 #[cfg(unix)]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_pull_legacy_ipv4() -> AnyhowResult<()> {
-    _test_pull_legacy(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9990)).await
+    _test_pull_legacy(
+        "test_pull_legacy_ipv4",
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9990),
+    )
+    .await
 }
 
 #[cfg(unix)]
 #[tokio::test(flavor = "multi_thread")]
 async fn test_pull_legacy_ipv6() -> AnyhowResult<()> {
-    _test_pull_legacy(SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 9991)).await
+    _test_pull_legacy(
+        "test_pull_legacy_ipv6",
+        SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 9991),
+    )
+    .await
 }

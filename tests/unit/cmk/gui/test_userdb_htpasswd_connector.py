5003fec0359488b6ff794d9acddef3f449749303
Hannes Rantzsch <hannes.rantzsch@tribe29.com>
2022-10-31 16:58:17 +0100

    14390 Automatically update deprectated password hashes
    Deprecated hashes of user passwords stored in the htpasswd file will now
    be automatically updated to a more modern hash format when the
    respective user logs in. Specifically, password hashes created with the
    sha256-crypt algorithm will be udpated to bcrypt hashes.
    
    sha256-crypt hashes are still considered secure for password hashing.
    However, we want to migrate all users' password hashes to the more
    modern bcrypt algorithm. For users whose passwords are hashed with
    sha256-crypt we can do so automatically in the background when they
    authenticate successfully.
    
    Older and less secure password hashes, such as MD5, are not updated
    automatically.
    
    CMK-11528
    
    Change-Id: I53f65fc539a10bef38aba0a677fbfc8c3b07420e

--- tests/unit/cmk/gui/test_userdb_htpasswd_connector.py
+++ tests/unit/cmk/gui/test_userdb_htpasswd_connector.py
@@ -17,30 +17,29 @@ import cmk.gui.plugins.userdb.htpasswd as htpasswd
 from cmk.gui.exceptions import MKUserError
 
 
-@pytest.fixture(name="htpasswd_file")
-def htpasswd_file_fixture(tmp_path: Path) -> Path:
+@pytest.fixture(name="htpasswd_file", autouse=True)
+def htpasswd_file_fixture(tmp_path: Path, monkeypatch: MonkeyPatch) -> Path:
     htpasswd_file_path = tmp_path / "htpasswd"
-    htpasswd_file_path.write_text(
-        "\n".join(
-            sorted(
-                [
-                    # Pre 1.6 hashing formats (see cmk.gui.plugins.userdb.htpasswd for more details)
-                    "bärnd:$apr1$/FU.SwEZ$Ye0XG1Huf2j7Jws7KD.h2/",
-                    "cmkadmin:NEr3kqi287FQc",
-                    "harry:$1$478020$ldQUQ3RIwRYk5wjKfsWPD.",
-                    # A disabled user
-                    "locked:!NEr3kqi287FQc",
-                    # A >= 1.6 sha256 hashed password
-                    "sha256user:$5$rounds=535000$5IFtH0zYpQ6STBre$Nkem2taHfBFswWj3xERRpmEI.20G5is0VBcPpUuf3J2",
-                    # A >= 2.1 bcrypt hashed password
-                    "bcrypt_user:$2b$12$3xoc9iu.EiyGVVPlDMC21esAiZqef9e6sogmM4UCi4s8qSvmvJWVC",
-                ]
-            )
-        )
-        + "\n",
-        encoding="utf-8",
-    )
-    return Path(htpasswd_file_path)
+    # HtpasswdUserConnector will use this path:
+    monkeypatch.setattr("cmk.utils.paths.htpasswd_file", htpasswd_file_path)
+
+    # all hashes below belong to the password "cmk"
+    hashes = [
+        # Pre 1.6 hashing formats (see cmk.gui.plugins.userdb.htpasswd for more details)
+        "bärnd:$apr1$/FU.SwEZ$Ye0XG1Huf2j7Jws7KD.h2/",
+        "cmkadmin:NEr3kqi287FQc",
+        "harry:$1$478020$ldQUQ3RIwRYk5wjKfsWPD.",
+        # A disabled user
+        "locked:!NEr3kqi287FQc",
+        # A >= 1.6 sha256 hashed password
+        "sha256user:$5$rounds=1000$.L//WfAGgL3rOSs3$QXLgMhQIDaL2oDagb7kLd.jRbyKLG9wsikCfzAq/w01",
+        "locked_sha256user:!$5$rounds=1000$.L//WfAGgL3rOSs3$QXLgMhQIDaL2oDagb7kLd.jRbyKLG9wsikCfzAq/w01",
+        # A >= 2.1 bcrypt hashed password
+        "bcrypt_user:$2b$04$IJJ8O2HLU5KEZL2ZbybonODhQ/0TPPgARwDhib74KFU5uRvyUupcO",
+    ]
+    htpasswd_file_path.write_text("\n".join(sorted(hashes)) + "\n", encoding="utf-8")
+
+    return htpasswd_file_path
 
 
 @pytest.mark.parametrize(
@@ -75,15 +74,61 @@ def test_truncation_error() -> None:
             htpasswd.hash_password("😀" * 19)
 
 
-def test_user_connector_verify_password(htpasswd_file: Path, monkeypatch: MonkeyPatch) -> None:
-    c = htpasswd.HtpasswdUserConnector({})
-    mock_htpwd = htpasswd.Htpasswd(htpasswd_file)
-    monkeypatch.setattr(c, "_htpasswd", mock_htpwd)
+@pytest.mark.parametrize(
+    "user,expect_update",
+    [
+        ("bärnd", False),
+        ("cmkadmin", False),
+        ("harry", False),
+        ("locked", False),
+        ("sha256user", True),
+        ("locked_sha256user", False),
+        ("bcrypt_user", False),
+    ],
+)
+def test_update_hashes(htpasswd_file: Path, user: UserId, expect_update: bool) -> None:
+    password = "cmk"
+    htpasswd_connector = htpasswd.HtpasswdUserConnector({})
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        htpasswd_connector.check_credentials(user, password)
 
-    assert c.check_credentials(UserId("cmkadmin"), "cmk") == "cmkadmin"
-    assert c.check_credentials(UserId("bärnd"), "cmk") == "bärnd"
-    assert c.check_credentials(UserId("sha256user"), "cmk") == "sha256user"
-    assert c.check_credentials(UserId("harry"), "cmk") == "harry"
-    assert c.check_credentials(UserId("bcrypt_user"), "cmk") == "bcrypt_user"
-    assert c.check_credentials(UserId("dingeling"), "aaa") is None
-    assert c.check_credentials(UserId("locked"), "locked") is False
+    # Note: bcrypt $2b$ hashes are not "updated" to $2y$
+    assert (
+        f"{user}:$2y$" in htpasswd_file.read_text()
+    ) == expect_update, "Only sha256-crypt hashes are updated"
+
+    if not "locked" in user:
+        assert (
+            htpasswd_connector.check_credentials(user, password) == user
+        ), "Password is still valid"
+
+
+def test_update_long_password(htpasswd_file: Path) -> None:
+    """This tests that passwords that were valid with sha256crypt but are not valid with bcrypt
+    (specifically passwords longer than 72 bytes), are handled properly during hash migration.
+    """
+    htpasswd_connector = htpasswd.HtpasswdUserConnector({})
+    usr = UserId("longcat")
+    pw = 74 * "x"  # too long for bcrypt
+    pw_hash = "$5$rounds=1000$FwEKt/q2WUEYYjOm$EhgODZbqGIl8LcdDtGYYjfFLECubBN.xNSavUiP5.UB"
+    htpasswd_file.write_text(f"{usr}:{pw_hash}\n")
+
+    assert htpasswd_connector.check_credentials(usr, pw) == usr, "Long passwords still work"
+    assert (
+        htpasswd_connector.check_credentials(usr, pw[:72]) == usr
+    ), "The password is now truncated"
+
+
+def test_user_connector_verify_password() -> None:
+    htpasswd_connector = htpasswd.HtpasswdUserConnector({})
+    assert htpasswd_connector.check_credentials(UserId("cmkadmin"), "cmk") == "cmkadmin"
+    assert htpasswd_connector.check_credentials(UserId("bärnd"), "cmk") == "bärnd"
+    assert htpasswd_connector.check_credentials(UserId("sha256user"), "cmk") == "sha256user"
+    assert htpasswd_connector.check_credentials(UserId("harry"), "cmk") == "harry"
+    assert htpasswd_connector.check_credentials(UserId("bcrypt_user"), "cmk") == "bcrypt_user"
+    assert htpasswd_connector.check_credentials(UserId("dingeling"), "aaa") is None
+    assert htpasswd_connector.check_credentials(UserId("locked"), "locked") is False
+
+    # Check no exception is raised, when setting a password > 72 chars a exception is raised...
+    assert htpasswd_connector.check_credentials(UserId("bcrypt_user"), "A" * 100) is False

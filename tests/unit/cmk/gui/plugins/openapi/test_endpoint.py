bfc4b0ed9e6c298a15826b9271f4578989d5d2b1
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-11-02 14:40:29 +0100

    Also check exceptions when checking status codes
    Before this change only responses where checked for status code
    mismatches. Now also ProblemException are considered.
    
    Change-Id: Ie8e0234537d60b2b2897e123fef3d11959867f53

--- tests/unit/cmk/gui/plugins/openapi/test_endpoint.py
+++ tests/unit/cmk/gui/plugins/openapi/test_endpoint.py
@@ -19,6 +19,7 @@ from cmk.gui.globals import request_local_attr
 from cmk.gui.http import Response
 from cmk.gui.plugins.openapi.restful_objects.decorators import Endpoint, WrappedEndpoint
 from cmk.gui.plugins.openapi.restful_objects.endpoint_registry import ENDPOINT_REGISTRY
+from cmk.gui.plugins.openapi.utils import ProblemException
 from cmk.gui.utils.script_helpers import session_wsgi_app
 
 from cmk import fields
@@ -130,3 +131,48 @@ def test_openapi_endpoint_decorator_resets_used_permissions(
     # and expect only "two" in the collection, because "one" was requested in
     # another call. before the fix, both "one" and "two" were in this set.
     assert test_endpoint.endpoint._used_permissions == set(["two"])
+
+
+@pytest.fixture(name="test_endpoint_raise_status_code")
+def install_endpoint_raise():
+    session_wsgi_app.cache_clear()
+
+    @Endpoint(
+        path="/raise_exception",
+        method="get",
+        link_relation="help",
+        output_empty=True,
+        tag_group="Monitoring",
+        update_config_generation=False,
+        skip_locking=True,
+    )
+    def test(param: Mapping[str, Any]) -> Response:
+        """Smth"""
+        raise ProblemException(418, "short", "long")
+
+    yield test
+
+    ENDPOINT_REGISTRY.remove_endpoint(test.endpoint)
+
+
+def test_openapi_endpoint_decorator_catches_status_code_exceptions(
+    test_endpoint_raise_status_code: WrappedEndpoint, aut_user_auth_wsgi_app: WebTestAppForCMK
+) -> None:
+    """
+    before this test, the Endpoint did not check for exceptions that change the
+    status code, but only for responses that return the wrong status code.
+    """
+
+    response = aut_user_auth_wsgi_app.call_method(
+        "get",
+        "/NO_SITE/check_mk/api/1.0/raise_exception",
+        "",
+        {"Accept": "application/json"},  # headers
+        status=500,
+    )
+    assert json.loads(response.text) == {
+        "title": "Unexpected status code returned: 418",
+        "status": 500,
+        "detail": "Endpoint tests.unit.cmk.gui.plugins.openapi.test_endpoint.test\nThis is a bug, please report.",
+        "ext": {"codes": [204]},
+    }

a94c2bcdaa180f64f0740c36b09612d681619e3c
Sven Rueß <github@sven-ruess.de>
2022-11-02 10:18:35 +0100

    Fix code formatting
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- cmk/gui/plugins/wato/check_parameters/heartbeat_rscstatus.py
+++ cmk/gui/plugins/wato/check_parameters/heartbeat_rscstatus.py
@@ -38,7 +38,9 @@ def _parameter_valuespec_heartbeat_rscstatus():
                 ),
             ),
         ],
-        ignored_keys=["discovered_state",],
+        ignored_keys=[
+            "discovered_state",
+        ],
     )
 
 

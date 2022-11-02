9b5877d5e86eaf499b51d36b78c0e26c5a009f7b
Lisa Pichler <lisa.pichler@tribe29.com>
2022-11-02 08:50:45 +0100

    14770 FIX mssql_instance: check shows UNKNOWN when the instance is not accessible
    SUP-12122
    
    Change-Id: I9e531594d859f1dca1994d82ecf9467364feae03

---
+++ tests/unit/checks/test_mssql_instance.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from testlib import Check  # type: ignore[import]
+
+
+@pytest.fixture
+def check_plugin() -> Check:
+    return Check("mssql_instance")
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_mssql_instance_vanished(check_plugin: Check) -> None:
+    assert list(check_plugin.run_check("MSSQL instance", {}, {})) == [
+        (2, "Database or necessary processes not running or login failed"),
+    ]

7b13056f22cb4b09a1d87b031ec0880b2ec61a9f
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-11-08 09:08:00 +0100

    remove old build script
    Change-Id: I2c96408231a8e1eccbea6f371abaf13c34756b92

--- agents/wnx/install/msbld.cmd
+++
@@ -1,5 +0,0 @@
-@echo off 
-set msbuild=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msBuild.exe
-if not exist "%msbuild%" powershell write-host -fore Red  Error: Install Visual Studio 2017 && exit
-"%msbuild%" check_mk_vanilla.wixproj
-"%msbuild%" check_mk_baked.wixproj

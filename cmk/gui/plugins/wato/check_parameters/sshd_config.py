922505ec1a99e173c4425ae058994078972731c2
Joerg Herbel <joerg.herbel@tribe29.com>
2022-11-08 18:15:38 +0100

    14827 FIX Rework agent plugin for monitoring SSH daemon configuration
    SUP-11861
    
    Change-Id: If26f5383d6cf0eefaf02d340f277ebc5815821ff

--- cmk/gui/plugins/wato/check_parameters/sshd_config.py
+++ cmk/gui/plugins/wato/check_parameters/sshd_config.py
@@ -31,107 +31,110 @@ def transform_ssh_config(choice):
     return choice
 
 
-def _parameter_valuespec_sshd_config():
-    return Dictionary(
-        elements=[
-            (
-                "PermitRootLogin",
-                Transform(
+def _parameter_valuespec_sshd_config() -> Transform:
+    return Transform(
+        forth=lambda p: {k.lower(): v for k, v in p.items()},
+        valuespec=Dictionary(
+            elements=[
+                (
+                    "permitrootlogin",
+                    Transform(
+                        DropdownChoice(
+                            title=_("Permit root login"),
+                            choices=[
+                                ("yes", _("yes")),
+                                ("key-based", _("without-password/prohibit-password (Key based)")),
+                                ("forced-commands-only", _("forced-commands-only")),
+                                ("no", _("no")),
+                            ],
+                            default_value="key-based",
+                        ),
+                        forth=transform_ssh_config,
+                    ),
+                ),
+                (
+                    "protocol",
                     DropdownChoice(
-                        title=_("Permit root login"),
+                        title=_("Allowed protocols"),
                         choices=[
-                            ("yes", _("yes")),
-                            ("key-based", _("without-password/prohibit-password (Key based)")),
-                            ("forced-commands-only", _("forced-commands-only")),
-                            ("no", _("no")),
+                            ("1", _("Version 1")),
+                            ("2", _("Version 2")),
+                            ("1,2", _("Version 1 and 2")),
                         ],
-                        default_value="key-based",
+                        default_value="2",
                     ),
-                    forth=transform_ssh_config,
                 ),
-            ),
-            (
-                "Protocol",
-                DropdownChoice(
-                    title=_("Allowed protocols"),
-                    choices=[
-                        ("1", _("Version 1")),
-                        ("2", _("Version 2")),
-                        ("1,2", _("Version 1 and 2")),
-                    ],
-                    default_value="2",
-                ),
-            ),
-            ("Port", ListOfNetworkPorts(title=_("Allowed Ports"), default_value=[22])),
-            (
-                "PasswordAuthentication",
-                DropdownChoice(
-                    title=_("Allow password authentication"),
-                    help=_("Specifies whether password authentication is allowed"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
-                ),
-            ),
-            (
-                "PermitEmptyPasswords",
-                DropdownChoice(
-                    title=_("Permit empty passwords"),
-                    help=_(
-                        "If password authentication is used this option "
-                        "specifies wheter the server allows login to accounts "
-                        "with empty passwords"
+                ("port", ListOfNetworkPorts(title=_("Allowed Ports"), default_value=[22])),
+                (
+                    "passwordauthentication",
+                    DropdownChoice(
+                        title=_("Allow password authentication"),
+                        help=_("Specifies whether password authentication is allowed"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
                     ),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
                 ),
-            ),
-            (
-                "ChallengeResponseAuthentication",
-                DropdownChoice(
-                    title=_("Allow challenge-response authentication"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
+                (
+                    "permitemptypasswords",
+                    DropdownChoice(
+                        title=_("Permit empty passwords"),
+                        help=_(
+                            "If password authentication is used this option "
+                            "specifies wheter the server allows login to accounts "
+                            "with empty passwords"
+                        ),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-            (
-                "X11Forwarding",
-                DropdownChoice(
-                    title=_("Permit X11 forwarding"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
+                (
+                    "challengeresponseauthentication",
+                    DropdownChoice(
+                        title=_("Allow challenge-response authentication"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-            (
-                "UsePAM",
-                DropdownChoice(
-                    title=_("Use pluggable authentication module"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
+                (
+                    "x11forwarding",
+                    DropdownChoice(
+                        title=_("Permit X11 forwarding"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-            (
-                "Ciphers",
-                ListOfStrings(
-                    title=_("Allowed Ciphers"),
-                    orientation="horizontal",
+                (
+                    "usepam",
+                    DropdownChoice(
+                        title=_("Use pluggable authentication module"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-        ],
+                (
+                    "ciphers",
+                    ListOfStrings(
+                        title=_("Allowed Ciphers"),
+                        orientation="horizontal",
+                    ),
+                ),
+            ],
+        ),
     )
 
 

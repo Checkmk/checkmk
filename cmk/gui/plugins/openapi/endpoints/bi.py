44281cf86308951813ee07819aac843470a54625
Benedikt Seidl <benedikt.seidl@tribe29.com>
2022-11-08 07:51:26 +0100

    13437 FIX Business Intelligence: Do not lock on getting state of aggregations
    Before this change a call to this endpoint aquited the WATO lock which blocked
    other operations, although there is no need to, because this endpoint does not
    write WATO configuration.
    
    Change-Id: Id8297241868551064c95738941d62bb4ea8f3ce5

--- cmk/gui/plugins/openapi/endpoints/bi.py
+++ cmk/gui/plugins/openapi/endpoints/bi.py
@@ -276,6 +276,7 @@ class BIAggregationStateResponseSchema(Schema):
     response_schema=BIAggregationStateResponseSchema,
     permissions_required=RO_PERMISSIONS,
     tag_group="Monitoring",
+    skip_locking=True,
 )
 def get_bi_aggregation_state(params):
     """Get the state of BI aggregations"""

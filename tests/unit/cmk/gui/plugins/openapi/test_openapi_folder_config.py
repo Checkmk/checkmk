c5c90097cab01569c8eeb1504ed4b17e2eb3d84b
Philipp Siegmantel <philipp.siegmantel@tribe29.com>
2022-11-15 11:52:28 +0200

    Fix GitHub Actions
    Change-Id: Id5aef94e0dc730266271198d95aaec5028ee85f9
    (cherry picked from commit e0fcd21ebe08c8f180a2fd9d99fcc65159300559)

--- tests/unit/cmk/gui/plugins/openapi/test_openapi_folder_config.py
+++ tests/unit/cmk/gui/plugins/openapi/test_openapi_folder_config.py
@@ -14,7 +14,7 @@ import pytest
 
 from tests.unit.cmk.gui.conftest import WebTestAppForCMK
 
-from cmk.utils import paths
+from cmk.utils import paths, version
 
 from cmk.gui.fields import FOLDER_PATTERN, FolderField
 from cmk.gui.fields.utils import BaseSchema
@@ -782,6 +782,7 @@ def test_openapi_folder_config_collections_recursive_list(aut_user_auth_wsgi_app
         assert "batman" not in folder["id"]
 
 
+@pytest.mark.skipif(version.is_raw_edition(), reason="Tested Attribute is not in RAW")
 def test_bake_agent_package_attribute_regression(
     base: str, aut_user_auth_wsgi_app: WebTestAppForCMK
 ) -> None:

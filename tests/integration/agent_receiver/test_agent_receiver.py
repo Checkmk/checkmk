02352ffee0bf90b25a120dc63973abb63c53c300
Joerg Herbel <joerg.herbel@tribe29.com>
2022-11-16 19:33:54 +0100

    Agent receiver: Overwrite verified-uuid header field
    The agent receiver relies on the HTTP header field verified-uuid to
    verify that the UUID in the URL used to call the endpoint matches the
    UUID in the client certificate. This mechanism could be overridden by
    manually adding verified-uuid to the HTTP header. As of this commit, we
    explicitly override this header field.
    
    Change-Id: I60ffc538b83a7028b99ea447cc015a81bcd68362

---
+++ tests/integration/agent_receiver/test_agent_receiver.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import uuid
+from http import HTTPStatus
+
+import requests
+
+from tests.testlib.site import Site
+
+
+def test_uuid_check_client_certificate(site: Site) -> None:
+    agent_receiver_port = int(
+        site.openapi.request(
+            method="get",
+            url="domain-types/internal/actions/discover-receiver/invoke",
+        ).text
+    )
+    # try to acces the status endpoint by explicitly writing a fake UUID into the HTTP header
+    uuid_ = uuid.uuid4()
+    agent_receiver_response = requests.get(
+        f"https://{site.http_address}:{agent_receiver_port}/{site.id}/agent-receiver/registration_status/{uuid_}",
+        headers={"verified-uuid": str(uuid_)},
+        verify=False,
+    )
+    assert agent_receiver_response.status_code == HTTPStatus.BAD_REQUEST
+    assert (
+        "Verified client UUID (missing: no client certificate provided) does not match UUID in URL"
+        in agent_receiver_response.json()["detail"]
+    )

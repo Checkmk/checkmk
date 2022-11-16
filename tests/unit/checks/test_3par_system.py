ec3cf9e4e7466793ae491538b98b3310a334440f
LukaRacic <luka.racic@tribe29.com>
2022-11-16 16:57:02 +0100

    14629 FIX 3par_system: Check plugin used to fail if a list of online nodes or cluster nodes was not available
    The check plugin used to fail if a list of online nodes or clusted nodes was not available in the agent output.
    
    Change-Id: I5cd18b08921f173c4fa7cd6bbb52c1a2da16367f

---
+++ tests/unit/checks/test_3par_system.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from testlib import Check
+
+pytestmark = pytest.mark.checks
+
+
+@pytest.mark.usefixtures("config_load_all_checks")
+def test_check_3par_system_no_online_and_cluster_nodes() -> None:
+    check_plugin = Check("3par_system")
+
+    string_table = [[
+        '{"id":168676,"name":"test-name","systemVersion":"9.5.3.12","IPv4Addr":"172.17.37.20","model":"HPEAlletra9060","serialNumber":"CZ222908M6","totalNodes":2,"masterNode":0}'
+    ]]
+
+    assert list(check_plugin.run_check("", {}, check_plugin.run_parse(string_table))) == [
+        (0, 'Model: HPEAlletra9060, Version: 9.5.3.12, Serial number: CZ222908M6, Online '
+         'nodes: 0/0')
+    ]

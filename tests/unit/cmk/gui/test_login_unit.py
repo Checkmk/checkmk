dbabfd10247c9243dd5cb883afeaf814f67e27b8
Ronny Bruska <ronny.bruska@tribe29.com>
2022-11-16 19:16:11 +0100

    14994 FIX Fix usage of "_trans_id=-1" in automation calls
    SUP-12339
    
    Change-Id: I366d11c39c1338cd2b319fec485d773eb8fb24ed

--- tests/unit/cmk/gui/test_login_unit.py
+++ tests/unit/cmk/gui/test_login_unit.py
@@ -13,7 +13,7 @@ import cmk.gui.login as login
 import cmk.gui.userdb as userdb
 from cmk.gui.config import load_config
 from cmk.gui.exceptions import MKAuthException
-from cmk.gui.globals import request, session, user
+from cmk.gui.globals import request, session, transactions, user
 from cmk.gui.utils.script_helpers import application_and_request_context
 
 
@@ -128,3 +128,12 @@ def test_web_server_auth_session(user_id):
             assert user.id == user_id
             assert session.user_id == user.id
         assert user.id is None
+
+
+def test_ignore_transaction_ids(request_context, monkeypatch, with_automation_user):
+    user_id, password = with_automation_user
+    request.set_var("_secret", password)
+    request.set_var("_username", user_id)
+    with login.authenticate(request):
+        assert transactions._ignore_transids
+    assert transactions._ignore_transids is False

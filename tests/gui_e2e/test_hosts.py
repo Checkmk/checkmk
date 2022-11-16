819a4ee01fbe3a2d68a4edbd955daa16af52e2cf
Matteo Stifano <matteo.stifano@tribe29.com>
2022-11-16 12:21:16 +0100

    Make test functions in test_hosts independent from previous status
    We here make each test function create a new host with a name defined
    by timestamps. In this way, a failure of a test function will not
    affect the execution of the follow up one.
    
    Change-Id: I72853c189dc6eff20ef7e5b2f73a7fdd2152810b

--- tests/gui_e2e/test_hosts.py
+++ tests/gui_e2e/test_hosts.py
@@ -2,12 +2,15 @@
 # Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+from time import time
+
 from tests.testlib.playwright.helpers import PPage
 
 
 class TestHost:
-    name = "test_host"
-    ip = "127.0.0.1"
+    def __init__(self, timestamp: float) -> None:
+        self.name = f"test_host_{timestamp}"
+        self.ip = "127.0.0.1"
 
 
 class TestHosts:
@@ -15,19 +18,21 @@ class TestHosts:
         """Creates a host and deletes it afterwards. Calling order of static methods
         is therefore essential!
         """
-        self._create_host(logged_in_page)
+        host = TestHost(round(time(), 2))
+        self._create_host(logged_in_page, host)
 
         logged_in_page.goto_monitoring_all_hosts()
-        logged_in_page.select_host(TestHost.name)
+        logged_in_page.select_host(host.name)
 
-        self._delete_host(logged_in_page)
+        self._delete_host(logged_in_page, host)
 
     def test_reschedule(self, logged_in_page: PPage, is_chromium: bool) -> None:
         """reschedules a check"""
-        self._create_host(logged_in_page)
+        host = TestHost(round(time(), 2))
+        self._create_host(logged_in_page, host)
 
         logged_in_page.goto_monitoring_all_hosts()
-        logged_in_page.select_host(TestHost.name)
+        logged_in_page.select_host(host.name)
 
         # Use the Check_MK Service. It is always there and the first.
         # There are two Services containing "Check_MK", using the first
@@ -39,17 +44,17 @@ class TestHosts:
         # otherwise the div stays open...
         logged_in_page.main_area.locator("div#popup_menu").wait_for(state="hidden")
 
-        self._delete_host(logged_in_page)
+        self._delete_host(logged_in_page, host)
 
     @staticmethod
-    def _create_host(logged_in_page: PPage) -> None:
+    def _create_host(logged_in_page: PPage, host: TestHost) -> None:
         """Creates a host by starting from a logged in page."""
         logged_in_page.goto_setup_hosts()
         logged_in_page.main_area.get_suggestion("Add host").click()
 
-        logged_in_page.main_area.get_input("host").fill(TestHost.name)
+        logged_in_page.main_area.get_input("host").fill(host.name)
         logged_in_page.main_area.get_attribute_label("ipaddress").click()
-        logged_in_page.main_area.get_input("ipaddress").fill(TestHost.ip)
+        logged_in_page.main_area.get_input("ipaddress").fill(host.ip)
 
         logged_in_page.main_area.get_suggestion("Save & go to service configuration").click()
         logged_in_page.main_area.get_element_including_texts(
@@ -60,11 +65,14 @@ class TestHosts:
         logged_in_page.expect_success_state()
 
     @staticmethod
-    def _delete_host(logged_in_page: PPage) -> None:
+    def _delete_host(logged_in_page: PPage, host: TestHost) -> None:
         """Deletes the former created host by starting from a logged in page."""
         logged_in_page.goto_setup_hosts()
 
-        logged_in_page.main_area.get_link_from_title("Delete this host").click()
+        # click on "delete host" for the given hostname via xpath selector
+        logged_in_page.main_area.locator(
+            f"//*[contains(@href,'delete_host%3D{host.name}')]"
+        ).click()
 
         logged_in_page.main_area.get_text("Yes").click()
         logged_in_page.main_area.get_element_including_texts(

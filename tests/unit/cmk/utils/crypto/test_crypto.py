b7ccc09df23c4e6ce21ff0329b868c57e9d3fa6a
Hannes Rantzsch <hannes.rantzsch@tribe29.com>
2022-11-17 14:59:48 +0100

    Password comparison
    add __eq__ to password, comparing passwords in a timing-attack safe way
    add tests for password creation and comparison
    
    Change-Id: Ifb4c9be45f826839a4f7c68149b843a1c8c36e5b

---
+++ tests/unit/cmk/utils/crypto/test_crypto.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import Any, AnyStr
+
+import pytest
+
+from cmk.utils.crypto import const_time_compare, Password
+
+
+@pytest.mark.parametrize(
+    "password",
+    [
+        "",
+        "test 😹",
+        "long" * 100,
+        b"\xf0\x9f\x98\xb9",
+    ],
+)
+def test_valid_password(password: AnyStr) -> None:
+    Password(password)
+
+
+@pytest.mark.parametrize(
+    "password",
+    ["\0", b"\0", b"\0".decode()],
+)
+def test_invalid_password(password: AnyStr) -> None:
+    with pytest.raises(ValueError, match="Invalid password"):
+        Password(password)
+
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (Password("😹"), Password("😹"), True),
+        (Password("😹"), Password(b"\xf0\x9f\x98\xb9"), True),
+        (Password("     "), Password(" "), False),
+        (Password(""), "", False),
+        (Password("123"), 123, False),
+    ],
+)
+def test_password_eq(a: Password, b: Any, expected: bool) -> None:
+    assert (a == b) == expected
+
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        ("test", "test", True),
+        (b"", "", True),
+        ("test", b"test", True),
+        (b"\xf0\x9f\x98\xb9", "😹", True),
+        ("hello", "test", False),
+        (b"hello", "test", False),
+        (b"hello", b"test", False),
+    ],
+)
+def test_const_time_compare(a: AnyStr, b: AnyStr, expected: bool) -> None:
+    assert const_time_compare(a, b) == expected

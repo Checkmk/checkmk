5046ceb75d44a672e31864bd8c2297220c1f9656
Lars Michelsen <lm@tribe29.com>
2022-11-18 10:19:44 +0100

    Move common registrations to a dedicated module
    Change-Id: Ie16798a1c904c40d31d216cfe1de2765d1ef4c14

---
+++ cmk/gui/plugins/main_modules/registration.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+"""Central module for common (non-edition specific) registrations"""
+
+
+import cmk.gui.pages
+from cmk.gui.config import register_post_config_load_hook
+from cmk.gui.painters.v0.base import painter_registry
+from cmk.gui.plugins.visuals.utils import visual_type_registry
+from cmk.gui.views import datasource_selection, perfometer
+from cmk.gui.views.command import (
+    command_group_registry,
+    command_registry,
+    register_command_groups,
+    register_commands,
+)
+from cmk.gui.views.data_source import data_source_registry, register_data_sources
+from cmk.gui.views.host_tag_plugins import register_tag_plugins
+from cmk.gui.views.layout import layout_registry, register_layouts
+from cmk.gui.views.page_ajax_filters import AjaxInitialViewFilters
+from cmk.gui.views.page_ajax_popup_action_menu import ajax_popup_action_menu
+from cmk.gui.views.page_ajax_reschedule import PageRescheduleCheck
+from cmk.gui.views.page_create_view import page_create_view
+from cmk.gui.views.page_edit_view import page_edit_view, PageAjaxCascadingRenderPainterParameters
+from cmk.gui.views.page_edit_views import page_edit_views
+from cmk.gui.views.page_show_view import page_show_view
+from cmk.gui.views.sorter import register_sorters, sorter_registry
+from cmk.gui.views.visual_type import VisualTypeViews
+
+cmk.gui.pages.register("view")(page_show_view)
+cmk.gui.pages.register("create_view")(datasource_selection.page_create_view)
+cmk.gui.pages.register("edit_view")(page_edit_view)
+cmk.gui.pages.register("edit_views")(page_edit_views)
+cmk.gui.pages.register("create_view_infos")(page_create_view)
+cmk.gui.pages.register("ajax_popup_action_menu")(ajax_popup_action_menu)
+cmk.gui.pages.page_registry.register_page("ajax_cascading_render_painer_parameters")(
+    PageAjaxCascadingRenderPainterParameters
+)
+cmk.gui.pages.page_registry.register_page("ajax_reschedule")(PageRescheduleCheck)
+cmk.gui.pages.page_registry.register_page("ajax_initial_view_filters")(AjaxInitialViewFilters)
+
+register_post_config_load_hook(register_tag_plugins)
+visual_type_registry.register(VisualTypeViews)
+register_layouts(layout_registry)
+register_sorters(sorter_registry)
+register_command_groups(command_group_registry)
+register_commands(command_registry)
+register_data_sources(data_source_registry)
+perfometer.register(sorter_registry, painter_registry)
--- cmk/gui/raw/plugins/main_modules/registration.py
+++ cmk/gui/raw/plugins/main_modules/registration.py
@@ -3,6 +3,8 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+"""Raw edition and only raw edition specific registrations"""
+
 from collections.abc import Sequence
 from functools import partial
 
@@ -10,33 +12,13 @@ import cmk.gui.pages
 import cmk.gui.plugins.metrics.graph_images as graph_images
 import cmk.gui.plugins.metrics.html_render as html_render
 import cmk.gui.plugins.views.painters as painters
-from cmk.gui.config import register_post_config_load_hook
 from cmk.gui.i18n import _
 from cmk.gui.metrics import page_graph_dashlet, page_host_service_graph_popup
 from cmk.gui.painters.v0.base import Cell, painter_registry
 from cmk.gui.plugins.metrics.utils import CombinedGraphMetricSpec
-from cmk.gui.plugins.visuals.utils import visual_type_registry
 from cmk.gui.type_defs import CombinedGraphSpec, Row
 from cmk.gui.view_utils import CellSpec
-from cmk.gui.views import datasource_selection, graph, perfometer
-from cmk.gui.views.command import (
-    command_group_registry,
-    command_registry,
-    register_command_groups,
-    register_commands,
-)
-from cmk.gui.views.data_source import data_source_registry, register_data_sources
-from cmk.gui.views.host_tag_plugins import register_tag_plugins
-from cmk.gui.views.layout import layout_registry, register_layouts
-from cmk.gui.views.page_ajax_filters import AjaxInitialViewFilters
-from cmk.gui.views.page_ajax_popup_action_menu import ajax_popup_action_menu
-from cmk.gui.views.page_ajax_reschedule import PageRescheduleCheck
-from cmk.gui.views.page_create_view import page_create_view
-from cmk.gui.views.page_edit_view import page_edit_view, PageAjaxCascadingRenderPainterParameters
-from cmk.gui.views.page_edit_views import page_edit_views
-from cmk.gui.views.page_show_view import page_show_view
-from cmk.gui.views.sorter import register_sorters, sorter_registry
-from cmk.gui.views.visual_type import VisualTypeViews
+from cmk.gui.views import graph
 
 
 def resolve_combined_single_metric_spec(
@@ -60,18 +42,6 @@ def register_pages() -> None:
         ("ajax_graph_hover", html_render.ajax_graph_hover),
     ):
         cmk.gui.pages.register(path)(partial(callback, resolve_combined_single_metric_spec))
-
-    cmk.gui.pages.register("view")(page_show_view)
-    cmk.gui.pages.register("create_view")(datasource_selection.page_create_view)
-    cmk.gui.pages.register("edit_view")(page_edit_view)
-    cmk.gui.pages.register("edit_views")(page_edit_views)
-    cmk.gui.pages.register("create_view_infos")(page_create_view)
-    cmk.gui.pages.register("ajax_popup_action_menu")(ajax_popup_action_menu)
-    cmk.gui.pages.page_registry.register_page("ajax_cascading_render_painer_parameters")(
-        PageAjaxCascadingRenderPainterParameters
-    )
-    cmk.gui.pages.page_registry.register_page("ajax_reschedule")(PageRescheduleCheck)
-    cmk.gui.pages.page_registry.register_page("ajax_initial_view_filters")(AjaxInitialViewFilters)
 
 
 def register_painters() -> None:
@@ -99,12 +69,3 @@ def register_painters() -> None:
 
 register_pages()
 register_painters()
-
-register_post_config_load_hook(register_tag_plugins)
-visual_type_registry.register(VisualTypeViews)
-register_layouts(layout_registry)
-register_sorters(sorter_registry)
-register_command_groups(command_group_registry)
-register_commands(command_registry)
-register_data_sources(data_source_registry)
-perfometer.register(sorter_registry, painter_registry)

0a49a7e4f61ce713fa76df7e15bb9cffcc9a9085
Rebekka Seltmann <rebekka.seltmann@tribe29.com>
2022-11-25 09:40:38 +0100

    Refactoring: Move shared gcp valuespec functions to own module
    Change-Id: I4499261c800324aa9e3e8bed171b52df43b67149

---
+++ cmk/gui/plugins/wato/check_parameters/gcp.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections.abc import Sequence
+
+from cmk.gui.i18n import _
+from cmk.gui.plugins.wato.utils import Levels
+from cmk.gui.plugins.wato.utils.simple_levels import SimpleLevels
+from cmk.gui.valuespec import CascadingDropdown, Dictionary, Percentage, ValueSpec
+
+# A notes about the names of the Dictionary elements. They correspond to the names of the metrics in
+# the check plugin. Please do not change them.
+
+
+def _vs_disk_elements() -> Sequence[tuple[str, ValueSpec]]:
+    return [
+        ("disk_utilization", Levels(title=_("Disk usage"), unit="%", default_value=(80, 90))),
+        ("disk_read_ios", Levels(title=_("Number of read IOPS"))),
+        ("disk_write_ios", Levels(title=_("Number of write IOPS"))),
+    ]
+
+
+def _vs_network_elements() -> Sequence[tuple[str, ValueSpec]]:
+    return [
+        ("net_data_sent", SimpleLevels(title=_("Data sent"), unit="bytes/s")),
+        ("net_data_recv", SimpleLevels(title=_("Data received"), unit="bytes/s")),
+    ]
+
+
+def _vs_run_cpu() -> ValueSpec:
+    return Dictionary(
+        title=_("Levels CPU"),
+        elements=[
+            ("util", SimpleLevels(Percentage, title=_("CPU utilization"), default_value=(80, 90))),
+        ],
+    )
+
+
+def _vs_percentile_choice(
+    dropdown_title: str, choice_title: str, choice_unit: str
+) -> CascadingDropdown:
+    return CascadingDropdown(
+        title=dropdown_title,
+        choices=[
+            (
+                50,
+                _("50th percentile"),
+                Levels(title=choice_title, unit=choice_unit),
+            ),
+            (
+                95,
+                _("95th percentile"),
+                Levels(title=choice_title, unit=choice_unit),
+            ),
+            (
+                99,
+                _("99th percentile"),
+                Levels(title=choice_title, unit=choice_unit),
+            ),
+        ],
+    )
--- cmk/gui/plugins/wato/check_parameters/gcp_gcs.py
+++ cmk/gui/plugins/wato/check_parameters/gcp_gcs.py
@@ -2,9 +2,14 @@
 # Copyright (C) 2022 tribe29 GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from collections.abc import Sequence
 
 from cmk.gui.i18n import _
+from cmk.gui.plugins.wato.check_parameters.gcp import (
+    _vs_disk_elements,
+    _vs_network_elements,
+    _vs_percentile_choice,
+    _vs_run_cpu,
+)
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
     CheckParameterRulespecWithoutItem,
@@ -14,7 +19,6 @@ from cmk.gui.plugins.wato.utils import (
 )
 from cmk.gui.plugins.wato.utils.simple_levels import SimpleLevels
 from cmk.gui.valuespec import (
-    CascadingDropdown,
     Dictionary,
     Filesize,
     Integer,
@@ -141,15 +145,6 @@ def _vs_run_memory() -> ValueSpec:
     )
 
 
-def _vs_run_cpu() -> ValueSpec:
-    return Dictionary(
-        title=_("Levels CPU"),
-        elements=[
-            ("util", SimpleLevels(Percentage, title=_("CPU utilization"), default_value=(80, 90))),
-        ],
-    )
-
-
 rulespec_registry.register(
     CheckParameterRulespecWithItem(
         check_group_name="gcp_sql_cpu",
@@ -172,13 +167,6 @@ rulespec_registry.register(
 )
 
 
-def _vs_network_elements() -> Sequence[tuple[str, ValueSpec]]:
-    return [
-        ("net_data_sent", SimpleLevels(title=_("Data sent"), unit="bytes/s")),
-        ("net_data_recv", SimpleLevels(title=_("Data received"), unit="bytes/s")),
-    ]
-
-
 def _vs_sql_network() -> ValueSpec:
     return Dictionary(
         title=_("Levels on network traffic"),
@@ -199,14 +187,6 @@ rulespec_registry.register(
         item_spec=_item_spec_sql,
     )
 )
-
-
-def _vs_disk_elements() -> Sequence[tuple[str, ValueSpec]]:
-    return [
-        ("disk_utilization", Levels(title=_("Disk usage"), unit="%", default_value=(80, 90))),
-        ("disk_read_ios", Levels(title=_("Number of read IOPS"))),
-        ("disk_write_ios", Levels(title=_("Number of write IOPS"))),
-    ]
 
 
 def _vs_disk() -> ValueSpec:
@@ -368,31 +348,6 @@ rulespec_registry.register(
         item_spec=_item_spec_http_lb,
     )
 )
-
-
-def _vs_percentile_choice(
-    dropdown_title: str, choice_title: str, choice_unit: str
-) -> CascadingDropdown:
-    return CascadingDropdown(
-        title=dropdown_title,
-        choices=[
-            (
-                50,
-                _("50th percentile"),
-                Levels(title=choice_title, unit=choice_unit),
-            ),
-            (
-                95,
-                _("95th percentile"),
-                Levels(title=choice_title, unit=choice_unit),
-            ),
-            (
-                99,
-                _("99th percentile"),
-                Levels(title=choice_title, unit=choice_unit),
-            ),
-        ],
-    )
 
 
 def _vs_gcs_http_lb_latencies() -> ValueSpec:

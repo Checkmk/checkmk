5eaf9be33e0f04535cb5b67eb95b4fc71f09d542
Joerg Herbel <joerg.herbel@tribe29.com>
2022-11-29 13:32:28 +0100

    14827 FIX Rework agent plugin for monitoring SSH daemon configuration
    SUP-11861
    CMK-11752
    
    Change-Id: Ib724d4e78dd5d397254871fd66aede0394fd6b26

--- cmk/gui/plugins/wato/check_parameters/sshd_config.py
+++ cmk/gui/plugins/wato/check_parameters/sshd_config.py
@@ -4,6 +4,8 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
+
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithoutItem,
@@ -19,24 +21,13 @@ from cmk.gui.valuespec import (
 )
 
 
-def transform_ssh_config(choice):
-    """
-    In the sshd_config the options without-password and
-    prohibit-password are equivalent. Therefore, we
-    transform the old Check_MK option without-password
-    to the new option key-based which represents both values.
-    """
-    if choice == "without-password":
-        return "key-based"
-    return choice
-
-
-def _parameter_valuespec_sshd_config():
-    return Dictionary(
-        elements=[
-            (
-                "PermitRootLogin",
-                Transform(
+def _parameter_valuespec_sshd_config() -> Transform:
+    return Transform(
+        forth=_migrate,
+        valuespec=Dictionary(
+            elements=[
+                (
+                    "permitrootlogin",
                     DropdownChoice(
                         title=_("Permit root login"),
                         choices=[
@@ -47,92 +38,107 @@ def _parameter_valuespec_sshd_config():
                         ],
                         default_value="key-based",
                     ),
-                    forth=transform_ssh_config,
                 ),
-            ),
-            (
-                "Protocol",
-                DropdownChoice(
-                    title=_("Allowed protocols"),
-                    choices=[
-                        ("1", _("Version 1")),
-                        ("2", _("Version 2")),
-                        ("1,2", _("Version 1 and 2")),
-                    ],
-                    default_value="2",
-                ),
-            ),
-            ("Port", ListOfNetworkPorts(title=_("Allowed Ports"), default_value=[22])),
-            (
-                "PasswordAuthentication",
-                DropdownChoice(
-                    title=_("Allow password authentication"),
-                    help=_("Specifies whether password authentication is allowed"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
-                ),
-            ),
-            (
-                "PermitEmptyPasswords",
-                DropdownChoice(
-                    title=_("Permit empty passwords"),
-                    help=_(
-                        "If password authentication is used this option "
-                        "specifies wheter the server allows login to accounts "
-                        "with empty passwords"
+                (
+                    "protocol",
+                    DropdownChoice(
+                        title=_("Allowed protocols (deprecated)"),
+                        help=_(
+                            "Note that since OpenSSH 7.6, only version 2 is supported. Therefore, "
+                            "newer versions neither use nor report this configuration variable anymore."
+                        ),
+                        choices=[
+                            ("1", _("Version 1")),
+                            ("2", _("Version 2")),
+                            ("1,2", _("Version 1 and 2")),
+                        ],
+                        default_value="2",
                     ),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
                 ),
-            ),
-            (
-                "ChallengeResponseAuthentication",
-                DropdownChoice(
-                    title=_("Allow challenge-response authentication"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
+                ("port", ListOfNetworkPorts(title=_("Allowed Ports"), default_value=[22])),
+                (
+                    "passwordauthentication",
+                    DropdownChoice(
+                        title=_("Allow password authentication"),
+                        help=_("Specifies whether password authentication is allowed"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-            (
-                "X11Forwarding",
-                DropdownChoice(
-                    title=_("Permit X11 forwarding"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
+                (
+                    "permitemptypasswords",
+                    DropdownChoice(
+                        title=_("Permit empty passwords"),
+                        help=_(
+                            "If password authentication is used this option "
+                            "specifies wheter the server allows login to accounts "
+                            "with empty passwords"
+                        ),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-            (
-                "UsePAM",
-                DropdownChoice(
-                    title=_("Use pluggable authentication module"),
-                    choices=[
-                        ("yes", _("Yes")),
-                        ("no", _("No")),
-                    ],
-                    default_value="no",
+                (
+                    "kbdinteractiveauthentication",
+                    DropdownChoice(
+                        title=_(
+                            "Allow keyboard-interactive (previously challenge-response) authentication"
+                        ),
+                        help=_(
+                            "Keyboard-interactive authentication (<tt>KbdInteractiveAuthentication</tt>) "
+                            "was previously called challenge-response authentication (<tt>ChallengeResponseAuthentication</tt>). "
+                            "If you active this option, Checkmk will check for both configuration settings."
+                        ),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-            (
-                "Ciphers",
-                ListOfStrings(
-                    title=_("Allowed Ciphers"),
-                    orientation="horizontal",
+                (
+                    "x11forwarding",
+                    DropdownChoice(
+                        title=_("Permit X11 forwarding"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
                 ),
-            ),
-        ],
+                (
+                    "usepam",
+                    DropdownChoice(
+                        title=_("Use pluggable authentication module"),
+                        choices=[
+                            ("yes", _("Yes")),
+                            ("no", _("No")),
+                        ],
+                        default_value="no",
+                    ),
+                ),
+                (
+                    "ciphers",
+                    ListOfStrings(
+                        title=_("Allowed Ciphers"),
+                        orientation="horizontal",
+                    ),
+                ),
+            ],
+        ),
     )
+
+
+def _migrate(p: Mapping[str, object]) -> Mapping[str, object]:
+    replaced_keys = {"challengeresponseauthentication": "kbdinteractiveauthentication"}
+    return {replaced_keys.get(k.lower(), k.lower()): v for k, v in p.items()}
 
 
 rulespec_registry.register(

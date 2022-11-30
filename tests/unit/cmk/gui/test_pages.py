f19ab18c2d0ad70009d1545eea457c810efd3c30
Simon Jess <simon.jess@tribe29.com>
2022-11-30 08:59:43 +0100

    licensing: Add offline verification page
    Change-Id: I8c3d1d87b09a6b9e42e931a745b5facdc7def7d7

--- tests/unit/cmk/gui/test_pages.py
+++ tests/unit/cmk/gui/test_pages.py
@@ -220,6 +220,7 @@ def test_registered_pages() -> None:
             "ajax_ntop_engaged_alerts",
             "ajax_ntop_past_alerts",
             "ajax_ntop_flow_alerts",
+            "licensing_download_verification_request",
         ]
 
     # TODO: Depending on how we call the test (single test or whole package) we

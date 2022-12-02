702d2ced16f7bcf407f105f5afb138816731fa60
Sergey Kipnis <sergey.kipnis@tribe29.com>
2022-12-02 13:17:32 +0100

    add gtest target to lib livestatus
    - deploy googletest from omd
    - link googletest into test
    - add simple TEST to test
    - manually check call
    
    Change-Id: I6793d4f6d715f25b29bb85cd8122a0ba8cf77d24

--- packages/livestatus/test/test.cc
+++ packages/livestatus/test/test.cc
@@ -1,13 +1,18 @@
+#include <gtest/gtest.h>
+
 #include <iostream>
 #include <memory>
 
 #include "livestatus/Logger.h"
 #include "livestatus/POSIXUtils.h"
 
-int main() {
+bool check_livestatus_available() {
     setThreadName("main");
     Logger *logger_cmk = Logger::getLogger("cmk");
     logger_cmk->setUseParentHandlers(false);
     std::cout << "Hello world, Logger works\n";
-    return 0;
+    return true;
 }
+
+// Demonstrate some basic assertions.
+TEST(LivestatusAccess, Linked) { ASSERT_TRUE(check_livestatus_available()); }

c5c40284c50564aec9dccaf3120a6920ec175366
Max Linke <max.linke@tribe29.com>
2022-12-05 12:48:49 +0100

    15015 Add OMD disk usage check plugin
    When running a site it would be nice to know if the disk is running full
    because of the rrds or extensive logging. This allows to access the
    information without having to access the server a site runs on directly.
    
    Change-Id: I86576cea69eef2caa197406ff3822e17177a2199

---
+++ cmk/base/plugins/agent_based/omd_diskusage.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+#
+from dataclasses import dataclass
+from typing import Iterator, Mapping, NewType, Sequence
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import check_levels, register, render, Service
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+    StringTable,
+)
+
+
+@dataclass(frozen=True)
+class Site:
+    site: int
+    logs: int
+    rrds: int
+
+
+Section = NewType("Section", Mapping[str, Site])
+
+
+def sub_section_parser(string_table: StringTable) -> Iterator[tuple[str, Sequence[str]]]:
+    sub_section: list[str] = []
+    name = ""
+    for line in string_table:
+        if line[0].startswith("[") and line[0].endswith("]"):
+            if len(sub_section) == 0:
+                name = line[0][1:-1]
+                continue
+            yield name, sub_section
+            name = line[0][1:-1]
+            sub_section = []
+        elif line[0] != "":
+            sub_section.append(line[0])
+    if len(sub_section) != 0:
+        yield name, sub_section
+
+
+def parse(string_table: StringTable) -> Section:
+    sites: dict[str, Site] = {}
+    for section_name, lines in sub_section_parser(string_table):
+        log = rrd = site = 0
+        for line in lines:
+            if "log" in line:
+                log = int(line.split()[0])
+            elif "rrd" in line:
+                rrd = int(line.split()[0])
+            else:
+                site = int(line.split()[0])
+            sites[section_name.split()[1]] = Site(site, log, rrd)
+    return Section(sites)
+
+
+register.agent_section(
+    name="omd_diskusage",
+    parse_function=parse,
+)
+
+
+def discovery(section: Section) -> DiscoveryResult:
+    for item in section:
+        yield Service(item=item)
+
+
+def check(item: str, section: Section) -> CheckResult:
+    site = section[item]
+    yield from check_levels(
+        site.site, metric_name="omd_size", label="Total", render_func=render.bytes
+    )
+    yield from check_levels(
+        site.logs, metric_name="omd_log_size", label="Logs", render_func=render.bytes
+    )
+    yield from check_levels(
+        site.rrds, metric_name="omd_rrd_size", label="RRDs", render_func=render.bytes
+    )
+
+
+register.check_plugin(
+    name="omd_diskusage",
+    service_name="OMD %s disk usage",
+    discovery_function=discovery,
+    check_function=check,
+)
---
+++ tests/unit/cmk/base/plugins/agent_based/test_omd_diskusage.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+#
+from dataclasses import dataclass
+
+import pytest
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
+from cmk.base.plugins.agent_based.omd_diskusage import check, discovery, parse, Section
+
+TABLE = [
+    [l]
+    for l in """[site heute]
+110891421231        /omd/sites/heute
+11089142        /omd/sites/heute/var/log
+110460211219    /omd/sites/heute/var/check_mk/rrd
+[site test]
+4096    /omd/sites/test/var/check_mk/rrd
+16668   /omd/sites/test/var/log
+26668   /omd/sites/test
+""".split(
+        "\n"
+    )
+]
+
+
+@pytest.fixture(name="section")
+def _section() -> Section:
+    return parse(TABLE)
+
+
+def test_discovery(section: Section) -> None:
+    services = list(discovery(section))
+    assert services == [Service(item="heute"), Service(item="test")]
+
+
+@dataclass
+class SiteTest:
+    item: str
+    expected_results: list[Result]
+    expected_metrics: list[Metric]
+
+
+Sites = [
+    pytest.param(
+        SiteTest(
+            item="heute",
+            expected_results=[
+                Result(state=State.OK, summary="Total: 103 GiB"),
+                Result(state=State.OK, summary="Logs: 10.6 MiB"),
+                Result(state=State.OK, summary="RRDs: 103 GiB"),
+            ],
+            expected_metrics=[
+                Metric("omd_size", 110891421231),
+                Metric("omd_log_size", 11089142),
+                Metric("omd_rrd_size", 110460211219),
+            ],
+        ),
+        id="heute",
+    ),
+    pytest.param(
+        SiteTest(
+            item="test",
+            expected_results=[
+                Result(state=State.OK, summary="Total: 26.0 KiB"),
+                Result(state=State.OK, summary="Logs: 16.3 KiB"),
+                Result(state=State.OK, summary="RRDs: 4.00 KiB"),
+            ],
+            expected_metrics=[
+                Metric("omd_size", 26668),
+                Metric("omd_log_size", 16668),
+                Metric("omd_rrd_size", 4096),
+            ],
+        ),
+        id="test",
+    ),
+]
+
+
+@pytest.mark.parametrize("site", Sites)
+def test_check(site: SiteTest, section: Section) -> None:
+    check_results = list(check(item=site.item, section=section))
+    results = [r for r in check_results if isinstance(r, Result)]
+    metrics = [m for m in check_results if isinstance(m, Metric)]
+    assert metrics == site.expected_metrics
+    assert results == site.expected_results

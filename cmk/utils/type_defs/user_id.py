eb22b6874a3a2ff2b0de763e40cdad9155bc9fcd
Hannes Rantzsch <hannes.rantzsch@tribe29.com>
2022-12-20 17:33:06 +0100

    Extended documentation for UserId
    Use the exact same regex in livestatus.py (does not change the behavior
    as it's effectively the same regex as before).
    
    Change-Id: I42431992bb6f720766c2d6650e569b9b6f46aec8

--- cmk/utils/type_defs/user_id.py
+++ cmk/utils/type_defs/user_id.py
@@ -9,6 +9,47 @@ import re
 
 
 class UserId(str):
+    """
+    A Checkmk user ID
+
+    UserIds must comply to a restricted set of allowed characters (see UserId.validate).
+
+    UserIds must either be compatible with all protocols and components we interface with, or we
+    must ensure proper encoding whenever UserIds leave Checkmk.
+    The following *not exhaustive* list provides a starting point for the interfaces we need to
+    keep in mind:
+
+        * HTML rendering in numerous instances in the GUI
+        * URLs and links
+            * usually these go through `makeuri_contextless()` or `urlencode()` at the call site
+        * GUI messages (user to user) (`cmk.gui.message`)
+        * the value of the auth cookie
+        * the RestAPI
+            * responses
+            * bearer token (not base64 encoded!)
+        * various error messages (displayed in the GUI)
+
+        * file paths like `var/check_mk/web/<UserId>`
+            * sometimes those check for `/` and `..` but currently there's no mechanism make sure
+        * Visuals like Graphs and Reports that serialize UserIds into .mk files
+        * persisted UserSpecs and Sessions (.mk files)
+        * cmk.gui.userdb and htpasswd (LDAP is only ingress of users)
+        * various logs, including Audit Log
+
+        * Microcore
+        * Nagios core
+        * livestatus queries and commands
+            * livestatus.py (the validation regex is currently duplicated here!)
+            * cmk.gui.livestatus_utils (acknowledgements, comments, downtimes, ...)
+        * event console for contacts, notifications, possibly more
+        * agent registration and background jobs
+
+        * ntop connector
+        * Grafana connector
+        * X509 certificates and the `Key` object (`cmk.gui.key_mgmt`)
+    """
+
+    # Note: livestatus.py duplicates the regex to validate incoming UserIds!
     USER_ID_REGEX = re.compile(r"^[\w$][-@.\w$]*$", re.UNICODE)
 
     @classmethod
@@ -18,6 +59,8 @@ class UserId(str):
         UserIds are used in a variety of contexts, including HTML, file paths and other external
         systems. Currently we have no means of ensuring proper output encoding wherever UserIds
         are used. Thus, we strictly limit the characters that can be used in a UserId.
+
+        See class docstring for an incomplete list of places where UserIds are processed.
 
         Examples:
 
@@ -30,6 +73,12 @@ class UserId(str):
                 >>> UserId.validate("cmkadmin")
                 >>> UserId.validate("$cmk_@dmün.1")
 
+            Anything considered a letter in Unicode and the dollar sign is allowed.
+
+                >>> UserId.validate("cmkadmin")
+                >>> UserId.validate("$cmkädmin")
+                >>> UserId.validate("ↄ𝒽ѥ𝕔𖹬-艋く")
+
             Special characters other than '$_-@.' are not allowed (see `USER_ID_REGEX`).
 
                 >>> UserId.validate("foo/../")
@@ -41,6 +90,18 @@ class UserId(str):
                 Traceback (most recent call last):
                 ...
                 ValueError: Invalid username: '%2F'
+
+            Some special characters are not allowed at the start.
+
+                >>> UserId.validate(".")
+                Traceback (most recent call last):
+                ...
+                ValueError: Invalid username: '.'
+
+                >>> UserId.validate("@example.com")
+                Traceback (most recent call last):
+                ...
+                ValueError: Invalid username: '@example.com'
 
         """
         if text == "":

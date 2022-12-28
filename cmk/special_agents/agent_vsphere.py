48bd9236ac89521de14c41c6b7643432af5e3347
LukaRacic <luka.racic@tribe29.com>
2022-12-28 14:41:05 +0100

    15496 esx_vsphere_snapshot: Added vm name to the snapshot summary
    With the introduction of this werk the esx_vsphere_vm_snapshots_summary will include the VM name in addition to the snapshot name.
    So now the summary will look something like this: "vm_name/snapshot_name".
    
    SUP-12725
    
    Change-Id: I3c9c12765f7c2e94beadb83fbba3bf76f96de94c

--- cmk/special_agents/agent_vsphere.py
+++ cmk/special_agents/agent_vsphere.py
@@ -1646,17 +1646,23 @@ def get_pattern(pattern, line):
 
 # snapshot.rootSnapshotList.summary 871 1605626114 poweredOn SnapshotName| 834 1605632160 poweredOff Snapshotname2
 def get_section_snapshot_summary(vms):
-    snapshots = [
-        vm.get("snapshot.rootSnapshotList").split(" ")
-        for vm in vms.values()
-        if vm.get("snapshot.rootSnapshotList")
-    ]
+    snapshots = []
+    for vm in vms.values():
+        if raw_snapshots := vm.get("snapshot.rootSnapshotList"):
+            snapshots.extend(
+                [
+                    raw_snapshot.split(" ", 3) + vm["name"].split()
+                    for raw_snapshot in raw_snapshots.split("|")
+                ]
+            )
+
     return ["<<<esx_vsphere_snapshots_summary:sep(0)>>>"] + [
         json.dumps(
             {
                 "time": int(snapshot[1]),
                 "state": snapshot[2],
                 "name": snapshot[3],
+                "vm": snapshot[4],
             }
         )
         for snapshot in snapshots

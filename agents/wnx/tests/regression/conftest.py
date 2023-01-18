b8cf2c2250d8e5d8fc647ffd4b997b63232393b7
Sergey Kipnis <sergey.kipnis@tribe29.com>
2023-01-18 13:53:40 +0100

    use telnetlib3 in regression tests
    Change-Id: Ic8ae9c2b8eb78925f0676779da35683e253377de

--- agents/wnx/tests/regression/conftest.py
+++ agents/wnx/tests/regression/conftest.py
@@ -3,11 +3,12 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import asyncio
 import subprocess
-import telnetlib  # nosec
 import time
 
 import pytest
+import telnetlib3  # type: ignore
 import yaml
 
 from .local import DEFAULT_CONFIG, host, main_exe, port, run_agent, user_yaml_config
@@ -36,34 +37,54 @@ def wait_agent_engine():
     return inner
 
 
+_result = ""
+
+
+async def _telnet_shell(reader: telnetlib3.TelnetReader, _: telnetlib3.TelnetWriter) -> None:
+    global _result
+    while True:
+        data = await reader.read(1024)
+        if not data:
+            break
+        _result += data
+
+
+def _read_client_data(addr_host: str, addr_port: int) -> None:
+    loop = asyncio.get_event_loop()
+    coro = telnetlib3.open_connection(addr_host, addr_port, shell=_telnet_shell)
+    _, writer = loop.run_until_complete(coro)
+    loop.run_until_complete(writer.protocol.waiter_closed)
+
+
+def _get_data_using_telnet(addr_host: str, addr_port: int) -> str:
+    # overloaded CI Node may delay start/init of the agent process
+    # we must retry connection few times to avoid complaints
+    global _result
+    _result = ""
+    for _ in range(5):
+        try:
+            _read_client_data(addr_host, addr_port)
+            if _result:
+                return _result
+            time.sleep(2)
+        except Exception as _:
+            # print('No connect, waiting for agent')
+            time.sleep(2)
+
+    return ""
+
+
 @pytest.fixture(name="actual_output")
 def actual_output_engine(write_config, wait_agent):
     # Run agent and yield telnet output.
-    telnet, p = None, None
+    p = None
     try:
         p = run_agent(main_exe)
-
         # Override wait_agent in tests to wait for async processes to start.
         wait_agent()
 
-        for _ in range(0, 5):
-            try:
-                telnet = telnetlib.Telnet(host, port)  # nosec
-                break
-            except Exception as _:
-                # print('No connect, waiting for agent')
-                time.sleep(1)
-
-        if telnet is None:
-            raise ConnectionRefusedError("can't connect")
-
-        result = telnet.read_all().decode(encoding="cp1252")
-
-        yield result.splitlines()
+        yield _get_data_using_telnet(host, port).splitlines()
     finally:
-        if telnet:
-            telnet.close()
-
         if p:
             p.terminate()
 

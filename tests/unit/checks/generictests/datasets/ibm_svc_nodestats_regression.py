4acbfce04106c65d304d671f691d079215fc8e75
Dirk Mayer <71777632+dirkhub@users.noreply.github.com>
2023-01-20 14:04:37 +0100

    fix handling of decimal values

--- tests/unit/checks/generictests/datasets/ibm_svc_nodestats_regression.py
+++ tests/unit/checks/generictests/datasets/ibm_svc_nodestats_regression.py
@@ -105,10 +105,10 @@ checks = {
             [
                 (
                     0,
-                    "Latency is 0 ms for read, 0 ms for write",
+                    "Latency is 0.0 ms for read, 0.0 ms for write",
                     [
-                        ("read_latency", 0, None, None, None, None),
-                        ("write_latency", 0, None, None, None, None),
+                        ("read_latency", 0.0, None, None, None, None),
+                        ("write_latency", 0.0, None, None, None, None),
                     ],
                 )
             ],
@@ -119,10 +119,10 @@ checks = {
             [
                 (
                     0,
-                    "Latency is 5 ms for read, 1 ms for write",
+                    "Latency is 5.0 ms for read, 1.0 ms for write",
                     [
-                        ("read_latency", 5, None, None, None, None),
-                        ("write_latency", 1, None, None, None, None),
+                        ("read_latency", 5.0, None, None, None, None),
+                        ("write_latency", 1.0, None, None, None, None),
                     ],
                 )
             ],
@@ -133,10 +133,10 @@ checks = {
             [
                 (
                     0,
-                    "Latency is 2 ms for read, 0 ms for write",
+                    "Latency is 2.0 ms for read, 0.0 ms for write",
                     [
-                        ("read_latency", 2, None, None, None, None),
-                        ("write_latency", 0, None, None, None, None),
+                        ("read_latency", 2.0, None, None, None, None),
+                        ("write_latency", 0.0, None, None, None, None),
                     ],
                 )
             ],
@@ -150,7 +150,10 @@ checks = {
                 (
                     0,
                     "0.00 B/s read, 0.00 B/s write",
-                    [("read", 0, None, None, None, None), ("write", 0, None, None, None, None)],
+                    [
+                        ("read", 0.0, None, None, None, None),
+                        ("write", 0.0, None, None, None, None),
+                    ],
                 )
             ],
         ),
@@ -162,8 +165,8 @@ checks = {
                     0,
                     "1.05 MB/s read, 16.8 MB/s write",
                     [
-                        ("read", 1048576, None, None, None, None),
-                        ("write", 16777216, None, None, None, None),
+                        ("read", 1048576.0, None, None, None, None),
+                        ("write", 16777216.0, None, None, None, None),
                     ],
                 )
             ],
@@ -175,7 +178,10 @@ checks = {
                 (
                     0,
                     "0.00 B/s read, 0.00 B/s write",
-                    [("read", 0, None, None, None, None), ("write", 0, None, None, None, None)],
+                    [
+                        ("read", 0.0, None, None, None, None),
+                        ("write", 0.0, None, None, None, None),
+                    ],
                 )
             ],
         ),
@@ -187,8 +193,11 @@ checks = {
             [
                 (
                     0,
-                    "0 IO/s read, 0 IO/s write",
-                    [("read", 0, None, None, None, None), ("write", 0, None, None, None, None)],
+                    "0.0 IO/s read, 0.0 IO/s write",
+                    [
+                        ("read", 0.0, None, None, None, None),
+                        ("write", 0.0, None, None, None, None),
+                    ],
                 )
             ],
         ),
@@ -198,8 +207,11 @@ checks = {
             [
                 (
                     0,
-                    "15 IO/s read, 865 IO/s write",
-                    [("read", 15, None, None, None, None), ("write", 865, None, None, None, None)],
+                    "15.0 IO/s read, 865.0 IO/s write",
+                    [
+                      ("read", 15.0, None, None, None, None),
+                      ("write", 865.0, None, None, None, None),
+                    ],
                 )
             ],
         ),
@@ -209,8 +221,11 @@ checks = {
             [
                 (
                     0,
-                    "19 IO/s read, 110 IO/s write",
-                    [("read", 19, None, None, None, None), ("write", 110, None, None, None, None)],
+                    "19.0 IO/s read, 110.0 IO/s write",
+                    [
+                      ("read", 19.0, None, None, None, None),
+                      ("write", 110.0, None, None, None, None),
+                    ],
                 )
             ],
         ),

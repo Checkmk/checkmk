3c6b711a89d0624249779559f081a1aa9dff153d
Wontek Hong <wontek.hong@tribe29.com>
2023-01-23 09:40:11 +0100

    simple_config_file: refactor WatoSimpleConfigFile to allow for multiple config file types
    Change-Id: I53224cbf7482aac6281f6c34674c81e65958d8bf

--- cmk/gui/watolib/simple_config_file.py
+++ cmk/gui/watolib/simple_config_file.py
@@ -6,15 +6,16 @@
 
 import abc
 from pathlib import Path
-from typing import Generic, Mapping, TypeVar
+from typing import Generic, TypeVar
 
 import cmk.utils.store as store
 
+_G = TypeVar("_G")
 _T = TypeVar("_T")
 
 
-class WatoSimpleConfigFile(abc.ABC, Generic[_T]):
-    """Manage simple .mk config file containing a single dict variable
+class WatoConfigFile(abc.ABC, Generic[_G]):
+    """Manage simple .mk config file
 
     The file handling logic is inherited from cmk.utils.store.load_from_mk_file()
     and cmk.utils.store.save_to_mk_file().
@@ -24,23 +25,39 @@ class WatoSimpleConfigFile(abc.ABC, Generic[_T]):
         self._config_file_path = config_file_path
         self._config_variable = config_variable
 
-    def load_for_reading(self) -> dict[str, _T]:
+    def load_for_reading(self) -> _G:
         return self._load_file(lock=False)
 
-    def load_for_modification(self) -> dict[str, _T]:
+    def load_for_modification(self) -> _G:
         return self._load_file(lock=True)
 
-    def _load_file(self, lock: bool = False) -> dict[str, _T]:
+    def _load_file(self, lock: bool) -> _G:
         return store.load_from_mk_file(
-            "%s" % self._config_file_path,
+            self._config_file_path,
             key=self._config_variable,
             default={},
             lock=lock,
         )
 
-    def save(self, cfg: Mapping[str, _T]) -> None:
+    def save(self, cfg: _G) -> None:
         self._config_file_path.parent.mkdir(mode=0o770, exist_ok=True, parents=True)
         store.save_to_mk_file(str(self._config_file_path), self._config_variable, cfg)
+
+
+class WatoSingleConfigFile(WatoConfigFile[_T], Generic[_T]):
+    """Manage simple .mk config file containing a single dict variable which represents
+    the overall configuration. The 1st level dict represents the configuration
+    {base_url: ..., credentials: ...}
+    """
+
+
+class WatoSimpleConfigFile(WatoConfigFile[dict[str, _T]], Generic[_T]):
+    """Manage simple .mk config file containing a single dict variable
+    with nested entries. The 1st level dict encompasses those entries where each entry
+    has its own configuration.
+
+    An example is {"password_1": {...}, "password_2": {...}}
+    """
 
     def filter_usable_entries(self, entries: dict[str, _T]) -> dict[str, _T]:
         return entries

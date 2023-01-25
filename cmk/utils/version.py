2fc99a2db7cc719fffe2229a99eee10b2ca57fdf
Checkmk release system <feedback@checkmk.com>
2023-01-25 12:03:29 +0100

    Set version to 2.1.0p21

--- cmk/utils/version.py
+++ cmk/utils/version.py
@@ -10,7 +10,7 @@ does not offer stable APIs. The code may change at any time."""
 
 from __future__ import annotations
 
-__version__ = "2.1.0p20"
+__version__ = "2.1.0p21"
 
 import datetime
 import enum

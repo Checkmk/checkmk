34af950a6b2df70e4654123ff750a36aa199c788
Sven Panne <sven.panne@tribe29.com>
2023-02-07 16:31:22 +0100

    By popular demand: Renamed 'run-ci' scripts to simply 'run'.
    Change-Id: I72afab8a67493675ddd7dbacb61e3a4f598d8f4b

--- buildscripts/scripts/test-livestatus.groovy
+++ buildscripts/scripts/test-livestatus.groovy
@@ -6,7 +6,7 @@ def main() {
     dir("${checkout_dir}") {
         docker_image_from_alias("IMAGE_TESTING").inside() {
             stage('Compile & Test Livestatus') {
-                sh("packages/livestatus/run-ci --clean --all");
+                sh("packages/livestatus/run --clean --all");
             }
         }
         stage("Analyse Issues") {

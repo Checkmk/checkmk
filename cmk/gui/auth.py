c211e3d5e73e890acc0d2f4c1068c7c386090f7b
Christoph Rauch <christoph.rauch@tribe29.com>
2023-02-10 18:04:32 +0100

    Revert "auth: refactor authentication methods"
    This reverts commit 55097a38ff2f9987fc4755fc64d6c522fa02bd4a.

--- cmk/gui/auth.py
+++ cmk/gui/auth.py
@@ -5,15 +5,12 @@
 
 from __future__ import annotations
 
-import base64
-import binascii
 import hmac
 import time
 import traceback
 from contextlib import suppress
 from datetime import datetime
 from pathlib import Path
-from typing import Callable
 
 import cmk.utils
 import cmk.utils.paths
@@ -27,181 +24,51 @@ from cmk.gui.http import request
 from cmk.gui.i18n import _
 from cmk.gui.log import logger
 from cmk.gui.type_defs import AuthType
-from cmk.gui.userdb.session import auth_cookie_name, generate_auth_hash
+from cmk.gui.userdb.session import auth_cookie_name, generate_auth_hash, on_succeeded_login
 from cmk.gui.utils.urls import requested_file_name
 from cmk.gui.wsgi.type_defs import RFC7662
 
-auth_logger = logger.getChild("auth")
-
-AuthFunction = Callable[[], UserId | None]
-
 
 def _check_auth() -> tuple[UserId, AuthType]:
-    # NOTE: To push this list into the global namespace, the ordering of the methods need to
-    #       be taken into account.
-    auth_methods: list[tuple[AuthFunction, AuthType]] = [
-        (check_auth_by_custom_http_header, "http_header"),
-        (check_auth_by_remote_user, "web_server"),
-        (check_auth_by_basic_header, "web_server"),
-        (check_auth_by_cookie, "cookie"),
-        # After cookie, so users can experiment with differing users in the REST API, without
-        # having to log out.
-        (check_auth_by_bearer_header, "bearer"),
-        # Automation authentication via _username and _secret overrules everything else.
-        (check_auth_by_automation_credentials_in_request_values, "automation"),
-    ]
-    candidates: list[tuple[UserId, AuthType]] = []
-    for auth_method, auth_type in auth_methods:
-        user_id = auth_method()
-        # NOTE: It's important for these methods to always raise an exception whenever something
-        # strange is happening. This will abort the whole process, regardless of which auth method
-        # succeeded or not.
-        if user_id:
-            candidates.append((user_id, auth_type))
+    if user_id := _check_auth_web_server():
+        return _check_user(user_id, "web_server")
 
-    if not candidates:
-        raise MKAuthException("Couldn't log in.")
+    if user_id := check_automation_auth_by_request_values():
+        return _check_user(user_id, "automation")
 
-    # We take the last one, which is the most specific one.
-    user_id, auth_type = candidates[-1]
-    return _check_user(user_id), auth_type
+    if user_id := _check_auth_http_header():
+        return _check_user(user_id, "http_header")
+
+    if user_id := check_auth_by_cookie():
+        return _check_user(user_id, "cookie")
+
+    raise MKAuthException("Couldn't log in.")
 
 
-def _check_user(user_id: UserId) -> UserId:
-    if not active_config.user_login and not is_site_login():
+def _check_user(user_id: UserId | None, auth_type: AuthType) -> tuple[UserId, AuthType]:
+    if not active_config.user_login:
         raise MKAuthException("Site can't be logged into.")
 
-    # NOTE
-    # These are meant to be safeguards for programming errors which wouldn't be caught
-    # by our linters and type-checkers. So leave them in place, even though the type
-    # says user_id can only ever be a UserId.
-    if not user_id:
-        raise MKAuthException("User can't log in.")
-
-    if not isinstance(user_id, str):
+    if (user_id is not None and not isinstance(user_id, str)) or user_id == "":
         raise MKInternalError(_("Invalid user authentication"))
 
-    if not userdb.is_customer_user_allowed_to_login(user_id):
+    if user_id and not userdb.is_customer_user_allowed_to_login(user_id):
         # A CME not assigned with the current sites customer
         # is not allowed to log in
         auth_logger.debug("User '%s' is not allowed to authenticate: Invalid customer" % user_id)
         raise MKAuthException("Unknown customer. Can't log in.")
 
-    return user_id
+    if user_id and auth_type in ("http_header", "web_server"):
+        if auth_cookie_name() not in request.cookies:
+            on_succeeded_login(user_id, datetime.now())
 
+    if not active_config.user_login and not is_site_login():
+        raise MKAuthException("Site can't be logged into.")
 
-def _try_user_id(user_id: str) -> UserId:
-    """Try wrapping a UserId, but throw an HTTPAuthException when impossible."""
-    if not user_id:
-        raise MKAuthException("Empty user not allowed.")
-    try:
-        return UserId(user_id)
-    except ValueError as exc:
-        raise MKAuthException("Illegal username.") from exc
+    if user_id is None:
+        raise MKAuthException("User can't log in.")
 
-
-def check_auth_by_basic_header() -> UserId | None:
-    """Authenticate the user via the HTTP_AUTHORIZATION header
-    Returns:
-        A UserId if authenticated successfully
-
-    Raises:
-        MKAuthError:
-            - when the user in REMOTE_USER is different to the one in HTTP_AUTHORIZATION
-    """
-    if auth_header := request.environ.get("HTTP_AUTHORIZATION", ""):
-        if auth_header.startswith("Basic "):
-            user_id, _secret = user_from_basic_header(auth_header)
-            if not verify_credentials(user_id, _secret):
-                raise MKAuthException("Wrong credentials (Basic header)")
-            # Even if the credentials verify that the user may log in, if these don't match, we
-            # abort anyway, because this doesn't make any sense.
-            if remote_user := check_auth_by_remote_user():
-                if user_id and user_id != remote_user:
-                    raise MKAuthException("Mismatch in authentication headers.")
-            return user_id
-
-    return None
-
-
-def check_auth_by_bearer_header() -> UserId | None:
-    """Authenticate the user via the HTTP_AUTHORIZATION header
-    Returns:
-        None - if not authenticated
-        A tuple of UserId and AuthType if authenticated successfully
-    Raises:
-        MKAuthError:
-            - when the user in REMOTE_USER is different to the one in HTTP_AUTHORIZATION
-    """
-    if auth_header := request.environ.get("HTTP_AUTHORIZATION", ""):
-        try:
-            auth_type, _ = auth_header.split(None, 1)
-        except ValueError:
-            return None
-
-        if auth_type == "Bearer":
-            user_id, secret = user_from_bearer_header(auth_header)
-            if not verify_credentials(user_id, secret):
-                raise MKAuthException("Wrong credentials (Bearer header)")
-
-            return user_id
-
-    return None
-
-
-def verify_credentials(user_id: UserId, password: Password) -> bool:
-    if verify_automation_secret(user_id, password.raw):
-        return True
-    if gui_user_auth(user_id, password, datetime.now()):
-        return True
-    return False
-
-
-def user_from_basic_header(auth_header: str) -> tuple[UserId, Password]:
-    """Decode a Basic Authorization header
-
-    Examples:
-        >>> user_from_basic_header("Basic Zm9vYmF6YmFyOmZvb2JhemJhcg==")  # doctest: +ELLIPSIS
-        ('foobazbar', <cmk.utils.crypto...>)
-
-        >>> import pytest
-        >>> with pytest.raises(MKAuthException):
-        ...     user_from_basic_header("Basic SGFsbG8gV2VsdCE=")  # 'Hallo Welt!'
-
-        >>> with pytest.raises(MKAuthException):
-        ...     user_from_basic_header("Basic foobazbar")
-
-        >>> with pytest.raises(MKAuthException):
-        ...      user_from_basic_header("Basic     ")
-
-    Args:
-        auth_header:
-            The content of the HTTP_AUTHORIZATION header as a string.
-
-    Returns:
-        A tuple of UserId and password.
-
-    """
-    try:
-        _, token = auth_header.split("Basic ", 1)
-    except ValueError as exc:
-        raise MKAuthException("Not a valid Basic token.") from exc
-    if not token.strip():
-        raise MKAuthException("Not a valid Basic token.")
-
-    try:
-        user_entry = base64.b64decode(token.strip()).decode("utf8")
-    except binascii.Error as exc:
-        raise MKAuthException("Not a valid Basic token.") from exc
-    except UnicodeDecodeError as exc:
-        raise MKAuthException("Not a valid Basic token.") from exc
-
-    try:
-        user_id, secret = user_entry.split(":", 1)
-    except ValueError as exc:
-        raise MKAuthException("Not a valid Basic token.") from exc
-
-    return _try_user_id(user_id), Password(secret)
+    return user_id, auth_type
 
 
 def verify_automation_secret(user_id: UserId, secret: str) -> bool:
@@ -216,45 +83,51 @@ def verify_automation_secret(user_id: UserId, secret: str) -> bool:
     return False
 
 
-def check_auth_by_custom_http_header() -> UserId | None:
+def _check_auth_automation() -> UserId:
+    secret = request.get_str_input_mandatory("_secret", "").strip()
+    user_id = request.get_validated_type_input_mandatory(UserId, "_username", UserId.builtin())
+
+    request.del_var_from_env("_username")
+    request.del_var_from_env("_secret")
+
+    if verify_automation_secret(user_id, secret):
+        return user_id
+    raise MKAuthException(_("Invalid automation secret for user %s") % user_id)
+
+
+def _check_auth_http_header() -> UserId | None:
     """When http header auth is enabled, try to read the user_id from the var"""
+    user_id = None
+
     if auth_by_http_header := active_config.auth_by_http_header:
-        if not (user_name := request.headers.get(auth_by_http_header, type=str)):
-            return None
+        user_id = request.headers.get(auth_by_http_header, type=str)
 
-        return _try_user_id(user_name)
+    if not user_id:
+        return None
 
-    return None
+    return UserId(user_id)
 
 
-def check_auth_by_remote_user() -> UserId | None:
+def _check_auth_web_server() -> UserId | None:
     """Try to get the authenticated user from the HTTP request
 
     The user may have configured (basic) authentication by the web server. In
     case a user is provided, we trust that user.
     """
     # ? type of Request.remote_user attribute is unclear
-    user_name = request.remote_user
-    if user_name is None:
+    user_id = request.remote_user
+    if user_id is None:
         return None
 
-    user_id = _try_user_id(user_name)
-    if userdb.user_exists(user_id):
-        return user_id
-
-    raise MKAuthException(f"User {user_id} does not exist.")
+    return UserId(user_id)
 
 
-def check_auth_by_automation_credentials_in_request_values() -> UserId | None:
-    """Check credentials either in query string or form encoded POST body
-
-    Raises:
-        MKAuthException: whenever an illegal username is detected.
-    """
+def check_automation_auth_by_request_values() -> UserId | None:
+    """Check credentials either in query string or form encoded POST body"""
     user_name = request.values.get("_username")
     password = request.values.get("_secret")
     if user_name is not None and password is not None:
-        user_id = _try_user_id(user_name)
+        user_id = UserId(user_name)
         if verify_automation_secret(user_id, password):
             return user_id
 
@@ -278,7 +151,6 @@ def check_auth_by_cookie() -> UserId | None:
         userdb.on_access(username, session_id, datetime.now())
         return username
     except MKAuthException:
-        # Why and how does this happen and why we can't do it another way.
         # Suppress cookie validation errors from other sites cookies
         auth_logger.debug(
             f"Exception while checking cookie {cookie_name}: {traceback.format_exc()}"
@@ -304,6 +176,9 @@ def is_site_login() -> bool:
         return bool(request.var("_edition_short") and request.var("_version"))
 
     return False
+
+
+auth_logger = logger.getChild("auth")
 
 
 def user_from_cookie(raw_cookie: str) -> tuple[UserId, str, str]:
@@ -343,7 +218,7 @@ def check_parsed_auth_cookie(username: UserId, session_id: str, cookie_hash: str
 
 
 def user_from_bearer_header(auth_header: str) -> tuple[UserId, Password]:
-    """Authenticate a user from username and password sent in the header.
+    """
 
     Examples:
 
@@ -353,10 +228,8 @@ def user_from_bearer_header(auth_header: str) -> tuple[UserId, Password]:
 
     Args:
         auth_header:
-            The content of the HTTP_AUTHORIZATION header as a string.
 
     Returns:
-        A tuple of UserId and password.
 
     """
     try:
@@ -369,8 +242,12 @@ def user_from_bearer_header(auth_header: str) -> tuple[UserId, Password]:
         raise MKAuthException("No user/password combination in Bearer token.")
     if not secret:
         raise MKAuthException("Empty password not allowed.")
+    if not user_id:
+        raise MKAuthException("Empty user not allowed.")
+    if "/" in user_id:
+        raise MKAuthException("No slashes / allowed in username.")
 
-    return _try_user_id(user_id), Password(secret)
+    return UserId(user_id), Password(secret)
 
 
 def automation_auth(user_id: UserId, secret: Password) -> RFC7662 | None:
@@ -380,9 +257,7 @@ def automation_auth(user_id: UserId, secret: Password) -> RFC7662 | None:
     return None
 
 
-def gui_user_auth(user_id: UserId | None, secret: Password, now: datetime) -> RFC7662 | None:
-    if user_id is None:
-        return None
+def gui_user_auth(user_id: UserId, secret: Password, now: datetime) -> RFC7662 | None:
     try:
         if userdb.check_credentials(user_id, secret, now):
             return rfc7662_subject(user_id, "bearer")

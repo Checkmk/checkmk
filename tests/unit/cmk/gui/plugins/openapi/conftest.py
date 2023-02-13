915f7283ca31a52cd00c9d1ced3f4512e1b9c478
Gav <gavin.mcguigan@tribe29.com>
2023-02-13 15:00:31 +0100

    time_period: update unit tests to use the rest api test client
    Change-Id: I5a89dd0d6857cb76114ea5e27d2ef582b711e85b

--- tests/unit/cmk/gui/plugins/openapi/conftest.py
+++ tests/unit/cmk/gui/plugins/openapi/conftest.py
@@ -9,7 +9,13 @@ from typing import Optional, Tuple
 
 import pytest
 
-from tests.testlib.rest_api_client import AuxTagTestClient, RequestHandler, Response, RestApiClient
+from tests.testlib.rest_api_client import (
+    AuxTagTestClient,
+    RequestHandler,
+    Response,
+    RestApiClient,
+    TimePeriodTestClient,
+)
 
 from tests.unit.cmk.gui.conftest import HTTPMethod, WebTestAppForCMK
 
@@ -65,3 +71,8 @@ def api_client(aut_user_auth_wsgi_app: WebTestAppForCMK, base: str) -> RestApiCl
 @pytest.fixture()
 def auxtag_client(aut_user_auth_wsgi_app: WebTestAppForCMK, base: str) -> AuxTagTestClient:
     return AuxTagTestClient(WebTestAppRequestHandler(aut_user_auth_wsgi_app), base)
+
+
+@pytest.fixture()
+def timeperiod_client(aut_user_auth_wsgi_app: WebTestAppForCMK, base: str) -> TimePeriodTestClient:
+    return TimePeriodTestClient(WebTestAppRequestHandler(aut_user_auth_wsgi_app), base)

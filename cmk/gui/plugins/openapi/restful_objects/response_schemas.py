3513322840337f4f314d6f87cf49fbe038deeec2
Gav <gavin.mcguigan@tribe29.com>
2023-02-15 07:47:00 +0100

    rest-api: move time period endpoints to their own module
    Change-Id: I92ddd06eb944b35c4fe540d98e132abeb2ba77c9

--- cmk/gui/plugins/openapi/restful_objects/response_schemas.py
+++ cmk/gui/plugins/openapi/restful_objects/response_schemas.py
@@ -12,8 +12,6 @@ from marshmallow import fields as _fields
 from marshmallow import post_load, Schema
 from marshmallow_oneofschema import OneOfSchema  # type:ignore[import]
 
-from cmk.utils.defines import weekday_ids
-
 import cmk.gui.userdb as userdb
 from cmk.gui import fields as gui_fields
 from cmk.gui.exceptions import MKUserError
@@ -840,120 +838,6 @@ class UserCollection(DomainObjectCollection):
     value = fields.List(
         fields.Nested(UserObject),
         description="A list of user objects.",
-    )
-
-
-TIME_FIELD = fields.String(
-    example="14:00",
-    format="time",
-    description="The hour of the time period.",
-)
-
-
-class ConcreteTimeRange(BaseSchema):
-    start = TIME_FIELD
-    end = TIME_FIELD
-
-
-class ConcreteTimeRangeActive(BaseSchema):
-    day = fields.String(
-        description="The day for which the time ranges are specified",
-        pattern=f"{'|'.join(weekday_ids())}",
-    )
-    time_ranges = fields.List(fields.Nested(ConcreteTimeRange))
-
-
-class ConcreteTimePeriodException(BaseSchema):
-    date = fields.String(
-        example="2020-01-01",
-        format="date",
-        description="The date of the time period exception." "8601 profile",
-    )
-    time_ranges = fields.List(
-        fields.Nested(ConcreteTimeRange),
-        example="[{'start': '14:00', 'end': '18:00'}]",
-    )
-
-
-class ConcreteTimePeriod(BaseSchema):
-    alias = fields.String(description="The alias of the time period", example="alias")
-    active_time_ranges = fields.List(
-        fields.Nested(ConcreteTimeRangeActive),
-        description="The days for which time ranges were specified",
-        example={"day": "all", "time_ranges": [{"start": "12:00", "end": "14:00"}]},
-    )
-    exceptions = fields.List(
-        fields.Nested(ConcreteTimePeriodException),
-        description="Specific day exclusions with their list of time ranges",
-        example=[{"date": "2020-01-01", "time_ranges": [{"start": "14:00", "end": "18:00"}]}],
-    )
-    exclude = fields.List(  # type: ignore[assignment]
-        fields.String(description="Name of excluding time period", example="holidays"),
-        description="The collection of time period aliases whose periods are excluded",
-    )
-
-
-EXAMPLE_TIME_PERIOD = {
-    "alias": "holidays",
-    "active_time_ranges": [
-        {
-            "day": "monday",
-            "time_ranges": [{"start": "12:00", "end": "15:00"}],
-        },
-    ],
-    "exceptions": [
-        {
-            "date": "2023-01-01",
-            "time_ranges": [{"start": "12:30", "end": "13:30"}],
-        },
-    ],
-    "exclude": [
-        "time_period_1",
-        "time_period_2",
-        "time_period_3",
-    ],
-}
-
-
-class TimePeriodResponse(Linkable):
-    domainType = fields.Constant(
-        "time_period",
-        description="The domain type of the object.",
-    )
-    id = fields.String(
-        description="The unique identifier for this time period.", example="time_period_name"
-    )
-    title = fields.String(
-        description="The time period name.",
-        example="time_period_alias.",
-    )
-    members: gui_fields.Field = fields.Dict(
-        description="The container for external resources, like linked foreign objects or actions.",
-    )
-    extensions = fields.Nested(
-        ConcreteTimePeriod,
-        description="The time period attributes.",
-        example=EXAMPLE_TIME_PERIOD,
-    )
-
-
-class TimePeriodResponseCollection(DomainObjectCollection):
-    domainType = fields.Constant(
-        "time_period",
-        description="The domain type of the objects in the collection.",
-    )
-    value = fields.List(
-        fields.Nested(TimePeriodResponse),
-        description="A list of time period objects.",
-        example=[
-            {
-                "links": [],
-                "domainType": "time_period",
-                "id": "time_period",
-                "members": {},
-                "extensions": EXAMPLE_TIME_PERIOD,
-            }
-        ],
     )
 
 

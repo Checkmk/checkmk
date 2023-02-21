ccdd23221860c68ce3ce70405e9690b9a32b9fa7
Moritz Kiemer <moritz.kiemer@tribe29.com>
2023-02-21 19:42:09 +0100

    extension packages: sort paths
    Change-Id: I3514e36782466025c6807f87fa1d389c2b3da99c

--- cmk/update_config/plugins/pre_actions/utils.py
+++ cmk/update_config/plugins/pre_actions/utils.py
@@ -14,23 +14,23 @@ from cmk.utils.packaging import disable, Installer, PackageID, PathConfig
 import cmk.ec.export as ec  # pylint: disable=cmk-module-layer-violation
 
 _PATH_CONFIG = PathConfig(
-    local_root=paths.local_root,
-    mkp_rule_pack_dir=ec.mkp_rule_pack_dir(),
     agent_based_plugins_dir=paths.local_agent_based_plugins_dir,
-    checks_dir=paths.local_checks_dir,
-    inventory_dir=paths.local_inventory_dir,
-    check_manpages_dir=paths.local_check_manpages_dir,
     agents_dir=paths.local_agents_dir,
-    notifications_dir=paths.local_notifications_dir,
-    gui_plugins_dir=paths.local_gui_plugins_dir,
-    web_dir=paths.local_web_dir,
-    pnp_templates_dir=paths.local_pnp_templates_dir,
-    doc_dir=paths.local_doc_dir,
-    locale_dir=paths.local_locale_dir,
-    bin_dir=paths.local_bin_dir,
-    lib_dir=paths.local_lib_dir,
-    mib_dir=paths.local_mib_dir,
     alert_handlers_dir=paths.local_alert_handlers_dir,
+    bin_dir=paths.local_bin_dir,
+    check_manpages_dir=paths.local_check_manpages_dir,
+    checks_dir=paths.local_checks_dir,
+    doc_dir=paths.local_doc_dir,
+    gui_plugins_dir=paths.local_gui_plugins_dir,
+    inventory_dir=paths.local_inventory_dir,
+    lib_dir=paths.local_lib_dir,
+    locale_dir=paths.local_locale_dir,
+    local_root=paths.local_root,
+    mib_dir=paths.local_mib_dir,
+    mkp_rule_pack_dir=ec.mkp_rule_pack_dir(),
+    notifications_dir=paths.local_notifications_dir,
+    pnp_templates_dir=paths.local_pnp_templates_dir,
+    web_dir=paths.local_web_dir,
 )
 
 

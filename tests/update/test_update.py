bd3227a5de847a97ff43a3e16ed959fa814956e0
Matteo Stifano <matteo.stifano@tribe29.com>
2023-02-24 16:20:03 +0100

    Parametrize cmk base version in test_update
    We here retrieve the test site via a pytest fixture and we parametrize
    such fixture with the cmk base version. In this way, it will be easy to
    extend the cmk-base-versions list to be used in the test.
    
    Change-Id: Ibd1fcac87921f0dc5c1632737e094fb7920eddcc

--- tests/update/test_update.py
+++ tests/update/test_update.py
@@ -3,22 +3,21 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 import logging
-import os
 
 import pytest
-
-from tests.update.conftest import get_site
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.mark.type("update")
-def test_update():
-    get_site(os.getenv("TEST_UPDATE_SOURCE_RELEASE", "2.1.0p1"))
+def test_update(test_site):
+    # get_site(os.getenv("TEST_UPDATE_SOURCE_RELEASE", "2.1.0p1"))
     # TODO: check source installation
     # TODO: set config
     # TODO: get baseline monitoring data
-    get_site(os.getenv("TEST_UPDATE_TARGET_RELEASE", "2.1.0p21"), update=True)
+    # get_site(os.getenv("TEST_UPDATE_TARGET_RELEASE", "2.1.0p21"), update=True)
     # TODO: check target installation
     # TODO: check config
     # TODO: compare baseline monitoring data
+
+    pass

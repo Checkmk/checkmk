ccc24fdc7ea787ed17eb824a0ced29daf3fc8926
Mathias Laurin <mathias.laurin@tribe29.com>
2023-02-24 13:01:04 +0100

    Ensure every case of FetcherType enum is handled
    CMK-12425
    
    Change-Id: I7c02046e4c343202c07b3ef8c8bdcf4e18568391

--- cmk/fetchers/config.py
+++ cmk/fetchers/config.py
@@ -3,9 +3,10 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import os
 import os.path
 from pathlib import Path
-from typing import Final
+from typing import assert_never, Final
 
 import cmk.utils.paths
 from cmk.utils.type_defs import HostName
@@ -22,15 +23,25 @@ def make_persisted_section_dir(
     ident: str,
 ) -> Path:
     var_dir: Final = Path(cmk.utils.paths.var_dir)
-    return {
-        FetcherType.PIGGYBACK: var_dir / "persisted_sections" / ident / str(host_name),
-        FetcherType.SNMP: var_dir / "persisted_sections" / ident / str(host_name),
-        FetcherType.IPMI: var_dir / "persisted_sections" / ident / str(host_name),
-        FetcherType.PROGRAM: var_dir / "persisted" / str(host_name),
-        FetcherType.SPECIAL_AGENT: var_dir / "persisted_sections" / ident / str(host_name),
-        FetcherType.PUSH_AGENT: var_dir / "persisted_sections" / ident / str(host_name),
-        FetcherType.TCP: var_dir / "persisted" / str(host_name),
-    }[fetcher_type]
+    match fetcher_type:
+        case FetcherType.NONE:
+            return Path(os.devnull)
+        case FetcherType.PIGGYBACK:
+            return var_dir / "persisted_sections" / ident / str(host_name)
+        case FetcherType.SNMP:
+            return var_dir / "persisted_sections" / ident / str(host_name)
+        case FetcherType.IPMI:
+            return var_dir / "persisted_sections" / ident / str(host_name)
+        case FetcherType.PROGRAM:
+            return var_dir / "persisted" / str(host_name)
+        case FetcherType.SPECIAL_AGENT:
+            return var_dir / "persisted_sections" / ident / str(host_name)
+        case FetcherType.PUSH_AGENT:
+            return var_dir / "persisted_sections" / ident / str(host_name)
+        case FetcherType.TCP:
+            return var_dir / "persisted" / str(host_name)
+        case _:
+            assert_never(fetcher_type)
 
 
 def make_file_cache_path_template(
@@ -41,12 +52,22 @@ def make_file_cache_path_template(
     # We create a *template* and not a path, so string manipulation
     # is the right thing to do.
     base_dir: Final = str(cmk.utils.paths.data_source_cache_dir)
-    return {
-        FetcherType.PIGGYBACK: os.path.join(base_dir, ident, "{hostname}"),
-        FetcherType.SNMP: os.path.join(base_dir, ident, "{mode}", "{hostname}"),
-        FetcherType.IPMI: os.path.join(base_dir, ident, "{hostname}"),
-        FetcherType.SPECIAL_AGENT: os.path.join(base_dir, ident, "{hostname}"),
-        FetcherType.PROGRAM: os.path.join(cmk.utils.paths.tcp_cache_dir, "{hostname}"),
-        FetcherType.PUSH_AGENT: os.path.join(base_dir, ident, "{hostname}", "agent_output"),
-        FetcherType.TCP: os.path.join(cmk.utils.paths.tcp_cache_dir, "{hostname}"),
-    }[fetcher_type]
+    match fetcher_type:
+        case FetcherType.NONE:
+            return os.devnull
+        case FetcherType.PIGGYBACK:
+            return os.path.join(base_dir, ident, "{hostname}")
+        case FetcherType.SNMP:
+            return os.path.join(base_dir, ident, "{mode}", "{hostname}")
+        case FetcherType.IPMI:
+            return os.path.join(base_dir, ident, "{hostname}")
+        case FetcherType.SPECIAL_AGENT:
+            return os.path.join(base_dir, ident, "{hostname}")
+        case FetcherType.PROGRAM:
+            return os.path.join(cmk.utils.paths.tcp_cache_dir, "{hostname}")
+        case FetcherType.PUSH_AGENT:
+            return os.path.join(base_dir, ident, "{hostname}", "agent_output")
+        case FetcherType.TCP:
+            return os.path.join(cmk.utils.paths.tcp_cache_dir, "{hostname}")
+        case _:
+            assert_never(fetcher_type)

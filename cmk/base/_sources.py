0cfdc27f905735e9dfbb3b7fb182beded01ff5f7
Mathias Laurin <mathias.laurin@tribe29.com>
2023-02-27 16:27:26 +0100

    Use abstract factory pattern in make_sources
    This removes some copy and paste and helps splitting the source
    selection logic from the instantiation.  Several bugs have been
    recently reported and the code is hard to maintain, so let's
    improve it a bit.
    
    Change-Id: Iaa61515d1556fac1420e17203c808c754dce9444

---
+++ cmk/base/_sources.py
@@ -0,0 +1,484 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import abc
+from collections.abc import Mapping
+from typing import Final, Generic
+
+from cmk.utils.exceptions import OnError
+from cmk.utils.type_defs import AgentRawData, HostAddress, HostName
+
+from cmk.snmplib.type_defs import SNMPRawData, TRawData
+
+from cmk.fetchers import Fetcher, FetcherType, NoFetcher, NoFetcherError, ProgramFetcher
+from cmk.fetchers.config import make_file_cache_path_template
+from cmk.fetchers.filecache import (
+    AgentFileCache,
+    FileCache,
+    FileCacheMode,
+    FileCacheOptions,
+    MaxAge,
+    NoCache,
+    SNMPFileCache,
+)
+
+from cmk.checkers import SourceInfo, SourceType
+from cmk.checkers.type_defs import SectionNameCollection
+
+import cmk.base.config as config  # This module should be free from base deps.
+import cmk.base.core_config as core_config
+from cmk.base.config import ConfigCache
+
+__all__ = [
+    "Source",
+    "SNMPSource",
+    "MgmtSNMPSource",
+    "IPMISource",
+    "ProgramSource",
+    "PushAgentSource",
+    "TCPSource",
+    "SpecialAgentSource",
+    "PiggybackSource",
+    "MissingIPSource",
+]
+
+
+class Source(Generic[TRawData], abc.ABC):
+    """Abstract source factory.
+
+    Note:
+        Pass arguments to `__init__` if they depend on the type of the source;
+        pass arguments to the factory method if they are independent.
+
+    See Also:
+        https://refactoring.guru/design-patterns/abstract-factory
+
+    """
+
+    @abc.abstractmethod
+    def source_info(self) -> SourceInfo:
+        ...
+
+    @abc.abstractmethod
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[TRawData]:
+        ...
+
+    @abc.abstractmethod
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[TRawData]:
+        ...
+
+
+class SNMPSource(Source[SNMPRawData]):
+    fetcher_type: Final = FetcherType.SNMP
+    source_type: Final = SourceType.HOST
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress,
+        *,
+        max_age: MaxAge,
+        on_scan_error: OnError,
+        selected_sections: SectionNameCollection,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+        self._on_scan_error: Final = on_scan_error
+        self._selected_sections: Final = selected_sections
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "snmp",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[SNMPRawData]:
+        return config_cache.make_snmp_fetcher(
+            self.host_name,
+            self.ipaddress,
+            on_scan_error=self._on_scan_error,
+            selected_sections=self._selected_sections,
+        )
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[SNMPRawData]:
+        return SNMPFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type,
+                ident=self.source_info().ident,
+            ),
+            max_age=self._max_age,
+            simulation=simulation,
+            use_only_cache=file_cache_options.use_only_cache,
+            file_cache_mode=file_cache_options.file_cache_mode(),
+        )
+
+
+class MgmtSNMPSource(Source[SNMPRawData]):
+    fetcher_type: Final = FetcherType.SNMP
+    source_type: Final = SourceType.MANAGEMENT
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress,
+        *,
+        max_age: MaxAge,
+        on_scan_error: OnError,
+        selected_sections: SectionNameCollection,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+        self._on_scan_error: Final = on_scan_error
+        self._selected_sections: Final = selected_sections
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "mgmt_snmp",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[SNMPRawData]:
+        return config_cache.make_snmp_fetcher(
+            self.host_name,
+            self.ipaddress,
+            on_scan_error=self._on_scan_error,
+            selected_sections=self._selected_sections,
+        )
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[SNMPRawData]:
+        return SNMPFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type, ident=self.source_info().ident
+            ),
+            max_age=self._max_age,
+            simulation=simulation,
+            use_only_cache=file_cache_options.use_only_cache,
+            file_cache_mode=file_cache_options.file_cache_mode(),
+        )
+
+
+class IPMISource(Source[AgentRawData]):
+    fetcher_type: Final = FetcherType.IPMI
+    source_type: Final = SourceType.MANAGEMENT
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress,
+        *,
+        max_age: MaxAge,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "mgmt_ipmi",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[AgentRawData]:
+        return config_cache.make_ipmi_fetcher(self.host_name, self.ipaddress)
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[AgentRawData]:
+        return AgentFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type, ident=self.source_info().ident
+            ),
+            max_age=self._max_age,
+            simulation=simulation,
+            use_only_cache=file_cache_options.use_only_cache,
+            file_cache_mode=file_cache_options.file_cache_mode(),
+        )
+
+
+class ProgramSource(Source[AgentRawData]):
+    fetcher_type: Final = FetcherType.PROGRAM
+    source_type: Final = SourceType.HOST
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress | None,
+        *,
+        config_cache: ConfigCache,
+        max_age: MaxAge,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+        # `make_program_commandline()` may raise LookupError if no datasource
+        # is configured.
+        self._cmdline: Final = config_cache.make_program_commandline(host_name, ipaddress)
+        self._stdin: Final = None
+        self._is_cmc: Final = config.is_cmc()
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "agent",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[AgentRawData]:
+        return ProgramFetcher(cmdline=self._cmdline, stdin=self._stdin, is_cmc=self._is_cmc)
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[AgentRawData]:
+        return AgentFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type, ident=self.source_info().ident
+            ),
+            max_age=self._max_age,
+            simulation=simulation,
+            use_only_cache=file_cache_options.use_only_cache,
+            file_cache_mode=file_cache_options.file_cache_mode(),
+        )
+
+
+class PushAgentSource(Source[AgentRawData]):
+    fetcher_type: Final = FetcherType.PUSH_AGENT
+    source_type: Final = SourceType.HOST
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress | None,
+        *,
+        max_age: MaxAge,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "push-agent",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[AgentRawData]:
+        return NoFetcher(NoFetcherError.NO_FETCHER)
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[AgentRawData]:
+        return AgentFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type, ident=self.source_info().ident
+            ),
+            max_age=(
+                MaxAge.unlimited()
+                if simulation or file_cache_options.use_outdated
+                else self._max_age
+            ),
+            simulation=simulation,
+            use_only_cache=True,
+            file_cache_mode=(
+                # Careful: at most read-only!
+                FileCacheMode.DISABLED
+                if file_cache_options.disabled
+                else FileCacheMode.READ
+            ),
+        )
+
+
+class TCPSource(Source[AgentRawData]):
+    fetcher_type: Final = FetcherType.TCP
+    source_type: Final = SourceType.HOST
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress,
+        *,
+        max_age: MaxAge,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "agent",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[AgentRawData]:
+        return config_cache.make_tcp_fetcher(self.host_name, self.ipaddress)
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[AgentRawData]:
+        return AgentFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type, ident=self.source_info().ident
+            ),
+            max_age=self._max_age,
+            simulation=simulation,
+            use_only_cache=(
+                file_cache_options.tcp_use_only_cache or file_cache_options.use_only_cache
+            ),
+            file_cache_mode=file_cache_options.file_cache_mode(),
+        )
+
+
+class SpecialAgentSource(Source[AgentRawData]):
+    fetcher_type: Final = FetcherType.SPECIAL_AGENT
+    source_type: Final = SourceType.HOST
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress | None,
+        *,
+        max_age: MaxAge,
+        agent_name: str,
+        params: Mapping[str, object],
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self._max_age: Final = max_age
+        self._agent_name: Final = agent_name
+        self._params: Final = params
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            f"special_{self._agent_name}",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[AgentRawData]:
+        return ProgramFetcher(
+            cmdline=core_config.make_special_agent_cmdline(
+                self.host_name,
+                self.ipaddress,
+                self._agent_name,
+                self._params,
+            ),
+            stdin=core_config.make_special_agent_stdin(
+                self.host_name,
+                self.ipaddress,
+                self._agent_name,
+                self._params,
+            ),
+            is_cmc=config.is_cmc(),
+        )
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[AgentRawData]:
+        return AgentFileCache(
+            self.host_name,
+            path_template=make_file_cache_path_template(
+                fetcher_type=self.fetcher_type, ident=self.source_info().ident
+            ),
+            max_age=self._max_age,
+            simulation=simulation,
+            use_only_cache=file_cache_options.use_only_cache,
+            file_cache_mode=file_cache_options.file_cache_mode(),
+        )
+
+
+class PiggybackSource(Source[AgentRawData]):
+    fetcher_type: Final = FetcherType.PIGGYBACK
+    source_type: Final = SourceType.HOST
+
+    def __init__(
+        self,
+        host_name: HostName,
+        ipaddress: HostAddress | None,
+    ) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            "piggyback",
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher[AgentRawData]:
+        return config_cache.make_piggyback_fetcher(self.host_name, self.ipaddress)
+
+    def file_cache(
+        self, *, simulation: bool, file_cache_options: FileCacheOptions
+    ) -> FileCache[AgentRawData]:
+        return NoCache(self.host_name)
+
+
+class MissingIPSource(Source):
+    fetcher_type: Final = FetcherType.NONE
+    source_type: Final = SourceType.HOST
+
+    def __init__(self, host_name: HostName, ipaddress: None, ident: str) -> None:
+        super().__init__()
+        self.host_name: Final = host_name
+        self.ipaddress: Final = ipaddress
+        self.ident: Final = ident
+
+    def source_info(self) -> SourceInfo:
+        return SourceInfo(
+            self.host_name,
+            self.ipaddress,
+            self.ident,
+            self.fetcher_type,
+            self.source_type,
+        )
+
+    def fetcher(self, /, config_cache: ConfigCache) -> Fetcher:
+        return NoFetcher(NoFetcherError.MISSING_IP)
+
+    def file_cache(self, *, simulation: bool, file_cache_options: FileCacheOptions) -> FileCache:
+        return NoCache(self.host_name)

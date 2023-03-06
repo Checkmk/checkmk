c2661534c03baacacea3535362f608ede5c1f5e7
Benedikt Seidl <benedikt.seidl@tribe29.com>
2023-03-06 11:16:51 +0100

    backporting synology_disks from master
    Change-Id: I1febc9e0d1cb52cbf9be8b5d19e631a8eff3b262

---
+++ tests/unit/cmk/base/plugins/agent_based/test_synology_disk.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.base.plugins.agent_based import synology_disks
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, State
+
+SECTION_TABLE = [
+    ["Disk 1", "WD40EFAX-68JH4N0", "1", "33"],
+    ["Disk 2", "WD40EFAX-68JH4N0", "2", "33"],
+    ["Disk 3", "WD40EFAX-68JH4N0", "3", "33"],
+    ["Disk 4", "WD40EFAX-68JH4N0", "4", "33"],
+    ["Disk 5", "WD40EFAX-68JH4N0", "5", "33"],
+]
+
+
+def test_parsing() -> None:
+    section = synology_disks.parse_synology(SECTION_TABLE)
+    assert len(section) == len(SECTION_TABLE)
+
+
+def test_discovery() -> None:
+    section = synology_disks.parse_synology(SECTION_TABLE)
+    services = list(synology_disks.discover_synology_disks(section))
+    assert {s.item for s in services} == {el[0] for el in SECTION_TABLE}
+
+
+def make_section(
+    state: int = 1, temperature: float = 42.1, disk: str = "none", model: str = "hello"
+) -> synology_disks.Section:
+    return {disk: synology_disks.Disk(state=state, temperature=temperature, disk=disk, model=model)}
+
+
+@pytest.mark.parametrize(
+    "state, expected",
+    [(1, State.OK), (2, State.OK), (3, State.WARN), (4, State.CRIT), (5, State.CRIT)],
+)
+def test_result_state(state: int, expected: State) -> None:
+    section = make_section(state=state)
+    item = list(section.keys())[0]
+    result = list(synology_disks.check_synology_disks(item=item, section=section, params={}))[-1]
+    assert isinstance(result, Result)
+    assert result.state == expected
+
+
+def test_temperature_metric() -> None:
+    temperature = 42.0
+    section = make_section(temperature=temperature)
+    item = list(section.keys())[0]
+    result = list(synology_disks.check_synology_disks(item=item, section=section, params={}))[0]
+    assert isinstance(result, Metric)
+    assert result.value == temperature
+    assert result.name == "temp"
+
+
+@pytest.mark.parametrize("model, expected", [("mSSD", True), ("mNVME", True), ("HDD", None)])
+def test_discovery_detect_cached(model: str, expected: bool) -> None:
+    section = make_section(model=model, state=3)
+    service = list(synology_disks.discover_synology_disks(section))[0]
+    assert service.parameters.get("used_as_cache") is expected
+
+
+@pytest.mark.parametrize("used_as_cache, expected", [(True, State.OK), (False, State.WARN)])
+def test_check_cached_is_ok(used_as_cache: bool, expected: State) -> None:
+    section = make_section(state=3)
+    item = list(section.keys())[0]
+    params = {"used_as_cache": used_as_cache}
+    result = list(synology_disks.check_synology_disks(section=section, item=item, params=params))[
+        -1
+    ]
+    assert isinstance(result, Result)
+    assert result.state == expected

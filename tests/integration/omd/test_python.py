9e39e54205ee9061865ea26308b495f7e52fff29
Timotheus Bachinger <timotheus.bachinger@tribe29.com>
2023-03-08 17:16:49 +0100

    Rework pip integration test
    * Clean up tests
    * Assert that we install into correct folder
    * Add all supported pip commands
    
    Change-Id: Ia17b86a33fad3cc5794b9bb2de8ed98cf6b3a5ff

--- tests/integration/omd/test_python.py
+++ tests/integration/omd/test_python.py
@@ -8,16 +8,60 @@
 
 import os
 import subprocess
+from pathlib import Path
+import importlib
+
 import pytest  # type: ignore[import]
 import pkg_resources as pkg
-from typing import List
+from typing import List, NamedTuple
 from pipfile import Pipfile  # type: ignore[import]
 from testlib import repo_path
+from tests.testlib.site import Site  # type: ignore[import]
 
-# TODO: Currently we need to invoke pip as a module and cannot use pip3 only.
-# This is most likley due to the fact, that we set "PIP_TARGET" in pip3 during intermediate install
-# os.environ["PIP_TARGET"] = os.path.join(os.environ["OMD_ROOT"], "local/lib/python3")
-PIP_CMD = ["python3", "-m", "pip"]
+
+class PipCommand(NamedTuple):
+    command: List[str]
+    needs_target_as_commandline: bool
+
+
+SUPPORTED_PIP_CMDS = (
+    PipCommand(["python3", "-m", "pip"], True),
+    PipCommand(["pip3"], False),  # Target is set in the wrapper script as cmd line
+    PipCommand(["pip3.8"], False),  # Target is set in the wrapper script as cmd line
+)
+
+
+def _local_package_installation_path(site: Site) -> Path:
+    return Path(f"{site.root}", "local/lib/python3")
+
+
+def assert_local_package_install_path(package_name: str,
+                                      local_package_installation_path: Path) -> None:
+    module = importlib.import_module(package_name)
+    assert module.__file__
+
+    # We only want to install into local as mkp will search there
+    assert local_package_installation_path in Path(module.__file__).parents
+
+
+def assert_install_package(cmd: List[str], package_name: str, site: Site) -> None:
+    p = site.execute(cmd, stdout=subprocess.PIPE)
+    install_stdout, _ = p.communicate()
+
+    assert "Successfully installed" in install_stdout, install_stdout
+    p = site.execute(["python3", "-c", f"import {package_name}"], stdout=subprocess.PIPE)
+    p.communicate()
+    assert p.returncode == 0
+
+
+def assert_uninstall_and_purge_cache(pip_cmd: PipCommand, package_name: str, site: Site) -> None:
+    for cmd in (
+            pip_cmd.command + ["uninstall", "-y", package_name],
+            pip_cmd.command + ["cache", "purge"],
+    ):
+        p = site.execute(cmd, stdout=subprocess.PIPE)
+        p.communicate()
+        assert p.returncode == 0
 
 
 def _get_import_names_from_dist_name(dist_name: str) -> List[str]:
@@ -88,38 +132,46 @@ def test_02_pip_path(site):
     assert path == "/omd/sites/%s/bin/pip3" % site.id
 
 
-def test_03_pip_interpreter_version(site):
-    p = site.execute(["python3", "-m", "pip", "-V"], stdout=subprocess.PIPE)
-    version = p.stdout.read()
+@pytest.mark.parametrize(
+    "pip_cmd",
+    SUPPORTED_PIP_CMDS,
+)
+def test_03_pip_interpreter_version(site: Site, pip_cmd):
+    p = site.execute(pip_cmd.command + ["-V"], stdout=subprocess.PIPE)
+    version = p.stdout.read() if p.stdout else "<NO STDOUT>"
     assert version.startswith("pip 22.0.4")
 
 
-@pytest.mark.parametrize(
-    "package_name",
-    [
-        # WARNING:
-        # Installing from source takes a long time - and so does the test.
-        # So be sure adding new packages here has a real benefit.
-        "ibm_db",  # Needed by check_sql: no wheels, so pip must be able to find the Python headers
-    ],
-)
-def test_04_pip_user_can_install_and_uninstall_packages_not_shipped_with_omd(site, package_name):
-    p = site.execute(
-        PIP_CMD + ["install", "--user", package_name],
-        stdout=subprocess.PIPE,
-    )
-    install_stdout = p.stdout.read()
+def test_04_pip_user_can_install_non_wheel_packages(site):
+    # ibm_db must be compiled from source, so choosing this also ensures that the Python headers
+    # can be found by pip
+    package_name = "ibm_db"
 
-    assert "Successfully installed" in install_stdout, install_stdout
+    # We're testing this only for one supported pip command as building from source just takes too long
+    pip_cmd = PipCommand(["pip3"], False)
 
-    for cmd in (
-        ["python3", "-c", f"import {package_name}"],
-            PIP_CMD + ["uninstall", "-y", package_name],
-            PIP_CMD + ["cache", "purge"],
-    ):
-        p = site.execute(cmd, stdout=subprocess.PIPE)
-        p.communicate()
-        assert p.returncode == 0
+    assert_install_package(pip_cmd.command + ["install", package_name], package_name, site)
+    assert_local_package_install_path(package_name, _local_package_installation_path(site))
+    assert_uninstall_and_purge_cache(pip_cmd, package_name, site)
+
+
+@pytest.mark.parametrize("pip_cmd", SUPPORTED_PIP_CMDS)
+def test_05_pip_user_can_install_wheel_packages(site: Site, pip_cmd: PipCommand):
+    # We're using here another package which is needed for check_sql but not deployed by us
+    package_name = "cx_Oracle"
+    if pip_cmd.needs_target_as_commandline:
+        command = pip_cmd.command + [
+            "install",
+            "--target",
+            str(_local_package_installation_path(site)),
+            package_name,
+        ]
+    else:
+        command = pip_cmd.command + ["install", package_name]
+
+    assert_install_package(command, package_name, site)
+    assert_local_package_install_path(package_name, _local_package_installation_path(site))
+    assert_uninstall_and_purge_cache(pip_cmd, package_name, site)
 
 
 @pytest.mark.parametrize("module_name", _get_import_names_from_pipfile())
@@ -130,7 +182,6 @@ def test_python_modules(site, module_name):
     # E       AttributeError: 'Context' object has no attribute 'pattern'
     if module_name in ("jsonschema", "openapi_spec_validator"):
         return
-    import importlib  # pylint: disable=import-outside-toplevel
     module = importlib.import_module(module_name)
 
     # Skip namespace modules, they don't have __file__

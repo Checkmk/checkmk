602063a81ed29bdceabdfe8e45e1cd013a54c5dd
Lisa Pichler <lisa.pichler@tribe29.com>
2023-03-08 15:47:20 +0100

    15442 FIX df_netscaler: wrong unit shown in graphs
    SUP-13557
    
    Change-Id: I93df415a308786e122783a29cbf9955e1be51533

--- cmk/gui/plugins/metrics/translation.py
+++ cmk/gui/plugins/metrics/translation.py
@@ -475,6 +475,7 @@ check_metrics["check_mk-nimble_volumes"] = df_translation
 check_metrics["check_mk-ceph_df"] = df_translation
 check_metrics["check_mk-kube_pvc"] = df_translation
 check_metrics["check_mk-lvm_vgs"] = df_translation
+check_metrics["check_mk-df_netscaler"] = df_translation
 check_metrics["check_mk-sansymphony_pool"] = {
     **df_translation,
     "percent_allocated": {"name": "fs_used_percent"},

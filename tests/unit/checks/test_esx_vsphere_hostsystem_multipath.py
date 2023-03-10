8ec82882464b0199502c27729dea0b026e6780fe
Lisa Pichler <lisa.pichler@tribe29.com>
2023-03-10 13:31:25 +0100

    esx_vsphere_hostsystem: add actual unit tests, remove legacy tests
    SUP-13533
    
    Change-Id: If3bf3b22a4ccc8af361169c2a79dbc6e987a8a22

--- tests/unit/checks/test_esx_vsphere_hostsystem_multipath.py
+++ tests/unit/checks/test_esx_vsphere_hostsystem_multipath.py
@@ -6,11 +6,13 @@
 
 import pytest
 
-from tests.testlib import Check
+from tests.unit.conftest import FixRegister
 
+from cmk.utils.type_defs import CheckPluginName
+
+from cmk.base.api.agent_based.checking_classes import CheckResult
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, State
 from cmk.base.plugins.agent_based.utils.esx_vsphere import Section
-
-pytestmark = pytest.mark.checks
 
 
 @pytest.mark.parametrize(
@@ -48,8 +50,11 @@ pytestmark = pytest.mark.checks
             ),
             "bla2:A0:B2:C21",
             [
-                0,
-                "0 active, 0 dead, 0 disabled, 0 standby, 0 unknown\nIncluded Paths:\nactive",
+                Result(
+                    state=State.OK,
+                    summary="0 active, 0 dead, 0 disabled, 0 standby, 0 unknown",
+                    details="0 active, 0 dead, 0 disabled, 0 standby, 0 unknown\nIncluded Paths:\nactive",
+                ),
             ],
         ),
         (
@@ -66,6 +71,8 @@ pytestmark = pytest.mark.checks
         ),
     ],
 )
-def test_check_esx_vsphere_hostsystem_multipath(section, item, check_results):
-    check = Check("esx_vsphere_hostsystem.multipath")
-    assert list(check.run_check(item, {}, section) or ()) == check_results
+def test_check_esx_vsphere_hostsystem_multipath(
+    fix_register: FixRegister, section: Section, item: str, check_results: CheckResult
+) -> None:
+    check = fix_register.check_plugins[CheckPluginName("esx_vsphere_hostsystem_multipath")]
+    assert list(check.check_function(item=item, params={}, section=section)) == check_results

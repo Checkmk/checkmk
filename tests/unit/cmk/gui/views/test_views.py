71aa2cdcb8ef420aca163bf7d795b7735a84a7f7
Ronny Bruska <ronny.bruska@tribe29.com>
2023-03-11 10:30:53 +0100

    Fix resilience unit test
    Change-Id: I0620ee7d691908f7b73ecc8908c0d0f7a6ca8589

--- tests/unit/cmk/gui/views/test_views.py
+++ tests/unit/cmk/gui/views/test_views.py
@@ -2576,6 +2576,9 @@ def test_get_inventory_display_hint() -> None:
 def test_view_page(
     logged_in_admin_wsgi_app: WebTestAppForCMK, mock_livestatus: MockLiveStatusConnection
 ) -> None:
+    if not cmk_version.is_raw_edition():
+        # needed for visuals.load()
+        cmk.utils.paths.local_reports_dir.mkdir(parents=True, exist_ok=True)
     wsgi_app = logged_in_admin_wsgi_app
 
     def _prepend(prefix, dict_):

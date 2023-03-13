d352a62a3c837ecb28ad9ed9ea5642ee4d9c2978
Sofia Colakovic <sofia.colakovic@tribe29.com>
2023-03-13 11:35:05 +0100

    FIX 15457 agent-receiver: Update old symlinks to fix crash after omd cp
    With the werk 14707, the cause of the the crash was removed, but the sites
    what have hosts registered with the version before 2.1.0p19 still experienced
    the same problem.
    
    With this werk, the old symlinks are updated and the crash no longer appears on
    any of the sites.
    
    CMK-12525
    
    Change-Id: I4ac0c93a70f1d2adbac9e95c1cc6a41620e29fbf

---
+++ cmk/update_config/plugins/actions/agent_registration.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from logging import Logger
+
+from cmk.utils.agent_registration import get_uuid_link_manager
+
+from cmk.update_config.registry import update_action_registry, UpdateAction
+from cmk.update_config.update_state import UpdateActionState
+
+
+class AgentRegistration(UpdateAction):
+    """Change absolute paths in registered hosts symlinks in var/agent-receiver/received-outputs to relative"""
+
+    def __call__(self, logger: Logger, update_action_state: UpdateActionState) -> None:
+        link_manager = get_uuid_link_manager()
+        for link in link_manager:
+            if link.target.is_absolute():
+                link.unlink()
+                link_manager.create_link(link.hostname, link.uuid, push_configured=False)
+
+
+update_action_registry.register(
+    AgentRegistration(
+        name="fix_agent_registration_symlinks",
+        title="Change absolute paths in registered hosts symlinks to relative",
+        sort_index=100,  # can run whenever
+    )
+)
---
+++ tests/unit/cmk/update_config/plugins/actions/test_agent_registration.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import logging
+from pathlib import Path
+
+import cmk.utils.paths
+from cmk.utils.agent_registration import get_uuid_link_manager
+
+from cmk.update_config.plugins.actions.agent_registration import AgentRegistration
+
+
+def test_fix_agent_receiver_symlinks() -> None:
+    source = Path(cmk.utils.paths.received_outputs_dir, "f0ca2e5d-06ea-4b04-89d1-67df0203d449")
+    source.parent.mkdir(parents=True, exist_ok=True)
+
+    target = Path(cmk.utils.paths.data_source_push_agent_dir, "my_new_host")
+    source.symlink_to(target)
+
+    link_manager = get_uuid_link_manager()
+    links = list(link_manager)
+
+    assert len(links) == 1
+    assert links[0].source == source
+    assert links[0].target == target
+
+    run_update = AgentRegistration(
+        name="fix_agent_registration_symlinks",
+        title="Change absolute paths in registered hosts symlinks to relative",
+        sort_index=100,  # can run whenever
+    )
+    run_update(logging.getLogger(), {})
+
+    links = list(link_manager)
+    assert len(links) == 1
+    assert links[0].source == source
+    assert links[0].target == Path(
+        "../../../../tmp/check_mk/data_source_cache/push-agent/inactive/my_new_host"
+    )

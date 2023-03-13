db292d98233edf952eefc9ecfdc405cc2f018e16
Solomon Jacobs <solomon.jacobs@tribe29.com>
2023-03-13 10:51:46 +0100

    gcp_status: remove time dependence
    The check only shows the incidents, which are currently affected,
    and therefore they should always be open. Thus, we can omit the check.
    We could have considered adding incidents, which are inactive. However,
    The user can use the Alert Statistics to see incidents, which affected
    them in the past. Moreover, the URI of the incident provides an
    overview of each incident and its resolution.
    
    Moreover, showing locations, if everything is grouped by location
    does not make a lot of sense. Thus, we omit this information also.
    
    Change-Id: If6c9621a5eeb2dbd89fb51d57492aeaa27219eb7

--- cmk/base/plugins/agent_based/gcp_status.py
+++ cmk/base/plugins/agent_based/gcp_status.py
@@ -4,14 +4,14 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 # mypy: disallow_untyped_defs
 
-import datetime
-from collections.abc import Sequence
+import dataclasses
+from collections.abc import Mapping, Sequence
 
 import pydantic
 
 from cmk.utils import gcp_constants  # pylint: disable=cmk-module-layer-violation
 
-from cmk.base.plugins.agent_based.agent_based_api.v1 import register, render, Result, Service, State
+from cmk.base.plugins.agent_based.agent_based_api.v1 import register, Result, Service, State
 from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
     CheckResult,
     DiscoveryResult,
@@ -19,10 +19,6 @@ from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
 )
 
 BASE_URL = "https://status.cloud.google.com/"
-_IGNORE_ENTRIES_OLDER_THAN = datetime.timedelta(days=3)  # Product-Management decision
-_DISPLAYED_AGE = render.timespan(_IGNORE_ENTRIES_OLDER_THAN.total_seconds()).removesuffix(
-    " 0 hours"
-)
 
 
 class DiscoveryParam(pydantic.BaseModel):
@@ -46,20 +42,11 @@ class AffectedLocation(pydantic.BaseModel):
 class Incident(pydantic.BaseModel):
     affected_products: Sequence[AffectedProduct]
     currently_affected_locations: Sequence[AffectedLocation]
-    end: datetime.datetime | None
     external_desc: str
     uri: str
 
-    def region_ids(self) -> list[str]:
-        return [l.id_ for l in self.currently_affected_locations]
 
-    def in_reference(self, smallest_accepted_time: datetime.datetime) -> bool:
-        # checking the last known status might not work because it was not set to available.
-        # for truly on going incidents the end marker is not existing.
-        return self.end is None or smallest_accepted_time < self.end
-
-
-class Section(pydantic.BaseModel):
+class AgentOutput(pydantic.BaseModel):
     discovery_param: DiscoveryParam
     # health_info corresponds to gcp json scheme provided here:
     # https://status.cloud.google.com/incidents.schema.json
@@ -67,33 +54,35 @@ class Section(pydantic.BaseModel):
     health_info: Sequence[Incident]
 
 
+@dataclasses.dataclass
+class Section:
+    discovery_param: DiscoveryParam
+    data: Mapping[str, Sequence[Incident]]
+
+
 def parse(string_table: StringTable) -> Section:
-    return Section.parse_raw(string_table[0][0])
+    output = AgentOutput.parse_raw(string_table[0][0])
+    data: dict[str, list[Incident]] = {}
+    for incident in output.health_info:
+        for location in incident.currently_affected_locations:
+            data.setdefault(gcp_constants.RegionMap[location.id_], []).append(incident)
+    return Section(discovery_param=output.discovery_param, data=data)
 
 
 register.agent_section(name="gcp_status", parse_function=parse)
 
 
 def check(item: str, section: Section) -> CheckResult:
-    smallest_accepted_time = datetime.datetime.now(datetime.UTC) - _IGNORE_ENTRIES_OLDER_THAN
-    relevant_incidents = [
-        incident
-        for incident in section.health_info
-        if incident.in_reference(smallest_accepted_time) and item in incident.region_ids()
-    ]
+    relevant_incidents = section.data.get(item, [])
     for incident in relevant_incidents:
         product_description = "Products: " + ", ".join(p.title for p in incident.affected_products)
-        location_description = "Locations: " + ", ".join(incident.region_ids())
         yield Result(
             state=State.CRIT,
             summary=incident.external_desc.replace("\n", ""),
-            details=f"{product_description} \n {location_description} \n {BASE_URL}{incident.uri}",
+            details=f"{product_description} \n {BASE_URL}{incident.uri}",
         )
     if not relevant_incidents:
-        yield Result(
-            state=State.OK,
-            summary=f"No known incident in the past {_DISPLAYED_AGE} {BASE_URL}",
-        )
+        yield Result(state=State.OK, summary=f"No known issues. Details: {BASE_URL}")
 
 
 def discovery(section: Section) -> DiscoveryResult:

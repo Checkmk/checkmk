a4c5b37969b450cd1e8ba73f9b2d342f6cd88c2b
Hannes Rantzsch <hannes.rantzsch@tribe29.com>
2023-03-21 10:40:31 +0100

    Add a note about a seemingly flaky test
    Change-Id: Ia99d1bee44721576a49d9dfd2109fe5317c8bd4d

--- tests/unit/cmk/utils/crypto/test_certificate.py
+++ tests/unit/cmk/utils/crypto/test_certificate.py
@@ -130,6 +130,12 @@ def test_serialize_rsa_key(tmp_path: Path) -> None:
     assert pem_enc.bytes.startswith(b"-----BEGIN ENCRYPTED PRIVATE KEY-----")
 
     with pytest.raises(WrongPasswordError):
+        # Note: we suspect this test might be flaky; as of 2023-03-21 we once saw an InvalidPEMError
+        # instead of the WrongPasswordError here. It seems cryptography gets a wrong error reason
+        # from openssl in backends.openssl.backend.py:_handle_key_loading_error().
+        # It uses openssl ERR_get_error (see `man 3 ERR_get_error`) for this -- maybe it gets the
+        # error from another test running in parallel instead? Spend more time investigating if this
+        # happens more often.
         RsaPrivateKey.load_pem(pem_enc, Password("wrong"))
 
     loaded_enc = RsaPrivateKey.load_pem(pem_enc, Password("verysecure"))

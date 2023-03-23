e6eda39df3c304ea9b8e7d8129fa4696eb562528
Moritz Kiemer <moritz.kiemer@tribe29.com>
2023-03-23 21:53:39 +0100

    move code for easier testing
    Change-Id: I1d57a8e543b679c0da547c61067aca4cfdaa25f5

--- tests/unit/checks/test_f5_bigip_conns.py
+++ tests/unit/checks/test_f5_bigip_conns.py
@@ -5,7 +5,7 @@
 
 import pytest
 
-from tests.testlib import Check
+from cmk.base.check_legacy_includes.f5_bigip import get_conn_rate_params
 
 pytestmark = pytest.mark.checks
 
@@ -52,8 +52,7 @@ pytestmark = pytest.mark.checks
     ],
 )
 def test_get_conn_rate_params(config, result) -> None:  # type: ignore[no-untyped-def]
-    check = Check("f5_bigip_conns")
-    assert check.context["get_conn_rate_params"](config) == result
+    assert get_conn_rate_params(config) == result
 
 
 @pytest.mark.parametrize(
@@ -81,6 +80,5 @@ def test_get_conn_rate_params(config, result) -> None:  # type: ignore[no-untype
 def test_get_conn_rate_params_exception(  # type: ignore[no-untyped-def]
     config, exception_msg
 ) -> None:
-    check = Check("f5_bigip_conns")
     with pytest.raises(ValueError, match=exception_msg):
-        check.context["get_conn_rate_params"](config)
+        get_conn_rate_params(config)

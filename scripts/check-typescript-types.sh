525fb75217d2272e272ad3048151fbe961550316
Mazen Alkatlabee <mazen.alkatlabee@tribe29.com>
2023-03-23 11:24:43 +0100

    add strict type check test for typescript
    Change-Id: I24e45036ca5a11025a1117a62cddd62716ae53f6

--- scripts/check-typescript-types.sh
+++ scripts/check-typescript-types.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-
-npx --no tsc --noEmit
+set -e
+echo "Check typescript with tsconfig.json"
+npx --no tsc
+echo "Check strict typescript with tsconfig.strict.json"
+npx --no tsc -- --project tsconfig.strict.json

31c8b4a105d57389b7e91122988db43471ff2c5c
Matteo Stifano <matteo.stifano@tribe29.com>
2023-03-24 10:58:45 +0100

    Modify test-update.groovy to use the daily build
    Our aim is to make test-update run once a day with the daily build as
    target-version. To do so, we adapt the groovy file accordingly.
    
    Change-Id: Iff70208e345dcf0de7da07fd7de00229c0564db4

--- buildscripts/scripts/test-update.groovy
+++ buildscripts/scripts/test-update.groovy
@@ -5,7 +5,6 @@
 def main() {
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
     def testing_helper = load("${checkout_dir}/buildscripts/scripts/utils/integration.groovy");
-    def branch_name = versioning.safe_branch_name(scm);
 
     check_environment_variables([
         "DOCKER_TAG",
@@ -15,14 +14,11 @@ def main() {
         DOCKER_GROUP_ID: get_docker_group_id(),
         DISTRO_LIST: ["ubuntu-20.04"],
         EDITION: "enterprise",
-        VERSION: "git",
-        DOCKER_TAG: versioning.select_docker_tag(
-            branch_name,
-            "",
-            ""),
+        VERSION: "daily",
+        DOCKER_TAG: "master-latest",
         MAKE_TARGET: "test-update-docker",
-        BRANCH: branch_name,
-        cmk_version: versioning.get_cmk_version(branch_name, "git"),
+        BRANCH: "master",
+        cmk_version: versioning.get_cmk_version("master", "daily"),
     );
 }
 return this;

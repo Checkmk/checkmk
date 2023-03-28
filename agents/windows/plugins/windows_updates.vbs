e894c2379af9e29213a895cc8a84e9b00a3f3fe2
Checkmk release system <feedback@checkmk.com>
2023-03-28 14:57:55 +0200

    Set version to 2.2.0b2

--- agents/windows/plugins/windows_updates.vbs
+++ agents/windows/plugins/windows_updates.vbs
@@ -21,7 +21,7 @@
 ' -----------------------------------------------------------------------------------------
 
 Option Explicit
-Const CMK_VERSION = "2.2.0b1"
+Const CMK_VERSION = "2.2.0b2"
 
 Dim fso
 Dim objStdout
--- cmk/special_agents/agent_vsphere.py
+++ cmk/special_agents/agent_vsphere.py
@@ -35,7 +35,7 @@ import cmk.special_agents.utils as utils
 #   |                                                                      |
 #   '----------------------------------------------------------------------'
 
-__version__ = "2.2.0b1"
+__version__ = "2.2.0b2"
 
 USER_AGENT = f"checkmk-special-vsphere-{__version__}"
 

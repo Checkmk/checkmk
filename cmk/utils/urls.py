4ec5d19a3a74ce3fec290cfbc514198f77698ea4
Sergey Kipnis <sergey.kipnis@tribe29.com>
2023-03-28 12:06:20 +0200

    change list on Sequence in function param
    Change-Id: Icd0e0916633be5d92bbb73dce8c002f3af4072c9

--- cmk/utils/urls.py
+++ cmk/utils/urls.py
@@ -4,11 +4,14 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import urllib.parse
+from typing import Sequence
 
 import cmk.utils.regex
 
 
-def is_allowed_url(url: str, cross_domain: bool = False, schemes: list[str] | None = None) -> bool:
+def is_allowed_url(
+    url: str, cross_domain: bool = False, schemes: Sequence[str] | None = None
+) -> bool:
     """Check if url is allowed
 
     >>> is_allowed_url("http://checkmk.com/")

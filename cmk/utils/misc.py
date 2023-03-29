b5862fa732adf034aa71cc4f178574f8b30c01b0
Andreas Umbreit <andreas.umbreit@tribe29.com>
2023-03-29 15:10:41 +0200

    15422 FIX Agent Bakery: New default UNIX agent folder permissions
    CMK-12673
    
    Change-Id: Ia8465847fe870082638134a3a19ac57fdd0373ab

--- cmk/utils/misc.py
+++ cmk/utils/misc.py
@@ -8,13 +8,11 @@ usable in all components of Check_MK
 Please try to find a better place for the things you want to put here."""
 
 import itertools
-import os
 import sys
 import time
 from collections.abc import Callable, Iterator, Mapping, MutableMapping, Sequence
-from contextlib import contextmanager
 from pathlib import Path
-from typing import Any, ParamSpec, TypeVar
+from typing import Any
 
 from cmk.utils.exceptions import MKGeneralException
 from cmk.utils.type_defs import HostAddress
@@ -111,30 +109,6 @@ def cachefile_age(path: Path | str) -> float:
         path = Path(path)
 
     return time.time() - path.stat().st_mtime
-
-
-P = ParamSpec("P")
-R = TypeVar("R")
-
-
-def with_umask(mask: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    def wrap(f: Callable[P, R]) -> Callable[P, R]:
-        def wrapped_f(*args: P.args, **kwargs: P.kwargs) -> R:
-            with umask(mask):
-                return f(*args, **kwargs)
-
-        return wrapped_f
-
-    return wrap
-
-
-@contextmanager
-def umask(mask: int) -> Iterator[None]:
-    old_mask = os.umask(mask)
-    try:
-        yield None
-    finally:
-        os.umask(old_mask)
 
 
 def normalize_ip_addresses(ip_addresses: str | Sequence[str]) -> list[HostAddress]:

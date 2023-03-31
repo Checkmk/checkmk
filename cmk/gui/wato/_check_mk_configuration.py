2ebcad8515bd12daf86237d7cbba83963bab03e4
Moritz Schlarb <schlarbm@uni-mainz.de>
2023-03-31 10:18:14 +0200

    Implement service state filter for quicksearch widget

--- cmk/gui/wato/_check_mk_configuration.py
+++ cmk/gui/wato/_check_mk_configuration.py
@@ -801,6 +801,7 @@ class ConfigVariableQuicksearchSearchOrder(ConfigVariable):
                             ("hg", _("Host group")),
                             ("sg", _("Service group")),
                             ("s", _("Service name")),
+                            ("st", _("Service state")),
                         ],
                     ),
                     DropdownChoice(

e57e23e0806da43bc98206d98f0639bddc3e4556
Simon Jess <simon.jess@tribe29.com>
2023-04-11 13:33:05 +0200

    licensing: Reduce monkeypatching
    Change-Id: I61845fc3518331fe7facfd2d500a40febd0bcb48

---
+++ tests/unit/cmk/utils/licensing/test_cre_registry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.utils.licensing.handler import LicensingHandler
+from cmk.utils.licensing.registry import licensing_handler_registry, register_cre_licensing_handler
+from cmk.utils.version import Edition
+
+
+def test_licensing_handler_registry_cre() -> None:
+    register_cre_licensing_handler()
+    handler_class = licensing_handler_registry[Edition.CRE]
+    assert handler_class.__name__ == "CRELicensingHandler"
+    assert issubclass(handler_class, LicensingHandler)

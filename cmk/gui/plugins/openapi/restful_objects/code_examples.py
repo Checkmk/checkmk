5ff1a6f4647d6a0617320b9727ddc341dd0580db
Gav <gavin.mcguigan@tribe29.com>
2023-04-14 12:02:09 +0200

    code_examples: curl examples with single quotes should be escaped
    When curl code examples include single quotes, they have to be
    escaped with '\' for the curl command to work.
    
    CMK-12693
    
    Change-Id: I53647863134509cdf9617b90554764400b91b95a

--- cmk/gui/plugins/openapi/restful_objects/code_examples.py
+++ cmk/gui/plugins/openapi/restful_objects/code_examples.py
@@ -167,6 +167,7 @@ out=$(
     --data '{{ request_schema |
             to_dict |
             to_json(indent=2, sort_keys=True) |
+            _escape_single_quotes |
             indent(skip_lines=1, spaces=8) }}' \\
 {%- endif %}
     "$API_URL{{ request_endpoint | fill_out_parameters }}")
@@ -575,12 +576,17 @@ def _jinja_environment() -> jinja2.Environment:
         to_json=json.dumps,
         to_python=yapf_format,
         repr=repr,
+        _escape_single_quotes=_escape_single_quotes,
     )
     # These objects will be available in the templates
     tmpl_env.globals.update(
         spec=SPEC,
     )
     return tmpl_env
+
+
+def _escape_single_quotes(text: str) -> str:
+    return text.replace("'", "'\\''")
 
 
 def to_param_dict(params: List[OpenAPIParameter]) -> Dict[str, OpenAPIParameter]:

aa4fca50b042125b81930ecd413c45b354801c32
Gav <gavin.mcguigan@tribe29.com>
2023-04-14 12:39:35 +0200

    rest-api-client: cleanup of auxtag client and tests
    Change-Id: Ice433efb97faf67dfe291a2cd3a2b67d81589b55

--- tests/unit/cmk/gui/plugins/openapi/test_openapi_auxtags.py
+++ tests/unit/cmk/gui/plugins/openapi/test_openapi_auxtags.py
@@ -4,97 +4,65 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from tests.testlib.rest_api_client import AuxTagTestClient, HostTagGroupTestClient, Response
+from typing import Any
+
+from tests.testlib.rest_api_client import AuxTagTestClient, HostTagGroupTestClient
 
 
 def test_create_auxtag_invalid_data(auxtag_client: AuxTagTestClient) -> None:
-    auxtag_client.create(
-        expect_ok=False,
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="",
-            topic="topic_1",
-            help="HELP",
-        ),
-    ).assert_status_code(400)
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data, expect_ok=False).assert_status_code(400)
 
-    auxtag_client.create(
-        expect_ok=False,
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title=None,
-            topic="topic_1",
-            help="HELP",
-        ),
-    ).assert_status_code(400)
+    test_data.update({"title": None})
+    auxtag_client.create(tag_data=test_data, expect_ok=False).assert_status_code(400)
 
-    auxtag_client.create(
-        expect_ok=False,
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="",
-            help="HELP",
-        ),
-    ).assert_status_code(400)
+    test_data.update({"title": "aux_tag_1", "topic": ""})
+    auxtag_client.create(tag_data=test_data, expect_ok=False).assert_status_code(400)
 
-    auxtag_client.create(
-        expect_ok=False,
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic=None,
-            help="HELP",
-        ),
-    ).assert_status_code(400)
+    test_data.update({"topic": None})
+    auxtag_client.create(tag_data=test_data, expect_ok=False).assert_status_code(400)
 
 
 def test_update_auxtag_invalid_data(auxtag_client: AuxTagTestClient) -> None:
-    auxtag_client.create(
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="topic_1",
-            help="HELP",
-        )
-    )
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "aux_tag_1",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data)
+
+    test_data.update({"title": ""})
     auxtag_client.edit(
-        expect_ok=False,
         aux_tag_id="aux_tag_id_1",
-        tag_data=auxtag_client.edit_model(
-            title="",
-            topic="topic_1",
-        ),
+        tag_data=test_data,
+        expect_ok=False,
     ).assert_status_code(400)
 
+    test_data.update({"title": "aux_tag_1", "topic": None})
     auxtag_client.edit(
-        expect_ok=False,
         aux_tag_id="aux_tag_id_1",
-        tag_data=auxtag_client.edit_model(
-            title=None,
-            topic="topic_1",
-            help="HELP",
-        ),
+        tag_data=test_data,
+        expect_ok=False,
     ).assert_status_code(400)
 
+    test_data.update({"topic": ""})
     auxtag_client.edit(
-        expect_ok=False,
         aux_tag_id="aux_tag_id_1",
-        tag_data=auxtag_client.edit_model(
-            title="aux_tag_1",
-            topic="",
-            help="HELP",
-        ),
+        tag_data=test_data,
+        expect_ok=False,
     ).assert_status_code(400)
 
+    test_data.update({"topic": None})
     auxtag_client.edit(
-        expect_ok=False,
         aux_tag_id="aux_tag_id_1",
-        tag_data=auxtag_client.edit_model(
-            title="aux_tag_1",
-            topic=None,
-            help="HELP",
-        ),
+        tag_data=test_data,
+        expect_ok=False,
     ).assert_status_code(400)
 
 
@@ -120,14 +88,13 @@ def test_get_builtin_auxtags(auxtag_client: AuxTagTestClient) -> None:
 
 
 def test_get_builtin_and_custom_auxtags(auxtag_client: AuxTagTestClient) -> None:
-    auxtag_client.create(
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="topic_1",
-            help="HELP",
-        )
-    )
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "aux_tag_1",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data)
 
     assert {t["id"] for t in auxtag_client.get_all().json["value"]} == {
         "aux_tag_id_1",
@@ -141,60 +108,52 @@ def test_get_builtin_and_custom_auxtags(auxtag_client: AuxTagTestClient) -> None
 
 
 def test_update_custom_aux_tag_title(auxtag_client: AuxTagTestClient) -> None:
-    auxtag_client.create(
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="topic_1",
-            help="HELP",
-        )
-    )
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "aux_tag_1",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data)
+
+    test_data.update({"title": "edited_title"})
+    test_data.pop("aux_tag_id")
+
     assert (
         auxtag_client.edit(
             aux_tag_id="aux_tag_id_1",
-            tag_data=auxtag_client.edit_model(
-                title="edited_title",
-                topic="topic_1",
-                help="HELP",
-            ),
+            tag_data=test_data,
         ).json["title"]
         == "edited_title"
     )
 
 
 def test_update_custom_aux_tag_topic_and_help(auxtag_client: AuxTagTestClient) -> None:
-    auxtag_client.create(
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="topic_1",
-            help="HELP",
-        )
-    )
-
-    response: Response = auxtag_client.edit(
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "aux_tag_1",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data)
+    test_data.update({"topic": "edited_topic", "help": "edited_help"})
+    test_data.pop("aux_tag_id")
+    resp = auxtag_client.edit(
         aux_tag_id="aux_tag_id_1",
-        tag_data=auxtag_client.edit_model(
-            title="edited_title",
-            topic="edited_topic",
-            help="edited_help",
-        ),
+        tag_data=test_data,
     )
-
-    assert response.json["extensions"]["topic"] == "edited_topic"
-    assert response.json["extensions"]["help"] == "edited_help"
+    assert resp.json["extensions"]["topic"] == "edited_topic"
+    assert resp.json["extensions"]["help"] == "edited_help"
 
 
 def test_delete_custom_aux_tag(auxtag_client: AuxTagTestClient) -> None:
-    auxtag_client.create(
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="topic_1",
-            help="HELP",
-        )
-    )
-
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "aux_tag_1",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data)
     auxtag_client.get(aux_tag_id="aux_tag_id_1")
     auxtag_client.delete(aux_tag_id="aux_tag_id_1").assert_status_code(status_code=204)
     auxtag_client.get(aux_tag_id="aux_tag_id_1", expect_ok=False).assert_status_code(
@@ -205,10 +164,7 @@ def test_delete_custom_aux_tag(auxtag_client: AuxTagTestClient) -> None:
 def test_edit_non_existing_aux_tag(auxtag_client: AuxTagTestClient) -> None:
     auxtag_client.edit(
         aux_tag_id="aux_tag_id_1",
-        tag_data=auxtag_client.edit_model(
-            title="edited_title",
-            topic="edited_topic",
-        ),
+        tag_data={},
         expect_ok=False,
         with_etag=False,
     ).assert_status_code(404)
@@ -217,14 +173,13 @@ def test_edit_non_existing_aux_tag(auxtag_client: AuxTagTestClient) -> None:
 def test_delete_tag_that_belongs_to_a_tag_group(
     auxtag_client: AuxTagTestClient, host_tag_group_client: HostTagGroupTestClient
 ) -> None:
-    auxtag_client.create(
-        tag_data=auxtag_client.create_model(
-            aux_tag_id="aux_tag_id_1",
-            title="aux_tag_1",
-            topic="topic_1",
-            help="HELP",
-        )
-    )
+    test_data: dict[str, Any] = {
+        "aux_tag_id": "aux_tag_id_1",
+        "title": "aux_tag_1",
+        "topic": "topic_1",
+        "help": "HELP",
+    }
+    auxtag_client.create(tag_data=test_data)
 
     host_tag_group_client.create(
         ident="tag_group_id_1",
@@ -258,13 +213,9 @@ def test_delete_tag_that_belongs_to_a_tag_group(
 def test_update_builtin_aux_tag(auxtag_client: AuxTagTestClient) -> None:
     r = auxtag_client.edit(
         aux_tag_id="ip-v4",
-        tag_data=auxtag_client.edit_model(
-            title="edited_title",
-            topic="edited_topic",
-            help="help",
-        ),
-        with_etag=False,
+        tag_data={},
         expect_ok=False,
+        with_etag=False,
     )
     r.assert_status_code(404)
     assert r.json["title"] == "Not Found"

55acdd776ec7d5c4c3bdf1e511c73e97913624b2
Sergey Kipnis <sergey.kipnis@tribe29.com>
2023-04-14 13:21:13 +0200

    fix systemtime description
    - supports not only Windows host, but ESX
    
    CMK-4881
    
    Change-Id: I20aa98ba5219c935fa63ee0693654f4f8f021dde

--- cmk/gui/plugins/wato/check_parameters/systemtime.py
+++ cmk/gui/plugins/wato/check_parameters/systemtime.py
@@ -36,6 +36,6 @@ rulespec_registry.register(
         check_group_name="systemtime",
         group=RulespecGroupCheckParametersOperatingSystem,
         parameter_valuespec=_parameter_valuespec_systemtime,
-        title=lambda: _("Windows system time offset"),
+        title=lambda: _("System time offset"),
     )
 )

9b5ec822eb5b68be71af08230402ccdc7b408729
Gav <gavin.mcguigan@tribe29.com>
2023-04-19 12:55:53 +0200

    test_automations: added the clients fixture
    The clients fixture was not available here.  Added new clients
    fixture to conftest to have access to the new client register.
    
    Change-Id: Ic8947f36d1c8f048e52e63cd4fa9d8539435b9f3

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -8,7 +8,7 @@ import pytest
 
 from tests.testlib.event_console import CMKEventConsole
 from tests.testlib.openapi_session import RequestSessionRequestHandler
-from tests.testlib.rest_api_client import RestApiClient
+from tests.testlib.rest_api_client import ClientRegistry, get_client_registry, RestApiClient
 from tests.testlib.site import get_site_factory, Site
 from tests.testlib.web_session import CMKWebSession
 
@@ -42,5 +42,14 @@ def rest_api_client(site: Site) -> RestApiClient:
     rq = RequestSessionRequestHandler()
     rq.set_credentials("cmkadmin", site.admin_password)
     return RestApiClient(
+        rq, f"{site.http_proto}://{site.http_address}:{site.apache_port}/{site.id}/check_mk/api/1.0"
+    )
+
+
+@pytest.fixture()
+def clients(site: Site) -> ClientRegistry:
+    rq = RequestSessionRequestHandler()
+    rq.set_credentials("cmkadmin", site.admin_password)
+    return get_client_registry(
         rq, f"{site.http_proto}://{site.http_address}:{site.apache_port}/{site.id}/check_mk/api/1.0"
     )

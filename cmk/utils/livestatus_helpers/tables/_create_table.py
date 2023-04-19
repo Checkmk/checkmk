6233dead2eb36a52bc23be4c0b8de8f4186c3bea
Kenneth Okoh <kenneth.okoh@tribe29.com>
2023-04-19 11:01:24 +0200

    Rebranding: Change license headers (XII)
    in cmk/utils/*
    CMK-12940
    
    Change-Id: Id784191642d65194f3d69acc2a12513a851bf8f5

--- cmk/utils/livestatus_helpers/tables/_create_table.py
+++ cmk/utils/livestatus_helpers/tables/_create_table.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 """Script to create livestatus table definition files.
@@ -34,7 +34,7 @@ from typing import Final
 import jinja2
 
 TABLE_FILE_TEMPLATE: Final = '''#!/usr/bin/env python3
-# Copyright (C) 2020 tribe29 GmbH - License: GNU General Public License v2
+# Copyright (C) 2020 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 from cmk.utils.livestatus_helpers.types import Column, Table

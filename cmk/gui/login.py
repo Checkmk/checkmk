507472aa36cd38f03c0518951d65c10dfb41d78c
Kenneth Okoh <kenneth.okoh@tribe29.com>
2023-04-19 08:45:02 +0200

    Rebranding: Change UI occurences of company name
    CMK-12951
    
    Change-Id: I3a59d5af75febc37816b12616fd0201e89192dc7

--- cmk/gui/login.py
+++ cmk/gui/login.py
@@ -303,7 +303,7 @@ class LoginPage(Page):
         footer.append(
             HTML(
                 "&copy; %s"
-                % HTMLWriter.render_a("tribe29 GmbH", href="https://tribe29.com", target="_blank")
+                % HTMLWriter.render_a("Checkmk GmbH", href="https://tribe29.com", target="_blank")
             )
         )
 

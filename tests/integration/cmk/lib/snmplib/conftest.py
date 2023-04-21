8c6e833c84f3613a59e57efef6f5966388d748f3
Lars Michelsen <lm@tribe29.com>
2023-04-21 10:49:31 +0200

    Make snmpsimd start in containerized environment
    Change-Id: I1ed56dff37be00d5add502766f2cd4d385227fdd

--- tests/integration/cmk/lib/snmplib/conftest.py
+++ tests/integration/cmk/lib/snmplib/conftest.py
@@ -7,11 +7,14 @@ from __future__ import annotations
 
 import logging
 import os
+import shutil
 import subprocess
 from collections.abc import Iterator
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import assert_never, NamedTuple
 
+import psutil
 import pytest
 from pysnmp.hlapi import (  # type: ignore[import]
     CommunityData,
@@ -23,7 +26,7 @@ from pysnmp.hlapi import (  # type: ignore[import]
     UdpTransportTarget,
 )
 
-from tests.testlib import is_enterprise_repo, wait_until
+from tests.testlib import is_enterprise_repo, repo_path, wait_until
 from tests.testlib.site import Site
 
 import cmk.utils.debug as debug
@@ -45,55 +48,78 @@ logger = logging.getLogger(__name__)
 
 
 class ProcessDef(NamedTuple):
+    with_sudo: bool
     port: int
     process: subprocess.Popen
 
 
-@pytest.fixture(name="snmp_data_dir", scope="module")
-def snmp_data_dir_fixture(request) -> Path:  # type:ignore[no-untyped-def]
-    return Path(request.fspath.dirname) / "snmp_data"
+@pytest.fixture(name="snmp_data_dir", scope="session")
+def snmp_data_dir_fixture() -> Path:
+    return Path(__file__).parent / "snmp_data"
 
 
-@pytest.fixture(name="snmpsim", scope="module", autouse=True)
-def snmpsim_fixture(
-    site: Site, snmp_data_dir: Path, tmp_path_factory: pytest.TempPathFactory
-) -> Iterator[None]:
-    tmp_path = tmp_path_factory.getbasetemp()
+@pytest.fixture(name="snmpsim", scope="session", autouse=True)
+def snmpsim_fixture(site: Site, snmp_data_dir: Path) -> Iterator[None]:
     log.logger.setLevel(logging.DEBUG)
     debug.enable()
 
-    process_definitions = [
-        _define_process(idx, auth, tmp_path, snmp_data_dir)
-        for idx, auth in enumerate(_create_auth_list())
-    ]
+    with_sudo = os.geteuid() == 0
 
-    for process_def in process_definitions:
-        wait_until(_create_listening_condition(process_def), timeout=20)
+    # In the CI the tests are started as root and snmpsimd needs to be started as
+    # "jenkins" user. We need to provide a tmp path which is writable by that user.
+    with TemporaryDirectory(prefix="snmpsim_") as d:
+        if with_sudo:
+            shutil.chown(d, "jenkins", "jenkins")
 
-    yield
+        process_definitions = [
+            _define_process(idx, auth, Path(d), snmp_data_dir, with_sudo)
+            for idx, auth in enumerate(_create_auth_list())
+        ]
 
-    log.logger.setLevel(logging.INFO)
-    debug.disable()
+        try:
+            for process_def in process_definitions:
+                wait_until(_create_listening_condition(process_def), timeout=20)
 
-    logger.debug("Stopping snmpsimd...")
-    for process_def in process_definitions:
-        process_def.process.terminate()
-        process_def.process.wait()
-    logger.debug("Stopped snmpsimd.")
+            yield
+        finally:
+            log.logger.setLevel(logging.INFO)
+            debug.disable()
+
+            logger.debug("Stopping snmpsimd...")
+            for process_def in process_definitions:
+                p = _snmpsimd_process(process_def)
+                if p:
+                    p.terminate()
+                process_def.process.wait()
+            logger.debug("Stopped snmpsimd.")
 
 
-def _define_process(index, auth, tmp_path, snmp_data_dir):
+def _define_process(index, auth, tmp_path, snmp_data_dir, with_sudo):
     port = 1337 + index
+
+    # The tests are executed as root user in the containerized environmen, which snmpsimd does not
+    # like. Switch the user context to the jenkins user to execute the daemon.
+    # When executed on a dev system, we run as lower privileged user and don't have to switch the
+    # context.
+    sudo = ["sudo", "-u", "jenkins"] if with_sudo else []
+
+    proc_tmp_path = tmp_path / f"snmpsim{index}"
+    proc_tmp_path.mkdir(parents=True, exist_ok=True)
+    if with_sudo:
+        shutil.chown(proc_tmp_path, "jenkins", "jenkins")
+
     return ProcessDef(
+        with_sudo=with_sudo,
         port=port,
         process=subprocess.Popen(
-            [
-                "snmpsimd.py",
+            sudo
+            + [
+                f"{repo_path()}/.venv/bin/snmpsimd.py",
                 "--log-level=error",
                 "--cache-dir",
                 # Each snmpsim instance needs an own cache directory otherwise
                 # some instances occasionally crash
-                str(tmp_path / "snmpsim%s") % index,
+                str(proc_tmp_path),
                 # Explicitly set engine ID to make data-dir override builtin dirs
                 # See https://snmplabs.thola.io/snmpsim/documentation/simulating-agents.html
                 "--v3-engine-id=010203040505060809",
@@ -161,13 +187,18 @@ def _is_listening(process_def: ProcessDef) -> bool:
     exitcode = p.poll()
     snmpsimd_died = exitcode is not None
 
+    process = _snmpsimd_process(process_def)
+    if process is None:
+        return False
+    pid = process.pid
+
     if not snmpsimd_died:
         # Wait for snmpsimd to initialize the UDP sockets
         num_sockets = 0
         try:
-            for e in os.listdir("/proc/%d/fd" % p.pid):
+            for e in os.listdir("/proc/%d/fd" % pid):
                 try:
-                    if os.readlink("/proc/%d/fd/%s" % (p.pid, e)).startswith("socket:"):
+                    if os.readlink("/proc/%d/fd/%s" % (pid, e)).startswith("socket:"):
                         num_sockets += 1
                 except OSError:
                     pass
@@ -186,7 +217,6 @@ def _is_listening(process_def: ProcessDef) -> bool:
 
     # We got the expected number of listen sockets. One for IPv4 and one for IPv6. Now test
     # whether or not snmpsimd is already answering.
-
     g = getCmd(
         SnmpEngine(),
         CommunityData("public"),
@@ -194,8 +224,24 @@ def _is_listening(process_def: ProcessDef) -> bool:
         ContextData(),
         ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
     )
-    assert len(list(g)) == 1
+    _error_indication, _error_status, _error_index, var_binds = next(g)
+    assert len(var_binds) == 1
+    assert (
+        var_binds[0][1].prettyPrint()
+        == "Linux zeus 4.8.6.5-smp #2 SMP Sun Nov 13 14:58:11 CDT 2016 i686"
+    )
     return True
+
+
+def _snmpsimd_process(process_def: ProcessDef) -> psutil.Process | None:
+    if process_def.with_sudo:
+        proc = psutil.Process(process_def.process.pid)
+        if not (children := proc.children()):
+            return None
+        if children[0].name() != "snmpsimd.py":
+            raise RuntimeError(f"Unexpected child processes: {children}")
+        return children[0]
+    return psutil.Process(process_def.process.pid)
 
 
 @pytest.fixture(name="backend", params=SNMPBackendEnum)

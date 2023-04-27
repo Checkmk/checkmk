b5026a9a914126ff8af217a596f87a068bbc91be
Kenneth Okoh <kenneth.okoh@tribe29.com>
2023-04-27 11:08:19 +0200

    Rebranding: Change license headers (VII)
    agents/*
    CMK-12940
    
    Change-Id: Ic4d9b85fb63be7368d07cb03968cc5977d22ef6d

--- agents/scripts/cmk-agent-useradd.sh
+++ agents/scripts/cmk-agent-useradd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2019 tribe29 GmbH - License: Checkmk Enterprise License
+# Copyright (C) 2019 Checkmk GmbH - License: Checkmk Enterprise License
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 

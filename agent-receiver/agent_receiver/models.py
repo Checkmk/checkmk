84bcc12a7e873801f05b4d791ba2e3d26e72fd98
Hannes Rantzsch <hannes.rantzsch@tribe29.com>
2023-05-02 13:29:42 +0200

    15190 FIX Allow agent registration using an IP address as hostname
    IP addresses can now be used as hostnames again when registering new
    agents with the Agent Controller.
    
    This fixes a regression that was introduced with Checkmk 2.1.0p12 /
    Werk #14385.
    
    Change-Id: I411ec9b2de3f98ad40164459ad7c1c6fa63da61d

--- agent-receiver/agent_receiver/models.py
+++ agent-receiver/agent_receiver/models.py
@@ -6,6 +6,7 @@
 from __future__ import annotations
 
 import re
+import socket
 from collections.abc import Callable, Iterator, Mapping
 from dataclasses import dataclass
 from enum import Enum
@@ -64,10 +65,10 @@ class RenewCertResponse(BaseModel, frozen=True):
     agent_cert: str
 
 
-# duplicated from cmk.gui.valuespec
 def _is_valid_host_name(hostname: str) -> bool:
+    # duplicated from cmk.gui.valuespec
     # http://stackoverflow.com/questions/2532053/validate-a-hostname-string/2532344#2532344
-    if len(hostname) > 255:
+    if not hostname or len(hostname) > 255:
         return False
 
     if hostname[-1] == ".":
@@ -83,6 +84,54 @@ def _is_valid_host_name(hostname: str) -> bool:
     return all(allowed.match(x) for x in hostname.split("."))
 
 
+def _is_valid_ipv4_address(address: str) -> bool:
+    # duplicated from cmk.gui.valuespec
+    # http://stackoverflow.com/questions/319279/how-to-validate-ip-address-in-python/4017219#4017219
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+
+        return address.count(".") == 3
+
+    except socket.error:  # not a valid address
+        return False
+
+    return True
+
+
+def _is_valid_ipv6_address(address: str) -> bool:
+    # duplicated from cmk.gui.valuespec
+    # http://stackoverflow.com/questions/319279/how-to-validate-ip-address-in-python/4017219#4017219
+    try:
+        address = address.split("%")[0]
+        socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:  # not a valid address
+        return False
+    return True
+
+
+def _is_valid_hostname_or_ip(uri: str) -> bool:
+    """Check if the given URI is a valid hostname or IP address
+
+    Some examples (see unit tests for more):
+    >>> _is_valid_hostname_or_ip("test.checkmk.com")
+    True
+    >>> _is_valid_hostname_or_ip("127.0.0.1")
+    True
+    >>> _is_valid_hostname_or_ip("2606:2800:220:1:248:1893:25c8:1946")
+    True
+    >>> _is_valid_hostname_or_ip("::1")
+    True
+    >>> _is_valid_hostname_or_ip("my/../host")
+    False
+    """
+    return _is_valid_host_name(uri) or _is_valid_ipv4_address(uri) or _is_valid_ipv6_address(uri)
+
+
 class RegistrationWithHNBody(BaseModel, frozen=True):
     uuid: UUID4
     host_name: str
@@ -90,7 +139,7 @@ class RegistrationWithHNBody(BaseModel, frozen=True):
     @validator("host_name")
     @classmethod
     def valid_hostname(cls, v):
-        if not _is_valid_host_name(v):
+        if not _is_valid_hostname_or_ip(v):
             raise HTTPException(
                 status_code=400,
                 detail=f"Invalid hostname: '{v}'",
@@ -106,7 +155,7 @@ class RegisterExistingBody(RegistrationWithHNBody):
     @validator("host_name")
     @classmethod
     def valid_hostname(cls, v):
-        if not _is_valid_host_name(v):
+        if not _is_valid_hostname_or_ip(v):
             raise HTTPException(
                 status_code=400,
                 detail=f"Invalid hostname: '{v}'",

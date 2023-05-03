f67967b775fb534a165646dce42e5ad923be662e
Sofia Colakovic <sofia.colakovic@checkmk.com>
2023-05-03 16:06:51 +0200

    15469 FIX aws_elb: Fix crash in AWS/ELB Statistics
    AWS/ELB Statistics check crashed during checking with error:
    C+:
    TypeError (invalid character(s) in metric name: ' ')
    C-:
    
    CMK-13099
    
    Change-Id: Ia891ab11c212d19051b0b3fa8d8bd5e912a4977f

---
+++ tests/unit/checks/test_aws_elb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from tests.unit.conftest import FixRegister
+
+from cmk.checkers.checking import CheckPluginName
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import CheckResult, StringTable
+
+
+@pytest.mark.parametrize(
+    "string_table, expected_result",
+    [
+        (
+            {
+                "RequestCount": 693.235,
+                "SurgeQueueLength": 1024.0,
+                "SpilloverCount": 0.058333333333333334,
+                "Latency": 4.2748083637903225e-06,
+                "HealthyHostCount": 1.8,
+                "UnHealthyHostCount": 0.0,
+                "BackendConnectionErrors": 0.058333333333333334,
+            },
+            [
+                Result(state=State.OK, summary="Surge queue length: 1024"),
+                Metric("aws_surge_queue_length", 1024.0),
+                Result(state=State.OK, summary="Spillover: 0.058/s"),
+                Metric("aws_spillover", 0.058333333333333334),
+            ],
+        )
+    ],
+)
+def test_check_aws_elb_statistics(
+    fix_register: FixRegister, string_table: StringTable, expected_result: CheckResult
+) -> None:
+    check_plugin = fix_register.check_plugins[CheckPluginName("aws_elb")]
+    result = list(check_plugin.check_function(item=None, params={}, section=string_table))
+    assert result == expected_result

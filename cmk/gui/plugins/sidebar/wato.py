ae6cda24fc4dc44444ef15a689abab1316ff4b92
Ronny Bruska <ronny.bruska@checkmk.com>
2023-05-03 16:42:47 +0200

    15487 FIX Fix indentation of tree of folders snapin
    SUP-13984
    
    Change-Id: I973fd2776aad5ecf17950799baa0d1a3a104c9d8

--- cmk/gui/plugins/sidebar/wato.py
+++ cmk/gui/plugins/sidebar/wato.py
@@ -285,7 +285,7 @@ def render_tree_folder(tree_id, folder, js_func):
             for subfolder in sorted(subfolders, key=lambda x: x["title"].lower()):
                 render_tree_folder(tree_id, subfolder, js_func)
     else:
-        html.li(title)
+        html.li(title, class_="single")
 
     html.close_ul()
 

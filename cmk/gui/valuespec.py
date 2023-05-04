5c736d76fdeddd604461affcf3fec44edff96bca
Lisa Pichler <lisa.pichler@checkmk.com>
2023-05-04 11:16:54 +0200

    SAML: improve Checkmk server URL validation & inline help
    CMK-12943
    
    Change-Id: I299ea39559825223365604a0d7e35bdf538ccf0f

--- cmk/gui/valuespec.py
+++ cmk/gui/valuespec.py
@@ -1567,6 +1567,7 @@ def HTTPSUrl(  # pylint: disable=redefined-builtin
     title: str | None = None,
     help: ValueSpecHelp | None = None,
     default_value: ValueSpecDefault[str] = DEF_VALUE,
+    validate: ValueSpecValidateFunc[str] | None = None,
 ) -> Url:
     """Valuespec for a HTTPS Url, that automatically adds https:// to the value if no scheme has been specified"""
     return Url(
@@ -1580,6 +1581,7 @@ def HTTPSUrl(  # pylint: disable=redefined-builtin
         title=title,
         help=help,
         default_value=default_value,
+        validate=validate,
     )
 
 

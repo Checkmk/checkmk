69ebbffc471244d14ffdf8a78a1e4036ccb06b44
Benedikt Seidl <benedikt.seidl@checkmk.com>
2023-05-04 11:42:53 +0200

    Simplify code to create ChangeLog
    We no longer have to try to read ChangeLog.in as it was removed with
    Change-Id: Ifa2b17349e4a4665fb3bc10d9b4c033ccdcf03a2
    
    Reduced use of functions, as the main code is quite short and readable
    even without additional functions.
    
    Change-Id: I185efe11398e38554345b8bd4fedcf6319cb6beb

--- cmk/utils/werks/__main__.py
+++ cmk/utils/werks/__main__.py
@@ -4,36 +4,18 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import argparse
-from collections.abc import Sequence
 from pathlib import Path
 
 from . import load_precompiled_werks_file, Werk, write_as_text
 
 
 def main_changelog(args: argparse.Namespace) -> None:
-    create_changelog(args.destination, [Path(p) for p in args.precompiled_werk])
-
-
-def create_changelog(dest_file_path: str, precompiled_werk_files: Sequence[Path]) -> None:
-    werks = load_werks(precompiled_werk_files)
-
-    with open(dest_file_path, "w", encoding="utf-8") as f:
-        write_as_text(werks, f)
-
-        # TODO: ChangeLog.in was removed with
-        # Change-Id: Ifa2b17349e4a4665fb3bc10d9b4c033ccdcf03a2
-        # Append previous werk changes
-        p = Path(dest_file_path + ".in")
-        if p.exists():
-            f.write("\n\n")
-            f.write(p.read_text())
-
-
-def load_werks(precompiled_werk_files: Sequence[Path]) -> dict[int, Werk]:
     werks: dict[int, Werk] = {}
-    for path in precompiled_werk_files:
+    for path in (Path(p) for p in args.precompiled_werk):
         werks.update(load_precompiled_werks_file(path))
-    return werks
+
+    with open(args.destination, "w", encoding="utf-8") as f:
+        write_as_text(werks, f)
 
 
 def parse_arguments() -> argparse.Namespace:

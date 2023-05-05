2f420745cc27ba18668133cf9b042472758682a7
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-05-05 15:26:59 +0200

    ipmi: compile user defined level in advance
    Change-Id: I15ccb5f622ca50dcc3fafafa5eb1b09791dd8b45

--- cmk/base/plugins/agent_based/utils/ipmi.py
+++ cmk/base/plugins/agent_based/utils/ipmi.py
@@ -39,6 +39,12 @@ IgnoreParams = Mapping[str, Sequence[str]]
 StatusTxtMapping = Callable[[str], State]
 
 
+@dataclass(frozen=True)
+class UserLevels:
+    upper: Optional[Tuple[float, float]]
+    lower: Optional[Tuple[float, float]]
+
+
 class DiscoveryParams(TypedDict):
     discovery_mode: Union[Tuple[Literal["summarize"], IgnoreParams], Tuple[Literal["single"],
                                                                            IgnoreParams],]
@@ -102,22 +108,13 @@ def _unit_to_render_func(unit: str) -> Callable[[float], str]:
     return lambda x: ("%.2f" + unit_suffix) % x
 
 
-def _check_numerical_levels(
-    sensor_name: str,
-    val: float,
-    params: Mapping[str, Any],
-    unit: str,
-) -> type_defs.CheckResult:
-    for this_sensorname, levels in params.get("numerical_sensor_levels", []):
-        if this_sensorname == sensor_name and levels:
-            yield from check_levels(
-                val,
-                levels_upper=levels.get("upper"),
-                levels_lower=levels.get("lower"),
-                render_func=_unit_to_render_func(unit),
-                label=sensor_name,
-            )
-            break
+def _compile_user_levels_map(params: Mapping[str, Any]) -> Mapping[str, UserLevels]:
+    return {
+        sensorname: UserLevels(
+            upper=levels.get("upper"),
+            lower=levels.get("lower"),
+        ) for sensorname, levels in reversed(params.get("numerical_sensor_levels", []))
+    }
 
 
 def _sensor_levels_to_check_levels(
@@ -198,12 +195,16 @@ def check_ipmi_detailed(
     )
     if metric:
         yield metric
-    yield from _check_numerical_levels(
-        item,
-        sensor.value,
-        params,
-        sensor.unit,
-    )
+
+    user_levels_map = _compile_user_levels_map(params)
+    if levels := user_levels_map.get(item):
+        yield from check_levels(
+            sensor.value,
+            levels_upper=levels.upper,
+            levels_lower=levels.lower,
+            render_func=_unit_to_render_func(sensor.unit),
+            label=item,
+        )
 
 
 def check_ipmi_summarized(
@@ -211,6 +212,8 @@ def check_ipmi_summarized(
     section: Section,
     status_txt_mapping: StatusTxtMapping,
 ) -> type_defs.CheckResult:
+    user_levels_map = _compile_user_levels_map(params)
+
     states = [State.OK]
     warn_texts = []
     crit_texts = []
@@ -231,21 +234,16 @@ def check_ipmi_summarized(
                 sensor_state = State(wato_status)
                 break
 
-        if sensor.value is not None:
-            sensor_result = next(
-                iter(_check_numerical_levels(
-                    sensor_name,
-                    sensor.value,
-                    params,
-                    sensor.unit,
-                )),
-                None,
+        if sensor.value is not None and (levels := user_levels_map.get(sensor_name)):
+            (sensor_result,) = check_levels(
+                sensor.value,
+                levels_upper=levels.upper,
+                levels_lower=levels.lower,
+                render_func=_unit_to_render_func(sensor.unit),
+                label=sensor_name,
             )
-
-            if sensor_result:
-                assert isinstance(sensor_result, Result)
-                sensor_state = State.worst(sensor_state, sensor_result.state)
-                txt = sensor_result.summary
+            sensor_state = State.worst(sensor_state, sensor_result.state)
+            txt = sensor_result.summary
 
         if sensor_state is State.WARN:
             warn_texts.append(txt)

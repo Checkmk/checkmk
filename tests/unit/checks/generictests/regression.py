5f0fdfe6728855e06439bf3f41ede7485c3f27c8
Kenneth Okoh <kenneth.okoh@checkmk.com>
2023-05-08 12:33:01 +0200

    Rebranding: Change license headers (XIV)
    tests/unit/checks/*
    CMK-12940
    
    Change-Id: Ie5bbfc16272ef7928bd865432226626ee0f7543b

--- tests/unit/checks/generictests/regression.py
+++ tests/unit/checks/generictests/regression.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 """Script to create regression tests
@@ -150,7 +150,7 @@ class WritableDataset:
             comments = [
                 "#!/usr/bin/env python3\n",
                 "# -*- encoding: utf-8 -*-\n",
-                "# Copyright (C) 2019 tribe29 GmbH - License: GNU General Public License v2\n",
+                "# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2\n",
                 "# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and\n",
                 "# conditions defined in the file COPYING, which is part of this source code package.\n",
                 "\n",

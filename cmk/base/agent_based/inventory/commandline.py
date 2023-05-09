2411afcf4279d7de35e6e7fdcd6b11d3e813672d
Mathias Laurin <mathias.laurin@checkmk.com>
2023-05-09 10:12:19 +0200

    Inline `_commandline_inventory_on_host()`
    Copy-paste != abstraction
    
    CMK-12298
    
    Change-Id: I39445ab7fcad021ba1fb882d6667167955225133

--- cmk/base/agent_based/inventory/commandline.py
+++ cmk/base/agent_based/inventory/commandline.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Callable, Container, Mapping
+from collections.abc import Container, Mapping
 from pathlib import Path
 
 import cmk.utils.cleanup
@@ -43,20 +43,33 @@ def commandline_inventory(
     run_plugin_names: Container[InventoryPluginName] = EVERYTHING,
 ) -> None:
     section.section_begin(hostname)
+    section.section_step("Inventorizing")
     try:
-        _commandline_inventory_on_host(
-            hostname,
-            config_cache=config_cache,
-            fetcher=fetcher,
-            parser=parser,
-            summarizer=summarizer,
-            inventory_parameters=config_cache.inventory_parameters,
-            parameters=parameters,
-            raw_intervals_from_config=raw_intervals_from_config,
-            section_plugins=section_plugins,
-            inventory_plugins=inventory_plugins,
-            run_plugin_names=run_plugin_names,
-        )
+        old_tree = load_tree(Path(cmk.utils.paths.inventory_output_dir, hostname))
+        if config_cache.is_cluster(hostname):
+            check_result = inventorize_cluster(
+                config_cache.nodes_of(hostname) or (),
+                parameters=parameters,
+                old_tree=old_tree,
+            ).check_result
+        else:
+            check_result = inventorize_host(
+                hostname,
+                fetcher=fetcher,
+                parser=parser,
+                summarizer=summarizer,
+                inventory_parameters=config_cache.inventory_parameters,
+                section_plugins=section_plugins,
+                inventory_plugins=inventory_plugins,
+                run_plugin_names=run_plugin_names,
+                parameters=parameters,
+                raw_intervals_from_config=raw_intervals_from_config,
+                old_tree=old_tree,
+            ).check_result
+        if check_result.state:
+            section.section_error(check_result.summary)
+        else:
+            section.section_success(check_result.summary)
 
     except Exception as e:
         if cmk.utils.debug.enabled():
@@ -64,48 +77,3 @@ def commandline_inventory(
         section.section_error("%s" % e)
     finally:
         cmk.utils.cleanup.cleanup_globals()
-
-
-def _commandline_inventory_on_host(
-    host_name: HostName,
-    *,
-    config_cache: ConfigCache,
-    fetcher: FetcherFunction,
-    parser: ParserFunction,
-    summarizer: SummarizerFunction,
-    inventory_parameters: Callable[[HostName, InventoryPlugin], Mapping[str, object]],
-    parameters: HWSWInventoryParameters,
-    raw_intervals_from_config: RawIntervalsFromConfig,
-    section_plugins: Mapping[SectionName, SectionPlugin],
-    inventory_plugins: Mapping[InventoryPluginName, InventoryPlugin],
-    run_plugin_names: Container[InventoryPluginName],
-) -> None:
-    section.section_step("Inventorizing")
-
-    old_tree = load_tree(Path(cmk.utils.paths.inventory_output_dir, host_name))
-
-    if config_cache.is_cluster(host_name):
-        check_result = inventorize_cluster(
-            config_cache.nodes_of(host_name) or (),
-            parameters=parameters,
-            old_tree=old_tree,
-        ).check_result
-    else:
-        check_result = inventorize_host(
-            host_name,
-            fetcher=fetcher,
-            parser=parser,
-            summarizer=summarizer,
-            inventory_parameters=inventory_parameters,
-            section_plugins=section_plugins,
-            inventory_plugins=inventory_plugins,
-            run_plugin_names=run_plugin_names,
-            parameters=parameters,
-            raw_intervals_from_config=raw_intervals_from_config,
-            old_tree=old_tree,
-        ).check_result
-
-    if check_result.state:
-        section.section_error(check_result.summary)
-    else:
-        section.section_success(check_result.summary)

6125ec2517fcf2aa703b26626c3e3b29aa6af7df
Ronny Bruska <ronny.bruska@checkmk.com>
2023-05-10 14:16:37 +0200

    Fix link 'Save & go to...' for pagetypes
    CMK-13187
    
    Change-Id: I3e0dd1fe5b5a9f51bc76e3e838e6a6279c8c1e18
    (cherry picked from commit ee9c4721cde58f6291a1494a63199505864aa9f9)

--- cmk/gui/pagetypes.py
+++ cmk/gui/pagetypes.py
@@ -1239,12 +1239,15 @@ class EditPage(Page, Generic[_T_OverridableSpec, _Self]):
             if not user_errors:
                 instances.add_page(new_page)
                 self._type.save_user_instances(instances, owner_id)
-                if mode == "create":
-                    redirect_url = new_page.after_create_url() or back_url
+                if request.var("save_and_view"):
+                    redirect_url = new_page.after_create_url() or makeuri_contextless(
+                        request,
+                        [("name", new_page.name())],
+                        filename="%s.py" % self._type.type_name(),
+                    )
                 else:
                     redirect_url = back_url
-
-                flash(_("Your changes haven been saved."))
+                    flash(_("Your changes have been saved."))
 
                 # Reload sidebar.TODO: This code logically belongs to PageRenderer. How
                 # can we simply move it there?

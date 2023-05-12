a595111e2fde26bc1e59873ce2c7543828eec144
Frans Fürst <frans.fuerst@checkmk.com>
2023-05-12 11:01:53 +0200

    Let Windows Agent build job add directory hashes to build info
    Change-Id: I39187d32cf5d0908cae98a3016a025bd9dfa8353

--- buildscripts/scripts/winagt-build.groovy
+++ buildscripts/scripts/winagt-build.groovy
@@ -13,6 +13,8 @@ def main() {
     def cmk_version = versioning.strip_rc_number_from_version(cmk_vers_rc_aware)
 
     dir("${checkout_dir}") {
+        setCustomBuildProperty(key: "path_hashes", value: scm_directory_hashes());
+
         stage("make setversion") {
             bat("make -C agents\\wnx NEW_VERSION='${cmk_version}' setversion")
         }

76841d11aa89e39905332029a11f8399637827ac
Kenneth Okoh <kenneth.okoh@checkmk.com>
2023-05-12 11:59:57 +0200

    Rebranding: Remove "tribe29" references from tests/* (II)
    CMK-12940
    
    Change-Id: I7e0273e84b4ff65686cd320f32755b945b1dd5e3

--- tests/unit/checks/generictests/datasets/azure_usagedetails.py
+++ tests/unit/checks/generictests/datasets/azure_usagedetails.py
@@ -13,15 +13,15 @@ checkname = 'azure_usagedetails'
 info = [
     ['Resource'],
     [
-        '{"id": "subscriptions/5698bf61-ff63-4299-94d2-5ed0f51e0366/providers/Microsoft.CostManagement/query/a2863eb3-b3f5-4356-8bb2-f8349a71fc1b", "name": "a2863eb3-b3f5-4356-8bb2-f8349a71fc1b-0", "type": "Microsoft.Consumption/usageDetails", "location": null, "sku": null, "eTag": null, "properties": {"Cost": 0.05327698616264571, "CostUSD": 0.054594222263465136, "ResourceType": "Microsoft.Compute/disks", "ResourceGroupName": "tribe29-dev", "Tags": [], "Currency": "EUR"}, "group": "tribe29-dev", "subscription": "5698bf61-ff63-4299-94d2-5ed0f51e0366", "provider": "Microsoft.CostManagement"}'
+        '{"id": "subscriptions/5698bf61-ff63-4299-94d2-5ed0f51e0366/providers/Microsoft.CostManagement/query/a2863eb3-b3f5-4356-8bb2-f8349a71fc1b", "name": "a2863eb3-b3f5-4356-8bb2-f8349a71fc1b-0", "type": "Microsoft.Consumption/usageDetails", "location": null, "sku": null, "eTag": null, "properties": {"Cost": 0.05327698616264571, "CostUSD": 0.054594222263465136, "ResourceType": "Microsoft.Compute/disks", "ResourceGroupName": "checkmk-dev", "Tags": [], "Currency": "EUR"}, "group": "checkmk-dev", "subscription": "5698bf61-ff63-4299-94d2-5ed0f51e0366", "provider": "Microsoft.CostManagement"}'
     ],
     ['Resource'],
     [
-        '{"id": "subscriptions/5698bf61-ff63-4299-94d2-5ed0f51e0366/providers/Microsoft.CostManagement/query/a2863eb3-b3f5-4356-8bb2-f8349a71fc1b", "name": "a2863eb3-b3f5-4356-8bb2-f8349a71fc1b-1", "type": "Microsoft.Consumption/usageDetails", "location": null, "sku": null, "eTag": null, "properties": {"Cost": 0.1, "CostUSD": 0.10247243, "ResourceType": "Microsoft.Compute/virtualMachines", "ResourceGroupName": "tribe29-dev", "Tags": [], "Currency": "EUR"}, "group": "tribe29-dev", "subscription": "5698bf61-ff63-4299-94d2-5ed0f51e0366", "provider": "Microsoft.CostManagement"}'
+        '{"id": "subscriptions/5698bf61-ff63-4299-94d2-5ed0f51e0366/providers/Microsoft.CostManagement/query/a2863eb3-b3f5-4356-8bb2-f8349a71fc1b", "name": "a2863eb3-b3f5-4356-8bb2-f8349a71fc1b-1", "type": "Microsoft.Consumption/usageDetails", "location": null, "sku": null, "eTag": null, "properties": {"Cost": 0.1, "CostUSD": 0.10247243, "ResourceType": "Microsoft.Compute/virtualMachines", "ResourceGroupName": "checkmk-dev", "Tags": [], "Currency": "EUR"}, "group": "checkmk-dev", "subscription": "5698bf61-ff63-4299-94d2-5ed0f51e0366", "provider": "Microsoft.CostManagement"}'
     ],
     ['Resource'],
     [
-        '{"id": "subscriptions/5698bf61-ff63-4299-94d2-5ed0f51e0366/providers/Microsoft.CostManagement/query/a2863eb3-b3f5-4356-8bb2-f8349a71fc1b", "name": "a2863eb3-b3f5-4356-8bb2-f8349a71fc1b-2", "type": "Microsoft.Consumption/usageDetails", "location": null, "sku": null, "eTag": null, "properties": {"Cost": 0.06867924528301887, "CostUSD": 0.07, "ResourceType": "Microsoft.Network/publicIPAddresses", "ResourceGroupName": "tribe29-dev", "Tags": [], "Currency": "EUR"}, "group": "tribe29-dev", "subscription": "5698bf61-ff63-4299-94d2-5ed0f51e0366", "provider": "Microsoft.CostManagement"}'
+        '{"id": "subscriptions/5698bf61-ff63-4299-94d2-5ed0f51e0366/providers/Microsoft.CostManagement/query/a2863eb3-b3f5-4356-8bb2-f8349a71fc1b", "name": "a2863eb3-b3f5-4356-8bb2-f8349a71fc1b-2", "type": "Microsoft.Consumption/usageDetails", "location": null, "sku": null, "eTag": null, "properties": {"Cost": 0.06867924528301887, "CostUSD": 0.07, "ResourceType": "Microsoft.Network/publicIPAddresses", "ResourceGroupName": "checkmk-dev", "Tags": [], "Currency": "EUR"}, "group": "checkmk-dev", "subscription": "5698bf61-ff63-4299-94d2-5ed0f51e0366", "provider": "Microsoft.CostManagement"}'
     ],
 ]
 

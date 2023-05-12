ed58f1d7f14cdb6ee6912b827d940673732d1d8e
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2023-05-12 13:14:16 +0200

    15562 FIX Drop unsupported distros
    CMK-13213
    
    Change-Id: Iec5dffce57179183a66e6b8c99c2ab3e50e1ed24

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -321,15 +321,9 @@ class Site:
 
         if preserve_env:
             # Skip the test cases calling this for some distros
-            # Ubuntu 16.04 does not support --preserve-env nor --whitelist-environment
-            # Ubuntu 18.04 does not support --whitelist-environment
             if os.environ.get("DISTRO") in (
-                "ubuntu-16.04",
-                "ubuntu-18.04",
                 "centos-7",
                 "centos-8",
-                "sles-12sp3",
-                "sles-12sp4",
             ):
                 pytest.skip("preserve env not possible in this environment")
             sudo_env_args = [f"--preserve-env={','.join(preserve_env)}"]

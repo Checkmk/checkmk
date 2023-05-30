9c0e55c9f04cbc4f7c7c0a2b8837060d93a59349
Andreas Umbreit <andreas.umbreit@checkmk.com>
2023-05-30 08:53:24 +0200

    Refactor smart check plugin VIII
    Change-Id: I38adc8329fb812b9fe360c5c7a0ae397fb97319a

--- cmk/base/plugins/agent_based/smart.py
+++ cmk/base/plugins/agent_based/smart.py
@@ -22,7 +22,8 @@
 # /dev/sda ATA WDC_SSC-D0128SC- 173 Unknown_Attribute       0x0012   100   100   000    Old_age   Always       -       4217788040605
 import time
 from collections.abc import Callable, Iterable, Mapping, MutableMapping, Sequence
-from typing import Any, Final, Tuple
+from enum import Enum
+from typing import Any, Final, NamedTuple
 
 from .agent_based_api.v1 import (
     get_rate,
@@ -41,6 +42,37 @@ Section = Mapping[str, Mapping[str, int]]
 Disk = MutableMapping[str, int]
 Disks = MutableMapping[str, Disk]
 
+
+class DiskAttributeItem(NamedTuple):
+    name: str
+    capture_on_discovery: bool
+    renderer: Callable[[int], str]
+
+
+class DiskAttribute(DiskAttributeItem, Enum):
+    REALLOCATED_SECTORS = DiskAttributeItem("Reallocated_Sectors", True, str)
+    POWER_ON_HOURS = DiskAttributeItem("Power_On_Hours", False, lambda h: render.timespan(h * 3600))
+    SPIN_RETRIES = DiskAttributeItem("Spin_Retries", True, str)
+    POWER_CYCLES = DiskAttributeItem("Power_Cycles", False, str)
+    END_TO_END_ERRORS = DiskAttributeItem("End-to-End_Errors", True, str)
+    UNCORRECTABLE_ERRORS = DiskAttributeItem("Uncorrectable_Errors", True, str)
+    COMMAND_TIMEOUT_COUNTER = DiskAttributeItem("Command_Timeout_Counter", True, str)
+    TEMPERATURE = DiskAttributeItem("Temperature", False, str)
+    REALLOCATED_EVENTS = DiskAttributeItem("Reallocated_Events", True, str)
+    PENDING_SECTORS = DiskAttributeItem("Pending_Sectors", True, str)
+    UDMA_CRC_ERRORS = DiskAttributeItem("UDMA_CRC_Errors", True, str)
+    CRC_ERRORS = DiskAttributeItem("CRC_Errors", True, str)
+    CRITICAL_WARNING = DiskAttributeItem("Critical_Warning", True, str)
+    MEDIA_AND_DATA_INTEGRITY_ERRORS = DiskAttributeItem(
+        "Media_and_Data_Integrity_Errors", True, str
+    )
+    AVAILABLE_SPARE = DiskAttributeItem("Available_Spare", False, render.percent)
+    PERCENTAGE_USED = DiskAttributeItem("Percentage_Used", False, render.percent)
+    ERROR_INFORMATION_LOG_ENTRIES = DiskAttributeItem("Error_Information_Log_Entries", False, str)
+    DATA_UNITS_READ = DiskAttributeItem("Data_Units_Read", False, render.bytes)
+    DATA_UNITS_WRITTEN = DiskAttributeItem("Data_Units_Written", False, render.bytes)
+
+
 # The command timeouts is a counter and we experienced that on some devices, it will already be
 # increased by one count after a simple reboot. In a faulty situation it will however increase in
 # much larger steps (100 or 1000). So we accept any rate below 100 counts per hour.
@@ -51,18 +83,18 @@ CRC_ERRORS_ID: Final = 199
 
 # This mapping also limits the used ATA attributes.
 # All ATA attributes not listed here will be discarded when parsing the raw agent section
-ATA_ID_TO_ATTRIBUTE_NAME: Final[Mapping[int, str]] = {
-    5: "Reallocated_Sectors",
-    9: "Power_On_Hours",
-    10: "Spin_Retries",
-    12: "Power_Cycles",
-    184: "End-to-End_Errors",
-    187: "Uncorrectable_Errors",
-    188: "Command_Timeout_Counter",
-    194: "Temperature",
-    196: "Reallocated_Events",
-    197: "Pending_Sectors",
-    CRC_ERRORS_ID: "CRC_Errors",
+ATA_ID_TO_DISK_ATTRIBUTE: Final[Mapping[int, DiskAttribute]] = {
+    5: DiskAttribute.REALLOCATED_SECTORS,
+    9: DiskAttribute.POWER_ON_HOURS,
+    10: DiskAttribute.SPIN_RETRIES,
+    12: DiskAttribute.POWER_CYCLES,
+    184: DiskAttribute.END_TO_END_ERRORS,
+    187: DiskAttribute.UNCORRECTABLE_ERRORS,
+    188: DiskAttribute.COMMAND_TIMEOUT_COUNTER,
+    194: DiskAttribute.TEMPERATURE,
+    196: DiskAttribute.REALLOCATED_EVENTS,
+    197: DiskAttribute.PENDING_SECTORS,
+    CRC_ERRORS_ID: DiskAttribute.CRC_ERRORS,
 }
 
 
@@ -157,19 +189,20 @@ def _parse_ata_lines(ata_lines: Iterable[Sequence[str]]) -> Section:
             # Since we explicitly distinguish between the two, we choose "UDMA_CRC_Error_Count"
             # whenever the ID 199 comes with this textual information.
             # Otherwise, we default to CRC_Error_Count.
-            _set_int_or_zero(disk, "UDMA_CRC_Errors", raw_value)
+            _set_int_or_zero(disk, DiskAttribute.UDMA_CRC_ERRORS.name, raw_value)
             continue
 
-        if (lookup_attribute_name := ATA_ID_TO_ATTRIBUTE_NAME.get(int(ID))) is None:
+        if (lookup_attribute := ATA_ID_TO_DISK_ATTRIBUTE.get(int(ID))) is None:
             if attribute_name in disk:
                 # Don't override already set attributes
                 continue
             _set_int_or_zero(disk, attribute_name, raw_value)
             continue
 
-        _set_int_or_zero(disk, lookup_attribute_name, raw_value)
+        _set_int_or_zero(disk, lookup_attribute.name, raw_value)
 
-        if lookup_attribute_name == "Reallocated_Events":  # special case, see check function
+        if lookup_attribute is DiskAttribute.REALLOCATED_EVENTS:
+            # special case, see check function
             try:
                 disk["_normalized_value_Reallocated_Events"] = int(value)
                 disk["_normalized_threshold_Reallocated_Events"] = int(threshold)
@@ -210,50 +243,14 @@ register.agent_section(
     parse_function=parse_raw_values,
 )
 
-DISCOVERED_PARAMETERS: Final = (
-    "Reallocated_Sectors",  # 5
-    "Spin_Retries",  # 10
-    "End-to-End_Errors",  # 184
-    "Uncorrectable_Errors",  # 187
-    "Command_Timeout_Counter",  # 188
-    "Reallocated_Events",  # 196
-    "Pending_Sectors",  # 197
-    "UDMA_CRC_Errors",  # 199
-    # nvme
-    "Critical_Warning",
-    "Media_and_Data_Integrity_Errors",
-)
-
 
 def discover_smart_stats(section: Section) -> DiscoveryResult:
     for disk_name, disk in section.items():
-        cleaned = {f: disk[f] for f in DISCOVERED_PARAMETERS if f in disk}
-        if cleaned:
-            yield Service(item=disk_name, parameters=cleaned)
-
-
-OUTPUT_FIELDS: Tuple[Tuple[str, Callable], ...] = (
-    # ATA
-    ("Reallocated_Sectors", str),  # 5
-    ("Power_On_Hours", lambda h: render.timespan(h * 3600)),  # 9, also nvme
-    ("Spin_Retries", str),  # 10
-    ("Power_Cycles", str),  # 12, also nvme
-    ("End-to-End_Errors", str),  # 184
-    ("Uncorrectable_Errors", str),  # 187
-    ("Command_Timeout_Counter", str),  # 188
-    ("Reallocated_Events", str),  # 196
-    ("Pending_Sectors", str),  # 197
-    ("UDMA_CRC_Errors", str),  # 199
-    ("CRC_errors", str),  # also 199
-    # nvme
-    ("Critical_Warning", str),
-    ("Media_and_Data_Integrity_Errors", str),
-    ("Available_Spare", render.percent),
-    ("Percentage_Used", render.percent),
-    ("Error_Information_Log_Entries", str),
-    ("Data_Units_Read", render.bytes),
-    ("Data_Units_Written", render.bytes),
-)
+        captured = {
+            f.name: disk[f.name] for f in DiskAttribute if f.capture_on_discovery and f.name in disk
+        }
+        if captured:
+            yield Service(item=disk_name, parameters=captured)
 
 
 def check_smart_stats(item: str, params: Mapping[str, int], section: Section) -> CheckResult:
@@ -262,24 +259,29 @@ def check_smart_stats(item: str, params: Mapping[str, int], section: Section) ->
     if disk is None:
         return
 
-    for field, renderer in OUTPUT_FIELDS:
-        value = disk.get(field)
+    for attribute in DiskAttribute:
+        if attribute is DiskAttribute.TEMPERATURE:
+            # Currently handled by separate check
+            continue
+
+        value = disk.get(attribute.name)
         if value is None:
             continue
         assert isinstance(value, int)
 
-        infotext = "%s: %s" % (_display_attribute_name(field), renderer(value))
+        infotext = "%s: %s" % (_display_attribute_name(attribute), attribute.renderer(value))
 
-        ref_value = params.get(field)
-        if field == "Available_Spare":
+        ref_value = params.get(attribute.name)
+
+        if attribute is DiskAttribute.AVAILABLE_SPARE:
             ref_value = disk["Available_Spare_Threshold"]
 
         if ref_value is None:
             yield Result(state=State.OK, summary=infotext)
-            yield Metric(field, value)
+            yield Metric(attribute.name, value)
             continue
 
-        if field == "Available_Spare":
+        if attribute is DiskAttribute.AVAILABLE_SPARE:
             state = State.CRIT if value < ref_value else State.OK
         else:
             state = State.CRIT if value > ref_value else State.OK
@@ -289,20 +291,20 @@ def check_smart_stats(item: str, params: Mapping[str, int], section: Section) ->
         # which still seem to be OK. The raw value increased by a small amount but the
         # aggregated value remained at it's initial/ok state. So we use the aggregated
         # value now. Only for this field.
-        if field == "Reallocated_Event_Count":
+        if attribute is DiskAttribute.REALLOCATED_EVENTS:
             try:
-                norm_value = disk[f"_normalized_value_{field}"]
-                norm_threshold = disk[f"_normalized_threshold_{field}"]
+                norm_value = disk[f"_normalized_value_{attribute.name}"]
+                norm_threshold = disk[f"_normalized_threshold_{attribute.name}"]
             except KeyError:
                 yield Result(state=State.OK, summary=infotext)
-                yield Metric(field, value)
+                yield Metric(attribute.name, value)
                 continue
             hints.append("normalized value: %d" % norm_value)
             if norm_value <= norm_threshold:
                 state = State.CRIT
                 hints[-1] += " (!!)"
 
-        if field == "Command_Timeout_Counter":
+        if attribute is DiskAttribute.COMMAND_TIMEOUT_COUNTER:
             rate = get_rate(get_value_store(), "cmd_timeout", time.time(), value)
             state = State.OK if rate < MAX_COMMAND_TIMEOUTS_PER_HOUR / (60 * 60) else State.CRIT
             hints = (
@@ -317,23 +319,23 @@ def check_smart_stats(item: str, params: Mapping[str, int], section: Section) ->
         yield Result(
             state=state, summary=infotext + " (%s)" % ", ".join(hints) if hints else infotext
         )
-        yield Metric(field, value)
+        yield Metric(attribute.name, value)
 
 
-def _display_attribute_name(attribute: str) -> str:
+def _display_attribute_name(attribute: DiskAttribute) -> str:
     if (
         lookup_translation := {
             # Can't automatically translate/maintain the upper case abbreviations.
-            "CRC_Errors": "CRC errors",
-            "UDMA_CRC_Errors": "UDMA CRC errors",
+            DiskAttribute.CRC_ERRORS: "CRC errors",
+            DiskAttribute.UDMA_CRC_ERRORS: "UDMA CRC errors",
             # "Power_On_Hours" also comes on nvme devices.
             # Since we decided to display "Powered On", we can't translate this automatically.
-            "Power_On_Hours": "Powered on",
+            DiskAttribute.POWER_ON_HOURS: "Powered on",
         }.get(attribute)
     ) is not None:
         return lookup_translation
 
-    match attribute.split("_", maxsplit=1):
+    match attribute.name.split("_", maxsplit=1):
         case [first, rest]:
             return f"{first} {rest.lower().replace('_', ' ')}"
         case [only_one]:

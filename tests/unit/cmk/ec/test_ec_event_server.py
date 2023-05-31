166adde6c9d60c1ec96037b7b99c50be711ba03b
Konstantin Baikov <konstantin.baikov@checkmk.com>
2023-05-31 15:44:10 +0200

    14857 FIX Handle the missing state pattern in rules correctly
    Change-Id: I7e346a2601b77fcca023c276db8d6c5c4227208f

---
+++ tests/unit/cmk/ec/test_ec_event_server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# pylint: disable=redefined-outer-name
+
+import pytest
+
+from tests.testlib import CMKEventConsole
+
+from cmk.utils.type_defs import HostName
+
+from cmk.ec.config import Config, Rule, ServiceLevel
+from cmk.ec.defaults import default_rule_pack
+from cmk.ec.main import Event, EventServer
+
+RULE: Rule = {
+    "actions": [],
+    "actions_in_downtime": True,
+    "autodelete": False,
+    "id": "patterns",
+    "invert_matching": False,
+    "sl": ServiceLevel(precedence="message", value=0),
+    "state": ("text_pattern", {"1": "supercrit", "2": "superwarn"}),
+}
+
+
+@pytest.fixture
+def config_with_host_patterns(config: Config) -> Config:
+    """Return a config with a rule with specific host patterns inside the default rule_pack."""
+    with_host_patterns: Config = config.copy()
+    with_host_patterns["rule_packs"] = [default_rule_pack([RULE])]
+    return with_host_patterns
+
+
+def test_event_rewrite(
+    event_server: EventServer,
+    config_with_host_patterns: Config,
+) -> None:
+    """
+    Event server rewrite_event() method should change event state
+    even if incomplete StatePatterns are given in rule["State"].
+    """
+    event_server.reload_configuration(config=config_with_host_patterns)
+    event = CMKEventConsole.new_event(
+        Event(
+            host=HostName("heute"),
+            text="SUPERWARN",
+            core_host=HostName("heute"),
+        )
+    )
+    assert "state" not in event
+
+    event_server.rewrite_event(rule=RULE, event=event, groups={})
+
+    assert event["text"] == "SUPERWARN"
+    assert event["state"] == 2

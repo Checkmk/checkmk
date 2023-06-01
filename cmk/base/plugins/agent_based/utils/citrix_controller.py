459f2e9314a100b1a2b0818c8c779b6c6e21dd3d
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-06-01 13:22:38 +0200

    citrix_controller: make section expandable
    Change-Id: I47215eca05514c9b40a33892efe12bcce4c93dca

---
+++ cmk/base/plugins/agent_based/utils/citrix_controller.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import dataclasses
+from typing import NewType
+
+from ..agent_based_api.v1.type_defs import StringTable
+
+Version = NewType("Version", str)
+
+
+@dataclasses.dataclass
+class Section:
+    version: None | Version = None
+
+
+def parse_citrix_controller(string_table: StringTable) -> Section:
+    section = Section()
+    for line in string_table:
+        if line[0] == "ControllerVersion":
+            section.version = Version(line[1])
+    return section

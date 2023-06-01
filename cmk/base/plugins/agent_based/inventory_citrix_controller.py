7f619e01498ac5f2fc19404bf43e5cf2ffa6f66c
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-06-01 13:22:38 +0200

    15623 FIX <tt>citrix</tt>: Add Missing Checks
    Squashes:
    
    I56cb809ac78de3d51967a438ff86720a21858f9b
    I47215eca05514c9b40a33892efe12bcce4c93dca
    I3fc562b0d5ab5cf474f843e4251fea503c37fda0
    If28f6f287cef44431a87c71dd50d691b0eb718e8
    I86fa2cda2bb98b59e1cc93f650d8bccaa1e310ed
    I48d26707e56589a2ad2feedb6b46aaf288023264
    I529798c529147a9f75c6a0615a1f61d01e40cca8
    
    SUP-14362
    
    Change-Id: Iae45cf3f4c302dd420acbc3cdbb23c59807068fe

--- cmk/base/plugins/agent_based/inventory_citrix_controller.py
+++ cmk/base/plugins/agent_based/inventory_citrix_controller.py
@@ -3,20 +3,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import NewType, Optional
-
 from .agent_based_api.v1 import Attributes, register
-from .agent_based_api.v1.type_defs import InventoryResult, StringTable
-
-Version = NewType("Version", str)
-
-
-def parse_citrix_controller(string_table: StringTable) -> Optional[Version]:
-    for line in string_table:
-        if line[0] == "ControllerVersion":
-            return Version(line[1])
-    return None
-
+from .agent_based_api.v1.type_defs import InventoryResult
+from .utils.citrix_controller import parse_citrix_controller, Section
 
 register.agent_section(
     name="citrix_controller",
@@ -24,11 +13,11 @@ register.agent_section(
 )
 
 
-def inventory_citrix_controller(section: Version) -> InventoryResult:
+def inventory_citrix_controller(section: Section) -> InventoryResult:
     yield Attributes(
         path=["software", "applications", "citrix", "controller"],
         inventory_attributes={
-            "controller_version": section,
+            "controller_version": section.version,
         },
     )
 

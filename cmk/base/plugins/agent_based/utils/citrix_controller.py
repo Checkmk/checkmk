6464e061038b7e5cedf68bc118510f28917c61d9
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-06-02 16:03:17 +0200

    citrix_controller: simplify
    Change-Id: I56cb809ac78de3d51967a438ff86720a21858f9b

--- cmk/base/plugins/agent_based/utils/citrix_controller.py
+++ cmk/base/plugins/agent_based/utils/citrix_controller.py
@@ -37,38 +37,29 @@ def parse_citrix_controller(string_table: StringTable) -> Section | None:
         return None
     section = Section()
     session = Session()
-    # piggy back data might deliver double data
-    detected_states = []
     for line in string_table:
-        if line[0] == "ControllerVersion" and len(line) > 1:
-            section.version = Version(line[1])
-        if line[0] == "ControllerState":
-            section.state = line[1] if len(line) > 1 else Error()
-        if (
-            line[0].lower() == "licensinggracestate"
-            and line[0] not in detected_states
-            and len(line) > 1
-        ):
-            detected_states.append(line[0])
-            section.licensing_grace_state = line[1]
-        if (
-            line[0].lower() == "licensingserverstate"
-            and line[0] not in detected_states
-            and len(line) > 1
-        ):
-            detected_states.append(line[0])
-            section.licensing_server_state = line[1]
-        if line[0] == "TotalFarmActiveSessions":
-            session.active = int(line[1])
-            section.session = session
-        elif line[0] == "TotalFarmInactiveSessions":
-            session.inactive = int(line[1])
-            section.session = session
-        if line[0] == "DesktopsRegistered":
-            try:
-                section.desktop_count = int(line[1])
-            except (IndexError, ValueError):
+        match line:
+            case ["ControllerVersion", version, *_]:
+                section.version = Version(version)
+            case ["ControllerState", state, *_]:
+                section.state = state
+            case ["ControllerState", *_]:
+                section.state = Error()
+            # piggy back data might deliver double data
+            case ["LicensingGraceState", grace, *_] if section.licensing_grace_state is None:
+                section.licensing_grace_state = grace
+            case ["LicensingServerState", server, *_] if section.licensing_server_state is None:
+                section.licensing_server_state = server
+            case ["TotalFarmActiveSessions", active, *_]:
+                session.active = int(active)
+                section.session = session
+            case ["TotalFarmInactiveSessions", inactive, *_]:
+                session.inactive = int(inactive)
+                section.session = session
+            case ["DesktopsRegistered", value, *_] if value.isdigit():
+                section.desktop_count = int(value)
+            case ["DesktopsRegistered", *_]:
                 section.desktop_count = Error()
-        if line[0] == "ActiveSiteServices":
-            section.active_site_services = " ".join(line[1:])
+            case ["ActiveSiteServices", *services]:
+                section.active_site_services = " ".join(services)
     return section

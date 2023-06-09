cbb14b9d995927bc1c2f00db1931ad69e8764545
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2023-06-09 14:15:12 +0200

    Refactoring: shorten switch/case
    Change-Id: I0d406af6646792fe0ad606789af7dd5d77de3615

--- buildscripts/scripts/build-cmk-image.groovy
+++ buildscripts/scripts/build-cmk-image.groovy
@@ -156,11 +156,9 @@ def main() {
 def registry_credentials_id(edition) {
    switch(edition) {
        case "raw":
-           return "11fb3d5f-e44e-4f33-a651-274227cc48ab"
        case "cloud":
            return "11fb3d5f-e44e-4f33-a651-274227cc48ab"
        case "enterprise":
-           return "registry.checkmk.com"
        case "managed":
            return "registry.checkmk.com"
        case "saas":

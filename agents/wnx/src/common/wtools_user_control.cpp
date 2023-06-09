e85c297df8028425bf33715b266113cec53772ef
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-06-09 13:31:50 +0200

    fix branding in Windows agent: non-critical
    comments, scripts, tests
    
    Change-Id: Ic1e20d2330966b7047a7dabc7c9a8e3c7164374f

--- agents/wnx/src/common/wtools_user_control.cpp
+++ agents/wnx/src/common/wtools_user_control.cpp
@@ -35,7 +35,7 @@ Status LdapControl::userAdd(std::wstring_view user_name,
     wchar_t user_home_dir[] = L"";
     user_info.usri1_home_dir = user_home_dir;
 
-    wchar_t user_comment[] = L"Temporary Check MK User";
+    wchar_t user_comment[] = L"Temporary Checkmk User";
     user_info.usri1_comment = user_comment;
 
     user_info.usri1_flags = UF_SCRIPT;

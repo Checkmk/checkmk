54ad43c0c0d0e84b7ecf6c67e82929b7da80aedf
Luka Racic <luka.racic@checkmk.com>
2023-06-12 19:38:55 +0200

    15905 FIX check_disk_smb: Support SMB share hosts in networks other than the Checkmk server network
    With this werk check_disk_smb now supports share hosts which are in a different network than the Checkmk server. To enable this, the corresponding ruleset now offers an option for configuring the IP address.
    Note that this option is only needed if the HOST is not in the same network as the Checkmk server.
    Before this werk, if the SMB share host was in a different network, the plugin crashed.
    
    Change-Id: Ie27af6732726a797fb1b1987abb41376adc2951e

--- cmk/gui/plugins/wato/active_checks.py
+++ cmk/gui/plugins/wato/active_checks.py
@@ -27,10 +27,12 @@ from cmk.gui.valuespec import (
     DropdownChoice,
     FixedValue,
     Float,
+    HostAddress,
     Hostname,
     Integer,
     ListOf,
     ListOfStrings,
+    NetworkPort,
     Password,
     Percentage,
     RegExp,
@@ -1950,77 +1952,121 @@ rulespec_registry.register(
 )
 
 
-def _valuespec_active_checks_disk_smb():
-    return Dictionary(
-        title=_("Check SMB share access"),
-        help=_(
-            "This ruleset helps you to configure the classical Nagios "
-            "plugin <tt>check_disk_smb</tt> that checks the access to "
-            "filesystem shares that are exported via SMB/CIFS."
-        ),
-        elements=[
-            (
-                "share",
-                TextInput(
-                    title=_("SMB share to check"),
-                    help=_(
-                        "Enter the plain name of the share only, e. g. <tt>iso</tt>, <b>not</b> "
-                        "the full UNC like <tt>\\\\servername\\iso</tt>"
+def _migrate(params: dict[str, Any]) -> Mapping[str, Any]:
+    if "host" not in params:
+        # Up to 2.1.0p31 and 2.2.0p5 the host was not required
+        params["host"] = "use_parent_host"
+
+    return params
+
+
+def _valuespec_active_checks_disk_smb() -> Transform:
+    return Transform(
+        valuespec=Dictionary(
+            title=_("Check SMB share access"),
+            help=_(
+                "This ruleset helps you to configure the classical Nagios "
+                "plugin <tt>check_disk_smb</tt> that checks the access to "
+                "filesystem shares that are exported via SMB/CIFS."
+            ),
+            elements=[
+                (
+                    "share",
+                    TextInput(
+                        title=_("SMB share to check"),
+                        help=_(
+                            "Enter the plain name of the share only, e. g. <tt>iso</tt>, <b>not</b> "
+                            "the full UNC like <tt>\\\\servername\\iso</tt>"
+                        ),
+                        size=32,
+                        allow_empty=False,
                     ),
-                    size=32,
-                    allow_empty=False,
                 ),
-            ),
-            (
-                "workgroup",
-                TextInput(
-                    title=_("Workgroup"),
-                    help=_("Workgroup or domain used (defaults to <tt>WORKGROUP</tt>)"),
-                    size=32,
-                    allow_empty=False,
+                (
+                    "workgroup",
+                    TextInput(
+                        title=_("Workgroup"),
+                        help=_("Workgroup or domain used (defaults to <tt>WORKGROUP</tt>)"),
+                        size=32,
+                        allow_empty=False,
+                    ),
                 ),
-            ),
-            (
-                "host",
-                TextInput(
-                    title=_("NetBIOS name of the server"),
-                    help=_("If omitted then the IP address is being used."),
-                    size=32,
-                    allow_empty=False,
+                (
+                    "host",
+                    CascadingDropdown(
+                        title=_("NetBIOS name of the server"),
+                        help=_(
+                            "Choose, whether you want to use the parent host information for the NetBIOS server name,"
+                            " or if you want to specify one."
+                        ),
+                        choices=[
+                            (
+                                "use_parent_host",
+                                _("Use parent host information for the NetBIOS server name"),
+                            ),
+                            (
+                                "define_host",
+                                _("Define name of NetBIOS server"),
+                                HostAddress(
+                                    title="Define name of NetBIOS server",
+                                    allow_empty=False,
+                                    help=_("You can specify the NetBIOS server name."),
+                                    size=32,
+                                ),
+                            ),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "port",
-                Integer(
-                    title=_("TCP Port"),
-                    help=_("TCP port number to connect to. Usually either 139 or 445."),
-                    default_value=445,
-                    minvalue=1,
-                    maxvalue=65535,
+                (
+                    "ip_address",
+                    HostAddress(
+                        title=_("IP Address"),
+                        help=_(
+                            "IP address of SMB share host (only necessary if SMB share host is in another network)"
+                        ),
+                        size=32,
+                        allow_host_name=False,
+                        allow_empty=False,
+                    ),
                 ),
-            ),
-            (
-                "levels",
-                Tuple(
-                    title=_("Levels for used disk space"),
-                    elements=[
-                        Percentage(title=_("Warning if above"), default_value=85, allow_int=True),
-                        Percentage(title=_("Critical if above"), default_value=95, allow_int=True),
-                    ],
+                (
+                    "port",
+                    NetworkPort(
+                        title=_("TCP Port"),
+                        help=_("TCP port number to connect to. Usually either 139 or 445."),
+                        default_value=445,
+                        minvalue=1,
+                        maxvalue=65535,
+                    ),
                 ),
-            ),
-            (
-                "auth",
-                Tuple(
-                    title=_("Authorization"),
-                    elements=[
-                        TextInput(title=_("Username"), allow_empty=False, size=24),
-                        Password(title=_("Password"), allow_empty=False, size=12),
-                    ],
+                (
+                    "levels",
+                    Tuple(
+                        title=_("Levels for used disk space"),
+                        elements=[
+                            Percentage(
+                                title=_("Warning if above"), default_value=85, allow_int=True
+                            ),
+                            Percentage(
+                                title=_("Critical if above"), default_value=95, allow_int=True
+                            ),
+                        ],
+                    ),
                 ),
-            ),
-        ],
-        required_keys=["share", "levels"],
+                (
+                    "auth",
+                    Tuple(
+                        title=_("Authorization"),
+                        elements=[
+                            TextInput(title=_("Username"), allow_empty=False, size=24),
+                            Password(title=_("Password"), allow_empty=False, size=12),
+                        ],
+                    ),
+                ),
+            ],
+            required_keys=["share", "levels", "host"],
+        ),
+        forth=_migrate,
     )
 
 

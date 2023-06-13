dcb9c85a70888f277185b405b4ba189c11f94f61
Gav <gavin.mcguigan@checkmk.com>
2023-06-13 08:02:08 +0200

    version: move endpoints to their own module
    CMK-13752
    
    Change-Id: I696e28fc45edfbb6223cf0cd800d2ad4ab2e6b46

--- cmk/gui/plugins/openapi/restful_objects/response_schemas.py
+++ cmk/gui/plugins/openapi/restful_objects/response_schemas.py
@@ -930,19 +930,6 @@ class PasswordCollection(DomainObjectCollection):
     )
 
 
-class InstalledVersions(BaseSchema):
-    site = fields.String(
-        description="The site where this API call was made on.", example="production"
-    )
-    group = fields.String(
-        description="The Apache WSGI application group this call was made on.", example="de"
-    )
-    rest_api = fields.Dict(description="The REST-API version", example={"revision": "1.0.0"})
-    versions = fields.Dict(description="Some version numbers", example={"checkmk": "1.8.0p1"})
-    edition = fields.String(description="The Checkmk edition.", example="raw")
-    demo = fields.Boolean(description="Whether this is a demo version or not.", example=False)
-
-
 class VersionCapabilities(BaseSchema):
     blobsClobs = fields.Boolean(
         required=False,

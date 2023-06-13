b9406a08db6e5f5f7d5a6be31ff0f6c888471e69
Gav <gavin.mcguigan@checkmk.com>
2023-06-13 16:36:02 +0200

    host_tag_group: move endpoints to their own module
    CMK-13737
    
    Change-Id: I42fd9a11366e08fa2637fc87d8544e37774bde64

--- cmk/gui/plugins/openapi/restful_objects/response_schemas.py
+++ cmk/gui/plugins/openapi/restful_objects/response_schemas.py
@@ -464,28 +464,6 @@ class CollectionItem(OneOfSchema):
     type_field_remove = False
 
 
-class HostTag(BaseSchema):
-    id = fields.String(description="The unique identifier of this host tag", allow_none=True)
-    title = fields.String(description="The title of this host tag")
-    aux_tags = fields.List(fields.String(), description="The auxiliary tags this tag included in.")
-
-
-class HostTagExtensions(BaseSchema):
-    topic = fields.String(description="The topic this host tag group is organized in.")
-    tags = fields.List(fields.Nested(HostTag()), description="The list of tags in this group.")
-
-
-class ConcreteHostTagGroup(DomainObject):
-    domainType = fields.Constant(
-        "host_tag_group",
-        required=True,
-        description="The domain type of the object.",
-    )
-    extensions = fields.Nested(
-        HostTagExtensions(), description="Additional fields for objects of this type."
-    )
-
-
 class DomainObjectCollection(Linkable):
     id = fields.String(
         description="The name of this collection.",
@@ -524,17 +502,6 @@ class FolderCollection(DomainObjectCollection):
     value = fields.List(
         fields.Nested(FolderSchema()),
         description="A list of folder objects.",
-    )
-
-
-class HostTagGroupCollection(DomainObjectCollection):
-    domainType = fields.Constant(
-        "host_tag_group",
-        description="The domain type of the objects in the collection.",
-    )
-    value = fields.List(
-        fields.Nested(ConcreteHostTagGroup()),
-        description="A list of host tag group objects.",
     )
 
 

e631dd4316c7f0527e9d7b8f3a618d49fd015657
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2023-06-16 09:57:38 +0200

    Integration test for ASVS V14.5.1
    Change-Id: I7252183e602fbd0b00e8e2a08fa7f160eb248b01

---
+++ tests/integration/omd/test_apache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import re
+import uuid
+from pathlib import Path
+
+import pytest
+import requests
+
+from tests.testlib.site import Site
+
+
+def test_http_methods(site: Site) -> None:
+    """Check that some methods are forbidden and the denial is logged
+
+    ASVS: V14.5.1"""
+
+    user_agent = str(uuid.uuid4())
+    response = requests.request(
+        "PROPFIND", site.internal_url, timeout=5, headers={"User-Agent": user_agent}
+    )
+    assert response.status_code == 405
+    with (Path(site.root) / "var/log/apache/access_log").open(encoding="utf-8") as apache_log_file:
+        for line in apache_log_file:
+            if re.match(
+                r'^.*"PROPFIND /\w+/check_mk/ HTTP/1.1" 405 \d+ "-" "' + user_agent + '"$', line
+            ):
+                return
+    pytest.fail("Could not find regex in logfile")

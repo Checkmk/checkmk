4f50784b8ab553621cc721fe0592e8fed1fc5ad9
Joerg Herbel <joerg.herbel@checkmk.com>
2023-06-23 12:52:06 +0200

    Finalized groovy script for extension compatibility tests
    Change-Id: I6f50dbe38ec62971221b4210790987e3a39546e8

--- buildscripts/scripts/test-extension-compatibility.groovy
+++ buildscripts/scripts/test-extension-compatibility.groovy
@@ -1,16 +1,36 @@
 #!groovy
-​
+
 /// file: test-extension-compatibility.groovy
-​
+
 def main() {
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
     def testing_helper = load("${checkout_dir}/buildscripts/scripts/utils/integration.groovy");
     def branch_name = versioning.safe_branch_name(scm);
-​
+
     check_environment_variables([
         "DOCKER_TAG",
     ]);
-​
+            
+    stage("Check for extension actuality") {
+        dir("${checkout_dir}") {
+            docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
+                docker_image_from_alias("IMAGE_TESTING").inside() {
+                    catchError(buildResult: 'UNSTABLE', stageResult: 'UNSTABLE') {
+                        sh("""
+                            scripts/run-pipenv run \
+                              tests/extension_compatibility/output_extensions_sorted_by_downloads.py \
+                                | sed -n "1,\$(wc -l < tests/extension_compatibility/current_extensions_under_test.txt)p" \
+                                > /tmp/extension_compatibility.txt
+                            diff -u --color \
+                                tests/extension_compatibility/current_extensions_under_test.txt \
+                                /tmp/extension_compatibility.txt
+                        """);
+                    }
+                }
+            }
+        }
+    }
+
     testing_helper.run_make_targets(
         DOCKER_GROUP_ID: get_docker_group_id(),
         DISTRO_LIST: ["ubuntu-20.04"],

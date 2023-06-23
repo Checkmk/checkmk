0b59dddcedfe4439116fa2f6a652136aec800dd6
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-06-23 23:16:09 +0200

    move test where it belongs
    Change-Id: I1596e3919713116bb6f0f091c6a6c0755073ab78

--- tests/unit/checks/test_ac_check_dns.py
+++ tests/unit/checks/test_ac_check_dns.py
@@ -9,8 +9,6 @@ import pytest
 
 from tests.testlib import ActiveCheck
 
-from cmk.gui.plugins.wato.active_checks.dns import _migrate
-
 
 @pytest.mark.parametrize(
     "params, result",
@@ -83,77 +81,3 @@ def test_ac_check_dns_expected_addresses(
 ) -> None:
     active_check = ActiveCheck("check_dns")
     assert active_check.run_argument_function(params) == result
-
-
-@pytest.mark.parametrize(
-    "params, result",
-    [
-        [
-            (
-                "google.de",
-                {},
-            ),
-            {
-                "hostname": "google.de",
-                "server": None,
-            },
-        ],
-        [
-            (
-                "google.de",
-                {"expected_address": "1.2.3.4,C0FE::FE11"},
-            ),
-            {
-                "hostname": "google.de",
-                "server": None,
-                "expect_all_addresses": True,
-                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
-            },
-        ],
-        [
-            (
-                "google.de",
-                {
-                    "expected_address": "1.2.3.4,C0FE::FE11",
-                    "server": "127.0.0.53",
-                    "timeout": 10,
-                    "response_time": (1.0, 2.0),
-                    "expected_authority": True,
-                },
-            ),
-            {
-                "hostname": "google.de",
-                "expect_all_addresses": True,
-                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
-                "server": "127.0.0.53",
-                "timeout": 10,
-                "response_time": (1.0, 2.0),
-                "expected_authority": True,
-            },
-        ],
-        [
-            (
-                "google.de",
-                {
-                    "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
-                    "server": "127.0.0.53",
-                    "timeout": 10,
-                    "response_time": (1.0, 2.0),
-                    "expected_authority": True,
-                },
-            ),
-            {
-                "hostname": "google.de",
-                "expected_addresses_list": ["1.2.3.4", "C0FE::FE11"],
-                "server": "127.0.0.53",
-                "timeout": 10,
-                "response_time": (1.0, 2.0),
-                "expected_authority": True,
-            },
-        ],
-    ],
-)
-def test_legacy_params(
-    params: Mapping[str, object] | tuple[str, Mapping[str, object]], result: Mapping[str, object]
-) -> None:
-    assert _migrate(params) == result

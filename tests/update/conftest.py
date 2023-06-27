6e91eef55a8d5cc1a2096447527f3cf0d9987479
Matteo Stifano <matteo.stifano@checkmk.com>
2023-06-27 15:15:18 +0200

    Restart httpd within a utils function
    Since such functionality is used in more than one test suite, it is
    moved in the common utils module.
    
    Change-Id: I1108804415acf42923b10cd00c4d5dea7d399705

--- tests/update/conftest.py
+++ tests/update/conftest.py
@@ -22,8 +22,8 @@ from tests.testlib.agent import (
 from tests.testlib.site import Site, SiteFactory
 from tests.testlib.utils import (
     current_base_branch_name,
-    execute,
     PExpectDialog,
+    restart_httpd,
     spawn_expect_process,
 )
 from tests.testlib.version import CMKVersion, version_gte
@@ -279,14 +279,7 @@ def _get_site(
     assert site.is_running(), "Site is not running!"
     logger.info("Test-site %s is up", site.id)
 
-    if os.environ.get("DISTRO") in {"centos-7", "centos-8", "almalinux-9"}:
-        # TODO: copied from composition-tests. Refactor to avoid code-duplication
-        # On RHEL-based distros, such as CentOS and AlmaLinux, we have to manually restart httpd
-        # after creating a new site. Otherwise, the site's REST API won't be reachable via port 80,
-        # preventing e.g. the controller from querying the agent receiver port.
-        # Note: the mere presence of httpd is not enough to determine whether we have to restart or
-        # not, see eg. sles-15sp4.
-        execute(["sudo", "httpd", "-k", "restart"])
+    restart_httpd()
 
     site_version, site_edition = get_omd_version(site).rsplit(".", 1)
     assert (

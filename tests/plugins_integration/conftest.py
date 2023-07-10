5af961ee30c174bb9aba992315de92e4566082e5
René Slowenski <rene.slowenski@checkmk.com>
2023-07-10 15:50:17 +0200

    Plugins test: Splitted into single and bulk mode
    Change-Id: I75bafe64cab409b77f4d6b7f6c46ac271ffdb387

--- tests/plugins_integration/conftest.py
+++ tests/plugins_integration/conftest.py
@@ -3,6 +3,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 import logging
 import os
+import re
 from collections.abc import Iterator
 
 import pytest
@@ -11,7 +12,7 @@ from tests.testlib.site import get_site_factory, Site
 from tests.testlib.utils import execute
 
 from tests.plugins_integration import constants
-from tests.plugins_integration.checks import get_host_names
+from tests.plugins_integration.checks import cleanup_hosts, get_host_names, setup_hosts
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,110 +24,128 @@ def pytest_addoption(parser):
         default=False,
         help="Store checks-output files to be used as static references",
     )
-
     parser.addoption(
         "--skip-cleanup",
         action="store_true",
         default=False,
-        help="Skip cleanup process after tests' execution",
+        help="Skip cleanup process after test execution",
+    )
+    parser.addoption(
+        "--skip-masking",
+        action="store_true",
+        default=False,
+        help="Skip regexp masking during check validation",
+    )
+    parser.addoption(
+        "--bulk-mode",
+        action="store_true",
+        default=False,
+        help="Enable bulk mode execution",
+    )
+    parser.addoption(
+        "--chunk-index",
+        action="store",
+        type=int,
+        default=0,
+        help="Chunk index for bulk mode",
+    )
+    parser.addoption(
+        "--chunk-size",
+        action="store",
+        type=int,
+        default=100,
+        help="Chunk size for bulk mode",
+    )
+    parser.addoption(
+        "--host-names",
+        action="store",
+        default="",
+        help="Host name allow list",
+    )
+    parser.addoption(
+        "--check-names",
+        action="store",
+        default="",
+        help="Check name allow list",
     )
 
 
+def pytest_collection_modifyitems(config, items):
+    """Enable or disable tests based on their bulk mode"""
+    BULK_MODE = config.getoption(name="--bulk-mode")
+    items[:] = [_ for _ in items if _.name.startswith("test_bulk") == BULK_MODE]
+    if BULK_MODE:
+        chunk_index = config.getoption(name="--chunk-index")
+        chunk_size = config.getoption(name="--chunk-size")
+        items[:] = items[chunk_index * chunk_size : (chunk_index + 1) * chunk_size]
+    print(f"selected {len(items)} items")
+    # parse additional options that control the test execution
+    if config.getoption(name="--host-names"):
+        constants.HOST_NAMES = config.getoption(name="--host-names").split(",")
+    if config.getoption(name="--check-names"):
+        constants.CHECK_NAMES = config.getoption(name="--check-names").split(",")
+
+
 @pytest.fixture(name="test_site", scope="session")
-def get_site() -> Iterator[Site]:
-    yield from get_site_factory(prefix="plugins_", update_from_git=False).get_test_site()
-
-
-@pytest.fixture(scope="session")
-def setup(test_site: Site, request: pytest.FixtureRequest) -> Iterator:
+def _get_site(request: pytest.FixtureRequest) -> Iterator[Site]:
     """Setup test-site and perform cleanup after test execution."""
-    dump_path = test_site.path(f"var/check_mk/{constants.DUMP_DIR}")
+    for site in get_site_factory(prefix="plugins_", update_from_git=False).get_test_site():
+        dump_path = site.path(f"var/check_mk/{constants.DUMP_DIR}")
+        # NOTE: the snmpwalks folder cannot be changed!
+        walk_path = site.path("var/check_mk/snmpwalks")
+        # create dump folder in the test site
+        LOGGER.info('Creating folder "%s"...', dump_path)
+        rc = site.execute(["mkdir", "-p", dump_path]).wait()
+        assert rc == 0
 
-    # create dump folder in the test site
-    LOGGER.info('Creating folder "%s"...', dump_path)
-    rc = test_site.execute(["mkdir", "-p", dump_path]).wait()
-    assert rc == 0
+        LOGGER.info("Injecting agent-output...")
+        for file_name in os.listdir(constants.DUMP_DIR_PATH):
+            assert (
+                execute(
+                    [
+                        "sudo",
+                        "cp",
+                        "-f",
+                        f"{constants.DUMP_DIR_PATH}/{file_name}",
+                        f"{walk_path}/{file_name}"
+                        if re.search(r"\bsnmp\b", file_name)
+                        else f"{dump_path}/{file_name}",
+                    ]
+                ).returncode
+                == 0
+            )
 
-    LOGGER.info("Injecting agent-output...")
-    assert execute(["sudo", "cp", "-r", f"{constants.DUMP_DIR_PATH}/.", dump_path]).returncode == 0
+        for dump_type in constants.DUMP_TYPES:
+            host_folder = f"/{dump_type}"
+            if site.openapi.get_folder(host_folder):
+                LOGGER.info('Host folder "%s" already exists!', host_folder)
+            else:
+                LOGGER.info('Creating host folder "%s"...', host_folder)
+                site.openapi.create_folder(host_folder)
+            ruleset_name = "usewalk_hosts" if dump_type == "snmp" else "datasource_programs"
+            LOGGER.info('Creating rule "%s"...', ruleset_name)
+            site.openapi.create_rule(
+                ruleset_name=ruleset_name,
+                value=(True if dump_type == "snmp" else f"cat {dump_path}/<HOST>"),
+                folder=host_folder,
+            )
+            LOGGER.info('Rule "%s" created!', ruleset_name)
 
-    for dump_type in constants.DUMP_TYPES:
-        host_folder = f"/{dump_type}"
-        if test_site.openapi.get_folder(host_folder):
-            LOGGER.info('Host folder "%s" already exists!', host_folder)
-        else:
-            LOGGER.info('Creating host folder "%s"...', host_folder)
-            test_site.openapi.create_folder(host_folder)
-        ruleset_name = "usewalk_hosts" if dump_type == "snmp" else "datasource_programs"
-        LOGGER.info('Creating rule "%s"...', ruleset_name)
-        test_site.openapi.create_rule(
-            ruleset_name=ruleset_name,
-            value=(True if dump_type == "snmp" else f"cat {dump_path}/<HOST>"),
-            folder=host_folder,
-        )
-        LOGGER.info('Rule "%s" created!', ruleset_name)
-        LOGGER.info("Getting host names...")
-        host_names = [_ for _ in get_host_names() if ("snmp" in _) == (dump_type == "snmp")]
-        LOGGER.info("Creating hosts...")
-        host_attributes = {
-            "ipaddress": "127.0.0.1",
-            "tag_agent": ("cmk-agent" if dump_type == "agent" else "no-agent"),
-        }
-        if dump_type == "snmp":
-            host_attributes["tag_snmp_ds"] = "snmpv2"
-        test_site.openapi.bulk_create_hosts(
-            [
-                {
-                    "host_name": host_name,
-                    "folder": host_folder,
-                    "attributes": host_attributes,
-                }
-                for host_name in host_names
-            ],
-            bake_agent=False,
-            ignore_existing=True,
-        )
+        yield site
 
-    LOGGER.info("Activating changes & reloading core...")
-    test_site.activate_changes_and_wait_for_core_reload()
+        if os.getenv("CLEANUP", "1") == "1" and not request.config.getoption("--skip-cleanup"):
+            # cleanup existing agent-output folder in the test site
+            LOGGER.info('Removing folder "%s"...', dump_path)
+            assert execute(["sudo", "rm", "-rf", dump_path]).returncode == 0
 
-    LOGGER.info("Running update-config...")
-    assert test_site.execute(["cmk-update-config"]).wait() == 0
 
-    LOGGER.info("Running service discovery...")
-    if os.getenv("CMK_SERVICE_DISCOVERY") == "1":
-        assert test_site.execute(["cmk", "-vI"]).wait() == 0
-    else:
-        test_site.openapi.bulk_discover_services(host_names, bulk_size=10, wait_for_completion=True)
-
-    LOGGER.info("Activating changes & reloading core...")
-    test_site.activate_changes_and_wait_for_core_reload()
-
-    LOGGER.info("Checking for pending services...")
-    pending_checks = {_: test_site.openapi.get_host_services(_, pending=True) for _ in host_names}
-    num_tries = 3
-    for _ in range(num_tries):
-        for host_name in list(pending_checks.keys())[:]:
-            test_site.schedule_check(host_name, "Check_MK", 0, 60)
-            pending_checks[host_name] = test_site.openapi.get_host_services(host_name, pending=True)
-            if len(pending_checks[host_name]) == 0:
-                pending_checks.pop(host_name, None)
-                continue
-
-    for host_name in pending_checks:
-        LOGGER.info(
-            '%s pending service(s) found on host "%s": %s',
-            len(pending_checks[host_name]),
-            host_name,
-            ",".join(
-                _.get("extensions", {}).get("description", _.get("id"))
-                for _ in pending_checks[host_name]
-            ),
-        )
-
+@pytest.fixture(name="bulk_setup", scope="session")
+def _bulk_setup(test_site: Site, pytestconfig: pytest.Config) -> Iterator:
+    """Setup multiple test hosts."""
+    LOGGER.info("Getting host names...")
+    chunk_index = pytestconfig.getoption(name="--chunk-index")
+    chunk_size = pytestconfig.getoption(name="--chunk-size")
+    host_names = get_host_names()[chunk_index * chunk_size : (chunk_index + 1) * chunk_size]
+    setup_hosts(test_site, host_names)
     yield
-
-    if not request.config.getoption("--skip-cleanup"):
-        # cleanup existing agent-output folder in the test site
-        LOGGER.info('Removing folder "%s"...', dump_path)
-        assert execute(["sudo", "rm", "-rf", dump_path]).returncode == 0
+    cleanup_hosts(test_site, host_names)
---
+++ tests/scripts/merge-junit-suites.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""Used to merge bulk-mode chunk report."""
+import os
+import sys
+from xml.etree import ElementTree
+
+args = sys.argv[1:]
+argc = len(args)
+
+stats_only = "--stats-only" in args
+print_stats = stats_only or "--stats" in args
+if any(_.startswith("-") for _ in args):
+    files_idx = min(args.index(_) for _ in args if not _.startswith("-"))
+    files = args[files_idx : min(args.index(_, files_idx) for _ in args if _.startswith("-"))]
+else:
+    files = args
+input_files = files if stats_only else files[0:-1]
+output_file = None if stats_only else files[-1]
+pytest_suite_name = (
+    args[args.index("--pytest-suite-name") + 1] if "--pytest-suite-name" in args else "pytest"
+)
+
+if len(files) < 1 and stats_only:
+    print("Insufficient arguments to generate statistics! Please provide one or more input files.")
+    sys.exit(1)
+if len(files) < 2 and not stats_only:
+    print(
+        "Insufficient arguments to merge reports! Please provide one or more input files and one output file."
+    )
+    sys.exit(1)
+
+if not all(os.path.exists(file) for file in input_files):
+    print("Please make sure all input files exist and can be read!")
+    sys.exit(2)
+
+
+if output_file and os.path.exists(output_file):
+    print(f'Output file "{output_file}" already exists!')
+    sys.exit(3)
+
+tree = ElementTree.parse(input_files[0])
+root = tree.getroot()
+
+# merge additional source reports (if any)
+for input_file in input_files[1:]:
+    for _ in ElementTree.parse(input_file).getroot():
+        root.append(_)
+
+# override default pytest suite name (if specified)
+if pytest_suite_name != "pytest":
+    for _ in root.findall("testsuite"):
+        if _.attrib.get("name") == "pytest":
+            _.attrib["name"] = pytest_suite_name
+
+suites: dict[str, dict] = {str(_.attrib.get("name")): {} for _ in root}
+sum_attr = {"errors", "failures", "skipped", "tests", "time"}
+set_attr = {"hostname"}
+min_attr = {"timestamp"}
+
+# merge suites of same name and get combined statistics
+for suite in suites:
+    nodes = root.findall(f"testsuite[@name='{suite}']")
+    last_node_idx = len(nodes) - 1
+    for node_idx, child in enumerate(reversed(nodes)):
+        for attr in sum_attr:
+            if child.attrib.get(attr).isdecimal():
+                suites[suite][attr] = suites[suite].get(attr, 0) + int(child.attrib.get(attr))
+                continue
+            suites[suite][attr] = suites[suite].get(attr, 0.0) + float(child.attrib.get(attr))
+        for attr in set_attr:
+            items = suites[suite].get(attr, "").split(",") + [child.attrib.get(attr)]
+            suites[suite][attr] = ", ".join(set(_ for _ in items if _))
+        for attr in min_attr:
+            suites[suite][attr] = min(
+                child.attrib.get(attr), suites[suite].get(attr, child.attrib.get(attr))
+            )
+        if node_idx < last_node_idx:
+            for grandchild in child:
+                if node := root.find(f"testsuite[@name='{suite}']"):
+                    node.append(grandchild)
+            root.remove(child)
+    for key in suites[suite]:
+        if node := root.find(f"testsuite[@name='{suite}']"):
+            node.attrib[key] = str(suites[suite][key])
+        if print_stats:
+            print(f"{suite}.{key}: {suites[suite][key]}")
+
+if output_file and not stats_only:
+    tree.write(output_file)

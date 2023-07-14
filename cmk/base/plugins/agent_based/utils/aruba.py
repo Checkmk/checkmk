c431ecf8cecfb3156741df1b15b7554e60812aba
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-07-14 11:34:29 +0200

    15865 FIX Aruba 2930M: Fix detection
    FEED-7358
    
    Change-Id: I9b14900f330cf2261e76dcab4e60c3200651789b

--- cmk/base/plugins/agent_based/utils/aruba.py
+++ cmk/base/plugins/agent_based/utils/aruba.py
@@ -4,7 +4,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from ..agent_based_api.v1 import startswith
+from ..agent_based_api.v1 import matches, startswith
 
-DETECT_2930M = startswith(".1.3.6.1.2.1.1.1.0", "Aruba 2930M")
+DETECT_2930M = matches(".1.3.6.1.2.1.1.1.0", "Aruba.+2930M.*")
 DETECT_WLC = startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.14823.1.1")

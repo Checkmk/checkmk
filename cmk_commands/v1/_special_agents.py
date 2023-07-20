69b0e8662e75f6f3575a019beb25812494337456
Sofia Colakovic <sofia.colakovic@checkmk.com>
2023-07-20 10:26:40 +0200

    command API: API first version
    Change-Id: Ic864b374e486fef4c63b9f7155368fd8d7b87f9c

---
+++ cmk_commands/v1/_special_agents.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections.abc import Iterable, Mapping, Sequence
+from dataclasses import dataclass
+from typing import Callable, Generic, TypeVar
+
+from cmk_commands.v1._utils import EnvironmentConfig, HostConfig, Secret
+
+_T = TypeVar("_T")
+
+
+@dataclass(frozen=True)
+class SpecialAgentConfig:
+    command_arguments: Sequence[str | Secret]
+    stdin: str | None = None
+
+
+@dataclass(frozen=True)
+class SpecialAgentCommand(Generic[_T]):
+    name: str
+    parameter_parser: Callable[[Mapping[str, object]], _T]
+    config_function: Callable[[_T, HostConfig, EnvironmentConfig], Iterable[SpecialAgentConfig]]

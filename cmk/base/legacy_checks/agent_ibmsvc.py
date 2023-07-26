17f2bb558620fbeba088207f3c6467f87c23a225
Frans Fürst <frans.fuerst@checkmk.com>
2023-07-26 16:28:29 +0200

    13270 FIX agent_ibmsvc: option -i/--modules not recognized without --accept-any-hostkey
    The information regarding 'Retrieve information about...' provided for the `ibmsvc` special agent
    would be ignored if 'Accept any SSH Host Key' was unchecked.
    This change makes the information about modules be used unconditionally.
    
    Change-Id: I8ef7051977c951f24d04e33a933f55a771a88ea7

--- cmk/base/legacy_checks/agent_ibmsvc.py
+++ cmk/base/legacy_checks/agent_ibmsvc.py
@@ -12,9 +12,9 @@ from cmk.base.config import special_agent_info
 def agent_ibmsvc_arguments(
     params: Mapping[str, Any], hostname: str, ipaddress: Optional[str]
 ) -> Sequence[str]:
-    args = ["-u", params["user"]]
+    args = ["-u", params["user"], "-i", ",".join(params["infos"])]
     if params["accept-any-hostkey"] is True:
-        args += ["--accept-any-hostkey", "-i", ",".join(params["infos"])]
+        args += ["--accept-any-hostkey"]
 
     args.append(ipaddress or hostname)
     return args
--- tests/unit/checks/test_agent_ibmsvc.py
+++ tests/unit/checks/test_agent_ibmsvc.py
@@ -40,9 +40,9 @@ pytestmark = pytest.mark.checks
             [
                 "-u",
                 "user",
-                "--accept-any-hostkey",
                 "-i",
                 "lshost,lslicense,lsmdisk,lsmdiskgrp,lsnode,lsnodestats,lssystem,lssystemstats,lsportfc,lsenclosure,lsenclosurestats,lsarray,disks",
+                "--accept-any-hostkey",
                 "address",
             ],
         ),
@@ -66,7 +66,13 @@ pytestmark = pytest.mark.checks
                 "user": "user",
                 "accept-any-hostkey": False,
             },
-            ["-u", "user", "address"],
+            [
+                "-u",
+                "user",
+                "-i",
+                "lshost,lslicense,lsmdisk,lsmdiskgrp,lsnode,lsnodestats,lssystem,lssystemstats,lsportfc,lsenclosure,lsenclosurestats,lsarray,disks",
+                "address",
+            ],
         ),
     ],
 )

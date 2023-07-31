c70b3aa98a0547bbcc745934d96c4319e562a5a0
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-07-31 10:43:00 +0200

    cpu_load: refactor
    We'll apply this for all three types of
    values soon.
    
    SUP-13363
    
    Change-Id: I64adb5fe4bb0224c362d64ce52134e1b4397c4e5

--- cmk/base/plugins/agent_based/utils/cpu_load.py
+++ cmk/base/plugins/agent_based/utils/cpu_load.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Any, Dict, Tuple, TypedDict, Union
+from typing import Any, Dict, Literal, Tuple, TypedDict, Union
 
 from ..agent_based_api.v1 import check_levels, check_levels_predictive, Metric, Result, State
 from ..agent_based_api.v1.type_defs import CheckResult
@@ -25,37 +25,12 @@ def _processor_type_info(proc_type: ProcessorType) -> str:
 
 
 def check_cpu_load(params: CPULoadParams, section: Section) -> CheckResult:
-    levels = params.get("levels")
-    num_cpus = section.num_cpus
-    label = "15 min load"
-
-    if isinstance(levels, dict):
-        # predictive levels
-        yield from check_levels_predictive(
-            section.load.load15,
-            levels=levels,
-            metric_name="load15",
-            label=label,
-        )
-    else:
-        # fixed level thresholds
-        levels_upper = None
-        if isinstance(levels, tuple):
-            # warning and critical levels are dependent on cpu count;
-            # rule defines levels for one cpu.
-            levels_upper = (levels[0] * num_cpus, levels[1] * num_cpus)
-        yield from check_levels(
-            section.load.load15,
-            metric_name="load15",
-            levels_upper=levels_upper,
-            label=label,
-        )
-
-    # provide additional info text
-    yield Result(
-        state=State.OK,
-        summary=f"15 min load per core: {(section.load.load15/num_cpus):.2f} "
-        f"({num_cpus} {_processor_type_info(section.type)}cores)",
+    yield from _check_cpu_load_type(
+        params.get("levels"),
+        section.load.load15,
+        "15",
+        section.num_cpus,
+        _processor_type_info(section.type),
     )
 
     for level_name, level_value in section.load._asdict().items():
@@ -67,5 +42,40 @@ def check_cpu_load(params: CPULoadParams, section: Section) -> CheckResult:
             level_name,
             level_value,
             # upper bound of load1 is used for displaying cpu count in graph title
-            boundaries=(0, num_cpus),
+            boundaries=(0, section.num_cpus),
         )
+
+
+def _check_cpu_load_type(
+    levels: dict[str, Any] | tuple[float, float] | None,
+    value: float,
+    avg: Literal["15"],
+    num_cpus: int,
+    proc_name: str,
+) -> CheckResult:
+    label = f"{avg} min load"
+
+    if isinstance(levels, dict):
+        # predictive levels
+        yield from check_levels_predictive(
+            value,
+            levels=levels,
+            metric_name=f"load{avg}",
+            label=label,
+        )
+    else:
+        # warning and critical levels are dependent on cpu count;
+        # rule defines levels for one cpu.
+        levels_upper = (
+            (levels[0] * num_cpus, levels[1] * num_cpus) if isinstance(levels, tuple) else None
+        )
+        yield from check_levels(
+            value,
+            metric_name=f"load{avg}",
+            levels_upper=levels_upper,
+            label=label,
+        )
+
+    # provide additional info text
+    per_core_txt = f"{avg} min load per core: {(value/num_cpus):.2f} ({num_cpus} {proc_name}cores)"
+    yield Result(state=State.OK, summary=per_core_txt)

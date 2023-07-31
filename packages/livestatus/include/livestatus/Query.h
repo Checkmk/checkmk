e310481054b414b4e0f704ad80b03b2f8358f4dc
Sven Panne <sven.panne@checkmk.com>
2023-07-31 13:18:58 +0200

    Moved ParsedQuery to separate files.
    Change-Id: I6143cc817b5209747fa34995f9c20b43b2612b46

--- packages/livestatus/include/livestatus/Query.h
+++ packages/livestatus/include/livestatus/Query.h
@@ -10,89 +10,22 @@
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
-#include <functional>
 #include <list>
 #include <map>
 #include <memory>
 #include <optional>
 #include <string>
 #include <unordered_set>
-#include <utility>
 #include <vector>
 
 #include "livestatus/Aggregator.h"  // IWYU pragma: keep
 #include "livestatus/Filter.h"
-#include "livestatus/OutputBuffer.h"
+#include "livestatus/ParsedQuery.h"
 #include "livestatus/Renderer.h"
-#include "livestatus/RendererBrokenCSV.h"
-#include "livestatus/Row.h"
-#include "livestatus/StatsColumn.h"
-#include "livestatus/Triggers.h"
-#include "livestatus/User.h"
-class Column;
 class Logger;
+class OutputBuffer;
+class Row;
 class Table;
-
-struct ParsedQuery {
-    ParsedQuery(const std::list<std::string> &lines, const Table &table,
-                OutputBuffer &output);
-
-    std::unordered_set<std::string> all_column_names;
-    std::vector<std::shared_ptr<Column>> columns;
-    std::unique_ptr<Filter> filter;
-    std::unique_ptr<Filter> wait_condition;
-    std::vector<std::unique_ptr<StatsColumn>> stats_columns;
-    bool show_column_headers{true};
-    int limit{-1};
-    std::optional<
-        std::pair<std::chrono::seconds, std::chrono::steady_clock::time_point>>
-        time_limit;
-    CSVSeparators separators{"\n", ";", ",", "|"};
-    OutputFormat output_format{OutputFormat::broken_csv};
-    bool keepalive{false};
-    OutputBuffer::ResponseHeader response_header{
-        OutputBuffer::ResponseHeader::off};
-    std::unique_ptr<const User> user;
-    std::chrono::milliseconds wait_timeout{0};
-    Triggers::Kind wait_trigger{Triggers::Kind::all};
-    Row wait_object{nullptr};
-    std::chrono::seconds timezone_offset{0};
-
-private:
-    using ColumnCreator =
-        std::function<std::shared_ptr<Column>(const std::string &)>;
-
-    using FilterStack = Filters;
-
-    using LogicalConnective =
-        std::function<std::unique_ptr<Filter>(Filter::Kind, const Filters &)>;
-
-    void parseFilterLine(char *line, FilterStack &filters,
-                         const ColumnCreator &make_column);
-    void parseStatsLine(char *line, const ColumnCreator &make_column);
-    static void parseAndOrLine(char *line, Filter::Kind kind,
-                               const LogicalConnective &connective,
-                               FilterStack &filters);
-    static void parseNegateLine(char *line, FilterStack &filters);
-    void parseStatsAndOrLine(char *line, const LogicalConnective &connective);
-    void parseStatsNegateLine(char *line);
-    void parseColumnsLine(const char *line, const ColumnCreator &make_column);
-    void parseColumnHeadersLine(char *line);
-    void parseLimitLine(char *line);
-    void parseTimelimitLine(char *line);
-    void parseSeparatorsLine(char *line);
-    void parseOutputFormatLine(const char *line);
-    void parseKeepAliveLine(char *line);
-    void parseResponseHeaderLine(char *line);
-    void parseAuthUserHeader(const char *line,
-                             const std::function<std::unique_ptr<const User>(
-                                 const std::string &name)> &find_user);
-    void parseWaitTimeoutLine(char *line);
-    void parseWaitTriggerLine(char *line);
-    void parseWaitObjectLine(
-        const char *line, const std::function<Row(const std::string &)> &get);
-    void parseLocaltimeLine(char *line);
-};
 
 class Query {
 public:

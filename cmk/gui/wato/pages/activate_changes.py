9a6e124f14f3a0c99f302c8dd0e4834fd3d91928
Kenneth Okoh <kenneth.okoh@checkmk.com>
2023-08-01 13:26:59 +0200

    16041 New permissions "Discard changes" and "Discard foreign changes"
    We introduce two new permissions that let administrators allow/disallow users to discard changes.
    * "Discard changes" lets a user discard their own pending changes; defaults to "yes" for the role "normal monitoring user".
    * "Discard foreign changes" lets a user discard other users' pending changes (foreign changes); defaults to "no" for the role "normal monitoring user".
    
    SUP-8678
    
    Change-Id: I38068cec14029e052646ce66c8f303ca059e6f7b

--- cmk/gui/wato/pages/activate_changes.py
+++ cmk/gui/wato/pages/activate_changes.py
@@ -180,7 +180,7 @@ class ModeActivateChanges(WatoMode, activate_changes.ActivateChanges):
             )
 
     def _page_menu_entries_all_sites(self) -> Iterator[PageMenuEntry]:
-        if not self._may_activate_changes():
+        if not self._may_discard_changes():
             return
 
         yield PageMenuEntry(
@@ -195,21 +195,26 @@ class ModeActivateChanges(WatoMode, activate_changes.ActivateChanges):
         if not self._may_activate_changes():
             return
 
-        if self._may_activate_changes():
-            yield PageMenuEntry(
-                title=_("Activate on selected sites"),
-                icon_name={
-                    "icon": "save",
-                    "emblem": "refresh",
-                },
-                item=make_javascript_link('cmk.activation.activate_changes("selected")'),
-                name="activate_selected",
-                is_shortcut=True,
-                is_suggested=True,
-                is_enabled=self.has_changes(),
-            )
+        yield PageMenuEntry(
+            title=_("Activate on selected sites"),
+            icon_name={
+                "icon": "save",
+                "emblem": "refresh",
+            },
+            item=make_javascript_link('cmk.activation.activate_changes("selected")'),
+            name="activate_selected",
+            is_shortcut=True,
+            is_suggested=True,
+            is_enabled=self.has_changes(),
+        )
 
     def _may_discard_changes(self) -> bool:
+        if not user.may("wato.discard"):
+            return False
+
+        if not user.may("wato.discardforeign") and self._has_foreign_changes_on_any_site():
+            return False
+
         if not self._may_activate_changes():
             return False
 

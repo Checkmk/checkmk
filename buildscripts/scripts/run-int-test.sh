37204e4cd4bed33ce0425a7c4633c2375d8448e5
Frans Fürst <frans.fuerst@checkmk.com>
2023-08-03 15:42:08 +0200

    fix 'test-docker' test script
    Change-Id: Iec55d6d7daec16733b541bed1414494a0c7cb6eb

--- buildscripts/scripts/run-int-test.sh
+++ buildscripts/scripts/run-int-test.sh
@@ -4,7 +4,6 @@ export RESULTS_DIR="$WORKSPACE/results"
 mkdir -p "$RESULTS_DIR"
 rm -rf "$RESULTS_DIR/"*
 
-export PYTEST_ADDOPTS="--junitxml='$RESULTS_DIR/junit.xml'"
 export BRANCH="2.0.0"
 export CMK_VERSION="${BRANCH}-$(date "+%F" | tr - .)"
 PKG_URL="https://download.checkmk.com/checkmk/${CMK_VERSION}/check-mk-enterprise-${CMK_VERSION}_0.buster_amd64.deb"
@@ -22,11 +21,11 @@ DOCKER_RUN_ADDOPTS="\
     --network=host \
     -e BRANCH \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $WORKSPACE:$WORKSPACE:ro \
+    -v $WORKSPACE:$WORKSPACE \
     -v $HOME/.cmk-credentials:$HOME/.cmk-credentials:ro \
     -v $HOME/.docker/config.json:$HOME/.docker/config.json:ro \
     " \
 IMAGE_ID="artifacts.lan.tribe29.com:4000/ubuntu-20.04:2.0.0-latest" \
-PYTEST_ADDOPTS="-x" \
+PYTEST_ADDOPTS="--junitxml='$RESULTS_DIR/junit.xml'" \
 scripts/run-in-docker.sh make -C tests test-docker
 

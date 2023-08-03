28eb34678431bfdca7ed5c3c71394acb43b0e380
Alex Zurhake <alex.zurhake@checkmk.com>
2023-08-03 13:18:22 +0200

    Limit Bazel to 4 parallel executions
    Change-Id: I77a09ae323493f7cae56ed21ea7990cb50bfca1a

--- scripts/run-bazel-build.sh
+++ scripts/run-bazel-build.sh
@@ -69,6 +69,7 @@ bazel build \
     --host_action_env=PATH="$PATH" \
     --host_action_env=SYSTEM_DIGEST="$SYSTEM_DIGEST" \
     --experimental_ui_max_stdouterr_bytes=10000000 \
+    --jobs=4 \
     "${BAZEL_REMOTE_CACHE_ARGUMENT}" \
     ${BAZEL_EXTRA_ARGS} \
     "${TARGET}"

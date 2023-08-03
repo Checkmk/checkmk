ea7797470dbd3be1fa236866f55971aec694332a
Frans Fürst <frans.fuerst@checkmk.com>
2023-08-03 16:35:33 +0200

    Fix 'compile-all-werks' pipeline script
    Change-Id: I4385b23c0c895aeb161b2dade56473ea82bf498c

--- buildscripts/scripts/compile-all-werks.groovy
+++ buildscripts/scripts/compile-all-werks.groovy
@@ -1,15 +1,15 @@
 #!groovy
 
 /// file: compile-all-werks.groovy
+
 def provide_clone(repo_name, credentials_id) {
     dir("${WORKSPACE}/${repo_name}") {
         checkout([$class: "GitSCM",
             userRemoteConfigs: [[
                 credentialsId: credentials_id,
                 url: "ssh://jenkins@review.lan.tribe29.com:29418/${repo_name}",
-                // refspec: "${refspec}",
             ]],
-            // branches: [new hudson.plugins.git.BranchSpec(branches_str)],
+            branches: [new hudson.plugins.git.BranchSpec("FETCH_HEAD")],
             extensions: [
                 [$class: 'CloneOption',
                     // reference: "${reference_clone}",
@@ -20,23 +20,34 @@ def provide_clone(repo_name, credentials_id) {
 }
 
 def main() {
+    def docker_args = "${mount_reference_repo_dir}";
+
+    print(
+        """
+        |===== CONFIGURATION ===============================
+        |docker_args:.............. │${docker_args}│
+        |checkout_dir:............. │${checkout_dir}│
+        |===================================================
+        """.stripMargin());
+
     stage("Checkout repositories") {
-        provide_clone("cma", "ssh-git-gerrit-jenkins");
         provide_clone("checkmk_kube_agent", "ssh-git-gerrit-jenkins");
+        provide_clone("cma", "ssh-git-gerrit-jenkins");
     }
+
     stage("Compile werks") {
         docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
-            docker_image_from_alias("IMAGE_TESTING").inside() {
+            docker_image_from_alias("IMAGE_TESTING").inside("${docker_args}") {
                 dir("${checkout_dir}") {
                     sh("""
                     scripts/run-pipenv run echo build venv..
-                    scripts/run-pipenv run python3 -m cmk.utils.werks collect cmk ./cmk > cmk.json
+                    scripts/run-pipenv run python3 -m cmk.utils.werks collect cmk  ${checkout_dir} > cmk.json
                     scripts/run-pipenv run python3 -m cmk.utils.werks collect cma ${WORKSPACE}/cma > cma.json
                     scripts/run-pipenv run python3 -m cmk.utils.werks collect checkmk_kube_agent ${WORKSPACE}/checkmk_kube_agent > kube.json
 
                     # jq -s '.[0] * .[1] * .[2]' cma.json cmk.json kube.json > all_werks.json
                     # no need to install jq!!!!!
-                    python3 -c 'import json, sys; print(json.dumps({k: v for f in sys.argv[1:] for k, v in json.load(open(f)).items() }, ident=4))' \
+                    python3 -c 'import json, sys; print(json.dumps({k: v for f in sys.argv[1:] for k, v in json.load(open(f)).items()}, indent=4))' \
                         cmk.json \
                         cma.json \
                         kube.json \
@@ -46,9 +57,10 @@ def main() {
             }
         }
     }
+
     stage("Archive") {
         archiveArtifacts(
-            artifacts: "${WORKSPACE}/all_werks.json",
+            artifacts: "all_werks.json",
             fingerprint: true,
         )
     }

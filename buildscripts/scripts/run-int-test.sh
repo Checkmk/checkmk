9b9527684a1f58d566ca745a0f4de9d45bb38c07
Frans Fürst <frans.fuerst@checkmk.com>
2023-08-03 15:19:36 +0200

    Add dockerized 'test-docker wrapper script
    Change-Id: Id271b11d8997a3bbf06eb4b9cd5ae92b0b9d44ac

---
+++ buildscripts/scripts/run-int-test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+export RESULTS_DIR="$WORKSPACE/results"
+mkdir -p "$RESULTS_DIR"
+rm -rf "$RESULTS_DIR/"*
+
+export PYTEST_ADDOPTS="--junitxml='$RESULTS_DIR/junit.xml'"
+export BRANCH="2.0.0"
+export CMK_VERSION="${BRANCH}-$(date "+%F" | tr - .)"
+PKG_URL="https://download.checkmk.com/checkmk/${CMK_VERSION}/check-mk-enterprise-${CMK_VERSION}_0.buster_amd64.deb"
+
+echo "CMK_VERSION:$CMK_VERSION"
+echo "PKG_URL:$PKG_URL"
+
+cd "$WORKSPACE/check_mk"
+(cd docker; curl -O -u "$(cat ~/.cmk-credentials)" "$PKG_URL")
+
+#    -e HOME \
+#    -e WORKSPACE \
+
+DOCKER_RUN_ADDOPTS="\
+    --network=host \
+    -e BRANCH \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $WORKSPACE:$WORKSPACE:ro \
+    -v $HOME/.cmk-credentials:$HOME/.cmk-credentials:ro \
+    -v $HOME/.docker/config.json:$HOME/.docker/config.json:ro \
+    " \
+IMAGE_ID="artifacts.lan.tribe29.com:4000/ubuntu-20.04:2.0.0-latest" \
+PYTEST_ADDOPTS="-x" \
+scripts/run-in-docker.sh make -C tests test-docker
+

6cae207874d77af060f52d564f403afad97e97ee
Benedikt Seidl <benedikt.seidl@checkmk.com>
2023-08-10 08:04:05 +0200

    Better grammar for error message
    Follow up to Ia1597d80c187e99c02869f538a6612d5b58f0c4a
    and I569aa82747faff0ad2d94b44309e28d6bb08e889
    
    Change-Id: Ie7092afe6f6d51ddd25460bdaedf823567408c37

--- tests/gui_e2e/test_change_password.py
+++ tests/gui_e2e/test_change_password.py
@@ -92,7 +92,7 @@ def test_user_change_password_success(
         ("", "new", "cmk", "need to change your password"),
         ("cmk", "", "cmk", "new password must differ"),
         # Regression for Werk 14392 -- spaces are not stripped:
-        ("new", "new  ", "cmk", "new passwords do not match"),
+        ("new", "new  ", "cmk", "New passwords don't match"),
     ],
 )
 def test_user_change_password_errors(

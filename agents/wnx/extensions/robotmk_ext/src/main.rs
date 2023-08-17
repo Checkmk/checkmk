3303797f6fd56aa01f657409999bb655caa04487
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-08-17 20:23:56 +0200

    add to dummy robotmk possibility to log
    Write in c:\ log file to inform that system works as intended
    
    Change-Id: Ie9d789cf054a0aa68c5f2e124c2fee400a536987

--- agents/wnx/extensions/robotmk_ext/src/main.rs
+++ agents/wnx/extensions/robotmk_ext/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
-use std::io::Read;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::{thread, time};
 
 #[derive(Parser)]
 #[command(about = "RobotMk Windows agent extension.", version = "0.1")]
@@ -11,16 +13,26 @@ pub struct Cli {
 pub enum Mode {
     Daemon,
     Test,
+    WithLogInC,
 }
 
 fn main() {
     let cli = Cli::parse();
     match cli.mode {
         Mode::Test => println!("test"),
-        Mode::Daemon => {
-            println!("Press ENTER to stop daemon...");
-            let buffer = &mut [0u8];
-            std::io::stdin().read_exact(buffer).unwrap()
-        }
+        Mode::Daemon => loop {
+            thread::sleep(time::Duration::from_millis(1000));
+        },
+        Mode::WithLogInC => loop {
+            let mut f = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open("c:\\robot_mk_ext.log")
+                .unwrap();
+            thread::sleep(time::Duration::from_millis(1000));
+            let data = "This is just for test\n";
+            f.write_all(data.as_bytes()).expect("Unable to write data");
+        },
     }
 }

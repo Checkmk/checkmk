a1a91b07756aa56d1f83e93e576830544c71751b
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-08-18 12:57:56 +0200

    refactor a bit code for robotmk
    Change-Id: Ic623c0f935eb72fb2373d0f1c3f0c209bfdceef3

--- agents/wnx/src/engine/extensions.cpp
+++ agents/wnx/src/engine/extensions.cpp
@@ -38,33 +38,36 @@ Mode ToMode(std::string_view mode) {
     return Mode::no;
 }
 
+std::vector<Extension> GatherExtensions(const YAML::Node &group) {
+    auto executions = GetNode(group, vars::kExtensionsExecution);
+    std::vector<Extension> exts;
+    std::set<std::string> names;
+    for (auto &&entry : executions) {
+        const auto name = GetVal<std::string>(entry, vars::kExecutionName, {});
+        if (names.contains(name)) {
+            XLOG::t("duplicated name in extensions {}", name);
+            continue;
+        }
+        names.emplace(name);
+
+        const auto binary =
+            GetVal<std::string>(entry, vars::kExecutionBinary, "");
+        const auto command_line =
+            GetVal<std::string>(entry, vars::kExecutionCmdLine, "");
+        auto mode = GetVal<std::string>(entry, vars::kExecutionRun, "");
+        exts.emplace_back(Extension{.name = name,
+                                    .binary = binary,
+                                    .command_line = command_line,
+                                    .mode = ToMode(mode)});
+    }
+    return exts;
+}
+
 }  // namespace
 std::vector<Extension> GetAll(YAML::Node node) {
-    const auto x = GetNode(node, groups::kExtensions);
-    if (x.IsMap()) {
-        auto executions = GetNode(x, vars::kExtensionsExecution);
-        std::vector<Extension> exts;
-        std::set<std::string> names;
-        for (auto &&entry : executions) {
-            const auto name =
-                GetVal<std::string>(entry, vars::kExecutionName, {});
-            if (names.contains(name)) {
-                XLOG::t("duplicated name in extensions {}", name);
-                continue;
-            }
-            names.emplace(name);
-
-            const auto binary =
-                GetVal<std::string>(entry, vars::kExecutionBinary, "");
-            const auto command_line =
-                GetVal<std::string>(entry, vars::kExecutionCmdLine, "");
-            auto mode = GetVal<std::string>(entry, vars::kExecutionRun, "");
-            exts.emplace_back(Extension{.name = name,
-                                        .binary = binary,
-                                        .command_line = command_line,
-                                        .mode = ToMode(mode)});
-        }
-        return exts;
+    const auto group = GetNode(node, groups::kExtensions);
+    if (group.IsMap() && GetVal(group, vars::kEnabled, false)) {
+        return GatherExtensions(group);
     }
     return {};
 }

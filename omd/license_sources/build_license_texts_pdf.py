80fc5816184bcdc35f388ea3e8de75122414dcac
Kenneth Okoh <kenneth.okoh@checkmk.com>
2023-08-24 13:23:56 +0200

    Update the list of licenses
    ... as well as the scripts and the Checkmk logo (rendered in PDF)
    
    Change-Id: I59adef5ca936b604a8988acf324ca2a37b42f549

--- omd/license_sources/build_license_texts_pdf.py
+++ omd/license_sources/build_license_texts_pdf.py
@@ -5,53 +5,56 @@
 """Python module for generating a PDF containing all license texts that can be
 found under ./license_texts/"""
 
+import csv
 import sys
 import traceback
-import csv
-
 from pathlib import Path
+
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.styles import ParagraphStyle as PS
-from reportlab.lib.units import inch, cm
+from reportlab.lib.units import cm, inch
 from reportlab.pdfbase.pdfmetrics import registerFont
 from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.platypus import Image, Paragraph, SimpleDocTemplate, Spacer, PageBreak
+from reportlab.platypus import Image, PageBreak, Paragraph, SimpleDocTemplate, Spacer
 from reportlab.platypus.doctemplate import PageTemplate
 from reportlab.platypus.frames import Frame
 from reportlab.platypus.tableofcontents import TableOfContents
+from svglib.svglib import svg2rlg
 
 
 class MyDocTemplate(SimpleDocTemplate):
     """Custom DocTemplate configured for handling of table of contents"""
+
     def __init__(self, filename, **kw) -> None:
         self.allowSplitting = 0
         SimpleDocTemplate.__init__(self, filename, **kw)
-        template = PageTemplate('normal', [Frame(2.5 * cm, 2.5 * cm, 15 * cm, 25 * cm, id='F1')])
+        template = PageTemplate("normal", [Frame(2.5 * cm, 2.5 * cm, 15 * cm, 25 * cm, id="F1")])
         self.addPageTemplates(template)
 
     def afterFlowable(self, flowable):
         # Registers TOC entries
-        if flowable.__class__.__name__ == 'Paragraph':
+        if flowable.__class__.__name__ == "Paragraph":
             text = flowable.getPlainText()
             style = flowable.style.name
-            if style == 'Heading1':
+            if style == "Heading1":
                 level = 0
-            elif style == 'Heading2':
+            elif style == "Heading2":
                 level = 1
             else:
                 return
 
             E = [level, text, self.page]
             # If we have a bookmark name append that to our notify data
-            bn = getattr(flowable, '_bookmarkName', None)
+            bn = getattr(flowable, "_bookmarkName", None)
             if bn is not None:
                 E.append(bn)
-            self.notify('TOCEntry', tuple(E))
+            self.notify("TOCEntry", tuple(E))
 
 
 # This function makes our headings
 def heading(text, sty):
     from hashlib import sha1
+
     # Create bookmarkname
     bn = sha1((text + sty.name).encode("utf-8")).hexdigest()
     # Modify paragraph text to include an anchor point with name bn
@@ -87,36 +90,45 @@ def main():
 
     path_license_texts = path_omd / "license_sources/license_texts/"
     path_pdf = path_omd / "License_texts.pdf"
-    path_logo = path_omd / "license_sources/check_mk_logo_large1.png"
+    path_logo = path_omd / "license_sources/checkmk_logo.svg"
     path_licenses_csv = path_omd / "Licenses.csv"
 
     used_licenses = used_licenses_from_csv(path_licenses_csv)
 
-    registerFont(TTFont('Calibri', 'Calibri.ttf'))
-    doc = SimpleDocTemplate(str(path_pdf),
-                            pagesize=letter,
-                            bottomMargin=.4 * inch,
-                            topMargin=.6 * inch,
-                            rightMargin=.8 * inch,
-                            leftMargin=.8 * inch)
+    registerFont(TTFont("Calibri", "Calibri.ttf"))
+    doc = SimpleDocTemplate(
+        str(path_pdf),
+        pagesize=letter,
+        bottomMargin=0.4 * inch,
+        topMargin=0.6 * inch,
+        rightMargin=0.8 * inch,
+        leftMargin=0.8 * inch,
+    )
     toc = TableOfContents()
     toc.levelStyles = [
-        PS(fontName='Calibri',
-           fontSize=14,
-           name='TOCHeading1',
-           leftIndent=20,
-           firstLineIndent=-20,
-           spaceBefore=5,
-           leading=16),
-        PS(fontSize=12, name='TOCHeading2', leftIndent=40, firstLineIndent=-20, leading=12),
+        PS(
+            fontName="Calibri",
+            fontSize=14,
+            name="TOCHeading1",
+            leftIndent=20,
+            firstLineIndent=-20,
+            spaceBefore=5,
+            leading=16,
+        ),
+        PS(fontSize=12, name="TOCHeading2", leftIndent=40, firstLineIndent=-20, leading=12),
     ]
-    cover = PS(name='Cover', fontSize=16, leading=22, alignment=1)
-    title = PS(name='Title', fontSize=24, leading=16)
-    h1 = PS(name='Heading1', fontSize=16, leading=16)
-    normal = PS(name='Normal', fontSize=8)
+    cover = PS(name="Cover", fontSize=16, leading=22, alignment=1)
+    title = PS(name="Title", fontSize=24, leading=16)
+    h1 = PS(name="Heading1", fontSize=16, leading=16)
+    normal = PS(name="Normal", fontSize=8)
     spacer = Spacer(width=0, height=2 * cm)
 
-    cover_logo = Image(str(path_logo), width=3.5 * inch, height=1 * inch)
+    drawing = svg2rlg(str(path_logo))
+    sx = sy = 2
+    drawing.width, drawing.height = drawing.minWidth() * sx, drawing.height * sy
+    drawing.scale(sx, sy)
+    cover_logo = Image(drawing)
+    cover_logo.hAllign = "CENTER"
     cover_text = "Open Source licenses included in:<br /><br />\n\
             Checkmk Enterprise Edition<br />\n\
             Checkmk Managed Services Edition"
@@ -127,7 +139,7 @@ def main():
     story.append(Spacer(width=0, height=1 * cm))
     story.append(Paragraph(cover_text, cover))
     story.append(PageBreak())
-    story.append(Paragraph('<b>Content</b>', title))
+    story.append(Paragraph("<b>Content</b>", title))
     story.append(spacer)
     story.append(toc)
 
@@ -144,8 +156,7 @@ def main():
             story.append(heading(headline, h1))
             story.append(Paragraph(text_content, normal))
         else:
-            print("No license text file found for ID \"%s\" and path %s" %
-                  (used_license, file_path))
+            print('No license text file found for ID "%s" and path %s' % (used_license, file_path))
 
     doc = MyDocTemplate(str(path_pdf))
     doc.multiBuild(story, onLaterPages=add_page_number)

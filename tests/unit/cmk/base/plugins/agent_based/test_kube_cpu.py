37847dbbe1d1635ef22e27f50cf5c3458ad85805
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-08-31 12:19:38 +0200

    kube_cpu: remove unnecessary tests
    Compared test coverage of
    `cmk/base/plugins/agent_based/utils/kube_resources.py`
    before and after removal of tests.
    
    CMK-9643
    
    Change-Id: Ic894b0e6217a0a2bec6198744db3765de21abd29
    Co-Authored-By: Benedikt Seidl <benedikt.seidl@checkmk.com>

--- tests/unit/cmk/base/plugins/agent_based/test_kube_cpu.py
+++ tests/unit/cmk/base/plugins/agent_based/test_kube_cpu.py
@@ -4,14 +4,13 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import itertools
-from collections.abc import Sequence
 
 import pytest
 from pydantic_factories import ModelFactory
 
 import cmk.base.plugins.agent_based.utils.kube
 from cmk.base.plugins.agent_based import kube_cpu
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, render, Result, State
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, State
 from cmk.base.plugins.agent_based.utils import kube_resources
 from cmk.base.plugins.agent_based.utils.kube import Cpu, PerformanceUsage
 
@@ -60,31 +59,6 @@ def test_discovery() -> None:
         assert len(list(kube_cpu.discovery_kube_cpu(s1, s2, s3))) == 1
 
 
-def test_check_results_without_usage():
-    expected_beginnings = ["Requests: 0.180", "Limits: 0.360"]
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, None, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    results = [r for r in check_result if isinstance(r, Result)]
-    assert all(
-        r.summary.startswith(beginning) for r, beginning in zip(results, expected_beginnings)
-    )
-    assert all(r.state == State.OK for r in results)
-
-
-def test_check_metrics_without_usage() -> None:
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, None, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    expected_metrics = {
-        Metric("kube_cpu_allocatable", ALLOCATABLE, boundaries=(0.0, None)),
-        Metric("kube_cpu_request", 0.18, boundaries=(0.0, None)),
-        Metric("kube_cpu_limit", 0.36, boundaries=(0.0, None)),
-    }
-    metrics = {r for r in check_result if isinstance(r, Metric)}
-    assert expected_metrics == metrics
-
-
 def test_check_if_no_resources() -> None:
     """Crashing is expected, because section_kube_cpu is only missing, if data from the api
     server missing."""
@@ -95,182 +69,36 @@ def test_check_if_no_resources() -> None:
         list(check_result)
 
 
-def test_check_beginning_of_summaries_with_all_sections_present() -> None:
-    limit = RESOURCES_SECTION.limit
-    request = RESOURCES_SECTION.request
-    expected_beginnings = [
-        f"Usage: {USAGE:0.3f}",
-        f"Requests utilization: {render.percent(USAGE /  request * 100)} - {USAGE:0.3f} of {request:0.3f}",
-        f"Limits utilization: {render.percent(USAGE / limit * 100)} - {USAGE:0.3f} of {limit:0.3f}",
-    ]
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, USAGE_SECTION, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
+def test_performance_cpu() -> None:
+    check_result = list(
+        kube_cpu._check_kube_cpu(
+            PARAMS, USAGE_SECTION, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
+        )
     )
-    results = [r for r in check_result if isinstance(r, Result)]
-    assert all(
-        r.summary.startswith(beginning) for r, beginning in zip(results, expected_beginnings)
-    )
-
-
-def test_check_yields_multiple_metrics_with_values() -> None:
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, USAGE_SECTION, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    expected = [
-        ("kube_cpu_usage", USAGE),
-        ("kube_cpu_request", RESOURCES_SECTION.request),
-        ("kube_cpu_request_utilization", USAGE / RESOURCES_SECTION.request * 100),
-        ("kube_cpu_limit", RESOURCES_SECTION.limit),
-        ("kube_cpu_limit_utilization", USAGE / RESOURCES_SECTION.limit * 100),
-        ("kube_cpu_allocatable", ALLOCATABLE),
-        ("kube_cpu_node_allocatable_utilization", pytest.approx(USAGE / ALLOCATABLE * 100)),
-    ]
-    assert [(m.name, m.value) for m in check_result if isinstance(m, Metric)] == expected
-
-
-def test_check_all_states_ok():
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, USAGE_SECTION, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    assert all(r.state == State.OK for r in check_result if isinstance(r, Result))
-
-
-@pytest.mark.parametrize(
-    "limit, request_",
-    [
-        (0.18, 0.18),
-        (0.12, 0.12),
-        (0.09, 0.09),
-    ],
-)
-def test_check_all_states_ok_params_ignore(limit: int, request_: int) -> None:
-    resources_section = ResourcesFactory.build(limit=limit, request=request_)
-    check_result = kube_cpu._check_kube_cpu(
-        kube_resources.DEFAULT_PARAMS,
-        USAGE_SECTION,
-        resources_section,
-        ALLOCATABLE_RESOURCE_SECTION,
-        1.0,
-        {},
-    )
-
-    assert all(r.state == State.OK for r in check_result if isinstance(r, Result))
-
-
-@pytest.mark.parametrize(
-    "limit, request_",
-    [
-        (0.18, 0.18),
-        (0.12, 0.12),
-        (0.09, 0.09),
-    ],
-)
-@pytest.mark.parametrize(
-    "params, expected_states",
-    [
-        (
-            kube_resources.Params(
-                usage=("levels", (0.01, 1.0)),
-                request="no_levels",
-                limit="no_levels",
-                cluster="no_levels",
-                node="no_levels",
-            ),
-            [State.WARN, State.OK, State.OK, State.OK],
+    assert check_result == [
+        Result(state=State.OK, summary="Usage: 0.090"),
+        Metric("kube_cpu_usage", 0.09, boundaries=(0.0, None)),
+        Metric("kube_cpu_request", 0.18),
+        Result(
+            state=State.OK,
+            summary="Requests utilization: 50.00% - 0.090 of 0.180 (2/2 containers with requests)",
         ),
-        (
-            kube_resources.Params(
-                usage=("levels", (0.01, 0.01)),
-                request="no_levels",
-                limit="no_levels",
-                cluster="no_levels",
-                node="no_levels",
-            ),
-            [State.CRIT, State.OK, State.OK, State.OK],
+        Metric("kube_cpu_request_utilization", 50.0, levels=(60.0, 90.0), boundaries=(0.0, None)),
+        Metric("kube_cpu_limit", 0.36),
+        Result(
+            state=State.OK,
+            summary="Limits utilization: 25.00% - 0.090 of 0.360 (2/2 containers with limits)",
         ),
-    ],
-)
-def test_check_abs_levels_with_mixed(
-    params: kube_resources.Params,
-    request_: int,
-    limit: int,
-    expected_states: Sequence[State],
-) -> None:
-    resources_section = ResourcesFactory.build(limit=limit, request=request_)
-    check_result = kube_cpu._check_kube_cpu(
-        params, USAGE_SECTION, resources_section, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    assert [r.state for r in check_result if isinstance(r, Result)] == expected_states
-
-
-@pytest.mark.parametrize(
-    "request_, limit, expected_states",
-    [
-        (0.18, 0.36, [State.OK, State.OK, State.OK, State.OK]),
-        (0.18, 0.24, [State.OK, State.OK, State.WARN, State.OK]),
-        (0.18, 0.18, [State.OK, State.OK, State.CRIT, State.OK]),
-        (0.12, 0.36, [State.OK, State.WARN, State.OK, State.OK]),
-        (0.09, 0.36, [State.OK, State.CRIT, State.OK, State.OK]),
-        (0.12, 0.24, [State.OK, State.WARN, State.WARN, State.OK]),
-        (0.12, 0.18, [State.OK, State.WARN, State.CRIT, State.OK]),
-        (0.09, 0.24, [State.OK, State.CRIT, State.WARN, State.OK]),
-        (0.09, 0.18, [State.OK, State.CRIT, State.CRIT, State.OK]),
-    ],
-)
-def test_check_result_states_mixed(
-    request_: int,
-    limit: int,
-    expected_states: Sequence[State],
-) -> None:
-    resources_section = ResourcesFactory.build(limit=limit, request=request_)
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, USAGE_SECTION, resources_section, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    assert [r.state for r in check_result if isinstance(r, Result)] == expected_states
-
-
-def test_overview_requests_contained_no_usage_section() -> None:
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, None, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    overview_requests_ignored = kube_resources.count_overview(RESOURCES_SECTION, "request")
-    results = [r for r in check_result if isinstance(r, Result)]
-    requests_results = [r for r in results if "Request" in r.summary]
-    assert len(requests_results) == 1
-    assert [r for r in results if overview_requests_ignored in r.summary] == requests_results
-
-
-def test_overview_requests_contained() -> None:
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, USAGE_SECTION, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    overview_requests_ignored = kube_resources.count_overview(RESOURCES_SECTION, "request")
-    results = [r for r in check_result if isinstance(r, Result)]
-    requests_results = [r for r in results if "Request" in r.summary]
-    assert len(requests_results) == 1
-    assert [r for r in results if overview_requests_ignored in r.summary] == requests_results
-
-
-def test_overview_limits_contained_no_usage() -> None:
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, None, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    overview_limits_ignored = kube_resources.count_overview(RESOURCES_SECTION, "limit")
-    results = [r for r in check_result if isinstance(r, Result)]
-    limits_results = [r for r in results if "Limit" in r.summary]
-    assert len(limits_results) == 1
-    assert [r for r in results if overview_limits_ignored in r.summary] == limits_results
-
-
-def test_overview_limits_contained() -> None:
-    check_result = kube_cpu._check_kube_cpu(
-        PARAMS, None, RESOURCES_SECTION, ALLOCATABLE_RESOURCE_SECTION, 1.0, {}
-    )
-    overview_limits_ignored = kube_resources.count_overview(RESOURCES_SECTION, "limit")
-    results = [r for r in check_result if isinstance(r, Result)]
-    limits_results = [r for r in results if "Limit" in r.summary]
-    assert len(limits_results) == 1
-    assert [r for r in results if overview_limits_ignored in r.summary] == limits_results
+        Metric("kube_cpu_limit_utilization", 25.0, levels=(30.0, 45.0), boundaries=(0.0, None)),
+        Metric("kube_cpu_allocatable", 5.0),
+        Result(state=State.OK, summary="Node utilization: 1.80% - 0.090 of 5.000"),
+        Metric(
+            "kube_cpu_node_allocatable_utilization",
+            1.8,
+            levels=(15.0, 22.5),
+            boundaries=(0.0, None),
+        ),
+    ]
 
 
 def test_stored_usage_value() -> None:

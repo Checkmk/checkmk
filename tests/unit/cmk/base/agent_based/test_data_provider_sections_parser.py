2e50dc4d5ba02f00fbed191c32e223dd5c822b5f
Mathias Laurin <mathias.laurin@checkmk.com>
2023-09-04 16:00:31 +0200

    sectionparser tests: fix layering + remove noop code
    Change-Id: I4868a47598c178d4271689bd0f54d196a062e5d5

--- tests/unit/cmk/base/agent_based/test_data_provider_sections_parser.py
+++ tests/unit/cmk/base/agent_based/test_data_provider_sections_parser.py
@@ -3,9 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Callable, Sequence
-from typing import Any
-
 import pytest
 
 from cmk.utils.hostaddress import HostName
@@ -13,16 +10,6 @@ from cmk.utils.sectionname import SectionMap, SectionName
 
 from cmk.checkengine.parser import AgentRawDataSectionElem, HostSections
 from cmk.checkengine.sectionparser import SectionsParser
-
-from cmk.base.api.agent_based.register.section_plugins import trivial_section_factory
-
-_ParseFunction = Callable[[Sequence[AgentRawDataSectionElem]], Any]
-
-
-def _section(name: str, parse_function: Callable) -> tuple[SectionName, _ParseFunction]:
-    """create a simple section for testing"""
-    section = trivial_section_factory(SectionName(name))
-    return section.name, parse_function
 
 
 class TestSectionsParser:
@@ -44,7 +31,10 @@ class TestSectionsParser:
         sections_parser: SectionsParser[AgentRawDataSectionElem],
     ) -> None:
         counter = iter((1,))
-        section_name, parse_function = _section("one", lambda x: next(counter))
+        section_name = SectionName("one")
+
+        def parse_function(*args: object, **kw: object) -> object:
+            return next(counter)
 
         _ = sections_parser.parse(section_name, parse_function)
         parsing_result = sections_parser.parse(section_name, parse_function)
@@ -57,17 +47,18 @@ class TestSectionsParser:
     def test_parsing_errors(
         sections_parser: SectionsParser[AgentRawDataSectionElem],
     ) -> None:
-        section_name, parse_function = _section("one", lambda x: 1 / 0)
+        section_name = SectionName("one")
 
-        assert sections_parser.parse(section_name, parse_function) is None
+        assert sections_parser.parse(section_name, lambda *args, **kw: 1 / 0) is None
         assert len(sections_parser.parsing_errors) == 1
         assert sections_parser.parsing_errors == ["error"]
 
     @staticmethod
     def test_parse(sections_parser: SectionsParser[AgentRawDataSectionElem]) -> None:
         parsed_data = object()
-        section_name, parse_function = _section("one", lambda x: parsed_data)
-        parsing_result = sections_parser.parse(section_name, parse_function)
+        section_name = SectionName("one")
+
+        parsing_result = sections_parser.parse(section_name, lambda *args, **kw: parsed_data)
 
         assert parsing_result is not None
         assert parsing_result.data is parsed_data
@@ -75,25 +66,24 @@ class TestSectionsParser:
 
     @staticmethod
     def test_disable(sections_parser: SectionsParser[AgentRawDataSectionElem]) -> None:
-        section_name, parse_function = _section("one", lambda x: 42)
+        section_name = SectionName("one")
+
         sections_parser.disable([section_name])
 
-        assert sections_parser.parse(section_name, parse_function) is None
+        assert sections_parser.parse(section_name, lambda *args, **kw: 42) is None
 
     @staticmethod
     def test_parse_missing_section(
         sections_parser: SectionsParser[AgentRawDataSectionElem],
     ) -> None:
-        section_name, parse_function = _section(
-            "missing_section", lambda x: 42
-        )  # function does not matter
+        section_name = SectionName("missing_section")
 
-        assert sections_parser.parse(section_name, parse_function) is None
+        assert sections_parser.parse(section_name, lambda *args, **kw: 42) is None
 
     @staticmethod
     def test_parse_section_returns_none(
         sections_parser: SectionsParser[AgentRawDataSectionElem],
     ) -> None:
-        section_name, parse_function = _section("one", lambda x: None)
+        section_name = SectionName("one")
 
-        assert sections_parser.parse(section_name, parse_function) is None
+        assert sections_parser.parse(section_name, lambda *args, **kw: None) is None

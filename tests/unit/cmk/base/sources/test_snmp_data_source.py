7b27c84b520137a3eca053ddfac3cec7918a9173
Mathias Laurin <mathias.laurin@checkmk.com>
2023-09-04 14:11:42 +0200

    Remove unused fixture (dead code)
    Change-Id: I65ee434397bffb3efb5ea379f4d89af5a2692851

--- tests/unit/cmk/base/sources/test_snmp_data_source.py
+++ tests/unit/cmk/base/sources/test_snmp_data_source.py
@@ -3,7 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import pytest
 from pytest import MonkeyPatch
 
 from tests.testlib.base import Scenario
@@ -11,12 +10,8 @@ from tests.testlib.base import Scenario
 from cmk.utils.exceptions import MKIPAddressLookupError
 from cmk.utils.hostaddress import HostName
 
-from cmk.checkengine.checking import CheckPluginName
-from cmk.checkengine.sectionparser import ParsedSectionName
-
 import cmk.base.config as config
 import cmk.base.ip_lookup as ip_lookup
-from cmk.base.api.agent_based.checking_classes import CheckPlugin
 
 
 def test_snmp_ipaddress_from_mgmt_board_unresolvable(
@@ -39,21 +34,3 @@ def test_snmp_ipaddress_from_mgmt_board_unresolvable(
     )
 
     assert config.lookup_mgmt_board_ip_address(config_cache, hostname) is None
-
-
-@pytest.fixture(name="check_plugin")
-def fixture_check_plugin():
-    return CheckPlugin(
-        CheckPluginName("unit_test_check_plugin"),
-        [ParsedSectionName("norris")],
-        "Unit Test",
-        None,  # type: ignore[arg-type]  # irrelevant for test
-        None,
-        None,
-        None,  # type: ignore[arg-type]  # irrelevant for test
-        None,  # type: ignore[arg-type]  # irrelevant for test
-        None,
-        None,
-        None,
-        None,
-    )

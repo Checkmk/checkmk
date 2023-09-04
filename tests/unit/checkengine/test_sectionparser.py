e649101f516261c8753c3d5033e0878900cc56a5
Mathias Laurin <mathias.laurin@checkmk.com>
2023-09-04 16:02:21 +0200

    Move checkengine.sectionparser tests ... #2
    Change-Id: If18cb1a4f987295dc5d94fc3541c488220e68ea3

--- tests/unit/checkengine/test_sectionparser.py
+++ tests/unit/checkengine/test_sectionparser.py
@@ -5,6 +5,8 @@
 
 from collections.abc import Callable, Iterable, Sequence
 
+import pytest
+
 from cmk.utils.hostaddress import HostName
 from cmk.utils.sectionname import SectionMap, SectionName
 
@@ -210,3 +212,80 @@ def test_parse_sections_superseded() -> None:
         ).resolve(ParsedSectionName("parsed"))
         is None
     )
+
+
+class TestSectionsParser:
+    @pytest.fixture
+    def sections_parser(self) -> SectionsParser[AgentRawDataSectionElem]:
+        return SectionsParser[AgentRawDataSectionElem](
+            host_sections=HostSections[SectionMap[AgentRawDataSectionElem]](
+                sections={
+                    SectionName("one"): [],
+                    SectionName("two"): [],
+                }
+            ),
+            host_name=HostName("only-neede-for-crash-reporting"),
+            error_handling=lambda *args, **kw: "error",
+        )
+
+    @staticmethod
+    def test_parse_function_called_once(
+        sections_parser: SectionsParser[AgentRawDataSectionElem],
+    ) -> None:
+        counter = iter((1,))
+        section_name = SectionName("one")
+
+        def parse_function(*args: object, **kw: object) -> object:
+            return next(counter)
+
+        _ = sections_parser.parse(section_name, parse_function)
+        parsing_result = sections_parser.parse(section_name, parse_function)
+
+        assert parsing_result is not None
+        assert parsing_result.data == 1
+
+    @staticmethod
+    @pytest.mark.usefixtures("disable_debug")
+    def test_parsing_errors(
+        sections_parser: SectionsParser[AgentRawDataSectionElem],
+    ) -> None:
+        section_name = SectionName("one")
+
+        assert sections_parser.parse(section_name, lambda *args, **kw: 1 / 0) is None
+        assert len(sections_parser.parsing_errors) == 1
+        assert sections_parser.parsing_errors == ["error"]
+
+    @staticmethod
+    def test_parse(sections_parser: SectionsParser[AgentRawDataSectionElem]) -> None:
+        parsed_data = object()
+        section_name = SectionName("one")
+
+        parsing_result = sections_parser.parse(section_name, lambda *args, **kw: parsed_data)
+
+        assert parsing_result is not None
+        assert parsing_result.data is parsed_data
+        assert parsing_result.cache_info is None
+
+    @staticmethod
+    def test_disable(sections_parser: SectionsParser[AgentRawDataSectionElem]) -> None:
+        section_name = SectionName("one")
+
+        sections_parser.disable([section_name])
+
+        assert sections_parser.parse(section_name, lambda *args, **kw: 42) is None
+
+    @staticmethod
+    def test_parse_missing_section(
+        sections_parser: SectionsParser[AgentRawDataSectionElem],
+    ) -> None:
+        section_name = SectionName("missing_section")
+
+        assert sections_parser.parse(section_name, lambda *args, **kw: 42) is None
+
+    @staticmethod
+    def test_parse_section_returns_none(
+        sections_parser: SectionsParser[AgentRawDataSectionElem],
+    ) -> None:
+        section_name = SectionName("one")
+
+        assert sections_parser.parse(section_name, lambda *args, **kw: None) is None

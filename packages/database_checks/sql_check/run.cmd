9c0b2c58b5e1524af4c0dbfe054fb82ec2097bbc
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-09-07 14:57:02 +0200

    add MSSQL check prototype
    - uses "agentbuild3.lan.tribe29.com" as an SQL endpoint
    - credentials are wrong, use BitWarden tresor to get correct one
    - scripts corrected to be universal
    
    TESTS: manual in Windows and Linux
    - build
    - connect with credentials from the BitWarden
    
    Change-Id: I4e929d48bd5c265563d8815ea2019e9cb9207a2c

---
+++ packages/database_checks/sql_check/run.cmd
@@ -0,0 +1,85 @@
+@echo off
+::
+:: Script to build/test/sign/deploy sql_check plugin
+::
+:: Prefix worker, standart for NON-COMPOSITE run.cmd
+::
+:: Similar in functionality with run scripts from packages
+::
+
+SETLOCAL EnableDelayedExpansion
+set worker_name=sql_check
+
+if "%*" == "" (
+echo: Run default...
+set ctl_arg_all=1
+) else (
+:CheckOpts
+if "%~1"=="-h" goto Usage
+if "%~1"=="-help" goto Usage
+if "%~1"=="-?" goto Usage
+
+if "%~1"=="-A"              (set worker_arg_all=1)          & shift & goto CheckOpts
+if "%~1"=="--all"           (set worker_arg_all=1)          & shift & goto CheckOpts
+
+if "%~1"=="-c"              (set worker_arg_clean=1)        & shift & goto CheckOpts
+if "%~1"=="--clean"         (set worker_arg_clean=1)        & shift & goto CheckOpts
+
+if "%~1"=="-f"              (set worker_arg_format=1)       & shift & goto CheckOpts
+if "%~1"=="--format"        (set worker_arg_format=1)       & shift & goto CheckOpts
+
+if "%~1"=="-F"              (set worker_arg_check_format=1) & shift & goto CheckOpts
+if "%~1"=="--check-format"  (set worker_arg_check_format=1) & shift & goto CheckOpts
+
+if "%~1"=="-C"              (set worker_arg_clippy=1)       & shift & goto CheckOpts
+if "%~1"=="--clippy"        (set worker_arg_clippy=1)       & shift & goto CheckOpts
+
+if "%~1"=="-B"              (set worker_arg_build=1)        & shift & goto CheckOpts
+if "%~1"=="--build"         (set worker_arg_build=1)        & shift & goto CheckOpts
+
+if "%~1"=="-T"              (set worker_arg_test=1)         & shift & goto CheckOpts
+if "%~1"=="--test"          (set worker_arg_test=1)         & shift & goto CheckOpts
+
+if "%~1"=="-D"              (set worker_arg_doc=1)          & shift & goto CheckOpts
+if "%~1"=="--documentation" (set worker_arg_doc=1)          & shift & goto CheckOpts
+
+if "%~1"=="--sign"          (set worker_arg_sign_file=%1) & (set worker_arg_sign_secret=%2)   & (set worker_arg_sign=1) & shift & shift & shift goto CheckOpts
+)
+if "%worker_arg_all%"=="1" (set worker_arg_clippy=1) & (set worker_arg_build=1) & (set worker_arg_test=1) & (set worker_arg_check_format=1)
+
+set worker_cur_dir=%cd%
+set worker_root_dir=%worker_cur_dir%\..\..\..
+set worker_arte=%worker_root_dir%\artefacts
+
+set ci_root_dir=workdir\workspace
+set ci_junction_to_root_dir=x
+set script_to_run=.\scripts\cargo_build_core.cmd
+powershell -ExecutionPolicy ByPass -File scripts/shorten_dir_and_call.ps1 %ci_root_dir% %ci_junction_to_root_dir% %script_to_run%
+GOTO :EOF
+
+
+:Usage
+echo.
+echo.Usage:
+echo.
+echo.%~nx0 [arguments]
+echo.
+echo.Available arguments:
+echo.  -?, -h, --help       display help and exit
+echo.  -A, --all            shortcut to -F -C -B -T:  check format, clippy, build and test
+echo.  -c, --clean          clean artifacts
+echo.  -C, --clippy         run clippy for %worker_name%
+echo.  -D, --documentation  create documentation
+echo.  -f, --format         format sources
+echo.  -F, --check-format   check for correct formatting
+echo.  -B, --build          build %worker_name%
+echo.  -T, --test           test %worker_name%
+echo.  --sign file secret   sign %worker_name% with file and secret
+echo.
+echo.Examples:
+echo.
+echo %~nx0 --clippy
+echo %~nx0 --build --test
+echo %~nx0 --build --test -S mypasswd -C
+echo %~nx0 -A
+GOTO :EOF

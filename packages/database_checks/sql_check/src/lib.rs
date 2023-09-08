bf18285c2939620134a6270f541d5f79c87463ac
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-09-08 10:48:34 +0200

    move check_connect into lib::ms_sql::api
    Change-Id: Iabfb29b3e42ce67042e31f5b4924845775ce8dda

---
+++ packages/database_checks/sql_check/src/lib.rs
@@ -0,0 +1,1 @@
+pub mod ms_sql;
--- packages/database_checks/sql_check/src/ms_sql.rs
+++ packages/database_checks/sql_check/src/ms_sql.rs
@@ -2,10 +2,4 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    sql_check::ms_sql::api::check_connect("agentbuild3.lan.tribe29.com", 1433, "u", "u").await?;
-    println!("Success");
-
-    Ok(())
-}
+pub mod api;

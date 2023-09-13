f5d2fd6724aec4112f24947a9075f8a1d61052dc
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2023-09-13 16:49:13 +0200

    Alter default params if we're timer triggered
    ... as this is the production mode
    
    Change-Id: Ifce6f985149561e8b1cfef58b55be8bc58edd0c1

--- buildscripts/scripts/send-werks-to-mailing-lists.groovy
+++ buildscripts/scripts/send-werks-to-mailing-lists.groovy
@@ -2,8 +2,8 @@
 
 /// file: send-werks-to-mailing-lists.groovy
 
-def validate_parameters() {
-    if (params.SEND_WERK_MAILS && !params.ADD_WERK_GIT_NOTES) {
+def validate_parameters(send_werk_mails, add_werk_git_notes) {
+    if (send_werk_mails && !add_werk_git_notes) {
         error "Sending the werk mails but not adding the git notes is dangerous: " +
             "We may re-send already published werks again."
     }
@@ -32,6 +32,10 @@ def build_cmd_options_from_params(send_werk_mails, add_werk_git_notes, assume_no
     return cmd_line
 }
 
+def was_timer_triggered() {
+    return currentBuild.rawBuild.getCauses()[0].toString().contains('TimerTriggerCause')
+}
+
 def main() {
     check_job_parameters([
         "SEND_WERK_MAILS_OF_BRANCHES",
@@ -41,7 +45,6 @@ def main() {
         "WERKS_MAIL_ADDRESS",
     ]);
 
-    validate_parameters();
     def docker_args = "${mount_reference_repo_dir} " +
         "-h lists.checkmk.com " +
         "-v /etc/nullmailer:/etc/nullmailer:ro " +
@@ -51,6 +54,16 @@ def main() {
     def add_werk_git_notes = params.ADD_WERK_GIT_NOTES;
     def assume_no_mails_sent_except = params.ASSUME_NO_MAILS_SENT_EXCEPT;
     def werks_mail_address = params.WERKS_MAIL_ADDRESS;
+
+    if (was_timer_triggered()) {
+        println("Current job was triggered by Timer, so we need to use the production parameters.");
+        send_werk_mails_of_branches = ["master", "2.2.0", "2.1.0", "2.0.0"];
+        send_werk_mails = true;
+        add_werk_git_notes = true;
+        assume_no_mails_sent_except = "";
+        werks_mail_address = "";
+    }
+    validate_parameters(send_werk_mails, add_werk_git_notes);
     def cmd_line = build_cmd_options_from_params(send_werk_mails, add_werk_git_notes, assume_no_mails_sent_except, werks_mail_address);
 
     print(

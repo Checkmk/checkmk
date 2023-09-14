e46036d7b4c7e5b9ea215b00b2f28d5a6e6b74d2
Simon Jess <simon.jess@checkmk.com>
2023-09-14 10:34:21 +0200

    Cleanup tests for graphing._color
    Change-Id: I76f2a3700e7ace39d0abba0b27e1da7fce738c86

--- tests/unit/cmk/gui/graphing/test_utils.py
+++ tests/unit/cmk/gui/graphing/test_utils.py
@@ -15,7 +15,6 @@ from cmk.utils.metrics import MetricName
 import cmk.gui.graphing._utils as utils
 import cmk.gui.metrics as metrics
 from cmk.gui.config import active_config
-from cmk.gui.graphing._color import _COLOR_WHEEL_SIZE, _hex_color_to_rgb_color, indexed_color
 from cmk.gui.graphing._graph_specification import HorizontalRule
 from cmk.gui.graphing._unit_info import unit_info
 from cmk.gui.graphing._utils import (
@@ -1187,61 +1186,6 @@ def test_horizontal_rules_from_thresholds(
         )
         == result
     )
-
-
-@pytest.mark.parametrize(
-    "hex_color, expected_rgb",
-    [
-        ("#112233", (17, 34, 51)),
-        ("#123", (17, 34, 51)),
-    ],
-)
-def test__hex_color_to_rgb_color(hex_color: str, expected_rgb: tuple[int, int, int]) -> None:
-    assert _hex_color_to_rgb_color(hex_color) == expected_rgb
-
-
-@pytest.mark.parametrize(
-    ["idx", "total"],
-    [
-        (-1, -1),
-        (-1, 0),
-        (0, 0),
-        (1, 0),
-    ],
-)
-def test_indexed_color_raises(idx: int, total: int) -> None:
-    with pytest.raises(MKGeneralException):
-        indexed_color(idx, total)
-
-
-@pytest.mark.parametrize(
-    "idx",
-    range(0, _COLOR_WHEEL_SIZE),
-)
-def test_indexed_color_uses_color_wheel_first(idx: int) -> None:
-    assert "/" in indexed_color(idx, _COLOR_WHEEL_SIZE)
-
-
-@pytest.mark.parametrize(
-    ["idx", "total"],
-    [
-        (89, 143),
-        (55, 55),
-        (355, 552),
-        (90, 100),
-        (67, 89),
-        (95, 452),
-        (111, 222),
-    ],
-)
-def test_indexed_color_sanity(idx: int, total: int) -> None:
-    color = indexed_color(idx, total)
-    assert "/" not in color
-    r, g, b = _hex_color_to_rgb_color(color)
-    if r == g == b:
-        assert all(100 <= component <= 200 for component in (r, g, b))
-    else:
-        assert all(60 <= component <= 230 for component in (r, g, b) if component)
 
 
 @pytest.mark.parametrize(

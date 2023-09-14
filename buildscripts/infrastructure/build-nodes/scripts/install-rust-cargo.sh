6c25ce725a8280df7037abc4c48ba8bf0f3287e1
Frans Fürst <frans.fuerst@checkmk.com>
2023-09-14 18:42:26 +0200

    Revert "switch to rust 1.72" Revert "invalidate build cache for rust" Revert "use unused build id for rust"
    This reverts commit 2126bbe61c6c0a4e29d91ecadb0caad6b0abb07c.
    This reverts commit 252d972a96707e84b3f08641240aa634c3e6d4a7.
    This reverts commit 34dc9cd4c8102e57d135bb5eda7603c57b1e141d.
    
    Change-Id: I68043eef2b34171ba934f42b54251da9efed5dfa

--- buildscripts/infrastructure/build-nodes/scripts/install-rust-cargo.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-rust-cargo.sh
@@ -17,7 +17,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # define toolchain version explicitly
 # 'stable' is allowed only for main(master) branch
 # https://github.com/rust-lang/rust/issues/112286 for the reason of pinning the version
-TOOLCHAIN_VERSION="1.72"
+TOOLCHAIN_VERSION="1.69"
 
 DEFAULT_TOOLCHAIN="${TOOLCHAIN_VERSION}-x86_64-unknown-linux-gnu"
 DIR_NAME="rust"
@@ -29,7 +29,7 @@ RUSTUP_HOME="$TARGET_DIR/$DIR_NAME/rustup"
 export RUSTUP_HOME
 
 # Increase this to enforce a recreation of the build cache
-BUILD_ID=11
+BUILD_ID=9
 
 build_package() {
     WORK_DIR=$(mktemp -d)

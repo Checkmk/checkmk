9ab567e8b32d22df16fb6ca61fcce6d1dc08da20
Hannes Rantzsch <hannes.rantzsch@checkmk.com>
2023-09-14 16:52:38 +0200

    add docstring for aes gcm symmetric crypto
    Change-Id: Ieebb7b7d54aebd8fce52da68f6b7f4d657a6b30d

--- cmk/utils/crypto/symmetric.py
+++ cmk/utils/crypto/symmetric.py
@@ -27,6 +27,28 @@ class TaggedCiphertext:
 
 
 def aes_gcm_encrypt(key: bytes, nonce: bytes, plaintext: str) -> TaggedCiphertext:
+    """Symmetrically encrypt a plaintext given a key and a nonce.
+
+    AES GCM is an "authenticated encryption with associated data (AEAD)" mode. This means that MAC
+    calculation, i.e. verification that the message has not been altered, is included in the
+    encryption scheme.
+
+    Args:
+        key: The key can be 16, 24, or 32 bytes long. Note that a password or human-readable random
+        characters are not suitable as key material. The key should be either generated randomly
+        (e.g. using `secrets.token_bytes()`), or derived from a password with a key derivation
+        function like argon2id.
+
+        nonce: The nonce (number used once) should be 16 bytes long. It MUST NEVER be re-used.
+        Obtain it by generating 16 random bytes and transfer it together with the cipher text. The
+        nonce is not secret.
+
+        plaintext: The plaintext to be encrypted.
+
+    Returns:
+        The ciphertext and the tag, which is used to authenticate the message. Both values need to
+        be transferred and provided to the decryption function in the same order.
+    """
     encrypted = AESGCM(key).encrypt(nonce, plaintext.encode("utf-8"), associated_data=None)
     return TaggedCiphertext(
         ciphertext=encrypted[: -TaggedCiphertext.TAG_LENGTH],
@@ -35,6 +57,11 @@ def aes_gcm_encrypt(key: bytes, nonce: bytes, plaintext: str) -> TaggedCiphertex
 
 
 def aes_gcm_decrypt(key: bytes, nonce: bytes, ciphertext: TaggedCiphertext) -> str:
+    """Decrypt a ciphertext given a key and a nonce.
+
+    This is the inverse method for `aes_gcm_encrypt`. See the docstring of `aes_gcm_encrypt` for
+    further information.
+    """
     return (
         AESGCM(key)
         .decrypt(nonce, ciphertext.ciphertext + ciphertext.tag, associated_data=None)

2e80af474b9749a6896292fb2db51f2496327246
Mathias Laurin <mathias.laurin@checkmk.com>
2023-09-18 15:49:50 +0200

    check_cert: Add CLI
    Add CLI options for URL, port, timeout, warn and crit limits, and
    disabling SNI.
    
    Note: CLI only, no implementation.
    
    CMK-14258
    
    Change-Id: Id41bb6c406ebd5d9d2ec0cc38d002f1c12acf853

--- packages/check-cert/src/main.rs
+++ packages/check-cert/src/main.rs
@@ -1,3 +1,39 @@
+// Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+// This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+// conditions defined in the file COPYING, which is part of this source code package.
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(about = "check_cert")]
+struct Args {
+    /// URL to check
+    #[arg(short, long)]
+    url: String,
+
+    /// Port
+    #[arg(short, long, default_value_t = 443)]
+    port: u16,
+
+    /// Set timeout in seconds
+    #[arg(long, default_value_t = 10)]
+    timeout: u64,
+
+    /// Warn if certificate expires in n days
+    #[arg(long, default_value_t = 30)]
+    warn: u32,
+
+    /// Crit if certificate expires in n days
+    #[arg(long, default_value_t = 0)]
+    crit: u32,
+
+    /// Disable SNI extension
+    #[arg(long, action = clap::ArgAction::SetTrue)]
+    disable_sni: bool,
+}
+
 fn main() {
+    let _args = Args::parse();
+
     println!("Hello, world!");
 }

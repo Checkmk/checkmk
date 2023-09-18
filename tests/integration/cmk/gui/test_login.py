f8091e83493b7d3a26beeadc781cbed65837b04d
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2023-09-18 17:48:24 +0200

    Fix cookie flag and add test
    Change-Id: I0606e51b80af71d5452dff01c41994464ac9202f

--- tests/integration/cmk/gui/test_login.py
+++ tests/integration/cmk/gui/test_login.py
@@ -21,3 +21,16 @@ def test_01_login_and_logout(site: Site) -> None:
     web.logout()
     r = web.get("wato.py?mode=globalvars", allow_redirect_to_login=True)
     assert "Global settings" not in r.text
+
+
+def test_session_cookie(site: Site) -> None:
+    web = CMKWebSession(site)
+
+    web.login()
+    for cookie in web.session.cookies:
+        if not cookie.name == f"auth_{site.id}":
+            continue
+        assert cookie.path == f"/{site.id}/"
+        # This is ugly but IMHO the only way...
+        assert "HttpOnly" in cookie.__dict__.get("_rest", {})
+        assert cookie.__dict__.get("_rest", {}).get("SameSite") == "Lax"

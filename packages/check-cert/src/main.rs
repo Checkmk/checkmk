5a24aa89d390f70238e1fff5736ebfb5d21c14fe
Mathias Laurin <mathias.laurin@checkmk.com>
2023-09-19 12:23:39 +0200

    Check certificate validity
    CMK-14258
    
    Change-Id: I9f2ec9766f5442aa1c3df018821a9dfca0326620

--- packages/check-cert/src/main.rs
+++ packages/check-cert/src/main.rs
@@ -4,6 +4,7 @@
 
 use anyhow::{Context, Result};
 use clap::Parser;
+use openssl::asn1::{Asn1Time, Asn1TimeRef};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use openssl::x509::X509;
 use std::net::TcpStream;
@@ -54,9 +55,97 @@ fn fetch_server_cert(server: &str, port: &u16) -> Result<X509> {
     Ok(cert)
 }
 
-fn main() {
-    let args = Args::parse();
-    let _cert = fetch_server_cert(&args.url, &args.port);
+#[derive(PartialEq, Eq)]
+enum Validity {
+    OK,
+    Warn,
+    Crit,
+}
 
-    println!("Hello, world!");
+fn check_validity(x: &Asn1TimeRef, warn: &Asn1Time, crit: &Asn1Time) -> Validity {
+    std::assert!(warn >= crit);
+
+    if crit >= x {
+        Validity::Crit
+    } else if warn >= x {
+        Validity::Warn
+    } else {
+        Validity::OK
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let warn_time = Asn1Time::days_from_now(args.warn).context("Invalid warn value")?;
+    let crit_time = Asn1Time::days_from_now(args.crit).context("Invalid crit value")?;
+    if warn_time < crit_time {
+        eprintln!("crit limit larger than warn limit");
+        std::process::exit(1);
+    }
+
+    let cert = fetch_server_cert(&args.url, &args.port)?;
+    match check_validity(cert.not_after(), &warn_time, &crit_time) {
+        Validity::OK => println!("OK!"),
+        Validity::Warn => println!("WARN!"),
+        Validity::Crit => println!("CRIT!"),
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod test_check_validity {
+    use crate::{check_validity, Validity};
+    use openssl::asn1::Asn1Time;
+
+    fn days_from_now(days: u32) -> Asn1Time {
+        Asn1Time::days_from_now(days).unwrap()
+    }
+
+    #[test]
+    fn test_check_validity_ok() {
+        assert!(
+            check_validity(
+                days_from_now(30).as_ref(),
+                &days_from_now(0),
+                &days_from_now(0),
+            ) == Validity::OK
+        );
+        assert!(
+            check_validity(
+                days_from_now(30).as_ref(),
+                &days_from_now(15),
+                &days_from_now(7),
+            ) == Validity::OK
+        );
+    }
+
+    #[test]
+    fn test_check_validity_warn() {
+        assert!(
+            check_validity(
+                days_from_now(10).as_ref(),
+                &days_from_now(15),
+                &days_from_now(7),
+            ) == Validity::Warn
+        );
+    }
+
+    #[test]
+    fn test_check_validity_crit() {
+        assert!(
+            check_validity(
+                days_from_now(3).as_ref(),
+                &days_from_now(15),
+                &days_from_now(7),
+            ) == Validity::Crit
+        );
+        assert!(
+            check_validity(
+                days_from_now(3).as_ref(),
+                &days_from_now(15),
+                &days_from_now(15),
+            ) == Validity::Crit
+        );
+    }
 }

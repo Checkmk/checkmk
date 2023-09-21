918e16ba939d3b839b0aaeeb80bf3e96eaa38459
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-09-21 16:46:56 +0200

    postgres: make old config format available again
    SUP-15600
    SUP-15731
    SUP-15740
    SUP-15692
    
    Change-Id: Ib770f0e1bc0162f89657e3aed3097fa1f097513b

--- agents/plugins/mk_postgres.py
+++ agents/plugins/mk_postgres.py
@@ -1194,17 +1194,19 @@ def parse_postgres_cfg(postgres_cfg, config_separator):
         if key == "DBUSER":
             dbuser = value.rstrip()
         if key == "INSTANCE":
-            env_file, pg_user, pg_passfile, instance_name = value.split(config_separator)
+            keys = value.split(config_separator)
+            if len(keys) == 3:
+                # Old format (deprecated in Werk 16016), but we don't force updates unless there is
+                # a substantial benefit.
+                env_file, pg_user, pg_passfile = keys
+                instance_name = ""
+            else:
+                env_file, pg_user, pg_passfile, instance_name = keys
             env_file = env_file.strip()
-            instance_name = instance_name or env_file.split(os.sep)[-1].split(".")[0]
-            if not instance_name:
-                raise ValueError(
-                    "Instance name can not be inferred from .env file, instance name should be specified explicitly"
-                )
             pg_database, pg_port, pg_version = parse_env_file(env_file)
             instances.append(
                 {
-                    "name": env_file.split(os.sep)[-1].split(".")[0],
+                    "name": instance_name or env_file.split(os.sep)[-1].split(".")[0],
                     "pg_user": pg_user.strip(),
                     "pg_passfile": pg_passfile.strip(),
                     "pg_database": pg_database,

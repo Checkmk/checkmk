90efd459db143e40562b7a9871c2fd9cfb70d160
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-09-22 11:11:12 +0200

    15644 FIX postgres_processes: Restore Monitoring if Instance Name is Missing
    SUP-14739
    SUP-15600
    SUP-15731
    SUP-15740
    SUP-15692
    
    Change-Id: I5eaa7726301e82b8098942538ffe9dfc6a19837f

--- cmk/base/plugins/agent_based/postgres_instances.py
+++ cmk/base/plugins/agent_based/postgres_instances.py
@@ -3,32 +3,41 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package
 
+import dataclasses
 from collections.abc import Mapping
 
 from .agent_based_api.v1 import register, Result, Service, State
 from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, StringTable
 
-_InstancesSection = Mapping[str, int | None]
+
+@dataclasses.dataclass
+class Section:
+    instance_to_pid: Mapping[str, int | None]
+    all_pids: frozenset[int]
 
 
-def parse_postgres_instances(string_table: StringTable) -> _InstancesSection:
-    parsed: dict[str, int | None] = {}
+def parse_postgres_instances(string_table: StringTable) -> Section:
+    instance_to_pid: dict[str, int | None] = {}
+    all_pids = []
     is_single = False
     for line in string_table:
         if line[0].startswith("[[[") and line[0].endswith("]]]"):
-            db_id = line[0][3:-3]
+            instance_name = line[0][3:-3].upper()
             is_single = True
-            parsed.setdefault(db_id.upper(), None)
+            instance_to_pid.setdefault(instance_name, None)
         elif len(line) >= 4:
             if not is_single:
-                db_id = line[3].split("/")[-1]
+                instance_name = line[3].split("/")[-1].upper()
+                instance_to_pid.setdefault(instance_name, None)
             try:
-                parsed.setdefault(db_id.upper(), None)
-                parsed.update({db_id.upper(): int(line[0])})
+                pid = int(line[0])
+                all_pids.append(pid)
+                instance_to_pid[instance_name] = pid
             except ValueError:
                 pass
+    instance_to_pid.pop("", None)
 
-    return parsed
+    return Section(instance_to_pid=instance_to_pid, all_pids=frozenset(all_pids))
 
 
 register.agent_section(
@@ -37,12 +46,12 @@ register.agent_section(
 )
 
 
-def discover_postgres_instances(section: _InstancesSection) -> DiscoveryResult:
-    yield from (Service(item=name) for name in section)
+def discover_postgres_instances(section: Section) -> DiscoveryResult:
+    yield from (Service(item=name) for name in section.instance_to_pid)
 
 
-def check_postgres_instances(item: str, section: _InstancesSection) -> CheckResult:
-    if (pid := section.get(item)) is not None:
+def check_postgres_instances(item: str, section: Section) -> CheckResult:
+    if (pid := section.instance_to_pid.get(item)) is not None:
         yield Result(state=State.OK, summary=f"Status: running with PID {pid}")
     else:
         yield Result(
@@ -59,4 +68,27 @@ register.check_plugin(
     service_name="PostgreSQL Instance %s",
     discovery_function=discover_postgres_instances,
     check_function=check_postgres_instances,
+)
+
+
+def discover_postgres_processes(section: Section) -> DiscoveryResult:
+    yield Service()
+
+
+def check_postgres_processes(section: Section) -> CheckResult:
+    count = len(section.all_pids)
+    if count == 0:
+        yield Result(state=State.CRIT, summary=str(count), details="No process matched")
+        return
+    yield Result(state=State.OK, summary=str(count), details="PIDs")
+    for pid in section.all_pids:
+        yield Result(state=State.OK, notice=str(pid))
+
+
+register.check_plugin(
+    name="postgres_processes",
+    sections=["postgres_instances"],
+    service_name="PostgreSQL Process Count",
+    discovery_function=discover_postgres_processes,
+    check_function=check_postgres_processes,
 )

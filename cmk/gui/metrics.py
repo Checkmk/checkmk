b72c10296407338951ef2b7b864507b12e3fe935
Joerg Herbel <joerg.herbel@checkmk.com>
2023-09-22 15:53:03 +0200

    Fix graph dashlet rendering after pydantic v2 upgrade
    Cannot create graph
    Traceback (most recent call last):
      File "/omd/sites/heute/lib/python3/cmk/gui/graphing/_html_render.py", line 828, in _render_graph_content_html
        graph_artwork = compute_graph_artwork(
                        ^^^^^^^^^^^^^^^^^^^^^^
      File "/omd/sites/heute/lib/python3/cmk/gui/graphing/_artwork.py", line 244, in compute_graph_artwork
        return GraphArtwork(
               ^^^^^^^^^^^^^
      File "/omd/sites/heute/lib/python3.11/site-packages/pydantic/main.py", line 165, in __init__
        __pydantic_self__.__pydantic_validator__.validate_python(data, self_instance=__pydantic_self__)
    pydantic_core._pydantic_core.ValidationError: 1 validation error for GraphArtwork
    display_id
      Input should be a valid string [type=string_type, input_value=0, input_type=int]
        For further information visit https://errors.pydantic.dev/2.3/v/string_type
    
    Change-Id: I1e00e34e34b9f03e71ab3c454f32ac50bd819480

--- cmk/gui/metrics.py
+++ cmk/gui/metrics.py
@@ -832,5 +832,5 @@ def page_graph_dashlet(
         graph_specification,
         custom_graph_render_options,
         resolve_combined_single_metric_spec,
-        graph_display_id=json.loads(request.get_str_input_mandatory("id")),
+        graph_display_id=request.get_str_input_mandatory("id"),
     )

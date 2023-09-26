5b48a6b96daf9006ee25867dd9f9b425bb1daaf4
Kenneth Okoh <kenneth.okoh@checkmk.com>
2023-09-26 11:38:38 +0200

    check_mail_loop: Enable EWS mail receiving
    CMK-14044
    
    Change-Id: I2c2e2a125439c96cf865068e2192c4051b93238f

--- cmk/gui/plugins/wato/active_checks_mailbox.py
+++ cmk/gui/plugins/wato/active_checks_mailbox.py
@@ -280,7 +280,7 @@ def _valuespec_active_checks_mail_loop():
             help=_(
                 "This active check sends out special E-Mails to a defined mail address using "
                 "the SMTP protocol and then tries to receive these mails back by querying the "
-                "inbox of a IMAP or POP3 mailbox. With this check you can verify that your whole "
+                "inbox of an IMAP, POP3 or EWS mailbox. With this check you can verify that your whole "
                 "mail delivery progress is working."
             ),
             optional_keys=[
@@ -354,7 +354,7 @@ def _valuespec_active_checks_mail_loop():
                         ],
                     ),
                 ),
-                _mail_receiving_params({"IMAP", "POP3"}),
+                _mail_receiving_params({"IMAP", "POP3", "EWS"}),
                 (
                     "mail_from",
                     EmailAddress(

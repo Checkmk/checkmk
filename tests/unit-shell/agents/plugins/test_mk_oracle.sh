2a40a2a547c9dd31385b0bf02761a70116b3d446
Benedikt Seidl <benedikt.seidl@checkmk.com>
2023-09-27 15:22:21 +0200

    Test for skipping custom sqls based on $SQLS_SIDS
    This should not alter the current behaviour, just document it.
    
    Change-Id: I3e575caba790d603787f797d37888a9d466e78d0

--- tests/unit-shell/agents/plugins/test_mk_oracle.sh
+++ tests/unit-shell/agents/plugins/test_mk_oracle.sh
@@ -866,5 +866,16 @@ test_mk_oracle_mk_ora_db_connect_sqls_tnsalias_tnsping_ok_sid_specific_prefix_br
     teardown_tnsping
 }
 
+test_mk_oracle_sid_matches_defined_sids() {
+    assertTrue 'sid_matches_defined_sids "prefixed_value" "prefix"' # THIS IS BROKEN! TODO: should not match!
+    assertTrue 'sid_matches_defined_sids "some,value,here" "value"'
+    assertFalse 'sid_matches_defined_sids "some,hello,here" "value"'
+    # documentation says, that customer may us "$SIDS" when the section should
+    # be executed for all elements, but this variable contains a newline
+    # seperated list of sids:
+    assertTrue 'sid_matches_defined_sids "some\nvalue\nhere" "value"'
+    assertTrue 'sid_matches_defined_sids "some\nprefixed_value\nhere" "value"' # THIS IS BROKEN! TODO: should not match!
+}
+
 # shellcheck disable=SC1090 # Can't follow
 . "$UNIT_SH_SHUNIT2"

0532e3a56e8ccf8f55823326f9f98862f4b09403
thl-cmk <65558014+thl-cmk@users.noreply.github.com>
2023-10-03 20:20:29 +0200

    keep pylint happy, Unused variable 'voltage_str' (unused-variable)
    just to keep pylint happy, Unused variable 'voltage_str' (unused-variable) 
    'voltage_str' could be completely removed, it's not used in this snmp_section. should go to a separate check plugin (i.e. ups_voltage).

--- cmk/base/plugins/agent_based/ups_power.py
+++ cmk/base/plugins/agent_based/ups_power.py
@@ -19,6 +19,10 @@ def parse_ups_power(
             power = int(power_str)
         except ValueError:
             continue
+        # just to keep pylint happy, Unused variable 'voltage_str' (unused-variable) 
+        # voltage_str could be completely removed
+        voltage_str = voltage_str
+        
         # Some "RPS SpA" systems are not RFC conform in this value.
         # The values can get negative but should never be.
         if power < 0:

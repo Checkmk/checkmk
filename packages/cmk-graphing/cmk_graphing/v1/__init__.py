e827d35d498092092f5979b3e7e67f204753ccc5
Simon Jess <simon.jess@checkmk.com>
2023-10-05 09:28:17 +0200

    Add _color.py
    Change-Id: I6d6541b45b5fcf93a431a94a68bfdeae36f50f94

--- packages/cmk-graphing/cmk_graphing/v1/__init__.py
+++ packages/cmk-graphing/cmk_graphing/v1/__init__.py
@@ -3,6 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from ._color import Color
 from ._localize import Localizable
 
-__all__ = ["Localizable"]
+__all__ = ["Color", "Localizable"]

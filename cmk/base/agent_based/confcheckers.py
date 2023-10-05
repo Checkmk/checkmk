93f6a4a769b4ef01159131397e5e226f947103cc
Sofia Colakovic <sofia.colakovic@checkmk.com>
2023-10-05 10:45:46 +0200

    16161 FIX discovery: Fix missing datasource error for cluster hosts
    Previously, a missing datasouce error would be shown in a discovery
    page of a cluster host if the cluster host had 'No IP' configured
    but had nodes with an agent datasource.
    
    Clustered services of a cluster host would also become vanished.
    
    Checkmk now supports the described setup, there is no error in the
    discovery page and clustered services are discovered.
    
    SUP-15433
    
    Change-Id: I0d1a949603467b9bb72e560e652e7e6777369c31

--- cmk/base/agent_based/confcheckers.py
+++ cmk/base/agent_based/confcheckers.py
@@ -242,7 +242,7 @@ class ConfiguredFetcher:
                 make_sources(
                     host_name_,
                     ip_address_,
-                    ConfigCache.address_family(host_name),
+                    ConfigCache.address_family(host_name_),
                     config_cache=self.config_cache,
                     force_snmp_cache_refresh=(
                         self.force_snmp_cache_refresh if nodes is None else False

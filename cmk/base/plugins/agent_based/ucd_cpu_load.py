320b10c8fc4dccffddf11ef8ef53a03d095ed1b5
thl-cmk <65558014+thl-cmk@users.noreply.github.com>
2023-10-08 14:43:33 +0200

    formating
    adding empty line

--- cmk/base/plugins/agent_based/ucd_cpu_load.py
+++ cmk/base/plugins/agent_based/ucd_cpu_load.py
@@ -4,6 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from typing import List
+
 from .agent_based_api.v1 import register, SNMPTree
 from .agent_based_api.v1.type_defs import StringTable
 from .utils.cpu import Load, Section

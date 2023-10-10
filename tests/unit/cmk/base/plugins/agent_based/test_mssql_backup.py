92dbfefbde2835d6dac84299d68046d27366f308
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2023-10-10 17:11:53 +0200

    15976 FIX mssql_backup: Correct timezone difference for last backup date
    This werk is relevant for users monitoring the age of the last backup
    time of mssql databases in different timezones.
    
    The date/time of the last backup of a mssql database is currently stored
    in local host time without the information about the host timezone. When
    this time is used to check the age of the last backup, it is interpreted
    in the Checkmk server timezone.
    
    When using different timezones, this leads to incorrect values for "Age
    of last database backup" and  if the age is negative, in newer Checkmk
    versions to the warning "Cannot reasonably calculate time since last
    backup (hosts time running ahead)".
    
    The mssql agent plugin will now store the time in UTC and the
    mssql_backup check will interpret the time accordingly.
    
    You will need to update the agent plugin mssql.vbs to receive the
    corrected times.
    
    SUP-14924
    SUP-15900
    SUP-15976
    
    Co-authored-by: Benedikt Seidl <benedikt.seidl@checkmk.com>
    Change-Id: I6e6e9192632d1f35ff1516b6a3f123b74fb4cd90

--- tests/unit/cmk/base/plugins/agent_based/test_mssql_backup.py
+++ tests/unit/cmk/base/plugins/agent_based/test_mssql_backup.py
@@ -4,30 +4,39 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
+import pytest
+
 from tests.testlib import on_time
 
 from cmk.base.plugins.agent_based import mssql_backup as msb
 from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
 
 STRING_TABLE = [
-    ["MSSQL_SQL0x4", "master", "2016-07-08 20:20:27", "D"],
-    ["MSSQL_SQL0x4", "model", "2016-07-08 20:20:28", "D"],
-    ["MSSQL_SQL0x4", "model", "2016-07-12 09:09:42", "L"],
-    ["MSSQL_SQL0x4", "model", "2016-07-11 20:20:07", "I"],
-    ["MSSQL_SQL0x4", "msdb", "2016-07-08", "20:20:43", "D"],
+    ["MSSQL_SQL0x4", "master", "2016-07-08 20:20:27+00:00", "D"],
+    ["MSSQL_SQL0x4", "model", "2016-07-08 20:20:28+00:00", "D"],
+    ["MSSQL_SQL0x4", "model", "2016-07-12 09:09:42+00:00", "L"],
+    ["MSSQL_SQL0x4", "model", "2016-07-11 20:20:07+00:00", "I"],
+    ["MSSQL_SQL0x4", "msdb", "2016-07-08", "20:20:43+00:00", "D"],
     ["MSSQL_SQL0x4", "msdb", "-", "-", "-", "no backup found"],
     ["MSSQL_SQL0x4", "foo"],
     ["MSSQL_SQL0x4", "bar", "12345678"],
     ["MSSQL_Parrot", "Polly", "-", "-", "-", "ERROR: Polly has no crackers"],
 ]
 
+STRING_TABLE_WITHOUT_TZ = [
+    ["MSSQL_SQL0x4", "master", "2016-07-08 20:20:27", "D"],
+]
 
-def _get_section() -> msb.Section:
-    return msb.parse_mssql_backup(STRING_TABLE)
+
+def _get_section(string_table: list[list[str]]) -> msb.Section:
+    return msb.parse_mssql_backup(string_table)
 
 
 def test_discovery_default() -> None:
-    assert sorted(msb.discover_mssql_backup({"mode": "summary"}, _get_section())) == sorted(
+    assert sorted(
+        msb.discover_mssql_backup({"mode": "summary"}, _get_section(STRING_TABLE))
+    ) == sorted(
         [
             Service(item="MSSQL_SQL0x4 master"),
             Service(item="MSSQL_SQL0x4 model"),
@@ -36,13 +45,15 @@ def test_discovery_default() -> None:
             Service(item="MSSQL_Parrot Polly"),
         ]
     )
-    assert not list(msb.discover_mssql_backup_per_type({"mode": "summary"}, _get_section()))
+    assert not list(
+        msb.discover_mssql_backup_per_type({"mode": "summary"}, _get_section(STRING_TABLE))
+    )
 
 
 def test_discovery_single() -> None:
-    assert not list(msb.discover_mssql_backup({"mode": "per_type"}, _get_section()))
+    assert not list(msb.discover_mssql_backup({"mode": "per_type"}, _get_section(STRING_TABLE)))
     assert sorted(
-        msb.discover_mssql_backup_per_type({"mode": "per_type"}, _get_section())
+        msb.discover_mssql_backup_per_type({"mode": "per_type"}, _get_section(STRING_TABLE))
     ) == sorted(
         [
             Service(item="MSSQL_SQL0x4 master Database"),
@@ -57,10 +68,20 @@ def test_discovery_single() -> None:
     )
 
 
-def test_check() -> None:
-    with on_time("2016-07-15", "UTC"):
-        assert list(msb.check_mssql_backup("MSSQL_SQL0x4 master", {}, _get_section())) == [
-            Result(state=State.OK, summary="[database] Last backup: Jul 08 2016 20:20:27"),
+@pytest.mark.parametrize(
+    ["string_table", "local_timezone", "rendered_datetime"],
+    [
+        pytest.param(STRING_TABLE, "UTC", "Jul 08 2016 20:20:27", id="UTC"),
+        pytest.param(STRING_TABLE, "Europe/Berlin", "Jul 08 2016 22:20:27", id="CEST"),
+        pytest.param(STRING_TABLE_WITHOUT_TZ, "UTC", "Jul 08 2016 20:20:27", id="UTC without tz"),
+    ],
+)
+def test_check(string_table: StringTable, local_timezone: str, rendered_datetime: str) -> None:
+    with on_time("2016-07-15", local_timezone):
+        assert list(
+            msb.check_mssql_backup("MSSQL_SQL0x4 master", {}, _get_section(string_table))
+        ) == [
+            Result(state=State.OK, summary=f"[database] Last backup: {rendered_datetime}"),
             Result(state=State.OK, summary="Time since last backup: 6 days 3 hours"),
             Metric("backup_age_database", 531573.0),
         ]
@@ -68,7 +89,7 @@ def test_check() -> None:
 
 def test_check_with_seconds_metric() -> None:
     with on_time("2016-07-15", "UTC"):
-        assert list(msb.check_mssql_backup("MSSQL_SQL0x4 bar", {}, _get_section())) == [
+        assert list(msb.check_mssql_backup("MSSQL_SQL0x4 bar", {}, _get_section(STRING_TABLE))) == [
             Result(state=State.OK, summary="[database] Last backup: May 23 1970 21:21:18"),
             Result(state=State.OK, summary="Time since last backup: 46 years 64 days"),
             Metric("seconds", 1456195122.0),
@@ -76,6 +97,6 @@ def test_check_with_seconds_metric() -> None:
 
 
 def test_check_error() -> None:
-    assert list(msb.check_mssql_backup("MSSQL_Parrot Polly", {}, _get_section())) == [
+    assert list(msb.check_mssql_backup("MSSQL_Parrot Polly", {}, _get_section(STRING_TABLE))) == [
         Result(state=State.CRIT, summary="Polly has no crackers"),
     ]

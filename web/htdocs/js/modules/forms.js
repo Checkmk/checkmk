411fcd8e0ec645bc09e785234998fdeec1df1a59
Christoph Rauch <christoph.rauch@checkmk.com>
2023-10-10 09:15:19 +0200

    15098 FIX setup form page rendering stuck when page has lots of select boxes
    Previously, when a HTML form page was rendered with a lot (>hundreds) of
    select boxes, the augmentation process to make them searchable was taking
    a very long time.
    
    Until a proper fix for this performance problem can be found, we now prevent
    the augmentation process whenever it would take more than 3 seconds.
    
    The user-interface is almost not affected, only the select boxes are no longer
    searchable. No manual action of the user is necessary to make this work.
    
    Change-Id: Ic0d81df11be84289476876801d0c1ba00f331b1b

--- web/htdocs/js/modules/forms.js
+++ web/htdocs/js/modules/forms.js
@@ -25,6 +25,11 @@ export function enable_select2_dropdowns(container) {
     if (!container) container = $(document);
 
     elements = $(container).find(".select2-enable").not(".vlof_prototype .select2-enable");
+    const max_wait_seconds = 3; // Let the user wait for max 3 seconds. We can convert ~60-80 selects per second.
+    if (elements.length > max_wait_seconds * 60) {
+        // If we've got too many of those, we bail out early instead of letting the user wait potentially "forever".
+        return;
+    }
     elements.select2({
         dropdownAutoWidth: true,
         minimumResultsForSearch: 5,

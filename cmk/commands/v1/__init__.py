154e969becf51c52a8901bf12a658080000bba3c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-10-11 14:32:12 +0200

    commands: use relative imports
    Change-Id: I8d0011f2d75cd0ce0ff56ea83e57db77b64793bd

--- cmk/commands/v1/__init__.py
+++ cmk/commands/v1/__init__.py
@@ -2,18 +2,10 @@
 # Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-# pylint: disable=cmk-module-layer-violation  # TODO: Fix the layering checker
 
-from cmk.commands.v1._active_checks import ActiveCheckCommand, ActiveService
-from cmk.commands.v1._special_agents import SpecialAgentCommand, SpecialAgentConfig
-from cmk.commands.v1._utils import (
-    EnvironmentConfig,
-    HostConfig,
-    HTTPProxy,
-    IPAddressFamily,
-    Secret,
-    SecretType,
-)
+from ._active_checks import ActiveCheckCommand, ActiveService
+from ._special_agents import SpecialAgentCommand, SpecialAgentConfig
+from ._utils import EnvironmentConfig, HostConfig, HTTPProxy, IPAddressFamily, Secret, SecretType
 
 __all__ = [
     "ActiveCheckCommand",
--- cmk/commands/v1/_active_checks.py
+++ cmk/commands/v1/_active_checks.py
@@ -2,13 +2,12 @@
 # Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-# pylint: disable=cmk-module-layer-violation  # TODO: Fix the layering checker
 
 from collections.abc import Callable, Iterable, Mapping, Sequence
 from dataclasses import dataclass
 from typing import Generic, TypeVar
 
-from cmk.commands.v1._utils import EnvironmentConfig, HostConfig, Secret
+from ._utils import EnvironmentConfig, HostConfig, Secret
 
 _T = TypeVar("_T")
 

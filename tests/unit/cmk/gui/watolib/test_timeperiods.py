0f44de3c2263c14368c876846a3321e9f82748e2
Lars Michelsen <lm@checkmk.com>
2023-10-13 18:31:56 +0200

    Fix github actions
    Change-Id: I0cd8e9b3bbd5877e980e5d0767721c9ef82f23da

--- tests/unit/cmk/gui/watolib/test_timeperiods.py
+++ tests/unit/cmk/gui/watolib/test_timeperiods.py
@@ -3,18 +3,22 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from tests.testlib.utils import is_enterprise_repo
+
 from cmk.gui.watolib.timeperiods import timeperiod_usage_finder_registry
 
 
 def test_group_usage_finder_registry_entries() -> None:
     expected = [
-        "find_timeperiod_usage_in_alert_handler_rules",
         "find_timeperiod_usage_in_ec_rules",
         "find_timeperiod_usage_in_host_and_service_rules",
         "find_timeperiod_usage_in_notification_rules",
         "find_timeperiod_usage_in_time_specific_parameters",
         "find_timeperiod_usage_in_users",
     ]
+
+    if is_enterprise_repo():
+        expected.append("find_timeperiod_usage_in_alert_handler_rules")
 
     registered = [f.__name__ for f in timeperiod_usage_finder_registry.values()]
     assert sorted(registered) == sorted(expected)

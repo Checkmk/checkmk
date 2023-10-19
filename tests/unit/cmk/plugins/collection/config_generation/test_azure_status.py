adc1ca848718f4695a882c0e245bddfc11968804
Sofia Colakovic <sofia.colakovic@checkmk.com>
2023-10-19 15:33:16 +0200

    config generation: migrate azure_status to the new API
    Change-Id: I308aa1ca93243f2d15fb54f3c67f3c50b14f19da

---
+++ tests/unit/cmk/plugins/collection/config_generation/test_azure_status.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.config_generation.v1 import HostConfig, IPAddressFamily, SpecialAgentCommand
+from cmk.plugins.collection.config_generation.azure_status import special_agent_azure_status
+
+HOST_CONFIG = HostConfig(
+    name="hostname",
+    address="0.0.0.1",
+    alias="host_alias",
+    ip_family=IPAddressFamily.IPv4,
+    ipv4address=None,
+    ipv6address=None,
+    additional_ipv4addresses=[],
+    additional_ipv6addresses=[],
+)
+
+
+def test_azure_status_argument_parsing() -> None:
+    param_dict = {"regions": ["eastus", "centralus", "northcentralus"]}
+    params = special_agent_azure_status.parameter_parser(param_dict)
+    commands = list(special_agent_azure_status.config_function(params, HOST_CONFIG, {}))
+
+    assert len(commands) == 1
+    assert commands[0] == SpecialAgentCommand(
+        command_arguments=["eastus", "centralus", "northcentralus"]
+    )

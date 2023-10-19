ab1107a3b9f0d3575b7b8a8c6e9dc6ae31f59298
matthias1232 <matthias1232@users.noreply.github.com>
2023-10-20 00:07:09 +0200

    Update notifications.py

--- cmk/gui/plugins/wato/notifications.py
+++ cmk/gui/plugins/wato/notifications.py
@@ -16,9 +16,17 @@ from cmk.utils.ms_teams_constants import (
 )
 from cmk.utils.site import url_prefix
 
+import cmk.gui.mkeventd as mkeventd
 from cmk.gui.http import request
 from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import notification_macro_help
+from cmk.gui.plugins.wato.utils import (
+    HTTPProxyReference,
+    IndividualOrStoredPassword,
+    MigrateToIndividualOrStoredPassword,
+    notification_macro_help,
+    notification_parameter_registry,
+    NotificationParameter,
+)
 from cmk.gui.valuespec import (
     Age,
     Alternative,
@@ -38,13 +46,6 @@ from cmk.gui.valuespec import (
     TextInput,
     Transform,
     Tuple,
-)
-from cmk.gui.wato import (
-    HTTPProxyReference,
-    IndividualOrStoredPassword,
-    MigrateToIndividualOrStoredPassword,
-    notification_parameter_registry,
-    NotificationParameter,
 )
 from cmk.gui.watolib.password_store import passwordstore_choices
 
@@ -202,6 +203,7 @@ def _vs_add_common_mail_elements(elements):
 
 
 def _get_url_prefix_specs(default_choice, default_value=DEF_VALUE):
+
     return Transform(
         valuespec=CascadingDropdown(
             title=_("URL prefix for links to Checkmk"),
@@ -311,7 +313,7 @@ class NotificationParameterMail(NotificationParameter):
             ]
         )
 
-        if cmk_version.edition() is not cmk_version.Edition.CRE:
+        if not cmk_version.is_raw_edition():
             import cmk.gui.cee.plugins.wato.syncsmtp  # pylint: disable=no-name-in-module
 
             elements += cmk.gui.cee.plugins.wato.syncsmtp.cee_html_mail_smtp_sync_option
@@ -362,7 +364,7 @@ class NotificationParameterMail(NotificationParameter):
                         ),
                     ),
                 ),
-            ),        
+            ),
             (
                 "graphs_per_notification",
                 Integer(
@@ -1607,6 +1609,43 @@ $LONGSERVICEOUTPUT$
                         help=_("Is used to specify which domain the alert is related to."),
                         allow_empty=False,
                         size=68,
+                    ),
+                ),
+            ],
+        )
+
+
+@notification_parameter_registry.register
+class NotificationParameterMKEventDaemon(NotificationParameter):
+    @property
+    def ident(self) -> str:
+        return "mkeventd"
+
+    @property
+    def spec(self):
+        return Dictionary(
+            title=_("Create notification with the following parameters"),
+            elements=[
+                (
+                    "facility",
+                    DropdownChoice(
+                        title=_("Syslog Facility to use"),
+                        help=_(
+                            "The notifications will be converted into syslog messages with "
+                            "the facility that you choose here. In the Event Console you can "
+                            "later create a rule matching this facility."
+                        ),
+                        choices=mkeventd.syslog_facilities,
+                    ),
+                ),
+                (
+                    "remote",
+                    IPv4Address(
+                        title=_("IP Address of remote Event Console"),
+                        help=_(
+                            "If you set this parameter then the notifications will be sent via "
+                            "syslog/UDP (port 514) to a remote Event Console or syslog server."
+                        ),
                     ),
                 ),
             ],

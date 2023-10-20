cda9a5c73a290ad01a120a4eb3b4c93de7bd717d
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2023-10-20 15:29:42 +0200

    Small refactoring
    Change-Id: I1128e79e44fcd1f5f471e90201c0e05c29e256f1

--- cmk/gui/sidebar/_snapin/_search.py
+++ cmk/gui/sidebar/_snapin/_search.py
@@ -8,7 +8,7 @@ import itertools
 import json
 import re
 import traceback
-from collections.abc import Callable, Iterable
+from collections.abc import Iterable
 from dataclasses import dataclass
 from enum import Enum, unique
 from typing import Final, Literal, TypeVar
@@ -1372,14 +1372,22 @@ match_plugin_registry.register(MonitorMenuMatchPlugin())
 #   '----------------------------------------------------------------------'
 
 
-class MenuSearchResultsRenderer:
+class MenuSearchResultsRenderer(abc.ABC):
     MAX_RESULTS_BEFORE_SHOW_ALL: Final = 10
 
-    def __init__(self, search_type: Literal["monitoring", "setup"]) -> None:
-        generate_results, max_results_after_show_all = self._search_type_specific_setup(search_type)
-        self.generate_results: Final = generate_results
-        self.max_results_after_show_all: Final = max_results_after_show_all
-        self.search_type: Final = search_type
+    @abc.abstractmethod
+    def generate_results(self, query: SearchQuery) -> SearchResultsByTopic:
+        raise NotImplementedError()
+
+    @property
+    @abc.abstractmethod
+    def search_type(self) -> Literal["monitoring", "setup"]:
+        raise NotImplementedError()
+
+    @property
+    @abc.abstractmethod
+    def max_results_after_show_all(self) -> None | int:
+        raise NotImplementedError()
 
     def render(self, query: str) -> str:
         try:
@@ -1391,25 +1399,6 @@ class MenuSearchResultsRenderer:
         except MKException as error:
             return self._render_error(error)
         return self._render_results(results)
-
-    @staticmethod
-    def _search_type_specific_setup(
-        search_type: Literal["monitoring", "setup"]
-    ) -> tuple[Callable[[str], SearchResultsByTopic], int | None]:
-        if search_type == "monitoring":
-            return (
-                QuicksearchManager(raise_too_many_rows_error=False).generate_results,
-                None,
-            )
-        if search_type == "setup":
-            return (
-                IndexSearcher(
-                    get_redis_client(),
-                    PermissionsHandler(),
-                ).search,
-                80,
-            )
-        raise NotImplementedError(f"Renderer not implemented for type '{search_type}'")
 
     def _render_error(self, error: MKException) -> str:
         with output_funnel.plugged():
@@ -1553,6 +1542,31 @@ class MenuSearchResultsRenderer:
         html.close_li()
 
 
+class MonitorMenuSearchResultsRenderer(MenuSearchResultsRenderer):
+    max_results_after_show_all: Final = None
+    search_type: Final = "monitoring"
+
+    def __init__(self) -> None:
+        self._search_manager: Final = QuicksearchManager(raise_too_many_rows_error=False)
+
+    def generate_results(self, query: SearchQuery) -> SearchResultsByTopic:
+        return self._search_manager.generate_results(query)
+
+
+class SetupMenuSearchResultsRenderer(MenuSearchResultsRenderer):
+    max_results_after_show_all: Final = 80
+    search_type: Final = "setup"
+
+    def __init__(self) -> None:
+        self._search_manager: Final = IndexSearcher(
+            get_redis_client(),
+            PermissionsHandler(),
+        )
+
+    def generate_results(self, query: SearchQuery) -> SearchResultsByTopic:
+        return self._search_manager.search(query)
+
+
 _TIterItem = TypeVar("_TIterItem")
 
 
@@ -1574,14 +1588,14 @@ def _evaluate_iterable_up_to(
 class PageSearchMonitoring(AjaxPage):
     def page(self) -> PageResult:
         query = request.get_str_input_mandatory("q")
-        return MenuSearchResultsRenderer("monitoring").render(query)
+        return MonitorMenuSearchResultsRenderer().render(query)
 
 
 class PageSearchSetup(AjaxPage):
     def page(self) -> PageResult:
         query = request.get_str_input_mandatory("q")
         try:
-            return MenuSearchResultsRenderer("setup").render(query)
+            return SetupMenuSearchResultsRenderer().render(query)
         except IndexNotFoundException:
             with output_funnel.plugged():
                 html.open_div(class_="topic")

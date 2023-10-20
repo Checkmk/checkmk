e71b169e4eb4bcc77a7679afd708b6fd82d6597e
Sofia Colakovic <sofia.colakovic@checkmk.com>
2023-10-20 13:39:55 +0200

    ftp: add tests
    Change-Id: I54e5c36f4a6f2132a841d1a24e84425bbd1d765a

--- tests/unit/checks/test_check_ftp.py
+++ tests/unit/checks/test_check_ftp.py
@@ -15,8 +15,43 @@ pytestmark = pytest.mark.checks
 @pytest.mark.parametrize(
     "params,expected_args",
     [
-        ({}, ["-H", "$HOSTADDRESS$"]),
-        ({"port": 21}, ["-H", "$HOSTADDRESS$", "-p", 21]),
+        pytest.param({}, ["-H", "$HOSTADDRESS$"], id="params empty"),
+        pytest.param({"port": 21}, ["-H", "$HOSTADDRESS$", "-p", 21], id="some params present"),
+        pytest.param(
+            {
+                "port": 21,
+                "response_time": (100.0, 200.0),
+                "timeout": 10,
+                "refuse_state": "crit",
+                "send_string": "abc",
+                "expect": ["cde"],
+                "ssl": True,
+                "cert_days": (5, 6),
+            },
+            [
+                "-H",
+                "$HOSTADDRESS$",
+                "-p",
+                21,
+                "-w",
+                "0.100000",
+                "-c",
+                "0.200000",
+                "-t",
+                10,
+                "-r",
+                "crit",
+                "-s",
+                "abc",
+                "-e",
+                "cde",
+                "--ssl",
+                "-D",
+                5,
+                6,
+            ],
+            id="all params present",
+        ),
     ],
 )
 def test_check_ftp_argument_parsing(
@@ -25,3 +60,16 @@ def test_check_ftp_argument_parsing(
     """Tests if all required arguments are present."""
     active_check = ActiveCheck("check_ftp")
     assert active_check.run_argument_function(params) == expected_args
+
+
+@pytest.mark.parametrize(
+    "params, expected_item",
+    [
+        pytest.param({}, "FTP", id="no port"),
+        pytest.param({"port": 21}, "FTP", id="port 21"),
+        pytest.param({"port": 22}, "FTP Port 22", id="port different from 21"),
+    ],
+)
+def test_check_ftp_get_item(params: Mapping[str, object], expected_item: str) -> None:
+    active_check = ActiveCheck("check_ftp")
+    assert active_check.run_service_description(params) == expected_item

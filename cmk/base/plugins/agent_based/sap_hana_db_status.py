8534c0deb6a242508c89fded26ac4b1db1c89105
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-10-20 13:02:49 +0200

    15876 FIX sap_hana_db_status: Be ok for passive nodes
    Change-Id: Iaf3ba01dc71386081ba2f1d3b1ee843ca90784d4

--- cmk/base/plugins/agent_based/sap_hana_db_status.py
+++ cmk/base/plugins/agent_based/sap_hana_db_status.py
@@ -27,16 +27,43 @@ register.agent_section(
 )
 
 
-def discover_sap_hana_db_status(section: SectionDBStatus) -> DiscoveryResult:
-    for item in section:
+def discover_sap_hana_db_status(
+    section_sap_hana_db_status: SectionDBStatus | None,
+    section_sap_hana_replication_status: sap_hana.ParsedSection | None,
+) -> DiscoveryResult:
+    if not section_sap_hana_db_status:
+        return
+    for item in section_sap_hana_db_status:
         yield Service(item=item)
 
 
-def check_sap_hana_db_status(item: str, section: SectionDBStatus) -> CheckResult:
-    db_status = section.get(item)
+def check_sap_hana_db_status(
+    item: str,
+    section_sap_hana_db_status: SectionDBStatus | None,
+    section_sap_hana_replication_status: sap_hana.ParsedSection | None,
+) -> CheckResult:
+    if section_sap_hana_db_status is None:
+        return
+
+    db_status = section_sap_hana_db_status.get(item)
 
     if not db_status:
         raise IgnoreResultsError("Login into database failed.")
+
+    db_state = MAP_DB_STATUS.get(db_status, State.CRIT)
+    repl_state = (
+        None
+        if section_sap_hana_replication_status is None
+        else section_sap_hana_replication_status.get(item, {}).get("sys_repl_status")
+    )
+
+    if (
+        db_state is State.CRIT
+        and repl_state is not None
+        and sap_hana.get_replication_state(repl_state)[1] == "passive"
+    ):
+        yield Result(state=State.OK, summary="System is in passive mode")
+        return
 
     yield Result(state=MAP_DB_STATUS.get(db_status, State.CRIT), summary=db_status)
 
@@ -44,6 +71,7 @@ def check_sap_hana_db_status(item: str, section: SectionDBStatus) -> CheckResult
 register.check_plugin(
     name="sap_hana_db_status",
     service_name="SAP HANA Database Status %s",
+    sections=["sap_hana_db_status", "sap_hana_replication_status"],
     discovery_function=discover_sap_hana_db_status,
     check_function=check_sap_hana_db_status,
 )

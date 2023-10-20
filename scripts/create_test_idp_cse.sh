729387ee9d71f38fa685228e4cd50559174e2a81
Max Linke <max.linke@checkmk.com>
2023-10-20 13:57:50 +0200

    add script to start oauth server
    Change-Id: I1e4fa08a0644484ac3b952831effb0965f599e49

---
+++ scripts/create_test_idp_cse.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+#
+set -e
+
+REPO_PATH="$(dirname "$(dirname "$(realpath "$0")")")"
+
+# acquire sudo to run to change the configuration
+sudo true
+
+configure_cognito() {
+    idp_url="$1"
+    checkmk_port="$2"
+
+    client_id="notused"
+    base_url="http://localhost:$checkmk_port"
+    well_known="$idp_url/.well-known/openid-configuration"
+
+    # Create JSON object
+    JSON=$(printf '{\n "%s":"%s",\n "%s":"%s",\n "%s":"%s" \n}' \
+        "client_id" "$client_id" \
+        "base_url" "$base_url" \
+        "well_known" "$well_known")
+
+    # Write JSON object to file
+    sudo mkdir -p /etc/cse
+    echo "$JSON" | sudo tee /etc/cse/cognito-cmk.json >/dev/null
+}
+
+
+PORT=5551
+# URL under which we can reach the openid provider
+export URL="http://localhost:${PORT}"
+
+# checkmk uses port 5000 by default
+configure_cognito $URL 5000
+
+export PYTHONPATH="${REPO_PATH}/tests/testlib"
+$REPO_PATH/scripts/run-pipenv run uvicorn openid_oauth_provider:application --port "$PORT"

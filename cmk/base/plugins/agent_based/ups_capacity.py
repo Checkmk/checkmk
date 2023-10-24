6016115724356fc63256f206eae8ba226f0f5f2b
Benedikt Seidl <benedikt.seidl@checkmk.com>
2023-10-24 18:56:33 +0200

    stricter license header tests
    Change-Id: Idbb7eb550f47efe8092bfe90abf21a948ffcbe8a

--- cmk/base/plugins/agent_based/ups_capacity.py
+++ cmk/base/plugins/agent_based/ups_capacity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (C) 2020 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-# conditions defined in the file COPYING, which is part of this source code package
+# conditions defined in the file COPYING, which is part of this source code package.
 
 
 from .agent_based_api.v1 import register, SNMPTree
--- tests/unit/cmk/special_agents/agent_kube/test_namespace.py
+++ tests/unit/cmk/special_agents/agent_kube/test_namespace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (C) 2022 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
-# conditions defined in the file COPYING, which is part of this source code package
+# conditions defined in the file COPYING, which is part of this source code package.
+
 from tests.unit.cmk.special_agents.agent_kube.factory import (
     APINamespaceFactory,
     APIPodFactory,

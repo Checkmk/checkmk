6005c87b899789e247e47ad3a07cd59820895bb5
Lars Michelsen <lm@checkmk.com>
2023-10-25 10:27:23 +0200

    Fix autocompleter test in github actions
    Change-Id: Icc559b62320690fcd346ecae3dc59167dee991a2

--- tests/unit/cmk/gui/test_autocompleters.py
+++ tests/unit/cmk/gui/test_autocompleters.py
@@ -3,28 +3,32 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from tests.testlib.utils import is_enterprise_repo
+
 from cmk.gui.valuespec import autocompleter_registry
 
 
 def test_builtin_autocompleters_registered() -> None:
     registered = autocompleter_registry.keys()
-    assert sorted(registered) == sorted(
-        [
-            "allgroups",
-            "available_graphs",
-            "check_cmd",
-            "combined_graphs",
-            "config_hostname",
-            "kubernetes_labels",
-            "label",
-            "monitored_hostname",
-            "monitored_metrics",
-            "monitored_service_description",
-            "service_levels",
-            "sites",
-            "syslog_facilities",
-            "tag_groups",
-            "tag_groups_opt",
-            "wato_folder_choices",
-        ]
-    )
+    expected = [
+        "allgroups",
+        "available_graphs",
+        "check_cmd",
+        "config_hostname",
+        "kubernetes_labels",
+        "label",
+        "monitored_hostname",
+        "monitored_metrics",
+        "monitored_service_description",
+        "service_levels",
+        "sites",
+        "syslog_facilities",
+        "tag_groups",
+        "tag_groups_opt",
+        "wato_folder_choices",
+    ]
+
+    if is_enterprise_repo():
+        expected.append("combined_graphs")
+
+    assert sorted(registered) == sorted(expected)

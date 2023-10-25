92da33ebaf7169a8bbdd891cbdfa24631ba8ab10
Jonas Scharpf <jonas.scharpf@checkmk.com>
2023-10-25 07:58:44 +0200

    Install missing packages for OpenSSL in Centos 6
    rework OpenSSL install command
    
    Change-Id: I6d5b0134d72313575aec6c2f6d654e44ef2befe4

--- buildscripts/infrastructure/build-nodes/scripts/install-openssl.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-openssl.sh
@@ -13,12 +13,12 @@ OPENSSL_VERSION=3.0.11
 DIR_NAME=openssl-${OPENSSL_VERSION}
 ARCHIVE_NAME=${DIR_NAME}.tar.gz
 TARGET_DIR="/opt"
+TARGET="" # for x64, use the default target
 
 # OpenSSL "config" seems to have problems with detecting 32bit architecture in some cases
-CONFIG_COMMAND=config
-[ "${ARCHITECTURE}" = i386 ] && CONFIG_COMMAND="Configure linux-x86"
+[ "${ARCHITECTURE}" = i386 ] && TARGET="linux-x86"
 # Increase this to enforce a recreation of the build cache
-BUILD_ID=7
+BUILD_ID=8
 
 build_package() {
     mkdir -p "$TARGET_DIR/src"
@@ -30,7 +30,7 @@ build_package() {
     # Now build the package
     tar xf "${ARCHIVE_NAME}"
     cd "${DIR_NAME}"
-    ./"${CONFIG_COMMAND}" --libdir=lib --prefix="${TARGET_DIR}/${DIR_NAME}" enable-md2 -Wl,-rpath,/opt/"${DIR_NAME}"/lib
+    ./config "${TARGET}" --libdir=lib --prefix="${TARGET_DIR}/${DIR_NAME}" enable-md2 -Wl,-rpath,/opt/"${DIR_NAME}"/lib
     make -j6
     make install
 

Checkmk/checkmk

commit e7f4ce6e1116d3a925020215126b19e71c7dd66e

Add basic version and command check safeguard for infra install scripts
CMK-14498

Change-Id: Id4126377a2280be13d5e2565b8d309b39febf459

buildscripts/infrastructure/build-nodes/scripts/install-nodejs.sh
@@ -28,19 +28,6 @@ install_package() {
     rm -rf /var/lib/apt/lists/*
 }
 
-test_package() {
-    log "Testing for node v$NODEJS_VERSION in \$PATH"
-    node --version | grep "^v$NODEJS_VERSION\." >/dev/null 2>&1 || (
-        echo "Invalid version: $(node --version)"
-        exit 1
-    )
-
-    log "Testing for npm $NPM_VERSION in \$PATH"
-    npm --version | grep "^$NPM_VERSION\." >/dev/null 2>&1 || (
-        echo "Invalid version: $(npm --version)"
-        exit 1
-    )
-}
-
-install_package
-test_package
+# install_package
+test_package "node --version" "^v$NODEJS_VERSION\."
+test_package "npm --version" "^$NPM_VERSION\."

buildscripts/infrastructure/build-nodes/scripts/build_lib.sh
@@ -195,3 +195,11 @@ get_version() {
     local NAME="$2"
     make --no-print-directory --file="$(find_defines_make "$SEARCH_PATH")" print-"$NAME"
 }
+
+test_package() {
+    log "Testing for ${1% *} in \$PATH"
+    $1 | grep "$2" >/dev/null 2>&1 || (
+        echo "Invalid version: $($1)"
+        exit 1
+    )
+}

buildscripts/infrastructure/build-nodes/scripts/install-bazel.sh
@@ -29,3 +29,5 @@ if [ "$1" != "link-only" ]; then
 fi
 
 ln -s "${TARGET_DIR}/${DIR_NAME}/${BAZEL_EXE_FILE}" "/usr/bin/bazel"
+
+test_package "bazel --version" "^bazel $BAZEL_VERSION$"

buildscripts/infrastructure/build-nodes/scripts/install-buildifier.sh
@@ -21,13 +21,7 @@ install_package() {
         go get github.com/bazelbuild/buildtools/buildifier@${BUILDIFIER_VERSION}
 }
 
-test_package() {
-    log "Testing for go in \$PATH"
-    go version | grep "go$GO_VERSION\." >/dev/null 2>&1 || (
-        echo "Invalid version: $(go version)"
-        exit 1
-    )
-}
+test_package "go version" "go$GO_VERSION\."
 
 case "$DISTRO" in
     ubuntu-20.04)

buildscripts/infrastructure/build-nodes/scripts/install-cmake.sh
@@ -37,3 +37,5 @@ if [ "$1" != "link-only" ]; then
     cached_build "${TARGET_DIR}" "${DIR_NAME}" "${BUILD_ID}" "${DISTRO}" "${BRANCH_VERSION}"
 fi
 ln -sf "${TARGET_DIR}/${DIR_NAME}/bin/"* /usr/bin/
+
+test_package "cmake --version" $CMAKE_VERSION

buildscripts/infrastructure/build-nodes/scripts/install-gnu-toolchain.sh
@@ -160,3 +160,22 @@ if [ "$1" != "link-only" ]; then
     cached_build "${TARGET_DIR}" "${DIR_NAME}" "${BUILD_ID}" "${DISTRO}" "${BRANCH_VERSION}"
 fi
 set_symlinks
+
+test_packages() {
+    for i in $(dpkg -L binutils | grep '/bin/'); do
+        this_version=$($i --version)
+        if [[ "$this_version" == *"Binutils)"* ]]; then
+            echo "$this_version" | grep -q "$BINUTILS_VERSION" >/dev/null 2>&1 || (
+                echo "Invalid version: $(i)"
+                exit 1
+            )
+        else
+            echo "$i not of interest"
+            # e.g. /usr/bin/dwp would report "GNU dwp (GNU Binutils for Ubuntu) 2.34"
+        fi
+    done
+}
+
+test_packages
+test_package "gcc --version" "$GCC_VERSION$"
+test_package "gdb --version" "$GDB_VERSION$"

buildscripts/infrastructure/build-nodes/scripts/install-openssl.sh
@@ -39,3 +39,5 @@ build_package() {
 }
 
 cached_build "${TARGET_DIR}" "${DIR_NAME}" "${BUILD_ID}" "${DISTRO}" "${BRANCH_VERSION}"
+
+test_package "openssl version" "^OpenSSL $OPENSSL_VERSION"

buildscripts/infrastructure/build-nodes/scripts/install-patchelf.sh
@@ -5,6 +5,10 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+# shellcheck source=build_lib.sh
+. "${SCRIPT_DIR}/build_lib.sh"
+
 VERSION="0.14.3"
 INSTALL_PREFIX=/opt/patchelf-${VERSION}
 
@@ -41,3 +45,5 @@ echo "$PWD"
 make -j8 install
 
 ln -sf "${INSTALL_PREFIX}/bin/"* /usr/bin
+
+test_package "patchelf --version" "patchelf $VERSION$"

commit 5aaf0567f6ad5d1112d020f52b65b2a5af11b58f

Do not skip NodeJS installation
... bug introduced with e7f4ce6e1116d3a925020215126b19e71c7dd66e

Change-Id: Ie24b3ac49233389921cf2df7b4e6d83f999bbb94

buildscripts/infrastructure/build-nodes/scripts/install-nodejs.sh
@@ -28,6 +28,6 @@ install_package() {
     rm -rf /var/lib/apt/lists/*
 }
 
-# install_package
+install_package
 test_package "node --version" "^v$NODEJS_VERSION\."
 test_package "npm --version" "^$NPM_VERSION\."
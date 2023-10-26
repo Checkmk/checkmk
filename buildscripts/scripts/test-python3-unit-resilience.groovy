7a304f8e1fea7d7f280cfbac9645661bc1c7b210
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2023-10-26 09:48:32 +0200

    Reland "unit resilience test: Store & archive junit report"
    Change-Id: I6c77e0397c63f4e8a3b97a43f9d42a7636382307

--- buildscripts/scripts/test-python3-unit-resilience.groovy
+++ buildscripts/scripts/test-python3-unit-resilience.groovy
@@ -4,17 +4,28 @@
 
 def main() {
     def docker_args = "--ulimit nofile=1024:1024 --init";
+    def relative_result_path = "results/junit-resilience.xml"
+    def result_path = "${checkout_dir}/${relative_result_path}";
     docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
         docker_image_from_alias("IMAGE_TESTING").inside(docker_args) {
             stage('run test-unit-resilience') {
                 dir("${checkout_dir}") {
                     try {
-                        sh("make -C tests test-unit-resilience");
-                    }
-                    catch(Exception e) {
+                        withEnv([
+                            "PYTEST_ADDOPTS='--junitxml=${result_path}'",
+                        ]) {
+                            sh("make -C tests test-unit-resilience");
+                        }
+                    } catch(Exception e) {
                         // We want to keep failed resilience builds in order to follow a process, see CMK-14487
                         currentBuild.setKeepLog(true)
                         throw e
+                    } finally {
+                        step([
+                            $class: "JUnitResultArchiver",
+                            // Absolutley no idea why I cannot use an absolute path here...
+                            testResults: "${relative_result_path}",
+                        ])
                     }
                 }
             }

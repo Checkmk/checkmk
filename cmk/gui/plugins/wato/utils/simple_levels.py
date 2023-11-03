ddeb7cc71253f4e7ebeaa17eb562826821532639
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-11-03 12:03:41 +0100

    SimpleLevels: allow for lower levels
    Change-Id: I85543fba83beff6b7a0b7e878585b2e3c52f8b3d

--- cmk/gui/plugins/wato/utils/simple_levels.py
+++ cmk/gui/plugins/wato/utils/simple_levels.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2022 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+from typing import Literal
+
 from cmk.gui.i18n import _
 from cmk.gui.valuespec import Age, Alternative, FixedValue, Float, Integer, Percentage, Tuple
 
@@ -21,6 +23,7 @@ def _FixedLevels(
     value_spec: _Spec,
     default_value: tuple[float | int, float | int],
     unit: str,
+    direction: Literal["upper", "lower"],
 ) -> Tuple:
     type_ = int if value_spec is Integer else float
     kw = {} if value_spec is Age else {"unit": unit}
@@ -28,12 +31,12 @@ def _FixedLevels(
         title=_("Fixed Levels"),
         elements=[
             value_spec(
-                title=_("Warning at"),
+                title=_("Warning at") if direction == "upper" else _("Warning below"),
                 default_value=type_(default_value[0]),
                 **kw,  # type: ignore[arg-type]
             ),
             value_spec(
-                title=_("Critical at"),
+                title=_("Critical at") if direction == "upper" else _("Critical below"),
                 default_value=type_(default_value[1]),
                 **kw,  # type: ignore[arg-type]
             ),
@@ -48,6 +51,7 @@ def SimpleLevels(
     default_value: tuple[float, float] | None = None,
     title: str | None = None,
     unit: str | None = None,
+    direction: Literal["upper", "lower"] = "upper",
 ) -> Alternative:
     """
     Internal API. Might change between versions
@@ -60,7 +64,7 @@ def SimpleLevels(
         help=help,
         elements=[
             _NoLevels(),
-            _FixedLevels(spec, default_value=default_levels, unit=unit or ""),
+            _FixedLevels(spec, default_value=default_levels, unit=unit or "", direction=direction),
         ],
         match=lambda v: 0 if v is None else 1,
         default_value=default_value,

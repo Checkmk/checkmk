bb2612bf05b577c92722c8fc2f1085d48d0a7f78
Gav <gavin.mcguigan@checkmk.com>
2023-11-03 07:33:59 +0100

    16159 linux-bonding: introduce check for expected bonding mode
    The service monitoring rule "Linux monitoring service status"
    was previously being used for both Linux and OVS.  This werk
    now splits this rule in two.  We now have the following rules.
    
    Bonding interface status
    OVS bonding interface status
    
    The reason behind this, is that we have now introduced an
    expected mode configuration parameter only for the Linux
    rule. So you are now able to select the expected bonding
    mode and the state you want to display when this condition
    is not met.
    
    SUP-14636
    
    Change-Id: Ia85528d34e7c736cae02991ee6f1fc2bfcbffefe

--- cmk/gui/plugins/wato/check_parameters/bonding.py
+++ cmk/gui/plugins/wato/check_parameters/bonding.py
@@ -3,7 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from typing import Literal
 
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
@@ -14,8 +13,8 @@ from cmk.gui.plugins.wato.utils import (
 from cmk.gui.valuespec import Dictionary, DropdownChoice, MonitoringState, TextInput, Tuple
 
 
-def get_elements_for(which: Literal["bonding", "lnx_bonding"]) -> list:
-    elements = [
+def get_common_elements() -> list:
+    return [
         (
             "expect_active",
             DropdownChoice(
@@ -37,64 +36,49 @@ def get_elements_for(which: Literal["bonding", "lnx_bonding"]) -> list:
         ),
     ]
 
-    if which == "bonding":
-        return elements
-
-    elements.append(
-        (
-            "expected_bonding_mode_and_state",
-            Tuple(
-                title=_("Expected bonding mode"),
-                elements=[
-                    DropdownChoice(
-                        title=_("Mode"),
-                        choices=[
-                            ("balance-rr", _("balance-rr")),
-                            ("active-backup", _("active-backup")),
-                            ("balance-xor", _("balance-xor")),
-                            ("broadcast", _("broadcast")),
-                            ("802.3ad", _("802.3ad")),
-                            ("balance-tlb", _("balance-tlb")),
-                            ("balance-alb", _("balance-alb")),
-                        ],
-                        default_value="balance-rr",
-                    ),
-                    MonitoringState(
-                        title=_("State if not as expected"),
-                        default_value=2,
-                    ),
-                ],
-                help=_("Specify the monitoring state when the bonding mode is not as expected."),
-            ),
-        )
-    )
-    return elements
-
 
 def _parameter_valuespec_lnx_bonding():
     return Dictionary(
-        elements=get_elements_for("lnx_bonding"),
+        elements=[
+            *get_common_elements(),
+            (
+                "expected_bonding_mode_and_state",
+                Tuple(
+                    title=_("Expected bonding mode"),
+                    elements=[
+                        DropdownChoice(
+                            title=_("Mode"),
+                            choices=[
+                                ("balance-rr", _("balance-rr")),
+                                ("active-backup", _("active-backup")),
+                                ("balance-xor", _("balance-xor")),
+                                ("broadcast", _("broadcast")),
+                                ("802.3ad", _("802.3ad")),
+                                ("balance-tlb", _("balance-tlb")),
+                                ("balance-alb", _("balance-alb")),
+                            ],
+                            default_value="balance-rr",
+                        ),
+                        MonitoringState(
+                            title=_("State if not as expected"),
+                            default_value=2,
+                        ),
+                    ],
+                    help=_(
+                        "Specify the monitoring state when the bonding mode is not as expected."
+                    ),
+                ),
+            ),
+        ],
         ignored_keys=["primary"],
     )
 
 
-def _parameter_valuespec_bonding():
+def _parameter_valuespec_ovs_bonding():
     return Dictionary(
-        elements=get_elements_for("bonding"),
+        elements=get_common_elements(),
         ignored_keys=["primary"],
     )
-
-
-rulespec_registry.register(
-    CheckParameterRulespecWithItem(
-        check_group_name="lnx_bonding",
-        group=RulespecGroupCheckParametersNetworking,
-        item_spec=lambda: TextInput(title=_("Name of the bonding interface")),
-        match_type="dict",
-        parameter_valuespec=_parameter_valuespec_lnx_bonding,
-        title=lambda: _("Linux bonding interface status"),
-    )
-)
 
 
 rulespec_registry.register(
@@ -103,7 +87,18 @@ rulespec_registry.register(
         group=RulespecGroupCheckParametersNetworking,
         item_spec=lambda: TextInput(title=_("Name of the bonding interface")),
         match_type="dict",
-        parameter_valuespec=_parameter_valuespec_bonding,
-        title=lambda: _("Bonding interface status"),
+        parameter_valuespec=_parameter_valuespec_lnx_bonding,
+        title=lambda: _("Linux bonding interface status"),
+    )
+)
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="ovs_bonding",
+        group=RulespecGroupCheckParametersNetworking,
+        item_spec=lambda: TextInput(title=_("Name of the bonding interface")),
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_ovs_bonding,
+        title=lambda: _("OVS bonding interface status"),
     )
 )

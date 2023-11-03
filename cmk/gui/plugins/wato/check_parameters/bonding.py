a81d9fd49fd19ec471decdb78bb186807a8c530b
Gav <gavin.mcguigan@checkmk.com>
2023-11-03 07:33:59 +0100

    16159 linux-bonding: introduce check for expected bonding mode
    The service monitoring rule "Linux monitoring service status"
    was previously being used for both Linux and OVS.  This werk
    now splits this rule in two.  We now have the following rules.
    
    Linux bonding interface status -> Linux
    Bonding interface status -> OVS
    
    The reason behind this, is that we have now introduced an
    expected mode configuration parameter only for the Linux
    rule. So you are now able to select the expected bonding
    mode and the state you want to display when this condition
    is not met.
    
    SUP-14636
    
    Change-Id: Iadb06a9fc1ec1dae7cb4c87268ca00abd1e72b23

--- cmk/gui/plugins/wato/check_parameters/bonding.py
+++ cmk/gui/plugins/wato/check_parameters/bonding.py
@@ -3,40 +3,98 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Literal
+
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
     rulespec_registry,
     RulespecGroupCheckParametersNetworking,
 )
-from cmk.gui.valuespec import Dictionary, DropdownChoice, MonitoringState, TextInput
+from cmk.gui.valuespec import Dictionary, DropdownChoice, MonitoringState, TextInput, Tuple
+
+
+def get_elements_for(which: Literal["bonding", "lnx_bonding"]) -> list:
+    elements = [
+        (
+            "expect_active",
+            DropdownChoice(
+                title=_("Warn on unexpected active interface"),
+                choices=[
+                    ("ignore", _("ignore which one is active")),
+                    ("primary", _("require primary interface to be active")),
+                    ("lowest", _("require interface that sorts lowest alphabetically")),
+                ],
+                default_value="ignore",
+            ),
+        ),
+        (
+            "ieee_302_3ad_agg_id_missmatch_state",
+            MonitoringState(
+                title=_("State for mismatching Aggregator IDs for LACP"),
+                default_value=1,
+            ),
+        ),
+    ]
+
+    if which == "bonding":
+        return elements
+
+    elements.append(
+        (
+            "expected_bonding_mode_and_state",
+            Tuple(
+                title=_("Expected bonding mode"),
+                elements=[
+                    DropdownChoice(
+                        title=_("Mode"),
+                        choices=[
+                            ("balance-rr", _("balance-rr")),
+                            ("active-backup", _("active-backup")),
+                            ("balance-xor", _("balance-xor")),
+                            ("broadcast", _("broadcast")),
+                            ("802.3ad", _("802.3ad")),
+                            ("balance-tlb", _("balance-tlb")),
+                            ("balance-alb", _("balance-alb")),
+                        ],
+                        default_value="balance-rr",
+                    ),
+                    MonitoringState(
+                        title=_("State if not as expected"),
+                        default_value=2,
+                    ),
+                ],
+                help=_("Specify the monitoring state when the bonding mode is not as expected."),
+            ),
+        )
+    )
+    return elements
+
+
+def _parameter_valuespec_lnx_bonding():
+    return Dictionary(
+        elements=get_elements_for("lnx_bonding"),
+        ignored_keys=["primary"],
+    )
 
 
 def _parameter_valuespec_bonding():
     return Dictionary(
-        elements=[
-            (
-                "expect_active",
-                DropdownChoice(
-                    title=_("Warn on unexpected active interface"),
-                    choices=[
-                        ("ignore", _("ignore which one is active")),
-                        ("primary", _("require primary interface to be active")),
-                        ("lowest", _("require interface that sorts lowest alphabetically")),
-                    ],
-                    default_value="ignore",
-                ),
-            ),
-            (
-                "ieee_302_3ad_agg_id_missmatch_state",
-                MonitoringState(
-                    title=_("State for mismatching Aggregator IDs for LACP"),
-                    default_value=1,
-                ),
-            ),
-        ],
+        elements=get_elements_for("bonding"),
         ignored_keys=["primary"],
     )
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="lnx_bonding",
+        group=RulespecGroupCheckParametersNetworking,
+        item_spec=lambda: TextInput(title=_("Name of the bonding interface")),
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_lnx_bonding,
+        title=lambda: _("Linux bonding interface status"),
+    )
+)
 
 
 rulespec_registry.register(
@@ -46,6 +104,6 @@ rulespec_registry.register(
         item_spec=lambda: TextInput(title=_("Name of the bonding interface")),
         match_type="dict",
         parameter_valuespec=_parameter_valuespec_bonding,
-        title=lambda: _("Linux bonding interface status"),
+        title=lambda: _("Bonding interface status"),
     )
 )

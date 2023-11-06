c042187b95558bb98278be0e9cc44a490aa425c9
Mathias Laurin <mathias.laurin@checkmk.com>
2023-11-06 15:34:19 +0100

    Move check functions to checking namespace
    CMK-14257
    
    Change-Id: I84709280637e35849d2a8adc90ac6a667aff2096

--- packages/check-http/src/checking.rs
+++ packages/check-http/src/checking.rs
@@ -2,7 +2,15 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
+use crate::cli::{DocumentAgeLevels, PageSizeLimits, ResponseTimeLevels};
+use bytes::Bytes;
+use http::HeaderMap;
+use httpdate::parse_http_date;
+use reqwest::{Error as ReqwestError, StatusCode, Version};
 use std::fmt::{Display, Formatter, Result as FormatResult};
+use std::time::{Duration, SystemTime};
+
+use crate::cli::OnRedirect;
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub enum State {
@@ -37,4 +45,155 @@ impl From<State> for i32 {
 pub struct CheckResult {
     pub state: State,
     pub summary: String,
+}
+
+pub fn check_status(
+    status: StatusCode,
+    version: Version,
+    onredirect: OnRedirect,
+) -> Option<CheckResult> {
+    let state = if status.is_client_error() {
+        State::Warn
+    } else if status.is_server_error() {
+        State::Crit
+    } else if status.is_redirection() {
+        match onredirect {
+            OnRedirect::Warning => State::Warn,
+            OnRedirect::Critical => State::Crit,
+            _ => State::Ok, // If we reach this point, the redirect is ok
+        }
+    } else {
+        State::Ok
+    };
+
+    Some(CheckResult {
+        state,
+        summary: format!("{:?} {}", version, status),
+    })
+}
+
+pub fn check_body(
+    body: Option<Result<Bytes, ReqwestError>>,
+    page_size_limits: Option<PageSizeLimits>,
+) -> Option<CheckResult> {
+    let body = body?;
+
+    match body {
+        Ok(bd) => Some(check_page_size(bd.len(), page_size_limits)),
+        Err(_) => Some(CheckResult {
+            state: State::Crit,
+            summary: "Error fetching the reponse body".to_string(),
+        }),
+    }
+}
+
+pub fn check_page_size(page_size: usize, page_size_limits: Option<PageSizeLimits>) -> CheckResult {
+    let state = match page_size_limits {
+        Some((lower, _)) if page_size < lower => State::Warn,
+        Some((_, Some(upper))) if page_size > upper => State::Warn,
+        _ => State::Ok,
+    };
+
+    CheckResult {
+        state,
+        summary: format!("Page size: {} bytes", page_size),
+    }
+}
+
+pub fn check_response_time(
+    response_time: Duration,
+    response_time_levels: Option<ResponseTimeLevels>,
+) -> Option<CheckResult> {
+    let state = match response_time_levels {
+        Some((_, Some(crit))) if response_time.as_secs_f64() >= crit => State::Crit,
+        Some((warn, _)) if response_time.as_secs_f64() >= warn => State::Warn,
+        _ => State::Ok,
+    };
+
+    Some(CheckResult {
+        state,
+        summary: format!(
+            "Response time: {}.{}s",
+            response_time.as_secs(),
+            response_time.subsec_millis()
+        ),
+    })
+}
+
+pub fn check_document_age(
+    headers: &HeaderMap,
+    document_age_levels: Option<DocumentAgeLevels>,
+) -> Option<CheckResult> {
+    let document_age_levels = document_age_levels?;
+
+    let now = SystemTime::now();
+
+    let age_header = headers.get("last-modified").or(headers.get("date"));
+    let Some(document_age) = age_header else {
+        return Some(CheckResult {
+            state: State::Crit,
+            summary: "Can't determine document age".to_string(),
+        });
+    };
+    let Ok(Ok(age)) = document_age.to_str().map(parse_http_date) else {
+        return Some(CheckResult {
+            state: State::Crit,
+            summary: "Can't decode document age".to_string(),
+        });
+    };
+
+    //TODO(au): Specify "too old" in Output
+    match document_age_levels {
+        (_, Some(crit)) if now - Duration::from_secs(crit) > age => Some(CheckResult {
+            state: State::Crit,
+            summary: "Document age too old".to_string(),
+        }),
+        (warn, _) if now - Duration::from_secs(warn) > age => Some(CheckResult {
+            state: State::Warn,
+            summary: "Document age too old".to_string(),
+        }),
+        _ => None,
+    }
+}
+
+#[cfg(test)]
+mod test_check_functions {
+    use crate::checking::check_page_size;
+    use crate::checking::State;
+
+    #[test]
+    fn test_check_page_size_without_limits() {
+        assert_eq!(check_page_size(42, None).state, State::Ok);
+    }
+
+    #[test]
+    fn test_check_page_size_with_lower_within_bounds() {
+        assert_eq!(check_page_size(42, Some((12, None))).state, State::Ok);
+    }
+
+    #[test]
+    fn test_check_page_size_with_lower_out_of_bounds() {
+        assert_eq!(check_page_size(42, Some((56, None))).state, State::Warn);
+    }
+
+    #[test]
+    fn test_check_page_size_with_lower_and_higher_within_bounds() {
+        assert_eq!(check_page_size(56, Some((42, Some(100)))).state, State::Ok);
+    }
+
+    #[test]
+    fn test_check_page_size_with_lower_and_higher_too_low() {
+        assert_eq!(
+            check_page_size(42, Some((56, Some(100)))).state,
+            State::Warn
+        );
+    }
+
+    #[test]
+    fn test_check_page_size_with_lower_and_higher_too_high() {
+        assert_eq!(
+            check_page_size(142, Some((56, Some(100)))).state,
+            State::Warn
+        );
+    }
 }
--- packages/check-http/src/lib.rs
+++ packages/check-http/src/lib.rs
@@ -2,11 +2,10 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
+use crate::cli::{Cli, ForceIP};
 use anyhow::Result as AnyhowResult;
 use bytes::Bytes;
-use cli::{Cli, DocumentAgeLevels, ForceIP, PageSizeLimits, ResponseTimeLevels};
 use http::{HeaderMap, HeaderName, HeaderValue};
-use httpdate::parse_http_date;
 use reqwest::{
     header::USER_AGENT,
     redirect::{Action, Attempt, Policy},
@@ -14,7 +13,7 @@ use reqwest::{
 };
 use std::{
     net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
-    time::{Duration, Instant, SystemTime},
+    time::{Duration, Instant},
 };
 
 use crate::checking::{CheckResult, State};
@@ -81,10 +80,10 @@ pub async fn collect_checks(args: Cli) -> Vec<CheckResult> {
     };
 
     vec![
-        check_status(response.status, response.version, args.onredirect),
-        check_body(response.body, args.page_size),
-        check_response_time(response.elapsed, args.response_time_levels),
-        check_document_age(&response.headers, args.document_age_levels),
+        checking::check_status(response.status, response.version, args.onredirect),
+        checking::check_body(response.body, args.page_size),
+        checking::check_response_time(response.elapsed, args.response_time_levels),
+        checking::check_document_age(&response.headers, args.document_age_levels),
     ]
     .into_iter()
     .flatten()
@@ -238,115 +237,6 @@ async fn perform_request(
     })
 }
 
-fn check_status(
-    status: StatusCode,
-    version: Version,
-    onredirect: OnRedirect,
-) -> Option<CheckResult> {
-    let state = if status.is_client_error() {
-        State::Warn
-    } else if status.is_server_error() {
-        State::Crit
-    } else if status.is_redirection() {
-        match onredirect {
-            OnRedirect::Warning => State::Warn,
-            OnRedirect::Critical => State::Crit,
-            _ => State::Ok, // If we reach this point, the redirect is ok
-        }
-    } else {
-        State::Ok
-    };
-
-    Some(CheckResult {
-        state,
-        summary: format!("{:?} {}", version, status),
-    })
-}
-
-fn check_body(
-    body: Option<Result<Bytes, ReqwestError>>,
-    page_size_limits: Option<PageSizeLimits>,
-) -> Option<CheckResult> {
-    let body = body?;
-
-    match body {
-        Ok(bd) => Some(check_page_size(bd.len(), page_size_limits)),
-        Err(_) => Some(CheckResult {
-            state: State::Crit,
-            summary: "Error fetching the reponse body".to_string(),
-        }),
-    }
-}
-
-fn check_page_size(page_size: usize, page_size_limits: Option<PageSizeLimits>) -> CheckResult {
-    let state = match page_size_limits {
-        Some((lower, _)) if page_size < lower => State::Warn,
-        Some((_, Some(upper))) if page_size > upper => State::Warn,
-        _ => State::Ok,
-    };
-
-    CheckResult {
-        state,
-        summary: format!("Page size: {} bytes", page_size),
-    }
-}
-
-fn check_response_time(
-    response_time: Duration,
-    response_time_levels: Option<ResponseTimeLevels>,
-) -> Option<CheckResult> {
-    let state = match response_time_levels {
-        Some((_, Some(crit))) if response_time.as_secs_f64() >= crit => State::Crit,
-        Some((warn, _)) if response_time.as_secs_f64() >= warn => State::Warn,
-        _ => State::Ok,
-    };
-
-    Some(CheckResult {
-        state,
-        summary: format!(
-            "Response time: {}.{}s",
-            response_time.as_secs(),
-            response_time.subsec_millis()
-        ),
-    })
-}
-
-fn check_document_age(
-    headers: &HeaderMap,
-    document_age_levels: Option<DocumentAgeLevels>,
-) -> Option<CheckResult> {
-    let document_age_levels = document_age_levels?;
-
-    let now = SystemTime::now();
-
-    let age_header = headers.get("last-modified").or(headers.get("date"));
-    let Some(document_age) = age_header else {
-        return Some(CheckResult {
-            state: State::Crit,
-            summary: "Can't determine document age".to_string(),
-        });
-    };
-    let Ok(Ok(age)) = document_age.to_str().map(parse_http_date) else {
-        return Some(CheckResult {
-            state: State::Crit,
-            summary: "Can't decode document age".to_string(),
-        });
-    };
-
-    //TODO(au): Specify "too old" in Output
-    match document_age_levels {
-        (_, Some(crit)) if now - Duration::from_secs(crit) > age => Some(CheckResult {
-            state: State::Crit,
-            summary: "Document age too old".to_string(),
-        }),
-        (warn, _) if now - Duration::from_secs(warn) > age => Some(CheckResult {
-            state: State::Warn,
-            summary: "Document age too old".to_string(),
-        }),
-        _ => None,
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::net::SocketAddr;
@@ -413,47 +303,5 @@ mod tests {
             filter_socket_addrs(addrs.clone(), Some(ForceIP::Ipv4))
         );
         assert_eq!(ipv6_addrs, filter_socket_addrs(addrs, Some(ForceIP::Ipv6)));
-    }
-}
-
-#[cfg(test)]
-mod test_check_functions {
-    use crate::check_page_size;
-    use crate::checking::State;
-
-    #[test]
-    fn test_check_page_size_without_limits() {
-        assert_eq!(check_page_size(42, None).state, State::Ok);
-    }
-
-    #[test]
-    fn test_check_page_size_with_lower_within_bounds() {
-        assert_eq!(check_page_size(42, Some((12, None))).state, State::Ok);
-    }
-
-    #[test]
-    fn test_check_page_size_with_lower_out_of_bounds() {
-        assert_eq!(check_page_size(42, Some((56, None))).state, State::Warn);
-    }
-
-    #[test]
-    fn test_check_page_size_with_lower_and_higher_within_bounds() {
-        assert_eq!(check_page_size(56, Some((42, Some(100)))).state, State::Ok);
-    }
-
-    #[test]
-    fn test_check_page_size_with_lower_and_higher_too_low() {
-        assert_eq!(
-            check_page_size(42, Some((56, Some(100)))).state,
-            State::Warn
-        );
-    }
-
-    #[test]
-    fn test_check_page_size_with_lower_and_higher_too_high() {
-        assert_eq!(
-            check_page_size(142, Some((56, Some(100)))).state,
-            State::Warn
-        );
     }
 }

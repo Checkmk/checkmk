186e836b01f22b5583016c55aa43362a0460b1a6
Joerg Herbel <joerg.herbel@checkmk.com>
2023-11-07 07:23:55 +0100

    Implement `robotmk_config` section
    CMK-15069
    
    Change-Id: I61114812eeece09875fcbb2a82cbe3308351e7ef

--- cmk/base/plugins/agent_based/robotmk_scheduler_status.py
+++ cmk/base/plugins/agent_based/robotmk_scheduler_status.py
@@ -3,27 +3,113 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.base.plugins.agent_based.agent_based_api.v1 import register, Result, Service, State
-from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import CheckResult, DiscoveryResult
-from cmk.base.plugins.agent_based.utils import robotmk_api
+from collections.abc import Mapping, Sequence
+from enum import Enum
+from pathlib import Path
+from typing import Literal
+
+from pydantic import BaseModel, Field, TypeAdapter
+
+from .agent_based_api.v1 import register, Result, Service, State
+from .agent_based_api.v1.type_defs import CheckResult, DiscoveryResult, StringTable
 
 
-def discover_scheduler_status(section: robotmk_api.Section) -> DiscoveryResult:
-    if section.config or section.config_reading_error:
-        yield Service()
+class ConfigFileContent(BaseModel, frozen=True):
+    FileContent: str
 
 
-def check_scheduler_status(section: robotmk_api.Section) -> CheckResult:
-    if not (section.config or section.config_reading_error):
-        return
+class ConfigReadingError(BaseModel, frozen=True):
+    ReadingError: str
 
+
+class RobotFrameworkConfig(BaseModel, frozen=True):
+    robot_target: Path
+    command_line_args: Sequence[str] = Field(default=[])
+
+
+class ExecutionConfig(BaseModel, frozen=True):
+    n_retries_max: int
+    retry_strategy: str
+    execution_interval_seconds: int
+    timeout: int
+
+
+class EnvironmentConfigSystem(Enum):
+    System = "System"
+
+
+class EnvironmentConfigRcc(BaseModel, frozen=True):
+    type: Literal["Rcc"]
+    robot_yaml_path: Path
+    build_timeout: int
+    env_json_path: Path | None
+
+
+class SessionConfigCurrentEnum(Enum):
+    Current = "Current"
+
+
+class SessionConfigCurrent(BaseModel, frozen=True):
+    type: SessionConfigCurrentEnum
+
+
+class UserSessionConfig(BaseModel, frozen=True):
+    user_name: str
+
+
+class SessionConfigSpecificUser(BaseModel, frozen=True):
+    SpecificUser: UserSessionConfig
+
+
+class SuiteConfig(BaseModel, frozen=True):
+    robot_framework_config: RobotFrameworkConfig
+    execution_config: ExecutionConfig
+    environment_config: EnvironmentConfigSystem | EnvironmentConfigRcc
+    session_config: SessionConfigCurrent | SessionConfigSpecificUser
+
+
+class Config(BaseModel, frozen=True):
+    working_directory: str
+    results_directory: str
+    rcc_binary_path: str
+    suites: Mapping[str, SuiteConfig]
+
+
+def parse_robotmk_config(string_table: StringTable) -> Config | ConfigReadingError | None:
+    if not string_table:
+        return None
+
+    match (
+        config_data := TypeAdapter(ConfigFileContent | ConfigReadingError).validate_json(
+            string_table[0][0]
+        )
+    ):
+        case ConfigReadingError():
+            return config_data
+        case ConfigFileContent():
+            return Config.model_validate_json(config_data.FileContent)
+
+    raise ValueError("Invalid configuration data")
+
+
+register.agent_section(
+    name="robotmk_config",
+    parse_function=parse_robotmk_config,
+)
+
+
+def discover_scheduler_status(section: Config | ConfigReadingError) -> DiscoveryResult:
+    yield Service()
+
+
+def check_scheduler_status(section: Config | ConfigReadingError) -> CheckResult:
     # TODO: Determine the conditions for the status
     yield Result(state=State.OK, summary="The Scheduler status is OK")
 
 
 register.check_plugin(
     name="robotmk_scheduler_status",
-    sections=["robotmk_v2"],
+    sections=["robotmk_config"],
     service_name="Robotmk Scheduler Status",
     discovery_function=discover_scheduler_status,
     check_function=check_scheduler_status,
--- cmk/base/plugins/agent_based/utils/robotmk_api.py
+++ cmk/base/plugins/agent_based/utils/robotmk_api.py
@@ -6,11 +6,10 @@
 from collections.abc import Mapping, Sequence
 from dataclasses import dataclass, field
 from enum import Enum
-from pathlib import Path
 
 import xmltodict
 from pydantic import BaseModel, BeforeValidator, Field, RootModel, TypeAdapter
-from typing_extensions import Annotated, Literal
+from typing_extensions import Annotated
 
 from .robotmk_parse_xml import Rebot
 
@@ -117,96 +116,21 @@ class SuiteExecutionReport(BaseModel, frozen=True):
     outcome: ExecutionReport | ExecutionReportAlreadyRunning
 
 
-class ConfigReadingError(BaseModel, frozen=True):
-    config_reading_error: str
-
-
-class RobotFrameworkConfig(BaseModel, frozen=True):
-    robot_target: Path
-    command_line_args: Sequence[str] = Field(default=[])
-
-
-class ExecutionConfig(BaseModel, frozen=True):
-    n_retries_max: int
-    retry_strategy: str
-    execution_interval_seconds: int
-    timeout: int
-
-
-class EnvironmentConfigSystem(Enum):
-    System = "System"
-
-
-class EnvironmentConfigRcc(BaseModel, frozen=True):
-    type: Literal["Rcc"]
-    robot_yaml_path: Path
-    build_timeout: int
-    env_json_path: Path | None
-
-
-class SessionConfigCurrentEnum(Enum):
-    Current = "Current"
-
-
-class SessionConfigCurrent(BaseModel, frozen=True):
-    type: SessionConfigCurrentEnum
-
-
-class UserSessionConfig(BaseModel, frozen=True):
-    user_name: str
-
-
-class SessionConfigSpecificUser(BaseModel, frozen=True):
-    SpecificUser: UserSessionConfig
-
-
-class SuiteConfig(BaseModel, frozen=True):
-    robot_framework_config: RobotFrameworkConfig
-    execution_config: ExecutionConfig
-    environment_config: EnvironmentConfigSystem | EnvironmentConfigRcc
-    session_config: SessionConfigCurrent | SessionConfigSpecificUser
-
-
-class Config(BaseModel, frozen=True):
-    working_directory: str
-    results_directory: str
-    rcc_binary_path: str
-    suites: Mapping[str, SuiteConfig]
-
-
-class ConfigFileContent(BaseModel, frozen=True):
-    config_file_content: str
-
-
 @dataclass(frozen=True, kw_only=True)
 class Section:
-    config: Config | None = None
-    config_reading_error: ConfigReadingError | None = None
     rcc_setup_failures: RCCSetupFailures | None = None
     environment_build_statuses: EnvironmentBuildStatuses | None = None
     suite_execution_reports: Sequence[SuiteExecutionReport] = field(default_factory=list)
 
 
 def parse(string_table: Sequence[Sequence[str]]) -> Section:
-    config: Config | None = None
-    config_reading_error: ConfigReadingError | None = None
     rcc_setup_failures: RCCSetupFailures | None = None
     environment_build_statuses: EnvironmentBuildStatuses | None = None
     suite_execution_reports = []
 
-    type_adapter = TypeAdapter(
-        ConfigReadingError
-        | ConfigFileContent
-        | SuiteExecutionReport
-        | EnvironmentBuildStatuses
-        | RCCSetupFailures
-    )
+    type_adapter = TypeAdapter(SuiteExecutionReport | EnvironmentBuildStatuses | RCCSetupFailures)
     for sub_section in (type_adapter.validate_json(line[0]) for line in string_table):
         match sub_section:
-            case ConfigReadingError():
-                config_reading_error = sub_section
-            case ConfigFileContent():
-                config = Config.model_validate_json(sub_section.config_file_content)
             case RCCSetupFailures():
                 rcc_setup_failures = sub_section
             case EnvironmentBuildStatuses():
@@ -215,8 +139,6 @@ def parse(string_table: Sequence[Sequence[str]]) -> Section:
                 suite_execution_reports.append(sub_section)
 
     return Section(
-        config=config,
-        config_reading_error=config_reading_error,
         rcc_setup_failures=rcc_setup_failures,
         environment_build_statuses=environment_build_statuses,
         suite_execution_reports=suite_execution_reports,

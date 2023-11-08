474260a803dbadce9131ef4d047ed641f4ea2b45
David Gervasoni <david.gervasoni@checkmk.com>
2023-11-08 11:59:35 +0100

    Create section cisco_ucs_mem
    CMK-15085
    
    Change-Id: I560e45630ad5034d98cd198e8cdad0b6d0f834b3

--- cmk/base/legacy_checks/cisco_ucs_mem.py
+++ cmk/base/legacy_checks/cisco_ucs_mem.py
@@ -3,79 +3,37 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Iterator, Mapping
+from typing import Any
 
 from cmk.base.check_api import LegacyCheckDefinition
-from cmk.base.check_legacy_includes.cisco_ucs import DETECT, map_operability, map_presence
 from cmk.base.config import check_info
-from cmk.base.plugins.agent_based.agent_based_api.v1 import SNMPTree
-
-# comNET GmbH, Fabian Binder - 2018-05-08
-
-# .1.3.6.1.4.1.9.9.719.1.30.11.1.3  cucsMemoryUnitRn
-# .1.3.6.1.4.1.9.9.719.1.30.11.1.19 cucsMemoryUnitSerial
-# .1.3.6.1.4.1.9.9.719.1.30.11.1.23 cucsMemoryUnitType
-# .1.3.6.1.4.1.9.9.719.1.30.11.1.6  cucsMemoryUnitCapacity
-# .1.3.6.1.4.1.9.9.719.1.30.11.1.14 cucsMemoryUnitOperability
-# .1.3.6.1.4.1.9.9.719.1.30.11.1.17 cucsMemoryUnitPresence
-
-map_memtype = {
-    "0": (0, "undiscovered"),
-    "1": (0, "other"),
-    "2": (0, "unknown"),
-    "3": (0, "dram"),
-    "4": (0, "edram"),
-    "5": (0, "vram"),
-    "6": (0, "sram"),
-    "7": (0, "ram"),
-    "8": (0, "rom"),
-    "9": (0, "flash"),
-    "10": (0, "eeprom"),
-    "11": (0, "feprom"),
-    "12": (0, "eprom"),
-    "13": (0, "cdram"),
-    "14": (0, "n3DRAM"),
-    "15": (0, "sdram"),
-    "16": (0, "sgram"),
-    "17": (0, "rdram"),
-    "18": (0, "ddr"),
-    "19": (0, "ddr2"),
-    "20": (0, "ddr2FbDimm"),
-    "24": (0, "ddr3"),
-    "25": (0, "fbd2"),
-    "26": (0, "ddr4"),
-}
+from cmk.base.plugins.agent_based.agent_based_api.v1 import State
+from cmk.base.plugins.agent_based.cisco_ucs_mem import MemoryModule
+from cmk.base.plugins.agent_based.utils.cisco_ucs import Presence
 
 
-def inventory_cisco_ucs_mem(info):
-    for name, _serial, _memtype, _capacity, _status, presence in info:
-        if presence != "11":  # do not discover missing units
-            yield name, None
+def inventory_cisco_ucs_mem(section: Mapping[str, MemoryModule]) -> Iterator[Any]:
+    yield from (
+        (name, None)
+        for name, memory_module in section.items()
+        if memory_module.presence is not Presence.missing
+    )
 
 
-def check_cisco_ucs_mem(item, _no_params, info):
-    for name, serial, memtype, capacity, status, presence in info:
-        if name == item:
-            state, state_readable = map_operability.get(
-                status, (3, "Unknown, status code %s" % status)
-            )
-            presence_state, presence_readable = map_presence.get(
-                presence, (3, "Unknown, status code %s" % presence)
-            )
-            memtype_state, memtype_readable = map_memtype.get(
-                memtype, (3, "Unknown memory type %s" % memtype)
-            )
-            yield state, "Status: %s" % state_readable
-            yield presence_state, "Presence: %s" % presence_readable
-            yield memtype_state, "Type: %s" % memtype_readable
-            yield 0, f"Size: {capacity} MB, SN: {serial}"
+def check_cisco_ucs_mem(
+    item: str, _no_params: object, section: Mapping[str, MemoryModule]
+) -> Iterator[Any]:
+    if not (memory_module := section.get(item)):
+        return
+
+    yield memory_module.operability.monitoring_state().value, f"Status: {memory_module.operability.name}"
+    yield memory_module.presence.monitoring_state().value, f"Presence: {memory_module.presence.name}"
+    yield State.OK.value, f"Type: {memory_module.memtype.name}"
+    yield State.OK.value, f"Size: {memory_module.capacity} MB, SN: {memory_module.serial}"
 
 
 check_info["cisco_ucs_mem"] = LegacyCheckDefinition(
-    detect=DETECT,
-    fetch=SNMPTree(
-        base=".1.3.6.1.4.1.9.9.719.1.30.11.1",
-        oids=["3", "19", "23", "6", "14", "17"],
-    ),
     service_name="Memory %s",
     discovery_function=inventory_cisco_ucs_mem,
     check_function=check_cisco_ucs_mem,
--- tests/unit/cmk/base/plugins/agent_based/test_cisco_ucs_mem.py
+++ tests/unit/cmk/base/plugins/agent_based/test_cisco_ucs_mem.py
@@ -3,42 +3,48 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 
 import pytest
 
 from cmk.base.legacy_checks.cisco_ucs_mem import check_cisco_ucs_mem, inventory_cisco_ucs_mem
-
-STRING_TABLE = [
-    ["mem-1", "0357CDB9", "26", "32768", "1", "10"],
-    ["mem-2", "0357CF3B", "26", "32768", "1", "10"],
-    ["mem-3", "418686A5", "26", "32768", "1", "10"],
-    ["mem-4", "41867F58", "26", "32768", "1", "10"],
-    ["mem-5", "41869BEB", "26", "32768", "1", "10"],
-    ["mem-6", "NA", "0", "0", "0", "11"],
-    ["mem-7", "0357CE24", "26", "32768", "1", "10"],
-    ["mem-8", "0357CCDF", "26", "32768", "1", "10"],
-    ["mem-9", "0357CD64", "26", "32768", "1", "10"],
-    ["mem-10", "0357CE2E", "26", "32768", "1", "10"],
-    ["mem-11", "418D4B03", "26", "32768", "1", "10"],
-    ["mem-12", "NA", "0", "0", "0", "11"],
-    ["mem-13", "NA", "0", "0", "0", "11"],
-    ["mem-14", "NA", "0", "0", "0", "11"],
-    ["mem-15", "0357CDF9", "26", "32768", "1", "10"],
-    ["mem-16", "0357CE15", "26", "32768", "1", "10"],
-    ["mem-17", "41867C12", "26", "32768", "1", "10"],
-    ["mem-18", "NA", "0", "0", "0", "11"],
-    ["mem-19", "03584CA9", "26", "32768", "1", "10"],
-    ["mem-20", "418CCC3E", "26", "32768", "1", "10"],
-    ["mem-21", "418686E5", "26", "32768", "1", "10"],
-    ["mem-22", "419B0A7A", "26", "32768", "1", "10"],
-    ["mem-23", "41868484", "26", "32768", "1", "10"],
-    ["mem-24", "NA", "0", "0", "0", "11"],
-]
+from cmk.base.plugins.agent_based.cisco_ucs_mem import MemoryModule, parse_cisco_ucs_mem
 
 
-def test_inventory_cisco_ucs_mem() -> None:
-    assert list(inventory_cisco_ucs_mem(STRING_TABLE)) == [
+@pytest.fixture(name="section", scope="module")
+def fixture_section() -> dict[str, MemoryModule]:
+    return parse_cisco_ucs_mem(
+        [
+            ["mem-1", "0357CDB9", "26", "32768", "1", "10"],
+            ["mem-2", "0357CF3B", "26", "32768", "1", "10"],
+            ["mem-3", "418686A5", "26", "32768", "1", "10"],
+            ["mem-4", "41867F58", "26", "32768", "1", "10"],
+            ["mem-5", "41869BEB", "26", "32768", "1", "10"],
+            ["mem-6", "NA", "0", "0", "0", "11"],
+            ["mem-7", "0357CE24", "26", "32768", "1", "10"],
+            ["mem-8", "0357CCDF", "26", "32768", "1", "10"],
+            ["mem-9", "0357CD64", "26", "32768", "1", "10"],
+            ["mem-10", "0357CE2E", "26", "32768", "1", "10"],
+            ["mem-11", "418D4B03", "26", "32768", "1", "10"],
+            ["mem-12", "NA", "0", "0", "0", "11"],
+            ["mem-13", "NA", "0", "0", "0", "11"],
+            ["mem-14", "NA", "0", "0", "0", "11"],
+            ["mem-15", "0357CDF9", "26", "32768", "1", "10"],
+            ["mem-16", "0357CE15", "26", "32768", "1", "10"],
+            ["mem-17", "41867C12", "26", "32768", "1", "10"],
+            ["mem-18", "NA", "0", "0", "0", "11"],
+            ["mem-19", "03584CA9", "26", "32768", "1", "10"],
+            ["mem-20", "418CCC3E", "26", "32768", "1", "10"],
+            ["mem-21", "418686E5", "26", "32768", "1", "10"],
+            ["mem-22", "419B0A7A", "26", "32768", "1", "10"],
+            ["mem-23", "41868484", "26", "32768", "1", "10"],
+            ["mem-24", "NA", "0", "0", "0", "11"],
+        ]
+    )
+
+
+def test_inventory_cisco_ucs_mem(section: Mapping[str, MemoryModule]) -> None:
+    assert list(inventory_cisco_ucs_mem(section)) == [
         ("mem-1", None),
         ("mem-2", None),
         ("mem-3", None),
@@ -76,5 +82,7 @@ def test_inventory_cisco_ucs_mem() -> None:
         ),
     ],
 )
-def test_check_cisco_ucs_mem(item: str, expected_output: Sequence[object]) -> None:
-    assert list(check_cisco_ucs_mem(item, None, STRING_TABLE)) == expected_output
+def test_check_cisco_ucs_mem(
+    section: Mapping[str, MemoryModule], item: str, expected_output: Sequence[object]
+) -> None:
+    assert list(check_cisco_ucs_mem(item, None, section)) == expected_output

d7c5863455f775ad4568e02ea2d71e274daff632
Sofia Colakovic <sofia.colakovic@checkmk.com>
2023-11-10 14:42:08 +0100

    server-side calls: add tests
    Change-Id: I4aac91bdb39417d9530b7d78bdcb6e11b6ccb784

---
+++ packages/cmk-server-side-calls/tests/v1/test_special_agents.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections.abc import Iterator, Mapping, Sequence
+from typing import Literal
+
+from pydantic import BaseModel
+
+from cmk.server_side_calls.v1 import (
+    get_secret_from_params,
+    HostConfig,
+    HTTPProxy,
+    IPAddressFamily,
+    PlainTextSecret,
+    Secret,
+    SpecialAgentCommand,
+    SpecialAgentConfig,
+)
+
+
+class ExampleParams(BaseModel):
+    protocol: str
+    user: str
+    password: tuple[Literal["store", "password"], str]
+
+
+def parse_example_params(params: Mapping[str, object]) -> ExampleParams:
+    return ExampleParams.model_validate(params)
+
+
+def generate_example_commands(
+    params: ExampleParams,
+    _host_config: HostConfig,
+    _http_proxies: Mapping[str, HTTPProxy],
+) -> Iterator[SpecialAgentCommand]:
+    args: Sequence[str | Secret] = [
+        "-p",
+        params.protocol,
+        "-u",
+        params.user,
+        "-s",
+        get_secret_from_params(params.password[0], params.password[1]),
+    ]
+    yield SpecialAgentCommand(command_arguments=args)
+
+
+special_agent_example = SpecialAgentConfig(
+    name="example",
+    parameter_parser=parse_example_params,
+    commands_function=generate_example_commands,
+)
+
+
+def test_active_check_config() -> None:
+    host_config = HostConfig(
+        name="hostname",
+        address="0.0.0.1",
+        alias="host_alias",
+        ip_family=IPAddressFamily.IPV4,
+        ipv4address="0.0.0.1",
+    )
+    params = {
+        "protocol": "HTTP",
+        "user": "example_user",
+        "password": ("password", "password1234"),
+    }
+
+    parsed_params = special_agent_example.parameter_parser(params)
+    commands = list(special_agent_example.commands_function(parsed_params, host_config, {}))
+
+    assert len(commands) == 1
+    assert commands[0] == SpecialAgentCommand(
+        command_arguments=[
+            "-p",
+            "HTTP",
+            "-u",
+            "example_user",
+            "-s",
+            PlainTextSecret(value="password1234", format="%s"),
+        ],
+    )

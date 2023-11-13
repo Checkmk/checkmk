4bb9f7199af9dbaf9faf618da194cac82843a054
Konstantin Baikov <konstantin.baikov@checkmk.com>
2023-11-13 16:45:04 +0100

    Remove redundant code and reformat
    Change-Id: I97812a9e21cc579a03095809cefde35abb2704da

--- cmk/ec/main.py
+++ cmk/ec/main.py
@@ -971,7 +971,6 @@ class EventServer(ECServerThread):
         now = time.time()
         for rule in self._rules:
             if "expect" in rule:
-
                 if not self._rule_matcher.event_rule_matches_site(rule, event={}):
                     continue
 
@@ -3224,7 +3223,6 @@ def replication_pull(  # pylint: disable=too-many-branches
 
     if need_sync:
         with event_status.lock, lock_configuration:
-
             try:
                 new_state = get_state_from_master(config, slave_status)
                 replication_update_state(settings, config, event_status, event_server, new_state)
@@ -3649,5 +3647,4 @@ def main() -> None:  # pylint: disable=too-many-branches
 
 
 if __name__ == "__main__":
-    main()
     main()

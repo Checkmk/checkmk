a85cb1acb54dcc0c82813d6d0b225f8c14af8f02
Simon Jess <simon.jess@checkmk.com>
2023-11-15 11:43:59 +0100

    Fix some units; add test for duplicate metric infos
    Change-Id: I9e1a946d67b83c0c1ecf7b01c94ee10b8285d2be

--- tests/unit/cmk/gui/graphing/test_metric_info.py
+++ tests/unit/cmk/gui/graphing/test_metric_info.py
@@ -18,11 +18,9 @@ def test_metric_info_unit() -> None:
     assert metric_info
     unit_info_keys = list(unit_info.keys())
     assert unit_info_keys
-    assert not [
-        name
-        for name, info in metric_info.items()
-        if name not in _METRICS_WITH_UNKNOWN_UNIT and info["unit"] not in unit_info_keys
-    ]
+    assert [
+        name for name, info in metric_info.items() if info["unit"] not in unit_info_keys
+    ] == _METRICS_WITH_UNKNOWN_UNIT
 
 
 def _is_valid_color(color: str) -> bool:
@@ -66,3 +64,24 @@ def _is_valid_color(color: str) -> bool:
 def test_metric_info_color() -> None:
     assert metric_info
     assert not [name for name, info in metric_info.items() if not _is_valid_color(info["color"])]
+
+
+_DUPLICATE_METRIC_INFOS = [
+    ["db_read_latency", "read_latency"],
+    ["db_write_latency", "write_latency"],
+    ["aws_dynamodb_consumed_rcu_perc", "aws_dynamodb_consumed_wcu_perc"],
+    ["kube_memory_request_utilization", "kube_cpu_request_utilization"],
+    ["kube_memory_limit_utilization", "kube_cpu_limit_utilization"],
+    ["kube_memory_cluster_allocatable_utilization", "kube_cpu_cluster_allocatable_utilization"],
+    ["kube_memory_node_allocatable_utilization", "kube_cpu_node_allocatable_utilization"],
+]
+
+
+def test_metric_info_duplicates() -> None:
+    assert metric_info
+
+    duplicates: dict[tuple[tuple[str, object], ...], list[str]] = {}
+    for name, info in metric_info.items():
+        duplicates.setdefault(tuple(sorted(info.items())), []).append(name)
+
+    assert [names for names in duplicates.values() if len(names) > 1] == _DUPLICATE_METRIC_INFOS

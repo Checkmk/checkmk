55031ab79e1932a32a0520a25deac12ed1e23f19
Joerg Herbel <joerg.herbel@checkmk.com>
2023-11-15 17:06:07 +0100

    16299 FIX Fix inconsistent interaction for graphs in dashboards
    SUP-16339
    
    Change-Id: I355b3a400251a2112b7a62bb80fd69946730d546

--- web/htdocs/js/modules/graphs.ts
+++ web/htdocs/js/modules/graphs.ts
@@ -1247,6 +1247,7 @@ function update_mouse_indicator(canvas, graph, graph_node, x) {
         canvas.clientHeight -
         to_display_coord(graph_bottom_border(graph)) +
         "px";
+    indicator.style.pointerEvents = "none";
 }
 
 function remove_all_mouse_indicators() {

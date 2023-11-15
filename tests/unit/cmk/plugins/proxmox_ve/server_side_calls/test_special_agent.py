433615868516ca0d78e41afa79047016f4f1b477
Solomon Jacobs <solomon.jacobs@checkmk.com>
2023-11-15 10:28:16 +0100

    migrate: legacy_checks/agent_proxmox_ve.py
    Uses the server_side_calls API.
    
    CMK-15077
    
    Change-Id: I5d075d62a1d66661eed86e77cdbbaf72681776a1

--- tests/unit/cmk/plugins/proxmox_ve/server_side_calls/test_special_agent.py
+++ tests/unit/cmk/plugins/proxmox_ve/server_side_calls/test_special_agent.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from collections.abc import Mapping, Sequence
-from typing import Any
 
 import pytest
+from polyfactory.factories import DataclassFactory
 
-from tests.testlib import SpecialAgent
+from cmk.plugins.proxmox_ve.server_side_calls.special_agent import config
+from cmk.server_side_calls.v1 import HostConfig, HTTPProxy, PlainTextSecret, StoredSecret
+
+
+class HostConfigFactory(DataclassFactory):
+    __model__ = HostConfig
 
 
 @pytest.mark.parametrize(
@@ -27,7 +32,7 @@ from tests.testlib import SpecialAgent
                 "-u",
                 "user",
                 "-p",
-                "passwd",
+                PlainTextSecret(value="passwd"),
                 "--port",
                 "443",
                 "--no-cert-check",
@@ -49,7 +54,7 @@ from tests.testlib import SpecialAgent
                 "-u",
                 "user",
                 "-p",
-                ("store", "passwd", "%s"),
+                StoredSecret(value="passwd"),
                 "--timeout",
                 "40",
                 "testhost",
@@ -59,14 +64,15 @@ from tests.testlib import SpecialAgent
     ],
 )
 def test_agent_proxmox_ve_arguments(
-    params: Mapping[str, Any],
-    expected_result: Sequence[Any],
+    params: Mapping[str, object], expected_result: Sequence[str]
 ) -> None:
-    assert (
-        SpecialAgent("agent_proxmox_ve").argument_func(
-            params,
-            "testhost",
-            "1.2.3.4",
-        )
-        == expected_result
-    )
+    # Assemble
+    host_config = HostConfigFactory.build(name="testhost")
+    http_proxies = {"my_proxy": HTTPProxy("my_proxy", "My Proxy", "proxy.com")}
+    # Act
+    parsed_params = config.parameter_parser(params)
+    commands = list(config.commands_function(parsed_params, host_config, http_proxies))
+    # Assert
+    assert len(commands) == 1
+    command = commands[0].command_arguments
+    assert command == expected_result

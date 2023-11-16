73f673de61a4bd723f29088fb29717a727f508eb
Andreas Boesl <andreas.boesl@checkmk.com>
2023-11-16 08:10:26 +0100

    migrated check_by_ssh to CommandAPI
    Change-Id: Ia6cc1a040371c3a5048c2f8a26c28d0df16e1ed1

--- tests/unit/checks/test_check_by_ssh.py
+++ tests/unit/checks/test_check_by_ssh.py
@@ -4,24 +4,33 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from collections.abc import Sequence
+from typing import Mapping
 
 import pytest
 
-from tests.testlib import ActiveCheck
+from cmk.plugins.ssh.server_side_calls.check_by_ssh import active_check_by_ssh
+from cmk.server_side_calls.v1 import HostConfig, IPAddressFamily
 
 pytestmark = pytest.mark.checks
+
+TEST_HOST_CONFIG = HostConfig(
+    "my_host", "1.2.3.4", "my_alias", IPAddressFamily.IPV4, ipv4address="my.ipv4.address"
+)
 
 
 @pytest.mark.parametrize(
     "params,expected_args",
     [
-        (["foo", {}], ["-H", "$HOSTADDRESS$", "-C", "foo"]),
-        (["foo", {"port": 22}], ["-H", "$HOSTADDRESS$", "-C", "foo", "-p", 22]),
+        ({"options": ("foo", {})}, ["-H", "1.2.3.4", "-C", "foo"]),
+        ({"options": ("foo", {"port": 22})}, ["-H", "1.2.3.4", "-C", "foo", "-p", 22]),
     ],
 )
 def test_check_by_ssh_argument_parsing(
-    params: Sequence[object], expected_args: Sequence[object]
+    params: Mapping[str, object], expected_args: Sequence[object]
 ) -> None:
     """Tests if all required arguments are present."""
-    active_check = ActiveCheck("check_by_ssh")
-    assert active_check.run_argument_function(params) == expected_args
+    (cmd,) = active_check_by_ssh.commands_function(
+        active_check_by_ssh.parameter_parser(params), TEST_HOST_CONFIG, {}
+    )
+
+    assert cmd.command_arguments == expected_args

ecc5bbf9b1b45aaa7e369630b5b1a75e3c554720
Christoph Rauch <christoph.rauch@checkmk.com>
2023-11-21 12:21:50 +0100

    forms.js: save some cycles when searching for select2 fields
    Change-Id: Ib072a3bb1169ccafd5db186b67c60a5fb51c62be

--- web/htdocs/js/modules/forms.js
+++ web/htdocs/js/modules/forms.js
@@ -21,15 +21,15 @@ var g_previous_timeout_id = null;
 var g_ajax_obj = null;
 
 export function enable_select2_dropdowns(container) {
-    let not_autocompleting;
     if (!container) container = $(document);
+
+    let candidates;
+    candidates = $(container).find(".select2-enable").not(".vlof_prototype .select2-enable");
 
     // These can work upgraded with select2 and in plain mode,
     // so we can skip converting these when it would take too long.
-    not_autocompleting = $(container)
-        .find(".select2-enable")
-        .not(".vlof_prototype .select2-enable")
-        .not("[data-autocompleter]");
+    let not_autocompleting;
+    not_autocompleting = candidates.not("[data-autocompleter]");
     const max_wait_seconds = 3; // Let the user wait for max 3 seconds. We can convert ~60-80 selects per second.
     if (not_autocompleting.length <= max_wait_seconds * 60) {
         // If we've got too many of those, we bail out early instead of letting the user wait potentially "forever".
@@ -41,9 +41,7 @@ export function enable_select2_dropdowns(container) {
 
     // We always have to convert these, because they can only work with select2, and would stop working otherwise.
     let autocompleting;
-    autocompleting = $(container)
-        .find(".select2-enable[data-autocompleter]")
-        .not(".vlof_prototype .select2-enable");
+    autocompleting = candidates.filter("[data-autocompleter]");
 
     autocompleting.select2({
         dropdownAutoWidth: true,

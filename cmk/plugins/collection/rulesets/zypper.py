dfa0aa4324971e91090236f63e22993816079c5d
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2023-11-22 17:22:49 +0100

    Ruleset API: Migrate zypper to new API
    CMK-14984
    
    Change-Id: I90b4ebb5d376fadc51ca7e82d0308eb06bf8e964

---
+++ cmk/plugins/collection/rulesets/zypper.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.rulesets.v1 import (
+    CheckParameterRuleSpecWithoutItem,
+    DictElement,
+    Dictionary,
+    Localizable,
+    Migrate,
+    MonitoringState,
+    State,
+    Topic,
+)
+
+
+def _parameter_form_zypper() -> Dictionary:
+    return Dictionary(
+        elements={
+            "security": DictElement(
+                parameter_form=MonitoringState(
+                    title=Localizable("State when security updates are pending"),
+                    prefill_value=State.CRIT,
+                ),
+            ),
+            "recommended": DictElement(
+                parameter_form=MonitoringState(
+                    title=Localizable("State when recommended updates are pending"),
+                    prefill_value=State.WARN,
+                ),
+            ),
+            "other": DictElement(
+                parameter_form=MonitoringState(
+                    title=Localizable(
+                        "State when updates are pending, which are neither recommended or a "
+                        "security update"
+                    ),
+                    prefill_value=State.OK,
+                ),
+            ),
+            "locks": DictElement(
+                parameter_form=MonitoringState(
+                    title=Localizable("State when packages are locked"),
+                    prefill_value=State.WARN,
+                ),
+            ),
+        },
+        # TODO remove before 2.3 release, showcases migration
+        transform=Migrate(raw_to_form=lambda v: v if isinstance(v, dict) and v else {}),
+    )
+
+
+rule_spec_zypper = CheckParameterRuleSpecWithoutItem(
+    name="zypper",
+    topic=Topic.OPERATING_SYSTEM,
+    parameter_form=_parameter_form_zypper,
+    title=Localizable("Zypper Updates"),
+)

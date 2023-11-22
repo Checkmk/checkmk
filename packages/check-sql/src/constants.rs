909f23acdb1ffc87f1ea7acfd760ea69adf431a5
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-11-22 17:26:11 +0100

    add correct version and about
    [CMK-15236]
    
    Change-Id: Ic3855ac0360fcf6f262c0ff71b0cccefe433cf99

--- packages/check-sql/src/constants.rs
+++ packages/check-sql/src/constants.rs
@@ -1,6 +1,9 @@
 // Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
+
+pub const VERSION: &str = "2.3.0b1";
+
 use lazy_static::lazy_static;
 use std::path::{Path, PathBuf};
 pub mod log {

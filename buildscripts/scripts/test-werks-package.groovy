4b59d840b113e6edc200f7dfb8744c5331242729
Benedikt Seidl <benedikt.seidl@checkmk.com>
2023-11-23 10:42:06 +0100

    only validate werks in 2.2.0 branch
    python code from packages/cmk-werks is directly in cmk/werks
    but lacks tests and the run script
    
    Change-Id: I61a9a0821807bef6a7584f8307d601140af22cc0

--- buildscripts/scripts/test-werks-package.groovy
+++ buildscripts/scripts/test-werks-package.groovy
@@ -5,10 +5,6 @@
 def main() {
     dir("${checkout_dir}") {
         docker_image_from_alias("IMAGE_TESTING").inside() {
-            stage('Test Package cmk-werks') {
-                sh("packages/cmk-werks/run --clean --all");
-            }
-
             stage('Validate .werks') {
                 sh("scripts/run-pipenv run python -m cmk.werks.validate");
             }

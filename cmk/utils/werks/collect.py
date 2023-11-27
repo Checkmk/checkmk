ba1b606ca17892f946ac4b4b146b4541cc23dd43
Benedikt Seidl <benedikt.seidl@checkmk.com>
2023-11-27 15:58:53 +0100

    replace old broken compatible values
    It's easier to implement some exceptions than to commit to those very
    very old branches.
    
    Change-Id: I7355e73d8f65793aabf1ca8ded6948cfaafa0fc8

--- cmk/utils/werks/collect.py
+++ cmk/utils/werks/collect.py
@@ -8,7 +8,7 @@ import logging
 import re
 from collections import defaultdict
 from pathlib import Path
-from typing import Literal
+from typing import Iterator, Literal
 
 from git.repo import Repo
 
@@ -81,7 +81,25 @@ class CmkConfig(Config):
         if werk_filename == "3229":
             # don't want to commit to branch 1.2.8
             return werk_string.replace("name inventorized\\", "name inventorized")
+        if werk_filename in {"1071", "198", "4045", "10589", "7032", "10579"}:
+            return _replace_compatible(werk_string, "compat")
+        if werk_filename in {"4914", "4737", "10303", "11202", "11277", "7048", "11159", "11475"}:
+            return _replace_compatible(werk_string, "incomp")
         return werk_string
+
+
+COMP_MATCHER = re.compile("^Compatible: (comp|multisite|incompat|imcompat|compa)$")
+
+
+def _replace_compatible(werk_string: str, compatible: str) -> str:
+    def generator() -> Iterator[str]:
+        for line in werk_string.split("\n"):
+            if COMP_MATCHER.match(line):
+                line = f"Compatible: {compatible}"
+            yield line
+            yield "\n"
+
+    return "".join(generator())
 
 
 class KubeConfig(Config):

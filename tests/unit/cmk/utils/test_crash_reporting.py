efa40079b7e735b4d70c40cd4ff6d70f045fc84f
Lukas Lengler <lukas.lengler@checkmk.com>
2023-11-27 13:00:29 +0100

    16033 FIX Unable to create crash report
    When custom check plugins that used tuples as dictionary keys in the section crashed,
    the crash creation failed and crashed itself.
    
    Now the crash creation no longer fails and the crash is created successfully.
    
    Change-Id: I9a91c55c8be95b59325640990341149c8b0dcae2

--- tests/unit/cmk/utils/test_crash_reporting.py
+++ tests/unit/cmk/utils/test_crash_reporting.py
@@ -16,7 +16,12 @@ import pytest
 
 import cmk.utils.paths
 import cmk.utils.version as cmk_version
-from cmk.utils.crash_reporting import _format_var_for_export, ABCCrashReport, CrashReportStore
+from cmk.utils.crash_reporting import (
+    _format_var_for_export,
+    ABCCrashReport,
+    CrashInfo,
+    CrashReportStore,
+)
 
 
 class UnitTestCrashReport(ABCCrashReport):
@@ -157,3 +162,29 @@ def test_crash_report_store_cleanup(crash_dir, n_crashes) -> None:  # type:ignor
 
     assert len(set(crash_dir.glob("*"))) <= store._keep_num_crashes
     assert {e.name for e in crash_dir.glob("*")} == set(crash_ids[-store._keep_num_crashes :])
+
+
+@pytest.mark.parametrize(
+    "crash_info, expected",
+    [
+        (
+            {
+                "details": {
+                    "section": {
+                        ("foo", "bar"): {
+                            "id": "1337",
+                            "name": "foobar",
+                        },
+                    },
+                },
+            },
+            (
+                '{"details": {"section": {"[\\"foo\\", \\"bar\\"]": {"id": "1337", "name": '
+                '"foobar"}}}}'
+            ),
+        ),
+        ({"foo": "bar"}, '{"foo": "bar"}'),
+    ],
+)
+def test_crash_report_json_dump(crash_info: CrashInfo, expected: str) -> None:
+    assert CrashReportStore.dump_crash_info(crash_info) == expected

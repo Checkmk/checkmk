5e84d6630fb5833a29a486775ca6de3713747f00
Gav <gavin.mcguigan@checkmk.com>
2023-11-28 06:31:59 +0100

    migrate: agent_gcp to server_side_calls API
    CMK-15074
    
    Change-Id: Id9385772f60acbcb2926526395b2afe3b0bdbac5

--- tests/unit/cmk/plugins/collection/server_side_calls/test_gcp.py
+++ tests/unit/cmk/plugins/collection/server_side_calls/test_gcp.py
@@ -9,9 +9,17 @@ from typing import Any
 import pytest
 from freezegun import freeze_time
 
-from tests.testlib import SpecialAgent
+from cmk.plugins.collection.server_side_calls.gcp import special_agent_gcp
+from cmk.server_side_calls.v1 import HostConfig, IPAddressFamily, PlainTextSecret, StoredSecret
 
 pytestmark = pytest.mark.checks
+
+HOST_CONFIG = HostConfig(
+    name="hostname",
+    address="0.0.0.1",
+    alias="host_alias",
+    ip_family=IPAddressFamily.IPV4,
+)
 
 
 @freeze_time("2022-01-12")
@@ -21,14 +29,14 @@ pytestmark = pytest.mark.checks
         pytest.param(
             {
                 "project": "test",
-                "credentials": "definitely some json",
+                "credentials": ("password", "a_very_important_secret"),
                 "services": ["gcs", "run"],
             },
             [
                 "--project",
                 "test",
                 "--credentials",
-                "definitely some json",
+                PlainTextSecret(value="a_very_important_secret", format="%s"),
                 "--date",
                 "2022-01-12",
                 "--services",
@@ -42,7 +50,7 @@ pytestmark = pytest.mark.checks
         pytest.param(
             {
                 "project": "test",
-                "credentials": "definitely some json",
+                "credentials": ("password", "a_very_important_secret"),
                 "cost": {"tableid": "checkmk"},
                 "services": [],
             },
@@ -50,7 +58,7 @@ pytestmark = pytest.mark.checks
                 "--project",
                 "test",
                 "--credentials",
-                "definitely some json",
+                PlainTextSecret(value="a_very_important_secret", format="%s"),
                 "--date",
                 "2022-01-12",
                 "--cost_table",
@@ -63,7 +71,7 @@ pytestmark = pytest.mark.checks
         pytest.param(
             {
                 "project": "test",
-                "credentials": "definitely some json",
+                "credentials": ("store", "password_id_1"),
                 "cost": {"tableid": "checkmk"},
                 "services": ["gcs"],
             },
@@ -71,7 +79,7 @@ pytestmark = pytest.mark.checks
                 "--project",
                 "test",
                 "--credentials",
-                "definitely some json",
+                StoredSecret(value="password_id_1", format="%s"),
                 "--date",
                 "2022-01-12",
                 "--cost_table",
@@ -86,7 +94,7 @@ pytestmark = pytest.mark.checks
         pytest.param(
             {
                 "project": "test",
-                "credentials": "definitely some json",
+                "credentials": ("store", "password_id_2"),
                 "services": [],
                 "piggyback": {"prefix": "custom-prefix", "piggyback_services": ["gce"]},
             },
@@ -94,7 +102,7 @@ pytestmark = pytest.mark.checks
                 "--project",
                 "test",
                 "--credentials",
-                "definitely some json",
+                StoredSecret(value="password_id_2", format="%s"),
                 "--date",
                 "2022-01-12",
                 "--services",
@@ -110,11 +118,6 @@ def test_gcp_argument_parsing(
     params: Mapping[str, Any],
     expected_result: Sequence[str],
 ) -> None:
-    assert (
-        SpecialAgent("agent_gcp").argument_func(
-            params,
-            "testhost",
-            "address",
-        )
-        == expected_result
-    )
+    parsed_params = special_agent_gcp.parameter_parser(params)
+    commands = list(special_agent_gcp.commands_function(parsed_params, HOST_CONFIG, {}))
+    assert commands[0].command_arguments == expected_result

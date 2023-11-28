06c35c78e0cb364dd96503ec3c5924159ef9bd12
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2023-11-28 13:27:17 +0100

    Use CI_TEST_SQL_DB_ENDPOINT in test-check-sql.groovy
    Change-Id: I58bf577948551e06a7791a7a2bbbf58c0195c11f

--- buildscripts/scripts/test-check-sql.groovy
+++ buildscripts/scripts/test-check-sql.groovy
@@ -6,7 +6,12 @@ def main() {
     dir("${checkout_dir}") {
         docker_image_from_alias("IMAGE_TESTING").inside() {
             stage('Compile & Test Check SQL') {
-                sh("packages/check-sql/run --setup-environment --clean --all");
+                withCredentials([string(
+                    credentialsId: "CI_TEST_SQL_DB_ENDPOINT",
+                    variable:"CI_TEST_SQL_DB_ENDPOINT"
+                )]) {
+                    sh("packages/check-sql/run --setup-environment --clean --all");
+                }
             }
         }
     }

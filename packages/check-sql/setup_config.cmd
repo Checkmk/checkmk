cca1b3fc9dd8132d6bb54c9df9263023f67cde33
Sergey Kipnis <sergey.kipnis@checkmk.com>
2023-11-29 15:27:11 +0100

    add log if check sql test env var not defined
    should be defined by CI or manual
    
    Change-Id: I560037bd8cc78596bf568c454ed763bb969a8c19

--- packages/check-sql/setup_config.cmd
+++ packages/check-sql/setup_config.cmd
@@ -1,6 +1,8 @@
 @echo off
 :: Script to define mandatory variables individual for the project
 
+if "%CI_TEST_SQL_DB_ENDPOINT%" == "" echo "CI_TEST_SQL_DB_ENDPOINT is not defined, full testing is impossible"
+
 :: for the run.cmd
 set worker_name=check-sql
 :: artefacts

c93d1b804b067c619648a51486d0dc505304b992
Mathias Laurin <mathias.laurin@checkmk.com>
2023-11-30 10:07:00 +0100

    check-cert: Move common types to prelude
    CMK-14683
    
    Change-Id: I4b3eecbef4ec816ef2a49e890e66b1db1329b581

--- packages/check-cert/src/prelude.rs
+++ packages/check-cert/src/prelude.rs
@@ -2,11 +2,5 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
-pub mod check;
-pub mod checker {
-    pub mod certificate;
-    pub mod validation;
-}
-pub mod fetcher;
-pub mod prelude;
-pub mod truststore;
+pub type Der = Vec<u8>;
+pub type Chain = Vec<Der>;

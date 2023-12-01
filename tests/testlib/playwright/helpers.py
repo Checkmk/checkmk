ef261d4dc2e926e28f391ea41fa020fa750034dd
Sven Panne <sven.panne@checkmk.com>
2023-12-01 09:09:42 +0100

    Fix imports & unpin playwright.
    Pinning should not be done lightlyheartedly, because it restricts
    transitive dependencies in an unknown way, too.  This can easily
    cause a dependency hell.
    
    Sorted the dev-packages alphabetically on the way, they had become
    quite chaotic. Initially, things were grouped by their purpose a
    bit, but those days are over.
    
    Change-Id: I2b2f2b8d6fc8179b51fddd83feccc8025b20dfd8

--- tests/testlib/playwright/helpers.py
+++ tests/testlib/playwright/helpers.py
@@ -12,8 +12,8 @@ from re import Pattern
 from typing import Literal
 from urllib.parse import urljoin, urlsplit
 
-from playwright._impl import _api_types
-from playwright.sync_api import expect, Locator, Page, Response
+from playwright.sync_api import Error, expect, Locator, Page, Response
+from playwright.sync_api import TimeoutError as PWTimeoutError
 
 from tests.testlib.playwright.timeouts import TemporaryTimeout, TIMEOUT_ACTIVATE_CHANGES_MS
 
@@ -355,7 +355,7 @@ class PPage(LocatorHelper):
                 try:
                     locator.click()
                     clicked = True
-                except _api_types.TimeoutError:
+                except PWTimeoutError:
                     pass
 
             if clicked:
@@ -372,7 +372,7 @@ class PPage(LocatorHelper):
             try:
                 if reload_on_error:
                     self.page.reload(wait_until="networkidle")
-            except _api_types.Error:
+            except Error:
                 continue
 
         raise AssertionError(

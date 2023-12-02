1ae4226829ab1788f703937988cba241f0395579
marczako <michaelmarczak@gmail.com>
2023-12-02 19:54:37 +0100

    Update MIB for FortiAuthenicator
    https://mibs.observium.org/mib/FORTINET-FORTIAUTHENTICATOR-MIB/

--- cmk/plugins/lib/fortinet.py
+++ cmk/plugins/lib/fortinet.py
@@ -6,6 +6,6 @@
 from cmk.agent_based.v2 import equals, startswith
 
 DETECT_FORTISANDBOX = equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.12356.118.1.30006")
-DETECT_FORTIAUTHENTICATOR = equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072.3.2.10")
+DETECT_FORTIAUTHENTICATOR = startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.12356.113.")
 DETECT_FORTIGATE = startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.12356.101.1.")
 DETECT_FORTIMAIL = equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.12356.105")

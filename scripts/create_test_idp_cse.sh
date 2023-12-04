2c3116becdcab20b9230623a59ecf25fa82e7e39
René Slowenski <rene.slowenski@checkmk.com>
2023-12-04 19:32:25 +0100

    cse integration test: Improve IDP script
    Allow passing the full IDP url to the IDP script to simplify
    debugging containerized runs (which might not use localhost).
    
    Change-Id: I787d26c10f08c3b0fc9c2fcdc1cf7d70ff73c241

--- scripts/create_test_idp_cse.sh
+++ scripts/create_test_idp_cse.sh
@@ -8,15 +8,22 @@ REPO_PATH="$(dirname "$(dirname "$(realpath "$0")")")"
 sudo true
 
 # PORT and URL under which we can reach the openid provider
-PORT=${1:-5551}
-export URL="http://localhost:${PORT}"
+if [[ "$1" == *"://"* ]]; then
+    export URL=$1
+    HOST=$(echo "${URL}" | cut -d: -f2 | sed "s-^//--")
+    PORT=$(echo "${URL}" | cut -d: -f3)
+else
+    HOST=localhost
+    PORT=${1:-5551}
+    export URL=http://${HOST}:${PORT}
+fi
 
 # URL of the checkmk instance; checkmk uses port 5000 by default
-CMK_URL="http://localhost:${2:-5000}"
+CMK_URL=${2:-http://localhost:5000}
 
 # Write cognito configuration file
 sudo mkdir -p /etc/cse
 "$(dirname "$0")/create_cognito_config_cse.sh" "${URL}" "${CMK_URL}" | sudo tee /etc/cse/cognito-cmk.json >/dev/null
 
 export PYTHONPATH="${REPO_PATH}:${REPO_PATH}/tests/testlib"
-"${REPO_PATH}/scripts/run-pipenv" run uvicorn cse.openid_oauth_provider:application --port "${PORT}"
+"${REPO_PATH}/scripts/run-pipenv" run uvicorn cse.openid_oauth_provider:application --host "${HOST}" --port "${PORT}"

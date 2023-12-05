85d060999f55ef724092b70ef3ccbaf074dd788b
Mathias Laurin <mathias.laurin@checkmk.com>
2023-12-05 10:47:22 +0100

    check-cert: Remove previous self-signed check
    CMK-14683
    
    Change-Id: I1e2fb69a72376bad311ba67ac4dc60d9cd88bd04

--- packages/check-cert/src/checker/verification.rs
+++ packages/check-cert/src/checker/verification.rs
@@ -2,38 +2,8 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
-use crate::check::{self, Collection, SimpleCheckResult};
+use crate::check::{Collection, SimpleCheckResult};
 use typed_builder::TypedBuilder;
-
-mod selfsigned {
-    use super::check;
-    use x509_parser::certificate::X509Certificate;
-    use x509_parser::prelude::FromDer;
-
-    pub enum SelfSigned {
-        Yes,
-        No,
-        Invalid,
-    }
-
-    pub fn is_self_signed(der: &[u8]) -> SelfSigned {
-        let cert = match X509Certificate::from_der(der) {
-            Ok((_rem, cert)) => cert,
-            Err(_) => check::abort("Failed to parse certificate"),
-        };
-
-        if cert.subject() == cert.issuer() {
-            match cert.verify_signature(None) {
-                Ok(_) => SelfSigned::Yes,
-                Err(_) => SelfSigned::Invalid,
-            }
-        } else {
-            SelfSigned::No
-        }
-    }
-}
-
-use selfsigned::SelfSigned;
 
 mod verify {
     use openssl::stack::Stack;
@@ -84,25 +54,11 @@ pub struct Config<'a> {
 pub fn check(chain: &[Vec<u8>], config: Config) -> Collection {
     assert!(!chain.is_empty());
 
-    Collection::from(&mut vec![
-        check_self_signed(
-            selfsigned::is_self_signed(&chain[0]),
-            config.allow_self_signed,
-        )
-        .into(),
-        check_verify_chain(chain, config.trust_store, config.allow_self_signed).into(),
-    ])
-}
-
-fn check_self_signed(self_signed: SelfSigned, allow: bool) -> SimpleCheckResult {
-    match self_signed {
-        SelfSigned::No => SimpleCheckResult::ok("Certificate is not self signed"),
-        SelfSigned::Yes => match allow {
-            true => SimpleCheckResult::ok("Certificate is self signed"),
-            false => SimpleCheckResult::warn("Certificate is self signed"),
-        },
-        SelfSigned::Invalid => SimpleCheckResult::warn("Self signed signature is invalid"),
-    }
+    Collection::from(check_verify_chain(
+        chain,
+        config.trust_store,
+        config.allow_self_signed,
+    ))
 }
 
 fn check_verify_chain(

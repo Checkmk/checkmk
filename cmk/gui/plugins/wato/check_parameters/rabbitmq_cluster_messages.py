8caa50e990e599b0670fca76f5757371ebc3b878
Moritz Kiemer <moritz.kiemer@checkmk.com>
2023-12-06 21:33:14 +0100

    16272 FIX rabbitmq_cluster_messages: Most of the configrable parametes have not been considered
    Most of the parameters configurable in the ruleset <i>"RabbitMQ cluster messages"</i> have not been considered by the plugin.
    
    Change-Id: Ie285d4abaf668a4aa4f7488c56c8ab3c10279a6a

--- cmk/gui/plugins/wato/check_parameters/rabbitmq_cluster_messages.py
+++ cmk/gui/plugins/wato/check_parameters/rabbitmq_cluster_messages.py
@@ -4,179 +4,223 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import enum
+
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithoutItem,
     rulespec_registry,
     RulespecGroupCheckParametersApplications,
 )
-from cmk.gui.valuespec import Dictionary, Float, Integer, Tuple
+from cmk.gui.valuespec import Dictionary, Float, Integer, Transform, Tuple
+
+
+class MessageType(enum.Enum):
+    """Watch out! The values must match the check plugin!
+
+    For now copy'n'paste. Should go to cmk/plugins/rabbitmq sometay (TM).
+    """
+
+    TOTAL = "messages"
+    TOTAL_RATE = "messages_rate"
+    READY = "messages_ready"
+    UNACKNOWLEDGED = "messages_unacknowledged"
+    PUBLISH = "messages_publish"
+    PUBLISH_RATE = "messages_publish_rate"
+    DELIVER = "messages_deliver"
+    DELIVER_RATE = "messages_deliver_rate"
+
+
+_KEY_MAP = {
+    "message_rate_upper": f"{MessageType.TOTAL_RATE.value}_upper",
+    "message_rate_lower": f"{MessageType.TOTAL_RATE.value}_lower",
+    "msg_ready_upper": f"{MessageType.READY.value}_upper",
+    "msg_ready_lower": f"{MessageType.READY.value}_lower",
+    "msg_unack_upper": f"{MessageType.UNACKNOWLEDGED.value}_upper",
+    "msg_unack_lower": f"{MessageType.UNACKNOWLEDGED.value}_lower",
+    "msg_publish_upper": f"{MessageType.PUBLISH.value}_upper",
+    "msg_publish_lower": f"{MessageType.PUBLISH.value}_lower",
+    "msg_publish_rate_upper": f"{MessageType.PUBLISH_RATE.value}_upper",
+    "msg_publish_rate_lower": f"{MessageType.PUBLISH_RATE.value}_lower",
+    "msg_deliver_upper": f"{MessageType.DELIVER.value}_upper",
+    "msg_deliver_lower": f"{MessageType.DELIVER.value}_lower",
+    "msg_deliver_rate_upper": f"{MessageType.DELIVER_RATE.value}_upper",
+    "msg_deliver_rate_lower": f"{MessageType.DELIVER_RATE.value}_lower",
+}
+
+
+def _rename_keys(p: dict[str, object]) -> dict[str, object]:
+    """The plugin really needs these keys."""
+    return {_KEY_MAP.get(k, k): v for k, v in p.items()}
 
 
 def _parameter_valuespec_rabbitmq_cluster_messages():
-    return Dictionary(
-        elements=[
-            (
-                "messages_upper",
-                Tuple(
-                    title=_("Upper level for total number of queued messages"),
-                    elements=[
-                        Integer(title=_("Warning at"), unit="messages"),
-                        Integer(title=_("Critical at"), unit="messages"),
-                    ],
+    return Transform(
+        Dictionary(
+            elements=[
+                (
+                    f"{MessageType.TOTAL.value}_upper",
+                    Tuple(
+                        title=_("Upper level for total number of queued messages"),
+                        elements=[
+                            Integer(title=_("Warning at"), unit="messages"),
+                            Integer(title=_("Critical at"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "messages_lower",
-                Tuple(
-                    title=_("Lower level for total number of queued messages"),
-                    elements=[
-                        Integer(title=_("Warning below"), unit="messages"),
-                        Integer(title=_("Critical below"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.TOTAL.value}_lower",
+                    Tuple(
+                        title=_("Lower level for total number of queued messages"),
+                        elements=[
+                            Integer(title=_("Warning below"), unit="messages"),
+                            Integer(title=_("Critical below"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "message_rate_upper",
-                Tuple(
-                    title=_("Upper level for message rate"),
-                    elements=[
-                        Float(title=_("Warning at"), unit="1/s"),
-                        Float(title=_("Critical at"), unit="1/s"),
-                    ],
+                (
+                    f"{MessageType.TOTAL_RATE.value}_upper",
+                    Tuple(
+                        title=_("Upper level for message rate"),
+                        elements=[
+                            Float(title=_("Warning at"), unit="1/s"),
+                            Float(title=_("Critical at"), unit="1/s"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "message_rate_lower",
-                Tuple(
-                    title=_("Lower level for message rate"),
-                    elements=[
-                        Float(title=_("Warning below"), unit="1/s"),
-                        Float(title=_("Critical below"), unit="1/s"),
-                    ],
+                (
+                    f"{MessageType.TOTAL_RATE.value}_lower",
+                    Tuple(
+                        title=_("Lower level for message rate"),
+                        elements=[
+                            Float(title=_("Warning below"), unit="1/s"),
+                            Float(title=_("Critical below"), unit="1/s"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_ready_upper",
-                Tuple(
-                    title=_("Upper level for total number of ready messages"),
-                    elements=[
-                        Integer(title=_("Warning at"), unit="messages"),
-                        Integer(title=_("Critical at"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.READY.value}_upper",
+                    Tuple(
+                        title=_("Upper level for total number of ready messages"),
+                        elements=[
+                            Integer(title=_("Warning at"), unit="messages"),
+                            Integer(title=_("Critical at"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_ready_lower",
-                Tuple(
-                    title=_("Lower level for total number of ready messages"),
-                    elements=[
-                        Integer(title=_("Warning below"), unit="messages"),
-                        Integer(title=_("Critical below"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.READY.value}_lower",
+                    Tuple(
+                        title=_("Lower level for total number of ready messages"),
+                        elements=[
+                            Integer(title=_("Warning below"), unit="messages"),
+                            Integer(title=_("Critical below"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_unack_upper",
-                Tuple(
-                    title=_("Upper level for total number of unacknowledged messages"),
-                    elements=[
-                        Integer(title=_("Warning at"), unit="messages"),
-                        Integer(title=_("Critical at"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.UNACKNOWLEDGED.value}_upper",
+                    Tuple(
+                        title=_("Upper level for total number of unacknowledged messages"),
+                        elements=[
+                            Integer(title=_("Warning at"), unit="messages"),
+                            Integer(title=_("Critical at"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_unack_lower",
-                Tuple(
-                    title=_("Lower level for total number of unacknowledged messages"),
-                    elements=[
-                        Integer(title=_("Warning below"), unit="messages"),
-                        Integer(title=_("Critical below"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.UNACKNOWLEDGED.value}_lower",
+                    Tuple(
+                        title=_("Lower level for total number of unacknowledged messages"),
+                        elements=[
+                            Integer(title=_("Warning below"), unit="messages"),
+                            Integer(title=_("Critical below"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_publish_upper",
-                Tuple(
-                    title=_("Upper level for total number of published messages"),
-                    elements=[
-                        Integer(title=_("Warning at"), unit="messages"),
-                        Integer(title=_("Critical at"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.PUBLISH.value}_upper",
+                    Tuple(
+                        title=_("Upper level for total number of published messages"),
+                        elements=[
+                            Integer(title=_("Warning at"), unit="messages"),
+                            Integer(title=_("Critical at"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_publish_lower",
-                Tuple(
-                    title=_("Lower level for total number of published messages"),
-                    elements=[
-                        Integer(title=_("Warning below"), unit="messages"),
-                        Integer(title=_("Critical below"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.PUBLISH.value}_lower",
+                    Tuple(
+                        title=_("Lower level for total number of published messages"),
+                        elements=[
+                            Integer(title=_("Warning below"), unit="messages"),
+                            Integer(title=_("Critical below"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_publish_rate_upper",
-                Tuple(
-                    title=_("Upper level for published message rate"),
-                    elements=[
-                        Float(title=_("Warning at"), unit="1/s"),
-                        Float(title=_("Critical at"), unit="1/s"),
-                    ],
+                (
+                    f"{MessageType.PUBLISH_RATE.value}_upper",
+                    Tuple(
+                        title=_("Upper level for published message rate"),
+                        elements=[
+                            Float(title=_("Warning at"), unit="1/s"),
+                            Float(title=_("Critical at"), unit="1/s"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_publish_rate_lower",
-                Tuple(
-                    title=_("Lower level for published message rate"),
-                    elements=[
-                        Float(title=_("Warning below"), unit="1/s"),
-                        Float(title=_("Critical below"), unit="1/s"),
-                    ],
+                (
+                    f"{MessageType.PUBLISH_RATE.value}_lower",
+                    Tuple(
+                        title=_("Lower level for published message rate"),
+                        elements=[
+                            Float(title=_("Warning below"), unit="1/s"),
+                            Float(title=_("Critical below"), unit="1/s"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_deliver_upper",
-                Tuple(
-                    title=_("Upper level for total number of delivered messages"),
-                    elements=[
-                        Integer(title=_("Warning at"), unit="messages"),
-                        Integer(title=_("Critical at"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.DELIVER.value}_upper",
+                    Tuple(
+                        title=_("Upper level for total number of delivered messages"),
+                        elements=[
+                            Integer(title=_("Warning at"), unit="messages"),
+                            Integer(title=_("Critical at"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_deliver_lower",
-                Tuple(
-                    title=_("Lower level for total number of delivered messages"),
-                    elements=[
-                        Integer(title=_("Warning below"), unit="messages"),
-                        Integer(title=_("Critical below"), unit="messages"),
-                    ],
+                (
+                    f"{MessageType.DELIVER.value}_lower",
+                    Tuple(
+                        title=_("Lower level for total number of delivered messages"),
+                        elements=[
+                            Integer(title=_("Warning below"), unit="messages"),
+                            Integer(title=_("Critical below"), unit="messages"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_deliver_rate_upper",
-                Tuple(
-                    title=_("Upper level for delivered message rate"),
-                    elements=[
-                        Float(title=_("Warning at"), unit="1/s"),
-                        Float(title=_("Critical at"), unit="1/s"),
-                    ],
+                (
+                    f"{MessageType.DELIVER_RATE.value}_rate_upper",
+                    Tuple(
+                        title=_("Upper level for delivered message rate"),
+                        elements=[
+                            Float(title=_("Warning at"), unit="1/s"),
+                            Float(title=_("Critical at"), unit="1/s"),
+                        ],
+                    ),
                 ),
-            ),
-            (
-                "msg_deliver_rate_lower",
-                Tuple(
-                    title=_("Lower level for delivered message rate"),
-                    elements=[
-                        Float(title=_("Warning below"), unit="1/s"),
-                        Float(title=_("Critical below"), unit="1/s"),
-                    ],
+                (
+                    f"{MessageType.DELIVER_RATE.value}_lower",
+                    Tuple(
+                        title=_("Lower level for delivered message rate"),
+                        elements=[
+                            Float(title=_("Warning below"), unit="1/s"),
+                            Float(title=_("Critical below"), unit="1/s"),
+                        ],
+                    ),
                 ),
-            ),
-        ],
+            ],
+        ),
+        forth=_rename_keys,
     )
 
 

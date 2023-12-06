2b27ea7cdb96686352757f2b60e46313ed17b154
Simon Jess <simon.jess@checkmk.com>
2023-12-06 16:29:23 +0100

    Fix github actions
    Change-Id: I27e96b33da3d687a8a97f5df9b1fc9728f029a50

--- tests/unit/cmk/gui/graphing/test_rrd_fetch.py
+++ tests/unit/cmk/gui/graphing/test_rrd_fetch.py
@@ -19,31 +19,12 @@ from cmk.gui.graphing._graph_specification import (
     GraphDataRange,
     GraphMetric,
     GraphRecipe,
-    MetricOpRRDChoice,
     MetricOpRRDSource,
-    NeededElementForTranslation,
 )
 from cmk.gui.graphing._graph_templates import TemplateGraphSpecification
 from cmk.gui.graphing._rrd_fetch import fetch_rrd_data_for_graph, translate_and_merge_rrd_columns
-from cmk.gui.graphing.cee._graphs import MetricOpTransformation, TransformationParametersPercentile
 from cmk.gui.time_series import TimeSeries, TimeSeriesValues
 from cmk.gui.utils.temperate_unit import TemperatureUnit
-
-
-def test_needed_elements_of_expression() -> None:
-    assert set(
-        MetricOpTransformation(
-            parameters=TransformationParametersPercentile(percentile=95),
-            operands=[
-                MetricOpRRDChoice(
-                    host_name=HostName("heute"),
-                    service_name="CPU utilization",
-                    metric_name="util",
-                    consolidation_func_name="max",
-                )
-            ],
-        ).needed_elements(lambda *args: ())
-    ) == {NeededElementForTranslation(HostName("heute"), "CPU utilization")}
 
 
 @contextmanager

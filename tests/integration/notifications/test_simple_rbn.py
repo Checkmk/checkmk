f68838e6fc54c043ee6b9708fa3fff18239a8d60
Mathias Laurin <mathias.laurin@checkmk.com>
2023-12-12 14:58:01 +0100

    SRP should be even more important in fixtures
    ... but we're certainly not there yet.
    
    Anyway, this change makes the notification tests a little bit easier
    to understand and change.
    
    CMK-15196
    
    Change-Id: I5d6f46df5ca7ef40725dc2bd412af0593ff8ad73

--- tests/integration/notifications/test_simple_rbn.py
+++ tests/integration/notifications/test_simple_rbn.py
@@ -13,6 +13,8 @@ import pytest
 from tests.testlib import WatchLog
 from tests.testlib.site import Site
 
+from cmk.utils.hostaddress import HostName
+
 
 @pytest.fixture(name="fake_sendmail")
 def fake_sendmail_fixture(site: Site) -> Iterator[None]:
@@ -49,22 +51,16 @@ def fixture_test_user(site: Site) -> Iterator[None]:
         site.openapi.delete_user(username)
 
 
-@pytest.fixture(name="test_log")
-def fixture_test_log(site: Site) -> Iterator[WatchLog]:
-    # This checks the following log files: `var/log/nagios.log` or `var/check_mk/core/history`.
-    site.openapi.create_host(
-        "notify-test",
-        attributes={
-            "ipaddress": "127.0.0.1",
-        },
-    )
+@pytest.fixture(name="host")
+def fixture_host(site: Site) -> Iterator[HostName]:
+    hostname = HostName("notify-test")
+    site.openapi.create_host(hostname, attributes={"ipaddress": "127.0.0.1"})
     site.activate_changes_and_wait_for_core_reload()
 
     try:
-        with WatchLog(site, default_timeout=20) as l:
-            yield l
+        yield hostname
     finally:
-        site.openapi.delete_host("notify-test")
+        site.openapi.delete_host(hostname)
         site.activate_changes_and_wait_for_core_reload()
 
 
@@ -72,44 +68,46 @@ def fixture_test_log(site: Site) -> Iterator[WatchLog]:
 @pytest.mark.usefixtures("test_user")
 @pytest.mark.usefixtures("disable_checks")
 @pytest.mark.usefixtures("disable_flap_detection")
-def test_simple_rbn_host_notification(test_log: WatchLog, site: Site) -> None:
-    site.send_host_check_result("notify-test", 1, "FAKE DOWN", expected_state=1)
+def test_simple_rbn_host_notification(host: HostName, site: Site) -> None:
+    site.send_host_check_result(host, 1, "FAKE DOWN", expected_state=1)
 
-    test_log.check_logged(
-        "] HOST NOTIFICATION: check-mk-notify;notify-test;DOWN;check-mk-notify;FAKE DOWN"
-    )
-    test_log.check_logged("] HOST NOTIFICATION: hh;notify-test;DOWN;mail;FAKE DOWN")
-    test_log.check_logged(
-        "] HOST NOTIFICATION RESULT: hh;notify-test;OK;mail;Spooled mail to local mail transmission agent;"
-    )
+    with WatchLog(site, default_timeout=20) as log:
+        # This checks the following log files: `var/log/nagios.log` or `var/check_mk/core/history`.
+        log.check_logged(
+            f"] HOST NOTIFICATION: check-mk-notify;{host};DOWN;check-mk-notify;FAKE DOWN"
+        )
+        log.check_logged(f"] HOST NOTIFICATION: hh;{host};DOWN;mail;FAKE DOWN")
+        log.check_logged(
+            f"] HOST NOTIFICATION RESULT: hh;{host};OK;mail;Spooled mail to local mail transmission agent;"
+        )
 
 
 @pytest.mark.skip("broken on nagios")
 @pytest.mark.usefixtures("fake_sendmail")
 @pytest.mark.usefixtures("test_user")
 @pytest.mark.usefixtures("disable_flap_detection")
-def test_simple_rbn_service_notification(test_log: WatchLog, site: Site) -> None:
+def test_simple_rbn_service_notification(host: HostName, site: Site) -> None:
     service: Final = "Check_MK"  # Site has 'Check_MK' and 'Check_MK Discovery'.
     assert service in itertools.chain.from_iterable(
         site.live.query("GET services\nColumns: description\n")
     )
 
     # Trigger a check cycle
-    site.send_host_check_result("notify-test", 1, "FAKE DOWN", expected_state=1)
+    site.send_host_check_result(host, 1, "FAKE DOWN", expected_state=1)
     time.sleep(0.1)
     # But keep the site up or the service notifications will be postponed.
-    site.send_host_check_result("notify-test", 0, "FAKE UP", expected_state=0)
+    site.send_host_check_result(host, 0, "FAKE UP", expected_state=0)
 
     # Now generate the service notification.
-    site.send_service_check_result("notify-test", service, 2, "FAKE CRIT")
+    site.send_service_check_result(host, service, 2, "FAKE CRIT")
 
     # And check that the notifications are recorded in the log.
-    test_log.check_logged(
-        f"] SERVICE NOTIFICATION: check-mk-notify;notify-test;{service};CRITICAL;check-mk-notify;FAKE CRIT"
-    )
-    test_log.check_logged(
-        f"] SERVICE NOTIFICATION: hh;notify-test;{service};CRITICAL;mail;FAKE CRIT"
-    )
-    test_log.check_logged(
-        f"] SERVICE NOTIFICATION RESULT: hh;notify-test;{service};OK;mail;Spooled mail to local mail transmission agent;"
-    )
+    with WatchLog(site, default_timeout=20) as log:
+        # This checks the following log files: `var/log/nagios.log` or `var/check_mk/core/history`.
+        log.check_logged(
+            f"] SERVICE NOTIFICATION: check-mk-notify;{host};{service};CRITICAL;check-mk-notify;FAKE CRIT"
+        )
+        log.check_logged(f"] SERVICE NOTIFICATION: hh;{host};{service};CRITICAL;mail;FAKE CRIT")
+        log.check_logged(
+            f"] SERVICE NOTIFICATION RESULT: hh;{host};{service};OK;mail;Spooled mail to local mail transmission agent;"
+        )

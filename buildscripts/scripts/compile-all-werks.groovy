1e2ac73a199237af94f65f3a487de9dfdb7780f9
Jonas Scharpf <jonas.scharpf@checkmk.com>
2023-12-13 17:04:31 +0100

    Fixup paths for JSON files and exit if HTML validation failed
    CMK-14590
    
    Change-Id: I94ae4d4f8be113cea9ec1cdaf47ecbe4b1be787a

--- buildscripts/scripts/compile-all-werks.groovy
+++ buildscripts/scripts/compile-all-werks.groovy
@@ -5,12 +5,12 @@
 def main() {
     def docker_args = "${mount_reference_repo_dir}";
 
-    def target_path = "/home/mkde/werks/all_werks_v2.json"
+    def target_path = "/home/mkde/werks/all_werks_v2.json";
     def targets_credentials = [
         [env.WEB_STAGING, "web-staging"],
         ["checkmk.com", "checkmk-deploy"],
         ["customer.checkmk.com", "customer-deploy"]
-    ]
+    ];
 
     print(
         """
@@ -30,56 +30,59 @@ def main() {
             docker_image_from_alias("IMAGE_TESTING").inside("${docker_args}") {
                 dir("${checkout_dir}") {
                     sh("""
-                    scripts/run-pipenv run echo build venv..
-                    scripts/run-pipenv run python3 -m cmk.utils.werks collect cmk  ${checkout_dir} > cmk.json
-                    scripts/run-pipenv run python3 -m cmk.utils.werks collect cma ${WORKSPACE}/cma > cma.json
-                    scripts/run-pipenv run python3 -m cmk.utils.werks collect checkmk_kube_agent ${WORKSPACE}/checkmk_kube_agent > kube.json
+                        scripts/run-pipenv run echo build venv...
+                        scripts/run-pipenv run python3 -m cmk.utils.werks collect cmk ./ > cmk.json
+                        scripts/run-pipenv run python3 -m cmk.utils.werks collect cma cma > cma.json
+                        scripts/run-pipenv run python3 -m cmk.utils.werks collect checkmk_kube_agent checkmk_kube_agent > kube.json
 
-                    # jq -s '.[0] * .[1] * .[2]' cma.json cmk.json kube.json > all_werks.json
-                    # no need to install jq!!!!!
-                    python3 -c 'import json, sys; print(json.dumps({k: v for f in sys.argv[1:] for k, v in json.load(open(f)).items()}, indent=4))' \
-                        cmk.json \
-                        cma.json \
-                        kube.json \
-                        > ${WORKSPACE}/all_werks.json
+                        # jq -s '.[0] * .[1] * .[2]' cma.json cmk.json kube.json > all_werks.json
+                        # no need to install jq!!!!!
+                        python3 -c 'import json, sys; print(json.dumps({k: v for f in sys.argv[1:] for k, v in json.load(open(f)).items()}, indent=4))' \
+                            cmk.json \
+                            cma.json \
+                            kube.json \
+                            > all_werks.json
                     """);
+
+                    archiveArtifacts(
+                        artifacts: "all_werks.json",
+                        fingerprint: true,
+                    );
                 }
             }
-        }
-    }
-
-    stage("Archive") {
-        show_duration("archiveArtifacts") {
-            archiveArtifacts(
-                artifacts: "all_werks.json",
-                fingerprint: true,
-            );
         }
     }
 
     stage("Validate HTML") {
         docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
             docker_image_from_alias("IMAGE_TESTING").inside("${docker_args}") {
-                try {
-                    dir("${checkout_dir}") {
+                dir("${checkout_dir}") {
+                    try {
+                        sh(script: """
+                            ./scripts/npm-ci
+                            echo '<!DOCTYPE html><html lang="en"><head><title>werks</title></head><body>' > validate-werks.html
+                            # still no need for jq!
+                            python3 -c 'import json; print("\\n".join((f"\\n\\n<p>{}</p>\\n{}".format(key, value["description"]) for key, value in json.load(open("all_werks.json")).items())))' >> validate-werks.html
+                            echo '</body></html>' >> validate-werks.html
+                            java \
+                                -jar node_modules/vnu-jar/build/dist/vnu.jar \
+                                --filterpattern 'The .tt. element is obsolete\\. Use CSS instead\\.' \
+                                --stdout \
+                                --format gnu \
+                                - < validate-werks.html \
+                                > validate-werks.error.txt
+                        """);
+                    } catch(Exception e) {
+                        archiveArtifacts(
+                            artifacts: "validate-werks.*",
+                            fingerprint: true,
+                        );
                         sh("""
-                        ./scripts/npm-ci
-                        echo '<!DOCTYPE html><html lang="en"><head><title>werks</title></head><body>' >${WORKSPACE}/validate-werks.html
-                        # still no need for jq!
-                        python3 -c 'import json; print("\\n".join(("\\n\\n<p>{}</p>\\n{}".format(key, value["description"]) for key, value in json.load(open("${WORKSPACE}/all_werks.json")).items())))' >>${WORKSPACE}/validate-werks.html
-                        echo '</body></html>' >>${WORKSPACE}/validate-werks.html
-                        java -jar node_modules/vnu-jar/build/dist/vnu.jar --filterpattern 'The .tt. element is obsolete\\. Use CSS instead\\.' --stdout --format gnu - <${WORKSPACE}/validate-werks.html >${WORKSPACE}/validate-werks.error.txt
-                        """)
+                            cat "validate-werks.error.txt"
+                            echo "Found invalid HTML. See errors above, compare the line numbers with validate-werks.html artifact."
+                        """);
+                        throw e;
                     }
-                } catch(Exception) {
-                    archiveArtifacts(
-                        artifacts: "validate-werks.*",
-                        fingerprint: true,
-                    );
-                    sh("""
-                    cat "${WORKSPACE}/validate-werks.error.txt"
-                    echo "Found invalid html. See errors above, compare the line numbers with validate-werks.html artifact."
-                    """)
                 }
             }
         }
@@ -93,14 +96,15 @@ def main() {
             withCredentials([
                 sshUserPrivateKey(credentialsId: credentials_id, keyFileVariable: 'keyfile', usernameVariable: 'user')
             ]) {
-                sh """
-                rsync --verbose \
-                    -e "ssh -o StrictHostKeyChecking=no -i ${keyfile} -p ${WEB_STAGING_PORT}" \
-                    ${WORKSPACE}/all_werks.json \
-                    ${user}@${target}:${target_path}
-            """
+                sh("""
+                    rsync --verbose \
+                        -e "ssh -o StrictHostKeyChecking=no -i ${keyfile} -p ${WEB_STAGING_PORT}" \
+                        ${WORKSPACE}/all_werks.json \
+                        ${user}@${target}:${target_path}
+                """);
             }
         }
     }
 }
+
 return this;

dc396d228e06d870ae1eac3c9dfe3d4aa1486554
thl-cmk <thl-cmk@outlook.com>
2023-12-17 19:12:18 +0100

    improves Cisco Meraki device status
    This PR makes better use of the data we already have.
    
    - now also detects offline devices (could be made configurable via a detection rule)
    - adds upper levels/metrics to the last reported time
    - adds rule set for device status
    - adds check for power supplies
    - adds power supplies to the inventory
    
    Change-Id: Ie0fffa33aa68fbfc5e4d3b7f1ee68ba35ea92b4f

--- cmk/plugins/lib/cisco_meraki.py
+++ cmk/plugins/lib/cisco_meraki.py
@@ -7,6 +7,7 @@ import json
 import time
 from collections.abc import Mapping, Sequence
 
+from cmk.agent_based.v1 import check_levels
 from cmk.agent_based.v2 import CheckResult, render, Result, State, StringTable
 
 MerakiAPIData = Mapping[str, object]
@@ -19,7 +20,11 @@ def load_json(string_table: StringTable) -> Sequence[MerakiAPIData]:
         return []
 
 
-def check_last_reported_ts(last_reported_ts: float) -> CheckResult:
+def check_last_reported_ts(
+    last_reported_ts: float,
+    levels_upper: tuple[int, int] | None = None,
+    as_metric: bool = False,
+) -> CheckResult:
     if (age := time.time() - last_reported_ts) < 0:
         yield Result(
             state=State.OK,
@@ -27,7 +32,10 @@ def check_last_reported_ts(last_reported_ts: float) -> CheckResult:
         )
         return
 
-    yield Result(
-        state=State.OK,
-        summary=f"Time since last report: {render.timespan(age)}",
+    yield from check_levels(
+        value=age,
+        label="Time since last report",
+        metric_name="last_reported" if as_metric else None,
+        levels_upper=levels_upper,
+        render_func=render.timespan,
     )
--- tests/unit/cmk/base/plugins/agent_based/test_cisco_meraki_org_device_status.py
+++ tests/unit/cmk/base/plugins/agent_based/test_cisco_meraki_org_device_status.py
@@ -10,7 +10,7 @@ import pytest
 from tests.testlib import on_time
 
 from cmk.base.plugins.agent_based import cisco_meraki_org_device_status
-from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, Service, State
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Metric, Result, Service, State
 from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import StringTable
 
 _STRING_TABLE = [
@@ -57,7 +57,7 @@ _STRING_TABLE_OFFLINE = [
         ),
         (
             _STRING_TABLE_OFFLINE,
-            [],
+            [Service()],
         ),
     ],
 )
@@ -81,6 +81,7 @@ def test_discover_device_status(
             [
                 Result(state=State.OK, summary="Status: online"),
                 Result(state=State.OK, summary="Time since last report: 23 hours 59 minutes"),
+                Metric("last_reported", 86399.90979003906),
             ],
         ),
     ],
@@ -88,4 +89,12 @@ def test_discover_device_status(
 def test_check_device_status(string_table: StringTable, expected_results: Sequence[Result]) -> None:
     section = cisco_meraki_org_device_status.parse_device_status(string_table)
     with on_time("2000-01-15 00:00:00", "UTC"):
-        assert list(cisco_meraki_org_device_status.check_device_status(section)) == expected_results
+        assert (
+            list(
+                cisco_meraki_org_device_status.check_device_status(
+                    cisco_meraki_org_device_status.Parameters(),
+                    section,
+                )
+            )
+            == expected_results
+        )

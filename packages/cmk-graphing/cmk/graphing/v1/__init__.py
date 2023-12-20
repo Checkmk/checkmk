176b38ba1bde3b924ca4aca14e73cb11a6c8ce09
Simon Jess <simon.jess@checkmk.com>
2023-12-20 16:01:25 +0100

    Docs: Fix missing articles
    Change-Id: If1db6e3327b134d2de859e04371058a61802724b

--- packages/cmk-graphing/cmk/graphing/v1/__init__.py
+++ packages/cmk-graphing/cmk/graphing/v1/__init__.py
@@ -3,8 +3,20 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from . import graphs, metrics, perfometers, translations
 from ._color import Color
 from ._localize import Localizable
 from ._unit import PhysicalUnit, ScientificUnit, Unit
 
-__all__ = ["Color", "Localizable", "PhysicalUnit", "ScientificUnit", "Unit"]
+__all__ = [
+    # Do not change this order, it's important for Sphinx doc
+    "metrics",
+    "Localizable",
+    "Unit",
+    "PhysicalUnit",
+    "ScientificUnit",
+    "Color",
+    "perfometers",
+    "graphs",
+    "translations",
+]

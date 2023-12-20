3e50fcdd32167e39baadf89521bdd1f7ef9d5de6
Matteo Stifano <matteo.stifano@checkmk.com>
2023-12-20 13:58:16 +0100

    Fix not-defined parameter in test-update Jenkins job
    Change-Id: I6b49173e0b6a1deb4e0b4d8f7f4d7ddbbec77144

--- buildscripts/scripts/test-update.groovy
+++ buildscripts/scripts/test-update.groovy
@@ -20,16 +20,12 @@ def main() {
     def testing_helper = load("${checkout_dir}/buildscripts/scripts/utils/integration.groovy");
     def branch_version = versioning.get_branch_version(checkout_dir);
 
-    check_job_parameters([
-        "OVERRIDE_DISTROS",
-    ]);
-
     check_environment_variables([
         "DOCKER_TAG",
         "EDITION"
     ]);
 
-    def distros = versioning.configured_or_overridden_distros(EDITION, OVERRIDE_DISTROS);
+    def distros = versioning.configured_or_overridden_distros(EDITION, false);
     def make_target = build_make_target(EDITION);
 
     testing_helper.run_make_targets(

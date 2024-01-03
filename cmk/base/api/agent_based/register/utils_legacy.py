2439f87f084c9fe2ca56c5010ec6019a1af6a595
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-01-04 00:34:01 +0100

    improve typing of LegacyCheckDefinition
    Change-Id: I695077c365ddfc2ba2bb4c87790907edf8b53992

--- cmk/base/api/agent_based/register/utils_legacy.py
+++ cmk/base/api/agent_based/register/utils_legacy.py
@@ -4,21 +4,41 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from collections.abc import Callable, Iterable, Mapping
-from typing import Any, NotRequired, TypedDict
+from typing import Any, Generator, NotRequired, TypedDict
 
 from cmk.agent_based.v2 import Service, SNMPDetectSpecification, SNMPTree
+from cmk.agent_based.v2.type_defs import DiscoveryResult
 
 _DiscoveredParameters = Mapping | tuple | str | None
+
+
+_DiscoveryFunctionLegacy = Callable[..., None | Iterable[tuple[str | None, _DiscoveredParameters]]]
+_DiscoveryFunctionV2Compliant = Callable[..., DiscoveryResult]
+
+_OptNumber = None | int | float
+
+_MetricTuple = (
+    tuple[str, float]
+    | tuple[str, float, _OptNumber, _OptNumber]
+    | tuple[str, float, _OptNumber, _OptNumber, _OptNumber, _OptNumber]
+)
+
+_SingleResult = tuple[int, str] | tuple[int, str, list[_MetricTuple]]
+
+
+_CheckFunctionLegacy = Callable[
+    ...,
+    None | _SingleResult | Iterable[_SingleResult] | Generator[_SingleResult, None, None],
+]
+_CheckFunctionV2Compliant = Callable[..., Generator[Service, None, None]]
 
 
 class LegacyCheckDefinition(TypedDict):
     detect: NotRequired[SNMPDetectSpecification]
     fetch: NotRequired[list[SNMPTree] | SNMPTree]
     sections: NotRequired[list[str]]
-    check_function: NotRequired[Callable]
-    discovery_function: NotRequired[
-        Callable[..., None | Iterable[tuple[str | None, _DiscoveredParameters]] | Iterable[Service]]
-    ]
+    check_function: NotRequired[_CheckFunctionV2Compliant | _CheckFunctionLegacy]
+    discovery_function: NotRequired[_DiscoveryFunctionV2Compliant | _DiscoveryFunctionLegacy]
     parse_function: NotRequired[Callable[[list], object]]
     check_ruleset_name: NotRequired[str]
     check_default_parameters: NotRequired[Mapping[str, Any]]

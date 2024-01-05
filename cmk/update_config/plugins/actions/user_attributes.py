5a9097c08c026b5e4d91aa1e11af6692cf947f76
Kenneth Okoh <kenneth.okoh@checkmk.com>
2024-01-05 12:14:18 +0100

    Update config: remove deprecated language None
    CMK-13940
    
    Change-Id: I28838532f95a8b43fb53a9bcd8f7e6e492855d29

--- cmk/update_config/plugins/actions/user_attributes.py
+++ cmk/update_config/plugins/actions/user_attributes.py
@@ -6,6 +6,9 @@
 from datetime import datetime
 from logging import Logger
 
+from cmk.utils.log import VERBOSE
+
+from cmk.gui.type_defs import Users
 from cmk.gui.userdb import load_users, save_users
 
 from cmk.update_config.registry import update_action_registry, UpdateAction
@@ -14,7 +17,30 @@ from cmk.update_config.update_state import UpdateActionState
 
 class UpdateUserAttributes(UpdateAction):
     def __call__(self, logger: Logger, update_action_state: UpdateActionState) -> None:
-        save_users(load_users(lock=True), datetime.now())
+        users = load_users(lock=True)
+        save_users(
+            _remove_deprecated_language_none(logger, users),
+            datetime.now(),
+        )
+
+
+def _remove_deprecated_language_none(logger: Logger, users: Users) -> Users:
+    """
+    With version 2.2.0 we retyped user languages from None | str to str only.
+    This function removes language params set to None in existing user configs.
+    """
+    changed_user_specs: bool = False
+    for user_spec in users.values():
+        if user_spec.get("language", -1) is None:
+            changed_user_specs = True
+            del user_spec["language"]
+
+    if changed_user_specs:
+        logger.log(
+            VERBOSE,
+            "Removing deprecated user languages set to None. (The default remains English)",
+        )
+    return users
 
 
 update_action_registry.register(

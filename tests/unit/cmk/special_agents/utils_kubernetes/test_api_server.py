3eeddac5328460d5c55df13d90520ddd25cc6559
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-01-09 09:54:36 +0100

    KUBE: Addition of support for Kubernetes version 1.28
    CMK-14597
    
    Change-Id: Ib8a97c5539f460876a5b30d8721a648692872a21

--- tests/unit/cmk/special_agents/utils_kubernetes/test_api_server.py
+++ tests/unit/cmk/special_agents/utils_kubernetes/test_api_server.py
@@ -36,7 +36,7 @@ def _core_api() -> CoreAPI:
 
 
 CALL_API = "cmk.special_agents.utils_kubernetes.api_server.send_request"
-SUPPORTED_VERSION_STR = "Supported versions are v1.23, v1.24, v1.25, v1.26, v1.27."
+SUPPORTED_VERSION_STR = "Supported versions are v1.24, v1.25, v1.26, v1.27, v1.28."
 
 
 def test_raw_api_get_healthz_ok(core_api: CoreAPI) -> None:
@@ -279,17 +279,17 @@ def test_decompose_git_version(
             ],
         ),
         (
-            api.KubernetesVersion(git_version=api.GitVersion("v1.23.2"), major=1, minor=23),
+            api.KubernetesVersion(git_version=api.GitVersion("v1.24.2"), major=1, minor=24),
             [],
         ),
         (
-            api.KubernetesVersion(git_version=api.GitVersion("v1.24.0"), major=1, minor=24),
+            api.KubernetesVersion(git_version=api.GitVersion("v1.25.0"), major=1, minor=25),
             [],
         ),
         (
-            api.KubernetesVersion(git_version=api.GitVersion("v1.28.0"), major=1, minor=28),
+            api.KubernetesVersion(git_version=api.GitVersion("v1.29.0"), major=1, minor=29),
             [
-                "WARNING Unsupported Kubernetes version 'v1.28.0'. " + SUPPORTED_VERSION_STR,
+                "WARNING Unsupported Kubernetes version 'v1.29.0'. " + SUPPORTED_VERSION_STR,
                 "WARNING Processing data is done on a best effort basis.",
             ],
         ),

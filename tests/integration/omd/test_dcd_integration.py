ad2069d24d93f91e8cb653dc41ef8980fc27e687
Joerg Herbel <joerg.herbel@checkmk.com>
2024-01-11 16:48:59 +0100

    Integration tests: Use markers from testlib for edition-dep. skipping
    Change-Id: Ib7c367de1d61176966cb99443ba8fac75e568b36

--- tests/integration/omd/test_dcd_integration.py
+++ tests/integration/omd/test_dcd_integration.py
@@ -5,37 +5,36 @@
 
 import subprocess
 
-import pytest
-
+from tests.testlib.pytest_helpers.marks import skip_if_raw_edition
 from tests.testlib.site import Site
 
 
-@pytest.mark.usefixtures("skip_in_raw_edition")
+@skip_if_raw_edition
 def test_dcd_exists(site: Site) -> None:
     assert site.file_exists("bin/dcd")
 
 
-@pytest.mark.usefixtures("skip_in_raw_edition")
+@skip_if_raw_edition
 def test_dcd_path(site: Site) -> None:
     p = site.execute(["which", "dcd"], stdout=subprocess.PIPE)
     path = p.stdout.read().strip() if p.stdout else "<NO STDOUT>"
     assert path == "/omd/sites/%s/bin/dcd" % site.id
 
 
-@pytest.mark.usefixtures("skip_in_raw_edition")
+@skip_if_raw_edition
 def test_dcd_version(site: Site) -> None:
     p = site.execute(["dcd", "-V"], stdout=subprocess.PIPE)
     version = p.stdout.read() if p.stdout else "<NO STDOUT>"
     assert version.startswith("dcd version %s" % site.version.version)
 
 
-@pytest.mark.usefixtures("skip_in_raw_edition")
+@skip_if_raw_edition
 def test_dcd_daemon(site: Site) -> None:
     p = site.execute(["omd", "status", "--bare", "dcd"], stdout=subprocess.PIPE)
     assert p.wait() == 0
     assert p.stdout.read() == "dcd 0\nOVERALL 0\n" if p.stdout else False
 
 
-@pytest.mark.usefixtures("skip_in_raw_edition")
+@skip_if_raw_edition
 def test_dcd_logrotate(site: Site) -> None:
     assert site.file_exists("etc/logrotate.d/dcd")

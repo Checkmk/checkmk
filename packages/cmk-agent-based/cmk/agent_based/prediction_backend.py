cec2c30b13e3d94e2ce0813054a09999112ec4f7
Sofia Colakovic <sofia.colakovic@checkmk.com>
2024-01-12 10:38:59 +0100

    agent_based v2: implement check_levels
    Change-Id: Iaef8caf07448f3b668bbea32ca9c0865b9f4bb1a

--- packages/cmk-agent-based/cmk/agent_based/prediction_backend.py
+++ packages/cmk-agent-based/cmk/agent_based/prediction_backend.py
@@ -4,11 +4,16 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import time
+from collections.abc import Mapping
 from typing import Literal, Self
 
 from pydantic import BaseModel
 
 _LevelsSpec = tuple[Literal["absolute", "relative", "stdev"], tuple[float, float]]
+
+_Direction = Literal["upper", "lower"]
+_Prediction = float
+_EstimatedLevels = tuple[float, float]
 
 _ONE_DAY = 24 * 3600
 
@@ -19,6 +24,11 @@ class PredictionParameters(BaseModel, frozen=True):  # type: ignore[misc]  # hid
     levels_upper: _LevelsSpec | None = None
     levels_upper_min: tuple[float, float] | None = None
     levels_lower: _LevelsSpec | None = None
+
+
+class InjectedParameters(BaseModel, frozen=True):  # type: ignore[misc]  # hidden Any
+    meta_file_path_template: str
+    predictions: Mapping[int, tuple[_Prediction, _EstimatedLevels] | None]
 
 
 class PredictionInfo(BaseModel, frozen=True):  # type: ignore[misc]  # hidden Any
@@ -45,3 +55,12 @@ def _start_of_day(timestamp: float) -> int:
     t = time.localtime(timestamp)
     sec_of_day = t.tm_hour * 3600 + t.tm_min * 60 + t.tm_sec
     return int(timestamp) - sec_of_day
+
+
+def lookup_predictive_levels(  # type: ignore[empty-body]  # remove after implementing
+    _metric: str,
+    _direction: _Direction,
+    _parameters: PredictionParameters,
+    _injected: InjectedParameters,
+) -> tuple[_Prediction | None, _EstimatedLevels | None]:
+    ...

4a05bec453fbca281e55e39cc3f862eba5ec698c
Gav <gavin.mcguigan@checkmk.com>
2024-01-17 12:32:04 +0100

    16384 FIX notification rule: allow for non builtin service levels
    When configuring a notification rule via the Rest API, you could not
    set the value for "match_service_levels" to anything but the default
    service levels.  This werk addresses this issue by now allowing any
    of the service levels configured to be used.  This change mean that
    there is a change to the request schema. Previously, we accepeted
    the service level string value, whereas now we accept the integer
    value.
    
    Previous schema
    C+:
    {"match_service_levels": {"state": "enabled", "value": {"from_level": "silver", "to_level": "gold"}}}
    C-:
    
    New schema
    C+:
    {"match_service_levels: {"state": "enabled", "value": {"from_level" 10, "to_level": 20}}}
    C-:
    
    SUP-17080
    
    Change-Id: I2661d03f48e699054b89291f2b94affa10841910

--- cmk/gui/fields/custom_fields.py
+++ cmk/gui/fields/custom_fields.py
@@ -10,6 +10,7 @@ from marshmallow import validate, ValidationError
 
 from cmk.utils.tags import BuiltinTagConfig, TagID
 
+from cmk.gui.config import active_config
 from cmk.gui.groups import load_contact_group_information
 from cmk.gui.plugins.userdb import utils
 from cmk.gui.watolib.password_store import PasswordStore
@@ -422,3 +423,35 @@ class PasswordStoreIDField(fields.String):
         if self.presence == "should_not_exist":
             if value in pw_ids:
                 raise self.make_error("should_not_exist", store_id=value)
+
+
+class ServiceLevelField(fields.Integer):
+    default_error_messages = {
+        "should_exist": "The provided service level {value!r} does not exist. The available service levels are [{choices!r}]",
+        "should_not_exist": "The provided service level {value!r} already exists.]",
+    }
+
+    def __init__(
+        self,
+        required: bool = True,
+        example: int = 10,
+        presence: Literal["should_exist", "should_not_exist"] = "should_exist",
+        description: str = "A service level represented as an integer",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(required=required, example=example, description=description, **kwargs)
+        self.presence = presence
+
+    def _validate(self, value):
+        super()._validate(value)
+        choices = [int_val for int_val, _str_val in active_config.mkeventd_service_levels]
+
+        if self.presence == "should_exist":
+            if value not in choices:
+                raise self.make_error(
+                    "should_exist", value=value, choices=", ".join([str(c) for c in choices])
+                )
+
+        if self.presence == "should_not_exist":
+            if value in choices:
+                raise self.make_error("should_not_exist", value=value)

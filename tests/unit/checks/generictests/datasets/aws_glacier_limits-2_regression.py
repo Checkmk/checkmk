164671930eb2d015ed14c36f3464e5ea043324a2
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-01-18 15:35:25 +0100

    nuke statemarker usages
    Change-Id: Id51a1da7aa1a5ba9cbc9841ac528243ffa39e39c

--- tests/unit/checks/generictests/datasets/aws_glacier_limits-2_regression.py
+++ tests/unit/checks/generictests/datasets/aws_glacier_limits-2_regression.py
@@ -46,7 +46,7 @@ checks = {
                     "Levels reached: Vaults",
                     [("aws_glacier_number_of_vaults", 910, None, None, None, None)],
                 ),
-                (0, "\nVaults: 910 (of max. 1000), Usage: 91.00% (warn/crit at 80.00%/90.00%)(!!)"),
+                (2, "\nVaults: 910 (of max. 1000), Usage: 91.00% (warn/crit at 80.00%/90.00%)"),
             ],
         ),
         (
@@ -71,8 +71,8 @@ checks = {
                     [("aws_glacier_number_of_vaults", 1001, None, None, None, None)],
                 ),
                 (
-                    0,
-                    "\nVaults: 1001 (of max. 1000), Usage: 100.10% (warn/crit at 80.00%/90.00%)(!!)",
+                    2,
+                    "\nVaults: 1001 (of max. 1000), Usage: 100.10% (warn/crit at 80.00%/90.00%)",
                 ),
             ],
         ),

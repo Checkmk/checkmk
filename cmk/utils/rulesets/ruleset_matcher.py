Checkmk/checkmk

commit 2f9304a9fe0b5dbe5b6b84caff0c712ee20566c6

Move more MatchObject pessimization to matcher
where it is used

CMK-14467

Change-Id: Ie7d26ef29f64f169b63c730f279fece4f511bc71

cmk/utils/rulesets/ruleset_matcher.py
@@ -24,7 +24,7 @@ from cmk.utils.rulesets.tuple_rulesets import (
     NEGATE,
     PHYSICAL_HOSTS,
 )
-from cmk.utils.servicename import ServiceName
+from cmk.utils.servicename import Item, ServiceName
 from cmk.utils.tags import TagConfig, TagGroupID, TagID
 
 from .conditions import HostOrServiceConditions, HostOrServiceConditionsSimple
@@ -207,7 +207,9 @@ class RulesetMatcher:
             object,
         ] = {}
         # Expensive and mostly useless caching.
-        self.__service_match_obj: dict[tuple[HostName, ServiceName], RulesetMatchObject] = {}
+        self.__service_match_obj: dict[
+            tuple[HostName, ServiceName, Item | None], RulesetMatchObject
+        ] = {}
 
     def get_host_bool_value(self, hostname: HostName, ruleset: Iterable[RuleSpec[bool]]) -> bool:
         """Compute outcome of a ruleset set that just says yes/no
@@ -265,14 +267,21 @@ class RulesetMatcher:
     def cache_service_labels(
         self, hostname: HostName, description: ServiceName, labels: Labels
     ) -> None:
-        cache_id = (hostname, description)
+        cache_id = (hostname, description, None)
+        if cache_id not in self.__service_match_obj:
+            self.__service_match_obj[cache_id] = RulesetMatchObject(hostname, description, labels)
+
+    def cache_service_checkgroup(
+        self, hostname: HostName, description: ServiceName, item: Item, labels: Labels
+    ) -> None:
+        cache_id = (hostname, description, item)
         if cache_id not in self.__service_match_obj:
             self.__service_match_obj[cache_id] = RulesetMatchObject(hostname, description, labels)
 
     def _service_match_object(
         self, hostname: HostName, description: ServiceName
     ) -> RulesetMatchObject:
-        cache_id = (hostname, description)
+        cache_id = (hostname, description, None)
         with contextlib.suppress(KeyError):
             return self.__service_match_obj[cache_id]
 
@@ -281,6 +290,18 @@ class RulesetMatcher:
             RulesetMatchObject(
                 hostname, description, self.labels_of_service(hostname, description)
             ),
+        )
+
+    def _checkgroup_match_object(
+        self, hostname: HostName, description: ServiceName, item: Item
+    ) -> RulesetMatchObject:
+        cache_id = (hostname, description, item)
+        with contextlib.suppress(KeyError):
+            return self.__service_match_obj[cache_id]
+
+        return self.__service_match_obj.setdefault(
+            cache_id,
+            RulesetMatchObject(hostname, item, self.labels_of_service(hostname, description)),
         )
 
     def get_service_bool_value(

cmk/automations/results.py
@@ -22,9 +22,9 @@ from cmk.utils.labels import HostLabel, HostLabelValueDict, Labels
 from cmk.utils.notify_types import NotifyAnalysisInfo, NotifyBulks
 from cmk.utils.plugin_registry import Registry
 from cmk.utils.rulesets.ruleset_matcher import LabelSources, RulesetName
-from cmk.utils.servicename import ServiceName
+from cmk.utils.servicename import Item, ServiceName
 
-from cmk.checkengine.checking import CheckPluginNameStr, Item
+from cmk.checkengine.checking import CheckPluginNameStr
 from cmk.checkengine.discovery import CheckPreviewEntry
 from cmk.checkengine.discovery import DiscoveryResult as SingleHostDiscoveryResult
 from cmk.checkengine.legacy import LegacyCheckParameters

cmk/base/api/agent_based/value_store/_utils.py
@@ -23,8 +23,9 @@ import cmk.utils.store as store
 from cmk.utils.exceptions import MKGeneralException
 from cmk.utils.hostaddress import HostName
 from cmk.utils.log import logger
+from cmk.utils.servicename import Item
 
-from cmk.checkengine.checking import CheckPluginName, Item, ServiceID
+from cmk.checkengine.checking import CheckPluginName, ServiceID
 
 _PluginName = str
 _UserKey = str

cmk/base/config.py
@@ -85,12 +85,11 @@ from cmk.utils.rulesets.ruleset_matcher import (
     LabelManager,
     LabelSources,
     RulesetMatcher,
-    RulesetMatchObject,
     RulesetName,
     RuleSpec,
 )
 from cmk.utils.sectionname import SectionName
-from cmk.utils.servicename import ServiceName
+from cmk.utils.servicename import Item, ServiceName
 from cmk.utils.site import omd_site
 from cmk.utils.store.host_storage import (
     apply_hosts_file_to_object,
@@ -126,7 +125,6 @@ from cmk.checkengine.checking import (
     CheckPluginName,
     CheckPluginNameStr,
     ConfiguredService,
-    Item,
     ServiceID,
 )
 from cmk.checkengine.discovery import (
@@ -1990,9 +1988,7 @@ def _get_checkgroup_parameters(
             return config_cache.ruleset_matcher.get_host_values(host, rules)
 
         # checks with an item need service-specific rules
-        match_object = config_cache.ruleset_match_object_for_checkgroup_parameters(
-            host, item, descr
-        )
+        match_object = config_cache.ruleset_matcher._checkgroup_match_object(host, descr, item)
         return list(
             config_cache.ruleset_matcher.get_service_ruleset_values(
                 match_object, rules, is_binary=False
@@ -2161,12 +2157,7 @@ class ConfigCache:
 
         self._cache_section_name_of: dict[CheckPluginNameStr, str] = {}
 
-        self._cache_match_object_service_checkgroup: dict[
-            tuple[HostName, Item, ServiceName], RulesetMatchObject
-        ] = {}
-
         # Host lookup
-
         self._all_configured_hosts = set()
         self._all_configured_clusters = set()
         self._all_configured_realhosts = set()
@@ -3561,39 +3552,6 @@ class ConfigCache:
             return explicit_service_custom_variables[(hostname, description)]
         except KeyError:
             return {}
-
-    def cache_ruleset_match_object_for_checkgroup_parameters(
-        self, hostname: HostName, item: Item, svc_desc: ServiceName, labels: Labels
-    ) -> None:
-        cache_id = (hostname, item, svc_desc)
-        if cache_id not in self._cache_match_object_service_checkgroup:
-            self._cache_match_object_service_checkgroup[cache_id] = RulesetMatchObject(
-                hostname, svc_desc, labels
-            )
-
-    def ruleset_match_object_for_checkgroup_parameters(
-        self, hostname: HostName, item: Item, svc_desc: ServiceName
-    ) -> RulesetMatchObject:
-        """Construct the object that is needed to match checkgroup parameters rulesets
-
-        Please note that the host attributes like host_folder and host_tags are
-        not set in the object, because the rule optimizer already processes all
-        these host conditions. Adding these attributes here would be
-        consequent, but create some overhead.
-        """
-
-        cache_id = (hostname, item, svc_desc)
-        with contextlib.suppress(KeyError):
-            return self._cache_match_object_service_checkgroup[cache_id]
-
-        return self._cache_match_object_service_checkgroup.setdefault(
-            cache_id,
-            RulesetMatchObject(
-                host_name=hostname,
-                service_description=item,
-                service_labels=self.ruleset_matcher.labels_of_service(hostname, svc_desc),
-            ),
-        )
 
     def get_autochecks_of(self, hostname: HostName) -> Sequence[ConfiguredService]:
         return self._autochecks_manager.get_autochecks_of(

cmk/base/core_config.py
@@ -25,10 +25,10 @@ from cmk.utils.labels import Labels
 from cmk.utils.licensing.handler import LicenseState, LicensingHandler
 from cmk.utils.licensing.helper import get_licensed_state_file_path, write_licensed_state
 from cmk.utils.paths import core_helper_config_dir
-from cmk.utils.servicename import ServiceName
+from cmk.utils.servicename import Item, ServiceName
 from cmk.utils.store import load_object_from_file, lock_checkmk_configuration, save_object_to_file
 
-from cmk.checkengine.checking import CheckPluginName, ConfiguredService, Item, ServiceID
+from cmk.checkengine.checking import CheckPluginName, ConfiguredService, ServiceID
 from cmk.checkengine.parameters import TimespecificParameters
 
 import cmk.base.api.agent_based.register as agent_based_register

cmk/base/export.py
@@ -12,9 +12,9 @@ from the configuration.
 from cmk.utils.hostaddress import HostName
 from cmk.utils.labels import Labels
 from cmk.utils.rulesets.ruleset_matcher import RulesetMatcher, RulesetMatchObject
-from cmk.utils.servicename import ServiceName
+from cmk.utils.servicename import Item, ServiceName
 
-from cmk.checkengine.checking import CheckPluginName, CheckPluginNameStr, Item
+from cmk.checkengine.checking import CheckPluginName, CheckPluginNameStr
 
 import cmk.base.config as config
 
@@ -62,11 +62,9 @@ def ruleset_match_object_for_checkgroup_parameters(
 ) -> RulesetMatchObject:
     """Construct the object that is needed to match checkgroup parameter rulesets"""
     _load_config()
-    config_cache = config.get_config_cache()
-    config_cache.cache_ruleset_match_object_for_checkgroup_parameters(
-        hostname, item, svc_desc, svc_labels
-    )
-    return config_cache.ruleset_match_object_for_checkgroup_parameters(hostname, item, svc_desc)
+    matcher = config.get_config_cache().ruleset_matcher
+    matcher.cache_service_checkgroup(hostname, svc_desc, item, svc_labels)
+    return matcher._checkgroup_match_object(hostname, svc_desc, item)
 
 
 def get_host_labels(hostname: HostName) -> Labels:

cmk/checkengine/checking/__init__.py
@@ -10,7 +10,6 @@ from ._plugin import (
     CheckPluginName,
     CheckPluginNameStr,
     ConfiguredService,
-    Item,
     ServiceID,
 )
 from ._timing import make_timing_results
@@ -23,7 +22,6 @@ __all__ = [
     "CheckPluginNameStr",
     "ConfiguredService",
     "execute_checkmk_checks",
-    "Item",
     "make_timing_results",
     "ServiceID",
 ]

cmk/checkengine/checking/_plugin.py
@@ -12,7 +12,7 @@ from typing import Final, NamedTuple, Protocol
 from cmk.utils.hostaddress import HostName
 from cmk.utils.labels import ServiceLabel
 from cmk.utils.rulesets import RuleSetName
-from cmk.utils.servicename import ServiceName
+from cmk.utils.servicename import Item, ServiceName
 from cmk.utils.validatedstr import ValidatedString
 
 from cmk.checkengine.checkresults import ServiceCheckResult
@@ -27,13 +27,11 @@ __all__ = [
     "CheckPluginName",
     "CheckPluginNameStr",
     "ConfiguredService",
-    "Item",
     "ServiceID",
 ]
 
 
 CheckPluginNameStr = str
-Item = str | None
 
 
 class CheckPluginName(ValidatedString):

commit 9bb1589cbe934bb7e8bc31ce669e88a87f7be94b

Fix RulesetMatchObject for checkgroups
2f9304a9fe0b5dbe5b6b84caff0c712ee20566c6 broke it

CMK-15593

Change-Id: Ie5aecb9b289bb202ddd5a24649a3ac6a4e986cfd

cmk/utils/rulesets/ruleset_matcher.py
@@ -121,9 +121,12 @@ class LabelManager(NamedTuple):
 
 @dataclasses.dataclass(frozen=True, slots=True)
 class RulesetMatchObject:
-    # TODO: Get rid of this.  Or at least, make it private to this module.
     host_name: HostName | HostAddress
-    service_description: ServiceName | None = None
+    # The next field is:
+    #   * None to match hosts
+    #   * ServiceName to match services
+    #   * Item to match checkgroups
+    service_description: ServiceName | Item | None = None
     service_labels: Labels | None = None
 
 
@@ -232,7 +235,7 @@ class RulesetMatcher:
     ) -> None:
         cache_id = (hostname, description, item)
         if cache_id not in self.__service_match_obj:
-            self.__service_match_obj[cache_id] = RulesetMatchObject(hostname, description, labels)
+            self.__service_match_obj[cache_id] = RulesetMatchObject(hostname, item, labels)
 
     def _service_match_object(
         self, hostname: HostName, description: ServiceName
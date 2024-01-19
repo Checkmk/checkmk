9f4fc6b304b699edfd0f2e7f7ae47dad31696f3c
Lars Michelsen <lm@checkmk.com>
2024-01-19 13:13:27 +0100

    Extend #15721 to fix files created by previous 2.2.0 versions
    Change-Id: I9031ed0113ddcf7cceea21c95a6f3d2fb28c1c73

--- omd/packages/omd/omdlib/main.py
+++ omd/packages/omd/omdlib/main.py
@@ -922,10 +922,15 @@ def update_file(  # pylint: disable=too-many-branches
         "###EDITION###": new_edition,
     }
 
-    old_replacements = {
-        **site.replacements,
-        "###EDITION###": old_edition,
-    }
+    old_replacements = _patch_livestatus_nagios_cfg_replacements(
+        relpath,
+        old_edition,
+        site.dir,
+        {
+            **site.replacements,
+            "###EDITION###": old_edition,
+        },
+    )
 
     old_path = old_skel + "/" + relpath
     new_path = new_skel + "/" + relpath
@@ -1261,6 +1266,28 @@ def update_file(  # pylint: disable=too-many-branches
                 StateMarkers.error
                 + f" Permission:    cannot change {user_perm:04o} -> {new_perm:04o} {fn}: {e}\n"
             )
+
+
+def _patch_livestatus_nagios_cfg_replacements(
+    relpath: str, old_edition: str, site_dir: str, replacements: Replacements
+) -> Replacements:
+    """Patch replacements for mk-livestatus.cfg to make transition from 2.2 sites work
+
+    Previously "edition=raw" was written into the mk-livestatus.cfg for all sites which were
+    created with 2.2.0 or newer, independent of the actual used edition. Sites updated from 2.1.0
+    are not affected. This was due to the fact that the edition detection was broken during site
+    creation. The previous mechanism always reported 'raw' instead of the correct edition. We now
+    specifically detect this situation and fix it. See also #15721.
+
+    This can be removed in 2.4.
+    """
+    if (
+        relpath == "etc/mk-livestatus/nagios.cfg"
+        and old_edition != "raw"
+        and "edition=raw" in Path(site_dir, "etc/mk-livestatus/nagios.cfg").read_text()
+    ):
+        return {**replacements, "###EDITION###": "raw"}
+    return replacements
 
 
 def permission_action(

c890df3ce8cacd5d07aab3f5b5be5f92b9b858d3
Lars Michelsen <lm@checkmk.com>
2024-01-19 20:00:46 +0100

    Cleanup: Get rid of read_skel_permissions
    The function was caching once loaded permissions on module scope. By propagating
    the loaded permissions through the calls stack, we can prevent loading the
    skell permissions too often without such caching approach.
    
    Change-Id: I55d76ce0f870107d9f83d8698e251e9463587d2e

--- tests/unit/omdlib/test_omdlib_skel_permissions.py
+++ tests/unit/omdlib/test_omdlib_skel_permissions.py
@@ -3,18 +3,12 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# pylint: disable=redefined-outer-name
+from pathlib import Path
 
-import omdlib.main
-import omdlib.skel_permissions
+from omdlib.skel_permissions import load_skel_permissions_from
 
 
-def test_read_skel_permissions(monkeypatch, tmp_path) -> None:  # type:ignore[no-untyped-def]
+def test_load_skel_permissions_from(tmp_path: Path) -> None:
     pfile = tmp_path / "skel.permissions"
     pfile.open("w", encoding="utf-8").write("bla 755\nblub 644\n")
-
-    monkeypatch.setattr(
-        omdlib.skel_permissions, "skel_permissions_file_path", lambda v: "%s" % (pfile)
-    )
-
-    assert omdlib.main.read_skel_permissions() == {"bla": 493, "blub": 420}
+    assert load_skel_permissions_from(str(pfile)) == {"bla": 493, "blub": 420}

0974ce007eda1c8a92a075a52d828ba499936433
Lars Michelsen <lm@checkmk.com>
2024-01-19 20:00:46 +0100

    Cleanup: Get rid of read_skel_permissions
    The function was caching once loaded permissions on module scope. By propagating
    the loaded permissions through the calls stack, we can prevent loading the
    skell permissions too often without such caching approach.
    
    Change-Id: I55d76ce0f870107d9f83d8698e251e9463587d2e

--- omd/packages/omd/omdlib/skel_permissions.py
+++ omd/packages/omd/omdlib/skel_permissions.py
@@ -6,31 +6,7 @@
 """Deal with file owners, permissions and the the skel hierarchy"""
 
 
-import omdlib
-
 Permissions = dict[str, int]
-
-_skel_permissions: Permissions = {}
-
-
-def read_skel_permissions() -> Permissions:
-    """Returns a permission map for the skel files
-    This runtime cache is important, because the function is called very often while processing
-    the skel hierarchy."""
-    global _skel_permissions
-    if _skel_permissions:
-        return _skel_permissions
-
-    _skel_permissions = load_skel_permissions(omdlib.__version__)
-    if not _skel_permissions:
-        raise Exception(
-            "%s is missing or currupted." % skel_permissions_file_path(omdlib.__version__)
-        )
-    return _skel_permissions
-
-
-def load_skel_permissions(version: str) -> Permissions:
-    return load_skel_permissions_from(skel_permissions_file_path(version))
 
 
 def load_skel_permissions_from(path: str) -> Permissions:

3f88e20898c56656e092138c956aa4a664cfb236
Simon Jess <simon.jess@checkmk.com>
2024-01-22 14:00:51 +0100

    Separate subscription details used in aggregation from verification
    Change-Id: I0495648cdc85913bf1379766381d6f4dc743b6c4

--- cmk/utils/licensing/export.py
+++ cmk/utils/licensing/export.py
@@ -11,7 +11,7 @@ from collections.abc import Mapping, Sequence
 from dataclasses import dataclass
 from datetime import datetime
 from enum import auto, Enum
-from typing import Final, Protocol
+from typing import Final, Literal, Protocol
 from uuid import UUID
 
 from dateutil.relativedelta import relativedelta
@@ -608,6 +608,30 @@ class LicenseUsageSample:
 #   '----------------------------------------------------------------------'
 
 
+class RawSubscriptionDetailsForAggregation(TypedDict):
+    start: int | None
+    end: int | None
+    limit: Literal["unlimited"] | int | None
+
+
+@dataclass(frozen=True)
+class SubscriptionDetailsForAggregation:
+    start: int | None
+    end: int | None
+    limit: Literal["unlimited"] | int | None
+
+    def __post_init__(self) -> None:
+        if isinstance(self.limit, int) and self.limit <= 0:
+            raise ValueError(self.limit)
+
+    def for_report(self) -> RawSubscriptionDetailsForAggregation:
+        return RawSubscriptionDetailsForAggregation(
+            start=self.start,
+            end=self.end,
+            limit=self.limit,
+        )
+
+
 @dataclass(frozen=True)
 class MonthlyServiceAverage:
     sample_date: datetime
@@ -622,14 +646,12 @@ class MonthlyServiceAverage:
 
 
 class RawMonthlyServiceAggregation(TypedDict):
+    subscription_details: RawSubscriptionDetailsForAggregation
     daily_services: Sequence[Mapping[str, float]]
     monthly_service_averages: Sequence[Mapping[str, float]]
     last_service_report: Mapping[str, float] | None
     highest_service_report: Mapping[str, float] | None
     subscription_exceeded_first: Mapping[str, float] | None
-    subscription_start: float | int | None
-    subscription_end: float | int | None
-    subscription_limit: int | None
 
 
 class MonthlyServiceAverages:
@@ -637,17 +659,10 @@ class MonthlyServiceAverages:
 
     def __init__(
         self,
-        subscription_details: SubscriptionDetails | None,
+        subscription_details: SubscriptionDetailsForAggregation,
         short_samples: Sequence[tuple[int, int]],
     ) -> None:
-        self._subscription_start = (
-            None if subscription_details is None else subscription_details.start
-        )
-        self._subscription_end = None if subscription_details is None else subscription_details.end
-        self._subscription_limit_value = (
-            None if subscription_details is None else subscription_details.limit.value
-        )
-
+        self._subscription_details = subscription_details
         self._daily_services = self._calculate_daily_services(short_samples)
         self._monthly_service_averages: list[MonthlyServiceAverage] = []
 
@@ -676,34 +691,32 @@ class MonthlyServiceAverages:
         "This method prepares the following data for javascript rendering"
         self._calculate_averages()
         return RawMonthlyServiceAggregation(
+            subscription_details=self._subscription_details.for_report(),
             daily_services=[d.for_report() for d in self._daily_services],
             monthly_service_averages=[a.for_report() for a in self._monthly_service_averages],
             last_service_report=self._get_last_service_report(),
             highest_service_report=self._get_highest_service_report(),
             subscription_exceeded_first=self._get_subscription_exceeded_first(),
-            subscription_start=self._subscription_start,
-            subscription_end=self._subscription_end,
-            subscription_limit=self._subscription_limit_value,
         )
 
     def _calculate_averages(self) -> None:
         if not self._daily_services:
             return
 
-        if self._subscription_start is None or self._subscription_end is None:
+        if self._subscription_details.start is None or self._subscription_details.end is None:
             # It does not make sense to calculate monthly averages if we do not know where to
             # start or end.
             return
 
         monthly_services: dict[datetime, Counter[str]] = {}
-        month_start = datetime.fromtimestamp(self._subscription_start).replace(
+        month_start = datetime.fromtimestamp(self._subscription_details.start).replace(
             hour=0,
             minute=0,
             second=0,
             microsecond=0,
         )
         month_end = month_start + relativedelta(months=+1)
-        subscription_end_date = datetime.fromtimestamp(self._subscription_end).replace(
+        subscription_end_date = datetime.fromtimestamp(self._subscription_details.end).replace(
             hour=0,
             minute=0,
             second=0,
@@ -744,12 +757,12 @@ class MonthlyServiceAverages:
         return max(self._monthly_service_averages, key=lambda d: d.num_services).for_report()
 
     def _get_subscription_exceeded_first(self) -> Mapping[str, float] | None:
-        if self._subscription_limit_value is None or self._subscription_limit_value < 0:
+        if (
+            self._subscription_details.limit == "unlimited"
+            or self._subscription_details.limit is None
+        ):
             return None
         for service_average in self._monthly_service_averages:
-            if service_average.num_services >= self._subscription_limit_value:
+            if service_average.num_services >= self._subscription_details.limit:
                 return service_average.for_report()
         return None
-
-
-# .

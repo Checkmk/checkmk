7ccfec5ea53b675b81192add1a41dd724098e9ad
Simon Jess <simon.jess@checkmk.com>
2024-01-23 15:43:06 +0100

    EC: Fix missing cleanup action
    Change-Id: Ic28c6f9f6003c3e8202ece709572650cbf16a3d8

--- tests/unit/cmk/ec/test_ec_config.py
+++ tests/unit/cmk/ec/test_ec_config.py
@@ -12,15 +12,44 @@ def test_save_active_config(settings: Settings) -> None:
     (settings.paths.config_dir.value / "wato").mkdir(parents=True, exist_ok=True)
     (settings.paths.config_dir.value / "wato/global.mk").touch()
     (settings.paths.config_dir.value / "wato/rules.mk").touch()
+
+    settings.paths.active_config_dir.value.mkdir(parents=True, exist_ok=True)
+    (settings.paths.active_config_dir.value / "old-rules.mk").touch()
+
     save_active_config(settings, [])
-    assert sorted(settings.paths.config_dir.value.glob("**/*.mk")) == (
+
+    assert sorted(settings.paths.config_dir.value.glob("**/*.mk")) == sorted(
         [
             settings.paths.config_dir.value / "my-config.mk",
             settings.paths.config_dir.value / "wato/global.mk",
             settings.paths.config_dir.value / "wato/rules.mk",
         ]
     )
-    assert sorted(settings.paths.active_config_dir.value.glob("**/*.mk")) == (
+    assert sorted(settings.paths.active_config_dir.value.glob("**/*.mk")) == sorted(
+        [
+            settings.paths.active_config_dir.value / "my-config.mk",
+            settings.paths.active_config_dir.value / "wato/global.mk",
+            settings.paths.active_config_dir.value / "wato/rules.mk",
+        ]
+    )
+
+
+def test_save_active_config_no_active_config_dir(settings: Settings) -> None:
+    (settings.paths.config_dir.value / "my-config.mk").touch()
+    (settings.paths.config_dir.value / "wato").mkdir(parents=True, exist_ok=True)
+    (settings.paths.config_dir.value / "wato/global.mk").touch()
+    (settings.paths.config_dir.value / "wato/rules.mk").touch()
+
+    save_active_config(settings, [])
+
+    assert sorted(settings.paths.config_dir.value.glob("**/*.mk")) == sorted(
+        [
+            settings.paths.config_dir.value / "my-config.mk",
+            settings.paths.config_dir.value / "wato/global.mk",
+            settings.paths.config_dir.value / "wato/rules.mk",
+        ]
+    )
+    assert sorted(settings.paths.active_config_dir.value.glob("**/*.mk")) == sorted(
         [
             settings.paths.active_config_dir.value / "my-config.mk",
             settings.paths.active_config_dir.value / "wato/global.mk",

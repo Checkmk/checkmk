96fecb70424c33767907c0eed6b9edc82e857d7c
Andreas Umbreit <andreas.umbreit@checkmk.com>
2024-01-23 13:47:59 +0100

    check_http: Parse httpv2 rule in server side call plugin
    Parse entire rule with Pydantic.
    Standard/indivdual settings are not in use yet.
    
    Change-Id: I448f8483eacdff1b0e22656f204f24386193b059

--- cmk/plugins/collection/server_side_calls/httpv2.py
+++ cmk/plugins/collection/server_side_calls/httpv2.py
@@ -2,44 +2,234 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Iterator, Mapping, Sequence
+from enum import StrEnum
+from typing import Iterator, Literal, Mapping
 
 from pydantic import BaseModel
 
 from cmk.server_side_calls.v1 import ActiveCheckCommand, ActiveCheckConfig, HostConfig, HTTPProxy
 
 
-class Endpoint(BaseModel):
-    name: str
-    protocol_prefix: bool
-    url: str
+class HttpVersion(StrEnum):
+    AUTO = "auto"
+    HTTP_2 = "http_2"
+    HTTP_1_1 = "http_1_1"
 
 
-HTTPParams = Sequence[Endpoint]
+class TlsVersion(StrEnum):
+    AUTO = "auto"
+    TLS_1_0 = "tls_1_0"
+    TLS_1_1 = "tls_1_1"
+    TLS_1_2 = "tls_1_2"
+    TLS_1_3 = "tls_1_3"
 
 
-def parse_http_params(raw_params: Mapping[str, object]) -> HTTPParams:
-    assert isinstance(raw_params["endpoints"], list)
-    return [
-        Endpoint.model_validate(
-            {
-                "name": endpoint["service_name"]["name"],
-                "protocol_prefix": endpoint["service_name"]["prefix"] == "auto",
-                "url": endpoint["url"],
-            }
-        )
-        for endpoint in raw_params["endpoints"]
+class RedirectPolicy(StrEnum):
+    OK = "ok"
+    WARNING = "warning"
+    CRITICAL = "critical"
+    FOLLOW = "follow"
+    STICKY = "sticky"
+    STICKYPORT = "stickyport"
+
+
+class DocumentBodyOption(StrEnum):
+    FETCH = "fetch"
+    IGNORE = "ignore"
+
+
+class LevelsType(StrEnum):
+    NO_LEVELS = "no_levels"
+    FIXED = "fixed"
+
+
+class Validation(StrEnum):
+    NO_VALIDATION = "no_validation"
+    VALIDATE = "validate"
+
+
+class HttpMethod(StrEnum):
+    GET = "get"
+    POST = "post"
+    PUT = "put"
+    DELETE = "delete"
+    OPTIONS = "options"
+    TRACE = "trace"
+    HEAD = "head"
+    CONNECT = "connect"
+    CONNECT_POST = "connect_post"
+    PROPFIND = "propfind"
+
+
+class SendDataType(StrEnum):
+    COMMON = "common"
+    CUSTOM = "custom"
+
+
+class AuthMode(StrEnum):
+    BASIC_AUTH = "user_auth"
+    TOKEN_AUTH = "token_auth"
+
+
+class ContentType(StrEnum):
+    APPLICATION_JSON = "application_json"
+    APPLICATION_OCTET_STREAM = "application_octet_stream"
+    APPLICATION_XML = "application_xml"
+    APPLICATION_ZIP = "application_zip"
+    TEXT_CSV = "text_csv"
+    TEXT_PLAIN = "text_plain"
+    TEXT_XML = "text_xml"
+    TEXT_HTML = "text_html"
+
+
+class PasswordType(StrEnum):
+    PASSWORD = "password"
+    STORE = "store"
+
+
+class ServicePrefix(StrEnum):
+    AUTO = "auto"
+    NONE = "none"
+
+
+class MatchType(StrEnum):
+    STRING = "string"
+    REGEX = "regex"
+
+
+PasswordSpec = tuple[PasswordType, str]
+
+FloatLevels = (
+    tuple[Literal[LevelsType.NO_LEVELS], None]
+    | tuple[Literal[LevelsType.FIXED], tuple[float, float]]
+)
+
+IntLevels = (
+    tuple[Literal[LevelsType.NO_LEVELS], None] | tuple[Literal[LevelsType.FIXED], tuple[int, int]]
+)
+
+
+class EnforceTlsVersion(BaseModel):
+    min_version: TlsVersion
+    allow_higher: bool
+
+
+class SendData(BaseModel):
+    body_text: str
+    content_type: tuple[Literal[SendDataType.COMMON], ContentType] | tuple[
+        Literal[SendDataType.CUSTOM], str
     ]
 
 
+class HeaderSpec(BaseModel):
+    header_name: str
+    header_value: str
+
+
+class HeaderRegexSpec(BaseModel):
+    header_name_pattern: str
+    header_value_pattern: str
+
+
+class UserAuth(BaseModel):
+    user: str
+    password: PasswordSpec
+
+
+class TokenAuth(BaseModel):
+    header: str
+    token: PasswordSpec
+
+
+class Connection(BaseModel):
+    http_versions: HttpVersion | None = None
+    tls_versions: EnforceTlsVersion | None = None
+    method: tuple[HttpMethod, SendData | None] | None = None  # TODO(ma): CMK-15749
+    redirects: RedirectPolicy | None = None
+    timeout: int | None = None
+    user_agent: str | None = None
+    add_headers: list[HeaderSpec] | None = None
+    auth: tuple[Literal[AuthMode.BASIC_AUTH], UserAuth] | tuple[
+        Literal[AuthMode.TOKEN_AUTH], TokenAuth
+    ] | None = None
+
+
+class ServerResponse(BaseModel):
+    expected: list[int]
+
+
+class PageSize(BaseModel):
+    min: int | None = None
+    max: int | None = None
+
+
+class Document(BaseModel):
+    document_body: DocumentBodyOption
+    max_age: int | None = None
+    page_size: PageSize | None = None
+
+
+class HeaderRegex(BaseModel):
+    regex: HeaderRegexSpec
+    case_insensitive: bool
+    invert: bool
+
+
+class BodyRegex(BaseModel):
+    regex: str
+    case_insensitive: bool
+    multiline: bool
+    invert: bool
+
+
+class Content(BaseModel):
+    header: tuple[Literal[MatchType.STRING], str] | tuple[
+        Literal[MatchType.REGEX], HeaderRegex
+    ] | None = None
+    body: tuple[Literal[MatchType.STRING], str] | tuple[
+        Literal[MatchType.REGEX], BodyRegex
+    ] | None = None
+
+
+class HttpSettings(BaseModel):
+    connection: Connection | None = None
+    response_time: FloatLevels | None = None
+    server_response: ServerResponse | None = None
+    cert: tuple[Literal[Validation.NO_VALIDATION], None] | tuple[
+        Literal[Validation.VALIDATE], IntLevels
+    ] | None = None
+    document: Document | None = None
+    content: Content | None = None
+
+
+class ServiceDescription(BaseModel):
+    prefix: ServicePrefix
+    name: str
+
+
+class Endpoint(BaseModel):
+    service_name: ServiceDescription
+    url: str
+    individual_settings: HttpSettings | None = None
+
+
+class HttpParams(BaseModel):
+    endpoints: list[Endpoint]
+    standard_settings: HttpSettings
+
+
+def parse_http_params(raw_params: Mapping[str, object]) -> HttpParams:
+    return HttpParams.model_validate(raw_params)
+
+
 def generate_http_services(
-    params: HTTPParams, host_config: HostConfig, _http_proxies: Mapping[str, HTTPProxy]
+    params: HttpParams, host_config: HostConfig, _http_proxies: Mapping[str, HTTPProxy]
 ) -> Iterator[ActiveCheckCommand]:
-    for endpoint in params:
+    for endpoint in params.endpoints:
         protocol = "HTTPS" if endpoint.url.startswith("https://") else "HTTP"
-        prefix = f"{protocol} " if endpoint.protocol_prefix else ""
+        prefix = f"{protocol} " if endpoint.service_name.prefix is ServicePrefix.AUTO else ""
         yield ActiveCheckCommand(
-            service_description=f"{prefix}{endpoint.name}",
+            service_description=f"{prefix}{endpoint.service_name.name}",
             command_arguments=["-u", endpoint.url],
         )
 

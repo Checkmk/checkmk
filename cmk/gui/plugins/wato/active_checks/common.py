07f09de51389e98231a45a1d6b5f373eb1631df3
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-01-24 15:14:58 +0100

    check smtp: mark ip address family optional
    CMK-15745
    
    Change-Id: I9224ae40c62e0ed1a760c487d6ff9d9317a7622f

--- cmk/gui/plugins/wato/active_checks/common.py
+++ cmk/gui/plugins/wato/active_checks/common.py
@@ -3,11 +3,13 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from typing import Literal
+
 from cmk.gui.i18n import _
 from cmk.gui.valuespec import DropdownChoice
 
 
-def ip_address_family_element():
+def ip_address_family_element() -> tuple[Literal["address_family"], DropdownChoice]:
     return (
         "address_family",
         DropdownChoice(

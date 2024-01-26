3c324e062355fe5aead9a7350588e2fe91830c01
David Gervasoni <david.gervasoni@checkmk.com>
2024-01-26 17:24:13 +0100

    netapp_ontap_temp: add temperature section and plugin
    Change-Id: Id1dadc7c8dbaa42ed33a6cc76fbb99efae6638d9

---
+++ cmk/base/plugins/agent_based/netapp_ontap_temp.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+
+import json
+from collections.abc import Mapping
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import get_value_store, register, Service
+from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+    StringTable,
+)
+from cmk.base.plugins.agent_based.utils import netapp_ontap_models as models
+from cmk.base.plugins.agent_based.utils.temperature import check_temperature, TempParamDict
+
+Section = Mapping[str, models.ShelfTemperatureModel]
+
+# <<<netapp_ontap_temp:sep(0)>>>
+# {
+#     "id": "10",
+#     "temperature_sensors": [
+#         {
+#             "id": 1,
+#             "temperature": 28.0,
+#             "threshold": {
+#                 "high": {"critical": 52.0, "warning": 47.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#         {
+#             "id": 2,
+#             "temperature": 27.0,
+#             "threshold": {
+#                 "high": {"critical": 60.0, "warning": 55.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#         {
+#             "id": 3,
+#             "temperature": 27.0,
+#             "threshold": {
+#                 "high": {"critical": 60.0, "warning": 55.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#         {
+#             "id": 4,
+#             "temperature": 27.0,
+#             "threshold": {
+#                 "high": {"critical": 60.0, "warning": 55.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#         {
+#             "id": 5,
+#             "temperature": 27.0,
+#             "threshold": {
+#                 "high": {"critical": 60.0, "warning": 55.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#         {
+#             "id": 6,
+#             "temperature": 29.0,
+#             "threshold": {
+#                 "high": {"critical": 62.0, "warning": 57.0},
+#                 "low": {"critical": 5.0, "warning": 10.0},
+#             },
+#         },
+#         {
+#             "id": 7,
+#             "temperature": 47.0,
+#             "threshold": {
+#                 "high": {"critical": 100.0, "warning": 95.0},
+#                 "low": {"critical": 5.0, "warning": 10.0},
+#             },
+#         },
+#         {
+#             "id": 8,
+#             "temperature": 29.0,
+#             "threshold": {
+#                 "high": {"critical": 62.0, "warning": 57.0},
+#                 "low": {"critical": 5.0, "warning": 10.0},
+#             },
+#         },
+#         {
+#             "id": 9,
+#             "temperature": 46.0,
+#             "threshold": {
+#                 "high": {"critical": 100.0, "warning": 95.0},
+#                 "low": {"critical": 5.0, "warning": 10.0},
+#             },
+#         },
+#         {
+#             "id": 10,
+#             "temperature": 36.0,
+#             "threshold": {
+#                 "high": {"critical": 90.0, "warning": 85.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#         {
+#             "id": 11,
+#             "temperature": 38.0,
+#             "threshold": {
+#                 "high": {"critical": 90.0, "warning": 85.0},
+#                 "low": {"critical": 0.0, "warning": 5.0},
+#             },
+#         },
+#     ],
+# }
+
+
+def parse_netapp_ontap_temp(string_table: StringTable) -> Section:
+    return {
+        fan.item_name(): fan
+        for line in string_table
+        if (fan := models.ShelfTemperatureModel(**json.loads(line[0])))
+    }
+
+
+register.agent_section(
+    name="netapp_ontap_temp",
+    parse_function=parse_netapp_ontap_temp,
+)
+
+
+def discovery_netapp_ontap_temp(section: Section) -> DiscoveryResult:
+    for sensor_name, sensor in section.items():
+        yield Service(item=f"Ambient Shelf Sensor {sensor_name}") if sensor.ambient else Service(
+            item=f"Internal Shelf Sensor {sensor_name}"
+        )
+
+
+def check_netapp_ontap_temp(item: str, params: TempParamDict, section: Section) -> CheckResult:
+    if not (sensor := section.get(item.split()[-1])):
+        return
+
+    dev_levels = (
+        (
+            float(sensor.high_warning),
+            float(sensor.high_critical),
+        )
+        if sensor.high_warning is not None and sensor.high_critical is not None
+        else None
+    )
+    dev_levels_lower = (
+        (
+            float(sensor.low_warning),
+            float(sensor.low_critical),
+        )
+        if sensor.low_warning is not None and sensor.low_critical is not None
+        else None
+    )
+
+    yield from check_temperature(
+        reading=sensor.temperature,
+        params=params,
+        unique_name=f"Temperature sensor {sensor.item_name()}",
+        dev_levels=dev_levels,
+        dev_levels_lower=dev_levels_lower,
+        value_store=get_value_store(),
+    )
+
+
+register.check_plugin(
+    name="netapp_ontap_temp",
+    service_name="Temperature %s",
+    sections=["netapp_ontap_temp"],
+    discovery_function=discovery_netapp_ontap_temp,
+    check_function=check_netapp_ontap_temp,
+    check_ruleset_name="temperature",
+    check_default_parameters={},
+)

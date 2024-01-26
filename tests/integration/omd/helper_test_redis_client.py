d0587613eabd1f62bcd406d6513f200b4bba2c4d
Lars Michelsen <lm@checkmk.com>
2024-01-26 07:43:44 +0100

    Add test to ensure the client can connect to redis
    Change-Id: I450b98b193eaaa2e8ed1d1f78cd9a7fd9705a5ed

---
+++ tests/integration/omd/helper_test_redis_client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.utils.redis import get_redis_client, redis_server_reachable
+
+print(redis_server_reachable(get_redis_client()))

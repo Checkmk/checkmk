095fb662910ebf83a8dbec11ac43367f0704b1f3
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-01-26 22:14:49 +0100

    fix shorten path for 2.3.0
    Change-Id: I4666b19c21e003ba912e9d1a062df725f6ee7a18

--- packages/cmk-agent-ctl/run.cmd
+++ packages/cmk-agent-ctl/run.cmd
@@ -61,8 +61,8 @@ set worker_arte=%worker_root_dir%\artefacts
 mkdir %worker_arte% 2> nul
 
 :: Setup shortcut call for CI(to make names shorter than 255 chars)
-set ci_root_dir=workdir\workspace\checkmk\master
-set ci_junction_to_root_dir=y
+set ci_root_dir=workdir\workspace\checkmk\2.3.0
+set ci_junction_to_root_dir=yy
 set script_to_run=.\scripts\cargo_build_core.cmd
 powershell -ExecutionPolicy ByPass -File %worker_root_dir%/scripts/windows/shorten_dir_and_call.ps1 %ci_root_dir% %ci_junction_to_root_dir% %script_to_run%
 GOTO :EOF

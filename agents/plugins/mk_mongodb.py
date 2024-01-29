b8e806314111010723aa7bbcc1463bee3e69f326
Checkmk release system <feedback@checkmk.com>
2024-01-29 15:59:52 +0100

    Set version to 2.2.0p22

--- agents/plugins/mk_mongodb.py
+++ agents/plugins/mk_mongodb.py
@@ -24,7 +24,7 @@ version of pymongo (at least 2.8).
 
 """
 
-__version__ = "2.2.0p21"
+__version__ = "2.2.0p22"
 
 import argparse
 import configparser
--- cmk/special_agents/agent_vsphere.py
+++ cmk/special_agents/agent_vsphere.py
@@ -35,7 +35,7 @@ import cmk.special_agents.utils as utils
 #   |                                                                      |
 #   '----------------------------------------------------------------------'
 
-__version__ = "2.2.0p21"
+__version__ = "2.2.0p22"
 
 USER_AGENT = f"checkmk-special-vsphere-{__version__}"
 

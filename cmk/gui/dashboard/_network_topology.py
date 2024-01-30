b49b6e63b1c1faf654568bf5d1ad25846c445380
Andreas Boesl <andreas.boesl@checkmk.com>
2024-01-30 15:11:58 +0100

    14221 FIX Parent/Child topology: No longer ignoring additionally configured filters
    Change-Id: I95b7a98d7e5ea10293c8a6642e23074adfd2128d

--- cmk/gui/dashboard/_network_topology.py
+++ cmk/gui/dashboard/_network_topology.py
@@ -6,19 +6,28 @@
 
 from collections.abc import Mapping
 
+from cmk.utils.user import UserId
+
 from cmk.gui import visuals
+from cmk.gui.data_source import data_source_registry
+from cmk.gui.http import request
 from cmk.gui.view import View
-from cmk.gui.views.store import get_permitted_views
+from cmk.gui.views.page_show_view import get_all_active_filters
+from cmk.gui.views.store import get_all_views, get_permitted_views
 from cmk.gui.visuals.filter import Filter
 
 
 def get_topology_context_and_filters() -> tuple[Mapping[str, Mapping[str, str]], list[Filter]]:
     view_name = "topology_filters"
-    view_spec = get_permitted_views()[view_name]
-    view = View(view_name, view_spec, view_spec.get("context", {}))
-    context = visuals.active_context_from_request(view.datasource.infos, view.spec["context"])
-    filters = visuals.filters_of_visual(
-        view.spec, view.datasource.infos, link_filters=view.datasource.link_filters
+    view_spec = visuals.get_permissioned_visual(
+        view_name,
+        request.get_validated_type_input(UserId, "owner"),
+        "view",
+        get_permitted_views(),
+        get_all_views(),
     )
-    show_filters = visuals.visible_filters_of_visual(view.spec, filters)
-    return context, show_filters
+
+    datasource = data_source_registry[view_spec["datasource"]]()
+    context = visuals.active_context_from_request(datasource.infos, view_spec["context"])
+    view = View(view_name, view_spec, context)
+    return context, visuals.visible_filters_of_visual(view.spec, get_all_active_filters(view))

dc2aaf037a0f5b7a3a49cf90875c2a1aacff14f9
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-01-31 11:31:38 +0100

    mk-sql: improve typing - InstanceName
    Change-Id: Id38e939981e838945b09bc5f9552c2f7883ea5af

--- packages/mk-sql/src/types.rs
+++ packages/mk-sql/src/types.rs
@@ -2,7 +2,7 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
-use derive_more::{From, Into};
+use derive_more::{Display, From, Into};
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct Port(pub u16);
@@ -24,3 +24,6 @@ pub struct MaxConnections(pub u32);
 
 #[derive(PartialEq, From, Debug, Clone)]
 pub struct MaxQueries(pub u32);
+
+#[derive(PartialEq, From, Debug, Display, Clone, Default, Into, Hash, Eq)]
+pub struct InstanceName(pub String);

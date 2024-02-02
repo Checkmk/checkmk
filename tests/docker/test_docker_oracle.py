1b1217a64f7752970f2ed59a445e8d7eb40e0f07
René Slowenski <rene.slowenski@checkmk.com>
2024-02-02 14:43:49 +0100

    docker test: Add Oracle sanity test
    This adds an Oracle connection and check sanity docker test,
    which will fetch and run an Oracle docker container, configure it
    and integrate it within a Checkmk docker instance.
    
    As part of the process, DB connection as well as service discovery
    sanity tests are executed.
    
    Change-Id: Ic5902eb31c491a8ff9844f5c1e3c4e001944d97b
    (cherry picked from commit 860946a098a22ff7b2f7d022d8238df4d3e91e8d)

---
+++ tests/docker/test_docker_oracle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# pylint: disable=redefined-outer-name
+
+import logging
+
+import docker  # type: ignore[import-untyped]
+import pytest
+
+from tests.testlib.docker import checkmk_docker_api_request
+
+logger = logging.getLogger()
+
+
+@pytest.mark.skip("Not yet run in 2.3 branch.")
+def test_docker_oracle(
+    checkmk: docker.models.containers.Container,
+    oracle: docker.models.containers.Container,
+) -> None:
+    hostname = "oracle"
+
+    oracle_services = [
+        _
+        for _ in checkmk_docker_api_request(
+            checkmk, "get", f"/objects/host/{hostname}/collections/services"
+        ).json()["value"]
+        if _.get("title").startswith("ORA")
+    ]
+
+    assert len(oracle_services) >= 18

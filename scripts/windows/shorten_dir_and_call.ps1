b499787e0faac558fae8933f8b2e2d1f2061445b
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-02-06 09:31:04 +0100

    report current dir building rust project on Windows
    - shorten dir
    
    Change-Id: Ib5ca66ae17f9f6fa5db3caa757baf0a67447d84d

--- scripts/windows/shorten_dir_and_call.ps1
+++ scripts/windows/shorten_dir_and_call.ps1
@@ -20,5 +20,6 @@
 [string]$new = $inp.tolower().replace($args[0], $args[1])
 Write-Host "shorten dir: " $new
 Set-Location $new
+Write-Host Used: "$PWD.Path"
 &$args[2]
 [Environment]::Exit($LASTEXITCODE)

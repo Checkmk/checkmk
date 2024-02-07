e1daa2f8bea71f368cee8476c150b71befa715b6
Christopher Krause <christopher.krause@checkmk.com>
2024-02-07 17:12:29 +0100

    16472 FIX iLert notifications: ignore error when event is already closed
    When attempting to resolve an incident which was already manually resolved, an
    error occured.
    
    This kind of error will now be ignored.
    
    Change-Id: Ibd6ecb4151f1e3267b24daee4a231da110169fe2
    JIRA-Ref: SUP-16100

--- cmk/notification_plugins/ilert.py
+++ cmk/notification_plugins/ilert.py
@@ -5,7 +5,12 @@
 #       iLert <support@ilert.com>
 # License: GNU Public License v2
 
+import sys
+from http.client import responses as http_responses
 from os import environ
+from typing import NoReturn
+
+import requests
 
 from cmk.notification_plugins.utils import post_request, process_by_result_map
 from cmk.notification_plugins.utils import retrieve_from_passwordstore as passwords
@@ -34,7 +39,25 @@ def _ilert_url() -> str:
     return f"https://api.ilert.com/api/v1/events/checkmk-ext/{password}"
 
 
-def main() -> int:
-    return process_by_result_map(
-        post_request(lambda context: {**context}, url=_ilert_url(), headers=HEADERS), RESULT_MAP
-    )
+def _check_return_code(response: requests.Response) -> None:
+    # this is a hotfix for 2.2
+    # in 2.3, process_by_result_map was changed so that this function will no longer be needed
+    try:
+        body = response.json()
+    except requests.JSONDecodeError:
+        return
+
+    if not (code := body.get("code")):
+        return
+
+    status_code = response.status_code
+    summary = f"{status_code}: {http_responses[status_code]}"
+    if status_code == 400 and code == "NONE_OPEN_FOR_KEY":
+        sys.stderr.write(f"Event already closed in iLert: {response.text}\n{summary}\n")
+        sys.exit(0)
+
+
+def main() -> NoReturn:
+    response = post_request(lambda context: {**context}, url=_ilert_url(), headers=HEADERS)
+    _check_return_code(response)
+    process_by_result_map(response, RESULT_MAP)

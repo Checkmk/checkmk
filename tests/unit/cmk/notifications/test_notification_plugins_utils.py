8fe4f6ddcf230ff9ee45a27222b530e719291674
Christopher Krause <christopher.krause@checkmk.com>
2024-02-07 17:12:29 +0100

    16472 FIX iLert notifications: ignore error when event is already closed
    When attempting to resolve an incident which was already manually resolved, an
    error occured.
    
    This kind of error will now be ignored.
    
    Change-Id: Ibd6ecb4151f1e3267b24daee4a231da110169fe2
    JIRA-Ref: SUP-16100

--- tests/unit/cmk/notifications/test_notification_plugins_utils.py
+++ tests/unit/cmk/notifications/test_notification_plugins_utils.py
@@ -330,55 +330,152 @@ def test_escape_context(input_context: dict[str, str], expected_context: Mapping
 
 
 @pytest.mark.parametrize(
-    "response, result_map, expected_exit_msg, expected_exit_code",
+    "response, matchers, expected_exit_msg, expected_exit_code",
     [
         (
             Mock(requests.models.Response, status_code=200, text="whatever"),
-            {},
+            [],
             "Details for Status Code are not defined\n200: OK\n",
             3,
         ),
         (
             Mock(requests.models.Response, status_code=200, text="whatever"),
-            {
-                (400, 500): utils.StateInfo(state=0, type="str", title="some title"),
-            },
+            [
+                (
+                    utils.StatusCodeMatcher(range=(400, 500)),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
             "Details for Status Code are not defined\n200: OK\n",
             3,
         ),
         (
             Mock(requests.models.Response, status_code=200, text="whatever"),
-            {
-                (200, 300): utils.StateInfo(state=0, type="str", title="some title"),
-            },
-            "some title\n200: OK\n",
+            [
+                (
+                    utils.StatusCodeMatcher(range=(200, 300)),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "some title: whatever\n200: OK\n",
             0,
         ),
         (
             Mock(requests.models.Response, status_code=201, text="whatever"),
-            {
-                (200, 300): utils.StateInfo(state=0, type="str", title="some title"),
-            },
-            "some title\n200: OK\n",
+            [
+                (
+                    utils.StatusCodeMatcher(range=(200, 300)),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "some title: whatever\n201: Created\n",
             0,
         ),
         (
             Mock(requests.models.Response, status_code=300, text="whatever"),
-            {
-                (200, 300): utils.StateInfo(state=0, type="str", title="some title"),
-            },
-            "some title\n200: OK\n",
+            [
+                (
+                    utils.StatusCodeMatcher(range=(200, 300)),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "some title: whatever\n300: Multiple Choices\n",
             0,
+        ),
+        (
+            Mock(
+                requests.models.Response,
+                status_code=200,
+                text="whatever",
+                json=lambda: {"code": "all good"},
+            ),
+            [
+                (
+                    utils.JsonFieldMatcher(field="not_found", value="all good"),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "Details for Status Code are not defined\n200: OK\n",
+            3,
+        ),
+        (
+            Mock(
+                requests.models.Response,
+                status_code=200,
+                text="whatever",
+                json=lambda: {"code": "all good"},
+            ),
+            [
+                (
+                    utils.JsonFieldMatcher(field="code", value="wrong value"),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "Details for Status Code are not defined\n200: OK\n",
+            3,
+        ),
+        (
+            Mock(
+                requests.models.Response,
+                status_code=200,
+                text="whatever",
+                json=lambda: {"code": "all good"},
+            ),
+            [
+                (
+                    utils.JsonFieldMatcher(field="code", value="all good"),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "some title: whatever\n200: OK\n",
+            0,
+        ),
+        (
+            Mock(
+                requests.models.Response,
+                status_code=200,
+                text="whatever",
+                json=lambda: {"code": "all good"},
+            ),
+            [
+                (
+                    utils.StatusCodeMatcher(range=(200, 300)).and_(
+                        utils.JsonFieldMatcher(field="code", value="all good")
+                    ),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "some title: whatever\n200: OK\n",
+            0,
+        ),
+        (
+            Mock(
+                requests.models.Response,
+                status_code=400,
+                text="whatever",
+                json=lambda: {"code": "all good"},
+            ),
+            [
+                (
+                    utils.StatusCodeMatcher(range=(200, 300)).and_(
+                        utils.JsonFieldMatcher(field="code", value="all good")
+                    ),
+                    utils.StateInfo(state=0, type="str", title="some title"),
+                ),
+            ],
+            "Details for Status Code are not defined\n400: Bad Request\n",
+            3,
         ),
     ],
 )
-def test_process_by_result_map(
-    response,
-    result_map,
-    expected_exit_msg,
-    expected_exit_code,
-):
+def test_process_by_matchers(
+    response: Mock,
+    matchers: list[tuple[utils.ResponseMatcher, utils.StateInfo]],
+    expected_exit_msg: str,
+    expected_exit_code: int,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
     with pytest.raises(SystemExit) as sys_exit:
-        utils.process_by_result_map(response, result_map)
-        assert sys_exit.value == expected_exit_msg
-        assert sys_exit.value.code == expected_exit_code
+        utils.process_by_matchers(response, matchers)
+    assert sys_exit.value.code == expected_exit_code
+    assert capsys.readouterr().err == expected_exit_msg

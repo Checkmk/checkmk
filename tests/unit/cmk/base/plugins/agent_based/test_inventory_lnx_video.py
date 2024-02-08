382e963f4b12d0da5a40ae94f62a607acc831628
Luka Racic <luka.racic@checkmk.com>
2024-02-08 15:25:06 +0100

    16336 FIX lnx_video: Fix section parsing
    The output of the 'lspci' command was not consistent. This caused the parsing of the lnx_video section to fail.
    As a consequnce, the graphics cards were not shown in the HW/SW inventory.
    This has now been fixed and the graphics cards will be shown.
    
    CMK-10845
    
    Change-Id: Ia807800c25a69427f9f4a108b5c9292be9345535

--- tests/unit/cmk/base/plugins/agent_based/test_inventory_lnx_video.py
+++ tests/unit/cmk/base/plugins/agent_based/test_inventory_lnx_video.py
@@ -60,6 +60,38 @@ from cmk.base.plugins.agent_based.inventory_lnx_video import inventory_lnx_video
                 ),
             ],
         ),
+        (
+            [
+                [
+                    "0000",
+                    "00",
+                    "02.0 VGA compatible controller",
+                    " Intel Corporation Device 9a49 (rev 01) (prog-if 00 [VGA controller])",
+                ],
+                ["Subsystem", " Dell Device 0a38"],
+                ["Flags", " bus master, fast devsel, latency 0, IRQ 167"],
+                ["Memory at 6054000000 (64-bit, non-prefetchable) [size=16M]"],
+                ["Memory at 4000000000 (64-bit, prefetchable) [size=256M]"],
+                ["I/O ports at 3000 [size=64]"],
+                ["Expansion ROM at 000c0000 [virtual] [disabled] [size=128K]"],
+                ["Capabilities", " <access denied>"],
+                ["Kernel driver in use", " i915"],
+                ["Kernel modules", " i915"],
+            ],
+            [
+                TableRow(
+                    path=["hardware", "video"],
+                    key_columns={
+                        "name": " Intel Corporation Device 9a49 (rev 01) (prog-if 00 [VGA controller])",
+                    },
+                    inventory_columns={
+                        "subsystem": " Dell Device 0a38",
+                        "driver": " i915",
+                    },
+                    status_columns={},
+                ),
+            ],
+        ),
     ],
 )
 def test_inventory_lnx_video(string_table: StringTable, expected_result: InventoryResult) -> None:

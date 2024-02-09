ceca25bc7f12b2c10a7fd8153c9662a238917b41
Matteo Stifano <matteo.stifano@checkmk.com>
2024-02-09 13:41:37 +0100

    test_plugin: Temporary skip SNMP-rittal dumps
    Change-Id: I48ec2eb516f084b3c944e82fdd63f187bcb2c452

--- tests/plugins_integration/checks.py
+++ tests/plugins_integration/checks.py
@@ -29,6 +29,8 @@ class SkippedDumps:
     SKIPPED_DUMPS = [
         # TODO: restore after CMK-15510
         "agent-2.2.0p12-mk-oracle-centos",
+        # TODO: restore after CMK-15029
+        "snmp-rittal-CMCIII",
     ]
 
 

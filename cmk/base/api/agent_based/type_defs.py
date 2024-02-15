cd0c23d01f6f816fdab917aa85986ee26af8f720
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-02-15 12:38:38 +0100

    unbreak m365_service_health MKP
    Change-Id: I62c4c2dd21b65088cb2e6f132e61c826ea1ee81b

---
+++ cmk/base/api/agent_based/type_defs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""Temporary compatibility layer for m365_service_health-1.2.1.mkp"""
+from cmk.agent_based.v1.type_defs import StringTable  # pylint: disable=unused-import
+
+Parameters = object  # not even used by the plugin

881735c646f524020fd4582e5802c518bb051156
Sven Panne <sven.panne@checkmk.com>
2024-02-15 15:27:16 +0100

    Moved MKP-related helpers closer to their use sites.
    Change-Id: I30bf025f3425304a49cd399a21bdf01e3de54bde

--- cmk/ec/mkp.py
+++ cmk/ec/mkp.py
@@ -3,22 +3,76 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Mapping
+from collections.abc import Iterable, Mapping
+from pathlib import Path
 
 from cmk.mkp_tool import PackageOperationCallbacks, PackagePart
 
-from .rule_packs import (
-    install_packaged_rule_packs,
-    release_packaged_rule_packs,
-    uninstall_packaged_rule_packs,
-)
+from .config import MkpRulePackProxy
+from .rule_packs import export_rule_pack, load_rule_packs, save_rule_packs
+
+
+def _install_packaged_rule_packs(file_names: Iterable[Path]) -> None:
+    """
+    Adds rule pack proxy objects to the list of rule packs given a list
+    of file names. The file names without the file extension are used as
+    the ID of the rule pack.
+    """
+    rule_packs = list(load_rule_packs())
+    rule_pack_ids = {rp["id"]: i for i, rp in enumerate(rule_packs)}
+    ids = [fn.stem for fn in file_names]
+    for id_ in ids:
+        index = rule_pack_ids.get(id_)
+        if index is not None and isinstance(rule_packs[index], MkpRulePackProxy):
+            rule_packs[index] = MkpRulePackProxy(id_)
+        else:
+            rule_packs.append(MkpRulePackProxy(id_))
+    save_rule_packs(rule_packs)
+
+
+def _uninstall_packaged_rule_packs(file_names: Iterable[Path]) -> None:
+    """
+    This function synchronizes the rule packs in rules.mk and the packaged rule packs
+    of a MKP upon deletion of that MKP. When a modified or an unmodified MKP is
+    deleted the exported rule pack and the rule pack in rules.mk are both deleted.
+    """
+    affected_ids = {fn.stem for fn in file_names}
+    save_rule_packs(rp for rp in load_rule_packs() if rp["id"] not in affected_ids)
+
+
+def _release_packaged_rule_packs(file_names: Iterable[Path]) -> None:
+    """
+    This function synchronizes the rule packs in rules.mk and the rule packs
+    packaged in a MKP upon release of that MKP. The following cases have
+    to be distinguished:
+
+        1. Upon release of an unmodified MKP package the proxy in rules.mk
+           and the exported rule pack are unchanged.
+        2. Upon release of a MKP package with locally modified rule packs the
+           modified rule pack updates the exported version.
+    """
+    rule_packs = list(load_rule_packs())
+    rule_pack_ids = [rp["id"] for rp in rule_packs]
+    affected_ids = [fn.stem for fn in file_names]
+
+    save = False
+    for id_ in affected_ids:
+        index = rule_pack_ids.index(id_)
+        rp = rule_packs[index]
+        if not isinstance(rp, MkpRulePackProxy):
+            save = True
+            export_rule_pack(rp)
+            rule_packs[index] = MkpRulePackProxy(id_)
+
+    if save:
+        save_rule_packs(rule_packs)
 
 
 def mkp_callbacks() -> Mapping[PackagePart, PackageOperationCallbacks]:
     return {
         PackagePart.EC_RULE_PACKS: PackageOperationCallbacks(
-            install=install_packaged_rule_packs,
-            uninstall=uninstall_packaged_rule_packs,
-            release=release_packaged_rule_packs,
+            install=_install_packaged_rule_packs,
+            uninstall=_uninstall_packaged_rule_packs,
+            release=_release_packaged_rule_packs,
         ),
     }

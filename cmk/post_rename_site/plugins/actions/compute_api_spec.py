3a3f706ad2bc50fbf709cc2e1bae44a270774a0b
Lars Michelsen <lm@checkmk.com>
2024-02-17 14:13:49 +0100

    Execute the API spec generation in various situations
    This change registers the new cmk-compose-api-spec command into various
    events happening during the lifecycle of a site.
    
    - post-create hook: Create the initial spec after a site has been created
    - post rename action: Update the spec after a site has been copied,
      restored or renamed
    - update-config action: Update the spec after Checkmk has been updated
    
    It might be necessary to run the command in some other situations. We'll
    have to find out about those along the way.
    
    CMK-15981
    
    Change-Id: I29eeffd60ec11a797f55d103a8a86fd74fdf3935

---
+++ cmk/post_rename_site/plugins/actions/compute_api_spec.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import subprocess
+from logging import Logger
+
+from livestatus import SiteId
+
+from cmk.utils.i18n import _
+
+from cmk.post_rename_site.registry import rename_action_registry, RenameAction
+
+
+def compute_api_spec(old_site_id: SiteId, new_site_id: SiteId, logger: Logger) -> None:
+    subprocess.check_call(["cmk-compute-api-spec"])
+
+
+rename_action_registry.register(
+    RenameAction(
+        name="compute_api_spec",
+        title=_("Compute REST API specification"),
+        sort_index=950,
+        handler=compute_api_spec,
+    )
+)

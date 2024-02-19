c44c3a543c4d036d6506e8e1575eb4118776f82e
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-02-20 00:03:14 +0100

    mem: split up checks
    Change-Id: If472fe115f24f59efc45a89573edb41a2eb93c8b

--- tests/unit/cmk/base/legacy_checks/test_mem.py
+++ tests/unit/cmk/base/legacy_checks/test_mem.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.base.legacy_checks.mem import check_mem_vmalloc, inventory_mem_vmalloc
+from cmk.base.legacy_checks.mem_vmalloc import check_mem_vmalloc, inventory_mem_vmalloc
 
 _SECTION = {
     "VmallocTotal": 2 * 1024**2,

fe217d03374c6dc69109fb77b5fe416f532de7a4
Frans Fürst <frans.fuerst@checkmk.com>
2024-02-22 16:42:26 +0100

    build-build-images: improve docker build call and iterate over all distros to keep build matrix
    Change-Id: If8ddeedc18d5dfa7f2bec6c5fac5a365bf3acdec

--- buildscripts/scripts/build-build-images.groovy
+++ buildscripts/scripts/build-build-images.groovy
@@ -24,6 +24,7 @@ def main() {
     ]);
 
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+    def all_distros = versioning.configured_or_overridden_distros("enterprise", "", "release");
     def distros = versioning.configured_or_overridden_distros("enterprise", OVERRIDE_DISTROS);
 
     def vers_tag = versioning.get_docker_tag(scm, checkout_dir);
@@ -34,6 +35,7 @@ def main() {
     print(
         """
         |===== CONFIGURATION ===============================
+        |all_distros:....................(local)  │${all_distros}│
         |distros:........................(local)  │${distros}│
         |publish_images:.................(local)  │${publish_images}│
         |vers_tag:.......................(local)  │${vers_tag}│
@@ -52,63 +54,71 @@ def main() {
     withCredentials([
         usernamePassword(
             credentialsId: 'nexus',
-            usernameVariable: 'USERNAME',
-            passwordVariable: 'PASSWORD')
+            usernameVariable: 'NEXUS_USERNAME',
+            passwordVariable: 'NEXUS_PASSWORD')
     ]) {
-        def alias_names = [:];
-        def image_ids = [:];
-        def dockerfiles = [:];
+        def distro_base_image_id = [:];
+        def real_distro_name = [:];
 
-        dir("${checkout_dir}") {
-            distros.each { distro ->
-                dockerfiles[distro] = "${distro}/Dockerfile";
-                alias_names[distro] = cmd_output(
-                    "grep 'ARG IMAGE_' " +
-                    "buildscripts/infrastructure/build-nodes/${dockerfiles[distro]}" +
-                    " | awk '{print \$2}'").replaceAll("[\r\n]+", "");
-                image_ids[distro] = resolve_docker_image_alias(alias_names[distro]);
+        stage("Provide\nupstream images") {
+            dir("${checkout_dir}") {
+                distro_base_image_id = distros.collectEntries { distro -> [
+                    (distro) : {
+                        real_distro_name[distro] = cmd_output(
+                            "basename \$(realpath buildscripts/infrastructure/build-nodes/${distro})");
+                        resolve_docker_image_alias(
+                            "IMAGE_${real_distro_name[distro].toUpperCase().replaceAll('\\.', '_').replaceAll('-', '_')}")
+                    }()
+                ]};
+                sh("""
+                    cp defines.make static_variables.bzl package_versions.bzl .bazelversion omd/strip_binaries \
+                    buildscripts/infrastructure/build-nodes/scripts
+
+                    cp omd/distros/*.mk buildscripts/infrastructure/build-nodes/scripts
+                """);
             }
-            sh("""
-                cp defines.make static_variables.bzl package_versions.bzl .bazelversion omd/strip_binaries \
-                buildscripts/infrastructure/build-nodes/scripts
-
-                cp omd/distros/*.mk buildscripts/infrastructure/build-nodes/scripts
-            """);
         }
 
-        dir("${checkout_dir}/buildscripts/infrastructure/build-nodes") {
-            // TODO: here it would be nice to iterate through all known distros
-            //       and use a conditional_stage(distro in distros) approach
-            def stages = distros.collectEntries { distro ->
-                [("${distro}") : {
-                        stage("Build ${distro}") {
-                            def DOCKER_ARGS = (
-                                " --build-arg ${alias_names[distro]}=${image_ids[distro]}" +
-                                " --build-arg DOCKER_REGISTRY='${docker_registry_no_http}'" +
-                                " --build-arg NEXUS_ARCHIVES_URL='$NEXUS_ARCHIVES_URL'" +
-                                " --build-arg DISTRO='$distro'" +
-                                " --build-arg NEXUS_USERNAME='$USERNAME'" +
-                                " --build-arg NEXUS_PASSWORD='$PASSWORD'" +
-                                " --build-arg ARTIFACT_STORAGE='$ARTIFACT_STORAGE'" +
-                                " --build-arg VERS_TAG='$vers_tag'" +
-                                " --build-arg BRANCH_VERSION='$branch_version'" +
-                                " -f ${dockerfiles[distro]} .");
+        // TODO: here it would be nice to iterate through all known distros
+        //       and use a conditional_stage(distro in distros) approach
+        def stages = all_distros.collectEntries { distro ->
+            [("${distro}") : {
+                    conditional_stage("Build\n${distro}", distro in distros) {
+                        def image_name = "${distro}:${vers_tag}";
+                        def distro_mk_file_name = "${real_distro_name[distro].toUpperCase().replaceAll('-', '_')}.mk";
+                        def docker_build_args = (""
+                            + " --build-arg DISTRO_IMAGE_BASE='${distro_base_image_id[distro]}'"
+                            + " --build-arg DISTRO_MK_FILE='${distro_mk_file_name}'"
+                            + " --build-arg DISTRO='${distro}'"
+                            + " --build-arg VERS_TAG='${vers_tag}'"
+                            + " --build-arg BRANCH_VERSION='${branch_version}'"
 
-                            if (params.BUILD_IMAGE_WITHOUT_CACHE) {
-                                DOCKER_ARGS = "--no-cache " + DOCKER_ARGS;
-                            }
-                            docker.build("${distro}:${vers_tag}", DOCKER_ARGS);
-                        }}
-                ]
-            }
-            def images = parallel(stages);
+                            + " --build-arg DOCKER_REGISTRY='${docker_registry_no_http}'"
+                            + " --build-arg NEXUS_ARCHIVES_URL='${NEXUS_ARCHIVES_URL}'"
+                            + " --build-arg NEXUS_USERNAME='${NEXUS_USERNAME}'"
+                            + " --build-arg NEXUS_PASSWORD='${NEXUS_PASSWORD}'"
+                            + " --build-arg ARTIFACT_STORAGE='${ARTIFACT_STORAGE}'"
+                            + " -f '${distro}/Dockerfile'"
+                            + " ."
+                        );
 
-            conditional_stage('upload images', publish_images) {
-                docker.withRegistry(DOCKER_REGISTRY, "nexus") {
-                    images.each { distro, image ->
-                        image.push();
-                        image.push("${branch_name}-latest");
+                        if (params.BUILD_IMAGE_WITHOUT_CACHE) {
+                            docker_build_args = "--no-cache " + docker_build_args;
+                        }
+                        dir("${checkout_dir}/buildscripts/infrastructure/build-nodes") {
+                            docker.build(image_name, docker_build_args);
+                        }
                     }
+                }
+            ]
+        }
+        def images = parallel(stages);
+
+        conditional_stage('upload images', publish_images) {
+            docker.withRegistry(DOCKER_REGISTRY, "nexus") {
+                images.each { distro, image ->
+                    image.push();
+                    image.push("${branch_name}-latest");
                 }
             }
         }

Checkmk/checkmk

commit 1bc7815a515884033aadbefd9031ce4c10fe0c5d

Sync return types in EC and GUI mkevend
Similar types will allow for code deduplication
as a next step.

Change-Id: I4672472a65352d265002fbd09e31f7a4c46cb782

cmk/gui/mkeventd/wato.py
@@ -16,7 +16,7 @@ from collections.abc import Callable, Collection, Iterable, Iterator, Mapping, S
 from dataclasses import dataclass
 from html import escape as html_escape
 from pathlib import Path
-from typing import Any, overload, TypeVar
+from typing import Any, cast, overload, TypeVar
 
 from pysmi.codegen.pysnmp import PySnmpCodeGen  # type: ignore[import]
 from pysmi.compiler import MibCompiler  # type: ignore[import]
@@ -1463,7 +1463,7 @@ class ABCEventConsoleMode(WatoMode, abc.ABC):
                 return nr, entry
         raise MKUserError(None, _("The requested rule pack does not exist."))
 
-    def _show_event_simulator(self) -> dict[str, Any] | None:
+    def _show_event_simulator(self) -> ec.Event | None:
         event = user.load_file("simulated_event", {})
         html.begin_form("simulator")
         self._vs_mkeventd_event().render_input("event", event)
@@ -1475,7 +1475,8 @@ class ABCEventConsoleMode(WatoMode, abc.ABC):
         html.br()
 
         if request.var("_simulate") or request.var("_generate"):
-            return self._vs_mkeventd_event().from_html_vars("event")
+            # TODO: remove cast improve type by explicit type conversion to Event
+            return cast(ec.Event, self._vs_mkeventd_event().from_html_vars("event"))
         return None
 
     def _event_simulation_action(self) -> bool:
@@ -1977,8 +1978,8 @@ class ModeEventConsoleRulePacks(ABCEventConsoleMode):
 
                     for rule in rule_pack["rules"]:
                         result = event_rule_matches(rule_pack, rule, event)
-                        if isinstance(result, tuple):
-                            cancelling, groups = result
+                        if isinstance(result, ec.MatchSuccess):
+                            cancelling, groups = result.cancelling, result.match_groups
 
                             if not cancelling and rule.get("drop") == "skip_pack":
                                 matches += 1
@@ -2306,10 +2307,10 @@ class ModeEventConsoleRules(ABCEventConsoleMode):
                     )
                 elif event:
                     result = event_rule_matches(self._rule_pack, rule, event)
-                    if not isinstance(result, tuple):
-                        html.icon("rulenmatch", _("Rule does not match: %s") % result)
+                    if not isinstance(result, ec.MatchSuccess):
+                        html.icon("rulenmatch", _("Rule does not match: %s") % result.reason)
                     else:
-                        cancelling, groups = result
+                        cancelling, groups = result.match_groups, result.match_groups
                         if have_match:
                             msg = _("This rule matches, but is overruled by a previous match.")
                             icon = "rulepmatch"

cmk/ec/event.py
@@ -14,6 +14,8 @@ from typing import TypedDict
 from dateutil.parser import isoparse
 from dateutil.tz import tzlocal
 
+from livestatus import SiteId
+
 from cmk.utils.type_defs import HostName
 
 
@@ -50,6 +52,7 @@ class Event(TypedDict, total=False):
     owner: str
     phase: str
     rule_id: str | None
+    site: SiteId
     sl: int
     state: int
 

cmk/ec/export.py
@@ -6,10 +6,26 @@
 # flake8: noqa
 # pylint: disable=unused-import
 
-from .config import ConfigFromWATO, ECRulePack, ECRulePackSpec, MkpRulePackProxy, Rule, TextPattern
+from .config import (
+    ConfigFromWATO,
+    ECRulePack,
+    ECRulePackSpec,
+    MkpRulePackProxy,
+    Rule,
+    TextMatchResult,
+    TextPattern,
+)
 from .defaults import default_config, default_rule_pack
+from .event import Event
 from .forward import SyslogForwarderUnixSocket, SyslogMessage
-from .main import match_ipv4_network, SyslogFacility, SyslogPriority
+from .main import (
+    match_ipv4_network,
+    MatchFailure,
+    MatchResult,
+    MatchSuccess,
+    SyslogFacility,
+    SyslogPriority,
+)
 from .rule_packs import (
     add_rule_pack_proxies,
     ECRuleSpec,

cmk/gui/mkeventd/rule_matching.py
@@ -5,7 +5,6 @@
 
 import re
 import socket
-from typing import Any
 
 import cmk.utils.paths
 import cmk.utils.version as cmk_version
@@ -23,32 +22,32 @@ from cmk.gui.i18n import _
 def event_rule_matches(
     rule_pack: ec.ECRulePack,
     rule: ec.Rule,
-    event: dict[str, Any],
-) -> str | tuple[bool, tuple]:
+    event: ec.Event,
+) -> ec.MatchResult:
     result = event_rule_matches_non_inverted(rule_pack, rule, event)
     if rule.get("invert_matching"):
-        if isinstance(result, tuple):
-            return _("The rule would match, but matching is inverted.")
-        return False, ()
+        if isinstance(result, ec.MatchSuccess):
+            return ec.MatchFailure(_("The rule would match, but matching is inverted."))
+        return ec.MatchSuccess(False, {})
     return result
 
 
 def event_rule_matches_non_inverted(  # pylint: disable=too-many-branches
     rule_pack: ec.ECRulePack,
     rule: ec.Rule,
-    event: dict[str, Any],
-) -> str | tuple[bool, tuple]:
+    event: ec.Event,
+) -> ec.MatchResult:
     if not ec.match_ipv4_network(rule.get("match_ipaddress", "0.0.0.0/0"), event["ipaddress"]):
-        return _("The source IP address does not match.")
+        return ec.MatchFailure(_("The source IP address does not match."))
 
     if match(rule.get("match_host"), event["host"], complete=True) is False:
-        return _("The host name does not match.")
+        return ec.MatchFailure(_("The host name does not match."))
 
     if match(rule.get("match_application"), event["application"], complete=False) is False:
-        return _("The application (syslog tag) does not match")
+        return ec.MatchFailure(_("The application (syslog tag) does not match"))
 
     if "match_facility" in rule and event["facility"] != rule["match_facility"]:
-        return _("The syslog facility does not match")
+        return ec.MatchFailure(_("The syslog facility does not match"))
 
     # First try cancelling rules
     if "match_ok" in rule or "cancel_priority" in rule:
@@ -62,14 +61,14 @@ def event_rule_matches_non_inverted(  # pylint: disable=too-many-branches
         if match_groups is not False and cp:
             if match_groups is True:
                 match_groups = ()
-            return True, match_groups
+            return ec.MatchSuccess(True, {"match_groups_message": match_groups})
 
     try:
         match_groups = match(rule.get("match"), event["text"], complete=False)
     except Exception as e:
-        return _("Invalid regular expression: %s") % e
+        return ec.MatchFailure(_("Invalid regular expression: %s") % e)
     if match_groups is False:
-        return _("The message text does not match the required pattern.")
+        return ec.MatchFailure(_("The message text does not match the required pattern."))
 
     if "match_priority" in rule:
         prio_from, prio_to = rule["match_priority"]
@@ -77,23 +76,21 @@ def event_rule_matches_non_inverted(  # pylint: disable=too-many-branches
             prio_to, prio_from = prio_from, prio_to
         p = event["priority"]
         if p < prio_from or p > prio_to:
-            return _("The syslog priority is not in the required range.")
+            return ec.MatchFailure(_("The syslog priority is not in the required range."))
 
     if "match_sl" in rule:
         sl_from, sl_to = rule["match_sl"]
         if sl_from > sl_to:
             sl_to, sl_from = sl_from, sl_to
-        p = event.get("sl")
-        if p is None:
-            return _("No service level is set in event")
+        p = event.get("sl", 0)
 
         if p < sl_from or p > sl_to:
-            return _("Wrong service level %d (need %d..%d)") % (p, sl_from, sl_to)
+            return ec.MatchFailure(_("Wrong service level %d (need %d..%d)") % (p, sl_from, sl_to))
 
     if "match_timeperiod" in rule:
         reason = check_timeperiod(rule["match_timeperiod"])
         if reason:
-            return reason
+            return ec.MatchFailure(reason)
 
     if cmk_version.is_managed_edition():
         import cmk.gui.cme.managed as managed  # pylint: disable=no-name-in-module,import-outside-toplevel
@@ -106,11 +103,11 @@ def event_rule_matches_non_inverted(  # pylint: disable=too-many-branches
         site_customer_id = managed.get_customer_id(active_config.sites[event["site"]])
 
         if rule_customer_id not in (managed.SCOPE_GLOBAL, site_customer_id):
-            return _("Wrong customer")
+            return ec.MatchFailure(_("Wrong customer"))
 
     if match_groups is True:
         match_groups = ()  # no matching groups
-    return False, match_groups
+    return ec.MatchSuccess(False, {"match_groups_message": match_groups})
 
 
 def check_timeperiod(tpname: str) -> str | None:
@@ -131,7 +128,7 @@ def check_timeperiod(tpname: str) -> str | None:
         return _("Cannot update timeperiod information for %s: %s") % (tpname, e)
 
 
-def match(pattern: ec.TextPattern, text: str, complete: bool = True) -> bool | tuple:
+def match(pattern: ec.TextPattern, text: str, complete: bool = True) -> bool | ec.TextMatchResult:
     if pattern is None:
         return True
     assert not isinstance(pattern, re.Pattern)  # Hmmm...

commit 7bed8a3adc3c8ba45951b37d745bca030900c233

16529 FIX EC: fix a wrong message on a matched rule
CMK-15873

The issue was introduced by a commit 1bc7815a515884033aadbefd9031ce4c10fe0c5d
that refactored the result type into MatchSuccess and
MatchFailure

Change-Id: I4f56e985e1e4612e5aab6246378b5e5578363e41

cmk/gui/mkeventd/wato.py
@@ -2426,7 +2426,7 @@ class ModeEventConsoleRules(ABCEventConsoleMode):
                     if not isinstance(result, ec.MatchSuccess):
                         html.icon("hyphen", _("Rule does not match: %s") % result.reason)
                     else:
-                        cancelling, groups = result.match_groups, result.match_groups
+                        cancelling, groups = result.cancelling, result.match_groups
                         if have_match:
                             msg = _("This rule matches, but is overruled by a previous match.")
                             icon = "checkmark_plus"
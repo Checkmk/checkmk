6db80209708e064469c2d0f7d614224c1823e9ae
Ronny Bruska <ronny.bruska@checkmk.com>
2024-02-23 13:55:14 +0100

    Fix typo
    CMK-16180
    
    Change-Id: Ifd37f4101ab1e538a602a55cfea9ebc8a5f66121
    (cherry picked from commit 3483b5765446a6cd727078384eb8be6a4454627e)

--- cmk/post_rename_site/plugins/actions/warn_changed_ports.py
+++ cmk/post_rename_site/plugins/actions/warn_changed_ports.py
@@ -22,8 +22,8 @@ def warn_about_network_ports(old_site_id: SiteId, new_site_id: SiteId, logger: L
     console.warning(
         "Network port configuration may need your attention\n\n"
         "It seems like you copied an existing site. In case you plan to use both on the same "
-        "system, you may have to review the network port configuration of your sites. Two sites"
-        "with the same configuration may cause network port conflicts."
+        "system, you may have to review the network port configuration of your sites. Two sites "
+        "with the same configuration may cause network port conflicts. "
         "For example if you enabled livestatus to listen via TCP or enabled the Event Console "
         "to listen for incoming Syslog messages or SNMP traps, you may have to update the "
         "configuration in one of the sites to resolve the conflicts.\n"

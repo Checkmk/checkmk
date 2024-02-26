33c093cef0060fe87f7481e2414903873b0a0f30
Dhananjay Shah <dhananjay.shah@checkmk.com>
2024-02-26 14:22:11 +0100

    unit-test: extend validation of FetcherMemoryObserver.
    Change-Id: I673596b0983df3d45355fffaa43799234d0fc61e

--- tests/unit/cmk/utils/test_observer.py
+++ tests/unit/cmk/utils/test_observer.py
@@ -8,13 +8,14 @@ from cmk.utils.observer import FetcherMemoryObserver, vm_size
 
 LOG_MESSAGE = "13;heute;checking;60"
 ONE_KiB = 2**10  # 1024
+STEADY_CYCLE = 5
 
 
 def _change_state(
     observer: FetcherMemoryObserver, log: str | None = None, steady: bool = False
 ) -> None:
     """Change state of 'observer' by one cycle, unless 'steady' is mentioned."""
-    num_cycles = 5 if steady else 1
+    num_cycles = STEADY_CYCLE if steady else 1
     try:
         for _ in range(num_cycles):  # '5 checks to achieve steady state' is a business rule
             observer.check_resources(log, verbose=False)
@@ -25,31 +26,41 @@ def _change_state(
 def test_fetcher_memory_observer_before_steady() -> None:
     memory_used = ONE_KiB
     observer = FetcherMemoryObserver(100, lambda: memory_used)
+    initial_memory_usage = observer.memory_usage()
     _change_state(observer)
     # exceed 'hard_limit' of memory usage.
     memory_used += ONE_KiB
     # expected NO reaction on overflow BEFORE steady achieved
     _change_state(observer)
+    assert (
+        initial_memory_usage == observer.memory_usage() == 0
+    ), "Memory usage is updated ONLY at steady state!"
+    assert observer._num_check_cycles == 2
 
 
 def test_fetcher_memory_observer_steady_setup() -> None:
-    observer = FetcherMemoryObserver(100)
-    assert observer.memory_usage() == 0
+    memory_usage = ONE_KiB
+    observer = FetcherMemoryObserver(100, lambda: memory_usage)
     _change_state(observer, steady=True, log=LOG_MESSAGE)
-    assert observer._context() == '[cycle 5, command "13;heute;checking;60"]'
-    assert observer.memory_usage() != 0
+    assert observer._context() == f'[cycle {STEADY_CYCLE}, command "{LOG_MESSAGE}"]'
+    assert observer.memory_usage() == memory_usage
 
 
 def test_fetcher_memory_observer_overflow() -> None:
     memory_used = ONE_KiB
     observer = FetcherMemoryObserver(100, lambda: memory_used)
     _change_state(observer, steady=True, log=LOG_MESSAGE)
+    steady_state_memory_usage = observer.memory_usage()
     # exceed 'hard_limit' of memory usage.
     overflow = 1
     memory_used += overflow
     # expected EXIT on overflow AFTER steady achieved
     with pytest.raises(SystemExit) as exit_expected:
         observer.check_resources(None, False)
+    assert observer._num_check_cycles == STEADY_CYCLE + 1
+    assert (
+        observer.memory_usage() == steady_state_memory_usage
+    ), "Memory usage is updated ONLY at steady state!"
     assert exit_expected.value.code == 14
 
 
@@ -60,10 +71,14 @@ def test_fetcher_memory_observer_no_overflow(delta: int) -> None:
     # 'allowed_growth' calculates 'hard_limit' as a factor of current memory usage.
     observer = FetcherMemoryObserver(factor * 100, lambda: memory_used)
     _change_state(observer, steady=True, log=LOG_MESSAGE)
+    steady_state_memory_usage = observer.memory_usage()
     # define 'hard_limit' of memory usage.
     memory_used = (memory_used * factor) - delta
     # expected NO reaction
     _change_state(observer)
+    assert (
+        observer.memory_usage() == steady_state_memory_usage
+    ), "Memory usage is updated ONLY at steady state!"
 
 
 def test_fetcher_memory_obeserver_vm_size():

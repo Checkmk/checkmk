f24714a2ce8d918cc4d76a755e94130cf7eb1bc6
Dhananjay Shah <dhananjay.shah@checkmk.com>
2024-02-26 12:04:48 +0100

    unit-tests: reuse code; 'achieve_steady_state' -> 'change_state'.
    Reuse try-except block for test-cases.
    
    Change-Id: Ie943aacd2996edf9227500d7b5d7ec160666072c

--- tests/unit/cmk/utils/test_observer.py
+++ tests/unit/cmk/utils/test_observer.py
@@ -6,41 +6,43 @@ import pytest
 
 from cmk.utils.observer import FetcherMemoryObserver
 
+LOG_MESSAGE = "13;heute;checking;60"
 
-def _achieve_steady_state(observer: FetcherMemoryObserver) -> None:
-    """Wait for steady and ignore any problems"""
+
+def _change_state(
+    observer: FetcherMemoryObserver, log: str | None = None, steady: bool = False
+) -> None:
+    """Change state of 'observer' by one cycle, unless 'steady' is mentioned."""
+    num_cycles = 5 if steady else 1
     try:
-        for _ in range(5):  # '5 checks ti achieve steady state' is a business rule
-            observer.check_resources("13;heute;checking;60")
-    except SystemExit:
-        pytest.fail("Should not exit at the phase")
+        for _ in range(num_cycles):  # '5 checks to achieve steady state' is a business rule
+            observer.check_resources(log, verbose=False)
+    except SystemExit as excp:
+        pytest.fail(f"Should not exit at the phase\n{excp}")
 
 
 def test_fetcher_memory_observer_before_steady() -> None:
     observer = FetcherMemoryObserver(100)
-    observer.check_resources(None)
+    _change_state(observer)
     # use real RAM
     very_big_object = []
     for _ in range(10000):
         very_big_object.append(10000 * "aaa")
     # expected NO reaction on overflow BEFORE steady achieved
-    try:
-        observer.check_resources(None)
-    except SystemExit:
-        pytest.fail("Should not exit at the phase")
+    _change_state(observer)
 
 
 def test_fetcher_memory_observer_steady_setup() -> None:
     observer = FetcherMemoryObserver(100)
     assert observer.memory_usage() == 0
-    _achieve_steady_state(observer)
+    _change_state(observer, steady=True, log=LOG_MESSAGE)
     assert observer._context() == '[cycle 5, command "13;heute;checking;60"]'
     assert observer.memory_usage() != 0
 
 
 def test_fetcher_memory_overflow() -> None:
     observer = FetcherMemoryObserver(100)
-    _achieve_steady_state(observer)
+    _change_state(observer, steady=True, log=LOG_MESSAGE)
     # use real RAM
     very_big_object = []
     for _ in range(10000):
@@ -54,21 +56,18 @@ def test_fetcher_memory_overflow() -> None:
 
 def test_fetcher_memory_observer_no_overflow() -> None:
     observer = FetcherMemoryObserver(1000)
-    _achieve_steady_state(observer)
+    _change_state(observer, steady=True, log=LOG_MESSAGE)
     very_big_object = []
     for _ in range(10000):
         very_big_object.append(10000 * "aaa")
     # no reaction for very big numbers(as checker)
-    try:
-        observer.check_resources(None, False)
-    except SystemExit:
-        pytest.fail("Should not exit at the phase")
+    _change_state(observer)
 
 
 def test_fetcher_memory_observer_hard_limit() -> None:
     ram_size = 10000
     observer = FetcherMemoryObserver(200, lambda: ram_size)
-    _achieve_steady_state(observer)
+    _change_state(observer, steady=True, log=LOG_MESSAGE)
 
     ram_size = observer.memory_usage() * 4 + 1000  # simulate hard limit break
     with pytest.raises(SystemExit) as exit_expected:

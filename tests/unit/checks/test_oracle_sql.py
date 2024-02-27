120d10ad2ea00ae1d49990b1f80dbb9b231ffbc3
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2024-02-27 15:50:09 +0100

    Migrate oracle_sql to v2
    Change-Id: Ide03494e5a8fc7bcce76d078afbc725ea34e0128

--- tests/unit/checks/test_oracle_sql.py
+++ tests/unit/checks/test_oracle_sql.py
@@ -4,9 +4,14 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 import pytest
 
-from tests.testlib import Check
-
-from .checktestlib import assertDiscoveryResultsEqual, DiscoveryResult
+from cmk.agent_based.v2 import Metric, Result, Service, State
+from cmk.plugins.oracle.agent_based.oracle_sql import (
+    check_oracle_sql,
+    discovery_oracle_sql,
+    Instance,
+    parse_metrics,
+    parse_oracle_sql,
+)
 
 pytestmark = pytest.mark.checks
 check_name = "oracle_sql"
@@ -66,12 +71,12 @@ INFO_4 = [
         (
             INFO_1,
             {
-                "FOOBAR1 SQL YOLBE AFS RABAT REPL ERROR STMT": {
-                    "details": [],
-                    "elapsed": None,
-                    "exit": 0,
-                    "long": [],
-                    "parsing_error": {
+                "FOOBAR1 SQL YOLBE AFS RABAT REPL ERROR STMT": Instance(
+                    details=[],
+                    elapsed=None,
+                    exit=0,
+                    long=[],
+                    parsing_error={
                         ("instance", "PL/SQL failure", 2): [
                             "ERROR "
                             "at "
@@ -96,84 +101,117 @@ INFO_4 = [
                             "5:"
                         ]
                     },
-                    "perfdata": [],
-                }
+                    metrics=[],
+                )
             },
         ),
         (
             INFO_2,
             {
-                "FOOBAR1 SQL NBA SESSION LEVEL": {
-                    "details": ["Session Level: 5"],
-                    "elapsed": 0.26815,
-                    "exit": 0,
-                    "long": [],
-                    "parsing_error": {},
-                    "perfdata": [],
-                }
+                "FOOBAR1 SQL NBA SESSION LEVEL": Instance(
+                    details=["Session Level: 5"],
+                    elapsed=0.26815,
+                    exit=0,
+                    long=[],
+                    parsing_error={},
+                    metrics=[],
+                )
             },
         ),
         (
             INFO_3,
             {
-                "BULU SQL BLABLI NBA SHA FILE": {
-                    "details": [],
-                    "elapsed": 0.29285,
-                    "exit": 2,
-                    "long": [
+                "BULU SQL BLABLI NBA SHA FILE": Instance(
+                    details=[],
+                    elapsed=0.29285,
+                    exit=2,
+                    long=[
                         "Monitoring SHA/RAB Resultat = 1",
                         "TODO siehe FOOBAR; Monitoring SHA",
                     ],
-                    "parsing_error": {
+                    parsing_error={
                         ("unknown", 'Unexpected Keyword: "detail". Line was', 3): [
                             "detail:SHA-TT File (sha-ra), welches Sachen macht."
                         ]
                     },
-                    "perfdata": [],
-                }
+                    metrics=[],
+                )
             },
         ),
         (
             INFO_4,
             {
-                "YOBLE1 SQL NBA SESSIONS": {
-                    "details": [
-                        "Active sessions: 0 (warn/crit at "
-                        "10/20) / Inactive sessions: 0 "
-                        "(warn/crit at 10/40)"
+                "YOBLE1 SQL NBA SESSIONS": Instance(
+                    details=[
+                        "Active sessions: 0 (warn/crit "
+                        "at 10/20) / Inactive sessions: "
+                        "0 (warn/crit at 10/40)"
                     ],
-                    "elapsed": 0.29444,
-                    "exit": 0,
-                    "long": ["Avara SEP_ID: 301"],
-                    "parsing_error": {},
-                    "perfdata": [
-                        ("sessions_active", 0, 10, 20),
-                        ("sessions_inactive", 0, 10, 40),
-                        ("sessions_maxage", 0),
+                    elapsed=0.29444,
+                    exit=0,
+                    long=["Avara SEP_ID: 301"],
+                    parsing_error={},
+                    metrics=[
+                        Metric(name="sessions_active", value=0, levels=(10, 20), boundaries=None),
+                        Metric(name="sessions_inactive", value=0, levels=(10, 40), boundaries=None),
+                        Metric(name="sessions_maxage", value=0, levels=None, boundaries=None),
                     ],
-                }
+                )
             },
         ),
     ],
 )
 def test_oracle_sql_parse(info, expected):
-    assert Check(check_name).run_parse(info) == expected
+    assert parse_oracle_sql(info) == expected
+
+
+@pytest.mark.parametrize(
+    "line,expected",
+    [
+        (
+            "sessions_active=0",
+            [Metric(name="sessions_active", value=0, levels=None, boundaries=None)],
+        ),
+        (
+            "sessions_active=0;10;20",
+            [Metric(name="sessions_active", value=0, levels=(10, 20), boundaries=None)],
+        ),
+        (
+            "sessions_active=0;10;20;30;40",
+            [Metric(name="sessions_active", value=0, levels=(10, 20), boundaries=(30, 40))],
+        ),
+        (
+            "one=0;10;20;30;40 two=1;2;3 three=2;3;4;5;6",
+            [
+                Metric(name="one", value=0, levels=(10, 20), boundaries=(30, 40)),
+                Metric(name="two", value=1, levels=(2, 3), boundaries=None),
+                Metric(name="three", value=2, levels=(3, 4), boundaries=(5, 6)),
+            ],
+        ),
+        (
+            "tasks_waiting=2;;100 task_working=7",
+            [
+                Metric(name="tasks_waiting", value=2, levels=(None, 100), boundaries=None),
+                Metric(name="task_working", value=7, levels=None, boundaries=None),
+            ],
+        ),
+    ],
+)
+def test_parse_metrics(line, expected):
+    assert list(parse_metrics(line)) == expected
 
 
 @pytest.mark.parametrize(
     "info,expected",
     [
-        (INFO_1, [("FOOBAR1 SQL YOLBE AFS RABAT REPL ERROR STMT", {})]),
-        (INFO_2, [("FOOBAR1 SQL NBA SESSION LEVEL", {})]),
-        (INFO_3, [("BULU SQL BLABLI NBA SHA FILE", {})]),
-        (INFO_4, [("YOBLE1 SQL NBA SESSIONS", {})]),
+        (INFO_1, [Service(item="FOOBAR1 SQL YOLBE AFS RABAT REPL ERROR STMT")]),
+        (INFO_2, [Service(item="FOOBAR1 SQL NBA SESSION LEVEL")]),
+        (INFO_3, [Service(item="BULU SQL BLABLI NBA SHA FILE")]),
+        (INFO_4, [Service(item="YOBLE1 SQL NBA SESSIONS")]),
     ],
 )
 def test_oracle_sql_discovery(info, expected):
-    oracle_sql = Check(check_name)
-    discovery_result = DiscoveryResult(oracle_sql.run_discovery(oracle_sql.run_parse(info)))
-    discovery_expected = DiscoveryResult(expected)
-    assertDiscoveryResultsEqual(oracle_sql, discovery_result, discovery_expected)
+    assert list(discovery_oracle_sql(parse_oracle_sql(info))) == expected
 
 
 @pytest.mark.parametrize(
@@ -183,51 +221,56 @@ def test_oracle_sql_discovery(info, expected):
             INFO_1,
             "FOOBAR1 SQL YOLBE AFS RABAT REPL ERROR STMT",
             [
-                (
-                    2,
-                    "PL/SQL failure: ERROR at line 17: ORA-06550: line 17, column 5: PL/SQL: "
-                    "ORA-00933: SQL command not properly ended ORA-06550: line 7, column 5:",
+                Result(
+                    state=State.CRIT,
+                    summary="PL/SQL failure: ERROR at line 17: ORA-06550: line 17, "
+                    "column 5: PL/SQL: ORA-00933: "
+                    "SQL command not properly ended ORA-06550: line 7, column 5:",
                 )
             ],
         ),
         (
             INFO_2,
             "FOOBAR1 SQL NBA SESSION LEVEL",
-            [(0, "Session Level: 5", [("elapsed_time", 0.26815)])],
+            [
+                Result(state=State.OK, summary="Session Level: 5"),
+                Metric("elapsed_time", 0.26815),
+            ],
         ),
         (
             INFO_3,
             "BULU SQL BLABLI NBA SHA FILE",
             [
-                (
-                    3,
-                    'Unexpected Keyword: "detail". Line was: detail:SHA-TT File (sha-ra), '
-                    "welches Sachen macht.",
+                Result(
+                    state=State.UNKNOWN,
+                    summary='Unexpected Keyword: "detail". Line was: '
+                    "detail:SHA-TT File (sha-ra), welches Sachen macht.",
                 ),
-                (0, "\nMonitoring SHA/RAB Resultat = 1\nTODO siehe FOOBAR; Monitoring SHA"),
+                Result(
+                    state=State.OK,
+                    summary="Monitoring SHA/RAB Resultat = 1",
+                    details="TODO siehe FOOBAR; Monitoring SHA",
+                ),
             ],
         ),
         (
             INFO_4,
             "YOBLE1 SQL NBA SESSIONS",
             [
-                (
-                    0,
-                    "Active sessions: 0 (warn/crit at 10/20) / Inactive sessions: 0 (warn/crit "
-                    "at 10/40)",
-                    [
-                        ("sessions_active", 0, 10, 20),
-                        ("sessions_inactive", 0, 10, 40),
-                        ("sessions_maxage", 0),
-                        ("elapsed_time", 0.29444),
-                    ],
+                Result(
+                    state=State.OK,
+                    summary="Active sessions: 0 (warn/crit at 10/20) / "
+                    "Inactive sessions: 0 (warn/crit at 10/40)",
                 ),
-                (0, "\nAvara SEP_ID: 301"),
+                Metric("sessions_active", 0, levels=(10, 20)),
+                Metric("sessions_inactive", 0, levels=(10, 40)),
+                Metric("sessions_maxage", 0),
+                Metric("elapsed_time", 0.29444),
+                Result(state=State.OK, summary="Avara SEP_ID: 301"),
             ],
         ),
     ],
 )
 def test_oracle_sql_check(info, item, expected):
-    oracle_sql = Check(check_name)
-    result = list(oracle_sql.run_check(item, {}, oracle_sql.run_parse(info)))
+    result = list(check_oracle_sql(item, {}, parse_oracle_sql(info)))
     assert result == expected

1b72c824553b04a09fa6059f2d8e6691922c9410
m3rlinux <m3rlinux.it@gmail.com>
2024-02-27 16:18:21 +0100

    17337 new txt mapping for PS redundancy lost
    The IPMI sensor check would result in an OK state when confronted with a status
    message like this:
    
        "ok (redundancy lost)"
    
    This change adds one more mapping indicating an error and reports a service with
    the above string CRIT.
    
    closes #760
    
    Change-Id: I044ebe905522e6ecc8dee3c67fa58745c19e8641

--- cmk/base/plugins/agent_based/ipmi.py
+++ cmk/base/plugins/agent_based/ipmi.py
@@ -268,6 +268,7 @@ def ipmi_status_txt_mapping(status_txt: str) -> State:
             "drive fault",
             "predictive failure",
             "power supply ac lost",
+            "redundancy lost",
         )
     ):
         return State.OK

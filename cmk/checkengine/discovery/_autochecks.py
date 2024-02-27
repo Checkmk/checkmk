1ce49494a44962804a2dd3df6408c2583f983f19
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-02-27 09:28:44 +0100

    Revert "ignore errors in autocheck when config is generated"
    This reverts commit a90fcf18765b8251a1746b8c1d8996729efb0608.

--- cmk/checkengine/discovery/_autochecks.py
+++ cmk/checkengine/discovery/_autochecks.py
@@ -10,11 +10,9 @@ from pathlib import Path
 from typing import NamedTuple, TypedDict
 
 import cmk.utils.paths
-import cmk.utils.tty as tty
 from cmk.utils.exceptions import MKGeneralException
 from cmk.utils.hostaddress import HostName
 from cmk.utils.labels import ServiceLabel
-from cmk.utils.log import console
 from cmk.utils.servicename import Item, ServiceName
 from cmk.utils.store import ObjectStore
 
@@ -231,15 +229,9 @@ class AutochecksManager:
         self,
         hostname: HostName,
     ) -> Sequence[AutocheckEntry]:
-        try:
-            if hostname not in self._raw_autochecks_cache:
-                self._raw_autochecks_cache[hostname] = AutochecksStore(hostname).read()
-            return self._raw_autochecks_cache[hostname]
-        except MKGeneralException as e:
-            console.error(
-                f"\n{tty.red + tty.bold}Error{tty.normal} processing autochecks: \n{tty.yellow + tty.bold}{e}{tty.normal}\n"
-            )
-        return []
+        if hostname not in self._raw_autochecks_cache:
+            self._raw_autochecks_cache[hostname] = AutochecksStore(hostname).read()
+        return self._raw_autochecks_cache[hostname]
 
 
 def set_autochecks_of_real_hosts(

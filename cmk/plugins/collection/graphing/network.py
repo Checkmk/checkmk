2be2537994fe0cbdbd24d7f68670d1ce0b1cd2a4
Simon Jess <simon.jess@checkmk.com>
2024-03-05 11:16:06 +0100

    Add missing metrics
    Change-Id: Ia9c45b2d87973b6ecd4ce22ce0fb99c8f2d506ff

--- cmk/plugins/collection/graphing/network.py
+++ cmk/plugins/collection/graphing/network.py
@@ -5,7 +5,23 @@
 
 from cmk.graphing.v1 import graphs, metrics, Title
 
+UNIT_BYTES_PER_SECOND = metrics.Unit(metrics.SINotation("B/s"))
+UNIT_BYTES_PER_REQUEST = metrics.Unit(metrics.SINotation("B/req"))
 UNIT_NUMBER = metrics.Unit(metrics.DecimalNotation(""), metrics.StrictPrecision(2))
+
+metric_data_transfer_rate = metrics.Metric(
+    name="data_transfer_rate",
+    title=Title("Data transfer rate"),
+    unit=UNIT_BYTES_PER_SECOND,
+    color=metrics.Color.LIGHT_BLUE,
+)
+
+metric_request_transfer_rate = metrics.Metric(
+    name="request_transfer_rate",
+    title=Title("Request transfer rate"),
+    unit=UNIT_BYTES_PER_REQUEST,
+    color=metrics.Color.LIGHT_GREEN,
+)
 
 metric_active_connections = metrics.Metric(
     name="active_connections",

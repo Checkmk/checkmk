71fe83879289543ed3d0b608a6291e4eb5398ca9
Sven Panne <sven.panne@checkmk.com>
2024-03-11 13:20:49 +0100

    Added -I/--features flag to Rust run scripts and use it.
    This makes things more explicit and consistent.
    
    Change-Id: I8b5279d1648e899e977978d35c652e81eabafd73

--- buildscripts/scripts/test-package-check-cert.groovy
+++ buildscripts/scripts/test-package-check-cert.groovy
@@ -6,7 +6,7 @@ def main() {
     dir("${checkout_dir}") {
         docker_image_from_alias("IMAGE_TESTING").inside() {
             stage('Test Package check-cert') {
-                sh("packages/check-cert/run --setup-environment --clean --all");
+                sh("packages/check-cert/run --setup-environment --clean --all --features=vendored");
             }
         }
     }

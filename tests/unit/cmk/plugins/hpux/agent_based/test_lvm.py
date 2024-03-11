4bf22e40b7feafc35a60ead1b7e62f66955c6de5
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-03-11 14:48:08 +0100

    16381 HP-UX: Mirror State of LVM Logical Volumes: be more robust in state evaluation
    The plugin previously only considered the status to be OK if the device reported a literal "available,syncd".
    It now is OK for any combination of (some of) the words "available", "syncd", "snapshot" and "space_efficient".
    
    FEED-7925
    
    Change-Id: Icd42709e240abdeda213d9cfadeb4dc7bcf8e66b

--- tests/unit/cmk/plugins/hpux/agent_based/test_lvm.py
+++ tests/unit/cmk/plugins/hpux/agent_based/test_lvm.py
@@ -11,8 +11,8 @@ vg_name=/dev/vg00:vg_write_access=read,write:vg_status=available:max_lv=255:\
 cur_lv=8:open_lv=8:max_pv=16:cur_pv=4:act_pv=4:max_pe_per_pv=4384:vgda=8:pe_size=16:to
 tal_pe=17388:alloc_pe=13920:free_pe=3468:total_pvg=0:total_spare_pvs=0:total_spare_pvs_in_use=0:vg_version=1.0.0
 lv_name=/dev/vg00/lvol1:lv_status=available,syncd:lv_size=1792:current_le=112:allocated_pe=224:used_pv=2
-lv_name=/dev/vg00/lvol2:lv_status=available,syncd:lv_size=32768:current_le=2048:allocated_pe=4096:used_pv=2
-lv_name=/dev/vg00/lvol3:lv_status=available,syncd:lv_size=2048:current_le=128:allocated_pe=256:used_pv=2
+lv_name=/dev/vg00/lvol2:lv_status=available,syncd,snapshot,space_efficient:lv_size=32768:current_le=2048:allocated_pe=4096:used_pv=2
+lv_name=/dev/vg00/lvol3:lv_status=available,syncd,possesssed_by_demon:lv_size=2048:current_le=128:allocated_pe=256:used_pv=2
 lv_name=/dev/vg00/lvol4:lv_status=available,syncd:lv_size=32768:current_le=2048:allocated_pe=4096:used_pv=2
 lv_name=/dev/vg00/lvol5:lv_status=available,syncd:lv_size=12288:current_le=768:allocated_pe=1536:used_pv=2
 lv_name=/dev/vg00/lvol6:lv_status=available,syncd:lv_size=5120:current_le=320:allocated_pe=640:used_pv=2
@@ -54,5 +54,22 @@ def test_check_hpux_lvm_missing() -> None:
 def test_check_hpux_lvm_ok() -> None:
     assert list(lvm.check_plugin_hpux_lvm.check_function("/dev/vg00/lvol1", _test_section())) == [
         Result(state=State.OK, summary="Status: available,syncd"),
+        Result(state=State.OK, summary="Volume group: /dev/vg00"),
+    ]
+
+
+def test_check_hpux_lvm_ok2() -> None:
+    assert list(lvm.check_plugin_hpux_lvm.check_function("/dev/vg00/lvol2", _test_section())) == [
+        Result(state=State.OK, summary="Status: available,syncd,snapshot,space_efficient"),
+        Result(state=State.OK, summary="Volume group: /dev/vg00"),
+    ]
+
+
+def test_check_hpux_lvm_made_up_not_ok() -> None:
+    assert list(lvm.check_plugin_hpux_lvm.check_function("/dev/vg00/lvol3", _test_section())) == [
+        Result(
+            state=State.CRIT,
+            summary="Status: available,syncd,possesssed_by_demon",  # that's probably not good
+        ),
         Result(state=State.OK, summary="Volume group: /dev/vg00"),
     ]

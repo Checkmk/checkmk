f3b2172f4411dd93cc8236358550f9976ac64937
Christopher Krause <christopher.krause@checkmk.com>
2024-03-14 11:07:32 +0100

    16476 add labels for Nutanix
    This werk adds the following labels:
    
    For the host with the Nutanix special agent:
    * cmk/nutanix/object:control_plane
    
    For Nutanix hosts:
    * cmk/nutanix/object:node
    * cmk/os_platform:nutanix
    * cmk/os_name:Nutanix AHV
    
    For Nutanix VMs:
    * cmk/nutanix/object:vm
    
    Change-Id: Ifcbae438343319e2bf376903c9f7be501eb59aaf
    JIRA-Ref: CMK-15660

--- cmk/base/plugins/agent_based/prism_vm.py
+++ cmk/base/plugins/agent_based/prism_vm.py
@@ -8,8 +8,8 @@ from typing import Any
 
 from cmk.plugins.lib.prism import load_json
 
-from .agent_based_api.v1 import register
-from .agent_based_api.v1.type_defs import StringTable
+from .agent_based_api.v1 import HostLabel, register
+from .agent_based_api.v1.type_defs import HostLabelGenerator, StringTable
 
 Section = Mapping[str, Any]
 
@@ -18,7 +18,20 @@ def parse_prism_vm(string_table: StringTable) -> Section:
     return load_json(string_table)
 
 
+def host_label_prism_vm(section: Section) -> HostLabelGenerator:
+    """Host label function
+
+    Labels:
+        cmk/nutanix/object:
+            This label is set to either "control_plane" for the host with the special agent, "node"
+            for Nutanix nodes or "vm" for VMs hosted in Nutanix.
+
+    """
+    yield HostLabel("cmk/nutanix/object", "vm")
+
+
 register.agent_section(
     name="prism_vm",
     parse_function=parse_prism_vm,
+    host_label_function=host_label_prism_vm,
 )

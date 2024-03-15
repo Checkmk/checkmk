7470da211f124c9d34ebf6d8a0705efd10edf1c4
Gavin McGuigan <gavin.mcguigan@checkmk.com>
2024-03-15 11:36:10 +0100

    Revert "ldap_connection: Implementation of GET endpoints"
    Revert submission 73595-master-endpoints-ldap
    
    Reason for revert: <Failing tests>
    
    Reverted changes: /q/submissionid:73595-master-endpoints-ldap
    
    Change-Id: I5ac879e3ecb6f1d7a9706506bc53cdf7c3a0561e

--- cmk/gui/openapi/registration.py
+++ cmk/gui/openapi/registration.py
@@ -22,7 +22,6 @@ from cmk.gui.openapi.endpoints import (
     host_group_config,
     host_internal,
     host_tag_group,
-    ldap_connection,
     metric,
     notification_rules,
     password,
@@ -54,7 +53,6 @@ def register(endpoint_registry: EndpointRegistry, job_registry: BackgroundJobReg
     contact_group_config.register(endpoint_registry)
     downtime.register(endpoint_registry)
     folder_config.register(endpoint_registry)
-    ldap_connection.register(endpoint_registry)
     host.register(endpoint_registry)
     host_config.register(endpoint_registry)
     host_group_config.register(endpoint_registry)

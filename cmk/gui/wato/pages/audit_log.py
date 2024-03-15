ed331a1c800ce2599aa375b76ab5304349e102ac
Ronny Bruska <ronny.bruska@checkmk.com>
2024-03-15 12:04:47 +0100

    Fix mode on page reload for audit log
    CMK-15799
    
    Change-Id: I4f9656f7b21866647517199cdd27657503c8557b

--- cmk/gui/wato/pages/audit_log.py
+++ cmk/gui/wato/pages/audit_log.py
@@ -274,7 +274,7 @@ class ModeAuditLog(WatoMode):
             user.need_permission("wato.auditlog")
             return self._export_audit_log(self._parse_audit_log())
 
-        return None
+        return redirect(makeuri(request, []))
 
     def page(self) -> None:
         with html.form_context("fileselection_form", method="POST"):

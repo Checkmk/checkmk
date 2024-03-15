b2f9e5d544f60e3957299588ff69401c054a96b6
Gavin McGuigan <gavin.mcguigan@checkmk.com>
2024-03-15 11:36:10 +0100

    Revert "ldap_connection: implementation of DELETE & POST endpoints"
    Revert submission 73595-master-endpoints-ldap
    
    Reason for revert: <Failing tests>
    
    Reverted changes: /q/submissionid:73595-master-endpoints-ldap
    
    Change-Id: I079698f017bb287cc38a06fcd90d3f9c0b0c2c95

--- cmk/gui/openapi/endpoints/ldap_connection/__init__.py
+++ cmk/gui/openapi/endpoints/ldap_connection/__init__.py
@@ -15,11 +15,6 @@ from cmk.gui.openapi.endpoints.ldap_connection.internal_to_restapi_interface imp
     LDAPConnectionInterface,
     request_ldap_connection,
     request_ldap_connections,
-    request_to_create_ldap_connection,
-    request_to_delete_ldap_connection,
-)
-from cmk.gui.openapi.endpoints.ldap_connection.request_schemas import (
-    LDAPConnectionConfigCreateRequest,
 )
 from cmk.gui.openapi.endpoints.ldap_connection.response_schemas import (
     LDAPConnectionResponse,
@@ -30,9 +25,7 @@ from cmk.gui.openapi.restful_objects.constructors import (
     collection_href,
     collection_object,
     domain_object,
-    hash_of_dict,
     object_href,
-    require_etag,
     response_with_etag_created_from_dict,
 )
 from cmk.gui.openapi.restful_objects.registry import EndpointRegistry
@@ -106,49 +99,6 @@ def show_ldap_connections(params: Mapping[str, Any]) -> Response:
     )
 
 
-@Endpoint(
-    object_href("ldap_connection", "{ldap_connection_id}"),
-    ".../delete",
-    method="delete",
-    etag="input",
-    path_params=[LDAP_CONNECTION_ID_EXISTS],
-    output_empty=True,
-    permissions_required=RW_PERMISSIONS,
-)
-def delete_ldap_connection(params: Mapping[str, Any]) -> Response:
-    """Delete an LDAP connection"""
-    user.need_permission("wato.edit")
-    user.need_permission("wato.seeall")
-    user.need_permission("wato.users")
-    ldap_id = params["ldap_connection_id"]
-    if (connection := request_ldap_connection(ldap_id=ldap_id)) is not None:
-        require_etag(hash_of_dict(connection.api_response()))
-        request_to_delete_ldap_connection(params["ldap_connection_id"])
-    return Response(status=204)
-
-
-@Endpoint(
-    collection_href("ldap_connection"),
-    "cmk/create",
-    method="post",
-    etag="output",
-    tag_group="Setup",
-    request_schema=LDAPConnectionConfigCreateRequest,
-    response_schema=LDAPConnectionResponse,
-    permissions_required=RW_PERMISSIONS,
-)
-def create_ldap_connection(params: Mapping[str, Any]) -> Response:
-    """Create an LDAP connection"""
-    user.need_permission("wato.edit")
-    user.need_permission("wato.seeall")
-    user.need_permission("wato.users")
-    connection = request_to_create_ldap_connection(params["body"])
-    return response_with_etag_created_from_dict(
-        serve_json(_serialize_ldap_connection(connection)),
-        connection.api_response(),
-    )
-
-
 def _serialize_ldap_connection(connection: LDAPConnectionInterface) -> DomainObject:
     return domain_object(
         domain_type="ldap_connection",
@@ -163,5 +113,3 @@ def _serialize_ldap_connection(connection: LDAPConnectionInterface) -> DomainObj
 def register(endpoint_registry: EndpointRegistry) -> None:
     endpoint_registry.register(show_ldap_connection)
     endpoint_registry.register(show_ldap_connections)
-    endpoint_registry.register(delete_ldap_connection)
-    endpoint_registry.register(create_ldap_connection)

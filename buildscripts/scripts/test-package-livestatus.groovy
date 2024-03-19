ddd78feb3ef2150c56efdac672e758f8c1eff24c
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-03-19 09:01:21 +0100

    Use specific file for issue analysis of C tests
    ... streamlined with stages.yml command
    
    CMK-16548
    
    Change-Id: I0f2ceff19a3a788e613af09b942047aa472b3896

--- buildscripts/scripts/test-package-livestatus.groovy
+++ buildscripts/scripts/test-package-livestatus.groovy
@@ -3,22 +3,17 @@
 /// file: test-livestatus.groovy
 
 def main() {
+    def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+
     dir("${checkout_dir}") {
         docker_image_from_alias("IMAGE_TESTING").inside() {
-            stage('Compile & Test Livestatus') {
-                sh("GCC_TOOLCHAIN=/opt/gcc-13.2.0 packages/livestatus/run --clean --all");
-            }
-        }
-        stage("Analyse Issues") {
-            publishIssues(
-                issues: [scanForIssues( tool: gcc())],
-                trendChartType: 'TOOLS_ONLY',
-                qualityGates: [[
-                    threshold: 1,
-                    type: 'TOTAL',
-                    unstable: false,
-                ]],
-            );
+            test_jenkins_helper.execute_test([
+                name: "test-livestatus",
+                cmd: "GCC_TOOLCHAIN=/opt/gcc-13.2.0 packages/livestatus/run --clean --all",
+                output_file: "livestatus.txt",
+            ]);
+
+            test_jenkins_helper.analyse_issues("GCC", "livestatus.txt");
         }
     }
 }

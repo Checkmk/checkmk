cfd5f45c3524c5f16ae4ecd84a8b49e15ed981fb
Giordano Tomassorri <giordano.tomassorri@checkmk.com>
2024-03-20 21:00:42 +0100

    Implement CSE license handler for the beta
    The beta phase of the SaaS edition will have a trial period that will
    go on until we do the actual release.
    During this period, the users can only have a limited number of services
    
    SAASDEV-1498
    
    Change-Id: Ie7d626b7365318108bba3a2fd009480a4ba9818b

--- cmk/base/core_config.py
+++ cmk/base/core_config.py
@@ -24,8 +24,8 @@ from cmk.utils.config_path import VersionedConfigPath
 from cmk.utils.exceptions import MKGeneralException
 from cmk.utils.hostaddress import HostAddress, HostName
 from cmk.utils.labels import Labels
-from cmk.utils.licensing.handler import LicenseState, LicensingHandler
-from cmk.utils.licensing.helper import get_licensed_state_file_path, write_licensed_state
+from cmk.utils.licensing.handler import LicensingHandler
+from cmk.utils.licensing.helper import get_licensed_state_file_path
 from cmk.utils.paths import core_helper_config_dir
 from cmk.utils.servicename import Item, ServiceName
 from cmk.utils.store import load_object_from_file, lock_checkmk_configuration, save_object_to_file
@@ -71,7 +71,7 @@ class MonitoringCore(abc.ABC):
         hosts_to_update: set[HostName] | None = None,
     ) -> None:
         licensing_handler = self._licensing_handler_type.make()
-        self._persist_licensed_state(licensing_handler.state)
+        licensing_handler.persist_licensed_state(get_licensed_state_file_path())
         self._create_config(
             config_path, config_cache, licensing_handler, passwords, hosts_to_update
         )
@@ -86,9 +86,6 @@ class MonitoringCore(abc.ABC):
         hosts_to_update: set[HostName] | None = None,
     ) -> None:
         raise NotImplementedError
-
-    def _persist_licensed_state(self, license_state: LicenseState) -> None:
-        write_licensed_state(get_licensed_state_file_path(), license_state)
 
 
 ActiveServiceID = tuple[str, Item]  # TODO: I hope the str someday (tm) becomes "CheckPluginName",
--- tests/unit/cmk/utils/licensing/test_handler.py
+++ tests/unit/cmk/utils/licensing/test_handler.py
@@ -3,9 +3,48 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from pathlib import Path
+
 import pytest
 
-from cmk.utils.licensing.handler import LicenseState
+from cmk.utils.licensing.handler import (
+    LicenseState,
+    LicensingHandler,
+    NotificationHandler,
+    RemainingTrialTime,
+    UserEffect,
+)
+
+
+class LicensingHandlerMock(LicensingHandler):
+    def __init__(self, state: LicenseState) -> None:
+        self._state = state
+
+    @property
+    def state(self) -> LicenseState:
+        return self._state
+
+    @classmethod
+    def make(cls) -> LicensingHandler:
+        raise NotImplementedError()
+
+    @property
+    def message(self) -> str:
+        raise NotImplementedError()
+
+    def effect_core(self, num_services: int, num_hosts_shadow: int) -> UserEffect:
+        raise NotImplementedError()
+
+    def effect(self, licensing_settings_link: str | None = None) -> UserEffect:
+        raise NotImplementedError()
+
+    @property
+    def notification_handler(self) -> NotificationHandler:
+        raise NotImplementedError()
+
+    @property
+    def remaining_trial_time_rounded(self) -> RemainingTrialTime:
+        raise NotImplementedError()
 
 
 @pytest.mark.parametrize(
@@ -19,3 +58,21 @@ from cmk.utils.licensing.handler import LicenseState
 )
 def test_license_state_readable(license_state: LicenseState, expected_readable: str) -> None:
     assert license_state.readable == expected_readable
+
+
+@pytest.mark.parametrize(
+    "license_state, expected_file_content",
+    [
+        (LicenseState.TRIAL, "0"),
+        (LicenseState.FREE, "0"),
+        (LicenseState.LICENSED, "1"),
+        (LicenseState.UNLICENSED, "0"),
+    ],
+)
+def test_write_licensed_file(
+    tmp_path: Path, license_state: LicenseState, expected_file_content: str
+) -> None:
+    state_file_path = tmp_path / "licensed_state"
+    licensing_handler = LicensingHandlerMock(license_state)
+    licensing_handler.persist_licensed_state(state_file_path)
+    assert state_file_path.read_text() == expected_file_content

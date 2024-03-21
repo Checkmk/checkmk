5f8e51b13ea871f18ee20d1e49e5f3a4a73a03c8
Simon Jess <simon.jess@checkmk.com>
2024-03-21 15:59:23 +0100

    livestatus_status graphing: Fix minor issues
    Change-Id: I022e6312792f823591b7a8d85ee938a859323e56

--- cmk/gui/graphing/_expression.py
+++ cmk/gui/graphing/_expression.py
@@ -16,6 +16,7 @@ from cmk.utils.metrics import MetricName
 from cmk.gui.graphing._type_defs import TranslatedMetric, UnitInfo
 
 from ._color import mix_colors, parse_color, render_color, scalar_colors
+from ._loader import get_unit_info
 from ._type_defs import GraphConsoldiationFunction
 from ._unit_info import unit_info
 
@@ -41,7 +42,7 @@ def _choose_operator_color(a: str, b: str) -> str:
 
 
 def _make_unit_info(explicit_unit_name: str, unit_info_: UnitInfo) -> UnitInfo:
-    return unit_info[explicit_unit_name] if explicit_unit_name else unit_info_
+    return get_unit_info(explicit_unit_name) if explicit_unit_name else unit_info_
 
 
 @dataclass(frozen=True)

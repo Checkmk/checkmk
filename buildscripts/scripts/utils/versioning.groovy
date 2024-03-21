a39a6ef68de59d340921f0856f5e60babeccc7f8
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2024-03-21 11:27:20 +0100

    Keep saas builds internally
    * the docker images for saas already go into our nexus registry
    * but the packages, nor the docker tar gz is needed in public
    
    CMK-16561
    
    Change-Id: I4161cfe805edc774c9b29178ef2626f1c83c2db6

--- buildscripts/scripts/utils/versioning.groovy
+++ buildscripts/scripts/utils/versioning.groovy
@@ -105,13 +105,13 @@ def configured_or_overridden_distros(edition, distros, use_case="daily") {
     }
 }
 
-def get_internal_distros_pattern() {
+def get_internal_artifacts_pattern() {
     docker_image_from_alias("IMAGE_TESTING").inside("${mount_reference_repo_dir}") {
         dir("${checkout_dir}") {
             return sh(script: """scripts/run-pipenv run \
                   buildscripts/scripts/get_distros.py \
                   --editions_file "editions.yml" \
-                  internal_distros \
+                  internal_build_artifacts \
                   --as-codename \
                   --as-rsync-exclude-pattern;
             """, returnStdout: true).trim();

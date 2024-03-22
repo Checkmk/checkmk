Checkmk/checkmk

commit ab43e5dddfd6d4b1252970ac7601e1e2ce154916

Add possibility to configure log rotation for win-agent
CMK-4526

Change-Id: Ie76cff5287a9af0bc5b447d882df5f3a90efc74c

agents/wnx/src/engine/service_processor.cpp
@@ -181,8 +181,7 @@ std::string FindWinPerfExe() {
         for (const auto &name : names) {
             std::error_code ec;
             if (fs::exists(name, ec)) {
-                exe_name = name.u8string();
-                XLOG::d.i("Using file '{}' for winperf", exe_name);
+                XLOG::d.i("Using file '{}' for winperf", name);
                 break;
             }
         }

agents/wnx/install/resources/check_mk.user.yml
@@ -169,6 +169,9 @@ global:
         # you may disable your eventlog ability
         eventlog: yes
 
+        max_file_count: 5  # log rotation files quantity,  allowed 1..1024 
+        max_file_size: 8000000 # allowed 200K..200MB
+
 ps:
     # enabled: yes
     # use_wmi: yes

agents/wnx/install/resources/check_mk.yml
@@ -129,6 +129,9 @@ global:
         # you may disable your eventlog ability
         eventlog: yes
 
+        max_file_count: 5  # log rotation files quantity,  allowed 1..1024 
+        max_file_size: 8000000 # allowed 200K..200MB
+
 ps:
     enabled: yes
     # This is required for the additional ps feature below.

agents/wnx/src/common/cfg_info.h
@@ -63,8 +63,8 @@ enum LogLevel { kLogBase = 0, kLogDebug = 1, kLogAll = 2 };
 /// \brief  If true, than modules will be moved to %temp% for later usage
 constexpr bool g_quick_module_reinstall_allowed{true};
 
-constexpr int kBackupLogMaxCount = 5;
-constexpr size_t kBackupLogMaxSize = 8 * 1024 * 1024;
+constexpr int kLogFileMaxCount = 5;
+constexpr size_t kLogFileMaxSize = 8'000'000;
 
 constexpr uint32_t kMaxOhmErrorsBeforeRestart = 3;
 

agents/wnx/src/common/cfg_yaml.h
@@ -80,6 +80,8 @@ const char *const kLogDebug = "debug";        // string no, yes, all
 const char *const kLogWinDbg = "windbg";      // bool
 const char *const kLogEvent = "eventlog";     // bool
 const char *const kLogFile = "file";          // string
+const char *const kLogFileMaxFileCount = "max_file_count";  // string
+const char *const kLogFileMaxFileSize = "max_file_size";    // string
 
 // group plugins
 const char *const kPluginsFolders = "folders";            // seq

agents/wnx/src/engine/cfg.h
@@ -773,6 +773,8 @@ private:
     bool windbg_;
     bool event_log_;
     std::string log_file_name_;
+    uint32_t log_file_max_count_;
+    uint32_t log_file_max_size_;
 
     // derivative
     std::filesystem::path logfile_;

agents/wnx/src/engine/cfg_details.h
@@ -398,10 +398,6 @@ public:
         return path_to_msi_exec_;
     }
 
-    size_t getBackupLogMaxSize() const noexcept { return backup_log_max_size_; }
-
-    int getBackupLogMaxCount() const noexcept { return backup_log_max_count_; }
-
     void setConfiguredLogFileDir(const std::wstring &Path) {
         std::lock_guard lk(lock_);
         logfile_dir_ = Path;
@@ -469,9 +465,6 @@ private:
     bool aggregated_ = false;
     bool generated_ = false;
     bool ok_ = false;
-
-    std::atomic<int> backup_log_max_count_ = kBackupLogMaxCount;
-    std::atomic<size_t> backup_log_max_size_ = kBackupLogMaxSize;
 
     static std::atomic<uint64_t> g_uniq_id;
 

agents/wnx/src/engine/groups.cpp
@@ -103,6 +103,10 @@ void Global::loadFromMainConfig() {
         event_log_ = GetVal(logging, vars::kLogEvent, true);
 
         log_file_name_ = GetVal(logging, vars::kLogFile, std::string());
+        log_file_max_count_ =
+            GetVal(logging, vars::kLogFile, cfg::kLogFileMaxCount);
+        log_file_max_size_ =
+            GetVal(logging, vars::kLogFile, cfg::kLogFileMaxCount);
         updateLogNames();
     }
     // UNLOCK HERE

commit f1dcd775d35fe1efad653b984ece761994d564bb

wa: use separate process to get winperf data
This fixes the regression introduced in ab43e5dddfd6d4b1252970ac7601e1e2ce154916

Change-Id: I5340daa26290d53b3a7bcd0fe6a289eab5a0dfc4

agents/wnx/include/wnx/service_processor.h
@@ -702,6 +702,8 @@ void WaitForAsyncPluginThreads(std::chrono::duration<T, B> allowed_wait) {
               PluginEntry::threadCount(), (allowed_wait - wait_time).count());
 }
 
+std::string FindWinPerfExe(std::string_view exe_name);
+
 }  // namespace cma::srv
 
 #endif  // SERVICE_PROCESSOR_H

agents/wnx/src/engine/service_processor.cpp
@@ -209,39 +209,35 @@ void ServiceProcessor::stopTestingMainThread() {
     thread_.join();
 }
 
-namespace {
-std::string FindWinPerfExe() {
-    auto exe_name = cfg::groups::g_winperf.exe();
-
-    if (tools::IsEqual(exe_name, "agent")) {
-        XLOG::t.i("Looking for default agent");
-        const fs::path f{cfg::GetRootDir()};
-        std::vector names{f / cfg::kDefaultAppFileName};
-
-        if constexpr (tgt::Is64bit()) {
-            names.emplace_back(f / "check_mk_service64.exe");
-        }
-
-        names.emplace_back(f / "check_mk_service32.exe");
-
-        exe_name.clear();
-        for (const auto &name : names) {
-            std::error_code ec;
-            if (fs::exists(name, ec)) {
-                XLOG::d.i("Using file '{}' for winperf", name);
-                break;
-            }
-        }
-        if (exe_name.empty()) {
-            XLOG::l.crit("In folder '{}' not found binaries to exec winperf");
-            return {};
-        }
-    } else {
+std::string FindWinPerfExe(std::string_view exe_name) {
+    if (!tools::IsEqual(exe_name, "agent")) {
         XLOG::d.i("Looking for agent '{}'", exe_name);
+        return std::string{exe_name};
     }
-    return exe_name;
+
+    XLOG::t.i("Looking for default agent");
+    const fs::path f{cfg::GetRootDir()};
+    std::vector names{f / cfg::kDefaultAppFileName};
+
+    if constexpr (tgt::Is64bit()) {
+        names.emplace_back(f / "check_mk_service64.exe");
+    }
+
+    names.emplace_back(f / "check_mk_service32.exe");
+
+    for (const auto &name : names) {
+        std::error_code ec;
+        if (fs::exists(name, ec)) {
+            XLOG::d.i("Using file '{}' for winperf", name);
+            return name.string();
+        }
+    }
+
+    XLOG::l.crit("In folder '{}' not found binaries to exec winperf");
+    return {};
 }
 
+namespace {
 std::wstring GetWinPerfLogFile() {
     return cfg::groups::g_winperf.isTrace()
                ? (fs::path{cfg::GetLogDir()} / "winperf.log").wstring()
@@ -257,7 +253,8 @@ void ServiceProcessor::kickWinPerf(AnswerId answer_id,
         cmd_line = L"ip:" + wtools::ConvertToUtf16(ip_addr) + L" " + cmd_line;
     }
 
-    auto exe_name = wtools::ConvertToUtf16(FindWinPerfExe());
+    auto exe_name =
+        wtools::ConvertToUtf16(FindWinPerfExe(cfg::groups::g_winperf.exe()));
     const auto timeout = cfg::groups::g_winperf.timeout();
     auto prefix = wtools::ConvertToUtf16(cfg::groups::g_winperf.prefix());
 

agents/wnx/watest/test-service-processor.cpp
@@ -371,4 +371,21 @@ TEST(ServiceProcessorTest, DirectCallWmi) {
                                    provider::kSubSectionComputerSystem));
 }
 
+TEST(ServiceProcessorTest, FindWinPerfDefault) {
+    const auto temp_fs = tst::TempCfgFs::Create();
+    ASSERT_TRUE(temp_fs->loadFactoryConfig());
+    tst::CreateTextFile(temp_fs->root() / "check-mk-service32.exe", "");
+    EXPECT_TRUE(FindWinPerfExe("agent").empty());
+    tst::CreateTextFile(temp_fs->root() / "check_mk_service32.exe", "");
+    EXPECT_EQ(FindWinPerfExe("agent"),
+              temp_fs->root() / "check_mk_service32.exe");
+    tst::CreateTextFile(temp_fs->root() / "check_mk_agent.exe", "");
+    EXPECT_EQ(FindWinPerfExe("agent"), temp_fs->root() / "check_mk_agent.exe");
+}
+
+TEST(ServiceProcessorTest, FindWinPerfCustom) {
+    EXPECT_TRUE(FindWinPerfExe("agent").empty());
+    EXPECT_EQ(FindWinPerfExe("agent.exe"), "agent.exe");
+}
+
 }  // namespace cma::srv
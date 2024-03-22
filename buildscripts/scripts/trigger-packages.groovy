f595f7633c3b5267a64c0043c0676a5ac463197f
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2024-03-22 14:13:20 +0100

    Switch to fetch_job_artifacts
    Change-Id: I60564786372ffc1426d63724210fb8dfe6d76759

--- buildscripts/scripts/trigger-packages.groovy
+++ buildscripts/scripts/trigger-packages.groovy
@@ -4,33 +4,36 @@
 
 def build_stages(packages_file) {
     def packages = load_json(packages_file);
-    def package_job = "build-package"
     def notify = load("${checkout_dir}/buildscripts/scripts/utils/notify.groovy");
-    def jobs = [:];
 
-    return packages.collectEntries { p ->
-        [("${p.name}"): {
-            stage(p.name) {
-                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                    // TODO: Switch to ci_artifcats / fetch_artifacts
-                    jobs[p.name] = build(
-                        job: package_job,
-                        parameters: [
-                            string(name: "PACKAGE_PATH", value: p.path),
-                            string(name: "SECRET_VARS", value: p.sec_vars.join(",")),
-                            string(name: "COMMAND_LINE", value: p.command_line),
-                            string(name: "CUSTOM_GIT_REF", value: cmd_output("git rev-parse HEAD")),
-                        ],
-                        propagate: false,
-                    );
-                    if (jobs[p.name].result != "SUCCESS") {
-                        notify.notify_maintainer_of_package(p.maintainers, p.name, "${jobs[p.name].getAbsoluteUrl()}" + "console")
-                        throw new Exception("Job ${p.name} failed");
+    docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
+        docker_image_from_alias("IMAGE_TESTING").inside() {
+            sh("make .venv")
+            parallel packages.collectEntries { p ->
+                [("${p.name}"): {
+                    stage(p.name) {
+                        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+                            def job = upstream_build(
+                                force_build: true,
+                                download: false,
+                                relative_job_name: "builders/build-cmk-package",
+                                build_params: [
+                                    "PACKAGE_PATH":  p.path,
+                                    "SECRET_VARS": p.sec_vars.join(","),
+                                    "COMMAND_LINE": p.command_line,
+                                    "CUSTOM_GIT_REF": cmd_output("git rev-parse HEAD"),
+                                ]
+                            )
+                            if (job.result != "SUCCESS") {
+                                notify.notify_maintainer_of_package(p.maintainers, p.name, "${job.url}" + "console")
+                                throw new Exception("Job ${p.name} failed");
+                            }
+                        }
                     }
                 }
+                ]
             }
         }
-        ]
     }
 }
 
@@ -53,7 +56,7 @@ def main() {
 
         preparation(packages_file)
 
-        parallel build_stages(packages_file);
+        build_stages(packages_file);
 
         show_duration("archiveArtifacts") {
             archiveArtifacts(allowEmptyArchive: true, artifacts: 'results/*');

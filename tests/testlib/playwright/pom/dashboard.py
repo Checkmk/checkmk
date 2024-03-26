8644c99250cccbad33c51096044c536ea892abc4
Dhananjay Shah <dhananjay.shah@checkmk.com>
2024-03-26 16:24:40 +0100

    gui-e2e tests: migrate base functionality into "CmkPage".
    + generalize argument type to "str" for "LocatorHelper::locator".
    + define base class: CmkPage corresponding to general GUI page interaction.
        The object inherits from "LocatorHelper".
    + migrate base functionality away from PPage into CmkPage.
        PPage inherits from CmkPage.
    
    Change-Id: Ib6c021e45d0a112b36af39bd44f1b9a932886110

--- tests/testlib/playwright/pom/dashboard.py
+++ tests/testlib/playwright/pom/dashboard.py
@@ -6,27 +6,16 @@
 from typing import Literal
 from urllib.parse import urljoin, urlsplit
 
-from playwright.sync_api import Error, expect, Locator, Page, Response
-from playwright.sync_api import TimeoutError as PWTimeoutError
+from playwright.sync_api import Page, Response
 
-from tests.testlib.playwright.helpers import Keys, LocatorHelper
-from tests.testlib.playwright.pom.navigation import MainArea, MainMenu, Sidebar
-from tests.testlib.playwright.timeouts import TemporaryTimeout, TIMEOUT_ACTIVATE_CHANGES_MS
+from tests.testlib.playwright.pom.navigation import CmkPage
 
 
-class PPage(LocatorHelper):
+class PPage(CmkPage):
     """Playwright Page, wrapper around the page"""
 
-    def __init__(
-        self,
-        page: Page,
-        site_id: str,
-        site_url: str | None = None,
-    ) -> None:
+    def __init__(self, page: Page, site_id: str, site_url: str | None = None) -> None:
         super().__init__(page)
-        self.main_menu = MainMenu(self.page)
-        self.main_area = MainArea(self.page)
-        self.sidebar = Sidebar(self.page)
         self.site_id = site_id
         if site_url:
             self.site_url = site_url
@@ -34,9 +23,6 @@ class PPage(LocatorHelper):
             self.site_url = "".join(urlsplit(self.page.url)[0:2])
         self.username = ""
         self.password = ""
-
-    def locator(self, selector: str = "xpath=.") -> Locator:
-        return self.page.locator(selector)
 
     def login(self, username: str = "", password: str = "") -> None:
         """login to cmk"""
@@ -53,39 +39,6 @@ class PPage(LocatorHelper):
     def logout(self) -> None:
         self.main_menu.user_logout.click()
 
-    def activate_selected(self) -> None:
-        with TemporaryTimeout(self.page, TIMEOUT_ACTIVATE_CHANGES_MS):
-            self.main_area.locator("#menu_suggestion_activate_selected").click()
-
-    def expect_success_state(self) -> None:
-        expect(
-            self.main_area.locator("#site_gui_e2e_central_status.msg.state_success")
-        ).to_be_visible(timeout=TIMEOUT_ACTIVATE_CHANGES_MS)
-
-        expect(
-            self.main_area.locator("#site_gui_e2e_central_progress.progress.state_success")
-        ).to_be_visible(timeout=TIMEOUT_ACTIVATE_CHANGES_MS)
-
-        # assert no further changes are pending
-        expect(self.main_area.locator("div.page_state.no_changes")).to_be_visible(
-            timeout=TIMEOUT_ACTIVATE_CHANGES_MS
-        )
-
-    def goto_main_dashboard(self) -> None:
-        """Click the banner and wait for the dashboard"""
-        self.main_menu.main_page.click()
-        self.main_area.check_page_title("Main dashboard")
-
-    def select_host(self, host_name: str) -> None:
-        self.main_area.locator(f"td a:has-text('{host_name}')").click()
-
-    def goto_add_sidebar_element(self) -> None:
-        self.locator("div#check_mk_sidebar >> div#add_snapin > a").click()
-        self.main_area.check_page_title("Add sidebar element")
-
-    def press_keyboard(self, key: Keys) -> None:
-        self.page.keyboard.press(str(key.value))
-
     def go(
         self,
         url: str | None = None,
@@ -96,48 +49,4 @@ class PPage(LocatorHelper):
         """calls page.goto() but will accept relative urls"""
         return self.page.goto(
             urljoin(self.site_url, url), timeout=timeout, wait_until=wait_until, referer=referer
-        )
-
-    def click_and_wait(
-        self,
-        locator: Locator,
-        navigate: bool = False,
-        expected_locator: Locator | None = None,
-        reload_on_error: bool = False,
-        max_tries: int = 10,
-    ) -> None:
-        """Wait until the specified locator could be clicked.
-        After a successful click, wait until the current URL has changed and is loaded
-        or an expected locator is found.
-        """
-        url = self.page.url
-        clicked = False
-
-        for _ in range(max_tries):
-            if not clicked:
-                try:
-                    locator.click()
-                    clicked = True
-                except PWTimeoutError:
-                    pass
-
-            if clicked:
-                try:
-                    if navigate:
-                        expect(self.page).not_to_have_url(url)
-                    if expected_locator:
-                        expect(expected_locator).to_be_visible()
-                    self.page.wait_for_load_state("networkidle")
-                    return
-                except AssertionError:
-                    pass
-
-            try:
-                if reload_on_error:
-                    self.page.reload(wait_until="networkidle")
-            except Error:
-                continue
-
-        raise AssertionError(
-            "Current URL did not change, expected locator not found or page failed to reload."
         )

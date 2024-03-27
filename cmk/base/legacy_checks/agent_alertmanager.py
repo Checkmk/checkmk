88a24867c2177b53fd0cc77827ba765c77b0ae20
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-03-27 14:41:39 +0100

    alertmanager: remove stdin
    This fixes the missing macro replacement.
    
    CMK-16217
    
    Change-Id: I3ca60a7836806a16cb9bbfd606fdca36a9bb99da

--- cmk/base/legacy_checks/agent_alertmanager.py
+++ cmk/base/legacy_checks/agent_alertmanager.py
@@ -5,27 +5,16 @@
 
 
 from collections.abc import Mapping, Sequence
-from typing import Any, NamedTuple
+from typing import Any
 
 from cmk.base.config import special_agent_info
-
-# NOTE: This code is temporarily duplicated from cmk/base/config.py to resolve
-# a layering violation.
-# This will be resovled with CMK-3812.
-# DO NOT USE THIS!!!
-
-
-class SpecialAgentConfiguration(NamedTuple):
-    args: Sequence[str]
-    # None makes the stdin of subprocess /dev/null
-    stdin: str | None
 
 
 def agent_alertmanager_arguments(
     params: Mapping[str, Any], hostname: str, ipaddress: str | None
-) -> SpecialAgentConfiguration:
+) -> Sequence[str]:
     alertmanager_params = {**params, "host_address": ipaddress, "host_name": hostname}
-    return SpecialAgentConfiguration([], repr(alertmanager_params))
+    return ["--config", repr(alertmanager_params)]
 
 
 special_agent_info["alertmanager"] = agent_alertmanager_arguments

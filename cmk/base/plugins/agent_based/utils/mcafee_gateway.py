a4b6cb5cde5803be78b42425732d5d184ad8a4f9
David Gervasoni <david.gervasoni@checkmk.com>
2024-03-28 11:30:51 +0100

    Added notice on mcafee webgateway developments
    Change-Id: I015bedea40188803ff7a6acfd104828197debe60

--- cmk/base/plugins/agent_based/utils/mcafee_gateway.py
+++ cmk/base/plugins/agent_based/utils/mcafee_gateway.py
@@ -2,6 +2,16 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+"""
+For version 2.2.* mcafee plugins have been migrated (and modified) only as an mk package.
+Further breaking changes to the plugins (e.g. changes in versions 2.3 onwards) should not be backported
+to this version 2.2.* in order to keep the mkp in question functional.
+
+Relevant tickets:
+- https://jira.lan.tribe29.com/browse/SUP-17218
+- https://jira.lan.tribe29.com/browse/SUP-17842
+"""
+
 
 from ..agent_based_api.v1 import contains
 

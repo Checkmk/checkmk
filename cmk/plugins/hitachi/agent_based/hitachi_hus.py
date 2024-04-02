9adfd72099c70e9abf38c2b5c0b449efc1605401
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-04-02 12:29:49 +0200

    migrate hitachi_hus.py
    Change-Id: I5965d89bf5556cdc52dd87a76c08f6644e21503d

--- cmk/plugins/hitachi/agent_based/hitachi_hus.py
+++ cmk/plugins/hitachi/agent_based/hitachi_hus.py
@@ -6,10 +6,19 @@
 from collections.abc import Mapping, Sequence
 from dataclasses import dataclass
 
-from cmk.base.check_api import LegacyCheckDefinition
-from cmk.base.config import check_info
-
-from cmk.agent_based.v2 import any_of, contains, SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    any_of,
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 
 # For Hitachi Unified Storage (HUS) devices which support the USPMIB
 # This devices has two units: Disk Controller (DKC) and Disk Unit (DKC)
@@ -36,7 +45,7 @@ from cmk.agent_based.v2 import any_of, contains, SNMPTree, StringTable
 @dataclass(frozen=True)
 class PropertyState:
     label: str
-    state: int
+    state: State
     description: str
 
 
@@ -52,12 +61,12 @@ _DETECT_HUS = any_of(
 
 
 _HUS_MAP_STATES = {
-    "0": (3, "unknown"),
-    "1": (0, "no error"),
-    "2": (2, "acute"),
-    "3": (2, "serious"),
-    "4": (1, "moderate"),
-    "5": (1, "service"),
+    "0": (State.UNKNOWN, "unknown"),
+    "1": (State.OK, "no error"),
+    "2": (State.CRIT, "acute"),
+    "3": (State.CRIT, "serious"),
+    "4": (State.WARN, "moderate"),
+    "5": (State.WARN, "service"),
 }
 
 
@@ -92,40 +101,52 @@ def parse_hitachi_hus_dku(string_table: StringTable) -> Section:
     }
 
 
-def inventory_hitachi_hus(section):
+def inventory_hitachi_hus(section: Section) -> DiscoveryResult:
     for item in section:
-        yield item, None
+        yield Service(item=item)
 
 
-def check_hitachi_hus(item, _no_params, section):
+def check_hitachi_hus(item: str, section: Section) -> CheckResult:
 
     if (data := section.get(item)) is None:
         return
 
     for prop in data:
-        yield prop.state, f"{prop.label}: {prop.description}"
+        yield Result(state=prop.state, summary=f"{prop.label}: {prop.description}")
 
 
-check_info["hitachi_hus_dkc"] = LegacyCheckDefinition(
-    parse_function=parse_hitachi_hus_dkc,
+snmp_section_hitachi_hus_dkc = SimpleSNMPSection(
+    name="hitachi_hus_dkc",
     detect=_DETECT_HUS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.116.5.11.4.1.1.6.1",
         oids=["1", "2", "3", "4", "5", "6", "7", "8", "9"],
     ),
+    parse_function=parse_hitachi_hus_dkc,
+)
+
+
+check_plugin_hitachi_hus_dkc = CheckPlugin(
+    name="hitachi_hus_dkc",
     service_name="HUS DKC Chassis %s",
     discovery_function=inventory_hitachi_hus,
     check_function=check_hitachi_hus,
 )
 
 
-check_info["hitachi_hus_dku"] = LegacyCheckDefinition(
-    parse_function=parse_hitachi_hus_dku,
+snmp_section_hitachi_hus_dku = SimpleSNMPSection(
+    name="hitachi_hus_dku",
     detect=_DETECT_HUS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.116.5.11.4.1.1.7.1",
         oids=["1", "2", "3", "4", "5"],
     ),
+    parse_function=parse_hitachi_hus_dku,
+)
+
+
+check_plugin_hitachi_hus_dku = CheckPlugin(
+    name="hitachi_hus_dku",
     service_name="HUS DKU Chassis %s",
     discovery_function=inventory_hitachi_hus,
     check_function=check_hitachi_hus,

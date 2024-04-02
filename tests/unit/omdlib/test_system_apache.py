a27b438a09e443ed0388549d3a4940f022044a10
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-04-02 15:34:33 +0200

    omd: 5 remove SiteContext mock
    Change-Id: I3e07ec783bf81ef8efb192cc457a16b11bbc92ee

--- tests/unit/omdlib/test_system_apache.py
+++ tests/unit/omdlib/test_system_apache.py
@@ -31,37 +31,24 @@ def fixture_reload_apache(mocker: MockerFixture) -> MagicMock:
 
 
 @pytest.fixture(autouse=True)
-def fake_config(site_context: SiteContext) -> None:
-    site_context._config_loaded = True
-    site_context._config = {"APACHE_TCP_PORT": "5000", "APACHE_TCP_ADDR": "127.0.0.1"}
-
-
-@pytest.fixture(autouse=True)
 def fake_version_info(version_info: VersionInfo) -> None:
     version_info.APACHE_CTL = "/usr/sbin/apachectl"
 
 
 @pytest.fixture(name="apache_config")
 def fixture_apache_config(tmp_path: Path, site_context: SiteContext) -> Path:
-    return tmp_path.joinpath("omd", "apache", f"{site_context.name}.conf")
+    return tmp_path.joinpath("omd", "apache", "unit.conf")
 
 
 def test_register_with_system_apache(
+    tmp_path: Path,
     apache_config: Path,
     version_info: VersionInfo,
-    site_context: SiteContext,
     reload_apache: MagicMock,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
 
-    register_with_system_apache(
-        version_info,
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        True,
-    )
+    register_with_system_apache(version_info, "unit", str(tmp_path), "127.0.0.1", "5000", True)
 
     content = apache_config.read_bytes()
     assert (
@@ -76,45 +63,31 @@ def test_register_with_system_apache(
 
 
 def test_unregister_from_system_apache(
+    tmp_path: Path,
     apache_config: Path,
     version_info: VersionInfo,
-    site_context: SiteContext,
     reload_apache: MagicMock,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
-    register_with_system_apache(
-        version_info,
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        True,
-    )
+    register_with_system_apache(version_info, "unit", str(tmp_path), "127.0.0.1", "5000", True)
     assert apache_config.exists()
     reload_apache.reset_mock()
 
-    unregister_from_system_apache(version_info, site_context.name, apache_reload=True)
+    unregister_from_system_apache(version_info, "unit", apache_reload=True)
     assert not apache_config.exists()
     reload_apache.assert_called_once_with(["/usr/sbin/apachectl", "graceful"])
 
 
 def test_delete_apache_hook(
+    tmp_path: Path,
     apache_config: Path,
     version_info: VersionInfo,
-    site_context: SiteContext,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
-    register_with_system_apache(
-        version_info,
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        True,
-    )
+    register_with_system_apache(version_info, "unit", str(tmp_path), "127.0.0.1", "5000", True)
     assert apache_config.exists()
 
-    delete_apache_hook(site_context.name)
+    delete_apache_hook("unit")
     assert not apache_config.exists()
 
 
@@ -123,96 +96,66 @@ def test_delete_apache_hook_not_existing(
     version_info: VersionInfo,
     site_context: SiteContext,
 ) -> None:
-    delete_apache_hook(site_context.name)
+    delete_apache_hook("unit")
     assert not apache_config.exists()
 
 
-def test_is_apache_hook_up_to_date(
-    apache_config: Path,
-    site_context: SiteContext,
-) -> None:
+def test_is_apache_hook_up_to_date(tmp_path: Path, apache_config: Path) -> None:
     apache_config.parent.mkdir(parents=True)
-    create_apache_hook(
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        apache_hook_version(),
-    )
+    create_apache_hook("unit", str(tmp_path), "127.0.0.1", "5000", apache_hook_version())
     assert apache_config.exists()
 
-    assert is_apache_hook_up_to_date(site_context.name) is True
+    assert is_apache_hook_up_to_date("unit") is True
 
 
-def test_is_apache_hook_up_to_date_not_readable(
-    apache_config: Path,
-    site_context: SiteContext,
-) -> None:
+def test_is_apache_hook_up_to_date_not_readable(tmp_path: Path, apache_config: Path) -> None:
     apache_config.parent.mkdir(parents=True)
-    create_apache_hook(
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        apache_hook_version(),
-    )
+    create_apache_hook("unit", str(tmp_path), "127.0.0.1", "5000", apache_hook_version())
     assert apache_config.exists()
     apache_config.chmod(0o200)
 
     with pytest.raises(PermissionError):
-        is_apache_hook_up_to_date(site_context.name)
+        is_apache_hook_up_to_date("unit")
 
 
 def test_is_apache_hook_up_to_date_outdated(
+    tmp_path: Path,
     apache_config: Path,
-    site_context: SiteContext,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
-    create_apache_hook(
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        0,
-    )
+    create_apache_hook("unit", str(tmp_path), "127.0.0.1", "5000", 0)
     assert apache_config.exists()
 
-    assert is_apache_hook_up_to_date(site_context.name) is False
+    assert is_apache_hook_up_to_date("unit") is False
 
 
 def test_has_old_apache_hook_in_site(
+    tmp_path: Path,
     apache_config: Path,
-    site_context: SiteContext,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
     with apache_config.open("w") as f:
-        f.write(f"Include /omd/sites/{site_context.name}/etc/apache/mode.conf")
+        f.write("Include /omd/sites/unit/etc/apache/mode.conf")
 
-    assert is_apache_hook_up_to_date(site_context.name) is False
+    assert is_apache_hook_up_to_date("unit") is False
 
 
 def test_has_apache_hook_in_site(
+    tmp_path: Path,
     apache_config: Path,
-    site_context: SiteContext,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
     with apache_config.open("w") as f:
-        f.write(f"Include /omd/sites/{site_context.name}/etc/apache/mode.conf")
+        f.write("Include /omd/sites/unit/etc/apache/mode.conf")
     assert apache_config.exists()
 
-    assert is_apache_hook_up_to_date(site_context.name) is False
+    assert is_apache_hook_up_to_date("unit") is False
 
 
 def test_create_apache_hook_world_readable(
+    tmp_path: Path,
     apache_config: Path,
-    site_context: SiteContext,
 ) -> None:
     apache_config.parent.mkdir(parents=True)
-    create_apache_hook(
-        site_context.name,
-        site_context.dir,
-        site_context.conf["APACHE_TCP_ADDR"],
-        site_context.conf["APACHE_TCP_PORT"],
-        0,
-    )
+    create_apache_hook("unit", str(tmp_path), "127.0.0.1", "5000", 0)
     assert apache_config.stat().st_mode & stat.S_IROTH

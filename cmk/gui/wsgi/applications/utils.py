0af39acd5e044d449858b0db613768c12ed375d8
Wontek Hong <wontek.hong@checkmk.com>
2024-04-04 16:58:45 +0200

    16628 FIX Enforcing password change redirect with 2FA enabled
    Before this werk, the site failed to redirect users to the
    "Change Password" page following a successful login when
    two-factor authentication (2FA) was enabled. This werk resolves
    the issue by ensuring that, after completing 2FA, users are now
    redirected correctly.
    
    Co-authored-by: Wontek Hong <wontek.hong@checkmk.com>
    Co-authored-by: Gavin McGuigan <gavin.mcguigan@checkmk.com>
    
    Change-Id: If455e9d66f9f59438bcd81c961f304a9add1c82b

--- cmk/gui/wsgi/applications/utils.py
+++ cmk/gui/wsgi/applications/utils.py
@@ -67,12 +67,6 @@ def ensure_authentication(func: pages.PageHandlerFunc) -> Callable[[], Response]
                 return response
 
             user_id = session.user.ident
-            if requested_file_name(request) != "user_change_pw":
-                if change_reason := userdb.need_to_change_pw(user_id, datetime.now()):
-                    raise HTTPRedirect(
-                        f"user_change_pw.py?_origtarget={urlencode(makeuri(request, []))}&reason={change_reason}"
-                    )
-
             two_factor_ok = requested_file_name(request) in (
                 "user_login_two_factor",
                 "user_webauthn_login_begin",
@@ -87,6 +81,12 @@ def ensure_authentication(func: pages.PageHandlerFunc) -> Callable[[], Response]
                 raise HTTPRedirect(
                     "user_login_two_factor.py?_origtarget=%s" % urlencode(makeuri(request, []))
                 )
+
+            if not two_factor_ok and requested_file_name(request) != "user_change_pw":
+                if change_reason := userdb.need_to_change_pw(user_id, datetime.now()):
+                    raise HTTPRedirect(
+                        f"user_change_pw.py?_origtarget={urlencode(makeuri(request, []))}&reason={change_reason}"
+                    )
 
             # When displaying the crash report message, the user authentication context
             # has already been left. We need to preserve this information to be able to

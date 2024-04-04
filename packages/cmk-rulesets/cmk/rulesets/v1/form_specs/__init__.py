27c5dae3009986da24bc38a20392d690404e303f
Sofia Colakovic <sofia.colakovic@checkmk.com>
2024-04-04 14:17:11 +0200

    ruleset api: Datamodel changes for TimePeriod form spec
    CMK-16530
    
    Change-Id: I342d642298380d6e42e2b926eb7bcb64a5491f50
    (cherry picked from commit de51be0cf26ad442e24d6de9a7d29e2ca7ed57fb)

--- packages/cmk-rulesets/cmk/rulesets/v1/form_specs/__init__.py
+++ packages/cmk-rulesets/cmk/rulesets/v1/form_specs/__init__.py
@@ -56,6 +56,7 @@ from ._migrations import (
     migrate_to_lower_integer_levels,
     migrate_to_password,
     migrate_to_proxy,
+    migrate_to_time_period,
     migrate_to_upper_float_levels,
     migrate_to_upper_integer_levels,
 )
@@ -106,6 +107,7 @@ __all__ = [
     "migrate_to_integer_simple_levels",
     "migrate_to_password",
     "migrate_to_proxy",
+    "migrate_to_time_period",
     "MonitoredHost",
     "MonitoredService",
     "MatchingScope",

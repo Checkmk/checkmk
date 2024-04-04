fe0c5aa0e6cd575918cea904dbd6ff40d0b5484e
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-04-04 14:00:37 +0200

    fix cisco prime params parsing
    Change-Id: I87885c4a167a0a1c6ea4c02f39d2404962090b4d

--- cmk/plugins/cisco/server_side_calls/prime.py
+++ cmk/plugins/cisco/server_side_calls/prime.py
@@ -4,7 +4,8 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from collections.abc import Iterator, Mapping
+from collections.abc import Iterator
+from typing import assert_never, Literal
 
 from pydantic import BaseModel, Field
 
@@ -22,25 +23,37 @@ class _CiscoPrimeAuth(BaseModel):
     password: Secret
 
 
+class _CustomHost(BaseModel):
+    host: str
+
+
 class CiscoPrimeParams(BaseModel):
     basicauth: _CiscoPrimeAuth | None = None
     port: int | None = None
     no_tls: bool = Field(default=False)
     no_cert_check: bool = Field(default=False)
     timeout: int | None = None
-    host: str | tuple[str, Mapping[str, str]] | None = None
+    host: (
+        tuple[Literal["ip_address"], None]
+        | tuple[Literal["host_name"], None]
+        | tuple[Literal["custom"], _CustomHost]
+    ) = ("ip_address", None)
 
 
 def generate_cisco_prime_command(
     params: CiscoPrimeParams, host_config: HostConfig
 ) -> Iterator[SpecialAgentCommand]:
-    match params.host:
-        case "host_name":
-            host = host_config.name
-        case ("custom", {"host": str(custom_host_name)}):
-            host = replace_macros(custom_host_name, host_config.macros)
-        case _:
-            host = host_config.primary_ip_config.address
+    match params.host[1]:
+        case None:
+            host = (
+                host_config.primary_ip_config.address
+                if params.host[0] == "ip_address"
+                else host_config.name
+            )
+        case _CustomHost() as custom:
+            host = replace_macros(custom.host, host_config.macros)
+        case other:
+            assert_never(other)
 
     if params.basicauth:
         auth: tuple[str | Secret, ...] = (

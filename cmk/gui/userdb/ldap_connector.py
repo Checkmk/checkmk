4476854769bf6498f18411db96e419bdba371802
Ronny Bruska <ronny.bruska@checkmk.com>
2024-04-05 07:36:29 +0200

    Fix crash on edit of LDAP users
    CMK-16688
    
    Change-Id: Ia2a30e500b38b4a4d22707905b996b8d030744a2
    (cherry picked from commit b7023cea5e7ca6b6c608d0bd329deef86a2b9de0)

--- cmk/gui/userdb/ldap_connector.py
+++ cmk/gui/userdb/ldap_connector.py
@@ -2025,7 +2025,7 @@ class LDAPAttributePluginAuthExpire(LDAPBuiltinAttributePlugin):
                     "locked": True,
                     "serial": user.get("serial", 0) + 1,
                 }
-            if not locked_in_ad and locked_in_cmk:
+            if not locked_in_ad:
                 return {
                     "locked": False,
                 }

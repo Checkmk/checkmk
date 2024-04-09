347eb37c3ab964c8977fd199716385fb4e01d2ca
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-04-09 10:49:23 +0200

    16421 FIX Synthetic Monitoring: Adopt 'Plan' Naming Convention
    * rmk_id -> plan_id
    * RMK ID -> Plan ID
    * robotmk_suite -> robotmk_plan
    
    CMK-16727
    
    Change-Id: Id537f98e398d005b674953a252c53398a3182994

--- cmk/update_config/plugins/actions/rulesets.py
+++ cmk/update_config/plugins/actions/rulesets.py
@@ -33,6 +33,7 @@ from cmk.update_config.update_state import format_warning, UpdateActionState
 
 REPLACED_RULESETS: Mapping[RulesetName, RulesetName] = {
     "checkgroup_parameters:fileinfo-groups": "checkgroup_parameters:fileinfo_groups_checking",
+    "checkgroup_parameters:robotmk_suite": "checkgroup_parameters:robotmk_plan",
     "static_checks:fileinfo-groups": "static_checks:fileinfo_groups_checking",
     "checkgroup_parameters:if": "checkgroup_parameters:interfaces",
     "static_checks:if": "static_checks:interfaces",

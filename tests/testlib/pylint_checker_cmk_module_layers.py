ecf794af289143be8d73fe003d90a5472271bcf1
Lukas Lengler <lukas.lengler@checkmk.com>
2024-04-09 12:43:27 +0200

    rmk: add dashboard for synthetic monitoring
    CMK-16879
    
    Change-Id: Ic4c804e05c40563ce0dab7538f8acdf87b21965e

--- tests/testlib/pylint_checker_cmk_module_layers.py
+++ tests/testlib/pylint_checker_cmk_module_layers.py
@@ -268,6 +268,7 @@ def _allow_for_gui_cee(
             _in_component(imported=imported, component=Component("cmk.fetchers")),
             _in_component(imported=imported, component=Component("cmk.cee.bakery")),
             _in_component(imported=imported, component=Component("cmk.cee.robotmk.views")),
+            _in_component(imported=imported, component=Component("cmk.cee.robotmk.dashboards")),
             _in_component(imported=imported, component=Component("cmk.cee.robotmk.pages")),
             _in_component(
                 imported=imported, component=Component("cmk.cee.robotmk.bakery.rulespecs")

0edded1137cdec3946803ca822addd4002bde1bc
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-04-19 11:02:42 +0200

    mk_oracle.ps1: fix message
    Change-Id: I7bd2951bb1fef350afddc78d985f1de1fd4cd3ab

--- agents/windows/plugins/mk_oracle.ps1
+++ agents/windows/plugins/mk_oracle.ps1
@@ -2384,7 +2384,7 @@ if ($the_count -gt 0) {
                }
                if ($Null -ne $result) {
                     Write-Output "<<<oracle_instance:sep(124)>>>"
-                    Write-Output "$ORACLE_SID|FAILURE|$result - Execution is blocked because you try to run unsafe binary as an administrator. Please, disable 'Write', 'Modify' and 'Full control' access to the the file by non-admin users. Alternatively, you can try to run the plugin as a user or run plugin as a group using the rule 'Run plugins and local checks using non-system account' or adjust settings in 'ORACLE databases (Linux, Solaris, AIX, Windows)'."
+                    Write-Output "$ORACLE_SID|FAILURE|$result - Execution is blocked because you try to run unsafe binary as an administrator. Please, disable 'Write', 'Modify' and 'Full control' access to the the file by non-admin users. Alternatively, you can try to adjust the settings in 'ORACLE databases (Linux, Solaris, AIX, Windows)'."
                     continue
                }
           }

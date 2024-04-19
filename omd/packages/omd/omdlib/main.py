23c55fc3259cc90ebf7aec7aba1a67a481d3bbf3
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-04-19 10:21:52 +0200

    omd: fix missing edition
    This is a regression introduced by the change with ID:
    I7e1d1008498f0f3df75c5ba1844a68c5a1ffb36c
    
    Change-Id: I719f299341e956973f26bfa322e7699b5ab49a08

--- omd/packages/omd/omdlib/main.py
+++ omd/packages/omd/omdlib/main.py
@@ -917,6 +917,7 @@ def update_file(  # pylint: disable=too-many-branches
         {
             "###SITE###": site.name,
             "###ROOT###": site.dir,
+            "###EDITION###": old_edition,
         },
     )
 

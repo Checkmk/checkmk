3a333563cd3a478322ad93e918fae003f73e7b95
Hojjat Afsharan <hojjat.afsharan@checkmk.com>
2024-04-23 10:32:37 +0200

    provide saas onboarding only for admin user role
    Change-Id: I988ba43f8bf3af7ddfe2b4b589b82a79c344d7a8

--- cmk/gui/main_menu.py
+++ cmk/gui/main_menu.py
@@ -12,6 +12,9 @@ from cmk.utils.licensing.registry import get_license_message
 from cmk.utils.plugin_registry import Registry
 from cmk.utils.version import __version__, edition, Edition
 
+if edition() is Edition.CSE:
+    from cmk.gui.cse.utils.roles import user_may_see_saas_onboarding
+
 from cmk.gui.htmllib.generator import HTMLWriter
 from cmk.gui.http import request
 from cmk.gui.i18n import _, _l
@@ -108,7 +111,7 @@ def _help_menu_topics() -> list[TopicMenuTopic]:
         ),
     ]
 
-    if edition() == Edition.CSE:
+    if edition() == Edition.CSE and user_may_see_saas_onboarding(user.id):
         learning_items.append(
             TopicMenuItem(
                 name="getting_started", title=_("Getting started"), sort_index=10, url=""

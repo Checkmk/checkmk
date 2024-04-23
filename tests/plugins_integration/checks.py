33301dc0e9ea6c9def13a0b606195040ac805714
Matteo Stifano <matteo.stifano@checkmk.com>
2024-04-23 16:32:44 +0200

    test_plugin: Improve AssertionError message
    Change-Id: I9db800c9cd75021216d99d8ff9a4760f55e7721f
    (cherry picked from commit 3ddfa5db619f3c4e0cc854add60e1d10be223a46)

--- tests/plugins_integration/checks.py
+++ tests/plugins_integration/checks.py
@@ -311,7 +311,7 @@ def process_check_output(
     site: Site,
     host_name: str,
     output_dir: Path,
-) -> list[str]:
+) -> dict[str, str]:
     """Process the check output and either dump or compare it."""
     if host_name in SkippedDumps.SKIPPED_DUMPS:
         pytest.skip(reason=f"{host_name} dumps currently skipped.")
@@ -381,7 +381,7 @@ def process_check_output(
         ) as json_file:
             json.dump(check_canons, json_file, indent=4, sort_keys=True)
 
-    return sorted(diffs.keys())
+    return diffs
 
 
 def setup_site(site: Site, dump_path: str) -> None:

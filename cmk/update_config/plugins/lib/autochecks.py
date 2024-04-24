12412359e74024377093fbbcd941e3268782aea2
Sofia Colakovic <sofia.colakovic@checkmk.com>
2024-04-24 15:04:01 +0200

    update config: report one error per plugin in PreUpdateAgentBasedPlugins action
    In case of a missconfigured check, there could be thousands of faulty services
    across multiple hosts. We previously required user interaction for each service,
    which led to unmanagable update.
    
    CMK-17096
    
    Change-Id: If31fac91880873b6f9bd5b46df776ea7d90bb554

--- cmk/update_config/plugins/lib/autochecks.py
+++ cmk/update_config/plugins/lib/autochecks.py
@@ -160,6 +160,7 @@ _ALL_EXPLICIT_DISCOVERED_PARAMETERS_TRANSFORMS: TDiscoveredParametersTransforms 
 class RewriteError:
     message: str
     host_name: HostName
+    plugin: CheckPluginName | None = None
 
 
 def rewrite_yielding_errors(*, write: bool) -> Iterable[RewriteError]:
@@ -178,7 +179,7 @@ def _get_fixed_autochecks(
     except Exception as exc:
         if debug.enabled():
             raise
-        yield RewriteError(f"Failed to load autochecks: {exc}", host_name)
+        yield RewriteError(message=f"Failed to load autochecks: {exc}", host_name=host_name)
         return []
 
     fixed_autochecks: list[AutocheckEntry] = []
@@ -188,7 +189,9 @@ def _get_fixed_autochecks(
         except Exception as exc:
             if debug.enabled():
                 raise
-            yield RewriteError(str(exc), host_name)
+            yield RewriteError(
+                message=str(exc), host_name=host_name, plugin=entry.check_plugin_name
+            )
 
     return fixed_autochecks
 

19a7982ae5dda483779b08da8de55e3281b38f48
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2024-04-25 15:17:03 +0200

    Add credentials to groovy for asserting artifacts on nexus
    Change-Id: I058a32355839565fb37221b1edbb62124c2fb81b

--- buildscripts/scripts/assert-release-build-artifacts.groovy
+++ buildscripts/scripts/assert-release-build-artifacts.groovy
@@ -8,14 +8,20 @@ def main() {
             "VERSION_TO_CHECK",
         ])
         docker_image_from_alias("IMAGE_TESTING").inside("-v \$HOME/.cmk-credentials:\$HOME/.cmk-credentials -v /var/run/docker.sock:/var/run/docker.sock --group-add=${get_docker_group_id()}") {
-            withEnv(["PYTHONUNBUFFERED=1"]) {
-                dir("${checkout_dir}") {
-                    sh(script: """scripts/run-pipenv run \
-                    buildscripts/scripts/get_distros.py \
-                    --editions_file "${checkout_dir}/editions.yml" \
-                    assert_build_artifacts \
-                    --version "${VERSION_TO_CHECK}" \
-                    """);
+            withCredentials([
+                usernamePassword(
+                    credentialsId: 'nexus',
+                    passwordVariable: 'NEXUS_PASSWORD',
+                    usernameVariable: 'NEXUS_USER')]) {
+                    withEnv(["PYTHONUNBUFFERED=1"]) {
+                        dir("${checkout_dir}") {
+                            sh(script: """scripts/run-pipenv run \
+                        buildscripts/scripts/get_distros.py \
+                        --editions_file "${checkout_dir}/editions.yml" \
+                        assert_build_artifacts \
+                        --version "${VERSION_TO_CHECK}" \
+                        """);
+                    }
                 }
             }
         }

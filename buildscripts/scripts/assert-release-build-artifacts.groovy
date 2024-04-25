11eedafca9f4e0abfce470570a4c1d523e55151b
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2024-04-25 15:17:03 +0200

    Add credentials to groovy for asserting artifacts on nexus
    Change-Id: I058a32355839565fb37221b1edbb62124c2fb81b

--- buildscripts/scripts/assert-release-build-artifacts.groovy
+++ buildscripts/scripts/assert-release-build-artifacts.groovy
@@ -12,14 +12,20 @@ def main() {
             mount_credentials: true,
             priviliged: true,
         ) {
-            withEnv(["PYTHONUNBUFFERED=1"]) {
-                dir("${checkout_dir}") {
-                    sh(script: """scripts/run-pipenv run \
-                    buildscripts/scripts/get_distros.py \
-                    --editions_file "${checkout_dir}/editions.yml" \
-                    assert_build_artifacts \
-                    --version "${VERSION_TO_CHECK}" \
-                    """);
+            withCredentials([
+                usernamePassword(
+                    credentialsId: 'nexus',
+                    passwordVariable: 'NEXUS_PASSWORD',
+                    usernameVariable: 'NEXUS_USER')]) {
+                withEnv(["PYTHONUNBUFFERED=1"]) {
+                    dir("${checkout_dir}") {
+                        sh(script: """scripts/run-pipenv run \
+                        buildscripts/scripts/get_distros.py \
+                        --editions_file "${checkout_dir}/editions.yml" \
+                        assert_build_artifacts \
+                        --version "${VERSION_TO_CHECK}" \
+                        """);
+                    }
                 }
             }
         }

a7c9667acfecc17f7caf5e554e7f60505aa6290a
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2024-04-26 09:02:23 +0200

    16793 FIX infoblox_temp: Add support for Nios version > 8.6
    The relevant OIDs have changed for newer Nios versions. The check now differentiates between the different version, meaning the check das not crash asnymore when parsing newer sections.
    
    SUP-18264
    
    Change-Id: Ibe480203475a363a6eaee294c9ca670b17dca26a

--- tests/unit/cmk/base/plugins/agent_based/utils/test_infoblox_temp.py
+++ tests/unit/cmk/base/plugins/agent_based/utils/test_infoblox_temp.py
@@ -16,19 +16,46 @@ from cmk.utils.type_defs import CheckPluginName, SectionName
 
 from cmk.base.api.agent_based.checking_classes import Metric, Result, Service, State
 
-WALK = """
+WALK_NIOS_7_2_7 = """
+.1.3.6.1.4.1.7779.3.1.1.2.1.7.0 7.2.7
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.37 5
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.38 1
 .1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.39 1
 .1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.40 5
 .1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.41 1
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.37 No power information available.
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.38 The NTP service resumed synchronization.
 .1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.39 CPU_TEMP: +36.00 C
 .1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.40 No temperature information available.
 .1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.41 SYS_TEMP: +34.00 C
 """
 
 
+WALK_NIOS_9_0_3 = """
+.1.3.6.1.4.1.7779.3.1.1.2.1.7.0 9.0.3-50212
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.37 1
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.38 5
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.39 1
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.40 5
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.2.41 1
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.37 CPU_TEMP: +36.00 C
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.38 No temperature information available.
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.39 SYS_TEMP: +34.00 C
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.40
+.1.3.6.1.4.1.7779.3.1.1.2.1.10.1.3.41 CPU Usage: 20%
+"""
+
+
 @pytest.mark.usefixtures("fix_register")
-def test_parse_infoblox_temp(as_path: Callable[[str], Path]) -> None:
-    section = get_parsed_snmp_section(SectionName("infoblox_temp"), as_path(WALK))
+@pytest.mark.parametrize(
+    ["input_walk"],
+    [
+        pytest.param(WALK_NIOS_7_2_7, id="Nios 7.2.7"),
+        pytest.param(WALK_NIOS_9_0_3, id="Nios 9.0.3"),
+    ],
+)
+def test_parse_infoblox_temp(input_walk: str, as_path: Callable[[str], Path]) -> None:
+    section = get_parsed_snmp_section(SectionName("infoblox_temp"), as_path(input_walk))
     assert section == {
         "CPU_TEMP 1": {"reading": 36.0, "state": (0, "working"), "unit": "C"},
         "No temperature information available. 2": {
@@ -41,9 +68,17 @@ def test_parse_infoblox_temp(as_path: Callable[[str], Path]) -> None:
 
 
 @pytest.mark.usefixtures("fix_register")
-def test_inventory_infoblox_temp(fix_register: FixRegister, as_path: Callable[[str], Path]) -> None:
-
-    section = get_parsed_snmp_section(SectionName("infoblox_temp"), as_path(WALK))
+@pytest.mark.parametrize(
+    ["input_walk"],
+    [
+        pytest.param(WALK_NIOS_7_2_7, id="Nios 7.2.7"),
+        pytest.param(WALK_NIOS_9_0_3, id="Nios 9.0.3"),
+    ],
+)
+def test_inventory_infoblox_temp(
+    fix_register: FixRegister, input_walk: str, as_path: Callable[[str], Path]
+) -> None:
+    section = get_parsed_snmp_section(SectionName("infoblox_temp"), as_path(input_walk))
     assert section is not None
     assert list(
         fix_register.check_plugins[CheckPluginName("infoblox_temp")].discovery_function(section)
@@ -51,6 +86,13 @@ def test_inventory_infoblox_temp(fix_register: FixRegister, as_path: Callable[[s
 
 
 @pytest.mark.usefixtures("fix_register")
+@pytest.mark.parametrize(
+    ["input_walk"],
+    [
+        pytest.param(WALK_NIOS_7_2_7, id="Nios 7.2.7"),
+        pytest.param(WALK_NIOS_9_0_3, id="Nios 9.0.3"),
+    ],
+)
 @pytest.mark.parametrize(
     ["item", "params", "expected"],
     [
@@ -82,12 +124,13 @@ def test_inventory_infoblox_temp(fix_register: FixRegister, as_path: Callable[[s
 )
 def test_check_infoblox_temp(
     fix_register: FixRegister,
-    as_path: Callable[[str], Path],
+    input_walk: str,
     item: str,
     params: Mapping[str, tuple[float, float]],
     expected: list,
+    as_path: Callable[[str], Path],
 ) -> None:
-    section = get_parsed_snmp_section(SectionName("infoblox_temp"), as_path(WALK))
+    section = get_parsed_snmp_section(SectionName("infoblox_temp"), as_path(input_walk))
     assert section is not None
 
     assert (

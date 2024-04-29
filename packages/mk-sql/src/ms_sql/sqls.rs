a392b3d6316d278c9142f0c091eed9e5ab3d4a90
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-04-29 20:38:02 +0200

    mk-sql: fix encoding error for query
    Change-Id: I9b3ae4746d6d475bfd5390d1e2cf4470086f7941

--- packages/mk-sql/src/ms_sql/sqls.rs
+++ packages/mk-sql/src/ms_sql/sqls.rs
@@ -173,18 +173,35 @@ END
 EXEC (@SQLCommand)
 ";
 
+    //Script to create a table with 'unsupported' collation
+    //master;
+    //GO
+    //IF DB_ID (N'MyOptionsTest') IS NOT NULL
+    //    DROP DATABASE MyOptionsTest;
+    //GO
+    // CREATE DATABASE MyOptionsTest
+    //    COLLATE SQL_Latin1_General_CP850_CI_AS; -- French_CI_AS;
+    // GO
+    //
+    // SELECT name, collation_name
+    // FROM sys.databases
+    // WHERE name = N'MyOptionsTest';
+    //
+
+    /// NOTE: cast( ... as nvarchar) is a workaround gainst unsupported collations
     pub const TRANSACTION_LOGS: &str = "SELECT name, physical_name,\
   cast(max_size/128 as bigint) as MaxSize,\
   cast(size/128 as bigint) as AllocatedSize,\
   cast(FILEPROPERTY (name, 'spaceused')/128 as bigint) as UsedSize,\
-  case when max_size = '-1' then '1' else '0' end as Unlimited \
+  cast(case when max_size = '-1' then '1' else '0' end as nvarchar) as Unlimited \
  FROM sys.database_files WHERE type_desc = 'LOG'";
 
+    /// NOTE: cast( ... as nvarchar) is a workaround gainst unsupported collations
     pub const DATAFILES: &str = "SELECT name, physical_name,\
   cast(max_size/128 as bigint) as MaxSize,\
   cast(size/128 as bigint) as AllocatedSize,\
   cast(FILEPROPERTY (name, 'spaceused')/128 as bigint) as UsedSize,\
-  case when max_size = '-1' then '1' else '0' end as Unlimited \
+    cast(case when max_size = '-1' then '1' else '0' end  as nvarchar) as Unlimited  \
 FROM sys.database_files WHERE type_desc = 'ROWS'";
 
     pub const DATABASES: &str = "SELECT name, \

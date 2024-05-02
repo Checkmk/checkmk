adb0d5e8327510512c788828dc5f794cb85b9afa
Matteo Stifano <matteo.stifano@checkmk.com>
2024-05-02 11:32:52 +0200

    test_update_from_backup: Temporary skip test
    Backup needs to be updated with CMK version 2.3.0.
    Meanwhile, we are here skipping such test.
    
    Change-Id: I694044ea3113791d9ea8b81895f1fd2f1d2dcb4a

--- tests/update/test_update_from_backup.py
+++ tests/update/test_update_from_backup.py
@@ -78,6 +78,7 @@ def _base_site_demo(site_factory_demo):
 
 
 @pytest.mark.cee
+@pytest.mark.skip("Re-generate backup using a 2.3.0 release.")
 def test_update_from_backup(site_factory: SiteFactory, base_site: Site, agent_ctl: Path) -> None:
     backup_path = qa_test_data_path() / Path("update/backups/update_central_2.3.0b1_backup.tar.gz")
     assert backup_path.exists()

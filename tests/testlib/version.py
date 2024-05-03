158543103988f3473f437461d8ab35fd31b94a5f
Matteo Stifano <matteo.stifano@checkmk.com>
2024-05-03 18:05:44 +0200

    testlib/version: Fix logical flaw in version comparison (2)
    Change-Id: Ibcd4f2628778837dbdf03c269ea7be6995590d47

--- tests/testlib/version.py
+++ tests/testlib/version.py
@@ -141,11 +141,12 @@ def version_gte(version: str, min_version: str) -> bool:
     logger.debug("min_version=%s; min_version_values=%s", min_version, min_version_values)
 
     # compare the version numbers segment by segment
-    # if any is higher, return True
     for i, min_val in enumerate(min_version_values):
         if int(cmp_version_values[i]) > int(min_val):
             return True
-    return False
+        if int(cmp_version_values[i]) < int(min_val):
+            return False
+    return True
 
 
 def get_min_version() -> str:

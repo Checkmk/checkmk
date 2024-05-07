a977a8e5bf6d83c44535e59c526ecf737eadd542
Joerg Herbel <joerg.herbel@checkmk.com>
2024-05-07 20:46:57 +0200

    Cleanup pagetypes update actions
    All done already in 2.3.
    
    CMK-17315
    
    Change-Id: I378ad2dae08c626f9cbc8010409721fc030c1725

--- cmk/update_config/plugins/actions/pagetypes.py
+++ cmk/update_config/plugins/actions/pagetypes.py
@@ -5,14 +5,11 @@
 
 from collections.abc import Mapping
 from logging import Logger
-from pathlib import Path
-from typing import Final, Generic, Protocol, TypeVar
+from typing import Generic, Protocol, TypeVar
 
 from cmk.utils.plugin_registry import Registry
-from cmk.utils.store import load_object_from_file
 from cmk.utils.user import UserId
 
-from cmk.gui.logged_in import LoggedInUser
 from cmk.gui.pagetypes import (
     all_page_types,
     InstanceId,
@@ -20,8 +17,6 @@ from cmk.gui.pagetypes import (
     OverridableInstances,
     OverridableSpec,
 )
-from cmk.gui.sidebar._snapin._bookmarks import BookmarkList, BookmarkListSpec
-from cmk.gui.userdb import load_users
 
 from cmk.update_config.registry import update_action_registry, UpdateAction
 from cmk.update_config.update_state import UpdateActionState
@@ -73,60 +68,3 @@ update_action_registry.register(
         sort_index=120,  # can run whenever
     )
 )
-
-
-class BookmarkListUpdater:
-    def __init__(self) -> None:
-        self.target_type: Final = BookmarkList
-
-    def __call__(
-        self, page_dicts: Mapping[InstanceId, dict[str, object]]
-    ) -> Mapping[InstanceId, dict[str, object]]:
-        bookmark_lists_by_instance_id: dict[InstanceId, BookmarkListSpec] = {}
-        for user_id in load_users():
-            # Don't load the legacy bookmarks when there is already a my_bookmarks list
-            if (user_id, "my_bookmarks") in page_dicts:
-                continue
-
-            # Also don't load them when the user has at least one bookmark list
-            for user_id_bookmarks, _name in page_dicts:
-                if user_id == user_id_bookmarks:
-                    continue
-
-            # Ensure that the user has a confdir
-            if not (user_confdir := LoggedInUser(user_id).confdir):
-                continue
-
-            bookmark_lists_by_instance_id.setdefault(
-                (user_id, "my_bookmarks"),
-                {
-                    "title": "My Bookmarks",
-                    "public": False,
-                    "owner": user_id,
-                    "name": "my_bookmarks",
-                    "description": "Your personal bookmarks",
-                    "default_topic": "My Bookmarks",
-                    "bookmarks": [
-                        {
-                            "title": title,
-                            "url": url,
-                            "icon": None,
-                            "topic": None,
-                        }
-                        for title, url in self._load_and_delete_legacy_bookmarks(Path(user_confdir))
-                    ],
-                },
-            )
-
-        return page_dicts
-
-    @staticmethod
-    def _load_and_delete_legacy_bookmarks(user_confdir: Path) -> list[tuple[str, str]]:
-        if not (legacy_path := user_confdir / "bookmarks.mk").exists():
-            return []
-        legacy_bookmarks = load_object_from_file(legacy_path, default=[])
-        legacy_path.unlink()
-        return list(legacy_bookmarks)  # make mypy happy
-
-
-pagetype_updater_registry.register(BookmarkListUpdater())

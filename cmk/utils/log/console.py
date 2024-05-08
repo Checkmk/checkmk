c98474c7fe0cbd7af219e86f76da7b64ca88cde3
Sven Panne <sven.panne@checkmk.com>
2024-05-08 09:37:43 +0200

    Refactoring: Simplified console API.
    Change-Id: Ifcc356555f49ac297b064bab07c7da5cfa00c6ab

--- cmk/utils/log/console.py
+++ cmk/utils/log/console.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from __future__ import annotations
-
 import logging
 import sys
 from collections.abc import Generator
@@ -47,26 +45,26 @@ def format_warning(text: str) -> str:
     return f"{indent}{tty.bold}{tty.yellow}WARNING:{tty.normal} {stripped}"
 
 
-def log(level: int, text: str, *args: object, stream: TextIO | None = None) -> None:
+def _log(level: int, text: str, *args: object, stream: TextIO | None = None) -> None:
     with set_stream(_console, _handler, sys.stdout if stream is None else stream):
         _console.log(level, text, *args)
 
 
-def debug(text: str, *args: object, stream: TextIO | None = None) -> None:
-    log(logging.DEBUG, text, *args, stream=stream)
+def debug(text: str, *args: object) -> None:
+    _log(logging.DEBUG, text, *args)
 
 
 def verbose(text: str, *args: object, stream: TextIO | None = None) -> None:
-    log(VERBOSE, text, *args, stream=stream)
+    _log(VERBOSE, text, *args, stream=stream)
 
 
-def info(text: str, *args: object, stream: TextIO | None = None) -> None:
-    log(logging.INFO, text, *args, stream=stream)
+def info(text: str, *args: object) -> None:
+    _log(logging.INFO, text, *args)
 
 
 def warning(text: str, *args: object, stream: TextIO | None = None) -> None:
-    log(logging.WARNING, text, *args, stream=stream)
+    _log(logging.WARNING, text, *args, stream=stream)
 
 
 def error(text: str, *args: object) -> None:
-    log(logging.ERROR, text, *args, stream=sys.stderr)
+    _log(logging.ERROR, text, *args, stream=sys.stderr)

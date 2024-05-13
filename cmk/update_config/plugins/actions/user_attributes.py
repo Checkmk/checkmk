ad33dd6ab74ddac02550b0d35a69fda7f4aec1b4
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-05-13 17:09:01 +0200

    Remove old user updates
    CMK-17137
    
    Change-Id: I53257385c8720ecd85d7d7ec77ba150a76661ed8

--- cmk/update_config/plugins/actions/user_attributes.py
+++ cmk/update_config/plugins/actions/user_attributes.py
@@ -6,7 +6,6 @@
 from datetime import datetime
 from logging import Logger
 
-from cmk.utils.log import VERBOSE
 from cmk.utils.user import UserId
 
 from cmk.gui.type_defs import Users, UserSpec
@@ -24,26 +23,6 @@ class UpdateUserAttributes(UpdateAction):
         )
 
 
-def _update_disable_notifications(user_spec: UserSpec) -> None:
-    disable_notifications = user_spec.get("disable_notifications")
-    if isinstance(disable_notifications, bool):
-        user_spec["disable_notifications"] = {"disable": True} if disable_notifications else {}
-
-
-def _add_or_update_locked_attr(user_spec: UserSpec) -> None:
-    """
-    Until 2.3 the "locked" attribute was not set in all cases and could be
-    bool | None
-    """
-    if "locked" not in user_spec:
-        user_spec.setdefault("locked", False)
-        return
-
-    if user_spec.get("locked", False) is None:
-        user_spec["locked"] = False
-    return
-
-
 def _add_alias(user_id: UserId, user_spec: UserSpec) -> None:
     """
     Until 2.4 the "alias" or full name could be missing for users created by LDAP.
@@ -52,26 +31,8 @@ def _add_alias(user_id: UserId, user_spec: UserSpec) -> None:
 
 
 def _update_user_attributes(logger: Logger, users: Users) -> Users:
-    """
-    With version 1.6.0 we deprecated boolean disable_notifications. SUP-17012
-
-    With version 2.2.0 we retyped user languages from None | str to str only.
-    This function removes language params set to None in existing user configs.
-    """
-    changed_user_specs: bool = False
     for user_id, user_spec in users.items():
-        if user_spec.get("language", -1) is None:
-            changed_user_specs = True
-            del user_spec["language"]
-        _update_disable_notifications(user_spec)
-        _add_or_update_locked_attr(user_spec)
         _add_alias(user_id, user_spec)
-
-    if changed_user_specs:
-        logger.log(
-            VERBOSE,
-            "Removing deprecated user languages set to None. (The default remains English)",
-        )
     return users
 
 

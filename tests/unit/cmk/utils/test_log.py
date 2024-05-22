bf5256724524e23b41528221e905750f8630f8be
Sven Panne <sven.panne@checkmk.com>
2024-05-22 12:54:28 +0200

    Make cleanup more robust.
    Change-Id: I1222a469eaddd5e4e2dbd0718fbee0e6f91d0627

--- tests/unit/cmk/utils/test_log.py
+++ tests/unit/cmk/utils/test_log.py
@@ -17,68 +17,66 @@ import cmk.utils.log as log
 from cmk.utils.log.security_event import log_security_event, SecurityEvent
 
 
+@contextlib.contextmanager
+def set_log_level(logger: logging.Logger, level: int | str) -> Iterator[None]:
+    old_level = logger.level
+    logger.setLevel(level)
+    yield
+    logger.setLevel(old_level)
+
+
 def test_get_logger() -> None:
     assert logging.getLogger("cmk.asd").parent == log.logger
 
 
 def test_setup_console_logging(capsys: CaptureFixture[str]) -> None:
-    assert ("", "") == capsys.readouterr()
-    log.clear_console_logging()
-
     logging.getLogger("cmk.test").info("test123")
     assert ("", "") == capsys.readouterr()
 
-    log.setup_console_logging()
-    logging.getLogger("cmk.test").info("test123")
-    log.logger.handlers.pop()
-    assert ("test123\n", "") == capsys.readouterr()
+    old_handlers = log.logger.handlers
+    try:
+        log.setup_console_logging()
+        logging.getLogger("cmk.test").warning("test123")
+        assert ("test123\n", "") == capsys.readouterr()
+    finally:
+        log.logger.handlers = old_handlers
 
 
 def test_set_verbosity() -> None:
-    root = logging.getLogger("cmk")
-    root.setLevel(logging.INFO)
+    with set_log_level(logging.getLogger("cmk"), logging.INFO):
+        l = logging.getLogger("cmk.test_logger")
 
-    l = logging.getLogger("cmk.test_logger")
-    assert l.getEffectiveLevel() == logging.INFO
-    assert l.isEnabledFor(log.VERBOSE) is False
-    assert l.isEnabledFor(logging.DEBUG) is False
+        assert l.getEffectiveLevel() == logging.INFO
+        assert l.isEnabledFor(log.VERBOSE) is False
+        assert l.isEnabledFor(logging.DEBUG) is False
 
-    log.logger.setLevel(log.verbosity_to_log_level(0))
-    assert l.getEffectiveLevel() == logging.INFO
-    assert l.isEnabledFor(log.VERBOSE) is False
-    assert l.isEnabledFor(logging.DEBUG) is False
+        with set_log_level(log.logger, log.verbosity_to_log_level(0)):
+            assert l.getEffectiveLevel() == logging.INFO
+            assert l.isEnabledFor(log.VERBOSE) is False
+            assert l.isEnabledFor(logging.DEBUG) is False
 
-    log.logger.setLevel(log.verbosity_to_log_level(1))
-    assert l.getEffectiveLevel() == log.VERBOSE
-    assert l.isEnabledFor(log.VERBOSE) is True
-    assert l.isEnabledFor(logging.DEBUG) is False
+        with set_log_level(log.logger, log.verbosity_to_log_level(1)):
+            assert l.getEffectiveLevel() == log.VERBOSE
+            assert l.isEnabledFor(log.VERBOSE) is True
+            assert l.isEnabledFor(logging.DEBUG) is False
 
-    log.logger.setLevel(log.verbosity_to_log_level(2))
-    assert l.getEffectiveLevel() == logging.DEBUG
-    assert l.isEnabledFor(log.VERBOSE) is True
-    assert l.isEnabledFor(logging.DEBUG) is True
+        with set_log_level(log.logger, log.verbosity_to_log_level(2)):
+            assert l.getEffectiveLevel() == logging.DEBUG
+            assert l.isEnabledFor(log.VERBOSE) is True
+            assert l.isEnabledFor(logging.DEBUG) is True
 
-    # Use debug level (highest supported)
-    log.logger.setLevel(log.verbosity_to_log_level(3))
-    assert l.getEffectiveLevel() == logging.DEBUG
-    assert l.isEnabledFor(log.VERBOSE) is True
-    assert l.isEnabledFor(logging.DEBUG) is True
-
-    # Reset verbosity for next test run.
-    log.logger.setLevel(log.verbosity_to_log_level(0))
+        with set_log_level(log.logger, log.verbosity_to_log_level(3)):
+            assert l.getEffectiveLevel() == logging.DEBUG
+            assert l.isEnabledFor(log.VERBOSE) is True
+            assert l.isEnabledFor(logging.DEBUG) is True
 
 
 @contextlib.contextmanager
 def queue_log_sink(logger: logging.Logger) -> Iterator[queue.Queue[logging.LogRecord]]:
-    old_level = logger.level
-
-    logger.setLevel(logging.INFO)
     q: queue.Queue[logging.LogRecord] = queue.Queue()
     queue_handler = logging.handlers.QueueHandler(q)
     logger.addHandler(queue_handler)
     yield q
-
-    logger.setLevel(old_level)
     logger.removeHandler(queue_handler)
 
 
@@ -89,11 +87,13 @@ def test_security_event(tmp_path: Path) -> None:
         SecurityEvent.Domain.auth,
     )
 
-    with queue_log_sink(logging.getLogger("cmk_security")) as log_queue:
-        log_security_event(event)
-        entry = log_queue.get_nowait()
-        assert entry.name == "cmk_security.auth"
-        assert (
-            entry.getMessage() == '{"summary": "test security event", '
-            '"details": {"a": ["serialize", "me"], "b": {"b.1": 42.23}}}'
-        )
+    logger = logging.getLogger("cmk_security")
+    with set_log_level(logger, logging.INFO):
+        with queue_log_sink(logger) as log_queue:
+            log_security_event(event)
+            entry = log_queue.get_nowait()
+            assert entry.name == "cmk_security.auth"
+            assert (
+                entry.getMessage() == '{"summary": "test security event", '
+                '"details": {"a": ["serialize", "me"], "b": {"b.1": 42.23}}}'
+            )

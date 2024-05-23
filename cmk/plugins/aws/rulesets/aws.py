97c9d24cfb7bf29a541bd084093c9ce8c4672860
Moritz Kirschner <moritz.kirschner@checkmk.com>
2024-05-23 15:30:44 +0200

    aws: Split helper funcs to prepare edition split
    Change-Id: Ie200e962fd94d3a065031fe1343dfd82aef3a71a

--- cmk/plugins/aws/rulesets/aws.py
+++ cmk/plugins/aws/rulesets/aws.py
@@ -2,23 +2,23 @@
 # Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
-from collections.abc import Sequence
-from typing import Any, Literal
-
 from cmk.plugins.aws.lib import aws_region_to_monitor
+from cmk.plugins.aws.ruleset_helper import (
+    convert_tag_list,
+    formspec_aws_limits,
+    formspec_aws_tags,
+    migrate_global_service,
+    migrate_regional_service,
+    service_dict_element,
+)
 from cmk.rulesets.v1 import Help, Label, Message, Title
 from cmk.rulesets.v1.form_specs import (
-    CascadingSingleChoice,
-    CascadingSingleChoiceElement,
     DefaultValue,
     DictElement,
     Dictionary,
     FieldSize,
     FixedValue,
-    FormSpec,
     Integer,
-    List,
     migrate_to_password,
     MultipleChoice,
     MultipleChoiceElement,
@@ -28,27 +28,26 @@ from cmk.rulesets.v1.form_specs import (
     String,
     validators,
 )
-from cmk.rulesets.v1.form_specs.validators import ValidationError
 from cmk.rulesets.v1.rule_specs import SpecialAgent, Topic
 
 
 def _all_global_services() -> dict[str, DictElement]:
     return {
-        "ce": _service_dict_element(
+        "ce": service_dict_element(
             Title("Costs and usage (CE)"),
             {},
             default_enabled=False,
             filterable_by_name=False,
             filterable_by_tags=False,
         ),
-        "route53": _service_dict_element(
+        "route53": service_dict_element(
             Title("Route53"),
             {},
             default_enabled=False,
             filterable_by_name=False,
             filterable_by_tags=False,
         ),
-        "cloudfront": _service_dict_element(
+        "cloudfront": service_dict_element(
             Title("CloudFront"),
             {
                 "host_assignment": DictElement(
@@ -83,106 +82,18 @@ def _all_global_services() -> dict[str, DictElement]:
     }
 
 
-def _service_dict_element(
-    title: Title,
-    extra_elements: dict[str, DictElement],
-    default_enabled: bool = True,
-    filterable_by_tags: bool = True,
-    filterable_by_name: bool = True,
-) -> DictElement:
-    elements: list[CascadingSingleChoiceElement] = [
-        CascadingSingleChoiceElement(
-            title=Title("Do not monitor service"),
-            name="none",
-            parameter_form=FixedValue(value=None),
-        ),
-        CascadingSingleChoiceElement(
-            name="all",
-            title=Title("Monitor all instances"),
-            parameter_form=Dictionary(
-                elements={
-                    **extra_elements,
-                }
-            ),
-        ),
-    ]
-    if filterable_by_tags:
-        elements.append(
-            CascadingSingleChoiceElement(
-                name="tags",
-                title=Title(
-                    "Monitor instances with explicit AWS service tags ignoring overall tag filters"
-                ),
-                parameter_form=Dictionary(
-                    elements={
-                        "tags": DictElement(
-                            parameter_form=_formspec_aws_tags(),
-                            required=True,
-                        ),
-                        **extra_elements,
-                    },
-                ),
-            )
-        )
-    if filterable_by_name:
-        elements.append(
-            CascadingSingleChoiceElement(
-                name="names",
-                title=Title(
-                    "Monitor instances with explicit service names ignoring overall tag filters"
-                ),
-                parameter_form=Dictionary(
-                    elements={
-                        "names": DictElement(
-                            parameter_form=List(
-                                element_template=String(label=Label("Service name")),
-                                add_element_label=Label("Add new name"),
-                                remove_element_label=Label("Remove name"),
-                                no_element_label=Label("No names defined"),
-                                editable_order=False,
-                            ),
-                            required=True,
-                        ),
-                        **extra_elements,
-                    },
-                ),
-            )
-        )
-
-    return DictElement(
-        parameter_form=CascadingSingleChoice(
-            help_text=Help(
-                "<b>Monitor all instances:</b> "
-                "<br>If overall tags are specified below, then all "
-                "service instances will be filtered by those tags. Otherwise, "
-                "all instances will be collected.<br><br><b>Monitor instances "
-                "with explicit AWS service tags ignoring overall tag "
-                "filters:</b><br>Specify explicit tags for these services. The "
-                "overall tags will be ignored for these services.<br><br><b>Monitor "
-                "instances with explicit service names ignoring overall tag "
-                "filters:</b><br>Use this option to specify explicit names. "
-                "The overall tags will be ignored for these services."
-            ),
-            title=title,
-            elements=elements,
-            prefill=DefaultValue("all" if default_enabled else "none"),
-        ),
-        required=True,
-    )
-
-
 def _all_regional_services() -> dict[str, DictElement]:
     return {
-        "ec2": _service_dict_element(
-            Title("Elastic Compute Cloud (EC2)"), extra_elements=_formspec_aws_limits()
+        "ec2": service_dict_element(
+            Title("Elastic Compute Cloud (EC2)"), extra_elements=formspec_aws_limits()
         ),
-        "ebs": _service_dict_element(
-            Title("Elastic Block Storage (EBS)"), extra_elements=_formspec_aws_limits()
+        "ebs": service_dict_element(
+            Title("Elastic Block Storage (EBS)"), extra_elements=formspec_aws_limits()
         ),
-        "s3": _service_dict_element(
+        "s3": service_dict_element(
             Title("Simple Storage Service (S3)"),
             {
-                **_formspec_aws_limits(),
+                **formspec_aws_limits(),
                 "requests": DictElement(
                     parameter_form=FixedValue(
                         value=None,
@@ -204,27 +115,27 @@ def _all_regional_services() -> dict[str, DictElement]:
                 ),
             },
         ),
-        "glacier": _service_dict_element(
-            Title("Amazon S3 Glacier (Glacier)"), extra_elements=_formspec_aws_limits()
+        "glacier": service_dict_element(
+            Title("Amazon S3 Glacier (Glacier)"), extra_elements=formspec_aws_limits()
         ),
-        "elb": _service_dict_element(
-            Title("Classic Load Balancing (ELB)"), extra_elements=_formspec_aws_limits()
+        "elb": service_dict_element(
+            Title("Classic Load Balancing (ELB)"), extra_elements=formspec_aws_limits()
         ),
-        "elbv2": _service_dict_element(
+        "elbv2": service_dict_element(
             Title("Application and Network Load Balancing (ELBv2)"),
-            extra_elements=_formspec_aws_limits(),
+            extra_elements=formspec_aws_limits(),
         ),
-        "rds": _service_dict_element(
-            Title("Relational Database Service (RDS)"), extra_elements=_formspec_aws_limits()
+        "rds": service_dict_element(
+            Title("Relational Database Service (RDS)"), extra_elements=formspec_aws_limits()
         ),
-        "cloudwatch_alarms": _service_dict_element(
-            Title("CloudWatch Alarms"), _formspec_aws_limits(), filterable_by_tags=False
+        "cloudwatch_alarms": service_dict_element(
+            Title("CloudWatch Alarms"), formspec_aws_limits(), filterable_by_tags=False
         ),
-        "dynamodb": _service_dict_element(Title("DynamoDB"), extra_elements=_formspec_aws_limits()),
-        "wafv2": _service_dict_element(
+        "dynamodb": service_dict_element(Title("DynamoDB"), extra_elements=formspec_aws_limits()),
+        "wafv2": service_dict_element(
             Title("Web Application Firewall (WAFV2)"),
             extra_elements={
-                **_formspec_aws_limits(),
+                **formspec_aws_limits(),
                 "cloudfront": DictElement(
                     parameter_form=FixedValue(
                         title=Title("CloudFront WAFs"),
@@ -237,104 +148,17 @@ def _all_regional_services() -> dict[str, DictElement]:
                 ),
             },
         ),
-        "aws_lambda": _service_dict_element(Title("Lambda"), extra_elements=_formspec_aws_limits()),
-        "sns": _service_dict_element(
-            Title("Simple Notification Service (SNS)"), extra_elements=_formspec_aws_limits()
+        "aws_lambda": service_dict_element(Title("Lambda"), extra_elements=formspec_aws_limits()),
+        "sns": service_dict_element(
+            Title("Simple Notification Service (SNS)"), extra_elements=formspec_aws_limits()
         ),
-        "ecs": _service_dict_element(
-            Title("Elastic Container Service (ECS)"), extra_elements=_formspec_aws_limits()
+        "ecs": service_dict_element(
+            Title("Elastic Container Service (ECS)"), extra_elements=formspec_aws_limits()
         ),
-        "elasticache": _service_dict_element(
-            Title("ElastiCache"), extra_elements=_formspec_aws_limits()
+        "elasticache": service_dict_element(
+            Title("ElastiCache"), extra_elements=formspec_aws_limits()
         ),
     }
-
-
-def _formspec_aws_limits() -> dict[str, DictElement]:
-    return {
-        "limits": DictElement(
-            required=True,
-            parameter_form=SingleChoice(
-                title=Title("Service limits"),
-                elements=[
-                    SingleChoiceElement(
-                        title=Title("Monitor limits"),
-                        name="limits",
-                    ),
-                    SingleChoiceElement(
-                        title=Title("Do not monitor limits"),
-                        name="no_limits",
-                    ),
-                ],
-                prefill=DefaultValue("limits"),
-                help_text=Help(
-                    "If limits are monitored, all instances will be fetched "
-                    "regardless of any name or tag restrictions that may have been "
-                    "configured."
-                ),
-            ),
-        )
-    }
-
-
-def _validate_aws_tags(values: Sequence[Any]) -> object:
-    used_keys = []
-    for tag_dict in values:
-        tag_key = tag_dict["key"]
-        tag_values = tag_dict["values"]
-        if tag_key not in used_keys:
-            used_keys.append(tag_key)
-        else:
-            raise ValidationError(
-                Message("Each tag key must be unique and cannot be used multiple times.")
-            )
-        if tag_key.startswith("aws:"):
-            raise ValidationError(Message("Do not use 'aws:' prefix for the key."))
-        if len(tag_key) > 128:
-            raise ValidationError(Message("The maximum key length is 128 characters."))
-        if len(values) > 50:
-            raise ValidationError(Message("The maximum number of tags per resource is 50."))
-        for tag_value in tag_values:
-            if len(tag_value) > 256:
-                raise ValidationError(Message("The maximum value length is 256 characters."))
-            if tag_value.startswith("aws:"):
-                raise ValidationError(Message("Do not use 'aws:' prefix for the value."))
-    return values
-
-
-def _formspec_aws_tags(title: Title | None = None) -> FormSpec:
-    return List(
-        help_text=Help(
-            "For information on AWS tag configuration, visit "
-            "https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Using_Tags.html"
-        ),
-        title=title,
-        element_template=Dictionary(
-            elements={
-                "key": DictElement(
-                    parameter_form=String(
-                        title=Title("Key"),
-                    ),
-                    required=True,
-                ),
-                "values": DictElement(
-                    parameter_form=List(
-                        element_template=String(label=Label("Value")),
-                        add_element_label=Label("Add new value"),
-                        remove_element_label=Label("Remove value"),
-                        no_element_label=Label("No values defined"),
-                        editable_order=False,
-                    ),
-                    required=True,
-                ),
-            },
-        ),
-        add_element_label=Label("Add new tag"),
-        remove_element_label=Label("Remove tag"),
-        no_element_label=Label("No tags defined"),
-        editable_order=False,
-        custom_validate=[_validate_aws_tags],
-    )
 
 
 def _fromspec_aws_api_access() -> dict[str, DictElement]:
@@ -444,64 +268,11 @@ def _migrate_access_vs_to_fs(values: object) -> None:
         assert not isinstance(values["role_arn_id"], tuple)
 
 
-def _convert_tag_list(tags: list[tuple[str, list[str] | str]]) -> list[dict[str, list[str] | str]]:
-    return [
-        {"key": key, "values": values if isinstance(values, list) else [values]}
-        for key, values in tags
-    ]
-
-
-def _convert_selection(
-    selection: Any,
-) -> tuple[Literal["all", "tags", "names"], dict[str, object]]:
-    if selection == "all":
-        return "all", {}
-    selection_type, selection_values = selection
-    if selection_type == "tags":
-        assert isinstance(selection_values, list)
-        return selection_type, {"tags": _convert_tag_list(selection_values)}
-    if selection_type == "names":
-        return selection_type, {"names": selection_values}
-
-    raise ValueError(f"Unknown selection type: {selection_type}")
-
-
 def _migrate_global_services_vs_to_fs(values: object) -> None:
     assert isinstance(values, dict)
     values["ce"] = ("all", {}) if "ce" in values else ("none", None)
     values["route53"] = ("all", {}) if "route53" in values else ("none", None)
-    _migrate_global_service(values, "cloudfront")
-
-
-def _migrate_global_service(values: dict, service: str) -> None:
-    if service not in values:
-        values[service] = ("none", None)
-
-    if isinstance(values[service], dict):
-        selection, selection_vars = _convert_selection(values[service].pop("selection"))
-        values[service] = (
-            selection,
-            {**selection_vars, **values[service]},
-        )
-
-
-def _migrate_regional_service(
-    values: dict, service: str, selection_vs_str: str = "selection"
-) -> None:
-    if service not in values:
-        values[service] = ("none", None)
-
-    if isinstance(values[service], dict):
-        if "limits" in values[service]:
-            values[service]["limits"] = "limits"
-        else:
-            values[service]["limits"] = "no_limits"
-
-        selection, selection_vars = _convert_selection(values[service].pop(selection_vs_str))
-        values[service] = (
-            selection,
-            {**selection_vars, **values[service]},
-        )
+    migrate_global_service(values, "cloudfront")
 
 
 def _migrate_regional_services_vs_to_fs(values: object) -> None:
@@ -509,20 +280,20 @@ def _migrate_regional_services_vs_to_fs(values: object) -> None:
     if "lambda" in values:
         values["aws_lambda"] = values.pop("lambda")
 
-    _migrate_regional_service(values, "ec2")
-    _migrate_regional_service(values, "ebs")
-    _migrate_regional_service(values, "s3")
-    _migrate_regional_service(values, "glacier")
-    _migrate_regional_service(values, "elb")
-    _migrate_regional_service(values, "elbv2")
-    _migrate_regional_service(values, "rds")
-    _migrate_regional_service(values, "cloudwatch_alarms", selection_vs_str="alarms")
-    _migrate_regional_service(values, "dynamodb")
-    _migrate_regional_service(values, "wafv2")
-    _migrate_regional_service(values, "aws_lambda")
-    _migrate_regional_service(values, "sns")
-    _migrate_regional_service(values, "ecs")
-    _migrate_regional_service(values, "elasticache")
+    migrate_regional_service(values, "ec2")
+    migrate_regional_service(values, "ebs")
+    migrate_regional_service(values, "s3")
+    migrate_regional_service(values, "glacier")
+    migrate_regional_service(values, "elb")
+    migrate_regional_service(values, "elbv2")
+    migrate_regional_service(values, "rds")
+    migrate_regional_service(values, "cloudwatch_alarms", selection_vs_str="alarms")
+    migrate_regional_service(values, "dynamodb")
+    migrate_regional_service(values, "wafv2")
+    migrate_regional_service(values, "aws_lambda")
+    migrate_regional_service(values, "sns")
+    migrate_regional_service(values, "ecs")
+    migrate_regional_service(values, "elasticache")
 
 
 def _convert_regions(values: object) -> list[str]:
@@ -548,7 +319,7 @@ def _pre_24_to_formspec_migration(values: object) -> dict[str, object]:
     _migrate_global_services_vs_to_fs(values["global_services"])
     _migrate_regional_services_vs_to_fs(values["services"])
     if "overall_tags" in values:
-        values["overall_tags"] = _convert_tag_list(values["overall_tags"])
+        values["overall_tags"] = convert_tag_list(values["overall_tags"])
 
     values["regions_to_monitor"] = values.pop("regions")
     return values
@@ -638,7 +409,7 @@ def _formspec_aws():
             ),
             **_formspec_aws_piggyback_naming_convention(),
             "overall_tags": DictElement(
-                parameter_form=_formspec_aws_tags(
+                parameter_form=formspec_aws_tags(
                     Title("Restrict monitoring services by one of these AWS tags")
                 ),
             ),

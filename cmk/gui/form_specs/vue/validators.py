48a258fa96bbbedf2dcde0580d6ad8825db441ce
Andreas Boesl <andreas.boesl@checkmk.com>
2024-05-26 14:01:28 +0200

    Vue: Disassembled VueFormSpecVisitor, introduced basic registries for vue form_specs and validators
    Change-Id: Id44fe46a9e216e46efdd0600a8db4240998c5430

--- cmk/gui/form_specs/vue/validators.py
+++ cmk/gui/form_specs/vue/validators.py
@@ -12,50 +12,79 @@ from cmk.gui.i18n import _
 import cmk.rulesets.v1.form_specs.validators as formspec_validators
 
 
-def build_vue_validators(
-    validators: Sequence[Callable[[Any], object]] | None,
-) -> list[VueValidators]:
-    if validators is None:
-        return []
+def build_vue_validators(validators: Sequence[object]) -> list[VueValidators]:
     result = []
     for validator in validators:
         result.extend(_build_vue_validator(validator))
     return result
 
 
-def _build_vue_validator(validator: Callable[[Any], object]) -> list[VueValidators]:
-    if isinstance(validator, formspec_validators.NumberInRange):
-        min_validator = formspec_validators.NumberInRange(min_value=validator.range[0])
-        max_validator = formspec_validators.NumberInRange(max_value=validator.range[1])
-        return [
-            # vue_validators.VueNumberInRange(
-            #    min_value=validator.range[0],
-            #    max_value=validator.range[1],
-            #    error_message=validator.error_msg.localize(_),
-            # ),
-            vue_validators.NumberInRange(
-                min_value=None,
-                max_value=max_validator.range[1],
-                error_message=max_validator.error_msg.localize(_),
-            ),
-            vue_validators.NumberInRange(
-                min_value=min_validator.range[0],
-                max_value=None,
-                error_message=min_validator.error_msg.localize(_),
-            ),
-        ]
-    if isinstance(validator, formspec_validators.LengthInRange):
-        return [
-            vue_validators.LengthInRange(
-                min_value=validator.range[0],
-                max_value=validator.range[1],
-                error_message=validator.error_msg.localize(_),
-            )
-        ]
-    if isinstance(validator, private_form_specs_validators.IsInteger):
-        return [
-            vue_validators.IsNumber(
-                error_message=validator.error_msg.localize(_),
-            )
-        ]
+def _build_vue_validator(validator: object) -> list[VueValidators]:
+    try:
+        if build_function := _validator_registry.get(validator.__class__):
+            return build_function(validator)
+    except AttributeError:
+        pass
     return []
+
+
+def _build_in_range_validator(validator: formspec_validators.NumberInRange) -> list[VueValidators]:
+    value_from, value_to = validator.range
+    assert not isinstance(value_from, float)
+    assert not isinstance(value_to, float)
+    min_validator = formspec_validators.NumberInRange(min_value=validator.range[0])
+    max_validator = formspec_validators.NumberInRange(max_value=validator.range[1])
+    return [
+        vue_validators.NumberInRange(
+            min_value=None,
+            max_value=value_to,
+            error_message=max_validator.error_msg.localize(_),
+        ),
+        vue_validators.NumberInRange(
+            min_value=value_from,
+            max_value=None,
+            error_message=min_validator.error_msg.localize(_),
+        ),
+    ]
+
+
+def _build_length_in_range_validator(
+    validator: formspec_validators.LengthInRange,
+) -> list[VueValidators]:
+    value_from, value_to = validator.range
+    assert not isinstance(value_from, float)
+    assert not isinstance(value_to, float)
+    return [
+        vue_validators.LengthInRange(
+            min_value=value_from,
+            max_value=value_to,
+            error_message=validator.error_msg.localize(_),
+        )
+    ]
+
+
+def _build_is_number_validator(
+    validator: private_form_specs_validators.IsInteger,
+) -> list[VueValidators]:
+    return [
+        vue_validators.IsNumber(
+            error_message=validator.error_msg.localize(_),
+        )
+    ]
+
+
+VueValidatorCreator = Callable[[Any], list[VueValidators]]
+_validator_registry: dict[type, VueValidatorCreator] = {}
+
+
+def register_class(validator_class: object, build_function: VueValidatorCreator) -> None:
+    _validator_registry[validator_class.__class__] = build_function
+
+
+def register_validators():
+    register_class(formspec_validators.NumberInRange, _build_in_range_validator)
+    register_class(formspec_validators.LengthInRange, _build_length_in_range_validator)
+    register_class(private_form_specs_validators.IsInteger, _build_is_number_validator)
+
+
+register_validators()

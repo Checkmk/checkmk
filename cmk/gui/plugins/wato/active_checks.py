39d4673b65417e55da8dcb5a55c1ecb6f1faa510
Sofia Colakovic <sofia.colakovic@checkmk.com>
2024-05-28 13:20:33 +0200

    16856 FIX disk_smb: Allow macros in 'NetBIOS name of the server' field
    With the rework of disk_smb active check in version 2.1.0, using macros in
    'NetBIOS name of the server' was disallowed. Now, the macros are enabled
    for this field again.
    
    SUP-18728
    
    Change-Id: Iaff2aca9501d5d8fe6a2da9bcbe93e276a27463a

--- cmk/gui/plugins/wato/active_checks.py
+++ cmk/gui/plugins/wato/active_checks.py
@@ -2021,7 +2021,7 @@ def _valuespec_active_checks_disk_smb() -> Transform:
                             (
                                 "define_host",
                                 _("Define name of NetBIOS server"),
-                                HostAddress(
+                                TextInput(
                                     title="Define name of NetBIOS server",
                                     allow_empty=False,
                                     help=_("You can specify the NetBIOS server name."),

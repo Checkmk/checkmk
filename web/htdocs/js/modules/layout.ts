21079f389b9d577f7708b7bd14f09407a7720ca9
Moritz Kirschner <moritz.kirschner@checkmk.com>
2024-05-28 18:07:58 +0200

    16827 FIX page_handler errors appearing at the bottom of the page
    Non-user errors that were thrown during the page_handler() call
    were sometimes shown after a lot of content has been rendered,
    depending on when the error was raised. Since they could appear
    after having written a lot of html, we use a small javascript
    helper function to move the error to the top of the page.
    
    SUP-18178
    
    Change-Id: Ic32b69912c2b0b6ecfe2a3d793ad99957a3c4613

---
+++ web/htdocs/js/modules/layout.ts
@@ -0,0 +1,12 @@
+// Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+// This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+// conditions defined in the file COPYING, which is part of this source code package.
+
+export function insertBefore(newElementId: string, targetElementId: string) {
+    const newElement = document.getElementById(newElementId);
+    const targetElement = document.getElementById(targetElementId);
+    const parentElement = targetElement?.parentNode;
+    if (parentElement && newElement && targetElement) {
+        parentElement.insertBefore(newElement, targetElement);
+    }
+}

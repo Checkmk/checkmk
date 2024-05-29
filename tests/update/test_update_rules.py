0b41fcf976d196f77a89e8c94dd409ecee013936
Matteo Stifano <matteo.stifano@checkmk.com>
2024-05-29 11:26:18 +0200

    test_update_rules: Refactoring
    Since there is no agent data involved in this test, we here remove the
    code related to it.
    
    Change-Id: I2630d59f38f55b2335ed2c3546448aadd45d2eb2

--- tests/update/test_update_rules.py
+++ tests/update/test_update_rules.py
@@ -8,9 +8,8 @@ import os
 
 import pytest
 
-from tests.testlib.agent import wait_until_host_receives_data
 from tests.testlib.site import Site
-from tests.testlib.utils import current_base_branch_name, get_services_with_status, repo_path
+from tests.testlib.utils import current_base_branch_name, repo_path
 from tests.testlib.version import CMKVersion, version_from_env
 
 from cmk.utils.hostaddress import HostName
@@ -40,37 +39,7 @@ def test_update_rules(
     )
     logger.info("Creating new host group: %s", host_group_name)
     test_site.openapi.create_host_group(host_group_name, host_group_name)
-
     test_site.activate_changes_and_wait_for_core_reload()
-    wait_until_host_receives_data(test_site, host_name)
-
-    logger.info("Discovering services and waiting for completion...")
-    test_site.openapi.bulk_discover_services_and_wait_for_completion([host_name])
-    test_site.openapi.activate_changes_and_wait_for_completion()
-
-    base_data = {}
-    base_ok_services = {}
-
-    test_site.reschedule_services(host_name)
-
-    # get baseline monitoring data for each host(s)
-    base_data[host_name] = test_site.get_host_services(host_name)
-    ignore_data = [
-        # OMD status service turning into CRIT after the update (looks like for performance reasons)
-        # See CMK-16608. TODO: restore service after ticket is done.
-        f"OMD {test_site.id} status",
-        # "Notification Spooler" results in "No status information, Spooler not running"
-        # See CMK-16760. TODO: restore service after ticket is done.
-        f"OMD {test_site.id} Notification Spooler",
-    ]
-
-    for data in ignore_data:
-        if data in base_data[host_name]:
-            base_data[host_name].pop(data)
-
-    base_ok_services[host_name] = get_services_with_status(base_data[host_name], 0)
-
-    assert len(base_ok_services[host_name]) > 0
 
     target_version = version_from_env(
         fallback_version_spec=CMKVersion.DAILY,
@@ -104,40 +73,3 @@ def test_update_rules(
     assert get_site_status(target_site) == "running", "Invalid service status after updating!"
 
     logger.info("Successfully tested updating %s>%s!", base_version.version, target_version.version)
-
-    logger.info("Discovering services and waiting for completion...")
-    target_site.openapi.bulk_discover_services_and_wait_for_completion([str(host_name)])
-    target_site.openapi.activate_changes_and_wait_for_completion()
-
-    target_data = {}
-    target_ok_services = {}
-
-    target_site.reschedule_services(host_name)
-
-    # get update monitoring data
-    target_data[host_name] = target_site.get_host_services(host_name)
-
-    target_ok_services[host_name] = get_services_with_status(target_data[host_name], 0)
-
-    not_found_services = [
-        service for service in base_data[host_name] if service not in target_data[host_name]
-    ]
-    err_msg = (
-        f"In the {host_name} host the following services were found in base-version but not in "
-        f"target-version: "
-        f"{not_found_services}"
-    )
-    assert len(target_data[host_name]) >= len(base_data[host_name]), err_msg
-
-    not_ok_services = [
-        service
-        for service in base_ok_services[host_name]
-        if service not in target_ok_services[host_name]
-    ]
-    err_msg = (
-        f"In the {host_name} host the following services were `OK` in base-version but not in "
-        f"target-version: "
-        f"{not_ok_services}"
-        f"\nDetails: {[(s, target_data[host_name][s].summary) for s in not_ok_services]})"
-    )
-    assert base_ok_services[host_name].issubset(target_ok_services[host_name]), err_msg

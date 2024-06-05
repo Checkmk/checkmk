94fc845c63b40a5c86211b2c297462f790bbcd87
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-06-05 15:38:22 +0200

    veeam_jobs: delete comment
    This agent ouput is not valid, since it does not have tab seperators. It
    also does not cover any cases relevant to monitoring.
    
    SUP-18723
    
    Change-Id: I14ef26714326622d24f70f99c9db7cf1785619c3

--- cmk/base/plugins/agent_based/veeam_jobs.py
+++ cmk/base/plugins/agent_based/veeam_jobs.py
@@ -3,16 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# <<<veeam_jobs:sep(9) >>>
-# BACKUP_RIS      Backup  Stopped Success 27.10.2013 22:00:17     27.10.2013 22:06:12
-# BACKUP_R43-local_HXWH44 Backup  Stopped Success 26.10.2013 18:00:20     26.10.2013 18:46:03
-# BACKUP_R43-Pool4_HXWH44 Backup  Stopped Failed  26.10.2013 23:13:13     27.10.2013 00:51:17
-# BACKUP_R43-Pool3_HXWH44 Backup  Stopped Failed  27.10.2013 02:59:29     27.10.2013 08:59:51
-# REPL_KNESXIDMZ  Replica Stopped Success 27.10.2013 44:00:01     27.10.2013 44:44:26
-# BACKUP_KNESXI   Backup  Stopped Success 28.10.2013 05:00:04     28.10.2013 05:32:15
-# BACKUP_KNESXit  Backup  Stopped Success 26.10.2013 22:30:02     27.10.2013 02:37:30
-# BACKUP_R43-Pool5_HXWH44 Backup  Stopped Success 27.10.2013 23:00:00     27.10.2013 23:04:53
-# BACKUP_R43-Pool2_HXWH44 Backup  Stopped Failed  27.10.2013 02:37:45     27.10.2013 02:45:35
 
 from collections.abc import Mapping
 from dataclasses import dataclass

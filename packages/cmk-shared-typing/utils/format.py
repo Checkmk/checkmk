53769e733918658fdac2797f885f026c2ed50264
Moritz Kirschner <moritz.kirschner@checkmk.com>
2024-06-05 09:24:45 +0200

    vue: Update shared typing generator
    Change-Id: I77895899816be87ab78b79cc8cee5f61d9615f21

---
+++ packages/cmk-shared-typing/utils/format.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+from datamodel_code_generator.format import CustomCodeFormatter
+from source.vue_formspec.postprocess import postprocess_vue_formspec_components
+
+
+class CodeFormatter(CustomCodeFormatter):
+    def apply(self, code: str) -> str:
+        code = postprocess_vue_formspec_components(code)
+        return code

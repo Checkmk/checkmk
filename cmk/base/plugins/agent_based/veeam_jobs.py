709b46b60c6629d2214f1bc4f315a2ef9e5043a5
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-06-05 15:53:22 +0200

    veeam_jobs: factor parse function out
    SUP-18723
    
    Change-Id: Ia13a96e7ad0a9726120807e71a1f054c2b134c20

--- cmk/base/plugins/agent_based/veeam_jobs.py
+++ cmk/base/plugins/agent_based/veeam_jobs.py
@@ -14,6 +14,8 @@
 # BACKUP_R43-Pool5_HXWH44 Backup  Stopped Success 27.10.2013 23:00:00     27.10.2013 23:04:53
 # BACKUP_R43-Pool2_HXWH44 Backup  Stopped Failed  27.10.2013 02:37:45     27.10.2013 02:45:35
 
+from collections.abc import Mapping
+from dataclasses import dataclass
 
 from cmk.base.plugins.agent_based.agent_based_api.v1 import register, Result, Service, State
 from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
@@ -23,48 +25,62 @@ from cmk.base.plugins.agent_based.agent_based_api.v1.type_defs import (
 )
 
 
-def discovery_veeam_jobs(section: StringTable) -> DiscoveryResult:
-    yield from (Service(item=line[0]) for line in section)
+@dataclass(frozen=True, kw_only=True)
+class Job:
+    type_: str
+    last_state: str
+    last_result: str
+    creation_time: str
+    end_time: str
 
 
-def check_veeam_jobs(item: str, section: StringTable) -> CheckResult:
-    for line in section:
+def discovery_veeam_jobs(section: Mapping[str, Job | None]) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section.keys())
+
+
+def check_veeam_jobs(item: str, section: Mapping[str, Job | None]) -> CheckResult:
+    if (job := section.get(item)) is None:
+        return
+
+    if job.last_state in ["Starting", "Working", "Postprocessing"]:
+        summary = f"Running since {job.creation_time} (current state is: {job.last_state})"
+        yield Result(state=State.OK, summary=summary)
+        return
+
+    if job.last_result == "Success":
+        state = State.OK
+    elif job.last_state == "Idle" and job.type_ == "BackupSync":
+        # A sync job is always idle
+        state = State.OK
+    elif job.last_result == "Failed":
+        state = State.CRIT
+    elif job.last_state == "Stopped" and job.last_result == "Warning":
+        state = State.WARN
+    else:
+        state = State.UNKNOWN
+
+    yield Result(state=State.OK, summary=f"State: {job.last_state}")
+    yield Result(state=state, summary=f"Result: {job.last_result}")
+    yield Result(state=State.OK, summary=f"Creation time: {job.creation_time}")
+    yield Result(state=State.OK, summary=f"End time: {job.end_time}")
+    yield Result(state=State.OK, summary=f"Type: {job.type_}")
+
+
+def parse_veeam_jobs(string_table: StringTable) -> Mapping[str, Job | None]:
+    section: dict[str, Job | None] = {}
+    for line in string_table:
         if len(line) < 6:
-            continue  # Skip incomplete lines
-
-        job_name, job_type, job_last_state, job_last_result, job_creation_time, job_end_time = line[
-            :6
-        ]
-
-        if job_name != item:
-            continue  # Skip not matching lines
-
-        if job_last_state in ["Starting", "Working", "Postprocessing"]:
-            summary = f"Running since {job_creation_time} (current state is: {job_last_state})"
-            yield Result(state=State.OK, summary=summary)
-            return
-
-        if job_last_result == "Success":
-            state = State.OK
-        elif job_last_state == "Idle" and job_type == "BackupSync":
-            # A sync job is always idle
-            state = State.OK
-        elif job_last_result == "Failed":
-            state = State.CRIT
-        elif job_last_state == "Stopped" and job_last_result == "Warning":
-            state = State.WARN
+            section[line[0]] = None
         else:
-            state = State.UNKNOWN
-
-        yield Result(state=State.OK, summary=f"State: {job_last_state}")
-        yield Result(state=state, summary=f"Result: {job_last_result}")
-        yield Result(state=State.OK, summary=f"Creation time: {job_creation_time}")
-        yield Result(state=State.OK, summary=f"End time: {job_end_time}")
-        yield Result(state=State.OK, summary=f"Type: {job_type}")
-
-
-def parse_veeam_jobs(string_table: StringTable) -> StringTable:
-    return string_table
+            type_, last_state, last_result, creation_time, end_time = line[1:6]
+            section[line[0]] = Job(
+                type_=type_,
+                last_state=last_state,
+                last_result=last_result,
+                creation_time=creation_time,
+                end_time=end_time,
+            )
+    return section
 
 
 register.agent_section(

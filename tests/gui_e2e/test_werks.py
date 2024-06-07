79065860fc43faca129d186c8490a4f2a2deb54a
Dhananjay Shah <dhananjay.shah@checkmk.com>
2024-06-07 18:06:12 +0200

    tests.gui_e2e: semantic changes to `test_werks`.
    + converted log messages into assert messages.
        Consolidating all the error messages and present them together
        for ease of reading.
    + changed variable names
    
    Change-Id: I3db6c08a05c001be1aac0d955890052460d3a749

--- tests/gui_e2e/test_werks.py
+++ tests/gui_e2e/test_werks.py
@@ -30,43 +30,45 @@ def test_werks_available(logged_in_page: LoginPage) -> None:
     logger.info("Checking for editions: %s", ",".join(str(e.value) for e in werk_editions))
     # get all werks (list is required to retain the order)
     raw_werks = cmk.utils.werks.load_raw_files(repo_path() / ".werks")
-    # sort werks by date
-    internal_werks = {werk.id: werk for werk in sorted(raw_werks, key=lambda item: item.date)}
-    # filter werks by edition
-    internal_werks = {
-        werk_id: werk for werk_id, werk in internal_werks.items() if werk.edition in werk_editions
+    # sort werks by date and by edition
+    werks_from_repo = {
+        werk.id: werk
+        for werk in sorted(raw_werks, key=lambda item: item.date)
+        if werk.edition in werk_editions
     }
-    internal_werk_ids = list(internal_werks.keys())
-    assert len(internal_werk_ids) > 0, "No werks found in the repo!"
+    werk_ids_from_repo = list(werks_from_repo.keys())
+    assert len(werk_ids_from_repo) > 0, "No werks found in the repo!"
 
     # get all werks on the werks page (list is required to retain the order)
     werks_page = Werks(logged_in_page.page)
     displayed_werks = werks_page.get_recent_werks()
     displayed_werk_ids = list(displayed_werks.keys())
-    assert len(displayed_werk_ids) > 0, "Werk page does not return any werks!"
+    assert len(displayed_werk_ids) > 0, "Checkmk site does not display any werks!"
 
     # get the relevant slice of werks for comparison (displayed werks are sorted descending)
-    oldest_displayed_werk_index = internal_werk_ids.index(displayed_werk_ids[-1])
-    current_internal_werk_ids = internal_werk_ids[oldest_displayed_werk_index:]
+    oldest_displayed_werk_id_index = werk_ids_from_repo.index(displayed_werk_ids[-1])
+    current_werk_ids_from_repo = werk_ids_from_repo[oldest_displayed_werk_id_index:]
     assert (
-        len(current_internal_werk_ids) > 0
+        len(current_werk_ids_from_repo) > 0
     ), "Could not detect current werks! Site not in sync with repo?"
 
     # detect any missing werks
-    different_werks = set(current_internal_werk_ids).symmetric_difference(set(displayed_werk_ids))
+    different_werks = set(current_werk_ids_from_repo).symmetric_difference(set(displayed_werk_ids))
+    assert_msg = []
     if different_werks:
-        missing_werks = set(current_internal_werk_ids).difference(set(displayed_werk_ids))
-        if missing_werks:
-            logger.warning("Missing werks detected: %s", missing_werks)
-        unexpected_werks = set(displayed_werk_ids).difference(set(internal_werk_ids))
-        if unexpected_werks:
-            logger.warning("Unexpected werks found: %s Site ahead of repo?", unexpected_werks)
-        missing_werks_cnt = len(missing_werks)
-        # check if the site is outdated (i.e. missing werks are identical to the latest werks)
-        latest_werks = set(current_internal_werk_ids[-missing_werks_cnt:])
-        if missing_werks == latest_werks:
-            logger.warning("Latest werks missing! Site not in sync with repo?")
-    assert len(different_werks) == 0, "Werks mismatch! Make sure site and repo are in sync!"
+        if missing_werks := set(current_werk_ids_from_repo) - set(displayed_werk_ids):
+            assert_msg.append(f"Checkmk site does not list following werks: {missing_werks}")
+            missing_werks_cnt = len(missing_werks)
+            # check if the site is outdated (i.e. missing werks are identical to the latest werks)
+            latest_werks = set(current_werk_ids_from_repo[-missing_werks_cnt:])
+            if missing_werks == latest_werks:
+                assert_msg.append("Latest werks missing! Site not in sync with repo?")
+        if unexpected_werks := set(displayed_werk_ids) - set(werk_ids_from_repo):
+            assert_msg.append(
+                f"Checkmk site lists unexpected werks: {unexpected_werks}\n" "Site ahead of repo?"
+            )
+
+    assert len(different_werks) == 0, "\n".join(assert_msg)
 
     # check that all werk links share the same url format
     for werk in displayed_werks:

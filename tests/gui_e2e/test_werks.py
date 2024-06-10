4ab9da89aa40f60e981ff5217c3dc1c6e7678198
Dhananjay Shah <dhananjay.shah@checkmk.com>
2024-06-10 08:41:10 +0200

    tests.gui_e2e: modify tests within `gui_e2e.test_werks`
    + unskip `test_werks_available` and update test-validation
        Limiting the test-case to validate the filtering mechanism, and
        navigating into individual Werks.
    
        Why?
        `werks` being displayed on GUI were compared to the ones
        present within repository at the time of the test run.
    
        This comparison is not reliable because,
        * `werks` displayed in GUI correspond to the ones
            present in the repository at the time of Checkmk build.
        * `werks` present in the repository may change in the time-period
            between a Checkmk build and test run.
    
        Thus, resulting in a mismatch in the comparison.
    
    + add `test_navigate_to_werks`
        + sanity check for the werks page.
        + corresponding updates to Werks object.
    
    CMK-17126
    
    Change-Id: I5bfb95ae9cf4182413408a95270e9af9efbceece

--- tests/gui_e2e/test_werks.py
+++ tests/gui_e2e/test_werks.py
@@ -4,73 +4,57 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import logging
+from collections.abc import Iterator
 
 import pytest
+from playwright.sync_api import expect
+from playwright.sync_api import TimeoutError as PWTimeoutError
 
 from tests.testlib.playwright.pom.login import LoginPage
 from tests.testlib.playwright.pom.werks import Werks
-from tests.testlib.repo import repo_path
-
-import cmk.utils.werks
-
-from cmk.werks.models import Edition
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skip(reason="skip until CMK-17126.")
-def test_werks_available(logged_in_page: LoginPage) -> None:
-    # get the expected editions for the werks
-    # NOTE: We can not use cmk_version to detect the edition due to monkey-patching in the testlib!
-    # since the tests are always running in a CEE environment, we do not consider other editions
-    werk_editions = {
-        Edition.CRE,
-        Edition.CEE,
-    }
-    logger.info("Checking for editions: %s", ",".join(str(e.value) for e in werk_editions))
-    # get all werks (list is required to retain the order)
-    raw_werks = cmk.utils.werks.load_raw_files(repo_path() / ".werks")
-    # sort werks by date and by edition
-    werks_from_repo = {
-        werk.id: werk
-        for werk in sorted(raw_werks, key=lambda item: item.date)
-        if werk.edition in werk_editions
-    }
-    werk_ids_from_repo = list(werks_from_repo.keys())
-    assert len(werk_ids_from_repo) > 0, "No werks found in the repo!"
+@pytest.fixture(name="werks_page", scope="function")
+def fixture_werks_page(logged_in_page: LoginPage) -> Iterator[Werks]:
+    yield Werks(logged_in_page.page)
+
+
+def test_werks_available(werks_page: Werks) -> None:
 
     # get all werks on the werks page (list is required to retain the order)
-    werks_page = Werks(logged_in_page.page)
     displayed_werks = werks_page.get_recent_werks()
     displayed_werk_ids = list(displayed_werks.keys())
     assert len(displayed_werk_ids) > 0, "Checkmk site does not display any werks!"
-
-    # get the relevant slice of werks for comparison (displayed werks are sorted descending)
-    oldest_displayed_werk_id_index = werk_ids_from_repo.index(displayed_werk_ids[-1])
-    current_werk_ids_from_repo = werk_ids_from_repo[oldest_displayed_werk_id_index:]
-    assert (
-        len(current_werk_ids_from_repo) > 0
-    ), "Could not detect current werks! Site not in sync with repo?"
-
-    # detect any missing werks
-    different_werks = set(current_werk_ids_from_repo).symmetric_difference(set(displayed_werk_ids))
-    assert_msg = []
-    if different_werks:
-        if missing_werks := set(current_werk_ids_from_repo) - set(displayed_werk_ids):
-            assert_msg.append(f"Checkmk site does not list following werks: {missing_werks}")
-            missing_werks_cnt = len(missing_werks)
-            # check if the site is outdated (i.e. missing werks are identical to the latest werks)
-            latest_werks = set(current_werk_ids_from_repo[-missing_werks_cnt:])
-            if missing_werks == latest_werks:
-                assert_msg.append("Latest werks missing! Site not in sync with repo?")
-        if unexpected_werks := set(displayed_werk_ids) - set(werk_ids_from_repo):
-            assert_msg.append(
-                f"Checkmk site lists unexpected werks: {unexpected_werks}\n" "Site ahead of repo?"
-            )
-
-    assert len(different_werks) == 0, "\n".join(assert_msg)
 
     # check that all werk links share the same url format
     for werk in displayed_werks:
         response = werks_page.go(displayed_werks[werk])
         assert response and response.ok, f"Could not navigate to werk {werk}!"
+
+
+def test_navigate_to_werks(werks_page: Werks) -> None:
+    # validate presence of dropdown buttons
+    for button_name in werks_page.dropdown_buttons:
+        expect(werks_page.dropdown_button(button_name)).to_be_visible()
+
+    # validate 'Filter' button works
+    werks_page.get_link("Filter").click()
+    expect(
+        werks_page.main_area.locator().get_by_role(role="heading", name="Filter")
+    ).to_be_visible()
+    expect(werks_page.apply_filter).to_have_count(1)
+    expect(werks_page.reset_filter).to_have_count(1)
+
+    # validate 'Acnowledge all' button is disabled
+    with pytest.raises(PWTimeoutError):
+        werks_page.get_link("Acknowledge all").click(timeout=5000)
+
+    # validate presence of Werks
+    max_number_of_werks_displayed = 100
+    number_of_werks_displayed = werks_page.get_link("#", exact=False).count()
+    assert number_of_werks_displayed > 0, "Checkmk site does not display any werks!"
+    assert (
+        number_of_werks_displayed <= max_number_of_werks_displayed
+    ), f"Checkmk site displays a maximum of {max_number_of_werks_displayed} werks, by default!"
--- tests/testlib/playwright/pom/werks.py
+++ tests/testlib/playwright/pom/werks.py
@@ -3,11 +3,14 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from playwright.sync_api import Locator
+
 from tests.testlib.playwright.pom.page import CmkPage
 
 
 class Werks(CmkPage):
     page_title: str = "Change log (Werks)"
+    dropdown_buttons: list[str] = ["Werks", "Display", "Help"]
 
     def navigate(self) -> str:
         self.click_and_wait(self.main_menu.help_werks, navigate=True)
@@ -16,10 +19,11 @@ class Werks(CmkPage):
         return self.page.url
 
     def get_recent_werks(self, count: int = 100) -> dict[int, str]:
-        self.main_area.locator("#menu_suggestion_filters").click()
+        """Group werks by `Day of creation` using the filter mechanism."""
+        self.get_link("Filter").click()
         filter_popup = self.main_area.locator("#popup_filters")
         filter_popup.locator("#wo_grouping").select_option(label="Day of creation")
-        self.click_and_wait(filter_popup.locator("#apply"), navigate=True)
+        self.click_and_wait(self.apply_filter, navigate=True)
 
         links = self.main_area.locator("a").get_by_text("#").element_handles()[:count]
         werks = {
@@ -27,3 +31,11 @@ class Werks(CmkPage):
         }
 
         return werks
+
+    @property
+    def apply_filter(self) -> Locator:
+        return self.main_area.locator().get_by_role(role="button", name="Apply")
+
+    @property
+    def reset_filter(self) -> Locator:
+        return self.main_area.locator().get_by_role(role="button", name="Reset")

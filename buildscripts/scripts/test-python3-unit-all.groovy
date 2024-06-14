cd03eb334cd601fbb1dfaa038e334e80bca07d31
René Slowenski <rene.slowenski@checkmk.com>
2024-06-14 12:57:29 +0200

    Merge unit-resilience tests into unit-tests
    It adds no value to have the randomized and the unrandomized unit tests.
    
    Additionally, the benefit of running each test twice is close to zero.
    
    Therefore this change merges the resilience tests into the default unit
    tests and removes --count=2.
    
    Also there is no distinction between xdist and sequential execution anymore,
    as it adds no value and running the tests sequentially actually adds the risk
    to break the parallelized tests without noticing.
    
    Finally, this change replaces test-unit-slow with test-unit-all, which can
    be run as part of the post-commit-chain.
    
    Change-Id: Id3d5a3b4cc2d70d2c9aca7057ba94ad03dd10f6d

--- buildscripts/scripts/test-python3-unit-all.groovy
+++ buildscripts/scripts/test-python3-unit-all.groovy
@@ -1,14 +1,14 @@
 #!groovy
 
-/// file: test-python3-unit-slow.groovy
+/// file: test-python3-unit-all.groovy
 
 def main() {
     def docker_args = "--ulimit nofile=1024:1024 --init";
     docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
         docker_image_from_alias("IMAGE_TESTING").inside(docker_args) {
-            stage('run test-unit-slow') {
+            stage('run test-unit-all') {
                 dir("${checkout_dir}") {
-                    sh("make -C tests test-unit-slow");
+                    sh("make -C tests test-unit-all");
                 }
             }
         }

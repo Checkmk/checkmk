9e36b6b1ffefd2f23e4fa8896bec88a61f8bd761
Anastasiia Shevchuk <anastasiia.shevchuk@checkmk.com>
2024-06-21 09:45:16 +0200

    gui-e2e: add sanity test for default Dashboard page
    Dashboard Page Pom object is created. Sanity test to check
    specific elements on default Dashboard page is added: CMK-17863.
    
    Change-Id: Id625a2f1ca7a85e2afbe14f5c8d26fc0c5101f35

---
+++ tests/gui_e2e/test_dashboard.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from playwright.sync_api import expect
+
+from tests.testlib.playwright.pom.dashboard import Dashboard
+from tests.testlib.playwright.pom.login import LoginPage
+
+
+def test_dashboard_sanity_check(logged_in_page: LoginPage) -> None:
+    dashboard_page = Dashboard(logged_in_page.page, navigate_to_page=False)
+
+    for dashlet_title in dashboard_page.default_dashlets_list:
+        expect(dashboard_page.dashlet(dashlet_title)).to_be_visible()
+
+    for dropdown_button_name in dashboard_page.dropdown_buttons:
+        expect(dashboard_page.dropdown_button(dropdown_button_name)).to_be_visible()
+
+    for icon_title in dashboard_page.icons_list:
+        expect(dashboard_page.menu_icon(icon_title)).to_be_visible()
+
+    expect(dashboard_page.dashlet_svg("Host statistics")).to_be_visible()
+    expect(dashboard_page.dashlet_svg("Service statistics")).to_be_visible()
---
+++ tests/testlib/playwright/pom/dashboard.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+import re
+
+from playwright.sync_api import expect, Locator, Page
+
+from tests.testlib.playwright.pom.page import CmkPage
+
+
+class Dashboard(CmkPage):
+    """Represent the page `Main dashboard`."""
+
+    page_title: str = "Main dashboard"
+
+    default_dashlets_list: list[str] = [
+        "Host statistics",
+        "Service statistics",
+        "Problem notifications",
+        "Host overview",
+        "Total host problems",
+        "Total service problems",
+        "Percentage of total service problems",
+        "Top alerters (last 7 days)",
+    ]
+
+    icons_list: list[str] = [
+        "Main dashboard",
+        "Problem dashboard",
+        "Checkmk dashboard",
+        "Filter",
+    ]
+
+    dropdown_buttons: list[str] = ["Dashboard", "Add", "Dashboards", "Display", "Help"]
+
+    def __init__(self, page: Page, navigate_to_page: bool = True) -> None:
+        super().__init__(page, navigate_to_page)
+
+    def navigate(self) -> None:
+        self.main_menu.main_page.click()
+        self.page.wait_for_url(url=re.compile("dashboard.py$"), wait_until="load")
+        self._validate_page()
+
+    def _validate_page(self) -> None:
+        self.main_area.check_page_title(self.page_title)
+        expect(self.dashlet("Host statistics")).to_be_visible()
+        expect(self.menu_icon("Filter")).to_be_visible()
+
+    def menu_icon(self, icon_title: str) -> Locator:
+        return self.main_area.locator().get_by_title(icon_title)
+
+    def dashlet(self, dashlet_name: str) -> Locator:
+        return self.main_area.locator(
+            f'div[class*="dashlet "]:has(text:text-is("{dashlet_name}")), '
+            f'div[class*="dashlet "]:has(a:text-is("{dashlet_name}"))'
+        )
+
+    def dashlet_svg(self, dashlet_name: str) -> Locator:
+        return self.dashlet(dashlet_name).locator("svg")

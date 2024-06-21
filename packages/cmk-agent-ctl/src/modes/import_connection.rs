2228b6d81a37e2f0d6329df80df16d0955bd075a
Joerg Herbel <joerg.herbel@checkmk.com>
2024-06-21 14:37:25 +0200

    17070 FIX Agent controller on Linux: More informative error message in case of file reading erros in import mode
    SUP-19112
    
    Change-Id: I09d2d9426c2529efb8a035c303cc9a32f8478552

--- packages/cmk-agent-ctl/src/modes/import_connection.rs
+++ packages/cmk-agent-ctl/src/modes/import_connection.rs
@@ -2,6 +2,8 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
+#[cfg(unix)]
+use crate::constants;
 use crate::modes::registration::ProxyPullData;
 use crate::{cli, config};
 use anyhow::{Context, Result as AnyhowResult};
@@ -17,8 +19,22 @@ struct ImportDataFromFile {
 
 impl ImportDataProvider for ImportDataFromFile {
     fn provide(&self) -> AnyhowResult<ProxyPullData> {
-        ProxyPullData::load(&self.path)
-            .context(format!("Failed to read file {}", &self.path.display()))
+        #[cfg(unix)]
+        let additional_error_hint = format!(
+            " In case of permission issues, please keep in mind that the agent controller runs as \
+            the {} user, independently of the user who executed it. Hence, {} must have read \
+            access to the input file. Alternatively, the file content can be passed via STDIN, see \
+            the command-line help of the import mode (-h flag).",
+            constants::CMK_AGENT_USER,
+            constants::CMK_AGENT_USER
+        );
+        #[cfg(not(unix))]
+        let additional_error_hint = String::new();
+        ProxyPullData::load(&self.path).context(format!(
+            "Failed to read file {}.{}",
+            &self.path.display(),
+            &additional_error_hint
+        ))
     }
 }
 

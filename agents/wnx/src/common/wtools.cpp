edefde2458c1b248fad14658ba73045aeca7a28d
Sergey Kipnis <sergey.kipnis@checkmk.com>
2024-06-24 10:13:54 +0200

    Revert "wa: fix permissions in data folder"
    This reverts commit ddda64d2119aff5abf1c26f8aa7b6a3283a1cef8.

--- agents/wnx/src/common/wtools.cpp
+++ agents/wnx/src/common/wtools.cpp
@@ -2753,6 +2753,9 @@ void ProtectPathFromUserWrite(const fs::path &path,
         fmt::format(L"icacls \"{}\" /grant:r *S-1-5-32-545:(OI)(CI)(RX) /c",
                     path.wstring()));
 
+    for (auto const t : commands) {
+        commands.emplace_back(t);
+    }
     XLOG::l.i("Protect path from User write '{}'", path);
 }
 

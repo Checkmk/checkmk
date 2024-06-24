e8663a115f57488b212deaafe2439e7c6957de84
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-06-24 14:33:09 +0200

    piggyback: make interface explicit
    Change-Id: I976bbcdaa8a0e315e4a7224537b6c37b8224e184

---
+++ cmk/piggyback/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from ._storage import (
+    cleanup_piggyback_files,
+    get_piggyback_raw_data,
+    get_piggybacked_host_with_sources,
+    PiggybackFileInfo,
+    PiggybackRawDataInfo,
+    remove_source_status_file,
+    store_piggyback_raw_data,
+)
+
+__all__ = [
+    "cleanup_piggyback_files",
+    "get_piggybacked_host_with_sources",
+    "get_piggyback_raw_data",
+    "PiggybackFileInfo",
+    "PiggybackRawDataInfo",
+    "remove_source_status_file",
+    "store_piggyback_raw_data",
+]

c7a664f2c58d3ad277502918e4260d15a591963c
Leonardo Petrora <leonardo.petrora@checkmk.com>
2024-06-27 14:28:56 +0200

    Added basic ShadCN components in order to use them in Quick Setup component
    Change-Id: I6e65bcaf48709b23a93977bdb239583765cb5c25

---
+++ packages/cmk-frontend-vue/src/components/ui/collapsible/index.ts
@@ -0,0 +1,3 @@
+export { default as Collapsible } from './UiCollapsible.vue'
+export { default as CollapsibleTrigger } from './CollapsibleTrigger.vue'
+export { default as CollapsibleContent } from './CollapsibleContent.vue'
---
+++ packages/cmk-frontend-vue/src/components/ui/label/index.ts
@@ -0,0 +1,1 @@
+export { default as Label } from './UiLabel.vue'

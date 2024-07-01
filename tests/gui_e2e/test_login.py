9af4d4b16587eb77b922ba5bfdf06c9a20e56f3a
Dhananjay Shah <dhananjay.shah@checkmk.com>
2024-07-01 16:27:58 +0200

    gui-e2e: remove redundant test-cases.
    * `test_login_works` <> `test_dashboard::test_dashboard_sanity_check`
    * `test_logout_works` <> `test_menu_user::test_user_logout`
    
    Change-Id: I71869cb04590840843b27e6a64935db2f1812911

--- tests/gui_e2e/test_login.py
+++ tests/gui_e2e/test_login.py
@@ -12,15 +12,6 @@ from tests.testlib.playwright.pom.login import LoginPage
 from tests.testlib.site import Site
 
 
-def test_login_works(logged_in_page: LoginPage) -> None:
-    logged_in_page.main_area.check_page_title("Main dashboard")
-
-
-def test_logout_works(logged_in_page: LoginPage) -> None:
-    logged_in_page.main_menu.user_logout.click()
-    logged_in_page.page.locator("#login_window").is_visible()
-
-
 @pytest.mark.parametrize(
     ["url", "mobile_device"],
     [

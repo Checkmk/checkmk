e3ef008310592b0f363cfc7891f798b49eeedbd8
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-07-04 09:46:53 +0200

    omd: move load_config
    SUP-18665
    
    Change-Id: Ie2fadc3f5813e08788d28ca0a52df9ca209808b1

--- omd/packages/omd/omdlib/main.py
+++ omd/packages/omd/omdlib/main.py
@@ -42,6 +42,7 @@ from omdlib.config_hooks import (
     ConfigHooks,
     create_config_environment,
     hook_exists,
+    load_config,
     load_config_hooks,
     load_defaults,
     load_hook_dependencies,
@@ -2319,7 +2320,7 @@ def init_site(
     # Change ownership of all files and dirs to site user
     chown_tree(site.dir, site.name)
 
-    site.load_config(load_defaults(site))  # load default values from all hooks
+    site.set_config(load_config(site, load_defaults(site)))
     if config_settings:  # add specific settings
         for hook_name, value in config_settings.items():
             site.conf[hook_name] = value
@@ -2370,7 +2371,7 @@ def finalize_site(
     # The config changes above, made with the site user, have to be also available for
     # the root user, so load the site config again. Otherwise e.g. changed
     # APACHE_TCP_PORT would not be recognized
-    site.load_config(load_defaults(site))
+    site.set_config(load_config(site, load_defaults(site)))
     register_with_system_apache(version_info, site, apache_reload, verbose=verbose)
 
 
@@ -2506,7 +2507,7 @@ def main_update_apache_config(
     args: Arguments,
     options: CommandOptions,
 ) -> None:
-    site.load_config(load_defaults(site))
+    site.set_config(load_config(site, load_defaults(site)))
     if _is_apache_enabled(site):
         register_with_system_apache(
             version_info, site, apache_reload=True, verbose=global_opts.verbose
@@ -2635,7 +2636,7 @@ def main_mv_or_cp(  # pylint: disable=too-many-branches
     sys.stdout.write("OK\n")
 
     # Now switch over to the new site as currently active site
-    new_site.load_config(load_defaults(new_site))
+    new_site.set_config(load_config(new_site, load_defaults(new_site)))
     set_environment(new_site)
 
     # Entry for tmps in /etc/fstab
@@ -3023,7 +3024,7 @@ def main_update(  # pylint: disable=too-many-branches
 
         # Prepare for config_set_all: Refresh the site configuration, because new hooks may introduce
         # new settings and default values.
-        site.load_config(load_defaults(site))
+        site.set_config(load_config(site, load_defaults(site)))
 
         legacy_agent_ca = cert_dir(Path(site.dir)) / "agents/legacy_ca.pem"
         if legacy_agent_ca.is_symlink():
@@ -3233,7 +3234,7 @@ def main_init_action(  # pylint: disable=too-many-branches
         if site.is_disabled():
             continue
 
-        site.load_config(load_defaults(site))
+        site.set_config(load_config(site, load_defaults(site)))
 
         # Handle non autostart sites
         if command in ["start", "restart", "reload"] or ("auto" in options and command == "status"):
@@ -3477,7 +3478,7 @@ def _restore_backup_from_tar(  # pylint: disable=too-many-branches
         sys.stdout.write("Restoring site from %s...\n" % source_descr)
         sys.stdout.flush()
 
-        site.load_config(load_defaults(site))
+        site.set_config(load_config(site, load_defaults(site)))
         orig_apache_port = site.conf["APACHE_TCP_PORT"]
 
         prepare_restore_as_site_user(site, global_opts, options)
@@ -3507,7 +3508,7 @@ def _restore_backup_from_tar(  # pylint: disable=too-many-branches
 
         tar.extract(tarinfo, path=site.dir)
 
-    site.load_config(load_defaults(site))
+    site.set_config(load_config(site, load_defaults(site)))
 
     # give new user all files
     chown_tree(site.dir, site.name)
@@ -4776,7 +4777,7 @@ def main() -> None:  # pylint: disable=too-many-branches
             exec_other_omd(v)
 
     if isinstance(site, SiteContext):
-        site.load_config(load_defaults(site))
+        site.set_config(load_config(site, load_defaults(site)))
 
     # Commands which affect a site and can be called as root *or* as
     # site user should always run with site user privileges. That way

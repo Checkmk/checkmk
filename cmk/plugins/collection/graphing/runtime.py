f775a0aa441758b5feb87338324a466f88408a2c
Simon Jess <simon.jess@checkmk.com>
2024-07-07 16:43:48 +0200

    Migrate storage metrics #1
    Change-Id: I6ccc9099c5825293b66f7904bdcdba78113d0392

---
+++ cmk/plugins/collection/graphing/runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.graphing.v1 import metrics, perfometers, Title
+
+UNIT_TIME = metrics.Unit(metrics.TimeNotation())
+
+metric_runtime = metrics.Metric(
+    name="runtime",
+    title=Title("Process runtime"),
+    unit=UNIT_TIME,
+    color=metrics.Color.BLUE,
+)
+
+perfometer_runtime = perfometers.Perfometer(
+    name="runtime",
+    focus_range=perfometers.FocusRange(
+        perfometers.Closed(0),
+        perfometers.Open(2000000),
+    ),
+    segments=["runtime"],
+)

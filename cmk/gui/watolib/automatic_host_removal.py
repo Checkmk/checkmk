a8234392d5e515176ac1e7568d26fbe94d6d5c5d
Lars Michelsen <lm@checkmk.com>
2024-07-08 08:13:22 +0200

    16562 FIX Fix automatic host removal in case one remote site is not logged in
    The automatic host removal job is executed regularly in the background to remove
    hosts from the monitoring once they cease to exist. In particular for but not
    limited to automatically registered hosts.
    
    This job failed in case one remote site was configured but not logged in, not
    only affecting the not logged in site, but all sites. The not logged in site is
    now being skipped, leaving the mechanism intact for all other sites.
    
    Change-Id: I7f8a67eb8624354c76b74ba5ff7f43152c68b1bf

--- cmk/gui/watolib/automatic_host_removal.py
+++ cmk/gui/watolib/automatic_host_removal.py
@@ -28,7 +28,7 @@ from cmk.gui.session import SuperUserContext
 from cmk.gui.site_config import get_site_config, is_wato_slave_site, site_is_local, wato_site_ids
 from cmk.gui.watolib.activate_changes import ActivateChangesManager
 from cmk.gui.watolib.automation_commands import AutomationCommand
-from cmk.gui.watolib.automations import do_remote_automation
+from cmk.gui.watolib.automations import do_remote_automation, MKAutomationException
 from cmk.gui.watolib.check_mk_automations import delete_hosts
 from cmk.gui.watolib.hosts_and_folders import folder_tree, Host
 from cmk.gui.watolib.rulesets import SingleRulesetRecursively, UseHostFolder
@@ -124,8 +124,10 @@ def _hosts_to_be_removed_for_site(
                     [],
                 )
             )
-        except MKUserError:  # Site may be down
-            job_interface.send_progress_update(f"Skipping remote site {site_id}, might be down")
+        except (MKUserError, MKAutomationException) as e:
+            job_interface.send_progress_update(
+                f"Skipping remote site {site_id}, might be down or not logged in ({e})"
+            )
             return
         hostnames = json.loads(hostnames_serialized)
 

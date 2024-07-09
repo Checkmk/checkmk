49c31cf41b3fd95a52fda5ae4c96c0a9c192521a
Simon Jess <simon.jess@checkmk.com>
2024-07-09 12:30:23 +0200

    Graphing objects bundles: Fix licenses
    Change-Id: Ifd5fc690835cd8f2bfad7cb90c8413c20d4f4713

--- tests/unit/cmk/gui/graphing/test_loader.py
+++ tests/unit/cmk/gui/graphing/test_loader.py
@@ -285,7 +285,6 @@ _SKIP_MODULES: Final[Sequence[str]] = [
     "cmk.plugins.collection.graphing.interfaces",
     "cmk.plugins.collection.graphing.kernel",
     "cmk.plugins.collection.graphing.kube",
-    "cmk.plugins.collection.graphing.licenses",
     "cmk.plugins.collection.graphing.mail",
     "cmk.plugins.collection.graphing.mqtt",
     "cmk.plugins.collection.graphing.network",

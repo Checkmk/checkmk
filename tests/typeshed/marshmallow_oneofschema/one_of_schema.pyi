82973ef76fa97e4c15fff0e2a08c0da1e6fdb581
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-07-10 09:43:04 +0200

    fix missing headers
    Change-Id: I144172de7a8b07b07b7b8fb6e552c8b010d5ec6e

--- tests/typeshed/marshmallow_oneofschema/one_of_schema.pyi
+++ tests/typeshed/marshmallow_oneofschema/one_of_schema.pyi
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 from collections.abc import Mapping
 from typing import Any, Type
 
 from marshmallow import Schema
-
 
 class OneOfSchema(Schema):
     type_field: str
--- tests/typeshed/robot/result.pyi
+++ tests/typeshed/robot/result.pyi
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 from collections.abc import Sequence
 from datetime import timedelta
 from typing import Literal

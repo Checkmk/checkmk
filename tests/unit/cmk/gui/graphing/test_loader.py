780ea17df214083f3bb79837d99855c67d28f043
Joerg Herbel <joerg.herbel@checkmk.com>
2024-07-10 14:36:50 +0200

    Fix Robotmk graphing & metrics module layout
    Also drop the runtime peak-attempt and test runtime graph definitions,
    since they were unnecessary.
    
    Change-Id: I46880238e4f9160263c50b034f02648866b4ac0e

--- tests/unit/cmk/gui/graphing/test_loader.py
+++ tests/unit/cmk/gui/graphing/test_loader.py
@@ -295,7 +295,6 @@ _SKIP_MODULES: Final[Sequence[str]] = [
     "cmk.plugins.collection.graphing.interfaces",
     "cmk.plugins.collection.graphing.network",
     "cmk.plugins.oracle.graphing.oracle",
-    "cmk.plugins.robotmk.graphing.cee",
 ]
 
 

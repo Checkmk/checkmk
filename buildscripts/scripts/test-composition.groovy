1bbba89714f5d5088b80c5979abe247a24e04090
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-07-11 15:54:11 +0200

    If it's a safe branch name, call it a safe_branch_name
    Part II:
    Basically rename the branch_name variable to safe_branch_name
    
    Prepare CMK-18210 and CMK-15794
    
    Change-Id: I853b066ee3ee287b6144bb6ea8d377e3538b1d01

--- buildscripts/scripts/test-composition.groovy
+++ buildscripts/scripts/test-composition.groovy
@@ -25,12 +25,12 @@ def main() {
 
     def distros = versioning.get_distros(edition: EDITION, use_case: "daily_tests", override: OVERRIDE_DISTROS);
 
-    def branch_name = versioning.safe_branch_name(scm);
+    def safe_branch_name = versioning.safe_branch_name(scm);
     def branch_version = versioning.get_branch_version(checkout_dir);
     def docker_tag = versioning.select_docker_tag(
-        branch_name,     // 'branch' returns '<BRANCH>-latest'
-        env.DOCKER_TAG,  // 'build tag'
-        env.DOCKER_TAG); // FIXME was DOCKER_TAG_DEFAULT before, 'folder tag'
+        safe_branch_name,   // 'branch' returns '<BRANCH>-latest'
+        env.DOCKER_TAG,     // 'build tag'
+        env.DOCKER_TAG);    // FIXME was DOCKER_TAG_DEFAULT before, 'folder tag'
 
     currentBuild.description += (
         """
@@ -57,8 +57,8 @@ def main() {
                 VERSION: VERSION,
                 DOCKER_TAG: docker_tag,
                 MAKE_TARGET: "test-composition-docker",
-                BRANCH: branch_name,  // FIXME was BRANCH before
-                cmk_version: versioning.get_cmk_version(branch_name, branch_version, VERSION),
+                BRANCH: safe_branch_name,  // FIXME was BRANCH before
+                cmk_version: versioning.get_cmk_version(safe_branch_name, branch_version, VERSION),
             );
         }
     }

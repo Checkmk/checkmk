7e19399cf13ee751d1e626292c23877a04ad3acd
Konstantin Baikov <konstantin.baikov@checkmk.com>
2024-07-17 10:06:10 +0200

    Fix strange test failures with spool files
    The hypothesis is that if the tests take a bit more time
    the additional spool files appear.
    That is fixed with tick=False which is different from the
    default befaviour of the previous freezegun lib.
    
    Change-Id: If73a4a8bb1efd1fdc013767ad39bf1481cb6f09d

--- tests/unit/cmk/plugins/logwatch/agent_based/test_logwatch_ec.py
+++ tests/unit/cmk/plugins/logwatch/agent_based/test_logwatch_ec.py
@@ -752,7 +752,7 @@ def test_forward_tcp_message_update_old_spoolfiles() -> None:
 
     # first we create a spooled message for a logwatch_ec service with "separate_checks" = False
     # this is the same as the old behaviour, before werk 15397 with "seperate_checks" = True
-    with time_machine.travel(datetime.datetime.fromisoformat("2023-10-31 16:02:00Z")):
+    with time_machine.travel(datetime.datetime.fromisoformat("2023-10-31 16:02:00Z"), tick=False):
         _result, messages_forwarded = _forward_message(
             tcp_result="set exception",
             method=SPOOL_METHOD,
@@ -762,7 +762,7 @@ def test_forward_tcp_message_update_old_spoolfiles() -> None:
     assert list(f.name for f in spool_dir.iterdir()) == ["spool.1698768120.00"]
 
     # now we do the same, but for a different item:
-    with time_machine.travel(datetime.datetime.fromisoformat("2023-10-31 16:03:00Z")):
+    with time_machine.travel(datetime.datetime.fromisoformat("2023-10-31 16:03:00Z"), tick=False):
         _result, messages_forwarded = _forward_message(
             tcp_result="set exception",
             method=SPOOL_METHOD,
@@ -777,7 +777,7 @@ def test_forward_tcp_message_update_old_spoolfiles() -> None:
     # this was the old behaviour. now we image the customer installed the new version of checkmk.
     # their logwatch_ec services had separate_checks = True from the beginning.
 
-    with time_machine.travel(datetime.datetime.fromisoformat("2023-10-31 16:04:00Z")):
+    with time_machine.travel(datetime.datetime.fromisoformat("2023-10-31 16:04:00Z"), tick=False):
         _result, messages_forwarded = _forward_message(
             tcp_result="ok",
             method=SPOOL_METHOD,

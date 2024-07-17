17b194667382887784d77327f2c478c09ab6500a
Wontek Hong <wontek.hong@checkmk.com>
2024-07-17 10:52:07 +0200

    configuration_bundles: introduce function to identify references of bundles
    Co-authored-by: Wontek Hong <wontek.hong@checkmk.com>
    Co-authored-by: Christopher Krause <christopher.krause@checkmk.com>
    
    Change-Id: I9f62f5af8da0c9559dd031066bc9191c4a9a0902

--- cmk/gui/watolib/configuration_bundles.py
+++ cmk/gui/watolib/configuration_bundles.py
@@ -4,10 +4,122 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from pathlib import Path
-from typing import Literal, NotRequired, TypedDict
+from typing import Callable, Iterable, Literal, Mapping, NewType, NotRequired, TypedDict
 
+from cmk.utils.global_ident_type import GlobalIdent, PROGRAM_ID_QUICK_SETUP
+from cmk.utils.hostaddress import HostName
+from cmk.utils.password_store import Password
+
+from cmk.gui.watolib.hosts_and_folders import Folder, Host
+from cmk.gui.watolib.passwords import load_passwords
+from cmk.gui.watolib.rulesets import AllRulesets, Rule
 from cmk.gui.watolib.simple_config_file import ConfigFileRegistry, WatoSingleConfigFile
 from cmk.gui.watolib.utils import multisite_dir
+
+from cmk.ccc.exceptions import MKGeneralException
+
+BundleId = NewType("BundleId", str)
+IDENT_FINDER = Callable[[GlobalIdent | None], BundleId | None]
+
+
+class SpecialAgentBundleReferences(TypedDict):
+    # TODO: introduce dcd
+    host_name: HostName
+    password_id: str | None
+    special_agent_rule_id: str
+
+
+def identify_special_agent_bundle_references(
+    bundle_group: str, bundle_ids: set[BundleId]
+) -> Mapping[BundleId, SpecialAgentBundleReferences]:
+    """Identify the configuration references of the special agent based configuration bundles.
+
+    Assumptions currently made (this could change in the future if necessary):
+        * one bundle references exactly one host, one special agent rule and optionally one
+        password and dcd config
+        * it is therefore a config error if multiple entities of the same config type are
+        referenced by the same bundle
+    """
+    bundles: dict[BundleId, SpecialAgentBundleReferences] = {}
+    bundle_id_finder = prepare_bundle_id_finder(PROGRAM_ID_QUICK_SETUP, bundle_ids)
+    bundle_rule_ids = dict(
+        collect_rules(
+            finder=bundle_id_finder,
+            rules=AllRulesets.load_all_rulesets().get(bundle_group).get_rules(),
+        )
+    )
+    bundle_password_ids = dict(
+        collect_passwords(finder=bundle_id_finder, passwords=load_passwords())
+    )
+
+    for bundle_id, hostname in collect_hosts(finder=bundle_id_finder, hosts=Host.all().values()):
+        bundles[bundle_id] = {
+            "host_name": hostname,
+            "password_id": bundle_password_ids.get(bundle_id),
+            "special_agent_rule_id": bundle_rule_ids[bundle_id],
+        }
+
+    if len(bundles) != len(bundle_ids):
+        raise MKGeneralException("Not all bundle ids could be resolved")
+
+    return bundles
+
+
+def collect_hosts(
+    finder: IDENT_FINDER, hosts: Iterable[Host]
+) -> Iterable[tuple[BundleId, HostName]]:
+    seen_ids: set[BundleId] = set()
+    for host in hosts:
+        if bundle_id := finder(host.locked_by()):
+            if bundle_id in seen_ids:
+                raise MKGeneralException(
+                    f"One bundle should reference only one host, but bundle {bundle_id} references multiple hosts"
+                )
+            seen_ids.add(bundle_id)
+            yield bundle_id, host.name()
+
+
+def collect_passwords(
+    finder: IDENT_FINDER, passwords: Mapping[str, Password]
+) -> Iterable[tuple[BundleId, str]]:
+    seen_ids: set[BundleId] = set()
+    for password_id, password in passwords.items():
+        if bundle_id := finder(password.get("locked_by")):
+            if bundle_id in seen_ids:
+                raise MKGeneralException(
+                    f"One bundle should reference only one password, but bundle {bundle_id} references multiple passwords"
+                )
+            seen_ids.add(bundle_id)
+            yield bundle_id, password_id
+
+
+def collect_rules(
+    finder: IDENT_FINDER, rules: Iterable[tuple[Folder, int, Rule]]
+) -> Iterable[tuple[BundleId, str]]:
+    seen_ids: set[BundleId] = set()
+    for _folder, _idx, rule in rules:
+        if bundle_id := finder(rule.locked_by):
+            if bundle_id in seen_ids:
+                raise MKGeneralException(
+                    f"One bundle should reference only one rule, but bundle {bundle_id} references multiple rules"
+                )
+            seen_ids.add(bundle_id)
+            yield bundle_id, rule.id
+
+
+def prepare_bundle_id_finder(bundle_program_id: str, bundle_ids: set[BundleId]) -> IDENT_FINDER:
+    def find_matching_bundle_id(
+        ident: GlobalIdent | None,
+    ) -> BundleId | None:
+        if (
+            ident is not None
+            and ident["program_id"] == bundle_program_id
+            and ident["instance_id"] in bundle_ids
+        ):
+            return BundleId(ident["instance_id"])
+        return None
+
+    return find_matching_bundle_id
 
 
 class ConfigBundle(TypedDict):
@@ -19,7 +131,7 @@ class ConfigBundle(TypedDict):
     """
 
     # General properties
-    id: str
+    id: BundleId
     title: str
     comment: str
 

0502472ef7974a56b31a218a4733a91fc86b1485
Wontek Hong <wontek.hong@checkmk.com>
2024-07-24 13:53:48 +0200

    special_agent: add punctuation
    Change-Id: I08030821f9c83d7395f6cc34f515456ed36e1a46

--- cmk/plugins/emcvnx/rulesets/special_agent.py
+++ cmk/plugins/emcvnx/rulesets/special_agent.py
@@ -36,7 +36,7 @@ def _parameter_form() -> Dictionary:
                         "These need to be created manually before using. Therefor run as "
                         "instance user (if using OMD) or Nagios user (if not using OMD) "
                         "a command like "
-                        "<tt>naviseccli -AddUserSecurity -scope 0 -password PASSWORD -user USER</tt> "
+                        "<tt>naviseccli -AddUserSecurity -scope 0 -password PASSWORD -user USER</tt>. "
                         "This creates <tt>SecuredCLISecurityFile.xml</tt> and "
                         "<tt>SecuredCLIXMLEncrypted.key</tt> in the home directory of the user "
                         "and these files are used then."

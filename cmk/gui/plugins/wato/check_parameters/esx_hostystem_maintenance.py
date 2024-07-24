6d4b677b9c6b0b3cb7c6f9a25fa9c71c12b88185
Wontek Hong <wontek.hong@checkmk.com>
2024-07-24 13:20:57 +0200

    esx: fix spelling
    Change-Id: I4af580fda657c02dcbcdeea66acfdf75b27a263d

--- cmk/gui/plugins/wato/check_parameters/esx_hostystem_maintenance.py
+++ cmk/gui/plugins/wato/check_parameters/esx_hostystem_maintenance.py
@@ -36,6 +36,6 @@ rulespec_registry.register(
         group=RulespecGroupCheckParametersStorage,
         match_type="dict",
         parameter_valuespec=_parameter_valuespec_esx_hostystem_maintenance,
-        title=lambda: _("ESX Hostsystem Maintenance Mode"),
+        title=lambda: _("ESX Host System Maintenance Mode"),
     )
 )

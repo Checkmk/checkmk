9c38f5c4dd9e8d45884b674859e836bec58b1213
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-08-01 10:21:31 +0200

    Specify git root for jobs with custom git ref
    ... while getting the branch name via versioning.groovy
    
    CMK-15794
    
    Change-Id: I77278c8f78680bcbdb5e9a2a15c24ab3f4a4be4e

--- buildscripts/scripts/utils/versioning.groovy
+++ buildscripts/scripts/utils/versioning.groovy
@@ -70,7 +70,7 @@ def branch_name(scm) {
         if (branch_name_is_branch_version("${checkout_dir}")) {
             // this is only required as "master" is called "stable branch + 0.1.0"
             // e.g. 2.3.0 (stable) + 0.1.0 = 2.4.0
-            return env.GERRIT_BRANCH ?: get_branch_version()
+            return env.GERRIT_BRANCH ?: get_branch_version("${checkout_dir}")
         } else {
             return env.GERRIT_BRANCH ?: "master"
         }

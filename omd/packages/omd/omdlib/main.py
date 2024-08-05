fd124007420e6052a34fb97dfe2739f6561d3ad6
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-08-05 10:55:50 +0200

    17206 FIX omd update: Log "Verifying site configuration"
    CMK-17551
    
    Change-Id: I0a41db81ee3bd871ea195a5c46f05acd1b1ff50b

--- omd/packages/omd/omdlib/main.py
+++ omd/packages/omd/omdlib/main.py
@@ -2966,8 +2966,13 @@ def main_update(  # pylint: disable=too-many-branches
         mu.prepare_and_populate_tmpfs(version_info, site)
 
         process = subprocess.run(
-            ["cmk-update-config", "--conflict", conflict_mode, "--dry-run"], check=False
+            ["cmk-update-config", "--conflict", conflict_mode, "--dry-run"],
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
         )
+        sys.stdout.write(process.stdout)
         if process.returncode != 0:
             sys.exit(process.returncode)
         sys.stdout.write(

ec0d8f548d9421cdd5c21781aeaeed2b3d6ea2c1
Joerg Herbel <joerg.herbel@checkmk.com>
2024-08-07 09:04:13 +0200

    17229 FIX CPU utilization checking: Alert if utilization is exactly at threshold for too long
    SUP-19338
    
    Change-Id: I48d472bd9b2d771b7cd02a969307de8e06b99cff

--- cmk/plugins/lib/cpu_util.py
+++ cmk/plugins/lib/cpu_util.py
@@ -403,7 +403,7 @@ def cpu_util_time(
     Only sends `notice` output .
     """
     core_states = value_store.get("cpu.util.core.high", {})
-    if perc <= threshold:
+    if perc < threshold:
         # drop core from states
         value_store["cpu.util.core.high"] = {k: v for k, v in core_states.items() if k != core}
         return

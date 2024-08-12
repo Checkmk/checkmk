035cb99bd2e2716cf4c85ef92460d42f0f1cea85
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-08-12 16:11:20 +0200

    Add CIPARAM_OVERRIDE_DOCKER_TAG_BUILD and VERSION parameter to test-update
    Prepare CMK-16523
    Leftover of CMK-18210
    
    Change-Id: I896f4e9c1a7334f096d03ec1521f43bb99228a97
    (cherry picked from commit 09297a982ad9b8e174fe7912bf662472e582dd5b)

--- buildscripts/scripts/test-update.groovy
+++ buildscripts/scripts/test-update.groovy
@@ -4,31 +4,34 @@
 
 def main() {
     check_job_parameters([
-        ["OVERRIDE_DISTROS"],
+        "VERSION",
+        "OVERRIDE_DISTROS",
+        "CIPARAM_OVERRIDE_DOCKER_TAG_BUILD",
     ]);
 
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
     def testing_helper = load("${checkout_dir}/buildscripts/scripts/utils/integration.groovy");
 
     def safe_branch_name = versioning.safe_branch_name(scm);
-    def cmk_version_rc_aware = versioning.get_cmk_version(safe_branch_name, "daily");
+    def cmk_version_rc_aware = versioning.get_cmk_version(safe_branch_name, VERSION);
     def cmk_version = versioning.strip_rc_number_from_version(cmk_version_rc_aware);
 
     def distros = versioning.configured_or_overridden_distros(EDITION, OVERRIDE_DISTROS, "daily_tests");
     def docker_tag = versioning.select_docker_tag(
-        "",                // 'build tag'
-        safe_branch_name,  // 'branch' returns '<BRANCH>-latest'
+        CIPARAM_OVERRIDE_DOCKER_TAG_BUILD,  // 'build tag'
+        safe_branch_name,                   // 'branch' returns '<BRANCH>-latest'
     );
 
     testing_helper.run_make_targets(
         DOCKER_GROUP_ID: get_docker_group_id(),
         DISTRO_LIST: distros,
         EDITION: "enterprise",
-        VERSION: "daily",
+        VERSION: VERSION,
         DOCKER_TAG: docker_tag,
         MAKE_TARGET: "test-update-docker",
         BRANCH: safe_branch_name,
         cmk_version: cmk_version,
     );
 }
+
 return this;

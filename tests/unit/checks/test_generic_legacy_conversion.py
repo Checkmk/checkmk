a7d0dd50204a4f9e4b9a21c70afae54a47563f51
Logan Connolly <logan.connolly@checkmk.com>
2024-08-13 17:43:33 +0200

    17240 splunk: migrate health plugin to v2 architecture
    In addition to the migration, I took the liberty to adapt the summary
    lines to be more useful to the user. In the legacy plugin, all the
    service feature health statuses would be grouped together into one
    multiline string. Now in the migrated plugin, the features are
    summarized alongside their respective service.
    
    Change-Id: I4fff72dbb6b17f9da819b2b567e5a251c459b484

--- tests/unit/checks/test_generic_legacy_conversion.py
+++ tests/unit/checks/test_generic_legacy_conversion.py
@@ -1087,7 +1087,6 @@ def test_no_new_or_vanished_legacy_checks(fix_plugin_legacy: FixPluginLegacy) ->
         "sophos_memory",
         "sophos_messages",
         "splunk_alerts",
-        "splunk_health",
         "splunk_jobs",
         "splunk_license_state",
         "splunk_license_usage",
---
+++ tests/unit/cmk/plugins/splunk/test_health.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# Copyright (C) 2021 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.agent_based.v2 import CheckResult, Result, State
+from cmk.plugins.splunk.agent_based import health
+
+
+def test_parse_splunk_health() -> None:
+    string_table = [
+        ["Overall_state", "green"],
+        ["File_monitor_input", "red"],
+        ["File_monitor_input", "Tailreader-0", "green"],
+        ["File_monitor_input", "Batchreader-0", "red"],
+    ]
+
+    actual = health.parse_splunk_health(string_table)
+    expected = [
+        health.HealthItem(
+            name=health.SplunkServiceName("Overall state"),
+            health=health.HealthStatus.GREEN,
+            features={},
+        ),
+        health.HealthItem(
+            name=health.SplunkServiceName("File monitor input"),
+            health=health.HealthStatus.RED,
+            features={
+                "Tailreader-0": health.HealthStatus.GREEN,
+                "Batchreader-0": health.HealthStatus.RED,
+            },
+        ),
+    ]
+
+    assert actual == expected
+
+
+def test_parse_splunk_health_does_not_parse_successfully_when_main_service_missing() -> None:
+    string_table_missing_main_service_line = [["File_monitor_input", "Tailreader-0", "green"]]
+
+    actual = health.parse_splunk_health(string_table_missing_main_service_line)
+    expected: health.HealthSection = []
+
+    assert actual == expected
+
+
+def test_parse_splunk_health_does_not_parse_when_bad_value_passed_to_pydantic_model() -> None:
+    bad_health_value = "cyan"
+    string_table = [["File_monitor_input", bad_health_value]]
+
+    actual = health.parse_splunk_health(string_table)
+    expected: health.HealthSection = []
+
+    assert actual == expected
+
+
+def test_check_splunk_health() -> None:
+    sections = [
+        health.HealthItem(
+            name=health.SplunkServiceName("Overall state"),
+            health=health.HealthStatus.GREEN,
+        ),
+        health.HealthItem(
+            name=health.SplunkServiceName("File monitor input"),
+            health=health.HealthStatus.RED,
+            features={
+                "Tailreader-0": health.HealthStatus.GREEN,
+                "Batchreader-0": health.HealthStatus.RED,
+            },
+        ),
+    ]
+    default_params: health.CheckParams = {"green": 0, "yellow": 1, "red": 2}
+
+    actual = list(health.check_splunk_health(params=default_params, section=sections))
+    expected: CheckResult = [
+        Result(
+            state=State.OK,
+            summary="Overall state: green",
+        ),
+        Result(
+            state=State.CRIT,
+            summary="File monitor input: red (Batchreader-0: red, Tailreader-0: green)",
+        ),
+    ]
+
+    assert actual == expected
+
+
+def test_check_splunk_health_custom_params_sets_red_to_warn() -> None:
+    sections = [
+        health.HealthItem(
+            name=health.SplunkServiceName("Overall state"),
+            health=health.HealthStatus.RED,
+        )
+    ]
+    custom_params: health.CheckParams = {"green": 0, "yellow": 1, "red": 1}
+
+    actual = list(health.check_splunk_health(params=custom_params, section=sections))
+    expected: CheckResult = [Result(state=State.WARN, summary="Overall state: red")]
+
+    assert actual == expected

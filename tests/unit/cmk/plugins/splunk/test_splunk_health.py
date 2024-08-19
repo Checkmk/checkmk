f01157bd9434f397a7daed5043a0dab827bd2ac7
Logan Connolly <logan.connolly@checkmk.com>
2024-08-19 12:43:07 +0200

    splunk_health: fix the naming of agent plugin and ruleset
    Agent based plugins should be prefixed with `splunk_*", while the
    rulesets name can simply be the component "health".
    
    Change-Id: Ie4ee09a90ccc6fca8ee8b99d86141a2edfc9d31c

--- tests/unit/cmk/plugins/splunk/test_splunk_health.py
+++ tests/unit/cmk/plugins/splunk/test_splunk_health.py
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.agent_based.v2 import CheckResult, Result, State
-from cmk.plugins.splunk.agent_based import health
+from cmk.plugins.splunk.agent_based import splunk_health as health
 
 
 def test_parse_splunk_health() -> None:

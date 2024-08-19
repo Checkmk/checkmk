19faf747d2a0ada638fe52e7e89fea219e57d118
Logan Connolly <logan.connolly@checkmk.com>
2024-08-19 12:48:49 +0200

    splunk_health: rename import alias to plugin in test
    To be consistent with the other test files in the splunk package namespace,
    let's rename the import from `health` to `plugin`.
    
    Change-Id: Idea75d54890f136ee4a83bbdac08c0b44fc5ad5d

--- tests/unit/cmk/plugins/splunk/test_splunk_health.py
+++ tests/unit/cmk/plugins/splunk/test_splunk_health.py
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.agent_based.v2 import CheckResult, Result, State
-from cmk.plugins.splunk.agent_based import splunk_health as health
+from cmk.plugins.splunk.agent_based import splunk_health as plugin
 
 
 def test_parse_splunk_health() -> None:
@@ -15,19 +15,19 @@ def test_parse_splunk_health() -> None:
         ["File_monitor_input", "Batchreader-0", "red"],
     ]
 
-    actual = health.parse_splunk_health(string_table)
+    actual = plugin.parse_splunk_health(string_table)
     expected = [
-        health.HealthItem(
-            name=health.SplunkServiceName("Overall state"),
-            health=health.HealthStatus.GREEN,
+        plugin.HealthItem(
+            name=plugin.SplunkServiceName("Overall state"),
+            health=plugin.HealthStatus.GREEN,
             features={},
         ),
-        health.HealthItem(
-            name=health.SplunkServiceName("File monitor input"),
-            health=health.HealthStatus.RED,
+        plugin.HealthItem(
+            name=plugin.SplunkServiceName("File monitor input"),
+            health=plugin.HealthStatus.RED,
             features={
-                "Tailreader-0": health.HealthStatus.GREEN,
-                "Batchreader-0": health.HealthStatus.RED,
+                "Tailreader-0": plugin.HealthStatus.GREEN,
+                "Batchreader-0": plugin.HealthStatus.RED,
             },
         ),
     ]
@@ -38,8 +38,8 @@ def test_parse_splunk_health() -> None:
 def test_parse_splunk_health_does_not_parse_successfully_when_main_service_missing() -> None:
     string_table_missing_main_service_line = [["File_monitor_input", "Tailreader-0", "green"]]
 
-    actual = health.parse_splunk_health(string_table_missing_main_service_line)
-    expected: health.HealthSection = []
+    actual = plugin.parse_splunk_health(string_table_missing_main_service_line)
+    expected: plugin.HealthSection = []
 
     assert actual == expected
 
@@ -48,34 +48,34 @@ def test_parse_splunk_health_does_not_parse_when_bad_value_passed_to_pydantic_mo
     bad_health_value = "cyan"
     string_table = [["File_monitor_input", bad_health_value]]
 
-    actual = health.parse_splunk_health(string_table)
-    expected: health.HealthSection = []
+    actual = plugin.parse_splunk_health(string_table)
+    expected: plugin.HealthSection = []
 
     assert actual == expected
 
 
 def test_check_splunk_health() -> None:
     sections = [
-        health.HealthItem(
-            name=health.SplunkServiceName("Overall state"),
-            health=health.HealthStatus.GREEN,
+        plugin.HealthItem(
+            name=plugin.SplunkServiceName("Overall state"),
+            health=plugin.HealthStatus.GREEN,
         ),
-        health.HealthItem(
-            name=health.SplunkServiceName("File monitor input"),
-            health=health.HealthStatus.RED,
+        plugin.HealthItem(
+            name=plugin.SplunkServiceName("File monitor input"),
+            health=plugin.HealthStatus.RED,
             features={
-                "Tailreader-0": health.HealthStatus.GREEN,
-                "Batchreader-0": health.HealthStatus.RED,
+                "Tailreader-0": plugin.HealthStatus.GREEN,
+                "Batchreader-0": plugin.HealthStatus.RED,
             },
         ),
     ]
-    default_params: health.CheckParams = {
+    default_params: plugin.CheckParams = {
         "green": State.OK.value,
         "yellow": State.WARN.value,
         "red": State.CRIT.value,
     }
 
-    actual = list(health.check_splunk_health(params=default_params, section=sections))
+    actual = list(plugin.check_splunk_health(params=default_params, section=sections))
     expected: CheckResult = [
         Result(
             state=State.OK,
@@ -92,18 +92,18 @@ def test_check_splunk_health() -> None:
 
 def test_check_splunk_health_custom_params_sets_red_to_warn() -> None:
     sections = [
-        health.HealthItem(
-            name=health.SplunkServiceName("Overall state"),
-            health=health.HealthStatus.RED,
+        plugin.HealthItem(
+            name=plugin.SplunkServiceName("Overall state"),
+            health=plugin.HealthStatus.RED,
         )
     ]
-    custom_params: health.CheckParams = {
+    custom_params: plugin.CheckParams = {
         "green": State.OK.value,
         "yellow": State.WARN.value,
         "red": State.WARN.value,
     }
 
-    actual = list(health.check_splunk_health(params=custom_params, section=sections))
+    actual = list(plugin.check_splunk_health(params=custom_params, section=sections))
     expected: CheckResult = [Result(state=State.WARN, summary="Overall state: red")]
 
     assert actual == expected

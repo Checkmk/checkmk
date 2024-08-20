be0e3eaeae5d989e4e94c57829b6944a4fb4ace1
Niko Wenselowski <niko.wenselowski@checkmk.com>
2024-08-20 17:06:04 +0200

    build-cmk-packages: Add workspace sanity check
    Change-Id: Ice3d43dcd1c2258d5ea4b62b22aa55a09c892be4

--- buildscripts/scripts/build-cmk-packages.groovy
+++ buildscripts/scripts/build-cmk-packages.groovy
@@ -330,6 +330,10 @@ def main() {
                         );
                     }
                 }
+
+                stage("${distro} workspace sanity check") {
+                    check_for_workspace_sanity();
+                }
             }
         }]
     }

d5010da98fa258dc4445ca59625c26730a8cf94f
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2024-08-21 12:12:24 +0200

    17148 SEC Persist known host keys for checks that use SSH
    CMK-18046
    
    Change-Id: I9b66118a3c9ad9a92cf4f73879e6ed9de8d7b504

--- cmk/special_agents/agent_vnx_quotas.py
+++ cmk/special_agents/agent_vnx_quotas.py
@@ -8,6 +8,7 @@ import sys
 from typing import Any
 
 import cmk.utils.password_store
+from cmk.utils.ssh_client import get_ssh_client
 
 
 def parse_arguments(argv):
@@ -24,10 +25,7 @@ def parse_arguments(argv):
 
 def get_client_connection(args):
     try:
-        import paramiko  # type: ignore[import] # pylint: disable=import-outside-toplevel
-
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client = get_ssh_client()
         client.connect(args.hostname, username=args.username, password=args.password, timeout=5)
         return client
 

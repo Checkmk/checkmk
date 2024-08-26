Checkmk/checkmk

commit 4e322c41885f9b69e4df2e2adad2ecd7d8cc412e

Quick setup: fix passwords and dcd connections to keep "locked_by"
after editing and saving passwords and dcd connections the "locked_by"
attribute used to get lost.
Now it is kept if it existed before.

CMK-16812

Change-Id: I140cb11d4b44bbfaf96a00f3de78b4b22c8788d5

cmk/gui/wato/pages/_simple_modes.py
@@ -503,10 +503,14 @@ class SimpleEditMode(_SimpleWatoModeBase[_T], abc.ABC):
         if "ident" in config:
             self._ident = config.pop("ident")
         assert self._ident is not None
+        entries = self._store.load_for_modification()
+
+        # Keep the "locked_by" attribute if it exists in the current entry
+        if "locked_by" in entries[self._ident]:
+            config = {**config, "locked_by": entries[self._ident]["locked_by"]}
+
         # No typing support from valuespecs here, so we need to cast
         self._entry = cast(_T, config)
-
-        entries = self._store.load_for_modification()
 
         if self._new and self._ident in entries:
             raise MKUserError("ident", _("This ID is already in use. Please choose another one."))

commit f83c375fb53df15fb94ac5cadeb1877af899a18b

Fix crash when creating new password or dcd connection
regression caused by: 4e322c41885f9b69e4df2e2adad2ecd7d8cc412e

Change-Id: I7b7779186944eeddbef56fbebe273e001459ff0c

cmk/gui/wato/pages/_simple_modes.py
@@ -506,7 +506,7 @@ class SimpleEditMode(_SimpleWatoModeBase[_T], abc.ABC):
         entries = self._store.load_for_modification()
 
         # Keep the "locked_by" attribute if it exists in the current entry
-        if "locked_by" in entries[self._ident]:
+        if self._ident in entries and "locked_by" in entries[self._ident]:
             config = {**config, "locked_by": entries[self._ident]["locked_by"]}
 
         # No typing support from valuespecs here, so we need to cast
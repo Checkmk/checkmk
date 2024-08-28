f014be070425984b4425882f6064e09446226d74
Christopher Krause <christopher.krause@checkmk.com>
2024-08-28 15:58:09 +0200

    REST-API: add include_links and include_extensions to list saml connections
    Change-Id: Ib450317582378912c64ec73c1d8f3c453fb586cf
    JIRA-Ref: CMK-18835

--- tests/testlib/rest_api_client.py
+++ tests/testlib/rest_api_client.py
@@ -2712,11 +2712,20 @@ class SAMLConnectionClient(RestApiClient):
             expect_ok=expect_ok,
         )
 
-    def get_all(self, expect_ok: bool = True) -> Response:
+    def get_all(
+        self,
+        *,
+        include_links: bool | None = None,
+        include_extensions: bool | None = None,
+        expect_ok: bool = True,
+    ) -> Response:
         return self.request(
             "get",
             url=f"/domain-types/{self.domain}/collections/all",
             expect_ok=expect_ok,
+            query_params=_only_set_keys(
+                {"include_links": include_links, "include_extensions": include_extensions}
+            ),
         )
 
     def create(

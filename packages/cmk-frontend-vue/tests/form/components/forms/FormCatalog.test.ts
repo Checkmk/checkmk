c3faf4a06be5f56f82257bf92db4a8e3eb1c26f6
Benedikt Seidl <benedikt.seidl@checkmk.com>
2024-09-04 13:35:56 +0200

    add basic tests for FormCatalog
    Change-Id: I15ad13b26f38e3b65c5244b185769e332a1b5e67

---
+++ packages/cmk-frontend-vue/tests/form/components/forms/FormCatalog.test.ts
@@ -0,0 +1,163 @@
+import { fireEvent, render, screen } from '@testing-library/vue'
+import FormCatalog from '@/form/components/forms/FormCatalog.vue'
+import type {
+  String as StringSpec,
+  Dictionary as DictionarySpec,
+  DictionaryElement,
+  Catalog
+} from '@/form/components/vue_formspec_components'
+import { renderFormWithData } from '../cmk-form-helper'
+
+type PartialExcept<T, K extends keyof T> = Pick<T, K> & Partial<Omit<T, K>>
+
+function getStringFormspec(
+  title: string,
+  options?: Partial<Omit<DictionarySpec, 'type'>>
+): StringSpec {
+  return {
+    type: 'string',
+    title: title,
+    help: `ut help ${title}`,
+    validators: [],
+    input_hint: `ut input hint ${title}`,
+    ...options
+  }
+}
+
+function getDictionaryFormspec(
+  dictionaryOptions: Partial<Omit<DictionarySpec, 'type'>>,
+  elements: Array<PartialExcept<Omit<DictionaryElement, 'type'>, 'ident' | 'parameter_form'>>
+): DictionarySpec {
+  return {
+    type: 'dictionary',
+    title: 'dictionary title',
+    help: 'dictionary help',
+    groups: [],
+    layout: 'one_column',
+    validators: [],
+    elements: elements.map((element) => {
+      return {
+        required: false,
+        default_value: '',
+        ...element
+      }
+    }),
+    ...dictionaryOptions
+  }
+}
+
+function renderSimpleCatalog() {
+  return render(FormCatalog, {
+    props: {
+      spec: {
+        type: 'catalog',
+        title: 'catalog title',
+        help: 'catalog help',
+        validators: [],
+        topics: [
+          {
+            key: 'some_ut_key',
+            dictionary: getDictionaryFormspec(
+              {
+                title: 'ut embedded dictionary title'
+                // this title will be moved into the catalog and displayed there a stitle
+              },
+              [
+                {
+                  ident: 'ut_string_1',
+                  parameter_form: getStringFormspec('title of string input')
+                }
+              ]
+            )
+          }
+        ]
+      },
+      data: { some_ut_key: {} }, // TODO: some_ut_key is required. should it be?
+      backendValidation: []
+    }
+  })
+}
+
+test('FormCatalog open/close topic', async () => {
+  renderSimpleCatalog()
+
+  // just make sure that the string input is rendered
+  await screen.findByText('title of string input')
+
+  const headline = await screen.findByText('ut embedded dictionary title')
+  // the visibility of elements is changed via classes and css, but the css is not
+  // available in the tests, so we have to manually check if the classes are added.
+  // TODO: we should really change our code to make the following possible:
+  // it should be quite easy to use v-show for that...
+  // expect(title).not.toBeVisible()
+  const parent = headline.parentElement!.parentElement!.parentElement!
+  expect(parent).toHaveClass('open')
+  expect(parent).not.toHaveClass('closed')
+
+  await fireEvent.click(headline.parentElement!)
+  expect(parent).not.toHaveClass('open')
+  expect(parent).toHaveClass('closed')
+
+  await fireEvent.click(headline.parentElement!)
+  expect(parent).toHaveClass('open')
+  expect(parent).not.toHaveClass('closed')
+})
+
+test('FormCatalog collapse/open all', async () => {
+  renderSimpleCatalog()
+  await screen.findByText('title of string input')
+
+  const headline = await screen.findByText('ut embedded dictionary title')
+  const parent = headline.parentElement!.parentElement!.parentElement!
+  expect(parent).toHaveClass('open')
+  expect(parent).not.toHaveClass('closed')
+
+  await fireEvent.click(screen.getByText('Collapse all'))
+  expect(parent).toHaveClass('closed')
+  expect(parent).not.toHaveClass('open')
+
+  await fireEvent.click(screen.getByText('Open all'))
+  expect(parent).toHaveClass('open')
+  expect(parent).not.toHaveClass('closed')
+})
+
+test('FormCatalog default value', async () => {
+  function getDefinition(stringIdent: string) {
+    return {
+      spec: {
+        type: 'catalog',
+        title: 'catalog title',
+        help: 'catalog help',
+        validators: [],
+        topics: [
+          {
+            key: 'some_ut_key',
+            dictionary: getDictionaryFormspec(
+              {
+                title: 'ut embedded dictionary title'
+                // this title will be moved into the catalog and displayed there a stitle
+              },
+              [
+                {
+                  ident: stringIdent,
+                  parameter_form: getStringFormspec('title of string input'),
+                  default_value: 'ut_string_1 default value'
+                }
+              ]
+            )
+          }
+        ]
+      } as Catalog,
+      data: { some_ut_key: {} },
+      backendValidation: []
+    }
+  }
+  const { getCurrentData } = renderFormWithData(getDefinition('ut_string_1_ident_default'))
+
+  // wait until everything is rendered:
+  await screen.findByText('title of string input')
+
+  expect(getCurrentData()).toBe(
+    '{"some_ut_key":{"ut_string_1_ident_default":"ut_string_1 default value"}}'
+  )
+})
--- packages/cmk-frontend-vue/tests/setup-tests.ts
+++ packages/cmk-frontend-vue/tests/setup-tests.ts
@@ -1,6 +1,7 @@
 import { mixinUniqueId } from '@/plugins'
 import { config } from '@vue/test-utils'
 import failOnConsole from 'vitest-fail-on-console'
+import '@testing-library/jest-dom/vitest'
 
 failOnConsole({
   shouldFailOnAssert: true,

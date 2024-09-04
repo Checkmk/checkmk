7c65c06c3ee5fee74928f6fb7bc5e686a601405d
Benedikt Seidl <benedikt.seidl@checkmk.com>
2024-09-04 15:43:53 +0200

    add basic test for catalog
    Change-Id: If33681e685e74200c70a238974f61b7c108722d4

--- cmk/gui/form_specs/private/__init__.py
+++ cmk/gui/form_specs/private/__init__.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from .catalog import Catalog
+from .catalog import Catalog, Topic
 from .definitions import (
     LegacyValueSpec,
     SingleChoiceElementExtended,
@@ -15,6 +15,7 @@ from .list_extended import ListExtended
 from .optional_choice import OptionalChoice
 
 __all__ = [
+    "Topic",
     "Catalog",
     "DictionaryExtended",
     "LegacyValueSpec",
---
+++ tests/unit/cmk/gui/form_specs/vue/visitors/test_catalog.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+
+from cmk.utils.user import UserId
+
+from cmk.gui.form_specs.private import Catalog, Topic
+from cmk.gui.form_specs.vue.shared_type_defs import ValidationMessage
+from cmk.gui.form_specs.vue.visitors import DataOrigin, get_visitor
+from cmk.gui.form_specs.vue.visitors._type_defs import VisitorOptions
+
+from cmk.rulesets.v1.form_specs import DictElement, Dictionary, String
+from cmk.rulesets.v1.form_specs.validators import LengthInRange
+
+
+def test_catalog_validation_simple(
+    request_context: None,
+    patch_theme: None,
+    with_user: tuple[UserId, str],
+) -> None:
+
+    spec = Catalog(
+        topics=[
+            Topic(
+                key="some_key",
+                dictionary=Dictionary(
+                    elements={
+                        "key": DictElement(
+                            parameter_form=String(custom_validate=[LengthInRange(5, None)])
+                        )
+                    }
+                ),
+            )
+        ]
+    )
+    visitor = get_visitor(spec, VisitorOptions(data_origin=DataOrigin.DISK))
+
+    validation_messages = visitor.validate({"some_key": {"key": "string"}})
+    assert validation_messages == []
+
+    validation_messages = visitor.validate({"some_key": {"key": ""}})
+    assert validation_messages == [
+        ValidationMessage(
+            location=[
+                "some_key",
+                "key",
+            ],
+            message="The minimum allowed length is 5.",
+            invalid_value="",
+        )
+    ]

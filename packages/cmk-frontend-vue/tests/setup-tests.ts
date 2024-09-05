9eaac1f37b6563f2776d26059894177651bb7c13
Benedikt Seidl <benedikt.seidl@checkmk.com>
2024-09-05 08:11:59 +0200

    add vitest-fail-on-console
    this forbids any console output in tests and
    raises warnings to errors
    
    Change-Id: I79517f24dc0e4bb825b4c514d87671ef26ffcd1b

--- packages/cmk-frontend-vue/tests/setup-tests.ts
+++ packages/cmk-frontend-vue/tests/setup-tests.ts
@@ -1,4 +1,12 @@
 import { mixinUniqueId } from '@/plugins'
 import { config } from '@vue/test-utils'
+import failOnConsole from 'vitest-fail-on-console'
+
+failOnConsole({
+  shouldFailOnAssert: true,
+  shouldFailOnDebug: true,
+  shouldFailOnInfo: true,
+  shouldFailOnLog: true
+})
 
 config.global.plugins = [mixinUniqueId]

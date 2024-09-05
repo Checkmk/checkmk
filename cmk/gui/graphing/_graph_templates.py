d940b2086ea516a61012ebb88040608f5527abd6
Simon Jess <simon.jess@checkmk.com>
2024-09-05 15:20:36 +0200

    Reorder
    Change-Id: Ie20c978ded2c7206f3b3465082979fae33a0fb0b

--- cmk/gui/graphing/_graph_templates.py
+++ cmk/gui/graphing/_graph_templates.py
@@ -390,255 +390,6 @@ def get_evaluated_graph_templates(
             yield get_graph_template_from_name(metric_name)
 
 
-class TemplateGraphSpecification(GraphSpecification, frozen=True):
-    # Overwritten in cmk/gui/graphing/cee/__init__.py
-    TUNE_GRAPH_TEMPLATE: ClassVar[
-        Callable[[GraphTemplate, TemplateGraphSpecification], GraphTemplate | None]
-    ] = lambda graph_template, _spec: graph_template
-
-    site: SiteId | None
-    host_name: HostName
-    service_description: ServiceName
-    graph_index: int | None = None
-    graph_id: str | None = None
-    destination: str | None = None
-
-    @staticmethod
-    def graph_type_name() -> Literal["template"]:
-        return "template"
-
-    def recipes(self) -> list[GraphRecipe]:
-        row = get_graph_data_from_livestatus(self.site, self.host_name, self.service_description)
-        translated_metrics = translated_metrics_from_row(row)
-        return [
-            recipe
-            for index, graph_template in _matching_graph_templates(
-                graph_id=self.graph_id,
-                graph_index=self.graph_index,
-                translated_metrics=translated_metrics,
-            )
-            if (
-                recipe := self._build_recipe_from_template(
-                    graph_template=graph_template,
-                    row=row,
-                    translated_metrics=translated_metrics,
-                    index=index,
-                )
-            )
-        ]
-
-    def _build_recipe_from_template(
-        self,
-        *,
-        graph_template: GraphTemplate,
-        row: Row,
-        translated_metrics: Mapping[str, TranslatedMetric],
-        index: int,
-    ) -> GraphRecipe | None:
-        if not (
-            graph_template_tuned := TemplateGraphSpecification.TUNE_GRAPH_TEMPLATE(
-                graph_template,
-                self,
-            )
-        ):
-            return None
-
-        return create_graph_recipe_from_template(
-            row["site"],
-            row["host_name"],
-            row.get("service_description", "_HOST_"),
-            graph_template_tuned,
-            translated_metrics,
-            specification=TemplateGraphSpecification(
-                site=self.site,
-                host_name=self.host_name,
-                service_description=self.service_description,
-                destination=self.destination,
-                # Performance graph dashlets already use graph_id, but for example in reports, we still
-                # use graph_index. We should switch to graph_id everywhere (CMK-7308). Once this is
-                # done, we can remove the line below.
-                graph_index=index,
-                graph_id=graph_template_tuned.id,
-            ),
-        )
-
-
-# Performance graph dashlets already use graph_id, but for example in reports, we still use
-# graph_index. Therefore, this function needs to support both. We should switch to graph_id
-# everywhere (CMK-7308) and remove the support for graph_index. However, note that we cannot easily
-# build a corresponding transform, so even after switching to graph_id everywhere, we will need to
-# keep this functionality here for some time to support already created dashlets, reports etc.
-def _matching_graph_templates(
-    *,
-    graph_id: str | None,
-    graph_index: int | None,
-    translated_metrics: Mapping[str, TranslatedMetric],
-) -> Iterable[tuple[int, GraphTemplate]]:
-    # Single metrics
-    if (
-        isinstance(graph_id, str)
-        and graph_id.startswith("METRIC_")
-        and graph_id[7:] in translated_metrics
-    ):
-        yield (0, get_graph_template_from_name(graph_id))
-        return
-
-    yield from (
-        (index, graph_template)
-        for index, graph_template in enumerate(get_evaluated_graph_templates(translated_metrics))
-        if (graph_index is None or index == graph_index)
-        and (graph_id is None or graph_template.id == graph_id)
-    )
-
-
-def _replace_expressions(text: str, translated_metrics: Mapping[str, TranslatedMetric]) -> str:
-    """Replace expressions in strings like CPU Load - %(load1:max@count) CPU Cores"""
-    # Note: The 'CPU load' graph is the only example with such a replacement. We do not want to
-    # offer such replacements in a generic way.
-    reg = regex.regex(r"%\([^)]*\)")
-    if m := reg.search(text):
-        if (
-            result := parse_legacy_simple_expression(m.group()[2:-1], translated_metrics).evaluate(
-                translated_metrics
-            )
-        ).is_error():
-            return text.split("-")[0].strip()
-        return reg.sub(
-            get_render_function(result.ok.unit_spec)(result.ok.value).strip(),
-            text,
-        )
-
-    return text
-
-
-def _horizontal_rules_from_thresholds(
-    metric_expressions: Iterable[MetricExpression],
-    translated_metrics: Mapping[str, TranslatedMetric],
-) -> Sequence[HorizontalRule]:
-    horizontal_rules = []
-    for metric_expression in metric_expressions:
-        if (result := metric_expression.evaluate(translated_metrics)).is_error():
-            # Scalar value like min and max are always optional. This makes configuration
-            # of graphs easier.
-            if result.error.metric_name:
-                continue
-            return []
-
-        horizontal_rules.append(
-            HorizontalRule(
-                value=result.ok.value,
-                rendered_value=get_render_function(result.ok.unit_spec)(result.ok.value),
-                color=result.ok.color,
-                title=metric_expression.title,
-            )
-        )
-    return horizontal_rules
-
-
-def create_graph_recipe_from_template(
-    site_id: SiteId,
-    host_name: HostName,
-    service_name: ServiceName,
-    graph_template: GraphTemplate,
-    translated_metrics: Mapping[str, TranslatedMetric],
-    specification: GraphSpecification,
-) -> GraphRecipe:
-    # TODO Cleanup evaluations:
-    # - Either:
-    #   - Remove evaluation in TemplateGraphSpecification::recipes::_matching_graph_templates)
-    #   - Add evaluation in SingleTimeseriesGraphSpecification::recipes
-    #   - Remove evaluation here
-    # - Or:
-    #   - Only evaluate here
-    metrics = [
-        GraphMetric(
-            title=evaluated.title,
-            line_type=evaluated.line_type,
-            operation=metric_expression_to_graph_recipe_expression(
-                site_id,
-                host_name,
-                service_name,
-                metric_expression,
-                translated_metrics,
-                graph_template.consolidation_function or "max",
-            ),
-            unit=(
-                evaluated.unit_spec
-                if isinstance(evaluated.unit_spec, ConvertibleUnitSpecification)
-                else evaluated.unit_spec.id
-            ),
-            color=evaluated.color,
-        )
-        for metric_expression, evaluated in applicable_metrics(graph_template, translated_metrics)
-    ]
-    units = {m.unit for m in metrics}
-
-    # We cannot validate the hypothetical case of a mixture of metrics from the legacy and the new API
-    if all(isinstance(m.unit, str) for m in metrics) or all(
-        isinstance(m.unit, ConvertibleUnitSpecification) for m in metrics
-    ):
-        if len(units) > 1:
-            raise MKGeneralException(
-                _("Cannot create graph with metrics of different units '%s'")
-                % ", ".join(repr(unit) for unit in units)
-            )
-
-    title = _replace_expressions(graph_template.title or "", translated_metrics)
-    if not title:
-        title = next((m.title for m in metrics), "")
-
-    painter_options = PainterOptions.get_instance()
-    if painter_options.get("show_internal_graph_and_metric_ids"):
-        title = title + f" (Graph ID: {graph_template.id})"
-
-    return GraphRecipe(
-        title=title,
-        metrics=metrics,
-        unit_spec=(
-            LegacyUnitSpecification(id=unit) if isinstance(unit := units.pop(), str) else unit
-        ),
-        explicit_vertical_range=evaluate_graph_template_range(
-            graph_template.range,
-            translated_metrics,
-        ),
-        horizontal_rules=_horizontal_rules_from_thresholds(
-            graph_template.scalars, translated_metrics
-        ),  # e.g. lines for WARN and CRIT
-        omit_zero_metrics=graph_template.omit_zero_metrics,
-        consolidation_function=graph_template.consolidation_function or "max",
-        specification=specification,
-    )
-
-
-def evaluate_graph_template_range(
-    graph_template_range: FixedGraphTemplateRange | MinimalGraphTemplateRange | None,
-    translated_metrics: Mapping[str, TranslatedMetric],
-) -> FixedVerticalRange | MinimalVerticalRange | None:
-    match graph_template_range:
-        case FixedGraphTemplateRange(min=min_, max=max_):
-            return FixedVerticalRange(
-                min=_evaluate_graph_template_range_boundary(min_, translated_metrics),
-                max=_evaluate_graph_template_range_boundary(max_, translated_metrics),
-            )
-        case MinimalGraphTemplateRange(min=min_, max=max_):
-            return MinimalVerticalRange(
-                min=_evaluate_graph_template_range_boundary(min_, translated_metrics),
-                max=_evaluate_graph_template_range_boundary(max_, translated_metrics),
-            )
-        case None:
-            return None
-        case _:
-            assert_never(graph_template_range)
-
-
-def _evaluate_graph_template_range_boundary(
-    base_metric_expression: BaseMetricExpression, translated_metrics: Mapping[str, TranslatedMetric]
-) -> float | None:
-    if (result := base_metric_expression.evaluate(translated_metrics)).is_error():
-        return None
-    return result.ok.value
-
-
 def _to_metric_operation(
     site_id: SiteId,
     host_name: HostName,
@@ -821,3 +572,252 @@ def metric_expression_to_graph_recipe_expression(
         translated_metrics,
         consolidation_function,
     )
+
+
+def _replace_expressions(text: str, translated_metrics: Mapping[str, TranslatedMetric]) -> str:
+    """Replace expressions in strings like CPU Load - %(load1:max@count) CPU Cores"""
+    # Note: The 'CPU load' graph is the only example with such a replacement. We do not want to
+    # offer such replacements in a generic way.
+    reg = regex.regex(r"%\([^)]*\)")
+    if m := reg.search(text):
+        if (
+            result := parse_legacy_simple_expression(m.group()[2:-1], translated_metrics).evaluate(
+                translated_metrics
+            )
+        ).is_error():
+            return text.split("-")[0].strip()
+        return reg.sub(
+            get_render_function(result.ok.unit_spec)(result.ok.value).strip(),
+            text,
+        )
+
+    return text
+
+
+def _evaluate_graph_template_range_boundary(
+    base_metric_expression: BaseMetricExpression, translated_metrics: Mapping[str, TranslatedMetric]
+) -> float | None:
+    if (result := base_metric_expression.evaluate(translated_metrics)).is_error():
+        return None
+    return result.ok.value
+
+
+def evaluate_graph_template_range(
+    graph_template_range: FixedGraphTemplateRange | MinimalGraphTemplateRange | None,
+    translated_metrics: Mapping[str, TranslatedMetric],
+) -> FixedVerticalRange | MinimalVerticalRange | None:
+    match graph_template_range:
+        case FixedGraphTemplateRange(min=min_, max=max_):
+            return FixedVerticalRange(
+                min=_evaluate_graph_template_range_boundary(min_, translated_metrics),
+                max=_evaluate_graph_template_range_boundary(max_, translated_metrics),
+            )
+        case MinimalGraphTemplateRange(min=min_, max=max_):
+            return MinimalVerticalRange(
+                min=_evaluate_graph_template_range_boundary(min_, translated_metrics),
+                max=_evaluate_graph_template_range_boundary(max_, translated_metrics),
+            )
+        case None:
+            return None
+        case _:
+            assert_never(graph_template_range)
+
+
+def _horizontal_rules_from_thresholds(
+    metric_expressions: Iterable[MetricExpression],
+    translated_metrics: Mapping[str, TranslatedMetric],
+) -> Sequence[HorizontalRule]:
+    horizontal_rules = []
+    for metric_expression in metric_expressions:
+        if (result := metric_expression.evaluate(translated_metrics)).is_error():
+            # Scalar value like min and max are always optional. This makes configuration
+            # of graphs easier.
+            if result.error.metric_name:
+                continue
+            return []
+
+        horizontal_rules.append(
+            HorizontalRule(
+                value=result.ok.value,
+                rendered_value=get_render_function(result.ok.unit_spec)(result.ok.value),
+                color=result.ok.color,
+                title=metric_expression.title,
+            )
+        )
+    return horizontal_rules
+
+
+def create_graph_recipe_from_template(
+    site_id: SiteId,
+    host_name: HostName,
+    service_name: ServiceName,
+    graph_template: GraphTemplate,
+    translated_metrics: Mapping[str, TranslatedMetric],
+    specification: GraphSpecification,
+) -> GraphRecipe:
+    # TODO Cleanup evaluations:
+    # - Either:
+    #   - Remove evaluation in TemplateGraphSpecification::recipes::_matching_graph_templates)
+    #   - Add evaluation in SingleTimeseriesGraphSpecification::recipes
+    #   - Remove evaluation here
+    # - Or:
+    #   - Only evaluate here
+    metrics = [
+        GraphMetric(
+            title=evaluated.title,
+            line_type=evaluated.line_type,
+            operation=metric_expression_to_graph_recipe_expression(
+                site_id,
+                host_name,
+                service_name,
+                metric_expression,
+                translated_metrics,
+                graph_template.consolidation_function or "max",
+            ),
+            unit=(
+                evaluated.unit_spec
+                if isinstance(evaluated.unit_spec, ConvertibleUnitSpecification)
+                else evaluated.unit_spec.id
+            ),
+            color=evaluated.color,
+        )
+        for metric_expression, evaluated in applicable_metrics(graph_template, translated_metrics)
+    ]
+    units = {m.unit for m in metrics}
+
+    # We cannot validate the hypothetical case of a mixture of metrics from the legacy and the new API
+    if all(isinstance(m.unit, str) for m in metrics) or all(
+        isinstance(m.unit, ConvertibleUnitSpecification) for m in metrics
+    ):
+        if len(units) > 1:
+            raise MKGeneralException(
+                _("Cannot create graph with metrics of different units '%s'")
+                % ", ".join(repr(unit) for unit in units)
+            )
+
+    title = _replace_expressions(graph_template.title or "", translated_metrics)
+    if not title:
+        title = next((m.title for m in metrics), "")
+
+    painter_options = PainterOptions.get_instance()
+    if painter_options.get("show_internal_graph_and_metric_ids"):
+        title = title + f" (Graph ID: {graph_template.id})"
+
+    return GraphRecipe(
+        title=title,
+        metrics=metrics,
+        unit_spec=(
+            LegacyUnitSpecification(id=unit) if isinstance(unit := units.pop(), str) else unit
+        ),
+        explicit_vertical_range=evaluate_graph_template_range(
+            graph_template.range,
+            translated_metrics,
+        ),
+        horizontal_rules=_horizontal_rules_from_thresholds(
+            graph_template.scalars, translated_metrics
+        ),  # e.g. lines for WARN and CRIT
+        omit_zero_metrics=graph_template.omit_zero_metrics,
+        consolidation_function=graph_template.consolidation_function or "max",
+        specification=specification,
+    )
+
+
+# Performance graph dashlets already use graph_id, but for example in reports, we still use
+# graph_index. Therefore, this function needs to support both. We should switch to graph_id
+# everywhere (CMK-7308) and remove the support for graph_index. However, note that we cannot easily
+# build a corresponding transform, so even after switching to graph_id everywhere, we will need to
+# keep this functionality here for some time to support already created dashlets, reports etc.
+def _matching_graph_templates(
+    *,
+    graph_id: str | None,
+    graph_index: int | None,
+    translated_metrics: Mapping[str, TranslatedMetric],
+) -> Iterable[tuple[int, GraphTemplate]]:
+    # Single metrics
+    if (
+        isinstance(graph_id, str)
+        and graph_id.startswith("METRIC_")
+        and graph_id[7:] in translated_metrics
+    ):
+        yield (0, get_graph_template_from_name(graph_id))
+        return
+
+    yield from (
+        (index, graph_template)
+        for index, graph_template in enumerate(get_evaluated_graph_templates(translated_metrics))
+        if (graph_index is None or index == graph_index)
+        and (graph_id is None or graph_template.id == graph_id)
+    )
+
+
+class TemplateGraphSpecification(GraphSpecification, frozen=True):
+    # Overwritten in cmk/gui/graphing/cee/__init__.py
+    TUNE_GRAPH_TEMPLATE: ClassVar[
+        Callable[[GraphTemplate, TemplateGraphSpecification], GraphTemplate | None]
+    ] = lambda graph_template, _spec: graph_template
+
+    site: SiteId | None
+    host_name: HostName
+    service_description: ServiceName
+    graph_index: int | None = None
+    graph_id: str | None = None
+    destination: str | None = None
+
+    @staticmethod
+    def graph_type_name() -> Literal["template"]:
+        return "template"
+
+    def _build_recipe_from_template(
+        self,
+        *,
+        graph_template: GraphTemplate,
+        row: Row,
+        translated_metrics: Mapping[str, TranslatedMetric],
+        index: int,
+    ) -> GraphRecipe | None:
+        if not (
+            graph_template_tuned := TemplateGraphSpecification.TUNE_GRAPH_TEMPLATE(
+                graph_template,
+                self,
+            )
+        ):
+            return None
+
+        return create_graph_recipe_from_template(
+            row["site"],
+            row["host_name"],
+            row.get("service_description", "_HOST_"),
+            graph_template_tuned,
+            translated_metrics,
+            specification=TemplateGraphSpecification(
+                site=self.site,
+                host_name=self.host_name,
+                service_description=self.service_description,
+                destination=self.destination,
+                # Performance graph dashlets already use graph_id, but for example in reports, we still
+                # use graph_index. We should switch to graph_id everywhere (CMK-7308). Once this is
+                # done, we can remove the line below.
+                graph_index=index,
+                graph_id=graph_template_tuned.id,
+            ),
+        )
+
+    def recipes(self) -> list[GraphRecipe]:
+        row = get_graph_data_from_livestatus(self.site, self.host_name, self.service_description)
+        translated_metrics = translated_metrics_from_row(row)
+        return [
+            recipe
+            for index, graph_template in _matching_graph_templates(
+                graph_id=self.graph_id,
+                graph_index=self.graph_index,
+                translated_metrics=translated_metrics,
+            )
+            if (
+                recipe := self._build_recipe_from_template(
+                    graph_template=graph_template,
+                    row=row,
+                    translated_metrics=translated_metrics,
+                    index=index,
+                )
+            )
+        ]

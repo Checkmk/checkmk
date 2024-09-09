73b9986bb319af6bd05ae405f75b06d76da16aa0
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-09-09 11:52:33 +0200

    Outsource bazel logs parser to extra file
    ... and add the parser to the build-cmk-distro-package script
    
    Change-Id: I01a48bcec58de621bc1a001cbc90454bc0688eac

--- buildscripts/scripts/build-cmk-packages.groovy
+++ buildscripts/scripts/build-cmk-packages.groovy
@@ -38,10 +38,11 @@ def main() {
 
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
     def artifacts_helper = load("${checkout_dir}/buildscripts/scripts/utils/upload_artifacts.groovy");
+    def bazel_logs = load("${checkout_dir}/buildscripts/scripts/utils/bazel_logs.groovy");
 
     shout("configure");
 
-    def bazel_log_prefix = "bazel_log_"
+    def bazel_log_prefix = "bazel_log_";
 
     def (jenkins_base_folder, use_case, omd_env_vars, upload_path_suffix) = (
         env.JOB_BASE_NAME == "testbuild" ? [
@@ -318,7 +319,7 @@ def main() {
                                 [ ! -f bazel-memory.profile ] || mv bazel-memory.profile bazel-memory-${distro}.profile
                                 """)
 
-                                try_parse_bazel_execution_log(distro, distro_dir, bazel_log_prefix)
+                                bazel_logs.try_parse_bazel_execution_log(distro, distro_dir, bazel_log_prefix)
                             }
                         }
                     }
@@ -362,7 +363,7 @@ def main() {
     parallel package_builds;
 
     stage("Plot cache hits") {
-        try_plot_cache_hits(bazel_log_prefix, distros);
+        bazel_logs.try_plot_cache_hits(bazel_log_prefix, distros);
     }
 
     conditional_stage('Upload', !jenkins_base_folder.startsWith("Testing")) {
@@ -388,63 +389,6 @@ def main() {
                 artifacts_helper.deploy_to_website(cmk_version_rc_aware);
             }
         }
-    }
-}
-
-def try_parse_bazel_execution_log(distro, distro_dir, bazel_log_prefix) {
-    try {
-        dir("${distro_dir}") {
-            def summary_file="${distro_dir}/${bazel_log_prefix}execution_summary_${distro}.json";
-            def cache_hits_file="${distro_dir}/${bazel_log_prefix}cache_hits_${distro}.csv";
-            sh("""python3 \
-            buildscripts/scripts/bazel_execution_log_parser.py \
-            --execution_logs_root "${distro_dir}" \
-            --bazel_log_file_pattern "bazel_execution_log*" \
-            --summary_file "${summary_file}" \
-            --cachehit_csv "${cache_hits_file}" \
-            --distro "${distro}"
-        """);
-            stash(name: "${bazel_log_prefix}${distro}", includes: "${bazel_log_prefix}*")
-
-            // remove large execution log summary file to save some space, approx 1.6GB per workspace
-            sh("rm -rf ${distro_dir}/${bazel_log_prefix}*.json");
-        }
-    } catch (Exception e) {
-        print("Failed to parse bazel execution logs: ${e}");
-    }
-}
-
-def try_plot_cache_hits(bazel_log_prefix, distros) {
-    try {
-        distros.each { distro ->
-            try {
-                print("Unstashing for distro ${distro}...")
-                unstash(name: "${bazel_log_prefix}${distro}")
-            }
-            catch (Exception e) {
-                print("No stash for ${distro}")
-            }
-        }
-
-        plot csvFileName: 'bazel_cache_hits.csv',
-            csvSeries:
-                distros.collect {[file: "${bazel_log_prefix}cache_hits_${it}.csv"]},
-            description: 'Bazel Remote Cache Analysis',
-            group: 'Bazel Cache',
-            numBuilds: '30',
-            propertiesSeries: [[file: '', label: '']],
-            style: 'line',
-            title: 'Cache hits',
-            yaxis: 'Cache hits in percent',
-            yaxisMaximum: '100',
-            yaxisMinimum: '0'
-
-        archiveArtifacts(
-           artifacts: "${bazel_log_prefix}*",
-        )
-    }
-    catch (Exception e) {
-        print("Failed to plot cache hits: ${e}");
     }
 }
 

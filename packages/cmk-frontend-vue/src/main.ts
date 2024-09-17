50a02ccbed2218588ea3dfd902e17d9e57de45a4
Benedikt Seidl <benedikt.seidl@checkmk.com>
2024-09-17 08:13:16 +0200

    useId: add support for multiple vue apps
    CMK-18653
    
    Change-Id: Icf7f61a4ce094c9e6c8c92ab9b94f4a0ccee921f

--- packages/cmk-frontend-vue/src/main.ts
+++ packages/cmk-frontend-vue/src/main.ts
@@ -23,69 +23,71 @@ import NotificationOverview from './notification/NotificationOverviewApp.vue'
 import { FormApp } from '@/form'
 
 function setupVue() {
-  document.querySelectorAll<HTMLFormElement>('div[data-cmk_vue_app_name]').forEach((div) => {
-    const dataset = div.dataset
-    if (dataset === undefined) {
-      return
-    }
+  document
+    .querySelectorAll<HTMLFormElement>('div[data-cmk_vue_app_name]')
+    .forEach((div, divIndex) => {
+      const dataset = div.dataset
+      if (dataset === undefined) {
+        return
+      }
 
-    const appName = dataset['cmk_vue_app_name']
-    const appDataRaw = dataset['cmk_vue_app_data']
-    if (appName === undefined || appDataRaw === undefined) {
-      return
-    }
-    const appData = JSON.parse(appDataRaw)
+      const appName = dataset['cmk_vue_app_name']
+      const appDataRaw = dataset['cmk_vue_app_data']
+      if (appName === undefined || appDataRaw === undefined) {
+        return
+      }
+      const appData = JSON.parse(appDataRaw)
 
-    let app
+      let app
 
-    switch (appName) {
-      case 'form_spec': {
-        app = createApp(FormApp, {
-          id: appData.id,
-          spec: appData.spec,
-          // eslint has a false positive: assuming `data` is part of a vue component
-          // eslint-disable-next-line vue/no-deprecated-data-object-declaration, vue/no-shared-component-data
-          data: appData.data,
-          backendValidation: appData.validation,
-          renderMode: appData.render_mode
-        })
-        break
+      switch (appName) {
+        case 'form_spec': {
+          app = createApp(FormApp, {
+            id: appData.id,
+            spec: appData.spec,
+            // eslint has a false positive: assuming `data` is part of a vue component
+            // eslint-disable-next-line vue/no-deprecated-data-object-declaration, vue/no-shared-component-data
+            data: appData.data,
+            backendValidation: appData.validation,
+            renderMode: appData.render_mode
+          })
+          break
+        }
+        case 'd3_table': {
+          console.log('vue create table')
+          app = createApp(D3Table, {
+            table_spec: appData.component
+          })
+          console.log('vue fully mounted table')
+          break
+        }
+        case 'vue_table': {
+          console.log('vue create table')
+          app = createApp(Table, {
+            table_spec: appData.component
+          })
+          console.log('vue fully mounted table')
+          break
+        }
+        case 'quick_setup': {
+          app = createApp(QuickSetup, { quick_setup_id: appData.quick_setup_id })
+          break
+        }
+        case 'notification_overview': {
+          app = createApp(NotificationOverview, {
+            fallback_warning: appData.fallback_warning,
+            notification_stats: appData.notification_stats,
+            core_stats: appData.core_stats,
+            rule_sections: appData.rule_sections
+          })
+          break
+        }
+        default:
+          throw `can not load vue app "${appName}"`
       }
-      case 'd3_table': {
-        console.log('vue create table')
-        app = createApp(D3Table, {
-          table_spec: appData.component
-        })
-        console.log('vue fully mounted table')
-        break
-      }
-      case 'vue_table': {
-        console.log('vue create table')
-        app = createApp(Table, {
-          table_spec: appData.component
-        })
-        console.log('vue fully mounted table')
-        break
-      }
-      case 'quick_setup': {
-        app = createApp(QuickSetup, { quick_setup_id: appData.quick_setup_id })
-        break
-      }
-      case 'notification_overview': {
-        app = createApp(NotificationOverview, {
-          fallback_warning: appData.fallback_warning,
-          notification_stats: appData.notification_stats,
-          core_stats: appData.core_stats,
-          rule_sections: appData.rule_sections
-        })
-        break
-      }
-      default:
-        throw `can not load vue app "${appName}"`
-    }
-
-    app.mount(div)
-  })
+      app.config.idPrefix = `app${divIndex}` // useId for multiple vue apps
+      app.mount(div)
+    })
 }
 
 addEventListener('load', () => {

44917eee437eda270d473c66ee931773e413b7a2
Jörg Herbel <joerg.herbel@checkmk.com>
2024-09-20 12:04:03 +0200

    RMK: Scaffolding for managed robots page
    The new page is reachable via
    http://localhost/{site_name}/check_mk/robotmk_managed_robots.py
    but currently not linked anywhere in the UI.
    
    CMK-19239
    
    Change-Id: If3566aea8736cca6d92b5a772919cd5a7da8eda8

--- tests/unit/cmk/gui/test_pages.py
+++ tests/unit/cmk/gui/test_pages.py
@@ -228,6 +228,7 @@ def test_registered_pages() -> None:
             "robotmk_suite_log",
             "robotmk_suite_report",
             "download_robotmk_suite_report",
+            "robotmk_managed_robots",
         ]
 
     if cmk_version.edition(paths.omd_root) is cmk_version.Edition.CSE:

f785e7ea8c6d011e695d11da9c92643562dbec5d
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2024-09-23 09:23:09 +0200

    Update pydantic
    Change-Id: I82ccf743a46dfcbb9dfa6d61a3ac1d8d8f6def6d

--- cmk/special_agents/utils_kubernetes/query.py
+++ cmk/special_agents/utils_kubernetes/query.py
@@ -135,8 +135,8 @@ _AllConfigs = CollectorSessionConfig | PrometheusSessionConfig | NoUsageConfig
 
 
 def parse_session_config(arguments: argparse.Namespace) -> _AllConfigs:
-    adapter = TypeAdapter(_AllConfigs)
-    return adapter.validate_python(arguments.__dict__)  # type: ignore[arg-type, return-value]
+    adapter: TypeAdapter[_AllConfigs] = TypeAdapter(_AllConfigs)
+    return adapter.validate_python(arguments.__dict__)
 
 
 def parse_api_session_config(arguments: argparse.Namespace) -> APISessionConfig:

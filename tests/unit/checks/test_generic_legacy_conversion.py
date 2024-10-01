69f751fd3de542a783067051db7eee72e4d39b8f
Mehran Shahidi <mehran.shahidi@checkmk.com>
2024-10-01 08:11:13 +0200

    apc_ats_output: Migrate the plugin (WATO ruleset + check)
    Migrate both apc_ats_output ruleset and api to version 2.
    CMK-17700
    
    Change-Id: I1fe590e83b8b495543f2a7bd826a0b3dbe55958c

--- tests/unit/checks/test_generic_legacy_conversion.py
+++ tests/unit/checks/test_generic_legacy_conversion.py
@@ -136,7 +136,6 @@ def test_no_new_or_vanished_legacy_checks(fix_plugin_legacy: FixPluginLegacy) ->
         "allnet_ip_sensoric.temp",
         "allnet_ip_sensoric.humidity",
         "allnet_ip_sensoric.pressure",
-        "apc_ats_output",
         "apc_ats_status",
         "apc_humidity",
         "apc_inrow_airflow",

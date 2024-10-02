b650f0c767af5461c509877f7b2c5896372481aa
Lukas Lengler <lukas.lengler@checkmk.com>
2024-10-02 14:09:29 +0200

    notifications: add first stage components
    CMK-19148
    
    Change-Id: Ic4e7fde0010713c38ec4539719f488ffa9ab863e

--- cmk/gui/wato/pages/notifications/quick_setup.py
+++ cmk/gui/wato/pages/notifications/quick_setup.py
@@ -4,23 +4,205 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from collections.abc import Sequence
+from typing import Literal
 
+from cmk.gui.form_specs.converter.tuple import Tuple
+from cmk.gui.form_specs.private import (
+    DictionaryExtended,
+    ListExtended,
+    SingleChoiceElementExtended,
+    SingleChoiceExtended,
+)
+from cmk.gui.form_specs.vue.shared_type_defs import DictionaryLayout
 from cmk.gui.i18n import _
 from cmk.gui.quick_setup.v0_unstable._registry import QuickSetupRegistry
 from cmk.gui.quick_setup.v0_unstable.setups import QuickSetup, QuickSetupSaveAction, QuickSetupStage
-from cmk.gui.quick_setup.v0_unstable.type_defs import ParsedFormData, QuickSetupId
-from cmk.gui.quick_setup.v0_unstable.widgets import Widget
+from cmk.gui.quick_setup.v0_unstable.type_defs import (
+    GeneralStageErrors,
+    ParsedFormData,
+    QuickSetupId,
+    StageIndex,
+)
+from cmk.gui.quick_setup.v0_unstable.widgets import FormSpecId, FormSpecWrapper, Widget
 from cmk.gui.watolib.mode import mode_url
+
+from cmk.rulesets.v1 import Label, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    FixedValue,
+    HostState,
+    ServiceState,
+    SingleChoice,
+    SingleChoiceElement,
+)
+
+
+def _host_states() -> Sequence[tuple[int, Title]]:
+    return [
+        (-1, Title("Any")),
+        (HostState.UP, Title("UP")),
+        (HostState.DOWN, Title("DOWN")),
+        (HostState.UNREACH, Title("UNREACHABLE")),
+    ]
+
+
+def _service_states() -> Sequence[tuple[int, Title]]:
+    return [
+        (-1, Title("Any")),
+        (ServiceState.OK, Title("OK")),
+        (ServiceState.WARN, Title("WARN")),
+        (ServiceState.CRIT, Title("CRIT")),
+        (ServiceState.UNKNOWN, Title("UNKNOWN")),
+    ]
+
+
+def _get_states(what: Literal["host", "service"]) -> Sequence[tuple[int, Title]]:
+    match what:
+        case "host":
+            return _host_states()
+        case "service":
+            return _service_states()
+        case _:
+            raise ValueError(f"Invalid value for 'what': {what}")
+
+
+def _event_choices(what: Literal["host", "service"]) -> Sequence[CascadingSingleChoiceElement]:
+    return [
+        CascadingSingleChoiceElement(
+            name="status_change",
+            title=Title("Status change"),
+            parameter_form=Tuple(
+                layout="horizontal",
+                elements=[
+                    SingleChoiceExtended(
+                        label=Label("From"),
+                        prefill=DefaultValue(-1),
+                        type=int,
+                        elements=[
+                            SingleChoiceElementExtended(name=state, title=title)
+                            for state, title in _get_states(what)
+                        ],
+                    ),
+                    SingleChoiceExtended(
+                        label=Label("to"),
+                        prefill=DefaultValue(1) if what == "host" else DefaultValue(2),
+                        type=int,
+                        elements=[
+                            SingleChoiceElementExtended(name=state, title=title)
+                            for state, title in _get_states(what)
+                        ],
+                    ),
+                ],
+            ),
+        ),
+        CascadingSingleChoiceElement(
+            name="downtime",
+            title=Title("Start or end of downtime"),
+            parameter_form=FixedValue(value=None),
+        ),
+        CascadingSingleChoiceElement(
+            name="acknowledgement",
+            title=Title("Acknowledgement of problem"),
+            parameter_form=FixedValue(value=None),
+        ),
+        CascadingSingleChoiceElement(
+            name="flapping_state",
+            title=Title("Start or end of flapping state"),
+            parameter_form=FixedValue(value=None),
+        ),
+        CascadingSingleChoiceElement(
+            name="alert_handler",
+            title=Title("Alert handler execution"),
+            parameter_form=SingleChoice(
+                prefill=DefaultValue("success"),
+                elements=[
+                    SingleChoiceElement(name="success", title=Title("Success")),
+                    SingleChoiceElement(name="failure", title=Title("Failure")),
+                ],
+            ),
+        ),
+    ]
+
+
+def _validate_at_least_one_event(
+    _quick_setup_id: QuickSetupId,
+    _stage_index: StageIndex,
+    form_data: ParsedFormData,
+) -> GeneralStageErrors:
+    if not form_data[FormSpecId("triggering_events")].get("host_events") and not form_data[
+        FormSpecId("triggering_events")
+    ].get("service_events"):
+        return [
+            "No triggering events selected. "
+            "Please select at least one event to trigger the notification."
+        ]
+    return []
 
 
 def triggering_events() -> QuickSetupStage:
     def _components() -> Sequence[Widget]:
-        return []
+        return [
+            FormSpecWrapper(
+                id=FormSpecId("triggering_events"),
+                form_spec=DictionaryExtended(
+                    layout=DictionaryLayout.two_columns,
+                    prefill=DefaultValue(
+                        {
+                            "host_events": [
+                                ("status_change", (-1, HostState.DOWN)),
+                                ("status_change", (-1, HostState.UP)),
+                            ],
+                            "service_events": [
+                                ("status_change", (-1, ServiceState.CRIT)),
+                                ("status_change", (-1, ServiceState.UNKNOWN)),
+                                ("status_change", (-1, ServiceState.WARN)),
+                                ("status_change", (-1, ServiceState.OK)),
+                            ],
+                        }
+                    ),
+                    elements={
+                        "host_events": DictElement(
+                            parameter_form=ListExtended(
+                                title=Title("Host events"),
+                                prefill=DefaultValue([]),
+                                element_template=CascadingSingleChoice(
+                                    # TODO: add horizontal layout (CMK-18894)
+                                    elements=_event_choices("host"),
+                                ),
+                                add_element_label=Label("Add event"),
+                                editable_order=False,
+                            )
+                        ),
+                        "service_events": DictElement(
+                            parameter_form=ListExtended(
+                                title=Title("Service events"),
+                                prefill=DefaultValue([]),
+                                element_template=CascadingSingleChoice(
+                                    # TODO: add horizontal layout (CMK-18894)
+                                    elements=_event_choices("service"),
+                                ),
+                                add_element_label=Label("Add event"),
+                                editable_order=False,
+                            )
+                        ),
+                        "ec_alerts": DictElement(
+                            parameter_form=FixedValue(
+                                title=Title("Event console alerts"),
+                                value=None,
+                            ),
+                        ),
+                    },
+                ),
+            )
+        ]
 
     return QuickSetupStage(
         title=_("Triggering events"),
         configure_components=_components,
-        custom_validators=[],
+        custom_validators=[_validate_at_least_one_event],
         recap=[],
         button_label=_("Next step: Specify host/services"),
     )

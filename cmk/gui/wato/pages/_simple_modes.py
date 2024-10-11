5ed313506cdf096f0da7b6fc7bd16a817d2fe696
Andreas Boesl <andreas.boesl@checkmk.com>
2024-10-11 11:17:58 +0200

    added simple styling for locked_by attribute
    Change-Id: I87d1777cbfcfbd5cbcd474f018cc23c626f68c05

--- cmk/gui/wato/pages/_simple_modes.py
+++ cmk/gui/wato/pages/_simple_modes.py
@@ -555,11 +555,12 @@ class SimpleEditMode(_SimpleWatoModeBase[_T], abc.ABC):
             )
 
         if "locked_by" in self._entry:
+            shown_info = ", ".join(f"{x}: {y}" for x, y in self._entry["locked_by"].items())
             elements["locked_by"] = form_specs.DictElement(
                 required=True,
                 parameter_form=form_specs.FixedValue(
                     title=Title("Locked by"),
-                    value=repr(self._entry["locked_by"]),
+                    value=shown_info,
                 ),
             )
 

94254bddf493b74e0c66208aa84661699a9e7632
Sven Panne <sven.panne@checkmk.com>
2024-10-14 14:00:52 +0200

    Tiny ruff fixes
    Change-Id: I70b1c79a155e98b08dc8bd27892939aed70b9b98

--- tests/unit/cmk/gui/wato/_notification_parameter/test_ilert.py
+++ tests/unit/cmk/gui/wato/_notification_parameter/test_ilert.py
@@ -9,7 +9,7 @@ from typing import Literal
 import pytest
 from pytest import MonkeyPatch
 
-import cmk.utils.password_store as password_store
+from cmk.utils import password_store
 
 from cmk.gui.wato._notification_parameter._ilert import _migrate_to_password
 

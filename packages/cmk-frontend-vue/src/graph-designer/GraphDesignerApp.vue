b62ad250ba6d0a7d7fd2abf4dfa58962e3510791
Simon Jess <simon.jess@checkmk.com>
2024-10-16 11:40:02 +0200

    Graph designer: Add graph options content
    CMK-19687
    
    Change-Id: If05a50cb77da99eb0ff3d7ad2a41f28bf2ceb2e6

--- packages/cmk-frontend-vue/src/graph-designer/GraphDesignerApp.vue
+++ packages/cmk-frontend-vue/src/graph-designer/GraphDesignerApp.vue
@@ -5,15 +5,253 @@ conditions defined in the file COPYING, which is part of this source code packag
 -->
 
 <script setup lang="ts">
+import FormEdit from '@/form/components/FormEdit.vue'
 import TopicsRenderer from '@/graph-designer/components/TopicsRenderer.vue'
+import { ref } from 'vue'
 import { type I18N } from '@/graph-designer/type_defs'
 import { type Topic } from '@/graph-designer/components/type_defs'
+import { type ValidationMessages } from '@/form'
 
 const props = defineProps<{
   i18n: I18N
 }>()
 
 // Specs
+
+const dataUnit = ref(['first_with_unit', null])
+const specUnit = {
+  type: 'cascading_single_choice',
+  title: '',
+  help: '',
+  validators: [],
+  elements: [
+    {
+      name: 'first_with_unit',
+      title: props.i18n.graph_options.unit_first_with_unit,
+      parameter_form: {
+        type: 'fixed_value',
+        title: '',
+        help: '',
+        validators: []
+      },
+      default_value: null
+    },
+    {
+      name: 'custom',
+      title: props.i18n.graph_options.unit_custom,
+      parameter_form: {
+        type: 'dictionary',
+        title: '',
+        help: '',
+        validators: [],
+        elements: [
+          {
+            ident: 'notation',
+            required: true,
+            parameter_form: {
+              type: 'cascading_single_choice',
+              title: props.i18n.graph_options.unit_custom_notation,
+              help: '',
+              validators: [],
+              elements: [
+                {
+                  name: 'decimal',
+                  title: props.i18n.graph_options.unit_custom_notation_decimal,
+                  parameter_form: {
+                    type: 'string',
+                    title: props.i18n.graph_options.unit_custom_notation_symbol,
+                    help: '',
+                    validators: [],
+                    input_hint: 'symbol',
+                    field_size: 'SMALL'
+                  },
+                  default_value: ''
+                },
+                {
+                  name: 'si',
+                  title: props.i18n.graph_options.unit_custom_notation_si,
+                  parameter_form: {
+                    type: 'string',
+                    title: props.i18n.graph_options.unit_custom_notation_symbol,
+                    help: '',
+                    validators: [],
+                    input_hint: 'symbol',
+                    field_size: 'SMALL'
+                  },
+                  default_value: ''
+                },
+                {
+                  name: 'iec',
+                  title: props.i18n.graph_options.unit_custom_notation_iec,
+                  parameter_form: {
+                    type: 'string',
+                    title: props.i18n.graph_options.unit_custom_notation_symbol,
+                    help: '',
+                    validators: [],
+                    input_hint: 'symbol',
+                    field_size: 'SMALL'
+                  },
+                  default_value: ''
+                },
+                {
+                  name: 'standard_scientific',
+                  title: props.i18n.graph_options.unit_custom_notation_standard_scientific,
+                  parameter_form: {
+                    type: 'string',
+                    title: props.i18n.graph_options.unit_custom_notation_symbol,
+                    help: '',
+                    validators: [],
+                    input_hint: 'symbol',
+                    field_size: 'SMALL'
+                  },
+                  default_value: ''
+                },
+                {
+                  name: 'engineering_scientific',
+                  title: props.i18n.graph_options.unit_custom_notation_engineering_scientific,
+                  parameter_form: {
+                    type: 'string',
+                    title: props.i18n.graph_options.unit_custom_notation_symbol,
+                    help: '',
+                    validators: [],
+                    input_hint: 'symbol',
+                    field_size: 'SMALL'
+                  },
+                  default_value: ''
+                },
+                {
+                  name: 'time',
+                  title: props.i18n.graph_options.unit_custom_notation_time,
+                  parameter_form: {
+                    type: 'fixed_value',
+                    title: '',
+                    help: '',
+                    validators: []
+                  },
+                  default_value: null
+                }
+              ]
+            },
+            default_value: { decimal: '' }
+          },
+          {
+            ident: 'precision',
+            required: true,
+            parameter_form: {
+              type: 'dictionary',
+              title: props.i18n.graph_options.unit_custom_precision,
+              help: '',
+              validators: [],
+              elements: [
+                {
+                  ident: 'rounding_mode',
+                  required: true,
+                  parameter_form: {
+                    type: 'single_choice',
+                    title: props.i18n.graph_options.unit_custom_precision_rounding_mode,
+                    help: '',
+                    validators: [],
+                    elements: [
+                      {
+                        name: 'auto',
+                        title: props.i18n.graph_options.unit_custom_precision_rounding_mode_auto
+                      },
+                      {
+                        name: 'strict',
+                        title: props.i18n.graph_options.unit_custom_precision_rounding_mode_strict
+                      }
+                    ],
+                    frozen: false
+                  },
+                  default_value: 'auto'
+                },
+                {
+                  ident: 'digits',
+                  required: true,
+                  parameter_form: {
+                    type: 'float',
+                    title: props.i18n.graph_options.unit_custom_precision_digits,
+                    help: '',
+                    validators: []
+                  },
+                  default_value: 2
+                }
+              ]
+            },
+            default_value: { rounding_mode: 'auto', digits: 2 }
+          }
+        ]
+      },
+      default_value: { notation: ['decimal', ''], precision: { rounding_mode: 'auto', digits: 2 } }
+    }
+  ]
+}
+const backendValidationUnit: ValidationMessages = []
+
+const dataVerticalRange = ref(['auto', null])
+const specVerticalRange = {
+  type: 'cascading_single_choice',
+  title: '',
+  help: '',
+  validators: [],
+  elements: [
+    {
+      name: 'auto',
+      title: props.i18n.graph_options.vertical_range_auto,
+      parameter_form: {
+        type: 'fixed_value',
+        title: 'i18n:Title',
+        help: '',
+        validators: []
+      }
+    },
+    {
+      name: 'explicit',
+      title: props.i18n.graph_options.vertical_range_explicit,
+      parameter_form: {
+        type: 'dictionary',
+        title: '',
+        help: '',
+        validators: [],
+        elements: [
+          {
+            ident: 'lower',
+            required: true,
+            parameter_form: {
+              type: 'float',
+              title: props.i18n.graph_options.vertical_range_explicit_lower,
+              help: '',
+              validators: []
+            },
+            default_value: 0.0
+          },
+          {
+            ident: 'upper',
+            required: true,
+            parameter_form: {
+              type: 'float',
+              title: props.i18n.graph_options.vertical_range_explicit_upper,
+              help: '',
+              validators: []
+            },
+            default_value: 1.0
+          }
+        ]
+      },
+      default_value: { lower: 0.0, upper: 1.0 }
+    }
+  ]
+}
+const backendValidationVerticalRange: ValidationMessages = []
+
+const dataMetricsWithZeroValues = ref(true)
+const specMetricsWithZeroValues = {
+  type: 'boolean_choice',
+  title: '',
+  help: '',
+  validators: []
+}
+const backendValidationMetricsWithZeroValues: ValidationMessages = []
 
 const topics: Topic[] = [
   {
@@ -49,5 +287,33 @@ const topics: Topic[] = [
 </script>
 
 <template>
-  <TopicsRenderer :topics="topics"> </TopicsRenderer>
+  <TopicsRenderer :topics="topics">
+    <template #unit>
+      <div>
+        <FormEdit
+          v-model:data="dataUnit"
+          :spec="specUnit"
+          :backend-validation="backendValidationUnit"
+        />
+      </div>
+    </template>
+    <template #vertical_range>
+      <div>
+        <FormEdit
+          v-model:data="dataVerticalRange"
+          :spec="specVerticalRange"
+          :backend-validation="backendValidationVerticalRange"
+        />
+      </div>
+    </template>
+    <template #metrics_with_zero_values>
+      <div>
+        <FormEdit
+          v-model:data="dataMetricsWithZeroValues"
+          :spec="specMetricsWithZeroValues"
+          :backend-validation="backendValidationMetricsWithZeroValues"
+        />
+      </div>
+    </template>
+  </TopicsRenderer>
 </template>
--- packages/cmk-frontend-vue/src/graph-designer/type_defs.ts
+++ packages/cmk-frontend-vue/src/graph-designer/type_defs.ts
@@ -13,7 +13,29 @@ export interface GraphDesignerContent {
   i18n: I18N;
 }
 export interface I18N {
+  graph_options: I18NGraphOptions;
   topics: I18NTopics;
+}
+export interface I18NGraphOptions {
+  unit_first_with_unit: string;
+  unit_custom: string;
+  unit_custom_notation: string;
+  unit_custom_notation_symbol: string;
+  unit_custom_notation_decimal: string;
+  unit_custom_notation_si: string;
+  unit_custom_notation_iec: string;
+  unit_custom_notation_standard_scientific: string;
+  unit_custom_notation_engineering_scientific: string;
+  unit_custom_notation_time: string;
+  unit_custom_precision: string;
+  unit_custom_precision_rounding_mode: string;
+  unit_custom_precision_rounding_mode_auto: string;
+  unit_custom_precision_rounding_mode_strict: string;
+  unit_custom_precision_digits: string;
+  vertical_range_auto: string;
+  vertical_range_explicit: string;
+  vertical_range_explicit_lower: string;
+  vertical_range_explicit_upper: string;
 }
 export interface I18NTopics {
   metric: string;

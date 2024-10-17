7efa1bf62b7c46fe1072d29edea3ffe24c12f9da
Christopher Krause <christopher.krause@checkmk.com>
2024-10-17 14:53:40 +0200

    17268 FIX Opsgenie: use Checkmk CA trust store
    The Opsgenie notification plugin will now use the Checkmk CA trust store
    (`~/var/ssl/ca-certificates.crt`) if an HTTP proxy is configured.
    
    Change-Id: I6460734a4ed1f4271e4a58b1ed3c7de721e283ea
    JIRA-Ref: SUP-20550

--- cmk/notification_plugins/opsgenie_issues.py
+++ cmk/notification_plugins/opsgenie_issues.py
@@ -22,6 +22,8 @@ from opsgenie_sdk.exceptions import (  # type: ignore[import-untyped]
     AuthenticationException,
 )
 
+from cmk.utils.paths import trusted_ca_file
+
 from cmk.notification_plugins import utils
 from cmk.notification_plugins.utils import retrieve_from_passwordstore
 
@@ -35,6 +37,7 @@ class Connector:
             conf.host = "%s" % host_url
         if proxy_url is not None:
             conf.proxy = proxy_url
+            conf.ssl_ca_cert = trusted_ca_file
 
         api_client: "ApiClient" = ApiClient(configuration=conf)
         self.alert_api = AlertApi(api_client=api_client)

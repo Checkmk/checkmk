66a0fd96151ba398dce8f607ab8c833e6ed68e59
Sven Panne <sven.panne@checkmk.com>
2024-10-21 13:42:52 +0200

    Naming only
    Change-Id: I3ec5a53a5c6df3f63e610b7c9ede627581359e7a

--- packages/livestatus/src/TableStateHistory.cc
+++ packages/livestatus/src/TableStateHistory.cc
@@ -279,9 +279,9 @@ std::unique_ptr<Filter> TableStateHistory::createPartialFilter(
 void TableStateHistory::answerQuery(Query &query, const User &user,
                                     const ICore &core) {
     log_cache_->apply(
-        [this, &query, &user, &core](const LogFiles &log_cache,
+        [this, &query, &user, &core](const LogFiles &log_files,
                                      size_t /*num_cached_log_messages*/) {
-            answerQueryInternal(query, user, core, log_cache);
+            answerQueryInternal(query, user, core, log_files);
         });
 }
 

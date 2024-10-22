fe762092c6b0d4d530659f54365f2097949815fd
Solomon Jacobs <solomon.jacobs@checkmk.com>
2024-10-22 11:01:03 +0200

    omd: relax requirements for log
    This test was very specific about `LogFormat` used by the site Apache.
    Currently, our packaging might provide different `LogFormat`s (likely
    due to a bazel issue).
    
    This should fix the integration test no matter which `apache-omd`
    artifact is shipped by our packaging.
    
    Change-Id: I35018527c7da0e4c3f6b3e9bd79aab86e9cf6844

--- tests/integration/omd/test_apache.py
+++ tests/integration/omd/test_apache.py
@@ -26,7 +26,7 @@ def test_http_methods(site: Site) -> None:
     apache_log_file = check_output(["cat", site.path("var/log/apache/access_log")])
     for line in apache_log_file.splitlines():
         if re.match(
-            r'^.*"PROPFIND /\w+/check_mk/ HTTP/1.1" 405 \d+ "-" "' + user_agent + '"$',
+            r'^.*"PROPFIND /\w+/check_mk/ HTTP/1.1" 405 \d+ "-" "' + user_agent,
             line,
         ):
             return

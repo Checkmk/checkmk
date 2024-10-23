Checkmk/checkmk

commit a1bee480c04729f2b57472068e7985d5b0f73d96

vue: changed ToggleButtonGroup to 2way data binding
Change-Id: I1a5603a8004cf4d51405509e4ac82c5426d90537

packages/cmk-frontend-vue/src/quick-setup/QuickSetupAsync.vue
@@ -321,13 +321,20 @@ const wizardMode: Ref<WizardMode> = usePersistentRef<WizardMode>(
   props.mode
 )
 
-const setWizardMode = async (mode: WizardMode) => {
-  wizardMode.value = mode
-  quickSetupHook.setMode(mode)
-  if (mode === 'overview' && !loadedAllStages.value) {
-    stages.value = await loadAllStages()
+const quickSetupHook = useWizard(stages.value.length, props.mode)
+
+const currentMode = computed({
+  get(): WizardMode {
+    return quickSetupHook.mode.value
+  },
+  set(newMode: WizardMode) {
+    wizardMode.value = newMode
+    quickSetupHook.setMode(newMode)
+    if (newMode === 'overview' && !loadedAllStages.value) {
+      loadAllStages()
+    }
   }
-}
+})
 
 switch (props.mode) {
   case 'guided':
@@ -337,19 +344,17 @@ switch (props.mode) {
     stages.value = await loadAllStages()
     break
 }
-const quickSetupHook = useWizard(stages.value.length, props.mode)
 showQuickSetup.value = true
 </script>
 
 <template>
   <ToggleButtonGroup
     v-if="toggleEnabled"
+    v-model="currentMode"
     :options="[
       { label: GUIDED_MODE_LABEL, value: 'guided' },
       { label: OVERVIEW_MODE_LABEL, value: 'overview' }
     ]"
-    :value="quickSetupHook.mode.value"
-    @change="setWizardMode"
   />
   <QuickSetup
     v-if="showQuickSetup"

packages/cmk-frontend-vue/src/components/ToggleButtonGroup.vue
@@ -16,13 +16,13 @@ export interface ToggleButtonGroupProps {
   value?: string | null
 }
 
-const props = withDefaults(defineProps<ToggleButtonGroupProps>(), {
-  value: null
-})
+defineProps<ToggleButtonGroupProps>()
+const model = defineModel<string>({ required: true })
 
-defineEmits(['change'])
-
-const isSelected = (value: string) => value === props.value
+const isSelected = (value: string) => value === model.value
+function setSelectedOption(value: string) {
+  model.value = value
+}
 </script>
 
 <template>
@@ -33,7 +33,7 @@ const isSelected = (value: string) => value === props.value
       class="toggle_option"
       :class="{ selected: isSelected(option.value) }"
       :aria-label="`Toggle ${option.label}`"
-      @click="$emit('change', option.value)"
+      @click.prevent="setSelectedOption(option.value)"
       >{{ option.label }}</CmkButton
     >
   </div>

packages/cmk-frontend-vue/src/components/_demo/DemoToggleButtonGroup.vue
@@ -8,21 +8,20 @@ conditions defined in the file COPYING, which is part of this source code packag
 import { ref } from 'vue'
 import ToggleButtonGroup from '@/components/ToggleButtonGroup.vue'
 
-const selectedOption = ref<string>('1')
+const model = ref<string>('1')
 </script>
 
 <template>
   <ToggleButtonGroup
+    v-model="model"
     :options="[
       { label: 'label1', value: '1' },
       { label: 'label2', value: '2' },
       { label: 'label3', value: '3' }
     ]"
-    :value="selectedOption"
-    @change="(newValue) => (selectedOption = newValue)"
   />
 
-  <pre>selectedOption={{ selectedOption }}</pre>
+  <pre>selectedOption={{ model }}</pre>
 </template>
 
 <style scoped>

packages/cmk-frontend-vue/src/form/components/forms/FormCascadingSingleChoice.vue
@@ -117,11 +117,6 @@ const buttonGroupButtons = computed((): Array<{ label: string; value: string }> 
     return { label: element.title, value: element.name }
   })
 })
-
-const toggledButton = (value: string) => {
-  event!.preventDefault()
-  selectedOption.value = value
-}
 </script>
 
 <template>
@@ -138,11 +133,7 @@ const toggledButton = (value: string) => {
       </select>
     </template>
     <template v-else>
-      <ToggleButtonGroup
-        :options="buttonGroupButtons"
-        :value="selectedOption"
-        @change="toggledButton"
-      />
+      <ToggleButtonGroup v-model="selectedOption" :options="buttonGroupButtons" />
     </template>
     <HelpText :help="props.spec.help" />
   </span>

commit c2e42e39c1c178f78522ed3267850a12faede574

Fix Quick setup: fix loading of overview stages
caused by a1bee480c04729f2b57472068e7985d5b0f73d96

Change-Id: Ia0a7bfb036047af13f26a55c6bf35f9ad509b719

packages/cmk-frontend-vue/src/quick-setup/QuickSetupAsync.vue
@@ -4,7 +4,7 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
-import { computed, ref, toValue, type Ref } from 'vue'
+import { computed, ref, toValue, type Ref, watch } from 'vue'
 
 import QuickSetup from './components/quick-setup/QuickSetup.vue'
 
@@ -321,16 +321,13 @@ const wizardMode: Ref<WizardMode> = usePersistentRef<WizardMode>(
   props.mode
 )
 
-const currentMode = computed({
-  get(): WizardMode {
-    return quickSetupHook.mode.value
-  },
-  set(newMode: WizardMode) {
-    wizardMode.value = newMode
-    quickSetupHook.setMode(newMode)
-    if (newMode === 'overview' && !loadedAllStages.value) {
-      loadAllStages()
-    }
+const currentMode = ref<WizardMode>(props.mode)
+
+watch(currentMode, async (mode: WizardMode) => {
+  wizardMode.value = mode
+  quickSetupHook.setMode(mode)
+  if (mode === 'overview' && !loadedAllStages.value) {
+    stages.value = await loadAllStages()
   }
 })
 
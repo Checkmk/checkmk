772ffce9c03c2551289f159c9d10f2fb18cf2c09
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-10-23 13:20:55 +0200

    Fixup deployment of dev documentation
    Change-Id: I1c6e207cb7ca2e2b5d43273c6a83cdc406fcf9af

--- buildscripts/scripts/update-architecture-documentation.groovy
+++ buildscripts/scripts/update-architecture-documentation.groovy
@@ -13,7 +13,7 @@ def main() {
         stage("Deploy") {
             withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {    // groovylint-disable DuplicateMapLiteral
                 sh("""
-                    scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} doc/documentation/_build/htmlhelp ${DEV_DOCS_URL}/devdoc
+                    scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} doc/documentation/_build/htmlhelp/* ${DEV_DOCS_URL}/devdoc
                 """);
             }
         }

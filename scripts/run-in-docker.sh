152f30afe6de6900a618b1718bd0985ad90da9f8
Frans Fürst <frans.fuerst@checkmk.com>
2024-10-24 14:36:13 +0200

    Revert "Fix clean up of *.cid files"
    This reverts commit acae143e04b7fd00dfd0aa5cce16f46cdf4ee6d3.
    
    Change-Id: I6b647162a1d48e614f81c8e0575c020fff78eec6

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -36,9 +36,6 @@ echo >&2 "run-in-docker.sh checkpoint 1"
 
 echo >&2 "run-in-docker.sh checkpoint 2 $IMAGE_ID"
 
-# TODO: Remove all operations on *.cid files as soon as we finished debugging
-find "${CHECKOUT_ROOT}" -name "*.cid" -delete
-
 IMAGE_VERSION="$(docker run --rm -v "${CHECKOUT_ROOT}/omd:/tmp" "${IMAGE_ID}" /tmp/distro '-')"
 
 echo >&2 "run-in-docker.sh checkpoint 3 $IMAGE_VERSION"
@@ -138,7 +135,7 @@ trap cleanup EXIT
 
 # shellcheck disable=SC2086
 docker run -a stdout -a stderr \
-    --cidfile "${CHECKOUT_ROOT}/${CONTAINER_NAME}.cid" \
+    --cidfile "$CONTAINER_NAME.cid" \
     --rm \
     --name $CONTAINER_NAME \
     ${TERMINAL_FLAG} \

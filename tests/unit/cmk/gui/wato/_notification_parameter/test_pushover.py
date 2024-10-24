723dc5bcecdcb1e621d5866696a284ed74dab1f7
Ronny Bruska <ronny.bruska@checkmk.com>
2024-10-24 16:58:45 +0200

    Migrate pushover notification to FormSpecs
    CMK-19298
    
    Change-Id: Ibe09e0a89dae82f9184e6fa88d8c221275454b49

---
+++ tests/unit/cmk/gui/wato/_notification_parameter/test_pushover.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import Literal
+
+import pytest
+
+from cmk.gui.wato._notification_parameter._pushover import _migrate_to_priority
+
+
+@pytest.mark.parametrize(
+    "old_priority, new_priority",
+    [
+        pytest.param(
+            {"priority": "0"},
+            {"priority": ("normal", None)},
+            id="Normal",
+        ),
+        pytest.param(
+            {
+                "priority": "2",
+                "retry": 0,
+                "expire": 0,
+                "receipts": "ergerahtrehrthrthrhrhaergherhtgrsth",
+            },
+            {"priority": ("emergency", (0.0, 0.0, "ergerahtrehrthrthrhrhaergherhtgrsth"))},
+            id="Emergency",
+        ),
+    ],
+)
+def test__migrate_to_priority(
+    old_priority: dict[str, str],
+    new_priority: dict[
+        str,
+        tuple[
+            Literal["normal", "high", "low", "lowest", "emergency"],
+            None | tuple[float, float, str],
+        ],
+    ],
+) -> None:
+    assert _migrate_to_priority(old_priority) == new_priority

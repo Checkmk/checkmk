7f09df077a6e736ec27de260cfc87743bcc4c367
Mathias Laurin <mathias.laurin@checkmk.com>
2024-10-29 07:33:12 +0100

    Hardcode version of mkp_tool
    Importlib or parsing with toml assume different packaging techniques
    than ours.
    
    CMK-19825
    
    Change-Id: I0960f53b82906d021b1be3b04d08a3adbd1ffc89

--- packages/cmk-mkp-tool/cmk/mkp_tool/__version__.py
+++ packages/cmk-mkp-tool/cmk/mkp_tool/__version__.py
@@ -3,12 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from pathlib import Path
-
-import toml
-
 # Using `importlib.metadata.version` would be nicer but assumes that we install
 # packages as wheels in the site, which we currently don't.
-__version__ = toml.load(Path(__file__).parent.parent.parent / "pyproject.toml")["project"][
-    "version"
-]
+__version__ = "0.2.0"

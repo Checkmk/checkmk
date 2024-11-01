d44d888682b250bc8a9a859d0ddcc1e71359f406
Moritz Kiemer <moritz.kiemer@checkmk.com>
2024-11-01 18:52:36 +0100

    Remove obsolete test
    We no longer need to assure consistency with
    a long deprecated API.
    
    Change-Id: I21591d009c732b572e30bf5df272e92b0c9d9b55

--- tests/unit/cmk/plugins/collection/agent_based/test_check_plugin_properties.py
+++ tests/unit/cmk/plugins/collection/agent_based/test_check_plugin_properties.py
@@ -3,94 +3,13 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Generator, Sequence
-
 from cmk.checkengine.sectionparser import ParsedSectionName
 
-import cmk.base.api.agent_based.register as agent_based_register
 from cmk.base.api.agent_based.plugin_classes import (
     CheckPlugin,
     InventoryPlugin,
-    SectionPlugin,
-    SNMPSectionPlugin,
 )
 from cmk.base.api.agent_based.register import AgentBasedPlugins
-
-
-def _section_permutations(
-    parsed_section_names: Sequence[ParsedSectionName],
-) -> Generator[tuple[SectionPlugin, ...], None, None]:
-    if not parsed_section_names:
-        yield ()
-        return
-
-    if len(parsed_section_names) >= 1:
-        for section_name in agent_based_register.get_section_producers(parsed_section_names[0]):
-            for perm in _section_permutations(parsed_section_names[1:]):
-                yield (agent_based_register.get_section_plugin(section_name),) + perm
-
-
-def _get_empty_parsed_result(section: SectionPlugin) -> object:
-    return (
-        section.parse_function(len(section.trees) * [[]])  # type: ignore[arg-type]
-        if isinstance(section, SNMPSectionPlugin)
-        else None
-    )
-
-
-def test_check_plugins_do_not_discover_upon_empty_snmp_input(
-    agent_based_plugins: AgentBasedPlugins,
-) -> None:
-    """
-    In Checkmk < 1.6 the parse function has not been called for empty table data,
-    unless "handle_empty_info" has been set.
-
-    From version 2.0 on, the parse function will be called always.
-    In case no further processing is desired, the parse functions should return `None`.
-
-    (Returning something falsey usually means nothing will be discovered!)
-
-    Since this was the behaviour for *almost* every plug-in we maintain this test
-    with a list of known exceptions, to ensure the old behaviour is not changed.
-
-    However: There is nothing wrong with not returning None, in principle.
-    If you whish to do that (see one of the listed exceptions for examples),
-    just add an exception below. If maintaining this test becomes too tedious,
-    we can probably just remove it.
-    """
-    plugins_expected_to_discover_upon_empty = {
-        "ewon",
-        "printer_alerts",
-        "liebert_system_events",
-        "apc_inrow_system_events",
-    }
-
-    plugins_discovering_upon_empty = set()
-    for _name, plugin in sorted(agent_based_plugins.check_plugins.items()):
-        for sections in _section_permutations(plugin.sections):
-            kwargs = {
-                str(section.parsed_section_name): _get_empty_parsed_result(section)
-                for section in sections
-            }
-            if all(v is None for v in kwargs.values()):
-                continue
-
-            if len(kwargs) > 1:
-                kwargs = {f"section_{k}": v for k, v in kwargs.items()}
-            else:
-                kwargs = {"section": v for v in kwargs.values()}
-
-            if plugin.discovery_default_parameters is not None:
-                kwargs["params"] = (
-                    plugin.discovery_default_parameters
-                    if (plugin.discovery_ruleset_type == "merged")
-                    else [plugin.discovery_default_parameters]
-                )
-
-            if list(plugin.discovery_function(**kwargs)):
-                plugins_discovering_upon_empty.add(str(plugin.name))
-
-    assert plugins_discovering_upon_empty == plugins_expected_to_discover_upon_empty
 
 
 def test_no_plugins_with_trivial_sections(agent_based_plugins: AgentBasedPlugins) -> None:

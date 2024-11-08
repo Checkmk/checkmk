78e364f84a3a1de026a7464be5aaa90314ba0424
René Slowenski <rene.slowenski@checkmk.com>
2024-11-08 06:44:08 +0100

    integration test: Fix pip_exists and python_interpreter_exists
    These two tests check for the existence of pip and python in the
    site. The path was created in a wrong way though, effectively
    checking for the files in the /bin folder. This change fixes that.
    
    Change-Id: I710038ed99e39da166fdec1c538a7de6b2dad388

--- tests/integration/omd/test_python.py
+++ tests/integration/omd/test_python.py
@@ -84,7 +84,7 @@ def _load_pipfile_data() -> dict:
 
 
 def test_01_python_interpreter_exists(site: Site) -> None:
-    assert site.path(f"/bin/python{_get_python_version_from_defines_make().major}").exists()
+    assert site.path(f"bin/python{_get_python_version_from_defines_make().major}").exists()
 
 
 def test_02_python_interpreter_path(site: Site) -> None:
@@ -125,7 +125,7 @@ def test_03_python_path(site: Site) -> None:
 
 
 def test_01_pip_exists(site: Site) -> None:
-    assert site.path("/bin/pip3").exists()
+    assert site.path("bin/pip3").exists()
 
 
 def test_02_pip_path(site: Site) -> None:

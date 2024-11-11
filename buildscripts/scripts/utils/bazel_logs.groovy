624a15317820d30a754959c8ab4b0e0f9579a5e2
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-11-11 16:00:39 +0100

    Remove propertiesSeries from groovy plot function
    ... and remove large execution log summary files before stashing
    
    CMK-19571
    
    Change-Id: Ie7bdf1d6bfc4ae14fcce17cbc53279da3abf89f0

--- buildscripts/scripts/utils/bazel_logs.groovy
+++ buildscripts/scripts/utils/bazel_logs.groovy
@@ -8,17 +8,17 @@ def try_parse_bazel_execution_log(distro, distro_dir, bazel_log_prefix) {
             def summary_file="${distro_dir}/${bazel_log_prefix}execution_summary_${distro}.json";
             def cache_hits_file="${distro_dir}/${bazel_log_prefix}cache_hits_${distro}.csv";
             sh("""bash \
-            buildscripts/scripts/bazel_execution_log_parser.sh \
-            --execution_logs_root "${distro_dir}" \
-            --bazel_log_file_pattern "bazel_execution_log*" \
-            --summary_file "${summary_file}" \
-            --cachehit_csv "${cache_hits_file}" \
-            --distro "${distro}"
-        """);
-            stash(name: "${bazel_log_prefix}${distro}", includes: "${bazel_log_prefix}*")
+                buildscripts/scripts/bazel_execution_log_parser.sh \
+                --execution_logs_root "${distro_dir}" \
+                --bazel_log_file_pattern "bazel_execution_log*" \
+                --summary_file "${summary_file}" \
+                --cachehit_csv "${cache_hits_file}" \
+                --distro "${distro}"
+            """);
 
             // remove large execution log summary file to save some space, approx 1.6GB per workspace
             sh("rm -rf ${distro_dir}/${bazel_log_prefix}*.json");
+            stash(name: "${bazel_log_prefix}${distro}", includes: "${bazel_log_prefix}*")
         }
     } catch (e) {
         print("Failed to parse bazel execution logs: ${e}");
@@ -44,7 +44,6 @@ def try_plot_cache_hits(bazel_log_prefix, distros) {
             description: 'Bazel Remote Cache Analysis',
             group: 'Bazel Cache',
             numBuilds: '30',
-            propertiesSeries: [[file: '', label: '']],
             style: 'line',
             title: 'Cache hits',
             yaxis: 'Cache hits in percent',

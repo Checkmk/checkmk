35bc6d025c13ea423e383265e6dc9a3624be1d81
Ronny Bruska <ronny.bruska@checkmk.com>
2024-11-12 15:27:25 +0100

    17169 FIX Also rename parents in folder properties
    CMK-21232
    
    Change-Id: I9d26cec7816550b4b9497ed592321793002d49fd

--- cmk/gui/watolib/hosts_and_folders.py
+++ cmk/gui/watolib/hosts_and_folders.py
@@ -2599,6 +2599,7 @@ class Folder(FolderProtocol):
         if not changed:
             return False
 
+        self.attributes["parents"] = [HostName(h) for h in new_parents]
         add_change(
             "rename-parent",
             _l('Renamed parent from %s to %s in folder "%s"')

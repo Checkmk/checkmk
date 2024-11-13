deda6bca650e8f87eb9621e66b6be0d3eeb1e60e
Hojjat Afsharan <hojjat.afsharan@checkmk.com>
2024-11-13 16:12:54 +0100

    remove tailwind-merge
    Change-Id: Icceb5b2febac6678e3274576a22f6fb0c5a07798

--- packages/cmk-frontend-vue/src/lib/utils.ts
+++ packages/cmk-frontend-vue/src/lib/utils.ts
@@ -3,12 +3,6 @@
  * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
  * conditions defined in the file COPYING, which is part of this source code package.
  */
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 export const localStorageHandler = {
   get: (key: string, defaultValue: unknown = null): unknown => {

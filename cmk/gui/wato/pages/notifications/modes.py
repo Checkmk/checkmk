c8e6261435f249a343df48fe40c0c6e55c319d39
Logan Connolly <logan.connolly@checkmk.com>
2024-11-13 08:13:14 +0100

    notifications: drop legacy notifications rule link
    We don't want to have two notification setup "worlds". With this change,
    the transition to the new setup is finalized in the frontend.
    
    CMK-19139
    
    Change-Id: I14c2572894a77fe6fc32818ef29f5a679074dfe3

--- cmk/gui/wato/pages/notifications/modes.py
+++ cmk/gui/wato/pages/notifications/modes.py
@@ -651,21 +651,12 @@ class ModeNotifications(ABCNotificationsMode):
                             title=_("Add new"),
                             entries=[
                                 PageMenuEntry(
-                                    title=_("New notification setup"),
+                                    title=_("Add notification rule"),
                                     icon_name="new",
                                     item=make_simple_link(
                                         folder_preserving_link(
                                             [("mode", "notification_rule_quick_setup")]
                                         )
-                                    ),
-                                    is_shortcut=True,
-                                    is_suggested=True,
-                                ),
-                                PageMenuEntry(
-                                    title=_("Add notification rule"),
-                                    icon_name="new",
-                                    item=make_simple_link(
-                                        folder_preserving_link([("mode", "notification_rule")])
                                     ),
                                     is_shortcut=True,
                                     is_suggested=True,

b147ef349479a3310882573930faeee13613f7aa
Wontek Hong <wontek.hong@checkmk.com>
2024-11-18 13:39:12 +0100

    validate_mk_files: introduce dedicated validation for timeperiods
    Change-Id: I122f7b12a76801ababe929ee7aab79e0f1392bb8

--- cmk/update_config/plugins/actions/validate_mk_files.py
+++ cmk/update_config/plugins/actions/validate_mk_files.py
@@ -4,17 +4,25 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from logging import Logger
-from typing import Generator
+from pathlib import Path
+from typing import Callable, Generator
 
 from pydantic import TypeAdapter, ValidationError
 
 from cmk.utils import tty
 from cmk.utils.config_validation_layer.validation_utils import ConfigValidationError
 from cmk.utils.rulesets.ruleset_matcher import RuleSpec
+from cmk.utils.timeperiod import (
+    TimeperiodSpecs,
+    validate_day_time_ranges,
+    validate_timeperiod_exceptions,
+)
 
 from cmk.gui.watolib.hosts_and_folders import Folder, folder_tree
 from cmk.gui.watolib.rulesets import FolderRulesets, InvalidRuleException
 from cmk.gui.watolib.simple_config_file import config_file_registry
+from cmk.gui.watolib.timeperiods import load_timeperiods
+from cmk.gui.watolib.utils import wato_root_dir
 
 from cmk.update_config.registry import update_action_registry, UpdateAction
 
@@ -27,24 +35,34 @@ class LABELS:
 class ValidateConfigFiles(UpdateAction):
     def __call__(self, logger: Logger) -> None:
         """
-        Validate the data in all registered mk config files.
+        1. Validate the data in all registered mk config files.
+        2. Validate timeperiods
+        3. Validate rules of rulesets in folders
         """
 
         failures: bool = False
         counter = 1
         for relative_file_path in config_file_registry:
-            result = _file_format(relative_file_path, counter)
-            try:
-                config_file_registry[relative_file_path].read_file_and_validate()
-            except (NotImplementedError, ConfigValidationError) as exc:
-                result += f"{LABELS.failed}\n\t{str(exc)}"
-                failures = True
-            else:
-                result += LABELS.passed
-
+            result, has_failure = _run_and_evaluate_validation(
+                relative_file_path,
+                counter,
+                config_file_registry[relative_file_path].read_file_and_validate,
+            )
+            failures |= has_failure
             counter += 1
             logger.info(result)
 
+        # Validate timeperiods.mk
+        result, has_failure = _run_and_evaluate_validation(
+            str(Path(wato_root_dir()) / "timeperiods.mk"),
+            counter,
+            lambda: validate_timeperiods(load_timeperiods()),
+        )
+        failures |= has_failure
+        counter += 1
+        logger.info(result)
+
+        # Validate rulesets in folders
         rule_validator = TypeAdapter(RuleSpec)  # nosemgrep: type-adapter-detected
         for folder_path, validate_result, has_failure in _validate_folder_ruleset_rules(
             folder_tree().root_folder(), rule_validator
@@ -69,6 +87,37 @@ class ValidateConfigFiles(UpdateAction):
 
 def _file_format(file_path: str, row_number: int) -> str:
     return f"    {tty.yellow}{row_number:02d}{tty.normal} {file_path:.<60} "
+
+
+def _run_and_evaluate_validation(
+    file_path: str, counter: int, validation_call: Callable[[], None]
+) -> tuple[str, bool]:
+    evaluation_message = _file_format(file_path, counter)
+    has_failure = False
+    try:
+        validation_call()
+    except (NotImplementedError, ConfigValidationError, ValueError, Exception) as exc:
+        evaluation_message += f"{LABELS.failed}\n\t{str(exc)}"
+        has_failure = True
+    else:
+        evaluation_message += LABELS.passed
+    return evaluation_message, has_failure
+
+
+def validate_timeperiods(time_periods: TimeperiodSpecs) -> None:
+    validator = TypeAdapter(TimeperiodSpecs)  # nosemgrep: type-adapter-detected
+    validator.validate_python(time_periods, strict=True)
+
+    invalid_timeperiods = []
+    for _name, timeperiod in time_periods.items():
+        try:
+            validate_day_time_ranges(timeperiod)
+            validate_timeperiod_exceptions(timeperiod)
+        except ValueError as exc:
+            invalid_timeperiods.append(f"{_name}: {str(exc)}")
+
+    if invalid_timeperiods:
+        raise ValueError(f"Invalid timeperiods:\n\t{invalid_timeperiods}")
 
 
 def _validate_folder_ruleset_rules(

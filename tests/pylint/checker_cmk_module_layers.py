49c58d953b6261a0376c2f145f8cc202992a63f4
Simon Jess <simon.jess@checkmk.com>
2024-11-18 13:04:03 +0100

    OTEL: Add basic config
    Change-Id: I39cd31a92e2b03177bab57a984e99d1a88565e3a

--- tests/pylint/checker_cmk_module_layers.py
+++ tests/pylint/checker_cmk_module_layers.py
@@ -86,6 +86,7 @@ def _is_allowed_import(imported: ModuleName) -> bool:
             _in_component(imported=imported, component=Component("cmk.graphing")),
             _in_component(imported=imported, component=Component("cmk.trace")),
             _in_component(imported=imported, component=Component("cmk.events")),
+            _in_component(imported=imported, component=Component("cmk.otel")),
         )
     )
 

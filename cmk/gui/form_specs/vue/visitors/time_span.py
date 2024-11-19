76777bdb4065523d6d9d0811838f1cde7fdb7386
Benedikt Seidl <benedikt.seidl@checkmk.com>
2024-11-19 14:08:37 +0100

    better validation message for TimeSpan
    this comes with a drawback:
    you lose the custom error message of the validator.
    
    CMK-18892
    
    Change-Id: Id765a16f0af02b86df0b18f82e1977fab7522207

--- cmk/gui/form_specs/vue/visitors/time_span.py
+++ cmk/gui/form_specs/vue/visitors/time_span.py
@@ -4,14 +4,17 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from collections.abc import Callable, Sequence
+from collections.abc import Callable, Iterator, Sequence
+
+from cmk.utils.render import SecondsRenderer
 
 from cmk.gui.form_specs.private.validators import IsFloat
 from cmk.gui.form_specs.vue import shared_type_defs
 from cmk.gui.form_specs.vue.validators import build_vue_validators
 
-from cmk.rulesets.v1 import Label, Title
+from cmk.rulesets.v1 import Label, Message, Title
 from cmk.rulesets.v1.form_specs import TimeMagnitude, TimeSpan
+from cmk.rulesets.v1.form_specs.validators import NumberInRange
 
 from ._base import FormSpecVisitor
 from ._type_defs import DEFAULT_VALUE, DefaultValue, EMPTY_VALUE, EmptyValue
@@ -42,6 +45,11 @@ def magnitude_translator(magnitude: TimeMagnitude) -> shared_type_defs.TimeSpanT
             raise RuntimeError()
 
 
+def _render_value(value: float) -> str:
+    _whole_seconds, frac = divmod(value, 1.0)
+    return SecondsRenderer.detailed_str(int(value)) + (f" {round(frac * 1000)} ms" if frac else "")
+
+
 class TimeSpanVisitor(FormSpecVisitor[TimeSpan, float]):
     def _parse_value(self, raw_value: object) -> float | EmptyValue:
         if isinstance(raw_value, DefaultValue):
@@ -60,7 +68,36 @@ class TimeSpanVisitor(FormSpecVisitor[TimeSpan, float]):
             return EMPTY_VALUE
 
     def _validators(self) -> Sequence[Callable[[float], object]]:
-        return [IsFloat()] + compute_validators(self.form_spec)
+        def custom_validate() -> Iterator[Callable[[float], object]]:
+            for validator in compute_validators(self.form_spec):
+                if isinstance(validator, NumberInRange):
+                    min_value, max_value = validator.range
+
+                    if max_value is not None and min_value is not None:
+                        message = Message("Allowed values range from %s to %s.") % (
+                            _render_value(min_value),
+                            _render_value(max_value),
+                        )
+                    elif min_value is None and max_value is not None:
+                        message = Message("The maximum allowed value is %s.") % _render_value(
+                            max_value
+                        )
+                    elif min_value is not None and max_value is None:
+                        message = Message("The minimum allowed value is %s.") % _render_value(
+                            min_value
+                        )
+                    else:
+                        raise RuntimeError()  # is impossible because of NumberInRange init function
+
+                    yield NumberInRange(
+                        min_value=min_value,
+                        max_value=max_value,
+                        error_msg=message,
+                    )
+                else:
+                    yield validator
+
+        return [IsFloat()] + list(custom_validate())
 
     def _to_vue(
         self, raw_value: object, parsed_value: float | EmptyValue

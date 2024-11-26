878350153cf6a57954601961cdfeb1b4139de947
Jonas Scharpf <jonas.scharpf@checkmk.com>
2024-11-26 13:28:32 +0100

    Replace Ubuntu 23.04 with Ubuntu 24.04 to test the laptop setup script
    ... as almost all users have migrated
    
    CMK-20446
    
    Change-Id: I64af2a6132aba441231e440c45720a054b8a74f1

--- buildscripts/scripts/test-laptop-setup.groovy
+++ buildscripts/scripts/test-laptop-setup.groovy
@@ -43,7 +43,7 @@ def main() {
         );
         // no support for 20.04, sorry
         // python2 would be required, and the system Python does not support typing in "strip_binaries"
-        def ubuntu_versions = ["22.04", "23.04"];
+        def ubuntu_versions = ["22.04", "24.04"];
 
         dir("${checkout_dir}") {
             sh("""

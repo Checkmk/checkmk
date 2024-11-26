fb0826f5d8b3d4371ffd7bb564cbe38598a65ab3
Leonardo Petrora <leonardo.petrora@checkmk.com>
2024-11-26 16:02:58 +0100

    Remove 'Undocumented Endpoint' from documentation
    This change ensures that endpoints in the `Undocumented Endpoint` tag
    group do not appear in the Rest API documentation
    
    Change-Id: I529814c8c98c49cb93a2cecbbbb0be95d7178773

--- cmk/gui/openapi/spec/spec_generator.py
+++ cmk/gui/openapi/spec/spec_generator.py
@@ -459,7 +459,7 @@ Ident = tuple[str, str]
 __version__ = "1.0"
 
 
-def main(args: Sequence[str]) -> int:
+def main() -> int:
     main_modules.load_plugins()
     if errors := get_failed_plugins():
         raise Exception(f"The following errors occured during plug-in loading: {errors}")
@@ -490,7 +490,7 @@ def _generate_spec(
     seen_paths: dict[Ident, OperationObject] = {}
     ident: Ident
     for endpoint in sorted(endpoint_registry, key=sort_key):
-        if target in endpoint.blacklist_in:
+        if target in endpoint.blacklist_in or endpoint.tag_group == "Undocumented Endpoint":
             continue
 
         for path, operation_dict in _operation_dicts(spec, endpoint):

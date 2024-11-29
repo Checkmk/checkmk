eb7894ed1c93c97f4e1de0465e32a86c0f54397d
Niko Wenselowski <niko.wenselowski@checkmk.com>
2024-11-29 11:34:41 +0100

    trigger-all-nightlies-fast: Use smart_stage
    This will show unprocessed editions as skipped in order to have a nicer view in Jenkins.
    
    Change-Id: I65d1114eb3228fdb06116ae75ffc70133df44c8f

--- buildscripts/scripts/trigger-all-nightlies-fast.groovy
+++ buildscripts/scripts/trigger-all-nightlies-fast.groovy
@@ -7,11 +7,12 @@
 def main() {
     def base_folder = "${currentBuild.fullProjectName.split('/')[0..-2].join('/')}/";
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
-    def editions = versioning.get_editions();
+    def all_editions = versioning.get_editions();
+    def editions_to_test = all_editions;
 
     if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) in 12..15) {
         // build only enterprise on high noon or a little bit later
-        editions = ["enterprise"];
+        editions_to_test = ["enterprise"];
     }
 
     def job_parameters = [
@@ -20,22 +21,29 @@ def main() {
 
     def override_editions = params.EDITIONS.trim() ?: "";
     if (override_editions) {
-        editions = override_editions.replaceAll(',', ' ').split(' ').grep();
+        editions_to_test = override_editions.replaceAll(',', ' ').split(' ').grep();
     }
 
     print(
         """
         |===== CONFIGURATION ===============================
-        |editions:.............. │${editions}│
+        |editions:.............. │${editions_to_test}│
         |base_folder:........... │${base_folder}│
         |job_parameters:........ │${job_parameters}│
         |fixed_node:............ |${params.TRIGGER_CIPARAM_OVERRIDE_BUILD_NODE}|
         |===================================================
         """.stripMargin());
 
-    for ( edition in editions ) {
-        catchError(buildResult: "FAILURE", stageResult: "FAILURE") {
-            stage("Trigger ${edition}") {
+    for ( edition in all_editions ) {
+        def run_condition = edition in editions_to_test;
+        println("Should ${edition} be triggered? ${run_condition}");
+
+        smart_stage(
+            name: "Trigger ${edition}",
+            condition: run_condition,
+            raiseOnError: false,
+        ) {
+            catchError(buildResult: "FAILURE", stageResult: "FAILURE") {
                 def this_job_parameters = job_parameters + [$class: 'StringParameterValue', name: 'EDITION', value: edition];
 
                 print(

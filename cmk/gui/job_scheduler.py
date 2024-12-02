b087fe9d53e63564f0880a5de5aedd5130d15bc6
Lars Michelsen <lm@checkmk.com>
2024-12-02 18:09:57 +0100

    Fix typo
    Change-Id: I10cff6ed19c9fb9088490abdacdb0458bf9c4c2b

--- cmk/gui/job_scheduler.py
+++ cmk/gui/job_scheduler.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-"""Runs and obseves regular jobs in the cmk.gui context"""
+"""Runs and observes regular jobs in the cmk.gui context"""
 
 import datetime
 import logging

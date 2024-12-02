9aaec6e0a705991b6735e4b785ecf82b7b750357
Frans Fürst <frans.fuerst@checkmk.com>
2024-12-02 14:38:43 +0100

    cisco_power: refactoring for SUP-21397
    Change-Id: I717839e644aedc42db3afde67a6ee9e5c392c2b7

--- cmk/plugins/collection/agent_based/cisco_power.py
+++ cmk/plugins/collection/agent_based/cisco_power.py
@@ -17,7 +17,6 @@ from cmk.agent_based.v2 import (
     StringTable,
 )
 from cmk.plugins.lib.cisco import DETECT_CISCO
-from cmk.plugins.lib.cisco_sensor_item import cisco_sensor_item
 
 # .1.3.6.1.4.1.9.9.13.1.5.1.2.1 "removed"
 # .1.3.6.1.4.1.9.9.13.1.5.1.2.2 "AC Power Supply"
@@ -46,33 +45,71 @@ cisco_power_sources = (
 )
 
 
+def item_name_from(description: str, sensor_id: str) -> str:
+    """Returns a combination from device description and OID index which can
+    be used as service item name.
+    >>> item_name_from("", "123")
+    ' 123'
+    >>> item_name_from("removed", "123")
+    'removed 123'
+    >>> item_name_from("Switch#1, PowerSupply#1, Status is Normal, RPS Not Present", "123")
+    'Switch 1 PowerSupply 1'
+    >>> item_name_from("Sw1, PS1 Normal, RPS NotExist", "123")
+    'Sw1 PS1'
+    >>> item_name_from("Switch 1 - Power Supply A, Normal", "123")
+    'Switch 1 - Power Supply A 123'
+    >>> item_name_from("Sw1, PSA Normal", "123")
+    'Sw1 PSA 123'
+    >>> item_name_from("Switch#1, PowerSupply 1", "123")
+    'Switch 1 PowerSupply 1'
+    """
+    # The 'description part' of given description string which might
+    # contain extra status information (comma separated) or might even be empty
+    # description can be, depending on the device model.
+    if len(splitted := [x.strip() for x in description.split(",")]) == 1:
+        device_description = description
+    elif "#" in splitted[-1] or "Power" in splitted[-1]:
+        device_description = " ".join(splitted)
+    elif splitted[-1].startswith("PS"):
+        device_description = " ".join([splitted[0], splitted[-1].split(" ")[0]])
+    elif splitted[-2].startswith("PS"):
+        device_description = " ".join(splitted[:-2] + splitted[-2].split(" ")[:-1])
+    elif splitted[-2].startswith("Status"):
+        device_description = " ".join(splitted[:-2])
+    else:
+        device_description = " ".join(splitted[:-1])
+
+    item = device_description.replace("#", " ")
+    # Different sensors may have identical descriptions. To prevent
+    # duplicate items the sensor_id is appended. This leads to
+    # redundant information sensors are enumerated with letters like
+    # e.g. "PSA" and "PSB", but to be backwards compatible we do not
+    # modify this behaviour.
+    return item if item and item[-1].isdigit() else f"{item} {sensor_id}"
+
+
 def inventory_cisco_power(section: StringTable) -> DiscoveryResult:
     # Note: the name of the power supply is not unique. We have seen
     # a Cisco with four entries in the MIB. So we force uniqueness
     # by appending a "/4" for ID 4 if the name is not unique
     discovered: dict[str, list[str]] = {}
-    for sid, textinfo, state, _source in section:
-        if state != "5":
-            name = cisco_sensor_item(textinfo, sid)
-            discovered.setdefault(name, []).append(sid)
+
+    for sid, textinfo in (head for *head, state, _source in section if state != "5"):
+        discovered.setdefault(item_name_from(textinfo, sid), []).append(sid)
 
     for name, entries in discovered.items():
         if len(entries) == 1:
             yield Service(item=name)
         else:
-            for entry in entries:
-                yield Service(item=f"{name} {entry}")
+            yield from (Service(item=f"{name} {entry}") for entry in entries)
 
 
 def check_cisco_power(item: str, section: StringTable) -> CheckResult:
-    for sid, textinfo, r_state, r_source in section:
-        if (
-            cisco_sensor_item(textinfo, sid) == item
-            or cisco_sensor_item(textinfo, sid) + " " + sid == item
-            or cisco_sensor_item(textinfo, sid) + "/" + sid == item
-        ):
-            state = int(r_state)
-            source = int(r_source)
+    for sid, textinfo, state_str, source_str in section:
+        item_name_base = item_name_from(textinfo, sid)
+        if item in (item_name_base, f"{item_name_base} {sid}", f"{item_name_base}/{sid}"):
+            state = int(state_str)
+            source = int(source_str)
             yield Result(
                 state={1: State.OK, 2: State.WARN}.get(state, State.CRIT),
                 summary=f"Status: {cisco_power_states[state]}, Source: {cisco_power_sources[source]}",
@@ -88,7 +125,12 @@ snmp_section_cisco_power = SimpleSNMPSection(
     detect=DETECT_CISCO,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.9.9.13.1.5.1",
-        oids=[OIDEnd(), "2", "3", "4"],
+        oids=[
+            OIDEnd(),  # becomes SID
+            "2",  # textinfo
+            "3",  # state
+            "4",  # source
+        ],
     ),
     parse_function=parse_cisco_power,
 )

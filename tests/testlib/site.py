9f1b6b875e4c97e70a1ba825f81791ff24b0edda
Matteo Stifano <matteo.stifano@checkmk.com>
2024-12-05 17:11:40 +0100

    site: Ignore crash report
    A new agent-dump has been included for the test-plugin. This is leading to
    a site-crash. Here we temporary ignore the error coming from the crash
    report.
    
    CMK-18657
    
    Change-Id: If74f0ff304d6bd3598e311a0af691e3618ad7631

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -1337,6 +1337,9 @@ class Site:
             if re.search("Cannot render negative timespan", crash_detail):
                 logger.warning("Ignored crash report due to CMK-18635!")
                 continue
+            if re.search("systime", crash_detail):
+                logger.warning("Ignored crash report. See CMK-20674")
+                continue
             pytest_check.fail(
                 f"""Crash report detected! {crash_type}: {crash_detail}.
                 See {crash_file} for more details."""

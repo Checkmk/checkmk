1f9a2c1a84466375d711951a9f0c9bba77db9950
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2024-12-05 10:56:21 +0100

    Split up ruff and pylint
    Change-Id: I3d596d37c815f51515c57cb2c58aeb9bfcd984a1

--- buildscripts/scripts/test-python3-ruff.groovy
+++ buildscripts/scripts/test-python3-ruff.groovy
@@ -1,6 +1,6 @@
 #!groovy
 
-/// file: test-python3-pylint.groovy
+/// file: test-python3-ruff.groovy
 
 def main() {
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
@@ -9,12 +9,12 @@ def main() {
         inside_container() {
             lock(label: "bzl_lock_${env.NODE_NAME.split('\\.')[0].split('-')[-1]}", quantity: 1, resource : null) {
                 test_jenkins_helper.execute_test([
-                    name       : "test-pylint",
-                    cmd        : "PYLINT_ARGS=--output-format=parseable make -C tests test-pylint",
-                    output_file: "pylint.txt",
+                    name       : "test-ruff",
+                    cmd        : "RUFF_ARGS=--output-format=pylint make -C tests test-ruff",
+                    output_file: "ruff.txt",
                 ]);
             }
-            test_jenkins_helper.analyse_issues("PYLINT", "pylint.txt");
+            test_jenkins_helper.analyse_issues("PYLINT", "ruff.txt");
         }
     }
 }

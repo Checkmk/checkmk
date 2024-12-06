164d2ed38482d9e7cd86c92a1e9909741288a0ff
Jörg Herbel <joerg.herbel@checkmk.com>
2024-12-06 15:16:56 +0100

    RMK: Move all gui code into common sub-module
    The goal is to avoid transitive imports of `cmk.gui` in `cmk.base` and vice
    versa. To this end, we encapsulate all Robotmk gui code into
    `cmk.cee.robotmk.gui`.
    
    Change-Id: I78dbd2f0dc2552e8fc85d9687f7ce7ab43d3a891

--- tests/pylint/checker_cmk_module_layers.py
+++ tests/pylint/checker_cmk_module_layers.py
@@ -274,9 +274,7 @@ def _allow_for_gui_cee(
             _in_component(imported=imported, component=Component("cmk.checkengine")),
             _in_component(imported=imported, component=Component("cmk.fetchers")),
             _in_component(imported=imported, component=Component("cmk.cee.bakery")),
-            _in_component(
-                imported=imported, component=Component("cmk.cee.robotmk.gui_registration")
-            ),
+            _in_component(imported=imported, component=Component("cmk.cee.robotmk.gui")),
         )
     )
 
@@ -297,9 +295,7 @@ def _allow_for_gui_cce(
             _in_component(imported=imported, component=Component("cmk.checkengine")),
             _in_component(imported=imported, component=Component("cmk.fetchers")),
             _in_component(imported=imported, component=Component("cmk.cee.bakery")),
-            _in_component(
-                imported=imported, component=Component("cmk.cee.robotmk.gui_registration")
-            ),
+            _in_component(imported=imported, component=Component("cmk.cee.robotmk.gui")),
         )
     )
 
@@ -319,9 +315,7 @@ def _allow_for_gui_cme(
             _in_component(imported=imported, component=Component("cmk.checkengine")),
             _in_component(imported=imported, component=Component("cmk.fetchers")),
             _in_component(imported=imported, component=Component("cmk.cee.bakery")),
-            _in_component(
-                imported=imported, component=Component("cmk.cee.robotmk.gui_registration")
-            ),
+            _in_component(imported=imported, component=Component("cmk.cee.robotmk.gui")),
         )
     )
 
@@ -338,9 +332,7 @@ def _allow_for_gui_cse(
                 _in_component(imported=imported, component=Component("cmk.gui"))
                 and not _is_a_plugin_import(imported=imported)
             ),
-            _in_component(
-                imported=imported, component=Component("cmk.cee.robotmk.gui_registration")
-            ),
+            _in_component(imported=imported, component=Component("cmk.cee.robotmk.gui")),
         )
     )
 

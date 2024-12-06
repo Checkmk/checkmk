265cde9cc8f22804b0180f488a2657255131794b
Luka Racic <luka.racic@checkmk.com>
2024-12-07 00:49:47 +0100

    AudioCodes SIP Calls: Introduce new check plugin
    CMK-20248
    
    Change-Id: I0df86929da724441f2364e8f9d9652ef6ee80b43

---
+++ cmk/plugins/audiocodes/graphing/sip_calls_fax_attempted_calls.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.graphing.v1 import graphs, metrics, Title
+
+UNIT_PER_SECOND = metrics.Unit(metrics.DecimalNotation("/s"))
+
+metric_audiocodes_tel2ip_fax_attempted_calls = metrics.Metric(
+    name="audiocodes_tel2ip_fax_attempted_calls",
+    title=Title("Number of Attempted SIP/H323 fax calls"),
+    unit=UNIT_PER_SECOND,
+    color=metrics.Color.GREEN,
+)
+metric_audiocodes_ip2tel_fax_attempted_calls = metrics.Metric(
+    name="audiocodes_ip2tel_fax_attempted_calls",
+    title=Title("Number of Attempted SIP/H323 fax calls"),
+    unit=UNIT_PER_SECOND,
+    color=metrics.Color.PURPLE,
+)
+graph_audiocodes_sip_calls_fax_attempted_calls = graphs.Bidirectional(
+    name="audiocodes_sip_calls_fax_attempted_calls",
+    title=Title("Number of Attempted SIP/H323 fax calls"),
+    lower=graphs.Graph(
+        name="tel2ip_fax_attempted_calls",
+        title=Title("Tel2IP"),
+        compound_lines=[
+            "audiocodes_tel2ip_fax_attempted_calls",
+        ],
+    ),
+    upper=graphs.Graph(
+        name="ip2tel_fax_attempted_calls",
+        title=Title("IP2Tel"),
+        compound_lines=[
+            "audiocodes_ip2tel_fax_attempted_calls",
+        ],
+    ),
+)

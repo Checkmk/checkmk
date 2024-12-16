3eae6fee1955728a41d766f995bf956d0b8d8c97
Leonardo Petrora <leonardo.petrora@checkmk.com>
2024-12-16 13:30:10 +0100

    quick_setup: introduce endpoint to fetch action result
    Depends on https://review.lan.tribe29.com/c/check_mk/+/91436
    
    CMK-20707
    
    Change-Id: Ia3380c9946f91ff9e7421352f29a09c80c3ede27

--- packages/cmk-frontend-vue/src/quick-setup/rest-api/constants.ts
+++ packages/cmk-frontend-vue/src/quick-setup/rest-api/constants.ts
@@ -8,6 +8,10 @@ const API_ROOT = 'api/1.0'
 /** @constant {string} GET_QUICK_SETUP_OVERVIEW_URL - Endpoint used to fetch the quick setup overview and first stage */
 export const GET_QUICK_SETUP_OVERVIEW_URL = `${API_ROOT}/objects/quick_setup/{QUICK_SETUP_ID}`
 
+/** @constant {string} GET_QUICK_SETUP_STAGE_ACTION_JOB_RESULT_URL - Fetch the Quick setup stage action background job result
+ */
+export const GET_QUICK_SETUP_STAGE_ACTION_JOB_RESULT_URL = `${API_ROOT}/objects/quick_setup_stage_action_result/{JOB_ID}`
+
 /** @constant {string} VALIDATE_QUICK_SETUP_STAGE_URL - Endpoint used to validate a stage and get the next stage */
 export const VALIDATE_QUICK_SETUP_STAGE_URL = `${API_ROOT}/domain-types/quick_setup/collections/all`
 

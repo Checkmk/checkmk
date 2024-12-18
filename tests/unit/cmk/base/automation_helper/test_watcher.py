395c9d2dddc45921ecb4a114b8ebfc602f253b12
Jörg Herbel <joerg.herbel@checkmk.com>
2024-12-18 09:04:03 +0100

    Automation helper: Polish watcher unit tests a bit
    Change-Id: I110a61170ff80981ac96ad2942e5c5c10a73dfa5

--- tests/unit/cmk/base/automation_helper/test_watcher.py
+++ tests/unit/cmk/base/automation_helper/test_watcher.py
@@ -31,57 +31,55 @@ from cmk.base.automation_helper._watcher import (
     run,
 )
 
-MK_PATTERN: Final = "*.mk"
-MK_FILE: Final = "foo.mk"
-TXT_FILE: Final = "foo.txt"
-DIRECTORY: Final = "local"
+_WATCHED_MK_PATTERN: Final = "*.mk"
+_WATCHED_TXT_FILE: Final = "foo.txt"
+_WATCHED_DIRECTORY: Final = "some_dir"
+_MK_FILE: Final = "foo.mk"
 
 
 @pytest.fixture(name="cache")
 def get_cache() -> Generator[Cache]:
-    cache = Cache.setup(client=FakeRedis())
-    yield cache
+    yield Cache.setup(client=FakeRedis())
 
 
-@pytest.fixture(scope="function", name="file_watcher_handler")
-def get_file_watcher_handler(cache: Cache) -> _AutomationWatcherHandler:
-    return _AutomationWatcherHandler(cache=cache, patterns=[MK_PATTERN], ignore_directories=True)
+@pytest.fixture(name="mk_file_watcher_handler")
+def get_mk_file_watcher_handler(cache: Cache) -> _AutomationWatcherHandler:
+    return _AutomationWatcherHandler(
+        cache=cache, patterns=[_WATCHED_MK_PATTERN], ignore_directories=True
+    )
 
 
-@pytest.fixture(scope="function", name="directory_watcher_handler")
+@pytest.fixture(name="directory_watcher_handler")
 def get_directory_watcher_handler(cache: Cache) -> _AutomationWatcherHandler:
-    return _AutomationWatcherHandler(cache=cache, patterns=[DIRECTORY], ignore_directories=False)
+    return _AutomationWatcherHandler(
+        cache=cache, patterns=[_WATCHED_DIRECTORY], ignore_directories=False
+    )
 
 
-@pytest.fixture(scope="function", name="target_directory")
-def get_target_directory(tmp_path: Path) -> Path:
-    target_directory = tmp_path / DIRECTORY
-    target_directory.mkdir(parents=True)
-    return target_directory
+@pytest.fixture(name="target_mk_file")
+def get_mk_target_file(tmp_path: Path) -> Path:
+    return tmp_path / _MK_FILE
 
 
-@pytest.fixture(scope="function", name="target_file")
-def get_target_file(target_directory: Path) -> Path:
-    return target_directory / MK_FILE
-
-
-@pytest.fixture(scope="function", name="observer")
-def get_observer(cache: Cache, target_directory: Path) -> ContextManager:
-    schedules: list[Schedule] = [
-        Schedule(
-            path=target_directory,
-            ignore_directories=True,
-            recursive=True,
-            patterns=[MK_PATTERN],
-        ),
-        Schedule(
-            path=target_directory,
-            ignore_directories=True,
-            recursive=True,
-            patterns=[TXT_FILE],
-        ),
-    ]
-    return run(schedules, cache)
+@pytest.fixture(name="observer")
+def get_observer(cache: Cache, tmp_path: Path) -> ContextManager:
+    return run(
+        [
+            Schedule(
+                path=tmp_path,
+                ignore_directories=True,
+                recursive=True,
+                patterns=[_WATCHED_MK_PATTERN],
+            ),
+            Schedule(
+                path=tmp_path,
+                ignore_directories=True,
+                recursive=True,
+                patterns=[_WATCHED_TXT_FILE],
+            ),
+        ],
+        cache,
+    )
 
 
 def wait_for_observer_log_output(
@@ -100,50 +98,50 @@ def wait_for_observer_log_output(
 
 @pytest.mark.xfail(reason="Cannot reproduce the 'moved' event with shutil")
 def test_observer_handles_moved_mk_file(
-    caplog: pytest.LogCaptureFixture, tmp_path: Path, observer: ContextManager, target_file: Path
+    caplog: pytest.LogCaptureFixture, tmp_path: Path, observer: ContextManager, target_mk_file: Path
 ) -> None:
-    target_file.write_bytes(b"hello")
-    tmp_file = tmp_path / f"{MK_FILE}.tmp"
+    target_mk_file.write_bytes(b"hello")
+    tmp_file = tmp_path / f"{_MK_FILE}.tmp"
     tmp_file.write_bytes(b"goodbye")
 
     with caplog.at_level(logging.INFO), observer:
-        shutil.move(tmp_file, target_file)
+        shutil.move(tmp_file, target_mk_file)
         wait_for_observer_log_output(caplog, "foo.mk (overwritten)")
 
 
 def test_observer_handles_created_mk_file(
-    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_file: Path
+    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_mk_file: Path
 ) -> None:
     with caplog.at_level(logging.INFO), observer:
-        target_file.touch()
+        target_mk_file.touch()
         wait_for_observer_log_output(caplog, "foo.mk (created)")
 
 
 def test_observer_handles_modified_mk_file(
-    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_file: Path
+    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_mk_file: Path
 ) -> None:
-    target_file.touch()
+    target_mk_file.touch()
 
     with caplog.at_level(logging.INFO), observer:
-        target_file.write_bytes(b"hello")
+        target_mk_file.write_bytes(b"hello")
         wait_for_observer_log_output(caplog, "foo.mk (modified)")
 
 
 def test_observer_handles_deleted_mk_file(
-    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_file: Path
+    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_mk_file: Path
 ) -> None:
-    target_file.touch()
+    target_mk_file.touch()
 
     with caplog.at_level(logging.INFO), observer:
-        target_file.unlink()
+        target_mk_file.unlink()
         wait_for_observer_log_output(caplog, "foo.mk (deleted)")
 
 
 def test_observer_also_handles_txt_files(
-    caplog: pytest.LogCaptureFixture, observer: ContextManager, target_directory: Path
+    caplog: pytest.LogCaptureFixture, observer: ContextManager, tmp_path: Path
 ) -> None:
     with caplog.at_level(logging.INFO), observer:
-        (target_directory / TXT_FILE).touch()
+        (tmp_path / _WATCHED_TXT_FILE).touch()
         wait_for_observer_log_output(caplog, "foo.txt (created)")
 
 
@@ -151,23 +149,23 @@ def test_observer_also_handles_txt_files(
     "event, output",
     [
         pytest.param(
-            FileMovedEvent(src_path="", dest_path=MK_FILE),
+            FileMovedEvent(src_path="", dest_path=_MK_FILE),
             "foo.mk (overwritten)",
             id="file overwritten",
         ),
-        pytest.param(FileCreatedEvent(src_path=MK_FILE), "foo.mk (created)", id="file created"),
-        pytest.param(FileModifiedEvent(src_path=MK_FILE), "foo.mk (modified)", id="file modified"),
-        pytest.param(FileDeletedEvent(src_path=MK_FILE), "foo.mk (deleted)", id="file deleted"),
+        pytest.param(FileCreatedEvent(src_path=_MK_FILE), "foo.mk (created)", id="file created"),
+        pytest.param(FileModifiedEvent(src_path=_MK_FILE), "foo.mk (modified)", id="file modified"),
+        pytest.param(FileDeletedEvent(src_path=_MK_FILE), "foo.mk (deleted)", id="file deleted"),
     ],
 )
 def test_automation_watcher_logging_pattern_match(
     caplog: pytest.LogCaptureFixture,
-    file_watcher_handler: _AutomationWatcherHandler,
+    mk_file_watcher_handler: _AutomationWatcherHandler,
     event: FileSystemEvent,
     output: str,
 ) -> None:
     with caplog.at_level(logging.INFO):
-        file_watcher_handler.dispatch(event)
+        mk_file_watcher_handler.dispatch(event)
 
     assert output in caplog.text
 
@@ -175,19 +173,19 @@ def test_automation_watcher_logging_pattern_match(
 @pytest.mark.parametrize(
     "event",
     [
-        pytest.param(FileMovedEvent(src_path="", dest_path=TXT_FILE), id="no match overwritten"),
-        pytest.param(FileCreatedEvent(src_path=TXT_FILE), id="no match created"),
-        pytest.param(FileModifiedEvent(src_path=TXT_FILE), id="no match modified"),
-        pytest.param(FileDeletedEvent(src_path=TXT_FILE), id="no match deleted"),
+        pytest.param(FileMovedEvent(src_path="", dest_path="foo.bar"), id="no match overwritten"),
+        pytest.param(FileCreatedEvent(src_path="foo.bar"), id="no match created"),
+        pytest.param(FileModifiedEvent(src_path="foo.bar"), id="no match modified"),
+        pytest.param(FileDeletedEvent(src_path="foo.bar"), id="no match deleted"),
     ],
 )
 def test_automation_watcher_logging_no_match(
     caplog: pytest.LogCaptureFixture,
-    file_watcher_handler: _AutomationWatcherHandler,
+    mk_file_watcher_handler: _AutomationWatcherHandler,
     event: FileSystemEvent,
 ) -> None:
     with caplog.at_level(logging.INFO):
-        file_watcher_handler.dispatch(event)
+        mk_file_watcher_handler.dispatch(event)
 
     assert not caplog.text
 
@@ -196,13 +194,25 @@ def test_automation_watcher_logging_no_match(
     "event, output",
     [
         pytest.param(
-            DirMovedEvent(src_path="", dest_path=DIRECTORY),
-            "local (overwritten)",
+            DirMovedEvent(src_path="", dest_path=_WATCHED_DIRECTORY),
+            f"{_WATCHED_DIRECTORY} (overwritten)",
             id="dir overwritten",
         ),
-        pytest.param(DirCreatedEvent(src_path=DIRECTORY), "local (created)", id="dir created"),
-        pytest.param(DirModifiedEvent(src_path=DIRECTORY), "local (modified)", id="dir modified"),
-        pytest.param(DirDeletedEvent(src_path=DIRECTORY), "local (deleted)", id="dir deleted"),
+        pytest.param(
+            DirCreatedEvent(src_path=_WATCHED_DIRECTORY),
+            f"{_WATCHED_DIRECTORY} (created)",
+            id="dir created",
+        ),
+        pytest.param(
+            DirModifiedEvent(src_path=_WATCHED_DIRECTORY),
+            f"{_WATCHED_DIRECTORY} (modified)",
+            id="dir modified",
+        ),
+        pytest.param(
+            DirDeletedEvent(src_path=_WATCHED_DIRECTORY),
+            f"{_WATCHED_DIRECTORY} (deleted)",
+            id="dir deleted",
+        ),
     ],
 )
 def test_automation_watcher_logging_directory_match(
@@ -221,20 +231,20 @@ def test_automation_watcher_logging_directory_match(
     "event",
     [
         pytest.param(
-            DirMovedEvent(src_path="", dest_path=DIRECTORY),
+            DirMovedEvent(src_path="", dest_path=_WATCHED_DIRECTORY),
             id="no match dir overwritten",
         ),
-        pytest.param(DirCreatedEvent(src_path=DIRECTORY), id="no match dir created"),
-        pytest.param(DirModifiedEvent(src_path=DIRECTORY), id="no match dir modified"),
-        pytest.param(DirDeletedEvent(src_path=DIRECTORY), id="no match dir deleted"),
+        pytest.param(DirCreatedEvent(src_path=_WATCHED_DIRECTORY), id="no match dir created"),
+        pytest.param(DirModifiedEvent(src_path=_WATCHED_DIRECTORY), id="no match dir modified"),
+        pytest.param(DirDeletedEvent(src_path=_WATCHED_DIRECTORY), id="no match dir deleted"),
     ],
 )
 def test_automation_watcher_logging_directories_ignored(
     caplog: pytest.LogCaptureFixture,
-    file_watcher_handler: _AutomationWatcherHandler,
+    mk_file_watcher_handler: _AutomationWatcherHandler,
     event: FileSystemEvent,
 ) -> None:
     with caplog.at_level(logging.INFO):
-        file_watcher_handler.dispatch(event)
+        mk_file_watcher_handler.dispatch(event)
 
     assert not caplog.text

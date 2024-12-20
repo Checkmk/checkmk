Checkmk/checkmk

commit 9bd90ae4522a5875947bff47aebb47281ef52f00

Improve typing for crash reporting
also fix an error where GUI crash reports have been
created without the required request context.

Now the request context is being set as unknown.

CMK-19816

Change-Id: Id1b43bca34cd65ba127b65df05e93279e3267068

cmk/gui/crash_reporting/helpers.py
@@ -4,7 +4,8 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from collections.abc import Sequence
+from traceback import FrameSummary
 
 
-def local_files_involved_in_crash(exc_traceback: Sequence[tuple[str, int, str, str]]) -> list[str]:
+def local_files_involved_in_crash(exc_traceback: Sequence[FrameSummary]) -> list[str]:
     return [filepath for filepath, _lineno, _func, _line in exc_traceback if "/local/" in filepath]

cmk/gui/crash_reporting/pages.py
@@ -12,7 +12,7 @@ import tarfile
 import time
 import traceback
 from collections.abc import Iterator, Mapping
-from typing import Final, TypedDict
+from typing import Final, Sequence, TypedDict
 
 import livestatus
 from livestatus import SiteId
@@ -396,7 +396,9 @@ class PageCrash(ABCCrashReportPage):
 
         _crash_row(_("Crash Type"), info["crash_type"], odd=False, legend=True)
         _crash_row(
-            _("Time"), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(info["time"])), odd=True
+            _("Time"),
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(info["time"]))),
+            odd=True,
         )
         _crash_row(_("Operating System"), info["os"], False)
         _crash_row(_("Checkmk Version"), info["version"], True)
@@ -409,7 +411,7 @@ class PageCrash(ABCCrashReportPage):
 
         html.close_table()
 
-    def _format_traceback(self, tb: list[traceback.FrameSummary]) -> str:
+    def _format_traceback(self, tb: Sequence[traceback.FrameSummary]) -> str:
         return "".join(traceback.format_list(tb))
 
     def _show_crash_report_details(self, crash_info: CrashInfo, row: CrashReportRow) -> None:
@@ -512,8 +514,9 @@ class ReportRendererCheck(ABCReportRenderer):
     def page_menu_entries_related_monitoring(
         self, crash_info: CrashInfo, site_id: SiteId
     ) -> Iterator[PageMenuEntry]:
-        host = crash_info["details"]["host"]
-        service = crash_info["details"]["description"]
+        details = crash_info["details"]
+        host = details["host"]
+        service = details["description"]
 
         host_url = makeuri(
             request,
@@ -554,7 +557,7 @@ class ReportRendererCheck(ABCReportRenderer):
         _show_agent_output(row)
 
     def _show_crashed_check_details(self, info: CrashInfo) -> None:
-        def format_bool(val):
+        def format_bool(val: bool | None) -> str:
             return {
                 True: _("Yes"),
                 False: _("No"),

packages/cmk-ccc/cmk/ccc/crash_reporting.py
@@ -17,16 +17,48 @@ import sys
 import traceback
 import urllib.parse
 import uuid
-from collections.abc import Iterator, Mapping
+from collections.abc import Iterator
 from contextlib import suppress
 from itertools import islice
 from pathlib import Path
-from typing import Any, Final, Self
+from typing import Any, Final, Generic, NotRequired, Self, Sequence, TypedDict, TypeVar
 
 import cmk.ccc.plugin_registry
 from cmk.ccc import store
 
-CrashInfo = dict[str, Any]  # TODO: improve this type
+
+class BaseDetails(TypedDict):
+    argv: list
+    env: dict
+
+
+class VersionInfo(TypedDict):
+    core: str
+    python_version: str
+    edition: str
+    python_paths: Sequence[str]
+    version: str
+    time: float
+    os: str
+
+
+T = TypeVar("T")
+
+
+class ContactDetails(TypedDict):
+    name: NotRequired[str]
+    email: NotRequired[str]
+
+
+class CrashInfo(Generic[T], VersionInfo):
+    exc_type: str | None
+    crash_type: str
+    exc_traceback: NotRequired[Sequence[traceback.FrameSummary]]
+    local_vars: str
+    details: T
+    exc_value: str
+    contact: NotRequired[ContactDetails]
+    id: str
 
 
 # The default JSON encoder raises an exception when detecting unknown types. For the crash
@@ -117,7 +149,11 @@ class CrashReportStore:
                 crash_dir.rmdir()
 
 
-class ABCCrashReport(abc.ABC):
+class SerializedCrashReport(TypedDict):
+    crash_info: CrashInfo
+
+
+class ABCCrashReport(Generic[T], abc.ABC):
     """Base class for the component specific crash report types"""
 
     def __init__(self, crashdir: Path, crash_info: CrashInfo) -> None:
@@ -134,8 +170,8 @@ class ABCCrashReport(abc.ABC):
     def from_exception(
         cls,
         crashdir: Path,
-        version_info: dict[str, object],
-        details: Mapping[str, Any] | None = None,
+        version_info: VersionInfo,
+        details: T | None = None,
         type_specific_attributes: dict[str, Any] | None = None,
     ) -> Self:
         """Create a crash info object from the current exception context
@@ -146,13 +182,13 @@ class ABCCrashReport(abc.ABC):
                                    are set as attributes on the crash objects.
         """
         attributes = {
-            "crash_info": _get_generic_crash_info(cls.type(), version_info, details or {}),
+            "crash_info": _get_generic_crash_info(cls.type(), version_info, details),
         }
         attributes |= type_specific_attributes or {}
         return cls(crashdir, **attributes)
 
     @classmethod
-    def deserialize(cls, crashdir: Path, serialized: dict[str, dict[str, str]]) -> ABCCrashReport:
+    def deserialize(cls, crashdir: Path, serialized: SerializedCrashReport) -> ABCCrashReport:
         """Deserialize the object"""
         class_ = crash_report_registry[serialized["crash_info"]["crash_type"]]
         return class_(crashdir, **serialized)
@@ -199,8 +235,8 @@ class ABCCrashReport(abc.ABC):
 
 def _get_generic_crash_info(
     type_name: str,
-    version_info: dict[str, object],
-    details: Mapping[str, Any],
+    version_info: VersionInfo,
+    details: T,
 ) -> CrashInfo:
     """Produces the crash info data structure.
 
@@ -210,19 +246,22 @@ def _get_generic_crash_info(
 
     tb_list = traceback.extract_tb(exc_traceback)
 
-    version_info.update(
-        {
-            "id": str(uuid.uuid1()),
-            "crash_type": type_name,
-            "exc_type": exc_type.__name__ if exc_type else None,
-            "exc_value": str(exc_value),
-            # Py3: Make traceback.FrameSummary serializable
-            "exc_traceback": [tuple(e) for e in tb_list],
-            "local_vars": _get_local_vars_of_last_exception(),
-            "details": details,
-        }
+    return CrashInfo(
+        id=str(uuid.uuid1()),
+        crash_type=type_name,
+        exc_type=exc_type.__name__ if exc_type else None,
+        exc_value=str(exc_value),
+        exc_traceback=[e for e in tb_list],
+        local_vars=_get_local_vars_of_last_exception(),
+        details=details,
+        core=version_info["core"],
+        python_version=version_info["python_version"],
+        edition=version_info["edition"],
+        python_paths=version_info["python_paths"],
+        version=version_info["version"],
+        time=version_info["time"],
+        os=version_info["os"],
     )
-    return version_info
 
 
 def _get_local_vars_of_last_exception() -> str:

cmk/ec/crash_reporting.py
@@ -10,7 +10,7 @@ CrashReportStore = cmk.ccc.crash_reporting.CrashReportStore
 
 
 @cmk.ccc.crash_reporting.crash_report_registry.register
-class ECCrashReport(cmk.ccc.crash_reporting.ABCCrashReport):
+class ECCrashReport(cmk.ccc.crash_reporting.ABCCrashReport[cmk.ccc.crash_reporting.VersionInfo]):
     @classmethod
     def type(cls) -> str:
         return "ec"

cmk/special_agents/v0_unstable/crash_reporting.py
@@ -8,6 +8,7 @@ from typing import Literal
 
 import cmk.ccc.version as cmk_version
 from cmk.ccc import crash_reporting
+from cmk.ccc.crash_reporting import VersionInfo
 
 import cmk.utils.paths
 
@@ -15,7 +16,7 @@ CrashReportStore = crash_reporting.CrashReportStore
 
 
 @crash_reporting.crash_report_registry.register
-class AgentCrashReport(crash_reporting.ABCCrashReport):
+class AgentCrashReport(crash_reporting.ABCCrashReport[VersionInfo]):
     @classmethod
     def type(cls) -> Literal["agent"]:
         return "agent"

bin/check_mk
@@ -28,7 +28,13 @@ if not os.environ.get("OMD_SITE"):
 
 import cmk.ccc.debug
 import cmk.ccc.version as cmk_version
-from cmk.ccc.crash_reporting import ABCCrashReport, crash_report_registry, CrashReportStore
+from cmk.ccc.crash_reporting import (
+    ABCCrashReport,
+    BaseDetails,
+    crash_report_registry,
+    CrashReportStore,
+    VersionInfo,
+)
 from cmk.ccc.exceptions import MKBailOut, MKGeneralException, MKTerminate
 from cmk.ccc.site import get_omd_config, omd_site, resource_attributes_from_config
 
@@ -74,7 +80,7 @@ if trace_context := trace.extract_context_from_environment(dict(os.environ)):
 
 # We probably don't really need to register that here, do we?
 @crash_report_registry.register
-class CrashReport(ABCCrashReport):
+class CrashReport(ABCCrashReport[BaseDetails]):
     @classmethod
     def type(cls) -> str:
         return "base"
@@ -83,8 +89,8 @@ class CrashReport(ABCCrashReport):
     def from_exception(
         cls,
         crashdir: Path,
-        version_info: dict[str, object],
-        _details: object = None,
+        version_info: VersionInfo,
+        _details: BaseDetails | None = None,
         _type_specific_attributes: object = None,
     ) -> Self:
         return super().from_exception(

bin/cmk-piggyback-hub
@@ -9,6 +9,7 @@ from typing import Literal
 
 import cmk.ccc.version as cmk_version
 from cmk.ccc import crash_reporting
+from cmk.ccc.crash_reporting import VersionInfo
 
 import cmk.utils.paths
 
@@ -18,7 +19,7 @@ CrashReportStore = crash_reporting.CrashReportStore
 
 
 @crash_reporting.crash_report_registry.register
-class PiggybackHubCrashReport(crash_reporting.ABCCrashReport):
+class PiggybackHubCrashReport(crash_reporting.ABCCrashReport[VersionInfo]):
     @classmethod
     def type(cls) -> Literal["piggyback-hub"]:
         return "piggyback-hub"

cmk/base/diagnostics.py
@@ -502,10 +502,18 @@ class GeneralDiagnosticsElement(ABCDiagnosticsElementJSONDump):
 
     def _collect_infos(self) -> DiagnosticsElementJSONResult:
         version_infos = cmk_version.get_general_version_infos(omd_root)
-        version_infos["arch"] = platform.machine()
-        time_obj = datetime.fromtimestamp(version_infos.get("time", 0))
-        version_infos["time_human_readable"] = time_obj.isoformat(sep=" ")
-        return version_infos
+        time_obj = datetime.fromtimestamp(version_infos.get("time", 0.0))
+        return {
+            "arch": platform.machine(),
+            "time_human_readable": time_obj.isoformat(sep=" "),
+            "time": version_infos["time"],
+            "os": version_infos["os"],
+            "version": version_infos["version"],
+            "edition": version_infos["edition"],
+            "core": version_infos["core"],
+            "python_version": version_infos["python_version"],
+            "python_paths": list(version_infos["python_paths"]),
+        }
 
 
 class PerfDataDiagnosticsElement(ABCDiagnosticsElementJSONDump):

commit 5631a094f7db3f29ce6f43c01934844b73998f7f

Fix crash reporting
Crash reporting was broken since 9bd90ae4522a5875947bff47aebb47281ef52f00
because the exception traceback was not properly serialized.

CMK-19816

Change-Id: If71ed904c4c73a07c47827ed1f3b9df84d313759

cmk/gui/crash_reporting/helpers.py
@@ -4,8 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from collections.abc import Sequence
-from traceback import FrameSummary
 
 
-def local_files_involved_in_crash(exc_traceback: Sequence[FrameSummary]) -> list[str]:
+def local_files_involved_in_crash(exc_traceback: Sequence[tuple[str, int, str, str]]) -> list[str]:
     return [filepath for filepath, _lineno, _func, _line in exc_traceback if "/local/" in filepath]

cmk/gui/crash_reporting/pages.py
@@ -411,7 +411,7 @@ class PageCrash(ABCCrashReportPage):
 
         html.close_table()
 
-    def _format_traceback(self, tb: Sequence[traceback.FrameSummary]) -> str:
+    def _format_traceback(self, tb: Sequence[tuple[str, int, str, str]]) -> str:
         return "".join(traceback.format_list(tb))
 
     def _show_crash_report_details(self, crash_info: CrashInfo, row: CrashReportRow) -> None:

packages/cmk-ccc/cmk/ccc/crash_reporting.py
@@ -53,7 +53,7 @@ class ContactDetails(TypedDict):
 class CrashInfo(Generic[T], VersionInfo):
     exc_type: str | None
     crash_type: str
-    exc_traceback: NotRequired[Sequence[traceback.FrameSummary]]
+    exc_traceback: NotRequired[Sequence[tuple[str, int, str, str]]]
     local_vars: str
     details: T
     exc_value: str
@@ -251,7 +251,7 @@ def _get_generic_crash_info(
         crash_type=type_name,
         exc_type=exc_type.__name__ if exc_type else None,
         exc_value=str(exc_value),
-        exc_traceback=[e for e in tb_list],
+        exc_traceback=[tuple(e) for e in tb_list],
         local_vars=_get_local_vars_of_last_exception(),
         details=details,
         core=version_info["core"],
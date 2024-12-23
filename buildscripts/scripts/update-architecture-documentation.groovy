55050acc0480d5739e1a252009f1189b916d5c5b
Niko Wenselowski <niko.wenselowski@checkmk.com>
2024-12-23 11:48:09 +0100

    doc/documentation: Switch sphinx builder to html
    With htmlhelp no search.html get's generated.
    
    See https://tribe29.slack.com/archives/C01EA6ZBG58/p1732520679625799
    
    Change-Id: I6d975e0a34f30fdef6b6a5ef3fbf44909d406ec5

--- buildscripts/scripts/update-architecture-documentation.groovy
+++ buildscripts/scripts/update-architecture-documentation.groovy
@@ -4,16 +4,21 @@
 
 def main() {
     dir("${checkout_dir}") {
+        // Sphinx supports many different builders.
+        // We have to use the same for creating new documentation and for
+        // copying the documentation over.
+        def sphinx_builder = "html"
+
         stage("Update") {
             inside_container() {
-                sh("make -C doc/documentation htmlhelp");
+                sh("make -C doc/documentation ${sphinx_builder}");
             }
         }
 
         stage("Deploy") {
             withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {    // groovylint-disable DuplicateMapLiteral
                 sh("""
-                    scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} doc/documentation/_build/htmlhelp/* ${DEV_DOCS_URL}/devdoc
+                    scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} doc/documentation/_build/${sphinx_builder}/* ${DEV_DOCS_URL}/devdoc
                 """);
             }
         }

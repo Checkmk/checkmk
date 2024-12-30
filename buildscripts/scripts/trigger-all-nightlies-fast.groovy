799777584ea94efaa17212199c17a784698e6954
Niko Wenselowski <niko.wenselowski@checkmk.com>
2024-12-30 10:59:02 +0100

    trigger-all-nightlies-fast: log excluded editions...
    ...instead of silently skipping them.
    
    Change-Id: Ibde01cfb85fa34620dd6e1211905d0e511c54b1a

--- buildscripts/scripts/trigger-all-nightlies-fast.groovy
+++ buildscripts/scripts/trigger-all-nightlies-fast.groovy
@@ -44,6 +44,8 @@ def main() {
                 // see checkmk_ci decision log
                 if (edition != "cloud" && edition != "managed") {
                     build(job: "${base_folder}/trigger-cmk-build-chain-${edition}", parameters: this_job_parameters);
+                } else {
+                    println("Not triggering a job for edition ${edition}.");
                 }
             }
         }

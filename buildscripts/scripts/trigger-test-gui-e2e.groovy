89707e7e5455041c2319f42657d3c44f9e63cbe6
Frans Fürst <frans.fuerst@checkmk.com>
2025-01-10 11:55:21 +0100

    all pipelines: adds selected_editions_default with params from gerrit comments
    CMK-20807
    
    Including fixup of CMK-21152
    
    Change-Id: I2127c3b20d0b8d01c653535f7bc354a0edc1ac86

--- buildscripts/scripts/trigger-test-gui-e2e.groovy
+++ buildscripts/scripts/trigger-test-gui-e2e.groovy
@@ -5,72 +5,43 @@
 /// Runs `test-gui-e2e-f12less` for specified editions
 /// in separate builds paralelly
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-import java.util.Base64
-import groovy.json.JsonSlurperClassic
-
-/// Process Gerrit comments with embedded arguments
-/// - future: allow YAML rather than JSON
-/// - future: also parse git commit message
-/// Format:
-/// start: trigger-test-gui-e2e
-/// ---
-/// {
-///   "editions": ["cse", "cee"]
-/// }
-/// ---
-///
-def arguments_from_comments() {
-    def comment_msg = new String(Base64.getDecoder().decode(env.GERRIT_EVENT_COMMENT_TEXT ?: ""));
-    currentBuild.description += "<br>Gerrit comment:<br>---<br><b>${comment_msg}</b><br>---";
-
-    def matcher = comment_msg =~ /(?s)\n---\n(.*)\n---/;
-    if (! matcher.find()) {
-        return [];
-    }
-    def arg_text = matcher.group(1);
-    currentBuild.description += "<br>Args from comment:<br>---<br><tt><b>${arg_text.replace("\n", "<br>")}</b></tt><br>---";
-    return (new groovy.json.JsonSlurperClassic()).parseText(arg_text);
-}
 
 def main() {
     /// make sure the listed parameters are set
     check_job_parameters([
         "CIPARAM_OVERRIDE_EDITIONS",
     ]);
-    /// Might also be taken from editions.yml - there we also have 'saas' and 'raw' but
-    /// AFAIK there is no way to extract the editions we want to test generically, so we
-    /// hard-code these:
-    def editions_from_comment = arguments_from_comments()["editions"].collect {
-        [
-            "cee": "enterpise",
-            "cre": "raw",
-            "cme": "managed",
-            "cse": "saas",
-            "cce": "cloud",
-        ].get(it, it)
-    };
+
+    def package_helper = load("${checkout_dir}/buildscripts/scripts/utils/package_helper.groovy");
+
+    /// This will get us the location to e.g. "checkmk/master" or "Testing/<name>/checkmk/master"
+    def branch_base_folder = package_helper.branch_base_folder(with_testing_prefix: true);
+
     def all_editions = ["enterprise", "cloud", "managed", "raw", "saas"];
     def selected_editions_default = ["enterprise", "cloud", "saas"];
-    def selected_editions = (
-        params.CIPARAM_OVERRIDE_EDITIONS.replaceAll(',', ' ').split(' ').grep()
-        ?: editions_from_comment
-        ?: selected_editions_default
-    );
-    def base_folder = "${currentBuild.fullProjectName.split('/')[0..-2].join('/')}";
+    def params_editions = params.CIPARAM_OVERRIDE_EDITIONS.replaceAll(',', ' ').split(' ').grep();
+    def selected_editions = [];
+    if (params_editions) {
+      selected_editions = params_editions;
+    } else if ("editions" in job_params_from_comments) {
+      selected_editions = job_params_from_comments.get("editions");
+    } else {
+      selected_editions = selected_editions_default;
+    }
 
     print(
         """
         |===== CONFIGURATION ===============================
-        |all_editions:..... │${all_editions}│
-        |selected_edtions:. │${selected_editions}│
-        |base_folder:...... │${base_folder}│
+        |all_editions:....... │${all_editions}│
+        |selected_edtions:... │${selected_editions}│
+        |branch_base_folder:. │${branch_base_folder}│
         |===================================================
         """.stripMargin());
     currentBuild.description += "<br>Selected editions: <b>${selected_editions.join(" ")}</b>";
 
     currentBuild.result = parallel(
-        all_editions.collectEntries { edition -> [
-            ("${edition}") : {
+        all_editions.collectEntries { edition ->
+            [("${edition}") : {
                 def run_condition = edition in selected_editions;
                 /// this makes sure the whole parallel thread is marked as skipped
                 if (! run_condition){
@@ -81,13 +52,13 @@ def main() {
                     condition: run_condition,
                     raiseOnError: false,
                 ) {
-                    build(
-                        job: "${base_folder}/builders/test-gui-e2e-f12less",
+                    smart_build(
+                        job: "${branch_base_folder}/builders/test-gui-e2e-f12less",
                         parameters: [
-                            string(name: 'EDITION', value: edition),
-                            string(name: 'CUSTOM_GIT_REF', value: effective_git_ref),
-                            string(name: 'CIPARAM_OVERRIDE_BUILD_NODE', value: params.CIPARAM_OVERRIDE_BUILD_NODE),
-                            string(name: 'CIPARAM_CLEANUP_WORKSPACE', value: params.CIPARAM_CLEANUP_WORKSPACE),
+                            stringParam(name: 'EDITION', value: edition),
+                            stringParam(name: 'CUSTOM_GIT_REF', value: effective_git_ref),
+                            stringParam(name: 'CIPARAM_OVERRIDE_BUILD_NODE', value: params.CIPARAM_OVERRIDE_BUILD_NODE),
+                            stringParam(name: 'CIPARAM_CLEANUP_WORKSPACE', value: params.CIPARAM_CLEANUP_WORKSPACE),
                         ]
                     );
                 }

118d2894c67d28086c0a5150b5b750e5217d6792
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-01-13 10:57:11 +0100

    Fixup closing braket for test update
    Change-Id: If0bac0dd03a2b8c87c69d3143bd8160eb7e29690

--- buildscripts/scripts/test-update.groovy
+++ buildscripts/scripts/test-update.groovy
@@ -104,7 +104,7 @@ def main() {
                         fingerprintArtifacts: true
                     );
                 }
-            ]}
+            }]
         }
     ).values().every { it } ? "SUCCESS" : "FAILURE";
 

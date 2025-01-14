9fb3639a2976229a37e6870cbe9f8baedfb8d9e8
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-01-14 18:14:58 +0100

    Outsource duplicated code of test single jobs to helper script
    CMK-20920
    
    Change-Id: I20fdf1421ee3508d663a719616e46ed9bec3711e

--- buildscripts/scripts/test-update-single-f12less.groovy
+++ buildscripts/scripts/test-update-single-f12less.groovy
@@ -38,16 +38,7 @@ def main() {
         "OTEL_EXPORTER_OTLP_ENDPOINT",
     ]);
 
-    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
-
-    def safe_branch_name = versioning.safe_branch_name(scm);
-    def branch_version = versioning.get_branch_version(checkout_dir);
-    def cmk_version_rc_aware = versioning.get_cmk_version(safe_branch_name, branch_version, params.VERSION);
-    def cmk_version = versioning.strip_rc_number_from_version(cmk_version_rc_aware);
-    def docker_tag = versioning.select_docker_tag(
-        "",                 // 'build tag'
-        safe_branch_name,   // 'branch' returns '<BRANCH>-latest'
-    );
+    def single_tests = load("${checkout_dir}/buildscripts/scripts/utils/single_tests.groovy");
 
     def version = params.VERSION;
     def distro = params.DISTRO;
@@ -61,43 +52,11 @@ def main() {
     def make_target = build_make_target(edition, cross_edition_target);
     def download_dir = "package_download";
 
-    currentBuild.description += (
-        """
-        |Run update tests for packages<br>
-        |safe_branch_name: ${safe_branch_name}<br>
-        |branch_version: ${branch_version}<br>
-        |cmk_version: ${cmk_version}<br>
-        |cmk_version_rc_aware: ${cmk_version_rc_aware}<br>
-        |docker_tag: ${docker_tag}<br>
-        |edition: ${edition}<br>
-        |distro: ${distro}<br>
-        |make_target: ${make_target}<br>
-        """.stripMargin());
+    def setup_values = single_tests.common_prepare(version: params.VERSION, make_target: make_target);
 
-    print(
-        """
-        |===== CONFIGURATION ===============================
-        |safe_branch_name:...... │${safe_branch_name}│
-        |branch_version:........ │${branch_version}│
-        |cmk_version:........... │${cmk_version}
-        |cmk_version_rc_aware:.. │${cmk_version_rc_aware}
-        |branch_version:........ │${branch_version}│
-        |docker_tag:............ │${docker_tag}│
-        |cross_edition_target:.. |${cross_edition_target}|
-        |edition:............... │${edition}│
-        |distro:................ │${distro}│
-        |checkout_dir:.......... │${checkout_dir}│
-        |make_target:........... │${make_target}│
-        |===================================================
-        """.stripMargin());
-
-    // this is a quick fix for FIPS based tests, see CMK-20851
-    def build_node = params.CIPARAM_OVERRIDE_BUILD_NODE;
-    if (build_node == "fips") {
-        // Do not start builds on FIPS node
-        println("Detected build node 'fips', switching this to 'fra'.");
-        build_node = "fra"
-    }
+    // todo: add upstream project to description
+    // todo: add error to description
+    // todo: build progress mins?
 
     stage("Prepare workspace") {
         inside_container(
@@ -109,64 +68,36 @@ def main() {
             mount_credentials: true,
             priviliged: true,
         ) {
+            single_tests.prepare_workspace(
+                cleanup: [
+                    "${WORKSPACE}/test-results",
+                    "${checkout_dir}/${download_dir}"
+                ],
+                make_venv: true
+            );
+
             dir("${checkout_dir}") {
-                // Cleanup test results directory before starting the test to prevent previous
-                // runs somehow affecting the current run.
-                sh("rm -rf ${WORKSPACE}/test-results");
-
-                /// remove downloaded packages since they consume dozens of MiB
-                sh("""rm -rf "${checkout_dir}/${download_dir}" """);
-
-                // Initialize our virtual environment before parallelization
-                sh("make .venv");
-
                 stage("Fetch Checkmk package") {
-                    upstream_build(
-                        relative_job_name: "builders/build-cmk-distro-package",
-                        build_params: [
-                            /// currently CUSTOM_GIT_REF must match, but in the future
-                            /// we should define dependency paths for build-cmk-distro-package
-                            CUSTOM_GIT_REF: cmd_output("git rev-parse HEAD"),
-                            EDITION: edition,
-                            DISTRO: distro,
-                        ],
-                        build_params_no_check: [
-                            CIPARAM_OVERRIDE_BUILD_NODE: build_node,
-                        ],
-                        dest: download_dir,
-                    );
+                    single_tests.fetch_package(edition: edition, distro: distro, download_dir: download_dir);
                 }
                 try {
                     stage("Run `make ${make_target}`") {
                         dir("${checkout_dir}/tests") {
-                            docker.withRegistry(DOCKER_REGISTRY, "nexus") {
-                                sh("""
-                                    RESULT_PATH='${WORKSPACE}/test-results/${distro}' \
-                                    EDITION='${edition}' \
-                                    DOCKER_TAG='${docker_tag}' \
-                                    VERSION='${VERSION == "daily" ? VERSION : cmk_version}' \
-                                    DISTRO='${distro}' \
-                                    BRANCH='${safe_branch_name}' \
-                                    OTEL_EXPORTER_OTLP_ENDPOINT='${env.OTEL_EXPORTER_OTLP_ENDPOINT}' \
-                                    make ${make_target}
-                                """);
-                            }
+                            single_tests.run_make_target(
+                                result_path: "${WORKSPACE}/test-results/${distro}",
+                                edition: edition,
+                                docker_tag: setup_values.docker_tag,
+                                version: "daily",
+                                distro: distro,
+                                branch_name: setup_values.safe_branch_name,
+                                make_target: make_target,
+                            );
                         }
                     }
                 } finally {
                     stage("Archive / process test reports") {
                         dir("${WORKSPACE}") {
-                            show_duration("archiveArtifacts") {
-                                archiveArtifacts("test-results/**");
-                            }
-                            xunit([Custom(
-                                customXSL: "$JENKINS_HOME/userContent/xunit/JUnit/0.1/pytest-xunit.xsl",
-                                deleteOutputFiles: true,
-                                failIfNotNew: true,
-                                pattern: "**/junit.xml",
-                                skipNoTestFiles: false,
-                                stopProcessingIfError: true
-                            )]);
+                            single_tests.archive_and_process_reports(test_results: "test-results/**");
                         }
                     }
                 }

f4439028e3f7ce48cf1c6c92faae36ba209bb32c
Sven Panne <sven.panne@checkmk.com>
2025-01-15 11:33:35 +0100

    Nuked keepalive global variable
    Change-Id: I62220ccb7ca0ce3b6d38df1b935542ba86e60ff6

--- cmk/checkengine/submitters.py
+++ cmk/checkengine/submitters.py
@@ -89,6 +89,7 @@ def get_submitter(
     dry_run: bool,
     perfdata_format: Literal["pnp", "standard"],
     show_perfdata: bool,
+    keepalive: bool,
 ) -> Submitter:
     """Enterprise should use `cmk.base.cee.keepalive.submitters`."""
     if dry_run:

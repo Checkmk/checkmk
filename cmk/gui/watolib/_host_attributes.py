976f4f88f57ad3d9ff6757fc3de55cd2ec3baad2
Lars Michelsen <lm@checkmk.com>
2025-01-16 09:22:01 +0100

    Move migration of host attribute topics to update config action
    This way we can be sure the migration was performed and finally
    drop the code in 2.5 after the backport to the 2.4.
    
    Change-Id: I8e8885f1a9d94a6adfc81ed9c39bf38b8c74926c

--- cmk/gui/watolib/_host_attributes.py
+++ cmk/gui/watolib/_host_attributes.py
@@ -8,7 +8,6 @@ from cmk.gui.watolib.host_attributes import (
     _clear_config_based_host_attributes,
     _declare_host_tag_attributes,
     declare_custom_host_attrs,
-    transform_pre_16_host_topics,
 )
 from cmk.gui.watolib.hosts_and_folders import folder_tree
 
@@ -22,12 +21,6 @@ def _update_config_based_host_attributes() -> None:
         return str(hash(repr(active_config.tags.get_dict_format()))) + repr(
             active_config.wato_host_attrs
         )
-
-    # The topic conversion needs to take place before the _compute_config_hash runs
-    # The actual generated topics may be pre-1.5 converted topics
-    # e.g. "Custom attributes" -> "custom_attributes"
-    # If we do not convert the topics here, the config_hash comparison will always fail
-    transform_pre_16_host_topics(active_config.wato_host_attrs)
 
     if _update_config_based_host_attributes_config_hash == _compute_config_hash():
         return  # No re-register needed :-)

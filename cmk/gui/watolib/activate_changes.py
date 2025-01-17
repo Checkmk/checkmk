db879b193fdf45555ea1f55ce1ad1bd332942cc4
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-01-17 16:14:08 +0100

    activate change typo fix
    Change-Id: I5917ac3ec5eb0f14bdf1c144b5fddd3c9f8202a5

--- cmk/gui/watolib/activate_changes.py
+++ cmk/gui/watolib/activate_changes.py
@@ -1751,7 +1751,7 @@ class ActivateChangesManager(ActivateChanges):
                     "There are some changes made by your colleagues and you did not "
                     "confirm to activate these changes. In order to proceed, you will "
                     "have to confirm the activation or ask you colleagues to activate "
-                    "these changes in their own."
+                    "these changes on their own."
                 ),
             )
 

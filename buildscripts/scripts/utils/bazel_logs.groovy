ca086806acf27f4a31ca3b50530f685ded6ff5fe
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-01-20 16:24:10 +0100

    Plot bazel cache hits in build-cmk-deliverables job
    ... and do no longer stash and unstash unnecessary data
    
    Follows ee3f1a462cce2d1dec6b62e4f2cc3fd68a024f9a
    
    CMK-21256
    
    Change-Id: I5ddedd1eec58fa12497723248832cdb67b620bc5

--- buildscripts/scripts/utils/bazel_logs.groovy
+++ buildscripts/scripts/utils/bazel_logs.groovy
@@ -18,7 +18,6 @@ def try_parse_bazel_execution_log(distro, distro_dir, bazel_log_prefix) {
 
             // remove large execution log summary file to save some space, approx 1.6GB per workspace
             sh("rm -rf ${distro_dir}/${bazel_log_prefix}*.json");
-            stash(name: "${bazel_log_prefix}${distro}", includes: "${bazel_log_prefix}*")
         }
     } catch (e) {
         print("Failed to parse bazel execution logs: ${e}");
@@ -27,16 +26,6 @@ def try_parse_bazel_execution_log(distro, distro_dir, bazel_log_prefix) {
 
 def try_plot_cache_hits(bazel_log_prefix, distros) {
     try {
-        distros.each { distro ->
-            try {
-                print("Unstashing for distro ${distro}...");
-                unstash(name: "${bazel_log_prefix}${distro}");
-            }
-            catch (e) {
-                print("No stash for ${distro}");
-            }
-        }
-
         plot(
             csvFileName: 'bazel_cache_hits.csv',
             csvSeries:
@@ -50,10 +39,6 @@ def try_plot_cache_hits(bazel_log_prefix, distros) {
             yaxisMaximum: '100',
             yaxisMinimum: '0'
         );
-
-        archiveArtifacts(
-           artifacts: "${bazel_log_prefix}*",
-        )
     }
     catch (e) {
         print("Failed to plot cache hits: ${e}");

f44b2dbdc6a8c913d6ab2d9efb25f4cfe74e7e08
Andreas Umbreit <andreas.umbreit@checkmk.com>
2025-01-21 08:56:42 +0100

    Rework manage-agent-user.sh
    Apply uid, gid and creation mode settings
    
    In this state, the script handles each creation mode separately
    and avoids shared code deliberately, since the final behavior is
    not yet fully specified.
    
    Hence, keeping the logic separated will facilitate further adaptions.
    
    CMK-21137
    
    Change-Id: I46dd2f23c5f920b41a275916b676cf010ae33ad5

--- agents/scripts/manage-agent-user.sh
+++ agents/scripts/manage-agent-user.sh
@@ -45,21 +45,126 @@ HERE
 }
 
 _set_agent_user_permissions() {
-    chown -R :"${AGENT_USER}" "${MK_INSTALLDIR}/package/config"
-    chown -R :"${AGENT_USER}" "${MK_INSTALLDIR}/package/agent"
-    chown -R "${AGENT_USER}":"${AGENT_USER}" "${MK_INSTALLDIR}/runtime"
+    chown -R :"${GROUP_REF}" "${MK_INSTALLDIR}/package/config"
+    chown -R :"${GROUP_REF}" "${MK_INSTALLDIR}/package/agent"
+    chown -R "${USER_REF}":"${GROUP_REF}" "${MK_INSTALLDIR}/runtime"
 }
 
 _set_agent_controller_user_permissions() {
     # Get more finegrained access for the agent controller user only
-    chown :"${AGENT_USER}" "${MK_INSTALLDIR}/package/config"
+    chown -R "${USER_REF}":"${GROUP_REF}" "${MK_INSTALLDIR}/runtime/controller"
+    chown :"${GROUP_REF}" "${MK_INSTALLDIR}/package/config"
     agent_controller_config="${MK_INSTALLDIR}/package/config/cmk-agent-ctl.toml"
-    [ -e "${agent_controller_config}" ] && chown :"${AGENT_USER}" "${agent_controller_config}"
+    [ -e "${agent_controller_config}" ] && chown :"${GROUP_REF}" "${agent_controller_config}"
     pre_configured_connections="${MK_INSTALLDIR}/package/config/pre_configured_connections.json"
-    [ -e "${pre_configured_connections}" ] && chown :"${AGENT_USER}" "${pre_configured_connections}"
+    [ -e "${pre_configured_connections}" ] && chown :"${GROUP_REF}" "${pre_configured_connections}"
 }
 
-_add_user() {
+_create_user() {
+    # Entirely create agent user with specified uid and gid.
+    # Abort if any of the specified entities (name, uid, gid) exists before.
+    # If group or user creation fails, also abort.
+
+    # Some tests, before we touch anything
+    id "${AGENT_USER}" >/dev/null 2>&1 && {
+        printf "User %s already exists, aborting.\n" "${AGENT_USER}"
+        exit 1
+    }
+    [ -n "${AGENT_USER_UID}" ] && {
+        id "${AGENT_USER_UID}" >/dev/null 2>&1 && {
+            printf "User with uid %s already exists, aborting.\n" "${AGENT_USER_UID}"
+            exit 1
+        }
+        uid_argument="--uid ${AGENT_USER_UID}"
+    }
+
+    if [ -n "${AGENT_USER_GID}" ]; then
+        # If we have an explicit gid, we must add it before, because we can't choose on user creation
+        groupadd --gid "${AGENT_USER_GID}" "${AGENT_USER}" || exit 1
+        group_argument="--no-user-group --gid ${AGENT_USER_GID}"
+    else
+        group_argument="--user-group"
+    fi
+
+    printf "Creating %s user account ...\n" "${AGENT_USER}"
+    useradd ${uid_argument} \
+        ${group_argument} \
+        --comment "Checkmk agent system user" \
+        --system \
+        --home-dir "${HOMEDIR}" \
+        --no-create-home \
+        --shell "/bin/false" \
+        "${AGENT_USER}" || exit 1
+
+    _allow_legacy_pull
+    _issue_legacy_pull_warning
+}
+
+_check_user() {
+    # Confirm that name, uid, and gid exist as specified.
+    # Abort if they don't.
+
+    id -u "${AGENT_USER}" >/dev/null 2>&1 || {
+        printf "Agent user %s doesn't exist, aborting.\n" "${AGENT_USER}"
+        exit 1
+    }
+
+    [ -n "${AGENT_USER_UID}" ] || [ -n "${AGENT_USER_GID}" ] || return 0
+
+
+    [ -n "${AGENT_USER_UID}" ] && [ ! "$(id -u "${AGENT_USER}")" = "${AGENT_USER_UID}" ] && {
+        printf "Agent user %s doesn't have specified uid %s, aborting.\n" "${AGENT_USER}" "${AGENT_USER_UID}"
+        exit 1
+    }
+
+    [ -n "${AGENT_USER_GID}" ] && ! id -G "${AGENT_USER}" | grep -qw "${AGENT_USER_GID}" && {
+        printf "Agent user %s is not member of group %s, aborting.\n" "${AGENT_USER}" "${AGENT_USER_GID}"
+        exit 1
+    }
+
+    printf "Note: Using existing agent user %s.\n" "${AGENT_USER}"
+}
+
+_update_user() {
+    # 1. If specified user exists, check if it has the specified uid. Abort if not.
+    # 2. Create the specified group with gid and specified agent user name if it doesn't exist.
+    # 3. If agent user exist, add it to specified group.
+    # 4. If agent user doesn't exist, create it with specified uid and gid. Abort if this fails.
+
+    [ -n "${AGENT_USER_UID}" ] && [ ! "$(id -u "${AGENT_USER}")" = "${AGENT_USER_UID}" ] && {
+        printf "Agent user %s exists, but doesn't have specified uid %s, aborting.\n" "${AGENT_USER}" "${AGENT_USER_UID}"
+        exit 1
+    }
+
+    [ -n "${AGENT_USER_GID}"] && ! getent group "${AGENT_USER_GID}" && groupadd --gid "${AGENT_USER_GID}" "${AGENT_USER}"
+
+    if id "${AGENT_USER}" >/dev/null 2>&1; then
+        [ -n "${AGENT_USER_GID}" ] && usermod -G "${AGENT_USER_GID}" "${AGENT_USER}"
+    else
+        [ -n "${AGENT_USER_UID}" ] && uid_argument="--uid ${AGENT_USER_UID}"
+
+        if [ -n "${AGENT_USER_GID}" ]; then
+            group_argument="--no-user-group --gid ${AGENT_USER_GID}"
+        else
+            group_argument="--user-group"
+        fi
+
+        printf "Creating %s user account ...\n" "${AGENT_USER}"
+        useradd ${uid_argument} \
+            ${group_argument} \
+            --comment "Checkmk agent system user" \
+            --system \
+            --home-dir "${HOMEDIR}" \
+            --no-create-home \
+            --shell "/bin/false" \
+            "${AGENT_USER}" || exit 1
+
+        _allow_legacy_pull
+        _issue_legacy_pull_warning
+    fi
+}
+
+_handle_user_legacy() {
     # add Checkmk agent system user
     printf "Creating/updating %s user account ...\n" "${AGENT_USER}"
     comment="Checkmk agent system user"
@@ -97,19 +202,51 @@ _add_user() {
         _issue_legacy_pull_warning
     fi
     unset homedir comment usershell
-
 }
 
 main() {
+
+    case "${AGENT_USER_CREATION}" in
+        "auto")
+            handle_user="_update_user"
+            ;;
+        "create")
+            handle_user="_create_user"
+            ;;
+        "use-existing")
+            handle_user="_check_user"
+            ;;
+        *)
+            printf "Unknown value for AGENT_USER_CREATION: %s\n" "${AGENT_USER_CREATION}"
+            exit 1
+            ;;
+    esac
+
+    if [ -n "${AGENT_USER_UID}" ]; then
+        USER_REF="${AGENT_USER_UID}"
+    else
+        USER_REF="${AGENT_USER}"
+    fi
+
+    if [ -n "${AGENT_USER_GID}" ]; then
+        GROUP_REF="${AGENT_USER_GID}"
+    else
+        GROUP_REF="${AGENT_USER}"
+    fi
+
     [ "${DEPLOYMENT_MODE}" = "non-root" ] && {
-        _add_user
+        "${handle_user}"
         _set_agent_user_permissions
         exit 0
     }
 
     "${CONTROLLER_BINARY}" --version >/dev/null 2>&1 && {
-        _add_user
-        [ -n "${MK_INSTALLDIR}" ] && _set_agent_controller_user_permissions
+        if [ -n "${MK_INSTALLDIR}" ]; then
+            "${handle_user}"
+            _set_agent_controller_user_permissions
+        else
+            handle_user_legacy
+        fi
     }
 }
 

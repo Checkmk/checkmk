1c085b7a94876994a7af8fd2e50543dfef9ec2f8
Matteo Stifano <matteo.stifano@checkmk.com>
2025-01-22 15:02:57 +0100

    test_plugin_piggyback: Restore test
    CMK-21207
    
    Change-Id: I0bf0d6b2ffd3225a3b135987943a22f72fd528d6

--- tests/plugins_integration/test_plugin_piggyback.py
+++ tests/plugins_integration/test_plugin_piggyback.py
@@ -78,7 +78,6 @@ def _wait_for_pb_host_deletion(site: Site, source_host_name: str, pb_host_name: 
     ), f"Host {pb_host_name} was not removed from the site."
 
 
-@pytest.mark.xfail(reason="Test is failing. Temporary xfailing it to investigate the problem.")
 @pytest.mark.parametrize("source_host_name", get_host_names(piggyback=True))
 def test_plugin_piggyback(
     test_site_piggyback: Site,

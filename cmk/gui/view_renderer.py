121c92bcc7c54440e6b33c64d82eed6a8d487cc4
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-01-29 13:23:36 +0100

    17492 Fix filter indicator
    CMK-13446
    
    Change-Id: Iac55b26c44bc82681a0eb0634b508a40f775f4e9

--- cmk/gui/view_renderer.py
+++ cmk/gui/view_renderer.py
@@ -43,6 +43,7 @@ from cmk.gui.page_menu_entry import toggle_page_menu_entries
 from cmk.gui.page_menu_utils import collect_context_links, get_context_page_menu_dropdowns
 from cmk.gui.painter_options import PainterOptions
 from cmk.gui.type_defs import HTTPVariables, InfoName, Rows, ViewSpec
+from cmk.gui.utils.filter import requested_filter_is_not_default
 from cmk.gui.utils.html import HTML
 from cmk.gui.utils.output_funnel import output_funnel
 from cmk.gui.utils.selection_id import SelectionId
@@ -50,6 +51,7 @@ from cmk.gui.utils.transaction_manager import transactions
 from cmk.gui.utils.urls import DocReference, makeuri, makeuri_contextless
 from cmk.gui.view import View
 from cmk.gui.views.command import Command, do_actions, get_command_groups, should_show_command_form
+from cmk.gui.views.page_ajax_filters import AjaxInitialViewFilters
 from cmk.gui.visuals import view_title
 from cmk.gui.visuals.filter import Filter
 from cmk.gui.watolib.activate_changes import get_pending_changes_tooltip, has_pending_changes
@@ -508,8 +510,9 @@ class GUIViewRenderer(ABCViewRenderer):
             )
 
     def _page_menu_entries_filter(self, show_filters: list[Filter]) -> Iterator[PageMenuEntry]:
-        is_filter_set = request.var("filled_in") == "filter"
-
+        is_filter_set = requested_filter_is_not_default(
+            AjaxInitialViewFilters().get_context(page_name=self.view.name)
+        )
         yield PageMenuEntry(
             title=_("Filter"),
             icon_name={"icon": "filter", "emblem": "warning"} if is_filter_set else "filter",

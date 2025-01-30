c2f0ccae89032e4dd0225d4d0b6d24ca7fee34cc
Christopher Krause <christopher.krause@checkmk.com>
2025-01-30 14:08:01 +0100

    17506 FIX REST API: Reintroduce audit_log delete endpoint
    This change reintroduces the deprecated `DELETE /domain-types/audit_log/collections/all` endpoint.
    It was accidentally removed as part of https://checkmk.com/werk/17174
    and will be removed again in Checkmk 2.4.
    
    Users should instead use archive action described in https://checkmk.com/werk/16367
    
    Change-Id: Idb45b4c0e4c8ebe31c153c4e9f3ba3e846fb9015
    JIRA-Ref: CMK-21245

--- cmk/gui/openapi/endpoints/audit_log/__init__.py
+++ cmk/gui/openapi/endpoints/audit_log/__init__.py
@@ -64,6 +64,24 @@ def archive_logs(params: Mapping[str, Any]) -> Response:
 
 @Endpoint(
     constructors.collection_href("audit_log"),
+    ".../delete",
+    method="delete",
+    output_empty=True,
+    permissions_required=CLEAR_PERMISSION,
+    update_config_generation=False,
+    deprecated_urls={"/domain-types/audit_log/collections/all": 16367},
+)
+def clear_logs(params: Mapping[str, Any]) -> Response:
+    """Move audit log entries to archive"""
+    user.need_permission("wato.edit")
+    user.need_permission("wato.auditlog")
+    user.need_permission("wato.clear_auditlog")
+    AuditLogStore().clear()
+    return Response(status=204)
+
+
+@Endpoint(
+    constructors.collection_href("audit_log"),
     ".../collection",
     method="get",
     response_schema=AuditLogEntryCollection,
@@ -130,3 +148,4 @@ def _get_start_end_day_timestamp(value: datetime.date) -> tuple[int, int]:
 def register(endpoint_registry: EndpointRegistry) -> None:
     endpoint_registry.register(get_all)
     endpoint_registry.register(archive_logs)
+    endpoint_registry.register(clear_logs)

59ddde633808756ea83633b59df8d8ee1a5f45ed
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-01-31 07:42:54 +0100

    Bring back update step for autochecks
    This brings back a simplified version of the autocheck migration
    that was removed in commit e735e11e3234bb3d64979b9421030e01cbc9bb62.
    
    Change-Id: Iec60bb0f6d8c2e300abdd9df6b2e7cd5b65fa79a

---
+++ cmk/update_config/plugins/lib/replaced_check_plugins.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections.abc import Mapping
+
+from cmk.checkengine.checking import CheckPluginName
+
+# Please keep this functionality even if we currently don't have any replaced check plugins!
+_REPLACED_CHECK_PLUGINS: dict[CheckPluginName, CheckPluginName] = {}
+
+ALL_REPLACED_CHECK_PLUGINS: Mapping[CheckPluginName, CheckPluginName] = {
+    **_REPLACED_CHECK_PLUGINS,
+    **{
+        old_name.create_management_name(): new_name.create_management_name()
+        for old_name, new_name in _REPLACED_CHECK_PLUGINS.items()
+    },
+}

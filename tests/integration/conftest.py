eff0e88ca3391bbb2b0659da12e5ba608edb37d6
Jörg Herbel <joerg.herbel@checkmk.com>
2025-02-03 20:13:33 +0100

    Revert "Disable automation helper reloader in integration tests"
    This reverts commit d2c948fa69411352a3cc92d085d400d126937841.
    
    Change-Id: I3a8337433e48755359c4b0d875b7869f232b2c29

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -11,14 +11,6 @@ from tests.testlib.pytest_helpers.calls import exit_pytest_on_exceptions
 from tests.testlib.site import get_site_factory, Site
 from tests.testlib.web_session import CMKWebSession
 
-from cmk.base.automation_helper._config import (  # pylint: disable=cmk-module-layer-violation
-    Config,
-    default_config,
-    RELATIVE_CONFIG_PATH_FOR_TESTING,
-    ReloaderConfig,
-    ServerConfig,
-)
-
 from .event_console import CMKEventConsole
 
 logger = logging.getLogger(__name__)
@@ -49,32 +41,3 @@ def fixture_web(site: Site) -> CMKWebSession:
 @pytest.fixture(scope="session")
 def ec(site: Site) -> CMKEventConsole:
     return CMKEventConsole(site)
-
-
-# this is a temporary measure until we find a more robust implementation for the reloader
-@pytest.fixture(scope="session", autouse=True)
-def deactivate_automation_helper_reloader(site: Site) -> None:
-    default_configuration = default_config(
-        omd_root=site.root,
-        run_directory=site.root / "tmp" / "run",
-        log_directory=site.root / "var" / "log" / "automation-helper",
-    )
-    site.write_text_file(
-        RELATIVE_CONFIG_PATH_FOR_TESTING,
-        Config(
-            server_config=ServerConfig(
-                unix_socket=default_configuration.server_config.unix_socket,
-                pid_file=default_configuration.server_config.pid_file,
-                access_log=default_configuration.server_config.access_log,
-                error_log=default_configuration.server_config.error_log,
-                num_workers=1,
-            ),
-            watcher_config=default_configuration.watcher_config,
-            reloader_config=ReloaderConfig(
-                active=False,
-                poll_interval=default_configuration.reloader_config.poll_interval,
-                cooldown_interval=default_configuration.reloader_config.cooldown_interval,
-            ),
-        ).model_dump_json(),
-    )
-    site.run(["omd", "restart", "automation-helper"])

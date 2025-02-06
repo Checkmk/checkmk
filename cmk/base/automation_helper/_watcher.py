f82d8378289f9770f5bd252f2113d620998c971b
Jörg Herbel <joerg.herbel@checkmk.com>
2025-02-06 10:53:16 +0100

    Automation helper: Set inotify buffer delay to 0
    In the composition tests, we are observing events arriving too late according to
    the automation helper log. This change might solve the issue.
    
    CMK-21699
    
    Change-Id: Ieb48770101972a3deffd0d1c41554de92b310253

--- cmk/base/automation_helper/_watcher.py
+++ cmk/base/automation_helper/_watcher.py
@@ -8,17 +8,28 @@ import time
 from collections.abc import Generator, Sequence
 
 from watchdog.events import FileSystemEvent, PatternMatchingEventHandler
-from watchdog.observers import Observer
+from watchdog.observers.inotify import InotifyObserver
+from watchdog.observers.inotify_buffer import InotifyBuffer
 
 from ._cache import Cache, CacheError
 from ._config import WatcherConfig
 from ._log import LOGGER
 
+# When a file or folder is moved, two events are created: IN_MOVED_FROM and IN_MOVED_TO.
+# watchdog tries to combine these two events into a single move event. In case it received
+# IN_MOVED_FROM but not yet in IN_MOVED_TO, it waits up `InotifyBuffer.delay` seconds for
+# IN_MOVED_TO, thereby delaying the reporting of other events. This might be an actual problem
+# for us if an automation call is triggered right after files relevant to this call were modified.
+# In such scenarios, the reload notification from the watcher might arrive too late if watchdog
+# adds some delay. As a result, the automation call uses an outdated configuration.
+# See also https://man7.org/linux/man-pages/man7/inotify.7.html, "Dealing with rename() events"
+InotifyBuffer.delay = 0.0
+
 
 @contextlib.contextmanager
 def run(config: WatcherConfig, cache: Cache) -> Generator[None]:
     LOGGER.info("[watcher] Initializing")
-    observer = Observer()
+    observer = InotifyObserver(generate_full_events=True)
     for schedule in config.schedules:
         handler = _AutomationWatcherHandler(
             cache=cache,

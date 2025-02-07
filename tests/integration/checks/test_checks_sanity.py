ac19415ca3a057114933fe4ec0c911f13acd3a16
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-02-07 14:43:11 +0100

    unbreak cme/cce tests
    Change-Id: I186cbaffc46307f9beba1cf1d7ba656aefe9517b

--- tests/integration/checks/test_checks_sanity.py
+++ tests/integration/checks/test_checks_sanity.py
@@ -155,7 +155,7 @@ def test_shipped_ps_disocvery(host_services: dict[str, ServiceInfo], site: Site)
             # f"Process {site.id} real-time helper",  # not enabled
             f"Process {site.id} rrd helper",
         }
-    if site.version.is_enterprise_edition():
+    if not site.version.is_raw_edition():
         expected_ps_services |= {
             f"Process {site.id} dcd",
         }

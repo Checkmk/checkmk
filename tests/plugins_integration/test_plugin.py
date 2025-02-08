eaa18cdb49d3c3d588bb114cc1445c30e6f423f6
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-02-08 10:03:37 +0100

    plugins_integration: skip broken agents
    CMK-21720
    
    Change-Id: I7b41bed43beaee0d53c618d06706eeb075315719

--- tests/plugins_integration/test_plugin.py
+++ tests/plugins_integration/test_plugin.py
@@ -21,7 +21,18 @@ from tests.plugins_integration.checks import (
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.parametrize("host_name", get_host_names())
+@pytest.mark.parametrize(
+    "host_name",
+    [
+        host
+        for host in get_host_names()
+        if host
+        not in (
+            "agent-2.2.0p14-windows-dhcp",  # CMK-21720
+            "agent-2.2.0p14-windows-veeam-backup",  # CMK-21720
+        )
+    ],
+)
 def test_plugin(
     test_site: Site,
     host_name: str,

37dc91b45751923343f11c721c2ba3d6bcb8be66
Owen Synge <owen.synge@checkmk.com>
2025-02-11 16:38:38 +0100

    cmk-agent-ctl: Typo
    Change-Id: I776450e5bff6c8eedd30f3c64121678ab9e36f63

--- packages/host/cmk-agent-ctl/tests/test_pull.rs
+++ packages/host/cmk-agent-ctl/tests/test_pull.rs
@@ -2,7 +2,7 @@
 // This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 // conditions defined in the file COPYING, which is part of this source code package.
 
-// Test files are compiled to seperate crates, so there
+// Test files are compiled to separate crates, so there
 // may be some unused functions in the common module
 #![allow(dead_code)]
 

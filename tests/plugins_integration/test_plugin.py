78eab9204eb54a0b8b270177b58b4d97669a0285
Matteo Stifano <matteo.stifano@checkmk.com>
2025-02-11 09:21:46 +0100

    test_plugin: Restore test cases
    CMK-21720
    
    Change-Id: Ic5edb994fe1cb0aa8719c399cdff6b6cea742f76

--- tests/plugins_integration/test_plugin.py
+++ tests/plugins_integration/test_plugin.py
@@ -21,19 +21,7 @@ from tests.plugins_integration.checks import (
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.parametrize(
-    "host_name",
-    [
-        host
-        for host in get_host_names()
-        if host
-        not in (
-            "agent-2.2.0p14-windows-dhcp",  # CMK-21720
-            "agent-2.2.0p14-windows-veeam-backup",  # CMK-21720
-            "agent-2.2.0p14-windows-mssql",  # CMK-21720
-        )
-    ],
-)
+@pytest.mark.parametrize("host_name", [host for host in get_host_names() if host])
 def test_plugin(
     test_site: Site,
     host_name: str,

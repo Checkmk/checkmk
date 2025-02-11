d2aace2f2cd4868f9f4b57be3f55666b8216dc59
Simon Jess <simon.jess@checkmk.com>
2025-02-11 11:15:40 +0100

    otel: Add int test for self monitoring
    CMK-21090
    
    Change-Id: I9d79ea519bd2455f6ed6f26eeef2e7f6112fdc8e

--- tests/integration/omd/test_otel_collector.py
+++ tests/integration/omd/test_otel_collector.py
@@ -4,6 +4,8 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import os
+from collections.abc import Iterator
+from contextlib import contextmanager
 from pathlib import Path
 
 import pytest
@@ -51,3 +53,57 @@ def test_otel_collector_version(site: Site) -> None:
         "--version",
     ]
     assert "0.113.0" in site.check_output(cmd)
+
+
+@contextmanager
+def _modify_test_site(site: Site, hostname: str) -> Iterator[None]:
+    rule_id = ""
+    try:
+        site.set_config("OPENTELEMETRY_COLLECTOR", "on", with_restart=True)
+        site.openapi.hosts.create(
+            hostname,
+            attributes={
+                "tag_address_family": "no-ip",
+                "tag_agent": "special-agents",
+                "tag_piggyback": "no-piggyback",
+            },
+            folder="/",
+        )
+        rule_id = site.openapi.rules.create(
+            ruleset_name="special_agents:otel",
+            value={"include_self_monitoring": True},
+            conditions={
+                "host_name": {
+                    "match_on": [hostname],
+                    "operator": "one_of",
+                },
+            },
+            folder="/",
+        )
+        yield
+    finally:
+        if rule_id:
+            site.openapi.rules.delete(rule_id)
+        site.openapi.hosts.delete(hostname)
+        site.set_config("OPENTELEMETRY_COLLECTOR", "off", with_restart=True)
+
+
+def test_otel_collector_self_monitoring(site: Site) -> None:
+    hostname = "otelhost"
+    with _modify_test_site(site, hostname):
+        site.ensure_running()
+        site.openapi.service_discovery.run_discovery_and_wait_for_completion(hostname)
+        result = site.openapi.service_discovery.get_discovery_result(hostname)
+    assert result["extensions"] == {
+        "check_table": {},
+        "host_labels": {
+            "cmk/otel/metrics": {
+                # Means: section is there (special agent otel was executed!) but empty.
+                # First dataset will be generated 60000ms after the start
+                "value": "pending",
+                "plugin_name": "otel_metrics",
+            }
+        },
+        "vanished_labels": {},
+        "changed_labels": {},
+    }

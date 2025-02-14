e94b53bfcb702c49600ca495a5d320266299a643
Simon Jess <simon.jess@checkmk.com>
2025-02-14 12:16:38 +0100

    dmidecode (inv plugin): Remove doc test; we have an unit test
    Change-Id: I428c8ae65c8049f2c0ea80604a6a90922ac878dc

--- cmk/plugins/collection/agent_based/inventory_dmidecode.py
+++ cmk/plugins/collection/agent_based/inventory_dmidecode.py
@@ -22,65 +22,6 @@ Section = list[tuple[str, StringTable]]
 
 def parse_dmidecode(string_table: StringTable) -> Section:
     """Parse the output of `dmidecode -q | sed 's/\t/:/g'` with sep(58)
-
-    This is a *massively* reduced example:
-
-        >>> from pprint import pprint
-        >>> string_table = [line.split(':') for line in [
-        ...     'Processor Information',
-        ...     ':Type: Central Processor',
-        ...     ':Family: Core i7',
-        ...     ':Manufacturer: Intel(R) Corporation',
-        ...     ':ID: 61 06 04 00 FF FB EB BF',
-        ...     ':Flags:',
-        ...     '::FPU (Floating-point unit on-chip)',
-        ...     '::VME (Virtual mode extension)',
-        ...     '::DE (Debugging extension)',
-        ...     '',
-        ...     'Chassis Information',
-        ...     ':Manufacturer: Apple Inc.',
-        ...     ':Type: Laptop',
-        ...     '',
-        ...     'Onboard Device',
-        ...     ':Reference Designation: Integrated Video Controller',
-        ...     ':Type: Video',
-        ...     ':Status: Enabled',
-        ...     ':Type Instance: 1',
-        ...     ':Bus Address: 0000:00:00.0',
-        ...     '',
-        ...     'Physical Memory Array',
-        ...     ':Location: System Board Or Motherboard',
-        ...     ':Number Of Devices: 2',
-        ...     '',
-        ...     'Memory Device',
-        ...     ':Bank Locator: BANK 0',
-        ...     '',
-        ...     'Memory Device',
-        ...     ':Bank Locator: BANK 1',
-        ... ] if line]
-        >>> pprint(parse_dmidecode(string_table))
-        [('Processor Information',
-          [['Type', 'Central Processor'],
-           ['Family', 'Core i7'],
-           ['Manufacturer', 'Intel(R) Corporation'],
-           ['ID', '61 06 04 00 FF FB EB BF'],
-           ['Flags', ''],
-           ['', 'FPU (Floating-point unit on-chip)'],
-           ['', 'VME (Virtual mode extension)'],
-           ['', 'DE (Debugging extension)']]),
-         ('Chassis Information', [['Manufacturer', 'Apple Inc.'], ['Type', 'Laptop']]),
-         ('Onboard Device',
-          [['Reference Designation', 'Integrated Video Controller'],
-           ['Type', 'Video'],
-           ['Status', 'Enabled'],
-           ['Type Instance', '1'],
-           ['Bus Address', '0000', '00', '00.0']]),
-         ('Physical Memory Array',
-          [['Location', 'System Board Or Motherboard'], ['Number Of Devices', '2']]),
-         ('Memory Device', [['Bank Locator', 'BANK 0']]),
-         ('Memory Device', [['Bank Locator', 'BANK 1']])]
-
-
     Note: on Linux \t is replaced by : and then the split is done by :.
     On Windows the \t comes 1:1 and no splitting is being done.
     So we need to split manually here.

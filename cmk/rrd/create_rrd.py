f5b35bf837e236f93e6764c5454c437e13625cb4
Andreas Umbreit <andreas.umbreit@checkmk.com>
2025-02-14 12:23:32 +0100

    cmk.rrd: Eliminate cmk.base import
    Change-Id: Ic14bd372406654417743645de99449425378a127

--- cmk/rrd/create_rrd.py
+++ cmk/rrd/create_rrd.py
@@ -3,16 +3,23 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# NOTE: rrdtool is missing type hints
 
+import signal
+from types import FrameType
+from typing import NoReturn
 
-from cmk.base.utils import register_sigint_handler  # pylint: disable=cmk-module-layer-violation
+from cmk.ccc.exceptions import MKTerminate
 
 from .config import RRDConfigImpl  # pylint: disable=cmk-module-layer-violation
 from .interface import RRDInterface  # pylint: disable=cmk-module-layer-violation
 from .rrd import RRDCreator  # pylint: disable=cmk-module-layer-violation
 
 
+# register SIGINT handler for consistent CTRL+C handling
+def _handle_keepalive_interrupt(signum: int, frame: FrameType | None) -> NoReturn:
+    raise MKTerminate()
+
+
 def create_rrd(rrd_interface: RRDInterface) -> None:
-    register_sigint_handler()
+    signal.signal(signal.SIGINT, _handle_keepalive_interrupt)
     RRDCreator(rrd_interface).create_rrds_keepalive(config=RRDConfigImpl())

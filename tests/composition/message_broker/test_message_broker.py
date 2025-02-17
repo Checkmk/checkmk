83e540e01746dd7e23f0a88e833929dc232197a3
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2025-02-17 16:08:39 +0100

    Message broker composition tests: Dump rabbitmq info on failure
    Change-Id: I82de4301fbb69aa9d6fc9f6b97bedd19e1f0159a

--- tests/composition/message_broker/test_message_broker.py
+++ tests/composition/message_broker/test_message_broker.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2022 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
 import logging
 from typing import IO
 
@@ -14,6 +13,7 @@ from tests.composition.message_broker.utils import (
     broker_stopped,
     check_broker_ping,
     p2p_connection,
+    rabbitmq_info_on_failure,
     Timeout,
     timeout,
 )
@@ -44,28 +44,32 @@ class TestCMKBrokerTest:
 
     def test_ping_ok(self, central_site: Site) -> None:
         """Test if we can ping ourselves"""
-        check_broker_ping(central_site, central_site.id)
+        with rabbitmq_info_on_failure([central_site]):
+            check_broker_ping(central_site, central_site.id)
 
     def test_ping_fail(self, central_site: Site) -> None:
         """Test if we can't ping a non-existing site"""
-        with pytest.raises(Timeout):
-            check_broker_ping(central_site, "this-goes-nowhere")
+        with rabbitmq_info_on_failure([central_site]):
+            with pytest.raises(Timeout):
+                check_broker_ping(central_site, "this-goes-nowhere")
 
     def test_pong(self, central_site: Site) -> None:
         """Test if we can pong"""
-        with broker_pong(central_site) as pong:
-            assert pong.returncode is None  # it's running
+        with rabbitmq_info_on_failure([central_site]):
+            with broker_pong(central_site) as pong:
+                assert pong.returncode is None  # it's running
 
     def test_pong_received_message(self, central_site: Site, remote_site: Site) -> None:
         """Test if the `pong_received_message` works as intended"""
-        with broker_pong(central_site) as pong:
-            assert pong.stdout  # for mypy
-            # nothing received so far
-            assert not pong_received_message(pong.stdout, wait_for=1)
-            # we can't ping locally, that will bypass the pong
-            check_broker_ping(remote_site, central_site.id)
-            # if the _check_broker_ping worked, the pong *must* have received a message
-            assert pong_received_message(pong.stdout, wait_for=1)
+        with rabbitmq_info_on_failure([central_site, remote_site]):
+            with broker_pong(central_site) as pong:
+                assert pong.stdout  # for mypy
+                # nothing received so far
+                assert not pong_received_message(pong.stdout, wait_for=1)
+                # we can't ping locally, that will bypass the pong
+                check_broker_ping(remote_site, central_site.id)
+                # if the _check_broker_ping worked, the pong *must* have received a message
+                assert pong_received_message(pong.stdout, wait_for=1)
 
 
 def _next_free_port(site: Site, key: str, port: str) -> int:
@@ -76,7 +80,8 @@ def _next_free_port(site: Site, key: str, port: str) -> int:
 class TestMessageBroker:
     def test_message_broker_central_remote(self, central_site: Site, remote_site: Site) -> None:
         """Test if the connection between central and remote site works"""
-        assert_message_exchange_working(central_site, remote_site)
+        with rabbitmq_info_on_failure([central_site, remote_site]):
+            assert_message_exchange_working(central_site, remote_site)
 
     def test_message_broker_remote_remote_via_central(
         self,
@@ -84,20 +89,22 @@ class TestMessageBroker:
         remote_site: Site,
         remote_site_2: Site,
     ) -> None:
-        with broker_pong(remote_site):
-            # test complement: should not work without the central site running:
-            with broker_stopped(central_site):
-                with pytest.raises(Timeout):
-                    check_broker_ping(remote_site_2, remote_site.id)
+        with rabbitmq_info_on_failure([central_site, remote_site, remote_site_2]):
+            with broker_pong(remote_site):
+                # test complement: should not work without the central site running:
+                with broker_stopped(central_site):
+                    with pytest.raises(Timeout):
+                        check_broker_ping(remote_site_2, remote_site.id)
 
-            check_broker_ping(remote_site_2, remote_site.id)
+                check_broker_ping(remote_site_2, remote_site.id)
 
     def test_message_broker_remote_remote_p2p(
         self, central_site: Site, remote_site: Site, remote_site_2: Site
     ) -> None:
-        with (
-            p2p_connection(central_site, remote_site, remote_site_2),
-            broker_pong(remote_site),
-            broker_stopped(central_site),
-        ):
-            check_broker_ping(remote_site_2, remote_site.id)
+        with rabbitmq_info_on_failure([central_site, remote_site, remote_site_2]):
+            with (
+                p2p_connection(central_site, remote_site, remote_site_2),
+                broker_pong(remote_site),
+                broker_stopped(central_site),
+            ):
+                check_broker_ping(remote_site_2, remote_site.id)

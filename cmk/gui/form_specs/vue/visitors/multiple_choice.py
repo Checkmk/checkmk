aa6e27ca1f1502c90f02bda4f7717fe3a5f7fc4d
Logan Connolly <logan.connolly@checkmk.com>
2025-02-18 16:31:59 +0100

    gui(multiple_choice): remove unused method
    It appears that this method may have been copy-pasta from the
    `SingleChoice` formspec, since this method is never invoked.
    
    Change-Id: Ibaee999cb41f41ed4d7c56413d1e6205e94110d5

--- cmk/gui/form_specs/vue/visitors/multiple_choice.py
+++ cmk/gui/form_specs/vue/visitors/multiple_choice.py
@@ -28,11 +28,6 @@ _FrontendModel = Sequence[str]
 class MultipleChoiceVisitor(
     FormSpecVisitor[MultipleChoiceExtended, _ParsedValueModel, _FrontendModel]
 ):
-    def _is_valid_choice(self, value: str) -> bool:
-        if isinstance(self.form_spec.elements, shared_type_defs.Autocompleter):
-            return True
-        return value in [x.name for x in self.form_spec.elements]
-
     def _strip_invalid_choices(self, raw_value: list[str]) -> list[str]:
         if isinstance(self.form_spec.elements, shared_type_defs.Autocompleter):
             return raw_value

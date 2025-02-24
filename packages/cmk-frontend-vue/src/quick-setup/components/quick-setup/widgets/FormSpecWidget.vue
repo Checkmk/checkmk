dbbab97f8290f92ac0f6920ae1f295c1008721d0
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-02-24 16:02:57 +0100

    Revert "Fix broken data binding"
    This reverts commit e575200672a608e4882a689880837b38dcd94d94.
    
    Reason for revert: breaks aws quick setup
    
    Change-Id: I4500d0ae1e1f98347bbaeb52c1b4bffbb5776c74

--- packages/cmk-frontend-vue/src/quick-setup/components/quick-setup/widgets/FormSpecWidget.vue
+++ packages/cmk-frontend-vue/src/quick-setup/components/quick-setup/widgets/FormSpecWidget.vue
@@ -4,7 +4,7 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import FormEdit from '@/form/components/FormEdit.vue'
 import { type FormSpecWidgetProps } from './widget_types'
 import type { ValidationMessages } from '@/form'
@@ -14,7 +14,7 @@ const props = defineProps<FormSpecWidgetProps>()
 const emit = defineEmits(['update'])
 
 const formSpecId = props.form_spec.id as string
-const internal = ref<unknown>(props?.data![formSpecId] || props.form_spec.data || {})
+const internal = ref(props?.data![formSpecId] || props.form_spec.data || {})
 
 const validationErrors = computed((): ValidationMessages => {
   const errors = props?.errors![formSpecId] || []
@@ -24,10 +24,9 @@ const validationErrors = computed((): ValidationMessages => {
 //This will set a starting value on the quick setup component for this form spec
 emit('update', formSpecId, internal)
 
-function formEditDataWasUpdated(newValue: unknown) {
-  internal.value = newValue
+watch(internal.value, (newValue) => {
   emit('update', formSpecId, newValue)
-}
+})
 </script>
 
 <template>
@@ -37,10 +36,9 @@ function formEditDataWasUpdated(newValue: unknown) {
         <td>
           <HelpText :help="form_spec.spec.help" />
           <FormEdit
-            :data="internal"
+            v-model:data="internal"
             :spec="form_spec.spec"
             :backend-validation="validationErrors"
-            @update:data="formEditDataWasUpdated"
           />
         </td>
       </tr>

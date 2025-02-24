a022dc8201625220c490ed47ecaac443d18c0968
Logan Connolly <logan.connolly@checkmk.com>
2025-02-24 12:00:58 +0100

    gerrit: match tests file structure to source code
    Change-Id: I51fce50526ad7fd1d518d8c2907391ffabd27a5a

--- tests/unit/cmk/plugins/gerrit/lib/test_agent.py
+++ tests/unit/cmk/plugins/gerrit/lib/test_agent.py
@@ -3,15 +3,11 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-
 import argparse
 
 import pytest
 
-from cmk.utils.semantic_version import SemanticVersion
-
 from cmk.plugins.gerrit.lib import agent
-from cmk.plugins.gerrit.lib.collectors.version import LatestVersions
 from cmk.plugins.gerrit.lib.shared_typing import SectionName, Sections
 
 
@@ -60,22 +56,4 @@ def test_write_sections(capsys: pytest.CaptureFixture) -> None:
 {"current": "1.2.3", "latest": {"major": null, "minor": "1.3.4", "patch": "1.2.5"}}
 """
 
-    assert value == expected
-
-
-def test_latest_versions() -> None:
-    curr, latest_patch, latest_minor, latest_major = "1.2.3", "1.2.5", "1.3.1", "3.0.1"
-    raw_versions = [curr, "1.2.4", latest_patch, "1.3.0", latest_minor, "3.0.0", latest_major]
-    current = SemanticVersion.from_string(curr)
-    versions = [SemanticVersion.from_string(v) for v in raw_versions]
-
-    value = LatestVersions.build(current, versions)
-    expected = LatestVersions(major=latest_major, minor=latest_minor, patch=latest_patch)
-
-    assert value == expected
-
-
-def test_latest_versions_no_available_updates() -> None:
-    value = LatestVersions.build(current=SemanticVersion.from_string("1.2.3"), versions=[])
-    expected = LatestVersions(major=None, minor=None, patch=None)
     assert value == expected

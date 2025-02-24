dc0b5e87f65d1b222bbf89912da266e3feea1a17
Lars Michelsen <lm@checkmk.com>
2025-02-24 14:54:21 +0100

    EC: Fix exception on 2nd config reload via signal
    The exception was raised because run_eventd still held the reference to
    the old history object and re-used it in the next invocation of
    reload_configuration.
    
    There is the same issue left in StatusServer.handle_command_reload which
    will be fixed in a separate commit.
    
    CMK-21197
    
    Change-Id: Ieaffe2b1053bd2a60a742ce6db1ec0e6d206d433

--- cmk/ec/main.py
+++ cmk/ec/main.py
@@ -2495,7 +2495,7 @@ def run_eventd(  # pylint: disable=too-many-branches
         except MKSignalException as e:
             if e.signum == 1:
                 logger.info("Received SIGHUP - going to reload configuration")
-                reload_configuration(
+                history = reload_configuration(
                     settings,
                     logger,
                     lock_configuration,
@@ -3351,7 +3351,7 @@ def reload_configuration(
     event_server: EventServer,
     status_server: StatusServer,
     slave_status: SlaveStatus,
-) -> None:
+) -> History:
     with lock_configuration:
         config = load_configuration(settings, logger, slave_status)
 
@@ -3364,6 +3364,7 @@ def reload_configuration(
     event_status.reload_configuration(config, history)
     status_server.reload_configuration(config, history)
     logger.info("Reloaded configuration.")
+    return history
 
 
 # .

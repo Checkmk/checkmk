dfde2be26fd2790416121505a4c56bc4413d9a86
Jörg Herbel <joerg.herbel@checkmk.com>
2025-02-24 22:42:55 +0100

    Linux memory service: Improve handling of active and inactive memory
    * Always combine these two metrics into one graph. Before, this wasn't the case
      if we had finer-grained metrics (files and anonymous).
    * Place the now always shown graph before the graph containig active and
      inactive memory split by files and anonymous.
    
    CMK-21996
    
    Change-Id: Ia24b98733a4322dd1f7909e961a324dfcfcbfe76
    (cherry picked from commit 8e58cab8607ee8016c24facd7a50018965efe91b)

---
+++ cmk/plugins/collection/graphing/active_and_inactive_memory_files_and_anon.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.graphing.v1 import graphs, metrics, Title
+
+UNIT_BYTES = metrics.Unit(metrics.IECNotation("B"))
+
+metric_mem_lnx_active_anon = metrics.Metric(
+    name="mem_lnx_active_anon",
+    title=Title("Active (anonymous)"),
+    unit=UNIT_BYTES,
+    color=metrics.Color.BLUE,
+)
+metric_mem_lnx_active_file = metrics.Metric(
+    name="mem_lnx_active_file",
+    title=Title("Active (files)"),
+    unit=UNIT_BYTES,
+    color=metrics.Color.GREEN,
+)
+metric_mem_lnx_inactive_anon = metrics.Metric(
+    name="mem_lnx_inactive_anon",
+    title=Title("Inactive (anonymous)"),
+    unit=UNIT_BYTES,
+    color=metrics.Color.PURPLE,
+)
+metric_mem_lnx_inactive_file = metrics.Metric(
+    name="mem_lnx_inactive_file",
+    title=Title("Inactive (files)"),
+    unit=UNIT_BYTES,
+    color=metrics.Color.BROWN,
+)
+
+graph_active_and_inactive_memory_anon = graphs.Graph(
+    name="active_and_inactive_memory_anon",
+    title=Title("Active and inactive memory - files and anonymous"),
+    compound_lines=[
+        "mem_lnx_inactive_anon",
+        "mem_lnx_inactive_file",
+        "mem_lnx_active_anon",
+        "mem_lnx_active_file",
+    ],
+)

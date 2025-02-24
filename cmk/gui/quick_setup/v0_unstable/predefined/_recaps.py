19d97bde1aa21419510b83361476669c59280235
Wontek Hong <wontek.hong@checkmk.com>
2025-02-24 13:23:35 +0100

    quick_setup/_recaps: remove unused definitions
    Change-Id: I93707524ce24b2683d465308668622200f3c7988

--- cmk/gui/quick_setup/v0_unstable/predefined/_recaps.py
+++ cmk/gui/quick_setup/v0_unstable/predefined/_recaps.py
@@ -3,31 +3,21 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Callable, Mapping, Sequence
-from functools import partial
+from collections.abc import Sequence
 
 from cmk.gui.form_specs.vue.form_spec_visitor import serialize_data_for_frontend
 from cmk.gui.form_specs.vue.visitors import DataOrigin
-from cmk.gui.i18n import ungettext
 from cmk.gui.quick_setup.v0_unstable._registry import quick_setup_registry
 from cmk.gui.quick_setup.v0_unstable.predefined._common import (
-    _collect_params_with_defaults_from_form_data,
     build_formspec_map_from_stages,
 )
-from cmk.gui.quick_setup.v0_unstable.predefined._utils import (
-    get_service_discovery_preview,
-    group_services_by_interest,
-)
-from cmk.gui.quick_setup.v0_unstable.setups import CallableRecap, ProgressLogger
+from cmk.gui.quick_setup.v0_unstable.setups import ProgressLogger
 from cmk.gui.quick_setup.v0_unstable.type_defs import (
     ParsedFormData,
     QuickSetupId,
-    ServiceInterest,
     StageIndex,
 )
-from cmk.gui.quick_setup.v0_unstable.widgets import FormSpecRecap, ListOfWidgets, Text, Widget
-
-from cmk.rulesets.v1.form_specs import Dictionary
+from cmk.gui.quick_setup.v0_unstable.widgets import FormSpecRecap, Widget
 
 
 def recaps_form_spec(
@@ -56,62 +46,3 @@ def recaps_form_spec(
         for form_spec_id, form_data in parsed_form_data.items()
         if form_spec_id in quick_setup_formspec_map
     ]
-
-
-def recap_service_discovery_custom_collect_params(
-    rulespec_name: str,
-    parameter_form: Dictionary,
-    services_of_interest: Sequence[ServiceInterest],
-    custom_collect_params: Callable[[ParsedFormData, Dictionary], Mapping[str, object]],
-) -> CallableRecap:
-    return partial(
-        _recap_service_discovery,
-        rulespec_name,
-        parameter_form,
-        services_of_interest,
-        custom_collect_params,
-    )
-
-
-def recap_service_discovery(
-    rulespec_name: str,
-    parameter_form: Dictionary,
-    services_of_interest: Sequence[ServiceInterest],
-) -> CallableRecap:
-    return partial(
-        _recap_service_discovery,
-        rulespec_name,
-        parameter_form,
-        services_of_interest,
-        _collect_params_with_defaults_from_form_data,
-    )
-
-
-def _recap_service_discovery(
-    rulespec_name: str,
-    parameter_form: Dictionary,
-    services_of_interest: Sequence[ServiceInterest],
-    collect_params: Callable[[ParsedFormData, Dictionary], Mapping[str, object]],
-    _quick_setup_id: QuickSetupId,
-    _stage_index: StageIndex,
-    all_stages_form_data: ParsedFormData,
-) -> Sequence[Widget]:
-    service_discovery_result = get_service_discovery_preview(
-        rulespec_name=rulespec_name,
-        all_stages_form_data=all_stages_form_data,
-        parameter_form=parameter_form,
-        collect_params=collect_params,
-    )
-    check_preview_entry_by_service_interest, others = group_services_by_interest(
-        services_of_interest, service_discovery_result
-    )
-    items: list[Widget] = [
-        Text(
-            text=f"{len(check_preview_entries)} {service_interest.label}",
-        )
-        for service_interest, check_preview_entries in check_preview_entry_by_service_interest.items()
-    ]
-    if len(others) >= 1:
-        items.append(Text(text=ungettext("%s other service", "%s other services", len(others))))
-
-    return [ListOfWidgets(items=items, list_type="check")]

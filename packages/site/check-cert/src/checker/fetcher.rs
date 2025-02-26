f0b19830087b89d6b0f616e72101caa37c87b582
Lukas Lengler <lukas.lengler@checkmk.com>
2025-02-26 14:32:33 +0100

    17284 check_cert: remove defaults for response time
    When no levels for the response time have been passed
    to `check_cert` the active check used some default levels which
    caused some unexpected behaviour.
    
    Now, without any levels being passed, it will still show the a result but
    without a levels computation.
    
    CMK-21859
    
    Change-Id: Ifde548b1901331e65b70f81e371105e242537eb7

--- packages/site/check-cert/src/checker/fetcher.rs
+++ packages/site/check-cert/src/checker/fetcher.rs
@@ -16,28 +16,38 @@ pub fn check(response_time: Duration, config: Config) -> Check {
     Check::from(&mut vec![check_response_time(
         response_time,
         config.response_time,
-    )
-    .unwrap_or_default()])
+    )])
 }
 
 fn check_response_time(
     response_time: Duration,
     levels: Option<Levels<Duration>>,
-) -> Option<CheckResult<Real>> {
-    levels.map(|levels| {
-        CheckResult::notice_from_levels(
-            pretty_levels(
-                &format!("Response time: {} ms", response_time.as_millis()),
-                levels.clone().map(|x| Real::from(x.as_millis() as isize)),
-                "ms",
-            ),
+) -> CheckResult<Real> {
+    levels.map_or(
+        CheckResult::notice(
+            format!("Response time: {} ms", response_time.as_millis(),),
             Metric::builder()
                 .label("overall_response_time")
                 .value(response_time)
                 .uom("s".parse().unwrap())
-                .levels(Some(levels))
                 .build()
                 .map(|x| Real::from(x.as_secs_f64())),
-        )
-    })
+        ),
+        |levels| {
+            CheckResult::notice_from_levels(
+                pretty_levels(
+                    &format!("Response time: {} ms", response_time.as_millis()),
+                    levels.clone().map(|x| Real::from(x.as_millis() as isize)),
+                    "ms",
+                ),
+                Metric::builder()
+                    .label("overall_response_time")
+                    .value(response_time)
+                    .uom("s".parse().unwrap())
+                    .levels(Some(levels))
+                    .build()
+                    .map(|x| Real::from(x.as_secs_f64())),
+            )
+        },
+    )
 }

e7cc1c58bce3b01ae05ab53b2bc7ad5ae03c1ecc
Logan Connolly <logan.connolly@checkmk.com>
2025-02-28 09:09:58 +0100

    test(quick_setup): notification count limit
    CMK-22121
    
    Change-Id: I2849b003395a08d2f559dab31668fc961f36d046

--- tests/unit/cmk/gui/quick_setup/test_validators.py
+++ tests/unit/cmk/gui/quick_setup/test_validators.py
@@ -5,7 +5,10 @@
 
 import pytest
 
-from cmk.gui.wato.pages.notifications.quick_setup import validate_throttling_values
+from cmk.gui.wato.pages.notifications.quick_setup import (
+    validate_notification_count_values,
+    validate_throttling_values,
+)
 
 from cmk.rulesets.v1.form_specs.validators import ValidationError
 
@@ -35,3 +38,61 @@ def test_validate_throttling_values(payload: tuple[int, ...]) -> None:
 def test_validate_throttling_values_raises(payload: tuple[int, ...]) -> None:
     with pytest.raises(ValidationError):
         validate_throttling_values(payload)
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        pytest.param((1, 2), id="second value is greater than first"),
+        pytest.param(
+            (1, 1),
+            marks=pytest.mark.xfail(reason="this should be allowed"),
+            id="values are the same",
+        ),
+    ],
+)
+def test_validate_notification_count_values(payload: tuple[int, ...]) -> None:
+    assert validate_notification_count_values(payload) is None  # type: ignore[func-returns-value]
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        pytest.param(
+            tuple(),
+            marks=pytest.mark.xfail(reason="this should raise"),
+            id="no arguments provided",
+        ),
+        pytest.param(
+            (0, 1),
+            marks=pytest.mark.xfail(reason="this should raise"),
+            id="zero is invalid lower bound",
+        ),
+        pytest.param(
+            (0, 0),
+            id="zero is invalid upper bound",
+        ),
+        pytest.param(
+            (2, 1),
+            id="second value less than first",
+        ),
+        pytest.param(
+            ("foo", 2),
+            marks=pytest.mark.xfail(reason="this should raise"),
+            id="first value is the wrong type",
+        ),
+        pytest.param(
+            (1, "bar"),
+            marks=pytest.mark.xfail(reason="this should raise"),
+            id="second value is wrong type",
+        ),
+        pytest.param(
+            (1, 2, 3),
+            marks=pytest.mark.xfail(reason="this should raise"),
+            id="we only expect two inputs",
+        ),
+    ],
+)
+def test_validate_notification_count_values_raises(payload: tuple[int, ...]) -> None:
+    with pytest.raises(ValidationError):
+        validate_notification_count_values(payload)

2b38e2f348171ce3139083185469b546874298d2
Owen Synge <owen.synge@checkmk.com>
2025-03-04 14:33:12 +0100

    Bazel: Update rust to 1.79.0 and bazel rules rust to 0.54.1
    After changing MODULE.bazel and bazel/rules/rust_workspace.bzl
    other fileanges where autogenerated with the following commands:
    
        bazel mod deps --lockfile_mode=update
        CARGO_BAZEL_REPIN=1 bazel sync --only=cargo_deps_site
        CARGO_BAZEL_REPIN=1 bazel sync --only=cargo_deps_host
    
    CMK-22201
    
    Change-Id: Id96f2fcdd8c48d34a08f57dd99d5a23e199393a2

--- buildscripts/infrastructure/build-nodes/scripts/install-rust-cargo.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-rust-cargo.sh
@@ -14,14 +14,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # define toolchain version explicitly
 # 'stable' is allowed only for main(master) branch
-DEFAULT_TOOLCHAIN_VERSION="1.78"
+DEFAULT_TOOLCHAIN_VERSION="1.79"
 # Some packages require specific toolchain versions.
 # These versions will be installed in addition to the default toolchain version.
 # List the versions separated by space, e.g. "1 2 3", and add a reason below.
 #
 # Reasons for added toolchains:
 # - 1.75: Currently used by all packages
-ADDITIONAL_TOOLCHAIN_VERSIONS="1.75"
+ADDITIONAL_TOOLCHAIN_VERSIONS="1.78"
 
 DEFAULT_TARGET="x86_64-unknown-linux-gnu"
 # List additional targets here, separated by space.

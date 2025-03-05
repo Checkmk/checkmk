2ca5f2cbfda0e6bf0d79636e4ae933016aced1cd
Kenneth Okoh <kenneth.okoh@checkmk.com>
2025-03-05 14:43:17 +0100

    17007 FIX Allow sorting for view columns "IP addresses" and "IPv6 address"
    Views such as "All hosts" were not sortable by columns "IP adresses" ("Host addresses (IPv4/IPv6)") and "IPv6 address" ("Host address (IPv6)").
    
    With this werk views are now sortable by these two columns. By clicking the column header the table rows are sorted based on the respective table cell value.
    
    Change-Id: I0d8ba4f11731d4f20d438f41bb16943b41dd2a22

--- cmk/gui/views/sorter/sorters.py
+++ cmk/gui/views/sorter/sorters.py
@@ -6,7 +6,7 @@
 import time
 from collections.abc import Mapping, Sequence
 from functools import partial
-from typing import Any
+from typing import Any, Literal
 
 from cmk.gui import utils
 from cmk.gui.config import Config
@@ -54,6 +54,8 @@ def register_sorters(registry: SorterRegistry) -> None:
     registry.register(SorterSvcPerfVal10)
     registry.register(SorterCustomHostVariable)
     registry.register(SorterHostIpv4Address)
+    registry.register(SorterHostIpv6Address)
+    registry.register(SorterHostIpAddresses)
     registry.register(SorterNumProblems)
 
     declare_simple_sorter("svcdescr", _("Service name"), "service_description", cmp_service_name)
@@ -578,7 +580,8 @@ SorterCustomHostVariable = ParameterizedSorter(
 )
 
 
-def _sort_host_ipv4_address(
+def _sort_host_ip_addresses(
+    ip_versions: Sequence[Literal["ipv4", "ipv6"]],
     r1: Row,
     r2: Row,
     *,
@@ -586,20 +589,41 @@ def _sort_host_ipv4_address(
     config: Config,
     request: Request,
 ) -> int:
-    def get_address(row):
+    def get_address(row: Row, ipv: Literal["ipv4", "ipv6"]) -> str:
         custom_vars = dict(
             zip(row["host_custom_variable_names"], row["host_custom_variable_values"])
         )
-        return custom_vars.get("ADDRESS_4", "")
+        if ipv == "ipv4":
+            return custom_vars.get("ADDRESS_4", "")
+        return custom_vars.get("ADDRESS_6", "")
 
-    return compare_ips(get_address(r1), get_address(r2))
+    for ipv in ip_versions:
+        if (result := compare_ips(get_address(r1, ipv), get_address(r2, ipv), ipv)) != 0:
+            return result
+    return 0
 
 
 SorterHostIpv4Address = Sorter(
     ident="host_ipv4_address",
     title=_l("Host IPv4 address"),
     columns=["host_custom_variable_names", "host_custom_variable_values"],
-    sort_function=_sort_host_ipv4_address,
+    sort_function=partial(_sort_host_ip_addresses, ["ipv4"]),
+)
+
+
+SorterHostIpv6Address = Sorter(
+    ident="host_ipv6_address",
+    title=_l("Host IPv6 address"),
+    columns=["host_custom_variable_names", "host_custom_variable_values"],
+    sort_function=partial(_sort_host_ip_addresses, ["ipv6"]),
+)
+
+
+SorterHostIpAddresses = Sorter(
+    ident="host_addresses",
+    title=_l("Host addresses (IPv4/IPv6)"),
+    columns=["host_custom_variable_names", "host_custom_variable_values"],
+    sort_function=partial(_sort_host_ip_addresses, ["ipv4", "ipv6"]),
 )
 
 

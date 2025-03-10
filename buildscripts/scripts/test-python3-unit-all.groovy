fa3b83e07914c79bfefc101e5fd898b4fa1df4d8
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-03-10 09:47:18 +0100

    Switch to inside_container
    this automatically fixes the missing mount of the reference repo
    
    Change-Id: Ib0612521c6a6dd7200d60016eda101f17d75e884

--- buildscripts/scripts/test-python3-unit-all.groovy
+++ buildscripts/scripts/test-python3-unit-all.groovy
@@ -3,13 +3,10 @@
 /// file: test-python3-unit-all.groovy
 
 def main() {
-    def docker_args = "--ulimit nofile=1024:1024 --init";
-    docker.withRegistry(DOCKER_REGISTRY, 'nexus') {
-        docker_image_from_alias("IMAGE_TESTING").inside(docker_args) {
-            stage('run test-unit-all') {
-                dir("${checkout_dir}") {
-                    sh("make -C tests test-unit-all");
-                }
+    inside_container(init: true, ulimit_nofile: "1024") {
+        stage('run test-unit-all') {
+            dir("${checkout_dir}") {
+                sh("make -C tests test-unit-all");
             }
         }
     }

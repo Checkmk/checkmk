77283a6c5c0c378fb26143968ea6002f4bc63407
Gav <gavin.mcguigan@checkmk.com>
2025-03-11 16:30:31 +0100

    17581 FIX View: Events of host and services log details
    This werk addresses an issue when displaying log details
    within the Events of host & services view.
    
    Previously it was incorrectly showing an error when no
    html text was part of the log details.
    
    "HTML output can not be rendered because of truncated
    data. (Increase limit for future entries)WARN"
    
    With this werk, this error is no longer shown.
    
    SUP-21088
    
    Change-Id: I9e76266b3b46ca459fe56e6430d658d5a4dfa339

--- cmk/gui/views/painter/v0/painters.py
+++ cmk/gui/views/painter/v0/painters.py
@@ -4345,12 +4345,12 @@ class PainterLogDetailsHistory(Painter):
         # See werk #15523.
         is_ps_check = row["service_check_command"] == "check_mk-ps"
         # We can only display tables if they are complete.
-        displayable = long_output.endswith("</table>")
+        non_displayable_html = "<table>" in long_output and not long_output.endswith("</table>")
         # Only hand over relevant row to ensure correct escaping options in
         # case of ps_check. Otherwise "ESCAPE_PLUGIN_OUTPUT" would be used in
         # format_plugin_output()
         row_to_format = (
-            {"log_long_plugin_output": long_output} if is_ps_check and not displayable else row
+            {"log_long_plugin_output": long_output} if is_ps_check and non_displayable_html else row
         )
         content = format_plugin_output(
             output=long_output,
@@ -4361,7 +4361,7 @@ class PainterLogDetailsHistory(Painter):
         # escaping
         custom_vars = row.get("service_custom_variables", row.get("host_custom_variables", {}))
         escape_plugin_output = custom_vars.get("ESCAPE_PLUGIN_OUTPUT", "1") == "0"
-        if long_output_len > max_len and escape_plugin_output and not displayable:
+        if long_output_len > max_len and escape_plugin_output and not non_displayable_html:
             setting_url = makeuri_contextless(
                 request,
                 [

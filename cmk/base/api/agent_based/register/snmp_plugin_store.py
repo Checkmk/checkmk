abeb5458b08a812cd1b640582fd72bf04ddceef1
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-03-12 23:32:14 +0100

    inline helper function
    The usage of this function made things more complicated
    than necessary.
    
    Change-Id: I661b6986ff9ac70615940d2d0d479529b7d65e25

--- cmk/base/api/agent_based/register/snmp_plugin_store.py
+++ cmk/base/api/agent_based/register/snmp_plugin_store.py
@@ -3,9 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import itertools
-
-from cmk.utils.sectionname import SectionName
 
 from cmk.snmplib import (
     BackendSNMPTree,  # pylint: disable=cmk-module-layer-violation
@@ -19,34 +16,21 @@ from cmk.fetchers.snmp import (  # pylint: disable=cmk-module-layer-violation
 
 from cmk.base.api.agent_based.plugin_classes import AgentBasedPlugins
 
-from .utils import (
-    filter_relevant_raw_sections,
-)
-
 __all__ = ["make_plugin_store"]
 
 
-def _make_inventory_sections(plugins: AgentBasedPlugins) -> frozenset[SectionName]:
-    return frozenset(
-        s
-        for s in filter_relevant_raw_sections(
-            consumers=plugins.inventory_plugins.values(),
-            sections=itertools.chain(
-                plugins.agent_sections.values(), plugins.snmp_sections.values()
-            ),
-        )
-        if s in plugins.snmp_sections
-    )
-
-
 def make_plugin_store(plugins: AgentBasedPlugins) -> SNMPPluginStore:
-    inventory_sections = _make_inventory_sections(plugins)
+    parsed_sections_relevant_for_inventory = {
+        section_name
+        for plugin in plugins.inventory_plugins.values()
+        for section_name in plugin.sections
+    }
     return SNMPPluginStore(
         {
             s.name: SNMPPluginStoreItem(
                 [BackendSNMPTree.from_frontend(base=t.base, oids=t.oids) for t in s.trees],
                 SNMPDetectSpec(s.detect_spec),
-                s.name in inventory_sections,
+                s.parsed_section_name in parsed_sections_relevant_for_inventory,
             )
             for s in plugins.snmp_sections.values()
         }

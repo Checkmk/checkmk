8e4563ec7074f480d02a57ae06e828fd2c5b7c4d
Gav <gavin.mcguigan@checkmk.com>
2025-03-13 17:38:32 +0100

    17582 FIX downtimes: users with access to services only, should not be able to set a downtime on the host
    Previously, when a user assigned to a contact group that was in turn
    assigned to services via a rule, they could create a downtime on the
    host the services belonged to but then didn't have permission to
    remove the downtime.  This werk addresses this issue by not allowing
    the user to set the downtime if they do not have persmission to do so.
    
    SUP-22380
    
    Change-Id: Ibca3885a9f0d5bcc96446e4f1e0ab90fbadd4782

--- cmk/gui/views/command/commands.py
+++ cmk/gui/views/command/commands.py
@@ -1261,6 +1261,13 @@ PermissionRemoveAllDowntimes = Permission(
 )
 
 
+@request_memoize()
+def hosts_user_can_see() -> Sequence[str]:
+    return livestatus.LocalConnection().query_column(
+        query=f"GET hosts\nColumns: name\nAuthUser: {user.ident}"
+    )
+
+
 class CommandGroupDowntimes(CommandGroup):
     @property
     def ident(self) -> str:
@@ -1538,11 +1545,12 @@ class CommandScheduleDowntimesForm:
             html.close_div()
 
         open_submit_button_container_div(tooltip=tooltip_submission_disabled)
-        html.button(
-            "_down_host",
-            _("On host: Schedule downtime"),
-            cssclass="disabled" + ("" if is_service else " hot"),
-        )
+        if user.may("general.see_all") or hosts_user_can_see():
+            html.button(
+                "_down_host",
+                _("On host: Schedule downtime"),
+                cssclass="disabled" + ("" if is_service else " hot"),
+            )
         html.close_div()
 
         html.buttonlink(makeuri(request, [], delvars=["filled_in"]), _("Cancel"))
@@ -1808,6 +1816,10 @@ class CommandScheduleDowntimesForm:
             # as an approximation. This is good enough to count the affected hosts.
             seen = set()
             host_action_rows = []
+
+            if not user.may("general.see_all"):
+                action_rows = [ar for ar in action_rows if ar["host_name"] in hosts_user_can_see()]
+
             for action_row in action_rows:
                 if action_row["host_name"] not in seen:
                     seen.add(action_row["host_name"])

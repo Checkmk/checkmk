e4049af5e48324b7c4b5f1a32138e9b0262a1159
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-03-14 14:00:54 +0100

    http migrate: add command summary
    Change-Id: Ib20885ba329056149fd91e89c9d4146d6503b156

--- cmk/update_config/https/commands.py
+++ cmk/update_config/https/commands.py
@@ -5,6 +5,7 @@
 
 import dataclasses
 import sys
+from typing import NamedTuple
 
 from cmk.gui.utils import gen_id
 from cmk.gui.watolib.rulesets import Rule, Ruleset
@@ -13,24 +14,43 @@ from cmk.update_config.https.arguments import SearchArgs
 from cmk.update_config.https.conflict_options import Config
 from cmk.update_config.https.conflicts import Conflict, detect_conflicts, ForMigration
 from cmk.update_config.https.migrate import migrate
-from cmk.update_config.https.render import MIGRATE_POSTFIX
+from cmk.update_config.https.render import (
+    MIGRATE_POSTFIX,
+    print_summary_dryrun,
+    print_summary_finalize,
+    print_summary_write,
+)
 from cmk.update_config.https.search import select, with_allrulesets
+
+
+class _Count(NamedTuple):
+    conflicts: int
+    rules: int
+    skipped: int
 
 
 def _new_migrated_rules(
     search: SearchArgs, config: Config, ruleset_v1: Ruleset, ruleset_v2: Ruleset
-) -> None:
+) -> _Count:
+    conflict_count = 0
+    rule_count = 0
+    skip_count = 0
     for folder, rule_index, rule_v1 in select(ruleset_v1, search):
+        rule_count += 1
         if _migrated_rule(rule_v1.id, ruleset_v2) is None:
             for_migration = detect_conflicts(config, rule_v1.value)
             rule_str = _render_rule(folder.title(), rule_index)
             sys.stdout.write(f"{rule_str}\n")
             if isinstance(for_migration, Conflict):
+                conflict_count += 1
                 sys.stdout.write(f"Can't migrate: {for_migration.type_.value}\n")
                 continue
             rule_v2 = _construct_v2_rule(rule_v1, for_migration, ruleset_v2)
             index = ruleset_v2.append_rule(rule_v1.folder, rule_v2)
             sys.stdout.write(f"Migrated to v2 rule with index #{index}.\n")
+        else:
+            skip_count += 1
+    return _Count(conflicts=conflict_count, rules=rule_count, skipped=skip_count)
 
 
 def _render_rule(folder_title: str, rule_index: int) -> str:
@@ -78,10 +98,13 @@ def migrate_main(search: SearchArgs, config: Config, write: bool) -> None:
     with with_allrulesets() as all_rulesets:
         ruleset_v1 = all_rulesets.get("active_checks:http")
         ruleset_v2 = all_rulesets.get("active_checks:httpv2")
-        _new_migrated_rules(search, config, ruleset_v1, ruleset_v2)
+        count = _new_migrated_rules(search, config, ruleset_v1, ruleset_v2)
         if write:
             sys.stdout.write("Saving rule sets...\n")
             all_rulesets.save()
+            print_summary_write(count.conflicts, count.rules, count.skipped)
+        else:
+            print_summary_dryrun(count.conflicts, count.rules, count.skipped)
 
 
 def _strip_postfix(value: dict) -> None:
@@ -95,6 +118,8 @@ def finalize_main(search: SearchArgs) -> None:
     with with_allrulesets() as all_rulesets:
         ruleset_v1 = all_rulesets.get("active_checks:http")
         ruleset_v2 = all_rulesets.get("active_checks:httpv2")
+        rulecount_v1 = 0
+        rulecount_v2 = 0
         for folder, rule_index, rule_v2 in select(ruleset_v2, search):
             if (rule_v1_id := _migrated_from(rule_v2)) is not None:
                 rule_str = _render_rule(folder.title(), rule_index)
@@ -111,10 +136,13 @@ def finalize_main(search: SearchArgs) -> None:
                 )
                 _strip_postfix(new_rule_v2.value)
                 ruleset_v2.edit_rule(rule_v2, new_rule_v2)
+                rulecount_v2 += 1
                 if rule_v1 is not None:
+                    rulecount_v1 += 1
                     sys.stdout.write(f"Deleted v1 counter-part #{rule_v1.index()}.\n")
                     ruleset_v1.delete_rule(rule_v1)
                 sys.stdout.write("Finalized v2 rule #{rule_index}.\n")
+        print_summary_finalize(rulecount_v1, rulecount_v2)
         sys.stdout.write("Saving rule sets...\n")
         all_rulesets.save()
 
--- cmk/update_config/https/render.py
+++ cmk/update_config/https/render.py
@@ -3,4 +3,40 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import sys
+
 MIGRATE_POSTFIX = " (migrated)"
+
+
+def print_summary_dryrun(conflicts: int, rules: int, skipped: int) -> None:
+    sys.stdout.write(
+        f"Dry run results: {conflicts} conflict(s), {rules - skipped - conflicts} rule(s) can be migrated, {skipped} skipped.\n"
+    )
+    sys.stdout.write(
+        "Resolve conflicts by using the respective option. Use ‘cmk-migrate-http migrate -h’ for all options. Once you are satisfied with the results, use ‘cmk-migrate-http migrate --write’ to create the new rules.\n"
+    )
+
+
+def print_summary_write(conflicts: int, rules: int, skipped: int) -> None:
+    sys.stdout.write(
+        f"Summary: {conflicts} conflict(s), {rules - skipped - conflicts} rule(s) have been written to disk, {skipped} skipped.\n"
+    )
+    sys.stdout.write("Rules created by the script are deactivated by default.\n")
+    sys.stdout.write(
+        f"The new v2 rules have the id of their v1 counterpart in the description, and use ‘{MIGRATE_POSTFIX}’ suffix in the service name.\n"
+    )
+    sys.stdout.write(
+        "To see the new services in monitoring and compare the output with the old services activate the rules using ‘cmk-migrate-http activate’.\n"
+    )
+
+
+def print_summary_finalize(rulecount_v1: int, rulecount_v2: int) -> None:
+    sys.stdout.write(
+        f"Summary: {rulecount_v1} v1 rule(s) deleted, {rulecount_v2} v2 rule(s) finalized\n"
+    )
+    if rulecount_v1:
+        sys.stdout.write("Migrated v1 rule(s) have been deleted.\n")
+    if rulecount_v2:
+        sys.stdout.write(
+            f"The v2 rule(s) have now been edited so that the ‘{MIGRATE_POSTFIX}’ suffix is removed from the service name and the reference to the v1 rule has been deleted from the description. \n"
+        )

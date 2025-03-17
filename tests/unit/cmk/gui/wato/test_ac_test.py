10e640c321b61982d00a190cb7119e6f4365663d
Simon Jess <simon.jess@checkmk.com>
2025-03-17 14:00:41 +0100

    Deprecations: Compute deprecation results
    CMK-22393
    
    Change-Id: Ibda6a742d86aaba06506ee2f5971d60731e25a53

--- tests/unit/cmk/gui/wato/test_ac_test.py
+++ tests/unit/cmk/gui/wato/test_ac_test.py
@@ -2,9 +2,17 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+
+from pathlib import Path
+
+import pytest
+
+from livestatus import SiteId
+
 from cmk.utils.livestatus_helpers.testing import MockLiveStatusConnection
 
-from cmk.gui.wato._ac_tests import ACTestGenericCheckHelperUsage
+from cmk.gui.wato._ac_tests import _compute_deprecation_result, ACTestGenericCheckHelperUsage
+from cmk.gui.watolib.analyze_configuration import ACResultState, ACSingleResult
 
 
 def test_local_connection_mocked(
@@ -22,3 +30,73 @@ def test_local_connection_mocked(
     with live(expect_status_query=False):
         gen = ACTestGenericCheckHelperUsage().execute()
         list(gen)
+
+
+@pytest.mark.parametrize(
+    "version, result",
+    [
+        pytest.param(
+            "1.2.6",
+            ACSingleResult(
+                state=ACResultState.CRIT,
+                text="Entity uses an API (API) which was removed in Checkmk 1.2.5.",
+                site_id=SiteId("site_id"),
+                path=Path("/path/to/file"),
+            ),
+            id="was_removed",
+        ),
+        pytest.param(
+            "1.2.5",
+            ACSingleResult(
+                state=ACResultState.CRIT,
+                text="Entity uses an API (API) which was marked as deprecated in Checkmk 1.2.3 and is removed in Checkmk 1.2.5.",
+                site_id=SiteId("site_id"),
+                path=Path("/path/to/file"),
+            ),
+            id="removed",
+        ),
+        pytest.param(
+            "1.2.4",
+            ACSingleResult(
+                state=ACResultState.WARN,
+                text="Entity uses an API (API) which was marked as deprecated in Checkmk 1.2.3 and will be removed in Checkmk 1.2.5.",
+                site_id=SiteId("site_id"),
+                path=Path("/path/to/file"),
+            ),
+            id="was_deprecated",
+        ),
+        pytest.param(
+            "1.2.3",
+            ACSingleResult(
+                state=ACResultState.WARN,
+                text="Entity uses an API (API) which is marked as deprecated in Checkmk 1.2.3 and will be removed in Checkmk 1.2.5.",
+                site_id=SiteId("site_id"),
+                path=Path("/path/to/file"),
+            ),
+            id="deprecated",
+        ),
+        pytest.param(
+            "1.2.2",
+            ACSingleResult(
+                state=ACResultState.OK,
+                text="",
+                site_id=SiteId("site_id"),
+                path=Path("/path/to/file"),
+            ),
+            id="ok",
+        ),
+    ],
+)
+def test__compute_deprecation_result(version: str, result: ACSingleResult) -> None:
+    assert (
+        _compute_deprecation_result(
+            version=version,
+            deprecated_version="1.2.3",
+            removed_version="1.2.5",
+            title_entity="Entity",
+            title_api="API",
+            site_id=SiteId("site_id"),
+            path=Path("/path/to/file"),
+        )
+        == result
+    )

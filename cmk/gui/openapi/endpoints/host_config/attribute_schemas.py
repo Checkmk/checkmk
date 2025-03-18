743bb9efcb9284377ed7022fb57329f01a2d2e70
Wontek Hong <wontek.hong@checkmk.com>
2025-03-18 09:17:30 +0100

    attribute_schemas: introduce base class for host attribute schema
    Change-Id: I1cac17ffcbac3af66dca7f552bb19bea1a4a9410

--- cmk/gui/openapi/endpoints/host_config/attribute_schemas.py
+++ cmk/gui/openapi/endpoints/host_config/attribute_schemas.py
@@ -31,12 +31,13 @@ from cmk.gui.watolib.groups import HostAttributeContactGroups
 from cmk import fields
 
 
-class HostCreateAttribute(BaseSchema):
+class BaseHostAttribute(BaseSchema):
+    """Base class for all host attribute schemas."""
+
     alias = HostAttributeAlias().openapi_field()
     site = HostAttributeSite().openapi_field()
     parents = HostAttributeParents().openapi_field()
 
-    # Definition should be removed
     contactgroups = fields.Nested(
         HostContactGroup,
         description=HostAttributeContactGroups().help(),
@@ -54,9 +55,9 @@ class HostCreateAttribute(BaseSchema):
     snmp_community = HostAttributeSNMPCommunity().openapi_field()
 
     labels = HostAttributeLabels().openapi_field()
+    waiting_for_discovery = HostAttributeWaitingForDiscovery().openapi_field()
 
     network_scan = HostAttributeNetworkScan().openapi_field()
-    waiting_for_discovery = HostAttributeWaitingForDiscovery().openapi_field()
 
     management_protocol = HostAttributeManagementProtocol().openapi_field()
     management_address = HostAttributeManagementAddress().openapi_field()
@@ -68,118 +69,18 @@ class HostCreateAttribute(BaseSchema):
     inventory_failed = HostAttributeDiscoveryFailed().openapi_field()
 
 
-class HostViewAttribute(BaseSchema):
-    alias = HostAttributeAlias().openapi_field()
-    site = HostAttributeSite().openapi_field()
+class HostCreateAttribute(BaseHostAttribute):
+    pass
 
-    contactgroups = fields.Nested(
-        HostContactGroup,
-        description=HostAttributeContactGroups().help(),
-    )
 
-    parents = HostAttributeParents().openapi_field()
-
-    ipaddress = HostAttributeIPv4Address().openapi_field()
-    ipv6address = HostAttributeIPv6Address().openapi_field()
-    additional_ipv4addresses = HostAttributeAdditionalIPv4Addresses().openapi_field()
-    additional_ipv6addresses = HostAttributeAdditionalIPv6Addresses().openapi_field()
-
-    # TODO: CEE specific host attribute must be added without violating the import structure
-    #  the registration should be edition specific instead (temporarily it would also be fine
-    #  to do something like the customer attribute to keep simplicity
-    # bake_agent_package = HostAttributeBakeAgentPackage().openapi_field()
-
-    snmp_community = HostAttributeSNMPCommunity().openapi_field()
-
-    labels = HostAttributeLabels().openapi_field()
-    waiting_for_discovery = HostAttributeWaitingForDiscovery().openapi_field()
-
-    network_scan = HostAttributeNetworkScan().openapi_field()
+class HostViewAttribute(BaseHostAttribute):
     network_scan_result = HostAttributeNetworkScanResult().openapi_field()
-
-    management_protocol = HostAttributeManagementProtocol().openapi_field()
-    management_address = HostAttributeManagementAddress().openapi_field()
-    management_snmp_community = HostAttributeManagementSNMPCommunity().openapi_field()
-    management_ipmi_credentials = HostAttributeManagementIPMICredentials().openapi_field()
-
     meta_data = HostAttributeMetaData().openapi_field()
 
-    locked_by = HostAttributeLockedBy().openapi_field()
-    locked_attributes = HostAttributeLockedAttributes().openapi_field()
-    inventory_failed = HostAttributeDiscoveryFailed().openapi_field()
+
+class HostUpdateAttribute(BaseHostAttribute):
+    pass
 
 
-class HostUpdateAttribute(BaseSchema):
-    alias = HostAttributeAlias().openapi_field()
-    site = HostAttributeSite().openapi_field()
-
-    contactgroups = fields.Nested(
-        HostContactGroup,
-        description=HostAttributeContactGroups().help(),
-    )
-
-    parents = HostAttributeParents().openapi_field()
-
-    ipaddress = HostAttributeIPv4Address().openapi_field()
-    ipv6address = HostAttributeIPv6Address().openapi_field()
-    additional_ipv4addresses = HostAttributeAdditionalIPv4Addresses().openapi_field()
-    additional_ipv6addresses = HostAttributeAdditionalIPv6Addresses().openapi_field()
-
-    # TODO: CEE specific host attribute must be added without violating the import structure
-    #  the registration should be edition specific instead (temporarily it would also be fine
-    #  to do something like the customer attribute to keep simplicity
-    # bake_agent_package = HostAttributeBakeAgentPackage().openapi_field()
-
-    snmp_community = HostAttributeSNMPCommunity().openapi_field()
-
-    labels = HostAttributeLabels().openapi_field()
-    waiting_for_discovery = HostAttributeWaitingForDiscovery().openapi_field()
-
-    network_scan = HostAttributeNetworkScan().openapi_field()
-
-    management_protocol = HostAttributeManagementProtocol().openapi_field()
-    management_address = HostAttributeManagementAddress().openapi_field()
-    management_snmp_community = HostAttributeManagementSNMPCommunity().openapi_field()
-    management_ipmi_credentials = HostAttributeManagementIPMICredentials().openapi_field()
-
-    locked_by = HostAttributeLockedBy().openapi_field()
-    locked_attributes = HostAttributeLockedAttributes().openapi_field()
-    inventory_failed = HostAttributeDiscoveryFailed().openapi_field()
-
-
-class ClusterCreateAttribute(BaseSchema):
-    alias = HostAttributeAlias().openapi_field()
-    site = HostAttributeSite().openapi_field()
-
-    contactgroups = fields.Nested(
-        HostContactGroup,
-        description=HostAttributeContactGroups().help(),
-    )
-
-    parents = HostAttributeParents().openapi_field()
-
-    ipaddress = HostAttributeIPv4Address().openapi_field()
-    ipv6address = HostAttributeIPv6Address().openapi_field()
-    additional_ipv4addresses = HostAttributeAdditionalIPv4Addresses().openapi_field()
-    additional_ipv6addresses = HostAttributeAdditionalIPv6Addresses().openapi_field()
-
-    # TODO: CEE specific host attribute must be added without violating the import structure
-    #  the registration should be edition specific instead (temporarily it would also be fine
-    #  to do something like the customer attribute to keep simplicity
-    # bake_agent_package = HostAttributeBakeAgentPackage().openapi_field()
-
-    snmp_community = HostAttributeSNMPCommunity().openapi_field()
-
-    labels = HostAttributeLabels().openapi_field()
-    waiting_for_discovery = HostAttributeWaitingForDiscovery().openapi_field()
-
-    network_scan = HostAttributeNetworkScan().openapi_field()
-
-    management_protocol = HostAttributeManagementProtocol().openapi_field()
-    management_address = HostAttributeManagementAddress().openapi_field()
-    management_snmp_community = HostAttributeManagementSNMPCommunity().openapi_field()
-    management_ipmi_credentials = HostAttributeManagementIPMICredentials().openapi_field()
-
-    locked_by = HostAttributeLockedBy().openapi_field()
-    locked_attributes = HostAttributeLockedAttributes().openapi_field()
-    inventory_failed = HostAttributeDiscoveryFailed().openapi_field()
+class ClusterCreateAttribute(BaseHostAttribute):
+    pass

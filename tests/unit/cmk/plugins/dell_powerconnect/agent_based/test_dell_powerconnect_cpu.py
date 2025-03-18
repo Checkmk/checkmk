335531fe3cf67b14a3b030f21cacc00b47eb33c9
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-03-18 16:28:01 +0100

    dell_powerconnect: add test to document current behaviour
    CMK-21722
    
    Change-Id: Ie49ee6d8094f4dd8c55a5ee75b088f2c1370c7b0

--- tests/unit/cmk/plugins/dell_powerconnect/agent_based/test_dell_powerconnect_cpu.py
+++ tests/unit/cmk/plugins/dell_powerconnect/agent_based/test_dell_powerconnect_cpu.py
@@ -6,11 +6,18 @@
 from collections.abc import Callable
 from pathlib import Path
 
+import pytest
+
 from tests.unit.cmk.plugins.collection.agent_based.snmp import (
     get_parsed_snmp_section,
     snmp_is_detected,
 )
 
+from cmk.checkengine.checking import CheckPluginName
+
+from cmk.base.api.agent_based.register import AgentBasedPlugins
+
+from cmk.agent_based.v2 import CheckResult, IgnoreResultsError, Metric, Result, Service, State
 from cmk.plugins.dell_powerconnect.agent_based.dell_powerconnect_cpu import (
     snmp_section_dell_powerconnect_cpu,
 )
@@ -34,3 +41,74 @@ def test_cpu_parse(
     assert get_parsed_snmp_section(snmp_section_dell_powerconnect_cpu, snmp_walk) == [
         ["1", "91", "10", "4"]
     ]
+
+
+def test_cpu_discover(agent_based_plugins: AgentBasedPlugins) -> None:
+    plugin = agent_based_plugins.check_plugins[CheckPluginName("dell_powerconnect_cpu")]
+    assert (
+        list(
+            plugin.discovery_function(
+                [["1", "91", "10", "4"]],
+            )
+        )
+    ) == [
+        Service(),
+    ]
+
+
+@pytest.mark.parametrize(
+    "section, result",
+    [
+        (
+            [["1", "91", "10", "4"]],
+            [
+                Result(
+                    state=State.CRIT, summary="CPU utilization: 91.00% (warn/crit at 80.00%/90.00%)"
+                ),
+                Metric("util", 91.0, levels=(80.0, 90.0), boundaries=(0.0, 100.0)),
+                Metric("util1", 10.0, boundaries=(0.0, 100.0)),
+                Metric("util5", 4.0, boundaries=(0.0, 100.0)),
+            ],
+        ),
+        (
+            [["0", "91", "10", "4"]],
+            [],
+        ),
+        (
+            [["1", "-10", "10", "4"]],
+            [],
+        ),
+        (
+            [["1", "999", "10", "4"]],
+            [],
+        ),
+    ],
+)
+def test_cpu_check(
+    agent_based_plugins: AgentBasedPlugins, section: object, result: CheckResult
+) -> None:
+    plugin = agent_based_plugins.check_plugins[CheckPluginName("dell_powerconnect_cpu")]
+
+    assert (
+        list(
+            plugin.check_function(
+                item=None,
+                params={"levels": (80.0, 90.0)},
+                section=section,
+            )
+        )
+        == result
+    )
+
+
+def test_cpu_check_ignore(agent_based_plugins: AgentBasedPlugins) -> None:
+    plugin = agent_based_plugins.check_plugins[CheckPluginName("dell_powerconnect_cpu")]
+
+    with pytest.raises(IgnoreResultsError, match="Ignoring empty data"):
+        list(
+            plugin.check_function(
+                item=None,
+                params={"levels": (80.0, 90.0)},
+                section=[["something"]],
+            )
+        )
--- tests/unit/cmk/plugins/dell_powerconnect/agent_based/test_dell_powerconnect_temp.py
+++ tests/unit/cmk/plugins/dell_powerconnect/agent_based/test_dell_powerconnect_temp.py
@@ -6,11 +6,18 @@
 from collections.abc import Callable
 from pathlib import Path
 
+import pytest
+
 from tests.unit.cmk.plugins.collection.agent_based.snmp import (
     get_parsed_snmp_section,
     snmp_is_detected,
 )
 
+from cmk.checkengine.checking import CheckPluginName
+
+from cmk.base.api.agent_based.register import AgentBasedPlugins
+
+from cmk.agent_based.v2 import CheckResult, Metric, Result, Service, State
 from cmk.plugins.dell_powerconnect.agent_based.dell_powerconnect_temp import (
     snmp_section_dell_powerconnect_temp,
 )
@@ -30,3 +37,63 @@ def test_temp_parse(
     assert snmp_is_detected(snmp_section_dell_powerconnect_temp, snmp_walk)
 
     assert get_parsed_snmp_section(snmp_section_dell_powerconnect_temp, snmp_walk) == (42.0, "OK")
+
+
+def test_temp_discover(agent_based_plugins: AgentBasedPlugins) -> None:
+    plugin = agent_based_plugins.check_plugins[CheckPluginName("dell_powerconnect_temp")]
+    assert (
+        list(
+            plugin.discovery_function(
+                (42.0, "OK"),
+            )
+        )
+    ) == [
+        Service(item="Ambient"),
+    ]
+
+
+@pytest.mark.parametrize(
+    "section, result",
+    [
+        (
+            (None, "OK"),
+            [
+                Result(state=State.OK, summary="Status: OK"),
+            ],
+        ),
+        (
+            (41.0, "OK"),
+            [
+                Result(state=State.OK, summary="41.0 °C"),
+                Metric("temp", 41.0),
+            ],
+        ),
+        (
+            (41.0, "unavailable"),
+            [
+                Result(state=State.WARN, summary="41.0 °C, State on device: unavailable"),
+                Metric("temp", 41.0),
+            ],
+        ),
+        (
+            (41.0, "non operational"),
+            [
+                Result(state=State.CRIT, summary="41.0 °C, State on device: non operational"),
+                Metric("temp", 41.0),
+            ],
+        ),
+        (
+            (41.0, "something"),
+            [
+                Result(state=State.UNKNOWN, summary="41.0 °C, State on device: something"),
+                Metric("temp", 41.0),
+            ],
+        ),
+    ],
+)
+def test_temp_check(
+    agent_based_plugins: AgentBasedPlugins, section: object, result: CheckResult
+) -> None:
+    plugin = agent_based_plugins.check_plugins[CheckPluginName("dell_powerconnect_temp")]
+
+    assert list(plugin.check_function(item="Ambient", params={}, section=section)) == result

34f7325d9b710ad5328696d82fac1c7a9ae4fc78
Wontek Hong <wontek.hong@checkmk.com>
2025-03-18 07:52:28 +0100

    host_config: addition of static schema for host view attributes
    Change-Id: I855b3bf60787f9f2cfe2b34ee7d83f377ef387c2

--- cmk/gui/openapi/endpoints/host_config/attribute_schemas.py
+++ cmk/gui/openapi/endpoints/host_config/attribute_schemas.py
@@ -18,7 +18,9 @@ from cmk.gui.watolib.builtin_attributes import (
     HostAttributeManagementIPMICredentials,
     HostAttributeManagementProtocol,
     HostAttributeManagementSNMPCommunity,
+    HostAttributeMetaData,
     HostAttributeNetworkScan,
+    HostAttributeNetworkScanResult,
     HostAttributeParents,
     HostAttributeSite,
     HostAttributeSNMPCommunity,
@@ -60,6 +62,47 @@ class HostCreateAttribute(BaseSchema):
     management_address = HostAttributeManagementAddress().openapi_field()
     management_snmp_community = HostAttributeManagementSNMPCommunity().openapi_field()
     management_ipmi_credentials = HostAttributeManagementIPMICredentials().openapi_field()
+
+    locked_by = HostAttributeLockedBy().openapi_field()
+    locked_attributes = HostAttributeLockedAttributes().openapi_field()
+    inventory_failed = HostAttributeDiscoveryFailed().openapi_field()
+
+
+class HostViewAttribute(BaseSchema):
+    alias = HostAttributeAlias().openapi_field()
+    site = HostAttributeSite().openapi_field()
+
+    contactgroups = fields.Nested(
+        HostContactGroup,
+        description=HostAttributeContactGroups().help(),
+    )
+
+    parents = HostAttributeParents().openapi_field()
+
+    ipaddress = HostAttributeIPv4Address().openapi_field()
+    ipv6address = HostAttributeIPv6Address().openapi_field()
+    additional_ipv4addresses = HostAttributeAdditionalIPv4Addresses().openapi_field()
+    additional_ipv6addresses = HostAttributeAdditionalIPv6Addresses().openapi_field()
+
+    # TODO: CEE specific host attribute must be added without violating the import structure
+    #  the registration should be edition specific instead (temporarily it would also be fine
+    #  to do something like the customer attribute to keep simplicity
+    # bake_agent_package = HostAttributeBakeAgentPackage().openapi_field()
+
+    snmp_community = HostAttributeSNMPCommunity().openapi_field()
+
+    labels = HostAttributeLabels().openapi_field()
+    waiting_for_discovery = HostAttributeWaitingForDiscovery().openapi_field()
+
+    network_scan = HostAttributeNetworkScan().openapi_field()
+    network_scan_result = HostAttributeNetworkScanResult().openapi_field()
+
+    management_protocol = HostAttributeManagementProtocol().openapi_field()
+    management_address = HostAttributeManagementAddress().openapi_field()
+    management_snmp_community = HostAttributeManagementSNMPCommunity().openapi_field()
+    management_ipmi_credentials = HostAttributeManagementIPMICredentials().openapi_field()
+
+    meta_data = HostAttributeMetaData().openapi_field()
 
     locked_by = HostAttributeLockedBy().openapi_field()
     locked_attributes = HostAttributeLockedAttributes().openapi_field()

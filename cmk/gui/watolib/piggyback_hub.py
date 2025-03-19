6f383b8b5cbf50fe175ee45c8ffa830afb9b8a32
David Gervasoni <david.gervasoni@checkmk.com>
2025-03-19 15:10:14 +0100

    Piggyback-hub: restore config distribution via broker
    if piggyback-hub becomes enabled on a remote site, the remote site is marked as ‘dirty’
    in order to trigger a sync of changes
    and distribute the configuration for the piggyback-hub, via the message broker.
    
    Change-Id: I89a5ebb497ef2ae139c639d06a8df9b904a05c59

--- cmk/gui/watolib/piggyback_hub.py
+++ cmk/gui/watolib/piggyback_hub.py
@@ -3,6 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import subprocess
 from collections.abc import Collection, Iterable, Mapping
 
 from livestatus import SiteConfiguration, SiteId
@@ -15,16 +16,20 @@ from cmk.utils.paths import omd_root
 from cmk.gui.type_defs import GlobalSettings
 from cmk.gui.watolib.site_changes import ChangeSpec
 
+from cmk.messaging.rabbitmq import rabbitmqctl_running
 from cmk.piggyback.hub import HostLocations, publish_persisted_locations
 
 _HOST_CHANGES = (
-    "edit-host",
+    "edit-host",  # includes moving a host from a site to another
     "create-host",
     "delete-host",
     "rename-host",
     "move-host",
     "edit-folder",
+    "piggyback-hub-turned-on",
 )
+
+_REMOTE_PIGGYBACK_STATUS_RELPATH = "etc/check_mk/remote_piggyback_hub_status"
 
 
 def has_piggyback_hub_relevant_changes(pending_changes: Iterable[ChangeSpec]) -> bool:
@@ -81,3 +86,42 @@ def _filter_for_enabled_piggyback_hub(
         for site_id, site_config in configured_sites.items()
         if _piggyback_hub_enabled(site_config, global_settings) is True
     }
+
+
+def _read_stored_piggyback_hub_status() -> Mapping[SiteId, str]:
+    try:
+        with open(omd_root.joinpath(_REMOTE_PIGGYBACK_STATUS_RELPATH), "r") as f:
+            return {SiteId(site): status for site, status in (line.split() for line in f)}
+    except FileNotFoundError:
+        return {}
+
+
+def _store_piggyback_hub_status(sites_status: Mapping[SiteId, int]) -> None:
+    with open(omd_root.joinpath(_REMOTE_PIGGYBACK_STATUS_RELPATH), "w") as f:
+        for site, status in sites_status.items():
+            f.write(f"{site} {"on" if status == 0 else "off"}\n")
+
+
+def changed_remote_piggyback_hub_status(sites_status: Mapping[SiteId, int]) -> set[SiteId]:
+    previous_status = _read_stored_piggyback_hub_status()
+    _store_piggyback_hub_status(sites_status)
+
+    sites_changed = set()
+    for site, status in sites_status.items():
+        status_literal = "on" if status == 0 else "off"
+        if status_literal == "off" or previous_status.get(site, "off") == status_literal:
+            continue
+
+        # a site is "changed" if the current status is "on"
+        # and the previous status was "off" or not present
+        sites_changed.add(site)
+
+    if sites_changed and not rabbitmqctl_running():
+        subprocess.Popen(
+            ["omd", "start", "rabbitmq"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+    return sites_changed

0a4674897e0e2ef8c897ea48a807264b13fd4e3e
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-03-19 14:08:38 +0100

    Revert "werks: restore website-essential workflow"
    This reverts commit 722265ec5117cd9d0a5edc461bf624fab00ed5af.
    
    The workaround is no longer needed
    
    CMK-19085
    
    Change-Id: Iab55afb5997f9413f99d42ceb1f0bb05c2b07ddd

--- cmk/utils/werks/__init__.py
+++ cmk/utils/werks/__init__.py
@@ -23,8 +23,7 @@ from typing import Protocol, TypeVar
 
 import cmk.utils.paths
 
-from cmk.werks.models import Werk, WerkV1
-from cmk.werks.parse import parse_werk_v1
+from cmk.werks.models import Werk
 from cmk.werks.utils import (
     load_precompiled_werks_file,
 )
@@ -50,25 +49,3 @@ def load(base_dir: Path | None = None) -> dict[int, Werk]:
     for file_name in [(base_dir / "werks"), *base_dir.glob("werks-*")]:
         werks.update(load_precompiled_werks_file(file_name))
     return werks
-
-
-# THIS IS A TEMPORARY FIX FOR THE OLD WORKFLOW
-
-
-def load_raw_files_old(werks_dir: Path) -> list[WerkV1]:
-    if werks_dir is None:
-        werks_dir = _compiled_werks_dir()
-    werks: list[WerkV1] = []
-    for file_name in werks_dir.glob("[0-9]*"):
-        try:
-            parsed = parse_werk_v1(file_name.read_text(), int(file_name.name))
-            werk: dict[str, str | int | list[str]] = {}
-            werk.update(parsed.metadata)
-            werk["description"] = parsed.description
-            werks.append(WerkV1.model_validate(werk))
-        except Exception as e:
-            raise RuntimeError(f"Could not parse werk {file_name.absolute()}") from e
-    return werks
-
-
-# TEMPORARY FIX END!

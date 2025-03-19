cd819413410f6536b7ecbfab7c11ca65a8c9d17a
Luka Racic <luka.racic@checkmk.com>
2025-03-19 19:41:25 +0100

    Changed the formatting of the warning message
    CMK-22481
    
    Change-Id: Ib927cab631457a7004bb5dde3bd2f8e93daa64a6

--- cmk/update_config/plugins/actions/http_rules.py
+++ cmk/update_config/plugins/actions/http_rules.py
@@ -2,9 +2,10 @@
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
 from logging import Logger
 from typing import override
+
+from cmk.utils import tty
 
 from cmk.gui.watolib.rulesets import AllRulesets
 
@@ -17,18 +18,18 @@ class CheckHTTPRules(UpdateAction):
         http_ruleset = AllRulesets.load_all_rulesets().get("active_checks:http")
         if http_ruleset is not None:
             if (count := len(http_ruleset.get_rules())) > 0:
-                logger.warning(
-                    f"WARNING: You have {count} rules using the ruleset Check HTTP service."
-                )
                 logger.info(
-                    "This ruleset will be deprecated along with the check_http monitoring plug-in in the next version(s) of Checkmk."
-                )
-                logger.info(
-                    "Rules must therefore be migrated to the new ruleset of the check_httpv2 plugin."
-                )
-                logger.info(
-                    "Rule migration can be done manually or by calling migrate_httpv2 as site user. See migrate_httpv2 --help for more information "
-                    "to this helper script. For additional information please see the werk #17665."
+                    tty.format_warning(
+                        f"You have {tty.yellow}{count} {tty.white}{'rule' if count == 1 else 'rules'} "
+                        f"using the ruleset {tty.yellow}Check HTTP service deprecated{tty.white}.\n"
+                        "This ruleset will be deprecated along with the old HTTP "
+                        "monitoring plug-in in the next version(s) of Checkmk.\n"
+                        "Rules must therefore be migrated to the new ruleset which is used by the httpv2 plugin.\n"
+                        f"Rule migration can be done manually or by calling {tty.yellow}cmk-migrate-http "
+                        f"{tty.white}as site user. See cmk-migrate-http --help "
+                        "for more information on this helper script.\n"
+                        "For additional information on the deprecation of the HTTP plug-in see the werk #17665."
+                    )
                 )
 
 

b487e9ca636a40130f203c2f93c95a59cc2e38b2
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-03-21 13:17:16 +0100

    17631 FIX sap_hana_status: Always discover the Status service
    Minimal fix for SUP-22109. Consolidation of these services
    would be desirable.
    
    Change-Id: I2602590ff4654b58dbb064aeb665041896ff78a6

--- cmk/base/plugins/agent_based/sap_hana_status.py
+++ cmk/base/plugins/agent_based/sap_hana_status.py
@@ -34,7 +34,9 @@ def parse_sap_hana_status(string_table: StringTable) -> sap_hana.ParsedSection:
                     "instance": sid_instance,
                     "version": line[2],
                 }
-            section["%s %s" % (item_name, sid_instance)] = item_data
+            # always discover "Status", even if we don't have an error now
+            section[f"Status {sid_instance}"] = item_data
+            section[f"{item_name} {sid_instance}"] = item_data
 
     return section
 

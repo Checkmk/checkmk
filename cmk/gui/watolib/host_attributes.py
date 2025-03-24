b5b152bf77a107c752d85735d49fa8827008e1d2
Andreas Boesl <andreas.boesl@checkmk.com>
2025-03-24 11:51:40 +0100

    added memoized version of host_attribute default_value
    Change-Id: Ifa4f4aaf37fa6270c757d0a652e0a1d96d2d7547

--- cmk/gui/watolib/host_attributes.py
+++ cmk/gui/watolib/host_attributes.py
@@ -1320,3 +1320,8 @@ def EnumAttribute(
             "_enumlist": enumlist,
         },
     )
+
+
+@request_memoize(maxsize=1024)
+def get_host_attribute_default_value(host_attribute: ABCHostAttribute) -> Any:
+    return host_attribute.default_value()

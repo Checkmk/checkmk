3e2db871a3aa588c8f898e79dae0da0dde47f3b9
Logan Connolly <logan.connolly@checkmk.com>
2025-03-25 15:33:40 +0100

    wato: specifiy override in child class
    Change-Id: Id5f23f4c412f02bef97e908a90573fbfe6188668

--- cmk/gui/wato/pages/_rule_conditions.py
+++ cmk/gui/wato/pages/_rule_conditions.py
@@ -2,8 +2,8 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from collections.abc import Sequence
-from typing import Any, TypeVar
+from collections.abc import Mapping, Sequence
+from typing import Any, override, TypeVar
 
 from cmk.utils.tags import AuxTag, TagGroup, TagGroupID, TagID
 
@@ -20,6 +20,7 @@ from cmk.gui.valuespec import (
     ListOfMultiple,
     Transform,
     Tuple,
+    ValueSpec,
 )
 
 _TagChoiceID = TypeVar("_TagChoiceID", TagGroupID, TagID)
@@ -215,5 +216,6 @@ class DictHostTagCondition(Transform):
 
 
 class PageAjaxDictHostTagConditionGetChoice(ABCPageListOfMultipleGetChoice):
-    def _get_choices(self, api_request):
+    @override
+    def _get_choices(self, api_request: Mapping[str, str]) -> Sequence[tuple[str, ValueSpec]]:
         return _get_tag_group_choices()

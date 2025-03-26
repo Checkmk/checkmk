Checkmk/checkmk

commit b8ebe0b94dd9af30c111ba463aa93af755caad34

17714 FIX Fix change detection of clustered services for discovered changes
On clustered services the discovery parameters are written to each individual autochecks
file, but when the currently active node was not the first node in the cluster the
Service discovery detected this a a changed service, even though the correct parameters
have already been written to the correct nodes autocheck file.

Now, active nodes are being prioritized when detecting changed services which means this
problem will no longer occur.

CMK-22351

Change-Id: I5342a0e27a61646848995f2f032b4c47de610486

cmk/checkengine/discovery/_autochecks.py
@@ -138,19 +138,21 @@ class AutochecksStore:
 
 def merge_cluster_autochecks(
     autochecks: Mapping[HostName, Sequence[AutocheckEntry]],
+    active_nodes: Sequence[HostName],
     appears_on_cluster: Callable[[HostName, AutocheckEntry], bool],
 ) -> Sequence[AutocheckEntry]:
     # filter for cluster and flatten:
-    entries = [
-        e
+    entries = {
+        node_name: e
         for node_name, entries in autochecks.items()
         for e in entries
         if appears_on_cluster(node_name, e)
-    ]
+    }
 
     # group by service id and reverse order to make the first node win in merging
+    # but prioritize the current active nodes
     entries_by_id: dict[ServiceID, list[AutocheckEntry]] = defaultdict(list)
-    for entry in entries:
+    for node_name, entry in sorted(entries.items(), key=lambda x: x[0] not in active_nodes):
         entries_by_id[entry.id()].insert(0, entry)
 
     return [

tests/unit/cmk/checkengine/discovery/test__autodiscovery.py
@@ -41,6 +41,24 @@ AUTOCHECK_2 = AutocheckEntry(
     {"parameter_common": "2", "parameter_2": "2"},
     {"label_common": "2", "label_2": "2"},
 )
+AUTOCHECK_3A = AutocheckEntry(
+    CheckPluginName("check_plugin_3"),
+    "Item",
+    {"parameter_common": "a"},
+    {"label_common": "a"},
+)
+AUTOCHECK_3B = AutocheckEntry(
+    CheckPluginName("check_plugin_3"),
+    "Item",
+    {"parameter_common": "b"},
+    {"label_common": "b"},
+)
+AUTOCHECK_3_MERGED_B_ACTIVE = AutocheckEntry(
+    CheckPluginName("check_plugin_3"),
+    "Item",
+    {"parameter_common": "b"},
+    {"label_common": "b"},
+)
 
 
 class _AutochecksConfigDummy:
@@ -266,6 +284,33 @@ def test_get_host_services_by_host_name_swaps_on_cluster() -> None:
         "unchanged": [
             AutocheckServiceWithNodes(
                 service=DiscoveredItem(previous=AUTOCHECK_1A, new=AUTOCHECK_1A),
+                nodes=[NODE_2],
+            )
+        ],
+    }
+
+
+def test_get_host_services_by_host_name_params_prio_on_active_nodes() -> None:
+    assert get_host_services_by_host_name(
+        CLUSTER,
+        existing_services={
+            NODE_1: [AUTOCHECK_3A],
+            NODE_2: [AUTOCHECK_3B],
+        },
+        discovered_services={
+            NODE_1: [],
+            NODE_2: [AUTOCHECK_3B],
+        },
+        is_cluster=True,
+        cluster_nodes=(NODE_1, NODE_2),
+        autochecks_config=_AutochecksConfigDummy(effective_host=CLUSTER),
+        enforced_services={},
+    )[CLUSTER] == {
+        "unchanged": [
+            AutocheckServiceWithNodes(
+                service=DiscoveredItem(
+                    previous=AUTOCHECK_3_MERGED_B_ACTIVE, new=AUTOCHECK_3_MERGED_B_ACTIVE
+                ),
                 nodes=[NODE_2],
             )
         ],

cmk/base/config.py
@@ -438,6 +438,7 @@ def _get_clustered_services(
             cluster_name,
             merge_cluster_autochecks(
                 {node: get_autochecks(node) for node in nodes},
+                nodes,
                 appears_on_cluster,
             ),
         )

cmk/checkengine/discovery/_autodiscovery.py
@@ -765,13 +765,21 @@ def _get_cluster_services(
         hn: make_table(hn, entries, autochecks_config)
         for hn, entries in nodes_discovery_results.items()
     }
+    active_nodes = [
+        hn
+        for hn, q in nodes_discovery_results.items()
+        for e in q.current
+        if appears_on_cluster(hn, e)
+    ]
     clusters_discovery_result = QualifiedDiscovery(
         preexisting=merge_cluster_autochecks(
             {hn: q.preexisting for hn, q in nodes_discovery_results.items()},
+            active_nodes,
             appears_on_cluster,
         ),
         current=merge_cluster_autochecks(
             {hn: q.current for hn, q in nodes_discovery_results.items()},
+            active_nodes,
             appears_on_cluster,
         ),
     )

commit 042cedd9eadf9b92ee5ca6b9637d96ad3ff08d2d

17714 merge_cluster_autochecks: follow-up fix
Broken by b8ebe0b94dd9af30c111ba463aa93af755caad34

Due to the dict comprehension we could only have one autocheck entry
per node.

Now we use a list of tuples instead.

Such a rookie mistake...

CMK-22351

Change-Id: I2f0882582d5a4c11382a0d66532fde608564f0d8

cmk/checkengine/discovery/_autochecks.py
@@ -87,17 +87,17 @@ def merge_cluster_autochecks(
     appears_on_cluster: Callable[[HostName, AutocheckEntry], bool],
 ) -> Sequence[AutocheckEntry]:
     # filter for cluster and flatten:
-    entries = {
-        node_name: e
+    entries = [
+        (node_name, e)
         for node_name, entries in autochecks.items()
         for e in entries
         if appears_on_cluster(node_name, e)
-    }
+    ]
 
     # group by service id and reverse order to make the first node win in merging
     # but prioritize the current active nodes
     entries_by_id: dict[ServiceID, list[AutocheckEntry]] = defaultdict(list)
-    for node_name, entry in sorted(entries.items(), key=lambda x: x[0] not in active_nodes):
+    for node_name, entry in sorted(entries, key=lambda x: x[0] not in active_nodes):
         entries_by_id[entry.id()].insert(0, entry)
 
     return [

tests/unit/cmk/checkengine/discovery/test__autodiscovery.py
@@ -15,6 +15,7 @@ from cmk.checkengine.plugins import AutocheckEntry, CheckPluginName
 
 NODE_1 = HostAddress("node1")
 NODE_2 = HostAddress("node2")
+NODE_3 = HostAddress("node3")
 CLUSTER = HostAddress("cluster")
 
 AUTOCHECK_1A = AutocheckEntry(
@@ -313,5 +314,36 @@ def test_get_host_services_by_host_name_params_prio_on_active_nodes() -> None:
                 ),
                 nodes=[NODE_2],
             )
+        ],
+    }
+
+
+def test_get_host_services_by_host_name_move_mutiple_nodes_and_autochecks() -> None:
+    assert get_host_services_by_host_name(
+        CLUSTER,
+        existing_services={
+            NODE_1: [AUTOCHECK_1A, AUTOCHECK_2],
+            NODE_2: [],
+            NODE_3: [],
+        },
+        discovered_services={
+            NODE_1: [],
+            NODE_2: [],
+            NODE_3: [AUTOCHECK_1A, AUTOCHECK_2],
+        },
+        is_cluster=True,
+        cluster_nodes=(NODE_1, NODE_2, NODE_3),
+        autochecks_config=_AutochecksConfigDummy(effective_host=CLUSTER),
+        enforced_services={},
+    )[CLUSTER] == {
+        "unchanged": [
+            AutocheckServiceWithNodes(
+                service=DiscoveredItem(previous=AUTOCHECK_1A, new=AUTOCHECK_1A),
+                nodes=[NODE_3],
+            ),
+            AutocheckServiceWithNodes(
+                service=DiscoveredItem(previous=AUTOCHECK_2, new=AUTOCHECK_2),
+                nodes=[NODE_3],
+            ),
         ],
     }
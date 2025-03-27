b14d4558954aeafcf804b3a11fa8a913f564f3c8
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-03-27 08:48:09 +0100

    cmk-migrate-http: adopt existing summary
    Change-Id: I5543a5fdf26ecc4c9cf5f35482ab6b42b8964bb7

--- cmk/update_config/https/render.py
+++ cmk/update_config/https/render.py
@@ -13,7 +13,8 @@ def print_summary_dryrun(conflicts: int, rules: int, skipped: int) -> None:
         f"Dry run results: {conflicts} conflict(s), {rules - skipped - conflicts} rule(s) can be migrated, {skipped} skipped.\n"
     )
     sys.stdout.write(
-        "Resolve conflicts by using the respective option. Use ‘cmk-migrate-http migrate -h’ for all options. Once you are satisfied with the results, use ‘cmk-migrate-http migrate --write’ to create the new rules.\n"
+        "Resolve conflicts by adding the corresponding  options. Use ‘cmk-migrate-http migrate -h’ for all options. "
+        "Once you are satisfied with the results, use ‘cmk-migrate-http migrate --write’ to create the new rules.\n"
     )
 
 
@@ -28,15 +29,27 @@ def print_summary_write(conflicts: int, rules: int, skipped: int) -> None:
     sys.stdout.write(
         "To see the new services in monitoring and compare the output with the old services activate the rules using ‘cmk-migrate-http activate’.\n"
     )
+    sys.stdout.write(
+        "Once satisfied with the monitoring, use ‘cmk-migrate-http finalize’ to delete the v1 rules and clean up the v2 rules.\n"
+    )
 
 
 def print_summary_finalize(rulecount_v1: int, rulecount_v2: int) -> None:
     sys.stdout.write(
         f"Summary: {rulecount_v1} v1 rule(s) deleted, {rulecount_v2} v2 rule(s) finalized\n"
     )
-    if rulecount_v1:
-        sys.stdout.write("Migrated v1 rule(s) have been deleted.\n")
-    if rulecount_v2:
+    if rulecount_v1 or rulecount_v2:
+        if rulecount_v1 < rulecount_v2:
+            sys.stdout.write(
+                "Script was not able to find and delete all v1 rules that were migrated. "
+                "This could be because they were already manually deleted or the reference in the v2 rule had been removed. "
+                "The v2 rules have now been edited so that the ‘migrated’ prefix is removed from the service name and the reference to the v1 rule has been deleted from the description. "
+            )
+        else:
+            sys.stdout.write(
+                "Migrated v1 rule(s) have been deleted. "
+                f"The v2 rule(s) have now been edited so that the ‘{MIGRATE_POSTFIX}’ suffix is removed from the service name and the reference to the v1 rule has been deleted from the description. "
+            )
         sys.stdout.write(
-            f"The v2 rule(s) have now been edited so that the ‘{MIGRATE_POSTFIX}’ suffix is removed from the service name and the reference to the v1 rule has been deleted from the description. \n"
+            "For the changes to take effect, complete the process by activating the changes on the site.\n"
         )

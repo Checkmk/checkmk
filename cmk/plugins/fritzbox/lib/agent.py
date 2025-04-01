a3a047a9da4d3b169b2acae57f2f9bcac34fbc36
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-04-01 12:21:43 +0200

    Fix-up version for special agents
    used command was:
    make -C agents NEW_VERSION=2.4.0b4 setversion
    
    Change-Id: Ib6a7dd9f0f98a17aa819d2db5415bce0ed3f0879

--- cmk/plugins/fritzbox/lib/agent.py
+++ cmk/plugins/fritzbox/lib/agent.py
@@ -39,7 +39,7 @@ import requests
 
 from cmk.special_agents.v0_unstable.misc import vcrtrace
 
-__version__ = "2.4.0b3"
+__version__ = "2.4.0b4"
 
 USER_AGENT = f"checkmk-special-fritzbox-{__version__}"
 

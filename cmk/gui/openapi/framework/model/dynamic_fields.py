298a798a56b02ca35f81c9968dd9cd770fdf2ba6
Christopher Krause <christopher.krause@checkmk.com>
2025-04-01 14:41:10 +0200

    rest-api: add WithDynamicFields model
    Change-Id: I7aabb3a4a1523cc0ca10551bd5600c310c113d01

---
+++ cmk/gui/openapi/framework/model/dynamic_fields.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""Handling of dynamic fields in REST API models.
+
+This module provides the `WithDynamicFields` class, which is used as a base class for models that
+support dynamic fields. An example would be user defined host attributes.
+"""
+
+import dataclasses
+from collections.abc import Mapping
+
+from pydantic import GetJsonSchemaHandler
+from pydantic.json_schema import JsonSchemaValue
+from pydantic_core import ArgsKwargs, core_schema
+from pydantic_core.core_schema import CoreSchema, SerializerFunctionWrapHandler
+
+
+@dataclasses.dataclass(kw_only=True, slots=True)
+class WithDynamicFields:
+    """This will add support for dynamic fields in the schema.
+
+    Instantiation of the class still requires the dynamic fields to be explicitly specified under
+    the `dynamic_fields` key. The specific use case here is the validation and serialization with
+    pydantic/TypeAdapters.
+
+    The `dynamic_fields` field can be overridden, meeting the following requirements:
+    * the type must be a mapping, with the keys being strings
+    * the mappings values can be any type, but they must be (de-)serializable to JSON
+    * the field must have a default or default_factory
+    * the field can include additional metadata, which will be included in the OpenAPI schema.
+    """
+
+    dynamic_fields: Mapping[str, object] = dataclasses.field(default_factory=dict)
+
+    @classmethod
+    def _populate_dynamic_fields(cls, values: object) -> object:
+        known_fields = {f.name for f in dataclasses.fields(cls)}
+        if isinstance(values, ArgsKwargs):
+            if values.kwargs is None:
+                return values
+
+            extra_fields = values.kwargs.keys() - known_fields
+            values.kwargs["dynamic_fields"] = {f: values.kwargs.pop(f) for f in extra_fields}
+        elif isinstance(values, dict):
+            extra_fields = values.keys() - known_fields
+            values["dynamic_fields"] = {f: values.pop(f) for f in extra_fields}
+        else:
+            raise TypeError(f"Unsupported type {type(values)}")
+        return values
+
+    @classmethod
+    def _serialize_dynamic_fields(cls, value: object, nxt: SerializerFunctionWrapHandler) -> object:
+        serialized = nxt(value)
+        serialized.update(serialized.pop("dynamic_fields", {}))
+        return serialized
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: CoreSchema, handler: GetJsonSchemaHandler
+    ) -> CoreSchema:
+        return core_schema.no_info_before_validator_function(
+            cls._populate_dynamic_fields,
+            schema=handler(source_type),
+            serialization=core_schema.wrap_serializer_function_ser_schema(
+                cls._serialize_dynamic_fields,
+                info_arg=False,
+            ),
+        )
+
+    @classmethod
+    def __get_pydantic_json_schema__(
+        cls, core_schema: CoreSchema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        json_schema = handler(core_schema)
+        json_schema = handler.resolve_ref_schema(json_schema)
+        # remove the field from the schema, it should be replaced by "additionalProperties"
+        if (
+            (dynamic_field_schema := json_schema["properties"].pop("dynamic_fields", None))
+            and isinstance(dynamic_field_schema, dict)
+            and dynamic_field_schema.get("type") == "object"
+        ):
+            # the schema for `dynamic_fields` is a dict. `additionalProperties` in the json schema
+            # should only be the value schema, as the keys are implicitly strings.
+            dynamic_field_schema.update(dynamic_field_schema.pop("additionalProperties", {}))
+            json_schema["additionalProperties"] = dynamic_field_schema
+        else:
+            # this is supported by OpenAPI
+            json_schema["additionalProperties"] = True
+        return json_schema
---
+++ tests/unit/cmk/gui/openapi/framework/test_dynamic_fields.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+import dataclasses
+import datetime
+import json
+
+from pydantic import TypeAdapter
+
+from cmk.gui.openapi.framework.model import json_dump_without_omitted
+from cmk.gui.openapi.framework.model.dynamic_fields import WithDynamicFields
+
+_DT_2025 = datetime.datetime(2025, 1, 1, 0, 0, 0, tzinfo=datetime.UTC)
+
+
+@dataclasses.dataclass
+class _TestModel(WithDynamicFields):
+    field: int
+
+
+def test_dynamic_field_serialization() -> None:
+    model = _TestModel(
+        field=123,
+        dynamic_fields={
+            "extra_field": "extra_value",
+            "other_type": _DT_2025,
+        },
+    )
+
+    result = TypeAdapter(_TestModel).dump_json(model)  # nosemgrep: type-adapter-detected
+    # to not rely on the key order, we compare the loaded json
+    assert json.loads(result) == {
+        "field": 123,
+        "extra_field": "extra_value",
+        "other_type": "2025-01-01T00:00:00Z",
+    }
+
+
+def test_dynamic_field_serialization_without_omitted() -> None:
+    # we need to make sure this works with the `json_dump_without_omitted` function
+    model = _TestModel(
+        field=123,
+        dynamic_fields={
+            "extra_field": "extra_value",
+            "other_type": _DT_2025,
+        },
+    )
+
+    result = json_dump_without_omitted(_TestModel, model)
+    assert result == {
+        "field": 123,
+        "extra_field": "extra_value",
+        "other_type": "2025-01-01T00:00:00Z",
+    }
+
+
+def test_dynamic_field_deserialization() -> None:
+    adapter = TypeAdapter(_TestModel)  # nosemgrep: type-adapter-detected
+    model = adapter.validate_python(
+        {
+            "field": 123,
+            "extra_field": "extra_value",
+            "other_type": _DT_2025,
+        }
+    )
+    assert model.field == 123
+    assert model.dynamic_fields["extra_field"] == "extra_value"
+    assert model.dynamic_fields["other_type"] == _DT_2025
+
+
+@dataclasses.dataclass
+class _NestedModel:
+    nested: _TestModel
+    nested_list: list[_TestModel]
+
+
+def test_dynamic_field_serialization_nested() -> None:
+    model = _NestedModel(
+        nested=_TestModel(
+            field=123,
+            dynamic_fields={
+                "other_type": _DT_2025,
+            },
+        ),
+        nested_list=[
+            _TestModel(
+                field=456,
+                dynamic_fields={
+                    "extra_field": "extra_value",
+                },
+            )
+        ],
+    )
+
+    result = TypeAdapter(_NestedModel).dump_json(model)  # nosemgrep: type-adapter-detected
+    # to not rely on the key order, we compare the loaded json
+    assert json.loads(result) == {
+        "nested": {"field": 123, "other_type": "2025-01-01T00:00:00Z"},
+        "nested_list": [{"field": 456, "extra_field": "extra_value"}],
+    }
+
+
+def test_dynamic_field_serialization_nested_without_omitted() -> None:
+    # we need to make sure this works with the `json_dump_without_omitted` function
+    model = _NestedModel(
+        nested=_TestModel(
+            field=123,
+            dynamic_fields={
+                "other_type": _DT_2025,
+            },
+        ),
+        nested_list=[
+            _TestModel(
+                field=456,
+                dynamic_fields={
+                    "extra_field": "extra_value",
+                },
+            )
+        ],
+    )
+
+    result = json_dump_without_omitted(_NestedModel, model)
+    assert result == {
+        "nested": {"field": 123, "other_type": "2025-01-01T00:00:00Z"},
+        "nested_list": [{"field": 456, "extra_field": "extra_value"}],
+    }
+
+
+def test_dynamic_field_deserialization_nested() -> None:
+    adapter = TypeAdapter(_NestedModel)  # nosemgrep: type-adapter-detected
+    model = adapter.validate_python(
+        {
+            "nested": {
+                "field": 123,
+                "other_type": _DT_2025,
+            },
+            "nested_list": [
+                {
+                    "field": 456,
+                    "extra_field": "extra_value",
+                }
+            ],
+        }
+    )
+    assert model.nested.field == 123
+    assert model.nested.dynamic_fields["other_type"] == _DT_2025
+    assert len(model.nested_list) == 1
+    assert model.nested_list[0].field == 456
+    assert model.nested_list[0].dynamic_fields["extra_field"] == "extra_value"
+
+
+def test_json_schema_dynamic_field() -> None:
+    model = TypeAdapter(_TestModel)  # nosemgrep: type-adapter-detected
+    schema = model.json_schema()
+    # the `WithDynamicFields` class should only add the `additionalProperties` key
+    # properties and required must not change
+    assert schema["type"] == "object"
+    assert schema["title"] == "_TestModel"
+    assert "field" in schema["properties"]
+    assert len(schema["properties"]) == 1, (
+        f"Expected only `field` in properties: {schema['properties']}"
+    )
+    assert schema["required"] == ["field"]
+    # no specific type for dynamic fields set, default should be `object`
+    assert schema["additionalProperties"]["type"] == "object"
+
+
+def test_json_schema_dynamic_field_custom_type() -> None:
+    @dataclasses.dataclass(kw_only=True)
+    class _CustomType(WithDynamicFields):
+        field: int
+        dynamic_fields: dict[str, str] = dataclasses.field(
+            default_factory=dict,
+            metadata={
+                "title": "custom_title",
+                "description": "custom_description",
+                "examples": ["custom_example"],
+            },
+        )
+
+    model = TypeAdapter(_CustomType)  # nosemgrep: type-adapter-detected
+    schema = model.json_schema()
+    assert "dynamic_fields" not in schema["properties"]
+    assert "dynamic_fields" not in schema["required"]
+    assert schema["additionalProperties"]["type"] == "string"
+    assert schema["additionalProperties"]["title"] == "custom_title"
+    assert schema["additionalProperties"]["description"] == "custom_description"
+    assert schema["additionalProperties"]["examples"] == ["custom_example"]

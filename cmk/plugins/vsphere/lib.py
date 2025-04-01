5e8b1981fae4db771a38fbf27a6b62e5e0061750
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-04-01 11:48:26 +0200

    Clean up ruleset for vSphere special agent
    This change introduces different "Type of queries" with
    pre-selected infos to be queried.
    
    CMK-3217
    
    Change-Id: If74b60a21db19188c57a59cdfa540852df2bd63a

---
+++ cmk/plugins/vsphere/lib.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+from enum import StrEnum
+from typing import Literal
+
+
+class QueryType(StrEnum):
+    #: Queried host is a vCenter
+    VCENTER = "vcenter"
+    #: Queried host is a ESXi host (vCenter integrated)
+    HOST_SYSTEM = "host_system"
+    #: Queried host is a ESXi host (Standalone / not vCenter integrated)
+    STANDALONE = "standalone"
+
+
+InfoSelection = Literal["hostsystem", "virtualmachine", "datastore", "counters"]

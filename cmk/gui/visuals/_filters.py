1e6d7bb9eb80cd05246525dcd62249df841ed0da
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-04-01 16:10:39 +0200

    17830 Enable negate for log summary output filter
    Unlike the service summary regex filter, it was not possible to negate the log summary regex.
    
    This option is now enabled.
    
    Change-Id: I660a001b7959c2b6841ec5e9f020ca4fc9ed7289

--- cmk/gui/visuals/_filters.py
+++ cmk/gui/visuals/_filters.py
@@ -1126,6 +1126,7 @@ def register_log_filters(filter_registry: FilterRegistry) -> None:
             query_filter=query_filters.TextQuery(
                 ident="log_plugin_output",
                 op="~~",
+                negateable=True,
             ),
         )
     )

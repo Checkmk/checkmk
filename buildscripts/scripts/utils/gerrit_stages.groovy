a8a7d4dcd91b130b624dbfa7213753a4dbdbc296
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-04-03 15:47:09 +0200

    Fixup groovy code to pass linter
    Change-Id: Ia03fc3ab7726646550266db656b9188c6cba269c

--- buildscripts/scripts/utils/gerrit_stages.groovy
+++ buildscripts/scripts/utils/gerrit_stages.groovy
@@ -10,6 +10,7 @@ def log_stage_duration(last_stage_date) {
         this_stage_date,
         last_stage_date,
     );
+    println("+ Stage duration: ${duration}");
     return this_stage_date;
 }
 

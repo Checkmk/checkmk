d199d7709fd3ee0b0cc27a48ceb89d5a531e7dc3
Dhananjay Shah <dhananjay.shah@checkmk.com>
2025-04-03 10:22:56 +0200

    integration-tests: validation presence and version of 'check_httpv2'.
    Added docstring to 'test_monitoring_plugins_can_be_executed'.
    
    CMK-21661
    
    Change-Id: Ib8ec84b51684d46310a49286b454c17ce10065fb
    (cherry picked from commit 23d84126892b3b6ef626af85957dc764231dcbfc)

--- tests/integration/omd/test_packages.py
+++ tests/integration/omd/test_packages.py
@@ -18,16 +18,26 @@ StreamType = Literal["stderr", "stdout"]
 
 @dataclass(frozen=True)
 class MonitoringPlugin:
+    """Data corresponding to 3rd party plugins.
+
+    These plugins are maintained by 'Monitoring Plugins'
+    (https://www.monitoring-plugins.org/).
+    """
+
     binary_name: str
     path: str = "lib/nagios/plugins"
+    # by default, output the version of the plugin
     cmd_line_option: str = "-V"
     expected: str = "v2.4.0"
 
 
 @dataclass(frozen=True)
 class CheckmkActiveCheck:
+    """Data corresponding to Checkmk active checks."""
+
     binary_name: str
     path: str = "lib/nagios/plugins"
+    usage_text: str = "usage"
 
     @property
     def cmd_line_option(self) -> str:
@@ -35,7 +45,7 @@ class CheckmkActiveCheck:
 
     @property
     def expected(self) -> str:
-        return f"usage: {self.binary_name} "
+        return f"{self.usage_text}: {self.binary_name} "
 
 
 Plugin = MonitoringPlugin | CheckmkActiveCheck
@@ -101,6 +111,7 @@ MONITORING_PLUGINS: Sequence[Plugin] = (
     MonitoringPlugin("urlize"),
     MonitoringPlugin("check_mysql"),
     MonitoringPlugin("check_mysql_query"),
+    CheckmkActiveCheck("check_httpv2", usage_text="Usage"),
     CheckmkActiveCheck("check_always_crit"),
     CheckmkActiveCheck("check_sftp"),
     CheckmkActiveCheck(
@@ -143,6 +154,7 @@ MONITORING_PLUGINS: Sequence[Plugin] = (
     (pytest.param(p, id=f"{p.binary_name}") for p in MONITORING_PLUGINS),
 )
 def test_monitoring_plugins_can_be_executed(plugin: Plugin, site: Site) -> None:
+    """Validate the plugin's presence and version in the site."""
     abort_if_not_containerized(
         plugin.binary_name == "check_mysql"
     )  # What? Why? Is printing the version dangerous?

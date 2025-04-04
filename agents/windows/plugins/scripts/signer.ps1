e629cb5ab431ecdd8e2a369fde45ebd0fb15134c
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-04-04 16:14:23 +0200

    wa: add better diagnostic to signer.ps1
    Change-Id: I18be7e1b8b13a65bb22dcdba6a562532a7151dff

--- agents/windows/plugins/scripts/signer.ps1
+++ agents/windows/plugins/scripts/signer.ps1
@@ -12,6 +12,12 @@ if (Test-Path "c:\common\scsigntool.exe" -PathType Leaf) {
     $pin = 469673
     $cert = "7b97b15df65358623576584b7aafbe04d6668a0e"
     &c:\common\scsigntool.exe -pin $pin sign /sha1 $cert /tr http://timestamp.sectigo.com /td sha256 /fd sha256 $target
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Sign $target failed with exit code $LASTEXITCODE" -foreground Red
+    }
+    else {
+        Write-Host "Sign $target succeeded" -foreground Green
+    }
 }
 else {
     Write-Host "skip signing"

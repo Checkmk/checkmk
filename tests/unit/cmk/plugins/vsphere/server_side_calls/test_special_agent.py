bdb49b32a749ecac514ea1098ecac8e57cab79d6
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-04-04 10:55:52 +0200

    Add missing option to vSphere query selection
    The Literal defining the possible information to be
    queries from a vCenter or ESX host was missing the
    "licenses" option.
    
    CMK-3217
    
    Change-Id: I3d18066ab62272f36fec18c688a987c57fb230f5

--- tests/unit/cmk/plugins/vsphere/server_side_calls/test_special_agent.py
+++ tests/unit/cmk/plugins/vsphere/server_side_calls/test_special_agent.py
@@ -97,6 +97,47 @@ from cmk.server_side_calls.v1 import HostConfig, IPv4Config, Secret, SpecialAgen
             ),
             id="with store password",
         ),
+        pytest.param(
+            {
+                "tcp_port": 443,
+                "direct": (
+                    QueryType.HOST_SYSTEM,
+                    [
+                        "licenses",
+                    ],
+                ),
+                "skip_placeholder_vms": True,
+                "ssl": ("deactivated", None),
+                "secret": Secret(23),
+                "spaces": "cut",
+                "user": "username",
+                "snapshots_on_host": False,
+            },
+            HostConfig(
+                name="host",
+                ipv4_config=IPv4Config(address="1.2.3.4"),
+            ),
+            SpecialAgentCommand(
+                command_arguments=[
+                    "-p",
+                    "443",
+                    "-u",
+                    "username",
+                    Secret(23).unsafe("-s=%s"),
+                    "-i",
+                    "licenses",
+                    "--direct",
+                    "--hostname",
+                    "host",
+                    "-P",
+                    "--spaces",
+                    "cut",
+                    "--no-cert-check",
+                    "1.2.3.4",
+                ],
+            ),
+            id="only licenses",
+        ),
     ],
 )
 def test_vsphere_argument_parsing(

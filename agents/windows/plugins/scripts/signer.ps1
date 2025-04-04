95ca77dc03fceffe39e98433e3e423f534649e2c
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-04-04 16:56:32 +0200

    wa: improve logic in the signer.ps1
    Change-Id: I1c7a211a89b7415f37b6fb0e63822018c1b36a41

--- agents/windows/plugins/scripts/signer.ps1
+++ agents/windows/plugins/scripts/signer.ps1
@@ -5,20 +5,21 @@ param (
     $target
 )
 
-Write-Host "Copy $source to $target"
-Copy-Item -Path $source -Destination $target -Force
-Write-Host "Sign $target"
 if (Test-Path "c:\common\scsigntool.exe" -PathType Leaf) {
+    Write-Host "Copy $source to $target"
+    Copy-Item -Path $source -Destination $target -Force
+    Write-Host "Sign $target"
     $pin = 469673
     $cert = "7b97b15df65358623576584b7aafbe04d6668a0e"
-    &c:\common\scsigntool.exe -pin $pin sign /sha1 $cert /tr http://timestamp.sectigo.com /td sha256 /fd sha256 $target
+    &c:\common\scsigntool.exe -pin $pin sign /sha1 $cert /tr http://timestamp.sectigo.com /td sha256 /fd sha256 $target *>&1
     if ($LASTEXITCODE -ne 0) {
-        Write-Host "Sign $target failed with exit code $LASTEXITCODE" -foreground Red
+        Write-Host "Sign $target failed with exit code <$LASTEXITCODE>" -foreground Red
+        Remove-Item -Path $target -Force -ErrorAction SilentlyContinue
     }
     else {
         Write-Host "Sign $target succeeded" -foreground Green
     }
 }
 else {
-    Write-Host "skip signing"
+    Write-Host "Skip signing $source to $target, scsigntool.exe not found" -foreground Red
 }

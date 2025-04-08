be673275a18ceacc08a7c42cdc68b1dcef5c60e6
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-04-08 10:07:50 +0200

    improve dropdown rendering
    vuejs dropdowns should now look more like select2 dropdowns used in the
    old gui components
    
    Change-Id: I58a102ef33fef11b3f9dbbe2a96f47324b32174b

--- packages/cmk-frontend-vue/src/components/graphics/ArrowDown.vue
+++ packages/cmk-frontend-vue/src/components/graphics/ArrowDown.vue
@@ -3,14 +3,15 @@ Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
 This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 conditions defined in the file COPYING, which is part of this source code package.
 -->
+
 <template>
   <svg
-    width="2.508mm"
-    height="2.1721mm"
+    width="100%"
+    height="100%"
     version="1.1"
-    viewBox="0 0 2.508 2.1721"
+    viewBox="0 0 50 31.22"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="m1.2539 2.1718-1.254-2.1721h2.508l-0.62702 1.086z" fill="currentColor" />
+    <path d="m0 0h50l-25 31.22z" fill="currentColor" />
   </svg>
 </template>

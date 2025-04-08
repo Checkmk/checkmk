cee8f1658fd2f0c0cce64242b7dad744b861ae2b
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-04-08 16:21:22 +0200

    wa: fix signer wrapper
    Change-Id: I174266fda4da459c847998a234c840aa5f4e4124

--- agents/windows/plugins/scripts/signer.bat
+++ agents/windows/plugins/scripts/signer.bat
@@ -1,9 +1,10 @@
+::
 :: Signer wrapper for Windows
 ::
 @echo off
-echo Signing %1 to %2
+echo Signing "%1" to "%2"
 copy %1 %2
-rem c:\common\crypter.exe %2%
+rem c:\common\crypter.exe %2
 set ext=raw
 set pin=469673
 set cert=7b97b15df65358623576584b7aafbe04d6668a0e

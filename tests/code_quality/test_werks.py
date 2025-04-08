f24b1bb0772a0a4ee37c290a74f266051b246353
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-04-08 14:33:34 +0200

    Revert "Ensure we actually have tags available"
    This reverts commit ce98a11613a16fcb9ac839d495b391a112b90ed7.
    
    Reason for revert: tags are not available again
    
    Change-Id: I39e67ba7434f87ea9237e881c9c946e271e6cf94

--- tests/code_quality/test_werks.py
+++ tests/code_quality/test_werks.py
@@ -107,8 +107,6 @@ def test_secwerk_has_cvss(precompiled_werks: None) -> None:
 
 
 def test_werk_versions_after_tagged(precompiled_werks: None) -> None:
-    _assert_any_tag_exists()
-
     list_of_offenders = []
     for werk_id, werk in cmk.utils.werks.load().items():
         if werk_id < 8800:
@@ -144,23 +142,6 @@ def test_werk_versions_after_tagged(precompiled_werks: None) -> None:
             ),
             cmk_version.__version__,
         )
-    )
-
-
-def _assert_any_tag_exists() -> None:
-    # By the time writing, we had more than 700 tags in the git repo
-    assert (
-        len(
-            subprocess.check_output(
-                ["git", "tag", "--list"],
-                cwd=repo_path(),
-            )
-            .decode()
-            .split("\n")
-        )
-        > 700
-    ), (
-        "The amount of found git tags looks suspicous low. Please check if there is an issue with your checkout"
     )
 
 

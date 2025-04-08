3340d483a3161ffc2bd8ad4f9421a0ca1d5d7c02
Simon Jess <simon.jess@checkmk.com>
2025-04-08 08:39:39 +0200

    Fix 'reset scheduling' during 'cmk-update-config'
    Change-Id: Iffbcae7a4c2174d418946da599f1924681371233

--- cmk/update_config/plugins/actions/deprecations.py
+++ cmk/update_config/plugins/actions/deprecations.py
@@ -5,14 +5,17 @@
 
 from logging import Logger
 
-from cmk.gui import deprecations
+from cmk.gui import job_scheduler
 
 from cmk.update_config.registry import update_action_registry, UpdateAction
 
 
 class ResetDeprecationsScheduling(UpdateAction):  # pylint: disable=too-few-public-methods
     def __call__(self, logger: Logger) -> None:
-        deprecations.reset_scheduling()
+        # Note: We have to call 'reset_scheduling' from 'job_scheduler' directly and NOT via
+        # fast API '/reset_scheduling' because the job-scheduler is not yet running during
+        # cmk-update-config.
+        job_scheduler.reset_scheduling("execute_deprecation_tests_and_notify_users")
 
 
 update_action_registry.register(

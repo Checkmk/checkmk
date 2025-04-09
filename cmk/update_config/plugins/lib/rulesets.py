ebaa4cfc8e32ba7bd4bdaebff9e63d25872843db
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-04-09 08:31:56 +0200

    Skip validation of fetch intervals rule
    No werk needed in here, but refer to #17638 in 2.4
    
    Change-Id: Ie6fead90fd9e1e9edb294788cc24b15587850662

--- cmk/update_config/plugins/lib/rulesets.py
+++ cmk/update_config/plugins/lib/rulesets.py
@@ -40,10 +40,11 @@ SKIP_ACTION: Final = {
     RuleGroup.ExtraServiceConf("_sla_config"),
     # Validating the ignored checks ruleset does not make sense:
     # Invalid choices are the plugins that don't exist (anymore).
-    # These do no harm, they are dropped upon rule edit. On the other hand, the plugin
-    # could be missing only temporarily, so better not remove it.
+    # These do no harm, they are dropped upon rule edit.
+    # Note that migration code will still be executed, it's just the validation that is skipped.
     "ignored_checks",
     "snmp_exclude_sections",  # same as "ignored_checks".
+    "snmp_check_interval",  # same as "ignored_checks".
 }
 
 SKIP_PREACTION: Final = SKIP_ACTION | {

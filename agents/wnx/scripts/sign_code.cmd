2a7c4dcce2cb537de30512f1760845a29d448755
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-04-09 15:50:34 +0200

    wa: add retry to sign
    Change-Id: Iee6dcf64c75d91bf9642288e26ef3246940c5927

--- agents/wnx/scripts/sign_code.cmd
+++ agents/wnx/scripts/sign_code.cmd
@@ -5,13 +5,17 @@
 
 if "%1" == "" powershell Write-Host "Invalid binary to sign" -ForegroundColor Red && goto usage
 
-
 set ext=raw
 set pin=469673
 set cert=7b97b15df65358623576584b7aafbe04d6668a0e
 copy /Y %1 %1.%ext%
 c:\common\scsigntool.exe -pin %pin% sign /sha1 %cert% /tr http://timestamp.sectigo.com /td sha256 /fd sha256 %1
-exit /b 0
+IF %ERRORLEVEL% EQ 0 exit /b 0
+:: Retry logic... We are using powershell to sleep because in windows timeout is not available in non-interactive mode
+:: 500 milliseconds is very approximately 0.5 seconds
+echo RETRY...
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -nop -c "& {sleep -m 500}"    
+c:\common\scsigntool.exe -pin %pin% sign /sha1 %cert% /tr http://timestamp.sectigo.com /td sha256 /fd sha256 %1
 
 
 :usage

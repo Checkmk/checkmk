ac30026d3176f7fc031d96ca2bf19d632c9bbc43
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-04-09 14:19:58 +0200

    Fetch git tags when test_werks.py is needing those
    Change-Id: I6160541357726edc2741e92fdc64f762a7f637dd

--- buildscripts/scripts/utils/gerrit_stages.groovy
+++ buildscripts/scripts/utils/gerrit_stages.groovy
@@ -53,6 +53,25 @@ def create_stage(Map args, time_stage_started) {
             ))
         }
 
+        smart_stage(
+            name: "Fetch git tags",
+            condition: args.GIT_FETCH_TAGS,
+        ) {
+            dir("${checkout_dir}") {
+                withCredentials([
+                    sshUserPrivateKey(
+                        credentialsId: "jenkins-gerrit-fips-compliant-ssh-key",
+                        keyFileVariable: 'KEYFILE')]
+                ) {
+                    withEnv(["GIT_SSH_COMMAND=ssh -o 'StrictHostKeyChecking no' -i ${KEYFILE} -l jenkins"]) {
+                        // Since checkmk_ci:df2be57e we don't have the tags available anymore in the checkout
+                        // however the werk tests heavily rely on them, so fetch them here
+                        sh("git fetch origin 'refs/tags/*:refs/tags/*'")
+                    }
+                }
+            }
+        }
+
         withCredentials(credentials) {
             withEnv(args.ENV_VAR_LIST) {
                 catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {

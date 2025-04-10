cdfb041cc73d7e99a626d2737a7290ceb221e055
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-04-10 14:07:07 +0200

    wa: add retry to sign # 2
    Change-Id: I7506e74475ada54f38af13530768cdc89d37731a

--- agents/wnx/scripts/sign_code.cmd
+++ agents/wnx/scripts/sign_code.cmd
@@ -11,8 +11,14 @@ set pin=469673
 set cert=7b97b15df65358623576584b7aafbe04d6668a0e
 copy /Y %1 %1.%ext%
 c:\common\scsigntool.exe -pin %pin% sign /sha1 %cert% /tr http://timestamp.sectigo.com /td sha256 /fd sha256 %1
+IF %ERRORLEVEL% EQU 0 exit /b 0
+:: Retry logic... We are using powershell to sleep because in windows timeout is not available in non-interactive mode
+:: 500 milliseconds is very approximately 0.5 seconds
+echo RETRY...
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -nop -c "& {sleep -m 500}"    
+c:\common\scsigntool.exe -pin %pin% sign /sha1 %cert% /tr http://timestamp.sectigo.com /td sha256 /fd sha256 %1
+:: dont care any more
 exit /b 0
-
 
 :usage
 powershell Write-Host "Usage:" -ForegroundColor DarkGreen

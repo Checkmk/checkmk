8f6610e1ddcd84c5d325c77cc0bf5f9bf3a31ae6
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-04-10 13:36:35 +0200

    extract custom dropdown button to own component
    also improve demo
    also prepare grouping feature for dropdown button, needed in next
    commit.
    
    Change-Id: I6907e0a30d5d58f2ecbbeb6617cc37d8d368884e

--- packages/cmk-frontend-vue/src/components/CmkDropdown.vue
+++ packages/cmk-frontend-vue/src/components/CmkDropdown.vue
@@ -4,13 +4,13 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
-import { type Ref, computed, nextTick, ref, watch } from 'vue'
-import CmkButton from './CmkButton.vue'
+import { type Ref, computed, useTemplateRef, nextTick, ref, watch } from 'vue'
 import useClickOutside from '@/lib/useClickOutside'
 import FormRequired from '@/form/private/FormRequired.vue'
-import ArrowDown from '@/components/graphics/ArrowDown.vue'
+import CmkDropdownButton from './CmkDropdownButton.vue'
 import CmkSuggestions from './CmkSuggestions.vue'
 import { type Suggestions } from './CmkSuggestions.vue'
+import ArrowDown from '@/components/graphics/ArrowDown.vue'
 
 export interface DropdownOption {
   name: string
@@ -50,7 +50,8 @@ const multipleChoicesAvailable = computed(() => options.suggestions.length !== 0
 
 const suggestionsShown = ref(false)
 const suggestionsRef = ref<InstanceType<typeof CmkSuggestions> | null>(null)
-const comboboxButtonRef = ref<HTMLButtonElement | null>(null)
+const comboboxButtonRef =
+  useTemplateRef<InstanceType<typeof CmkDropdownButton>>('comboboxButtonRef')
 
 const filterString = ref('')
 const filteredOptions = ref<number[]>(options.suggestions.map((_, index) => index))
@@ -112,30 +113,24 @@ function selectOption(option: DropdownOption): void {
       }
     "
     class="cmk-dropdown"
-    :class="{
-      'cmk-dropdown-suggestions-shown': suggestionsShown
-    }"
   >
-    <CmkButton
+    <CmkDropdownButton
       :id="componentId"
       ref="comboboxButtonRef"
-      role="combobox"
       :aria-label="label"
       :aria-expanded="suggestionsShown"
-      class="cmk-dropdown__button"
-      :class="{
-        disabled,
-        no_choices: !multipleChoicesAvailable,
-        no_value: selectedOption === null
-      }"
+      :disabled="disabled"
+      :multiple-choices-available="multipleChoicesAvailable"
+      :value-is-selected="selectedOption !== null"
+      :open="suggestionsShown"
       @click.prevent="showSuggestions"
     >
       {{ dropdownButtonLabel
       }}<template v-if="requiredText !== '' && selectedOption === null">
-        {{ ' '
-        }}<FormRequired :show="true" :space="'before'" :i18n-required="requiredText" /></template
-      ><ArrowDown class="cmk-dropdown__button_arrow" />
-    </CmkButton>
+        {{ ' ' }}<FormRequired :show="true" :space="'before'" :i18n-required="requiredText"
+      /></template>
+      <ArrowDown class="cmk-dropdown-button_arrow" :class="{ rotated: suggestionsShown }" />
+    </CmkDropdownButton>
     <CmkSuggestions
       v-if="!!suggestionsShown"
       ref="suggestionsRef"
@@ -154,61 +149,17 @@ function selectOption(option: DropdownOption): void {
   display: inline-block;
   position: relative;
   white-space: nowrap;
-}
 
-.cmk-dropdown-suggestions-shown {
-  .cmk-dropdown__button_arrow {
-    transform: rotate(180deg);
-  }
-}
-
-.cmk-dropdown__button {
-  height: var(--form-field-height);
-  margin: 0;
-  padding: 3px 1.9em 4px 6px;
-  vertical-align: baseline;
-  background-color: var(--default-form-element-bg-color);
-  border: none;
-  font-weight: var(--font-weight-default);
-  cursor: pointer;
-
-  .cmk-dropdown__button_arrow {
-    position: absolute;
-    right: 7px;
+  .cmk-dropdown-button_arrow {
+    padding-left: 0.8em;
     width: 0.7em;
     /* This replicates the dropdown in checkmk, which useses select2 which
        uses #888 as color by default. The color is not themed there, so we
        also don't theme it. */
     color: #888;
-  }
 
-  &:hover {
-    background-color: var(--input-hover-bg-color);
-  }
-
-  &.disabled {
-    cursor: auto;
-    color: var(--font-color-dimmed);
-    &:hover {
-      background-color: var(--default-form-element-bg-color);
-    }
-  }
-
-  &.no_value {
-    color: var(--font-color-dimmed);
-
-    > .cmk-dropdown__button_arrow {
-      color: var(--font-color);
-    }
-  }
-
-  &.no_choices {
-    cursor: auto;
-    &:hover {
-      background-color: var(--default-form-element-bg-color);
-    }
-    > .cmk-dropdown__button_arrow {
-      opacity: 0.4;
+    &.rotated {
+      transform: rotate(180deg);
     }
   }
 }

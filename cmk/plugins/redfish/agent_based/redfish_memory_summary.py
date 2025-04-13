e9b9a6444b237fa8bc9b285b27f6179fe55f40af
Andreas Doehler <andreas.doehler@gmail.com>
2025-04-13 20:32:35 +0200

    redfish: the memory summary was "broken" after mainlining
    Closes: #811
    
    CMK-22397
    
    Change-Id: I656df39a1da67988f2f075bdee7bd3f507eddc1d

--- cmk/plugins/redfish/agent_based/redfish_memory_summary.py
+++ cmk/plugins/redfish/agent_based/redfish_memory_summary.py
@@ -18,11 +18,25 @@ from cmk.plugins.redfish.lib import (
 
 
 def discover_redfish_memory_summary(section: SectionSystem) -> DiscoveryResult:
-    yield from (Service(item=item) for item in section)
+    if len(section) == 1:
+        if not list(section.values())[0].get("MemorySummary", {}):
+            return
+        yield Service(item="Summary")
+    else:
+        for item, data in section.items():
+            if not data.get("MemorySummary", {}):
+                continue
+            yield Service(item=f"Summary {item}")
 
 
 def check_redfish_memory_summary(item: str, section: SectionSystem) -> CheckResult:
-    if not (result := section.get(item)):
+    result = {}
+    if len(section) == 1:
+        result = list(section.values())[0].get("MemorySummary", {})
+    else:
+        systemid = item.split(" ")[-1]
+        result = section.get(systemid, {}).get("MemorySummary", {})
+    if not result:
         return
 
     state = result.get("Status", {"Health": "Unknown"})
@@ -34,7 +48,7 @@ def check_redfish_memory_summary(item: str, section: SectionSystem) -> CheckResu
 
 check_plugin_redfish_memory_summary = CheckPlugin(
     name="redfish_memory_summary",
-    service_name="Memory Summary %s",
+    service_name="Memory %s",
     sections=["redfish_system"],
     discovery_function=discover_redfish_memory_summary,
     check_function=check_redfish_memory_summary,

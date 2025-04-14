667113ebd4598104bcdb58c8240105d6b1e45147
David Gervasoni <david.gervasoni@checkmk.com>
2025-04-14 09:56:02 +0200

    Fix return type of transform_wato_rulesets_params
    Change-Id: I4014751098ae324ee155999c003b421de2c0dd95

--- cmk/update_config/plugins/lib/rulesets.py
+++ cmk/update_config/plugins/lib/rulesets.py
@@ -5,7 +5,7 @@
 
 
 import re
-from collections.abc import Iterable, Mapping, Sequence
+from collections.abc import Collection, Iterable, Mapping, Sequence
 from logging import Logger
 from typing import Final
 
@@ -200,7 +200,7 @@ def transform_wato_rulesets_params(
     logger: Logger,
     all_rulesets: RulesetCollection,
     raise_errors: bool = False,
-) -> Iterable[RulesetName]:
+) -> Collection[RulesetName]:
     migrated_rulesets = set()
     for ruleset in all_rulesets.get_rulesets().values():
         try:

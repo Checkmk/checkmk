4de82245aad06ac9e5ccbae78272d84a830ed438
Dennis Schmalacker <dennis.schmalacker@checkmk.com>
2025-04-14 17:17:33 +0200

    acme_agent_session: Move to new tree structure
    Change-Id: I4ad7f020102fc1857a0dae54ae5652e380074e99

--- tests/unit/cmk/plugins/acme_sbc/server_side_calls/test_special_agent.py
+++ tests/unit/cmk/plugins/acme_sbc/server_side_calls/test_special_agent.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.plugins.acme_sbc.server_side_calls.special_agent import special_agent_acme_sbc
+from cmk.plugins.acme.server_side_calls.special_agent import special_agent_acme_sbc
 from cmk.server_side_calls.v1 import HostConfig, SpecialAgentCommand
 
 

4b11d35144596cadfaa2ab4b37df8ccf7cb22da9
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2025-04-14 16:43:30 +0200

    Add cmk_module_bazel scanner
    This will find more bazel deps
    
    CMK-21260
    
    Change-Id: I99d5b004775718b6b11227453f71c88e6b973fce

--- buildscripts/scripts/build-cmk-bom.groovy
+++ buildscripts/scripts/build-cmk-bom.groovy
@@ -63,7 +63,7 @@ def main() {
                         --outfile '${bom_path}' \
                         --research_file researched_master.yml \
                         --license_cache license_cache_master.json \
-                        --type bazel_files --type cargo --type omd --type package-lock --type cmk_runtime_lock \
+                        --type bazel_files --type cargo --type omd --type package-lock --type cmk_runtime_lock --type cmk_module_bazel\
                         '${checkout_dir}'
                     """);
                 }

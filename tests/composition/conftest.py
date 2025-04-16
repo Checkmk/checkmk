5d758bbec35086a2a83f02bc033b2462a179a3f9
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2025-04-16 11:50:17 +0200

    Allow testing of a distributed setup in gui-e2e tests
    CMK-23205
    
    Change-Id: Ia5ba40328810a8f32056bf1138b3c43ec93fdb63

--- tests/composition/conftest.py
+++ tests/composition/conftest.py
@@ -6,7 +6,6 @@
 import logging
 import os
 from collections.abc import Iterator
-from contextlib import contextmanager
 from pathlib import Path
 from shutil import which
 from typing import Literal
@@ -23,6 +22,7 @@ from tests.testlib.agent import (
     install_agent_package,
 )
 from tests.testlib.site import (
+    connection,
     get_site_factory,
     GlobalSettingsUpdate,
     Site,
@@ -92,83 +92,8 @@ def _make_connected_remote_site(
         auto_restart_httpd=True,
         tracing_config=tracing_config_from_env(os.environ),
     ) as remote_site:
-        with _connection(central_site=central_site, remote_site=remote_site):
+        with connection(central_site=central_site, remote_site=remote_site):
             yield remote_site
-
-
-@contextmanager
-def _connection(
-    *,
-    central_site: Site,
-    remote_site: Site,
-) -> Iterator[None]:
-    if central_site.edition.is_managed_edition():
-        basic_settings = {
-            "alias": "Remote Testsite",
-            "site_id": remote_site.id,
-            "customer": "provider",
-        }
-    else:
-        basic_settings = {
-            "alias": "Remote Testsite",
-            "site_id": remote_site.id,
-        }
-
-    logger.info("Create site connection from '%s' to '%s'", central_site.id, remote_site.id)
-    central_site.openapi.sites.create(
-        {
-            "basic_settings": basic_settings,
-            "status_connection": {
-                "connection": {
-                    "socket_type": "tcp",
-                    "host": remote_site.http_address,
-                    "port": remote_site.livestatus_port,
-                    "encrypted": False,
-                    "verify": False,
-                },
-                "proxy": {
-                    "use_livestatus_daemon": "direct",
-                },
-                "connect_timeout": 2,
-                "persistent_connection": False,
-                "url_prefix": f"/{remote_site.id}/",
-                "status_host": {"status_host_set": "disabled"},
-                "disable_in_status_gui": False,
-            },
-            "configuration_connection": {
-                "enable_replication": True,
-                "url_of_remote_site": remote_site.internal_url,
-                "disable_remote_configuration": True,
-                "ignore_tls_errors": True,
-                "direct_login_to_web_gui_allowed": True,
-                "user_sync": {"sync_with_ldap_connections": "all"},
-                "replicate_event_console": True,
-                "replicate_extensions": True,
-                "message_broker_port": remote_site.message_broker_port,
-            },
-        }
-    )
-    logger.info("Establish site login '%s' to '%s'", central_site.id, remote_site.id)
-    central_site.openapi.sites.login(remote_site.id)
-    logger.info("Activating site setup changes")
-    central_site.openapi.changes.activate_and_wait_for_completion(
-        # this seems to be necessary to avoid sporadic CI failures
-        force_foreign_changes=True,
-    )
-    try:
-        logger.info("Connection from '%s' to '%s' established", central_site.id, remote_site.id)
-        yield
-    finally:
-        if os.environ.get("CLEANUP", "1") == "1":
-            logger.info("Remove site connection from '%s' to '%s'", central_site.id, remote_site.id)
-            logger.info("Hosts left: %s", central_site.openapi.hosts.get_all_names())
-            logger.info("Delete remote site connection '%s'", remote_site.id)
-            central_site.openapi.sites.delete(remote_site.id)
-            logger.info("Activating site removal changes")
-            central_site.openapi.changes.activate_and_wait_for_completion(
-                # this seems to be necessary to avoid sporadic CI failures
-                force_foreign_changes=True,
-            )
 
 
 @pytest.fixture(name="installed_agent_ctl_in_unknown_state", scope="function")

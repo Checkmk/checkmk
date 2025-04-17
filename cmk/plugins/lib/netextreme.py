00ace8084deeedd568cec4fbdb5bb901a426d416
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-04-17 10:33:55 +0200

    15363 Allow extreme checks to detect Avaya switches
    The extreme checks now successfully detect Avaya switches.
    
    SUP-23301
    
    Change-Id: Ifb7dafae5bb8b742c81114810b1f776723bca629

--- cmk/plugins/lib/netextreme.py
+++ cmk/plugins/lib/netextreme.py
@@ -3,6 +3,13 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.agent_based.v2 import startswith
+from cmk.agent_based.v2 import any_of, startswith
 
-DETECT_NETEXTREME = startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.1916.2")
+DETECT_NETEXTREME = any_of(
+    startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.1916.2"),
+    startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.2272.2"),
+    # verified for:
+    # .1.3.6.1.4.1.2272.202 : VSP-4850GTS
+    # .1.3.6.1.4.1.2272.209 : VSP-7254
+    # .1.3.6.1.4.1.2272.220 : VSP-8404C
+)

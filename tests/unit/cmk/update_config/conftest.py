3ec2c16e2c09dcf7dbdfbbfe25e71939d844ffc7
Anastasiia Shevchuk <anastasiia.shevchuk@checkmk.com>
2025-04-23 13:36:51 +0200

    Replace imports from gui/conftest with fixture definitions
    According to pytest documentation, it's not recommended
    to import pytest fixtures. With this change such imports in
    update_config/conftest.py are replaced with fixture definitions.
    
    Jira ticket: CMK-22885
    
    Change-Id: I334ea00ed365cde16dfb0572dc729ba4242a589f

--- tests/unit/cmk/update_config/conftest.py
+++ tests/unit/cmk/update_config/conftest.py
@@ -3,13 +3,80 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from tests.unit.cmk.gui.conftest import (  # noqa: F401
-    flask_app,
-    gui_cleanup_after_test,
-    load_plugins,
-    request_context,
-    with_admin,
-    with_admin_login,
-    with_user,
-    wsgi_app,
+from collections.abc import Iterator
+
+import pytest
+from flask import Flask
+from pytest_mock import MockerFixture
+
+from tests.unit.cmk.gui.common_fixtures import (
+    create_flask_app,
+    create_wsgi_app,
+    perform_gui_cleanup_after_test,
+    perform_load_config,
+    perform_load_plugins,
 )
+from tests.unit.cmk.gui.users import create_and_destroy_user
+from tests.unit.cmk.web_test_app import (
+    WebTestAppForCMK,
+)
+
+from cmk.utils.user import UserId
+
+from cmk.gui import login
+
+
+@pytest.fixture()
+def flask_app(
+    patch_omd_site: None,
+    use_fakeredis_client: None,
+    load_plugins: None,
+) -> Iterator[Flask]:
+    yield from create_flask_app()
+
+
+@pytest.fixture(autouse=True)
+def gui_cleanup_after_test(
+    mocker: MockerFixture,
+) -> Iterator[None]:
+    yield from perform_gui_cleanup_after_test(mocker)
+
+
+@pytest.fixture()
+def load_config(request_context: None) -> Iterator[None]:
+    yield from perform_load_config()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_plugins() -> None:
+    perform_load_plugins()
+
+
+@pytest.fixture()
+def request_context(flask_app: Flask) -> Iterator[None]:
+    """Empty fixture. Invokes usage of `flask_app` fixture."""
+    yield
+
+
+@pytest.fixture()
+def with_admin(load_config: None) -> Iterator[tuple[UserId, str]]:
+    with create_and_destroy_user(automation=False, role="admin") as user:
+        yield user
+
+
+@pytest.fixture()
+def with_admin_login(with_admin: tuple[UserId, str]) -> Iterator[UserId]:
+    user_id = with_admin[0]
+    with login.TransactionIdContext(user_id):
+        yield user_id
+
+
+@pytest.fixture()
+def with_user(load_config: None) -> Iterator[tuple[UserId, str]]:
+    with create_and_destroy_user(automation=False, role="user") as user:
+        yield user
+
+
+@pytest.fixture()
+def wsgi_app(flask_app: Flask) -> Iterator[WebTestAppForCMK]:
+    yield from create_wsgi_app(flask_app)

6b225b8d2ca0680fc4931e559043c6b1dec17bd3
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-04-23 14:56:18 +0200

    livestatus_status: add params type
    Change-Id: Ibc727f89b62da6d35d4512890ab8ee0a2e110f23

--- cmk/plugins/collection/agent_based/livestatus_status.py
+++ cmk/plugins/collection/agent_based/livestatus_status.py
@@ -3,8 +3,8 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 import time
-from collections.abc import Mapping, MutableMapping
-from typing import Any
+from collections.abc import MutableMapping
+from typing import Any, Literal, NotRequired, TypedDict
 
 from cmk.agent_based.v1 import check_levels as check_levels_v1
 from cmk.agent_based.v2 import (
@@ -29,31 +29,68 @@ from cmk.plugins.lib.livestatus_status import LivestatusSection
 # accept_passive_host_checks;accept_passive_service_checks;cached_log_messages;check_external_commands;check_host_freshness;check_service_freshness;connections;connections_rate;enable_event_handlers;enable_flap_detection;enable_notifications;execute_host_checks;execute_service_checks;external_command_buffer_max;external_command_buffer_slots;external_command_buffer_usage;external_commands;external_commands_rate;forks;forks_rate;host_checks;host_checks_rate;interval_length;last_command_check;last_log_rotation;livecheck_overflows;livecheck_overflows_rate;livechecks;livechecks_rate;livestatus_active_connections;livestatus_queued_connections;livestatus_threads;livestatus_version;log_messages;log_messages_rate;nagios_pid;neb_callbacks;neb_callbacks_rate;num_hosts;num_services;obsess_over_hosts;obsess_over_services;process_performance_data;program_start;program_version;requests;requests_rate;service_checks;service_checks_rate
 # 1;1;0;1;0;1;231;1.0327125668e-01;1;1;1;1;1;0;32768;0;0;0.0000000000e+00;0;0.0000000000e+00;0;0.0000000000e+00;60;1359471450;0;0;0.0000000000e+00;0;0.0000000000e+00;1;0;20;2013.01.23;0;0.0000000000e+00;15126;15263;6.5307324420e+00;0;0;0;0;1;1359469039;3.2.3;230;1.0327125668e-01;0;0.0000000000e+00
 
-_PEM_PATH_TEMPLATE = "/omd/sites/{site}/etc/ssl/sites/{site}.pem"
 
-livestatus_status_default_levels = {
-    "site_stopped": 2,
-    "execute_host_checks": 2,
-    "execute_service_checks": 2,
-    "accept_passive_host_checks": 2,
-    "accept_passive_service_checks": 2,
-    "check_host_freshness": 0,  # Was in OMD the default up to now, better not warn
-    "check_service_freshness": 1,
-    "enable_event_handlers": 1,
-    "enable_flap_detection": 1,
-    "enable_notifications": 2,
-    "process_performance_data": 1,
-    "check_external_commands": 2,
-    "site_cert_days": (30, 7),
-    "average_latency_generic": (30, 60),
-    "average_latency_cmk": (30, 60),
-    "average_latency_fetcher": (30, 60),
-    "helper_usage_generic": (80.0, 90.0),
-    "helper_usage_fetcher": (80.0, 90.0),
-    "helper_usage_checker": (80.0, 90.0),
-    "livestatus_usage": (60.0, 80.0),
-    "livestatus_overflows_rate": (0.01, 0.02),
-}
+type _StateInt = Literal[0, 1, 2, 3]
+
+
+class LivestatusStatusParameters(TypedDict):
+    site_stopped: _StateInt
+    execute_host_checks: _StateInt
+    execute_service_checks: _StateInt
+    accept_passive_host_checks: _StateInt
+    accept_passive_service_checks: _StateInt
+    check_host_freshness: _StateInt
+    check_service_freshness: _StateInt
+    enable_event_handlers: _StateInt
+    enable_flap_detection: _StateInt
+    enable_notifications: _StateInt
+    process_performance_data: _StateInt
+    check_external_commands: _StateInt
+    site_cert_days: tuple[int, int]  # >= 0
+    average_latency_generic: tuple[int, int]
+    average_latency_cmk: tuple[int, int]
+    average_latency_fetcher: tuple[int, int]
+    helper_usage_generic: tuple[float, float]
+    helper_usage_fetcher: tuple[float, float]
+    helper_usage_checker: tuple[float, float]
+    livestatus_usage: tuple[float, float]
+    livestatus_overflows_rate: tuple[float, float]
+    levels_hosts: NotRequired[tuple[int, int]]
+    levels_services: NotRequired[tuple[int, int]]
+    carbon_overflows_rate: NotRequired[tuple[float, float]]
+    carbon_queue_usage: NotRequired[tuple[float, float]]
+    influxdb_overflows_rate: NotRequired[tuple[float, float]]
+    influxdb_queue_usage: NotRequired[tuple[float, float]]
+    rrdcached_overflows_rate: NotRequired[tuple[float, float]]
+    rrdcached_queue_usage: NotRequired[tuple[float, float]]
+
+
+livestatus_status_default_levels = LivestatusStatusParameters(
+    site_stopped=2,
+    execute_host_checks=2,
+    execute_service_checks=2,
+    accept_passive_host_checks=2,
+    accept_passive_service_checks=2,
+    check_host_freshness=0,  # Was in OMD the default up to now, better not warn
+    check_service_freshness=1,
+    enable_event_handlers=1,
+    enable_flap_detection=1,
+    enable_notifications=2,
+    process_performance_data=1,
+    check_external_commands=2,
+    site_cert_days=(30, 7),
+    average_latency_generic=(30, 60),
+    average_latency_cmk=(30, 60),
+    average_latency_fetcher=(30, 60),
+    helper_usage_generic=(80.0, 90.0),
+    helper_usage_fetcher=(80.0, 90.0),
+    helper_usage_checker=(80.0, 90.0),
+    livestatus_usage=(60.0, 80.0),
+    livestatus_overflows_rate=(0.01, 0.02),
+)
+
+
+_PEM_PATH_TEMPLATE = "/omd/sites/{site}/etc/ssl/sites/{site}.pem"
 
 
 def parse_livestatus_status(string_table: StringTable) -> LivestatusSection:
@@ -114,7 +151,7 @@ def discovery_livestatus_status(
 
 def check_livestatus_status(
     item: str,
-    params: Mapping[str, Any],
+    params: LivestatusStatusParameters,
     section_livestatus_status: LivestatusSection | None,
     section_livestatus_ssl_certs: LivestatusSection | None,
 ) -> CheckResult:
@@ -142,7 +179,7 @@ def _check_livestatus_cert(
     section_livestatus_ssl_certs: LivestatusSection,
     item: str,
     this_time: float,
-    params: Mapping[str, Any],
+    params: LivestatusStatusParameters,
 ) -> CheckResult:
     # cert_valid_until should only be empty in one case that we know of so far:
     # the value is collected via the linux special agent with the command 'date'
@@ -175,7 +212,7 @@ def _check_livestatus_cert(
 
 def _generate_livestatus_results(
     item: str,
-    params: Mapping[str, Any],
+    params: LivestatusStatusParameters,
     section_livestatus_status: LivestatusSection | None,
     section_livestatus_ssl_certs: LivestatusSection | None,
     value_store: MutableMapping[str, Any],
@@ -284,7 +321,8 @@ def _generate_livestatus_results(
             yield from check_levels_v1(
                 value=value,
                 metric_name=key,
-                levels_upper=params.get(key),
+                # Suppression: the key is not always one of the literals :-(
+                levels_upper=params.get(key),  # type: ignore[call-overload]
                 render_func=render_func,
                 label=label,
                 notice_only=True,
@@ -316,23 +354,23 @@ def _generate_livestatus_results(
     if section_livestatus_ssl_certs is not None:
         yield from _check_livestatus_cert(section_livestatus_ssl_certs, item, this_time, params)
 
-    settings = [
-        ("execute_host_checks", "Active host checks are disabled"),
-        ("execute_service_checks", "Active service checks are disabled"),
-        ("accept_passive_host_checks", "Passive host check are disabled"),
-        ("accept_passive_service_checks", "Passive service checks are disabled"),
-        ("check_host_freshness", "Host freshness checking is disabled"),
-        ("check_service_freshness", "Service freshness checking is disabled"),
-        #   ("enable_event_handlers",         "Alert handlers are disabled"), # special case below
-        ("enable_flap_detection", "Flap detection is disabled"),
-        ("enable_notifications", "Notifications are disabled"),
-        ("process_performance_data", "Performance data is disabled"),
-        ("check_external_commands", "External commands are disabled"),
-    ]
+    setting_messages = {
+        "execute_host_checks": "Active host checks are disabled",
+        "execute_service_checks": "Active service checks are disabled",
+        "accept_passive_host_checks": "Passive host check are disabled",
+        "accept_passive_service_checks": "Passive service checks are disabled",
+        "check_host_freshness": "Host freshness checking is disabled",
+        "check_service_freshness": "Service freshness checking is disabled",
+        #   "enable_event_handlers":         "Alert handlers are disabled", # special case below
+        "enable_flap_detection": "Flap detection is disabled",
+        "enable_notifications": "Notifications are disabled",
+        "process_performance_data": "Performance data is disabled",
+        "check_external_commands": "External commands are disabled",
+    }
     # Check settings of enablings. Here we are quiet unless a non-OK state is found
-    for settingname, title in settings:
-        if status[settingname] != "1":
-            yield Result(state=State(params[settingname]), notice=title)
+    for p_name, p_value in params.items():
+        if p_name in setting_messages and status[p_name] != "1":
+            yield Result(state=State(p_value), notice=setting_messages[p_name])
 
     # special considerations for enable_event_handlers
     if status["program_version"].startswith("Check_MK 1.2.6"):
--- tests/unit/cmk/plugins/collection/agent_based/test_livestatus_status.py
+++ tests/unit/cmk/plugins/collection/agent_based/test_livestatus_status.py
@@ -10,8 +10,6 @@ from zoneinfo import ZoneInfo
 import pytest
 import time_machine
 
-from cmk.checkengine.parameters import Parameters
-
 from cmk.agent_based.v2 import CheckResult, Metric, Result, Service, State
 from cmk.plugins.collection.agent_based import livestatus_status
 
@@ -668,7 +666,7 @@ def test_check_new_counters_in_oldstabe(
     yielded_results = list(
         livestatus_status._generate_livestatus_results(
             "oldstable",
-            Parameters(livestatus_status.livestatus_status_default_levels),
+            livestatus_status.livestatus_status_default_levels,
             PARSED_STATUS,
             PARSED_SSL,
             {
@@ -771,7 +769,7 @@ def test_check() -> None:
         yielded_results = list(
             livestatus_status._generate_livestatus_results(
                 "heute",
-                Parameters(livestatus_status.livestatus_status_default_levels),
+                livestatus_status.livestatus_status_default_levels,
                 PARSED_STATUS,
                 PARSED_SSL,
                 {

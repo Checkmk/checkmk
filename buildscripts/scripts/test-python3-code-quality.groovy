96871390a6edaf1e2ac66d68f9418f5c855c867f
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-04-24 10:59:21 +0200

    Revert "No tags fetching needed"
    This reverts commit 61902468000aba47391cfeea0cacf464b6db0898.
    
    Change-Id: I4856104e6e6dddd5482e7f6888e63fc721e10343

--- buildscripts/scripts/test-python3-code-quality.groovy
+++ buildscripts/scripts/test-python3-code-quality.groovy
@@ -8,8 +8,17 @@ def main() {
         docker_image_from_alias("IMAGE_TESTING").inside(docker_args) {
             stage('test python3 code quality') {
                 dir("${checkout_dir}") {
-
-                    // Attention: This job needs all git tags available in the workspace!
+                    withCredentials([
+                        sshUserPrivateKey(
+                            credentialsId: "jenkins-gerrit-fips-compliant-ssh-key",
+                            keyFileVariable: 'KEYFILE')]
+                    ) {
+                        withEnv(["GIT_SSH_COMMAND=ssh -o 'StrictHostKeyChecking no' -i ${KEYFILE} -l jenkins"]) {
+                            // Since checkmk_ci:df2be57e we don't have the tags available anymore in the checkout
+                            // however the werk tests heavily rely on them, so fetch them here
+                            sh("git fetch origin 'refs/tags/*:refs/tags/*'")
+                        }
+                    }
                     sh("make -C tests test-code-quality");
                 }
             }

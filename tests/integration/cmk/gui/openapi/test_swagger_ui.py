8bdc3968263c4707a937f69799a3bd49cc24d82d
Christopher Krause <christopher.krause@checkmk.com>
2025-04-24 09:10:56 +0200

    tests/rest-api: adjust version in swagger ui tests
    Change-Id: I297a90aa34465fad183459ceaf7a50a13b68c7c3

--- tests/integration/cmk/gui/openapi/test_swagger_ui.py
+++ tests/integration/cmk/gui/openapi/test_swagger_ui.py
@@ -11,25 +11,23 @@ from tests.testlib.web_session import CMKWebSession
 @skip_if_saas_edition
 def test_swagger_ui_http_unauthenticated(site: Site) -> None:
     session = CMKWebSession(site)
-    session.get(f"/{site.id}/check_mk/api/1.0.0/ui/index.html", expected_code=401)
+    session.get(f"/{site.id}/check_mk/api/v1/ui/index.html", expected_code=401)
 
 
 @skip_if_saas_edition
 def test_swagger_ui_resource_urls_unauthenticated(site: Site) -> None:
     session = CMKWebSession(site)
-    session.get(f"/{site.id}/check_mk/api/0/ui/swagger-ui.js", expected_code=401)
+    session.get(f"/{site.id}/check_mk/api/v1/ui/swagger-ui.js", expected_code=401)
 
 
 @skip_if_saas_edition
 def test_swagger_ui_http(site: Site) -> None:
     session = CMKWebSession(site)
     session.login()
-    resp = session.get(f"/{site.id}/check_mk/api/1.0.0/ui/index.html", expected_code=200)
+    resp = session.get(f"/{site.id}/check_mk/api/v1/ui/index.html", expected_code=200)
     assert resp.headers["content-type"] == "text/html; charset=utf-8"
     assert '<script src="./swagger-initializer.js"' in resp.text
-    resp = session.get(
-        f"/{site.id}/check_mk/api/1.0.0/ui/swagger-initializer.js", expected_code=200
-    )
+    resp = session.get(f"/{site.id}/check_mk/api/v1/ui/swagger-initializer.js", expected_code=200)
     assert "petstore" not in resp.text
     assert "openapi-swagger-ui.yaml" in resp.text
 
@@ -38,12 +36,12 @@ def test_swagger_ui_http(site: Site) -> None:
 def test_swagger_ui_resource_urls(site: Site) -> None:
     session = CMKWebSession(site)
     session.login()
-    resp = session.get(f"/{site.id}/check_mk/api/0/ui/swagger-ui.js", expected_code=200)
+    resp = session.get(f"/{site.id}/check_mk/api/v1/ui/swagger-ui.js", expected_code=200)
     assert resp.headers["content-type"] in (
         "application/javascript; charset=utf-8",
         "text/javascript; charset=utf-8",
     )
-    resp = session.get(f"/{site.id}/check_mk/api/0/ui/swagger-ui.css", expected_code=200)
+    resp = session.get(f"/{site.id}/check_mk/api/v1/ui/swagger-ui.css", expected_code=200)
     assert resp.headers["content-type"] == "text/css; charset=utf-8"
-    resp = session.get(f"/{site.id}/check_mk/api/0/ui/swagger-ui.css.map", expected_code=200)
+    resp = session.get(f"/{site.id}/check_mk/api/v1/ui/swagger-ui.css.map", expected_code=200)
     assert resp.headers["content-type"] == "application/octet-stream"

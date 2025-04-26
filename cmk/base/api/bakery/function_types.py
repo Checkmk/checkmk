d1e89de26846a6560aa533798ec59f33ed64bd41
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-04-26 22:18:21 +0200

    bakery: detach form internal requirements
    While this is a slight duplication, it is preferable
    to have the API decoupled from internal requirements.
    Note that changes in `ValidatedString` would have
    implied a version change of the bakery API...
    
    Change-Id: Ib9c5fb2633ec080e544056e3297f3e889de961cc

--- cmk/base/api/bakery/function_types.py
+++ cmk/base/api/bakery/function_types.py
@@ -5,10 +5,9 @@
 
 import functools
 import inspect
+import string
 from collections.abc import Callable, Iterator, Sequence
-from typing import NamedTuple
-
-from cmk.utils.validatedstr import ValidatedString
+from typing import Final, NamedTuple, Self
 
 from .artifact_types import (
     Plugin,
@@ -45,8 +44,61 @@ _PossibleReturnType = FileReturnType | ScriptletReturnType | WindowsConfigReturn
 _PossibleFunctionType = FilesFunction | ScriptletsFunction | WindowsConfigFunction
 
 
-class BakeryPluginName(ValidatedString):
-    pass
+class BakeryPluginName:
+    # A plug-in name must be a non-empty string consisting only
+    # of letters A-z, digits and the underscore.
+    VALID_CHARACTERS: Final = string.ascii_letters + "_" + string.digits
+
+    @classmethod
+    def _validate_args(cls, /, __str: str) -> str:
+        if not isinstance(__str, str):
+            raise TypeError(f"{cls.__name__} must initialized from str")
+        if not __str:
+            raise ValueError(f"{cls.__name__} initializer must not be empty")
+
+        if invalid := "".join(c for c in __str if c not in cls.VALID_CHARACTERS):
+            raise ValueError(f"Invalid characters in {__str!r} for {cls.__name__}: {invalid!r}")
+
+        return __str
+
+    def __getnewargs__(self) -> tuple[str]:
+        return (str(self),)
+
+    def __new__(cls, /, __str: str) -> Self:
+        cls._validate_args(__str)
+        return super().__new__(cls)
+
+    def __init__(self, /, __str: str) -> None:
+        self._value: Final = __str
+        self._hash: Final = hash(type(self).__name__ + self._value)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self._value!r})"
+
+    def __str__(self) -> str:
+        return self._value
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, self.__class__):
+            raise TypeError(f"cannot compare {self!r} and {other!r}")
+        return self._value == other._value
+
+    def __lt__(self, other: Self) -> bool:
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self._value < other._value
+
+    def __le__(self, other: Self) -> bool:
+        return self < other or self == other
+
+    def __gt__(self, other: Self) -> bool:
+        return not self <= other
+
+    def __ge__(self, other: Self) -> bool:
+        return not self < other
+
+    def __hash__(self) -> int:
+        return self._hash
 
 
 class BakeryPlugin(NamedTuple):

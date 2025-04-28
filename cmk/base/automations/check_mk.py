a59905ffbdb174dc7b3fd57c4b72c447398b6759
David Gervasoni <david.gervasoni@checkmk.com>
2025-04-28 11:44:53 +0200

    17939 FIX Core restart did not correctly check for updated files
    SUP-23400
    
    Change-Id: I4e1e44d9add2e6781394a8eba73c4147a795cf78

--- cmk/base/automations/check_mk.py
+++ cmk/base/automations/check_mk.py
@@ -59,6 +59,8 @@ from cmk.utils.paths import (
     discovered_host_labels_dir,
     local_agent_based_plugins_dir,
     local_checks_dir,
+    local_cmk_addons_plugins_dir,
+    local_cmk_plugins_dir,
     logwatch_dir,
     nagios_startscript,
     omd_root,
@@ -1827,6 +1829,8 @@ class AutomationRestart(Automation):
         for checks_path in [
             local_checks_dir,
             local_agent_based_plugins_dir,
+            local_cmk_addons_plugins_dir,
+            local_cmk_plugins_dir,
         ]:
             if not checks_path.exists():
                 continue

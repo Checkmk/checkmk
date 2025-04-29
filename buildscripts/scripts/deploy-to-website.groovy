12e82bdb48b3223fb3d8530ba12674b647f641e6
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-04-29 13:46:05 +0200

    bom: Update symlinks
    Change-Id: I4875ed3e4290203a177439d798d70d57a17b20b2
    JIRA-Ref: CMK-22941

--- buildscripts/scripts/deploy-to-website.groovy
+++ buildscripts/scripts/deploy-to-website.groovy
@@ -40,6 +40,17 @@ def main() {
     }
 
     smart_stage(
+        name: "Update bill-of-materials symlinks",
+        condition: params.UPDATE_BRANCH_LATEST_BOM_SYMLINKS || params.UPDATE_LATEST_BOM_SYMLINKS,
+    ) {
+        artifacts_helper.update_bom_symlinks(
+            cmk_version_rc_aware,
+            branch_latest=params.UPDATE_BRANCH_LATEST_BOM_SYMLINKS,
+            latest=params.UPDATE_LATEST_BOM_SYMLINKS
+        );
+    }
+
+    smart_stage(
         name: "Cleanup RC candicates",
         condition: params.CIPARAM_REMOVE_RC_CANDIDATES,
     ) {

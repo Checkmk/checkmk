899d4d5a2ce7b219edfabc6a36153d8e6d2e8081
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-04-30 09:19:17 +0200

    18005 FIX hp_proliant_da_cntlr: Convey meaning of state "other"
    If an HP Proliant device returned a state "other" in its SNMP data, the service state in Checkmk was reported as "UNKNOWN".
    
    This state in the SNMP data points to a problem with the HP instrument agent that cannot determine the state.
    
    Now, it gets reported as "WARN" with the notice that an administrator can address the problem by upgrading the instrument agent.
    
    SUP-23523
    
    Change-Id: I6f01a8e6aa5dc6d9459de34e41f2def630c8c277

--- tests/unit/checks/generictests/datasets/hp_proliant_da_cntlr.py
+++ tests/unit/checks/generictests/datasets/hp_proliant_da_cntlr.py
@@ -10,9 +10,10 @@ checkname = "hp_proliant_da_cntlr"
 info = [
     ["0", "54", "0", "2", "1", "2", "2", "N/A"],
     ["3", "52", "3", "3", "1", "2", "2", "PXXXX0BRH6X59X"],
+    ['6', '1', '6', '2', '1', '8', '1', 'PEYHN0ARCC307J'],
 ]
 
-discovery = {"": [("0", None), ("3", None)]}
+discovery = {"": [("0", None), ("3", None), ("6", None)]}
 
 checks = {
     "": [
@@ -37,6 +38,17 @@ checks = {
                     [],
                 )
             ],
+        ),
+        (
+            "6",
+            {},
+            [
+                (
+                    1,
+                    "Condition: ok, Board-Condition: other (The instrument agent does not recognize the status of the controller. You may need to upgrade the instrument agent.) (!), Board-Status: enabled, (Role: other, Model: 1, Slot: 6, Serial: PEYHN0ARCC307J)",
+                    []
+                )
+            ]
         ),
     ]
 }

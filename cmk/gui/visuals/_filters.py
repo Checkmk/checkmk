cebed46bea018b034881c8c7f103f3ddf7a84d15
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-05-02 08:15:32 +0200

    17925 FIX Rename filters "Host/Service check command" to indicate using regex
    The "Host check command" and "Service check command" filters are executed as regex filters.
    However, the name does not indicate this behaviour. Therefore, both filter names have been modified by adding "(regex)" to the filter name.
    
    Change-Id: I95f78097adc5d2595d1025700da0c79b93274b89

--- cmk/gui/visuals/_filters.py
+++ cmk/gui/visuals/_filters.py
@@ -245,7 +245,7 @@ def register_host_and_service_basic_filters(filter_registry: FilterRegistry) -> 
 
     filter_registry.register(
         AjaxDropdownFilter(
-            title=_l("Host check command"),
+            title=_l("Host check command (regex)"),
             sort_index=110,
             info="host",
             autocompleter=AutocompleterConfig(ident="check_cmd"),
@@ -258,7 +258,7 @@ def register_host_and_service_basic_filters(filter_registry: FilterRegistry) -> 
 
     filter_registry.register(
         AjaxDropdownFilter(
-            title=_l("Service check command"),
+            title=_l("Service check command (regex)"),
             sort_index=210,
             info="service",
             autocompleter=AutocompleterConfig(ident="check_cmd"),

1bb8bd4c0eebf754563cf8840125d3da1ffd23d3
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-05-06 15:44:36 +0200

    Revert "mkp-tool: add integration test"
    This reverts commit 9f192932ddd05e26032838ed94446df3825cbaef.
    
    Change-Id: I3312534f55449179362e66b7903b9eae2b970b1a

--- tests/integration/bin/test_mkp.py
+++ tests/integration/bin/test_mkp.py
@@ -3,16 +3,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import json
-from collections.abc import Iterator, Sequence
-from contextlib import contextmanager
-from pathlib import Path
-
 import pytest
 
 from tests.testlib.site import Site
-
-from cmk.mkp_tool import Manifest, PackageName, PackagePart, PackageVersion
 
 
 def test_mkp_help(site: Site) -> None:
@@ -38,114 +31,3 @@ def test_mkp_update_active(site: Site) -> None:
 def test_mkp_non_existing(site: Site) -> None:
     with pytest.raises(Exception):
         site.check_output(["mkp", "nubbel"])
-
-
-def test_mkp_install_with_legacy_lib_file(site: Site) -> None:
-    """We're removing the link local/lib/check_mk -> local/lib/python3/cmk.
-
-    Make sure we can install mkps that expect the old structure."""
-    # nothing there yet:
-    assert not _local_stored_mkps(site)
-
-    # add the mkp:
-    with _build_mkp_with_legacy_lib_file(site) as manifest:
-        assert _local_stored_mkps(site) == [manifest.name]
-        assert not _installed_mkps(site)
-
-        # We're set up. Now, even though
-        assert not site.file_exists(Path("local/lib/check_mk"))
-        # but
-        assert "check_mk/myfile.py" in _lib_files_of_mkp(site, manifest.name)
-        # we can still install it:
-        with _enabled_mkp(site, manifest.name):
-            assert _installed_mkps(site) == [manifest.name]
-
-            # We also should correctly recognise all files as part of the mkp,
-            # so we only see the mkp file itself as unpackaged:
-            (mkp_file,) = _unpackaged_files(site)
-            assert mkp_file.endswith(f"{manifest.name}-{manifest.version}.mkp")
-
-    # make sure we've cleared everything:
-    assert not _local_stored_mkps(site)
-
-
-@contextmanager
-def _enabled_mkp(site: Site, name: str) -> Iterator[None]:
-    _mkp_cmd(site, "enable", name)
-    try:
-        yield
-    finally:
-        _mkp_cmd(site, "disable", name)
-
-
-@contextmanager
-def _temporary_manifest_file(site: Site, manifest: Manifest) -> Iterator[Path]:
-    manifest_file = Path("tmp/manifest.tmp")
-    site.write_text_file(manifest_file, manifest.file_content())
-    try:
-        yield manifest_file
-    finally:
-        site.delete_file(manifest_file)
-
-
-@contextmanager
-def _legacy_lib_file(site: Site) -> Iterator[Path]:
-    lib_dir = Path("local/lib")
-    lib_file = Path("check_mk", "myfile.py")
-    site_rel_lib_file = lib_dir / lib_file
-
-    try:
-        site.makedirs(site_rel_lib_file.parent)
-        site.write_text_file(site_rel_lib_file, "")
-        yield lib_file
-    finally:
-        site.delete_file(site_rel_lib_file)  # should be gone anyway
-        site.delete_dir(site_rel_lib_file.parent)
-
-
-@contextmanager
-def _build_mkp_with_legacy_lib_file(site: Site) -> Iterator[Manifest]:
-    with _legacy_lib_file(site) as lib_file:
-        manifest = Manifest(
-            title="Integration test",
-            name=PackageName("integration_test"),
-            description="Test mkp for integration tests",
-            version=PackageVersion("1.0.0"),
-            version_packaged="mkp-tool 1.0.0",
-            version_min_required="2.0.0",
-            version_usable_until="42.0.0",  # I should be retired by then
-            author="",
-            download_url="",
-            files={PackagePart.LIB: [lib_file]},
-        )
-        with _temporary_manifest_file(site, manifest) as manifest_file:
-            _mkp_cmd(site, "package", str(manifest_file))
-
-        _mkp_cmd(site, "disable", manifest.name)
-
-    try:
-        yield manifest
-    finally:
-        _mkp_cmd(site, "remove", manifest.name)
-
-
-def _mkp_cmd(site: Site, *args: str) -> str:
-    return site.check_output(["mkp", "--debug", *args])
-
-
-def _local_stored_mkps(site: Site) -> Sequence[str]:
-    # Get the list of installed packages
-    return [p["name"] for p in json.loads(_mkp_cmd(site, "list", "--json"))["stored"]["local"]]
-
-
-def _installed_mkps(site: Site) -> Sequence[str]:
-    # Get the list of installed packages
-    return [p["name"] for p in json.loads(_mkp_cmd(site, "list", "--json"))["installed"]]
-
-
-def _lib_files_of_mkp(site: Site, mkp_name: str) -> Sequence[str]:
-    return json.loads(_mkp_cmd(site, "show", "--json", mkp_name))["files"].get("lib", [])
-
-
-def _unpackaged_files(site: Site) -> Sequence[str]:
-    return [p["file"] for p in json.loads(_mkp_cmd(site, "find", "--json"))]

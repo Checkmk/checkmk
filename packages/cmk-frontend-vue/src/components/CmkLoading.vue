909f4da4d1850389ef33368eb17a69cc941320be
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-05-06 09:33:00 +0200

    quick_setup: background job running indication
    Under certain undetermined circumstances, a background job may continue
    to report active status even after all subtasks have been completed,
    giving the user the impression that the process is hung. A loading
    indicator is added to the BackgroundJobLog component that will be
    active as long as the associated background job is active.
    
    CMK-23299
    
    Change-Id: I0aa37566cfdf17a111bdd0839303e8c91d90cc47

---
+++ packages/cmk-frontend-vue/src/components/CmkLoading.vue
@@ -0,0 +1,68 @@
+<!--
+Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+interface CmkLoadingProps {
+  height?: string
+}
+withDefaults(defineProps<CmkLoadingProps>(), {
+  height: '8px'
+})
+</script>
+
+<template>
+  <div class="spinner">
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+  </div>
+</template>
+
+<style scoped>
+.spinner > div {
+  width: v-bind(height);
+  height: v-bind(height);
+  background-color: var(--success-dimmed-2);
+
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
+
+.spinner .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+
+.spinner .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+
+@-webkit-keyframes sk-bouncedelay {
+  0%,
+  80%,
+  100% {
+    -webkit-transform: scale(0);
+  }
+  40% {
+    -webkit-transform: scale(1);
+  }
+}
+
+@keyframes sk-bouncedelay {
+  0%,
+  80%,
+  100% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  40% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+}
+</style>

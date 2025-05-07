f5b63ee2e148c976822034323b92a422227eeac7
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-05-07 09:23:24 +0200

    Build all editions before triggering post submit heavy chain
    ... to make the post submit heavy chain a only testing chain and reduce
    the runtime of the triggered jobs as well as the API calls requesting
    the package build status. Furthermore the used executors are freed
    faster as they are not just waiting for the package to be available but
    only run the tests.
    
    Follows 8e8300f496553e59b79bf654839bee3e640f1703
    
    CMK-23344
    
    Change-Id: Iab3d0c2b4943730dc9fb0c9f164c0748376d93c0

--- buildscripts/scripts/trigger-build-upload-cmk-distro-package.groovy
+++ buildscripts/scripts/trigger-build-upload-cmk-distro-package.groovy
@@ -20,7 +20,6 @@ def main() {
     def package_helper = load("${checkout_dir}/buildscripts/scripts/utils/package_helper.groovy");
 
     def distro = params.DISTRO;
-    def edition = params.EDITION;
     def fake_windows_artifacts = params.FAKE_WINDOWS_ARTIFACTS;
     def trigger_post_submit_heavy_chain = params.TRIGGER_POST_SUBMIT_HEAVY_CHAIN;
     def build_node = params.CIPARAM_OVERRIDE_BUILD_NODE;
@@ -39,17 +38,7 @@ def main() {
     def download_dir = "package_download";
     def incremented_counter = "";
     def setup_values = single_tests.common_prepare(version: "daily");
-    def build_instance = null;
-
-    currentBuild.description += (
-        """
-        |Build and upload package<br>
-        |safe_branch_name: ${safe_branch_name}<br>
-        |branch_version: ${branch_version}<br>
-        |cmk_version: ${cmk_version}<br>
-        |edition: ${edition}<br>
-        |distro: ${distro}<br>
-        """.stripMargin());
+    def all_editions = ["cloud", "enterprise", "managed", "saas", params.EDITION].unique();
 
     print(
         """
@@ -59,7 +48,7 @@ def main() {
         |cmk_version:.............. │${cmk_version}│
         |cmk_version_rc_aware:..... │${cmk_version_rc_aware}│
         |branch_version:........... │${branch_version}│
-        |edition:.................. │${edition}│
+        |all_editions:............. │${all_editions}│
         |distro:................... │${distro}│
         |checkout_dir:............. │${checkout_dir}│
         |branch_base_folder:....... │${branch_base_folder}│
@@ -79,61 +68,72 @@ def main() {
         }
     }
 
-    smart_stage(
-        name: "Trigger package build",
-        raiseOnError: true,
-    ) {
-        inside_container_minimal(safe_branch_name: safe_branch_name) {
-            build_instance = smart_build(
-                // see global-defaults.yml, needs to run in minimal container
-                use_upstream_build: true,
-                relative_job_name: "${branch_base_folder}/builders/build-cmk-distro-package",
-                build_params: [
-                    CUSTOM_GIT_REF: effective_git_ref,
-                    DISTRO: distro,
-                    EDITION: edition,
-                    // DISABLE_CACHE: params.DISABLE_CACHE,
-                    FAKE_WINDOWS_ARTIFACTS: fake_windows_artifacts,
-                ],
-                build_params_no_check: [
-                    CIPARAM_OVERRIDE_BUILD_NODE: build_node,
-                    CIPARAM_CLEANUP_WORKSPACE: params.CIPARAM_CLEANUP_WORKSPACE,
-                    CIPARAM_BISECT_COMMENT: params.CIPARAM_BISECT_COMMENT,
-                    CIPARAM_OVERRIDE_DOCKER_TAG_BUILD: setup_values.docker_tag,
-                ],
-                no_remove_others: true, // do not delete other files in the dest dir
-                download: false,    // use copyArtifacts to avoid nested directories
-            );
-        }
+    def stages = all_editions.collectEntries { edition ->
+        [("${edition}") : {
+            def build_instance = null;
+
+            smart_stage(
+                name: "Trigger ${edition} package build",
+                raiseOnError: true,
+            ) {
+                build_instance = smart_build(
+                    // see global-defaults.yml, needs to run in minimal container
+                    use_upstream_build: true,
+                    relative_job_name: "${branch_base_folder}/builders/build-cmk-distro-package",
+                    build_params: [
+                        CUSTOM_GIT_REF: effective_git_ref,
+                        VERSION: params.VERSION,
+                        EDITION: edition,
+                        DISTRO: distro,
+                        DISABLE_CACHE: params.DISABLE_CACHE,
+                        CIPARAM_OVERRIDE_DOCKER_TAG_BUILD: setup_values.docker_tag,
+                        FAKE_WINDOWS_ARTIFACTS: fake_windows_artifacts,
+                    ],
+                    build_params_no_check: [
+                        CIPARAM_OVERRIDE_BUILD_NODE: build_node,
+                        CIPARAM_CLEANUP_WORKSPACE: params.CIPARAM_CLEANUP_WORKSPACE,
+                        CIPARAM_BISECT_COMMENT: params.CIPARAM_BISECT_COMMENT,
+                    ],
+                    no_remove_others: true, // do not delete other files in the dest dir
+                    download: false,    // use copyArtifacts to avoid nested directories
+                );
+            }
+
+            smart_stage(
+                name: "Copy artifacts",
+                condition: build_instance,
+                raiseOnError: true,
+            ) {
+                copyArtifacts(
+                    projectName: "${branch_base_folder}/builders/build-cmk-distro-package",
+                    selector: specific(build_instance.getId()),
+                    target: "${checkout_dir}/${download_dir}/",
+                    fingerprintArtifacts: true,
+                )
+            }
+        }]
     }
 
-    smart_stage(
-        name: "Copy artifacts",
-        condition: build_instance,
-        raiseOnError: true,
-    ) {
-        copyArtifacts(
-            projectName: "${branch_base_folder}/builders/build-cmk-distro-package",
-            selector: specific(build_instance.getId()),
-            target: "${checkout_dir}/${download_dir}/",
-            fingerprintArtifacts: true,
-        )
+    inside_container_minimal(safe_branch_name: safe_branch_name) {
+        currentBuild.result = parallel(stages).values().every { it } ? "SUCCESS" : "FAILURE";
     }
 
     stage("Upload artifacts") {
-        def package_name = versioning.get_package_name("${checkout_dir}/${download_dir}", distro_package_type(distro), edition, cmk_version);
-        def upload_path = "${INTERNAL_DEPLOY_DEST}/testbuild/${cmk_version_rc_aware}/${edition}/${incremented_counter}-${effective_git_ref}/";
+        for (edition in all_editions) {
+            def package_name = versioning.get_package_name("${checkout_dir}/${download_dir}", distro_package_type(distro), edition, cmk_version);
+            def upload_path = "${INTERNAL_DEPLOY_DEST}/testbuild/${cmk_version_rc_aware}/${edition}/${incremented_counter}-${effective_git_ref}/";
 
-        println("package name is: ${package_name}");
-        println("upload_path: ${upload_path}");
+            println("package name is: ${package_name}");
+            println("upload_path: ${upload_path}");
 
-        artifacts_helper.upload_version_dir(
-            "${checkout_dir}/${download_dir}/${package_name}",
-            "${upload_path}",
-            INTERNAL_DEPLOY_PORT,
-            "",
-            "--mkpath",
-        );
+            artifacts_helper.upload_version_dir(
+                "${checkout_dir}/${download_dir}/${package_name}",
+                "${upload_path}",
+                INTERNAL_DEPLOY_PORT,
+                "",
+                "--mkpath",
+            );
+        }
     }
 
     smart_stage(

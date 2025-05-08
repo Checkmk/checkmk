45e28850b9baf38743916f1d1975ab2581f964d4
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-05-08 11:37:13 +0200

    Fixup add missing safe_branch_name
    Fix for d726a12aad9c341c141688b0f93c9d3ca8376e52
    
    Change-Id: I8aa8564de0bbca70f6583669209e603648dbaf69

--- buildscripts/scripts/trigger-post-submit-test-cascade-heavy.groovy
+++ buildscripts/scripts/trigger-post-submit-test-cascade-heavy.groovy
@@ -6,6 +6,9 @@
 
 def main() {
     def package_helper = load("${checkout_dir}/buildscripts/scripts/utils/package_helper.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+
+    def safe_branch_name = versioning.safe_branch_name();
 
     /// This will get us the location to e.g. "checkmk/master" or "Testing/<name>/checkmk/master"
     def branch_base_folder = package_helper.branch_base_folder(with_testing_prefix: true);
@@ -27,6 +30,7 @@ def main() {
     print(
         """
         |===== CONFIGURATION ===============================
+        |safe_branch_name:.... │${safe_branch_name}│
         |job_names:........... │${job_names}│
         |branch_base_folder:.. │${checkout_dir}│
         |===================================================

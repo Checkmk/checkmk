964f8932900b0bb36e4855fcedce033a32891e21
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-05-09 07:38:10 +0200

    haproxy: Move ruleset
    Change-Id: Ib0d0b716e6f056ecfec9d8b4fd111ccaad172043

--- cmk/plugins/haproxy/rulesets/haproxy.py
+++ cmk/plugins/haproxy/rulesets/haproxy.py
@@ -2,9 +2,11 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from enum import Enum
 from typing import cast
 
+from cmk.plugins.haproxy.lib import DEFAULT_FRONTEND_STATES as FRONTEND_STATES
+from cmk.plugins.haproxy.lib import DEFAULT_SERVER_STATES as SERVER_STATES
+from cmk.plugins.haproxy.lib import HAProxyFrontendStatus, HAProxyServerStatus
 from cmk.rulesets.v1 import Help, Title
 from cmk.rulesets.v1.form_specs import (
     DefaultValue,
@@ -13,47 +15,6 @@ from cmk.rulesets.v1.form_specs import (
     ServiceState,
 )
 from cmk.rulesets.v1.rule_specs import CheckParameters, HostAndItemCondition, Topic
-
-
-class HAProxyFrontendStatus(Enum):
-    OPEN = "OPEN"
-    STOP = "STOP"
-
-
-class HAProxyServerStatus(Enum):
-    """
-    https://www.haproxy.com/documentation/haproxy-configuration-manual/2-5r1/management/
-
-    Notes:
-        * MAINT (resolution) and MAINT (via) are listed explicitly as states in the linked
-        documentation, therefore including them in the enum instead of treating them as a partial
-        status.
-    """
-
-    UP = "UP"
-    DOWN = "DOWN"
-    NOLB = "NOLB"
-    MAINT = "MAINT"
-    MAINT_RES = "MAINT (resolution)"
-    MAINT_VIA = "MAINT (via)"
-    DRAIN = "DRAIN"
-    NO_CHECK = "no check"
-
-
-FRONTEND_STATES = {
-    HAProxyFrontendStatus.OPEN: ServiceState.OK,
-    HAProxyFrontendStatus.STOP: ServiceState.CRIT,
-}
-SERVER_STATES = {
-    HAProxyServerStatus.UP: ServiceState.OK,
-    HAProxyServerStatus.DOWN: ServiceState.CRIT,
-    HAProxyServerStatus.NOLB: ServiceState.CRIT,
-    HAProxyServerStatus.MAINT: ServiceState.CRIT,
-    HAProxyServerStatus.MAINT_VIA: ServiceState.WARN,
-    HAProxyServerStatus.MAINT_RES: ServiceState.WARN,
-    HAProxyServerStatus.DRAIN: ServiceState.CRIT,
-    HAProxyServerStatus.NO_CHECK: ServiceState.CRIT,
-}
 
 
 def _migrate_dict_keys(

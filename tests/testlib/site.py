1d424ac28b85dc51c0a68a9115f04057c63e1b40
René Slowenski <rene.slowenski@checkmk.com>
2025-05-12 18:33:51 +0200

    testlib.site: Stop site before removal in get_test_site()
    The get_test_site() helper was forcefully removing the site w/o
    stopping it. We should stop the site before calling rm(), since
    this will make debugging easier in case there is an issue while
    stopping the site.
    
    Change-Id: I97f5ebe87b1b1a9b241d7d618aa21619d98a7e1d
    (cherry picked from commit 5e5ddc046ccb68bf4adb9e2895c5cb1de89cef8c)

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -2130,6 +2130,7 @@ class SiteFactory:
                 site.report_crashes()
             if auto_cleanup and cleanup_site:
                 logger.info('Dropping site "%s" (CLEANUP=1)', site.id)
+                site.stop()
                 site.rm()
 
     def remove_site(self, name: str) -> None:

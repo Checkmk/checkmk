0c8728b7900e7a6ec485dedc7b663a187c15b7b1
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-05-13 10:28:41 +0200

    prism_hosts: migrate
    SUP-23638
    
    Change-Id: I1ef533e9b716c2fffbf311bce11a87066525a9f1

---
+++ cmk/plugins/prism/rulesets/prism_hosts.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.rulesets.v1 import form_specs, rule_specs, Title
+
+
+def _parameters_valuespec_prism_hosts() -> form_specs.Dictionary:
+    return form_specs.Dictionary(
+        elements={
+            "system_state": form_specs.DictElement(
+                parameter_form=form_specs.String(
+                    title=Title("Wanted Host State"),
+                    prefill=form_specs.DefaultValue("NORMAL"),
+                    custom_validate=(form_specs.validators.LengthInRange(min_value=1),),
+                ),
+            )
+        },
+        title=Title("Wanted Host State for defined Nutanix Host"),
+    )
+
+
+rule_spec_prism_hosts = rule_specs.CheckParameters(
+    name="prism_hosts",
+    topic=rule_specs.Topic.VIRTUALIZATION,
+    parameter_form=_parameters_valuespec_prism_hosts,
+    title=Title("Nutanix Host State"),
+    condition=rule_specs.HostAndItemCondition(item_title=Title("Host")),
+)

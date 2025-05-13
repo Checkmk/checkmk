81f4ad0b3d850766b72601693cd3487be788ad54
Robin Loesch <robin.loesch@checkmk.com>
2025-05-13 14:28:11 +0200

    Make elasticsearch plugin a namespace package in deployments
    Needed to backport a recent change to the plugin into the 2.4 branch via
    MKP
    
    CMK-22703
    
    Change-Id: I502815e7b2a4624804bc040314be38bf2ed79d66

--- cmk/plugins/elasticsearch/rulesets/__init__.py
+++ cmk/plugins/elasticsearch/rulesets/__init__.py
@@ -2,3 +2,16 @@
 # Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+
+#
+#    This file must not contain any code.
+#
+#    It is only here to make our linters happy.
+#    It will not be deployed.
+#
+#    This folder is part of a namespace package, that can be shadowed/extended
+#    using the local/ hierarchy.
+#
+#    Do not change the following line, it is picked up by the build process:
+#    check_mk.make: do-not-deploy
+#

adeb252bff980515e0568aaba66142c5cf200162
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-05-14 14:07:07 +0200

    Remove unused groovy code
    Found via CMK-22367
    
    Change-Id: If841a44b352a1c80b9c9fbfad87abae35ab1b86f

--- buildscripts/scripts/utils/notify.groovy
+++ buildscripts/scripts/utils/notify.groovy
@@ -2,30 +2,6 @@
 
 /// file: notify.groovy
 
-def get_author_email() {
-    // Workaround since CHANGE_AUTHOR_EMAIL is not available
-    // Bug: https://issues.jenkins-ci.org/browse/JENKINS-39838
-    return (
-        onWindows ?
-            /// windows will replace %ae with ae..
-            cmd_output('git log -1 --pretty=format:%%ae') :
-            cmd_output('git log -1 --pretty=format:%ae'));
-}
-
-// Send a build failed massage to jenkins
-def slack_build_failed(error) {
-    slackSend(
-        botUser: true,
-        color: 'danger',
-        message: ("""
-            |Build Failed:
-            |    ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)
-            |Error Message:
-            |    ${error}
-            |""".stripMargin()),
-    );
-}
-
 def notify_error(error) {
     // It seems the option "Allowed domains" is not working properly.
     // See: https://ci.lan.tribe29.com/configure
@@ -126,23 +102,6 @@ def notify_error(error) {
     } catch (Exception exc) {    // groovylint-disable CatchException
         print("Could not report error by mail - got ${exc}");
     }
-
-    // Disabled for the moment. It currently does not work because of some
-    // wrong configuration.
-    //
-    // From the build logs:
-    //
-    // [Pipeline] slackSend
-    // Slack Send Pipeline step running, values are - baseUrl: <empty>,
-    // teamDomain: <empty>, channel: build-notifications, color: danger,
-    // botUser: true, tokenCredentialId: <empty>, iconEmoji <empty>, username
-    // <empty>
-    //ERROR: Slack notification failed with exception:
-    //java.lang.IllegalArgumentException: the token with the provided ID could not be found and no token was specified
-    //
-    //slack_build_failed(error)
-    // after notifying everybody, the error needs to be thrown again
-    // This ensures that the build status is set correctly
 }
 
 return this;

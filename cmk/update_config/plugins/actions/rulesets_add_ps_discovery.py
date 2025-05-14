f2fe47c2ce88efb9901e4d85941b8c3a160c9910
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-05-14 14:15:22 +0200

    18052 FIX Piggyback Hub: Fix rule for self-monitoring of RabbitMQ
    In version 2.4.0 the piggyback hub, RabbitMQ and self-monitoring for them were introduced.
    Even when the hub was disabled, an automatically added "_Process discovery_" rule could pick up related processes and lead to the discovery of a "_Process * rabbitmq_" service, which would transition to CRIT after a restart or reboot.
    
    With this werk the pattern in the rule gets adjusted to only count "rabbitmq-server" processes.
    
    SUP-23722
    
    Change-Id: I7c2c696af20539314009c4df4e3bf63185c7ec1e

--- cmk/update_config/plugins/actions/rulesets_add_ps_discovery.py
+++ cmk/update_config/plugins/actions/rulesets_add_ps_discovery.py
@@ -14,6 +14,7 @@ from cmk.gui.watolib.sample_config import PS_DISCOVERY_RULES
 from cmk.update_config.registry import update_action_registry, UpdateAction
 
 PS_DISCOVERY_RULE_NAME = "inventory_processes_rules"
+RABBITMQ_RULE_ID = "65a3dca4-8d71-45d8-8887-53ef0c63d06f"
 
 
 class UpdatePSDiscovery(UpdateAction):
@@ -21,6 +22,7 @@ class UpdatePSDiscovery(UpdateAction):
     def __call__(self, logger: Logger) -> None:
         all_rulesets = AllRulesets.load_all_rulesets()
         add_ps_discovery_rules(logger, all_rulesets)
+        overwrite_ps_discovery_rules(logger, all_rulesets)
         all_rulesets.save(pprint_value=active_config.wato_pprint_config)
 
 
@@ -40,6 +42,30 @@ def add_ps_discovery_rules(
         ps_discovery_rules.prepend_rule(
             root_folder, Rule.from_config(root_folder, ps_discovery_rules, rule)
         )
+
+
+def overwrite_ps_discovery_rules(logger: Logger, all_rulesets: RulesetCollection) -> None:
+    if (ps_discovery_rules := all_rulesets.get_rulesets().get(PS_DISCOVERY_RULE_NAME)) is None:
+        return
+
+    try:
+        rabbitmq_rule = ps_discovery_rules.get_rule_by_id(RABBITMQ_RULE_ID)
+    except KeyError:
+        logger.debug("No default rule for self-monitoring of RabbitMQ.")
+        return
+
+    if rabbitmq_rule.value["match"] == "~(?:/omd/versions/.*/lib/erlang)|(?:.*bin/rabbitmq)":
+        try:
+            default_rule = next(
+                rule for rule in PS_DISCOVERY_RULES if rule["id"] == RABBITMQ_RULE_ID
+            )
+        except StopIteration:
+            logger.debug("No actual rule for self-monitoring of RabbitMQ.")
+            return
+        rabbitmq_rule.value["match"] = default_rule["value"]["match"]
+        logger.info("Overwriting default value: %s", rabbitmq_rule.rule_options.description)
+    else:
+        logger.debug("Rule for self-monitoring of RabbitMQ was changed. Nothing to do.")
 
 
 def _some_shipped_rules_present(current_rules: Ruleset) -> bool:
--- tests/unit/cmk/update_config/plugins/actions/test_rulesets_add_ps_discovery.py
+++ tests/unit/cmk/update_config/plugins/actions/test_rulesets_add_ps_discovery.py
@@ -8,12 +8,15 @@ import logging
 import pytest
 
 from cmk.gui.watolib.hosts_and_folders import folder_tree
-from cmk.gui.watolib.rulesets import Rule, Ruleset, RulesetCollection
+from cmk.gui.watolib.rulesets import Rule, RuleConditions, RuleOptions, Ruleset, RulesetCollection
 from cmk.gui.watolib.sample_config import PS_DISCOVERY_RULES
+from cmk.gui.watolib.sample_config._constants import _PS_COMMON_OPTS
 
 from cmk.update_config.plugins.actions.rulesets_add_ps_discovery import (
     add_ps_discovery_rules,
+    overwrite_ps_discovery_rules,
     PS_DISCOVERY_RULE_NAME,
+    RABBITMQ_RULE_ID,
 )
 
 
@@ -47,3 +50,50 @@ def test_update_with_preexisting_rulesets() -> None:
 
     add_ps_discovery_rules(logging.getLogger(), rulesets)
     assert rulesets.get_rulesets()[PS_DISCOVERY_RULE_NAME].num_rules() == 1
+
+
+@pytest.mark.usefixtures("request_context")
+@pytest.mark.parametrize(
+    ["preexisting", "expected"],
+    [
+        pytest.param(None, "~(?:.*bin/rabbitmq-server)", id="no preexisting rule"),
+        pytest.param(
+            "~(?:/omd/versions/.*/lib/erlang)|(?:.*bin/rabbitmq)",
+            "~(?:.*bin/rabbitmq-server)",
+            id="overwrite old default value in rule",
+        ),
+        pytest.param("foobar", "foobar", id="keep manually changed value in rule"),
+    ],
+)
+def test_update_rule_default_for_rabbitmq(preexisting: str | None, expected: str) -> None:
+    ruleset = Ruleset(PS_DISCOVERY_RULE_NAME, {})
+    if preexisting is not None:
+        folder = folder_tree().root_folder()
+        ruleset.append_rule(
+            folder,
+            Rule(
+                id_=RABBITMQ_RULE_ID,
+                folder=folder,
+                ruleset=ruleset,
+                conditions=RuleConditions(folder.path()),
+                options=RuleOptions(
+                    disabled=False,
+                    description="",
+                    comment="",
+                    docu_url="",
+                    predefined_condition_id=None,
+                ),
+                value={"descr": "Something", "match": preexisting, **_PS_COMMON_OPTS},
+            ),
+        )
+
+    overwrite_ps_discovery_rules(
+        logging.getLogger(), RulesetCollection({PS_DISCOVERY_RULE_NAME: ruleset})
+    )
+
+    if preexisting is None:
+        assert ruleset.num_rules() == 0
+    else:
+        assert ruleset.num_rules() == 1
+        rule = ruleset.get_rule_by_id(RABBITMQ_RULE_ID)
+        assert rule.value["match"] == expected

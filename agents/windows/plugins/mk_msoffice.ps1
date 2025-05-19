139891628972ebcabac6d02c2c0c3b2eada36edc
Diana Ivanchevska <diana.ivanchevska@checkmk.com>
2025-05-19 13:08:51 +0200

    wa: use json formatted config by mk_msoffice.ps1 plugin
    Change-Id: I2b8b696c8e2264ba12979f87e373e470da0efecd

--- agents/windows/plugins/mk_msoffice.ps1
+++ agents/windows/plugins/mk_msoffice.ps1
@@ -1,30 +1,63 @@
-$CMK_VERSION = "2.5.0b1"
-## filename for timestamp
-$MK_CONFDIR = $env:MK_CONFDIR
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
 
-# Configuration file for this agent plugin
-$CONFIG_FILE="${MK_CONFDIR}\msoffice.cfg.ps1"
-if (test-path -path "${CONFIG_FILE}" ) {
-     . "${CONFIG_FILE}"
-} else {
+$CMK_VERSION = "2.5.0b1"
+$MK_CONFDIR = $env:MK_CONFDIR
+$ClientId = ""
+$TenantId = ""
+$ClientSecret = ""
+
+if(-not $MK_CONFDIR) {
+    Write-Host "MK_CONFDIR is not initialized, exiting."
+    exit
+}
+
+$confJson = Join-Path $MK_CONFDIR "msoffice_cfg.json"
+
+if(-not (Test-Path $confJson)) {
+    Write-Host "Configuration file msoffice_cfg.json not found in $MK_CONFDIR, exiting."
+    exit
+}
+
+$jsonContent = Get-Content -Path $confJson -Raw
+$config = ConvertFrom-Json -InputObject $jsonContent
+if(-not $config) {
+    Write-Host "Failed to parse JSON configuration from the ${MK_CONFDIR}\msoffice_cfg.json, exiting."
+    exit
+}
+
+$ClientId = $config.ClientId
+$TenantId = $config.TenantId
+$ClientSecret = $config.ClientSecret
+
+if ([string]::IsNullOrEmpty($ClientId) -or
+    [string]::IsNullOrEmpty($TenantId) -or
+    [string]::IsNullOrEmpty($ClientSecret)) {
+    Write-Host "One or more required credentials are empty, check your configuration in ${MK_CONFDIR}\msoffice_cfg.json."
     exit
 }
 
 if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
-    Install-Module -Name Microsoft.Graph -Scope CurrentUser -Force -ErrorAction SilentlyContinue
+    try {
+        Install-Module -Name Microsoft.Graph -Scope CurrentUser -Force -ErrorAction Stop
+    } catch {
+        Write-Host "Failed to install Microsoft.Graph module: $_"
+        exit
+    }
 }
 
 try {
     $SecureClientSecret = ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
     $ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ClientId, $SecureClientSecret
-    Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome
+    Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome -ErrorAction Stop
 } catch {
     Write-Host "Failed to connect to Microsoft Graph: $_"
     exit
 }
 
 try {
-    $licenses = Get-MgSubscribedSku
+    $licenses = Get-MgSubscribedSku -ErrorAction Stop
 } catch {
     Write-Host "Failed to fetch licenses: $_"
     exit

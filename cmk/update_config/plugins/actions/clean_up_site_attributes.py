3ee2779983995a1d2341aa40cdf4d38052785ba1
Lars Michelsen <lm@checkmk.com>
2025-05-27 13:13:36 +0200

    Add update config action to standardize site config
    Add update config action to standardize the site connection
    configuration data structures.
    
    CMK-23722
    
    Change-Id: Ie078d10652f843eec444dd3edb1f168f135d8ded

---
+++ cmk/update_config/plugins/actions/clean_up_site_attributes.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from logging import Logger
+from typing import override
+
+from cmk.gui.config import active_config
+from cmk.gui.watolib.sites import site_management_registry
+
+from cmk.update_config.registry import update_action_registry, UpdateAction
+
+
+class CleanUpSiteAttributes(UpdateAction):
+    @override
+    def __call__(self, logger: Logger) -> None:
+        site_mgmt = site_management_registry["site_management"]
+        configured_sites = site_mgmt.load_sites()
+
+        for site_id, site_spec in configured_sites.items():
+            site_spec.setdefault("alias", str(site_id))
+            site_spec.setdefault("socket", ("local", None))
+            site_spec.setdefault("url_prefix", "../")  # relative URL from /check_mk/
+            site_spec["id"] = site_id
+
+        site_mgmt.save_sites(
+            configured_sites,
+            activate=False,
+            pprint_value=active_config.wato_pprint_config,
+        )
+
+
+update_action_registry.register(
+    CleanUpSiteAttributes(
+        name="clean_up_site_attributes",
+        title="Clean up site connections",
+        sort_index=30,
+    )
+)
---
+++ tests/unit/cmk/update_config/plugins/actions/test_clean_up_site_attributes.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import logging
+
+import pytest
+
+from livestatus import SiteConfiguration, SiteConfigurations
+
+from cmk.ccc.site import SiteId
+
+from cmk.gui.watolib.sites import site_management_registry
+
+from cmk.update_config.plugins.actions.clean_up_site_attributes import (
+    CleanUpSiteAttributes,
+)
+
+
+@pytest.mark.usefixtures("load_config")
+def test_clean_up_missing_alias() -> None:
+    site_mgmt = site_management_registry["site_management"]
+    site_mgmt.save_sites(
+        SiteConfigurations(
+            {
+                SiteId("abc"): SiteConfiguration(
+                    {
+                        "id": SiteId("abc"),
+                        "socket": ("local", None),
+                        "disable_wato": True,
+                        "disabled": False,
+                        "insecure": False,
+                        "url_prefix": "/abc/",
+                        "multisiteurl": "",
+                        "persist": False,
+                        "replicate_ec": False,
+                        "replication": None,
+                        "timeout": 5,
+                        "user_login": True,
+                        "proxy": None,
+                    }
+                )
+            }
+        ),
+        activate=False,
+        pprint_value=True,
+    )
+
+    CleanUpSiteAttributes(
+        name="clean_up_site_attributes",
+        title="Clean up site connections",
+        sort_index=30,
+    )(logging.getLogger())
+
+    assert site_mgmt.load_sites()[SiteId("abc")]["alias"] == "abc"
+
+
+@pytest.mark.usefixtures("load_config")
+def test_clean_up_missing_socket() -> None:
+    site_mgmt = site_management_registry["site_management"]
+    site_mgmt.save_sites(
+        SiteConfigurations(
+            {
+                SiteId("abc"): SiteConfiguration(
+                    {
+                        "id": SiteId("abc"),
+                        "alias": "abc",
+                        "disable_wato": True,
+                        "disabled": False,
+                        "insecure": False,
+                        "url_prefix": "/abc/",
+                        "multisiteurl": "",
+                        "persist": False,
+                        "replicate_ec": False,
+                        "replication": None,
+                        "timeout": 5,
+                        "user_login": True,
+                        "proxy": None,
+                    }
+                )
+            }
+        ),
+        activate=False,
+        pprint_value=True,
+    )
+
+    CleanUpSiteAttributes(
+        name="clean_up_site_attributes",
+        title="Clean up site connections",
+        sort_index=30,
+    )(logging.getLogger())
+
+    assert site_mgmt.load_sites()[SiteId("abc")]["socket"] == ("local", None)
+
+
+@pytest.mark.usefixtures("load_config")
+def test_clean_up_missing_url_prefix() -> None:
+    site_mgmt = site_management_registry["site_management"]
+    site_mgmt.save_sites(
+        SiteConfigurations(
+            {
+                SiteId("abc"): SiteConfiguration(
+                    {
+                        "id": SiteId("abc"),
+                        "alias": "abc",
+                        "socket": ("local", None),
+                        "disable_wato": True,
+                        "disabled": False,
+                        "insecure": False,
+                        "multisiteurl": "",
+                        "persist": False,
+                        "replicate_ec": False,
+                        "replication": None,
+                        "timeout": 5,
+                        "user_login": True,
+                        "proxy": None,
+                    }
+                )
+            }
+        ),
+        activate=False,
+        pprint_value=True,
+    )
+
+    CleanUpSiteAttributes(
+        name="clean_up_site_attributes",
+        title="Clean up site connections",
+        sort_index=30,
+    )(logging.getLogger())
+
+    assert site_mgmt.load_sites()[SiteId("abc")]["url_prefix"] == "../"
+
+
+@pytest.mark.usefixtures("load_config")
+def test_clean_up_missing_id() -> None:
+    site_mgmt = site_management_registry["site_management"]
+    site_mgmt.save_sites(
+        SiteConfigurations(
+            {
+                SiteId("abc"): SiteConfiguration(
+                    {
+                        "alias": "abc",
+                        "socket": ("local", None),
+                        "disable_wato": True,
+                        "disabled": False,
+                        "insecure": False,
+                        "multisiteurl": "",
+                        "persist": False,
+                        "replicate_ec": False,
+                        "replication": None,
+                        "timeout": 5,
+                        "user_login": True,
+                        "proxy": None,
+                    }
+                )
+            }
+        ),
+        activate=False,
+        pprint_value=True,
+    )
+
+    CleanUpSiteAttributes(
+        name="clean_up_site_attributes",
+        title="Clean up site connections",
+        sort_index=30,
+    )(logging.getLogger())
+
+    assert site_mgmt.load_sites()[SiteId("abc")]["id"] == "abc"

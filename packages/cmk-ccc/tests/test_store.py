73e6f01910b03caa93ff2ec390dfc72980115f9e
Sven Panne <sven.panne@checkmk.com>
2025-05-30 09:35:12 +0200

    Moved store test to where it belongs
    We needed to inline some test utility functions to decouple this.
    Some simplifications, ruff fixes, etc. on the way...
    
    Change-Id: If5fc5570cdf27995595cade0c2aa1c8783dc9b83

--- packages/cmk-ccc/tests/test_store.py
+++ packages/cmk-ccc/tests/test_store.py
@@ -5,19 +5,20 @@
 
 import enum
 import errno
+import importlib.machinery
+import importlib.util
 import os
 import queue
+import sys
 import threading
-import types
-from collections.abc import Callable, Generator, Iterator, Sequence
+import time
+from collections.abc import Generator, Iterator, Sequence
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
+from types import ModuleType
 
 import pytest
 from pytest import MonkeyPatch
-
-from tests.testlib.common.utils import wait_until
-from tests.testlib.unit.utils import import_module_hack
 
 import cmk.ccc.debug
 from cmk.ccc import store
@@ -72,8 +73,8 @@ def test_object_store_locked(io: type[FileIo], exists: bool, tmp_path: Path) -> 
     test_file.unlink(missing_ok=True)
 
 
-@pytest.mark.parametrize("io,exists", [(RealIo, True), (FakeIo, False)])
-def test_object_store_default(io: type[FileIo], exists: bool, tmp_path: Path) -> None:
+@pytest.mark.parametrize("io", [RealIo, FakeIo])
+def test_object_store_default(io: type[FileIo], tmp_path: Path) -> None:
     test_file = tmp_path / "locked_hurz"
     a = ObjectStore(test_file, serializer=TextSerializer(), io=io)
     assert a.read_obj(default="zz") == "zz"
@@ -207,16 +208,17 @@ def test_save_text_to_file(tmp_path: Path, data: str) -> None:
     assert store.load_text_from_file(path) == data
 
 
-@pytest.mark.parametrize(
-    "load_fun", [store.load_bytes_from_file, store.load_object_from_file, store.load_text_from_file]
-)
 @pytest.mark.parametrize("permissions", [0o002, 0o666, 0o777])
-def test_load_world_writable_file(tmp_path: Path, load_fun: Callable, permissions: int) -> None:
+def test_load_world_writable_file(tmp_path: Path, permissions: int) -> None:
     path = tmp_path / "writeme.txt"
     store.save_text_to_file(path, "")
     os.chmod(path, permissions)
     with pytest.raises(MKGeneralException, match="Refusing to read world writable file"):
-        load_fun(path, default="")
+        store.load_bytes_from_file(path, default=b"")
+    with pytest.raises(MKGeneralException, match="Refusing to read world writable file"):
+        store.load_object_from_file(path, default=object())
+    with pytest.raises(MKGeneralException, match="Refusing to read world writable file"):
+        store.load_text_from_file(path, default="")
 
 
 @pytest.mark.parametrize(
@@ -249,8 +251,7 @@ def test_save_to_mk_file(tmp_path: Path) -> None:
     assert actual == {"x": {"a": 1, "y": 1}}
 
 
-@pytest.mark.parametrize("path_type", [str, Path])
-def test_acquire_lock_not_existing(tmp_path: Path, path_type: type[str] | type[Path]) -> None:
+def test_acquire_lock_not_existing(tmp_path: Path) -> None:
     assert store.acquire_lock(tmp_path / "asd") is True
 
 
@@ -298,10 +299,10 @@ def test_try_locked_fails(
 ) -> None:
     path = path_type(test_file)
 
-    def _is_already_locked(path: Path, blocking: object) -> bool:
+    def _is_already_locked(path: Path, blocking: object) -> bool:  # noqa: ARG001
         raise OSError(errno.EAGAIN, "%s is already locked" % path)
 
-    monkeypatch.setattr(store._locks, "acquire_lock", _is_already_locked)
+    monkeypatch.setattr(store._locks, "acquire_lock", _is_already_locked)  # noqa: SLF001
 
     assert store.have_lock(path) is False
 
@@ -352,7 +353,7 @@ def test_release_lock_already_closed(test_file: Path, path_type: type[str] | typ
     path = path_type(test_file)
 
     store.acquire_lock(path)
-    fd = store._locks._get_lock(str(path))
+    fd = store._locks._get_lock(str(path))  # noqa: SLF001
     assert isinstance(fd, int)
     os.close(fd)
     store.release_lock(path)
@@ -387,7 +388,7 @@ def test_release_all_locks_already_closed(
     path = path_type(test_file)
     store.acquire_lock(path)
 
-    fd = store._locks._get_lock(str(path))
+    fd = store._locks._get_lock(str(path))  # noqa: SLF001
     assert isinstance(fd, int)
     os.close(fd)
 
@@ -402,7 +403,7 @@ class LockTestJob(enum.Enum):
 
 
 class LockTestThread(threading.Thread):
-    def __init__(self, store_mod: types.ModuleType, path: Path) -> None:
+    def __init__(self, store_mod: ModuleType, path: Path) -> None:
         super().__init__()
         self.daemon = True
 
@@ -411,7 +412,7 @@ class LockTestThread(threading.Thread):
 
         self._jobs: queue.Queue[LockTestJob] = queue.Queue()
 
-    def run(self):
+    def run(self) -> None:
         while True:
             try:
                 job = self._jobs.get(block=True, timeout=0.1)
@@ -436,26 +437,26 @@ class LockTestThread(threading.Thread):
             finally:
                 self._jobs.task_done()
 
-    def terminate(self):
+    def terminate(self) -> None:
         """Send terminate command to thread from outside and wait for completion"""
         self._jobs.put(LockTestJob.TERMINATE)
         self.join()
 
-    def lock(self):
+    def lock(self) -> None:
         """Send lock command to thread from outside and wait for completion"""
         self.lock_nowait()
         self._jobs.join()
 
-    def unlock(self):
+    def unlock(self) -> None:
         """Send unlock command to thread from outside and wait for completion"""
         self._jobs.put(LockTestJob.UNLOCK)
         self._jobs.join()
 
-    def lock_nowait(self):
+    def lock_nowait(self) -> None:
         """Send lock command to thread from outside without waiting for completion"""
         self._jobs.put(LockTestJob.LOCK)
 
-    def join_jobs(self):
+    def join_jobs(self) -> None:
         self._jobs.join()
 
 
@@ -473,6 +474,32 @@ def fixture_test_thread_1(test_file: Path) -> Iterator[LockTestThread]:
     t.terminate()
 
 
+def import_module_hack(pathname: str) -> ModuleType:
+    """Return the module loaded from `pathname`.
+
+    `pathname` is a path relative to the top-level directory
+    of the repository.
+
+    This function loads the module at `pathname` even if it does not have
+    the ".py" extension.
+
+    See: https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+    """
+    name = os.path.splitext(os.path.basename(pathname))[0]
+    location = os.path.join(repo_path(), pathname)
+    loader = importlib.machinery.SourceFileLoader(name, location)
+    spec = importlib.machinery.ModuleSpec(name, loader, origin=location)
+    spec.has_location = True
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
+
+
+def repo_path() -> Path:
+    return Path(__file__).resolve().parent.parent.parent.parent
+
+
 @pytest.fixture(name="t2")
 def fixture_test_thread_2(test_file: Path) -> Iterator[LockTestThread]:
     # HACK: We abuse modules as data containers, so we have to do this Kung Fu...
@@ -487,10 +514,10 @@ def fixture_test_thread_2(test_file: Path) -> Iterator[LockTestThread]:
     t.terminate()
 
 
-def _wait_for_waiting_lock():
+def _wait_for_waiting_lock() -> None:
     """Use /proc/locks to wait until one lock is in waiting state
 
-    https://man7.org/linux/man-pages/man5/proc.5.html
+    https://man7.org/linux/man-pages/man5/proc_locks.5.html
     """
 
     def has_waiting_lock() -> bool:
@@ -498,11 +525,18 @@ def _wait_for_waiting_lock():
         with Path("/proc/locks").open() as f:
             for line in f:
                 p = line.strip().split()
+                # NOTE: The "->" is *not* mentioned on the proc_locks(5) man page!
+                # Nevertheless, it means the PID on the line is waiting for the lock.
                 if p[1] == "->" and p[2] == "FLOCK" and p[4] == "WRITE" and p[5] == str(pid):
                     return True
         return False
 
-    wait_until(has_waiting_lock, timeout=1, interval=0.01)
+    start = time.time()
+    while time.time() - start < 1:
+        if has_waiting_lock():
+            return
+        time.sleep(0.1)
+    raise TimeoutError("Timeout waiting for 'has_waiting_lock' to finish (Timeout: 1 sec)")
 
 
 @pytest.mark.parametrize("path_type", [str, Path])
@@ -513,7 +547,7 @@ def test_blocking_context_manager_from_multiple_threads(
 
     acquired = []
 
-    def acquire(n):
+    def acquire(_n: int) -> None:
         with store.locked(path):
             acquired.append(1)
             assert len(acquired) == 1
@@ -534,7 +568,7 @@ def test_blocking_lock_from_multiple_threads(
     acquired = []
     saw_someone_wait: list[int] = []
 
-    def acquire(n):
+    def acquire(_n: int) -> None:
         assert not store.have_lock(path)
         store.acquire_lock(path, blocking=True)
         assert store.have_lock(path)
@@ -573,7 +607,7 @@ def test_non_blocking_lock_from_multiple_threads(
     acquired = []
 
     # Only one thread will ever be able to acquire this lock.
-    def acquire(_):
+    def acquire(_n: int) -> None:
         try:
             store.acquire_lock(path, blocking=False)
             acquired.append(1)

7800c4ac6f5af043406f487f033514718b0d486e
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-06-02 18:06:51 +0200

    17929 FIX Fix filterquery for host/serice check command (exact match) filter
    For the new host/service check command (exact match), the wrong filter query was used, which led to no results.
    
    This has now been fixed.
    
    Change-Id: Icbccc4c85dd9f333f8ccc5b82729dca8fa5db957

--- cmk/gui/visuals/_filters.py
+++ cmk/gui/visuals/_filters.py
@@ -264,7 +264,7 @@ def register_host_and_service_basic_filters(filter_registry: FilterRegistry) -> 
             sort_index=110,
             info="host",
             autocompleter=AutocompleterConfig(ident="check_cmd"),
-            query_filter=query_filters.CheckCommandQuery(
+            query_filter=query_filters.TextQuery(
                 ident="host_check_command",
                 op="=",
             ),
@@ -291,7 +291,7 @@ def register_host_and_service_basic_filters(filter_registry: FilterRegistry) -> 
             sort_index=210,
             info="service",
             autocompleter=AutocompleterConfig(ident="check_cmd"),
-            query_filter=query_filters.CheckCommandQuery(
+            query_filter=query_filters.TextQuery(
                 ident="check_command",
                 op="=",
                 column="service_check_command",

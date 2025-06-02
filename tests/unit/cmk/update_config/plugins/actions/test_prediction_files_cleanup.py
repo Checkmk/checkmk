c5e8d4a9ea280b772add23c23b60f1b94afde0a3
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-06-02 16:16:33 +0200

    update_config/prediction_files_cleanup: Reduce code duplication in test
    Change-Id: I96c34b3e356e330201ae044189350d95a4fafb98

--- tests/unit/cmk/update_config/plugins/actions/test_prediction_files_cleanup.py
+++ tests/unit/cmk/update_config/plugins/actions/test_prediction_files_cleanup.py
@@ -5,6 +5,8 @@
 
 from pathlib import Path
 
+import pytest
+
 from cmk.utils.prediction import DataStat, PredictionData
 
 from cmk.agent_based.prediction_backend import PredictionInfo, PredictionParameters
@@ -12,19 +14,32 @@ from cmk.update_config.plugins.actions.prediction_files_cleanup import (
     RemoveUnreadablePredictions,
 )
 
+PREDICTION_INFO = PredictionInfo(
+    valid_interval=(23, 42),
+    metric="kuchen_count",
+    direction="upper",
+    params=PredictionParameters(horizon=3, period="wday", levels=("absolute", (1, 2))),
+).model_dump_json()
 
-def test_ok_files_are_kept(tmp_path: Path) -> None:
+
+@pytest.mark.parametrize(
+    ["info_file_content", "file_expected"],
+    [
+        pytest.param(
+            PREDICTION_INFO,
+            True,
+            id="Valid files are kept",
+        ),
+        pytest.param("boo", False, id="Corrupt files are removed"),
+    ],
+)
+def test_cleanup_unreadable_files(
+    tmp_path: Path, info_file_content: str, file_expected: bool
+) -> None:
     info_file = tmp_path / "my_test_prediction.info"
     data_file = tmp_path / "my_test_prediction"
 
-    info_file.write_text(
-        PredictionInfo(
-            valid_interval=(23, 42),
-            metric="kuchen_count",
-            direction="upper",
-            params=PredictionParameters(horizon=3, period="wday", levels=("absolute", (1, 2))),
-        ).model_dump_json(),
-    )
+    info_file.write_text(info_file_content)
     data_file.write_text(
         PredictionData(
             points=[
@@ -38,27 +53,5 @@ def test_ok_files_are_kept(tmp_path: Path) -> None:
 
     RemoveUnreadablePredictions.cleanup_unreadable_files(tmp_path)
 
-    assert info_file.exists()
-    assert data_file.exists()
-
-
-def test_corrupt_files_are_removed(tmp_path: Path) -> None:
-    info_file = tmp_path / "my_test_prediction.info"
-    data_file = tmp_path / "my_test_prediction"
-
-    info_file.write_text("boo")
-    data_file.write_text(
-        PredictionData(
-            points=[
-                DataStat(average=1.0, max_=2.0, min_=3.0, stdev=1.0),
-                DataStat(average=1.0, max_=2.0, min_=4.0, stdev=5.0),
-            ],
-            start=1,
-            step=2,
-        ).model_dump_json(),
-    )
-
-    RemoveUnreadablePredictions.cleanup_unreadable_files(tmp_path)
-
-    assert not info_file.exists()
-    assert not data_file.exists()
+    assert info_file.exists() is file_expected
+    assert data_file.exists() is file_expected

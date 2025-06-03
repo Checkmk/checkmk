7dc4bf13940181c3d58f739b57f23f42a92ae352
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-06-03 18:04:13 +0200

    Toggle parallel CV via Jenkins feature env flag
    CMK-23833
    
    Change-Id: Ic420162fa946e4d9ef4a86f0c4a03f0d07670083

--- buildscripts/scripts/test-gerrit.groovy
+++ buildscripts/scripts/test-gerrit.groovy
@@ -4,7 +4,7 @@
 
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
-def main() {
+def main_parallel() {
     def package_helper = load("${checkout_dir}/buildscripts/scripts/utils/package_helper.groovy");
     def test_gerrit_helper = load("${checkout_dir}/buildscripts/scripts/utils/gerrit_stages.groovy");
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
@@ -254,4 +254,122 @@ def main() {
     }
 }
 
+def main_sequential() {
+    def test_gerrit_helper = load("${checkout_dir}/buildscripts/scripts/utils/gerrit_stages.groovy");
+    // no `def` - must be global
+    test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+
+    def result_dir = "${checkout_dir}/results";
+    def time_job_started = new Date();
+    def time_stage_started = time_job_started;
+    def safe_branch_name = versioning.safe_branch_name();
+
+    print(
+        """
+        |===== CONFIGURATION ===============================
+        |GERRIT_PATCHSET_REVISION:.(global)  │${GERRIT_PATCHSET_REVISION}│
+        |GERRIT_CHANGE_SUBJECT:....(global)  │${GERRIT_CHANGE_SUBJECT}│
+        |GERRIT_BRANCH:............(global)  │${GERRIT_BRANCH}│
+        |===================================================
+        """.stripMargin());
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'nexus',
+            passwordVariable: 'DOCKER_PASSPHRASE',
+            usernameVariable: 'DOCKER_USERNAME')]) {
+        sh('echo  "${DOCKER_PASSPHRASE}" | docker login "${DOCKER_REGISTRY}" -u "${DOCKER_USERNAME}" --password-stdin');
+    }
+
+    /// Add description to the build
+    test_gerrit_helper.desc_init();
+    test_gerrit_helper.desc_add_line("${GERRIT_CHANGE_SUBJECT}");
+    test_gerrit_helper.desc_add_table();
+    test_gerrit_helper.desc_add_row('Stage', 'Duration', 'Status', 'Result files');
+
+    stage("Prepare workspace") {
+        dir("${checkout_dir}") {
+
+            inside_container() {
+                sh("buildscripts/scripts/ensure-workspace-integrity");
+            }
+            sh("rm -rf ${result_dir}; mkdir ${result_dir}");
+        }
+    }
+    try {
+
+        dir("${checkout_dir}") {
+            stage("Create stages") {
+                inside_container_minimal(safe_branch_name: safe_branch_name) {
+                    /// Generate list of stages to be added - save them locally for reference
+                    sh("""python buildscripts/scripts/validate_changes.py \
+                          --env "RESULTS=${result_dir}" \
+                          --env "WORKSPACE=${checkout_dir}" \
+                          --env "PATCHSET_REVISION=${GERRIT_PATCHSET_REVISION}" \
+                          --write-file=${result_dir}/stages.json \
+                          buildscripts/scripts/stages.yml
+                    """);
+                }
+                time_stage_started = new Date();
+            }
+            test_gerrit_helper.desc_add_status_row("Preparation",
+                groovy.time.TimeCategory.minus(new Date(), time_job_started), 0, '--');
+            def stage_info = load_json("${result_dir}/stages.json");
+            def allStagesPassed = true;
+            def thisStagePassed = true;
+            // privileged/set_docker_group_id aka mounting the docker is needed for agent plugin tests: they do docker in docker
+            inside_container(privileged: true, set_docker_group_id: true) {
+                stage_info.STAGES.each { item ->
+                    (thisStagePassed, thisIssues) = test_gerrit_helper.create_stage(item, time_stage_started);
+                    allStagesPassed = thisStagePassed && allStagesPassed;
+                    if (thisIssues && !thisStagePassed) {
+                        stage("Analyse Issues") {
+                            thisIssues.each { issue ->
+                                publishIssues(
+                                    issues: [issue],
+                                    name: "${item.NAME}",
+                                    // Only characters, digits, dashes and underscores allowed
+                                    // ID must match the regex \p{Alnum}[\p{Alnum}-_]*).
+                                    id: "${item.RESULT_CHECK_FILE_PATTERN}".replaceAll("""([^A-Za-z0-9\\-\\_]+)""", "-"),
+                                    trendChartType: 'TOOLS_ONLY',
+                                    qualityGates: [[threshold: 1, type: 'TOTAL', unstable: false]]
+                                );
+                            }
+                        }
+                    }
+                    time_stage_started = new Date();
+                }
+            }
+            currentBuild.result = allStagesPassed ? "SUCCESS" : "FAILED";
+        }
+    } finally {
+        test_gerrit_helper.desc_add_line("Executed on: ${NODE_NAME} in ${WORKSPACE}");
+        stage("Analyse Issues") {
+            dir("${checkout_dir}") {
+                xunit([
+                    Custom(
+                        customXSL: "$JENKINS_HOME/userContent/xunit/JUnit/0.1/pytest-xunit.xsl",
+                        deleteOutputFiles: false,
+                        failIfNotNew: true,
+                        pattern: "results/*junit.xml",
+                        skipNoTestFiles: true,
+                        stopProcessingIfError: true,
+                )]);
+
+                show_duration("archiveArtifacts") {
+                    archiveArtifacts(allowEmptyArchive: true, artifacts: 'results/**');
+                }
+            }
+        }
+    }
+}
+
+def main() {
+    if ("${env.PARALLEL_CV_240_FEATURE_FLAG}" == "1") {
+        main_parallel();
+    } else {
+        main_sequential();
+    }
+}
 return this;
--- buildscripts/scripts/utils/gerrit_stages.groovy
+++ buildscripts/scripts/utils/gerrit_stages.groovy
@@ -4,6 +4,135 @@
 
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
+// Creates a stage with provided properties. The created stage will be marked as
+// successful or failed if its command returns with non-zero and can thus be used
+// to implement tests.
+// NAME:     Display name of the Stage, should describe the test
+// SKIPPED:  Only available when stage shall be skipped. Contains information to be
+//           displayed if the test was skipped
+// DIR:      Directory in which the test is executed
+// ENV_VARS: Array [] of environment variables needed for the test
+// COMMAND:  command that should be executed. It should be possible to use this exact
+//           command to reproduce the test locally in the coresponding DIR
+// JENKINS_API_ACCESS: boolean that configures if env variables for Jenkins API access are present.
+def create_stage(Map args, time_stage_started) {
+    def duration;
+    def issues = [];
+    stage(args.NAME) {
+        if (args.SKIPPED) {
+            Utils.markStageSkippedForConditional(STAGE_NAME);
+            println("SKIPPED: ${args.SKIPPED}");
+            duration = groovy.time.TimeCategory.minus(new Date(), time_stage_started);
+            desc_add_status_row(args.NAME, duration, 'skipped', '--');
+            return [true, issues];
+        }
+
+        println("CMD: ${args.COMMAND}");
+        def cmd_status;
+
+        // We can't use the arguments from SEC_VAR_LIST for accessing the API credentials, because
+        // this does not work with the credential type of our API token (username + password).
+        // Since we need to bind the values to multiple variables that will be usable later in
+        // the job we hardcode these.
+        def credentials = args.SEC_VAR_LIST.collect{string(credentialsId: it, variable: it)}
+        if (args.JENKINS_API_ACCESS) {
+            credentials.add(usernamePassword(
+                credentialsId: 'jenkins-api-token',
+                usernameVariable: 'JENKINS_USERNAME',
+                passwordVariable: 'JENKINS_PASSWORD'
+            ))
+        }
+
+        def jenkins_gerrit_key = sshUserPrivateKey(
+            credentialsId: "jenkins-gerrit-fips-compliant-ssh-key",
+            keyFileVariable: 'KEYFILE'
+        )
+
+        smart_stage(
+            name: "Fetch git notes",
+            condition: args.GIT_FETCH_NOTES,
+        ) {
+            dir("${checkout_dir}") {
+                withCredentials(([jenkins_gerrit_key])
+                ) {
+                    withEnv(["GIT_SSH_COMMAND=ssh -o 'StrictHostKeyChecking no' -i ${KEYFILE} -l jenkins"]) {
+                        // Since checkmk_ci:df2be57e we don't have the notes available anymore in the checkout
+                        // however the werk commands tests heavily rely on them, so fetch them here.
+                        // The order of the operations is important, because we rely on FETCH_HEAD
+                        // being the checked out revision.
+                        // Fetching with --shallow-since requires an explicit git commit hash to
+                        // work properly.
+                        // We use the same commit limitation as the werk commands uses in stages.yml.
+                        sh("""
+                            git fetch \
+                                --no-tags \
+                                --shallow-since=\$(date --date='4 weeks ago' --iso=seconds) \
+                                origin \
+                                \$(cat .git/FETCH_HEAD | cut -f 1)
+                            git fetch origin 'refs/notes/*:refs/notes/*'
+                        """)
+                    }
+                }
+            }
+        }
+
+        smart_stage(
+            name: "Fetch git tags",
+            condition: args.GIT_FETCH_TAGS,
+        ) {
+            dir("${checkout_dir}") {
+                withCredentials([jenkins_gerrit_key]) {
+                    withEnv(["GIT_SSH_COMMAND=ssh -o 'StrictHostKeyChecking no' -i ${KEYFILE} -l jenkins"]) {
+                        // Since checkmk_ci:df2be57e we don't have the tags available anymore in the checkout
+                        // however the werk tests heavily rely on them, so fetch them here
+                        sh("git fetch origin 'refs/tags/*:refs/tags/*'")
+                    }
+                }
+            }
+        }
+
+        withCredentials(credentials) {
+            withEnv(args.ENV_VAR_LIST) {
+                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+                    dir(args.DIR) {
+                        // be very carefull here. Setting quantity to 0 or null, takes all available resources
+                        if (args.BAZEL_LOCKS_AMOUNT >= 1) {
+                            lock(
+                                label: 'bzl_lock_' + env.NODE_NAME.split("\\.")[0].split("-")[-1],
+                                quantity: args.BAZEL_LOCKS_AMOUNT,
+                                resource : null,
+                            ) {
+                                cmd_status = sh(script: args.COMMAND, returnStatus: true);
+                            }
+                        } else {
+                            cmd_status = sh(script: args.COMMAND, returnStatus: true);
+                        }
+                    }
+                    duration = groovy.time.TimeCategory.minus(new Date(), time_stage_started);
+                    desc_add_status_row(
+                        args.NAME,
+                        duration, cmd_status==0 ? "success" : "failure",
+                        "${args.RESULT_CHECK_FILE_PATTERN}"
+                    );
+
+                    println("Check results: ${args.RESULT_CHECK_TYPE}");
+                    if (args.RESULT_CHECK_TYPE) {
+                        issues = test_jenkins_helper.analyse_issues(
+                            args.RESULT_CHECK_TYPE,
+                            args.RESULT_CHECK_FILE_PATTERN,
+                            false
+                        );
+                    }
+
+                    /// make the stage fail if the command returned nonzero
+                    sh("exit ${cmd_status}");
+                }
+            }
+        }
+        return [cmd_status == 0, issues];
+    }
+}
+
 def desc_init() {
     // add new content to next line, but do not overwrite existing content
     currentBuild.description += "<br>";
@@ -13,12 +142,47 @@ def desc_add_line(TEXT) {
     currentBuild.description += "<p>${TEXT}</p>";
 }
 
+def desc_add_table_head() {
+    currentBuild.description += "<table>";
+}
+
 def desc_add_table_bottom() {
     currentBuild.description += "</table>";
 }
 
+def desc_add_table() {
+    desc_add_table_head();
+    desc_add_table_bottom();
+}
+
+def desc_rm_table_bottom() {
+    currentBuild.description -= "</table>";
+}
+
 def desc_add_table(items_list) {
     currentBuild.description += """<table><tr><th>${items_list.join("</th><th>")}</th></tr>""";
+}
+
+def desc_add_row(ITEM_1, ITEM_2, ITEM_3, ITEM_4) {
+    desc_rm_table_bottom();
+    currentBuild.description += """<tr>
+    <td>${ITEM_1}</td><td>${ITEM_2}</td><td>${ITEM_3}</td><td>${ITEM_4}</td>
+    </tr>""";
+    desc_add_table_bottom();
+}
+
+def desc_add_status_row(STAGE, DURATION, status, PATTERN) {
+    desc_rm_table_bottom();
+    if (PATTERN != '' && PATTERN != '--') {
+        PATTERN = "<a href=\"artifact/${PATTERN}\">${PATTERN}</a>";
+    }
+    currentBuild.description += """<tr>
+    <td>${STAGE}</td>
+    <td>${DURATION}</td>
+    <td style=\"color: ${['success': 'green', 'skipped': 'grey', 'failure': 'red'][status]};\">${status}</td>
+    <td>${PATTERN}</td>
+    </tr>""";
+    desc_add_table_bottom();
 }
 
 def desc_add_status_row_gerrit(Map args) {

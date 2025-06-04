af7c611ec86b5bddbe5c93db1fcf0cc3affe0940
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-06-04 08:35:35 +0200

    17929 adjust werk text and add fix
    Change-Id: I36e2b706b465758a571a07b0558c00f4b486b337

--- cmk/gui/visuals/_filters.py
+++ cmk/gui/visuals/_filters.py
@@ -262,7 +262,7 @@ def register_host_and_service_basic_filters(filter_registry: FilterRegistry) -> 
             info="host",
             autocompleter=AutocompleterConfig(ident="check_cmd"),
             query_filter=query_filters.TextQuery(
-                ident="host_check_command",
+                ident="host_check_command_exact",
                 op="=",
             ),
         )
@@ -289,7 +289,7 @@ def register_host_and_service_basic_filters(filter_registry: FilterRegistry) -> 
             info="service",
             autocompleter=AutocompleterConfig(ident="check_cmd"),
             query_filter=query_filters.TextQuery(
-                ident="check_command",
+                ident="check_command_exact",
                 op="=",
                 column="service_check_command",
             ),

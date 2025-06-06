ead9adb32bcc623c973dac94720a8a000144b0fc
Mathias Laurin <mathias.laurin@checkmk.com>
2025-06-06 22:37:55 +0200

    Revert "18161 otel-collector: Add autocomplete field in the UI for host name computation"
    This reverts commit 5c17d7cbb0613e74e7edcf4813d5fda26cdac9b1.
    
    Change-Id: Ifa3f7fa049196967ebebe7223a83d71b117f4976

--- tests/unit/cmk/gui/test_autocompleters.py
+++ tests/unit/cmk/gui/test_autocompleters.py
@@ -22,7 +22,6 @@ def test_builtin_autocompleters_registered() -> None:
         "monitored_hostname",
         "monitored_metrics",
         "monitored_service_description",
-        "otel_host_name_computation",
         "service_levels",
         "sites",
         "syslog_facilities",

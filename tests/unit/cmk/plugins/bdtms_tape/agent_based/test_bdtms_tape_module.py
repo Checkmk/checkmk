d75b3d843ecc2cd6de8903df458a492ce576d697
Jörg Herbel <joerg.herbel@checkmk.com>
2025-06-10 09:56:19 +0200

    Add unit tests for bdtms_tape_module
    Change-Id: I9bb5c86cbbd3fa5eb6f013b115eb22622a3df020

---
+++ tests/unit/cmk/plugins/bdtms_tape/agent_based/test_bdtms_tape_module.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.agent_based.v2 import Result, Service, State
+from cmk.plugins.bdtms_tape.agent_based.bdtms_tape_module import (
+    check_bdtms_tape_module,
+    inventory_bdtms_tape_module,
+    parse_bdtms_tape_module,
+)
+
+_SECTION = parse_bdtms_tape_module(
+    [
+        ["1", "OK", "OK", "OK"],
+        ["2", "OK", "OK", "OK"],
+        [
+            "3",
+            "BROKEN",  # probably not a realistic value, but sufficient for testing
+            "OK",
+            "OK",
+        ],
+    ]
+)
+
+
+def test_discover_bdtms_tape_module() -> None:
+    assert list(inventory_bdtms_tape_module(_SECTION)) == [
+        Service(item="1"),
+        Service(item="2"),
+        Service(item="3"),
+    ]
+
+
+def test_check_bdtms_tape_module_all_ok() -> None:
+    assert list(check_bdtms_tape_module("1", _SECTION)) == [
+        Result(state=State.OK, summary="Module: ok"),
+        Result(state=State.OK, summary="Board: ok"),
+        Result(state=State.OK, summary="Power supply: ok"),
+    ]
+
+
+def test_check_bdtms_tape_module_with_issues() -> None:
+    assert list(check_bdtms_tape_module("3", _SECTION)) == [
+        Result(state=State.CRIT, summary="Module: broken"),
+        Result(state=State.OK, summary="Board: ok"),
+        Result(state=State.OK, summary="Power supply: ok"),
+    ]
+
+
+def test_check_bdtms_tape_module_item_missing() -> None:
+    assert not list(check_bdtms_tape_module("missing", _SECTION))

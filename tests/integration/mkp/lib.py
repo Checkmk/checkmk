7426d9cd0007237960dde84a71245cbab8ca579f
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-06-12 12:44:47 +0200

    MKP integration test
    Test that disabling an MKP does not render the site UI unusable
    
    CMK-23624
    
    Change-Id: Ia8ff688f411dcf888c2427fe4a0cce55575e99a2

--- tests/integration/mkp/lib.py
+++ tests/integration/mkp/lib.py
@@ -2,7 +2,13 @@
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+from pathlib import Path
+
 from tests.testlib.site import Site
+
+
+def add_extension(site: Site, path: Path) -> None:
+    site.check_output(["mkp", "add", path.as_posix()])
 
 
 def enable_extension(site: Site, name: str) -> None:

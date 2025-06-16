ffbea7cf01381b417ac641e475366296edecc349
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-06-16 15:43:46 +0200

    Move jobs trigger by post submit chains into folders
    CMK-24036
    
    Change-Id: I37d31a90e2a5571f4f01c26093d0479708ea86db

--- buildscripts/scripts/trigger-post-submit-test-cascade-heavy.groovy
+++ buildscripts/scripts/trigger-post-submit-test-cascade-heavy.groovy
@@ -41,7 +41,7 @@ def main() {
                 smart_build(
                     // see global-defaults.yml, needs to run in minimal container
                     use_upstream_build: true,
-                    relative_job_name: "${branch_base_folder}/${job_name}",
+                    relative_job_name: "${branch_base_folder}/heavy/${job_name}",
                     build_params: [
                         CUSTOM_GIT_REF: effective_git_ref,
                     ],

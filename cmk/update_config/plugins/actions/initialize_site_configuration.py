ff0cdc4e5f2953026c5e077928e03d7594be5d05
Lars Michelsen <lm@checkmk.com>
2025-06-19 11:27:03 +0200

    Reapply "Create local site connection with sample config"
    This reverts commit 469d8a2366531f64d99f0020f0edf14c93ca2852.
    
    Now new: update config ensures that the sample config is created for
    sites which don't have a sites.mk file yet.
    
    Change-Id: I5c121336c2080f8d1055f4766b64a502fd7e3d19

---
+++ cmk/update_config/plugins/actions/initialize_site_configuration.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from logging import Logger
+from typing import override
+
+from cmk.gui.watolib.sample_config import ConfigGeneratorLocalSiteConnection
+from cmk.gui.watolib.sites import site_management_registry
+
+from cmk.update_config.registry import update_action_registry, UpdateAction
+
+
+class InitializeSiteConfiguration(UpdateAction):
+    @override
+    def __call__(self, logger: Logger) -> None:
+        if site_management_registry["site_management"].load_sites():
+            return
+
+        ConfigGeneratorLocalSiteConnection().generate()
+
+
+update_action_registry.register(
+    InitializeSiteConfiguration(
+        name="initialize_site_configuration",
+        title="Initialize site configuration",
+        # Needs to run before "Create precompiled host and folder files". Otherwise the sites
+        # attribute will be missing.
+        sort_index=2,
+    )
+)
---
+++ tests/unit/cmk/update_config/plugins/actions/test_initialize_site_config.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import logging
+from collections.abc import Iterator
+
+import pytest
+
+import cmk.utils.paths
+
+from cmk.gui.watolib.sites import site_management_registry
+
+from cmk.update_config.plugins.actions.initialize_site_configuration import (
+    InitializeSiteConfiguration,
+)
+
+
+@pytest.fixture
+def move_sites_mk_out_of_the_way() -> Iterator[None]:
+    sites_mk = cmk.utils.paths.default_config_dir / "multisite.d/sites.mk"
+    sites_mk_sav = cmk.utils.paths.default_config_dir / "multisite.d/sites.mk.sav"
+    if sites_mk.exists():
+        sites_mk.rename(sites_mk_sav)
+        yield
+        sites_mk_sav.rename(sites_mk)
+
+
+@pytest.mark.usefixtures("load_config", "move_sites_mk_out_of_the_way")
+def test_initialize_missing_file() -> None:
+    assert not (sites_mk := cmk.utils.paths.default_config_dir / "multisite.d/sites.mk").exists()
+
+    InitializeSiteConfiguration(
+        name="initialize_site_configuration",
+        title="Initialize site configuration",
+        sort_index=30,
+    )(logging.getLogger())
+
+    assert sites_mk.exists()
+    sites_mk.unlink()
+
+
+@pytest.mark.usefixtures("load_config", "move_sites_mk_out_of_the_way")
+def test_initialize_empty_config() -> None:
+    (
+        sites_mk := cmk.utils.paths.default_config_dir / "multisite.d/sites.mk"
+    ).write_text("""# Written by Checkmk store
+
+sites.update({})""")
+
+    InitializeSiteConfiguration(
+        name="initialize_site_configuration",
+        title="Initialize site configuration",
+        sort_index=30,
+    )(logging.getLogger())
+
+    site_mgmt = site_management_registry["site_management"]
+    assert "NO_SITE" in site_mgmt.load_sites()
+    sites_mk.unlink()
+
+
+@pytest.mark.usefixtures("load_config", "move_sites_mk_out_of_the_way")
+def test_initialize_missing_file_not_touch_existing_file() -> None:
+    (sites_mk := cmk.utils.paths.default_config_dir / "multisite.d/sites.mk").write_text(
+        "sites.update({'x': {}})"
+    )
+
+    InitializeSiteConfiguration(
+        name="initialize_site_configuration",
+        title="Initialize site configuration",
+        sort_index=30,
+    )(logging.getLogger())
+
+    assert sites_mk.read_text() == "sites.update({'x': {}})"
+    sites_mk.unlink()

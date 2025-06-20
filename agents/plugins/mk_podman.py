4d87c1f4c651a9fb8797fb96b9341430bdf31cd5
Luka Racic <luka.racic@checkmk.com>
2025-06-20 11:05:11 +0200

    mk_podman: Add section for Errors
    * Wrapped the requests_unixsocket import in a try-except block to handle
      potential ImportError exceptions.
    
    CMK-24067
    
    Change-Id: Ifadbfe17f0ab47ffb91a864ec1129dee0d3e99da

--- agents/plugins/mk_podman.py
+++ agents/plugins/mk_podman.py
@@ -6,116 +6,142 @@
 import json
 import sys
 from collections.abc import Mapping, Sequence
-
-import requests
-import requests_unixsocket  # type: ignore[import-untyped]
+from dataclasses import dataclass
+from typing import assert_never
 
 DEFAULT_SOCKET_PATH = "/run/podman/podman.sock"
 
 
-def write_section(name: str, json_content: str) -> None:
+@dataclass(frozen=True)
+class JSONSection:
+    name: str
+    content: str
+
+
+@dataclass(frozen=True)
+class Error:
+    label: str
+    message: str
+
+
+def write_section(section: JSONSection | Error) -> None:
+    match section:
+        case JSONSection():
+            _write_serialized_section(section.name, section.content)
+        case Error():
+            _write_serialized_section(
+                "errors", json.dumps({"endpoint": section.label, "message": section.message})
+            )
+        case _:
+            assert_never(section)
+
+
+def _write_serialized_section(name: str, json_content: str) -> None:
     sys.stdout.write(f"<<<podman_{name}:sep(0)>>>\n")
     sys.stdout.write(f"{json_content}\n")
     sys.stdout.flush()
 
 
-def write_piggyback_section(target_host: str, section_name: str, json_content: str) -> None:
+try:
+    import requests_unixsocket  # type: ignore[import-untyped]
+except ImportError as e:
+    write_section(
+        Error(
+            label="requests_unixsocket",
+            message=f"Failed to import requests_unixsocket: {e}. "
+            "Please install the requests-unixsocket package.",
+        )
+    )
+
+
+def write_piggyback_section(target_host: str, section: JSONSection | Error) -> None:
     sys.stdout.write(f"<<<<{target_host}>>>>\n")
-    write_section(section_name, json_content)
+    write_section(section)
     sys.stdout.write("<<<<>>>>\n")
     sys.stdout.flush()
 
 
-def _handle_exception(e: Exception, endpoint: str) -> dict:
-    if isinstance(e, requests.exceptions.HTTPError):
-        return {
-            "error": f"HTTP errorfor endpoint {endpoint}: {str(e)}",
-            "status_code": e.response.status_code if e.response else None,
-        }
-    return {"error": f"Error accessing endpoint {endpoint}: {str(e)}"}
+def _build_url_human_readable(socket_path: str, endpoint_uri: str) -> str:
+    return f"{socket_path}{endpoint_uri}"
+
+
+def _build_url_callable(socket_path: str, endpoint_uri: str) -> str:
+    return f"http+unix://{socket_path.replace('/', '%2F')}{endpoint_uri}"
 
 
 def query_containers(
-    session: requests_unixsocket.Session, base_url: str
+    session: requests_unixsocket.Session, socket_path: str
 ) -> Sequence[Mapping[str, object]]:
     endpoint = "/v4.0.0/libpod/containers/json"
     try:
-        response = session.get(base_url + endpoint)
+        response = session.get(_build_url_callable(socket_path, endpoint))
         response.raise_for_status()
         output = response.json()
     except Exception as e:
-        output = _handle_exception(e, endpoint)
-    write_section("containers", json.dumps(output))
+        write_section(Error(_build_url_human_readable(socket_path, endpoint), str(e)))
+    write_section(JSONSection("containers", json.dumps(output)))
     return output
 
 
-def query_disk_usage(session: requests_unixsocket.Session, base_url: str) -> None:
+def query_disk_usage(session: requests_unixsocket.Session, socket_path: str) -> None:
     endpoint = "/v4.0.0/libpod/system/df"
     try:
-        response = session.get(base_url + endpoint)
+        response = session.get(_build_url_callable(socket_path, endpoint))
         response.raise_for_status()
-        output = response.json()
     except Exception as e:
-        output = _handle_exception(e, endpoint)
-    write_section("disk_usage", json.dumps(output))
+        write_section(Error(_build_url_human_readable(socket_path, endpoint), str(e)))
+    write_section(JSONSection("disk_usage", json.dumps(response.json())))
 
 
-def query_engine(session: requests_unixsocket.Session, base_url: str) -> None:
+def query_engine(session: requests_unixsocket.Session, socket_path: str) -> None:
     endpoint = "/v4.0.0/libpod/info"
     try:
-        response = session.get(base_url + endpoint)
+        response = session.get(_build_url_callable(socket_path, endpoint))
         response.raise_for_status()
-        output = response.json()
     except Exception as e:
-        output = _handle_exception(e, endpoint)
-    write_section("engine", json.dumps(output))
+        write_section(Error(_build_url_human_readable(socket_path, endpoint), str(e)))
+    write_section(JSONSection("engine", json.dumps(response.json())))
 
 
-def query_pods(session: requests_unixsocket.Session, base_url: str) -> None:
+def query_pods(session: requests_unixsocket.Session, socket_path: str) -> None:
     endpoint = "/v4.0.0/libpod/pods/json"
     try:
-        response = session.get(base_url + endpoint)
+        response = session.get(_build_url_callable(socket_path, endpoint))
         response.raise_for_status()
-        output = response.json()
     except Exception as e:
-        output = _handle_exception(e, endpoint)
-    write_section("pods", json.dumps(output))
+        write_section(Error(_build_url_human_readable(socket_path, endpoint), str(e)))
+    write_section(JSONSection("pods", json.dumps(response.json())))
 
 
 def query_container_inspect(
     session: requests_unixsocket.Session,
-    base_url: str,
+    socket_path: str,
     container_id: str,
 ) -> None:
     endpoint = f"/v4.0.0/libpod/containers/{container_id}/json"
     try:
-        response = session.get(base_url + endpoint)
+        response = session.get(_build_url_callable(socket_path, endpoint))
         response.raise_for_status()
-        output = response.json()
+        section: JSONSection | Error = JSONSection("container_inspect", json.dumps(response.json()))
     except Exception as e:
-        output = _handle_exception(e, endpoint)
-    write_piggyback_section(
-        target_host=container_id, section_name="container_inspect", json_content=json.dumps(output)
-    )
+        section = Error(_build_url_human_readable(socket_path, endpoint), str(e))
+    write_piggyback_section(target_host=container_id, section=section)
 
 
 def query_container_stats(
-    session: requests_unixsocket.Session, base_url: str
+    session: requests_unixsocket.Session, socket_path: str
 ) -> Mapping[str, object]:
     endpoint = "/v4.0.0/libpod/containers/stats?stream=false&all=true"
     try:
-        response = session.get(base_url + endpoint)
+        response = session.get(_build_url_callable(socket_path, endpoint))
         response.raise_for_status()
-        output = response.json()
+        return response.json()
     except Exception as e:
-        output = _handle_exception(e, endpoint)
-    return output
+        write_section(Error(_build_url_human_readable(socket_path, endpoint), str(e)))
+    return {}
 
 
 def _container_id_to_stats(stats_data: Mapping[str, object]) -> Mapping[str, object]:
-    if error := stats_data.get("Error"):
-        return {"Error": error}
-
     if not isinstance(stats := stats_data.get("Stats", []), list):
         return {}
     return {stat["ContainerID"]: stat for stat in stats if "ContainerID" in stat}
@@ -123,26 +149,25 @@ def _container_id_to_stats(stats_data: Mapping[str, object]) -> Mapping[str, obj
 
 def main() -> None:
     with requests_unixsocket.Session() as session:
-        base_url = f"http+unix://{DEFAULT_SOCKET_PATH.replace('/', '%2F')}"
+        containers = query_containers(session, DEFAULT_SOCKET_PATH)
+        query_disk_usage(session, DEFAULT_SOCKET_PATH)
+        query_engine(session, DEFAULT_SOCKET_PATH)
+        query_pods(session, DEFAULT_SOCKET_PATH)
 
-        containers = query_containers(session, base_url)
-        query_disk_usage(session, base_url)
-        query_engine(session, base_url)
-        query_pods(session, base_url)
-
-        container_to_stats = _container_id_to_stats(query_container_stats(session, base_url))
+        container_to_stats = _container_id_to_stats(
+            query_container_stats(session, DEFAULT_SOCKET_PATH)
+        )
 
         for container in containers:
             if not (container_id := str(container.get("Id", ""))):
                 continue
 
-            query_container_inspect(session, base_url, container_id)
+            query_container_inspect(session, DEFAULT_SOCKET_PATH, container_id)
 
-            if stats := container_to_stats.get(container_id) or container_to_stats.get("Error"):
+            if stats := container_to_stats.get(container_id):
                 write_piggyback_section(
                     target_host=container_id,
-                    section_name="container_stats",
-                    json_content=json.dumps(stats),
+                    section=JSONSection("container_stats", json.dumps(stats)),
                 )
 
 

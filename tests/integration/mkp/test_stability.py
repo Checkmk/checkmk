6fd41e0ff3ac5332b6bc78735d5eda490fae1e58
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-06-20 16:15:13 +0200

    MKP integration test: Proper cleanup
    CMK-23624
    
    Change-Id: I81918a7b7043b9460dafb53a08c4d3350fae0c09

--- tests/integration/mkp/test_stability.py
+++ tests/integration/mkp/test_stability.py
@@ -14,7 +14,7 @@ from tests.testlib.web_session import CMKWebSession
 
 
 @skip_if_saas_edition(reason="Not available in SaaS")
-def test_site_aivalability_after_mkp_removal(site: Site, web: CMKWebSession) -> None:
+def test_site_availability_after_mkp_removal(site: Site, web: CMKWebSession) -> None:
     """
     Removing or disabling MKPs should not cause the entire UI to crash
 
@@ -35,6 +35,8 @@ def test_site_aivalability_after_mkp_removal(site: Site, web: CMKWebSession) -> 
     lib.add_extension(site, tmp_mkp_path)
     lib.enable_extension(site, mkp_name)
     lib.disable_extension(site, mkp_name)
+    lib.remove_extension(site, mkp_name)
+    tmp_mkp_path.unlink(missing_ok=True)
 
     web.get("")
     # No explicit assertion needed. The crash report in the response will be detected by the site

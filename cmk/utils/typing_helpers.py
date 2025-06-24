e13d5565f948d7b9ffb11c1e6c547eff58273d3a
Giordano Tomassorri <giordano.tomassorri@checkmk.com>
2025-06-24 17:24:46 +0200

    Introduce in_termination operational state
    This commit introduces the new in_termination license operational state.
    
    The actual handling will be performed in a later commit.
    
    SAASDEV-4342
    
    Change-Id: Iefe5da755086203da70783eac90cada7ec64927c

---
+++ cmk/utils/typing_helpers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import Never
+
+
+def assert_never_without_raising(value: Never) -> None:
+    """
+    Same as typing.assert_never but does not raise an exception.
+    """
+    pass

1130ace7308d2958046efdf216d40280edadfb9e
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-06-25 08:37:23 +0200

    Filter files to upload in groovy
    ... as the bash approach was incompatible with Jenkins bash due to a
    syntax error near unexpected token `(' although the command is correct.
    
    Fixup 4ed0ed3f04eb33422fd4f598fdd60aa06d279d4b
    
    CMK-24123
    
    Change-Id: I6bf69d1185c24e2e08ecbede384e49af95ae8331

--- buildscripts/scripts/build-cmk-deliverables.groovy
+++ buildscripts/scripts/build-cmk-deliverables.groovy
@@ -249,12 +249,20 @@ def main() {
         /// on our own..
         def files_to_upload = {
             dir("${deliverables_dir}") {
-                cmd_output("ls *.{deb,rpm,cma,tar.gz} !(${bazel_log_prefix}*).{csv,json} || true").split().toList();
+                cmd_output("ls *.{deb,rpm,cma,tar.gz,json,csv} || true").split().toList();
             }
         }();
         print("Found files to upload: ${files_to_upload}");
 
-        files_to_upload.each { filename ->
+        def filtered_files_to_upload = [];
+        files_to_upload.each { item ->
+            if (!item.startsWith(bazel_log_prefix)) {
+                filtered_files_to_upload += item;
+            }
+        }
+        print("Filtered files to upload: ${filtered_files_to_upload}")
+
+        filtered_files_to_upload.each { filename ->
             artifacts_helper.upload_via_rsync(
                 "${WORKSPACE}/deliverables",
                 "${cmk_version_rc_aware}",

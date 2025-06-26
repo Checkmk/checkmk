7dace5dddcb2afaded1502e624b8b75e0b802122
Alejandro Yunta <alejandro.yunta@checkmk.com>
2025-06-26 12:08:49 +0200

    Implement Welcome Footer
    CMK-24136
    
    Change-Id: I45cfe5b6efd91170b316b343b537ee37103156cf

--- packages/cmk-frontend-vue/src/welcome/WelcomeApp.vue
+++ packages/cmk-frontend-vue/src/welcome/WelcomeApp.vue
@@ -7,29 +7,43 @@ conditions defined in the file COPYING, which is part of this source code packag
 <script setup lang="ts">
 import usei18n from '@/lib/i18n'
 import WelcomeBanner from './components/WelcomeBanner.vue'
+import WelcomeFooter from './components/WelcomeFooter.vue'
 
 const { t } = usei18n('welcome-app')
 </script>
 
 <template>
-  <WelcomeBanner :completed-steps="1" :total-steps="5" />
+  <div class="welcome-app">
+    <WelcomeBanner :completed-steps="1" :total-steps="5" />
+    <p>
+      {{
+        t(
+          'placeholder',
+          'This is just a placeholder for the Welcome page. It will be used to guide you through the first steps of using Checkmk.'
+        )
+      }}
+    </p>
+    <p>
+      {{
+        t(
+          'change-start-url',
+          'In case you want to change the start URL to the main dashboard, you can do so under User > Edit profile > Start URL.'
+        )
+      }}
+    </p>
 
-  <p>
-    {{
-      t(
-        'placeholder',
-        'This is just a placeholder for the Welcome page. It will be used to guide you through the first steps of using Checkmk.'
-      )
-    }}
-  </p>
-  <p>
-    {{
-      t(
-        'change-start-url',
-        'In case you want to change the start URL to the main dashboard, you can do so under User > Edit profile > Start URL.'
-      )
-    }}
-  </p>
+    <WelcomeFooter class="welcome-app__footer" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.welcome-app {
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 20px);
+}
+
+.welcome-app__footer {
+  margin-top: auto;
+}
+</style>
---
+++ packages/cmk-frontend-vue/src/welcome/components/WelcomeFooter.vue
@@ -0,0 +1,47 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import usei18n from '@/lib/i18n'
+import CmkCheckbox from '@/components/CmkCheckbox.vue'
+import CmkBodyText from '@/components/typography/CmkBodyText.vue'
+
+const { t } = usei18n('welcome-footer')
+
+const showWelcomeOnStart = ref(true)
+</script>
+
+<template>
+  <section class="welcome-footer">
+    <CmkCheckbox
+      v-model="showWelcomeOnStart"
+      :label="t('show-on-start', 'Show welcome page on start (not implemented yet)')"
+    />
+    <CmkBodyText class="welcome-footer__hint">
+      {{
+        t(
+          'access-breadcrumbs',
+          'You can still access it later via Help > Learning Checkmk > Get started'
+        )
+      }}
+    </CmkBodyText>
+  </section>
+</template>
+
+<style scoped>
+.welcome-footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 12px;
+}
+
+.welcome-footer__hint {
+  color: var(--font-color-dimmed);
+  margin-top: 4px;
+}
+</style>

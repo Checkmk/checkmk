2382b1b354c5b2eb3745dd9515b2d9ff7720ce08
Giordano Tomassorri <giordano.tomassorri@checkmk.com>
2025-06-30 10:56:04 +0200

    Move protocol version function to a separate module
    This will simplify merge operations with the license server code.
    
    SAASDEV-4392
    
    Change-Id: I204e92227b08bc9c90633753c70004fe935da768

---
+++ cmk/utils/licensing/protocol_version.py
@@ -0,0 +1,14 @@
+from typing import Literal
+
+from cmk.ccc.version import Edition, edition
+
+from cmk.utils.paths import omd_root
+
+
+def get_licensing_protocol_version() -> Literal[
+    "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "2.0", "2.1", "3.0", "3.1"
+]:
+    """Returns the current licensing protocol version."""
+    if edition(omd_root) == Edition.CSE:
+        return "3.1"
+    return "3.1"

Checkmk/checkmk

commit 7969dddd6397e2982644040612d17a03e25eea97

Run pre package build steps in parallel
Decreasing the runtime of this phase from around 20min to 10min

CMK-24188

Change-Id: Id31ce8fec04a571cc6743cd9a1f5805ca34692df

buildscripts/scripts/build-cmk-distro-package.groovy
@@ -102,8 +102,10 @@ def main() {
                 versioning.configure_checkout_folder(edition, cmk_version);
             }
         }
+    }
 
-        inside_container_minimal(safe_branch_name: safe_branch_name) {
+    def stages = [
+        "Build BOM": {
             def build_instance = null;
             smart_stage(
                 name: "Build BOM",
@@ -140,22 +142,31 @@ def main() {
                     fingerprintArtifacts: true,
                 )
             }
+        },
+    ];
 
-            smart_stage(
-                name: 'Fetch agent binaries',
-                condition: !params.FAKE_WINDOWS_ARTIFACTS,
-                raiseOnError: true,
-            ) {
-                package_helper.provide_agent_binaries(version, edition, disable_cache, params.CIPARAM_BISECT_COMMENT);
-            }
-
-            smart_stage(name: 'Fake agent binaries', condition: params.FAKE_WINDOWS_ARTIFACTS) {
-                dir("${checkout_dir}") {
-                    sh("scripts/fake-artifacts");
-                }
+    if (!params.FAKE_WINDOWS_ARTIFACTS) {
+        stages += package_helper.provide_agent_binaries(
+            version,
+            edition,
+            disable_cache,
+            params.CIPARAM_BISECT_COMMENT,
+            safe_branch_name,
+            "tmp_artifacts",
+        );
+    } else {
+        smart_stage(name: 'Fake agent binaries') {
+            dir("${checkout_dir}") {
+                sh("scripts/fake-artifacts");
             }
         }
     }
+
+    inside_container_minimal(safe_branch_name: safe_branch_name) {
+        currentBuild.result = parallel(stages).values().every { it } ? "SUCCESS" : "FAILURE";
+    }
+
+    package_helper.cleanup_provided_agent_binaries("tmp_artifacts");
 
     stage("Build package") {
         lock(label: "bzl_lock_${env.NODE_NAME.split('\\.')[0].split('-')[-1]}", quantity: 1, resource : null) {

buildscripts/scripts/build-cmk-source_tgz.groovy
@@ -24,6 +24,7 @@ def main() {
     def disable_cache = DISABLE_CACHE;
     def safe_branch_name = versioning.safe_branch_name();
     def branch_version = versioning.get_branch_version(checkout_dir);
+    def branch_base_folder = package_helper.branch_base_folder(with_testing_prefix: false);
     def cmk_version_rc_aware = versioning.get_cmk_version(safe_branch_name, branch_version, version);
     def cmk_version = versioning.strip_rc_number_from_version(cmk_version_rc_aware);
 
@@ -53,40 +54,56 @@ def main() {
         }
     }
 
-    inside_container_minimal(safe_branch_name: safe_branch_name) {
-        stage("Build BOM") {
-            upstream_build(
-                relative_job_name: "${package_helper.branch_base_folder(with_testing_prefix=false)}/builders/build-cmk-bom",
-                build_params: [
-                    /// currently CUSTOM_GIT_REF must match, but in the future
-                    /// we should define dependency paths for build-cmk-distro-package
-                    CUSTOM_GIT_REF: effective_git_ref,
-                    VERSION: version,
-                    EDITION: edition,
-                    DISABLE_CACHE: disable_cache,
-                ],
-                build_params_no_check: [
-                    CIPARAM_OVERRIDE_BUILD_NODE: params.CIPARAM_OVERRIDE_BUILD_NODE,
-                    CIPARAM_CLEANUP_WORKSPACE: params.CIPARAM_CLEANUP_WORKSPACE,
-                    CIPARAM_BISECT_COMMENT: params.CIPARAM_BISECT_COMMENT,
-                ],
-                no_remove_others: true, // do not delete other files in the dest dir
-                no_venv: true,          // run ci-artifacts call without venv
-                omit_build_venv: true,  // do not check or build a venv first
-                dest: "/checkmk/",
-            );
-        }
+    def stages = [
+        "Build BOM": {
+            smart_stage(
+                name: "Build BOM",
+                raiseOnError: true,
+            ) {
+                smart_build(
+                    // see global-defaults.yml, needs to run in minimal container
+                    use_upstream_build: true,
+                    relative_job_name: "${branch_base_folder}/builders/build-cmk-bom",
+                    build_params: [
+                        CUSTOM_GIT_REF: effective_git_ref,
+                        VERSION: version,
+                        EDITION: edition,
+                        DISABLE_CACHE: disable_cache,
+                    ],
+                    build_params_no_check: [
+                        CIPARAM_OVERRIDE_BUILD_NODE: params.CIPARAM_OVERRIDE_BUILD_NODE,
+                        CIPARAM_CLEANUP_WORKSPACE: params.CIPARAM_CLEANUP_WORKSPACE,
+                        CIPARAM_BISECT_COMMENT: params.CIPARAM_BISECT_COMMENT,
+                    ],
+                    no_remove_others: true, // do not delete other files in the dest dir
+                    dest: "/checkmk/",
+                );
+            }
+        },
+    ];
 
-        smart_stage(name: 'Fetch agent binaries', condition: !params.FAKE_WINDOWS_ARTIFACTS) {
-            package_helper.provide_agent_binaries(version, edition, disable_cache, params.CIPARAM_BISECT_COMMENT);
-        }
-
-        smart_stage(name: 'Fake agent binaries', condition: params.FAKE_WINDOWS_ARTIFACTS) {
+    if (!params.FAKE_WINDOWS_ARTIFACTS) {
+        stages += package_helper.provide_agent_binaries(
+            version,
+            edition,
+            disable_cache,
+            params.CIPARAM_BISECT_COMMENT,
+            safe_branch_name,
+            "tmp_artifacts",
+        );
+    } else {
+        smart_stage(name: 'Fake agent binaries') {
             dir("${checkout_dir}") {
                 sh("scripts/fake-artifacts");
             }
         }
     }
+
+    inside_container_minimal(safe_branch_name: safe_branch_name) {
+        currentBuild.result = parallel(stages).values().every { it } ? "SUCCESS" : "FAILURE";
+    }
+
+    package_helper.cleanup_provided_agent_binaries("tmp_artifacts");
 
     inside_container(ulimit_nofile: 2048) {
         def source_package_name = {

buildscripts/scripts/utils/package_helper.groovy
@@ -10,6 +10,8 @@
 /// values which should not be here. If that gets on `master`, it should be gotten
 /// rid of as soon as possible
 
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
 /// Returns the Jenkins 'branch folder' of the currently running job, either with or without
 /// the 'Testing/..' prefix
 /// So "Testing/bla.blubb/checkmk/2.4.0/some_job" will result in
@@ -23,7 +25,7 @@ def branch_base_folder(with_testing_prefix) {
     return project_name_components[checkmk_index..checkmk_index + 1].join('/');
 }
 
-def provide_agent_binaries(version, edition, disable_cache, bisect_comment) {
+def provide_agent_binaries(version, edition, disable_cache, bisect_comment, safe_branch_name, artifacts_base_dir) {
     // This _should_ go to an externally maintained file (single point of truth), see
     // https://jira.lan.tribe29.com/browse/CMK-13857
     // and https://review.lan.tribe29.com/c/check_mk/+/67387
@@ -118,31 +120,53 @@ def provide_agent_binaries(version, edition, disable_cache, bisect_comment) {
         ],
     ];
 
-    def artifacts_base_dir = "tmp_artifacts";
+    def stages = upstream_job_details.collectEntries { job_name, details ->
+        [("${job_name}") : {
+            def run_condition = details.get("condition", true);
+            def build_instance = null;
 
-    upstream_job_details.collect { job_name, details ->
-        if ( ! details.get("condition", true) ) {
-            return;
-        }
-        upstream_build(
-            relative_job_name: details.relative_job_name,
-            build_params: [
-                DISABLE_CACHE: disable_cache,
-                VERSION: version,
-            ],
-            build_params_no_check: [
-                CIPARAM_BISECT_COMMENT: bisect_comment,
-            ],
-            dependency_paths: details.dependency_paths,
-            no_venv: true,          // run ci-artifacts call without venv
-            omit_build_venv: true,  // do not check or build a venv first
-            dest: "${artifacts_base_dir}/${job_name}",
-        );
-        dir("${checkout_dir}/${artifacts_base_dir}/${job_name}") {
-            sh(details.install_cmd);
-        }
+            if (! run_condition) {
+                Utils.markStageSkippedForConditional("${distro}");
+            }
+
+            smart_stage(
+                name: job_name,
+                condition: run_condition,
+                raiseOnError: false,
+            ) {
+                build_instance = smart_build(
+                    // see global-defaults.yml, needs to run in minimal container
+                    use_upstream_build: true,
+                    relative_job_name: details.relative_job_name,
+                    build_params: [
+                        VERSION: version,
+                        DISABLE_CACHE: disable_cache,
+                    ],
+                    build_params_no_check: [
+                        CIPARAM_BISECT_COMMENT: bisect_comment,
+                    ],
+                    dependency_paths: details.dependency_paths,
+                    dest: "${artifacts_base_dir}/${job_name}",
+                    no_remove_others: true, // do not delete other files in the dest dir
+                );
+            }
+
+            smart_stage(
+                name: "Move artifacts around",
+                condition: run_condition && build_instance,
+                raiseOnError: false,
+            ) {
+                dir("${checkout_dir}/${artifacts_base_dir}/${job_name}") {
+                    sh(details.install_cmd);
+                }
+            }
+        }]
     }
 
+    return stages;
+}
+
+def cleanup_provided_agent_binaries(artifacts_base_dir) {
     /// Cleanup
     sh("""
         # needed only because upstream_build() only downloads relative

commit 4d1e1d268e4fbd25056bd1e5244d0625b1551c3b

Do not remove the artifacts_base_dir in a parallel step
Fix for issue introduced with 7969dddd6397e2982644040612d17a03e25eea97

Before going parallel the BOM stage was the last stage and it was no
issue to delete the artifacts_base_dir. With the parallel stage
execution the artifacts_base_dir shall only be deleted after all stages
passed. The existing function cleanup_provided_agent_binaries is doing
this properly and at the correct point in time.

CMK-24188

Change-Id: I0d69f4b60e94125dca4ce118d6f25e95328a6b66

buildscripts/scripts/build-cmk-distro-package.groovy
@@ -143,7 +143,6 @@ def main() {
                     # needed only because upstream_build() only downloads relative
                     # to `base-dir` which has to be `checkout_dir`
                     cp ${checkout_dir}/${artifacts_base_dir}/omd/* ${checkout_dir}/omd
-                    rm -rf ${checkout_dir}/${artifacts_base_dir}
                 """);
             }
         },
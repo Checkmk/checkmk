a85b098d9dac54f5d10984fe2801a6cad34c28e9
Logan Connolly <logan.connolly@checkmk.com>
2025-07-04 11:39:54 +0200

    18233 gui(bi): fix bulk delete/move actions
    Using `RequireConfirmation` here is problematic as it uses the
    `preventDefault` function in JavaScript. Therefore, the action is
    completely ignored. Removing this drops the dialog pop-up for bulk move,
    but is consistent with how we perform bulk move on the Hosts page.
    
    SUP-23039
    
    Co-authored-by: Benjamin Knapp <benjamin.knapp@checkmk.com>
    Change-Id: I8d724d59b2ae66dd436446aec94c3662115a1c36

--- cmk/gui/bi/_config.py
+++ cmk/gui/bi/_config.py
@@ -26,7 +26,6 @@ from cmk.gui.groups import GroupName
 from cmk.gui.htmllib.foldable_container import foldable_container
 from cmk.gui.htmllib.generator import HTMLWriter
 from cmk.gui.htmllib.html import html
-from cmk.gui.htmllib.type_defs import RequireConfirmation
 from cmk.gui.http import request
 from cmk.gui.i18n import _, _l, ungettext
 from cmk.gui.logged_in import user
@@ -2209,9 +2208,6 @@ class BIModeAggregations(ABCBIMode):
         with html.form_context(
             "bulk_action_form",
             method="POST",
-            require_confirmation=RequireConfirmation(
-                html=_("Do you really want to move the selected aggregations?")
-            ),
         ):
             self._render_aggregations()
             html.hidden_field("selection_id", SelectionId.from_request(request))

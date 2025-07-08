265adfd00d7499876c411c2dc85cd2c65eb5c8b4
Jörg Herbel <joerg.herbel@checkmk.com>
2025-07-08 14:13:50 +0200

    Add unit test for `aix_hacmp_nodes` plugin
    SUP-24524
    SUP-24545
    CMK-24360
    
    Change-Id: I2d97af18615a65135eca7021d40cda989ba3c6d0

---
+++ tests/unit/cmk/base/legacy_checks/test_aix_hacmp_nodes.py
@@ -0,0 +1,320 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from collections.abc import Sequence
+
+import pytest
+
+from cmk.base.legacy_checks.aix_hacmp_nodes import (
+    check_aix_hacmp_nodes,
+    inventory_aix_hacmp_nodes,
+    parse_aix_hacmp_nodes,
+)
+
+from cmk.agent_based.v1.type_defs import StringTable
+
+_STRING_TABLE_1 = [
+    ["pasv0450"],
+    ["pasv0449"],
+    ["NODE", "pasv1112:"],
+    ["Interfaces", "to", "network", "TEST_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasv0449,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.4",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0159,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.5",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0158,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.9",
+    ],
+    ["NODE", "pasv0449:"],
+    ["Interfaces", "to", "network", "prod_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasv0449,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.4",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0159,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.5",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0158,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.9",
+    ],
+    ["Interfaces", "to", "network", "TEST_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasv0449,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.4",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0159,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.5",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0158,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.9",
+    ],
+    ["NODE", "pasv1111:"],
+    ["Interfaces", "to", "network", "TEST_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasv0449,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.4",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0159,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.5",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0158,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.9",
+    ],
+    ["NODE", "pasv0450:"],
+    ["Interfaces", "to", "network", "prod_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasv0450,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.15",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0159,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.5",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "pasc0158,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.9",
+    ],
+]
+
+_STRING_TABLE_2 = [
+    ["sasv0121"],
+    ["sasv0122"],
+    ["NODE", "sasv0121:"],
+    ["Interfaces", "to", "network", "prod_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "sasv0121,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.10",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "sasc0016,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.13",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "sasc0015,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.12",
+    ],
+    ["NODE", "sasv0122:"],
+    ["Interfaces", "to", "network", "prod_net"],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "sasv0122,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.11",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "sasc0016,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.13",
+    ],
+    [
+        "Communication",
+        "Interface:",
+        "Name",
+        "sasc0015,",
+        "Attribute",
+        "public,",
+        "IP",
+        "address",
+        "1.2.3.12",
+    ],
+]
+
+
+@pytest.mark.parametrize(
+    ("string_table", "expected_result"),
+    [
+        (_STRING_TABLE_1, [("pasv0450", None), ("pasv0449", None)]),
+        (_STRING_TABLE_2, [("sasv0121", None), ("sasv0122", None)]),
+    ],
+)
+def test_inventory_aix_hacmp_nodes(
+    string_table: StringTable,
+    expected_result: Sequence[tuple[str, None]],
+) -> None:
+    assert inventory_aix_hacmp_nodes(parse_aix_hacmp_nodes(string_table)) == expected_result
+
+
+@pytest.mark.parametrize(
+    ("string_table", "item", "expected_result"),
+    [
+        (
+            _STRING_TABLE_1,
+            "pasv0450",
+            (
+                0,
+                "Network: prod_net, interface: pasv0450, attribute: public, IP: 1.2.3.15, interface: pasc0159, attribute: public, IP: 1.2.3.5, interface: pasc0158, attribute: public, IP: 1.2.3.9",
+            ),
+        ),
+        (
+            _STRING_TABLE_2,
+            "sasv0122",
+            (
+                0,
+                "Network: prod_net, interface: sasv0122, attribute: public, IP: 1.2.3.11, interface: sasc0016, attribute: public, IP: 1.2.3.13, interface: sasc0015, attribute: public, IP: 1.2.3.12",
+            ),
+        ),
+    ],
+)
+def test_check_aix_hacmp_nodes(
+    string_table: StringTable,
+    item: str,
+    expected_result: tuple[int, str],
+) -> None:
+    assert (
+        check_aix_hacmp_nodes(
+            item,
+            None,
+            parse_aix_hacmp_nodes(string_table),
+        )
+        == expected_result
+    )

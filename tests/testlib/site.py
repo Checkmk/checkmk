c2a4b9e4d78f8dc7a13279f1d22af83311d1daec
Dhananjay Shah <dhananjay.shah@checkmk.com>
2025-07-10 11:53:56 +0200

    testlib: update 'get_omd_service_names_and_statuses'.
    `tests.testlib.site::Site.get_omd_service_names_and_statuses`
    'seemed' like an undecorated static-method.
    
    From its usage, its rather a normal class-method.
    
    Change-Id: I95cdc6ea7dbc47455177a655c1a337f878ed0ff3
    (cherry picked from commit 92247fe9d3fdbbf7e110dd939c95da7d32368799)

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -1210,15 +1210,15 @@ class Site:
     def is_running(self) -> bool:
         return self.omd("status").returncode == 0
 
-    def get_omd_service_names_and_statuses(site: Site, service: str = "") -> dict[str, int]:
+    def get_omd_service_names_and_statuses(self, service: str = "") -> dict[str, int]:
         """
         Return all service names and their statuses for the given site.
         """
         # Get all service names from 'omd status --bare <service>' command.
-        cmd = ["omd", "status", "--bare"]
+        cmd = ["status", "--bare"]
         if service:
             cmd.append(service)
-        p = site.run(cmd, check=False)
+        p = self.omd(*cmd, check=False)
         services = {}
         for line in p.stdout.strip().splitlines():
             parts = line.split()
@@ -1230,7 +1230,10 @@ class Site:
                         f"Expected status code to be an integer for service {parts[0]}"
                     ) from e
         if not services:
-            raise RuntimeError("No services found in 'omd status --bare' output:\n" + p.stdout)
+            raise RuntimeError(
+                f"No status found for '{' '.join(cmd)}'!\nSTDOUT:\n{p.stdout}\n",
+                f"STDERR:\n{p.stderr}\n",
+            )
         return services
 
     def is_stopped(self) -> bool:

28d426d79584f6313876e56b72b93d6b6cb5b2fb
Logan Connolly <logan.connolly@checkmk.com>
2025-07-14 21:20:02 +0200

    gui(utils): use generic type syntax
    With the new generic type annotations in Python 3.12, there is no need
    to create global type variables. This change keeps these variables tied
    to this function's scope. Note: the `**P` denotes a `ParamSpec`.
    
    This is now being adopted in other parts of the the codebase:
    
        packages/cmk-trace/cmk/trace/_tracer.py
    
    Change-Id: I202e2e0b53f80eabf9e9fc5fcf29d81a02b94299

--- cmk/gui/utils/request_context.py
+++ cmk/gui/utils/request_context.py
@@ -2,18 +2,13 @@
 # Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-import typing
+from collections.abc import Callable
 from functools import update_wrapper
 
 from flask.globals import _cv_request
 
-RV = typing.TypeVar("RV")
-P = typing.ParamSpec("P")
 
-FuncSpec = typing.Callable[P, RV]
-
-
-def copy_request_context(func: FuncSpec) -> FuncSpec:
+def copy_request_context[**P, R](func: Callable[P, R]) -> Callable[P, R]:
     # RATIONALE
     #
     # This code contains stuff copied over from `copy_current_request_context`, which doesn't
@@ -28,7 +23,7 @@ def copy_request_context(func: FuncSpec) -> FuncSpec:
 
     copied = ctx.copy()
 
-    def wrapper(*args, **kw):
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         # NOTE
         #
         # This copied context only exists during the run of the function wrapper and gets discarded
@@ -39,6 +34,6 @@ def copy_request_context(func: FuncSpec) -> FuncSpec:
         # `RequestContext` and the code would crash. This is slower than the other solution, but
         # whenever we run in a ThreadPool, there is no other way.
         with copied.copy():
-            return copied.app.ensure_sync(func)(*args, **kw)
+            return copied.app.ensure_sync(func)(*args, **kwargs)
 
     return update_wrapper(wrapper, func)

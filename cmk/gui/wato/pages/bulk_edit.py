063158dcf3dc2229a0efea4e7dc4e9ee5154f263
Sven Panne <sven.panne@checkmk.com>
2025-07-18 09:40:24 +0200

    Removed unused import
    Change-Id: Ic63a5693e53e577f46b6351140e8ff7e4da70d77

--- cmk/gui/wato/pages/bulk_edit.py
+++ cmk/gui/wato/pages/bulk_edit.py
@@ -36,7 +36,6 @@ from cmk.gui.watolib.hosts_and_folders import (
     SearchFolder,
 )
 from cmk.gui.watolib.mode import ModeRegistry, redirect, WatoMode
-from cmk.utils.tags import TagGroup
 
 from ._bulk_actions import get_hostnames_from_checkboxes, get_hosts_from_checkboxes
 from ._host_attributes import configure_attributes

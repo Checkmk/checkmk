241b20fb51b489c7eb8e3abef2eeb1978a761d61
Alejandro Yunta <alejandro.yunta@checkmk.com>
2025-07-21 17:09:26 +0200

    Adapt accordion extended background to welcome page Figma design
    Change-Id: Icbdd6d5f021f1424a139a6ce62069c84ca397be0

--- packages/cmk-frontend-vue/src/components/CmkAccordion/CmkAccordionItem.vue
+++ packages/cmk-frontend-vue/src/components/CmkAccordion/CmkAccordionItem.vue
@@ -49,7 +49,6 @@ const { headerAs = 'h3', value = '', disabled = false } = defineProps<CmkAccordi
 
   &[data-state='open'] {
     border: 1px solid var(--color-digital-green-50);
-    background: var(--ux-theme-5);
   }
 
   &:last-of-type {

fbc244596f9bcadfacb41ec19f3b59b103bad94b
Luka Racic <luka.racic@checkmk.com>
2025-07-22 12:02:47 +0200

    juniper_alarm: Introduce unit tests
    SUP-24757
    
    Change-Id: Ifcd1dabc50e82f35fba0bb2876ac8fb0f6b276bc

---
+++ tests/unit/cmk/base/legacy_checks/test_juniper_alarm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Copyright (C) 2021 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+import pytest
+
+from cmk.base.legacy_checks.juniper_alarm import check_juniper_alarm, inventory_juniper_alarm
+
+from cmk.agent_based.legacy.v0_unstable import LegacyCheckResult
+from cmk.agent_based.v2 import StringTable
+
+STRING_TABLE_UNKNOWN = [["7"], ["2"], ["2"]]
+STRING_TABLE_OK = [["2"], ["2"], ["2"]]
+STRING_TABLE_CRIT = [["4"], ["2"], ["2"]]
+
+
+def test_inventory_juniper_alarm() -> None:
+    assert list(inventory_juniper_alarm(STRING_TABLE_UNKNOWN)) == [(None, None)]
+
+
+@pytest.mark.parametrize(
+    "info, expected",
+    [
+        pytest.param(
+            STRING_TABLE_UNKNOWN,
+            [3, "Status: unhandled alarm type '7'"],
+            id="Unknown alarm type",
+        ),
+        pytest.param(
+            STRING_TABLE_OK,
+            [0, "Status: OK, good, normally working"],
+            id="OK status",
+        ),
+        pytest.param(
+            STRING_TABLE_CRIT,
+            [2, "Status: alert, failed, not working (major)"],
+            id="Critical status",
+        ),
+    ],
+)
+def test_check_juniper_alarm(info: StringTable, expected: LegacyCheckResult) -> None:
+    assert list(check_juniper_alarm("", {}, info)) == list(expected)

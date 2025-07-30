3df77672f99d490087a3ca82eea28fbba249b12e
Ronny Bruska <ronny.bruska@checkmk.com>
2025-07-30 11:27:25 +0200

    Remove unnecessary help text
    SUP-24830
    
    Change-Id: Iefcdca7c822bfac836bb306aecd5627171e02af9

--- cmk/gui/views/command/commands.py
+++ cmk/gui/views/command/commands.py
@@ -1802,12 +1802,6 @@ class CommandScheduleDowntimes(Command):
                     Checkbox(
                         title=_("Only for hosts: Set child hosts in downtime"),
                         label=_("Include indirectly connected hosts (recursively)"),
-                        help=_(
-                            "Either verify the server certificate using the "
-                            "site local CA or accept any certificate offered by "
-                            "the server. It is highly recommended to leave this "
-                            "enabled."
-                        ),
                     ),
                 ),
             ],

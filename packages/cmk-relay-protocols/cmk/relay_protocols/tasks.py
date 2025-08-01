05844a2e7e2c9239c33f80cdb84774a2cc868a35
Jose Naranjo <jose.naranjo@checkmk.com>
2025-08-01 13:31:38 +0200

    chore: adjust relay protocol to manage requests and responses
    The fetcher itself is able to do multiple operations depending on the payload, so for the moment we just need a task to indicate the Agent Receiver and the Relay that this task is to be proxied to the fetcher, and probably another task that will be consumed by the Relay to configure it.
    
    Regarding the payload, all those parameters like "mode", "fetcher" and its internal fields or "snmp_plugin_store" are already given from the checkmk internals so we just need to pass those parameters. They are consumed after encoding a json from a string, so we just need a payload of type string.
    
    The RELAY_CONFIG task can be also serialized into a json, so a string payload should work for any task.
    
    Regarding the response payload, fetch-ad-hoc returns a json serialized. So I would just go with a string in that field.
    
    Change-Id: I4945a652bdedf14bd2abed3705f4389ee4282186

--- packages/cmk-relay-protocols/cmk/relay_protocols/tasks.py
+++ packages/cmk-relay-protocols/cmk/relay_protocols/tasks.py
@@ -2,22 +2,19 @@ from datetime import datetime
 from enum import StrEnum
 from uuid import UUID
 from typing import Any, Dict, Optional
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class TaskType(StrEnum):
-    SNMP_DISCOVERY = "SNMP_DISCOVERY"
-    CONFIG = "CONFIG"
-    NETWORK_CHECK = "NETWORK_CHECK"
-    HOST_CHECK = "HOST_CHECK"
-    SERVICE_CHECK_FORCE = "SERVICE_CHECK_FORCE"
+    RELAY_CONFIG = "RELAY_CONFIG"
+    FETCH_AD_HOC = "FETCH_AD_HOC"
 
 
 class TaskRequest(BaseModel):
     model_config = ConfigDict(frozen=True)
     id: UUID
     type: TaskType
-    payload: Optional[Dict[str, Any]] = None
+    payload: str
     version: int = 1
 
 
@@ -36,6 +33,11 @@ class TaskResponse(TaskRequest):
     model_config = ConfigDict(frozen=True)
     status: TaskStatus
     result_type: ResultType
-    result_payload: Optional[Dict[str, Any]] = None
+    result_payload: str
     creation_timestamp: datetime
     update_timestamp: datetime
+
+
+# TODO: In this package we should define a RELAY_CONFIG payload that would be used from the relay and the agent receiver to configure the relay
+# class RelayConfigPayload(BaseModel):
+#     number_of_fetchers: int = Field(..., description="Number of fetchers in the relay")

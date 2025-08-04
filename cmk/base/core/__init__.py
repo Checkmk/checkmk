9edcdef90db0e763753e05fca204aa57b161109f
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-08-04 13:14:34 +0200

    make cmk.base.core a module
    Change-Id: Iefc8d5676b8b6c15ff9787f813532d8b3ea37d77

---
+++ cmk/base/core/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+
+from ._control import activation_lock as activation_lock
+from ._control import CoreAction as CoreAction
+from ._control import do_core_action as do_core_action
+from ._control import do_reload as do_reload
+from ._control import do_restart as do_restart

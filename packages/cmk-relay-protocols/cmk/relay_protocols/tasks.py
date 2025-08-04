6201e2f6b12d36fcb0804bef83750b5d58fc6fc1
Sven Panne <sven.panne@checkmk.com>
2025-08-04 15:18:27 +0200

    Fixed BUILD file. Added missing headers. Fixed findings.
    Change-Id: I4192cc31f589aec3ef1f86ada7f04d051c45a0c1

--- packages/cmk-relay-protocols/cmk/relay_protocols/tasks.py
+++ packages/cmk-relay-protocols/cmk/relay_protocols/tasks.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 from datetime import datetime
 from enum import StrEnum
 from uuid import UUID
-from typing import Any, Dict, Optional
-from pydantic import BaseModel, ConfigDict, Field
+
+from pydantic import BaseModel, ConfigDict
 
 
 class TaskType(StrEnum):

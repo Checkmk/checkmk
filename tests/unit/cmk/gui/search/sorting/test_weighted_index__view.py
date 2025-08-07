50a09b964365dbfd11e746e4c06aa8c57087619b
Logan Connolly <logan.connolly@checkmk.com>
2025-08-07 17:16:00 +0200

    test(search): use consistent capitalization
    Only the first word for a word should be capitalized. This is consistent
    with how the search results are returned from the search engine. Let's
    make the tests match it for consistency.
    
    Change-Id: I699dc05b748cb2dc65a0fe811a3a3cbd87588472

--- tests/unit/cmk/gui/search/sorting/test_weighted_index__view.py
+++ tests/unit/cmk/gui/search/sorting/test_weighted_index__view.py
@@ -16,16 +16,16 @@ I = functools.partial(UnifiedSearchResultItem, url="", provider="setup")
 def get_results_alphabetically() -> list[UnifiedSearchResultItem]:
     return [
         I(title="Certificate overview", topic="Setup"),
-        I(title="Cisco Meraki Organisation Licenses Overview", topic="Enforced Services"),
-        I(title="Cisco Meraki Organisation Licenses Overview", topic="Service Monitoring Rules"),
-        I(title="Couchbase Node: Size of couch views", topic="Enforced Services"),
-        I(title="Couchbase Node: Size of couch views", topic="Service Monitoring Rules"),
-        I(title="Couchbase Node: Size of spacial views", topic="Enforced Services"),
-        I(title="Couchbase Node: Size of spacial views", topic="Service Monitoring Rules"),
-        I(title="Hide hosttags in Setup folder view", topic="Global Settings"),
-        I(title="Limit the number of rows in View tables", topic="Global Settings"),
-        I(title="Sounds in Views", topic="Global Settings"),
-        I(title="Threshold for slow views", topic="Global Settings"),
+        I(title="Cisco Meraki Organisation Licenses Overview", topic="Enforced services"),
+        I(title="Cisco Meraki Organisation Licenses Overview", topic="Service monitoring rules"),
+        I(title="Couchbase Node: Size of couch views", topic="Enforced services"),
+        I(title="Couchbase Node: Size of couch views", topic="Service monitoring rules"),
+        I(title="Couchbase Node: Size of spacial views", topic="Enforced services"),
+        I(title="Couchbase Node: Size of spacial views", topic="Service monitoring rules"),
+        I(title="Hide hosttags in Setup folder view", topic="Global settings"),
+        I(title="Limit the number of rows in View tables", topic="Global settings"),
+        I(title="Sounds in Views", topic="Global settings"),
+        I(title="Threshold for slow views", topic="Global settings"),
         I(title="Views", topic="Visualization", provider="customize"),
     ]
 
@@ -38,17 +38,17 @@ def test_weighted_index_sorting_with_view_query() -> None:
     value = [(result.title, result.topic) for result in results]
     expected = [
         ("Views", "Visualization"),  # customize
-        ("Couchbase Node: Size of couch views", "Service Monitoring Rules"),
-        ("Couchbase Node: Size of spacial views", "Service Monitoring Rules"),
-        ("Hide hosttags in Setup folder view", "Global Settings"),
-        ("Limit the number of rows in View tables", "Global Settings"),
-        ("Sounds in Views", "Global Settings"),
-        ("Threshold for slow views", "Global Settings"),
-        ("Couchbase Node: Size of couch views", "Enforced Services"),
-        ("Couchbase Node: Size of spacial views", "Enforced Services"),
+        ("Couchbase Node: Size of couch views", "Service monitoring rules"),
+        ("Couchbase Node: Size of spacial views", "Service monitoring rules"),
+        ("Hide hosttags in Setup folder view", "Global settings"),
+        ("Limit the number of rows in View tables", "Global settings"),
+        ("Sounds in Views", "Global settings"),
+        ("Threshold for slow views", "Global settings"),
+        ("Couchbase Node: Size of couch views", "Enforced services"),
+        ("Couchbase Node: Size of spacial views", "Enforced services"),
         ("Certificate overview", "Setup"),
-        ("Cisco Meraki Organisation Licenses Overview", "Service Monitoring Rules"),
-        ("Cisco Meraki Organisation Licenses Overview", "Enforced Services"),
+        ("Cisco Meraki Organisation Licenses Overview", "Service monitoring rules"),
+        ("Cisco Meraki Organisation Licenses Overview", "Enforced services"),
     ]
 
     assert value == expected

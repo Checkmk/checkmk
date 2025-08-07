fc7f0aeb3c65570e609afd3aeeef5a66ffe0d9b8
Logan Connolly <logan.connolly@checkmk.com>
2025-08-07 16:09:17 +0200

    search: improve weighted index search
    The weighted-index sorting is now based on three tiers:
    
      1. Match rank: how closely the query relates to a result item.
      2. Topic rank: which result topics should be prioritized.
      3. Alphabetical: sort by title in ascending order.
    
    Whenever, there is a tie-break in a given tier, the next tier will be
    invoked to resolve the order.
    
    I was unable to remove the xfails for the curated test cases because the
    algorithm is currently unable to handle different forms of a word.
    
    For example, the query "organize" should also match "organizes" and
    "organizing", or "democracy" to its derivations: "democratic" and
    "democratization". In traditional NLP, this was done with lemmatization
    [1], and recently, vector databases [2] have proven to be an even more
    robust solution.
    
    However, to support this in Checkmk, it would require introducing new
    dependencies, multi-language support, and for the latter, architectural
    changes. So for now, this is out-of-scope.
    
    CMK-25121
    
    Change-Id: I560b3f2c70bd2f16daf5cf237bdf7f07137945bd
    
    [1] https://en.wikipedia.org/wiki/Lemmatization
    [2] https://en.wikipedia.org/wiki/Vector_database
    
    Change-Id: I739648cd78e7c2b7d243a6f8659d2acec06e4969

--- cmk/gui/search/sorting.py
+++ cmk/gui/search/sorting.py
@@ -3,6 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import enum
 from collections.abc import Callable
 
 from .type_defs import SortType, UnifiedSearchResultItem
@@ -28,22 +29,69 @@ def _get_alphabetical_sorter(items: list[UnifiedSearchResultItem]) -> None:
     items.sort(key=lambda item: item.title)
 
 
+# NOTE: in the future, we want to support matching on different forms of a word.
+#
+# For example, the query "organize" should also match "organizes" and "organizing", or
+# "democracy" to its derivations: "democratic" and "democratization". In traditional NLP,
+# this was done with lemmatization, and recently, vector databases have proven to be an
+# even more robust solution.
+#
+# However, to support this in Checkmk, it would require introducing new dependencies,
+# multi-language support, and for the latter, architectural changes. So for now, this
+# is out-of-scope.
 def _get_weighted_index_sorter(query: str) -> Sorter:
-    def algorithm(item: UnifiedSearchResultItem) -> tuple[int, str]:
-        weighting = 5
-        if (title_idx := item.title.lower().find(query)) >= 0:
-            weighting = 1
-            if len(item.title) == len(query):
-                weighting = 0
+    query_ = query.lower()
+    topic_ranking_map = {topic: rank for rank, topic in enumerate(_TOPIC_RANKING)}
+    unranked_topic = max(topic_ranking_map.values()) + 1
 
-            if title_idx > 0:
-                weighting = 2
+    def algorithm(item: UnifiedSearchResultItem) -> tuple[_MatchRank, int, str]:
+        title_ = item.title.lower()
+        topic_ = item.topic.lower()
+
+        if query_ == title_:
+            match_rank = _MatchRank.EXACT_TITLE
+        elif f"({query_})" in title_:
+            match_rank = _MatchRank.EXACT_TITLE_IN_PARENTHESES
+        elif title_.startswith(query_):
+            match_rank = _MatchRank.TITLE_STARTS_WITH_QUERY
+        elif is_deprecated_result_item(title_, topic_):
+            match_rank = _MatchRank.DEPRECATED_RESULT_ITEM
         else:
-            title_idx = len(item.title)
+            match_rank = _MatchRank.DEFAULT_RANK
 
-        return weighting * title_idx, item.title
+        topic_rank = topic_ranking_map.get(topic_, unranked_topic)
+
+        return match_rank, topic_rank, item.title
 
     def sorter(items: list[UnifiedSearchResultItem]) -> None:
         items.sort(key=algorithm)
 
     return sorter
+
+
+# TODO: move this out of the code; it 't belongs somewhere where it's easy to adjust.
+_TOPIC_RANKING = (
+    "setup",
+    "monitor",
+    "host monitoring rules",
+    "notification parameter",
+    "service monitoring rules",
+    "global settings",
+    "vm, cloud, container",
+    "enforced services",
+)
+
+
+class _MatchRank(enum.IntEnum):
+    EXACT_TITLE = 0
+    EXACT_TITLE_IN_PARENTHESES = 1
+    TITLE_STARTS_WITH_QUERY = 2
+    DEFAULT_RANK = 3
+    DEPRECATED_RESULT_ITEM = 4
+
+
+# TODO: once metadata is factored out of code, introduce a "deprecated" attribute to result item.
+def is_deprecated_result_item(title: str, topic: str) -> bool:
+    if title.startswith("push notifications") and topic == "service monitoring rules":
+        return True
+    return False

4fbd44d153814201662b1af81736fb0af265c345
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-08-07 13:49:46 +0200

    inline DemoErrorBoundary.vue into test file
    this duplicates a bit of code, but on the other hand we don't want to
    import from the demo...
    
    Change-Id: I8ec62d6d404c0311c679880406a217b8a5737c49

--- packages/cmk-frontend-vue/tests/components/ErrorBoundary.test.ts
+++ packages/cmk-frontend-vue/tests/components/ErrorBoundary.test.ts
@@ -5,12 +5,56 @@
  */
 
 import { fireEvent, render, screen } from '@testing-library/vue'
-import DemoErrorBoundary from '@/components/_demo/DemoErrorBoundary.vue'
+import { defineComponent } from 'vue'
+import { useErrorBoundary } from '@/components/useErrorBoundary'
+import { CmkError } from '@/lib/error.ts'
 
 test('ErrorBoundary shows full stack', async () => {
-  render(DemoErrorBoundary, {
-    props: { screenshotMode: false }
+  class DemoError<T extends Error> extends CmkError<T> {
+    override name = 'DemoError'
+    override getContext(): string {
+      return 'DemoErrorContext'
+    }
+  }
+
+  const testComponent = defineComponent({
+    components: {},
+    setup() {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      const { ErrorBoundary } = useErrorBoundary()
+      function throwCmkError() {
+        try {
+          try {
+            throw new Error('something happened in code we can not control')
+          } catch (error: unknown) {
+            throw new DemoError('internal error handler, but keeps bubbeling', error as Error)
+          }
+        } catch (error: unknown) {
+          throw new CmkError('this is a cmk error', error as Error)
+        }
+      }
+
+      function throwError(message: string) {
+        throw new Error(message)
+      }
+      return {
+        ErrorBoundary,
+        throwError,
+        throwCmkError
+      }
+    },
+    template: `
+      <div>
+        <component :is=ErrorBoundary>
+          <button @click="throwError('this is a test error')">throw new Error()</button>
+          <button @click="throwCmkError()">throw new CmkError()</button>
+        </component>
+      </div>
+      <button @click="throwError('another error')">throw new Error() outside error boundary</button>
+    `
   })
+
+  render(testComponent)
 
   const button = screen.getByRole<HTMLButtonElement>('button', { name: 'throw new CmkError()' })
   // we just tested that CmkErrorBoundary renders the content passed in the <slot>

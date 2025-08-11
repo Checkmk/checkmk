2e28214345b88a6418074c6db1ef12c627cf02b9
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-08-11 13:57:02 +0200

    Add dummy make target for QA performance test
    CMK-25103
    
    Change-Id: Idcde3d959370fd201f1b31cc0377987b2f233582

---
+++ buildscripts/scripts/test-performance.groovy
@@ -0,0 +1,54 @@
+#!groovy
+
+/// file: test-performance.groovy
+
+def main() {
+    check_job_parameters([
+        "EDITION",
+        "DISTRO",
+        "FAKE_WINDOWS_ARTIFACTS",
+    ]);
+
+    def single_tests = load("${checkout_dir}/buildscripts/scripts/utils/single_tests.groovy");
+    def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+
+    def distro = params.DISTRO;
+    def edition = params.EDITION;
+    def fake_windows_artifacts = params.FAKE_WINDOWS_ARTIFACTS;
+
+    // Use the directory also used by tests/testlib/containers.py to have it find
+    // the downloaded package.
+    def download_dir = "package_download";
+    def make_target = "test-performance";
+
+    def setup_values = single_tests.common_prepare(version: "daily", make_target: make_target, docker_tag: params.CIPARAM_OVERRIDE_DOCKER_TAG_BUILD);
+
+    dir("${checkout_dir}") {
+        stage("Fetch Checkmk package") {
+            single_tests.fetch_package(
+                edition: edition,
+                distro: distro,
+                download_dir: download_dir,
+                bisect_comment: params.CIPARAM_BISECT_COMMENT,
+                fake_windows_artifacts: fake_windows_artifacts,
+                docker_tag: setup_values.docker_tag,
+                safe_branch_name: setup_values.safe_branch_name,
+            );
+        }
+
+        test_jenkins_helper.execute_test([
+            name: make_target,
+            cmd: "make -C tests ${make_target}",
+            output_file: "test-performance.txt",
+            container_name: "ubuntu-2404-${setup_values.safe_branch_name}-latest",
+        ]);
+
+        stage("Archive / process test reports") {
+            show_duration("archiveArtifacts") {
+                archiveArtifacts("test-performance.txt");
+            }
+        }
+    }
+}
+
+return this;

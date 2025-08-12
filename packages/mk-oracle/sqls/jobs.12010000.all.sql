caabb953c67536b1476dadd4667739ce4de7bb82
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-08-12 12:28:05 +0200

    mk-oracle: fix jobs 12+
    Change-Id: Ie7c822fe734be4236bdfa9b8aabd0b2594c9a0e4

--- packages/mk-oracle/sqls/jobs.12010000.all.sql
+++ packages/mk-oracle/sqls/jobs.12010000.all.sql
@@ -2,7 +2,7 @@
 -- This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 -- conditions defined in the file COPYING, which is part of this source code package.
 -- Section jobs
-SELECT UPPER(decode(NVL(:IGNORE_DB_NAME, 0), 0, vp.NAME, iii.instance_name))
+SELECT vp.NAME
            || '|' || j.owner
            || '|' || j.job_name
            || '|' || j.state
@@ -12,37 +12,43 @@ SELECT UPPER(decode(NVL(:IGNORE_DB_NAME, 0), 0, vp.NAME, iii.instance_name))
            || '|' || NVL(j.next_run_date, to_date('1970-01-01', 'YYYY-MM-DD'))
            || '|' || NVL(j.schedule_name, '-')
            || '|' || jd.status
-FROM v$instance iii, cdb_scheduler_jobs j
-                         JOIN (
-    SELECT vp.con_id,
-           d.name || '|' || vp.name name
-    FROM v$containers vp
-             JOIN v$database d
-                  ON 1=1
-    WHERE d.cdb = 'YES' and vp.con_id <> 2
-      AND d.database_role = 'PRIMARY'
-      AND d.open_mode = 'READ WRITE'
-    UNION ALL
-    SELECT 0, name
-    FROM v$database d
-    WHERE d.database_role = 'PRIMARY'
-      AND d.open_mode = 'READ WRITE') vp ON j.con_id = vp.con_id
-                         LEFT OUTER JOIN (
-    SELECT con_id,
-           owner,
-           job_name,
-           max(log_id) log_id
-    FROM cdb_scheduler_job_run_details dd
-    GROUP BY con_id,
-             owner,
-             job_name
-) jm
-                                         ON jm.job_name = j.job_name
-                                             AND jm.owner=j.owner
-                                             AND jm.con_id = j.con_id
-                         LEFT OUTER JOIN cdb_scheduler_job_run_details jd
-                                         ON jd.con_id = jm.con_id
-                                             AND jd.owner = jm.owner
-                                             AND jd.job_name = jm.job_name
-                                             AND jd.log_id = jm.log_id
-WHERE NOT (j.auto_drop = 'TRUE' AND repeat_interval IS NULL)
+    FROM cdb_scheduler_jobs j
+    JOIN (
+        SELECT c.con_id,
+           UPPER(decode(NVL(:IGNORE_DB_NAME, 0), 1, d.NAME, i.instance_name ))  || '|' || c.name name
+        FROM v$containers c
+        JOIN v$database d
+            ON 1=1
+        LEFT OUTER JOIN v$instance i
+            ON i.con_id=d.con_id
+        WHERE d.cdb = 'YES' and c.con_id <> 2
+            AND d.database_role = 'PRIMARY'
+            AND d.open_mode = 'READ WRITE'
+        UNION ALL
+        SELECT 0, name
+            FROM v$database d
+            JOIN v$instance i
+                ON i.con_id=d.con_id
+            WHERE d.database_role = 'PRIMARY'
+                AND d.open_mode = 'READ WRITE'
+    ) vp
+        ON j.con_id = vp.con_id
+    LEFT OUTER JOIN (
+        SELECT con_id,
+                owner,
+                job_name,
+                max(log_id) log_id
+        FROM cdb_scheduler_job_run_details dd
+        GROUP BY con_id,
+                owner,
+                job_name
+    ) jm
+        ON jm.job_name = j.job_name
+            AND jm.owner=j.owner
+            AND jm.con_id = j.con_id
+    LEFT OUTER JOIN cdb_scheduler_job_run_details jd
+        ON jd.con_id = jm.con_id
+            AND jd.owner = jm.owner
+            AND jd.job_name = jm.job_name
+            AND jd.log_id = jm.log_id
+    WHERE NOT (j.auto_drop = 'TRUE' AND repeat_interval IS NULL)

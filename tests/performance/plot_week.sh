e3eb6ab2318f70fb4fc84e4923d7d0803fe91849
René Slowenski <rene.slowenski@checkmk.com>
2025-08-15 12:09:30 +0200

    performance test: Add plotting helper script
    CMK-23293
    
    Change-Id: I6d10dc39ca4e766a6ff4f18421a2a91e2c4538f7

---
+++ tests/performance/plot_week.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+_GIT_ROOT=$(git rev-parse --show-toplevel)
+find "${_GIT_ROOT}/results/performance" -mindepth 1 -maxdepth 1 -type d | sort | tail -n5 | xargs "${_GIT_ROOT}/tests/performance/perftest_plot.py" --alert-on-failure

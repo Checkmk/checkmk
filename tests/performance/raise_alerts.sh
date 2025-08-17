43378ade0fe541a80646fec6f7c9d4ae7e91e1ac
René Slowenski <rene.slowenski@checkmk.com>
2025-08-17 11:08:09 +0200

    performance test: Make alerting helper work with DB runs
    Instead of listing the output folders, which we can not rely on
    in a CI environment, we pass the releases used for the validation
    explicitly. This also asserts that we do not work with old data.
    
    CMK-23293
    
    Change-Id: I52324b55ab1bfee250ca3c54280582bbfab121a1

--- tests/performance/raise_alerts.sh
+++ tests/performance/raise_alerts.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 _GIT_ROOT=$(git rev-parse --show-toplevel)
-find "${_GIT_ROOT}/results/performance" -mindepth 1 -maxdepth 1 -type d | sort | tail -n5 | xargs "${_GIT_ROOT}/tests/performance/perftest_plot.py" "$@"
+_BRANCH=$(make --no-print-directory --file="${_GIT_ROOT}/defines.make" print-BRANCH_VERSION)
+_EDITION=cee
+_RELEASES=()
+for ((i = -5; i <= 0; i++)); do
+    _RELEASES+=("${_BRANCH}-$(date -d "$i days" +"%Y.%m.%d").${_EDITION}")
+done
+_DAILY_RELEASE=${_BRANCH}-$(date +"%Y.%m.%d").${_EDITION}
+"${_GIT_ROOT}/tests/performance/perftest_plot.py" "${_RELEASES[@]}" --validate-baselines --alert-on-failure "${@}"

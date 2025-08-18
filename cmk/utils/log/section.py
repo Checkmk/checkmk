0f72ce26777a59cc466b595a8b4ff93ba69d5fd4
Sven Panne <sven.panne@checkmk.com>
2025-08-18 09:03:48 +0200

    Reduced copy-n-paste a bit
    Change-Id: I0bfa63361fa64209d4e213244d4f1de9bc0c3286

--- cmk/utils/log/section.py
+++ cmk/utils/log/section.py
@@ -14,21 +14,15 @@ def section_begin(text: str) -> None:
     console.verbose(f"{tty.bold}{text}{tty.normal}:")
 
 
+def section_step(text: str, add_info: str = "", verbose: bool = True) -> None:
+    (console.verbose if verbose else console.info)(
+        f"{tty.yellow}+{tty.normal} {text.upper()}{f' ({add_info})' if add_info else add_info}"
+    )
+
+
 def section_success(text: str) -> None:
     console.verbose(f"{tty.green}SUCCESS{tty.normal} - {text}")
 
 
 def section_error(text: str, verbose: bool = True) -> None:
-    if verbose:
-        console.verbose(f"{tty.error} - {text}")
-    else:
-        console.info(f"{tty.error} - {text}")
-
-
-def section_step(text: str, add_info: str = "", verbose: bool = True) -> None:
-    if add_info:
-        add_info = f" ({add_info})"  # Additional information, not titlecased
-    if verbose:
-        console.verbose(f"{tty.yellow}+{tty.normal} {text.upper()}{add_info}")
-    else:
-        console.info(f"{tty.yellow}+{tty.normal} {text.upper()}{add_info}")
+    (console.verbose if verbose else console.info)(f"{tty.error} - {text}")

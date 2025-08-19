c01b8ce33c08cdcf903c6f6a9bb54403d303748b
Lukas Lengler <lukas.lengler@checkmk.com>
2025-08-19 15:43:42 +0200

    feat(vue): introduce CmkWizardModeToggle
    CMK-25395
    
    Change-Id: Idb2b52d423908e497f8d02a8d5523ad779088b34

---
+++ packages/cmk-frontend-vue/src/components/CmkWizard/CmkWizardModeToggle.vue
@@ -0,0 +1,25 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+
+<script setup lang="ts">
+import ToggleButtonGroup from '@/components/ToggleButtonGroup.vue'
+import usei18n from '@/lib/i18n.ts'
+
+const { _t } = usei18n()
+const currentMode = defineModel<string>({ required: true })
+</script>
+
+<template>
+  <ToggleButtonGroup
+    v-model="currentMode"
+    :options="[
+      { label: _t('Guided'), value: 'guided' },
+      { label: _t('Overview'), value: 'overview' }
+    ]"
+  />
+</template>
+
+<style scoped></style>

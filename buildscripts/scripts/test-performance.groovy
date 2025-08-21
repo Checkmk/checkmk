9c238e1d889da22d450877e034516d20923b1822
René Slowenski <rene.slowenski@checkmk.com>
2025-08-21 13:47:41 +0200

    performance test: Run dockerized
    Change-Id: I8276e611aeae350e6f43a004acfa426e8f37c630

--- buildscripts/scripts/test-performance.groovy
+++ buildscripts/scripts/test-performance.groovy
@@ -19,7 +19,7 @@ def main() {
     // Use the directory also used by tests/testlib/containers.py to have it find
     // the downloaded package.
     def download_dir = "package_download";
-    def make_target = "test-performance";
+    def make_target = "test-performance-docker";
 
     def setup_values = single_tests.common_prepare(version: "daily", make_target: make_target, docker_tag: params.CIPARAM_OVERRIDE_DOCKER_TAG_BUILD);
 

89d99ce1e662f22f535eaddf30b6fabd1b233f84
Jose Naranjo <jose.naranjo@checkmk.com>
2025-08-21 16:57:47 +0200

    fix: incorrect type alias name
    Change-Id: Idc1261c87ddc9a068bf3731bad2466609275bf86

--- packages/cmk-agent-receiver/cmk/agent_receiver/relay/lib/relays_repository.py
+++ packages/cmk-agent-receiver/cmk/agent_receiver/relay/lib/relays_repository.py
@@ -13,7 +13,7 @@ class RelayNotFoundError(Exception):
 
 
 # Note: Consider use Annotated in case we would like to have some pydantic validation
-TaskID = str
+RelayID = str
 
 # Persistence layer is not thread safe yet.
 # Note: Since we are using async endpoints in the agent-receiver
@@ -21,25 +21,25 @@ TaskID = str
 # The persistence layer is for now an in memory dict so we won't need
 # to make this thread-safe as this should not be accessed by multiple threads
 # concurrently.
-GLOBAL_RELAYS: dict[TaskID, list[TaskResponse]] = {}
+GLOBAL_RELAYS: dict[RelayID, list[TaskResponse]] = {}
 
 
 @dataclasses.dataclass
 class RelaysRepository:
-    def add_relay(self, relay_id: TaskID) -> None:
+    def add_relay(self, relay_id: RelayID) -> None:
         if relay_id not in GLOBAL_RELAYS:
             GLOBAL_RELAYS[relay_id] = []
 
-    def list_relays(self) -> list[TaskID]:
+    def list_relays(self) -> list[RelayID]:
         return list(GLOBAL_RELAYS.keys())
 
-    def get_relay_tasks(self, relay_id: TaskID) -> list[TaskResponse]:
+    def get_relay_tasks(self, relay_id: RelayID) -> list[TaskResponse]:
         if relay_id not in GLOBAL_RELAYS:
             raise RelayNotFoundError(f"Relay ID {relay_id} not found.")
         return GLOBAL_RELAYS[relay_id]
 
-    def has_relay(self, relay_id: TaskID) -> bool:
+    def has_relay(self, relay_id: RelayID) -> bool:
         return relay_id in GLOBAL_RELAYS
 
-    def remove_relay(self, relay_id: TaskID) -> None:
+    def remove_relay(self, relay_id: RelayID) -> None:
         del GLOBAL_RELAYS[relay_id]

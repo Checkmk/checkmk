d05ca15465da3fba8d0de101892a5e384cac66da
Diana Ivanchevska <diana.ivanchevska@checkmk.com>
2025-08-21 09:11:45 +0200

    18207 SEC Fix security vulnerability in win_license.bat plugin
    On Windows hosts to force the English output from the `win_license.bat` plugin,
    special copying logic is used (this way, the default `slmgr.vbs` script cannot find the language files).
    As the script is copied to a global, unprotected location, every user has access to edit this script.
    This can be exploited for malicious intent.
    To eliminate this vulnerability, the `slmgr.vbs` script is copied to the protected location
    in `%SystemDrive%\ProgramData\checkmk\agent\tmp` and is deleted afterwards.
    
    **Note:** Only users who use the Windows License plug-in are affected by this issue.
    
    Change-Id: I795f8354b409e833174b3586baec941ad11234a8

--- agents/windows/plugins/win_license.bat
+++ agents/windows/plugins/win_license.bat
@@ -6,12 +6,14 @@ REM ***
 
 echo ^<^<^<win_license^>^>^>
 
-REM create a copy of slmgr.vbs outside the system32 folder to force english text output, because this way the language files can't be found anymore
-copy /Y "%windir%\system32\slmgr.vbs" "%temp%\checkmk_slmgr.vbs" > NUL
+set "CMK_TMP=%SystemDrive%\ProgramData\checkmk\agent\tmp"
 
-if exist "%temp%\checkmk_slmgr.vbs" (
-    cscript //NoLogo "%temp%\checkmk_slmgr.vbs" /dli
-    del "%temp%\checkmk_slmgr.vbs" > NUL
+REM create a copy of slmgr.vbs outside the system32 folder to force english text output, because this way the language files can't be found anymore
+copy /Y "%windir%\system32\slmgr.vbs" "%CMK_TMP%\checkmk_slmgr.vbs" > NUL
+
+if exist "%CMK_TMP%\checkmk_slmgr.vbs" (
+    cscript //NoLogo "%CMK_TMP%\checkmk_slmgr.vbs" /dli
+    del "%CMK_TMP%\checkmk_slmgr.vbs" > NUL
 ) else (
     REM fallback in case temp directory cannot be written to
     cscript //NoLogo "%windir%\System32\slmgr.vbs" /dli

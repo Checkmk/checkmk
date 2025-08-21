d673cec7d253a411b53599ef13aa0ea6d6da8955
Diana Ivanchevska <diana.ivanchevska@checkmk.com>
2025-08-21 08:31:16 +0200

    wa: add checkmk copyright headers to ps1 tests
    Change-Id: I521912556febc999695f2f567797603f8c7ee953

--- agents/windows/unit_tests_ps1/kaspersky_av_client.Tests.ps1
+++ agents/windows/unit_tests_ps1/kaspersky_av_client.Tests.ps1
@@ -1,3 +1,7 @@
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 BeforeAll {
     $scriptPath = Join-Path $PSScriptRoot "..\plugins\kaspersky_av_client.ps1"
     . $scriptPath

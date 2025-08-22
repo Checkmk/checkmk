bfcd60706c06277bc64ef2b76dc637ef2f6b6079
Sven Panne <sven.panne@checkmk.com>
2025-08-22 11:33:39 +0200

    Don't descend into the Bazel external dependencies
    Change-Id: I30bc9e5c97eb892253958bec116430d04d6fd171

--- tests/code_quality/test_requirements.py
+++ tests/code_quality/test_requirements.py
@@ -152,6 +152,8 @@ def iter_sourcefiles(basepath: Path) -> Iterable[Path]:
         # TODO: We need to find a better way for the bazel-* folders created by bazel
         if "bazel-" in sub_path.name:
             continue
+        if sub_path.name == "external":  # Bazel external dependencies
+            continue
         if sub_path.name == "node_modules":
             continue
         if sub_path.name.startswith("."):

c0cccc85d5f29a3c53138ef733850ce2d7ecebce
Anastasiia Shevchuk <anastasiia.shevchuk@checkmk.com>
2025-08-22 18:07:59 +0200

    otel-qa: add integration tests for otel service monitoring rule
    CMK-25058
    
    Change-Id: I2e8d2b824da970cdd8a768e36ef970702e47ac66
    (cherry picked from commit cf38d8a54880c7457f2cf1d768ba31cfceb19edc)

--- tests/testlib/agent_dumps.py
+++ tests/testlib/agent_dumps.py
@@ -19,6 +19,66 @@ from tests.testlib.utils import logger, run
 from tests.testlib.version import CMKVersion, get_min_version
 
 
+def create_agent_dump_rule(
+    site: Site,
+    dumps_path: Path | None = None,
+    rule_folder: str = "/",
+) -> tuple[str, Path]:
+    """Create a rule to read agent data from a dump path.
+
+    Returns the rule ID and the path of the dumps.
+
+    Args:
+        site: The test site.
+        dumps_path: The path where the dumps are stored in the site.
+        rule_folder: The host folder in the site to create the rule.
+    """
+    dumps_path = dumps_path or site.path("var/check_mk/dumps")
+    ruleset_name = "datasource_programs"
+    rule_value = f'cat "{dumps_path.as_posix()}/$HOSTNAME$"'
+    if site.is_dir(dumps_path):
+        logger.info('Creating folder "%s"...', dumps_path)
+        _ = site.run(["mkdir", "-p", dumps_path.as_posix()])
+    logger.info('Creating rule "%s"...', ruleset_name)
+    rule_id = site.openapi.rules.create(
+        value=rule_value,
+        ruleset_name=ruleset_name,
+        folder=rule_folder,
+    )
+    return rule_id, dumps_path
+
+
+def copy_dumps(
+    site: Site,
+    source_dir: Path,
+    target_dir: Path,
+    prefix: str = "agent-",
+    source_filename: str | None = None,
+    target_filenames: list[str] | None = None,
+) -> None:
+    """Copy SNMP/agent dumps from source dir to target dir, ignoring subdirectories.
+
+    Args:
+        site: The test site.
+        source_dir: The folder to copy the dumps from.
+        target_dir: The folder to copy the dumps to.
+        prefix: The prefix of the files to copy.
+        source_filename: The specific source file name to copy (optional).
+        target_filenames: The specific target file names to copy the source_filename to (optional).
+    """
+    logger.info("Injecting agent-output...")
+    target_dir = site.path(target_dir)
+    if not site.is_dir(target_dir):
+        site.makedirs(target_dir)
+    source_path = f"{source_dir}/{(source_filename if source_filename else prefix + '*')}"
+    if source_filename and target_filenames:
+        target_paths = list((target_dir / filename) for filename in target_filenames)
+    else:
+        target_paths = [target_dir]
+    for target_path in target_paths:
+        run(["bash", "-c", f'cp -f {source_path} "{target_path}"'], sudo=True)
+
+
 def _dumps_up_to_date(dumps_dir: Path, min_version: CMKVersion) -> None:
     """Check if the dumps are up-to-date with the minimum-version branch."""
     dumps = list(dumps_dir.glob("*"))

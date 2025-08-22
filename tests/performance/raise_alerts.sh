fded35ea2db8762c8aaf7848630090d7c2fd6566
René Slowenski <rene.slowenski@checkmk.com>
2025-08-22 16:00:39 +0200

    performance test: Enable database connection
    CMK-24170
    
    Change-Id: I8439b539c6db084e79682233feff296054a3f4e2

--- tests/performance/raise_alerts.sh
+++ tests/performance/raise_alerts.sh
@@ -7,4 +7,4 @@ for ((i = -5; i <= 0; i++)); do
     _RELEASES+=("${_BRANCH}-$(date -d "$i days" +"%Y.%m.%d").${_EDITION}")
 done
 _DAILY_RELEASE=${_BRANCH}-$(date +"%Y.%m.%d").${_EDITION}
-"${_GIT_ROOT}/tests/performance/perftest_plot.py" "${_RELEASES[@]}" --validate-baselines --alert-on-failure "${@}"
+"${_GIT_ROOT}/tests/performance/perftest_plot.py" "${_RELEASES[@]}" --dbhost=qa.lan.checkmk.net --validate-baselines --alert-on-failure "${@}"

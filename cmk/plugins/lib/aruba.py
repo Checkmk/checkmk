64c95425eab1e6d76c8dc0a84531a6c082965648
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-08-25 07:54:49 +0200

    Revert "18466 Fix temp thresholds of Aruba 6100+ switches"
    This reverts commit b82dda4f7b332ba27d3da16382193fc4dd5510a8.
    
    Reason for revert: breaks test-plugins
    
    Change-Id: I40cb12910bfa9dcbcac4946a8145a4f0b41405b4

--- cmk/plugins/lib/aruba.py
+++ cmk/plugins/lib/aruba.py
@@ -5,5 +5,6 @@
 
 from cmk.agent_based.v2 import matches, startswith
 
+DETECT_6100 = matches(".1.3.6.1.2.1.1.1.0", "Aruba.+6100.*")
 DETECT_2930M = matches(".1.3.6.1.2.1.1.1.0", "Aruba.+2930M.*")
 DETECT_WLC = startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.14823.1.1")

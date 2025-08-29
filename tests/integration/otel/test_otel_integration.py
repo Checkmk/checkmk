667313a6e869576f010e3b9f31a873351c62e4fb
Dennis Schmalacker <dennis.schmalacker@checkmk.com>
2025-08-29 11:57:22 +0200

    18526 otel-collector: Move self monitoring into site checks
    This way we treat it the same way as  e.g. cmk-monitor-broker
    
    CMK-25442
    
    Change-Id: I9efebe2598b913cd83e8e72c58d6a7f9563b9fd2

--- tests/integration/otel/test_otel_integration.py
+++ tests/integration/otel/test_otel_integration.py
@@ -192,8 +192,7 @@ def test_otel_collector_with_receiver_config(
 
         logger.info("Adding a rule for OpenTelemetry special agent")
         rule_id = otel_enabled_site.openapi.rules.create(
-            ruleset_name="special_agents:otel",
-            value={"include_self_monitoring": False},
+            ruleset_name="special_agents:otel", value={}
         )
         otel_enabled_site.openapi.changes.activate_and_wait_for_completion()
 
@@ -270,8 +269,7 @@ def test_otel_collector_with_prometheus_scrape_config(otel_enabled_site: Site) -
 
         logger.info("Adding a rule for OpenTelemetry special agent")
         rule_id = otel_enabled_site.openapi.rules.create(
-            ruleset_name="special_agents:otel",
-            value={"include_self_monitoring": False},
+            ruleset_name="special_agents:otel", value={}
         )
         otel_enabled_site.openapi.changes.activate_and_wait_for_completion()
 
@@ -343,8 +341,7 @@ def test_otel_collector_with_prometheus_scrape_config_tls(otel_enabled_site: Sit
 
         logger.info("Adding a rule for OpenTelemetry special agent")
         rule_id = otel_enabled_site.openapi.rules.create(
-            ruleset_name="special_agents:otel",
-            value={"include_self_monitoring": False},
+            ruleset_name="special_agents:otel", value={}
         )
         otel_enabled_site.openapi.changes.activate_and_wait_for_completion()
 

aaa8b3dedefa95794c737f3d20423fd74b857d7d
Cornel Craveta <cornel.craveta@checkmk.com>
2025-09-03 13:39:01 +0200

    Split big task update test in smaller, more focused tests
    Change-Id: I1bc40ba72d4c46d166f467fc6d04022286f0054c

--- packages/cmk-agent-receiver/tests/component/test_update_task.py
+++ packages/cmk-agent-receiver/tests/component/test_update_task.py
@@ -20,6 +20,7 @@ from .test_lib.agent_receiver import AgentReceiverClient
 from .test_lib.tasks import get_relay_tasks, push_task
 
 RESPONSE_PAYLOAD = "some response payload"
+TASKS_COUNT = 5
 
 
 @pytest.mark.parametrize(
@@ -29,42 +30,21 @@ RESPONSE_PAYLOAD = "some response payload"
         pytest.param("ERROR", ResultType.ERROR, TaskStatus.FAILED, id="result type failure"),
     ],
 )
-def test_update_task(
+def test_updating_task_should_change_stored_task_object(
+    relay_id: str,
     agent_receiver: AgentReceiverClient,
     result_type_input: str,
     result_type_output: ResultType,
     expected_status: TaskStatus,
 ) -> None:
-    # add the relay
-
-    relay_id = str(uuid.uuid4())
-    agent_receiver.register_relay(relay_id=relay_id)
-
-    # add some tasks
-    task_ids = add_tasks(5, agent_receiver, relay_id)
-
-    # pick the testing task
-    task_id = task_ids[2]
-
-    # the task is not updated yet, should be pending
-
-    all_tasks = get_all_tasks(agent_receiver, relay_id)
-    task = find_task_with_id(task_id, all_tasks)
-    assert_is_pending_task(task, task_id=task_id)
-
-    all_pending_tasks = get_pending_tasks(agent_receiver, relay_id)
-    task = find_task_with_id(task_id, all_pending_tasks)
-    assert_is_pending_task(task, task_id)
-
-    # save copies for later
-
-    task_when_created = task
-    orig_tasks = all_tasks
+    """
+    The stored task object should be modified so that it contains the result.
+    The task status should reflect the type of the attached result.
+    """
+    task_ids = add_tasks(1, agent_receiver, relay_id)
+    task_id = task_ids[0]
 
     # do the update
-
-    # sleep a bit to ensure different update timestamp
-    time.sleep(0.05)
 
     update_response = agent_receiver.update_task(
         relay_id=relay_id,
@@ -95,23 +75,106 @@ def test_update_task(
         result_payload=RESPONSE_PAYLOAD,
     )
 
-    assert task.creation_timestamp == task_when_created.creation_timestamp
-    assert task.update_timestamp > task_when_created.update_timestamp
 
-    # the task is no longer in the list of pending tasks
+@pytest.mark.parametrize("result_type_input", ["OK", "ERROR"])
+def test_task_no_longer_pending(
+    agent_receiver: AgentReceiverClient, relay_id: str, result_type_input: str
+) -> None:
+    """
+    Once a task has been updated with a result, it should no longer appear in the list of pending tasks.
+    """
 
+    task_ids = add_tasks(3, agent_receiver, relay_id)
+    task_id = task_ids[1]
+
+    all_tasks = get_all_tasks(agent_receiver, relay_id)
+    task = find_task_with_id(task_id, all_tasks)
+    assert_is_pending_task(task, task_id=task_id)
+
+    all_pending_tasks = get_pending_tasks(agent_receiver, relay_id)
+    task = find_task_with_id(task_id, all_pending_tasks)
+    assert_is_pending_task(task, task_id)
+
+    _ = agent_receiver.update_task(
+        relay_id=relay_id,
+        task_id=task_id,
+        result_type=result_type_input,
+        result_payload=RESPONSE_PAYLOAD,
+    )
     all_pending_tasks = get_pending_tasks(agent_receiver, relay_id)
     assert_task_not_in_the_list(task_id, all_pending_tasks)
 
-    # TODO assert the other tasks have not been touched
+
+@pytest.mark.parametrize("result_type_input", ["OK", "ERROR"])
+def test_timestamps_are_handled(
+    agent_receiver: AgentReceiverClient, relay_id: str, result_type_input: str
+) -> None:
+    """
+    A call to update task should modify the "update_timestamp" values and it should not
+    modify the "creation_timestamp" value.
+    """
+
+    task_ids = add_tasks(1, agent_receiver, relay_id)
+    task_id = task_ids[0]
+
+    all_tasks = get_all_tasks(agent_receiver, relay_id)
+    task_when_created = find_task_with_id(task_id, all_tasks)
+
+    # sleep a bit to ensure different update timestamp
+    time.sleep(0.05)
+
+    _ = agent_receiver.update_task(
+        relay_id=relay_id,
+        task_id=task_id,
+        result_type=result_type_input,
+        result_payload=RESPONSE_PAYLOAD,
+    )
+
+    all_tasks = get_all_tasks(agent_receiver, relay_id)
+    task = find_task_with_id(task_id, all_tasks)
+
+    assert task.creation_timestamp == task_when_created.creation_timestamp
+    assert task.update_timestamp > task_when_created.update_timestamp
+
+
+@pytest.mark.parametrize("result_type_input", ["OK", "ERROR"])
+def test_the_other_tasks_are_not_changed(
+    agent_receiver: AgentReceiverClient,
+    relay_id: str,
+    result_type_input: str,
+) -> None:
+    """
+    A call to update task should not modify any other possible tasks that belong to the relay.
+    """
+
+    task_ids = add_tasks(3, agent_receiver, relay_id)
+    task_id = task_ids[1]
+
+    orig_tasks = get_all_tasks(agent_receiver, relay_id)
+
+    _ = agent_receiver.update_task(
+        relay_id=relay_id,
+        task_id=task_id,
+        result_type=result_type_input,
+        result_payload=RESPONSE_PAYLOAD,
+    )
+
+    current_tasks = get_all_tasks(agent_receiver, relay_id)
 
     other_task_ids = set(task_ids)
     other_task_ids.remove(task_id)
 
     for tid in other_task_ids:
         t1 = find_task_with_id(tid, orig_tasks)
-        t2 = find_task_with_id(tid, stored_tasks)
+        t2 = find_task_with_id(tid, current_tasks)
         assert t1 == t2
+
+
+@pytest.fixture
+def relay_id(agent_receiver: AgentReceiverClient) -> str:
+    relay_id = str(uuid.uuid4())
+    agent_receiver.register_relay(relay_id=relay_id)
+    return relay_id
 
 
 def add_tasks(count: int, agent_receiver: AgentReceiverClient, relay_id: str) -> list[TaskID]:

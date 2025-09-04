d45361b859e7f52564069971ee8a7a0979fd6d96
Logan Connolly <logan.connolly@checkmk.com>
2025-09-04 16:27:55 +0200

    18567 nginx_status: prefer iproute2 over netstat
    With this change, we add a network stats handler with the `ss` utility
    from iproute2. Since `netstat` is deprecated, `ss` should take
    precedence. However, if `ss` is not available, fall back to `netstat`.
    
    The example iproute2 (ss) output was derived by:
    
      - starting an nginx:latest (debian) container.
      - install iproute2 package to get access to `ss` utility.
      - run `os.popen("ss -tlnp 2>/dev/null").readlines()` in Python
        REPL and copy out contents.
    
    This doesn't cover all edge cases for dealing with ss output, but
    it's a real life example of what it could look like when nginx is
    running on a host.
    
    CMK-25567
    
    Change-Id: Ie096f49f8bb5ef5b07e4ab61286ac6d7b05ccd9f

--- tests/agent-plugin-unit/test_nginx_status.py
+++ tests/agent-plugin-unit/test_nginx_status.py
@@ -47,3 +47,66 @@ class TestExtractStatsFromNetstat:
         assert nginx_status.extract_stats_from_netstat(lines, [443]) == [
             ("https", "127.0.0.1", 443),
         ]
+
+
+class TestExtractStatsFromIproute2:
+    """Check that network stats are correctly extracted from iproute2 (ss) output."""
+
+    def test_with_empty_lines(self) -> None:
+        assert nginx_status.extract_stats_from_iproute2([], [443]) == []
+
+    def test_nginx_process_not_found(self) -> None:
+        lines = [
+            "State  Recv-Q  Send-Q  Local Address:Port  Peer Address:Port  Process \n",
+            'LISTEN  0  511  0.0.0.0:80  0.0.0.0:*  users:(("foo",pid=1,fd=6))\n',
+        ]
+        assert not nginx_status.extract_stats_from_iproute2(lines, [443])
+
+    def test_with_http_port(self) -> None:
+        lines = [
+            "State  Recv-Q  Send-Q  Local Address:Port  Peer Address:Port  Process \n",
+            'LISTEN  0  511  0.0.0.0:80  0.0.0.0:*  users:(("nginx",pid=1,fd=6))\n',
+            'LISTEN  0  511     [::]:80     [::]:*  users:(("nginx",pid=1,fd=7))\n',
+        ]
+        assert nginx_status.extract_stats_from_iproute2(lines, [443]) == [
+            ("http", "127.0.0.1", 80),
+        ]
+
+    def test_with_https_port(self) -> None:
+        lines = [
+            "State   Recv-Q  Send-Q  Local Address:Port  Peer Address:Port  Process \n",
+            'LISTEN  0  511  0.0.0.0:443  0.0.0.0:*  users:(("nginx",pid=1,fd=6))\n',
+            'LISTEN  0  511     [::]:443     [::]:*  users:(("nginx",pid=1,fd=7))\n',
+        ]
+        assert nginx_status.extract_stats_from_iproute2(lines, [443]) == [
+            ("https", "127.0.0.1", 443),
+        ]
+
+    def test_with_child_processes(self) -> None:
+        lines = [
+            "State  Recv-Q  Send-Q  Local Address:Port  Peer Address:Port  Process \n",
+            'LISTEN  0  511  0.0.0.0:80  0.0.0.0:*  users:(("nginx",pid=1,fd=6),("nginx",pid=2,fd=6))\n',
+        ]
+        assert nginx_status.extract_stats_from_iproute2(lines, [443]) == [
+            ("http", "127.0.0.1", 80),
+        ]
+
+
+def test_netstat_and_iproute2_return_same_stats() -> None:
+    """Sanity check to see if there is drift between the two strategies."""
+    netstat_lines = [
+        "Active Internet connections (only servers)\n",
+        "Proto Recv-Q Send-Q Local Address  Foreign Address  State   PID/Program name \n",
+        "tcp        0      0 0.0.0.0:80     0.0.0.0:*        LISTEN  1/nginx: master pro \n",
+        "tcp6       0      0 :::80          :::*             LISTEN  1/nginx: master pro \n",
+    ]
+    iproute2_lines = [
+        "State   Recv-Q  Send-Q  Local Address:Port  Peer Address:Port  Process \n",
+        'LISTEN  0  511  0.0.0.0:80  0.0.0.0:*  users:(("nginx",pid=1,fd=6))\n',
+        'LISTEN  0  511     [::]:80     [::]:*  users:(("nginx",pid=1,fd=7))\n',
+    ]
+
+    netstat_stats = nginx_status.extract_stats_from_netstat(netstat_lines, [443])
+    iproute2_stats = nginx_status.extract_stats_from_iproute2(iproute2_lines, [443])
+
+    assert netstat_stats == iproute2_stats

aef38c22157d832d094fe8798d7db1ad1ffd3e92
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-09-04 15:38:24 +0200

    test diskspace: multiple tests for different cases
    CMK-22923
    
    Change-Id: Iaaa5263460aa9ee464e3fba7b8dada8e2e1cab28

--- tests/unit/cmk/diskspace/test_abandoned.py
+++ tests/unit/cmk/diskspace/test_abandoned.py
@@ -9,17 +9,31 @@ from pathlib import Path
 from cmk.diskspace.abandoned import _cleanup_host_directories
 
 
-def test_cleanup_host_directories(tmp_path: Path) -> None:
-    (outdated := tmp_path / "outdated_dir").mkdir()
-    (outdated_file := outdated / "outdated").touch()
-    os.utime(outdated_file, (12300000.0, 12300000.0))
+def test_cleanup_host_directories_recent_enough(tmp_path: Path) -> None:
+    (recent_enough := tmp_path / "recent_enough").mkdir()
+    (recent_enough_file1 := recent_enough / "file1").touch()
+    os.utime(recent_enough, (12300003.0, 12300003.0))
+
+    assert set() == _cleanup_host_directories(12300005.0, 3, set(), str(tmp_path))
+    assert recent_enough_file1.exists()
+
+
+def test_cleanup_host_directories_one_old_one_recent(tmp_path: Path) -> None:
     (recent_enough := tmp_path / "recent_enough").mkdir()
     (recent_enough_file1 := recent_enough / "file1").touch()
     os.utime(recent_enough, (12300003.0, 12300003.0))
     (ignored_outdated_file := recent_enough / "outdated").touch()
     os.utime(ignored_outdated_file, (12300000.0, 12300000.0))
 
-    _cleanup_host_directories(12300005.0, 3, {"keep"}, str(tmp_path))
-    assert not outdated.exists()
+    assert set() == _cleanup_host_directories(12300005.0, 3, set(), str(tmp_path))
     assert recent_enough_file1.exists()
     assert ignored_outdated_file.exists()
+
+
+def test_cleanup_host_directories_outdated(tmp_path: Path) -> None:
+    (outdated := tmp_path / "outdated_dir").mkdir()
+    (outdated_file := outdated / "outdated").touch()
+    os.utime(outdated_file, (12300000.0, 12300000.0))
+
+    assert {"outdated_dir"} == _cleanup_host_directories(12300005.0, 3, set(), str(tmp_path))
+    assert not outdated.exists()

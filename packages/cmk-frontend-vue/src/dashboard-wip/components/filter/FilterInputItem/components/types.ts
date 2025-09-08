d576e19d148ca350b3d04d8f25bbf763c4782d63
Wontek Hong <wontek.hong@checkmk.com>
2025-09-08 21:09:20 +0200

    filter: introduce Dropdown component
    Change-Id: I29249c81cfa30d7ad1e10c0e9c72f7763296198e

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/filter/FilterInputItem/components/types.ts
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+ * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+ * conditions defined in the file COPYING, which is part of this source code package.
+ */
+import type { ConfiguredValues } from '@/dashboard-wip/components/filter/types.ts'
+
+export interface FilterEmits {
+  'update-filter-values': [filterId: string, values: ConfiguredValues]
+}
+
+export interface ComponentEmits {
+  'update-component-values': [componentId: string, values: ConfiguredValues]
+}
+
+export interface FilterComponentProps<T> {
+  component: T
+  configuredValues: ConfiguredValues | null
+}

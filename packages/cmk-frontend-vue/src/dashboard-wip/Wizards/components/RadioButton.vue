90f26ff5772bf1ae214c09e3e1eeb96c2b9cc6fc
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-09-08 14:04:00 +0200

    dashboard_wip: implement base component RadioButton
    Change-Id: I61275d4f731201a1276c51c2050abf375dfb7e29

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/Wizards/components/RadioButton.vue
@@ -0,0 +1,33 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+import { useId } from 'vue'
+
+import type { TranslatedString } from '@/lib/i18nString'
+
+interface RadioButtonProp {
+  value: string
+  label?: TranslatedString
+  disabled?: boolean
+  name?: string
+}
+
+defineProps<RadioButtonProp>()
+const model = defineModel<string>({ required: true })
+const id = useId()
+</script>
+
+<template>
+  <input
+    :id="id"
+    v-model="model"
+    type="radio"
+    :value="value"
+    :disabled="!!disabled"
+    :name="name || ''"
+  />
+  <label v-if="label" :for="id">{{ label }}</label>
+</template>

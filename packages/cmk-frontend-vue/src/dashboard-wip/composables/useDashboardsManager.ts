296ca2ea4feb10484a8e7f28170ba85d9eebb364
Wontek Hong <wontek.hong@checkmk.com>
2025-09-09 15:33:06 +0200

    dashboard/composables: introduce useDashboardsManager for managing dashboards
    Co-authored-by: Christopher Krause <christopher.krause@checkmk.com>
    
    Change-Id: Ia7d6ea8dffbb759fd590e02ddc6791fc8b6b7503

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/composables/useDashboardsManager.ts
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+ * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+ * conditions defined in the file COPYING, which is part of this source code package.
+ */
+import { type Ref, computed, onBeforeMount, ref } from 'vue'
+
+import {
+  type ContentRelativeGrid,
+  type ContentResponsiveGrid,
+  DashboardLayout,
+  type DashboardModel
+} from '@/dashboard-wip/types/dashboard'
+import type { DashboardConstants } from '@/dashboard-wip/types/dashboard.ts'
+import { dashboardAPI } from '@/dashboard-wip/utils.ts'
+
+export function useDashboardsManager() {
+  const constants = ref<DashboardConstants>()
+
+  onBeforeMount(async () => {
+    constants.value = await dashboardAPI.getDashboardConstants()
+  })
+
+  const dashboards = ref<Map<string, DashboardModel>>(new Map())
+  const activeDashboardName: Ref<string | undefined> = ref(undefined)
+
+  const activeDashboard = computed<DashboardModel | undefined>(() => {
+    return activeDashboardName.value ? dashboards.value.get(activeDashboardName.value) : undefined
+  })
+
+  const isInitialized = computed<boolean>(() => {
+    return constants.value !== undefined && activeDashboard.value !== undefined
+  })
+
+  async function loadDashboard(name: string, layoutType: DashboardLayout) {
+    let content: ContentRelativeGrid | ContentResponsiveGrid
+    let dashboardResp
+
+    if (layoutType === DashboardLayout.RELATIVE_GRID) {
+      const resp = await dashboardAPI.getRelativeDashboard(name)
+      dashboardResp = resp.extensions
+      content = {
+        layout: dashboardResp.layout,
+        widgets: dashboardResp.widgets
+      } as ContentRelativeGrid
+    } else {
+      const resp = await dashboardAPI.getResponsiveDashboard(name)
+      dashboardResp = resp.extensions
+      content = {
+        layout: dashboardResp.layout,
+        widgets: dashboardResp.widgets
+      } as ContentResponsiveGrid
+    }
+
+    const dashboard: DashboardModel = {
+      general_settings: dashboardResp.general_settings,
+      filter_context: dashboardResp.filter_context,
+      content
+    }
+
+    dashboards.value.set(name, dashboard)
+    activeDashboardName.value = name
+  }
+
+  return {
+    constants,
+    dashboards,
+    activeDashboard,
+    activeDashboardName,
+    isInitialized,
+
+    loadDashboard
+  }
+}
+
+export type DashboardsManager = ReturnType<typeof useDashboardsManager>

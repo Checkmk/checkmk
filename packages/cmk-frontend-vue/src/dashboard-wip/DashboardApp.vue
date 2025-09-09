2cbaa43a4748ea3e572e6c6483d54ce3ae500b21
Wontek Hong <wontek.hong@checkmk.com>
2025-09-09 15:56:04 +0200

    DashboardApp: add mechanic to load a dashboard
    Change-Id: I3d1fb9f4f686ae4ad74d271e218e4c073951264d

--- packages/cmk-frontend-vue/src/dashboard-wip/DashboardApp.vue
+++ packages/cmk-frontend-vue/src/dashboard-wip/DashboardApp.vue
@@ -4,13 +4,24 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
+import { onBeforeMount } from 'vue'
+
 import usei18n from '@/lib/i18n.ts'
 
+import { useDashboardsManager } from '@/dashboard-wip/composables/useDashboardsManager.ts'
 import type { DashboardPageProperties } from '@/dashboard-wip/types/page.ts'
 
 const { _t } = usei18n()
 
-defineProps<DashboardPageProperties>()
+const props = defineProps<DashboardPageProperties>()
+
+const dashboardsManager = useDashboardsManager()
+
+onBeforeMount(async () => {
+  if (props.dashboard) {
+    await dashboardsManager.loadDashboard(props.dashboard.name, props.dashboard.layout_type)
+  }
+})
 </script>
 
 <template>

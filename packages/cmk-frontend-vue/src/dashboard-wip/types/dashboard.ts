b94f52c930c13e8199db37d7caf2a1eb634a65a3
Wontek Hong <wontek.hong@checkmk.com>
2025-09-09 15:01:37 +0200

    dashboard/types: introduce further types
    Change-Id: I710fb787271e4b22c22b50298e2e42ecd05f04d6

--- packages/cmk-frontend-vue/src/dashboard-wip/types/dashboard.ts
+++ packages/cmk-frontend-vue/src/dashboard-wip/types/dashboard.ts
@@ -5,6 +5,8 @@
  */
 import type { components } from '@/lib/rest-api-client/openapi_internal'
 
+import type { RelativeGridWidget, ResponsiveGridWidget } from '@/dashboard-wip/types/widget.ts'
+
 export interface Thresholds {
   height: number
   width: number
@@ -15,3 +17,25 @@ export type RefreshAction = string | (() => void)
 export type DashboardMetadataModel = components['schemas']['DashboardMetadataModel']
 export type DashboardMetadata = components['schemas']['DashboardMetadata']
 export type DashboardConstants = components['schemas']['DashboardConstantsResponse']
+
+// Dashboard Response types
+export type DashboardFilterContext = components['schemas']['DashboardFilterContext']
+export type DashboardGeneralSettings = components['schemas']['DashboardGeneralSettings']
+export type DashboardRelativeGridLayout = components['schemas']['DashboardRelativeGridLayout']
+export type DashboardResponsiveGridLayout = components['schemas']['DashboardResponsiveGridLayout']
+
+export type ContentRelativeGrid = {
+  layout: DashboardRelativeGridLayout
+  widgets: { [key: string]: RelativeGridWidget }
+}
+
+export type ContentResponsiveGrid = {
+  layout: DashboardResponsiveGridLayout
+  widgets: { [key: string]: ResponsiveGridWidget }
+}
+
+export type DashboardModel = {
+  general_settings: DashboardGeneralSettings
+  filter_context: DashboardFilterContext
+  content: ContentResponsiveGrid | ContentRelativeGrid
+}

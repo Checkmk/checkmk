f2a560d841cd315bacc688eea6512fe6b1fa3380
Simon Jess <simon.jess@checkmk.com>
2025-09-09 14:01:25 +0200

    PRO TIP: migrate display hints of node 'hardware > memory' and related attributes
    Notable changes:
    
     * The path is a list of node names.
    
     * The localization function `_l` is replaced by `Title`.
    
     * Python's dictionaries respect the order of keys. Thus there is no extra field for `key_order` anymore.
    
     * The `paint` function `bytes_rounded` is replaced by `NumberField` and a render function.
    
    CMK-25075
    
    Change-Id: If929cf3bd4c719b717f18bea68155376fd30ce57

--- cmk/plugins/collection/inventory_ui/hardware.py
+++ cmk/plugins/collection/inventory_ui/hardware.py
@@ -3,10 +3,23 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.inventory_ui.v1_alpha import Node, Title
+from cmk.inventory_ui.v1_alpha import IECNotation, Node, NumberField, Title, Unit
+
+UNIT_BYTES = Unit(IECNotation("B"))
 
 node_hardware = Node(
     name="hardware",
     path=["hardware"],
     title=Title("Hardware"),
+)
+
+node_hardware_memory = Node(
+    name="hardware_memory",
+    path=["hardware", "memory"],
+    title=Title("Memory (RAM)"),
+    attributes={
+        "total_ram_usable": NumberField(Title("Total usable RAM"), render=UNIT_BYTES),
+        "total_swap": NumberField(Title("Total swap space"), render=UNIT_BYTES),
+        "total_vmalloc": NumberField(Title("Virtual addresses for mapping"), render=UNIT_BYTES),
+    },
 )

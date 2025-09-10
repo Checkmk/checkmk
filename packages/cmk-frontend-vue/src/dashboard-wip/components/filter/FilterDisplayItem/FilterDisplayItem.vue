38ef7292da9da8ce84c4a970499d7e41a7d92cff
Wontek Hong <wontek.hong@checkmk.com>
2025-09-10 15:46:56 +0200

    filter: introduce FilterDisplayItem component
    Change-Id: Id82914a24bec03381f5494dad04632b2f28008b6

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/filter/FilterDisplayItem/FilterDisplayItem.vue
@@ -0,0 +1,65 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+
+import type { ConfiguredValues, FilterDefinition } from '../types.ts'
+import FilterDisplayComponentRenderer from './FilterDisplayComponentRenderer.vue'
+
+interface Props {
+  filterId: string
+  configuredValues: ConfiguredValues
+}
+
+const props = defineProps<Props>()
+
+const filterDefinitions = inject<Record<string, FilterDefinition> | null>('filterDefinitions', null)
+if (!filterDefinitions) {
+  console.error('Filter definitions not found in the provided context')
+  throw new Error('Filter definitions are not available')
+}
+const filterDefinition = filterDefinitions[props.filterId]
+if (!filterDefinition) {
+  console.error(`Filter definition for ${props.filterId} not found`)
+  throw new Error(`Filter definition for ${props.filterId} not found`)
+}
+
+const components = computed(() => {
+  return filterDefinition.extensions.components || []
+})
+</script>
+
+<template>
+  <div class="filter-container">
+    <div class="filter-title">
+      {{ filterDefinition.title }}
+    </div>
+    <div class="filter-components">
+      <FilterDisplayComponentRenderer
+        v-for="(component, index) in components"
+        :key="index"
+        :component="component"
+        :configured-filter-values="configuredValues"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.filter-title {
+  font-weight: var(--font-weight-bold);
+  font-size: var(--font-size-normal);
+}
+
+.filter-components {
+  width: 100%;
+  display: flex;
+  margin-top: var(--dimension-2);
+  flex-direction: column;
+  gap: var(--dimension-6);
+  justify-content: space-between;
+}
+</style>

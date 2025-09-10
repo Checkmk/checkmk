d225ed8bb0ff49397404ff48f046ff8d7a48c001
Wontek Hong <wontek.hong@checkmk.com>
2025-09-10 22:06:50 +0200

    filter: introduce CategoryFilterSelection component
    Change-Id: I3b8be4c4eff4a1ac3103a969b71882bead909ade

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/filter/utils.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+ * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+ * conditions defined in the file COPYING, which is part of this source code package.
+ */
+import type { FilterDefinition, FilterType } from './types.ts'
+
+export function parseFilterTypes(
+  filterDefsRecord: Record<string, FilterDefinition>,
+  categoryNames: Set<string>
+): Map<string, FilterType[]> {
+  const filtersByCategory = new Map<string, FilterDefinition[]>()
+
+  Object.values(filterDefsRecord).forEach((filter) => {
+    const categoryName = filter.extensions.info
+    if (!filtersByCategory.has(categoryName)) {
+      filtersByCategory.set(categoryName, [])
+    }
+    filtersByCategory.get(categoryName)!.push(filter)
+  })
+
+  const categories = new Map<string, FilterType[]>()
+
+  categoryNames.forEach((categoryName) => {
+    const categoryFilters = filtersByCategory.get(categoryName) || []
+
+    const sortedFilterElements = categoryFilters
+      .sort((a, b) => a.title!.localeCompare(b.title!, undefined, { sensitivity: 'base' }))
+      .map(
+        (filterDef): FilterType => ({
+          type: 'filter',
+          id: filterDef.id!,
+          title: filterDef.title!
+        })
+      )
+
+    categories.set(categoryName, sortedFilterElements)
+  })
+
+  return categories
+}

c47645bf5700378bb784ff208574d4b67d96f44e
Simon Jess <simon.jess@checkmk.com>
2025-09-10 10:58:18 +0200

    Migrate display hint 'networking > addresses'
    CMK-25075
    
    Change-Id: Ia8aef487fa6367c8197023999388c7387ba9a5df

--- cmk/plugins/collection/inventory_ui/networking.py
+++ cmk/plugins/collection/inventory_ui/networking.py
@@ -5,15 +5,28 @@
 
 from cmk.inventory_ui.v1_alpha import (
     DecimalNotation,
+    Label,
     Node,
     NumberField,
     StrictPrecision,
+    Table,
     TextField,
     Title,
     Unit,
 )
 
 UNIT_COUNT = Unit(DecimalNotation(""), StrictPrecision(0))
+
+
+def _render_ip_address_type(value: str) -> Label | str:
+    match value:
+        case "ipv4":
+            return "IPv4"
+        case "ipv6":
+            return "IPv6"
+        case _:
+            return value
+
 
 node_networking = Node(
     name="networking",
@@ -25,4 +38,17 @@ node_networking = Node(
         "total_ethernet_ports": NumberField(Title("Ports"), render=UNIT_COUNT),
         "available_ethernet_ports": NumberField(Title("Ports available"), render=UNIT_COUNT),
     },
+)
+
+node_networking_addresses = Node(
+    name="networking_addresses",
+    path=["networking", "addresses"],
+    title=Title("IP addresses"),
+    table=Table(
+        columns={
+            "address": TextField(Title("Address")),
+            "device": TextField(Title("Device")),
+            "type": TextField(Title("Address type"), render=_render_ip_address_type),
+        },
+    ),
 )

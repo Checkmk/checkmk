3932b29dce1578bbaa98afa755c994100310a1d1
Simon Jess <simon.jess@checkmk.com>
2025-09-10 10:54:24 +0200

    Migrate display hint 'hardware > volumes > physical_volumes'
    CMK-25075
    
    Change-Id: Iee849c2e49c472473c9228538346444205b76b41

--- cmk/plugins/collection/inventory_ui/hardware.py
+++ cmk/plugins/collection/inventory_ui/hardware.py
@@ -469,3 +469,20 @@ node_hardware_volumes = Node(
     path=["hardware", "volumes"],
     title=Title("Volumes"),
 )
+
+node_hardware_volumes_physical_volumes = Node(
+    name="hardware_volumes_physical_volumes",
+    path=["hardware", "volumes", "physical_volumes"],
+    title=Title("Physical volumes"),
+    table=Table(
+        columns={
+            "volume_group_name": TextField(Title("Volume group name")),
+            "physical_volume_name": TextField(Title("Physical volume name")),
+            "physical_volume_status": TextField(Title("Physical volume status")),
+            "physical_volume_total_partitions": TextField(
+                Title("Physical volume total partitions")
+            ),
+            "physical_volume_free_partitions": TextField(Title("Physical volume free partitions")),
+        },
+    ),
+)

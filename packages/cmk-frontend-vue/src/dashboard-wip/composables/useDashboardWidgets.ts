61c0f28e7e79f0e3a3a8d4e8339595d4bd5daf22
Christopher Krause <christopher.krause@checkmk.com>
2025-09-10 09:04:40 +0200

    dashboard/composables: introduce useDashboardWidgets
    Co-authored-by: Christopher Krause <christopher.krause@checkmk.com>
    Co-authored-by: Wontek Hong <wontek.hong@checkmk.com>
    
    Change-Id: I2022d24da2f45d85511f53d51abf6d08ffb69649

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/composables/useDashboardWidgets.ts
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+ * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+ * conditions defined in the file COPYING, which is part of this source code package.
+ */
+import { type Ref, computed } from 'vue'
+
+import type { ConfiguredFilters } from '@/dashboard-wip/components/filter/types.ts'
+import type {
+  RelativeGridWidgets,
+  ResponsiveGridWidgets,
+  WidgetContent,
+  WidgetFilterContext,
+  WidgetGeneralSettings,
+  WidgetRelativeGridLayout,
+  WidgetResponsiveGridLayouts
+} from '@/dashboard-wip/types/widget'
+
+export interface WidgetCore {
+  widget_id: string
+  general_settings: WidgetGeneralSettings
+  content: WidgetContent
+  filter_context: WidgetFilterContext
+}
+
+export function useDashboardWidgets(
+  widgetsRef: Ref<RelativeGridWidgets | ResponsiveGridWidgets | undefined>
+) {
+  const widgetCores = computed<Record<string, WidgetCore>>(() => {
+    const widgets = widgetsRef.value
+    if (!widgets) {
+      return {}
+    }
+    const record: Record<string, WidgetCore> = {}
+    for (const [widgetId, widget] of Object.entries(widgets)) {
+      record[widgetId] = {
+        widget_id: widgetId,
+        general_settings: widget.general_settings,
+        filter_context: widget.filter_context,
+        content: widget.content
+      }
+    }
+    return record
+  })
+
+  function getWidget(widgetId: string): WidgetCore | null {
+    return widgetCores.value[widgetId] || null
+  }
+
+  function addWidget(
+    id: string,
+    content: WidgetContent,
+    generalSettings: WidgetGeneralSettings,
+    configuredFilters: ConfiguredFilters,
+    layout: WidgetResponsiveGridLayouts | WidgetRelativeGridLayout
+  ) {
+    const widgets = widgetsRef.value
+    if (!widgets) {
+      throw new Error('Cannot add widget: widgetsRef is undefined')
+    }
+    if (widgets[id]) {
+      throw new Error(`Widget with ID '${id}' already exists`)
+    }
+
+    widgets[id] = {
+      general_settings: generalSettings,
+      content,
+      filter_context: {
+        uses_infos: [],
+        // @ts-expect-error TODO: change configuredFilters to be <string, string> only
+        filters: configuredFilters
+      },
+      layout
+    }
+  }
+
+  function deleteWidget(widgetId: string) {
+    const widgets = widgetsRef.value
+    if (!widgets) {
+      throw new Error('Cannot delete widget: widgetsRef is undefined')
+    }
+    if (widgets[widgetId]) {
+      delete widgets[widgetId]
+    }
+  }
+
+  // TODO: edit method should be added
+
+  return {
+    widgetCores,
+    getWidget,
+    addWidget,
+    deleteWidget
+  }
+}
+
+export type DashboardWidgets = ReturnType<typeof useDashboardWidgets>

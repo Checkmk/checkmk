50819d37af635c9cd59207145d19dc9b7ea9c544
Simon Jess <simon.jess@checkmk.com>
2025-09-10 10:37:10 +0200

    Migrate display hint 'hardware > components > psus'
    CMK-25075
    
    Change-Id: Ia356008cdbd5bb9b8b4829801c65290f85e99311

--- cmk/plugins/collection/inventory_ui/hardware.py
+++ cmk/plugins/collection/inventory_ui/hardware.py
@@ -154,6 +154,25 @@ node_hardware_components_others = Node(
     ),
 )
 
+node_hardware_components_psus = Node(
+    name="hardware_components_psus",
+    path=["hardware", "components", "psus"],
+    title=Title("Power supplies"),
+    table=Table(
+        view=View(name="invpsu", title=Title("Power supplies")),
+        columns={
+            "index": TextField(Title("Index")),
+            "name": TextField(Title("Name")),
+            "description": TextField(Title("Description")),
+            "software": TextField(Title("Software")),
+            "serial": TextField(Title("Serial number")),
+            "manufacturer": TextField(Title("Manufacturer")),
+            "model": TextField(Title("Model name")),
+            "location": TextField(Title("Location")),
+        },
+    ),
+)
+
 node_hardware_memory = Node(
     name="hardware_memory",
     path=["hardware", "memory"],

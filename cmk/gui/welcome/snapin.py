0017971db895bedd2ac3e5dedfa713db6a77b162
Alejandro Yunta <alejandro.yunta@checkmk.com>
2025-09-11 16:36:42 +0200

    Fix unexpected 'is_start_url' prop warning in WelcomeSnapin
    Change-Id: I16568a671b44dfc5435d315699be8c6331d92ac2

--- cmk/gui/welcome/snapin.py
+++ cmk/gui/welcome/snapin.py
@@ -51,4 +51,6 @@ class SidebarWelcomeSnapin(SidebarSnapin):
 
     @override
     def show(self, config: Config) -> None:
-        html.vue_component("cmk-welcome-snapin", data=asdict(get_welcome_data()))
+        snapin_props = asdict(get_welcome_data())
+        snapin_props.pop("is_start_url", None)
+        html.vue_component("cmk-welcome-snapin", data=snapin_props)

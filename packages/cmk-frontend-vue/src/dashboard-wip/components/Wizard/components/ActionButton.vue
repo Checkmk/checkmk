cb04f1f40b58362d5bf31c041f32ecea31369d07
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-09-12 16:10:16 +0200

    dashboard: Migrate Actionbar and Action button
    Move components to final location
    
    Change-Id: I16aaf98fcc9a3d9ef7409a8f7726c6f75228edb6

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/Wizard/components/ActionButton.vue
@@ -0,0 +1,35 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+import type { TranslatedString } from '@/lib/i18nString'
+
+import type { ButtonVariants } from '@/components/CmkButton.vue'
+import CmkButton from '@/components/CmkButton.vue'
+import CmkIcon from '@/components/CmkIcon.vue'
+
+interface ActionButtonIcon {
+  name: string
+  side?: 'left' | 'right'
+  rotate?: number
+}
+
+interface ActionButtonProps {
+  label: TranslatedString
+  variant: ButtonVariants['variant']
+  icon?: ActionButtonIcon
+  action: () => void
+}
+
+defineProps<ActionButtonProps>()
+</script>
+
+<template>
+  <CmkButton :variant="variant" @click="action">
+    <span v-if="!icon || icon?.side === 'right'">{{ label }}{{ '\xa0' }}</span>
+    <CmkIcon v-if="icon" :name="icon.name" :rotate="icon.rotate || 0" variant="inline" />
+    <span v-if="icon?.side === 'left'">{{ '\xa0' }}{{ label }}</span>
+  </CmkButton>
+</template>

9397aacb6118768620a5658868f29241ec383e78
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-09-12 16:10:16 +0200

    dashboard: Migrate Actionbar and Action button
    Move components to final location
    
    Change-Id: I16aaf98fcc9a3d9ef7409a8f7726c6f75228edb6

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/Wizard/components/ActionBar.vue
@@ -0,0 +1,33 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+interface ActionBarProps {
+  alignItems?: 'left' | 'center' | 'right'
+}
+
+const props = withDefaults(defineProps<ActionBarProps>(), {
+  alignItems: 'left'
+})
+
+const flexAlignmentMap = {
+  left: 'flex-start',
+  center: 'center',
+  right: 'flex-end'
+}
+</script>
+
+<template>
+  <div class="db-action-bar" :style="`justifyContent: ${flexAlignmentMap[props.alignItems]}`">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.db-action-bar {
+  display: flex;
+  gap: var(--spacing-double);
+}
+</style>

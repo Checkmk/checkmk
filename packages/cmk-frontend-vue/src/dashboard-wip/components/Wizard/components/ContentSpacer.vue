4e914fda692add278b28c9b81ce72821cd448de8
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-09-12 16:14:51 +0200

    dashboard: auxiliar Wizard components
    Added auxiliar components to their final location
    
    Change-Id: Ifd776cd02c606921b70040452c2048816d36d4e2

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/Wizard/components/ContentSpacer.vue
@@ -0,0 +1,40 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface ContentSpacerProps {
+  variant?: 'blank' | 'line'
+  height?: number
+}
+
+const props = withDefaults(defineProps<ContentSpacerProps>(), {
+  variant: 'blank',
+  height: 20
+})
+
+const pixelHeight = computed(() => `${props.height}px`)
+</script>
+
+<template>
+  <div class="content-spacer">
+    <hr v-if="variant === 'line'" class="content-spacer__horizontal-line" />
+  </div>
+</template>
+
+<style scoped>
+.content-spacer {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  min-height: v-bind(pixelHeight);
+}
+
+.content-spacer__horizontal-line {
+  width: 100%;
+  border-color: var(--ux-theme-2);
+}
+</style>
---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/Wizard/components/StepsHeader.vue
@@ -0,0 +1,53 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+import type { TranslatedString } from '@/lib/i18nString'
+
+import CmkIconButton from '@/components/CmkIconButton.vue'
+import CmkLabel from '@/components/CmkLabel.vue'
+
+interface BackButtonProps {
+  title: TranslatedString
+  subtitle?: TranslatedString
+}
+
+const emit = defineEmits(['back'])
+
+defineProps<BackButtonProps>()
+</script>
+
+<template>
+  <div class="back-button__container">
+    <div class="back-button__icon">
+      <CmkIconButton
+        name="back"
+        variant="plain"
+        size="xxlarge"
+        :colored="false"
+        @click="() => emit('back')"
+      />
+    </div>
+    <div class="back-button__label">
+      <CmkLabel variant="title">{{ title }}</CmkLabel>
+      <CmkLabel v-if="subtitle" variant="subtitle">{{ subtitle }}</CmkLabel>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.back-button__icon {
+  position: relative;
+  left: -3px;
+}
+
+.back-button__container {
+  display: flex;
+}
+
+.back-button__label {
+  padding-left: var(--spacing);
+}
+</style>

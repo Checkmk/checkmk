55441d251c742aa4f4141696f17771ce3957ea68
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-09-12 16:05:44 +0200

    dashboard: adjust types for GraphTimeRange component
    Adjusted types and added composable which provides needed references and
    produces widget data
    
    Change-Id: Ib4d61cb0e374e9ce863bdadbe83c30d3a1e462b8

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/TimeRange/types.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+ * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+ * conditions defined in the file COPYING, which is part of this source code package.
+ */
+import type { components } from '@/lib/rest-api-client/openapi_internal'
+
+export type TimerangeModel = components['schemas']['TimerangeModel']
+export type PreDefinedTimeRange = components['schemas']['_Predefined']

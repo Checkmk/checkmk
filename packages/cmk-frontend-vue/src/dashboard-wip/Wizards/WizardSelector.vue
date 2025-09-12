00fae5afba233595de96a3435f778bc74cbd5765
Sebastian Groemcke <sebastian.groemcke@checkmk.com>
2025-09-12 16:31:08 +0200

    dashboard: add WizardSelector
    Change-Id: I7643e57dc899da20f00d068ddac8ca572f29a551

---
+++ packages/cmk-frontend-vue/src/dashboard-wip/Wizards/WizardSelector.vue
@@ -0,0 +1,32 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+<script setup lang="ts">
+import type { DashboardConstants } from '@/dashboard-wip/types/dashboard.ts'
+
+interface AllWizardsProps {
+  selectedWizard: string
+  dashboardConstants: DashboardConstants
+  dashboardName: string
+  dashboardOwner: string
+}
+
+const emit = defineEmits(['back-button'])
+
+const props = defineProps<AllWizardsProps>()
+
+// @ts-expect-error: TODO: use this
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const handleGoBack = () => {
+  emit('back-button')
+}
+</script>
+
+<template>
+  <div v-if="!props.selectedWizard"></div>
+  <div v-else>
+    <div />
+  </div>
+</template>

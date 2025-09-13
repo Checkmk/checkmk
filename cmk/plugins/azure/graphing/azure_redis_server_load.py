f5001082ad85e6b55472abcf7a14be471ccda0a6
Rick Elrod <rick.elrod@checkmk.com>
2025-09-13 13:51:07 +0200

    Azure Redis: Server load: Add missing perfometer
    CMK-25915
    
    Change-Id: I16c25ebd7d4a14e9530a34f3d703abdfd34e989a

--- cmk/plugins/azure/graphing/azure_redis_server_load.py
+++ cmk/plugins/azure/graphing/azure_redis_server_load.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.graphing.v1 import metrics, Title
+from cmk.graphing.v1 import metrics, perfometers, Title
 
 UNIT_PERCENTAGE = metrics.Unit(metrics.DecimalNotation("%"))
 
@@ -14,9 +14,14 @@ metric_azure_redis_server_load = metrics.Metric(
     color=metrics.Color.LIGHT_GREEN,
 )
 
-metric_azure_redis_server_load_average = metrics.Metric(
-    name="azure_redis_server_load_average",
-    title=Title("Server load (average)"),
-    unit=UNIT_PERCENTAGE,
-    color=metrics.Color.LIGHT_BLUE,
+# We show the instantaneous number here even if averaging is enabled. Probably
+# non-ideal but otherwise we'd have to emit a third metric that always gets
+# updated whether or not averaging was enabled, and then we'd get extra graphs.
+perfometer_azure_redis_server_load = perfometers.Perfometer(
+    name="azure_redis_server_load",
+    focus_range=perfometers.FocusRange(
+        perfometers.Closed(0),
+        perfometers.Closed(100.0),
+    ),
+    segments=["azure_redis_server_load"],
 )

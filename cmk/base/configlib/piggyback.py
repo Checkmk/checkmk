3381d18206875cf4e9d2534ed09410a483fe944b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-09-15 13:09:59 +0200

    piggyback: detach helper from config_cache
    Change-Id: I59dbb9bd0fa2cfb281d2eadc55b3d344cd50361b

--- cmk/base/configlib/piggyback.py
+++ cmk/base/configlib/piggyback.py
@@ -7,11 +7,39 @@
 from collections.abc import Callable, Iterable
 
 from cmk.ccc.hostaddress import HostName
-from cmk.piggyback.backend import parse_flattened_piggyback_time_settings, PiggybackTimeSettings
+from cmk.piggyback.backend import (
+    get_current_piggyback_sources_of_host,
+    parse_flattened_piggyback_time_settings,
+    PiggybackTimeSettings,
+)
 from cmk.utils.labels import Labels
+from cmk.utils.paths import omd_root
 from cmk.utils.rulesets.ruleset_matcher import RulesetMatcher
 
 from .loaded_config import LoadedConfigFragment
+
+
+def guess_piggybacked_hosts_time_settings(
+    loaded_config: LoadedConfigFragment,
+    matcher: RulesetMatcher,
+    labels_of_host: Callable[[HostName], Labels],
+    piggybacked_hostname: HostName,
+) -> PiggybackTimeSettings:
+    # NOTE: piggyback time settings are configured in rules matching on the source hosts,
+    # but applied when dealing with the destination hosts (aka piggybacked hosts) in the
+    # fetcher and the summarizer.
+    # This *guesses* which rulesets are relevant, by matching on the hosts that *currently*
+    # provide piggyback data for the given piggybacked host.
+    # For the fetcher, this function is evaluated at config creation time, so this might
+    # well be wrong.
+    return make_piggyback_time_settings(
+        loaded_config,
+        matcher,
+        labels_of_host,
+        source_host_names=sorted(
+            get_current_piggyback_sources_of_host(omd_root, piggybacked_hostname)
+        ),
+    )
 
 
 def make_piggyback_time_settings(

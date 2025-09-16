f2bef7c5b3d7cb52acfe19e97b54a3bb59de510e
Andreas Boesl <andreas.boesl@checkmk.com>
2025-09-16 10:50:00 +0200

    made alternative_utils.py more robust. (improved boolean/tuple support)
    Change-Id: Id9313f3129027acdcde1a147d0782405bc1ee0e6

--- cmk/gui/form_specs/generators/alternative_utils.py
+++ cmk/gui/form_specs/generators/alternative_utils.py
@@ -4,29 +4,35 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 from typing import Any
 
-from cmk.gui.form_specs.converter import TransformDataForLegacyFormatOrRecomposeFunction
+from cmk.ccc.exceptions import MKGeneralException
+from cmk.gui.form_specs.converter import TransformDataForLegacyFormatOrRecomposeFunction, Tuple
 from cmk.gui.form_specs.private import SingleChoiceExtended
 from cmk.gui.form_specs.vue import DEFAULT_VALUE, get_visitor, VisitorOptions
-from cmk.rulesets.v1.form_specs import CascadingSingleChoice, SingleChoice
+from cmk.rulesets.v1.form_specs import CascadingSingleChoice, FormSpec, SingleChoice
 
 
-def _get_type_of_object_as_string(value: object) -> str:
+def _get_type_of_object_as_string(
+    value: object, form_spec_type_hint: FormSpec[Any] | None = None
+) -> str:
     """
     Returns a string representation of the type of the given object.
     This is used to create a mapping for the CascadingSingleChoice elements.
     """
     if value is None:
         return "None"
+    if isinstance(form_spec_type_hint, SingleChoice | SingleChoiceExtended):
+        return "str"
+    if isinstance(form_spec_type_hint, Tuple) or isinstance(value, tuple):
+        assert isinstance(value, tuple | list)
+        return f"tuple[{', '.join(_get_type_of_object_as_string(v) for v in value)}]"
     if isinstance(value, bool):
-        return "bool"
+        return f"bool:{value}"
     if isinstance(value, int):
         return "int"
     if isinstance(value, str):
         return "str"
     if isinstance(value, list):
         return "list"
-    if isinstance(value, tuple):
-        return f"tuple[{', '.join(_get_type_of_object_as_string(v) for v in value)}]"
     if isinstance(value, dict):
         return "dict"
     raise TypeError(f"Unsupported type: {type(value)}")
@@ -54,25 +60,31 @@ def enable_deprecated_alternative(
 
     mapping: dict[str, str] = {}
     for element in wrapped_form_spec.elements:
-        if isinstance(element.parameter_form, SingleChoice | SingleChoiceExtended):
-            # A single choice reports None as default value
-            # A SingleChoiceExtended might report all kinds of data types, but this is not supported here
-            mapping[_get_type_of_object_as_string("")] = element.name
-            continue
-
         visitor = get_visitor(
             element.parameter_form, VisitorOptions(migrate_values=False, mask_values=False)
         )
-        name_for_type = _get_type_of_object_as_string(visitor.to_vue(DEFAULT_VALUE)[1])
-        mapping[name_for_type] = element.name
+
+        # Try to determine the best fitting value
+        # to_disk obviously returns the value as it would be stored on disk
+        # since to_disk may raise a MKGeneralException with the DEFAULT_VALUE,
+        # we use the to_vue method as fallback
+        try:
+            example_value_for_mapping = visitor.to_disk(DEFAULT_VALUE)
+        except MKGeneralException:
+            example_value_for_mapping = visitor.to_vue(DEFAULT_VALUE)[1]
+
+        mapping[
+            _get_type_of_object_as_string(
+                example_value_for_mapping, form_spec_type_hint=element.parameter_form
+            )
+        ] = element.name
 
     def to_disk(value: object) -> object:
         assert isinstance(value, tuple) and len(value) == 2
         return value[1]
 
     def from_disk(value: object) -> tuple[str, Any]:
-        name_for_type = _get_type_of_object_as_string(value)
-        return mapping[name_for_type], value
+        return mapping[_get_type_of_object_as_string(value)], value
 
     return TransformDataForLegacyFormatOrRecomposeFunction(
         wrapped_form_spec=wrapped_form_spec,

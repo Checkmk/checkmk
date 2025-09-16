93f4aa1fa60fdacfa1d5a16000660673ddf71aa7
Andreas Doehler <andreas.doehler@gmail.com>
2025-09-16 17:04:38 +0200

    handle missing voltage data
    No Werk, since this is purely informational message.
    
    CMK-26048
    
    Closes: #853
    Change-Id: I5f29ccfd06c89c475dd46698c9a6c78d452dc283

--- cmk/plugins/redfish/agent_based/redfish_voltage.py
+++ cmk/plugins/redfish/agent_based/redfish_voltage.py
@@ -67,6 +67,8 @@ def check_redfish_voltage(item: str, section: RedfishAPIData) -> CheckResult:
             render_func=lambda v: f"{v:.1f} V",
             boundaries=perfdata.boundaries,
         )
+    else:
+        yield Result(state=State(0), summary="No voltage data found")
 
     dev_state, dev_msg = redfish_health_state(voltage.get("Status", {}))
     yield Result(state=State(dev_state), notice=dev_msg)

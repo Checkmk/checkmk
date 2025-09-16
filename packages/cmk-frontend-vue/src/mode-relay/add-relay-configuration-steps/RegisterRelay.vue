ae6a7a392c0c87ff4b3b66083d9f34e7e2b2a3dc
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-09-16 13:07:37 +0200

    feat(relay) add CmkWizard for mode-relay
    CMK-26006
    
    Change-Id: I86af59a1ddbc266d8741e279c3469897b9ca26cc

---
+++ packages/cmk-frontend-vue/src/mode-relay/add-relay-configuration-steps/RegisterRelay.vue
@@ -0,0 +1,21 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+
+<script setup lang="ts">
+import usei18n from '@/lib/i18n'
+
+import CmkParagraph from '@/components/typography/CmkParagraph.vue'
+
+const { _t } = usei18n()
+</script>
+
+<template>
+  <CmkParagraph>
+    {{
+      _t('This is a placeholder for the relay setup wizard. The actual implementation is pending.')
+    }}
+  </CmkParagraph>
+</template>

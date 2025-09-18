a6a37b036f7a06892dfe63ba88a7988e801ff921
Ramadan Khalifa <ramadan.khalifa@checkmk.com>
2025-09-18 15:33:09 +0200

    Revert "18358 OTel: Enable OpenTelemetry collector configuration via UI"
    This reverts commit 9556ea25df1a86a3d3f442ecf4ce0fb08fc98efa.
    
    Reason for revert: integration test fail
    
    Change-Id: I87c03dec91591ea32a11a727219af87fc48341d6

--- tests/unit/cmk/gui/watolib/test_watolib.py
+++ tests/unit/cmk/gui/watolib/test_watolib.py
@@ -217,7 +217,6 @@ def test_registered_configvars() -> None:
         "load_frontend_vue",
         "site_trace_send",
         "site_trace_receive",
-        "site_opentelemetry_collector",
     ]
 
     if cmk_version.edition(paths.omd_root) is not cmk_version.Edition.CRE:
@@ -322,7 +321,6 @@ def test_registered_configvar_groups() -> None:
             "Reporting",
             "Monitoring core",
             "Ntopng (chargeable add-on)",
-            "Application Monitoring",
         ]
 
     registered = sorted(config_variable_group_registry.keys())

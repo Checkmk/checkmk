db56304fc4fe260cc9e7e95846486730a465d95b
Rick Elrod <rick.elrod@checkmk.com>
2025-09-18 20:51:22 +0200

    Windows time: Fix prototype agent plugin command
    The parser expects /verbose now, update the plugin command
    accordingly, and add license header.
    
    Change-Id: I986bb0b6a6cc777002b91e15d364b526cd2390ef

--- agents/windows/plugins/w32time_status.ps1
+++ agents/windows/plugins/w32time_status.ps1
@@ -1,3 +1,7 @@
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
 $service = Get-Service -Name w32time -ErrorAction SilentlyContinue
 if ($service -eq $null -or $service.Status -ne 'Running') {
     return
@@ -5,7 +9,7 @@ if ($service -eq $null -or $service.Status -ne 'Running') {
 
 Write-Host "<<<w32time_status:sep(0)>>>"
 try {
-    $output = & w32tm /query /status 2>&1
+    $output = & w32tm /query /status /verbose 2>&1
     if ($LASTEXITCODE -eq 0) {
         $output
     } else {

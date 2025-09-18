0370e39dda04804b706692d18898c84460550523
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2025-09-18 17:38:23 +0200

    Add command line script to lookup passwords in store
    CMK-25615
    
    Change-Id: I9125a8a2ca5bbd0b412f3017d46f39bde8a00839
    (cherry picked from commit 513da05f3e7476e754ca02543fbd7fe51873b99e)

---
+++ cmk/utils/password_store/cli.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import argparse
+import sys
+from collections.abc import Sequence
+
+from cmk.utils.password_store import lookup, pending_password_store_path
+
+
+def _parse_args(args: Sequence[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="""
+Retrieve a password from the Checkmk password store.
+"""
+    )
+    parser.add_argument(
+        "--lookup",
+        required=True,
+        type=str,
+        metavar="PW_ID",
+        help="The ID of the password to retrieve from the password store",
+    )
+    return parser.parse_args(args)
+
+
+def main(args: Sequence[str]) -> int:
+    parsed_args = _parse_args(args)
+
+    try:
+        password = lookup(pending_password_store_path(), parsed_args.lookup)
+    except ValueError as e:
+        sys.stderr.write(f"cmk-passwordstore: {e}\n")
+        return 1
+    sys.stdout.write(password)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))

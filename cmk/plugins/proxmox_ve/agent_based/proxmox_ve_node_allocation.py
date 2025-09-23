1cf64ab9561259d2552e28acc9e75088f25f119b
Luka Racic <luka.racic@checkmk.com>
2025-09-23 09:46:28 +0200

    proxmox_ve_node_allocation: Fix parsing issue
    Change-Id: Ifec921d57ea704a468ac9d0c9c1b43b52b9aba15

--- cmk/plugins/proxmox_ve/agent_based/proxmox_ve_node_allocation.py
+++ cmk/plugins/proxmox_ve/agent_based/proxmox_ve_node_allocation.py
@@ -2,14 +2,14 @@
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
+import json
 
 from cmk.agent_based.v2 import AgentSection, StringTable
 from cmk.plugins.proxmox_ve.lib.node_allocation import SectionNodeAllocation
 
 
 def parse_proxmox_ve_node_allocation(string_table: StringTable) -> SectionNodeAllocation:
-    return SectionNodeAllocation.model_validate_json(string_table[0][0])
+    return SectionNodeAllocation.model_validate_json(json.loads(string_table[0][0]))
 
 
 agent_section_proxmox_ve_node_allocation = AgentSection(

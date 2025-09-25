e98690600906b42f82d5a30a323f830ff592b51c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-09-25 15:01:58 +0200

    bakery: fix representation issue
    Change-Id: I65d0c505e1a4b798cf38bda484f0f9c4bd00d479

--- packages/cmk-plugin-apis/cmk/bakery/v2_alpha/_config.py
+++ packages/cmk-plugin-apis/cmk/bakery/v2_alpha/_config.py
@@ -20,20 +20,23 @@ class Secret(NamedTuple):
     Example:
 
         # This is passed by the backend to the bakery plugin
-        >>> s = Secret("s3cr3t")
+        >>> s = Secret("s3cr3t", "", "")
 
         >>> print(f"This is the secret as string: {s}")
         This is the secret as string: 4e738ca5563c06cfd0018299933d58db1dd8bf97f6973dc99bf6cdc64b5550bd
 
-        >>> print(f"This is the secrets `repr`: {s!r}")
-        This is the secrets `repr`: Secret('4e738ca5563c06cfd0018299933d58db1dd8bf97f6973dc99bf6cdc64b5550bd')
-
         >>> print(f"But we can see the actual value: {s.revealed!r}")
         But we can see the actual value: 's3cr3t'
+
+        # Deviating from what we'd ususally expect from `repr`, this deliberately does not show the actual value:
+        >>> s.revealed in repr(s)
+        False
 
     """
 
     revealed: str
+    source: str
+    id: str
 
     def _hash(self) -> str:
         return hashlib.sha256(self.revealed.encode("utf-8")).hexdigest()
@@ -42,10 +45,16 @@ class Secret(NamedTuple):
         return self._hash()
 
     def __repr__(self) -> str:
-        """Mask the actual value of the secret
+        """*UNSTABLE* Masks the actual value of the secret
 
         This deliberately breaks the semantics of the `repr` function.
+        The exact format of this string may change.
         """
-        # The backend uses the `repr` function (pprint) to create a hash of the secret,
-        # so make sure to not return something constant here.
-        return f"Secret('{self._hash()}')"
+        # The backend uses the `repr` function (via pprint) to create a hash of the agent configuration,
+        # so make sure to not return something constant here. Otherwise a changed secret will not trigger
+        # a rebake.
+        # Worse: In the bakery backend we try to `literal_eval` the resulting string,
+        # in oder to render the configuration to the user.
+        # As a result, the repr here needs to be evalable, and be compatible with the password model
+        # that the backend uses to render passwords in the UI.
+        return repr(("cmk_postprocessed", self.source, (self.id, self._hash())))

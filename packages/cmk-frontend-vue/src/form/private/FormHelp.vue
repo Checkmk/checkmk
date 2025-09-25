495b278c58014eb0ac2d8e68da28f24424cd9472
Benedikt Seidl <benedikt.seidl@checkmk.com>
2025-09-25 17:14:48 +0200

    inline global styles from old frontend: FormHelp
    CMK-23566
    
    Change-Id: I65084f8b3232ed78d8bc834f2414f7ee9939a5f9

--- packages/cmk-frontend-vue/src/form/private/FormHelp.vue
+++ packages/cmk-frontend-vue/src/form/private/FormHelp.vue
@@ -13,17 +13,33 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="help" class="help form-help__container">
-    <div class="info_icon">
+  <div v-if="help" class="form-help__container">
+    <div class="form-help__info-icon">
       <CmkIcon name="info" size="small" />
     </div>
-    <div class="help_text"><CmkHtml :html="help" /></div>
+    <div class="form-help__help-text"><CmkHtml :html="help" /></div>
   </div>
 </template>
 
 <style scoped>
+.form-help__info-icon {
+  display: flex;
+  align-items: center;
+  background-color: #0667c1;
+  border-radius: 4px 0 0 4px;
+  padding: 5px;
+}
+
+.form-help__help-text {
+  flex-grow: 1;
+  padding: 10px;
+  background-color: #11181d;
+  border-radius: 0 4px 4px 0;
+}
+
 div.form-help__container {
   display: none;
+  margin: 8px 0;
 }
 
 /* the old frontend sets a class on the body to hide or show the inline help */

a269201e21230f63912606e41f430d090f6ee8a9
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-09-26 08:48:47 +0200

    config generation: renamings
    We want to properly distinguish relay _engine_
    and relay _fetcher_ configs.
    The former `HelperConfigWriter` can also be
    used for other configs, not only 'Helpers'.
    
    Change-Id: I69880a86e07458081a85e61a3a63b499991207a7

--- cmk/base/core_factory.py
+++ cmk/base/core_factory.py
@@ -46,10 +46,10 @@ def create_core(
             )
             from cmk.base.core.cee.cmc import (  # type: ignore[import-not-found, import-untyped, unused-ignore]
                 CmcPb,
-                HelperConfigWriter,
+                ConfigWriterInterface,
             )
 
-            helper_config_writers: list[HelperConfigWriter] = [
+            helper_config_writers: list[ConfigWriterInterface] = [
                 make_fetcher_config_writer(
                     edition,
                     loaded_config,
@@ -62,11 +62,11 @@ def create_core(
 
             if edition in (Edition.CCE, Edition.CME, Edition.CSE):
                 from cmk.base.configlib.cce.relay import (  # type: ignore[import-not-found, import-untyped, unused-ignore]
-                    make_relay_config_writer,
+                    make_relay_fetcher_config_writer,
                 )
 
                 helper_config_writers.append(
-                    make_relay_config_writer(
+                    make_relay_fetcher_config_writer(
                         loaded_config, config_cache, label_manager, plugins, snmp_plugin_store
                     )
                 )

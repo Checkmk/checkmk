3637b56d127b69d088742c326ae73ce3452cec8a
Max Linke <max.linke@checkmk.com>
2025-09-29 15:28:10 +0200

    introduce bazel testlib component for relay-engine
    Generally with the component test approach (black box testing of stuff that
    runs in a process) we write a lot of helper tooling.
    In the tests folder it's kind of weird. testlib is similar to tests/testlib.
    
    Change-Id: Ib91b7bec133e9c82f0219b1b6affb1ba02da70b7

--- tests/code_quality/test_requirements.py
+++ tests/code_quality/test_requirements.py
@@ -159,6 +159,8 @@ def iter_sourcefiles(basepath: Path) -> Iterable[Path]:
             continue
         if sub_path.name == "tests":
             continue
+        if "testlib" in sub_path.name:
+            continue
         if sub_path.name.startswith("."):
             continue
         if sub_path.is_file() and is_python_file(sub_path):

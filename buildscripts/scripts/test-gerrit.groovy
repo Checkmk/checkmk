52c87197c5fc80a179f2fb6307efe125089e3e19
Niko Wenselowski <niko.wenselowski@checkmk.com>
2025-10-02 12:01:26 +0200

    Don't run Python Werks Test CV on k8s
    Fetching the tags leads to OOM kills.
    
    References CMK-25972
    
    References CMK-26379
    
    Change-Id: Ia3a39169d63221ef78f4dcd023be306ae0735235

--- buildscripts/scripts/test-gerrit.groovy
+++ buildscripts/scripts/test-gerrit.groovy
@@ -147,6 +147,7 @@ def main() {
                         case "Package cmk-ccc":             // Seems to require non-root permissions, see CMK-24586
                         case "Package cmk-update-agent":    // Failing tests in k8s, detailed analysis outstanding
                         case "Package mk-oracle":           // Runs integration tests in CV, wants to install libaio
+                        case "Python Werks Test":           // Get's OOM killed, see CMK-26379
                             relative_job_name = "${branch_base_folder}/cv/test-gerrit-single";
                             break;
                         default:

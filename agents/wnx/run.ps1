52cb4bd5b05d5c5c8b770427d9855b6d57783fd6
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-10-02 16:24:39 +0200

    wa: test msi signing
    If not signed - RED
    
    Change-Id: I4cb5dd5cf4ba40b057e1aafb0a4013e9034d64f6

--- agents/wnx/run.ps1
+++ agents/wnx/run.ps1
@@ -680,6 +680,7 @@ function Test-MsiSigning($file) {
     }
     catch {
         Write-Host "Failed to validate signing: $_" -ForegroundColor Red
+        throw
     }
     finally {
         Remove-Item -Path $random_dir -Recurse -Force -ErrorAction SilentlyContinue

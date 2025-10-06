0ca7a720868635a16eb5df688d66869201a23634
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-10-06 08:05:51 +0200

    Revert "feat: Send relay config tasks on agent receiver startup"
    This reverts commit d13625be3360b89194c565aeac5cc105243c9155.
    
    Reason for revert: breaks all heavy tests
    
    Change-Id: I8a7c33cd98fea656dd69da730d5f8e42a6e1eeda

--- packages/cmk-agent-receiver/cmk/agent_receiver/relay/api/dependencies/relays_repository.py
+++ packages/cmk-agent-receiver/cmk/agent_receiver/relay/api/dependencies/relays_repository.py
@@ -2,15 +2,10 @@
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Annotated
-
-import fastapi
-
-from cmk.agent_receiver.config import Config, get_config
+from cmk.agent_receiver.config import get_config
 from cmk.agent_receiver.relay.lib.relays_repository import RelaysRepository
 
 
-def get_relays_repository(
-    config: Annotated[Config, fastapi.Depends(get_config)],
-) -> RelaysRepository:
+def get_relays_repository() -> RelaysRepository:
+    config = get_config()
     return RelaysRepository.from_site(config.site_url, config.site_name)

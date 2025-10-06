79681cf2f3d62ce6f4cde1e730aa42ab25e92b6f
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-10-06 15:40:05 +0200

    clean up test_aws
    Change-Id: I68b981d12f1fc447e5f61e077921de5ff94d4671

--- tests/unit/cmk/gui/quick_setup/test_aws_params.py
+++ tests/unit/cmk/gui/quick_setup/test_aws_params.py
@@ -3,12 +3,10 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from unittest.mock import ANY
 
 from cmk.gui.quick_setup.config_setups.aws.stages import aws_transform_to_disk
 from cmk.plugins.aws.server_side_calls.aws_agent_call import special_agent_aws
-from cmk.server_side_calls.v1 import HostConfig
-from cmk.server_side_calls_backend.config_processing import process_configuration_to_parameters
+from cmk.server_side_calls.v1 import HostConfig, Secret
 
 QUICK_SETUP_PARAMS = {
     "access_key_id": "my_access_key",
@@ -44,7 +42,7 @@ QUICK_SETUP_PARAMS = {
     },
 }
 
-EXPECTED_RULE_PARAMS = {
+EXPECTED_RULE_CONFIG = {
     "auth": (
         "access_key",
         {
@@ -88,18 +86,53 @@ EXPECTED_RULE_PARAMS = {
 }
 
 
+PLUGIN_PARAMS = {
+    "auth": (
+        "access_key",
+        {
+            "access_key_id": "my_access_key",
+            "secret_access_key": Secret(1),
+        },
+    ),
+    "global_services": {
+        "ce": None,
+        "route53": None,
+        "cloudfront": {
+            "selection": "all",
+            "host_assignment": "aws_host",
+        },
+    },
+    "access": {},
+    "regions": ["eu-central-1"],
+    "regional_services": {
+        "ec2": {"selection": "all", "limits": True},
+        "ebs": {"selection": "all", "limits": True},
+        "s3": {"selection": "all", "limits": True},
+        "glacier": {"selection": "all", "limits": True},
+        "elb": {"selection": "all", "limits": True},
+        "elbv2": {"selection": "all", "limits": True},
+        "rds": {"selection": "all", "limits": True},
+        "cloudwatch_alarms": {"alarms": "all", "limits": True},
+        "dynamodb": {"selection": "all", "limits": True},
+        "wafv2": {"selection": "all", "limits": True, "cloudfront": None},
+        "lambda": {"selection": "all", "limits": True},
+        "sns": {"selection": "all", "limits": True},
+        "ecs": {"selection": "all", "limits": True},
+        "elasticache": {"selection": "all", "limits": True},
+    },
+    "piggyback_naming_convention": "ip_region_instance",
+    "overall_tags": [("foo", ["a", "b"])],
+    "import_tags": ("filter_tags", "foo:bar"),
+}
+
+
 def test_quick_setup_aws_transform_to_valuespec() -> None:
-    assert aws_transform_to_disk(QUICK_SETUP_PARAMS) == EXPECTED_RULE_PARAMS
+    assert aws_transform_to_disk(QUICK_SETUP_PARAMS) == EXPECTED_RULE_CONFIG
 
 
 def test_quick_setup_aws_to_ssc() -> None:
-    # GIVEN
-    qs_params = aws_transform_to_disk(QUICK_SETUP_PARAMS)
-    # this is an implementation detail of the backend and should be removed from this test.
-    params = process_configuration_to_parameters(qs_params, proxy_config=None, is_alpha=False)
-
     # WHEN
-    special_agent_calls = list(special_agent_aws(params.value, HostConfig(name="foo")))
+    special_agent_calls = list(special_agent_aws(PLUGIN_PARAMS, HostConfig(name="foo")))
 
     # THEN
     assert len(special_agent_calls) == 1
@@ -108,7 +141,7 @@ def test_quick_setup_aws_to_ssc() -> None:
         "--access-key-id",
         "my_access_key",
         "--secret-access-key-reference",
-        ANY,
+        Secret(1),
         "--regions",
         "eu-central-1",
         "--global-services",

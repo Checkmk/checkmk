0172667d2c71ed169af3495f221599f847a76afc
Rick Elrod <rick.elrod@checkmk.com>
2025-10-07 01:32:59 +0200

    csv bulk import refactor: Fold new class back in
    Fold the new CSVBulkImport class back into
    cmk.gui.wato.pages.bulk_import and combine unit tests into one file.
    
    Change-Id: Ib6e08519c35b0412356e4806cb3acdc14c02f0fb

--- cmk/gui/wato/pages/bulk_import.py
+++ cmk/gui/wato/pages/bulk_import.py
@@ -6,15 +6,16 @@
 single Setup folder. The hosts can either be provided by uploading a CSV file or
 by pasting the contents of a CSV file into a textbox."""
 
+import csv
 import itertools
 import operator
 import time
 import typing
 import uuid
-from collections.abc import Collection, Mapping, Sequence
+from collections.abc import Collection, Iterator, Mapping, Sequence
 from difflib import SequenceMatcher
 from pathlib import Path
-from typing import Any
+from typing import Any, TextIO
 
 import cmk.gui.pages
 from cmk.ccc import store
@@ -58,7 +59,6 @@ from cmk.gui.valuespec import (
 from cmk.gui.wato.pages.custom_attributes import ModeCustomHostAttrs
 from cmk.gui.wato.pages.folders import ModeFolder
 from cmk.gui.watolib import bakery
-from cmk.gui.watolib.csv_bulk_import import CSVBulkImport, get_handle_for_csv
 from cmk.gui.watolib.host_attributes import ABCHostAttribute, all_host_attributes, HostAttributes
 from cmk.gui.watolib.hosts_and_folders import Folder, folder_from_request
 from cmk.gui.watolib.mode import mode_url, ModeRegistry, redirect, WatoMode
@@ -201,6 +201,149 @@ def _host_rows_to_bulk(
             raise MKUserError(None, _("The host name attribute needs to be assigned to a column."))
 
         yield _host_name, typing.cast(HostAttributes, entry), None
+
+
+def _get_custom_csv_dialect(delim: str) -> type[csv.Dialect]:
+    class CustomCSVDialect(csv.excel):
+        delimiter = delim
+
+    return CustomCSVDialect
+
+
+def get_handle_for_csv(path: Path) -> TextIO:
+    """
+    Public function to attempt to open a CSV file with the correct encoding,
+    from the path given.
+    """
+    try:
+        return path.open(encoding="utf-8")
+    except OSError:
+        raise MKUserError(
+            None, _("Failed to read the previously uploaded CSV file. Please upload it again.")
+        )
+
+
+class CSVBulkImport:
+    def __init__(self, handle: TextIO, has_title_line: bool, delimiter: str | None = None):
+        self._handle = handle  # Take a handle instead of a Path for easier testing
+        self._dialect = self._determine_dialect(delimiter)
+        self._reader = csv.reader(self._handle, self._dialect)
+
+        self._num_fields: int | None = None
+        self._num_fields = self.row_length
+
+        self._has_title_line = has_title_line
+        self._title_row: list[str] | None = None
+        if self._has_title_line:
+            self._title_row = self.title_row
+
+    def _determine_dialect(self, delimiter: str | None) -> type[csv.Dialect]:
+        """
+        Attempt to return a csv.Dialect that works to parse the file.
+
+        Called only by the constructor: Calling this method later might manipulate the file cursor
+        and cause the instance to lose track of where it was in the file.
+        """
+        if delimiter is not None:
+            return _get_custom_csv_dialect(delimiter)
+
+        try:
+            dialect = csv.Sniffer().sniff(self._handle.read(2048), delimiters=",;\t:")
+        except csv.Error as e:
+            if "Could not determine delimiter" in str(e):
+                # Default to splitting on ;
+                dialect = _get_custom_csv_dialect(";")
+            else:
+                raise
+
+        self._handle.seek(0)
+        return dialect
+
+    def skip_to_and_return_next_row(self) -> list[str] | None:
+        """
+        Skip ahead to the next row that has data in it (if any). If there are no remaining
+        rows with data, return None.
+        """
+        for row in self._reader:
+            if row:
+                # This very function is called to determine the row length.
+                # In that case, self._num_fields won't be set yet.
+                if self._num_fields is not None and len(row) != self.row_length:
+                    raise MKUserError(
+                        None,
+                        _(
+                            "All rows in the CSV file must have the same number of columns. "
+                            "The following row had a different number of columns than the first "
+                            "row (or the title row, if one is present): %s"
+                        )
+                        % repr(row),
+                    )
+                return row
+        return None
+
+    def rows(self) -> Iterator[list[str]]:
+        while (next_row := self.skip_to_and_return_next_row()) is not None:
+            yield next_row
+        return
+
+    def __iter__(self) -> Iterator[list[str]]:
+        yield from self.rows()
+
+    @property
+    def row_length(self) -> int:
+        if self._num_fields is not None:
+            return self._num_fields
+
+        current_pos = self._handle.tell()
+        next_row = self.skip_to_and_return_next_row() or []
+        self._handle.seek(current_pos)
+        return len(next_row)
+
+    @property
+    def title_row(self) -> list[str] | None:
+        """
+        Return the title row, if one exists, taking care to only ever advance the reader
+        cursor once, even if called multiple times.
+        """
+        if not self._has_title_line:
+            # If we aren't expecting a title line and we are called anyway, do not
+            # advance the reader cursor.
+            return None
+
+        if self._title_row is not None:
+            # If we've already established the title row, then just return it.
+            return self._title_row
+
+        # TODO: Consider throwing if there is no next row
+        return self.skip_to_and_return_next_row()
+
+    @property
+    def has_title_line(self) -> bool:
+        return self._has_title_line
+
+    def rows_as_dict(self, attr_names: Sequence[str]) -> Iterator[dict[str, str]]:
+        """
+        Yield each row rendered as a dictionary with keys being the names given in attr_names
+        and values being the fields from the CSV.
+
+        In other words:
+        Given attr_names=["host_name", "ipaddress"]
+
+        ..and a row like:
+        "server01,192.168.100.1"
+
+        ...we would yield:
+        {"host_name": "server01", "ipaddress": "192.168.100.1"}.
+
+        Raises an exception if the number of attr_names differs from the number of fields.
+        """
+        if len(attr_names) != self.row_length:
+            raise ValueError(
+                f"Got {len(attr_names)} attribute names, but row length is {self.row_length}"
+            )
+
+        while (row := self.skip_to_and_return_next_row()) is not None:
+            yield dict(zip(attr_names, row))
 
 
 class ModeBulkImport(WatoMode):
--- tests/unit/cmk/gui/wato/pages/test_bulk_import.py
+++ tests/unit/cmk/gui/wato/pages/test_bulk_import.py
@@ -2,8 +2,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from io import StringIO
+from pathlib import Path
 from unittest import mock
 
 import pytest
@@ -18,10 +19,11 @@ from cmk.gui.wato.pages.bulk_import import (
     _detect_attribute,
     _host_rows_to_bulk,
     _prevent_reused_attr_names,
+    CSVBulkImport,
+    get_handle_for_csv,
     ImportTuple,
     ModeBulkImport,
 )
-from cmk.gui.watolib.csv_bulk_import import CSVBulkImport
 from cmk.gui.watolib.host_attributes import all_host_attributes
 from cmk.gui.watolib.hosts_and_folders import folder_tree
 from cmk.utils.tags import TagGroup
@@ -51,43 +53,6 @@ def attr_choices_with_tag_groups_and_host_attrs(tag_groups: Sequence[TagGroup]) 
     return _attribute_choices(tag_groups, host_attrs)
 
 
-CSV_NORMAL_WITH_TITLE = """
-thehostname,the_v4_ip_address
-alinux01,10.10.10.1
-alinux02,10.10.10.2
-alinux03,10.10.10.3
-""".strip()
-
-CSV_NORMAL_NO_TITLE = """
-alinux01,10.10.10.1
-alinux02,10.10.10.2
-alinux03,10.10.10.3
-""".strip()
-
-CSV_WITH_NEWLINES_AND_TITLE = """
-
-thehostname,the_v4_ip_address
-
-alinux01,10.10.10.1
-
-alinux02,10.10.10.2
-
-alinux03,10.10.10.3
-
-"""
-
-CSV_WITH_NEWLINES_NO_TITLE = """
-
-
-alinux01,10.10.10.1
-
-
-alinux02,10.10.10.2
-
-alinux03,10.10.10.3
-
-"""
-
 CSV_WITH_NON_ASCII_CHAR_IN_FIELD = """
 thehostname,the_v4_ip_address
 alinux01,10.10.10.1
@@ -109,27 +74,87 @@ alinux02-weird,ten...ten....ten....two
 alinux03,10.10.10.3
 """.strip()
 
+CSV_NO_TITLE_LINE = """
+server01,192.168.1.101
+server02,192.168.1.102
+server03,192.168.1.103
+""".strip()
+
+CSV_WITH_SEMICOLONS = CSV_NO_TITLE_LINE.replace(",", ";")
+
+# Multiple characters that "could" be a delimiter
+CSV_MULTI_DELIM = """
+2001:16c2:a123:2345:2ac4:c3e:6794:85cc,server01
+2001:16c2:a123:2345:2ac4:c3e:6794:85cd,server02
+2001:16c2:a123:2345:2ac4:c3e:6794:85ce,server03
+""".strip()
+
+CSV_WITH_TITLE_LINE = "hostname,ipaddr\n" + CSV_NO_TITLE_LINE
+CSV_WITH_TITLE_LINE_BLANK_LINES_BEFORE = ("\n" * 10) + CSV_WITH_TITLE_LINE
+
+CSV_WITH_ONE_LINE_NO_TITLE_LINE = "server01,192.168.1.101"
+
+# An edge-case sample where dialect sniffing doesn't work because the first 2048
+# characters don't have one of the default delimiters in them.
+EDGE_CASE_CSV_CANT_SNIFF = ("\n" * 2048) + CSV_NO_TITLE_LINE
+
+# An edge-case sample with scattered newlines throughout the rows
+EDGE_CASE_CSV_SCATTERED_NEWLINES_WITH_TITLE_LINE = """
+
+
+hostname,ipaddr
+
+server01,192.168.1.101
+
+
+server02,192.168.1.102
+server03,192.168.1.103
+
+"""
+
+EDGE_CASE_CSV_SCATTERED_NEWLINES_NO_TITLE_LINE = """
+
+
+server01,192.168.1.101
+
+
+server02,192.168.1.102
+server03,192.168.1.103
+
+"""
+
+EDGE_CASE_CSV_MALFORMED = """
+host_name,ipaddress
+server01;127.0.0.1
+server02;127.0.0.2
+"""
+
 
 @pytest.mark.parametrize(
     "csvtext, has_title_line",
     [
         pytest.param(
-            CSV_NORMAL_WITH_TITLE,
+            CSV_WITH_TITLE_LINE,
             True,
             id="normal csv with no blank lines and titles",
         ),
         pytest.param(
-            CSV_NORMAL_NO_TITLE,
+            CSV_NO_TITLE_LINE,
             False,
             id="normal csv with no blank lines and no titles",
         ),
         pytest.param(
-            CSV_WITH_NEWLINES_AND_TITLE,
+            CSV_WITH_TITLE_LINE_BLANK_LINES_BEFORE,
+            True,
+            id="csv with title line and some blank lines before",
+        ),
+        pytest.param(
+            EDGE_CASE_CSV_SCATTERED_NEWLINES_WITH_TITLE_LINE,
             True,
             id="csv with blank lines and titles",
         ),
         pytest.param(
-            CSV_WITH_NEWLINES_NO_TITLE,
+            EDGE_CASE_CSV_SCATTERED_NEWLINES_NO_TITLE_LINE,
             False,
             id="csv with blank lines and no titles",
         ),
@@ -150,7 +175,6 @@ def test_bulk_import_csv_parsing(
         active_config.wato_host_attrs, active_config.tags.get_tag_groups_by_topic()
     )
     mode_bulk_import = ModeBulkImport()
-    mode_bulk_import._has_title_line = has_title_line
     request.set_var("attribute_0", "host_name")
     request.set_var("attribute_1", "ipaddress")
 
@@ -162,9 +186,9 @@ def test_bulk_import_csv_parsing(
 
     hosts = folder_tree().root_folder().hosts()
     assert len(hosts) == 3
-    assert hosts[HostAddress("alinux01")].attributes["ipaddress"] == "10.10.10.1"
-    assert hosts[HostAddress("alinux02")].attributes["ipaddress"] == "10.10.10.2"
-    assert hosts[HostAddress("alinux03")].attributes["ipaddress"] == "10.10.10.3"
+    assert hosts[HostAddress("server01")].attributes["ipaddress"] == "192.168.1.101"
+    assert hosts[HostAddress("server02")].attributes["ipaddress"] == "192.168.1.102"
+    assert hosts[HostAddress("server03")].attributes["ipaddress"] == "192.168.1.103"
 
 
 @pytest.mark.parametrize(
@@ -236,22 +260,22 @@ def test_detect_attribute(
     "sample,headers,expected",
     [
         pytest.param(
-            CSV_NORMAL_WITH_TITLE,
+            CSV_WITH_TITLE_LINE,
             ["host_name", "ipaddress"],
             [
-                ("alinux01", {"ipaddress": "10.10.10.1"}, None),
-                ("alinux02", {"ipaddress": "10.10.10.2"}, None),
-                ("alinux03", {"ipaddress": "10.10.10.3"}, None),
+                ("server01", {"ipaddress": "192.168.1.101"}, None),
+                ("server02", {"ipaddress": "192.168.1.102"}, None),
+                ("server03", {"ipaddress": "192.168.1.103"}, None),
             ],
             id="green path with two attributes",
         ),
         pytest.param(
-            CSV_NORMAL_WITH_TITLE,
+            CSV_WITH_TITLE_LINE,
             ["host_name", "-"],
             [
-                ("alinux01", {}, None),
-                ("alinux02", {}, None),
-                ("alinux03", {}, None),
+                ("server01", {}, None),
+                ("server02", {}, None),
+                ("server03", {}, None),
             ],
             id="green path with one field unassigned",
         ),
@@ -299,7 +323,7 @@ def test_host_rows_to_bulk(
             id="field does not pass validation",
         ),
         pytest.param(
-            CSV_NORMAL_WITH_TITLE,
+            CSV_WITH_TITLE_LINE,
             ["-", "ipaddress"],
             "host name attribute",
             id="host_name not assigned",
@@ -320,3 +344,222 @@ def test_host_rows_to_bulk_exceptions(
     )
     with pytest.raises(MKUserError, match=exc_matches):
         list(_host_rows_to_bulk(raw_rows, host_attributes))
+
+
+@pytest.mark.parametrize(
+    "sample, delimiter, expected_delimeter",
+    [
+        pytest.param(
+            CSV_NO_TITLE_LINE,
+            ",",
+            ",",
+            id="normal CSV file with specified delim",
+        ),
+        pytest.param(
+            CSV_NO_TITLE_LINE,
+            None,
+            ",",
+            id="normal CSV file, no delim given",
+        ),
+        pytest.param(
+            CSV_WITH_SEMICOLONS,
+            None,
+            ";",
+            id="CSV with semicolons, delim not specified",
+        ),
+        pytest.param(
+            EDGE_CASE_CSV_CANT_SNIFF,
+            None,
+            ";",
+            id="CSV with no delim in first 2048 bytes falls back to semicolon",
+        ),
+        pytest.param(
+            CSV_MULTI_DELIM,
+            None,
+            ",",
+            id="CSV with multiple chars that could be a delim prefers comma",
+        ),
+        pytest.param(
+            "",
+            None,
+            ";",
+            id="Empty CSV defaults to semicolon (no crash)",
+        ),
+    ],
+)
+def test_determine_dialect(sample: str, delimiter: str, expected_delimeter: str) -> None:
+    handle = StringIO(sample)
+    assert (
+        CSVBulkImport(handle=handle, has_title_line=False, delimiter=delimiter)._dialect.delimiter
+        == expected_delimeter
+    )
+
+
+def test_get_handle_for_csv() -> None:
+    with pytest.raises(MKUserError):
+        get_handle_for_csv(
+            Path("/this/path/does/not/exist/hopefully/otherwise/this/test/will/really/fail.csv")
+        )
+
+
+@pytest.mark.parametrize(
+    "sample, expected_first_row, expected_second_row",
+    [
+        pytest.param(
+            CSV_NO_TITLE_LINE,
+            ["server01", "192.168.1.101"],
+            ["server02", "192.168.1.102"],
+            id="CSV file with no title line",
+        ),
+        pytest.param(
+            EDGE_CASE_CSV_SCATTERED_NEWLINES_NO_TITLE_LINE,
+            ["server01", "192.168.1.101"],
+            ["server02", "192.168.1.102"],
+            id="CSV file with scattered newlines",
+        ),
+        pytest.param(
+            CSV_WITH_ONE_LINE_NO_TITLE_LINE,
+            ["server01", "192.168.1.101"],
+            None,
+            id="CSV file with only one line (no title line)",
+        ),
+        pytest.param(
+            "",
+            None,
+            None,
+            id="CSV file no lines",
+        ),
+    ],
+)
+def test_skip_to_and_return_next_row(
+    sample: str,
+    expected_first_row: Sequence[str],
+    expected_second_row: Sequence[str],
+) -> None:
+    handle = StringIO(sample)
+    cbi = CSVBulkImport(handle=handle, has_title_line=False)
+    assert cbi.skip_to_and_return_next_row() == expected_first_row
+    assert cbi.skip_to_and_return_next_row() == expected_second_row
+
+
+@pytest.mark.parametrize(
+    "sample, has_title_line, expected_title_row, expected_next_row",
+    [
+        pytest.param(
+            CSV_NO_TITLE_LINE,
+            False,
+            None,
+            ["server01", "192.168.1.101"],
+            id="CSV file with no title line",
+        ),
+        pytest.param(
+            CSV_WITH_TITLE_LINE,
+            True,
+            ["hostname", "ipaddr"],
+            ["server01", "192.168.1.101"],
+            id="CSV file with title line",
+        ),
+        pytest.param(
+            EDGE_CASE_CSV_SCATTERED_NEWLINES_WITH_TITLE_LINE,
+            True,
+            ["hostname", "ipaddr"],
+            ["server01", "192.168.1.101"],
+            id="CSV file with scattered newlines and title line",
+        ),
+        pytest.param(
+            EDGE_CASE_CSV_SCATTERED_NEWLINES_NO_TITLE_LINE,
+            False,
+            None,
+            ["server01", "192.168.1.101"],
+            id="CSV file with scattered newlines and no title line",
+        ),
+    ],
+)
+def test_title_row(
+    sample: str,
+    has_title_line: bool,
+    expected_title_row: Sequence[str],
+    expected_next_row: Sequence[str],
+) -> None:
+    handle = StringIO(sample)
+    cbi = CSVBulkImport(handle=handle, has_title_line=has_title_line)
+    # We call title_row() twice to ensure the cursor only advances once
+    assert cbi.title_row == expected_title_row
+    assert cbi.title_row == expected_title_row
+    assert cbi.skip_to_and_return_next_row() == expected_next_row
+
+
+@pytest.mark.parametrize(
+    "sample, has_title_line, attr_names, expected_result",
+    [
+        pytest.param(
+            CSV_NO_TITLE_LINE,
+            False,
+            ["host_name", "ipaddress"],
+            [
+                {"host_name": "server01", "ipaddress": "192.168.1.101"},
+                {"host_name": "server02", "ipaddress": "192.168.1.102"},
+                {"host_name": "server03", "ipaddress": "192.168.1.103"},
+            ],
+            id="CSV, no title line, green path",
+        ),
+        pytest.param(
+            CSV_WITH_TITLE_LINE,
+            True,
+            ["host_name", "ipaddress"],
+            [
+                {"host_name": "server01", "ipaddress": "192.168.1.101"},
+                {"host_name": "server02", "ipaddress": "192.168.1.102"},
+                {"host_name": "server03", "ipaddress": "192.168.1.103"},
+            ],
+            id="CSV with title line, green path",
+        ),
+    ],
+)
+def test_next_row_as_dict(
+    sample: str,
+    has_title_line: bool,
+    attr_names: Sequence[str],
+    expected_result: Sequence[Mapping[str, str]],
+) -> None:
+    handle = StringIO(sample)
+    cbi = CSVBulkImport(handle=handle, has_title_line=has_title_line)
+    rows = list(cbi.rows_as_dict(attr_names))
+    assert rows == expected_result
+
+
+def test_next_row_as_dict_throws_len_mismatch() -> None:
+    """
+    rows_as_dict should raise ValueError if attr_names isn't exactly the row
+    length (2 in our sample).
+    """
+    handle = StringIO(CSV_NO_TITLE_LINE)
+    cbi = CSVBulkImport(handle=handle, has_title_line=False)
+
+    with pytest.raises(ValueError):
+        next(cbi.rows_as_dict(["host_name"]))
+
+    with pytest.raises(ValueError):
+        next(cbi.rows_as_dict(["host_name", "ipaddress", "alias"]))
+
+
+def test_skip_to_and_return_next_row_throws_len_mismatch() -> None:
+    """
+    skip_to_and_return_next_row should report to the user if they have lines
+    that aren't the same length as the first line (or header line) and bail out.
+    """
+    handle = StringIO(EDGE_CASE_CSV_MALFORMED)
+    cbi = CSVBulkImport(handle=handle, has_title_line=True)
+
+    with pytest.raises(MKUserError, match=r"^All rows"):
+        cbi.skip_to_and_return_next_row()
+
+
+def test_iter() -> None:
+    handle = StringIO(CSV_WITH_TITLE_LINE)
+    cbi = CSVBulkImport(handle=handle, has_title_line=True)
+    assert list(cbi) == [
+        ["server01", "192.168.1.101"],
+        ["server02", "192.168.1.102"],
+        ["server03", "192.168.1.103"],
+    ]

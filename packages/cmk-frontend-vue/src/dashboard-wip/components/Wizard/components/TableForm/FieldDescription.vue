b73f70de6302d24f4d2517e48e6d3434d4cb879b
Leonardo Petrora <leonardo.petrora@checkmk.com>
2025-10-09 10:03:42 +0200

    dashboard: enable dotted line for titles
    The FieldDescription component now accepts the `dots` parameter. When
    set to true, will display a line of dots after the content. Set to
    `true` by default
    
    Change-Id: I51df404d64e6a552558cb18f2863f1f41ff8187b

--- packages/cmk-frontend-vue/src/dashboard-wip/components/Wizard/components/TableForm/FieldDescription.vue
+++ packages/cmk-frontend-vue/src/dashboard-wip/components/Wizard/components/TableForm/FieldDescription.vue
@@ -3,13 +3,22 @@ Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 conditions defined in the file COPYING, which is part of this source code package.
 -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface FieldDescriptionProps {
+  dots?: boolean
+}
+
+withDefaults(defineProps<FieldDescriptionProps>(), {
+  dots: true
+})
+</script>
 
 <template>
   <div class="table-form-row">
     <div class="field-description">
       <span class="field-description__label">
         <slot />
+        <span v-if="dots" class="field-description-dots"></span>
       </span>
     </div>
   </div>
@@ -32,11 +41,26 @@ conditions defined in the file COPYING, which is part of this source code packag
   overflow: hidden;
   text-align: left;
   text-wrap-mode: nowrap;
+  position: relative;
+  margin-right: 10px;
 }
 
 /* stylelint-disable-next-line checkmk/vue-bem-naming-convention */
 .field-description__label {
   width: 100%;
   display: block;
+}
+
+/* stylelint-disable-next-line checkmk/vue-bem-naming-convention */
+.field-description-dots {
+  position: absolute;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+/* stylelint-disable-next-line checkmk/vue-bem-naming-convention */
+.field-description-dots::after {
+  content: '........................................................................................................................................................................................................';
 }
 </style>

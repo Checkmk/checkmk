9f0b745ca05ae17876bb092eaf34930f001e01ce
Anvar Umuraliev <anvar.umuraliev@checkmk.com>
2025-10-13 09:13:59 +0200

    Migrate omd apache check plugin to v2 API
    CMK-23027
    
    Change-Id: I483cf434fe25994b6ce05e9413f266ba9b6428e0

--- tests/unit/cmk/base/legacy_checks/test_generic_legacy_conversion.py
+++ tests/unit/cmk/base/legacy_checks/test_generic_legacy_conversion.py
@@ -790,7 +790,6 @@ def test_no_new_or_vanished_legacy_checks(fix_plugin_legacy: FixPluginLegacy) ->
         "nvidia_temp",
         "nvidia_temp_core",
         "nvidia_errors",
-        "omd_apache",
         "openbsd_sensors",
         "openbsd_sensors_fan",
         "openbsd_sensors_voltage",

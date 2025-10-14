416c3bb0ab6d477b2f97702ed589423d1749e12c
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-10-14 16:43:30 +0200

    agent_receiver: Initialize app once in fixture
    Instead of initializing a second time one can
    just use the mounted sub app.
    
    SAASDEV-5579
    
    Change-Id: Ic7f671a18cdb3e7beec4049e901b0d137bafe6c2

--- packages/cmk-agent-receiver/tests/unit/agent_receiver/conftest.py
+++ packages/cmk-agent-receiver/tests/unit/agent_receiver/conftest.py
@@ -12,10 +12,11 @@ import pytest
 from fastapi.testclient import TestClient
 from pydantic import UUID4
 from pytest_mock import MockerFixture
+from starlette.routing import Mount
 
 from cmk.agent_receiver.checkmk_rest_api import ControllerCertSettings
 from cmk.agent_receiver.config import get_config
-from cmk.agent_receiver.main import create_sub_app, main_app
+from cmk.agent_receiver.main import main_app
 
 _CA = b"""-----BEGIN PRIVATE KEY-----
 MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDpDGxoGtI59lZM
@@ -160,8 +161,10 @@ def mock_controller_certificate_settings(mocker: MockerFixture) -> None:
 
 @pytest.fixture(name="client")
 def fixture_client() -> TestClient:
-    main_app()
-    return TestClient(create_sub_app())
+    app = main_app()
+    assert len(app.routes) == 1
+    assert isinstance(mount := app.routes[0], Mount)
+    return TestClient(mount.app)
 
 
 @pytest.fixture(name="uuid")

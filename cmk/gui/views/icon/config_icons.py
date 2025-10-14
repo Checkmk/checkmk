e89609a0f98019d05da6502e016964fe06b1ba19
Lars Michelsen <lm@checkmk.com>
2025-10-14 13:33:37 +0200

    Config based icons: Get rid of partial
    The partial limits our tool support and essentially makes us blind for
    typing issues during development. This commit replaces the partial with
    regular functions, so that we get better type coverage again.
    
    Change-Id: Ibcdec9ac03e7383e30c03df4797a552256434dec

--- cmk/gui/views/icon/config_icons.py
+++ cmk/gui/views/icon/config_icons.py
@@ -4,8 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from collections.abc import Mapping, Sequence
-from functools import partial
+from collections.abc import Callable, Mapping, Sequence
 from typing import Literal
 
 from cmk.gui.config import default_authorized_builtin_role_ids
@@ -13,10 +12,9 @@ from cmk.gui.i18n import _
 from cmk.gui.permissions import declare_permission, permission_registry
 from cmk.gui.type_defs import BuiltinIconVisibility, IconSpec, Row
 from cmk.gui.utils.roles import UserPermissions
-from cmk.gui.views.icon.base import IconConfig
 from cmk.utils.tags import TagID
 
-from .base import Icon
+from .base import Icon, IconConfig
 
 
 def update_builtin_icons_from_config(
@@ -36,21 +34,6 @@ def update_builtin_icons_from_config(
 def config_based_icons(user_icons_and_actions: dict[str, IconSpec]) -> dict[str, Icon]:
     declare_icons_and_actions_perm(user_icons_and_actions)
 
-    def _render(
-        _what: Literal["host", "service"],
-        _row: Row,
-        _tags: Sequence[TagID],
-        _custom_vars: Mapping[str, str],
-        _user_permissions: UserPermissions,
-        _icon_cfg: IconConfig,
-        icon_spec: IconSpec,
-    ) -> tuple[str, str | None, tuple[str, str] | None]:
-        return (
-            icon_spec["icon"],
-            icon_spec.get("title"),
-            icon_spec.get("url"),
-        )
-
     return {
         icon_id: Icon(
             ident=icon_id,
@@ -58,13 +41,43 @@ def config_based_icons(user_icons_and_actions: dict[str, IconSpec]) -> dict[str,
             columns=[],
             host_columns=[],
             service_columns=[],
-            render=partial(_render, icon_spec=icon_spec),
+            render=_make_render_function(icon_spec=icon_spec),
             type_="custom_icon",
             sort_index=icon_spec.get("sort_index", 15),
             toplevel=icon_spec.get("toplevel", False),
         )
         for icon_id, icon_spec in user_icons_and_actions.items()
     }
+
+
+def _make_render_function(
+    icon_spec: IconSpec,
+) -> Callable[
+    [
+        Literal["host", "service"],
+        Row,
+        Sequence[TagID],
+        Mapping[str, str],
+        UserPermissions,
+        IconConfig,
+    ],
+    tuple[str, str | None, tuple[str, str] | None],
+]:
+    def _render(
+        _what: Literal["host", "service"],
+        _row: Row,
+        _tags: Sequence[TagID],
+        _custom_vars: Mapping[str, str],
+        _user_permissions: UserPermissions,
+        _icon_cfg: IconConfig,
+    ) -> tuple[str, str | None, tuple[str, str] | None]:
+        return (
+            icon_spec["icon"],
+            icon_spec.get("title"),
+            icon_spec.get("url"),
+        )
+
+    return _render
 
 
 def declare_icons_and_actions_perm(icons: dict[str, IconSpec]) -> None:

d358a6d7dbbee41ad46e08d758a1cb8b3c622eda
Alejandro Yunta <alejandro.yunta@checkmk.com>
2025-10-15 13:45:30 +0200

    feat(relay config): Implement RegisterRelay step
    CMK-26231
    
    Change-Id: I4baaecf9c990d8f90d3792ab4a97de54258cc1ac

--- packages/cmk-frontend-vue/src/mode-relay/add-relay-configuration-steps/RegisterRelay.vue
+++ packages/cmk-frontend-vue/src/mode-relay/add-relay-configuration-steps/RegisterRelay.vue
@@ -7,6 +7,8 @@ conditions defined in the file COPYING, which is part of this source code packag
 <script setup lang="ts">
 import usei18n from '@/lib/i18n'
 
+import CmkAlertBox from '@/components/CmkAlertBox.vue'
+import CmkCode from '@/components/CmkCode.vue'
 import CmkParagraph from '@/components/typography/CmkParagraph.vue'
 
 const { _t } = usei18n()
@@ -16,8 +18,26 @@ defineProps<{ relayName: string }>()
 
 <template>
   <CmkParagraph>
-    {{
-      _t('This is a placeholder for the relay setup wizard. The actual implementation is pending.')
-    }}
+    {{ _t('Register the Relay to authorize it for communication with your Checkmk site.') }}
   </CmkParagraph>
+  <CmkCode
+    :code_txt="`cmk-relay register -s <SERVER> -i <SITE> -U <USERNAME> -P <PASSWORD> -n ${relayName}`"
+  >
+  </CmkCode>
+  <CmkAlertBox variant="info">
+    {{
+      _t(
+        'This code snippet includes a one-time token. If you plan to use it multiple times in larger environments, you can check '
+      )
+    }}
+    <a
+      href="https://docs.checkmk.com/latest/en/not-yet-defined"
+      style="text-decoration: underline"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {{ _t('this article') }}
+    </a>
+    {{ _t(' for more details.') }}
+  </CmkAlertBox>
 </template>

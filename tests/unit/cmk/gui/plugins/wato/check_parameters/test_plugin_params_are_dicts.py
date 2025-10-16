6805ca8507c0c6486e52ce3d06aab3c99df90805
Andreas Boesl <andreas.boesl@checkmk.com>
2025-10-16 17:53:06 +0200

    test_plugin_parameters_are_dict: added form spec support
    Change-Id: I96f68902fdf4ad996cbe6ef5f0a3a575fbfd9d9e

--- tests/unit/cmk/gui/plugins/wato/check_parameters/test_plugin_params_are_dicts.py
+++ tests/unit/cmk/gui/plugins/wato/check_parameters/test_plugin_params_are_dicts.py
@@ -2,19 +2,25 @@
 # Copyright (C) 2023 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-
 # mypy: disable-error-code="type-arg"
+from typing import Any
 
-
+from cmk.gui.form_specs.unstable import TimeSpecific
+from cmk.gui.form_specs.unstable.legacy_converter import (
+    TransformDataForLegacyFormatOrRecomposeFunction,
+)
 from cmk.gui.inventory import RulespecGroupInventory
 from cmk.gui.plugins.wato.utils import RulespecGroupCheckParametersDiscovery
 from cmk.gui.valuespec import Dictionary, Migrate, Transform, ValueSpec
 from cmk.gui.watolib.rulespecs import (
     CheckParameterRulespecWithItem,
     CheckParameterRulespecWithoutItem,
+    FormSpecNotImplementedError,
     rulespec_registry,
     TimeperiodValuespec,
 )
+from cmk.rulesets.v1.form_specs import Dictionary as FSDictionary
+from cmk.rulesets.v1.form_specs import FormSpec
 
 _KNOWN_OFFENDERS = {
     "inv_retention_intervals",
@@ -22,9 +28,19 @@ _KNOWN_OFFENDERS = {
 
 
 def _get_first_actual_valuespec(vspec: ValueSpec) -> ValueSpec:
+    if isinstance(vspec, TimeperiodValuespec):
+        return _get_first_actual_valuespec(vspec._enclosed_valuespec)
     if isinstance(vspec, Transform | Migrate):
         return _get_first_actual_valuespec(vspec._valuespec)
     return vspec
+
+
+def _get_first_actual_form_spec(form_spec: FormSpec[Any]) -> FormSpec[Any]:
+    if isinstance(form_spec, TransformDataForLegacyFormatOrRecomposeFunction):
+        return _get_first_actual_form_spec(form_spec.wrapped_form_spec)
+    if isinstance(form_spec, TimeSpecific):
+        return _get_first_actual_form_spec(form_spec.parameter_form)
+    return form_spec
 
 
 def test_plugin_parameters_are_dict() -> None:
@@ -38,12 +54,12 @@ def test_plugin_parameters_are_dict() -> None:
         ):
             continue
 
-        vspec = element._valuespec()
-        if isinstance(vspec, TimeperiodValuespec):
-            vspec = vspec._enclosed_valuespec
-
-        if isinstance(_get_first_actual_valuespec(vspec), Dictionary):
-            continue
+        try:
+            if isinstance(_get_first_actual_form_spec(element.form_spec), FSDictionary):
+                continue
+        except FormSpecNotImplementedError:
+            if isinstance(_get_first_actual_valuespec(element.valuespec), Dictionary):
+                continue
 
         findings.add(element.name)
 

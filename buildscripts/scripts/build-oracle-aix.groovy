7846b1d4c3d4598b449d9a5cf82024f08a083c69
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-10-17 10:08:49 +0200

    Build mk-oracle on AIX
    CMK-26676
    
    Change-Id: Id31dfc8d6d2fc65df1dc8f71e3ce0ba1730f4cbf

---
+++ buildscripts/scripts/build-oracle-aix.groovy
@@ -0,0 +1,38 @@
+#!groovy
+
+// file: build-oracle-aix.groovy
+
+def main() {
+    check_job_parameters([
+        "VERSION",
+    ])
+
+    stage("Build AIX") {
+        inside_container() {
+            dir("${checkout_dir}") {
+                withCredentials([
+                    sshUserPrivateKey(
+                        credentialsId: "jenkins-aix-build-ssh-key",
+                        keyFileVariable: 'KEYFILE'
+                    ),
+                    file(
+                        credentialsId: 'know_hosts_ssh_aix',
+                        variable: 'KNOWN_HOSTS_FILE'
+                    ),
+                    usernamePassword(
+                        credentialsId: 'oracle_test_db_user_password',
+                        usernameVariable: 'ORACLEDB_USER',
+                        passwordVariable: 'ORACLEDB_PASSWORD'
+                    ),
+                ]) {
+                    sh("""
+                        checkout_dir=${checkout_dir} REMOTE_USER=au packages/mk-oracle/ssh-run-ci AIX -bu
+                    """)
+                     archiveArtifacts(allowEmptyArchive: true, artifacts: "mk-oracle-aix");
+                }
+            }
+        }
+    }
+}
+
+return this;

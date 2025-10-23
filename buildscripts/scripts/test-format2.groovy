1e8fbfe0e91a86c4aa0884b8ac424c6627012454
Alex Zurhake <alex.zurhake@checkmk.com>
2025-10-23 12:02:12 +0200

    Move formatting experiment to separate buildscript
    Change-Id: I4c2cfc935aca77fc4229e2c34ed26b2e12009d78

--- buildscripts/scripts/test-format2.groovy
+++ buildscripts/scripts/test-format2.groovy
@@ -6,6 +6,7 @@ def main() {
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
 
     dir("${checkout_dir}") {
+		sh('echo "--repository_cache=%workspace%/repository_cache" > user.bazelrc')
         test_jenkins_helper.execute_test([
             name: "Check format",
             cmd: "bazel run //:format.check",

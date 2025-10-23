0dd22dd7e98bba0affdf599045f14116d6740545
Sergey Kipnis <sergey.kipnis@checkmk.com>
2025-10-23 09:09:20 +0200

    wa: remove testing of python scripts
    The reason - python may be not available.
    
    It will be fixed later with revamp of all infrastructure
    
    Change-Id: I17b3b4b5f63e5a2e1ce9c53b0e731bef81cb5e7d

--- agents/wnx/run_tests.ps1
+++ agents/wnx/run_tests.ps1
@@ -435,9 +435,10 @@ try {
     Invoke-PluginsUnitTests
     Invoke-RegressionTest
     Invoke-IntegrationTest
+    Write-Host "PUYTHON TESTING IS DISABLED" -foreground Red
     try {
         Set-Location $repo_root
-        Invoke-Exe -run $testPlugins -name "plugins" -exe "py"  "-3" "-m" "pytest" "$cur_dir\tests\ap\test_mk_logwatch_win.py"
+        # Invoke-Exe -run $testPlugins -name "plugins" -exe "py"  "-3" "-m" "pytest" "$cur_dir\tests\ap\test_mk_logwatch_win.py"
     }
     finally {
         Set-Location $cur_dir

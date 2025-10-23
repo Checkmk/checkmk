a1710441b5a359335fb18206e4cbf77f8d90d22d
Alex Zurhake <alex.zurhake@checkmk.com>
2025-10-23 14:31:58 +0200

    Try to persist the repo cache
    Change-Id: I5f6585abba488361dc091f804b654479553205f4

--- buildscripts/scripts/test-format2.groovy
+++ buildscripts/scripts/test-format2.groovy
@@ -6,11 +6,17 @@ def main() {
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
 
     dir("${checkout_dir}") {
-		sh('echo "--repository_cache=%workspace%/repository_cache" > user.bazelrc')
-        test_jenkins_helper.execute_test([
-            name: "Check format",
-            cmd: "bazel run //:format.check",
-        ]);
+        sh("mkdir -p ${checkout_dir}/../repo_cache; mv ${checkout_dir}/../repo_cache ${checkout_dir}/repo_cache")
+        inside_container() {
+            sh("mv ${checkout_dir}/repo_cache ${checkout_dir}/../repo_cache")
+		    sh('echo "--repository_cache=${checkout_dir}/../repo_cache/" >> .bazelrc')
+            test_jenkins_helper.execute_test([
+                name: "Check format",
+                cmd: "bazel run //:format.check",
+            ]);
+            sh("mv ${checkout_dir}/../repo_cache ${checkout_dir}/repo_cache")
+        }
+        sh("mv ${checkout_dir}/repo_cache ${checkout_dir}/../repo_cache")
     }
 }
 

0e10be6290f583b95d55987d383b5c6623cfe982
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-10-23 15:08:38 +0200

    omdlib tests: don't import load_instance_id
    This is covered by an integration test, see the change with ID:
    I77fd4e6e9e85b5ec7c8e0fb1374b58c557a264f4
    
    CMK-26739
    
    Change-Id: I99833c71327ff4d43f677cfdd687c64a4c95e150

--- tests/unit/omdlib/test_instance_id.py
+++ tests/unit/omdlib/test_instance_id.py
@@ -8,18 +8,22 @@ from uuid import UUID
 
 from omdlib.instance_id import create_instance_id
 
-from cmk.utils.licensing.helper import get_instance_id_file_path, load_instance_id
+
+def _load_instance_id(site_home: Path) -> UUID | None:
+    try:
+        return UUID((site_home / "etc/omd/instance_id").read_text())
+    except (FileNotFoundError, ValueError):
+        return None
 
 
 def test_create_instance_id(tmp_path: Path) -> None:
     instance_id_1 = UUID("bb28ec27-4f77-409f-982e-92df861b25be")
     instance_id_2 = UUID("e65f2f7b-abcb-400c-9859-b5571b3d2679")
 
-    instance_id_file_path = get_instance_id_file_path(tmp_path)
-    instance_id_file_path.parent.mkdir(parents=True, exist_ok=True)
+    (tmp_path / "etc/omd/").mkdir(parents=True)
 
     create_instance_id(site_home=tmp_path, instance_id=instance_id_1)
-    assert load_instance_id(instance_id_file_path) == instance_id_1
+    assert _load_instance_id(tmp_path) == instance_id_1
 
     create_instance_id(site_home=tmp_path, instance_id=instance_id_2)
-    assert load_instance_id(instance_id_file_path) == instance_id_2
+    assert _load_instance_id(tmp_path) == instance_id_2

Checkmk/checkmk

commit 0ab82f5451af4262d6675d65758d797172860f83

omd: don't depend on cmk.utils.paths
CMK-26737

Change-Id: If798a411a9e03bb7b8c6826c4cee43cc98161764

omd/packages/omd/omdlib/config_hooks.py
@@ -38,7 +38,6 @@ from omdlib.type_defs import Config, ConfigChoiceHasError
 import cmk.ccc.resulttype as result
 from cmk.ccc.exceptions import MKTerminate
 from cmk.ccc.version import edition
-from cmk.utils import paths
 
 if TYPE_CHECKING:
     from omdlib.contexts import SiteContext
@@ -266,7 +265,7 @@ def load_config(site: "SiteContext", verbose: bool) -> Config:
         for hook_name in sort_hooks(os.listdir(site.hook_dir)):
             if hook_name[0] != "." and hook_name not in config:
                 config[hook_name] = call_hook(
-                    site, hook_name, ["default", edition(paths.omd_root).short], verbose
+                    site, hook_name, ["default", edition(site_home).short], verbose
                 )[1]
     return config
 

commit 46fcbfef9075dd38d813253728d97c356066ec40

fix omd create: unsupported operand type(s)
This bug was introduced in commit 0ab82f5451af4262d6675d65758d797172860f83.

Change-Id: I038e012e34ab1c61d16f3f25696b322f08c54a49

omd/packages/omd/omdlib/config_hooks.py
@@ -265,7 +265,7 @@ def load_config(site: "SiteContext", verbose: bool) -> Config:
         for hook_name in sort_hooks(os.listdir(site.hook_dir)):
             if hook_name[0] != "." and hook_name not in config:
                 config[hook_name] = call_hook(
-                    site, hook_name, ["default", edition(site_home).long], verbose
+                    site, hook_name, ["default", edition(Path(site_home)).long], verbose
                 )[1]
     return config
 
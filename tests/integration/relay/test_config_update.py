97110b6cf11c5373a98f326de68c088ac4d6027f
Max Linke <max.linke@checkmk.com>
2025-10-24 05:51:54 +0200

    Reapply "Enable host config update tests in relay"
    This reverts commit c1818fa88bba27ebd69dc7c0d60adaa5da9d97df.
    
    Change-Id: I0f27f60652cf1172bfe06cd3eccfd17d7e0124c1

--- tests/integration/relay/test_config_update.py
+++ tests/integration/relay/test_config_update.py
@@ -3,6 +3,9 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import io
+import tarfile
+
 import pytest
 
 from cmk.agent_receiver.certs import serialize_to_pem
@@ -15,9 +18,6 @@ from ..agent_receiver.test_agent_receiver import generate_csr_pair
 pytestmark = pytest.mark.skip_if_not_edition("cloud", "managed")
 
 
-@pytest.skip(
-    "Not all parts are integrated yet. This will only cause test failures", allow_module_level=True
-)
 class TestRelay:
     registered_relays: set[str] = set()
     created_hosts: set[str] = set()
@@ -37,24 +37,24 @@ class TestRelay:
         if self._site is None:
             return
 
+        if self.created_hosts:
+            self._site.openapi.hosts.bulk_delete(list(self.created_hosts))
+            self.created_hosts.clear()
+
         for relay_id in self.registered_relays:
             _, etag = self._site.openapi.relays.get(relay_id)
             self._site.openapi.relays.delete(relay_id, etag)
-
         self.registered_relays.clear()
 
-        if self.created_hosts:
-            self._site.openapi.hosts.bulk_delete(list(self.created_hosts))
-            self._site.openapi.changes.activate_and_wait_for_completion()
-            self.created_hosts.clear()
+        self._site.openapi.changes.activate_and_wait_for_completion()
 
+    @pytest.mark.skip("not supported")
     def test_config_update(self, site: Site) -> None:
         """
         Test that unrelated config change triggers submission of the new activated config to relays
         """
         self._site = site
-        alias = "foo_alias"
-        relay_id = self._register_relay(alias=alias)
+        relay_id = self._register_relay(alias="foo_alias")
 
         hostname = "localhost"
         site.openapi.hosts.create(hostname=hostname, attributes={"ipaddress": "127.0.0.1"})
@@ -66,6 +66,7 @@ class TestRelay:
         assert len(tasks) == 1
         assert isinstance(tasks[0].spec, RelayConfigTask)
 
+    @pytest.mark.skip("not supported")
     def test_config_update_of_relay(self, site: Site) -> None:
         """
         Test that change to relay's config triggers submission of the new activated config to relays
@@ -88,3 +89,32 @@ class TestRelay:
         tasks = site.openapi_agent_receiver.relays.get_tasks(relay_id)
         assert len(tasks) == 1
         assert isinstance(tasks[0].spec, RelayConfigTask)
+
+    def test_host_update_triggers_relay_config(self, site: Site) -> None:
+        """
+        Test that change to relay's config triggers submission of the new activated config to relays
+        """
+        self._site = site
+        alias = "foo_alias"
+        relay_id = self._register_relay(alias=alias)
+
+        hostname = "localhost"
+        _ = site.openapi.hosts.create(
+            hostname=hostname, attributes={"ipaddress": "127.0.0.1", "relay": relay_id}
+        )
+        self.created_hosts.add(hostname)
+
+        site.openapi.changes.activate_and_wait_for_completion()
+
+        tasks = site.openapi_agent_receiver.relays.get_tasks(relay_id)
+        assert len(tasks) == 1
+        assert isinstance(tasks[0].spec, RelayConfigTask)
+
+        # unpack
+        tar_data = tasks[0].spec.tar_data
+        with tarfile.open(fileobj=io.BytesIO(tar_data), mode="r:") as tf:
+            members = tf.getmembers()
+            # Check for empty archive
+            assert members, "Empty config archive in test"
+            member_names = {m.name for m in members}
+            assert any(hostname in m for m in member_names)

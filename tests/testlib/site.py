c82d2fa9cae59c0f3f754e9861d980f653939505
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2025-10-28 13:08:28 +0100

    Fix composition tests
    See Werk #17998
    
    Change-Id: I580ec5e56d8c3be2c0accff871ae38ba2b0c3e0a

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -2629,6 +2629,7 @@ def connection(
         "user_sync": {"sync_with_ldap_connections": "all"},
         "replicate_event_console": True,
         "replicate_extensions": True,
+        "is_trusted": False,
     }
     # stay backwards-compatible for performance tests:
     # only set message_broker_port for CMK2.4.0+

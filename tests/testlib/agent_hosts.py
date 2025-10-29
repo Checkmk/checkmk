ad050502991871f0b1df57f373069bce28b317a8
René Slowenski <rene.slowenski@checkmk.com>
2025-10-29 20:43:48 +0100

    testlib.agent_hosts: Increase discovery_wait_timeout
    Change-Id: I5953d28f1c9fef264a2c71f0e37755756289f4e6

--- tests/testlib/agent_hosts.py
+++ tests/testlib/agent_hosts.py
@@ -75,7 +75,7 @@ def _discover_services_of_piggybacked_hosts(
     dcd_interval: int = 5,
     cleanup: bool = True,
     discovery_max_count: int = 3,
-    discovery_wait_timeout: int = 1,
+    discovery_wait_timeout: int = 3,
 ) -> Iterator[None]:
     """Discover the services of piggybacked hosts.
 

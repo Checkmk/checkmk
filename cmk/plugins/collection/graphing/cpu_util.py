c7de393efccefba5db716c7526d9f4ea1ba1b1b4
Simon Jess <simon.jess@checkmk.com>
2025-10-29 17:37:13 +0100

    graph: Deduplicate 'CPU utilization' title IX
    Change-Id: I41353e10420136654cca5919da745acccc098541

--- cmk/plugins/collection/graphing/cpu_util.py
+++ cmk/plugins/collection/graphing/cpu_util.py
@@ -353,7 +353,7 @@ graph_cpu_utilization_5_util = graphs.Graph(
 )
 graph_cpu_utilization_6_steal = graphs.Graph(
     name="cpu_utilization_6_steal",
-    title=Title("CPU utilization"),
+    title=Title("CPU utilization (User, system, IO wait, steal)"),
     minimal_range=graphs.MinimalRange(
         0,
         100,

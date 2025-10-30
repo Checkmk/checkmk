e8ff0dd35e66db73d90d8281074b0dbd684cb0fe
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-10-30 12:43:53 +0100

    feat(ai-button) add contents for explain-this-issue
    Change-Id: I149efb3571e38b78c2b77ec36f86ecc5ffca8037

--- packages/cmk-frontend-vue/src/ai/lib/conversation-templates/explain-this-issue.ts
+++ packages/cmk-frontend-vue/src/ai/lib/conversation-templates/explain-this-issue.ts
@@ -6,7 +6,11 @@
 import type { CmkState } from '@/lib/cmkState'
 import usei18n from '@/lib/i18n'
 
-import type { AiApiClient } from '@/ai/lib/ai-api-client'
+import type {
+  AiApiClient,
+  AiInference,
+  DataToBeProvidedToLlmResponse
+} from '@/ai/lib/ai-api-client'
 import { AiRole } from '@/ai/lib/utils'
 
 import {
@@ -50,10 +54,85 @@ export class ExplainThisIssueAiTemplate extends AiConversationBaseTemplate {
   }
 
   public override async getDataToBeProvidedToAi(): Promise<TAiConversationElementContent[]> {
-    return []
+    let res: DataToBeProvidedToLlmResponse
+    try {
+      res = await this.api.getDataToBeProvidedToLlm()
+    } catch (e) {
+      console.error('Error fetching data to be provided to LLM:', e)
+      res = {
+        list_host_cols: ['host_name', 'ip_address', 'os_type', 'location'],
+        list_service_cols: ['service_description', 'status', 'last_check', 'performance_data']
+      }
+    }
+
+    return [
+      {
+        type: 'dialog',
+        message: _t(
+          'For a comprehensive analysis, the following data will be shared with our AI model. ' +
+            'This information is required for the AI to process and complete the analytical task. ' +
+            'By proceeding, you acknowledge and consent to this data being shared with our AI model.'
+        )
+      },
+      {
+        type: 'code',
+        title: _t('Shared data:'),
+        code:
+          `Host "${this.data.host}":\n` +
+          ` - ${res.list_host_cols.join('\n - ')}` +
+          '\n\n' +
+          `Service "${this.data.service}":\n` +
+          ` - ${res.list_service_cols.join('\n - ')}`
+      }
+    ]
   }
 
   protected async getInference(): Promise<TAiConversationElementContent[]> {
-    return []
+    let res: AiInference
+    try {
+      res = await this.api.inference(this.data.host, this.data.service, this.data.state)
+    } catch (e) {
+      console.error('Error fetching inference from AI API:', e)
+      return [
+        {
+          type: 'alert',
+          variant: 'error',
+          text: "Something went wrong. We couldn't generate the explanation."
+        }
+      ]
+    }
+
+    const explainContent: TAiConversationElementContent[] = [
+      {
+        type: 'markdown',
+        title: _t('Short summary'),
+        markdown: res.response.short_summary
+      },
+      {
+        type: 'markdown',
+        title: _t('Detected problem'),
+        markdown: res.response.detected_problem
+      },
+      {
+        type: 'code',
+        title: _t('Context'),
+        code: JSON.stringify(res.response.context, null, 2)
+      },
+      {
+        type: 'markdown',
+        title: _t('Potential solutions'),
+        markdown: res.response.potential_solutions
+      }
+    ]
+
+    if (res.response.might_be_misconfigured) {
+      explainContent.push({
+        type: 'alert',
+        variant: 'warning',
+        text: res.response.might_be_misconfigured
+      })
+    }
+
+    return explainContent
   }
 }

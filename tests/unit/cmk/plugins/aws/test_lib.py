cbd2dc42322a9b126eea374c527be96848f928b8
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2025-11-04 09:18:39 +0100

    18963 FIX: aws_*: Fix services not receiving data
    Fix extracting relevant metrics by ignoring case on both sides of the comparison
    
    SUP-26075
    
    Change-Id: I1065749dc2c936845447eaf4a598416d93cf0152

--- tests/unit/cmk/plugins/aws/test_lib.py
+++ tests/unit/cmk/plugins/aws/test_lib.py
@@ -6,7 +6,6 @@
 # mypy: disable-error-code="misc"
 # mypy: disable-error-code="no-untyped-def"
 # mypy: disable-error-code="type-arg"
-
 import pytest
 
 from cmk.agent_based.v2 import CheckResult, Metric, Result, State, StringTable
@@ -724,7 +723,7 @@ def test_check_aws_limits(test_input: list[list], output: CheckResult) -> None:
 @pytest.mark.parametrize(
     "expected_metric_names, section, extra_kwargs, expected_result",
     [
-        (
+        pytest.param(
             [
                 "CPUCreditUsage",
                 "CPUCreditBalance",
@@ -834,7 +833,7 @@ def test_check_aws_limits(test_input: list[list], output: CheckResult) -> None:
                 }
             },
         ),
-        (
+        pytest.param(
             [
                 "Requests",
                 "BytesDownloaded",
@@ -940,6 +939,105 @@ def test_check_aws_limits(test_input: list[list], output: CheckResult) -> None:
                     "5xxErrorRate": 0.0,
                 }
             },
+        ),
+        pytest.param(
+            ["VolumeWriteBytes"],
+            [
+                {
+                    "Id": "id_0_CPUUtilization",
+                    "Label": "test-redis-cluster-1-0001-001",
+                    "StatusCode": "Complete",
+                    "Timestamps": ["2023-01-03 09:47:00+00:00"],
+                    "Values": [[3.550924784186728, None]],
+                },
+            ],
+            {},
+            {},
+            id="no match",
+        ),
+        pytest.param(
+            ["CPUUtilization", "EngineCPUUtilization"],
+            [
+                {
+                    "Id": "id_0_CPUUtilization",
+                    "Label": "test-redis-cluster-1-0001-001",
+                    "StatusCode": "Complete",
+                    "Timestamps": ["2023-01-03 09:47:00+00:00"],
+                    "Values": [[3.550924784186728, None]],
+                },
+                {
+                    "Id": "id_0_EngineCPUUtilization",
+                    "Label": "test-redis-cluster-1-0001-001",
+                    "StatusCode": "Complete",
+                    "Timestamps": ["2023-01-03 09:47:00+00:00"],
+                    "Values": [[0.28168917995511694, None]],
+                },
+            ],
+            {},
+            {
+                "test-redis-cluster-1-0001-001": {
+                    "CPUUtilization": 3.550924784186728,
+                    "EngineCPUUtilization": 0.28168917995511694,
+                }
+            },
+            id="metric name contained in another metric",
+        ),
+        pytest.param(
+            ["HTTPCode_ELB_3XX_Count"],
+            [
+                {
+                    "Id": "id_0_HTTPCode_ELB_3XX_Count",
+                    "Label": "test-redis-cluster-1-0001-001",
+                    "StatusCode": "Complete",
+                    "Timestamps": ["2023-01-03 09:47:00+00:00"],
+                    "Values": [[572.0, None]],
+                },
+            ],
+            {},
+            {
+                "test-redis-cluster-1-0001-001": {
+                    "HTTPCode_ELB_3XX_Count": 572.0,
+                }
+            },
+            id="metric name with underscores",
+        ),
+        pytest.param(
+            ["RequestCount"],
+            [
+                {
+                    "Id": "id_0_consul_port_80_RequestCount",
+                    "Label": "consul-port-80",
+                    "StatusCode": "Complete",
+                    "Timestamps": ["2023-01-03 09:47:00+00:00"],
+                    "Values": [[1177.0, None]],
+                },
+            ],
+            {},
+            {
+                "consul-port-80": {
+                    "RequestCount": 1177.0,
+                }
+            },
+            id="id with additional args",
+        ),
+        pytest.param(
+            ["HTTPCode_Target_4XX_Count"],
+            [
+                {
+                    "Id": "id_0_consul_port_80_HTTPCode_Target_4XX_Count",
+                    "Label": "consul-port-80",
+                    "StatusCode": "Complete",
+                    "Timestamps": ["2023-01-03 09:47:00+00:00"],
+                    "Values": [[582.0, None]],
+                },
+            ],
+            {},
+            {
+                "consul-port-80": {
+                    "HTTPCode_Target_4XX_Count": 582.0,
+                }
+            },
+            id="id with additional args and metric name with underscores",
         ),
     ],
 )

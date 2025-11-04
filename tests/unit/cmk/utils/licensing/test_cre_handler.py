bf4027a42f409d0b07bd3d776241cc3a519681ad
Lars Michelsen <lm@checkmk.com>
2025-11-04 13:30:37 +0100

    Fix CRELicensingHandler imports
    Change-Id: Iec451370801c33c8fae46906b914b5bc62bd2a05

--- tests/unit/cmk/utils/licensing/test_cre_handler.py
+++ tests/unit/cmk/utils/licensing/test_cre_handler.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.utils.licensing.cre_handler import CRELicensingHandler
+from cmk.utils.licensing.community_handler import CRELicensingHandler
 from cmk.utils.licensing.handler import LicenseState, UserEffect
 
 

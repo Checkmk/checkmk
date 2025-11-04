33ffbafe8abd638016ce06f22a6d7398e820d4e2
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-11-04 10:15:53 +0100

    test_systemtime: fix typing
    CMK-27225
    
    Change-Id: I75cf802c7d03905f20b5da43ee18fd4c859f8ffb

--- tests/unit/cmk/base/legacy_checks/test_systemtime.py
+++ tests/unit/cmk/base/legacy_checks/test_systemtime.py
@@ -1,12 +1,10 @@
+#!/usr/bin/env python3
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-
 import datetime
 from collections.abc import Sequence
-from typing import Any
 from zoneinfo import ZoneInfo
 
 import pytest
@@ -54,7 +52,7 @@ def test_inventory_systemtime(
     ],
 )
 def test_check_systemtime(
-    item: str, params: Params, string_table: StringTable, expected_results: Sequence[Any]
+    item: str, params: Params, string_table: StringTable, expected_results: Sequence[object]
 ) -> None:
     """Test check function for systemtime check."""
     with time_machine.travel(

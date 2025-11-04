6943d3a19974526edb3cae0e76a2fdc3b0127425
Sebastian Groemcke <sebastian.groemcke@checkmk.com>
2025-11-04 17:20:22 +0100

    18972 apc_netbotz_drycontact: Use configured sensor state as base instead of static states
    Checkmk did not alert when normally closed contacts opened
    because the open state was always considered OK.
    
    Checkmk now respects each sensor’s configured state settings.
    The Checkmk state for a sensor depends directly on the configuration defined within that sensor.
    
    For example, if a sensor’s normal state is set to "closed", an "open" state will be reported as
    invalid, and vice versa. This ensures alerts accurately reflect the sensor’s intended behavior,
    reducing false positives and false negatives.
    
    Additionally, Checkmk now uses the severity defined in the sensor configuration instead of a
    static default severity.
    
    Review sensor configurations and alert rules to ensure alignment;
    no manual action is required unless alert patterns change unexpectedly.
    
    SUP-24522
    
    Change-Id: Ie6b303d5340a275e4a42db9e3bae60c8e755cb62

--- tests/unit/cmk/plugins/collection/agent_based/test_apc_netbotz_drycontact.py
+++ tests/unit/cmk/plugins/collection/agent_based/test_apc_netbotz_drycontact.py
@@ -49,34 +49,66 @@ def test_apc_netbotz_drycontact_inventory(
     [
         (
             [
-                ["1.6", "Pumpe 0 RZ4", "Kaeltepark RZ4", "2"],
-                ["2.5", "Pumpe 1 RZ4", "Kaeltepark RZ4", "1"],
-                ["2.6", "Pumpe 2 RZ4", "Kaeltepark RZ4", "3"],
+                ["1.6", "Pumpe 0 RZ4", "Kaeltepark RZ4", "2", "1", "3"],
+                ["2.5", "Pumpe 1 RZ4", "Kaeltepark RZ4", "1", "1", "3"],
+                ["2.6", "Pumpe 2 RZ4", "Kaeltepark RZ4", "3", "1", "3"],
             ],
             {
                 "Pumpe 0 RZ4 1.6": Data(
-                    location="Kaeltepark RZ4", state=("Open low mem", State.OK)
+                    location="Kaeltepark RZ4",
+                    state=("State: Open low mem [2] but expected Closed high mem [1]", State.CRIT),
                 ),
                 "Pumpe 1 RZ4 2.5": Data(
-                    location="Kaeltepark RZ4", state=("Closed high mem", State.CRIT)
+                    location="Kaeltepark RZ4",
+                    state=("Normal state (Closed high mem [1])", State.OK),
                 ),
-                "Pumpe 2 RZ4 2.6": Data(location="Kaeltepark RZ4", state=("Disabled", State.WARN)),
+                "Pumpe 2 RZ4 2.6": Data(
+                    location="Kaeltepark RZ4",
+                    state=("State: Disabled [3] but expected Closed high mem [1]", State.CRIT),
+                ),
             },
         ),
         (
-            [["1.6", "Leckagekontrolle-RZ4", "Kaeltepark RZ4", "25"]],
+            # Test severity: not normal state, but only WARN because severity is Warning
+            [["1.6", "Leckagekontrolle-RZ4", "Kaeltepark RZ4", "25", "2", "2"]],
             {
                 "Leckagekontrolle-RZ4 1.6": Data(
                     location="Kaeltepark RZ4",
-                    state=("unknown[25]", State.UNKNOWN),
+                    state=("State: unknown [25] but expected Open low mem [2]", State.WARN),
                 ),
             },
         ),
         (
-            [["1.6", "Pumpe 1", "Kaeltepark RZ4", "3"]],
-            {"Pumpe 1 1.6": Data(location="Kaeltepark RZ4", state=("Disabled", State.WARN))},
+            # Test severity: not normal state, but OK because severity is Informational
+            [["1.6", "Pumpe 1", "Kaeltepark RZ4", "3", "2", "1"]],
+            {
+                "Pumpe 1 1.6": Data(
+                    location="Kaeltepark RZ4",
+                    state=("State: Disabled [3] but expected Open low mem [2]", State.OK),
+                )
+            },
         ),
         ([], {}),
+        (
+            [
+                ["1.6", "Pumpe 0 RZ4", "Kaeltepark RZ4", "2", "2", "3"],
+                ["2.5", "Pumpe 1 RZ4", "Kaeltepark RZ4", "1", "2", "3"],
+                ["2.6", "Pumpe 2 RZ4", "Kaeltepark RZ4", "3", "2", "4"],
+            ],
+            {
+                "Pumpe 0 RZ4 1.6": Data(
+                    location="Kaeltepark RZ4", state=("Normal state (Open low mem [2])", State.OK)
+                ),
+                "Pumpe 1 RZ4 2.5": Data(
+                    location="Kaeltepark RZ4",
+                    state=("State: Closed high mem [1] but expected Open low mem [2]", State.CRIT),
+                ),
+                "Pumpe 2 RZ4 2.6": Data(
+                    location="Kaeltepark RZ4",
+                    state=("State: Disabled [3] but expected Open low mem [2]", State.UNKNOWN),
+                ),
+            },
+        ),
     ],
 )
 def test_apc_netbotz_drycontact_parse(info: StringTable, expected: Mapping[str, object]) -> None:
@@ -88,33 +120,78 @@ def test_apc_netbotz_drycontact_parse(info: StringTable, expected: Mapping[str, 
     [
         (
             "Pumpe 1",
-            {"Pumpe 1": Data(location="Kaeltepark", state=("Open low mem", State.OK))},
-            Result(state=State.OK, summary="[Kaeltepark] State: Open low mem"),
+            {
+                "Pumpe 1": Data(
+                    location="Kaeltepark",
+                    state=("State: Closed high mem [1] but expected Open low mem [2]", State.CRIT),
+                )
+            },
+            Result(
+                state=State.CRIT,
+                summary="[Kaeltepark] State: Closed high mem [1] but expected Open low mem [2]",
+            ),
         ),
         (
             "Pumpe 2",
-            {"Pumpe 2": Data(location="Waermepark", state=("Closed high mem", State.CRIT))},
-            Result(state=State.CRIT, summary="[Waermepark] State: Closed high mem"),
+            {
+                "Pumpe 2": Data(
+                    location="Waermepark",
+                    state=("Normal state (Closed high mem [1])", State.OK),
+                )
+            },
+            Result(
+                state=State.OK,
+                summary="[Waermepark] Normal state (Closed high mem [1])",
+            ),
         ),
         (
             "Pumpe 3",
-            {"Pumpe 3": Data(location="Kaeltepark", state=("Disabled", State.WARN))},
-            Result(state=State.WARN, summary="[Kaeltepark] State: Disabled"),
+            {
+                "Pumpe 3": Data(
+                    location="Kaeltepark",
+                    state=("State: Disabled [3] but expected Open low mem [2]", State.WARN),
+                )
+            },
+            Result(
+                state=State.WARN,
+                summary="[Kaeltepark] State: Disabled [3] but expected Open low mem [2]",
+            ),
         ),
         (
             "Pumpe 4",
-            {"Pumpe 4": Data(location="Kaeltepark", state=("Not applicable", State.UNKNOWN))},
-            Result(state=State.UNKNOWN, summary="[Kaeltepark] State: Not applicable"),
+            {
+                "Pumpe 4": Data(
+                    location="Kaeltepark",
+                    state=("State: unknown [25] but expected Open low mem [2]", State.UNKNOWN),
+                )
+            },
+            Result(
+                state=State.UNKNOWN,
+                summary="[Kaeltepark] State: unknown [25] but expected Open low mem [2]",
+            ),
         ),
         (
             "Pumpe 5",
-            {"Pumpe 5": Data(location="Kaeltepark", state=("unknown[5]", State.UNKNOWN))},
-            Result(state=State.UNKNOWN, summary="[Kaeltepark] State: unknown[5]"),
+            {
+                "Pumpe 5": Data(
+                    location="Kaeltepark",
+                    state=("State: unknown [5] but expected Open low mem [2]", State.UNKNOWN),
+                )
+            },
+            Result(
+                state=State.UNKNOWN,
+                summary="[Kaeltepark] State: unknown [5] but expected Open low mem [2]",
+            ),
         ),
         (
             "Pumpe without location",
-            {"Pumpe without location": Data(location="", state=("unknown[5]", State.UNKNOWN))},
-            Result(state=State.UNKNOWN, summary="State: unknown[5]"),
+            {
+                "Pumpe without location": Data(
+                    location="",
+                    state=("State: unknown [5] but expected Open low mem [2]", State.UNKNOWN),
+                )
+            },
+            Result(state=State.UNKNOWN, summary="State: unknown [5] but expected Open low mem [2]"),
         ),
     ],
 )

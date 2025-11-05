e9cc6345afa4e4c42227e39dcd03fe444f3dbbc0
Anastasiia Shevchuk <anastasiia.shevchuk@checkmk.com>
2025-11-05 09:33:31 +0100

    [OTel-QA] Add gui-e2e test for collector (Prometheus scraping) creation
    The tests creates a collector configuration (Prometheus scraping),
    verifies it and deletes via UI. After that the collector with the
    same configuration is created via API and the config files are compared.
    
    Change-Id: I51cfc41aa4d1dd938b37c9f13b1cf86f9189ef5a

---
+++ tests/gui_e2e/testlib/playwright/pom/setup/otel/add_open_telemetry_collector_prometheus_scraping.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+import logging
+import re
+from typing import Any, override
+from urllib.parse import quote_plus
+
+from playwright.sync_api import expect, Locator, Page
+
+from tests.gui_e2e.testlib.playwright.helpers import DropdownListNameToID
+from tests.gui_e2e.testlib.playwright.pom.page import CmkPage
+from tests.gui_e2e.testlib.playwright.pom.setup.otel.open_telemetry_collector_prometheus_scraping import (
+    OpenTelemetryCollectorPrometheusScraping,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class AddOpenTelemetryCollectorPrometheusScraping(CmkPage):
+    """Represent the page `Add OpenTelemetry collector: Prometheus scraping (experimental)`"""
+
+    def __init__(
+        self,
+        page: Page,
+        navigate_to_page: bool = True,
+    ) -> None:
+        self.page_title = "Add OpenTelemetry collector: Prometheus scraping (experimental)"
+        super().__init__(page, navigate_to_page)
+
+    @override
+    def navigate(self) -> None:
+        """Instructions to navigate to `Add OpenTelemetry collector: Prometheus scraping (experimental)` page."""
+        otel_collector_prom_scrape_page = OpenTelemetryCollectorPrometheusScraping(self.page)
+        otel_collector_prom_scrape_page.add_open_telemetry_collector_configuration_btn.click()
+        _url_pattern: str = quote_plus("mode=edit_otel_collectors_prom_scrape")
+        self.page.wait_for_url(url=re.compile(_url_pattern), wait_until="load")
+        self.validate_page()
+
+    @override
+    def validate_page(self) -> None:
+        logger.info(f"Validate that current page is '{self.page_title}' page")
+        self.main_area.check_page_title(self.page_title)
+        expect(self.unique_id_textfield).to_be_visible()
+        expect(self.add_new_scraper_configuration_button)
+
+    @override
+    def _dropdown_list_name_to_id(self) -> DropdownListNameToID:
+        return DropdownListNameToID()
+
+    @property
+    def save_configuration_button(self) -> Locator:
+        return self.main_area.get_suggestion("Save")
+
+    @property
+    def unique_id_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_label("Unique ID")
+
+    @property
+    def title_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_label("Title")
+
+    @property
+    def comment_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_label("Comment")
+
+    @property
+    def documentation_url_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_label("Documentation URL")
+
+    def site_restriction_checkbox(self, site_name: str) -> Locator:
+        return self.main_area.locator().get_by_label("Site restriction").get_by_text(site_name)
+
+    @property
+    def add_new_scraper_configuration_button(self) -> Locator:
+        return self.main_area.locator().get_by_role("button", name="Add new scrape configuration")
+
+    @property
+    def job_name_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_role("textbox", name="Job name")
+
+    @property
+    def scrape_interval_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_role("spinbutton", name="Scrape interval")
+
+    @property
+    def encrypt_communication_with_tls_checkbox(self) -> Locator:
+        return self.main_area.locator().get_by_role(
+            "checkbox", name="Encrypt communication with TLS"
+        )
+
+    @property
+    def metrics_path_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_role("textbox", name="Metrics path")
+
+    @property
+    def add_new_target_button(self) -> Locator:
+        return self.main_area.locator().get_by_role("button", name="Add new target")
+
+    @property
+    def ip_address_or_hostname_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_role("textbox", name="IP address or hostname")
+
+    @property
+    def port_textfield(self) -> Locator:
+        return self.main_area.locator().get_by_role("spinbutton", name="Port")
+
+    @property
+    def add_new_rule_button(self) -> Locator:
+        return self.main_area.locator().get_by_role("button", name="Add new rule")
+
+    @property
+    def host_name_rules_dropdown(self) -> Locator:
+        return self.main_area.locator().get_by_role("combobox", name="Host name rules")
+
+    def dropdown_option(self, option: str) -> Locator:
+        return self.main_area.locator().get_by_role("option", name=option, exact=True)
+
+    @property
+    def add_new_field_button(self) -> Locator:
+        return self.main_area.locator().get_by_role("button", name="Add new field")
+
+    @property
+    def host_name_field_type_dropdown(self) -> Locator:
+        return (
+            self.main_area.locator()
+            .get_by_role("combobox")
+            .get_by_text(re.compile(r"(Value of attribute|Literal string)", re.IGNORECASE))
+        )
+
+    @property
+    def value_of_attribute_dropdown(self) -> Locator:
+        return self.main_area.locator("button.cmk-dropdown-button--group-start")
+
+    @property
+    def value_of_attribute_filter(self) -> Locator:
+        return self.main_area.locator().get_by_label("filter")
+
+    @property
+    def literal_string_textfield(self) -> Locator:
+        return (
+            self.main_area.locator()
+            .get_by_text("Host name computation The")
+            .locator("input[type='text']")
+        )
+
+    @staticmethod
+    def click_on_last_locator(locator: Locator) -> None:
+        target_index = locator.count() - 1
+        locator.nth(target_index).click()
+
+    @staticmethod
+    def fill_last_locator(locator: Locator, value: str) -> None:
+        target_index = locator.count() - 1
+        locator.nth(target_index).fill(value)
+
+    def fill_collector_scrape_properties(
+        self,
+        properties: dict[str, Any],
+    ) -> None:
+        logger.info(
+            "Fill in the 'OpenTelemetry Collector: Prometheus Scraping (Experimental) properties' form"
+        )
+        self.add_new_scraper_configuration_button.click()
+        self.job_name_textfield.fill(properties["job_name"])
+        self.scrape_interval_textfield.fill(str(properties["scrape_interval"]))
+        if properties["encryption"]:
+            self.encrypt_communication_with_tls_checkbox.check()
+        self.metrics_path_textfield.fill(properties["metrics_path"])
+        for target in properties["targets"]:
+            self.add_new_target_button.click()
+            self.fill_last_locator(self.ip_address_or_hostname_textfield, target["address"])
+            self.fill_last_locator(self.port_textfield, str(target["port"]))
+        for rule in properties["host_name_rules"]:
+            self.add_new_rule_button.click()
+            if rule["type"] == "service_instance_id":
+                continue  # to remove
+                # self.click_on_last_locator(self.host_name_rules_dropdown)
+                # self.dropdown_option("Host name / IP address").click()
+            elif rule["type"] == "custom":
+                self.click_on_last_locator(self.host_name_rules_dropdown)
+                self.dropdown_option("Custom host name computation").click()
+                for custom_field in rule["value"]:
+                    self.click_on_last_locator(self.add_new_field_button)
+                    if custom_field["type"] != "key":  # to remove
+                        self.click_on_last_locator(self.host_name_field_type_dropdown)
+                    if custom_field["type"] == "key":
+                        # self.dropdown_option("Value of attribute").click() to uncomment
+                        if custom_field["value"] != "service.name":  # to remove
+                            self.click_on_last_locator(self.value_of_attribute_dropdown)
+                            self.fill_last_locator(
+                                self.value_of_attribute_filter,
+                                custom_field["value"],
+                            )
+                            self.dropdown_option(custom_field["value"]).click()
+                    elif custom_field["type"] == "free":
+                        self.dropdown_option("Literal string").click()
+                        self.fill_last_locator(self.literal_string_textfield, custom_field["value"])
+                    else:
+                        raise ValueError(f"Unknown host name field type: {custom_field['type']}")
+            else:
+                raise ValueError(f"Unknown host name rule type: {rule['type']}")

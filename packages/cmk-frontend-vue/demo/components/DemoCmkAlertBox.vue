79cfe7143bfe59d90695b1cc0c3dd64fac9ff39c
Alejandro Yunta <alejandro.yunta@checkmk.com>
2025-11-10 14:42:04 +0100

    fix(Frontend demo): Limit alert box demo width
    Limit alert box demo width, in order to demonstrate text wrapping.
    
    Relates to CMK-26874
    
    Change-Id: Iecfd9cf29f5a46feacb415e1a8ec1c220871364b

--- packages/cmk-frontend-vue/demo/components/DemoCmkAlertBox.vue
+++ packages/cmk-frontend-vue/demo/components/DemoCmkAlertBox.vue
@@ -11,28 +11,34 @@ defineProps<{ screenshotMode: boolean }>()
 </script>
 
 <template>
-  Variants:
-  <ul>
-    <li><CmkAlertBox variant="success">This is the success variant</CmkAlertBox></li>
-    <li><CmkAlertBox variant="info">This is the info variant</CmkAlertBox></li>
-    <li><CmkAlertBox variant="warning">This is the warning variant</CmkAlertBox></li>
-    <li><CmkAlertBox variant="error">This is the error variant</CmkAlertBox></li>
-  </ul>
-  Simple text heading:
-  <CmkAlertBox variant="error" heading="Error occurred"> Something went wrong... </CmkAlertBox>
-  Rich content heading via slot:
-  <CmkAlertBox variant="warning">
-    <template #heading> <strong>Warning:</strong> <em>Please review</em> </template>
-    Content here...
-  </CmkAlertBox>
-  Long text:
-  <CmkAlertBox variant="success"
-    >Very long message in order to showcase the text wrapping and the final position of the text and
-    the icon</CmkAlertBox
-  >
+  <div class="demo-cmk-alert-box__container">
+    Variants:
+    <ul>
+      <li><CmkAlertBox variant="success">This is the success variant</CmkAlertBox></li>
+      <li><CmkAlertBox variant="info">This is the info variant</CmkAlertBox></li>
+      <li><CmkAlertBox variant="warning">This is the warning variant</CmkAlertBox></li>
+      <li><CmkAlertBox variant="error">This is the error variant</CmkAlertBox></li>
+    </ul>
+    Simple text heading:
+    <CmkAlertBox variant="error" heading="Error occurred"> Something went wrong... </CmkAlertBox>
+    Rich content heading via slot:
+    <CmkAlertBox variant="warning">
+      <template #heading> <strong>Warning:</strong> <em>Please review</em> </template>
+      Content here...
+    </CmkAlertBox>
+    Long text:
+    <CmkAlertBox variant="success"
+      >Very long message in order to showcase the text wrapping and the final position of the text
+      and the icon</CmkAlertBox
+    >
+  </div>
 </template>
 
 <style scoped>
+.demo-cmk-alert-box__container {
+  max-width: 400px;
+}
+
 ul {
   list-style-type: none;
   padding: 0;

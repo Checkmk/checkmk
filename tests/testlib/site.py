c98187dfa2c2c6f472fd4ea10def41c8aa14bccf
René Slowenski <rene.slowenski@checkmk.com>
2025-11-12 20:02:52 +0100

    testlib.site: Disable replication for different edition remotes
    Change-Id: Iece0d20390320813c3ee18f26c5e04c6489783a6

--- tests/testlib/site.py
+++ tests/testlib/site.py
@@ -2416,20 +2416,25 @@ def connection(
     if central_site.version.is_managed_edition():
         basic_settings["customer"] = "provider"
 
-    configuration_connection = {
-        "enable_replication": True,
-        "url_of_remote_site": remote_site.internal_url,
-        "disable_remote_configuration": True,
-        "ignore_tls_errors": True,
-        "direct_login_to_web_gui_allowed": True,
-        "user_sync": {"sync_with_ldap_connections": "all"},
-        "replicate_event_console": True,
-        "replicate_extensions": True,
-    }
-    # stay backwards-compatible for adding older remote sites:
-    # only set message_broker_port for CMK2.4.0+
-    if remote_site.version >= CMKVersion("2.4.0", remote_site.version.edition):
-        configuration_connection["message_broker_port"] = remote_site.message_broker_port
+    if central_site.version.edition == remote_site.version.edition:
+        configuration_connection = {
+            "enable_replication": True,
+            "url_of_remote_site": remote_site.internal_url,
+            "disable_remote_configuration": True,
+            "ignore_tls_errors": True,
+            "direct_login_to_web_gui_allowed": True,
+            "user_sync": {"sync_with_ldap_connections": "all"},
+            "replicate_event_console": True,
+            "replicate_extensions": True,
+        }
+        # stay backwards-compatible for adding older remote sites:
+        # only set message_broker_port for CMK2.4.0+
+        if remote_site.version >= CMKVersion("2.4.0", remote_site.version.edition):
+            configuration_connection["message_broker_port"] = remote_site.message_broker_port
+    else:
+        configuration_connection = {
+            "enable_replication": False,
+        }
     site_config: dict[str, object] = {
         "basic_settings": basic_settings,
         "status_connection": {

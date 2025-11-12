52e6819309c34da86577ac4238e0431a3df916de
Luka Racic <luka.racic@checkmk.com>
2025-11-12 11:39:34 +0100

    Enhance Proxmox VE VM Info plugin with lock duration handling
    CMK-27477
    
    Change-Id: Id01f49c88f129146b19251ffb39f3ddcd2985cb1

--- cmk/plugins/proxmox_ve/agent_based/proxmox_ve_vm_info.py
+++ cmk/plugins/proxmox_ve/agent_based/proxmox_ve_vm_info.py
@@ -2,16 +2,23 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from collections.abc import Mapping
-from typing import Any
+import time
+from collections.abc import Mapping, MutableMapping
+from typing import Any, cast, TypedDict
 
 from cmk.agent_based.v2 import (
     AgentSection,
+    check_levels,
     CheckPlugin,
     CheckResult,
     DiscoveryResult,
+    FixedLevelsT,
+    get_value_store,
     HostLabel,
     HostLabelGenerator,
+    IgnoreResultsError,
+    NoLevelsT,
+    render,
     Result,
     Service,
     State,
@@ -19,7 +26,18 @@ from cmk.agent_based.v2 import (
 )
 from cmk.plugins.lib.uptime import check as check_uptime_seconds
 from cmk.plugins.lib.uptime import Section as UptimeSection
-from cmk.plugins.proxmox_ve.lib.vm_info import SectionVMInfo
+from cmk.plugins.proxmox_ve.lib.vm_info import LockState, SectionVMInfo
+
+
+class Params(TypedDict):
+    required_vm_status: str
+    lock_duration: NoLevelsT | FixedLevelsT[float]
+
+
+class GetLockDurationError(IgnoreResultsError):
+    """The exception raised by :func:`._get_lock_duration`.
+    If unhandled, this exception will make the service go stale.
+    """
 
 
 def parse_proxmox_ve_vm_info(string_table: StringTable) -> SectionVMInfo:
@@ -43,7 +61,68 @@ def discover_single(section: SectionVMInfo) -> DiscoveryResult:
     yield Service()
 
 
-def check_proxmox_ve_vm_info(params: Mapping[str, Any], section: SectionVMInfo) -> CheckResult:
+def _get_lock_duration(
+    value_store: Mapping[str, Any],
+    key: str,
+    time: float,
+    value: LockState,
+) -> float | None:
+    value_store = cast(MutableMapping[str, object], value_store)
+
+    last_state = value_store.get(key)
+    match last_state:
+        case (
+            float() | int() as last_time,
+            LockState() as last_value,
+        ):
+            pass
+        case _other:
+            return None
+
+    if time <= last_time:
+        raise GetLockDurationError("No duration available (time anomaly detected)")
+
+    if value is last_value:
+        return time - last_time
+
+    value_store[key] = (time, value)
+    return None
+
+
+def _check_lock_state_and_duration(
+    value_store: Mapping[str, Any],
+    lock_value: LockState,
+    key: str,
+    now: float,
+    lock_duration_params: NoLevelsT | FixedLevelsT[float],
+) -> CheckResult:
+    lock_duration = _get_lock_duration(
+        value_store=value_store,
+        key=key,
+        time=now,
+        value=lock_value,
+    )
+
+    yield Result(
+        state=State.OK,
+        summary=f"Config lock: {lock_value.value}",
+    )
+
+    if lock_duration is None or lock_duration_params[0] == "no_levels":
+        return
+
+    yield from check_levels(
+        value=lock_duration,
+        levels_upper=lock_duration_params,
+        render_func=render.timespan,
+        label="Config lock duration",
+        notice_only=True,
+    )
+
+
+def _check_proxmox_ve_vm_info_testable(
+    params: Params, section: SectionVMInfo, value_store: Mapping[str, Any]
+) -> CheckResult:
     yield Result(state=State.OK, summary=f"VM ID: {section.vmid}")
 
     vm_status = section.status.lower() if section.status else "n/a"
@@ -60,9 +139,30 @@ def check_proxmox_ve_vm_info(params: Mapping[str, Any], section: SectionVMInfo) 
         section=UptimeSection(uptime_sec=section.uptime, message=None),
     )
 
-    yield Result(
-        state=State.OK if not section.lock else State.CRIT,
-        notice=f"Config lock: {section.lock or 'none'}",
+    if not section.lock:
+        yield Result(
+            state=State.OK,
+            notice="Config lock: none",
+        )
+        return
+
+    yield from _check_lock_state_and_duration(
+        value_store=value_store,
+        lock_value=section.lock,
+        key=f"proxmox_ve_vm_info.lock_duration.{section.vmid}",
+        now=time.time(),
+        lock_duration_params=params["lock_duration"],
+    )
+
+
+def check_proxmox_ve_vm_info(
+    params: Params,
+    section: SectionVMInfo,
+) -> CheckResult:
+    yield from _check_proxmox_ve_vm_info_testable(
+        params=params,
+        section=section,
+        value_store=get_value_store(),
     )
 
 
@@ -80,5 +180,6 @@ check_plugin_proxmox_ve_vm_info = CheckPlugin(
     check_ruleset_name="proxmox_ve_vm_info",
     check_default_parameters={
         "required_vm_status": "",
+        "lock_duration": ("fixed", (15 * 60, 30 * 60)),
     },
 )
--- cmk/plugins/proxmox_ve/lib/vm_info.py
+++ cmk/plugins/proxmox_ve/lib/vm_info.py
@@ -2,13 +2,13 @@
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from enum import StrEnum
+from enum import Enum
 from typing import Literal
 
 from pydantic import BaseModel, Field
 
 
-class LockState(StrEnum):
+class LockState(Enum):
     BACKUP = "backup"
     CLONE = "clone"
     CREATE = "create"

1bfe5733a68e4bacaf7ca410ef15006c65481bbd
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2025-11-12 14:41:05 +0100

    agent_receiver: Add missing license header
    Change-Id: I1118243c07febfff63ac9488667a5781e3829d59

--- packages/cmk-agent-receiver/tests/component/test_forward_monitoring_data.py
+++ packages/cmk-agent-receiver/tests/component/test_forward_monitoring_data.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: Checkmk Enterprise License
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
 import base64
 import queue
 import secrets

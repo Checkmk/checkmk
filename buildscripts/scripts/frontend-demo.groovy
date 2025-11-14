83664e1054a75506e63383283370fdb22a18abd7
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-11-14 12:01:09 +0100

    Add frontend-demo app script
    CMK-24762
    
    Change-Id: I1b081a76a379db12e487fa09040bf7e2ebb98fb6

---
+++ buildscripts/scripts/frontend-demo.groovy
@@ -0,0 +1,40 @@
+#!groovy
+
+/// file: frontend-demo.groovy
+
+def main() {
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+    def safe_branch_name = versioning.safe_branch_name();
+    def output_artifacts = "bazel-bin/packages/cmk-frontend-vue/demo/dist";
+
+    dir("${checkout_dir}") {
+        container("ubuntu-2404-${safe_branch_name}-latest") {
+            stage("Build") {
+                sh("""
+                    bazel build @@//packages/cmk-frontend-vue:dist-demo-hosted
+                """);
+            }
+
+            stage("Deploy") {
+                // groovylint-disable-next-line DuplicateMapLiteral
+                withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
+                    sh("""
+                        scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} \
+                        ${output_artifacts}/* ${DEV_DOCS_URL}/frontend-demo
+                    """);
+                }
+            }
+
+            stage("Archive stuff") {
+                show_duration("archiveArtifacts") {
+                    archiveArtifacts(
+                        artifacts: "${output_artifacts}/**/*",
+                        fingerprint: true,
+                    );
+                }
+            }
+        }
+    }
+}
+
+return this;

ada2707321c402f830d98edf0f198baf5c2c1be8
Christopher Krause <christopher.krause@checkmk.com>
2025-11-14 10:38:15 +0100

    token_auth: reexport TokenId
    Change-Id: I593fdd2e6dbfab359605a38a647042f81805a0eb
    JIRA-Ref: CMK-27411

--- cmk/gui/token_auth/__init__.py
+++ cmk/gui/token_auth/__init__.py
@@ -3,15 +3,23 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.gui.token_auth._registry import handle_token_page as handle_token_page
 from cmk.gui.token_auth._registry import (
-    token_authenticated_page_registry as token_authenticated_page_registry,
+    handle_token_page,
+    token_authenticated_page_registry,
+    TokenAuthenticatedEndpoint,
+    TokenAuthenticatedPage,
+    TokenAuthenticatedPageRegistry,
 )
-from cmk.gui.token_auth._registry import TokenAuthenticatedEndpoint as TokenAuthenticatedEndpoint
-from cmk.gui.token_auth._registry import TokenAuthenticatedPage as TokenAuthenticatedPage
-from cmk.gui.token_auth._registry import (
-    TokenAuthenticatedPageRegistry as TokenAuthenticatedPageRegistry,
-)
-from cmk.gui.token_auth._store import AuthToken as AuthToken
-from cmk.gui.token_auth._store import DashboardToken as DashboardToken
-from cmk.gui.token_auth._store import TokenStore as TokenStore
+from cmk.gui.token_auth._store import AuthToken, DashboardToken, TokenId, TokenStore
+
+__all__ = [
+    "AuthToken",
+    "DashboardToken",
+    "TokenAuthenticatedEndpoint",
+    "TokenAuthenticatedPage",
+    "TokenAuthenticatedPageRegistry",
+    "TokenId",
+    "TokenStore",
+    "handle_token_page",
+    "token_authenticated_page_registry",
+]

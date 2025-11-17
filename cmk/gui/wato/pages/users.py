Checkmk/checkmk

commit 8ccd90b966ce4508f87def113ad6eb73dc5c60e0

18624 FIX ntopng: fix displaying of ntopng username in user settings
With [Werk 18620](https://checkmk.com/werk/18620) the username would be displayed for site-specific
ntopng configurations, unfortunately this broke the displaying of the ntopng username when using
normal global settings and also when editing users.

Now this works again as well.

CMK-27516

Change-Id: I67071a4f88e367b2ee2366b3b80b4e440b97d0ae

cmk/gui/wato/pages/users.py
@@ -66,7 +66,6 @@ from cmk.gui.utils.flashed_messages import flash, get_flashed_messages
 from cmk.gui.utils.html import HTML
 from cmk.gui.utils.ntop import (
     get_ntop_connection_by_site,
-    get_ntop_connection_mandatory,
     is_ntop_available,
 )
 from cmk.gui.utils.roles import UserPermissions, UserPermissionSerializableConfig
@@ -1000,15 +999,19 @@ class ModeEditUser(WatoMode):
 
         # ntopng
         if is_ntop_available():
-            ntop_connection = get_ntop_connection_mandatory()
-            # ntop_username_attribute will be the name of the custom attribute or false
-            # see corresponding Setup rule
-            ntop_username_attribute = ntop_connection.get("use_custom_attribute_as_ntop_username")
-            if ntop_username_attribute:
-                # TODO: Dynamically fiddling around with a TypedDict is a bit questionable
-                user_attrs[ntop_username_attribute] = request.get_str_input_mandatory(  # type: ignore[typeddict-unknown-key]
-                    ntop_username_attribute
+            ntop_connections = get_ntop_connection_by_site()
+            for _site_id, ntop_connection in ntop_connections.items():
+                # ntop_username_attribute will be the name of the custom attribute or false
+                # see corresponding Setup rule
+                ntop_username_attribute = ntop_connection.get(
+                    "use_custom_attribute_as_ntop_username"
                 )
+                if ntop_username_attribute:
+                    # TODO: Dynamically fiddling around with a TypedDict is a bit questionable
+                    user_attrs[ntop_username_attribute] = request.get_str_input_mandatory(  # type: ignore[literal-required]
+                        ntop_username_attribute
+                    )
+                    return
 
     def _increment_auth_serial(self, user_attrs: UserSpec) -> None:
         user_attrs["serial"] = user_attrs.get("serial", 0) + 1

cmk/gui/utils/ntop.py
@@ -10,7 +10,7 @@ Needs to be part of the generic code, not packed into NTOP addon.
 # mypy: disable-error-code="no-any-return"
 # mypy: disable-error-code="type-arg"
 
-from cmk.ccc.site import SiteId
+from cmk.ccc.site import omd_site, SiteId
 from cmk.gui.config import active_config
 from cmk.gui.i18n import _
 from cmk.gui.logged_in import user
@@ -31,7 +31,7 @@ def get_ntop_connection_by_site() -> dict[SiteId, dict]:
     return {
         site_id: get_site_globals(site_id, site_config).get("ntop_connection", {})
         for site_id, site_config in active_config.sites.items()
-    }
+    } | {omd_site(): get_ntop_connection()}
 
 
 def get_ntop_connection_mandatory() -> NtopConnectionSpec:

commit 718f5cbca1eb505d03884a8bd1482991c8a068bc

Fix crash when opening user page without ntopng connection configured
caused by 8ccd90b966ce4508f87def113ad6eb73dc5c60e0

CMK-27516

Change-Id: I3da9b4d62ab94858577235fe90c6d8d84559d686

cmk/gui/wato/pages/users.py
@@ -1001,6 +1001,8 @@ class ModeEditUser(WatoMode):
         if is_ntop_available():
             ntop_connections = get_ntop_connection_by_site()
             for _site_id, ntop_connection in ntop_connections.items():
+                if not ntop_connection:
+                    continue
                 # ntop_username_attribute will be the name of the custom attribute or false
                 # see corresponding Setup rule
                 ntop_username_attribute = ntop_connection.get(
@@ -1325,6 +1327,8 @@ class ModeEditUser(WatoMode):
             # ntop_username_attribute will be the name of the custom attribute or false
             # see corresponding Setup rule
             for site_id, ntop_connection in ntop_connections.items():
+                if not ntop_connection:
+                    continue
                 ntop_username_attribute = ntop_connection.get(
                     "use_custom_attribute_as_ntop_username"
                 )
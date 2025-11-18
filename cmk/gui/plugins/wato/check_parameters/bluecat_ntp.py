5f80c1d9f00263ea9e9463015a16a0a44562eb81
Lars Michelsen <lm@checkmk.com>
2025-11-18 16:08:20 +0100

    Clean up no-untyped-def #145
    Change-Id: Ifd9ff34051096968f6ef6c4b104583c90dd357c8

--- cmk/gui/plugins/wato/check_parameters/bluecat_ntp.py
+++ cmk/gui/plugins/wato/check_parameters/bluecat_ntp.py
@@ -3,7 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
@@ -22,7 +21,7 @@ bluecat_operstates = [
 ]
 
 
-def _parameter_valuespec_bluecat_ntp():
+def _parameter_valuespec_bluecat_ntp() -> Dictionary:
     return Dictionary(
         elements=[
             (

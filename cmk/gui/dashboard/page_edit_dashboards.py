bf09293b4e8edb5ee18e2ba3512cd35e7b4796fe
Sebastian Groemcke <sebastian.groemcke@checkmk.com>
2025-11-19 09:06:19 +0100

    PEV: Implement new table columns for dashboard listing
    CMK-26949
    
    Change-Id: Iae09c7f9ede5427d477d9fa12f78a78314c95157

--- cmk/gui/dashboard/page_edit_dashboards.py
+++ cmk/gui/dashboard/page_edit_dashboards.py
@@ -3,6 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import datetime as dt
 from collections.abc import Callable
 
 from cmk.ccc.user import UserId
@@ -13,8 +14,12 @@ from cmk.gui.i18n import _
 from cmk.gui.logged_in import user
 from cmk.gui.pages import PageContext
 from cmk.gui.permissions import permission_registry
+from cmk.gui.table import Table
+from cmk.gui.token_auth import get_token_store, TokenId
+from cmk.gui.type_defs import VisualName
 from cmk.gui.utils.roles import UserPermissions
 from cmk.gui.utils.urls import makeuri_contextless
+from cmk.gui.visuals._store import TVisual
 
 from .store import get_all_dashboards
 from .type_defs import DashboardConfig, DashboardName
@@ -29,6 +34,7 @@ def page_edit_dashboards(ctx: PageContext) -> None:
         visuals=get_all_dashboards(),
         user_permissions=UserPermissions.from_config(ctx.config, permission_registry),
         render_custom_buttons=_render_dashboard_buttons(ctx.request),
+        render_custom_columns=_render_dashboard_columns(),
     )
 
 
@@ -58,5 +64,52 @@ def _render_dashboard_buttons(request: Request) -> Callable[[DashboardName, Dash
                     title=_("Edit layout only available if header is enabled"),
                     cssclass="colorless",
                 )
+
+    return render
+
+
+def _render_dashboard_columns() -> Callable[[Table, VisualName, TVisual], None]:
+    # Read the token store once instead of for each table row
+    with get_token_store().read_locked() as data:
+        _tokens = data
+
+    def _get_token_status_label(valid_until: dt.datetime | None, disabled: bool) -> str:
+        """The human-readable status string for the dashboard share."""
+        if disabled:
+            return _("Disabled")
+
+        if valid_until is None:
+            return _("Shared")
+
+        now = dt.datetime.now(dt.UTC)
+        if valid_until > now:
+            return _("Expires %s") % valid_until.strftime("%Y-%m-%d")
+
+        return _("Expired %s") % valid_until.strftime("%Y-%m-%d")
+
+    def _render_not_shared(table: Table) -> None:
+        table.cell(_("Public link"), _("Not shared"))
+        table.cell(_("Link created"), "")
+        table.cell(_("Comments"), "")
+
+    def render(table: Table, dashboard_name: VisualName, dashboard_config: TVisual) -> None:
+        # do not add columns for built-in dashboards
+        if dashboard_config.get("owner") == UserId.builtin():
+            return
+
+        if not (plain_token_id := dashboard_config.get("public_token_id")):
+            _render_not_shared(table)
+            return
+
+        if not (auth_token := _tokens.get(TokenId(str(plain_token_id)))):
+            _render_not_shared(table)
+            return
+
+        table.cell(
+            _("Public link"),
+            _get_token_status_label(auth_token.valid_until, auth_token.details.disabled),
+        )
+        table.cell(_("Link created"), auth_token.issued_at.strftime("%Y-%m-%d"))
+        table.cell(_("Comments"), auth_token.details.comment)
 
     return render

877bd2ca51974eb543b5215b41162a3c5daa971f
Logan Connolly <logan.connolly@checkmk.com>
2025-11-20 11:24:52 +0100

    cisco_meraki: port over and improve proxy tests
    Change-Id: I5b12a6a8871d7c6e736de4e4416552cc34a92f94

--- tests/unit/cmk/plugins/cisco_meraki/rulesets/test_agent.py
+++ tests/unit/cmk/plugins/cisco_meraki/rulesets/test_agent.py
@@ -5,9 +5,13 @@
 
 import pytest
 
+from cmk.ccc.version import Edition
+from cmk.gui.utils.rule_specs.legacy_converter import convert_to_legacy_rulespec
+from cmk.gui.watolib.rulespecs import Rulespec
 from cmk.plugins.cisco_meraki.rulesets.agent_cisco_meraki import (
     _check_for_duplicates,
     _migrate_to_valid_ident,
+    rule_spec_cisco_meraki,
 )
 from cmk.rulesets.v1.form_specs.validators import ValidationError
 
@@ -39,3 +43,23 @@ class TestCheckForDuplicates:
     def test_bad_input_raises(self) -> None:
         with pytest.raises(ValidationError):
             _check_for_duplicates(["abc", "abc"])
+
+
+class TestLegacyProxyMigration:
+    @pytest.fixture
+    def rule(self) -> Rulespec:
+        return convert_to_legacy_rulespec(rule_spec_cisco_meraki, Edition.COMMUNITY, lambda x: x)
+
+    @pytest.fixture
+    def payload(self) -> dict[str, object]:
+        return {"api_key": ("password", "test-password")}
+
+    def test_no_legacy_proxy(self, rule: Rulespec, payload: dict[str, object]) -> None:
+        payload["proxy"] = ("no_proxy", None)
+        rule.valuespec.validate_datatype(payload, "")
+        rule.valuespec.validate_value(payload, "")
+
+    def test_with_legacy_proxy(self, rule: Rulespec, payload: dict[str, object]) -> None:
+        payload["proxy"] = ("url", "https://example.com")
+        rule.valuespec.validate_datatype(payload, "")
+        rule.valuespec.validate_value(payload, "")

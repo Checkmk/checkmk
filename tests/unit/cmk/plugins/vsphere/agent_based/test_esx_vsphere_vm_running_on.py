4bb5f233c0438a1f5974707c47cdba833b531b20
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-11-21 16:29:24 +0100

    vsphere: cleanup II
    Change-Id: Ifb30d31caf2f6dea62f2c850464b57e79d4139d3

--- tests/unit/cmk/plugins/vsphere/agent_based/test_esx_vsphere_vm_running_on.py
+++ tests/unit/cmk/plugins/vsphere/agent_based/test_esx_vsphere_vm_running_on.py
@@ -3,54 +3,24 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 from cmk.agent_based.v2 import Result, State
 from cmk.plugins.vsphere.agent_based import esx_vsphere_vm, esx_vsphere_vm_running_on
-from cmk.plugins.vsphere.lib.esx_vsphere import SectionESXVm
-
-
-def test_parse_esx_vsphere_running_on_host():
-    host = esx_vsphere_vm._parse_esx_vm_running_on_host(
-        {"runtime.host": ["zh1wagesx02.widag.local"]}
-    )
-    assert host == "zh1wagesx02.widag.local"
 
 
 def test_check_running_on_host() -> None:
-    running_on_host = "host"
-    check_result = list(
-        esx_vsphere_vm_running_on.check_running_on(_esx_vm_section(running_on_host))
-    )
+    assert (section := esx_vsphere_vm.parse_esx_vsphere_vm([["runtime.host", "my.host.tld"]]))
+    check_result = list(esx_vsphere_vm_running_on.check_running_on(section))
     results = [r for r in check_result if isinstance(r, Result)]
     assert len(results) == 1
     assert results[0].state == State.OK
-    assert results[0].summary == "Running on host"
+    assert results[0].summary == "Running on my.host.tld"
 
 
 def test_check_running_on_host_missing() -> None:
-    running_on_host = None
-    check_result = list(
-        esx_vsphere_vm_running_on.check_running_on(_esx_vm_section(running_on_host))
-    )
+    assert (section := esx_vsphere_vm.parse_esx_vsphere_vm([]))
+    check_result = list(esx_vsphere_vm_running_on.check_running_on(section))
     results = [r for r in check_result if isinstance(r, Result)]
     assert len(results) == 1
     assert results[0].state == State.UNKNOWN
     assert results[0].summary == "Runtime host information is missing"
-
-
-def _esx_vm_section(host: str | None) -> SectionESXVm:
-    return SectionESXVm(
-        mounted_devices=(),
-        snapshots=(),
-        status=None,
-        power_state=None,
-        memory=None,
-        cpu=None,
-        datastores=(),
-        heartbeat=None,
-        host=host,
-        name=None,
-        systime=None,
-    )

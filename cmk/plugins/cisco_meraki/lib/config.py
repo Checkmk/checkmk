444c0190d19c686c4a838eb395943c5dcf043715
Logan Connolly <logan.connolly@checkmk.com>
2025-11-21 17:00:29 +0100

    cisco_meraki: make helpers private
    Also, moved this code to the bottom so that the public functions and
    classes are at the top of the file.
    
    Change-Id: I1f007bf78d80d82d3c9f49296271d1957ee5960c

--- cmk/plugins/cisco_meraki/lib/config.py
+++ cmk/plugins/cisco_meraki/lib/config.py
@@ -3,6 +3,8 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from __future__ import annotations
+
 from argparse import Namespace
 from collections.abc import Callable, Sequence
 from dataclasses import dataclass
@@ -14,42 +16,6 @@ from cmk.server_side_programs.v1_unstable import Storage
 
 from . import cache, constants, schema
 
-type CacheDecorator[**P, R] = Callable[[Callable[P, R]], Callable[P, R]]
-
-
-@dataclass(frozen=True)
-class CacheConfig:
-    devices: CacheDecorator[[str, str], dict[str, schema.Device]]
-    device_statuses: CacheDecorator[[str], Sequence[schema.RawDevicesStatus]]
-    licenses_overview: CacheDecorator[[str, str], schema.LicensesOverview | None]
-    organizations: CacheDecorator[[], Sequence[schema.RawOrganisation]]
-    sensor_readings: CacheDecorator[[str], Sequence[schema.RawSensorReadings]]
-
-    @classmethod
-    def build(cls, args: Namespace) -> Self:
-        return cls(
-            devices=cache.cache_ttl(
-                Storage("cisco_meraki_devices", host=args.hostname),
-                ttl=args.cache_devices,
-            ),
-            device_statuses=cache.cache_ttl(
-                Storage("cisco_meraki_devices_statuses", host=args.hostname),
-                ttl=args.cache_device_statuses,
-            ),
-            licenses_overview=cache.cache_ttl(
-                Storage("cisco_meraki_licenses_overview", host=args.hostname),
-                ttl=args.cache_licenses_overview,
-            ),
-            organizations=cache.cache_ttl(
-                Storage("cisco_meraki_organizations", host=args.hostname),
-                ttl=args.cache_organizations,
-            ),
-            sensor_readings=cache.cache_ttl(
-                Storage("cisco_meraki_sensor_readings", host=args.hostname),
-                ttl=args.cache_sensor_readings,
-            ),
-        )
-
 
 @dataclass(frozen=True)
 class MerakiConfig:
@@ -58,7 +24,7 @@ class MerakiConfig:
     section_names: Sequence[str]
     org_id_as_prefix: bool
     no_cache: bool
-    cache: CacheConfig
+    cache: _CacheConfig
 
     @classmethod
     def build(cls, args: Namespace) -> Self:
@@ -68,7 +34,7 @@ class MerakiConfig:
             section_names=args.sections,
             org_id_as_prefix=args.org_id_as_prefix,
             no_cache=args.no_cache,
-            cache=CacheConfig.build(args),
+            cache=_CacheConfig.build(args),
         )
 
     @property
@@ -110,3 +76,40 @@ def get_meraki_dashboard(api_key: str, region: str, debug: bool, proxy: str | No
         # TODO: dashboard api always expects a string, but proxy can be None.
         requests_proxy=proxy,
     )
+
+
+type _CacheDecorator[**P, R] = Callable[[Callable[P, R]], Callable[P, R]]
+
+
+@dataclass(frozen=True)
+class _CacheConfig:
+    devices: _CacheDecorator[[str, str], dict[str, schema.Device]]
+    device_statuses: _CacheDecorator[[str], Sequence[schema.RawDevicesStatus]]
+    licenses_overview: _CacheDecorator[[str, str], schema.LicensesOverview | None]
+    organizations: _CacheDecorator[[], Sequence[schema.RawOrganisation]]
+    sensor_readings: _CacheDecorator[[str], Sequence[schema.RawSensorReadings]]
+
+    @classmethod
+    def build(cls, args: Namespace) -> Self:
+        return cls(
+            devices=cache.cache_ttl(
+                Storage("cisco_meraki_devices", host=args.hostname),
+                ttl=args.cache_devices,
+            ),
+            device_statuses=cache.cache_ttl(
+                Storage("cisco_meraki_devices_statuses", host=args.hostname),
+                ttl=args.cache_device_statuses,
+            ),
+            licenses_overview=cache.cache_ttl(
+                Storage("cisco_meraki_licenses_overview", host=args.hostname),
+                ttl=args.cache_licenses_overview,
+            ),
+            organizations=cache.cache_ttl(
+                Storage("cisco_meraki_organizations", host=args.hostname),
+                ttl=args.cache_organizations,
+            ),
+            sensor_readings=cache.cache_ttl(
+                Storage("cisco_meraki_sensor_readings", host=args.hostname),
+                ttl=args.cache_sensor_readings,
+            ),
+        )

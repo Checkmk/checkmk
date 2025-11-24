7f9f3eee9fe348f8e7d0c70ec9b82744e27de01f
Lukas Lengler <lukas.lengler@checkmk.com>
2025-11-24 10:47:54 +0100

    18626 FIX No longer put .pkl files under version control
    SUP-26539
    
    Change-Id: Ib402192062537609ef182e7545d53c0fff4c0c57

--- cmk/gui/watolib/git.py
+++ cmk/gui/watolib/git.py
@@ -141,6 +141,7 @@ def _write_gitignore_files() -> None:
             "!.gitignore\n"
             "*swp\n"
             "*.mk.new\n"
+            "*.pkl\n"
         )
 
     for subdir in config_dir.iterdir():

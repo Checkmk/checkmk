a5dc47c784c1b4228d6e5db6b298573b57aac06a
Lars Michelsen <lm@checkmk.com>
2025-11-24 09:14:49 +0100

    Clean up no-untyped-def #242
    Change-Id: Ia7a88831e1e06739f092be83134c1e48c6319807

--- cmk/plugins/checkpoint/rulesets/checkpoint_vsx_connections.py
+++ cmk/plugins/checkpoint/rulesets/checkpoint_vsx_connections.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.rulesets.v1 import Help, Label, Title
 from cmk.rulesets.v1.form_specs import (
     DefaultValue,
@@ -22,7 +20,7 @@ from cmk.rulesets.v1.form_specs import (
 from cmk.rulesets.v1.rule_specs import CheckParameters, HostAndItemCondition, Topic
 
 
-def _parameter_valuespec_checkpoint_vsx_connections():
+def _parameter_valuespec_checkpoint_vsx_connections() -> Dictionary:
     return Dictionary(
         help_text=Help(
             "This rule allows you to configure the number of maximum connections for a given VSID."

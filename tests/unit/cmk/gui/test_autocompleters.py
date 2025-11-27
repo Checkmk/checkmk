8348dfb684e38e52de659800927f22265ad9ed8c
Jonas Scharpf <jonas.scharpf@checkmk.com>
2025-11-27 14:50:40 +0100

    Revert "OTel: Cleanup host name computation from OTel collector configurations"
    This reverts commit 13422bce9fcd916a432096922c2ddf0097d60ada.
    
    Reason for revert: breaks test-gui-e2e-f12less-ultimate/205
    
    Change-Id: I7f89e54ff9d0a65253c7d0e9d9e1b3bd80060a78

--- tests/unit/cmk/gui/test_autocompleters.py
+++ tests/unit/cmk/gui/test_autocompleters.py
@@ -46,6 +46,7 @@ def test_builtin_autocompleters_registered() -> None:
 
     if is_ultimate_repo():
         expected += [
+            "otel_host_name_computation",
             "otel_metric_name_autocompleter",
             "relay_choices",
             "azure_labels",

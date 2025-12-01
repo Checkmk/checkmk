0d9feab8997c4f2024275a3d609e5adfc7062cd8
Lukas Lengler <lukas.lengler@checkmk.com>
2025-12-01 13:56:52 +0100

    OAuth2 connections: add SimpleListMode to display OAuth2 connections
    CMK-27719
    
    Change-Id: Icbc0b15219c593f5e5320c002d1f489817cd1e9a

--- cmk/gui/oauth2_connections/wato/_modes.py
+++ cmk/gui/oauth2_connections/wato/_modes.py
@@ -6,7 +6,7 @@
 
 from collections.abc import Collection
 from dataclasses import asdict
-from typing import override, TypedDict
+from typing import override
 
 from cmk.gui.breadcrumb import Breadcrumb
 from cmk.gui.config import Config
@@ -16,10 +16,14 @@ from cmk.gui.i18n import _
 from cmk.gui.page_menu import (
     PageMenu,
 )
+from cmk.gui.table import Table
 from cmk.gui.type_defs import PermissionName
 from cmk.gui.utils.urls import makeuri
-from cmk.gui.wato.pages.password_store import ModePasswords
+from cmk.gui.wato import SimpleListMode, SimpleModeType
+from cmk.gui.watolib.config_domain_name import ABCConfigDomain
+from cmk.gui.watolib.config_domains import ConfigDomainCore
 from cmk.gui.watolib.mode import ModeRegistry, WatoMode
+from cmk.gui.watolib.oauth_connections import OAuthConnection, OAuthConnectionsConfigFile
 from cmk.shared_typing.mode_oauth2_connection import (
     AuthorityUrls,
     MsGraphApi,
@@ -29,14 +33,10 @@ from cmk.shared_typing.mode_oauth2_connection import (
 )
 
 
-class OAuthConnection(TypedDict):
-    access_token: str
-    refresh_token: str
-
-
 def register(mode_registry: ModeRegistry) -> None:
     mode_registry.register(ModeRedirectOAuth2Connection)
     mode_registry.register(ModeCreateOAuth2Connection)
+    mode_registry.register(ModeOAuth2Connections)
 
 
 def get_oauth2_connection_config(connection: MsGraphApi | None = None) -> Oauth2ConnectionConfig:
@@ -56,21 +56,67 @@ def get_oauth2_connection_config(connection: MsGraphApi | None = None) -> Oauth2
     )
 
 
+class OAuth2ModeType(SimpleModeType[OAuthConnection]):
+    def type_name(self) -> str:
+        return "oauth2_connection"
+
+    def name_singular(self) -> str:
+        return _("OAuth2 connection")
+
+    def is_site_specific(self) -> bool:
+        return False
+
+    def can_be_disabled(self) -> bool:
+        return False
+
+    def affected_config_domains(self) -> list[ABCConfigDomain]:
+        return [ConfigDomainCore()]
+
+
+class ModeOAuth2Connections(SimpleListMode[OAuthConnection]):
+    @classmethod
+    def name(cls) -> str:
+        return "oauth2_connections"
+
+    def _table_title(self) -> str:
+        return _("OAuth2 connections")
+
+    @staticmethod
+    def static_permissions() -> Collection[PermissionName]:
+        return ["hosts"]  # TODO: add correct permissions
+
+    def __init__(self) -> None:
+        super().__init__(
+            mode_type=OAuth2ModeType(),
+            store=OAuthConnectionsConfigFile(),
+        )
+
+    def title(self) -> str:
+        return _("OAuth2 connections")
+
+    def page(self, config: Config) -> None:
+        super().page(config)
+
+    def _show_entry_cells(self, table: Table, ident: str, entry: OAuthConnection) -> None:
+        table.cell(_("ID"), ident)
+        table.cell(_("Title"), entry["title"])
+
+
 class ModeCreateOAuth2Connection(WatoMode[None]):
     @classmethod
     @override
     def name(cls) -> str:
-        return "create_oauth2_connection"
+        return "edit_oauth2_connection"
 
     @staticmethod
     @override
     def static_permissions() -> Collection[PermissionName]:
-        return ["hosts"]
+        return ["hosts"]  # TODO: add correct permissions
 
     @classmethod
     @override
     def parent_mode(cls) -> type[WatoMode[None]] | None:
-        return ModePasswords
+        return ModeOAuth2Connections
 
     @override
     def title(self) -> str:
@@ -101,7 +147,7 @@ class ModeRedirectOAuth2Connection(WatoMode[None]):
     @staticmethod
     @override
     def static_permissions() -> Collection[PermissionName]:
-        return ["hosts"]
+        return ["hosts"]  # TODO: add correct permissions
 
     @override
     def page_menu(self, config: Config, breadcrumb: Breadcrumb) -> PageMenu:

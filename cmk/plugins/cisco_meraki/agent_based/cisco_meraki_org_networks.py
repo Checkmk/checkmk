b649c8678d2f7a4dd0752b3edc40c1f61c5f5274
Logan Connolly <logan.connolly@checkmk.com>
2025-12-02 12:43:01 +0100

    cisco_meraki: integrate networks inventory plugin
    Main changes from original plugin:
    
    - validate agent section data with pydantic.
    - use American English spelling, i.e. organization.
    - added test coverage for inventory plugin.
    - general clean ups to adhere to coding guidelines.
    
    CMK-27060
    
    Co-authored-by: th.l <thl-cmk@outlook.com>
    Change-Id: If3ef586f1ebef035d4508f4eadd906b4556523ba

---
+++ cmk/plugins/cisco_meraki/agent_based/cisco_meraki_org_networks.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+#
+# Original author: thl-cmk[at]outlook[dot]com
+
+import json
+from collections.abc import Sequence
+
+from pydantic import BaseModel, Field
+
+from cmk.agent_based.v2 import (
+    AgentSection,
+    InventoryPlugin,
+    InventoryResult,
+    StringTable,
+    TableRow,
+)
+
+
+class Network(BaseModel, frozen=True):
+    id: str
+    name: str
+    organization_id: str = Field(alias="organizationId")
+    organization_name: str = Field(alias="organizationName")
+    product_types: list[str] = Field(alias="productTypes")
+    time_zone: str = Field(alias="timeZone")
+    tags: list[str]
+    enrollment_string: str = Field(alias="enrollmentString")
+    url: str
+    notes: str
+    is_bound_to_config_template: bool = Field(alias="isBoundToConfigTemplate")
+
+
+def parse_meraki_networks(string_table: StringTable) -> Sequence[Network]:
+    match string_table:
+        case [[payload]] if payload:
+            network_map = json.loads(payload)[0]
+            return [Network.model_validate(data) for data in network_map.values()]
+        case _:
+            return []
+
+
+agent_section_cisco_meraki_org_networks = AgentSection(
+    name="cisco_meraki_org_networks",
+    parse_function=parse_meraki_networks,
+)
+
+
+def inventory_meraki_networks(section: Sequence[Network]) -> InventoryResult:
+    for network in section:
+        inventory_columns = {
+            "is_bound_to_template": "yes" if network.is_bound_to_config_template else "no",
+            "network_name": network.name,
+            "organization_id": network.organization_id,
+            "organization_name": network.organization_name,
+            "product_types": ", ".join(m.title() for m in network.product_types),
+            "time_zone": network.time_zone,
+            "url": network.url,
+        }
+        if network.notes:
+            inventory_columns["notes"] = network.notes
+        if network.enrollment_string:
+            inventory_columns["enrollment_string"] = network.enrollment_string
+        if network.tags:
+            inventory_columns["tags"] = ", ".join(network.tags)
+
+        yield TableRow(
+            path=["software", "applications", "cisco_meraki", "networks"],
+            key_columns={"network_id": network.id},
+            inventory_columns=inventory_columns,
+        )
+
+
+inventory_plugin_cisco_meraki_org_networks = InventoryPlugin(
+    name="cisco_meraki_org_networks",
+    inventory_function=inventory_meraki_networks,
+)
---
+++ tests/unit/cmk/plugins/cisco_meraki/agent_based/test_cisco_meraki_org_networks.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import json
+
+import pytest
+from polyfactory.factories import TypedDictFactory
+
+from cmk.agent_based.v2 import StringTable, TableRow
+from cmk.plugins.cisco_meraki.agent_based.cisco_meraki_org_networks import (
+    inventory_meraki_networks,
+    parse_meraki_networks,
+)
+from cmk.plugins.cisco_meraki.lib.schema import Network
+
+
+class _NetworkFactory(TypedDictFactory[Network]):
+    __check_model__ = False
+
+
+def test_inventory_device_info() -> None:
+    device = _NetworkFactory.build(id="netid-123")
+    string_table = [[f"[{json.dumps({device['id']: device})}]"]]
+    section = parse_meraki_networks(string_table)
+
+    row = next(row for row in inventory_meraki_networks(section) if isinstance(row, TableRow))
+
+    assert not row.status_columns
+    assert row.key_columns == {"network_id": "netid-123"}
+    assert row.path == ["software", "applications", "cisco_meraki", "networks"]
+
+    expected_inventory_columns = {
+        "enrollment_string",
+        "is_bound_to_template",
+        "network_name",
+        "notes",
+        "organization_id",
+        "organization_name",
+        "product_types",
+        "tags",
+        "time_zone",
+        "url",
+    }
+
+    assert set(row.inventory_columns.keys()) == expected_inventory_columns
+
+
+@pytest.mark.parametrize("string_table ", [[], [[]], [[""]]])
+def test_inventory_device_info_no_payload(string_table: StringTable) -> None:
+    section = parse_meraki_networks(string_table)
+    assert not list(inventory_meraki_networks(section))

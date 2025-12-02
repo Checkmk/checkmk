b21103cbbbd129c9a5438178db434503bc3062c4
Frans Fürst <frans.fuerst@checkmk.com>
2025-12-02 15:30:26 +0100

    integrates win_ip_address from GH-875 into inventory_win_networkadapter
    CMK-27309
    
    Change-Id: I70b93d6c2fcbaef1101252e533d4787b2bc72c22

--- cmk/plugins/windows/agent_based/inventory_win_networkadapter.py
+++ cmk/plugins/windows/agent_based/inventory_win_networkadapter.py
@@ -19,10 +19,118 @@
 # DefaultGateway: 192.168.178.1
 
 from collections.abc import Mapping, Sequence
+from contextlib import suppress
+from ipaddress import (
+    AddressValueError,
+    ip_interface,
+    IPv4Interface,
+    IPv6Interface,
+    NetmaskValueError,
+)
 
-from cmk.agent_based.v2 import AgentSection, InventoryPlugin, InventoryResult, StringTable, TableRow
+from pydantic import BaseModel, field_validator
 
-Section = Sequence[Mapping]
+from cmk.agent_based.v2 import (
+    AgentSection,
+    HostLabel,
+    HostLabelGenerator,
+    InventoryPlugin,
+    InventoryResult,
+    StringTable,
+    TableRow,
+)
+
+Section = Sequence[Mapping[str, str]]
+
+
+class Adapter(BaseModel):
+    # Original author: thl-cmk[at]outlook[dot]com
+    #
+    # refactor-me: this model should be moved to cmk.plugins.lib and used for other plugins, too,
+    #              e.g. lnx_if and ip_addresses
+    name: str
+    ipv4_address: str | None = None
+    ipv4_subnet: str | None = None
+    ipv6_address: str | None = None
+    ipv6_subnet: str | None = None
+
+    @staticmethod
+    def is_broadcast(interface: IPv4Interface | IPv6Interface) -> bool:
+        """
+        >>> Adapter.is_broadcast(IPv4Interface("1.2.3.4/24"))
+        False
+        """
+        return (
+            (interface.version == 4 and interface.network.prefixlen != 32)
+            or (interface.version == 6 and interface.network.prefixlen != 128)
+        ) and (interface.ip.compressed == interface.network.broadcast_address.compressed)
+
+    def interface_ips(self) -> Sequence[IPv4Interface | IPv6Interface]:
+        def interface_from(address: str, subnet: str) -> None | IPv4Interface | IPv6Interface:
+            """Returns an interface instance from valid non-broadcast input"""
+            with suppress(AddressValueError, NetmaskValueError, ValueError):
+                # drop broadcast IPs
+                if not Adapter.is_broadcast(ip_if := ip_interface(f"{address}/{subnet}")):
+                    return ip_if
+            return None
+
+        return [
+            interface
+            for addresses, subnets in (
+                (self.ipv4_address, self.ipv4_subnet),
+                (self.ipv6_address, self.ipv6_subnet),
+            )
+            if addresses and subnets
+            for raw_address, raw_subnet in zip(addresses.split(", "), subnets.split(", "))
+            if (interface := interface_from(raw_address, raw_subnet))
+        ]
+
+    @field_validator("*", mode="after")
+    @classmethod
+    def validate_name(cls, value: str) -> str:
+        return value.strip().strip(",").strip()
+
+
+def host_label_win_ip_address(section: Section) -> HostLabelGenerator:
+    """
+    Host label function
+    Labels:
+        cmk/l3v4_topology:
+            "singlehomed" is set for all devices with one IPv4 address
+            "multihomed" is set for all devices with more than one IPv4 address.
+        cmk/l3v6_topology:
+            "singlehomed" is set for all devices with one IPv6 address
+            "multihomed" is set for all devices with more than one IPv6 address.
+
+        Link-local ("FE80::/64), unspecified ("::") and local-host ("127.0.0.0/8", "::1") IPs don't count.
+    """
+    # Original author: thl-cmk[at]outlook[dot]com
+    #
+    # refactor-me: this should go to cmk.plugins.network.agent_based.ip_addresses.host_label_ip_addresses
+    #              but callers first have to use Adapter
+    valid_v4_ips = 0
+    valid_v6_ips = 0
+    for raw_adapter in section:
+        adapter = Adapter.model_validate(raw_adapter)
+        for interface_ip in adapter.interface_ips():
+            if interface_ip.version == 4 and not interface_ip.is_loopback:
+                valid_v4_ips += 1
+                if valid_v4_ips == 1:
+                    yield HostLabel(name="cmk/l3v4_topology", value="singlehomed")
+                if valid_v4_ips == 2:
+                    yield HostLabel(name="cmk/l3v4_topology", value="multihomed")
+
+            elif (
+                interface_ip.version == 6
+                and not interface_ip.is_loopback
+                and not interface_ip.is_link_local
+                and not interface_ip.is_unspecified
+            ):
+                valid_v6_ips += 1
+                if valid_v6_ips == 1:
+                    yield HostLabel(name="cmk/l3v6_topology", value="singlehomed")
+                if valid_v6_ips == 2:
+                    yield HostLabel(name="cmk/l3v6_topology", value="multihomed")
 
 
 def parse_win_networkadapter(
@@ -89,6 +197,9 @@ def parse_win_networkadapter(
 agent_section_win_networkadapter = AgentSection(
     name="win_networkadapter",
     parse_function=parse_win_networkadapter,
+    # refactor-me: should use cmk.plugins.network.agent_based.ip_addresses.host_label_ip_addresses
+    #              but that has to be refactored first to use Adapter
+    host_label_function=host_label_win_ip_address,
 )
 
 
@@ -114,7 +225,57 @@ def inventory_win_networkadapter(section: Section) -> InventoryResult:
             )
 
 
+def inventory_win_ip_address(section: Section) -> InventoryResult:
+    """
+    >>> list(inventory_win_ip_address([{
+    ...    "type": "ETH 802.3",
+    ...    "macaddress": " 3C:7C:3F:49:7C:22",
+    ...    "name": "Adaptor",
+    ...    "speed": 23,
+    ...    "ipv4_address": ", 1.2.3.4",
+    ...    "ipv6_subnet": "",
+    ...    "ipv4_subnet": "255.255.255.0",
+    ... }]))
+    [TableRow(path=['networking', 'addresses'], key_columns={'address': '1.2.3.4', 'device': 'Adaptor'}, inventory_columns={'type': 'ipv4', 'network': '1.2.3.0', 'netmask': '255.255.255.0', 'cidr': 24, 'broadcast': '1.2.3.255'}, status_columns={})]
+    """
+    # Original author: thl-cmk[at]outlook[dot]com
+    #
+    # refactor-me: this should go to cmk.plugins.network.agent_based.ip_addresses.host_label_ip_addresses
+    #              but callers first have to use Adapter
+    address_type = {
+        4: "ipv4",
+        6: "ipv6",
+    }
+
+    yield from (
+        TableRow(
+            path=["networking", "addresses"],
+            key_columns={
+                "address": str(interface.ip.compressed),
+                "device": adapter.name,
+            },
+            inventory_columns={
+                "type": address_type.get(interface.version, "").lower(),
+                "network": str(interface.network.network_address),
+                "netmask": str(interface.network.netmask),
+                "cidr": interface.network.prefixlen,
+                "broadcast": str(interface.network.broadcast_address),
+            },
+            status_columns={},
+        )
+        for adapter in map(Adapter.model_validate, section)
+        if adapter.name
+        for interface in adapter.interface_ips()
+    )
+
+
 inventory_plugin_win_networkadapter = InventoryPlugin(
     name="win_networkadapter",
     inventory_function=inventory_win_networkadapter,
+)
+
+inventory_plugin_win_ip_address = InventoryPlugin(
+    name="win_ip_address",
+    sections=["win_networkadapter"],
+    inventory_function=inventory_win_ip_address,
 )
--- tests/unit/cmk/plugins/windows/agent_based/test_inventory_win_networkadapter.py
+++ tests/unit/cmk/plugins/windows/agent_based/test_inventory_win_networkadapter.py
@@ -4,12 +4,28 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
+from ipaddress import (
+    IPv4Interface,
+    IPv6Interface,
+)
+from pathlib import Path
+
 import pytest
 
-from cmk.agent_based.v2 import InventoryResult, StringTable, TableRow
+from cmk.agent_based.v2 import (
+    HostLabel,
+    HostLabelGenerator,
+    InventoryResult,
+    StringTable,
+    TableRow,
+)
 from cmk.plugins.windows.agent_based.inventory_win_networkadapter import (
+    Adapter,
+    host_label_win_ip_address,
+    inventory_win_ip_address,
     inventory_win_networkadapter,
     parse_win_networkadapter,
+    Section,
 )
 
 
@@ -86,3 +102,160 @@ def test_inventory_win_networkadapter(
         list(inventory_win_networkadapter(parse_win_networkadapter(string_table)))
         == expected_result
     )
+
+
+__section = [
+    {
+        "type": " Ethernet 802.3",
+        "macaddress": " 3C:7C:3F:49:7C:22",
+        "name": " ASUS USB-AC68 USB Wireless adapter",
+        "speed": 1300000000,
+        "ipv4_address": ", 192.168.10.11",
+        "ipv6_subnet": "",
+        "ipv4_subnet": "255.255.255.0",
+    },
+    {
+        "type": " Ethernet 802.3",
+        "name": " VMware Virtual Ethernet Adapter for VMnet1",
+        "macaddress": " 00:50:56:C0:00:01",
+        "speed": 100000000,
+        "ipv4_address": ", 169.254.0.1, 192.168.1.100",
+        "ipv4_subnet": "255.255.0.0, 255.255.255.0",
+        "ipv6_address": "fe80::5669:a1eb:3add:e9b2, 2c::1",
+        "ipv6_subnet": ", 64, 127",
+    },
+    {
+        "type": " Ethernet 802.3",
+        "macaddress": " 00:1A:7D:DA:71:06",
+        "name": " Bluetooth Device (Personal Area Network)",
+    },
+    {
+        "type": " Ethernet 802.3",
+        "macaddress": " 3E:7C:3F:49:7C:22",
+        "name": " Microsoft Wi-Fi Direct Virtual Adapter",
+        "speed": 9223372036854775807,
+    },
+    {
+        "type": " Ethernet 802.3",
+        "macaddress": " 3C:7C:3F:49:7C:22",
+        "name": " Microsoft Wi-Fi Direct Virtual Adapter #2",
+        "speed": 9223372036854775807,
+    },
+]
+
+
+__adapter = Adapter(  # type: ignore[call-arg] # ip_data not known by Adapter
+    name="VMware Virtual Ethernet Adapter for VMnet1",
+    ipv4_address="169.254.0.1, 192.168.1.100",
+    ipv4_subnet="255.255.0.0, 255.255.255.0",
+    ipv6_address="fe80::5669:a1eb:3add:e9b2, 2c::1",
+    ipv6_subnet="64, 127",
+    ip_data=[
+        IPv4Interface("169.254.0.1/16"),
+        IPv4Interface("192.168.1.100/24"),
+        IPv6Interface("fe80::5669:a1eb:3add:e9b2/64"),
+        IPv6Interface("2c::1/127"),
+    ],
+)
+
+
+@pytest.mark.parametrize(
+    "section, expected_result",
+    [
+        (
+            __section,
+            [
+                TableRow(
+                    path=["networking", "addresses"],
+                    key_columns={
+                        "address": "192.168.10.11",
+                        "device": "ASUS USB-AC68 USB Wireless adapter",
+                    },
+                    inventory_columns={
+                        "broadcast": "192.168.10.255",
+                        "cidr": 24,
+                        "netmask": "255.255.255.0",
+                        "network": "192.168.10.0",
+                        "type": "ipv4",
+                    },
+                    status_columns={},
+                ),
+                TableRow(
+                    path=["networking", "addresses"],
+                    key_columns={
+                        "address": "169.254.0.1",
+                        "device": "VMware Virtual Ethernet Adapter for VMnet1",
+                    },
+                    inventory_columns={
+                        "broadcast": "169.254.255.255",
+                        "cidr": 16,
+                        "netmask": "255.255.0.0",
+                        "network": "169.254.0.0",
+                        "type": "ipv4",
+                    },
+                    status_columns={},
+                ),
+                TableRow(
+                    path=["networking", "addresses"],
+                    key_columns={
+                        "address": "192.168.1.100",
+                        "device": "VMware Virtual Ethernet Adapter for VMnet1",
+                    },
+                    inventory_columns={
+                        "broadcast": "192.168.1.255",
+                        "cidr": 24,
+                        "netmask": "255.255.255.0",
+                        "network": "192.168.1.0",
+                        "type": "ipv4",
+                    },
+                    status_columns={},
+                ),
+                TableRow(
+                    path=["networking", "addresses"],
+                    key_columns={
+                        "address": "fe80::5669:a1eb:3add:e9b2",
+                        "device": "VMware Virtual Ethernet Adapter for VMnet1",
+                    },
+                    inventory_columns={
+                        "broadcast": "fe80::ffff:ffff:ffff:ffff",
+                        "cidr": 64,
+                        "netmask": "ffff:ffff:ffff:ffff::",
+                        "network": "fe80::",
+                        "type": "ipv6",
+                    },
+                    status_columns={},
+                ),
+            ],
+        ),
+    ],
+)
+def test_inventory_win_ip_address(section: Section, expected_result: InventoryResult) -> None:
+    assert list(inventory_win_ip_address(section)) == expected_result
+
+
+@pytest.mark.parametrize(
+    "section, expected_result",
+    [
+        (
+            __section,
+            [
+                HostLabel("cmk/l3v4_topology", "singlehomed"),
+                HostLabel("cmk/l3v4_topology", "multihomed"),
+            ],
+        ),
+    ],
+)
+def test_host_label_win_ip_address(section: Section, expected_result: HostLabelGenerator) -> None:
+    assert list(host_label_win_ip_address(section)) == list(expected_result)
+
+
+if __name__ == "__main__":
+    # Please keep these lines - they make TDD easy and have no effect on normal test runs.
+    # Just set _PYTEST_RAISES=1 and run this file from your IDE and dive into the code.
+    source_file_path = (
+        (base := (test_file := Path(__file__)).parents[6])
+        / test_file.parent.relative_to(base / "tests/unit")
+        / test_file.name.lstrip("test_")
+    ).as_posix()
+    assert pytest.main(["--doctest-modules", source_file_path]) in {0, 5}
+    pytest.main(["-vvsx", __file__])

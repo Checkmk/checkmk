b5d2e8b4675bfd3a7b9aead29b4c1b8f1d48e2ce
Lars Michelsen <lm@checkmk.com>
2025-12-05 07:40:12 +0100

    Clean up no-untyped-def #370 (batch of 5 check_parameters)
    Change-Id: I3f687140dee44298a2baaf26b47c6486f2a8da52

--- cmk/gui/plugins/wato/check_parameters/hostsystem_sensors.py
+++ cmk/gui/plugins/wato/check_parameters/hostsystem_sensors.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from typing import Any
 
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
@@ -14,7 +14,7 @@ from cmk.gui.plugins.wato.utils import (
 from cmk.gui.valuespec import Dictionary, ListOf, Migrate, MonitoringState, TextInput
 
 
-def _parameter_valuespec_hostsystem_sensors():
+def _parameter_valuespec_hostsystem_sensors() -> Migrate[dict[str, Any]]:
     return Migrate(
         valuespec=Dictionary(
             elements=[

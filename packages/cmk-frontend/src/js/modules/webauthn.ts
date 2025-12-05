d612f42f8e146b6c9149240dd6d38b5389a97e2a
Zoey Hilton <zoey.hilton@checkmk.com>
2025-12-05 13:34:11 +0100

    Fix python-fido2 1.x to 2.0 migration
    - CMK-28289
    - Updated the registered and login process to use RegistrationResponse and AuthenticationResponse
    
    Change-Id: Ice1cfed7984a561eb0b8984fd31bb8964e0bb337

--- packages/cmk-frontend/src/js/modules/webauthn.ts
+++ packages/cmk-frontend/src/js/modules/webauthn.ts
@@ -83,6 +83,7 @@ export function register() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
+          credentialId: btoa(String.fromCharCode(...new Uint8Array(attestationPkc.rawId))),
           attestationObject: btoa(
             String.fromCharCode(...new Uint8Array(attestationPkcResponse.attestationObject))
           ),

ff404f0402b82a97a4dff1db88969b1283147c39
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-12-07 17:15:29 +0100

    migrate eltek_systemstatus I
    Change-Id: I699c7d5da9cb81965d2f34dc4f2f4e3497dcbe51

--- cmk/base/legacy_checks/eltek_systemstatus.py
+++ cmk/base/legacy_checks/eltek_systemstatus.py
@@ -3,45 +3,56 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # .1.3.6.1.4.1.12148.9.2.2.0 1 --> ELTEK-DISTRIBUTED-MIB::systemOperationalStatus.0
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.eltek.lib import DETECT_ELTEK
 
-check_info = {}
+
+def inventory_eltek_systemstatus(section: StringTable) -> DiscoveryResult:
+    yield Service()
 
 
-def inventory_eltek_systemstatus(info):
-    return [(None, None)]
-
-
-def check_eltek_systemstatus(_no_item, _no_params, info):
+def check_eltek_systemstatus(section: StringTable) -> CheckResult:
     map_state = {
-        "0": (2, "float, voltage regulated"),
-        "1": (0, "float, temperature comp. regulated"),
-        "2": (2, "battery boost"),
-        "3": (2, "battery test"),
+        "0": (State.CRIT, "float, voltage regulated"),
+        "1": (State.OK, "float, temperature comp. regulated"),
+        "2": (State.CRIT, "battery boost"),
+        "3": (State.CRIT, "battery test"),
     }
-    state, state_readable = map_state[info[0][0]]
-    return state, "Operational status: %s" % state_readable
+    state, state_readable = map_state[section[0][0]]
+    yield Result(state=state, summary="Operational status: %s" % state_readable)
+    return
 
 
 def parse_eltek_systemstatus(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["eltek_systemstatus"] = LegacyCheckDefinition(
+snmp_section_eltek_systemstatus = SimpleSNMPSection(
     name="eltek_systemstatus",
-    parse_function=parse_eltek_systemstatus,
     detect=DETECT_ELTEK,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.12148.9.2",
         oids=["2"],
     ),
+    parse_function=parse_eltek_systemstatus,
+)
+
+
+check_plugin_eltek_systemstatus = CheckPlugin(
+    name="eltek_systemstatus",
     service_name="System Status",
     discovery_function=inventory_eltek_systemstatus,
     check_function=check_eltek_systemstatus,

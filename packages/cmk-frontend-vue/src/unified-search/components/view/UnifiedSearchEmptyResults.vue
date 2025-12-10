7302f46343f556ee4988ab1d06e99ff0828535f4
Benjamin Knapp <benjamin.knapp@checkmk.com>
2025-12-10 10:14:53 +0100

    fix(unified-search): use paragrah instead of div for no result msg
    CMK-28299
    
    Change-Id: Iac7043112a35ad09b559224e120ac00f773e6eae

--- packages/cmk-frontend-vue/src/unified-search/components/view/UnifiedSearchEmptyResults.vue
+++ packages/cmk-frontend-vue/src/unified-search/components/view/UnifiedSearchEmptyResults.vue
@@ -11,6 +11,7 @@ import usei18n from '@/lib/i18n'
 import CmkAlertBox from '@/components/CmkAlertBox.vue'
 import CmkButton from '@/components/CmkButton.vue'
 import CmkIcon from '@/components/CmkIcon'
+import CmkParagraph from '@/components/typography/CmkParagraph.vue'
 
 import type { HistoryEntry } from '@/unified-search/lib/searchHistory'
 import type { UnifiedSearchError } from '@/unified-search/lib/unified-search'
@@ -92,9 +93,9 @@ onBeforeUnmount(() => {
     </CmkAlertBox>
     <template v-else>
       <div class="shruggy">{{ _t('¯\\_(ツ)_/¯') }}</div>
-      <div>
+      <CmkParagraph>
         {{ _t('No results found for your search') }}
-      </div>
+      </CmkParagraph>
     </template>
 
     <CmkButton ref="reset-button" class="reset-button" @click.stop="searchUtils.resetSearch()">

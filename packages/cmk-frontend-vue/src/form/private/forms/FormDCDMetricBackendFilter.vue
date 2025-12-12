78d90f475434d2726e366bf6e84e825ef3b9d7ad
Max Höhl <max.hoehl@checkmk.com>
2025-12-12 16:11:51 +0100

    Add FormDCDMetricBackend form spec
    Jira: CMK-28377
    Change-Id: I09d01e1737f324187e9f89601a6cb17db793f964

---
+++ packages/cmk-frontend-vue/src/form/private/forms/FormDCDMetricBackendFilter.vue
@@ -0,0 +1,37 @@
+<!--
+Copyright (C) 2025 Checkmk GmbH - License: Checkmk Enterprise License
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+
+<script setup lang="ts">
+import type { DcdMetricBackendFilter } from 'cmk-shared-typing/typescript/vue_formspec_components'
+
+import { type ValidationMessages } from '@/form/private/validation'
+
+import FormMetricBackendAttributes from '@/metric-backend/FormMetricBackendAttributes.vue'
+
+defineProps<{
+  spec: DcdMetricBackendFilter
+  backendValidation: ValidationMessages
+}>()
+
+const data = defineModel<DcdMetricBackendFilter>('data', { required: true })
+</script>
+
+<template>
+  <FormMetricBackendAttributes
+    v-model:resource-attributes="data.resource_attributes"
+    v-model:scope-attributes="data.scope_attributes"
+    v-model:data-point-attributes="data.data_point_attributes"
+    :strict="false"
+    :disable-values-on-empty-key="true"
+  />
+</template>
+
+<style scoped>
+table {
+  border-collapse: separate;
+  border-spacing: 5px;
+}
+</style>

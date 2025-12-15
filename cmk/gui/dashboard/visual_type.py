59f2b48116766660ee26712ce30ffd9b6f077ffc
Wontek Hong <wontek.hong@checkmk.com>
2025-12-15 14:26:27 +0100

    dashboard/visual_type: adjust to add_visual_handler to handle views
    CMK-28109
    
    Change-Id: I42a6cf02824e8017001fc9d0733a5c7921305d06

--- cmk/gui/dashboard/visual_type.py
+++ cmk/gui/dashboard/visual_type.py
@@ -7,6 +7,7 @@
 # mypy: disable-error-code="type-arg"
 
 import json
+import uuid
 from collections.abc import Iterator
 from typing import cast, override
 
@@ -21,11 +22,15 @@ from cmk.gui.http import Request, response
 from cmk.gui.i18n import _
 from cmk.gui.logged_in import user
 from cmk.gui.page_menu import make_javascript_link, PageMenuEntry
-from cmk.gui.type_defs import IconNames, StaticIcon, VisualContext
+from cmk.gui.type_defs import DashboardEmbeddedViewSpec, IconNames, StaticIcon, VisualContext
 from cmk.gui.utils.roles import UserPermissions
 from cmk.gui.visuals.type import VisualType
 
-from .dashlet import copy_view_into_dashlet, dashlet_registry, DashletConfig
+from .dashlet import (
+    copy_view_into_dashlet,
+    dashlet_registry,
+    DashletConfig,
+)
 from .store import (
     add_dashlet,
     get_all_dashboards,
@@ -36,6 +41,7 @@ from .type_defs import (
     ABCGraphDashletConfig,
     DashboardConfig,
     DashboardName,
+    EmbeddedViewDashletConfig,
     ViewDashletConfig,
 )
 
@@ -100,6 +106,8 @@ class VisualTypeDashboards(VisualType):
         parameters: dict,
         user_permissions: UserPermissions,
     ) -> None:
+        # TODO: this function really warrants some refactoring, there are more comments than code
+        #  at this point (the used bits are the ones which should be refactored though)
         if not user.may("general.edit_dashboards"):
             # Exceptions do not work here.
             return
@@ -139,21 +147,28 @@ class VisualTypeDashboards(VisualType):
         dashlet_spec["context"] = context
         if add_type == "view":
             view_name = parameters["name"]
-        else:
-            # We don't know if what we get as parameters actually fits a DashletConfig.
-            dashlet_spec.update(parameters)  # type: ignore[typeddict-item]
-
-        # When a view shall be added to the dashboard, load the view and put it into the dashlet
-        # FIXME: Move this to the dashlet plugins
-        if add_type == "view":
             dashlet_spec = cast(ViewDashletConfig, dashlet_spec)
-            # save the original context and override the context provided by the view
-            context = dashlet_spec["context"]
             copy_view_into_dashlet(
-                request, dashlet_spec, len(dashboard["dashlets"]), view_name, add_context=context
+                request,
+                dashlet_spec,
+                len(dashboard["dashlets"]),
+                view_name,
+                add_context=context,
             )
 
-        elif add_type in ["pnpgraph", "custom_graph"]:
+            view_spec, embedded_view_dashlet = self._create_embedded_view_spec_and_dashlet(
+                dashlet_spec
+            )
+            dashboard.setdefault("embedded_views", {})[embedded_view_dashlet["name"]] = view_spec
+            add_dashlet(embedded_view_dashlet, dashboard)
+            # Directly go to the dashboard in edit mode. We send the URL as an answer
+            # to the AJAX request
+            response.set_data("OK dashboard.py?name=" + target_visual_name + "&edit=1")
+            return None
+
+        # We don't know if what we get as parameters actually fits a DashletConfig.
+        dashlet_spec.update(parameters)  # type: ignore[typeddict-item]
+        if add_type in ["pnpgraph", "custom_graph"]:
             dashlet_spec = cast(ABCGraphDashletConfig, dashlet_spec)
 
             # The "add to visual" popup does not provide a timerange information,
@@ -202,3 +217,68 @@ class VisualTypeDashboards(VisualType):
             _("Graph specification '%s' is insufficient for dashboard.")
             % graph_specification.graph_type
         )
+
+    @staticmethod
+    def _create_embedded_view_spec_and_dashlet(
+        dashlet_spec: ViewDashletConfig,
+    ) -> tuple[DashboardEmbeddedViewSpec, EmbeddedViewDashletConfig]:
+        embedded_view_id = str(uuid.uuid4())
+
+        view_spec: DashboardEmbeddedViewSpec = {
+            "single_infos": dashlet_spec["single_infos"],
+            "datasource": dashlet_spec["datasource"],
+            "layout": dashlet_spec["layout"],
+            "group_painters": dashlet_spec["group_painters"],
+            "painters": dashlet_spec["painters"],
+            "browser_reload": dashlet_spec["browser_reload"],
+            "num_columns": dashlet_spec["num_columns"],
+            "column_headers": dashlet_spec["column_headers"],
+            "sorters": dashlet_spec["sorters"],
+        }
+
+        if "add_headers" in dashlet_spec:
+            view_spec["add_headers"] = dashlet_spec["add_headers"]
+
+        if "mobile" in dashlet_spec:
+            view_spec["mobile"] = dashlet_spec["mobile"]
+
+        if "mustsearch" in dashlet_spec:
+            view_spec["mustsearch"] = dashlet_spec["mustsearch"]
+
+        if "force_checkboxes" in dashlet_spec:
+            view_spec["force_checkboxes"] = dashlet_spec["force_checkboxes"]
+
+        if "user_sortable" in dashlet_spec:
+            view_spec["user_sortable"] = dashlet_spec["user_sortable"]
+
+        if "play_sounds" in dashlet_spec:
+            view_spec["play_sounds"] = dashlet_spec["play_sounds"]
+
+        if "inventory_join_macros" in dashlet_spec:
+            view_spec["inventory_join_macros"] = dashlet_spec["inventory_join_macros"]
+
+        if "modified_at" in dashlet_spec:
+            view_spec["modified_at"] = dashlet_spec["modified_at"]
+
+        embedded_view_dashlet = EmbeddedViewDashletConfig(
+            {
+                "type": "embedded_view",
+                # We use the defaults for embedded views
+                "position": dashlet_registry["embedded_view"].initial_position(),
+                "size": dashlet_registry["embedded_view"].initial_size(),
+                "show_title": True,  # Defaults to True for now
+                "name": embedded_view_id,
+                "datasource": dashlet_spec["datasource"],
+            }
+        )
+
+        if "context" in dashlet_spec:
+            embedded_view_dashlet["context"] = dashlet_spec["context"]
+
+        if "title" in dashlet_spec:
+            embedded_view_dashlet["title"] = dashlet_spec["title"]
+
+        if "title_url" in dashlet_spec:
+            embedded_view_dashlet["title_url"] = dashlet_spec["title_url"]
+
+        return view_spec, embedded_view_dashlet

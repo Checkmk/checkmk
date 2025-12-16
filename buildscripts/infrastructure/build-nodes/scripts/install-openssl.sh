3dcafc5b86673e2498ee83684bb5b1e7e8f79fea
Nigel Banks <nigel.banks@checkmk.com>
2025-12-16 13:51:44 +0100

    Revert^4 "18986 Update OpenSSL to version 3.5.4 (LTS)"
    This reverts commit 9c7fb4b20691a3ed08177c5bb88e376509245009.
    
    Reason for revert: Time to retry getting this to work.
    
    CMK-27317
    
    Change-Id: Idcf85540f5b030cdc366ebf88b21849c5560927b

--- buildscripts/infrastructure/build-nodes/scripts/install-openssl.sh
+++ buildscripts/infrastructure/build-nodes/scripts/install-openssl.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck source=buildscripts/infrastructure/build-nodes/scripts/build_lib.sh
 . "${SCRIPT_DIR}/build_lib.sh"
 
-OPENSSL_VERSION=3.0.19
+OPENSSL_VERSION=3.5.4
 DIR_NAME=openssl-${OPENSSL_VERSION}
 ARCHIVE_NAME=${DIR_NAME}.tar.gz
 TARGET_DIR="${TARGET_DIR:-/opt}"

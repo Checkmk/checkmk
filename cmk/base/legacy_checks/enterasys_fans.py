fb6374f8cb84ff365f12eb181c79e5c412541e5c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-12-18 13:43:37 +0100

    Migrate enterasys_fans check plugin to new API (in-place)
    Change-Id: Ibea5cc09917eda681dba515ab387cde653c8a8a9

--- cmk/base/legacy_checks/enterasys_fans.py
+++ cmk/base/legacy_checks/enterasys_fans.py
@@ -6,47 +6,63 @@
 # mypy: disable-error-code="no-untyped-def"
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    OIDEnd,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.enterasys.lib import DETECT_ENTERASYS
 
-check_info = {}
+
+def inventory_enterasys_fans(section: StringTable) -> DiscoveryResult:
+    for num, state in section:
+        if state != "2":
+            yield Service(item=num)
 
 
-def inventory_enterasys_fans(info):
-    return [(x[0], None) for x in info if x[1] != "2"]
-
-
-def check_enterasys_fans(item, _no_params, info):
+def check_enterasys_fans(item: str, section: StringTable) -> CheckResult:
     fan_states = {
         "1": "info not available",
         "2": "not installed",
         "3": "installed and operating",
         "4": "installed and not operating",
     }
-    for num, state in info:
+    for num, state in section:
         if num == item:
-            message = "FAN State: %s" % (fan_states[state])
+            message = f"FAN State: {fan_states[state]}"
             if state in ["1", "2"]:
-                return 3, message
-            if state == "4":
-                return 2, message
-            return 0, message
-    return None
+                yield Result(state=State.UNKNOWN, summary=message)
+            elif state == "4":
+                yield Result(state=State.CRIT, summary=message)
+            else:
+                yield Result(state=State.OK, summary=message)
+            return
 
 
 def parse_enterasys_fans(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["enterasys_fans"] = LegacyCheckDefinition(
+snmp_section_enterasys_fans = SimpleSNMPSection(
     name="enterasys_fans",
-    parse_function=parse_enterasys_fans,
     detect=DETECT_ENTERASYS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.52.4.3.1.3.1.1",
         oids=[OIDEnd(), "2"],
     ),
+    parse_function=parse_enterasys_fans,
+)
+
+
+check_plugin_enterasys_fans = CheckPlugin(
+    name="enterasys_fans",
     service_name="FAN %s",
     discovery_function=inventory_enterasys_fans,
     check_function=check_enterasys_fans,

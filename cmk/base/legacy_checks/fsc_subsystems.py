c530bcc8f3795e770493b0c268c17585f78e0b2c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-12-21 20:01:51 +0100

    migrate fsc_subsystems I
    Change-Id: I64f3f83187d4c443382cdf1bb4a5fae69e5e4038

--- cmk/base/legacy_checks/fsc_subsystems.py
+++ cmk/base/legacy_checks/fsc_subsystems.py
@@ -6,50 +6,55 @@
 # mypy: disable-error-code="no-untyped-def"
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
 from cmk.agent_based.v2 import (
     all_of,
     any_of,
+    CheckPlugin,
+    CheckResult,
     DiscoveryResult,
     exists,
+    Result,
     Service,
+    SimpleSNMPSection,
     SNMPTree,
     startswith,
+    State,
     StringTable,
 )
 
-check_info = {}
+
+def discover_fsc_subsystems(section: StringTable) -> DiscoveryResult:
+    yield from (Service(item=line[0]) for line in section if int(line[1]) > 0)
 
 
-def discover_fsc_subsystems(string_table: StringTable) -> DiscoveryResult:
-    yield from (Service(item=line[0]) for line in string_table if int(line[1]) > 0)
-
-
-def check_fsc_subsystems(item, _no_params, info):
-    for line in info:  # , value1, value2 in info:
+def check_fsc_subsystems(item: str, section: StringTable) -> CheckResult:
+    for line in section:
         name = line[0]
         if name != item:
             continue
         if line[1] == "":
-            return 3, "Status not found in SNMP data"
+            yield Result(state=State.UNKNOWN, summary="Status not found in SNMP data")
+            return
         status = int(line[1])
         statusname = {1: "ok", 2: "degraded", 3: "error", 4: "failed", 5: "unknown-init"}.get(
             status, "invalid"
         )
         if status in {1, 5}:
-            return (0, "%s - no problems" % statusname)
+            yield Result(state=State.OK, summary=f"{statusname} - no problems")
+            return
         if 2 <= status <= 4:
-            return (2, "%s" % statusname)
-        return (3, "unknown status %d" % status)
+            yield Result(state=State.CRIT, summary=f"{statusname}")
+            return
+        yield Result(state=State.UNKNOWN, summary=f"unknown status {status}")
+        return
 
 
 def parse_fsc_subsystems(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["fsc_subsystems"] = LegacyCheckDefinition(
+snmp_section_fsc_subsystems = SimpleSNMPSection(
     name="fsc_subsystems",
-    parse_function=parse_fsc_subsystems,
     detect=all_of(
         any_of(
             startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.231"),
@@ -62,6 +67,12 @@ check_info["fsc_subsystems"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.231.2.10.2.11.3.1.1",
         oids=["2", "3"],
     ),
+    parse_function=parse_fsc_subsystems,
+)
+
+
+check_plugin_fsc_subsystems = CheckPlugin(
+    name="fsc_subsystems",
     service_name="FSC %s",
     discovery_function=discover_fsc_subsystems,
     check_function=check_fsc_subsystems,

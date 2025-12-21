a5aded69e1d41f8b5e1add859b473cc209cd3683
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-12-21 20:15:43 +0100

    migrate genua_state_correlation I
    Change-Id: I225884612e15400f55a92c475f17b3568dcd7b9d

--- cmk/base/legacy_checks/genua_state_correlation.py
+++ cmk/base/legacy_checks/genua_state_correlation.py
@@ -8,11 +8,18 @@
 
 from collections.abc import Sequence
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.genua.lib import DETECT_GENUA
-
-check_info = {}
 
 # Example Agent Output:
 # GENUA-MIB:
@@ -29,18 +36,17 @@ check_info = {}
 # .1.3.6.1.4.1.3137.2.1.2.1.7.10 = INTEGER: 2
 
 
-def inventory_genua_state(info):
+def inventory_genua_state(section: Sequence[StringTable]) -> DiscoveryResult:
     # remove empty elements due to two alternative enterprise ids in snmp_info
-    info = [_f for _f in info if _f]
-    if info and info[0]:
+    section = [_f for _f in section if _f]
+    if section and section[0]:
         numifs = 0
-        for _ifIndex, _ifName, _ifType, _ifLinkState, ifCarpState in info[0]:
+        for _ifIndex, _ifName, _ifType, _ifLinkState, ifCarpState in section[0]:
             if ifCarpState in ["0", "1", "2"]:
                 numifs += 1
         # inventorize only if we find at least two carp interfaces
         if numifs > 1:
-            return [(None, None)]
-    return None
+            yield Service()
 
 
 def genua_state_str(st):
@@ -52,16 +58,17 @@ def genua_state_str(st):
     return names.get(st, st)
 
 
-def check_genua_state(item, _no_params, info):
+def check_genua_state(section: Sequence[StringTable]) -> CheckResult:
     # remove empty elements due to two alternative enterprise ids in snmp_info
-    info = [_f for _f in info if _f]
-    if not info[0]:
-        return (3, "Invalid Output from Agent")
+    section = [_f for _f in section if _f]
+    if not section[0]:
+        yield Result(state=State.UNKNOWN, summary="Invalid Output from Agent")
+        return
 
     state = 0
     carp_info = []
 
-    for ifIndex, ifName, ifType, ifLinkState, ifCarpState in info[0]:
+    for ifIndex, ifName, ifType, ifLinkState, ifCarpState in section[0]:
         if ifType == "6":
             carp_info.append((ifIndex, ifName, ifType, ifLinkState, ifCarpState))
 
@@ -75,18 +82,18 @@ def check_genua_state(item, _no_params, info):
     output = "Number of carp IFs in states "
     for i in ("0", "1", "2"):
         output += genua_state_str(i)
-        output += ":%d " % carp_states[int(i)]
+        output += f":{carp_states[int(i)]} "
 
-    return (state, output)
+    yield Result(state=State(state), summary=output)
+    return
 
 
 def parse_genua_state_correlation(string_table: Sequence[StringTable]) -> Sequence[StringTable]:
     return string_table
 
 
-check_info["genua_state_correlation"] = LegacyCheckDefinition(
+snmp_section_genua_state_correlation = SNMPSection(
     name="genua_state_correlation",
-    parse_function=parse_genua_state_correlation,
     detect=DETECT_GENUA,
     fetch=[
         SNMPTree(
@@ -98,6 +105,12 @@ check_info["genua_state_correlation"] = LegacyCheckDefinition(
             oids=["1", "2", "3", "4", "7"],
         ),
     ],
+    parse_function=parse_genua_state_correlation,
+)
+
+
+check_plugin_genua_state_correlation = CheckPlugin(
+    name="genua_state_correlation",
     service_name="Carp Correlation",
     discovery_function=inventory_genua_state,
     check_function=check_genua_state,

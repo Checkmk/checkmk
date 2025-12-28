6787439e98d1e2bcb85039c1f5b832cf9cb1dc73
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-12-28 15:02:10 +0100

    migrate pandacom_sys_temp I
    Change-Id: I258319b989faa52a8ee9afe99d9b4e6e03265fd4

--- cmk/base/legacy_checks/pandacom_sys_temp.py
+++ cmk/base/legacy_checks/pandacom_sys_temp.py
@@ -3,39 +3,48 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.base.check_legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamDict
 from cmk.plugins.pandacom.lib import DETECT_PANDACOM
-
-check_info = {}
 
 
 def parse_pandacom_sys_temp(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-def inventory_pandacom_sys_temp(info):
-    return [("System", {})]
+def inventory_pandacom_sys_temp(section: StringTable) -> DiscoveryResult:
+    yield Service(item="System")
 
 
-def check_pandacom_sys_temp(item, params, info):
-    return check_temperature(int(info[0][0]), params, "pandacom_sys_%s" % item)
+def check_pandacom_sys_temp(item: str, params: TempParamDict, section: StringTable) -> CheckResult:
+    yield from check_temperature(int(section[0][0]), params)
 
 
-check_info["pandacom_sys_temp"] = LegacyCheckDefinition(
+snmp_section_pandacom_sys_temp = SimpleSNMPSection(
     name="pandacom_sys_temp",
-    parse_function=parse_pandacom_sys_temp,
     detect=DETECT_PANDACOM,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3652.3.1.1",
         oids=["6"],
     ),
+    parse_function=parse_pandacom_sys_temp,
+)
+
+
+check_plugin_pandacom_sys_temp = CheckPlugin(
+    name="pandacom_sys_temp",
     service_name="Temperature %s",
     discovery_function=inventory_pandacom_sys_temp,
     check_function=check_pandacom_sys_temp,
     check_ruleset_name="temperature",
-    check_default_parameters={"levels": (35.0, 40.0)},
+    check_default_parameters=TempParamDict(levels=(35.0, 40.0)),
 )

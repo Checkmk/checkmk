c97c7953275884b8598e2a505b51df4053831bd4
Moritz Kiemer <moritz.kiemer@checkmk.com>
2025-12-28 14:23:57 +0100

    migrate pandacom_fan I
    Change-Id: I0f4053558068e4c8af5dd5c1997eb15bf54de449

--- cmk/base/legacy_checks/pandacom_fan.py
+++ cmk/base/legacy_checks/pandacom_fan.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # .1.3.6.1.4.1.3652.3.2.3.1.2.1 3 --> SPEEDCARRIER-MIB::nmFanGroupStatus.1
 # .1.3.6.1.4.1.3652.3.2.3.1.2.2 3 --> SPEEDCARRIER-MIB::nmFanGroupStatus.2
 # .1.3.6.1.4.1.3652.3.2.3.1.2.3 3 --> SPEEDCARRIER-MIB::nmFanGroupStatus.3
@@ -17,46 +15,60 @@
 # .1.3.6.1.4.1.3652.3.2.3.1.2.10 3 --> SPEEDCARRIER-MIB::nmFanGroupStatus.10
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.pandacom.lib import DETECT_PANDACOM
 
-check_info = {}
+
+def inventory_pandacom_fan(section: StringTable) -> DiscoveryResult:
+    for fan_nr, fan_state in section:
+        if fan_state not in ["0", "5"]:
+            yield Service(item=fan_nr)
 
 
-def inventory_pandacom_fan(info):
-    return [(fan_nr, None) for fan_nr, fan_state in info if fan_state not in ["0", "5"]]
-
-
-def check_pandacom_fan(item, params, info):
+def check_pandacom_fan(item: str, section: StringTable) -> CheckResult:
     map_fan_state = {
-        "0": (3, "not available"),
-        "1": (0, "on"),
-        "2": (2, "off"),
-        "3": (0, "pass"),
-        "4": (2, "fail"),
-        "5": (3, "not installed"),
-        "6": (0, "auto"),
+        "0": (State.UNKNOWN, "not available"),
+        "1": (State.OK, "on"),
+        "2": (State.CRIT, "off"),
+        "3": (State.OK, "pass"),
+        "4": (State.CRIT, "fail"),
+        "5": (State.UNKNOWN, "not installed"),
+        "6": (State.OK, "auto"),
     }
-    for fan_nr, fan_state in info:
+    for fan_nr, fan_state in section:
         if fan_nr == item:
             state, state_readable = map_fan_state[fan_state]
-            return state, "Operational status: %s" % state_readable
-    return None
+            yield Result(state=state, summary=f"Operational status: {state_readable}")
+            return
 
 
 def parse_pandacom_fan(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["pandacom_fan"] = LegacyCheckDefinition(
+snmp_section_pandacom_fan = SimpleSNMPSection(
     name="pandacom_fan",
-    parse_function=parse_pandacom_fan,
     detect=DETECT_PANDACOM,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3652.3.2.3.1",
         oids=["1", "2"],
     ),
+    parse_function=parse_pandacom_fan,
+)
+
+
+check_plugin_pandacom_fan = CheckPlugin(
+    name="pandacom_fan",
     service_name="Fan %s",
     discovery_function=inventory_pandacom_fan,
     check_function=check_pandacom_fan,

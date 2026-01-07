9b151bc972b1e23920632c75e9716168d7b8a686
Anastasiia Shevchuk <anastasiia.shevchuk@checkmk.com>
2026-01-07 13:36:40 +0100

    Fix and temporary skip 'test_all_packages_pinned'
    * Fix the path to the 'dev-requirements.in' file
    * Temporary skip the tests as not all requirements are fixed yet
    
    Change-Id: Ife9763c8c6d7617a8f7253cfb16728cd0bc0d6b0

--- tests/code_quality/test_requirements.py
+++ tests/code_quality/test_requirements.py
@@ -44,8 +44,12 @@ IGNORED_LIBS |= {"__future__"}  # other builtin stuff
 
 
 def requirements_files() -> dict[str, list[Path]]:
-    dev_req_files_list = list((repo_path() / "packages").glob("*/dev-requirements.in")) + list(
-        (repo_path() / "non-free" / "packages").glob("*/dev-requirements.in")
+    dev_req_files_list = (
+        [
+            repo_path() / "tests/dev-requirements.in",
+        ]
+        + list((repo_path() / "packages").glob("*/dev-requirements.in"))
+        + list((repo_path() / "non-free" / "packages").glob("*/dev-requirements.in"))
     )
 
     runtime_req_files_list = (
@@ -126,7 +130,7 @@ def loaded_requirements():
     return load_requirements("all")
 
 
-@pytest.mark.skip("requirements are pinned with `constraints.txt`, now")
+@pytest.mark.skip(reason="Not all requirements are pinned yet")
 @pytest.mark.skipif(
     branch_from_env(env_var="GERRIT_BRANCH", fallback=current_base_branch_name) == "master",
     reason="pinning is only enforced in release branches",

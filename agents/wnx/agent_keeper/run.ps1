484e55a6f57c9fadd7599e5a74670ad63c6427a5
Nigel Banks <nigel.banks@checkmk.com>
2026-01-07 12:47:09 +0100

    Fix short path for 2.5.0
    Change-Id: I2c0be7e08abf1d30f98b5124d68ce583e9325964

--- agents/wnx/agent_keeper/run.ps1
+++ agents/wnx/agent_keeper/run.ps1
@@ -36,7 +36,7 @@ $packDoc = $false
 # repo/branch specific short path
 # TODO(sk): move it to CI upon confirmation that screen works as intended
 $shortenPath = "workdir\workspace\checkmk\2.5.0"
-$shortenLink = "ym"
+$shortenLink = "y5"
 
 if ("$env:arg_var_value" -ne "") {
     $env:arg_val_name = $env:arg_var_value

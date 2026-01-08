5b30af2f75307a58aca747356a4073b39513b763
Matteo Stifano <matteo.stifano@checkmk.com>
2026-01-08 11:13:17 +0100

    test_update: Ignore agent-bakery errors in log files
    We encounter an error in web.log when updating a site
    that uses a OpenSSL version older than the one
    installed in the system (see SUP-26629).
    
    This error needs to be ignored since no fix can be
    applied to older versions.
    
    Here we use a more generic regex-pattern to ignore
    such an error since the previous pattern was not catching
    all error-lines in the log, leading to the test failure.
    
    CMK-28862
    
    Change-Id: I9975153869005ccd041f21fbbedf9ed1968f99f6

--- tests/update/helpers.py
+++ tests/update/helpers.py
@@ -12,9 +12,13 @@ def check_errors_in_log_files(site: Site) -> None:
     # * TODO: Remove "sigterm" lookahead from pattern after CMK-24766 is done
     # * TODO: Remove "Interface_2" lookahead from pattern after CMK-18603 is done
     # * TODO: Remove "wsgi" lookahead from pattern after CMK-27248 is done
-    # * using OPENSSL version > 3.4.0 in test-containers leads to an error in web.log when starting
-    #   a cmk site with version <= 2.3.0p40
-    content_pattern = "^(?!.*(sigterm))(?!.*(Interface_2.rrd))(?!.*(wsgi))(?!.*(Error building RPM packet)).*error.*$"
+    # * using OPENSSL version > 3.4.0 in test-containers leads to an error in web.log, related to
+    #   the agent bakery, when starting a cmk site with version <= 2.3.0p40.
+    #   This issue is reported in SUP-26629. It needs to be ignored since no fix can be applied to
+    #   older versions.
+    content_pattern = (
+        "^(?!.*(sigterm))(?!.*(Interface_2.rrd))(?!.*(wsgi))(?!.*(bake-agents)).*error.*$"
+    )
 
     error_match_dict = parse_files(
         path_name=site.logs_dir,

039bcae02031d77421f38f3360a66cb3807543b9
Dennis Schmalacker <dennis.schmalacker@checkmk.com>
2026-01-08 17:28:04 +0100

    18734 FIX Stormshield: Change OID for SNMP detection
    Stormshield products updating to firmware version 4.8 have changed their OID.
    Detect all Stormshield products by using a more general OID starting with their enterprise OID .1.3.6.1.4.1.11256.
    
    SUP-27032
    
    Change-Id: I3dbd642cf461ce4b7a36c16aa62bb74f8d091728

--- cmk/plugins/stormshield/agent_based/stormshield_cluster.py
+++ cmk/plugins/stormshield/agent_based/stormshield_cluster.py
@@ -9,21 +9,17 @@
 
 
 from cmk.agent_based.v2 import (
-    all_of,
-    any_of,
     CheckPlugin,
     CheckResult,
     DiscoveryResult,
-    equals,
-    exists,
     Result,
     Service,
     SimpleSNMPSection,
     SNMPTree,
-    startswith,
     State,
     StringTable,
 )
+from cmk.plugins.stormshield.lib import DETECT_STORMSHIELD_CLUSTER
 
 sync_name_mapping = {
     "1": "Synced",
@@ -75,13 +71,7 @@ def parse_stormshield_cluster(string_table: StringTable) -> StringTable | None:
 
 snmp_section_stormshield_cluster = SimpleSNMPSection(
     name="stormshield_cluster",
-    detect=all_of(
-        any_of(
-            startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072.3.2.8"),
-            equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.2.0"),
-        ),
-        exists(".1.3.6.1.4.1.11256.1.11.*"),
-    ),
+    detect=DETECT_STORMSHIELD_CLUSTER,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.11256.1.11",
         oids=["1", "2", "3", "5", "6", "8"],
--- cmk/plugins/stormshield/agent_based/stormshield_cluster_node.py
+++ cmk/plugins/stormshield/agent_based/stormshield_cluster_node.py
@@ -8,23 +8,19 @@ from dataclasses import dataclass
 from typing import Any, Literal
 
 from cmk.agent_based.v2 import (
-    all_of,
-    any_of,
     check_levels,
     CheckPlugin,
     CheckResult,
     DiscoveryResult,
-    equals,
-    exists,
     render,
     Result,
     Service,
     SimpleSNMPSection,
     SNMPTree,
-    startswith,
     State,
     StringTable,
 )
+from cmk.plugins.stormshield.lib import DETECT_STORMSHIELD_CLUSTER
 
 
 @dataclass(frozen=True)
@@ -105,13 +101,7 @@ def check_stormshield_cluster_node(
 
 snmp_section_stormshield_cluster_node = SimpleSNMPSection(
     name="stormshield_cluster_node",
-    detect=all_of(
-        any_of(
-            startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072.3.2.8"),
-            equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.2.0"),
-        ),
-        exists(".1.3.6.1.4.1.11256.1.11.*"),
-    ),
+    detect=DETECT_STORMSHIELD_CLUSTER,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.11256.1.11.7.1",
         oids=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],

78e56858c7021a32ee117ced283b3fd266130cb3
Lars Michelsen <lm@checkmk.com>
2026-01-08 21:03:19 +0100

    Clean up no-untyped-def #381-385
    Change-Id: Ia1d4c94b66eb1cc7209bdbb46d89d10d6e661814

--- cmk/gui/plugins/wato/check_parameters/hp_hh3c_ext_states.py
+++ cmk/gui/plugins/wato/check_parameters/hp_hh3c_ext_states.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
@@ -14,7 +12,7 @@ from cmk.gui.plugins.wato.utils import (
 from cmk.gui.valuespec import Dictionary, MonitoringState, TextInput
 
 
-def _parameter_valuespec_hp_hh3c_ext_states():
+def _parameter_valuespec_hp_hh3c_ext_states() -> Dictionary:
     return Dictionary(
         elements=[
             (

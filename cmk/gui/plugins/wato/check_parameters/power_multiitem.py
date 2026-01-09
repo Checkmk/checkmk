112be1924cf21721dfbb16ca539b5ad1ae1ae827
Lars Michelsen <lm@checkmk.com>
2026-01-09 17:42:42 +0100

    Clean up no-untyped-def #491-495
    Change-Id: I43bef9ee39c862b727ccf0bb67679a318cbe2d73

--- cmk/gui/plugins/wato/check_parameters/power_multiitem.py
+++ cmk/gui/plugins/wato/check_parameters/power_multiitem.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
@@ -14,14 +12,14 @@ from cmk.gui.plugins.wato.utils import (
 from cmk.gui.valuespec import Dictionary, Integer, TextInput, Tuple
 
 
-def _item_spec_power_multiitem():
+def _item_spec_power_multiitem() -> TextInput:
     return TextInput(
         title=_("Component"),
         help=_("The identifier of device component the consumed power is related to."),
     )
 
 
-def _parameter_valuespec_power_multiitem():
+def _parameter_valuespec_power_multiitem() -> Dictionary:
     return Dictionary(
         elements=[
             (

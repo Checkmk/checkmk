22dd6794bf074469d94556cd772f117eecd3601b
Lars Michelsen <lm@checkmk.com>
2026-01-09 14:24:19 +0100

    Clean up no-untyped-def #471-475
    Change-Id: I4c647a31174223fce5c17cca79c53fe6c24237f7

--- cmk/gui/plugins/wato/check_parameters/jira_custom_svc.py
+++ cmk/gui/plugins/wato/check_parameters/jira_custom_svc.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
@@ -14,7 +12,7 @@ from cmk.gui.plugins.wato.utils import (
 from cmk.gui.valuespec import Age, Dictionary, Float, Integer, TextInput, Tuple
 
 
-def _parameter_valuespec_jira_custom_svc():
+def _parameter_valuespec_jira_custom_svc() -> Dictionary:
     return Dictionary(
         elements=[
             (

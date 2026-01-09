6451448888e45f3f478d089a2aa946ea4738934d
Lars Michelsen <lm@checkmk.com>
2026-01-09 14:13:11 +0100

    Clean up no-untyped-def #461-465
    Remove no-untyped-def suppressions and add return type annotations (Dictionary) to _parameter_valuespec_* functions in:
    - oracle_longactivesessions.py
    - oracle_locks.py
    - cisco_meraki.py
    - heartbeat_rscstatus.py
    - varnish_worker.py
    
    Change-Id: I3684a627e7fcdbba66434123c7f623fe8cb91f78

--- cmk/gui/plugins/wato/check_parameters/cisco_meraki.py
+++ cmk/gui/plugins/wato/check_parameters/cisco_meraki.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
@@ -14,7 +12,7 @@ from cmk.gui.plugins.wato.utils import (
 from cmk.gui.valuespec import Age, Dictionary, TextInput, Tuple
 
 
-def _parameter_valuespec_cisco_meraki_org_licenses_overview():
+def _parameter_valuespec_cisco_meraki_org_licenses_overview() -> Dictionary:
     return Dictionary(
         title=_("Cisco Meraki organization licenses overview"),
         optional_keys=True,

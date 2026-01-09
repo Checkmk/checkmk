333881cb19a34434cada09debc84dddfba263b8e
Lars Michelsen <lm@checkmk.com>
2026-01-09 17:12:56 +0100

    Clean up no-untyped-def #481-485
    Change-Id: I40ac5ad2ae4686ffd13e782d2409f8de840f2d5a

--- cmk/gui/plugins/wato/check_parameters/mcafee_web_gateway_misc.py
+++ cmk/gui/plugins/wato/check_parameters/mcafee_web_gateway_misc.py
@@ -8,8 +8,6 @@ Where possibile the "McAfee" string has been removed in favor of more generic th
 The old plug-in names, value_store dict keys, and ruleset names have been kept for compatibility/history-keeping reasons.
 """
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithoutItem,
@@ -21,7 +19,7 @@ from cmk.gui.plugins.wato.utils.simple_levels import SimpleLevels
 from cmk.gui.valuespec import Dictionary, Integer
 
 
-def _parameter_valuespec_mcafee_web_gateway_misc():
+def _parameter_valuespec_mcafee_web_gateway_misc() -> Dictionary:
     return Dictionary(
         elements=[
             (
--- cmk/gui/plugins/wato/check_parameters/memory_multiitem.py
+++ cmk/gui/plugins/wato/check_parameters/memory_multiitem.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
@@ -22,7 +20,7 @@ from cmk.gui.valuespec import (
 )
 
 
-def _parameter_valuespec_memory_multiitem():
+def _parameter_valuespec_memory_multiitem() -> Dictionary:
     return Dictionary(
         help=_(
             "The memory levels for one specific module of this host. This is relevant for hosts that have "

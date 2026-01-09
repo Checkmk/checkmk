c2cce6e994c7934e1c3c6912b4bfefc326dc91bb
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-01-09 16:56:20 +0100

    improve vcrtrace storage location
    For security reasons we only allow the `vrctrace` feature
    to write to a specific folder.
    For consistency we change it from 'tmp/debug' to 'tmp/check_mk/debug'.
    For convenience we create the directory upon site creation.
    
    CMK-27519
    
    Change-Id: Id5284cab9ad9b3becbce910a46557b26a466d36f

--- packages/cmk-plugin-apis/tests/cmk/server_side_programs/v1/test_vcrtrace.py
+++ packages/cmk-plugin-apis/tests/cmk/server_side_programs/v1/test_vcrtrace.py
@@ -12,6 +12,8 @@ import pytest
 
 from cmk.server_side_programs.v1_unstable._vcrtrace import _check_path
 
+ALLOWDIR = "tmp/check_mk/debug"
+
 
 @pytest.fixture(name="mocked_home")
 def _mocked_home(tmp_path: Path) -> Iterator[Path]:
@@ -20,39 +22,39 @@ def _mocked_home(tmp_path: Path) -> Iterator[Path]:
 
 
 def test_check_path_relative_ok(mocked_home: Path) -> None:
-    (mocked_home / "tmp/debug").mkdir(parents=True, exist_ok=True)
+    (mocked_home / ALLOWDIR).mkdir(parents=True, exist_ok=True)
     with chdir(mocked_home):
-        _check_path("tmp/debug/foo")
+        _check_path(f"{ALLOWDIR}/foo")
 
 
 def test_check_path_relative_missing(mocked_home: Path) -> None:
     with chdir(mocked_home), pytest.raises(NotADirectoryError):
-        _check_path("tmp/debug/foo")
+        _check_path(f"{ALLOWDIR}/foo")
 
 
 def test_check_path_relative_too_far_down(mocked_home: Path) -> None:
-    (mocked_home / "tmp/debug").mkdir(parents=True, exist_ok=True)
+    (mocked_home / ALLOWDIR).mkdir(parents=True, exist_ok=True)
     with chdir(mocked_home / "tmp"), pytest.raises(ValueError):
-        _check_path("tmp/debug/foo")
+        _check_path(f"{ALLOWDIR}/foo")
 
 
 def test_check_path_relative_too_far_up(mocked_home: Path) -> None:
-    (mocked_home / "tmp/debug").mkdir(parents=True, exist_ok=True)
+    (mocked_home / ALLOWDIR).mkdir(parents=True, exist_ok=True)
     with chdir(mocked_home / ".."), pytest.raises(ValueError):
-        _check_path("tmp/debug/foo")
+        _check_path(f"{ALLOWDIR}/foo")
 
 
 def test_check_path_absolute_ok(mocked_home: Path) -> None:
-    (mocked_home / "tmp/debug").mkdir(parents=True, exist_ok=True)
-    _check_path(str(mocked_home / "tmp/debug/foo"))
+    (mocked_home / ALLOWDIR).mkdir(parents=True, exist_ok=True)
+    _check_path(str(mocked_home / f"{ALLOWDIR}/foo"))
 
 
 def test_check_path_absolute_missing(mocked_home: Path) -> None:
     with pytest.raises(NotADirectoryError):
-        _check_path(str(mocked_home / "tmp/debug/foo"))
+        _check_path(str(mocked_home / f"{ALLOWDIR}/foo"))
 
 
 def test_check_path_absolute_invalid(mocked_home: Path) -> None:
-    (mocked_home / "tmp/debug").mkdir(parents=True, exist_ok=True)
+    (mocked_home / ALLOWDIR).mkdir(parents=True, exist_ok=True)
     with chdir(mocked_home / "tmp"), pytest.raises(ValueError):
-        _check_path(str(mocked_home / "tmp/degug/foo"))
+        _check_path(str(mocked_home / "something/else/foo"))

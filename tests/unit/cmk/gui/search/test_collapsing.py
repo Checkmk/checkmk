c99454609b3cb8f78d985a9703f8216c12b152ac
Logan Connolly <logan.connolly@checkmk.com>
2026-01-13 09:31:49 +0100

    search(collapsing): add test case for missing item
    In the current implementation, we assume that the Setup item is always
    first in the list of host items, but this is not necessarily true. For
    instance, if a user does not have permission to access the setup link,
    they will only be shown the monitoring options. With the current logic,
    we then wrongly link the "Host Alias" to the Edit button target.
    
    When the fix is applied, this test will be unmarked as xfail.
    
    CMK-28790
    
    Change-Id: I63aa75e2ff226266ec80d7f2f75aa173f13b9b37

--- tests/unit/cmk/gui/search/test_collapsing.py
+++ tests/unit/cmk/gui/search/test_collapsing.py
@@ -3,6 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import pytest
 from polyfactory.factories import DataclassFactory
 
 from cmk.gui.search.collapsing import get_collapser
@@ -93,3 +94,40 @@ def test_host_collapsing_disabled() -> None:
 
     assert results == initial_results
     assert counts == initial_counts
+
+
+@pytest.mark.xfail(reason="Setup item may be missing if user has no permissions.", strict=True)
+def test_host_collapsing_setup_item_missing() -> None:
+    initial_results = [
+        UnifiedSearchResultItemFactory.build(
+            title="testhost",
+            provider=ProviderName.monitoring,
+            topic="Host name",
+            target={"url": "/monitoring/testhost"},
+        ),
+        UnifiedSearchResultItemFactory.build(
+            title="testhost",
+            provider=ProviderName.monitoring,
+            topic="Hostalias",
+            target={"url": "/monitoring/testhost"},
+        ),
+    ]
+    initial_count = UnifiedSearchResultCounts(total=2, setup=0, monitoring=2, customize=0)
+    collapse = get_collapser(provider=None, disabled=False)
+
+    results, counts = collapse(initial_results, initial_count)
+
+    # expecting only one result here
+    assert len(results) == 1
+
+    # check that there is no inline setup edit button
+    assert not results[0].inline_buttons
+
+    # check that matching monitoring results were merged
+    assert results[0].title == "testhost"
+    assert results[0].provider == ProviderName.monitoring
+    assert results[0].target.url == "/monitoring/testhost"
+    assert results[0].context == "Host name, Hostalias"
+
+    # check that counts were updated accordingly
+    assert counts == UnifiedSearchResultCounts(total=1, setup=0, monitoring=1, customize=0)

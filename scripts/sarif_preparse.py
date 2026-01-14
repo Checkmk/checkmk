97831ad6fbe84628df40e3392d33e7199ef8f1bd
Mathias Laurin <mathias.laurin@checkmk.com>
2026-01-14 17:44:10 +0100

    Faster `sarif_preparse` implementation
    Replace parsing stdin with loading `*.report` files.
    
    CMK-21274
    
    Change-Id: Id3a3b02663120b8853b6939219ca0430686782e5

--- scripts/sarif_preparse.py
+++ scripts/sarif_preparse.py
@@ -3,68 +3,45 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import argparse
 import json
 import sys
-import tempfile
-from collections.abc import Sequence
-from contextlib import redirect_stdout
 from pathlib import Path
 
 from sarif import __version__ as SARIF_TOOLS_PACKAGE_VERSION  # type: ignore[import-untyped]
 from sarif.operations import copy_op  # type: ignore[import-untyped]
 from sarif.sarif_file import SarifFile, SarifFileSet  # type: ignore[import-untyped]
 
-
-def parse_header(header: str) -> Path:
-    target = header.rsplit(" ", 1)[-1]
-    directory, name = target.split(":", 1)
-    return Path(directory) / name[: name.index(":")]
+SARIF_MIN_SIZE = 0
 
 
-def parse(raw_data: Sequence[str]) -> SarifFileSet:
+def load_all(root_dir: Path) -> SarifFileSet:
     results = SarifFileSet()
-    header: str = ""
-    payload = ""
-    for line in raw_data:
-        if not line.strip():
-            continue
-        if line and not header:
-            assert line.startswith("Lint results for"), line
-            header = line
-            continue
-        if line and header:
-            payload += line
-
+    for file_path in root_dir.rglob("*AspectRulesLint*.report"):
         try:
-            data = json.loads(payload)
-        except json.decoder.JSONDecodeError:
-            pass
-        else:
-            # Successful runs miss "results".
-            if any(_.get("results") for _ in data.get("runs", [])):
-                with tempfile.NamedTemporaryFile(
-                    mode="w+", prefix=parse_header(header).name, encoding="utf-8"
-                ) as f:
-                    json.dump(data, f)
-                    sarif = SarifFile(f.name, data)
-                results.add_file(sarif)
-            header = ""
-            payload = ""
+            if file_path.stat().st_size <= SARIF_MIN_SIZE:
+                continue
+
+            with file_path.open("rt") as fd:
+                results.add_file(SarifFile(file_path, json.load(fd)))
+        except (json.JSONDecodeError, OSError) as exc:
+            sys.stderr.write("{!s}: {!s}\n".format(file_path, exc))
     return results
 
 
 def main() -> None:
-    results = parse(sys.stdin.readlines())
-    with redirect_stdout(None):
-        result = copy_op.generate_sarif(
-            input_files=results,
-            output="out.sarif",
-            append_timestamp=False,
-            sarif_tools_version=SARIF_TOOLS_PACKAGE_VERSION,
-            cmdline="bazel lint",
-        )
-    sys.stdout.write(json.dumps(result.data, indent=4, sort_keys=True))
-    sys.stdout.write("\n")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root_dir", type=Path, default="bazel-bin")
+    parser.add_argument("--output", type=Path, default="out.sarif")
+    args = parser.parse_args(sys.argv[1:])
+    results = load_all(args.root_dir)
+    copy_op.generate_sarif(
+        input_files=results,
+        output=args.output,
+        append_timestamp=False,
+        sarif_tools_version=SARIF_TOOLS_PACKAGE_VERSION,
+        cmdline="bazel lint",
+    )
 
 
 if __name__ == "__main__":

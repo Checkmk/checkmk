6317d6561ebdde72a6f7d61e40b5de97392a25c9
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-01-14 18:07:25 +0100

    feat(welcome): add CmkWizard to "Create a Dashboard" step
    CMK-28506
    
    Change-Id: Ic4bdd2bc6afe5a89a1d3cd64b250348cc441b57e
    (cherry picked from commit 3ffe205bc7cdfeef5ce86ef9d1fca32135b0f9e3)

--- packages/cmk-frontend-vue/src/welcome/components/steps/CreateDashboard.vue
+++ packages/cmk-frontend-vue/src/welcome/components/steps/CreateDashboard.vue
@@ -6,22 +6,36 @@ conditions defined in the file COPYING, which is part of this source code packag
 
 <script setup lang="ts">
 import type { WelcomeCards } from 'cmk-shared-typing/typescript/welcome'
+import type { Ref } from 'vue'
 
 import usei18n from '@/lib/i18n'
+import usePersistentRef from '@/lib/usePersistentRef'
 
 import CmkAccordionStepPanelItem from '@/components/CmkAccordionStepPanel/CmkAccordionStepPanelItem.vue'
 import CmkLinkCard from '@/components/CmkLinkCard'
+import CmkWizard, { CmkWizardButton, CmkWizardStep } from '@/components/CmkWizard'
+import CmkHeading from '@/components/typography/CmkHeading.vue'
 
 import StepCardsRow from '@/welcome/components/steps/components/StepCardsRow.vue'
 import StepParagraph from '@/welcome/components/steps/components/StepParagraph.vue'
 
+import type { StepId } from './utils'
+
 const { _t } = usei18n()
 
-defineProps<{
+const props = defineProps<{
   step: number
-  urls: WelcomeCards
+  stepId: StepId
+  cards: WelcomeCards
   accomplished: boolean
 }>()
+
+const currentStep: Ref<number> = usePersistentRef<number>(
+  `${props.stepId}-currentStep`,
+  0,
+  (v) => v as number,
+  'local'
+)
 </script>
 
 <template>
@@ -44,19 +58,58 @@ defineProps<{
       }}
     </StepParagraph>
 
-    <StepCardsRow>
-      <CmkLinkCard
-        icon-name="new"
-        :title="_t('Add custom dashboard')"
-        :url="urls.add_custom_dashboard"
-        :open-in-new-tab="false"
-      />
-      <CmkLinkCard
-        icon-name="dashboard"
-        :title="_t('List of all dashboards')"
-        :url="urls.all_dashboards"
-        :open-in-new-tab="false"
-      />
-    </StepCardsRow>
+    <CmkWizard v-model="currentStep" mode="guided">
+      <CmkWizardStep :index="0" :is-completed="() => currentStep >= 0">
+        <template #header>
+          <CmkHeading type="h3">{{ _t('Explore the Checkmk built-in dashboards') }}</CmkHeading>
+        </template>
+        <template #content>
+          <StepParagraph>
+            {{
+              _t(
+                'Explore all of the built-in dashboards available in Checkmk. You can also clone and modify them to suit your needs.'
+              )
+            }}
+          </StepParagraph>
+          <StepCardsRow>
+            <CmkLinkCard
+              icon-name="dashboard"
+              :title="_t('View list of all dashboards')"
+              :url="cards.all_dashboards"
+              :open-in-new-tab="false"
+            />
+          </StepCardsRow>
+        </template>
+        <template #actions>
+          <CmkWizardButton type="next" />
+        </template>
+      </CmkWizardStep>
+
+      <CmkWizardStep :index="1" :is-completed="() => currentStep >= 1">
+        <template #header>
+          <CmkHeading type="h3">{{ _t('Create your own custom dashboard') }}</CmkHeading>
+        </template>
+        <template #content>
+          <StepParagraph>
+            {{
+              _t(
+                "Now it's time to create your own customized dashboard so you can see what matters at a glance."
+              )
+            }}
+          </StepParagraph>
+          <StepCardsRow>
+            <CmkLinkCard
+              icon-name="new"
+              :title="_t('Add custom dashboard')"
+              :url="cards.add_custom_dashboard"
+              :open-in-new-tab="false"
+            />
+          </StepCardsRow>
+        </template>
+        <template #actions>
+          <CmkWizardButton type="previous" />
+        </template>
+      </CmkWizardStep>
+    </CmkWizard>
   </CmkAccordionStepPanelItem>
 </template>

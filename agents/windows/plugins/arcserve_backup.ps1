1c7db8fd8654790edfc5f0f16fc354e819cbf3a7
Checkmk release system <noreply@checkmk.com>
2026-01-14 09:05:29 +0100

    Set version to 2.3.0p43

--- agents/windows/plugins/arcserve_backup.ps1
+++ agents/windows/plugins/arcserve_backup.ps1
@@ -1,4 +1,4 @@
-$CMK_VERSION = "2.3.0p42"
+$CMK_VERSION = "2.3.0p43"
 ####
 ## ArcServe.ps1
 ####
--- agents/windows/plugins/nvidia_smi.ps1
+++ agents/windows/plugins/nvidia_smi.ps1
@@ -1,4 +1,4 @@
-$CMK_VERSION = "2.3.0p42"
+$CMK_VERSION = "2.3.0p43"
 
 Write-Host "<<<nvidia_smi:sep(9)>>>"
 

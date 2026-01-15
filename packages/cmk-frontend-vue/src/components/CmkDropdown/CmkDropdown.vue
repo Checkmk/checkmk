f0516acfce21efcf1621b8ce8eafe14157723e15
Max Höhl <max.hoehl@checkmk.com>
2026-01-15 08:42:28 +0100

    CmkDropdown: add button slots
    With this change we add button named slots before and after the
    DropdownButtom so buttons added from a parent component can be
    positioned correctly.
    
    Change-Id: I99b57711fc1cf0dc68c357a0157716f1d8e7f0ac
    (cherry picked from commit 022d591bd5cb928d80b518167539955fd9aa2e25)

--- packages/cmk-frontend-vue/src/components/CmkDropdown/CmkDropdown.vue
+++ packages/cmk-frontend-vue/src/components/CmkDropdown/CmkDropdown.vue
@@ -167,6 +167,7 @@ const truncatedButtonLabel = computed(() =>
     "
     class="cmk-dropdown"
   >
+    <slot name="buttons-start"></slot>
     <CmkDropdownButton
       v-bind="componentId!! ? { id: componentId } : {}"
       ref="comboboxButtonRef"
@@ -190,6 +191,7 @@ const truncatedButtonLabel = computed(() =>
         class="cmk-dropdown--arrow"
         :class="{ rotated: suggestionsShown, disabled: disabled || !canOpenDropdown }"
     /></CmkDropdownButton>
+    <slot name="buttons-end"></slot>
     <CmkSuggestions
       v-if="!!suggestionsShown"
       ref="suggestionsRef"

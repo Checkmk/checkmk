5e87dd1317d7fae44fd7ad0161ef1761bfd3bd3e
Christopher Krause <christopher.krause@checkmk.com>
2026-01-16 08:07:48 +0100

    dashboard: style responsive grid
    Change-Id: I9e3c1c1b5ffd1283a7e9570ab43338d0f324aaa6
    JIRA-Ref: CMK-26253

--- packages/cmk-frontend-vue/src/dashboard/components/ResponsiveGrid/ResponsiveGridWidget.vue
+++ packages/cmk-frontend-vue/src/dashboard/components/ResponsiveGrid/ResponsiveGridWidget.vue
@@ -6,11 +6,11 @@ conditions defined in the file COPYING, which is part of this source code packag
 <script setup lang="ts">
 import usei18n from '@/lib/i18n'
 
-import CmkIconButton from '@/components/CmkIconButton.vue'
-
 import DashboardContent from '@/dashboard/components/DashboardContent/DashboardContent.vue'
 import type { ContentProps } from '@/dashboard/components/DashboardContent/types'
 import MissingFiltersMsg from '@/dashboard/components/DashboardFilterSettings/MissingFiltersMsg.vue'
+
+import ResponsiveGridWidgetButton from './ResponsiveGridWidgetButton.vue'
 
 const { _t } = usei18n()
 
@@ -36,25 +36,9 @@ defineEmits<{
   >
     <div v-if="isEditing" class="db-responsive-grid-widget__edit-controls">
       <div class="db-responsive-grid-widget__edit-controls-buttons">
-        <!-- TODO: icons are too small, likely need re-export of the SVG and adjustment here -->
-        <CmkIconButton
-          class="db-responsive-widget-frame__edit-controls-button"
-          name="widget-delete"
-          size="medium"
-          @click="$emit('click:delete')"
-        />
-        <CmkIconButton
-          class="db-responsive-grid-widget__edit-controls-button"
-          name="widget-clone"
-          size="medium"
-          @click="$emit('click:clone')"
-        />
-        <CmkIconButton
-          class="db-responsive-grid-widget__edit-controls-button"
-          name="widget-edit"
-          size="medium"
-          @click="$emit('click:edit')"
-        />
+        <ResponsiveGridWidgetButton icon-name="db-widget-delete" @click="$emit('click:delete')" />
+        <ResponsiveGridWidgetButton icon-name="db-widget-clone" @click="$emit('click:clone')" />
+        <ResponsiveGridWidgetButton icon-name="db-widget-edit" @click="$emit('click:edit')" />
       </div>
     </div>
     <MissingFiltersMsg :effective-filter-context="spec.effective_filter_context">
@@ -67,7 +51,6 @@ defineEmits<{
 .db-responsive-grid-widget__frame {
   height: 100%;
   width: 100%;
-  background-color: var(--color-midnight-grey-70);
   position: relative;
   overflow: hidden;
 }
@@ -78,13 +61,11 @@ defineEmits<{
 
   /* show border, reduce margin to keep size the same */
   margin: -1px;
-  border-radius: 4px;
+  border-radius: var(--border-radius);
   border: 1px solid var(--color-corporate-green-80);
-  transition: border-color 200ms ease;
 
-  &:hover {
-    border-color: var(--color-corporate-green-50);
-  }
+  /* NOTE: the hover effect is defined in ResponsiveGrid.vue */
+  transition: border-color 200ms ease;
 }
 
 .db-responsive-grid-widget__content,
@@ -99,32 +80,22 @@ defineEmits<{
 .db-responsive-grid-widget__edit-controls {
   /* this must be above the content and placeholder, but below the resizer */
   z-index: 4;
-  background-color: color-mix(in srgb, var(--color-midnight-grey-100) 40%, transparent);
+  background-color: color-mix(in srgb, var(--color-daylight-grey-50) 50%, transparent);
+}
+
+body[data-theme='modern-dark'] .db-responsive-grid-widget__edit-controls {
+  background-color: color-mix(in srgb, var(--color-midnight-grey-90) 50%, transparent);
 }
 
 .db-responsive-grid-widget__edit-controls-buttons {
-  /* menu bar on top */
-  height: 23px;
-  width: 100%;
-  background-color: color-mix(in srgb, var(--color-midnight-grey-100) 80%, transparent);
+  position: absolute;
+  top: 0;
+  right: 0;
 
   /* align buttons on the right */
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: flex-end;
-  gap: 4px;
-}
-
-.db-responsive-grid-widget__edit-controls-button {
-  padding: 4px;
-
-  /* filter to make icons white */
-  filter: brightness(0) saturate(100%) invert(100%) brightness(70%);
-  transition: filter 200ms ease;
-
-  &:hover {
-    filter: brightness(0) saturate(100%) invert(100%) brightness(100%);
-  }
 }
 </style>

8b84a895e6e0fa1f7615c877ecd6aaebcf29ebaa
Sergey Kipnis <sergey.kipnis@checkmk.com>
2026-01-20 12:04:32 +0100

    18550 Improve diagnostic for mk-oracle plugins
    CMK-29328
    
    Change-Id: Ib63893bd6da35a577e5a226de2a3cfa8dc697341

--- cmk/plugins/oracle/agents/oracle_unified_async.ps1
+++ cmk/plugins/oracle/agents/oracle_unified_async.ps1
@@ -4,4 +4,24 @@
 
 $CMK_VERSION = "2.5.0b1"
 
+if (-not $env:MK_LIBDIR -or -not (Test-Path -Path $env:MK_LIBDIR -PathType Container)) {
+    Write-Host "MK_LIBDIR is not set or not directory"
+    exit 1
+}
+
+if (-not (Test-Path -Path (Join-Path $env:MK_LIBDIR 'mk-oracle.exe') -PathType Leaf)) {
+    Write-Host "mk-oracle.exe in $env:MK_LIBDIR is not executable or not found"
+    exit 1
+}
+
+if (-not $env:MK_CONFDIR -or -not (Test-Path -Path $env:MK_CONFDIR -PathType Container)) {
+    Write-Host "MK_CONFDIR is not set or not directory"
+    exit 1
+}
+
+if (-not (Test-Path -Path (Join-Path $env:MK_CONFDIR 'oracle.yml') -PathType Leaf)) {
+    Write-Host "Configuration file oracle.yml not found in $env:MK_CONFDIR"
+    exit 1
+}
+
 & $env:MK_PLUGINSDIR/packages/mk-oracle/mk-oracle.exe -c $env:MK_CONFDIR/oracle.yml --filter async

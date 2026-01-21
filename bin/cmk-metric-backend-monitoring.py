b898adcd3d44157acc45a59dd2a12a88318b0684
Jörg Herbel <joerg.herbel@checkmk.com>
2026-01-21 16:18:12 +0100

    Move functionality to bin/cmk-monitor-metric-backend
    We will add information about the license usage history in the subsequent
    changes. Since we must not import licensing code in `cmk.metric_backend`, we
    move some functionality into `bin/cmk-monitor-metric-backend` (where we are
    allowed to import licensing code) to keep things uniform.
    
    CMK-28536
    
    Change-Id: Idf21682e66413ef3cdf492c7197b2ef957053067

--- bin/cmk-metric-backend-monitoring.py
+++ bin/cmk-metric-backend-monitoring.py
@@ -3,8 +3,25 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import os
 import sys
+from pathlib import Path
 
-from cmk.metric_backend.monitor import main
+from cmk.metric_backend import monitor
 
-sys.exit(main())
+
+def main() -> int:
+    site_id = os.environ["OMD_SITE"]
+    config = monitor.get_config_if_self_hosted(Path(os.environ["OMD_ROOT"]))
+    if not config:
+        return 0
+
+    request = monitor.make_ping_section(site_id, config)
+    sys.stdout.write("<<<metric_backend_omd_ping:sep(0)>>>\n")
+    sys.stdout.write(request.model_dump_json() + "\n")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())

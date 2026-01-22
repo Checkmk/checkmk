ae8a2644972cf9c297009fe162ce1b5ada272bd5
Jörg Herbel <joerg.herbel@checkmk.com>
2026-01-22 08:47:22 +0100

    Metric backend monitoring: Add license usage information
    * Current active series count
    * Last reported usage
    
    CMK-28536
    
    Change-Id: I1c319f62be78af72c4e840ef16be5435331670a6

--- bin/cmk-metric-backend-monitoring.py
+++ bin/cmk-metric-backend-monitoring.py
@@ -7,7 +7,16 @@ import os
 import sys
 from pathlib import Path
 
+from clickhouse_connect.driver.exceptions import ClickHouseError
+
 from cmk.metric_backend import monitor
+from cmk.metric_backend.client import ClickHouseClient, make_raw_read_only_client
+from cmk.metric_backend.config import ConfigMetricBackendSelfHosted
+from cmk.utils.licensing.usage import (
+    get_license_usage_report_file_path,
+    load_raw_license_usage_report,
+    LocalLicenseUsageHistory,
+)
 
 
 def main() -> int:
@@ -20,7 +29,34 @@ def main() -> int:
     sys.stdout.write("<<<metric_backend_omd_ping:sep(0)>>>\n")
     sys.stdout.write(request.model_dump_json() + "\n")
 
+    license_usage = _make_license_usage(site_id, config)
+    sys.stdout.write("<<<metric_backend_omd_license_usage:sep(0)>>>\n")
+    sys.stdout.write(license_usage.model_dump_json() + "\n")
+
     return 0
+
+
+def _make_license_usage(
+    site_id: str,
+    config: ConfigMetricBackendSelfHosted,
+) -> monitor.LicenseUsage:
+    try:
+        client = ClickHouseClient(make_raw_read_only_client(config))
+        number_of_active_series_now = client.active_series_count()
+    except ClickHouseError:
+        number_of_active_series_now = None
+
+    license_usage_history = LocalLicenseUsageHistory.parse(
+        load_raw_license_usage_report(get_license_usage_report_file_path())
+    )
+
+    return monitor.LicenseUsage(
+        site_id=site_id,
+        current_active_series_count=number_of_active_series_now,
+        last_reported_active_series_count=last_license_usage_sample.num_active_metric_series
+        if (last_license_usage_sample := license_usage_history.last)
+        else None,
+    )
 
 
 if __name__ == "__main__":

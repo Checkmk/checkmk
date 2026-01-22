342f3ebed5ee07e5d0c4dddae57ce16017489192
Sergey Kipnis <sergey.kipnis@checkmk.com>
2026-01-22 15:15:46 +0100

    mk-oracle: fix wrapping scripts
    MK_LIBDIR is used correctly
    
    CMK-29516
    
    TESTS: locally, manually, Windows/Linux
    
    Change-Id: Ic878e14dae85190c1a4c60bbb360c0fda0b56325

--- cmk/plugins/oracle/agents/oracle_unified_sync.ps1
+++ cmk/plugins/oracle/agents/oracle_unified_sync.ps1
@@ -9,8 +9,10 @@ if (-not $env:MK_LIBDIR -or -not (Test-Path -Path $env:MK_LIBDIR -PathType Conta
     exit 1
 }
 
-if (-not (Test-Path -Path (Join-Path $env:MK_LIBDIR 'mk-oracle.exe') -PathType Leaf)) {
-    Write-Host "mk-oracle.exe in $env:MK_LIBDIR is not executable or not found"
+$bin_exe=(Join-Path $env:MK_PLUGINSDIR/packages/mk-oracle 'mk-oracle.exe')
+
+if (-not (Test-Path -Path $bin_exe -PathType Leaf)) {
+    Write-Host "$bin_exe is not executable or not found"
     exit 1
 }
 
@@ -24,4 +26,4 @@ if (-not (Test-Path -Path (Join-Path $env:MK_CONFDIR 'oracle.yml') -PathType Lea
     exit 1
 }
 
-& $env:MK_PLUGINSDIR\packages\mk-oracle\mk-oracle.exe -c $env:MK_CONFDIR/oracle.yml --filter sync
+& $bin_exe -c $env:MK_CONFDIR/oracle.yml --filter sync

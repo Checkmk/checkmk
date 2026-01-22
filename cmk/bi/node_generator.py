7074f54deafa2ad19e7d6695558f8d6950bf88b9
Lars Michelsen <lm@checkmk.com>
2026-01-22 09:22:47 +0100

    Clean up no-untyped-def #628
    Change-Id: I504734a96c90169ed7e6dafce0cf005c6fe305e5

--- cmk/bi/node_generator.py
+++ cmk/bi/node_generator.py
@@ -3,7 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
 #   .--NodeGen.------------------------------------------------------------.
 #   |              _   _           _       ____                            |

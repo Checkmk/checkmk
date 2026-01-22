Checkmk/checkmk

commit 71e40a4d4b631e2a62b0fd80d29d8abcfd50dd3d

Ping test: Respect inherited address family
CMK-28499

Change-Id: Idd6f0f75bcfb7a7b747ea3f415a7d944e5d7205e

packages/cmk-frontend-vue/src/mode-host/ping-host/PingHost.vue
@@ -15,6 +15,7 @@ const props = defineProps<{
   i18n: I18NPingHost
   formElement: HTMLFormElement
   ipAddressFamilySelectElement: HTMLSelectElement
+  ipAddressFamilyDefaultElement: HTMLDivElement
   ipAddressFamilyInputElement: HTMLInputElement
   hostnameInputElement: HTMLInputElement
   ipv4InputElement: HTMLInputElement
@@ -53,7 +54,9 @@ interface Result {
 
 const statusElements: Ref<Record<string, Result>> = ref({})
 const isNoIP = ref(
-  props.ipAddressFamilyInputElement.checked && props.ipAddressFamilySelectElement.value === 'no-ip'
+  (props.ipAddressFamilyInputElement.checked &&
+    props.ipAddressFamilySelectElement.value === 'no-ip') ||
+    props.ipAddressFamilyDefaultElement.textContent?.includes('No IP')
 )
 const isRelay = ref(props.relayInputButtonElement?.checked)
 const controller = ref(new AbortController())
@@ -84,7 +87,16 @@ onMounted(() => {
       case props.formElement:
         switch (props.ipAddressFamilySelectElement.value) {
           case 'ip-v4-only':
+            callPingHostOnElement(props.hostnameInputElement, PingCmd.Ping, false)
+            isNoIP.value = false
+            break
           case 'ip-v6-only':
+            if (props.ipv6InputElement.value) {
+              isNoIP.value = false
+            } else {
+              isNoIP.value = true
+            }
+            break
           case 'ip-v4v6':
             isNoIP.value = false
             break
@@ -96,8 +108,10 @@ onMounted(() => {
         break
       case props.ipAddressFamilyInputElement:
         isNoIP.value =
-          props.ipAddressFamilyInputElement.checked &&
-          props.ipAddressFamilySelectElement.value === 'no-ip'
+          (props.ipAddressFamilyInputElement.checked &&
+            props.ipAddressFamilySelectElement.value === 'no-ip') ||
+          (!props.ipAddressFamilyInputElement.checked &&
+            props.ipAddressFamilyDefaultElement.textContent?.includes('No IP'))
         break
       case props.relayInputButtonElement:
         isRelay.value = props.relayInputButtonElement?.checked
@@ -129,6 +143,15 @@ onMounted(() => {
       return
     }
     callPingHostOnElement(props.ipv6InputElement, PingCmd.Ping6, true)
+  })
+  props.ipAddressFamilyInputElement.addEventListener('change', () => {
+    if (
+      props.ipAddressFamilyInputElement.checked &&
+      props.ipAddressFamilySelectElement.value === 'ip-v4-only'
+    ) {
+      callPingHostOnElement(props.hostnameInputElement, PingCmd.Ping, false)
+      return
+    }
   })
 })
 

packages/cmk-frontend-vue/src/mode-host/ModeHostApp.vue
@@ -38,6 +38,7 @@ const ipv6InputElement: Ref<HTMLInputElement | null> = ref(null)
 const ipv6InputButtonElement: Ref<HTMLInputElement | null> = ref(null)
 const relayInputButtonElement: Ref<HTMLInputElement | null> = ref(null)
 const ipAddressFamilySelectElement: Ref<HTMLSelectElement | null> = ref(null)
+const ipAddressFamilyDefaultElement: Ref<HTMLDivElement | null> = ref(null)
 const ipAddressFamilyInputElement: Ref<HTMLInputElement | null> = ref(null)
 const tagAgentInputSelectElement: Ref<HTMLSelectElement | null> = ref(null)
 const tagAgentInputButtonElement: Ref<HTMLInputElement | null> = ref(null)
@@ -66,6 +67,9 @@ onMounted(() => {
   siteSelectElement.value = getElementBySelector(`select[name="${props.form_keys.site}"]`)
   ipAddressFamilySelectElement.value = getElementBySelector(
     `select[name="${props.form_keys.ip_address_family}"]`
+  )
+  ipAddressFamilyDefaultElement.value = getElementBySelector(
+    `div[id="attr_default_${props.form_keys.ip_address_family}"]`
   )
   ipAddressFamilyInputElement.value = getElementBySelector(
     `input[id="${props.form_keys.cb_change}_${props.form_keys.ip_address_family}"]`
@@ -111,6 +115,7 @@ function getElementBySelector<T>(selector: string): T {
       ipv4InputElement &&
       ipv6InputElement &&
       ipAddressFamilySelectElement &&
+      ipAddressFamilyDefaultElement &&
       ipAddressFamilyInputElement &&
       ipv4InputButtonElement &&
       ipv6InputButtonElement
@@ -118,6 +123,7 @@ function getElementBySelector<T>(selector: string): T {
     :i18n="i18n_ping_host"
     :form-element="formElement"
     :ip-address-family-select-element="ipAddressFamilySelectElement"
+    :ip-address-family-default-element="ipAddressFamilyDefaultElement"
     :ip-address-family-input-element="ipAddressFamilyInputElement"
     :hostname-input-element="hostnameInputElement"
     :ipv4-input-element="ipv4InputElement"
@@ -138,6 +144,7 @@ function getElementBySelector<T>(selector: string): T {
       ipv6InputButtonElement &&
       ipv6InputElement &&
       ipAddressFamilySelectElement &&
+      ipAddressFamilyDefaultElement &&
       ipAddressFamilyInputElement &&
       tagAgentInputSelectElement &&
       tagAgentInputButtonElement &&
@@ -154,6 +161,7 @@ function getElementBySelector<T>(selector: string): T {
     :ipv6-input-button-element="ipv6InputButtonElement"
     :site-select-element="siteSelectElement"
     :ip-address-family-select-element="ipAddressFamilySelectElement"
+    :ip-address-family-default-element="ipAddressFamilyDefaultElement"
     :ip-address-family-input-element="ipAddressFamilyInputElement"
     :cmk-agent-connection-mode-select-element="cmkConnectionModeSelectElement"
     :sites="sites"

commit b1474e12cb4fb55da117718f82ea94bdfe2d1644

Fix ping test on IPv4 and IPv6
* broken with 71e40a4d4b631e2a62b0fd80d29d8abcfd50dd3d

CMK-29490

Change-Id: I881ae0207a798cd79367de20dc0dcf0d33b0ec61

packages/cmk-frontend-vue/src/mode-host/ping-host/PingHost.vue
@@ -87,16 +87,7 @@ onMounted(() => {
       case props.formElement:
         switch (props.ipAddressFamilySelectElement.value) {
           case 'ip-v4-only':
-            callPingHostOnElement(props.hostnameInputElement, PingCmd.Ping, false)
-            isNoIP.value = false
-            break
           case 'ip-v6-only':
-            if (props.ipv6InputElement.value) {
-              isNoIP.value = false
-            } else {
-              isNoIP.value = true
-            }
-            break
           case 'ip-v4v6':
             isNoIP.value = false
             break
@@ -144,13 +135,26 @@ onMounted(() => {
     }
     callPingHostOnElement(props.ipv6InputElement, PingCmd.Ping6, true)
   })
-  props.ipAddressFamilyInputElement.addEventListener('change', () => {
-    if (
-      props.ipAddressFamilyInputElement.checked &&
-      props.ipAddressFamilySelectElement.value === 'ip-v4-only'
-    ) {
-      callPingHostOnElement(props.hostnameInputElement, PingCmd.Ping, false)
-      return
+  props.ipv4InputButtonElement.addEventListener('change', () => {
+    if (props.ipv4InputButtonElement.checked) {
+      if (showPingHost.value) {
+        statusElements.value = {}
+      }
+      if (props.ipv4InputElement.value) {
+        callPingHostOnElement(props.ipv4InputElement, PingCmd.Ping, true)
+        return
+      }
+    }
+  })
+  props.ipv6InputButtonElement.addEventListener('change', () => {
+    if (props.ipv6InputButtonElement.checked) {
+      if (showPingHost.value) {
+        statusElements.value = {}
+      }
+      if (props.ipv6InputElement.value) {
+        callPingHostOnElement(props.ipv6InputElement, PingCmd.Ping6, true)
+        return
+      }
     }
   })
 })
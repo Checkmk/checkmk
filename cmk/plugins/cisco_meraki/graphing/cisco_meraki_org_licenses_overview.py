6e2135268278451a23a8a5a580008e90745e78ef
Logan Connolly <logan.connolly@checkmk.com>
2026-01-23 15:18:44 +0100

    cisco_meraki: add graphing for licenses overview
    CMK-28344
    
    Change-Id: I864c61e2fb4fc402c6d7da0f1e3b5860f3938e92

---
+++ cmk/plugins/cisco_meraki/graphing/cisco_meraki_org_licenses_overview.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+#
+# Original author: thl-cmk[at]outlook[dot]com
+
+from cmk.graphing.v1 import graphs, metrics, perfometers, Title
+
+metric_license_total = metrics.Metric(
+    name="license_total",
+    title=Title("Licensed devices"),
+    unit=metrics.Unit(metrics.DecimalNotation("")),
+    color=metrics.Color.LIGHT_GREEN,
+)
+
+metric_remaining_time = metrics.Metric(
+    name="remaining_time",
+    title=Title("Remaining time"),
+    unit=metrics.Unit(metrics.TimeNotation()),
+    color=metrics.Color.GREEN,
+)
+
+graph_cisco_meraki_remaining_time = graphs.Graph(
+    name="cisco_meraki_remaining_time",
+    title=Title("Cisco Meraki licenses remaining time"),
+    compound_lines=["remaining_time"],
+    simple_lines=[
+        metrics.WarningOf("remaining_time"),
+        metrics.CriticalOf("remaining_time"),
+    ],
+    minimal_range=graphs.MinimalRange(0, 180),
+)
+
+graph_cisco_meraki_licensed_devices = graphs.Graph(
+    name="cisco_meraki_licensed_devices",
+    title=Title("Cisco Meraki licensed devices"),
+    compound_lines=["license_total"],
+    minimal_range=graphs.MinimalRange(0, 10),
+)
+
+perfometer_licensing = perfometers.Stacked(
+    name="cisco_meraki_licensing",
+    # upper and lower are in the wrong order
+    lower=perfometers.Perfometer(
+        name="license_total",
+        focus_range=perfometers.FocusRange(perfometers.Open(0), perfometers.Open(100)),
+        segments=["license_total"],
+    ),
+    upper=perfometers.Perfometer(
+        name="remaining_time",
+        focus_range=perfometers.FocusRange(perfometers.Open(0), perfometers.Open(180)),
+        segments=["remaining_time"],
+    ),
+)

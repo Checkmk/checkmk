19fa5e770a5b78897bb53c35c4066de3a6c2da9d
Lars Michelsen <lm@checkmk.com>
2026-01-25 14:12:18 +0100

    Clean up no-untyped-def #645
    Change-Id: Idc81a59de6fa2a2a86d9dc983eaa4f96e8dfa6d7

--- cmk/plugins/azure_v2/rulesets/azure.py
+++ cmk/plugins/azure_v2/rulesets/azure.py
@@ -4,7 +4,6 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 # mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
 # mypy: disable-error-code="type-arg"
 
 from collections.abc import Mapping, Sequence

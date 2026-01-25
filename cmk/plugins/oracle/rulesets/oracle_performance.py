0d92a70256682e56a4a876eb466178ad3925bfe3
Lars Michelsen <lm@checkmk.com>
2026-01-25 14:20:37 +0100

    Clean up no-untyped-def #647
    Change-Id: Ie7cc109aa23f77e5ab09c5ffaae6874df5078b63

--- cmk/plugins/oracle/rulesets/oracle_performance.py
+++ cmk/plugins/oracle/rulesets/oracle_performance.py
@@ -3,7 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 # mypy: disable-error-code="type-arg"
 
 from cmk.plugins.oracle.constants import (
@@ -116,7 +115,7 @@ _PERSEC_LEVELS = SimpleLevels(
 
 # FIXME: This is currently written in a way that prevents us from adding
 # any localization.
-def _parameter_valuespec_oracle_performance():
+def _parameter_valuespec_oracle_performance() -> Dictionary:
     # memory
     memory_choices = [
         CascadingSingleChoiceElement(

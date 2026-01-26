5802cba97df6112b04a0e1407998e4d88554c662
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-01-26 09:39:22 +0100

    feat(welcome): add "Activate changes" onboarding topic
    CMK-29003
    
    Change-Id: Ica1defdce75de6d4d0c8744d95394c42ec49a3c6

--- packages/cmk-frontend-vue/src/welcome/components/steps/stepComponents.ts
+++ packages/cmk-frontend-vue/src/welcome/components/steps/stepComponents.ts
@@ -12,18 +12,21 @@ import AssignResponsibilities from '@/welcome/components/steps/AssignResponsibil
 import CreateDashboard from '@/welcome/components/steps/CreateDashboard.vue'
 import EnableNotifications from '@/welcome/components/steps/EnableNotifications.vue'
 
+import ActivateChanges from './ActivateChanges.vue'
+
 export type StepId = StageInformation['finished'][number]
 
 export const stepComponents: { component: Component; stepNumber: number; stepId: StepId }[] = [
   { component: AddHost, stepNumber: 1, stepId: 'add_host' },
+  { component: ActivateChanges, stepNumber: 2, stepId: 'activate_changes' },
   {
     component: AdjustServices,
-    stepNumber: 2,
+    stepNumber: 3,
     stepId: 'adjust_services'
   },
-  { component: AssignResponsibilities, stepNumber: 3, stepId: 'assign_responsibilities' },
-  { component: EnableNotifications, stepNumber: 4, stepId: 'enable_notifications' },
-  { component: CreateDashboard, stepNumber: 5, stepId: 'create_dashboard' }
+  { component: AssignResponsibilities, stepNumber: 4, stepId: 'assign_responsibilities' },
+  { component: EnableNotifications, stepNumber: 5, stepId: 'enable_notifications' },
+  { component: CreateDashboard, stepNumber: 6, stepId: 'create_dashboard' }
 ]
 
 export const totalSteps = stepComponents.length

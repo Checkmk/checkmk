ddb72307fbc2ae21443ff7f12f9df3452d7d5272
Dennis Schmalacker <dennis.schmalacker@checkmk.com>
2026-01-26 17:53:20 +0100

    Stormshield: Change OID for SNMP detection, the second.
    Stormshield products updating to firmware version 4.8 have changed their OID. Detect all Stormshield products by using a more general OID starting with their enterprise OID .1.3.6.1.4.1.11256.1
    
    SUP-27032
    
    Change-Id: I676ae226277eb69dfa3b015c74a1b14c7c42ced2

--- cmk/plugins/stormshield/lib.py
+++ cmk/plugins/stormshield/lib.py
@@ -7,24 +7,21 @@ from cmk.agent_based.v2 import all_of, any_of, equals, exists, startswith
 
 DETECT_STORMSHIELD = all_of(
     any_of(
-        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072"),
-        equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.2.0"),
-        equals(".1.3.6.1.2.1.1.2.0", ".10.0.0.1.4.1.11256.1.20.10"),
+        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072"),  # General NetSNMP
+        equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.2.0"),  # Old undocumented Stormshield OID
+        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.1"),  # New documented Stormshield OID
     ),
-    any_of(
-        exists(".1.3.6.1.4.1.11256.1.0.1"),
-        exists(".1.3.6.1.4.1.11256.1.11.1.0"),
-    ),
+    exists(".1.3.6.1.4.1.11256.1.0.1.0"),  # Stormshield Basic Info
 )
 
 DETECT_STORMSHIELD_CLUSTER = all_of(
     any_of(
-        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072.3.2.8"),
-        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.2.0"),
-        equals(".1.3.6.1.2.1.1.2.0", ".10.0.0.1.4.1.11256.1.20.10"),
+        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.8072"),  # General NetSNMP
+        equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.2.0"),  # Old undocumented Stormshield OID
+        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.11256.1"),  # New documented Stormshield OID
     ),
-    any_of(
-        exists(".1.3.6.1.4.1.11256.1.0.1"),
-        exists(".1.3.6.1.4.1.11256.1.11.1.0"),
+    all_of(
+        exists(".1.3.6.1.4.1.11256.1.0.1.0"),  # Stormshield Basic Info
+        exists(".1.3.6.1.4.1.11256.1.11.1.0"),  # Stormshield HA Info
     ),
 )

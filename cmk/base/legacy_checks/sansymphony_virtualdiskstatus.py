fcbbadf0b80ee42c1308d4d6e556f01b84d209cd
Lars Michelsen <lm@checkmk.com>
2026-01-26 15:15:00 +0100

    Clean up no-untyped-def #662
    Change-Id: I8bb4c1cd69cb08484a0deec60aabe7d1897c54e9

--- cmk/base/legacy_checks/sansymphony_virtualdiskstatus.py
+++ cmk/base/legacy_checks/sansymphony_virtualdiskstatus.py
@@ -3,35 +3,41 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # <<<sansymphony_virtualdiskstatus>>>
 # testvmfs01 Online
 # vmfs10 Online
 
 
-# mypy: disable-error-code="var-annotated"
+from collections.abc import Iterator, Mapping
+from typing import Any
 
 from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
+from cmk.agent_based.v2 import StringTable
 
 check_info = {}
 
+Section = dict[str, str]
 
-def parse_sansymphony_virtualdiskstatus(string_table):
-    parsed = {}
+
+def parse_sansymphony_virtualdiskstatus(string_table: StringTable) -> Section:
+    parsed: Section = {}
     for line in string_table:
         parsed.setdefault(line[0], " ".join(line[1:]))
     return parsed
 
 
-def check_sansymphony_virtualdiskstatus(item, _no_params, parsed):
+def check_sansymphony_virtualdiskstatus(
+    item: str, _no_params: Mapping[str, Any], parsed: Section
+) -> Iterator[tuple[int, str]]:
     if not (data := parsed.get(item)):
         return
     state = 0 if data == "Online" else 2
     yield state, "Volume state is: %s" % data
 
 
-def discover_sansymphony_virtualdiskstatus(section):
+def discover_sansymphony_virtualdiskstatus(
+    section: Section,
+) -> Iterator[tuple[str, dict[str, object]]]:
     yield from ((item, {}) for item in section)
 
 

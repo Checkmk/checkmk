0936b7f28407225f48168c078f1b0293719b0e15
René Slowenski <rene.slowenski@checkmk.com>
2026-01-26 14:50:45 +0100

    Performance test: Move tests for distributed site to separate module
    Change-Id: I455e615324753cb4cfd80059bd99c14ba5a340bd
    (cherry picked from commit 07732ab122ff9fa324360dabaa8fb574f10d4d09)

--- tests/performance/test_performance.py
+++ tests/performance/test_performance.py
@@ -3,9 +3,10 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="misc"
 
-"""To run this test with all reporting enabled, use the following command:
+"""Performance test: Single site
+
+To run this test with all reporting enabled, use the following command:
 
 $ pytest tests/performance --html=performance.htm --self-contained-html --ignore-running-procs
   --benchmark-json="benchmark.json" --benchmark-save-data --benchmark-name=long --benchmark-verbose
@@ -28,32 +29,10 @@ from tests.testlib.version import CMKVersion, version_from_env
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(name="perftest", scope="session")
+@pytest.fixture(name="perftest", scope="module")
 def _perftest(single_site: Site, pytestconfig: pytest.Config) -> Iterator[PerformanceTest]:
     """Single-site performance test"""
     yield PerformanceTest(single_site, remote_sites=None, pytestconfig=pytestconfig)
-
-
-@pytest.fixture(name="perftest_dist", scope="session")
-def _perftest_dist(
-    central_site: Site, remote_site: Site, remote_site_2: Site, pytestconfig: pytest.Config
-) -> Iterator[PerformanceTest]:
-    """Distributed performance test with 2 remote sites"""
-    yield PerformanceTest(
-        central_site, remote_sites=[remote_site, remote_site_2], pytestconfig=pytestconfig
-    )
-
-
-def test_performance_hosts(
-    perftest_dist: PerformanceTest, benchmark: BenchmarkFixture, track_system_resources: None
-) -> None:
-    """Bulk host creation"""
-    benchmark.pedantic(  # type: ignore[no-untyped-call]
-        perftest_dist.scenario_create_and_delete_hosts,
-        args=[],
-        rounds=perftest_dist.rounds,
-        iterations=perftest_dist.iterations,
-    )
 
 
 def test_performance_hosts_restart(perftest: PerformanceTest, benchmark: BenchmarkFixture) -> None:
@@ -117,18 +96,4 @@ def test_performance_ui_response(
         args=[context, page_url],
         rounds=perftest.rounds,
         iterations=perftest.iterations,
-    )
-
-
-def test_performance_bulk_change_activation(
-    perftest_dist: PerformanceTest, benchmark: BenchmarkFixture, track_system_resources: None
-) -> None:
-    """Bulk host creation"""
-    benchmark.pedantic(  # type: ignore[no-untyped-call]
-        perftest_dist.scenario_bulk_change_activation,
-        args=[],
-        setup=perftest_dist.setup_bulk_change_activation,
-        teardown=perftest_dist.teardown_bulk_change_activation,
-        rounds=perftest_dist.rounds,
-        iterations=perftest_dist.iterations,
     )

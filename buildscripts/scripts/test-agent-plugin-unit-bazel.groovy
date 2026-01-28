37c3b44c8e3ed4f8f193526a68a1987ec7276553
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-01-28 10:15:09 +0100

    Pipe output of test-agent-plugin-unit to file as well
    ... and archive it in all jobs as well as in the trigger job
    
    CMK-25972
    
    Change-Id: Ibf838cfe3c931bf3f8a6b800eaa0194435fb370d
    (cherry picked from commit d5cb6f57b1e6c5724d423fc029ceb7eac999ff61)

--- buildscripts/scripts/test-agent-plugin-unit-bazel.groovy
+++ buildscripts/scripts/test-agent-plugin-unit-bazel.groovy
@@ -4,12 +4,19 @@
 
 void main() {
     def test_jenkins_helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+    def output_file = "agent-plugin-unit-junit-bazel.txt";
 
     dir("${checkout_dir}") {
         test_jenkins_helper.execute_test([
             name: "Test for remaining python versions",
             cmd: "bazel test //tests/agent-plugin-unit:supported_python_versions",
+            output_file: output_file,
         ]);
+
+        archiveArtifacts(
+            artifacts: "${output_file}",
+            fingerprint: true,
+        );
     }
 }
 

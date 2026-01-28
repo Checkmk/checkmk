cb609ec13b5409b8322d538d05d78dc99ce8fef9
Lars Michelsen <lm@checkmk.com>
2026-01-28 20:21:43 +0100

    Clean up no-untyped-def #720
    Change-Id: I2744337028122129502264a1b3541627816b7b0f

--- cmk/base/legacy_checks/netgear_powersupplies.py
+++ cmk/base/legacy_checks/netgear_powersupplies.py
@@ -3,7 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
 # .1.3.6.1.4.1.4526.10.43.1.7.1.3.1.0 2 --> FASTPATH-BOXSERVICES-PRIVATE-MIB::boxServicesPowSupplyItemState.1.0
 # .1.3.6.1.4.1.4526.10.43.1.7.1.3.1.1 1 --> FASTPATH-BOXSERVICES-PRIVATE-MIB::boxServicesPowSupplyItemState.1.1
@@ -13,27 +12,36 @@
 
 # mypy: disable-error-code="var-annotated"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.legacy.v0_unstable import (
+    LegacyCheckDefinition,
+    LegacyCheckResult,
+    LegacyDiscoveryResult,
+)
+from cmk.agent_based.v2 import OIDEnd, SNMPTree, StringTable
 from cmk.plugins.netgear.lib import DETECT_NETGEAR
 
 check_info = {}
 
 
-def parse_netgear_powersupplies(string_table):
+def parse_netgear_powersupplies(string_table: StringTable) -> dict[str, str]:
     parsed = {}
     for oid_end, sstate in string_table:
         parsed.setdefault("%s" % oid_end.replace(".", "/"), sstate)
     return parsed
 
 
-def discover_netgear_powersupplies(parsed):
+def discover_netgear_powersupplies(parsed: dict[str, str]) -> LegacyDiscoveryResult:
     return [
         (sensorname, {}) for sensorname, sensorinfo in parsed.items() if sensorinfo not in ["1"]
     ]
 
 
-def check_netgear_powersupplies(item, params, parsed):
+def check_netgear_powersupplies(
+    item: str, params: Mapping[str, Any], parsed: dict[str, str]
+) -> LegacyCheckResult:
     map_states = {
         "1": (1, "not present"),
         "2": (0, "operational"),
@@ -41,8 +49,7 @@ def check_netgear_powersupplies(item, params, parsed):
     }
     if item in parsed:
         state, state_readable = map_states[parsed[item]]
-        return state, "Status: %s" % state_readable
-    return None
+        yield state, "Status: %s" % state_readable
 
 
 check_info["netgear_powersupplies"] = LegacyCheckDefinition(

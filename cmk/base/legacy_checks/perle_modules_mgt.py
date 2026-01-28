01e4186b403be856d121dd67a966a719aa96335d
Lars Michelsen <lm@checkmk.com>
2026-01-28 20:36:32 +0100

    Clean up no-untyped-def #723
    Change-Id: Ie17a8753ac21ac45212b832909ad15d14cebf855

--- cmk/base/legacy_checks/perle_modules_mgt.py
+++ cmk/base/legacy_checks/perle_modules_mgt.py
@@ -3,10 +3,11 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
+from collections.abc import Generator, Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
+from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition, LegacyResult
 from cmk.agent_based.v2 import SNMPTree, StringTable
 from cmk.plugins.perle.lib import DETECT_PERLE
 
@@ -17,11 +18,13 @@ check_info = {}
 # .1.3.6.1.4.1.1966.21.1.1.1.1.4.5.3.1.4.1.1 0 --> PERLE-MCR-MGT-MIB::mcrMgtLedALM.1.1
 
 
-def discover_perle_modules_mgt(info):
+def discover_perle_modules_mgt(info: StringTable) -> list[tuple[str, None]]:
     return [(index, None) for index, _name, _descr, _alarm_led, _status in info]
 
 
-def check_perle_modules_mgt(item, _no_params, info):
+def check_perle_modules_mgt(
+    item: str, _no_params: Mapping[str, Any], info: StringTable
+) -> Generator[LegacyResult]:
     mappings = {
         "alarm_led": {
             "0": (0, "no alarms"),

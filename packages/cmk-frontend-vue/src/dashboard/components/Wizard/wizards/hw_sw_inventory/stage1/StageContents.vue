6957ccfdeae0a9b305a54aa1c223aa069a7749b1
Wontek Hong <wontek.hong@checkmk.com>
2026-01-28 09:07:20 +0100

    HWSWInventory: VQA adjustments
    CMK-28238
    
    Change-Id: I621775bd114e3e391b38d5e42e7be45f082a7998

--- packages/cmk-frontend-vue/src/dashboard/components/Wizard/wizards/hw_sw_inventory/stage1/StageContents.vue
+++ packages/cmk-frontend-vue/src/dashboard/components/Wizard/wizards/hw_sw_inventory/stage1/StageContents.vue
@@ -4,22 +4,24 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import usei18n from '@/lib/i18n'
 
-import CmkCatalogPanel from '@/components/CmkCatalogPanel.vue'
 import CmkDropdown from '@/components/CmkDropdown'
+import CmkIndent from '@/components/CmkIndent.vue'
+import CmkLabel from '@/components/CmkLabel.vue'
 import type { Suggestion } from '@/components/CmkSuggestions'
+import CmkInlineValidation from '@/components/user-input/CmkInlineValidation.vue'
+import CmkLabelRequired from '@/components/user-input/CmkLabelRequired.vue'
 
+import ContentSpacer from '@/dashboard/components/Wizard/components/ContentSpacer.vue'
 import type { ConfiguredFilters, ConfiguredValues } from '@/dashboard/components/filter/types'
 import type { ContextFilters } from '@/dashboard/types/filter'
 import type { ObjectType } from '@/dashboard/types/shared'
 
 import SectionBlock from '../../../components/SectionBlock.vue'
 import Stage1Header from '../../../components/Stage1Header.vue'
-import FieldComponent from '../../../components/TableForm/FieldComponent.vue'
-import FieldDescription from '../../../components/TableForm/FieldDescription.vue'
-import TableForm from '../../../components/TableForm/TableForm.vue'
-import TableFormRow from '../../../components/TableForm/TableFormRow.vue'
 import SingleMultiWidgetObjectFilterConfiguration from '../../../components/filter/SingleMultiWidgetObjectFilterConfiguration.vue'
 import type { ElementSelection } from '../../../types'
 
@@ -31,6 +33,7 @@ interface Stage1Props {
   widgetActiveFilters: string[]
   isInFilterSelectionMenuFocus: (objectType: ObjectType) => boolean
   inventoryPaths: Suggestion[]
+  validationError?: string | undefined
 }
 
 const props = defineProps<Stage1Props>()
@@ -49,51 +52,69 @@ const gotoNextStage = () => {
 const hostFilterType = defineModel<ElementSelection>('hostFilterType', { required: true })
 const inventoryPath = defineModel<string | null>('inventoryPath', { required: true })
 const hostObjectType = 'host'
+
+const inventoryPathError = computed(() => {
+  if (props.validationError) {
+    return [props.validationError]
+  }
+  return undefined
+})
 </script>
 
 <template>
   <Stage1Header @click="gotoNextStage" />
 
   <SectionBlock :title="_t('Host selection')">
-    <SingleMultiWidgetObjectFilterConfiguration
-      v-model:mode-selection="hostFilterType"
-      :object-type="hostObjectType"
-      :configured-filters-of-object-type="props.widgetConfiguredFilters"
-      :context-filters="contextFilters"
-      :in-selection-menu-focus="isInFilterSelectionMenuFocus(hostObjectType)"
-      :single-only="true"
-      @set-focus="emit('set-focus', $event)"
-      @update-filter-values="(filterId, values) => emit('update-filter-values', filterId, values)"
-      @reset-object-type-filters="emit('reset-object-type-filters', $event)"
-      @remove-filter="(filterId) => emit('remove-filter', filterId)"
-    />
+    <CmkIndent>
+      <SingleMultiWidgetObjectFilterConfiguration
+        v-model:mode-selection="hostFilterType"
+        :object-type="hostObjectType"
+        :configured-filters-of-object-type="props.widgetConfiguredFilters"
+        :context-filters="contextFilters"
+        :in-selection-menu-focus="isInFilterSelectionMenuFocus(hostObjectType)"
+        :single-only="true"
+        @set-focus="emit('set-focus', $event)"
+        @update-filter-values="(filterId, values) => emit('update-filter-values', filterId, values)"
+        @reset-object-type-filters="emit('reset-object-type-filters', $event)"
+        @remove-filter="(filterId) => emit('remove-filter', filterId)"
+      />
+    </CmkIndent>
   </SectionBlock>
 
-  <CmkCatalogPanel :title="_t('Metric selection')">
-    <TableForm>
-      <TableFormRow>
-        <FieldDescription>
-          {{ _t('HW/SW inventory property') }}
-        </FieldDescription>
-        <FieldComponent>
-          <div class="db-stage-contents__field-component-item">
-            <CmkDropdown
-              v-model:selected-option="inventoryPath"
-              :input-hint="_t('Select inventory path')"
-              :label="_t('Inventory path')"
-              :options="{
-                type: 'fixed',
-                suggestions: props.inventoryPaths
-              }"
-            />
-          </div>
-        </FieldComponent>
-      </TableFormRow>
-    </TableForm>
-  </CmkCatalogPanel>
+  <SectionBlock :title="_t('Metric selection')">
+    <CmkIndent>
+      <div class="db-stage-contents__base-container">
+        <CmkLabel style="font-weight: var(--font-weight-bold)">{{
+          _t('HW/SW inventory property')
+        }}</CmkLabel
+        ><CmkLabelRequired space="before" />
+
+        <ContentSpacer :dimension="4" />
+        <div class="db-stage-contents__field-component-item">
+          <CmkInlineValidation :validation="inventoryPathError" />
+          <CmkDropdown
+            v-model:selected-option="inventoryPath"
+            width="fill"
+            :input-hint="_t('Select inventory path')"
+            :label="_t('Inventory path')"
+            :options="{
+              type: 'fixed',
+              suggestions: props.inventoryPaths
+            }"
+            :form-validation="inventoryPathError !== undefined"
+          />
+        </div>
+      </div>
+    </CmkIndent>
+  </SectionBlock>
 </template>
 
 <style scoped>
+.db-stage-contents__base-container {
+  background-color: var(--ux-theme-3);
+  padding: 10px;
+}
+
 .db-stage-contents__field-component-item {
   display: block;
   padding-bottom: var(--spacing-half);

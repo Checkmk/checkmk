4a05ad0883ddac4c6754191a555c038556eaac44
Lars Michelsen <lm@checkmk.com>
2026-01-28 10:40:12 +0100

    Clean up no-untyped-def #709
    Change-Id: I40fb5bb10814f9f44976908d0866b830c5326b7e

--- cmk/base/legacy_checks/msexch_owa.py
+++ cmk/base/legacy_checks/msexch_owa.py
@@ -3,27 +3,29 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 # mypy: disable-error-code="arg-type"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
+from collections.abc import Mapping
+
+from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition, LegacyCheckResult
 from cmk.base.check_legacy_includes.wmi import (
     inventory_wmi_table_total,
     wmi_yield_raw_counter,
     wmi_yield_raw_persec,
 )
-from cmk.plugins.windows.agent_based.libwmi import parse_wmi_table
+from cmk.plugins.windows.agent_based.libwmi import parse_wmi_table, WMISection
 
 check_info = {}
 
 
-def discover_msexch_owa(parsed):
+def discover_msexch_owa(parsed: WMISection) -> list[tuple[None, dict[str, object]]]:
     return inventory_wmi_table_total(parsed)
 
 
-def check_msexch_owa(_no_item, params, parsed):
+def check_msexch_owa(
+    _no_item: None, params: Mapping[str, object], parsed: WMISection
+) -> LegacyCheckResult:
     table = parsed[""]
     yield from wmi_yield_raw_persec(
         table, None, "RequestsPersec", infoname="Requests/sec", perfvar="requests_per_sec"

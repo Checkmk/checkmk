9a091455d04544a91088bd6de51baaf0354d4e48
Simon Jess <simon.jess@checkmk.com>
2026-02-05 10:30:13 +0100

    19048 FIX (Windows) HW/SW Inventory: Remove leading comma from the 'Network adapters: IPv4 address' cell
    CMK-29663
    
    Change-Id: Id86ec9bd3bf2311b026d1a31e05c3d7ad91d1db9

--- cmk/plugins/collection/agent_based/inventory_win_networkadapter.py
+++ cmk/plugins/collection/agent_based/inventory_win_networkadapter.py
@@ -38,7 +38,7 @@ def parse_win_networkadapter(  # pylint: disable=too-many-branches
 
         stripped_line = [w.strip() for w in line]
         varname = stripped_line[0]
-        value = ":".join(line[1:])
+        value = ":".join(line[1:]).strip()
 
         # empty? skip!
         if not value:
@@ -75,8 +75,8 @@ def parse_win_networkadapter(  # pylint: disable=too-many-branches
             array["gateway"] = value
 
         # address string array in comma-separated string packing: ['a1','a2',...] -> 'a1,a2...'
-        for addrtype in addrtypes:
-            array[addrtype] = ", ".join(addrtypes[addrtype])
+        for addrtype, values in addrtypes.items():
+            array[addrtype] = ", ".join(values)
 
     # Append last array
     if array:

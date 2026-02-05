6f2c917a908395bbc034d96ea21213cd72fcd34b
Logan Connolly <logan.connolly@checkmk.com>
2026-02-05 14:37:00 +0100

    19181 mem_win: use consistent language
    In some places, "commit charge" was used while in other areas, "virtual
    memory" was displayed to the user. This led to confusion among users.
    
    To be more consistent, we will now only refer to it as "virtual memory".
    This required updates to the man page, rulesets, and graph definitions.
    It's important to note that this is only a UI facing change, so no
    migrations necessary for the related metric names.
    
    SUP-26841
    
    Change-Id: I45341ad325929da66e84b247003ae26865fbbd8e

--- cmk/plugins/memory/graphing/pagefile_absolute_avg.py
+++ cmk/plugins/memory/graphing/pagefile_absolute_avg.py
@@ -33,10 +33,10 @@ metric_pagefile_free_avg = Metric(
 
 graph_pagefile_absolute_avg = Graph(
     name="pagefile_absolute_avg",
-    title=Title("Commit charge (averaged)"),
+    title=Title("Virtual memory (averaged)"),
     simple_lines=(
         Sum(
-            Title("Total commitable memory"),
+            Title("Total virtual memory"),
             Color.DARK_BLUE,
             ("pagefile_used_avg", "pagefile_free_avg"),
         ),

30925699e414d68924842b9b728f6ea82e1c28ba
Frans Fürst <frans.fuerst@checkmk.com>
2026-02-05 15:29:22 +0100

    agent_hp_msa: apply linter findings and modernize
    .. as preparation to for SUP-23747 and SUP-23681
    
    Change-Id: I24df1ea8dd0c043cc1213b7b2f2471300bb0ead8

--- cmk/special_agents/agent_hp_msa.py
+++ cmk/special_agents/agent_hp_msa.py
@@ -2,6 +2,12 @@
 # Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+"""agent_hp_msa
+
+Checkmk special agent for monitoring HP MSA storage systems.
+"""
+
+# mypy: disable-error-code="type-arg"
 
 import argparse
 import atexit
@@ -131,10 +137,10 @@ class AuthError(RuntimeError):
 
 
 class HPMSAConnection:
-    def __init__(self, hostaddress: str, opt_timeout: int) -> None:
+    def __init__(self, *, hostaddress: str, timeout: int) -> None:
         self._host = hostaddress
-        self._base_url = "https://%s/api/" % self._host
-        self._timeout = opt_timeout
+        self._base_url = f"https://{self._host}/api/"
+        self._timeout = timeout
         self._session = requests.Session()
         self._session.headers = CaseInsensitiveDict({"User-agent": "Checkmk special agent_hp_msa"})
         # we cannot use self._session.verify because it will be overwritten by
@@ -146,17 +152,24 @@ class HPMSAConnection:
         try:
             session_key = self._get_session_key(hashlib.sha256, username, password)
         except (requests.exceptions.ConnectionError, AuthError):
-            # Try to connect to old API if no connection to new API can be established
-            self._base_url = "https://%s/v3/api/" % self._host
+            LOGGER.warning("Could not connect via /api/, retry with /v3/api.")
+            self._base_url = f"https://{self._host}/v3/api/"
             session_key = self._get_session_key(hashlib.md5, username, password)
 
         self._session.headers.update(sessionKey=session_key)
 
+    def logout(self) -> None:
+        if not (session_key := self._session.headers.pop("sessionKey", None)):
+            LOGGER.warning("No session key found during logout")
+            return
+        self.get(uri=f"logout/{str(session_key)}")
+        LOGGER.debug("Successfully logged out from HP MSA device")
+
     def _get_session_key(self, hash_class: Callable, username: str, password: str) -> str:
         login_hash = hash_class()
         login_hash.update(f"{username}_{password}".encode())
-        login_url = "login/%s" % login_hash.hexdigest()
-        response = self.get(login_url)
+        login_uri = f"login/{login_hash.hexdigest()}"
+        response = self.get(uri=login_uri)
         xml_tree = ET.fromstring(response.text)
         response_element = xml_tree.find("./OBJECT/PROPERTY[@name='response']")
         if response_element is None:
@@ -166,14 +179,13 @@ class HPMSAConnection:
             raise Exception("invalid response element")
         if session_key.lower() == "authentication unsuccessful":
             raise AuthError(
-                "Connecting to %s failed. Please verify host address & login details"
-                % self._base_url
+                f"Connecting to {self._base_url} failed. Please verify host address & login details"
             )
         return session_key
 
-    def get(self, url_suffix: str) -> requests.Response:
-        url = urljoin(self._base_url, url_suffix)
-        LOGGER.info("GET %r", url)
+    def get(self, *, uri: str) -> requests.Response:
+        url = urljoin(self._base_url, uri)
+        LOGGER.debug("GET %r", url)
         # we must provide the verify keyword to every individual request call!
         response = self._session.get(url, timeout=self._timeout, verify=self._verify_ssl)
         if response.status_code != 200:
@@ -183,38 +195,35 @@ class HPMSAConnection:
         LOGGER.debug("RESPONSE.text\n%s", response.text)
         return response
 
-    def logout(self) -> None:
-        try:
-            session_key = self._session.headers.pop("sessionKey")
-        except KeyError:
-            LOGGER.info("Tried to logout without an active session.")
-            return None
-        else:
-            self.get(f"logout/{str(session_key)}")
-
 
 def main(argv: Sequence[str] | None = None) -> int:
     replace_passwords()
     args = parse_arguments(argv or sys.argv[1:])
     opt_timeout = 10
 
-    connection = HPMSAConnection(args.hostaddress, opt_timeout)
+    connection = HPMSAConnection(hostaddress=args.hostaddress, timeout=opt_timeout)
     connection.login(args.username, args.password)
     atexit.register(connection.logout)
 
     parser = HTMLObjectParser()
 
     for element in api_get_objects:
-        response = connection.get("show/%s" % element)
+        response = connection.get(uri="show/%s" % element)
         try:
             parser.feed(response.text)
-        except Exception:
+        except Exception as exc:  # broad exception because we don't know what to catch
             if args.debug:
                 raise
+            LOGGER.warning("Failed to parse response for %s: %s", element, exc)
 
     # Output sections
     for section, lines in sections.items():
         sys.stdout.write("<<<hp_msa_%s>>>\n" % section)
-        sys.stdout.write("\n".join(x for x in lines) + "\n")
+        sys.stdout.write("\n".join(lines))
+        sys.stdout.write("\n")
 
     return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())

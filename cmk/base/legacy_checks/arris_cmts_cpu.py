fb33225c783dbadb50c4894a891dd55be1a2ae6f
Max Höhl <max.hoehl@checkmk.com>
2026-02-05 15:53:56 +0100

    19028 FIX arris_cmts_cpu: Fix Average configuration of CPU utilization of Devices with Modules ruleset
    Prior to this change the average configuration provided by the
    `cpu_utilization_multiitem` ruleset would be ignored.
    
    Jira: SUP-27626
    Change-Id: Id9628c430ff1978617e5aaef74b2a7518156344a

--- cmk/base/legacy_checks/arris_cmts_cpu.py
+++ cmk/base/legacy_checks/arris_cmts_cpu.py
@@ -3,9 +3,11 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import time
 
 from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import equals, OIDEnd, SNMPTree, StringTable
+from cmk.agent_based.v2 import equals, get_value_store, OIDEnd, SNMPTree, StringTable
+from cmk.plugins.lib.cpu_util import check_cpu_util
 
 check_info = {}
 
@@ -28,18 +30,13 @@ def check_arris_cmts_cpu(item, params, info):
 
         if citem == item:
             # We get the IDLE percentage, but need the usage
-            cpu_util = 100 - int(cpu_idle_util)
-            warn, crit = params["levels"]
-
-            infotext = "Current utilization is: %d %% " % cpu_util
-            levels = f" (warn/crit at {warn:.1f}/{crit:.1f} %)"
-            perfdata = [("util", cpu_util, warn, crit)]
-            if cpu_util >= crit:
-                yield 2, infotext + levels, perfdata
-            elif cpu_util >= warn:
-                yield 1, infotext + levels, perfdata
-            else:
-                yield 0, infotext, perfdata
+            cpu_util = 100.0 - float(cpu_idle_util)
+            yield from check_cpu_util(
+                util=cpu_util,
+                params=params,
+                value_store=get_value_store(),
+                this_time=time.time(),
+            )
             return
 
 

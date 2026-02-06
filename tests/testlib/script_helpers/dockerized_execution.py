44fce4118a3ed49b08705450ef94ba52a72681e4
Nigel Banks <nigel.banks@checkmk.com>
2026-02-06 14:40:18 +0100

    Change system tests to no longer modify /etc/sudoers
    Appending to the end of the file no longer works as of SLES-16.0, as the
    file doesn't exist in that distro and creating it overrides the
    default behavior, removing the ability of root to use sudo.
    
    Instead write to a file in /etc/sudoers.d/ as that is supported on all
    distros
    
    CMK-24291
    
    Change-Id: I81600bc95c5b99b8773da90b7f935896ed7dd780
    (cherry picked from commit 559c9f71017b7f9275d9fe2a902b3ca1787264da)

--- tests/testlib/script_helpers/dockerized_execution.py
+++ tests/testlib/script_helpers/dockerized_execution.py
@@ -827,7 +827,7 @@ def _prepare_testuser(container: docker.models.containers.Container, username: s
     )
     _exec_run(
         container,
-        ["bash", "-c", f'echo "{username} ALL=(ALL) NOPASSWD: ALL">>/etc/sudoers'],
+        ["bash", "-c", f'echo "{username} ALL=(ALL) NOPASSWD: ALL">>/etc/sudoers.d/{username}'],
     )
 
     _exec_run(container, ["mkdir", "-p", f"/home/{username}/.cache"])

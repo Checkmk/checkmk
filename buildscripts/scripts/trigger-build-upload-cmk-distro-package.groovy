5356651c661240af92e356793911c96ea6d69efe
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2026-02-09 11:15:52 +0100

    Fixup #2
    Change-Id: I107ec36b341340d71ddad1321ec22e44e30622c6

--- buildscripts/scripts/trigger-build-upload-cmk-distro-package.groovy
+++ buildscripts/scripts/trigger-build-upload-cmk-distro-package.groovy
@@ -59,13 +59,11 @@ void main() {
 
     def stages = all_editions.collectEntries { edition ->
         [("${edition}") : {
-            def build_instance = null;
-
             smart_stage(
                 name: "Trigger ${edition} package build",
                 raiseOnError: true,
             ) {
-                build_instance = smart_build(
+                smart_build(
                     // see global-defaults.yml, needs to run in minimal container
                     use_upstream_build: true,
                     relative_job_name: "${branch_base_folder}/builders/trigger-cmk-distro-package",

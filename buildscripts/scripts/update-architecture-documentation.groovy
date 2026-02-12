e2e4771f8755a232b3b9500abe752c135244f962
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-02-12 10:31:39 +0100

    Rework update-architecture-documentation to run in k8s
    CMK-29585
    
    Change-Id: I91adae08e374f06cfc1a9c4aa2098f5b4a0801ad

--- buildscripts/scripts/update-architecture-documentation.groovy
+++ buildscripts/scripts/update-architecture-documentation.groovy
@@ -3,15 +3,29 @@
 /// file: update-architecture-documentation.groovy
 
 void main() {
-    dir("${checkout_dir}") {
+    def helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+    def safe_branch_name = versioning.safe_branch_name();
+
+    dir(checkout_dir) {
         // Sphinx supports many different builders.
         // We have to use the same for creating new documentation and for
         // copying the documentation over.
         def sphinx_builder = "html";
 
-        stage("Update") {
-            inside_container() {
-                sh("make -C doc/documentation ${sphinx_builder}");
+        stage("Create") {
+            def container_name = "testing-ubuntu-2204-checkmk-${safe_branch_name.replace('.', '-')}";
+
+            def lock_label = "bzl_lock_${env.NODE_NAME.split('\\.')[0].split('-')[-1]}";
+            if (kubernetes_inherit_from != "UNSET") {
+                lock_label = "bzl_lock_k8s";
+            }
+            lock(label: lock_label, quantity: 1, resource : null) {
+                helper.execute_test([
+                    name       : "documentation",
+                    cmd        : "make -C doc/documentation ${sphinx_builder}",
+                    container_name: container_name,
+                ]);
             }
         }
 

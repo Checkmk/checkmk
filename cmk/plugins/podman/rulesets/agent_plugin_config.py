14703d90580b5dcbc19bc9382b607c33fd056b24
Luka Racic <luka.racic@checkmk.com>
2026-02-16 18:29:18 +0100

    fix: rename Podman rule title for clarity
    CMK-29487
    
    Change-Id: I3d5a9d02a423ad0effa66e037462b15323f1f4b8

--- cmk/plugins/podman/rulesets/agent_plugin_config.py
+++ cmk/plugins/podman/rulesets/agent_plugin_config.py
@@ -102,7 +102,7 @@ def _validate_absolute_posix_path(value: str) -> None:
 
 rule_spec_podman_bakelet = AgentConfig(
     name="mk_podman",
-    title=Title("Podman node and containers"),
+    title=Title("Podman hosts and containers (Linux)"),
     topic=Topic.APPLICATIONS,
     parameter_form=_agent_config_mk_podman,
 )

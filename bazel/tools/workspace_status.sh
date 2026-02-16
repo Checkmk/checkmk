bdc5f1bb4389797f7394705d1ab58f352e62691d
Jose Naranjo <jose.naranjo@checkmk.com>
2026-02-16 17:52:38 +0100

    fix: add the creation timestamp to the relay image
    CMK-31468
    
    Change-Id: I1a9b648b23ca749833a92df46c2e71fe6cc81a95
    (cherry picked from commit 8010e129e69009cfeb9f0521310114ff5d9953b5)

--- bazel/tools/workspace_status.sh
+++ bazel/tools/workspace_status.sh
@@ -3,3 +3,6 @@ set -e
 
 # A stable identifier: commit hash
 echo "STABLE_GIT_COMMIT $(git rev-parse HEAD)"
+
+# Volatile: current build timestamp in ISO 8601 format (for OCI image metadata)
+echo "BUILD_TIMESTAMP_ISO $(date -u +%Y-%m-%dT%H:%M:%SZ)"

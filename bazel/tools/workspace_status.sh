65553eb27e7ae73e4d4d4d1d2b3498b9edd04f8e
Jose Naranjo <jose.naranjo@checkmk.com>
2026-02-19 13:36:22 +0100

    chore: obtain the build timestamp from the last commit
    CMK-31468
    
    Change-Id: Ib849824552d6b963c03fa2da2d1a67c2b83ea1bf
    (cherry picked from commit ff18c72e150cc627aa9cfb5a2e9f41ef2589725f)

--- bazel/tools/workspace_status.sh
+++ bazel/tools/workspace_status.sh
@@ -4,5 +4,5 @@ set -e
 # A stable identifier: commit hash
 echo "STABLE_GIT_COMMIT $(git rev-parse HEAD)"
 
-# Volatile: current build timestamp in ISO 8601 format (for OCI image metadata)
-echo "BUILD_TIMESTAMP_ISO $(date -u +%Y-%m-%dT%H:%M:%SZ)"
+# Volatile: last git commit timestamp in ISO 8601 format UTC (for OCI image metadata)
+echo "BUILD_TIMESTAMP_ISO $(date -u -d @"$(git log -1 --format=%ct HEAD)" +%Y-%m-%dT%H:%M:%SZ)"

c4ac54e339d3e406e4c0029015d59d7cb96e10e6
Sebastian Groemcke <sebastian.groemcke@checkmk.com>
2026-02-20 15:52:42 +0100

    Shareable dashboards: show placeholder
    for sidebar widgets instead of redirecting to login
    
    JIRA-Ref: CMK-29666
    Change-Id: I3e193181b94ef46f5c4970a82d837dfde058719d

--- packages/cmk-frontend-vue/src/dashboard/components/DashboardContent/DashboardContentSidebarElement.vue
+++ packages/cmk-frontend-vue/src/dashboard/components/DashboardContent/DashboardContentSidebarElement.vue
@@ -6,12 +6,19 @@ conditions defined in the file COPYING, which is part of this source code packag
 <script setup lang="ts">
 import { computed } from 'vue'
 
+import usei18n from '@/lib/i18n'
+
+import { useInjectIsPublicDashboard } from '@/dashboard/composables/useIsPublicDashboard'
 import type { IFrameContent, SidebarElementContent } from '@/dashboard/types/widget.ts'
 
+import DashboardContentContainer from './DashboardContentContainer.vue'
 import DashboardContentIFrame from './DashboardContentIFrame.vue'
 import type { ContentProps } from './types.ts'
 
+const { _t } = usei18n()
+
 const props = defineProps<ContentProps<SidebarElementContent>>()
+const isPublicDashboard = useInjectIsPublicDashboard()
 
 const iFrameProps = computed(() => {
   const urlParams = new URLSearchParams({
@@ -29,5 +36,21 @@ const iFrameProps = computed(() => {
 </script>
 
 <template>
-  <DashboardContentIFrame v-bind="iFrameProps" />
+  <DashboardContentContainer
+    v-if="isPublicDashboard"
+    :effective-title="effectiveTitle"
+    :general_settings="general_settings"
+  >
+    <div class="db-content-sidebar-element__not-available">
+      {{ _t('Not available on shared dashboards') }}
+    </div>
+  </DashboardContentContainer>
+  <DashboardContentIFrame v-else v-bind="iFrameProps" />
 </template>
+
+<style scoped>
+.db-content-sidebar-element__not-available {
+  padding: var(--spacing);
+  color: var(--font-color);
+}
+</style>

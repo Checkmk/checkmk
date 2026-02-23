634c64ed2e2dd682b7eb8c767385a4bf4f2da571
Logan Connolly <logan.connolly@checkmk.com>
2026-02-23 19:20:58 +0100

    search: do not collapse host alias entries
    The host alias entry was originally collapsed before we introduced topic
    grouping. Now that we are grouping, we should just show it in its own
    dedicated topic. This greatly reduces the complexity, particularly in
    regards to the monitoring count.
    
    Picked and squashed from these changes on master:
    
     * I5f6c42a06e05a1f1444fb4757f531d0545d1a248
     * I92f36044900bb62ec5bf903531f9546393d47d9c
     * Ib8655625c87103b01d3d2b0e8a1b216e04d9e04d
    
    CMK-31144
    
    Change-Id: I7a8042930fbb9d289a787d453ff8d28ae20d2c0f

--- cmk/gui/search/collapsing.py
+++ cmk/gui/search/collapsing.py
@@ -3,6 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+import dataclasses
 from itertools import groupby
 from typing import Protocol
 
@@ -47,83 +48,51 @@ def _collapse_items(
     collapsed_results: list[UnifiedSearchResultItem] = []
     collapsed_result_count = 0
 
-    tr_hostsetup, tr_hostname, tr_hostalias = _("Hosts"), _("Host name"), _("Hostalias")
-    tr_host_keys = frozenset({tr_hostsetup, tr_hostname, tr_hostalias})
-
     for _title, group in groupby(results, key=lambda item: item.title):
-        host_items: dict[str, UnifiedSearchResultItem] = {}
+        host_setup_item: UnifiedSearchResultItem | None = None
+        host_monitoring_item: UnifiedSearchResultItem | None = None
         other_items: list[UnifiedSearchResultItem] = []
 
         # WARN: this logic only works because of some assumptions we make about the ordering from
         # the sort algorithm. We expect the following (translated) topics:
         #   1. setup host (topic "Hosts")
         #   2. monitoring host (topic "Host name")
-        #   3. optionally: monitoring host alias (topic "Hostalias")
         # to be grouped together and in this order in the unified search result. When that changes,
         # then this functionality will no longer work.
         for item in group:
-            if item.topic in tr_host_keys:
-                host_items.update({item.topic: item})
+            if item.topic == _("Hosts"):
+                host_setup_item = item
+            elif item.topic == _("Host name"):
+                host_monitoring_item = item
             else:
                 other_items.append(item)
 
-        match (
-            host_items.get(tr_hostsetup),
-            host_items.get(tr_hostname),
-            host_items.get(tr_hostalias),
-        ):
-            case (
-                UnifiedSearchResultItem() as setup,
-                UnifiedSearchResultItem() as name,
-                UnifiedSearchResultItem() as alias,
-            ):
-                collapsed_results.append(_collapse_host_items([name, alias], setup))
+        match host_setup_item, host_monitoring_item:
+            case (UnifiedSearchResultItem() as setup, UnifiedSearchResultItem() as monitoring):
+                collapsed_results.append(_collapse_host_items(monitoring, setup))
                 collapsed_result_count += 1
-            case (
-                UnifiedSearchResultItem() as setup,
-                UnifiedSearchResultItem() as name,
-                None,
-            ):
-                collapsed_results.append(_collapse_host_items([name], setup))
-            case (
-                None,
-                UnifiedSearchResultItem() as name,
-                UnifiedSearchResultItem() as alias,
-            ):
-                collapsed_results.append(_collapse_host_items([name, alias]))
-                collapsed_result_count += 1
-            case (
-                None,
-                UnifiedSearchResultItem() as name,
-                None,
-            ):
-                collapsed_results.append(_collapse_host_items([name]))
+            case (None, UnifiedSearchResultItem() as monitoring):
+                collapsed_results.append(_collapse_host_items(monitoring))
+            case (UnifiedSearchResultItem() as setup, None):
+                collapsed_results.append(setup)
             case _:
-                collapsed_results.extend(host_items.values())
+                pass
 
         if other_items:
             collapsed_results.extend(other_items)
 
-    updated_counts = UnifiedSearchResultCounts(
-        total=len(collapsed_results),
-        setup=counts.setup,
-        monitoring=counts.monitoring - collapsed_result_count,
-        customize=counts.customize,
-    )
-
-    return collapsed_results, updated_counts
+    return collapsed_results, dataclasses.replace(counts, total=len(collapsed_results))
 
 
 def _collapse_host_items(
-    monitoring_items: list[UnifiedSearchResultItem],
+    monitoring_item: UnifiedSearchResultItem,
     setup_item: UnifiedSearchResultItem | None = None,
 ) -> UnifiedSearchResultItem:
     return UnifiedSearchResultItem(
-        title=monitoring_items[0].title,
-        target=monitoring_items[0].target,
+        title=monitoring_item.title,
+        target=monitoring_item.target,
         provider=ProviderName.monitoring,
         topic="Hosts",
-        context=", ".join(item.topic for item in monitoring_items),
         inline_buttons=[
             UnifiedSearchResultItemInlineButton(
                 target=setup_item.target,
--- tests/unit/cmk/gui/search/test_collapsing.py
+++ tests/unit/cmk/gui/search/test_collapsing.py
@@ -31,6 +31,7 @@ def test_host_collapsing() -> None:
             topic="Host name",
             target={"url": "/monitoring/testhost"},
         ),
+        # Host alias does not get collapsed
         UnifiedSearchResultItemFactory.build(
             title="testhost",
             provider=ProviderName.monitoring,
@@ -44,37 +45,20 @@ def test_host_collapsing() -> None:
     results, counts = collapse(initial_results, initial_count)
 
     # expecting only one result here
-    assert len(results) == 1
+    assert len(results) == 2
 
     # check that matching monitoring results were merged
     assert results[0].title == "testhost"
     assert results[0].provider == ProviderName.monitoring
     assert results[0].target.url == "/monitoring/testhost"
-    assert results[0].context == "Host name, Hostalias"
 
     # check the inline setup edit button
     assert results[0].inline_buttons
     assert results[0].inline_buttons[0].title == "Edit"
     assert results[0].inline_buttons[0].target.url == "/setup/testhost"
 
-    # check that counts were updated accordingly
-    assert counts == UnifiedSearchResultCounts(total=1, setup=1, monitoring=1, customize=0)
-
-
-def test_host_collapsing_unmatched_alias() -> None:
-    initial_results = [
-        UnifiedSearchResultItemFactory.build(title="testhost", topic="Hosts"),
-        UnifiedSearchResultItemFactory.build(title="testhost", topic="Host name"),
-        UnifiedSearchResultItemFactory.build(title="Test Host", topic="Hostalias"),
-    ]
-    initial_counts = UnifiedSearchResultCounts(total=3, setup=1, monitoring=2, customize=0)
-    collapse = get_collapser(provider=None, disabled=False)
-
-    (collapsed_result, alias_result), counts = collapse(initial_results, initial_counts)
-
-    # check that matching monitoring results were merged
-    assert collapsed_result.title == "testhost"
-    assert alias_result.title == "Test Host"
+    # Host alias is second result
+    assert results[1].topic == "Hostalias"
 
     # check that counts were updated accordingly
     assert counts == UnifiedSearchResultCounts(total=2, setup=1, monitoring=2, customize=0)
@@ -95,42 +79,6 @@ def test_host_collapsing_disabled() -> None:
     assert counts == initial_counts
 
 
-def test_host_collapsing_setup_item_missing() -> None:
-    initial_results = [
-        UnifiedSearchResultItemFactory.build(
-            title="testhost",
-            provider=ProviderName.monitoring,
-            topic="Host name",
-            target={"url": "/monitoring/testhost"},
-        ),
-        UnifiedSearchResultItemFactory.build(
-            title="testhost",
-            provider=ProviderName.monitoring,
-            topic="Hostalias",
-            target={"url": "/monitoring/testhost"},
-        ),
-    ]
-    initial_count = UnifiedSearchResultCounts(total=2, setup=0, monitoring=2, customize=0)
-    collapse = get_collapser(provider=None, disabled=False)
-
-    results, counts = collapse(initial_results, initial_count)
-
-    # expecting only one result here
-    assert len(results) == 1
-
-    # check that there is no inline setup edit button
-    assert not results[0].inline_buttons
-
-    # check that matching monitoring results were merged
-    assert results[0].title == "testhost"
-    assert results[0].provider == ProviderName.monitoring
-    assert results[0].target.url == "/monitoring/testhost"
-    assert results[0].context == "Host name, Hostalias"
-
-    # check that counts were updated accordingly
-    assert counts == UnifiedSearchResultCounts(total=1, setup=0, monitoring=1, customize=0)
-
-
 def test_host_collapsing_ignored_with_only_setup_item() -> None:
     initial_results = [
         UnifiedSearchResultItemFactory.build(title="testhost", topic="Hosts"),
@@ -145,16 +93,14 @@ def test_host_collapsing_ignored_with_only_setup_item() -> None:
     assert counts == initial_count
 
 
-def test_host_collapsing_ignored_with_setup_item_and_alias() -> None:
+def test_host_name_topic_converted_to_hosts_without_setup_item() -> None:
     initial_results = [
-        UnifiedSearchResultItemFactory.build(title="testhost", topic="Hosts"),
-        UnifiedSearchResultItemFactory.build(title="testhost", topic="Hostalias"),
+        UnifiedSearchResultItemFactory.build(title="testhost", topic="Host name"),
     ]
-    initial_count = UnifiedSearchResultCounts(total=2, setup=1, monitoring=1, customize=0)
+    initial_count = UnifiedSearchResultCounts(total=1, setup=0, monitoring=1, customize=0)
     collapse = get_collapser(provider=None, disabled=False)
 
     results, counts = collapse(initial_results, initial_count)
 
-    # check that no transformation occurs when no monitoring items are available
-    assert results == initial_results
-    assert counts == initial_count
+    # We want the topic name to be consistent even if you cannot edit the host.
+    assert results[0].topic == "Hosts"

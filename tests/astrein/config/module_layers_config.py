ff04d5d859a42be05251e55554358c5b926fa533
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-02-25 15:43:08 +0100

    bakery merge type: extend and test legacy merge type list
    This extends the list of agent rulesets with special merge types
    by all legacy rulesets. We expect the merge type to be `DICT`
    once we migrate to the new API, this is ensured by a test.
    
    CMK-28655
    
    Change-Id: I0efb5b08e4e16777f2fd8ea77cc2432c1911f896

--- tests/astrein/config/module_layers_config.py
+++ tests/astrein/config/module_layers_config.py
@@ -1161,6 +1161,8 @@ COMPONENTS: Mapping[Component, ImportCheckerProtocol] = {
         "cmk.fetchers",
         "cmk.gui",
         "cmk.helper_interface",
+        "cmk.logwatch.config",
+        "cmk.nonfree.pro.bakery",
         "cmk.piggyback",
         "cmk.server_side_calls_backend",
         "cmk.utils",

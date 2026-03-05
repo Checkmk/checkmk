1ae88c1d6c24905867bca6766b5966b8aaf9630d
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-03-05 09:18:06 +0100

    Audit log: Add user id for activate changes/bakery calls
    The newly introduced Bakery audit log entry includes the user id for
    manual calls from the GUI, so it should also be included for calls
    triggered by activate changes.
    
    Since the triggering activate changes call happens right above that,
    it makes sense to include the user id also there.
    
    SUP-25359
    
    Change-Id: If5713db0a8478013cf5b5c5575001910d4f4353e

--- cmk/gui/watolib/activate_changes.py
+++ cmk/gui/watolib/activate_changes.py
@@ -1793,10 +1793,18 @@ class ActivateChangesManager(ActivateChanges):
 
     def _log_activation(self):
         log_msg = "Starting activation (Sites: %s)" % ",".join(self._sites)
-        log_audit("activate-changes", log_msg)
+        log_audit(
+            "activate-changes",
+            log_msg,
+            user_id=user.id,
+        )
 
         if self._comment:
-            log_audit("activate-changes", "Comment: %s" % self._comment)
+            log_audit(
+                "activate-changes",
+                "Comment: %s" % self._comment,
+                user_id=user.id,
+            )
 
         # Baking will happen on core config generation
         # Since we don't have access to the GUI there, we log the call here.
@@ -1805,6 +1813,7 @@ class ActivateChangesManager(ActivateChanges):
                 action="bake-agents",
                 message="Bake agents (triggered by: Activate Changes)",
                 diff_text="Hosts: All hosts",
+                user_id=user.id,
             )
 
     def get_state(self) -> ActivationState:

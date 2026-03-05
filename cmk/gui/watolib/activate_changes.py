8a5b48c786453ddf3a67167abc5b29167397cbc7
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-03-05 07:29:01 +0100

    Agent Bakery: Log calls from GUI to audit log
    All calls from the GUI are logged to the audit log now
    This includes:
    - Manual bake/bake and sign action from Agent Bakery GUI
    - Automatic bake on host creation
    - Automatic bake on Activate Changes (if activated in Global Settings)
    - Initial bake on site creation
    
    This does not include:
    - Manual/debug 'cmk --bake-agents' call on command line as site user
    
    SUP-25359
    
    Change-Id: Iaf484ba2bc74ec8837f8ebfde2c15055b4e003c7

--- cmk/gui/watolib/activate_changes.py
+++ cmk/gui/watolib/activate_changes.py
@@ -2039,6 +2039,17 @@ class ActivateChangesManager:
                 use_git=use_git,
             )
 
+        # Baking will happen on core config generation
+        # Since we don't have access to the GUI there, we log the call here.
+        if load_configuration_settings().get("bake_agents_on_restart"):
+            log_audit(
+                action="bake-agents",
+                message="Bake agents (triggered by: Activate Changes)",
+                user_id=user.id,
+                use_git=use_git,
+                diff_text="Hosts: All hosts",
+            )
+
     def get_state(self) -> ActivationState:
         return {"sites": {site_id: self.get_site_state(site_id) for site_id in self._sites}}  #
 

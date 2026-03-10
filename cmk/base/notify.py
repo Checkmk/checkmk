92e387ace678e2d2b500f2121346a813944507a0
Ronny Bruska <ronny.bruska@checkmk.com>
2026-03-10 10:56:52 +0100

    19091: FIX Fix possible ValueError on user notification rules
    SUP-27333
    
    Change-Id: I6ed8c1010273360312a4421ab8ef6e5be7d6de28

--- cmk/base/notify.py
+++ cmk/base/notify.py
@@ -890,7 +890,7 @@ def _create_notifications(
         plugin_parameters = (
             parameters[plugin_name][plugin_parameter_id]["parameter_properties"]
             if isinstance(plugin_parameter_id, str)
-            else plugin_parameter_id
+            else {"params": plugin_parameter_id}  # type: ignore[typeddict-item]
         )
 
         final_parameters: NotifyPluginParamsDict = _rbn_finalize_plugin_parameters(

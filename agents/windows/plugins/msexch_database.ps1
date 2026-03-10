8251998f8ded32b5541e045a80002f48d5cb835a
Checkmk release system <noreply@checkmk.com>
2026-03-10 15:25:39 +0100

    Set version to 2.3.0p45

--- agents/windows/plugins/msexch_database.ps1
+++ agents/windows/plugins/msexch_database.ps1
@@ -1,4 +1,4 @@
-$CMK_VERSION = "2.3.0p44"
+$CMK_VERSION = "2.3.0p45"
 ## MS Exchange Database counters
 
 ## localize counter name
--- agents/windows/plugins/storcli.bat
+++ agents/windows/plugins/storcli.bat
@@ -1,5 +1,5 @@
 @ECHO off & setlocal EnableDelayedExpansion
-set CMK_VERSION="2.3.0p44"
+set CMK_VERSION="2.3.0p45"
 REM ***
 REM * plugin to to monitor RAID status via StorCLI utility
 REM * customize StorCli path to your needs

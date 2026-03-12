82c3c787a8d1ac63864a51cb1ed019710439a159
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-03-12 10:39:53 +0100

    Move test-composition to k8s
    ... as this only blocks an executor slot on classic build node and 90%
    of the time is waiting for another job to complete.
    It is a classic trigger job
    
    Requires https://review.lan.tribe29.com/c/checkmk_ci/+/138858
    
    CMK-31467
    
    Change-Id: I483ca7f6ffe2c89c5d758b0b5177154bee856f80

--- buildscripts/scripts/test-composition.groovy
+++ buildscripts/scripts/test-composition.groovy
@@ -17,19 +17,26 @@ void main() {
     def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
     def package_helper = load("${checkout_dir}/buildscripts/scripts/utils/package_helper.groovy");
 
+    // TODO: we should always use USE_CASE directly from the job parameters
+    def use_case = (USE_CASE == "fips") ? USE_CASE : "daily_tests";
+    def all_distros = [];
+    def selected_distros = [];
+    def safe_branch_name = versioning.safe_branch_name();
+    def branch_version = versioning.get_branch_version(checkout_dir);
+
+    inside_container_minimal(safe_branch_name: safe_branch_name) {
+        // run everything requiring python in this container
+        all_distros = versioning.get_distros(override: "all");
+        selected_distros = versioning.get_distros(
+            edition: EDITION,
+            use_case: use_case,
+            override: OVERRIDE_DISTROS
+        );
+    }
+
     /// This will get us the location to e.g. "checkmk/master" or "Testing/<name>/checkmk/master"
     def branch_base_folder = package_helper.branch_base_folder(true);
 
-    // TODO: we should always use USE_CASE directly from the job parameters
-    def use_case = (USE_CASE == "fips") ? USE_CASE : "daily_tests";
-    def all_distros = versioning.get_distros(override: "all");
-    def selected_distros = versioning.get_distros(
-        edition: EDITION,
-        use_case: use_case,
-        override: OVERRIDE_DISTROS
-    );
-    def safe_branch_name = versioning.safe_branch_name();
-    def branch_version = versioning.get_branch_version(checkout_dir);
     // When building from a git tag (VERSION != "daily"), we cannot get the branch name from the scm so used defines.make instead.
     // this is save on master as there are no tags/versions built other than daily
     def branch_name = (params.VERSION == "daily") ? safe_branch_name : branch_version;
@@ -39,6 +46,7 @@ void main() {
         params.CIPARAM_OVERRIDE_DOCKER_TAG_BUILD,  // 'build tag'
         safe_branch_name,                   // 'branch' returns '<BRANCH>-latest'
     );
+    def deliverables_dir = "${checkout_dir}/test-results";
 
     currentBuild.description += (
         """
@@ -57,6 +65,7 @@ void main() {
         |cmk_version:.............. │${cmk_version}│
         |cmk_version_rc_aware:..... │${cmk_version_rc_aware}│
         |branch_version:........... │${branch_version}│
+        |deliverables_dir:......... │${deliverables_dir}│
         |docker_tag:............... │${docker_tag}│
         |===================================================
         """.stripMargin());
@@ -64,13 +73,20 @@ void main() {
     def relative_job_name = "${branch_base_folder}/builders/test-composition-single-f12less-k8s";
 
     /// avoid failures due to leftover artifacts from prior runs
-    sh("rm -rf ${checkout_dir}/test-results");
+    /// and create folder before entering containers to not delete the folder after leaving the container
+    stage("Prepare workspace") {
+        dir("${checkout_dir}") {
+            sh("""
+                rm -rf ${deliverables_dir}
+                mkdir ${deliverables_dir}
+            """);
+        }
+    }
 
     def test_stages = all_distros.collectEntries { distro -> [
         ("Test ${distro}") : {
             def stepName = "Test ${distro}";
             def run_condition = distro in selected_distros;
-            def build_instance = null;
 
             /// this makes sure the whole parallel thread is marked as skipped
             if (! run_condition) {
@@ -82,7 +98,7 @@ void main() {
                 condition: run_condition,
                 raiseOnError: false,
             ) {
-                build_instance = smart_build(
+                smart_build(
                     // see global-defaults.yml, needs to run in minimal container
                     use_upstream_build: true,
                     relative_job_name: relative_job_name,
@@ -99,19 +115,8 @@ void main() {
                         CIPARAM_BISECT_COMMENT: params.CIPARAM_BISECT_COMMENT,
                     ],
                     no_remove_others: true, // do not delete other files in the dest dir
-                    download: false,    // use copyArtifacts to avoid nested directories
-                );
-            }
-            smart_stage(
-                name: "Copy artifacts",
-                condition: run_condition && build_instance,
-                raiseOnError: false,
-            ) {
-                copyArtifacts(
-                    projectName: relative_job_name,
-                    selector: specific(build_instance.getId()), // buildNumber shall be a string
-                    target: "${checkout_dir}/test-results",
-                    fingerprintArtifacts: true
+                    download: true,
+                    dest: deliverables_dir,
                 );
             }
         }]
@@ -130,15 +135,15 @@ void main() {
                     fingerprint: true,
                 );
             }
-            xunit([Custom(
-                customXSL: "$JENKINS_HOME/userContent/xunit/JUnit/0.1/pytest-xunit.xsl",
-                deleteOutputFiles: true,
-                failIfNotNew: false,    // as they are copied from the single tests
-                pattern: "**/junit.xml",
-                skipNoTestFiles: false,
-                stopProcessingIfError: true
-            )]);
         }
+        xunit([Custom(
+            customXSL: "${checkout_dir}/buildscripts/scripts/schema/pytest-xunit.xsl",
+            deleteOutputFiles: true,
+            failIfNotNew: false,    // as they are copied from the single tests
+            pattern: "checkout/test-results/**/junit.xml",
+            skipNoTestFiles: false,
+            stopProcessingIfError: true
+        )]);
     }
 }
 

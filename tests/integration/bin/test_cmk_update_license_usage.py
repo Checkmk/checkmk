c59f407983df3cdd9ac2fee3295dcc6a113c86f8
Edward Toroshchin <eduard.toroshchin@checkmk.com>
2026-03-13 12:11:50 +0000

    fix: use correct imports in cmk-update-license-usage
    Update test_cmk_update_license_usage.py to verify that the binary
    updates the license usage samples and produces no error messages in the
    logs.
    
    Tested:
     * injected bin/cmk-update-license-usage
     * ran this test with REUSE=1
     * ran cmk-update-license-usage manually
    
    CMK-32415
    
    Change-Id: I62af7761f07332e18cb6c4c2749c6ca45983d4f8
    (cherry picked from commit b84040aac4be7c4c924a2e97a145bf64fd232953)

--- tests/integration/bin/test_cmk_update_license_usage.py
+++ tests/integration/bin/test_cmk_update_license_usage.py
@@ -4,15 +4,46 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
+import json
 import logging
+from datetime import datetime, timedelta
 
+from cmk.utils.licensing.export import RawLicenseUsageReport
+from cmk.utils.licensing.helper import rot47
 from tests.testlib.site import Site
 
 LOGGER = logging.getLogger(__name__)
 
 
+def _get_max_sample_time(history_json: RawLicenseUsageReport) -> datetime | None:
+    return max(
+        datetime.fromtimestamp(entry["sample_time"]) for entry in history_json.get("history", [])
+    )
+
+
 def test_cmk_update_license_usage(site: Site) -> None:
+    # Clear the sample file and next_run to force license usage sample generation.
+    site.delete_file(site.licensing_dir / "history.json")
+    site.delete_file(site.licensing_dir / "next_run")
+
+    # Run cmk-update-license-usage for the first time, ensure it succeeds.
     p = site.run(["cmk-update-license-usage"])
     LOGGER.info("STDOUT: %s", p.stdout)
     LOGGER.info("STDERR: %s", p.stderr)
     p.check_returncode()
+
+    # Check that the sample timestamp is plausible.
+    history_json = json.loads(rot47(site.read_file(site.licensing_dir / "history.json")))
+    latest_sample_first_run = _get_max_sample_time(history_json)
+    assert latest_sample_first_run is not None
+    assert datetime.now() - latest_sample_first_run < timedelta(days=2)
+
+    # Run cmk-update-license-usage for the second time, ensure it succeeds.
+    p = site.run(["cmk-update-license-usage", "--force"])
+    LOGGER.info("STDOUT: %s", p.stdout)
+    LOGGER.info("STDERR: %s", p.stderr)
+    p.check_returncode()
+
+    # Check that no error messages were written in the licensing log.
+    error_logs = site.read_file(site.logs_dir / "licensing.log")
+    assert not error_logs, f"Unexpected error messages in licensing.log:\n{error_logs}"

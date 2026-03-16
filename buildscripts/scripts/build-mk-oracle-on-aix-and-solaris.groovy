97222ed70ef9f4ec6e30b1052bb3d362e8009ace
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-03-16 11:49:10 +0100

    Move build-mk-oracle-on-aix-and-solaris to k8s
    ... as this only blocks an executor slot on classic build node and 90%
    of the time is waiting for another job to complete.
    It is a classic trigger job
    
    Requires https://review.lan.tribe29.com/c/checkmk_ci/+/138862
    
    CMK-31467
    
    Change-Id: I208a52d845d195e6a81d0775ed36b78296e31fa3

--- buildscripts/scripts/build-mk-oracle-on-aix-and-solaris.groovy
+++ buildscripts/scripts/build-mk-oracle-on-aix-and-solaris.groovy
@@ -15,49 +15,47 @@ void main() {
     def cmk_version_rc_aware = versioning.get_cmk_version(safe_branch_name, branch_version, version);
     def cmk_version = versioning.strip_rc_number_from_version(cmk_version_rc_aware);
 
-    stage("Set version") {
-        inside_container() {
+    inside_container_minimal(safe_branch_name: safe_branch_name) {
+        stage("Set version") {
             dir("${checkout_dir}") {
                 // mk-oracle is not edition specific, use "ultimatemt" as it is the superset
                 versioning.configure_checkout_folder("ultimatemt", cmk_version);
             }
         }
-    }
 
-    parallel(["aix", "solaris"].collectEntries { distro ->
-        [("Building mk-oracle on ${distro}"): {
-            smart_stage(
-                name: "Building mk-oracle on ${distro}",
-                condition: true,
-                raiseOnError: true,
-            ) {
-                inside_container() {
-                    dir("${checkout_dir}") {
-                        withCredentials([
-                            // We use the same SSH key as for the aix and solaris machine
-                            sshUserPrivateKey(
-                                credentialsId: "jenkins-aix-build-ssh-key",
-                                keyFileVariable: 'KEYFILE'
-                            ),
-                            file(
-                                credentialsId: "know_hosts_ssh_${distro}",
-                                variable: 'KNOWN_HOSTS_FILE'
-                            ),
-                            usernamePassword(
-                                credentialsId: 'oracle_test_db_user_password',
-                                usernameVariable: 'ORACLEDB_USER',
-                                passwordVariable: 'ORACLEDB_PASSWORD'
-                            ),
-                        ]) {
-                            sh("""
-                                checkout_dir=${checkout_dir} REMOTE_USER=jenkins packages/mk-oracle/ssh-run-ci ${distro} -bu
-                            """)
+        parallel(["aix", "solaris"].collectEntries { distro ->
+            [("Building mk-oracle on ${distro}"): {
+                smart_stage(
+                    name: "Building mk-oracle on ${distro}",
+                    condition: true,
+                    raiseOnError: true,
+                ) {
+                        dir("${checkout_dir}") {
+                            withCredentials([
+                                // We use the same SSH key as for the aix and solaris machine
+                                sshUserPrivateKey(
+                                    credentialsId: "jenkins-aix-build-ssh-key",
+                                    keyFileVariable: 'KEYFILE'
+                                ),
+                                file(
+                                    credentialsId: "know_hosts_ssh_${distro}",
+                                    variable: 'KNOWN_HOSTS_FILE'
+                                ),
+                                usernamePassword(
+                                    credentialsId: 'oracle_test_db_user_password',
+                                    usernameVariable: 'ORACLEDB_USER',
+                                    passwordVariable: 'ORACLEDB_PASSWORD'
+                                ),
+                            ]) {
+                                sh("""
+                                    checkout_dir=${checkout_dir} REMOTE_USER=jenkins packages/mk-oracle/ssh-run-ci ${distro} -bu
+                                """)
+                            }
                         }
-                    }
                 }
-            }
-        }]
-    })
+            }]
+        })
+    }
 
     stage("Archive artifacts") {
         dir("${checkout_dir}/packages/mk-oracle") {

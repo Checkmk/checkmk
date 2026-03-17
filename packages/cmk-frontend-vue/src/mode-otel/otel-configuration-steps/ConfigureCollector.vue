4358d1c0f38850b07a18a76b1fa4460210aadde3
ramadan khalifa <ramadan.khalifa@checkmk.com>
2026-03-17 12:04:20 +0100

    OTel: QuickSetup (Step 2 - OpenTelemetry) - Create the GRPC and HTTP sections
    CMK-29616
    
    Change-Id: I5a0d59f5b09f01bb0a89b2f55ee88804497c9e7f

--- packages/cmk-frontend-vue/src/mode-otel/otel-configuration-steps/ConfigureCollector.vue
+++ packages/cmk-frontend-vue/src/mode-otel/otel-configuration-steps/ConfigureCollector.vue
@@ -4,11 +4,35 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import usei18n from '@/lib/i18n'
 
+import CmkTabs, { CmkTab, CmkTabContent } from '@/components/CmkTabs'
+import CmkParagraph from '@/components/typography/CmkParagraph.vue'
+
 const { _t } = usei18n()
+
+const activeTab = ref('grpc')
 </script>
 
 <template>
-  {{ _t('Select the site where the OpenTelemetry collector will run.') }}
+  <CmkTabs v-model="activeTab">
+    <template #tabs>
+      <CmkTab id="grpc">{{ _t('GRPC') }}</CmkTab>
+      <CmkTab id="http">{{ _t('HTTP') }}</CmkTab>
+    </template>
+    <template #tab-contents>
+      <CmkTabContent id="grpc">
+        <CmkParagraph>{{
+          _t('Configure a GRPC-based OTLP receiver that will collect OpenTelemetry data.')
+        }}</CmkParagraph>
+      </CmkTabContent>
+      <CmkTabContent id="http">
+        <CmkParagraph>{{
+          _t('Configure an HTTP-based OTLP receiver that will collect OpenTelemetry data.')
+        }}</CmkParagraph>
+      </CmkTabContent>
+    </template>
+  </CmkTabs>
 </template>

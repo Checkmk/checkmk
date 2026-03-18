37a3a3b471643bc6ad914e22a4af2fed49dac71f
Sven Panne <sven.panne@checkmk.com>
2026-03-18 15:45:48 +0100

    pyupgraded code to 3.7+
    Change-Id: I5d976ecd20f72354faccffc18b3468e9ea237787

--- cmk/utils/diagnostics.py
+++ cmk/utils/diagnostics.py
@@ -373,7 +373,7 @@ REDACT_PATTERNS: list[RedactPattern] = [
         replace_regex=re.compile(
             r"(\('authPriv', '[^']+', '[^']+', [\"']).*?([\"'], '[^']+', [\"']).*?([\"']\))"
         ),
-        replacement=r"\1{}\2{}\3".format(REDACT_STRING, REDACT_STRING),
+        replacement=rf"\1{REDACT_STRING}\2{REDACT_STRING}\3",
     ),
     # This one is for the fact that the SNMP community is always stored as cleartext
     # Can be removed, once the password store can be used, here

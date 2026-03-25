d321bc26f9dc57f27655fb2fe1f5daa741a921ff
Logan Connolly <logan.connolly@checkmk.com>
2026-03-25 13:34:19 +0100

    19551 cisco_meraki: fix agent cache implementation
    By using a sentinel object for the separation between args and kwargs in
    the cache implementation, this led to the cache always being invalidated
    between agent runs.
    
    CMK-32772
    
    Change-Id: I9a080fbfed584d7f8bc3b8ecdf4af1b3d9928dfe

--- cmk/plugins/cisco_meraki/lib/cache.py
+++ cmk/plugins/cisco_meraki/lib/cache.py
@@ -13,8 +13,6 @@ from cmk.server_side_programs.v1_unstable import Storage
 
 # A namespace to match a string to a uuid hash.
 _HASH_NAMESPACE: Final = uuid.UUID("be8bc5b5-a401-4acc-b8db-a875cca98c79")
-# A sentinel for separating args from kwargs.
-_KWARG_MARK: Final = object()
 
 
 def cache_ttl[**P, R](store: Storage, *, ttl: float) -> Callable[[Callable[P, R]], Callable[P, R]]:
@@ -36,7 +34,7 @@ def cache_ttl[**P, R](store: Storage, *, ttl: float) -> Callable[[Callable[P, R]
                 raise ValueError(f"Unhashable kwarg values: {kwargs}")
 
             # generate a unique key hash based on the passed arguments.
-            key = _hash(str(args + (_KWARG_MARK,) + tuple(sorted(kwargs.items()))))
+            key = _hash(f"args={args}, kwargs={tuple(sorted(kwargs.items()))}")
 
             if (data := store.read(key, None)) is not None:
                 raw_cache = json.loads(data)

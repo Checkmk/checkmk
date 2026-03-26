81eec1cd220b7156a7a8da3444af0558a05e15d9
ramadan khalifa <ramadan.khalifa@checkmk.com>
2026-03-26 16:40:49 +0100

    CMK-32515: Legacy check migration: stulz_temp I
    Convert stulz_temp from legacy check API to agent-based API v2 in place.
    
    CMK-32515
    
    Change-Id: I0c777606b8a864aaebbe73de458fc2b297747cb5

--- cmk/legacy_checks/stulz_temp.py
+++ cmk/legacy_checks/stulz_temp.py
@@ -3,18 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
-# mypy: disable-error-code="var-annotated"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree
-from cmk.legacy_includes.temperature import check_temperature
-from cmk.plugins.stulz.lib import DETECT_STULZ
-
-check_info = {}
-
 # .1.3.6.1.4.1.29462.10.2.1.1.1.1.1.1.1.1170.1.1.1 220 --> Stulz-WIB8000-MIB::unitAirTemperature.1.1.1
 # .1.3.6.1.4.1.29462.10.2.1.1.1.1.1.1.1.1170.1.2.1 216 --> Stulz-WIB8000-MIB::unitAirTemperature.1.2.1
 # .1.3.6.1.4.1.29462.10.2.1.1.1.1.1.1.1.1175.1.1.1 220 --> Stulz-WIB8000-MIB::unitSetpointAirTratureCorrected.1.1.1
@@ -36,51 +24,72 @@ check_info = {}
 # .1.3.6.1.4.1.29462.10.2.1.1.1.1.3.1.1.1208.1.2.1 16 --> Stulz-WIB8000-MIB::currentRaisedFloorPressure.1.2.1
 
 
-def parse_stulz_temp(string_table):
-    map_types = {
-        "1170": "unit air",
-        "1192": "unit return air",
-        "1193": "unit supply air",
-        "1196": "unit outside air",
-        "1243": "unit supply 3",
-        "1244": "unit return air 2",
-        "1245": "unit return air 3",
-        "1246": "unit return air temrn",
-        "1247": "unit return air temly",
-        "1248": "unit supply air 2",
-        "10210": "condensor",
-        "10211": "supply 1",
-        "10212": "supply 2",
-        "10264": "FCB room air",
-        "10266": "supply air comfort unit 1",
-        "10267": "supply air comfort unit 2",
-        "10268": "FCB outside air",
-    }
+from collections.abc import Mapping
 
-    parsed = {}
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    OIDEnd,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
+from cmk.plugins.stulz.lib import DETECT_STULZ
+
+Section = Mapping[str, float]
+
+_MAP_TYPES = {
+    "1170": "unit air",
+    "1192": "unit return air",
+    "1193": "unit supply air",
+    "1196": "unit outside air",
+    "1243": "unit supply 3",
+    "1244": "unit return air 2",
+    "1245": "unit return air 3",
+    "1246": "unit return air temrn",
+    "1247": "unit return air temly",
+    "1248": "unit supply air 2",
+    "10210": "condensor",
+    "10211": "supply 1",
+    "10212": "supply 2",
+    "10264": "FCB room air",
+    "10266": "supply air comfort unit 1",
+    "10267": "supply air comfort unit 2",
+    "10268": "FCB outside air",
+}
+
+
+def parse_stulz_temp(string_table: StringTable) -> Section:
+    parsed: dict[str, float] = {}
     for oidend, reading_str in string_table:
         oids = oidend.split(".")
         temp_ty = oids[0]
         index = oids[2]
-        if temp_ty in map_types and reading_str != "999":
-            itemname = f"{map_types[temp_ty]}-{index}"
+        if temp_ty in _MAP_TYPES and reading_str != "999":
+            itemname = f"{_MAP_TYPES[temp_ty]}-{index}"
             parsed.setdefault(itemname, float(reading_str) / 10)
-
     return parsed
 
 
-def discover_stulz_temp(parsed):
-    for item in parsed:
-        yield item, {}
+def discover_stulz_temp(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
 
 
-def check_stulz_temp(item, params, parsed):
-    if item in parsed:
-        return check_temperature(parsed[item], params, "stulz_temp_%s" % item)
-    return None
+def check_stulz_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
+    if item in section:
+        yield from check_temperature(
+            section[item],
+            params,
+            unique_name=f"stulz_temp_{item}",
+            value_store=get_value_store(),
+        )
 
 
-check_info["stulz_temp"] = LegacyCheckDefinition(
+snmp_section_stulz_temp = SimpleSNMPSection(
     name="stulz_temp",
     detect=DETECT_STULZ,
     fetch=SNMPTree(
@@ -88,6 +97,10 @@ check_info["stulz_temp"] = LegacyCheckDefinition(
         oids=[OIDEnd(), "1"],
     ),
     parse_function=parse_stulz_temp,
+)
+
+check_plugin_stulz_temp = CheckPlugin(
+    name="stulz_temp",
     service_name="Temperature %s",
     discovery_function=discover_stulz_temp,
     check_function=check_stulz_temp,

133ffa477cbae25911cbb1de8d3b86335ae052a5
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-26 08:27:00 +0100

    bakery: Migrate lvm rulespec to v1 API + modernize bakelet
    Change-Id: Ic669da1003da170a619d959fb2a96f9edf0fbbda

--- cmk/gui/plugins/legacy_bakery_rulesets/lvm.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/lvm.py
@@ -3,28 +3,69 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import DropdownChoice
-from cmk.utils.rulesets.definition import RuleGroup
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_lvm() -> DropdownChoice[bool]:
-    return DropdownChoice(
-        title=_("LVM, Logical Volume Groups (Linux)"),
-        choices=[
-            (True, _("Deploy LVM plug-in")),
-            (None, _("Do not deploy LVM plug-in")),
-        ],
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict):
+        return value
+    return {"deployment": ("sync" if value else "do_not_deploy", None)}
+
+
+def _valuespec_agent_config_lvm() -> Dictionary:
+    return Dictionary(
+        help_text=Help("Deploy the agent plug-in <tt>lvm</tt> on your target system."),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name=RuleGroup.AgentConfig("lvm"),
-        valuespec=_valuespec_agent_config_lvm,
-    )
+rule_spec_lvm = AgentConfig(
+    title=Title("LVM, Logical Volume Groups (Linux)"),
+    name="lvm",
+    topic=Topic.STORAGE,
+    parameter_form=_valuespec_agent_config_lvm,
 )

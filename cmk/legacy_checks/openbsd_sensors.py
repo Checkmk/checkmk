55eed395265d8323ac413d38fcc6bd62cb3d6e1d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-26 08:48:57 +0100

    Legacy check migration: openbsd_sensors I
    Change-Id: Idb2129c7f4f4f7b3befa8720274e23d13500b55f

--- cmk/legacy_checks/openbsd_sensors.py
+++ cmk/legacy_checks/openbsd_sensors.py
@@ -3,134 +3,64 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    exists,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.elphase import check_elphase, ElPhase, ReadingWithState
+from cmk.plugins.lib.fan import check_fan
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
+
+_OPENBSD_MAP_STATE: Mapping[str, State] = {
+    "0": State.UNKNOWN,
+    "1": State.OK,
+    "2": State.WARN,
+    "3": State.CRIT,
+}
+
+_OPENBSD_MAP_TYPE: Mapping[str, str] = {
+    "0": "temp",
+    "1": "fan",
+    "2": "voltage",
+    "9": "indicator",
+    "13": "drive",
+    "21": "powersupply",
+}
+
+SensorEntry = Mapping[str, Any]
+Section = Mapping[str, SensorEntry]
 
 
-# mypy: disable-error-code="var-annotated"
+def parse_openbsd_sensors(string_table: StringTable) -> Section:
+    parsed: dict[str, dict[str, Any]] = {}
+    used_descriptions: set[str] = set()
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import exists, SNMPTree
-from cmk.legacy_includes.elphase import check_elphase
-from cmk.legacy_includes.fan import check_fan
-from cmk.legacy_includes.temperature import check_temperature
-
-check_info = {}
-
-#   .--example output------------------------------------------------------.
-#   |                                               _                      |
-#   |                 _____  ____ _ _ __ ___  _ __ | | ___                 |
-#   |                / _ \ \/ / _` | '_ ` _ \| '_ \| |/ _ \                |
-#   |               |  __/>  < (_| | | | | | | |_) | |  __/                |
-#   |                \___/_/\_\__,_|_| |_| |_| .__/|_|\___|                |
-#   |                                        |_|                           |
-#   |                               _               _                      |
-#   |                    ___  _   _| |_ _ __  _   _| |_                    |
-#   |                   / _ \| | | | __| '_ \| | | | __|                   |
-#   |                  | (_) | |_| | |_| |_) | |_| | |_                    |
-#   |                   \___/ \__,_|\__| .__/ \__,_|\__|                   |
-#   |                                  |_|                                 |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
-
-# .1.3.6.1.4.1.30155.2.1.2.1.2.2  sd0
-# .1.3.6.1.4.1.30155.2.1.2.1.2.18 temp0
-# .1.3.6.1.4.1.30155.2.1.2.1.2.20 temp2
-# .1.3.6.1.4.1.30155.2.1.2.1.2.27 fan3
-# .1.3.6.1.4.1.30155.2.1.2.1.2.31 fan7
-# .1.3.6.1.4.1.30155.2.1.2.1.2.37 Chassis Intru
-# .1.3.6.1.4.1.30155.2.1.2.1.2.38 PS1 Status
-# .1.3.6.1.4.1.30155.2.1.2.1.2.40 VTT
-# .1.3.6.1.4.1.30155.2.1.2.1.2.47 volt9
-
-# .1.3.6.1.4.1.30155.2.1.2.1.3.2  13
-# .1.3.6.1.4.1.30155.2.1.2.1.3.18 0
-# .1.3.6.1.4.1.30155.2.1.2.1.3.20 0
-# .1.3.6.1.4.1.30155.2.1.2.1.3.27 1
-# .1.3.6.1.4.1.30155.2.1.2.1.3.31 1
-# .1.3.6.1.4.1.30155.2.1.2.1.3.37 9
-# .1.3.6.1.4.1.30155.2.1.2.1.3.38 21
-# .1.3.6.1.4.1.30155.2.1.2.1.3.40 2
-# .1.3.6.1.4.1.30155.2.1.2.1.3.47 2
-
-# .1.3.6.1.4.1.30155.2.1.2.1.5.2  online
-# .1.3.6.1.4.1.30155.2.1.2.1.5.18 -273.15
-# .1.3.6.1.4.1.30155.2.1.2.1.5.20 56.00
-# .1.3.6.1.4.1.30155.2.1.2.1.5.27 4179
-# .1.3.6.1.4.1.30155.2.1.2.1.5.31 329
-# .1.3.6.1.4.1.30155.2.1.2.1.5.37 off
-# .1.3.6.1.4.1.30155.2.1.2.1.5.38 present
-# .1.3.6.1.4.1.30155.2.1.2.1.5.40 1.15
-# .1.3.6.1.4.1.30155.2.1.2.1.5.47 0.00
-
-# .1.3.6.1.4.1.30155.2.1.2.1.6.2
-# .1.3.6.1.4.1.30155.2.1.2.1.6.18 degC
-# .1.3.6.1.4.1.30155.2.1.2.1.6.20 degC
-# .1.3.6.1.4.1.30155.2.1.2.1.6.27 RPM
-# .1.3.6.1.4.1.30155.2.1.2.1.6.31 RPM
-# .1.3.6.1.4.1.30155.2.1.2.1.6.37
-# .1.3.6.1.4.1.30155.2.1.2.1.6.38
-# .1.3.6.1.4.1.30155.2.1.2.1.6.40 V DC
-# .1.3.6.1.4.1.30155.2.1.2.1.6.47 V DC
-
-# .1.3.6.1.4.1.30155.2.1.2.1.7.2  1
-# .1.3.6.1.4.1.30155.2.1.2.1.7.18 0
-# .1.3.6.1.4.1.30155.2.1.2.1.7.20 0
-# .1.3.6.1.4.1.30155.2.1.2.1.7.27 0
-# .1.3.6.1.4.1.30155.2.1.2.1.7.31 0
-# .1.3.6.1.4.1.30155.2.1.2.1.7.37 1
-# .1.3.6.1.4.1.30155.2.1.2.1.7.38 1
-# .1.3.6.1.4.1.30155.2.1.2.1.7.40 0
-# .1.3.6.1.4.1.30155.2.1.2.1.7.47 0
-
-# .
-#   .--parse---------------------------------------------------------------.
-#   |                                                                      |
-#   |                      _ __   __ _ _ __ ___  ___                       |
-#   |                     | '_ \ / _` | '__/ __|/ _ \                      |
-#   |                     | |_) | (_| | |  \__ \  __/                      |
-#   |                     | .__/ \__,_|_|  |___/\___|                      |
-#   |                     |_|                                              |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
-
-
-def parse_openbsd_sensors(string_table):
-    parsed = {}
-    used_descriptions = set()
-
-    def get_item_name(name):
+    def get_item_name(name: str) -> str:
         idx = 0
         new_name = name
         while True:
             if new_name in used_descriptions:
-                new_name = "%s/%d" % (name, idx)
+                new_name = f"{name}/{idx}"
                 idx += 1
             else:
                 used_descriptions.add(new_name)
                 break
         return new_name
 
-    openbsd_map_state = {
-        "0": 3,
-        "1": 0,
-        "2": 1,
-        "3": 2,
-    }
-
-    openbsd_map_type = {
-        "0": "temp",
-        "1": "fan",
-        "2": "voltage",
-        "9": "indicator",
-        "13": "drive",
-        "21": "powersupply",
-    }
-
     for descr, sensortype, value, unit, state in string_table:
-        if sensortype not in openbsd_map_type:
+        if sensortype not in _OPENBSD_MAP_TYPE:
             continue
         if (sensortype == "0" and value == "-273.15") or (
             sensortype in ["1", "2"] and float(value) == 0
@@ -138,65 +68,42 @@ def parse_openbsd_sensors(string_table):
             continue
 
         try:
-            value_converted = float(value)
+            value_converted: float | str = float(value)
         except ValueError:
             value_converted = value
 
         item_name = get_item_name(descr)
         parsed[item_name] = {
-            "state": openbsd_map_state[state],
+            "state": _OPENBSD_MAP_STATE[state],
             "value": value_converted,
             "unit": unit,
-            "type": openbsd_map_type[sensortype],
+            "type": _OPENBSD_MAP_TYPE[sensortype],
         }
     return parsed
 
 
-# .
-#   .--inventory-----------------------------------------------------------.
-#   |             _                      _                                 |
-#   |            (_)_ ____   _____ _ __ | |_ ___  _ __ _   _               |
-#   |            | | '_ \ \ / / _ \ '_ \| __/ _ \| '__| | | |              |
-#   |            | | | | \ V /  __/ | | | || (_) | |  | |_| |              |
-#   |            |_|_| |_|\_/ \___|_| |_|\__\___/|_|   \__, |              |
-#   |                                                  |___/               |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
-
-
-def inventory_openbsd_sensors(parsed, sensortype):
-    inventory = []
-    for key, values in parsed.items():
+def _discover_openbsd_sensors(section: Section, sensortype: str) -> DiscoveryResult:
+    for key, values in section.items():
         if values["type"] == sensortype:
-            inventory.append((key, {}))
-    return inventory
+            yield Service(item=key)
 
 
-# .
-#   .--temperature---------------------------------------------------------.
-#   |      _                                      _                        |
-#   |     | |_ ___ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
-#   |     | __/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _ \      |
-#   |     | ||  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | |  __/      |
-#   |      \__\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \___|      |
-#   |                       |_|                                            |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
+def discover_openbsd_sensors(section: Section) -> DiscoveryResult:
+    yield from _discover_openbsd_sensors(section, "temp")
 
 
-def check_openbsd_sensors(item, params, parsed):
-    if not (data := parsed.get(item)):
+def check_openbsd_sensors(item: str, params: TempParamType, section: Section) -> CheckResult:
+    if not (data := section.get(item)):
         return
-    yield check_temperature(data["value"], params, "openbsd_sensors_%s" % item)
+    yield from check_temperature(
+        data["value"],
+        params,
+        unique_name=f"openbsd_sensors_{item}",
+        value_store=get_value_store(),
+    )
 
 
-def discover_openbsd_sensors(parsed):
-    return inventory_openbsd_sensors(parsed, "temp")
-
-
-check_info["openbsd_sensors"] = LegacyCheckDefinition(
+snmp_section_openbsd_sensors = SimpleSNMPSection(
     name="openbsd_sensors",
     detect=exists(".1.3.6.1.4.1.30155.2.1.1.0"),
     fetch=SNMPTree(
@@ -204,36 +111,32 @@ check_info["openbsd_sensors"] = LegacyCheckDefinition(
         oids=["2", "3", "5", "6", "7"],
     ),
     parse_function=parse_openbsd_sensors,
+)
+
+
+check_plugin_openbsd_sensors = CheckPlugin(
+    name="openbsd_sensors",
     service_name="Temperature %s",
     discovery_function=discover_openbsd_sensors,
     check_function=check_openbsd_sensors,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )
 
-# .
-#   .--fan-----------------------------------------------------------------.
-#   |                            __                                        |
-#   |                           / _| __ _ _ __                             |
-#   |                          | |_ / _` | '_ \                            |
-#   |                          |  _| (_| | | | |                           |
-#   |                          |_|  \__,_|_| |_|                           |
-#   |                                                                      |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
+
+def discover_openbsd_sensors_fan(section: Section) -> DiscoveryResult:
+    yield from _discover_openbsd_sensors(section, "fan")
 
 
-def check_openbsd_sensors_fan(item, params, parsed):
-    if not (data := parsed.get(item)):
+def check_openbsd_sensors_fan(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if not (data := section.get(item)):
         return
-    yield check_fan(data["value"], params)
+    yield from check_fan(data["value"], params)
 
 
-def discover_openbsd_sensors_fan(parsed):
-    return inventory_openbsd_sensors(parsed, "fan")
-
-
-check_info["openbsd_sensors.fan"] = LegacyCheckDefinition(
+check_plugin_openbsd_sensors_fan = CheckPlugin(
     name="openbsd_sensors_fan",
     service_name="Fan %s",
     sections=["openbsd_sensors"],
@@ -246,130 +149,83 @@ check_info["openbsd_sensors.fan"] = LegacyCheckDefinition(
     },
 )
 
-# .
-#   .--voltage-------------------------------------------------------------.
-#   |                             _ _                                      |
-#   |                 __   _____ | | |_ __ _  __ _  ___                    |
-#   |                 \ \ / / _ \| | __/ _` |/ _` |/ _ \                   |
-#   |                  \ V / (_) | | || (_| | (_| |  __/                   |
-#   |                   \_/ \___/|_|\__\__,_|\__, |\___|                   |
-#   |                                        |___/                         |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
+
+def discover_openbsd_sensors_voltage(section: Section) -> DiscoveryResult:
+    yield from _discover_openbsd_sensors(section, "voltage")
 
 
-def check_openbsd_sensors_voltage(item, params, parsed):
-    if not (data := parsed.get(item)):
+def check_openbsd_sensors_voltage(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if not (data := section.get(item)):
         return
-    item_elphase = {}
-    parsed_elphase = {}
-    item_elphase["voltage"] = data["value"]
-    parsed_elphase[item] = item_elphase
-
-    yield from check_elphase(item, params, parsed_elphase)
+    elphase = ElPhase(voltage=ReadingWithState(value=data["value"]))
+    yield from check_elphase(params, elphase)
 
 
-def discover_openbsd_sensors_voltage(parsed):
-    return inventory_openbsd_sensors(parsed, "voltage")
-
-
-check_info["openbsd_sensors.voltage"] = LegacyCheckDefinition(
+check_plugin_openbsd_sensors_voltage = CheckPlugin(
     name="openbsd_sensors_voltage",
     service_name="Voltage Type %s",
     sections=["openbsd_sensors"],
     discovery_function=discover_openbsd_sensors_voltage,
     check_function=check_openbsd_sensors_voltage,
     check_ruleset_name="el_inphase",
+    check_default_parameters={},
 )
-# .
-#   .--powersupply---------------------------------------------------------.
-#   |                                                        _             |
-#   |     _ __   _____      _____ _ __ ___ _   _ _ __  _ __ | |_   _       |
-#   |    | '_ \ / _ \ \ /\ / / _ \ '__/ __| | | | '_ \| '_ \| | | | |      |
-#   |    | |_) | (_) \ V  V /  __/ |  \__ \ |_| | |_) | |_) | | |_| |      |
-#   |    | .__/ \___/ \_/\_/ \___|_|  |___/\__,_| .__/| .__/|_|\__, |      |
-#   |    |_|                                    |_|   |_|      |___/       |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
 
 
-def check_openbsd_sensors_powersupply(item, _no_params, parsed):
-    if not (data := parsed.get(item)):
+def discover_openbsd_sensors_powersupply(section: Section) -> DiscoveryResult:
+    yield from _discover_openbsd_sensors(section, "powersupply")
+
+
+def check_openbsd_sensors_powersupply(item: str, section: Section) -> CheckResult:
+    if not (data := section.get(item)):
         return
-    yield data["state"], "Status: %s" % data["value"]
+    yield Result(state=data["state"], summary=f"Status: {data['value']}")
 
 
-def discover_openbsd_sensors_powersupply(parsed):
-    return inventory_openbsd_sensors(parsed, "powersupply")
-
-
-check_info["openbsd_sensors.powersupply"] = LegacyCheckDefinition(
+check_plugin_openbsd_sensors_powersupply = CheckPlugin(
     name="openbsd_sensors_powersupply",
     service_name="Powersupply %s",
     sections=["openbsd_sensors"],
     discovery_function=discover_openbsd_sensors_powersupply,
     check_function=check_openbsd_sensors_powersupply,
 )
-# .
-#   .--indicator-----------------------------------------------------------.
-#   |               _           _ _           _                            |
-#   |              (_)_ __   __| (_) ___ __ _| |_ ___  _ __                |
-#   |              | | '_ \ / _` | |/ __/ _` | __/ _ \| '__|               |
-#   |              | | | | | (_| | | (_| (_| | || (_) | |                  |
-#   |              |_|_| |_|\__,_|_|\___\__,_|\__\___/|_|                  |
-#   |                                                                      |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
 
 
-def check_openbsd_sensors_indicator(item, params, parsed):
-    if not (data := parsed.get(item)):
+def discover_openbsd_sensors_indicator(section: Section) -> DiscoveryResult:
+    yield from _discover_openbsd_sensors(section, "indicator")
+
+
+def check_openbsd_sensors_indicator(item: str, section: Section) -> CheckResult:
+    if not (data := section.get(item)):
         return
-    yield data["state"], "Status: %s" % data["value"]
+    yield Result(state=data["state"], summary=f"Status: {data['value']}")
 
 
-def discover_openbsd_sensors_indicator(parsed):
-    return inventory_openbsd_sensors(parsed, "indicator")
-
-
-check_info["openbsd_sensors.indicator"] = LegacyCheckDefinition(
+check_plugin_openbsd_sensors_indicator = CheckPlugin(
     name="openbsd_sensors_indicator",
     service_name="Indicator %s",
     sections=["openbsd_sensors"],
     discovery_function=discover_openbsd_sensors_indicator,
     check_function=check_openbsd_sensors_indicator,
 )
-# .
-#   .--drive---------------------------------------------------------------.
-#   |                           _      _                                   |
-#   |                        __| |_ __(_)_   _____                         |
-#   |                       / _` | '__| \ \ / / _ \                        |
-#   |                      | (_| | |  | |\ V /  __/                        |
-#   |                       \__,_|_|  |_| \_/ \___|                        |
-#   |                                                                      |
-#   +----------------------------------------------------------------------+
-#   |                                                                      |
-#   '----------------------------------------------------------------------'
 
 
-def check_openbsd_sensors_drive(item, params, parsed):
-    if not (data := parsed.get(item)):
+def discover_openbsd_sensors_drive(section: Section) -> DiscoveryResult:
+    yield from _discover_openbsd_sensors(section, "drive")
+
+
+def check_openbsd_sensors_drive(item: str, section: Section) -> CheckResult:
+    if not (data := section.get(item)):
         return
-    yield data["state"], "Status: %s" % data["value"]
+    yield Result(state=data["state"], summary=f"Status: {data['value']}")
 
 
-def discover_openbsd_sensors_drive(parsed):
-    return inventory_openbsd_sensors(parsed, "drive")
-
-
-check_info["openbsd_sensors.drive"] = LegacyCheckDefinition(
+check_plugin_openbsd_sensors_drive = CheckPlugin(
     name="openbsd_sensors_drive",
     service_name="Drive %s",
     sections=["openbsd_sensors"],
     discovery_function=discover_openbsd_sensors_drive,
     check_function=check_openbsd_sensors_drive,
 )
-# .

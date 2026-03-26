62869126207add0305a79ff96a25f3ced980f553
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-26 08:21:51 +0100

    Legacy check migration: rms200_temp I
    Change-Id: Ic1674e225265a68253eebc7a14de39c8cce455e8

--- cmk/legacy_checks/rms200_temp.py
+++ cmk/legacy_checks/rms200_temp.py
@@ -3,46 +3,60 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    equals,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import equals, SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
-
-check_info = {}
-
-
-def discover_rms200_temp(info):
-    for line in info:
+def discover_rms200_temp(section: StringTable) -> DiscoveryResult:
+    for line in section:
         if line[2] != "-27300":
-            yield (line[0], {})
+            yield Service(item=line[0])
         # otherwise no sensor is connected
 
 
-def check_rms200_temp(item, params, info):
-    for line in info:
+def check_rms200_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for line in section:
         if line[0] == item:
-            status, infotext, perfdata = check_temperature(
-                float(line[2]) / 100, params, "rms200_temp_%s" % item
+            yield from check_temperature(
+                float(line[2]) / 100,
+                params,
+                unique_name=f"rms200_temp_{item}",
+                value_store=get_value_store(),
             )
-            infotext += " (%s)" % line[1]  # Name from SNMP data
-            return status, infotext, perfdata
-    return None
+            yield Result(state=State.OK, summary=f"({line[1]})")
+            return
 
 
 def parse_rms200_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["rms200_temp"] = LegacyCheckDefinition(
+snmp_section_rms200_temp = SimpleSNMPSection(
     name="rms200_temp",
-    parse_function=parse_rms200_temp,
     detect=equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.1909.13"),
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.1909.13.1.1.1",
         oids=["1", "2", "5"],
     ),
+    parse_function=parse_rms200_temp,
+)
+
+
+check_plugin_rms200_temp = CheckPlugin(
+    name="rms200_temp",
     service_name="Temperature %s ",
     discovery_function=discover_rms200_temp,
     check_function=check_rms200_temp,

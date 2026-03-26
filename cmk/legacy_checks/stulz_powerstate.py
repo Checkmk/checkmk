9f83a76800d1944a6910aacc5f39a3eff2e4b34e
ramadan khalifa <ramadan.khalifa@checkmk.com>
2026-03-26 16:36:43 +0100

    CMK-32515: Legacy check migration: stulz_powerstate I
    Convert stulz_powerstate from legacy check API to agent-based API v2 in place.
    
    CMK-32515
    
    Change-Id: Ieb482cf32944c923fcb62712e83887128dc1ba87

--- cmk/legacy_checks/stulz_powerstate.py
+++ cmk/legacy_checks/stulz_powerstate.py
@@ -4,24 +4,32 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from collections.abc import Mapping
-from typing import Any
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Metric,
+    OIDEnd,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.stulz.lib import DETECT_STULZ
 
-check_info = {}
+
+def parse_stulz_powerstate(string_table: StringTable) -> StringTable:
+    return string_table
 
 
-def discover_stulz_powerstate(info: StringTable) -> list[tuple[str, None]]:
-    return [(x[0], None) for x in info]
+def discover_stulz_powerstate(section: StringTable) -> DiscoveryResult:
+    yield from (Service(item=line[0]) for line in section)
 
 
-def check_stulz_powerstate(
-    item: str, _no_params: Mapping[str, Any], info: StringTable
-) -> tuple[int, str] | tuple[int, str, list[tuple[str, float]]]:
-    for line in info:
+def check_stulz_powerstate(item: str, section: StringTable) -> CheckResult:
+    for line in section:
         if line[0] == item:
             if line[1] != "1":
                 message = "Device powered off"
@@ -30,22 +38,23 @@ def check_stulz_powerstate(
                 message = "Device powered on"
                 power_state = 6
 
-            return 0, message, [("state", power_state)]
-    return 3, "No information found about the device"
+            yield Result(state=State.OK, summary=message)
+            yield Metric("state", power_state)
+            return
 
 
-def parse_stulz_powerstate(string_table: StringTable) -> StringTable:
-    return string_table
-
-
-check_info["stulz_powerstate"] = LegacyCheckDefinition(
+snmp_section_stulz_powerstate = SimpleSNMPSection(
     name="stulz_powerstate",
-    parse_function=parse_stulz_powerstate,
     detect=DETECT_STULZ,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.29462.10.2.1.4.1.1.1.1013",
         oids=[OIDEnd(), "1"],
     ),
+    parse_function=parse_stulz_powerstate,
+)
+
+check_plugin_stulz_powerstate = CheckPlugin(
+    name="stulz_powerstate",
     service_name="State %s ",
     discovery_function=discover_stulz_powerstate,
     check_function=check_stulz_powerstate,

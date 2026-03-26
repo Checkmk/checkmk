96ca34043904097b2a34bc7ff54a0677eec98264
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-26 09:09:36 +0100

    Legacy check migration: papouch_th2e_sensors I
    Change-Id: I6a998c435b7497a83174d36f874ec80d2e11b000

--- cmk/legacy_checks/papouch_th2e_sensors.py
+++ cmk/legacy_checks/papouch_th2e_sensors.py
@@ -3,18 +3,27 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-
-# mypy: disable-error-code="var-annotated"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import all_of, contains, OIDEnd, SNMPTree, startswith
-from cmk.legacy_includes.humidity import check_humidity
-from cmk.legacy_includes.temperature import check_temperature
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    all_of,
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    get_value_store,
+    OIDEnd,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.humidity import check_humidity
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 # .1.3.6.1.4.1.18248.20.1.2.1.1.1.1 0
 # .1.3.6.1.4.1.18248.20.1.2.1.1.1.2 0
@@ -26,39 +35,42 @@ check_info = {}
 # .1.3.6.1.4.1.18248.20.1.2.1.1.3.2 3
 # .1.3.6.1.4.1.18248.20.1.2.1.1.3.3 0
 
+_MAP_SENSOR_TYPE = {
+    "1": "temp",
+    "2": "humidity",
+    "3": "dewpoint",
+}
 
-def parse_papouch_th2e_sensors(string_table):
-    map_sensor_type = {
-        "1": "temp",
-        "2": "humidity",
-        "3": "dewpoint",
-    }
+_MAP_UNITS = {
+    "0": "c",
+    "1": "f",
+    "2": "k",
+    "3": "percent",
+}
 
-    map_units = {
-        "0": "c",
-        "1": "f",
-        "2": "k",
-        "3": "percent",
-    }
+_MAP_STATES: Mapping[str, tuple[int, str]] = {
+    "0": (0, "OK"),
+    "1": (3, "not available"),
+    "2": (1, "over-flow"),
+    "3": (1, "under-flow"),
+    "4": (2, "error"),
+}
 
-    map_states = {
-        "0": (0, "OK"),
-        "1": (3, "not available"),
-        "2": (1, "over-flow"),
-        "3": (1, "under-flow"),
-        "4": (2, "error"),
-    }
+type _SensorData = tuple[tuple[int, str], float, str]
+type Section = Mapping[str, Mapping[str, _SensorData]]
 
-    parsed = {}
+
+def parse_papouch_th2e_sensors(string_table: StringTable) -> Section | None:
+    parsed: dict[str, dict[str, _SensorData]] = {}
     for oidend, state, reading_str, unit in string_table:
         if state != "3":
-            sensor_ty = map_sensor_type[oidend]
-            sensor_unit = map_units[unit]
+            sensor_ty = _MAP_SENSOR_TYPE[oidend]
+            sensor_unit = _MAP_UNITS[unit]
             parsed.setdefault(sensor_ty, {})
             parsed[sensor_ty].setdefault(
-                "Sensor %s" % oidend,
+                f"Sensor {oidend}",
                 (
-                    map_states[state],
+                    _MAP_STATES[state],
                     float(reading_str) / 10,
                     sensor_unit,
                 ),
@@ -67,114 +79,93 @@ def parse_papouch_th2e_sensors(string_table):
     return parsed or None
 
 
-#   .--temperature---------------------------------------------------------.
-#   |      _                                      _                        |
-#   |     | |_ ___ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
-#   |     | __/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _ \      |
-#   |     | ||  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | |  __/      |
-#   |      \__\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \___|      |
-#   |                       |_|                                            |
-#   +----------------------------------------------------------------------+
-#   |                               main check                             |
-#   '----------------------------------------------------------------------'
+def _discover_temp(section: Section, what: str) -> DiscoveryResult:
+    for item in section[what]:
+        yield Service(item=item)
 
 
-def inventory_papouch_th2e_sensors_temp(parsed, what):
-    for item in parsed[what]:
-        yield item, {}
-
-
-def check_papouch_th2e_sensors_temp(item, params, parsed, what):
-    if item in parsed[what]:
-        (state, state_readable), reading, unit = parsed[what][item]
-        return check_temperature(
+def _check_temp(item: str, params: TempParamType, section: Section, what: str) -> CheckResult:
+    if item in section[what]:
+        (state, state_readable), reading, unit = section[what][item]
+        yield from check_temperature(
             reading,
             params,
-            f"papouch_th2e_sensors_{what}_{item}",
+            unique_name=f"papouch_th2e_sensors_{what}_{item}",
+            value_store=get_value_store(),
             dev_unit=unit,
             dev_status=state,
             dev_status_name=state_readable,
         )
-    return None
 
 
-def discover_papouch_th2e_sensors(parsed):
-    return inventory_papouch_th2e_sensors_temp(parsed, "temp")
+def discover_papouch_th2e_sensors(section: Section) -> DiscoveryResult:
+    yield from _discover_temp(section, "temp")
 
 
-def check_papouch_th2e_sensors(item, params, parsed):
-    return check_papouch_th2e_sensors_temp(item, params, parsed, "temp")
+def check_papouch_th2e_sensors(item: str, params: TempParamType, section: Section) -> CheckResult:
+    yield from _check_temp(item, params, section, "temp")
 
 
-check_info["papouch_th2e_sensors"] = LegacyCheckDefinition(
+snmp_section_papouch_th2e_sensors = SimpleSNMPSection(
     name="papouch_th2e_sensors",
     detect=all_of(
-        contains(".1.3.6.1.2.1.1.1.0", "th2e"), startswith(".1.3.6.1.2.1.1.2.0", ".0.10.43.6.1.4.1")
+        contains(".1.3.6.1.2.1.1.1.0", "th2e"),
+        startswith(".1.3.6.1.2.1.1.2.0", ".0.10.43.6.1.4.1"),
     ),
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.18248.20.1.2.1.1",
         oids=[OIDEnd(), "1", "2", "3"],
     ),
     parse_function=parse_papouch_th2e_sensors,
+)
+
+
+check_plugin_papouch_th2e_sensors = CheckPlugin(
+    name="papouch_th2e_sensors",
     service_name="Temperature %s",
     discovery_function=discover_papouch_th2e_sensors,
     check_function=check_papouch_th2e_sensors,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )
 
 
-def discover_papouch_th2e_sensors_dewpoint(parsed):
-    return inventory_papouch_th2e_sensors_temp(parsed, "dewpoint")
+def discover_papouch_th2e_sensors_dewpoint(section: Section) -> DiscoveryResult:
+    yield from _discover_temp(section, "dewpoint")
 
 
-def check_papouch_th2e_sensors_dewpoint(item, params, parsed):
-    return check_papouch_th2e_sensors_temp(item, params, parsed, "dewpoint")
+def check_papouch_th2e_sensors_dewpoint(
+    item: str, params: TempParamType, section: Section
+) -> CheckResult:
+    yield from _check_temp(item, params, section, "dewpoint")
 
 
-# .
-#   .--dew point-----------------------------------------------------------.
-#   |                _                             _       _               |
-#   |             __| | _____      __  _ __   ___ (_)_ __ | |_             |
-#   |            / _` |/ _ \ \ /\ / / | '_ \ / _ \| | '_ \| __|            |
-#   |           | (_| |  __/\ V  V /  | |_) | (_) | | | | | |_             |
-#   |            \__,_|\___| \_/\_/   | .__/ \___/|_|_| |_|\__|            |
-#   |                                 |_|                                  |
-#   '----------------------------------------------------------------------'
-
-
-check_info["papouch_th2e_sensors.dewpoint"] = LegacyCheckDefinition(
+check_plugin_papouch_th2e_sensors_dewpoint = CheckPlugin(
     name="papouch_th2e_sensors_dewpoint",
     service_name="Dew point %s",
     sections=["papouch_th2e_sensors"],
     discovery_function=discover_papouch_th2e_sensors_dewpoint,
     check_function=check_papouch_th2e_sensors_dewpoint,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )
 
-# .
-#   .--humidity------------------------------------------------------------.
-#   |              _                     _     _ _ _                       |
-#   |             | |__  _   _ _ __ ___ (_) __| (_) |_ _   _               |
-#   |             | '_ \| | | | '_ ` _ \| |/ _` | | __| | | |              |
-#   |             | | | | |_| | | | | | | | (_| | | |_| |_| |              |
-#   |             |_| |_|\__,_|_| |_| |_|_|\__,_|_|\__|\__, |              |
-#   |                                                  |___/               |
-#   '----------------------------------------------------------------------'
+
+def discover_papouch_th2e_sensors_humidity(section: Section) -> DiscoveryResult:
+    for item in section["humidity"]:
+        yield Service(item=item)
 
 
-def discover_papouch_th2e_sensors_humidity(parsed):
-    for item in parsed["humidity"]:
-        yield item, {}
+def check_papouch_th2e_sensors_humidity(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if item in section["humidity"]:
+        (state, state_readable), reading, _unit = section["humidity"][item]
+        yield Result(state=State(state), summary=f"Status: {state_readable}")
+        yield from check_humidity(reading, params)
 
 
-def check_papouch_th2e_sensors_humidity(item, params, parsed):
-    if item in parsed["humidity"]:
-        (state, state_readable), reading, _unit = parsed["humidity"][item]
-        yield state, "Status: %s" % state_readable
-        yield check_humidity(reading, params)
-
-
-check_info["papouch_th2e_sensors.humidity"] = LegacyCheckDefinition(
+check_plugin_papouch_th2e_sensors_humidity = CheckPlugin(
     name="papouch_th2e_sensors_humidity",
     service_name="Humidity %s",
     sections=["papouch_th2e_sensors"],

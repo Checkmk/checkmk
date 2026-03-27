51aa1f57cb401fab4a52f1b8c63025fff82e48bb
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-03-27 22:41:55 +0100

    inline bakery registry doc
    By inlining the doc into the rst sources we can move the sphinx
    documentation into cmk-plugin-apis. The 'register' module
    can not go there without exposing backend exclusive functionality
    in the v1 API version.
    
    Jira-Ref: CMK-31230
    Change-Id: I1e1c849a89ee921120863ac6d0ab57c2a0e1e488

--- cmk/base/api/bakery/register.py
+++ cmk/base/api/bakery/register.py
@@ -27,35 +27,7 @@ def bakery_plugin(
     scriptlets_function: ScriptletsFunction | None = None,
     windows_config_function: WindowsConfigFunction | None = None,
 ) -> None:
-    """Register a Bakery Plugin (Bakelet) to Checkmk
-
-    This registration function accepts a plug-in name (mandatory) and up to three
-    generator functions that may yield different types of artifacts.
-    The generator functions will be called with keyword-arguments 'conf' and/or 'aghash'
-    while processing the bakery plug-in (Callbacks), thus the specific call depends on the
-    argument names of the provided functions.
-    For keyword-arg 'conf', the corresponding WATO configuration will be provided.
-    For keyword-arg 'aghash', the configuration hash of the resulting agent package
-    will be provided.
-    Unused arguments can be omitted in the function's signatures.
-
-    Args:
-        name: The name of the agent plug-in to be processed. It must be unique, and match
-            the name of the corresponding WATO rule. It may only contain ascii
-            letters (A-Z, a-z), digits (0-9), and underscores (_).
-        files_function: Generator function that yields file artifacts.
-            Allowed function argument is 'conf'.
-            Yielded artifacts must must be of types 'Plugin', 'PluginConfig',
-            'SystemConfig', or 'SystemBinary'.
-        scriptlets_function: Generator function that yields scriptlet artifacts.
-            Allowed function arguments are 'conf' and 'aghash'.
-            Yielded artifacts must be of type 'Scriptlet'.
-        windows_config_function: generator function that yields windows config artifacts.
-            Allowed function arguments are 'conf' and 'aghash'.
-            Yielded artifacts must be of types 'WindowsConfigEntry', 'WindowsGlobalConigEntry',
-            'WindowsSystemConfigEntry', 'WindowsConfigItems', or 'WindowsPluginConfig'.
-    """
-
+    # Register a bakery plugin; doc is maintained in doc/plugin-api.
     bakery_plugin_object = create_bakery_plugin(
         name=name,
         files_function=files_function,
--- doc/plugin-api/source/cmk.base.plugins.bakery.bakery_api/index.rst
+++ doc/plugin-api/source/cmk.base.plugins.bakery.bakery_api/index.rst
@@ -20,9 +20,34 @@ bakery_api.v1
 bakery_api.v1.register
 **********************
 
-.. automodule:: cmk.base.plugins.bakery.bakery_api.v1.register
-   :members:
-   :show-inheritance:
+.. py:function:: bakery_plugin(*, name: str, files_function: FilesFunction | None = None, scriptlets_function: ScriptletsFunction | None = None, windows_config_function: WindowsConfigFunction | None = None) -> None
+
+   Register a Bakery Plugin (Bakelet) to Checkmk
+
+   This registration function accepts a plug-in name (mandatory) and up to three
+   generator functions that may yield different types of artifacts.
+   The generator functions will be called with keyword-arguments 'conf' and/or 'aghash'
+   while processing the bakery plug-in (Callbacks), thus the specific call depends on the
+   argument names of the provided functions.
+   For keyword-arg 'conf', the corresponding WATO configuration will be provided.
+   For keyword-arg 'aghash', the configuration hash of the resulting agent package
+   will be provided.
+   Unused arguments can be omitted in the function's signatures.
+
+   :param name: The name of the agent plug-in to be processed. It must be unique, and match
+       the name of the corresponding WATO rule. It may only contain ascii
+       letters (A-Z, a-z), digits (0-9), and underscores (_).
+   :param files_function: Generator function that yields file artifacts.
+       Allowed function argument is 'conf'.
+       Yielded artifacts must must be of types 'Plugin', 'PluginConfig',
+       'SystemConfig', or 'SystemBinary'.
+   :param scriptlets_function: Generator function that yields scriptlet artifacts.
+       Allowed function arguments are 'conf' and 'aghash'.
+       Yielded artifacts must be of type 'Scriptlet'.
+   :param windows_config_function: generator function that yields windows config artifacts.
+       Allowed function arguments are 'conf' and 'aghash'.
+       Yielded artifacts must be of types 'WindowsConfigEntry', 'WindowsGlobalConigEntry',
+       'WindowsSystemConfigEntry', 'WindowsConfigItems', or 'WindowsPluginConfig'.
 
 
 

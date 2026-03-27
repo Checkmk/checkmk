105fef8f4a7cf22851591f884485b9d1d4a0a08e
Alejandro Yunta <alejandro.yunta@checkmk.com>
2026-03-27 16:29:17 +0100

    fix(personal notifications): preserve search
    Preserve search query when creating a new notification in the personal
    notifications pages ("Custom notification table for user X" and
    "Your personal notification rules")
    
    SUP-27345
    
    Change-Id: I263dae4cf2b8f565162eea11a37fcc467375ad8c

--- cmk/gui/wato/pages/notifications/modes.py
+++ cmk/gui/wato/pages/notifications/modes.py
@@ -2731,6 +2731,9 @@ class ModeUserNotifications(ABCUserNotificationsMode):
             raise MKUserError("user", str(e))
 
     def page_menu(self, config: Config, breadcrumb: Breadcrumb) -> PageMenu:
+        search_vars: HTTPVariables = (
+            [("search", search)] if (search := request.get_str_input("search", "")) else []
+        )
         menu = PageMenu(
             dropdowns=[
                 PageMenuDropdown(
@@ -2748,7 +2751,9 @@ class ModeUserNotifications(ABCUserNotificationsMode):
                                             [
                                                 ("mode", "user_notification_rule"),
                                                 ("user", self._user_id()),
+                                                ("back_mode", self.name()),
                                             ]
+                                            + search_vars
                                         )
                                     ),
                                     is_shortcut=True,
@@ -2805,6 +2810,9 @@ class ModePersonalUserNotifications(ABCUserNotificationsMode):
         return self.mode_url()
 
     def page_menu(self, config: Config, breadcrumb: Breadcrumb) -> PageMenu:
+        search_vars: HTTPVariables = (
+            [("search", search)] if (search := request.get_str_input("search", "")) else []
+        )
         return PageMenu(
             dropdowns=[
                 PageMenuDropdown(
@@ -2818,11 +2826,17 @@ class ModePersonalUserNotifications(ABCUserNotificationsMode):
                                     title=_("Add rule"),
                                     icon_name=StaticIcon(IconNames.new),
                                     item=make_simple_link(
-                                        folder_preserving_link([("mode", "notification_rule_p")])
+                                        folder_preserving_link(
+                                            [
+                                                ("mode", "notification_rule_p"),
+                                                ("back_mode", self.name()),
+                                            ]
+                                            + search_vars
+                                        )
                                     ),
                                     is_shortcut=True,
                                     is_suggested=True,
-                                ),
+                                )
                             ],
                         )
                     ],

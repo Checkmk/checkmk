6058dea1e2c0b55aa7fbf69b0a172c8bb2add3cb
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-03-30 13:25:13 +0200

    Legacy check migration: huawei_switch_psu II
    Change-Id: If32fff0d0ce3dacf8d0a6960b1a513137926314b

--- tests/unit/cmk/plugins/huawei/agent_based/test_huawei_switch_psu.py
+++ tests/unit/cmk/plugins/huawei/agent_based/test_huawei_switch_psu.py
@@ -8,7 +8,7 @@
 import pytest
 
 from cmk.agent_based.v2 import Result, State, StringTable
-from cmk.legacy_checks.huawei_switch_psu import (
+from cmk.plugins.huawei.agent_based.huawei_switch_psu import (
     check_huawei_switch_psu,
     discover_huawei_switch_psu,
     parse_huawei_switch_psu,

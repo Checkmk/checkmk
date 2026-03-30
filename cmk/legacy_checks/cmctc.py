8230926b3248f37a9ad017c50d46325a693043ce
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-03-30 14:42:09 +0200

    Legacy check migration: cmctc_temp I
    Change-Id: I182e5ca8aacc3e1bd1957d77dbd30b81ca144ca0

--- cmk/legacy_checks/cmctc.py
+++ cmk/legacy_checks/cmctc.py
@@ -3,21 +3,25 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="type-arg"
-
-from collections.abc import Iterable, Mapping
+from collections.abc import Mapping, Sequence
 from typing import NamedTuple
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature, TempParamType
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 from cmk.plugins.rittal.lib.cmctc import (
     cmctc_translate_status,
     cmctc_translate_status_text,
     DETECT_CMCTC,
 )
-
-check_info = {}
 
 # Table columns:
 # 0: index
@@ -43,7 +47,7 @@ Section = Mapping[str, Sensor]
 _TABLES = ["3", "4", "5", "6"]
 
 
-def parse_cmctc_temp(string_table: list[StringTable]) -> Section:
+def parse_cmctc_temp(string_table: Sequence[StringTable]) -> Section | None:
     return {
         f"{table}.{item}": Sensor(
             status=int(status),
@@ -57,81 +61,56 @@ def parse_cmctc_temp(string_table: list[StringTable]) -> Section:
     }
 
 
-def discover_cmctc_temp(section: Section) -> Iterable[tuple[str, dict]]:
-    yield from ((item, {}) for item in section)
+def discover_cmctc_temp(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
 
 
-def check_cmctc_temp(item: str, params: TempParamType, section: Section) -> Iterable:
+def check_cmctc_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
     if (sensor := section.get(item)) is None:
         return
 
-    yield check_temperature(
+    yield from check_temperature(
         sensor.reading,
         params,
-        "cmctc_temp_%s" % item,
+        unique_name=f"cmctc_temp_{item}",
+        value_store=get_value_store(),
         dev_levels=sensor.levels,
         dev_levels_lower=sensor.levels_lower,
         dev_status=cmctc_translate_status(sensor.status),
-        dev_status_name="Unit: %s" % cmctc_translate_status_text(sensor.status),
+        dev_status_name=f"Unit: {cmctc_translate_status_text(sensor.status)}",
     )
 
 
-check_info["cmctc_temp"] = LegacyCheckDefinition(
+snmp_section_cmctc_temp = SNMPSection(
     name="cmctc_temp",
     detect=DETECT_CMCTC,
     fetch=[
         SNMPTree(
-            base=".1.3.6.1.4.1.2606.4.2.3",
-            oids=[
-                "5.2.1.1",
-                "5.2.1.2",
-                "5.2.1.4",
-                "5.2.1.5",
-                "5.2.1.6",
-                "5.2.1.7",
-                "5.2.1.8",
-            ],
+            base=".1.3.6.1.4.1.2606.4.2.3.5.2.1",
+            oids=["1", "2", "4", "5", "6", "7", "8"],
         ),
         SNMPTree(
-            base=".1.3.6.1.4.1.2606.4.2.4",
-            oids=[
-                "5.2.1.1",
-                "5.2.1.2",
-                "5.2.1.4",
-                "5.2.1.5",
-                "5.2.1.6",
-                "5.2.1.7",
-                "5.2.1.8",
-            ],
+            base=".1.3.6.1.4.1.2606.4.2.4.5.2.1",
+            oids=["1", "2", "4", "5", "6", "7", "8"],
         ),
         SNMPTree(
-            base=".1.3.6.1.4.1.2606.4.2.5",
-            oids=[
-                "5.2.1.1",
-                "5.2.1.2",
-                "5.2.1.4",
-                "5.2.1.5",
-                "5.2.1.6",
-                "5.2.1.7",
-                "5.2.1.8",
-            ],
+            base=".1.3.6.1.4.1.2606.4.2.5.5.2.1",
+            oids=["1", "2", "4", "5", "6", "7", "8"],
         ),
         SNMPTree(
-            base=".1.3.6.1.4.1.2606.4.2.6",
-            oids=[
-                "5.2.1.1",
-                "5.2.1.2",
-                "5.2.1.4",
-                "5.2.1.5",
-                "5.2.1.6",
-                "5.2.1.7",
-                "5.2.1.8",
-            ],
+            base=".1.3.6.1.4.1.2606.4.2.6.5.2.1",
+            oids=["1", "2", "4", "5", "6", "7", "8"],
         ),
     ],
     parse_function=parse_cmctc_temp,
+)
+
+
+check_plugin_cmctc_temp = CheckPlugin(
+    name="cmctc_temp",
     service_name="Temperature %s",
     discovery_function=discover_cmctc_temp,
     check_function=check_cmctc_temp,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )

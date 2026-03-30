1204d5d310616698f649de9dd647bdcd19be56ce
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-03-30 13:51:27 +0200

    Legacy check migration: cmc_temp I
    CMK-32703
    
    Change-Id: Ia91118998f862641e9097d5bb2ac0443924b1f86

--- cmk/legacy_checks/cmc_temp.py
+++ cmk/legacy_checks/cmc_temp.py
@@ -3,34 +3,40 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 from collections.abc import Sequence
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import contains, SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 # [[[u'26', u'26']], [[u'45', u'15', u'45', u'15']]]
 
 
-def discover_cmc_temp(info):
+def discover_cmc_temp(section: Sequence[StringTable]) -> DiscoveryResult:
     # There are always two sensors
-    yield "1", {}
-    yield "2", {}
+    yield Service(item="1")
+    yield Service(item="2")
 
 
-def check_cmc_temp(item, params, info):
+def check_cmc_temp(item: str, params: TempParamType, section: Sequence[StringTable]) -> CheckResult:
     offset = int(item) - 1
-    current_temp = int(info[0][0][offset])
-    dev_high, dev_low = map(int, info[1][0][offset * 2 :][:2])
-    return check_temperature(
+    current_temp = int(section[0][0][offset])
+    dev_high, dev_low = map(int, section[1][0][offset * 2 :][:2])
+    yield from check_temperature(
         current_temp,
         params,
-        "cmc_temp_%s" % item,
+        unique_name=f"cmc_temp_{item}",
+        value_store=get_value_store(),
         dev_levels=(dev_high, dev_high),
         dev_levels_lower=(dev_low, dev_low),
     )
@@ -40,9 +46,8 @@ def parse_cmc_temp(string_table: Sequence[StringTable]) -> Sequence[StringTable]
     return string_table if any(string_table) else None
 
 
-check_info["cmc_temp"] = LegacyCheckDefinition(
+snmp_section_cmc_temp = SNMPSection(
     name="cmc_temp",
-    parse_function=parse_cmc_temp,
     detect=contains(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.2606.1"),
     fetch=[
         SNMPTree(
@@ -54,6 +59,12 @@ check_info["cmc_temp"] = LegacyCheckDefinition(
             oids=["4", "5", "6", "7"],
         ),
     ],
+    parse_function=parse_cmc_temp,
+)
+
+
+check_plugin_cmc_temp = CheckPlugin(
+    name="cmc_temp",
     service_name="Temperature Sensor %s",
     discovery_function=discover_cmc_temp,
     check_function=check_cmc_temp,

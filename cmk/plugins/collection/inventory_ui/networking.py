04307b6fbb4d9b4661d532cfcdec0f9f5b873fc1
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-03-30 07:27:48 +0200

    19586 FIX Fix crash when sorting by Prefix length in IP addresses inventory
    CMK-32896
    
    Change-Id: I092b84d8456818379c40eeefbdb46f2becb123ab

--- cmk/plugins/collection/inventory_ui/networking.py
+++ cmk/plugins/collection/inventory_ui/networking.py
@@ -92,7 +92,7 @@ node_networking_addresses = Node(
             "type": TextField(Title("Address type"), render=_render_ip_address_type),
             "network": TextField(Title("Network")),
             "netmask": TextField(Title("Netmask")),
-            "prefixlength": TextField(Title("Prefix length")),
+            "prefixlength": NumberField(Title("Prefix length")),
             "broadcast": TextField(Title("Broadcast")),
             "scope_id": TextField(Title("Scope ID")),
         },

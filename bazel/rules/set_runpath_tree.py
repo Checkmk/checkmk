d8b3ad7ede5f8474cb3d839151735892ad197d4b
Nigel Banks <nigel.banks@checkmk.com>
2026-03-31 21:46:31 +0200

    Set RUNPATH on ELF files in perl-modules/net-snmp package
    CMK-29407
    
    Change-Id: I27591f905217aabe21276236673d30549ab560e3

---
+++ bazel/rules/set_runpath_tree.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+"""Patches ELF .so files in a directory tree with depth-relative $ORIGIN rpaths."""
+
+import os
+import re
+import shutil
+import subprocess
+import sys
+
+_SO_PATTERN = re.compile(r"\.so(\.\d+)*$")
+
+
+def _patch_elf_files(directory: str, patchelf: str, base_rpath: str) -> None:
+    for dirpath, _, filenames in os.walk(directory):
+        for filename in filenames:
+            if not _SO_PATTERN.search(filename):
+                continue
+            filepath = os.path.join(dirpath, filename)
+            if os.path.islink(filepath):
+                continue
+            rel_dir = os.path.relpath(dirpath, directory)
+            depth = len(rel_dir.split(os.sep)) if rel_dir != "." else 0
+            extra_ups = "/".join([".."] * (depth - 1)) if depth > 1 else ""
+            rpath = f"{base_rpath}/{extra_ups}" if extra_ups else base_rpath
+            subprocess.run([patchelf, "--set-rpath", rpath, filepath], check=True)
+
+
+def main() -> None:
+    src_dir, out_dir, patchelf, base_rpath = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    if src_dir != out_dir:
+        shutil.copytree(src_dir, out_dir, copy_function=shutil.copyfile, dirs_exist_ok=True)
+    _patch_elf_files(out_dir, patchelf, base_rpath)
+
+
+if __name__ == "__main__":
+    main()

2b273e4a86733a06047b2d15dd294e709f6bd945
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-03-31 12:30:20 +0200

    CMK-32074 Legacy check migration: huawei_osn_fan I
    Change-Id: I4dcd95a4719ff782b7ab4352ee07cd0b43ce29f5

--- cmk/legacy_checks/huawei_osn_fan.py
+++ cmk/legacy_checks/huawei_osn_fan.py
@@ -3,50 +3,59 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.agent_based.legacy.v0_unstable import (
-    LegacyCheckDefinition,
-    LegacyDiscoveryResult,
-    LegacyResult,
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
 )
-from cmk.agent_based.v2 import SNMPTree, StringTable
 from cmk.plugins.huawei.lib import DETECT_HUAWEI_OSN
 
-check_info = {}
+_TRANSLATE_SPEED: dict[str, tuple[State, str]] = {
+    "0": (State.WARN, "stop"),
+    "1": (State.OK, "low"),
+    "2": (State.OK, "mid-low"),
+    "3": (State.OK, "mid"),
+    "4": (State.OK, "mid-high"),
+    "5": (State.WARN, "high"),
+}
 
 
-def discover_huawei_osn_fan(info: StringTable) -> LegacyDiscoveryResult:
-    for line in info:
-        yield (line[0], {})
+def discover_huawei_osn_fan(section: StringTable) -> DiscoveryResult:
+    for line in section:
+        yield Service(item=line[0])
 
 
-def check_huawei_osn_fan(item: str, params: object, info: StringTable) -> LegacyResult | None:
-    translate_speed = {
-        "0": (1, "stop"),
-        "1": (0, "low"),
-        "2": (0, "mid-low"),
-        "3": (0, "mid"),
-        "4": (0, "mid-high"),
-        "5": (1, "high"),
-    }
-    for line in info:
+def check_huawei_osn_fan(item: str, section: StringTable) -> CheckResult:
+    for line in section:
         if item == line[0]:
-            state, state_readable = translate_speed[line[1]]
-            return state, "Speed: %s" % state_readable
-    return None
+            state, state_readable = _TRANSLATE_SPEED[line[1]]
+            yield Result(state=state, summary=f"Speed: {state_readable}")
+            return
 
 
 def parse_huawei_osn_fan(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["huawei_osn_fan"] = LegacyCheckDefinition(
+snmp_section_huawei_osn_fan = SimpleSNMPSection(
     name="huawei_osn_fan",
-    parse_function=parse_huawei_osn_fan,
     detect=DETECT_HUAWEI_OSN,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.2011.2.25.4.70.20.10.10.1",
         oids=["1", "2"],
     ),
+    parse_function=parse_huawei_osn_fan,
+)
+
+
+check_plugin_huawei_osn_fan = CheckPlugin(
+    name="huawei_osn_fan",
     service_name="Unit %s (Fan)",
     discovery_function=discover_huawei_osn_fan,
     check_function=check_huawei_osn_fan,

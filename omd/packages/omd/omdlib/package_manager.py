288d68a77cd61511a2a970582d6f947924974e5c
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-04-02 09:55:41 +0200

    18897 FIX omd cleanup: Don't try to purge dpkg-query
    CMK-32419
    
    Change-Id: Ica1e5cc5a31c4ad6e3812d398945692cadc19094

--- omd/packages/omd/omdlib/package_manager.py
+++ omd/packages/omd/omdlib/package_manager.py
@@ -7,7 +7,14 @@ import abc
 import os
 import subprocess
 import sys
-from typing import override
+from typing import override, Protocol
+
+
+class _Popen(Protocol):
+    def communicate(self) -> tuple[str, str]: ...
+
+    @property
+    def returncode(self) -> int | None: ...
 
 
 class PackageManager(abc.ABC):
@@ -28,12 +35,6 @@ class PackageManager(abc.ABC):
     def get_package(self, version_path: str, verbose: bool) -> list[str]:
         raise NotImplementedError()
 
-    def _execute_uninstall(self, cmd: list[str], verbose: bool) -> None:
-        p = self._execute(cmd, verbose)
-        output = p.communicate()[0]
-        if p.wait() != 0:
-            sys.exit("Failed to uninstall package:\n%s" % output)
-
     def _execute(self, cmd: list[str], verbose: bool) -> subprocess.Popen[str]:
         if verbose:
             sys.stdout.write("Executing: " + subprocess.list2cmdline(cmd) + "\n")
@@ -44,7 +45,7 @@ class PackageManager(abc.ABC):
             close_fds=True,
             stdin=subprocess.DEVNULL,
             stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            stderr=subprocess.PIPE,
             encoding="utf-8",
         )
 
@@ -52,27 +53,52 @@ class PackageManager(abc.ABC):
 class _PackageManagerDEB(PackageManager):
     @override
     def uninstall(self, package_name: str, verbose: bool) -> None:
-        self._execute_uninstall(["apt-get", "-y", "purge", package_name], verbose)
+        p = self._execute(["apt-get", "-y", "purge", package_name], verbose)
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            sys.stderr.write("Failed to uninstall package:\n")
+            sys.stderr.write(f"stdout:\n {stdout}\n")
+            sys.stderr.write(f"stderr:\n {stderr}\n")
+            sys.exit(1)
 
     @override
     def get_package(self, version_path: str, verbose: bool) -> list[str]:
         process = self._execute(["dpkg", "-S", version_path], verbose)
-        stdout = process.communicate()[0]
-        if process.wait() != 0:
-            sys.stderr.write(f"Failed to get packages owning {version_path}\n {stdout}")
+        return self._get_package(process, version_path)
+
+    @classmethod
+    def _get_package(cls, process: _Popen, version_path: str) -> list[str]:
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            sys.stderr.write(f"Failed to get packages owning {version_path}\n")
+            sys.stderr.write(f"stdout:\n {stdout}\n")
+            sys.stderr.write(f"stderr:\n {stderr}\n")
             return []
         return stdout.split(":", maxsplit=1)[0].split(",")
 
 
 class _PackageManagerRPM(PackageManager):
+    @override
     def uninstall(self, package_name: str, verbose: bool) -> None:
-        self._execute_uninstall(["rpm", "-e", package_name], verbose)
+        p = self._execute(["rpm", "-e", package_name], verbose)
+        stdout, stderr = p.communicate()
+        if p.returncode != 0:
+            sys.stderr.write("Failed to uninstall package:\n")
+            sys.stderr.write(f"stdout:\n {stdout}\n")
+            sys.stderr.write(f"stderr:\n {stderr}\n")
+            sys.exit(1)
 
     @override
     def get_package(self, version_path: str, verbose: bool) -> list[str]:
         process = self._execute(["rpm", "-qf", version_path], verbose)
-        stdout = process.communicate()[0]
-        if process.wait() != 0:
-            sys.stderr.write(f"Failed to get packages owning {version_path}\n {stdout}")
+        return self._get_package(process, version_path)
+
+    @classmethod
+    def _get_package(cls, process: _Popen, version_path: str) -> list[str]:
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            sys.stderr.write(f"Failed to get packages owning {version_path}\n")
+            sys.stderr.write(f"stdout:\n {stdout}\n")
+            sys.stderr.write(f"stderr:\n {stderr}\n")
             return []
         return stdout.splitlines()

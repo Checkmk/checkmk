15f5fa63044ae18e3c7687899a07691bade7c718
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-02 16:09:32 +0200

    Legacy check migration: hepta I
    Change-Id: I917a0d586b9ae66f975830e01c52f0255327752c

--- cmk/legacy_checks/hepta.py
+++ cmk/legacy_checks/hepta.py
@@ -3,46 +3,49 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="possibly-undefined"
-# mypy: disable-error-code="type-arg"
-
 # Device Manual: https://www.hopf.com/downloads/manuals/8030hepta-gps_v0400_en.pdf
 
 
 import struct
 import time
-from collections.abc import Iterable, Mapping
+from collections.abc import Mapping, Sequence
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, startswith
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
 
 Section = Mapping[str, str]
 
 
-def get_time(timefromdevice):
+def _get_time(timefromdevice: str) -> str:
     length = len(timefromdevice)
-    DateAndTime = dict({8: ">HBBBBBB", 11: ">HBBBBBBcBB"})
-    if length in {8, 11}:
-        timedecoded = struct.unpack(DateAndTime[length], timefromdevice.encode("latin-1"))
-        if length == 8:
-            timedevice = time.strftime(
-                "%d-%m-%Y %H:%M:%S", time.strptime(str(timedecoded), "(%Y, %m, %d, %H, %M, %S, %f)")
-            )
-        else:
-            offset = str(timedecoded[7]) + str(timedecoded[8]) + ":" + str(timedecoded[9])
-            date = time.strftime(
-                "%d-%m-%Y %H:%M:%S %Z",
-                time.strptime(str(timedecoded[0:7]), "(%Y, %m, %d, %H, %M, %S, %f)"),
-            )
-            timedevice = str(date) + "" + str(offset)
-    return timedevice
+    date_and_time = {8: ">HBBBBBB", 11: ">HBBBBBBcBB"}
+    if length == 8:
+        timedecoded = struct.unpack(date_and_time[length], timefromdevice.encode("latin-1"))
+        return time.strftime(
+            "%d-%m-%Y %H:%M:%S", time.strptime(str(timedecoded), "(%Y, %m, %d, %H, %M, %S, %f)")
+        )
+    if length == 11:
+        timedecoded = struct.unpack(date_and_time[length], timefromdevice.encode("latin-1"))
+        offset = str(timedecoded[7]) + str(timedecoded[8]) + ":" + str(timedecoded[9])
+        date = time.strftime(
+            "%d-%m-%Y %H:%M:%S %Z",
+            time.strptime(str(timedecoded[0:7]), "(%Y, %m, %d, %H, %M, %S, %f)"),
+        )
+        return f"{date}{offset}"
+    return ""
 
 
-def parse_hepta(string_table):
+def parse_hepta(string_table: Sequence[StringTable]) -> Section | None:
     if not any(string_table):
         return None
     (
@@ -61,35 +64,33 @@ def parse_hepta(string_table):
         "devicetype": device_type,
         "serialnumber": serial_number,
         "firmwareversion": fw_version,
-        "firmwaredate": get_time(fw_date),
+        "firmwaredate": _get_time(fw_date),
         "version": version,
         "ntpstratum": ntp_stratum,
         "syncmoduletimesyncstate": sync_state,
-        "syncmoduletimelocal": get_time(local),
+        "syncmoduletimelocal": _get_time(local),
     }
 
 
-def discover_hepta(section: Section) -> Iterable[tuple[None, dict]]:
+def discover_hepta(section: Section) -> DiscoveryResult:
     if section:
-        yield None, {}
+        yield Service()
 
 
-def check_hepta(item, params, parsed):
-    yield (
-        0,
-        "DeviceType %s ; SerialNumber %s ; FirmwareVersion %s ; FirmwareDate %s ; "
-        "Version %s"
-        % (
-            parsed["devicetype"],
-            parsed["serialnumber"],
-            parsed["firmwareversion"],
-            parsed["firmwaredate"],
-            parsed["version"],
+def check_hepta(section: Section) -> CheckResult:
+    yield Result(
+        state=State.OK,
+        summary=(
+            f"DeviceType {section['devicetype']} ; "
+            f"SerialNumber {section['serialnumber']} ; "
+            f"FirmwareVersion {section['firmwareversion']} ; "
+            f"FirmwareDate {section['firmwaredate']} ; "
+            f"Version {section['version']}"
         ),
     )
 
 
-check_info["hepta"] = LegacyCheckDefinition(
+snmp_section_hepta = SNMPSection(
     name="hepta",
     detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.12527"),
     fetch=[
@@ -103,6 +104,11 @@ check_info["hepta"] = LegacyCheckDefinition(
         ),
     ],
     parse_function=parse_hepta,
+)
+
+
+check_plugin_hepta = CheckPlugin(
+    name="hepta",
     service_name="HPF Info",
     discovery_function=discover_hepta,
     check_function=check_hepta,
@@ -127,24 +133,26 @@ check_info["hepta"] = LegacyCheckDefinition(
 #   '----------------------------------------------------------------------'
 
 
-def discover_hepta_time_sync(info):
-    yield "SyncModuleTimeSyncState", {}
+def discover_hepta_time_sync(section: Section) -> DiscoveryResult:
+    yield Service(item="SyncModuleTimeSyncState")
 
 
-def check_hepta_time_sync(item, params, parsed):
-    if parsed["syncmoduletimesyncstate"] == "R":
-        yield (0, "Radio synchronous with high precision")
-    elif parsed["syncmoduletimesyncstate"] == "r":
-        yield (1, "Radio synchronous with low precision")
-    elif parsed["syncmoduletimesyncstate"] == "C":
-        yield (2, "Crystal")  # In manual the Crystal mode means Invalid Statum
-    elif parsed["syncmoduletimesyncstate"] == "I":
-        yield (2, "Invalid time and date")
+def check_hepta_time_sync(item: str, section: Section) -> CheckResult:
+    if section["syncmoduletimesyncstate"] == "R":
+        yield Result(state=State.OK, summary="Radio synchronous with high precision")
+    elif section["syncmoduletimesyncstate"] == "r":
+        yield Result(state=State.WARN, summary="Radio synchronous with low precision")
+    elif section["syncmoduletimesyncstate"] == "C":
+        yield Result(
+            state=State.CRIT, summary="Crystal"
+        )  # In manual the Crystal mode means Invalid Statum
+    elif section["syncmoduletimesyncstate"] == "I":
+        yield Result(state=State.CRIT, summary="Invalid time and date")
     else:
-        yield (3, "No data available")
+        yield Result(state=State.UNKNOWN, summary="No data available")
 
 
-check_info["hepta.syncmoduletimesyncstate"] = LegacyCheckDefinition(
+check_plugin_hepta_syncmoduletimesyncstate = CheckPlugin(
     name="hepta_syncmoduletimesyncstate",
     service_name="%s",
     sections=["hepta"],
@@ -170,22 +178,22 @@ check_info["hepta.syncmoduletimesyncstate"] = LegacyCheckDefinition(
 #   +----------------------------------------------------------------------+
 #   |                                                                      |
 #   '----------------------------------------------------------------------'
-def discover_hepta_ntpsysstratum(info):
-    yield "ntpSysStratum", {}
+def discover_hepta_ntpsysstratum(section: Section) -> DiscoveryResult:
+    yield Service(item="ntpSysStratum")
 
 
-def check_hepta_ntpsysstratum(item, params, parsed):
-    if parsed["ntpstratum"] == "1":
-        yield (0, "Stratum 1, Primary Reference ")
-    elif parsed["ntpstratum"] == "16":
-        yield (2, "Stratum Invalid")
-    elif parsed["ntpstratum"] == "0":
-        yield (3, "Stratum Unknown")
+def check_hepta_ntpsysstratum(item: str, section: Section) -> CheckResult:
+    if section["ntpstratum"] == "1":
+        yield Result(state=State.OK, summary="Stratum 1, Primary Reference ")
+    elif section["ntpstratum"] == "16":
+        yield Result(state=State.CRIT, summary="Stratum Invalid")
+    elif section["ntpstratum"] == "0":
+        yield Result(state=State.UNKNOWN, summary="Stratum Unknown")
     else:
-        yield (1, "Stratum is using secondary reference(via NTP)")
+        yield Result(state=State.WARN, summary="Stratum is using secondary reference(via NTP)")
 
 
-check_info["hepta.ntpsysstratum"] = LegacyCheckDefinition(
+check_plugin_hepta_ntpsysstratum = CheckPlugin(
     name="hepta_ntpsysstratum",
     service_name="%s",
     sections=["hepta"],
@@ -211,15 +219,15 @@ check_info["hepta.ntpsysstratum"] = LegacyCheckDefinition(
 #   +----------------------------------------------------------------------+
 #   |                                                                      |
 #   '----------------------------------------------------------------------'
-def discover_hepta_syncmoduletimelocal(info):
-    yield "SyncModuleTimeLocal", {}
+def discover_hepta_syncmoduletimelocal(section: Section) -> DiscoveryResult:
+    yield Service(item="SyncModuleTimeLocal")
 
 
-def check_hepta_syncmoduletimelocal(item, params, parsed):
-    yield (0, "Module Time: %s " % parsed["syncmoduletimelocal"])
+def check_hepta_syncmoduletimelocal(item: str, section: Section) -> CheckResult:
+    yield Result(state=State.OK, summary=f"Module Time: {section['syncmoduletimelocal']}")
 
 
-check_info["hepta.syncmoduletimelocal"] = LegacyCheckDefinition(
+check_plugin_hepta_syncmoduletimelocal = CheckPlugin(
     name="hepta_syncmoduletimelocal",
     service_name="%s",
     sections=["hepta"],

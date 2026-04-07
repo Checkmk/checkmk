5a7bcd7eb1bde2b2bbc40fe55281ee0fbb99bdab
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-08 00:35:51 +0200

    bakery: Move unitrends rulespec to cmk/plugins/unitrends/rulesets/
    Change-Id: I6aa53596992e7b6619dc17bd6f8d5d1e17cbac56

---
+++ tests/unit/cmk/plugins/unitrends/rulesets/test_bakery.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.plugins.unitrends.rulesets.bakery import migrate
+
+
+@pytest.mark.parametrize(
+    "old, expected",
+    [
+        pytest.param(True, {"deployment": ("sync", None)}, id="deploy"),
+        pytest.param(None, {"deployment": ("do_not_deploy", None)}, id="do_not_deploy"),
+        pytest.param(
+            {"deployment": ("sync", None)},
+            {"deployment": ("sync", None)},
+            id="already_migrated",
+        ),
+    ],
+)
+def test_migrate(old: object, expected: dict[str, object]) -> None:
+    assert migrate(old) == expected

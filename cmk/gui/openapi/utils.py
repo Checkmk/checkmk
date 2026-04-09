98729ba3c515346a04e44c68ad4c7fc110bcbe37
Lars Michelsen <lars.michelsen@checkmk.com>
2026-04-09 22:33:53 +0200

    Split cmk/gui: Extract watolib dependency targets
    Extract small targets from nodevis/, rest_api_types/, rule_specs/,
    oauth2_connections/, and openapi/ so that the full watolib target can
    depend on them without going through the monolithic gui (which would
    create a cycle).
    
    Change-Id: I1be14d2f702a0b8c3e5db52476fafb4e2b0c473d

--- cmk/gui/openapi/utils.py
+++ cmk/gui/openapi/utils.py
@@ -3,7 +3,7 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="mutable-override"
+# mypy: disable-error-code="mutable-override,no-any-return"
 
 from collections.abc import Iterable
 from typing import Any, Literal, NewType

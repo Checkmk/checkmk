bd874902f1915b5506ff79777684dbd3aa58258d
Alex Zurhake <alex.zurhake@checkmk.com>
2026-04-10 10:37:51 +0200

    Add medium chain jenkins script
    CMK-32650
    
    Change-Id: I1961dc7dd83921190b127b47165443e8f0091fbb

---
+++ buildscripts/scripts/test-medium-chain.groovy
@@ -0,0 +1,111 @@
+#!groovy
+
+/// file: test-medium-chain.groovy
+
+void main() {
+    check_job_parameters([
+        ["EDITION", true],  // the testees package long edition string (e.g. 'ultimate')
+        ["DISTRO", true],  // the testees package distro string (e.g. 'ubuntu-24.04')
+        "TEST_FILTER",  // a filter string to select which tests to run
+        "CIPARAM_OVERRIDE_DOCKER_TAG_BUILD",  // the docker tag to use for building and testing, forwarded to packages build job
+        "FAKE_ARTIFACTS",
+    ]);
+
+    check_environment_variables([
+        "DOCKER_REGISTRY",
+    ]);
+
+    def single_tests = load("${checkout_dir}/buildscripts/scripts/utils/single_tests.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+
+    def safe_branch_name = versioning.safe_branch_name();
+    def branch_version = versioning.get_branch_version(checkout_dir);
+    def cmk_version = versioning.get_cmk_version(safe_branch_name, branch_version, "daily");
+    def distro = params.DISTRO;
+    def edition = params.EDITION;
+    def fake_artifacts = params.FAKE_ARTIFACTS;
+
+    def make_target = "test-medium-chain-docker";
+    def download_dir = "package_download";
+
+    def setup_values = single_tests.common_prepare(
+        version: "daily",
+        make_target: make_target,
+        docker_tag: params.CIPARAM_OVERRIDE_DOCKER_TAG_BUILD
+    );
+
+    currentBuild.description += (
+        """
+        |Run medium chain tests<br>
+        |safe_branch_name: ${safe_branch_name}<br>
+        |branch_version: ${branch_version}<br>
+        |cmk_version: ${cmk_version}<br>
+        |docker_tag: ${setup_values.docker_tag}<br>
+        |edition: ${edition}<br>
+        |distro: ${distro}<br>
+        |make_target: ${make_target}<br>
+        """.stripMargin());
+
+    print(
+        """
+        |===== CONFIGURATION ===============================
+        |safe_branch_name:...... │${safe_branch_name}│
+        |branch_version:........ │${branch_version}│
+        |cmk_version:........... │${cmk_version}
+        |docker_tag:............ │${setup_values.docker_tag}│
+        |edition:............... │${edition}│
+        |distro:................ │${distro}│
+        |make_target:........... │${make_target}│
+        |===================================================
+        """.stripMargin());
+
+    dir("${checkout_dir}") {
+        stage("Fetch Checkmk package") {
+            single_tests.fetch_package(
+                edition: edition,
+                distro: distro,
+                docker_tag: setup_values.docker_tag,
+                download_dir: download_dir,
+                bisect_comment: params.CIPARAM_BISECT_COMMENT,
+                fake_artifacts: fake_artifacts,
+                safe_branch_name: setup_values.safe_branch_name,
+            );
+        }
+
+        inside_container(
+            args: [
+                "--env HOME=/home/jenkins",
+            ],
+            set_docker_group_id: true,
+            ulimit_nofile: 1024,
+            mount_credentials: true,
+            privileged: true,
+        ) {
+            try {
+                stage("Run `make ${make_target}`") {
+                    dir("${checkout_dir}/tests") {
+                        single_tests.run_make_target(
+                            result_path: "${checkout_dir}/test-results/${distro}",
+                            edition: edition,
+                            docker_tag: setup_values.docker_tag,
+                            version: setup_values.cmk_version,
+                            distro: distro,
+                            branch_name: setup_values.safe_branch_name,
+                            make_target: make_target,
+                            test_filter: params.TEST_FILTER,
+                            faked_artifacts: params.FAKE_ARTIFACTS,
+                            timeout: 420,
+                        );
+                    }
+                }
+            }
+            finally {
+                stage("Archive / process test reports") {
+                    single_tests.archive_and_process_reports(test_results: "test-results/**");
+                }
+            }
+        }
+    }
+}
+
+return this;

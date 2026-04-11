d2465ebc514a9cafae031de873082a706f08b284
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-11 22:16:29 +0200

    bakery: Migrate isc_dhcpd rulespec to v1 API + modernize bakelet
    - Rewrite cmk/gui/plugins/legacy_bakery_rulesets/isc_dhcpd.py to use
      cmk.rulesets.v1 API with AgentConfig, Dictionary, CascadingSingleChoice
    - Add migrate() function to handle old True/{} → sync / None → do_not_deploy
    - Modernize bakelet to use pydantic _Config model and support cached interval
    - Update bakelet test to use new config format and add do_not_deploy case
    - Add migrate unit test at tests/unit/cmk/plugins/collection/rulesets/test_isc_dhcpd.py
    
    Change-Id: Iec8cbe0468ffb363c07956d5b247b2a59dfd3111

--- cmk/gui/plugins/legacy_bakery_rulesets/isc_dhcpd.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/isc_dhcpd.py
@@ -3,31 +3,74 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import DropdownChoice
-from cmk.utils.rulesets.definition import RuleGroup
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_isc_dhcpd() -> DropdownChoice[dict[str, object]]:
-    return DropdownChoice(
-        title=_("ISC DHCP-Daemon (Linux)"),
-        help=_(
-            "The plug-in <tt>isc_dhcpd</tt> collects information about a DHCP server daemon on Linux."
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict) and "deployment" in value:
+        return value
+    if value is None:
+        return {"deployment": ("do_not_deploy", None)}
+    return {"deployment": ("sync", None)}
+
+
+def _valuespec_agent_config_isc_dhcpd() -> Dictionary:
+    return Dictionary(
+        help_text=Help(
+            "The plug-in <tt>isc_dhcpd</tt> collects information about a DHCP server"
+            " daemon on Linux."
         ),
-        choices=[
-            ({}, _("Deploy ISC DHCP-Daemon plug-in")),
-            (None, _("Do not deploy ISC DHCP-Daemon plug-in")),
-        ],
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name=RuleGroup.AgentConfig("isc_dhcpd"),
-        valuespec=_valuespec_agent_config_isc_dhcpd,
-    )
+rule_spec_isc_dhcpd = AgentConfig(
+    title=Title("ISC DHCP-Daemon (Linux)"),
+    name="isc_dhcpd",
+    topic=Topic.NETWORKING,
+    parameter_form=_valuespec_agent_config_isc_dhcpd,
 )

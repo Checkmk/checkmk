25602e0372d95e72888523983f72c819c063567a
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-13 07:59:30 +0200

    bakery: Migrate mk_logins rulespec to v1 API + modernize bakelet
    Change-Id: If2e1de134910de9cb73c9b90caa9d6b8deef8215

--- cmk/gui/plugins/legacy_bakery_rulesets/mk_logins.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/mk_logins.py
@@ -3,29 +3,72 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import DropdownChoice
-from cmk.utils.rulesets.definition import RuleGroup
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_mk_logins() -> DropdownChoice[bool]:
-    return DropdownChoice(
-        title=_("Number of logged in users (Linux)"),
-        help=_("Hosts configured via this rule get the <tt>mk_logins</tt> plug-in deployed."),
-        choices=[
-            (True, _("Deploy logged in users plug-in")),
-            (None, _("Do not deploy logged in users plug-in")),
-        ],
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict):
+        return value
+    return {"deployment": ("sync" if value else "do_not_deploy", None)}
+
+
+def _valuespec_agent_config_mk_logins() -> Dictionary:
+    return Dictionary(
+        help_text=Help(
+            "Hosts configured via this rule get the <tt>mk_logins</tt> plug-in deployed."
+            " This plug-in counts the number of logged in users on Linux hosts."
+        ),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name=RuleGroup.AgentConfig("mk_logins"),
-        valuespec=_valuespec_agent_config_mk_logins,
-    )
+rule_spec_mk_logins = AgentConfig(
+    title=Title("Number of logged in users (Linux)"),
+    name="mk_logins",
+    topic=Topic.OPERATING_SYSTEM,
+    parameter_form=_valuespec_agent_config_mk_logins,
 )

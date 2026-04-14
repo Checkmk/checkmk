739f9567867110b985f39fe625d8d38a9c204b97
Leonardo Petrora <leonardo.petrora@checkmk.com>
2026-04-14 12:50:16 +0200

    18997 FIX "Host name or alias (regex)" filter
    The `Host name or alias (regex)` filter was using the incorrect
    Livestatus column name `alias` instead of `host_alias` when matching
    against the host alias. This caused errors on dashboards that used this
    filter. The column name has been corrected so the filter now works as
    expected.
    
    SUP-25992
    
    Change-Id: I7f0c2c26792a20a52ab77b698a97f2c69e32549f

--- tests/unit/cmk/gui/visuals/filter/test_filters.py
+++ tests/unit/cmk/gui/visuals/filter/test_filters.py
@@ -422,7 +422,7 @@ filter_tests = [
         request_vars=[
             ("hostnameoralias", "abc"),
         ],
-        expected_filters=("Filter: host_name ~~ abc\nFilter: alias ~~ abc\nOr: 2\n"),
+        expected_filters=("Filter: host_name ~~ abc\nFilter: host_alias ~~ abc\nOr: 2\n"),
     ),
     FilterTest(
         ident="hosts_having_service_problems",
@@ -573,7 +573,7 @@ filter_tests = [
     FilterTest(
         ident="hostnameoralias",
         request_vars=[("hostnameoralias", "horst")],
-        expected_filters="Filter: host_name ~~ horst\nFilter: alias ~~ horst\nOr: 2\n",
+        expected_filters="Filter: host_name ~~ horst\nFilter: host_alias ~~ horst\nOr: 2\n",
     ),
     # Testing FilterCommaSeparatedStringList
     FilterTest(

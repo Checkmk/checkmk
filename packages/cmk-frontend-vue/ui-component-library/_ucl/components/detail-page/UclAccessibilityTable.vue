1516c1bb63033ce63603d5ca516b5b7299539571
Benedikt Seidl <benedikt.seidl@checkmk.com>
2026-04-14 11:18:13 +0200

    change to a table implementation
    this way the split between Key and Descritpion columns does not have to
    be 50:50 but can be adjusted dynamically by the rendering engine.
    
    Change-Id: I83bb7b4b2f2745af7f957834d8e7d8edbd1b6db2

--- packages/cmk-frontend-vue/ui-component-library/_ucl/components/detail-page/UclAccessibilityTable.vue
+++ packages/cmk-frontend-vue/ui-component-library/_ucl/components/detail-page/UclAccessibilityTable.vue
@@ -24,82 +24,90 @@ const normalizedData = computed(() =>
 </script>
 
 <template>
-  <div class="ucl-accessibility-table__table" role="table" aria-label="Keyboard accessibility">
-    <div class="ucl-accessibility-table__table-header" role="row">
-      <CmkHeading type="h4" role="columnheader">Key</CmkHeading>
-      <CmkHeading type="h4" role="columnheader">Description</CmkHeading>
-    </div>
-    <template v-if="normalizedData.length">
-      <div
-        v-for="item in normalizedData"
-        :key="item.description"
-        class="ucl-accessibility-table__table-row"
-        role="row"
-      >
-        <div class="ucl-accessibility-table__table-cell" role="cell">
+  <table class="ucl-accessibility-table__table" aria-label="Keyboard accessibility">
+    <thead>
+      <tr>
+        <th><CmkHeading type="h4">Key</CmkHeading></th>
+        <th><CmkHeading type="h4">Description</CmkHeading></th>
+      </tr>
+    </thead>
+    <tbody v-if="normalizedData.length">
+      <tr v-for="item in normalizedData" :key="item.description">
+        <td>
           <template v-for="(group, groupIdx) in item.keys" :key="`${groupIdx}-${group.join('+')}`">
             <span v-if="groupIdx > 0" class="ucl-accessibility-table__key-separator">or</span>
-            <span class="ucl-accessibility-table__key-group">
-              <template v-for="(keyName, keyIdx) in group" :key="`${keyIdx}-${keyName}`">
-                <span class="ucl-accessibility-table__key-pill">{{ keyName }}</span>
-                <span
-                  v-if="keyIdx < group.length - 1"
-                  class="ucl-accessibility-table__key-separator"
-                  >+</span
-                >
-              </template>
+            <span v-for="(keyName, keyIdx) in group" :key="`${keyIdx}-${keyName}`">
+              <span class="ucl-accessibility-table__key-pill">{{ keyName }}</span>
+              <span v-if="keyIdx < group.length - 1" class="ucl-accessibility-table__key-separator"
+                >+</span
+              >
             </span>
           </template>
-        </div>
-        <div class="ucl-accessibility-table__table-cell" role="cell">
+        </td>
+        <td>
           {{ item.description }}
-        </div>
-      </div>
-    </template>
-    <div v-else class="ucl-accessibility-table__empty-message" role="row">
-      <div role="cell" colspan="2">No keyboard accessibility available for this component</div>
-    </div>
-  </div>
+        </td>
+      </tr>
+    </tbody>
+    <tbody v-else>
+      <tr class="ucl-accessibility-table__empty-message">
+        <td colspan="2">No keyboard accessibility available for this component</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
 .ucl-accessibility-table__table {
-  border: 1px solid var(--ucl-elements-border-color);
   background: var(--ucl-detail-section-bg-color);
-  border-radius: 8px;
   width: 100%;
   margin-bottom: var(--spacing);
-}
+  border-collapse: separate;
+  border-spacing: 0;
 
-.ucl-accessibility-table__table-header {
-  display: flex;
-  background: var(--ucl-elements-background-color);
-  border-bottom: 1px solid var(--ucl-elements-border-color);
-}
+  td,
+  th {
+    padding: 12px 16px;
+    text-align: left;
+    align-items: center;
+    color: var(--ucl-body-text-color);
+  }
 
-.ucl-accessibility-table__table-header > * {
-  flex: 1;
-  padding: 12px 16px;
-  text-align: left;
-}
+  tr th,
+  tr td {
+    border-bottom: 1px solid var(--ucl-elements-border-color);
+  }
 
-.ucl-accessibility-table__table-row {
-  display: flex;
-  border-top: 1px solid var(--ucl-elements-border-color);
-}
+  tr th {
+    border-top: 1px solid var(--ucl-elements-border-color);
+    background: var(--ucl-elements-background-color);
+  }
 
-.ucl-accessibility-table__table-cell {
-  flex: 1;
-  padding: 12px 16px;
-  display: flex;
-  align-items: center;
-  gap: var(--spacing-half);
-  color: var(--ucl-body-text-color);
-}
+  tr th:last-child,
+  tr td:last-child {
+    border-right: 1px solid var(--ucl-elements-border-color);
+  }
 
-.ucl-accessibility-table__key-group {
-  display: inline-flex;
-  align-items: center;
+  tr td:first-child,
+  tr th:first-child {
+    border-left: 1px solid var(--ucl-elements-border-color);
+  }
+
+  tr:first-child th:first-child {
+    border-top-left-radius: 8px;
+  }
+
+  tr:first-child th:last-child {
+    border-top-right-radius: 8px;
+  }
+
+  tr:last-child td:first-child {
+    border-bottom-left-radius: 8px;
+  }
+
+  tr:last-child td:last-child {
+    border-bottom-right-radius: 8px;
+  }
 }
 
 .ucl-accessibility-table__key-separator {

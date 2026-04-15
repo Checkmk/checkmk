da525842ebf30797e5880cf0bf9f11d1308e0a7a
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-15 17:30:11 +0200

    legacy check migration: nimble_volumes I
    Change-Id: I553046ec19c4dcbdd1e1f2cae215ff19e32bd5f5

--- cmk/legacy_checks/nimble_volumes.py
+++ cmk/legacy_checks/nimble_volumes.py
@@ -3,47 +3,66 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+import time
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.df import df_check_filesystem_list, FILESYSTEM_DEFAULT_PARAMS
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, startswith, StringTable
-from cmk.legacy_includes.df import df_check_filesystem_list, FILESYSTEM_DEFAULT_PARAMS
-
-check_info = {}
-
-# example output
-
-
-def discover_nimble_volumes(info):
-    for line in info:
+def discover_nimble_volumes(section: StringTable) -> DiscoveryResult:
+    for line in section:
         if line[4] == "1":
-            yield (line[1], {})
+            yield Service(item=line[1])
 
 
-def check_nimble_volumes(item, params, info):
-    for line in info:
+def check_nimble_volumes(item: str, params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    for line in section:
         if line[1] == item:
             if line[4] == "0":
-                yield 3, "Volume is offline!"
+                yield Result(state=State.UNKNOWN, summary="Volume is offline!")
                 continue
             total = int(line[2])
             free = total - int(line[3])
-            yield df_check_filesystem_list(item, params, [(item, total, free, 0)])
+            yield from df_check_filesystem_list(
+                get_value_store(),
+                item,
+                params,
+                [(item, total, free, 0)],
+                this_time=time.time(),
+            )
 
 
 def parse_nimble_volumes(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["nimble_volumes"] = LegacyCheckDefinition(
+snmp_section_nimble_volumes = SimpleSNMPSection(
     name="nimble_volumes",
-    parse_function=parse_nimble_volumes,
     detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.37447.3.1"),
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.37447.1.2.1",
         oids=["2", "3", "4", "6", "10"],
     ),
+    parse_function=parse_nimble_volumes,
+)
+
+
+check_plugin_nimble_volumes = CheckPlugin(
+    name="nimble_volumes",
     service_name="Volume %s",
     discovery_function=discover_nimble_volumes,
     check_function=check_nimble_volumes,

04e98e93a7d3d4541f1c61e2eba0d5edb846ff5d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-15 17:23:33 +0200

    legacy check migration: orion_system I
    Change-Id: Ib327a0c78d90d948f49a98d5e59984ba169b05b4

--- cmk/legacy_checks/orion_system.py
+++ cmk/legacy_checks/orion_system.py
@@ -3,22 +3,33 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, startswith
-from cmk.legacy_includes.elphase import check_elphase
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.elphase import check_elphase, ElPhase
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
-check_info = {}
+Section = dict[str, Any]
 
 
-def parse_orion_system(string_table):
+def parse_orion_system(string_table: StringTable) -> Section | None:
     if not string_table:
         return None
 
-    # states do not belong to the parsing :-(
-    map_charge_states = {
+    map_charge_states: dict[str, tuple[int, str]] = {
         "1": (0, "float charging"),
         "2": (0, "discharge"),
         "3": (0, "equalize"),
@@ -40,19 +51,17 @@ def parse_orion_system(string_table):
         system_power,
     ) = string_table[0]
 
-    temperature = dict[str, float]()
+    temperature: dict[str, float] = {}
     if battery_temp != "2147483647":
-        # From MIB: The max. value 2147483647 is used to indicate an invalid value."
         temperature["Battery"] = int(battery_temp) * 0.1
 
-    electrical = dict[str, dict[str, float]]()
+    electrical: dict[str, dict[str, float]] = {}
     for what, value, factor in [
         ("voltage", system_voltage, 0.01),
         ("current", load_current, 0.1),
         ("power", system_power, 1),
     ]:
         if value != "2147483647":
-            # From MIB: The max. value 2147483647 is used to indicate an invalid value."
             system_data = electrical.setdefault("System", {})
             system_data[what] = int(value) * factor
 
@@ -61,31 +70,34 @@ def parse_orion_system(string_table):
         ("Rectifier", rectifier_current),
     ]:
         if value != "2147483647":
-            # From MIB: The max. value 2147483647 is used to indicate an invalid value."
             item_data = electrical.setdefault(item, {})
             item_data["current"] = int(battery_temp) * 0.1
 
     return {
         "charging": {
-            "Battery": map_charge_states.get(charge_state, (3, "unknown[%s]" % charge_state))
+            "Battery": map_charge_states.get(charge_state, (3, f"unknown[{charge_state}]"))
         },
         "temperature": temperature,
         "electrical": electrical,
     }
 
 
-def discover_orion_system_temp(parsed):
-    for entity in parsed["temperature"]:
-        yield entity, {}
+def discover_orion_system_temp(section: Section) -> DiscoveryResult:
+    for entity in section["temperature"]:
+        yield Service(item=entity)
 
 
-def check_orion_system_temp(item, params, parsed):
-    if item in parsed["temperature"]:
-        return check_temperature(parsed["temperature"][item], params, "orion_system_temp.%s" % item)
-    return None
+def check_orion_system_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
+    if item in section["temperature"]:
+        yield from check_temperature(
+            section["temperature"][item],
+            params,
+            unique_name=f"orion_system_temp.{item}",
+            value_store=get_value_store(),
+        )
 
 
-check_info["orion_system"] = LegacyCheckDefinition(
+snmp_section_orion_system = SimpleSNMPSection(
     name="orion_system",
     detect=startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.20246"),
     fetch=SNMPTree(
@@ -93,26 +105,31 @@ check_info["orion_system"] = LegacyCheckDefinition(
         oids=["1", "2", "3", "4", "5", "6", "7", "8"],
     ),
     parse_function=parse_orion_system,
+)
+
+
+check_plugin_orion_system = CheckPlugin(
+    name="orion_system",
     service_name="Temperature %s",
     discovery_function=discover_orion_system_temp,
     check_function=check_orion_system_temp,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )
 
 
-def discover_orion_system_charging(parsed):
-    for entity in parsed["charging"]:
-        yield entity, {}
+def discover_orion_system_charging(section: Section) -> DiscoveryResult:
+    for entity in section["charging"]:
+        yield Service(item=entity)
 
 
-def check_orion_system_charging(item, params, parsed):
-    if item in parsed["charging"]:
-        state, state_readable = parsed["charging"][item]
-        return state, "Status: %s" % state_readable
-    return None
+def check_orion_system_charging(item: str, section: Section) -> CheckResult:
+    if item in section["charging"]:
+        state_int, state_readable = section["charging"][item]
+        yield Result(state=State(state_int), summary=f"Status: {state_readable}")
 
 
-check_info["orion_system.charging"] = LegacyCheckDefinition(
+check_plugin_orion_system_charging = CheckPlugin(
     name="orion_system_charging",
     service_name="Charge %s",
     sections=["orion_system"],
@@ -121,20 +138,23 @@ check_info["orion_system.charging"] = LegacyCheckDefinition(
 )
 
 
-def discover_orion_system_electrical(parsed):
-    for entity in parsed["electrical"]:
-        yield entity, {}
+def discover_orion_system_electrical(section: Section) -> DiscoveryResult:
+    for entity in section["electrical"]:
+        yield Service(item=entity)
 
 
-def check_orion_system_electrical(item, params, parsed):
-    return check_elphase(item, params, parsed["electrical"])
+def check_orion_system_electrical(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    yield from check_elphase(params, ElPhase.from_dict(section["electrical"].get(item, {})))
 
 
-check_info["orion_system.dc"] = LegacyCheckDefinition(
+check_plugin_orion_system_dc = CheckPlugin(
     name="orion_system_dc",
     service_name="Direct Current %s",
     sections=["orion_system"],
     discovery_function=discover_orion_system_electrical,
     check_function=check_orion_system_electrical,
     check_ruleset_name="ups_outphase",
+    check_default_parameters={},
 )

755b2919a6ed67b5ce05a1141f4ecbe875b74f5a
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-15 10:55:48 +0200

    legacy check migration: pfsense_status I
    Change-Id: I14a9b584858fc8e6594b60d4fd9e63cfc4aea49e

--- cmk/legacy_checks/pfsense_status.py
+++ cmk/legacy_checks/pfsense_status.py
@@ -3,43 +3,53 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import contains, SNMPTree, StringTable
-
-check_info = {}
-
-
-def discover_pfsense_status(info):
-    if info:
-        return [(None, None)]
-    return []
+def discover_pfsense_status(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-def check_pfsense_status(_no_item, params, info):
-    statusvar = info[0][0]
-    if statusvar == "1":
-        return 0, "Running"
-    if statusvar == "2":
-        return 2, "Not running"
-
-    raise Exception("Unknown status value %s" % statusvar)
+def check_pfsense_status(section: StringTable) -> CheckResult:
+    match section[0][0]:
+        case "1":
+            yield Result(state=State.OK, summary="Running")
+        case "2":
+            yield Result(state=State.CRIT, summary="Not running")
+        case other:
+            yield Result(state=State.UNKNOWN, summary=f"Unknown status value: {other!r}")
 
 
 def parse_pfsense_status(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["pfsense_status"] = LegacyCheckDefinition(
+snmp_section_pfsense_status = SimpleSNMPSection(
     name="pfsense_status",
-    parse_function=parse_pfsense_status,
     detect=contains(".1.3.6.1.2.1.1.1.0", "pfsense"),
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.12325.1.200.1.1",
         oids=["1"],
     ),
+    parse_function=parse_pfsense_status,
+)
+
+
+check_plugin_pfsense_status = CheckPlugin(
+    name="pfsense_status",
     service_name="pfSense Status",
     discovery_function=discover_pfsense_status,
     check_function=check_pfsense_status,

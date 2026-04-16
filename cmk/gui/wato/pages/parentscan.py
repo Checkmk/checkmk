f0ca43560bd92b8d3a5b78725534622850a22a56
Kenneth Okoh <kenneth.okoh@checkmk.com>
2026-04-16 12:16:40 +0200

    parent scan: fix help text rendering
    ... to keep column widths stable.
    
    CMK-12528
    
    Change-Id: I3bb35a84958903f4a27458dfb409f49ca2f82439

--- cmk/gui/wato/pages/parentscan.py
+++ cmk/gui/wato/pages/parentscan.py
@@ -322,6 +322,12 @@ class ModeParentScan(WatoMode):
         html.open_tr()
         html.open_td()
         html.write_text_permissive(_("Number of PING probes") + ":")
+        html.close_td()
+        html.open_td()
+        html.text_input("ping_probes", str(self._settings.ping_probes), size=2, cssclass="number")
+        html.close_td()
+        html.close_tr()
+        html.close_table()
         html.help(
             _(
                 "After a gateway has been found, Checkmk checks if it is reachable "
@@ -330,12 +336,6 @@ class ModeParentScan(WatoMode):
                 "the number of PING probes to 0."
             )
         )
-        html.close_td()
-        html.open_td()
-        html.text_input("ping_probes", str(self._settings.ping_probes), size=2, cssclass="number")
-        html.close_td()
-        html.close_tr()
-        html.close_table()
 
         # Configuring parent
         forms.section(_("Configuration"))

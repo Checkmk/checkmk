b05a65033540bdce77ba521695d9bf2bbdea5514
Leonardo Petrora <leonardo.petrora@checkmk.com>
2026-04-16 12:10:50 +0200

    Fix time zone handling for autodiscovery excluded time windows
    Checkmk allows configuring time windows during which automatic host
    discovery should not run. Due to a bug, the Checkmk server's local time
    zone was not taken into account when evaluating these windows: the
    comparison was done against UTC rather than local time. On servers not
    running in UTC, this caused the exclusion windows to be applied at the
    wrong hours.
    
    This has been fixed by using local time for the comparison.
    
    If your Checkmk server is not running in UTC and you have configured
    excluded time windows for automatic discovery, you should review those
    windows to ensure they reflect the intended local times.
    
    SUP-24991
    
    Change-Id: If01d820a610ff1326aa32cef46dd0ea81376e6bf

--- tests/unit/cmk/checkengine/discovery/test__autodiscovery.py
+++ tests/unit/cmk/checkengine/discovery/test__autodiscovery.py
@@ -4,11 +4,18 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
+import datetime
+import time
+from zoneinfo import ZoneInfo
+
+import time_machine
+
 from cmk.utils.hostaddress import HostAddress, HostName
 
 from cmk.checkengine.checking import CheckPluginName
 from cmk.checkengine.discovery._autochecks import AutocheckEntry, AutocheckServiceWithNodes
-from cmk.checkengine.discovery._autodiscovery import get_host_services_by_host_name
+from cmk.checkengine.discovery._autodiscovery import _may_rediscover, get_host_services_by_host_name
+from cmk.checkengine.discovery._filters import RediscoveryParameters
 from cmk.checkengine.discovery._utils import DiscoveredItem
 
 NODE_1 = HostAddress("node1")
@@ -380,3 +387,41 @@ def test_get_host_services_by_host_name_move_mutiple_nodes_and_autochecks() -> N
             ),
         ],
     }
+
+
+def test_may_rediscover_relies_on_time_zone_when_disallowing() -> None:
+    rediscovery_parameters = RediscoveryParameters(
+        {"excluded_time": [((9, 15), (9, 45))], "group_time": 3600},
+    )
+
+    with time_machine.travel(
+        datetime.datetime(2026, 1, 6, 9, 30, tzinfo=ZoneInfo("Europe/Berlin")),
+        tick=False,
+    ):
+        assert (
+            _may_rediscover(
+                rediscovery_parameters=rediscovery_parameters,
+                reference_time=time.mktime(time.localtime()),
+                oldest_queued=0.0,
+            )
+            == "we are currently in a disallowed time of day"
+        )
+
+
+def test_may_rediscover_relies_on_time_zone_when_allowing() -> None:
+    rediscovery_parameters = RediscoveryParameters(
+        {"excluded_time": [((9, 15), (9, 45))], "group_time": 3600},
+    )
+
+    with time_machine.travel(
+        datetime.datetime(2026, 1, 6, 10, 30, tzinfo=ZoneInfo("Europe/Berlin")),
+        tick=False,
+    ):
+        assert (
+            _may_rediscover(
+                rediscovery_parameters=rediscovery_parameters,
+                reference_time=time.mktime(time.localtime()),
+                oldest_queued=0.0,
+            )
+            == ""
+        )

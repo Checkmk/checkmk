df4962d0d41edd0e02946ab4da84852a9068a61b
Matthew Rindel <matthew.rindel@checkmk.com>
2026-04-17 15:20:27 +0200

    Legacy check migration: juniper_screenos_cpu I CMK-33770
    Change-Id: I5ec6078aec60c1563f6784d7437720718cfc3901

--- cmk/legacy_checks/juniper_screenos_cpu.py
+++ cmk/legacy_checks/juniper_screenos_cpu.py
@@ -3,45 +3,75 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import NamedTuple
 
-
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import render, SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    check_levels,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    LevelsT,
+    render,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.juniper.lib import DETECT_JUNIPER_SCREENOS
 
-check_info = {}
+
+class Section(NamedTuple):
+    util1: float
+    util15: float
 
 
-def discover_juniper_screenos_cpu(info):
-    yield None, {}
+def _migrate_levels(levels: object) -> LevelsT[float] | None:
+    # Legacy rules stored bare tuples (warn, crit); v2 API requires tagged tuples.
+    if isinstance(levels, tuple) and len(levels) == 2 and isinstance(levels[0], float):
+        return ("fixed", levels)
+    return levels  # type: ignore[return-value]
 
 
-def check_juniper_screenos_cpu(_no_item, params, info):
-    util1, util15 = map(float, info[0])
-    warn, crit = params.get("util", (None, None)) if isinstance(params, dict) else params
+def discover_juniper_screenos_cpu(section: Section) -> DiscoveryResult:
+    yield Service()
 
-    # Report 1min utilization as informational
-    yield check_levels(util1, "util1", None, human_readable_func=render.percent, infoname="1min")
 
-    # Check levels on 15min utilization
-    yield check_levels(
-        util15, "util15", (warn, crit), human_readable_func=render.percent, infoname="15min"
+def check_juniper_screenos_cpu(params: Mapping[str, object], section: Section) -> CheckResult:
+    yield from check_levels(
+        section.util1,
+        metric_name="util1",
+        render_func=render.percent,
+        label="1min",
+    )
+    yield from check_levels(
+        section.util15,
+        levels_upper=_migrate_levels(params.get("util")),
+        metric_name="util15",
+        render_func=render.percent,
+        label="15min",
     )
 
 
-def parse_juniper_screenos_cpu(string_table: StringTable) -> StringTable | None:
-    return string_table or None
+def parse_juniper_screenos_cpu(string_table: StringTable) -> Section | None:
+    if not string_table:
+        return None
+    util1, util15 = map(float, string_table[0])
+    return Section(util1=util1, util15=util15)
 
 
-check_info["juniper_screenos_cpu"] = LegacyCheckDefinition(
+snmp_section_juniper_screenos_cpu = SimpleSNMPSection(
     name="juniper_screenos_cpu",
-    parse_function=parse_juniper_screenos_cpu,
     detect=DETECT_JUNIPER_SCREENOS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3224.16.1",
         oids=["2", "4"],
     ),
+    parse_function=parse_juniper_screenos_cpu,
+)
+
+check_plugin_juniper_screenos_cpu = CheckPlugin(
+    name="juniper_screenos_cpu",
     service_name="CPU utilization",
     discovery_function=discover_juniper_screenos_cpu,
     check_function=check_juniper_screenos_cpu,

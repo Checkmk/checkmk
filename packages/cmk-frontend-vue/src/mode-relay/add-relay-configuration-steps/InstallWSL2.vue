a03c88e2576acbf3f9f3939314a730a11ade21d3
Jose Naranjo <jose.naranjo@checkmk.com>
2026-04-17 10:41:09 +0200

    relay: add Windows relay registration wizard (experimental)
    - Add windows_relay_registration_enabled flag to CreateRelay shared typing
    - Extend RELAY_SUPPORTED_OS with Windows Server 2022 and 2025
    - Read relay_windows_registration from experimental.mk in ModeCreateRelay
    - Allow cmk.utils.experimental_config in cmk.gui.nonfree.ultimate layer
    - Add Windows wizard steps: DownloadMsiInstaller, InstallWSL2, RunMsiInstaller
    - Move OS selection inline (toggle buttons below supported OS list)
    
    CMK-33352
    
    Change-Id: I9a64ecce5754192b88f2b004788147cf99deb79b

---
+++ packages/cmk-frontend-vue/src/mode-relay/add-relay-configuration-steps/InstallWSL2.vue
@@ -0,0 +1,51 @@
+<!--
+Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+
+<script setup lang="ts">
+import usei18n from '@/lib/i18n'
+
+import CmkAlertBox from '@/components/CmkAlertBox.vue'
+import CmkCode from '@/components/CmkCode.vue'
+import { CmkWizardButton, CmkWizardStep } from '@/components/CmkWizard'
+import type { CmkWizardStepProps } from '@/components/CmkWizard'
+import CmkHeading from '@/components/typography/CmkHeading.vue'
+import CmkParagraph from '@/components/typography/CmkParagraph.vue'
+
+const { _t } = usei18n()
+
+defineProps<CmkWizardStepProps>()
+
+// TODO: Verify this command is correct once the actual MSI installer is ready.
+const installCommand = 'wsl --install'
+</script>
+
+<template>
+  <CmkWizardStep :index="index" :is-completed="isCompleted">
+    <template #header>
+      <CmkHeading type="h2">{{ _t('Install WSL2') }}</CmkHeading>
+    </template>
+
+    <template #content>
+      <CmkParagraph>
+        {{
+          _t(
+            'The Relay on Windows runs inside WSL2 (Windows Subsystem for Linux 2). ' +
+              'Run the command below in an elevated PowerShell or Command Prompt to install WSL2.'
+          )
+        }}
+      </CmkParagraph>
+      <CmkCode :code_txt="installCommand" data-testid="install-wsl2-command"></CmkCode>
+      <CmkAlertBox variant="info">
+        {{ _t('A system reboot may be required after installing WSL2 before proceeding.') }}
+      </CmkAlertBox>
+    </template>
+
+    <template #actions>
+      <CmkWizardButton type="next" />
+      <CmkWizardButton type="previous" />
+    </template>
+  </CmkWizardStep>
+</template>

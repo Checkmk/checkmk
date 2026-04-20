02988dbc30c6348575e36be864099e02b9ce3564
Moritz Kirschner <moritz.kirschner@checkmk.com>
2026-04-20 11:50:19 +0200

    19821 Fix dropdown stretching in certain instances
    Previously, the dropdown suggestions would stretch horizontally to fill
    the full width of the page. This has been fixed.
    
    CMK-28474
    
    Change-Id: Ia8a2221c1efa6ef0390b52ee83206e1f5908639e

--- packages/cmk-frontend-vue/src/components/CmkDropdown/CmkDropdown.vue
+++ packages/cmk-frontend-vue/src/components/CmkDropdown/CmkDropdown.vue
@@ -234,6 +234,7 @@ const group = computed<ButtonVariants['group']>(() => {
   display: inline-block;
   position: relative;
   white-space: nowrap;
+  align-self: flex-start;
 
   .cmk-dropdown--arrow {
     flex-shrink: 0;

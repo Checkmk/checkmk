c2d3a1654861eb519ce22598d735d5ab63fbd9df
Hartmut Leister <hartmut.leister@checkmk.com>
2026-04-23 06:24:42 +0200

    security: add .py extension to bin/cmk-cert and bin/cmk-passwd
    CMK-25064
    
    Renames bin/cmk-cert and bin/cmk-passwd to their .py variants with
    Bazel rename-on-deploy, so the site-installed filenames stay
    unchanged. The existing cmk_cert_py and cmk_passwd_py py_library
    targets already declare proper deps; only their srcs and the
    pkg_files entries needed updating.
    
    Adds astrein cmk-module-layer-violation suppressions on the two
    cross-layer imports, matching the pattern of the other entry-point
    scripts in this chain. Completes the rename the component owner had
    partially started.
    
    Change-Id: I9f454812308f45e829b7502579abcadd78f7bb3d

--- bin/cmk-passwd.py
+++ bin/cmk-passwd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# Copyright (C) 2022 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
 import sys
 
-from cmk.gui.cmkcert.main import main
+from cmk.cmkpasswd.main import main  # astrein: disable=cmk-module-layer-violation
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))

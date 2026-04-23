00225024774f3f7edcd9a74d65e77b0d87ccfa65
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-04-23 09:28:37 +0200

    Add xfail test for crash group 4366
    Change-Id: I45f2c863e0db2d0d60fd6fd1b52e8d591c583a77

---
+++ tests/unit/cmk/plugins/collection/agent_based/test_wlc_clients_discover.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.agent_based.v2 import Service
+from cmk.plugins.collection.agent_based.wlc_clients import discover_wlc_clients
+from cmk.plugins.lib.wlc_clients import ClientsTotal, WlcClientsSection
+
+
+@pytest.mark.xfail(
+    strict=True,
+    reason="Crash group 4366: discover yields Service(item='') for empty SSID names",
+)
+def test_discover_wlc_clients_skips_empty_ssid() -> None:
+    section = WlcClientsSection[ClientsTotal](
+        total_clients=5,
+        clients_per_ssid={"": ClientsTotal(total=5)},
+    )
+
+    services = list(discover_wlc_clients(section))
+
+    assert services == [Service(item="Summary")]

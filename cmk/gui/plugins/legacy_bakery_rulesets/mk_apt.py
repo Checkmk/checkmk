3bf2b50f3653236b4ddf7b2570d182617a8c2967
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-27 14:00:23 +0200

    Revert "bakery: Migrate mk_apt rulespec to v1 API + modernize bakelet"
    This reverts commit b5e5f2ec022678482817e13e91581d6c1c6e0291.
    
    Change-Id: I389450dc9ba2fe669bdb20e15fb109a794e5dbac

--- cmk/gui/plugins/legacy_bakery_rulesets/mk_apt.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/mk_apt.py
@@ -3,104 +3,61 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from collections.abc import Mapping
 
-from cmk.rulesets.v1 import Help, Title
-from cmk.rulesets.v1.form_specs import (
-    BooleanChoice,
-    CascadingSingleChoice,
-    CascadingSingleChoiceElement,
-    DefaultValue,
-    DictElement,
-    Dictionary,
-    FixedValue,
-    SingleChoice,
-    SingleChoiceElement,
-    TimeMagnitude,
-    TimeSpan,
-)
-from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
+from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
+from cmk.gui.i18n import _
+from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
+from cmk.gui.valuespec import Age, Alternative, Checkbox, Dictionary, DropdownChoice, FixedValue
+from cmk.utils.rulesets.definition import RuleGroup
 
 
-def migrate(value: object) -> Mapping[str, object]:
-    if value is None:
-        return {"deployment": ("do_not_deploy", None)}
-    if not isinstance(value, dict):
-        raise ValueError(f"Unexpected value: {value!r}")
-    if "deployment" in value:
-        return value
-    interval = value["interval"]
-    return {
-        "deployment": ("cached", float(interval)) if interval else ("sync", None),
-        "method": value["method"],
-        "update": value["update"],
-    }
-
-
-def _valuespec_agent_config_mk_apt() -> Dictionary:
-    return Dictionary(
-        help_text=Help(
+def _valuespec_agent_config_mk_apt() -> Alternative:
+    return Alternative(
+        title=_("APT normal and security updates (Linux)"),
+        help=_(
             "This will deploy the agent plug-in <tt>mk_apt</tt>. This will activate the "
-            "check <tt>apt</tt> on DEB-based hosts (like Debian and Ubuntu) and monitor "
-            "pending normal and security updates."
+            "check <tt>apt</tt> on DEB-based hosts (like Debian and Ubuntu) and monitor pending normal and security updates."
         ),
-        elements={
-            "deployment": DictElement(
-                required=True,
-                parameter_form=CascadingSingleChoice(
-                    title=Title("Deployment type"),
-                    elements=(
-                        CascadingSingleChoiceElement(
-                            name="sync",
-                            title=Title("Deploy the plug-in and run it synchronously"),
-                            parameter_form=FixedValue(value=None),
-                        ),
-                        CascadingSingleChoiceElement(
-                            name="cached",
-                            title=Title("Deploy the plug-in and run it asynchronously"),
-                            parameter_form=TimeSpan(
-                                displayed_magnitudes=(
-                                    TimeMagnitude.HOUR,
-                                    TimeMagnitude.MINUTE,
-                                ),
-                                prefill=DefaultValue(24 * 3600),
-                            ),
-                        ),
-                        CascadingSingleChoiceElement(
-                            name="do_not_deploy",
-                            title=Title("Do not deploy the plug-in"),
-                            parameter_form=FixedValue(value=None),
+        elements=[
+            Dictionary(
+                title=_("Deploy the APT plug-in"),
+                elements=[
+                    (
+                        "interval",
+                        Age(title="Interval for checking for updates"),
+                    ),
+                    (
+                        "method",
+                        DropdownChoice(
+                            title=_("Method"),
+                            choices=[
+                                ("upgrade", _("apt-get upgrade")),
+                                ("dist-upgrade", _("apt-get dist-upgrade")),
+                            ],
                         ),
                     ),
-                    prefill=DefaultValue("cached"),
-                ),
-            ),
-            "method": DictElement(
-                parameter_form=SingleChoice(
-                    title=Title("Method"),
-                    elements=[
-                        SingleChoiceElement(name="upgrade", title=Title("apt-get upgrade")),
-                        SingleChoiceElement(
-                            name="dist-upgrade", title=Title("apt-get dist-upgrade")
+                    (
+                        "update",
+                        Checkbox(
+                            title=_("Update package database"),
+                            label=_("Do an <tt>apt-get update</tt> before the check"),
                         ),
-                    ],
-                    prefill=DefaultValue("upgrade"),
-                ),
+                    ),
+                ],
+                optional_keys=False,
             ),
-            "update": DictElement(
-                parameter_form=BooleanChoice(
-                    title=Title("Update package database"),
-                    prefill=DefaultValue(True),
-                ),
+            FixedValue(
+                value=None, title=_("Do not deploy the APT plug-in"), totext=_("(disabled)")
             ),
-        },
-        migrate=migrate,
+        ],
+        default_value={"interval": 86400, "method": "upgrade", "update": True},
     )
 
 
-rule_spec_mk_apt = AgentConfig(
-    title=Title("APT normal and security updates (Linux)"),
-    name="mk_apt",
-    topic=Topic.OPERATING_SYSTEM,
-    parameter_form=_valuespec_agent_config_mk_apt,
+rulespec_registry.register(
+    HostRulespec(
+        group=RulespecGroupMonitoringAgentsAgentPlugins,
+        name=RuleGroup.AgentConfig("mk_apt"),
+        valuespec=_valuespec_agent_config_mk_apt,
+    )
 )

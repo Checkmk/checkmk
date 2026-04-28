1c2f6c284c4f77f5dcac83f92a8979e8e82a7cd7
Makeila Lundy <makeila.lundy@checkmk.com>
2026-04-28 12:24:30 +0200

    strip license header from displayed code examples
    keeps the code example clean and easier to read in ucl
    
    CMK-33884
    
    Change-Id: I122bc3469dfc1275906a7af6b618c4430ca0ffb4

--- packages/cmk-frontend-vue/ui-component-library/_ucl/components/detail-page/UclDetailPageCodeExample.vue
+++ packages/cmk-frontend-vue/ui-component-library/_ucl/components/detail-page/UclDetailPageCodeExample.vue
@@ -4,18 +4,22 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import CmkCode from '@/components/CmkCode.vue'
 import CmkHeading from '@/components/typography/CmkHeading.vue'
 
-defineProps<{
+const props = defineProps<{
   code: string
 }>()
+
+const displayCode = computed(() => props.code.replace(/^<!--[\s\S]*?-->\n?/, '').trimStart())
 </script>
 
 <template>
   <div class="ucl-detail-page-code-example__section">
     <CmkHeading type="h2">Code example</CmkHeading>
-    <CmkCode :code_txt="code" width="fill" />
+    <CmkCode :code_txt="displayCode" width="fill" />
   </div>
 </template>
 

c528200a28d1706cf6e7786a4863fa2114305312
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-04-28 22:25:10 +0200

    Migrate win_dhcp_pools bakery rulespec to rulesets v1 API
    Change-Id: I6ad3294efed71a726666072fb6093affc44404cb

--- cmk/gui/plugins/legacy_bakery_rulesets/win_dhcp_pools.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/win_dhcp_pools.py
@@ -3,28 +3,69 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import DropdownChoice
-from cmk.utils.rulesets.definition import RuleGroup
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_win_dhcp_pools() -> DropdownChoice[bool]:
-    return DropdownChoice(
-        title=_("DHCP pools (Windows)"),
-        choices=[
-            (True, _("Deploy plug-in for Windows DHCP pools")),
-            (None, _("Do not deploy plug-in for Windows DHCP pools")),
-        ],
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict):
+        return value
+    return {"deployment": ("sync" if value else "do_not_deploy", None)}
+
+
+def _form_spec_agent_config_win_dhcp_pools() -> Dictionary:
+    return Dictionary(
+        help_text=Help("This plug-in monitors DHCP pool utilization on Windows hosts."),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
+            ),
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name=RuleGroup.AgentConfig("win_dhcp_pools"),
-        valuespec=_valuespec_agent_config_win_dhcp_pools,
-    )
+rule_spec_win_dhcp_pools = AgentConfig(
+    title=Title("DHCP pools (Windows)"),
+    name="win_dhcp_pools",
+    topic=Topic.NETWORKING,
+    parameter_form=_form_spec_agent_config_win_dhcp_pools,
 )
--- tests/unit/cmk/base/plugins/bakery/test_win_dhcp_pools.py
+++ tests/unit/cmk/base/plugins/bakery/test_win_dhcp_pools.py
@@ -9,8 +9,13 @@ from cmk.bakery.v1 import OS, Plugin
 from cmk.base.plugins.bakery.win_dhcp_pools import get_win_dhcp_pools_files
 
 
-def test_win_dhcp_pools_files() -> None:
-    result = list(get_win_dhcp_pools_files(None))
+def test_win_dhcp_pools_files_enabled() -> None:
+    result = list(get_win_dhcp_pools_files({"deployment": ("sync", None)}))
     assert result == [
-        Plugin(base_os=OS.WINDOWS, source=Path("win_dhcp_pools.bat")),
+        Plugin(base_os=OS.WINDOWS, source=Path("win_dhcp_pools.bat"), interval=None),
     ]
+
+
+def test_win_dhcp_pools_files_disabled() -> None:
+    result = list(get_win_dhcp_pools_files({"deployment": ("do_not_deploy", None)}))
+    assert result == []

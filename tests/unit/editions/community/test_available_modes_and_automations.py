7d7edad3c8022082ae1b1fe92148883077204edb
Sven Panne <sven.panne@checkmk.com>
2026-04-29 13:28:33 +0200

    Simplify automation-/mode-related tests
    * Don't express sets via sorted lists
    * Dictionary views have set semantics
    * Don't test the same thing twice
    * Construct the CheckmkBaseApp only once
    
    Change-Id: Ic45585ff1770fd61b910bc34bf4beb11f6c8c7ca

--- tests/unit/editions/community/test_available_modes_and_automations.py
+++ tests/unit/editions/community/test_available_modes_and_automations.py
@@ -2,14 +2,20 @@
 # Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
-from typing import Final
 
-from cmk.base.automations.automations import Automations
+import pytest
+
+from cmk.base.base_app import CheckmkBaseApp
 from cmk.base.community_app import make_app
 
 
-def test_registered_modes() -> None:
-    expected = [
+@pytest.fixture(scope="module", name="app")
+def app_fixture() -> CheckmkBaseApp:
+    return make_app()
+
+
+def test_registered_modes(app: CheckmkBaseApp) -> None:
+    expected = {
         "automation",
         "browse-man",
         "check",
@@ -39,57 +45,48 @@ def test_registered_modes() -> None:
         "update",
         "update-dns-cache",
         "version",
-    ]
-
-    assert sorted(expected) == sorted([m.long_option for m in make_app().modes._modes])
-
-
-NEEDED_AUTOMATIONS: Final = [
-    "active-check",
-    "analyse-host",
-    "analyze-host-rule-effectiveness",
-    "analyze-host-rule-matches",
-    "analyze-service-rule-matches",
-    "get-services-labels",
-    "analyse-service",
-    "create-diagnostics-dump",
-    "delete-hosts",
-    "delete-hosts-known-remote",
-    "diag-cmk-agent",
-    "diag-host",
-    "diag-snmp",
-    "diag-special-agent",
-    "autodiscovery",
-    "service-discovery",
-    "service-discovery-preview",
-    "special-agent-discovery-preview",
-    "get-agent-output",
-    "get-check-information",
-    "get-configuration",
-    "get-section-information",
-    "get-service-name",
-    "notification-analyse",
-    "notification-get-bulks",
-    "notification-replay",
-    "notification-test",
-    "ping-host",
-    "reload",
-    "rename-hosts",
-    "restart",
-    "scan-parents",
-    "set-autochecks-v2",
-    "update-dns-cache",
-    "update-host-labels",
-    "update-passwords-merged-file",
-    "find-unknown-check-parameter-rule-sets",
-]
+    }
+    assert expected == {m.long_option for m in app.modes._modes}
 
 
-def test_registered_automations() -> None:
-    assert sorted(NEEDED_AUTOMATIONS) == sorted(make_app().automations._automations.keys())
-
-
-def test_discovered_automations() -> None:
-    a = Automations()
-    a.discover()
-    assert sorted(NEEDED_AUTOMATIONS) == sorted(a._automations.keys())
+def test_registered_automations(app: CheckmkBaseApp) -> None:
+    expected = {
+        "active-check",
+        "analyse-host",
+        "analyze-host-rule-effectiveness",
+        "analyze-host-rule-matches",
+        "analyze-service-rule-matches",
+        "get-services-labels",
+        "analyse-service",
+        "create-diagnostics-dump",
+        "delete-hosts",
+        "delete-hosts-known-remote",
+        "diag-cmk-agent",
+        "diag-host",
+        "diag-snmp",
+        "diag-special-agent",
+        "autodiscovery",
+        "service-discovery",
+        "service-discovery-preview",
+        "special-agent-discovery-preview",
+        "get-agent-output",
+        "get-check-information",
+        "get-configuration",
+        "get-section-information",
+        "get-service-name",
+        "notification-analyse",
+        "notification-get-bulks",
+        "notification-replay",
+        "notification-test",
+        "ping-host",
+        "reload",
+        "rename-hosts",
+        "restart",
+        "scan-parents",
+        "set-autochecks-v2",
+        "update-dns-cache",
+        "update-host-labels",
+        "update-passwords-merged-file",
+        "find-unknown-check-parameter-rule-sets",
+    }
+    assert expected == app.automations._automations.keys()

1dd5f3f59034cafb1f325af78004a4b78c58f750
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-04-29 14:08:45 +0200

    Run nightly composition tests on k8s
    Prepare for CMK-27917
    
    Change-Id: I854de6c7e6cc0b334fe786399f3f51fe26107a07

--- buildscripts/scripts/test-composition.groovy
+++ buildscripts/scripts/test-composition.groovy
@@ -72,6 +72,9 @@ void main() {
         """.stripMargin());
 
     def relative_job_name = "${branch_base_folder}/builders/test-composition-single-f12less";
+    if (env.USE_K8S_COMPOSITION_SYSTEMTESTS == "1") {
+        relative_job_name = "${branch_base_folder}/builders/test-composition-single-f12less-k8s";
+    }
 
     /// avoid failures due to leftover artifacts from prior runs
     /// and create folder before entering containers to not delete the folder after leaving the container

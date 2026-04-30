f2b7503d517a316fa4165c39cf9b657229ff7843
David Gervasoni <david.gervasoni@checkmk.com>
2026-04-30 18:23:47 +0200

    19951 FIX Fix crash in NetApp ONTAP special agent when SnapVaults are enabled on MetroCluster systems
    Ignore `destination_svm` field for Netapp SnapMirrors
    
    SUP-28199
    
    Change-Id: I94ec811802820e471dec75dcbc72faa3f30c5301

--- packages/cmk-plugins/cmk/plugins/netapp/special_agent/agent_netapp_ontap.py
+++ packages/cmk-plugins/cmk/plugins/netapp/special_agent/agent_netapp_ontap.py
@@ -928,12 +928,11 @@ def fetch_snapmirror(
         element_data = element.to_dict()
 
         yield models.SnapMirrorModel(
-            destination_svm=element_data["destination"]["svm"]["name"],
             policy_name=element_data.get("policy", {}).get("name"),
             policy_type=element_data.get("policy", {}).get("type"),
             state=element_data.get("state"),
             transfer_state=element_data.get("transfer", {}).get("state"),
-            source_svm_name=element_data["source"]["svm"]["name"],
+            source_svm_name=element_data.get("source", {}).get("svm", {}).get("name"),
             lag_time=element_data.get("lag_time"),
             destination=element_data["destination"]["path"],
         )

0aadec1d549bda01c9581d5f25dc165cb895be4c
Matthew Rindel <matthew.rindel@checkmk.com>
2026-04-30 07:54:35 +0200

    Update Windows agent Python runtime to 3.13.13
    CMK-33667
    
    Change-Id: Ia5fa47cfa494dab1b81f6a02f1a46054dcf7290d

--- agents/wnx/scripts/os_setup/wc1.cmd
+++ agents/wnx/scripts/os_setup/wc1.cmd
@@ -13,7 +13,7 @@ powershell Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\FileSy
 
 rem python, version is latest
 set pkg=python
-set version=--version 3.12.1
+set version=--version 3.13.13
 call :process
 
 rem make, version is latest

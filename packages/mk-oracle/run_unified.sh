f38d42649d892cd4482e38cef77fc3960c8a92be
Sergey Kipnis <sergey.kipnis@checkmk.com>
2026-05-04 17:44:43 +0200

    mk-oracle: measure time
    CMK-33365
    
    Change-Id: I07ef12727b0ec63e04ca988b16fb353047fb2892

--- packages/mk-oracle/run_unified.sh
+++ packages/mk-oracle/run_unified.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # Run mk-oracle as it has been built
 # config asnd log are located in the tests/regression
+# Usage: ./run_unified.sh [--binary <path>]
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+BINARY=""
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --binary)
+            BINARY="$2"
+            shift 2
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
 # agent setup
 PKG_DIR="${SCRIPT_DIR}/tests/regression/mk-oracle"
@@ -18,4 +32,10 @@ export DB_SECTION="${DB_SECTION:-instance}"
 envsubst <"${MK_CONFDIR}/mk-oracle.yml.conf" >"${MK_CONFDIR}/mk-oracle.yml"
 
 # run
-./cargo_run run -- -c "${PKG_DIR}/etc/check_mk/mk-oracle.yml"
+if [[ -n "${BINARY}" ]]; then
+    path_to_runtime="${SCRIPT_DIR}/runtimes/plugins/packages/mk-oracle"
+    export LD_LIBRARY_PATH="${path_to_runtime}:${LD_LIBRARY_PATH}"
+    "${BINARY}" -c "${PKG_DIR}/etc/check_mk/mk-oracle.yml"
+else
+    ./cargo_run run -- -c "${PKG_DIR}/etc/check_mk/mk-oracle.yml"
+fi

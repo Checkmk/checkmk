6dd2bd140833674fc46031f3c340ab5ec3146f0c
Rebekka Seltmann <rebekka.seltmann@checkmk.com>
2026-05-05 16:31:23 +0200

    Fix test_init_scripts after metric backend was included in cloud
    CMK-32539
    
    Change-Id: I40050384975ab97df1a6e472960d7ee87243f618

--- tests/integration/omd/test_init_scripts.py
+++ tests/integration/omd/test_init_scripts.py
@@ -34,7 +34,13 @@ def test_init_scripts(site: Site) -> None:
             "mknotifyd",
         }
     if site.edition.is_ultimate_edition() or site.edition.is_ultimatemt_edition():
-        scripts |= {"otel-collector", "metric-backend"}
+        scripts |= {"otel-collector"}
+    if (
+        site.edition.is_cloud_edition()
+        or site.edition.is_ultimate_edition()
+        or site.edition.is_ultimatemt_edition()
+    ):
+        scripts |= {"metric-backend"}
     if not site.edition.is_cloud_edition():
         scripts |= {"jaeger"}
 

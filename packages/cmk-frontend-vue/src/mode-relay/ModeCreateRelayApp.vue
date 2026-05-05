c565d0a5258cf171a7732c2612587cd4d8f1009d
Jose Naranjo <jose.naranjo@checkmk.com>
2026-05-05 13:57:25 +0200

    Revert "relay: add Windows relay registration wizard (experimental)"
    This reverts commit 5bffc5630fcb4660f4a0c794f5afdd5105bcff66.
    
    Reason for revert: Made a mistake in a merge conflict
    
    Change-Id: I2d338552a78496e60273fece5af8303975f63d6e

--- packages/cmk-frontend-vue/src/mode-relay/ModeCreateRelayApp.vue
+++ packages/cmk-frontend-vue/src/mode-relay/ModeCreateRelayApp.vue
@@ -6,21 +6,17 @@ conditions defined in the file COPYING, which is part of this source code packag
 
 <script setup lang="ts">
 import type { CreateRelay } from 'cmk-shared-typing/typescript/create_relay'
-import { computed, ref, watch } from 'vue'
+import { ref } from 'vue'
 
 import usei18n from '@/lib/i18n'
 
-import CmkToggleButtonGroup from '@/components/CmkToggleButtonGroup.vue'
 import CmkWizard from '@/components/CmkWizard'
 import CmkParagraph from '@/components/typography/CmkParagraph.vue'
 
-import DownloadMsiInstaller from './add-relay-configuration-steps/DownloadMsiInstaller.vue'
 import ExecuteInstallationScript from './add-relay-configuration-steps/ExecuteInstallationScript.vue'
 import InstallPodman from './add-relay-configuration-steps/InstallPodman.vue'
 import InstallRelay from './add-relay-configuration-steps/InstallRelay.vue'
-import InstallWSL2 from './add-relay-configuration-steps/InstallWSL2.vue'
 import NameRelay from './add-relay-configuration-steps/NameRelay.vue'
-import RunMsiInstaller from './add-relay-configuration-steps/RunMsiInstaller.vue'
 import VerifyRegistration from './add-relay-configuration-steps/VerifyRegistration.vue'
 
 const { _t } = usei18n()
@@ -30,18 +26,6 @@ const props = defineProps<CreateRelay>()
 const currentStep = ref<number>(1)
 const relayAlias = ref<string>('')
 const relayId = ref<string>('')
-const selectedOs = ref<'linux' | 'windows'>('linux')
-
-const isWindows = computed(() => selectedOs.value === 'windows')
-
-watch(selectedOs, () => {
-  currentStep.value = 1
-})
-
-const osOptions = [
-  { label: _t('Linux'), value: 'linux' },
-  { label: _t('Windows'), value: 'windows' }
-]
 
 const openCreateHostPage = () => {
   const url = `${props.urls.create_host}&relayid=${relayId.value}&prefill=relay`
@@ -64,87 +48,40 @@ const openRelayOverviewPage = () => {
         <li v-for="os in props.supported_os" :key="os">{{ os }}</li>
       </ul>
     </CmkParagraph>
-
-    <CmkToggleButtonGroup
-      v-if="props.exp_add_windows_relay_enabled"
-      v-model="selectedOs"
-      :options="osOptions"
-    />
-
     <CmkWizard v-model="currentStep" mode="guided">
-      <!-- Linux path -->
-      <template v-if="!isWindows">
-        <InstallRelay
-          :domain="props.domain"
-          :site-name="props.site_name"
-          :server-port="props.server_port ?? null"
-          :index="1"
-          :is-completed="() => currentStep > 1"
-        />
-        <NameRelay
-          v-model="relayAlias"
-          :index="2"
-          :is-completed="() => currentStep > 2"
-          :alias-validation-regex="props.alias_validation.regex"
-          :alias-validation-regex-help="props.alias_validation.regex_help"
-        />
-        <InstallPodman :index="3" :is-completed="() => currentStep > 3" />
-        <ExecuteInstallationScript
-          :relay-alias="relayAlias"
-          :site-name="props.site_name"
-          :domain="props.domain"
-          :agent-receiver-port="props.agent_receiver_port"
-          :site-version="props.site_version"
-          :index="4"
-          :is-completed="() => currentStep > 4"
-        />
-        <VerifyRegistration
-          v-model="relayId"
-          :relay-alias="relayAlias"
-          :documentation-url="props.urls.documentation"
-          :index="5"
-          :is-completed="() => currentStep > 5"
-          @open-create-host-page="openCreateHostPage"
-          @open-relay-overview-page="() => openRelayOverviewPage()"
-        />
-      </template>
-
-      <!-- Windows path -->
-      <template v-else>
-        <DownloadMsiInstaller
-          :domain="props.domain"
-          :site-name="props.site_name"
-          :server-port="props.server_port ?? null"
-          :index="1"
-          :is-completed="() => currentStep > 1"
-        />
-        <NameRelay
-          v-model="relayAlias"
-          :index="2"
-          :is-completed="() => currentStep > 2"
-          :alias-validation-regex="props.alias_validation.regex"
-          :alias-validation-regex-help="props.alias_validation.regex_help"
-        />
-        <InstallWSL2 :index="3" :is-completed="() => currentStep > 3" />
-        <RunMsiInstaller
-          :relay-alias="relayAlias"
-          :site-name="props.site_name"
-          :domain="props.domain"
-          :agent-receiver-port="props.agent_receiver_port"
-          :site-version="props.site_version"
-          :index="4"
-          :is-completed="() => currentStep > 4"
-        />
-        <VerifyRegistration
-          v-model="relayId"
-          :relay-alias="relayAlias"
-          :documentation-url="props.urls.documentation"
-          :index="5"
-          :is-completed="() => currentStep > 5"
-          @open-create-host-page="openCreateHostPage"
-          @open-relay-overview-page="() => openRelayOverviewPage()"
-        />
-      </template>
+      <InstallRelay
+        :domain="props.domain"
+        :site-name="props.site_name"
+        :server-port="props.server_port ?? null"
+        :index="1"
+        :is-completed="() => currentStep > 1"
+      />
+      <NameRelay
+        v-model="relayAlias"
+        :index="2"
+        :is-completed="() => currentStep > 2"
+        :alias-validation-regex="props.alias_validation.regex"
+        :alias-validation-regex-help="props.alias_validation.regex_help"
+      />
+      <InstallPodman :index="3" :is-completed="() => currentStep > 3" />
+      <ExecuteInstallationScript
+        :relay-alias="relayAlias"
+        :site-name="props.site_name"
+        :domain="props.domain"
+        :agent-receiver-port="props.agent_receiver_port"
+        :site-version="props.site_version"
+        :index="4"
+        :is-completed="() => currentStep > 4"
+      />
+      <VerifyRegistration
+        v-model="relayId"
+        :relay-alias="relayAlias"
+        :documentation-url="props.urls.documentation"
+        :index="5"
+        :is-completed="() => currentStep > 5"
+        @open-create-host-page="openCreateHostPage"
+        @open-relay-overview-page="() => openRelayOverviewPage()"
+      />
     </CmkWizard>
   </div>
 </template>

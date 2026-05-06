6cb6edb27e9fdb0a4ba07c3afe1f6886e22eec28
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-05-06 16:49:16 +0200

    update-config 29: Remove tplink_mem
    Change-Id: If1a33ca9e72138eeb49c8c816963ccebf21b5bc5
    Jira: CMK-33768

--- cmk/update_config/plugins/lib/replaced_check_plugins.py
+++ cmk/update_config/plugins/lib/replaced_check_plugins.py
@@ -8,9 +8,7 @@ from collections.abc import Mapping
 from cmk.checkengine.plugins import CheckPluginName
 
 # Please keep this functionality even if we currently don't have any replaced check plugins!
-_REPLACED_CHECK_PLUGINS: dict[CheckPluginName, CheckPluginName] = {
-    CheckPluginName("tplink_mem"): CheckPluginName("memory_utilization")
-}
+_REPLACED_CHECK_PLUGINS: dict[CheckPluginName, CheckPluginName] = {}
 
 ALL_REPLACED_CHECK_PLUGINS: Mapping[CheckPluginName, CheckPluginName] = {
     **_REPLACED_CHECK_PLUGINS,

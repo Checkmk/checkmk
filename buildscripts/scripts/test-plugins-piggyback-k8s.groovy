03a259208a4695f13786fd808caac616df5caec6
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-05-06 09:15:30 +0200

    Add k8s test-plugins* jobs
    Same as in I07e28d98cef43c38a30c6350937c405babcb641d
    
    Prepare for CMK-27917
    
    Requires https://review.lan.tribe29.com/c/checkmk_ci/+/137647
    
    Change-Id: Iadab5bd5f270d78cc932fd45de94e9ca8ca43f93

---
+++ buildscripts/scripts/test-plugins-piggyback-k8s.groovy
@@ -0,0 +1,97 @@
+#!groovy
+
+/// file: test-plugins-piggyback-k8s.groovy
+
+void main() {
+    check_job_parameters([
+        ["EDITION", true],  // the testees package long edition string (e.g. 'pro')
+        ["DISTRO", true],  // the testees package distro string (e.g. 'ubuntu-24.04')
+        "USE_CASE",
+        "VERSION",
+        "FAKE_ARTIFACTS",
+        "TEST_FILTER",  // a filter string to select which tests to run
+        "CIPARAM_OVERRIDE_DOCKER_TAG_BUILD",  // the docker tag to use for building and testing, forwarded to packages build job
+    ]);
+
+    check_environment_variables([
+        "DOCKER_REGISTRY",
+    ]);
+
+    def single_tests = load("${checkout_dir}/buildscripts/scripts/utils/single_tests.groovy");
+    def helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+
+    def distro = params.DISTRO;
+    def edition = params.EDITION;
+    def fake_artifacts = params.FAKE_ARTIFACTS;
+    def use_case = (params.USE_CASE == "fips") ? params.USE_CASE : "daily_tests";
+    helper.assert_fips_testing(use_case, NODE_LABELS);
+
+    def download_dir = "package_download";
+    def test_results_dir = "test-results";
+    def make_target = "test-plugins-piggyback";
+
+    def setup_values = single_tests.common_prepare(
+        version: params.VERSION,
+        make_target: make_target,
+        docker_tag: params.CIPARAM_OVERRIDE_DOCKER_TAG_BUILD
+    );
+
+    dir("${checkout_dir}") {
+        stage("Fetch Checkmk package") {
+            single_tests.fetch_package(
+                // use the cross edition target or fall back to the value of edition
+                edition: edition,
+                distro: distro,
+                download_dir: download_dir,
+                bisect_comment: params.CIPARAM_BISECT_COMMENT,
+                fake_artifacts: fake_artifacts,
+                docker_tag: setup_values.docker_tag,
+                safe_branch_name: setup_values.safe_branch_name,
+            );
+        }
+
+        helper.execute_test([
+            // k8s specific configs
+            name: "${make_target}",
+            container_name: "this-distro-container",
+            callback: single_tests.&run_make_target_k8s,
+
+            // test environment specific configs
+            disable_hot_cache: true,
+            prepare_fake_git_overlay: true,
+            credentialsUsernameId: "cmk-credentials",
+            credentialsLocation: "/etc/.cmk-credentials",
+
+            // test specific configs
+            result_path: "${checkout_dir}/test-results/${distro}",
+            archive_pattern: "${test_results_dir}/**",
+            edition: edition,
+            docker_tag: setup_values.docker_tag,
+            version: setup_values.cmk_version,
+            distro: distro,
+            branch_name: setup_values.safe_branch_name,
+            make_target: "-C tests ${make_target}", // k8s does not allow dir()
+            test_filter: params.TEST_FILTER,
+            faked_artifacts: fake_artifacts,
+            // can hit 5min during the heavy chain runs (without wait time)
+            // using FoS of 3
+            timeout: 15,
+        ]);
+    }
+
+    stage("Process test reports") {
+        // In k8s the generated JUnit files need to be created on workspace level to avoid
+        // Cannot create directory '<JOB_NAME>/checkout/generatedJUnitFiles/<RANDOM_HASH>'
+        // See also Change-Id: Id7495a6bf311d77adec239d44be243aebb07b2cf
+        xunit([Custom(
+            customXSL: "${checkout_dir}/buildscripts/scripts/schema/pytest-xunit.xsl",
+            deleteOutputFiles: true,
+            failIfNotNew: false,
+            pattern: "checkout/test-results/**/junit.xml",
+            skipNoTestFiles: false,
+            stopProcessingIfError: true
+        )]);
+    }
+}
+
+return this;

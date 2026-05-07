647ab910438ed898c9bc5c3017fbc20e7f3114dd
Matthew Rindel <matthew.rindel@checkmk.com>
2026-05-07 17:12:13 +0200

    Legacy check migration: bluecat_command_server I
    CMK-33600
    
    Change-Id: I87a8abf73a0d58944f617b5105bdef4ee0507422

--- cmk/legacy_checks/bluecat_command_server.py
+++ cmk/legacy_checks/bluecat_command_server.py
@@ -3,42 +3,53 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.bluecat.lib import DETECT_BLUECAT
 
-check_info = {}
-
-
-def discover_bluecat_command_server(info):
-    return [(None, None)]
-
-
-def check_bluecat_command_server(item, params, info):
-    oper_state = int(info[0][0])
-    oper_states = {
-        1: "running normally",
-        2: "not running",
-        3: "currently starting",
-        4: "currently stopping",
-        5: "fault",
-    }
-    state = 0
-    if oper_state in params["oper_states"]["warning"]:
-        state = 1
-    elif oper_state in params["oper_states"]["critical"]:
-        state = 2
-    yield state, "Command Server is %s" % oper_states[oper_state]
+_OPER_STATE_MAP = {
+    1: "running normally",
+    2: "not running",
+    3: "currently starting",
+    4: "currently stopping",
+    5: "fault",
+}
 
 
 def parse_bluecat_command_server(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["bluecat_command_server"] = LegacyCheckDefinition(
+def discover_bluecat_command_server(section: StringTable) -> DiscoveryResult:
+    yield Service()
+
+
+def check_bluecat_command_server(
+    params: Mapping[str, Any],
+    section: StringTable,
+) -> CheckResult:
+    oper_state = int(section[0][0])
+    state = State.OK
+    if oper_state in params["oper_states"]["warning"]:
+        state = State.WARN
+    elif oper_state in params["oper_states"]["critical"]:
+        state = State.CRIT
+    yield Result(state=state, summary=f"Command Server is {_OPER_STATE_MAP[oper_state]}")
+
+
+snmp_section_bluecat_command_server = SimpleSNMPSection(
     name="bluecat_command_server",
     parse_function=parse_bluecat_command_server,
     detect=DETECT_BLUECAT,
@@ -46,6 +57,10 @@ check_info["bluecat_command_server"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.13315.3.1.7.2.1",
         oids=["1"],
     ),
+)
+
+check_plugin_bluecat_command_server = CheckPlugin(
+    name="bluecat_command_server",
     service_name="Command Server",
     discovery_function=discover_bluecat_command_server,
     check_function=check_bluecat_command_server,

f5d62f1c74fcba9dff64aefe1f72b6d1fbb4c9e3
Matthew Rindel <matthew.rindel@checkmk.com>
2026-05-07 17:16:57 +0200

    Legacy check migration: bluecat_ha I
    CMK-33601
    
    Change-Id: Ibf7b32bb67e07ea8852e9eee7db02aad84e2dd61

--- cmk/legacy_checks/bluecat_ha.py
+++ cmk/legacy_checks/bluecat_ha.py
@@ -3,48 +3,58 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import DiscoveryResult, Service, SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.bluecat.lib import DETECT_BLUECAT
 
-check_info = {}
+_OPER_STATE_MAP = {
+    1: "standalone",
+    2: "active",
+    3: "passiv",
+    4: "stopped",
+    5: "stopping",
+    6: "becoming active",
+    7: "becomming passive",
+    8: "fault",
+}
+
+
+def parse_bluecat_ha(string_table: StringTable) -> StringTable | None:
+    return string_table or None
 
 
 def discover_bluecat_ha(section: StringTable) -> DiscoveryResult:
     # Only add if device is not in standalone mode
-    if section and section[0][0] != "1":
+    if section[0][0] != "1":
         yield Service()
 
 
-def check_bluecat_ha(_no_item, params, info):
-    oper_state = int(info[0][0])
-    oper_states = {
-        1: "standalone",
-        2: "active",
-        3: "passiv",
-        4: "stopped",
-        5: "stopping",
-        6: "becoming active",
-        7: "becomming passive",
-        8: "fault",
-    }
-
-    state = 0
+def check_bluecat_ha(
+    params: Mapping[str, Any],
+    section: StringTable,
+) -> CheckResult:
+    oper_state = int(section[0][0])
+    state = State.OK
     if oper_state in params["oper_states"]["warning"]:
-        state = 1
+        state = State.WARN
     elif oper_state in params["oper_states"]["critical"]:
-        state = 2
-    yield state, "State is %s" % oper_states[oper_state]
+        state = State.CRIT
+    yield Result(state=state, summary=f"State is {_OPER_STATE_MAP[oper_state]}")
 
 
-def parse_bluecat_ha(string_table: StringTable) -> StringTable:
-    return string_table
-
-
-check_info["bluecat_ha"] = LegacyCheckDefinition(
+snmp_section_bluecat_ha = SimpleSNMPSection(
     name="bluecat_ha",
     parse_function=parse_bluecat_ha,
     detect=DETECT_BLUECAT,
@@ -52,6 +62,10 @@ check_info["bluecat_ha"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.13315.3.1.5.2.1",
         oids=["1"],
     ),
+)
+
+check_plugin_bluecat_ha = CheckPlugin(
+    name="bluecat_ha",
     service_name="HA State",
     discovery_function=discover_bluecat_ha,
     check_function=check_bluecat_ha,

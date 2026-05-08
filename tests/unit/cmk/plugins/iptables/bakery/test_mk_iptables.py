c4f75a76d3bd3c924c9f11ff1351954e03168410
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-05-08 09:34:17 +0200

    bakery: Migrate mk_iptables
    Change-Id: I89349784ea68f51ddbb99d731355d068d1c3009b

--- tests/unit/cmk/plugins/iptables/bakery/test_mk_iptables.py
+++ tests/unit/cmk/plugins/iptables/bakery/test_mk_iptables.py
@@ -7,8 +7,8 @@ from pathlib import Path
 
 import pytest
 
-from cmk.bakery.v1 import OS, Plugin
-from cmk.base.plugins.bakery.mk_iptables import get_mk_iptables_files
+from cmk.bakery.v2_unstable import OS, Plugin
+from cmk.plugins.iptables.bakery.mk_iptables import bakery_plugin_mk_iptables
 
 
 @pytest.mark.parametrize(
@@ -20,7 +20,7 @@ from cmk.base.plugins.bakery.mk_iptables import get_mk_iptables_files
         ),
         (
             {"deployment": ("cached", 3600.0)},
-            [Plugin(base_os=OS.LINUX, source=Path("mk_iptables"), interval=3600)],
+            [Plugin(base_os=OS.LINUX, source=Path("mk_iptables"), interval=3600.0)],
         ),
         (
             {"deployment": ("do_not_deploy", None)},
@@ -32,5 +32,6 @@ def test_mk_iptables_files(
     conf: dict[str, object],
     expected_files: list[Plugin],
 ) -> None:
-    result = list(get_mk_iptables_files(conf))
+    parsed = bakery_plugin_mk_iptables.parameter_parser(conf)
+    result = list(bakery_plugin_mk_iptables.files_function(parsed))
     assert result == expected_files

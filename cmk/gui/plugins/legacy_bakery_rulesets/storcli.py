7b828bcfa45ea1c16e50671595c0bbde29ecce25
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-08 16:12:30 +0200

    bakery: migrate storcli rulespec to v1 API
    Rewrites the storcli rulespec from the legacy GUI API to the rulesets
    v1 API. Updates the core bakelet to use pydantic _Config(BaseModel)
    with the new deployment tuple format.
    
    Old format: str (path to storcli executable) or None (don't deploy)
    New format: {"deployment": ("sync"|"cached"|"do_not_deploy", ...), "storcli_path": str}
    
    Change-Id: I7640d70ecc5442cdec8800d73f5ddad6759ae79b

--- cmk/gui/plugins/legacy_bakery_rulesets/storcli.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/storcli.py
@@ -3,39 +3,84 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import Alternative, FixedValue, TextInput
-from cmk.utils.rulesets.definition import RuleGroup
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    String,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_storcli() -> Alternative:
-    return Alternative(
-        title=_("LSI Raid Controller Status (via StorCLI)"),
-        help=_(
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict) and "deployment" in value:
+        return value
+    if value is None:
+        return {"deployment": ("do_not_deploy", None)}
+    if isinstance(value, str):
+        return {"deployment": ("sync", None), "storcli_path": value}
+    raise ValueError(f"Unexpected value: {value!r}")
+
+
+def _form_spec() -> Dictionary:
+    return Dictionary(
+        help_text=Help(
             "This plug-in collects information on the logical volumes and physical disks "
             "of LSI RAID controllers using the StorCLI utility. StorCLI must be installed "
             "on the target system for this plug-in to work."
         ),
-        elements=[
-            TextInput(
-                title=_("Deploy StorCLI plug-in"),
-                default_value=r"C:\Program Files\StorCLI\storcli64.exe",
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the StorCLI plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                )
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the StorCLI plug-in"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                    ),
+                    prefill=DefaultValue("sync"),
+                ),
             ),
-            FixedValue(
-                value=None, title=_("Do not deploy the StorCLI plug-in"), totext=_("(disabled)")
+            "storcli_path": DictElement(
+                parameter_form=String(
+                    title=Title("Path to StorCLI executable"),
+                    prefill=DefaultValue(r"C:\Program Files\StorCLI\storcli64.exe"),
+                ),
             ),
-        ],
-        default_value=None,
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name=RuleGroup.AgentConfig("storcli"),
-        valuespec=_valuespec_agent_config_storcli,
-    )
+rule_spec_storcli = AgentConfig(
+    title=Title("LSI Raid Controller Status (via StorCLI)"),
+    name="storcli",
+    topic=Topic.STORAGE,
+    parameter_form=_form_spec,
 )

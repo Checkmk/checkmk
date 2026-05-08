97120bf277130a4fcc1be15ecd897f1bf91f7694
Logan Connolly <logan.connolly@checkmk.com>
2026-05-08 16:49:53 +0200

    gui(bi): remove internal helpers from public api
    Change-Id: I5a5f304aaca2aba4ef89f5177e1e9b2703ddaef9

--- cmk/gui/bi/__init__.py
+++ cmk/gui/bi/__init__.py
@@ -4,9 +4,6 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from ._compiler import is_part_of_aggregation as is_part_of_aggregation
-from ._packs import aggregation_group_choices as aggregation_group_choices
-from ._packs import get_aggregation_group_trees as get_aggregation_group_trees
-from ._packs import get_cached_bi_packs as get_cached_bi_packs
 from ._valuespecs import (
     bi_config_aggregation_function_registry as bi_config_aggregation_function_registry,
 )
@@ -17,8 +14,5 @@ __all__ = [
     "BIManager",
     "FoldableTreeRendererTree",
     "is_part_of_aggregation",
-    "get_aggregation_group_trees",
-    "aggregation_group_choices",
-    "get_cached_bi_packs",
     "get_bi_config_path",
 ]

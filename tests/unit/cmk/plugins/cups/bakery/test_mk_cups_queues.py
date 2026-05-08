5ca45588b3075fc7e0b34d14b0353bcb845e88be
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-05-08 09:33:21 +0200

    bakery: Migrate mk_cups_queues
    Change-Id: Ie455722e9aa1dbdcef649e963978c4adc63864f6

---
+++ tests/unit/cmk/plugins/cups/bakery/test_mk_cups_queues.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from pathlib import Path
+
+from cmk.bakery.v2_unstable import OS, Plugin
+from cmk.plugins.cups.bakery.mk_cups_queues import bakery_plugin_mk_cups_queues
+
+
+def test_mk_cups_queues_deploy_sync() -> None:
+    conf = bakery_plugin_mk_cups_queues.parameter_parser({"deployment": ("sync", None)})
+    result = list(bakery_plugin_mk_cups_queues.files_function(conf))
+    assert result == [Plugin(base_os=OS.LINUX, source=Path("mk_cups_queues"), interval=None)]
+
+
+def test_mk_cups_queues_deploy_cached() -> None:
+    conf = bakery_plugin_mk_cups_queues.parameter_parser({"deployment": ("cached", 300.0)})
+    result = list(bakery_plugin_mk_cups_queues.files_function(conf))
+    assert result == [Plugin(base_os=OS.LINUX, source=Path("mk_cups_queues"), interval=300.0)]
+
+
+def test_mk_cups_queues_do_not_deploy() -> None:
+    conf = bakery_plugin_mk_cups_queues.parameter_parser({"deployment": ("do_not_deploy", None)})
+    assert list(bakery_plugin_mk_cups_queues.files_function(conf)) == []

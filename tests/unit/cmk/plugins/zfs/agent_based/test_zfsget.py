3f5007f132769296c7f5bac6e4b58d3aec2a6b92
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-11 08:42:14 +0200

    Create cmk/plugins/zfs/ family from collection/
    Change-Id: Ie089dad2e7332ffa6e26f1f59eb2c99ecc84bff4

--- tests/unit/cmk/plugins/zfs/agent_based/test_zfsget.py
+++ tests/unit/cmk/plugins/zfs/agent_based/test_zfsget.py
@@ -8,7 +8,7 @@ from collections.abc import Mapping, Sequence
 import pytest
 
 from cmk.agent_based.v2 import Service, StringTable
-from cmk.plugins.collection.agent_based import zfsget
+from cmk.plugins.zfs.agent_based import zfsget
 
 
 @pytest.mark.parametrize(

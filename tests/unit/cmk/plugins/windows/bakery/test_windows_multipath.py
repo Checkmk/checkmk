44a6d79adedc5f5c3bce09474164a36a9268912c
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-05-11 16:23:02 +0200

    bakery: Migrate windows_multipath
    CMK-34031
    
    Change-Id: Ibe5f542de27a38767797837f91f3af0d452be9ff

---
+++ tests/unit/cmk/plugins/windows/bakery/test_windows_multipath.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from pathlib import Path
+
+import pytest
+
+from cmk.bakery.v2_unstable import OS, Plugin
+from cmk.plugins.windows.bakery.windows_multipath import bakery_plugin_windows_multipath
+
+
+@pytest.mark.parametrize(
+    "conf, expected",
+    [
+        pytest.param(
+            {"deployment": ("sync", None)},
+            [Plugin(base_os=OS.WINDOWS, source=Path("windows_multipath.ps1"), interval=None)],
+            id="sync",
+        ),
+        pytest.param(
+            {"deployment": ("do_not_deploy", None)},
+            [],
+            id="do_not_deploy",
+        ),
+    ],
+)
+def test_windows_multipath_files(conf: dict[str, object], expected: list[Plugin]) -> None:
+    parsed = bakery_plugin_windows_multipath.parameter_parser(conf)
+    result = list(bakery_plugin_windows_multipath.files_function(parsed))
+    assert result == expected

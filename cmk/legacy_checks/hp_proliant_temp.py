5d05af04f9cdc0d9e706a3337161c0bf5b8f6235
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-11 12:09:50 +0200

    Legacy check migration: hp_proliant_temp I
    Change-Id: I47cd98c8f543140cdcfabe282eec1238d0cd7a29

--- cmk/legacy_checks/hp_proliant_temp.py
+++ cmk/legacy_checks/hp_proliant_temp.py
@@ -3,19 +3,20 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
 
+from collections.abc import Sequence
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.hp_proliant.lib import DETECT, STATUS_MAP
-
-check_info = {}
-
-hp_proliant_status2nagios_map = {k: int(v) for k, v in STATUS_MAP.items()}
-
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 hp_proliant_locale = {
     1: "other",
@@ -47,48 +48,51 @@ hp_proliant_status_map = {
 }
 
 
-def parse_hp_proliant_temp(string_table: StringTable) -> StringTable:
+Section = Sequence[Sequence[str]]
+
+
+def parse_hp_proliant_temp(string_table: StringTable) -> Section:
     return string_table
 
 
-def format_hp_proliant_name(line):
+def _format_hp_proliant_name(line: Sequence[str]) -> str:
     return f"{line[0]} ({hp_proliant_locale[int(line[1])]})"
 
 
-def discover_hp_proliant_temp(info):
-    for line in info:
+def discover_hp_proliant_temp(section: Section) -> DiscoveryResult:
+    for line in section:
         if line[-1] != "1":
             # other(1): Temperature could not be determined
-            yield format_hp_proliant_name(line), {}
+            yield Service(item=_format_hp_proliant_name(line))
 
 
-def check_hp_proliant_temp(item, params, info):
-    for line in info:
-        if format_hp_proliant_name(line) == item:
-            value, threshold, status = line[2:]
+def check_hp_proliant_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
+    for line in section:
+        if _format_hp_proliant_name(line) != item:
+            continue
+        value, threshold, status = line[2:]
 
-            # This case means no threshold available and
-            # the devices' web interface displays "N/A"
-            if threshold in ("-99", "0"):
-                devlevels = None
-            else:
-                threshold = float(threshold)
-                devlevels = (threshold, threshold)
+        # This case means no threshold available and
+        # the devices' web interface displays "N/A"
+        if threshold in ("-99", "0"):
+            dev_levels = None
+        else:
+            threshold_f = float(threshold)
+            dev_levels = (threshold_f, threshold_f)
 
-            snmp_status = hp_proliant_status_map[int(status)]
+        snmp_status = hp_proliant_status_map[int(status)]
 
-            return check_temperature(
-                float(value),
-                params,
-                "hp_proliant_temp_%s" % item,
-                dev_levels=devlevels,
-                dev_status=hp_proliant_status2nagios_map[snmp_status],
-                dev_status_name="Unit: %s" % snmp_status,
-            )
-    return 3, "item not found in snmp data"
+        yield from check_temperature(
+            float(value),
+            params,
+            dev_levels=dev_levels,
+            dev_status=int(STATUS_MAP[snmp_status]),
+            dev_status_name=f"Unit: {snmp_status}",
+        )
+        return
 
 
-check_info["hp_proliant_temp"] = LegacyCheckDefinition(
+snmp_section_hp_proliant_temp = SimpleSNMPSection(
     name="hp_proliant_temp",
     parse_function=parse_hp_proliant_temp,
     detect=DETECT,
@@ -96,8 +100,14 @@ check_info["hp_proliant_temp"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.232.6.2.6.8.1",
         oids=["2", "3", "4", "5", "6"],
     ),
+)
+
+
+check_plugin_hp_proliant_temp = CheckPlugin(
+    name="hp_proliant_temp",
     service_name="Temperature %s",
     discovery_function=discover_hp_proliant_temp,
     check_function=check_hp_proliant_temp,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )

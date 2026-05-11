2e556e2d814006e4b269b0c2c4c9b3ced4de5b60
Andreas Umbreit <andreas.umbreit@checkmk.com>
2026-05-11 12:07:25 +0200

    bakery: Migrate windows_broadcom_bonding
    CMK-34031
    
    Change-Id: I68a8b06afbd0cc9e3b4c49f9a655d1bb67b57e63

--- agents/windows/plugins/windows_broadcom_bonding.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CMK_VERSION="2.6.0b1"
-echo ^<^<^<windows_broadcom_bonding^>^>^>
-
-rem Tested with BroadCom BASP v1.6.3
-wmic /namespace:\\root\BrcmBnxNS path brcm_redundancyset get caption,redundancystatus

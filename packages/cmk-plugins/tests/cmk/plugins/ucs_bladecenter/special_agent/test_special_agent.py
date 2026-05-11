604c1acc97621db802b2cd6fb4df961c3b531871
Matthew Rindel <matthew.rindel@checkmk.com>
2026-05-11 14:00:31 +0200

    ucs_bladecenter: added detection for C220M5 hardware with additional tests
    SUP-28343
    
    Change-Id: I7e4ce707dd1cc2e8e1cf3a15d65c3122c83a5887

--- packages/cmk-plugins/tests/cmk/plugins/ucs_bladecenter/special_agent/test_special_agent.py
+++ packages/cmk-plugins/tests/cmk/plugins/ucs_bladecenter/special_agent/test_special_agent.py
@@ -3,18 +3,42 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-
 from cmk.plugins.ucs_bladecenter.special_agent.agent_ucs_bladecenter import (
     B_SERIES_REGEX,
     C_SERIES_REGEX,
 )
 
 
-def test_c_series_regex() -> None:
-    assert C_SERIES_REGEX.match("HXAF240C")
+def test_c_series_regex_matches() -> None:
     assert C_SERIES_REGEX.match("UCSC")
     assert C_SERIES_REGEX.match("APIC")
+    assert C_SERIES_REGEX.match("HX220C")
+    assert C_SERIES_REGEX.match("HX240C")
+    assert C_SERIES_REGEX.match("HX480C")
+    assert C_SERIES_REGEX.match("HXAF220C")
+    assert C_SERIES_REGEX.match("HXAF240C")
+    assert C_SERIES_REGEX.match("HXAF480C")
+    assert C_SERIES_REGEX.match("BE6H")
+    assert C_SERIES_REGEX.match("BE7H")
+    assert C_SERIES_REGEX.match("BE6M")
+    assert C_SERIES_REGEX.match("BE7M")
 
 
-def test_b_series_regex() -> None:
+def test_c_series_regex_no_match() -> None:
+    assert not C_SERIES_REGEX.match("UNKNOWN")
+    assert not C_SERIES_REGEX.match("UCSB")
+    assert not C_SERIES_REGEX.match("UCSBA")
+    assert not C_SERIES_REGEX.match("")
+
+
+def test_b_series_regex_matches() -> None:
     assert B_SERIES_REGEX.match("UCSB")
+
+
+def test_b_series_regex_no_match() -> None:
+    assert not C_SERIES_REGEX.match("UNKNOWN")
+    assert not B_SERIES_REGEX.match("UCSC")
+    assert not B_SERIES_REGEX.match("APIC")
+    assert not B_SERIES_REGEX.match("UCSBA")
+    assert not B_SERIES_REGEX.match("BE6H")
+    assert not B_SERIES_REGEX.match("")

2ce300c691681c5f666ab31b02d7583e5f14cbfe
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-12 09:18:58 +0200

    Legacy check migration: hp_procurve_cpu I
    Change-Id: Ic831e1a3f54f7ca76fa2aaf4c301068d1a626e8f

--- cmk/legacy_checks/hp_procurve_cpu.py
+++ cmk/legacy_checks/hp_procurve_cpu.py
@@ -3,40 +3,52 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
 
+import time
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import any_of, contains, DiscoveryResult, Service, SNMPTree, StringTable
-from cmk.legacy_includes.cpu_util import check_cpu_util
-
-check_info = {}
-
-
-def discover_hp_procurve_cpu(string_table: StringTable) -> DiscoveryResult:
-    if len(string_table) == 1 and 0 <= int(string_table[0][0]) <= 100:
-        yield Service()
-
-
-def check_hp_procurve_cpu(item, params, info):
-    try:
-        util = int(info[0][0])
-    except (IndexError, ValueError):
-        return None
-
-    if 0 <= util <= 100:
-        return check_cpu_util(util, params)
-    return None
+from cmk.agent_based.v2 import (
+    any_of,
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.cpu_util import check_cpu_util
 
 
 def parse_hp_procurve_cpu(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["hp_procurve_cpu"] = LegacyCheckDefinition(
+def discover_hp_procurve_cpu(section: StringTable) -> DiscoveryResult:
+    if len(section) == 1 and 0 <= int(section[0][0]) <= 100:
+        yield Service()
+
+
+def check_hp_procurve_cpu(params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    try:
+        util = int(section[0][0])
+    except (IndexError, ValueError):
+        return
+
+    if 0 <= util <= 100:
+        yield from check_cpu_util(
+            util=util,
+            params=params,
+            value_store=get_value_store(),
+            this_time=time.time(),
+        )
+
+
+snmp_section_hp_procurve_cpu = SimpleSNMPSection(
     name="hp_procurve_cpu",
-    parse_function=parse_hp_procurve_cpu,
     detect=any_of(
         contains(".1.3.6.1.2.1.1.2.0", ".11.2.3.7.11"),
         contains(".1.3.6.1.2.1.1.2.0", ".11.2.3.7.8"),
@@ -45,6 +57,12 @@ check_info["hp_procurve_cpu"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.11.2.14.11.5.1.9.6",
         oids=["1"],
     ),
+    parse_function=parse_hp_procurve_cpu,
+)
+
+
+check_plugin_hp_procurve_cpu = CheckPlugin(
+    name="hp_procurve_cpu",
     service_name="CPU utilization",
     discovery_function=discover_hp_procurve_cpu,
     check_function=check_hp_procurve_cpu,

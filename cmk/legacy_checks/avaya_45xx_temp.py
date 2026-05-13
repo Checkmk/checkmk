a222f0d210e0ac75b371af539a658fe5863f7ac1
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-13 14:25:14 +0200

    Legacy check migration: avaya_45xx_temp I
    Change-Id: Ifd064a9c58e9627ed95858a3172214a0ecfd07da

--- cmk/legacy_checks/avaya_45xx_temp.py
+++ cmk/legacy_checks/avaya_45xx_temp.py
@@ -3,40 +3,54 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import contains, SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
-
-check_info = {}
-
-
-def discover_avaya_45xx_temp(info):
-    for idx, _line in enumerate(info):
-        yield str(idx), {}
-
-
-def check_avaya_45xx_temp(item, params, info):
-    for idx, temp in enumerate(info):
-        if str(idx) == item:
-            return check_temperature(float(temp[0]) / 2.0, params, "avaya_45xx_temp_%s" % item)
-    return None
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
 def parse_avaya_45xx_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["avaya_45xx_temp"] = LegacyCheckDefinition(
+def discover_avaya_45xx_temp(section: StringTable) -> DiscoveryResult:
+    for idx, _line in enumerate(section):
+        yield Service(item=str(idx))
+
+
+def check_avaya_45xx_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for idx, temp in enumerate(section):
+        if str(idx) == item:
+            yield from check_temperature(
+                float(temp[0]) / 2.0,
+                params,
+                unique_name=f"avaya_45xx_temp_{item}",
+                value_store=get_value_store(),
+            )
+            return
+
+
+snmp_section_avaya_45xx_temp = SimpleSNMPSection(
     name="avaya_45xx_temp",
-    parse_function=parse_avaya_45xx_temp,
     detect=contains(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.45.3"),
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.45.1.6.3.7.1.1.5",
         oids=["5"],
     ),
+    parse_function=parse_avaya_45xx_temp,
+)
+
+
+check_plugin_avaya_45xx_temp = CheckPlugin(
+    name="avaya_45xx_temp",
     service_name="Temperature Chassis %s",
     discovery_function=discover_avaya_45xx_temp,
     check_function=check_avaya_45xx_temp,

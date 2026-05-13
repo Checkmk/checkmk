091625a487ca7cd0a070d97a00df9515c323763d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-13 14:39:57 +0200

    Legacy check migration: avaya_chassis_temp I
    Change-Id: Idb52148957420ba46f98a277f4a708db39dadd23

--- cmk/legacy_checks/avaya_chassis_temp.py
+++ cmk/legacy_checks/avaya_chassis_temp.py
@@ -3,39 +3,51 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.avaya.lib import DETECT_AVAYA
-
-check_info = {}
-
-
-def discover_avaya_chassis_temp(info):
-    if info:
-        return [("Chassis", {})]
-    return []
-
-
-def check_avaya_chassis_temp(item, params, info):
-    return check_temperature(int(info[0][0]), params, "avaya_chassis_temp_%s" % item)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
 def parse_avaya_chassis_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["avaya_chassis_temp"] = LegacyCheckDefinition(
+def discover_avaya_chassis_temp(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service(item="Chassis")
+
+
+def check_avaya_chassis_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    yield from check_temperature(
+        int(section[0][0]),
+        params,
+        unique_name=f"avaya_chassis_temp_{item}",
+        value_store=get_value_store(),
+    )
+
+
+snmp_section_avaya_chassis_temp = SimpleSNMPSection(
     name="avaya_chassis_temp",
-    parse_function=parse_avaya_chassis_temp,
     detect=DETECT_AVAYA,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.2272.1.100.1",
         oids=["2"],
     ),
+    parse_function=parse_avaya_chassis_temp,
+)
+
+
+check_plugin_avaya_chassis_temp = CheckPlugin(
+    name="avaya_chassis_temp",
     service_name="Temperature %s",
     discovery_function=discover_avaya_chassis_temp,
     check_function=check_avaya_chassis_temp,

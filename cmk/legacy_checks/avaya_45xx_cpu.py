e3f9b5d59022430549618b5f4244c9c72586ab0c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-13 14:18:16 +0200

    Legacy check migration: avaya_45xx_cpu I
    Change-Id: Icc4f49c1b7189a485b2f505bd5c57c1b9509b540

--- cmk/legacy_checks/avaya_45xx_cpu.py
+++ cmk/legacy_checks/avaya_45xx_cpu.py
@@ -3,44 +3,58 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
-
-
 import time
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import contains, SNMPTree, StringTable
-from cmk.legacy_includes.cpu_util import check_cpu_util
-
-check_info = {}
-
-
-def discover_avaya_45xx_cpu(info):
-    for idx, _line in enumerate(info):
-        yield str(idx), {}
-
-
-def check_avaya_45xx_cpu(item, params, info):
-    now = time.time()
-    for idx, used_perc in enumerate(info):
-        if str(idx) == item:
-            return check_cpu_util(int(used_perc[0]), params, now)
-    return None
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.cpu_util import check_cpu_util
 
 
 def parse_avaya_45xx_cpu(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["avaya_45xx_cpu"] = LegacyCheckDefinition(
+def discover_avaya_45xx_cpu(section: StringTable) -> DiscoveryResult:
+    for idx, _line in enumerate(section):
+        yield Service(item=str(idx))
+
+
+def check_avaya_45xx_cpu(item: str, params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    for idx, used_perc in enumerate(section):
+        if str(idx) == item:
+            yield from check_cpu_util(
+                util=int(used_perc[0]),
+                params=params,
+                value_store=get_value_store(),
+                this_time=time.time(),
+            )
+            return
+
+
+snmp_section_avaya_45xx_cpu = SimpleSNMPSection(
     name="avaya_45xx_cpu",
-    parse_function=parse_avaya_45xx_cpu,
     detect=contains(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.45.3"),
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.45.1.6.3.8.1.1.5",
         oids=["3"],
     ),
+    parse_function=parse_avaya_45xx_cpu,
+)
+
+
+check_plugin_avaya_45xx_cpu = CheckPlugin(
+    name="avaya_45xx_cpu",
     service_name="CPU utilization CPU %s",
     discovery_function=discover_avaya_45xx_cpu,
     check_function=check_avaya_45xx_cpu,

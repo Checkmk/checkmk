b4d7ebe55875877fa9ab0042186fd159dd546c80
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-13 09:29:42 +0200

    Legacy check migration: hp_blade I
    Change-Id: I133a40c0c5fef35c5c69651f3e1bc7be8150765e

--- cmk/legacy_checks/hp_blade.py
+++ cmk/legacy_checks/hp_blade.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # Author: Lars Michelsen <lm@mathias-kettner.de>
 
 # General Status:
@@ -15,50 +13,58 @@
 # '.1.3.6.1.4.1.232.22.2.3.1.1.1.16' => 'cpqRackCommonEnclosureCondition',
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.hp_blade.lib import DETECT_HP_BLADE
 
-check_info = {}
-
-# GENERAL MAPS:
-
-hp_blade_status_map = {1: "Other", 2: "Ok", 3: "Degraded", 4: "Failed"}
-hp_blade_status2nagios_map = {
-    "Other": 2,
-    "Ok": 0,
-    "Degraded": 1,
-    "Failed": 2,
+_STATUS_MAP: dict[int, tuple[State, str]] = {
+    1: (State.CRIT, "Other"),
+    2: (State.OK, "Ok"),
+    3: (State.WARN, "Degraded"),
+    4: (State.CRIT, "Failed"),
 }
-
-
-def discover_hp_blade_general(info):
-    if len(info) > 0 and len(info[0]) > 1:
-        return [(None, None)]
-    return []
-
-
-def check_hp_blade_general(item, params, info):
-    snmp_state = hp_blade_status_map[int(info[0][1])]
-    status = hp_blade_status2nagios_map[snmp_state]
-    return (
-        status,
-        f"General Status is {snmp_state} (Firmware: {info[0][0]}, S/N: {info[0][2]})",
-    )
 
 
 def parse_hp_blade(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["hp_blade"] = LegacyCheckDefinition(
+snmp_section_hp_blade = SimpleSNMPSection(
     name="hp_blade",
-    parse_function=parse_hp_blade,
     detect=DETECT_HP_BLADE,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.232.22.2.3.1.1.1",
         oids=["8", "16", "7"],
     ),
+    parse_function=parse_hp_blade,
+)
+
+
+def discover_hp_blade_general(section: StringTable) -> DiscoveryResult:
+    if section and len(section[0]) > 1:
+        yield Service()
+
+
+def check_hp_blade_general(section: StringTable) -> CheckResult:
+    firmware, raw_state, serial = section[0]
+    state, state_readable = _STATUS_MAP[int(raw_state)]
+    yield Result(
+        state=state,
+        summary=f"General Status is {state_readable} (Firmware: {firmware}, S/N: {serial})",
+    )
+
+
+check_plugin_hp_blade = CheckPlugin(
+    name="hp_blade",
     service_name="General Status",
     discovery_function=discover_hp_blade_general,
     check_function=check_hp_blade_general,

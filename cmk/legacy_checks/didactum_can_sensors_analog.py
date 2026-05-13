70fe826db5a69a58e980ce946cde2a368371905d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-13 08:59:59 +0200

    Legacy check migration: didactum_can_sensors_analog I
    Change-Id: I48ad558b573393f0e7728792259ac49ca5778106

--- cmk/legacy_checks/didactum_can_sensors_analog.py
+++ cmk/legacy_checks/didactum_can_sensors_analog.py
@@ -3,42 +3,34 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree
-from cmk.legacy_includes.didactum import (
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    SimpleSNMPSection,
+    SNMPTree,
+)
+from cmk.plugins.didactum.lib import (
     check_didactum_sensors_humidity,
     check_didactum_sensors_temp,
     check_didactum_sensors_voltage,
+    DETECT_DIDACTUM,
     discover_didactum_sensors,
     parse_didactum_sensors,
+    Section,
 )
-from cmk.plugins.didactum.lib import DETECT_DIDACTUM
-
-check_info = {}
+from cmk.plugins.lib.temperature import TempParamType
 
 # .1.3.6.1.4.1.46501.6.2.1.5.201007 alpha-bravo_doppelboden_frischluft --> DIDACTUM-SYSTEM-MIB::ctlCANSensorsAnalogName.201007
 # .1.3.6.1.4.1.46501.6.2.1.6.201007 normal --> DIDACTUM-SYSTEM-MIB::ctlCANSensorsAnalogState.201007
 # .1.3.6.1.4.1.46501.6.2.1.7.201007 14.9 --> DIDACTUM-SYSTEM-MIB::ctlCANSensorsAnalogValue.201007
 
-#   .--temperature---------------------------------------------------------.
-#   |      _                                      _                        |
-#   |     | |_ ___ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
-#   |     | __/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _ \      |
-#   |     | ||  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | |  __/      |
-#   |      \__\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \___|      |
-#   |                       |_|                                            |
-#   +----------------------------------------------------------------------+
-#   |                              main check                              |
-#   '----------------------------------------------------------------------'
 
-
-def discover_didactum_can_sensors_analog_temp(parsed):
-    return discover_didactum_sensors(parsed, "temperature")
-
-
-check_info["didactum_can_sensors_analog"] = LegacyCheckDefinition(
+snmp_section_didactum_can_sensors_analog = SimpleSNMPSection(
     name="didactum_can_sensors_analog",
     detect=DETECT_DIDACTUM,
     fetch=SNMPTree(
@@ -46,56 +38,68 @@ check_info["didactum_can_sensors_analog"] = LegacyCheckDefinition(
         oids=["4", "5", "6", "7", "10", "11", "12", "13"],
     ),
     parse_function=parse_didactum_sensors,
-    service_name="Temperature CAN %s",
-    discovery_function=discover_didactum_can_sensors_analog_temp,
-    check_function=check_didactum_sensors_temp,
-    check_ruleset_name="temperature",
 )
 
-# .
-#   .--humidity------------------------------------------------------------.
-#   |              _                     _     _ _ _                       |
-#   |             | |__  _   _ _ __ ___ (_) __| (_) |_ _   _               |
-#   |             | '_ \| | | | '_ ` _ \| |/ _` | | __| | | |              |
-#   |             | | | | |_| | | | | | | | (_| | | |_| |_| |              |
-#   |             |_| |_|\__,_|_| |_| |_|_|\__,_|_|\__|\__, |              |
-#   |                                                  |___/               |
-#   '----------------------------------------------------------------------'
+
+def discover_didactum_can_sensors_analog_temp(section: Section) -> DiscoveryResult:
+    yield from discover_didactum_sensors(section, "temperature")
 
 
-def discover_didactum_can_sensors_analog_humid(parsed):
-    return discover_didactum_sensors(parsed, "humidity")
+def check_didactum_can_sensors_analog_temp(
+    item: str, params: TempParamType, section: Section
+) -> CheckResult:
+    yield from check_didactum_sensors_temp(
+        item, params, section, unique_name=f"didactum_can_sensors_analog_temp.{item}"
+    )
 
 
-check_info["didactum_can_sensors_analog.humidity"] = LegacyCheckDefinition(
+check_plugin_didactum_can_sensors_analog = CheckPlugin(
+    name="didactum_can_sensors_analog",
+    service_name="Temperature CAN %s",
+    discovery_function=discover_didactum_can_sensors_analog_temp,
+    check_function=check_didactum_can_sensors_analog_temp,
+    check_ruleset_name="temperature",
+    check_default_parameters={},
+)
+
+
+def discover_didactum_can_sensors_analog_humidity(section: Section) -> DiscoveryResult:
+    yield from discover_didactum_sensors(section, "humidity")
+
+
+def check_didactum_can_sensors_analog_humidity(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    yield from check_didactum_sensors_humidity(item, params, section)
+
+
+check_plugin_didactum_can_sensors_analog_humidity = CheckPlugin(
     name="didactum_can_sensors_analog_humidity",
     service_name="Humidity CAN %s",
     sections=["didactum_can_sensors_analog"],
-    discovery_function=discover_didactum_can_sensors_analog_humid,
-    check_function=check_didactum_sensors_humidity,
+    discovery_function=discover_didactum_can_sensors_analog_humidity,
+    check_function=check_didactum_can_sensors_analog_humidity,
     check_ruleset_name="humidity",
+    check_default_parameters={},
 )
 
-# .
-#   .--voltage-------------------------------------------------------------.
-#   |                             _ _                                      |
-#   |                 __   _____ | | |_ __ _  __ _  ___                    |
-#   |                 \ \ / / _ \| | __/ _` |/ _` |/ _ \                   |
-#   |                  \ V / (_) | | || (_| | (_| |  __/                   |
-#   |                   \_/ \___/|_|\__\__,_|\__, |\___|                   |
-#   |                                        |___/                         |
-#   '----------------------------------------------------------------------'
+
+def discover_didactum_can_sensors_analog_voltage(section: Section) -> DiscoveryResult:
+    yield from discover_didactum_sensors(section, "voltage")
 
 
-def discover_didactum_can_sensors_analog_volt(parsed):
-    return discover_didactum_sensors(parsed, "voltage")
+def check_didactum_can_sensors_analog_voltage(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    yield from check_didactum_sensors_voltage(item, params, section)
 
 
-check_info["didactum_can_sensors_analog.voltage"] = LegacyCheckDefinition(
+check_plugin_didactum_can_sensors_analog_voltage = CheckPlugin(
     name="didactum_can_sensors_analog_voltage",
     service_name="Phase CAN %s",
     sections=["didactum_can_sensors_analog"],
-    discovery_function=discover_didactum_can_sensors_analog_volt,
-    check_function=check_didactum_sensors_voltage,
+    discovery_function=discover_didactum_can_sensors_analog_voltage,
+    check_function=check_didactum_can_sensors_analog_voltage,
     check_ruleset_name="el_inphase",
+    check_default_parameters={},
 )
--- cmk/plugins/didactum/lib.py
+++ cmk/plugins/didactum/lib.py
@@ -3,6 +3,132 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.agent_based.v2 import contains
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.elphase import check_elphase, ElPhase, ReadingState, ReadingWithState
+from cmk.plugins.lib.humidity import check_humidity
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 DETECT_DIDACTUM = contains(".1.3.6.1.2.1.1.1.0", "didactum")
+
+
+_STATE_MAP: Mapping[str, State] = {
+    "alarm": State.CRIT,
+    "high alarm": State.CRIT,
+    "low alarm": State.CRIT,
+    "warning": State.WARN,
+    "high warning": State.WARN,
+    "low warning": State.WARN,
+    "normal": State.OK,
+    "not connected": State.UNKNOWN,
+    "on": State.OK,
+    "off": State.UNKNOWN,
+}
+
+
+SensorData = Mapping[str, Any]
+Section = Mapping[str, Mapping[str, SensorData]]
+
+
+# elements (not exactly sensors!) can be:
+# temperature, analog voltage, usb-cam, reader, GSM modem, magnet,
+# smoke, unknown, induct relay, pushbutton, timer
+def parse_didactum_sensors(string_table: StringTable) -> Section:
+    parsed: dict[str, dict[str, dict[str, Any]]] = {}
+    for line in string_table:
+        ty, name, status = line[:3]
+        if status in _STATE_MAP:
+            state = _STATE_MAP[status]
+            state_readable = status
+        else:
+            state = State.UNKNOWN
+            state_readable = f"unknown[{status}]"
+
+        sensor: dict[str, Any] = {
+            "state": state,
+            "state_readable": state_readable,
+        }
+
+        if len(line) >= 4:
+            value_str = line[3]
+            if value_str.isdigit():
+                sensor["value"] = int(value_str)
+            else:
+                try:
+                    sensor["value"] = float(value_str)
+                except ValueError:
+                    sensor["value"] = value_str
+
+        if len(line) == 8:
+            crit_lower, warn_lower, warn, crit = line[4:]
+            sensor["levels"] = (float(warn), float(crit))
+            sensor["levels_lower"] = (float(warn_lower), float(crit_lower))
+
+        parsed.setdefault(ty, {}).setdefault(name, sensor)
+
+    return parsed
+
+
+def discover_didactum_sensors(section: Section, what: str) -> DiscoveryResult:
+    for sensor_name, attrs in section.get(what, {}).items():
+        if attrs["state_readable"] not in ("off", "not connected"):
+            yield Service(item=sensor_name)
+
+
+def check_didactum_sensors_temp(
+    item: str, params: TempParamType, section: Section, unique_name: str
+) -> CheckResult:
+    if (data := section.get("temperature", {}).get(item)) is None:
+        return
+    yield from check_temperature(
+        data["value"],
+        params,
+        unique_name=unique_name,
+        value_store=get_value_store(),
+        dev_levels=data["levels"],
+        dev_levels_lower=data["levels_lower"],
+        dev_status=int(data["state"]),
+        dev_status_name=data["state_readable"],
+    )
+
+
+def check_didactum_sensors_humidity(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (data := section.get("humidity", {}).get(item)) is None:
+        return
+    yield from check_humidity(data["value"], params)
+
+
+def check_didactum_sensors_voltage(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (data := section.get("voltage", {}).get(item)) is None:
+        return
+    yield from check_elphase(
+        params=params,
+        elphase=ElPhase(
+            voltage=ReadingWithState(
+                value=data["value"],
+                state=ReadingState(state=data["state"], text=data["state_readable"]),
+            ),
+        ),
+    )
+
+
+def check_didactum_sensor_status(item: str, section: Section, *types: str) -> CheckResult:
+    for ty in types:
+        if (data := section.get(ty, {}).get(item)) is not None:
+            yield Result(state=data["state"], summary=f"Status: {data['state_readable']}")
+            return

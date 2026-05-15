f1a0dab041c93b52b07cbc21ab82d9f240d7a1aa
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-15 10:57:14 +0200

    Legacy check migration: kentix_amp_sensors I
    Change-Id: I483c70694b5a97534b0c9dec82a398745e081512

--- cmk/legacy_checks/kentix_amp_sensors.py
+++ cmk/legacy_checks/kentix_amp_sensors.py
@@ -3,16 +3,26 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping, Sequence
+from typing import Any, TypedDict
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree
-from cmk.legacy_includes.humidity import check_humidity
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v1 import check_levels as check_levels_v1
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    render,
+    Result,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.kentix.lib import DETECT_KENTIX
-
-check_info = {}
+from cmk.plugins.lib.humidity import check_humidity
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 # .1.3.6.1.4.1.37954.1.2.7.1.0 RZ1SE-KLIMA-NEU  sensor name
 # .1.3.6.1.4.1.37954.1.2.7.2.0 159              temperature     INTEGER (0..1000)
@@ -26,62 +36,38 @@ check_info = {}
 # .1.3.6.1.4.1.37954.1.2.7.9.0 0                digital out     INTEGER (0..1)
 # .1.3.6.1.4.1.37954.1.2.7.10.0 0               comError        INTEGER (0..1)
 
-# parsed:
-# {'AlarmManager'   : { 'smoke': 0, 'humidity': 0.0,  'temp': 0.0 , 'leakage':0 },
-#  'RZ1SE-INNENRAUM': { 'smoke': 0, 'humidity': 35.9, 'temp': 21.8, 'leakage':1 },
-#  'RZ1SE-KLIMA-ALT': { 'smoke': 0, 'humidity': 34.4, 'temp': 22.5, 'leakage':0 },
-#  'RZ1SE-KLIMA-NEU': { 'smoke': 0, 'humidity': 47.4, 'temp': 15.9, 'leakage':0 },
-#  'RZ1SELI1'       : { 'smoke': 0, 'humidity': 35.6, 'temp': 21.6, 'leakage':0 },
-#  'RZ1SERE1'       : { 'smoke': 0, 'humidity': 47.3, 'temp': 16.7, 'leakage':0 },
-#  'RZ2AMR001'      : { 'smoke': 0, 'humidity': 36.7, 'temp': 16.6, 'leakage':0 },
-#  'RZ2SE-INNENRAUM': { 'smoke': 0, 'humidity': 34.9, 'temp': 18.3, 'leakage':0 },
-#  'RZ2SELI1'       : { 'smoke': 0, 'humidity': 41.9, 'temp': 15.1, 'leakage':0 }
-# }
+
+class Sensor(TypedDict, total=False):
+    temp: float
+    humidity: float
+    smoke: int
+    leakage: int
 
 
-def parse_kentix_amp_sensors(string_table):
+Section = Mapping[str, Sensor]
+
+
+def parse_kentix_amp_sensors(string_table: Sequence[StringTable]) -> Section:
     info_flattened = []
-
     for i in range(0, len(string_table[0]), 10):
         info_flattened.append([a[0] for a in string_table[0][i : i + 10]])
 
-    parsed = {}
+    parsed: dict[str, Sensor] = {}
     for line in info_flattened:
         if line[0] != "":
-            parsed[line[0]] = {
+            sensor: Sensor = {
                 "temp": float(line[1]) / 10,
                 "humidity": float(line[2]) / 10,
                 "smoke": int(line[4]),
             }
             if line[6] != "":
-                parsed[line[0]]["leakage"] = int(line[6])
+                sensor["leakage"] = int(line[6])
+            parsed[line[0]] = sensor
 
     return parsed
 
 
-#   .--temperature---------------------------------------------------------.
-#   |      _                                      _                        |
-#   |     | |_ ___ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
-#   |     | __/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _ \      |
-#   |     | ||  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | |  __/      |
-#   |      \__\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \___|      |
-#   |                       |_|                                            |
-#   +----------------------------------------------------------------------+
-#   |                            main check                                |
-#   '----------------------------------------------------------------------'
-
-
-def discover_kentix_amp_sensors(section):
-    yield from ((key, {}) for key in section)
-
-
-def check_kentix_amp_sensors(item, params, parsed):
-    if item in parsed:
-        return check_temperature(parsed[item]["temp"], params, "kentix_amp_sensors_%s" % item)
-    return None
-
-
-check_info["kentix_amp_sensors"] = LegacyCheckDefinition(
+snmp_section_kentix_amp_sensors = SNMPSection(
     name="kentix_amp_sensors",
     detect=DETECT_KENTIX,
     fetch=[
@@ -91,120 +77,91 @@ check_info["kentix_amp_sensors"] = LegacyCheckDefinition(
         )
     ],
     parse_function=parse_kentix_amp_sensors,
+)
+
+
+def discover_kentix_amp_sensors(section: Section) -> DiscoveryResult:
+    yield from (Service(item=key) for key in section)
+
+
+def check_kentix_amp_sensors(item: str, params: TempParamType, section: Section) -> CheckResult:
+    if (sensor := section.get(item)) is None:
+        return
+    yield from check_temperature(
+        reading=sensor["temp"],
+        params=params,
+        unique_name=f"kentix_amp_sensors_{item}",
+        value_store=get_value_store(),
+    )
+
+
+check_plugin_kentix_amp_sensors = CheckPlugin(
+    name="kentix_amp_sensors",
     service_name="Temperature %s",
     discovery_function=discover_kentix_amp_sensors,
     check_function=check_kentix_amp_sensors,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )
 
-# .
-#   .--humidity------------------------------------------------------------.
-#   |              _                     _     _ _ _                       |
-#   |             | |__  _   _ _ __ ___ (_) __| (_) |_ _   _               |
-#   |             | '_ \| | | | '_ ` _ \| |/ _` | | __| | | |              |
-#   |             | | | | |_| | | | | | | | (_| | | |_| |_| |              |
-#   |             |_| |_|\__,_|_| |_| |_|_|\__,_|_|\__|\__, |              |
-#   |                                                  |___/               |
-#   +----------------------------------------------------------------------+
+
+def check_kentix_amp_sensors_humidity(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (sensor := section.get(item)) is None:
+        return
+    yield from check_humidity(sensor["humidity"], params)
 
 
-def discover_kentix_amp_sensors_humidity(section):
-    yield from ((key, {}) for key in section)
-
-
-def check_kentix_amp_sensors_humidity(item, params, parsed):
-    if item in parsed:
-        return check_humidity(parsed[item]["humidity"], params)
-    return None
-
-
-check_info["kentix_amp_sensors.humidity"] = LegacyCheckDefinition(
+check_plugin_kentix_amp_sensors_humidity = CheckPlugin(
     name="kentix_amp_sensors_humidity",
     service_name="Humidity %s",
     sections=["kentix_amp_sensors"],
-    discovery_function=discover_kentix_amp_sensors_humidity,
+    discovery_function=discover_kentix_amp_sensors,
     check_function=check_kentix_amp_sensors_humidity,
     check_ruleset_name="humidity",
+    check_default_parameters={},
 )
 
 
-# .
-#   .--smoke---------------------------------------------------------------.
-#   |                                        _                             |
-#   |                    ___ _ __ ___   ___ | | _____                      |
-#   |                   / __| '_ ` _ \ / _ \| |/ / _ \                     |
-#   |                   \__ \ | | | | | (_) |   <  __/                     |
-#   |                   |___/_| |_| |_|\___/|_|\_\___|                     |
-#   |                                                                      |
-#   +----------------------------------------------------------------------+
+def check_kentix_amp_sensors_smoke(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (sensor := section.get(item)) is None:
+        return
+    yield from check_levels_v1(
+        sensor["smoke"],
+        metric_name="smoke_perc",
+        levels_upper=params["levels"],
+        render_func=render.percent,
+        boundaries=(0, 100),
+    )
 
 
-def discover_kentix_amp_sensors_smoke(section):
-    yield from ((key, {}) for key in section)
-
-
-def check_kentix_amp_sensors_smoke(item, params, parsed):
-    if item in parsed:
-        sensor_smoke = parsed[item]["smoke"]
-
-        warn, crit = params["levels"]
-
-        if sensor_smoke >= crit:
-            status = 2
-        elif sensor_smoke >= warn:
-            status = 1
-        else:
-            status = 0
-
-        infotext = "%.1f%%" % sensor_smoke
-
-        if status > 0:
-            infotext += f" (warn/crit at {warn:.1f}%/{crit:.1f}%)"
-
-        perfdata = [("smoke_perc", sensor_smoke, warn, crit, 0, 100)]
-
-        yield status, infotext, perfdata
-
-
-check_info["kentix_amp_sensors.smoke"] = LegacyCheckDefinition(
+check_plugin_kentix_amp_sensors_smoke = CheckPlugin(
     name="kentix_amp_sensors_smoke",
     service_name="Smoke Detector %s",
     sections=["kentix_amp_sensors"],
-    discovery_function=discover_kentix_amp_sensors_smoke,
+    discovery_function=discover_kentix_amp_sensors,
     check_function=check_kentix_amp_sensors_smoke,
     check_ruleset_name="smoke",
     check_default_parameters={"levels": (1.0, 5.0)},
 )
 
-# .
-#   .--leakage-------------------------------------------------------------.
-#   |                  _            _                                      |
-#   |                 | | ___  __ _| | ____ _  __ _  ___                   |
-#   |                 | |/ _ \/ _` | |/ / _` |/ _` |/ _ \                  |
-#   |                 | |  __/ (_| |   < (_| | (_| |  __/                  |
-#   |                 |_|\___|\__,_|_|\_\__,_|\__, |\___|                  |
-#   |                                         |___/                        |
-#   +----------------------------------------------------------------------+
+
+def check_kentix_amp_sensors_leakage(item: str, section: Section) -> CheckResult:
+    if (sensor := section.get(item)) is None:
+        return
+    if sensor.get("leakage", 0) > 0:
+        yield Result(state=State.CRIT, summary="Alarm or disconnected")
+    else:
+        yield Result(state=State.OK, summary="Connected")
 
 
-def discover_kentix_amp_sensors_leakage(section):
-    yield from ((key, {}) for key in section)
-
-
-def check_kentix_amp_sensors_leakage(item, params, parsed):
-    if item in parsed:
-        if parsed[item]["leakage"] > 0:
-            return 2, "Alarm or disconnected"
-        return 0, "Connected"
-    return None
-
-
-check_info["kentix_amp_sensors.leakage"] = LegacyCheckDefinition(
+check_plugin_kentix_amp_sensors_leakage = CheckPlugin(
     name="kentix_amp_sensors_leakage",
     service_name="Leakage %s",
     sections=["kentix_amp_sensors"],
-    discovery_function=discover_kentix_amp_sensors_leakage,
+    discovery_function=discover_kentix_amp_sensors,
     check_function=check_kentix_amp_sensors_leakage,
 )
-
-# .

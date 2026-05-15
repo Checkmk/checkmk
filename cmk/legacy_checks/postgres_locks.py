dac385a5cf8f9de621b7fc6f5da4c82fa480b24a
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-15 23:26:56 +0200

    Legacy check migration: postgres_locks I
    Change-Id: I8169947109adc4eb5928c710392b1e5483bcd343

--- cmk/legacy_checks/postgres_locks.py
+++ cmk/legacy_checks/postgres_locks.py
@@ -3,16 +3,21 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-
-# mypy: disable-error-code="var-annotated"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import IgnoreResultsError
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    IgnoreResultsError,
+    Metric,
+    Result,
+    Service,
+    State,
+)
 from cmk.plugins.postgres import lib as postgres
-
-check_info = {}
 
 # <<<postgres_locks>>>
 # [databases_start]
@@ -37,49 +42,59 @@ check_info = {}
 # ...
 
 
-def discover_postgres_locks(parsed):
-    for entry in parsed:
-        yield entry, {}
+def discover_postgres_locks(section: postgres.Section) -> DiscoveryResult:
+    for entry in section:
+        yield Service(item=entry)
 
 
-def check_postgres_locks(item, params, parsed):
-    if item not in parsed:
+def check_postgres_locks(
+    item: str, params: Mapping[str, Any], section: postgres.Section
+) -> CheckResult:
+    if item not in section:
         # In case of missing information we assume that the login into
         # the database has failed and we simply skip this check. It won't
         # switch to UNKNOWN, but will get stale.
         raise IgnoreResultsError("Login into database failed")
 
-    locks = {}
-    for element in parsed[item]:
+    locks: dict[str, int] = {}
+    for element in section[item]:
         if element["granted"]:
             locks.setdefault(element["mode"], 0)
             locks[element["mode"]] += 1
 
     shared_locks = locks.get("AccessShareLock", 0)
-    yield 0, "Access Share Locks %d" % shared_locks, [("shared_locks", shared_locks)]
+    yield Result(state=State.OK, summary=f"Access Share Locks {shared_locks}")
+    yield Metric("shared_locks", shared_locks)
 
     if "levels_shared" in params:
         warn, crit = params["levels_shared"]
         if shared_locks >= crit:
-            yield 2, "too high (Levels at %d/%d)" % (warn, crit)
+            yield Result(state=State.CRIT, summary=f"too high (Levels at {warn}/{crit})")
         elif shared_locks >= warn:
-            yield 1, "too high (Levels at %d/%d)" % (warn, crit)
+            yield Result(state=State.WARN, summary=f"too high (Levels at {warn}/{crit})")
 
     exclusive_locks = locks.get("ExclusiveLock", 0)
-    yield 0, "Exclusive Locks %d" % exclusive_locks, [("exclusive_locks", exclusive_locks)]
+    yield Result(state=State.OK, summary=f"Exclusive Locks {exclusive_locks}")
+    yield Metric("exclusive_locks", exclusive_locks)
     if "levels_exclusive" in params:
         warn, crit = params["levels_exclusive"]
         if exclusive_locks >= crit:
-            yield 2, "too high (Levels at %d/%d)" % (warn, crit)
+            yield Result(state=State.CRIT, summary=f"too high (Levels at {warn}/{crit})")
         elif exclusive_locks >= warn:
-            yield 1, "too high (Levels at %d/%d)" % (warn, crit)
+            yield Result(state=State.WARN, summary=f"too high (Levels at {warn}/{crit})")
 
 
-check_info["postgres_locks"] = LegacyCheckDefinition(
+agent_section_postgres_locks = AgentSection(
     name="postgres_locks",
     parse_function=postgres.parse_dbs,
+)
+
+
+check_plugin_postgres_locks = CheckPlugin(
+    name="postgres_locks",
     service_name="PostgreSQL Locks %s",
     discovery_function=discover_postgres_locks,
     check_function=check_postgres_locks,
     check_ruleset_name="postgres_locks",
+    check_default_parameters={},
 )

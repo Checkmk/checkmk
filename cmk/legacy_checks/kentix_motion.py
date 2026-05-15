89b1be468b6e1ca555d4d412a526c594a75832c4
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-15 11:19:04 +0200

    Legacy check migration: kentix_motion I
    Change-Id: I48f2b38d4473e6000bb8fb66802a28b0110f77af

--- cmk/legacy_checks/kentix_motion.py
+++ cmk/legacy_checks/kentix_motion.py
@@ -3,26 +3,28 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="comparison-overlap"
-
-# mypy: disable-error-code="redundant-expr"
-
-# mypy: disable-error-code="type-arg"
-
 #
 # 2017 comNET GmbH, Bjoern Mueller
 
 import time
-from collections.abc import Iterable, Mapping, Sequence
+from collections.abc import Mapping, Sequence
 from dataclasses import dataclass
 from itertools import chain
 from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Metric,
+    Result,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.kentix.lib import DETECT_KENTIX
-
-check_info = {}
 
 
 @dataclass(frozen=True)
@@ -34,7 +36,7 @@ class Sensor:
 Section = Mapping[str, Sensor]
 
 
-def parse_kentix_motion(string_table: list[StringTable]) -> Section:
+def parse_kentix_motion(string_table: Sequence[StringTable]) -> Section:
     return {
         index: Sensor(
             value=int(value),
@@ -44,51 +46,53 @@ def parse_kentix_motion(string_table: list[StringTable]) -> Section:
     }
 
 
-def discover_kentix_motion(section: Section) -> Iterable[tuple[str, dict]]:
-    yield from ((index, {}) for index in section)
+def discover_kentix_motion(section: Section) -> DiscoveryResult:
+    yield from (Service(item=index) for index in section)
 
 
-def check_kentix_motion(
-    item: str, params: Mapping[str, Any], section: Section
-) -> Iterable[tuple[int, str, list]]:
-    def test_in_period(
-        time_tuple: tuple[int, int],
-        periods: Sequence[tuple[tuple[int, int], tuple[int, int]]],
-    ) -> bool:
-        time_mins = time_tuple[0] * 60 + time_tuple[1]
-        for per in periods:
-            per_mins_low = per[0][0] * 60 + per[0][1]
-            per_mins_high = per[1][0] * 60 + per[1][1]
-            if per_mins_low <= time_mins < per_mins_high:
-                return True
-        return False
+def _test_in_period(
+    time_tuple: tuple[int, int],
+    periods: Sequence[tuple[tuple[int, int], tuple[int, int]]],
+) -> bool:
+    time_mins = time_tuple[0] * 60 + time_tuple[1]
+    for per in periods:
+        per_mins_low = per[0][0] * 60 + per[0][1]
+        per_mins_high = per[1][0] * 60 + per[1][1]
+        if per_mins_low <= time_mins < per_mins_high:
+            return True
+    return False
 
+
+_WEEKDAYS = (
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+)
+
+
+def check_kentix_motion(item: str, params: Mapping[str, Any], section: Section) -> CheckResult:
     if (sensor := section.get(item)) is None:
         return
 
-    weekdays = [
-        "monday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday",
-        "sunday",
-    ]
     today = time.localtime()
-    if params is not None and "time_periods" in params:
-        periods = params["time_periods"][weekdays[today.tm_wday]]
+    if "time_periods" in params:
+        periods = params["time_periods"][_WEEKDAYS[today.tm_wday]]
     else:
         periods = [((0, 0), (24, 0))]
 
     if sensor.value >= sensor.maximum:
-        status = 1 if test_in_period((today.tm_hour, today.tm_min), periods) else 0
-        yield status, "Motion detected", [("motion", sensor.value, sensor.maximum, None, 0, 100)]
+        state = State.WARN if _test_in_period((today.tm_hour, today.tm_min), periods) else State.OK
+        yield Result(state=state, summary="Motion detected")
     else:
-        yield 0, "No motion detected", [("motion", sensor.value, sensor.maximum, None, 0, 100)]
+        yield Result(state=State.OK, summary="No motion detected")
+    yield Metric("motion", sensor.value, levels=(sensor.maximum, None), boundaries=(0, 100))
 
 
-check_info["kentix_motion"] = LegacyCheckDefinition(
+snmp_section_kentix_motion = SNMPSection(
     name="kentix_motion",
     detect=DETECT_KENTIX,
     fetch=[
@@ -102,8 +106,14 @@ check_info["kentix_motion"] = LegacyCheckDefinition(
         ),
     ],
     parse_function=parse_kentix_motion,
+)
+
+
+check_plugin_kentix_motion = CheckPlugin(
+    name="kentix_motion",
     service_name="Motion Detector %s",
     discovery_function=discover_kentix_motion,
     check_function=check_kentix_motion,
     check_ruleset_name="motion",
+    check_default_parameters={},
 )

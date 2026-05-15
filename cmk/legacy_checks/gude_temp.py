de6c0ea7e2e07fa61900e4d29b1e583608248a33
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-15 10:49:25 +0200

    Legacy check migration: gude_temp I
    Change-Id: I82bb506e64c114299f448985f1ad91145e091671

--- cmk/legacy_checks/gude_temp.py
+++ cmk/legacy_checks/gude_temp.py
@@ -3,44 +3,53 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="type-arg"
-
-from collections.abc import Iterable, Mapping
+from collections.abc import Mapping, Sequence
 from itertools import chain
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import any_of, OIDEnd, SNMPTree, startswith, StringTable
-from cmk.legacy_includes.temperature import check_temperature, TempParamType
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    any_of,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    OIDEnd,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    startswith,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 # 19:1100, 38:822X
 # .1.3.6.1.4.1.28507.**.1.6.1.1.2.1 225 --> GUDEADS-EPC****-MIB::epc****TempSensor.1
 
-# Similar default levels in other checks
 Section = Mapping[str, float]
 
 
-def parse_gude_temp(string_table: list[StringTable]) -> Section:
+def parse_gude_temp(string_table: Sequence[StringTable]) -> Section:
     return {
         f"Sensor {index}": float(reading) / 10
         for index, reading in chain.from_iterable(string_table)
     }
 
 
-def discover_gude_temp(section: Section) -> Iterable[tuple[str, dict]]:
-    yield from ((name, {}) for name, reading in section.items() if reading != -999.9)
+def discover_gude_temp(section: Section) -> DiscoveryResult:
+    yield from (Service(item=name) for name, reading in section.items() if reading != -999.9)
 
 
-def check_gude_temp(
-    item: str, params: TempParamType, section: Section
-) -> Iterable[tuple[int, str, list]]:
+def check_gude_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
     if (reading := section.get(item)) is None:
         return
-    yield check_temperature(reading, params, "gude_temp.%s" % item)
+    yield from check_temperature(
+        reading=reading,
+        params=params,
+        unique_name=f"gude_temp.{item}",
+        value_store=get_value_store(),
+    )
 
 
-check_info["gude_temp"] = LegacyCheckDefinition(
+snmp_section_gude_temp = SNMPSection(
     name="gude_temp",
     detect=any_of(
         startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.28507.19"),
@@ -56,6 +65,11 @@ check_info["gude_temp"] = LegacyCheckDefinition(
         for table in ["19", "38", "66", "67"]
     ],
     parse_function=parse_gude_temp,
+)
+
+
+check_plugin_gude_temp = CheckPlugin(
+    name="gude_temp",
     service_name="Temperature %s",
     discovery_function=discover_gude_temp,
     check_function=check_gude_temp,

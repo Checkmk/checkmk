77be781db68eb76354ea2d94078e9d47cc8ea7be
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-15 10:09:39 +0200

    Legacy check migration: couchbase_nodes_size I
    Change-Id: I7dd19ef5b07fbbb43f55c07c7a7a5a504331b226

--- cmk/legacy_checks/couchbase_nodes_size.py
+++ cmk/legacy_checks/couchbase_nodes_size.py
@@ -3,101 +3,104 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
 
+from collections.abc import Callable, Mapping
+from typing import Any, Literal
 
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import render
-from cmk.plugins.couchbase.lib import parse_couchbase_lines
-
-check_info = {}
-
-
-def discover_couchbase_nodes_size(section):
-    yield from ((item, {}) for item in section)
-
-
-check_info["couchbase_nodes_size"] = LegacyCheckDefinition(
-    name="couchbase_nodes_size",
-    parse_function=parse_couchbase_lines,
-    discovery_function=discover_couchbase_nodes_size,
+from cmk.agent_based.v2 import (
+    AgentSection,
+    check_levels,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    render,
+    Service,
 )
+from cmk.plugins.couchbase.lib import parse_couchbase_lines, Section
 
 
-def get_couchbase_check_by_keys(key_disk, key_size):
-    def check_couchbase_nodes_size(item, params, parsed):
-        if not (data := parsed.get(item)):
+def _levels_upper(
+    levels: tuple[int, int] | None,
+) -> tuple[Literal["fixed"], tuple[int, int]] | None:
+    return ("fixed", levels) if levels is not None else None
+
+
+def discover_couchbase_nodes_size(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
+
+
+def _make_check(
+    key_disk: str, key_size: str
+) -> Callable[[str, Mapping[str, Any], Section], CheckResult]:
+    def check(item: str, params: Mapping[str, Any], section: Section) -> CheckResult:
+        if not (data := section.get(item)):
             return
         on_disk = data.get(key_disk)
         if on_disk is not None:
-            yield check_levels(
+            yield from check_levels(
                 on_disk,
-                "size_on_disk",
-                params.get("size_on_disk"),
-                human_readable_func=render.bytes,
-                infoname="Size on disk",
+                levels_upper=_levels_upper(params.get("size_on_disk")),
+                metric_name="size_on_disk",
+                render_func=render.bytes,
+                label="Size on disk",
             )
 
         size = data.get(key_size)
         if size is not None:
-            yield check_levels(
+            yield from check_levels(
                 size,
-                "data_size",
-                params.get("size"),
-                human_readable_func=render.bytes,
-                infoname="Data size",
+                levels_upper=_levels_upper(params.get("size")),
+                metric_name="data_size",
+                render_func=render.bytes,
+                label="Data size",
             )
 
-    return check_couchbase_nodes_size
+    return check
 
 
-def discover_couchbase_nodes_size_docs(section):
-    yield from ((item, {}) for item in section)
+agent_section_couchbase_nodes_size = AgentSection(
+    name="couchbase_nodes_size",
+    parse_function=parse_couchbase_lines,
+)
 
 
-check_info["couchbase_nodes_size.docs"] = LegacyCheckDefinition(
+check_plugin_couchbase_nodes_size_docs = CheckPlugin(
     name="couchbase_nodes_size_docs",
     service_name="Couchbase %s Documents",
     sections=["couchbase_nodes_size"],
-    discovery_function=discover_couchbase_nodes_size_docs,
-    check_function=get_couchbase_check_by_keys(
+    discovery_function=discover_couchbase_nodes_size,
+    check_function=_make_check(
         "couch_docs_actual_disk_size",
         "couch_docs_data_size",
     ),
     check_ruleset_name="couchbase_size_docs",
+    check_default_parameters={},
 )
 
 
-def discover_couchbase_nodes_size_spacial_views(section):
-    yield from ((item, {}) for item in section)
-
-
-check_info["couchbase_nodes_size.spacial_views"] = LegacyCheckDefinition(
+check_plugin_couchbase_nodes_size_spacial_views = CheckPlugin(
     name="couchbase_nodes_size_spacial_views",
     service_name="Couchbase %s Spacial Views",
     sections=["couchbase_nodes_size"],
-    discovery_function=discover_couchbase_nodes_size_spacial_views,
-    check_function=get_couchbase_check_by_keys(
+    discovery_function=discover_couchbase_nodes_size,
+    check_function=_make_check(
         "couch_spatial_disk_size",
         "couch_spatial_data_size",
     ),
     check_ruleset_name="couchbase_size_spacial",
+    check_default_parameters={},
 )
 
 
-def discover_couchbase_nodes_size_couch_views(section):
-    yield from ((item, {}) for item in section)
-
-
-check_info["couchbase_nodes_size.couch_views"] = LegacyCheckDefinition(
+check_plugin_couchbase_nodes_size_couch_views = CheckPlugin(
     name="couchbase_nodes_size_couch_views",
     service_name="Couchbase %s Couch Views",
     sections=["couchbase_nodes_size"],
-    discovery_function=discover_couchbase_nodes_size_couch_views,
-    check_function=get_couchbase_check_by_keys(
+    discovery_function=discover_couchbase_nodes_size,
+    check_function=_make_check(
         "couch_views_actual_disk_size",
         "couch_views_data_size",
     ),
     check_ruleset_name="couchbase_size_couch",
+    check_default_parameters={},
 )

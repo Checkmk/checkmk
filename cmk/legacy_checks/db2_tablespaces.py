3c3ea0b481257f52726a0ff21c04a82fe8ec722d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-16 15:37:53 +0200

    Legacy check migration: db2_tablespaces I
    Change-Id: Id6ee6f0e343e29e37e5462eee25e09987d8570d6

--- cmk/legacy_checks/db2_tablespaces.py
+++ cmk/legacy_checks/db2_tablespaces.py
@@ -3,16 +3,24 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
 
+from collections.abc import Mapping
+from typing import Any
 
 import cmk.plugins.lib.db
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import IgnoreResultsError, render
-from cmk.plugins.db2.agent_based.lib import parse_db2_dbs
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    IgnoreResultsError,
+    Metric,
+    render,
+    Result,
+    Service,
+    State,
+)
+from cmk.plugins.db2.agent_based.lib import parse_db2_dbs, Section
 
 # No used space check for Tablsspaces with CONTENTS in ('TEMPORARY','UNDO')
 # It is impossible to check the used space in UNDO and TEMPORARY Tablespaces
@@ -36,20 +44,23 @@ check_info = {}
 db_get_tablespace_levels_in_bytes = cmk.plugins.lib.db.get_tablespace_levels_in_bytes
 
 
-def discover_db2_tablespaces(parsed):
-    for instance, values in parsed[1].items():
+def discover_db2_tablespaces(section: Section) -> DiscoveryResult:
+    for instance, values in section[1].items():
         for table in values[1:]:
-            yield f"{instance}.{table[0]}", {}
+            yield Service(item=f"{instance}.{table[0]}")
 
 
-def check_db2_tablespaces(item, params, parsed):
+def check_db2_tablespaces(item: str, params: Mapping[str, Any], section: Section) -> CheckResult:
     try:
         instance, tbsname = item.split(".")
     except ValueError:
-        yield 3, "Invalid check item given (must be <instance>.<tablespace>)"
+        yield Result(
+            state=State.UNKNOWN,
+            summary="Invalid check item given (must be <instance>.<tablespace>)",
+        )
         return
 
-    db = parsed[1].get(instance)
+    db = section[1].get(instance)
     if not db:
         raise IgnoreResultsError("Login into database failed")
 
@@ -71,40 +82,48 @@ def check_db2_tablespaces(item, params, parsed):
     if tbsp_type == "SMS":
         usable = free  # for SMS free size is the amount of disk space available to the db file
 
-    warn, crit, levels_text, as_perc = db_get_tablespace_levels_in_bytes(usable, params)
+    warn, crit, levels_text, as_perc = db_get_tablespace_levels_in_bytes(usable, params)  # type: ignore[no-untyped-call]
 
-    infotext = f"{render.disksize(used)} of {render.disksize(usable)} used"
-    perfdata = [
-        ("tablespace_size", usable, max(0, total - (warn or 0)), max(0, total - (crit or 0))),
-        ("tablespace_used", used),
-        ("tablespace_max_size", total),
-    ]
-    yield 0, infotext, perfdata
+    yield Result(
+        state=State.OK, summary=f"{render.disksize(used)} of {render.disksize(usable)} used"
+    )
+    yield Metric(
+        "tablespace_size",
+        usable,
+        levels=(max(0.0, total - (warn or 0)), max(0.0, total - (crit or 0))),
+    )
+    yield Metric("tablespace_used", used)
+    yield Metric("tablespace_max_size", total)
 
     perc_free = 100.0 - used / usable * 100.0
     abs_free = usable - used
 
-    state = 0
-    infotext = "%s free" % render.percent(perc_free)
+    state = State.OK
+    infotext = f"{render.percent(perc_free)} free"
     if crit is not None and abs_free <= crit:
-        state = 2
+        state = State.CRIT
     elif warn is not None and abs_free <= warn:
-        state = 1
-    if state:
-        if as_perc:
-            value_str = render.percent(perc_free)
-        else:
-            value_str = render.disksize(abs_free)
+        state = State.WARN
+    if state is not State.OK:
+        value_str = render.percent(perc_free) if as_perc else render.disksize(abs_free)
         infotext = f"only {value_str} left {levels_text}"
-    yield state, infotext
+    yield Result(state=state, summary=infotext)
 
-    yield tbsp_state.lower() != "normal" and 1 or 0, "State: %s" % tbsp_state
-    yield 0, "Type: %s" % tbsp_type
+    yield Result(
+        state=State.WARN if tbsp_state.lower() != "normal" else State.OK,
+        summary=f"State: {tbsp_state}",
+    )
+    yield Result(state=State.OK, summary=f"Type: {tbsp_type}")
 
 
-check_info["db2_tablespaces"] = LegacyCheckDefinition(
+agent_section_db2_tablespaces = AgentSection(
     name="db2_tablespaces",
     parse_function=parse_db2_dbs,
+)
+
+
+check_plugin_db2_tablespaces = CheckPlugin(
+    name="db2_tablespaces",
     service_name="DB2 Tablespace %s",
     discovery_function=discover_db2_tablespaces,
     check_function=check_db2_tablespaces,

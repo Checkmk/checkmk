e5dc66dfdb43db1b7e5ca5fd4576e522ea88c1ad
Simon Jess <simon.jess@checkmk.com>
2026-05-17 10:26:39 +0200

    cmk-graphing-engine: Add package
    CMK-34782
    
    Change-Id: Iec5296c8d5f47dd578280f5d930694951ca6fa70

---
+++ packages/cmk-graphing-engine/tests/test_smoke.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk import graphing_engine
+
+
+def test_package_imports() -> None:
+    assert graphing_engine.__name__ == "cmk.graphing_engine"

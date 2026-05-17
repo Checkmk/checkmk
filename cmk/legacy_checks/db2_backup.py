42a20e6293bba0bc75a0c81626df668fe1746c55
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-17 14:27:09 +0200

    Legacy check migration: db2_backup I
    Change-Id: I48b2819bb004a6aabf6ad08406a7fa9541952b47

--- cmk/legacy_checks/db2_backup.py
+++ cmk/legacy_checks/db2_backup.py
@@ -3,29 +3,37 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 import time
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import IgnoreResultsError, render
-from cmk.plugins.db2.agent_based.lib import parse_db2_dbs
-
-check_info = {}
+from cmk.agent_based.v1 import check_levels as check_levels_v1
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    IgnoreResultsError,
+    render,
+    Result,
+    Service,
+    State,
+)
+from cmk.plugins.db2.agent_based.lib import parse_db2_dbs, Section
 
 # <<<db2_backup>>>
 # [[[db2taddm:CMDBS1]]]
 # 2015-03-12-04.00.13.000000
 
 
-def discover_db2_backup(parsed):
-    for instance in parsed[1]:
-        yield instance, {}
+def discover_db2_backup(section: Section) -> DiscoveryResult:
+    for instance in section[1]:
+        yield Service(item=instance)
 
 
-def check_db2_backup(item, params, parsed):
-    db = parsed[1].get(item)
+def check_db2_backup(item: str, params: Mapping[str, Any], section: Section) -> CheckResult:
+    db = section[1].get(item)
     if not db:
         raise IgnoreResultsError("Login into database failed")
 
@@ -33,24 +41,31 @@ def check_db2_backup(item, params, parsed):
         last_backup = time.mktime(time.strptime(db[0][0][:19], "%Y-%m-%d-%H.%M.%S"))
     except Exception:
         if db[0][0] == "-":
-            yield 1, "No backup available"
+            yield Result(state=State.WARN, summary="No backup available")
         else:
-            yield 3, "Last backup contains an invalid timestamp: %s" % db[0][0]
+            yield Result(
+                state=State.UNKNOWN,
+                summary=f"Last backup contains an invalid timestamp: {db[0][0]}",
+            )
         return
 
     age = time.time() - last_backup
-    yield check_levels(
+    yield from check_levels_v1(
         age,
-        None,
-        params["levels"],
-        human_readable_func=render.timespan,
-        infoname="Time since last backup",
+        levels_upper=params["levels"],
+        render_func=render.timespan,
+        label="Time since last backup",
     )
 
 
-check_info["db2_backup"] = LegacyCheckDefinition(
+agent_section_db2_backup = AgentSection(
     name="db2_backup",
     parse_function=parse_db2_dbs,
+)
+
+
+check_plugin_db2_backup = CheckPlugin(
+    name="db2_backup",
     service_name="DB2 Backup %s",
     discovery_function=discover_db2_backup,
     check_function=check_db2_backup,

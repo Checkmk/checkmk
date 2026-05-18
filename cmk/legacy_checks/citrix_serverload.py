911071bead04b480c8c552ddf6834ef37fc55c46
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-18 13:25:45 +0200

    Legacy check migration: citrix_serverload I
    Change-Id: If76843375b79bd012a098272a4da780aec7ddb74

--- cmk/legacy_checks/citrix_serverload.py
+++ cmk/legacy_checks/citrix_serverload.py
@@ -3,48 +3,62 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # <<<citrix_serverload>>>
 # 100
 
 
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import render, StringTable
+from collections.abc import Mapping
+from typing import Any
 
-check_info = {}
-
-
-def discover_citrix_serverload(info):
-    yield None, {}
-
-
-def check_citrix_serverload(_no_item, params, info):
-    try:
-        load = int(info[0][0])
-    except (IndexError, ValueError):
-        return
-
-    if load == 20000:
-        yield 1, "License error"
-        load = 10000
-
-    yield check_levels(
-        load / 100.0,
-        "citrix_load",
-        params["levels"],
-        human_readable_func=render.percent,
-        infoname="Current Citrix Load",
-    )
+from cmk.agent_based.v1 import check_levels as check_levels_v1
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    render,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
 def parse_citrix_serverload(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["citrix_serverload"] = LegacyCheckDefinition(
+def discover_citrix_serverload(section: StringTable) -> DiscoveryResult:
+    yield Service()
+
+
+def check_citrix_serverload(params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    try:
+        load = int(section[0][0])
+    except (IndexError, ValueError):
+        return
+
+    if load == 20000:
+        yield Result(state=State.WARN, summary="License error")
+        load = 10000
+
+    yield from check_levels_v1(
+        load / 100.0,
+        metric_name="citrix_load",
+        levels_upper=params["levels"],
+        render_func=render.percent,
+        label="Current Citrix Load",
+    )
+
+
+agent_section_citrix_serverload = AgentSection(
     name="citrix_serverload",
     parse_function=parse_citrix_serverload,
+)
+
+
+check_plugin_citrix_serverload = CheckPlugin(
+    name="citrix_serverload",
     service_name="Citrix Serverload",
     discovery_function=discover_citrix_serverload,
     check_function=check_citrix_serverload,

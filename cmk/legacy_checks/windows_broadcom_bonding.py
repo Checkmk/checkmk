7486ac81731c5fcfe5c099587e7cebc24a951019
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-19 18:11:50 +0200

    Legacy check migration: windows_broadcom_bonding I
    Change-Id: Ia213ba1c9fd21e311876de5a058a29ecf814a2cd

--- cmk/legacy_checks/windows_broadcom_bonding.py
+++ cmk/legacy_checks/windows_broadcom_bonding.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # <<<windows_broadcom_bonding>>>
 # Caption            RedundancyStatus
 # BOND_10.3          2
@@ -12,40 +10,53 @@
 #
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-
-check_info = {}
-
-
-def discover_windows_broadcom_bonding(info):
-    inventory = []
-    for line in info[1:]:
-        inventory.append((" ".join(line[:-1]), None))
-    return inventory
-
-
-def check_windows_broadcom_bonding(item, params, info):
-    for line in info:
-        if " ".join(line[:-1]) == item:
-            status = int(line[-1])
-            if status == 5:
-                return 2, "Bond not working"
-            if status == 4:
-                return 1, "Bond partly working"
-            if status == 2:
-                return 0, "Bond fully working"
-            return 3, "Bond status cannot be recognized"
-    return 3, "Bond %s not found in agent output" % item
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
 def parse_windows_broadcom_bonding(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["windows_broadcom_bonding"] = LegacyCheckDefinition(
+def discover_windows_broadcom_bonding(section: StringTable) -> DiscoveryResult:
+    for line in section[1:]:
+        yield Service(item=" ".join(line[:-1]))
+
+
+def check_windows_broadcom_bonding(item: str, section: StringTable) -> CheckResult:
+    for line in section:
+        if " ".join(line[:-1]) == item:
+            status = int(line[-1])
+            if status == 5:
+                yield Result(state=State.CRIT, summary="Bond not working")
+                return
+            if status == 4:
+                yield Result(state=State.WARN, summary="Bond partly working")
+                return
+            if status == 2:
+                yield Result(state=State.OK, summary="Bond fully working")
+                return
+            yield Result(state=State.UNKNOWN, summary="Bond status cannot be recognized")
+            return
+    yield Result(state=State.UNKNOWN, summary=f"Bond {item} not found in agent output")
+
+
+agent_section_windows_broadcom_bonding = AgentSection(
     name="windows_broadcom_bonding",
     parse_function=parse_windows_broadcom_bonding,
+)
+
+
+check_plugin_windows_broadcom_bonding = CheckPlugin(
+    name="windows_broadcom_bonding",
     service_name="Bonding Interface %s",
     discovery_function=discover_windows_broadcom_bonding,
     check_function=check_windows_broadcom_bonding,

562cd4f2031c9b0997a8295110c36dd1f351ed10
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-05-21 15:50:45 +0200

    Legacy check migration: knuerr_rms_temp I
    Change-Id: I7066300a9892046a5d81403ea625df6f4041394a

--- cmk/legacy_checks/knuerr_rms_temp.py
+++ cmk/legacy_checks/knuerr_rms_temp.py
@@ -3,37 +3,51 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.knuerr.lib import DETECT_KNUERR
-
-check_info = {}
-
-
-def discover_knuerr_rms_temp(info):
-    return [("Ambient", {})]
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
-def check_knuerr_rms_temp(_no_item, params, info):
-    return check_temperature(float(info[0][0]) / 10, params, "knuerr_rms_temp")
+def discover_knuerr_rms_temp(section: StringTable) -> DiscoveryResult:
+    yield Service(item="Ambient")
+
+
+def check_knuerr_rms_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    yield from check_temperature(
+        float(section[0][0]) / 10,
+        params,
+        unique_name="knuerr_rms_temp",
+        value_store=get_value_store(),
+    )
 
 
 def parse_knuerr_rms_temp(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["knuerr_rms_temp"] = LegacyCheckDefinition(
+snmp_section_knuerr_rms_temp = SimpleSNMPSection(
     name="knuerr_rms_temp",
-    parse_function=parse_knuerr_rms_temp,
     detect=DETECT_KNUERR,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3711.15.1.1.1.1",
         oids=["4"],
     ),
+    parse_function=parse_knuerr_rms_temp,
+)
+
+
+check_plugin_knuerr_rms_temp = CheckPlugin(
+    name="knuerr_rms_temp",
     service_name="Temperature %s",
     discovery_function=discover_knuerr_rms_temp,
     check_function=check_knuerr_rms_temp,

2943f99a06aad041aea22c43e2fe9f5b6fa774ed
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-05-21 15:55:00 +0200

    Legacy check migration: knuerr_sensors I
    Change-Id: I687b3e5431aee9f58858aecdac314f77ea6b6273

--- cmk/legacy_checks/knuerr_sensors.py
+++ cmk/legacy_checks/knuerr_sensors.py
@@ -3,43 +3,55 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.knuerr.lib import DETECT_KNUERR
 
-check_info = {}
 
-
-def discover_knuerr_sensors(info):
-    for sensor, _state in info:
+def discover_knuerr_sensors(section: StringTable) -> DiscoveryResult:
+    for sensor, _state in section:
         if sensor:
-            yield sensor, None
+            yield Service(item=sensor)
 
 
-def check_knuerr_sensors(item, _no_params, info):
-    for sensor, state in info:
+def check_knuerr_sensors(item: str, section: StringTable) -> CheckResult:
+    for sensor, state in section:
         if sensor == item:
             if state != "0":
-                return 2, "Sensor triggered"
-            return 0, "Sensor not triggered"
-    return 3, "Sensor no longer found"
+                yield Result(state=State.CRIT, summary="Sensor triggered")
+            else:
+                yield Result(state=State.OK, summary="Sensor not triggered")
+            return
+    yield Result(state=State.UNKNOWN, summary="Sensor no longer found")
 
 
 def parse_knuerr_sensors(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["knuerr_sensors"] = LegacyCheckDefinition(
+snmp_section_knuerr_sensors = SimpleSNMPSection(
     name="knuerr_sensors",
-    parse_function=parse_knuerr_sensors,
     detect=DETECT_KNUERR,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3711.15.1.1.2",
         oids=["1", "5"],
     ),
+    parse_function=parse_knuerr_sensors,
+)
+
+
+check_plugin_knuerr_sensors = CheckPlugin(
+    name="knuerr_sensors",
     service_name="Sensor %s",
     discovery_function=discover_knuerr_sensors,
     check_function=check_knuerr_sensors,

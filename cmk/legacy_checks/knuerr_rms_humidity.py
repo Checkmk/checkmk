0cc045680da1cdf91caf670256301e4afec0307f
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-05-21 15:46:25 +0200

    Legacy check migration: knuerr_rms_humidity I
    Change-Id: I2c2723013a6d3cd0e936322abfe1a16335d63b8a

--- cmk/legacy_checks/knuerr_rms_humidity.py
+++ cmk/legacy_checks/knuerr_rms_humidity.py
@@ -3,38 +3,46 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.humidity import check_humidity
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.knuerr.lib import DETECT_KNUERR
-
-check_info = {}
-
-
-def discover_knuerr_rms_humidity(info):
-    yield None, {}
+from cmk.plugins.lib.humidity import check_humidity, CheckParams
 
 
-def check_knuerr_rms_humidity(_no_item, params, info):
-    _name, reading = info[0]
-    return check_humidity(float(reading) / 10, params)
+def discover_knuerr_rms_humidity(section: StringTable) -> DiscoveryResult:
+    yield Service()
+
+
+def check_knuerr_rms_humidity(params: CheckParams, section: StringTable) -> CheckResult:
+    _name, reading = section[0]
+    yield from check_humidity(float(reading) / 10, params)
 
 
 def parse_knuerr_rms_humidity(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["knuerr_rms_humidity"] = LegacyCheckDefinition(
+snmp_section_knuerr_rms_humidity = SimpleSNMPSection(
     name="knuerr_rms_humidity",
-    parse_function=parse_knuerr_rms_humidity,
     detect=DETECT_KNUERR,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3711.15.1.1.1.2",
         oids=["2", "4"],
     ),
+    parse_function=parse_knuerr_rms_humidity,
+)
+
+
+check_plugin_knuerr_rms_humidity = CheckPlugin(
+    name="knuerr_rms_humidity",
     service_name="Humidity",
     discovery_function=discover_knuerr_rms_humidity,
     check_function=check_knuerr_rms_humidity,

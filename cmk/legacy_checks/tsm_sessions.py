56b84c452bf0469cab5d195fd04e03b6d4549df3
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-21 09:57:24 +0200

    Legacy check migration: tsm_sessions I
    Jira-Ref: CMK-34764
    Change-Id: I583777ec0b5dab21a093b89f90493694ee4cff61

--- cmk/legacy_checks/tsm_sessions.py
+++ cmk/legacy_checks/tsm_sessions.py
@@ -3,41 +3,40 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-# Example output from agent:
-# Put here the example output from your TCP-Based agent. If the
-# check is SNMP-Based, then remove this section
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-
-check_info = {}
-
-
-def saveint(i: str) -> int:
-    """Tries to cast a string to an integer and return it. In case this
-    fails, it returns 0.
-
-    Advice: Please don't use this function in new code. It is understood as
-    bad style these days, because in case you get 0 back from this function,
-    you can not know whether it is really 0 or something went wrong."""
+def _saveint(value: str) -> int:
     try:
-        return int(i)
+        return int(value)
     except (TypeError, ValueError):
         return 0
 
 
-def discover_tsm_sessions(info):
-    yield None, {}
+def parse_tsm_sessions(string_table: StringTable) -> StringTable:
+    return string_table
 
 
-def check_tsm_sessions(item, _no_params, info):
-    state = 0
+def discover_tsm_sessions(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
+
+
+def check_tsm_sessions(section: StringTable) -> CheckResult:
     warn, crit = 300, 600
+    state = State.OK
     count = 0
-    for entry in info:
+    for entry in section:
         if len(entry) == 4:
             _sid, _client_name, proc_state, wait = entry
         elif len(entry) > 4:
@@ -45,24 +44,31 @@ def check_tsm_sessions(item, _no_params, info):
         else:
             _sid, proc_state, wait = entry
 
-        if proc_state in ["RecvW", "MediaW"]:
-            wait = saveint(wait)
-            if wait >= crit:
-                state = 2
-                count += 1
-            elif wait >= warn:
-                state = max(state, 1)
-                count += 1
-    return state, "%d sessions too long in RecvW or MediaW state" % count
+        if proc_state not in ("RecvW", "MediaW"):
+            continue
+
+        wait_seconds = _saveint(wait)
+        if wait_seconds >= crit:
+            state = State.CRIT
+            count += 1
+        elif wait_seconds >= warn:
+            state = State.worst(state, State.WARN)
+            count += 1
+
+    yield Result(
+        state=state,
+        summary=f"{count} sessions too long in RecvW or MediaW state",
+    )
 
 
-def parse_tsm_sessions(string_table: StringTable) -> StringTable:
-    return string_table
-
-
-check_info["tsm_sessions"] = LegacyCheckDefinition(
+agent_section_tsm_sessions = AgentSection(
     name="tsm_sessions",
     parse_function=parse_tsm_sessions,
+)
+
+
+check_plugin_tsm_sessions = CheckPlugin(
+    name="tsm_sessions",
     service_name="tsm_sessions",
     discovery_function=discover_tsm_sessions,
     check_function=check_tsm_sessions,

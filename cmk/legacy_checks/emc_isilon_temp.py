0d2685baa6e50ab380aa1bb147442f3e344c1bd0
ramadan khalifa <ramadan.khalifa@checkmk.com>
2026-05-22 15:06:45 +0200

    Legacy check migration: emc_isilon_temp I
    Jira-Ref: CMK-32507
    Change-Id: I3092c89206e3b4fdf6c37ad9f9801341eddb2310

--- cmk/legacy_checks/emc_isilon_temp.py
+++ cmk/legacy_checks/emc_isilon_temp.py
@@ -3,30 +3,18 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
-
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.emc.lib import DETECT_ISILON
-
-check_info = {}
-
-
-def inventory_isilon_temp(info, is_cpu):
-    for sensor_name, _value in info:
-        item_name = isilon_temp_item_name(sensor_name)
-        if is_cpu == item_name.startswith("CPU"):
-            yield item_name, {}
-
-
-def check_isilon_temp(item, params, info):
-    for sensor_name, value in info:
-        if item == isilon_temp_item_name(sensor_name):
-            return check_temperature(float(value), params, "isilon_%s" % item)
-    return None
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
 # Expected sensor names:
@@ -37,37 +25,44 @@ def check_isilon_temp(item, params, info):
 # "Temp Power Supply 1"
 # "Temp Power Supply 2"
 # "Temp System"
-def isilon_temp_item_name(sensor_name):
+def _isilon_temp_item_name(sensor_name: str) -> str:
     if "CPU Throttle" in sensor_name:
-        return sensor_name.split("(")[1].split(")")[0]  # "CPU 1"
+        return sensor_name.split("(")[1].split(")", maxsplit=1)[0]  # "CPU 1"
     return sensor_name[5:]  # "Front Panel"
-
-
-#   .--Air Temperature-----------------------------------------------------.
-#   |                              _    _                                  |
-#   |                             / \  (_)_ __                             |
-#   |                            / _ \ | | '__|                            |
-#   |                           / ___ \| | |                               |
-#   |                          /_/   \_\_|_|                               |
-#   |                                                                      |
-#   |     _____                                   _                        |
-#   |    |_   _|__ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
-#   |      | |/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _ \      |
-#   |      | |  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | |  __/      |
-#   |      |_|\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \___|      |
-#   |                       |_|                                            |
-#   '----------------------------------------------------------------------'
 
 
 def parse_emc_isilon_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_emc_isilon_temp(info):
-    return inventory_isilon_temp(info, is_cpu=False)
+def _check_isilon_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for sensor_name, value in section:
+        if item == _isilon_temp_item_name(sensor_name):
+            yield from check_temperature(
+                float(value),
+                params,
+                unique_name=f"isilon_{item}",
+                value_store=get_value_store(),
+            )
+            return
 
 
-check_info["emc_isilon_temp"] = LegacyCheckDefinition(
+def _discover_isilon_temp(section: StringTable, is_cpu: bool) -> DiscoveryResult:
+    for sensor_name, _value in section:
+        item_name = _isilon_temp_item_name(sensor_name)
+        if is_cpu == item_name.startswith("CPU"):
+            yield Service(item=item_name)
+
+
+def discover_emc_isilon_temp(section: StringTable) -> DiscoveryResult:
+    yield from _discover_isilon_temp(section, is_cpu=False)
+
+
+def discover_emc_isilon_temp_cpu(section: StringTable) -> DiscoveryResult:
+    yield from _discover_isilon_temp(section, is_cpu=True)
+
+
+snmp_section_emc_isilon_temp = SimpleSNMPSection(
     name="emc_isilon_temp",
     parse_function=parse_emc_isilon_temp,
     detect=DETECT_ISILON,
@@ -75,9 +70,14 @@ check_info["emc_isilon_temp"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.12124.2.54.1",
         oids=["3", "4"],
     ),
+)
+
+
+check_plugin_emc_isilon_temp = CheckPlugin(
+    name="emc_isilon_temp",
     service_name="Temperature %s",
     discovery_function=discover_emc_isilon_temp,
-    check_function=check_isilon_temp,
+    check_function=_check_isilon_temp,
     check_ruleset_name="temperature",
     check_default_parameters={
         "levels": (28.0, 33.0),  # assumed useful levels for ambient / air temperature
@@ -85,34 +85,14 @@ check_info["emc_isilon_temp"] = LegacyCheckDefinition(
 )
 
 
-def discover_emc_isilon_temp_cpu(info):
-    return inventory_isilon_temp(info, is_cpu=True)
-
-
-# .
-#   .--CPU Temperature-----------------------------------------------------.
-#   |                           ____ ____  _   _                           |
-#   |                          / ___|  _ \| | | |                          |
-#   |                         | |   | |_) | | | |                          |
-#   |                         | |___|  __/| |_| |                          |
-#   |                          \____|_|    \___/                           |
-#   |                                                                      |
-#   |     _____                                   _                        |
-#   |    |_   _|__ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
-#   |      | |/ _ \ '_ ` _ \| '_ \ / _ \ '__/ _` | __| | | | '__/ _ \      |
-#   |      | |  __/ | | | | | |_) |  __/ | | (_| | |_| |_| | | |  __/      |
-#   |      |_|\___|_| |_| |_| .__/ \___|_|  \__,_|\__|\__,_|_|  \___|      |
-#   |                       |_|                                            |
-#   '----------------------------------------------------------------------'
-
-check_info["emc_isilon_temp.cpu"] = LegacyCheckDefinition(
+check_plugin_emc_isilon_temp_cpu = CheckPlugin(
     name="emc_isilon_temp_cpu",
     service_name="Temperature %s",
     sections=["emc_isilon_temp"],
     discovery_function=discover_emc_isilon_temp_cpu,
-    check_function=check_isilon_temp,
+    check_function=_check_isilon_temp,
     check_ruleset_name="temperature",
     check_default_parameters={
-        "levels": (75.0, 85.0),  # assumed useful levels for ambient / air temperature
+        "levels": (75.0, 85.0),
     },
 )

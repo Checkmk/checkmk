0f3e5e012cc802b19440a55b53202371f3ebe6cd
ramadan khalifa <ramadan.khalifa@checkmk.com>
2026-05-22 15:01:00 +0200

    Legacy check migration: emc_isilon_fans I
    Jira-Ref: CMK-32507
    Change-Id: Iff4102e07de2244d4a980ddff4523abdd920712b

--- cmk/legacy_checks/emc_isilon_fans.py
+++ cmk/legacy_checks/emc_isilon_fans.py
@@ -3,16 +3,20 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.fan import check_fan
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.emc.lib import DETECT_ISILON
-
-check_info = {}
+from cmk.plugins.lib.fan import check_fan
 
 
 # Examples for sensor names:
@@ -21,27 +25,29 @@ check_info = {}
 # Chassis Fan3 (ISI F3)
 # Power Supply 1 Fan1 --> Power Supply 1 1
 # Power Supply 2 Fan1
-def isilon_fan_item_name(sensor_name):
+def _isilon_fan_item_name(sensor_name: str) -> str:
     return sensor_name.replace("Fan", "").split("(")[0].strip()
-
-
-def discover_emc_isilon_fans(info):
-    for fan_name, _value in info:
-        yield isilon_fan_item_name(fan_name), {}
-
-
-def check_emc_isilon_fans(item, params, info):
-    for fan_name, value in info:
-        if item == isilon_fan_item_name(fan_name):
-            return check_fan(float(value), params)
-    return None
 
 
 def parse_emc_isilon_fans(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["emc_isilon_fans"] = LegacyCheckDefinition(
+def discover_emc_isilon_fans(section: StringTable) -> DiscoveryResult:
+    for fan_name, _value in section:
+        yield Service(item=_isilon_fan_item_name(fan_name))
+
+
+def check_emc_isilon_fans(
+    item: str, params: Mapping[str, Any], section: StringTable
+) -> CheckResult:
+    for fan_name, value in section:
+        if item == _isilon_fan_item_name(fan_name):
+            yield from check_fan(float(value), params)
+            return
+
+
+snmp_section_emc_isilon_fans = SimpleSNMPSection(
     name="emc_isilon_fans",
     parse_function=parse_emc_isilon_fans,
     detect=DETECT_ISILON,
@@ -49,6 +55,11 @@ check_info["emc_isilon_fans"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.12124.2.53.1",
         oids=["3", "4"],
     ),
+)
+
+
+check_plugin_emc_isilon_fans = CheckPlugin(
+    name="emc_isilon_fans",
     service_name="Fan %s",
     discovery_function=discover_emc_isilon_fans,
     check_function=check_emc_isilon_fans,

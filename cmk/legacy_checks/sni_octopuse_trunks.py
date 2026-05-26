12989fee66b4a01663a5adc8ec8a3491952e0e4b
Andreas Hasenkopf <andreas.hasenkopf@checkmk.com>
2026-05-26 09:52:14 +0200

    Legacy check migration: sni_octopuse_trunks I
    Change-Id: I097362bb9bcfe469ba424c27fc1dabf3b29687d4

--- cmk/legacy_checks/sni_octopuse_trunks.py
+++ cmk/legacy_checks/sni_octopuse_trunks.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # .1.3.6.1.4.1.231.7.2.9.3.8.1.3.1  "OpenStage 30"
 # .1.3.6.1.4.1.231.7.2.9.3.8.1.3.2  "OpenStage 30"
 # .1.3.6.1.4.1.231.7.2.9.3.8.1.3.7  "P. O. T."
@@ -18,46 +16,51 @@
 
 from collections.abc import Sequence
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.sni_octopuse.lib import DETECT_SNI_OCTOPUSE
 
-check_info = {}
+_TRUNKPORTS = ("S0 trunk: extern",)
 
 
-def discover_octopus_trunks(info):
-    trunkports = ["S0 trunk: extern"]
-    inventory = []
-    for line in info[0]:
+def discover_octopus_trunks(section: Sequence[StringTable]) -> DiscoveryResult:
+    for line in section[0]:
         if len(line) == 4:
             portindex, cardindex, porttype, portstate = line
-            portdesc = f"{cardindex}/{portindex}"
-            if porttype in trunkports and portstate == "2":
-                inventory.append((portdesc, None))
-    return inventory
+            if porttype in _TRUNKPORTS and portstate == "2":
+                yield Service(item=f"{cardindex}/{portindex}")
 
 
-def check_octopus_trunks(item, _no_params, info):
-    for line in info[0]:
+def check_octopus_trunks(item: str, section: Sequence[StringTable]) -> CheckResult:
+    for line in section[0]:
         portindex, cardindex, porttype, portstate = line
-        portdesc = f"{cardindex}/{portindex}"
-        if item == portdesc:
+        if item == f"{cardindex}/{portindex}":
             # There are two relevant card states, we use the one from
             # octoPortTable
             if portstate == "1":
-                return (2, "Port [%s] is inactive" % porttype)
-            return (0, "Port [%s] is active" % porttype)
+                yield Result(state=State.CRIT, summary=f"Port [{porttype}] is inactive")
+                return
+            yield Result(state=State.OK, summary=f"Port [{porttype}] is active")
+            return
 
-    return (3, "UNKW - unknown data received from agent")
+    yield Result(state=State.UNKNOWN, summary="UNKW - unknown data received from agent")
 
 
 def parse_sni_octopuse_trunks(string_table: Sequence[StringTable]) -> Sequence[StringTable]:
     return string_table
 
 
-check_info["sni_octopuse_trunks"] = LegacyCheckDefinition(
+snmp_section_sni_octopuse_trunks = SNMPSection(
     name="sni_octopuse_trunks",
-    parse_function=parse_sni_octopuse_trunks,
     detect=DETECT_SNI_OCTOPUSE,
     fetch=[
         SNMPTree(
@@ -65,6 +68,12 @@ check_info["sni_octopuse_trunks"] = LegacyCheckDefinition(
             oids=["1", "2", "3", "4"],
         )
     ],
+    parse_function=parse_sni_octopuse_trunks,
+)
+
+
+check_plugin_sni_octopuse_trunks = CheckPlugin(
+    name="sni_octopuse_trunks",
     service_name="Trunk Port %s",
     discovery_function=discover_octopus_trunks,
     check_function=check_octopus_trunks,

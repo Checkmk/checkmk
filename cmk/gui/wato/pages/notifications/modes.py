d7b653a3ad4e2f44efc32159e163775b969c925b
Ronny Bruska <ronny.bruska@checkmk.com>
2026-05-27 10:12:42 +0200

    20047 FIX Parameters for notification methods: Keep search filter when editing a parameter
    SUP-28770
    
    Change-Id: I577ba626f56890cedbe1c4eabbe13f2d72b0366b

--- cmk/gui/wato/pages/notifications/modes.py
+++ cmk/gui/wato/pages/notifications/modes.py
@@ -3982,6 +3982,17 @@ class ABCNotificationParameterMode(WatoMode):
     def _back_mode(self) -> ActionResult:
         raise NotImplementedError()
 
+    def _search_vars(self) -> HTTPVariables:
+        if search := request.get_str_input("search"):
+            return [("search", search)]
+        return []
+
+    def _preserved_vars(self) -> dict[str, str]:
+        preserved_vars: dict[str, str] = {"method": self._method()}
+        if search := request.get_str_input("search"):
+            preserved_vars["search"] = search
+        return preserved_vars
+
     def _load_parameters(self) -> NotificationParameterSpecs:
         if transactions.is_transaction():
             return NotificationParameterConfigFile().load_for_modification()
@@ -4090,7 +4101,7 @@ class ABCNotificationParameterMode(WatoMode):
 
         self._parameters = self._load_parameters()
         if (method_parameters := self._parameters.get(self._method())) is None:
-            return redirect(mode_url("notification_parameters", method=self._method()))
+            return redirect(mode_url("notification_parameters", **self._preserved_vars()))
 
         method_parameter_list = list(method_parameters.items())
         if request.has_var("_delete"):
@@ -4103,8 +4114,8 @@ class ABCNotificationParameterMode(WatoMode):
                 return redirect(
                     mode_url(
                         "notification_parameters",
-                        method=self._method(),
                         _parameter_id_with_related_rules=parameter_id,
+                        **self._preserved_vars(),
                     )
                 )
 
@@ -4141,9 +4152,9 @@ class ABCNotificationParameterMode(WatoMode):
             )
 
         if back_mode := request.var("back_mode"):
-            return redirect(mode_url(back_mode, method=self._method()))
+            return redirect(mode_url(back_mode, **self._preserved_vars()))
 
-        return redirect(mode_url("notification_parameters", method=self._method()))
+        return redirect(mode_url("notification_parameters", **self._preserved_vars()))
 
     def _vue_field_id(self) -> str:
         return "_vue_edit_notification_parameter"
@@ -4212,6 +4223,7 @@ class ModeNotificationParameters(ABCNotificationParameterMode):
                                                 ("mode", "edit_notification_parameter"),
                                                 ("method", self._method()),
                                             ]
+                                            + self._search_vars()
                                         )
                                     ),
                                     is_shortcut=True,
@@ -4371,6 +4383,7 @@ class ModeNotificationParameters(ABCNotificationParameterMode):
             ("back_mode", "notification_parameters"),
             ("method", self._method()),
         ]
+        additional_vars += self._search_vars()
 
         delete_url = make_confirm_delete_link(
             url=make_action_link(
@@ -4432,7 +4445,7 @@ class ModeEditNotificationParameter(ABCNotificationParameterMode):
             return super().breadcrumb()
 
     def _back_mode(self) -> ActionResult:
-        return redirect(mode_url("notification_parameters", method=self._method()))
+        return redirect(mode_url("notification_parameters", **self._preserved_vars()))
 
     def title(self) -> str:
         if self._new:
@@ -4486,7 +4499,7 @@ class ModeEditNotificationParameter(ABCNotificationParameterMode):
         )
 
         if back_mode := request.var("back_mode"):
-            return redirect(mode_url(back_mode, method=self._method()))
+            return redirect(mode_url(back_mode, **self._preserved_vars()))
 
         return self._back_mode()
 

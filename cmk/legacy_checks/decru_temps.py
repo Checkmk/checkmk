2d37129cbcbbd7f6f886b0bf3599050caec8953d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-27 11:12:58 +0200

    Legacy check migration: decru_temps I
    Change-Id: I15c1357126d057db614a9cd2a7c920ced831a11e

--- cmk/legacy_checks/decru_temps.py
+++ cmk/legacy_checks/decru_temps.py
@@ -3,46 +3,60 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature, fahrenheit_to_celsius
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.decru.lib import DETECT_DECRU
-
-check_info = {}
-
-
-def discover_decru_temps(info):
-    for name, rawtemp in info:
-        rawtemp = int(fahrenheit_to_celsius(rawtemp))
-        # device doesn't provide warning/critical levels
-        # instead, this uses the temperature at inventory-time +4/+8
-        yield name, {"levels": (rawtemp + 4, rawtemp + 8)}
-
-
-def check_decru_temps(item, params, info):
-    for name, rawtemp in info:
-        if name == item:
-            temp = fahrenheit_to_celsius(int(rawtemp))
-            return check_temperature(temp, params, "decru_temps_%s" % item)
-    return None
+from cmk.plugins.lib.temperature import check_temperature, fahrenheit_to_celsius, TempParamType
 
 
 def parse_decru_temps(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["decru_temps"] = LegacyCheckDefinition(
+def discover_decru_temps(section: StringTable) -> DiscoveryResult:
+    for name, rawtemp in section:
+        # device doesn't provide warning/critical levels
+        # instead, this uses the temperature at inventory-time +4/+8
+        temp_c = int(fahrenheit_to_celsius(float(rawtemp)))
+        yield Service(item=name, parameters={"levels": (temp_c + 4, temp_c + 8)})
+
+
+def check_decru_temps(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for name, rawtemp in section:
+        if name == item:
+            yield from check_temperature(
+                fahrenheit_to_celsius(float(rawtemp)),
+                params,
+                unique_name=f"decru_temps_{item}",
+                value_store=get_value_store(),
+            )
+            return
+
+
+snmp_section_decru_temps = SimpleSNMPSection(
     name="decru_temps",
-    parse_function=parse_decru_temps,
     detect=DETECT_DECRU,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.12962.1.2.4.1",
         oids=["2", "3"],
     ),
+    parse_function=parse_decru_temps,
+)
+
+
+check_plugin_decru_temps = CheckPlugin(
+    name="decru_temps",
     service_name="Temperature %s",
     discovery_function=discover_decru_temps,
     check_function=check_decru_temps,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )

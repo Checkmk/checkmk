2fa7825545f84e97325ea8298f805c34f625abf5
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-27 11:16:48 +0200

    Legacy check migration: enterasys_cpu_util I
    Change-Id: Ieecfa99532144dff2c2783b1d14f1953d9594d1b

--- cmk/legacy_checks/enterasys_cpu_util.py
+++ cmk/legacy_checks/enterasys_cpu_util.py
@@ -3,50 +3,62 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-
-
+import time
 from collections.abc import Mapping
 from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import (
-    LegacyCheckDefinition,
-    LegacyCheckResult,
-    LegacyDiscoveryResult,
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    OIDEnd,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
 )
-from cmk.agent_based.v2 import OIDEnd, SNMPTree, StringTable
-from cmk.legacy_includes.cpu_util import check_cpu_util
 from cmk.plugins.enterasys.lib import DETECT_ENTERASYS
-
-check_info = {}
-
-
-def discover_enterasys_cpu_util(info: StringTable) -> LegacyDiscoveryResult:
-    # [:-2] to remove the oid end
-    return [(x[0][:-2], {}) for x in info]
-
-
-def check_enterasys_cpu_util(
-    item: str, params: Mapping[str, Any], info: StringTable
-) -> LegacyCheckResult:
-    for core, util in info:
-        if item == core[:-2]:
-            usage = int(util) / 10.0
-            yield from check_cpu_util(usage, params)
+from cmk.plugins.lib.cpu_util import check_cpu_util
 
 
 def parse_enterasys_cpu_util(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["enterasys_cpu_util"] = LegacyCheckDefinition(
+def discover_enterasys_cpu_util(section: StringTable) -> DiscoveryResult:
+    # [:-2] to remove the oid end
+    for entry in section:
+        yield Service(item=entry[0][:-2])
+
+
+def check_enterasys_cpu_util(
+    item: str, params: Mapping[str, Any], section: StringTable
+) -> CheckResult:
+    for core, util in section:
+        if item == core[:-2]:
+            yield from check_cpu_util(
+                util=int(util) / 10.0,
+                params=params,
+                value_store=get_value_store(),
+                this_time=time.time(),
+            )
+            return
+
+
+snmp_section_enterasys_cpu_util = SimpleSNMPSection(
     name="enterasys_cpu_util",
-    parse_function=parse_enterasys_cpu_util,
     detect=DETECT_ENTERASYS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.5624.1.2.49.1.1.1.1",
         oids=[OIDEnd(), "3"],
     ),
+    parse_function=parse_enterasys_cpu_util,
+)
+
+
+check_plugin_enterasys_cpu_util = CheckPlugin(
+    name="enterasys_cpu_util",
     service_name="CPU util %s",
     discovery_function=discover_enterasys_cpu_util,
     check_function=check_enterasys_cpu_util,

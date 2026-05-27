388df49e534344a92e9473c4d52b33cb30c4d3a9
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-05-27 11:39:29 +0200

    Legacy check migration: enterasys_temp I
    Change-Id: I1cb1599ee8fc954f9a25dfa5f00cc92761c88d32

--- cmk/legacy_checks/enterasys_temp.py
+++ cmk/legacy_checks/enterasys_temp.py
@@ -4,52 +4,62 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature, TempParamType
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.enterasys.lib import DETECT_ENTERASYS
-
-check_info = {}
-
-
-def discover_enterasys_temp(info: StringTable) -> list[tuple[str, dict[str, object]]]:
-    if info and info[0][0] != "0":
-        return [("Ambient", {})]
-    return []
-
-
-def check_enterasys_temp(
-    item: str, params: TempParamType, info: StringTable
-) -> (
-    tuple[int, str]
-    | tuple[
-        int, str, list[tuple[str, float, float | None, float | None, float | None, float | None]]
-    ]
-):
-    # info for MIB: The ambient temperature of the room in which the chassis
-    # is located. If this sensor is broken or not supported, then
-    # this object will be set to zero. The value of this object
-    # is the actual temperature in degrees Fahrenheit * 10.
-    if info[0][0] == "0":
-        return 3, "Sensor broken or not supported"
-
-    # temp = fahrenheit_to_celsius(int(info[0][0]) / 10.0)
-    temp = int(info[0][0]) / 10.0
-    return check_temperature(temp, params, "enterasys_temp_%s" % item, dev_unit="f")
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
 def parse_enterasys_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["enterasys_temp"] = LegacyCheckDefinition(
+def discover_enterasys_temp(section: StringTable) -> DiscoveryResult:
+    if section and section[0][0] != "0":
+        yield Service(item="Ambient")
+
+
+def check_enterasys_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    # info for MIB: The ambient temperature of the room in which the chassis
+    # is located. If this sensor is broken or not supported, then
+    # this object will be set to zero. The value of this object
+    # is the actual temperature in degrees Fahrenheit * 10.
+    if section[0][0] == "0":
+        yield Result(state=State.UNKNOWN, summary="Sensor broken or not supported")
+        return
+
+    yield from check_temperature(
+        int(section[0][0]) / 10.0,
+        params,
+        unique_name=f"enterasys_temp_{item}",
+        value_store=get_value_store(),
+        dev_unit="f",
+    )
+
+
+snmp_section_enterasys_temp = SimpleSNMPSection(
     name="enterasys_temp",
-    parse_function=parse_enterasys_temp,
     detect=DETECT_ENTERASYS,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.52.4.1.1.8.1",
         oids=["1"],
     ),
+    parse_function=parse_enterasys_temp,
+)
+
+
+check_plugin_enterasys_temp = CheckPlugin(
+    name="enterasys_temp",
     service_name="Temperature %s",
     discovery_function=discover_enterasys_temp,
     check_function=check_enterasys_temp,

6cd833c81a58bb0c0c072312aeee76606878757e
Moritz Kirschner <moritz.kirschner@checkmk.com>
2026-06-01 17:29:51 +0200

    Test tab behavior on CmkHelpText and CmkSwitch
    Change-Id: If28fd6479a42b7e1cbf694a7a8c14cd5a3b625d0

--- packages/cmk-frontend-vue/tests/components/CmkHelpText.test.ts
+++ packages/cmk-frontend-vue/tests/components/CmkHelpText.test.ts
@@ -3,62 +3,84 @@
  * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
  * conditions defined in the file COPYING, which is part of this source code package.
  */
-import { fireEvent, render, screen, waitFor } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from '@testing-library/vue'
 
 import CmkHelpText from '@/components/CmkHelpText.vue'
 
 describe('CmkHelpText', () => {
   test('renders help tooltip when click on trigger', async () => {
+    const user = userEvent.setup()
     render(CmkHelpText, {
       props: {
         help: 'fooHelp'
       }
     })
     const trigger = await screen.findByRole('button')
-    await fireEvent.click(trigger)
+    await user.click(trigger)
     const helpText = await screen.findAllByText('fooHelp')
     expect(helpText).toBeTruthy()
   })
 
   test('keep tooltip open when click on tooltip', async () => {
+    const user = userEvent.setup()
     render(CmkHelpText, {
       props: {
         help: 'fooHelp'
       }
     })
     const trigger = await screen.findByRole('button')
-    await fireEvent.click(trigger)
+    await user.click(trigger)
     const helpTooltip = await screen.findByRole('tooltip')
-    await fireEvent.click(helpTooltip)
+    await user.click(helpTooltip)
     await screen.findByRole('tooltip')
   })
 
   test('close open tooltip when clicking on icon', async () => {
+    const user = userEvent.setup()
     render(CmkHelpText, {
       props: {
         help: 'fooHelp'
       }
     })
     const trigger = await screen.findByRole('button')
-    await fireEvent.click(trigger)
+    await user.click(trigger)
     expect(screen.queryByRole('tooltip')).toBeInTheDocument()
-    await fireEvent.click(trigger)
+    await user.click(trigger)
     await waitFor(() => {
       expect(screen.queryByRole('tooltip')).not.toBeInTheDocument()
     })
   })
 
   test('close tooltip when pressing escape key', async () => {
+    const user = userEvent.setup()
     render(CmkHelpText, {
       props: {
         help: 'fooHelp'
       }
     })
     const trigger = await screen.findByRole('button')
-    await fireEvent.click(trigger)
+    await user.click(trigger)
     await screen.findByRole('tooltip')
 
-    await fireEvent.keyDown(document, { key: 'Escape', code: 'Escape' })
+    await user.keyboard('[Escape]')
+    await waitFor(() => {
+      expect(screen.queryByRole('tooltip')).not.toBeInTheDocument()
+    })
+  })
+
+  test('is keyboard focusable and toggles via Enter', async () => {
+    const user = userEvent.setup()
+    render(CmkHelpText, { props: { help: 'fooHelp' } })
+
+    await user.tab()
+
+    await user.keyboard('[Enter]')
+
+    screen.getByRole('tooltip')
+
+    await user.keyboard('[Enter]')
+
     await waitFor(() => {
       expect(screen.queryByRole('tooltip')).not.toBeInTheDocument()
     })
---
+++ packages/cmk-frontend-vue/tests/components/CmkSwitch.test.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+ * This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+ * conditions defined in the file COPYING, which is part of this source code package.
+ */
+import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/vue'
+
+import CmkSwitch from '@/components/CmkSwitch.vue'
+
+test('switch is keyboard focusable and toggles via Space/Enter', async () => {
+  const user = userEvent.setup()
+  render(CmkSwitch, { props: { data: false } })
+
+  screen.getByRole('switch', { checked: false })
+
+  await user.tab()
+
+  await user.keyboard('[Enter]')
+
+  screen.getByRole('switch', { checked: true })
+
+  await user.keyboard('[Space]')
+
+  screen.getByRole('switch', { checked: false })
+})

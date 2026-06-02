eaa3080554d9a0811d7cfea03a21192d7803bb06
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2026-06-02 09:22:23 +0200

    Fix path
    The artifact collection suddenly cannot handle the full path
    
    Change-Id: Ifcb64eed1c90ea475cd56da1cd8c7895e51f8665

--- buildscripts/scripts/build-cmk-relay-image.groovy
+++ buildscripts/scripts/build-cmk-relay-image.groovy
@@ -61,7 +61,7 @@ void main() {
             }
 
             stage(name: 'Archive SBOM') {
-                archiveArtifacts(artifacts: "${artifact_directory}/${cmk_version_rc_aware}/${sbom_name}")
+                archiveArtifacts(artifacts: "artifacts/${cmk_version_rc_aware}/${sbom_name}")
             }
 
             stage(name: 'Upload tarball to internal deploy dest') {

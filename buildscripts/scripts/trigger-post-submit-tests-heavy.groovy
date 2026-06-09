ede5da9fea23f500f82e1abb1b2857546a210fd8
Niko Wenselowski <niko.wenselowski@checkmk.com>
2026-06-09 13:53:02 +0200

    Heavy chain: Add cloud update tests
    This should fill a testing gap we have during daytime.
    
    Change-Id: Ic22bc2365df2a07cacafbb7d34e1c08a778c24ec

--- buildscripts/scripts/trigger-post-submit-tests-heavy.groovy
+++ buildscripts/scripts/trigger-post-submit-tests-heavy.groovy
@@ -28,6 +28,7 @@ void main() {
         "test-integration-single-node",
         "test-plugins",
         "test-plugins-piggyback",
+        "test-update-cloud",
         "test-update-community",
         "test-update-pro",
         "test-update-ultimatemt",

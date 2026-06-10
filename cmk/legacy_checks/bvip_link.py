3f5626d141b1899070ccf8527874ae57a4926eee
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-06-10 22:35:34 +0200

    Legacy check migration: bvip_link I
    Change-Id: I36777b29f5722a454ef2d2bae678616f4057a6fe

--- cmk/legacy_checks/bvip_link.py
+++ cmk/legacy_checks/bvip_link.py
@@ -4,25 +4,32 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from collections.abc import Iterator, Mapping, Sequence
+from collections.abc import Mapping, Sequence
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.bvip.lib import DETECT_BVIP
 
-check_info = {}
+
+def parse_bvip_link(string_table: StringTable) -> StringTable:
+    return string_table
 
 
-def discover_bvip_link(info: StringTable) -> list[tuple[None, dict[str, object]]]:
-    if info:
-        return [(None, {})]
-    return []
+def discover_bvip_link(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-def check_bvip_link(
-    _no_item: None, params: Mapping[str, Sequence[int]], info: StringTable
-) -> Iterator[tuple[int, str]]:
-    count = 0
+def check_bvip_link(params: Mapping[str, Sequence[int]], section: StringTable) -> CheckResult:
     states = {
         0: "No Link",
         1: "10 MBit - HalfDuplex",
@@ -32,38 +39,35 @@ def check_bvip_link(
         5: "1 Gbit - FullDuplex",
         7: "Wifi",
     }
-    for line in info:
-        count += 1
+    for count, line in enumerate(section, start=1):
         link_status = int(line[0])
         if link_status in params["ok_states"]:
-            state = 0
+            state = State.OK
         elif link_status in params["crit_states"]:
-            state = 2
+            state = State.CRIT
         elif link_status in params["warn_states"]:
-            state = 1
+            state = State.WARN
         else:
-            state = 3
-        yield (
-            state,
-            "{}: State: {}".format(
-                count,
-                states.get(link_status, "Not Implemented (%s)" % link_status),
-            ),
+            state = State.UNKNOWN
+        yield Result(
+            state=state,
+            summary=f"{count}: State: {states.get(link_status, f'Not Implemented ({link_status})')}",
         )
 
 
-def parse_bvip_link(string_table: StringTable) -> StringTable:
-    return string_table
-
-
-check_info["bvip_link"] = LegacyCheckDefinition(
+snmp_section_bvip_link = SimpleSNMPSection(
     name="bvip_link",
-    parse_function=parse_bvip_link,
     detect=DETECT_BVIP,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3967.1.5.1.8",
         oids=["1"],
     ),
+    parse_function=parse_bvip_link,
+)
+
+
+check_plugin_bvip_link = CheckPlugin(
+    name="bvip_link",
     service_name="Network Link",
     discovery_function=discover_bvip_link,
     check_function=check_bvip_link,

f36b1b46ebf01ba16f1d444481dfe61a820367e7
Max Höhl <max.hoehl@checkmk.com>
2026-06-10 12:05:01 +0200

    cmk-frontend-vue: expose focus() on CmkIconButton
    Add an imperative `focus()` method to CmkIconButton's public surface,
    moving keyboard focus to the underlying button element. Mirrors the
    focus() method on CmkDropdown so callers manipulating focus across
    mixed control types don't have to special-case CmkIconButton by
    reaching into `$el`.
    
    Change-Id: Ic707a9fc04c618a4cf78c6cf327c1126a6a42f3f

--- packages/cmk-frontend-vue/src/components/CmkIconButton.vue
+++ packages/cmk-frontend-vue/src/components/CmkIconButton.vue
@@ -4,15 +4,26 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
+import { useTemplateRef } from 'vue'
+
 import CmkIcon, { type CmkIconProps } from '@/components/CmkIcon'
 
 defineProps<CmkIconProps>()
 
 defineEmits(['click'])
+
+const button = useTemplateRef<HTMLButtonElement>('button')
+
+defineExpose({
+  focus: () => {
+    button.value?.focus()
+  }
+})
 </script>
 
 <template>
   <button
+    ref="button"
     type="button"
     class="cmk-icon-button"
     :title="title"

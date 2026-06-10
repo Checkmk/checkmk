63921de8fbbe49d1dac5b6622b6cd8b28f159e9c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-06-10 22:43:24 +0200

    Legacy check migration: bvip_temp I
    Change-Id: Ib87072f826edbad6f7f19b541ea210374364b5be

--- cmk/legacy_checks/bvip_temp.py
+++ cmk/legacy_checks/bvip_temp.py
@@ -3,43 +3,57 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    OIDEnd,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.bvip.lib import DETECT_BVIP
-
-check_info = {}
-
-
-def discover_bvip_temp(info):
-    for line in info:
-        # line[0] contains nice names like "CPU" and "System"
-        yield line[0], {}
-
-
-def check_bvip_temp(item, params, info):
-    for nr, value in info:
-        if nr == item:
-            degree_celsius = float(value) / 10
-            return check_temperature(degree_celsius, params, "bvip_temp_%s" % item)
-    return None
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 
 def parse_bvip_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["bvip_temp"] = LegacyCheckDefinition(
+def discover_bvip_temp(section: StringTable) -> DiscoveryResult:
+    for line in section:
+        # line[0] contains nice names like "CPU" and "System"
+        yield Service(item=line[0])
+
+
+def check_bvip_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for nr, value in section:
+        if nr == item:
+            yield from check_temperature(
+                float(value) / 10,
+                params,
+                unique_name=f"bvip_temp_{item}",
+                value_store=get_value_store(),
+            )
+            return
+
+
+snmp_section_bvip_temp = SimpleSNMPSection(
     name="bvip_temp",
-    parse_function=parse_bvip_temp,
     detect=DETECT_BVIP,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.3967.1.1.7.1",
         oids=[OIDEnd(), "1"],
     ),
+    parse_function=parse_bvip_temp,
+)
+
+
+check_plugin_bvip_temp = CheckPlugin(
+    name="bvip_temp",
     service_name="Temperature %s",
     discovery_function=discover_bvip_temp,
     check_function=check_bvip_temp,

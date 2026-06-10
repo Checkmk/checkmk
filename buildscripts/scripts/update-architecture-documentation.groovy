90f752b9241d58dfd5885dc232ba4fe48243b89b
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-06-10 09:01:59 +0200

    Remove bzl_lock_k8s from groovy code after full migration to k8s
    Followed by https://review.lan.tribe29.com/c/ansible-config/+/141646
    
    CMK-24309
    
    Change-Id: I42a73d4ae78a10d50dab3b41833787be4088ab72

--- buildscripts/scripts/update-architecture-documentation.groovy
+++ buildscripts/scripts/update-architecture-documentation.groovy
@@ -10,24 +10,18 @@ void main() {
         stage("Build & Deploy") {
             def container_name = "testing-ubuntu-2204-checkmk-${safe_branch_name.replace('.', '-')}";
 
-            def lock_label = "bzl_lock_${env.NODE_NAME.split('\\.')[0].split('-')[-1]}";
-            if (kubernetes_inherit_from != "UNSET") {
-                lock_label = "bzl_lock_k8s";
-            }
-            lock(label: lock_label, quantity: 1, resource : null) {
-                container(container_name) {
-                    sh("bazel build //doc/documentation")
+            container(container_name) {
+                sh("bazel build //doc/documentation")
 
-                    // groovylint-disable-next-line DuplicateMapLiteral
-                    withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
-                        // The chmod u+w is required:  Bazel creates read-only files, so `scp`
-                        // uploads them as read-only.  The next run then fails to overwrite them.
-                        sh("""
-                            chmod -R u+w bazel-bin/doc/documentation/documentation_html/
-                            scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} \
-                            bazel-bin/doc/documentation/documentation_html/* ${DEV_DOCS_URL}/devdoc
-                        """);
-                    }
+                // groovylint-disable-next-line DuplicateMapLiteral
+                withCredentials([file(credentialsId: 'Release_Key', variable: 'RELEASE_KEY')]) {
+                    // The chmod u+w is required:  Bazel creates read-only files, so `scp`
+                    // uploads them as read-only.  The next run then fails to overwrite them.
+                    sh("""
+                        chmod -R u+w bazel-bin/doc/documentation/documentation_html/
+                        scp -rs -o StrictHostKeyChecking=accept-new -i ${RELEASE_KEY} \
+                        bazel-bin/doc/documentation/documentation_html/* ${DEV_DOCS_URL}/devdoc
+                    """);
                 }
             }
         }

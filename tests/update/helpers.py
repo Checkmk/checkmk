483fc11adbfd129894fc879eb9151a1b647f4708
Matteo Stifano <matteo.stifano@checkmk.com>
2026-06-17 14:14:06 +0200

    test-update: Do not skip tests if base-version is not found
    We are here removing the implicit skip whenever the base-version cannot be
    downloaded.
    
    Implicitly skipping the tests can lead to 'no tests execution' going
    unnoticed.
    
    Change-Id: Ie4ad464084766a2d1d852c5d38321d4f7854d404

--- tests/update/helpers.py
+++ tests/update/helpers.py
@@ -99,23 +99,17 @@ def create_site(base_package: CMKPackageInfo) -> Site:
 
         create_cloud_initial_config()
 
-    try:
-        site = site_factory.get_site(site_name)
-        if is_cloud:
-            from tests.testlib.system.cloud.utils import (  # type: ignore[import-untyped, unused-ignore, import-not-found]
-                cloud_environment,
-            )
-
-            ctx: AbstractContextManager[object] = cloud_environment(site.apache_port)
-        else:
-            ctx = nullcontext()
-        with ctx:
-            site_factory.initialize_site(site, auto_restart_httpd=True)
-    except FileNotFoundError:
-        pytest.skip(
-            f"Base-version '{base_package.version.version}' is not available for distro "
-            f"{os.environ.get('DISTRO')}"
+    site = site_factory.get_site(site_name)
+    if is_cloud:
+        from tests.testlib.system.cloud.utils import (  # type: ignore[import-untyped, unused-ignore, import-not-found]
+            cloud_environment,
         )
+
+        ctx: AbstractContextManager[object] = cloud_environment(site.apache_port)
+    else:
+        ctx = nullcontext()
+    with ctx:
+        site_factory.initialize_site(site, auto_restart_httpd=True)
 
     return site
 

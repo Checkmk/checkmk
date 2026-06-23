ce1ae71aa7c9bcb91e0126bb9605bc4e80b9e23c
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-06-23 14:44:23 +0200

    feat(mon-pages): introduce StateIcon component to wrap icons in mon tables
    Change-Id: I95142a424a5ecec05fd1d0aec9bb25c8a4bb57b8

---
+++ packages/cmk-frontend-vue/src/monitoring/shared/components/StateIcon.vue
@@ -0,0 +1,25 @@
+<!--
+Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+conditions defined in the file COPYING, which is part of this source code package.
+-->
+
+<script setup lang="ts"></script>
+
+<template>
+  <div class="monitoring-state-icon">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.monitoring-state-icon {
+  background-color: var(--ux-theme-5);
+  border-radius: var(--border-radius);
+  height: var(--dimension-7);
+  width: var(--dimension-7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>

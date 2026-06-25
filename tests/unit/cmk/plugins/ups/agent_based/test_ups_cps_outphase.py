f14194ffb8bb2dbcf7d383853fe554389f7d613c
ramadan khalifa <ramadan.khalifa@checkmk.com>
2026-06-25 07:57:16 +0200

    Legacy check migration: ups_cps_outphase II
    Change-Id: I30392169e5b9f078670dcaa4dfe2e26864c5348b

--- tests/unit/cmk/plugins/ups/agent_based/test_ups_cps_outphase.py
+++ tests/unit/cmk/plugins/ups/agent_based/test_ups_cps_outphase.py
@@ -6,7 +6,7 @@
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.ups_cps_outphase import (
+from cmk.plugins.ups.agent_based.ups_cps_outphase import (
     check_ups_cps_outphase,
     discover_ups_cps_outphase,
     parse_ups_cps_outphase,

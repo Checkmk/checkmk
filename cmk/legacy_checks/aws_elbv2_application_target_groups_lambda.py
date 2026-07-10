489f042dff62b2e19b9a6399dbb1a51c158c80cf
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-10 16:25:52 +0200

    Legacy check migration: aws_elbv2_application_target_groups_lambda I
    levels_lambda now defaults to None instead of {}, since check_levels only
    accepts a 2-tuple or None; this preserves the 'no levels' semantics.
    
    Change-Id: I93284d2355323423896da8303d535583bbc8bc0b

--- cmk/legacy_checks/aws_elbv2_application_target_groups_lambda.py
+++ cmk/legacy_checks/aws_elbv2_application_target_groups_lambda.py
@@ -3,45 +3,51 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 
 from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import IgnoreResultsError, StringTable
-from cmk.legacy_includes.aws import (
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    IgnoreResultsError,
+    Service,
+    StringTable,
+)
+from cmk.plugins.aws.lib import (
     check_aws_error_rate,
     check_aws_request_rate,
+    extract_aws_metrics_by_labels,
     get_data_or_go_stale,
+    parse_aws,
 )
-from cmk.plugins.aws.lib import extract_aws_metrics_by_labels, parse_aws
-
-check_info = {}
 
 Section = Mapping[str, Mapping[str, float]]
 
 
 def parse_aws_elbv2_target_groups_lambda(string_table: StringTable) -> Section:
-    return {
-        label: {name: float(value) for name, value in metrics.items()}
-        for label, metrics in extract_aws_metrics_by_labels(
-            ["RequestCount", "LambdaUserError"], parse_aws(string_table)
-        ).items()
-    }
+    return extract_aws_metrics_by_labels(
+        ["RequestCount", "LambdaUserError"], parse_aws(string_table)
+    )
 
 
-def discover_aws_elbv2_target_groups_lambda(section: Section):
-    yield from ((item, {}) for item, data in section.items() if "RequestCount" in data)
+def discover_aws_elbv2_target_groups_lambda(section: Section) -> DiscoveryResult:
+    for item, data in section.items():
+        if "RequestCount" in data:
+            yield Service(item=item)
 
 
-def check_aws_application_elb_target_groups_lambda(item, params, section: Section):
+def check_aws_application_elb_target_groups_lambda(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
     data = get_data_or_go_stale(item, section)
     request_rate = data.get("RequestCount")
     if request_rate is None:
         raise IgnoreResultsError("Currently no data from AWS")
 
-    yield check_aws_request_rate(request_rate)
+    yield from check_aws_request_rate(request_rate)
 
     lambda_error_rate = data.get("LambdaUserError")
     if lambda_error_rate is None:
@@ -52,16 +58,22 @@ def check_aws_application_elb_target_groups_lambda(item, params, section: Sectio
         request_rate,
         "aws_lambda_users_errors_rate",
         "aws_lambda_users_errors_perc",
-        params.get("levels_lambda", {}),
+        params.get("levels_lambda"),
         "Lambda user errors",
     )
 
 
-check_info["aws_elbv2_application_target_groups_lambda"] = LegacyCheckDefinition(
+agent_section_aws_elbv2_application_target_groups_lambda = AgentSection(
     name="aws_elbv2_application_target_groups_lambda",
     parse_function=parse_aws_elbv2_target_groups_lambda,
+)
+
+
+check_plugin_aws_elbv2_application_target_groups_lambda = CheckPlugin(
+    name="aws_elbv2_application_target_groups_lambda",
     service_name="AWS/ApplicationELB Lambda %s",
     discovery_function=discover_aws_elbv2_target_groups_lambda,
     check_function=check_aws_application_elb_target_groups_lambda,
     check_ruleset_name="aws_elbv2_target_errors",
+    check_default_parameters={},
 )

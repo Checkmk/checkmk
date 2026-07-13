8f5ff6b3df5626555984be933042c59af9ae504e
Checkmk release system <noreply@checkmk.com>
2026-07-13 10:20:07 +0200

    Set version to 2.5.0p10

--- agents/plugins/isc_dhcpd.py
+++ agents/plugins/isc_dhcpd.py
@@ -7,7 +7,7 @@
 # mypy: disable-error-code="no-untyped-def"
 # mypy: disable-error-code="possibly-undefined"
 
-__version__ = "2.5.0p9"
+__version__ = "2.5.0p10"
 
 # Monitor leases if ISC-DHCPD
 import calendar
--- agents/plugins/mk_podman.py
+++ agents/plugins/mk_podman.py
@@ -32,7 +32,7 @@ except ImportError:
         return func
 
 
-__version__ = "2.5.0p9"
+__version__ = "2.5.0p10"
 
 LOGGER = logging.getLogger(__name__)
 

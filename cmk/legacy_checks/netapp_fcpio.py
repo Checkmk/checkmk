99b6599c8e463201666df692777e8d48ffbffc1c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-14 15:54:23 +0200

    Legacy check migration: netapp_fcpio I
    Change-Id: Idd3f015f9c1575c49fd3628bc666804feedd2296

--- cmk/legacy_checks/netapp_fcpio.py
+++ cmk/legacy_checks/netapp_fcpio.py
@@ -3,28 +3,33 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
 import time
+from collections.abc import Mapping
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
+from cmk.agent_based.legacy.conversion import (
+    # Temporary compatibility layer until we migrate the corresponding ruleset.
+    check_levels_legacy_compatible as check_levels,
+)
 from cmk.agent_based.v2 import (
     all_of,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
     exists,
     get_rate,
     get_value_store,
     render,
+    Service,
+    SimpleSNMPSection,
     SNMPTree,
     startswith,
     StringTable,
 )
 
-check_info = {}
 
-
-def check_netapp_fcpio(item, params, info):
-    read, write = map(int, info[0])
+def check_netapp_fcpio(params: Mapping[str, Any], section: StringTable) -> CheckResult:
+    read, write = map(int, section[0])
     this_time = int(time.time())
     avg_read = get_rate(
         get_value_store(), "netapp_fcpio.read", this_time, read, raise_overflow=True
@@ -33,7 +38,7 @@ def check_netapp_fcpio(item, params, info):
         get_value_store(), "netapp_fcpio.write", this_time, write, raise_overflow=True
     )
 
-    yield check_levels(
+    yield from check_levels(
         avg_read,
         "read",
         params.get("read"),
@@ -41,7 +46,7 @@ def check_netapp_fcpio(item, params, info):
         infoname="Read",
     )
 
-    yield check_levels(
+    yield from check_levels(
         avg_write,
         "write",
         params.get("write"),
@@ -54,13 +59,12 @@ def parse_netapp_fcpio(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-def discover_netapp_fcpio(info):
-    return [(None, {})]
+def discover_netapp_fcpio(section: StringTable) -> DiscoveryResult:
+    yield Service()
 
 
-check_info["netapp_fcpio"] = LegacyCheckDefinition(
+snmp_section_netapp_fcpio = SimpleSNMPSection(
     name="netapp_fcpio",
-    parse_function=parse_netapp_fcpio,
     detect=all_of(
         startswith(".1.3.6.1.2.1.1.1.0", "NetApp Release"), exists(".1.3.6.1.4.1.789.1.17.20.0")
     ),
@@ -68,6 +72,12 @@ check_info["netapp_fcpio"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.789.1.17",
         oids=["20", "21"],
     ),
+    parse_function=parse_netapp_fcpio,
+)
+
+
+check_plugin_netapp_fcpio = CheckPlugin(
+    name="netapp_fcpio",
     service_name="FCP I/O",
     discovery_function=discover_netapp_fcpio,
     check_function=check_netapp_fcpio,

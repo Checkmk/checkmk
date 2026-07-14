e5761c5eb4ac09509806cda701b996b163e136e3
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-14 16:00:16 +0200

    Legacy check migration: netapp_vfiler I
    Change-Id: Ib36e348185e2138bb688a49f051e2c9cbdbc2ef7

--- cmk/legacy_checks/netapp_vfiler.py
+++ cmk/legacy_checks/netapp_vfiler.py
@@ -3,57 +3,70 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # netappFiler(1) vfiler(16) vfTable (3) vfEntry (1) vfName (2)
 #                                                   vfState(9)
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import all_of, contains, SNMPTree, startswith, StringTable
+from cmk.agent_based.v2 import (
+    all_of,
+    CheckPlugin,
+    CheckResult,
+    contains,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    startswith,
+    State,
+    StringTable,
+)
 
-check_info = {}
 
-
-def discover_netapp_vfiler(info):
-    inventory = []
-    for line in info:
+def discover_netapp_vfiler(section: StringTable) -> DiscoveryResult:
+    for line in section:
         # If we find an entry consisting of name and status, add it to inventory.
         # otherwise we don't inventorize anything.
         if len(line) == 2:
-            inventory.append((line[0], None))
-
-    return inventory
+            yield Service(item=line[0])
 
 
-def check_netapp_vfiler(item, _no_params, info):
-    for vfEntry in info:
+def check_netapp_vfiler(item: str, section: StringTable) -> CheckResult:
+    for vfEntry in section:
         vfName, vfState = vfEntry
         if vfName == item:
             if vfState == "2":
-                return (0, "vFiler is running")
+                yield Result(state=State.OK, summary="vFiler is running")
+                return
             if vfState == "1":
-                return (2, "vFiler is stopped")
-            return (3, "UNKOWN - vFiler status unknown")
-    return (3, "vFiler not found in SNMP output")
+                yield Result(state=State.CRIT, summary="vFiler is stopped")
+                return
+            yield Result(state=State.UNKNOWN, summary="UNKOWN - vFiler status unknown")
+            return
+    yield Result(state=State.UNKNOWN, summary="vFiler not found in SNMP output")
+    return
 
 
 def parse_netapp_vfiler(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["netapp_vfiler"] = LegacyCheckDefinition(
+snmp_section_netapp_vfiler = SimpleSNMPSection(
     name="netapp_vfiler",
-    parse_function=parse_netapp_vfiler,
     detect=all_of(
         contains(".1.3.6.1.2.1.1.1.0", "netapp release"),
         startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.789"),
     ),
-    # get the vfName and vfState from the vfEntry table
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.789.1.16.3.1",
         oids=["2", "9"],
     ),
+    parse_function=parse_netapp_vfiler,
+)
+
+
+check_plugin_netapp_vfiler = CheckPlugin(
+    name="netapp_vfiler",
     service_name="vFiler Status %s",
     discovery_function=discover_netapp_vfiler,
     check_function=check_netapp_vfiler,

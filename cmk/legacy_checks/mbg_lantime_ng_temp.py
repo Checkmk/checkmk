84fc112a5d4133c5c7f734b818ea9ee10093481d
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-24 10:12:42 +0200

    Legacy check migration: mbg_lantime_ng_temp I
    Change-Id: I237f592fd1be192005fc06902a8a186bf9f6dff5

--- cmk/legacy_checks/mbg_lantime_ng_temp.py
+++ cmk/legacy_checks/mbg_lantime_ng_temp.py
@@ -3,39 +3,53 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 from cmk.plugins.meinberg.liblantime import DETECT_MBG_LANTIME_NG
 
-check_info = {}
+
+def discover_mbg_lantime_ng_temp(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service(item="System")
 
 
-def discover_mbg_lantime_ng_temp(info):
-    if info:
-        return [("System", {})]
-    return []
-
-
-def check_mbg_lantime_ng_temp(item, params, info):
-    return check_temperature(float(info[0][0]), params, "mbg_lantime_ng_temp_%s" % item)
+def check_mbg_lantime_ng_temp(
+    item: str, params: TempParamType, section: StringTable
+) -> CheckResult:
+    yield from check_temperature(
+        reading=float(section[0][0]),
+        params=params,
+        unique_name=f"mbg_lantime_ng_temp_{item}",
+        value_store=get_value_store(),
+    )
 
 
 def parse_mbg_lantime_ng_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["mbg_lantime_ng_temp"] = LegacyCheckDefinition(
+snmp_section_mbg_lantime_ng_temp = SimpleSNMPSection(
     name="mbg_lantime_ng_temp",
-    parse_function=parse_mbg_lantime_ng_temp,
     detect=DETECT_MBG_LANTIME_NG,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.5597.30.0.5.2",
         oids=["1"],
     ),
+    parse_function=parse_mbg_lantime_ng_temp,
+)
+
+
+check_plugin_mbg_lantime_ng_temp = CheckPlugin(
+    name="mbg_lantime_ng_temp",
     service_name="Temperature %s",
     discovery_function=discover_mbg_lantime_ng_temp,
     check_function=check_mbg_lantime_ng_temp,

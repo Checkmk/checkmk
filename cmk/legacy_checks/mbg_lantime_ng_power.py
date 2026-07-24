8834fe1816e986b926181144f75712a69ce19a25
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-24 09:55:21 +0200

    Legacy check migration: mbg_lantime_ng_power I
    Change-Id: I13febb8531d1007d92a546d055dc263e9d7652d8

--- cmk/legacy_checks/mbg_lantime_ng_power.py
+++ cmk/legacy_checks/mbg_lantime_ng_power.py
@@ -4,35 +4,37 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from cmk.agent_based.legacy.v0_unstable import (
-    LegacyCheckDefinition,
-    LegacyCheckResult,
-    LegacyDiscoveryResult,
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
 )
-from cmk.agent_based.v2 import SNMPTree, StringTable
 from cmk.plugins.meinberg.liblantime import DETECT_MBG_LANTIME_NG
-
-check_info = {}
 
 type Section = StringTable
 
 
-def discover_mbg_lantime_ng_power(info: Section) -> LegacyDiscoveryResult:
-    for line in info:
-        yield line[0], {}
+def discover_mbg_lantime_ng_power(section: Section) -> DiscoveryResult:
+    for line in section:
+        yield Service(item=line[0])
 
 
-def check_mbg_lantime_ng_power(item: str, _no_params: object, info: Section) -> LegacyCheckResult:
+def check_mbg_lantime_ng_power(item: str, section: Section) -> CheckResult:
     power_states = {
-        "0": (2, "not available"),
-        "1": (2, "down"),
-        "2": (0, "up"),
+        "0": (State.CRIT, "not available"),
+        "1": (State.CRIT, "down"),
+        "2": (State.OK, "up"),
     }
-    for index, power_status in info:
+    for index, power_status in section:
         if item == index:
             power_state, power_state_name = power_states[power_status]
-            infotext = "Status: %s" % power_state_name
-            yield power_state, infotext
+            yield Result(state=power_state, summary=f"Status: {power_state_name}")
             return
 
 
@@ -40,14 +42,19 @@ def parse_mbg_lantime_ng_power(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["mbg_lantime_ng_power"] = LegacyCheckDefinition(
+snmp_section_mbg_lantime_ng_power = SimpleSNMPSection(
     name="mbg_lantime_ng_power",
-    parse_function=parse_mbg_lantime_ng_power,
     detect=DETECT_MBG_LANTIME_NG,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.5597.30.0.5.0.2.1",
         oids=["1", "2"],
     ),
+    parse_function=parse_mbg_lantime_ng_power,
+)
+
+
+check_plugin_mbg_lantime_ng_power = CheckPlugin(
+    name="mbg_lantime_ng_power",
     service_name="Power Supply %s",
     discovery_function=discover_mbg_lantime_ng_power,
     check_function=check_mbg_lantime_ng_power,

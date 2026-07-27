4b30a821324c5572e498a0b13484f612d50bbc97
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-27 21:10:20 +0200

    metric-backend: migrate mk_docker_container_piggybacked rulespec to v1 API
    Replace old GUI API imports and HostRulespec registration with the
    rulesets v1 AgentConfig. Apply Age/Alternative → deployment tuple
    migration pattern. Mark deprecated as before.
    
    Change-Id: Ie257c106eaac87a451202e148353914d8c3b0024

--- cmk/gui/plugins/legacy_bakery_rulesets/mk_docker_container_piggybacked.py
+++ cmk/gui/plugins/legacy_bakery_rulesets/mk_docker_container_piggybacked.py
@@ -3,48 +3,79 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+from collections.abc import Mapping
 
-from cmk.gui.agent_bakery import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato.utils import HostRulespec, rulespec_registry
-from cmk.gui.valuespec import Age, Alternative, Dictionary, FixedValue
-from cmk.ruleset_matcher.definition import RuleGroup
+from cmk.rulesets.v1 import Help, Title
+from cmk.rulesets.v1.form_specs import (
+    CascadingSingleChoice,
+    CascadingSingleChoiceElement,
+    DefaultValue,
+    DictElement,
+    Dictionary,
+    FixedValue,
+    TimeMagnitude,
+    TimeSpan,
+)
+from cmk.rulesets.v1.rule_specs import AgentConfig, Topic
 
 
-def _valuespec_agent_config_mk_docker_container_piggybacked() -> Alternative:
-    return Alternative(
-        title=_("Piggybacked Docker containers"),
-        help=_('This rule is deprecated. Please use "%(name)s" instead.')
-        % {"name": _("Docker node and containers")},
-        elements=[
-            Dictionary(
-                title=_("Deploy the Docker container plug-in"),
-                elements=[
-                    (
-                        "interval",
-                        Age(
-                            title=_("Run asynchronously"),
-                            label=_("Interval for collecting data"),
-                            default_value=300,
+def migrate(value: object) -> Mapping[str, object]:
+    if isinstance(value, dict) and "deployment" in value:
+        return value
+    if value is None:
+        return {"deployment": ("do_not_deploy", None)}
+    if isinstance(value, dict):
+        interval = value.get("interval", 60)
+        if isinstance(interval, (int, float)) and interval > 60:
+            return {"deployment": ("cached", float(interval))}
+        return {"deployment": ("sync", None)}
+    raise ValueError(f"Unexpected value: {value!r}")
+
+
+def _form_spec() -> Dictionary:
+    return Dictionary(
+        help_text=Help('This rule is deprecated. Please use "Docker node and containers" instead.'),
+        elements={
+            "deployment": DictElement(
+                required=True,
+                parameter_form=CascadingSingleChoice(
+                    title=Title("Deployment type"),
+                    elements=(
+                        CascadingSingleChoiceElement(
+                            name="sync",
+                            title=Title("Deploy the plug-in and run it synchronously"),
+                            parameter_form=FixedValue(value=None),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="cached",
+                            title=Title("Deploy the plug-in and run it asynchronously"),
+                            parameter_form=TimeSpan(
+                                displayed_magnitudes=(
+                                    TimeMagnitude.HOUR,
+                                    TimeMagnitude.MINUTE,
+                                    TimeMagnitude.SECOND,
+                                ),
+                                prefill=DefaultValue(300.0),
+                            ),
+                        ),
+                        CascadingSingleChoiceElement(
+                            name="do_not_deploy",
+                            title=Title("Do not deploy the plug-in"),
+                            parameter_form=FixedValue(value=None),
                         ),
                     ),
-                ],
-                optional_keys=["interval"],
+                    prefill=DefaultValue("sync"),
+                ),
             ),
-            FixedValue(
-                value=None,
-                title=_("Do not deploy the Docker container plug-in"),
-                totext=_("(disabled)"),
-            ),
-        ],
+        },
+        migrate=migrate,
     )
 
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        is_deprecated=True,
-        name=RuleGroup.AgentConfig("mk_docker_container_piggybacked"),
-        valuespec=_valuespec_agent_config_mk_docker_container_piggybacked,
-    )
+rule_spec_mk_docker_container_piggybacked = AgentConfig(
+    title=Title("Piggybacked Docker containers"),
+    name="mk_docker_container_piggybacked",
+    topic=Topic.VIRTUALIZATION,
+    parameter_form=_form_spec,
+    is_deprecated=True,
 )

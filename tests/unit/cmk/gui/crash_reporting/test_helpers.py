e972a1f475d66029fb762e27b660d89ac12387d5
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-07-29 09:00:21 +0200

    20139 FIX Crash reports view with broken traceback
    Previously, opening _Monitor_ > _System_ > _Crash reports_ could fail entirely
    with:
    
    _Internal error: too many values to unpack (expected 4)_
    
    This happened when a single stored crash report contained a traceback Checkmk
    could not interpret, for example one recorded as plain text rather than as
    individual code locations. One such report made the whole list inaccessible.
    
    The list now renders. Reports with an uninterpretable traceback are still shown;
    only their _Crash source_ column can no longer tell whether an extension was
    involved. No currently supported Checkmk version writes reports like this, so
    this is a safeguard for reports left behind by other producers.
    
    Crash-Group-ID: 3953
    Jira: CMK-37347
    Change-Id: I8d8054084b9363a7e3a96eee67deb9616b3f134e

---
+++ tests/unit/cmk/gui/crash_reporting/test_helpers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.gui.crash_reporting.helpers import local_files_involved_in_crash
+
+
+def test_local_files_involved_in_crash() -> None:
+    assert local_files_involved_in_crash(
+        [
+            ("/omd/sites/heute/lib/python3/cmk/base/modes/check_mk.py", 1, "main", "do_check()"),
+            (
+                "/omd/sites/heute/local/lib/python3/cmk_addons/plugins/acme/agent_based/acme.py",
+                2,
+                "parse_acme",
+                "return int(line)",
+            ),
+        ]
+    ) == ["/omd/sites/heute/local/lib/python3/cmk_addons/plugins/acme/agent_based/acme.py"]
+
+
+def test_local_files_involved_in_crash_with_stringified_frames() -> None:
+    # Some stored crash reports hold the string representation of the traceback
+    # frames instead of the (filename, lineno, function, line) tuples.
+    assert (
+        local_files_involved_in_crash(
+            [
+                "<FrameSummary file /omd/sites/heute/lib/python3/cmk/checkengine/"
+                "sectionparser.py, line 121 in _parse_raw_data>",
+            ]
+        )
+        == []
+    )

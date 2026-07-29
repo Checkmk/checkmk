5c88e66d88e834da2a3cc817bad6f7c5742b7b42
Alejandro Yunta <alejandro.yunta@checkmk.com>
2026-07-29 11:27:58 +0200

    refactor(mon-hosts): column definitions
    Move host table column definitions to columns.ts
    
    Preparation for CMK-37113
    
    Change-Id: Iee2ad92fe0a54a12fe8b13b9fac526e084fca4bc

--- packages/cmk-frontend-vue/src/monitoring/all-hosts/AllHostsApp.vue
+++ packages/cmk-frontend-vue/src/monitoring/all-hosts/AllHostsApp.vue
@@ -4,11 +4,7 @@ This file is part of Checkmk (https://checkmk.com). It is subject to the terms a
 conditions defined in the file COPYING, which is part of this source code package.
 -->
 <script setup lang="ts">
-import {
-  type ColumnDef,
-  type ColumnPinningState,
-  type RowSelectionState
-} from '@tanstack/vue-table'
+import { type RowSelectionState } from '@tanstack/vue-table'
 import type { MonitoringAllHostsApp } from 'cmk-shared-typing/typescript/monitoring/all_hosts'
 import CmkButton from 'cmk-ui-library/components/CmkButton/CmkButton.vue'
 import CmkIcon from 'cmk-ui-library/components/CmkIcon/CmkIcon.vue'
@@ -26,12 +22,6 @@ import type { HostEntry, HostRef, HostState } from '@/monitoring/shared/api/type
 import { MONITORING_SERVICE } from '@/monitoring/shared/components/MonitoringTableContext'
 import type { CellAction } from '@/monitoring/shared/components/cell/ActionsCell.vue'
 import QuickFilterChip from '@/monitoring/shared/components/filter/QuickFilterChip.vue'
-import type {
-  BooleanGroupFilter,
-  CheckboxListFilter,
-  NumericFilter,
-  StringInputFilter
-} from '@/monitoring/shared/components/filter/types'
 import { ACTION_REFRESH_DELAY_MS, HOST_LIMIT_TIERS } from '@/monitoring/shared/constants'
 
 import MonitoringEmptyState from '../shared/components/MonitoringEmptyState.vue'
@@ -61,6 +51,7 @@ import { createActionRegistry } from '../shared/components/action/registry'
 import { useMonitoringActions } from '../shared/services/useMonitoringActions'
 import { HostActionMenuApi } from './api/actionMenu'
 import { HostApi } from './api/hosts'
+import { buildHostColumnPinning, buildHostColumns } from './columns'
 import HostRow from './components/HostRow.vue'
 import HostOverviewSkeleton from './components/slide-in/HostOverviewSkeleton.vue'
 import HostOverviewTab from './components/slide-in/HostOverviewTab.vue'
@@ -127,194 +118,8 @@ async function loadActionMenu(host: HostRef): Promise<CellAction[]> {
   ]
 }
 
-const stateFilter: CheckboxListFilter<'state'> = {
-  type: 'checkbox-list',
-  field: 'state',
-  options: [
-    { value: 'UP', title: _t('UP') },
-    { value: 'DOWN', title: _t('DOWN') },
-    { value: 'UNREACHABLE', title: _t('UNREACH') }
-  ] satisfies { value: HostState; title: string }[]
-}
-
-const nameFilter: StringInputFilter<'name'> = {
-  type: 'string-input',
-  field: 'name'
-}
-
-const addressFilter: StringInputFilter<'address'> = {
-  type: 'string-input',
-  field: 'address'
-}
-
-const totalServicesFilter: NumericFilter<'num_services'> = {
-  type: 'numeric',
-  field: 'num_services'
-}
-
-const okServicesFilter: NumericFilter<'num_services_ok'> = {
-  type: 'numeric',
-  field: 'num_services_ok'
-}
-
-const warnServicesFilter: NumericFilter<'num_services_warn'> = {
-  type: 'numeric',
-  field: 'num_services_warn'
-}
-
-const critServicesFilter: NumericFilter<'num_services_crit'> = {
-  type: 'numeric',
-  field: 'num_services_crit'
-}
-
-const unknownServicesFilter: NumericFilter<'num_services_unknown'> = {
-  type: 'numeric',
-  field: 'num_services_unknown'
-}
-
-const pendingServicesFilter: NumericFilter<'num_services_pending'> = {
-  type: 'numeric',
-  field: 'num_services_pending'
-}
-
-const modesFilter: BooleanGroupFilter<'in_downtime' | 'acknowledged'> = {
-  type: 'boolean-group',
-  groups: [
-    { field: 'in_downtime', title: _t('In downtime') },
-    { field: 'acknowledged', title: _t('Acknowledged') }
-  ]
-}
-
-const columns: ColumnDef<HostEntry>[] = [
-  {
-    id: 'select',
-    header: '',
-    enableSorting: false,
-    minSize: 36,
-    maxSize: 36,
-    meta: { selectColumn: true, justify: 'center' }
-  },
-  {
-    accessorKey: 'state',
-    header: _t('State'),
-    sortDescFirst: true,
-    minSize: 74,
-    maxSize: 100,
-    meta: { filter: stateFilter }
-  },
-  {
-    accessorKey: 'modes',
-    header: _t('Mode'),
-    enableSorting: false,
-    minSize: 80,
-    maxSize: 80,
-    meta: { justify: 'left', filter: modesFilter }
-  },
-  {
-    accessorKey: 'name',
-    header: _t('Host'),
-    sortDescFirst: false,
-    minSize: 150,
-    meta: { filter: nameFilter }
-  },
-  {
-    accessorKey: 'address',
-    header: _t('IP address'),
-    sortDescFirst: false,
-    minSize: 100,
-    maxSize: 300,
-    meta: { filter: addressFilter }
-  },
-  {
-    accessorKey: 'num_services',
-    header: _t('All services'),
-    sortDescFirst: true,
-    meta: {
-      justify: 'right',
-      filter: totalServicesFilter,
-      headerTitle: _t('Total number of services')
-    },
-    minSize: 70,
-    maxSize: 130
-  },
-  {
-    accessorKey: 'num_services_ok',
-    header: _t('OK'),
-    sortDescFirst: true,
-    meta: {
-      justify: 'right',
-      filter: okServicesFilter,
-      headerTitle: _t('Number of services in OK state')
-    },
-    minSize: 70,
-    maxSize: 70
-  },
-  {
-    accessorKey: 'num_services_warn',
-    header: _t('Wa'),
-    sortDescFirst: true,
-    meta: {
-      justify: 'right',
-      filter: warnServicesFilter,
-      headerTitle: _t('Number of services in warning state')
-    },
-    minSize: 70,
-    maxSize: 70
-  },
-  {
-    accessorKey: 'num_services_crit',
-    header: _t('Cr'),
-    sortDescFirst: true,
-    meta: {
-      justify: 'right',
-      filter: critServicesFilter,
-      headerTitle: _t('Number of services in critical state')
-    },
-    minSize: 70,
-    maxSize: 70
-  },
-  {
-    accessorKey: 'num_services_unknown',
-    header: _t('Un'),
-    sortDescFirst: true,
-    meta: {
-      justify: 'right',
-      filter: unknownServicesFilter,
-      headerTitle: _t('Number of services in unknown state')
-    },
-    minSize: 70,
-    maxSize: 70
-  },
-  {
-    accessorKey: 'num_services_pending',
-    header: _t('Pd'),
-    sortDescFirst: true,
-    meta: {
-      justify: 'right',
-      filter: pendingServicesFilter,
-      headerTitle: _t('Number of services in pending state')
-    },
-    minSize: 70,
-    maxSize: 70
-  },
-  ...(hasRowActions
-    ? [
-        {
-          id: 'actions',
-          header: _t('Actions'),
-          enableSorting: false,
-          minSize: 75,
-          maxSize: 75,
-          meta: { justify: 'right' }
-        } satisfies ColumnDef<HostEntry>
-      ]
-    : [])
-]
-
-const columnPinning: ColumnPinningState = {
-  left: ['select', 'state', 'modes', 'name'],
-  ...(hasRowActions ? { right: ['actions'] } : {})
-}
+const columns = buildHostColumns({ includeActions: hasRowActions })
+const columnPinning = buildHostColumnPinning({ includeActions: hasRowActions })
 
 const hostApi = new HostApi()
 

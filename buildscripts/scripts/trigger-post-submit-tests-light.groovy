c6c156bd2b99636104bd1e04afcac8bb3c6af125
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-07-29 11:13:31 +0200

    trigger-post-submit-tests: Ensure stable order
    This way the Jenkins job overview has comparable results and the inrush
    of job spawned is stretched a little bit.
    
    Contributes to CMK-36601
    
    Change-Id: I008481ee8f23ea699874d9875e02263d20bd0c2e
    (cherry picked from commit f66f8ec95c38961afcedb0344fa65dfd56fd8f56)

--- buildscripts/scripts/trigger-post-submit-tests-light.groovy
+++ buildscripts/scripts/trigger-post-submit-tests-light.groovy
@@ -34,6 +34,11 @@ def main() {
         "test-shell-unit",
         "test-typescript-types",
     ];
+    /// In order to ensure a fixed order for stages executed in parallel,
+    /// we wait an increasing amount of time (N * 100ms).
+    /// Without this we end up with a capped build overview matrix in the job view (Jenkins doesn
+    /// like changing order or amount of stages, which will happen with stages started `via paral
+    def timeOffsetForOrder = 0;
 
     print(
         """
@@ -46,6 +51,8 @@ def main() {
 
     def stages = job_names.collectEntries { job_name ->
         [("${job_name}") : {
+            sleep(0.1 * timeOffsetForOrder++);
+
             smart_stage(
                 name: "Trigger ${job_name}",
             ) {

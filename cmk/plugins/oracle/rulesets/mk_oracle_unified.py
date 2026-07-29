3ed1b0442bd4326c2a20552bb157095f4812df71
Sergey Kipnis <sergey.kipnis@checkmk.com>
2026-07-29 17:22:19 +0200

    mk-oracle: add permissions check to GUI
    CMK-37143
    
    Change-Id: I7fa16722c9659945dcecd53bcfaa6649e2db8e51

--- cmk/plugins/oracle/rulesets/mk_oracle_unified.py
+++ cmk/plugins/oracle/rulesets/mk_oracle_unified.py
@@ -553,6 +553,52 @@ def _discovery() -> Dictionary:
     )
 
 
+def _permissions() -> CascadingSingleChoice:
+    return CascadingSingleChoice(
+        title=Title("Oracle binaries permissions check (Windows only)"),
+        help_text=Help(
+            "Due to security reasons the plug-in being executed as admin will check "
+            "permissions for Oracle executables. If the modification of Oracle binaries "
+            "is allowed for non-admin users or groups then the plug-in stops processing "
+            "thus breaking Oracle monitoring: execution of normal user code at "
+            "elevated level means security vulnerability. "
+            "You may <tt>disable</tt> this option if it is the only method to "
+            "continue monitoring the Oracle database. "
+            "Usually you need to <tt>disable</tt> the option only in the following case: "
+            "It's impossible to correctly adjust permissions for Oracle binaries, "
+            "mode group with group Administrator is not applicable and "
+            "it is not possible to use a custom account to monitor the Oracle database. "
+            "Even if the check is enabled you may allow some groups and/or users to "
+            "still have write access to the Oracle binaries."
+        ),
+        prefill=DefaultValue("enabled"),
+        elements=[
+            CascadingSingleChoiceElement(
+                name="enabled",
+                title=Title("Enable"),
+                parameter_form=Dictionary(
+                    elements={
+                        "safe_entries": DictElement(
+                            required=False,
+                            parameter_form=List(
+                                title=Title("Safe groups and/or users"),
+                                element_template=String(),
+                                add_element_label=Label("Add new group or user"),
+                                editable_order=False,
+                            ),
+                        ),
+                    }
+                ),
+            ),
+            CascadingSingleChoiceElement(
+                name="disabled",
+                title=Title("Disable"),
+                parameter_form=FixedValue(value=None),
+            ),
+        ],
+    )
+
+
 def _oracle_client_library_options() -> Dictionary:
     return Dictionary(
         title=Title("Oracle Instant Client options"),
@@ -654,6 +700,7 @@ def _options(is_default_options: bool = True) -> Dictionary:
             parameter_form=_oracle_client_library_options(),
             required=False,
         ),
+        "validate_permissions": DictElement(parameter_form=_permissions(), required=False),
     }
     if not is_default_options:
         _ = elements.pop("max_connections")

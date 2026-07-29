ad117f638d271d7ce8863c657be438cf390f8754
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-29 16:30:30 +0200

    mkp-tool: add smoke test
    Change-Id: I6a15a856a1676c8d8838360749013efc04883bcc

---
+++ packages/cmk-mkp-tool/tests/test___version__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.mkp_tool.__version__ import __version__
+
+
+def test___version___is_set() -> None:
+    assert __version__

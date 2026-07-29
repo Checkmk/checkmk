b28daa49681cd5373781d9f87fdff8fa71f58c3e
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-29 14:07:37 +0200

    validate config: prepare to move entry point
    Change-Id: Ia5dc936da2203658383f0b937af17f8fb41020e3

---
+++ cmk/validate_config/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from ._impl import validate_mk_files as validate_mk_files
+from ._impl import validate_timeperiods as validate_timeperiods
+from ._impl import ValidationResult as ValidationResult

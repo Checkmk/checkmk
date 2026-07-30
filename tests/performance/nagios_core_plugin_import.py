5d80d6870903e0a8fdc052012b9f1ef8fe0d2b9b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-30 12:54:14 +0200

    tests/performance: clean up
    * don't use private symbols
    * allow legitimate imports
    * use dedicated APIs
    
    Change-Id: Ic847ec864f9dc7c4a8a484f35158ee5b00109f29

--- tests/performance/nagios_core_plugin_import.py
+++ tests/performance/nagios_core_plugin_import.py
@@ -4,21 +4,21 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 import importlib
 import importlib.resources
-import re
-import sys
-from collections.abc import Generator
+from collections.abc import Sequence
 from os import getenv
 from pathlib import Path
 from pprint import pformat
-from types import ModuleType
-from typing import Final, get_args
+from typing import Final
 
-import cmk.base.core.nagios  # astrein: disable=cmk-module-layer-violation
-from cmk.base.core.nagios import HostCheckConfig  # astrein: disable=cmk-module-layer-violation
-from cmk.ccc.hostaddress import HostAddress, HostName  # astrein: disable=cmk-module-layer-violation
-from cmk.discover_plugins import PluginLocation  # astrein: disable=cmk-module-layer-violation
-from cmk.utils.plugin_loader import _find_modules  # astrein: disable=cmk-module-layer-violation
-from cmk.validate_plugins import _AgentBasedPlugins  # astrein: disable=cmk-module-layer-violation
+import cmk.base.core.nagios
+from cmk.agent_based import v2, v3_unstable
+from cmk.base.core.nagios import HostCheckConfig
+from cmk.ccc.hostaddress import HostAddress, HostName
+from cmk.discover_plugins import (
+    discover_all_plugins,
+    PluginGroup,
+    PluginLocation,
+)
 
 
 class NagiosCorePluginImport:
@@ -31,84 +31,29 @@ class NagiosCorePluginImport:
         self.omd_root: Final[Path] = Path(getenv("OMD_ROOT", "/omd/sites/foobar"))
         self.host_name: Final[HostName] = HostName("localhost")
         self.host_ip: Final[HostAddress] = HostAddress("127.0.0.1")
-        self.agent_based_plugins: dict[str, list[str]] = {}
         self.host_check_folder = (
             self.omd_root / "var/check_mk/core/helper_config/latest/host_checks"
         )
         self.host_check_file = self.host_check_folder / f"check_{self.host_name}.py"
 
-    @staticmethod
-    def load_agent_based_plugin_modules() -> Generator[tuple[str, ModuleType]]:
-        """Load all agent based plugin modules
+    def discover_agent_based_plugins(self) -> Sequence[PluginLocation]:
+        return list(
+            discover_all_plugins(
+                PluginGroup.AGENT_BASED,
+                {**v2.entry_point_prefixes(), **v3_unstable.entry_point_prefixes()},
+                skip_wrong_types=False,
+                raise_errors=True,
+            ).plugins
+        )
 
-        This method imports all agent based plugin modules and yields the module name
-        and the imported module.
-
-        Returns:
-            A generator of 2-tuples of module-name and module.
-
-        Raises:
-            ImportError
-        """
-        package_name = "cmk.plugins"
-        module_pattern = r".*\.agent_based\..*"
-
-        def onerror_func(name: str) -> None:
-            if exc := sys.exc_info()[1]:
-                raise ImportError(name=name) from exc
-
-        try:
-            __import__(package_name)
-        except BaseException as exc:
-            raise ImportError from exc
-        for module_name in _find_modules(sys.modules[package_name], onerror=onerror_func):
-            if not re.match(module_pattern, module_name):
-                continue
-            try:
-                yield module_name, importlib.import_module(module_name)
-            except BaseException as exc:
-                raise ImportError from exc
-
-    def list_agent_based_plugins(self) -> dict[str, list[str]]:
-        """List all agent based plugins
-
-        This method walks through all agent based plugin modules and returns
-        a dictionary mapping module names to lists of plugin names.
-
-        Returns:
-            dict[str, list[str]]: A dictionary where keys are module names and values are lists of
-                plugin names found in each module.
-        """
-        # Collecting plugin list by walking through all plugin modules recursively
-        modules_and_plugins: dict[str, list[str]] = {}
-        for module_name, module in self.load_agent_based_plugin_modules():
-            for plugin_name in dir(module):
-                try:
-                    plugin_type = getattr(module, plugin_name).__class__.__name__
-                    AgentBasedPluginTypes = [_.__name__ for _ in get_args(_AgentBasedPlugins)]
-                    if plugin_type not in AgentBasedPluginTypes:
-                        continue
-                except AttributeError:
-                    continue
-                if module_name not in modules_and_plugins:
-                    modules_and_plugins[module_name] = [plugin_name]
-                if plugin_name not in modules_and_plugins[module_name]:
-                    modules_and_plugins[module_name].append(plugin_name)
-        return modules_and_plugins
-
-    def dump_host_check_file(self) -> None:
+    def dump_host_check_file(self, plugin_locations: Sequence[PluginLocation]) -> None:
         """Creates a host check file which loads all agent based plugins"""
-        plugin_locations = [
-            PluginLocation(module=module_name, name=plugin_name)
-            for module_name, plugin_names in self.agent_based_plugins.items()
-            for plugin_name in plugin_names
-        ]
         host_check_config = HostCheckConfig(
             delay_precompile=False,
             src=self.host_check_file.as_posix(),
             dst=self.host_check_file.as_posix().removesuffix(".py"),
             verify_site_python=True,
-            locations=plugin_locations,
+            locations=list(plugin_locations),
             checks_to_load=[
                 f"{self.omd_root}/share/check_mk/checks/kernel",
                 f"{self.omd_root}/share/check_mk/checks/mem_linux",
@@ -127,8 +72,7 @@ class NagiosCorePluginImport:
         self.host_check_file.write_text(host_check_text, encoding="utf-8")
 
     def main(self) -> None:
-        self.agent_based_plugins = self.list_agent_based_plugins()
-        self.dump_host_check_file()
+        self.dump_host_check_file(self.discover_agent_based_plugins())
 
 
 if __name__ == "__main__":

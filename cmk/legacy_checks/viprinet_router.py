84dd8e726f817fb0db57773bdf456a69e3ccb39a
Logan Connolly <logan.connolly@checkmk.com>
2026-07-31 19:02:49 +0200

    Legacy check migration: viprinet_router I
    Change-Id: Ief320d09129e58098082d1b4e5448d87ab31309c

--- cmk/legacy_checks/viprinet_router.py
+++ cmk/legacy_checks/viprinet_router.py
@@ -3,40 +3,49 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from typing import Any
 
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import DiscoveryResult, Service, SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.viprinet.lib import DETECT_VIPRINET
 
-check_info = {}
 
-
-def check_viprinet_router(_no_item, params, info):
+def check_viprinet_router(params: Mapping[str, Any], section: StringTable) -> CheckResult:
     router_mode_map = {
         "0": "Node",
         "1": "Hub",
         "2": "Hub running as HotSpare",
         "3": "Hotspare-Hub replacing another router",
     }
-    current_mode = info[0][0][0]
+    current_mode = section[0][0][0]
     mode = router_mode_map.get(current_mode)
 
-    expect_mode = params.get("expect_mode")
-    if expect_mode:
+    if expect_mode := params.get("expect_mode"):
         # Requires mode found on inventory
         if expect_mode == "inv":
             expect_mode = params.get("mode_inv")
         if expect_mode in router_mode_map and expect_mode != current_mode:
-            return (
-                2,
-                f"Mode '{mode}' differs from expected mode '{router_mode_map.get(expect_mode)}'",
+            yield Result(
+                state=State.CRIT,
+                summary=f"Mode '{mode}' differs from expected mode '{router_mode_map.get(expect_mode)}'",
             )
+            return
 
     if mode:
-        return (0, mode)
-    return (3, "Undefined Mode")
+        yield Result(state=State.OK, summary=mode)
+        return
+
+    yield Result(state=State.UNKNOWN, summary="Undefined Mode")
 
 
 def parse_viprinet_router(string_table: StringTable) -> StringTable:
@@ -48,16 +57,22 @@ def discover_viprinet_router(section: StringTable) -> DiscoveryResult:
         yield Service(parameters={"mode_inv": section[0][0][0]})
 
 
-check_info["viprinet_router"] = LegacyCheckDefinition(
+snmp_section_viprinet_router = SimpleSNMPSection(
     name="viprinet_router",
-    parse_function=parse_viprinet_router,
     detect=DETECT_VIPRINET,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.35424.1.1",
         oids=["5"],
     ),
+    parse_function=parse_viprinet_router,
+)
+
+
+check_plugin_viprinet_router = CheckPlugin(
+    name="viprinet_router",
     service_name="Router Mode",
     discovery_function=discover_viprinet_router,
     check_function=check_viprinet_router,
     check_ruleset_name="viprinet_router",
+    check_default_parameters={},
 )

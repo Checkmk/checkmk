fd7765201b386bbf46b5c3c1afda5b6769bd0bc1
Logan Connolly <logan.connolly@checkmk.com>
2026-07-31 19:12:54 +0200

    Legacy check migration: viprinet_temp I
    Change-Id: Icfdc9f03c97acda4944f004dc30673198864ddf9

--- cmk/legacy_checks/viprinet_temp.py
+++ cmk/legacy_checks/viprinet_temp.py
@@ -3,20 +3,28 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import DiscoveryResult, Service, SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 from cmk.plugins.viprinet.lib import DETECT_VIPRINET
 
-check_info = {}
 
-
-def check_viprinet_temp(item, params, info):
-    reading = int(info[0][item == "System"])
-    return check_temperature(reading, params, "viprinet_temp_%s" % item)
+def check_viprinet_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    reading = int(section[0][item == "System"])
+    yield from check_temperature(
+        reading=reading,
+        params=params,
+        unique_name=f"viprinet_temp_{item}",
+        value_store=get_value_store(),
+    )
 
 
 def parse_viprinet_temp(string_table: StringTable) -> StringTable:
@@ -29,16 +37,22 @@ def discover_viprinet_temp(section: StringTable) -> DiscoveryResult:
         yield Service(item="System")
 
 
-check_info["viprinet_temp"] = LegacyCheckDefinition(
+snmp_section_viprinet_temp = SimpleSNMPSection(
     name="viprinet_temp",
-    parse_function=parse_viprinet_temp,
     detect=DETECT_VIPRINET,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.35424.1.2",
         oids=["3", "4"],
     ),
+    parse_function=parse_viprinet_temp,
+)
+
+
+check_plugin_viprinet_temp = CheckPlugin(
+    name="viprinet_temp",
     service_name="Temperature %s",
     discovery_function=discover_viprinet_temp,
     check_function=check_viprinet_temp,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )

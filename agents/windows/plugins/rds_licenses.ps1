67c23b1d16c2a77aaaf615f300d5d176232d5e61
Matthew Rindel <matthew.rindel@checkmk.com>
2026-08-03 07:52:02 +0200

    rds_licenses updated to not populate agent output
    SUP-29618
    
    Change-Id: Iee3045a056f0dc9f452124bb7381a2736b09a5ec

--- agents/windows/plugins/rds_licenses.ps1
+++ agents/windows/plugins/rds_licenses.ps1
@@ -28,9 +28,9 @@
 $CMK_VERSION = "3.0.0b1"
 
 function Show-Help {
-    Write-Host "Usage: GeneratePerDeviceReport.ps1 [-Server ServerName]"
-    Write-Host "If no ServerName is provided, then report generation"
-    Write-Host "is attempted at the host machine"
+    Write-Error "Usage: GeneratePerDeviceReport.ps1 [-Server ServerName]"
+    Write-Error "If no ServerName is provided, then report generation"
+    Write-Error "is attempted at the host machine"
     exit 1
 }
 
@@ -43,11 +43,11 @@ function Generate-PerDeviceReport {
         $ObjWMIService = Get-CimInstance -Namespace $NameSpace -ComputerName $ServerName -ClassName "Win32_TSLicenseKeyPack" -ErrorAction Stop
     }
     catch {
-        Write-Host "Unable to connect to the Namespace" -ForegroundColor Red
+        Write-Error "Unable to connect to the Namespace"
         exit 2
     }
     if ($ObjWMIService.Count -eq 0) {
-        Write-Host "No license key packs found" -ForegroundColor Red
+        Write-Error "No license key packs found"
         exit 5
     }
     Write-Host "<<<rds_licenses:sep(44)>>>"

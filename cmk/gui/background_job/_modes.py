18ef7110a475a9005a2fba29b269b01652640017
Maximilian Wirtz <maximilian.wirtz@checkmk.com>
2026-08-05 12:17:24 +0200

    22115 SEC Fix missing authorization for viewing background jobs
    VULNM-32
    CMK-37255
    
    Change-Id: I20874a20d0a10cdd4582b2d6f3737b63cea95149

--- cmk/gui/background_job/_modes.py
+++ cmk/gui/background_job/_modes.py
@@ -10,6 +10,7 @@ from collections.abc import Collection, Iterator
 from cmk.gui import gui_background_job
 from cmk.gui.background_job import BackgroundJob, BackgroundStatusSnapshot, job_registry
 from cmk.gui.breadcrumb import Breadcrumb
+from cmk.gui.exceptions import MKUserError
 from cmk.gui.htmllib.html import html
 from cmk.gui.http import request
 from cmk.gui.i18n import _
@@ -150,6 +151,9 @@ class ModeBackgroundJobDetails(WatoMode):
 
     def page(self) -> None:
         job = BackgroundJob(job_id := request.get_ascii_input_mandatory("job_id"))
+        if not job.is_available():
+            raise MKUserError(None, _("Background job info is not available"))
+
         if job.is_active():
             html.div(html.render_message(_("Loading...")), id_="async_progress_msg")
             html.div("", id_="status_container")
@@ -159,11 +163,9 @@ class ModeBackgroundJobDetails(WatoMode):
             )
         else:
             job_snapshot: BackgroundStatusSnapshot | None = _get_job_snaphot(job)
-            if job_snapshot is not None and job.exists():
+            if job_snapshot is not None:
                 job_manager = gui_background_job.GUIBackgroundJobManager()
                 job_manager.show_job_details_from_snapshot(job_snapshot)
-            else:
-                html.show_message(_("Background job info is not available"))
 
 
 class ModeAjaxBackgroundJobDetails(AjaxPage):
@@ -186,7 +188,7 @@ class ModeAjaxBackgroundJobDetails(AjaxPage):
 
     def _show_details_page(self, job_id: str) -> BackgroundStatusSnapshot | None:
         job = BackgroundJob(job_id)
-        if not job.exists():
+        if not job.is_available():
             html.show_message(_("Background job info is not available"))
             return None
 

49076a63f97dcbfc377dba4fc776b37a9406d593
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-05 16:49:43 +0200

    fix integration test even more
    Change-Id: I234dc235f8469e630f79055ca3b3fb283c0fdb74

--- tests/system/singlesite/omd/test_python.py
+++ tests/system/singlesite/omd/test_python.py
@@ -209,8 +209,8 @@ def test_python_optimized_and_lto_enable(site: Site) -> None:
     [
         pytest.param(
             "cmk.base.localize",
-            f"lib/python{PYVER.major}.{PYVER.minor}/site-packages/cmk/base/config.py",
-            f"lib/python{PYVER.major}.{PYVER.minor}/site-packages/cmk/base/__pycache__/config.cpython-{PYVER.major}{PYVER.minor}.pyc",
+            f"lib/python{PYVER.major}.{PYVER.minor}/site-packages/cmk/base/localize.py",
+            f"lib/python{PYVER.major}.{PYVER.minor}/site-packages/cmk/base/__pycache__/localize.cpython-{PYVER.major}{PYVER.minor}.pyc",
             id="pyc for imports from the big monolith cmk namespace",
         ),
         pytest.param(

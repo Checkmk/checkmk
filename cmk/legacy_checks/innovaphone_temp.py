43828330e6f5c381accdf17c13f1bedd2f9fbbf1
Logan Connolly <logan.connolly@checkmk.com>
2026-08-06 09:50:37 +0200

    innovaphone(temp): migrate legacy plugin
    Change-Id: If2faca91a01a645bc13e5106a2fa347916ea7390

--- cmk/legacy_checks/innovaphone_temp.py
+++ cmk/legacy_checks/innovaphone_temp.py
@@ -3,30 +3,43 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Service,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-from cmk.legacy_includes.temperature import check_temperature
 
-check_info = {}
+def discover_innovaphone_temp(section: StringTable) -> DiscoveryResult:
+    yield Service(item="Ambient")
 
 
-def discover_innovaphone_temp(info):
-    yield "Ambient", {}
-
-
-def check_innovaphone_temp(item, params, info):
-    return check_temperature(int(info[0][1]), params, "innovaphone_temp_%s" % item)
+def check_innovaphone_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    yield from check_temperature(
+        int(section[0][1]),
+        params,
+        unique_name=f"innovaphone_temp_{item}",
+        value_store=get_value_store(),
+    )
 
 
 def parse_innovaphone_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["innovaphone_temp"] = LegacyCheckDefinition(
+agent_section_innovaphone_temp = AgentSection(
     name="innovaphone_temp",
     parse_function=parse_innovaphone_temp,
+)
+
+
+check_plugin_innovaphone_temp = CheckPlugin(
+    name="innovaphone_temp",
     service_name="Temperature %s",
     discovery_function=discover_innovaphone_temp,
     check_function=check_innovaphone_temp,

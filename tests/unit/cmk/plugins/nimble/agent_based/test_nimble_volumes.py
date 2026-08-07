a0eacc15d533aadeedad44c547a924862220fbbd
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-08-07 09:37:08 +0200

    22141 FIX nimble_volumes: Fix crash on volumes without monitoring data
    Jira: CMK-37374
    Crash-Group-ID: 4842
    Latent-Since: I117714255a44857304c78a59e5c5fa5e042c8549
    Change-Id: Ib4e361eef82c98376a4e9caf251d5e76319b08b7

--- tests/unit/cmk/plugins/nimble/agent_based/test_nimble_volumes.py
+++ tests/unit/cmk/plugins/nimble/agent_based/test_nimble_volumes.py
@@ -3,8 +3,6 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-import pytest
-
 from cmk.agent_based.v2 import Metric, Result, Service, State
 from cmk.plugins.lib.df import FILESYSTEM_DEFAULT_PARAMS
 from cmk.plugins.nimble.agent_based.nimble_volumes import (
@@ -35,7 +33,6 @@ def test_check_ok_volume() -> None:
     assert Result(state=State.OK, summary="Used: 50.00% - 512 TiB of 1.00 PiB") in results
 
 
-@pytest.mark.xfail(strict=True, reason="CMK-37374")
 def test_check_does_not_crash_on_empty_size_values() -> None:
     section = parse_nimble_volumes(STRING_TABLE)
     assert not list(

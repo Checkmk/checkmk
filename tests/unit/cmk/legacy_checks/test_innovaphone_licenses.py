1a13c22d6bdcf11978dfb197e7a67839641f40b4
Logan Connolly <logan.connolly@checkmk.com>
2026-08-07 07:03:02 +0200

    innovaphone(licenses): add xfail test coverage
    These are not meant to be full test coverage of the licenses module, but
    rather only to showcase the behavior change of the fix.
    
    CMK-37821
    
    Change-Id: I6d99b28bd50db470497ec3089ad97a3a0bd24bd2

---
+++ tests/unit/cmk/legacy_checks/test_innovaphone_licenses.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# mypy: disable-error-code="misc"
+# mypy: disable-error-code="no-untyped-call"
+
+import pytest
+
+from cmk.legacy_checks.innovaphone_licenses import check_innovaphone_licenses
+
+
+@pytest.mark.xfail(strict=True, reason="metric perfdata is missing its upper boundary")
+def test_check_innovaphone_licenses_metric_boundaries() -> None:
+    _state, _message, perf = check_innovaphone_licenses(
+        None, {"levels": (90.0, 95.0)}, [["100", "50"]]
+    )
+    assert perf == [("licenses", 50.0, None, None, 0, 100.0)]
+
+
+@pytest.mark.xfail(strict=True, raises=ZeroDivisionError, reason="a total of 0 crashes the check")
+def test_check_innovaphone_licenses_zero_total() -> None:
+    assert check_innovaphone_licenses(None, {"levels": (90.0, 95.0)}, [["0", "0"]]) == (
+        3,
+        "Used 0/0 Licences",
+        [("licenses", 0.0, None, None, 0, 0.0)],
+    )

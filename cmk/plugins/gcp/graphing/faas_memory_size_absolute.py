0db9840ef730e7fffd1673d16afb5f6e4c649ba4
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-08-13 09:00:12 +0200

    chore(i18n): source string fixes (plugins_gcp)
    Jira: CMK-37968
    Change-Id: I9005a0ce6cc0d7c0f3a7b0d0b7c44ced8c2fa968

--- cmk/plugins/gcp/graphing/faas_memory_size_absolute.py
+++ cmk/plugins/gcp/graphing/faas_memory_size_absolute.py
@@ -9,26 +9,26 @@ UNIT_BYTES = metrics.Unit(metrics.IECNotation("B"))
 
 metric_faas_memory_size_absolute_50 = metrics.Metric(
     name="faas_memory_size_absolute_50",
-    title=Title("Memory Size (50th percentile)"),
+    title=Title("Memory size (50th percentile)"),
     unit=UNIT_BYTES,
     color=metrics.Color.BLUE,
 )
 metric_faas_memory_size_absolute_95 = metrics.Metric(
     name="faas_memory_size_absolute_95",
-    title=Title("Memory Size (95th percentile)"),
+    title=Title("Memory size (95th percentile)"),
     unit=UNIT_BYTES,
     color=metrics.Color.GREEN,
 )
 metric_faas_memory_size_absolute_99 = metrics.Metric(
     name="faas_memory_size_absolute_99",
-    title=Title("Memory Size (99th percentile)"),
+    title=Title("Memory size (99th percentile)"),
     unit=UNIT_BYTES,
     color=metrics.Color.PURPLE,
 )
 
 graph_faas_memory_size_absolute = graphs.Graph(
     name="faas_memory_size_absolute",
-    title=Title("Memory Size"),
+    title=Title("Memory size"),
     simple_lines=[
         "faas_memory_size_absolute_50",
         "faas_memory_size_absolute_95",

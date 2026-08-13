f84afa9ac5b1d15431bc023f3cbb2bcca8a64bf6
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-08-13 08:29:16 +0200

    chore(i18n): source string fixes (plugins_varnish)
    Jira: CMK-37968
    Change-Id: I9a39d7664b426f3b63714f28fe4b148da90abd60

--- cmk/plugins/varnish/graphing/esi_errors_and_warnings.py
+++ cmk/plugins/varnish/graphing/esi_errors_and_warnings.py
@@ -15,14 +15,14 @@ metric_varnish_esi_errors_rate = metrics.Metric(
 )
 metric_varnish_esi_warnings_rate = metrics.Metric(
     name="varnish_esi_warnings_rate",
-    title=Title("ESI Warnings"),
+    title=Title("ESI warnings"),
     unit=UNIT_PER_SECOND,
     color=metrics.Color.BLUE,
 )
 
 graph_varnish_esi_errors_and_warnings = graphs.Graph(
     name="varnish_esi_errors_and_warnings",
-    title=Title("Varnish ESI Errors and Warnings"),
+    title=Title("Varnish ESI errors and warnings"),
     simple_lines=[
         "varnish_esi_errors_rate",
         "varnish_esi_warnings_rate",

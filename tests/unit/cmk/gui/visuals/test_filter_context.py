fb9c711b5d7e5653f0b59b4f3ca38dfbf31d3e2b
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-08-18 08:18:58 +0200

    Add xfail test for crash group 3218
    Change-Id: Ib511876b599b40b8971a378c363f283a78972957

---
+++ tests/unit/cmk/gui/visuals/test_filter_context.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import cast
+
+import pytest
+
+from cmk.gui.type_defs import VisualContext
+from cmk.gui.visuals._filter_context import missing_context_filters
+
+# A context that stores the filter value directly instead of the mapping of HTTP variables the
+# type demands, as written by older versions or by hand editing
+SCALAR_CONTEXT = cast(VisualContext, {"host": "myhost"})
+
+
+@pytest.mark.xfail(
+    strict=True, reason="Crash group 3218: AttributeError in missing_context_filters"
+)
+def test_missing_context_filters_reports_scalar_filter_context_as_missing() -> None:
+    assert missing_context_filters({"host"}, SCALAR_CONTEXT) == {"host"}

c7e18a14dc7ff2530a4f64d4a48ba6341669c26d
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-08-18 08:28:37 +0200

    Add xfail test for crash group 4447
    Change-Id: I3b38dce61161b1363f174709ec8de7d80617d6d0

---
+++ tests/unit/cmk/gui/visuals/test_title.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import cast
+
+import pytest
+
+from cmk.gui.type_defs import Visual
+from cmk.gui.visuals import visual_title
+
+# A saved visual from before the "Context information" option existed, so it carries no
+# add_context_to_title key
+VISUAL_WITHOUT_ADD_CONTEXT_TO_TITLE = cast(Visual, {"title": "My view", "single_infos": []})
+
+
+@pytest.mark.xfail(strict=True, reason="Crash group 4447: KeyError 'add_context_to_title'")
+def test_visual_title_without_add_context_to_title(request_context: None) -> None:
+    assert visual_title("view", VISUAL_WITHOUT_ADD_CONTEXT_TO_TITLE, {}) == "My view"

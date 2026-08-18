f61acf0276eaf74139de05494a02cf674349c194
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-08-18 09:10:01 +0200

    Add xfail test for crash group 4245
    Change-Id: Ia35e2f604c3ddef1dc14397ef1dbd303d66d79a0

---
+++ tests/unit/cmk/gui/test_view_breadcrumbs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from typing import cast
+
+import pytest
+
+from cmk.ccc.hostaddress import HostName
+from cmk.gui import view_breadcrumbs
+from cmk.gui.type_defs import ViewSpec
+from cmk.gui.utils.roles import UserPermissions
+
+ALLHOSTS_VIEW_SPEC = cast(
+    ViewSpec,
+    {"title": "All hosts", "topic": "overview", "single_infos": [], "add_context_to_title": False},
+)
+
+
+@pytest.mark.xfail(strict=True, reason="Crash group 4245: KeyError in make_host_breadcrumb")
+def test_make_host_breadcrumb_without_permission_for_the_host_view(
+    monkeypatch: pytest.MonkeyPatch, request_context: None
+) -> None:
+    # The user may see the host list, but not the host home page view
+    monkeypatch.setattr(
+        view_breadcrumbs, "get_permitted_views", lambda: {"allhosts": ALLHOSTS_VIEW_SPEC}
+    )
+
+    breadcrumb = view_breadcrumbs.make_host_breadcrumb(
+        HostName("myhost"), UserPermissions({}, {}, {}, [])
+    )
+
+    assert [item.title for item in breadcrumb][-2:] == ["All hosts", "myhost"]

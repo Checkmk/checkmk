f6f1e6a36e34717a5844fa0042d324c3cb6a1c96
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-08-18 09:27:18 +0200

    22173 FIX Views: fix crash without permission for the Host view
    Opening a host or service page crashed with `KeyError: host` for users who are
    not permitted to see the built-in "Host" view, or on sites where that view was
    removed.
    
    The breadcrumb of such a page now simply omits the host home page level, which
    the user could not have opened anyway, and the page renders again.
    
    Crash-Group-ID: 4245
    Jira: CMK-38144
    Change-Id: Ic588fad26ed7d380354d71a0dd6f56093f1da286

--- cmk/gui/view_breadcrumbs.py
+++ cmk/gui/view_breadcrumbs.py
@@ -78,16 +78,16 @@ def make_host_breadcrumb(host_name: HostName) -> Breadcrumb:
     )
 
     # 3. level: host home page
-    host_view_spec = permitted_views["host"]
-    breadcrumb.append(
-        BreadcrumbItem(
-            title=view_title(host_view_spec, context={}),
-            url=makeuri_contextless(
-                request,
-                append_site_from_request([("view_name", "host"), ("host", host_name)]),
-                filename="view.py",
-            ),
+    if host_view_spec := permitted_views.get("host"):
+        breadcrumb.append(
+            BreadcrumbItem(
+                title=view_title(host_view_spec, context={}),
+                url=makeuri_contextless(
+                    request,
+                    append_site_from_request([("view_name", "host"), ("host", host_name)]),
+                    filename="view.py",
+                ),
+            )
         )
-    )
 
     return breadcrumb

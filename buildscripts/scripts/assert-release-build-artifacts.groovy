786ee1ca7f2967907e16938d06006e25ac14da9a
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-08-19 15:45:18 +0200

    Port assert_build_artifacts.py script to k8s
    ... by actually removing all imports of checkmk packages and thereby
    the need for a venv to be able to run this script in the kaniko pod
    with bare raw Python and the minimal container of the pod
    
    Requires https://review.lan.tribe29.com/c/checkmk_ci/+/151511
    
    CMK-37300
    
    Change-Id: I69398a2f21e4432ccb7769255cae78dd47453be7

--- buildscripts/scripts/assert-release-build-artifacts.groovy
+++ buildscripts/scripts/assert-release-build-artifacts.groovy
@@ -18,6 +18,13 @@ void main() {
 
     def use_case = params.USE_CASE.trim() ?: "daily";
 
+    def all_editions = [];
+
+    inside_container_minimal(safe_branch_name: safe_branch_name) {
+        // run everything requiring python in this container
+        all_editions = versioning.get_editions();
+    }
+
     print(
         """
         |===== CONFIGURATION ===============================
@@ -28,53 +35,110 @@ void main() {
         |===================================================
         """.stripMargin());
 
-    stage("Assert release build artifacts") {
-        inside_container(
-            set_docker_group_id: true,
-            mount_credentials: true,
-            privileged: true,
-        ) {
+    dir("${checkout_dir}") {
+        stage("Assert release build artifacts") {
+            inside_container_minimal(safe_branch_name: safe_branch_name) {
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'nexus',
+                        passwordVariable: 'NEXUS_PASSWORD',
+                        usernameVariable: 'NEXUS_USER')
+                ]) {
+                    withCredentialUsernamePasswordAtLocation(
+                        creds: [
+                            [credentialsId: "cmk-credentials", location: "/etc/.cmk-credentials"]
+                        ]
+                    ) {
+                        withEnv(["PYTHONUNBUFFERED=1"]) {
+                            def result = sh(
+                                script: """python3 \
+                                buildscripts/scripts/assert_build_artifacts.py \
+                                --editions_file "${checkout_dir}/editions.yml" \
+                                assert_build_artifacts \
+                                --version "${cmk_version_rc_aware}" \
+                                --use_case "${use_case}"
+                                """,
+                                returnStatus: true,
+                            );
+
+                            /// Team Donau wants to be explicitly notified about the missing relay image.
+                            /// returnCode == 2 means at least the relay is missing on dockerhub.
+                            if (result == 2 && use_case == "release") {
+                                mail(
+                                    to: "team-donau@checkmk.com",
+                                    from: "\"CI\" <${env.JENKINS_MAIL}>",
+                                    replyTo: "${env.TEAM_CI_MAIL}",
+                                    subject: "[Release] Relay image missing on Docker Hub: checkmk/check-mk-relay:${cmk_version}",
+                                    body: ("""
+        |The relay image checkmk/check-mk-relay:${cmk_version} is not available on Docker Hub.
+        |
+        |Please get in touch with the release coordinator (see slack channel #release-coordination).
+        |
+        |Build: ${env.BUILD_URL}
+        |""".stripMargin()),
+                                );
+                            }
+
+                            if (result != 0) {
+                                error("assert_build_artifacts failed");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        stage("Assert Docker images") {
+            def docker_file_location = "dirty_workspace/Dockerfile";
+            def docker_build_args = (""
+                + " --dockerfile=${docker_file_location}"
+                + " --context ${docker_file_location.split('/')[0]}"
+                + " --destination unused:latest"
+                + " --no-push"
+            );
+            def docker_result = 1;
+            def container_name_base = "";
             withCredentials([
                 usernamePassword(
                     credentialsId: 'nexus',
                     passwordVariable: 'NEXUS_PASSWORD',
-                    usernameVariable: 'NEXUS_USER')]) {
-                withEnv(["PYTHONUNBUFFERED=1"]) {
-                    dir("${checkout_dir}") {
-                        def result = sh(
-                            script: """scripts/run-uvenv \
-                            buildscripts/scripts/assert_build_artifacts.py \
-                            --editions_file "${checkout_dir}/editions.yml" \
-                            assert_build_artifacts \
-                            --version "${cmk_version_rc_aware}" \
-                            --use_case "${use_case}"
+                    usernameVariable: 'NEXUS_USERNAME')
+            ]) {
+                for (edition in all_editions) {
+                    println("docker_build_args: ${docker_build_args}");
+                    container("kaniko-alpine") {
+                        if (edition == "cloud") {
+                            container_name_base = "artifacts.lan.tribe29.com:4000";
+                            /* groovylint-disable LineLength */
+                            sh("""#!/bin/sh
+                                echo '{"auths":{"${docker_registry_no_http}":{"username":"${NEXUS_USERNAME}","password":"${NEXUS_PASSWORD}"}}}' > /kaniko/.docker/config.json
+                            """);
+                            /* groovylint-enable LineLength */
+                        } else {
+                            container_name_base = "checkmk";
+                            sh("""#!/bin/sh
+                                rm /kaniko/.docker/config.json || true
+                            """);
+                        }
+
+                        sh("""#!/bin/sh
+                            mkdir -p dirty_workspace
+                            echo "FROM ${container_name_base}/check-mk-${edition}:${cmk_version_rc_aware}" > ${docker_file_location}
+                            echo "RUN echo 'Hello'" >> ${docker_file_location}
+                        """);
+
+                        docker_result = sh(
+                            script: """#!/bin/sh
+                                /kaniko/executor ${docker_build_args}
                             """,
                             returnStatus: true,
                         );
-
-                        /// Team Donau wants to be explicitly notified about the missing relay image.
-                        /// returnCode == 2 means at least the relay is missing on dockerhub.
-                        if (result == 2 && use_case == "release") {
-                            mail(
-                                to: "team-donau@checkmk.com",
-                                from: "\"CI\" <${env.JENKINS_MAIL}>",
-                                replyTo: "${env.TEAM_CI_MAIL}",
-                                subject: "[Release] Relay image missing on Docker Hub: checkmk/check-mk-relay:${cmk_version}",
-                                body: ("""
-    |The relay image checkmk/check-mk-relay:${cmk_version} is not available on Docker Hub.
-    |
-    |Please get in touch with the release coordinator (see slack channel #release-coordination).
-    |
-    |Build: ${env.BUILD_URL}
-    |""".stripMargin()),
-                            );
-                        }
-                        if (result != 0) {
-                            error("assert_build_artifacts failed");
-                        }
+                    }
+                    if (docker_result != 0) {
+                        error("Failed to verify Docker image availability");
                     }
                 }
-                    }
+            }
         }
     }
 }

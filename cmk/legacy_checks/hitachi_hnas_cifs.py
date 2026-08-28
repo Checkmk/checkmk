7a116717953460c6fcf96be68c53c37f1939b9de
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-28 19:33:05 +0200

    Legacy check migration: hitachi_hnas_cifs I
    Change-Id: I6057dac6386ad7e894cd6697b2a857ab43cbee18

--- cmk/legacy_checks/hitachi_hnas_cifs.py
+++ cmk/legacy_checks/hitachi_hnas_cifs.py
@@ -3,35 +3,41 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Metric,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.hitachi_hnas.lib import DETECT
-
-check_info = {}
-
-
-def discover_hitachi_hnas_cifs(info):
-    inventory = []
-    for evs_id, share_name, _users in info:
-        inventory.append((evs_id + " " + share_name, None))
-    return inventory
-
-
-def check_hitachi_hnas_cifs(item, _no_params, info):
-    for evs_id, share_name, users in info:
-        if evs_id + " " + share_name == item:
-            perfdata = [("users", users, "", "", 0)]
-            return 0, "%s users" % users, perfdata
-    return 3, "Share not found"
 
 
 def parse_hitachi_hnas_cifs(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["hitachi_hnas_cifs"] = LegacyCheckDefinition(
+def discover_hitachi_hnas_cifs(section: StringTable) -> DiscoveryResult:
+    for evs_id, share_name, _users in section:
+        yield Service(item=f"{evs_id} {share_name}")
+
+
+def check_hitachi_hnas_cifs(item: str, section: StringTable) -> CheckResult:
+    for evs_id, share_name, users in section:
+        if f"{evs_id} {share_name}" == item:
+            yield Result(state=State.OK, summary=f"{users} users")
+            yield Metric("users", float(users), boundaries=(0, None))
+            return
+
+    yield Result(state=State.UNKNOWN, summary="Share not found")
+
+
+snmp_section_hitachi_hnas_cifs = SimpleSNMPSection(
     name="hitachi_hnas_cifs",
     parse_function=parse_hitachi_hnas_cifs,
     detect=DETECT,
@@ -39,6 +45,11 @@ check_info["hitachi_hnas_cifs"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.11096.6.1.1.3.2.1.3.1",
         oids=["1", "2", "5"],
     ),
+)
+
+
+check_plugin_hitachi_hnas_cifs = CheckPlugin(
+    name="hitachi_hnas_cifs",
     service_name="CIFS Share EVS %s",
     discovery_function=discover_hitachi_hnas_cifs,
     check_function=check_hitachi_hnas_cifs,

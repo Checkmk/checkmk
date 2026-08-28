38c9de06fe464b4e69653130de7fdf4dea84833e
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-28 20:58:54 +0200

    Legacy check migration: sentry_pdu_systempower I
    Change-Id: I1117163793324a14b608d50fe589d2feeda7153f

--- cmk/legacy_checks/sentry_pdu_systempower.py
+++ cmk/legacy_checks/sentry_pdu_systempower.py
@@ -3,32 +3,47 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+# mypy: disable-error-code="explicit-any"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import equals, SNMPTree
-from cmk.legacy_includes.elphase import check_elphase
+from collections.abc import Mapping
+from typing import Any
 
-check_info = {}
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    equals,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.elphase import check_elphase, ElPhase, ReadingWithState
 
 # .1.3.6.1.4.1.1718.3.1.6.0 2111
 
-# parsed = {
-#  'Power Supply System': { 'power': (2111, None) }
-# }
+Section = Mapping[str, ElPhase]
 
 
-def parse_sentry_pdu_systempower(string_table):
-    return (
-        {"Power Supply System": {"power": (int(string_table[0][0]), {})}} if string_table else None
-    )
+def parse_sentry_pdu_systempower(string_table: StringTable) -> Section | None:
+    if not string_table:
+        return None
+    return {"Power Supply System": ElPhase(power=ReadingWithState(value=int(string_table[0][0])))}
 
 
-def discover_sentry_pdu_systempower(section):
-    yield from ((item, {}) for item in section)
+def discover_sentry_pdu_systempower(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
 
 
-check_info["sentry_pdu_systempower"] = LegacyCheckDefinition(
+def check_sentry_pdu_systempower(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (elphase := section.get(item)) is None:
+        return
+    yield from check_elphase(params, elphase)
+
+
+snmp_section_sentry_pdu_systempower = SimpleSNMPSection(
     name="sentry_pdu_systempower",
     detect=equals(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.1718.3"),
     fetch=SNMPTree(
@@ -36,8 +51,14 @@ check_info["sentry_pdu_systempower"] = LegacyCheckDefinition(
         oids=["6"],
     ),
     parse_function=parse_sentry_pdu_systempower,
+)
+
+
+check_plugin_sentry_pdu_systempower = CheckPlugin(
+    name="sentry_pdu_systempower",
     service_name="%s",
     discovery_function=discover_sentry_pdu_systempower,
-    check_function=check_elphase,
+    check_function=check_sentry_pdu_systempower,
     check_ruleset_name="el_inphase",
+    check_default_parameters={},
 )

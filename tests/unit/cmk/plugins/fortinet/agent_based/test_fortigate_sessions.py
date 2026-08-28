2a4801d734e62a13ea014236666ceb59105b0c89
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-28 08:31:58 +0200

    Legacy check migration: fortigate_sessions II
    Change-Id: I6070f962ac11432cf8792bd52e589bd521f59b31

--- tests/unit/cmk/plugins/fortinet/agent_based/test_fortigate_sessions.py
+++ tests/unit/cmk/plugins/fortinet/agent_based/test_fortigate_sessions.py
@@ -6,7 +6,7 @@
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.fortigate_sessions import (
+from cmk.plugins.fortinet.agent_based.fortigate_sessions import (
     check_fortigate_sessions,
     discover_fortigate_sessions,
     parse_fortigate_sessions,

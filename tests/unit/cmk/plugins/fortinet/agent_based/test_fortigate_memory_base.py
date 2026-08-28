1669cfb87a09db5b60d75636e8ef0f7d791eab8c
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-28 08:23:57 +0200

    Legacy check migration: fortigate_memory_base II
    Change-Id: I9cafa0df2bf8a936d3c6058fa7371f16e51e7129

--- tests/unit/cmk/plugins/fortinet/agent_based/test_fortigate_memory_base.py
+++ tests/unit/cmk/plugins/fortinet/agent_based/test_fortigate_memory_base.py
@@ -8,7 +8,7 @@ from collections.abc import Mapping, Sequence
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.fortigate_memory_base import (
+from cmk.plugins.fortinet.agent_based.fortigate_memory_base import (
     check_fortigate_memory_base,
     discover_fortigate_memory_base,
     parse_fortigate_memory_base,

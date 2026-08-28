34c4af91068f4b6d5e92f958e368dada9fb76980
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-28 19:24:52 +0200

    Legacy check migration: hitachi_hnas_bossock I
    Change-Id: Ic8786eb8cb431ef00d68103ad8b230931941dd9d

--- cmk/legacy_checks/hitachi_hnas_bossock.py
+++ cmk/legacy_checks/hitachi_hnas_bossock.py
@@ -3,18 +3,20 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="type-arg"
+from collections.abc import Mapping
 
-from collections.abc import Iterable, Mapping
-
-from cmk.agent_based.legacy.v0_unstable import check_levels, LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    check_levels,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
 from cmk.plugins.hitachi_hnas.lib import DETECT
 
-check_info = {}
-
-DiscoveryResult = Iterable[tuple[str, Mapping]]
-CheckResult = Iterable[tuple[int, str, list]]
 Section = Mapping[str, int]
 
 
@@ -24,29 +26,38 @@ def parse_hitachi_hnas_bossock(string_table: StringTable) -> Section:
 
 def discover_hitachi_hnas_bossock(section: Section) -> DiscoveryResult:
     for clusternode in section:
-        yield clusternode, {}
+        yield Service(item=clusternode)
 
 
 def check_hitachi_hnas_bossock(
-    item: str, params: Mapping[str, tuple[int, int]], section: Section
+    item: str, params: Mapping[str, tuple[int, int] | None], section: Section
 ) -> CheckResult:
     if (fibers := section.get(item)) is None:
         return
 
-    yield check_levels(
-        fibers, "fibers", params["levels"], human_readable_func=str, infoname="Running"
+    yield from check_levels(
+        fibers,
+        metric_name="fibers",
+        levels_upper=("fixed", levels) if (levels := params["levels"]) else ("no_levels", None),
+        render_func=str,
+        label="Running",
     )
 
 
-check_info["hitachi_hnas_bossock"] = LegacyCheckDefinition(
+snmp_section_hitachi_hnas_bossock = SimpleSNMPSection(
     name="hitachi_hnas_bossock",
     detect=DETECT,
     fetch=SNMPTree(
         base=".1.3.6.1.4.1.11096.6.1.1.6.7.4.1",
         oids=["1", "2"],
     ),
-    service_name="Bossock Fibers on Node %s",
     parse_function=parse_hitachi_hnas_bossock,
+)
+
+
+check_plugin_hitachi_hnas_bossock = CheckPlugin(
+    name="hitachi_hnas_bossock",
+    service_name="Bossock Fibers on Node %s",
     discovery_function=discover_hitachi_hnas_bossock,
     check_function=check_hitachi_hnas_bossock,
     check_ruleset_name="bossock_fibers",

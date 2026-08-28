3373b3310ac7c0f825e5b1210c204c938bd5bc56
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-28 13:29:32 +0200

    Legacy check migration: infoblox_replication_status I
    Change-Id: I012353ba9e5af520068bfde8d6fece495abe4de9

--- cmk/legacy_checks/infoblox_replication_status.py
+++ cmk/legacy_checks/infoblox_replication_status.py
@@ -3,13 +3,18 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.infoblox.lib import DETECT_INFOBLOX
-
-check_info = {}
 
 # .1.3.6.1.4.1.7779.3.1.1.2.1.2.1.1.X.X.X.X.X X.X.X.X --> IB-PLATFORMONE-MIB::ibNodeIPAddress."11.112.133.14"
 # .1.3.6.1.4.1.7779.3.1.1.2.1.2.1.1.X.X.X.X.X X.X.X.X --> IB-PLATFORMONE-MIB::ibNodeIPAddress."11.112.133.17"
@@ -25,35 +30,11 @@ check_info = {}
 # .1.3.6.1.4.1.7779.3.1.1.2.1.2.1.6.X.X.X.X.X 2016/04/13 14:15:51 --> IB-PLATFORMONE-MIB::ibNodeLastRepTimeToMaster."11.112.133.17"
 
 
-def discover_infoblox_replication_status(info):
-    return [(line[0], None) for line in info]
-
-
-def check_infoblox_replication_status(item, _no_params, info):
-    for (
-        ip_addr,
-        status,
-        queue_from_master,
-        time_from_master,
-        queue_to_master,
-        time_to_master,
-    ) in info:
-        if ip_addr == item:
-            status_readable = status.lower()
-            state = 0 if status_readable == "online" else 2
-
-            return (
-                state,
-                f"Status: {status_readable}, Queue from master: {queue_from_master} ({time_from_master}), Queue to master: {queue_to_master} ({time_to_master})",
-            )
-    return None
-
-
 def parse_infoblox_replication_status(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["infoblox_replication_status"] = LegacyCheckDefinition(
+snmp_section_infoblox_replication_status = SimpleSNMPSection(
     name="infoblox_replication_status",
     parse_function=parse_infoblox_replication_status,
     detect=DETECT_INFOBLOX,
@@ -61,6 +42,39 @@ check_info["infoblox_replication_status"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.7779.3.1.1.2.1.2.1",
         oids=["1", "2", "3", "4", "5", "6"],
     ),
+)
+
+
+def discover_infoblox_replication_status(section: StringTable) -> DiscoveryResult:
+    for line in section:
+        yield Service(item=line[0])
+
+
+def check_infoblox_replication_status(item: str, section: StringTable) -> CheckResult:
+    for (
+        ip_addr,
+        status,
+        queue_from_master,
+        time_from_master,
+        queue_to_master,
+        time_to_master,
+    ) in section:
+        if ip_addr == item:
+            status_readable = status.lower()
+
+            yield Result(
+                state=State.OK if status_readable == "online" else State.CRIT,
+                summary=(
+                    f"Status: {status_readable}, "
+                    f"Queue from master: {queue_from_master} ({time_from_master}), "
+                    f"Queue to master: {queue_to_master} ({time_to_master})"
+                ),
+            )
+            return
+
+
+check_plugin_infoblox_replication_status = CheckPlugin(
+    name="infoblox_replication_status",
     service_name="Replication %s",
     discovery_function=discover_infoblox_replication_status,
     check_function=check_infoblox_replication_status,

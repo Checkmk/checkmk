40f27b0f14b43f93c91735d94ab03bd6665dea3f
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 21:33:46 +0200

    Legacy check migration: palo_alto_sessions I
    Change-Id: I2d1fa3e96080f1f90ca7ef0a5e542924a225a685

--- cmk/legacy_checks/palo_alto_sessions.py
+++ cmk/legacy_checks/palo_alto_sessions.py
@@ -3,65 +3,64 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Metric,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.palo_alto.lib import DETECT_PALO_ALTO
 
-check_info = {}
+
+def discover_palo_alto_sessions(section: StringTable) -> DiscoveryResult:
+    yield Service()
 
 
-def discover_palo_alto_sessions(info):
-    return [(None, None)]
-
-
-def check_palo_alto_sessions(_no_item, params, info):
-    sessions_supported, total, tcp, udp, icmp, sslproxy = map(int, info[0])
+def check_palo_alto_sessions(
+    params: Mapping[str, tuple[int, int]], section: StringTable
+) -> CheckResult:
+    sessions_supported, total, tcp, udp, icmp, sslproxy = map(int, section[0])
 
     sessions_used_perc = 0.0 if sessions_supported == 0 else float(total) * 100 / sessions_supported
 
-    infotext = "%d total active sessions: %d TCP, %d UDP, %d ICMP, %d SSL Proxy." % (
-        total,
-        tcp,
-        udp,
-        icmp,
-        sslproxy,
+    infotext = (
+        f"{total} total active sessions: {tcp} TCP, {udp} UDP, {icmp} ICMP, {sslproxy} SSL Proxy."
     )
-    infotext += " %.1f%% of %d supported sessions in use." % (
-        sessions_used_perc,
-        sessions_supported,
-    )
+    infotext += f" {sessions_used_perc:.1f}% of {sessions_supported} supported sessions in use."
 
     warn, crit = params["levels_sessions_used"]
-    levelstext = " (warn/crit at %d/%d%%)" % (warn, crit)
-
-    perfdata = [
-        ("total_active_sessions", total),
-        ("tcp_active_sessions", tcp),
-        ("udp_active_sessions", udp),
-        ("icmp_active_sessions", icmp),
-        ("sslproxy_active_sessions", sslproxy),
-    ]
 
     if sessions_used_perc >= crit:
-        status = 2
+        state = State.CRIT
     elif sessions_used_perc >= warn:
-        status = 1
+        state = State.WARN
     else:
-        status = 0
+        state = State.OK
 
-    if status:
-        infotext += levelstext
+    if state is not State.OK:
+        infotext += f" (warn/crit at {warn}/{crit}%)"
 
-    return status, infotext, perfdata
+    yield Result(state=state, summary=infotext)
+    yield Metric("total_active_sessions", total)
+    yield Metric("tcp_active_sessions", tcp)
+    yield Metric("udp_active_sessions", udp)
+    yield Metric("icmp_active_sessions", icmp)
+    yield Metric("sslproxy_active_sessions", sslproxy)
 
 
 def parse_palo_alto_sessions(string_table: StringTable) -> StringTable | None:
     return string_table or None
 
 
-check_info["palo_alto_sessions"] = LegacyCheckDefinition(
+snmp_section_palo_alto_sessions = SimpleSNMPSection(
     name="palo_alto_sessions",
     parse_function=parse_palo_alto_sessions,
     detect=DETECT_PALO_ALTO,
@@ -69,6 +68,11 @@ check_info["palo_alto_sessions"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.25461.2.1.2.3",
         oids=["2", "3", "4", "5", "6", "7"],
     ),
+)
+
+
+check_plugin_palo_alto_sessions = CheckPlugin(
+    name="palo_alto_sessions",
     service_name="Palo Alto Sessions",
     discovery_function=discover_palo_alto_sessions,
     check_function=check_palo_alto_sessions,

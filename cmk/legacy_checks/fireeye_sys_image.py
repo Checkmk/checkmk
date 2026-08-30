7376b02b40b9cafa7cc1c0d01ee3b9b1b52551d5
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 21:25:18 +0200

    Legacy check migration: fireeye_sys_image I
    Change-Id: I8f25feb03fd024cb0d71f6fbf8fc50e569cdc63a

--- cmk/legacy_checks/fireeye_sys_image.py
+++ cmk/legacy_checks/fireeye_sys_image.py
@@ -3,13 +3,18 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fireeye.lib import DETECT
-
-check_info = {}
 
 # .1.3.6.1.4.1.25597.11.5.1.1.0 eMPS (eMPS) 7.6.5.442663 --> FE-FIREEYE-MIB::feInstalledSystemImage.0
 # .1.3.6.1.4.1.25597.11.5.1.2.0 7.6.5 --> FE-FIREEYE-MIB::feSystemImageVersionCurrent.0
@@ -17,27 +22,28 @@ check_info = {}
 # .1.3.6.1.4.1.25597.11.5.1.4.0 1 --> FE-FIREEYE-MIB::feIsSystemImageLatest.0
 
 
-def check_fireeye_sys_image(_no_item, _no_params, info):
-    installed, version, latest_version, is_latest = info[0]
-    state = 0
+def check_fireeye_sys_image(section: StringTable) -> CheckResult:
+    installed, version, latest_version, is_latest = section[0]
+    state = State.OK
     infotext = f"Image: {installed}, Version: {version}"
 
     if is_latest != "1":
-        state = 1
-        infotext += ", Latest version: %s" % latest_version
+        state = State.WARN
+        infotext += f", Latest version: {latest_version}"
 
-    return state, infotext
+    yield Result(state=state, summary=infotext)
 
 
 def parse_fireeye_sys_image(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_fireeye_sys_image(info):
-    yield from [(None, None)] if info else []
+def discover_fireeye_sys_image(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-check_info["fireeye_sys_image"] = LegacyCheckDefinition(
+snmp_section_fireeye_sys_image = SimpleSNMPSection(
     name="fireeye_sys_image",
     parse_function=parse_fireeye_sys_image,
     detect=DETECT,
@@ -45,6 +51,11 @@ check_info["fireeye_sys_image"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.25597.11.5.1",
         oids=["1", "2", "3", "4"],
     ),
+)
+
+
+check_plugin_fireeye_sys_image = CheckPlugin(
+    name="fireeye_sys_image",
     service_name="System image",
     discovery_function=discover_fireeye_sys_image,
     check_function=check_fireeye_sys_image,

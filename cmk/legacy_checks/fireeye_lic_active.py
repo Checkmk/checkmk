6d591377ee493646f723b3268c06ed97eb5fa6ac
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 20:45:56 +0200

    Legacy check migration: fireeye_lic_active I
    Change-Id: I941779a4a53356563b66f9c7838b5473719155b7

--- cmk/legacy_checks/fireeye_lic_active.py
+++ cmk/legacy_checks/fireeye_lic_active.py
@@ -3,37 +3,43 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fireeye.lib import DETECT
-
-check_info = {}
 
 # .1.3.6.1.4.1.25597.11.5.1.10.0 1
 # .1.3.6.1.4.1.25597.11.5.1.11.0 1
 # .1.3.6.1.4.1.25597.11.5.1.12.0 1
 
 
-def check_fireeye_lic_active(_no_item, _no_params, info):
-    product, content, support = info[0]
+def check_fireeye_lic_active(section: StringTable) -> CheckResult:
+    product, content, support = section[0]
     for feature, value in [("Product", product), ("Content", content), ("Support", support)]:
         if value == "1":
-            yield 0, "%s license active" % feature
+            yield Result(state=State.OK, summary=f"{feature} license active")
         else:
-            yield 2, "%s license not active" % feature
+            yield Result(state=State.CRIT, summary=f"{feature} license not active")
 
 
 def parse_fireeye_lic_active(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_fireeye_lic_active(info):
-    yield from [(None, None)] if info else []
+def discover_fireeye_lic_active(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-check_info["fireeye_lic_active"] = LegacyCheckDefinition(
+snmp_section_fireeye_lic_active = SimpleSNMPSection(
     name="fireeye_lic_active",
     parse_function=parse_fireeye_lic_active,
     detect=DETECT,
@@ -41,6 +47,11 @@ check_info["fireeye_lic_active"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.25597.11.5.1",
         oids=["10", "11", "12"],
     ),
+)
+
+
+check_plugin_fireeye_lic_active = CheckPlugin(
+    name="fireeye_lic_active",
     service_name="Active Licenses",
     discovery_function=discover_fireeye_lic_active,
     check_function=check_fireeye_lic_active,

fa4a15f077d061c0f9a64b2850419f06ebf30341
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 21:10:56 +0200

    Legacy check migration: fireeye_powersupplies I
    Change-Id: I0472c78c204f48b0f8ce1a13f25d13273a75ec19

--- cmk/legacy_checks/fireeye_powersupplies.py
+++ cmk/legacy_checks/fireeye_powersupplies.py
@@ -3,42 +3,41 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="explicit-any"
-
-from collections.abc import Iterator, Mapping
-from typing import Any
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fireeye.lib import DETECT, HEALTH_MAP, STATUS_MAP
-
-check_info = {}
 
 # .1.3.6.1.4.1.25597.11.3.1.1.0 Good --> FE-FIREEYE-MIB::fePowerSupplyOverallStatus.0
 # .1.3.6.1.4.1.25597.11.3.1.2.0 1 --> FE-FIREEYE-MIB::fePowerSupplyOverallIsHealthy.0
 
 
-def check_fireeye_powersupplies(
-    _no_item: None, _no_params: Mapping[str, Any], info: StringTable
-) -> Iterator[tuple[int, str]]:
-    status, health = info[0]
+def check_fireeye_powersupplies(section: StringTable) -> CheckResult:
+    status, health = section[0]
     state, state_readable = STATUS_MAP.get(status.lower(), (2, f"unknown: {status}"))
-    yield state, f"Status: {state_readable}"
+    yield Result(state=State(state), summary=f"Status: {state_readable}")
     state, state_readable = HEALTH_MAP.get(health, (2, f"unknown: {health}"))
-    yield state, f"Health: {state_readable}"
+    yield Result(state=State(state), summary=f"Health: {state_readable}")
 
 
 def parse_fireeye_powersupplies(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_fireeye_powersupplies(
-    info: StringTable,
-) -> list[tuple[None, None]]:
-    return [(None, None)] if info else []
+def discover_fireeye_powersupplies(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-check_info["fireeye_powersupplies"] = LegacyCheckDefinition(
+snmp_section_fireeye_powersupplies = SimpleSNMPSection(
     name="fireeye_powersupplies",
     parse_function=parse_fireeye_powersupplies,
     detect=DETECT,
@@ -46,6 +45,11 @@ check_info["fireeye_powersupplies"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.25597.11.3.1",
         oids=["1", "2"],
     ),
+)
+
+
+check_plugin_fireeye_powersupplies = CheckPlugin(
+    name="fireeye_powersupplies",
     service_name="Power supplies summary",
     discovery_function=discover_fireeye_powersupplies,
     check_function=check_fireeye_powersupplies,

0db384a648a9a55363c531c57ed01b480d096e82
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 21:17:55 +0200

    Legacy check migration: fireeye_raid I
    Change-Id: I38614a6aa0ca93e0b228bbd0a36d3029001332c2

--- cmk/legacy_checks/fireeye_raid.py
+++ cmk/legacy_checks/fireeye_raid.py
@@ -3,13 +3,21 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Sequence
+from typing import NamedTuple
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fireeye.lib import DETECT, DISK_STATUS_MAP, HEALTH_MAP, STATUS_MAP
-
-check_info = {}
 
 # .1.3.6.1.4.1.25597.11.2.1.1.0 Good --> FE-FIREEYE-MIB::feRaidStatus.0
 # .1.3.6.1.4.1.25597.11.2.1.2.0 1 --> FE-FIREEYE-MIB::feRaidIsHealthy.0
@@ -32,32 +40,45 @@ check_info = {}
 #   '----------------------------------------------------------------------'
 
 
-def parse_fireeye_raid(string_table):
+class Disk(NamedTuple):
+    name: str
+    status: str
+    health: str
+
+
+class Section(NamedTuple):
+    raid: tuple[str, str] | None
+    disks: Sequence[Disk]
+
+
+def parse_fireeye_raid(string_table: Sequence[StringTable]) -> Section:
     # We only discover in case of a raid system
-    parsed: dict[str, object] = {}
-    disks: list[list[str]] = []
-    if len(string_table[1]) > 1:
-        for diskname, diskstatus, diskhealth in string_table[1]:
-            parsed.setdefault("raid", string_table[0][0])
-            parsed.setdefault("disks", disks)
-            disks.append([diskname, diskstatus, diskhealth])
+    if len(string_table[1]) <= 1:
+        return Section(raid=None, disks=[])
 
-    return parsed
+    status, health = string_table[0][0]
+    return Section(
+        raid=(status, health),
+        disks=[Disk(*line) for line in string_table[1]],
+    )
 
 
-def check_fireeye_raid(_no_item, _no_params, parsed):
-    status, health = parsed["raid"]
+def check_fireeye_raid(section: Section) -> CheckResult:
+    if section.raid is None:
+        return
+    status, health = section.raid
     state, state_readable = STATUS_MAP.get(status.lower(), (2, f"unknown: {status}"))
-    yield state, f"Status: {state_readable}"
+    yield Result(state=State(state), summary=f"Status: {state_readable}")
     state, state_readable = HEALTH_MAP.get(health, (2, f"unknown: {health}"))
-    yield state, f"Health: {state_readable}"
+    yield Result(state=State(state), summary=f"Health: {state_readable}")
 
 
-def discover_fireeye_raid(parsed):
-    yield from [(None, None)] if parsed.get("raid", []) else []
+def discover_fireeye_raid(section: Section) -> DiscoveryResult:
+    if section.raid is not None:
+        yield Service()
 
 
-check_info["fireeye_raid"] = LegacyCheckDefinition(
+snmp_section_fireeye_raid = SNMPSection(
     name="fireeye_raid",
     detect=DETECT,
     fetch=[
@@ -71,6 +92,11 @@ check_info["fireeye_raid"] = LegacyCheckDefinition(
         ),
     ],
     parse_function=parse_fireeye_raid,
+)
+
+
+check_plugin_fireeye_raid = CheckPlugin(
+    name="fireeye_raid",
     service_name="RAID status",
     discovery_function=discover_fireeye_raid,
     check_function=check_fireeye_raid,
@@ -87,23 +113,23 @@ check_info["fireeye_raid"] = LegacyCheckDefinition(
 #   '----------------------------------------------------------------------'
 
 
-def check_fireeye_raid_disks(item, _no_params, parsed):
-    for diskname, diskstatus, diskhealth in parsed["disks"]:
-        if diskname == item:
+def check_fireeye_raid_disks(item: str, section: Section) -> CheckResult:
+    for disk in section.disks:
+        if disk.name == item:
             state, state_readable = DISK_STATUS_MAP.get(
-                diskstatus.lower(), (2, f"unknown: {diskstatus}")
+                disk.status.lower(), (2, f"unknown: {disk.status}")
             )
-            yield state, f"Disk status: {state_readable}"
-            state, state_readable = HEALTH_MAP.get(diskhealth, (2, f"unknown: {diskhealth}"))
-            yield state, f"Health: {state_readable}"
+            yield Result(state=State(state), summary=f"Disk status: {state_readable}")
+            state, state_readable = HEALTH_MAP.get(disk.health, (2, f"unknown: {disk.health}"))
+            yield Result(state=State(state), summary=f"Health: {state_readable}")
 
 
-def discover_fireeye_raid_disks(parsed):
-    for line in parsed.get("disks", []):
-        yield line[0], None
+def discover_fireeye_raid_disks(section: Section) -> DiscoveryResult:
+    for disk in section.disks:
+        yield Service(item=disk.name)
 
 
-check_info["fireeye_raid.disks"] = LegacyCheckDefinition(
+check_plugin_fireeye_raid_disks = CheckPlugin(
     name="fireeye_raid_disks",
     service_name="Disk status %s",
     sections=["fireeye_raid"],

dcbaf935d17dd87e0354de3f8696de40d1c7e781
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 21:21:23 +0200

    Legacy check migration: fireeye_smtp_conn I
    Change-Id: Ic60e6839d71ce55b3b6f10828a75e330cfaf147b

--- cmk/legacy_checks/fireeye_smtp_conn.py
+++ cmk/legacy_checks/fireeye_smtp_conn.py
@@ -3,31 +3,39 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Metric,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fireeye.lib import DETECT
-
-check_info = {}
 
 # .1.3.6.1.4.1.25597.13.1.46.0 8
 
 
-def check_fireeye_smtp_conn(_no_item, _no_params, info):
-    smtp_conns = int(info[0][0])
-    yield 0, "Open SMTP connections: %d" % smtp_conns, [("connections", smtp_conns)]
+def check_fireeye_smtp_conn(section: StringTable) -> CheckResult:
+    smtp_conns = int(section[0][0])
+    yield Result(state=State.OK, summary=f"Open SMTP connections: {smtp_conns}")
+    yield Metric("connections", smtp_conns)
 
 
 def parse_fireeye_smtp_conn(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_fireeye_smtp_conn(info):
-    yield from [(None, None)] if info else []
+def discover_fireeye_smtp_conn(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-check_info["fireeye_smtp_conn"] = LegacyCheckDefinition(
+snmp_section_fireeye_smtp_conn = SimpleSNMPSection(
     name="fireeye_smtp_conn",
     parse_function=parse_fireeye_smtp_conn,
     detect=DETECT,
@@ -35,6 +43,11 @@ check_info["fireeye_smtp_conn"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.25597.13.1",
         oids=["46"],
     ),
+)
+
+
+check_plugin_fireeye_smtp_conn = CheckPlugin(
+    name="fireeye_smtp_conn",
     service_name="SMTP Connections",
     discovery_function=discover_fireeye_smtp_conn,
     check_function=check_fireeye_smtp_conn,

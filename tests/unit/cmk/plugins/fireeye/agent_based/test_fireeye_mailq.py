2f75d3010cdbc8e7303f1ac9376daef7c0da5806
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-30 21:09:41 +0200

    Legacy check migration: fireeye_mailq II
    Change-Id: I6d327036fdb123ecabc984baba7f48464a58baa1

--- tests/unit/cmk/plugins/fireeye/agent_based/test_fireeye_mailq.py
+++ tests/unit/cmk/plugins/fireeye/agent_based/test_fireeye_mailq.py
@@ -6,7 +6,7 @@
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State
-from cmk.legacy_checks.fireeye_mailq import (
+from cmk.plugins.fireeye.agent_based.fireeye_mailq import (
     check_fireeye_mailq,
     dicsover_fireeye_mailq,
     parse_fireeye_mailq,

d63a609b188cf47c537cbb390d0472d07008205b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-31 17:12:24 +0200

    Legacy check migration: fsc_sc2_temp I
    Change-Id: I414e7f2bda5dd62bf7f09df4592e094c931f5670

--- cmk/legacy_checks/fsc_sc2_temp.py
+++ cmk/legacy_checks/fsc_sc2_temp.py
@@ -3,14 +3,32 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fujitsu.lib import DETECT_FSC_SC2
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
-check_info = {}
+_TEMP_STATUS = {
+    "1": (State.UNKNOWN, "unknown"),
+    "2": (State.OK, "not-available"),
+    "3": (State.OK, "ok"),
+    "4": (State.CRIT, "sensor-failed"),
+    "5": (State.CRIT, "failed"),
+    "6": (State.WARN, "temperature-warning-toohot"),
+    "7": (State.CRIT, "temperature-critical-toohot"),
+    "8": (State.OK, "temperature-normal"),
+    "9": (State.WARN, "temperature-warning"),
+}
 
 # .1.3.6.1.4.1.231.2.10.2.2.10.5.1.1.3.1.1 "Ambient"
 # .1.3.6.1.4.1.231.2.10.2.2.10.5.1.1.3.1.2 "Systemboard 1"
@@ -63,51 +81,38 @@ def parse_fsc_sc2_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_fsc_sc2_temp(info):
-    for line in info:
+def discover_fsc_sc2_temp(section: StringTable) -> DiscoveryResult:
+    for line in section:
         if line[1] != "2":
-            yield line[0], {}
+            yield Service(item=line[0])
 
 
-def check_fsc_sc2_temp(item, params, info):
-    temp_status = {
-        "1": (3, "unknown"),
-        "2": (0, "not-available"),
-        "3": (0, "ok"),
-        "4": (2, "sensor-failed"),
-        "5": (2, "failed"),
-        "6": (1, "temperature-warning-toohot"),
-        "7": (2, "temperature-critical-toohot"),
-        "8": (0, "temperature-normal"),
-        "9": (1, "temperature-warning"),
-    }
-
-    for designation, status, temp, dev_warn, dev_crit in info:
+def check_fsc_sc2_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for designation, status, temp, dev_warn, dev_crit in section:
         if designation == item:
             if not temp:
-                return 3, "Did not receive temperature data"
+                yield Result(state=State.UNKNOWN, summary="Did not receive temperature data")
+                return
 
-            dev_status, dev_status_name = temp_status.get(status, (3, "unknown"))
+            dev_status, dev_status_name = _TEMP_STATUS.get(status, (State.UNKNOWN, "unknown"))
 
             if not dev_warn or not dev_crit:
-                return 3, "Did not receive device levels"
+                yield Result(state=State.UNKNOWN, summary="Did not receive device levels")
+                return
 
-            dev_levels = int(dev_warn), int(dev_crit)
-
-            return check_temperature(
+            yield from check_temperature(
                 int(temp),
                 params,
-                "temp_{}".format(item.replace(" ", "_")),
-                "c",
-                dev_levels,
-                None,
-                dev_status,
-                dev_status_name,
+                unique_name="temp_{}".format(item.replace(" ", "_")),
+                value_store=get_value_store(),
+                dev_levels=(int(dev_warn), int(dev_crit)),
+                dev_status=int(dev_status),
+                dev_status_name=dev_status_name,
             )
-    return None
+            return
 
 
-check_info["fsc_sc2_temp"] = LegacyCheckDefinition(
+snmp_section_fsc_sc2_temp = SimpleSNMPSection(
     name="fsc_sc2_temp",
     parse_function=parse_fsc_sc2_temp,
     detect=DETECT_FSC_SC2,
@@ -115,8 +120,14 @@ check_info["fsc_sc2_temp"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.231.2.10.2.2.10.5.1.1",
         oids=["3", "5", "6", "7", "8"],
     ),
+)
+
+
+check_plugin_fsc_sc2_temp = CheckPlugin(
+    name="fsc_sc2_temp",
     service_name="Temperature %s",
     discovery_function=discover_fsc_sc2_temp,
     check_function=check_fsc_sc2_temp,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )

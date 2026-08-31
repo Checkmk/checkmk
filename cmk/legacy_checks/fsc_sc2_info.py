faf27514ac356a52487d0ccab7e9b1ead02de8f8
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-31 16:51:22 +0200

    Legacy check migration: fsc_sc2_info I
    Change-Id: I65755e765dafce8b6f1cd88655901691adca1b86

--- cmk/legacy_checks/fsc_sc2_info.py
+++ cmk/legacy_checks/fsc_sc2_info.py
@@ -3,13 +3,18 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fujitsu.lib import DETECT_FSC_SC2
-
-check_info = {}
 
 # .1.3.6.1.4.1.231.2.10.2.2.10.2.3.1.5.1 "PRIMERGY RX300 S8"
 # .1.3.6.1.4.1.231.2.10.2.2.10.2.3.1.7.1 "--"
@@ -20,22 +25,21 @@ def parse_fsc_sc2_info(string_table: StringTable) -> StringTable:
     return string_table
 
 
-def discover_fsc_sc2_info(info):
-    if info:
-        return [(None, None)]
-    return []
+def discover_fsc_sc2_info(section: StringTable) -> DiscoveryResult:
+    if section:
+        yield Service()
 
 
-def check_fsc_sc2_info(_no_item, _no_params, info):
-    if info:
-        return (
-            0,
-            f"Model: {info[0][0]}, Serial Number: {info[0][1]}, BIOS Version: {info[0][2]}",
+def check_fsc_sc2_info(section: StringTable) -> CheckResult:
+    if section:
+        model, serial_number, bios_version = section[0]
+        yield Result(
+            state=State.OK,
+            summary=f"Model: {model}, Serial Number: {serial_number}, BIOS Version: {bios_version}",
         )
-    return None
 
 
-check_info["fsc_sc2_info"] = LegacyCheckDefinition(
+snmp_section_fsc_sc2_info = SimpleSNMPSection(
     name="fsc_sc2_info",
     parse_function=parse_fsc_sc2_info,
     detect=DETECT_FSC_SC2,
@@ -43,6 +47,11 @@ check_info["fsc_sc2_info"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.231.2.10.2.2.10",
         oids=["2.3.1.5.1", "2.3.1.7.1", "4.1.1.11.1"],
     ),
+)
+
+
+check_plugin_fsc_sc2_info = CheckPlugin(
+    name="fsc_sc2_info",
     service_name="Server Info",
     discovery_function=discover_fsc_sc2_info,
     check_function=check_fsc_sc2_info,

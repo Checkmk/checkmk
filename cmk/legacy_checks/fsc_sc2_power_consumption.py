acd54ae5172997fb9f707d0e11329a0f80e6d78b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-31 17:02:01 +0200

    Legacy check migration: fsc_sc2_power_consumption I
    Change-Id: Ie126bd7f415c7f96cc2d7b60168f75dc7367f066

--- cmk/legacy_checks/fsc_sc2_power_consumption.py
+++ cmk/legacy_checks/fsc_sc2_power_consumption.py
@@ -3,15 +3,25 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="type-arg"
+# mypy: disable-error-code="explicit-any"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree
-from cmk.legacy_includes.elphase import check_elphase
+from collections.abc import Mapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fujitsu.lib import DETECT_FSC_SC2
+from cmk.plugins.lib.elphase import check_elphase, ElPhase, ReadingWithState
 
-check_info = {}
+type Section = Mapping[str, ElPhase]
 
 # .1.3.6.1.4.1.231.2.10.2.2.10.6.7.1.4.1.3.1 "CPU1 Power"
 # .1.3.6.1.4.1.231.2.10.2.2.10.6.7.1.4.1.3.2 "CPU2 Power"
@@ -31,24 +41,33 @@ check_info = {}
 # .1.3.6.1.4.1.231.2.10.2.2.10.6.7.1.5.1.224.2 68
 
 
-def parse_fsc_sc2_power_consumption(info):
-    parsed: dict = {}
-    for designation, value in info:
+def parse_fsc_sc2_power_consumption(string_table: StringTable) -> Section:
+    parsed: dict[str, ElPhase] = {}
+    for designation, value in string_table:
         # sometimes the device does not return a value
         if not value:
             parsed.setdefault(
-                designation, {"device_state": (3, "Error on device while reading value")}
+                designation,
+                ElPhase(device_state=(State.UNKNOWN, "Error on device while reading value")),
             )
         else:
-            parsed.setdefault(designation, {"power": int(value)})
+            parsed.setdefault(designation, ElPhase(power=ReadingWithState(value=int(value))))
     return parsed
 
 
-def discover_fsc_sc2_power_consumption(section):
-    yield from ((item, {}) for item in section)
+def discover_fsc_sc2_power_consumption(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
 
 
-check_info["fsc_sc2_power_consumption"] = LegacyCheckDefinition(
+def check_fsc_sc2_power_consumption(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (elphase := section.get(item)) is None:
+        return
+    yield from check_elphase(params, elphase)
+
+
+snmp_section_fsc_sc2_power_consumption = SimpleSNMPSection(
     name="fsc_sc2_power_consumption",
     detect=DETECT_FSC_SC2,
     fetch=SNMPTree(
@@ -56,8 +75,14 @@ check_info["fsc_sc2_power_consumption"] = LegacyCheckDefinition(
         oids=["4", "5"],
     ),
     parse_function=parse_fsc_sc2_power_consumption,
+)
+
+
+check_plugin_fsc_sc2_power_consumption = CheckPlugin(
+    name="fsc_sc2_power_consumption",
     service_name="Power Comsumption %s",
     discovery_function=discover_fsc_sc2_power_consumption,
-    check_function=check_elphase,
+    check_function=check_fsc_sc2_power_consumption,
     check_ruleset_name="el_inphase",
+    check_default_parameters={},
 )

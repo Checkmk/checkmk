4d5679b6a0c83a1e922cfab0e2bb10b5402ea7d7
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-31 17:25:12 +0200

    Legacy check migration: fsc_temp I
    Change-Id: I4fd2d81900968abcb4f1b7c872465a19bc160314

--- cmk/legacy_checks/fsc_temp.py
+++ cmk/legacy_checks/fsc_temp.py
@@ -4,21 +4,24 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition, LegacyResult
 from cmk.agent_based.v2 import (
     all_of,
     any_of,
+    CheckPlugin,
+    CheckResult,
     DiscoveryResult,
     exists,
+    get_value_store,
     not_exists,
+    Result,
     Service,
+    SimpleSNMPSection,
     SNMPTree,
     startswith,
+    State,
     StringTable,
 )
-from cmk.legacy_includes.temperature import check_temperature, TempParamType
-
-check_info = {}
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 # We fetch the following columns from SNMP:
 # 13: name of the temperature sensor (used as item)
@@ -27,29 +30,34 @@ check_info = {}
 # 8:  critical level
 
 
-def discover_fsc_temp(string_table: StringTable) -> DiscoveryResult:
+def discover_fsc_temp(section: StringTable) -> DiscoveryResult:
     # Ignore non-connected sensors
-    yield from (Service(item=line[0]) for line in string_table if int(line[1]) < 500)
+    yield from (Service(item=line[0]) for line in section if int(line[1]) < 500)
 
 
-def check_fsc_temp(item: str, params: TempParamType, info: StringTable) -> LegacyResult | None:
-    for name, rawtemp, warn, crit in info:
+def check_fsc_temp(item: str, params: TempParamType, section: StringTable) -> CheckResult:
+    for name, rawtemp, warn, crit in section:
         if name == item:
             temp = int(rawtemp)
             if temp in {-1, 4294967295}:
-                return 3, "Sensor or component missing"
+                yield Result(state=State.UNKNOWN, summary="Sensor or component missing")
+                return
 
-            return check_temperature(
-                temp, params, "fsc_temp_%s" % item, dev_levels=(int(warn), int(crit))
+            yield from check_temperature(
+                temp,
+                params,
+                unique_name=f"fsc_temp_{item}",
+                value_store=get_value_store(),
+                dev_levels=(int(warn), int(crit)),
             )
-    return None
+            return
 
 
 def parse_fsc_temp(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["fsc_temp"] = LegacyCheckDefinition(
+snmp_section_fsc_temp = SimpleSNMPSection(
     name="fsc_temp",
     parse_function=parse_fsc_temp,
     detect=all_of(
@@ -67,8 +75,14 @@ check_info["fsc_temp"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.231.2.10.2.2.5.2.1.1",
         oids=["13", "11", "6", "8"],
     ),
+)
+
+
+check_plugin_fsc_temp = CheckPlugin(
+    name="fsc_temp",
     service_name="Temperature %s",
     discovery_function=discover_fsc_temp,
     check_function=check_fsc_temp,
     check_ruleset_name="temperature",
+    check_default_parameters={},
 )

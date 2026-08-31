d9575b2daa35f873b6f49faf09b266513a6198c0
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-31 17:07:02 +0200

    Legacy check migration: fsc_sc2_psu I
    Change-Id: I402c38239f976d0d6fdeae9d7a372f2a5b19c197

--- cmk/legacy_checks/fsc_sc2_psu.py
+++ cmk/legacy_checks/fsc_sc2_psu.py
@@ -3,13 +3,35 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Metric,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.fujitsu.lib import DETECT_FSC_SC2
 
-check_info = {}
+_PSU_STATUS = {
+    "1": (State.UNKNOWN, "Status is unknown"),
+    "2": (State.WARN, "Status is not-present"),
+    "3": (State.OK, "Status is ok"),
+    "4": (State.CRIT, "Status is failed"),
+    "5": (State.CRIT, "Status is ac-fail"),
+    "6": (State.CRIT, "Status is dc-fail"),
+    "7": (State.CRIT, "Status is critical-temperature"),
+    "8": (State.WARN, "Status is not-manageable"),
+    "9": (State.WARN, "Status is fan-failure-predicted"),
+    "10": (State.CRIT, "Status is fan-failure"),
+    "11": (State.WARN, "Status is power-safe-mode"),
+    "12": (State.WARN, "Status is non-redundant-dc-fail"),
+    "13": (State.WARN, "Status is non-redundant-ac-fail"),
+}
 
 # .1.3.6.1.4.1.231.2.10.2.2.10.6.2.1.3.1.1 "PSU1"
 # .1.3.6.1.4.1.231.2.10.2.2.10.6.2.1.3.1.2 "PSU2"
@@ -21,46 +43,32 @@ check_info = {}
 # .1.3.6.1.4.1.231.2.10.2.2.10.6.2.1.7.1.2 448
 
 
-def discover_fsc_sc2_psu(info):
-    for line in info:
-        if line[1] not in ["2"]:
-            yield line[0], None
+def discover_fsc_sc2_psu(section: StringTable) -> DiscoveryResult:
+    for line in section:
+        if line[1] != "2":
+            yield Service(item=line[0])
 
 
-def check_fsc_sc2_psu(item, _no_params, info):
-    psu_status = {
-        "1": (3, "Status is unknown"),
-        "2": (1, "Status is not-present"),
-        "3": (0, "Status is ok"),
-        "4": (2, "Status is failed"),
-        "5": (2, "Status is ac-fail"),
-        "6": (2, "Status is dc-fail"),
-        "7": (2, "Status is critical-temperature"),
-        "8": (1, "Status is not-manageable"),
-        "9": (1, "Status is fan-failure-predicted"),
-        "10": (2, "Status is fan-failure"),
-        "11": (1, "Status is power-safe-mode"),
-        "12": (1, "Status is non-redundant-dc-fail"),
-        "13": (1, "Status is non-redundant-ac-fail"),
-    }
-
-    for designation, status, load, nominal in info:
+def check_fsc_sc2_psu(item: str, section: StringTable) -> CheckResult:
+    for designation, status, load, nominal in section:
         if designation == item:
-            yield psu_status.get(status, (3, "Status is unknown"))
+            state, state_readable = _PSU_STATUS.get(status, (State.UNKNOWN, "Status is unknown"))
+            yield Result(state=state, summary=state_readable)
             if nominal and load:
-                infotext = f"Nominal load: {nominal} W, Actual load: {load} W"
-                perfdata = [("power", int(load))]
+                yield Result(
+                    state=State.OK,
+                    summary=f"Nominal load: {nominal} W, Actual load: {load} W",
+                )
+                yield Metric("power", int(load))
             else:
-                infotext = "Did not receive load data"
-                perfdata = []
-            yield 0, infotext, perfdata
+                yield Result(state=State.OK, summary="Did not receive load data")
 
 
 def parse_fsc_sc2_psu(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["fsc_sc2_psu"] = LegacyCheckDefinition(
+snmp_section_fsc_sc2_psu = SimpleSNMPSection(
     name="fsc_sc2_psu",
     parse_function=parse_fsc_sc2_psu,
     detect=DETECT_FSC_SC2,
@@ -68,6 +76,11 @@ check_info["fsc_sc2_psu"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.231.2.10.2.2.10.6.2.1",
         oids=["3", "5", "6", "7"],
     ),
+)
+
+
+check_plugin_fsc_sc2_psu = CheckPlugin(
+    name="fsc_sc2_psu",
     service_name="FSC %s",
     discovery_function=discover_fsc_sc2_psu,
     check_function=check_fsc_sc2_psu,

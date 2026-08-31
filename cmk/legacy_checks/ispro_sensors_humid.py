47fe1a7188a9ce279230db65dc13885ef25e0e59
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-08-31 17:35:22 +0200

    Legacy check migration: ispro_sensors_humid I
    Change-Id: Ib36d472265b81451dfcc368b3334a5ca4da16baf

--- cmk/legacy_checks/ispro_sensors_humid.py
+++ cmk/legacy_checks/ispro_sensors_humid.py
@@ -3,38 +3,43 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
-from cmk.legacy_includes.humidity import check_humidity
-from cmk.legacy_includes.ispro import ispro_sensors_alarm_states
-from cmk.plugins.ispro.lib import DETECT_ISPRO_SENSORS
-
-check_info = {}
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.ispro.lib import DETECT_ISPRO_SENSORS, sensors_alarm_states
+from cmk.plugins.lib.humidity import check_humidity, CheckParams
 
 # .1.3.6.1.4.1.19011.1.3.2.1.3.1.2.1.2.1 "Humidity-R" --> ISPRO-MIB::isDeviceMonitorHumidityName
 # .1.3.6.1.4.1.19011.1.3.2.1.3.1.2.1.3.1 4407 --> ISPRO-MIB::isDeviceMonitorHumidity
 # .1.3.6.1.4.1.19011.1.3.2.1.3.1.2.1.4.1 3 --> ISPRO-MIB::isDeviceMonitorHumidityAlarm
 
 
-def discover_ispro_sensors_humid(info):
-    return [(name, None) for name, _reading_str, status in info if status not in ["1", "2"]]
+def discover_ispro_sensors_humid(section: StringTable) -> DiscoveryResult:
+    yield from (
+        Service(item=name) for name, _reading_str, status in section if status not in ["1", "2"]
+    )
 
 
-def check_ispro_sensors_humid(item, params, info):
-    for name, reading_str, status in info:
+def check_ispro_sensors_humid(item: str, params: CheckParams, section: StringTable) -> CheckResult:
+    for name, reading_str, status in section:
         if item == name:
-            devstatus, devstatus_name = ispro_sensors_alarm_states(status)
-            yield devstatus, "Device status: %s" % devstatus_name
-            yield check_humidity(float(reading_str) / 100.0, params)
+            devstatus, devstatus_name = sensors_alarm_states(status)
+            yield Result(state=devstatus, summary=f"Device status: {devstatus_name}")
+            yield from check_humidity(float(reading_str) / 100.0, params)
 
 
 def parse_ispro_sensors_humid(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["ispro_sensors_humid"] = LegacyCheckDefinition(
+snmp_section_ispro_sensors_humid = SimpleSNMPSection(
     name="ispro_sensors_humid",
     parse_function=parse_ispro_sensors_humid,
     detect=DETECT_ISPRO_SENSORS,
@@ -42,8 +47,14 @@ check_info["ispro_sensors_humid"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.19011.1.3.2.1.3.1.2.1",
         oids=["2", "3", "4"],
     ),
+)
+
+
+check_plugin_ispro_sensors_humid = CheckPlugin(
+    name="ispro_sensors_humid",
     service_name="Humidity %s",
     discovery_function=discover_ispro_sensors_humid,
     check_function=check_ispro_sensors_humid,
     check_ruleset_name="humidity",
+    check_default_parameters={},
 )
--- cmk/plugins/ispro/lib.py
+++ cmk/plugins/ispro/lib.py
@@ -3,6 +3,18 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-from cmk.agent_based.v2 import startswith
+from cmk.agent_based.v2 import startswith, State
 
 DETECT_ISPRO_SENSORS = startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.19011.1.3.2")
+
+
+def sensors_alarm_states(status: str) -> tuple[State, str]:
+    return {
+        "1": (State.UNKNOWN, "unknown"),
+        "2": (State.WARN, "disable"),
+        "3": (State.OK, "normal"),
+        "4": (State.WARN, "below low warning"),
+        "5": (State.CRIT, "below low critical"),
+        "6": (State.WARN, "above high warning"),
+        "7": (State.CRIT, "above high critical"),
+    }.get(status, (State.UNKNOWN, f"unexpected({status})"))

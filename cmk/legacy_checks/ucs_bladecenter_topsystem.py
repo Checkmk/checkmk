e53632a54682c59d0526fb87b9bfafddf0885e87
Matthew Rindel <matthew.rindel@checkmk.com>
2026-09-01 14:01:09 +0200

    Legacy check migration: ucs_bladecenter_topsystem I
    CMK-38600
    
    Change-Id: I4e3b7b30d1940050e2bb6d5124d13317e23f016d

--- cmk/legacy_checks/ucs_bladecenter_topsystem.py
+++ cmk/legacy_checks/ucs_bladecenter_topsystem.py
@@ -3,36 +3,45 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # <<<ucs_bladecenter_topsystem:sep(9)>>>
 # topSystem   Address 172.20.33.175   CurrentTime 2015-07-15T16:40:27.600 Ipv6Addr :: Mode cluster    Name svie23ucsfi01  SystemUpTime 125:16:10:53
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import StringTable
-
-check_info = {}
-
-
-def discover_ucs_bladecenter_topsystem(info):  # noqa: ARG001
-    yield None, None
-
-
-def check_ucs_bladecenter_topsystem(item, _no_params, info):  # noqa: ARG001
-    for entry in info[0][1:]:
-        tokens = entry.split(" ", 1)
-        if len(tokens) == 2:
-            yield 0, "%s: %s" % tuple(tokens)
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
 def parse_ucs_bladecenter_topsystem(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["ucs_bladecenter_topsystem"] = LegacyCheckDefinition(
+def discover_ucs_bladecenter_topsystem(section: StringTable) -> DiscoveryResult:  # noqa: ARG001
+    yield Service()
+
+
+def check_ucs_bladecenter_topsystem(section: StringTable) -> CheckResult:
+    for entry in section[0][1:]:
+        tokens = entry.split(" ", 1)
+        if len(tokens) == 2:
+            yield Result(state=State.OK, summary=f"{tokens[0]}: {tokens[1]}")
+
+
+agent_section_ucs_bladecenter_topsystem = AgentSection(
     name="ucs_bladecenter_topsystem",
     parse_function=parse_ucs_bladecenter_topsystem,
+)
+
+
+check_plugin_ucs_bladecenter_topsystem = CheckPlugin(
+    name="ucs_bladecenter_topsystem",
     service_name="UCS TopSystem Info",
     discovery_function=discover_ucs_bladecenter_topsystem,
     check_function=check_ucs_bladecenter_topsystem,
--- tests/unit/cmk/legacy_checks/test_ucs_bladecenter_topsystem.py
+++ tests/unit/cmk/legacy_checks/test_ucs_bladecenter_topsystem.py
@@ -1,85 +1,38 @@
 #!/usr/bin/env python3
-# Copyright (C) 2025 Checkmk GmbH - License: GNU General Public License v2
+# Copyright (C) 2019 Checkmk GmbH - License: GNU General Public License v2
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-
-from collections.abc import Mapping, Sequence
-
-import pytest
-
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import Result, Service, State
 from cmk.legacy_checks.ucs_bladecenter_topsystem import (
     check_ucs_bladecenter_topsystem,
     discover_ucs_bladecenter_topsystem,
     parse_ucs_bladecenter_topsystem,
 )
 
-
-@pytest.mark.parametrize(
-    "string_table, expected_discoveries",
+# The first element names the class and is skipped, as is any element without a value.
+_SECTION = parse_ucs_bladecenter_topsystem(
     [
-        (
-            [
-                [
-                    "topSystem",
-                    "Address 192.168.1.1",
-                    "CurrentTime 2015-07-15T16:40:27.600",
-                    "Ipv6Addr ::",
-                    "Mode cluster",
-                    "Name svie23ucsfi01",
-                    "SystemUpTime 125:16:10:53",
-                ]
-            ],
-            [(None, None)],
-        ),
-    ],
+        [
+            "topSystem",
+            "Address 172.20.33.175",
+            "CurrentTime 2015-07-15T16:40:27.600",
+            "Mode cluster",
+            "Name svie23ucsfi01",
+            "NoValue",
+        ]
+    ]
 )
-def test_discover_ucs_bladecenter_topsystem(
-    string_table: StringTable, expected_discoveries: Sequence[tuple[str, Mapping[str, object]]]
-) -> None:
-    """Test discovery function for ucs_bladecenter_topsystem check."""
-    parsed = parse_ucs_bladecenter_topsystem(string_table)
-    result = list(discover_ucs_bladecenter_topsystem(parsed))
-    assert sorted(result) == sorted(expected_discoveries)
 
 
-@pytest.mark.parametrize(
-    "item, params, string_table, expected_results",
-    [
-        (
-            None,
-            {},
-            [
-                [
-                    "topSystem",
-                    "Address 192.168.1.1",
-                    "CurrentTime 2015-07-15T16:40:27.600",
-                    "Ipv6Addr ::",
-                    "Mode cluster",
-                    "Name svie23ucsfi01",
-                    "SystemUpTime 125:16:10:53",
-                ]
-            ],
-            [
-                (0, "Address: 192.168.1.1"),
-                (0, "CurrentTime: 2015-07-15T16:40:27.600"),
-                (0, "Ipv6Addr: ::"),
-                (0, "Mode: cluster"),
-                (0, "Name: svie23ucsfi01"),
-                (0, "SystemUpTime: 125:16:10:53"),
-            ],
-        ),
-    ],
-)
-def test_check_ucs_bladecenter_topsystem(
-    item: str,
-    params: Mapping[str, object],
-    string_table: StringTable,
-    expected_results: Sequence[object],
-) -> None:
-    """Test check function for ucs_bladecenter_topsystem check."""
-    parsed = parse_ucs_bladecenter_topsystem(string_table)
-    result = list(check_ucs_bladecenter_topsystem(item, params, parsed))
-    assert result == expected_results
+def test_discover_ucs_bladecenter_topsystem() -> None:
+    assert list(discover_ucs_bladecenter_topsystem(_SECTION)) == [Service()]
+
+
+def test_check_ucs_bladecenter_topsystem() -> None:
+    assert list(check_ucs_bladecenter_topsystem(_SECTION)) == [
+        Result(state=State.OK, summary="Address: 172.20.33.175"),
+        Result(state=State.OK, summary="CurrentTime: 2015-07-15T16:40:27.600"),
+        Result(state=State.OK, summary="Mode: cluster"),
+        Result(state=State.OK, summary="Name: svie23ucsfi01"),
+    ]

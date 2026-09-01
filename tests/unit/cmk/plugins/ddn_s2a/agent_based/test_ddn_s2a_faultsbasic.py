1f4bf71750fd27c7d88bd0a5a93381649514857b
Matthew Rindel <matthew.rindel@checkmk.com>
2026-09-01 12:00:25 +0200

    Legacy check migration: ddn_s2a_faultsbasic II
    CMK-33616
    CMK-33617
    CMK-33618
    CMK-33619
    CMK-33620
    CMK-33621
    CMK-33622
    CMK-33623
    CMK-33624
    
    Change-Id: I8e99ac1f683daaf0026d277327a932e5cca926b6

--- tests/unit/cmk/plugins/ddn_s2a/agent_based/test_ddn_s2a_faultsbasic.py
+++ tests/unit/cmk/plugins/ddn_s2a/agent_based/test_ddn_s2a_faultsbasic.py
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.agent_based.v2 import Result, Service, State
-from cmk.legacy_checks.ddn_s2a_faultsbasic import (
+from cmk.plugins.ddn_s2a.agent_based.ddn_s2a_faultsbasic import (
     check_ddn_s2a_faultsbasic,
     check_ddn_s2a_faultsbasic_bootstatus,
     check_ddn_s2a_faultsbasic_cachecoh,

7bf46d1a1790a8b66d35904944d629d3556bed8f
Matthew Rindel <matthew.rindel@checkmk.com>
2026-09-01 14:01:12 +0200

    Legacy check migration: ucs_c_rack_server_health II
    CMK-38600
    
    Change-Id: I308773938bb41c85b3a63a0e2655d23048419f7e

--- packages/cmk-plugins/tests/cmk/plugins/ucs_bladecenter/agent_based/test_ucs_c_rack_server_health.py
+++ packages/cmk-plugins/tests/cmk/plugins/ucs_bladecenter/agent_based/test_ucs_c_rack_server_health.py
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.agent_based.v2 import Result, Service, State
-from cmk.legacy_checks.ucs_c_rack_server_health import (
+from cmk.plugins.ucs_bladecenter.agent_based.ucs_c_rack_server_health import (
     check_ucs_c_rack_server_health,
     discover_ucs_c_rack_server_health,
     parse_ucs_c_rack_server_health,

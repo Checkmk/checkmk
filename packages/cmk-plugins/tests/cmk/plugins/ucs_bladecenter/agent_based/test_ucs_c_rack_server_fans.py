1147881fb29c94919616f1f7f7c0ba2f499c9ef4
Matthew Rindel <matthew.rindel@checkmk.com>
2026-09-01 14:01:13 +0200

    Legacy check migration: ucs_c_rack_server_fans II
    CMK-38600
    
    Change-Id: I0451535afc6d80b0f86845116397622fca19fb7e

--- packages/cmk-plugins/tests/cmk/plugins/ucs_bladecenter/agent_based/test_ucs_c_rack_server_fans.py
+++ packages/cmk-plugins/tests/cmk/plugins/ucs_bladecenter/agent_based/test_ucs_c_rack_server_fans.py
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.agent_based.v2 import Result, Service, State
-from cmk.legacy_checks.ucs_c_rack_server_fans import (
+from cmk.plugins.ucs_bladecenter.agent_based.ucs_c_rack_server_fans import (
     check_ucs_c_rack_server_fans,
     discover_ucs_c_rack_server_fans,
     parse_ucs_c_rack_server_fans,

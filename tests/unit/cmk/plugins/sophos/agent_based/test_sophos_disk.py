e5364911e64bfbc51a48b9e0fa6a30017e50bb28
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-09-02 15:11:47 +0200

    Legacy check migration: sophos_disk II
    Jira: CMK-38558
    Change-Id: I1ea526e80fea1ca1e826f51d3192d0b53a05e6dc

--- tests/unit/cmk/plugins/sophos/agent_based/test_sophos_disk.py
+++ tests/unit/cmk/plugins/sophos/agent_based/test_sophos_disk.py
@@ -8,7 +8,7 @@ from collections.abc import Sequence
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.sophos_disk import (
+from cmk.plugins.sophos.agent_based.sophos_disk import (
     check_sophos_disk,
     discover_sophos_disk,
     Params,

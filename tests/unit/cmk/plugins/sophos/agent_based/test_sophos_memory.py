43dd6a351463b2d939aeb67932728fc913d9bb30
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-09-02 15:25:18 +0200

    Legacy check migration: sophos_memory II
    Jira: CMK-38558
    Change-Id: Iad8f248b88ba779e51b8547be763779c412db437

--- tests/unit/cmk/plugins/sophos/agent_based/test_sophos_memory.py
+++ tests/unit/cmk/plugins/sophos/agent_based/test_sophos_memory.py
@@ -8,7 +8,7 @@ from collections.abc import Sequence
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.sophos_memory import (
+from cmk.plugins.sophos.agent_based.sophos_memory import (
     check_sophos_memory,
     discover_sophos_memory,
     Params,

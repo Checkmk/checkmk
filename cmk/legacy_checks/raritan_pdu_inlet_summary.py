d4c3617a890cc98d03e4128d46bab6cfdaac7ecf
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-09-02 09:02:29 +0200

    Legacy check migration: raritan_pdu_inlet_summary I
    Change-Id: If5fcca94b35becba930c2ac549b8b366f4773dde

--- cmk/legacy_checks/raritan_pdu_inlet_summary.py
+++ cmk/legacy_checks/raritan_pdu_inlet_summary.py
@@ -3,39 +3,61 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="type-arg"
+# mypy: disable-error-code="explicit-any"
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree
-from cmk.legacy_includes.elphase import check_elphase
-from cmk.legacy_includes.raritan import raritan_map_state, raritan_map_type
-from cmk.plugins.raritan.lib import DETECT_RARITAN
+from collections.abc import Mapping
+from typing import Any
 
-check_info = {}
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    OIDEnd,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
+from cmk.plugins.lib.elphase import check_elphase, ElPhase, ReadingState, ReadingWithState
+from cmk.plugins.raritan.lib import (
+    DETECT_RARITAN,
+    elphase_from_readings,
+    STATE_MAPPING,
+    TYPE_MAPPING,
+)
 
-
-def parse_raritan_pdu_inlet_summary(string_table):
-    summary: dict[str, tuple] = {}
-    for sensor_type, decimal_digits, availability, sensor_state, value in string_table:
-        if availability == "1" and sensor_type in raritan_map_type:  # handled sensor types
-            key, _key_info = raritan_map_type[sensor_type]  # get key for elphase.include
-            value = float(value) / 10 ** int(decimal_digits)
-            state, state_info = raritan_map_state[sensor_state]
-
-            if state > 0:
-                summary[key] = (value, (state, state_info))
-            else:
-                summary[key] = (value, None)
-
-    return {"Summary": summary} if summary else None
-
-
-def discover_raritan_pdu_inlet_summary(section):
-    yield from ((item, {}) for item in section)
+type Section = Mapping[str, ElPhase]
 
 
-check_info["raritan_pdu_inlet_summary"] = LegacyCheckDefinition(
+def parse_raritan_pdu_inlet_summary(string_table: StringTable) -> Section | None:
+    readings: dict[str, ReadingWithState] = {}
+    for sensor_type, decimal_digits, availability, sensor_state, value_str in string_table:
+        if availability == "1" and sensor_type in TYPE_MAPPING:  # handled sensor types
+            key, _key_info = TYPE_MAPPING[sensor_type]
+            value = float(value_str) / 10 ** int(decimal_digits)
+            state, state_readable = STATE_MAPPING[sensor_state]
+            readings[key] = ReadingWithState(
+                value=value,
+                state=None if state is State.OK else ReadingState(state=state, text=state_readable),
+            )
+
+    return {"Summary": elphase_from_readings(readings)} if readings else None
+
+
+def discover_raritan_pdu_inlet_summary(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section)
+
+
+def check_raritan_pdu_inlet_summary(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if (elphase := section.get(item)) is None:
+        return
+    yield from check_elphase(params, elphase)
+
+
+snmp_section_raritan_pdu_inlet_summary = SimpleSNMPSection(
     name="raritan_pdu_inlet_summary",
     detect=DETECT_RARITAN,
     fetch=SNMPTree(
@@ -43,8 +65,14 @@ check_info["raritan_pdu_inlet_summary"] = LegacyCheckDefinition(
         oids=[OIDEnd(), "3.3.4.1.7.1.1", "5.2.3.1.2.1.1", "5.2.3.1.3.1.1", "5.2.3.1.4.1.1"],
     ),
     parse_function=parse_raritan_pdu_inlet_summary,
+)
+
+
+check_plugin_raritan_pdu_inlet_summary = CheckPlugin(
+    name="raritan_pdu_inlet_summary",
     service_name="Input %s",
     discovery_function=discover_raritan_pdu_inlet_summary,
-    check_function=check_elphase,
+    check_function=check_raritan_pdu_inlet_summary,
     check_ruleset_name="el_inphase",
+    check_default_parameters={},
 )

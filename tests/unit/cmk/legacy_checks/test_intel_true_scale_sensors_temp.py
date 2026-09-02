5cd341e1e6502fb706938654c635f7a6faf10a38
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-09-02 18:00:27 +0200

    intel_true_scale_sensors_temp: cover discovery filter and sensor list output before migration
    Jira: CMK-38562
    Change-Id: I6456c6192a69ce39afaf785f5f9a16e4f0b853dd

---
+++ tests/unit/cmk/legacy_checks/test_intel_true_scale_sensors_temp.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+# mypy: disable-error-code="no-untyped-call"
+
+from collections.abc import Sequence
+
+import pytest
+
+from cmk.agent_based.v2 import StringTable
+from cmk.legacy_checks.intel_true_scale_sensors_temp import (
+    check_intel_true_scale_sensors_temp,
+    discover_intel_true_scale_sensors_temp,
+    parse_intel_true_scale_sensors,
+)
+
+# synthetic data
+STRING_TABLE: Sequence[StringTable] = [
+    [["1", "2"], ["2", "9"], ["3", "0"], ["4", "20"], ["5", "5"]],
+    [
+        ["1.1.1", "2", "4", "FUSION -- baseboard temp", "41"],
+        ["1.1.2", "2", "3", "FUSION -- fusion temp", "32"],
+        ["1.1.3", "2", "1", "FUSION -- inlet air temp", "35"],
+        ["1.1.4", "2", "5", " FUSION -- baseboard temp", "36"],
+        ["1.2.1", "2", "2", "FUSION -- baseboard temp", "49"],
+        ["1.2.2", "2", "0", "FUSION -- fusion temp", "31"],
+        ["1.3.1", "3", "4", "FUSION -- fan1 speed", "3000"],
+        ["9.4.7", "2", "3", "FUSION -- outlet air temp", "40"],
+        ["1.4.1", "5", "1", "FUSION -- psu1 voltage", "12000"],
+    ],
+]
+
+
+def test_discover_intel_true_scale_sensors_temp_needs_a_temperature_sensor() -> None:
+    assert list(
+        discover_intel_true_scale_sensors_temp(parse_intel_true_scale_sensors(STRING_TABLE))
+    ) == [("slot 1", {}), ("slot 2", {}), ("slot 4", {})]
+
+
+@pytest.mark.parametrize(
+    "item, expected_results",
+    [
+        pytest.param(
+            "slot 1",
+            [
+                (0, "Sensors: 4", []),
+                (0, "Highest: 41.0 °C", [("temp", 41.0)]),
+                (0, "Average: 36.0 °C", []),
+                (0, "Lowest: 32.0 °C", []),
+                (1, "2 fusion: 32.0 °C, State on device: warning", []),
+                (3, "3 inlet air: 35.0 °C, State on device: unknown", []),
+                (3, "4 -- baseboard: 36.0 °C, State on device: disabled", []),
+            ],
+            id="good_sensor_stays_silent_and_names_keep_their_sensor_index",
+        ),
+        pytest.param(
+            "slot 2",
+            [
+                (0, "Sensors: 2", []),
+                (0, "Highest: 49.0 °C", [("temp", 49.0)]),
+                (0, "Average: 40.0 °C", []),
+                (0, "Lowest: 31.0 °C", []),
+                (2, "1 baseboard: 49.0 °C, State on device: bad", []),
+                (2, "2 fusion: 31.0 °C, State on device: invalid", []),
+            ],
+            id="bad_and_invalid_are_both_crit",
+        ),
+        pytest.param(
+            "slot 4",
+            [
+                (0, "Sensors: 1", []),
+                (0, "Highest: 40.0 °C", [("temp", 40.0)]),
+                (0, "Average: 40.0 °C", []),
+                (0, "Lowest: 40.0 °C", []),
+                (1, "7 outlet air: 40.0 °C, State on device: warning", []),
+            ],
+            id="voltage_sensor_is_left_out_and_only_the_last_two_oid_components_count",
+        ),
+        pytest.param("slot 9", [], id="unknown_item"),
+    ],
+)
+def test_check_intel_true_scale_sensors_temp(item: str, expected_results: Sequence[object]) -> None:
+    assert (
+        list(
+            check_intel_true_scale_sensors_temp(
+                item, {}, parse_intel_true_scale_sensors(STRING_TABLE)
+            )
+        )
+        == expected_results
+    )

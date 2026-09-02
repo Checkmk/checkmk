3ba8623b068c1dcd13ad8bfa97d526d795f4c521
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-09-02 09:18:13 +0200

    Legacy check migration: raritan_pdu_plugs II
    Change-Id: I26bdfa73fb01a20d997cd98ae4850a313a784eed

--- tests/unit/cmk/plugins/raritan/agent_based/test_raritan_pdu_plugs.py
+++ tests/unit/cmk/plugins/raritan/agent_based/test_raritan_pdu_plugs.py
@@ -8,7 +8,7 @@ from collections.abc import Sequence
 import pytest
 
 from cmk.agent_based.v2 import Result, Service, State
-from cmk.legacy_checks.raritan_pdu_plugs import (
+from cmk.plugins.raritan.agent_based.raritan_pdu_plugs import (
     check_raritan_pdu_plugs,
     CombinedParams,
     discover_raritan_pdu_plugs,

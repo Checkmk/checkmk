593ca68b41e2df4cf9b530990fff0ea3d2f0f463
Solomon Jacobs <solomon.jacobs@checkmk.com>
2026-09-02 17:20:23 +0200

    Legacy check migration: intel_true_scale_chassis_temp II
    Jira: CMK-38562
    Change-Id: I03e05215c7a15036ab5056548d792d799934a8c0

--- tests/unit/cmk/plugins/intel/agent_based/test_intel_true_scale_chassis_temp.py
+++ tests/unit/cmk/plugins/intel/agent_based/test_intel_true_scale_chassis_temp.py
@@ -6,7 +6,7 @@
 import pytest
 
 from cmk.agent_based.v2 import Result, Service, State, StringTable
-from cmk.legacy_checks.intel_true_scale_chassis_temp import (
+from cmk.plugins.intel.agent_based.intel_true_scale_chassis_temp import (
     check_intel_true_scale_chassis_temp,
     discover_intel_true_scale_chassis_temp,
     parse_intel_true_scale_chassis_temp,

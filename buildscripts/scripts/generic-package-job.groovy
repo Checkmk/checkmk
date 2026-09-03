44fa5c438c7dee4009b93b3bcbc881e32bf04b4a
Jonas Scharpf <jonas.scharpf@checkmk.com>
2026-09-03 11:36:19 +0200

    Use static docker image for generic package job
    CMK-38667
    
    Change-Id: Iac2687c3a8b4a13d61221186e0a28e5b37a2240d

--- buildscripts/scripts/generic-package-job.groovy
+++ buildscripts/scripts/generic-package-job.groovy
@@ -22,12 +22,25 @@ def main() {
     validate_parameters();
 
     def helper = load("${checkout_dir}/buildscripts/scripts/utils/test_helper.groovy");
+    def versioning = load("${checkout_dir}/buildscripts/scripts/utils/versioning.groovy");
+
+    def safe_branch_name = versioning.safe_branch_name();
+
     currentBuild.description = "Running ${params.PACKAGE_PATH}<br>${currentBuild.description}";
 
     def output_file = params.PACKAGE_PATH.split("/")[-1] + ".log"
+
+    def container_name = "testing-ubuntu-22.04-checkmk-${safe_branch_name}";
+    def docker_tag = "latest-with-docker";
+    // testing-ubuntu-22.04-checkmk-2.4.0:latest-with-docker
+
     dir(checkout_dir) {
         lock(label: "bzl_lock_${env.NODE_NAME.split('\\.')[0].split('-')[-1]}", quantity: 1, resource : null) {
-            inside_container(init: true) {
+            inside_container(
+                image: docker.image("${docker_registry_no_http}/${container_name}:${docker_tag}"),
+                pull: true,
+                init: true,
+            ) {
                 withCredentials(secret_list(params.SECRET_VARS).collect { string(credentialsId: it, variable: it) }) {
                     helper.execute_test([
                         name       : params.PACKAGE_PATH,

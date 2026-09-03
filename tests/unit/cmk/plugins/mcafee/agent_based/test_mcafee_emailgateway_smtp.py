af535ed4cc81384faf31531ea75fe96b5bb6f41a
Max Höhl <max.hoehl@checkmk.com>
2026-09-03 13:45:25 +0200

    Legacy check migration: mcafee_emailgateway_smtp II
    Move mcafee_emailgateway_smtp and its unit tests to their final
    location under cmk/plugins/mcafee/agent_based/.
    
    Jira: CMK-38647
    
    Change-Id: I5e312ffbc6af121d1020a5896d3b876c323cb11e

--- tests/unit/cmk/plugins/mcafee/agent_based/test_mcafee_emailgateway_smtp.py
+++ tests/unit/cmk/plugins/mcafee/agent_based/test_mcafee_emailgateway_smtp.py
@@ -4,7 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 
 from cmk.agent_based.v2 import render, Result, Service, State, StringTable
-from cmk.legacy_checks.mcafee_emailgateway_smtp import (
+from cmk.plugins.mcafee.agent_based.mcafee_emailgateway_smtp import (
     check_mcafee_emailgateway_smtp,
     discover_mcafee_emailgateway_smtp,
 )

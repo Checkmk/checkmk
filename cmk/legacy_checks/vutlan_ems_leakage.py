ec425911476552f6adf6eb6c6c6c9c06ad4e662a
racicLuka <luka.racic@checkmk.com>
2026-09-04 16:11:15 +0200

    Legacy check migration: vutlan_ems_leakage I
    CMK-38823
    
    Change-Id: I2715df02823020fb9cd6461d61ab6fea1dea1613

--- cmk/legacy_checks/vutlan_ems_leakage.py
+++ cmk/legacy_checks/vutlan_ems_leakage.py
@@ -3,57 +3,71 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # vutlan is not a typo!
 # MIB can also be browsed on
 # https://mibs.observium.org/mib/SKYCONTROL-SYSTEM-MIB/#
 
+from collections.abc import Mapping
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    OIDEnd,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.vutlan.lib import DETECT_VUTLAN_EMS
 
-check_info = {}
+Section = Mapping[str, bool]
 
 
-def parse_vutlan_ems_leakage(string_table):
+def parse_vutlan_ems_leakage(string_table: StringTable) -> Section:
     parsed = {}
-    for line in string_table[0]:
+    for line in string_table:
         if line[0].startswith("107"):
             # all OIDs 107xxx are leakage-related
             parsed[line[1]] = bool(int(line[2]))
-
     return parsed
 
 
-def discover_vutlan_ems_leakage(parsed):
-    for sensor_name in parsed:
-        yield sensor_name, {}
-
-
-def check_vutlan_ems_leakage(item, _no_params, parsed):
-    if parsed.get(item) is None:
-        return
-
-    leakage_detected = bool(int(parsed[item]))
-    if leakage_detected:
-        yield 2, "Leak detected"
-        return
-
-    yield 0, "No leak detected"
-
-
-check_info["vutlan_ems_leakage"] = LegacyCheckDefinition(
+snmp_section_vutlan_ems_leakage = SimpleSNMPSection(
     name="vutlan_ems_leakage",
-    detect=DETECT_VUTLAN_EMS,
-    fetch=[
-        SNMPTree(
-            base=".1.3.6.1.4.1.39052.1.3.1",
-            oids=[OIDEnd(), "7", "9"],
-        )
-    ],
     parse_function=parse_vutlan_ems_leakage,
+    detect=DETECT_VUTLAN_EMS,
+    fetch=SNMPTree(
+        base=".1.3.6.1.4.1.39052.1.3.1",
+        oids=[
+            OIDEnd(),
+            "7",  # vutlan.mib::ctlUnitElementName (can be user-defined)
+            "9",  # vutlan.mib::ctlUnitElementValue
+        ],
+    ),
+)
+
+
+def discover_vutlan_ems_leakage(section: Section) -> DiscoveryResult:
+    yield from (Service(item=sensor_name) for sensor_name in section)
+
+
+def check_vutlan_ems_leakage(item: str, section: Section) -> CheckResult:
+    leakage_detected = section.get(item)
+    if leakage_detected is None:
+        return
+
+    if leakage_detected:
+        yield Result(state=State.CRIT, summary="Leak detected")
+        return
+
+    yield Result(state=State.OK, summary="No leak detected")
+
+
+check_plugin_vutlan_ems_leakage = CheckPlugin(
+    name="vutlan_ems_leakage",
     service_name="Leakage %s",
     discovery_function=discover_vutlan_ems_leakage,
     check_function=check_vutlan_ems_leakage,
--- tests/unit/cmk/legacy_checks/test_vutlan_ems_leakage.py
+++ tests/unit/cmk/legacy_checks/test_vutlan_ems_leakage.py
@@ -3,122 +3,72 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-
-from collections.abc import Mapping, Sequence
+from collections.abc import Sequence
 
 import pytest
 
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import Result, Service, State, StringTable
 from cmk.legacy_checks.vutlan_ems_leakage import (
     check_vutlan_ems_leakage,
     discover_vutlan_ems_leakage,
     parse_vutlan_ems_leakage,
 )
 
+STRING_TABLE = [
+    ["101001", "Dry-1", "0"],
+    ["101002", "Dry-2", "0"],
+    ["101003", "Dry-3", "0"],
+    ["101004", "Dry-4", "0"],
+    ["106001", "Analog-5", "0"],
+    ["107001", "Analog-6", "0"],
+    ["201001", "Onboard Temperature", "32.80"],
+    ["201002", "Analog-1", "22.00"],
+    ["201003", "Analog-2", "22.10"],
+    ["202001", "Analog-3", "46.20"],
+    ["202002", "Analog-4", "42.10"],
+    ["203001", "Onboard Voltage DC", "12.06"],
+    ["301001", "Analog Power", "on"],
+    ["304001", "Power-1", "off"],
+    ["304002", "Power-2", "off"],
+    ["403001", "USB Web camera", "0"],
+    ["107002", "Banana", "1"],
+]
+
 
 @pytest.mark.parametrize(
     "string_table, expected_discoveries",
     [
-        (
-            [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                    ["107002", "Banana", "1"],
-                ]
-            ],
-            [("Analog-6", {}), ("Banana", {})],
-        ),
+        (STRING_TABLE, [Service(item="Analog-6"), Service(item="Banana")]),
     ],
 )
 def test_discover_vutlan_ems_leakage(
-    string_table: StringTable, expected_discoveries: Sequence[tuple[str, Mapping[str, object]]]
+    string_table: StringTable, expected_discoveries: Sequence[Service]
 ) -> None:
-    """Test discovery function for vutlan_ems_leakage check."""
-    parsed = parse_vutlan_ems_leakage(string_table)
-    result = list(discover_vutlan_ems_leakage(parsed))
+    section = parse_vutlan_ems_leakage(string_table)
+    result = list(discover_vutlan_ems_leakage(section))
     assert sorted(result) == sorted(expected_discoveries)
 
 
 @pytest.mark.parametrize(
-    "item, params, string_table, expected_results",
+    "item, string_table, expected_results",
     [
         (
             "Analog-6",
-            {},
-            [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                    ["107002", "Banana", "1"],
-                ]
-            ],
-            [(0, "No leak detected")],
+            STRING_TABLE,
+            [Result(state=State.OK, summary="No leak detected")],
         ),
         (
             "Banana",
-            {},
-            [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                    ["107002", "Banana", "1"],
-                ]
-            ],
-            [(2, "Leak detected")],
+            STRING_TABLE,
+            [Result(state=State.CRIT, summary="Leak detected")],
         ),
     ],
 )
 def test_check_vutlan_ems_leakage(
     item: str,
-    params: Mapping[str, object],
     string_table: StringTable,
     expected_results: Sequence[object],
 ) -> None:
-    """Test check function for vutlan_ems_leakage check."""
-    parsed = parse_vutlan_ems_leakage(string_table)
-    result = list(check_vutlan_ems_leakage(item, params, parsed))
+    section = parse_vutlan_ems_leakage(string_table)
+    result = list(check_vutlan_ems_leakage(item, section))
     assert result == expected_results

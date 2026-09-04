dbe59445934147506b85e52f8e5c9352963b5959
racicLuka <luka.racic@checkmk.com>
2026-09-04 16:13:00 +0200

    Legacy check migration: vutlan_ems_temp I
    CMK-38824
    
    Change-Id: I1c7a93e94b6c1718143071d4eff5abf2e513a571

--- cmk/legacy_checks/vutlan_ems_temp.py
+++ cmk/legacy_checks/vutlan_ems_temp.py
@@ -3,57 +3,83 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import OIDEnd, SNMPTree
-from cmk.legacy_includes.temperature import check_temperature
-from cmk.plugins.vutlan.lib import DETECT_VUTLAN_EMS
-
-check_info = {}
-
 # vutlan is not a typo!
 # MIB can also be browsed on
 # https://mibs.observium.org/mib/SKYCONTROL-SYSTEM-MIB/#
 # NOTE: the unit is not given in the SNMP walk, it is °C
 
+# mypy: disable-error-code="explicit-any"
 
-def parse_vutlan_ems_temp(string_table):
+from collections.abc import Mapping, MutableMapping
+from typing import Any
+
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    OIDEnd,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    StringTable,
+)
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
+from cmk.plugins.vutlan.lib import DETECT_VUTLAN_EMS
+
+Section = Mapping[str, float]
+
+
+def parse_vutlan_ems_temp(string_table: StringTable) -> Section:
     parsed = {}
-    for line in string_table[0]:
+    for line in string_table:
         if line[0].startswith("201"):
             # all OIDs 201xxx are temperature-related
             parsed[line[1]] = float(line[2])
-
     return parsed
 
 
-def discover_vutlan_ems_temp(parsed):
-    for sensor_name in parsed:
-        yield sensor_name, {}
+snmp_section_vutlan_ems_temp = SimpleSNMPSection(
+    name="vutlan_ems_temp",
+    parse_function=parse_vutlan_ems_temp,
+    detect=DETECT_VUTLAN_EMS,
+    fetch=SNMPTree(
+        base=".1.3.6.1.4.1.39052.1.3.1",
+        oids=[
+            OIDEnd(),
+            "7",  # vutlan.mib::ctlUnitElementName (can be user-defined)
+            "9",  # vutlan.mib::ctlUnitElementValue
+        ],
+    ),
+)
 
 
-def check_vutlan_ems_temp(item, params, parsed):
-    if not parsed.get(item):
+def discover_vutlan_ems_temp(section: Section) -> DiscoveryResult:
+    yield from (Service(item=sensor_name) for sensor_name in section)
+
+
+def check_vutlan_ems_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
+    yield from check_vutlan_ems_temp_impl(item, params, section, get_value_store())
+
+
+def check_vutlan_ems_temp_impl(
+    item: str,
+    params: TempParamType,
+    section: Section,
+    value_store: MutableMapping[str, Any],
+) -> CheckResult:
+    if (reading := section.get(item)) is None:
         return
-
-    yield check_temperature(
-        parsed[item],
+    yield from check_temperature(
+        reading,
         params,
-        "vutlan_ems",  # unique name is needed to activate trend computing
+        unique_name="vutlan_ems",  # unique name is needed to activate trend computing
+        value_store=value_store,
     )
 
 
-check_info["vutlan_ems_temp"] = LegacyCheckDefinition(
+check_plugin_vutlan_ems_temp = CheckPlugin(
     name="vutlan_ems_temp",
-    detect=DETECT_VUTLAN_EMS,
-    fetch=[
-        SNMPTree(
-            base=".1.3.6.1.4.1.39052.1.3.1",
-            oids=[OIDEnd(), "7", "9"],
-        )
-    ],
-    parse_function=parse_vutlan_ems_temp,
     service_name="Temperature %s",
     discovery_function=discover_vutlan_ems_temp,
     check_function=check_vutlan_ems_temp,
--- tests/unit/cmk/legacy_checks/test_vutlan_ems_temp.py
+++ tests/unit/cmk/legacy_checks/test_vutlan_ems_temp.py
@@ -3,144 +3,122 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-
-from collections.abc import Mapping, Sequence
+from collections.abc import Sequence
 
 import pytest
 
-from cmk.agent_based.v2 import StringTable
+from cmk.agent_based.v2 import CheckResult, Metric, Result, Service, State, StringTable
 from cmk.legacy_checks.vutlan_ems_temp import (
-    check_vutlan_ems_temp,
+    check_vutlan_ems_temp_impl,
     discover_vutlan_ems_temp,
     parse_vutlan_ems_temp,
 )
+from cmk.plugins.lib.temperature import TempParamType
+
+STRING_TABLE = [
+    ["101001", "Dry-1", "0"],
+    ["101002", "Dry-2", "0"],
+    ["101003", "Dry-3", "0"],
+    ["101004", "Dry-4", "0"],
+    ["106001", "Analog-5", "0"],
+    ["107001", "Analog-6", "0"],
+    ["201001", "Onboard Temperature", "32.80"],
+    ["201002", "Analog-1", "22.00"],
+    ["201003", "Analog-2", "22.10"],
+    ["201004", "Zero-Analog", "0.00"],
+    ["202001", "Analog-3", "46.20"],
+    ["202002", "Analog-4", "42.10"],
+    ["203001", "Onboard Voltage DC", "12.06"],
+    ["301001", "Analog Power", "on"],
+    ["304001", "Power-1", "off"],
+    ["304002", "Power-2", "off"],
+    ["403001", "USB Web camera", "0"],
+]
 
 
 @pytest.mark.parametrize(
     "string_table, expected_discoveries",
     [
         (
+            STRING_TABLE,
             [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                ]
+                Service(item="Analog-1"),
+                Service(item="Analog-2"),
+                Service(item="Zero-Analog"),
+                Service(item="Onboard Temperature"),
             ],
-            [("Analog-1", {}), ("Analog-2", {}), ("Onboard Temperature", {})],
         ),
     ],
 )
 def test_discover_vutlan_ems_temp(
-    string_table: StringTable, expected_discoveries: Sequence[tuple[str, Mapping[str, object]]]
+    string_table: StringTable, expected_discoveries: Sequence[Service]
 ) -> None:
-    """Test discovery function for vutlan_ems_temp check."""
-    parsed = parse_vutlan_ems_temp(string_table)
-    result = list(discover_vutlan_ems_temp(parsed))
+    section = parse_vutlan_ems_temp(string_table)
+    result = list(discover_vutlan_ems_temp(section))
     assert sorted(result) == sorted(expected_discoveries)
 
 
 @pytest.mark.parametrize(
-    "item, params, string_table, expected_results",
+    "item, params, expected_results",
     [
         (
             "Analog-1",
             {"levels": (80.0, 90.0)},
             [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                ]
+                Metric("temp", 22.0, levels=(80.0, 90.0)),
+                Result(state=State.OK, summary="Temperature: 22.0 °C"),
+                Result(
+                    state=State.OK,
+                    notice="Configuration: prefer user levels over device levels (used user levels)",
+                ),
             ],
-            [(0, "22.0 °C", [("temp", 22.0, 80.0, 90.0)])],
         ),
         (
             "Analog-2",
             {"levels": (10.0, 20.0)},
             [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                ]
+                Metric("temp", 22.1, levels=(10.0, 20.0)),
+                Result(
+                    state=State.CRIT,
+                    summary="Temperature: 22.1 °C (warn/crit at 10.0 °C/20.0 °C)",
+                ),
+                Result(
+                    state=State.OK,
+                    notice="Configuration: prefer user levels over device levels (used user levels)",
+                ),
             ],
-            [(2, "22.1 °C (warn/crit at 10.0/20.0 °C)", [("temp", 22.1, 10.0, 20.0)])],
         ),
         (
             "Onboard Temperature",
             {"levels": (80.0, 90.0)},
             [
-                [
-                    ["101001", "Dry-1", "0"],
-                    ["101002", "Dry-2", "0"],
-                    ["101003", "Dry-3", "0"],
-                    ["101004", "Dry-4", "0"],
-                    ["106001", "Analog-5", "0"],
-                    ["107001", "Analog-6", "0"],
-                    ["201001", "Onboard Temperature", "32.80"],
-                    ["201002", "Analog-1", "22.00"],
-                    ["201003", "Analog-2", "22.10"],
-                    ["202001", "Analog-3", "46.20"],
-                    ["202002", "Analog-4", "42.10"],
-                    ["203001", "Onboard Voltage DC", "12.06"],
-                    ["301001", "Analog Power", "on"],
-                    ["304001", "Power-1", "off"],
-                    ["304002", "Power-2", "off"],
-                    ["403001", "USB Web camera", "0"],
-                ]
+                Metric("temp", 32.8, levels=(80.0, 90.0)),
+                Result(state=State.OK, summary="Temperature: 32.8 °C"),
+                Result(
+                    state=State.OK,
+                    notice="Configuration: prefer user levels over device levels (used user levels)",
+                ),
             ],
-            [(0, "32.8 °C", [("temp", 32.8, 80.0, 90.0)])],
         ),
     ],
 )
-def test_check_vutlan_ems_temp(
-    item: str,
-    params: Mapping[str, object],
-    string_table: StringTable,
-    expected_results: Sequence[object],
+def testcheck_vutlan_ems_temp_impl(
+    item: str, params: TempParamType, expected_results: CheckResult
 ) -> None:
-    """Test check function for vutlan_ems_temp check."""
-    parsed = parse_vutlan_ems_temp(string_table)
-    result = list(check_vutlan_ems_temp(item, params, parsed))
+    section = parse_vutlan_ems_temp(STRING_TABLE)
+    result = list(check_vutlan_ems_temp_impl(item, params, section, {}))
     assert result == expected_results
+
+
+def testcheck_vutlan_ems_temp_impl_reports_zero_degree_reading() -> None:
+    """A 0.0 °C reading used to be treated as falsy and silently skipped."""
+    section = parse_vutlan_ems_temp(STRING_TABLE)
+    result = list(check_vutlan_ems_temp_impl("Zero-Analog", {"levels": (80.0, 90.0)}, section, {}))
+    assert result == [
+        Metric("temp", 0.0, levels=(80.0, 90.0)),
+        Result(state=State.OK, summary="Temperature: 0.0 °C"),
+        Result(
+            state=State.OK,
+            notice="Configuration: prefer user levels over device levels (used user levels)",
+        ),
+    ]

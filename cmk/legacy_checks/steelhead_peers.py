9633d1550da88556f047e3745b0a948ced968fc6
Anvar Umuraliev <anvar.umuraliev@checkmk.com>
2026-09-07 18:28:50 +0200

    Legacy check migration: steelhead_peers I
    CMK-38872
    
    Change-Id: Idb657e98b9506b8249212c49dbe72968b0b10f98

--- cmk/legacy_checks/steelhead_peers.py
+++ cmk/legacy_checks/steelhead_peers.py
@@ -3,37 +3,31 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
-
 # .1.3.6.1.4.1.17163.1.1.2.6.1.1.2.1 vadetsh1 --> STEELHEAD-MIB::peerHostname.1
 # .1.3.6.1.4.1.17163.1.1.2.6.1.1.3.1 8.5.3c --> STEELHEAD-MIB::peerVersion.1
 # .1.3.6.1.4.1.17163.1.1.2.6.1.1.4.1 10.1.0.14 --> STEELHEAD-MIB::peerAddress.1
 # .1.3.6.1.4.1.17163.1.1.2.6.1.1.5.1 CX770 --> STEELHEAD-MIB::peerModel.1
 
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree, StringTable
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.steelhead.lib import DETECT_STEELHEAD
-
-check_info = {}
-
-
-def discover_steelhead_peers(info):
-    return [(x[0], None) for x in info if x[-1] != "Steelhead Mobile"]
-
-
-def check_steelhead_peers(item, _no_params, info):
-    for host, version, client, client_type in info:
-        if host == item:
-            return 0, f"Version: {version}, Client Address: {client} ({client_type})"
-    return 2, "Peer not connected"
 
 
 def parse_steelhead_peers(string_table: StringTable) -> StringTable:
     return string_table
 
 
-check_info["steelhead_peers"] = LegacyCheckDefinition(
+snmp_section_steelhead_peers = SimpleSNMPSection(
     name="steelhead_peers",
     parse_function=parse_steelhead_peers,
     detect=DETECT_STEELHEAD,
@@ -41,6 +35,30 @@ check_info["steelhead_peers"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.17163.1.1.2.6.1.1",
         oids=["2", "3", "4", "5"],
     ),
+)
+
+
+def discover_steelhead_peers(section: StringTable) -> DiscoveryResult:
+    yield from (
+        Service(item=host)
+        for host, _version, _client, client_type in section
+        if client_type != "Steelhead Mobile"
+    )
+
+
+def check_steelhead_peers(item: str, section: StringTable) -> CheckResult:
+    for host, version, client, client_type in section:
+        if host == item:
+            yield Result(
+                state=State.OK,
+                summary=f"Version: {version}, Client Address: {client} ({client_type})",
+            )
+            return
+    yield Result(state=State.CRIT, summary="Peer not connected")
+
+
+check_plugin_steelhead_peers = CheckPlugin(
+    name="steelhead_peers",
     service_name="Peer %s",
     discovery_function=discover_steelhead_peers,
     check_function=check_steelhead_peers,
---
+++ tests/unit/cmk/legacy_checks/test_steelhead_peers.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+from cmk.agent_based.v2 import Result, Service, State
+from cmk.legacy_checks.steelhead_peers import (
+    check_steelhead_peers,
+    discover_steelhead_peers,
+    parse_steelhead_peers,
+)
+
+STRING_TABLE = [
+    ["vadetsh1", "8.5.3c", "10.1.0.14", "CX770"],
+    ["vadetsh2", "9.2.0a", "10.1.0.15", "CX570"],
+    ["laptop-01", "4.8.1", "10.1.0.55", "Steelhead Mobile"],
+]
+
+
+def test_discovery_skips_steelhead_mobile_peers() -> None:
+    section = parse_steelhead_peers(STRING_TABLE)
+
+    result = list(discover_steelhead_peers(section))
+
+    assert result == [Service(item="vadetsh1"), Service(item="vadetsh2")]
+
+
+def test_check_is_ok_for_connected_peer() -> None:
+    section = parse_steelhead_peers(STRING_TABLE)
+
+    result = list(check_steelhead_peers("vadetsh1", section))
+
+    assert result == [
+        Result(state=State.OK, summary="Version: 8.5.3c, Client Address: 10.1.0.14 (CX770)")
+    ]
+
+
+def test_check_is_crit_for_missing_peer() -> None:
+    section = parse_steelhead_peers(STRING_TABLE)
+
+    result = list(check_steelhead_peers("vadetsh3", section))
+
+    assert result == [Result(state=State.CRIT, summary="Peer not connected")]

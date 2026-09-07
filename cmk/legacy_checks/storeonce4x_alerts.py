d35cabfc650776b84ea5ad7723008aa660f4d5b5
Anvar Umuraliev <anvar.umuraliev@checkmk.com>
2026-09-07 17:39:24 +0200

    Legacy check migration: storeonce4x_alerts I
    CMK-38874
    
    Change-Id: Ibddab56e475aa42d6399f77a3d6fde121b59472a

--- cmk/legacy_checks/storeonce4x_alerts.py
+++ cmk/legacy_checks/storeonce4x_alerts.py
@@ -3,65 +3,85 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="explicit-any"
-# mypy: disable-error-code="no-untyped-def"
-# mypy: disable-error-code="type-arg"
-
 import json
-from collections.abc import Iterable, Mapping, Sequence
-from typing import Any, Literal
+from collections.abc import Sequence
+from dataclasses import dataclass
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-
-check_info = {}
-
-Section = Mapping[Literal["alerts"], Sequence[Mapping[str, Any]]]
-
-
-STATUS_MAP = {"CRITICAL": 2, "WARNING": 1, "OK": 0, "UNKNOWN": 3, "DISABLED": 3}
-
-
-def parse_storeonce4x_alerts(string_table):
-    return {
-        "alerts": [
-            {
-                "cleared": alert["state"].upper() == "CLEARED",
-                "status": STATUS_MAP[alert["status"].upper()],
-                "alertState": alert["alertState"],
-                "description": alert["description"],
-            }
-            for alert in json.loads(string_table[0][0])["members"]
-        ]
-    }
+from cmk.agent_based.v2 import (
+    AgentSection,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    Result,
+    Service,
+    State,
+    StringTable,
+)
 
 
-def discover_storeonce4x_alerts(section: Section) -> Iterable[tuple[None, dict]]:
-    if section:
-        yield None, {}
+@dataclass(frozen=True)
+class Alert:
+    cleared: bool
+    status: State
+    alert_state: str
+    description: str
 
 
-def check_storeonce4x_alerts(_item, _param, parsed):
-    if not parsed["alerts"]:
-        yield 0, "No alerts at all found"
+Section = Sequence[Alert]
+
+
+_STATUS_MAP = {
+    "CRITICAL": State.CRIT,
+    "WARNING": State.WARN,
+    "OK": State.OK,
+    "UNKNOWN": State.UNKNOWN,
+    "DISABLED": State.UNKNOWN,
+}
+
+
+def parse_storeonce4x_alerts(string_table: StringTable) -> Section:
+    return [
+        Alert(
+            cleared=alert["state"].upper() == "CLEARED",
+            status=_STATUS_MAP[alert["status"].upper()],
+            alert_state=alert["alertState"],
+            description=alert["description"],
+        )
+        for alert in json.loads(string_table[0][0])["members"]
+    ]
+
+
+agent_section_storeonce4x_alerts = AgentSection(
+    name="storeonce4x_alerts",
+    parse_function=parse_storeonce4x_alerts,
+)
+
+
+def discover_storeonce4x_alerts(section: Section) -> DiscoveryResult:  # noqa: ARG001
+    yield Service()
+
+
+def check_storeonce4x_alerts(section: Section) -> CheckResult:
+    if not section:
+        yield Result(state=State.OK, summary="No alerts at all found")
         return
 
-    if all(alert["cleared"] for alert in parsed["alerts"]):
-        yield 0, "No uncleared alerts found"
+    if all(alert.cleared for alert in section):
+        yield Result(state=State.OK, summary="No uncleared alerts found")
         return
 
     yield from (
-        (
-            alert["status"],
-            f"Alert State: {alert['alertState']}, Description: {alert['description']}",
+        Result(
+            state=alert.status,
+            summary=f"Alert State: {alert.alert_state}, Description: {alert.description}",
         )
-        for alert in parsed["alerts"]
-        if not alert["cleared"]
+        for alert in section
+        if not alert.cleared
     )
 
 
-check_info["storeonce4x_alerts"] = LegacyCheckDefinition(
+check_plugin_storeonce4x_alerts = CheckPlugin(
     name="storeonce4x_alerts",
-    parse_function=parse_storeonce4x_alerts,
     service_name="Alerts",
     discovery_function=discover_storeonce4x_alerts,
     check_function=check_storeonce4x_alerts,

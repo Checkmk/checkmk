1bc8153cd85947166595cce0c9aa2f48731ab0df
Mehran Shahidi <mehran.shahidi@checkmk.com>
2026-09-08 15:16:21 +0200

    Legacy check migration: f5_bigip_apm II
    CMK-37612
    
    Change-Id: I49d4b961d76b12472cc5f4145fc2dd098c463d8d

--- tests/unit/cmk/plugins/f5_bigip/agent_based/test_f5_bigip_apm.py
+++ tests/unit/cmk/plugins/f5_bigip/agent_based/test_f5_bigip_apm.py
@@ -6,7 +6,7 @@
 import pytest
 
 from cmk.agent_based.v2 import Metric, Result, Service, State, StringTable
-from cmk.legacy_checks.f5_bigip_apm import (
+from cmk.plugins.f5_bigip.agent_based.f5_bigip_apm import (
     check_f5_bigip_apm,
     discover_f5_bigip_apm,
     parse_f5_bigip_apm,

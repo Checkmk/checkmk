86c2b84288bd77532bfb32b891c7ce9800e69668
David Gervasoni <david.gervasoni@checkmk.com>
2026-09-09 11:39:39 +0200

    AWS agent: move the RDS sections into sections/rds.py
    Pure move
    
    CMK-38917
    
    Change-Id: Ic0bd2fc2bce94b7b71ee5afd506b7b48dac36cd9

--- packages/cmk-plugins/tests/cmk/plugins/aws/special_agent/test_agent_aws_rds.py
+++ packages/cmk-plugins/tests/cmk/plugins/aws/special_agent/test_agent_aws_rds.py
@@ -10,7 +10,6 @@ from typing import Protocol
 
 import pytest
 
-from cmk.plugins.aws.special_agent.agent_aws import RDS, RDSLimits, RDSSummary
 from cmk.plugins.aws.special_agent.config import (
     AWSConfig,
     NamingConvention,
@@ -19,6 +18,7 @@ from cmk.plugins.aws.special_agent.config import (
     TagsOption,
 )
 from cmk.plugins.aws.special_agent.sections.core import ResultDistributor
+from cmk.plugins.aws.special_agent.sections.rds import RDS, RDSLimits, RDSSummary
 
 from .agent_aws_fake_clients import (
     FakeCloudwatchClient,

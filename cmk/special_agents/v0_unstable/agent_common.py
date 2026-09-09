9ab969455a5a462e9177ee1dd6248c887709b70b
Max Höhl <max.hoehl@checkmk.com>
2026-09-09 13:24:48 +0200

    log: render special agent logs in the new format
    Special agents kept their own log format. Move the shared special
    agent output onto the unified Checkmk log format so it stays
    consistent with the rest of the product.
    
    Change-Id: I9204b8f35b8fa241bda3a28e70cd0038717de9ea

--- cmk/special_agents/v0_unstable/agent_common.py
+++ cmk/special_agents/v0_unstable/agent_common.py
@@ -25,6 +25,7 @@ from typing import Any, override
 
 import urllib3
 
+from cmk.ccc.log import CMKFormatter
 from cmk.special_agents.v0_unstable.crash_reporting import create_agent_crash_dump
 from cmk.utils.password_store import lookup as lookup_stored_passwords
 from cmk.utils.password_store.hack import resolve_password_hack
@@ -123,9 +124,10 @@ def _special_agent_main_core(
 ) -> int:
     """Main logic special agents"""
     args = parse_arguments(argv)
-    logging.basicConfig(  # astrein: disable=logging-formatter
-        format="%(levelname)s %(asctime)s %(name)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+    handler = logging.StreamHandler()
+    handler.setFormatter(CMKFormatter())
+    logging.basicConfig(
+        handlers=[handler],
         level={0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}.get(
             args.verbose, logging.DEBUG
         ),

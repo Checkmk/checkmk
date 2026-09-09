90bfc417a15cabfa0ed23dacc9cce0eed25e15b0
David Gervasoni <david.gervasoni@checkmk.com>
2026-09-09 11:28:02 +0200

    Reapply "19205 Veeam jobs: Fix flapping when agent plugin retries..."
    CMK-34670
    
    This reverts commit 6fd1857854732323143bee4474385f365fafc080.
    
    Change-Id: Ifd7d2d19f68ee7ab7648a5e6e32ed0a1def97e0d

--- cmk/base/plugins/agent_based/veeam_jobs.py
+++ cmk/base/plugins/agent_based/veeam_jobs.py
@@ -8,6 +8,7 @@ from collections.abc import Mapping
 from dataclasses import dataclass
 
 from cmk.base.plugins.agent_based.agent_based_api.v1 import (
+    IgnoreResultsError,
     register,
     Result,
     Service,
@@ -36,8 +37,11 @@ def discovery_veeam_jobs(section: Mapping[str, Job | None]) -> DiscoveryResult:
 def monitoring_state(last_state: str, last_result: str, type_: str) -> State:
     if last_result == "None":
         if last_state in ["Starting", "Working", "Postprocessing"]:
-            return State.OK
+            # If result is absent and the job is still running, we cannot check the result yet.
+            # We try to get previous result from the last run.
+            raise IgnoreResultsError("Data not present at the moment")
         if last_state == "Idle" and type_ == "BackupSync":
+            # We assume that if the job is OK on idle and a backup sync job
             return State.OK
     if last_result == "Success":
         return State.OK

eaac4b307981fd9a4638c597e9f441050f400f3f
Lars Michelsen <lars.michelsen@checkmk.com>
2026-09-14 14:58:34 +0200

    Setup: Inline check_icmp_params into its two callers
    The helper shared four valuespec entries between the ICMP active check and
    the ping_levels rule set. The indirection is not worth the shared definition,
    so inline it and drop the helper from cmk.gui.plugins.wato.utils.
    
    Change-Id: I6421bc7ef85e991b4e34af47d93c342eb27b6996

--- cmk/gui/plugins/wato/utils/__init__.py
+++ cmk/gui/plugins/wato/utils/__init__.py
@@ -4,9 +4,7 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 """Module to hold shared code for Setup internals and the Setup plugins"""
 
-from cmk.gui.i18n import _
 from cmk.gui.valuespec import CascadingDropdown as CascadingDropdown
-from cmk.gui.valuespec import DictionaryEntry, Float, Integer, Percentage, Tuple
 from cmk.gui.valuespec import FixedValue as FixedValue
 from cmk.gui.valuespec import ListOf as ListOf
 from cmk.gui.valuespec import RegExp as RegExp
@@ -81,59 +79,3 @@ from cmk.gui.watolib.rulespecs import rulespec_registry as rulespec_registry
 from cmk.gui.watolib.rulespecs import RulespecGroup as RulespecGroup
 from cmk.gui.watolib.rulespecs import RulespecSubGroup as RulespecSubGroup
 from cmk.gui.watolib.rulespecs import TimeperiodValuespec as TimeperiodValuespec
-
-
-def check_icmp_params() -> list[DictionaryEntry]:
-    return [
-        (
-            "rta",
-            Tuple(
-                title=_("Round trip average"),
-                elements=[
-                    Float(title=_("Warning if above"), unit="ms", default_value=200.0),
-                    Float(title=_("Critical if above"), unit="ms", default_value=500.0),
-                ],
-            ),
-        ),
-        (
-            "loss",
-            Tuple(
-                title=_("Packet loss"),
-                help=_(
-                    "When the percentage of lost packets is equal or greater then "
-                    "this level, then the according state is triggered. The default for critical "
-                    "is 100%. That means that the check is only critical if <b>all</b> packets "
-                    "are lost."
-                ),
-                elements=[
-                    Percentage(title=_("Warning at"), default_value=80.0),
-                    Percentage(title=_("Critical at"), default_value=100.0),
-                ],
-            ),
-        ),
-        (
-            "packets",
-            Integer(
-                title=_("Number of packets"),
-                help=_(
-                    "Number ICMP echo request packets to send to the target host on each "
-                    "check execution. All packets are sent directly on check execution. Afterwards "
-                    "the check waits for the incoming packets."
-                ),
-                minvalue=1,
-                maxvalue=20,
-                default_value=5,
-            ),
-        ),
-        (
-            "timeout",
-            Integer(
-                title=_("Total timeout of check"),
-                help=_(
-                    "After this time (in seconds) the check is aborted, regardless "
-                    "of how many packets have been received yet."
-                ),
-                minvalue=1,
-            ),
-        ),
-    ]

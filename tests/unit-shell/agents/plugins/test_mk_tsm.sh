afae9affd4a851031f55f9c5d9d699068b9a7e91
Dennis Schmalacker <dennis.schmalacker@checkmk.com>
2026-09-15 13:31:54 +0200

    19148 FIX mk_tsm: Fix plug-in failing on AIX
    On AIX '/usr/bin/ksh' is a ksh88, whose documented redirections do not
    include the here-string '<<<'. The plug-in therefore aborted with a
    syntax error and delivered no data at all.
    
    Let shellcheck hold the file to POSIX sh ('shellcheck shell=sh');
    
    SUP-27472
    
    Change-Id: I580bb89d5c6d2240c2f4963f903423bc7b3263f4

--- tests/unit-shell/agents/plugins/test_mk_tsm.sh
+++ tests/unit-shell/agents/plugins/test_mk_tsm.sh
@@ -6,12 +6,24 @@
 # shellcheck source=agents/plugins/mk_tsm
 MK_SOURCE_ONLY=true source "${UNIT_SH_PLUGINS_DIR}/mk_tsm"
 
-test_export_env_resilient_against_command_injection() {
+# The plug-in has to keep the loop in export_extracted_env out of a subshell, which
+# rules out the obvious 'sed ... | while read'. Both tests below pin that down; that
+# the file stays free of bashisms is checked by shellcheck (shell=sh).
 
-    # whatch out: this will fail if you change "while..do..done <<< input" to "input | while..do..done" in the plugin.
+test_extracted_env_is_visible_to_the_caller() {
+    unset DSMSERV_DIR
+
+    export_extracted_env " dsmserv _=/usr/bin/dsmserv DSMSERV_DIR=/foobar_17g LC__FASTMSG=true"
+
+    assertEquals "/foobar_17g" "${DSMSERV_DIR}"
+}
+
+test_extracted_env_is_not_executed() {
+    unset DSMSERV_CONFIG
+
     export_extracted_env " dsmserv _=/usr/bin/dsmserv FOO=bar DSMSERV_CONFIG=/foobar;fail_if_this_is_a_command LC__FASTMSG=true"
-    assertEquals "/foobar;fail_if_this_is_a_command" "${DSMSERV_CONFIG}"
 
+    assertEquals "/foobar;fail_if_this_is_a_command" "${DSMSERV_CONFIG}"
 }
 
 # shellcheck disable=SC1090 # Can't follow

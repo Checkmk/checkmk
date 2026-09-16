25a3a87faf9692149e7c58e3350ae7dbe4c5a0ad
Simon Jess <simon.jess@checkmk.com>
2026-09-16 11:52:33 +0200

    gui: type the inventory history single spec
    Mirrors VisualInfo.single_spec, whose own bare ValueSpec is out of scope here.
    
    Change-Id: I67505089d640a089c5af612af762e8457f174264

--- cmk/gui/inventory/_visuals.py
+++ cmk/gui/inventory/_visuals.py
@@ -3,13 +3,11 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="type-arg"
-
 
 from typing import override
 
 from cmk.gui.i18n import _
-from cmk.gui.valuespec import ValueSpec
+from cmk.gui.valuespec import DictionaryEntry
 from cmk.gui.visuals.filter.components import FilterComponent
 from cmk.gui.visuals.info import VisualInfo
 
@@ -32,7 +30,7 @@ class VisualInfoInventoryHistory(VisualInfo):
 
     @property
     @override
-    def single_spec(self) -> list[tuple[str, ValueSpec]]:
+    def single_spec(self) -> list[DictionaryEntry]:
         return []
 
     @override

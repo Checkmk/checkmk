b9425780c6dd810ea3d68449cab5a7cadffccc27
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-09-22 12:54:01 +0200

    22476 pse_poe: let an unknown status reach the check
    The parser rejected any operational status outside on/off/faulty, so the
    check's own sanity check for exactly that value never ran.
    
    Crash-Group-ID: 4216
    Jira: CMK-39533
    Change-Id: I10f7fabb25669fc8ad44db01bc5996cccfc363c9

--- cmk/plugins/poe/agent_based/pse_poe.py
+++ cmk/plugins/poe/agent_based/pse_poe.py
@@ -21,7 +21,7 @@ from cmk.agent_based.v2 import (
 )
 
 # TODO: migrate pse_poe to packages/cmk-plugins and update this import to cmk.plugins.lib.poe
-from cmk.plugins.poe.lib import check_poe_data, PoeStatus, PoeValues
+from cmk.plugins.poe.lib import check_poe_data, poe_status_from_device, PoeValues
 
 # We fetch the following columns from SNMP:
 # 2 pethMainPsePower (The nominal power of the PSE expressed in Watts)
@@ -50,7 +50,7 @@ def parse_pse_poe(string_table: StringTable) -> Section:
         poe_dict[str(oid_end)] = PoeValues(
             poe_max=int(poe_max),
             poe_used=int(poe_used),
-            poe_status=PoeStatus(int(pse_op_status)),
+            poe_status=poe_status_from_device(int(pse_op_status)),
             poe_status_detail=None,
         )
     return poe_dict

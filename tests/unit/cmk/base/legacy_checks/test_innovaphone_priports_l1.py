293d480aa32cebabddf1b977a4a0931911bc8dd5
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-09-23 13:30:29 +0200

    test(innovaphone): add xfail for crash group 4100
    Change-Id: I1d13e0ccca15afb76ca938de73f5f91a65e5a0eb

---
+++ tests/unit/cmk/base/legacy_checks/test_innovaphone_priports_l1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+import time_machine
+
+from cmk.base.legacy_checks import innovaphone_priports_l1
+
+
+@pytest.mark.xfail(strict=True, reason="Crash report 4e70a284: KeyError 0")
+@time_machine.travel(60.0)
+def test_check_innovaphone_priports_l1_unknown_state(monkeypatch: pytest.MonkeyPatch) -> None:
+    # The device reported a port state that is neither Down (1) nor UP (2)
+    parsed = innovaphone_priports_l1.parse_innovaphone_priports_l1([["0", "0", "0", "0"]])
+    value_store = {"innovaphone_priports_l1.0": (50.0, 0)}
+    monkeypatch.setattr(innovaphone_priports_l1, "get_value_store", lambda: value_store)
+
+    assert list(
+        innovaphone_priports_l1.check_innovaphone_priports_l1("0", {"err_slip_count": 0}, parsed)
+    ) == [(3, "Current state is unknown (0)")]

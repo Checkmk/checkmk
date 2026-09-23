10afe78597669a91786354d0e5a45cffb6b93036
Christopher Krause <christopher.krause@checkmk.com>
2026-09-23 11:10:33 +0200

    test_mkbackup: freeze time in check tests
    JIRA-Ref: CMK-38355
    Change-Id: I4b138440c08cedbe5bf49ac106466a8e400648e0

--- tests/unit/cmk/plugins/checkmk/agent_based/test_mkbackup.py
+++ tests/unit/cmk/plugins/checkmk/agent_based/test_mkbackup.py
@@ -260,7 +260,7 @@ def test_mkbackup(info: StringTable, expect_check_result: bool) -> None:
 def test_check_mkbackup_ok_when_started() -> None:
     now = datetime(1970, 1, 1, tzinfo=UTC)
     started = now - timedelta(minutes=5)
-    with time_machine.travel(now):
+    with time_machine.travel(now, tick=False):
         results = list(
             check_mkbackup(
                 JobData(state="started", started=started.timestamp(), bytes_per_second=10)
@@ -279,7 +279,7 @@ def test_check_mkbackup_ok_when_running() -> None:
     now = datetime(1970, 1, 1, tzinfo=UTC)
     started = now - timedelta(minutes=5)
     job_state = JobData(state="running", started=started.timestamp(), bytes_per_second=10)
-    with time_machine.travel(now):
+    with time_machine.travel(now, tick=False):
         results = list(check_mkbackup(job_state))
 
     assert (
@@ -361,7 +361,7 @@ def test_check_mkbackup_ok_when_finished_and_next_schedule_is_on_time(
     started = datetime(1970, 1, 1, tzinfo=UTC)
     finished = started + timedelta(seconds=30)
 
-    with time_machine.travel(started):
+    with time_machine.travel(started, tick=False):
         results = list(
             check_mkbackup(
                 JobData(
@@ -398,7 +398,7 @@ def test_check_mkbackup_crit_when_finished_and_next_schedule_is_late(
     started = now - timedelta(days=7)
     finished = started + timedelta(seconds=30)
 
-    with time_machine.travel(now):
+    with time_machine.travel(now, tick=False):
         results = list(
             check_mkbackup(
                 JobData(

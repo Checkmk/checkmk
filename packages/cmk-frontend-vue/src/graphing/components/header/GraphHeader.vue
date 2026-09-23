9213a960d4f1fb025b54aa40cdfb2c96a15fae08
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2026-09-23 08:32:20 +0200

    Revert "igloo: remove the stray indent from the graph values options"
    This reverts commit a1c8c828893fa6472762cd11fc5fba941c1d666d.
    
    Reason for revert: see https://tribe29.slack.com/archives/C085MHGLPJ6/p1790144984373089
    
    Change-Id: I792fe6b526756de6350b236083b10f27bf684c76

--- packages/cmk-frontend-vue/src/graphing/components/header/GraphHeader.vue
+++ packages/cmk-frontend-vue/src/graphing/components/header/GraphHeader.vue
@@ -173,7 +173,6 @@ const resolutionLabel = computed(() => {
           :options="consolidationOptions"
           :label="_t('Graph values')"
           required
-          floating
         />
       </template>
       <span v-if="showTimestamp && timeRange" class="graphing-graph-header__timestamp">

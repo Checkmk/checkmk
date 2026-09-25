724c7ff20d5a4ba23e63d074adc81a68ba7b8d5e
Benjamin Knapp <benjamin.knapp@checkmk.com>
2026-09-25 09:00:52 +0200

    Add xfail test for crash group 4838
    Change-Id: I0f109011a88806eee0642cf0a359de9603e53511

---
+++ tests/unit/cmk/plugins/cisco/agent_based/test_cisco_cpu.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Checkmk GmbH - License: GNU General Public License v2
+# This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
+# conditions defined in the file COPYING, which is part of this source code package.
+
+import pytest
+
+from cmk.agent_based.v2 import Metric, Result, State
+from cmk.checkengine.specs.parameters import Parameters
+from cmk.plugins.cisco.agent_based.cisco_cpu import check_cisco_cpu, Section
+
+
+@pytest.mark.xfail(
+    strict=True,
+    raises=ValueError,
+    reason="Crash report cd83e33c-b02c-11f1-bac8-145a415bde61: ValueError",
+)
+def test_check_cisco_cpu_applies_levels_from_rule_parameters() -> None:
+    # The check engine passes the rule as an immutable Parameters mapping, not a dict.
+    params = Parameters({"util": (80.0, 90.0)})
+
+    assert list(check_cisco_cpu(params, Section(old_oid="", new_oid="85"))) == [
+        Result(
+            state=State.WARN,
+            summary="Utilization in the last 5 minutes: 85.00% (warn/crit at 80.00%/90.00%)",
+        ),
+        Metric("util", 85.0, levels=(80.0, 90.0), boundaries=(0.0, 100.0)),
+    ]
